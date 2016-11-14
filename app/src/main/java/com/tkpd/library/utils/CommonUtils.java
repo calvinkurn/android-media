07b6a94870df0f1b532b1f976459364aecb68f6e
nisie <nathania.sutedja@tokopedia.com>
2016-11-14 11:02:40 +0700

    Fix conflict

--- app/src/main/java/com/tkpd/library/utils/CommonUtils.java
+++ app/src/main/java/com/tkpd/library/utils/CommonUtils.java
@@ -532,12 +532,6 @@ public class CommonUtils {
 		mgr.hideSoftInputFromWindow(windowToken, 0);
 	}
 
-	public static void forceShowKeyboard(Context context){
-		((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE))
-				.toggleSoftInput(InputMethodManager.SHOW_FORCED,
-						InputMethodManager.HIDE_IMPLICIT_ONLY);
-	}
-
 	public static double round(double value, int places) {
 		if (places < 0) throw new IllegalArgumentException();
 
