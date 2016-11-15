312a98906e2f835d9a182e9875feb2f631034b56
Fatahillah Alvarisi <alvarisi01@gmail.com>
2016-11-15 14:06:14 +0700

    add adapter for shipment and shippingpackage

---
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/cart/adapter/ShipmentCartAdapter.java
@@ -0,0 +1,61 @@
+package com.tokopedia.transaction.cart.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+
+import com.tokopedia.transaction.cart.model.calculateshipment.Shipment;
+
+import java.util.ArrayList;
+
+/**
+ * @author  by alvarisi on 11/11/16.
+ */
+
+public class ShipmentCartAdapter extends ArrayAdapter<Shipment> {
+    private ArrayList<Shipment> mShipments;
+    private static class ViewHolder {
+        TextView shipment;
+    }
+
+    public static ShipmentCartAdapter newInstance(Context context){
+        ArrayList<Shipment> shipments = new ArrayList<>();
+        return new ShipmentCartAdapter(context, shipments);
+    }
+
+    private ShipmentCartAdapter(Context context, ArrayList<Shipment> shipments){
+        super(context, android.R.layout.simple_list_item_1, shipments);
+        this.mShipments = shipments;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        Shipment shipment = getItem(position);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(android.R.layout.select_dialog_item, parent, false);
+            viewHolder.shipment = (TextView) convertView.findViewById(android.R.id.text1);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.shipment.setText(shipment.getShipmentName());
+
+        return convertView;
+    }
+
+    public void setAdapterData(ArrayList<Shipment> shipments){
+        this.mShipments.clear();
+        this.mShipments.addAll(shipments);
+        notifyDataSetChanged();
+    }
+}
