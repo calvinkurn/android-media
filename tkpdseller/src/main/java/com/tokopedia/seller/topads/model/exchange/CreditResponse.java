69967ea0aede44149eaa6e37589616c3ebaf1737
nathaniel <nathaniel.khuana@tokopedia.com>
2016-11-16 11:12:18 +0700

    Migrate topads to new structure

---
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/model/exchange/CreditResponse.java
@@ -0,0 +1,38 @@
+package com.tokopedia.seller.topads.model.exchange;
+
+/**
+ * Created by zulfikarrahman on 11/4/16.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.tokopedia.seller.topads.model.data.DataCredit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditResponse {
+
+    @SerializedName("data")
+    @Expose
+    private List<DataCredit> data = new ArrayList<DataCredit>();
+
+    /**
+     *
+     * @return
+     * The data
+     */
+    public List<DataCredit> getData() {
+        return data;
+    }
+
+    /**
+     *
+     * @param data
+     * The data
+     */
+    public void setData(List<DataCredit> data) {
+        this.data = data;
+    }
+
+}
