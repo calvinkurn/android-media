d75b959ba7780f7d640f0b0ddd45d370c28dfd9e
nathaniel <nathaniel.khuana@tokopedia.com>
2016-11-22 10:14:16 +0700

    Initial topads dashboard page

---
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/view/activity/TopAdsDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.tokopedia.seller.topads.view.activity;
+
+import com.tokopedia.core.app.TActivity;
+import com.tokopedia.core.app.TkpdActivity;
+
+/**
+ * Created by Nathaniel on 11/22/2016.
+ */
+
+public class TopAdsDashboardActivity extends TActivity {
+
+    @Override
+    public String getScreenName() {
+        return null;
+    }
+}
