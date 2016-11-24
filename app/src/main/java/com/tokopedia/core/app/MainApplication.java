f3474e211c8f910e8e5bb937ac973c8d1cb85883
Erry Suprayogi <erry.suprayogi@gmail.com>
2016-11-24 15:23:28 +0700

    Redesign Category Menu Grid (cherry picked from commit 97ef36a)

--- app/src/main/java/com/tokopedia/core/app/MainApplication.java
+++ app/src/main/java/com/tokopedia/core/app/MainApplication.java
@@ -89,6 +89,7 @@ public class MainApplication extends TkpdMultiDexApplication {
         Localytics.autoIntegrate(this);
 
         InstabugHelper.initInstabug(this);
+        MultiDex.install(this);
     }
 
 
