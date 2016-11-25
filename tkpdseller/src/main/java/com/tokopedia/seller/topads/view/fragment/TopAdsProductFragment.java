7e4ff72b15f21ce0c6b3c1774b1d7b8de58cff05
nathaniel <nathaniel.khuana@tokopedia.com>
2016-11-25 17:02:06 +0700

    Implementing load statistic summary to UI

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsProductFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsProductFragment.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.TextView;
 
 import com.tkpd.library.utils.Logger;
 import com.tokopedia.core.app.BasePresenterFragment;
 import com.tokopedia.seller.R;
+import com.tokopedia.seller.R2;
 import com.tokopedia.seller.topads.model.data.Summary;
 import com.tokopedia.seller.topads.presenter.TopAdsProductFragmentPresenterImpl;
 import com.tokopedia.seller.topads.view.listener.TopAdsProductFragmentListener;
 
 import java.util.Calendar;
+
+import butterknife.Bind;
 
 /**
  * Created by Nisie on 5/9/16.
@@ -20,6 +24,24 @@ import java.util.Calendar;
 public class TopAdsProductFragment extends BasePresenterFragment<TopAdsProductFragmentPresenterImpl> implements TopAdsProductFragmentListener {
 
     private static String TAG = TopAdsProductFragment.class.getSimpleName();
+
+    @Bind(R2.id.layout_top_ads_info_text_impression)
+    View impressionInfoLayout;
+
+    @Bind(R2.id.layout_top_ads_info_text_click)
+    View clickInfoLayout;
+
+    @Bind(R2.id.layout_top_ads_info_text_ctr)
+    View ctrInfoLayout;
+
+    @Bind(R2.id.layout_top_ads_info_text_conversion)
+    View conversionInfoLayout;
+
+    @Bind(R2.id.layout_top_ads_info_text_average)
+    View averageMainInfoLayout;
+
+    @Bind(R2.id.layout_top_ads_info_text_cost)
+    View costInfoLayout;
 
     public static TopAdsProductFragment createInstance() {
         TopAdsProductFragment fragment = new TopAdsProductFragment();
@@ -83,6 +105,7 @@ public class TopAdsProductFragment extends BasePresenterFragment<TopAdsProductFr
     @Override
     public void onSummaryLoaded(@NonNull Summary summary) {
         Logger.i(TAG, "Cost Summary: " + summary.getCostSum());
+        updateSummaryLayout(summary);
     }
 
     @Override
@@ -103,5 +126,18 @@ public class TopAdsProductFragment extends BasePresenterFragment<TopAdsProductFr
     @Override
     protected void setActionVar() {
 
+    }
+
+    private void updateSummaryLayout(Summary summary) {
+        updateInfoValue(impressionInfoLayout, String.valueOf(summary.getImpressionSum()));
+        updateInfoValue(clickInfoLayout, String.valueOf(summary.getClickSum()));
+        updateInfoValue(ctrInfoLayout, String.valueOf(summary.getCtrPercentage()));
+        updateInfoValue(conversionInfoLayout, String.valueOf(summary.getConversionSum()));
+        updateInfoValue(averageMainInfoLayout, String.valueOf(summary.getCostAvg()));
+        updateInfoValue(costInfoLayout, String.valueOf(summary.getCostSum()));
+    }
+
+    private void updateInfoValue(View layout, String value) {
+        ((TextView) layout.findViewById(R.id.text_view_content)).setText(value);
     }
 }
