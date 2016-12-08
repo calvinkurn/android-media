78e557ab3b86010a9ce8d452801d01bc57994364
nathaniel <nathaniel.khuana@tokopedia.com>
2016-12-08 17:46:20 +0700

    Adding progressbar when load credit payment

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsPaymentCreditFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsPaymentCreditFragment.java
@@ -3,14 +3,19 @@ package com.tokopedia.seller.topads.view.fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.tokopedia.core.app.TkpdFragment;
 import com.tokopedia.core.loyaltysystem.util.URLGenerator;
+import com.tokopedia.core.payment.fragment.DynamicPaymentFragment;
 import com.tokopedia.seller.R;
 import com.tokopedia.seller.R2;
 import com.tokopedia.seller.topads.constant.TopAdsConstant;
@@ -23,6 +28,8 @@ public class TopAdsPaymentCreditFragment extends TkpdFragment {
 
     @Bind(R2.id.webview)
     WebView webView;
+    @Bind(R2.id.progressbar)
+    ProgressBar progressBar;
 
     private DataCredit dataCredit;
 
@@ -40,7 +47,7 @@ public class TopAdsPaymentCreditFragment extends TkpdFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_top_ads_payment_credit, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_general_web_view, container, false);
         return view;
     }
 
@@ -48,11 +55,26 @@ public class TopAdsPaymentCreditFragment extends TkpdFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
+        progressBar.setIndeterminate(true);
         loadWeb();
     }
 
     private void loadWeb() {
+        webView.clearCache(true);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setDisplayZoomControls(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    if (progressBar != null) progressBar.setVisibility(View.GONE);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
         webView.loadUrl(URLGenerator.generateURLSessionLogin(Uri.encode(dataCredit.getProductUrl()), getActivity()));
         webView.setWebViewClient(new WebViewClient() {
             @Override
