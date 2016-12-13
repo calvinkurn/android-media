8c6bd700dabbeef60b88207302c08503f4a2b606
nisie <nathania.sutedja@tokopedia.com>
2016-12-13 16:56:07 +0700

    Fix version name for sellerapp

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
@@ -1,10 +1,13 @@
 package com.tokopedia.sellerapp;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.app.TkpdCoreListener;
 import com.tokopedia.core.drawer.DrawerVariable;
+import com.tokopedia.core.util.GlobalConfig;
 import com.tokopedia.core.util.HockeyAppHelper;
 import com.tokopedia.sellerapp.drawer.DrawerVariableSeller;
 
@@ -26,6 +29,13 @@ public class SellerMainApplication extends MainApplication implements TkpdCoreLi
         super.onCreate();
         HockeyAppHelper.setEnableDistribution(BuildConfig.ENABLE_DISTRIBUTION);
         com.tokopedia.core.util.GlobalConfig.APPLICATION_TYPE = SELLER_APPLICATION;
+
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            com.tokopedia.core.util.GlobalConfig.VERSION_NAME = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
