981324b2c6d814fd9bea9369176a28f1d45132c4
Rico Harisin <rico.harisin@tokopedia.com>
2017-01-03 13:46:58 +0700

    fix set up environment

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -18,6 +18,8 @@ function copyEnvVarsToGradleProperties {
         echo "artifactory_username=$ARTIFACTORY_USERNAME_ENV" >> $GRADLE_PROPERTIES
 	echo "artifactory_password=$ARTIFACTORY_PASSWORD_ENV" >> $GRADLE_PROPERTIES
 	echo "artifactory_url=$ARTIFACTORY_URL_ENV" >> $GRADLE_PROPERTIES
-	gradle wrapper --gradle-version 2.14.1
+	echo "Run Gradle wrapper task...."
+	gradle wrapper
+	echo "FINISH!"
     fi
 }
