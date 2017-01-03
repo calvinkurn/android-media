ee3396ed45914e992a58cd1b81e5421bc810fb96
Rico Harisin <rico.harisin@tokopedia.com>
2017-01-03 16:09:05 +0700

    fix script setup

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -46,8 +46,8 @@ function getGradle {
 
 function setupAndroidSdk {
     echo "Downloading build tools 25"
-    android update sdk --no-ui --all --filter "build-tools-25"
-    echo "FINISH!!"
+    mkdir "%ANDROID_HOME%\licenses"    
+    echo |set /p="8933bad161af4178b1185d1a37fbf41ea5269c55" > "%ANDROID_HOME%\licenses\android-sdk-license"
 }
 
 
