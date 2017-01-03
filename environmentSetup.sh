4fd0577b7146c19fdcd94a1a9ddfd86e0390ebe7
Rico Harisin <rico.harisin@tokopedia.com>
2017-01-03 14:20:19 +0700

    add setup gradle function

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -8,19 +8,31 @@ function copyEnvVarsToGradleProperties {
     if [ ! -f "$GRADLE_PROPERTIES" ]; then
         echo "Gradle Properties does not exist"
 
-	echo "Creating gradle folder"
-	mkdir $HOME/.gradle
+    	echo "Creating gradle folder"
+    	mkdir $HOME/.gradle
 
         echo "Creating Gradle Properties file..."
         touch $GRADLE_PROPERTIES
 
         echo "Writing ARTIFACTORY CREDENTIALS to gradle.properties..."
         echo "artifactory_username=$ARTIFACTORY_USERNAME_ENV" >> $GRADLE_PROPERTIES
-	echo "artifactory_password=$ARTIFACTORY_PASSWORD_ENV" >> $GRADLE_PROPERTIES
-	echo "artifactory_url=$ARTIFACTORY_URL_ENV" >> $GRADLE_PROPERTIES
-	echo "Run Gradle wrapper task...."
-	gradle wrapper --gradle-distribution-url https://services.gradle.org/distributions/gradle-2.14.1-all.zip
-	echo "FINISH!"
-	echo "Starting build now!"
+    	echo "artifactory_password=$ARTIFACTORY_PASSWORD_ENV" >> $GRADLE_PROPERTIES
+    	echo "artifactory_url=$ARTIFACTORY_URL_ENV" >> $GRADLE_PROPERTIES
     fi
 }
+
+function setupGradleWrapper {
+    echo "Delete old gradle wrapper"
+    rm gradle/wrapper/gradle-wrapper.properties
+    
+    echo "Creating new gradle wrapper"
+    touch gradle/wrapper/gradle-wrapper.properties
+
+    echo "distributionBase=GRADLE_USER_HOME" >> gradle/wrapper/gradle-wrapper.properties
+    echo "distributionPath=wrapper/dists" >> gradle/wrapper/gradle-wrapper.properties
+    echo "zipStoreBase=GRADLE_USER_HOME" >> gradle/wrapper/gradle-wrapper.properties
+    echo "zipStorePath=wrapper/dists" >> gradle/wrapper/gradle-wrapper.properties
+    echo "distributionUrl=https\://services.gradle.org/distributions/gradle-2.14.1-all.zip" >> gradle/wrapper/gradle-wrapper.properties
+}
+
+
