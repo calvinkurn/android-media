3562db7ce5957eb404c150d37077f599729fa62a
zulfikar rahman <rahman.zulfikar94@gmail.com>
2017-01-10 15:49:40 +0100

    implement view graph topads

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsStatisticFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/view/fragment/TopAdsStatisticFragment.java
@@ -3,7 +3,9 @@ package com.tokopedia.seller.topads.view.fragment;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.IntRange;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,8 @@ import com.tokopedia.seller.topads.model.data.Cell;
 import com.tokopedia.seller.topads.presenter.TopAdsStatisticPresenter;
 import com.tokopedia.seller.topads.presenter.TopAdsStatisticPresenterImpl;
 import com.tokopedia.seller.topads.view.activity.TopAdsStatisticActivity;
+import com.tokopedia.seller.topads.view.library.util.GrossGraphChartConfig;
+import com.tokopedia.seller.topads.view.library.util.KMNumbers;
 import com.tokopedia.seller.topads.view.library.williamchart.view.LineChartView;
 import com.tokopedia.seller.topads.view.listener.TopAdsStatisticActivityViewListener;
 import com.tokopedia.seller.topads.view.listener.TopAdsStatisticViewListener;
@@ -23,7 +27,10 @@ import com.tokopedia.seller.topads.view.listener.TopAdsStatisticViewListener;
 import java.util.List;
 
 import butterknife.BindArray;
+import butterknife.BindDrawable;
 import butterknife.BindView;
+
+import static java.security.AccessController.getContext;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,8 +43,14 @@ public abstract class TopAdsStatisticFragment extends BasePresenterFragment<TopA
     @BindView(R2.id.content_graph)
     LineChartView contentGraph;
 
+    @BindDrawable(R.drawable.oval_2_copy_6)
+    Drawable oval2Copy6;
+
     TopAdsStatisticActivityViewListener topAdsStatisticActivityViewListener;
+    private GrossGraphChartConfig grossGraphChartConfig;
     private List<Cell> cells;
+    private String[] mLabels;
+    private float[] mValues;
 
     public TopAdsStatisticFragment() {
         // Required empty public constructor
@@ -92,7 +105,42 @@ public abstract class TopAdsStatisticFragment extends BasePresenterFragment<TopA
 
     @Override
     protected void initView(View view) {
+        mLabels = generateLabels();
+        mValues = generateValues();
+        grossGraphChartConfig = new GrossGraphChartConfig(mLabels, mValues);
+        generateLineChart();
+    }
 
+    private void generateLineChart() {
+        if(cells != null) {
+            grossGraphChartConfig
+                    .setmLabels(mLabels)
+                    .setmValues(mValues, new XRenderer.XRendererListener() {
+                        @Override
+                        public boolean filterX(@IntRange(from = 0L) int i) {
+                            if (i == 0 || mValues.length - 1 == i)
+                                return true;
+
+                            if (mValues.length <= 15) {
+                                return true;
+                            }
+
+                            return indexToDisplay.contains(i);
+
+                        }
+                    })
+                    .setDotDrawable(oval2Copy6)
+                    .setTooltip(new Tooltip(getContext(),
+                            layoutTooltip,
+                            R.id.gm_stat_tooltip_textview,
+                            new StringFormatRenderer() {
+                                @Override
+                                public String formatString(String s) {
+                                    return KMNumbers.formatNumbers(Float.valueOf(s));
+                                }
+                            }))
+                    .buildChart(grossGraphChartConfig.buildLineChart(contentGraph));
+        }
     }
 
     @Override
@@ -122,4 +170,8 @@ public abstract class TopAdsStatisticFragment extends BasePresenterFragment<TopA
     }
 
     public abstract String getValueData(Cell cell);
+
+    protected abstract String[] generateLabels();
+
+    protected abstract float[] generateValues();
 }
