e76913380fb6e08ac39718b37f3384b22761d63a
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-14 11:51:57 +0700

    mapping class supported

--- tkpdcore/src/main/java/com/tokopedia/core/gcm/FCMMessagingService.java
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/FCMMessagingService.java
@@ -11,6 +11,7 @@ import com.tokopedia.core.ManageGeneral;
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.router.CustomerRouter;
 import com.tokopedia.core.router.SessionRouter;
+import com.tokopedia.core.router.home.HomeRouter;
 import com.tokopedia.core.router.home.SimpleHomeRouter;
 import com.tokopedia.core.session.presenter.SessionView;
 import com.tokopedia.core.shopinfo.ShopInfoActivity;
@@ -41,8 +42,9 @@ public class FCMMessagingService extends FirebaseMessagingService {
 
         CommonUtils.dumper(data.toString());
         cacheManager = new FCMCacheManager(this);
-        if (cacheManager.isAllowToHandleNotif(data)) {
+        if (isAllowedNotification(data)) {
             cacheManager.setCache(this);
+
             if (isDedicatedNotification(data)) {
                 handleDedicatedNotification(data);
             } else {
@@ -52,62 +54,27 @@ public class FCMMessagingService extends FirebaseMessagingService {
         mNotificationAnalyticsReceiver.onNotificationReceived(data);
     }
 
+    private boolean isAllowedNotification(Bundle data) {
+        return GCMUtils.isValidForSellerApp(GCMUtils.getCode(data))
+                && cacheManager.isAllowToHandleNotif(data)
+                && cacheManager.checkLocalNotificationAppSettings(
+                Integer.parseInt(data.getString(ARG_NOTIFICATION_CODE))
+        );
+    }
+
     private void handlePromotionNotification(Bundle data) {
-        switch (GCMUtils.getCode(data)) {
-            case TkpdState.GCMServiceState.GCM_HOT_LIST:
-//                createNotification(data, BrowseHotDetail.class);
-                break;
-            case TkpdState.GCMServiceState.GCM_UPDATE_NOTIFICATION:
-                mAppNotificationReceiverUIBackground.sendUpdateAppsNotification(data);
-                break;
-            case TkpdState.GCMServiceState.GCM_PROMO:
-                sendMarketingPromoCode(data);
-                break;
-            case TkpdState.GCMServiceState.GCM_GENERAL:
-                sendGeneralNotification(data);
-                break;
-            case TkpdState.GCMServiceState.GCM_CATEGORY:
-//                createNotification(data, BrowseCategory.class);
-                break;
-            case TkpdState.GCMServiceState.GCM_SHOP:
-                mAppNotificationReceiverUIBackground.createNotification(data, ShopInfoActivity.class);
-                break;
-            case TkpdState.GCMServiceState.GCM_DEEPLINK:
-                if (CustomerRouter.getDeeplinkClass() != null) {
-                    mAppNotificationReceiverUIBackground.createNotification(data, CustomerRouter.getDeeplinkClass());
-                } else {
-                    return;
-                }
-                break;
-            case TkpdState.GCMServiceState.GCM_CART:
-                if (SessionHandler.isV4Login(this))
-                    mAppNotificationReceiverUIBackground.createNotification(data, Cart.class);
-                break;
-            case TkpdState.GCMServiceState.GCM_WISHLIST:
-                if (SessionHandler.isV4Login(this)) {
-                    mAppNotificationReceiverUIBackground.createNotification(data, SimpleHomeRouter.getSimpleHomeActivityClass());
-                }
-                break;
-            case TkpdState.GCMServiceState.GCM_VERIFICATION:
-                if (SessionHandler.isV4Login(this)) {
-                    mAppNotificationReceiverUIBackground.createNotification(data, ManageGeneral.class);
-                } else {
-                    data.putInt("keylogin1", TkpdState.DrawerPosition.LOGIN);
-                    data.putInt("keylogin2", SessionView.HOME);
-                    Intent intent = SessionRouter.getLoginActivityIntent(getApplicationContext());
-                    mAppNotificationReceiverUIBackground.createNotification(data, intent.getClass());
-                }
-                break;
-        }
+        mAppNotificationReceiverUIBackground.prepareAndExecutePromoNotification(data);
+
     }
 
     private void handleDedicatedNotification(Bundle data) {
         if (SessionHandler.isV4Login(this)
                 && SessionHandler.getLoginID(this).equals(data.getString("to_user_id"))) {
+
             resetNotificationStatus(data);
 
             if (mActivitiesLifecycleCallbacks.isAppOnBackground()) {
-                mAppNotificationReceiverUIBackground.sendNotification(data);
+                mAppNotificationReceiverUIBackground.prepareAndExecuteDedicationNotification(data);
             } else {
                 NotificationReceivedListener listener =
                         (NotificationReceivedListener) mActivitiesLifecycleCallbacks.getLiveActivityOrNull();
@@ -117,12 +84,10 @@ public class FCMMessagingService extends FirebaseMessagingService {
                             == TkpdState.GCMServiceState.GCM_CART_UPDATE) {
                         listener.onRefreshCart(data.getInt("is_cart_exists", 0));
                     } else {
-                        mAppNotificationReceiverUIBackground.sendNotification(data);
+                        mAppNotificationReceiverUIBackground.prepareAndExecuteDedicationNotification(data);
                     }
                 } else {
-                    if (isDedicatedNotification(data)) {
-                        mAppNotificationReceiverUIBackground.sendNotification(data);
-                    }
+                    mAppNotificationReceiverUIBackground.prepareAndExecuteDedicationNotification(data);
                 }
             }
             cacheManager.resetCache(data);
@@ -135,15 +100,6 @@ public class FCMMessagingService extends FirebaseMessagingService {
     private boolean isDedicatedNotification(Bundle data) {
         return Integer.parseInt(data.getString(ARG_NOTIFICATION_CODE)) < 500;
     }
-
-    private void sendMarketingPromoCode(Bundle data) {
-        mAppNotificationReceiverUIBackground.sendNotification(data);
-    }
-
-    private void sendGeneralNotification(Bundle data) {
-        mAppNotificationReceiverUIBackground.sendNotification(data);
-    }
-
 
     private void resetNotificationStatus(Bundle data) {
         switch (Integer.parseInt(data.getString(ARG_NOTIFICATION_CODE))) {
