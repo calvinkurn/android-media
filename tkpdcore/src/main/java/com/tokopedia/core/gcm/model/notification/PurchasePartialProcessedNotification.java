593aee6b33ad456fcf393774a92528a438df8401
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-16 16:43:44 +0700

    add purchase partial processed notification

---
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/model/notification/PurchasePartialProcessedNotification.java
@@ -0,0 +1,36 @@
+package com.tokopedia.core.gcm.model.notification;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tokopedia.core.R;
+import com.tokopedia.core.router.transactionmodule.TransactionPurchaseRouter;
+
+import static com.tokopedia.core.gcm.Constants.ARG_NOTIFICATION_DESCRIPTION;
+
+/**
+ * Created by alvarisi on 1/16/17.
+ */
+
+public class PurchasePartialProcessedNotification extends BaseNotification {
+    protected PurchasePartialProcessedNotification(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void configureNotificationData(Bundle data) {
+        mNotificationPass.mIntent = NotificationUtils.configureGeneralIntent(
+                new Intent(mContext, TransactionPurchaseRouter.getPurchaseActivityClass())
+        );
+        mNotificationPass.classParentStack = TransactionPurchaseRouter.getPurchaseActivityClass();
+        mNotificationPass.title = mContext.getString(R.string.title_notif_purchase_partial_accepted);
+        mNotificationPass.ticker = data.getString(ARG_NOTIFICATION_DESCRIPTION);
+        mNotificationPass.description = data.getString(ARG_NOTIFICATION_DESCRIPTION);
+        Bundle bundle = new Bundle();
+        bundle.putInt(TransactionPurchaseRouter.EXTRA_STATE_TAB_POSITION,
+                TransactionPurchaseRouter.TAB_POSITION_PURCHASE_STATUS_ORDER);
+        mNotificationPass.extraData = bundle;
+        mNotificationPass.mIntent.putExtras(bundle);
+    }
+}
