8b9381b08ce3d08b4b47bcfc65a5f99595e10a21
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-16 17:41:21 +0700

    create base promo notifications

--- tkpdcore/src/main/java/com/tokopedia/core/gcm/utils/NotificationUtils.java
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/utils/NotificationUtils.java
@@ -1,6 +1,9 @@
 package com.tokopedia.core.gcm.utils;
 
 import android.content.Intent;
+import android.os.Bundle;
+
+import com.tokopedia.core.session.presenter.SessionView;
 
 /**
  * Created by alvarisi on 1/12/17.
@@ -11,6 +14,20 @@ public class NotificationUtils {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.putExtra("from_notif", true);
         intent.putExtra("unread", false);
+        return intent;
+    }
+
+    public static Intent configurePromoIntent(Intent intent, Bundle data){
+        if (data.getInt("keylogin1", -99) != -99) {
+            intent.putExtra(
+                    com.tokopedia.core.session.presenter.Session.WHICH_FRAGMENT_KEY,
+                    data.getInt("keylogin1")
+            );
+            intent.putExtra(
+                    SessionView.MOVE_TO_CART_KEY,
+                    data.getInt("keylogin2")
+            );
+        }
         return intent;
     }
 }
