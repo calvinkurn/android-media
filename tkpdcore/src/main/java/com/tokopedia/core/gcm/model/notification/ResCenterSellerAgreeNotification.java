5bc893acc06abdfc1b6339fac9b922972fe47712
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-16 16:52:07 +0700

    add rescenter seller agree notification

---
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/model/notification/ResCenterSellerAgreeNotification.java
@@ -0,0 +1,36 @@
+package com.tokopedia.core.gcm.model.notification;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tokopedia.core.R;
+import com.tokopedia.core.router.InboxRouter;
+import com.tokopedia.core.var.TkpdState;
+
+import static com.tokopedia.core.gcm.Constants.ARG_NOTIFICATION_DESCRIPTION;
+
+/**
+ * Created by alvarisi on 1/16/17.
+ */
+
+public class ResCenterSellerAgreeNotification extends BaseNotification {
+    protected ResCenterSellerAgreeNotification(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void configureNotificationData(Bundle data) {
+        mNotificationPass.mIntent = NotificationUtils.configureGeneralIntent(
+                new Intent(mContext, InboxRouter.getInboxResCenterActivityClass())
+        );
+        mNotificationPass.classParentStack = InboxRouter.getInboxResCenterActivityClass();
+        mNotificationPass.title = mContext.getString(R.string.title_notif_rescenter);
+        mNotificationPass.ticker = data.getString(ARG_NOTIFICATION_DESCRIPTION);
+        mNotificationPass.description = data.getString(ARG_NOTIFICATION_DESCRIPTION);
+        Bundle bundle = new Bundle();
+        bundle.putInt(InboxRouter.EXTRA_STATE_TAB_POSITION,
+                TkpdState.InboxResCenter.RESO_MINE);
+        mNotificationPass.mIntent.putExtras(bundle);
+    }
+}
