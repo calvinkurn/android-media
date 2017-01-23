6eba4a5d881fed5980f7f77c1a4c1099bca375da
Angga Prasetiyo <agapras@gmail.com>
2017-01-23 10:56:26 +0700

    prepare delete

--- tkpdcore/src/main/java/com/tokopedia/core/cart/model/GatewayList.java
+++ tkpdcore/src/main/java/com/tokopedia/core/cart/model/GatewayList.java
@@ -1,73 +1,73 @@
-
-package com.tokopedia.core.cart.model;
-
-import android.content.Context;
-
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-import com.tokopedia.core.R;
-
-public class GatewayList {
-
-    @SerializedName("toppay_flag")
-    @Expose
-    private String toppayFlag;
-    @SerializedName("gateway")
-    @Expose
-    private Integer gateway;
-    @SerializedName("gateway_image")
-    @Expose
-    private String gatewayImage;
-    @SerializedName("gateway_name")
-    @Expose
-    private String gatewayName;
-    @SerializedName("gateway_desc")
-    @Expose
-    private String gatewayDesc;
-
-    public String getToppayFlag() {
-        return toppayFlag;
-    }
-
-    public void setToppayFlag(String toppayFlag) {
-        this.toppayFlag = toppayFlag;
-    }
-
-    public Integer getGateway() {
-        return gateway;
-    }
-
-    public void setGateway(Integer gateway) {
-        this.gateway = gateway;
-    }
-
-    public String getGatewayImage() {
-        return gatewayImage;
-    }
-
-    public void setGatewayImage(String gatewayImage) {
-        this.gatewayImage = gatewayImage;
-    }
-
-    public String getGatewayName() {
-        return gatewayName;
-    }
-
-    public void setGatewayName(String gatewayName) {
-        this.gatewayName = gatewayName;
-    }
-
-    public String getFeeInformation(Context context) {
-        if (this.gatewayDesc != null) return gatewayDesc;
-        switch (gateway) {
-            case 10:
-                return context.getString(R.string.msg_tokopedia_indomaret_fee);
-            case 12:
-                return context.getString(R.string.installment_payment_label);
-            case 8:
-                return context.getString(R.string.msg_tokopedia_cc_fee);
-            default:
-                return context.getString(R.string.msg_tokopedia_free);
-        }
-    }
-}
+//
+//package com.tokopedia.core.cart.model;
+//
+//import android.content.Context;
+//
+//import com.google.gson.annotations.Expose;
+//import com.google.gson.annotations.SerializedName;
+//import com.tokopedia.core.R;
+//
+//public class GatewayList {
+//
+//    @SerializedName("toppay_flag")
+//    @Expose
+//    private String toppayFlag;
+//    @SerializedName("gateway")
+//    @Expose
+//    private Integer gateway;
+//    @SerializedName("gateway_image")
+//    @Expose
+//    private String gatewayImage;
+//    @SerializedName("gateway_name")
+//    @Expose
+//    private String gatewayName;
+//    @SerializedName("gateway_desc")
+//    @Expose
+//    private String gatewayDesc;
+//
+//    public String getToppayFlag() {
+//        return toppayFlag;
+//    }
+//
+//    public void setToppayFlag(String toppayFlag) {
+//        this.toppayFlag = toppayFlag;
+//    }
+//
+//    public Integer getGateway() {
+//        return gateway;
+//    }
+//
+//    public void setGateway(Integer gateway) {
+//        this.gateway = gateway;
+//    }
+//
+//    public String getGatewayImage() {
+//        return gatewayImage;
+//    }
+//
+//    public void setGatewayImage(String gatewayImage) {
+//        this.gatewayImage = gatewayImage;
+//    }
+//
+//    public String getGatewayName() {
+//        return gatewayName;
+//    }
+//
+//    public void setGatewayName(String gatewayName) {
+//        this.gatewayName = gatewayName;
+//    }
+//
+//    public String getFeeInformation(Context context) {
+//        if (this.gatewayDesc != null) return gatewayDesc;
+//        switch (gateway) {
+//            case 10:
+//                return context.getString(R.string.msg_tokopedia_indomaret_fee);
+//            case 12:
+//                return context.getString(R.string.installment_payment_label);
+//            case 8:
+//                return context.getString(R.string.msg_tokopedia_cc_fee);
+//            default:
+//                return context.getString(R.string.msg_tokopedia_free);
+//        }
+//    }
+//}
--- tkpdcore/src/main/java/com/tokopedia/core/cart/model/cancelcart/CancelCartData.java
+++ tkpdcore/src/main/java/com/tokopedia/core/cart/model/cancelcart/CancelCartData.java
@@ -1,56 +1,56 @@
-package com.tokopedia.core.cart.model.cancelcart;
-
-import com.tokopedia.core.cart.model.CartModel;
-
-/**
- * Created by Angga.Prasetiyo on 30/05/2016.
- */
-public class CancelCartData {
-    private static final String TAG = CancelCartData.class.getSimpleName();
-
-    private boolean isSuccessCancel;
-    private boolean isSuccessRefresh;
-    private CartModel cartData;
-    private String messageCancelCart = "";
-    private String messageRefreshCart = "";
-
-    public boolean isSuccessCancel() {
-        return isSuccessCancel;
-    }
-
-    public void setSuccessCancel(boolean successCancel) {
-        isSuccessCancel = successCancel;
-    }
-
-    public boolean isSuccessRefresh() {
-        return isSuccessRefresh;
-    }
-
-    public void setSuccessRefresh(boolean successRefresh) {
-        isSuccessRefresh = successRefresh;
-    }
-
-    public CartModel getCartData() {
-        return cartData;
-    }
-
-    public void setCartData(CartModel cartData) {
-        this.cartData = cartData;
-    }
-
-    public String getMessageCancelCart() {
-        return messageCancelCart;
-    }
-
-    public void setMessageCancelCart(String messageCancelCart) {
-        this.messageCancelCart = messageCancelCart;
-    }
-
-    public String getMessageRefreshCart() {
-        return messageRefreshCart;
-    }
-
-    public void setMessageRefreshCart(String messageRefreshCart) {
-        this.messageRefreshCart = messageRefreshCart;
-    }
-}
+//package com.tokopedia.core.cart.model.cancelcart;
+//
+//import com.tokopedia.core.cart.model.CartModel;
+//
+///**
+// * Created by Angga.Prasetiyo on 30/05/2016.
+// */
+//public class CancelCartData {
+//    private static final String TAG = CancelCartData.class.getSimpleName();
+//
+//    private boolean isSuccessCancel;
+//    private boolean isSuccessRefresh;
+//    private CartModel cartData;
+//    private String messageCancelCart = "";
+//    private String messageRefreshCart = "";
+//
+//    public boolean isSuccessCancel() {
+//        return isSuccessCancel;
+//    }
+//
+//    public void setSuccessCancel(boolean successCancel) {
+//        isSuccessCancel = successCancel;
+//    }
+//
+//    public boolean isSuccessRefresh() {
+//        return isSuccessRefresh;
+//    }
+//
+//    public void setSuccessRefresh(boolean successRefresh) {
+//        isSuccessRefresh = successRefresh;
+//    }
+//
+//    public CartModel getCartData() {
+//        return cartData;
+//    }
+//
+//    public void setCartData(CartModel cartData) {
+//        this.cartData = cartData;
+//    }
+//
+//    public String getMessageCancelCart() {
+//        return messageCancelCart;
+//    }
+//
+//    public void setMessageCancelCart(String messageCancelCart) {
+//        this.messageCancelCart = messageCancelCart;
+//    }
+//
+//    public String getMessageRefreshCart() {
+//        return messageRefreshCart;
+//    }
+//
+//    public void setMessageRefreshCart(String messageRefreshCart) {
+//        this.messageRefreshCart = messageRefreshCart;
+//    }
+//}
