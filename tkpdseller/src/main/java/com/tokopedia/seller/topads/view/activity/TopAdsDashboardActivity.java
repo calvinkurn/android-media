a453a2acc6eb5f772b5c855844534c52fe9ceaa9
nathaniel <nathaniel.khuana@tokopedia.com>
2017-01-23 14:16:30 +0700

    Remove fab speed dial to avoid crash when parsing xml

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/view/activity/TopAdsDashboardActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/view/activity/TopAdsDashboardActivity.java
@@ -1,26 +1,19 @@
 package com.tokopedia.seller.topads.view.activity;
 
 import android.app.Fragment;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.MenuItem;
-import android.view.View;
 
-import com.tkpd.library.ui.floatbutton.FabSpeedDial;
-import com.tkpd.library.ui.floatbutton.ListenerFabClick;
-import com.tkpd.library.ui.floatbutton.SimpleMenuListenerAdapter;
-import com.tokopedia.core.network.NetworkErrorHelper;
-import com.tokopedia.core.network.SnackbarRetry;
-import com.tokopedia.seller.R2;
 import com.tokopedia.core.app.BasePresenterActivity;
 import com.tokopedia.core.listener.GlobalMainTabSelectedListener;
+import com.tokopedia.core.network.NetworkErrorHelper;
+import com.tokopedia.core.network.SnackbarRetry;
 import com.tokopedia.seller.R;
+import com.tokopedia.seller.R2;
 import com.tokopedia.seller.topads.constant.TopAdsConstant;
-import com.tokopedia.seller.topads.constant.TopAdsExtraConstant;
 import com.tokopedia.seller.topads.presenter.TopAdsDatePickerPresenterImpl;
 import com.tokopedia.seller.topads.view.adapter.TopAdsDashboardPagerAdapter;
 import com.tokopedia.seller.topads.view.fragment.TopAdsDashboardFragment;
@@ -42,8 +35,6 @@ public class TopAdsDashboardActivity extends BasePresenterActivity implements To
     ViewPager viewPager;
     @BindView(R2.id.indicator)
     TabLayout indicator;
-    @BindView(R2.id.fab_speed_dial)
-    FabSpeedDial fabSpeedDial;
 
     private SnackbarRetry snackbarRetry;
     private TopAdsDashboardShopFragment dashboardShopFragment;
@@ -84,29 +75,6 @@ public class TopAdsDashboardActivity extends BasePresenterActivity implements To
         indicator.setOnTabSelectedListener(new GlobalMainTabSelectedListener(viewPager));
         indicator.addTab(indicator.newTab().setText(R.string.title_top_ads_product));
         indicator.addTab(indicator.newTab().setText(R.string.title_top_ads_store));
-        fabSpeedDial.setListenerFabClick(new ListenerFabClick() {
-            @Override
-            public void onFabClick() {
-                if (!fabSpeedDial.isShown()) {
-                    fabSpeedDial.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-        fabSpeedDial.setMenuListener(new SimpleMenuListenerAdapter() {
-            @Override
-            public boolean onMenuItemSelected(MenuItem menuItem) {
-                if (menuItem.getItemId() == R.id.action_add_promo_group) {
-                    Intent intent = new Intent(TopAdsDashboardActivity.this, TopAdsNewPromoActivity.class);
-                    intent.putExtra(TopAdsExtraConstant.EXTRA_NEW_PROMO_CHOICE, TopAdsExtraConstant.TYPE_NEW_PROMO_GROUP);
-                    startActivity(intent);
-                } else if (menuItem.getItemId() == R.id.action_add_promo_product) {
-                    Intent intent = new Intent(TopAdsDashboardActivity.this, TopAdsNewPromoActivity.class);
-                    intent.putExtra(TopAdsExtraConstant.EXTRA_NEW_PROMO_CHOICE, TopAdsExtraConstant.TYPE_NEW_PROMO_EXIST_GROUP);
-                    startActivity(intent);
-                }
-                return false;
-            }
-        });
         snackbarRetry = NetworkErrorHelper.createSnackbarWithAction(this, new NetworkErrorHelper.RetryClickedListener() {
             @Override
             public void onRetryClicked() {
