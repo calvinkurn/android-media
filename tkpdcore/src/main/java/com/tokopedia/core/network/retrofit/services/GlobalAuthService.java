f2607b5e92d4285826c8bc78efd12396bb372d7a
nathaniel <nathaniel.khuana@tokopedia.com>
2017-01-23 15:21:01 +0700

    Remove unused import

--- tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/services/GlobalAuthService.java
+++ tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/services/GlobalAuthService.java
@@ -3,25 +3,12 @@ package com.tokopedia.core.network.retrofit.services;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.tokopedia.core.BuildConfig;
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
-import com.tokopedia.core.network.retrofit.coverters.GeneratedHostConverter;
-import com.tokopedia.core.network.retrofit.coverters.StringResponseConverter;
-import com.tokopedia.core.network.retrofit.coverters.TkpdResponseConverter;
 import com.tokopedia.core.network.retrofit.interceptors.GlobalTkpdAuthInterceptor;
-import com.tokopedia.core.network.retrofit.interceptors.TkpdBaseInterceptor;
-
-import net.hockeyapp.android.metrics.model.Base;
 
 import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
