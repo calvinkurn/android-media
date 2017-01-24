4482e1b477b04c8fc8b587f3ee6f9e517919b9ef
Angga Prasetiyo <agapras@gmail.com>
2017-01-24 11:15:20 +0700

    prepare delete

--- tkpdcore/src/main/java/com/tokopedia/core/briepay/presenter/BRIePayInterface.java
+++ tkpdcore/src/main/java/com/tokopedia/core/briepay/presenter/BRIePayInterface.java
@@ -1,14 +1,14 @@
-package com.tokopedia.core.briepay.presenter;
-
-import android.app.DialogFragment;
-
-/**
- * Created by Default05 on 1/5/2016.
- */
-public interface BRIePayInterface {
-
-    void canceleBRIPayment(DialogFragment dialogFragment);
-    void verifyeBRIPayment(String tid);
-
-
-}
+//package com.tokopedia.core.briepay.presenter;
+//
+//import android.app.DialogFragment;
+//
+///**
+// * Created by Default05 on 1/5/2016.
+// */
+//public interface BRIePayInterface {
+//
+//    void canceleBRIPayment(DialogFragment dialogFragment);
+//    void verifyeBRIPayment(String tid);
+//
+//
+//}
