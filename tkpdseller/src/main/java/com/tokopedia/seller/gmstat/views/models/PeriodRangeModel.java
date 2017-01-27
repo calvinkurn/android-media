b8a67565383280453373d9bfd7c342f83313f210
noiz354 <noiz354@gmail.com>
2017-01-27 15:53:16 +0700

    remove parceler lib from GMStat. add testing to verify if working for BasePeriodModel and PeriodRangeModel. (PeriodRangeModelTest)

--- tkpdseller/src/main/java/com/tokopedia/seller/gmstat/views/models/PeriodRangeModel.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/gmstat/views/models/PeriodRangeModel.java
@@ -1,6 +1,6 @@
 package com.tokopedia.seller.gmstat.views.models;
 
-import org.parceler.Parcel;
+import android.os.Parcelable;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -13,8 +13,7 @@ import static com.tokopedia.seller.gmstat.views.models.StartOrEndPeriodModel.YES
  * Created by normansyahputa on 1/18/17.
  */
 
-@Parcel
-public class PeriodRangeModel extends BasePeriodModel {
+public class PeriodRangeModel extends BasePeriodModel implements Parcelable {
 
     public static final int TYPE = 1;
     public boolean isChecked;
@@ -61,4 +60,45 @@ public class PeriodRangeModel extends BasePeriodModel {
         cal.add(Calendar.DATE, daysAgo);
         return cal;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(android.os.Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+
+        dest.writeByte(this.isChecked ? (byte) 1 : (byte) 0);
+        dest.writeString(this.headerText);
+        dest.writeString(this.formatText);
+        dest.writeByte(this.isRange ? (byte) 1 : (byte) 0);
+        dest.writeInt(this.range);
+        dest.writeLong(this.startDate);
+        dest.writeLong(this.endDate);
+    }
+
+    protected PeriodRangeModel(android.os.Parcel in) {
+        super(in);
+        this.isChecked = in.readByte() != 0;
+        this.headerText = in.readString();
+        this.formatText = in.readString();
+        this.isRange = in.readByte() != 0;
+        this.range = in.readInt();
+        this.startDate = in.readLong();
+        this.endDate = in.readLong();
+    }
+
+    public static final Parcelable.Creator<PeriodRangeModel> CREATOR = new Parcelable.Creator<PeriodRangeModel>() {
+        @Override
+        public PeriodRangeModel createFromParcel(android.os.Parcel source) {
+            return new PeriodRangeModel(source);
+        }
+
+        @Override
+        public PeriodRangeModel[] newArray(int size) {
+            return new PeriodRangeModel[size];
+        }
+    };
 }
