319da619a9ef75f5a72890e7980e81c9da337331
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-31 14:34:08 +0700

    add deeplink module on transaction

---
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/deeplink/TransactionDeeplinkModule.java
@@ -0,0 +1,10 @@
+package com.tokopedia.transaction.deeplink;
+
+import com.airbnb.deeplinkdispatch.DeepLinkModule;
+
+/**
+ * Created by alvarisi on 1/31/17.
+ */
+@DeepLinkModule
+public class TransactionDeeplinkModule {
+}
