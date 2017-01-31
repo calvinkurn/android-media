84abc123e2a4482387f4d806d9c90ac6d074c89e
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-01-31 14:37:22 +0700

    add deeplink receiver for logging purpose for now, next to handle wild card

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -2,11 +2,15 @@ package com.tokopedia.tkpd;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 
+import com.airbnb.deeplinkdispatch.DeepLinkHandler;
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.router.home.HomeRouter;
 import com.tokopedia.core.util.GlobalConfig;
 import com.tokopedia.core.util.HockeyAppHelper;
+import com.tokopedia.tkpd.deeplink.DeepLinkReceiver;
 
 /**
  * Created by ricoharisin on 11/11/16.
@@ -20,5 +24,8 @@ public class ConsumerMainApplication extends ConsumerRouterApplication {
         GlobalConfig.VERSION_CODE = BuildConfig.VERSION_CODE;
         GlobalConfig.VERSION_NAME = BuildConfig.VERSION_NAME;
         super.onCreate();
+
+        IntentFilter intentFilter = new IntentFilter(DeepLinkHandler.ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new DeepLinkReceiver(), intentFilter);
     }
 }
