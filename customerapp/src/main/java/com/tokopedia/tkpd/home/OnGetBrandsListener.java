a6b9e636753139c0d6856f2e8d6eca324a0ce63b
Hafizh Herdi <hherdinaufal@gmail.com>
2017-02-03 16:54:56 +0700

    added interactoor

---
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/OnGetBrandsListener.java
@@ -0,0 +1,8 @@
+package com.tokopedia.tkpd.home;
+
+/**
+ * Created by Herdi_WORK on 03.02.17.
+ */
+
+public interface OnGetBrandsListener {
+}
---
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/presenter/BrandsPresenterImpl.java
@@ -0,0 +1,8 @@
+package com.tokopedia.tkpd.home.presenter;
+
+/**
+ * Created by Herdi_WORK on 03.02.17.
+ */
+
+public class BrandsPresenterImpl {
+}
