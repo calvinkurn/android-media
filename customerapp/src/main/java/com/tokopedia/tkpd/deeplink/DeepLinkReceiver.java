3544023cb71e24a5e030eee03884b78ca12c4831
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-02-07 15:05:56 +0700

    applink unmatched

--- customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeepLinkReceiver.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeepLinkReceiver.java
@@ -3,9 +3,11 @@ package com.tokopedia.tkpd.deeplink;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 import com.airbnb.deeplinkdispatch.DeepLinkHandler;
+import com.tokopedia.tkpd.deeplink.activity.DeepLinkActivity;
 
 /**
  * Created by alvarisi on 1/31/17.
@@ -21,8 +23,9 @@ public class DeepLinkReceiver extends BroadcastReceiver {
         if (intent.getBooleanExtra(DeepLinkHandler.EXTRA_SUCCESSFUL, false)) {
             Log.i(TAG, "Success deep linking: " + deepLinkUri);
         } else {
-            String errorMessage = intent.getStringExtra(DeepLinkHandler.EXTRA_ERROR_MESSAGE);
-            Log.e(TAG, "Error deep linking: " + deepLinkUri + " with error message +" + errorMessage);
+            Intent deeplinkIntent = new Intent(context, DeepLinkActivity.class);
+            deeplinkIntent.setData(Uri.parse(deepLinkUri));
+            context.startActivity(deeplinkIntent);
         }
     }
 }
