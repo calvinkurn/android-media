370c22793e68b8f8874c30cef76524076bded1f6
sebastianuskh <sebastianus.kurniawan@tokopedia.com>
2017-02-14 15:53:04 +0700

    revert butterknife session

--- tkpdsession/src/main/java/com/tokopedia/session/register/fragment/RegisterStep1Fragment.java
+++ tkpdsession/src/main/java/com/tokopedia/session/register/fragment/RegisterStep1Fragment.java
@@ -70,6 +70,7 @@ import rx.subscriptions.CompositeSubscription;
 public class RegisterStep1Fragment extends BasePresenterFragment<RegisterStep1Presenter>
         implements RegisterStep1ViewListener, RegisterConstant {
 
+    @BindView(R2.id.register_email)
     AutoCompleteTextView email;
 
     @BindView(R2.id.register_password)
@@ -163,8 +164,6 @@ public class RegisterStep1Fragment extends BasePresenterFragment<RegisterStep1Pr
     @Override
     protected void initView(View view) {
         final Typeface typeface = registerPassword.getTypeface();
-
-        email = (AutoCompleteTextView) view.findViewById(R.id.register_email);
 
 
         String sourceString = "Sudah punya akun? Masuk";
