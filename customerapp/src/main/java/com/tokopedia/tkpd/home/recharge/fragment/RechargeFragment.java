b4f2a9b38c44e18fc020cc03ecd867c3a2124fa4
Angga Prasetiyo <agapras@gmail.com>
2017-02-20 15:30:52 +0700

    merge

--- customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/fragment/RechargeFragment.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/fragment/RechargeFragment.java
@@ -614,8 +614,8 @@ public class RechargeFragment extends Fragment implements RechargeEditText.Recha
             RadioButton radioButton = new RadioButton(getActivity());
             radioButton.setId(i);
             radioButton.setText(operators.get(i).name);
-            radioButton.setTextSize(getResources().getDimension(R.dimen.text_size_xsmall)/
-                getResources().getDisplayMetrics().density);
+            radioButton.setTextSize(getResources().getDimension(R.dimen.text_size_xsmall) /
+                    getResources().getDisplayMetrics().density);
             radioButton.setTextColor(ContextCompat.getColor(getActivity(), R.color.grey_600));
             radGroup.addView(radioButton);
         }
