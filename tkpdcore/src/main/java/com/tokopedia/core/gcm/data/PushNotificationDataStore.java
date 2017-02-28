e0f3d2fe3279084ea442da56073b1652f6d4e5a0
Fatahillah Alvarisi <alvarisi01@gmail.com>
2017-02-28 18:08:16 +0700

    data store interface

---
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/data/PushNotificationDataStore.java
@@ -0,0 +1,36 @@
+package com.tokopedia.core.gcm.data;
+
+import com.tokopedia.core.gcm.domain.PushNotification;
+import com.tokopedia.core.gcm.data.entity.FCMTokenUpdateEntity;
+import com.tokopedia.core.gcm.model.DeviceRegistrationDataResponse;
+import com.tokopedia.core.gcm.model.FCMTokenUpdate;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by alvarisi on 2/21/17.
+ */
+
+public interface PushNotificationDataStore {
+    Observable<FCMTokenUpdateEntity> updateTokenServer(FCMTokenUpdate data);
+
+    Observable<DeviceRegistrationDataResponse> deviceRegistration();
+
+    Observable<Boolean> saveRegistrationDevice(String registrationDevice);
+
+    Observable<List<PushNotification>> getSavedPushNotification();
+
+    Observable<List<PushNotification>> getPushSavedPushNotification(String category);
+
+    Observable<List<PushNotification>> getPushSavedPushNotificationWithOrderBy(String category, boolean ascendant);
+
+    Observable<Boolean> deleteSavedPushNotificationByCategory(String category);
+
+    Observable<Boolean> deleteSavedPushNotification();
+
+    Observable<Boolean> savePushNotification(String category, String response, String customIndex);
+
+    Observable<Boolean> savePushNotification(String category, String response);
+}
