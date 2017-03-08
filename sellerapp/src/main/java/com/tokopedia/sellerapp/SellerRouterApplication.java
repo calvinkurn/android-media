0911a74c530eee1c30189dcb6fc47310ba0559d2
sebastianuskh <sebastianuskh@outlook.co.id>
2017-03-08 13:21:51 +0700

    error compile double implementation in seller router module

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -73,11 +73,6 @@ public class SellerRouterApplication extends MainApplication implements TkpdCore
     }
 
     @Override
-    public void goToMerchantRedirect(Context context) {
-
-    }
-
-    @Override
     public void goToHome(Context context) {
         Intent intent = new Intent(context,
                 SellerHomeActivity.class);
