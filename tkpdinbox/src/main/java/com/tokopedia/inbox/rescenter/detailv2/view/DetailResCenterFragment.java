b23d60adab49a26821f932e3a0566eac8aebf94f
hangnadi <hangnadi@gmail.com>
2017-03-09 13:39:48 +0700

    create class java and layout for native detail resolution center

---
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/detailv2/view/DetailResCenterFragment.java
@@ -0,0 +1,95 @@
+package com.tokopedia.inbox.rescenter.detailv2.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.tokopedia.core.app.BasePresenterFragment;
+import com.tokopedia.inbox.R;
+import com.tokopedia.inbox.rescenter.detailv2.view.listener.DetailResCenterFragmentView;
+import com.tokopedia.inbox.rescenter.detailv2.view.presenter.DetailResCenterFragmentPresenter;
+
+/**
+ * Created by hangnadi on 3/8/17.
+ */
+
+public class DetailResCenterFragment extends BasePresenterFragment<DetailResCenterFragmentPresenter>
+        implements DetailResCenterFragmentView {
+
+    private static final String EXTRA_PARAM_RESOLUTION_ID = "resolution_id";
+
+    public static DetailResCenterFragment createInstance(String resolutionID) {
+        DetailResCenterFragment fragment = new DetailResCenterFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(EXTRA_PARAM_RESOLUTION_ID, resolutionID);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    protected boolean isRetainInstance() {
+        return false;
+    }
+
+    @Override
+    protected void onFirstTimeLaunched() {
+
+    }
+
+    @Override
+    public void onSaveState(Bundle state) {
+
+    }
+
+    @Override
+    public void onRestoreState(Bundle savedState) {
+
+    }
+
+    @Override
+    protected boolean getOptionsMenuEnable() {
+        return false;
+    }
+
+    @Override
+    protected void initialPresenter() {
+
+    }
+
+    @Override
+    protected void initialListener(Activity activity) {
+
+    }
+
+    @Override
+    protected void setupArguments(Bundle arguments) {
+
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_detail_rescenter;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void setViewListener() {
+
+    }
+
+    @Override
+    protected void initialVar() {
+
+    }
+
+    @Override
+    protected void setActionVar() {
+
+    }
+
+
+}
