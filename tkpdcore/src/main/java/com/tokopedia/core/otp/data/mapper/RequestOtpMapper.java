5b340e5e6ebe1615dabf2c587fc3d6888fee6c49
stevenfredian <steven.fredian@tokopedia.com>
2017-03-10 10:19:26 +0700

    change endpoint of request otp on sq

---
+++ tkpdcore/src/main/java/com/tokopedia/core/otp/data/mapper/RequestOtpMapper.java
@@ -0,0 +1,45 @@
+package com.tokopedia.core.otp.data.mapper;
+
+import com.tokopedia.core.network.entity.otp.RequestOtpData;
+import com.tokopedia.core.network.retrofit.response.TkpdResponse;
+import com.tokopedia.core.otp.data.RequestOtpModel;
+
+import retrofit2.Response;
+import rx.functions.Func1;
+
+/**
+ * Created by nisie on 3/7/17.
+ */
+
+public class RequestOtpMapper implements Func1<Response<TkpdResponse>, RequestOtpModel> {
+    @Override
+    public RequestOtpModel call(Response<TkpdResponse> response) {
+        return mappingResponse(response);
+    }
+
+    private RequestOtpModel mappingResponse(Response<TkpdResponse> response) {
+        RequestOtpModel model = new RequestOtpModel();
+
+        if (response.isSuccessful()) {
+            if (!response.body().isError()) {
+                RequestOtpData data = response.body().convertDataObj(RequestOtpData.class);
+                model.setSuccess(true);
+                model.setRequestOtpData(data);
+            } else {
+                if (response.body().getErrorMessages() == null
+                        && response.body().getErrorMessages().isEmpty()) {
+                    model.setSuccess(false);
+                } else {
+                    model.setSuccess(false);
+                    model.setErrorMessage(response.body().getErrorMessageJoined());
+                }
+            }
+            model.setStatusMessage(response.body().getStatusMessageJoined());
+        } else {
+            model.setSuccess(false);
+        }
+        model.setResponseCode(response.code());
+        return model;
+    }
+
+}
