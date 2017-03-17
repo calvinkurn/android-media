a262875fb6f83376e67f13672f288df1063ff213
fatahillah alvarisi <alvarisi01@gmail.com>
2017-03-17 13:10:36 +0700

    proguard

--- customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeeplinkHandlerActivity.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeeplinkHandlerActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.airbnb.deeplinkdispatch.DeepLinkHandler;
+import com.airbnb.deeplinkdispatch.DeepLinkResult;
+import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.core.analytics.UnifyTracking;
 import com.tokopedia.core.deeplink.CoreDeeplinkModule;
 import com.tokopedia.core.deeplink.CoreDeeplinkModuleLoader;
@@ -30,7 +32,12 @@ public class DeeplinkHandlerActivity extends AppCompatActivity {
         if (getIntent() != null) {
             Intent intent = getIntent();
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            deepLinkDelegate.dispatchFrom(this, intent);
+            DeepLinkResult deepLinkResult = deepLinkDelegate.dispatchFrom(this, intent);
+            if (deepLinkResult.isSuccessful()){
+                CommonUtils.dumper(deepLinkResult.error());
+            }else {
+                CommonUtils.dumper(deepLinkResult.error());
+            }
             if (getIntent().getExtras() != null)
             {
                 Bundle bundle = getIntent().getExtras();
