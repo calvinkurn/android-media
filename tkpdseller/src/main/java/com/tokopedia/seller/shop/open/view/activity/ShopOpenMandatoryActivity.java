dbe80cb6e4e5aa710f00f6073653abf758a1d7d7
sebastianuskh <sebastianuskh@outlook.co.id>
2017-03-17 14:20:13 +0700

    create view for open shop location fragment

--- tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/activity/ShopOpenMandatoryActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/activity/ShopOpenMandatoryActivity.java
@@ -1,8 +1,76 @@
 package com.tokopedia.seller.shop.open.view.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.tokopedia.core.app.BasePresenterActivity;
+import com.tokopedia.seller.R;
+import com.tokopedia.seller.shop.open.view.presenter.ShopOpenMandatoryPresenter;
+import com.tokopedia.seller.shop.setting.view.fragment.ShopSettingLocationFragment;
+
 /**
  * Created by Nathaniel on 3/16/2017.
  */
 
-public class ShopOpenMandatoryActivity {
+public class ShopOpenMandatoryActivity extends BasePresenterActivity<ShopOpenMandatoryPresenter> {
+    private FrameLayout container;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void setupURIPass(Uri data) {
+
+    }
+
+    @Override
+    protected void setupBundlePass(Bundle extras) {
+
+    }
+
+    @Override
+    protected void initialPresenter() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_simple_fragment;
+    }
+
+    @Override
+    protected void initView() {
+        container = (FrameLayout) findViewById(R.id.container);
+    }
+
+    @Override
+    protected void setViewListener() {
+
+    }
+
+    @Override
+    protected void initVar() {
+        fragmentManager = getFragmentManager();
+
+    }
+
+    @Override
+    protected void setActionVar() {
+        Fragment fragment = fragmentManager.findFragmentByTag(ShopSettingLocationFragment.TAG);
+        if (fragment == null) {
+            fragment = ShopSettingLocationFragment.getInstance();
+        }
+        inflateFragment(fragment, false, ShopSettingLocationFragment.TAG);
+    }
+
+    private void inflateFragment(Fragment fragment, boolean isAddToBackStack, String tag) {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.container, fragment, tag);
+        if (isAddToBackStack) {
+            ft.addToBackStack(null);
+        }
+        ft.commit();
+    }
 }
--- tkpdseller/src/main/java/com/tokopedia/seller/shop/setting/view/fragment/ShopSettingLocationFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/shop/setting/view/fragment/ShopSettingLocationFragment.java
@@ -1,8 +1,87 @@
 package com.tokopedia.seller.shop.setting.view.fragment;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.tokopedia.core.app.BasePresenterFragment;
+import com.tokopedia.seller.R;
+
 /**
  * Created by Nathaniel on 3/16/2017.
  */
 
-public class ShopSettingLocationFragment {
+public class ShopSettingLocationFragment extends BasePresenterFragment {
+    public static final String TAG = "ShopSettingLocation";
+
+    public static ShopSettingLocationFragment getInstance() {
+        return new ShopSettingLocationFragment();
+    }
+
+    @Override
+    protected boolean isRetainInstance() {
+        return false;
+    }
+
+    @Override
+    protected void onFirstTimeLaunched() {
+
+    }
+
+    @Override
+    public void onSaveState(Bundle state) {
+
+    }
+
+    @Override
+    public void onRestoreState(Bundle savedState) {
+
+    }
+
+    @Override
+    protected boolean getOptionsMenuEnable() {
+        return false;
+    }
+
+    @Override
+    protected void initialPresenter() {
+
+    }
+
+    @Override
+    protected void initialListener(Activity activity) {
+
+    }
+
+    @Override
+    protected void setupArguments(Bundle arguments) {
+
+    }
+
+    @Override
+    protected int getFragmentLayout() {
+        return R.layout.fragment_shop_setting_location;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void setViewListener() {
+
+    }
+
+    @Override
+    protected void initialVar() {
+
+    }
+
+    @Override
+    protected void setActionVar() {
+
+    }
+
+
 }
