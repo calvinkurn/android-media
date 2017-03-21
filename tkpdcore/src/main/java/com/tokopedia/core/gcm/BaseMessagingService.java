339dfe70ffc8feaf8bbece93d4eb07fb4577e8b7
Hafizh Herdi <hherdinaufal@gmail.com>
2017-03-21 15:18:06 +0700

    revamp moengage to leverage fcm listener

--- tkpdcore/src/main/java/com/tokopedia/core/gcm/BaseMessagingService.java
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/BaseMessagingService.java
@@ -3,12 +3,15 @@ package com.tokopedia.core.gcm;
 import android.os.Bundle;
 
 import com.google.firebase.messaging.RemoteMessage;
+import com.moengage.pushbase.push.MoEngageNotificationUtils;
 import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.core.gcm.base.BaseNotificationMessagingService;
 import com.tokopedia.core.gcm.base.IAppNotificationReceiver;
 import com.tokopedia.core.router.SellerAppRouter;
 import com.tokopedia.core.router.home.HomeRouter;
 import com.tokopedia.core.util.GlobalConfig;
+
+import java.util.Map;
 
 /**
  * Created by alvarisi on 3/17/17.
@@ -30,7 +33,14 @@ public class BaseMessagingService extends BaseNotificationMessagingService {
             appNotificationReceiver = HomeRouter.getAppNotificationReceiver();
             if (appNotificationReceiver != null) {
                 appNotificationReceiver.init(getApplication());
-                appNotificationReceiver.onNotificationReceived(remoteMessage.getFrom(), data);
+
+                if(MoEngageNotificationUtils.isFromMoEngagePlatform(remoteMessage.getData()))
+                {
+                    appNotificationReceiver.onMoengageNotificationReceived(remoteMessage);
+                } else {
+                    appNotificationReceiver.onNotificationReceived(remoteMessage.getFrom(), data);
+                }
+
             }
         }
     }
