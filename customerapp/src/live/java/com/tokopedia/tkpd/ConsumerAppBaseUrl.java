3bdc012869cf705697c6ec8ed20628930ba27cb3
alvarisi <alvarisi01@gmail.com>
2017-03-27 11:54:03 +0700

    confirm booking request ride and recalculate fare estimate, still bugs on ui for back from booking and sliding panel

--- customerapp/src/live/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
+++ customerapp/src/live/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
@@ -7,20 +7,20 @@ package com.tokopedia.tkpd;
 @SuppressWarnings("WeakerAccess")
 public class ConsumerAppBaseUrl {
 
-    public static final String BASE_DOMAIN = "https://ws-staging.tokopedia.com/";
-    public static final String BASE_ACE_DOMAIN = "https://ace-staging.tokopedia.com/";
-    public static final String BASE_CLOVER_DOMAIN = "https://points-staging.tokopedia.com/";
-    public static final String BASE_TOPADS_DOMAIN = "https://ta-staging.tokopedia.com/";
-    public static final String BASE_MOJITO_DOMAIN = "https://mojito-staging.tokopedia.com/";
-    public static final String BASE_HADES_DOMAIN = "https://hades-staging.tokopedia.com";
-    public static final String BASE_RECHARGE_API_DOMAIN = "https://pulsa-api-staging.tokopedia.com/";
-    public static final String BASE_ACCOUNTS_DOMAIN = "https://accounts-staging.tokopedia.com/";
-    public static final String BASE_INBOX_DOMAIN = "https://inbox-staging.tokopedia.com";
-    public static final String BASE_JS_DOMAIN = "https://js-staging.tokopedia.com/";
-    public static final String BASE_KERO_DOMAIN = "https://kero-staging.tokopedia.com/";
-    public static final String BASE_JAHE_DOMAIN = "https://jahe-staging.tokopedia.com/";
-    public static final String BASE_PULSA_WEB_DOMAIN = "https://pulsa-staging.tokopedia.com";
+    public static final String BASE_DOMAIN = "https://ws.tokopedia.com/";
+    public static final String BASE_ACE_DOMAIN = "https://ace.tokopedia.com/";
+    public static final String BASE_CLOVER_DOMAIN = "https://points.tokopedia.com/";
+    public static final String BASE_TOPADS_DOMAIN = "https://ta.tokopedia.com/";
+    public static final String BASE_MOJITO_DOMAIN = "https://mojito.tokopedia.com/";
+    public static final String BASE_HADES_DOMAIN = "https://hades.tokopedia.com";
+    public static final String BASE_RECHARGE_API_DOMAIN = "https://pulsa-api.tokopedia.com/";
+    public static final String BASE_ACCOUNTS_DOMAIN = "https://accounts.tokopedia.com/";
+    public static final String BASE_INBOX_DOMAIN = "https://inbox.tokopedia.com";
+    public static final String BASE_JS_DOMAIN = "https://js.tokopedia.com/";
+    public static final String BASE_KERO_DOMAIN = "https://kero.tokopedia.com/";
+    public static final String BASE_JAHE_DOMAIN = "https://jahe.tokopedia.com";
+    public static final String BASE_PULSA_WEB_DOMAIN = "https://pulsa.tokopedia.com";
     public static final String BASE_GOLD_MERCHANT_DOMAIN = "https://goldmerchant.tokopedia.com";
-    public static final String BASE_WEB_DOMAIN = "https://staging.tokopedia.com/";
-    public static final String BASE_MOBILE_DOMAIN = "https://m-staging.tokopedia.com/";
+    public static final String BASE_WEB_DOMAIN = "https://www.tokopedia.com/";
+    public static final String BASE_MOBILE_DOMAIN = "https://m.tokopedia.com/";
 }
