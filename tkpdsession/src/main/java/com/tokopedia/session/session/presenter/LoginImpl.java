16292212c21566dcb6af0a9385be65686ada4b8a
stevenfredian <steven.fredian@tokopedia.com>
2017-03-27 16:58:03 +0700

    implement smartlock

--- tkpdsession/src/main/java/com/tokopedia/session/session/presenter/LoginImpl.java
+++ tkpdsession/src/main/java/com/tokopedia/session/session/presenter/LoginImpl.java
@@ -20,9 +20,7 @@ import com.tokopedia.core.analytics.AppEventTracking;
 import com.tokopedia.core.analytics.AppScreen;
 import com.tokopedia.core.analytics.ScreenTracking;
 import com.tokopedia.core.analytics.handler.UserAuthenticationAnalytics;
-import com.tokopedia.core.analytics.nishikino.Nishikino;
 import com.tokopedia.core.gcm.GCMHandler;
-import com.tokopedia.core.network.retrofit.utils.AuthUtil;
 import com.tokopedia.core.service.DownloadService;
 import com.tokopedia.core.service.constant.DownloadServiceConstant;
 import com.tokopedia.core.session.model.CreatePasswordModel;
@@ -38,6 +36,7 @@ import com.tokopedia.core.util.EncoderDecoder;
 import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.var.FacebookContainer;
 import com.tokopedia.session.R;
+import com.tokopedia.session.register.activity.SmartLockActivity;
 import com.tokopedia.session.session.fragment.LoginFragment;
 import com.tokopedia.session.session.interactor.LoginInteractor;
 import com.tokopedia.session.session.interactor.LoginInteractorImpl;
@@ -49,7 +48,6 @@ import org.parceler.Parcels;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -471,7 +469,7 @@ public class LoginImpl implements Login {
                             loginSecurityModel.getUser_id());
                 } else if (sessionHandler.isV4Login()) {// go back to home
                     loginView.triggerSaveAccount();
-                    loginView.destroyActivity();
+                    loginView.setSmartLock(SmartLockActivity.RC_SAVE);
                 } else if (data.getInt(DownloadService.VALIDATION_OF_DEVICE_ID, LoginEmailModel.INVALID_DEVICE_ID) == LoginEmailModel.INVALID_DEVICE_ID) {
 
                 }
