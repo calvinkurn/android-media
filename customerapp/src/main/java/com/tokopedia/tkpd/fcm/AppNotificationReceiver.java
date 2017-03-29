deaef7d525023c5897e1723909866e230fa34684
Hafizh Herdi <hherdinaufal@gmail.com>
2017-03-29 16:45:31 +0700

    handle push applinks from moengage and localytics

--- customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
@@ -3,10 +3,17 @@ package com.tokopedia.tkpd.fcm;
 import android.app.Application;
 import android.os.Bundle;
 
+import com.google.firebase.messaging.RemoteMessage;
+import com.tkpd.library.utils.CommonUtils;
+import com.tokopedia.core.gcm.Constants;
 import com.tokopedia.core.gcm.FCMCacheManager;
 import com.tokopedia.core.gcm.INotificationAnalyticsReceiver;
 import com.tokopedia.core.gcm.NotificationAnalyticsReceiver;
 import com.tokopedia.core.gcm.base.IAppNotificationReceiver;
+import com.tokopedia.core.gcm.utils.GCMUtils;
+import com.tokopedia.tkpd.ConsumerMainApplication;
+
+import java.util.Map;
 
 import rx.Observable;
 
@@ -29,7 +36,21 @@ public class AppNotificationReceiver implements IAppNotificationReceiver {
         mNotificationAnalyticsReceiver = new NotificationAnalyticsReceiver();
     }
 
+    @Override
+    public void onMoengageNotificationReceived(RemoteMessage message) {
+        CommonUtils.dumper("FCM messaging moengage " + message.getData().toString());
+        if(message.getData().containsKey(Constants.ARG_NOTIFICATION_APPLINK)){
+            Map<String, String> appLinkData = message.getData();
+            appLinkData.put("origin","moengage");
+            mAppNotificationReceiverUIBackground.notifyReceiverBackgroundMessage(Observable.just(GCMUtils.convertMap(appLinkData)));
+        }
+    }
+
     public void onNotificationReceived(String from, Bundle bundle) {
+        CommonUtils.dumper("FCM messaging " + bundle.toString());
+        if(bundle.containsKey(Constants.ARG_NOTIFICATION_APPLINK)) {
+            bundle.putString("origin", "moengage");
+        }
         mAppNotificationReceiverUIBackground.notifyReceiverBackgroundMessage(Observable.just(bundle));
         mNotificationAnalyticsReceiver.onNotificationReceived(Observable.just(bundle));
     }
