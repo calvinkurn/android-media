d1157bbf2797213527a470a6f4e4743b4f831d09
Hafizh Herdi <hherdinaufal@gmail.com>
2017-04-04 15:57:41 +0700

    revert changes on appnotifreceiver

--- customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
@@ -3,21 +3,14 @@ package com.tokopedia.tkpd.fcm;
 import android.app.Application;
 import android.os.Bundle;
 
-import com.google.firebase.messaging.RemoteMessage;
 import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.core.gcm.Constants;
-import com.tokopedia.core.gcm.FCMCacheManager;
 import com.tokopedia.core.gcm.INotificationAnalyticsReceiver;
 import com.tokopedia.core.gcm.NotificationAnalyticsReceiver;
 import com.tokopedia.core.gcm.base.IAppNotificationReceiver;
-import com.tokopedia.core.gcm.utils.GCMUtils;
-import com.tokopedia.tkpd.ConsumerMainApplication;
-
-import java.util.Map;
 
 import rx.Observable;
 
-import static com.tokopedia.core.gcm.Constants.ARG_NOTIFICATION_CODE;
 
 /**
  * Created by alvarisi on 1/17/17.
