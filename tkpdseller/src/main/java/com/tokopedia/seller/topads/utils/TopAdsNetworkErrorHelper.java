76f8e45fa682a6005f630b5d57f0d18c89966137
zulfikarrahman <rahman.zulfikar94@gmail.com>
2017-04-04 14:29:00 +0700

    depecrated topadsnetworkerrorhelper and remove library snackbar nispok

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/utils/TopAdsNetworkErrorHelper.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/utils/TopAdsNetworkErrorHelper.java
@@ -2,54 +2,36 @@ package com.tokopedia.seller.topads.utils;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 
-import com.nispok.snackbar.Snackbar;
-import com.nispok.snackbar.SnackbarManager;
-import com.nispok.snackbar.enums.SnackbarType;
-import com.nispok.snackbar.listeners.ActionClickListener;
 import com.tokopedia.core.network.NetworkErrorHelper;
+import com.tokopedia.core.network.SnackbarRetry;
+import com.tokopedia.seller.R;
 import com.tokopedia.seller.gmstat.utils.GMNetworkErrorHelper;
 
 /**
  * Created by normansyahputa on 2/25/17.
  */
 
+@Deprecated
 public class TopAdsNetworkErrorHelper extends GMNetworkErrorHelper {
 
-    private int textDescriptionColor = Color.WHITE;
-    private int actionColor = Color.GREEN;
-    private int backgroundColor = Color.BLACK;
+    SnackbarRetry snackbarRetry;
 
     public TopAdsNetworkErrorHelper(NetworkErrorHelper.RetryClickedListener listener, View rootView) {
         super(listener, rootView);
     }
 
     public void showSnackbar(String titleText, String actionText
-            , ActionClickListener onActionClickListener, Activity activity) {
-        SnackbarManager.show(
-                Snackbar.with(activity.getApplicationContext()) // context
-                        .type(SnackbarType.MULTI_LINE)
-                        .duration(Snackbar.SnackbarDuration.LENGTH_INDEFINITE)
-                        .textColor(textDescriptionColor)
-                        .actionColor(actionColor)
-                        .color(backgroundColor)
-                        .text(titleText) // text to display
-                        .actionLabel(actionText) // action button label
-                        .actionListener(onActionClickListener) // action button's ActionClickListener
-                , activity); // activity where it is displayed
+            , NetworkErrorHelper.RetryClickedListener onActionClickListener, Activity activity) {
+        snackbarRetry = NetworkErrorHelper.createSnackbarWithAction(activity, titleText, onActionClickListener);
+        snackbarRetry.setColorActionRetry(ContextCompat.getColor(activity, R.color.tkpd_main_green));
+        snackbarRetry.showRetrySnackbar();
     }
 
     public void dismissSnackbar() {
-        if (SnackbarManager.getCurrentSnackbar() != null)
-            SnackbarManager.dismiss();
-    }
-
-    public void setBackgroundColor(int backgroundColor) {
-        this.backgroundColor = backgroundColor;
-    }
-
-    public void setActionColor(int actionColor) {
-        this.actionColor = actionColor;
+        if (snackbarRetry != null)
+            snackbarRetry.hideRetrySnackbar();
     }
 }
