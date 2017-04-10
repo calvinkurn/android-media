d663fef9e56fd4626c0b01aab11c7f7c7e54ea6e
Hafizh Herdi <hherdinaufal@gmail.com>
2017-04-10 15:38:23 +0700

    remove unused converter json object to hashmap

--- tkpdcore/src/main/java/com/tokopedia/core/gcm/NotificationAnalyticsReceiver.java
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/NotificationAnalyticsReceiver.java
@@ -2,6 +2,7 @@ package com.tokopedia.core.gcm;
 
 import android.os.Bundle;
 
+import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.core.analytics.AppEventTracking;
 import com.tokopedia.core.analytics.TrackingUtils;
 
@@ -50,21 +51,8 @@ public class NotificationAnalyticsReceiver implements INotificationAnalyticsRece
 
     @Override
     public Bundle buildAnalyticNotificationData(Bundle data) {
-        if (data != null && data.containsKey(AppEventTracking.LOCA.NOTIFICATION_BUNDLE)) {
-            HashMap<String, Object> maps = new HashMap<>();
-            try {
-                JSONObject llObject = new JSONObject(data.getString(AppEventTracking.LOCA.NOTIFICATION_BUNDLE));
-                Iterator<?> keys = llObject.keys();
-                while (keys.hasNext()) {
-                    String key = (String) keys.next();
-                    maps.put(key, llObject.getString(key));
-                }
-                data.putSerializable(AppEventTracking.LOCA.NOTIFICATION_BUNDLE, maps);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            sendLocalyticsPushReceived(data);
-        }
+        CommonUtils.dumper("FCM GAv4 inside localytics bundle notif "+data);
+        sendLocalyticsPushReceived(data);
         return data;
 
     }
