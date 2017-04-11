895404a48b3a4b8545930fc4ee0e0ba8d6215539
Angga Prasetiyo <agapras@gmail.com>
2017-04-11 19:11:38 +0700

    revert buy from widget to webview

--- customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/fragment/RechargeFragment.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/fragment/RechargeFragment.java
@@ -383,7 +383,8 @@ public class RechargeFragment extends Fragment implements RechargeEditText.Recha
                         selectedOperatorId, String.valueOf(selectedOperator.defaultProductId));
             } else {
                 if (checkStockProduct(selectedProduct))
-                    goToNativeCheckout();
+//                    goToNativeCheckout();
+                    goToCheckout(getUrlCheckout());
             }
         } else {
             gotoLogin();
@@ -549,8 +550,8 @@ public class RechargeFragment extends Fragment implements RechargeEditText.Recha
     public void showProductById(Product product) {
         selectedProduct = product;
         if (checkStockProduct(selectedProduct))
-            goToNativeCheckout();
-//            goToCheckout(getUrlCheckout());
+//            goToNativeCheckout();
+            goToCheckout(getUrlCheckout());
     }
 
     @Override
