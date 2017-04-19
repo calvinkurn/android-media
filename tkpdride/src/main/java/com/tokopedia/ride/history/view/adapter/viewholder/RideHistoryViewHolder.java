15003eb3cae37ce2ce97d8b98460a30010fbd237
fatahillah alvarisi <alvarisi01@gmail.com>
2017-04-19 13:46:36 +0700

    binding view holder

--- tkpdride/src/main/java/com/tokopedia/ride/history/view/adapter/viewholder/RideHistoryViewHolder.java
+++ tkpdride/src/main/java/com/tokopedia/ride/history/view/adapter/viewholder/RideHistoryViewHolder.java
@@ -4,9 +4,13 @@ import android.support.annotation.LayoutRes;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.tokopedia.core.base.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.ride.R;
 import com.tokopedia.ride.R2;
+import com.tokopedia.ride.common.animator.RouteMapAnimator;
 import com.tokopedia.ride.history.view.adapter.ItemClickListener;
 import com.tokopedia.ride.history.view.viewmodel.RideHistoryViewModel;
 
@@ -17,7 +21,7 @@ import butterknife.OnClick;
  * Created by alvarisi on 4/11/17.
  */
 
-public class RideHistoryViewHolder extends AbstractViewHolder<RideHistoryViewModel> {
+public class RideHistoryViewHolder extends AbstractViewHolder<RideHistoryViewModel> implements OnMapReadyCallback {
     @LayoutRes
     public static final int LAYOUT = R.layout.row_ride_history;
 
@@ -31,21 +35,42 @@ public class RideHistoryViewHolder extends AbstractViewHolder<RideHistoryViewMod
     TextView rideFareTextView;
     @BindView(R2.id.tv_ride_status)
     TextView rideStatusTextView;
+    @BindView(R2.id.mapview)
+    MapView mapView;
+
+    GoogleMap googleMap;
 
     private final ItemClickListener mItemClickListener;
 
     public RideHistoryViewHolder(View itemView, ItemClickListener mItemClickListener) {
         super(itemView);
         this.mItemClickListener = mItemClickListener;
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
     }
 
     @Override
     public void bind(RideHistoryViewModel element) {
         mItem = element;
+        rideStartTimeTextView.setText(element.getRequestTime());
+        driverCarDisplayNameTextView.setText(element.getDriverCarDisplay());
+        rideFareTextView.setText(element.getFare());
+        rideStatusTextView.setText(element.getStatus());
+        if (element.getLatLngs() != null && element.getLatLngs().size() > 1) {
+            RouteMapAnimator.getInstance().animateRoute(googleMap, element.getLatLngs());
+        }
     }
 
     @OnClick(R2.id.container)
-    public void actionItemClicked(){
+    public void actionItemClicked() {
         mItemClickListener.onHistoryClicked(mItem);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
     }
 }
