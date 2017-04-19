b150d7695225328a0c59498579fda549eafbcfe6
fatahillah alvarisi <alvarisi01@gmail.com>
2017-04-19 13:45:13 +0700

    create ride history view contract

---
+++ tkpdride/src/main/java/com/tokopedia/ride/history/view/RideHistoryContract.java
@@ -0,0 +1,30 @@
+package com.tokopedia.ride.history.view;
+
+import com.tokopedia.core.base.adapter.Visitable;
+import com.tokopedia.core.base.domain.RequestParams;
+import com.tokopedia.core.base.presentation.CustomerPresenter;
+import com.tokopedia.core.base.presentation.CustomerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alvarisi on 4/19/17.
+ */
+
+public interface RideHistoryContract {
+    interface View extends CustomerView{
+
+        RequestParams getHistoriesParam();
+
+        void showFailedGetHistoriesMessage();
+
+        void renderHistoryLists(ArrayList<Visitable> histories);
+
+        void renderUpdatedHistoryRow(int position, Visitable history);
+    }
+
+    interface Presenter extends CustomerPresenter<View>{
+
+        void initialize();
+    }
+}
