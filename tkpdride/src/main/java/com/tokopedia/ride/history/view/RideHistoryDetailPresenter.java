6d5d1b65b40faab2da51861151a3061f4604e113
alvarisi <alvarisi01@gmail.com>
2017-04-20 11:56:35 +0700

    add ridehistory detail presenter

---
+++ tkpdride/src/main/java/com/tokopedia/ride/history/view/RideHistoryDetailPresenter.java
@@ -0,0 +1,12 @@
+package com.tokopedia.ride.history.view;
+
+import com.tokopedia.core.base.presentation.BaseDaggerPresenter;
+
+/**
+ * Created by alvarisi on 4/20/17.
+ */
+
+public class RideHistoryDetailPresenter extends BaseDaggerPresenter<RideHistoryDetailContract.View> implements RideHistoryDetailContract.View {
+    public RideHistoryDetailPresenter() {
+    }
+}
