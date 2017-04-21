c639002b49e51f164daffe1e661eaf89668a22d6
sebastianuskurniawan <sebastianus.kurniawan@tokopedia.com>
2017-04-21 13:49:58 +0700

    delete product share activity and add product edit activity

--- tkpdseller/src/main/java/com/tokopedia/seller/product/view/activity/ProductEditActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/view/activity/ProductEditActivity.java
@@ -2,22 +2,22 @@ package com.tokopedia.seller.product.view.activity;
 
 import android.os.Bundle;
 
-import com.tokopedia.core.app.TActivity;
-import com.tokopedia.core.share.fragment.ProductShareFragment;
 import com.tokopedia.seller.R;
+import com.tokopedia.seller.product.view.fragment.ProductEditFragment;
 
 /**
  * @author sebastianuskh on 4/21/17.
  */
 
-public class ProductUploadShareActivity extends TActivity{
+public class ProductEditActivity extends ProductAddActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         inflateView(R.layout.activity_product_add);
-        getFragmentManager().beginTransaction().disallowAddToBackStack()
-                .add(R.id.container, ProductShareFragment.newInstance(true), ProductShareFragment.TAG)
+        getSupportFragmentManager().beginTransaction().disallowAddToBackStack()
+                .add(R.id.container, ProductEditFragment.createInstance(), ProductEditFragment.class.getSimpleName())
                 .commit();
     }
+
 }
---
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/view/fragment/ProductEditFragment.java
@@ -0,0 +1,8 @@
+package com.tokopedia.seller.product.view.fragment;
+
+/**
+ * @author sebastianuskh on 4/21/17.
+ */
+
+public class ProductEditFragment extends ProductAddFragment{
+}
