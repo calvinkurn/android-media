6dcd287ae64c7ef414df1e679090c14d8f694129
fatahillah alvarisi <alvarisi01@gmail.com>
2017-04-21 08:08:35 +0700

    add ride home dependency injection

---
+++ tkpdride/src/main/java/com/tokopedia/ride/bookingride/di/RideHomeDependencyInjection.java
@@ -0,0 +1,155 @@
+package com.tokopedia.ride.bookingride.di;
+
+import com.google.gson.Gson;
+import com.tokopedia.core.base.data.executor.JobExecutor;
+import com.tokopedia.core.base.domain.executor.PostExecutionThread;
+import com.tokopedia.core.base.domain.executor.ThreadExecutor;
+import com.tokopedia.core.base.presentation.UIThread;
+import com.tokopedia.core.network.retrofit.coverters.GeneratedHostConverter;
+import com.tokopedia.core.network.retrofit.coverters.StringResponseConverter;
+import com.tokopedia.core.network.retrofit.coverters.TkpdResponseConverter;
+import com.tokopedia.ride.bookingride.domain.GetCurrentRideRequestUseCase;
+import com.tokopedia.ride.common.network.RideInterceptor;
+import com.tokopedia.ride.common.ride.data.BookingRideDataStoreFactory;
+import com.tokopedia.ride.common.ride.data.BookingRideRepositoryData;
+import com.tokopedia.ride.common.ride.data.ProductEntityMapper;
+import com.tokopedia.ride.common.ride.data.TimeEstimateEntityMapper;
+import com.tokopedia.ride.common.ride.data.source.api.RideApi;
+import com.tokopedia.ride.common.ride.data.source.api.RideUrl;
+import com.tokopedia.ride.common.ride.domain.BookingRideRepository;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by alvarisi on 4/21/17.
+ */
+
+public class RideHomeDependencyInjection {
+    private Gson provideGson() {
+        return new Gson();
+    }
+
+    private ThreadExecutor provideThreadExecutor() {
+        return new JobExecutor();
+    }
+
+    private PostExecutionThread providePostExecutionThread() {
+        return new UIThread();
+    }
+
+    private GeneratedHostConverter provideGeneratedHostConverter() {
+        return new GeneratedHostConverter();
+    }
+
+    private TkpdResponseConverter provideTkpdResponseConverter() {
+        return new TkpdResponseConverter();
+    }
+
+    private StringResponseConverter provideResponseConverter() {
+        return new StringResponseConverter();
+    }
+
+    private GsonConverterFactory provideGsonConverterFactory(Gson gson) {
+        return GsonConverterFactory.create(gson);
+    }
+
+    private RxJavaCallAdapterFactory provideRxJavaCallAdapterFactory() {
+        return RxJavaCallAdapterFactory.create();
+    }
+
+    private RideInterceptor provideRideInterceptor(String token, String userId) {
+        return new RideInterceptor(token, userId);
+    }
+
+    private HttpLoggingInterceptor provideLoggingInterceptory() {
+        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return logInterceptor;
+    }
+
+    private OkHttpClient provideRideOkHttpClient(RideInterceptor rideInterceptor, HttpLoggingInterceptor loggingInterceptor) {
+        OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+        clientBuilder.connectTimeout(45L, TimeUnit.SECONDS);
+        clientBuilder.readTimeout(45L, TimeUnit.SECONDS);
+        clientBuilder.writeTimeout(45L, TimeUnit.SECONDS);
+        clientBuilder.interceptors().add(rideInterceptor);
+        clientBuilder.interceptors().add(loggingInterceptor);
+        return clientBuilder.build();
+    }
+
+    private Retrofit provideRideRetrofit(OkHttpClient client,
+                                         GeneratedHostConverter hostConverter,
+                                         TkpdResponseConverter tkpdResponseConverter,
+                                         StringResponseConverter stringResponseConverter,
+                                         GsonConverterFactory gsonConverterFactory,
+                                         RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
+        return createRetrofit(RideUrl.BASE_URL,
+                client,
+                hostConverter,
+                tkpdResponseConverter,
+                stringResponseConverter,
+                gsonConverterFactory,
+                rxJavaCallAdapterFactory);
+    }
+
+    private RideApi provideRideApi(Retrofit retrofit) {
+        return retrofit.create(RideApi.class);
+    }
+
+    private BookingRideDataStoreFactory provideBookingRideDataStoreFactory(RideApi rideApi) {
+        return new BookingRideDataStoreFactory(rideApi);
+    }
+
+    private BookingRideRepository provideBookingRideRepository(BookingRideDataStoreFactory factory,
+                                                               ProductEntityMapper mapper, TimeEstimateEntityMapper estimateEntityMapper) {
+        return new BookingRideRepositoryData(factory, mapper, estimateEntityMapper);
+    }
+
+    private static Retrofit createRetrofit(String baseUrl,
+                                           OkHttpClient client,
+                                           GeneratedHostConverter hostConverter,
+                                           TkpdResponseConverter tkpdResponseConverter,
+                                           StringResponseConverter stringResponseConverter,
+                                           GsonConverterFactory gsonConverterFactory,
+                                           RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(hostConverter)
+                .addConverterFactory(tkpdResponseConverter)
+                .addConverterFactory(stringResponseConverter)
+                .addConverterFactory(gsonConverterFactory)
+                .addCallAdapterFactory(rxJavaCallAdapterFactory)
+                .build();
+    }
+
+    private GetCurrentRideRequestUseCase provideGetProductAndEstimatedUseCase(String token, String userId) {
+        return new GetCurrentRideRequestUseCase(
+                provideThreadExecutor(),
+                providePostExecutionThread(),
+                provideBookingRideRepository(
+                        provideBookingRideDataStoreFactory(
+                                provideRideApi(
+                                        provideRideRetrofit(
+                                                provideRideOkHttpClient(provideRideInterceptor(token, userId),
+                                                        provideLoggingInterceptory()),
+                                                provideGeneratedHostConverter(),
+                                                provideTkpdResponseConverter(),
+                                                provideResponseConverter(),
+                                                provideGsonConverterFactory(provideGson()),
+                                                provideRxJavaCallAdapterFactory()
+                                        )
+                                )
+                        ),
+                        new ProductEntityMapper(),
+                        new TimeEstimateEntityMapper()
+                )
+        );
+    }
+}
