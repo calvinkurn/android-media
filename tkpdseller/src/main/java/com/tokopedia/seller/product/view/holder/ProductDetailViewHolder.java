287c405950be82024177844b5ca868164b04e904
hendrysetiadi89 <hendrysetiadi@bahaso.com>
2017-05-09 11:13:16 +0700

    Not display message error because user not enter any number yet

--- tkpdseller/src/main/java/com/tokopedia/seller/product/view/holder/ProductDetailViewHolder.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/view/holder/ProductDetailViewHolder.java
@@ -15,6 +15,7 @@ import android.text.TextUtils;
 import android.util.Pair;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -147,18 +148,17 @@ public class ProductDetailViewHolder extends ProductViewHolder
             }
 
             private void onItemClicked(int position) {
-                if (goldMerchant) {
-                    priceSpinnerCounterInputView.setCounterValue(Float.parseFloat(priceSpinnerCounterInputView.getContext().getString(R.string.product_default_counter_text)));
-                    isPriceValid();
-                    return;
-                }
-                if (priceSpinnerCounterInputView.getSpinnerValue(position).equalsIgnoreCase(priceSpinnerCounterInputView.getContext().getString(R.string.product_currency_value_usd))) {
+                priceSpinnerCounterInputView.setCounterValue(Float.parseFloat(priceSpinnerCounterInputView.getContext().getString(R.string.product_default_counter_text)));
+                EditText editText = priceSpinnerCounterInputView.getCounterEditText();
+                editText.setSelection(editText.getText().length());
+                priceSpinnerCounterInputView.setCounterError(null);
+
+                if (!goldMerchant && priceSpinnerCounterInputView.getSpinnerValue(position).equalsIgnoreCase(priceSpinnerCounterInputView.getContext().getString(R.string.product_currency_value_usd))) {
+                    priceSpinnerCounterInputView.setSpinnerValue(priceSpinnerCounterInputView.getContext().getString(R.string.product_currency_value_idr));
                     Snackbar.make(priceSpinnerCounterInputView.getRootView().findViewById(android.R.id.content), R.string.product_error_must_be_gold_merchant, Snackbar.LENGTH_LONG)
                             .setActionTextColor(ContextCompat.getColor(priceSpinnerCounterInputView.getContext(), R.color.green_400))
                             .show();
-
                 }
-                priceSpinnerCounterInputView.setSpinnerValue(priceSpinnerCounterInputView.getContext().getString(R.string.product_currency_value_idr));
             }
         });
 
