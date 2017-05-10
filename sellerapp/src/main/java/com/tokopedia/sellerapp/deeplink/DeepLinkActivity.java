6b6451511721629704c4640dc7361ad310fc71f6
Hafizh Herdi <hherdinaufal@gmail.com>
2017-05-10 15:12:10 +0700

    add base deeplink

---
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/DeepLinkActivity.java
@@ -0,0 +1,8 @@
+package com.tokopedia.sellerapp.deeplink;
+
+/**
+ * Created by Herdi_WORK on 10.05.17.
+ */
+
+public class DeepLinkActivity {
+}
---
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/presenter/DeepLinkPresenter.java
@@ -0,0 +1,8 @@
+package com.tokopedia.sellerapp.deeplink.presenter;
+
+/**
+ * Created by Herdi_WORK on 10.05.17.
+ */
+
+public class DeepLinkPresenter {
+}
