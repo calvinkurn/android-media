5441b2829c78352193a4f7fac8208943937e2492
kris <christian.daniel@tokopedia.com>
2017-05-10 18:16:03 +0700

    finish view for product some view probably doesnt fetch the correct response some colored texts havnt been greyed out when disabled

--- tkpddigital/src/main/java/com/tokopedia/digital/product/adapter/ProductChooserAdapter.java
+++ tkpddigital/src/main/java/com/tokopedia/digital/product/adapter/ProductChooserAdapter.java
@@ -1,12 +1,15 @@
 package com.tokopedia.digital.product.adapter;
 
 import android.app.Fragment;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.tokopedia.core.network.NetworkErrorHelper;
 import com.tokopedia.digital.R;
 import com.tokopedia.digital.R2;
 import com.tokopedia.digital.product.model.Product;
@@ -32,6 +35,7 @@ public class ProductChooserAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     private List<Product> productList;
     private String productStyleView;
     private ActionListener actionListener;
+    private View mainAdapterView;
 
     public ProductChooserAdapter(Fragment hostFragment,
                                  List<Product> productList,
@@ -45,18 +49,14 @@ public class ProductChooserAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mainAdapterView = LayoutInflater.from(
+                hostFragment.getActivity()).inflate(viewType, parent, false);
         if (viewType == TYPE_HOLDER_PRODUCT_DESC_AND_PRICE_ITEM) {
-            return new ItemDescAndPriceHolder(LayoutInflater.from(
-                    hostFragment.getActivity()).inflate(viewType, parent, false
-            ));
+            return new ItemDescAndPriceHolder(mainAdapterView);
         } else if(viewType == TYPE_HOLDER_PRODUCT_PRICE_PLUS_ADMIN_AND_DESC) {
-            return new ItemPriceAdmin(LayoutInflater.from(
-                    hostFragment.getActivity()).inflate(viewType, parent, false
-            ));
+            return new ItemPriceAdmin(mainAdapterView);
         } else if(viewType == TYPE_HOLDER_PRODUCT_PROMO) {
-            return new ItemHolderPromoProduct(LayoutInflater.from(
-                    hostFragment.getActivity()).inflate(viewType, parent, false
-            ));
+            return new ItemHolderPromoProduct(mainAdapterView);
         }
         return null;
     }
@@ -67,33 +67,36 @@ public class ProductChooserAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         final Product product = productList.get(position);
         if (type == TYPE_HOLDER_PRODUCT_DESC_AND_PRICE_ITEM) {
             ItemDescAndPriceHolder itemDescAndPriceHolder = (ItemDescAndPriceHolder) holder;
-            itemDescAndPriceHolder.tvDesc.setText(product.getDesc());
+            itemDescAndPriceHolder.tvTitlePrice.setText(product.getDesc());
             itemDescAndPriceHolder.tvPrice.setText(product.getPrice());
         } else if (type == TYPE_HOLDER_PRODUCT_PRICE_PLUS_ADMIN_AND_DESC) {
             ItemPriceAdmin itemPriceAdmin = (ItemPriceAdmin) holder;
-            itemPriceAdmin.tvProductPrice.setText(product.getPricePlain());
-            itemPriceAdmin.tvProductDescription.setText(product.getDesc());
+            itemPriceAdmin.tvProductPrice.setText(product.getPrice());
+            itemPriceAdmin.tvProductDescription.setText(product.getDetail());
             itemPriceAdmin.tvProductTotalPrice.setText(product.getPrice());
         } else if (type == TYPE_HOLDER_PRODUCT_PROMO) {
             ItemHolderPromoProduct itemHolderPromoProduct = (ItemHolderPromoProduct) holder;
-            itemHolderPromoProduct.tvProductPromoTitle.setText(product.getDesc());
-            itemHolderPromoProduct.tvProductPromoDescription.setText(product.getPromo()
-                    .getBonusText());
-            itemHolderPromoProduct.tvPromoProductPrice.setText(product.getPrice());
-            itemHolderPromoProduct.tvProductPromoOldPrice.setText(product.getPromo().getNewPrice());
+            //itemHolderPromoProduct.tvProductPromoTitle.setText(product.getPromo().getValueText());
+            itemHolderPromoProduct.tvProductPromoTitle.setText("Test Title");
+/*            itemHolderPromoProduct.tvProductPromoDescription.setText(product.getPromo()
+                    .getBonusText());*/
+            itemHolderPromoProduct.tvProductPromoDescription.setText("Test Title");
+            itemHolderPromoProduct.tvPromoProductPrice.setText(product.getPromo().getNewPrice());
+            itemHolderPromoProduct.tvProductPromoOldPrice.setText(product.getPrice());
+            itemHolderPromoProduct.tvProductPromoOldPrice
+                    .setPaintFlags(itemHolderPromoProduct.tvProductPromoOldPrice.getPaintFlags()
+                            | Paint.STRIKE_THRU_TEXT_FLAG);
         }
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actionListener.onProductItemSelected(product);
-            }
-        });
+        setProductAvailability(holder, product);
     }
 
     @Override
     public int getItemViewType(int position) {
-        return TYPE_HOLDER_PRODUCT_DESC_AND_PRICE_ITEM;
+        if(productList.get(position).getPromo()!=null)
+            return TYPE_HOLDER_PRODUCT_PROMO;
+        else if(!productList.get(position).getDetail().isEmpty())
+            return TYPE_HOLDER_PRODUCT_PRICE_PLUS_ADMIN_AND_DESC;
+        else return TYPE_HOLDER_PRODUCT_DESC_AND_PRICE_ITEM;
     }
 
     @Override
@@ -102,8 +105,8 @@ public class ProductChooserAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     }
 
     static class ItemDescAndPriceHolder extends RecyclerView.ViewHolder {
-        @BindView(R2.id.tv_desc)
-        TextView tvDesc;
+        @BindView(R2.id.title_price)
+        TextView tvTitlePrice;
         @BindView(R2.id.tv_price)
         TextView tvPrice;
 
@@ -145,5 +148,22 @@ public class ProductChooserAdapter extends RecyclerView.Adapter<RecyclerView.Vie
 
     public interface ActionListener {
         void onProductItemSelected(Product product);
+    }
+
+    private void setProductAvailability(final RecyclerView.ViewHolder holder,
+                                        final Product product) {
+        if(product.getStatus() == 3) {
+            for(int i = 0; i < ((ViewGroup)mainAdapterView).getChildCount(); i++) {
+                ((ViewGroup) mainAdapterView).getChildAt(i).setEnabled(false);
+            }
+
+        } else {
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    actionListener.onProductItemSelected(product);
+                }
+            });
+        }
     }
 }
