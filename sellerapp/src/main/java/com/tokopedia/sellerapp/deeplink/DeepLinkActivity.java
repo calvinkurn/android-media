3694feaf9c68cc9788b3ff222531627ebc95b5f9
Hafizh Herdi <hherdinaufal@gmail.com>
2017-05-10 15:12:31 +0700

    implement deeplink presenter

--- sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/DeepLinkActivity.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/DeepLinkActivity.java
@@ -1,8 +1,56 @@
 package com.tokopedia.sellerapp.deeplink;
 
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.tokopedia.core.app.BasePresenterActivity;
+import com.tokopedia.sellerapp.deeplink.presenter.DeepLinkPresenter;
+
 /**
  * Created by Herdi_WORK on 10.05.17.
  */
 
-public class DeepLinkActivity {
+public class DeepLinkActivity extends BasePresenterActivity<DeepLinkPresenter> {
+
+    @Override
+    protected void setupURIPass(Uri data) {
+
+    }
+
+    @Override
+    protected void setupBundlePass(Bundle extras) {
+
+    }
+
+    @Override
+    protected void initialPresenter() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setViewListener() {
+
+    }
+
+    @Override
+    protected void initVar() {
+
+    }
+
+    @Override
+    protected void setActionVar() {
+
+    }
+
+
 }
--- sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/presenter/DeepLinkPresenterImpl.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/presenter/DeepLinkPresenterImpl.java
@@ -1,8 +1,35 @@
 package com.tokopedia.sellerapp.deeplink.presenter;
 
+import android.net.Uri;
+
 /**
  * Created by Herdi_WORK on 10.05.17.
  */
 
-public class DeepLinkPresenterImpl {
+public class DeepLinkPresenterImpl implements DeepLinkPresenter {
+
+    @Override
+    public void processDeepLinkAction(Uri uri) {
+
+    }
+
+    @Override
+    public void processAFlistener() {
+
+    }
+
+    @Override
+    public void sendCampaignGTM(String campaignUri, String screenName) {
+
+    }
+
+    @Override
+    public boolean isLandingPageWebView(Uri uri) {
+        return false;
+    }
+
+    @Override
+    public void checkUriLogin(Uri uriData) {
+
+    }
 }
