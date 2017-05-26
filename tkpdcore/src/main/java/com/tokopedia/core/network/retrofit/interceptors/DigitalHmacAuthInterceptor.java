5e35e036c2731cf7971352047aec279b494cc57c
Angga Prasetiyo <agapras@gmail.com>
2017-05-26 18:32:32 +0700

    improvement exception error

--- tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/interceptors/DigitalHmacAuthInterceptor.java
+++ tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/interceptors/DigitalHmacAuthInterceptor.java
@@ -4,8 +4,12 @@ import android.util.Log;
 
 import com.tokopedia.core.network.exception.HttpErrorException;
 import com.tokopedia.core.network.exception.ResponseErrorException;
+import com.tokopedia.core.network.exception.ServerErrorRequestDeniedException;
+import com.tokopedia.core.network.retrofit.exception.ServerErrorMaintenanceException;
+import com.tokopedia.core.network.retrofit.exception.ServerErrorTimeZoneException;
 import com.tokopedia.core.network.retrofit.response.TkpdDigitalResponse;
 import com.tokopedia.core.network.retrofit.utils.AuthUtil;
+import com.tokopedia.core.util.MethodChecker;
 
 import java.io.IOException;
 import java.util.Map;
@@ -30,8 +34,35 @@ public class DigitalHmacAuthInterceptor extends TkpdAuthInterceptor {
         Log.d(TAG, "Error body response : " + errorBody);
         if (!errorBody.isEmpty()) {
             TkpdDigitalResponse.DigitalErrorResponse digitalErrorResponse =
-                    TkpdDigitalResponse.DigitalErrorResponse.factory(errorBody);
-            throw new ResponseErrorException(digitalErrorResponse.getErrorMessageFormatted());
+                    TkpdDigitalResponse.DigitalErrorResponse.factory(errorBody, response.code());
+            if (digitalErrorResponse.getTypeOfError()
+                    == TkpdDigitalResponse.DigitalErrorResponse.ERROR_DIGITAL) {
+                throw new ResponseErrorException(digitalErrorResponse.getDigitalErrorMessageFormatted());
+            } else if (digitalErrorResponse.getTypeOfError()
+                    == TkpdDigitalResponse.DigitalErrorResponse.ERROR_SERVER) {
+                if (digitalErrorResponse.getStatus().equalsIgnoreCase("UNDER_MAINTENANCE")) {
+                    throw new ServerErrorMaintenanceException(
+                            digitalErrorResponse.getServerErrorMessageFormatted(), errorBody,
+                            response.code(), response.request().url().toString()
+                    );
+                } else if (digitalErrorResponse.getStatus().equalsIgnoreCase("REQUEST_DENIED")) {
+                    throw new ServerErrorRequestDeniedException(
+                            digitalErrorResponse.getServerErrorMessageFormatted(), errorBody,
+                            response.code(), response.request().url().toString()
+                    );
+                } else if (digitalErrorResponse.getStatus().equalsIgnoreCase("FORBIDDEN")
+                        && MethodChecker.isTimezoneNotAutomatic()) {
+                    throw new ServerErrorTimeZoneException(
+                            digitalErrorResponse.getServerErrorMessageFormatted(), errorBody,
+                            response.code(), response.request().url().toString()
+                    );
+                } else {
+                    throw new HttpErrorException(response.code());
+                }
+            } else {
+                throw new HttpErrorException(response.code());
+            }
+
         }
         throw new HttpErrorException(response.code());
     }
