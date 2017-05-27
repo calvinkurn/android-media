147cfc78a08c0d74defc4d3195f3ed2ad9ca5f35
okasurya <brilliantsurya@gmail.com>
2017-05-27 10:59:52 +0700

    fix countdown timer

--- tkpdcore/src/main/java/com/tokopedia/core/product/customview/HeaderInfoView.java
+++ tkpdcore/src/main/java/com/tokopedia/core/product/customview/HeaderInfoView.java
@@ -32,6 +32,10 @@ public class HeaderInfoView extends BaseView<ProductDetailData, ProductDetailVie
     private static final String TAG = HeaderInfoView.class.getSimpleName();
     private static final String DATE_TIME_FORMAT = "yyyy-MM-dd hh:mm:ss";
 
+    private static final String WEEK_TIMER_FORMAT = "%dw : ";
+    private static final String DAY_TIMER_FORMAT = "%dd : ";
+    private static final String HOUR_MIN_SEC_TIMER_FORMAT = "%02dh : %02dm : %02ds";
+
     @BindView(R2.id.tv_name)
     TextView tvName;
     @BindView(R2.id.tv_price)
@@ -58,8 +62,6 @@ public class HeaderInfoView extends BaseView<ProductDetailData, ProductDetailVie
     TextView textDiscountTimer;
     @BindString(R2.string.label_discount)
     public String discount;
-    @BindString(R2.string.label_discount_timer)
-    String labelPromotionTimer;
     @BindString(R2.string.label_price_with_idr)
     String labelPriceWithIdr;
 
@@ -120,10 +122,6 @@ public class HeaderInfoView extends BaseView<ProductDetailData, ProductDetailVie
 
     public void renderProductCampaign(final ProductCampaign data) {
         if(data != null && data.getOriginalPrice() != null) {
-            cashbackHolder.setVisibility(VISIBLE);
-            textDiscount.setVisibility(VISIBLE);
-            textOriginalPrice.setVisibility(VISIBLE);
-
             textOriginalPrice.setText(String.format(labelPriceWithIdr, data.getOriginalPrice()));
             textOriginalPrice.setPaintFlags(
                     textOriginalPrice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG
@@ -140,29 +138,48 @@ public class HeaderInfoView extends BaseView<ProductDetailData, ProductDetailVie
                 new CountDownTimer(delta, 1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        textDiscountTimer.setText(
-                                String.format(
-                                        labelPromotionTimer,
-                                        TimeUnit.MILLISECONDS.toDays(millisUntilFinished),
-                                        (millisUntilFinished / (1000 * 60 * 60)) % 24,
-                                        (millisUntilFinished / (1000 * 60)) % 60,
-                                        (millisUntilFinished / (1000)) % 60
-                                )
-                        );
+                        textDiscountTimer.setText(getCountdownText(millisUntilFinished));
                     }
 
                     @Override
                     public void onFinish() {
                         linearDiscountTimerHolder.setVisibility(GONE);
                         textDiscount.setVisibility(GONE);
+                        textOriginalPrice.setVisibility(GONE);
                         tvPrice.setText(data.getOriginalPrice());
                     }
                 }.start();
 
+                cashbackHolder.setVisibility(VISIBLE);
+                textDiscount.setVisibility(VISIBLE);
+                textOriginalPrice.setVisibility(VISIBLE);
                 linearDiscountTimerHolder.setVisibility(VISIBLE);
             } catch (ParseException ex) {
                 ex.printStackTrace();
             }
         }
+    }
+
+    private String getCountdownText(long millisUntilFinished) {
+        String countdown = "";
+        long day = TimeUnit.MILLISECONDS.toDays(millisUntilFinished);
+        long week = day / 7;
+
+        if(week != 0) {
+            countdown += String.format(WEEK_TIMER_FORMAT, week);
+            day = day % 7;
+            countdown += String.format(DAY_TIMER_FORMAT, day);
+        } else if (day != 0) {
+            countdown += String.format(DAY_TIMER_FORMAT, day);
+        }
+
+        countdown += String.format(
+                HOUR_MIN_SEC_TIMER_FORMAT,
+                (millisUntilFinished / (1000 * 60 * 60)) % 24,
+                (millisUntilFinished / (1000 * 60)) % 60,
+                (millisUntilFinished / (1000)) % 60
+        );
+
+        return countdown;
     }
 }
