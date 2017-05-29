9a0f7950d1cf6ebc9dd8d8bb2eda8ea11812b857
sebastianuskh <sebastianuskh@outlook.co.id>
2017-05-29 17:35:47 +0700

    revert topads view holder

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/keyword/view/adapter/viewholder/TopAdsKeywordViewHolder.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/keyword/view/adapter/viewholder/TopAdsKeywordViewHolder.java
@@ -2,15 +2,11 @@ package com.tokopedia.seller.topads.keyword.view.adapter.viewholder;
 
 import android.support.annotation.StringRes;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 
-import com.tkpd.library.utils.CurrencyFormatHelper;
 import com.tokopedia.seller.R;
 import com.tokopedia.seller.topads.keyword.domain.model.Datum;
-import com.tokopedia.seller.topads.keyword.view.activity.TopAdsKeywordEditDetailActivity;
-import com.tokopedia.seller.topads.keyword.view.model.TopAdsKeywordEditDetailViewModel;
 
 /**
  * Created by normansyahputa on 5/19/17.
@@ -41,33 +37,7 @@ public class TopAdsKeywordViewHolder extends RecyclerView.ViewHolder {
         return itemView.getContext().getString(stringRes, value);
     }
 
-
-    private double getPrice(String price){
-        String valueString = CurrencyFormatHelper
-                .removeCurrencyPrefix(price);
-        valueString = CurrencyFormatHelper.RemoveNonNumeric(valueString);
-        if (TextUtils.isEmpty(valueString)) {
-            return 0;
-        }
-        return Double.parseDouble(valueString);
-    }
-
-    public void bindDataAds(final Datum datum) {
-        titleProduct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TopAdsKeywordEditDetailViewModel model = new TopAdsKeywordEditDetailViewModel();
-
-                model.setKeywordTag(datum.getKeywordTag());
-                model.setKeywordId(datum.getKeywordId());
-//                model.setKeywordTypeId(Integer.parseInt(datum.getKeywordTypeId()));
-                model.setGroupId((datum.getGroupId()));
-                model.setPriceBid(getPrice(datum.getKeywordPriceBidFmt()));
-                model.setToggle(datum.getKeywordStatusToogle());
-
-                TopAdsKeywordEditDetailActivity.start(titleProduct.getContext(), model);
-            }
-        });
+    public void bindDataAds(Datum datum) {
         titleProduct.setText(datum.getKeywordTag());
         statusActive.setText(datum.getKeywordStatusDesc());
         pricePromoPerClick.setText(getString(R.string.top_ads_per_click_detail, datum.getKeywordPriceBidFmt()));
