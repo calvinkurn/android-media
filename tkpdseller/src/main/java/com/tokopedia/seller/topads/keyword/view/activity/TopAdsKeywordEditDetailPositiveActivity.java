bf93310fe988dd515a9f965797f4d765d835ba4c
sebastianuskh <sebastianuskh@outlook.co.id>
2017-05-30 15:52:30 +0700

    add negative activity

--- tkpdseller/src/main/java/com/tokopedia/seller/topads/keyword/view/activity/TopAdsKeywordEditDetailPositiveActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/topads/keyword/view/activity/TopAdsKeywordEditDetailPositiveActivity.java
@@ -11,6 +11,7 @@ import com.tokopedia.core.app.BaseActivity;
 import com.tokopedia.core.base.di.component.AppComponent;
 import com.tokopedia.core.base.di.component.HasComponent;
 import com.tokopedia.seller.R;
+import com.tokopedia.seller.topads.keyword.view.fragment.TopAdsKeywordEditDetailFragment;
 import com.tokopedia.seller.topads.keyword.view.fragment.TopAdsKeywordEditDetailFragmentListener;
 import com.tokopedia.seller.topads.keyword.view.fragment.TopAdsKeywordEditDetailPositiveFragment;
 import com.tokopedia.seller.topads.keyword.view.model.KeywordAd;
@@ -19,7 +20,7 @@ import com.tokopedia.seller.topads.keyword.view.model.KeywordAd;
  * @author sebastianuskh on 5/23/17.
  */
 
-public class TopAdsKeywordEditDetailActivity extends BaseActivity implements HasComponent<AppComponent>, TopAdsKeywordEditDetailFragmentListener {
+public class TopAdsKeywordEditDetailPositiveActivity extends BaseActivity implements HasComponent<AppComponent>, TopAdsKeywordEditDetailFragmentListener {
 
     public static final String KEYWORD_DETAIL_MODEL = "KEYWORD_DETAIL_MODEL";
 
@@ -28,7 +29,7 @@ public class TopAdsKeywordEditDetailActivity extends BaseActivity implements Has
     }
 
     public static Intent createIntent(Context context, KeywordAd model){
-        Intent starter = new Intent(context, TopAdsKeywordEditDetailActivity.class);
+        Intent starter = new Intent(context, TopAdsKeywordEditDetailPositiveActivity.class);
         starter.putExtra(KEYWORD_DETAIL_MODEL, model);
         return starter;
     }
@@ -39,15 +40,19 @@ public class TopAdsKeywordEditDetailActivity extends BaseActivity implements Has
         setContentView(R.layout.activity_top_ads_keyword_edit_detail);
 
         FragmentManager supportFragmentManager = getSupportFragmentManager();
-        Fragment fragment = supportFragmentManager.findFragmentByTag(TopAdsKeywordEditDetailPositiveFragment.TAG);
+        Fragment fragment = supportFragmentManager.findFragmentByTag(TopAdsKeywordEditDetailFragment.TAG);
         if (fragment == null) {
             KeywordAd model = getIntent().getParcelableExtra(KEYWORD_DETAIL_MODEL);
-            fragment = TopAdsKeywordEditDetailPositiveFragment.createInstance(model);
+            fragment = getFragment(model);
             supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.container, fragment, TopAdsKeywordEditDetailPositiveFragment.TAG)
+                    .replace(R.id.container, fragment, TopAdsKeywordEditDetailFragment.TAG)
                     .commit();
         }
+    }
+
+    protected TopAdsKeywordEditDetailFragment getFragment(KeywordAd model) {
+        return TopAdsKeywordEditDetailPositiveFragment.createInstance(model);
     }
 
     @Override
