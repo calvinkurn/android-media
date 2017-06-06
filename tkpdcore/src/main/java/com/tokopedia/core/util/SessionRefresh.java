6dea3c0c95639a5446af0e05fdf46cb334e5f9bd
Rico Harisin <rico.harisin@tokopedia.com>
2017-06-06 14:04:23 +0700

    clean class session refresh

--- tkpdcore/src/main/java/com/tokopedia/core/util/SessionRefresh.java
+++ tkpdcore/src/main/java/com/tokopedia/core/util/SessionRefresh.java
@@ -1,24 +1,18 @@
 package com.tokopedia.core.util;
 
 import android.content.Context;
-import android.os.Bundle;
 
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.base.domain.RequestParams;
-import com.tokopedia.core.network.apiservices.accounts.AccountsService;
 import com.tokopedia.core.network.apiservices.accounts.apis.AccountsApi;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
 import com.tokopedia.core.network.core.OkHttpFactory;
-import com.tokopedia.core.network.retrofit.coverters.GeneratedHostConverter;
 import com.tokopedia.core.network.retrofit.coverters.StringResponseConverter;
-import com.tokopedia.core.network.retrofit.coverters.TkpdResponseConverter;
-import com.tokopedia.core.network.retrofit.response.TkpdResponse;
 import com.tokopedia.core.session.presenter.Login;
 
 import java.io.IOException;
 
 import retrofit2.Call;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 
 /**
@@ -28,17 +22,15 @@ import retrofit2.Retrofit;
 public class SessionRefresh {
 
     public String refreshLogin() throws IOException {
-        Bundle bundle = new Bundle();
         Context context = MainApplication.getAppContext();
         SessionHandler sessionHandler = new SessionHandler(context);
-        String authKey;
-        authKey = sessionHandler.getTokenType(context) + " " + sessionHandler.getAccessToken(context);
-        bundle.putString(AccountsService.AUTH_KEY, authKey);
-        bundle.putString(AccountsService.WEB_SERVICE, AccountsService.WS);
+        String authKey = sessionHandler.getTokenType(context)
+                + " " + sessionHandler.getAccessToken(context);
         RequestParams params = RequestParams.create();
         params.putString(Login.UUID_KEY, sessionHandler.getUUID());
         params.putString(Login.USER_ID, sessionHandler.getLoginID());
-        Call<String> responseCall = getRetrofit(authKey).create(AccountsApi.class).makeLoginsynchronous(params.getParameters());
+        Call<String> responseCall = getRetrofit(authKey)
+                .create(AccountsApi.class).makeLoginsynchronous(params.getParameters());
         return responseCall.execute().body();
     }
 
