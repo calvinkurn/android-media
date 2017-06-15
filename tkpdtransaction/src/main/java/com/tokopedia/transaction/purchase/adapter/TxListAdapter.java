2287049ab426fd366d89f04a0a2a369acebece34
nisie <nathania.sutedja@tokopedia.com>
2017-06-15 10:57:24 +0700

    Comment cancel replacement

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/adapter/TxListAdapter.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/adapter/TxListAdapter.java
@@ -166,9 +166,9 @@ public class TxListAdapter extends ArrayAdapter<OrderData> {
         switch (instanceType) {
             case TxListFragment.INSTANCE_STATUS:
                 switch (Integer.parseInt(item.getOrderDetail().getDetailOrderStatus())) {
-                    case TkpdState.OrderStatusState.ORDER_OPPORTUNITY:
-                        if (item.getOrderButton().getButtonCancelReplacement().equals("1"))
-                            holder.btnOverflow.setVisibility(View.VISIBLE);
+//                    case TkpdState.OrderStatusState.ORDER_OPPORTUNITY:
+//                        if (item.getOrderButton().getButtonCancelReplacement().equals("1"))
+//                            holder.btnOverflow.setVisibility(View.VISIBLE);
                     case TkpdState.OrderStatusState.ORDER_SHIPPING:
                     case TkpdState.OrderStatusState.ORDER_SHIPPING_REF_NUM_EDITED:
                     case TkpdState.OrderStatusState.ORDER_SHIPPING_TRACKER_INVALID:
@@ -213,9 +213,9 @@ public class TxListAdapter extends ArrayAdapter<OrderData> {
                 break;
             case TxListFragment.INSTANCE_STATUS:
                 switch (Integer.parseInt(item.getOrderDetail().getDetailOrderStatus())) {
-                    case TkpdState.OrderStatusState.ORDER_OPPORTUNITY:
-                            MenuID = R.menu.order_status_menu_cancel_replacement;
-                        break;
+//                    case TkpdState.OrderStatusState.ORDER_OPPORTUNITY:
+//                            MenuID = R.menu.order_status_menu_cancel_replacement;
+//                        break;
                     case TkpdState.OrderStatusState.ORDER_SHIPPING:
                     case TkpdState.OrderStatusState.ORDER_WAITING_STATUS_FROM_SHIPPING_AGENCY:
                     case TkpdState.OrderStatusState.ORDER_SHIPPING_REF_NUM_EDITED:
