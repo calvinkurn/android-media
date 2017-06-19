1152441d58786b5a357bb89cf209bf0e2903abf1
nisie <nathania.sutedja@tokopedia.com>
2017-06-19 15:17:57 +0700

    Fix mapping

--- tkpdsession/src/main/java/com/tokopedia/profilecompletion/data/mapper/GetUserInfoMapper.java
+++ tkpdsession/src/main/java/com/tokopedia/profilecompletion/data/mapper/GetUserInfoMapper.java
@@ -1,19 +1,32 @@
 package com.tokopedia.profilecompletion.data.mapper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.tokopedia.core.network.ErrorMessageException;
 import com.tokopedia.core.network.retrofit.response.TkpdResponse;
+import com.tokopedia.core.session.model.TokenModel;
 import com.tokopedia.profilecompletion.data.pojo.GetUserInfoData;
 import com.tokopedia.profilecompletion.domain.model.GetUserInfoDomainData;
 import com.tokopedia.profilecompletion.domain.model.GetUserInfoDomainModel;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import retrofit2.Response;
 import rx.functions.Func1;
+
+import static com.facebook.stetho.inspector.network.PrettyPrinterDisplayType.JSON;
 
 /**
  * @author by nisie on 6/19/17.
  */
 
 public class GetUserInfoMapper implements Func1<Response<String>, GetUserInfoDomainModel> {
+
+    private static final String ERROR = "error";
+    private static final String ERROR_DESCRIPTION = "error_description";
+
     @Override
     public GetUserInfoDomainModel call(Response<String> response) {
         return mappingResponse(response);
@@ -22,8 +35,19 @@ public class GetUserInfoMapper implements Func1<Response<String>, GetUserInfoDom
     private GetUserInfoDomainModel mappingResponse(Response<String> response) {
         GetUserInfoDomainModel model = new GetUserInfoDomainModel();
         if (response.isSuccessful()) {
+            String responseJson = String.valueOf(response.body());
+            try {
+                JSONObject json = new JSONObject(responseJson);
+                if (!json.getString(ERROR).equals("")
+                        && !json.getString(ERROR_DESCRIPTION).equals(""))
+                    throw new ErrorMessageException(json.getString(ERROR_DESCRIPTION));
+            } catch (JSONException e) {
+                GetUserInfoData data = new GsonBuilder().create().fromJson(responseJson, GetUserInfoData.class);
+                model.setGetUserInfoDomainData(mappingToDomainData(data));
+                model.setSuccess(true);
+            }
 
-            model.setResponseCode(response.code());
+
         } else {
             throw new RuntimeException(String.valueOf(response.code()));
         }
