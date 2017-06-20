11cda249f6585ead382b4a32ac1749c0311601a3
hangnadi <hangnadi@gmail.com>
2017-06-20 11:12:03 +0700

    create new file upload video end point

---
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/discussion/domain/interactor/NewReplyDiscussionUseCase.java
@@ -0,0 +1,8 @@
+package com.tokopedia.inbox.rescenter.discussion.domain.interactor;
+
+/**
+ * Created by hangnadi on 6/16/17.
+ */
+
+public class NewReplyDiscussionUseCase {
+}
---
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/discussion/domain/model/NewReplyDiscussionModel.java
@@ -0,0 +1,8 @@
+package com.tokopedia.inbox.rescenter.discussion.domain.model;
+
+/**
+ * Created by hangnadi on 6/16/17.
+ */
+
+class NewReplyDiscussionModel {
+}
