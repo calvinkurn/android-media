fb616d0edb25c46a07659463c91854c6c2f47e04
hendry <hendry.setiadi.89@gmail.com>
2017-06-20 15:25:15 +0700

    change TopAds baseList generic OnClicked to U change DraftFragment to TkpdActivity

--- tkpdseller/src/main/java/com/tokopedia/seller/product/view/activity/ProductDraftListActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/view/activity/ProductDraftListActivity.java
@@ -2,17 +2,13 @@ package com.tokopedia.seller.product.view.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.tokopedia.seller.R;
+import com.tokopedia.core.app.TkpdActivity;
 import com.tokopedia.seller.product.data.mapper.ProductDraftMapper;
 import com.tokopedia.seller.product.data.source.db.ProductDraftDataManager;
 import com.tokopedia.seller.product.data.source.db.model.ProductDraftDataBase;
 import com.tokopedia.seller.product.domain.model.UploadProductInputDomainModel;
-import com.tokopedia.seller.product.view.fragment.ProductDraftListFragment;
-import com.tokopedia.seller.topads.keyword.view.activity.TopAdsBaseSimpleActivity;
 
 import java.util.List;
 
@@ -24,22 +20,13 @@ import rx.functions.Func1;
  * Created by User on 6/19/2017.
  */
 
-public class ProductDraftListActivity extends TopAdsBaseSimpleActivity {
+public class ProductDraftListActivity extends TkpdActivity {
     public static final String TAG = ProductDraftListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected Fragment getNewFragment() {
-        return ProductDraftListFragment.newInstance();
-    }
-
-    @Override
-    protected String getTagFragment() {
-        return ProductDraftListFragment.TAG;
+        inflateView(com.tokopedia.core.R.layout.activity_manage_product);
     }
 
     @Override
