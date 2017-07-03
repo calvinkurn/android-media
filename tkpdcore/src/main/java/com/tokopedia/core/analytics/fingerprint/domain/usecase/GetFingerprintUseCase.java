6bcd5c2043127199a651eab1caf37f9b2fdc810c
Hafizh Herdi <hherdinaufal@gmail.com>
2017-07-03 13:52:29 +0700

    create fingerprint usecase

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/usecase/GetFingerprintUseCase.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/usecase/GetFingerprintUseCase.java
@@ -1,11 +1,30 @@
 package com.tokopedia.core.analytics.fingerprint.domain.usecase;
 
+import com.tokopedia.core.analytics.fingerprint.domain.FingerprintRepository;
+import com.tokopedia.core.analytics.fingerprint.domain.model.FingerPrint;
+import com.tokopedia.core.base.domain.RequestParams;
+import com.tokopedia.core.base.domain.UseCase;
+import com.tokopedia.core.base.domain.executor.PostExecutionThread;
+import com.tokopedia.core.base.domain.executor.ThreadExecutor;
+
+import rx.Observable;
+
 /**
  * Created by Herdi_WORK on 20.06.17.
  */
 
-public class SendFingerPrintUseCase {
+public class GetFingerprintUseCase extends UseCase<String>{
 
+    private final FingerprintRepository fingerprintRepository;
 
+    public GetFingerprintUseCase(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread, FingerprintRepository fpRepository){
+        super(threadExecutor, postExecutionThread);
+        fingerprintRepository = fpRepository;
+    }
+
+    @Override
+    public Observable<String> createObservable(RequestParams requestParams) {
+        return fingerprintRepository.getFingerPrint(new FingerPrint());
+    }
 
 }
