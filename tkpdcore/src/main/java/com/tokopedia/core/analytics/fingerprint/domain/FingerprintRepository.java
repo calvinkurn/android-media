6085b310a2190b371605fea8669135507e7ed96f
Hafizh Herdi <hherdinaufal@gmail.com>
2017-07-03 13:55:34 +0700

    create fingerprint domain repository

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/FingerprintRepository.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/FingerprintRepository.java
@@ -1,8 +1,15 @@
 package com.tokopedia.core.analytics.fingerprint.domain;
 
+import com.tokopedia.core.analytics.fingerprint.domain.model.FingerPrint;
+
+import rx.Observable;
+
 /**
  * Created by Herdi_WORK on 20.06.17.
  */
 
-public class FPRepository {
+public interface FingerprintRepository {
+
+    Observable<String> getFingerPrint(FingerPrint data);
+
 }
