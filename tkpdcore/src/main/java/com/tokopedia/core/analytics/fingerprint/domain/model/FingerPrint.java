645ef6d1ac6f00695546e63a34548cf51df779bc
Hafizh Herdi <hherdinaufal@gmail.com>
2017-07-03 13:51:56 +0700

    create fingerprint model builder

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/model/FingerPrint.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/domain/model/FingerPrint.java
@@ -1,4 +1,4 @@
-package com.tokopedia.core.analytics.fingerprint.model;
+package com.tokopedia.core.analytics.fingerprint.domain.model;
 
 /**
  * Created by Herdi_WORK on 20.06.17.
@@ -22,6 +22,25 @@ public class FingerPrint {
     private String screenResolution;
     private String installedKeyboard;
     private String language;
+
+    private FingerPrint(FingerPrintBuilder fingerPrintBuilder){
+        deviceID = fingerPrintBuilder.deviceID;
+        deviceName = fingerPrintBuilder.deviceName;
+        deviceManufacturer = fingerPrintBuilder.deviceManufacturer;
+        currentOS = fingerPrintBuilder.currentOS;
+        isJailBreak = fingerPrintBuilder.isJailBreak;
+        timezone = fingerPrintBuilder.timezone;
+        userAgent = fingerPrintBuilder.userAgent;
+        isEmulator = fingerPrintBuilder.isEmulator;
+        istablet = fingerPrintBuilder.istablet;
+        lat = fingerPrintBuilder.lat;
+        lng = fingerPrintBuilder.lng;
+        buildNumber = fingerPrintBuilder.buildNumber;
+        ipAddress = fingerPrintBuilder.ipAddress;
+        screenResolution = fingerPrintBuilder.screenResolution;
+        installedKeyboard = fingerPrintBuilder.installedKeyboard;
+        language = fingerPrintBuilder.language;
+    }
 
     public String getLanguage() {
         return language;
@@ -151,5 +170,112 @@ public class FingerPrint {
         this.userAgent = userAgent;
     }
 
+    public static class FingerPrintBuilder {
+        private String deviceID;
+        private String deviceName;
+        private String deviceManufacturer;
+        private String currentOS;
+        private boolean isJailBreak;
+        private String timezone;
+        private String userAgent;
+        private boolean isEmulator;
+        private boolean istablet;
+        private double lat;
+        private double lng;
+        private String buildNumber;
+        private String ipAddress;
+        private String screenResolution;
+        private String installedKeyboard;
+        private String language;
+
+        public FingerPrintBuilder() {
+
+        }
+
+        public FingerPrintBuilder language(String language) {
+            this.language = language;
+            return this;
+        }
+
+        public FingerPrintBuilder installedKeyboard(String installedKeyboard) {
+            this.installedKeyboard = installedKeyboard;
+            return this;
+        }
+
+        public FingerPrintBuilder screenReso(String screenResolution) {
+            this.screenResolution = screenResolution;
+            return this;
+        }
+
+        public FingerPrintBuilder ipAddres(String ipAddress) {
+            this.ipAddress = ipAddress;
+            return this;
+        }
+
+        public FingerPrintBuilder buildNumber(String buildNumber) {
+            this.buildNumber = buildNumber;
+            return this;
+        }
+
+        public FingerPrintBuilder deviceLng(double lng) {
+            this.lng = lng;
+            return this;
+        }
+
+        public FingerPrintBuilder deviceLat(double lat) {
+            this.lat = lat;
+            return this;
+        }
+
+        public FingerPrintBuilder tablet(boolean istablet) {
+            this.istablet = istablet;
+            return this;
+        }
+
+        public FingerPrintBuilder emulator(boolean isEmulator) {
+            this.isEmulator = isEmulator;
+            return this;
+        }
+
+        public FingerPrintBuilder userAgent(String userAgent) {
+            this.userAgent = userAgent;
+            return this;
+        }
+
+        public FingerPrintBuilder timezone(String timezone) {
+            this.timezone = timezone;
+            return this;
+        }
+
+        public FingerPrintBuilder deviceID(String deviceID) {
+            this.deviceID = deviceID;
+            return this;
+        }
+
+        public FingerPrintBuilder deviceName(String deviceName) {
+            this.deviceName = deviceName;
+            return this;
+        }
+
+        public FingerPrintBuilder deviceManufacturer(String deviceMfn) {
+            this.deviceManufacturer = deviceMfn;
+            return this;
+        }
+
+        public FingerPrintBuilder currentOS(String currentOS) {
+            this.currentOS = currentOS;
+            return this;
+        }
+
+        public FingerPrintBuilder jailbreak(boolean isJailBreak) {
+            this.isJailBreak = isJailBreak;
+            return this;
+        }
+
+        public FingerPrint build() {
+            return new FingerPrint(this);
+        }
+
+    }
 
 }
