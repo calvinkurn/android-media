bd7cd406b0a6c7401e697f52814f9ba3ba757548
okasurya <brilliantsurya@gmail.com>
2017-07-04 10:40:32 +0700

    Reformat code & optimize import

--- customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/BrandsRecyclerViewAdapter.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/BrandsRecyclerViewAdapter.java
@@ -1,13 +1,11 @@
 package com.tokopedia.tkpd.home.adapter;
 
 import android.annotation.SuppressLint;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.tkpd.library.utils.ImageHandler;
@@ -19,13 +17,13 @@ import com.tokopedia.tkpd.R;
  * Created by Herdi_WORK on 31.01.17.
  */
 
-public class BrandsRecyclerViewAdapter extends RecyclerView.Adapter<BrandsRecyclerViewAdapter.ItemRowHolder>{
+public class BrandsRecyclerViewAdapter extends RecyclerView.Adapter<BrandsRecyclerViewAdapter.ItemRowHolder> {
 
     private Brands brands;
     private OnItemClickListener clickListener;
     private final int homeMenuWidth;
 
-    public BrandsRecyclerViewAdapter(OnItemClickListener itemListener, int homeWidth){
+    public BrandsRecyclerViewAdapter(OnItemClickListener itemListener, int homeWidth) {
         clickListener = itemListener;
         brands = new Brands();
         homeMenuWidth = homeWidth;
@@ -46,26 +44,26 @@ public class BrandsRecyclerViewAdapter extends RecyclerView.Adapter<BrandsRecycl
 
     @Override
     public void onBindViewHolder(final ItemRowHolder holder, int position) {
-        if(position<brands.getData().size()){
+        if (position < brands.getData().size()) {
             final Brand singleBrand = brands.getData().get(position);
 
-            if(singleBrand.getIsNew() != 1) {
+            if (singleBrand.getIsNew() != 1) {
                 holder.labelNew.setVisibility(View.GONE);
             } else {
                 holder.labelNew.setVisibility(View.VISIBLE);
             }
 
-            if((position+1) % 3 == 0 ){
+            if ((position + 1) % 3 == 0) {
                 holder.separator.setVisibility(View.GONE);
             } else {
                 holder.separator.setVisibility(View.VISIBLE);
             }
 
-            ImageHandler.LoadImage(holder.ivBrands,singleBrand.getLogoUrl());
+            ImageHandler.LoadImage(holder.ivBrands, singleBrand.getLogoUrl());
             holder.ivBrands.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    clickListener.onItemClicked(singleBrand.getShopName(),singleBrand, holder.getAdapterPosition());
+                    clickListener.onItemClicked(singleBrand.getShopName(), singleBrand, holder.getAdapterPosition());
                 }
             });
         }
@@ -74,7 +72,7 @@ public class BrandsRecyclerViewAdapter extends RecyclerView.Adapter<BrandsRecycl
 
     @Override
     public int getItemCount() {
-        if(brands.getData()!=null)
+        if (brands.getData() != null)
             return brands.getData().size();
         else
             return 0;
