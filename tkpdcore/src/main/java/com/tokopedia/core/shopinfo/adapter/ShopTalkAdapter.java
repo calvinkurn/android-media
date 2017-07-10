89dd373e2a23f17485881d7dccf030877d17b0a9
yoasfs <yoas.fs@gmail.com>
2017-07-10 17:28:50 +0700

    clearing code

--- tkpdcore/src/main/java/com/tokopedia/core/shopinfo/adapter/ShopTalkAdapter.java
+++ tkpdcore/src/main/java/com/tokopedia/core/shopinfo/adapter/ShopTalkAdapter.java
@@ -2,7 +2,6 @@ package com.tokopedia.core.shopinfo.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
