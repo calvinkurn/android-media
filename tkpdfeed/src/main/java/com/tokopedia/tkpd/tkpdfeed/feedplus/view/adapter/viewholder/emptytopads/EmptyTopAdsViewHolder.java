31ba9a9ee49931cca2cf5a97d415c18fcf334357
eriksuprayogi <eriksuprayogi@gmail.com>
2017-07-11 11:41:44 +0700

    add missing item

---
+++ tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/adapter/viewholder/emptytopads/EmptyTopAdsViewHolder.java
@@ -0,0 +1,22 @@
+package com.tokopedia.tkpd.tkpdfeed.feedplus.view.adapter.viewholder.emptytopads;
+
+import android.view.View;
+
+import com.tokopedia.core.base.adapter.viewholders.AbstractViewHolder;
+import com.tokopedia.tkpd.tkpdfeed.feedplus.view.viewmodel.EmptyTopAdsModel;
+
+/**
+ * @author by nisie on 7/11/17.
+ */
+
+public class EmptyTopAdsViewHolder extends AbstractViewHolder<EmptyTopAdsModel> {
+
+    public EmptyTopAdsViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void bind(EmptyTopAdsModel element) {
+
+    }
+}
---
+++ tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/viewmodel/EmptyTopAdsModel.java
@@ -0,0 +1,8 @@
+package com.tokopedia.tkpd.tkpdfeed.feedplus.view.viewmodel;
+
+/**
+ * @author by nisie on 7/11/17.
+ */
+
+public class EmptyTopAdsModel {
+}
