cb3896929b9d236ecd1351cd1a7f45ca7700b40d
Angga Prasetiyo <agapras@gmail.com>
2017-07-11 16:06:46 +0700

    base url per varian build

--- customerapp/src/alpha/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
+++ customerapp/src/alpha/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
@@ -26,5 +26,6 @@ public class ConsumerAppBaseUrl {
     public static final String BASE_MOBILE_DOMAIN = "https://m-alpha.tokopedia.com/";
 
     public static final String BASE_DIGITAL_API_DOMAIN = "https://pulsa-api-alpha.tokopedia.com/";
+    public static final String BASE_PAYMENT_URL_DOMAIN = "https://pay-alpha.tokopedia.com";
 
 }
