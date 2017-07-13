8fbc8d02c27bf128bad2d1755dc471cc4d10427b
noiz354 <noiz354@gmail.com>
2017-07-13 13:58:13 +0700

    change to topadsbaselistfragment.

--- tkpdseller/src/main/java/com/tokopedia/seller/goldmerchant/statistic/view/fragment/GMStatisticTransactionTableFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/goldmerchant/statistic/view/fragment/GMStatisticTransactionTableFragment.java
@@ -3,16 +3,15 @@ package com.tokopedia.seller.goldmerchant.statistic.view.fragment;
 import android.support.v4.app.Fragment;
 
 import com.tokopedia.seller.base.view.adapter.BaseListAdapter;
-import com.tokopedia.seller.base.view.fragment.BaseListFragment;
 import com.tokopedia.seller.goldmerchant.statistic.view.adapter.GMStatisticTransactionTableAdapter;
 import com.tokopedia.seller.goldmerchant.statistic.view.adapter.model.GMStatisticTransactionTableModel;
 import com.tokopedia.seller.goldmerchant.statistic.view.presenter.GMStatisticTransactionTablePresenter;
+import com.tokopedia.seller.topads.keyword.view.fragment.TopAdsBaseListFragment;
 
 /**
- * Created by normansyahputa on 7/13/17.
+ * @author normansyahputa on 7/13/17.
  */
-
-public class GMStatisticTransactionTableFragment extends BaseListFragment<GMStatisticTransactionTablePresenter, GMStatisticTransactionTableModel> {
+public class GMStatisticTransactionTableFragment extends TopAdsBaseListFragment<GMStatisticTransactionTablePresenter, GMStatisticTransactionTableModel> {
 
     public static final String TAG = "GMStatisticTransactionT";
 
