836e9834b453388a34e1ec34ad4d53907b4315ad
Nisie <nathaniasutedja@gmail.com>
2017-07-13 11:40:10 +0700

    Update ConsumerMainApplication.java

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -60,6 +60,7 @@ public class ConsumerMainApplication extends ConsumerRouterApplication {
         TkpdBaseURL.TOKO_CASH_DOMAIN = ConsumerAppBaseUrl.BASE_TOKO_CASH_DOMAIN;
         TkpdBaseURL.BASE_ACTION = ConsumerAppBaseUrl.BASE_DOMAIN + "v4/action/";
         TkpdBaseURL.DIGITAL_API_DOMAIN = ConsumerAppBaseUrl.BASE_DIGITAL_API_DOMAIN;
+        TkpdBaseURL.TopCash.HMAC_KEY = AuthUtil.KEY.TOKO_CASH_HMAC;
         TkpdBaseURL.GRAPHQL_DOMAIN = ConsumerAppBaseUrl.GRAPHQL_DOMAIN;
     }
 
