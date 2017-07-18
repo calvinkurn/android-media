33257166e27c1782e04c5ae5196f40b22a5019b4
nisie <nathania.sutedja@tokopedia.com>
2017-07-18 13:00:19 +0700

    Fix layout feed when empty

--- tkpdtopadssdk/src/main/java/com/tokopedia/topads/sdk/data/ModelConverter.java
+++ tkpdtopadssdk/src/main/java/com/tokopedia/topads/sdk/data/ModelConverter.java
@@ -42,9 +42,10 @@ public class ModelConverter {
         return viewModel;
     }
 
-    public static ShopFeedViewModel convertToShopFeedViewModel(Data data) {
+    public static ShopFeedViewModel convertToShopFeedViewModel(Data data, DisplayMode displayMode) {
         ShopFeedViewModel viewModel = new ShopFeedViewModel();
         viewModel.setData(data);
+        viewModel.setDisplayMode(displayMode);
         return viewModel;
     }
 
@@ -72,7 +73,7 @@ public class ModelConverter {
             } else if(displayMode == DisplayMode.FEED && visitable instanceof ShopGridViewModel
                     || displayMode == DisplayMode.FEED_EMPTY && visitable instanceof ShopGridViewModel) {
                 list.set(i, ModelConverter.convertToShopFeedViewModel(((ShopGridViewModel)
-                        visitable).getData()));
+                        visitable).getData(), displayMode));
             }
         }
     }
