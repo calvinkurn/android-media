1d1aca9ad664c56a3d7c6959ec5fd0437020e87b
fatahillah alvarisi <alvarisi01@gmail.com>
2017-07-19 16:27:30 +0700

    T29848 Applink digital checkout - apps tokopedia can generate idem_potency key if the idem_potency_key is null

--- tkpddigital/src/main/java/com/tokopedia/digital/cart/activity/CartDigitalActivity.java
+++ tkpddigital/src/main/java/com/tokopedia/digital/cart/activity/CartDigitalActivity.java
@@ -9,11 +9,14 @@ import android.support.v4.app.TaskStackBuilder;
 
 import com.airbnb.deeplinkdispatch.DeepLink;
 import com.tokopedia.core.app.BasePresenterActivity;
+import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.gcm.Constants;
+import com.tokopedia.core.network.retrofit.utils.AuthUtil;
 import com.tokopedia.core.router.SellerAppRouter;
 import com.tokopedia.core.router.digitalmodule.passdata.DigitalCheckoutPassData;
 import com.tokopedia.core.router.home.HomeRouter;
 import com.tokopedia.core.util.GlobalConfig;
+import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.digital.R;
 import com.tokopedia.digital.cart.fragment.CartDigitalFragment;
 
@@ -45,7 +48,9 @@ public class CartDigitalActivity extends BasePresenterActivity implements
         passData.setUtmMedium(bundle.getString(DigitalCheckoutPassData.PARAM_UTM_MEDIUM));
         passData.setUtmSource(bundle.getString(DigitalCheckoutPassData.PARAM_UTM_SOURCE));
         passData.setUtmContent(bundle.getString(DigitalCheckoutPassData.PARAM_UTM_CONTENT));
-        passData.setIdemPotencyKey(bundle.getString(DigitalCheckoutPassData.PARAM_IDEM_POTENCY_KEY));
+        passData.setIdemPotencyKey(bundle.getString(DigitalCheckoutPassData.PARAM_IDEM_POTENCY_KEY,
+                generateATokenRechargeCheckout())
+        );
         return new Intent(context, CartDigitalActivity.class)
                 .putExtra(EXTRA_PASS_DIGITAL_CART_DATA, passData);
     }
@@ -120,5 +125,11 @@ public class CartDigitalActivity extends BasePresenterActivity implements
     @Override
     public void setTitleCart(String title) {
         toolbar.setTitle(title);
+    }
+
+    private static String generateATokenRechargeCheckout() {
+        String timeMillis = String.valueOf(System.currentTimeMillis());
+        String token = AuthUtil.md5(timeMillis);
+        return SessionHandler.getLoginID(MainApplication.getAppContext()) + "_" + (token.isEmpty() ? timeMillis : token);
     }
 }
