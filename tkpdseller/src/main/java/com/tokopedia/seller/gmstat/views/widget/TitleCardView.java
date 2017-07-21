f964f53ee91045882b34737a368ba3d2f64f6fc2
hendry <hendry.setiadi.89@gmail.com>
2017-07-21 15:00:32 +0700

    create loading_state_view as component of titlecardview

--- tkpdseller/src/main/java/com/tokopedia/seller/gmstat/views/widget/TitleCardView.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/gmstat/views/widget/TitleCardView.java
@@ -23,23 +23,17 @@ import com.tokopedia.seller.R;
  */
 
 public class TitleCardView extends CardView {
-
-    @LayoutRes
-    public static final int DEFAULT_LOADING_LAYOUT_RES = R.layout.widget_line_chart_container_loading;
     @LayoutRes
     public static final int DEFAULT_LOADING_TITLE_LAYOUT_RES = R.layout.widget_title_card_loading;
 
-    FrameLayout mFrameLayout;
+    LoadingStateView mLoadingStateView;
     OnArrowDownClickListener onArrowDownClickListener;
     private TextView tvTitle;
     private float mTitleTextSize;
     private ImageView ivArrowDown;
     private ViewGroup vgTitle;
 
-    private View emptyView;
-    private View loadingView;
     private View loadingTitleView;
-    private View contentView;
 
     private CharSequence mTitleString;
     private int mLoadingLayoutRes;
@@ -71,7 +65,7 @@ public class TitleCardView extends CardView {
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TitleCardView);
         mTitleString = a.getString(R.styleable.TitleCardView_title);
         mTitleTextSize = a.getDimensionPixelSize(R.styleable.TitleCardView_title_size, 0);
-        mLoadingLayoutRes = a.getResourceId(R.styleable.TitleCardView_loading_layout, DEFAULT_LOADING_LAYOUT_RES);
+        mLoadingLayoutRes = a.getResourceId(R.styleable.TitleCardView_loading_layout, 0);
         mEmptyLayoutRes = a.getResourceId(R.styleable.TitleCardView_empty_layout, 0);
         mIconDrawable = a.getDrawable(R.styleable.TitleCardView_icon);
         mUseGradientTitleLoading = a.getBoolean(R.styleable.TitleCardView_use_gradient_title_loading, true);
@@ -95,7 +89,13 @@ public class TitleCardView extends CardView {
         }
         ivArrowDown = (ImageView) vgTitleContent.findViewById(R.id.iv_arrow_down);
 
-        mFrameLayout = (FrameLayout) view.findViewById(R.id.frame_content);
+        mLoadingStateView = (LoadingStateView) view.findViewById(R.id.loading_state_view);
+        if (mEmptyLayoutRes!= 0) {
+            mLoadingStateView.setEmptyViewRes(mEmptyLayoutRes);
+        }
+        if (mLoadingLayoutRes!= 0) {
+            mLoadingStateView.setLoadingViewRes(mLoadingLayoutRes);
+        }
 
         setTitle(mTitleString);
         ivArrowDown.setVisibility(View.GONE);
@@ -103,7 +103,7 @@ public class TitleCardView extends CardView {
         this.setPreventCornerOverlap(false);
 
         setAddStatesFromChildren(true);
-        mFrameLayout.setAddStatesFromChildren(true);
+        mLoadingStateView.setAddStatesFromChildren(true);
     }
 
     public void setOnArrowDownClickListener(final OnArrowDownClickListener onArrowDownClickListener) {
@@ -148,77 +148,46 @@ public class TitleCardView extends CardView {
             // Carry on adding the View...
             super.addView(child, index, params);
         } else {
-            // remove the link between child and previous parent before add (if any)
-            if (child.getParent()!= null) {
-                ViewGroup viewParent = (ViewGroup) child.getParent();
-                viewParent.removeView(child);
-            }
-            mFrameLayout.addView(child, params);
-            // assume first view added is the content;
-            if (contentView == null) {
-                contentView = child;
-            }
+            mLoadingStateView.addView(child, index, params);
         }
     }
 
     public void setLoadingViewRes(int loadingViewRes) {
-        View emptyView = LayoutInflater.from(getContext()).inflate(loadingViewRes, mFrameLayout, false);
-        setLoadingView(emptyView);
+        mLoadingStateView.setLoadingViewRes(loadingViewRes);
     }
 
     public View getLoadingView() {
-        return loadingView;
+        return mLoadingStateView.getLoadingView();
     }
 
     public View getContentView() {
-        return contentView;
+        return mLoadingStateView.getContentView();
     }
 
     public void setLoadingView(View loadingView) {
-        if (this.loadingView!= null) {
-            mFrameLayout.removeView(this.loadingView);
-        }
-        this.loadingView = loadingView;
-        loadingView.setVisibility(View.GONE);
-        addView(loadingView);
+        mLoadingStateView.setLoadingView(loadingView);
     }
 
     public void setEmptyViewRes(int emptyViewRes) {
-        View emptyView = LayoutInflater.from(getContext()).inflate(emptyViewRes, mFrameLayout, false);
-        setEmptyView(emptyView);
+        mLoadingStateView.setEmptyViewRes(emptyViewRes);
     }
 
     public View getEmptyView() {
-        return emptyView;
+        return mLoadingStateView.getEmptyView();
     }
 
     public void setEmptyView(View emptyView) {
-        if (this.emptyView!= null) {
-            mFrameLayout.removeView(this.emptyView);
-        }
-        this.emptyView = emptyView;
-        emptyView.setVisibility(View.GONE);
-        addView(emptyView);
+        mLoadingStateView.setEmptyView(emptyView);
     }
 
     // showing that the content to loading state
     public void setLoadingState (boolean isLoading){
-        if (loadingView == null) {
-            setDefaultLoadingView();
-        }
-        if (isLoading && this.loadingView != null) {
-            if (contentView!= null) {
-                contentView.setVisibility(View.GONE);
-            }
-            if (emptyView!= null) {
-                emptyView.setVisibility(View.GONE);
-            }
-            loadingView.setVisibility(View.VISIBLE);
+        mLoadingStateView.setLoadingState(isLoading);
+        if (isLoading && mLoadingStateView.getLoadingView()!= null) {
             if (mUseGradientTitleLoading) {
                 setLoadingTitleState(true);
             }
         } else {
-            setContentVisible();
             setLoadingTitleState(false);
         }
     }
@@ -244,50 +213,13 @@ public class TitleCardView extends CardView {
     }
 
     public void setEmptyState (boolean isEmpty){
-        if (emptyView == null) {
-            setDefaultEmptyView();
-        }
-        if (isEmpty && emptyView!= null) {
-            if (contentView!= null){
-                contentView.setVisibility(View.GONE);
-            }
-            if (this.loadingView != null) {
-                loadingView.setVisibility(View.GONE);
-            }
-            emptyView.setVisibility(View.VISIBLE);
-        } else {
-            setContentVisible();
-        }
-    }
-
-    public void setContentVisible() {
-        if (this.loadingView!= null) {
-            loadingView.setVisibility(View.GONE);
-        }
-        if (emptyView!=null) {
-            emptyView.setVisibility(View.GONE);
-        }
-        contentView.setVisibility(View.VISIBLE);
-    }
-
-    public void setDefaultLoadingView(){
-        if (mLoadingLayoutRes == 0) return;
-        this.loadingView = LayoutInflater.from(getContext()).inflate(mLoadingLayoutRes, mFrameLayout, false);
-        loadingView.setVisibility(View.GONE);
-        addView(loadingView);
+        mLoadingStateView.setEmptyState(isEmpty);
     }
 
     public void setDefaultLoadingTitleView(){
         this.loadingTitleView = LayoutInflater.from(getContext()).inflate(DEFAULT_LOADING_TITLE_LAYOUT_RES, vgTitle, false);
         loadingTitleView.setVisibility(View.GONE);
         vgTitle.addView(loadingTitleView);
-    }
-
-    public void setDefaultEmptyView(){
-        if (mEmptyLayoutRes == 0) return;
-        this.emptyView = LayoutInflater.from(getContext()).inflate(mEmptyLayoutRes, mFrameLayout, false);
-        emptyView.setVisibility(View.GONE);
-        addView(emptyView);
     }
 
     public void setTitle(CharSequence title) {
