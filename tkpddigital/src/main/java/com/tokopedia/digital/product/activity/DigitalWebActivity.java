c7ac79fcfb6e7be79a4cd7a77f67583abda0c3b9
Angga Prasetiyo <agapras@gmail.com>
2017-08-03 14:13:17 +0700

    add menu toolbar digitalwebactivity

--- tkpddigital/src/main/java/com/tokopedia/digital/product/activity/DigitalWebActivity.java
+++ tkpddigital/src/main/java/com/tokopedia/digital/product/activity/DigitalWebActivity.java
@@ -4,9 +4,17 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.tokopedia.core.app.BasePresenterActivity;
+import com.tokopedia.core.router.InboxRouter;
+import com.tokopedia.core.router.home.HomeRouter;
 import com.tokopedia.core.webview.fragment.FragmentGeneralWebView;
 import com.tokopedia.digital.R;
 
@@ -22,6 +30,29 @@ public class DigitalWebActivity extends BasePresenterActivity
     public static Intent newInstance(Context context, String url) {
         return new Intent(context, DigitalWebActivity.class)
                 .putExtra(EXTRA_URL, url);
+    }
+
+    @Override
+    protected void setupToolbar() {
+        super.setupToolbar();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window.setStatusBarColor(getResources().getColor(R.color.white, null));
+            } else {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    //noinspection deprecation
+                    window.setStatusBarColor(getResources().getColor(R.color.white));
+                }
+            }
+        }
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setHomeAsUpIndicator(com.tokopedia.core.R.drawable.ic_webview_back_button);
+
+        toolbar.setBackgroundResource(com.tokopedia.core.R.color.white);
+        toolbar.setTitleTextAppearance(this, com.tokopedia.core.R.style.WebViewToolbarText);
     }
 
     @Override
@@ -81,5 +112,22 @@ public class DigitalWebActivity extends BasePresenterActivity
     @Override
     public void onWebViewProgressLoad() {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(com.tokopedia.core.R.menu.menu_web_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == com.tokopedia.core.R.id.menu_home) {
+            startActivity(HomeRouter.getHomeActivity(this));
+        } else if (item.getItemId() == com.tokopedia.core.R.id.menu_help) {
+            startActivity(InboxRouter.getContactUsActivityIntent(this));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
