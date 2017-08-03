20c22d5997eb19110dfcbd36693e277b95fa8a56
yoasfs <yoas.fs@gmail.com>
2017-08-03 17:36:40 +0700

    update code

--- tkpdcore/src/main/java/com/tokopedia/core/util/TagHandlerUtil.java
+++ tkpdcore/src/main/java/com/tokopedia/core/util/TagHandlerUtil.java
@@ -10,30 +10,35 @@ import org.xml.sax.XMLReader;
  */
 
 public class TagHandlerUtil implements Html.TagHandler {
-    boolean first= true;
-    String parent=null;
-    int index=1;
+    boolean first = true;
+    String parent = null;
+    int index = 1;
+
     @Override
-    public void handleTag(boolean opening, String tag, Editable output,
+    public void handleTag(boolean opening,
+                          String tag,
+                          Editable output,
                           XMLReader xmlReader) {
 
-        if(tag.equals("ul")) parent="ul";
-        else if(tag.equals("ol")) parent="ol";
-        if(tag.equals("li")){
-            if(parent.equals("ul")){
-                if(first){
+        if(tag.equals("ul"))
+            parent="ul";
+        else if(tag.equals("ol"))
+            parent="ol";
+
+        if(tag.equals("li")) {
+            if(parent.equals("ul")) {
+                if(first) {
                     output.append("\n\t•");
-                    first= false;
+                    first = false;
                 }else{
                     first = true;
                 }
-            }
-            else{
-                if(first){
+            } else {
+                if(first) {
                     output.append("\n\t"+index+". ");
                     first= false;
                     index++;
-                }else{
+                } else {
                     first = true;
                 }
             }
