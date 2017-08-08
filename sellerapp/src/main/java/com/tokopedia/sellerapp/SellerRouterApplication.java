f570a9956f31560e2b689761e6a240dd001183ff
nathaniel <nathaniel.khuana@tokopedia.com>
2017-08-08 08:46:19 +0700

    Bug fix compile error on seller app

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -37,6 +37,7 @@ import com.tokopedia.core.welcome.WelcomeActivity;
 import com.tokopedia.digital.cart.activity.CartDigitalActivity;
 import com.tokopedia.digital.product.activity.DigitalProductActivity;
 import com.tokopedia.digital.product.activity.DigitalWebActivity;
+import com.tokopedia.digital.tokocash.activity.ActivateTokoCashActivity;
 import com.tokopedia.digital.widget.activity.DigitalCategoryListActivity;
 import com.tokopedia.payment.router.IPaymentModuleRouter;
 import com.tokopedia.profilecompletion.data.factory.ProfileSourceFactory;
@@ -378,6 +379,11 @@ public class SellerRouterApplication extends MainApplication
         intent.putExtras(bundle);
         intent.setData(Uri.parse(applinks));
         deepLinkDelegate.dispatchFrom(activity, intent);
+    }
+
+    @Override
+    public Intent instanceIntentTokoCashActivation() {
+        return ActivateTokoCashActivity.newInstance(this);
     }
 
     @Override
