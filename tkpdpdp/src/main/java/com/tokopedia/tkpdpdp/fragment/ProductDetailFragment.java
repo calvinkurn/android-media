f23b0638aa437f89c5212badf37a41a314f37dfb
alifanuraniputri <alifanuraniputri@gmail.com>
2017-08-16 11:00:01 +0700

    fix UI on most helpful

--- tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/fragment/ProductDetailFragment.java
+++ tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/fragment/ProductDetailFragment.java
@@ -878,6 +878,7 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
     public void showMostHelpfulReview(List<Review> reviews) {
         Log.d("alifanuraniputri", "alifanuraniputri: ");
         this.mostHelpfulReviewView.renderData(reviews);
+        this.mostHelpfulReviewView.updateTotalReviews(productData.getStatistic().getProductReviewCount());
         this.mostHelpfulReviewView.setVisibility(View.VISIBLE);
     }
 
