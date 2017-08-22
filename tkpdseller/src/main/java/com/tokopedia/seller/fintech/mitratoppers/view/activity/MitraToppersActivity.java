7bcab985ce9b4f558038337ba1af0ff0b58be35a
nathaniel <nathaniel.khuana@tokopedia.com>
2017-08-22 19:21:11 +0700

    Rename package for mitra toppers

--- tkpdseller/src/main/java/com/tokopedia/seller/fintech/mitratoppers/view/activity/MitraToppersActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/fintech/mitratoppers/view/activity/MitraToppersActivity.java
@@ -1,9 +1,9 @@
-package com.tokopedia.seller.digital.mitratoppers.view.activity;
+package com.tokopedia.seller.fintech.mitratoppers.view.activity;
 
 import android.support.v4.app.Fragment;
 
 import com.tokopedia.seller.base.view.activity.BaseSimpleActivity;
-import com.tokopedia.seller.digital.mitratoppers.view.fragment.MitraToppersFragment;
+import com.tokopedia.seller.fintech.mitratoppers.view.fragment.MitraToppersFragment;
 
 /**
  * Created by nathan on 8/18/17.
