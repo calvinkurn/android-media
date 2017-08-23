d9e3535a36353275001a14d4d1292e95c1ec7905
alifanuraniputri <alifanuraniputri@gmail.com>
2017-08-23 11:35:27 +0700

    using adapter

--- tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/VariantActivity.java
+++ tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/VariantActivity.java
@@ -4,37 +4,49 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tokopedia.core.analytics.model.Product;
 import com.tokopedia.core.app.TActivity;
+import com.tokopedia.core.network.entity.variant.Option;
 import com.tokopedia.core.network.entity.variant.ProductVariant;
+import com.tokopedia.core.network.entity.variant.VariantDatum;
+import com.tokopedia.tkpdpdp.adapter.VariantOptionAdapter;
 import com.tokopedia.tkpdpdp.adapter.VariantParentAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class VariantActivity extends TActivity {
 
     public static final String KEY_VARIANT_DATA = "VARIANT_DATA";
 
-    private RecyclerView parentRecyclerView;
+    //private RecyclerView parentRecyclerView;
     private TextView topBarTitle;
-
-    private VariantParentAdapter parentAdapter;
+    private ProductVariant variant;
+   // private VariantParentAdapter parentAdapter;
+    private ImageView productImage;
+    private TextView productName;
+    private TextView productPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        variant = getIntent().getParcelableExtra(KEY_VARIANT_DATA);
         setContentView(R.layout.activity_variant);
         hideToolbar();
         initView();
-        setupAdapter();
-        setupRecyclerView();
+        setupData();
+        //setupRecyclerView();
         setupTopbar();
     }
 
 
     private void initView() {
         topBarTitle = (TextView) findViewById(R.id.simple_top_bar_title);
-        parentRecyclerView = (RecyclerView) findViewById(R.id.variant_parent_list);
+        //parentRecyclerView = (RecyclerView) findViewById(R.id.variant_parent_list);
         findViewById(R.id.simple_top_bar_close_button)
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -42,16 +54,22 @@ public class VariantActivity extends TActivity {
                         finish();
                     }
                 });
+        productName = (TextView) itemView.findViewById(R.id.variant_product_name);
+        productPrice = (TextView) itemView.findViewById(R.id.variant_product_price);
+        productImage = (ImageView) itemView.findViewById(R.id.variant_image_title);
+    }
+
+    public void setupData() {
+
     }
 
     private void setupAdapter() {
-        ProductVariant variant = getIntent().getParcelableExtra(KEY_VARIANT_DATA);
-        parentAdapter = new VariantParentAdapter(this, variant);
+        //parentAdapter = new VariantParentAdapter(this, variant);
     }
 
     private void setupRecyclerView() {
-        parentRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        parentRecyclerView.setAdapter(parentAdapter);
+        //parentRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        //parentRecyclerView.setAdapter(parentAdapter);
     }
 
     private void setupTopbar() {
