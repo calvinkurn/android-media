5cee47ceddcaed94c20a5f6c8c1434cf9b274120
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2017-08-31 19:03:37 +0700

    refactor repository pending tokocash cashback and add new tokocash repository

--- tkpddigital/src/main/java/com/tokopedia/digital/tokocash/domain/TokoCashRepository.java
+++ tkpddigital/src/main/java/com/tokopedia/digital/tokocash/domain/TokoCashRepository.java
@@ -1,48 +1,64 @@
 package com.tokopedia.digital.tokocash.domain;
 
-import com.tokopedia.core.network.apiservices.transaction.TokoCashCashBackService;
+import com.tokopedia.core.network.apiservices.transaction.TokoCashService;
 import com.tokopedia.core.network.retrofit.response.TkpdDigitalResponse;
-import com.tokopedia.core.network.retrofit.utils.TKPDMapParam;
-import com.tokopedia.core.util.SessionHandler;
-import com.tokopedia.digital.tokocash.entity.ResponseCashBack;
-import com.tokopedia.digital.tokocash.mapper.ITokoCashMapper;
-import com.tokopedia.digital.tokocash.model.CashBackData;
+import com.tokopedia.digital.tokocash.entity.TokoCashHistoryEntity;
+import com.tokopedia.digital.tokocash.mapper.ActivateTokoCashMapper;
+import com.tokopedia.digital.tokocash.model.ActivateTokoCashData;
 
 import retrofit2.Response;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
- * Created by kris on 6/16/17. Tokopedia
+ * Created by nabillasabbaha on 7/24/17.
  */
 
 public class TokoCashRepository implements ITokoCashRepository {
 
-    private final TokoCashCashBackService tokoCashCashBackService;
-    private final ITokoCashMapper tokoCashMapper;
+    private final TokoCashService tokoCashService;
+    private final ActivateTokoCashMapper activateTokoCashMapper;
 
-    public TokoCashRepository(TokoCashCashBackService tokoCashCashBackService,
-                              ITokoCashMapper iTokoCashMapper) {
-        this.tokoCashCashBackService = tokoCashCashBackService;
-        this.tokoCashMapper = iTokoCashMapper;
+    public TokoCashRepository(TokoCashService tokoCashService) {
+        this.tokoCashService = tokoCashService;
+        this.activateTokoCashMapper = new ActivateTokoCashMapper();
     }
 
     @Override
-    public Observable<CashBackData> getTokoCashPending() {
-        TKPDMapParam<String, String> tokoCashPendingParams = new TKPDMapParam<>();
-        tokoCashPendingParams.put("msisdn", SessionHandler.getPhoneNumber());
-        return tokoCashCashBackService.getApi().getTokoCashPending(tokoCashPendingParams)
-                .map(getFuncResponseToCashBackData());
+    public Observable<ActivateTokoCashData> requestOTPWallet() {
+        return tokoCashService.getApi()
+                .requestOtpWallet()
+                .map(activateTokoCashMapper)
+                .subscribeOn(Schedulers.newThread())
+                .unsubscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 
-    private Func1<Response<TkpdDigitalResponse>, CashBackData>
-    getFuncResponseToCashBackData() {
-        return new Func1<Response<TkpdDigitalResponse>, CashBackData>() {
-            @Override
-            public CashBackData call(Response<TkpdDigitalResponse> tkpdDigitalResponse) {
-                return tokoCashMapper.transformTokoCashCashbackData(tkpdDigitalResponse.body()
-                        .convertDataObj(ResponseCashBack.class));
-            }
-        };
+    @Override
+    public Observable<ActivateTokoCashData> linkedWalletToTokoCash(String otpCode) {
+        return tokoCashService.getApi()
+                .linkedWalletToTokocash(otpCode)
+                .map(activateTokoCashMapper)
+                .subscribeOn(Schedulers.newThread())
+                .unsubscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Observable<TokoCashHistoryEntity> getTokoCashHistoryData(String type, String startDate,
+                                                                    String endDate, String afterId) {
+        return tokoCashService.getApi().getHistoryTokocash(type, startDate, endDate, afterId)
+                .flatMap(new Func1<Response<TkpdDigitalResponse>, Observable<TokoCashHistoryEntity>>() {
+                    @Override
+                    public Observable<TokoCashHistoryEntity> call(Response<TkpdDigitalResponse> response) {
+                        return Observable
+                                .just(response.body().convertDataObj(TokoCashHistoryEntity.class));
+                    }
+                })
+                .subscribeOn(Schedulers.newThread())
+                .unsubscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
