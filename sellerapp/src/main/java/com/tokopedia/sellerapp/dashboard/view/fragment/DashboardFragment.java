6ac502b6306b7ebaa7257628e86cbef1b6c1d4bd
nathaniel <nathaniel.khuana@tokopedia.com>
2017-09-06 10:14:46 +0700

    Adding dashboard fragment layout

--- sellerapp/src/main/java/com/tokopedia/sellerapp/dashboard/view/fragment/DashboardFragment.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/dashboard/view/fragment/DashboardFragment.java
@@ -1,6 +1,13 @@
 package com.tokopedia.sellerapp.dashboard.view.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.tokopedia.core.base.presentation.BaseDaggerFragment;
+import com.tokopedia.sellerapp.R;
 
 /**
  * Created by nathan on 9/6/17.
@@ -10,6 +17,13 @@ public class DashboardFragment extends BaseDaggerFragment {
 
     public static DashboardFragment newInstance() {
         return new DashboardFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        return view;
     }
 
     @Override
