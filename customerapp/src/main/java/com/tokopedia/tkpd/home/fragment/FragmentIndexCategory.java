3afcb58bc7b3e4caf24de4ae272880c9acc207a0
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2017-09-06 19:00:38 +0700

    refactor bottom sheet in customer app (tokocash cashback pending) using bottom sheet in tkpd design

--- customerapp/src/main/java/com/tokopedia/tkpd/home/fragment/FragmentIndexCategory.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/fragment/FragmentIndexCategory.java
@@ -79,6 +79,7 @@ import com.tokopedia.core.util.NonScrollLinearLayoutManager;
 import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.var.TkpdCache;
 import com.tokopedia.core.widgets.DividerItemDecoration;
+import com.tokopedia.design.bottomsheet.BottomSheetView;
 import com.tokopedia.digital.product.activity.DigitalProductActivity;
 import com.tokopedia.digital.tokocash.model.CashBackData;
 import com.tokopedia.digital.widget.domain.DigitalWidgetRepository;
@@ -166,7 +167,7 @@ public class FragmentIndexCategory extends TkpdBaseV4Fragment implements
     private TokoCashPresenter tokoCashPresenter;
     private SnackbarRetry messageSnackbar;
     private TokoCashBroadcastReceiver tokoCashBroadcastReceiver;
-    private BottomSheetTokoCash bottomSheetDialogTokoCash;
+    private BottomSheetView bottomSheetDialogTokoCash;
 
     private DrawerTokoCash tokoCashData;
 
@@ -787,13 +788,42 @@ public class FragmentIndexCategory extends TkpdBaseV4Fragment implements
 
     @Override
     public void onReceivePendingCashBack(CashBackData cashBackData) {
-        //TODO Uncomment Later
         if (cashBackData.getAmount() > 0) {
-            bottomSheetDialogTokoCash = new BottomSheetTokoCash(getActivity());
-            bottomSheetDialogTokoCash.setCashBackText(cashBackData.getAmountText());
-            bottomSheetDialogTokoCash.setActivationUrl(tokoCashData.getRedirectUrl());
+            bottomSheetDialogTokoCash = new BottomSheetView(getActivity());
+            bottomSheetDialogTokoCash.setListener(getActinListener());
+            bottomSheetDialogTokoCash.renderBottomSheet(new BottomSheetView
+                    .BottomSheetField.BottomSheetFieldBuilder()
+                    .setTitle(getString(R.string.toko_cash_pending_title))
+                    .setBody(String.format(getString(R.string.toko_cash_pending_body),
+                            cashBackData.getAmountText()))
+                    .setImg(R.drawable.group_2)
+                    .setUrlButton(tokoCashData.getRedirectUrl(),
+                            getString(R.string.toko_cash_pending_proceed_button))
+                    .build());
             holder.tokoCashHeaderView.showPendingTokoCash(cashBackData.getAmountText());
         }
+    }
+
+    private BottomSheetView.ActionListener getActinListener() {
+        return new BottomSheetView.ActionListener() {
+            @Override
+            public void clickOnTextLink(String url) {
+
+            }
+
+            @Override
+            public void clickOnButton(String url) {
+                String seamlessUrl;
+                seamlessUrl = URLGenerator.generateURLSessionLogin((Uri.encode(url)),
+                        getContext());
+                if (getActivity() instanceof Activity) {
+                    if ((getActivity()).getApplication() instanceof TkpdCoreRouter) {
+                        ((TkpdCoreRouter) (getActivity()).getApplication())
+                                .goToWallet(getActivity(), seamlessUrl);
+                    }
+                }
+            }
+        };
     }
 
     @Override
