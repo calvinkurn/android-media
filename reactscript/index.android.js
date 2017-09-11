1fa9e91ac4430679e17e2eb492b66f20980f6e9b
Yogie Putra <yogie.putra8@gmail.com>
2017-09-11 09:32:06 +0700

    Activate codepush

--- reactscript/index.android.js
+++ reactscript/index.android.js
@@ -15,7 +15,7 @@ import {
 import OfficialStore from './src/pages/official-store/setup'
 
 
-// let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
 class Home extends Component {
   render(){
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
   },
 });
 
-// Home = codePush({ checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.ON_NEXT_RESUME })(Home);
+Home = codePush({ checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.ON_NEXT_RESUME })(Home);
 module.exports = Home;
 AppRegistry.registerComponent('MAIN', () => Home);
 
