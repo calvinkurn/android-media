8756542db3f8dff48feae1e4e6f3af9438742f8b
alvarisi <alvarisi01@gmail.com>
2017-09-13 15:46:50 +0700

    move react native classes from core to module

---
+++ tkpdreactnative/src/main/java/com/tokopedia/tkpdreactnative/react/ReactNativeHostFactory.java
@@ -0,0 +1,86 @@
+package com.tokopedia.tkpdreactnative.react;
+
+import android.app.Application;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.tokopedia.core.util.GlobalConfig;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author ricoharisin .
+ */
+
+public class ReactNativeHostFactory {
+
+
+    public static ReactNativeHost init(Application application) {
+        /*reactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(application)
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("reactscript/index.android")
+                .addPackage(new MainReactPackage())
+                .addPackage(new CoreReactPackage())
+                .setUseDeveloperSupport(true)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();*/
+
+        return createReactNativeHost(application);
+
+    }
+
+    private static ReactNativeHost createReactNativeHost(final Application application) {
+        return new ReactNativeHost(application) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return GlobalConfig.isAllowDebuggingTools();
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return getListPackages(application);
+            }
+
+            @Override
+            protected String getJSBundleFile() {
+                return CodePush.getJSBundleFile();
+            }
+        };
+    }
+
+    private static ReactNativeHost createReactNativeHostDev(final Application application) {
+        return new ReactNativeHost(application) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return true;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return getListPackages(application);
+            }
+
+            @Override
+            protected String getJSBundleFile() {
+                return "index.android.bundle";
+            }
+
+            @Override
+            protected String getJSMainModuleName() {
+                return "reactscript/index.android";
+            }
+        };
+    }
+
+    private static List<ReactPackage> getListPackages(Application application) {
+        return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                new CoreReactPackage(),
+                new CodePush(ReactConst.CODE_PUSH_DEPLOYMENT_KEY, application, GlobalConfig.isAllowDebuggingTools())
+        );
+    }
+}
