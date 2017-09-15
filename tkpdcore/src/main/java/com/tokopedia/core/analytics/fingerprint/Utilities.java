193cd89d03f5d235216c2cf9b87d38109b013c0c
vishalgupta1987 <vishalgupta1987@gmail.com>
2017-09-15 11:06:12 +0530

    merge changes

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/Utilities.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/fingerprint/Utilities.java
@@ -130,8 +130,8 @@ public class Utilities {
         if (networkInfo != null) {
             if (networkInfo.isConnected()) {
                 if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                    final WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+                    WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                    WifiInfo connectionInfo = wifiManager.getConnectionInfo();
                     if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
                         ssid = connectionInfo.getSSID();
                     }
