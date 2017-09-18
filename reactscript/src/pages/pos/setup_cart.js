667f4ea7eb51562c4c9762c54c6e0d6a5003c618
Syed Haani <haani.rizvi@tokopedia.com>
2017-09-18 15:33:03 +0530

    Remove onRequestClose warn

--- reactscript/src/pages/pos/setup_cart.js
+++ reactscript/src/pages/pos/setup_cart.js
@@ -30,7 +30,7 @@ class Root extends Component {
     render() {
         return (
             <Provider store={store}>
-                <CartContainer />
+                <CartContainer onBackPress={() => {}}/>
             </Provider>
         )
     }
