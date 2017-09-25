f811a1b1eda5841b8ed7e57e0e982b767f695a49
alvarisi <alvarisi01@gmail.com>
2017-09-25 14:27:36 +0700

    merge conflict

--- tkpdride/src/main/java/com/tokopedia/ride/common/ride/domain/model/PendingPayment.java
+++ tkpdride/src/main/java/com/tokopedia/ride/common/ride/domain/model/PendingPayment.java
@@ -1,4 +1,4 @@
-package com.tokopedia.ride.completetrip.domain.model;
+package com.tokopedia.ride.common.ride.domain.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -20,6 +20,7 @@ public class PendingPayment implements Parcelable {
     private String currencyCode;
     private List<TokoCashProduct> topUpOptions;
     private String topupUrl;
+    private boolean showTopupOptions;
 
     public PendingPayment() {
     }
@@ -37,6 +38,7 @@ public class PendingPayment implements Parcelable {
             topUpOptions = null;
         }
         topupUrl = in.readString();
+        showTopupOptions = in.readByte() != 0x00;
     }
 
     public static final Creator<PendingPayment> CREATOR = new Creator<PendingPayment>() {
@@ -107,6 +109,14 @@ public class PendingPayment implements Parcelable {
         this.categoryId = categoryId;
     }
 
+    public boolean isShowTopupOptions() {
+        return showTopupOptions;
+    }
+
+    public void setShowTopupOptions(boolean showTopupOptions) {
+        this.showTopupOptions = showTopupOptions;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -126,5 +136,6 @@ public class PendingPayment implements Parcelable {
             parcel.writeList(topUpOptions);
         }
         parcel.writeString(topupUrl);
+        parcel.writeByte((byte) (showTopupOptions ? 0x01 : 0x00));
     }
 }
