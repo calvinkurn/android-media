8df3efe513ce40a44d296e54af6c94856a8560aa
eriksuprayogi <eriksuprayogi@gmail.com>
2017-09-26 16:24:38 +0700

    remove empty state on submit search

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/fragment/ProductFragment.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/fragment/ProductFragment.java
@@ -601,7 +601,6 @@ public class ProductFragment extends BaseFragment<FragmentDiscoveryPresenter>
     public void addHotListHeader(ProductAdapter.HotListBannerModel hotListBannerModel) {
         topAdsRecyclerAdapter.setHasHeader(true);
         productAdapter.addHotListHeader(hotListBannerModel);
-        ((BrowseProductActivity) getActivity()).removeEmptyState();
         Log.d(TAG, "addHotListHeader");
     }
 
