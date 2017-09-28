92cb77d6b95136f89f07e8ceb6662a4a4113234b
Hafizh Herdi <hherdinaufal@gmail.com>
2017-09-28 19:25:13 +0700

    reworking on favoritefragment

--- customerapp/src/main/java/com/tokopedia/tkpd/home/favorite/view/FragmentFavorite.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/favorite/view/FragmentFavorite.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
+import com.google.firebase.perf.metrics.Trace;
 import com.tkpd.library.ui.view.LinearLayoutManager;
 import com.tokopedia.core.analytics.AppScreen;
 import com.tokopedia.core.analytics.ScreenTracking;
@@ -72,11 +73,12 @@ public class FragmentFavorite extends BaseDaggerFragment
     private boolean isTopAdsShopNetworkFailed;
     private View favoriteShopViewSelected;
     private TopAdsShopItem shopItemSelected;
+    private Trace trace;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        TrackingUtils.startTrace("favorite_trace");
+        trace = TrackingUtils.startTrace("favorite_trace");
         super.onCreate(savedInstanceState);
     }
 
@@ -97,6 +99,8 @@ public class FragmentFavorite extends BaseDaggerFragment
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+        if(trace!=null)
+            trace.stop();
         favoritePresenter.detachView();
     }
 
