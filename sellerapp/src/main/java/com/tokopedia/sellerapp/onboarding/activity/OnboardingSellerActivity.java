461ab9aa7c057fcf578535894ecc2f7d2388aede
Rico Harisin <rico.harisin@tokopedia.com>
2017-09-29 11:12:55 +0700

    fix navigation bar not shown on onboarding

--- sellerapp/src/main/java/com/tokopedia/sellerapp/onboarding/activity/OnboardingSellerActivity.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/onboarding/activity/OnboardingSellerActivity.java
@@ -39,10 +39,6 @@ public class OnboardingSellerActivity extends OnboardingActivity {
         decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setFlowAnimation();
         try {
@@ -59,10 +55,6 @@ public class OnboardingSellerActivity extends OnboardingActivity {
         if (hasFocus) {
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
