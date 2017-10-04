c8d0bf1e3e71ccdc67db0a50062f029beb0df1d0
Hafizh Herdi <hherdinaufal@gmail.com>
2017-10-04 14:32:17 +0700

    create graphQL datasource

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/data/source/CloudAttrDataSource.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/data/source/CloudAttrDataSource.java
@@ -1,10 +1,11 @@
-package com.tokopedia.core.analytics.data;
+package com.tokopedia.core.analytics.data.source;
 
 import com.apollographql.android.rx.RxApollo;
 import com.apollographql.apollo.ApolloClient;
 import com.apollographql.apollo.ApolloWatcher;
 import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.anals.UserAttribute;
+import com.tokopedia.core.analytics.domain.usecase.GetUserAttributesUseCase;
 import com.tokopedia.core.base.domain.RequestParams;
 
 import rx.Observable;
@@ -22,15 +23,20 @@ public class CloudAttrDataSource {
         apolloClient = aplClient;
     }
 
-    protected Observable<String> getUserAttributes(RequestParams requestParams){
+    public Observable<String> getUserAttributes(RequestParams requestParams){
+
+        CommonUtils.dumper("rxapollo called userID "+requestParams.getInt(GetUserAttributesUseCase.PARAM_USER_ID, 0));
+
         ApolloWatcher<UserAttribute.Data> apolloWatcher = apolloClient.newCall(UserAttribute.builder()
-                .userID(0)
+                .userID(requestParams.getInt(GetUserAttributesUseCase.PARAM_USER_ID, 0))
                 .build()
                 ).watcher();
 
         return RxApollo.from(apolloWatcher).map(new Func1<UserAttribute.Data, String>() {
             @Override
             public String call(UserAttribute.Data data) {
+
+
                 CommonUtils.dumper("rxapollo "+data.toString());
                 return data.toString();
             }
