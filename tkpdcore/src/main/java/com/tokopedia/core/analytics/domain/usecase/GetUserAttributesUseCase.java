1b008b7c5317bd87e3a64f2c33c2073550c124d1
Hafizh Herdi <hherdinaufal@gmail.com>
2017-10-04 14:55:32 +0700

    implementing graphQL usecase

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/domain/usecase/GetUserAttributesUseCase.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/domain/usecase/GetUserAttributesUseCase.java
@@ -1,8 +1,38 @@
-package com.tokopedia.core.analytics.domain.usecase.model;
+package com.tokopedia.core.analytics.domain.usecase;
+
+import com.tokopedia.core.analytics.data.UserAttributesRepository;
+import com.tokopedia.core.base.domain.RequestParams;
+import com.tokopedia.core.base.domain.UseCase;
+import com.tokopedia.core.base.domain.executor.PostExecutionThread;
+import com.tokopedia.core.base.domain.executor.ThreadExecutor;
+import com.tokopedia.core.util.SessionHandler;
+
+import rx.Observable;
 
 /**
  * Created by Herdi_WORK on 03.10.17.
  */
 
-public class GetUserAttributesUseCase {
+public class GetUserAttributesUseCase extends UseCase<String>{
+    public static final String PARAM_USER_ID = "PARAM_USER_ID";
+    protected UserAttributesRepository userAttributesRepository;
+
+    public GetUserAttributesUseCase(ThreadExecutor threadExecutor,
+                                    PostExecutionThread postExecutionThread,
+                                    UserAttributesRepository userRepository){
+        super(threadExecutor, postExecutionThread);
+        userAttributesRepository = userRepository;
+    }
+
+    @Override
+    public Observable<String> createObservable(RequestParams requestParams) {
+        return userAttributesRepository.getUserAttributes(requestParams) ;
+    }
+
+    public RequestParams getUserAttrParam(SessionHandler sessionHandler){
+        RequestParams params = RequestParams.create();
+        params.putInt(GetUserAttributesUseCase.PARAM_USER_ID,
+                Integer.parseInt(sessionHandler.getLoginID()));
+        return params;
+    }
 }
