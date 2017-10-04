21523736e2d4efdc6da0220babc1140902b3d75c
Hafizh Herdi <hherdinaufal@gmail.com>
2017-10-04 14:55:10 +0700

    implementing graphQL repository

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/data/UserAttributesRepository.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/data/UserAttributesRepository.java
@@ -1,8 +1,15 @@
 package com.tokopedia.core.analytics.data;
 
+import com.tokopedia.core.base.domain.RequestParams;
+
+import rx.Observable;
+
 /**
  * Created by Herdi_WORK on 03.10.17.
  */
 
-public class UserAttributesRepository {
+public interface UserAttributesRepository {
+
+    Observable<String> getUserAttributes(RequestParams parameters);
+
 }
--- tkpdcore/src/main/java/com/tokopedia/core/analytics/data/UserAttributesRepositoryImpl.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/data/UserAttributesRepositoryImpl.java
@@ -1,8 +1,24 @@
 package com.tokopedia.core.analytics.data;
 
+import com.tokopedia.core.analytics.data.factory.UserAttributesFactory;
+import com.tokopedia.core.base.domain.RequestParams;
+
+import rx.Observable;
+
 /**
  * Created by Herdi_WORK on 03.10.17.
  */
 
-public class UserAttributesRepositoryImpl {
+public class UserAttributesRepositoryImpl implements UserAttributesRepository {
+
+    private UserAttributesFactory userFactory;
+
+    public UserAttributesRepositoryImpl(UserAttributesFactory userAttributesFactory){
+        userFactory = userAttributesFactory;
+    }
+
+    @Override
+    public Observable<String> getUserAttributes(RequestParams parameters) {
+        return userFactory.createCloudAttrDataSource().getUserAttributes(parameters);
+    }
 }
