ac5b7b93967dc37d913961450d21f867463e4e40
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2017-10-04 16:20:30 +0700

    delete unused code get recharge category fragment

--- customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/adapter/RechargeViewPagerAdapter.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/recharge/adapter/RechargeViewPagerAdapter.java
@@ -1,6 +1,5 @@
 package com.tokopedia.tkpd.home.recharge.adapter;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -14,21 +13,15 @@ import java.util.List;
 
 /**
  * @author kulomady 05 on 7/11/2016.
+ *         Modified by Nabilla Sabbaha on 4/10/2017
  */
 public class RechargeViewPagerAdapter extends FragmentStatePagerAdapter {
     private List<Category> categoryList;
     private int currentPosition = -1;
-    private Bundle extraData = new Bundle();
 
     public RechargeViewPagerAdapter(FragmentManager fm, List<Category> categoryList) {
         super(fm);
         this.categoryList = categoryList;
-    }
-
-    public RechargeViewPagerAdapter(FragmentManager fm, List<Category> categoryList, Bundle extra) {
-        super(fm);
-        this.categoryList = categoryList;
-        this.extraData = extra;
     }
 
     @Override
