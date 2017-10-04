7d4bf430df82f2be44d627103c45d4f938ee2240
okasurya <brilliantsurya@gmail.com>
2017-10-04 10:45:54 +0700

    util react native module for payment

--- posapp/src/main/java/com/tokopedia/posapp/react/nativemodules/UtilRNModule.java
+++ posapp/src/main/java/com/tokopedia/posapp/react/nativemodules/UtilRNModule.java
@@ -1,11 +1,19 @@
 package com.tokopedia.posapp.react.nativemodules;
 
+import android.net.Uri;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.google.gson.Gson;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
 import com.tokopedia.core.network.retrofit.utils.AuthUtil;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by okasurya on 10/3/17.
@@ -28,11 +36,23 @@ public class UtilRNModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void convertParamToJson(String param, Promise promise) {
-        promise.resolve(param);
+        try {
+            String mock = "http://www.tokopedia.com?" + URLDecoder.decode(param, "UTF-8");
+
+            Uri uri = Uri.parse(mock).buildUpon().build();
+            Map<String, String> maps = new HashMap<>();
+            for(String key: uri.getQueryParameterNames()) {
+                maps.put(key, uri.getQueryParameter(key));
+            }
+            promise.resolve(new Gson().toJson(maps));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            promise.reject(e);
+        }
     }
 
     private String getSecretKey(String url) {
-        if(url.equals(TkpdBaseURL.PAYMENT_DOMAIN + TkpdBaseURL.Payment.PATH_PAYMENT)) {
+        if(url.equals(TkpdBaseURL.SCROOGE_DOMAIN + TkpdBaseURL.Payment.PATH_PAYMENT)) {
             return AuthUtil.KEY.KEY_PAYMENT;
         }
 
