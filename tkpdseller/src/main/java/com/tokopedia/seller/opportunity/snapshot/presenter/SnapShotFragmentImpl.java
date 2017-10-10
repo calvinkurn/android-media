727bd1964b2ea2c0242cd0d8ddec2cde2f416f71
nisie <nathania.sutedja@tokopedia.com>
2017-10-10 09:14:47 +0700

    Remove peluang button

--- tkpdseller/src/main/java/com/tokopedia/seller/opportunity/snapshot/presenter/SnapShotFragmentImpl.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/opportunity/snapshot/presenter/SnapShotFragmentImpl.java
@@ -3,16 +3,12 @@ package com.tokopedia.seller.opportunity.snapshot.presenter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import com.tokopedia.core.base.domain.RequestParams;
 import com.tokopedia.core.product.facade.NetworkParam;
 import com.tokopedia.core.product.interactor.RetrofitInteractor;
 import com.tokopedia.core.product.interactor.RetrofitInteractorImpl;
 import com.tokopedia.core.product.model.productdetail.ProductDetailData;
 import com.tokopedia.core.router.productdetail.passdata.ProductPass;
-import com.tokopedia.seller.opportunity.domain.interactor.AcceptReplacementUseCase;
-import com.tokopedia.seller.opportunity.presenter.subscriber.AcceptOpportunitySubscriber;
 import com.tokopedia.seller.opportunity.snapshot.listener.SnapShotFragmentView;
-import com.tokopedia.seller.opportunity.snapshot.view.subscriber.AcceptOpportunitySubscriberFromSnapshot;
 
 /**
  * Created by hangnadi on 3/1/17.
@@ -20,13 +16,9 @@ import com.tokopedia.seller.opportunity.snapshot.view.subscriber.AcceptOpportuni
 public class SnapShotFragmentImpl implements SnapShotFragmentPresenter {
     private final SnapShotFragmentView viewListener;
     private RetrofitInteractor retrofitInteractor;
-    private final AcceptReplacementUseCase acceptReplacementUseCase;
 
-
-    public SnapShotFragmentImpl(SnapShotFragmentView view,
-                                AcceptReplacementUseCase acceptReplacementUseCase) {
+    public SnapShotFragmentImpl(SnapShotFragmentView view) {
         this.viewListener = view;
-        this.acceptReplacementUseCase = acceptReplacementUseCase;
         this.retrofitInteractor = new RetrofitInteractorImpl();
     }
 
@@ -41,19 +33,6 @@ public class SnapShotFragmentImpl implements SnapShotFragmentPresenter {
                                      boolean forceNetwork) {
         viewListener.showProgressLoading();
         getProductDetailFromNetwork(context, productPass);
-    }
-
-    @Override
-    public void acceptOpportunity() {
-        viewListener.showLoadingProgress();
-        acceptReplacementUseCase.execute(getAcceptOpportunityParams(),
-                new AcceptOpportunitySubscriberFromSnapshot(viewListener));
-    }
-
-    private RequestParams getAcceptOpportunityParams() {
-        RequestParams params = RequestParams.create();
-        params.putString(AcceptReplacementUseCase.PARAMS_ID, viewListener.getOpportunityId());
-        return params;
     }
 
     private void getProductDetailFromNetwork(final Context context, ProductPass productPass) {
