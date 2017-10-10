da5abee4d81a45718200938b12c05dfa20195a1b
hendry <hendry.setiadi.89@gmail.com>
2017-10-10 10:27:49 +0700

    is editting draft

--- tkpdseller/src/main/java/com/tokopedia/seller/product/edit/view/fragment/ProductAddFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/edit/view/fragment/ProductAddFragment.java
@@ -587,26 +587,29 @@ public class ProductAddFragment extends BaseDaggerFragment implements ProductAdd
                 ImageSelectModel imageSelectModel = imagesSelectView.getSelectedImage();
                 if (imageSelectModel!= null) {
                     String path = imageSelectModel.getUriOrPath();
-                    if (!TextUtils.isEmpty(path) && getProductDraftId() <= 0) {
+                    if (!TextUtils.isEmpty(path) && !isEdittingDraft()) {
                         FileUtils.deleteAllCacheTkpdFile(path);
                     }
                 }
-
                 imagesSelectView.removeImage();
             }
         });
+    }
+
+    private boolean isEdittingDraft(){
+        return ((getStatusUpload()== ProductStatus.EDIT) && (getProductDraftId() > 0));
     }
 
     @Override
     public void onImageEditor(String uriOrPath) {
         ArrayList<String> imageUrls = new ArrayList<>();
         imageUrls.add(uriOrPath);
-        ImageEditorActivity.start(getContext(), ProductAddFragment.this, imageUrls, null, getProductDraftId() <= 0 );
+        ImageEditorActivity.start(getContext(), ProductAddFragment.this, imageUrls, null, !isEdittingDraft());
     }
 
     @Override
     public void onRemovePreviousPath(String uri) {
-        if (!TextUtils.isEmpty(uri) && getProductDraftId() <= 0) {
+        if (!TextUtils.isEmpty(uri) && !isEdittingDraft()) {
             FileUtils.deleteAllCacheTkpdFile(uri);
         }
     }
