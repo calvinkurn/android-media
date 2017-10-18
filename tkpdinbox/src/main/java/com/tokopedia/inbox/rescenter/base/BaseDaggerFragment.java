a17c3ea7da5b54470052d8f63669f0c78432abc4
yoasfs <yoas.fs@gmail.com>
2017-10-18 13:53:06 +0700

    improve class

--- tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/base/BaseDaggerFragment.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/base/BaseDaggerFragment.java
@@ -1,10 +1,14 @@
 package com.tokopedia.inbox.rescenter.base;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.tokopedia.core.app.BasePresenterFragment;
 import com.tokopedia.core.app.TkpdFragment;
@@ -96,6 +100,37 @@ public abstract class BaseDaggerFragment extends TkpdFragment {
             //tv1.setText(savedState.getString("text"));
             onRestoreState(savedState);
         }
+    }
+
+
+
+    public void setupUI(View view) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(getActivity());
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
     }
 
     public abstract void onRestoreState(Bundle savedState);
--- tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/createreso/view/fragment/SolutionDetailFragment.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/createreso/view/fragment/SolutionDetailFragment.java
@@ -8,11 +8,9 @@ import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -319,33 +317,5 @@ public class SolutionDetailFragment extends BaseDaggerFragment implements Soluti
     public void onDestroy() {
         super.onDestroy();
         presenter.detachView();
-    }
-
-    public void setupUI(View view) {
-        // Set up touch listener for non-text box views to hide keyboard.
-        if (!(view instanceof EditText)) {
-            view.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    hideSoftKeyboard(getActivity());
-                    return false;
-                }
-            });
-        }
-
-        //If a layout container, iterate over children and seed recursion.
-        if (view instanceof ViewGroup) {
-            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
-                View innerView = ((ViewGroup) view).getChildAt(i);
-                setupUI(innerView);
-            }
-        }
-    }
-
-    public static void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager =
-                (InputMethodManager) activity.getSystemService(
-                        Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(
-                activity.getCurrentFocus().getWindowToken(), 0);
     }
 }
