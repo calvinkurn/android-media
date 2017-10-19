e67e076cb39e04944cb2fcda01c2d9f39f7f02cb
Hafizh Herdi <hherdinaufal@gmail.com>
2017-10-19 19:20:21 +0700

    create apollo user attr data source

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/data/source/CloudAttrDataSource.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/data/source/CloudAttrDataSource.java
@@ -6,9 +6,11 @@ import com.apollographql.apollo.ApolloWatcher;
 import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.anals.UserAttribute;
 import com.tokopedia.core.analytics.domain.usecase.GetUserAttributesUseCase;
+import com.tokopedia.core.analytics.handler.AnalyticsCacheHandler;
 import com.tokopedia.core.base.domain.RequestParams;
 
 import rx.Observable;
+import rx.functions.Action1;
 import rx.functions.Func1;
 
 /**
@@ -18,12 +20,14 @@ import rx.functions.Func1;
 public class CloudAttrDataSource {
 
     private ApolloClient apolloClient;
+    private AnalyticsCacheHandler analyticsCacheHandler;
 
     public CloudAttrDataSource( ApolloClient aplClient){
         apolloClient = aplClient;
+        analyticsCacheHandler = new AnalyticsCacheHandler();
     }
 
-    public Observable<String> getUserAttributes(RequestParams requestParams){
+    public Observable<UserAttribute.Data> getUserAttributes(RequestParams requestParams){
 
         CommonUtils.dumper("rxapollo called userID "+requestParams.getInt(GetUserAttributesUseCase.PARAM_USER_ID, 0));
 
@@ -32,14 +36,21 @@ public class CloudAttrDataSource {
                 .build()
                 ).watcher();
 
-        return RxApollo.from(apolloWatcher).map(new Func1<UserAttribute.Data, String>() {
+        return RxApollo.from(apolloWatcher).map(new Func1<UserAttribute.Data, UserAttribute.Data>() {
             @Override
-            public String call(UserAttribute.Data data) {
-
-
-                CommonUtils.dumper("rxapollo "+data.toString());
-                return data.toString();
+            public UserAttribute.Data call(UserAttribute.Data data) {
+                return data;
             }
-        });
+        }).doOnNext(setToCache());
+    }
+
+    private Action1<UserAttribute.Data> setToCache(){
+        return new Action1<UserAttribute.Data>() {
+            @Override
+            public void call(UserAttribute.Data data) {
+                if(data != null)
+                    analyticsCacheHandler.setUserDataGraphQLCache(data);
+            }
+        };
     }
 }
