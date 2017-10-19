b5833d17dc7a095c56963496868dd4c3944543a6
nathaniel <nathaniel.khuana@tokopedia.com>
2017-10-19 21:24:59 +0700

    Update base toolbar activity

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/activity/BaseToolbarActivity.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/activity/BaseToolbarActivity.java
@@ -9,14 +9,18 @@ import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.LayoutRes;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.tokopedia.abstraction.R;
+import com.tokopedia.abstraction.utils.MenuTintUtils;
 
 /**
  * Created by nathan on 7/11/17.
@@ -24,6 +28,8 @@ import com.tokopedia.abstraction.R;
 
 abstract class BaseToolbarActivity extends BaseActivity {
 
+    private final static int TOOLBAR_ELEVATION = 10;
+    private final static int TEXT_COLOR_BACKGROUND_WHITE = R.color.black;
     protected Toolbar toolbar;
 
     protected abstract void setupFragment(Bundle savedInstanceState);
@@ -41,8 +47,11 @@ abstract class BaseToolbarActivity extends BaseActivity {
         }
         setupLayout(savedInstanceState);
         setupFragment(savedInstanceState);
-        if(isToolbarWhite()){
+        if (isToolbarWhite()) {
             setToolbarColorWhite();
+        }
+        if (getSupportActionBar() != null && isShowCloseButton()) {
+            getSupportActionBar().setHomeAsUpIndicator(ContextCompat.getDrawable(this, R.drawable.ic_close));
         }
     }
 
@@ -50,22 +59,13 @@ abstract class BaseToolbarActivity extends BaseActivity {
         return R.style.Theme_Tokopedia3;
     }
 
-
-    private void setToolbarColorWhite() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            View view = getWindow().getDecorView();
-            int flags = view.getSystemUiVisibility();
-
-            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            view.setSystemUiVisibility(flags);
-            getWindow().setStatusBarColor(Color.WHITE);
-        }
-
+    protected void setToolbarColorWhite() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            toolbar.setElevation(10);
+            toolbar.setElevation(TOOLBAR_ELEVATION);
         }
-
-        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.grey_700));
+        int textColor = ContextCompat.getColor(this, TEXT_COLOR_BACKGROUND_WHITE);
+        toolbar.setTitleTextColor(textColor);
+        toolbar.setSubtitleTextColor(textColor);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.white)));
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -75,6 +75,14 @@ abstract class BaseToolbarActivity extends BaseActivity {
             upArrow.setColorFilter(ContextCompat.getColor(this, R.color.grey_700), PorterDuff.Mode.SRC_ATOP);
             getSupportActionBar().setHomeAsUpIndicator(upArrow);
         }
+    }
+
+    protected boolean isShowCloseButton() {
+        return false;
+    }
+
+    protected boolean isToolbarWhite() {
+        return false;
     }
 
     @CallSuper
@@ -90,6 +98,37 @@ abstract class BaseToolbarActivity extends BaseActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        updateOptionMenuColor(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void updateOptionMenuColor(Menu menu) {
+        if (isToolbarWhite()) {
+            MenuTintUtils.tintAllIcons(menu, TEXT_COLOR_BACKGROUND_WHITE);
+        }
+    }
+
+    public void updateTitle(String title) {
+        updateTitle(title, null);
+    }
+
+    public void updateTitle(String title, String subTitle) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null) {
+            return;
+        }
+        if (TextUtils.isEmpty(title)) {
+            title = "";
+        }
+        if (TextUtils.isEmpty(subTitle)) {
+            subTitle = "";
+        }
+        actionBar.setTitle(title);
+        actionBar.setSubtitle(subTitle);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -97,9 +136,5 @@ abstract class BaseToolbarActivity extends BaseActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    protected boolean isToolbarWhite() {
-        return false;
     }
 }
