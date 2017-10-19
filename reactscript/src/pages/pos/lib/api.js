cff56584010f1617810f0438f2978df7467aaa8a
Yogie Putra <yogie.putra8@gmail.com>
2017-10-19 11:43:53 +0700

    Upgrade API using Gateway API

--- reactscript/src/pages/pos/lib/api.js
+++ reactscript/src/pages/pos/lib/api.js
@@ -1,15 +1,31 @@
 export const BASE_API_URL_PAYMENT = {
-    STAGING: 'https://payment-staging.tokopedia.com',
+    STAGING: 'http://o2o-staging.tokopedia.com/o2o/get_payment_params',
     PRODUCTION: ''
 }
 
 export const BASE_API_URL_SCROOGE = {
-    STAGING: 'https://pay-staging.tokopedia.com',
-    PRODUCTION: 'https://pay.tokopedia.com'
+    STAGING: 'http://o2o-staging.tokopedia.com/o2o/v1/payment/create',
+    PRODUCTION: ''
 }
 
 export const BASE_API_URL_PCIDSS = {
-    STAGING: 'https://pay-staging.tokopedia.id',
-    PRODUCTION: 'https://pay.tokopedia.id'
+    STAGING: 'http://o2o-staging.tokopedia.com/o2o/v1/payment/process/creditcard',
+    PRODUCTION: ''
 }
+
+
+// export const BASE_API_URL_PAYMENT = {
+//     STAGING: 'https://payment-staging.tokopedia.com',
+//     PRODUCTION: ''
+// }
+
+// export const BASE_API_URL_SCROOGE = {
+//     STAGING: 'https://pay-staging.tokopedia.com',
+//     PRODUCTION: 'https://pay.tokopedia.com'
+// }
+
+// export const BASE_API_URL_PCIDSS = {
+//     STAGING: 'https://pay-staging.tokopedia.id',
+//     PRODUCTION: 'https://pay.tokopedia.id'
+// }
 
