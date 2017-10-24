43a66d3ba4bb520932d90ec8e2b41ce69e2d2ab0
hendry <hendry.setiadi.89@gmail.com>
2017-10-24 21:02:04 +0700

    add FlightModuleRouter, add flight mockup application

---
+++ flightmockapp/src/main/java/com/tokopedia/flightmockapp/FlightMainApplication.java
@@ -0,0 +1,38 @@
+package com.tokopedia.flightmockapp;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.config.TkpdFlightGeneratedDatabaseHolder;
+import com.tokopedia.abstraction.base.app.BaseMainApplication;
+import com.tokopedia.flight.FlightModuleRouter;
+import com.tokopedia.flight.common.di.component.DaggerFlightComponent;
+import com.tokopedia.flight.common.di.component.FlightComponent;
+
+/**
+ * Created by User on 10/24/2017.
+ */
+
+public class FlightMainApplication extends BaseMainApplication implements FlightModuleRouter{
+
+    private FlightComponent flightComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initDBFlow();
+    }
+
+    private void initDBFlow() {
+        FlowManager.init(new FlowConfig.Builder(this)
+                .addDatabaseHolder(TkpdFlightGeneratedDatabaseHolder.class)
+                .build());
+    }
+
+    @Override
+    public FlightComponent getFlightComponent() {
+        if (flightComponent == null) {
+            flightComponent = DaggerFlightComponent.builder().baseAppComponent(getBaseAppComponent()).build();
+        }
+        return flightComponent;
+    }
+}
