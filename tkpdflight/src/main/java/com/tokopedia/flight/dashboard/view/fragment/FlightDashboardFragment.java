5e5d014e27616440dc6b31b6fd58021ec7ad5200
alvarisi <alvarisi01@gmail.com>
2017-10-25 18:31:06 +0700

    integrate select passenger page with dashboard fligt

--- tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/fragment/FlightDashboardFragment.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/fragment/FlightDashboardFragment.java
@@ -11,9 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
-import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment;
 import com.tokopedia.flight.R;
 import com.tokopedia.flight.airport.view.activity.FlightAirportPickerActivity;
+import com.tokopedia.flight.dashboard.view.activity.SelectFlightPassengerActivity;
+import com.tokopedia.flight.dashboard.view.fragment.passdata.SelectFlightPassengerPassData;
 import com.tokopedia.flight.dashboard.view.widget.TextInputView;
 
 /**
@@ -24,6 +25,7 @@ public class FlightDashboardFragment extends Fragment {
 
     private static final int REQUEST_CODE_AIRPORT_DEPARTURE = 1;
     private static final int REQUEST_CODE_AIRPORT_ARRIVAL = 2;
+    private static final int REQUEST_CODE_AIRPORT_PASSENGER = 3;
 
     public static FlightDashboardFragment getInstance() {
         return new FlightDashboardFragment();
@@ -37,10 +39,11 @@ public class FlightDashboardFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup   container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_flight_dashboard, container, false);
         TextInputView airportDepartureTextInputView = (TextInputView) view.findViewById(R.id.text_input_view_departure_airport);
         TextInputView airportArrivalTextInputView = (TextInputView) view.findViewById(R.id.text_input_view_arrival_airport);
+        TextInputView passengerTextInputView = (TextInputView) view.findViewById(R.id.text_input_view_passenger);
         airportDepartureTextInputView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +58,16 @@ public class FlightDashboardFragment extends Fragment {
                 getActivity().startActivityForResult(intent, REQUEST_CODE_AIRPORT_ARRIVAL);
             }
         });
+        passengerTextInputView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectFlightPassengerPassData passData = new SelectFlightPassengerPassData.Builder()
+                        .build();
+                Intent intent = SelectFlightPassengerActivity.getCallingIntent(getActivity(), passData);
+                getActivity().startActivityForResult(intent, REQUEST_CODE_AIRPORT_PASSENGER);
+            }
+        });
+
         return view;
     }
 
