b6487225993814992d2466442a2b73d50a92b058
hendry <hendry.setiadi.89@gmail.com>
2017-10-27 15:30:50 +0700

    remove generated host

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/di/module/net/RetrofitModule.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/di/module/net/RetrofitModule.java
@@ -3,9 +3,6 @@ package com.tokopedia.abstraction.di.module.net;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tokopedia.abstraction.di.scope.ApplicationScope;
-import com.tokopedia.abstraction.retrofit.converters.StringResponseConverter;
-import com.tokopedia.abstraction.retrofit.converters.TkpdResponseConverter;
-import com.tokopedia.abstraction.retrofit.converters.GeneratedHostConverter;
 
 import dagger.Module;
 import dagger.Provides;
@@ -18,34 +15,10 @@ public class RetrofitModule {
 
     @ApplicationScope
     @Provides
-    public Retrofit.Builder provideRetrofitBuilder(GeneratedHostConverter generatedHostConverter,
-                                                   TkpdResponseConverter tkpdResponseConverter,
-                                                   StringResponseConverter stringResponseConverter,
-                                                   Gson gson) {
+    public Retrofit.Builder provideRetrofitBuilder(Gson gson) {
         return new Retrofit.Builder()
-                .addConverterFactory(generatedHostConverter)
-                .addConverterFactory(tkpdResponseConverter)
-                .addConverterFactory(stringResponseConverter)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
-    }
-
-    @ApplicationScope
-    @Provides
-    public GeneratedHostConverter provideGeneratedHostConverter() {
-        return new GeneratedHostConverter();
-    }
-
-    @ApplicationScope
-    @Provides
-    public TkpdResponseConverter provideTkpdResponseConverter() {
-        return new TkpdResponseConverter();
-    }
-
-    @ApplicationScope
-    @Provides
-    public StringResponseConverter provideStringResponseConverter() {
-        return new StringResponseConverter();
     }
 
     @ApplicationScope
