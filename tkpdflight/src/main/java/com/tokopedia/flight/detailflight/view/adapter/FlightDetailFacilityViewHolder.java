3f991d92bdc8028ae4e50fcd19a1954304a0b90d
zulfikar rahman <rahman.zulfikar94@gmail.com>
2017-10-30 17:07:08 +0700

    develop view detail flight

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/detailflight/view/adapter/FlightDetailFacilityViewHolder.java
@@ -0,0 +1,28 @@
+package com.tokopedia.flight.detailflight.view.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.GridView;
+
+import com.tokopedia.abstraction.base.view.adapter.holder.BaseViewHolder;
+import com.tokopedia.flight.search.data.cloud.model.Route;
+import com.tokopedia.flight.search.view.model.FlightSearchViewModel;
+
+/**
+ * Created by zulfikarrahman on 10/30/17.
+ */
+
+public class FlightDetailFacilityViewHolder extends BaseViewHolder<Route> {
+
+    RecyclerView listInfo;
+    GridView gridAmenity;
+
+    public FlightDetailFacilityViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void bindObject(Route route) {
+
+    }
+}
