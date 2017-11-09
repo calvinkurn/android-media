eafdab63be3c0860f2ceed2b2f761e0b0a35c807
hendry <hendry.setiadi.89@gmail.com>
2017-11-09 18:35:28 +0700

    fix comparator, optimize sort, fix get single flight use case

--- tkpdflight/src/main/java/com/tokopedia/flight/search/domain/FlightSearchWithSortUseCase.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/search/domain/FlightSearchWithSortUseCase.java
@@ -4,10 +4,12 @@ import com.tokopedia.flight.search.constant.FlightSortOption;
 import com.tokopedia.flight.search.domain.comparator.FlightCheapestComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightEarliestArrivalComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightEarliestDepartureComparator;
+import com.tokopedia.flight.search.domain.comparator.FlightLatestArrivalComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightLatestDepartureComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightLongestDurationComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightMostExpensiveComparator;
 import com.tokopedia.flight.search.domain.comparator.FlightShortestDurationComparator;
+import com.tokopedia.flight.search.domain.mapper.FlightSortMapper;
 import com.tokopedia.flight.search.util.FlightSearchParamUtil;
 import com.tokopedia.flight.search.view.model.FlightSearchViewModel;
 import com.tokopedia.usecase.RequestParams;
@@ -27,51 +29,18 @@ import rx.functions.Func1;
 
 public class FlightSearchWithSortUseCase extends UseCase<List<FlightSearchViewModel>> {
     private FlightSearchUseCase flightSearchUseCase;
+    private FlightSortMapper flightSortMapper;
 
     @Inject
-    public FlightSearchWithSortUseCase(FlightSearchUseCase flightSearchUseCase) {
+    public FlightSearchWithSortUseCase(FlightSearchUseCase flightSearchUseCase, FlightSortMapper flightSortMapper) {
         this.flightSearchUseCase = flightSearchUseCase;
+        this.flightSortMapper = flightSortMapper;
     }
 
     @Override
     public Observable<List<FlightSearchViewModel>> createObservable(final RequestParams requestParams) {
         return flightSearchUseCase.createObservable(requestParams)
-                .map(new Func1<List<FlightSearchViewModel>, List<FlightSearchViewModel>>() {
-                    @Override
-                    public List<FlightSearchViewModel> call(List<FlightSearchViewModel> flightSearchViewModels) {
-                        switch (FlightSearchParamUtil.getSortOptionId(requestParams)) {
-                            case FlightSortOption.NO_PREFERENCE:
-                                return flightSearchViewModels;
-                            case FlightSortOption.EARLIEST_DEPARTURE:
-                                Collections.sort(flightSearchViewModels, new FlightEarliestDepartureComparator());
-                                break;
-                            case FlightSortOption.LATEST_DEPARTURE:
-                                Collections.sort(flightSearchViewModels, new FlightLatestDepartureComparator());
-                                break;
-                            case FlightSortOption.SHORTEST_DURATION:
-                                Collections.sort(flightSearchViewModels, new FlightShortestDurationComparator());
-                                break;
-                            case FlightSortOption.LONGEST_DURATION:
-                                Collections.sort(flightSearchViewModels, new FlightLongestDurationComparator());
-                                break;
-                            case FlightSortOption.EARLIEST_ARRIVAL:
-                                Collections.sort(flightSearchViewModels, new FlightEarliestArrivalComparator());
-                                break;
-                            case FlightSortOption.LATEST_ARRIVAL:
-                                Collections.sort(flightSearchViewModels, new FlightLongestDurationComparator());
-                                break;
-                            case FlightSortOption.CHEAPEST:
-                                Collections.sort(flightSearchViewModels, new FlightCheapestComparator());
-                                break;
-                            case FlightSortOption.MOST_EXPENSIVE:
-                                Collections.sort(flightSearchViewModels, new FlightMostExpensiveComparator());
-                                break;
-                            default:
-                                return flightSearchViewModels;
-                        }
-                        return flightSearchViewModels;
-                    }
-                });
+                .map(flightSortMapper.withSortOptionId(FlightSearchParamUtil.getSortOptionId(requestParams)));
     }
 
     @Override
