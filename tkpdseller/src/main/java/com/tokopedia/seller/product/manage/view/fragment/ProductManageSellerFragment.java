a4f4cd83083341b74267d110443b806019c050b0
zulfikar rahman <rahman.zulfikar94@gmail.com>
2017-11-09 11:34:31 +0700

    bugfix crash manage product on fabric

--- tkpdseller/src/main/java/com/tokopedia/seller/product/manage/view/fragment/ProductManageSellerFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/manage/view/fragment/ProductManageSellerFragment.java
@@ -117,10 +117,14 @@ public class ProductManageSellerFragment extends ProductManageFragment implement
 
     private boolean isMyServiceRunning(Class<?> serviceClass) {
         ActivityManager manager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
+        try {
+            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                if (serviceClass.getName().equals(service.service.getClassName())) {
+                    return true;
+                }
             }
+        }catch (Exception e){
+            return false;
         }
         return false;
     }
