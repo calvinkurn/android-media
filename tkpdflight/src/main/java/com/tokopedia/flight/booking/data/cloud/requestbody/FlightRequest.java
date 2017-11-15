fb36555d97c626c510c300e02a9529e32b2e53d4
alvarisi <alvarisi01@gmail.com>
2017-11-15 14:01:01 +0700

    flight cart request body model

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/booking/data/cloud/requestbody/FlightRequest.java
@@ -0,0 +1,82 @@
+package com.tokopedia.flight.booking.data.cloud.requestbody;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * @author by alvarisi on 11/14/17.
+ */
+
+public class FlightRequest {
+    @SerializedName("adult")
+    @Expose
+    private int adult;
+    @SerializedName("child")
+    @Expose
+    private int child;
+    @SerializedName("infant")
+    @Expose
+    private int infant;
+    @SerializedName("class")
+    @Expose
+    private int classFlight;
+    @SerializedName("combo_key")
+    @Expose
+    private String comboKey;
+    @SerializedName("destination")
+    @Expose
+    private List<CartAirportRequest> destinations;
+
+    public FlightRequest() {
+    }
+
+    public int getAdult() {
+        return adult;
+    }
+
+    public void setAdult(int adult) {
+        this.adult = adult;
+    }
+
+    public int getChild() {
+        return child;
+    }
+
+    public void setChild(int child) {
+        this.child = child;
+    }
+
+    public int getInfant() {
+        return infant;
+    }
+
+    public void setInfant(int infant) {
+        this.infant = infant;
+    }
+
+    public int getClassFlight() {
+        return classFlight;
+    }
+
+    public void setClassFlight(int classFlight) {
+        this.classFlight = classFlight;
+    }
+
+    public String getComboKey() {
+        return comboKey;
+    }
+
+    public void setComboKey(String comboKey) {
+        this.comboKey = comboKey;
+    }
+
+    public List<CartAirportRequest> getDestinations() {
+        return destinations;
+    }
+
+    public void setDestinations(List<CartAirportRequest> destinations) {
+        this.destinations = destinations;
+    }
+}
