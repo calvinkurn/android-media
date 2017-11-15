18b03b61cafcc84bc6497f2b77cdf35ebf3a4732
yogieputra8 <yogie.putra8@gmail.com>
2017-11-15 18:01:26 +0700

    Support applink promo page without s

--- tkpdcore/src/main/java/com/tokopedia/core/gcm/Constants.java
+++ tkpdcore/src/main/java/com/tokopedia/core/gcm/Constants.java
@@ -110,6 +110,7 @@ public interface Constants {
         String RESCENTER = "tokopedia://resolution/{resolution_id}";
         String REFERRAL = "tokopedia://referral";
         String OFFICIAL_STORES_PROMO = "tokopedia://official-stores/promo/{slug}";
+        String OFFICIAL_STORE_PROMO = "tokopedia://official-store/promo/{slug}";
         String OFFICIAL_STORES_PROMO_TERMS = "tokopedia://official-stores/promo-terms";
         String BROWSER = "tokopedia://browser";
         String REGISTER = "tokopedia://registration";
