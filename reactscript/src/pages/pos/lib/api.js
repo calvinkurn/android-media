836cc5b2fe7d2c1c690ab2bd02941b4c584e601a
okasurya <brilliantsurya@gmail.com>
2017-11-16 16:07:14 +0700

    add url for production

--- reactscript/src/pages/pos/lib/api.js
+++ reactscript/src/pages/pos/lib/api.js
@@ -1,39 +1,24 @@
+export const BASE_API_URL = {
+    STAGING: 'http://o2o-staging.tokopedia.com/',
+    PRODUCTION: 'https://o2o.tokopedia.com/'
+}
+
 export const BASE_API_URL_PAYMENT = {
-    STAGING: 'http://o2o-staging.tokopedia.com/o2o/get_payment_params',
-    PRODUCTION: ''
+    STAGING: `${BASE_API_URL.STAGING}o2o/get_payment_params`,
+    PRODUCTION: `${BASE_API_URL.PRODUCTION}o2o/get_payment_params`
 }
 
 export const BASE_API_URL_SCROOGE = {
-    STAGING: 'http://o2o-staging.tokopedia.com/o2o/v1/payment/create',
-    PRODUCTION: ''
+    STAGING: `${BASE_API_URL.STAGING}o2o/v1/payment/create`,
+    PRODUCTION: `${BASE_API_URL.PRODUCTION}o2o/v1/payment/create`
 }
 
 export const BASE_API_URL_PCIDSS = {
-    STAGING: 'http://o2o-staging.tokopedia.com/o2o/v1/payment/process/creditcard',
-    PRODUCTION: ''
-}
-
-export const BASE_API_URL = {
-    STAGING: 'http://o2o-staging.tokopedia.com/',
-    PRODUCTION: ''
+    STAGING: `${BASE_API_URL.STAGING}o2o/v1/payment/process/creditcard`,
+    PRODUCTION: `${BASE_API_URL.PRODUCTION}o2o/v1/payment/process/creditcard`
 }
 
 export const BASE_API_URL_ORDER = {
-    STAGING: 'http://o2o-staging.tokopedia.com/o2o/v1/order/get_history',
-    PRODUCTION: 'https://o2o.tokopedia.com/o2o/v1/order/get_history'
+    STAGING: `${BASE_API_URL.STAGING}o2o/v1/order/get_history`,
+    PRODUCTION: `${BASE_API_URL.PRODUCTION}o2o/v1/order/get_history`
 }
-
-// export const BASE_API_URL_PAYMENT = {
-//     STAGING: 'https://payment-staging.tokopedia.com',
-//     PRODUCTION: ''
-// }
-
-// export const BASE_API_URL_SCROOGE = {
-//     STAGING: 'https://pay-staging.tokopedia.com',
-//     PRODUCTION: 'https://pay.tokopedia.com'
-// }
-
-// export const BASE_API_URL_PCIDSS = {
-//     STAGING: 'https://pay-staging.tokopedia.id',
-//     PRODUCTION: 'https://pay.tokopedia.id'
-// }
