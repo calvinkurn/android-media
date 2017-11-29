dd63d35c2a1abd377870874ea12579fe1ceaab62
Angga Prasetiyo <agapras@gmail.com>
2017-11-29 17:51:14 +0700

    pager

--- tkpdloyalty/src/main/java/com/tokopedia/loyalty/view/activity/LoyaltyActivity.java
+++ tkpdloyalty/src/main/java/com/tokopedia/loyalty/view/activity/LoyaltyActivity.java
@@ -1,20 +1,28 @@
 package com.tokopedia.loyalty.view.activity;
 
-import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 
 import com.tokopedia.core.app.BasePresenterActivity;
 import com.tokopedia.core.base.di.component.AppComponent;
 import com.tokopedia.core.base.di.component.HasComponent;
 import com.tokopedia.loyalty.R;
-import com.tokopedia.loyalty.view.fragment.PromoCodeFragment;
+import com.tokopedia.loyalty.R2;
+
+import butterknife.BindView;
 
 /**
  * @author anggaprasetiyo on 27/11/17.
  */
 
 public class LoyaltyActivity extends BasePresenterActivity implements HasComponent<AppComponent> {
+    @BindView(R2.id.pager)
+    ViewPager viewPager;
+    @BindView(R2.id.indicator)
+    TabLayout indicator;
+
     @Override
     protected void setupURIPass(Uri data) {
 
@@ -42,10 +50,6 @@ public class LoyaltyActivity extends BasePresenterActivity implements HasCompone
 
     @Override
     protected void setViewListener() {
-        Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
-        if (fragment == null || !(fragment instanceof PromoCodeFragment))
-            getFragmentManager().beginTransaction().replace(R.id.container,
-                    PromoCodeFragment.newInstance()).commit();
     }
 
     @Override
