fa98dfbd247f2911ed9900e571ea6cd927e720ab
noiz354 <noiz354@gmail.com>
2017-11-30 15:04:17 +0700

    add response model.

--- tkpdseller/src/main/java/com/tokopedia/seller/seller/info/view/adapter/SellerInfoViewHolder.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/seller/info/view/adapter/SellerInfoViewHolder.java
@@ -2,7 +2,12 @@ package com.tokopedia.seller.seller.info.view.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.tkpd.library.utils.ImageHandler;
+import com.tokopedia.seller.R;
+import com.tokopedia.seller.base.view.adapter.BaseViewHolder;
 import com.tokopedia.seller.seller.info.view.model.SellerInfoModel;
 import com.tokopedia.seller.seller.info.view.util.SellerInfoDateUtil;
 
@@ -10,13 +15,33 @@ import com.tokopedia.seller.seller.info.view.util.SellerInfoDateUtil;
  * Created by normansyahputa on 11/30/17.
  */
 
-public class SellerInfoViewHolder extends RecyclerView.ViewHolder{
+public class SellerInfoViewHolder extends BaseViewHolder<SellerInfoModel> {
+
+    private final ImageView imageSellerInfo;
+    private final TextView textDateDescription;
+    private final TextView textTitle;
+
+    private SellerInfoDateUtil sellerInfoDateUtil;
+
+    @Override
+    public void bindObject(SellerInfoModel sellerInfoModel) {
+        String s = sellerInfoDateUtil.fromUnixTime(sellerInfoModel.getCreateTimeUnix());
+        textDateDescription.setText(s);
+
+        textTitle.setText(sellerInfoModel.getTitle());
+
+        ImageHandler.LoadImage(imageSellerInfo, sellerInfoModel.getInfoThumbnailUrl());
+    }
+
+    public void setSellerInfoDateUtil(SellerInfoDateUtil sellerInfoDateUtil) {
+        this.sellerInfoDateUtil = sellerInfoDateUtil;
+    }
 
     public SellerInfoViewHolder(View itemView) {
         super(itemView);
-    }
 
-    public void bindView(SellerInfoDateUtil sellerInfoDateUtil, SellerInfoModel sellerInfoModel){
-
+        imageSellerInfo = itemView.findViewById(R.id.image_seller_info);
+        textDateDescription = itemView.findViewById(R.id.text_date_description);
+        textTitle = itemView.findViewById(R.id.text_title);
     }
 }
--- tkpdseller/src/main/java/com/tokopedia/seller/seller/info/view/fragment/SellerInfoFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/seller/info/view/fragment/SellerInfoFragment.java
@@ -1,9 +1,15 @@
 package com.tokopedia.seller.seller.info.view.fragment;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.tokopedia.seller.R;
 import com.tokopedia.seller.base.view.adapter.BaseListAdapter;
 import com.tokopedia.seller.base.view.fragment.BaseListFragment;
 import com.tokopedia.seller.base.view.presenter.BlankPresenter;
+import com.tokopedia.seller.seller.info.view.adapter.SellerInfoAdapter;
 import com.tokopedia.seller.seller.info.view.model.SellerInfoModel;
+import com.tokopedia.seller.seller.info.view.util.SellerInfoDateUtil;
 
 /**
  * Created by normansyahputa on 11/30/17.
@@ -11,13 +17,23 @@ import com.tokopedia.seller.seller.info.view.model.SellerInfoModel;
 
 public class SellerInfoFragment extends BaseListFragment<BlankPresenter, SellerInfoModel> {
 
+    private String[] monthNamesAbrev;
+    private SellerInfoDateUtil sellerInfoDateUtil;
+
     public static SellerInfoFragment newInstance(){
         return new SellerInfoFragment();
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        monthNamesAbrev = getResources().getStringArray(R.array.lib_date_picker_month_entries);
+        sellerInfoDateUtil = new SellerInfoDateUtil(monthNamesAbrev);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
     protected BaseListAdapter<SellerInfoModel> getNewAdapter() {
-        return null;
+        return new SellerInfoAdapter(sellerInfoDateUtil);
     }
 
     @Override
