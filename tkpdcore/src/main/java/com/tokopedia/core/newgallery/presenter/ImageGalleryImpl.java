676d61091256acf1dfdf2c8ef578697c995146f7
noiz354 <noiz354@gmail.com>
2017-12-04 15:14:23 +0700

    speed up using old style.

--- tkpdcore/src/main/java/com/tokopedia/core/newgallery/presenter/ImageGalleryImpl.java
+++ tkpdcore/src/main/java/com/tokopedia/core/newgallery/presenter/ImageGalleryImpl.java
@@ -39,9 +39,9 @@ public class ImageGalleryImpl implements ImageGallery {
 
     private final ContentResolver contentResolver;
     private ImageGalleryView imageGalleryView;
-    private Set<String> pathList = new HashSet<>();
     private ArrayList<ImageModel> dataAlbum = new ArrayList<>();
-    private ArrayList<ImageModel> dataListPhoto = new ArrayList<>();
+
+    private Map<String, List<String>> data = new HashMap<>();
 
     public ImageGalleryImpl(ImageGalleryView imageGalleryView){
         this.imageGalleryView = imageGalleryView;
@@ -51,54 +51,35 @@ public class ImageGalleryImpl implements ImageGallery {
         contentResolver = imageGalleryView.getContentResolver();
     }
 
-    public boolean isAlbumEmpty(){
-        return pathList.size() <= 0;
-    }
-
     @Override
     public void getItemAlbum() {
-        if (imageGalleryView != null && !isAlbumEmpty()) {
-            imageGalleryView.retrieveData(dataAlbum, new ArrayList<>(pathList));
+        if (imageGalleryView != null && (dataAlbum.size()>0)) {
+            imageGalleryView.retrieveData(dataAlbum);
         }else {
-            new GetItemAlbum().execute();
+            fetchImageUsingDb();
         }
     }
 
     public String getFolderPath(int position) {
-        return dataAlbum.get(position).getPathFolder();
-    }
-
-    public void getItemListAlbum(int position) {
-        new GetItemListAlbum(getFolderPath(position)).execute();
+        return dataAlbum.get(position).getName();
     }
 
     @Override
     public void getItemListAlbum(String folderPath) {
-        new GetItemListAlbum(folderPath).execute();
-    }
-
-    private boolean Check(String a, Set<String> list) {
-        return !list.isEmpty() && list.contains(a);
-    }
-
-    private boolean checkFile(File file) {
-        if (file == null) {
-            return false;
-        }
-
-        String name = file.getName();
-
-        if (name.startsWith(".") || file.length() == 0) {
-            return false;
-        }
-        boolean isCheck = false;
-        for (int k = 0; k < Constants.FORMAT_IMAGE.size(); k++) {
-            if (name.endsWith(Constants.FORMAT_IMAGE.get(k))) {
-                isCheck = true;
-                break;
+        if (imageGalleryView != null) {
+            List<String> pathFiles = data.get(folderPath);
+            ArrayList<ImageModel> imageModels = new ArrayList<>();
+            for (String pathFile : pathFiles) {
+                imageModels.add(new ImageModel(null, pathFile, null));
             }
+            imageGalleryView.retrieveItemData(imageModels);
         }
-        return isCheck;
+    }
+
+    @Override
+    public void detach() {
+        imageGalleryView = null;
+        data = null;
     }
 
     @Override
@@ -120,23 +101,24 @@ public class ImageGalleryImpl implements ImageGallery {
                                 null, null, BUCKET_ORDER_BY);
 
                         if (cur.moveToFirst()) {
-                            Map<String, List<String>> data = new HashMap<>();
-                            int bucketColumn = cur
-                                    .getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-
+                            data = new HashMap<>();
+                            int bucketColumn = cur.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
                             int dataColumn = cur.getColumnIndex(MediaStore.Images.Media.DATA);
                             do {
                                 String bucketName = cur.getString(bucketColumn);
                                 String path = cur.getString(dataColumn);
                                 List<String> value;
                                 if(data.get(bucketName)==null){
-                                    value = new ArrayList<String>() {};
+                                    value = new ArrayList<>();
+
+                                    ImageGalleryImpl.this.dataAlbum.add(new ImageModel(bucketName, path,  new File(path).getParent()));
                                 }else{
                                     value = data.get(bucketName);
                                 }
                                 value.add(path);
                                 data.put(bucketName, value);
                             } while (cur.moveToNext());
+
                             for(Iterator<Map.Entry<String, List<String>>> iterator = data.entrySet().iterator();iterator.hasNext();){
                                 Map.Entry<String, List<String>> entry = iterator.next();
                                 datas.add(new FolderModel(entry.getKey(), entry.getValue()));
@@ -165,8 +147,14 @@ public class ImageGalleryImpl implements ImageGallery {
                     @Override
                     public void onNext(List<FolderModel> folderModels) {
                         result.addAll(folderModels);
+                        // this to support old code
                         if(imageGalleryView!=null){
                             imageGalleryView.loadData(folderModels);
+                        }
+
+                        // this for new code
+                        if (imageGalleryView != null && (dataAlbum.size()>0)) {
+                            imageGalleryView.retrieveData(dataAlbum);
                         }
                     }
                 });
@@ -178,108 +166,5 @@ public class ImageGalleryImpl implements ImageGallery {
     @Override
     public List<FolderModel> fetchImageUsingDb() {
         return fetchImageUsingDb("");
-    }
-
-    private class GetItemAlbum extends AsyncTask<Void, Void, String> {
-        private GetItemAlbum() {
-        }
-
-        protected String doInBackground(Void... params) {
-            String[] projection = new String[]{MediaStore.Images.Media._ID,
-                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
-                    MediaStore.Images.Media.DATE_TAKEN,
-                    MediaStore.Images.Media.DATA};
-            String BUCKET_ORDER_BY = MediaStore.Images.Media.DATE_TAKEN + " DESC";
-            Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-
-            Cursor cursor = contentResolver.query(images, projection, null, null, BUCKET_ORDER_BY);
-            if (cursor != null) {
-                int column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                int bucketColumn = cursor.getColumnIndex(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-                int dataColumn = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
-                while (cursor.moveToNext()) {
-                    String pathFile = cursor.getString(column_index_data);
-                    String bucketName = cursor.getString(bucketColumn);
-                    if (TextUtils.isEmpty(pathFile) || TextUtils.isEmpty(bucketName)) {
-                        continue;
-                    }
-                    File file = new File(pathFile);
-                    boolean check = checkFile(file);
-                    if (!Check(file.getParent(), ImageGalleryImpl.this.pathList) && check) {
-                            ImageGalleryImpl.this.pathList.add(file.getParent());
-                            ImageGalleryImpl.this.dataAlbum.add(new ImageModel(bucketName, pathFile, file.getParent()));
-                    }
-                }
-                cursor.close();
-            }
-            return "";
-        }
-
-        protected void onPostExecute(String result) {
-            if (imageGalleryView != null) {
-                imageGalleryView.retrieveData(dataAlbum, new ArrayList<String>(pathList));
-            }
-        }
-
-        protected void onPreExecute() {
-        }
-
-        protected void onProgressUpdate(Void... values) {
-        }
-    }
-
-    private class GetItemListAlbum extends AsyncTask<Void, Void, String> {
-        String pathAlbum;
-
-        GetItemListAlbum(String pathAlbum) {
-            this.pathAlbum = pathAlbum;
-            dataListPhoto.clear();
-        }
-
-        protected String doInBackground(Void... params) {
-            File file = new File(this.pathAlbum);
-            if (file.isDirectory()) {
-                for (File fileTmp : file.listFiles()) {
-                    if (fileTmp.exists()) {
-                        boolean check = checkFile(fileTmp);
-                        if (!fileTmp.isDirectory() && check) {
-                            ImageGalleryImpl.this.dataListPhoto.add(new ImageModel(fileTmp.getName(), fileTmp.getAbsolutePath(), fileTmp.getAbsolutePath()));
-                            publishProgress();
-                        }
-                    }
-                }
-            }
-            return "";
-        }
-
-        protected void onPostExecute(String result) {
-            try {
-                Collections.sort(ImageGalleryImpl.this.dataListPhoto, new Comparator<ImageModel>() {
-                    @Override
-                    public int compare(ImageModel item, ImageModel t1) {
-                        File fileI = new File(item.getPathFolder());
-                        File fileJ = new File(t1.getPathFolder());
-                        if (fileI.lastModified() > fileJ.lastModified()) {
-                            return -1;
-                        }
-                        if (fileI.lastModified() < fileJ.lastModified()) {
-                            return 1;
-                        }
-                        return 0;
-                    }
-                });
-            } catch (Exception e) {
-            }
-
-            if (imageGalleryView != null) {
-                imageGalleryView.retrieveItemData(dataListPhoto, new ArrayList<>(pathList));
-            }
-        }
-
-        protected void onPreExecute() {
-        }
-
-        protected void onProgressUpdate(Void... values) {
-        }
     }
 }
