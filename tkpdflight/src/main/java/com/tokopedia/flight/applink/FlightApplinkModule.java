232c1ef680596633b5b4839367268d16da088e57
zulfikar rahman <rahman.zulfikar94@gmail.com>
2017-12-05 11:45:06 +0700

    dev applink

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/applink/FlightApplinkModule.java
@@ -0,0 +1,11 @@
+package com.tokopedia.flight.applink;
+
+import com.airbnb.deeplinkdispatch.DeepLinkModule;
+
+/**
+ * Created by alvarisi on 5/22/17.
+ */
+
+@DeepLinkModule
+public class FlightApplinkModule {
+}
--- tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/activity/FlightDashboardActivity.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/activity/FlightDashboardActivity.java
@@ -1,10 +1,15 @@
 package com.tokopedia.flight.dashboard.view.activity;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
 import com.tokopedia.abstraction.di.component.HasComponent;
+import com.tokopedia.abstraction.utils.Constants;
 import com.tokopedia.flight.FlightModuleRouter;
 import com.tokopedia.flight.dashboard.di.DaggerFlightDashboardComponent;
 import com.tokopedia.flight.dashboard.di.FlightDashboardComponent;
@@ -15,6 +20,15 @@ import com.tokopedia.flight.dashboard.view.fragment.FlightDashboardFragment;
  */
 
 public class FlightDashboardActivity extends BaseSimpleActivity implements HasComponent<FlightDashboardComponent> {
+
+    @DeepLink(Constants.Applinks.FLIGHT)
+    public static Intent getCallingApplinkIntent(Context context, Bundle extras) {
+            Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
+            Intent intent = new Intent(context, FlightDashboardActivity.class);
+            return intent
+                    .setData(uri.build())
+                    .putExtras(extras);
+    }
 
     @Override
     protected Fragment getNewFragment() {
