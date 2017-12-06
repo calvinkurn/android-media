fe2d5523a822da74af0f64964affa64e065e0dc8
okasurya <brilliantsurya@gmail.com>
2017-12-06 14:17:45 +0700

    add graphql query & schema.json for marketplace payment data

--- customerapp/src/main/java/com/tokopedia/tkpd/thankyou/data/source/MarketplaceTrackerCloudSource.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/thankyou/data/source/MarketplaceTrackerCloudSource.java
@@ -1,6 +1,11 @@
 package com.tokopedia.tkpd.thankyou.data.source;
 
+import com.apollographql.android.rx.RxApollo;
+import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.ApolloWatcher;
+import com.tkpdfeed.feeds.GetKolComments;
 import com.tokopedia.core.base.domain.RequestParams;
+import com.tokopedia.tkpd.tkpdfeed.feedplus.domain.usecase.GetKolCommentsUseCase;
 
 import rx.Observable;
 
@@ -9,12 +14,22 @@ import rx.Observable;
  */
 
 public class MarketplaceTrackerCloudSource extends ThanksTrackerCloudSource {
+    private ApolloClient apolloClient;
+
     public MarketplaceTrackerCloudSource(RequestParams requestParams) {
         super(requestParams);
     }
 
     @Override
     public Observable<String> sendAnalytics() {
+//        ApolloWatcher<MarkeplacePaymentData.Data> apolloWatcher = apolloClient.newCall(
+//                GetKolComments.builder()
+//                        .idPost(requestParams.getInt(GetKolCommentsUseCase.PARAM_ID, 0))
+//                        .cursor(requestParams.getString(GetKolCommentsUseCase.PARAM_CURSOR, ""))
+//                        .limit(requestParams.getInt(GetKolCommentsUseCase.PARAM_LIMIT,
+//                                GetKolCommentsUseCase.DEFAULT_LIMIT))
+//                        .build()).watcher();
+//        return RxApollo.from(apolloWatcher).map(new F);
         return null;
     }
 }
