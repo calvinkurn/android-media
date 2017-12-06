384a291866debd5c7fe81959a176fe5e44e6c4dd
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2017-12-06 13:54:40 +0700

    Add image view on image clicked on all adapter that loads image

--- tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/detailv2/view/customadapter/ChatProductAdapter.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/detailv2/view/customadapter/ChatProductAdapter.java
@@ -1,6 +1,8 @@
 package com.tokopedia.inbox.rescenter.detailv2.view.customadapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +12,11 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.tkpd.library.utils.ImageHandler;
+import com.tokopedia.core.PreviewProductImage;
 import com.tokopedia.inbox.R;
+import com.tokopedia.inbox.rescenter.detailv2.view.viewmodel.detailreschat.ConversationAttachmentDomain;
 import com.tokopedia.inbox.rescenter.detailv2.view.viewmodel.detailreschat.ConversationProductDomain;
+import com.tokopedia.inbox.rescenter.player.VideoPlayerActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +43,7 @@ public class ChatProductAdapter extends RecyclerView.Adapter<ChatProductAdapter.
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
+    public void onBindViewHolder(Holder holder, final int position) {
         ConversationProductDomain product = productList.get(position);
         ImageHandler.LoadImage(holder.ivImage, product.getImage().get(0).getThumb());
         holder.tvMore.setVisibility(View.GONE);
@@ -46,6 +51,34 @@ public class ChatProductAdapter extends RecyclerView.Adapter<ChatProductAdapter.
             holder.tvMore.setVisibility(View.VISIBLE);
             holder.tvMore.setText("+" + (productList.size() - position));
         }
+
+        holder.ivImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProductPreview(productList, position);
+            }
+        });
+    }
+
+    private void openProductPreview(List<ConversationProductDomain> list, int position) {
+        ArrayList<String> imageUrls = new ArrayList<>();
+        for (ConversationProductDomain model : list) {
+            imageUrls.add(model.getImage().get(0).getFull());
+        }
+        Intent intent = new Intent(context, PreviewProductImage.class);
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList("fileloc", imageUrls);
+        bundle.putInt("img_pos", position);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
+
+    private void openVideoPlayer(String urlVideo) {
+        Intent intent = new Intent(context, VideoPlayerActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(VideoPlayerActivity.PARAMS_URL_VIDEO, urlVideo);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
     }
 
     @Override
--- tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/detailv2/view/customadapter/ChatProveAdapter.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/detailv2/view/customadapter/ChatProveAdapter.java
@@ -1,6 +1,8 @@
 package com.tokopedia.inbox.rescenter.detailv2.view.customadapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +11,10 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.tkpd.library.utils.ImageHandler;
+import com.tokopedia.core.PreviewProductImage;
 import com.tokopedia.inbox.R;
 import com.tokopedia.inbox.rescenter.detailv2.view.viewmodel.detailreschat.ConversationAttachmentDomain;
+import com.tokopedia.inbox.rescenter.player.VideoPlayerActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +39,37 @@ public class ChatProveAdapter extends RecyclerView.Adapter<ChatProveAdapter.Hold
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
+    public void onBindViewHolder(Holder holder, final int position) {
         ConversationAttachmentDomain attachment = attachmentList.get(position);
         ImageHandler.LoadImage(holder.ivImage, attachment.getThumb());
+
+        holder.ivImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openProductPreview(attachmentList, position);
+            }
+        });
+    }
+
+    private void openProductPreview(List<ConversationAttachmentDomain> list, int position) {
+        ArrayList<String> imageUrls = new ArrayList<>();
+        for (ConversationAttachmentDomain model : list) {
+            imageUrls.add(model.getFull());
+        }
+        Intent intent = new Intent(context, PreviewProductImage.class);
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList("fileloc", imageUrls);
+        bundle.putInt("img_pos", position);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
+
+    private void openVideoPlayer(String urlVideo) {
+        Intent intent = new Intent(context, VideoPlayerActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(VideoPlayerActivity.PARAMS_URL_VIDEO, urlVideo);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
     }
 
     @Override
