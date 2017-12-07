2cfbd1dfc1feb38d2fac073b5c7e4c78cabbedba
hangnadi <hangnadi@tokopedia.com>
2017-12-07 10:02:18 +0700

    T38682 - [Android] [Discovery] Hot List Promo - adjust ui and ux for widget copy voucher code

--- tkpddesign/src/main/java/com/tokopedia/design/text/CopyPromoVoucher.java
+++ tkpddesign/src/main/java/com/tokopedia/design/text/CopyPromoVoucher.java
@@ -1,8 +1,12 @@
 package com.tokopedia.design.text;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
@@ -10,14 +14,21 @@ import android.widget.TextView;
 import com.tokopedia.design.R;
 import com.tokopedia.design.base.BaseCustomView;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
+
 /**
  * Created by nakama on 12/5/17.
  */
 
 public class CopyPromoVoucher extends BaseCustomView {
 
-    private TextView textVoucherCode;
+    private TextView tvVoucherCode;
     private View buttonCopy;
+    private View iconCopyCode;
+    private View textCopyCode;
+    private CountDownTimer timer;
+
+    private String textVoucherCode;
 
     public CopyPromoVoucher(@NonNull Context context) {
         super(context);
@@ -40,8 +51,61 @@ public class CopyPromoVoucher extends BaseCustomView {
 
     private void init() {
         View view = inflate(getContext(), R.layout.widget_copy_voucher_code, this);
-        textVoucherCode = (TextView) view.findViewById(R.id.text_voucher_code);
-        buttonCopy = view.findViewById(R.id.text_copy_code);
+        tvVoucherCode = (TextView) view.findViewById(R.id.text_voucher_code);
+        iconCopyCode = view.findViewById(R.id.icon_copy_code);
+        textCopyCode = view.findViewById(R.id.text_copy_code);
+        buttonCopy = view.findViewById(R.id.button_copy);
+
+        iconCopyCode.setVisibility(GONE);
+        textCopyCode.setVisibility(VISIBLE);
+
+        timer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                flipBackground(false);
+                tvVoucherCode.setText(textVoucherCode);
+            }
+        };
+
+        buttonCopy.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                copyTextToClipBoard();
+                flipBackground(true);
+                tvVoucherCode.setText(getContext().getString(R.string.success_copy_voucher_code));
+                timer.start();
+            }
+        });
+    }
+
+    private void flipBackground(boolean active) {
+        if (active) {
+            iconCopyCode.setVisibility(VISIBLE);
+            textCopyCode.setVisibility(INVISIBLE);
+            buttonCopy.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.shape_copy_voucher_code_right_active));
+            tvVoucherCode.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.shape_copy_voucher_code_left_active));
+        } else {
+            iconCopyCode.setVisibility(GONE);
+            textCopyCode.setVisibility(VISIBLE);
+            buttonCopy.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.shape_copy_voucher_code_right_default));
+            tvVoucherCode.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.shape_copy_voucher_code_left_default));
+        }
+    }
+
+    private void copyTextToClipBoard() {
+        ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("voucher code", textVoucherCode);
+        clipboard.setPrimaryClip(clip);
+    }
+
+    public void setTextVoucherCode(String textVoucherCode) {
+        this.textVoucherCode = textVoucherCode;
+        tvVoucherCode.setText(textVoucherCode);
     }
 
 }
