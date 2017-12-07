8dc214d8e99a9c4542440d744b038f2a364d7600
alvarisi <alvarisi01@gmail.com>
2017-12-07 10:13:06 +0700

    add/copy better adapter from core to abstraction

---
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/adapter/viewholders/ErrorNetworkViewHolder.java
@@ -0,0 +1,27 @@
+package com.tokopedia.abstraction.base.view.adapter.viewholders;
+
+
+import android.support.annotation.LayoutRes;
+import android.view.View;
+
+import com.tokopedia.core.R;
+import com.tokopedia.core.base.adapter.model.ErrorNetworkModel;
+
+/**
+ * @author by erry on 02/02/17.
+ */
+
+public class ErrorNetworkViewHolder extends AbstractViewHolder<ErrorNetworkModel> {
+
+    @LayoutRes
+    public final static int LAYOUT = R.layout.design_error_network;
+
+    public ErrorNetworkViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void bind(ErrorNetworkModel element) {
+
+    }
+}
