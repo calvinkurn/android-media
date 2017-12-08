eba9999af7b60e2717d3d8b90b6bb9b80e0a92c8
Hafizh Herdi <hherdinaufal@gmail.com>
2017-12-08 14:00:52 +0700

    update to 8.1

--- customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/fcm/AppNotificationReceiver.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.firebase.messaging.RemoteMessage;
 import com.moengage.push.PushManager;
+import com.tkpd.library.utils.CommonUtils;
 import com.tokopedia.core.gcm.Constants;
 import com.tokopedia.core.gcm.NotificationAnalyticsReceiver;
 import com.tokopedia.tkpd.ConsumerMainApplication;
@@ -31,10 +32,12 @@ public class AppNotificationReceiver implements IAppNotificationReceiver {
 
     @Override
     public void onMoengageNotificationReceived(RemoteMessage message) {
+        CommonUtils.dumper("moengage push "+message);
         PushManager.getInstance().getPushHandler().handlePushPayload(ConsumerMainApplication.getAppContext(), message.getData());
     }
 
     public void onNotificationReceived(String from, Bundle bundle) {
+        CommonUtils.dumper("moengage push "+from +" "+bundle);
         if (bundle.containsKey(Constants.ARG_NOTIFICATION_ISPROMO)) {
             bundle.putString(Constants.KEY_ORIGIN, Constants.ARG_NOTIFICATION_APPLINK_PROMO_LABEL);
         }
