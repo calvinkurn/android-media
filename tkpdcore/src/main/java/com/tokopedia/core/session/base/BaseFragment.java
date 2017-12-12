6760b9bb7256650771f1cf33b58faabde321a748
eriksuprayogi <eriksuprayogi@gmail.com>
2017-12-12 11:46:43 +0700

    fix crash open shop verify phone

--- tkpdcore/src/main/java/com/tokopedia/core/session/base/BaseFragment.java
+++ tkpdcore/src/main/java/com/tokopedia/core/session/base/BaseFragment.java
@@ -46,7 +46,6 @@ public abstract class BaseFragment<T extends Base> extends Fragment implements B
         inflateOtherView(parentView);
         unbinder = ButterKnife.bind(this, parentView);
         customView();
-        presenter.initData(getActivity());
         return this.parentView = onCreateView(parentView, savedInstanceState);
     }
 
@@ -64,6 +63,7 @@ public abstract class BaseFragment<T extends Base> extends Fragment implements B
     public void onResume() {
         super.onResume();
         presenter.subscribe();
+        presenter.initData(getActivity());
     }
 
     @Override
