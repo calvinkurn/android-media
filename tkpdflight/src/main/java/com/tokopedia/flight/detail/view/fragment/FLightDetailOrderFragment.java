52ecf65a404ed0823c82ee2f4d927af72eb0e328
zulfikar rahman <rahman.zulfikar94@gmail.com>
2017-12-12 16:14:41 +0700

    fix ui detail

--- tkpdflight/src/main/java/com/tokopedia/flight/detail/view/fragment/FLightDetailOrderFragment.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/detail/view/fragment/FLightDetailOrderFragment.java
@@ -2,6 +2,7 @@ package com.tokopedia.flight.detail.view.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,5 +33,9 @@ public class FLightDetailOrderFragment extends BaseDaggerFragment {
         View view = inflater.inflate(R.layout.fragment_flight_detail_order, container, false);
 
         return view;
+    }
+
+    public static Fragment createInstance() {
+        return null;
     }
 }
