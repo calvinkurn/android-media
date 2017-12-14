b94825b7bb0223ddbe4a13e1edcedafe867d2d1c
Irfan Khoirul Muhlishin <irfankhoirul@gmail.com>
2017-12-14 10:32:36 +0700

    Insurance TnC done

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/insurance/view/InsuranceTnCFragment.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/insurance/view/InsuranceTnCFragment.java
@@ -1,15 +1,11 @@
 package com.tokopedia.transaction.insurance.view;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -34,8 +30,8 @@ public class InsuranceTnCFragment extends BasePresenterFragment<InsuranceTnCCont
 
     @BindView(R2.id.web_view_terms_and_condition)
     WebView webViewTermsAndCondition;
-    @BindView(R2.id.progress_bar)
-    ProgressBar progressBar;
+    @BindView(R2.id.pb_loading)
+    ProgressBar pbLoading;
 
     @Inject
     InsuranceTnCContract.Presenter presenter;
@@ -70,10 +66,15 @@ public class InsuranceTnCFragment extends BasePresenterFragment<InsuranceTnCCont
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        presenter.loadWebViewData();
+    }
+
+    @Override
     protected void initialPresenter() {
         initializeInjector();
         presenter.attachView(this);
-        presenter.loadWebViewData();
     }
 
     private void initializeInjector() {
@@ -124,17 +125,16 @@ public class InsuranceTnCFragment extends BasePresenterFragment<InsuranceTnCCont
         webViewTermsAndCondition.setWebViewClient(new TermsAndConditionsWebViewClient());
         webViewTermsAndCondition.setWebChromeClient(new WebChromeClient());
         webViewTermsAndCondition.loadData(webViewData, "text/html", "UTF-8");
-        webViewTermsAndCondition.getSettings().setJavaScriptEnabled(true);
     }
 
     @Override
     public void showLoading() {
-        progressBar.setVisibility(View.VISIBLE);
+        pbLoading.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideLoading() {
-        progressBar.setVisibility(View.GONE);
+        pbLoading.setVisibility(View.GONE);
     }
 
     @Override
@@ -144,26 +144,6 @@ public class InsuranceTnCFragment extends BasePresenterFragment<InsuranceTnCCont
     }
 
     private class TermsAndConditionsWebViewClient extends WebViewClient {
-        @Override
-        public void onLoadResource(WebView view, String url) {
-            super.onLoadResource(view, url);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-        }
-
-        private String replaceTagHtml(String url){
-            String htmlTag = "</html>";
-            int indexTagHtml = url.indexOf(htmlTag);
-            return url.substring(indexTagHtml + htmlTag.length());
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
-        }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -175,16 +155,6 @@ public class InsuranceTnCFragment extends BasePresenterFragment<InsuranceTnCCont
         public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
             super.onReceivedSslError(view, handler, error);
             handler.cancel();
-        }
-
-        @Override
-        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-            return super.shouldInterceptRequest(view, request);
-        }
-
-        @Override
-        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
-            return super.shouldOverrideKeyEvent(view, event);
         }
 
     }
