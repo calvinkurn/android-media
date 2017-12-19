134f245c04a447e697430a75091dba4a0ff1a70a
noiz354 <noiz354@gmail.com>
2017-12-19 16:47:55 +0700

    add fragment shop info location first time.

--- tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/fragment/ShopOpenMandatoryLocationFragment.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/fragment/ShopOpenMandatoryLocationFragment.java
@@ -1,20 +1,54 @@
 package com.tokopedia.seller.shop.open.view.fragment;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tokopedia.core.base.presentation.BaseDaggerFragment;
+import com.tokopedia.seller.R;
 import com.tokopedia.seller.base.view.listener.StepperListener;
+import com.tokopedia.seller.shop.open.view.ShopOpenStepperModel;
 import com.tokopedia.seller.shop.setting.view.fragment.ShopSettingLocationFragment;
 import com.tokopedia.seller.shop.setting.view.model.ShopSettingLocationModel;
 
-/**
- * Created by nathan on 10/21/17.
- */
 
-public class ShopOpenMandatoryLocationFragment extends ShopSettingLocationFragment {
+/**
+ * @author normansyahputa
+ */
+public class ShopOpenMandatoryLocationFragment extends BaseDaggerFragment {
+
+    protected ShopOpenStepperModel stepperModel;
+    protected StepperListener stepperListener;
+
+    public static ShopOpenMandatoryLocationFragment getInstance(){
+        return new ShopOpenMandatoryLocationFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_shop_open_location, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 
     @Override
-    protected void onNextButtonClicked() {
-        if (getActivity() instanceof StepperListener) {
-//            ShopSettingLocationModel shopSettingLocationModel = getDataModel();
-            ((StepperListener) getActivity()).goToNextPage(null);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof StepperListener) {
+            this.stepperListener = (StepperListener) context;
         }
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected String getScreenName() {
+        return null;
     }
 }
