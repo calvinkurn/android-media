c404c149f3bdf29d803f624b15221e59ebf0bd90
alvarisi <alvarisi01@gmail.com>
2017-12-19 11:38:33 +0700

    base search list v2 framgnet

---
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/fragment/BaseSearchListV2Fragment.java
@@ -0,0 +1,82 @@
+package com.tokopedia.abstraction.base.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tokopedia.abstraction.R;
+import com.tokopedia.abstraction.base.view.adapter.Visitable;
+import com.tokopedia.design.text.SearchInputView;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author normansyahputa on 5/17/17.
+ */
+
+public abstract class BaseSearchListV2Fragment<T extends Visitable> extends BaseListV2Fragment<T> implements SearchInputView.Listener {
+
+    private static final long DEFAULT_DELAY_TEXT_CHANGED = TimeUnit.MILLISECONDS.toMillis(300);
+
+    protected SearchInputView searchInputView;
+
+    protected long getDelayTextChanged() {
+        return DEFAULT_DELAY_TEXT_CHANGED;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        searchInputView = getSearchInputView(view);
+        searchInputView.setDelayTextChanged(getDelayTextChanged());
+        searchInputView.setListener(this);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_base_search_list, container, false);
+    }
+
+    @NonNull
+    protected SearchInputView getSearchInputView(View view) {
+        return (SearchInputView) view.findViewById(R.id.search_input_view);
+    }
+
+    @Override
+    public void renderAddList(@NonNull List<T> list) {
+        super.renderAddList(list);
+        if (list.size() == 0) {
+            showSearchView(false);
+        } else {
+            showSearchView(true);
+        }
+    }
+
+    @Override
+    public void renderList(@NonNull List<T> list) {
+        super.renderList(list);
+    }
+
+    @Override
+    public void showGetListError() {
+        super.showGetListError();
+        if (getAdapter().getItemCount() > 0) {
+            showSearchView(true);
+        } else {
+            showSearchView(false);
+        }
+    }
+
+    private void showSearchView(boolean isVisible) {
+        if (isVisible) {
+            searchInputView.setVisibility(View.VISIBLE);
+        } else {
+            searchInputView.setVisibility(View.GONE);
+        }
+    }
+}
