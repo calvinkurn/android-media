c3d60b128d2fbf67bd12fbfaa77f045ccaed9cc9
alvarisi <alvarisi01@gmail.com>
2017-12-20 16:02:25 +0700

    flight checkout request

--- tkpdflight/src/main/java/com/tokopedia/flight/review/domain/verifybooking/model/response/MetaData.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/review/domain/verifybooking/model/response/MetaData.java
@@ -1,7 +1,6 @@
 
 package com.tokopedia.flight.review.domain.verifybooking.model.response;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -10,79 +9,16 @@ public class MetaData {
     @SerializedName("cart_id")
     @Expose
     private String cartId;
-    @SerializedName("adult")
+    @SerializedName("flight_id")
     @Expose
-    private int adult;
-    @SerializedName("child")
-    @Expose
-    private int child;
-    @SerializedName("infant")
-    @Expose
-    private int infant;
-    @SerializedName("class")
-    @Expose
-    private int _class;
-    @SerializedName("journeys")
-    @Expose
-    private List<Journey> journeys = null;
-    @SerializedName("passengers")
-    @Expose
-    private List<Passenger> passengers = null;
+    private String flightId;
+
 
     public String getCartId() {
         return cartId;
     }
 
-    public void setCartId(String cartId) {
-        this.cartId = cartId;
+    public String getFlightId() {
+        return flightId;
     }
-
-    public int getAdult() {
-        return adult;
-    }
-
-    public void setAdult(int adult) {
-        this.adult = adult;
-    }
-
-    public int getChild() {
-        return child;
-    }
-
-    public void setChild(int child) {
-        this.child = child;
-    }
-
-    public int getInfant() {
-        return infant;
-    }
-
-    public void setInfant(int infant) {
-        this.infant = infant;
-    }
-
-    public int getClass_() {
-        return _class;
-    }
-
-    public void setClass_(int _class) {
-        this._class = _class;
-    }
-
-    public List<Journey> getJourneys() {
-        return journeys;
-    }
-
-    public void setJourneys(List<Journey> journeys) {
-        this.journeys = journeys;
-    }
-
-    public List<Passenger> getPassengers() {
-        return passengers;
-    }
-
-    public void setPassengers(List<Passenger> passengers) {
-        this.passengers = passengers;
-    }
-
 }
