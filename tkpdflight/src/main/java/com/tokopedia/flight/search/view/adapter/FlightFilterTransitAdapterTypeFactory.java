8bc4bc8ad2ddd55224db0ec68139a9d955645420
fatahillah alvarisi <alvarisi01@gmail.com>
2017-12-22 00:09:33 +0700

    flight filter transit adapter

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/search/view/adapter/FlightFilterTransitAdapterTypeFactory.java
@@ -0,0 +1,36 @@
+package com.tokopedia.flight.search.view.adapter;
+
+import android.view.View;
+
+import com.tokopedia.abstraction.base.view.adapter.BaseAdapterTypeFactory;
+import com.tokopedia.abstraction.base.view.adapter.BaseListCheckableTypeFactory;
+import com.tokopedia.abstraction.base.view.adapter.holder.CheckableBaseViewHolder2;
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
+import com.tokopedia.flight.search.view.adapter.viewholder.FlightFilterTransitViewHolder;
+import com.tokopedia.flight.search.view.model.resultstatistics.TransitStat;
+
+/**
+ * Created by alvarisi on 12/21/17.
+ */
+
+public class FlightFilterTransitAdapterTypeFactory extends BaseAdapterTypeFactory implements BaseListCheckableTypeFactory<TransitStat> {
+    private CheckableBaseViewHolder2.CheckableInteractionListener interactionListener;
+
+    public FlightFilterTransitAdapterTypeFactory(CheckableBaseViewHolder2.CheckableInteractionListener interactionListener) {
+        this.interactionListener = interactionListener;
+    }
+
+    @Override
+    public int type(TransitStat viewModel) {
+        return FlightFilterTransitViewHolder.LAYOUT;
+    }
+
+    @Override
+    public AbstractViewHolder createViewHolder(View parent, int type) {
+        if (type == FlightFilterTransitViewHolder.LAYOUT) {
+            return new FlightFilterTransitViewHolder(parent, interactionListener);
+        } else {
+            return super.createViewHolder(parent, type);
+        }
+    }
+}
