ebe068f9a8f99dcc78f9884eb36a98310d2dc655
fatahillah alvarisi <alvarisi01@gmail.com>
2017-12-27 11:29:22 +0700

    delete unused

--- tkpdflight/src/main/java/com/tokopedia/flight/search/di/FlightSearchComponent.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/search/di/FlightSearchComponent.java
@@ -2,7 +2,6 @@ package com.tokopedia.flight.search.di;
 
 import com.tokopedia.flight.common.di.component.FlightComponent;
 import com.tokopedia.flight.search.view.activity.FlightSearchFilterActivity;
-import com.tokopedia.flight.search.view.fragment.FlightSearchFragment;
 import com.tokopedia.flight.search.view.fragment.FlightSearchV2Fragment;
 
 import dagger.Component;
@@ -13,7 +12,6 @@ import dagger.Component;
 @FlightSearchScope
 @Component(modules = FlightSearchModule.class, dependencies = FlightComponent.class)
 public interface FlightSearchComponent {
-    void inject(FlightSearchFragment flightSearchFragment);
 
     void inject(FlightSearchV2Fragment flightSearchFragment);
 
--- tkpdflight/src/main/java/com/tokopedia/flight/search/view/activity/FlightSearchActivity.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/search/view/activity/FlightSearchActivity.java
@@ -15,13 +15,12 @@ import com.tokopedia.flight.common.util.FlightDateUtil;
 import com.tokopedia.flight.common.util.FlightFlowUtil;
 import com.tokopedia.flight.common.view.BaseFlightActivity;
 import com.tokopedia.flight.dashboard.view.fragment.viewmodel.FlightPassengerViewModel;
-import com.tokopedia.flight.search.view.fragment.FlightSearchFragment;
 import com.tokopedia.flight.search.view.fragment.FlightSearchV2Fragment;
 import com.tokopedia.flight.search.view.model.FlightSearchPassDataViewModel;
 
 
 public class FlightSearchActivity extends BaseFlightActivity
-        implements FlightSearchFragment.OnFlightSearchFragmentListener, FlightSearchV2Fragment.OnFlightSearchFragmentListener {
+        implements FlightSearchV2Fragment.OnFlightSearchFragmentListener {
     protected static final String EXTRA_PASS_DATA = "EXTRA_PASS_DATA";
     private static final int REQUEST_CODE_BOOKING = 10;
     private static final int REQUEST_CODE_RETURN = 11;
@@ -125,8 +124,8 @@ public class FlightSearchActivity extends BaseFlightActivity
                     switch (data.getIntExtra(FlightFlowExtraConstant.EXTRA_FLOW_DATA, 0)) {
                         case FlightFlowConstant.PRICE_CHANGE:
                             Fragment fragment = getFragment();
-                            if (fragment instanceof FlightSearchFragment) {
-                                ((FlightSearchFragment) fragment).loadInitialData();
+                            if (fragment instanceof FlightSearchV2Fragment) {
+                                ((FlightSearchV2Fragment) fragment).loadInitialData();
                             }
                             break;
                         case FlightFlowConstant.EXPIRED_JOURNEY:
