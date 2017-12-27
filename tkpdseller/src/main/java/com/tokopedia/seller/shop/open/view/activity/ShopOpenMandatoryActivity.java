aef6fd6ea448826682c92c6a8a652f820a251ad4
noiz354 <noiz354@gmail.com>
2017-12-27 13:29:59 +0700

    fix crashes at dont keep activities.

--- tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/activity/ShopOpenMandatoryActivity.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/shop/open/view/activity/ShopOpenMandatoryActivity.java
@@ -113,6 +113,8 @@ public class ShopOpenMandatoryActivity extends BaseStepperActivity<ShopOpenStepp
 
     @Override
     public ShopOpenDomainComponent getComponent() {
+        initComponent();
+
         return component;
     }
 
