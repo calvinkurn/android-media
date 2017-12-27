899f681dd54ab18c236c92ed9f0f8724f9ba590a
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2017-12-27 17:34:58 +0700

    add model for banner

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/banner/data/source/cloud/model/BannerDetail.java
@@ -0,0 +1,11 @@
+package com.tokopedia.flight.banner.data.source.cloud.model;
+
+/**
+ * Created by nakama on 27/12/17.
+ */
+
+public class BannerDetail {
+    private String type;
+    private int id;
+    private BannerAttribute attributes;
+}
