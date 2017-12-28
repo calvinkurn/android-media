20b405a7f48866f3eb0bb292396ea5882a98b18e
yfsx <yoas.fs@gmail.com>
2017-12-28 18:18:58 +0700

    udpate kol schema

--- tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/presenter/KolFollowingListPresenter.java
+++ tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/presenter/KolFollowingListPresenter.java
@@ -3,12 +3,18 @@ package com.tokopedia.tkpd.tkpdfeed.feedplus.view.presenter;
 import com.tokopedia.core.base.presentation.BaseDaggerPresenter;
 import com.tokopedia.tkpd.tkpdfeed.feedplus.view.listener.KolFollowingList;
 
+import javax.inject.Inject;
+
 /**
  * Created by yfsx on 28/12/17.
  */
 
 public class KolFollowingListPresenter extends BaseDaggerPresenter<KolFollowingList.View>
         implements KolFollowingList.Presenter {
+
+    @Inject
+    public KolFollowingListPresenter() {
+    }
 
     @Override
     public void getKolFollowingList(int userId) {
