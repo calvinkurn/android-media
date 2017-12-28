bc37c97a8ecff8f00457a0e428c67a98c2828355
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2017-12-28 14:08:52 +0700

    add banner model

--- tkpdflight/src/main/java/com/tokopedia/flight/banner/data/source/cloud/model/BannerDetail.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/banner/data/source/cloud/model/BannerDetail.java
@@ -8,4 +8,28 @@ public class BannerDetail {
     private String type;
     private int id;
     private BannerAttribute attributes;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BannerAttribute getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(BannerAttribute attributes) {
+        this.attributes = attributes;
+    }
 }
