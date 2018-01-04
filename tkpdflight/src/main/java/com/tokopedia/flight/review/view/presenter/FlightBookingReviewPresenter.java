f86eafa0111c82bb3d4adbf19f764a08dec009b5
alvarisi <alvarisi01@gmail.com>
2018-01-04 15:09:18 +0700

    fix meta data

--- tkpdflight/src/main/java/com/tokopedia/flight/review/view/presenter/FlightBookingReviewPresenter.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/review/view/presenter/FlightBookingReviewPresenter.java
@@ -76,7 +76,7 @@ public class FlightBookingReviewPresenter extends FlightBaseBookingPresenter<Fli
                 if (dataResponseVerify.getAttributesData().getCartItems() != null && dataResponseVerify.getAttributesData().getCartItems().size() > 0) {
                     CartItem verifyCartItem = dataResponseVerify.getAttributesData().getCartItems().get(0);
                     int totalPrice = verifyCartItem.getConfiguration().getPrice();
-                    String flightId = verifyCartItem.getMetaData().getFlightId();
+                    String flightId = verifyCartItem.getMetaData().getInvoiceId();
                     RequestParams requestParams;
                     if (dataResponseVerify.getAttributesData().getPromo() != null && dataResponseVerify.getAttributesData().getPromo().getCode().length() > 0) {
                         requestParams = flightBookingCheckoutUseCase.createRequestParam(flightId, totalPrice, dataResponseVerify.getAttributesData().getPromo().getCode());
