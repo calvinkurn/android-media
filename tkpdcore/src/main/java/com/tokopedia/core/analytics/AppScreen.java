f5f043a3344039fd53c9cdb3712c86a07b050de0
nisie <nathania.sutedja@tokopedia.com>
2018-01-05 11:13:25 +0700

    Add email icon

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/AppScreen.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/AppScreen.java
@@ -268,6 +268,7 @@ public final class AppScreen {
     public static final String SCREEN_COTP_DEFAULT = "Account Verification Page";
     public static final String SCREEN_COTP_SMS = "Account Verification - SMS ";
     public static final String SCREEN_COTP_CALL = "Account Verification - Call";
+    public static final String SCREEN_COTP_EMAIL = "Account Verification - Email";
     public static final String SCREEN_CHOOSE_TOKOCASH_ACCOUNT = "Login Tokocash - Choose Account";
     public static final String SCREEN_NOT_CONNECTED_TO_TOKOCASH = "Login Tokocash - Not Connected";
 
