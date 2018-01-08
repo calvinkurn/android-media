0e5c7b7b2b785877667c4bf8bb12a2074316dd50
nathaniel <nathaniel.khuana@tokopedia.com>
2018-01-08 15:38:39 +0700

    Remove unused resources

--- tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/activity/FlightDashboardActivity.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/activity/FlightDashboardActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatDelegate;
 
 import com.airbnb.deeplinkdispatch.DeepLink;
 import com.tokopedia.abstraction.di.component.HasComponent;
-import com.tokopedia.abstraction.utils.Constants;
 import com.tokopedia.flight.FlightComponentInstance;
 import com.tokopedia.flight.applink.ApplinkConstant;
 import com.tokopedia.flight.common.view.BaseFlightActivity;
