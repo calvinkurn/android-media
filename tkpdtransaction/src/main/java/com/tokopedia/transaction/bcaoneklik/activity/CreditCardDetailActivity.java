344f1ad411d3916b964c0a0832a10fae5de39fb1
Aghny A. Putra <aghny@tuta.io>
2018-01-11 11:03:11 +0700

    Move delete cc dialog to custom vertical position

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/bcaoneklik/activity/CreditCardDetailActivity.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/bcaoneklik/activity/CreditCardDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 
--- tkpdtransaction/src/main/java/com/tokopedia/transaction/bcaoneklik/dialog/DeleteCreditCardDialog.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/bcaoneklik/dialog/DeleteCreditCardDialog.java
@@ -5,41 +5,54 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.tokopedia.transaction.R;
+import com.tokopedia.transaction.R2;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by kris on 8/23/17. Tokopedia
  */
 
-public class DeleteCreditCardDialog extends DialogFragment{
+public class DeleteCreditCardDialog extends DialogFragment {
 
     private static final String TOKEN_ID = "TOKEN_ID";
     private static final String CARD_ID = "CARD_ID";
 
     private DeleteCreditCardDialogListener listener;
 
+    @BindView(R2.id.delete_button) Button deleteButton;
+    @BindView(R2.id.cancel_button) Button cancelButton;
+
     public static DeleteCreditCardDialog createDialog(String tokenId, String cardId) {
-        DeleteCreditCardDialog cardDialog = new DeleteCreditCardDialog();
         Bundle bundle = new Bundle();
         bundle.putString(TOKEN_ID, tokenId);
         bundle.putString(CARD_ID, cardId);
+
+        DeleteCreditCardDialog cardDialog = new DeleteCreditCardDialog();
         cardDialog.setArguments(bundle);
+
         return cardDialog;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.credit_card_delete_dialog, container, false);
+        ButterKnife.bind(this, view);
+
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-        View view= inflater.inflate(R.layout.credit_card_delete_dialog, container, false);
-        Button deleteButton = view.findViewById(R.id.delete_button);
-        Button cancelButton = view.findViewById(R.id.cancel_button);
+        initWindowDialog(getDialog().getWindow());
+
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +66,7 @@ public class DeleteCreditCardDialog extends DialogFragment{
                 dismissDialog();
             }
         });
+
         return view;
     }
 
@@ -60,13 +74,21 @@ public class DeleteCreditCardDialog extends DialogFragment{
     public void onAttach(Context context) {
         super.onAttach(context);
         listener = (DeleteCreditCardDialogListener) context;
-
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         listener = (DeleteCreditCardDialogListener) activity;
+    }
+
+    private void initWindowDialog(Window window) {
+        window.setGravity(Gravity.TOP | Gravity.START);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.y = -200;
+        window.setAttributes(params);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
     }
 
     private void dismissDialog() {
@@ -76,4 +98,5 @@ public class DeleteCreditCardDialog extends DialogFragment{
     public interface DeleteCreditCardDialogListener {
         void onConfirmDelete(String tokenId);
     }
+
 }
