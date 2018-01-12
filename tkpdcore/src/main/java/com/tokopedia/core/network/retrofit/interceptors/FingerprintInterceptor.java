a3a6169fbc2a9f386c9208e17faef341892a8748
nisie <nathania.sutedja@tokopedia.com>
2018-01-12 09:28:04 +0700

    Open validation fingerprint for sellerapp and kit kat os

--- tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/interceptors/FingerprintInterceptor.java
+++ tkpdcore/src/main/java/com/tokopedia/core/network/retrofit/interceptors/FingerprintInterceptor.java
@@ -47,10 +47,10 @@ public class FingerprintInterceptor implements Interceptor {
     private Request.Builder addFingerPrint(final Request.Builder newRequest) {
         String json = "";
 
-        boolean sellerAppAndKitkat = GlobalConfig.isSellerApp() && Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT;
-        if (!sellerAppAndKitkat) {
-            json = getFingerPrintJson();
-        }
+//        boolean sellerAppAndKitkat = GlobalConfig.isSellerApp() && Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT;
+//        if (!sellerAppAndKitkat) {
+        json = getFingerPrintJson();
+//        }
 
         SessionHandler session = new SessionHandler(MainApplication.getAppContext());
         newRequest.addHeader(KEY_SESSION_ID, FCMCacheManager.getRegistrationIdWithTemp(MainApplication.getAppContext()));
