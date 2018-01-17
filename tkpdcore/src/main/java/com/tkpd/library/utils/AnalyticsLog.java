419b33cb3d36030418653e1787536d911b197bcc
nisie <nathania.sutedja@tokopedia.com>
2018-01-17 13:30:38 +0700

    Change format log

--- tkpdcore/src/main/java/com/tkpd/library/utils/AnalyticsLog.java
+++ tkpdcore/src/main/java/com/tkpd/library/utils/AnalyticsLog.java
@@ -21,27 +21,30 @@ public class AnalyticsLog {
     private static final String TOKEN_LOG_NOTIFIER = "2719adf1-18c8-4cc6-8c92-88a07594f7db";
 
     public static void logForceLogout(String url) {
-        AnalyticsLog.log("Force Logout! User: " + SessionHandler.getLoginID(MainApplication.getAppContext())
-                + " Last Access Url: " + url
-                + " App Version : " + GlobalConfig.getPackageApplicationName() + " " + GlobalConfig.VERSION_NAME
-                + " App Code : " + GlobalConfig.VERSION_CODE
-                + " Android Version : " + Build.VERSION.RELEASE
-                + " Android Model : " + android.os.Build.MODEL
-                + " Device ID: " + GCMHandler.getRegistrationId(MainApplication.getAppContext())
+        AnalyticsLog.log("Error Type = Force Logout!"
+                + " User ID = " + SessionHandler.getLoginID(MainApplication.getAppContext())
+                + " URL = " + url
+                + " App Package = " + GlobalConfig.getPackageApplicationName()
+                + " App Version = " + GlobalConfig.VERSION_NAME
+                + " App Code = " + GlobalConfig.VERSION_CODE
+                + " OS Version = " + Build.VERSION.RELEASE
+                + " Device Model = " + android.os.Build.MODEL
+                + " Device ID = " + GCMHandler.getRegistrationId(MainApplication.getAppContext())
 
         );
     }
 
     public static void logNetworkError(String url, int errorCode) {
-        AnalyticsLog.log("Error Network! "
-                + " Error Code: " + errorCode
-                + " User: " + SessionHandler.getLoginID(MainApplication.getAppContext())
-                + " URL: " + url
-                + " App Version : " + GlobalConfig.getPackageApplicationName() + " " + GlobalConfig.VERSION_NAME
-                + " App Code : " + GlobalConfig.VERSION_CODE
-                + " Android Version : " + Build.VERSION.RELEASE
-                + " Android Model : " + android.os.Build.MODEL
-                + " Device ID: " + GCMHandler.getRegistrationId(MainApplication.getAppContext())
+        AnalyticsLog.log("Error Type = Error Network! "
+                + " Error Code = " + errorCode
+                + " User ID = " + SessionHandler.getLoginID(MainApplication.getAppContext())
+                + " URL = " + url
+                + " App Package = " + GlobalConfig.getPackageApplicationName()
+                + " App Version = " + GlobalConfig.VERSION_NAME
+                + " App Code = " + GlobalConfig.VERSION_CODE
+                + " OS Version = " + Build.VERSION.RELEASE
+                + " Device Model = " + android.os.Build.MODEL
+                + " Device ID = " + GCMHandler.getRegistrationId(MainApplication.getAppContext())
         );
     }
 
