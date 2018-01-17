52369b2bc05d4cef08c81840113d791eec1ec714
nathaniel <nathaniel.khuana@tokopedia.com>
2018-01-17 09:09:57 +0700

    Move opportunity whitelist to specific module

--- tkpdseller/src/main/java/com/tokopedia/seller/opportunity/common/util/OpportunityWhiteList.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/opportunity/common/util/OpportunityWhiteList.java
@@ -1,8 +1,8 @@
-package com.tokopedia.topads.common.util;
+package com.tokopedia.seller.opportunity.common.util;
 
 import com.tokopedia.core.cache.domain.model.CacheApiWhiteListDomain;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
-import com.tokopedia.topads.dashboard.constant.TopAdsNetworkConstant;
+import com.tokopedia.seller.opportunity.data.constant.OpportunityConstant;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
  * Created by nathan on 12/28/17.
  */
 
-public class TopAdsWhiteList {
+public class OpportunityWhiteList {
 
     private static final long TEN_SECOND = TimeUnit.SECONDS.toSeconds(10);
     private static final long THIRTY_SECOND = TimeUnit.SECONDS.toSeconds(30);
@@ -26,21 +26,8 @@ public class TopAdsWhiteList {
     public static List<CacheApiWhiteListDomain> getWhiteList() {
         List<CacheApiWhiteListDomain> cacheApiWhiteList = new ArrayList<>();
 
-        // TopAds Deposit
-        cacheApiWhiteList.add(new CacheApiWhiteListDomain(TkpdBaseURL.TOPADS_DOMAIN,
-                TopAdsNetworkConstant.PATH_DASHBOARD_DEPOSIT, TEN_SECOND));
-
-        // TopAds Credit
-        cacheApiWhiteList.add(new CacheApiWhiteListDomain(TkpdBaseURL.TOPADS_DOMAIN,
-                TopAdsNetworkConstant.PATH_DASHBOARD_CREDIT, ONE_HOUR));
-
-        // TopAds Statistic
-        cacheApiWhiteList.add(new CacheApiWhiteListDomain(TkpdBaseURL.TOPADS_DOMAIN,
-                TopAdsNetworkConstant.PATH_DASHBOARD_STATISTIC, FIVE_MINUTE));
-
-        // Suggestion Bid
-        cacheApiWhiteList.add(new CacheApiWhiteListDomain(TkpdBaseURL.TOPADS_DOMAIN,
-                TopAdsNetworkConstant.GET_SUGGESTION, ONE_HOUR));
+        // Opportunity category
+        cacheApiWhiteList.add(new CacheApiWhiteListDomain(TkpdBaseURL.BASE_DOMAIN, OpportunityConstant.PATH_GET_CATEGORY, ONE_DAY));
 
         return cacheApiWhiteList;
     }
