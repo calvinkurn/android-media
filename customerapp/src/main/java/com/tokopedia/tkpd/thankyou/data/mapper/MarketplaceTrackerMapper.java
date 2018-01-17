41a782e43d24d09402d7662e4e6cc04d65073ab6
okasurya <brilliantsurya@gmail.com>
2018-01-17 15:44:40 +0700

    fix data layer typage marketplace

--- customerapp/src/main/java/com/tokopedia/tkpd/thankyou/data/mapper/MarketplaceTrackerMapper.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/thankyou/data/mapper/MarketplaceTrackerMapper.java
@@ -1,24 +1,20 @@
 package com.tokopedia.tkpd.thankyou.data.mapper;
 
 import android.util.Log;
+
+import com.google.android.gms.tagmanager.DataLayer;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
 import com.tokopedia.core.analytics.PurchaseTracking;
-import com.tokopedia.core.util.SessionHandler;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.payment.PaymentMethod;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.tracker.ActionField;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.tracker.Ecommerce;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.tracker.MarketplaceTrackerData;
 import com.tokopedia.core.database.manager.GlobalCacheManager;
+import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.var.TkpdCache;
 import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.GraphqlResponse;
 import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.PaymentGraphql;
 import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.payment.PaymentData;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.tracker.Product;
-import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.tracker.Purchase;
+import com.tokopedia.tkpd.thankyou.data.pojo.marketplace.payment.PaymentMethod;
 import com.tokopedia.tkpd.thankyou.domain.model.ThanksTrackerConst;
 import com.tokopedia.transaction.cart.model.cartdata.CartItem;
 import com.tokopedia.transaction.cart.model.cartdata.CartProduct;
@@ -36,7 +32,37 @@ import rx.functions.Func1;
 
 public class MarketplaceTrackerMapper implements Func1<Response<GraphqlResponse<PaymentGraphql>>, Boolean> {
     private static final String DATA = "data";
+
+    private static final String EVENT = "event";
+    private static final String PAYMENT_ID = "payment_id";
+    private static final String PAYMENT_STATUS = "payment_status";
+    private static final String PAYMENT_TYPE = "payment_type";
+    private static final String SHOP_ID = "shop_id";
+    private static final String SHOP_TYPE = "shopType";
+    private static final String LOGISTIC_TYPE = "logistic_type";
+    private static final String ECOMMERCE = "ecommerce";
+    private static final String USER_ID = "userId";
+
+    private static final String CURRENCY_CODE = "currencyCode";
+    private static final String PURCHASE = "purchase";
+
+    private static final String ACTION_FIELD = "actionField";
+    private static final String PRODUCTS = "products";
+
+    private static final String ID = "id";
+    private static final String AFFILIATION = "affiliation";
+    private static final String REVENUE = "revenue";
+    private static final String TAX = "tax";
+    private static final String SHIPPING = "shipping";
     private static final String COUPON = "coupon";
+
+    private static final String NAME = "name";
+    private static final String PRICE = "price";
+    private static final String BRAND = "brand";
+    private static final String CATEGORY = "category";
+    private static final String VARIANT = "variant";
+    private static final String QUANTITY = "quantity";
+
     private GlobalCacheManager globalCacheManager;
     private Gson gson;
     private SessionHandler sessionHandler;
@@ -50,19 +76,20 @@ public class MarketplaceTrackerMapper implements Func1<Response<GraphqlResponse<
 
     @Override
     public Boolean call(Response<GraphqlResponse<PaymentGraphql>> response) {
-        if(isResponseValid(response)) {
+        if (isResponseValid(response)) {
             JsonObject cacheData = new JsonParser().parse(
                     globalCacheManager.getValueString(TkpdCache.Key.CART_CACHE_TRACKER)
             ).getAsJsonObject();
 
             List<CartItem> cartItemList = gson.fromJson(
                     cacheData.get(DATA),
-                    new TypeToken<List<CartItem>>() {}.getType()
+                    new TypeToken<List<CartItem>>() {
+                    }.getType()
             );
             PaymentData paymentData = response.body().getData().getPayment();
             paymentData.setCoupon(cacheData.get(COUPON).isJsonNull() ? "" : cacheData.get(COUPON).toString());
 
-            if(cartItemList != null) {
+            if (cartItemList != null) {
                 for (CartItem cartItem : cartItemList) {
                     processData(paymentData, cartItem);
                 }
@@ -82,54 +109,46 @@ public class MarketplaceTrackerMapper implements Func1<Response<GraphqlResponse<
 
     @SuppressWarnings("unchecked")
     private void processData(PaymentData payment, CartItem cartItem) {
-        MarketplaceTrackerData marketplaceTrackerData = getTrackingData(payment, cartItem);
-        Log.d("oka", marketplaceTrackerData.toString());
-        PurchaseTracking.marketplace(marketplaceTrackerData);
+        PurchaseTracking.marketplace(getTrackingData(payment, cartItem));
     }
 
-    private MarketplaceTrackerData getTrackingData(PaymentData paymentData, CartItem cartItem) {
-        MarketplaceTrackerData trackingData = new MarketplaceTrackerData();
-        trackingData.setEvent(PurchaseTracking.TRANSACTION);
-
-        if(cartItem.getCartShop() != null) {
-            trackingData.setShopId(cartItem.getCartShop().getShopId());
+    private Map<String, Object> getTrackingData(PaymentData paymentData, CartItem cartItem) {
+        String shopId = "";
+        if (cartItem.getCartShop() != null) {
+            shopId = cartItem.getCartShop().getShopId();
         }
 
-        trackingData.setPaymentId(String.valueOf(paymentData.getPaymentId()));
-        trackingData.setShopType("");
-
-        trackingData.setPaymentType(
-                getPaymentType(paymentData.getPaymentMethod())
-        );
-
-        if(cartItem.getCartShipments() != null) {
-            trackingData.setLogisticType(cartItem.getCartShipments().getShipmentId());
+        String logisticType = "";
+        if (cartItem.getCartShipments() != null) {
+            logisticType = cartItem.getCartShipments().getShipmentId();
         }
 
-        trackingData.setUserId(sessionHandler.getLoginID());
-
-        trackingData.setEcommerce(
-                getEcommerceData(paymentData, cartItem)
+        return DataLayer.mapOf(
+                EVENT, PurchaseTracking.TRANSACTION,
+                SHOP_ID, shopId,
+                PAYMENT_ID, String.valueOf(paymentData.getPaymentId()),
+                PAYMENT_TYPE, getPaymentType(paymentData.getPaymentMethod()),
+                LOGISTIC_TYPE, logisticType,
+                USER_ID, sessionHandler.getLoginID(),
+                ECOMMERCE, getEcommerceData(paymentData, cartItem)
         );
-
-        return trackingData;
     }
 
     private String getPaymentType(PaymentMethod paymentMethod) {
-        if(paymentMethod != null && paymentMethod.getMethod() != null) {
+        if (paymentMethod != null && paymentMethod.getMethod() != null) {
             switch (paymentMethod.getMethod()) {
                 case ThanksTrackerConst.Template.TRANSFER:
-                    if(paymentMethod.getTransfer() != null && paymentMethod.getTransfer().getGateway() != null) {
+                    if (paymentMethod.getTransfer() != null && paymentMethod.getTransfer().getGateway() != null) {
                         return paymentMethod.getTransfer().getGateway().getGatewayName();
                     }
                     break;
                 case ThanksTrackerConst.Template.INSTANT:
-                    if(paymentMethod.getInstant() != null && paymentMethod.getInstant().getGateway() != null) {
+                    if (paymentMethod.getInstant() != null && paymentMethod.getInstant().getGateway() != null) {
                         return paymentMethod.getInstant().getGateway().getGatewayName();
                     }
                     break;
                 default:
-                    if(paymentMethod.getDefer() != null
+                    if (paymentMethod.getDefer() != null
                             && paymentMethod.getDefer().getGateway() != null) {
                         return paymentMethod.getDefer().getGateway().getGatewayName();
                     }
@@ -140,47 +159,40 @@ public class MarketplaceTrackerMapper implements Func1<Response<GraphqlResponse<
         return "";
     }
 
-    private Ecommerce getEcommerceData(PaymentData paymentData, CartItem cartItem) {
-        Ecommerce ecommerce = new Ecommerce();
-        ecommerce.setCurrencyCode("IDR");
-        ecommerce.setPurchase(
-                getPurchaseData(paymentData,cartItem)
+    private Map<String, Object> getEcommerceData(PaymentData paymentData, CartItem cartItem) {
+        return DataLayer.mapOf(
+                CURRENCY_CODE, "IDR",
+                PURCHASE, getPurchaseData(paymentData, cartItem)
         );
-
-        return ecommerce;
     }
 
-    private Purchase getPurchaseData(PaymentData paymentData, CartItem cartItem) {
-        Purchase purchase = new Purchase();
-        purchase.setActionField(getActionField(paymentData, cartItem));
-        purchase.setProducts(getProductList(paymentData, cartItem.getCartProducts()));
-        return purchase;
+    private Map<String, Object> getPurchaseData(PaymentData paymentData, CartItem cartItem) {
+        List<Object> products = getProductList(paymentData, cartItem.getCartProducts());
+        return DataLayer.mapOf(
+                ACTION_FIELD, getActionField(paymentData, cartItem),
+                PRODUCTS, DataLayer.listOf(products.toArray(new Object[products.size()]))
+        );
     }
 
-    private List<Product> getProductList(PaymentData paymentData, List<CartProduct> cartProducts) {
-        List<Product> products = new ArrayList<>();
-        for(CartProduct cartProduct : cartProducts) {
-            Product product = new Product();
-//            product.setBrand();
-//            product.setCategory(cartProduct.);
-            product.setCoupon(paymentData.getCoupon());
-            product.setId(cartProduct.getProductId());
-            product.setName(cartProduct.getProductName());
-            product.setPrice(cartProduct.getProductPrice());
-            product.setQuantity(cartProduct.getProductQuantity());
-//            product.setVariant(cartProduct.);
-            products.add(product);
+    private List<Object> getProductList(PaymentData paymentData, List<CartProduct> cartProducts) {
+        List<Object> products = new ArrayList<>();
+        for (CartProduct cartProduct : cartProducts) {
+            products.add(DataLayer.mapOf(
+                    COUPON, paymentData.getCoupon(),
+                    ID, cartProduct.getProductId(),
+                    NAME, cartProduct.getProductName(),
+                    PRICE, cartProduct.getProductPrice(),
+                    QUANTITY, cartProduct.getProductQuantity()
+            ));
         }
         return products;
     }
 
-    private ActionField getActionField(PaymentData paymentData, CartItem cartItem) {
-        ActionField actionField = new ActionField();
-//        actionField.setId(String.valueOf(paymentData.getPaymentId()));
-//        actionField.setAffiliation();
-        actionField.setCoupon(paymentData.getCoupon());
-        actionField.setRevenue(cartItem.getCartTotalAmount());
-        actionField.setShipping(cartItem.getCartShippingRate());
-        return actionField;
+    private Map<String, Object> getActionField(PaymentData paymentData, CartItem cartItem) {
+        return DataLayer.mapOf(
+                COUPON, paymentData.getCoupon(),
+                REVENUE, cartItem.getCartTotalAmount(),
+                SHIPPING, cartItem.getCartShippingRate()
+        );
     }
 }
