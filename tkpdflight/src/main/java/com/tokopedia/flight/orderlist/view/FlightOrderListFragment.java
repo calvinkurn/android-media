83b08a11a995c6a39dcd0adb3290a82022d77b9c
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2018-01-18 15:44:41 +0700

    change quick filter to the new one

--- tkpdflight/src/main/java/com/tokopedia/flight/orderlist/view/FlightOrderListFragment.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/orderlist/view/FlightOrderListFragment.java
@@ -19,6 +19,7 @@ import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter;
 import com.tokopedia.abstraction.base.view.fragment.BaseListFragment;
 import com.tokopedia.design.quickfilter.QuickFilterAdapter;
 import com.tokopedia.design.quickfilter.QuickFilterItem;
+import com.tokopedia.design.quickfilter.QuickSingleFilterView;
 import com.tokopedia.flight.FlightModuleRouter;
 import com.tokopedia.flight.R;
 import com.tokopedia.flight.common.constant.FlightUrl;
@@ -47,12 +48,13 @@ import javax.inject.Inject;
 
 public class FlightOrderListFragment extends BaseListFragment<Visitable, FlightOrderTypeFactory>
         implements FlightOrderListContract.View,
-        QuickFilterAdapter.ActionListener,
+        QuickSingleFilterView.ActionListener,
         FlightOrderAdapter.OnAdapterInteractionListener {
     public static final int PER_PAGE = 10;
     @Inject
     FlightOrderListPresenter presenter;
-    private QuickFilterAdapter filterAdapter;
+//    private QuickFilterAdapter filterAdapter;
+    private QuickSingleFilterView quickSingleFilterView;
 
     private String selectedFilter;
 
@@ -80,14 +82,16 @@ public class FlightOrderListFragment extends BaseListFragment<Visitable, FlightO
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_flight_order_list, container, false);
-        RecyclerView filtersRecyclerView = view.findViewById(R.id.rv_filters);
+        quickSingleFilterView = view.findViewById(R.id.quick_filter);
+        quickSingleFilterView.setListener(this);
+        /*RecyclerView filtersRecyclerView = view.findViewById(R.id.rv_filters);
 
         filtersRecyclerView.setHasFixedSize(true);
         filtersRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),
                 LinearLayoutManager.HORIZONTAL, false));
         filterAdapter = new QuickFilterAdapter();
         filtersRecyclerView.setAdapter(filterAdapter);
-        filterAdapter.setListener(this);
+        filterAdapter.setListener(this);*/
         return view;
     }
 
@@ -117,7 +121,8 @@ public class FlightOrderListFragment extends BaseListFragment<Visitable, FlightO
 
     @Override
     public void renderOrderStatus(List<QuickFilterItem> filterItems) {
-        filterAdapter.addQuickFilterItems(filterItems);
+        quickSingleFilterView.setDefaultItem(filterItems.get(0));
+        quickSingleFilterView.renderFilter(filterItems);
     }
 
     @Override
@@ -125,12 +130,12 @@ public class FlightOrderListFragment extends BaseListFragment<Visitable, FlightO
         return String.valueOf(selectedFilter);
     }
 
-    @Override
+/*    @Override
     public void clearFilter() {
         selectedFilter = "";
         showSwipeLoading();
         loadInitialData();
-    }
+    }*/
 
     @Override
     public void selectFilter(String typeFilter) {
