3e70cdb01ea81de073100aa8ae6dd12a1d97d62f
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-01-19 14:19:03 +0700

    fix context classcastexception

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -392,7 +392,7 @@ public abstract class SellerRouterApplication extends MainApplication
         Intent intent = new Intent(context, WelcomeActivity.class);
         if (SessionHandler.isV4Login(context)) {
             if (SessionHandler.isUserHasShop(context)) {
-                return DashboardActivity.createInstance((Activity) context);
+                return DashboardActivity.createInstance(context);
             } else {
                 return intent;
             }
