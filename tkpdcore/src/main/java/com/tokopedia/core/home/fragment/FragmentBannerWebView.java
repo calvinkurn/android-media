b62885bebbd03c0e2cf4ae5558cc794c5eedde17
okasurya <brilliantsurya@gmail.com>
2018-01-25 11:23:17 +0700

    fix back on webview if pdp not found

--- tkpdcore/src/main/java/com/tokopedia/core/home/fragment/FragmentBannerWebView.java
+++ tkpdcore/src/main/java/com/tokopedia/core/home/fragment/FragmentBannerWebView.java
@@ -169,6 +169,7 @@ public class FragmentBannerWebView extends Fragment {
                     return true;
                 case DeepLinkChecker.PRODUCT:
                     DeepLinkChecker.openProduct(url, getActivity());
+                    getActivity().finish();
                     return true;
                 case DeepLinkChecker.SHOP:
                     ((BannerWebView) getActivity()).openShop(url);
