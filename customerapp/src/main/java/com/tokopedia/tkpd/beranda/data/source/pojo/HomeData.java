873f4cf3b7994a94996195a3f2d163e9a1739b1f
Henry Priyono <henry.priyono@tokopedia.com>
2018-01-30 18:03:05 +0700

    Revert "Temporary Commit For Error Finding"
    This reverts commit 15a1ae68c8d39958b69aa3807c9e63990f2e47b2.

--- customerapp/src/main/java/com/tokopedia/tkpd/beranda/data/source/pojo/HomeData.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/beranda/data/source/pojo/HomeData.java
@@ -1,7 +1,6 @@
 package com.tokopedia.tkpd.beranda.data.source.pojo;
 
 import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
 import com.tokopedia.core.network.entity.home.Ticker;
 import com.tokopedia.tkpd.beranda.domain.model.banner.BannerDataModel;
 
@@ -10,9 +9,30 @@ import com.tokopedia.tkpd.beranda.domain.model.banner.BannerDataModel;
  */
 
 public class HomeData {
-    @SerializedName("ticker")
     @Expose
-    Ticker ticker;
+    private DynamicHomeChannel dynamicHomeChannel;
+
+    @Expose
+    private BannerDataModel slides;
+
+    @Expose
+    private Ticker ticker;
+
+    public DynamicHomeChannel getDynamicHomeChannel() {
+        return dynamicHomeChannel;
+    }
+
+    public void setDynamicHomeChannel(DynamicHomeChannel dynamicHomeChannel) {
+        this.dynamicHomeChannel = dynamicHomeChannel;
+    }
+
+    public BannerDataModel getSlides() {
+        return slides;
+    }
+
+    public void setSlides(BannerDataModel slides) {
+        this.slides = slides;
+    }
 
     public Ticker getTicker() {
         return ticker;
