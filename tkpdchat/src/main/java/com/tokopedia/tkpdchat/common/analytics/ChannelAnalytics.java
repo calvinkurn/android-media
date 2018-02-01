362634228c2fbdf5588fbb5c9af42e9976b3717f
nisie <nathania.sutedja@tokopedia.com>
2018-02-01 16:04:37 +0700

    Add tkpdchat

---
+++ tkpdchat/src/main/java/com/tokopedia/tkpdchat/common/analytics/ChannelAnalytics.java
@@ -0,0 +1,14 @@
+package com.tokopedia.tkpdchat.common.analytics;
+
+/**
+ * @author by nisie on 2/1/18.
+ */
+
+public class ChannelAnalytics {
+
+    public static class Screen {
+        public static final String CHANNEL_LIST = "Channel List";
+    }
+
+
+}
