427ed8a5599a766441124d9e845f904b64c8160f
Henry Priyono <henry.priyono@tokopedia.com>
2018-02-02 11:03:20 +0700

    Revert "Remove SaveToCache for problem finding"
    This reverts commit cc0e51720d36fca353b35a7dae52a4ae9c44f0c8.

--- customerapp/src/main/java/com/tokopedia/tkpd/beranda/data/source/HomeDataSource.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/beranda/data/source/HomeDataSource.java
@@ -69,11 +69,11 @@ public class HomeDataSource {
                 }
                 throw new RuntimeException("Cache is empty!!");
             }
-        });
+        }).onErrorResumeNext(getHomeData());
     }
 
     public Observable<List<Visitable>> getHomeData() {
-        return homeDataApi.getHomeData(getRequestPayload()).map(homeMapper);
+        return homeDataApi.getHomeData(getRequestPayload()).map(homeMapper).doOnNext(saveToCache());
     }
 
     private String getRequestPayload() {
