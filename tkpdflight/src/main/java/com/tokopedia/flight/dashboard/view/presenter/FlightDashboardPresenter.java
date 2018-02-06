6963af07fba2e99b5e432c34cabc37ba314166cc
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2018-02-06 17:46:16 +0700

    fix load from cache

--- tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/presenter/FlightDashboardPresenter.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/dashboard/view/presenter/FlightDashboardPresenter.java
@@ -464,8 +464,11 @@ public class FlightDashboardPresenter extends BaseDaggerPresenter<FlightDashboar
     private void actionRenderFromPassData(final boolean isSearchImmediately) {
         FlightDashboardPassDataViewModel flightDashboardPassDataViewModel = getView().getDashboardPassData();
         String[] departureDate = flightDashboardPassDataViewModel.getDepartureDate().split("-");
-        onDepartureDateChange(Integer.parseInt(departureDate[INDEX_DATE_YEAR]), Integer.parseInt(departureDate[INDEX_DATE_MONTH]), Integer.parseInt(departureDate[INDEX_DATE_DATE]));
-        onSingleTripChecked();
+
+        if (flightDashboardPassDataViewModel.getDepartureDate() != null && !flightDashboardPassDataViewModel.getDepartureDate().isEmpty() ) {
+            onDepartureDateChange(Integer.parseInt(departureDate[INDEX_DATE_YEAR]), Integer.parseInt(departureDate[INDEX_DATE_MONTH]), Integer.parseInt(departureDate[INDEX_DATE_DATE]));
+            onSingleTripChecked();
+        }
 
         if (!flightDashboardPassDataViewModel.getReturnDate().isEmpty() && flightDashboardPassDataViewModel.isRoundTrip()) {
             String[] returnDate = flightDashboardPassDataViewModel.getReturnDate().split("-");
