241321d5595a46916bf609d8b9ba32fdb6afedd9
eriksuprayogi <eriksuprayogi@gmail.com>
2018-02-07 23:32:28 +0700

    fix api module

--- tkpdhome/src/main/java/com/tokopedia/home/common/ApiModule.java
+++ tkpdhome/src/main/java/com/tokopedia/home/common/ApiModule.java
@@ -1,12 +1,18 @@
 package com.tokopedia.home.common;
 
+import com.google.gson.Gson;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
+import com.tokopedia.core.network.retrofit.coverters.StringResponseConverter;
+import com.tokopedia.core.network.retrofit.coverters.TkpdResponseConverter;
+import com.tokopedia.core.network.retrofit.interceptors.FingerprintInterceptor;
 
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author by errysuprayogi on 11/28/17.
@@ -29,17 +35,27 @@ public class ApiModule {
 
     @Provides
     public OkHttpClient provideOkHttpClient(OkHttpClient.Builder okHttpClientBuilder,
-                                            HttpLoggingInterceptor httpLoggingInterceptor) {
+                                            HttpLoggingInterceptor httpLoggingInterceptor,
+                                            HomeAuthInterceptor authInterceptor) {
         return okHttpClientBuilder
                 .addInterceptor(httpLoggingInterceptor)
+                .addInterceptor(authInterceptor)
+                .addInterceptor(new FingerprintInterceptor())
                 .build();
     }
 
     @HomeGraphQLQualifier
     @Provides
-    public Retrofit provideHomeGraphQlRetrofit(OkHttpClient okHttpClient,
-                                       Retrofit.Builder retrofitBuilder) {
-        return retrofitBuilder.baseUrl(TkpdBaseURL.HOME_DATA_BASE_URL).client(okHttpClient).build();
+    public Retrofit provideHomeGraphQlRetrofit(OkHttpClient okHttpClient, Gson gson) {
+        return new Retrofit.Builder()
+                .baseUrl(TkpdBaseURL.HOME_DATA_BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(new TkpdResponseConverter())
+                .addConverterFactory(new StringResponseConverter())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+
     }
 
 }
