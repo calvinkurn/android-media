c7e576ae715f3be75b49526863a9b20338b92e27
hangnadi <hangnadi@tokopedia.com>
2018-02-07 23:07:26 +0700

    fix implementation dagger home module to use abstraction (need to improve)

--- tkpdhome/src/main/java/com/tokopedia/home/common/ApiModule.java
+++ tkpdhome/src/main/java/com/tokopedia/home/common/ApiModule.java
@@ -1,12 +1,21 @@
 package com.tokopedia.home.common;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
+import com.tokopedia.core.network.core.OkHttpFactory;
+import com.tokopedia.core.network.core.OkHttpRetryPolicy;
+import com.tokopedia.core.network.retrofit.coverters.GeneratedHostConverter;
+import com.tokopedia.core.network.retrofit.coverters.StringResponseConverter;
+import com.tokopedia.core.network.retrofit.coverters.TkpdResponseConverter;
 
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author by errysuprayogi on 11/28/17.
@@ -30,16 +39,28 @@ public class ApiModule {
     @Provides
     public OkHttpClient provideOkHttpClient(OkHttpClient.Builder okHttpClientBuilder,
                                             HttpLoggingInterceptor httpLoggingInterceptor) {
-        return okHttpClientBuilder
-                .addInterceptor(httpLoggingInterceptor)
-                .build();
+        return OkHttpFactory.create()
+                .addOkHttpRetryPolicy(OkHttpRetryPolicy.createdDefaultOkHttpRetryPolicy())
+                .buildClientNoAuth();
     }
 
     @HomeGraphQLQualifier
     @Provides
-    public Retrofit provideHomeGraphQlRetrofit(OkHttpClient okHttpClient,
-                                       Retrofit.Builder retrofitBuilder) {
-        return retrofitBuilder.baseUrl(TkpdBaseURL.HOME_DATA_BASE_URL).client(okHttpClient).build();
+    public Retrofit provideHomeGraphQlRetrofit(OkHttpClient okHttpClient) {
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+        return new Retrofit.Builder()
+                .baseUrl(TkpdBaseURL.HOME_DATA_BASE_URL)
+                .addConverterFactory(new GeneratedHostConverter())
+                .addConverterFactory(new TkpdResponseConverter())
+                .addConverterFactory(new StringResponseConverter())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(okHttpClient)
+                .build();
     }
 
 }
