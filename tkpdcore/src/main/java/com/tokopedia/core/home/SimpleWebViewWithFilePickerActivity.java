00ef0478cf7723e9656d8a073d085e29e0be524d
baghira <sandeep.gupta@tokopedia.com>
2018-02-07 17:46:11 +0530

    Added new fragment instead of changing in SimpleWebViewFragment for file picker changes in webview.

---
+++ tkpdcore/src/main/java/com/tokopedia/core/home/SimpleWebViewWithFilePickerActivity.java
@@ -0,0 +1,60 @@
+package com.tokopedia.core.home;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.tokopedia.core.R;
+import com.tokopedia.core.app.TkpdCoreWebViewActivity;
+import com.tokopedia.core.home.fragment.SimpleWebViewWithFilePickerFragment;
+
+public class SimpleWebViewWithFilePickerActivity extends TkpdCoreWebViewActivity {
+    public static final String EXTRA_URL = "url";
+    private SimpleWebViewWithFilePickerFragment fragment;
+
+    public static Intent getIntent(Context context, String url) {
+        Intent intent = new Intent(context, SimpleWebViewWithFilePickerFragment.class);
+        intent.putExtra(EXTRA_URL, url);
+        return intent;
+    }
+
+    public static Intent getIntentWithTitle(Context context, String url, String title) {
+        Intent intent = new Intent(context, SimpleWebViewWithFilePickerFragment.class);
+        intent.putExtra(EXTRA_URL, url);
+        intent.putExtra(EXTRA_TITLE, title);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        inflateView(R.layout.activity_webview_container);
+        String url = getIntent().getExtras().getString(EXTRA_URL);
+        fragment = SimpleWebViewWithFilePickerFragment.createInstance(url);
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.container, fragment);
+            fragmentTransaction.commit();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+    }
+    @Override
+    public void onBackPressed() {
+        try {
+            if (fragment.getWebview().canGoBack()) {
+                fragment.getWebview().goBack();
+            } else {
+                super.onBackPressed();
+            }
+        } catch (Exception e) {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ tkpdcore/src/main/java/com/tokopedia/core/home/fragment/SimpleWebViewWithFilePickerFragment.java
@@ -0,0 +1,246 @@
+package com.tokopedia.core.home.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.tkpd.library.utils.CommonUtils;
+import com.tokopedia.core.R;
+import com.tokopedia.core.loyaltysystem.util.URLGenerator;
+import com.tokopedia.core.network.constants.TkpdBaseURL;
+import com.tokopedia.core.util.TkpdWebView;
+import static android.app.Activity.RESULT_OK;
+/**
+ * Created by okasurya on 1/23/18.
+ * For simple webview, without any override url and such logic, just load a url
+ * For another usage please use other webview, currently we have a lot of em
+ */
+
+public class SimpleWebViewWithFilePickerFragment extends Fragment {
+    private static final String SEAMLESS = "seamless";
+    public static final int PROGRESS_COMPLETED = 100;
+    private ProgressBar progressBar;
+    private TkpdWebView webview;
+    private String mCM;
+    private ValueCallback<Uri> callbackBeforeL;
+    public ValueCallback<Uri[]> callbackAfterL;
+    public final static int ATTACH_FILE_REQUEST = 1;
+    private static final String EXTRA_URL = "url";
+
+    private class MyWebViewClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            try {
+                if (newProgress == PROGRESS_COMPLETED) {
+                    view.setVisibility(View.VISIBLE);
+                    progressBar.setVisibility(View.GONE);
+                    getActivity().setProgressBarIndeterminateVisibility(false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+        //For Android 3.0+
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            callbackBeforeL = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            startActivityForResult(Intent.createChooser(i, "File Chooser"), ATTACH_FILE_REQUEST);
+        }
+
+        // For Android 3.0+, above method not supported in some android 3+ versions, in such case we use this
+        public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+            callbackBeforeL = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            startActivityForResult(
+                    Intent.createChooser(i, "File Browser"), ATTACH_FILE_REQUEST);
+        }
+
+        //For Android 4.1+
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            callbackBeforeL = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            startActivityForResult(Intent.createChooser(i, "File Chooser"), ATTACH_FILE_REQUEST);
+        }
+
+        //For Android 5.0+
+        public boolean onShowFileChooser(
+                WebView webView, ValueCallback<Uri[]> filePathCallback,
+                WebChromeClient.FileChooserParams fileChooserParams) {
+            if (callbackAfterL != null) {
+                callbackAfterL.onReceiveValue(null);
+            }
+            callbackAfterL = filePathCallback;
+
+            Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+            contentSelectionIntent.setType("*/*");
+            Intent[] intentArray = new Intent[0];
+
+            Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+            chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+            chooserIntent.putExtra(Intent.EXTRA_TITLE, "File Chooser");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+            startActivityForResult(chooserIntent, ATTACH_FILE_REQUEST);
+            return true;
+
+        }
+    }
+
+    private class MyWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            try {
+                getActivity().setProgressBarIndeterminateVisibility(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            super.onReceivedSslError(view, handler, error);
+            handler.cancel();
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            /**
+             * do not use override url here. please use another webview to do that task.
+             * e.g.: {@link com.tokopedia.core.webview.fragment.FragmentGeneralWebView}
+             */
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            CommonUtils.dumper("DEEPLINK " + errorCode + "  " + description + " " + failingUrl);
+            super.onReceivedError(view, errorCode, description, failingUrl);
+            progressBar.setVisibility(View.GONE);
+        }
+
+    }
+
+    public SimpleWebViewWithFilePickerFragment() {
+    }
+
+    public static SimpleWebViewWithFilePickerFragment createInstance(String url) {
+        SimpleWebViewWithFilePickerFragment fragment = new SimpleWebViewWithFilePickerFragment();
+        Bundle args = new Bundle();
+        if (!TextUtils.isEmpty(url)) {
+            args.putString(EXTRA_URL, url);
+        }
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Uri[] results = null;
+            //Check if response is positive
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == ATTACH_FILE_REQUEST) {
+                    if (null == callbackAfterL) {
+                        return;
+                    }
+
+                    String dataString = intent.getDataString();
+                    if (dataString != null) {
+                        results = new Uri[]{Uri.parse(dataString)};
+
+                    }
+                }
+            }
+            callbackAfterL.onReceiveValue(results);
+            callbackAfterL = null;
+        } else {
+            if (requestCode == ATTACH_FILE_REQUEST) {
+                if (null == callbackBeforeL) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                callbackBeforeL.onReceiveValue(result);
+                callbackBeforeL = null;
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment_general_web_view, container, false);
+        String url = getArguments().getString(EXTRA_URL, TkpdBaseURL.MOBILE_DOMAIN);
+        webview = (TkpdWebView) view.findViewById(R.id.webview);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
+        progressBar.setIndeterminate(true);
+        clearCache(webview);
+        if (!url.contains(SEAMLESS))
+            webview.loadAuthUrl(URLGenerator.generateURLSessionLogin(url, getActivity()));
+        else {
+            webview.loadAuthUrl(url);
+        }
+        webview.setWebViewClient(new SimpleWebViewWithFilePickerFragment.MyWebClient());
+        webview.setWebChromeClient(new SimpleWebViewWithFilePickerFragment.MyWebViewClient());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+            webview.setWebContentsDebuggingEnabled(true);
+            CommonUtils.dumper("webviewconf debugging = true");
+        }
+        getActivity().setProgressBarIndeterminateVisibility(true);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+        optimizeWebView();
+        CookieManager.getInstance().setAcceptCookie(true);
+        return view;
+    }
+
+    private void clearCache(WebView webView) {
+        if (webView != null) {
+            webView.clearCache(true);
+        }
+    }
+
+    public WebView getWebview() {
+        return webview;
+    }
+
+    public void setWebview(TkpdWebView webview) {
+        this.webview = webview;
+    }
+
+    private void optimizeWebView() {
+        webview.setLayerType(
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT ?
+                        View.LAYER_TYPE_HARDWARE : View.LAYER_TYPE_SOFTWARE,
+                null
+        );
+    }
+}
