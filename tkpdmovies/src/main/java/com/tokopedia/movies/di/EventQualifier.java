b03b9f00fb2f0e961c3f29a882c1a67d7461f7e0
Pranay Mohapatra <pranay.mohapatra@tokopedia.com>
2018-02-07 12:02:18 +0530

    removing module "Movies"

--- tkpdmovies/src/main/java/com/tokopedia/movies/di/EventQualifier.java
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.movies.di;
-
-import javax.inject.Qualifier;
-
-
-@Qualifier
-public @interface EventQualifier {
-}
--- tkpdmovies/src/main/java/com/tokopedia/movies/view/viewmodel/EventViewModel.java
+++
@@ -1,8 +0,0 @@
-
-package com.tokopedia.movies.view.viewmodel;
-
-/**
- * Created by ashwanityagi on 02/11/17.
- */
-public class EventViewModel {
-}
