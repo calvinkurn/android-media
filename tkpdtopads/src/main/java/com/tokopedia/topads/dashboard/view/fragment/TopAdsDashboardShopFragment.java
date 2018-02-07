0de7d46436f9c2b4bdb80c8193a88eb99ccfced6
noiz354 <noiz354@gmail.com>
2018-02-07 11:38:36 +0700

    [BUG] Detail shop promo still display pop up when open https://phab.tokopedia.com/T47998 [BUG] Pop up still showing after refresh page https://phab.tokopedia.com/T47938

--- tkpdtopads/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashboardShopFragment.java
+++ tkpdtopads/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashboardShopFragment.java
@@ -211,7 +211,7 @@ public class TopAdsDashboardShopFragment extends TopAdsDashboardFragment<TopAdsD
     void onShopItemClicked() {
         Intent intent = new Intent(getActivity(), TopAdsDetailShopActivity.class);
         intent.putExtra(TopAdsExtraConstant.EXTRA_AD, shopAd);
-        intent.putExtra(TopAdsNewScheduleNewGroupFragment.EXTRA_IS_ENOUGH_DEPOSIT,false);
+        intent.putExtra(TopAdsNewScheduleNewGroupFragment.EXTRA_IS_ENOUGH_DEPOSIT,true);
         startActivityForResult(intent, REQUEST_CODE_AD_STATUS);
     }
 }
