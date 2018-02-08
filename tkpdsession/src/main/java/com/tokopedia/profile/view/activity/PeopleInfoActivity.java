0d84066d724aa6bc85cbed1be6638c95b73b2e5b
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-02-08 14:30:30 +0700

    Add placeholder activity and fragment

---
+++ tkpdsession/src/main/java/com/tokopedia/profile/view/activity/PeopleInfoActivity.java
@@ -0,0 +1,8 @@
+package com.tokopedia.profile.view.activity;
+
+/**
+ * @author by milhamj on 08/02/18.
+ */
+
+public class PeopleInfoActivity {
+}
---
+++ tkpdsession/src/main/java/com/tokopedia/profile/view/fragment/PeopleInfoFragment.java
@@ -0,0 +1,8 @@
+package com.tokopedia.profile.view.fragment;
+
+/**
+ * @author by milhamj on 08/02/18.
+ */
+
+public class PeopleInfoFragment {
+}
