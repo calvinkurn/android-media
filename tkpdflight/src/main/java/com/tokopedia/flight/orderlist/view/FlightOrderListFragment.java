de87206ee6265927408c9c69b98c9878732ad375
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2018-02-09 17:16:24 +0700

    removed unused import

--- tkpdflight/src/main/java/com/tokopedia/flight/orderlist/view/FlightOrderListFragment.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/orderlist/view/FlightOrderListFragment.java
@@ -1,6 +1,5 @@
 package com.tokopedia.flight.orderlist.view;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
