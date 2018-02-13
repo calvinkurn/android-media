a8b22f069f94d5cb1993ac124f35b9454448341d
okasurya <brilliantsurya@gmail.com>
2018-02-13 13:49:59 +0700

    add base class for bottom sheet view

---
+++ tkpddesign/src/main/java/com/tokopedia/design/bottomsheet/BaseBottomSheetView.java
@@ -0,0 +1,42 @@
+package com.tokopedia.design.bottomsheet;
+
+import android.app.Service;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * @author okasurya on 2/13/18.
+ */
+
+public abstract class BaseBottomSheetView extends BottomSheetDialog {
+    public BaseBottomSheetView(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BaseBottomSheetView(@NonNull Context context, int theme) {
+        super(context, theme);
+        init(context);
+    }
+
+    protected BaseBottomSheetView(@NonNull Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Service.LAYOUT_INFLATER_SERVICE);
+        View bottomSheetView = layoutInflater.inflate(getLayoutId(), null);
+        setContentView(bottomSheetView);
+        setViewVariables();
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutId();
+
+    protected abstract void setViewVariables();
+}
