66b16aedfbfd0aaebf579d97fe621ba353c58544
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-02-13 16:40:01 +0700

    Add activity and fragment skeleton

--- tkpdsession/src/main/java/com/tokopedia/profile/view/activity/PeopleInfoActivity.java
+++ tkpdsession/src/main/java/com/tokopedia/profile/view/activity/PeopleInfoActivity.java
@@ -1,8 +1,24 @@
 package com.tokopedia.profile.view.activity;
 
+import android.support.v4.app.Fragment;
+
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
+import com.tokopedia.abstraction.common.di.component.HasComponent;
+import com.tokopedia.profile.view.fragment.PeopleInfoFragment;
+
 /**
  * @author by milhamj on 08/02/18.
  */
 
-public class PeopleInfoActivity {
+public class PeopleInfoActivity extends BaseSimpleActivity implements HasComponent {
+    @Override
+    protected Fragment getNewFragment() {
+        return PeopleInfoFragment.newInstance();
+    }
+
+    @Override
+    public Object getComponent() {
+        //TODO milhamj
+        return null;
+    }
 }
--- tkpdsession/src/main/java/com/tokopedia/profile/view/fragment/PeopleInfoFragment.java
+++ tkpdsession/src/main/java/com/tokopedia/profile/view/fragment/PeopleInfoFragment.java
@@ -1,8 +1,30 @@
 package com.tokopedia.profile.view.fragment;
 
+import android.os.Bundle;
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment;
+
 /**
  * @author by milhamj on 08/02/18.
  */
 
-public class PeopleInfoFragment {
+public class PeopleInfoFragment extends BaseDaggerFragment {
+
+    public static PeopleInfoFragment newInstance() {
+        PeopleInfoFragment fragment = new PeopleInfoFragment();
+        Bundle bundle = new Bundle();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    protected void initInjector() {
+        //TODO milhamj
+    }
+
+    @Override
+    protected String getScreenName() {
+        //TODO milhamj
+        return null;
+    }
 }
