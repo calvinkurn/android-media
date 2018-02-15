e36f4063284e6e4a483d9ea5393c3b05bbe40c82
alvarisi <alvarisi01@gmail.com>
2018-02-15 19:52:34 +0700

    flight otp nested fragment header text

--- customerapp/src/main/java/com/tokopedia/tkpd/flight/presentation/FlightPhoneVerificationActivity.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/flight/presentation/FlightPhoneVerificationActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
+import com.tokopedia.abstraction.common.di.component.HasComponent;
+import com.tokopedia.core.app.MainApplication;
+import com.tokopedia.core.base.di.component.AppComponent;
 import com.tokopedia.otp.phoneverification.view.fragment.PhoneVerificationFragment;
 
 /**
  * @author by alvarisi on 2/15/18.
  */
 
-public class FlightPhoneVerificationActivity extends BaseSimpleActivity {
+public class FlightPhoneVerificationActivity extends BaseSimpleActivity implements HasComponent<AppComponent>, PhoneVerificationFragment.PhoneVerificationFragmentListener {
 
     public static Intent getCallingIntent(Activity activity) {
         return new Intent(activity, FlightPhoneVerificationActivity.class);
@@ -19,23 +22,23 @@ public class FlightPhoneVerificationActivity extends BaseSimpleActivity {
 
     @Override
     protected Fragment getNewFragment() {
-        return PhoneVerificationFragment.createInstance
-                (getPhoneVerificationListener(), false);
+        return FlightPhoneVerificationFragment.newInstance();
     }
 
-    private PhoneVerificationFragment.PhoneVerificationFragmentListener getPhoneVerificationListener() {
-        return new PhoneVerificationFragment.PhoneVerificationFragmentListener() {
-            @Override
-            public void onSkipVerification() {
-                FlightPhoneVerificationActivity.this.setResult(Activity.RESULT_CANCELED);
-                FlightPhoneVerificationActivity.this.finish();
-            }
+    @Override
+    public void onSkipVerification() {
+        FlightPhoneVerificationActivity.this.setResult(Activity.RESULT_CANCELED);
+        FlightPhoneVerificationActivity.this.finish();
+    }
 
-            @Override
-            public void onSuccessVerification() {
-                FlightPhoneVerificationActivity.this.setResult(Activity.RESULT_OK);
-                FlightPhoneVerificationActivity.this.finish();
-            }
-        };
+    @Override
+    public void onSuccessVerification() {
+        FlightPhoneVerificationActivity.this.setResult(Activity.RESULT_OK);
+        FlightPhoneVerificationActivity.this.finish();
+    }
+
+    @Override
+    public AppComponent getComponent() {
+        return ((MainApplication) getApplication()).getAppComponent();
     }
 }
---
+++ customerapp/src/main/java/com/tokopedia/tkpd/flight/presentation/FlightPhoneVerificationFragment.java
@@ -0,0 +1,70 @@
+package com.tokopedia.tkpd.flight.presentation;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tokopedia.otp.phoneverification.view.fragment.PhoneVerificationFragment;
+import com.tokopedia.tkpd.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FlightPhoneVerificationFragment extends Fragment {
+    private PhoneVerificationFragment.PhoneVerificationFragmentListener phoneVerificationFragmentListener;
+
+    public FlightPhoneVerificationFragment() {
+        // Required empty public constructor
+    }
+
+    public static FlightPhoneVerificationFragment newInstance() {
+        return new FlightPhoneVerificationFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_flight_phone_verification, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addFragment(R.id.container, PhoneVerificationFragment.createInstance
+                (phoneVerificationFragmentListener, false));
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (activity instanceof PhoneVerificationFragment.PhoneVerificationFragmentListener) {
+            phoneVerificationFragmentListener = (PhoneVerificationFragment.PhoneVerificationFragmentListener) activity;
+        } else {
+            throw new RuntimeException("Activity must implement PhoneVerificationFragment.PhoneVerificationFragmentListener");
+        }
+    }
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+        if (activity instanceof PhoneVerificationFragment.PhoneVerificationFragmentListener) {
+            phoneVerificationFragmentListener = (PhoneVerificationFragment.PhoneVerificationFragmentListener) activity;
+        } else {
+            throw new RuntimeException("Activity must implement PhoneVerificationFragment.PhoneVerificationFragmentListener");
+        }
+    }
+
+    private void addFragment(int containerViewId, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerViewId, fragment);
+        fragmentTransaction.commit();
+    }
+}
