5bac0496430ef51ae40299aaa5b9006adfa85196
kris <christian.daniel@tokopedia.com>
2018-02-19 15:45:47 +0700

    fix error handling on accept partial

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/detail/activity/OrderDetailActivity.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/detail/activity/OrderDetailActivity.java
@@ -777,8 +777,6 @@ public class OrderDetailActivity extends TActivity
     @Override
     public void onAcceptPartialOrderCreated(String orderId, String remark, String param) {
         presenter.partialOrder(this, orderId, remark, param);
-        setResult(Activity.RESULT_OK);
-        finish();
     }
 
     @Override
