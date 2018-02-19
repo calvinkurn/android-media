3bd5406f0e075cae116afb21db043334f34374b5
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-02-19 17:39:36 +0700

    Fix header

--- tkpdsession/src/main/java/com/tokopedia/profile/view/activity/TopProfileActivity.java
+++ tkpdsession/src/main/java/com/tokopedia/profile/view/activity/TopProfileActivity.java
@@ -2,6 +2,12 @@ package com.tokopedia.profile.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.tokopedia.abstraction.base.view.activity.BaseEmptyActivity;
 import com.tokopedia.abstraction.common.di.component.HasComponent;
@@ -13,8 +19,21 @@ import com.tokopedia.session.R;
 
 public class TopProfileActivity extends BaseEmptyActivity implements HasComponent {
 
+    private AppBarLayout appBarLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Toolbar toolbar;
+
     public static Intent newInstance(Context context) {
         return new Intent(context, TopProfileActivity.class);
+    }
+
+    @Override
+    protected void setupLayout(Bundle savedInstanceState) {
+        super.setupLayout(savedInstanceState);
+        appBarLayout = findViewById(R.id.app_bar_layout);
+        collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        setupToolbar();
     }
 
     @Override
@@ -26,5 +45,47 @@ public class TopProfileActivity extends BaseEmptyActivity implements HasComponen
     public Object getComponent() {
         //TODO milhamj
         return null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Kucing");
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle("");
+                    isShow = false;
+                }
+            }
+        });
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.BLACK);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.TRANSPARENT);
+        toolbar.setBackgroundResource(R.drawable.bg_white_toolbar_drop_shadow);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
