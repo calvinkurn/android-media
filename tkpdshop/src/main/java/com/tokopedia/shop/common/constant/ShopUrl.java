ea457473bc3916ffcf1fe51d6d4cd50c5b6dd92a
nathaniel <nathaniel.khuana@tokopedia.com>
2018-02-20 22:42:54 +0700

    Adding cardview on address list

--- tkpdshop/src/main/java/com/tokopedia/shop/common/constant/ShopUrl.java
+++ tkpdshop/src/main/java/com/tokopedia/shop/common/constant/ShopUrl.java
@@ -8,7 +8,6 @@ public class ShopUrl extends ShopCommonUrl {
 
     public static String BASE_ACE_URL = "https://ace.tokopedia.com";
 
-    public static final String SHOP_INFO_PATH = "v1/web-service/shop/get_shop_info";
     public static final String SHOP_NOTE_PATH = "v1/shop/shop_note";
     public static final String SHOP_PRODUCT_PATH = "v1/web-service/shop/get_shop_product";
 
