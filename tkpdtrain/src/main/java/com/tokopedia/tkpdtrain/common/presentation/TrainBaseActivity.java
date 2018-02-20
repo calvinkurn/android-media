e714b076d24afcf1c0a522d0d3a58c2631259093
alvarisi <alvarisi01@gmail.com>
2018-02-20 10:17:38 +0700

    train homepage starter

---
+++ tkpdtrain/src/main/java/com/tokopedia/tkpdtrain/common/presentation/TrainBaseActivity.java
@@ -0,0 +1,10 @@
+package com.tokopedia.tkpdtrain.common.presentation;
+
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
+
+/**
+ * Created by alvarisi on 2/19/18.
+ */
+
+public abstract class TrainBaseActivity extends BaseSimpleActivity {
+}
---
+++ tkpdtrain/src/main/java/com/tokopedia/tkpdtrain/homepage/presentation/TrainHomepageActivity.java
@@ -0,0 +1,13 @@
+package com.tokopedia.tkpdtrain.homepage.presentation;
+
+import android.support.v4.app.Fragment;
+
+import com.tokopedia.tkpdtrain.common.presentation.TrainBaseActivity;
+
+
+public class TrainHomepageActivity extends TrainBaseActivity {
+    @Override
+    protected Fragment getNewFragment() {
+        return null;
+    }
+}
