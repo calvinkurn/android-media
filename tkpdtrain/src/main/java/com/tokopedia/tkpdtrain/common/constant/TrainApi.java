d7d2addd862e45bfba0a42c81558d3f7482e99e9
alvarisi <alvarisi01@gmail.com>
2018-02-20 10:15:59 +0700

    train constant

---
+++ tkpdtrain/src/main/java/com/tokopedia/tkpdtrain/common/constant/TrainApi.java
@@ -0,0 +1,8 @@
+package com.tokopedia.tkpdtrain.common.constant;
+
+/**
+ * @author  by alvarisi on 2/19/18.
+ */
+
+public interface TrainApi {
+}
---
+++ tkpdtrain/src/main/java/com/tokopedia/tkpdtrain/common/constant/TrainUrl.java
@@ -0,0 +1,8 @@
+package com.tokopedia.tkpdtrain.common.constant;
+
+/**
+ * Created by alvarisi on 2/19/18.
+ */
+
+public class TrainUrl {
+}
