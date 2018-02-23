d209308608949e460e83deaab70a9b4c38a87d9d
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-02-23 17:34:48 +0700

    Set adapter position for topads so favorite works

--- tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/adapter/viewholder/topads/FeedTopadsViewHolder.java
+++ tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/adapter/viewholder/topads/FeedTopadsViewHolder.java
@@ -1,22 +1,11 @@
 package com.tokopedia.tkpd.tkpdfeed.feedplus.view.adapter.viewholder.topads;
 
-import android.content.Context;
 import android.support.annotation.LayoutRes;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-import com.tokopedia.core.base.adapter.Visitable;
 import com.tokopedia.core.base.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.tkpd.tkpdfeed.R;
-import com.tokopedia.tkpd.tkpdfeed.feedplus.view.fragment.TopAdsInfoBottomSheet;
 import com.tokopedia.tkpd.tkpdfeed.feedplus.view.viewmodel.topads.FeedTopAdsViewModel;
-import com.tokopedia.topads.sdk.listener.TopAdsInfoClickListener;
 import com.tokopedia.topads.sdk.listener.TopAdsItemClickListener;
 import com.tokopedia.topads.sdk.widget.TopAdsWidgetView;
 
@@ -40,5 +29,6 @@ public class FeedTopadsViewHolder extends AbstractViewHolder<FeedTopAdsViewModel
     @Override
     public void bind(FeedTopAdsViewModel element) {
         topAdsWidgetView.setData(element.getList());
+        topAdsWidgetView.setAdapterPosition(getAdapterPosition());
     }
 }
