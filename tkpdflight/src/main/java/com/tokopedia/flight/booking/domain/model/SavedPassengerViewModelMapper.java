847760869f02f1223bddec522ca10ac554f1abe7
Muhammad Furqan <nanjihusaha@gmail.com>
2018-02-23 10:37:17 +0700

    create mapper from SavedPassenger Entity to PassengerViewModel

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/booking/domain/model/SavedPassengerViewModelMapper.java
@@ -0,0 +1,47 @@
+package com.tokopedia.flight.booking.domain.model;
+
+import com.tokopedia.flight.booking.data.cloud.entity.SavedPassengerEntity;
+import com.tokopedia.flight.booking.view.viewmodel.FlightBookingPassengerViewModel;
+import com.tokopedia.flight.common.util.FlightDateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+/**
+ * @author by furqan on 23/02/18.
+ */
+
+public class SavedPassengerViewModelMapper {
+
+    @Inject
+    public SavedPassengerViewModelMapper() {
+    }
+
+    public FlightBookingPassengerViewModel transform(SavedPassengerEntity savedPassengerEntity) {
+        FlightBookingPassengerViewModel flightBookingPassengerViewModel = new FlightBookingPassengerViewModel();
+        flightBookingPassengerViewModel.setPassengerBirthdate(
+                FlightDateUtil.formatDate(
+                        FlightDateUtil.FORMAT_DATE_API,
+                        FlightDateUtil.DEFAULT_FORMAT,
+                        savedPassengerEntity.getPassengerAttribute().getDob()
+                )
+        );
+        flightBookingPassengerViewModel.setPassengerFirstName(savedPassengerEntity.getPassengerAttribute().getFirstName());
+        flightBookingPassengerViewModel.setPassengerLastName(savedPassengerEntity.getPassengerAttribute().getLastName());
+        flightBookingPassengerViewModel.setPassengerTitleId(savedPassengerEntity.getPassengerAttribute().getTitle());
+
+        return flightBookingPassengerViewModel;
+    }
+
+    public List<FlightBookingPassengerViewModel> transform(List<SavedPassengerEntity> savedPassengerEntityList) {
+        List<FlightBookingPassengerViewModel> flightBookingPassengerViewModelList = new ArrayList<>();
+
+        for (SavedPassengerEntity savedPassengerEntity : savedPassengerEntityList) {
+            flightBookingPassengerViewModelList.add(this.transform(savedPassengerEntity));
+        }
+
+        return flightBookingPassengerViewModelList;
+    }
+}
