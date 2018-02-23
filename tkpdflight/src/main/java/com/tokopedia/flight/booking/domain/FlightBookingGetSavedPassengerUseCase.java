805f7f8bd1bf104f6769832f7c745636fccc8930
Muhammad Furqan <nanjihusaha@gmail.com>
2018-02-23 10:37:43 +0700

    create use case to get saved passenger

--- tkpdflight/src/main/java/com/tokopedia/flight/booking/domain/FlightBookingGetSavedPassengerUseCase.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/booking/domain/FlightBookingGetSavedPassengerUseCase.java
@@ -1,12 +1,16 @@
 package com.tokopedia.flight.booking.domain;
 
+import com.tokopedia.flight.booking.data.cloud.entity.SavedPassengerEntity;
+import com.tokopedia.flight.booking.domain.model.SavedPassengerViewModelMapper;
 import com.tokopedia.flight.booking.view.viewmodel.FlightBookingPassengerViewModel;
+import com.tokopedia.flight.common.domain.FlightRepository;
 import com.tokopedia.usecase.RequestParams;
 import com.tokopedia.usecase.UseCase;
 
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * @author by furqan on 22/02/18.
@@ -14,8 +18,26 @@ import rx.Observable;
 
 public class FlightBookingGetSavedPassengerUseCase extends UseCase<List<FlightBookingPassengerViewModel>> {
 
+    private final FlightRepository flightRepository;
+    private final SavedPassengerViewModelMapper savedPassengerViewModelMapper;
+
+    public FlightBookingGetSavedPassengerUseCase(FlightRepository flightRepository, SavedPassengerViewModelMapper savedPassengerViewModelMapper) {
+        this.flightRepository = flightRepository;
+        this.savedPassengerViewModelMapper = savedPassengerViewModelMapper;
+    }
+
     @Override
     public Observable<List<FlightBookingPassengerViewModel>> createObservable(RequestParams requestParams) {
-        return null;
+        return flightRepository.getSavedPassenger()
+                .map(new Func1<List<SavedPassengerEntity>, List<FlightBookingPassengerViewModel>>() {
+                    @Override
+                    public List<FlightBookingPassengerViewModel> call(List<SavedPassengerEntity> savedPassengerEntities) {
+                        return savedPassengerViewModelMapper.transform(savedPassengerEntities);
+                    }
+                });
+    }
+
+    public RequestParams createEmptyRequestParams() {
+        return RequestParams.EMPTY;
     }
 }
