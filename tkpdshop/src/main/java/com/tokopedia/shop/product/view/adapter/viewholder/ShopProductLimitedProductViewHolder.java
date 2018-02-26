fb16c91df2e62326be9a5863d3dc63d2d8712961
nathaniel <nathaniel.khuana@tokopedia.com>
2018-02-26 15:33:24 +0700

    Show product variant and product list at product tab

--- tkpdshop/src/main/java/com/tokopedia/shop/product/view/adapter/viewholder/ShopProductLimitedProductViewHolder.java
+++ tkpdshop/src/main/java/com/tokopedia/shop/product/view/adapter/viewholder/ShopProductLimitedProductViewHolder.java
@@ -1,10 +1,15 @@
 package com.tokopedia.shop.product.view.adapter.viewholder;
 
 import android.support.annotation.LayoutRes;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.shop.R;
+import com.tokopedia.shop.product.view.adapter.ShopProductLimitedFeaturedAdapter;
+import com.tokopedia.shop.product.view.adapter.ShopProductLimitedProductAdapter;
 import com.tokopedia.shop.product.view.model.ShopProductLimitedFeaturedViewModel;
 import com.tokopedia.shop.product.view.model.ShopProductLimitedProductViewModel;
 
@@ -14,8 +19,12 @@ import com.tokopedia.shop.product.view.model.ShopProductLimitedProductViewModel;
 
 public class ShopProductLimitedProductViewHolder extends AbstractViewHolder<ShopProductLimitedProductViewModel> {
 
+    private static final int SPAN_COUNT = 2;
+
     @LayoutRes
     public static final int LAYOUT = R.layout.item_shop_product_limited_product;
+
+    private RecyclerView recyclerView;
 
     public ShopProductLimitedProductViewHolder(View itemView) {
         super(itemView);
@@ -23,15 +32,16 @@ public class ShopProductLimitedProductViewHolder extends AbstractViewHolder<Shop
     }
 
     private void findViews(View view) {
-//        titleTextView = view.findViewById(R.id.title);
-
+        recyclerView = view.findViewById(R.id.recycler_view);
     }
 
     @Override
-    public void bind(ShopProductLimitedProductViewModel element) {
-//        titleTextView.setText(element.getProductName());
-//        priceTextView.setText(element.getProductPrice());
-//        ImageHandler.LoadImage(productImageView, element.getProductImage700());
+    public void bind(ShopProductLimitedProductViewModel shopProductLimitedProductViewModel) {
+        ShopProductLimitedProductAdapter adapter = new ShopProductLimitedProductAdapter();
+        adapter.setList(shopProductLimitedProductViewModel.getShopProductViewModelList());
+        LinearLayoutManager layoutManager = new GridLayoutManager(itemView.getContext(), SPAN_COUNT);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
-
 }
