ab4ccc6abe354fe114bc56cd7f9a841cb3ba6a23
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-02-28 10:14:42 +0700

    Add GetFeedUseCase in consumerapp

--- tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/di/FeedPlusComponent.java
+++ tkpdfeed/src/main/java/com/tokopedia/tkpd/tkpdfeed/feedplus/view/di/FeedPlusComponent.java
@@ -1,6 +1,8 @@
 package com.tokopedia.tkpd.tkpdfeed.feedplus.view.di;
 
 import com.tokopedia.core.base.di.component.AppComponent;
+import com.tokopedia.tkpd.tkpdfeed.feedplus.domain.usecase.FollowKolPostUseCase;
+import com.tokopedia.tkpd.tkpdfeed.feedplus.domain.usecase.LikeKolPostUseCase;
 import com.tokopedia.tkpd.tkpdfeed.feedplus.view.fragment.FeedPlusDetailFragment;
 import com.tokopedia.tkpd.tkpdfeed.feedplus.view.fragment.FeedPlusFragment;
 import com.tokopedia.tkpd.tkpdfeed.feedplus.view.fragment.KolCommentFragment;
@@ -27,5 +29,7 @@ public interface FeedPlusComponent {
 
     void inject(KolFollowingListFragment kolFollowingListFragment);
 
+    LikeKolPostUseCase getLikeKolPostUseCase();
 
+    FollowKolPostUseCase getFollowKolPostUseCase();
 }
