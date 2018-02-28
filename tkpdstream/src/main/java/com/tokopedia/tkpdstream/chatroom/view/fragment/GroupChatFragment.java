87d59ed87fd5cf917a75d65481e8d579aacc0d80
stevenfredian <steven.fredian@tokopedia.com>
2018-02-28 13:48:27 +0700

    fix bug

--- tkpdstream/src/main/java/com/tokopedia/tkpdstream/chatroom/view/fragment/GroupChatFragment.java
+++ tkpdstream/src/main/java/com/tokopedia/tkpdstream/chatroom/view/fragment/GroupChatFragment.java
@@ -2,6 +2,8 @@ package com.tokopedia.tkpdstream.chatroom.view.fragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
@@ -23,6 +25,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.EditText;
@@ -193,17 +197,27 @@ public class GroupChatFragment extends BaseDaggerFragment implements GroupChatCo
     }
 
     private void setupToolbar() {
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Window window = getActivity().getWindow();
-//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//            window.setStatusBarColor(getResources()
-//                    .getColor(R.color.grey_transparent_background));
-//        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//
+//        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
 //            Window w = getActivity().getWindow(); // in Activity's onCreate() for instance
 //            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-//            toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+//            w.getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
+//
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Window w = getActivity().getWindow();
+//            w.setNavigationBarColor(Color.BLACK);
+//            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//            w.getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//            w.setStatusBarColor(Color.TRANSPARENT);
+//            //setStatusBarTranslucent(true);
 //        }
 
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
