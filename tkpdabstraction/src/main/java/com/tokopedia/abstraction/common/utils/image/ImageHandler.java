ab39ed626060510134f2deb3dfcd07bbcccda9e1
Muhammad Furqan <nanjihusaha@gmail.com>
2018-03-01 15:59:43 +0700

    revert

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/image/ImageHandler.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/image/ImageHandler.java
@@ -14,7 +14,6 @@ import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.text.TextUtils;
