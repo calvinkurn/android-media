c39e7af2ea573f9ddc42c1dab9430da9ce64a5a6
noiz354 <noiz354@gmail.com>
2018-03-01 16:56:46 +0700

    fix etalase passing.

--- tkpdshop/src/main/java/com/tokopedia/shop/product/view/fragment/ShopProductListFragment.java
+++ tkpdshop/src/main/java/com/tokopedia/shop/product/view/fragment/ShopProductListFragment.java
@@ -181,7 +181,7 @@ public class ShopProductListFragment extends BaseSearchListFragment<ShopProductV
             }
         });
 
-        if (shopModuleRouter != null && !shopModuleRouter.isMyOwnShop(shopId)) {
+        if (shopModuleRouter != null && !shopModuleRouter.isMyOwnShop(shopId) && TextUtils.isEmpty(etalaseName)) {
             chooseEtalaseLabelView.setContent(getString(R.string.shop_info_filter_all_showcase));
         } else {
             chooseEtalaseLabelView.setContent(getString(R.string.shop_info_filter_menu_etalase_all));
