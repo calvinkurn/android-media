aaad4e311a45650895c3879ad08f8b6001ea6bce
kris <christian.daniel@tokopedia.com>
2018-03-02 20:36:56 +0700

    fix crash in add new address caused by no product id

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/checkout/view/view/shipmentform/MultipleAddressShipmentPresenter.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/checkout/view/view/shipmentform/MultipleAddressShipmentPresenter.java
@@ -1,8 +1,14 @@
 package com.tokopedia.transaction.checkout.view.view.shipmentform;
 
+import com.tokopedia.transaction.checkout.domain.datamodel.MultipleAddressItemData;
 import com.tokopedia.transaction.checkout.domain.datamodel.MultipleAddressPriceSummaryData;
 import com.tokopedia.transaction.checkout.domain.datamodel.MultipleAddressShipmentAdapterData;
+import com.tokopedia.transaction.checkout.domain.datamodel.cartshipmentform.CartShipmentAddressFormData;
+import com.tokopedia.transaction.checkout.domain.datamodel.cartshipmentform.GroupAddress;
+import com.tokopedia.transaction.checkout.domain.datamodel.cartshipmentform.GroupShop;
+import com.tokopedia.transaction.checkout.domain.datamodel.cartshipmentform.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,10 +17,51 @@ import java.util.List;
 
 public class MultipleAddressShipmentPresenter implements IMultipleAddressShipmentPresenter{
 
+    public MultipleAddressShipmentPresenter() {
+
+    }
+
     @Override
     public void sendData(List<MultipleAddressShipmentAdapterData> shipmentData,
                          MultipleAddressPriceSummaryData priceData) {
 
+    }
+
+    @Override
+    public List<MultipleAddressShipmentAdapterData> setAdapter(CartShipmentAddressFormData data) {
+        List<MultipleAddressShipmentAdapterData> adapterDataList = new ArrayList<>();
+        for (int addressIndex = 0; addressIndex < data.getGroupAddress().size(); addressIndex++) {
+            GroupAddress currentAddress = data.getGroupAddress().get(addressIndex);
+            List<GroupShop> groupShopList = data.getGroupAddress().get(addressIndex).getGroupShop();
+            for (int shopIndex = 0; shopIndex < groupShopList.size(); shopIndex++) {
+                GroupShop currentGroupShop = groupShopList.get(shopIndex);
+                List<Product> productList = currentGroupShop.getProducts();
+                for(int productIndex = 0; productIndex < productList.size(); productIndex++) {
+                    MultipleAddressShipmentAdapterData adapterData =
+                            new MultipleAddressShipmentAdapterData();
+                    Product currentProduct = productList.get(productIndex);
+                    adapterData.setProductName(currentProduct.getProductName());
+                    adapterData.setProductPriceNumber(currentProduct.getProductPrice());
+                    adapterData.setProductPrice(String.valueOf(currentProduct.getProductPrice()));
+                    MultipleAddressItemData addressItemData = new MultipleAddressItemData();
+                    addressItemData.setCartPosition(productIndex);
+                    addressItemData.setAddressPosition(0);
+                    addressItemData.setProductWeight(currentProduct.getProductWeightFmt());
+                    addressItemData.setProductNotes(currentProduct.getProductNotes());
+                    addressItemData.setProductQty(
+                            String.valueOf(currentProduct.getProductQuantity())
+                    );
+                    addressItemData.setAddressId(
+                            String.valueOf(currentAddress.getUserAddress().getAddressId())
+                    );
+                    addressItemData.setAddressStreet(currentAddress.getUserAddress().getAddress());
+                    addressItemData.setAddressCityName(currentAddress.getUserAddress().getCityName());
+                    //addressItemData.setAddressProvinceName(currentAddress.getUserAddress().ge);
+                    adapterData.setItemData(addressItemData);
+                }
+            }
+        }
+        return null;
     }
 
 }
