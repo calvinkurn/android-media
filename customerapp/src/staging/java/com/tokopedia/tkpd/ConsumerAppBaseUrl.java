dcedac53c77185205bbe0d9b86e2490c852c06c1
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-03-02 17:16:03 +0700

    Remove todo atin

--- customerapp/src/staging/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
+++ customerapp/src/staging/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
@@ -43,7 +43,5 @@ public class ConsumerAppBaseUrl {
     public static final String TOKOPOINT_API_DOMAIN = "https://gw-staging.tokopedia.com/tokopoints/api/";
     public static final String TOKO_CAMPAIGN_URL = "https://booking-staging.tokopedia.com/trigger/v1/api/";
     public static final String HOME_DATA_BASE_URL = "https://gql-staging.tokopedia.com/";
-
-    //TODO atin
     public static final String TOPPROFILE_DOMAIN = "https://18-feature-m-staging.tokopedia.com/graphql/";
 }
