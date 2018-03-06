64de89006ed98b7491e44d69e2165a8dca28f671
alvarisi <alvarisi01@gmail.com>
2018-03-06 17:41:34 +0700

    add max retry

--- tkpdflight/src/main/java/com/tokopedia/flight/common/di/module/FlightModule.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/common/di/module/FlightModule.java
@@ -8,6 +8,7 @@ import com.tokopedia.abstraction.common.di.scope.ApplicationScope;
 import com.tokopedia.abstraction.AbstractionRouter;
 import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+import com.tokopedia.abstraction.common.network.OkHttpRetryPolicy;
 import com.tokopedia.abstraction.common.network.interceptor.ErrorResponseInterceptor;
 import com.tokopedia.flight.airline.data.FlightAirlineDataListSource;
 import com.tokopedia.flight.airport.data.source.FlightAirportDataListBackgroundSource;
@@ -33,6 +34,8 @@ import com.tokopedia.flight.search.data.FlightSearchReturnDataSource;
 import com.tokopedia.flight.search.data.FlightSearchSingleDataSource;
 import com.tokopedia.flight.search.data.db.FlightMetaDataDBSource;
 
+import java.util.concurrent.TimeUnit;
+
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
@@ -46,6 +49,10 @@ import retrofit2.Retrofit;
 @FlightScope
 @Module
 public class FlightModule {
+    private static final int NET_READ_TIMEOUT = 30;
+    private static final int NET_WRITE_TIMEOUT = 30;
+    private static final int NET_CONNECT_TIMEOUT = 30;
+    private static final int NET_RETRY = 1;
 
     @FlightScope
     @Provides
@@ -59,8 +66,12 @@ public class FlightModule {
     @FlightScope
     @Provides
     public OkHttpClient provideOkHttpClient(@ApplicationScope HttpLoggingInterceptor httpLoggingInterceptor,
-                                            FlightAuthInterceptor flightAuthInterceptor) {
+                                            FlightAuthInterceptor flightAuthInterceptor,
+                                            @FlightQualifier OkHttpRetryPolicy okHttpRetryPolicy) {
         return new OkHttpClient.Builder()
+                .readTimeout(okHttpRetryPolicy.readTimeout, TimeUnit.SECONDS)
+                .writeTimeout(okHttpRetryPolicy.writeTimeout, TimeUnit.SECONDS)
+                .connectTimeout(okHttpRetryPolicy.connectTimeout, TimeUnit.SECONDS)
                 .addInterceptor(flightAuthInterceptor)
                 .addInterceptor(httpLoggingInterceptor)
                 .addInterceptor(new ErrorResponseInterceptor(FlightErrorResponse.class))
@@ -107,5 +118,12 @@ public class FlightModule {
     @Provides
     public FlightApi provideFlightAirportApi(@FlightQualifier Retrofit retrofit) {
         return retrofit.create(FlightApi.class);
+    }
+
+    @FlightScope
+    @FlightQualifier
+    @Provides
+    public OkHttpRetryPolicy provideOkHttpRetryPolicy(){
+        return new OkHttpRetryPolicy(NET_READ_TIMEOUT, NET_WRITE_TIMEOUT, NET_CONNECT_TIMEOUT, NET_RETRY);
     }
 }
