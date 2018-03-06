f84742fb5e13d8339af0db09870641d1df51e2a3
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-03-06 18:38:30 +0700

    Fix load more isn't working

--- tkpdcontent/src/main/java/com/tokopedia/tkpdcontent/feature/profile/view/fragment/KolPostFragment.java
+++ tkpdcontent/src/main/java/com/tokopedia/tkpdcontent/feature/profile/view/fragment/KolPostFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
@@ -106,7 +105,6 @@ public class KolPostFragment extends BaseDaggerFragment implements KolPostListen
                     .setSupportsChangeAnimations(false);
         }
         layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
-        ViewCompat.setNestedScrollingEnabled(kolRecyclerView, false);
         kolRecyclerView.setLayoutManager(layoutManager);
         kolRecyclerView.setAdapter(adapter);
     }
