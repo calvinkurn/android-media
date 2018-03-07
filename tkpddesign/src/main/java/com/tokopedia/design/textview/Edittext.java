62824c8b578d8c09e7b57fc3028f29f1b3f1c317
Meyta <meytajennist@gmail.com>
2018-03-07 11:31:49 +0700

    change edittext to customedittext with drawable

---
+++ tkpddesign/src/main/java/com/tokopedia/design/textview/Edittext.java
@@ -0,0 +1,23 @@
+package com.tokopedia.design.textview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by meyta on 07/03/18.
+ */
+
+public class Edittext extends TextView {
+
+    public Edittext(Context context) {
+        super(context);
+    }
+
+    public Edittext(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Edittext(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
