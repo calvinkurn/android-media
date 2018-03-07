f34f9fbd7b6190b2b413ef71ea25ee1419216062
Hendri <hendri@tokopedia.com>
2018-03-07 13:50:08 +0700

    fix seller app build issue

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -1071,4 +1071,22 @@ public abstract class SellerRouterApplication extends MainApplication
     public void logInvalidGrant(Response response) {
         AnalyticsLog.logInvalidGrant(response.request().url().toString());
     }
+
+
+    @Override
+    public Intent getProductDetailIntent(Context context, Integer productId, String productName, String productPrice, Long dateTimeInMilis) {
+        ProductPass productPass = ProductPass.Builder.aProductPass()
+                .setProductId(productId)
+                .setProductPrice(productPrice)
+                .setProductName(productName)
+                .setDateTimeInMilis(dateTimeInMilis)
+                .build();
+        Intent intent = ProductInfoActivity.createInstance(context, productPass);
+        return intent;
+    }
+
+    @Override
+    public void startAddProduct(Activity activity, String shopId) {
+        goToAddProduct(activity);
+    }
 }
