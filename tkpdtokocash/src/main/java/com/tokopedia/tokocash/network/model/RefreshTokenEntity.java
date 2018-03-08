56c423c44f3178388b7f7b7aa328217a0837bd0e
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-03-08 15:47:48 +0700

    add serialize name for data refresh token

--- tkpdtokocash/src/main/java/com/tokopedia/tokocash/network/model/RefreshTokenEntity.java
+++ tkpdtokocash/src/main/java/com/tokopedia/tokocash/network/model/RefreshTokenEntity.java
@@ -1,12 +1,17 @@
 package com.tokopedia.tokocash.network.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by nabillasabbaha on 11/17/17.
  */
 
 public class RefreshTokenEntity {
 
-   private WalletTokenEntity data;
+    @SerializedName("data")
+    @Expose
+    private WalletTokenEntity data;
 
     public WalletTokenEntity getData() {
         return data;
