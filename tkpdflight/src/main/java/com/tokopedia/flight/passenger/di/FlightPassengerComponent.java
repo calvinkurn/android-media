20945f584ee7bd46ed3df040ad091acd4a5f2e26
Muhammad Furqan <nanjihusaha@gmail.com>
2018-03-12 10:39:22 +0700

    flight passenger di

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/passenger/di/FlightPassengerComponent.java
@@ -0,0 +1,14 @@
+package com.tokopedia.flight.passenger.di;
+
+import com.tokopedia.flight.common.di.component.FlightComponent;
+
+import dagger.Component;
+
+/**
+ * @author by furqan on 12/03/18.
+ */
+
+@FlightPassengerScope
+@Component(modules = FlightPassengerModule.class, dependencies = FlightComponent.class)
+public class FlightPassengerComponent {
+}
