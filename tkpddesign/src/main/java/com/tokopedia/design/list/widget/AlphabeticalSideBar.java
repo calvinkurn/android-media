3a796f6ce09fbc65831cba2fb44275ca1a2cc873
Meyta <meytajennist@gmail.com>
2018-03-14 19:08:06 +0700

    fix dimen

--- tkpddesign/src/main/java/com/tokopedia/design/list/widget/AlphabeticalSideBar.java
+++ tkpddesign/src/main/java/com/tokopedia/design/list/widget/AlphabeticalSideBar.java
@@ -54,7 +54,7 @@ public class AlphabeticalSideBar extends View {
         alphabetText = "A.D.G.J.M.P.S.V.Z";
         itemTextSize = getContext().getResources().getDimensionPixelSize(R.dimen.sp_12);
         int magnifierTextSize = getContext().getResources().getDimensionPixelSize(R.dimen.sp_20);
-        magnifierPadding = getContext().getResources().getDimensionPixelSize(R.dimen.dp_100);
+        magnifierPadding = getContext().getResources().getDimensionPixelSize(R.dimen.dp_10);
         int magnifierToSidebarDistance = getContext().getResources().getDimensionPixelSize(R.dimen.dp_20);
         magnifierRadius = magnifierTextSize / 2 + magnifierPadding;
         touchAreaLeftOffset = magnifierRadius * 2 + magnifierToSidebarDistance;
