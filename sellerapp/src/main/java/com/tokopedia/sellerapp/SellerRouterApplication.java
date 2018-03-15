8dbfabc54288a39a55520c8d17b6340e0094047b
nisie <nathania.sutedja@tokopedia.com>
2018-03-15 15:15:21 +0700

    Add tracking event left navigation drawer

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -181,7 +181,7 @@ public abstract class SellerRouterApplication extends MainApplication
         implements TkpdCoreRouter, SellerModuleRouter, PdpRouter, GMModuleRouter, TopAdsModuleRouter,
         IPaymentModuleRouter, IDigitalModuleRouter, TkpdInboxRouter, TransactionRouter,
         ReputationRouter, LogisticRouter, SessionRouter,
-        MitraToppersRouter, AbstractionRouter, DigitalModuleRouter{
+        MitraToppersRouter, AbstractionRouter, DigitalModuleRouter {
 
     protected RemoteConfig remoteConfig;
     private DaggerProductComponent.Builder daggerProductBuilder;
@@ -675,8 +675,8 @@ public abstract class SellerRouterApplication extends MainApplication
     @Override
     public Intent getAskSellerIntent(Context context, String toShopId, String shopName, String customSubject, String customMessage, String source, String avatar) {
 
-            return ChatRoomActivity.getAskSellerIntent(context, toShopId, shopName,
-                    customSubject, customMessage, source, avatar);
+        return ChatRoomActivity.getAskSellerIntent(context, toShopId, shopName,
+                customSubject, customMessage, source, avatar);
 
     }
 
@@ -1085,10 +1085,10 @@ public abstract class SellerRouterApplication extends MainApplication
     }
 
 
-
     @Override
     public void init() {
     }
+
     @Override
     public void registerShake(String screenName) {
     }
@@ -1119,6 +1119,7 @@ public abstract class SellerRouterApplication extends MainApplication
     @Override
     public Intent getTopProfileIntent(Context context, String userId) {
         return TopProfileActivity.newInstance(context, userId);
+    }
 
     @Override
     public Intent getProductDetailIntent(Context context, ProductPass productPass) {
@@ -1130,8 +1131,14 @@ public abstract class SellerRouterApplication extends MainApplication
     public void startAddProduct(Activity activity, String shopId) {
         goToAddProduct(activity);
     }
+
     @Override
     public boolean isEnabledGroupChat() {
         return false;
+    }
+
+    @Override
+    public void sendTrackingGroupChatLeftNavigation() {
+
     }
 }
