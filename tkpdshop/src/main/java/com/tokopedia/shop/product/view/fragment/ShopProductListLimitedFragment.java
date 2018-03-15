a177094d2dde7afd3356f00a033f155056b782e5
nathaniel <nathaniel.khuana@tokopedia.com>
2018-03-15 20:57:38 +0700

    Put search input view as view holder on limited product fragment

--- tkpdshop/src/main/java/com/tokopedia/shop/product/view/fragment/ShopProductListLimitedFragment.java
+++ tkpdshop/src/main/java/com/tokopedia/shop/product/view/fragment/ShopProductListLimitedFragment.java
@@ -19,11 +19,13 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable;
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter;
 import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyViewHolder;
-import com.tokopedia.abstraction.base.view.fragment.BaseSearchListFragment;
+import com.tokopedia.abstraction.base.view.fragment.BaseListFragment;
+import com.tokopedia.abstraction.base.view.recyclerview.VerticalRecyclerView;
 import com.tokopedia.abstraction.common.network.exception.UserNotLoginException;
 import com.tokopedia.abstraction.common.utils.network.ErrorHandler;
 import com.tokopedia.abstraction.common.utils.snackbar.NetworkErrorHelper;
 import com.tokopedia.design.loading.LoadingStateView;
+import com.tokopedia.design.text.SearchInputView;
 import com.tokopedia.shop.R;
 import com.tokopedia.shop.ShopModuleRouter;
 import com.tokopedia.shop.analytic.ShopPageTracking;
@@ -57,8 +59,8 @@ import javax.inject.Inject;
  * Created by nathan on 2/15/18.
  */
 
-public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopProductBaseViewModel, ShopProductLimitedAdapterTypeFactory>
-        implements ShopProductLimitedPromoViewHolder.PromoViewHolderListener,
+public class ShopProductListLimitedFragment extends BaseListFragment<ShopProductBaseViewModel, ShopProductLimitedAdapterTypeFactory>
+        implements SearchInputView.Listener, ShopProductLimitedPromoViewHolder.PromoViewHolderListener,
         ShopProductListLimitedView, ShopProductClickedListener, EmptyViewHolder.Callback, ShopProductFeaturedViewHolder.ShopProductFeaturedListener {
 
     private static final int REQUEST_CODE_USER_LOGIN = 100;
@@ -68,7 +70,6 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     @Inject
     ShopPageTracking shopPageTracking;
     private ProgressDialog progressDialog;
-    private LoadingStateView loadingStateView;
     private ShopInfo shopInfo;
     private ShopModuleRouter shopModuleRouter;
     private ShopPagePromoWebView.Listener promoWebViewListener;
@@ -91,12 +92,6 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
         }
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_shop_product_limited_list, container, false);
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -107,22 +102,14 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         progressDialog = new ProgressDialog(getActivity());
-        loadingStateView = view.findViewById(R.id.loading_state_view_shop_page_product);
-        loadingStateView.setViewState(LoadingStateView.VIEW_LOADING);
         progressDialog.setMessage(getString(R.string.title_loading));
-        searchInputView.setSearchHint(getString(R.string.shop_product_search_hint));
-        searchInputView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                shopPageTracking.eventClickSearchProduct(getString(R.string.shop_info_title_tab_product), shopInfo.getInfo().getShopId(),
-                        shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()), ShopPageTracking.getShopType(shopInfo.getInfo()));
-            }
-        });
+        VerticalRecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.clearItemDecoration();
         getRecyclerView(view).addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                int lastVisibleItem = ((LinearLayoutManager)recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
-                if(lastVisibleItem >= recyclerView.getLayoutManager().getItemCount()){
+                int lastVisibleItem = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+                if (lastVisibleItem >= recyclerView.getLayoutManager().getItemCount()) {
                     onLastItemVisibleTracking();
                 }
             }
@@ -142,6 +129,12 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     @Override
     protected ShopProductLimitedAdapterTypeFactory getAdapterTypeFactory() {
         return new ShopProductLimitedAdapterTypeFactory(this, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shopPageTracking.eventClickSearchProduct(getString(R.string.shop_info_title_tab_product), shopInfo.getInfo().getShopId(),
+                        shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()), ShopPageTracking.getShopType(shopInfo.getInfo()));
+            }
+        }, this, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 shopPageTracking.eventClickSeeMoreProduct(getString(R.string.shop_info_title_tab_product), shopInfo.getInfo().getShopId(),
@@ -246,24 +239,20 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     @Override
     public void renderList(@NonNull List<ShopProductBaseViewModel> list) {
         super.renderList(list);
-        loadingStateView.setViewState(LoadingStateView.VIEW_CONTENT);
-        if (list.size() <= 0) {
-            searchInputView.setVisibility(View.GONE);
-        }
         trackingImpressionFeatureProduct(list);
     }
 
     private void trackingImpressionFeatureProduct(List<ShopProductBaseViewModel> list) {
-        for(ShopProductBaseViewModel shopProductBaseViewModel : list){
-            if(shopProductBaseViewModel instanceof ShopProductLimitedFeaturedViewModel){
+        for (ShopProductBaseViewModel shopProductBaseViewModel : list) {
+            if (shopProductBaseViewModel instanceof ShopProductLimitedFeaturedViewModel) {
                 shopPageTracking.eventViewProductFeaturedImpression(getString(R.string.shop_info_title_tab_product),
-                        ((ShopProductLimitedFeaturedViewModel)shopProductBaseViewModel).getShopProductViewModelList(),
+                        ((ShopProductLimitedFeaturedViewModel) shopProductBaseViewModel).getShopProductViewModelList(),
                         shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()), ShopPageTracking.getShopType(shopInfo.getInfo()));
-            }else if(shopProductBaseViewModel instanceof ShopProductLimitedProductViewModel){
+            } else if (shopProductBaseViewModel instanceof ShopProductLimitedProductViewModel) {
                 shopPageTracking.eventViewProductImpression(getString(R.string.shop_info_title_tab_product),
-                        ((ShopProductLimitedProductViewModel)shopProductBaseViewModel).getShopProductViewModelList(),
+                        ((ShopProductLimitedProductViewModel) shopProductBaseViewModel).getShopProductViewModelList(),
                         true, shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()), ShopPageTracking.getShopType(shopInfo.getInfo()));
-            }else if(shopProductBaseViewModel instanceof ShopProductLimitedPromoViewModel){
+            } else if (shopProductBaseViewModel instanceof ShopProductLimitedPromoViewModel) {
                 shopPageTracking.eventViewBannerImpression(getString(R.string.shop_info_title_tab_product),
                         shopInfo.getInfo().getShopName(), shopInfo.getInfo().getShopId(), shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()),
                         ShopPageTracking.getShopType(shopInfo.getInfo()));
@@ -274,7 +263,6 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     @Override
     public void showGetListError(Throwable throwable) {
         super.showGetListError(throwable);
-        loadingStateView.setViewState(LoadingStateView.VIEW_CONTENT);
     }
 
     @Override
@@ -368,7 +356,7 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
     public void onFeatureWishlistClickedTracking(ShopProductViewModel shopProductViewModel) {
         shopPageTracking.eventClickWishlistShopPageFeatured(getString(R.string.shop_info_title_tab_product),
                 shopProductViewModel.isWishList(), shopProductViewModel.getId(), shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()),
-                        ShopPageTracking.getShopType(shopInfo.getInfo()));
+                ShopPageTracking.getShopType(shopInfo.getInfo()));
     }
 
     @Override
@@ -380,7 +368,7 @@ public class ShopProductListLimitedFragment extends BaseSearchListFragment<ShopP
 
     @Override
     public void onProductTitleFeaturedClickedTracking(ShopProductViewModel shopProductViewModel, int adapterPosition) {
-        shopPageTracking.eventClickProductTitleFeaturedImpression(getString(R.string.shop_info_title_tab_product),shopProductViewModel.getName(),
+        shopPageTracking.eventClickProductTitleFeaturedImpression(getString(R.string.shop_info_title_tab_product), shopProductViewModel.getName(),
                 shopProductViewModel.getId(), shopProductViewModel.getOriginalPrice(), adapterPosition,
                 shopProductListLimitedPresenter.isMyShop(shopInfo.getInfo().getShopId()), ShopPageTracking.getShopType(shopInfo.getInfo()));
     }
