a43734830ff8cd0cd3568416530244e8f6978d20
nathaniel <nathaniel.khuana@tokopedia.com>
2018-03-16 13:04:57 +0700

    Simplify get attribute product list

--- tkpdshop/src/main/java/com/tokopedia/shop/product/domain/interactor/GetShopProductListWithAttributeUseCase.java
+++ tkpdshop/src/main/java/com/tokopedia/shop/product/domain/interactor/GetShopProductListWithAttributeUseCase.java
@@ -3,36 +3,28 @@ package com.tokopedia.shop.product.domain.interactor;
 import com.tokopedia.abstraction.common.data.model.response.PagingList;
 import com.tokopedia.abstraction.common.data.model.session.UserSession;
 import com.tokopedia.shop.product.data.source.cloud.model.ShopProduct;
-import com.tokopedia.shop.product.data.source.cloud.model.ShopProductCampaign;
 import com.tokopedia.shop.product.domain.model.ShopProductRequestModel;
 import com.tokopedia.shop.product.view.mapper.ShopProductMapper;
 import com.tokopedia.shop.product.view.model.ShopProductViewModel;
 import com.tokopedia.usecase.RequestParams;
-import com.tokopedia.usecase.UseCase;
 import com.tokopedia.wishlist.common.domain.interactor.GetWishListUseCase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import rx.Observable;
 import rx.functions.Func1;
-import rx.functions.Func2;
 
 /**
  * Created by normansyahputa on 2/23/18.
  */
 
-public class GetShopProductListWithAttributeUseCase extends UseCase<PagingList<ShopProductViewModel>> {
+public class GetShopProductListWithAttributeUseCase extends GetShopProductAttributeUseCase<PagingList<ShopProductViewModel>> {
 
     private final static String SHOP_REQUEST = "SHOP_REQUEST";
 
     private final GetShopProductListUseCase getShopProductListUseCase;
-    private final GetWishListUseCase getWishListUseCase;
-    private final UserSession userSession;
-    private final ShopProductMapper shopProductMapper;
-    private final GetProductCampaignsUseCase getProductCampaignsUseCase;
 
     @Inject
     public GetShopProductListWithAttributeUseCase(GetShopProductListUseCase getShopProductListUseCase,
@@ -40,11 +32,8 @@ public class GetShopProductListWithAttributeUseCase extends UseCase<PagingList<S
                                                   GetProductCampaignsUseCase getProductCampaignsUseCase,
                                                   UserSession userSession,
                                                   ShopProductMapper shopProductMapper) {
+        super(getWishListUseCase, getProductCampaignsUseCase, userSession, shopProductMapper);
         this.getShopProductListUseCase = getShopProductListUseCase;
-        this.getWishListUseCase = getWishListUseCase;
-        this.getProductCampaignsUseCase = getProductCampaignsUseCase;
-        this.userSession = userSession;
-        this.shopProductMapper = shopProductMapper;
     }
 
     public static RequestParams createRequestParam(ShopProductRequestModel shopProductRequestModel) {
@@ -66,38 +55,18 @@ public class GetShopProductListWithAttributeUseCase extends UseCase<PagingList<S
 
     private Observable<PagingList<ShopProductViewModel>> getShopProductViewModelList(
             final ShopProductRequestModel shopProductRequestModel, final PagingList<ShopProduct> shopProductPagingList) {
-        List<String> defaultWishList = new ArrayList<>();
-        List<ShopProductCampaign> defaultShopProductCampaignList = new ArrayList<>();
-        Observable<List<String>> wishlistObservable = Observable.just(defaultWishList);
-        Observable<List<ShopProductCampaign>> campaignListObservable = Observable.just(defaultShopProductCampaignList);
-
-        final List<String> productIdList = new ArrayList<>();
-        for (ShopProduct shopProduct : shopProductPagingList.getList()) {
-            productIdList.add(shopProduct.getProductId());
-        }
-        if (!isShopOwner(shopProductRequestModel.getShopId())) {
-            wishlistObservable = getWishListUseCase.createObservable(GetWishListUseCase.createRequestParam(userSession.getUserId(), productIdList));
-        }
-        if (shopProductRequestModel.isOfficialStore()) {
-            campaignListObservable = getProductCampaignsUseCase.createObservable(GetProductCampaignsUseCase.createRequestParam(productIdList));
-        }
-
-        return Observable.zip(wishlistObservable, campaignListObservable, new Func2<List<String>, List<ShopProductCampaign>, PagingList<ShopProductViewModel>>() {
-            @Override
-            public PagingList<ShopProductViewModel> call(List<String> wishList, List<ShopProductCampaign> shopProductCampaignList) {
-                List<ShopProductViewModel> shopProductViewModelList = shopProductMapper.convertFromShopProduct(shopProductPagingList.getList());
-                PagingList<ShopProductViewModel> pagingList = new PagingList<>();
-                pagingList.setTotalData(shopProductPagingList.getTotalData());
-                pagingList.setPaging(shopProductPagingList.getPaging());
-                pagingList.setList(shopProductViewModelList);
-                shopProductMapper.mergeShopProductViewModelWithWishList(shopProductViewModelList, wishList, !isShopOwner(shopProductRequestModel.getShopId()));
-                shopProductMapper.mergeShopProductViewModelWithProductCampaigns(shopProductViewModelList, shopProductCampaignList);
-                return pagingList;
-            }
-        });
-    }
-
-    private boolean isShopOwner(String shopId) {
-        return userSession.getShopId().equals(shopId);
+        List<ShopProductViewModel> shopProductViewModelList = shopProductMapper.convertFromShopProduct(shopProductPagingList.getList());
+        return getShopProductViewModelList(isShopOwner(shopProductRequestModel.getShopId()),
+                shopProductRequestModel.isOfficialStore(), shopProductViewModelList)
+                .flatMap(new Func1<List<ShopProductViewModel>, Observable<PagingList<ShopProductViewModel>>>() {
+                    @Override
+                    public Observable<PagingList<ShopProductViewModel>> call(List<ShopProductViewModel> shopProductViewModelList) {
+                        PagingList<ShopProductViewModel> pagingList = new PagingList<>();
+                        pagingList.setTotalData(shopProductPagingList.getTotalData());
+                        pagingList.setPaging(shopProductPagingList.getPaging());
+                        pagingList.setList(shopProductViewModelList);
+                        return Observable.just(pagingList);
+                    }
+                });
     }
 }
