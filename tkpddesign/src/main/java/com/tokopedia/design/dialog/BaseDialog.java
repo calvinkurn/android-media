dd08b175e181dccb5b222b911da67c21916494f9
Meyta <meytajennist@gmail.com>
2018-03-16 16:18:53 +0700

    dialog refactor

---
+++ tkpddesign/src/main/java/com/tokopedia/design/dialog/BaseDialog.java
@@ -0,0 +1,52 @@
+package com.tokopedia.design.dialog;
+
+import android.app.Activity;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+
+/**
+ * Created by meyta on 2/14/18.
+ */
+
+public abstract class BaseDialog {
+
+    private Activity context;
+
+    private AlertDialog alertDialog;
+
+    public abstract int layoutResId();
+
+    public abstract void initView(View dialogView);
+
+    public abstract void initListener(AlertDialog dialog);
+
+    public BaseDialog(Activity context) {
+        this.context = context;
+        init();
+    }
+
+    private void init() {
+        View dialogView = context.getLayoutInflater().inflate(layoutResId(), null);
+        initView(dialogView);
+
+        alertDialog = new AlertDialog.Builder(context)
+                .setView(dialogView)
+                .create();
+
+        initListener(alertDialog);
+    }
+
+    public AlertDialog getAlertDialog() {
+        return alertDialog;
+    }
+
+    public void show() {
+        if (alertDialog != null)
+            alertDialog.show();
+    }
+
+    public void dismiss() {
+        if (alertDialog != null)
+            alertDialog.dismiss();
+    }
+}
--- tkpddesign/src/main/java/com/tokopedia/design/dialog/Dialog.java
+++ tkpddesign/src/main/java/com/tokopedia/design/dialog/Dialog.java
@@ -3,50 +3,95 @@ package com.tokopedia.design.dialog;
 import android.app.Activity;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tokopedia.design.R;
 
 /**
  * Created by meyta on 2/14/18.
+ *
+ * How to use?
+ * setTitle("Title Goes Here");
+ * setDesc("Make the copy compact to communicate what you want, approx 50 character like this");
+ * setBtnOk("CTA Here");
+ * setBtnCancel("CTA Here");
+ * setOnOkClickListener(View.OnClickListener());
+ * setOnCancelClickListener(View.OnClickListener());
  */
 
-public abstract class Dialog {
+public class Dialog extends BaseDialog {
 
-    private Activity context;
+    private TextView title, desc;
+    private Button btnCancel, btnOk;
 
-    private AlertDialog alertDialog;
+    private Type type = Type.RETORIC;
 
-    public abstract int layoutResId();
-
-    public abstract void initView(View dialogView);
-
-    public abstract void initListener(AlertDialog dialog);
-
-    public Dialog(Activity context) {
-        this.context = context;
-        init();
+    public enum Type {
+        RETORIC,
+        PROMINANCE,
+        LONG_PROMINANCE
     }
 
-    private void init() {
-        View dialogView = context.getLayoutInflater().inflate(layoutResId(), null);
-        initView(dialogView);
-
-        alertDialog = new AlertDialog.Builder(context)
-                .setView(dialogView)
-                .create();
-
-        initListener(alertDialog);
+    public Dialog(Activity context, Type type) {
+        super(context);
+        this.type = type;
     }
 
-    public AlertDialog getAlertDialog() {
-        return alertDialog;
+    @Override
+    public int layoutResId() {
+        if (type == Type.LONG_PROMINANCE)
+            return R.layout.dialog_longprominance;
+        return R.layout.dialog_base;
+
     }
 
-    public void show() {
-        if (alertDialog != null)
-            alertDialog.show();
+    @Override
+    public void initView(View dialogView) {
+        title = dialogView.findViewById(R.id.tv_title_dialog);
+        desc = dialogView.findViewById(R.id.tv_desc_dialog);
+        btnOk = dialogView.findViewById(R.id.btn_ok_dialog);
+        btnCancel = dialogView.findViewById(R.id.btn_cancel_dialog);
+
+        if (type == Type.RETORIC)
+            btnCancel.setVisibility(View.GONE);
     }
 
-    public void dismiss() {
-        if (alertDialog != null)
-            alertDialog.dismiss();
+    @Override
+    public void initListener(AlertDialog dialog) {
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+    }
+
+    public void setOnOkClickListener(View.OnClickListener onOkClickListener) {
+        btnOk.setOnClickListener(onOkClickListener);
+    }
+
+    public void setOnCancelClickListener(View.OnClickListener onCancelClickListener) {
+        btnCancel.setOnClickListener((type == Type.RETORIC) ? null : onCancelClickListener);
+    }
+
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public void setDesc(String desc) {
+        this.desc.setText(desc);
+    }
+
+    public void setBtnOk(String title) {
+        this.btnOk.setText(title);
+    }
+
+    public void setBtnCancel(String title) {
+        this.btnCancel.setText(title);
+    }
+
+    public Button getBtnCancel() {
+        return btnCancel;
+    }
+
+    public Button getBtnOk() {
+        return btnOk;
     }
 }
