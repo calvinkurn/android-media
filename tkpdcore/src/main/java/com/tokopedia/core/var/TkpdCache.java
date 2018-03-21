e9d0216564a3fb8b2fe66c8603150a2c2251e141
eriksuprayogi <eriksuprayogi@gmail.com>
2018-03-21 14:19:31 +0700

    firebase config show/hide btn
    (cherry picked from commit 6704a1d)

--- tkpdcore/src/main/java/com/tokopedia/core/var/TkpdCache.java
+++ tkpdcore/src/main/java/com/tokopedia/core/var/TkpdCache.java
@@ -246,6 +246,7 @@ public class TkpdCache extends com.tokopedia.abstraction.constant.TkpdCache {
         public static final String APP_REFERRAL_TITLE = "app_referral_title";
         public static final String APP_REFERRAL_HOWITWORKS = "app_referral_howitworks";
         public static final String APP_SHOW_REFERRAL_BUTTON = "app_show_referral_button";
+        public static final String APP_SHOW_RECOMENDATION_BUTTON = "mainapp_show_recomendation_button";
         public static final String APP_SHARE_DEFAULT_MESSAGE = "app_share_default_message";
 
 
