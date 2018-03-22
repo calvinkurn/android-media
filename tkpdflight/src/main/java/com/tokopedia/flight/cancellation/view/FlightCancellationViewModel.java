b73d621c2d93de9676b58cbfe78d9c56f9fce589
Muhammad Furqan <nanjihusaha@gmail.com>
2018-03-22 10:49:25 +0700

    Flight Cancellation

--- tkpdflight/src/main/java/com/tokopedia/flight/cancellation/view/FlightCancellationViewModel.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/cancellation/view/FlightCancellationViewModel.java
@@ -1,17 +1,70 @@
 package com.tokopedia.flight.cancellation.view;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.tokopedia.abstraction.base.view.adapter.Visitable;
+import com.tokopedia.flight.booking.view.viewmodel.FlightBookingPassengerViewModel;
 import com.tokopedia.flight.cancellation.view.adapter.FlightCancellationTypeFactory;
+
+import java.util.List;
 
 /**
  * @author by furqan on 21/03/18.
  */
 
-public class FlightCancellationViewModel implements Visitable<FlightCancellationTypeFactory> {
+public class FlightCancellationViewModel implements Parcelable,
+        Visitable<FlightCancellationTypeFactory> {
+
+    private long journeyId;
+    private List<FlightBookingPassengerViewModel> passengerViewModelList;
+
+    protected FlightCancellationViewModel(Parcel in) {
+        journeyId = in.readLong();
+        passengerViewModelList = in.createTypedArrayList(FlightBookingPassengerViewModel.CREATOR);
+    }
+
+    public static final Creator<FlightCancellationViewModel> CREATOR = new Creator<FlightCancellationViewModel>() {
+        @Override
+        public FlightCancellationViewModel createFromParcel(Parcel in) {
+            return new FlightCancellationViewModel(in);
+        }
+
+        @Override
+        public FlightCancellationViewModel[] newArray(int size) {
+            return new FlightCancellationViewModel[size];
+        }
+    };
 
     @Override
     public int type(FlightCancellationTypeFactory typeFactory) {
         return typeFactory.type(this);
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(journeyId);
+        dest.writeTypedList(passengerViewModelList);
+    }
+
+    public long getJourneyId() {
+        return journeyId;
+    }
+
+    public void setJourneyId(long journeyId) {
+        this.journeyId = journeyId;
+    }
+
+    public List<FlightBookingPassengerViewModel> getPassengerViewModelList() {
+        return passengerViewModelList;
+    }
+
+    public void setPassengerViewModelList(List<FlightBookingPassengerViewModel> passengerViewModelList) {
+        this.passengerViewModelList = passengerViewModelList;
+    }
 }
--- tkpdflight/src/main/java/com/tokopedia/flight/cancellation/view/adapter/viewholder/FlightCancellationViewHolder.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/cancellation/view/adapter/viewholder/FlightCancellationViewHolder.java
@@ -1,7 +1,9 @@
 package com.tokopedia.flight.cancellation.view.adapter.viewholder;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.view.View;
+import android.widget.TextView;
 
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.flight.R;
@@ -16,8 +18,21 @@ public class FlightCancellationViewHolder extends AbstractViewHolder<FlightCance
     @LayoutRes
     public static int LAYOUT = R.layout.item_flight_cancellation;
 
+    private Context context;
+    private TextView txtDepartureDetail;
+    private TextView txtJourneyDetail;
+    private TextView txtAirlineName;
+    private TextView txtDuration;
+
     public FlightCancellationViewHolder(View itemView) {
         super(itemView);
+
+        txtDepartureDetail = itemView.findViewById(R.id.tv_departure_time_label);
+        txtJourneyDetail = itemView.findViewById(R.id.tv_journey_detail_label);
+        txtAirlineName = itemView.findViewById(R.id.airline_name);
+        txtDuration = itemView.findViewById(R.id.duration);
+        context = itemView.getContext();
+
     }
 
     @Override
