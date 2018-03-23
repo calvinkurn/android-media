cb48f9db55e265120dec4c69f51d5e93d94fcb83
noiz354 <noiz354@gmail.com>
2018-03-23 14:31:22 +0700

    1. add automatortest (for third party apps) part 2

--- customerapp/src/androidTest/java/com/tokopedia/tkpd/AutomatorTest.java
+++ customerapp/src/androidTest/java/com/tokopedia/tkpd/AutomatorTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 
 // UiAutomator library test
 /**
