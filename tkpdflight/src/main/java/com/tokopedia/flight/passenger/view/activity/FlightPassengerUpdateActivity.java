f8696d6304a039d6ef7e43105ba5982b403a182d
Muhammad Furqan <nanjihusaha@gmail.com>
2018-03-26 12:15:57 +0700

    abort update passenger? show confirmation dialog

--- tkpdflight/src/main/java/com/tokopedia/flight/passenger/view/activity/FlightPassengerUpdateActivity.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/passenger/view/activity/FlightPassengerUpdateActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
 import com.tokopedia.abstraction.common.di.component.HasComponent;
+import com.tokopedia.design.component.Dialog;
 import com.tokopedia.flight.FlightComponentInstance;
+import com.tokopedia.flight.R;
 import com.tokopedia.flight.booking.view.viewmodel.FlightBookingPassengerViewModel;
 import com.tokopedia.flight.passenger.di.DaggerFlightPassengerComponent;
 import com.tokopedia.flight.passenger.di.FlightPassengerComponent;
@@ -51,5 +54,32 @@ public class FlightPassengerUpdateActivity extends BaseSimpleActivity
         return DaggerFlightPassengerComponent.builder()
                 .flightComponent(FlightComponentInstance.getFlightComponent(getApplication()))
                 .build();
+    }
+
+    @Override
+    public void onBackPressed() {
+        showCancelUpdateDialog();
+    }
+
+    private void showCancelUpdateDialog() {
+        final Dialog dialog = new Dialog(this, Dialog.Type.PROMINANCE);
+        dialog.setTitle(getString(R.string.flight_passenger_update_abort_dialog_title));
+        dialog.setDesc(getString(R.string.flight_passenger_update_abort_dialog_description));
+        dialog.setBtnOk("Lanjut ubah");
+        dialog.setOnCancelClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+        dialog.setBtnCancel("Ya, batalkan");
+        dialog.setOnOkClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
