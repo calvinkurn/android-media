e25157a830e702c02005456f79b2a3a7687ada2b
alvarisi <alvarisi01@gmail.com>
2018-03-29 11:39:01 +0700

    flighct cancellation attachment contract

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/cancellation/view/contract/FlightCancellationReasonAndProofContract.java
@@ -0,0 +1,52 @@
+package com.tokopedia.flight.cancellation.view.contract;
+
+import android.app.Activity;
+
+import com.tokopedia.abstraction.base.view.listener.CustomerView;
+import com.tokopedia.abstraction.base.view.presenter.CustomerPresenter;
+import com.tokopedia.flight.cancellation.view.viewmodel.FlightCancellationAttachmentViewModel;
+import com.tokopedia.flight.cancellation.view.viewmodel.FlightCancellationViewModel;
+
+import java.util.List;
+
+/**
+ * Created by alvarisi on 3/26/18.
+ */
+
+public interface FlightCancellationReasonAndProofContract {
+    interface View extends CustomerView{
+
+        void showUploadAttachmentView();
+
+        void hideUploadAttachmentView();
+
+        void addAttachment(FlightCancellationAttachmentViewModel viewModel);
+
+        List<FlightCancellationAttachmentViewModel> getAttachments();
+
+        void showRequiredMinimalOneAttachmentErrorMessage(int resId);
+
+        FlightCancellationViewModel getCancellationViewModel();
+
+        String getString(int resId);
+
+        void showAttachmentGreaterThanPassengersTotalAndRequiredAttachmentErrorMessage(String errorMessage);
+
+        String getReason();
+
+        Activity getActivity();
+
+        void showFailedToNextStepErrorMessage(String errorMessage);
+
+        void navigateToNextStep(FlightCancellationViewModel viewModel);
+    }
+
+    interface Presenter extends CustomerPresenter<View>{
+
+        void initialize();
+
+        void onSuccessGetImage(String filepath);
+
+        void onNextButtonClicked();
+    }
+}
