275d7c05351a41115a9d1c5ccbbde7b7b5f22291
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-04-04 16:28:00 +0700

    add applink for gamification

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/cracktoken/activity/CrackTokenActivity.java
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/cracktoken/activity/CrackTokenActivity.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
+import com.tokopedia.gamification.ApplinkConstant;
 import com.tokopedia.gamification.R;
 import com.tokopedia.gamification.cracktoken.fragment.CrackEmptyTokenFragment;
 import com.tokopedia.gamification.cracktoken.fragment.CrackTokenFragment;
 
 public class CrackTokenActivity extends BaseSimpleActivity implements CrackTokenFragment.ActionListener {
+
+    @SuppressWarnings("unused")
+    @DeepLink(ApplinkConstant.GAMIFICATION)
+    public static Intent getcallingIntent(Context context, Bundle extras) {
+        return CrackTokenActivity.newInstance(context);
+    }
 
     public static Intent newInstance(Context context) {
         return new Intent(context, CrackTokenActivity.class);
