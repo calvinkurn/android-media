8b53b63aa4f9c9f4ae05c41f4993353c796a4447
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-04-05 11:03:16 +0700

    add TkpdUserId for header Gamification API

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/network/GamificationAuthInterceptor.java
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/network/GamificationAuthInterceptor.java
@@ -1,13 +1,19 @@
 package com.tokopedia.gamification.network;
 
 import android.content.Context;
+import android.support.v4.util.ArrayMap;
+import android.text.TextUtils;
 
 import com.tokopedia.abstraction.AbstractionRouter;
 import com.tokopedia.abstraction.common.data.model.session.UserSession;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
 import com.tokopedia.abstraction.common.network.interceptor.TkpdAuthInterceptor;
 
+import java.util.Map;
+
 import javax.inject.Inject;
+
+import okhttp3.Request;
 
 /**
  * Created by nabillasabbaha on 3/29/18.
@@ -15,10 +21,23 @@ import javax.inject.Inject;
 
 public class GamificationAuthInterceptor extends TkpdAuthInterceptor {
 
+    public static final String PARAM_TKPD_USER_ID = "Tkpd-UserId";
     @Inject
     public GamificationAuthInterceptor(@ApplicationContext Context context,
                                        AbstractionRouter abstractionRouter,
                                        UserSession userSession) {
         super(context, abstractionRouter, userSession);
+    }
+
+    protected Map<String, String> getHeaderMap(String path, String strParam,
+                                               String method, String authKey,
+                                               String contentTypeHeader) {
+        Map<String, String> headerMap = super.getHeaderMap(path, strParam, method, authKey, contentTypeHeader);
+        String userId = userSession.getUserId();
+        headerMap.remove(PARAM_TKPD_USER_ID);
+        if (!TextUtils.isEmpty(userId)) {
+            headerMap.put(PARAM_TKPD_USER_ID, userId);
+        }
+        return headerMap;
     }
 }
