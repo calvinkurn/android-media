a59c5aea2c46abbcfceaab0df61e9747a3fd408f
hangnadi <hangnadi@tokopedia.com>
2018-04-11 17:28:50 +0700

    search image into single usecase or single observable stream

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/base/BaseDiscoveryActivity.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/base/BaseDiscoveryActivity.java
@@ -9,13 +9,10 @@ import com.tokopedia.core.base.di.component.HasComponent;
 import com.tokopedia.core.home.BrandsWebViewActivity;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
 import com.tokopedia.core.router.discovery.DetailProductRouter;
-import com.tokopedia.discovery.imagesearch.domain.model.ImageSearchItemResponse;
 import com.tokopedia.discovery.intermediary.view.IntermediaryActivity;
 import com.tokopedia.discovery.newdiscovery.hotlist.view.activity.HotlistActivity;
 import com.tokopedia.discovery.newdiscovery.search.SearchActivity;
 import com.tokopedia.discovery.newdiscovery.search.fragment.product.viewmodel.ProductViewModel;
-
-import java.util.List;
 
 /**
  * Created by hangnadi on 9/26/17.
@@ -106,10 +103,6 @@ public class BaseDiscoveryActivity
     public void onHandleResponseSearch(ProductViewModel productViewModel) {
         SearchActivity.moveTo(this, productViewModel, isForceSwipeToShop());
         finish();
-    }
-
-    @Override
-    public void onHandleImageSearchResponseSuccess(List<String> productIdsList, String productIds) {
     }
 
     @Override
