bd7b6e4d131a9e9ef9df1a0c779486cffda4a5d9
yfsx <yoas.fs@gmail.com>
2018-04-12 11:22:40 +0700

    add removed class

--- tkpdevents/src/test/java/com/tokopedia/events/EventEntityUnitTest.java
+++ tkpdevents/src/test/java/com/tokopedia/events/EventEntityUnitTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
-* EventResponseEntity local unit test, which will execute on the development machine (host).
-*
-* @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
-*/
+ * EventResponseEntity local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
 public class EventEntityUnitTest {
-@Test
-public void addition_isCorrect() throws Exception {
+    @Test
+    public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
