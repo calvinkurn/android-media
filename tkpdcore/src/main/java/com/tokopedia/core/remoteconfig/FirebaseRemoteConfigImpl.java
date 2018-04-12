04adcb0ce428b9e0e9070e5d32544033bc4a6b10
Muhammad Furqan <nanjihusaha@gmail.com>
2018-04-12 18:26:54 +0700

    permintaan kk Varis

--- tkpdcore/src/main/java/com/tokopedia/core/remoteconfig/FirebaseRemoteConfigImpl.java
+++ tkpdcore/src/main/java/com/tokopedia/core/remoteconfig/FirebaseRemoteConfigImpl.java
@@ -20,7 +20,8 @@ import java.util.concurrent.TimeUnit;
 
 public class FirebaseRemoteConfigImpl implements RemoteConfig {
     private static final long THREE_HOURS = TimeUnit.HOURS.toSeconds(3);
-    private static final long CONFIG_CACHE_EXPIRATION = THREE_HOURS;
+    private static final long TWO_MINUTES = TimeUnit.MINUTES.toSeconds(2);
+    private static final long CONFIG_CACHE_EXPIRATION = TWO_MINUTES;
 
     private FirebaseRemoteConfig firebaseRemoteConfig;
     private Context context;
