bf57df84642d9899e2fa3e0be6dfe941d7df8743
alifanuraniputri <alifanuraniputri@gmail.com>
2018-04-12 14:09:25 +0700

    remove variant onboarding temporary

--- tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/fragment/ProductDetailFragment.java
+++ tkpdpdp/src/main/java/com/tokopedia/tkpdpdp/fragment/ProductDetailFragment.java
@@ -74,11 +74,6 @@ import com.tokopedia.core.util.MethodChecker;
 import com.tokopedia.core.util.RequestPermissionUtil;
 import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.webview.listener.DeepLinkWebViewHandleListener;
-import com.tokopedia.showcase.ShowCaseBuilder;
-import com.tokopedia.showcase.ShowCaseContentPosition;
-import com.tokopedia.showcase.ShowCaseDialog;
-import com.tokopedia.showcase.ShowCaseObject;
-import com.tokopedia.showcase.ShowCasePreference;
 import com.tokopedia.tkpdpdp.CourierActivity;
 import com.tokopedia.tkpdpdp.DescriptionActivity;
 import com.tokopedia.tkpdpdp.DinkFailedActivity;
@@ -146,8 +141,6 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
     private static final int FROM_COLLAPSED = 0;
     private static final int FROM_EXPANDED = 1;
     private static final int SCROLL_ELEVATION = 324;
-    private static final int SHOWCASE_MARGIN = 10;
-    private static final int SHOWCASE_HEIGHT = 100;
 
     public static final int REQUEST_CODE_SHOP_INFO = 998;
     public static final int REQUEST_CODE_TALK_PRODUCT = 1;
@@ -172,7 +165,6 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
     public static final String STATE_VIDEO = "STATE_VIDEO";
     public static final String STATE_PROMO_WIDGET = "STATE_PROMO_WIDGET";
     public static final String STATE_APP_BAR_COLLAPSED = "STATE_APP_BAR_COLLAPSED";
-    public static final String TAG_SHOWCASE_VARIANT = "-SHOWCASE_VARIANT";
     private static final String STATIC_VALUE_ENHANCE_NONE_OTHER = "none / other";
 
     private CoordinatorLayout coordinatorLayout;
@@ -223,7 +215,6 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
     private boolean onClickBuyWhileRequestingVariant = false;
 
     private RemoteConfig remoteConfig;
-    private ShowCaseDialog showCaseDialog;
 
     public static ProductDetailFragment newInstance(@NonNull ProductPass productPass) {
         ProductDetailFragment fragment = new ProductDetailFragment();
@@ -1298,8 +1289,6 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
         buttonBuyView.updateButtonForVariantProduct(productVariant.getChildFromProductId(
                 defaultChild).isIsBuyable(), productData);
 
-        startShowCase();
-
     }
 
     @Override
@@ -1484,48 +1473,6 @@ public class ProductDetailFragment extends BasePresenterFragment<ProductDetailPr
     @Override
     public boolean isSellerApp() {
         return GlobalConfig.isSellerApp();
-    }
-
-    private void startShowCase() {
-        final String showCaseTag = ProductInfoActivity.class.getName() + TAG_SHOWCASE_VARIANT;
-        if (ShowCasePreference.hasShown(getActivity(), showCaseTag) || showCaseDialog != null) {
-            return;
-        }
-        showCaseDialog = createShowCase();
-        showCaseDialog.setShowCaseStepListener(new ShowCaseDialog.OnShowCaseStepListener() {
-            @Override
-            public boolean onShowCaseGoTo(int previousStep, int nextStep, ShowCaseObject showCaseObject) {
-                return false;
-            }
-        });
-
-        Rect rectToShowCase = new Rect();
-        priceSimulationView.getGlobalVisibleRect(rectToShowCase);
-
-        ArrayList<ShowCaseObject> showCaseObjectList = new ArrayList<>();
-        showCaseObjectList.add(new ShowCaseObject(
-                priceSimulationView,
-                getResources().getString(R.string.product_variant),
-                getResources().getString(R.string.product_variant_onboarding),
-                ShowCaseContentPosition.TOP).withCustomTarget(new int[]{rectToShowCase.left + SHOWCASE_MARGIN,
-                rectToShowCase.top - 50, rectToShowCase.right - SHOWCASE_MARGIN, rectToShowCase.top + SHOWCASE_HEIGHT}));
-        showCaseDialog.show(getActivity(), showCaseTag, showCaseObjectList);
-    }
-
-    private ShowCaseDialog createShowCase() {
-        return new ShowCaseBuilder()
-                .customView(R.layout.view_onboarding_variant)
-                .titleTextColorRes(R.color.white)
-                .spacingRes(R.dimen.spacing_show_case)
-                .textColorRes(R.color.grey_400)
-                .shadowColorRes(R.color.shadow)
-                .backgroundContentColorRes(R.color.black)
-                .textSizeRes(R.dimen.fontvs)
-                .finishStringRes(R.string.title_understand)
-                .useCircleIndicator(true)
-                .clickable(true)
-                .useArrow(true)
-                .build();
     }
 
     @Override
