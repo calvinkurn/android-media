dea348bf2c09213f2d8f194fcb66b442632f7080
andtomat <andtomat@gmail.com>
2018-04-24 17:45:32 +0700

    remove commend

--- posapp/src/main/java/com/tokopedia/posapp/outlet/view/activity/OutletActivity.java
+++ posapp/src/main/java/com/tokopedia/posapp/outlet/view/activity/OutletActivity.java
@@ -50,11 +50,6 @@ public class OutletActivity extends DrawerPresenterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-//        sessionHandler = new SessionHandler(this);
-//        drawerCache = new LocalCacheHandler(this, DrawerHelper.DRAWER_CACHE);
-//        drawerHelper = DrawerInjector.getDrawerHelper(this, sessionHandler, drawerCache);
-//        drawerHelper.initDrawer(this);
-//        drawerHelper.setEnabled(true);
     }
 
     @Override
