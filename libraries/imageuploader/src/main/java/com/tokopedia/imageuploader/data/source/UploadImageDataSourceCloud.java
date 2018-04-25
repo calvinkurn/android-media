da53f4c1ae9826e69eb736ea0f7a59bf181db859
alvarisi <alvarisi01@gmail.com>
2018-04-25 13:50:43 +0700

    Image Uploader : Data Layer

---
+++ libraries/imageuploader/src/main/java/com/tokopedia/imageuploader/data/source/UploadImageDataSourceCloud.java
@@ -0,0 +1,41 @@
+package com.tokopedia.imageuploader.data.source;
+
+import com.tokopedia.imageuploader.data.source.api.ImageUploadApi;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import rx.Observable;
+import rx.functions.Func1;
+
+/**
+ * Created by zulfikarrahman on 12/19/17.
+ */
+
+public class UploadImageDataSourceCloud {
+
+    private Retrofit.Builder retrofitBuilder;
+    private OkHttpClient okHttpClient;
+
+    public UploadImageDataSourceCloud(Retrofit.Builder retrofitBuilder, OkHttpClient okHttpClient) {
+        this.retrofitBuilder = retrofitBuilder;
+        this.okHttpClient = okHttpClient;
+    }
+
+    public Observable<String> uploadImage(Map<String, RequestBody> params, String urlUploadImage) {
+        Retrofit retrofit = retrofitBuilder.client(okHttpClient).build();
+        return retrofit.create(ImageUploadApi.class)
+                .uploadImage(urlUploadImage, params)
+                .flatMap(new Func1<Response<String>, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(Response<String> stringResponse) {
+                        return Observable.just(stringResponse.body());
+                    }
+                });
+    }
+}
