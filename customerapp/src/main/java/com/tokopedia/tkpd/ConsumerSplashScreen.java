229d258893dc629cefb0ef52f0c1d793510b7714
vishalgupta1987 <vishalgupta1987@gmail.com>
2018-04-26 19:32:48 +0700

    remove logs and added a exceptional handler

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerSplashScreen.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerSplashScreen.java
@@ -69,9 +69,6 @@ public class ConsumerSplashScreen extends SplashScreen {
         RemoteConfig remoteConfig = new FirebaseRemoteConfigImpl(this);
         String imageUrl = remoteConfig.getString(KEY_SPLASH_IMAGE_URL);
 
-        CommonUtils.dumper("Vishal, Splash Image :: " + imageUrl);
-
-        //imageUrl = "https://lh3.googleusercontent.com/_EqErMcqOR4UiS84FzuXd31AX8cmFEA93LhM0KxeiS_FRC8GoTepMpOADrKzfba9Br-UbzcmgYXCL9R-zQaBVHW4UiiUOAH4fsAuSX-qlx4GIrHD0_z8Ji5Ft7Z7vMHcQ5hwZ7OQah7QZALawCfRjYBzewhU6hqGFM_zb5MrrS5nCY9eodZd74AiaT0XO0-17ndoXx-9D8Va2ockac1ggfG4TgCn8ui7wP1FHVlOuy2QsyZvn5g2p01YB9bObLz6Ah7Q_RJGG-yOgGjnZP87EQF_mMgaP2tW8LVmxjaQ7dKoQVw75Qk0kwSjB6nvuQ9B0iS-yQ6qXHxYm06cwXTsrRfa-e5mBSdpoQxy7sqmom36VT9O-IiGiY9_ezxnhFqzhoncWXKNNhbpnb6bqCBtxAc3Z462eCVMoK-Nwqa6Rq459kTcGiOYdH6p2giiF75eWQDV7a0qDY3aAQQAAwmoS7kqXI-AXhC1kqxfwOnSkitE1T3pYfolYngt2W1BPivIPId0nkxjxCZWNosfQ9Lzn3MRpcAPlCs_0m5-U_Gi13BgZEnrRaH3UQQNYQMALhhydd3p5E7buuVpiHCv2gtXaOYUA7hp3xujNDDWmvwu=w452-h803-no";
         if (TextUtils.isEmpty(imageUrl)) {
             return;
         }
@@ -92,18 +89,17 @@ public class ConsumerSplashScreen extends SplashScreen {
 
                     @Override
                     public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-                        CommonUtils.dumper("Vishal onResourceReady :: ");
-                        if (resource != null) {
-                            CommonUtils.dumper("Vishal onResourceReady 2:: ");
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                mainLayout.setBackground(new BitmapDrawable(getResources(), resource));
+                        try {
+                            if (resource != null) {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                                    mainLayout.setBackground(new BitmapDrawable(getResources(), resource));
+                                }
                             }
-                        }
+                        }catch (Exception ex){}
                     }
 
                     @Override
                     public void onLoadStarted(Drawable placeholder) {
-                        CommonUtils.dumper("Vishal onLoadStarted :: ");
                         super.onLoadStarted(placeholder);
                     }
                 });
