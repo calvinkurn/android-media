a3051bea2300c436e886126db3a03dcb5c92582a
Muhammad Furqan <nanjihusaha@gmail.com>
2018-04-27 13:53:42 +0700

    provide Global Cache Manager for Flight

--- tkpdflight/src/main/java/com/tokopedia/flight/common/di/module/FlightModule.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/common/di/module/FlightModule.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tokopedia.abstraction.AbstractionRouter;
 import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker;
+import com.tokopedia.abstraction.common.data.model.storage.CacheManager;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
 import com.tokopedia.abstraction.common.di.scope.ApplicationScope;
 import com.tokopedia.abstraction.common.network.OkHttpRetryPolicy;
@@ -73,6 +74,15 @@ public class FlightModule {
     public FlightModuleRouter provideFlightModuleRouter(@ApplicationContext Context context){
         if (context instanceof FlightModuleRouter) {
             return ((FlightModuleRouter) context);
+        }
+        throw new RuntimeException("App should implement " + FlightModuleRouter.class.getSimpleName());
+    }
+
+    @FlightScope
+    @Provides
+    public CacheManager provideCacheManager(@ApplicationContext Context context) {
+        if (context instanceof AbstractionRouter) {
+            return ((AbstractionRouter) context).getGlobalCacheManager();
         }
         throw new RuntimeException("App should implement " + FlightModuleRouter.class.getSimpleName());
     }
