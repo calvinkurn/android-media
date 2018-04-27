bce70d3edf41c1d6f2d4b0cdfe7d666bd3179271
Muhammad Furqan <nanjihusaha@gmail.com>
2018-04-27 16:43:57 +0700

    create flight detail cache source

---
+++ tkpdflight/src/main/java/com/tokopedia/flight/detail/data/cache/FlightDetailCacheSource.java
@@ -0,0 +1,51 @@
+package com.tokopedia.flight.detail.data.cache;
+
+import com.google.gson.reflect.TypeToken;
+import com.tokopedia.abstraction.common.data.model.storage.CacheManager;
+import com.tokopedia.abstraction.common.utils.network.CacheUtil;
+import com.tokopedia.flight.orderlist.domain.model.FlightOrder;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * @author by furqan on 27/04/18.
+ */
+
+public class FlightDetailCacheSource {
+
+    private static long FLIGHT_DETAIL_CACHE_TIMEOUT = TimeUnit.MINUTES.toSeconds(10);
+    private static String FLIGHT_DETAIL_CACHE_KEY = "FLIGHT_DETAIL_CACHE";
+
+    private CacheManager cacheManager;
+
+    @Inject
+    public FlightDetailCacheSource(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    public Observable<Boolean> saveCache(FlightOrder flightOrder) {
+        cacheManager.delete(FLIGHT_DETAIL_CACHE_KEY);
+
+        Type type = new TypeToken<FlightOrder>(){}.getType();
+
+        cacheManager.save(FLIGHT_DETAIL_CACHE_KEY,
+                CacheUtil.convertModelToString(flightOrder, type),
+                FLIGHT_DETAIL_CACHE_TIMEOUT);
+
+        return Observable.just(true);
+    }
+
+    public Observable<Boolean> isExpired() {
+        return Observable.just(cacheManager.isExpired(FLIGHT_DETAIL_CACHE_KEY));
+    }
+
+    public Observable<Boolean> deleteCache() {
+        cacheManager.delete(FLIGHT_DETAIL_CACHE_KEY);
+        return Observable.just(true);
+    }
+}
