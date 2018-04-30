b09facc0e1886f977765ab51a07f847d71f1008d
Lavekush Agrawal <lavekush@Lavekushs-MacBook-Air.local>
2018-04-30 14:40:58 +0530

    Removed author name from java class

--- tkpdtokocash/src/main/java/com/tokopedia/tokocash/network/exception/AutoSweepException.java
+++ tkpdtokocash/src/main/java/com/tokopedia/tokocash/network/exception/AutoSweepException.java
@@ -2,10 +2,6 @@ package com.tokopedia.tokocash.network.exception;
 
 import java.io.IOException;
 
-/**
- * Created by nabillasabbaha on 1/19/18.
- */
-
 public class AutoSweepException extends IOException {
 
     public AutoSweepException(String message) {
