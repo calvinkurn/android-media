d1ce0a62b8d20a34791efae4008b69115d7d72f2
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-05-03 11:15:38 +0700

    Move default id to kolpostviewmoel :construction:

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/adapter/viewholder/kol/KolRecommendationAdapter.java
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/adapter/viewholder/kol/KolRecommendationAdapter.java
@@ -18,6 +18,7 @@ import com.tokopedia.feedplus.view.listener.FeedPlus;
 import com.tokopedia.feedplus.view.viewmodel.kol.KolRecommendItemViewModel;
 import com.tokopedia.feedplus.view.viewmodel.kol.KolRecommendationViewModel;
 import com.tokopedia.feedplus.view.viewmodel.kol.KolViewModel;
+import com.tokopedia.kol.feature.post.view.viewmodel.KolPostViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -123,7 +124,7 @@ public class KolRecommendationAdapter extends RecyclerView.Adapter<KolRecommenda
         kolViewListener.onGoToKolProfile(data.getPage(),
                 data.getRowNumber(),
                 String.valueOf(kolItem.getId()),
-                KolViewModel.DEFAULT_ID);
+                KolPostViewModel.DEFAULT_ID);
     }
 
     @Override
