5f438939f1e27b63bbe429a28e23b8f7c08069a3
andtomat <andtomat@gmail.com>
2018-05-03 15:04:08 +0700

    deeplink add keyword

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/constant/ShopParamConstant.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/constant/ShopParamConstant.java
@@ -19,4 +19,6 @@ public class ShopParamConstant {
 
     public static final String KEY_SHOP_ID = "shop_id";
     public static final String KEY_ETALASE_ID = "etalase_id";
+    public static final String KEY_KEYWORD = "keyword";
+    public static final String KEY_SORT = "sort";
 }
--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/activity/ShopProductListActivity.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/activity/ShopProductListActivity.java
@@ -62,6 +62,17 @@ public class ShopProductListActivity extends BaseSimpleActivity implements HasCo
                 .putExtra(ShopParamConstant.EXTRA_ETALASE_ID, extras.getString(ShopParamConstant.KEY_ETALASE_ID));
     }
 
+    @DeepLink(ShopAppLink.SHOP_ETALASE_WITH_KEYWORD_AND_SORT)
+    public static Intent getCallingIntentEtalaseSelectedWithKeywordAndSort(Context context, Bundle extras) {
+        Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
+        return new Intent(context, ShopProductListActivity.class)
+                .setData(uri.build())
+                .putExtra(ShopParamConstant.EXTRA_SHOP_ID, extras.getString(ShopParamConstant.KEY_SHOP_ID))
+                .putExtra(ShopParamConstant.EXTRA_ETALASE_ID, extras.getString(ShopParamConstant.KEY_ETALASE_ID))
+                .putExtra(ShopParamConstant.EXTRA_PRODUCT_KEYWORD, extras.getString(ShopParamConstant.KEY_KEYWORD))
+                .putExtra(ShopParamConstant.EXTRA_SORT_ID, extras.getString(ShopParamConstant.KEY_SORT));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         shopId = getIntent().getStringExtra(ShopParamConstant.EXTRA_SHOP_ID);
