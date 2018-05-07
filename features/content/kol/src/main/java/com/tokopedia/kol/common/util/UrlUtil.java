864c6fb7caeeec04c893ad48913b294fa6a42889
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-05-07 16:18:23 +0700

    Add build version for add link :sparkles:

--- features/content/kol/src/main/java/com/tokopedia/kol/common/util/UrlUtil.java
+++ features/content/kol/src/main/java/com/tokopedia/kol/common/util/UrlUtil.java
@@ -1,5 +1,6 @@
 package com.tokopedia.kol.common.util;
 
+import android.os.Build;
 import android.text.util.Linkify;
 import android.widget.TextView;
 
@@ -11,8 +12,13 @@ import java.util.regex.Pattern;
 
 public class UrlUtil {
     private static final Pattern pattern = Pattern.compile("((https?|ftp|file)://)?(www.)?(tokopedia.com|tkp.me)([-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])?");
+    private static final String[] SCHEMES = {"http://", "http://"};
 
     public static void convertToClickableUrl(TextView textView) {
-        Linkify.addLinks(textView, pattern, "");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Linkify.addLinks(textView, pattern, SCHEMES[0], SCHEMES, null, null);
+        } else {
+            Linkify.addLinks(textView, pattern, SCHEMES[0]);
+        }
     }
 }
