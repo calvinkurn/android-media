5b6bf6243c48e69010319b3ab9d90349f0edc5b6
nisie <nathania.sutedja@tokopedia.com>
2018-05-08 15:27:20 +0700

    Reset config

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
@@ -1771,12 +1771,12 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
 
     @Override
     public boolean isEnabledGroupChat() {
-        return true;
+        return remoteConfig.getBoolean(TkpdInboxRouter.ENABLE_GROUPCHAT);
     }
 
     @Override
     public boolean isEnabledGroupChatRoom() {
-        return true;
+        return remoteConfig.getBoolean(GroupChatModuleRouter.ENABLE_GROUPCHAT_ROOM);
     }
 
     @Override
