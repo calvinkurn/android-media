070702d48c9946315becf70a716fe40bc14e6a71
Henry Priyono <henry.priyono@tokopedia.com>
2018-05-14 17:57:04 +0700

    Fix savedCheckedState Logic

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdynamicfilter/helper/OptionHelper.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdynamicfilter/helper/OptionHelper.java
@@ -34,10 +34,10 @@ public class OptionHelper {
     private static void saveCheckboxOptionInputState(Option option,
                                                      HashMap<String, Boolean> checkedState) {
 
-        if (!TextUtils.isEmpty(option.getInputState())) {
-            checkedState.put(option.getUniqueId(), Boolean.parseBoolean(option.getInputState()));
+        if (!TextUtils.isEmpty(option.getInputState()) && Boolean.parseBoolean(option.getInputState())) {
+            checkedState.put(option.getUniqueId(), true);
         } else {
-            checkedState.put(option.getUniqueId(), false);
+            checkedState.remove(option.getUniqueId());
         }
     }
 
