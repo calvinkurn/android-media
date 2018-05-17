30a8818c1f6f3b20fd20a95029538c6621d5d4d5
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-05-17 14:25:19 +0700

    Update like kol post usecase :recycle:

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/post/domain/interactor/LikeKolPostUseCase.java
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/post/domain/interactor/LikeKolPostUseCase.java
@@ -1,7 +1,10 @@
 package com.tokopedia.kol.feature.post.domain.interactor;
 
+import com.tokopedia.kol.feature.post.data.source.LikeKolPostSourceCloud;
 import com.tokopedia.usecase.RequestParams;
 import com.tokopedia.usecase.UseCase;
+
+import javax.inject.Inject;
 
 import rx.Observable;
 
@@ -11,28 +14,28 @@ import rx.Observable;
 
 public class LikeKolPostUseCase extends UseCase<Boolean> {
 
-    public static final String PARAM_ID = "PARAM_ID";
-    public static final String PARAM_ACTION = "PARAM_ACTION";
-
     public static final int ACTION_LIKE = 1;
     public static final int ACTION_UNLIKE = 0;
 
-//    FeedRepository feedRepository;
+    private static final String PARAM_ID = "idPost";
+    private static final String PARAM_ACTION = "action";
 
-    public LikeKolPostUseCase() {
-//        this.feedRepository = feedRepository;
+    private LikeKolPostSourceCloud likeKolPostSourceCloud;
+
+    @Inject
+    public LikeKolPostUseCase(LikeKolPostSourceCloud likeKolPostSourceCloud) {
+        this.likeKolPostSourceCloud = likeKolPostSourceCloud;
     }
 
     @Override
     public Observable<Boolean> createObservable(RequestParams requestParams) {
-        return null;
-//        return feedRepository.likeUnlikeKolPost(requestParams);
+        return likeKolPostSourceCloud.likeKolPost(requestParams);
     }
 
 
-    public static RequestParams getParam(int id, int action) {
+    public static RequestParams getParam(int postId, int action) {
         RequestParams params = RequestParams.create();
-        params.putInt(PARAM_ID, id);
+        params.putInt(PARAM_ID, postId);
         params.putInt(PARAM_ACTION, action);
         return params;
     }
