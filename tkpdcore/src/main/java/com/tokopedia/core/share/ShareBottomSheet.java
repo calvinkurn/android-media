5784b94714994b69993ac65dd090dd56b4b1d841
Meyta <meytajennist@gmail.com>
2018-05-21 09:05:25 +0700

    [Android] Change view of Share pdp 2 (with tracking)

--- tkpdcore/src/main/java/com/tokopedia/core/share/ShareBottomSheet.java
+++ tkpdcore/src/main/java/com/tokopedia/core/share/ShareBottomSheet.java
@@ -1,6 +1,5 @@
 package com.tokopedia.core.share;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -10,8 +9,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.tokopedia.core.R;
+import com.tokopedia.core.analytics.AppEventTracking;
+import com.tokopedia.core.analytics.TrackingUtils;
+import com.tokopedia.core.analytics.UnifyTracking;
 import com.tokopedia.core.product.model.share.ShareData;
 import com.tokopedia.core.share.adapter.ShareAdapter;
+import com.tokopedia.core.util.BranchSdkUtils;
+import com.tokopedia.core.util.ClipboardHandler;
 import com.tokopedia.core.util.ShareSocmedHandler;
 import com.tokopedia.design.component.BottomSheets;
 
@@ -94,39 +98,62 @@ public class ShareBottomSheet extends BottomSheets implements ShareAdapter.OnIte
     @Override
     public void onItemClick(String packageName) {
         if (packageName.equalsIgnoreCase("lainnya")) {
-            startActivity(Intent.createChooser(getIntent(""), "Share using"));
+            actionMore(packageName);
         } else if (packageName.equalsIgnoreCase("salinlink")) {
-            Toast.makeText(getActivity(), "will be available soon", Toast.LENGTH_SHORT).show();
+            actionCopy();
         } else {
-            ShareSocmedHandler.ShareSpecific(data, getActivity(),packageName,
-                    "text/plain", null, null);
+            actionShare(packageName);
         }
-
-        sendTracker();
     }
 
+    private void actionCopy() {
+        data.setSource("Copy");
+        BranchSdkUtils.generateBranchLink(data, getActivity(), new BranchSdkUtils.GenerateShareContents() {
+            @Override
+            public void onCreateShareContents(String shareContents, String shareUri, String branchUrl) {
+                ClipboardHandler.CopyToClipboard(getActivity(), shareUri);
+            }
+        });
 
-    private Intent getIntent(String packageName) {
+        Toast.makeText(getActivity(), "Teks berhasil disalin.", Toast.LENGTH_SHORT).show();
+        sendAnalyticsToGTM(data.getType(),"Copy");
+    }
+
+    private void actionShare(String packageName) {
+        String media = constantMedia(packageName);
+        data.setSource(media);
+
+        ShareSocmedHandler.ShareSpecific(data, getActivity(), packageName,
+                "text/plain", null, null);
+
+        sendTracker(packageName);
+    }
+
+    private void actionMore(final String packageName) {
+        BranchSdkUtils.generateBranchLink(data, getActivity(), new BranchSdkUtils.GenerateShareContents() {
+            @Override
+            public void onCreateShareContents(String shareContents, String shareUri, String branchUrl) {
+                Intent intent = getIntent(shareContents);
+                startActivity(Intent.createChooser(intent, "Lainnya"));
+
+                sendTracker(packageName);
+            }
+        });
+    }
+
+    private Intent getIntent(String contains) {
         final Intent mIntent = new Intent(Intent.ACTION_SEND);
         mIntent.setType("text/plain");
 
-        if (!packageName.isEmpty())
-            mIntent.setPackage(packageName);
-
         String title = "";
-        String desc = "";
-
         if (data != null) {
-
+            title = data.getName();
         }
 
         mIntent.putExtra(Intent.EXTRA_TITLE, title);
-        mIntent.putExtra(Intent.EXTRA_TEXT, desc);
+        mIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+        mIntent.putExtra(Intent.EXTRA_TEXT, contains);
         return mIntent;
-    }
-
-    private void sendTracker() {
-
     }
 
     private String getTitle(String type) {
@@ -151,5 +178,60 @@ public class ShareBottomSheet extends BottomSheets implements ShareAdapter.OnIte
                 return getString(R.string.promo_share_detail);
         }
         return "";
+    }
+
+    /**
+     * Tracking
+     * @param packageName
+     * @return String media tracking
+     */
+    private String constantMedia(String packageName) {
+        if (packageName.contains("whatsapp")) {
+            return AppEventTracking.SOCIAL_MEDIA.WHATSHAPP;
+        } else if (packageName.contains("line")) {
+            return AppEventTracking.SOCIAL_MEDIA.LINE;
+        } else if (packageName.contains("twitter")) {
+            return AppEventTracking.SOCIAL_MEDIA.TWITTER;
+        } else if (packageName.contains("facebook")) {
+            return AppEventTracking.SOCIAL_MEDIA.FACEBOOK;
+        } else if (packageName.contains("google")) {
+            return AppEventTracking.SOCIAL_MEDIA.GOOGLE_PLUS;
+        } else if (packageName.contains("lainnya")) {
+            return AppEventTracking.SOCIAL_MEDIA.OTHER;
+        }
+        return "";
+    }
+
+    private void sendTracker(String packageName) {
+        String media = constantMedia(packageName);
+        if (!media.isEmpty()) {
+            if (data.getType().equals(ShareData.CATEGORY_TYPE)) {
+                shareCategory(data, media);
+            } else {
+                sendAnalyticsToGTM(data.getType(), media);
+            }
+        }
+    }
+
+    private void shareCategory(ShareData data, String media) {
+        String[] shareParam = data.getSplittedDescription(",");
+        if (shareParam.length == 2) {
+            UnifyTracking.eventShareCategory(shareParam[0], shareParam[1] + "-" + media);
+        }
+    }
+
+    private void sendAnalyticsToGTM(String type, String channel) {
+        switch (type) {
+            case ShareData.REFERRAL_TYPE:
+                UnifyTracking.eventReferralAndShare(AppEventTracking.Action.SELECT_CHANNEL, channel);
+                TrackingUtils.sendMoEngageReferralShareEvent(channel);
+                break;
+            case ShareData.APP_SHARE_TYPE:
+                UnifyTracking.eventAppShareWhenReferralOff(AppEventTracking.Action.SELECT_CHANNEL, channel);
+                break;
+            default:
+                UnifyTracking.eventShare(channel);
+                break;
+        }
     }
 }
