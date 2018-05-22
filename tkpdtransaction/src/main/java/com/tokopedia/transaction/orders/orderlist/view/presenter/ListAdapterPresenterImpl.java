f77b4d6ddd79264658c1b6a93ade1e7c61705ef7
baghira <sandeep.gupta@tokopedia.com>
2018-05-22 12:14:24 +0530

    corrected the statuscolor check.

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/presenter/ListAdapterPresenterImpl.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/presenter/ListAdapterPresenterImpl.java
@@ -58,7 +58,8 @@ public class ListAdapterPresenterImpl extends BaseDaggerPresenter<ListAdapterCon
         view.setStatus(order.statusStr());
         if (order.status() == 0 || order.status() == 800 || order.status() == 901) {
             view.setStatusBgColor(0);
-        } else if (!order.statusColor().equals("")) {
+        }
+        if (!order.statusColor().equals("")) {
             view.setStatusBgColor(Color.parseColor(order.statusColor()));
         }
         if (!order.conditionalInfo().text().equals("")) {
