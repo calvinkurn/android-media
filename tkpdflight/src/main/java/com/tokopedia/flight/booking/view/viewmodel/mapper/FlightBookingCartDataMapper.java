6fa630ab97d42e6d64043d8effd672841a825f62
Muhammad Furqan <nanjihusaha@gmail.com>
2018-05-22 11:08:30 +0700

    mapper entity and model for AutoApply Voucher Implementation

--- tkpdflight/src/main/java/com/tokopedia/flight/booking/view/viewmodel/mapper/FlightBookingCartDataMapper.java
+++ tkpdflight/src/main/java/com/tokopedia/flight/booking/view/viewmodel/mapper/FlightBookingCartDataMapper.java
@@ -2,8 +2,10 @@ package com.tokopedia.flight.booking.view.viewmodel.mapper;
 
 import com.tokopedia.flight.booking.data.cloud.entity.Amenity;
 import com.tokopedia.flight.booking.data.cloud.entity.CartEntity;
+import com.tokopedia.flight.booking.data.cloud.entity.Voucher;
 import com.tokopedia.flight.booking.view.viewmodel.FlightBookingAmenityMetaViewModel;
 import com.tokopedia.flight.booking.view.viewmodel.FlightBookingCartData;
+import com.tokopedia.flight.booking.view.viewmodel.FlightBookingVoucherViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,12 +60,33 @@ public class FlightBookingCartDataMapper {
                 }
                 data.setLuggageViewModels(luggageMetaViewModels);
                 data.setMealViewModels(mealMetaViewModels);
+                data.setVoucherViewModel(transform(entity.getAttribute().getFlightAttribute().getVoucher()));
             } else {
                 data.setLuggageViewModels(new ArrayList<FlightBookingAmenityMetaViewModel>());
                 data.setMealViewModels(new ArrayList<FlightBookingAmenityMetaViewModel>());
             }
             data.setNewFarePrices(entity.getAttribute().getFlightAttribute().getNewPrices());
         }
+        return data;
+    }
+
+    private FlightBookingVoucherViewModel transform(Voucher voucher) {
+        FlightBookingVoucherViewModel data = new FlightBookingVoucherViewModel();
+
+        data.setEnableVoucher(voucher.getEnableVoucher());
+        data.setIsCouponActive(voucher.getIsCouponActive());
+        data.setAutoapplySuccess(voucher.getAutoApply().getIsSuccess());
+        data.setCode(voucher.getAutoApply().getCode());
+        data.setIsCoupon(voucher.getAutoApply().getIsCoupon());
+        data.setDiscountAmount(voucher.getAutoApply().getDiscountAmount());
+        data.setDiscountPrice(voucher.getAutoApply().getDiscountPrice());
+        data.setDiscountedAmount(voucher.getAutoApply().getDiscountedAmount());
+        data.setDiscountedPrice(voucher.getAutoApply().getDiscountedPrice());
+        data.setTitleDescription(voucher.getAutoApply().getTitleDescription());
+        data.setMessageSuccess(voucher.getAutoApply().getMessageSuccess());
+        data.setPromoId(voucher.getAutoApply().getPromoId());
+        data.setDefaultPromoTab(voucher.getDefaultPromoTab());
+
         return data;
     }
 
