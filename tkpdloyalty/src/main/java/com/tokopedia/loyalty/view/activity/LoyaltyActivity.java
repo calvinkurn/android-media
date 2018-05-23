72ce8d67ab7c6525eb31f134d48361b45d5bd619
Angga Prasetiyo <agapras@gmail.com>
2018-05-23 10:15:32 +0700

    tab change listener

--- tkpdloyalty/src/main/java/com/tokopedia/loyalty/view/activity/LoyaltyActivity.java
+++ tkpdloyalty/src/main/java/com/tokopedia/loyalty/view/activity/LoyaltyActivity.java
@@ -24,6 +24,7 @@ import com.tokopedia.loyalty.R2;
 import com.tokopedia.loyalty.di.component.DaggerLoyaltyViewComponent;
 import com.tokopedia.loyalty.di.component.LoyaltyViewComponent;
 import com.tokopedia.loyalty.di.module.LoyaltyViewModule;
+import com.tokopedia.loyalty.listener.LoyaltyActivityTabSelectedListener;
 import com.tokopedia.loyalty.view.adapter.LoyaltyPagerAdapter;
 import com.tokopedia.loyalty.view.data.LoyaltyPagerItem;
 import com.tokopedia.loyalty.view.fragment.PromoCodeFragment;
@@ -140,6 +141,7 @@ public class LoyaltyActivity extends BasePresenterActivity
         loyaltyPagerAdapter.addAllItem(loyaltyPagerItemListCouponActive);
         viewPager.setAdapter(loyaltyPagerAdapter);
         viewPager.addOnPageChangeListener(new OnTabPageChangeListener(indicator));
+        indicator.setOnTabSelectedListener(new LoyaltyActivityTabSelectedListener(viewPager));
         if (IRouterConstant.LoyaltyModule.ExtraLoyaltyActivity.MARKETPLACE_STRING.equalsIgnoreCase(platformString))
             indicator.addOnTabSelectedListener(onTabSelectedForTrackingCheckoutMarketPlace);
         setShowCase();
