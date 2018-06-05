a5de169e9a5a1131725b5a259334b86334ca09b7
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-06-05 15:46:29 +0700

    add departure hour column in train schedule database for filtering schedule by departure time

--- features/digital/train/src/main/java/com/tokopedia/train/search/data/specification/TrainScheduleDepartureTimeFilterSpecification.java
+++ features/digital/train/src/main/java/com/tokopedia/train/search/data/specification/TrainScheduleDepartureTimeFilterSpecification.java
@@ -2,6 +2,10 @@ package com.tokopedia.train.search.data.specification;
 
 import com.raizlabs.android.dbflow.sql.language.ConditionGroup;
 import com.tokopedia.train.common.specification.DbFlowSpecification;
+import com.tokopedia.train.search.data.databasetable.TrainScheduleDbTable_Table;
+import com.tokopedia.train.search.data.typedef.DepartureTimeTypeDef;
+
+import java.util.List;
 
 /**
  * @author rizkyfadillah on 15/03/18.
@@ -9,10 +13,29 @@ import com.tokopedia.train.common.specification.DbFlowSpecification;
 
 public class TrainScheduleDepartureTimeFilterSpecification implements DbFlowSpecification {
 
+    private List<String> departureTrains;
+
+    public TrainScheduleDepartureTimeFilterSpecification(List<String> departureTrains) {
+        this.departureTrains = departureTrains;
+    }
+
     @Override
     public ConditionGroup getCondition() {
         ConditionGroup conditions = ConditionGroup.clause();
+        for (int i = 0; i < departureTrains.size(); i++) {
+            String departureTrain = departureTrains.get(i);
+            switch (departureTrain) {
+                case DepartureTimeTypeDef.MORNING:
+                    conditions.or(TrainScheduleDbTable_Table.departure_hour.between(00).and(12));
+                    break;
+                case DepartureTimeTypeDef.AFTERNOON:
+                    conditions.or(TrainScheduleDbTable_Table.departure_hour.between(12).and(18));
+                    break;
+                case DepartureTimeTypeDef.NIGHT:
+                    conditions.or(TrainScheduleDbTable_Table.departure_hour.between(18).and(00));
+                    break;
+            }
+        }
         return conditions;
     }
-
 }
