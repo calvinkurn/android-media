acabaf9dcca95b5dc71d5d13ed8b35aa0bf2ad5b
noiz354 <noiz354@gmail.com>
2018-06-05 14:17:31 +0700

    try to fix drawable.

--- tkpdcore/src/main/java/com/tokopedia/core/app/TActivity.java
+++ tkpdcore/src/main/java/com/tokopedia/core/app/TActivity.java
@@ -16,6 +16,7 @@ import com.tokopedia.core.R;
 import com.tokopedia.core.router.discovery.BrowseProductRouter;
 import com.tokopedia.core.router.transactionmodule.TransactionCartRouter;
 import com.tokopedia.core.util.SessionHandler;
+import android.support.v7.graphics.drawable.VectorDrawableCompat;
 
 /**
  * Created by Nisie on 31/08/15.
@@ -126,7 +127,7 @@ public abstract class TActivity extends BaseActivity {
         } else {
             toolbar.setBackgroundResource(R.drawable.bg_white_toolbar_drop_shadow);
         }
-        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_toolbar_overflow_level_two_black);
+        Drawable drawable = VectorDrawableCompat.create(getResources(), R.drawable.ic_toolbar_overflow_level_two_black, null);
         drawable.setBounds(5, 5, 5, 5);
         toolbar.setOverflowIcon(drawable);
 
