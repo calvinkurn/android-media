545675b64903a2fdd41be5fb0d35ec60ab751a5d
alvarisi <alvarisi01@gmail.com>
2018-06-07 09:39:47 +0700

    passenger wagon

---
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/viewmodel/TrainPassengerViewModel.java
@@ -0,0 +1,10 @@
+package com.tokopedia.train.passenger.viewmodel;
+
+public class TrainPassengerViewModel {
+    private int salutationId;
+    private int paxType;
+    private String name;
+    private String number;
+    private String birthdate;
+    private String phone;
+}
--- features/digital/train/src/main/java/com/tokopedia/train/seat/presentation/widget/TrainSeatPassengerAndWagonView.java
+++ features/digital/train/src/main/java/com/tokopedia/train/seat/presentation/widget/TrainSeatPassengerAndWagonView.java
@@ -3,6 +3,8 @@ package com.tokopedia.train.seat.presentation.widget;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -10,6 +12,9 @@ import com.tokopedia.design.base.BaseCustomView;
 import com.tokopedia.tkpdtrain.R;
 
 public class TrainSeatPassengerAndWagonView extends BaseCustomView {
+    private AppCompatTextView passengerLabel, wagonLabel;
+    private RecyclerView passengerRecyclerView;
+
     public TrainSeatPassengerAndWagonView(@NonNull Context context) {
         super(context);
         init();
@@ -27,10 +32,13 @@ public class TrainSeatPassengerAndWagonView extends BaseCustomView {
 
     private void init() {
         View view = inflate(getContext(), R.layout.widget_train_seat_passenger_wagon, this);
-
+        passengerLabel = view.findViewById(R.id.tv_passenger_label);
+        wagonLabel = view.findViewById(R.id.tv_wagon_label);
+        passengerRecyclerView = view.findViewById(R.id.rv_passenger);
     }
 
     private void init(AttributeSet attributeSet) {
         init();
+
     }
 }
