8093951d4fda272b5c6347d31059831a5609c372
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-06-08 08:14:12 +0700

    Add feed query :construction:

---
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/data/api/FeedApi.java
@@ -0,0 +1,22 @@
+package com.tokopedia.feedplus.data;
+
+import com.tokopedia.abstraction.common.data.model.request.GraphqlRequest;
+import com.tokopedia.abstraction.common.data.model.response.GraphqlResponse;
+import com.tokopedia.kol.feature.post.data.pojo.GetUserKolPostResponse;
+
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import rx.Observable;
+
+/**
+ * @author by milhamj on 08/06/18.
+ */
+
+public interface FeedApi {
+    @POST("./")
+    @Headers({"Content-Type: application/json"})
+    Observable<Response<GraphqlResponse<GetUserKolPostResponse>>>
+    getProfileKolData(@Body GraphqlRequest requestBody);
+}
