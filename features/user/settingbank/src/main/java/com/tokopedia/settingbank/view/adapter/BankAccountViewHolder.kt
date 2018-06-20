d3a7c108c28404ba5bad629ae05670085477cb31
nisie <nathania.sutedja@tokopedia.com>
2018-06-20 11:39:26 +0700

    Fix layout

--- features/user/settingbank/src/main/java/com/tokopedia/settingbank/view/adapter/BankAccountViewHolder.kt
+++ features/user/settingbank/src/main/java/com/tokopedia/settingbank/view/adapter/BankAccountViewHolder.kt
@@ -1,23 +1,100 @@
 package com.tokopedia.settingbank.view.adapter
 
+import android.content.Context
+import android.support.v7.widget.PopupMenu
+import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.core.app.MainApplication
 import com.tokopedia.settingbank.R
 import com.tokopedia.settingbank.view.viewmodel.BankAccountViewModel
-import kotlinx.android.synthetic.main.item_bank_account.*
+
 /**
  * @author by nisie on 6/12/18.
  */
-class BankAccountViewHolder(itemView : View) : AbstractViewHolder<BankAccountViewModel>(itemView) {
+class BankAccountViewHolder(val v: View) : AbstractViewHolder<BankAccountViewModel>
+(v) {
 
+    val bankIcon: ImageView = itemView.findViewById(R.id.bank_icon)
+    val bankName: TextView = itemView.findViewById(R.id.bank_name)
+    val accountNumber: TextView = itemView.findViewById(R.id.account_number)
+    val accountName: TextView = itemView.findViewById(R.id.account_name)
+    val dropDownMenu: ImageView = itemView.findViewById(R.id.dropdown_menu)
+    val mainAccount: TextView = itemView.findViewById(R.id.main_account)
 
+    lateinit var popupMenu: PopupMenu
 
     companion object {
         val LAYOUT = R.layout.item_bank_account
     }
 
     override fun bind(element: BankAccountViewModel?) {
+        if (element != null) {
 
+            bankName.text = MethodChecker.fromHtml(element.bankName)
+            accountName.text = MethodChecker.fromHtml(element.accountName)
+            accountNumber.text = element.accountNumber
+
+            if (element.isDefaultBank) {
+                mainAccount.visibility = View.VISIBLE
+            } else {
+                mainAccount.visibility = View.GONE
+            }
+
+            ImageHandler.LoadImage(bankIcon, element.bankLogo)
+
+            dropDownMenu.setOnClickListener { v: View? ->
+                if (v != null) {
+                    dropDownMenuClickListener(dropDownMenu
+                            .context, v, element)
+                }
+            }
+        }
+    }
+
+    private fun dropDownMenuClickListener(context: Context, v: View, element: BankAccountViewModel?) {
+
+        if (!::popupMenu.isInitialized) {
+            popupMenu = PopupMenu(context, v)
+            popupMenu.setOnMenuItemClickListener { item: MenuItem? -> onPopupMenuClicked(item) }
+        }
+        setupPopupMenu(element, popupMenu)
+        popupMenu.show()
+    }
+
+    private fun onPopupMenuClicked(item: MenuItem?): Boolean {
+        return if (item != null) {
+            when (item.itemId) {
+                R.id.menu_make_main_account -> makeMainAccount()
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
+
+    private fun makeMainAccount(): Boolean {
+        return true
+    }
+
+    private fun setupPopupMenu(element: BankAccountViewModel?, popupMenu: PopupMenu) {
+        if (element != null) {
+
+            popupMenu.menu.clear()
+
+            if (!element.isDefaultBank) {
+                popupMenu.menu.add(1, R.id.menu_make_main_account, 1, MainApplication.getAppContext()
+                        .getString(R.string.menu_make_main_account))
+            }
+            popupMenu.menu.add(1, R.id.menu_edit, 2, MainApplication.getAppContext()
+                    .getString(R.string.menu_edit))
+            popupMenu.menu.add(1, R.id.menu_delete, 3, MainApplication.getAppContext()
+                    .getString(R.string.menu_delete))
+
+        }
     }
 }
--- features/user/settingbank/src/main/java/com/tokopedia/settingbank/view/fragment/SettingBankFragment.kt
+++ features/user/settingbank/src/main/java/com/tokopedia/settingbank/view/fragment/SettingBankFragment.kt
@@ -9,12 +9,12 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.common.utils.snackbar.NetworkErrorHelper
 import com.tokopedia.settingbank.R
-import com.tokopedia.settingbank.view.listener.SettingBankContract
-import com.tokopedia.settingbank.view.presenter.SettingBankPresenter
 import com.tokopedia.settingbank.common.analytics.SettingBankAnalytics
 import com.tokopedia.settingbank.common.di.SettingBankDependencyInjector
 import com.tokopedia.settingbank.view.adapter.BankAccountAdapter
 import com.tokopedia.settingbank.view.adapter.BankAccountTypeFactoryImpl
+import com.tokopedia.settingbank.view.listener.SettingBankContract
+import com.tokopedia.settingbank.view.presenter.SettingBankPresenter
 import com.tokopedia.settingbank.view.viewmodel.BankAccountListViewModel
 import com.tokopedia.settingbank.view.viewmodel.BankAccountViewModel
 import kotlinx.android.synthetic.main.fragment_setting_bank.*
@@ -66,9 +66,9 @@ class SettingBankFragment : SettingBankContract.View, BaseDaggerFragment() {
     private fun setupView() {
         val adapterTypeFactory = BankAccountTypeFactoryImpl()
         val listBank = ArrayList<Visitable<*>>()
-        listBank.add(BankAccountViewModel("1", "", "", "", true, "", "", true, ""))
-        listBank.add(BankAccountViewModel("2", "", "", "", true, "", "", true, ""))
-        listBank.add(BankAccountViewModel("3", "", "", "", true, "", "", true, ""))
+        listBank.add(BankAccountViewModel("1", "", "", "", true, "", "", false, ""))
+        listBank.add(BankAccountViewModel("2", "", "", "", true, "", "", false, ""))
+        listBank.add(BankAccountViewModel("3", "", "", "", true, "", "", false, ""))
 
         adapter = BankAccountAdapter(adapterTypeFactory, listBank)
         val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
