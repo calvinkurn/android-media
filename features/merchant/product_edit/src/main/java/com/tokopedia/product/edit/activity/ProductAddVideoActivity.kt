3b7474e060b818a1d782c65e4a6a148305e8e968
andtomat <andtomat@gmail.com>
2018-06-22 15:02:37 +0700

    add productaddview view

---
+++ features/merchant/product_edit/src/main/java/com/tokopedia/product/edit/activity/ProductAddVideoActivity.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.product.edit.activity
+
+import android.support.v4.app.Fragment
+
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
+class ProductAddVideoActivity : BaseSimpleActivity() {
+
+    override fun getNewFragment(): Fragment? {
+        return null
+    }
+}
---
+++ features/merchant/product_edit/src/main/java/com/tokopedia/product/edit/fragment/ProductAddVideoFragment.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.product.edit.fragment
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+
+class ProductAddVideoFragment : BaseDaggerFragment() {
+
+    override fun initInjector() {
+
+    }
+
+    override fun getScreenName(): String? {
+        return null
+    }
+}
