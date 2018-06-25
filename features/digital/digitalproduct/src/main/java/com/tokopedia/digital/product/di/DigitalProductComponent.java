8b1a5047169df0d31460b457ea9718bfb4531f14
Muhammad Furqan <nanjihusaha@gmail.com>
2018-06-25 15:46:43 +0700

    DI for Digital Product

---
+++ features/digital/digitalproduct/src/main/java/com/tokopedia/digital/product/di/DigitalProductComponent.java
@@ -0,0 +1,21 @@
+package com.tokopedia.digital.product.di;
+
+import android.content.Context;
+
+import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+
+import dagger.Component;
+
+/**
+ * @author by furqan on 25/06/18.
+ */
+
+@DigitalProductScope
+@Component (modules = DigitalProductModule.class, dependencies = BaseAppComponent.class)
+public interface DigitalProductComponent {
+
+    @ApplicationContext Context context();
+
+
+}
