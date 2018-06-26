dfd7c235ea2414d63e68f5d5891455b3a3c3ccd6
okasurya <brilliantsurya@gmail.com>
2018-06-26 19:34:56 +0700

    fix toolbar

--- libraries/analytics/src/main/java/com/tokopedia/analytics/debugger/ui/activity/AnalyticsDebuggerActivity.java
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/debugger/ui/activity/AnalyticsDebuggerActivity.java
@@ -2,14 +2,14 @@ package com.tokopedia.analytics.debugger.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
 import com.tokopedia.analytics.R;
 import com.tokopedia.analytics.debugger.ui.fragment.AnalyticsDebuggerFragment;
 
-public class AnalyticsDebuggerActivity extends BaseSimpleActivity {
+public class AnalyticsDebuggerActivity extends AppCompatActivity {
 
     public static Intent newInstance(Context context) {
         return new Intent(context, AnalyticsDebuggerActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -18,33 +18,14 @@ public class AnalyticsDebuggerActivity extends BaseSimpleActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getLayoutRes());
-        toolbar.setTitle("Analytics Debugger");
+        setContentView(R.layout.activity_analytics_debugger);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    @Override
-    protected Fragment getNewFragment() {
-        return AnalyticsDebuggerFragment.newInstance();
-    }
-
-    @Override
-    protected String getTagFragment() {
-        return AnalyticsDebuggerFragment.TAG;
-    }
-
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
-
-    @Override
-    protected boolean isShowCloseButton() {
-        return true;
-    }
-
-    @Override
-    protected int getLayoutRes() {
-        return super.getLayoutRes();
+        toolbar.setSubtitle("Tokopedia");
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, AnalyticsDebuggerFragment.newInstance(), AnalyticsDebuggerFragment.TAG)
+                    .commit();
+        }
     }
 }
