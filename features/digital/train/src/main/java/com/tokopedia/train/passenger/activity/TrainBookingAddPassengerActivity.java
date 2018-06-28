851b258856c93931111121deda06b82ba0d0a5f6
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-06-28 10:50:35 +0700

    finishing add new passenger

--- features/digital/train/src/main/java/com/tokopedia/train/passenger/activity/TrainBookingAddPassengerActivity.java
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/activity/TrainBookingAddPassengerActivity.java
@@ -1,23 +1,46 @@
 package com.tokopedia.train.passenger.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 import com.tokopedia.train.common.presentation.TrainBaseActivity;
 import com.tokopedia.train.passenger.fragment.TrainBookingAddPassengerFragment;
+import com.tokopedia.train.passenger.viewmodel.TrainPassengerViewModel;
 
 /**
  * Created by nabillasabbaha on 21/06/18.
  */
-public class TrainBookingAddPassengerActivity extends TrainBaseActivity {
+public class TrainBookingAddPassengerActivity extends TrainBaseActivity implements TrainBookingAddPassengerFragment.ActionListener {
 
-    public static Intent callingIntent(Context context) {
-        return new Intent(context, TrainBookingAddPassengerActivity.class);
+    public static final String PASSENGER_DATA = "passenger_data";
+
+    public static Intent callingIntent(Context context, TrainPassengerViewModel trainPassengerViewModel) {
+        Intent intent = new Intent(context, TrainBookingAddPassengerActivity.class);
+        intent.putExtra(PASSENGER_DATA, trainPassengerViewModel);
+        return intent;
     }
 
     @Override
     protected Fragment getNewFragment() {
-        return TrainBookingAddPassengerFragment.newInstance();
+        return TrainBookingAddPassengerFragment.newInstance(getIntent().getParcelableExtra(PASSENGER_DATA));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TrainPassengerViewModel trainPassengerViewModel = getIntent().getParcelableExtra(PASSENGER_DATA);
+        updateTitle(trainPassengerViewModel.getHeaderTitle());
+    }
+
+    @Override
+    public void navigateToBookingPassenger(TrainPassengerViewModel trainPassengerViewModel) {
+        Intent intent = new Intent();
+        intent.putExtra(TrainBookingAddPassengerActivity.PASSENGER_DATA, trainPassengerViewModel);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 }
