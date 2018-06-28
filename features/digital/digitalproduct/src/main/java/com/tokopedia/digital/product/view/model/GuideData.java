5abd0e6b412d462c8e00d639c2e181e865d0825a
Muhammad Furqan <nanjihusaha@gmail.com>
2018-06-28 15:14:50 +0700

    temporary, need to open another branch

---
+++ features/digital/digitalproduct/src/main/java/com/tokopedia/digital/product/view/model/GuideData.java
@@ -0,0 +1,13 @@
+package com.tokopedia.digital.product.view.model;
+
+import android.os.Parcelable;
+
+/**
+ * @author by furqan on 28/06/18.
+ */
+
+public class GuideData implements Parcelable {
+
+
+
+}
