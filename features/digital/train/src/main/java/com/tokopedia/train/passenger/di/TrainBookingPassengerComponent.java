3b6f1e927a346dc9246549638e33397b470f9c17
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-06-28 10:44:22 +0700

    add di package in train booking passenger

---
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/di/TrainBookingPassengerComponent.java
@@ -0,0 +1,19 @@
+package com.tokopedia.train.passenger.di;
+
+import com.tokopedia.train.common.di.TrainComponent;
+import com.tokopedia.train.passenger.fragment.TrainBookingAddPassengerFragment;
+import com.tokopedia.train.passenger.fragment.TrainBookingPassengerFragment;
+
+import dagger.Component;
+
+/**
+ * Created by nabillasabbaha on 26/06/18.
+ */
+@TrainBookingPassengerScope
+@Component(modules = TrainBookingPassengerModule.class, dependencies = TrainComponent.class)
+public interface TrainBookingPassengerComponent {
+
+    void inject(TrainBookingPassengerFragment trainBookingPassengerFragment);
+
+    void inject(TrainBookingAddPassengerFragment trainBookingAddPassengerFragment);
+}
---
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/di/TrainBookingPassengerModule.java
@@ -0,0 +1,12 @@
+package com.tokopedia.train.passenger.di;
+
+import dagger.Module;
+
+/**
+ * Created by nabillasabbaha on 26/06/18.
+ */
+@Module
+public class TrainBookingPassengerModule {
+
+
+}
