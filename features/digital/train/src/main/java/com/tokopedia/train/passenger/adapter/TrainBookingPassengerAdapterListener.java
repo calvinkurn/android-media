7c5d0cd381609eff24da9133a4af6b32bbde2341
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-06-28 10:42:45 +0700

    train booking passenger list adapter

---
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/adapter/TrainBookingPassengerAdapterListener.java
@@ -0,0 +1,11 @@
+package com.tokopedia.train.passenger.adapter;
+
+import com.tokopedia.train.passenger.viewmodel.TrainPassengerViewModel;
+
+/**
+ * Created by nabillasabbaha on 25/06/18.
+ */
+public interface TrainBookingPassengerAdapterListener {
+
+    void onChangePassengerData(TrainPassengerViewModel trainPassengerViewModel);
+}
