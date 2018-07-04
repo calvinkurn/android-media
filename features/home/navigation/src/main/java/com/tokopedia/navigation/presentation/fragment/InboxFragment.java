7b6be4d457efd1b2a24faa1fbdb64eba35cf0ddf
Meyta <meytajennist@gmail.com>
2018-07-04 17:22:41 +0700

    1. Rename ToolbarSearch to MainToolbar 2. Add NotificationToolbar

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/InboxFragment.java
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/InboxFragment.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 import com.tokopedia.navigation.R;
 import com.tokopedia.navigation.presentation.activity.MainParentActivity;
 import com.tokopedia.navigation.presentation.base.ParentFragment;
+import com.tokopedia.searchbar.NotificationToolbar;
 
 /**
  * Created by meta on 19/06/18.
@@ -16,6 +17,8 @@ public class InboxFragment extends ParentFragment {
         return new InboxFragment();
     }
 
+    NotificationToolbar toolbar;
+
     @Override
     public int resLayout() {
         return R.layout.fragment_inbox;
@@ -23,16 +26,15 @@ public class InboxFragment extends ParentFragment {
 
     @Override
     public void initView(View view) {
+        if (toolbar != null)
+            toolbar.setTitle("Inbox");
     }
 
     @Override
     public void setupToolbar(View view) {
         try {
-            toolbar = view.findViewById(R.id.toolbar);
-            TextView title = toolbar.findViewById(R.id.title_toolbar);
-            title.setText("Inbox");
+            this.toolbar = view.findViewById(R.id.toolbar);
             ((MainParentActivity) getActivity()).setSupportActionBar(toolbar);
-            getActivity().invalidateOptionsMenu();
         } catch (Exception ignored) {}
     }
 
--- features/home/searchbar/src/main/java/com/tokopedia/searchbar/NotificationToolbar.java
+++ features/home/searchbar/src/main/java/com/tokopedia/searchbar/NotificationToolbar.java
@@ -2,59 +2,63 @@ package com.tokopedia.searchbar;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.tokopedia.notification.presentation.activity.NotificationActivity;
 
 import q.rorbin.badgeview.QBadgeView;
 
 /**
- * Created by meta on 22/06/18.
+ * Created by meta on 04/07/18.
  */
-public class ToolbarSearch extends Toolbar {
+public class NotificationToolbar extends Toolbar {
 
-    private ImageButton btnQrCode;
-    private ImageButton btnNotification;
-    private EditText editTextSearch;
-
-    private QBadgeView badgeView;
-
-    public ToolbarSearch(Context context) {
+    public NotificationToolbar(Context context) {
         super(context);
         init();
     }
 
-    public ToolbarSearch(Context context, @Nullable AttributeSet attrs) {
+    public NotificationToolbar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public ToolbarSearch(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public NotificationToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
 
+    private TextView textViewTitle;
+    private ImageButton btnNotification;
+    private QBadgeView badgeView;
+
     private void init() {
-        inflate(getContext(), R.layout.toolbar_search, this);
-        btnQrCode = findViewById(R.id.btn_qrcode);
+        inflate(getContext(), R.layout.notification_toolbar, this);
+        textViewTitle = findViewById(R.id.textview_title);
         btnNotification = findViewById(R.id.btn_notification);
-        editTextSearch = findViewById(R.id.et_search);
 
         badgeView = new QBadgeView(getContext());
         badgeView.bindTarget(btnNotification);
         badgeView.setBadgeGravity(Gravity.END | Gravity.TOP);
         badgeView.setBadgeNumber(2);
 
-        btnQrCode.setOnClickListener(v ->
-                Toast.makeText(getContext(), "QR Code things", Toast.LENGTH_SHORT).show());
-
         btnNotification.setOnClickListener(v ->
                 NotificationActivity.start(getContext()));
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        if (textViewTitle != null)
+            textViewTitle.setText(title);
+    }
+
+    @Override
+    public void setTitle(int resId) {
+        if (textViewTitle != null)
+            textViewTitle.setText(resId);
     }
 }
