2c3e89344719e7f03a5cba1ec2e059855994dc37
okasurya <brilliantsurya@gmail.com>
2018-07-04 17:26:42 +0700

    code review

--- libraries/analytics/src/debug/java/com/tokopedia/analytics/debugger/data/mapper/GtmLogMapper.java
+++ libraries/analytics/src/debug/java/com/tokopedia/analytics/debugger/data/mapper/GtmLogMapper.java
@@ -30,7 +30,7 @@ public class GtmLogMapper implements Func1<GtmLogDB, Observable<Visitable>> {
         viewModel.setName(gtmLogDB.getName());
         viewModel.setCategory(gtmLogDB.getCategory());
         viewModel.setData(gtmLogDB.getData());
-        if(gtmLogDB.getData().length() > 30) {
+        if(gtmLogDB.getData().length() > 27) {
             viewModel.setDataExcerpt(gtmLogDB.getData().substring(0,27) + "...");
         } else {
             viewModel.setDataExcerpt(gtmLogDB.getData());
