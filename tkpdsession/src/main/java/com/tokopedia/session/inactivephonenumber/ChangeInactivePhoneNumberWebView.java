230c139e8c0324e5586bfe0a529c2f537a1d1666
nisie <nathania.sutedja@tokopedia.com>
2018-07-06 18:51:43 +0700

    Change wording

--- tkpdsession/src/main/java/com/tokopedia/session/inactivephonenumber/ChangeInactivePhoneNumberWebView.java
+++ tkpdsession/src/main/java/com/tokopedia/session/inactivephonenumber/ChangeInactivePhoneNumberWebView.java
@@ -1,5 +1,7 @@
 package com.tokopedia.session.inactivephonenumber;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 
 import com.tokopedia.core.home.SimpleWebViewWithFilePickerActivity;
@@ -9,8 +11,22 @@ import com.tokopedia.core.home.SimpleWebViewWithFilePickerActivity;
  */
 public class ChangeInactivePhoneNumberWebView extends SimpleWebViewWithFilePickerActivity {
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
+        return true;
+    }
+
+    public static Intent getIntent(Context context, String url) {
+        Intent intent = new Intent(context, ChangeInactivePhoneNumberWebView.class);
+        intent.putExtra(EXTRA_URL, url);
+        return intent;
+    }
+
+    public static Intent getIntentWithTitle(Context context, String url, String title) {
+        Intent intent = new Intent(context, ChangeInactivePhoneNumberWebView.class);
+        intent.putExtra(EXTRA_URL, url);
+        intent.putExtra(EXTRA_TITLE, title);
+        return intent;
     }
 }
