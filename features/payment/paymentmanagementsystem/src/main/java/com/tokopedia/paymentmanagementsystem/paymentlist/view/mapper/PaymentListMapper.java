57c38404ac541c5e7be328441db2a7c21ad1effb
zulfikar rahman <zulfikarrahman@201553-MacBook-Pro.local>
2018-07-10 20:32:58 +0700

    fix image bank payment

--- features/payment/paymentmanagementsystem/src/main/java/com/tokopedia/paymentmanagementsystem/paymentlist/view/mapper/PaymentListMapper.java
+++ features/payment/paymentmanagementsystem/src/main/java/com/tokopedia/paymentmanagementsystem/paymentlist/view/mapper/PaymentListMapper.java
@@ -29,7 +29,7 @@ public class PaymentListMapper {
             paymentListModel.setShowCancelTransaction(!paymentListInside.isShowHelpPage() && paymentListInside.isShowCancelButton() && getListOfAction(paymentListInside, context).size() <= 1);
             paymentListModel.setShowSeeDetail(!TextUtils.isEmpty(paymentListInside.getInvoiceUrl()));
             paymentListModel.setInvoiceUrl(paymentListInside.getInvoiceUrl());
-            paymentListModel.setPaymentImage(paymentListInside.getGatewayImg());
+            paymentListModel.setPaymentImage(TextUtils.isEmpty(paymentListInside.getBankImg()) ? paymentListInside.getGatewayImg() : paymentListInside.getBankImg());
             paymentListModel.setPaymentAmount(paymentListInside.getPaymentAmount());
             paymentListModel.setShowContainerDetailBankTransfer(isBankTransfer(paymentListInside));
             paymentListModel.setDestAccountNo(paymentListInside.getDestBankAccount().getAccNo());
