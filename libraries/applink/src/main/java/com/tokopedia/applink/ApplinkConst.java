bba319b70387f2681cf7fdd88308115ef614f1f7
yogieputra8 <yogie.putra8@gmail.com>
2018-07-11 01:18:20 +0700

    howtopay development

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -81,6 +81,7 @@ public interface ApplinkConst {
     String BRAND_LIST_WITH_SLASH = "tokopedia://official-store/brand/";
     String REGISTER = "tokopedia://registration";
     String PROFILE = "tokopedia://people/{user_id}";
+    String HOWTOPAY = "tokopedia://howtopay";
 
 
     String EVENTS = "tokopedia://events";
