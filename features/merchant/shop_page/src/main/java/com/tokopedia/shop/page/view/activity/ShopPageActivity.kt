4c4a86d6e8144fa72381239a9474eb30fa1ec9a3
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-07-11 16:18:36 +0700

    fix toolbar theme

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/activity/ShopPageActivity.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/activity/ShopPageActivity.kt
@@ -2,11 +2,10 @@ package com.tokopedia.shop.page.view.activity
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.TabLayout
-import android.support.v4.view.PagerAdapter
-import com.tokopedia.abstraction.base.view.activity.BaseTabActivity
+import android.support.v4.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.shop.R
 import com.tokopedia.shop.ShopComponentInstance
@@ -15,7 +14,7 @@ import com.tokopedia.shop.page.view.adapter.ShopPageViewPagerAdapter
 import com.tokopedia.shop.product.view.widget.ShopPagePromoWebView
 import kotlinx.android.synthetic.main.activity_shop_page.*
 
-class ShopPageActivity: BaseTabActivity(), ShopPagePromoWebView.Listener, HasComponent<ShopComponent> {
+class ShopPageActivity: BaseSimpleActivity(), ShopPagePromoWebView.Listener, HasComponent<ShopComponent> {
 
     override fun getComponent() = ShopComponentInstance.getComponent(application)
 
@@ -25,8 +24,9 @@ class ShopPageActivity: BaseTabActivity(), ShopPagePromoWebView.Listener, HasCom
     var shopDomain: String? = null
     var shopAttribution: String? = null
 
+    lateinit var shopPageViewPagerAdapter: ShopPageViewPagerAdapter;
+
     companion object {
-        const val PAGE_LIMIT = 2
         const val SHOP_ID = "EXTRA_SHOP_ID"
         const val SHOP_DOMAIN = "EXTRA_SHOP_DOMAIN"
         const val SHOP_ATTRIBUTION = "EXTRA_SHOP_ATTRIBUTION"
@@ -39,29 +39,32 @@ class ShopPageActivity: BaseTabActivity(), ShopPagePromoWebView.Listener, HasCom
 
     }
 
-    override fun getViewPagerAdapter(): PagerAdapter {
-        val titles = arrayOf(getString(R.string.shop_info_title_tab_product),
-                getString(R.string.shop_info_title_tab_info))
-        return ShopPageViewPagerAdapter(supportFragmentManager, titles, shopId, shopDomain, shopAttribution)
-    }
-
-    override fun getPageLimit() = PAGE_LIMIT
-
-    override fun getLayoutRes(): Int {
-        return R.layout.activity_shop_page
-    }
-
-    override fun setupLayout(savedInstanceState: Bundle?) {
-        super.setupLayout(savedInstanceState)
-        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE)
-        tabLayout.setupWithViewPager(viewPager)
-        setSupportActionBar(toolbar)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         shopId = intent.getStringExtra(SHOP_ID)
         shopDomain = intent.getStringExtra(SHOP_DOMAIN)
         shopAttribution = intent.getStringExtra(SHOP_ATTRIBUTION)
         super.onCreate(savedInstanceState)
+
+        initAdapter();
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        viewPager.adapter = shopPageViewPagerAdapter
+
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE)
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
+
+    override fun getLayoutRes(): Int {
+        return R.layout.activity_shop_page
+    }
+
+    override fun getNewFragment(): Fragment? {
+        return null
+    }
+
+    fun initAdapter(){
+        val titles = arrayOf(getString(R.string.shop_info_title_tab_product),
+                getString(R.string.shop_info_title_tab_info))
+        shopPageViewPagerAdapter = ShopPageViewPagerAdapter(supportFragmentManager, titles, shopId, shopDomain, shopAttribution)
     }
 }
