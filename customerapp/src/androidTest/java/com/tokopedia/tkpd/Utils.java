1776a1fc82d9963e4722f32a3bb4f5fa07165de8
Muhammad Furqan <nanjihusaha@gmail.com>
2018-07-17 15:57:12 +0700

    move TkpdFlight to features -> digital -> flight

--- customerapp/src/androidTest/java/com/tokopedia/tkpd/Utils.java
+++ customerapp/src/androidTest/java/com/tokopedia/tkpd/Utils.java
@@ -51,6 +51,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static java.util.concurrent.TimeUnit.SECONDS;
