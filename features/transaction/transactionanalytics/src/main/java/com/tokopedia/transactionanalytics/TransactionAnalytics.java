9d59ef34ac861000c9c08680458e870d061fc18c
Angga Prasetiyo <agapras@gmail.com>
2018-07-18 18:16:47 +0700

    rename method same as analytics data

--- features/transaction/transactionanalytics/src/main/java/com/tokopedia/transactionanalytics/TransactionAnalytics.java
+++ features/transaction/transactionanalytics/src/main/java/com/tokopedia/transactionanalytics/TransactionAnalytics.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public abstract class TransactionAnalytics {
 
-    protected final AnalyticTracker analyticTracker;
+    private final AnalyticTracker analyticTracker;
 
     TransactionAnalytics(AnalyticTracker analyticTracker) {
         this.analyticTracker = analyticTracker;
@@ -21,18 +21,18 @@ public abstract class TransactionAnalytics {
         if (analyticTracker != null) analyticTracker.sendScreen(activity, screenName);
     }
 
-    protected void sendEventCategoryActionLabel(String event, String eventCategory,
-                                                String eventAction, String eventLabel) {
+    void sendEventCategoryActionLabel(String event, String eventCategory,
+                                      String eventAction, String eventLabel) {
         if (analyticTracker != null)
             analyticTracker.sendEventTracking(event, eventCategory, eventAction, eventLabel);
     }
 
-    protected void sendEventCategoryAction(String event, String eventCategory,
-                                           String eventAction) {
+    void sendEventCategoryAction(String event, String eventCategory,
+                                 String eventAction) {
         sendEventCategoryActionLabel(event, eventCategory, eventAction, "");
     }
 
-    protected void sendEnhancedEcommerce(Map<String, Object> dataLayer) {
+    void sendEnhancedEcommerce(Map<String, Object> dataLayer) {
         if (analyticTracker != null)
             analyticTracker.sendEnhancedEcommerce(dataLayer);
     }
