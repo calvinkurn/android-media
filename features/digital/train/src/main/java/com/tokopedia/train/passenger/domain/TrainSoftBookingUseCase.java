81f7cb9b29e2c366332bde65f0b98771482082b2
rizkyfadillah <rizkyfadillah1994@gmail.com>
2018-07-18 09:13:53 +0700

    clean up code

--- features/digital/train/src/main/java/com/tokopedia/train/passenger/domain/TrainSoftBookingUseCase.java
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/domain/TrainSoftBookingUseCase.java
@@ -1,16 +1,9 @@
 package com.tokopedia.train.passenger.domain;
 
 import com.tokopedia.train.common.domain.TrainRepository;
-import com.tokopedia.train.passenger.domain.model.TrainPaxPassenger;
-import com.tokopedia.train.passenger.domain.model.TrainSeat;
 import com.tokopedia.train.passenger.domain.model.TrainSoftbook;
-import com.tokopedia.train.passenger.domain.model.TrainTrip;
-import com.tokopedia.train.passenger.domain.model.TrainTripBuilder;
 import com.tokopedia.usecase.RequestParams;
 import com.tokopedia.usecase.UseCase;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import rx.Observable;
 
