92d73b25f47180b847fe3a7b7ef8e94dd8729975
andtomat <andtomat@gmail.com>
2018-07-23 13:15:55 +0700

    create layout category product

--- features/merchant/product_edit/src/main/java/com/tokopedia/product/edit/price/BaseProductAddFragment.kt
+++ features/merchant/product_edit/src/main/java/com/tokopedia/product/edit/price/BaseProductAddFragment.kt
@@ -3,8 +3,9 @@ package com.tokopedia.product.edit.price
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.*
 import com.tokopedia.imagepicker.picker.gallery.type.GalleryType
 import com.tokopedia.imagepicker.picker.main.builder.ImageEditActionTypeDef.*
@@ -21,6 +22,8 @@ import com.tokopedia.product.edit.view.activity.BaseProductEditActivity
 import kotlinx.android.synthetic.main.fragment_product_add_new.*
 
 class BaseProductAddFragment : Fragment() {
+
+    private lateinit var nameTextWatcher: TextWatcher
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +46,27 @@ class BaseProductAddFragment : Fragment() {
         recyclerView.adapter = productCategoryRecommendationAdapter
         recyclerView.setHasFixedSize(true)
         recyclerView.isNestedScrollingEnabled = false
+
+        nameTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.isNotEmpty()) {
+                    textInputLayoutName.error = null
+                    llCategory.visibility = View.VISIBLE
+                } else {
+                    textInputLayoutName.error = "Error Nama"
+                    llCategory.visibility = View.GONE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+        editTextName.addTextChangedListener(nameTextWatcher)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
