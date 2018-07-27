f3958053d9921037fbcc9d7c4fe14567f4da13fc
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-07-27 19:41:50 +0700

    Work on see all comments :construction:

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/adapter/viewholder/SeeAllCommentsViewHolder.java
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/adapter/viewholder/SeeAllCommentsViewHolder.java
@@ -1,7 +1,9 @@
 package com.tokopedia.kol.feature.postdetail.view.adapter.viewholder;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.view.View;
+import android.widget.TextView;
 
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.kol.R;
@@ -13,14 +15,24 @@ import com.tokopedia.kol.feature.postdetail.view.viewmodel.SeeAllCommentsViewMod
 
 public class SeeAllCommentsViewHolder extends AbstractViewHolder<SeeAllCommentsViewModel> {
     @LayoutRes
-    public static final int LAYOUT = R.layout.explore_layout;
+    public static final int LAYOUT = R.layout.kol_comment_see_all;
+
+    private Context context;
+    private TextView seeAllComment;
 
     public SeeAllCommentsViewHolder(View itemView) {
         super(itemView);
+        context = itemView.getContext();
+        seeAllComment = itemView.findViewById(R.id.see_all_comment);
     }
 
     @Override
     public void bind(SeeAllCommentsViewModel element) {
-
+        seeAllComment.setText(
+                String.format(
+                        context.getString(R.string.see_all_comment),
+                        element.getTotalComments()
+                )
+        );
     }
 }
