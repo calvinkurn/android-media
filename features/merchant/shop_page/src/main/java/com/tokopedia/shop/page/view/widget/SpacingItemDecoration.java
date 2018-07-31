40e2e04060ae0f4db3ec2754d9149074172341d8
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-07-31 16:46:34 +0700

    fix refresh sticky when swipe refresh

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/widget/SpacingItemDecoration.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/widget/SpacingItemDecoration.java
@@ -8,6 +8,7 @@ import android.view.View;
 public class SpacingItemDecoration extends RecyclerView.ItemDecoration {
     private final int spacing;
     private int displayMode;
+    private int ignoreOffset = -1; // if set to 3, position 0 to 2 will be ignored.
 
     public static final int HORIZONTAL = 0;
     public static final int VERTICAL = 1;
@@ -20,6 +21,10 @@ public class SpacingItemDecoration extends RecyclerView.ItemDecoration {
     public SpacingItemDecoration(int spacing, int displayMode) {
         this.spacing = spacing;
         this.displayMode = displayMode;
+    }
+
+    public void setIgnoreOffset(int ignoreOffset) {
+        this.ignoreOffset = ignoreOffset;
     }
 
     @Override
@@ -39,30 +44,40 @@ public class SpacingItemDecoration extends RecyclerView.ItemDecoration {
         if (displayMode == -1) {
             displayMode = resolveDisplayMode(layoutManager);
         }
+        if (ignoreOffset > position) {
+            return;
+        }
 
         switch (displayMode) {
             case HORIZONTAL:
-                outRect.left = spacing;
-                outRect.right = position == itemCount - 1 ? spacing : 0;
+                outRect.left = position == 0 ? spacing * 2: spacing;
+                outRect.right = position == itemCount - 1 ? spacing * 2 : spacing;
                 outRect.top = spacing;
                 outRect.bottom = spacing;
                 break;
             case VERTICAL:
                 outRect.left = spacing;
                 outRect.right = spacing;
-                outRect.top = spacing;
-                outRect.bottom = position == itemCount - 1 ? spacing : 0;
+                outRect.top = position == 0 ? spacing * 2: spacing;
+                outRect.bottom = position == itemCount - 1 ? spacing * 2: spacing;
                 break;
             case GRID:
                 if (layoutManager instanceof GridLayoutManager) {
                     GridLayoutManager gridLayoutManager = (GridLayoutManager) layoutManager;
-                    int cols = gridLayoutManager.getSpanCount();
-                    int rows = itemCount / cols;
+                    int totalCol = gridLayoutManager.getSpanCount();
+                    int totalRow = itemCount / totalCol;
 
-                    outRect.left = spacing;
-                    outRect.right = position % cols == cols - 1 ? spacing : 0;
-                    outRect.top = spacing;
-                    outRect.bottom = position / cols == rows - 1 ? spacing : 0;
+                    int colPosition = position % totalCol;
+                    int rowPosition = position / totalCol;
+                    boolean isRightMost = colPosition == totalCol - 1;
+                    boolean isLeftMost = colPosition == 0;
+                    boolean isBottomMost = rowPosition == totalRow - 1;
+                    boolean isTopMost = rowPosition == 0;
+
+                    outRect.left = isLeftMost ? spacing * 2 : spacing;
+                    outRect.right = isRightMost ? spacing * 2 : spacing;
+                    outRect.top = isTopMost ? spacing * 2 : spacing;
+                    outRect.bottom = isBottomMost ? spacing * 2 : spacing;
                 }
                 break;
         }
