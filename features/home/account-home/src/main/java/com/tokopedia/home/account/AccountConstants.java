ca0e9b0fb03c5cbfeee81e46903a5a497d7e66ae
Meyta <meytajennist@gmail.com>
2018-07-31 11:44:08 +0700

    get background tokopedia pay from server

--- features/home/account-home/src/main/java/com/tokopedia/home/account/AccountConstants.java
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/AccountConstants.java
@@ -6,4 +6,8 @@ package com.tokopedia.home.account;
 public class AccountConstants {
     public static final String QUERY = "query";
     public static final String VARIABLES = "variables";
+
+    public class Url {
+        public static final String IMAGE_URL = "https://ecs7.tokopedia.net/img/android/others/";
+    }
 }
