c4fe800051205a57aa50099ef8278bf7e263ad95
alvinatin <alvin2304@gmail.com>
2018-07-31 15:24:12 +0700

    add url langganan and fav number account home

--- features/home/account-home/src/main/java/com/tokopedia/home/account/AccountConstants.java
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/AccountConstants.java
@@ -7,7 +7,15 @@ public class AccountConstants {
     public static final String QUERY = "query";
     public static final String VARIABLES = "variables";
 
-    public class Url {
+    public static class Url {
         public static final String IMAGE_URL = "https://ecs7.tokopedia.net/img/android/others/";
+
+        public class Pulsa {
+            public static final String BASE_PULSA_URL = "https://pulsa.tokopedia.com/";
+
+            public static final String PULSA_SUBSCRIBE = BASE_PULSA_URL + "subscribe/";
+
+            public static final String PULSA_FAV_NUMBER = BASE_PULSA_URL + "favorite-list/";
+        }
     }
 }
