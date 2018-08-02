82aa620a4bce22269b07623d46c532623c975426
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-08-02 14:58:12 +0700

    remove cache featrure product

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/presenter/ShopPagePresenter.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/page/view/presenter/ShopPagePresenter.kt
@@ -3,10 +3,7 @@ package com.tokopedia.shop.page.view.presenter
 import com.tokopedia.abstraction.base.view.presenter.BaseDaggerPresenter
 import com.tokopedia.abstraction.common.data.model.session.UserSession
 import com.tokopedia.abstraction.common.network.exception.UserNotLoginException
-import com.tokopedia.gm.common.domain.interactor.DeleteFeatureProductListCacheUseCase
-import com.tokopedia.reputation.common.data.source.cloud.model.ReputationSpeed
 import com.tokopedia.reputation.common.domain.interactor.DeleteReputationSpeedDailyCacheUseCase
-import com.tokopedia.reputation.common.domain.interactor.GetReputationSpeedUseCase
 import com.tokopedia.shop.common.data.source.cloud.model.ShopInfo
 import com.tokopedia.shop.common.domain.interactor.DeleteShopInfoUseCase
 import com.tokopedia.shop.common.domain.interactor.GetShopInfoByDomainUseCase
@@ -31,7 +28,6 @@ constructor(private val getShopInfoUseCase: GetShopInfoUseCase,
             private val getShopInfoByDomainUseCase: GetShopInfoByDomainUseCase,
             private val toggleFavouriteShopAndDeleteCacheUseCase: ToggleFavouriteShopAndDeleteCacheUseCase,
             private val deleteShopProductUseCase: DeleteShopProductUseCase,
-            private val deleteFeatureProductListCacheUseCase: DeleteFeatureProductListCacheUseCase,
             private val deleteShopInfoUseCase: DeleteShopInfoUseCase,
             private val deleteShopEtalaseUseCase: DeleteShopEtalaseUseCase,
             private val deleteShopNoteUseCase: DeleteShopNoteUseCase,
@@ -93,7 +89,6 @@ constructor(private val getShopInfoUseCase: GetShopInfoUseCase,
         deleteShopProductUseCase.executeSync()
         deleteShopEtalaseUseCase.executeSync()
         deleteShopNoteUseCase.executeSync()
-        deleteFeatureProductListCacheUseCase.executeSync()
         deleteReputationSpeedDailyUseCase.executeSync()
     }
 
@@ -106,7 +101,6 @@ constructor(private val getShopInfoUseCase: GetShopInfoUseCase,
         deleteShopProductUseCase.unsubscribe()
         deleteShopEtalaseUseCase.unsubscribe()
         deleteShopNoteUseCase.unsubscribe()
-        deleteFeatureProductListCacheUseCase.unsubscribe()
         deleteReputationSpeedDailyUseCase.unsubscribe()
     }
 }
