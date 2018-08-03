629f6ae709758c352847f603f72a279f88b72e8d
okasurya <brilliantsurya@gmail.com>
2018-08-03 17:38:45 +0700

    fix spacing account item

--- features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/fragment/AccountHomeFragment.java
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/fragment/AccountHomeFragment.java
@@ -89,24 +89,6 @@ public class AccountHomeFragment extends TkpdBaseV4Fragment implements
         return null;
     }
 
-//    @Override
-//    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//        inflater.inflate(R.menu.menu_account_home, menu);
-//        super.onCreateOptionsMenu(menu, inflater);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId() == R.id.menu_setting) {
-//            startActivity(GeneralSettingActivity.createIntent(getActivity()));
-//            return true;
-//        } else if (item.getItemId() == R.id.menu_notification) {
-//            Toast.makeText(getContext(), "Notif", Toast.LENGTH_LONG).show();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
     @Override
     public void renderData(AccountViewModel accountViewModel) {
         if(getContext() != null) {
@@ -173,6 +155,8 @@ public class AccountHomeFragment extends TkpdBaseV4Fragment implements
         adapter = new AccountHomePagerAdapter(getChildFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.label_account_buyer));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.label_account_seller));
     }
 
     private QBadgeView badgeView;
