7fcd5a639c94741ce46796b4748584974e845adc
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-08-07 12:16:05 +0700

    fix to always search etalase Use Ace from response

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/presenter/ShopProductListPresenter.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/presenter/ShopProductListPresenter.java
@@ -181,29 +181,27 @@ public class ShopProductListPresenter extends BaseDaggerPresenter<ShopProductDed
                     // So, if the selected name is empty, we know that the id might come from deeplink
                     // If the selectedName is not empty, we don't need to do find name anymore.
                     String selectedEtalaseName = getView().getSelectedEtalaseName();
-                    if (TextUtils.isEmpty(selectedEtalaseName)) { // name is empty, id might be from deeplink, or from other activity
+                    for (EtalaseModel etalaseModel : etalaseModelListTemp) {
+                        if (selectedEtalaseId.equalsIgnoreCase(etalaseModel.getEtalaseId())) {
+                            selectedEtalaseName = etalaseModel.getEtalaseName();
+                            shopProductRequestModel.setUseAce((etalaseModel.isUseAce()));
+                            break;
+                        }
+                    }
+                    // If etalase Id not found, then we check the selectedEtalaseId with name.
+                    if (TextUtils.isEmpty(selectedEtalaseName)) {
+                        String cleanedSelectedEtalaseId = cleanString(selectedEtalaseId);
                         for (EtalaseModel etalaseModel : etalaseModelListTemp) {
-                            if (selectedEtalaseId.equalsIgnoreCase(etalaseModel.getEtalaseId())) {
+                            String cleanedEtalaseName = cleanString(etalaseModel.getEtalaseName());
+                            if (cleanedSelectedEtalaseId.equalsIgnoreCase(cleanedEtalaseName)) {
+                                shopProductRequestModel.setEtalaseId(etalaseModel.getEtalaseId());
                                 selectedEtalaseName = etalaseModel.getEtalaseName();
                                 shopProductRequestModel.setUseAce((etalaseModel.isUseAce()));
                                 break;
                             }
                         }
-                        // If etalase Id not found, then we check the selectedEtalaseId with name.
                         if (TextUtils.isEmpty(selectedEtalaseName)) {
-                            String cleanedSelectedEtalaseId = cleanString(selectedEtalaseId);
-                            for (EtalaseModel etalaseModel : etalaseModelListTemp) {
-                                String cleanedEtalaseName = cleanString(etalaseModel.getEtalaseName());
-                                if (cleanedSelectedEtalaseId.equalsIgnoreCase(cleanedEtalaseName)) {
-                                    shopProductRequestModel.setEtalaseId(etalaseModel.getEtalaseId());
-                                    selectedEtalaseName = etalaseModel.getEtalaseName();
-                                    shopProductRequestModel.setUseAce((etalaseModel.isUseAce()));
-                                    break;
-                                }
-                            }
-                            if (TextUtils.isEmpty(selectedEtalaseName)) {
-                                shopProductRequestModel.setEtalaseId("");
-                            }
+                            shopProductRequestModel.setEtalaseId("");
                         }
                     }
 
