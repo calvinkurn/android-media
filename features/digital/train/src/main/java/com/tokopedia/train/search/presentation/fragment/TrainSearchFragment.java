efe2762bf1293690482644641e87f6e181463709
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-08-07 17:24:04 +0700

    delete unused import

--- features/digital/train/src/main/java/com/tokopedia/train/search/presentation/fragment/TrainSearchFragment.java
+++ features/digital/train/src/main/java/com/tokopedia/train/search/presentation/fragment/TrainSearchFragment.java
@@ -13,8 +13,6 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.github.rubensousa.bottomsheetbuilder.BottomSheetBuilder;
 import com.github.rubensousa.bottomsheetbuilder.adapter.BottomSheetItemClickListener;
