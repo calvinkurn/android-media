cd073ca2dc23be59da36f7bcad964eb1aa397ed5
ashwanityagi <ashwani.tyagi@tokopedia.com>
2018-08-08 19:00:13 +0530

    mu submissions list

---
+++ features/common/challenges/src/main/java/com/tokopedia/challenges/view/adapter/ChallengesListAdapter.java
@@ -0,0 +1,42 @@
+package com.tokopedia.challenges.view.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.tokopedia.challenges.view.model.Result;
+import com.tokopedia.challenges.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ashwanityagi on 07/08/18.
+ */
+
+public class ChallengesListAdapter extends RecyclerView.Adapter<ChallengesViewHolder> {
+    List<Result> challengesResultList= new ArrayList<>();
+    Context context;
+
+    public ChallengesListAdapter(Context context, List<Result> challengesResultList) {
+        this.context = context;
+        this.challengesResultList = challengesResultList;
+    }
+
+    @Override
+    public ChallengesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ChallengesViewHolder(LayoutInflater.from(context).inflate(R.layout.home_challenge_list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ChallengesViewHolder holder, int position) {
+        holder.bind(challengesResultList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return challengesResultList.size();
+    }
+
+}
