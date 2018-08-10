1ac9eddd0c7dfe1460d451cf671a02ae67be6228
sachinbansal91 <sachin.bansal@tokopedia.com>
2018-08-10 11:37:58 +0530

    bottom sheet view updated

--- features/user/updateinactivephone/src/main/java/com/tokopedia/updateinactivephone/design/view/UpdateInactivePhoneInfoBottomSheet.java
+++ features/user/updateinactivephone/src/main/java/com/tokopedia/updateinactivephone/design/view/UpdateInactivePhoneInfoBottomSheet.java
@@ -1,6 +1,8 @@
 package com.tokopedia.updateinactivephone.design.view;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ImageView;
@@ -20,4 +22,22 @@ public class UpdateInactivePhoneInfoBottomSheet extends BottomSheetView {
         return R.layout.bottom_sheet_info_layout;
     }
 
+    @Override
+    protected void init(Context context) {
+//        super.init(context);
+
+        if (context instanceof Activity) layoutInflater = ((Activity) context).getLayoutInflater();
+        else layoutInflater = ((Activity) ((ContextWrapper) context)
+                .getBaseContext()).getLayoutInflater();
+
+        bottomSheetView = layoutInflater.inflate(getLayout(), null);
+        setContentView(bottomSheetView);
+
+        bottomSheetView.findViewById(R.id.button_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
 }
