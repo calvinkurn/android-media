98be82b77f7c6c216bc5140712e669979705406a
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-08-14 08:26:44 +0700

    Fix ripple topads shop :lipstick:

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.java
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
 import android.support.design.widget.Snackbar;
@@ -171,7 +172,7 @@ public class FeedPlusFragment extends BaseDaggerFragment
 
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(FIRST_CURSOR, firstCursor);
     }
@@ -237,7 +238,7 @@ public class FeedPlusFragment extends BaseDaggerFragment
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
+    public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         setRetainInstance(true);
@@ -319,7 +320,7 @@ public class FeedPlusFragment extends BaseDaggerFragment
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
 
