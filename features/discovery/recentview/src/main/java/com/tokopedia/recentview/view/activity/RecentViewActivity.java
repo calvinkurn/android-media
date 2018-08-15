3ee70b24ca37e5ac9f133d4827dee74960459f3e
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-08-15 18:26:47 +0700

    Handle applink :sparkles:

--- features/discovery/recentview/src/main/java/com/tokopedia/recentview/view/activity/RecentViewActivity.java
+++ features/discovery/recentview/src/main/java/com/tokopedia/recentview/view/activity/RecentViewActivity.java
@@ -1,115 +1,48 @@
 package com.tokopedia.recentview.view.activity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.Toolbar;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
+import com.tokopedia.applink.ApplinkConst;
 import com.tokopedia.core.analytics.UnifyTracking;
-import com.tokopedia.core.app.BasePresenterActivity;
-import com.tokopedia.core.base.di.component.AppComponent;
-import com.tokopedia.core.base.di.component.HasComponent;
-import com.tokopedia.recentview.R;
 import com.tokopedia.recentview.view.fragment.RecentViewFragment;
 
 /**
  * @author by nisie on 7/4/17.
  */
 
-public class RecentViewActivity extends BasePresenterActivity implements HasComponent {
-    @Override
-    protected void setupURIPass(Uri data) {
+public class RecentViewActivity extends BaseSimpleActivity {
 
+    @DeepLink(ApplinkConst.RECENT_VIEW)
+    public static Intent getRecentViewApplinkIntent(Context context, Bundle extras) {
+        Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
+        return newInstance(context)
+                .setData(uri.build())
+                .putExtras(extras);
+    }
+
+    public static Intent newInstance(Context context) {
+        return new Intent(context, RecentViewActivity.class);
     }
 
     @Override
-    protected void setupBundlePass(Bundle extras) {
-
+    protected void onStart() {
+        super.onStart();
+        UnifyTracking.eventFeedViewAll();
     }
 
     @Override
-    protected void initialPresenter() {
-
-    }
-
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_simple_fragment;
-    }
-
-    @Override
-    protected void initView() {
-        setupToolbar();
+    protected Fragment getNewFragment() {
 
         Bundle bundle = new Bundle();
-        if (getIntent().getExtras() != null)
+        if (getIntent().getExtras() != null) {
             bundle.putAll(getIntent().getExtras());
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(RecentViewFragment.class.getSimpleName());
-        if (fragment == null) {
-            fragment = RecentViewFragment.createInstance(bundle);
         }
-        fragmentTransaction.replace(R.id.container,
-                fragment,
-                fragment.getClass().getSimpleName());
-        fragmentTransaction.commit();
-    }
-
-    @Override
-    protected boolean isLightToolbarThemes() {
-        return true;
-    }
-
-    @Override
-    public void setupToolbar() {
-        super.setupToolbar();
-
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
-                .getColor(R.color.white)));
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.grey_700));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            toolbar.setElevation(8);
-        }
-
-        Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_action_back);
-        if (upArrow != null) {
-            upArrow.setColorFilter(ContextCompat.getColor(this, R.color.grey_700), PorterDuff.Mode.SRC_ATOP);
-            getSupportActionBar().setHomeAsUpIndicator(upArrow);
-        }
-    }
-
-    @Override
-    protected void setViewListener() {
-
-    }
-
-    @Override
-    protected void initVar() {
-
-    }
-
-    @Override
-    protected void setActionVar() {
-
-    }
-
-    public static Intent getCallingIntent(FragmentActivity activity) {
-        UnifyTracking.eventFeedViewAll();
-        return new Intent(activity, RecentViewActivity.class);
-    }
-
-    @Override
-    public AppComponent getComponent() {
-        return getApplicationComponent();
+        return RecentViewFragment.createInstance(bundle);
     }
 }
