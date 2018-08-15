3cbbaf08ed69971593901cf644eae84f2c475dca
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2018-08-15 17:28:07 +0700

    bugfix lock edittext same as buyer in add new passenger

--- features/digital/train/src/main/java/com/tokopedia/train/passenger/presentation/fragment/TrainBookingPassengerFragment.java
+++ features/digital/train/src/main/java/com/tokopedia/train/passenger/presentation/fragment/TrainBookingPassengerFragment.java
@@ -279,7 +279,7 @@ public class TrainBookingPassengerFragment extends BaseDaggerFragment implements
         TrainBookingPassengerAdapterTypeFactory adapterTypeFactory = new TrainBookingPassengerAdapterTypeFactory(new TrainBookingPassengerAdapterListener() {
             @Override
             public void onChangePassengerData(TrainPassengerViewModel trainPassengerViewModel) {
-                startActivityForResult(TrainBookingAddPassengerActivity.callingIntent(getActivity(), trainPassengerViewModel, false), ADD_PASSENGER_REQUEST_CODE);
+                startActivityForResult(TrainBookingAddPassengerActivity.callingIntent(getActivity(), trainPassengerViewModel, sameAsBuyerCheckbox.isChecked()), ADD_PASSENGER_REQUEST_CODE);
             }
         });
         adapter = new TrainBookingPassengerAdapter(adapterTypeFactory, new ArrayList<Visitable>());
