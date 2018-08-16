03142d6b90f0490df5cb74b24e118eed15faa05f
Pranay Mohapatra <pranay.mohapatra@tokopedia.com>
2018-08-16 17:50:00 +0530

    Moved TouchImageAdapter and TouchImageView to Abstraction to remove dependency from core module

---
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/image/TouchImageAdapter.java
@@ -0,0 +1,67 @@
+package com.tokopedia.abstraction.common.utils.image;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tokopedia.abstraction.common.utils.view.TouchImageView;
+
+import java.util.ArrayList;
+
+public class TouchImageAdapter extends PagerAdapter {
+    public interface OnImageStateChange {
+        void OnStateDefault();
+
+        void OnStateZoom();
+    }
+
+    private Context context;
+    private int dp;
+    private ArrayList<String> FileLoc = new ArrayList<>();
+    private OnImageStateChange ImageStateChangeListener;
+
+    public TouchImageAdapter(Context context, ArrayList<String> FileLoc, int dp) {
+        this.context = context;
+        this.FileLoc = FileLoc;
+        this.dp = dp;
+    }
+
+    public void SetonImageStateChangeListener(OnImageStateChange Listener) {
+        ImageStateChangeListener = Listener;
+    }
+
+    @Override
+    public int getCount() {
+        return FileLoc.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        TouchImageView imageView = new TouchImageView(context, new TouchImageView.OnStateChange() {
+
+            @Override
+            public void OnStateChanged(float StateSize) {
+                // TODO Auto-generated method stub
+                if (StateSize <= 1)
+                    ImageStateChangeListener.OnStateDefault();
+                else
+                    ImageStateChangeListener.OnStateZoom();
+            }
+        });
+        ImageHandler.LoadImage(imageView, FileLoc.get(position));
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((TouchImageView) object);
+    }
+}
