e1ddc9e8429d1426ba6a431b2b82f8cf235c54eb
sachinbansal91 <sachin.bansal@tokopedia.com>
2018-08-18 09:42:53 +0530

    image crop ratios added

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/base/DiscoveryActivity.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/base/DiscoveryActivity.java
@@ -32,6 +32,7 @@ import com.tokopedia.imagepicker.picker.gallery.type.GalleryType;
 import com.tokopedia.imagepicker.picker.main.builder.ImagePickerBuilder;
 import com.tokopedia.imagepicker.picker.main.builder.ImagePickerEditorBuilder;
 import com.tokopedia.imagepicker.picker.main.builder.ImagePickerTabTypeDef;
+import com.tokopedia.imagepicker.picker.main.builder.ImageRatioTypeDef;
 import com.tokopedia.imagepicker.picker.main.view.ImagePickerActivity;
 
 import java.util.ArrayList;
@@ -420,10 +421,20 @@ public class DiscoveryActivity extends BaseDiscoveryActivity implements
     @Override
     public void onImageSearchClicked() {
 
+
+        ArrayList<ImageRatioTypeDef> imageRatioTypeDefArrayList = new ArrayList<>();
+
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.ORIGINAL);
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.RATIO_1_1);
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.RATIO_3_4);
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.RATIO_4_3);
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.RATIO_16_9);
+        imageRatioTypeDefArrayList.add(ImageRatioTypeDef.RATIO_9_16);
+
         ImagePickerEditorBuilder imagePickerEditorBuilder = new ImagePickerEditorBuilder
                 (new int[]{ACTION_CROP, ACTION_BRIGHTNESS, ACTION_CONTRAST},
                         false,
-                        null);
+                        imageRatioTypeDefArrayList);
 
         ImagePickerBuilder builder = new ImagePickerBuilder(getString(R.string.choose_image),
                 new int[]{ImagePickerTabTypeDef.TYPE_GALLERY, ImagePickerTabTypeDef.TYPE_CAMERA}, GalleryType.IMAGE_ONLY, ImagePickerBuilder.DEFAULT_MAX_IMAGE_SIZE_IN_KB,
