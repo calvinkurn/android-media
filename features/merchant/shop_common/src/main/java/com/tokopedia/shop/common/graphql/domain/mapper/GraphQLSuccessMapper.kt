028b24b7510c41b403db1e147d44a6ae741a0fbd
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-08-29 14:11:23 +0700

    convert usecase to kotlin

---
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/domain/mapper/GraphQLSuccessMapper.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.shop.common.graphql.domain.mapper
+
+import com.tokopedia.abstraction.common.network.exception.MessageErrorException
+import com.tokopedia.shop.common.graphql.data.GraphQLSuccessMessage
+
+import rx.Observable
+import rx.functions.Func1
+
+/**
+ * Created by hendry on 08/08/18.
+ */
+
+class GraphQLSuccessMapper : Func1<HasGraphQLSuccess, Observable<String>> {
+
+    override fun call(hasGraphQLSuccess: HasGraphQLSuccess): Observable<String> {
+        val graphQLSuccessMessage = hasGraphQLSuccess.graphQLSuccessMessage
+        return if (graphQLSuccessMessage.isSuccess) {
+            Observable.just(graphQLSuccessMessage.message)
+        } else {
+            Observable.error(MessageErrorException(graphQLSuccessMessage.message))
+        }
+    }
+}
