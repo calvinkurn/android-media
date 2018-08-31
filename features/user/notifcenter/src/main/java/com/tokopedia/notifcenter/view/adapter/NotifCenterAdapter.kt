b5712191109f00b385d243736f79870987164267
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-08-31 18:25:43 +0700

    Fix item margin bottom :lipstick:

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/view/adapter/NotifCenterAdapter.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/view/adapter/NotifCenterAdapter.kt
@@ -1,10 +1,6 @@
 package com.tokopedia.notifcenter.view.adapter
 
-import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseAdapter
-import com.tokopedia.abstraction.base.view.adapter.model.ErrorNetworkModel
-import com.tokopedia.abstraction.base.view.adapter.model.LoadingMoreModel
-import com.tokopedia.notifcenter.view.adapter.typefactory.NotifCenterTypeFactory
 import com.tokopedia.notifcenter.view.adapter.typefactory.NotifCenterTypeFactoryImpl
 
 /**
@@ -12,29 +8,4 @@ import com.tokopedia.notifcenter.view.adapter.typefactory.NotifCenterTypeFactory
  */
 
 class NotifCenterAdapter (typeFactory: NotifCenterTypeFactoryImpl)
-    : BaseAdapter<NotifCenterTypeFactoryImpl>(typeFactory)  {
-    private val list : ArrayList<Visitable<*>> = ArrayList()
-    private val loadingMoreModel: LoadingMoreModel = LoadingMoreModel()
-
-    lateinit var typeFactory : NotifCenterTypeFactory
-
-    fun addItem(visitable : Visitable<*>) {
-        val position = this.list.size
-        if (this.list.add(visitable)) {
-            notifyItemInserted(position)
-        }
-    }
-
-    fun removeItem(visitable : Visitable<*>) {
-        val position = this.list.indexOf(visitable)
-        if (this.list.remove(visitable)) {
-            notifyItemRemoved(position)
-        }
-    }
-
-    fun showError(message: String, onRetryListener: ErrorNetworkModel.OnRetryListener) {
-        errorNetworkModel.errorMessage = message
-        errorNetworkModel.onRetryListener = onRetryListener
-        addItem(errorNetworkModel)
-    }
-}
+    : BaseAdapter<NotifCenterTypeFactoryImpl>(typeFactory)
