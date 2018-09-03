05e33379c8475ecddfaccf959834e942f09356e7
Muhammad Furqan <mfurqan39@s1.informatika.unsyiah.ac.id>
2018-09-03 19:28:21 +0700

    Digital Browse HomePage DI

--- features/digital/digital_browse/src/main/java/com/tokopedia/browse/homepage/di/DigitalBrowseHomeComponent.java
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/homepage/di/DigitalBrowseHomeComponent.java
@@ -1,8 +1,8 @@
 package com.tokopedia.browse.homepage.di;
 
 import com.tokopedia.browse.common.di.DigitalBrowseComponent;
-import com.tokopedia.browse.homepage.presentation.fragment.DigitalBrowseBelanjaFragment;
-import com.tokopedia.browse.homepage.presentation.fragment.DigitalBrowseLayananFragment;
+import com.tokopedia.browse.homepage.presentation.fragment.DigitalBrowseMarketplaceFragment;
+import com.tokopedia.browse.homepage.presentation.fragment.DigitalBrowseServiceFragment;
 
 import dagger.Component;
 
@@ -14,8 +14,8 @@ import dagger.Component;
 @Component(modules = DigitalBrowseHomeModule.class, dependencies = DigitalBrowseComponent.class)
 public interface DigitalBrowseHomeComponent {
 
-    void inject(DigitalBrowseBelanjaFragment digitalBrowseBelanjaFragment);
+    void inject(DigitalBrowseMarketplaceFragment digitalBrowseMarketplaceFragment);
 
-    void inject(DigitalBrowseLayananFragment digitalBrowseLayananFragment);
+    void inject(DigitalBrowseServiceFragment digitalBrowseServiceFragment);
 
 }
