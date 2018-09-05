5d7d3a3da8f865036ea1f23c9b4cad128cfeca47
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2018-09-05 13:41:28 +0700

    set image url in search product list, hot list, category list to 700, set top ads product image url to m (300), set catalog list image to 300

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/catalog/adapter/CatalogAdapter.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/catalog/adapter/CatalogAdapter.java
@@ -184,4 +184,9 @@ public class CatalogAdapter extends SearchSectionGeneralAdapter {
     public boolean isEmptyItem(int position) {
         return checkDataSize(position) && getItemList().get(position) instanceof EmptySearchModel;
     }
+
+    @Override
+    public void changeSingleGridView() {
+        super.changeSingleGridView();
+    }
 }
--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/product/adapter/viewholder/BigGridProductItemViewHolder.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/product/adapter/viewholder/BigGridProductItemViewHolder.java
@@ -1,16 +1,36 @@
 package com.tokopedia.discovery.newdiscovery.search.fragment.product.adapter.viewholder;
 
+import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.tkpd.library.utils.ImageHandler;
 import com.tokopedia.discovery.R;
 import com.tokopedia.discovery.newdiscovery.search.fragment.product.adapter.listener.ItemClickListener;
+import com.tokopedia.discovery.newdiscovery.search.fragment.product.viewmodel.ProductItem;
 
 public class BigGridProductItemViewHolder extends GridProductItemViewHolder {
     @LayoutRes
     public static final int LAYOUT = R.layout.search_result_product_item_big_grid;
+    private final Context context;
+
+    private ImageView productImage;
+
 
     public BigGridProductItemViewHolder(View itemView, ItemClickListener itemClickListener) {
         super(itemView, itemClickListener);
+        productImage = (ImageView) itemView.findViewById(R.id.product_image);
+        context = itemView.getContext();
+    }
+
+    @Override
+    public void bind(ProductItem productItem) {
+        super.bind(productItem);
+    }
+
+    @Override
+    public void setImageProduct(ProductItem productItem) {
+        ImageHandler.loadImageSourceSize(context, productImage, productItem.getImageUrl700());
     }
 }
