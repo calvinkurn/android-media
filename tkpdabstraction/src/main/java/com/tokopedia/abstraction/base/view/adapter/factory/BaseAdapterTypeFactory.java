fa5cae32a45c7bf73c582834172fd15a6615479a
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-09-17 15:29:44 +0700

    fix java.lang.IndexOutOfBoundsException getItemViewType BaseAdapter

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/adapter/factory/BaseAdapterTypeFactory.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/adapter/factory/BaseAdapterTypeFactory.java
@@ -13,6 +13,7 @@ import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolde
 import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyResultViewHolder;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyViewHolder;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.ErrorNetworkViewHolder;
+import com.tokopedia.abstraction.base.view.adapter.viewholders.HideViewHolder;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.LoadingMoreViewHolder;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.LoadingViewholder;
 
@@ -66,7 +67,7 @@ public class BaseAdapterTypeFactory implements AdapterTypeFactory {
         } else if (type == EmptyViewHolder.LAYOUT) {
             creatViewHolder = new EmptyViewHolder(parent);
         } else {
-            throw TypeNotSupportedException.create("Layout not supported");
+            creatViewHolder = new HideViewHolder(parent);
         }
         return creatViewHolder;
     }
