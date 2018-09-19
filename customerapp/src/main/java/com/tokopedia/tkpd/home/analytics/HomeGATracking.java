b664056a472fee0090613b729d9bc9133ab9c47a
Abhay Raj Singh Yadav <abhay.yadav@tokopedia.com>
2018-09-19 14:48:59 +0530

    Tracking added in Youtube activity

--- customerapp/src/main/java/com/tokopedia/tkpd/home/analytics/HomeGATracking.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/analytics/HomeGATracking.java
@@ -10,13 +10,13 @@ public class HomeGATracking extends UnifyTracking {
     private static final String CATEGORY_VIDEO_PUSH = "video push";
     private static final String ACTION_VIDEO_NOTIFICATION_PAGE = "view video notification page";
     private static final String ACTION_CLICK_CTA_BUTTON = "click cta button";
-    private static final String LABEL_LANDING_SCREEN = "landing screenname";
+
 
     public static void eventYoutubeVideoImpression() {
         sendGTMEvent(new EventTracking(EVENT_VIDEO_VIEW,
                 CATEGORY_VIDEO_PUSH,
                 ACTION_VIDEO_NOTIFICATION_PAGE,
-                LABEL_LANDING_SCREEN)
+                "-")
                 .getEvent());
     }
 
