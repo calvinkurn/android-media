69408f2a2bbac93645615cda9f54bcec39f978d0
lavekush-t <lavekush.agrawal@tokopedia.com>
2018-09-20 12:03:54 +0530

    Updated with image icon reader

---
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/data/model/VccImageIcon.java
@@ -0,0 +1,20 @@
+package com.tokopedia.home.account.data.model;
+
+public class VccImageIcon {
+    String android;
+
+    public String getAndroid() {
+        return android;
+    }
+
+    public void setAndroid(String android) {
+        this.android = android;
+    }
+
+    @Override
+    public String toString() {
+        return "VccImageIcon{" +
+                "android='" + android + '\'' +
+                '}';
+    }
+}
