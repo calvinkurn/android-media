92a1d24ce19c70c1309dc36fb36e7a094742a18c
yfsx <yoas.fs@gmail.com>
2018-09-24 14:10:28 +0700

    remove unused class

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/network/AffiliateUrl.java
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.affiliate.common.network;
-
-/**
- * @author by nisie on 2/6/18.
- */
-
-public class AffiliateUrl {
-    public static String BASE_URL = "https://gql.tokopedia.com/";
-}
