2a6c154862e83ec21291e83ad87b8587456569db
stevenfredian <steven.fredian@tokopedia.com>
2018-09-24 11:45:23 +0700

    patch

--- features/user/talk/src/main/java/com/tokopedia/talk/producttalk/view/fragment/ProductTalkFragment.kt
+++ features/user/talk/src/main/java/com/tokopedia/talk/producttalk/view/fragment/ProductTalkFragment.kt
@@ -136,7 +136,7 @@ class ProductTalkFragment : BaseDaggerFragment(),
     }
 
     override fun onPrepareOptionsMenu(menu: Menu) {
-        menu.findItem(R.id.action_add).isVisible = !presenter.isMyShop(shopId)
+        menu.findItem(R.id.action_add).isVisible = !presenter.isMyShop(shopId) && presenter.isLoggedIn()
         super.onPrepareOptionsMenu(menu)
     }
 
@@ -297,6 +297,11 @@ class ProductTalkFragment : BaseDaggerFragment(),
     }
 
     private fun onMenuItemClicked(itemMenu: Menus.ItemMenus, bottomMenu: Menus, shopId: String, talkId: String, productId: String) {
+        if(presenter.isLoggedIn()){
+            goToLogin()
+            return;
+        }
+
         if (!::alertDialog.isInitialized) {
             alertDialog = Dialog(activity, Dialog.Type.PROMINANCE)
         }
