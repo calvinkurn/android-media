eadf0001d75f154c0fa323a12af557c966eabe1d
yfsx <yoas.fs@gmail.com>
2018-09-25 16:46:27 +0700

    fix explore

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/adapter/viewholder/ExploreViewHolder.java
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/adapter/viewholder/ExploreViewHolder.java
@@ -43,6 +43,7 @@ public class ExploreViewHolder extends AbstractViewHolder<ExploreViewModel> {
     }
 
     private void initView(ExploreViewModel element) {
+        ivImage.setMaxHeight(ivImage.getWidth());
         ImageHandler.loadImageRounded2(mainView.getContext(), ivImage, element.getImageUrl());
         tvTitle.setText(MethodChecker.fromHtml(element.getTitle()));
         tvCommission.setText(MethodChecker.fromHtml(element.getCommissionString()));
