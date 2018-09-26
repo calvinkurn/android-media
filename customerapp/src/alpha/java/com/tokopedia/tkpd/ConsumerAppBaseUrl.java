9a26c55dc63bca8773e9250d478d7fdddad697e6
Meyta <meytajennist@gmail.com>
2018-09-26 10:45:10 +0700

    upgrade build tools to 28.0.3 clean base url initialize instabug fix build buck

---
+++ customerapp/src/alpha/java/com/tokopedia/tkpd/ConsumerAppBaseUrl.java
@@ -0,0 +1,12 @@
+package com.tokopedia.tkpd;
+
+/**
+ * Created by hangnadi on 1/24/17.
+ */
+
+@SuppressWarnings("WeakerAccess")
+public class ConsumerAppBaseUrl {
+    public static final String LOGISTIC_BASE_DOMAIN = "https://gw-alpha.tokopedia.com/";
+    public static final String CART_BASE_DOMAIN = "https://api-alpha.tokopedia.com/";
+    public static final String TRANSACTION_BASE_DOMAIN = "https://ws-alpha.tokopedia.com/";
+}
---
+++ customerapp/src/prod/java/tkpd/InstabugInitalize.java
@@ -0,0 +1,13 @@
+package com.tokopedia.tkpd;
+
+import android.app.Application;
+
+/**
+ * Created by meta on 25/09/18.
+ */
+public class InstabugInitalize {
+
+    public static void init(Application application) {
+        // ignore
+    }
+}
