c06d388667ff61c051268469fe9898db03d52917
hadi-putra <s4putrh_dee@yahoo.com>
2018-09-26 15:42:27 +0700

    Add import viewgroup

--- tkpddesign/src/main/java/com/tokopedia/design/component/ticker/TickerView.java
+++ tkpddesign/src/main/java/com/tokopedia/design/component/ticker/TickerView.java
@@ -14,6 +14,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.tokopedia.design.R;
