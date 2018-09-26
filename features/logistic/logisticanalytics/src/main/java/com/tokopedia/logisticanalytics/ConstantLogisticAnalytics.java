896fa30d1b9884ce078f3e0a382b65dde6db1588
angga prasetiyo <agapras@gmail.com>
2018-09-26 10:45:14 +0700

    logistic analytics

---
+++ features/logistic/logisticanalytics/src/main/java/com/tokopedia/logisticanalytics/ConstantLogisticAnalytics.java
@@ -0,0 +1,31 @@
+package com.tokopedia.logisticanalytics;
+
+public interface ConstantLogisticAnalytics {
+
+    interface Key {
+        String EVENT = "event";
+        String EVENT_CATEGORY = "eventCategory";
+        String EVENT_ACTION = "eventAction";
+        String EVENT_LABEL = "eventLabel";
+        String E_COMMERCE = "ecommerce";
+    }
+    interface EventName {
+
+    }
+
+    interface EventCategory {
+
+    }
+
+    interface EventAction {
+
+    }
+
+    interface EventLabel {
+
+    }
+
+    interface ScreenName {
+
+    }
+}
