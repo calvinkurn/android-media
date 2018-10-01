925e3b403c38f25db8616ecce9f477280d44be88
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-10-01 14:43:06 +0700

    Add pojo model for booking code

---
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/detail/model/detail/response/OnlineBooking.java
@@ -0,0 +1,44 @@
+package com.tokopedia.transaction.purchase.detail.model.detail.response;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class OnlineBooking {
+
+    @SerializedName("booking_code")
+    @Expose
+    private String bookingCode;
+    @SerializedName("barcode_type")
+    @Expose
+    private String barcodeType;
+    @SerializedName("message")
+    @Expose
+    private List<String> message = null;
+
+    public String getBookingCode() {
+        return bookingCode;
+    }
+
+    public void setBookingCode(String bookingCode) {
+        this.bookingCode = bookingCode;
+    }
+
+    public String getBarcodeType() {
+        return barcodeType;
+    }
+
+    public void setBarcodeType(String barcodeType) {
+        this.barcodeType = barcodeType;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
+    }
+
+}
