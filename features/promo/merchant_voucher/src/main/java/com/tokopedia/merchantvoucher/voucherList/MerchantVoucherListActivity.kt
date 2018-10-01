e8cb20a5ee794457982770ef6d589e1ff307b7ba
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-10-01 20:00:42 +0700

    add shop common

--- features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherList/MerchantVoucherListActivity.kt
+++ features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherList/MerchantVoucherListActivity.kt
@@ -3,9 +3,14 @@ package com.tokopedia.merchantvoucher.voucherList
 import android.content.Context
 import android.content.Intent
 import android.support.v4.app.Fragment
+import android.view.Menu
+import android.view.MenuItem
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.merchantvoucher.MerchantVoucherModuleRouter
+import com.tokopedia.merchantvoucher.R
 import com.tokopedia.merchantvoucher.common.di.DaggerMerchantVoucherComponent
 import com.tokopedia.merchantvoucher.common.di.MerchantVoucherComponent
 
@@ -13,6 +18,9 @@ import com.tokopedia.merchantvoucher.common.di.MerchantVoucherComponent
  * Created by hendry on 21/09/18.
  */
 class MerchantVoucherListActivity : BaseSimpleActivity(), HasComponent<MerchantVoucherComponent> {
+
+//    var shopInfo: ShopInfo? = null
+
     override fun getComponent() = DaggerMerchantVoucherComponent.builder().baseAppComponent(
             (application as BaseMainApplication).getBaseAppComponent()).build()
 
@@ -20,6 +28,26 @@ class MerchantVoucherListActivity : BaseSimpleActivity(), HasComponent<MerchantV
 
     companion object {
         @JvmStatic
-        fun createIntent(context: Context) = Intent(context, MerchantVoucherListActivity::class.java)
+        fun createIntent(context: Context, shopId:String) = Intent(context, MerchantVoucherListActivity::class.java)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_share, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_share) {
+            onShareShop()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun onShareShop() {
+//        (application as MerchantVoucherModuleRouter).goToShareShop(this@MerchantVoucherListActivity,
+//                shopId, info.shopUrl, getString(R.string.shop_label_share_formatted,
+//                MethodChecker.fromHtml(info.shopName).toString(), info.shopLocation))
+
     }
 }
