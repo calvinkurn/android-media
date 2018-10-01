9122409c47487a57b6be36f3a271c33afe1f4d3f
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-10-01 13:49:34 +0700

    Add getter for locally stored image :construction:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/viewmodel/CreatePostViewModel.java
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/viewmodel/CreatePostViewModel.java
@@ -1,8 +1,11 @@
 package com.tokopedia.affiliate.feature.createpost.view.viewmodel;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class CreatePostViewModel {
+    private static final String FILE_PREFIX = "file:";
+
     private String pdpImage;
     private ArrayList<String> imageList;
 
@@ -18,13 +21,41 @@ public class CreatePostViewModel {
         this.imageList = imageList;
     }
 
+    /*
+    Get complete image list with PDP image in the end
+    */
     public ArrayList<String> getCompleteList() {
         ArrayList<String> completeList = new ArrayList<>(imageList);
         completeList.add(pdpImage);
         return completeList;
     }
 
+    /*
+    Get image list without PDP image
+    */
     public ArrayList<String> getImageList() {
         return imageList;
+    }
+
+    /*
+    Get only locally stored image, not from server
+    */
+    public ArrayList<String> getFileImageList() {
+        ArrayList<String> fileImageList = new ArrayList<>();
+        for (String image : imageList) {
+            if (urlIsFile(image)) {
+                fileImageList.add(image);
+            }
+        }
+        return fileImageList;
+    }
+
+    private static boolean urlIsFile(String input) {
+        if (input.startsWith(FILE_PREFIX)) return true;
+        try {
+            return new File(input).exists();
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
