38e006276bc71361517ecd7e3d644d5b68131bd6
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-10-03 15:10:12 +0700

    Implement analytics to consumerrouter

---
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/utils/OrderDetailConstant.java
@@ -0,0 +1,12 @@
+package com.tokopedia.transaction.purchase.utils;
+
+public class OrderDetailConstant {
+    public static final String EVENT = "event";
+    public static final String EVENT_CATEGORY = "eventCategory";
+    public static final String EVENT_ACTION = "eventAction";
+    public static final String EVENT_LABEL = "eventLabel";
+
+    public static final String VALUE_CLICK_ORDER = "clickOrder";
+    public static final String VALUE_SALES_SHIPPING = "sales shipping";
+
+}
