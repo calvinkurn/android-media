6bbfbe5af4b6597b4067aadd13a291753c3711a2
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-10-03 11:58:10 +0700

    remove HasComponent

--- features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherDetail/MerchantVoucherDetailActivity.kt
+++ features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherDetail/MerchantVoucherDetailActivity.kt
@@ -4,22 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
-import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.graphql.data.GraphqlClient
-import com.tokopedia.merchantvoucher.common.di.DaggerMerchantVoucherComponent
-import com.tokopedia.merchantvoucher.common.di.MerchantVoucherComponent
-import com.tokopedia.merchantvoucher.voucherList.MerchantVoucherListActivity
-import com.tokopedia.merchantvoucher.voucherList.MerchantVoucherListFragment
-import com.tokopedia.shop.common.data.source.cloud.model.ShopInfo
 
 /**
  * Created by hendry on 21/09/18.
  */
-class MerchantVoucherDetailActivity : BaseSimpleActivity(), HasComponent<MerchantVoucherComponent> {
-    override fun getComponent() = DaggerMerchantVoucherComponent.builder().baseAppComponent(
-            (application as BaseMainApplication).getBaseAppComponent()).build()
+class MerchantVoucherDetailActivity : BaseSimpleActivity() {
 
     var voucherId: Int = 0
 
