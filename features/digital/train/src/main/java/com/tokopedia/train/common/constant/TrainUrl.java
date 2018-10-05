72a1978f1ae28f76169ac7e5023a80f0e4b6f9d6
fatahillah alvarisi <alvarisi01@gmail.com>
2018-10-05 21:16:19 +0700

    help url

--- features/digital/train/src/main/java/com/tokopedia/train/common/constant/TrainUrl.java
+++ features/digital/train/src/main/java/com/tokopedia/train/common/constant/TrainUrl.java
@@ -13,12 +13,12 @@ public class TrainUrl {
 
     public static String BASE_URL = "http://gql-staging.tokopedia.com";
     public static String WEB_DOMAIN = "https://tiket.tokopedia.com/";
-    public static String BASE_WEB_DOMAIN = "https://tokopedia.com/";
+    public static String BASE_WEB_DOMAIN = "https://www.tokopedia.com/";
     public static String KAI_WEBVIEW = WEB_DOMAIN + "kereta-api";
 
     private static String PATH_USER_BOOKING_LIST = "/user/bookings";
     private static String PARAM_DIGITAL_ISPULSA = "?ispulsa=1";
 
     public static String TRAIN_ORDER_LIST = KAI_WEBVIEW + PATH_USER_BOOKING_LIST + PARAM_DIGITAL_ISPULSA;
-    public static String HELP_PAGE = BASE_WEB_DOMAIN + "contact-us#step1";
+    public static String HELP_PAGE = BASE_WEB_DOMAIN + "contact-us?flag_app=1";
 }
