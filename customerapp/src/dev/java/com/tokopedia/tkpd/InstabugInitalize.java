752807f86797062468bfb2d2f95effd2d2882f11
okasurya <brilliantsurya@gmail.com>
2018-10-09 19:20:54 +0700

    fix split on playstore build and crash on instabug

--- customerapp/src/dev/java/com/tokopedia/tkpd/InstabugInitalize.java
+++ customerapp/src/dev/java/com/tokopedia/tkpd/InstabugInitalize.java
@@ -2,6 +2,7 @@ package com.tokopedia.tkpd;
 
 import android.app.Activity;
 import android.app.Application;
+import android.os.Build;
 import android.view.MotionEvent;
 
 import com.instabug.library.Instabug;
@@ -40,7 +41,9 @@ public class InstabugInitalize {
         //setting user attributes
         Instabug.setUserAttribute("USER_TYPE", "instabug user");
 
-        Instabug.setAutoScreenRecordingEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Instabug.setAutoScreenRecordingEnabled(true);
+        }
     }
 
     public static void dispatchTouchEvent(Activity context, MotionEvent ev) {
