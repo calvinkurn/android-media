31ee0004db2f0650554ae10354e9518da5f73592
nisie <nathania.sutedja@tokopedia.com>
2018-10-10 14:46:37 +0700

    Fix tracking register

--- tkpdcore/src/main/java/com/tokopedia/core/analytics/UnifyTracking.java
+++ tkpdcore/src/main/java/com/tokopedia/core/analytics/UnifyTracking.java
@@ -984,42 +984,6 @@ public class UnifyTracking extends TrackingUtils {
         ).getEvent());
     }
 
-    public static void eventRegisterSuccess(String label) {
-        sendGTMEvent(new EventTracking(
-                AppEventTracking.Event.REGISTER_SUCCESS,
-                AppEventTracking.Category.REGISTER,
-                AppEventTracking.Action.REGISTER_SUCCESS,
-                label
-        ).getEvent());
-    }
-
-    public static void eventLoginSuccess(String label) {
-        sendGTMEvent(new EventTracking(
-                AppEventTracking.Event.LOGIN,
-                AppEventTracking.Category.LOGIN,
-                AppEventTracking.Action.LOGIN_SUCCESS,
-                label
-        ).getEvent());
-    }
-
-    public static void eventRegister(String label) {
-        sendGTMEvent(new EventTracking(
-                AppEventTracking.Event.REGISTER,
-                AppEventTracking.Category.REGISTER,
-                AppEventTracking.Action.CLICK,
-                label
-        ).getEvent());
-    }
-
-    public static void eventRegisterChannel(String label) {
-        sendGTMEvent(new EventTracking(
-                AppEventTracking.Event.REGISTER,
-                AppEventTracking.Category.REGISTER,
-                AppEventTracking.Action.CLICK_CHANNEL,
-                label
-        ).getEvent());
-    }
-
     public static void eventRegisterError(String label) {
         sendGTMEvent(new EventTracking(
                 AppEventTracking.Event.REGISTER_ERROR,
@@ -1305,15 +1269,6 @@ public class UnifyTracking extends TrackingUtils {
                 AppEventTracking.Category.OTP,
                 AppEventTracking.Action.CLICK,
                 AppEventTracking.EventLabel.VERIFICATION
-        ).getEvent());
-    }
-
-    public static void eventRegisterThroughLogin() {
-        sendGTMEvent(new EventTracking(
-                AppEventTracking.Event.REGISTER_LOGIN,
-                AppEventTracking.Category.LOGIN,
-                AppEventTracking.Action.REGISTER,
-                AppEventTracking.EventLabel.REGISTER
         ).getEvent());
     }
 
