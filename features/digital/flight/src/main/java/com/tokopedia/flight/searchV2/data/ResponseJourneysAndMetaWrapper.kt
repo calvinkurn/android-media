0cc05f28f95dbbc05d3f838b5f74aeb3cfb2b3db
Muhammad Furqan <nanjihusaha@gmail.com>
2018-10-12 17:30:51 +0700

    Merge branch 'flight_search_v2' of https://github.com/rizkyfadillah/android-tokopedia-core into flight_gds
    # Conflicts:
    #	features/digital/flight/src/main/java/com/tokopedia/flight/searchV2/data/repository/FlightSearchRepository.kt

--- features/digital/flight/src/main/java/com/tokopedia/flight/searchV2/data/ResponseJourneysAndMetaWrapper.kt
+++ features/digital/flight/src/main/java/com/tokopedia/flight/searchV2/data/ResponseJourneysAndMetaWrapper.kt
@@ -3,7 +3,6 @@ package com.tokopedia.flight.searchV2.data
 import com.tokopedia.flight.search.data.cloud.model.response.FlightDataResponse
 import com.tokopedia.flight.search.data.cloud.model.response.FlightSearchData
 import com.tokopedia.flight.search.data.cloud.model.response.Meta
-import com.tokopedia.flight.searchV2.data.db.JourneyAndRoutes
 
 /**
  * Created by Rizky on 01/10/18.
