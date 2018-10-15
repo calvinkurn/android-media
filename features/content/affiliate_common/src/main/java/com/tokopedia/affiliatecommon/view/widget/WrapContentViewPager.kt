d067709137f3ec15dce7d0f0c2e2bf3ff488c6d9
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-10-15 15:49:44 +0700

    Fix ration 1:1 for view page :lipstick:

--- features/content/affiliate_common/src/main/java/com/tokopedia/affiliatecommon/view/widget/WrapContentViewPager.kt
+++ features/content/affiliate_common/src/main/java/com/tokopedia/affiliatecommon/view/widget/WrapContentViewPager.kt
@@ -2,7 +2,6 @@ package com.tokopedia.affiliatecommon.view.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
 import com.tokopedia.abstraction.base.view.widget.TouchViewPager
 
 /**
@@ -14,20 +13,17 @@ class WrapContentViewPager : TouchViewPager {
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        var height = 0
-        for (i in 0 until childCount) {
-            val child = getChildAt(i)
-            child.measure(
-                    widthMeasureSpec,
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-            )
-            val h = child.measuredHeight
-            if (h > height) height = h
-        }
+//        var height = 0
+//        for (i in 0 until childCount) {
+//            val child = getChildAt(i)
+//            child.measure(
+//                    widthMeasureSpec,
+//                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+//            )
+//            val h = child.measuredHeight
+//            if (h > height) height = h
+//        }
 
-        super.onMeasure(
-                widthMeasureSpec,
-                View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY)
-        )
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
     }
 }
