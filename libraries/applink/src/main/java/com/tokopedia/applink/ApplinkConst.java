8b84488fa055458090f6b967ced24e7f54f27b70
yogieputra8 <yogie.putra8@gmail.com>
2018-10-16 13:23:56 +0700

    add applink category os

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -64,6 +64,7 @@ public interface ApplinkConst {
     String TOPPICK_DETAIL = "tokopedia://toppicks/{toppick_id}";
     String LOGIN = "tokopedia://login";
     String OFFICIAL_STORES = "tokopedia://official-stores";
+    String OFFICIAL_STORES_CATEGORY = "tokopedia://official-stores/{key_category}";
     String RESCENTER = "tokopedia://resolution/{resolution_id}";
     String RESCENTER_BUYER = "tokopedia://resolution/list/buyer";
     String RESCENTER_SELLER = "tokopedia://resolution/list/seller";
