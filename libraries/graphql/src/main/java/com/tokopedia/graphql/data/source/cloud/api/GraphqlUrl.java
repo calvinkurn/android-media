a8e39358102c2255865246ae278d223bdd4c1dfd
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2018-10-16 10:54:22 +0700

    revert graphqlUrl

--- libraries/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlUrl.java
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlUrl.java
@@ -6,7 +6,5 @@ import com.tokopedia.network.constant.TkpdBaseURL;
  * @author by yfsx on 19/07/18.
  */
 public class GraphqlUrl {
-    //staging
-    public static String BASE_URL = "https://gql-staging.tokopedia.com/";
-//    public static String BASE_URL = TkpdBaseURL.HOME_DATA_BASE_URL;
+    public static String BASE_URL = TkpdBaseURL.HOME_DATA_BASE_URL;
 }
