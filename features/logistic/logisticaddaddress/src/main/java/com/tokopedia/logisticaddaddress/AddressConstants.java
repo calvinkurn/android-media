b8b23c996ddbdd699014d8422f18bf5434dc0930
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-10-17 09:53:47 +0700

    Refactor constants

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/AddressConstants.java
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/AddressConstants.java
@@ -10,4 +10,24 @@ public class AddressConstants {
     // TODO: ?? supposed to be "People address"
     public static final String SCREEN_MANAGE_ADDRESS = "People Edit Address";
 
+    public static final int REQUEST_CODE = 0x12;
+    public static final int ADD_ADDRESS_REQUESTCODE = 0x1;
+    public static final String IS_DISTRICT_RECOMMENDATION = "district_recommendation";
+    public static final String EXTRA_PLATFORM_PAGE = "EXTRA_PLATFORM_PAGE";
+    public static final String PLATFORM_MARKETPLACE_CART = "PLATFORM_MARKETPLACE_CART";
+    public static final String EXTRA_FROM_CART_IS_EMPTY_ADDRESS_FIRST = "EXTRA_FROM_CART_IS_EMPTY_ADDRESS_FIRST";
+    public static final String IS_EDIT = "is_edit";
+    public static final String EDIT_PARAM = "EDIT_PARAM";
+    public static final int REQUEST_CODE_PARAM_CREATE = 101;
+    public static final int REQUEST_CODE_PARAM_EDIT = 102;
+    public static final int RESULT_ERROR = 999;
+
+    public static final int GET_LIST_PROVINCE = 1;
+    public static final int GET_LIST_DISTRICT = 2;
+    public static final int GET_LIST_CITY = 3;
+
+    public static final String EXTRA_ADDRESS = "EXTRA_ADDRESS";
+
+    public static final String KERO_TOKEN = "token";
+
 }
