be924331c06129bedf44e802df5684386d454893
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-10-17 18:08:08 +0700

    Set topads base url for staging :construction:

--- features/content/profile/src/main/java/com/tokopedia/profile/data/network/TopAdsConstant.kt
+++ features/content/profile/src/main/java/com/tokopedia/profile/data/network/TopAdsConstant.kt
@@ -4,5 +4,5 @@ package com.tokopedia.profile.data.network
  * @author by milhamj on 10/17/18.
  */
 
-const val TOPADS_BASE_URL = "https://ta-staging.tokopedia.com/"
+var TOPADS_BASE_URL = "https://ta.tokopedia.com/"
 const val AFFILIATE_TRACKER = "affiliate/v1/tracker/track"
