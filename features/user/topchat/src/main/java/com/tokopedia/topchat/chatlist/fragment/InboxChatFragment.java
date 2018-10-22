73acbb609dc4642230c8f3b0796f0e331f1dde32
hadi <hadi.saputro@tokopedia.com>
2018-10-22 11:45:53 +0700

    hotfix https://phab.tokopedia.com/project/view/2822/

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/fragment/InboxChatFragment.java
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/fragment/InboxChatFragment.java
@@ -163,7 +163,6 @@ public class InboxChatFragment extends BaseDaggerFragment
         notifier = parentView.findViewById(R.id.notifier);
         sendBroadcast = parentView.findViewById(R.id.tv_bm_action);
         if (GlobalConfig.isSellerApp()){
-            sendBroadcast.setVisibility(View.VISIBLE);
             sendBroadcast.setOnClickListener(v -> {
                 if (getActivity().getApplication() instanceof BroadcastMessageRouter && getContext() != null){
                     startActivity(((BroadcastMessageRouter) getActivity().getApplication()).getBroadcastMessageListIntent(getContext()));
