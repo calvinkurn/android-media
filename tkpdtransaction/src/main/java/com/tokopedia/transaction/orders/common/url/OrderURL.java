5d016427f71fee57c18bc34e5879085dcf3f9829
sandeepnitsgr <sandeep.gupta@tokopedia.com>
2018-10-25 12:41:22 +0530

    Merge remote-tracking branch 'upstream/release' into hotfix_remove_core_orderlist

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/common/url/OrderURL.java
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.transaction.orders.common.url;
-
-/**
- * Created by sandeepgoyal on 15/12/17.
- */
-
-public interface OrderURL {
-    String BASE_URL = "https://gql.tokopedia.com/";
-}
