bfb3cbcbf7809c66c2a64a90de4366eac8b841fc
naveengoyal87 <naveen.goyal@tokopedia.com>
2018-10-26 18:35:52 +0530

    Changes as per testing comments

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/util/MoEngageEventTracking.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/util/MoEngageEventTracking.java
@@ -16,12 +16,18 @@ public class MoEngageEventTracking {
     public static void sendCategory(String categoryId, String categoryName) {
         CATEGORY_ID = categoryId;
         CATEGORY = categoryName;
+        SUBCATEGORY = "";
+        SUBCATEGORY_ID ="";
+        PRODUCT_NAME = "";
+        PRODUCT_ID = "";
         TrackingUtils.sendMoEngageCategoryEvent(CATEGORY_ID, CATEGORY, SUBCATEGORY_ID, SUBCATEGORY, PRODUCT_ID, PRODUCT_NAME);
     }
 
     public static void sendSubCategory(String subCategoryId, String subCategoryName) {
         SUBCATEGORY_ID = subCategoryId;
         SUBCATEGORY = subCategoryName;
+        PRODUCT_ID = "";
+        PRODUCT_NAME = "";
         TrackingUtils.sendMoEngageCategoryEvent(CATEGORY_ID, CATEGORY, SUBCATEGORY_ID, SUBCATEGORY, PRODUCT_ID, PRODUCT_NAME);
     }
 
