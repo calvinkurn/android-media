5bf6bc7707f075d43bc8fa2602a64642908a3c7c
irfan khoirul <irfankhoirul@gmail.com>
2018-10-26 19:59:16 +0700

    Force check promo code

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentFragment.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentFragment.java
@@ -1504,11 +1504,11 @@ public class ShipmentFragment extends BaseCheckoutFragment implements ShipmentCo
 
     private void checkCourierPromo(CourierItemData courierItemData, int itemPosition) {
         if (isToogleYearEndPromoOn() && !TextUtils.isEmpty(courierItemData.getPromoCode())) {
-            if (checkCourierPromoStillExist()) {
-                setCourierPromoApplied(itemPosition);
-            } else {
+//            if (checkCourierPromoStillExist()) {
+//                setCourierPromoApplied(itemPosition);
+//            } else {
                 shipmentPresenter.processCheckPromoCodeFromSelectedCourier(courierItemData.getPromoCode(), itemPosition);
-            }
+//            }
         }
     }
 
