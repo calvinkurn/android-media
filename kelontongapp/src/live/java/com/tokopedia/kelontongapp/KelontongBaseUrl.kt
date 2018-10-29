71dfd2f8d7b396de2412a867343658fc8b3bf701
Meyta <meytajennist@gmail.com>
2018-10-29 16:27:30 +0700

    Add flavor

---
+++ kelontongapp/src/live/java/com/tokopedia/kelontongapp/KelontongBaseUrl.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.kelontongapp
+
+/**
+ * Created by meta on 12/10/18.
+ */
+object KelontongBaseUrl {
+    val BASE_URL = "https://mitra-staging.tokopedia.com/"
+    val COOKIE_URL = ".tokopedia.com"
+    val TOKOPEDIA_URL = "tokopedia.com"
+}
