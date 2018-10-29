cf2a280fe7333ed853a81001227dc8efee0d4964
sachinbansal <sachin.bansal@tokopedia.com>
2018-10-29 10:27:51 +0530

    saldo events added.

---
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/commom/analytics/SaldoDetailsAnalytics.java
@@ -0,0 +1,33 @@
+package com.tokopedia.saldodetails.commom.analytics;
+
+import android.content.Context;
+
+import com.tokopedia.abstraction.AbstractionRouter;
+import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker;
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+
+import javax.inject.Inject;
+
+public class SaldoDetailsAnalytics {
+
+    private AnalyticTracker tracker;
+
+    @Inject
+    public SaldoDetailsAnalytics(@ApplicationContext Context context) {
+        if (context != null && context.getApplicationContext() instanceof AbstractionRouter) {
+            tracker = ((AbstractionRouter) context.getApplicationContext()).getAnalyticTracker();
+        }
+    }
+
+    public void eventAnchorLabelClick(String eventAction) {
+        if (tracker == null) {
+            return;
+        }
+        tracker.sendEventTracking(
+                SaldoDetailsConstants.Event.EVENT_CLICK_FINTECH_MICROSITE,
+                SaldoDetailsConstants.Category.SALDO_MAIN_SCREEN,
+                eventAction,
+                SaldoDetailsConstants.EventLabel.SALDO_PAGE
+        );
+    }
+}
