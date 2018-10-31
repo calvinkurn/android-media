605e86977a472a040b0fb471454525341d245f2d
eriksuprayogi <eriksuprayogi@gmail.com>
2018-10-31 14:59:57 +0700

    remove unused code

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/BannerViewHolder.java
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/BannerViewHolder.java
@@ -95,21 +95,6 @@ public class BannerViewHolder extends AbstractViewHolder<BannerViewModel> implem
         }
     }
 
-    private boolean isBannerVisible() {
-        if (bannerView == null) {
-            return false;
-        }
-        if (!bannerView.isShown()) {
-            return false;
-        }
-        Rect actualPosition = new Rect();
-        bannerView.getGlobalVisibleRect(actualPosition);
-        Rect screen = new Rect(0, 0,
-                Resources.getSystem().getDisplayMetrics().widthPixels,
-                Resources.getSystem().getDisplayMetrics().heightPixels);
-        return actualPosition.intersect(screen);
-    }
-
     @Override
     public void onPromoAllClick() {
         HomePageTracking.eventClickViewAllPromo();
