0c10c5a8fee09a106ea6b79027c07003e0de5763
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-11-02 20:18:02 +0700

    Fix toolbar not back

--- features/logistic/logisticgeolocation/src/main/java/com/tokopedia/logisticgeolocation/GeolocationActivity.java
+++ features/logistic/logisticgeolocation/src/main/java/com/tokopedia/logisticgeolocation/GeolocationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.tokopedia.abstraction.AbstractionRouter;
@@ -86,6 +87,16 @@ public class GeolocationActivity extends BaseActivity implements ITransactionAna
     public void onBackPressed() {
         super.onBackPressed();
         sendAnalyticsOnBackPressClicked();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
