1df566d546fb45f681d0765c2241fa8462e6ed3d
yogieputra8 <yogie.putra8@gmail.com>
2018-11-07 14:40:18 +0700

    remove unused import

--- libraries/react_native/src/main/java/com/tokopedia/tkpdreactnative/react/app/ReactFragmentActivity.java
+++ libraries/react_native/src/main/java/com/tokopedia/tkpdreactnative/react/app/ReactFragmentActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
 
 import com.facebook.react.ReactApplication;
