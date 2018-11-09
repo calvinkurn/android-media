65a12775f921396626c7554fc7a3483c593c39d7
irfan khoirul <irfankhoirul@gmail.com>
2018-11-09 19:15:49 +0700

    Set flag to false

--- tkpdcore/src/main/java/com/tokopedia/core/product/model/productdetail/ProductDetailData.java
+++ tkpdcore/src/main/java/com/tokopedia/core/product/model/productdetail/ProductDetailData.java
@@ -178,7 +178,7 @@ public class ProductDetailData implements Parcelable{
     }
 
     public boolean isBigPromo() {
-        return true;
+        return false;
     }
 
     public void setBigPromo(boolean bigPromo) {
