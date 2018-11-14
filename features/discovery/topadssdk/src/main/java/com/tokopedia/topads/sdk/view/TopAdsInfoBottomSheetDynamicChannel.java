c0e320cb4503de10621024d2de121d528bddc3d0
Erry Suprayogi <eriksuprayogi@gmail.com>
2018-11-14 14:07:39 +0700

    upload the image to ecs.

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/view/TopAdsInfoBottomSheetDynamicChannel.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/view/TopAdsInfoBottomSheetDynamicChannel.java
@@ -8,7 +8,10 @@ import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.tokopedia.design.image.ImageLoader;
 import com.tokopedia.topads.sdk.R;
 
 
@@ -18,12 +21,14 @@ import com.tokopedia.topads.sdk.R;
 
 public class TopAdsInfoBottomSheetDynamicChannel {
 
+    public static final String IMAGE_URL = "https://ecs7.tokopedia.net/img/android/others/promo_image.jpeg";
     public static String ADS_URL = "https://www.tokopedia.com/iklan?source=tooltip&medium=android";
 
     public static final String TAG = "TAIBS";
     private BottomSheetDialog dialog;
     private View closeButton;
     private View moreButton;
+    private ImageView promoImage;
 
     public TopAdsInfoBottomSheetDynamicChannel() {
     }
@@ -34,11 +39,13 @@ public class TopAdsInfoBottomSheetDynamicChannel {
         frag.dialog.setContentView(R.layout.promoted_info_dialog_dynamic_channel);
         frag.closeButton = frag.dialog.findViewById(R.id.close_but);
         frag.moreButton = frag.dialog.findViewById(R.id.more);
+        frag.promoImage = frag.promoImage.findViewById(R.id.promo_image);
         frag.setView(context);
         return frag;
     }
 
     public void setView(final Context context) {
+        ImageLoader.LoadImage(promoImage, IMAGE_URL);
         dialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
             public void onShow(DialogInterface dialogInterface) {
