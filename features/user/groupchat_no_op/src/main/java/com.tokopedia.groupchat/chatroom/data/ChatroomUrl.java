a0152902c3a01bcc63b7c38cd6889c6af86aa367
stevenfredian <steven.fredian@tokopedia.com>
2018-11-15 15:45:10 +0700

    patch

--- features/user/groupchat_no_op/src/main/java/com.tokopedia.groupchat/chatroom/data/ChatroomUrl.java
+++ features/user/groupchat_no_op/src/main/java/com.tokopedia.groupchat/chatroom/data/ChatroomUrl.java
@@ -4,4 +4,5 @@ package com.tokopedia.groupchat.chatroom.data;
  * @author : Steven 13/11/18
  */
 public class ChatroomUrl {
+    public static final String DESKTOP_URL = "";
 }
--- features/user/groupchat_no_op/src/main/java/com.tokopedia.groupchat/common/data/GroupChatUrl.java
+++ features/user/groupchat_no_op/src/main/java/com.tokopedia.groupchat/common/data/GroupChatUrl.java
@@ -4,4 +4,5 @@ package com.tokopedia.groupchat.common.data;
  * @author : Steven 13/11/18
  */
 public class GroupChatUrl {
+    public static String BASE_URL;
 }
