783c0aa8cc7f35931041731249da6e9c1af531d2
Erry Suprayogi <eriksuprayogi@gmail.com>
2018-11-16 11:09:38 +0700

    add topads carousel viewholder

--- customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/WishListProductAdapter.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/WishListProductAdapter.java
@@ -19,8 +19,10 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.tkpd.library.utils.ImageHandler;
 import com.tokopedia.core.analytics.UnifyTracking;
+import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.customadapter.BaseRecyclerViewAdapter;
 import com.tokopedia.core.customwidget.FlowLayout;
 import com.tokopedia.core.gcm.GCMHandler;
@@ -41,17 +43,25 @@ import com.tokopedia.tkpd.home.wishlist.analytics.WishlistAnalytics;
 import com.tokopedia.tkpdpdp.ProductInfoActivity;
 import com.tokopedia.topads.sdk.base.Config;
 import com.tokopedia.topads.sdk.base.Endpoint;
+import com.tokopedia.topads.sdk.base.adapter.Item;
 import com.tokopedia.topads.sdk.domain.TopAdsParams;
+import com.tokopedia.topads.sdk.domain.Xparams;
 import com.tokopedia.topads.sdk.domain.model.Data;
 import com.tokopedia.topads.sdk.domain.model.Product;
 import com.tokopedia.topads.sdk.domain.model.Shop;
 import com.tokopedia.topads.sdk.listener.TopAdsItemClickListener;
+import com.tokopedia.topads.sdk.listener.TopAdsListener;
 import com.tokopedia.topads.sdk.view.DisplayMode;
+import com.tokopedia.topads.sdk.widget.TopAdsCarouselView;
 import com.tokopedia.topads.sdk.widget.TopAdsView;
+import com.tokopedia.user.session.UserSession;
 
 import java.util.List;
 
 import butterknife.ButterKnife;
+
+import static com.tokopedia.topads.sdk.domain.TopAdsParams.DEFAULT_KEY_EP;
+import static com.tokopedia.topads.sdk.domain.TopAdsParams.SRC_PDP_VALUE;
 
 /**
  * Created by Nisie on 16/06/15.
@@ -145,10 +155,71 @@ public class WishListProductAdapter extends BaseRecyclerViewAdapter {
         }
     }
 
-    public static class WishListTopAdsViewHolder extends RecyclerView.ViewHolder {
+    public static class WishListTopAdsViewHolder extends RecyclerView.ViewHolder
+            implements TopAdsListener, TopAdsItemClickListener {
+
+        private TopAdsCarouselView topAdsCarouselView;
+        private UserSession userSession;
 
         public WishListTopAdsViewHolder(View itemView) {
             super(itemView);
+            topAdsCarouselView = itemView.findViewById(R.id.topads);
+            userSession = new UserSession(itemView.getContext());
+            renderTopAds();
+        }
+
+        private void renderTopAds(){
+            Xparams xparams = new Xparams();
+            xparams.setProduct_id(12007464);
+            xparams.setProduct_name("Original Baterai Samsung Galaxy Mini GT-S5570 1200mAh");
+            xparams.setSource_shop_id(415979);
+
+            TopAdsParams params = new TopAdsParams();
+            params.getParam().put(TopAdsParams.KEY_SRC, "pdp");
+            params.getParam().put(TopAdsParams.KEY_EP, DEFAULT_KEY_EP);
+            params.getParam().put(TopAdsParams.KEY_ITEM, String.valueOf(5));
+            params.getParam().put(TopAdsParams.KEY_XPARAMS, new Gson().toJson(xparams));
+
+            Config config = new Config.Builder()
+                    .setSessionId(GCMHandler.getRegistrationId(MainApplication.getAppContext()))
+                    .setUserId(userSession.getUserId())
+                    .topAdsParams(params)
+                    .build();
+
+            topAdsCarouselView.setAdsItemClickListener(this);
+            topAdsCarouselView.setAdsListener(this);
+            topAdsCarouselView.setConfig(config);
+            topAdsCarouselView.loadTopAds();
+        }
+
+        @Override
+        public void onProductItemClicked(int position, Product product) {
+
+        }
+
+        @Override
+        public void onShopItemClicked(int position, Shop shop) {
+
+        }
+
+        @Override
+        public void onAddFavorite(int position, Data data) {
+
+        }
+
+        @Override
+        public void onAddWishList(int position, Data data) {
+
+        }
+
+        @Override
+        public void onTopAdsLoaded(List<Item> list) {
+
+        }
+
+        @Override
+        public void onTopAdsFailToLoad(int errorCode, String message) {
+
         }
     }
 
