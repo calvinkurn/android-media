47ea4fe61457b866e04439e501b887a16351a870
irfan khoirul <irfankhoirul@gmail.com>
2018-11-16 18:16:44 +0700

    Remove core data layer district recommendation

--- features/logistic/district_recommendation/src/main/java/com/tokopedia/district_recommendation/di/DistrictRecommendationComponent.java
+++ features/logistic/district_recommendation/src/main/java/com/tokopedia/district_recommendation/di/DistrictRecommendationComponent.java
@@ -1,8 +1,5 @@
 package com.tokopedia.district_recommendation.di;
 
-import com.tokopedia.core.base.di.component.AppComponent;
-import com.tokopedia.core.base.domain.executor.PostExecutionThread;
-import com.tokopedia.core.base.domain.executor.ThreadExecutor;
 import com.tokopedia.district_recommendation.view.DistrictRecommendationFragment;
 
 import dagger.Component;
@@ -11,12 +8,7 @@ import dagger.Component;
  * Created by Irfan Khoirul on 20/11/17.
  */
 @DistrictRecommendationScope
-@Component(modules = DistrictRecommendationModule.class, dependencies = AppComponent.class)
+@Component(modules = DistrictRecommendationModule.class)
 public interface DistrictRecommendationComponent {
     void inject(DistrictRecommendationFragment districtRecommendationFragment);
-
-    ThreadExecutor threadExecutor();
-
-    PostExecutionThread postExecutionThread();
-
 }
--- features/logistic/district_recommendation/src/main/java/com/tokopedia/district_recommendation/di/DistrictRecommendationModule.java
+++ features/logistic/district_recommendation/src/main/java/com/tokopedia/district_recommendation/di/DistrictRecommendationModule.java
@@ -1,17 +1,23 @@
 package com.tokopedia.district_recommendation.di;
 
-import com.tokopedia.core.base.domain.executor.PostExecutionThread;
-import com.tokopedia.core.base.domain.executor.ThreadExecutor;
+import com.google.gson.Gson;
+import com.tokopedia.abstraction.common.network.converter.TokopediaWsV4ResponseConverter;
 import com.tokopedia.district_recommendation.data.mapper.DistrictRecommendationEntityMapper;
 import com.tokopedia.district_recommendation.data.repository.DistrictRecommendationRepository;
+import com.tokopedia.district_recommendation.data.service.KeroApi;
 import com.tokopedia.district_recommendation.data.source.DistrictRecommendationDataStore;
 import com.tokopedia.district_recommendation.domain.usecase.GetDistrictRequestUseCase;
-import com.tokopedia.core.network.apiservices.kero.KeroAuthService;
 import com.tokopedia.district_recommendation.view.DistrictRecommendationContract;
 import com.tokopedia.district_recommendation.view.DistrictRecommendationPresenter;
+import com.tokopedia.logisticdata.data.converter.GeneratedHostConverter;
+import com.tokopedia.network.constant.TkpdBaseURL;
+import com.tokopedia.network.converter.StringResponseConverter;
 
 import dagger.Module;
 import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Irfan Khoirul on 20/11/17.
@@ -26,17 +32,36 @@ public class DistrictRecommendationModule {
     }
 
     // Provide KeroAuthService
+//    @Provides
+//    @DistrictRecommendationScope
+//    KeroAuthService provideKeroAuthService() {
+//        return new KeroAuthService(RETRY_COUNT);
+//    }
+
     @Provides
     @DistrictRecommendationScope
-    KeroAuthService provideKeroAuthService() {
-        return new KeroAuthService(RETRY_COUNT);
+    Retrofit providePeopleActRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(TkpdBaseURL.KERO_DOMAIN)
+                .addConverterFactory(new GeneratedHostConverter())
+                .addConverterFactory(new TokopediaWsV4ResponseConverter())
+                .addConverterFactory(new StringResponseConverter())
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build();
+    }
+
+    @Provides
+    @DistrictRecommendationScope
+    KeroApi provideKeroApi(Retrofit retrofit) {
+        return retrofit.create(KeroApi.class);
     }
 
     // Provide Data Store
     @Provides
     @DistrictRecommendationScope
-    DistrictRecommendationDataStore provideDistrictRecommendationDataStore(KeroAuthService keroAuthService) {
-        return new DistrictRecommendationDataStore(keroAuthService);
+    DistrictRecommendationDataStore provideDistrictRecommendationDataStore(KeroApi keroApi) {
+        return new DistrictRecommendationDataStore(keroApi);
     }
 
     // Provide EntityMapper
@@ -61,12 +86,9 @@ public class DistrictRecommendationModule {
     @Provides
     @DistrictRecommendationScope
     GetDistrictRequestUseCase provideGetDistrictRequestUseCase(
-            ThreadExecutor threadExecutor,
-            PostExecutionThread postExecutionThread,
             DistrictRecommendationRepository districtRecommendationRepository
     ) {
-        return new GetDistrictRequestUseCase(threadExecutor, postExecutionThread,
-                districtRecommendationRepository);
+        return new GetDistrictRequestUseCase(districtRecommendationRepository);
     }
 
     // Provide Presenter
