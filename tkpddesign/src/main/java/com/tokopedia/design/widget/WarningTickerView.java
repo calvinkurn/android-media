bda62cb85dfaa17c52af45284330118be3102ea4
nisie <nathania.sutedja@tokopedia.com>
2018-11-16 13:46:12 +0700

    Add missing import

--- tkpddesign/src/main/java/com/tokopedia/design/widget/WarningTickerView.java
+++ tkpddesign/src/main/java/com/tokopedia/design/widget/WarningTickerView.java
@@ -2,10 +2,12 @@ package com.tokopedia.design.widget;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.Html;
 import android.text.SpannableString;
-
+import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextPaint;
 import android.text.TextUtils;
@@ -16,8 +18,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.text.Html;
-import android.os.Build;
+
 import com.tokopedia.design.R;
 
 /**
