7b66c719cd9ef8fc7a431de356be281207977f1d
Erry Suprayogi <eriksuprayogi@gmail.com>
2018-11-16 11:09:38 +0700

    implement click listener

--- customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/viewholder/WishListTopAdsViewHolder.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/home/adapter/viewholder/WishListTopAdsViewHolder.java
@@ -1,11 +1,16 @@
 package com.tokopedia.tkpd.home.adapter.viewholder;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.google.gson.Gson;
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.gcm.GCMHandler;
+import com.tokopedia.core.router.productdetail.ProductDetailRouter;
+import com.tokopedia.core.var.ProductItem;
 import com.tokopedia.topads.sdk.base.Config;
 import com.tokopedia.topads.sdk.base.adapter.Item;
 import com.tokopedia.topads.sdk.domain.TopAdsParams;
@@ -26,27 +31,36 @@ import static com.tokopedia.topads.sdk.domain.TopAdsParams.DEFAULT_KEY_EP;
 /**
  * Author errysuprayogi on 25,November,2018
  */
-public class WishListTopAdsViewHolder extends RecyclerView.ViewHolder implements
-        TopAdsListener, TopAdsItemClickListener {
+public class WishListTopAdsViewHolder extends RecyclerView.ViewHolder implements TopAdsItemClickListener {
 
     private TopAdsCarouselView topAdsCarouselView;
     private UserSession userSession;
+    private Context context;
 
     public WishListTopAdsViewHolder(View itemView) {
         super(itemView);
+        this.context = itemView.getContext();
         topAdsCarouselView = itemView.findViewById(R.id.topads);
         userSession = new UserSession(itemView.getContext());
     }
 
     private void renderTopAds(TopAdsModel topAdsModel) {
         topAdsCarouselView.setAdsItemClickListener(this);
-        topAdsCarouselView.setAdsListener(this);
         topAdsCarouselView.setData(topAdsModel);
     }
 
     @Override
     public void onProductItemClicked(int position, Product product) {
-
+        ProductItem data = new ProductItem();
+        data.setId(product.getId());
+        data.setName(product.getName());
+        data.setPrice(product.getPriceFormat());
+        data.setImgUri(product.getImage().getM_ecs());
+        Bundle bundle = new Bundle();
+        Intent intent = ProductDetailRouter.createInstanceProductDetailInfoActivity(context);
+        bundle.putParcelable(ProductDetailRouter.EXTRA_PRODUCT_ITEM, data);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
     }
 
     @Override
@@ -64,13 +78,4 @@ public class WishListTopAdsViewHolder extends RecyclerView.ViewHolder implements
 
     }
 
-    @Override
-    public void onTopAdsLoaded(List<Item> list) {
-
-    }
-
-    @Override
-    public void onTopAdsFailToLoad(int errorCode, String message) {
-
-    }
 }
