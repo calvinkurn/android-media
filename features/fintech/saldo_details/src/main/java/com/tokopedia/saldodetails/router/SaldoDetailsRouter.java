62f2d0741c77eac8e5367acbc611f37b46add8a4
sachinbansal <sachin.bansal@tokopedia.com>
2018-11-16 10:26:54 +0530

    empty state click handled

--- features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/router/SaldoDetailsRouter.java
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/router/SaldoDetailsRouter.java
@@ -3,13 +3,18 @@ package com.tokopedia.saldodetails.router;
 import android.content.Context;
 import android.content.Intent;
 
-import com.tokopedia.abstraction.common.data.model.session.UserSession;
 import com.tokopedia.abstraction.common.data.model.storage.CacheManager;
 
 public interface SaldoDetailsRouter {
     Intent getWithdrawIntent(Context context);
+
     Intent getAddPasswordIntent(Context context);
+
     boolean isSaldoNativeEnabled();
+
     CacheManager getGlobalCacheManager();
-    public Intent getContactUsIntent(Context context);
+
+    Intent getContactUsIntent(Context context);
+
+    Intent getHomeIntent(Context context);
 }
