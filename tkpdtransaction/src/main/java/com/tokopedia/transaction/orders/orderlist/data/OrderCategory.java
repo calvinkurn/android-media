3c91ffb69fe5d7e19f65b59ffa85bc5586cd8540
naveengoyal87 <naveen.goyal@tokopedia.com>
2018-11-19 15:31:48 +0530

    Testing changes

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/data/OrderCategory.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/data/OrderCategory.java
@@ -9,7 +9,8 @@ public class OrderCategory {
     public static final String FLIGHTS = "FLIGHTS";
     public static final String EVENTS = "EVENTS";
     public static final String DEALS = "DEALS";
+    public static final String GIFTCARDS = "GIFTCARDS";
 
-    public static final String[] TABS_CATEGORY = {DIGITAL, FLIGHTS, EVENTS, DEALS};
+    public static final String[] TABS_CATEGORY = {DIGITAL, FLIGHTS, EVENTS, DEALS, GIFTCARDS};
     public static final String[] TABS_LABEL = {"Top-Up & Tagihan", "Pesawat", "Events", "Deals"};
 }
