e39dc34c812ba5d6ffff0ecacad2428c4567b53f
nisie <nathania.sutedja@tokopedia.com>
2018-11-20 11:05:10 +0700

    Test no op

---
+++ features/user/user_identification_no_op/src/main/java/com/tokopedia/useridentification/UserIdentificationApplinkModule.java
@@ -0,0 +1,10 @@
+package com.tokopedia.useridentification;
+
+import com.airbnb.deeplinkdispatch.DeepLinkModule;
+
+/**
+ * @author by nisie on 12/11/18.
+ */
+@DeepLinkModule
+public class UserIdentificationApplinkModule {
+}
