53f24d9bba445eeed33087e38e946252e8f76df7
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2018-11-21 11:27:09 +0700

    remove unused file

--- tkpdcore/src/main/java/com/tokopedia/core/network/constants/TkpdBaseURL.java
+++ tkpdcore/src/main/java/com/tokopedia/core/network/constants/TkpdBaseURL.java
@@ -604,7 +604,6 @@ public class TkpdBaseURL {
         public static final String PATH_TOP_PICKS = "/hoth/toppicks/widget";
         public static final String PATH_HOTLIST_CATEGORY = "/hoth/hotlist/v1/category";
         public static final String PATH_UNIVERSE_SEARCH = "/universe/v3";
-        public static final String PATH_UNIVERSE_SEARCH_V8 = "/universe/v8";
         public static final String PATH_DELETE_SEARCH = "/universe/v1";
 
         public static final String PATH_SEARCH_V3_1 = "search/product/v3.1";
