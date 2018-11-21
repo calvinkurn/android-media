dd4e3fb79bb0c9d20a74a39ef45fcfac55aaaed9
Ashwani Tyagi <ashwani.tyagi@tokopedia.com>
2018-11-21 17:27:07 +0530

    remove ios and android url in referral and challenges

--- tkpdcore/src/main/java/com/tokopedia/core/util/BranchSdkUtils.java
+++ tkpdcore/src/main/java/com/tokopedia/core/util/BranchSdkUtils.java
@@ -109,8 +109,6 @@ public class BranchSdkUtils {
         String desktopUrl = null;
 
         linkProperties.addControlParameter(BRANCH_DESKTOP_URL_KEY, data.renderShareUri());
-        linkProperties.addControlParameter(BRANCH_ANDROID_DESKTOP_URL_KEY, data.renderShareUri());
-        linkProperties.addControlParameter(BRANCH_IOS_DESKTOP_URL_KEY, data.renderShareUri());
 
         if (ShareData.PRODUCT_TYPE.equalsIgnoreCase(data.getType())) {
             deeplinkPath = getApplinkPath(Constants.Applinks.PRODUCT_INFO, data.getId());
@@ -135,7 +133,7 @@ public class BranchSdkUtils {
                 linkProperties.setFeature(data.getPrice());
                 linkProperties.setCampaign(String.format("%s - %s", data.getType(), data.getId()));
                 linkProperties.setChannel(String.format("%s - Android", data.getType()));
-                linkProperties.addControlParameter("$uri_redirect_mode","2");
+                linkProperties.addControlParameter("$uri_redirect_mode", "2");
             }
         } else if (ShareData.PROMO_TYPE.equalsIgnoreCase(data.getType())) {
             deeplinkPath = getApplinkPath(Constants.Applinks.PROMO_DETAIL, data.getId());
@@ -147,15 +145,18 @@ public class BranchSdkUtils {
 
         if (desktopUrl == null) {
             linkProperties.addControlParameter(BRANCH_DESKTOP_URL_KEY, data.renderShareUri());
+        }
+        if (!(ShareData.REFERRAL_TYPE.equalsIgnoreCase(data.getType()) ||
+                ShareData.INDI_CHALLENGE_TYPE.equalsIgnoreCase(data.getType()) ||
+                ShareData.GROUPCHAT_TYPE.equalsIgnoreCase(data.getType()))) {
             linkProperties.addControlParameter(BRANCH_ANDROID_DESKTOP_URL_KEY, data.renderShareUri());
             linkProperties.addControlParameter(BRANCH_IOS_DESKTOP_URL_KEY, data.renderShareUri());
         }
 
-
         linkProperties.addControlParameter(BRANCH_ANDROID_DEEPLINK_PATH_KEY, deeplinkPath == null ? "" : deeplinkPath);
         linkProperties.addControlParameter(BRANCH_IOS_DEEPLINK_PATH_KEY, deeplinkPath == null ? "" : deeplinkPath);
 
-        if (ShareData.GROUPCHAT_TYPE.equalsIgnoreCase(data.getType())){
+        if (ShareData.GROUPCHAT_TYPE.equalsIgnoreCase(data.getType())) {
             String connector = "";
             String renderedUrl = "";
             String tempUri = data.getUri();
@@ -166,14 +167,14 @@ public class BranchSdkUtils {
                 connector = "?";
             }
             String tags = "";
-            if(linkProperties.getTags().size() > 0) {
+            if (linkProperties.getTags().size() > 0) {
                 tags = linkProperties.getTags().get(0);
             }
             Uri uri = Uri.parse(String.format("groupchat/%s%sutm_source=%s&utm_medium=%s&utm_campaign=%s&utm_content=%s",
                     tempUri, connector, linkProperties.getChannel(), linkProperties.getFeature(), linkProperties.getCampaign(), tags));
             try {
-                renderedUrl = uri.toString().replace(" ","%20");
-            }catch (Exception e){
+                renderedUrl = uri.toString().replace(" ", "%20");
+            } catch (Exception e) {
                 e.printStackTrace();
                 renderedUrl = uri.toString();
             }
