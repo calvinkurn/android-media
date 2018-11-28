abd761e794f98a4e2aff19a04b1633d1f7941189
stevenfredian <steven.fredian@tokopedia.com>
2018-11-28 16:58:20 +0700

    patch

--- features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/view/presenter/GroupChatPresenter.java
+++ features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/view/presenter/GroupChatPresenter.java
@@ -147,10 +147,8 @@ public class GroupChatPresenter extends BaseDaggerPresenter<GroupChatContract.Vi
 
     private void connect(String userId, String deviceId, String accessToken
             , SettingGroupChat settingGroupChat, String groupChatToken) {
-        if (mSubscription != null && mSubscription.isUnsubscribed()) {
-            mSubscription.unsubscribe();
 
-        }
+        destroyWebSocket();
 
         if (mSubscription == null || mSubscription.isUnsubscribed()) {
             mSubscription = new CompositeSubscription();
@@ -213,7 +211,6 @@ public class GroupChatPresenter extends BaseDaggerPresenter<GroupChatContract.Vi
                     showDummy("onClose", "logger close");
                 }
                 destroyWebSocket();
-                //TODO Why is this connecting again?
                 connect(userId, deviceId, accessToken, finalSettingGroupChat, groupChatToken);
             }
 
@@ -232,6 +229,7 @@ public class GroupChatPresenter extends BaseDaggerPresenter<GroupChatContract.Vi
                 settingGroupChat.getDelay(), settingGroupChat.getMaxRetries()
                 , settingGroupChat.getPingInterval(), groupChatToken).subscribe(subscriber);
 
+        Log.i("doublesocket", "check before Add: " + mSubscription.hasSubscriptions());
 
         mSubscription.add(subscription);
     }
@@ -250,8 +248,9 @@ public class GroupChatPresenter extends BaseDaggerPresenter<GroupChatContract.Vi
     }
 
     public void destroyWebSocket() {
-        if (mSubscription != null && !mSubscription.isUnsubscribed()) {
-            mSubscription.unsubscribe();
+        if (mSubscription != null) {
+            Log.i("doublesocket", "hasSubscription: " + mSubscription.hasSubscriptions());
+            mSubscription.clear();
         }
     }
 
