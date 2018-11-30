9a043a970c8e33729ecebfb1cb8e4fefa61c2b98
Ashwani Tyagi <ashwani.tyagi@tokopedia.com>
2018-11-30 16:41:17 +0530

    url change

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/source/CMNotificationUrls.java
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/source/CMNotificationUrls.java
@@ -4,7 +4,7 @@ package com.tokopedia.notifications.data.source;
  * Created by Ashwani Tyagi on 24/10/18.
  */
 public interface CMNotificationUrls {
-    String CAMPAIGN_MANAGEMENT_DOMAIN = "https://usersampleapi.herokuapp.com/api/v1/";
+    String CAMPAIGN_MANAGEMENT_DOMAIN = "https://10.255.15.23:8080/api/v1/";
     String CM_TOKEN_UPDATE = CAMPAIGN_MANAGEMENT_DOMAIN + "user/add";
 
 }
