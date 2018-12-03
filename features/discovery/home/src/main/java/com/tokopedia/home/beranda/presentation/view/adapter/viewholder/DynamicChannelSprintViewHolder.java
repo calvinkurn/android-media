7fd494adcf107dfe3d48b7865599ac6ca4adc1e2
Erry Suprayogi <eriksuprayogi@gmail.com>
2018-12-03 16:03:42 +0700

    sprint sale lego tracking
    (cherry picked from commit 64701f7)

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/DynamicChannelSprintViewHolder.java
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/DynamicChannelSprintViewHolder.java
@@ -165,19 +165,16 @@ public class DynamicChannelSprintViewHolder extends AbstractViewHolder<DynamicCh
                         public void onClick(View view) {
                             if (isSprintSale(channel)) {
                                 HomePageTracking.eventEnhancedClickSprintSaleProduct(
-                                        context,
                                         channel.getEnhanceClickSprintSaleHomePage(position, countDownView.getCurrentCountDown()));
                                 String attr = channel.getHomeAttribution(position + 1, channel.getGrids()[position].getId());
                                 listener.onDynamicChannelClicked(DynamicLinkHelper.getActionLink(grid), attr);
                             } else if(isSprintSaleLego(channel)){
                                 HomePageTracking.eventEnhancedClickSprintSaleProduct(
-                                        context,
                                         channel.getEnhanceClickSprintSaleLegoHomePage(position, countDownView.getCurrentCountDown()));
                                 String attr = channel.getHomeAttribution(position + 1, channel.getGrids()[position].getId());
                                 listener.onDynamicChannelClicked(DynamicLinkHelper.getActionLink(grid), attr);
                             } else  {
                                 HomePageTracking.eventEnhancedClickDynamicChannelHomePage(
-                                        context,
                                         channel.getEnhanceClickDynamicChannelHomePage(grid, position + 1));
                                 String attr = channel.getHomeAttribution(position + 1, grid.getAttribution());
                                 listener.onDynamicChannelClicked(DynamicLinkHelper.getActionLink(grid), attr);
