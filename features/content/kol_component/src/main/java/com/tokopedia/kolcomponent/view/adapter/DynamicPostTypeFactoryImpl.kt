ef37fc8b912523eb4e20a3ce60811d2e16f8006b
yfsx <yoas.fs@gmail.com>
2018-12-04 14:51:09 +0700

    update ui

--- features/content/kol_component/src/main/java/com/tokopedia/kolcomponent/view/adapter/DynamicPostTypeFactoryImpl.kt
+++ features/content/kol_component/src/main/java/com/tokopedia/kolcomponent/view/adapter/DynamicPostTypeFactoryImpl.kt
@@ -5,7 +5,11 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kolcomponent.view.viewholder.DynamicPostViewHolder
+import com.tokopedia.kolcomponent.view.viewholder.ItemCreateContentViewHolder
+import com.tokopedia.kolcomponent.view.viewholder.ItemRecommendedViewHolder
 import com.tokopedia.kolcomponent.view.viewmodel.DynamicPostViewModel
+import com.tokopedia.kolcomponent.view.viewmodel.ItemCreateContentViewModel
+import com.tokopedia.kolcomponent.view.viewmodel.ItemRecommendedViewModel
 
 /**
  * @author by milhamj on 03/12/18.
@@ -15,9 +19,19 @@ class DynamicPostTypeFactoryImpl : BaseAdapterTypeFactory(), DynamicPostTypeFact
         return DynamicPostViewHolder.LAYOUT
     }
 
+    override fun type(itemRecommendedViewModel: ItemRecommendedViewModel): Int {
+        return ItemRecommendedViewHolder.LAYOUT
+    }
+
+    override fun type(itemCreateContentViewModel: ItemCreateContentViewModel): Int {
+        return ItemCreateContentViewHolder.LAYOUT
+    }
+
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when(type) {
             DynamicPostViewHolder.LAYOUT -> DynamicPostViewHolder(parent)
+            ItemRecommendedViewHolder.LAYOUT -> ItemRecommendedViewHolder(parent)
+            ItemCreateContentViewHolder.LAYOUT -> ItemCreateContentViewHolder(parent)
             else -> super.createViewHolder(parent, type)
         }
     }
