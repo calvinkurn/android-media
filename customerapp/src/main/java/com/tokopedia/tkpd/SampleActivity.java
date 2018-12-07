fcef132f822ecb0003c026be3bcda8f6bca3c333
Meyta <meytajennist@gmail.com>
2018-12-07 11:51:16 +0700

    add sample activity

---
+++ customerapp/src/main/java/com/tokopedia/tkpd/SampleActivity.java
@@ -0,0 +1,18 @@
+package com.tokopedia.tkpd;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by meta on 07/12/18.
+ */
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.id.activity_sample);
+
+    }
+}
