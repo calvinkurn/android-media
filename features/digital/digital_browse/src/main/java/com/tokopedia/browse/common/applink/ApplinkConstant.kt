4a132f26496453644c08a86c0dc8fbdd0a4d1004
Muhammad Furqan <nanjihusaha@gmail.com>
2018-12-10 10:55:22 +0700

    kotlinify common/applink

---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/common/applink/ApplinkConstant.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.browse.common.applink
+
+import com.tokopedia.abstraction.constant.TkpdAppLink
+
+/**
+ * @author by furqan on 30/08/18.
+ */
+
+class ApplinkConstant : TkpdAppLink() {
+    companion object {
+
+        val DIGITAL_BROWSE = "tokopedia://category-explore"
+    }
+
+}
