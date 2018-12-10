df19dc84df027495e1f21a6ab2197fe01a9b0bf8
Muhammad Furqan <nanjihusaha@gmail.com>
2018-12-10 11:03:27 +0700

    kotlinify common/di

---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/common/di/utils/DigitalBrowseComponentUtils.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.browse.common.di.utils
+
+import android.app.Application
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.browse.common.di.DigitalBrowseComponent
+
+/**
+ * @author by furqan on 30/08/18.
+ */
+
+object DigitalBrowseComponentUtils {
+    private var digitalBrowseComponent: DigitalBrowseComponent? = null
+
+    fun getDigitalBrowseComponent(application: Application): DigitalBrowseComponent {
+        if (digitalBrowseComponent == null) {
+            digitalBrowseComponent = DaggerDigitalBrowseComponent.builder().baseAppComponent(
+                    (application as BaseMainApplication).baseAppComponent).build()
+        }
+        return digitalBrowseComponent as DigitalBrowseComponent
+    }
+}
