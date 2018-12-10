14c47f501019399dfd8f5a0a325963a65ea1e648
Muhammad Furqan <nanjihusaha@gmail.com>
2018-12-10 10:55:59 +0700

    kotlinify common/data

---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/common/data/DigitalBrowsePopularAnalyticsModel.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.browse.common.data
+
+/**
+ * @author by furqan on 21/09/18.
+ */
+
+class DigitalBrowsePopularAnalyticsModel(
+        var bannerId: Long,
+        var brandName: String,
+        var position: Int)
---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/common/data/DigitalBrowseServiceAnalyticsModel.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.browse.common.data
+
+/**
+ * @author by furqan on 18/09/18.
+ */
+
+class DigitalBrowseServiceAnalyticsModel(
+        var headerName: String,
+        var headerPosition: Int,
+        var iconName: String,
+        var iconPosition: Int)
