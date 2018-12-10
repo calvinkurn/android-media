5166b393a2b6cc201b5b63c50be57acbdaf142d7
Muhammad Furqan <nanjihusaha@gmail.com>
2018-12-10 11:42:03 +0700

    kotlinify homepage/domain/subscriber

---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/homepage/domain/subscriber/GetDigitalCategorySubscriber.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.browse.homepage.domain.subscriber
+
+import com.tokopedia.browse.homepage.presentation.model.DigitalBrowseServiceViewModel
+import rx.Subscriber
+
+/**
+ * @author by furqan on 07/09/18.
+ */
+
+class GetDigitalCategorySubscriber(private val digitalCategoryActionListener: DigitalCategoryActionListener) : Subscriber<DigitalBrowseServiceViewModel>() {
+
+    override fun onCompleted() {
+
+    }
+
+    override fun onError(throwable: Throwable) {
+        digitalCategoryActionListener.onErrorGetDigitalCategory(throwable)
+    }
+
+    override fun onNext(viewModel: DigitalBrowseServiceViewModel) {
+        digitalCategoryActionListener.onSuccessGetDigitalCategory(viewModel)
+    }
+
+    interface DigitalCategoryActionListener {
+
+        fun onErrorGetDigitalCategory(throwable: Throwable)
+
+        fun onSuccessGetDigitalCategory(digitalBrowseServiceViewModel: DigitalBrowseServiceViewModel)
+
+    }
+}
