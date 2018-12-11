d863feea7f310caa64a5517b30f73314de9a25ae
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-12-11 11:03:33 +0700

    Fix crashes on refactoring shipping module

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/viewmodel/ShipmentNotifierModel.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/viewmodel/ShipmentNotifierModel.java
@@ -1,6 +1,6 @@
 package com.tokopedia.checkout.view.feature.shipment.viewmodel;
 
-import com.tokopedia.checkout.view.feature.shipment.ShipmentData;
+import com.tokopedia.shipping_recommendation.domain.shipping.ShipmentData;
 
 /**
  * Created by fajarnuha on 06/12/18.
