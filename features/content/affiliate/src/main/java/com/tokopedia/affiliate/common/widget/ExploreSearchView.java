c9c1f047d85e393c30b0c1f2e7dd064e5a2ecd69
yfsx <yoas.fs@gmail.com>
2018-12-12 12:25:48 +0700

    fix bugs again pt2

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ExploreSearchView.java
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ExploreSearchView.java
@@ -1,6 +1,7 @@
 package com.tokopedia.affiliate.common.widget;
 
 import android.content.Context;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 
 import com.tokopedia.design.text.SearchInputView;
@@ -9,24 +10,28 @@ import com.tokopedia.design.text.SearchInputView;
  * @author by yfsx on 06/12/18.
  */
 public class ExploreSearchView extends SearchInputView {
+    private TextWatcher searchTextWatcher;
     public ExploreSearchView(Context context) {
         super(context);
+        searchTextWatcher = getSearchTextWatcher();
     }
 
     public ExploreSearchView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        searchTextWatcher = getSearchTextWatcher();
     }
 
     public ExploreSearchView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        searchTextWatcher = getSearchTextWatcher();
     }
 
 
     public void addTextWatcherToSearch() {
-        getSearchTextView().addTextChangedListener(getSearchTextWatcher());
+        getSearchTextView().addTextChangedListener(searchTextWatcher);
     }
 
     public void removeSearchTextWatcher() {
-        getSearchTextView().removeTextChangedListener(getSearchTextWatcher());
+        getSearchTextView().removeTextChangedListener(searchTextWatcher);
     }
 }
