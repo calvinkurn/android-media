cbf62ef0f08f3540478c940fd155371f005bba55
nisie <nathania.sutedja@tokopedia.com>
2018-12-12 15:47:45 +0700

    Implement missing functions

--- features/user/topchat/src/main/java/com/tokopedia/topchat/revamp/presenter/TopChatRoomPresenter.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/revamp/presenter/TopChatRoomPresenter.kt
@@ -1,11 +1,14 @@
 package com.tokopedia.topchat.revamp.presenter
 
+import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.presenter.BaseDaggerPresenter
 import com.tokopedia.chat_common.domain.GetChatUseCase
+import com.tokopedia.chat_common.domain.pojo.ChatSocketPojo
 import com.tokopedia.chat_common.presenter.BaseChatPresenter
 import com.tokopedia.topchat.revamp.domain.mapper.TopChatRoomWebSocketMapper
 import com.tokopedia.topchat.revamp.listener.TopChatContract
 import com.tokopedia.user.session.UserSessionInterface
+import com.tokopedia.websocket.WebSocketResponse
 import javax.inject.Inject
 
 /**
@@ -17,6 +20,18 @@ class TopChatRoomPresenter @Inject constructor(
         var userSession: UserSessionInterface,
         var topChatWebSocketMapper: TopChatRoomWebSocketMapper)
     : BaseDaggerPresenter<TopChatContract.View>(), TopChatContract.Presenter {
+
+    override fun mappingEvent(webSocketResponse: WebSocketResponse, messageId: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun mapToVisitable(pojo: ChatSocketPojo): Visitable<*> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun sendMessage(sendMessage: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 
 }
