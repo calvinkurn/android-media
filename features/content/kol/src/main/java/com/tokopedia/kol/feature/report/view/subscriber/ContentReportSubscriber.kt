1597f4811e0a94902fcda5a5a01fd1b047c111ab
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2018-12-12 11:12:48 +0700

    Get error type for duplicate report :zap:

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/report/view/subscriber/ContentReportSubscriber.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/report/view/subscriber/ContentReportSubscriber.kt
@@ -3,6 +3,8 @@ package com.tokopedia.kol.feature.report.view.subscriber
 import com.tokopedia.abstraction.common.network.exception.MessageErrorException
 import com.tokopedia.abstraction.common.utils.network.ErrorHandler
 import com.tokopedia.kol.R
+import com.tokopedia.kol.feature.report.data.entity.SendReportResponse
+import com.tokopedia.kol.feature.report.domain.usecase.SendReportUseCase
 import com.tokopedia.kol.feature.report.view.listener.ContentReportContract
 import com.tokopedia.kotlin.extensions.view.debugTrace
 import rx.Subscriber
@@ -10,13 +12,22 @@ import rx.Subscriber
 /**
  * @author by milhamj on 15/11/18.
  */
-class ContentReportSubscriber(val view: ContentReportContract.View)
-     : Subscriber<Boolean>() {
-    override fun onNext(isSuccess: Boolean) {
-        if (isSuccess) {
-            view.onSuccessSendReport()
+class ContentReportSubscriber(val view: ContentReportContract.View?)
+    : Subscriber<SendReportResponse>() {
+    override fun onNext(response: SendReportResponse) {
+        if (response.feedReportSubmit.errorMessage.isEmpty().not()) {
+            if (response.feedReportSubmit.errorType == SendReportUseCase.ERROR_REPORT_DUPLICATE) {
+                view?.onErrorSendReportDuplicate(response.feedReportSubmit.errorMessage)
+            } else {
+                onError(MessageErrorException(response.feedReportSubmit.errorMessage))
+            }
+            return
+        }
+
+        if (response.feedReportSubmit.data.success == SendReportUseCase.REPORT_SUCCESS) {
+            view?.onSuccessSendReport()
         } else {
-            onError(MessageErrorException(view.getContext()!!.getString(R.string.kol_report_error)))
+            onError(MessageErrorException(view?.getContext()!!.getString(R.string.kol_report_error)))
         }
     }
 
@@ -26,12 +37,7 @@ class ContentReportSubscriber(val view: ContentReportContract.View)
     override fun onError(e: Throwable?) {
         e?.debugTrace()
 
-        val errorMsg = ErrorHandler.getErrorMessage(view.getContext(), e)
-        if (errorMsg.contains(view.getString(R.string.kol_duplicate), ignoreCase = true)) {
-            view.onErrorSendReportDuplicate(errorMsg)
-        } else {
-            view.hideLoading()
-            view.onErrorSendReport(errorMsg)
-        }
+        view?.hideLoading()
+        view?.onErrorSendReport(ErrorHandler.getErrorMessage(view.getContext(), e))
     }
 }
