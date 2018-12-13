f53fae39f8737b0f438d1bf6031696ca7ba5e4ba
yogieputra8 <yogie.putra8@gmail.com>
2018-12-13 18:16:54 +0700

    remove few dependencies from core

--- customerapp/src/main/java/com/tokopedia/tkpd/thankyou/domain/usecase/ThankYouPageTrackerUseCase.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/thankyou/domain/usecase/ThankYouPageTrackerUseCase.java
@@ -1,8 +1,8 @@
 package com.tokopedia.tkpd.thankyou.domain.usecase;
 
-import com.tokopedia.core.base.domain.RequestParams;
-import com.tokopedia.core.base.domain.UseCase;
 import com.tokopedia.tkpd.thankyou.data.repository.ThanksTrackerRepository;
+import com.tokopedia.usecase.RequestParams;
+import com.tokopedia.usecase.UseCase;
 
 import rx.Observable;
 
