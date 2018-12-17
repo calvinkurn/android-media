f534bf2d15dbadc6c31c2d2997831aedbca34617
nisie <nathania.sutedja@tokopedia.com>
2018-12-17 08:00:05 +0700

    Add missing import

--- features/user/chat_common/src/main/java/com/tokopedia/chat_common/view/activity/BaseChatToolbarActivity.kt
+++ features/user/chat_common/src/main/java/com/tokopedia/chat_common/view/activity/BaseChatToolbarActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
