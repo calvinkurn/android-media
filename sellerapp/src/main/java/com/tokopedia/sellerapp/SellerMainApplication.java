a200e24d9cdcc064702a17e7467ded2f55ca9318
naveengoyal87 <naveen.goyal@tokopedia.com>
2018-12-18 12:25:29 +0530

    Opacity has been changed.

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
@@ -1,8 +1,6 @@
 package com.tokopedia.sellerapp;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
