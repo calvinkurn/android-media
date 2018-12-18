ca2559ad6bcce944b2b97c007ca807ee761f8620
okasurya <brilliantsurya@gmail.com>
2018-12-18 19:13:56 +0700

    add configurable parameter for preinstall script

--- preinstall.py
+++ preinstall.py
@@ -1,33 +1,5 @@
 import xml.etree.ElementTree as ET
 
-# def main():
-#     filename = "./customerapp/src/main/AndroidManifest.xml"
-#     ET.register_namespace('android', 'http://schemas.android.com/apk/res/android')
-#     ET.register_namespace('tools', 'http://schemas.android.com/tools')
-#     tree = ET.parse(filename)
-#     root = tree.getroot()
-#     application = root.find('application')
-#
-#     md_channel = ET.SubElement(application, 'meta-data')
-#     md_channel.set("android:name", "CHANNEL")
-#     md_channel.set("android:value", "store")
-#
-#     md_af = ET.SubElement(application, 'meta-data')
-#     md_af.set("android:name", "AF_PRE_INSTALL_NAME")
-#     md_af.set("android:value", "store")
-#
-#     print ET.tostring(root)
-#     tree.write(filename)
-#
-#     f = open(filename,'r')
-#     temp = f.read()
-#     f.close()
-#
-#     f = open(filename, 'w')
-#     f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
-#     f.write("\n")
-#     f.write(temp)
-#     f.close()
 import sys, getopt
 
 def main(argv):
@@ -36,26 +8,56 @@ def main(argv):
     channel = ''
 
     try:
-        opts, args = getopt.getopt(argv,"ham:n:c",["android-manifest=","name=","channel="])
+        opts, args = getopt.getopt(argv,"h",["android-manifest=","name=","channel="])
     except getopt.GetoptError:
-        print 'preinstall.py -am <android manifest path> -n <name> -c <channel>'
+        print 'preinstall.py --android-manifest <android manifest path> --name <name> --channel <channel>'
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
             print 'preinstall.py -am <android manifest path> -n <name> -c <channel>'
             sys.exit()
-        elif opt in ("-am", "--android-manifest"):
+        elif opt in ("--android-manifest"):
             android_manifest = arg
-        elif opt in ("-n", "--name"):
+        elif opt in ("--name"):
             name = arg
-        elif opt in ("-c", "--channel"):
+        elif opt in ("--channel"):
             channel = arg
 
-    print android_manifest + " " + name + " " + channel
+    if(android_manifest is None or name is None or channel is None):
+        print 'preinstall.py -am <android manifest path> -n <name> -c <channel>'
+        sys.exit(1)
+    else:
+        appendManifest(android_manifest, name, channel)
 
+def appendManifest(android_manifest, name, channel):
+    ET.register_namespace('android', 'http://schemas.android.com/apk/res/android')
+    ET.register_namespace('tools', 'http://schemas.android.com/tools')
+    tree = ET.parse(android_manifest)
+    root = tree.getroot()
+    application = root.find('application')
 
-#     "oppo_int", "oppo_preinstall","oppo_store_site"
+    md_channel = ET.SubElement(application, 'meta-data')
+    md_channel.set("android:name", "CHANNEL")
+    md_channel.set("android:value", channel)
+
+    md_af = ET.SubElement(application, 'meta-data')
+    md_af.set("android:name", "AF_PRE_INSTALL_NAME")
+    md_af.set("android:value", name) 
+
+    print ET.tostring(root)
+    tree.write(android_manifest)
+
+    f = open(android_manifest,'r')
+    temp = f.read()
+    f.close()
+
+    f = open(android_manifest, 'w')
+    f.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+    f.write("\n")
+    f.write(temp)
+    f.close()
+    #"oppo_int", "oppo_preinstall","oppo_store_site"
 
 
 
