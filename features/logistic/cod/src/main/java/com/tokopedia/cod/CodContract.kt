2f7143682d4300b55e6ad9659e26ae068c5bb016
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2018-12-21 09:37:11 +0700

    Update contract cod

--- features/logistic/cod/src/main/java/com/tokopedia/cod/CodContract.kt
+++ features/logistic/cod/src/main/java/com/tokopedia/cod/CodContract.kt
@@ -1,19 +1,24 @@
 package com.tokopedia.cod
 
+import android.view.View
+
 /**
  * Created by fajarnuha on 17/12/18.
  */
 interface CodContract {
 
     interface View {
-        fun loadInformation()
+        fun initView()
         fun showError(message: String)
         fun showLoading()
         fun hideLoading()
+        fun onPayClicked(view: android.view.View)
+        fun navigateToThankYouPage()
     }
 
     interface Presenter {
         fun submitConfirmation()
+        fun confirmPayment()
     }
 
 }
