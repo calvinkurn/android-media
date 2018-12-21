b053c3e18154a9b22611c2ec14749c08a50a70fc
nisie <nathania.sutedja@tokopedia.com>
2018-12-21 16:23:45 +0700

    Fix unfound param

--- features/user/topchat/src/main/java/com/tokopedia/topchat/revamp/view/TopChatRoomActivity.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/revamp/view/TopChatRoomActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.chat_common.BaseChatToolbarActivity
 import com.tokopedia.topchat.revamp.di.DaggerTopChatRoomComponent
 import com.tokopedia.topchat.revamp.di.TopChatRoomComponent
@@ -17,7 +18,7 @@ class TopChatRoomActivity : BaseChatToolbarActivity(), HasComponent<TopChatRoomC
 
     override fun getNewFragment(): Fragment {
         val bundle = Bundle()
-        bundle.putString(PARAM_MESSAGE_ID, intent.getStringExtra(PARAM_MESSAGE_ID))
+        bundle.putString(ApplinkConst.Chat.MESSAGE_ID, intent.getStringExtra(ApplinkConst.Chat.MESSAGE_ID))
         return TopChatRoomFragment.createInstance(bundle)
     }
 }
