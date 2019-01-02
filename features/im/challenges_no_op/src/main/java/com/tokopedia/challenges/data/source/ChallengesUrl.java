df2971af6c7cdbcc44e99663952c46bc07dc711c
lavekush-t <lavekush.agrawal@tokopedia.com>
2019-01-02 12:56:41 +0530

    Cleanup challenges  NO-OP

--- features/im/challenges_no_op/src/main/java/com/tokopedia/challenges/data/source/ChallengesUrl.java
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.challenges.data.source;
-
-
-public class ChallengesUrl {
-    //Base Url
-    public static String INDI_DOMAIN = "https://api.indi.com/v1/";
-    public static String CHANNEL_ID = "d295e3d1-ef65-4783-adc1-e77168561a1a";
-    public static String CHALLENGE_ID = "%s";
-}
