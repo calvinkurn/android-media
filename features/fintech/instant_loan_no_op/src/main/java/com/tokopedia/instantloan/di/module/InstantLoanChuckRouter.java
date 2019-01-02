67cfa9a87ed791375def3a5f8fdc6d8a55b30179
lavekush-t <lavekush.agrawal@tokopedia.com>
2019-01-02 12:57:43 +0530

    Cleanup instant-loan  NO-OP

--- features/fintech/instant_loan_no_op/src/main/java/com/tokopedia/instantloan/di/module/InstantLoanChuckRouter.java
+++ features/fintech/instant_loan_no_op/src/main/java/com/tokopedia/instantloan/di/module/InstantLoanChuckRouter.java
@@ -2,10 +2,6 @@ package com.tokopedia.instantloan.di.module;
 
 import okhttp3.Interceptor;
 
-/**
- * Created by sachinbansal on 6/1/18.
- */
-
 public interface InstantLoanChuckRouter {
     Interceptor getChuckInterceptor();
 }
