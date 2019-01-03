6cdeebe3cca416570aa3530d58890d763811609b
Ashwani Tyagi <ashwani.tyagi@tokopedia.com>
2019-01-03 15:56:36 +0530

    staging url

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/source/CMNotificationUrls.java
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/source/CMNotificationUrls.java
@@ -5,6 +5,6 @@ package com.tokopedia.notifications.data.source;
  */
 public class CMNotificationUrls {
     public static String CAMPAIGN_MANAGEMENT_DOMAIN = "https://imt.tokopedia.com/api/v1/";
-    public static String CM_TOKEN_UPDATE = CAMPAIGN_MANAGEMENT_DOMAIN + "user/add";
+    public static String CM_TOKEN_UPDATE = "https://notifier-staging.tokopedia.com/imt/" + "user/add";
 
 }
