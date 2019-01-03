07aa5903a9b8d876d2a596d7f383f267095a9685
nisie <nathania.sutedja@tokopedia.com>
2019-01-03 11:09:53 +0700

    Add dagger module

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chattemplate/di/TemplateChatModule.java
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chattemplate/di/TemplateChatModule.java
@@ -2,10 +2,25 @@ package com.tokopedia.topchat.chattemplate.di;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
 import com.readystatesoftware.chuck.ChuckInterceptor;
 import com.tokopedia.abstraction.AbstractionRouter;
 import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+import com.tokopedia.abstraction.common.network.OkHttpRetryPolicy;
+import com.tokopedia.abstraction.common.network.converter.TokopediaWsV4ResponseConverter;
+import com.tokopedia.abstraction.common.network.exception.HeaderErrorListResponse;
+import com.tokopedia.abstraction.common.network.interceptor.ErrorResponseInterceptor;
+import com.tokopedia.abstraction.common.network.interceptor.HeaderErrorResponseInterceptor;
+import com.tokopedia.abstraction.common.utils.GlobalConfig;
+import com.tokopedia.cacheapi.interceptor.CacheApiInterceptor;
+import com.tokopedia.core.network.di.qualifier.InboxQualifier;
+import com.tokopedia.core.network.retrofit.interceptors.DigitalHmacAuthInterceptor;
+import com.tokopedia.network.NetworkRouter;
+import com.tokopedia.network.converter.StringResponseConverter;
+import com.tokopedia.network.interceptor.FingerprintInterceptor;
+import com.tokopedia.network.utils.AuthUtil;
+import com.tokopedia.topchat.chatlist.data.TopChatUrl;
 import com.tokopedia.topchat.chattemplate.data.factory.EditTemplateChatFactory;
 import com.tokopedia.topchat.chattemplate.data.factory.TemplateChatFactory;
 import com.tokopedia.topchat.chattemplate.data.mapper.EditTemplateChatMapper;
@@ -15,9 +30,18 @@ import com.tokopedia.topchat.chattemplate.data.repository.EditTemplateRepository
 import com.tokopedia.topchat.chattemplate.data.repository.TemplateRepository;
 import com.tokopedia.topchat.chattemplate.data.repository.TemplateRepositoryImpl;
 import com.tokopedia.topchat.common.chat.ChatService;
+import com.tokopedia.user.session.UserSession;
+import com.tokopedia.user.session.UserSessionInterface;
+
+import java.util.concurrent.TimeUnit;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by stevenfredian on 9/14/17.
@@ -26,6 +50,19 @@ import dagger.Provides;
 @Module
 public class TemplateChatModule {
 
+    @TemplateChatScope
+    @Provides
+    UserSessionInterface provideUserSessionInterface(
+            @ApplicationContext Context context) {
+        return new UserSession(context);
+    }
+
+    @TemplateChatScope
+    @Provides
+    NetworkRouter provideNetworkRouter(
+            @ApplicationContext Context context) {
+        return (NetworkRouter) context;
+    }
 
     @TemplateChatScope
     @Provides
@@ -39,6 +76,72 @@ public class TemplateChatModule {
     ChuckInterceptor provideChuckInterceptor(
             @ApplicationContext Context context) {
         return new ChuckInterceptor(context);
+    }
+
+
+    @Provides
+    public ErrorResponseInterceptor provideResponseInterceptor() {
+        return new HeaderErrorResponseInterceptor(HeaderErrorListResponse.class);
+    }
+
+    @TemplateChatScope
+    @Provides
+    OkHttpClient provideOkHttpClient(@InboxQualifier OkHttpRetryPolicy retryPolicy,
+                                     ErrorResponseInterceptor errorResponseInterceptor,
+                                     ChuckInterceptor chuckInterceptor,
+                                     HttpLoggingInterceptor httpLoggingInterceptor,
+                                     NetworkRouter networkRouter,
+                                     UserSessionInterface userSessionInterface) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .addInterceptor(new FingerprintInterceptor(networkRouter, userSessionInterface))
+                .addInterceptor(new CacheApiInterceptor())
+                .addInterceptor(new DigitalHmacAuthInterceptor(AuthUtil.KEY.KEY_WSV4))
+                .addInterceptor(errorResponseInterceptor)
+                .connectTimeout(retryPolicy.connectTimeout, TimeUnit.SECONDS)
+                .readTimeout(retryPolicy.readTimeout, TimeUnit.SECONDS)
+                .writeTimeout(retryPolicy.writeTimeout, TimeUnit.SECONDS);
+
+        if (GlobalConfig.isAllowDebuggingTools()) {
+            builder.addInterceptor(chuckInterceptor)
+                    .addInterceptor(httpLoggingInterceptor);
+        }
+        return builder.build();
+    }
+
+    private static final int NET_READ_TIMEOUT = 60;
+    private static final int NET_WRITE_TIMEOUT = 60;
+    private static final int NET_CONNECT_TIMEOUT = 60;
+    private static final int NET_RETRY = 1;
+
+    @TemplateChatScope
+    @InboxQualifier
+    @Provides
+    OkHttpRetryPolicy provideOkHttpRetryPolicy() {
+        return new OkHttpRetryPolicy(NET_READ_TIMEOUT,
+                NET_WRITE_TIMEOUT,
+                NET_CONNECT_TIMEOUT,
+                NET_RETRY);
+    }
+
+    @TemplateChatScope
+    @InboxQualifier
+    @Provides
+    Retrofit provideChatRetrofit(OkHttpClient okHttpClient,
+                                 Retrofit.Builder retrofitBuilder) {
+        return retrofitBuilder.baseUrl(TopChatUrl.BASE_URL)
+                .addConverterFactory(new TokopediaWsV4ResponseConverter())
+                .addConverterFactory(new StringResponseConverter())
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+
+    @TemplateChatScope
+    @Provides
+    ChatService provideChatService(@InboxQualifier Retrofit retrofit) {
+        return new ChatService(retrofit);
     }
 
     @TemplateChatScope
