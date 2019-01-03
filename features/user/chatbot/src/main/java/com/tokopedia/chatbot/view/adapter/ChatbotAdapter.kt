aa4803501cdb09901f524795c8e31722d35fb7fd
nisie <nathania.sutedja@tokopedia.com>
2019-01-03 17:09:43 +0700

    Revert loading

--- features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/ChatbotAdapter.kt
+++ features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/ChatbotAdapter.kt
@@ -10,15 +10,4 @@ import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactor
 
 class ChatbotAdapter(adapterTypeFactory: ChatbotTypeFactoryImpl)
     : BaseListAdapter<Visitable<*>, BaseAdapterTypeFactory>(adapterTypeFactory) {
-
-    override fun showLoading() {
-        if (!isLoading) {
-            if (isShowLoadingMore) {
-                visitables.add(0, loadingMoreModel)
-            } else {
-                visitables.add(0, loadingModel)
-            }
-            notifyItemInserted(0)
-        }
-    }
 }
