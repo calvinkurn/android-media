e8f94ac1d67fdc9307fe4d26224590b0e5de1ddf
M normansyah <noiz354@gmail.com>
2019-01-07 16:48:42 +0700

    standarized gradle for track & trackinterface.

--- tkpdcore/src/main/java/com/tokopedia/core/app/MainApplication.java
+++ tkpdcore/src/main/java/com/tokopedia/core/app/MainApplication.java
@@ -319,35 +319,6 @@ public abstract class MainApplication extends MainRouterApplication{
         TrackingUtils.runFirstTime(this, TrackingUtils.AnalyticsKind.MOENGAGE, getTkpdCoreRouter().legacySessionHandler());
         TrackingUtils.setMoEngageExistingUser(this, getTkpdCoreRouter().legacySessionHandler().isLoggedIn());
         TrackingUtils.enableDebugging(this, isDebug());
-
-        // Treck.getInstance().registerImplementation("GTM", SuicideGTM.class)
-        // mapOf("MOENGAGE", listOf(MOENGAGESuicide.class, "isExistingUser")
-
-        // configuration
-        // enable or not
-
-
-        // Treck.getInstance().get("GTM").sendEvent(Map<String,Object>)
-        // Treck.getInstance().get("MOENGAGE").get("isExistingUser")
-        // Treck.getInstance().get("MOENGAGE").set("isExistingUser", Object)
-
-        // Treck.getInstance().create(MOENGAGESuicide.class).saveAndBuild("MOENGAGE");
-        // Treck.get("MOENGAGE").isExistingUser();
-
-        /**
-         * @Moengage
-         * @Type("Moengage")
-         * class MOENGAGESuicide{
-         *
-         *   public void isExistingUser(){}
-         * }
-         *
-         @Type("GTM")
-          * class SuicideGTM{
-          *
-          *   public void sendEvent(Map<String,Object>){}
-          * }
-         */
     }
 
     public void initCrashlytics() {
