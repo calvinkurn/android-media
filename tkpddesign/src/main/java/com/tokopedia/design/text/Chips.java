523c2efc80a0f1c99799807503e0f94636ecbc8f
pranaytokopedia <pranay.mohapatra@tokopedia.com>
2019-01-08 15:14:39 +0530

    add custom view for chip(bad reason/filter options) type item

--- tkpddesign/src/main/java/com/tokopedia/design/text/Chips.java
+++ tkpddesign/src/main/java/com/tokopedia/design/text/Chips.java
@@ -5,7 +5,6 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.ViewGroup;
 
 import com.tokopedia.design.R;
@@ -30,14 +29,14 @@ public class Chips extends AppCompatTextView {
     }
 
     private void init() {
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) this.getLayoutParams();//new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        int margin_default = (int) (getContext().getResources().getDimension(R.dimen.dp_16));
-        int padding_top_bottom = (int) (getContext().getResources().getDimension(R.dimen.dp_8));
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) this.getLayoutParams();
+        int marginDefault = (int) (getContext().getResources().getDimension(R.dimen.dp_16));
+        int paddingTopBottom = (int) (getContext().getResources().getDimension(R.dimen.dp_8));
         params.height = (int) (getContext().getResources().getDimension(R.dimen.dp_40));
         params.width = ConstraintLayout.LayoutParams.WRAP_CONTENT;
-        params.setMargins(margin_default, margin_default, 0, 0);
-        setBackgroundResource(R.drawable.bg_button_select_deselect);
-        setPadding(margin_default, padding_top_bottom, margin_default, padding_top_bottom);
+        params.setMargins(marginDefault, marginDefault, 0, 0);
+        setBackgroundResource(R.drawable.chips_background);
+        setPadding(marginDefault, paddingTopBottom, marginDefault, paddingTopBottom);
         setEllipsize(TextUtils.TruncateAt.MARQUEE);
         setTextColor(getResources().getColor(R.color.black_56));
         setLayoutParams(params);
