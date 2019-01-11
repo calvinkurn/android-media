36a6253e23632a356f75fa53425388e9252c979e
yfsx <yoas.fs@gmail.com>
2019-01-11 16:33:39 +0700

    implement cexplore with filter

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/domain/usecase/ExploreUseCase.java
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/domain/usecase/ExploreUseCase.java
@@ -3,14 +3,19 @@ package com.tokopedia.affiliate.feature.explore.domain.usecase;
 import android.content.Context;
 import android.text.TextUtils;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
 import com.tokopedia.abstraction.common.utils.GraphqlHelper;
 import com.tokopedia.affiliate.R;
 import com.tokopedia.affiliate.feature.explore.data.pojo.ExploreData;
 import com.tokopedia.affiliate.feature.explore.view.viewmodel.ExploreParams;
+import com.tokopedia.affiliate.feature.explore.view.viewmodel.FilterViewModel;
 import com.tokopedia.graphql.data.model.GraphqlRequest;
 import com.tokopedia.graphql.domain.GraphqlUseCase;
 import com.tokopedia.usecase.RequestParams;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -23,6 +28,9 @@ public class ExploreUseCase extends GraphqlUseCase {
     private final static String PARAM_KEYWORD = "keyword";
     private final static String PARAM_FILTER= "filter";
     private final static String PARAM_SORT = "sort";
+    private final static String PARAM_FILTER_KEY = "key";
+    private final static String PARAM_FILTER_KEY_DATA = "d_id";
+    private final static String PARAM_FILTER_VALUE = "value";
 
     @Inject
     public ExploreUseCase(@ApplicationContext Context context) {
@@ -62,11 +70,21 @@ public class ExploreUseCase extends GraphqlUseCase {
             params.putString(PARAM_CURSOR, exploreParams.getCursor());
         }
         if (exploreParams.getFilters().size() != 0) {
-            params.putObject(PARAM_FILTER, exploreParams.getFilters());
+            params.putObject(PARAM_FILTER, constructFilterParams(exploreParams.getFilters()));
         }
 //        if (!TextUtils.isEmpty(exploreParams.getSort().getKey())) {
 //            params.putObject(PARAM_SORT, exploreParams.getSort());
 //        }
         return params;
+    }
+
+    private static JsonArray constructFilterParams(List<FilterViewModel> filterList) {
+        JsonArray dataArray = new JsonArray();
+        for (FilterViewModel filter : filterList) {
+            JsonObject object = new JsonObject();
+            object.addProperty(PARAM_FILTER_KEY, PARAM_FILTER_KEY_DATA);
+            object.addProperty(PARAM_FILTER_VALUE, filter.getIds().toString());
+        }
+        return dataArray;
     }
 }
