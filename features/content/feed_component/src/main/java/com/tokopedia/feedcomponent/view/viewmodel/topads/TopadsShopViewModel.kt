9e0bb3eb9d89a363a2e92d88442270db347e07f0
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-01-15 15:07:06 +0700

    Add impression and click tracking on topads model :chart_with_upwards_trend:

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/viewmodel/topads/TopadsShopViewModel.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/viewmodel/topads/TopadsShopViewModel.kt
@@ -4,6 +4,7 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.feedcomponent.data.pojo.feed.contentitem.Title
 import com.tokopedia.feedcomponent.data.pojo.template.Template
 import com.tokopedia.feedcomponent.view.adapter.post.DynamicFeedTypeFactory
+import com.tokopedia.feedcomponent.view.viewmodel.recommendation.TrackingRecommendationModel
 import com.tokopedia.topads.sdk.domain.model.Data
 
 /**
@@ -12,7 +13,8 @@ import com.tokopedia.topads.sdk.domain.model.Data
 data class TopadsShopViewModel(
         val title: Title = Title(),
         val dataList: MutableList<Data> = ArrayList(),
-        val template: Template = Template()
+        val template: Template = Template(),
+        val trackingList: List<TrackingRecommendationModel> = ArrayList()
 ): Visitable<DynamicFeedTypeFactory> {
     override fun type(typeFactory: DynamicFeedTypeFactory?): Int {
         return typeFactory!!.type(this)
