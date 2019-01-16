d2c61836b703485d7738bae07c8cb2593a06789a
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-01-16 14:34:53 +0700

    Get corousel list :sparkles:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/education/view/fragment/AffiliateEducationFragment.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/education/view/fragment/AffiliateEducationFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.common.utils.DisplayMetricUtils
 import com.tokopedia.affiliate.R
+import com.tokopedia.affiliate.common.constant.AffiliateConstant
+import com.tokopedia.affiliate.feature.education.view.viewmodel.EducationItemViewModel
 
 /**
  * @author by milhamj on 14/01/19.
@@ -30,9 +33,50 @@ class AffiliateEducationFragment : BaseDaggerFragment() {
     override fun getScreenName(): String? = null
 
     override fun initInjector() {
+
     }
 
     private fun initView() {
 
+    }
+
+    private fun getCarouselList(): MutableList<EducationItemViewModel> {
+        val list = arrayListOf<EducationItemViewModel>()
+
+        list.add(EducationItemViewModel(
+                getImagePath(AffiliateConstant.IMG_AFFILIATE_TEASER, AffiliateConstant.JPG),
+                getString(R.string.af_share_and_commission)
+        ))
+
+        list.add(EducationItemViewModel(
+                getImagePath(AffiliateConstant.IMG_AFFILIATE_SCREENSHOT_1, AffiliateConstant.PNG),
+                getString(R.string.af_choose_favorite_product),
+                1
+        ))
+
+        list.add(EducationItemViewModel(
+                getImagePath(AffiliateConstant.IMG_AFFILIATE_SCREENSHOT_2, AffiliateConstant.PNG),
+                getString(R.string.af_curate_product_profile),
+                2
+        ))
+
+        list.add(EducationItemViewModel(
+                getImagePath(AffiliateConstant.IMG_AFFILIATE_SCREENSHOT_3, AffiliateConstant.PNG),
+                getString(R.string.af_share_product_buy),
+                3
+        ))
+
+        return list
+    }
+
+    private fun getImagePath(imageName: String, imageFormat: String): String {
+        val screenDensity = DisplayMetricUtils.getScreenDensity(context)
+        return String.format(
+                AffiliateConstant.ANDROID_PATH_FORMAT,
+                imageName,
+                screenDensity,
+                imageName,
+                imageFormat
+        )
     }
 }
