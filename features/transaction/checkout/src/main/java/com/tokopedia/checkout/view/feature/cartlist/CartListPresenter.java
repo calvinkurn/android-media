d654c235e0137e8bc8a13241b2669f23373cdce6
fwidjaja <fwidjaja@live.com>
2019-01-18 16:10:22 +0700

    fix counter checkout button

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/cartlist/CartListPresenter.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/cartlist/CartListPresenter.java
@@ -513,6 +513,7 @@ public class CartListPresenter implements ICartListPresenter {
                 String parentId = data.getCartItemData().getOriginData().getParentId();
                 String productId = data.getCartItemData().getOriginData().getProductId();
                 int itemQty = data.getCartItemData().getUpdatedData().getQuantity();
+                totalItemQty += itemQty;
                 if (!TextUtils.isEmpty(parentId) && !parentId.equals("0")) {
                     for (CartItemHolderData dataForQty : allCartItemDataList) {
                         if (!productId.equals(dataForQty.getCartItemData().getOriginData().getProductId()) &&
@@ -524,7 +525,7 @@ public class CartListPresenter implements ICartListPresenter {
                     }
                 }
 
-                totalItemQty = totalItemQty + 1;
+                // totalItemQty = totalItemQty + 1;
                 List<WholesalePrice> wholesalePrices = data.getCartItemData().getOriginData().getWholesalePrice();
                 boolean hasCalculateWholesalePrice = false;
                 if (wholesalePrices != null && wholesalePrices.size() > 0) {
