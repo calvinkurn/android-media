9122c0f44e53d6203c3cb4a5e393d9c9130d9c5e
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2019-01-25 16:49:03 +0700

    refresh onresume

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/profile/ProfileListFragment.kt
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/search/fragment/profile/ProfileListFragment.kt
@@ -53,6 +53,11 @@ class ProfileListFragment : BaseListFragment<ProfileViewModel, ProfileListTypeFa
 
     var nextPage : Int = 1
 
+    override fun onResume() {
+        super.onResume()
+        onSwipeRefresh()
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         if (userVisibleHint) {
