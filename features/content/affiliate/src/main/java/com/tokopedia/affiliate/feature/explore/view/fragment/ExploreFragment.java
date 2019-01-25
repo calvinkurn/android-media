4c5b7162f8c803fb534cfae78343678337eb0533
yfsx <yoas.fs@gmail.com>
2019-01-25 15:13:04 +0700

    test 2

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/fragment/ExploreFragment.java
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/fragment/ExploreFragment.java
@@ -230,7 +230,6 @@ public class ExploreFragment
         //init image
         if (userSession.isLoggedIn()) {
             ImageHandler.loadImageCircle2(getActivity(), ivProfile, userSession.getProfilePicture(), R.drawable.loading_page);
-        } else {
         }
         //init red dot
     }
