560b44e8c5519c2bbef40804b3fd73f32cc38f13
yfsx <yoas.fs@gmail.com>
2019-01-28 17:12:09 +0700

    fix bug from release

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
@@ -530,7 +530,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
         TrackingPromoCheckoutRouter,
         TopAdsRouter,
         CMRouter,
-        SaldoDetailsRouter,
         ReferralRouter,
         SaldoDetailsRouter,
         ILoyaltyRouter {
