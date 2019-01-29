13cbb26c28bd8ec3190878e7ecac9f04ca55119d
okasurya <brilliantsurya@gmail.com>
2019-01-29 11:44:40 +0700

    fix router

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
@@ -529,7 +529,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
         TrackingPromoCheckoutRouter,
         TopAdsRouter,
         CMRouter,
-        SaldoDetailsRouter,
         ReferralRouter,
         SaldoDetailsRouter,
         ILoyaltyRouter,
