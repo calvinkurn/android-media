00acf04862d9d183a52e3ea3deedf3df6e315e1d
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-02-03 07:21:27 +0700

    Add some documentation :memo:

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/util/NullChecker.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/util/NullChecker.kt
@@ -8,8 +8,23 @@ import com.tokopedia.abstraction.common.utils.GlobalConfig
  * @author by milhamj on 29/01/19.
  */
 
+/**
+ * The pattern that's used to find null value in an object.
+ * It starts with a newline, then some whitespaces, variable name inside quotation marks,
+ * a colon `:`, some whitespaces, then the word `null`.
+ */
 private const val NULL_PATTERN = """\n\s*"\S+":\s*null"""
 
+/**
+ * Find null value in an object.
+ * This function turns the object into JSON then use REGEX to find the null value.
+ * The expected format is `"some_variable": null`.
+ * @see NULL_PATTERN
+ *
+ * @param `object` the object to find the null value in
+ * @param `actionWhenNull` optional lambda that will invoked when null value is found in the `object`
+ * @return Return true if null value is found, false otherwise
+ */
 @JvmOverloads
 fun isContainNull(`object`: Any?, actionWhenNull: (String) -> Unit = { }): Boolean {
     val whenNull = { errorMessage: String ->
@@ -22,13 +37,12 @@ fun isContainNull(`object`: Any?, actionWhenNull: (String) -> Unit = { }): Boole
         return true
     }
 
-    val gsonBuilder = GsonBuilder()
-    gsonBuilder.serializeNulls()
-    gsonBuilder.setPrettyPrinting()
-    val gson = gsonBuilder.create()
-
-    val objectInString = gson.toJson(`object`).toLowerCase()
-    val firstNullOccurrence = Regex(NULL_PATTERN).find(objectInString)
+    //serializeNulls is needed so null value won't be omitted in the JSON
+    //setPrettyPrinting is needed because it's easier to find a variable in a formatted JSON
+    //In a formatted JSON, a variable is always in a newline
+    val gson = GsonBuilder().serializeNulls().setPrettyPrinting().create()
+    val objectAsJson = gson.toJson(`object`).toLowerCase()
+    val firstNullOccurrence = Regex(NULL_PATTERN).find(objectAsJson)
 
     return if (firstNullOccurrence == null) {
         false
