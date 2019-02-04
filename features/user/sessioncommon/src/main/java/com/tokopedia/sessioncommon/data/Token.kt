5cf8d266644d5a751d9ecc3b501715b5f59060bc
nisie <nathania.sutedja@tokopedia.com>
2019-02-04 10:21:01 +0700

    Make token open

--- features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
@@ -3,7 +3,7 @@ package com.tokopedia.sessioncommon.data
 /**
  * @author by nisie on 04/02/19.
  */
-class Token {
+open class Token {
 
     companion object {
         const val GOOGLE_API_KEY = "AIzaSyCRkgwGBe8ZxjcK07Cnl3Auf72BpgA6lLo"
