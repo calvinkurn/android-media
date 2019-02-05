9deadfb1e90130eee940d952bcfd025af1fe94f7
Abhay Raj Singh Yadav <abhay.yadav@tokopedia.com>
2019-02-05 16:36:31 +0530

    Tokopoints bottomsheet button hiding bugfix

--- features/promo/tokopoints_notification/src/main/java/com/tokopedia/tokopoints/notification/view/TokoPointsPopupNotificationBottomSheet.java
+++ features/promo/tokopoints_notification/src/main/java/com/tokopedia/tokopoints/notification/view/TokoPointsPopupNotificationBottomSheet.java
@@ -25,22 +25,6 @@ public class TokoPointsPopupNotificationBottomSheet extends BottomSheets {
     }
 
     @Override
-    public void setupDialog(Dialog dialog, int style) {
-        super.setupDialog(dialog, style);
-
-        if (getActivity() == null) {
-            return;
-        }
-
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        int screenHeight = displaymetrics.heightPixels;
-        if (getBottomSheetBehavior() != null)
-            getBottomSheetBehavior().setPeekHeight(screenHeight / 2);
-    }
-
-
-    @Override
     public void initView(View view) {
         TextView count = view.findViewById(R.id.text_quota_count);
         TextView title = view.findViewById(R.id.text_title);
@@ -97,6 +81,7 @@ public class TokoPointsPopupNotificationBottomSheet extends BottomSheets {
             RouteManager.route(action.getContext(), mData.getAppLink());
             dismiss();
         });
+        updateHeight();
     }
 
     @Override
