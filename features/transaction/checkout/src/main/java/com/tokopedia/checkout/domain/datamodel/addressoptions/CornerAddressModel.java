9e9b3ec01157c7fb734902653f21f04564733ad3
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-02-09 23:26:15 +0700

    Implement parcelable on corner model

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/datamodel/addressoptions/CornerAddressModel.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/datamodel/addressoptions/CornerAddressModel.java
@@ -1,9 +1,12 @@
 package com.tokopedia.checkout.domain.datamodel.addressoptions;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by fajarnuha on 09/02/19.
  */
-public class CornerAddressModel {
+public class CornerAddressModel implements Parcelable {
 
     private int cornerId;
     private String cornerName;
@@ -53,4 +56,38 @@ public class CornerAddressModel {
     public void setSelected(boolean selected) {
         isSelected = selected;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.cornerId);
+        dest.writeString(this.cornerName);
+        dest.writeString(this.cornerBranchName);
+        dest.writeString(this.cornerBranchDesc);
+        dest.writeByte(this.isSelected ? (byte) 1 : (byte) 0);
+    }
+
+    protected CornerAddressModel(Parcel in) {
+        this.cornerId = in.readInt();
+        this.cornerName = in.readString();
+        this.cornerBranchName = in.readString();
+        this.cornerBranchDesc = in.readString();
+        this.isSelected = in.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<CornerAddressModel> CREATOR = new Parcelable.Creator<CornerAddressModel>() {
+        @Override
+        public CornerAddressModel createFromParcel(Parcel source) {
+            return new CornerAddressModel(source);
+        }
+
+        @Override
+        public CornerAddressModel[] newArray(int size) {
+            return new CornerAddressModel[size];
+        }
+    };
 }
