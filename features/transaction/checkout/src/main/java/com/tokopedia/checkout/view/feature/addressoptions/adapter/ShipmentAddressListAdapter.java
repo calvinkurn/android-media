2fdbd75b90569d6559313465c4348d06cb0f7f0f
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-02-09 20:32:53 +0700

    Update adapter for corner

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/addressoptions/adapter/ShipmentAddressListAdapter.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/addressoptions/adapter/ShipmentAddressListAdapter.java
@@ -2,11 +2,16 @@ package com.tokopedia.checkout.view.feature.addressoptions.adapter;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.tokopedia.checkout.R;
+import com.tokopedia.checkout.domain.datamodel.addressoptions.CornerAddressModel;
 import com.tokopedia.shipping_recommendation.domain.shipping.RecipientAddressModel;
 
 import java.util.ArrayList;
@@ -18,14 +23,14 @@ import java.util.List;
 
 public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private boolean adaSampai;
+    private CornerAddressModel mSampaiModel;
     private List<RecipientAddressModel> mAddressModelList;
     private ActionListener mActionListener;
 
     public ShipmentAddressListAdapter(ActionListener actionListener) {
         mActionListener = actionListener;
         mAddressModelList = new ArrayList<>();
-        adaSampai = false;
+        mSampaiModel = null;
     }
 
     @NonNull
@@ -40,7 +45,8 @@ public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerVie
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         if (getItemViewType(position) == R.layout.item_sampai) {
-
+            SampaiViewHolder sampaiViewHolder = (SampaiViewHolder) holder;
+            sampaiViewHolder.bind(mSampaiModel);
         } else {
             int addressPosition = position - getExtraCount();
             RecipientAddressViewHolder addressHolder = (RecipientAddressViewHolder) holder;
@@ -58,7 +64,7 @@ public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerVie
 
     @Override
     public int getItemViewType(int position) {
-        if (position == 0 && adaSampai) return R.layout.item_sampai;
+        if (position == 0 && mSampaiModel != null) return R.layout.item_sampai;
         else return RecipientAddressViewHolder.TYPE;
     }
 
@@ -69,10 +75,17 @@ public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerVie
 
     public void updateAddressList(List<RecipientAddressModel> addressModelList) {
         mAddressModelList.addAll(addressModelList);
+        notifyDataSetChanged();
     }
 
-    public void setSampai() {
-        adaSampai = true;
+    public void showSampaiWithoutSelected() {
+        mSampaiModel = new CornerAddressModel();
+        notifyDataSetChanged();
+    }
+
+    public void setSampai(CornerAddressModel cornerAddressModel) {
+        mSampaiModel = cornerAddressModel;
+        notifyDataSetChanged();
     }
 
     void setSelectedAddressData(int position) {
@@ -88,7 +101,7 @@ public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerVie
     }
 
     private int getExtraCount() {
-        return adaSampai ? 1 : 0;
+        return mSampaiModel != null ? 1 : 0;
     }
 
     /**
@@ -110,8 +123,31 @@ public class ShipmentAddressListAdapter extends RecyclerView.Adapter<RecyclerVie
 
     class SampaiViewHolder extends RecyclerView.ViewHolder {
 
+        Button mButton;
+        View mCornerView;
+        TextView mCornerName, mBranchName;
+        RadioButton mRadio;
+
         public SampaiViewHolder(View itemView) {
             super(itemView);
+            mCornerView = itemView.findViewById(R.id.view_tkpd_corner);
+            mButton = itemView.findViewById(R.id.button_tkpd_corner);
+            mCornerName = itemView.findViewById(R.id.text_view_corner);
+            mBranchName = itemView.findViewById(R.id.text_view_branch);
+            mRadio = itemView.findViewById(R.id.radio_button_corner);
+        }
+
+        public void bind(CornerAddressModel model) {
+            if (!TextUtils.isEmpty(model.getCornerName())) {
+                mCornerView.setVisibility(View.VISIBLE);
+                mCornerName.setText(model.getCornerName());
+                mBranchName.setText(model.getCornerBranchName());
+                mRadio.setSelected(model.isSelected());
+                mButton.setText("Ubah Lokasi Tokopedia Corner");
+            } else {
+                mCornerView.setVisibility(View.GONE);
+                mButton.setText("Pilih Lokasi Tokopedia Corner");
+            }
         }
     }
 
