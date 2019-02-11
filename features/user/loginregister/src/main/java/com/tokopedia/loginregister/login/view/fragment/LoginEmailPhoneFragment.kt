eceac33e6406eb4bef6789e769c1e4f46a9461d5
nisie <nathania.sutedja@tokopedia.com>
2019-02-11 09:50:05 +0700

    Change Api key again

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/view/fragment/LoginEmailPhoneFragment.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/view/fragment/LoginEmailPhoneFragment.kt
@@ -96,7 +96,10 @@ class LoginEmailPhoneFragment : BaseDaggerFragment(), LoginEmailPhoneContract.Vi
     private val REQUEST_WELCOME_PAGE = 115
     private val REQUEST_LOGIN_GOOGLE = 116
 
-    private val GOOGLE_API_KEY = "692092518182-rjgh0bja6q41dllpq2dptn134cmhiv9h.apps.googleusercontent.com"
+    private val GOOGLE_API_KEY = "692092518182-mnfsbr75hvhs9lrdbl0nrt5j6h9ehu32.apps.googleusercontent.com"
+
+    //UNAUTHORIZED
+//    private val GOOGLE_API_KEY = "692092518182-rjgh0bja6q41dllpq2dptn134cmhiv9h.apps.googleusercontent.com"
 
     val IS_AUTO_LOGIN = "auto_login"
     val AUTO_LOGIN_METHOD = "method"
