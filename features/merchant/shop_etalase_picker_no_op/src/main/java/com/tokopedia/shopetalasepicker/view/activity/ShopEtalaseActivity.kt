90c3482843f0746a1cdbf35c71f91ba584704176
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-02-12 20:31:59 +0700

    shopEtalaseActivity no op

---
+++ features/merchant/shop_etalase_picker_no_op/src/main/java/com/tokopedia/shopetalasepicker/view/activity/ShopEtalaseActivity.kt
@@ -0,0 +1,24 @@
+package com.tokopedia.shopetalasepicker.view.activity
+
+import android.content.Context
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+
+/**
+ * Created by normansyahputa on 2/28/18.
+ */
+
+class ShopEtalaseActivity() {
+
+    companion object {
+
+        @JvmStatic @JvmOverloads
+        fun createIntent(context: Context, shopId: String, selectedEtalaseId: String? = ""): Intent {
+            val startMain = Intent(Intent.ACTION_MAIN)
+            startMain.addCategory(Intent.CATEGORY_HOME)
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(startMain)
+        }
+    }
+
+}
