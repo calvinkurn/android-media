9ed985cc53e437bff587c5337ecc81a46f166bc5
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-02-12 18:56:32 +0700

    Check for null in graphql :sparkles:

--- libraries/graphql/src/main/java/com/tokopedia/graphql/data/repository/GraphqlRepositoryImpl.java
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/data/repository/GraphqlRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.tokopedia.graphql.data.repository;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
 import com.tokopedia.graphql.CommonUtils;
@@ -13,6 +14,9 @@ import com.tokopedia.graphql.data.model.GraphqlResponseInternal;
 import com.tokopedia.graphql.data.source.cache.GraphqlCacheDataStore;
 import com.tokopedia.graphql.data.source.cloud.GraphqlCloudDataStore;
 import com.tokopedia.graphql.domain.GraphqlRepository;
+import com.tokopedia.graphql.domain.GraphqlUseCase;
+import com.tokopedia.kotlin.util.ContainNullException;
+import com.tokopedia.kotlin.util.NullCheckerKt;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -64,8 +68,11 @@ public class GraphqlRepositoryImpl implements GraphqlRepository {
                 try {
                     JsonElement data = response.getOriginalResponse().get(i).getAsJsonObject().get(GraphqlConstant.GqlApiKeys.DATA);
                     if (data != null && !data.isJsonNull()) {
+                        Type type = requests.get(i).getTypeOfT();
+                        Object object = CommonUtils.fromJson(data.toString(), requests.get(i).getTypeOfT());
+                        checkForNull(object);
                         //Lookup for data
-                        results.put(requests.get(i).getTypeOfT(), CommonUtils.fromJson(data.toString(), requests.get(i).getTypeOfT()));
+                        results.put(type, object);
                     }
 
                     JsonElement error = response.getOriginalResponse().get(i).getAsJsonObject().get(GraphqlConstant.GqlApiKeys.ERROR);
@@ -93,5 +100,14 @@ public class GraphqlRepositoryImpl implements GraphqlRepository {
         } else {
             return mGraphqlCache.getResponse(requests, new GraphqlCacheStrategy.Builder(CacheType.NONE).build());
         }
+    }
+
+    private void checkForNull(Object object) {
+        NullCheckerKt.isContainNull(object, errorMessage -> {
+            String message = String.format("Found %s in %s", errorMessage, GraphqlUseCase.class.getSimpleName());
+            ContainNullException exception = new ContainNullException(message);
+            Crashlytics.logException(exception);
+            throw exception;
+        });
     }
 }
