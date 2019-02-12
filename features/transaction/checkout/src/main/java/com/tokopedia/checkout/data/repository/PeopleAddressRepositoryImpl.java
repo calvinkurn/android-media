c63be5d7ae5210f118540a5d6d72138063001d31
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-02-12 11:52:25 +0700

    Refactor: remove redundancy

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/repository/PeopleAddressRepositoryImpl.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/repository/PeopleAddressRepositoryImpl.java
@@ -23,8 +23,6 @@ import rx.functions.Func1;
 
 public class PeopleAddressRepositoryImpl implements PeopleAddressRepository {
 
-    private static final int FIRST_ELEMENT = 0;
-
     private final PeopleActApi peopleActApi;
     private GetAddressWithCornerUseCase addressWithCornerUseCase;
 
@@ -34,12 +32,6 @@ public class PeopleAddressRepositoryImpl implements PeopleAddressRepository {
         this.addressWithCornerUseCase = addressWithCornerUseCase;
     }
 
-    /**
-     * Get an {@link Observable} which will emits a {@link List < ShipmentAddressModel >}
-     *
-     * @param params Parameters used to retrieve address data
-     * @return List of address
-     */
     @Override
     public Observable<GetPeopleAddress> getAllAddress(Map<String, String> params) {
         return peopleActApi
@@ -51,23 +43,12 @@ public class PeopleAddressRepositoryImpl implements PeopleAddressRepository {
                             if (!response.body().isError()) {
                                 GetPeopleAddress peopleAddress = response.body()
                                         .convertDataObj(GetPeopleAddress.class);
-
                                 if (!peopleAddress.getList().isEmpty()) {
                                     // Successfully obtaining user's list of address
                                     return peopleAddress;
-                                } else {
-                                    // List of address is empty, therefore result will be treated
-                                    // as null data
                                 }
-
-                            } else if (response.body().isNullData()) {
-                                // Response body is literally null data
-                            } else {
-                                // Error is occurred
-                                String error = response.body().getErrorMessages().get(FIRST_ELEMENT);
                             }
                         }
-
                         return null;
                     }
                 });
