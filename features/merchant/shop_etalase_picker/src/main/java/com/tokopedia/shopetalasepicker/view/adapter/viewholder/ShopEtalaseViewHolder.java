be8e225f3592769abe92ac477a0305c6d153dba7
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-02-12 16:35:31 +0700

    fix etalase model

--- features/merchant/shop_etalase_picker/src/main/java/com/tokopedia/shopetalasepicker/view/adapter/viewholder/ShopEtalaseViewHolder.java
+++ features/merchant/shop_etalase_picker/src/main/java/com/tokopedia/shopetalasepicker/view/adapter/viewholder/ShopEtalaseViewHolder.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.abstraction.common.utils.image.ImageHandler;
-import com.tokopedia.shop.etalase.view.model.ShopEtalaseViewModel;
 import com.tokopedia.shopetalasepicker.R;
+import com.tokopedia.shopetalasepicker.view.model.ShopEtalaseViewModel;
 
 /**
  * Created by normansyahputa on 2/28/18.
