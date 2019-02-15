740799eb506c623abd942f85c363ed0ade218bfb
yfsx <yoas.fs@gmail.com>
2019-02-15 10:20:02 +0700

    add pinned product and profile

---
+++ features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/kotlin/view/viewmodel/pinned/PinnedProfileViewModel.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.groupchat.chatroom.kotlin.view.viewmodel.pinned
+
+/**
+ * @author by yfsx on 14/02/19.
+ */
+data class PinnedProfileViewModel(
+        val imageUrl: String = "",
+        val name: String = ""
+) {
+}
