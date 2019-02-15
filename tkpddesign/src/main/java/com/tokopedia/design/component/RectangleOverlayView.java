264cc5f4adc3aae28d97ee303e75560ac17d7e76
Varun Sharma <varun.sharma@tokopedia.com>
2019-02-15 14:49:06 +0530

    changes for Kyc overlay image

--- tkpddesign/src/main/java/com/tokopedia/design/component/RectangleOverlayView.java
+++ tkpddesign/src/main/java/com/tokopedia/design/component/RectangleOverlayView.java
@@ -9,6 +9,8 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
@@ -58,18 +60,43 @@ public class RectangleOverlayView extends LinearLayout {
                 getWidth() / 2 + getResources().getDimensionPixelSize(R.dimen.ktp_view_width),
                 getHeight() / 2 + getResources().getDimensionPixelSize(R.dimen.ktp_view_height));
 
+        RectF ktpRectangle_2 = new RectF(
+                (getWidth() / 2 - getResources().getDimensionPixelSize(R.dimen.ktp_view_width))+20,
+                (getHeight() / 2 - getResources().getDimensionPixelSize(R.dimen.ktp_view_height))+20,
+                (getWidth() / 2 + getResources().getDimensionPixelSize(R.dimen.ktp_view_width))-20,
+                (getHeight() / 2 + getResources().getDimensionPixelSize(R.dimen.ktp_view_height))-20);
+
 
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(getResources().getColor(R.color.colorPrimary));
+        paint.setColor(getResources().getColor(R.color.light_disabled));
 
         paint.setAlpha(255);
         osCanvas.drawRect(mainRectangle, paint);
 
+        osCanvas.drawBitmap(drawableToBitmap(getResources().getDrawable(R.drawable.img_frame_id)), null, ktpRectangle, null);
+
         paint.setColor(Color.TRANSPARENT);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+        osCanvas.drawRoundRect(ktpRectangle_2, 0, 0, paint);
 
-        osCanvas.drawRoundRect(ktpRectangle, 10, 10, paint);
+    }
 
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+        return bitmap;
     }
 
 }
