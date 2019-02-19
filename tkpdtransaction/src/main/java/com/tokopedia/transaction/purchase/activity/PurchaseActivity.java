afc1e865b5d267e38477ff1b99c44fc1be3371e0
naveengoyal87 <naveen.goyal@tokopedia.com>
2019-02-19 09:26:15 +0530

    Changes for push notification handling

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/activity/PurchaseActivity.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/purchase/activity/PurchaseActivity.java
@@ -1,10 +1,13 @@
 package com.tokopedia.transaction.purchase.activity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.view.inputmethod.InputMethodManager;
 
@@ -21,12 +24,16 @@ import com.tokopedia.core.router.transactionmodule.TransactionPurchaseRouter;
 import com.tokopedia.core.util.GlobalConfig;
 import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.var.TkpdState;
+import com.tokopedia.remoteconfig.RemoteConfigKey;
 import com.tokopedia.transaction.R;
 import com.tokopedia.transaction.R2;
+import com.tokopedia.transaction.orders.orderlist.data.OrderCategory;
+import com.tokopedia.transaction.orders.orderlist.view.activity.OrderListActivity;
 import com.tokopedia.transaction.purchase.adapter.PurchaseTabAdapter;
 import com.tokopedia.transaction.purchase.dialog.CancelTransactionDialog;
 import com.tokopedia.transaction.purchase.fragment.TxListFragment;
 import com.tokopedia.transaction.purchase.fragment.TxSummaryFragment;
+import com.tokopedia.transaction.router.ITransactionOrderDetailRouter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,6 +64,9 @@ public class PurchaseActivity extends BaseTemporaryDrawerActivity implements
 
     private int drawerPosition;
     private String stateTxFilterID;
+    private static String filterId = "0";
+    private BroadcastReceiver notifReceiver;
+    private String pushNotiCode = "";
 
     private PurchaseTabAdapter adapter;
 
@@ -75,46 +85,66 @@ public class PurchaseActivity extends BaseTemporaryDrawerActivity implements
     @DeepLink({ApplinkConst.PURCHASE_CONFIRMED, ApplinkConst.PURCHASE_ORDER})
     public static Intent getConfirmedIntent(Context context, Bundle extras) {
         Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
-        return new Intent(context, PurchaseActivity.class)
-                .setData(uri.build())
-                .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_CONFIRMED)
-                .putExtras(extras);
+        if (!filterId.equalsIgnoreCase("0")) {
+            return new Intent(context, PurchaseActivity.class)
+                    .setData(uri.build())
+                    .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_CONFIRMED)
+                    .putExtras(extras);
+        } else {
+            return getMarketPlaceIntent(context, extras);
+        }
     }
 
     @DeepLink(ApplinkConst.PURCHASE_PROCESSED)
     public static Intent getProcessedIntent(Context context, Bundle extras) {
         Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
-        return new Intent(context, PurchaseActivity.class)
-                .setData(uri.build())
-                .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_PROCESSED)
-                .putExtras(extras);
+        if (!filterId.equalsIgnoreCase("0")) {
+            return new Intent(context, PurchaseActivity.class)
+                    .setData(uri.build())
+                    .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_PROCESSED)
+                    .putExtras(extras);
+        } else {
+            return getMarketPlaceIntent(context, extras);
+        }
     }
 
     @DeepLink({ApplinkConst.PURCHASE_SHIPPED})
     public static Intent getShippedIntent(Context context, Bundle extras) {
         Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
-        return new Intent(context, PurchaseActivity.class)
-                .setData(uri.build())
-                .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_SHIPPED)
-                .putExtras(extras);
+        if (!filterId.equalsIgnoreCase("0")) {
+            return new Intent(context, PurchaseActivity.class)
+                    .setData(uri.build())
+                    .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_SHIPPED)
+                    .putExtras(extras);
+        } else {
+            return getMarketPlaceIntent(context, extras);
+        }
     }
 
     @DeepLink({ApplinkConst.PURCHASE_DELIVERED, ApplinkConst.PURCHASE_SHIPPING_CONFIRM})
     public static Intent getDeliveredIntent(Context context, Bundle extras) {
         Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
-        return new Intent(context, PurchaseActivity.class)
-                .setData(uri.build())
-                .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_DELIVERED)
-                .putExtras(extras);
+        if (!filterId.equalsIgnoreCase("0")) {
+            return new Intent(context, PurchaseActivity.class)
+                    .setData(uri.build())
+                    .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_DELIVERED)
+                    .putExtras(extras);
+        } else {
+            return getMarketPlaceIntent(context, extras);
+        }
     }
 
     @DeepLink(ApplinkConst.PURCHASE_HISTORY)
-    public static Intent getHistoryIntent(Context context, Bundle extras){
+    public static Intent getHistoryIntent(Context context, Bundle extras) {
         Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
-        return new Intent(context, PurchaseActivity.class)
-                .setData(uri.build())
-                .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_ALL_ORDER)
-                .putExtras(extras);
+        if (!filterId.equalsIgnoreCase("0")) {
+            return new Intent(context, PurchaseActivity.class)
+                    .setData(uri.build())
+                    .putExtra(EXTRA_STATE_TAB_POSITION, TAB_POSITION_PURCHASE_ALL_ORDER)
+                    .putExtras(extras);
+        } else {
+            return getMarketPlaceIntent(context, extras);
+        }
     }
 
     @Override
@@ -202,8 +232,67 @@ public class PurchaseActivity extends BaseTemporaryDrawerActivity implements
             finish();
         }
 
+        if (((ITransactionOrderDetailRouter) this.getApplicationContext()).getBooleanRemoteConfig(RemoteConfigKey.APP_ENABLE_ACCOUNT_AFFILIATE, true)) {
+            if (notifReceiver == null) {
+                notifReceiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        if (intent.getExtras() != null) {
+                            pushNotiCode = intent.getExtras().getString("tkp_code", "");
+                            switch (pushNotiCode) {
+                                case "309":
+                                case "305":
+                                    filterId = "14";
+                                    break;
+                                case "307":
+                                    filterId = "13";
+                                    break;
+                                case "310":
+                                    filterId = "5";
+                                    break;
+                                case "311":
+                                case "312":
+                                case "315":
+                                    filterId = "13";
+                                    break;
+                                case "308":
+                                    filterId = "7";
+                                    break;
+                                case "314":
+                                    filterId = "12";
+                                    break;
+                                case "313":
+                                    filterId = "17";
+                                    break;
+                                default:
+                                    filterId = "0";
+                                    break;
+                            }
+
+                        }
+                    }
+
+                };
+            }
+            try {
+                LocalBroadcastManager.getInstance(this).registerReceiver(notifReceiver, new IntentFilter
+                        (com.tokopedia.abstraction.constant.TkpdState.LOYALTY_GROUP_CHAT));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         setDrawerSidePosition(drawerPosition);
         super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (notifReceiver != null) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(notifReceiver);
+        }
     }
 
     @Override
@@ -272,6 +361,69 @@ public class PurchaseActivity extends BaseTemporaryDrawerActivity implements
             ((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
                     .hideSoftInputFromWindow(viewPager.getWindowToken(), 0);
         }
+    }
+
+    private void getPushNotiCode() {
+        if (notifReceiver == null) {
+            notifReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (intent.getExtras() != null) {
+                        pushNotiCode = intent.getExtras().getString("tkp_code", "");
+                        switch (pushNotiCode) {
+                            case "309":
+                            case "305":
+                                filterId = "14";
+                                break;
+                            case "307":
+                                filterId = "13";
+                                break;
+                            case "310":
+                                filterId = "5";
+                                break;
+                            case "311":
+                            case "312":
+                            case "315":
+                                filterId = "13";
+                                break;
+                            case "308":
+                                filterId = "7";
+                                break;
+                            case "314":
+                                filterId = "12";
+                                break;
+                            case "313":
+                                filterId = "17";
+                                break;
+                            default:
+                                filterId = "0";
+                                break;
+                        }
+
+                    }
+                }
+
+            };
+        }
+    }
+
+    private boolean openNewBom(String pushNotifCode) {
+
+        if (((ITransactionOrderDetailRouter) this.getApplicationContext()).getBooleanRemoteConfig(RemoteConfigKey.APP_ENABLE_ACCOUNT_AFFILIATE, true)) {
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static Intent getMarketPlaceIntent(Context context, Bundle extras) {
+        Uri.Builder uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon();
+        extras.putString("orderCategory", OrderCategory.MARKETPLACE);
+        extras.putString("filter_id", filterId);
+        Intent intent = new Intent(context, OrderListActivity.class);
+        intent.setData(uri.build());
+        return intent.putExtras(extras);
     }
 
 }
