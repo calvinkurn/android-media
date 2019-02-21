3a6297895a9d4954d297d78a40fc622233ec0513
Meyta <meytajennist@gmail.com>
2019-02-21 16:30:08 +0700

    ready to PR

--- tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/inboxv2/view/activity/ResoInboxActivity.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/rescenter/inboxv2/view/activity/ResoInboxActivity.java
@@ -72,9 +72,8 @@ public class ResoInboxActivity extends BasePresenterActivity implements HasCompo
     }
 
     private static boolean isToggleResoEnabled(Context context) {
-//        RemoteConfig remoteConfig = new FirebaseRemoteConfigImpl(context);
-//        return remoteConfig.getBoolean(APP_WEBVIEW_RESO_ENABLED_TOGGLE);
-        return true;
+        RemoteConfig remoteConfig = new FirebaseRemoteConfigImpl(context);
+        return remoteConfig.getBoolean(APP_WEBVIEW_RESO_ENABLED_TOGGLE);
     }
 
     private static Intent getApplinkIntent(Context context, String url) {
