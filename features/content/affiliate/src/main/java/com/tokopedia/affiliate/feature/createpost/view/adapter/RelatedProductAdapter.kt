7435d12336ddedb91df9a915f995f162e1b656ec
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-02-21 17:20:09 +0700

    Fix delete item :bug:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/adapter/RelatedProductAdapter.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/adapter/RelatedProductAdapter.kt
@@ -45,12 +45,13 @@ class RelatedProductAdapter : RecyclerView.Adapter<RelatedProductAdapter.ViewHol
         holder.itemView.name.text = element.name
         holder.itemView.price.text = element.price
         holder.itemView.delete.setOnClickListener {
-            val newList= ArrayList(list)
-            newList.removeAt(holder.adapterPosition)
-            if (newList.isEmpty()) {
-                newList.add(emptyItem)
+            list.removeAt(holder.adapterPosition)
+            notifyItemRemoved(holder.adapterPosition)
+
+            if (list.isEmpty()) {
+                list.add(emptyItem)
+                notifyItemInserted(list.size)
             }
-            addAll(newList)
         }
     }
 
