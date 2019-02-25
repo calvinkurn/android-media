e4f4672f43420b476d895d5830bf458a0ae294d1
Meyta <meytajennist@gmail.com>
2019-02-25 11:17:13 +0700

    try to fix

--- tkpdinbox/src/main/java/com/tokopedia/inbox/common/ResolutionUrl.java
+++ tkpdinbox/src/main/java/com/tokopedia/inbox/common/ResolutionUrl.java
@@ -9,7 +9,7 @@ public class ResolutionUrl {
     private static final String MOBILE =  "/mobile";
 
     public static final String RESO_CREATE = HOSTNAME + "resolution-center/create/%s" + MOBILE;
-    private static final String RESO_INBOX =  HOSTNAME + "resolution-center/inbox";
+    private static final String RESO_INBOX =  HOSTNAME + "resolution-center/inbox/";
     public static final String RESO_DETAIL =  HOSTNAME + "resolution-center/detail/%s" + MOBILE;
 
     public static final String RESO_INBOX_SELLER =  RESO_INBOX + "seller" + MOBILE;
