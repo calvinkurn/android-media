35cd5cc0dceb3a415520fb90ab41a4ea8f0b64c2
yfsx <yoas.fs@gmail.com>
2019-02-26 17:13:22 +0700

    tes 6

--- features/content/profile/src/main/java/com/tokopedia/profile/view/fragment/ProfileFragment.kt
+++ features/content/profile/src/main/java/com/tokopedia/profile/view/fragment/ProfileFragment.kt
@@ -586,12 +586,10 @@ class ProfileFragment : BaseListFragment<Visitable<*>, BaseAdapterTypeFactory>()
         app_bar_layout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
 
             override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-                if (verticalOffset == 0) {
-//                    toolbar_parallax.visibility = View.INVISIBLE
-                } else if (Math.abs(verticalOffset) >= appBarLayout.totalScrollRange) {
-
+                if (Math.abs(verticalOffset) >= appBarLayout.totalScrollRange) {
+                    toolbar_menu.visibility = View.VISIBLE
                 } else {
-//                    toolbar_parallax.visibility = View.VISIBLE
+                    toolbar_menu.visibility = View.GONE
                 }
 
             }
