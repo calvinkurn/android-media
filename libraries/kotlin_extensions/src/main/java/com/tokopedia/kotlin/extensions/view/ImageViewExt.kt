8b907fefa74d0d7948ab9d91db85b29836ef38e4
Erry Suprayogi <eriksuprayogi@gmail.com>
2019-02-26 16:49:52 +0700

    ads impression support kotlin ext imageview
    (cherry picked from commit 5557a9d)

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/ImageViewExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/ImageViewExt.kt
@@ -1,10 +1,15 @@
 package com.tokopedia.kotlin.extensions.view
 
+import android.content.res.Resources
+import android.graphics.Rect
 import android.support.annotation.DrawableRes
+import android.view.View
+import android.view.ViewTreeObserver
 import android.widget.ImageView
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.kotlin.extensions.R
+import com.tokopedia.kotlin.model.ImpressHolder
 
 /**
  * @author by milhamj on 30/11/18.
@@ -27,7 +32,7 @@ fun ImageView.loadImageWithoutPlaceholder(@DrawableRes drawableId: Int) {
 }
 
 fun ImageView.loadImageWithoutPlaceholder(url: String) {
-    ImageHandler.loadImageWithoutPlaceholderAndError( this, url)
+    ImageHandler.loadImageWithoutPlaceholderAndError(this, url)
 }
 
 fun ImageView.loadDrawable(@DrawableRes drawableId: Int) {
@@ -36,4 +41,75 @@ fun ImageView.loadDrawable(@DrawableRes drawableId: Int) {
 
 fun ImageView.clearImage() {
     ImageHandler.clearImage(this)
+}
+
+fun ImageView.setImpression(holder: ImpressHolder, listener: ViewHintListener) {
+    impressHolder = holder;
+    hintListener = listener;
+    this.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener{
+        override fun onViewAttachedToWindow(v: View?) {
+            if (impressHolder != null && !impressHolder!!.isInvoke) {
+                invoke(this)
+            }
+        }
+
+        override fun onViewDetachedFromWindow(v: View?) {
+            if (impressHolder != null && impressHolder!!.isInvoke) {
+                revoke(this)
+            }
+        }
+    })
+}
+
+private var hintListener: ViewHintListener? = null
+private var scrollChangedListener: ViewTreeObserver.OnScrollChangedListener? = null
+private var impressHolder: ImpressHolder? = null
+
+private fun invoke(view: View?) {
+    view.viewTreeObserver.addOnScrollChangedListener(
+            object : ViewTreeObserver.OnScrollChangedListener {
+                override fun onScrollChanged() {
+                    scrollChangedListener = this
+                    if (isVisible(view)) {
+                        if (impressHolder != null && !impressHolder!!.isInvoke) {
+                            if (hintListener != null) {
+                                hintListener!!.onViewHint()
+                            }
+                            impressHolder!!.invoke()
+                        }
+                        revoke()
+                    }
+                }
+            })
+}
+
+private fun revoke(view: View?) {
+    view.viewTreeObserver.removeOnScrollChangedListener(scrollChangedListener)
+}
+
+interface ViewHintListener {
+    fun onViewHint()
+}
+
+private fun isVisible(view: View?): Boolean {
+    if (view == null) {
+        return false
+    }
+    if (!view.isShown) {
+        return false
+    }
+    val screenWidth: Int = Resources.getSystem().displayMetrics.widthPixels
+    val screenHeight: Int = Resources.getSystem().displayMetrics.heightPixels
+    val screen = Rect(0, 0, screenWidth, screenHeight)
+
+    val location = IntArray(2)
+    view.getLocationOnScreen(location)
+    val X = location[0].toFloat()
+    val Y = location[1].toFloat()
+    return if (screen.top <= Y && screen.bottom >= Y &&
+            screen.left <= X && screen.right >= X) {
+        true
+    } else {
+        false
+    }
 }
