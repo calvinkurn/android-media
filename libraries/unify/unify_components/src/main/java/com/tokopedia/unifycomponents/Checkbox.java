d4492d41aedb00f74854351482f6928eb24b81e9
Meyta <meytajennist@gmail.com>
2019-02-27 18:46:31 +0700

    add some components

--- libraries/unify/unify_components/src/main/java/com/tokopedia/unifycomponents/Checkbox.java
+++ libraries/unify/unify_components/src/main/java/com/tokopedia/unifycomponents/Checkbox.java
@@ -1,7 +1,23 @@
 package com.tokopedia.unifycomponents;
 
+import android.content.Context;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.util.AttributeSet;
+
 /**
  * Created by meta on 27/02/19.
  */
-public class Checkbox {
+public class Checkbox extends AppCompatCheckBox {
+
+    public Checkbox(Context context) {
+        super(context);
+    }
+
+    public Checkbox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Checkbox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
