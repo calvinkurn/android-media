15108a8d646db8b48482ea6d6039dbcbce8be012
fwidjaja <fwidjaja@live.com>
2019-02-27 15:35:41 +0700

    add DI module

---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/promostacking/di/PromoMerchantModule.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.checkout.view.feature.promostacking.di
+
+/**
+ * Created by fwidjaja on 27/02/19.
+ */
+class PromoMerchantModule {
+}
--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/promostacking/di/PromoMerchantScope.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/promostacking/di/PromoMerchantScope.kt
@@ -1,13 +1,12 @@
 package com.tokopedia.checkout.view.feature.promostacking.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 import javax.inject.Scope
+import kotlin.annotation.Retention
 
 /**
  * Created by fwidjaja on 27/02/19.
  */
 
 @Scope
-@Retention(RetentionPolicy.CLASS)
+@Retention(AnnotationRetention.BINARY)
 annotation class PromoMerchantScope
