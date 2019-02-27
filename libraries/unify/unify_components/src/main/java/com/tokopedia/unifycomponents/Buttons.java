c338003c4e70b8ae645c535c189502c52235f208
Meyta <meytajennist@gmail.com>
2019-02-27 18:17:06 +0700

    add template for components

---
+++ libraries/unify/unify_components/src/main/java/com/tokopedia/unifycomponents/Buttons.java
@@ -0,0 +1,23 @@
+package com.tokopedia.unifycomponents;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+/**
+ * Created by meta on 27/02/19.
+ */
+public class Buttons extends AppCompatButton {
+
+    public Buttons(Context context) {
+        super(context);
+    }
+
+    public Buttons(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Buttons(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ libraries/unify/unify_principles/src/main/java/com/tokopedia/unifyprinciples/Typography.java
@@ -0,0 +1,7 @@
+package com.tokopedia.unifyprinciples;
+
+/**
+ * Created by meta on 27/02/19.
+ */
+public class Typography {
+}
