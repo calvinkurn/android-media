3875ebb43061eb05e2dd4c545011ecd53e755c4b
sachinbansal91 <sachin.bansal@tokopedia.com>
2019-02-28 17:32:17 +0530

    buy emas value update

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/adapter/ShipmentAdapter.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/adapter/ShipmentAdapter.java
@@ -432,11 +432,10 @@ public class ShipmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     private void updateEmasCostModel() {
         double totalPrice = shipmentCostModel.getTotalPrice();
         int valueTOCheck = (int) (totalPrice % LAST_THREE_DIGIT_MODULUS);
-        int buyEgoldValue = 0, k =0;
+        int buyEgoldValue = 0;
         for (int i = egoldAttributeModel.getMinEgoldRange(); i <= egoldAttributeModel.getMaxEgoldRange(); i++) {
-            valueTOCheck += k;
-            k++;
-            if (valueTOCheck % LAST_THREE_DIGIT_MODULUS == 0) {
+
+            if (((valueTOCheck + i) % LAST_THREE_DIGIT_MODULUS) == 0) {
                 buyEgoldValue = i;
                 break;
             }
