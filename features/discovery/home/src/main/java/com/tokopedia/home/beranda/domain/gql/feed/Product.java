38435ac890542ca01c6d7e5153fab0ca0048eeb3
Henry Priyono <henry.priyono@tokopedia.com>
2019-02-28 18:20:13 +0700

    Add Discount And SlashedPrice ProductCard

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/gql/feed/Product.java
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/gql/feed/Product.java
@@ -52,6 +52,9 @@ public class Product {
     @SerializedName("slashed_price_int")
     @Expose
     private Integer slashedPriceInt;
+    @SerializedName("discount_percentage")
+    @Expose
+    private Integer discountPercentage;
     @SerializedName("rating")
     @Expose
     private Integer rating;
@@ -189,6 +192,14 @@ public class Product {
 
     public void setSlashedPriceInt(Integer slashedPriceInt) {
         this.slashedPriceInt = slashedPriceInt;
+    }
+
+    public Integer getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(Integer discountPercentage) {
+        this.discountPercentage = discountPercentage;
     }
 
     public Integer getRating() {
