795a22a2452e9302260ff13773f01af70e4e27b7
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-03-04 21:05:59 +0700

    create a video recorder fragment

--- libraries/video_recorder/src/main/java/com/tokopedia/videorecorder/main/VideoPickerActivity.kt
+++ libraries/video_recorder/src/main/java/com/tokopedia/videorecorder/main/VideoPickerActivity.kt
@@ -1,8 +1,14 @@
 package com.tokopedia.videorecorder.main
 
+import android.support.v4.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
 /**
  * Created by isfaaghyth on 04/03/19.
  * github: @isfaaghyth
  */
-class VideoPickerActivity {
+class VideoPickerActivity: BaseSimpleActivity() {
+
+    override fun getNewFragment(): Fragment? = null
+
 }
