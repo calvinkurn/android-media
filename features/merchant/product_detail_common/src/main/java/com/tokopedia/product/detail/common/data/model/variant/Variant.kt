d8cf78f4449036d0fa4ee4202be73c0391dc3093
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-03-04 11:18:23 +0700

    fix variant camel case

--- features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/variant/Variant.kt
+++ features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/variant/Variant.kt
@@ -8,35 +8,35 @@ import com.google.gson.annotations.SerializedName
  */
 data class Variant(
 
-        @SerializedName("ProductVariantID")
+        @SerializedName("productVariantID")
         @Expose
         val pv: Int? = null, //ex: 6528
 
-        @SerializedName("VariantID")
+        @SerializedName("variantID")
         @Expose
         val v: Int? = null, //ex:1
 
-        @SerializedName("VariantUnitID")
+        @SerializedName("variantUnitID")
         @Expose
         val vu: Int? = null, // ex: 0
 
-        @SerializedName("Name")
+        @SerializedName("name")
         @Expose
         val name: String? = null, //ex:Warna
 
-        @SerializedName("Identifier")
+        @SerializedName("identifier")
         @Expose
         val identifier: String? = null, // ex: colour
 
-        @SerializedName("UnitName")
+        @SerializedName("unitName")
         @Expose
         val unitName: String? = null, //example: International
 
-        @SerializedName("Position")
+        @SerializedName("position")
         @Expose
         val position: Int? = null, //start from 1
 
-        @SerializedName("Option")
+        @SerializedName("option")
         @Expose
         val options: List<Option> = listOf()
 ) {
@@ -46,19 +46,19 @@ data class Variant(
 
 data class Option(
 
-        @SerializedName("ProductVariantOptionID") // ex:23454
+        @SerializedName("productVariantOptionID") // ex:23454
         @Expose
         val id: Int? = null,
 
-        @SerializedName("VariantUnitValueID") //ex: 1
+        @SerializedName("variantUnitValueID") //ex: 1
         @Expose
         val vuv: Int? = null,
 
-        @SerializedName("Value")
+        @SerializedName("value")
         @Expose
         val value: String? = null, // example: "White"
 
-        @SerializedName("Hex")
+        @SerializedName("hex")
         @Expose
         val hex: String? = null, // ex:#ff3303
 
