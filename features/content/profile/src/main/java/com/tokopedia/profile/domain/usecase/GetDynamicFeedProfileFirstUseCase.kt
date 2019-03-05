a3065c3c7e242dc8c17a54402cd058d6d1490724
yfsx <yoas.fs@gmail.com>
2019-03-05 17:46:10 +0700

    fix

--- features/content/profile/src/main/java/com/tokopedia/profile/domain/usecase/GetDynamicFeedProfileFirstUseCase.kt
+++ features/content/profile/src/main/java/com/tokopedia/profile/domain/usecase/GetDynamicFeedProfileFirstUseCase.kt
@@ -106,7 +106,7 @@ class GetDynamicFeedProfileFirstUseCase
     companion object {
         fun createRequestParams(selfUserId: String, targetUserId: String): RequestParams {
             val requestParams = GetDynamicFeedUseCase.createRequestParams(selfUserId, "", GetDynamicFeedUseCase.SOURCE_PROFILE)
-            requestParams.putString(GetProfileHeaderUseCase.PARAM_USER_ID_TARGET, targetUserId)
+            requestParams.putInt(GetProfileHeaderUseCase.PARAM_USER_ID_TARGET, targetUserId.toInt())
             requestParams.putString(SOURCE_ID, targetUserId)
             return requestParams
         }
