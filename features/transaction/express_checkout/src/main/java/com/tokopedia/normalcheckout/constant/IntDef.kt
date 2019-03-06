266b25c82e7607b044ead142bd18d4c4ce293af7
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-03-06 16:55:04 +0700

    remove atc and select

--- features/transaction/express_checkout/src/main/java/com/tokopedia/normalcheckout/constant/IntDef.kt
+++ features/transaction/express_checkout/src/main/java/com/tokopedia/normalcheckout/constant/IntDef.kt
@@ -2,10 +2,9 @@ package com.tokopedia.normalcheckout.constant
 
 import android.support.annotation.IntDef
 
-@IntDef(ATC_ONLY, ATC_AND_BUY, ATC_AND_SELECT)
+@IntDef(ATC_ONLY, ATC_AND_BUY)
 @Retention(AnnotationRetention.SOURCE)
 annotation class ProductAction {}
 
 const val ATC_ONLY = 0
 const val ATC_AND_BUY = 1 // or preorder
-const val ATC_AND_SELECT = 2
