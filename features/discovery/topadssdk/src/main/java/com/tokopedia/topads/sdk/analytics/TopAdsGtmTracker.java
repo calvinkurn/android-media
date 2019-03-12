a75850732424c89334ef0501d87011d3a139aa68
Erry Suprayogi <eriksuprayogi@gmail.com>
2019-03-12 17:28:24 +0700

    clear data layer impression on pause
    (cherry picked from commit 7bd90b1)

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/analytics/TopAdsGtmTracker.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/analytics/TopAdsGtmTracker.java
@@ -102,6 +102,7 @@ public class TopAdsGtmTracker {
                         )
                 ));
         trackingQueue.putEETracking((HashMap<String, Object>) map);
+        clearDataLayerList();
     }
 
     private Object getSearchResultProductViewImpressions(Product product, int position) {
