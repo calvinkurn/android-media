1c298406acec1ee97a5e2ea7b9d3fda657546c6e
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-03-12 21:46:37 +0700

    fix host

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConstInternal.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConstInternal.java
@@ -4,7 +4,7 @@ package com.tokopedia.applink;
  * This class is used to store deeplink started with scheme "tokopedia-android-internal".
  * Since it is for android internal only, if the applink is shared between iOS or site,
  * please create with "tokopedia" scheme and put into different file.
- *
+ * <p>
  * To make this deeplnks work, These deeplinks must be registered in the manifest using intent filter
  * (see ProductDetailActivity manifest for example)
  */
@@ -13,17 +13,19 @@ public class ApplinkConstInternal {
 
     public static final String HOST_MARKETPLACE = "marketplace";
 
+    public static final String INTERNAL_MARKETPLACE = INTERNAL_SCHEME + "://" + HOST_MARKETPLACE;
+
     // ProductDetailActivity
-    public static final String PRODUCT_DETAIL           = INTERNAL_SCHEME + "://" + HOST_MARKETPLACE + "/product/{id}/";
+    public static final String PRODUCT_DETAIL = INTERNAL_MARKETPLACE + "/product/{id}/";
 
     // ProductEditActivity
-    public static final String PRODUCT_EDIT     = INTERNAL_SCHEME + "://" + HOST_MARKETPLACE + "/product/{id}/edit";
+    public static final String PRODUCT_EDIT = INTERNAL_MARKETPLACE + "/product/{id}/edit";
     // ReviewProductActivity, "x_prd_nm" = productName
-    public static final String PRODUCT_REVIEW   = INTERNAL_SCHEME + "://" + HOST_MARKETPLACE + "/product/{id}/review";
+    public static final String PRODUCT_REVIEW = INTERNAL_MARKETPLACE + "/product/{id}/review";
 
     // CheckoutVariantActivity, "EXTRA_ATC_REQUEST" = AtcRequestParam
-    public static final String EXPRESS_CHECKOUT = INTERNAL_SCHEME + "://" + HOST_MARKETPLACE + "checkoutvariant";
+    public static final String EXPRESS_CHECKOUT = INTERNAL_MARKETPLACE + "checkoutvariant";
 
     // IntermediaryActivity
-    public static final String DISCOVERY_CATEGORY_DETAIL = INTERNAL_SCHEME+ "://"+HOST_MARKETPLACE+"/category/{DEPARTMENT_ID}";
+    public static final String DISCOVERY_CATEGORY_DETAIL = INTERNAL_MARKETPLACE + "/category/{DEPARTMENT_ID}";
 }
