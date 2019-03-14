b8ef0bf05ff6c19131d9a295d48d2e1773362a3d
angga prasetiyo <agapras@gmail.com>
2019-03-14 13:52:30 +0700

    make action listener non null

--- features/promo/loyalty/src/main/java/com/tokopedia/loyalty/view/adapter/PromoPagerAdapter.java
+++ features/promo/loyalty/src/main/java/com/tokopedia/loyalty/view/adapter/PromoPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
+import com.tokopedia.loyalty.view.activity.PromoListActivity;
 import com.tokopedia.loyalty.view.data.PromoMenuData;
 import com.tokopedia.loyalty.view.fragment.PromoListFragment;
 
@@ -18,16 +19,21 @@ public class PromoPagerAdapter extends FragmentStatePagerAdapter {
 
     private final String autoSelectCategoryId;
     private List<PromoMenuData> promoMenuDataList;
+    private PromoListFragment.OnFragmentInteractionListener promoListActionListener;
 
-    public PromoPagerAdapter(FragmentManager fm, List<PromoMenuData> promoMenuDataList, String autoSelectedCategoryId) {
+    public PromoPagerAdapter(PromoListFragment.OnFragmentInteractionListener promoListActionListener,
+                             FragmentManager fm, List<PromoMenuData> promoMenuDataList,
+                             String autoSelectedCategoryId) {
         super(fm);
+        this.promoListActionListener = promoListActionListener;
         this.promoMenuDataList = promoMenuDataList;
         this.autoSelectCategoryId = autoSelectedCategoryId;
     }
 
     @Override
     public Fragment getItem(int position) {
-        return PromoListFragment.newInstance(promoMenuDataList.get(position), autoSelectCategoryId);
+        return PromoListFragment.newInstance(promoListActionListener,
+                promoMenuDataList.get(position), autoSelectCategoryId);
     }
 
     /**
