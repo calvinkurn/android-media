bb85c16863d70f0432063c0edce5478f00bb319f
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-03-14 14:50:26 +0700

    Add data model from response at saf

---
+++ features/transaction/transactiondata/src/main/java/com/tokopedia/transactiondata/entity/response/shippingaddressform/Warehouse.java
@@ -0,0 +1,221 @@
+package com.tokopedia.transactiondata.entity.response.shippingaddressform;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by fajarnuha on 14/03/19.
+ */
+public class Warehouse {
+
+    @SerializedName("warehouse_id")
+    @Expose
+    private Integer warehouseId;
+    @SerializedName("partner_id")
+    @Expose
+    private Integer partnerId;
+    @SerializedName("shop_id")
+    @Expose
+    private Integer shopId;
+    @SerializedName("warehouse_name")
+    @Expose
+    private String warehouseName;
+    @SerializedName("district_id")
+    @Expose
+    private Integer districtId;
+    @SerializedName("district_name")
+    @Expose
+    private String districtName;
+    @SerializedName("city_id")
+    @Expose
+    private Integer cityId;
+    @SerializedName("city_name")
+    @Expose
+    private String cityName;
+    @SerializedName("province_id")
+    @Expose
+    private Integer provinceId;
+    @SerializedName("province_name")
+    @Expose
+    private String provinceName;
+    @SerializedName("status")
+    @Expose
+    private Integer status;
+    @SerializedName("postal_code")
+    @Expose
+    private String postalCode;
+    @SerializedName("is_default")
+    @Expose
+    private Integer isDefault;
+    @SerializedName("latlon")
+    @Expose
+    private String latlon;
+    @SerializedName("latitude")
+    @Expose
+    private String latitude;
+    @SerializedName("longitude")
+    @Expose
+    private String longitude;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address_detail")
+    @Expose
+    private String addressDetail;
+    @SerializedName("country_name")
+    @Expose
+    private String countryName;
+
+    public Integer getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(Integer warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public Integer getPartnerId() {
+        return partnerId;
+    }
+
+    public void setPartnerId(Integer partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    public Integer getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Integer shopId) {
+        this.shopId = shopId;
+    }
+
+    public String getWarehouseName() {
+        return warehouseName;
+    }
+
+    public void setWarehouseName(String warehouseName) {
+        this.warehouseName = warehouseName;
+    }
+
+    public Integer getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Integer getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(Integer provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(Integer isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    public String getLatlon() {
+        return latlon;
+    }
+
+    public void setLatlon(String latlon) {
+        this.latlon = latlon;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddressDetail() {
+        return addressDetail;
+    }
+
+    public void setAddressDetail(String addressDetail) {
+        this.addressDetail = addressDetail;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
+
