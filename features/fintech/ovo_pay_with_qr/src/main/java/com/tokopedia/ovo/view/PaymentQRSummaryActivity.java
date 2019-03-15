648441ac191cee8f4142c907f19c05219a28fe21
sandeepnitsgr <sandeep.gupta@tokopedia.com>
2019-03-15 11:13:21 +0530

    comments removed.

--- features/fintech/ovo_pay_with_qr/src/main/java/com/tokopedia/ovo/view/PaymentQRSummaryActivity.java
+++ features/fintech/ovo_pay_with_qr/src/main/java/com/tokopedia/ovo/view/PaymentQRSummaryActivity.java
@@ -4,10 +4,6 @@ import android.support.v4.app.Fragment;
 
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
 
-/**
- * Created by nathan on 8/18/17.
- */
-
 public class PaymentQRSummaryActivity extends BaseSimpleActivity {
 
     @Override
