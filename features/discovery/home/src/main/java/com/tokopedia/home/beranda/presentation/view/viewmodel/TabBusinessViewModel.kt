0dbd98b3ba35084f29b8e491aceacb90eab67d4d
hangnadi <hangnadi@tokopedia.com>
2019-03-15 23:56:54 +0700

    add validation

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/viewmodel/TabBusinessViewModel.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/viewmodel/TabBusinessViewModel.kt
@@ -2,28 +2,21 @@ package com.tokopedia.home.beranda.presentation.view.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import android.text.TextUtils
 import com.tokopedia.abstraction.common.network.exception.ResponseErrorException
-import com.tokopedia.abstraction.common.utils.GraphqlHelper
-import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.graphql.data.model.GraphqlResponse
-import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.home.beranda.data.model.HomeWidget
 import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
 import kotlinx.coroutines.experimental.*
-import rx.Subscriber
 import javax.inject.Inject
 import javax.inject.Named
 import kotlin.coroutines.experimental.CoroutineContext
 
 class TabBusinessViewModel @Inject constructor(
         private val graphqlRepository: GraphqlRepository,
-        private val graphqlUseCase: GraphqlUseCase,
         @Named("Main")
         private val baseDispatcher: CoroutineDispatcher
 ) : ViewModel(), CoroutineScope {
@@ -35,12 +28,8 @@ class TabBusinessViewModel @Inject constructor(
         get() = baseDispatcher + job
 
     fun getTabBusinessUnit(rawQuery: String) {
-        val graphqlRequest = GraphqlRequest(rawQuery, HomeWidget.Data::class.java)
-        graphqlUseCase.clearRequest()
-        graphqlUseCase.addRequest(graphqlRequest)
-        graphqlUseCase.execute(TabBusinessSubscriber(homeWidget))
 
-        /*launchCatchError(block = {
+        launchCatchError(block = {
             val data = withContext(Dispatchers.Default) {
                 val graphqlRequest = GraphqlRequest(
                         rawQuery,
@@ -49,7 +38,8 @@ class TabBusinessViewModel @Inject constructor(
                 graphqlRepository.getReseponse(listOf(graphqlRequest))
             }
 
-            if (data.getError(HomeWidget.Data::class.java).isEmpty()) {
+            if (data.getError(HomeWidget.Data::class.java) == null ||
+                    data.getError(HomeWidget.Data::class.java).isEmpty()) {
                 if (data.getData<HomeWidget.Data>(HomeWidget.Data::class.java) != null) {
                     homeWidget.value = Success(data.getData<HomeWidget.Data>(HomeWidget.Data::class.java).homeWidget)
                 } else {
@@ -63,36 +53,11 @@ class TabBusinessViewModel @Inject constructor(
         }){
             it.printStackTrace()
             homeWidget.value = Fail(it)
-        }*/
+        }
 
     }
 
     fun clearJob() {
         if (isActive) job.cancel()
     }
-}
-
-class TabBusinessSubscriber(private val homeWidget: MutableLiveData<Result<HomeWidget>>) : Subscriber<GraphqlResponse>() {
-    override fun onNext(data: GraphqlResponse) {
-        if (data.getError(HomeWidget.Data::class.java) == null ||
-                data.getError(HomeWidget.Data::class.java).isEmpty()) {
-            if (data.getData<HomeWidget.Data>(HomeWidget.Data::class.java) != null) {
-                homeWidget.value = Success(data.getData<HomeWidget.Data>(HomeWidget.Data::class.java).homeWidget)
-            } else {
-                homeWidget.value = Fail(ResponseErrorException("local handling error"))
-            }
-        } else {
-            val message = data.getError(HomeWidget.Data::class.java)[0].message
-            homeWidget.value = Fail(ResponseErrorException(message))
-        }
-    }
-
-    override fun onCompleted() {
-
-    }
-
-    override fun onError(e: Throwable) {
-        homeWidget.value = Fail(ResponseErrorException(e.message))
-    }
-
 }
