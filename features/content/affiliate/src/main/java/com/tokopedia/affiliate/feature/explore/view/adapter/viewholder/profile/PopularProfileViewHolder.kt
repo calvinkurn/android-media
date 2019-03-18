472c3595cc193a228f9756fb67011ac4e704b631
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-03-18 19:33:46 +0700

    Add item decoration :lipstick:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/adapter/viewholder/profile/PopularProfileViewHolder.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/explore/view/adapter/viewholder/profile/PopularProfileViewHolder.kt
@@ -1,10 +1,13 @@
 package com.tokopedia.affiliate.feature.explore.view.adapter.viewholder.profile
 
+import android.graphics.Rect
 import android.support.annotation.LayoutRes
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.affiliate.R
 import com.tokopedia.affiliate.feature.explore.view.viewmodel.PopularProfileViewModel
+import com.tokopedia.kotlin.extensions.view.getDimens
 import kotlinx.android.synthetic.main.item_af_popular_profile.view.*
 
 /**
@@ -14,6 +17,15 @@ class PopularProfileViewHolder(v: View) : AbstractViewHolder<PopularProfileViewM
 
     val adapter: PopularProfileAdapter by lazy {
         PopularProfileAdapter()
+    }
+
+    init {
+        itemView.profileRv.addItemDecoration(object : RecyclerView.ItemDecoration(){
+            override fun getItemOffsets(outRect: Rect, view: View,
+                                        parent: RecyclerView, state: RecyclerView.State) {
+                outRect.right = itemView.getDimens(R.dimen.dp_8)
+            }
+        })
     }
 
     companion object {
