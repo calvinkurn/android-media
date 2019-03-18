e8c4bf0ed7df31d349ae202f805d8999908255bf
hangnadi <hangnadi@tokopedia.com>
2019-03-18 15:48:14 +0700

    error fix

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/SizeMiddleBusinessViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/SizeMiddleBusinessViewHolder.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.AppCompatImageView
 import android.view.View
 import android.widget.TextView
 import com.tokopedia.home.R
+import kotlinx.android.synthetic.main.layout_template_icon_business_widget.view.*
 import kotlinx.android.synthetic.main.layout_template_mid_business.view.*
 
 class SizeMiddleBusinessViewHolder (itemView: View?) : SizeSmallBusinessViewHolder(itemView) {
