3e57b514cccca7fe721c1287b29abe458d918137
Meyta <meytajennist@gmail.com>
2019-03-19 20:16:01 +0700

    Right Round Corner

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/compoundview/HeaderHomeView.java
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/compoundview/HeaderHomeView.java
@@ -5,8 +5,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -23,6 +29,10 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
 import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker;
 import com.tokopedia.abstraction.common.utils.image.ImageHandler;
 import com.tokopedia.applink.ApplinkConst;
@@ -121,11 +131,11 @@ public class HeaderHomeView extends BaseCustomView {
             View container = view.findViewById(R.id.container_nonlogin);
             AppCompatImageView imgNonLogin = view.findViewById(R.id.bg_container_nonlogin);
 
+            int radius = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics()));
+
             Glide.with(getContext())
                     .load(BG_CONTAINER_URL)
-                    .bitmapTransform(new RoundedCornersTransformation(getContext(), Math.round(TypedValue.applyDimension(
-                            TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics())), 0,
-                            RoundedCornersTransformation.CornerType.RIGHT))
+                    .bitmapTransform(new RoundedRightCornerTransformation(getContext(), 8))
                     .into(imgNonLogin);
 
             container.setOnClickListener(onCheckNowListener());
@@ -413,5 +423,46 @@ public class HeaderHomeView extends BaseCustomView {
 
             listener.actionAppLinkWalletHeader(homeHeaderWalletAction.getAppLinkBalance());
         };
+    }
+
+    class RoundedRightCornerTransformation implements Transformation<Bitmap> {
+
+        private BitmapPool mBitmapPool;
+        private int mRadius;
+        private Context mContext;
+
+        RoundedRightCornerTransformation(Context context, int radius) {
+            mContext = context;
+            mBitmapPool = Glide.get(context).getBitmapPool();
+            mRadius = radius;
+        }
+
+        @Override
+        public Resource<Bitmap> transform(Resource<Bitmap> resource, int outWidth, int outHeight) {
+            Bitmap source = resource.get();
+
+            int radius = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, mRadius, mContext.getResources().getDisplayMetrics()));
+
+            int width = source.getWidth();
+            int height = source.getHeight();
+
+            Bitmap bitmap = mBitmapPool.get(width, height, Bitmap.Config.ARGB_8888);
+            if (bitmap == null) {
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            }
+
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setShader(new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+            canvas.drawRoundRect(new RectF(width -  8 * 2, 0, width, height), radius, radius,
+                    paint);
+            canvas.drawRect(new RectF(0, 0, width - radius, height), paint);
+            return BitmapResource.obtain(bitmap, mBitmapPool);
+        }
+
+        @Override public String getId() {
+            return "RoundedRightCornerTransformation";
+        }
     }
 }
