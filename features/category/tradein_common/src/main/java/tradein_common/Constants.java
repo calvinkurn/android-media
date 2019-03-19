6eb0df5473a2a31668873560b32ff0071b097631
pranaytokopedia <pranay.mohapatra@tokopedia.com>
2019-03-19 15:49:48 +0530

    added base url

--- features/category/tradein_common/src/main/java/tradein_common/Constants.java
+++ features/category/tradein_common/src/main/java/tradein_common/Constants.java
@@ -1,7 +1,7 @@
 package tradein_common;
 
 public class Constants {
-    public static String LAKU6_BASEURL = "https://www.laku6.com/";
+    public static String LAKU6_BASEURL = "https://www.laku6.com";
     public static final String APIKEY = "AIzaSyAJk4XqSdT0qWg5ahBxWYT5CJ9vzv2TqS4";
     public static final String APPID = "1:109002668043:android:f4cc247c743f7921";
 }
