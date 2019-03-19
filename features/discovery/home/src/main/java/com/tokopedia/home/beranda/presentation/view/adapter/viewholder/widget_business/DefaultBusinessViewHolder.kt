c7cba3b1f0654727119973a61dae2faeec4598d8
hangnadi <hangnadi@tokopedia.com>
2019-03-19 18:54:58 +0700

    fix crash

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/DefaultBusinessViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/DefaultBusinessViewHolder.kt
@@ -2,12 +2,14 @@ package com.tokopedia.home.beranda.presentation.view.adapter.viewholder.widget_b
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
-import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyViewHolder
 import com.tokopedia.home.R
-import com.tokopedia.home.beranda.data.model.HomeWidget
 
 class DefaultBusinessViewHolder (itemView: View?) : EmptyViewHolder(itemView) {
+
+    override fun bind(element: EmptyModel?) {
+
+    }
 
     companion object {
         val LAYOUT: Int = R.layout.layout_template_empty_business_widget
