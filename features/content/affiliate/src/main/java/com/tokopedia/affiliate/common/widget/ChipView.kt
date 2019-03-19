4295bb1c15e1995b5051993b485e9ad45ef14d9c
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-03-19 19:10:34 +0700

    Add style to chip :sparkles:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ChipView.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ChipView.kt
@@ -1,8 +1,10 @@
 package com.tokopedia.affiliate.common.widget
 
 import android.content.Context
+import android.graphics.Typeface
 import android.support.annotation.DrawableRes
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import com.tokopedia.affiliate.R
 import com.tokopedia.design.base.BaseCustomView
@@ -14,24 +16,57 @@ import kotlinx.android.synthetic.main.widget_af_chip.view.*
  */
 class ChipView : BaseCustomView {
 
+    private var mSize: Int = LARGE
+
     companion object {
         const val NO_ICON = -1
+
+        private const val LARGE = 1
+        private const val SMALL = 2
+
     }
 
     constructor(context: Context) : super(context) {
-        init()
+        initView()
     }
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        init()
+        initAttrs(attrs)
+        initView()
     }
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init()
+        initAttrs(attrs)
+        initView()
     }
 
-    private fun init() {
+    private fun initAttrs(attrs: AttributeSet?) {
+        attrs?.let {
+            val attributeArray = context.obtainStyledAttributes(attrs, R.styleable.ChipView)
+            mSize = attributeArray.getInteger(R.styleable.ChipView_chipViewSize, LARGE)
+            attributeArray.recycle()
+        }
+    }
+
+    private fun initView() {
         val rootView = View.inflate(context, R.layout.widget_af_chip, this)
         rootView.isSelected = false
         icon.hide()
+
+        when(mSize) {
+            LARGE -> {
+                title.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        title.resources.getDimension(R.dimen.sp_12)
+                )
+                title.typeface = Typeface.create("sans-serif-black", Typeface.BOLD)
+            }
+            SMALL -> {
+                title.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        title.resources.getDimension(R.dimen.sp_10)
+                )
+                title.typeface = Typeface.create("sans-serif", Typeface.NORMAL)
+            }
+        }
     }
 
     fun setTitle(titleText: String) {
