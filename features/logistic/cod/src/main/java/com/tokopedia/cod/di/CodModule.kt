ad6a7a2ee47c04850747cbc60ac7a74ff2a83ea9
M normansyah <noiz354@gmail.com>
2019-03-19 14:02:02 +0700

    add trackapp  trackinterface to affected modules. convert codes to trackapp implementations.

--- features/logistic/cod/src/main/java/com/tokopedia/cod/di/CodModule.kt
+++ features/logistic/cod/src/main/java/com/tokopedia/cod/di/CodModule.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.cod.di
 
 import com.tokopedia.abstraction.AbstractionRouter
-import com.tokopedia.abstraction.common.data.model.analytic.AnalyticTracker
 import com.tokopedia.cod.view.CodContract
 import com.tokopedia.cod.view.CodPresenter
 import com.tokopedia.logisticanalytics.CodAnalytics
@@ -17,10 +16,6 @@ class CodModule{
     @CodScope
     @Provides
     fun providePresenter(presenter: CodPresenter): CodContract.Presenter = presenter
-
-    @CodScope
-    @Provides
-    fun provideAnalyticsTracker(router: AbstractionRouter): AnalyticTracker = router.analyticTracker
 
     @CodScope
     @Provides
