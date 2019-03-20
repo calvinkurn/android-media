95f337f64acb47202b3ae534c0354e8b1930f473
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-03-20 12:34:06 +0700

    showing a dialog in createPostVideo if image exist

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/activity/CreatePostVideoPickerActivity.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/activity/CreatePostVideoPickerActivity.kt
@@ -3,6 +3,8 @@ package com.tokopedia.affiliate.feature.createpost.view.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.tokopedia.affiliate.R
+import com.tokopedia.design.component.Dialog
 import com.tokopedia.videorecorder.main.VideoPickerActivity
 
 /**
@@ -14,7 +16,20 @@ class CreatePostVideoPickerActivity: VideoPickerActivity() {
     override fun onVideoDoneClicked() {
         val isImageExist = intent?.getBooleanExtra(CreatePostVideoPickerActivity.IMAGE_EXIST, false)?: false
         if (isImageExist) {
-            //TODO(show dialog)
+            val dialog = Dialog(this, Dialog.Type.PROMINANCE)
+            dialog.setTitle(getString(R.string.af_title_update_post))
+            dialog.setDesc(getString(R.string.af_message_update_post))
+            dialog.setBtnOk(getString(R.string.cancel))
+            dialog.setBtnCancel(getString(R.string.af_continue))
+            dialog.setOnOkClickListener{
+                dialog.dismiss()
+            }
+            dialog.setOnCancelClickListener{
+                dialog.dismiss()
+                super.onVideoDoneClicked()
+            }
+            dialog.setCancelable(true)
+            dialog.show()
         } else {
             super.onVideoDoneClicked()
         }
