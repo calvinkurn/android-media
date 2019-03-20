e3f105fa6a756a6a8b7bc208a26f28cabc942a9e
okasurya <brilliantsurya@gmail.com>
2019-03-20 15:41:18 +0700

    fix calculating px to dp

--- libraries/react_native/src/main/java/com/tokopedia/tkpdreactnative/react/ReactCommonModule.java
+++ libraries/react_native/src/main/java/com/tokopedia/tkpdreactnative/react/ReactCommonModule.java
@@ -144,7 +144,7 @@ public class ReactCommonModule extends ReactContextBaseJavaModule {
         if(getCurrentActivity() != null) {
             int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
             if (resourceId > 0) {
-                result = (int) (context.getResources().getDimensionPixelSize(resourceId) / ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+                result = (int) (context.getResources().getDimensionPixelSize(resourceId) / context.getResources().getDisplayMetrics().density);
             }
         }
 
