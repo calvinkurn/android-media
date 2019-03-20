63854a054cad7eb4c61854366ebe1b05c447ce90
hangnadi <hangnadi@tokopedia.com>
2019-03-20 18:14:59 +0700

    error crash

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/ErrorBusinessViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/ErrorBusinessViewHolder.kt
@@ -1,16 +1,19 @@
 package com.tokopedia.home.beranda.presentation.view.adapter.viewholder.widget_business
 
 import android.view.View
+import com.tokopedia.abstraction.base.view.adapter.model.ErrorNetworkModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.base.view.adapter.viewholders.ErrorNetworkViewHolder
 import com.tokopedia.home.R
 import com.tokopedia.home.beranda.data.model.HomeWidget
 import com.tokopedia.home.beranda.presentation.view.fragment.BusinessUnitItemView
 import kotlinx.android.synthetic.main.layout_template_error_business_widget.view.*
 
 class ErrorBusinessViewHolder (itemView: View?,
-                               private val listener: BusinessUnitItemView) : AbstractViewHolder<HomeWidget.ContentItemTab>(itemView) {
+                               private val listener: BusinessUnitItemView)
+    : ErrorNetworkViewHolder(itemView) {
 
-    override fun bind(element: HomeWidget.ContentItemTab?) {
+    override fun bind(errorNetworkModel: ErrorNetworkModel?) {
         itemView.buttonReload.setOnClickListener {
             listener.onReloadButtonClick()
         }
