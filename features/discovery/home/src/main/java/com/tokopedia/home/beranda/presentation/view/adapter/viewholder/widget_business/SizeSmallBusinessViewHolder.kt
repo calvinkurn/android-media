c12cb0ba7ecff301060f43f943eb1abfadcc4916
hangnadi <hangnadi@tokopedia.com>
2019-03-21 13:10:46 +0700

    color label

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/SizeSmallBusinessViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/widget_business/SizeSmallBusinessViewHolder.kt
@@ -145,12 +145,20 @@ open class SizeSmallBusinessViewHolder (itemView: View?) : AbstractViewHolder<Ho
                         itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_pink))
                     }
                     2 -> {
-                        itemView.background = ContextCompat.getDrawable(itemView.context, R.drawable.bg_rounded_yellow_label)
-                        itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_yellow))
+                        ContextCompat.getDrawable(itemView.context, R.drawable.bg_rounded_blue_label)
+                        itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_blue))
                     }
                     3 -> {
                         ContextCompat.getDrawable(itemView.context, R.drawable.bg_rounded_green_label)
                         itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_green))
+                    }
+                    4 -> {
+                        itemView.background = ContextCompat.getDrawable(itemView.context, R.drawable.bg_rounded_yellow_label)
+                        itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_yellow))
+                    }
+                    5 -> {
+                        ContextCompat.getDrawable(itemView.context, R.drawable.bg_rounded_grey_label)
+                        itemView.tagLine.setTextColor(ContextCompat.getColor(itemView.context, R.color.label_grey))
                     }
                     else -> {
                         itemView.tagLine.visibility = View.GONE
