64b6724a1329ba695c5ff58fec25b907c86d552c
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2019-03-21 10:56:54 +0700

    add new module instant debit bca and bcaxcowidget aar

---
+++ features/digital/instant_debit_bca/src/main/java/com/tokopedia/instantdebitbca/DirectDebitBcaActivity.java
@@ -0,0 +1,29 @@
+package com.tokopedia.instantdebitbca;
+
+import com.bca.xco.widget.BCAXCOListener;
+
+/**
+ * Created by nabillasabbaha on 21/03/19.
+ */
+public class DirectDebitBcaActivity implements BCAXCOListener {
+
+    @Override
+    public void onBCASuccess(String s, String s1, String s2, String s3) {
+
+    }
+
+    @Override
+    public void onBCATokenExpired(String s) {
+
+    }
+
+    @Override
+    public void onBCARegistered(String s) {
+
+    }
+
+    @Override
+    public void onBCACloseWidget() {
+
+    }
+}
