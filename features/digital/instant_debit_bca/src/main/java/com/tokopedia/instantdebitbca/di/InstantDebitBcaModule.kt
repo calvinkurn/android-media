0449b647e76f7ea8740cf18fcb2d598a202cb0dc
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2019-03-22 11:42:24 +0700

    add provide user session in instant debit bca module

--- features/digital/instant_debit_bca/src/main/java/com/tokopedia/instantdebitbca/di/InstantDebitBcaModule.kt
+++ features/digital/instant_debit_bca/src/main/java/com/tokopedia/instantdebitbca/di/InstantDebitBcaModule.kt
@@ -1,6 +1,11 @@
 package com.tokopedia.instantdebitbca.di
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
+import dagger.Provides
 
 /**
  * Created by nabillasabbaha on 21/03/19.
@@ -8,4 +13,9 @@ import dagger.Module
 @Module
 class InstantDebitBcaModule {
 
+    @InstantDebitBcaScope
+    @Provides
+    fun provideUserSessionInterface(@ApplicationContext context: Context): UserSessionInterface {
+        return UserSession(context)
+    }
 }
