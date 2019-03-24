f5efed8a482b5c1d0ec23d1c1d0bb7bae053ee28
sachinbansal91 <sachin.bansal@tokopedia.com>
2019-03-24 12:15:52 +0530

    test new PDP - revert

--- libraries/applink/src/main/java/com/tokopedia/applink/ProductDetailRouteManager.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ProductDetailRouteManager.java
@@ -34,8 +34,8 @@ public class ProductDetailRouteManager {
 
     public static boolean isGoToOldProductDetail(Context context) {
         //TODO for next release
-//        return getRemoteConfig(context).getBoolean(RemoteConfigKey.MAIN_APP_DISABLE_NEW_PRODUCT_DETAIL);
-        return false;
+        return getRemoteConfig(context).getBoolean(RemoteConfigKey.MAIN_APP_DISABLE_NEW_PRODUCT_DETAIL);
+//        return false;
     }
 
     public static Intent getProductIntent(Context context, String productApplink) {
