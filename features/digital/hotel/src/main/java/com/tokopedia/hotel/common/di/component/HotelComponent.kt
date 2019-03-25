a285ec22c6d7b94c778816cf5c521ac8c5931a5c
Muhammad Furqan <nanjihusaha@gmail.com>
2019-03-25 20:23:28 +0700

    fix hotel build error

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/common/di/component/HotelComponent.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/common/di/component/HotelComponent.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.hotel.common.di.component
 
+import android.content.Context
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.hotel.common.di.module.HotelModule
@@ -7,7 +8,6 @@ import com.tokopedia.hotel.common.di.scope.HotelScope
 import com.tokopedia.hotel.common.presentation.HotelBaseActivity
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Component
-import javax.naming.Context
 
 /**
  * @author by furqan on 25/03/19
