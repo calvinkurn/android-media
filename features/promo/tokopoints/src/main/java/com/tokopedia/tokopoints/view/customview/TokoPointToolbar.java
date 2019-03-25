a557ec74d1ad8f256c4ff1e1155e6f23bf41f4b9
lalitsinghtokopedia <lalit.singh@tokopedia.com>
2019-03-25 12:07:07 +0530

    TokoPointToolbar Changes

---
+++ features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/customview/TokoPointToolbar.java
@@ -0,0 +1,100 @@
+package com.tokopedia.tokopoints.view.customview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tokopedia.tokopoints.R;
+
+public class TokoPointToolbar extends Toolbar {
+
+    private ToolbarState currentToolbarState = ToolbarState.TOOLBAR_TRANSPARENT;
+
+    TextView tvCouponCount;
+
+    ImageView ivLeaderBoard, ivMyCoupon;
+    Context mContext;
+
+
+    public TokoPointToolbar(Context context) {
+        super(context);
+        inflateResource(context);
+    }
+
+    public TokoPointToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        inflateResource(context);
+    }
+
+    public TokoPointToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        inflateResource(context);
+    }
+
+    private void inflateResource(Context context) {
+        mContext = context;
+        View.inflate(context, R.layout.tp_toolbar, this);
+        tvCouponCount = findViewById(R.id.tv_tpToolbar_couponCount);
+        ivLeaderBoard = findViewById(R.id.tv_tpToolbar_leaderboard);
+        ivMyCoupon = findViewById(R.id.iv_tpToolbar_coupon);
+        switchToTransparentMode();
+    }
+
+    enum ToolbarState {
+        TOOLBAR_DARK, TOOLBAR_TRANSPARENT
+    }
+
+    public void setCouponCount(int couponCount) {
+        if (couponCount == 0) {
+            tvCouponCount.setVisibility(INVISIBLE);
+        } else {
+            tvCouponCount.setVisibility(VISIBLE);
+            if (couponCount > 99) {
+                tvCouponCount.setText(String.format("%s", tvCouponCount));
+            } else {
+                tvCouponCount.setText("99+");
+            }
+        }
+    }
+
+    public void switchToDarkMode() {
+        if (currentToolbarState == ToolbarState.TOOLBAR_DARK)
+            return;
+        currentToolbarState = ToolbarState.TOOLBAR_DARK;
+        ivLeaderBoard.setImageResource(R.drawable.ic_tp_leaderboard_grey);
+        ivMyCoupon.setImageResource(R.drawable.ic_my_coupon_grey);
+        this.setNavigationIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_back_grey));
+    }
+
+    public void switchToTransparentMode() {
+        if (currentToolbarState == ToolbarState.TOOLBAR_TRANSPARENT)
+            return;
+        currentToolbarState = ToolbarState.TOOLBAR_TRANSPARENT;
+        ivLeaderBoard.setImageResource(R.drawable.ic_leaderboard);
+        ivMyCoupon.setImageResource(R.drawable.ic_my_coupon_white);
+        this.setNavigationIcon(ContextCompat.getDrawable(mContext, R.drawable.ic_action_back));
+    }
+
+    public void applyAlphaToToolbarBackground(float alpha) {
+        setBackgroundColor(
+                adjustAlpha(mContext.getResources().getColor(R.color.white), alpha));
+    }
+
+    @ColorInt
+    public static int adjustAlpha(@ColorInt int color, float factor) {
+        int alpha = Math.round(Color.alpha(color) * factor);
+        int red = Color.red(color);
+        int green = Color.green(color);
+        int blue = Color.blue(color);
+        return Color.argb(alpha, red, green, blue);
+    }
+
+
+}
