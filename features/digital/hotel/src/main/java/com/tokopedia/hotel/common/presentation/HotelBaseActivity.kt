85653b6bf489004b1e121edaa0ce25447de50691
Muhammad Furqan <nanjihusaha@gmail.com>
2019-03-25 19:39:00 +0700

    hotel base activity

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/common/presentation/HotelBaseActivity.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/common/presentation/HotelBaseActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.design.component.Menus
 import com.tokopedia.hotel.HotelComponentInstance
 import com.tokopedia.hotel.common.di.component.HotelComponent
 
@@ -14,6 +15,7 @@ import com.tokopedia.hotel.common.di.component.HotelComponent
 abstract class HotelBaseActivity: BaseSimpleActivity() {
 
     private var hotelComponent: HotelComponent? = null
+    private lateinit var menus: Menus
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +33,51 @@ abstract class HotelBaseActivity: BaseSimpleActivity() {
         getHotelComponent().inject(this)
     }
 
+    override fun onMenuOpened(featureId: Int, menu: Menu?): Boolean {
+        showBottomMenus()
+        return false
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menu?.clear()
-        // waiting for menu list from tribe
-        //        menuInflater.inflate(R.menu.some_menu, menu)
-        updateOptionMenuColorWhite(menu)
-        return shouldShowOptionMenu()
+        if (shouldShowOptionMenu()) {
+            menuInflater.inflate(R.menu.hotel_base_menu, menu)
+            updateOptionMenuColorWhite(menu)
+        }
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        // waiting for menu list from tribe
+        if (shouldShowOptionMenu()) {
+            if (item?.itemId ?: "" == R.id.action_overflow_menu) {
+                showBottomMenus()
+                return true
+            }
+        }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun showBottomMenus() {
+        menus = Menus(this)
+        val menuItem = arrayOf(resources.getString(R.string.hotel_homepage_bottom_menu_transaction_list),
+                resources.getString(R.string.hotel_homepage_bottom_menu_promo),
+                resources.getString(R.string.hotel_homepage_bottom_menu_help))
+        menus.setItemMenuList(menuItem)
+
+        menus.setOnActionClickListener { view -> menus.dismiss() }
+
+        menus.setOnItemMenuClickListener { itemMenus, pos ->
+/*            when (pos) {
+                MENU_ORDER_LIST -> // some action
+                MENU_PROMO -> // some action
+                MENU_HELP -> // some action
+            }*/
+            menus.dismiss()
+        }
+
+        menus.setActionText(resources.getString(R.string.train_homepage_bottom_menu_action_text))
+
+        menus.show()
     }
 
     protected fun getHotelComponent(): HotelComponent {
@@ -52,4 +88,10 @@ abstract class HotelBaseActivity: BaseSimpleActivity() {
     }
 
     abstract fun shouldShowOptionMenu(): Boolean
+
+    companion object {
+        private val MENU_ORDER_LIST = 0
+        private val MENU_PROMO = 1
+        private val MENU_HELP = 2
+    }
 }
