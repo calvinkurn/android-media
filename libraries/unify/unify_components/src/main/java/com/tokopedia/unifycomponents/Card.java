64a27223d4bb03ea895086ff8790304f887ad654
Meyta <meytajennist@gmail.com>
2019-03-25 11:10:08 +0700

    Component Card

--- libraries/unify/unify_components/src/main/java/com/tokopedia/unifycomponents/Card.java
+++ libraries/unify/unify_components/src/main/java/com/tokopedia/unifycomponents/Card.java
@@ -1,7 +1,31 @@
 package com.tokopedia.unifycomponents;
 
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
 /**
  * Created by meta on 27/02/19.
  */
-public class Card {
+public class Card extends FrameLayout {
+
+    public Card(Context context) {
+        super(context);
+    }
+
+    public Card(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public Card(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        this.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.bg_card));
+    }
 }
