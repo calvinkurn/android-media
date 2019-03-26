9e4141b8acb6915f96ea38410973b19304e25977
hadi <hadi.saputro@tokopedia.com>
2019-03-26 15:56:38 +0700

    temp tradein

--- features/category/tradein/src/main/java/viewmodel/TradeInVMFactory.java
+++ features/category/tradein/src/main/java/viewmodel/TradeInVMFactory.java
@@ -26,41 +26,11 @@ public class TradeInVMFactory extends ViewModelProvider.NewInstanceFactory {
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         //noinspection TryWithIdenticalCatches
         if (TradeInTextViewModel.class.isAssignableFrom(modelClass)) {
-            try {
-                return modelClass.getConstructor(FragmentActivity.class).newInstance(activityWeakReference.get());
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InstantiationException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            }
+            return (T) new TradeInTextViewModel(activityWeakReference.get());
         } else if (TradeInHomeViewModel.class.isAssignableFrom(modelClass)) {
-            try {
-                return modelClass.getConstructor(FragmentActivity.class).newInstance(activityWeakReference.get());
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InstantiationException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            }
+            return (T) new TradeInHomeViewModel(activityWeakReference.get());
         } else if (FinalPriceViewModel.class.isAssignableFrom(modelClass)) {
-            try {
-                return modelClass.getConstructor(FragmentActivity.class).newInstance(activityWeakReference.get());
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InstantiationException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException("Cannot create an instance of " + modelClass, e);
-            }
+            return (T) new FinalPriceViewModel(activityWeakReference.get());
         }
         return super.create(modelClass);
     }
