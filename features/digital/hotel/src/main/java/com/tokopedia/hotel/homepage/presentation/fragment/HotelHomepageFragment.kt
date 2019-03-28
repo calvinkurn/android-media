d14ac5415c08ec6a764db745be87df03a2ab40ca
Muhammad Furqan <nanjihusaha@gmail.com>
2019-03-28 17:38:36 +0700

    fix error build

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/homepage/presentation/fragment/HotelHomepageFragment.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/homepage/presentation/fragment/HotelHomepageFragment.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.hotel.homepage.di.HotelHomepageComponent
+import com.tokopedia.hotel.R
 
 /**
  * @author by furqan on 28/03/19
  */
-class HotelHomepageFragment: BaseDaggerFragment {
+class HotelHomepageFragment: BaseDaggerFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.activity_hotel_homepage, container, false)
