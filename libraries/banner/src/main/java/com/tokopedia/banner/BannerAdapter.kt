3321122cfa359f3de7faf2bd64db39d2f3ffd5b2
okasurya <brilliantsurya@gmail.com>
2019-03-28 11:58:10 +0700

    revert banner

--- libraries/banner/src/main/java/com/tokopedia/banner/BannerAdapter.kt
+++ libraries/banner/src/main/java/com/tokopedia/banner/BannerAdapter.kt
@@ -20,28 +20,10 @@ class BannerAdapter(bannerImageUrls : List<String>,
         BannerPagerAdapter(bannerImageUrls, onPromoClickListener) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.banner_item_layout, parent, false)
-
-        val layoutParams = itemView.layoutParams
-
-        if (parent.width != 0 ) {
-            layoutParams.width = (parent.width * 0.9).toInt()
-        } else {
-            val mWinMgr = itemView.context
-                    .getSystemService(Context.WINDOW_SERVICE) as WindowManager
-            val display = mWinMgr.defaultDisplay
-            val size = Point()
-            display.getSize(size)
-            val width = size.x
-
-            layoutParams.width = (width * 0.9).toInt()
-        }
-
-        itemView.layoutParams = layoutParams
-        itemView.banner_card.visibility = View.VISIBLE
-
-        return BannerViewHolder(itemView)
+        return BannerViewHolder(
+                LayoutInflater.from(parent.context)
+                        .inflate(R.layout.banner_item_layout, parent, false)
+        )
     }
 }
 
