2c9b73334dacaa974dc24dd705511b0a96945a0d
abhishek-toko <48905017+abhishek-toko@users.noreply.github.com>
2019-04-03 10:55:58 +0530

    Update MarketPlaceDetailFragment.java

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/view/fragment/MarketPlaceDetailFragment.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/view/fragment/MarketPlaceDetailFragment.java
@@ -95,8 +95,8 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
     public static final int REQUEST_CANCEL_ORDER = 101;
     public static final int REJECT_BUYER_REQUEST = 102;
     public static final int CANCEL_BUYER_REQUEST = 103;
-    public static final int TEXT_SIZE_TWELVE = 12;
-    public static final int TEXT_SIZE_FOURTEEEN = 14;
+    public static final int TEXT_SIZE_MEDIUM = 12;
+    public static final int TEXT_SIZE_LARGE = 14;
     @Inject
     OrderListDetailPresenter presenter;
     private LinearLayout mainView;
@@ -289,7 +289,7 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
             doubleTextView.setBottomText(detail.value());
             doubleTextView.setBottomTextColor(getContext().getResources().getColor(R.color.black_seventy_percent_));
             doubleTextView.setBottomTextStyle("bold");
-            doubleTextView.setBottomTextSize(TEXT_SIZE_TWELVE);
+            doubleTextView.setBottomTextSize(TEXT_SIZE_MEDIUM);
         } else {
             doubleTextView.setTopText(detail.label());
             String text = detail.value() + NO_SANIN_NEXT_LINE;
@@ -356,7 +356,7 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
         doubleTextView.setTopTextColor(getContext().getResources().getColor(R.color.font_black_secondary_54));
         doubleTextView.setBottomText(pricing.value());
         doubleTextView.setBottomTextColor(getContext().getResources().getColor(R.color.black_70));
-        doubleTextView.setBottomTextSize(TEXT_SIZE_TWELVE);
+        doubleTextView.setBottomTextSize(TEXT_SIZE_MEDIUM);
         doubleTextView.setBottomGravity(Gravity.RIGHT);
         infoValue.addView(doubleTextView);
     }
@@ -367,7 +367,7 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
         doubleTextView.setTopText(payMethod.getLabel());
         doubleTextView.setTopTextColor(getContext().getResources().getColor(R.color.font_black_secondary_54));
         doubleTextView.setBottomText(payMethod.getValue());
-        doubleTextView.setBottomTextSize(TEXT_SIZE_TWELVE);
+        doubleTextView.setBottomTextSize(TEXT_SIZE_MEDIUM);
         doubleTextView.setBottomTextColor(getContext().getResources().getColor(R.color.black_70));
         doubleTextView.setBottomGravity(Gravity.RIGHT);
         paymentMethod.addView(doubleTextView);
@@ -687,11 +687,11 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
         DoubleTextView doubleTextView = new DoubleTextView(getActivity(), LinearLayout.HORIZONTAL);
         doubleTextView.setTopText(paymentData.label());
         doubleTextView.setTopTextColor(getContext().getResources().getColor(R.color.black));
-        doubleTextView.setTopTextSize(TEXT_SIZE_FOURTEEEN);
+        doubleTextView.setTopTextSize(TEXT_SIZE_LARGE);
         doubleTextView.setBottomText(paymentData.value());
         if (!paymentData.textColor().equals(""))
             doubleTextView.setBottomTextColor(Color.parseColor(paymentData.textColor()));
-        doubleTextView.setBottomTextSize(TEXT_SIZE_FOURTEEEN);
+        doubleTextView.setBottomTextSize(TEXT_SIZE_LARGE);
         doubleTextView.setBottomTextColor(getContext().getResources().getColor(R.color.orange));
         doubleTextView.setBottomGravity(Gravity.RIGHT);
         totalPrice.addView(doubleTextView);
