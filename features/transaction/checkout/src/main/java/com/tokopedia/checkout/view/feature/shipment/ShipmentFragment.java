7f09088956fb8a5684fee9b53dd2c4523e073a20
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-04-05 19:44:08 +0700

    Prevent hitting first step when logistic promo

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentFragment.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentFragment.java
@@ -1807,7 +1807,7 @@ public class ShipmentFragment extends BaseCheckoutFragment implements ShipmentCo
     @Override
     public void onLogisticPromoChosen(List<ShippingCourierViewModel> shippingCourierViewModels, CourierItemData courierData, RecipientAddressModel recipientAddressModel, int cartPosition, int selectedServiceId, String selectedServiceName, boolean flagNeedToSetPinpoint) {
         onShippingDurationChoosen(shippingCourierViewModels, courierData, recipientAddressModel, cartPosition, selectedServiceId, selectedServiceName, flagNeedToSetPinpoint, false, true);
-        shipmentPresenter.processCheckPromoStackingLogisticPromo(cartPosition);
+        if (!flagNeedToSetPinpoint) shipmentPresenter.processCheckPromoStackingLogisticPromo(cartPosition);
     }
 
     @Override
