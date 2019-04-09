1a4a8e8d718a0c2d21e789abb3e59860d7b99244
jessicasean <jessica.sean@tokopedia.com>
2019-04-09 17:11:44 +0700

    change close button

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/destination/view/activity/HotelDestinationActivity.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/destination/view/activity/HotelDestinationActivity.kt
@@ -56,7 +56,7 @@ class HotelDestinationActivity: HotelBaseActivity(), HasComponent<HotelDestinati
     }
 
     fun initEditText() {
-        //set image close button
+        search_input_view.closeImageButton.setImageDrawable(resources.getDrawable(R.drawable.ic_system_action_close_grayscale_16))
         search_input_view.setListener(this)
         search_input_view.setResetListener(this)
     }
