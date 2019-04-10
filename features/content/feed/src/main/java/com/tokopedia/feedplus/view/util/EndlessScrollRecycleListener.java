8a4ce8a6335892508f8e64cc5c211329f276cefa
yfsx <yoas.fs@gmail.com>
2019-04-10 15:13:53 +0700

    do log

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/util/EndlessScrollRecycleListener.java
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/util/EndlessScrollRecycleListener.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 
 /**
  * Created by milhamj on 18/01/18.
@@ -55,25 +56,45 @@ public abstract class EndlessScrollRecycleListener extends RecyclerView.OnScroll
             lastVisibleItemPosition
                     = linearLayoutManager.findLastVisibleItemPosition();
         }
-
+        Log.d("Endless Scroll Listener","start scroll");
+        Log.d("Endless Scroll Listener","start func 1");
+        Log.d("init","totalItemCount :" + totalItemCount );
+        Log.d("init","previousTotalItemCount :" + previousTotalItemCount );
+        Log.d("init","loading :" + loading );
         if (totalItemCount < previousTotalItemCount) {
+            Log.d("Endless Scroll Listener","process func 1 - init");
             this.currentPage = this.startingPageIndex;
             this.previousTotalItemCount = totalItemCount;
             if (totalItemCount == 0) {
                 this.loading = true;
             }
         }
-
+        Log.d("func 1","totalItemCount :" + totalItemCount );
+        Log.d("func 1","previousTotalItemCount :" + previousTotalItemCount );
+        Log.d("func 1","loading :" + loading );
+        Log.d("Endless Scroll Listener","end func 1");
+        Log.d("Endless Scroll Listener","start func 2");
         if (loading && (totalItemCount > previousTotalItemCount)) {
+            Log.d("Endless Scroll Listener","process func 2 - reset to loadmore");
             loading = false;
             previousTotalItemCount = totalItemCount;
         }
-
+        Log.d("func 2","totalItemCount :" + totalItemCount );
+        Log.d("func 2","previousTotalItemCount :" + previousTotalItemCount );
+        Log.d("func 2","loading :" + loading );
+        Log.d("Endless Scroll Listener","end func 2");
+        Log.d("Endless Scroll Listener","start func 3");
         if (!loading && (lastVisibleItemPosition + visibleThreshold) > totalItemCount) {
+            Log.d("Endless Scroll Listener","process func 3 - do loadmore");
             currentPage++;
             onLoadMore(currentPage, totalItemCount);
             loading = true;
         }
+        Log.d("func 3","totalItemCount :" + totalItemCount );
+        Log.d("func 3","previousTotalItemCount :" + previousTotalItemCount );
+        Log.d("func 3","loading :" + loading );
+        Log.d("Endless Scroll Listener","end func 3");
+        Log.d("Endless Scroll Listener","end scroll");
 
         onScroll(lastVisibleItemPosition);
     }
