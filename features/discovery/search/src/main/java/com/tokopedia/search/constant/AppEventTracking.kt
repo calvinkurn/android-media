b66a1707498ae346d3103c92e432c44f4e54ff3e
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-12 07:44:34 +0700

    UNFINISHED COMMIT: Move Search Activity to new module

---
+++ features/discovery/search/src/main/java/com/tokopedia/search/constant/AppEventTracking.kt
@@ -0,0 +1,9 @@
+@file:JvmName("AppEventTracking")
+package com.tokopedia.search.constant
+
+class AppEventTracking {
+
+    companion object Event {
+        const val IMAGE_SEARCH_CLICK = "IMAGE_SEARCH_CLICK"
+    }
+}
---
+++ features/discovery/search/src/main/java/com/tokopedia/search/constant/SearchConstant.kt
@@ -0,0 +1,4 @@
+@file:JvmName("SearchConstant")
+package com.tokopedia.search.constant
+
+const val FROM_APP_SHORTCUTS = "FROM_APP_SHORTCUTS"
