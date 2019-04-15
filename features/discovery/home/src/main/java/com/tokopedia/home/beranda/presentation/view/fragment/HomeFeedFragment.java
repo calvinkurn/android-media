1dfd622e250f7ed0042bad3fb4ba51417c640253
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-04-15 13:12:35 +0700

    add check null for tracking queue

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFeedFragment.java
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFeedFragment.java
@@ -121,7 +121,7 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
     }
 
     private void hitHomeFeedImpressionTracker(HomeFeedViewModel homeFeedViewModel) {
-        if (userSession.isLoggedIn()){
+        if (userSession.isLoggedIn()) {
             HomePageTracking.eventImpressionOnProductRecommendationForLoggedInUser(
                     homeTrackingQueue,
                     homeFeedViewModel,
@@ -193,7 +193,7 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
     @Override
     public void onItemClicked(HomeFeedViewModel homeFeedViewModel) {
         if (userSession.isLoggedIn()) {
-            if(!homeFeedViewModel.isTopAds()){
+            if (!homeFeedViewModel.isTopAds()) {
                 HomePageTracking.eventClickOnHomeProductFeedForLoggedInUser(
                         getActivity(),
                         homeFeedViewModel,
@@ -201,7 +201,7 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
                 );
             }
         } else {
-            if(!homeFeedViewModel.isTopAds()){
+            if (!homeFeedViewModel.isTopAds()) {
                 HomePageTracking.eventClickOnHomeProductFeedForNonLoginUser(
                         getActivity(),
                         homeFeedViewModel,
@@ -209,7 +209,7 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
                 );
             }
         }
-        if(homeFeedViewModel.isTopAds()) {
+        if (homeFeedViewModel.isTopAds()) {
             new ImpresionTask().execute(homeFeedViewModel.getClickUrl());
             Product p = new Product();
             p.setId(homeFeedViewModel.getProductId());
@@ -230,9 +230,9 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
         getActivity().startActivity(getProductIntent(productId));
     }
 
-    private Intent getProductIntent(String productId){
+    private Intent getProductIntent(String productId) {
         if (getContext() != null) {
-            return RouteManager.getIntent(getContext(),ApplinkConstInternalMarketplace.PRODUCT_DETAIL, productId);
+            return RouteManager.getIntent(getContext(), ApplinkConstInternalMarketplace.PRODUCT_DETAIL, productId);
         } else {
             return null;
         }
@@ -286,7 +286,7 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
 
     @Override
     public void onProductImpression(HomeFeedViewModel model, int position) {
-        if(model.isTopAds()) {
+        if (model.isTopAds()) {
             Product p = new Product();
             p.setId(model.getProductId());
             p.setName(model.getProductName());
@@ -305,7 +305,9 @@ public class HomeFeedFragment extends BaseListFragment<HomeFeedViewModel, HomeFe
     public void onPause() {
         TopAdsGtmTracker.getInstance().eventRecomendationProductView(homeTrackingQueue,
                 tabName.toLowerCase(), userSession.isLoggedIn());
-        homeTrackingQueue.sendAll();
+        if (homeTrackingQueue != null) {
+            homeTrackingQueue.sendAll();
+        }
         super.onPause();
     }
 }
