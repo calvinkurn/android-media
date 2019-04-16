8b38cf633cf46b020ab75a39987a37fa5c984090
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-16 11:02:17 +0700

    Revert changes on SearchModule, UserSession already provided

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/di/module/SearchModule.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/di/module/SearchModule.java
@@ -2,7 +2,6 @@ package com.tokopedia.discovery.newdiscovery.di.module;
 
 import android.content.Context;
 
-import com.tokopedia.abstraction.AbstractionRouter;
 import com.tokopedia.core.base.di.qualifier.ApplicationContext;
 import com.tokopedia.discovery.imagesearch.di.module.ImageSearchModule;
 import com.tokopedia.discovery.imagesearch.domain.usecase.GetImageSearchUseCase;
@@ -16,7 +15,6 @@ import com.tokopedia.discovery.newdiscovery.search.fragment.product.ProductListP
 import com.tokopedia.discovery.newdiscovery.search.fragment.product.ProductListPresenterImpl;
 import com.tokopedia.discovery.newdiscovery.search.fragment.shop.ShopListPresenter;
 import com.tokopedia.discovery.newdiscovery.search.fragment.shop.ShopListPresenterImpl;
-import com.tokopedia.user.session.UserSession;
 import com.tokopedia.user.session.UserSessionInterface;
 
 import dagger.Module;
@@ -68,11 +66,5 @@ public class SearchModule {
     SearchTracking provideSearchTracking(@ApplicationContext Context context,
                                          UserSessionInterface userSessionInterface) {
         return new SearchTracking(context, userSessionInterface);
-    }
-
-    @SearchScope
-    @Provides
-    UserSessionInterface provideUserSession(@ApplicationContext Context context) {
-        return new UserSession(context);
     }
 }
