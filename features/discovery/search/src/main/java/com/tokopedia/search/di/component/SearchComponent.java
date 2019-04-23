21a4a2698e4e47faebf256e4ec92259e6b76e185
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-23 21:10:06 +0700

    Extend search component

--- features/discovery/search/src/main/java/com/tokopedia/search/di/component/SearchComponent.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/di/component/SearchComponent.java
@@ -1,13 +1,15 @@
 package com.tokopedia.search.di.component;
 
+import com.tokopedia.core.base.di.component.AppComponent;
+import com.tokopedia.discovery.newdiscovery.di.module.SearchModule;
 import com.tokopedia.discovery.newdiscovery.di.scope.SearchScope;
 import com.tokopedia.search.presentation.view.activity.SearchActivity;
 
 import dagger.Component;
 
 @SearchScope
-@Component
-public interface SearchComponent extends com.tokopedia.discovery.newdiscovery.di.component.SearchComponent {
+@Component(modules = SearchModule.class, dependencies = AppComponent.class)
+public interface SearchComponent {
     void inject(SearchActivity searchActivity);
 
 }
