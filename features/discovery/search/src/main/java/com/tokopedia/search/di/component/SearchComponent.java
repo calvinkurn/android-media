83fc040d9ec71347608c7563f66c8d01177036ee
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-23 14:28:28 +0700

    Add SearchScope

--- features/discovery/search/src/main/java/com/tokopedia/search/di/component/SearchComponent.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/di/component/SearchComponent.java
@@ -1,11 +1,13 @@
 package com.tokopedia.search.di.component;
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
+import com.tokopedia.discovery.newdiscovery.di.scope.SearchScope;
 import com.tokopedia.search.di.module.SearchModule;
 import com.tokopedia.search.presentation.view.activity.SearchActivity;
 
 import dagger.Component;
 
+@SearchScope
 @Component(modules = SearchModule.class, dependencies = BaseAppComponent.class)
 public interface SearchComponent {
     void inject(SearchActivity searchActivity);
