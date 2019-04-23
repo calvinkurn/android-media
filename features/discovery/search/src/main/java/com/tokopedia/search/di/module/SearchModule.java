d024e50257bac7656a149f602b7b3e6f24ce181f
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-23 21:29:50 +0700

    Fix compile error

--- features/discovery/search/src/main/java/com/tokopedia/search/di/module/SearchModule.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/di/module/SearchModule.java
@@ -3,21 +3,71 @@ package com.tokopedia.search.di.module;
 import android.content.Context;
 
 import com.tokopedia.core.base.di.qualifier.ApplicationContext;
+import com.tokopedia.discovery.imagesearch.di.module.ImageSearchModule;
 import com.tokopedia.discovery.imagesearch.domain.usecase.GetImageSearchUseCase;
+import com.tokopedia.discovery.imagesearch.search.fragment.product.ImageProductListPresenter;
+import com.tokopedia.discovery.imagesearch.search.fragment.product.ImageProductListPresenterImpl;
+import com.tokopedia.discovery.newdiscovery.analytics.SearchTracking;
+import com.tokopedia.discovery.newdiscovery.di.module.ApiModule;
+import com.tokopedia.discovery.newdiscovery.di.module.AttributeModule;
+import com.tokopedia.discovery.newdiscovery.di.module.BannerModule;
+import com.tokopedia.discovery.newdiscovery.di.module.CatalogModule;
+import com.tokopedia.discovery.newdiscovery.di.module.GuidedSearchModule;
+import com.tokopedia.discovery.newdiscovery.di.module.ProductModule;
+import com.tokopedia.discovery.newdiscovery.di.module.ShopModule;
 import com.tokopedia.discovery.newdiscovery.di.scope.SearchScope;
 import com.tokopedia.discovery.newdiscovery.domain.usecase.GetProductUseCase;
-import com.tokopedia.search.presentation.presenter.SearchPresenter;
+import com.tokopedia.discovery.newdiscovery.search.SearchPresenter;
+import com.tokopedia.discovery.newdiscovery.search.fragment.product.ProductListPresenter;
+import com.tokopedia.discovery.newdiscovery.search.fragment.product.ProductListPresenterImpl;
+import com.tokopedia.discovery.newdiscovery.search.fragment.shop.ShopListPresenter;
+import com.tokopedia.discovery.newdiscovery.search.fragment.shop.ShopListPresenterImpl;
+import com.tokopedia.user.session.UserSessionInterface;
 
 import dagger.Module;
 import dagger.Provides;
 
 @SearchScope
-@Module
-public class SearchModule extends com.tokopedia.discovery.newdiscovery.di.module.SearchModule {
+@Module(includes = {
+        GuidedSearchModule.class,
+        ProductModule.class,
+        ImageSearchModule.class,
+        BannerModule.class,
+        ApiModule.class,
+        CatalogModule.class,
+        ShopModule.class,
+        AttributeModule.class
+})
+public class SearchModule {
 
     @SearchScope
     @Provides
-    SearchPresenter provideSearchPresenter(@ApplicationContext Context context, GetProductUseCase getProductUseCase, GetImageSearchUseCase getImageSearchUseCase) {
+    ProductListPresenter provideProductListPresenter(@ApplicationContext Context context) {
+        return new ProductListPresenterImpl(context);
+    }
+
+    @SearchScope
+    @Provides
+    ImageProductListPresenter provideImageProductListPresenter(@ApplicationContext Context context) {
+        return new ImageProductListPresenterImpl(context);
+    }
+
+    @SearchScope
+    @Provides
+    ShopListPresenter provideShopListPresenter(@ApplicationContext Context context) {
+        return new ShopListPresenterImpl(context);
+    }
+
+    @SearchScope
+    @Provides
+    com.tokopedia.discovery.newdiscovery.search.SearchPresenter provideSearchPresenter(@ApplicationContext Context context, GetProductUseCase getProductUseCase, GetImageSearchUseCase getImageSearchUseCase) {
         return new SearchPresenter(context, getProductUseCase, getImageSearchUseCase);
+    }
+
+    @SearchScope
+    @Provides
+    SearchTracking provideSearchTracking(@ApplicationContext Context context,
+                                         UserSessionInterface userSessionInterface) {
+        return new SearchTracking(context, userSessionInterface);
     }
 }
