e037a25aeee2189b75c03bb13ee0202d1baa3e5f
fwidjaja <fwidjaja@live.com>
2019-04-24 17:34:10 +0700

    fix flow if clash on promocheckout

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListMarketplaceFragment.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListMarketplaceFragment.kt
@@ -144,6 +144,10 @@ class PromoCheckoutListMarketplaceFragment : BasePromoCheckoutListFragment(), Pr
                 val clashingInfoDetailUiModel: ClashingInfoDetailUiModel? = bundle?.getParcelable(EXTRA_CLASHING_DATA);
                 intent.putExtra(EXTRA_CLASHING_DATA, clashingInfoDetailUiModel)
                 activity?.setResult(RESULT_CLASHING, intent)
+
+                if (clashingInfoDetailUiModel != null) {
+                    activity?.finish()
+                }
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
