f0f66970e7fc6f6afe05a67cf0d20b26595ede14
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-24 20:27:49 +0700

    Remove search scope from module

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/SearchPresenter.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/SearchPresenter.java
@@ -3,6 +3,6 @@ package com.tokopedia.search.result.presentation.presenter;
 import com.tokopedia.abstraction.base.view.presenter.BaseDaggerPresenter;
 import com.tokopedia.search.result.presentation.SearchContract;
 
-public final class SearchPresenter extends BaseDaggerPresenter<SearchContract.View> implements SearchContract.Presenter {
+final class SearchPresenter extends BaseDaggerPresenter<SearchContract.View> implements SearchContract.Presenter {
 
 }
