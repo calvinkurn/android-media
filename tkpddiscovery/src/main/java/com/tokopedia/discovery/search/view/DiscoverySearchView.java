ca19220102f7bf0baa6722931a69efd5bf9de562
darianthedytoped <darian.thedy@tokopedia.com>
2019-04-25 15:59:17 +0700

    Test using transparent activity

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/search/view/DiscoverySearchView.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/search/view/DiscoverySearchView.java
@@ -780,6 +780,18 @@ public class DiscoverySearchView extends FrameLayout implements Filter.FilterLis
     }
 
     private void setVisibleWithAnimation() {
+        ViewTreeObserver viewTreeObserver = mSearchContainer.getViewTreeObserver();
+        if (viewTreeObserver.isAlive()) {
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    animateOnGlobalLayout(this);
+                }
+            });
+        }
+    }
+
+    private void animateOnGlobalLayout(ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener) {
         AnimationUtil.AnimationListener animationListener = new AnimationUtil.AnimationListener() {
             @Override
             public boolean onAnimationStart(View view) {
@@ -807,6 +819,8 @@ public class DiscoverySearchView extends FrameLayout implements Filter.FilterLis
         } else {
             AnimationUtil.fadeInView(mSearchLayout, mAnimationDuration, animationListener);
         }
+
+        mSearchContainer.getViewTreeObserver().removeOnGlobalLayoutListener(onGlobalLayoutListener);
     }
 
     private void setVisibleWithoutAnimation() {
