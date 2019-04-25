4631c3f8bef9bc797c3bd273f7f331b5bfcbc2d3
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2019-04-25 14:40:22 +0700

    adding custom attribute type for aspect ratio or orientation type

--- libraries/videoplayer/src/main/java/com/tokopedia/videoplayer/view/widget/TkpdVideoView.kt
+++ libraries/videoplayer/src/main/java/com/tokopedia/videoplayer/view/widget/TkpdVideoView.kt
@@ -2,7 +2,9 @@ package com.tokopedia.videoplayer.view.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import com.google.android.exoplayer2.ui.PlayerView
+import com.tokopedia.videoplayer.R
 
 /**
  * Created by isfaaghyth on 25/04/19.
@@ -12,8 +14,45 @@ class TkpdVideoView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null,
         defStyleAttr: Int = 0): PlayerView(context, attrs, defStyleAttr) {
 
-    init {
+    companion object {
+        private const val LANDSCAPE = "landscape"
+        private const val PORTRAIT = "portrait"
+        private const val SQUARE = "square"
 
+        private const val aspectRatio = .5625f
+    }
+
+    //attributes options
+    var attrRatioType: String? = "portrait"
+
+    init {
+        if (attrs != null) {
+            val attributeArray = context.obtainStyledAttributes(attrs, R.styleable.TkpdVideoView)
+            attrRatioType = attributeArray.getString(R.styleable.TkpdVideoView_type)
+            attributeArray.recycle()
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        when (attrRatioType) {
+            PORTRAIT -> {
+                val width = measuredWidth
+                val height = Math.round(width * aspectRatio)
+                setMeasuredDimension(width, height)
+            }
+            LANDSCAPE -> {
+                val height = measuredHeight
+                val width = Math.round(height * aspectRatio)
+                setMeasuredDimension(width, height)
+            }
+            SQUARE -> {
+                val size = View.MeasureSpec.getSize(widthMeasureSpec)
+                setMeasuredDimension(size, size)
+            }
+            else -> {
+                super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            }
+        }
     }
 
 }
