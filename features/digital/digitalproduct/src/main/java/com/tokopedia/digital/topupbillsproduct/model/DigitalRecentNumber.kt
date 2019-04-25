117497d22bd72c7e608644efda1dfd84854f0d10
Nabilla Sabbaha <nabilla.sabbaha@tokopedia.com>
2019-04-25 15:14:56 +0700

    recent numbers view on product digital

---
+++ features/digital/digitalproduct/src/main/java/com/tokopedia/digital/topupbillsproduct/model/DigitalRecentNumber.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.digital.topupbillsproduct.model
+
+/**
+ * Created by Rizky on 13/11/18.
+ */
+class DigitalRecentNumber(val iconUrl: String,
+                          val title: String,
+                          val clientNumber: String,
+                          val applink: String,
+                          val webLink: String,
+                          val productId: Int,
+                          val productName: String)
