816c327c0df6b5f90d6825bd903784166f515337
nisie <nathania.sutedja@tokopedia.com>
2019-04-29 13:32:27 +0700

    Remove double header

--- features/user/otp/src/main/java/com/tokopedia/otp/common/network/AuthorizationBearerInterceptor.java
+++ features/user/otp/src/main/java/com/tokopedia/otp/common/network/AuthorizationBearerInterceptor.java
@@ -26,8 +26,8 @@ public class AuthorizationBearerInterceptor implements Interceptor {
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request.Builder newRequest = chain.request().newBuilder();
+        newRequest.removeHeader(KEY_AUTHORIZATION);
         newRequest = addAccountsAuthorizationHeader(newRequest);
-
         return chain.proceed(newRequest.build());
     }
 
