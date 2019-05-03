d8c8af6f0cdf9dcbe6bfc54a2cb4f677d851d433
hadi <hadi.saputro@tokopedia.com>
2019-05-03 22:05:19 +0700

    remove old pdp

---
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/hotlist/view/listener/AppBarStateChangeListener.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.product.detail.view.util
+
+import android.support.design.widget.AppBarLayout
+import com.tokopedia.discovery.newdiscovery.hotlist.view.listener.AppBarState
+
+abstract class AppBarStateChangeListener: AppBarLayout.OnOffsetChangedListener {
+
+    @AppBarState
+    private var state = AppBarState.IDLE
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        if (appBarLayout == null)
+            return
+        state = when {
+            verticalOffset == 0 -> {
+                if (state != AppBarState.EXPANDED) onStateChanged(appBarLayout, AppBarState.EXPANDED)
+                AppBarState.EXPANDED
+            }
+            Math.abs(verticalOffset) >= appBarLayout.totalScrollRange -> {
+                if (state != AppBarState.COLLAPSED) onStateChanged(appBarLayout, AppBarState.COLLAPSED)
+                AppBarState.COLLAPSED
+            }
+            else -> {
+                if (state != AppBarState.IDLE) onStateChanged(appBarLayout, AppBarState.IDLE)
+                AppBarState.IDLE
+            }
+        }
+    }
+
+    abstract fun onStateChanged(appBarLayout: AppBarLayout?, @AppBarState state: Int)
+}
