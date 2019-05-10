1f64bfe920f12685f69ca7e3fe39ee6df45f7ab8
irfan khoirul <irfankhoirul@gmail.com>
2019-05-10 13:40:33 +0700

    Onboarding multiple address

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/mapper/CartMapper.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/mapper/CartMapper.java
@@ -547,6 +547,8 @@ public class CartMapper implements ICartMapper {
             cartListData.setAutoApplyStackData(autoApplyStackData);
         }
 
+        cartListData.setShowOnboarding(cartDataListResponse.isShowOnboarding());
+
         return cartListData;
 
     }
