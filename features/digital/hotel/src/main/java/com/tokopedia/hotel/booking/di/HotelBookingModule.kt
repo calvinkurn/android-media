c88adedc4b267083f61ac061d72bc91194a952c9
resakemal <resakemal@gmail.com>
2019-05-13 10:59:56 +0700

    Add hotel booking dependency injection

---
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/booking/di/HotelBookingModule.kt
@@ -0,0 +1,19 @@
+package com.tokopedia.hotel.booking.di
+
+import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import dagger.Module
+import dagger.Provides
+
+/**
+ * @author by resakemal on 13/05/19
+ */
+@Module
+class HotelBookingModule {
+
+    @HotelBookingScope
+    @Provides
+    fun provideMultiRequestGraphqlUseCase(graphqlRepository: GraphqlRepository): MultiRequestGraphqlUseCase =
+            MultiRequestGraphqlUseCase(graphqlRepository)
+
+}
