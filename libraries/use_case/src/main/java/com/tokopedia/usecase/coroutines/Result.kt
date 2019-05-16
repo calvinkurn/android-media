6acbea6ea27ad2b8ed17ea6df548b55aebc0452a
M normansyah <noiz354@gmail.com>
2019-05-16 09:21:37 +0700

    - move cache_manager,use_case to composite build - remove unnecessary link to kotlin_extensions for use_case - switch reference from direct to libraries to composite build.

--- libraries/use_case/src/main/java/com/tokopedia/usecase/coroutines/Result.kt
+++
@@ -1,5 +0,0 @@
-package com.tokopedia.usecase.coroutines
-
-sealed class Result<out T: Any>
-data class Success<out T: Any>(val data: T): Result<T>()
-data class Fail(val throwable: Throwable): Result<Nothing>()
