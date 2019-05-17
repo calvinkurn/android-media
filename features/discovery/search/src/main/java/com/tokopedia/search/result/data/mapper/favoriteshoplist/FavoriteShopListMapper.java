22c7a655a44bb4bb77d15919942c9e509399e1a5
darianthedytoped <darian.thedy@tokopedia.com>
2019-05-17 09:45:31 +0700

    Favorite shop list mapper always return new object

--- features/discovery/search/src/main/java/com/tokopedia/search/result/data/mapper/favoriteshoplist/FavoriteShopListMapper.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/data/mapper/favoriteshoplist/FavoriteShopListMapper.java
@@ -1,7 +1,5 @@
 package com.tokopedia.search.result.data.mapper.favoriteshoplist;
 
-import com.tokopedia.network.constant.ErrorNetMessage;
-import com.tokopedia.network.constant.ResponseStatus;
 import com.tokopedia.search.result.domain.model.FavoriteShopListModel;
 
 import retrofit2.Response;
@@ -18,33 +16,11 @@ final class FavoriteShopListMapper implements Func1<Response<FavoriteShopListMod
                 return favoriteShopListModel;
             }
             else {
-                throw new RuntimeException(favoriteShopListModelResponse.errorBody().toString());
+                return new FavoriteShopListModel();
             }
         }
         else {
-            throw new RuntimeException(getRuntimeErrorExceptionMessage(favoriteShopListModelResponse.code()));
+            return new FavoriteShopListModel();
         }
-    }
-
-    private String getRuntimeErrorExceptionMessage(int errorCode) {
-        String messageError;
-
-        switch (errorCode) {
-            case ResponseStatus.SC_INTERNAL_SERVER_ERROR:
-                messageError = ErrorNetMessage.MESSAGE_ERROR_SERVER;
-                break;
-            case ResponseStatus.SC_FORBIDDEN:
-                messageError = ErrorNetMessage.MESSAGE_ERROR_FORBIDDEN;
-                break;
-            case ResponseStatus.SC_REQUEST_TIMEOUT:
-            case ResponseStatus.SC_GATEWAY_TIMEOUT:
-                messageError = ErrorNetMessage.MESSAGE_ERROR_TIMEOUT;
-                break;
-            default:
-                messageError = ErrorNetMessage.MESSAGE_ERROR_DEFAULT;
-                break;
-        }
-
-        return messageError;
     }
 }
