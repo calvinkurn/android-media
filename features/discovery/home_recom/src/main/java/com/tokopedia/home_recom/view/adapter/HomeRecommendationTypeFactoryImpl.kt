1ea95b382b0ecd5aba8cf0f0543c68cfc4ede16b
lukaskris12 <lukaskris12@gmail.com>
2019-05-17 09:57:36 +0700

    renew applink

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/adapter/HomeRecommendationTypeFactoryImpl.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/adapter/HomeRecommendationTypeFactoryImpl.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.home_recom.view.adapter
 
 import android.view.View
-import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.home_recom.model.dataModel.ProductInfoDataModel
@@ -11,15 +10,15 @@ import com.tokopedia.home_recom.view.viewHolder.RecommendationScrollViewHolder
 
 class HomeRecommendationTypeFactoryImpl : BaseAdapterTypeFactory(), HomeRecommendationTypeFactory{
     override fun type(dataModel: ProductInfoDataModel): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ProductInfoDataModel.LAYOUT
     }
 
     override fun type(dataModel: RecommendationScrollDataModel): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return RecommendationScrollDataModel.LAYOUT
     }
 
     override fun type(dataModel: RecommendationCarouselDataModel): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return RecommendationCarouselDataModel.LAYOUT
     }
 
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<*> {
