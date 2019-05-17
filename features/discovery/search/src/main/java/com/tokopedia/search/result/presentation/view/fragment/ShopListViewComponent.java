ec680bd58cf3ad7b0b6e6f42f1eea84506b8d73e
darianthedytoped <darian.thedy@tokopedia.com>
2019-05-17 08:30:36 +0700

    Add SearchTrackingModule

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/fragment/ShopListViewComponent.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/fragment/ShopListViewComponent.java
@@ -1,8 +1,8 @@
 package com.tokopedia.search.result.presentation.view.fragment;
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
-import com.tokopedia.discovery.newdiscovery.analytics.SearchTracking;
 import com.tokopedia.discovery.newdiscovery.di.scope.SearchScope;
+import com.tokopedia.search.di.module.SearchTrackingModule;
 import com.tokopedia.search.di.module.UserSessionModule;
 import com.tokopedia.search.result.presentation.presenter.shop.ShopListPresenterModule;
 
@@ -12,7 +12,7 @@ import dagger.Component;
 @Component(modules = {
         ShopListPresenterModule.class,
         UserSessionModule.class,
-        SearchTracking.class
+        SearchTrackingModule.class
 }, dependencies = BaseAppComponent.class)
 public interface ShopListViewComponent {
 
