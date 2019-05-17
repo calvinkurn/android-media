db3e1ef34b9fe63d8e21c69f147863c7b1e89810
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2019-05-17 13:07:53 +0700

    add recommendation viewmodel

---
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/presentation/model/RecommendationList.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.recommendation_widget_common.presentation.model
+
+import com.tokopedia.kotlin.model.ImpressHolder
+
+class RecommendationList(val recommendationList: List<RecommendationModelDummy>) : ImpressHolder() {
+
+}
