6e1a46153a234c5dea6f7f604db35592e5674628
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-05-22 14:51:20 +0700

    Remove unnecessary import

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/customview/TopChatViewStateImpl.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/customview/TopChatViewStateImpl.kt
@@ -9,8 +9,6 @@ import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
-import android.util.ArrayMap
-import android.util.Log
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageButton
