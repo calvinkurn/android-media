aeab33b861dc7042ec30cb2e418f7bfe44a1f28f
lukaskris12 <lukaskris12@gmail.com>
2019-05-24 10:08:19 +0700

    add tracking

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/analytics/RecommendationPageTracking.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/analytics/RecommendationPageTracking.kt
@@ -15,6 +15,9 @@ class RecommendationPageTracking {
     val ECOMMERCE = "ecommerce"
     val ECOMMERCE_IMPRESSIONS = "impressions"
     val ECOMMERCE_CURRENCY_CODE = "currencyCode"
+    val ECOMMERCE_ADD = "add"
+
+    val ACTION_FIELD = "actionField"
 
     val FIELD_PRODUCT_NAME = "name"
     val FIELD_PRODUCT_ID = "id"
@@ -30,10 +33,31 @@ class RecommendationPageTracking {
     val VALUE_EMPTY = ""
     val VALUE_LIST_ORGANIC_PRODUCT_RECOMMENDATION = "/product - rekomendasi untuk anda - %s"
     val VALUE_LIST_TOPADS_PRODUCT_RECOMMENDATION = "/product - rekomendasi untuk anda - %s - product topads"
+    val VALUE_LIST_CLICK_ADD_TO_CART = "/recommendation - primary product"
 
     val EVENT_PRODUCT_VIEW = "productView"
+    val EVENT_CLICK_RECOMMENDATION = "clickRecommendation"
+    val EVENT_ADD_TO_CART = "addToCart"
+    val EVENT_PRODUCT_CLICK = "productClick"
     val EVENT_CATEGORY_PRODUCT_DETAIL_PAGE = "product detail page"
+    val EVENT_RECOMMENDATION_PAGE = "recommendation page"
+    val EVENT_ACTION_ADD_TO_CART = "click add to cart"
+    val EVENT_ACTION_CLICK_BACK = "click back"
+    val EVENT_ACTION_CLICK_SEE_CART = "click see cart"
+    val EVENT_CLICK_PRODUCT_ON_HEADER = "click product recommendation on %s - non login"
+    val EVENT_ACTION_IMPRESSION_PRODUCT_ON_HEADER = "impression product recommendation on %s - non login"
+    val EVENT_ACTION_IMPRESSION_PRODUCT_ON_HEADER_LOGIN = "impression product recommendation on %s"
     val EVENT_ACTION_IMPRESSION_PRODUCT_RECOMMENDATION_LOGIN = "impression - product recommendation"
+    val EVENT_ACTION_IMPRESSION_PRODUCT_WISHLIST = "click %s wishlist on primary product"
+    val EVENT_ACTION_IMPRESSION_PRODUCT_WISHLIST_NON_LOGIN = "click add wishlist on primary product - non login"
+
+    val ACTION_FIELD_LIST = "list"
+    val ACTION_FIELD_PRODUCTS = "products"
+    val ACTION_FIELD_LIST_CLICK_HEADER = "/recommendation - non login - %s - rekomendasi untuk anda - %s"
+    val ACTION_FIELD_LIST_CLICK_HEADER_TOP_ADS = "/recommendation - non login - %s - rekomendasi untuk anda - %s - product topads"
+
+    val CLICK = "click"
+
 
     fun getTracker(): ContextAnalytics {
         return TrackApp.getInstance().gtm
@@ -51,6 +75,30 @@ class RecommendationPageTracking {
                 FIELD_PRODUCT_CATEGORY, item.categoryBreadcrumbs,
                 FIELD_PRODUCT_LIST, list,
                 FIELD_PRODUCT_POSITION, position
+        )
+    }
+
+    fun convertRecommendationItemToDataObject(item: RecommendationItem,
+                                              list: String) {
+        DataLayer.mapOf(
+                FIELD_PRODUCT_NAME, item.name,
+                FIELD_PRODUCT_ID, item.productId,
+                FIELD_PRODUCT_PRICE, item.priceInt,
+                FIELD_PRODUCT_BRAND, VALUE_NONE_OTHER,
+                FIELD_PRODUCT_VARIANT, VALUE_NONE_OTHER,
+                FIELD_PRODUCT_CATEGORY, item.categoryBreadcrumbs,
+                FIELD_PRODUCT_LIST, list
+        )
+    }
+
+    fun convertRecommendationItemToDataObject(item: RecommendationItem) {
+        DataLayer.mapOf(
+                FIELD_PRODUCT_NAME, item.name,
+                FIELD_PRODUCT_ID, item.productId,
+                FIELD_PRODUCT_PRICE, item.priceInt,
+                FIELD_PRODUCT_BRAND, VALUE_NONE_OTHER,
+                FIELD_PRODUCT_VARIANT, VALUE_NONE_OTHER,
+                FIELD_PRODUCT_CATEGORY, item.categoryBreadcrumbs
         )
     }
 
@@ -73,6 +121,158 @@ class RecommendationPageTracking {
                 )
         )
 
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionUserClickProductToWishlistForUserLogin(
+            isAdded: Boolean
+    ){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_CLICK_RECOMMENDATION,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_IMPRESSION_PRODUCT_WISHLIST,
+                EVENT_LABEL, VALUE_EMPTY
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionUserClickProductToWishlistForNonLogin(){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_CLICK_RECOMMENDATION,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_IMPRESSION_PRODUCT_WISHLIST_NON_LOGIN,
+                EVENT_LABEL, VALUE_EMPTY
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+
+    fun eventUserClickAddToCart(
+            recommendationItem: RecommendationItem
+    ){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_ADD_TO_CART,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_ADD_TO_CART,
+                EVENT_LABEL, VALUE_EMPTY,
+                ECOMMERCE, DataLayer.mapOf(
+                        ECOMMERCE_CURRENCY_CODE, VALUE_IDR,
+                        ECOMMERCE_ADD, DataLayer.listOf(
+                                ACTION_FIELD, DataLayer.listOf(
+                                  ACTION_FIELD_LIST, VALUE_LIST_CLICK_ADD_TO_CART
+                                ),
+                                ACTION_FIELD_PRODUCTS, listOf(convertRecommendationItemToDataObject(recommendationItem))
+                        )
+                )
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventUserClickRecommendation(){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_CLICK_RECOMMENDATION,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, VALUE_EMPTY,
+                EVENT_LABEL, VALUE_EMPTY
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventUserClickOnHeaderNameProduct(
+            headerName: String,
+            recommendationItem: RecommendationItem,
+            position: Int
+    ){
+
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_PRODUCT_CLICK,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, String.format(EVENT_CLICK_PRODUCT_ON_HEADER, headerName),
+                EVENT_LABEL, VALUE_EMPTY,
+                ECOMMERCE, DataLayer.mapOf(
+                    CLICK, DataLayer.mapOf(
+                        ACTION_FIELD, DataLayer.mapOf(
+                            ACTION_FIELD_LIST, String.format
+                                (if(recommendationItem.isTopAds)
+                                    ACTION_FIELD_LIST_CLICK_HEADER_TOP_ADS
+                                else ACTION_FIELD_LIST_CLICK_HEADER,
+                                        headerName, recommendationItem.recommendationType)
+                        ),
+                        ACTION_FIELD_PRODUCTS, listOf(convertRecommendationItemToDataObject(recommendationItem, position.toString())
+                        )
+                    )
+                )
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionProductRecommendationOnHeaderNameLogin(
+            headerName: String,
+            recommendationItem: RecommendationItem,
+            position: Int
+    ){
+
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_PRODUCT_VIEW,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, String.format(EVENT_ACTION_IMPRESSION_PRODUCT_ON_HEADER_LOGIN, headerName),
+                EVENT_LABEL, VALUE_EMPTY,
+                ECOMMERCE, DataLayer.mapOf(
+                    ECOMMERCE_CURRENCY_CODE, VALUE_IDR,
+                    ECOMMERCE_IMPRESSIONS, listOf(convertRecommendationItemToDataObject(recommendationItem,
+                            VALUE_LIST_ORGANIC_PRODUCT_RECOMMENDATION, position.toString())
+                    )
+                )
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionProductRecommendationOnHeaderName(
+            recommendationItem: RecommendationItem,
+            position: Int
+    ){
+
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_PRODUCT_VIEW,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_IMPRESSION_PRODUCT_ON_HEADER,
+                EVENT_LABEL, VALUE_EMPTY,
+                ECOMMERCE, DataLayer.mapOf(
+                    ECOMMERCE_CURRENCY_CODE, VALUE_IDR,
+                    ECOMMERCE_IMPRESSIONS, listOf(convertRecommendationItemToDataObject(recommendationItem,
+                            VALUE_LIST_ORGANIC_PRODUCT_RECOMMENDATION, position.toString())
+                    )
+                )
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionUserClickBack(){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_CLICK_RECOMMENDATION,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_CLICK_BACK,
+                EVENT_LABEL, VALUE_EMPTY
+        )
+        tracker.sendEnhanceEcommerceEvent(data)
+    }
+
+    fun eventImpressionUserClickSeeToCart(){
+        val tracker = getTracker()
+        val data = DataLayer.mapOf(
+                EVENT, EVENT_CLICK_RECOMMENDATION,
+                EVENT_CATEGORY, EVENT_RECOMMENDATION_PAGE,
+                EVENT_ACTION, EVENT_ACTION_CLICK_SEE_CART,
+                EVENT_LABEL, VALUE_EMPTY
+        )
         tracker.sendEnhanceEcommerceEvent(data)
     }
 }
--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/di/GraphQLRawModule.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.home_recom.di
-
-import dagger.Module
-
-@HomeRecommendationScope
-@Module
-class GraphQLRawModule {
-
-}
