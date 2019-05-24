eb3463fdffeb5656fc723e20a8e2d42fae5dccc7
okasurya <brilliantsurya@gmail.com>
2019-05-24 19:30:53 +0700

    add migration function

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/Constants.kt
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/Constants.kt
@@ -4,6 +4,5 @@ package com.tokopedia.pushnotif.db
  * @author okasurya on 5/22/19.
  */
 
-const val PUSHNOTIF_DB_VERSION = 2
 const val PUSHNOTIF_DB = "PushNotificationDB2.db"
 const val HISTORY_NOTIFICATION_TABLE = "HistoryNotificationDB"
--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
@@ -11,14 +11,22 @@ import com.tokopedia.pushnotif.db.model.HistoryNotificationDB
 /**
  * @author okasurya on 5/22/19.
  */
-@Database(entities = [HistoryNotificationDB::class], version = PUSHNOTIF_DB_VERSION)
-abstract class PushNotificationDB: RoomDatabase() {
+@Database(entities = [HistoryNotificationDB::class], version = 2)
+abstract class PushNotificationDB : RoomDatabase() {
     abstract fun historyNotificationDao(): HistoryNotificationDao
 
     companion object {
         // For Singleton instantiation
         @Volatile
         private var instance: PushNotificationDB? = null
+
+        static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+            @Override
+            public void migrate(SupportSQLiteDatabase database) {
+                // We increment the version as a flag of dbflow to room migration
+                // Since we didn't alter the table, there's nothing else to do here.
+            }
+        };
 
         fun getInstance(context: Context): PushNotificationDB {
             return instance ?: synchronized(this) {
@@ -27,7 +35,12 @@ abstract class PushNotificationDB: RoomDatabase() {
         }
 
         private fun buildDatabase(context: Context): PushNotificationDB {
-            return Room.databaseBuilder(context, PushNotificationDB::class.java, PUSHNOTIF_DB).build()
+            return Room.databaseBuilder(
+                    context,
+                    PushNotificationDB::class.java,
+                    PUSHNOTIF_DB)
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
         }
     }
 }
