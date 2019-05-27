b60aa6c9b55f86912917c98b55301548018161bc
okasurya <brilliantsurya@gmail.com>
2019-05-27 19:01:14 +0700

    add nullable annotation for backward compatibility

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/model/HistoryNotificationDB.kt
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/model/HistoryNotificationDB.kt
@@ -4,16 +4,21 @@ import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import com.tokopedia.pushnotif.db.HISTORY_NOTIFICATION_TABLE
+import org.jetbrains.annotations.Nullable
 
 @Entity(tableName = HISTORY_NOTIFICATION_TABLE)
 data class HistoryNotificationDB(
     @ColumnInfo(name = "sender_name")
+    @Nullable
     var senderName: String?,
     @ColumnInfo(name = "message")
+    @Nullable
     var message: String?,
     @ColumnInfo(name = "notification_type")
+    @Nullable
     var notificationType: Int?,
     @ColumnInfo(name = "notification_id")
+    @Nullable
     var notificationId: Int?
 ) {
     @PrimaryKey(autoGenerate = true)
