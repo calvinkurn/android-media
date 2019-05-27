5f21da346ef4d86cdeb89f098100f3a8d7f2afc3
okasurya <brilliantsurya@gmail.com>
2019-05-27 11:56:56 +0700

    fix migration method for database

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
@@ -1,8 +1,10 @@
 package com.tokopedia.pushnotif.db
 
+import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import android.arch.persistence.room.migration.Migration
 import android.content.Context
 import com.tokopedia.pushnotif.db.dao.HistoryNotificationDao
 import com.tokopedia.pushnotif.db.model.HistoryNotificationDB
@@ -20,13 +22,12 @@ abstract class PushNotificationDB : RoomDatabase() {
         @Volatile
         private var instance: PushNotificationDB? = null
 
-        static final Migration MIGRATION_1_2 = new Migration(1, 2) {
-            @Override
-            public void migrate(SupportSQLiteDatabase database) {
-                // We increment the version as a flag of dbflow to room migration
+        private val MIGRATION_1_2 = object: Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
                 // Since we didn't alter the table, there's nothing else to do here.
+                // we just do it as a flag that this db using room since version 2
             }
-        };
+        }
 
         fun getInstance(context: Context): PushNotificationDB {
             return instance ?: synchronized(this) {
