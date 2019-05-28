89b2330dbe6f397984964d4c28163e200b8c03d3
nisie <nathania.sutedja@tokopedia.com>
2019-05-28 08:52:22 +0700

    Add body text to ktp form

--- features/user/user_identification/src/main/java/com/tokopedia/useridentification/view/fragment/UserIdentificationFormKtpFragment.java
+++ features/user/user_identification/src/main/java/com/tokopedia/useridentification/view/fragment/UserIdentificationFormKtpFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.tokopedia.abstraction.common.utils.image.ImageHandler;
 import com.tokopedia.abstraction.common.utils.view.MethodChecker;
@@ -16,8 +18,7 @@ import com.tokopedia.useridentification.view.activity.UserIdentificationFormActi
 import com.tokopedia.useridentification.view.viewmodel.UserIdentificationStepperModel;
 
 import static com.tokopedia.user_identification_common.KYCConstant.REQUEST_CODE_CAMERA_KTP;
-import static com.tokopedia.useridentification.view.fragment.UserIdentificationCameraFragment
-        .PARAM_VIEW_MODE_KTP;
+import static com.tokopedia.useridentification.view.fragment.UserIdentificationCameraFragment.PARAM_VIEW_MODE_KTP;
 
 /**
  * @author by alvinatin on 02/11/18.
@@ -27,11 +28,19 @@ public class UserIdentificationFormKtpFragment extends
         BaseUserIdentificationStepperFragment<UserIdentificationStepperModel>
         implements UserIdentificationFormActivity.Listener {
 
+    protected TextView subtitleBody;
+
     public static Fragment createInstance() {
         Fragment fragment = new UserIdentificationFormKtpFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    protected void initView(View view) {
+        super.initView(view);
+        subtitleBody = view.findViewById(R.id.subtitle_body);
     }
 
     @Override
@@ -49,6 +58,9 @@ public class UserIdentificationFormKtpFragment extends
     protected void setContentView() {
         title.setText(R.string.ktp_title);
         subtitle.setText(MethodChecker.fromHtml(getString(R.string.ktp_subtitle)));
+        subtitle.setGravity(Gravity.LEFT);
+        subtitleBody.setText(MethodChecker.fromHtml(getString(R.string.ktp_body)));
+        subtitleBody.setVisibility(View.VISIBLE);
         button.setText(R.string.ktp_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
