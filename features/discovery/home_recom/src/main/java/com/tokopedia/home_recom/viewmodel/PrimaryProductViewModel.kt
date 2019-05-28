47461edc06990bdef1a5134f4385de563684aa6e
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2019-05-28 17:14:37 +0700

    remove unused import

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/viewmodel/PrimaryProductViewModel.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/viewmodel/PrimaryProductViewModel.kt
@@ -1,24 +1,12 @@
 package com.tokopedia.home_recom.viewmodel
 
-import android.arch.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.CacheType
-import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
-import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.home_recom.HomeRecomRawQueryKeyConstant.QUERY_PRIMARY_PRODUCT
-import com.tokopedia.home_recom.PARAM_PRODUCT_ID
-import com.tokopedia.home_recom.PrimaryProductParams
-import com.tokopedia.home_recom.model.dataModel.ProductInfoDataModel
-import com.tokopedia.home_recom.model.entity.RecommendationProductDetail
-import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.wishlist.common.listener.WishListActionListener
 import com.tokopedia.wishlist.common.usecase.AddWishListUseCase
 import com.tokopedia.wishlist.common.usecase.RemoveWishListUseCase
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Named
 
