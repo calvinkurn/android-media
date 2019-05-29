7fef0b6a1d2f8e10200a361139ae7ea4ab7b5278
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2019-05-29 13:54:22 +0700

    delete base

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/model/dataModel/BaseHomeRecommendationDataModel.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.home_recom.model.dataModel
-
-import com.tokopedia.abstraction.base.view.adapter.Visitable
-import com.tokopedia.home_recom.view.adapter.HomeRecommendationTypeFactory
-
-interface BaseHomeRecommendationDataModel: Visitable<HomeRecommendationTypeFactory>
