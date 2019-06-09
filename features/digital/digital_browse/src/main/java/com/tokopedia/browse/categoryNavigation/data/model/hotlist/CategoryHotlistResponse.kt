a84bcbf9975387cfc74123a70b971c63f7fa32f6
abhishek-toko <abhishek.jain@tokopedia.com>
2019-06-09 19:46:55 +0530

    DownloadApplink handler

---
+++ features/digital/digital_browse/src/main/java/com/tokopedia/browse/categoryNavigation/data/model/hotlist/CategoryHotlistResponse.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.browse.categoryNavigation.data.model.hotlist
+
+import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+
+@Generated("com.robohorse.robopojogenerator")
+data class CategoryHotlistResponse(
+
+	@field:SerializedName("data")
+	val data: Data? = null
+)
