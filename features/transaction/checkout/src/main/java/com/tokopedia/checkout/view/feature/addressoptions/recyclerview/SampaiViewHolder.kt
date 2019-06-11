5c7c9264a4f0b8732f4a4c4a186d5c9014d9fc88
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-06-11 18:40:07 +0700

    Implement tooltip information sampai

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/addressoptions/recyclerview/SampaiViewHolder.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/addressoptions/recyclerview/SampaiViewHolder.kt
@@ -5,10 +5,12 @@ import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import com.tokopedia.checkout.R
 import com.tokopedia.checkout.domain.datamodel.addressoptions.CornerAddressModel
+import com.tokopedia.design.component.Tooltip
 
 /**
  * Created by fajarnuha on 2019-05-21.
@@ -20,6 +22,7 @@ class SampaiViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private var mCornerName: TextView = itemView.findViewById(R.id.text_view_corner)
     private var mBranchName: TextView = itemView.findViewById(R.id.text_view_branch)
     private var mRadio: RadioButton = itemView.findViewById(R.id.radio_button_corner)
+    private var mInfo: ImageView = itemView.findViewById(R.id.img_info)
 
     fun bind(model: CornerAddressModel, listener: ShipmentAddressListAdapter.ActionListener, position: Int) {
         if (model.cornerModel != null) {
@@ -33,7 +36,17 @@ class SampaiViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             mCornerView.visibility = View.GONE
             mButton.text = itemView.context.getString(R.string.button_choose_corner)
         }
+        mInfo.setOnClickListener(showCornerInfo)
         mButton.setOnClickListener { view -> listener.onCornerButtonClicked() }
+    }
+
+    private val showCornerInfo: (view: View) -> Unit = {
+        val tooltip = Tooltip(it.context)
+        tooltip.setTitle("Tokopedia Corner")
+        tooltip.setDesc("Pesanan akan dikirim ke Tokopedia Corner yang ada di kampus")
+        tooltip.setTextButton("Mengerti")
+        tooltip.btnAction.setOnClickListener { tooltip.dismiss() }
+        tooltip.show()
     }
 
     companion object {
