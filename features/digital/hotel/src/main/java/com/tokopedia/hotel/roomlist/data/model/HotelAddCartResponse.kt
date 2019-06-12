b732b63aaa1cfedcc4285961a2c4afadcab99a9d
jessicasean <jessica.sean@tokopedia.com>
2019-06-12 16:27:36 +0700

    fix atc

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/roomlist/data/model/HotelAddCartResponse.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/roomlist/data/model/HotelAddCartResponse.kt
@@ -1,9 +1,14 @@
 package com.tokopedia.hotel.roomlist.data.model
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 /**
  * @author by jessica on 08/05/19
  */
 
 data class HotelAddCartResponse(
+        @SerializedName("cartID")
+        @Expose
         val cartId: String = ""
 )
