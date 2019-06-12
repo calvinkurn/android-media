75fb4a3ec1ec399cc6b73acee5c3920bd56a2446
jessicasean <jessica.sean@tokopedia.com>
2019-06-12 14:37:57 +0700

    changes on hotelbasefragment

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/common/presentation/HotelBaseFragment.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/common/presentation/HotelBaseFragment.kt
@@ -3,6 +3,7 @@ package com.tokopedia.hotel.common.presentation
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
@@ -15,20 +16,27 @@ import kotlinx.android.synthetic.main.item_network_error_view.view.*
  */
 
 abstract class HotelBaseFragment: BaseDaggerFragment() {
+
     fun showErrorState(e: Throwable) {
-        val inflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
-        params.gravity = Gravity.CENTER
-        params.weight = 1.0f
-        val retryLoad = inflater.inflate(R.layout.item_network_error_view, view?.parent as ViewGroup)
+        try {
+            view!!.findViewById<View>(R.id.main_retry)!!.visibility = View.VISIBLE
+        } catch (e: Exception) {
+            val inflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+            params.gravity = Gravity.CENTER
+            params.weight = 1.0f
+            val retryLoad = inflater.inflate(R.layout.item_network_error_view, view?.parent as ViewGroup)
 
-        retryLoad.iv_icon.setImageResource(ErrorHandlerHotel.getErrorImage(e))
-        retryLoad.message_retry.text = ErrorHandlerHotel.getErrorTitle(context, e)
-        retryLoad.sub_message_retry.text = ErrorHandlerHotel.getErrorMessage(context, e)
+            retryLoad.iv_icon.setImageResource(ErrorHandlerHotel.getErrorImage(e))
+            retryLoad.message_retry.text = ErrorHandlerHotel.getErrorTitle(context, e)
+            retryLoad.sub_message_retry.text = ErrorHandlerHotel.getErrorMessage(context, e)
 
-        retryLoad.button_retry.setOnClickListener {
-            onErrorRetryClicked()
+            retryLoad.button_retry.setOnClickListener {
+                retryLoad.main_retry.visibility = View.GONE
+                onErrorRetryClicked()
+            }
         }
+
     }
 
     abstract fun onErrorRetryClicked()
