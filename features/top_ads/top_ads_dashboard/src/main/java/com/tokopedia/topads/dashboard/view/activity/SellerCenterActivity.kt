8f20b23e6c37e97ace743ba9fcb719dc67328d3d
yehezkiel <yehezkiel@tokopedia.com>
2019-06-13 16:57:32 +0700

    -

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/activity/SellerCenterActivity.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/activity/SellerCenterActivity.kt
@@ -9,7 +9,7 @@ import com.tokopedia.abstraction.base.view.fragment.BaseSessionWebViewFragment
  * Created by hadi.putra on 16/05/18.
  */
 
-class appSellerCenterActivity : BaseSimpleActivity() {
+class SellerCenterActivity : BaseSimpleActivity() {
 
     override fun getNewFragment() = BaseSessionWebViewFragment.newInstance(SELLER_CENTER_URL)
 
