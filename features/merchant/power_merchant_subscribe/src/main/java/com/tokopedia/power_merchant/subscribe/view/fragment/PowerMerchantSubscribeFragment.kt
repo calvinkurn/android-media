a62b6b94605c405caa9242d806c5c0fccf87ae12
yehezkiel <yehezkiel@tokopedia.com>
2019-06-14 06:43:12 +0700

    -

--- features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/fragment/PowerMerchantSubscribeFragment.kt
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/fragment/PowerMerchantSubscribeFragment.kt
@@ -23,10 +23,8 @@ import com.tokopedia.power_merchant.subscribe.view.viewholder.PartialTncViewHold
 import com.tokopedia.user.session.UserSessionInterface
 import kotlinx.android.synthetic.main.fragment_power_merchant_subscribe.*
 import kotlinx.android.synthetic.main.fragment_power_merchant_subscribe.view.*
-import kotlinx.android.synthetic.main.partial_member_power_merchant.*
-import kotlinx.android.synthetic.main.partial_member_power_merchant.view.*
-import kotlinx.android.synthetic.main.partial_power_merchant_benefit.*
 import kotlinx.android.synthetic.main.partial_tnc_power_merchant.*
+
 
 import javax.inject.Inject
 
@@ -124,9 +122,9 @@ class PowerMerchantSubscribeFragment : BaseDaggerFragment(), PmSubscribeContract
     }
 
     private fun initializePartialPart(view: View?) {
-//        if (!::partialMemberPmViewHolder.isInitialized) {
-//            partialMemberPmViewHolder = PartialMemberPmViewHolder.build(view!!.base_partial_member, activity)
-//        }
+        if (!::partialMemberPmViewHolder.isInitialized) {
+            partialMemberPmViewHolder = PartialMemberPmViewHolder.build(base_partial_member, activity)
+        }
         if (!::partialTncViewHolder.isInitialized) {
             partialTncViewHolder = PartialTncViewHolder.build(base_partial_tnc, activity)
         }
