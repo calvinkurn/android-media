0073e7e0091a1b6d4ad7d34d96715188b61cbadc
lukaskris12 <lukaskris12@gmail.com>
2019-06-14 10:21:03 +0700

    update search on resume

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/model/KeywordSearchData.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/model/KeywordSearchData.kt
@@ -5,18 +5,13 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.home.beranda.domain.model.SearchPlaceholder
 
-class KeywordSearchData : Parcelable {
+class KeywordSearchData(parcel: Parcel) : Parcelable {
     @SerializedName("universe_placeholder")
     lateinit var searchData: SearchPlaceholder
-
-    constructor(parcel: Parcel) : super() {
-    }
 
     override fun writeToParcel(dest: Parcel?, flags: Int) {}
 
     override fun describeContents(): Int = 0
-
-    fun getKeywordData() = searchData
 
     companion object CREATOR : Parcelable.Creator<KeywordSearchData> {
         override fun createFromParcel(parcel: Parcel): KeywordSearchData {
