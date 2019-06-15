6d8d73d0aaf84af325fcbe2d0f12a81aa718071a
Muhammad Ilham Jamaludin <milham.jamaludin@gmail.com>
2019-06-15 17:07:13 +0700

    Add shop score GQL :sparkles:

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/data/shopscore/ShopScore.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/data/shopscore/ShopScore.kt
@@ -1,8 +1,13 @@
 package com.tokopedia.shop.common.graphql.data.shopscore
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class ShopScore(
         @SerializedName("result")
-        val result: Result = Result()
+        @Expose
+        val result: Result = Result(),
+        @SerializedName("error")
+        @Expose
+        val error: ShopScoreError = ShopScoreError()
 )
