a907e0a5ba74455a0ce0c69fceefe97c1e76f13f
darianthedytoped <darian.thedy@tokopedia.com>
2019-06-18 14:17:18 +0700

    fix attr label format string

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/widget/Label.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/widget/Label.kt
@@ -2,9 +2,6 @@ package com.tokopedia.search.result.presentation.view.widget
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
-import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
@@ -12,7 +9,7 @@ import com.tokopedia.search.R
 
 class Label : AppCompatTextView {
 
-    constructor(context: Context) : super(context) {}
+    constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         initAttrs(context, attrs)
@@ -34,29 +31,40 @@ class Label : AppCompatTextView {
 
     private fun configLabelDesign(attributeArray: TypedArray) {
         val labelType = attributeArray.getString(R.styleable.Label_labelType)
-        val drawable = ContextCompat.getDrawable(context, R.drawable.search_label)
 
-        drawable?.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, R.color.green_500), PorterDuff.Mode.MULTIPLY)
+        setLabelDesign(labelType)
+    }
 
-//        val sdk = Build.VERSION.SDK_INT
-//        if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
-//            setBackgroundDrawable(drawable)
-//        } else {
-            background = drawable
-//        }
+    fun setLabelDesign(labelDesign: String) {
+        when(labelDesign) {
+            lightGrey -> setBackgroundAndTextColor(R.color.product_card_label_light_grey_background, R.color.product_card_label_light_grey_text)
+            lightBlue -> setBackgroundAndTextColor(R.color.product_card_label_light_blue_background, R.color.product_card_label_light_blue_text)
+            lightGreen -> setBackgroundAndTextColor(R.color.product_card_label_light_green_background, R.color.product_card_label_light_green_text)
+            lightRed -> setBackgroundAndTextColor(R.color.product_card_label_light_red_background, R.color.product_card_label_light_red_text)
+            lightOrange -> setBackgroundAndTextColor(R.color.product_card_label_light_orange_background, R.color.product_card_label_light_orange_text)
+            darkGrey -> setBackgroundAndTextColor(R.color.product_card_label_dark_grey_background, R.color.product_card_label_dark_grey_text)
+            darkBlue -> setBackgroundAndTextColor(R.color.product_card_label_dark_blue_background, R.color.product_card_label_dark_blue_text)
+            darkGreen -> setBackgroundAndTextColor(R.color.product_card_label_dark_green_background, R.color.product_card_label_dark_green_text)
+            darkRed -> setBackgroundAndTextColor(R.color.product_card_label_dark_red_background, R.color.product_card_label_dark_red_text)
+            darkOrange -> setBackgroundAndTextColor(R.color.product_card_label_dark_orange_background, R.color.product_card_label_dark_orange_text)
+        }
+    }
+
+    private fun setBackgroundAndTextColor(backgroundColorResourceId: Int, textColorResourceId: Int) {
+        setBackgroundColor(ContextCompat.getColor(context, backgroundColorResourceId))
+        setTextColor(ContextCompat.getColor(context, textColorResourceId))
     }
 
     companion object {
-
-        private val lightGrey = "lightGrey"
-        private val darkGrey = "darkGrey"
-        private val lightRed = "lightRed"
-        private val darkRed = "darkRed"
-        private val lightGreen = "lightGreen"
-        private val darkGreen = "darkGreen"
-        private val lightBlue = "lightBlue"
-        private val darkBlue = "darkBlue"
-        private val lightOrange = "lightOrange"
-        private val darkOrange = "darkOrange"
+        private const val lightGrey = "lightGrey"
+        private const val lightBlue = "lightBlue"
+        private const val lightGreen = "lightGreen"
+        private const val lightRed = "lightRed"
+        private const val lightOrange = "lightOrange"
+        private const val darkGrey = "darkGrey"
+        private const val darkBlue = "darkBlue"
+        private const val darkGreen = "darkGreen"
+        private const val darkRed = "darkRed"
+        private const val darkOrange = "darkOrange"
     }
 }
