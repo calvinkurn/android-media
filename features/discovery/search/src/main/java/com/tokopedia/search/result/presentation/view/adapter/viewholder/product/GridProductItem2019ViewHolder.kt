c17b5de6ae38c52dff9242cbdfdf1bdb1d3af470
darianthedytoped <darian.thedy@tokopedia.com>
2019-06-18 15:35:08 +0700

    Rating icon renamed

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/adapter/viewholder/product/GridProductItem2019ViewHolder.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/adapter/viewholder/product/GridProductItem2019ViewHolder.kt
@@ -118,13 +118,13 @@ class GridProductItem2019ViewHolder(
 
     private fun getRatingDrawable(param: Int): Int {
         return when (param) {
-            0 -> R.drawable.ic_star_none
-            1 -> R.drawable.ic_star_one
-            2 -> R.drawable.ic_star_two
-            3 -> R.drawable.ic_star_three
-            4 -> R.drawable.ic_star_four
-            5 -> R.drawable.ic_star_five
-            else -> R.drawable.ic_star_none
+            0 -> R.drawable.search_ic_star_none
+            1 -> R.drawable.search_ic_star_one
+            2 -> R.drawable.search_ic_star_two
+            3 -> R.drawable.search_ic_star_three
+            4 -> R.drawable.search_ic_star_four
+            5 -> R.drawable.search_ic_star_five
+            else -> R.drawable.search_ic_star_none
         }
     }
 
