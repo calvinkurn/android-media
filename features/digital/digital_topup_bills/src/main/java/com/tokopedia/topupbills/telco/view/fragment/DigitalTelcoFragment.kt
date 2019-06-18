4f48d02ecc932a6bb7ddfeb5a0704965158e9ef3
Nabilla <nabilla.sabbaha@tokopedia.com>
2019-06-18 19:27:04 +0700

    remove unused TODO code

--- features/digital/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/fragment/DigitalTelcoFragment.kt
+++ features/digital/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/fragment/DigitalTelcoFragment.kt
@@ -52,15 +52,15 @@ class DigitalTelcoFragment : BaseDaggerFragment() {
         viewPager.adapter = pagerAdapter
         viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
             }
 
             override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
             }
 
             override fun onPageSelected(position: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
             }
         })
 
