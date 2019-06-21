33ac55b7d37d27a0bc2f62ecb3b5467658c3c4d8
naveen.goyal@tokopedia.com <Naveen@1987>
2019-06-21 18:03:14 +0530

    Applink for Voucher has been added

--- features/digital/digital_deals/src/main/java/com/tokopedia/digital_deals/view/activity/AllBrandsActivity.java
+++ features/digital/digital_deals/src/main/java/com/tokopedia/digital_deals/view/activity/AllBrandsActivity.java
@@ -1,10 +1,15 @@
 package com.tokopedia.digital_deals.view.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.text.Layout;
@@ -21,6 +26,9 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
+import com.tokopedia.abstraction.base.app.BaseMainApplication;
+import com.tokopedia.abstraction.common.di.component.HasComponent;
 import com.tokopedia.abstraction.common.utils.view.KeyboardHandler;
 import com.tokopedia.applink.ApplinkConst;
 import com.tokopedia.applink.RouteManager;
@@ -28,13 +36,18 @@ import com.tokopedia.design.bottomsheet.CloseableBottomSheetDialog;
 import com.tokopedia.digital_deals.DealsModuleRouter;
 import com.tokopedia.digital_deals.R;
 import com.tokopedia.digital_deals.data.source.DealsUrl;
+import com.tokopedia.digital_deals.di.DaggerDealsComponent;
+import com.tokopedia.digital_deals.di.DealsComponent;
 import com.tokopedia.digital_deals.view.adapter.BrandsFragmentPagerAdapter;
 import com.tokopedia.digital_deals.view.adapter.DealsLocationAdapter;
+import com.tokopedia.digital_deals.view.contractor.AllBrandsHomeContract;
 import com.tokopedia.digital_deals.view.customview.SearchInputView;
 import com.tokopedia.digital_deals.view.fragment.AllBrandsFragment;
 import com.tokopedia.digital_deals.view.fragment.SelectLocationBottomSheet;
 import com.tokopedia.digital_deals.view.model.CategoriesModel;
+import com.tokopedia.digital_deals.view.model.CategoryItem;
 import com.tokopedia.digital_deals.view.model.Location;
+import com.tokopedia.digital_deals.view.presenter.AllCategoryPresenter;
 import com.tokopedia.digital_deals.view.utils.DealsAnalytics;
 import com.tokopedia.digital_deals.view.utils.Utils;
 import com.tokopedia.user.session.UserSession;
@@ -42,7 +55,9 @@ import com.tokopedia.user.session.UserSession;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AllBrandsActivity extends DealsBaseActivity implements SearchInputView.Listener, View.OnClickListener, AllBrandsFragment.UpdateLocation, PopupMenu.OnMenuItemClickListener, SelectLocationBottomSheet.CloseSelectLocationBottomSheet, DealsLocationAdapter.ActionListener {
+import javax.inject.Inject;
+
+public class AllBrandsActivity extends DealsBaseActivity implements AllBrandsHomeContract.View, HasComponent<DealsComponent>, SearchInputView.Listener, View.OnClickListener, AllBrandsFragment.UpdateLocation, PopupMenu.OnMenuItemClickListener, SelectLocationBottomSheet.CloseSelectLocationBottomSheet, DealsLocationAdapter.ActionListener {
 
     public static final String EXTRA_CATEGOTRY_LIST = "category_item_list";
     private ViewPager categoryViewPager;
@@ -54,6 +69,7 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
     private SearchInputView searchInputView;
     private ImageView backArrow, overFlowIcon;
     AllBrandsFragment allBrandsFragment;
+    CoordinatorLayout mainContent;
     DealsAnalytics dealsAnalytics;
     List<CategoriesModel> categoryList = new ArrayList<>();
     private String searchText;
@@ -61,7 +77,37 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
     private Menu mMenu;
     private int categoryId;
     private int position = 0;
+    @Inject
+    AllCategoryPresenter mPresenter;
     CloseableBottomSheetDialog selectLocationFragment;
+
+    public static final String FROM_VOUCHER = "isVoucher";
+
+
+    @DeepLink({DealsUrl.AppLink.DIGITAL_DEALS_ALL_BRAND})
+    public static TaskStackBuilder getAllBrandsStaticIntent(Context context, Bundle extras) {
+        Intent destination;
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+
+        Intent homeIntent = ((DealsModuleRouter) context.getApplicationContext()).getHomeIntent(context);
+        taskStackBuilder.addNextIntent(homeIntent);
+        taskStackBuilder.addNextIntent(new Intent(context, DealsHomeActivity.class));
+
+        if (extras != null) {
+            String deepLink = extras.getString(DeepLink.URI);
+            Uri.Builder uri = Uri.parse(deepLink).buildUpon();
+
+
+            extras.putString(FROM_VOUCHER, extras.getString(FROM_VOUCHER));
+            destination = new Intent(context, AllBrandsActivity.class)
+                    .setData(uri.build())
+                    .putExtras(extras);
+
+            taskStackBuilder.addNextIntent(destination);
+        }
+        return taskStackBuilder;
+    }
 
     @Override
     protected int getLayoutRes() {
@@ -71,6 +117,7 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getComponent().inject(this);
         overridePendingTransition(R.anim.slide_in_left_brands, R.anim.slide_out_right_brands);
         userSession = new UserSession(this);
         dealsAnalytics = new DealsAnalytics();
@@ -78,6 +125,7 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
     }
 
     private void setUpVariables() {
+        mainContent = findViewById(R.id.main_content);
         tabs = findViewById(R.id.tabs);
         searchInputView = findViewById(R.id.search_input_view);
         backArrow = findViewById(R.id.backArraw);
@@ -89,40 +137,47 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
         searchInputView.setSearchImageViewDimens(getResources().getDimensionPixelSize(R.dimen.dp_24), getResources().getDimensionPixelSize(R.dimen.dp_24));
         searchInputView.setListener(this);
         categoryViewPager = findViewById(R.id.container);
-        categoryList = getIntent().getParcelableArrayListExtra(EXTRA_LIST);
-        if (getIntent() != null && !TextUtils.isEmpty(getIntent().getStringExtra("cat_id"))) {
-            categoryId = Integer.parseInt(getIntent().getStringExtra("cat_id"));
-            for (int i = 0; i < categoryList.size(); i++) {
-                if (categoryId == categoryList.get(i).getCategoryId()) {
-                    position = i;
-                }
-            }
-        }
         searchText = getIntent().getStringExtra(SEARCH_TEXT);
         if (!TextUtils.isEmpty(searchText)) {
             searchInputView.setSearchText(searchText);
         }
-        brandsTabsPagerAdapter =
-                new BrandsFragmentPagerAdapter(getSupportFragmentManager(), categoryList, searchInputView.getSearchText());
-        categoryViewPager.setAdapter(brandsTabsPagerAdapter);
-        tabs.setupWithViewPager(categoryViewPager);
-        categoryViewPager.setOffscreenPageLimit(1);
-        categoryViewPager.setCurrentItem(position);
-        categoryViewPager.setSaveFromParentEnabled(false);
+        if (!TextUtils.isEmpty(getIntent().getStringExtra(FROM_VOUCHER))) {
+            toolbarTitle.setVisibility(View.GONE);
+            mPresenter.attachView(this);
+            mPresenter.getAllCategories();
+        } else {
+            categoryList = getIntent().getParcelableArrayListExtra(EXTRA_LIST);
+            toolbarTitle.setOnClickListener(this);
+            Location location = Utils.getSingletonInstance().getLocation(this);
+            if (location != null) {
+                toolbarTitle.setText(location.getName());
+            }
 
-        toolbarTitle.setOnClickListener(this);
-        KeyboardHandler.DropKeyboard(this, searchInputView);
+            if (getIntent() != null && !TextUtils.isEmpty(getIntent().getStringExtra("cat_id"))) {
+                categoryId = Integer.parseInt(getIntent().getStringExtra("cat_id"));
+                for (int i = 0; i < categoryList.size(); i++) {
+                    if (categoryId == categoryList.get(i).getCategoryId()) {
+                        position = i;
+                    }
+                }
+            }
+            brandsTabsPagerAdapter =
+                    new BrandsFragmentPagerAdapter(getSupportFragmentManager(), categoryList, searchInputView.getSearchText());
+            categoryViewPager.setAdapter(brandsTabsPagerAdapter);
+            tabs.setupWithViewPager(categoryViewPager);
+            categoryViewPager.setOffscreenPageLimit(1);
+            categoryViewPager.setCurrentItem(position);
+            categoryViewPager.setSaveFromParentEnabled(false);
 
-        Location location = Utils.getSingletonInstance().getLocation(this);
-        if (location != null) {
-            toolbarTitle.setText(location.getName());
+
+            KeyboardHandler.DropKeyboard(this, searchInputView);
+            Fragment fragment = brandsTabsPagerAdapter.getSelectedFragment(categoryViewPager.getCurrentItem());
+            if (fragment instanceof AllBrandsFragment) {
+                allBrandsFragment = (AllBrandsFragment) fragment;
+            }
+
+            setCategoryViewPagerListener();
         }
-        Fragment fragment = brandsTabsPagerAdapter.getSelectedFragment(categoryViewPager.getCurrentItem());
-        if (fragment instanceof AllBrandsFragment) {
-            allBrandsFragment = (AllBrandsFragment) fragment;
-        }
-
-        setCategoryViewPagerListener();
 
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -213,7 +268,7 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
         }
     }
 
-    private AllBrandsFragment getCurrentSelectedFragment(){
+    private AllBrandsFragment getCurrentSelectedFragment() {
         Fragment fragment = brandsTabsPagerAdapter.getSelectedFragment(categoryViewPager.getCurrentItem());
         if (fragment instanceof AllBrandsFragment) {
             return (AllBrandsFragment) fragment;
@@ -258,16 +313,75 @@ public class AllBrandsActivity extends DealsBaseActivity implements SearchInputV
 
     @Override
     public void onLocationItemSelected(boolean locationUpdated) {
-        Location location= Utils.getSingletonInstance().getLocation(this);
-        if (locationUpdated && location!= null) {
+        Location location = Utils.getSingletonInstance().getLocation(this);
+        if (locationUpdated && location != null) {
             toolbarTitle.setText(location.getName());
         }
-        AllBrandsFragment allBrandsFragment=getCurrentSelectedFragment();
-        if(allBrandsFragment!=null && locationUpdated){
+        AllBrandsFragment allBrandsFragment = getCurrentSelectedFragment();
+        if (allBrandsFragment != null && locationUpdated) {
             allBrandsFragment.onLocationUpdated();
         }
         if (selectLocationFragment != null) {
             selectLocationFragment.dismiss();
         }
+    }
+
+
+    @Override
+    public DealsComponent getComponent() {
+        return DaggerDealsComponent.builder()
+                .baseAppComponent(((BaseMainApplication) getApplication()).getBaseAppComponent())
+                .build();
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public View getRootView() {
+        return mainContent;
+    }
+
+    @Override
+    public void renderCategoryList(List<CategoryItem> categoryItems) {
+        for (int i = 0; i < categoryItems.size() - 1; i++) {
+            if (categoryItems.get(i).getIsCard() != 1) {
+                CategoriesModel categoriesModel = new CategoriesModel();
+                categoriesModel.setName(categoryItems.get(i).getName());
+                categoriesModel.setTitle(categoryItems.get(i).getTitle());
+                categoriesModel.setCategoryUrl(categoryItems.get(i).getCategoryUrl());
+                categoriesModel.setPosition(i);
+                categoriesModel.setCategoryId(categoryItems.get(i).getCategoryId());
+                categoriesModel.setItems(categoryItems.get(i).getItems());
+                categoryList.add(categoriesModel);
+            }
+        }
+
+        if (getIntent() != null && !TextUtils.isEmpty(getIntent().getStringExtra("cat_id"))) {
+            categoryId = Integer.parseInt(getIntent().getStringExtra("cat_id"));
+            for (int i = 0; i < categoryList.size(); i++) {
+                if (categoryId == categoryList.get(i).getCategoryId()) {
+                    position = i;
+                }
+            }
+        }
+        brandsTabsPagerAdapter =
+                new BrandsFragmentPagerAdapter(getSupportFragmentManager(), categoryList, searchInputView.getSearchText());
+        categoryViewPager.setAdapter(brandsTabsPagerAdapter);
+        tabs.setupWithViewPager(categoryViewPager);
+        categoryViewPager.setOffscreenPageLimit(1);
+        categoryViewPager.setCurrentItem(position);
+        categoryViewPager.setSaveFromParentEnabled(false);
+
+
+        KeyboardHandler.DropKeyboard(this, searchInputView);
+        Fragment fragment = brandsTabsPagerAdapter.getSelectedFragment(categoryViewPager.getCurrentItem());
+        if (fragment instanceof AllBrandsFragment) {
+            allBrandsFragment = (AllBrandsFragment) fragment;
+        }
+
+        setCategoryViewPagerListener();
     }
 }
