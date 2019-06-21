feed779a0fd21c7592222bf764a3142f3fa520d8
Erry Suprayogi <eriksuprayogi@gmail.com>
2019-06-21 09:52:23 +0700

    toaster

---
+++ features/top_ads/top_ads_autoads/src/main/java/com/tokopedia/topads/auto/view/widget/ToasterAutoAds.kt
@@ -0,0 +1,39 @@
+package com.tokopedia.topads.auto.view.widget
+
+import android.app.Activity
+import android.support.design.widget.Snackbar
+import android.view.View
+
+import com.tokopedia.design.base.BaseToaster
+import com.tokopedia.design.component.ToasterNormal
+import com.tokopedia.topads.auto.R
+
+/**
+ * Author errysuprayogi on 21,June,2019
+ */
+class ToasterAutoAds : BaseToaster() {
+    companion object {
+
+        fun make(view: View,
+                 snackbarText: String,
+                 @BaseToaster.Duration duration: Int): Snackbar {
+            return BaseToaster.Builder(view, snackbarText, duration)
+                    .setBackgroundDrawable(R.drawable.bg_toaster_autoads)
+                    .setTextColor(R.color.white)
+                    .setActionTextColor(R.color.white)
+                    .build()
+        }
+
+        fun showClose(activity: Activity,
+                      snackbarText: String) {
+            if (activity == null) {
+                return
+            }
+            ToasterNormal.make(activity.findViewById(android.R.id.content),
+                    snackbarText, BaseToaster.LENGTH_LONG)
+                    .setAction(activity.getString(com.tokopedia.design.R.string.close)) { v ->
+                        // no-op
+                    }.show()
+        }
+    }
+}
