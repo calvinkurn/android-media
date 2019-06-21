dee6bfb5996f10643fc31b7aee6d16878bf7a4f7
lukaskris12 <lukaskris12@gmail.com>
2019-06-21 13:37:05 +0700

    fix padding product rating

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/viewmodel/RecommendationPageViewModel.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/viewmodel/RecommendationPageViewModel.kt
@@ -33,7 +33,6 @@ class RecommendationPageViewModel @Inject constructor(private val graphqlReposit
 
     val xSource = "recom_landing_page"
     val pageName = "recom_1"
-//    val pageName = "recom_1,recom_2,recom_3"
 
     fun getPrimaryProduct(productId: String,
                           context: Context) {
