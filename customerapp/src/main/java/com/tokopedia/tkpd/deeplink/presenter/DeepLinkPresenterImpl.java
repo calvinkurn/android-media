04cda8672222192c47fd673f58561e0c276329b7
lukaskris12 <lukaskris12@gmail.com>
2019-06-23 21:21:18 +0700

    fix deeplink done

--- customerapp/src/main/java/com/tokopedia/tkpd/deeplink/presenter/DeepLinkPresenterImpl.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/deeplink/presenter/DeepLinkPresenterImpl.java
@@ -515,7 +515,6 @@ public class DeepLinkPresenterImpl implements DeepLinkPresenter {
     private void openHomeRecommendation(final List<String> linkSegment, final Uri uriData) {
         if (linkSegment != null && linkSegment.size() > 0) {
             Intent intent = RouteManager.getIntent(context  , ApplinkConstInternalMarketplace.HOME_RECOMMENDATION, linkSegment.get(1));
-//            intent.putExtra(HomeRecommendationActivity.PRODUCT_ID, linkSegment.get(1));
             context.startActivity(intent);
             context.finish();
         }
