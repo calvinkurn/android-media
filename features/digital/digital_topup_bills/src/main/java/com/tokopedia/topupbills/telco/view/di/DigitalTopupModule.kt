1bbb1fb5e9a842a43132107d4cab633159893499
Nabilla <nabilla.sabbaha@tokopedia.com>
2019-06-25 17:43:04 +0700

    provide user session in digital topup module

--- features/digital/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/di/DigitalTopupModule.kt
+++ features/digital/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/di/DigitalTopupModule.kt
@@ -1,8 +1,12 @@
 package com.tokopedia.topupbills.telco.view.di
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.permissionchecker.PermissionCheckerHelper
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
@@ -30,5 +34,10 @@ class DigitalTopupModule {
         return GraphqlInteractor.getInstance().graphqlRepository
     }
 
+    @DigitalTopupScope
+    @Provides
+    fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface {
+        return UserSession(context)
+    }
 
 }
