75961e416a2116783e9712b407bd2d30d3dfcb42
rahman.zulfikar94@gmail.com <rahman.zulfikar94@gmail.com>
2019-06-27 10:52:51 +0700

    improvement cache multirequest gql

--- libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/data/source/GraphqlCacheDataStore.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/data/source/GraphqlCacheDataStore.kt
@@ -3,6 +3,7 @@ package com.tokopedia.graphql.coroutines.data.source
 import com.google.gson.JsonParser
 import com.tokopedia.graphql.FingerprintManager
 import com.tokopedia.graphql.GraphqlCacheManager
+import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.data.model.GraphqlResponseInternal
@@ -14,18 +15,24 @@ class GraphqlCacheDataStore(private val mCacheManager: GraphqlCacheManager,
 
     override suspend fun getResponse(requests: List<GraphqlRequest>, cacheStrategy: GraphqlCacheStrategy): GraphqlResponseInternal {
         return withContext(Dispatchers.IO){
-            val indexOfEmptyCached = ArrayList<Int>()
-            val listOfCached = ArrayList<String>()
-            requests.forEachIndexed { index, graphqlRequest ->
-                val rawJson = mCacheManager.get(mFingerprintManager.generateFingerPrint(graphqlRequest.toString(),
-                        cacheStrategy.isSessionIncluded))
-                if (rawJson.isNullOrEmpty()){
-                    indexOfEmptyCached.add(index)
-                }else{
-                    listOfCached.add(rawJson)
+            if(cacheStrategy.type == CacheType.CACHE_FIRST) {
+                val indexOfEmptyCached = ArrayList<Int>()
+                val listOfCached = ArrayList<String>()
+                requests.forEachIndexed { index, graphqlRequest ->
+                    val rawJson = mCacheManager.get(mFingerprintManager.generateFingerPrint(graphqlRequest.toString(),
+                            cacheStrategy.isSessionIncluded))
+                    if (rawJson.isNullOrEmpty()) {
+                        indexOfEmptyCached.add(index)
+                    } else {
+                        listOfCached.add(rawJson)
+                    }
                 }
+                GraphqlResponseInternal(JsonParser().parse(listOfCached.toString()).asJsonArray, true, indexOfEmptyCached)
+            }else{
+                val rawJson = mCacheManager.get(mFingerprintManager.generateFingerPrint(requests.toString(),
+                        cacheStrategy.isSessionIncluded))
+                GraphqlResponseInternal(JsonParser().parse(rawJson).asJsonArray, true)
             }
-            GraphqlResponseInternal(JsonParser().parse(listOfCached.toString()).asJsonArray, true, indexOfEmptyCached)
         }
     }
 }
