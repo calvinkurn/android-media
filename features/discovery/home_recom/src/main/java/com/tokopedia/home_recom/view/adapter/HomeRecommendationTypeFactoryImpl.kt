b4a52400fae3b436db7af592527b240cf84f11f6
lukaskris12 <lukaskris12@gmail.com>
2019-06-28 12:50:42 +0700

    add loading shimmer grid

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/adapter/HomeRecommendationTypeFactoryImpl.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/adapter/HomeRecommendationTypeFactoryImpl.kt
@@ -2,7 +2,9 @@ package com.tokopedia.home_recom.view.adapter
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.base.view.adapter.viewholders.LoadingShimmeringGridViewHolder
 import com.tokopedia.home_recom.model.datamodel.TitleDataModel
 import com.tokopedia.home_recom.model.datamodel.RecommendationItemDataModel
 import com.tokopedia.home_recom.model.datamodel.RecommendationCarouselItemDataModel
@@ -35,12 +37,17 @@ class HomeRecommendationTypeFactoryImpl : BaseAdapterTypeFactory(), HomeRecommen
         return RecommendationCarouselItemDataModel.LAYOUT
     }
 
+    override fun type(viewModel: LoadingModel?): Int {
+        return LoadingShimmeringGridViewHolder.LAYOUT
+    }
+
     override fun createViewHolder(view: View, type: Int): AbstractViewHolder<*> {
         return when(type){
             RecommendationItemDataModel.LAYOUT -> RecommendationItemViewHolder(view)
             ProductInfoDataModel.LAYOUT -> ProductInfoViewHolder(view)
             RecommendationCarouselDataModel.LAYOUT -> RecommendationCarouselViewHolder(view)
             TitleDataModel.LAYOUT -> TitleViewHolder(view)
+            LoadingShimmeringGridViewHolder.LAYOUT -> LoadingShimmeringGridViewHolder(view)
             else -> super.createViewHolder(view, type)
         }
     }
