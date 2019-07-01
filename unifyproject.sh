542b92af6e9ec9dc90f65baaa538d89a2086eeb6
Meyta <meyta.taliti@tokopedia.com>
2019-07-01 14:05:04 +0700

    add shell script for unify dev

---
+++ unifyproject.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+BRANCH_NAME='branchname'
+
+help() {
+    echo "start              => Start Development"
+    echo "commit <message>   => Add + Commit to Unify"
+    echo "push               => Push to Unify"
+    echo "publish <project>  => Publish to Artifactory, list project: icon / principles / components"
+}
+
+startDev( ) {
+    echo "Starting development..."
+    #if [ `git branch | egrep "^[[:space:]]+${feature/unify/dev}$"` ];
+    #then
+    #    echo "git checkout -b feature/unify/dev"
+    #else
+    #    echo "git checkout feature/unify/dev"
+    #fi
+
+    echo $BRANCH_NAME | sed -e "s/\$BATCH_END/$BATCH_END/g"
+
+}
+
+commit( ) {
+    echo "Commit !"
+    echo $BRANCH_NAME
+}
+
+push( ) {
+    echo "Push !"
+}
+
+publish( ) {
+    echo "Publish !"
+    echo "$1"
+}
+
+if [ -z "$1" ]; then
+    help
+    exit 1
+fi
+
+if [ $1 = "start" ]; then
+    startDev $2
+elif [ $1 = "commit" ]; then
+    commit "$2"
+elif [ $1 = "push" ]; then
+    push
+elif [ $1 = "publish" ]; then
+     publish $2
+else
+    help
+fi
+
