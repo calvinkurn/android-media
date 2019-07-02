dc9ff3cd5f449ff2ff33c08bd6f0b3ffbb0008cd
lukaskris12 <lukaskris12@gmail.com>
2019-07-02 11:41:14 +0700

    set visibility gone badgeview

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/ProductCardView.java
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/ProductCardView.java
@@ -141,7 +141,7 @@ public class ProductCardView extends BaseCustomView {
 
     public void setBadgeUrl(String url){
         if(!url.isEmpty())  ImageHandler.loadImageFitCenter(getContext(), badgeView, url);
-        else badgeView.setVisibility(View.INVISIBLE);
+        else badgeView.setVisibility(View.GONE);
     }
 
     public void setLocation(String location){
