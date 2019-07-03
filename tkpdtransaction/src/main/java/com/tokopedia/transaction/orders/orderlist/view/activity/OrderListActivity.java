85bc6433c0c59a0f8a716466d4271b06e89d173e
Jessica Sean <jessica.sean@tokopedia.com>
2019-07-03 09:57:48 +0700

    fix compile error

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/activity/OrderListActivity.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/activity/OrderListActivity.java
@@ -77,43 +77,7 @@ public class OrderListActivity extends BaseSimpleActivity
         Intent intent = new Intent(context, OrderListActivity.class);
         return intent.putExtras(extras);
     }
-
-
-    @DeepLink({ApplinkConst.PURCHASE_CONFIRMED, ApplinkConst.PURCHASE_ORDER})
-    public static Intent getConfirmedIntent(Context context, Bundle extras) {
-        return getMarketPlaceIntent(context, extras);
-
-    }
-
-    @DeepLink(ApplinkConst.PURCHASE_PROCESSED)
-    public static Intent getProcessedIntent(Context context, Bundle extras) {
-        return getMarketPlaceIntent(context, extras);
-    }
-
-    @DeepLink({ApplinkConst.PURCHASE_SHIPPED})
-    public static Intent getShippedIntent(Context context, Bundle extras) {
-        return getMarketPlaceIntent(context, extras);
-    }
-
-    @DeepLink({ApplinkConst.PURCHASE_DELIVERED, ApplinkConst.PURCHASE_SHIPPING_CONFIRM})
-    public static Intent getDeliveredIntent(Context context, Bundle extras) {
-        return getMarketPlaceIntent(context, extras);
-    }
-
-    @DeepLink(ApplinkConst.PURCHASE_HISTORY)
-    public static Intent getHistoryIntent(Context context, Bundle extras) {
-        return getMarketPlaceIntent(context, extras);
-    }
-
-
-    private static Intent getMarketPlaceIntent(Context context, Bundle extras) {
-        Uri uri = Uri.parse(extras.getString(DeepLink.URI));
-        extras.putString(OrderCategory.KEY_LABEL, OrderCategory.MARKETPLACE);
-        extras.putString(OrderListContants.ORDER_FILTER_ID, uri.getQueryParameter(OrderListContants.ORDER_FILTER_ID));
-        Intent intent = new Intent(context, OrderListActivity.class);
-        return intent.putExtras(extras);
-    }
-
+    
     @DeepLink({ApplinkConst.DEALS_ORDER,
             ApplinkConst.DIGITAL_ORDER,
             ApplinkConst.EVENTS_ORDER,
