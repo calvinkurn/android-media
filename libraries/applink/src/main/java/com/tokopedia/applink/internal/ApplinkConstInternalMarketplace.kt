b0b11d3971d757b5936601197e26baee20d08c11
Henry Priyono <henry.priyono@tokopedia.com>
2019-07-03 10:46:38 +0700

    Add slash

--- libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalMarketplace.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalMarketplace.kt
@@ -43,7 +43,7 @@ object ApplinkConstInternalMarketplace {
     @JvmField
     val PRODUCT_DETAIL = "$INTERNAL_MARKETPLACE/product-detail/{id}/"
     @JvmField
-    val PRODUCT_DETAIL_WITH_WAREHOUSE_ID = "$INTERNAL_MARKETPLACE/product-detail/{id}?warehouse_id={whid}"
+    val PRODUCT_DETAIL_WITH_WAREHOUSE_ID = "$INTERNAL_MARKETPLACE/product-detail/{id}/?warehouse_id={whid}"
     @JvmField
     val PRODUCT_DETAIL_DOMAIN = "$INTERNAL_MARKETPLACE/product-detail/{shop_domain}/{product_key}/"
     // ProductEditActivity
