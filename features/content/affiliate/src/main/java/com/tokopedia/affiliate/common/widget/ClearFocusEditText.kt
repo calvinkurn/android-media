590eeed888aab60ccfdb5a283c857f9aa8931946
nakama.hs <hadi.saputro@tokopedia.com>
2019-07-04 14:53:17 +0700

    captions #424

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ClearFocusEditText.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/common/widget/ClearFocusEditText.kt
@@ -10,7 +10,7 @@ class ClearFocusEditText @JvmOverloads constructor(
 ) : EditTextCompat(context, attrs, defStyleAttr){
 
     override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK || event?.action == KeyEvent.ACTION_UP)
+        if (keyCode == KeyEvent.KEYCODE_BACK )
             clearFocus()
         return super.onKeyPreIme(keyCode, event)
     }
