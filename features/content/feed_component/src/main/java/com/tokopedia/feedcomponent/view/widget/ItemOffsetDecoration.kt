ac5404c27f7b4fbe5e4cd674880d0460f69a8e89
nakama.hs <hadi.saputro@tokopedia.com>
2019-07-05 18:24:21 +0700

    from TE Bug List 05/07

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/ItemOffsetDecoration.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/ItemOffsetDecoration.kt
@@ -9,7 +9,7 @@ import android.view.View
  **/
 class ItemOffsetDecoration(val space: Int) : RecyclerView.ItemDecoration(){
 
-    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
         outRect?.set(space, space, space, space)
     }
