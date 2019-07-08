3a4beaeaf06695f235764602231a5beaeca9d058
yogieputra8 <yogie.putra8@gmail.com>
2019-07-08 11:08:47 +0700

    Test inflate layout as a background in OS fragment

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/fragment/ReactNativeOfficialStoreFragment.java
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/fragment/ReactNativeOfficialStoreFragment.java
@@ -38,8 +38,12 @@ public class ReactNativeOfficialStoreFragment extends ReactNativeFragment
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ReactUtils.startTracing(MP_OFFICIAL_STORE); // start trace when view created
         super.onCreateView(inflater, container, savedInstanceState);
-        if (getActivity() != null) // set background color of react root view
-            reactRootView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
+        if (getActivity() != null) {
+            reactRootView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));	            // set background color of react root view
+            View view = inflater.inflate(R.layout.fragment_official_store, container, false);
+            return view;
+        }
+        reactRootView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.white));
         return reactRootView;
     }
 
