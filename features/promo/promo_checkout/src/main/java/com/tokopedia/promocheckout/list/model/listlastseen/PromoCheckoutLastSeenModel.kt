13fe10b68a5c379c18a36967b03add13b5375858
Resa Kemal Saharso <resa.saharso@tokopedia.com>
2019-07-11 17:12:14 +0700

    Add promo last seen API & data model

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/model/listlastseen/PromoCheckoutLastSeenModel.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/model/listlastseen/PromoCheckoutLastSeenModel.kt
@@ -1,4 +1,32 @@
 package com.tokopedia.promocheckout.list.model.listlastseen
 
-class PromoCheckoutLastSeenModel {
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by resakemal on 11/07/19.
+ */
+class PromoCheckoutLastSeenModel(
+        @SerializedName("id")
+        @Expose
+        val id: Int,
+        @SerializedName("img_url")
+        @Expose
+        val urlBannerPromo: String,
+        @SerializedName("title")
+        @Expose
+        val title: String,
+        @SerializedName("subtitle")
+        @Expose
+        val subtitle: String,
+        @SerializedName("promo_code")
+        @Expose
+        val promoCode: String,
+        var voucherCodeCopied: Boolean = false
+) {
+        class Response(
+                @SerializedName("promoModels")
+                @Expose
+                var promoModels: List<PromoCheckoutLastSeenModel> = listOf()
+        )
 }
--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
@@ -3,13 +3,16 @@ package com.tokopedia.promocheckout.list.view.adapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.tokopedia.promocheckout.list.model.listlastseen.PromoCheckoutLastSeenModel
+import kotlinx.android.synthetic.main.item_promo_last_seen.view.*
 
 class PromoLastSeenViewHolder(val view : View, val listenerLastSeen: ListenerLastSeen) : RecyclerView.ViewHolder(view) {
     fun bind(data: PromoCheckoutLastSeenModel) {
         view.setOnClickListener { listenerLastSeen.onClickItemLastSeen(data) }
+        view.voucherCode.text = data.promoCode
+        view.descPromo.text = data.subtitle.toUpperCase()
     }
 
     interface ListenerLastSeen{
-        fun onClickItemLastSeen(promoCheckoutLastSeenModel: PromoCheckoutLastSeenModel)
+        fun onClickItemLastSeen(promoCheckoutLastSeenModelModel: PromoCheckoutLastSeenModel)
     }
 }
