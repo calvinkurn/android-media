2254256302c3950dbf5a00d869392c17e65e3c56
Nisie <nathania.sutedja@tokopedia.com>
2019-07-15 13:40:35 +0700

    Fix dagger implementation

--- features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/di/ImageUploadModule.kt
+++ features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/di/ImageUploadModule.kt
@@ -3,21 +3,16 @@ package com.tokopedia.profilecompletion.di
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.tokopedia.abstraction.AbstractionRouter
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.network.OkHttpRetryPolicy
 import com.tokopedia.abstraction.common.network.interceptor.ErrorResponseInterceptor
 import com.tokopedia.config.GlobalConfig
-import com.tokopedia.imageuploader.ImageUploaderRouter
 import com.tokopedia.imageuploader.data.*
 import com.tokopedia.imageuploader.data.entity.ImageUploaderResponseError
 import com.tokopedia.imageuploader.data.source.GenerateHostCloud
 import com.tokopedia.imageuploader.data.source.GenerateHostDataSource
 import com.tokopedia.imageuploader.data.source.UploadImageDataSourceCloud
 import com.tokopedia.imageuploader.data.source.api.GenerateHostApi
-import com.tokopedia.imageuploader.di.qualifier.ImageUploaderAuthInterceptorQualifier
-import com.tokopedia.imageuploader.di.qualifier.ImageUploaderChuckQualifier
-import com.tokopedia.imageuploader.di.qualifier.ImageUploaderQualifier
 import com.tokopedia.imageuploader.domain.GenerateHostRepository
 import com.tokopedia.imageuploader.domain.UploadImageRepository
 import com.tokopedia.imageuploader.domain.UploadImageUseCase
@@ -29,16 +24,14 @@ import com.tokopedia.profilecompletion.data.UploadProfileImageModel
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
 import java.util.concurrent.TimeUnit
 
+@ProfileCompletionScope
 @Module
 class ImageUploadModule {
     private val GSON_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
@@ -48,33 +41,33 @@ class ImageUploadModule {
     private val NET_RETRY = 1
 
     @Provides
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     fun provideNetworkRouter(@ApplicationContext context: Context): NetworkRouter {
         return context as NetworkRouter
     }
 
     @Provides
-    @ImageUploaderQualifier
-    fun provideFingerprintInterceptor(@ImageUploaderQualifier networkRouter: NetworkRouter,
+    @ProfileCompletionQualifier
+    fun provideFingerprintInterceptor(@ProfileCompletionQualifier networkRouter: NetworkRouter,
                                       userSessionInterface: UserSessionInterface): FingerprintInterceptor {
         return FingerprintInterceptor(networkRouter, userSessionInterface)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
     fun okHttpRetryPolicy(): OkHttpRetryPolicy {
         return OkHttpRetryPolicy(NET_READ_TIMEOUT, NET_WRITE_TIMEOUT, NET_CONNECT_TIMEOUT, NET_RETRY)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
     fun provideTkpdAuthInterceptor(@ApplicationContext context: Context,
-                                   @ImageUploaderQualifier networkRouter: NetworkRouter,
+                                   @ProfileCompletionQualifier networkRouter: NetworkRouter,
                                    userSessionInterface: UserSessionInterface): TkpdAuthInterceptor {
         return TkpdAuthInterceptor(context, networkRouter, userSessionInterface)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
     fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
         val logging = HttpLoggingInterceptor()
@@ -86,19 +79,19 @@ class ImageUploadModule {
         return logging
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
     fun provideErrorResponseInterceptor(): ErrorResponseInterceptor {
         return ErrorResponseInterceptor(ImageUploaderResponseError::class.java)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
-    fun provideOkHttpClient(@ImageUploaderQualifier tkpdAuthInterceptor: TkpdAuthInterceptor,
-                            @ImageUploaderQualifier fingerprintInterceptor: FingerprintInterceptor,
-                            @ImageUploaderQualifier retryPolicy: OkHttpRetryPolicy,
-                            @ImageUploaderQualifier loggingInterceptor: HttpLoggingInterceptor,
-                            @ImageUploaderQualifier errorHandlerInterceptor: ErrorResponseInterceptor): OkHttpClient {
+    fun provideOkHttpClient(@ProfileCompletionQualifier tkpdAuthInterceptor: TkpdAuthInterceptor,
+                            @ProfileCompletionQualifier fingerprintInterceptor: FingerprintInterceptor,
+                            @ProfileCompletionQualifier retryPolicy: OkHttpRetryPolicy,
+                            @ProfileCompletionQualifier loggingInterceptor: HttpLoggingInterceptor,
+                            @ProfileCompletionQualifier errorHandlerInterceptor: ErrorResponseInterceptor): OkHttpClient {
         val builder = OkHttpClient.Builder()
         builder.addInterceptor(tkpdAuthInterceptor)
         builder.addInterceptor(fingerprintInterceptor)
@@ -113,16 +106,16 @@ class ImageUploadModule {
         return builder.build()
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
-    fun provideRetrofitBuilder(@ImageUploaderQualifier gson: Gson): Retrofit.Builder {
+    fun provideRetrofitBuilder(@ProfileCompletionQualifier gson: Gson): Retrofit.Builder {
         return Retrofit.Builder()
                 .addConverterFactory(StringResponseConverter())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
     fun provideGson(): Gson {
         return GsonBuilder()
@@ -133,69 +126,70 @@ class ImageUploadModule {
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideUploadImageDataSourceCloud(@ImageUploaderQualifier retrofit: Retrofit.Builder, @ImageUploaderQualifier okHttpClient: OkHttpClient): UploadImageDataSourceCloud {
+    @ProfileCompletionQualifier
+    fun provideUploadImageDataSourceCloud(@ProfileCompletionQualifier retrofit: Retrofit.Builder, @ProfileCompletionQualifier okHttpClient: OkHttpClient): UploadImageDataSourceCloud {
         return UploadImageDataSourceCloud(retrofit, okHttpClient)
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideUploadImageDataSource(@ImageUploaderQualifier uploadImageDataSourceCloud: UploadImageDataSourceCloud): UploadImageDataSource {
+    @ProfileCompletionQualifier
+    fun provideUploadImageDataSource(@ProfileCompletionQualifier uploadImageDataSourceCloud: UploadImageDataSourceCloud): UploadImageDataSource {
         return UploadImageDataSource(uploadImageDataSourceCloud)
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideUploadImageRepository(@ImageUploaderQualifier uploadImageDataSourceCloud: UploadImageDataSource): UploadImageRepository {
+    @ProfileCompletionQualifier
+    fun provideUploadImageRepository(@ProfileCompletionQualifier uploadImageDataSourceCloud: UploadImageDataSource): UploadImageRepository {
         return UploadImageRepositoryImpl(uploadImageDataSourceCloud)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
-    fun provideWsV4RetrofitWithErrorHandler(@ImageUploaderQualifier okHttpClient: OkHttpClient,
-                                            @ImageUploaderQualifier retrofitBuilder: Retrofit.Builder): Retrofit {
+    fun provideWsV4RetrofitWithErrorHandler(@ProfileCompletionQualifier okHttpClient: OkHttpClient,
+                                            @ProfileCompletionQualifier retrofitBuilder: Retrofit.Builder): Retrofit {
         return retrofitBuilder.baseUrl(ImageUploaderUrl.BASE_URL).client(okHttpClient).build()
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideGenerateHostApi(@ImageUploaderQualifier retrofit: Retrofit): GenerateHostApi {
+    @ProfileCompletionQualifier
+    fun provideGenerateHostApi(@ProfileCompletionQualifier retrofit: Retrofit): GenerateHostApi {
         return retrofit.create(GenerateHostApi::class.java)
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideGenerateHostCloud(@ImageUploaderQualifier generateHostApi: GenerateHostApi,
+    @ProfileCompletionQualifier
+    fun provideGenerateHostCloud(@ProfileCompletionQualifier generateHostApi: GenerateHostApi,
                                           userSession: UserSessionInterface): GenerateHostCloud {
         return GenerateHostCloud(generateHostApi, userSession)
     }
 
-    @ImageUploaderQualifier
+    @ProfileCompletionQualifier
     @Provides
-    internal fun provideGenerateHostDataSource(@ImageUploaderQualifier generateHostCloud: GenerateHostCloud): GenerateHostDataSource {
+    fun provideGenerateHostDataSource(@ProfileCompletionQualifier generateHostCloud: GenerateHostCloud): GenerateHostDataSource {
         return GenerateHostDataSource(generateHostCloud)
     }
 
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideGenerateHostRepository(@ImageUploaderQualifier generateHostDataSource: GenerateHostDataSource): GenerateHostRepository {
+    @ProfileCompletionQualifier
+    fun provideGenerateHostRepository(@ProfileCompletionQualifier generateHostDataSource: GenerateHostDataSource): GenerateHostRepository {
         return GenerateHostRepositoryImpl(generateHostDataSource)
     }
 
     @Provides
-    @ImageUploaderQualifier
-    internal fun provideImageUploaderUtils( userSession: UserSessionInterface): ImageUploaderUtils {
+    @ProfileCompletionQualifier
+    fun provideImageUploaderUtils( userSession: UserSessionInterface): ImageUploaderUtils {
         return ImageUploaderUtils(userSession)
     }
 
+    @ProfileCompletionQualifier
     @Provides
     fun provideUploadImageUseCase(
-            @ImageUploaderQualifier uploadImageRepository: UploadImageRepository,
-            @ImageUploaderQualifier generateHostRepository: GenerateHostRepository,
-            @ImageUploaderQualifier gson: Gson,
+            @ProfileCompletionQualifier uploadImageRepository: UploadImageRepository,
+            @ProfileCompletionQualifier generateHostRepository: GenerateHostRepository,
+            @ProfileCompletionQualifier gson: Gson,
             userSession: UserSessionInterface,
-            @ImageUploaderQualifier imageUploaderUtils: ImageUploaderUtils): UploadImageUseCase<UploadProfileImageModel> {
+            @ProfileCompletionQualifier imageUploaderUtils: ImageUploaderUtils): UploadImageUseCase<UploadProfileImageModel> {
         return UploadImageUseCase(uploadImageRepository, generateHostRepository, gson, userSession, UploadProfileImageModel::class.java, imageUploaderUtils)
     }
 }
