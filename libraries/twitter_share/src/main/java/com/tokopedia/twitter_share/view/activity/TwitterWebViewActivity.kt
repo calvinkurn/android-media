eb2d4a2dfdfc6d32e297a225acc82e8f93e2cccc
nakama <kenny.hadisaputra@tokopedia.com>
2019-07-15 15:29:12 +0700

    - add "Log in With Twitter" title to TwitterWebViewActivity

--- libraries/twitter_share/src/main/java/com/tokopedia/twitter_share/view/activity/TwitterWebViewActivity.kt
+++ libraries/twitter_share/src/main/java/com/tokopedia/twitter_share/view/activity/TwitterWebViewActivity.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.twitter_share.view.activity
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.twitter_share.R
 import com.tokopedia.twitter_share.TwitterAuthenticator
 import com.tokopedia.twitter_share.view.fragment.TwitterWebViewFragment
 
@@ -9,6 +11,13 @@ class TwitterWebViewActivity : BaseSimpleActivity(), TwitterWebViewActivityListe
 
     companion object {
         const val EXTRA_URL = "url"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        updateTitle(
+                getString(R.string.log_in_with_twitter)
+        )
     }
 
     override fun getNewFragment(): Fragment {
