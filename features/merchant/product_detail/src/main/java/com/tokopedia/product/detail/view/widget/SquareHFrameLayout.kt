98ca412fddabdf0f85846ac96b0cbda889df04ef
hansenpw <hansen.wijaya@tokopedia.com>
2019-07-18 17:39:31 +0700

    fix measure spec validation

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/widget/SquareHFrameLayout.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/widget/SquareHFrameLayout.kt
@@ -3,7 +3,6 @@ package com.tokopedia.product.detail.view.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import kotlin.math.min
 
 class SquareHFrameLayout : FrameLayout {
 
@@ -14,8 +13,11 @@ class SquareHFrameLayout : FrameLayout {
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {}
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minimumMeasureSpec = min(widthMeasureSpec, heightMeasureSpec)
-        super.onMeasure(minimumMeasureSpec, minimumMeasureSpec)
+        if (MeasureSpec.getSize(widthMeasureSpec) < MeasureSpec.getSize(heightMeasureSpec)) {
+            super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        } else {
+            super.onMeasure(heightMeasureSpec, heightMeasureSpec)
+        }
     }
 
 }
