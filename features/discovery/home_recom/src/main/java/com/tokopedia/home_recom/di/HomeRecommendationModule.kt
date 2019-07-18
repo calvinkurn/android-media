eb7846200dcc44d4801d190a4c6e6d4edf22dbff
lukaskris12 <lukaskris12@gmail.com>
2019-07-18 19:24:41 +0700

    temp

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/di/HomeRecommendationModule.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/di/HomeRecommendationModule.kt
@@ -8,7 +8,6 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.home_recom.R
 import com.tokopedia.recommendation_widget_common.domain.GetRecommendationUseCase
-import com.tokopedia.topads.sdk.di.TopAdsWishlistModule
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.wishlist.common.usecase.AddWishListUseCase
