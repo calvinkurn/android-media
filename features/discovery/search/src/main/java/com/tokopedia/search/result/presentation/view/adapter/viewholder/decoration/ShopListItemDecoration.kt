dbd22126b8c6527a0cb0fc9cdd299a2661fb2297
darianthedytoped <darian.thedy@tokopedia.com>
2019-07-18 15:41:38 +0700

    Fix item decoration

---
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/adapter/viewholder/decoration/ShopListItemDecoration.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.search.result.presentation.view.adapter.viewholder.decoration
+
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class ShopListItemDecoration(private val left: Int,
+                             private val right: Int,
+                             private val bottom: Int,
+                             private val top: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View,
+                                parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = left
+        outRect.right = right
+        outRect.bottom = bottom / 2
+        outRect.top = if (isTopProductItem(parent, view)) top / 2 else top
+    }
+
+    private fun isTopProductItem(parent: RecyclerView, view: View): Boolean {
+        return parent.getChildAdapterPosition(view) == 0
+    }
+}
