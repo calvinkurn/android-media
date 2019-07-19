dcc626e0e49c4e84ce381ec2dfe1e7d1e9e38d47
okasurya <brilliantsurya@gmail.com>
2019-07-19 13:38:28 +0700

    remove "/" in front of chat url path

--- features/user/chat_common/src/main/java/com/tokopedia/chat_common/network/ChatUrl.kt
+++ features/user/chat_common/src/main/java/com/tokopedia/chat_common/network/ChatUrl.kt
@@ -10,12 +10,12 @@ class ChatUrl {
 
     companion object {
 
-        var TOPCHAT = TokopediaUrl.getInstance().CHAT;
+        var TOPCHAT = TokopediaUrl.getInstance().CHAT
         var CHAT_WEBSOCKET_DOMAIN = TokopediaUrl.getInstance().WS_CHAT
 
-        const val CONNECT_WEBSOCKET = "/connect"
-        const val REPLY = "/tc/v1/reply"
-        const val DELETE = "/tc/v1/delete"
+        const val CONNECT_WEBSOCKET = "connect"
+        const val REPLY = "tc/v1/reply"
+        const val DELETE = "tc/v1/delete"
         const val GET_TEMPLATE = "tc/v1/templates"
     }
 }
