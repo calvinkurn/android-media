180e162c8807aa044dd237456bff8b6f288915ae
okasurya <brilliantsurya@gmail.com>
2019-07-19 13:02:27 +0700

    remove hardcoded url string in core_network

--- libraries/core_network/src/main/java/com/tokopedia/core/network/constants/TkpdBaseURL.java
+++ libraries/core_network/src/main/java/com/tokopedia/core/network/constants/TkpdBaseURL.java
@@ -27,7 +27,7 @@ public class TkpdBaseURL {
     public static String WEB_DOMAIN = TokopediaUrl.Companion.getInstance().getWEB();
     public static String MOBILE_DOMAIN = TokopediaUrl.Companion.getInstance().getMOBILEWEB();
     public static String BASE_CONTACT_US = WEB_DOMAIN + "contact-us";
-    public static String TOKOPEDIA_CART_DOMAIN = "https://fs.tokopedia.net/tkpdcart/";
+    public static String TOKOPEDIA_CART_DOMAIN = TokopediaUrl.getInstance().getFS() + "tkpdcart/";
     public static String BASE_ACTION = BASE_DOMAIN + "v4/action/";
     public static String DIGITAL_API_DOMAIN = TokopediaUrl.Companion.getInstance().getPULSA_API();
     public static String DIGITAL_WEBSITE_DOMAIN = TokopediaUrl.Companion.getInstance().getPULSA();
