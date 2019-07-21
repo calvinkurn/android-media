916af4cf8bd1cd749825a63b68286744e418860a
ignindra <ignindrakw@gmail.com>
2019-07-21 23:23:49 +0700

    [AN-1001] - ignindra - testing new dialog phase 8

--- features/common/nps/src/main/java/com/tokopedia/nps/presentation/view/dialog/AppFeedbackRatingBottomSheet.java
+++ features/common/nps/src/main/java/com/tokopedia/nps/presentation/view/dialog/AppFeedbackRatingBottomSheet.java
@@ -14,7 +14,7 @@ public class AppFeedbackRatingBottomSheet extends BottomSheets {
 
     @Override
     protected BottomSheetsState state() {
-        return BottomSheetsState.NORMAL;
+        return BottomSheetsState.FULL;
     }
 
     @Override
