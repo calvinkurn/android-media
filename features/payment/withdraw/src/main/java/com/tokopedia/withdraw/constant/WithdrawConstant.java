ca741f0c5cdae72a80de685044215a4fd047db5e
sachin <sachin.bansal@tokopedia.com>
2019-07-26 17:26:06 +0530

    domain url update

--- features/payment/withdraw/src/main/java/com/tokopedia/withdraw/constant/WithdrawConstant.java
+++ features/payment/withdraw/src/main/java/com/tokopedia/withdraw/constant/WithdrawConstant.java
@@ -1,6 +1,9 @@
 package com.tokopedia.withdraw.constant;
 
+import com.tokopedia.url.TokopediaUrl;
+
 public class WithdrawConstant {
-    public static final String SALDOLOCK_PAYNOW_URL="https://www.tokopedia.com/fm/modal-toko/dashboard/pembayaran";
-    public static final String SALDOLOCK_INFO="https://www.tokopedia.com/help/article/a-1815";
+    private static final String WEB_DOMAIN_URL = TokopediaUrl.getInstance().getWEB();
+    public static final String SALDOLOCK_PAYNOW_URL = WEB_DOMAIN_URL + "fm/modal-toko/dashboard/pembayaran";
+    public static final String SALDOLOCK_INFO = WEB_DOMAIN_URL + "help/article/a-1815";
 }
