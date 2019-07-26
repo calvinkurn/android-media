51c2ce6e5f88684ee15c0ec30f87d764151c6860
darianthedytoped <darian.thedy@tokopedia.com>
2019-07-26 11:44:04 +0700

    Revert search activity changes

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
@@ -2,10 +2,8 @@ package com.tokopedia.search.result.presentation.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
