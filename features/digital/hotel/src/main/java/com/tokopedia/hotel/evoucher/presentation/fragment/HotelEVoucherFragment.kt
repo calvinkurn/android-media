dc1c93327175324f029078ac157d3462f1d12d2d
Jessica Sean <jessica.sean@tokopedia.com>
2019-07-26 20:49:20 +0700

    save image fix

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/evoucher/presentation/fragment/HotelEVoucherFragment.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/evoucher/presentation/fragment/HotelEVoucherFragment.kt
@@ -142,7 +142,6 @@ class HotelEVoucherFragment : HotelBaseFragment(), HotelSharePdfBottomSheets.Sha
             val currentTime = TravelDateUtil.dateToString(TravelDateUtil.YYYY_MM_DD_T_HH_MM_SS_Z, TravelDateUtil.getCurrentCalendar().time)
             val filename = getString(R.string.hotel_share_file_name, currentTime)
             val file = File(myDir, filename)
-            if (file.exists()) file.delete()
             try {
                 val out = FileOutputStream(file)
                 bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
