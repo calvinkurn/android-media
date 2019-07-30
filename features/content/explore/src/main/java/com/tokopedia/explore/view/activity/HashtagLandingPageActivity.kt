8ac4ff613e8817f484960ca06ec5ada3c99c42ef
milhamj <ilham.jamaludin@tokopedia.com>
2019-07-30 10:32:55 +0700

    Add hashtag applink :sparkles:

--- features/content/explore/src/main/java/com/tokopedia/explore/view/activity/HashtagLandingPageActivity.kt
+++ features/content/explore/src/main/java/com/tokopedia/explore/view/activity/HashtagLandingPageActivity.kt
@@ -2,10 +2,13 @@ package com.tokopedia.explore.view.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import com.airbnb.deeplinkdispatch.DeepLink
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.constant.DeeplinkConstant
 import com.tokopedia.explore.di.DaggerExploreComponent
 import com.tokopedia.explore.di.ExploreComponent
@@ -13,12 +16,6 @@ import com.tokopedia.explore.view.fragment.HashtagLandingPageFragment
 import java.net.URLDecoder
 
 class HashtagLandingPageActivity : BaseSimpleActivity(), HasComponent<ExploreComponent> {
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        this.intent = intent
-        inflateFragment()
-    }
 
     override fun getNewFragment(): Fragment {
         val uri = intent.data
@@ -34,9 +31,18 @@ class HashtagLandingPageActivity : BaseSimpleActivity(), HasComponent<ExploreCom
             .baseAppComponent((application as BaseMainApplication).baseAppComponent).build()
 
     companion object {
+        const val EXTRA_PARAM_HASHTAG = "hashtag"
 
         @JvmStatic
         fun createIntent(context: Context, hastag: String) = Intent(context, HashtagLandingPageActivity::class.java)
                 .putExtra(HashtagLandingPageFragment.ARG_HASHTAG, hastag)
+    }
+
+    object DeeplinkIntent {
+        @DeepLink(ApplinkConst.FEED_HASHTAG)
+        @JvmStatic
+        fun createIntent(context: Context, extras: Bundle): Intent {
+            return createIntent(context, extras.getString(EXTRA_PARAM_HASHTAG, ""))
+        }
     }
 }
