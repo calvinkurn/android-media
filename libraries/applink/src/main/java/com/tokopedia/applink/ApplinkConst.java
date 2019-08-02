fc4ab6e199876fcde846d919775e90769eb7c54d
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-08-02 13:53:34 +0700

    Refactor common constant key to ApplinkConst

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -329,4 +329,20 @@ public interface ApplinkConst {
     interface Profile {
         String PARAM_USER_ID = "{user_id}";
     }
+
+    interface Transaction {
+        String EXTRA_SHOP_ID = "shop_id";
+        String EXTRA_PRODUCT_ID = "product_id";
+        String EXTRA_NOTES = "notes";
+        String EXTRA_QUANTITY = "quantity";
+        String EXTRA_SELECTED_VARIANT_ID = "selected_variant_id";
+        String EXTRA_ACTION = "action";
+        String EXTRA_PRODUCT_IMAGE = "product_image";
+        String EXTRA_SHOP_TYPE = "shop_type";
+        String EXTRA_SHOP_NAME = "shop_name";
+        String EXTRA_OCS = "ocs";
+        String EXTRA_TRADE_IN_PARAMS = "trade_in_params";
+        String TRACKER_ATTRIBUTION = "tracker_attribution";
+        String TRACKER_LIST_NAME = "tracker_list_name";
+    }
 }
