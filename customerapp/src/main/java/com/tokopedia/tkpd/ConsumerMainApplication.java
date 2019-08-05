133396b3513c36968f21808dc96713ac9cf478c5
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-08-05 16:01:12 +0700

    logentries, remove insight ops and use direct connection for logging

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -148,6 +148,7 @@ public class ConsumerMainApplication extends ConsumerRouterApplication implement
         }
         registerActivityLifecycleCallbacks(callback);
 
+        LogWrapper.init(this);
         TimberWrapper.init(this);
     }
 
