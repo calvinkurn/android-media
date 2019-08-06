6727b1ade57cfa934effac2bbd98760fd8e71013
jegul <kenny.hadisaputra@tokopedia.com>
2019-08-06 14:51:50 +0700

    - now MentionSpan extends from CLickableSpan instead of ReplacementSpan

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/span/MentionSpan.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/span/MentionSpan.kt
@@ -1,10 +1,10 @@
 package com.tokopedia.feedcomponent.view.span
 
-import android.graphics.Canvas
-import android.graphics.Paint
 import android.graphics.Typeface
 import android.support.annotation.ColorInt
-import android.text.style.ReplacementSpan
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.view.View
 
 /**
  * Created by jegul on 2019-08-05.
@@ -15,34 +15,18 @@ class MentionSpan(
         val fullText: String,
         val userId: String,
         val fullName: String
-) : ReplacementSpan() {
+) : ClickableSpan() {
 
     val length = fullName.length
 
-    override fun getSize(paint: Paint, text: CharSequence?, start: Int, end: Int, p4: Paint.FontMetricsInt?): Int {
-        return Math.round(measureText(paint, text, start, end))
+    override fun onClick(p0: View) {
+
     }
 
-    override fun draw(
-            canvas: Canvas,
-            text: CharSequence?,
-            start: Int,
-            end: Int,
-            x: Float,
-            top: Int,
-            y: Int,
-            bottom: Int,
-            paint: Paint
-    ) {
-        text?.let {
-            paint.color = color
-            paint.typeface = Typeface.DEFAULT_BOLD
-            canvas.drawText(it, start, end, x, y.toFloat(), paint)
-        }
-    }
-
-    private fun measureText(paint: Paint, text: CharSequence?, start: Int, end: Int): Float {
-        paint.typeface = Typeface.DEFAULT_BOLD
-        return paint.measureText(text, start, end)
+    override fun updateDrawState(ds: TextPaint) {
+        super.updateDrawState(ds)
+        ds.color = color
+        ds.typeface = Typeface.DEFAULT_BOLD
+        ds.isUnderlineText = false
     }
 }
