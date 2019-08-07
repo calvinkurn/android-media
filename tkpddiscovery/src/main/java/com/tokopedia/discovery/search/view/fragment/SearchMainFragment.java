5714345983780f14ddc3084718d6b0006c2e2c58
darianthedytoped <darian.thedy@tokopedia.com>
2019-08-07 07:55:19 +0700

    RouteManager getIntent instead of RouteManager route

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/search/view/fragment/SearchMainFragment.java
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/search/view/fragment/SearchMainFragment.java
@@ -1,5 +1,6 @@
 package com.tokopedia.discovery.search.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.tokopedia.abstraction.common.utils.snackbar.SnackbarManager;
 import com.tokopedia.analytics.performance.PerformanceMonitoring;
+import com.tokopedia.applink.RouteManager;
 import com.tokopedia.core.app.MainApplication;
 import com.tokopedia.core.app.TkpdBaseV4Fragment;
 import com.tokopedia.discovery.R;
@@ -216,7 +218,9 @@ public class SearchMainFragment extends TkpdBaseV4Fragment implements SearchCont
     private void startActivityFromAutoComplete(String applink) {
         if(getActivity() == null) return;
 
-//        RouteManager.route(getActivity(), applink);
+        Intent intent = RouteManager.getIntent(getActivity(), applink);
+
+        getActivity().startActivity(intent);
         getActivity().setResult(SearchConstant.AUTO_COMPLETE_ACTIVITY_RESULT_CODE_FINISH_ACTIVITY);
         getActivity().finish();
     }
