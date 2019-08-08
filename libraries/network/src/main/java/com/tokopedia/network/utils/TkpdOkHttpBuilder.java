013bfaecabdf0ccf4a6731ce66085eedaf8285ae
darianthedytoped <darian.thedy@tokopedia.com>
2019-08-08 16:50:26 +0700

    Remove android:contentInset-thing

--- libraries/network/src/main/java/com/tokopedia/network/utils/TkpdOkHttpBuilder.java
+++ libraries/network/src/main/java/com/tokopedia/network/utils/TkpdOkHttpBuilder.java
@@ -1,20 +1,29 @@
 package com.tokopedia.network.utils;
 
 import android.content.Context;
+import android.os.Build;
 
 import com.readystatesoftware.chuck.ChuckInterceptor;
 import com.tokopedia.config.GlobalConfig;
 import com.tokopedia.network.interceptor.TkpdBaseInterceptor;
 
+import java.security.KeyStore;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.CipherSuite;
 import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.TlsVersion;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
@@ -80,10 +89,44 @@ public class TkpdOkHttpBuilder {
         return new HttpLoggingInterceptor().setLevel(loggingLevel);
     }
 
+    public OkHttpClient.Builder enableTls12OnPreLollipop() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            try {
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init((KeyStore) null);
+                TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+                if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                    throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
+                }
+                X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+                SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, new TrustManager[] { trustManager }, null);
+
+                builder.sslSocketFactory(new TLSSocketFactory(), trustManager);
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                builder.connectionSpecs(specs);
+            } catch (Exception exc) {
+            }
+        }
+
+        return builder;
+    }
+
     public OkHttpClient build() {
         setOkHttpRetryPolicy();
         addDebugInterceptor();
         addLegacyChiper();
+        enableTls12OnPreLollipop();
+
         return builder.build();
     }
 }
