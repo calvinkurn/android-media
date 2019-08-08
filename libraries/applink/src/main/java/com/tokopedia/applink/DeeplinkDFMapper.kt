0c7950c9bf1cd5b7f73804a4ff3d7c7b7202630f
Nabilla <nabilla.sabbaha@tokopedia.com>
2019-08-08 11:57:04 +0700

    add mapping hotel in deeplink mapper

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -30,6 +30,7 @@ import com.crashlytics.android.Crashlytics;
 object DeeplinkDFMapper {
     // it should have the same name with the folder of dynamic feature
     private val MODULE_SHOP_SETTINGS_SELLERAPP = "shop_settings_sellerapp"
+    private val MODULE_HOTEL_TRAVEL = "hotel_travel"
 
     private var manager: SplitInstallManager? = null
 
@@ -52,8 +53,14 @@ object DeeplinkDFMapper {
                 else -> null
             }
         } else {
-            // currently cust app has no dynamic features module
-            return null
+            return when {
+                deeplink.startsWith(ApplinkConst.HOTEL) -> {
+                    getDFDeeplinkIfNotInstalled(context,
+                            deeplink, MODULE_HOTEL_TRAVEL,
+                            context.getString(R.string.title_hotel))
+                }
+                else -> null
+            }
         }
     }
 
