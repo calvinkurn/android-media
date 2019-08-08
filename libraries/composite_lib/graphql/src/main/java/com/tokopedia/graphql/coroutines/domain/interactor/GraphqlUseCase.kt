ab50c2d800ebddb437595c2433874e54f9c821e7
M normansyah <noiz354@gmail.com>
2019-08-08 11:53:42 +0700

    remove unused

--- libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/GraphqlUseCase.kt
+++ libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/GraphqlUseCase.kt
@@ -57,6 +57,18 @@ open class GraphqlUseCase<T: Any>(private val graphqlRepository: GraphqlReposito
 
     }
 
+
+    /**
+     * @author by milhamj on 2019-08-06.
+     */
+    fun Observable<*>.subscribeIgnoreError() {
+        this.subscribe({
+            //no op
+        }, {
+            //ignore error
+        })
+    }
+
     private fun initCacheManager() {
         if (mCacheManager == null) {
             mCacheManager = GraphqlCacheManager()
