801bd104e374c7ffe686d924d8adfcb312440be8
darianthedytoped <darian.thedy@tokopedia.com>
2019-08-08 07:33:15 +0700

    Undo accidental changes

--- features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/view/fragment/BroadcastMessagePreviewFragment.kt
+++ features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/view/fragment/BroadcastMessagePreviewFragment.kt
@@ -8,11 +8,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
 import android.widget.TextView
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.abstraction.common.utils.network.ErrorHandler
-import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.broadcast.message.R
 import com.tokopedia.broadcast.message.common.BroadcastMessageRouter
 import com.tokopedia.broadcast.message.common.constant.BroadcastMessageConstant
@@ -31,6 +31,7 @@ import kotlinx.android.synthetic.main.fragment_broadcast_message_preview.*
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 
 class BroadcastMessagePreviewFragment: BaseDaggerFragment(), BroadcastMessagePreviewView {
     @Inject
