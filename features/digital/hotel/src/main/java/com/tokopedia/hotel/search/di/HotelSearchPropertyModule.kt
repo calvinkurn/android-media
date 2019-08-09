92ade57ff78f9ea6c841ca93d0e6ff5840e14de8
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-08-09 14:44:28 +0700

    change R

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/search/di/HotelSearchPropertyModule.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/search/di/HotelSearchPropertyModule.kt
@@ -3,7 +3,6 @@ package com.tokopedia.hotel.search.di
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
-import com.tokopedia.hotel.R
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
@@ -16,5 +15,5 @@ class HotelSearchPropertyModule{
     @HotelSearchPropertyScope
     @Named("search_query")
     fun provideSearchQuery(@ApplicationContext context: Context): String =
-            GraphqlHelper.loadRawString(context.resources, R.raw.gql_get_property_search)
+            GraphqlHelper.loadRawString(context.resources, com.tokopedia.hotel.R.raw.gql_get_property_search)
 }
