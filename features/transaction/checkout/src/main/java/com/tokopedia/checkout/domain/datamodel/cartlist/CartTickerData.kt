b191f45ef3458c6fd95632ad7ced699d098ee601
hansenpw <hansen.wijaya@tokopedia.com>
2019-08-14 15:37:05 +0700

    convert cart ticker data to kotlin

---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/datamodel/cartlist/CartTickerData.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.checkout.domain.datamodel.cartlist
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class CartTickerData : Parcelable {
+
+    var id: Int = 0
+    var message: String = ""
+    var page: String = ""
+
+    constructor() {}
+
+    constructor(id: Int, message: String, page: String) {
+        this.id = id
+        this.message = message
+        this.page = page
+    }
+
+    private constructor(parcel: Parcel) {
+        id = parcel.readInt()
+        message = parcel.readString() ?: ""
+        page = parcel.readString() ?: ""
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(parcel: Parcel, i: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(message)
+        parcel.writeString(page)
+    }
+
+    companion object {
+
+        @JvmField
+        val CREATOR: Parcelable.Creator<CartTickerData> = object : Parcelable.Creator<CartTickerData> {
+            override fun createFromParcel(parcel: Parcel): CartTickerData {
+                return CartTickerData(parcel)
+            }
+
+            override fun newArray(size: Int): Array<CartTickerData?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+}
