b4e2a85d60dba4e465f80407a921b7ea55e7e3ed
sachin <sachin.bansal@tokopedia.com>
2019-08-14 19:05:09 +0530

    imports optimized

--- features/transaction/express_checkout/src/main/java/com/tokopedia/normalcheckout/model/ProductInfoAndVariant.kt
+++ features/transaction/express_checkout/src/main/java/com/tokopedia/normalcheckout/model/ProductInfoAndVariant.kt
@@ -2,7 +2,6 @@ package com.tokopedia.normalcheckout.model
 
 import com.tokopedia.product.detail.common.data.model.product.ProductInfo
 import com.tokopedia.product.detail.common.data.model.variant.ProductVariant
-import com.tokopedia.transactiondata.insurance.entity.response.InsuranceRecommendationGqlResponse
 
 data class ProductInfoAndVariant(
         var productInfo: ProductInfo = ProductInfo(),
