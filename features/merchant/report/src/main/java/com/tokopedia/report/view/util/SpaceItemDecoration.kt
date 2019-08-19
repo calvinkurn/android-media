30b8df3d18d4a51a96e916ec882b9e30a46f58e2
darianthedytoped <darian.thedy@tokopedia.com>
2019-08-19 13:11:37 +0700

    Fix space item decoration

--- features/merchant/report/src/main/java/com/tokopedia/report/view/util/SpaceItemDecoration.kt
+++ features/merchant/report/src/main/java/com/tokopedia/report/view/util/SpaceItemDecoration.kt
@@ -7,12 +7,12 @@ import android.view.View
 
 class SpaceItemDecoration(private val spaceDepth: Int, private val orientation: Int): RecyclerView.ItemDecoration(){
 
-    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
-        if (parent?.getChildAdapterPosition(view) != (parent?.adapter?.itemCount ?: 0) - 1){
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        if (parent.getChildAdapterPosition(view) != (parent.adapter?.itemCount ?: 0) - 1){
             if (orientation == LinearLayoutManager.HORIZONTAL){
-                outRect?.right = spaceDepth
+                outRect.right = spaceDepth
             } else {
-                outRect?.bottom = spaceDepth
+                outRect.bottom = spaceDepth
             }
         }
     }
