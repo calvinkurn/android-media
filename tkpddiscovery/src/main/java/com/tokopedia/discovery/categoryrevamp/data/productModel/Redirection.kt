bf9e58b512515f409ec31bded3dc658b31217b58
abhishekjain2808 <abhishek.jain@tokopedia.com>
2019-08-20 10:59:29 +0530

    removed unneccessary changes

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/productModel/Redirection.kt
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/productModel/Redirection.kt
@@ -1,9 +1,7 @@
 package com.tokopedia.discovery.categoryrevamp.data.productModel
 
-import javax.annotation.Generated
 import com.google.gson.annotations.SerializedName
 
-@Generated("com.robohorse.robopojogenerator")
 data class Redirection(
 
 	@field:SerializedName("departmentID")
--- tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/subCategoryModel/CategoryDetailQuery.kt
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/subCategoryModel/CategoryDetailQuery.kt
@@ -1,9 +1,7 @@
 package com.tokopedia.discovery.categoryrevamp.data.subCategoryModel
 
-import javax.annotation.Generated
 import com.google.gson.annotations.SerializedName
 
-@Generated("com.robohorse.robopojogenerator")
 data class CategoryDetailQuery(
 
 	@field:SerializedName("data")
