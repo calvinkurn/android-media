26007a2cbea8aeaa0c5b6b3f776c3c796ad347ac
yoasfs <yoas.fs@gmail.com>
2019-08-21 16:08:18 +0700

    fixx

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/adapter/viewholder/EmptyCommissionViewHolder.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/adapter/viewholder/EmptyCommissionViewHolder.kt
@@ -3,9 +3,11 @@ package com.tokopedia.affiliate.feature.dashboard.view.adapter.viewholder
 import android.support.annotation.LayoutRes
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.affiliate.R
 import com.tokopedia.affiliate.feature.dashboard.view.viewmodel.CommissionDetailItemViewModel
 import com.tokopedia.affiliate.feature.dashboard.view.viewmodel.EmptyCommissionViewModel
+import kotlinx.android.synthetic.main.item_af_commission_empty.view.*
 
 /**
  * @author by yoasfs on 2019-08-15
@@ -18,6 +20,6 @@ class EmptyCommissionViewHolder (v: View) : AbstractViewHolder<EmptyCommissionVi
         val LAYOUT = R.layout.item_af_commission_empty
     }
     override fun bind(element: EmptyCommissionViewModel) {
-
+        ImageHandler.LoadImage(itemView.iv_empty, element.iconRes)
     }
 }
