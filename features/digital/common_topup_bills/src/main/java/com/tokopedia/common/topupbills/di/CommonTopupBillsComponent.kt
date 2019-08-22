556ba8b969f50a6217ab9de33eb0d51200329e04
Resa Kemal Saharso <resa.saharso@tokopedia.com>
2019-08-22 08:34:02 +0700

    Add BaseTopupBillsFragment & TopupBillsCheckoutWidget in common topup bills

--- features/digital/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/di/CommonTopupBillsComponent.kt
+++ features/digital/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/di/CommonTopupBillsComponent.kt
@@ -1,17 +1,21 @@
 package com.tokopedia.common.topupbills.di
 
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.abstraction.base.view.adapter.factory.AdapterTypeFactory
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
-import com.tokopedia.common.topupbills.view.fragment.TopupBillsCheckoutFragment
+import com.tokopedia.common.topupbills.view.fragment.BaseTopupBillsFragment
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Component
 
 /**
  * Created by resakemal on 12/08/19.
  */
 @CommonTopupBillsScope
-@Component(modules = arrayOf(CommonTopupBillsModule::class),
-        dependencies = arrayOf(BaseAppComponent::class))
+@Component(modules = [CommonTopupBillsModule::class], dependencies = [BaseAppComponent::class])
 interface CommonTopupBillsComponent {
 
-    fun inject(topupBillsCheckoutFragment: TopupBillsCheckoutFragment)
+    fun userSessionInterface(): UserSessionInterface
+
+    fun inject(baseTopupBillsFragment: BaseTopupBillsFragment<Visitable<*>, AdapterTypeFactory>)
 
 }
---
+++ features/digital/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/view/fragment/BaseTopupBillsFragment.kt
@@ -0,0 +1,69 @@
+package com.tokopedia.common.topupbills.view.fragment
+
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.abstraction.base.view.adapter.factory.AdapterTypeFactory
+import com.tokopedia.abstraction.base.view.fragment.BaseListFragment
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.applink.internal.ApplinkConsInternalDigital
+import com.tokopedia.common.topupbills.generateRechargeCheckoutToken
+import com.tokopedia.common_digital.cart.view.model.DigitalCheckoutPassData
+import com.tokopedia.common_digital.common.constant.DigitalExtraParam
+import com.tokopedia.user.session.UserSessionInterface
+import javax.inject.Inject
+
+/**
+ * Created by resakemal on 12/08/19.
+ */
+abstract class BaseTopupBillsFragment<T: Visitable<*>, F: AdapterTypeFactory>: BaseListFragment<T, F>()  {
+
+    lateinit var checkoutPassData: DigitalCheckoutPassData
+
+    @Inject
+    lateinit var userSession: UserSessionInterface
+
+    fun processToCart() {
+        if (userSession.isLoggedIn) {
+            navigateToCart()
+        } else {
+            navigateToLoginPage()
+        }
+    }
+
+    private fun navigateToCart() {
+        if (::checkoutPassData.isInitialized) {
+            checkoutPassData.idemPotencyKey = userSession.userId.generateRechargeCheckoutToken()
+            val intent = RouteManager.getIntent(activity, ApplinkConsInternalDigital.CART_DIGITAL)
+            intent.putExtra(DigitalExtraParam.EXTRA_PASS_DIGITAL_CART_DATA, checkoutPassData)
+            startActivityForResult(intent, REQUEST_CODE_CART_DIGITAL)
+        }
+    }
+
+    fun navigateToLoginPage() {
+        val intent = RouteManager.getIntent(activity, ApplinkConst.LOGIN)
+        startActivityForResult(intent, REQUEST_CODE_LOGIN)
+    }
+
+//    fun navigateToPromoPage() {
+//        val intent = RouteManager.getIntent(activity, ApplinkConstInternalPromo.PROMO_LIST_DIGITAL)
+//        intent.putExtra("EXTRA_COUPON_ACTIVE", cartDigitalInfoData.attributes!!.isCouponActive)
+//        intent.putExtra("EXTRA_PROMO_DIGITAL_MODEL", getPromoDigitalModel())
+//        startActivityForResult(intent, REQUEST_CODE_LIST_PROMO)
+//    }
+//
+//    private fun getPromoDigitalModel(): PromoDigitalModel {
+//        return PromoDigitalModel(
+//                Integer.parseInt(cartPassData.categoryId!!),
+//                getProductId(),
+//                cartPassData.clientNumber!!,
+//                cartDigitalInfoData.attributes!!.pricePlain
+//        )
+//    }
+
+    companion object {
+        const val REQUEST_CODE_LOGIN = 1010
+        const val REQUEST_CODE_CART_DIGITAL = 1090
+        const val REQUEST_CODE_LIST_PROMO = 232
+    }
+
+}
