86ff7cddc05880a210af1cfce158123762a4467a
Muhammad Furqan <nanjihusaha@gmail.com>
2019-08-23 16:32:40 +0700

    Travel Subhomepage Tracking

--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/analytics/TravelHomepageTrackingConstant.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/analytics/TravelHomepageTrackingConstant.kt
@@ -3,5 +3,32 @@ package com.tokopedia.travel.homepage.analytics
 /**
  * @author by furqan on 23/08/2019
  */
-class TravelHomepageTrackingConstant {
+class TravelHomepageTrackingCategoryConstant {
+    val TRAVEL_HOMEPAGE_CATEGORY = "digital - subhomepage - travel&entertainment"
+}
+
+class TravelHomepageTrackingActionConstant {
+    val BANNER_IMPRESSION = "banner impression"
+    val BANNER_CLICK = "click banner"
+    val BANNER_CLICK_ALL = "click view all on banner"
+    val DYNAMIC_ICON_CLICK = "dynamic icon click"
+    val ORDER_CLICK = "click on widget bookings"
+    val ORDER_CLICK_ALL = "click view all on widget bookings"
+    val RECENT_SEARCH_CLICK = "click on widget recent search"
+    val POPULAR_SEARCH_CLICK = "click on widget popular search"
+    val DEALS_CLICK = "click  on widget deals"
+    val DEALS_CLICK_ALL = "click  view all on widget deals"
+    val POPULAR_DESTINATION_CLICK = "click on dynamic banner"
+}
+
+class TravelHomepageTrackingLabelConstant {
+    val CLICK = "click"
+    val OTHERS = "%s - %s"
+}
+
+class TravelHomepageTrackingEventNameConstant {
+    val PROMO_VIEW = "promoView"
+    val PROMO_CLICK = "promoClick"
+    val CLICK_HOMEPAGE = "clickHomepage"
+    val PRODUCT_CLICK = "productClick"
 }
--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/analytics/TravelHomepageTrackingUtil.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/analytics/TravelHomepageTrackingUtil.kt
@@ -4,4 +4,7 @@ package com.tokopedia.travel.homepage.analytics
  * @author by furqan on 23/08/2019
  */
 class TravelHomepageTrackingUtil {
+    fun travelHomepageClickAllBanner() {
+
+    }
 }
