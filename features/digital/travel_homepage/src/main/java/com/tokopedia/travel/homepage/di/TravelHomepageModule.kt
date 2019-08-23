3b0b09a43b614e6118bdfd5b4999baa4597ec890
Muhammad Furqan <nanjihusaha@gmail.com>
2019-08-23 17:53:58 +0700

    add travelhomepagetrackingutil injection

--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/di/TravelHomepageModule.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/di/TravelHomepageModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.travel.homepage.analytics.TravelHomepageTrackingUtil
 import com.tokopedia.travel.homepage.usecase.GetEmptyViewModelsUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
@@ -35,5 +36,9 @@ class TravelHomepageModule {
     @TravelHomepageScope
     @Provides
     fun provideGetEmptyVMsUseCase(): GetEmptyViewModelsUseCase = GetEmptyViewModelsUseCase()
+
+    @TravelHomepageScope
+    @Provides
+    fun provideTravelHomepageTrackingUtil(): TravelHomepageTrackingUtil = TravelHomepageTrackingUtil()
 
 }
