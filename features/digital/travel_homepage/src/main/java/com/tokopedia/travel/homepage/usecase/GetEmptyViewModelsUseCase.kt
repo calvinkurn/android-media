45e7a62ee8b0770c737c49bcce0384959876cadf
Muhammad Furqan <nanjihusaha@gmail.com>
2019-08-23 18:01:35 +0700

    bugfix

--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/usecase/GetEmptyViewModelsUseCase.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/usecase/GetEmptyViewModelsUseCase.kt
@@ -1,6 +1,9 @@
 package com.tokopedia.travel.homepage.usecase
 
 import com.tokopedia.travel.homepage.data.*
+import com.tokopedia.travel.homepage.presentation.fragment.TravelHomepageFragment.Companion.TYPE_ORDER_LIST
+import com.tokopedia.travel.homepage.presentation.fragment.TravelHomepageFragment.Companion.TYPE_RECENT_SEARCH
+import com.tokopedia.travel.homepage.presentation.fragment.TravelHomepageFragment.Companion.TYPE_RECOMMENDATION
 
 /**
  * @author by jessica on 2019-08-19
@@ -11,9 +14,9 @@ class GetEmptyViewModelsUseCase {
     fun requestEmptyViewModels(): List<TravelHomepageItemModel> {
         return listOf(TravelHomepageBannerModel(),
                 TravelHomepageCategoryListModel(),
-                TravelHomepageSectionViewModel(type = TravelHomepageSectionViewModel.TYPE_ORDER_LIST),
-                TravelHomepageSectionViewModel(type = TravelHomepageSectionViewModel.TYPE_RECENT_SEARCH),
-                TravelHomepageSectionViewModel(type = TravelHomepageSectionViewModel.TYPE_RECOMMENDATION),
+                TravelHomepageSectionViewModel(type = TYPE_ORDER_LIST),
+                TravelHomepageSectionViewModel(type = TYPE_RECENT_SEARCH),
+                TravelHomepageSectionViewModel(type = TYPE_RECOMMENDATION),
                 TravelHomepageDestinationModel())
     }
 }
