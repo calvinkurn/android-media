6392652e7b9b66bb11f43642b64d8421d5debb1b
Muhammad Furqan <nanjihusaha@gmail.com>
2019-08-23 10:34:56 +0700

    `destinasi impian` height should follow from backend

--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/presentation/adapter/TravelHomepageDestinationAdapter.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/presentation/adapter/TravelHomepageDestinationAdapter.kt
@@ -39,7 +39,7 @@ class TravelHomepageDestinationAdapter(private var list: List<TravelHomepageDest
                 if (position == 0) {
                     layoutParams.height = resources.getDimensionPixelSize(R.dimen.dp_145)
                 } else {
-                    layoutParams.height = resources.getDimensionPixelSize(R.dimen.dp_180)
+                    layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
                 }
                 destination_container.layoutParams = layoutParams
 
