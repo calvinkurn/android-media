dae107ac96bebe2b90f9d14ca032ce978dd3d511
stevenfredian <steven.fredian@tokopedia.com>
2019-08-27 12:58:11 +0700

    change sticky component to recyclerview add atc button on sticky button

--- features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/domain/mapper/StickyComponentMapper.kt
+++ features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/domain/mapper/StickyComponentMapper.kt
@@ -4,6 +4,7 @@ import com.tokopedia.abstraction.common.network.exception.MessageErrorException
 import com.tokopedia.groupchat.chatroom.domain.pojo.StickyComponentData
 import com.tokopedia.groupchat.chatroom.domain.pojo.StickyComponentPojo
 import com.tokopedia.groupchat.room.view.viewmodel.pinned.StickyComponentViewModel
+import com.tokopedia.groupchat.room.view.viewmodel.pinned.StickyComponentsViewModel
 import com.tokopedia.network.data.model.response.DataResponse
 import retrofit2.Response
 import rx.functions.Func1
@@ -13,9 +14,9 @@ import javax.inject.Inject
  * @author by nisie on 22/02/19.
  */
 class StickyComponentMapper @Inject constructor() : Func1<Response<DataResponse<StickyComponentPojo>>,
-        StickyComponentViewModel> {
+        StickyComponentsViewModel> {
 
-    override fun call(response: Response<DataResponse<StickyComponentPojo>>): StickyComponentViewModel {
+    override fun call(response: Response<DataResponse<StickyComponentPojo>>): StickyComponentsViewModel {
         val body = response.body()
         if (body != null) {
             if ((body.header == null ||
@@ -31,8 +32,30 @@ class StickyComponentMapper @Inject constructor() : Func1<Response<DataResponse<
         }
     }
 
-    public fun mapToViewModel(pojo: StickyComponentPojo): StickyComponentViewModel {
-        return StickyComponentViewModel(
+    public fun mapToViewModel(pojo: StickyComponentPojo): StickyComponentsViewModel {
+//        return StickyComponentViewModel(
+//                pojo.stickyComponent.componentId,
+//                pojo.stickyComponent.componentType,
+//                pojo.stickyComponent.imageUrl,
+//                pojo.stickyComponent.primaryText,
+//                pojo.stickyComponent.secondaryText,
+//                pojo.stickyComponent.linkUrl,
+//                pojo.stickyComponent.stickyTime,
+//                pojo.stickyComponent.relatedButton
+//        )
+//        for(i in 0 .. pojo.stickyComponents.size) {
+//            val stickyComp = pojo.stickyComponents[i]
+//            StickyComponentViewModel(
+//                stickyComp.componentId,
+//                stickyComp.componentType,
+//                stickyComp.imageUrl,
+//                stickyComp.primaryText,
+//                stickyComp.secondaryText,
+//                stickyComp.linkUrl,
+//                stickyComp.stickyTime,
+//                stickyComp.relatedButton)
+//        }
+        val model = StickyComponentViewModel(
                 pojo.stickyComponent.componentId,
                 pojo.stickyComponent.componentType,
                 pojo.stickyComponent.imageUrl,
@@ -40,13 +63,26 @@ class StickyComponentMapper @Inject constructor() : Func1<Response<DataResponse<
                 pojo.stickyComponent.secondaryText,
                 pojo.stickyComponent.linkUrl,
                 pojo.stickyComponent.stickyTime,
-                pojo.stickyComponent.relatedButton
-        )
+                pojo.stickyComponent.relatedButton,
+                pojo.stickyComponent.attributeData.toString())
+
+        return StickyComponentsViewModel(listOf(model))
     }
 
 
-    public fun mapToViewModel(pojo: StickyComponentData): StickyComponentViewModel {
-        return StickyComponentViewModel(
+    public fun mapToViewModel(pojo: StickyComponentData): StickyComponentsViewModel {
+//        return StickyComponentViewModel(
+//                pojo.componentId,
+//                pojo.componentType,
+//                pojo.imageUrl,
+//                pojo.primaryText,
+//                pojo.secondaryText,
+//                pojo.linkUrl,
+//                pojo.stickyTime,
+//                pojo.relatedButton
+//        )
+
+        val model = StickyComponentViewModel(
                 pojo.componentId,
                 pojo.componentType,
                 pojo.imageUrl,
@@ -54,7 +90,9 @@ class StickyComponentMapper @Inject constructor() : Func1<Response<DataResponse<
                 pojo.secondaryText,
                 pojo.linkUrl,
                 pojo.stickyTime,
-                pojo.relatedButton
-        )
+                pojo.relatedButton,
+                pojo.attributeData.toString())
+
+        return StickyComponentsViewModel(listOf(model))
     }
 }
---
+++ features/user/groupchat/src/main/java/com/tokopedia/groupchat/chatroom/view/adapter/chatroom/StickyComponentAdapter.kt
@@ -0,0 +1,153 @@
+package com.tokopedia.groupchat.chatroom.view.adapter.chatroom
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.graphics.Typeface
+import android.support.v7.widget.RecyclerView
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.gson.Gson
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.groupchat.R
+import com.tokopedia.groupchat.chatroom.domain.pojo.AttributeStickyComponentData
+import com.tokopedia.groupchat.room.view.viewmodel.pinned.StickyComponentViewModel
+import com.tokopedia.kotlin.extensions.view.hide
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Action1
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author : Steven 22/05/19
+ */
+
+class StickyComponentAdapter(var eventClickStickyComponent: (item: StickyComponentViewModel) -> Unit
+                             , var eventViewStickyComponent: (item: StickyComponentViewModel) -> Unit
+                             , var eventGoToAtc: (item: AttributeStickyComponentData) -> Unit)
+    : RecyclerView.Adapter<StickyComponentAdapter.StickyComponentViewHolder>() {
+
+    private var list: ArrayList<StickyComponentViewModel> = arrayListOf()
+
+    class StickyComponentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val subtitle = itemView.findViewById<TextView>(R.id.tv_subtitle)
+        val image = itemView.findViewById<ImageView>(R.id.iv_image)
+        val byMeIcon = itemView.findViewById<ImageView>(R.id.iv_byme)
+        val atcStickyPlay = itemView.findViewById<ImageView>(R.id.atc_sticky_play)
+
+
+        fun bindElement(
+                stickyComponentViewModel: StickyComponentViewModel,
+                dismissItem: () -> Unit,
+                eventClickStickyComponent: (item: StickyComponentViewModel) -> Unit,
+                eventViewStickyComponent: (item: StickyComponentViewModel) -> Unit,
+                eventGoToAtc : (item: AttributeStickyComponentData) -> Unit
+        ) {
+            byMeIcon.hide()
+
+            ImageHandler.LoadImage(image, stickyComponentViewModel.imageUrl)
+
+            title.text = MethodChecker.fromHtml(stickyComponentViewModel.title)
+            title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
+            subtitle.setTypeface(null, Typeface.NORMAL)
+            title.setTextColor(MethodChecker.getColor(title.context, R.color.black_70))
+
+            subtitle.text = MethodChecker.fromHtml(stickyComponentViewModel.subtitle)
+            subtitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
+            subtitle.setTypeface(null, Typeface.BOLD)
+            subtitle.setTextColor(MethodChecker.getColor(title.context, R.color.orange_red))
+
+
+            itemView.animate().setDuration(200)
+                    .alpha(1f)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            eventViewStickyComponent.invoke(stickyComponentViewModel)
+                        }
+                    })
+            val hideStickyComponent = Action1<Long> {
+                itemView.animate().setDuration(200)
+                        .alpha(0f)
+                        .setListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator) {
+                                itemView.hide()
+                                dismissItem.invoke()
+                            }
+                        })
+            }
+            if (stickyComponentViewModel.stickyTime != 0) {
+                Observable.timer(stickyComponentViewModel.stickyTime.toLong(), TimeUnit.SECONDS)
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(hideStickyComponent, Action1 { it.printStackTrace() })
+            }
+
+            itemView.setOnClickListener {
+                eventClickStickyComponent.invoke(stickyComponentViewModel)
+            }
+
+            setSpecificView(stickyComponentViewModel, eventGoToAtc)
+        }
+
+        private fun setSpecificView(
+                stickyComponentViewModel: StickyComponentViewModel,
+                eventGoToAtc: (item: AttributeStickyComponentData) -> Unit
+        ) {
+            when (stickyComponentViewModel.componentType.toLowerCase()) {
+                StickyComponentViewModel.TYPE_PRODUCT -> {
+                    atcStickyPlay.setOnClickListener {
+                        var attribute = Gson().fromJson(
+                                stickyComponentViewModel.attributeData,
+                                AttributeStickyComponentData::class.java
+                        )
+
+                        attribute = AttributeStickyComponentData("3987128", "533466369", 1, "Meraih Mimpi")
+
+                        eventGoToAtc.invoke(attribute)
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): StickyComponentViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_sticky_profile, parent, false)
+
+        return StickyComponentViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: StickyComponentViewHolder, position: Int) {
+        holder.bindElement(
+                list[position],
+                dismissItem(position),
+                eventClickStickyComponent,
+                eventViewStickyComponent,
+                eventGoToAtc
+        )
+    }
+
+    fun setList(list: List<StickyComponentViewModel>) {
+        this.list = ArrayList(list)
+        notifyDataSetChanged()
+    }
+
+    private fun dismissItem(position: Int): () -> Unit {
+        return {
+            list.removeAt(position)
+            notifyItemRemoved(position)
+        }
+    }
+}
