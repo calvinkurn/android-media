479dd2b700f9f33b474bb7ec750292eea62b5212
nathaniel <nathanielkhuana@gmail.com>
2019-08-29 22:30:15 +0700

    Adding dynamic feature digital topup

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import com.crashlytics.android.Crashlytics
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.tokopedia.applink.internal.ApplinkConsInternalDigital
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal.DYNAMIC_FEATURE_INSTALL
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal.DYNAMIC_FEATURE_INSTALL_BASE
 import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace.SHOP_SETTINGS_BASE
@@ -32,6 +33,7 @@ object DeeplinkDFMapper {
     private val MODULE_SHOP_SETTINGS_SELLERAPP = "shop_settings_sellerapp"
     private val MODULE_SHOP_SETTINGS_CUSTOMERAPP = "shop_settings"
     private val MODULE_HOTEL_TRAVEL = "hotel_travel"
+    private val MODULE_DIGITAL_TOPUP = "digital_topup"
 
     private var manager: SplitInstallManager? = null
 
@@ -60,6 +62,11 @@ object DeeplinkDFMapper {
 //                            deeplink, MODULE_HOTEL_TRAVEL,
 //                            context.getString(R.string.title_hotel))
 //                }
+                deeplink.startsWith(ApplinkConsInternalDigital.TELCO_DIGITAL) -> {
+                    getDFDeeplinkIfNotInstalled(context,
+                            deeplink, MODULE_DIGITAL_TOPUP,
+                            context.getString(R.string.digital_topup_title))
+                }
                 deeplink.startsWith(SHOP_SETTINGS_BASE) -> {
                     getDFDeeplinkIfNotInstalled(context,
                         deeplink, MODULE_SHOP_SETTINGS_CUSTOMERAPP,
