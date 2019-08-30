71ed31dc76a81b1a1287d90740521cb87f8a1775
Muhammad Furqan <nanjihusaha@gmail.com>
2019-08-30 10:56:27 +0700

    banner same as banner view

--- features/digital/hotel/src/main/java/com/tokopedia/hotel/homepage/presentation/adapter/viewholder/HotelPromoViewHolder.kt
+++ features/digital/hotel/src/main/java/com/tokopedia/hotel/homepage/presentation/adapter/viewholder/HotelPromoViewHolder.kt
@@ -1,15 +1,12 @@
 package com.tokopedia.hotel.homepage.presentation.adapter.viewholder
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.util.DisplayMetrics
 import android.view.View
-import android.view.WindowManager
-import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.hotel.R
 import com.tokopedia.hotel.homepage.data.cloud.entity.HotelPromoEntity
 import com.tokopedia.hotel.homepage.presentation.adapter.HotelPromoAdapter
+import com.tokopedia.kotlin.extensions.view.loadImage
 import kotlinx.android.synthetic.main.item_hotel_promo.view.*
 
 /**
@@ -19,17 +16,7 @@ class HotelPromoViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(element: HotelPromoEntity, listener: HotelPromoAdapter.PromoClickListener?, position: Int) {
         with(itemView) {
-
-            // banner height 1/3 of phone width
-            val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-            val metrics = DisplayMetrics()
-            wm.defaultDisplay.getMetrics(metrics)
-
-            val layoutParams = iv_hotel_promo.layoutParams
-            layoutParams.height = (metrics.widthPixels / 3) - resources.getDimensionPixelSize(R.dimen.dp_16)
-            iv_hotel_promo.layoutParams = layoutParams
-
-            ImageHandler.loadImageWithoutPlaceholder(this.iv_hotel_promo, element.attributes.imageUrl)
+            this.iv_hotel_promo.loadImage(element.attributes.imageUrl)
 
             this.setOnClickListener {
                 listener?.onPromoClicked(element, position)
