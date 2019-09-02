22b33933112c72dd3a5e7b615a3c602a5af038ba
stevenfredian <steven.fredian@tokopedia.com>
2019-09-02 14:39:04 +0700

    remote config youtube play fullscreen

--- libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RemoteConfigKey.java
+++ libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RemoteConfigKey.java
@@ -141,4 +141,6 @@ public class RemoteConfigKey {
     public static final String PLAY_PIP = "android_customer_enable_play_pip";
 
     public static final String CONFIG_ENABLE_NEW_ROUTE_REACT = "android_customer_enable_route_react";
+
+    public static final String PLAY_YOUTUBE_FULL_SCREEN = "android_customer_enable_play_youtube_fullscreen";
 }
