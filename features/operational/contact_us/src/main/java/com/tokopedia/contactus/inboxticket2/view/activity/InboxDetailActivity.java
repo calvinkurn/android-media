405b8571b958fec842c72a810ea9af5f928fe597
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2019-09-04 09:59:40 +0530

    feedback changes

--- features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxticket2/view/activity/InboxDetailActivity.java
+++ features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxticket2/view/activity/InboxDetailActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
@@ -13,12 +12,10 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.airbnb.deeplinkdispatch.DeepLink;
 import com.tokopedia.applink.ApplinkConst;
@@ -410,7 +407,7 @@ public class InboxDetailActivity extends InboxBaseActivity
 
     void sendMessage() {
         ((InboxDetailContract.InboxDetailPresenter) mPresenter).sendMessage();
-        edMessage.setHint(R.string.type_here);
+        edMessage.setHint(R.string.contact_us_type_here);
         ContactUsTracking.sendGTMInboxTicket("",
                 InboxTicketTracking.Category.EventInboxTicket,
                 InboxTicketTracking.Action.EventClickSubmitReply,
@@ -496,7 +493,7 @@ public class InboxDetailActivity extends InboxBaseActivity
         rvSelectedImages.setVisibility(View.GONE);
         edMessage.getText().clear();
         setSubmitButtonEnabled(false);
-        edMessage.setHint(R.string.type_here);
+        edMessage.setHint(R.string.contact_us_type_here);
         viewHelpRate.setVisibility(View.GONE);
         textToolbar.setVisibility(View.VISIBLE);
         rvMessageList.setPadding(0, 0, 0,
