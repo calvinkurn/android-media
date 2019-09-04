2cd0cb6c373ffc96670f443568944c9d6a0c3db1
abhishekjain2808 <abhishek.jain@tokopedia.com>
2019-09-04 21:49:30 +0530

    load top ads only if we have some products available

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/domain/mapper/ProductListMapper.kt
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/domain/mapper/ProductListMapper.kt
@@ -9,47 +9,45 @@ class ProductListMapper {
     fun transform(productListResponse: ProductListResponse, topAdsResponse: TopAdsResponse)
             : ProductListResponse {
 
-        val response = productListResponse
 
-        topAdsResponse.productAds?.data?.let {
+        if (productListResponse.searchProduct?.products?.isNotEmpty() == true) {
 
-            for (i in it) {
-                val item = ProductsItem()
+            topAdsResponse.productAds?.data?.let {
+                for (i in it) {
+                    val item = ProductsItem()
 
-                i?.let { dataItem ->
+                    i?.let { dataItem ->
 
-                    item.imageURL300 = dataItem.product?.image?.sEcs ?: ""
-                    item.imageURL = dataItem.product?.image?.sEcs ?: ""
-                    item.imageURL700 = dataItem.product?.image?.mEcs ?: ""
-                    item.name = dataItem.product?.name ?: ""
-                    item.shop.name = dataItem.shop?.name ?: ""
-                    item.discountPercentage = dataItem.product?.campaign?.discountPercentage ?: 0
-                    item.originalPrice = dataItem.product?.campaign?.originalPrice ?: ""
-                    item.priceRange = dataItem.product?.priceFormat ?: ""
-                    item.price = dataItem.product?.priceFormat ?: ""
-                    dataItem.shop?.let { shop ->
-                        shop.badges?.let { badgeList ->
-                            item.badges = badgeList
+                        item.imageURL300 = dataItem.product?.image?.sEcs ?: ""
+                        item.imageURL = dataItem.product?.image?.sEcs ?: ""
+                        item.imageURL700 = dataItem.product?.image?.mEcs ?: ""
+                        item.name = dataItem.product?.name ?: ""
+                        item.shop.name = dataItem.shop?.name ?: ""
+                        item.discountPercentage = dataItem.product?.campaign?.discountPercentage
+                                ?: 0
+                        item.originalPrice = dataItem.product?.campaign?.originalPrice ?: ""
+                        item.priceRange = dataItem.product?.priceFormat ?: ""
+                        item.price = dataItem.product?.priceFormat ?: ""
+                        dataItem.shop?.let { shop ->
+                            shop.badges?.let { badgeList ->
+                                item.badges = badgeList
+                            }
                         }
+                        item.shop.city = dataItem.shop?.city ?: ""
+                        item.isTopAds = true
+                        item.rating = dataItem.product?.productRating ?: 0
+
+                        item.countReview = (dataItem.product?.countReviewFormat?.toInt()) ?: 0
+                        item.wishlist = (dataItem.product?.wishlist) ?: false
+
+                        item.id = (dataItem.product?.id?.toInt()) ?: 0
+                        item.categoryID = (dataItem.product?.category?.id?.toInt()) ?: 0
                     }
-                    item.shop.city = dataItem.shop?.city ?: ""
-                    item.isTopAds = true
-                    item.rating = dataItem.product?.productRating ?: 0
 
-                    item.countReview = (dataItem.product?.countReviewFormat?.toInt()) ?: 0
-                    item.wishlist = (dataItem.product?.wishlist) ?: false
-
-                    item.id = (dataItem.product?.id?.toInt()) ?: 0
-                    item.categoryID = (dataItem.product?.category?.id?.toInt()) ?: 0
+                    productListResponse.searchProduct.products.add(0, item)
                 }
-
-                response.searchProduct?.products?.add(0, item)
             }
-
-
         }
-
-
-        return response
+        return productListResponse
     }
 }
