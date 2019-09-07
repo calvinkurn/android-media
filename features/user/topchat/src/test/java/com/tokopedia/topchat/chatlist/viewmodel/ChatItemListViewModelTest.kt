461d777a92e8824e5a5452464a292e42346a8d51
isfaaghyth <isfaaghyth@gmail.com>
2019-09-08 04:47:38 +0700

    create unit test for topchat based on delete chat by message id

---
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatlist/viewmodel/ChatItemListViewModelTest.kt
@@ -0,0 +1,103 @@
+package com.tokopedia.topchat.chatlist.viewmodel
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.google.gson.Gson
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.graphql.data.model.GraphqlResponse
+import com.tokopedia.topchat.chatlist.pojo.ChatDelete
+import com.tokopedia.topchat.chatlist.pojo.ChatDeleteStatus
+import com.tokopedia.topchat.chatlist.pojo.ChatListPojo
+import com.tokopedia.topchat.data.DataTest
+import com.tokopedia.topchat.data.MockDeleteChatList
+import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.user.session.UserSessionInterface
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.*
+import org.mockito.Matchers.any
+import org.mockito.Matchers.eq
+import org.mockito.Mockito.*
+
+class ChatItemListViewModelTest {
+
+    @get:Rule val rule = InstantTaskExecutorRule()
+
+    lateinit var viewModel: ChatItemListViewModel
+
+    @Mock lateinit var graphqlRepository: GraphqlRepository
+    @Mock lateinit var userSessionInterface: UserSessionInterface
+    @Mock lateinit var usecase: GraphqlUseCase<ChatListPojo>
+    @Mock lateinit var rawQuery: Map<String, String>
+
+    @Mock lateinit var result: Observer<Result<ChatDelete>>
+    @Captor lateinit var resultCaptor: ArgumentCaptor<Result<ChatDelete>>
+
+    @ExperimentalCoroutinesApi
+    @Before fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = ChatItemListViewModel(graphqlRepository, usecase, rawQuery, Dispatchers.Unconfined)
+        `when`(userSessionInterface.userId).thenReturn("12345")
+    }
+
+    private fun givenData(json: String): DataTest {
+        val mockResponse = Gson().fromJson(json, ChatDeleteStatus::class.java)
+
+        val request = GraphqlRequest(
+                any(String::class.java),
+                eq(ChatDeleteStatus::class.java),
+                anyMapOf(String::class.java, Any::class.java))
+
+        val response = GraphqlResponse(
+                /* response success */
+                mapOf(ChatDeleteStatus::class.java to mockResponse),
+                /* response error */
+                mapOf(ChatDeleteStatus::class.java to listOf()),
+                /* cache handler */
+                false
+        )
+
+        return DataTest(request, response)
+    }
+
+    @Test fun `delete chat item by message id`() = runBlocking {
+        //given
+        val (request, response) = givenData(MockDeleteChatList.chatMoveToTrash)
+
+        //when
+        `when`(graphqlRepository.getReseponse(listOf(request))).thenReturn(response.getSuccessData())
+
+        viewModel.deleteChat.observeForever(result)
+        viewModel.chatMoveToTrash(1, MockDeleteChatList.chatMoveToTrashQuery)
+
+        //then
+        verify(result, atLeastOnce()).onChanged(resultCaptor.capture())
+        val allValueCaptors = resultCaptor.allValues
+
+        println(allValueCaptors)
+    }
+
+    @Test fun `fail delete chat item`() = runBlocking {
+        //given
+        val (request, response) = givenData(MockDeleteChatList.failChatMoveToTrash)
+
+        //when
+        `when`(graphqlRepository.getReseponse(listOf(request))).thenReturn(response)
+        viewModel.deleteChat.observeForever(result)
+        viewModel.chatMoveToTrash(1, MockDeleteChatList.chatMoveToTrashQuery)
+
+        //then
+        verify(result, atLeastOnce()).onChanged(resultCaptor.capture())
+        val allValueCaptors = resultCaptor.allValues
+
+        println(allValueCaptors)
+    }
+
+}
