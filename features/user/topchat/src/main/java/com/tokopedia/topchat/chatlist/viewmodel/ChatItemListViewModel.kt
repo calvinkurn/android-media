5777890f1aa00ce509d8a0d9835904d4d962345f
isfaaghyth <isfaaghyth@gmail.com>
2019-09-08 01:54:45 +0700

    create a viewModel contract and simplify some of code

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/viewmodel/ChatItemListViewModel.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/viewmodel/ChatItemListViewModel.kt
@@ -1,29 +1,39 @@
 package com.tokopedia.topchat.chatlist.viewmodel
 
+import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
-import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant.PARAM_FILTER_ALL
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant.PARAM_FILTER_READ
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant.PARAM_FILTER_UNREAD
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant.PARAM_FILTER_UNREPLIED
 import com.tokopedia.topchat.chatlist.pojo.ChatListPojo
-import kotlinx.coroutines.CoroutineDispatcher
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 /**
  * Created by stevenfredian on 10/19/17.
  */
 
-class ChatItemListViewModel
-@Inject constructor(private val graphqlUseCase: GraphqlUseCase<ChatListPojo>,
-                                                private val rawQueries: Map<String, String>,
-                                                dispatcher: CoroutineDispatcher) : BaseViewModel(dispatcher) {
+interface ChatItemListContract {
+    fun getChatListMessage(page: Int, filterIndex: Int, tab: String)
+    fun chatMoveToTrash(messageId: Int, query: String)
+}
+
+class ChatItemListViewModel @Inject constructor(
+        private val chatListUseCase: GraphqlUseCase<ChatListPojo>,
+        private val rawQueries: Map<String, String>,
+        private val dispatcher: CoroutineDispatcher
+) : BaseViewModel(dispatcher), ChatItemListContract {
+
+    private val _mutateChatList = MutableLiveData<Result<ChatListPojo>>()
+    val mutateChatList: LiveData<Result<ChatListPojo>>
+        get() = _mutateChatList
 
     private val arrayFilterParam = arrayListOf(
             PARAM_FILTER_ALL,
@@ -32,37 +42,28 @@ class ChatItemListViewModel
             PARAM_FILTER_UNREPLIED
     )
 
-    val mutateChatListResponse = MutableLiveData<Result<ChatListPojo>>()
-
-    fun queryGetChatListMessage(
-            page: Int,
-            filterIndex: Int,
-            tab: String
-    ) {
-
-        queryGetChatListMessage(page, arrayFilterParam[filterIndex],tab)
+    override fun getChatListMessage(page: Int, filterIndex: Int, tab: String) {
+        queryGetChatListMessage(page, arrayFilterParam[filterIndex], tab)
     }
 
-    private fun queryGetChatListMessage(
-            page: Int,
-            filter: String,
-            tab: String
-            ) {
+    override fun chatMoveToTrash(messageId: Int, query: String) {
 
+    }
+
+    private fun queryGetChatListMessage(page: Int, filter: String, tab: String) {
         rawQueries[ChatListQueriesConstant.QUERY_CHAT_LIST_MESSAGE]?.let { query ->
             val params = mapOf(
                     ChatListQueriesConstant.PARAM_PAGE to page,
                     ChatListQueriesConstant.PARAM_FILTER to filter,
                     ChatListQueriesConstant.PARAM_TAB to tab
-
             )
 
-            graphqlUseCase.setTypeClass(ChatListPojo::class.java)
-            graphqlUseCase.setRequestParams(params)
-            graphqlUseCase.setGraphqlQuery(query)
-            graphqlUseCase.execute(
-                    onSuccessGetChatListMessage(page)
-                    , onErrorGetChatListMessage()
+            chatListUseCase.setTypeClass(ChatListPojo::class.java)
+            chatListUseCase.setRequestParams(params)
+            chatListUseCase.setGraphqlQuery(query)
+            chatListUseCase.execute(
+                    onSuccessGetChatListMessage(page),
+                    onErrorGetChatListMessage()
             )
         }
     }
@@ -70,13 +71,14 @@ class ChatItemListViewModel
     private fun onErrorGetChatListMessage(): (Throwable) -> Unit {
         return {
             it.printStackTrace()
-            mutateChatListResponse.value = Fail(it)
+            _mutateChatList.value = Fail(it)
         }
     }
 
     private fun onSuccessGetChatListMessage(page: Int): (ChatListPojo) -> Unit {
         return {
-            mutateChatListResponse.value = Success(it)
+            _mutateChatList.value = Success(it)
         }
     }
+
 }
