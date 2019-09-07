10781b0e02635948b0384869f0250384978d10f9
isfaaghyth <isfaaghyth@gmail.com>
2019-09-08 04:46:23 +0700

    mock json response of topchat

---
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/data/MockDeleteChatList.kt
@@ -0,0 +1,50 @@
+package com.tokopedia.topchat.data
+
+object MockDeleteChatList {
+
+    val chatMoveToTrash = """
+        {
+          "data": {
+            "chatMoveToTrash": {
+              "list": [
+                {
+                  "IsSuccess": 1,
+                  "DetailResponse": "Success",
+                  "MsgID": 772426858
+                }
+              ]
+            }
+          }
+        }
+    """.trimIndent()
+
+    val failChatMoveToTrash = """
+        {
+            "data": {
+                "chatMoveToTrash": null
+              },
+              "errors": [
+                {
+                  "message": "Maaf, Permohonan Anda tidak dapat diproses saat ini. Mohon dicoba kembali.",
+                  "path": [
+                    "chatMoveToTrash"
+                  ],
+                  "extensions": {}
+                }
+              ]
+        }
+    """.trimIndent()
+
+    val chatMoveToTrashQuery = """
+        mutation {
+         chatMoveToTrash(ids:[0]) {
+           list{
+             IsSuccess
+             DetailResponse
+             MsgID
+           }
+         }
+        }
+    """.trimIndent()
+
+}
