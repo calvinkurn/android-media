e49afbacf64d324a17821ee45ce943fda6f1f5eb
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-09-08 13:16:14 +0700

    Add query and data for new addition info data

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/AdditionalInfo.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/AdditionalInfo.java
@@ -23,13 +23,25 @@ public class AdditionalInfo {
     @SerializedName("imageUrl")
     @Expose
     private String imageUrl;
+    @SerializedName("notes")
+    @Expose
+    private String notes;
+    @SerializedName("urlDetail")
+    @Expose
+    private String urlDetail;
+    @SerializedName("urlText")
+    @Expose
+    private String urlText;
 
-    public AdditionalInfo(String label, String value, String textColor, String backgroundColor, String imageUrl) {
+    public AdditionalInfo(String label, String value, String textColor, String backgroundColor, String imageUrl, String notes, String urlDetail, String urlText) {
         this.label = label;
         this.value = value;
         this.textColor = textColor;
         this.backgroundColor = backgroundColor;
         this.imageUrl = imageUrl;
+        this.notes = notes;
+        this.urlDetail = urlDetail;
+        this.urlText = urlText;
     }
 
     public String label() {
@@ -52,14 +64,29 @@ public class AdditionalInfo {
         return imageUrl;
     }
 
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getUrlDetail() {
+        return urlDetail;
+    }
+
+    public String getUrlText() {
+        return urlText;
+    }
+
     @Override
     public String toString() {
-        return "[AdditionalInfo:{"
-                + "label="+label +" "
-                + "value="+value +" "
-                + "textColor="+textColor +" "
-                + "backgroundColor="+backgroundColor +" "
-                + "imageUrl="+imageUrl
-                + "}]";
+        return "AdditionalInfo{" +
+                "label='" + label + '\'' +
+                ", value='" + value + '\'' +
+                ", textColor='" + textColor + '\'' +
+                ", backgroundColor='" + backgroundColor + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", notes='" + notes + '\'' +
+                ", urlDetail='" + urlDetail + '\'' +
+                ", urlText='" + urlText + '\'' +
+                '}';
     }
 }
