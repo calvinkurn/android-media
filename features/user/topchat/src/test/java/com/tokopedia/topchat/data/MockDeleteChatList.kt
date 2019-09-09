09aaafa0a7a6e149752229d62b5780acd9af63d4
isfaaghyth <isfaaghyth@gmail.com>
2019-09-09 14:21:37 +0700

    response of mock listOfChatMessage have added

--- features/user/topchat/src/test/java/com/tokopedia/topchat/data/MockDeleteChatList.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/data/MockDeleteChatList.kt
@@ -35,4 +35,74 @@ object MockDeleteChatList {
         }
     """.trimIndent()
 
+    val listOfChatMessage = """
+        {
+          "data": {
+            "chatListMessage": {
+              "list": [
+                {
+                  "msgID": 801710918,
+                  "messageKey": "user-23824753~user-49032181",
+                  "attributes": {
+                    "contact": {
+                      "id": 23824753,
+                      "role": "Tokopedia Administrator",
+                      "domain": "",
+                      "name": "Tokopedia Info",
+                      "shopStatus": 0,
+                      "tag": "Official",
+                      "thumbnail": "https://accounts.tokopedia.com/image/v1/u/23824753/user_thumbnail/mobile"
+                    },
+                    "lastReplyMessage": "Halo toppers!",
+                    "readStatus": 1,
+                    "unreads": 1,
+                    "fraudStatus": 0
+                  }
+                },
+                {
+                  "msgID": 812174863,
+                  "messageKey": "user-6976645~shop-6543079",
+                  "attributes": {
+                    "contact": {
+                      "id": 6976645,
+                      "role": "user",
+                      "domain": "",
+                      "name": "Ade Fulki Hadian",
+                      "shopStatus": 0,
+                      "tag": "Pengguna",
+                      "thumbnail": "https://accounts.tokopedia.com/image/v1/u/6976645/user_thumbnail/mobile"
+                    },
+                    "lastReplyMessage": "Bisa dikirim hari ini ga? Terima kasih!",
+                    "readStatus": 2,
+                    "unreads": 0,
+                    "fraudStatus": 0
+                  }
+                },
+                {
+                  "msgID": 772456122,
+                  "messageKey": "user-49032181~shop-4471375",
+                  "attributes": {
+                    "contact": {
+                      "id": 4471375,
+                      "role": "shop",
+                      "domain": "nillkinstoree888",
+                      "name": "Nillkin Store 888",
+                      "shopStatus": 1,
+                      "tag": "Penjual",
+                      "thumbnail": "https://imagerouter.tokopedia.com/image/v1/s/4471375/shop_xs_thumbnail/desktop"
+                    },
+                    "lastReplyMessage": "ok, saya coba",
+                    "readStatus": 2,
+                    "unreads": 0,
+                    "fraudStatus": 0
+                  }
+                }
+              ],
+              "hasNext": true,
+              "pagingNext": true,
+              "showTimeMachine": 0
+            }
+          }
+        }
+    """.trimIndent()
 }
