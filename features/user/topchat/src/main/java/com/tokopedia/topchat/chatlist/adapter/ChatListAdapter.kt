57e9447ad474715740fe1ea3af3508babc2ba0a5
isfaaghyth <isfaaghyth@gmail.com>
2019-09-09 10:45:00 +0700

    add delete item behavior on adapter

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/adapter/ChatListAdapter.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/adapter/ChatListAdapter.kt
@@ -41,4 +41,11 @@ class ChatListAdapter(adapterTypeFactory: ChatListTypeFactoryImpl) :
 
         diff.dispatchUpdatesTo(this)
     }
+
+    fun deleteItem(position: Int) {
+        data.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, data.size)
+    }
+
 }
