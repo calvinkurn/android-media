1e92b179d6336b3051d1b56444d7a035e15f0aea
isfaaghyth <isfaaghyth@gmail.com>
2019-09-09 15:20:24 +0700

    remove groupchat channel fragment initialization on inboxChatActivity

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/activity/InboxChatActivity.java
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/activity/InboxChatActivity.java
@@ -114,14 +114,7 @@ public class InboxChatActivity extends BaseSimpleActivity
             indicatorLayout.setVisibility(View.GONE);
         }
 
-        if (getIntent().getExtras() != null
-                && getIntent().getExtras().getInt(ACTIVE_INDICATOR_POSITION, -1) != -1) {
-            indicatorAdapter.setActiveIndicator(getIntent().getExtras().getInt
-                    (ACTIVE_INDICATOR_POSITION, 0));
-            initGroupChatFragment();
-        } else {
-            initTopChatFragment();
-        }
+        initTopChatFragment();
     }
 
     private boolean isIndicatorVisible() {
@@ -165,35 +158,9 @@ public class InboxChatActivity extends BaseSimpleActivity
                 break;
             case POSITION_GROUP_CHAT:
                 analytics.eventClickInboxChannel();
-                initGroupChatFragment();
                 break;
             default:
         }
-    }
-
-    private void initGroupChatFragment() {
-
-        if (!TextUtils.isEmpty(getIntent().getExtras().getString("title"))) {
-            getSupportActionBar().setTitle(getIntent().getExtras().getString("title"));
-        }
-        ;
-
-        Bundle bundle = new Bundle();
-        if (getIntent().getExtras() != null) {
-            bundle.putAll(getIntent().getExtras());
-        }
-
-        TopChatRouter inboxRouter = (TopChatRouter) getApplicationContext();
-
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag
-                (inboxRouter.getChannelFragmentTag());
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        if (fragment == null) {
-            fragment = inboxRouter.getChannelFragment(bundle);
-        }
-        fragmentTransaction.replace(R.id.container, fragment, fragment.getClass().getSimpleName());
-        fragmentTransaction.commit();
     }
 
     private void initTopChatFragment() {
