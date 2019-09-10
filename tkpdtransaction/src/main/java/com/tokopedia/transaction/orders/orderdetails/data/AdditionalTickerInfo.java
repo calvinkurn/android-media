542ac4f8f46832fc34ec08a1f150bd25609099d7
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-09-10 14:03:30 +0700

    Add additional ticker info to models

---
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/AdditionalTickerInfo.java
@@ -0,0 +1,50 @@
+package com.tokopedia.transaction.orders.orderdetails.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AdditionalTickerInfo {
+
+    @SerializedName("notes")
+    @Expose
+    private String notes;
+    @SerializedName("urlDetail")
+    @Expose
+    private String urlDetail;
+    @SerializedName("urlText")
+    @Expose
+    private String urlText;
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getUrlDetail() {
+        return urlDetail;
+    }
+
+    public void setUrlDetail(String urlDetail) {
+        this.urlDetail = urlDetail;
+    }
+
+    public String getUrlText() {
+        return urlText;
+    }
+
+    public void setUrlText(String urlText) {
+        this.urlText = urlText;
+    }
+
+    @Override
+    public String toString() {
+        return "AdditionalTickerInfo{" +
+                "notes='" + notes + '\'' +
+                ", urlDetail='" + urlDetail + '\'' +
+                ", urlText='" + urlText + '\'' +
+                '}';
+    }
+}
