1a8ffe26f1ad4a3bd478975428278a3d460f4ce8
yogieputra8 <yogie.putra8@gmail.com>
2019-09-11 13:37:04 +0700

    Remove unused codes

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/viewholder/PushNotifCheckerViewHolder.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/viewholder/PushNotifCheckerViewHolder.kt
@@ -2,13 +2,10 @@ package com.tokopedia.settingnotif.usersetting.view.adapter.viewholder
 
 import android.support.annotation.LayoutRes
 import android.view.View
-import android.widget.Button
-import android.widget.Switch
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.settingnotif.R
 import com.tokopedia.settingnotif.usersetting.domain.pojo.PushNotifierTroubleshooterSetting
-import com.tokopedia.settingnotif.usersetting.view.listener.SettingFieldContract
-import kotlinx.android.synthetic.main.item_push_notif_checker.view.*
+
 
 class PushNotifCheckerViewHolder(
         val v: View,
@@ -20,23 +17,11 @@ class PushNotifCheckerViewHolder(
         val LAYOUT = R.layout.item_push_notif_checker
     }
 
-//    interface SettingListener {
-//        fun goToPushNotifCheckerPage()
-//    }
-
-//    private lateinit var settingFieldContract: SettingFieldContract.View
-
     override fun bind(element: PushNotifierTroubleshooterSetting?) {
         element?.run {
             itemView.setOnClickListener {
                 settingListener.goToPushNotificationChecker()
             }
         }
-
-//        if (::settingFieldContract.isInitialized) {
-//            itemView.btn_goto_push_notif.setOnClickListener {
-//                settingFieldContract?.onGoToPushNotifCheckerPage()
-//            }
-//        }
     }
 }
