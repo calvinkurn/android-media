67b04fb7219ccf3340afe4fe2a78c2c2b8027b3b
Jessica Sean <jessica.sean@tokopedia.com>
2019-09-11 17:00:19 +0700

    fix compile error

--- features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingModule.java
+++ features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingModule.java
@@ -1,10 +1,14 @@
 package com.tokopedia.flight.booking.di;
 
+import android.content.Context;
+
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
 import com.tokopedia.flight.booking.domain.FlightAddToCartUseCase;
 import com.tokopedia.flight.common.domain.FlightRepository;
-import com.tokopedia.promocheckout.common.domain.flight.FlightCancelVoucherUseCase;
 import com.tokopedia.flight.search.domain.usecase.FlightSearchJourneyByIdUseCase;
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor;
+import com.tokopedia.promocheckout.common.domain.flight.FlightCancelVoucherUseCase;
+import com.tokopedia.graphql.domain.GraphqlUseCase;
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase;
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository;
 
@@ -26,8 +30,9 @@ public class FlightBookingModule {
 
     @FlightBookingScope
     @Provides
-    FlightCancelVoucherUseCase flightCancelVoucherUseCase(FlightRepository flightRepository) {
-        return new FlightCancelVoucherUseCase(flightRepository);
+    FlightCancelVoucherUseCase flightCancelVoucherUseCase(@ApplicationContext Context context,
+                                                          GraphqlUseCase graphqlUseCase) {
+        return new FlightCancelVoucherUseCase(context, graphqlUseCase);
     }
 
     @FlightBookingScope
