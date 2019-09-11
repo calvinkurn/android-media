78a7b67e578fb2822af5eeadb64aee9412e313b1
darianthedytoped <darian.thedy@tokopedia.com>
2019-09-11 13:28:42 +0700

    Update model and config

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/v2/BlankSpaceConfig.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/v2/BlankSpaceConfig.kt
@@ -5,10 +5,21 @@ package com.tokopedia.productcard.v2
  * [When a component have true value]
  * That component will always reserve place even if the value of that component is empty
  */
-class BlankSpaceConfig {
-    var productName: Boolean = false
-    var rating: Boolean = false
-    var price: Boolean = false
-    var slashedPrice: Boolean = false
-    var discountPercentage: Boolean = false
-}
+class BlankSpaceConfig (
+    val imageProduct: Boolean = false,
+    val buttonWishlist: Boolean = false,
+    val labelPromo: Boolean = false,
+    val imageShop: Boolean = false,
+    val shopName: Boolean = false,
+    val productName: Boolean = false,
+    val discountPercentage: Boolean = false,
+    val slashedPrice: Boolean = false,
+    val price: Boolean = false,
+    val shopBadges: Boolean = false,
+    val shopLocation: Boolean = false,
+    val ratingCount: Boolean = false,
+    val reviewCount: Boolean = false,
+    val labelCredibility: Boolean = false,
+    val labelOffers: Boolean = false,
+    val iconTopAds: Boolean = false
+)
