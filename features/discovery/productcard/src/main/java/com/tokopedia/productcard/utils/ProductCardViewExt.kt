cc2b95d29e190e71fe6ef31ca0584de45f56c431
darianthedytoped <darian.thedy@tokopedia.com>
2019-09-11 16:18:48 +0700

    Commit review

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/ProductCardViewExt.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/ProductCardViewExt.kt
@@ -5,8 +5,7 @@ import android.support.constraint.ConstraintLayout
 import android.support.constraint.ConstraintSet
 import android.view.View
 import android.widget.TextView
-import com.tokopedia.productcard.v2.BlankSpaceConfig
-import com.tokopedia.productcard.v2.ProductCardView
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 
 val View.isVisible: Boolean
     get() = visibility == View.VISIBLE
@@ -37,16 +36,28 @@ fun ConstraintLayout?.applyConstraintSet(configureConstraintSet: (ConstraintSet)
     }
 }
 
-fun TextView.setValueWithConfig(textValue: String, blankSpaceConfigValue: Boolean) {
-    if (textValue.isNotEmpty()) {
-        text = textValue
-        visibility = View.VISIBLE
+fun TextView?.setTextWithBlankSpaceConfig(textValue: String, blankSpaceConfigValue: Boolean) {
+    this?.configureVisibilityWithBlankSpaceConfig(textValue.isNotEmpty(), blankSpaceConfigValue) {
+        it.text = MethodChecker.fromHtml(textValue)
+    }
+}
+
+fun <T: View> T?.configureVisibilityWithBlankSpaceConfig(isVisible: Boolean, blankSpaceConfigValue: Boolean, action: (T) -> Unit) {
+    if (this == null) return
+
+    visibility = if (isVisible) {
+        action(this)
+        View.VISIBLE
+    } else {
+        getViewNotVisibleWithBlankSpaceConfig(blankSpaceConfigValue)
+    }
+}
+
+private fun getViewNotVisibleWithBlankSpaceConfig(blankSpaceConfigValue: Boolean): Int {
+    return if (blankSpaceConfigValue) {
+        View.INVISIBLE
     }
     else {
-        visibility = if (blankSpaceConfigValue) {
-            View.INVISIBLE
-        } else {
-            View.GONE
-        }
+        View.GONE
     }
 }
