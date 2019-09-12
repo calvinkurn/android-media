ddff355d6b70549cde61a6fec4de22d63e6b54f3
stevenfredian <steven.fredian@tokopedia.com>
2019-09-12 22:53:06 +0700

    handle padding bottom

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/NotificationUpdateLongerTextFragment2.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/NotificationUpdateLongerTextFragment2.kt
@@ -6,30 +6,31 @@ import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.BottomSheetDialog
 import android.support.design.widget.BottomSheetDialogFragment
-import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.view.View.OnTouchListener
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.LinearLayout
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.design.image.RoundedCornerImageView
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.util.getParamString
 import com.tokopedia.navigation.R
+import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
-import kotlinx.android.synthetic.main.fragment_notification_update_longer2.*
 import kotlinx.android.synthetic.main.fragment_notification_update_longer2.view.*
+import kotlin.math.max
 
 /**
  * @author by nisie on 18/02/19.
  */
 class NotificationUpdateLongerTextFragment2 : BottomSheetDialogFragment() {
 
-    lateinit var contentImageView: ImageView
-    lateinit var contentTextView: TextView
-    lateinit var contentTitleView: TextView
+    lateinit var contentImageView: RoundedCornerImageView
+    lateinit var contentTextView: Typography
+    lateinit var contentTitleView: Typography
+    lateinit var ctaButton: UnifyButton
 
     private var contentImageUrl = ""
     private var contentImageViewType = ""
@@ -48,6 +49,7 @@ class NotificationUpdateLongerTextFragment2 : BottomSheetDialogFragment() {
         contentImageView = view.findViewById(R.id.content_image)
         contentTextView = view.findViewById(R.id.content_text)
         contentTitleView = view.findViewById(R.id.content_title)
+        ctaButton = view.findViewById(R.id.cta_button)
 
         return view
     }
@@ -61,23 +63,23 @@ class NotificationUpdateLongerTextFragment2 : BottomSheetDialogFragment() {
         bottomSheetDialog.setOnShowListener {
             //To Anchor View Bottom
             val dialog = it as BottomSheetDialog
-            dialog.findViewById<FrameLayout>(R.id.design_bottom_sheet) as FrameLayout
             val containerLayout: FrameLayout =
                     dialog.findViewById<FrameLayout>(R.id.container) as FrameLayout
-
-            val button = btnAnchor
-            val parent = button.parent as ViewGroup
-            parent.removeView(button)
-            containerLayout.addView(button, containerLayout.childCount)
 
             //To Expand Dialog when dialog showed
             val bottomSheet = dialog.findViewById<View>(R.id.design_bottom_sheet) as FrameLayout
             val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
             bottomSheetBehavior.skipCollapsed = true
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+
+            val param = contentTextView.layoutParams as LinearLayout.LayoutParams
+            val height = ctaButton.height
+            val margin = (ctaButton.layoutParams as ViewGroup.MarginLayoutParams).bottomMargin
+
+
 
             //To Make NestedScrollview as main concern and drag down when reach the top
-            containerLayout.scroll_main.getViewTreeObserver()
+            containerLayout.scroll_main.viewTreeObserver
                     .addOnScrollChangedListener {
                         //When Scroll view reach the bottom
                         if (!containerLayout.scroll_main.canScrollVertically(1)) {
@@ -88,6 +90,11 @@ class NotificationUpdateLongerTextFragment2 : BottomSheetDialogFragment() {
                             containerLayout.parent.requestDisallowInterceptTouchEvent(false)
                         }
                     }
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+//            param.setMargins(0,0,0,height + margin)
+//            contentTextView.layoutParams = param
+//            contentTextView.setPadding(0,0,0,height+margin)
         }
         return bottomSheetDialog
     }
@@ -96,20 +103,21 @@ class NotificationUpdateLongerTextFragment2 : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupViewModel(savedInstanceState)
-//        closeButton.setOnClickListener {
-//            dismiss()
-//        }
+        contentTextView.text = contentText
+        var lineCount = contentTextView.lineCount
+        var a = 4
 
-        val lorem = getString(R.string.dummy_longer_content)
-        val longContent = lorem.repeat(7)
-        contentTextView.text = longContent
+
+
         if (contentImageUrl.isNotBlank()) {
             ImageHandler.loadImage2(contentImageView, contentImageUrl, R.drawable.ic_loading_toped_new)
             contentImageView.show()
+            a = 8
         } else {
             contentImageView.hide()
         }
 
+        contentTextView.text = contentTextView.text.toString()+ ("\n").repeat(a)
         contentTitleView.text = contentTitle
     }
 
