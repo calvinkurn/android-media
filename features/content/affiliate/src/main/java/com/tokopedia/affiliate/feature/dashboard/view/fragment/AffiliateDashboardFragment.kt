390f718a3ff53c74c455146014886624646f0e92
jegul <kenny.hadisaputra@tokopedia.com>
2019-09-13 10:08:11 +0700

    - rename file and handle scroll

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/fragment/AffiliateDashboardFragment.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/fragment/AffiliateDashboardFragment.kt
@@ -121,7 +121,7 @@ class AffiliateDashboardFragment : BaseDaggerFragment(), AffiliateDashboardContr
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_af_affiliate_dashboard, container, false)
+        val view = inflater.inflate(R.layout.fragment_af_dashboard, container, false)
         presenter.attachView(this)
         return view
     }
