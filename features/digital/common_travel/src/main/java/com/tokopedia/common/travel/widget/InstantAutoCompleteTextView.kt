59732fabca863706e83e3a45fa0f549a392978ab
Jessica Sean <jessica.sean@tokopedia.com>
2019-09-16 13:26:46 +0700

    fix layout

--- features/digital/common_travel/src/main/java/com/tokopedia/common/travel/widget/InstantAutoCompleteTextView.kt
+++ features/digital/common_travel/src/main/java/com/tokopedia/common/travel/widget/InstantAutoCompleteTextView.kt
@@ -4,13 +4,14 @@ import android.content.Context
 import android.graphics.Rect
 import android.support.v7.widget.AppCompatAutoCompleteTextView
 import android.util.AttributeSet
+import android.widget.AutoCompleteTextView
 
 /**
  * @author by jessica on 2019-09-16
  */
 
 class InstantAutoCompleteTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
-    : AppCompatAutoCompleteTextView(context, attrs, defStyleAttr) {
+    : AutoCompleteTextView(context, attrs, defStyleAttr) {
     override fun enoughToFilter(): Boolean = true
 
     override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
