3c17d995423da215616b62c08a8c8d82264e74a6
milhamj <ilham.jamaludin@tokopedia.com>
2019-09-17 23:33:53 +0700

    Move  bind  logic to view  holder :sparkles:

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/adapter/ProductSuggestionAdapter.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/adapter/ProductSuggestionAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.affiliate.R
+import com.tokopedia.affiliate.feature.createpost.SuggestionClickListener
 import com.tokopedia.affiliate.feature.createpost.view.viewmodel.ProductSuggestionItem
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.inflateLayout
@@ -14,33 +15,25 @@ import kotlinx.android.synthetic.main.item_af_product_suggestion.view.*
 /**
  * @author by milhamj on 2019-09-17.
  */
+
 class ProductSuggestionAdapter(
-        private var onSuggestionItemClicked: (ProductSuggestionItem) -> Unit
+        private var onSuggestionItemClicked: SuggestionClickListener
 ) : RecyclerView.Adapter<ProductSuggestionAdapter.SuggestionViewHolder>() {
 
     private val list: MutableList<ProductSuggestionItem> = arrayListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuggestionViewHolder {
-        return SuggestionViewHolder(parent.inflateLayout(R.layout.item_af_product_suggestion))
+        return SuggestionViewHolder(
+                parent.inflateLayout(R.layout.item_af_product_suggestion),
+                onSuggestionItemClicked
+        )
     }
 
     override fun getItemCount(): Int = list.size
 
     override fun onBindViewHolder(holder: SuggestionViewHolder, position: Int) {
         val element = list[position]
-        with(holder.itemView) {
-            setOnClickListener { onSuggestionItemClicked(element) }
-
-            image.loadImage(element.imageUrl)
-            title.text = element.title
-            price.text = element.price
-
-            if (element.type == ProductSuggestionItem.TYPE_AFFILIATE) {
-                bymeLogo.visible()
-            } else {
-                bymeLogo.gone()
-            }
-        }
+        holder.bind(element)
     }
 
     fun addAll(list: List<ProductSuggestionItem>) {
@@ -51,5 +44,26 @@ class ProductSuggestionAdapter(
 
     fun isEmpty(): Boolean = this.list.isEmpty()
 
-    class SuggestionViewHolder(v: View): RecyclerView.ViewHolder(v)
+    class SuggestionViewHolder(
+            v: View,
+            private var onSuggestionItemClicked: SuggestionClickListener)
+        : RecyclerView.ViewHolder(v) {
+
+        fun bind(element: ProductSuggestionItem) {
+            with(itemView) {
+                setOnClickListener { onSuggestionItemClicked(element) }
+
+                image.loadImage(element.imageUrl)
+                title.text = element.title
+                price.text = element.price
+
+                if (element.type == ProductSuggestionItem.TYPE_AFFILIATE) {
+                    bymeLogo.visible()
+                } else {
+                    bymeLogo.gone()
+                }
+            }
+        }
+
+    }
 }
