74f78f14ecb38a81164a4068c4fa94ac0d6bb9fe
Henry Priyono <henry.priyono@tokopedia.com>
2019-09-17 13:45:31 +0700

    Fix DFMapper

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -55,8 +55,8 @@ object DeeplinkDFMapper {
 //            DFP({ it.startsWith(TRAVEL_SUBHOMEPAGE) }, HOMEPAGE_TRAVEL, R.string.title_travel_homepage)
 //            DFP({ it.startsWith(TELCO_DIGITAL) }, DIGITAL_TOPUP, R.string.digital_topup_title)
 //            DFP({ it.startsWith(OPEN_SHOP) }, SHOP_OPEN_CUSTOMERAPP, R.string.title_open_shop)
-            DFP({ it.startsWith(SHOP_SETTINGS_BASE) }, SHOP_SETTINGS_CUSTOMERAPP, R.string.shop_settings_title)
-            DFP({ it.startsWith(IMAGE_SEARCH_RESULT) }, IMAGE_SEARCH, R.string.title_image_search)
+            add(DFP({ it.startsWith(SHOP_SETTINGS_BASE) }, SHOP_SETTINGS_CUSTOMERAPP, R.string.shop_settings_title))
+            add(DFP({ it.startsWith(IMAGE_SEARCH_RESULT) }, IMAGE_SEARCH, R.string.title_image_search))
 //            DFP({ it.startsWith(SETTING_PROFILE) }, USER_PROFILE_COMPLETION, R.string.applink_profile_completion_title)
 //            DFP({ it.startsWith(SETTING_BANK) }, USER_SETTING_BANK, R.string.applink_setting_bank_title)
         }
