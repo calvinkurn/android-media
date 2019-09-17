6b14edfcb6874c06d3d796d03ffd2f1f46ca1238
milhamj <ilham.jamaludin@tokopedia.com>
2019-09-17 16:53:04 +0700

    Init view model  :sparkles:

---
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/createpost/view/viewmodel/ProductSuggestionItem.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.affiliate.feature.createpost.view.viewmodel
+
+/**
+ * @author by milhamj on 2019-09-17.
+ */
+data class ProductSuggestionItem(
+        val productId: String = "",
+        val adId: String = "",
+        val title: String = "",
+        val price: String = "",
+        val imageUrl: String = ""
+)
