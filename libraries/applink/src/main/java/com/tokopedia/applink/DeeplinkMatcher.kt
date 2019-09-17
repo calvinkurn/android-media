c220ff49e55a956748ba41c19eb5710ae4cadfbc
Calvyn <calvyn.julian@tokopedia.com>
2019-09-17 16:44:02 +0700

    Remove newline

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMatcher.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMatcher.kt
@@ -84,7 +84,6 @@ class DeeplinkMatcher() {
             add(Pattern(EQ, 1, null) to SHOP)
             add(Pattern(EQ, 2, null) to PRODUCT)
             add(Pattern(EQ, 2, mapOf(0 to "fm", 1 to "modal-toko")) to OTHER)
-
         }
     }
 
