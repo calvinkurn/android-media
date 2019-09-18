39c373f4609a7f1abb5c0537849af3577ec5b6ad
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-09-18 16:54:01 +0700

    Remove commented code

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentPresenter.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/feature/shipment/ShipmentPresenter.java
@@ -1106,8 +1106,6 @@ public class ShipmentPresenter extends BaseDaggerPresenter<ShipmentContract.View
                                 if (voucherOrdersItemUiModel.getCode().equalsIgnoreCase(code)) {
                                     if (TickerCheckoutUtilKt.mapToStatePromoStackingCheckout(voucherOrdersItemUiModel.getMessage().getState()) == TickerPromoStackingCheckoutView.State.FAILED) {
                                         mTrackerShipment.eventClickLanjutkanTerapkanPromoError(voucherOrdersItemUiModel.getMessage().getText());
-//                                        getView().showToastError(errMessage);
-//                                        getView().resetCourier(cartPosition);
                                     } else {
                                         mTrackerShipment.eventClickLanjutkanTerapkanPromoSuccess(code);
                                     }
