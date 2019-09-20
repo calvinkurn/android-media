3144752254e1d0c90bb17b6e354cd404f9ab07e4
milhamj <ilham.jamaludin@tokopedia.com>
2019-09-20 14:26:07 +0700

    Init fragment :sparkles:

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedOnboardingFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedOnboardingFragment.kt
@@ -1,15 +1,28 @@
 package com.tokopedia.feedplus.view.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 
 /**
  * @author by milhamj on 2019-09-20.
  */
 class FeedOnboardingFragment : BaseDaggerFragment() {
+
     override fun getScreenName(): String =  ""
 
     override fun initInjector() {
+
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
