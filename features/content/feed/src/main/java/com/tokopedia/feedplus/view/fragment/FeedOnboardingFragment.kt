6bc147be57de713971d8f359737afec0ee8ea007
milhamj <ilham.jamaludin@tokopedia.com>
2019-09-20 14:30:01 +0700

    Fix :sparkles:

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedOnboardingFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedOnboardingFragment.kt
@@ -5,24 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.feedplus.R
 
 /**
  * @author by milhamj on 2019-09-20.
  */
 class FeedOnboardingFragment : BaseDaggerFragment() {
 
-    override fun getScreenName(): String =  ""
-
-    override fun initInjector() {
-
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_feed_onboarding, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun getScreenName(): String =  ""
+
+    override fun initInjector() {
 
     }
 }
