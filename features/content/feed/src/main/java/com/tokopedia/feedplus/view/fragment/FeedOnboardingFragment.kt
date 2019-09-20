51eac6178e21c998f8ffa5dd8cae8ae95e4518f4
milhamj <ilham.jamaludin@tokopedia.com>
2019-09-20 14:09:16 +0700

    Init onboarding fragment :sparkles:

---
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedOnboardingFragment.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.feedplus.view.fragment
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+
+/**
+ * @author by milhamj on 2019-09-20.
+ */
+class FeedOnboardingFragment : BaseDaggerFragment() {
+    override fun getScreenName(): String =  ""
+
+    override fun initInjector() {
+    }
+
+
+}
