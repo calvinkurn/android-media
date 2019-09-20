12ead57aa6ce8700330787a88ffae4da43a5c3fa
Jessica Sean <jessica.sean@tokopedia.com>
2019-09-20 11:13:55 +0700

    fix layout

--- features/digital/flight/src/main/java/com/tokopedia/flight/booking/view/adapter/FlightSimpleAdapter.java
+++ features/digital/flight/src/main/java/com/tokopedia/flight/booking/view/adapter/FlightSimpleAdapter.java
@@ -163,14 +163,14 @@ public class FlightSimpleAdapter extends RecyclerView.Adapter<FlightSimpleAdapte
         private TextView titleTextView;
         private TextView contentTextView;
         private ImageView arrowImageView;
-        private LinearLayout containerLinearLayout;
+        private ViewGroup containerLinearLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             titleTextView = (TextView) itemView.findViewById(R.id.tv_title);
             contentTextView = (TextView) itemView.findViewById(R.id.tv_content);
             arrowImageView = (ImageView) itemView.findViewById(R.id.iv_arrow);
-            containerLinearLayout = (LinearLayout) itemView.findViewById(R.id.container);
+            containerLinearLayout = (ViewGroup) itemView.findViewById(R.id.container);
         }
 
         public void bind(final SimpleViewModel viewModel) {
