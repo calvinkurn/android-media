0b4f91b34c2e55b09c8788e90a2b6cad2842a0d1
Muhammad Furqan <nanjihusaha@gmail.com>
2019-09-23 14:47:03 +0700

    add kotlin to common payment

--- features/payment/common_payment/src/main/java/com/tokopedia/common/payment/model/PaymentCode.kt
+++ features/payment/common_payment/src/main/java/com/tokopedia/common/payment/model/PaymentCode.kt
@@ -3,10 +3,8 @@ package com.tokopedia.common.payment.model
 /**
  * @author by furqan on 23/09/2019
  */
-class PaymentCode {
-    companion object {
-        const val PAYMENT_SUCCESS = 5
-        const val PAYMENT_CANCELLED = 6
-        const val PAYMENT_FAILED = 7
-    }
+object PaymentCode {
+    const val PAYMENT_SUCCESS = 5
+    const val PAYMENT_CANCELLED = 6
+    const val PAYMENT_FAILED = 7
 }
