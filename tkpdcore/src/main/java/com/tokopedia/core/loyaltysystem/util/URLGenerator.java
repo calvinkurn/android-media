cd19a1414d8fd2ad0334bf3fb474c9024af1a509
vishalgupta1987 <vishal.gupta@tokopedia.com>
2019-09-23 10:45:44 +0530

    adding changes to pass ga client id in webview as param

--- tkpdcore/src/main/java/com/tokopedia/core/loyaltysystem/util/URLGenerator.java
+++ tkpdcore/src/main/java/com/tokopedia/core/loyaltysystem/util/URLGenerator.java
@@ -5,6 +5,9 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.util.Log;
 
+import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl;
+import com.tokopedia.remoteconfig.RemoteConfigKey;
+import com.tokopedia.track.TrackApp;
 import com.tokopedia.url.TokopediaUrl;
 import com.tokopedia.core2.BuildConfig;
 import com.tokopedia.core.app.MainApplication;
@@ -18,6 +21,7 @@ import com.tokopedia.core.util.SessionHandler;
 public class URLGenerator {
 
     private static final String SEAMLESS_LOGIN = "seamless?";
+    private static final String PARAM_APPCLIENT_ID = "appClientId?";
 
     public static String generateURLLucky(String url, Context context) {
         Uri uri = Uri.parse(url);
@@ -34,13 +38,18 @@ public class URLGenerator {
 
     public static String generateURLSessionLogin(String url, Context context) {
         Uri uri = Uri.parse(url);
-        String path = uri.getLastPathSegment();
-        String queryStart = uri.getQuery();
+        String newUrl = url;
+
+        if(uri != null) {
+            String clientID = TrackApp.getInstance().getGTM().getClientIDString();
+            newUrl = uri.buildUpon().appendQueryParameter(PARAM_APPCLIENT_ID, clientID).build().toString();
+        }
+
         String urlFinal = getBaseUrl() + SEAMLESS_LOGIN
                 + "token=" + GCMHandler.getRegistrationId(context)
                 + "&os_type=1"
                 + "&uid=" + SessionHandler.getLoginID(context)
-                + "&url=" + url;
+                + "&url=" + newUrl;
         return urlFinal;
     }
 
@@ -48,11 +57,15 @@ public class URLGenerator {
         Uri uri = Uri.parse(url);
         String path = uri.getLastPathSegment();
         String queryStart = uri.getQuery();
+
+        String clientID = TrackApp.getInstance().getGTM().getClientIDString();
+        String newUrl = uri.buildUpon().appendQueryParameter(PARAM_APPCLIENT_ID , clientID).build().toString();
+
         String urlFinal = getBaseUrl() + SEAMLESS_LOGIN
                 + "token=" + GCMHandler.getRegistrationId(context)
                 + "&os_type=1"
                 + "&uid=" + SessionHandler.getLoginID(context)
-                + "&url=" + url;
+                + "&url=" + newUrl;
         return urlFinal;
     }
 
@@ -73,10 +86,16 @@ public class URLGenerator {
                 baseUrl = TkpdBaseURL.JS_ALPHA_DOMAIN;
             } else if (pref.getString("DOMAIN_WS4", TokopediaUrl.Companion.getInstance().getWS()).contains("staging")) {
                 baseUrl = TkpdBaseURL.JS_STAGING_DOMAIN;
-                ;
             }
         }
         return baseUrl;
+    }
+
+    private static boolean isPassingCLientIdEnable(Context context){
+        if(context == null)  return false;
+
+        FirebaseRemoteConfigImpl remoteConfig = new FirebaseRemoteConfigImpl(context);
+        return remoteConfig.getBoolean(RemoteConfigKey.ENABLE_PASS_GA_CLIENT_ID_WEB, true);
     }
 
 }
