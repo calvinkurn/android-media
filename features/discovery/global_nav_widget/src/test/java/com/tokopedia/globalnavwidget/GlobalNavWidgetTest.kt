7635fef24dadca9be6c0c533f380e1d76b9e8ee7
darianthedytoped <darian.thedy@tokopedia.com>
2019-09-25 13:40:14 +0700

    Global nav widget initial commit

---
+++ features/discovery/global_nav_widget/src/test/java/com/tokopedia/globalnavwidget/GlobalNavWidgetTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.globalnavwidget
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class GlobalNavWidgetTest {
+
+    @Test
+    fun `test unit test`() {
+        assertThat(2 + 2, `is`(4))
+    }
+}
