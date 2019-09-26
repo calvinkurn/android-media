07e9b11405b005dbfb10aca5a80499d8e30fef4f
Henry Priyono <henry.priyono@tokopedia.com>
2019-09-26 12:26:21 +0700

    Extract to function

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
@@ -212,16 +212,15 @@ public class SearchActivity extends BaseActivity
     }
 
     private void configureToolbarOnClickListener() {
-        searchTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SearchTracking.trackEventClickSearchBar();
-                moveToAutoCompleteActivity();
-            }
-        });
+        searchTextView.setOnClickListener(v -> onSearchBarClicked());
         backButton.setOnClickListener(v -> onBackPressed());
         buttonChangeGrid.setOnClickListener(v -> changeGrid());
         buttonCart.setOnClickListener(v -> moveToCartActivity());
+    }
+    
+    private void onSearchBarClicked() {
+        SearchTracking.trackEventClickSearchBar();
+        moveToAutoCompleteActivity();
     }
 
     private void configureButtonCart() {
