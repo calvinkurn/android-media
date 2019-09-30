930d1cb84d56cdbd245b3b0d6838da312c3624d5
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-09-30 11:50:32 +0700

    Implement base army view holder

---
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingduration/view/ArmyViewHolder.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.logisticcart.shipping.features.shippingduration.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.tokopedia.logisticcart.R
+import com.tokopedia.logisticcart.shipping.model.LogisticPromoViewModel
+
+class ArmyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    companion object{
+        @JvmStatic val LAYOUT = R.layout.item_army
+    }
+
+    fun bindData(data: LogisticPromoViewModel, listener: ShippingDurationAdapterListener) {
+
+    }
+
+}
