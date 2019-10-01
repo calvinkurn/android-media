c01b2217bdb8a4d4276111d19162317c684d4554
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2019-10-01 22:10:45 +0700

    add feature otp method with miscall

---
+++ features/user/otp/src/main/java/com/tokopedia/otp/cotp/view/viewlistener/OnboardingOtpMiscall.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.otp.cotp.view.viewlistener
+
+import com.tokopedia.abstraction.base.view.listener.CustomerView
+import com.tokopedia.abstraction.base.view.presenter.CustomerPresenter
+
+interface OnboardingOtpMiscall {
+
+    interface View: CustomerView {
+        fun startAnimation()
+        fun stopAnimation()
+    }
+
+    interface Presenter: CustomerPresenter<OnboardingOtpMiscall.View> {
+
+    }
+}
