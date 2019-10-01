0e69ed94acbcaf13e5dca76886732a95341e894c
yoasfs <yoas.fs@gmail.com>
2019-10-01 18:27:04 +0700

    move create post classes to new module createpost

---
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/domain/entity/GetContentFormDomain.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.createpost.domain.entity
+
+import com.tokopedia.createpost.data.pojo.getcontentform.FeedContentResponse
+
+
+/**
+ * @author by milhamj on 01/03/19.
+ */
+data class GetContentFormDomain(
+        val feedContentResponse: FeedContentResponse? = FeedContentResponse(),
+        val checkQuotaQuery: CheckQuotaQuery? = CheckQuotaQuery()
+)
