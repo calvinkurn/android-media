e234cf8f78e1f517bd24d4d394537c9849f8fa05
Calvyn <calvyn.julian@tokopedia.com>
2019-10-02 18:26:01 +0700

    add temp image url

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/adapter/viewholder/ShopProductViewHolder.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/adapter/viewholder/ShopProductViewHolder.java
@@ -109,7 +109,7 @@ public class ShopProductViewHolder extends AbstractViewHolder<ShopProductViewMod
             ImageHandler.loadImage(
                     freeOngkirBanner.getContext(),
                     freeOngkirBanner,
-                    shopProductViewModel.getFreeOngkirPromoIcon(),
+                    "https://ecs7.tokopedia.net/img/ic_bebas_ongkir_beta.png",
                     R.drawable.ic_loading_image
             );
             freeOngkirBanner.setVisibility(View.VISIBLE);
