32ebe54a8dafbfe59a4ade378041c98523c420f9
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-10-03 18:52:34 +0700

    add play service version in log

--- libraries/dynamic_features/src/main/java/com/tokopedia/dynamicfeatures/DFInstallerLogUtil.kt
+++ libraries/dynamic_features/src/main/java/com/tokopedia/dynamicfeatures/DFInstallerLogUtil.kt
@@ -1,8 +1,11 @@
 package com.tokopedia.dynamicfeatures
 
 import android.content.Context
+import android.support.v4.content.pm.PackageInfoCompat
+import com.google.android.gms.common.GoogleApiAvailability
 import timber.log.Timber
 import java.io.File
+import java.lang.Exception
 
 /**
  * Created by hendry on 2019-10-03.
@@ -17,23 +20,35 @@ object DFInstallerLogUtil {
                            errorCode: List<String>? = null,
                            downloadTimes: Int = 1,
                            isSuccess: Boolean = false) {
-        val messageStringBuilder = StringBuilder()
-        messageStringBuilder.append("P1$tag {$modulesName}; times_dl: {$downloadTimes};")
+        try {
+            val messageStringBuilder = StringBuilder()
+            messageStringBuilder.append("P1$tag {$modulesName};")
+            val playServiceVersion = PackageInfoCompat.getLongVersionCode(
+                context.packageManager.getPackageInfo(
+                    GoogleApiAvailability.GOOGLE_PLAY_SERVICES_PACKAGE,
+                    0
+                )
+            )
+            if (playServiceVersion > 0) {
+                messageStringBuilder.append("play_srv: {$playServiceVersion}")
+            }
+            messageStringBuilder.append("times_dl: {$downloadTimes};")
 
-        val totalSize = File(context.filesDir.absoluteFile.toString()).freeSpace.toDouble()
-        val totalFreeSpaceSizeInMB = String.format("%.2fMB", totalSize / MEGA_BYTE)
-        messageStringBuilder.append("free: {$totalFreeSpaceSizeInMB}; ")
+            val totalSize = File(context.filesDir.absoluteFile.toString()).freeSpace.toDouble()
+            val totalFreeSpaceSizeInMB = String.format("%.2fMB", totalSize / MEGA_BYTE)
+            messageStringBuilder.append("free: {$totalFreeSpaceSizeInMB}; ")
 
-        if (moduleSize > 0) {
-            val moduleSizeInMB = String.format("%.2fMB", moduleSize.toDouble() / MEGA_BYTE)
-            messageStringBuilder.append("size: {$moduleSizeInMB}; ")
-        }
-        if (errorCode?.isNotEmpty() == true) {
-            messageStringBuilder.append("err: {${errorCode.joinToString("-")}}; ")
-        }
-        if (isSuccess) {
-            messageStringBuilder.append("Success; ")
-        }
-        Timber.w(messageStringBuilder.toString())
+            if (moduleSize > 0) {
+                val moduleSizeInMB = String.format("%.2fMB", moduleSize.toDouble() / MEGA_BYTE)
+                messageStringBuilder.append("size: {$moduleSizeInMB}; ")
+            }
+            if (errorCode?.isNotEmpty() == true) {
+                messageStringBuilder.append("err: {${errorCode.joinToString("-")}}; ")
+            }
+            if (isSuccess) {
+                messageStringBuilder.append("Success; ")
+            }
+            Timber.w(messageStringBuilder.toString())
+        } catch (ignored: Exception) { }
     }
 }
