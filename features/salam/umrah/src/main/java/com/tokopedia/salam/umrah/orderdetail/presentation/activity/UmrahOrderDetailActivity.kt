b63a0c46ba0f965059d6e4a5a0b592709fb575b1
Muhammad Furqan <nanjihusaha@gmail.com>
2019-10-08 17:32:23 +0700

    fix wrong component name

--- features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/orderdetail/presentation/activity/UmrahOrderDetailActivity.kt
+++ features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/orderdetail/presentation/activity/UmrahOrderDetailActivity.kt
@@ -4,14 +4,14 @@ import android.support.v4.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.salam.umrah.common.di.UmrahComponentInstance
-import com.tokopedia.salam.umrah.orderdetail.di.DaggerUmrahOrderComponent
+import com.tokopedia.salam.umrah.orderdetail.di.DaggerUmrahOrderDetailComponent
 import com.tokopedia.salam.umrah.orderdetail.di.UmrahOrderDetailComponent
 import com.tokopedia.salam.umrah.orderdetail.presentation.fragment.UmrahOrderDetailFragment
 
 class UmrahOrderDetailActivity : BaseSimpleActivity(), HasComponent<UmrahOrderDetailComponent> {
 
     override fun getComponent(): UmrahOrderDetailComponent =
-            DaggerUmrahOrderComponent.builder()
+            DaggerUmrahOrderDetailComponent.builder()
                     .umrahComponent(UmrahComponentInstance.getUmrahComponent(application))
                     .build()
 
