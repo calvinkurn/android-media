4852eb1b576f3f36c84c044de0a6e0844f39bc35
jegul <kenny.hadisaputra@tokopedia.com>
2019-10-09 12:57:19 +0700

    - add function getString to BaseViewHolder

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/helper/BaseViewHolder.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/helper/BaseViewHolder.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.feedcomponent.helper
 
+import android.support.annotation.StringRes
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
@@ -9,4 +10,12 @@ import android.view.View
 abstract class BaseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     abstract fun onViewRecycled()
+
+    protected fun getString(@StringRes stringRes: Int): String {
+        return itemView.context.getString(stringRes)
+    }
+
+    protected fun getString(@StringRes stringRes: Int, value: String): String {
+        return itemView.context.getString(stringRes, value)
+    }
 }
