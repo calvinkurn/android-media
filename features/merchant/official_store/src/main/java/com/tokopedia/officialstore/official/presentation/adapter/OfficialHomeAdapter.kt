e089a61449524afc754da11d1747e0a92fab9f57
Yogie Putra <yogieputra8@users.noreply.github.com>
2019-10-14 13:51:18 +0700

    Feature/yogie/os product recom (#7669)
    * Add new query for os product recom
    
    * override onBindViewHolder to separate type of layout
    
    * Revert get recom widget query

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/OfficialHomeAdapter.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/OfficialHomeAdapter.kt
@@ -1,12 +1,31 @@
 package com.tokopedia.officialstore.official.presentation.adapter
 
+import android.support.v7.widget.StaggeredGridLayoutManager
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseAdapter
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.officialstore.official.presentation.adapter.viewmodel.ProductRecommendationViewModel
 
 class OfficialHomeAdapter(adapterTypeFactory: OfficialHomeAdapterTypeFactory):
         BaseAdapter<OfficialHomeAdapterTypeFactory>(adapterTypeFactory) {
 
+    var twoSpanLayout = listOf(
+            ProductRecommendationViewModel.LAYOUT
+    )
+
     fun getDataByPosition(position: Int): Visitable<*> {
         return this.visitables[position]
+    }
+
+    override fun onBindViewHolder(holder: AbstractViewHolder<out Visitable<*>>, position: Int) {
+        val layoutParams = holder.itemView.getLayoutParams() as StaggeredGridLayoutManager.LayoutParams
+        layoutParams.isFullSpan = getItemViewType(position) !in twoSpanLayout
+        super.onBindViewHolder(holder, position)
+    }
+
+    override fun onBindViewHolder(holder: AbstractViewHolder<out Visitable<*>>, position: Int, payloads: MutableList<Any>) {
+        val layoutParams = holder.itemView.getLayoutParams() as StaggeredGridLayoutManager.LayoutParams
+        layoutParams.isFullSpan = getItemViewType(position) !in twoSpanLayout
+        super.onBindViewHolder(holder, position, payloads)
     }
 }
