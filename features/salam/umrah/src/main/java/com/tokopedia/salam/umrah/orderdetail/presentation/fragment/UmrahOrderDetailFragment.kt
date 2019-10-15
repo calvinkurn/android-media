267f7c8bac60ad6df6a5220634872f9593b4e6e8
Muhammad Furqan <nanjihusaha@gmail.com>
2019-10-15 17:15:35 +0700

    render umrah order detail data

--- features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/orderdetail/presentation/fragment/UmrahOrderDetailFragment.kt
+++ features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/orderdetail/presentation/fragment/UmrahOrderDetailFragment.kt
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -130,6 +131,7 @@ class UmrahOrderDetailFragment : BaseDaggerFragment() {
 
         val itemData = data.items[0]
         val metaData = Gson().fromJson(itemData.metadata, UmrahOrderDetailsMetaDataEntity::class.java)
+        Log.d("TextTitle", itemData.title)
         tg_umrah_package.text = itemData.title
         tg_umrah_travel.text = metaData.travelAgent
         tg_booking_code.text = metaData.bookingCode
