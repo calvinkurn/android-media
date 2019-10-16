29ba1271a646d57ffec5e3ed281dec98d99961ea
Lavekush Agrawal <lavekush.agrawal@tokopedia.com>
2019-10-16 16:55:12 +0530

    Fxed compile issue for release mode

--- features/common/developer_options/src/prod/java/com/tokopedia/translator/manager/TranslatorManager.java
+++ features/common/developer_options/src/prod/java/com/tokopedia/translator/manager/TranslatorManager.java
@@ -2,7 +2,7 @@ package com.tokopedia.translator.manager
 class TranslatorManager(){
 
         public TranslatorManager init(Application app, String key){
-
+            //Please do not remove, for detail slack to @lavekuhs-t(slack)
         }
 }
 
