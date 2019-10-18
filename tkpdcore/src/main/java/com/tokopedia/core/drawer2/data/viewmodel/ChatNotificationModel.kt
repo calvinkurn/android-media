d340c6a6d9f5c81ce354695c1039b4b0c1343182
Calvyn <calvyn.julian@tokopedia.com>
2019-10-18 13:37:55 +0700

    Change  from kotlin to java

--- tkpdcore/src/main/java/com/tokopedia/core/drawer2/data/viewmodel/ChatNotificationModel.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.core.drawer2.data.viewmodel
-
-data class ChatNotificationModel(
-        val notifUnreads: Int = 0,
-        val notifUnreadsSeller: Int = 0,
-        val notifUnreadsBuyer: Int = 0
-)
