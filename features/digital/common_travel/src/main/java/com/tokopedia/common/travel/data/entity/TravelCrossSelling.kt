a41b0fde5dd3c89efb74fe13317c5b2030f68c7b
Jessica Sean <jessica.sean@tokopedia.com>
2019-10-18 08:55:11 +0700

    add response

--- features/digital/common_travel/src/main/java/com/tokopedia/common/travel/data/entity/TravelCrossSelling.kt
+++ features/digital/common_travel/src/main/java/com/tokopedia/common/travel/data/entity/TravelCrossSelling.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.common.travel.data.entity
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -8,50 +9,64 @@ import com.google.gson.annotations.SerializedName
 
 data class TravelCrossSelling(
         @SerializedName("items")
+        @Expose
         val items: List<Item> = listOf(),
 
         @SerializedName("meta")
+        @Expose
         val meta: Meta = Meta()
 ) {
     data class Item(
             @SerializedName("product")
+            @Expose
             val product: String = "",
 
             @SerializedName("title")
+            @Expose
             val title: String = "",
 
             @SerializedName("content")
+            @Expose
             val content: String = "",
 
             @SerializedName("prefix")
+            @Expose
             val prefix: String = "",
 
             @SerializedName("imageURL")
+            @Expose
             val imageUrl: String = "",
 
             @SerializedName("uriWeb")
+            @Expose
             val uriWeb: String = "",
 
             @SerializedName("uri")
+            @Expose
             val uri: String = "",
 
             @SerializedName("value")
+            @Expose
             val value: String = ""
     )
 
     data class Meta(
             @SerializedName("title")
+            @Expose
             val title: String = "",
 
             @SerializedName("uri")
+            @Expose
             val uri: String = "",
 
             @SerializedName("uriWeb")
+            @Expose
             val uriWeb: String = ""
     )
 
     class Response(
             @SerializedName("crossSell")
+            @Expose
             val response: TravelCrossSelling = TravelCrossSelling()
     )
 }
