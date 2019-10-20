9da77f681dfb81d621b62ddaac9f15b0b3d64d41
abhishekjain2808 <abhishek.jain@tokopedia.com>
2019-10-20 21:41:58 +0530

    data models

---
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/newdiscovery/hotlistRevamp/data/hotListDetail/FilterAttribute.kt
@@ -0,0 +1,147 @@
+package com.tokopedia.discovery.newdiscovery.hotlistRevamp.data.hotListDetail
+
+import com.google.gson.annotations.SerializedName
+
+data class FilterAttribute(
+
+        @field:SerializedName("pmax")
+        val pmax: Int? = null,
+
+        @field:SerializedName("rt")
+        val rt: String? = null,
+
+        @field:SerializedName("scheme")
+        val scheme: String? = null,
+
+        @field:SerializedName("full_domain")
+        val fullDomain: String? = null,
+
+        @field:SerializedName("hot_id")
+        val hotId: Int? = null,
+
+        @field:SerializedName("etalase")
+        val etalase: Int? = null,
+
+        @field:SerializedName("discount")
+        val discount: Int? = null,
+
+        @field:SerializedName("official")
+        val official: Boolean? = null,
+
+        @field:SerializedName("fq")
+        val fq: String? = null,
+
+        @field:SerializedName("nuq")
+        val nuq: String? = null,
+
+        @field:SerializedName("source")
+        val source: String? = null,
+
+        @field:SerializedName("xsname")
+        val xsname: String? = null,
+
+        @field:SerializedName("image_square")
+        val imageSquare: Boolean? = null,
+
+        @field:SerializedName("sc_identifier")
+        val scIdentifier: Int? = null,
+
+        @field:SerializedName("fshop")
+        val fshop: Int? = null,
+
+        @field:SerializedName("sc")
+        val sc: String? = null,
+
+        @field:SerializedName("negative")
+        val negative: String? = null,
+
+        @field:SerializedName("ob")
+        val ob: Int? = null,
+
+        @field:SerializedName("wholesale")
+        val wholesale: Boolean? = null,
+
+        @field:SerializedName("shipping")
+        val shipping: String? = null,
+
+        @field:SerializedName("min_sold")
+        val minSold: Int? = null,
+
+        @field:SerializedName("pmin")
+        val pmin: Int? = null,
+
+        @field:SerializedName("fcity")
+        val fcity: String? = null,
+
+        @field:SerializedName("kreasi_lokal")
+        val kreasiLokal: Boolean? = null,
+
+        @field:SerializedName("dname")
+        val dname: String? = null,
+
+        @field:SerializedName("id")
+        val id: String? = null,
+
+        @field:SerializedName("negative_sc")
+        val negativeSc: String? = null,
+
+        @field:SerializedName("max_sold")
+        val maxSold: Int? = null,
+
+        @field:SerializedName("st")
+        val st: String? = null,
+
+        @field:SerializedName("ctg_id")
+        val ctgId: Int? = null,
+
+        @field:SerializedName("start")
+        val start: Int? = null,
+
+        @field:SerializedName("catalog_rows")
+        val catalogRows: Int? = null,
+
+        @field:SerializedName("floc")
+        val floc: String? = null,
+
+        @field:SerializedName("is_curated")
+        val isCurated: Boolean? = null,
+
+        @field:SerializedName("rows")
+        val rows: Int? = null,
+
+        @field:SerializedName("cashback")
+        val cashback: Boolean? = null,
+
+        @field:SerializedName("q")
+        val Q: String? = null,
+
+        @field:SerializedName("shop_id")
+        val shopId: String? = null,
+
+        @field:SerializedName("condition")
+        val condition: Int? = null,
+
+        @field:SerializedName("freereturns")
+        val freereturns: Boolean? = null,
+
+        @field:SerializedName("rf")
+        val rf: Boolean? = null,
+
+        @field:SerializedName("sname")
+        val sname: String? = null,
+
+        @field:SerializedName("xq")
+        val xq: String? = null,
+
+        @field:SerializedName("default_sc")
+        val defaultSc: String? = null,
+
+        @field:SerializedName("is_featured")
+        val isFeatured: Boolean? = null,
+
+        @field:SerializedName("preorder")
+        val preorder: Boolean? = null,
+
+        @field:SerializedName("cid")
+        val cid: Int? = null
+)
