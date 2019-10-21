c98bc2fce66d1a14ab48cacccf6885a6ef6d6b98
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-10-21 22:01:18 +0700

    sellerapp - remove instabug; add missing router function

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerRouterApplication.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
@@ -791,6 +792,11 @@ public abstract class SellerRouterApplication extends MainApplication
     public void refreshToken() throws IOException {
         AccessTokenRefresh accessTokenRefresh = new AccessTokenRefresh();
         accessTokenRefresh.refreshToken();
+    }
+
+    @Override
+    public void showAppFeedbackRatingDialog(FragmentManager fragmentManager, Context context, BottomSheets.BottomSheetDismissListener listener) {
+
     }
 
     public UserSession getSession() {
