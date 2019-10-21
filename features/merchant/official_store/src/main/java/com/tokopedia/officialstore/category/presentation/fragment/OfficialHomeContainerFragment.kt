112acb46791df2119d8d2f97f9b70c033b55267f
mzennis <meyta.taliti@tokopedia.com>
2019-10-21 19:45:03 +0700

    fix ui category, banner, featured brand

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/fragment/OfficialHomeContainerFragment.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/fragment/OfficialHomeContainerFragment.kt
@@ -54,10 +54,6 @@ class OfficialHomeContainerFragment : BaseDaggerFragment(), HasComponent<Officia
         OfficialHomeContainerAdapter(context, childFragmentManager)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_official_home, container, false)
     }
--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialFeaturedShopViewHolder.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialFeaturedShopViewHolder.kt
@@ -1,22 +1,47 @@
 package com.tokopedia.officialstore.official.presentation.adapter.viewholder
 
 import android.support.annotation.LayoutRes
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.officialstore.R
+import com.tokopedia.officialstore.official.data.model.Shop
 import com.tokopedia.officialstore.official.presentation.adapter.viewmodel.OfficialFeaturedShopViewModel
+import com.tokopedia.officialstore.official.presentation.widget.FeaturedShopAdapter
+import com.tokopedia.topads.sdk.view.SpacesItemDecoration
 
 class OfficialFeaturedShopViewHolder(view: View?): AbstractViewHolder<OfficialFeaturedShopViewModel>(view){
 
     private var recyclerView: RecyclerView? = null
+    private var link: TextView? = null
+
+    private var adapter: FeaturedShopAdapter? = null
 
     init {
         recyclerView = view?.findViewById(R.id.recycler_view_featured_shop)
+        link = view?.findViewById(R.id.link_featured_shop)
+
+        view?.context?.let {
+            adapter = FeaturedShopAdapter(it)
+            recyclerView?.layoutManager = GridLayoutManager(it, 2)
+            recyclerView?.addItemDecoration(SpacesItemDecoration(2))
+            recyclerView?.adapter = adapter
+        }
+
     }
 
     override fun bind(element: OfficialFeaturedShopViewModel?) {
+        link?.setOnClickListener {
+            // TODO route to applink
+        }
 
+        element?.featuredShop?.let {
+            adapter?.shopList = it
+            adapter?.notifyDataSetChanged()
+        }
     }
 
     companion object {
