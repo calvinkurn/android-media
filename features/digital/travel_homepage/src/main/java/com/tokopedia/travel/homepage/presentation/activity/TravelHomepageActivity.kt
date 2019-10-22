34575e3f3e7c5827b9d9db3c30fd71cf574fcc35
resakemal <resa.saharso@tokopedia.com>
2019-10-22 15:01:38 +0700

    Fix transparent navigation bar

--- features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/presentation/activity/TravelHomepageActivity.kt
+++ features/digital/travel_homepage/src/main/java/com/tokopedia/travel/homepage/presentation/activity/TravelHomepageActivity.kt
@@ -2,10 +2,8 @@ package com.tokopedia.travel.homepage.presentation.activity
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.WindowManager
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.graphql.data.GraphqlClient
