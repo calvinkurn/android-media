3aec51139136baccc1e4f84aaa3e0496a9023161
Calvyn <calvyn.julian@tokopedia.com>
2019-10-23 16:58:23 +0700

    remove unused import

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/activity/ProductFullDescriptionTabActivity.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/activity/ProductFullDescriptionTabActivity.kt
@@ -3,7 +3,6 @@ package com.tokopedia.product.detail.view.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.view.View
