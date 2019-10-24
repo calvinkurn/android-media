893911bca6b2c9286188d6595c7bf58a183779d6
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-10-24 13:08:14 +0700

    Add applink for NANA

--- libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalLogistic.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalLogistic.kt
@@ -37,9 +37,9 @@ object ApplinkConstInternalLogistic {
 
     /**
      * @see logistic/logisticaddaddress/../PinpointMapActivity.kt
-     *
+     * @param token [type: Token (logisticdata) ] token for Kero api hit (mandatory)
      * */
     @JvmField
-    val ADD_ADDRESS_V2 = "$INTERNAL_LOGISTIC/addaddress/v2/{ref}"
+    val ADD_ADDRESS_V2 = "$INTERNAL_LOGISTIC/addaddress/v2/"
 
 }
