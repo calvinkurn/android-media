137cd566deda711ab288e4ca8855fe76bca14523
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-10-24 14:30:13 +0700

    delete unused import

--- libraries/core_legacy/src/main/java/com/tokopedia/core/gcm/base/BaseNotificationMessagingService.java
+++ libraries/core_legacy/src/main/java/com/tokopedia/core/gcm/base/BaseNotificationMessagingService.java
@@ -2,7 +2,6 @@ package com.tokopedia.core.gcm.base;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
