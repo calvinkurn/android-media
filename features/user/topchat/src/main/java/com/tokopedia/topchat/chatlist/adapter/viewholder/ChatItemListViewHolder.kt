cb22fa0b88b0a83e0e649d26f8e273cd6e321a1f
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-10-25 11:15:10 +0700

    use uppercase H for hour

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/adapter/viewholder/ChatItemListViewHolder.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/adapter/viewholder/ChatItemListViewHolder.kt
@@ -21,7 +21,6 @@ import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatlist.listener.ChatListItemListener
 import com.tokopedia.topchat.chatlist.pojo.ItemChatListPojo
 import com.tokopedia.topchat.chatlist.pojo.ChatStateItem
-import com.tokopedia.topchat.common.util.ChatTimeConverter
 import com.tokopedia.unifycomponents.Label
 import com.tokopedia.unifycomponents.Toaster
 import com.tokopedia.unifyprinciples.Typography
@@ -222,9 +221,7 @@ class ChatItemListViewHolder(
     }
 
     private fun bindTimeStamp(lastReplyTimeStr: String) {
-        lastReplyTimeStr.toLongOrNull()?.let { unixTime ->
-            time.text = ChatTimeConverter.formatTimeStamp(unixTime)
-        }
+        time.text = convertToRelativeDate(lastReplyTimeStr)
     }
 
     private fun bindLabel(tag: String) {
@@ -242,6 +239,29 @@ class ChatItemListViewHolder(
             else -> label.hide()
         }
 
+    }
+
+    private fun convertToRelativeDate(timeStamp: String): String {
+        val smsTime = Calendar.getInstance()
+        smsTime.timeInMillis = timeStamp.toLongOrZero()
+
+        val now = Calendar.getInstance()
+
+        val timeFormatString = "HH:mm"
+        val dateTimeFormatString = "dd MMM"
+        val dateTimeYearFormatString = "dd MMM yy"
+        val HOURS = (60 * 60 * 60).toLong()
+        return if ((now.get(Calendar.DATE) == smsTime.get(Calendar.DATE))
+                && (now.get(Calendar.MONTH) == smsTime.get(Calendar.MONTH))) {
+            DateFormat.format(timeFormatString, smsTime).toString()
+        } else if ((now.get(Calendar.DATE) - smsTime.get(Calendar.DATE) == 1)
+                && (now.get(Calendar.MONTH) == smsTime.get(Calendar.MONTH))) {
+            "Kemarin"
+        } else if (now.get(Calendar.YEAR) == smsTime.get(Calendar.YEAR)) {
+            DateFormat.format(dateTimeFormatString, smsTime).toString()
+        } else {
+            DateFormat.format(dateTimeYearFormatString, smsTime).toString()
+        }
     }
 
     companion object {
