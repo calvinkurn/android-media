5e07280eb880c2203541fac7e6d9971b2c61bf54
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-10-28 13:57:17 +0700

    Add back arrow functionality

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/dropoff_picker/DropoffPickerActivity.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/dropoff_picker/DropoffPickerActivity.kt
@@ -1,12 +1,16 @@
 package com.tokopedia.logisticaddaddress.features.dropoff_picker
 
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.WindowManager
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
-import com.tokopedia.logisticaddaddress.R
 import com.google.android.gms.maps.model.MarkerOptions
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.logisticaddaddress.R
 
 
 class DropoffPickerActivity : BaseActivity(), OnMapReadyCallback {
@@ -21,7 +25,16 @@ class DropoffPickerActivity : BaseActivity(), OnMapReadyCallback {
         val toolbar = findViewById<Toolbar>(R.id.toolbar_search)
         setSupportActionBar(toolbar)
 
-        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map_dropoff) as SupportMapFragment
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = ContextCompat.getColor(this,
+                    com.tokopedia.design.R.color.green_600)
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        val mapFragment: SupportMapFragment =
+                supportFragmentManager.findFragmentById(R.id.map_dropoff) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -31,5 +44,15 @@ class DropoffPickerActivity : BaseActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
