71ec008469dfb2453e44513bc002afe88fb19888
Muhammad Furqan <nanjihusaha@gmail.com>
2019-10-29 16:03:06 +0700

    fix deeplink handler

--- customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeeplinkHandlerActivity.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/deeplink/DeeplinkHandlerActivity.java
@@ -54,8 +54,6 @@ import com.tokopedia.explore.applink.ExploreApplinkModule;
 import com.tokopedia.explore.applink.ExploreApplinkModuleLoader;
 import com.tokopedia.feedplus.view.deeplink.FeedDeeplinkModule;
 import com.tokopedia.feedplus.view.deeplink.FeedDeeplinkModuleLoader;
-import com.tokopedia.flight.applink.FlightApplinkModule;
-import com.tokopedia.flight.applink.FlightApplinkModuleLoader;
 import com.tokopedia.gamification.applink.GamificationApplinkModule;
 import com.tokopedia.gamification.applink.GamificationApplinkModuleLoader;
 import com.tokopedia.groupchat.common.applink.GroupChatApplinkModule;
@@ -224,7 +222,6 @@ public class DeeplinkHandlerActivity extends AppCompatActivity implements Deffer
                     new DiscoveryApplinkModuleLoader(),
                     new SessionApplinkModuleLoader(),
                     new FeedDeeplinkModuleLoader(),
-                    new FlightApplinkModuleLoader(),
                     new InstantDebitBcaApplinkModuleLoader(),
                     new DigitalBrowseApplinkModuleLoader(),
                     new ReputationApplinkModuleLoader(),
