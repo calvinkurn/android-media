c463df0da42cc6fe7d8ddd654f998a325e571ea5
irfan khoirul <irfankhoirul@gmail.com>
2019-10-29 08:47:28 +0700

    revert

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -193,7 +193,6 @@ class MainActivity : AppCompatActivity() {
          * startActivity(PlayActivity.getCallingIntent(this, "668", true))
          * or, you can use route like this:
          * RouteManager.route(this, ApplinkConstInternalMarketplace.SHOP_SETTINGS) */
-        RouteManager.route(this, ApplinkConstInternalMarketplace.CART)
     }
 
 }
