0591bbe605a0fde7f36db7c72bf808ece6ad6257
niranjan <niranjan.saxena@tokopedia.com>
2019-10-30 17:51:00 +0530

    reverted a change for trade in that caused crashes

--- features/category/tradein/src/main/java/com/tokopedia/tradein/view/viewcontrollers/TradeInHomeActivity.java
+++ features/category/tradein/src/main/java/com/tokopedia/tradein/view/viewcontrollers/TradeInHomeActivity.java
@@ -241,8 +241,8 @@ public class TradeInHomeActivity extends BaseTradeInActivity implements IAccessR
         String campaignId = Constants.CAMPAIGN_ID_PROD;
         if (Constants.LAKU6_BASEURL.equals(Constants.LAKU6_BASEURL_STAGING))
             campaignId = Constants.CAMPAIGN_ID_STAGING;
-//        laku6TradeIn = Laku6TradeIn.getInstance(context, campaignId,
-//                Constants.APPID, Constants.APIKEY, Constants.LAKU6_BASEURL, TRADEIN_TEST_TYPE);
+        laku6TradeIn = Laku6TradeIn.getInstance(context, campaignId,
+                Constants.APPID, Constants.APIKEY, Constants.LAKU6_BASEURL, TRADEIN_TEST_TYPE);
         requestPermission();
     }
 
