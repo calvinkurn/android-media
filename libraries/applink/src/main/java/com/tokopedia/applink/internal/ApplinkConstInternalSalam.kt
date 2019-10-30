b8e347f2e0653661d8975e7e9abfb1370a10b014
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2019-10-30 17:10:38 +0700

    Change AppLink Salam Umrah

--- libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalSalam.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalSalam.kt
@@ -3,10 +3,10 @@ package com.tokopedia.applink.internal
 import com.tokopedia.applink.constant.DeeplinkConstant
 
 object ApplinkConstInternalSalam{
-    const val HOST_SALAM = "umrah"
+    const val HOST_SALAM = "order-details"
 
 
     const val INTERNAL_SALAM = "${DeeplinkConstant.SCHEME_TOKOPEDIA}://${HOST_SALAM}"
 
-    const val SALAM_ORDER_DETAIL = "$INTERNAL_SALAM/order/"
+    const val SALAM_ORDER_DETAIL = "$INTERNAL_SALAM/umroh/.*\\"
 }
