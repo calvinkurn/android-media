478962d86dc11735a710e47c8a30f246773aace1
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-10-31 23:33:41 +0700

    Add base adapter for nearby location

---
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/dropoff_picker/NearbyStoreAdapter.kt
@@ -0,0 +1,39 @@
+package com.tokopedia.logisticaddaddress.features.dropoff_picker
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tokopedia.logisticaddaddress.R
+import com.tokopedia.logisticaddaddress.domain.model.dropoff.Data
+import kotlinx.android.synthetic.main.item_nearby_location.view.*
+
+class NearbyStoreAdapter : RecyclerView.Adapter<NearbyStoreAdapter.NearbiesViewHolder>() {
+
+    val mData: MutableList<Data> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NearbiesViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_nearby_location, parent, false)
+        return NearbiesViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = mData.size
+
+    override fun onBindViewHolder(holder: NearbiesViewHolder, position: Int) {
+        holder.bind(mData[position])
+    }
+
+    fun setData(item: List<Data>) {
+        mData.clear()
+        mData.addAll(item)
+        notifyDataSetChanged()
+    }
+
+    class NearbiesViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        fun bind(datum: Data) {
+            view.tv_location_title.text = datum.addrName
+            view.tv_location_desc.text = datum.districtName
+            view.tv_distance.text = datum.storeDistance
+        }
+    }
+}
