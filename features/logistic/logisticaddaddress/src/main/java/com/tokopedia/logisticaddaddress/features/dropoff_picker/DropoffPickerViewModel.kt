5bd3cbc23d8425944b381c41596efc45edc65de7
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-10-31 11:32:10 +0700

    Create base view model for dropoff

---
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/dropoff_picker/DropoffPickerViewModel.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.logisticaddaddress.features.dropoff_picker
+
+import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class DropoffPickerViewModel @Inject constructor(dispatcher: CoroutineDispatcher) : BaseViewModel(dispatcher) {
+
+}
