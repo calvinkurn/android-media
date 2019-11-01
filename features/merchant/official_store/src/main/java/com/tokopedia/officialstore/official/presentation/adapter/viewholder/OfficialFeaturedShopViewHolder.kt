c63e1aff31295a4524eda694f5c2e502f7b87c93
mzennis <meyta.taliti@tokopedia.com>
2019-11-01 10:57:01 +0700

    implement tracker feature brand

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialFeaturedShopViewHolder.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialFeaturedShopViewHolder.kt
@@ -6,7 +6,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.TextView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.toEmptyStringIfNull
 import com.tokopedia.officialstore.R
+import com.tokopedia.officialstore.analytics.OfficialStoreTracking
+import com.tokopedia.officialstore.official.data.model.Shop
 import com.tokopedia.officialstore.official.presentation.adapter.viewmodel.OfficialFeaturedShopViewModel
 import com.tokopedia.officialstore.official.presentation.widget.FeaturedShopAdapter
 import com.tokopedia.officialstore.official.presentation.widget.GridSpacingItemDecoration
@@ -19,12 +22,15 @@ class OfficialFeaturedShopViewHolder(view: View?): AbstractViewHolder<OfficialFe
 
     private var adapter: FeaturedShopAdapter? = null
 
+    private var officialStoreTracking: OfficialStoreTracking? = null
+
     init {
         recyclerView = view?.findViewById(R.id.recycler_view_featured_shop)
         link = view?.findViewById(R.id.link_featured_shop)
         title = view?.findViewById(R.id.title_featured_shop)
 
         view?.context?.let {
+            officialStoreTracking = OfficialStoreTracking(it)
             adapter = FeaturedShopAdapter(it)
             recyclerView?.layoutManager = GridLayoutManager(it, 2)
             recyclerView?.addItemDecoration(GridSpacingItemDecoration(2, 8))
@@ -34,6 +40,10 @@ class OfficialFeaturedShopViewHolder(view: View?): AbstractViewHolder<OfficialFe
 
     override fun bind(element: OfficialFeaturedShopViewModel?) {
         link?.setOnClickListener {
+
+            officialStoreTracking?.eventClickAllFeaturedBrand(
+                    element?.categoryName.toEmptyStringIfNull())
+
             // TODO route to ? webview or applink?
             //RouteManager.route(it.context, element?.headerShop?.link)
         }
@@ -44,6 +54,28 @@ class OfficialFeaturedShopViewHolder(view: View?): AbstractViewHolder<OfficialFe
         element?.featuredShop?.let {
             adapter?.shopList = it
             adapter?.notifyDataSetChanged()
+
+            val shopItem = it[0]
+            officialStoreTracking?.eventImpressionFeatureBrand(
+                    element.categoryName.toEmptyStringIfNull(),
+                    shopItem.shopId.toEmptyStringIfNull(),
+                    0,
+                    shopItem.name.toEmptyStringIfNull(),
+                    shopItem.imageUrl.toEmptyStringIfNull()
+            )
+
+            adapter?.onItemClickListener = object: FeaturedShopAdapter.OnItemClickListener {
+                override fun onItemClick(p0: Int, shop: Shop) {
+                    officialStoreTracking?.eventClickFeaturedBrand(
+                            element.categoryName.toEmptyStringIfNull(),
+                            shop.shopId.toEmptyStringIfNull(),
+                            p0,
+                            shop.name.toEmptyStringIfNull(),
+                            shop.imageUrl.toEmptyStringIfNull()
+                    )
+                }
+
+            }
         }
     }
 
--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/widget/FeaturedShopAdapter.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/widget/FeaturedShopAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ImageView
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.officialstore.R
@@ -12,6 +13,8 @@ import com.tokopedia.officialstore.official.data.model.Shop
 
 class FeaturedShopAdapter(private val context: Context, var shopList: List<Shop> = ArrayList()) :
         RecyclerView.Adapter<FeaturedShopAdapter.FeaturedShopViewHolder>() {
+
+    var onItemClickListener: OnItemClickListener? = null
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): FeaturedShopViewHolder {
         return FeaturedShopViewHolder(LayoutInflater.from(context).
@@ -30,6 +33,8 @@ class FeaturedShopAdapter(private val context: Context, var shopList: List<Shop>
                 shop.imageUrl,
                 R.drawable.ic_loading_image
         )
+
+        onItemClickListener?.onItemClick(p1, shop)
     }
 
     class FeaturedShopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -39,5 +44,9 @@ class FeaturedShopAdapter(private val context: Context, var shopList: List<Shop>
         init {
             imageView = itemView.findViewById(R.id.image_featured_shop)
         }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(p0: Int, shop: Shop)
     }
 }
