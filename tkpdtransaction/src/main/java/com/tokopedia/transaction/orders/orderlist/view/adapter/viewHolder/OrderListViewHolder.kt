b222c5fe7f78b00ae6d35e10f77a71eecd175d2c
niranjan <niranjan.saxena@tokopedia.com>
2019-11-01 14:50:14 +0530

    applink fix

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/adapter/viewHolder/OrderListViewHolder.kt
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/view/adapter/viewHolder/OrderListViewHolder.kt
@@ -221,7 +221,7 @@ class OrderListViewHolder(itemView: View?, var orderListAnalytics: OrderListAnal
                 orderListAnalytics.sendProductClickEvent(status?.text.toString())
                 orderListAnalytics.sendPageClickEvent("order - detail")
 
-                RouteManager.route(itemView.context, "${order.appLink}?upstream=${order.upstream}")
+                RouteManager.route(itemView.context, "${appLink}?upstream=${order.upstream}")
             }
         }
     }
