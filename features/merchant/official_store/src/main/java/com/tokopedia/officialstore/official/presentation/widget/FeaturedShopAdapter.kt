dcbb09a5238f66656f9ce6c44a31da930607f00a
mzennis <meyta.taliti@tokopedia.com>
2019-11-01 15:32:54 +0700

    fix feature brands

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/widget/FeaturedShopAdapter.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/widget/FeaturedShopAdapter.kt
@@ -5,9 +5,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ImageView
-import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.tokopedia.officialstore.R
 import com.tokopedia.officialstore.official.data.model.Shop
 
@@ -27,12 +27,12 @@ class FeaturedShopAdapter(private val context: Context, var shopList: List<Shop>
 
     override fun onBindViewHolder(p0: FeaturedShopViewHolder, p1: Int) {
         val shop = shopList[p1]
-        ImageHandler.loadImage(
-                context,
-                p0.imageView,
-                shop.imageUrl,
-                R.drawable.ic_loading_image
-        )
+        Glide.with(context)
+                .load(shop.imageUrl)
+                .dontAnimate()
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(p0.imageView)
 
         p0.itemView.setOnClickListener {
             onItemClickListener?.onItemClick(context, p1, shop)
