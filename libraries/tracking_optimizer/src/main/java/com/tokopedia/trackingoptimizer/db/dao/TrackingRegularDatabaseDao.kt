7e4b40a5c5b6d478c742229f3a5d19b4f1fdec0b
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-11-04 20:09:53 +0700

    fix db memory allocation - optimize tracking by sending per row limit

--- libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/db/dao/TrackingRegularDatabaseDao.kt
+++ libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/db/dao/TrackingRegularDatabaseDao.kt
@@ -3,6 +3,7 @@ package com.tokopedia.trackingoptimizer.db.dao
 import androidx.room.Dao
 import androidx.room.Query
 import com.tokopedia.trackingoptimizer.db.model.TrackingEEDbModel
+import com.tokopedia.trackingoptimizer.db.model.TrackingEEFullDbModel
 import com.tokopedia.trackingoptimizer.db.model.TrackingRegularDbModel
 
 @Dao
@@ -14,10 +15,16 @@ interface TrackingRegularDatabaseDao : TrackingDatabaseDao<TrackingRegularDbMode
     @Query("DELETE FROM ${TrackingRegularDbModel.TRACKING_REGULAR_TABLE_NAME}")
     override fun deleteTable()
 
+    @Query("DELETE FROM ${TrackingRegularDbModel.TRACKING_REGULAR_TABLE_NAME} WHERE id in (:keyList)")
+    override fun deleteByKeyList(keyList: List<String>)
+
     @Query("SELECT * FROM ${TrackingRegularDbModel.TRACKING_REGULAR_TABLE_NAME} WHERE id = :key LIMIT 1")
     override fun getTrackingModel(key: String): TrackingRegularDbModel?
 
     @Query("SELECT * FROM ${TrackingRegularDbModel.TRACKING_REGULAR_TABLE_NAME}")
     override fun getTrackingModelList(): Array<TrackingRegularDbModel>?
+
+    @Query("SELECT * FROM ${TrackingRegularDbModel.TRACKING_REGULAR_TABLE_NAME} LIMIT :limit")
+    override fun getTrackingModelList(limit: Int): Array<TrackingRegularDbModel>?
 
 }
