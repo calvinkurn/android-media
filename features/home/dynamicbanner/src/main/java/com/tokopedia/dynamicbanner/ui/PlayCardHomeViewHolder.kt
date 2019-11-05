4c801d7c36049a4e542207b7209d7cd463d38dd8
isfaaghyth <isfaaghyth@gmail.com>
2019-11-05 11:18:09 +0700

    added viewHolder of playHomeCard

--- features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/ui/PlayCardHomeViewHolder.kt
+++ features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/ui/PlayCardHomeViewHolder.kt
@@ -1,4 +1,10 @@
 package com.tokopedia.dynamicbanner.ui
 
-class PlayCardHomeViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PlayCardHomeViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+
+
 }
