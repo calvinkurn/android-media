8d011847d1d32920720d1cdf7cf057e3846116f6
isfaaghyth <isfaaghyth@gmail.com>
2019-11-05 11:17:55 +0700

    create playHomeCard for play banner

--- features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/domain/PlayCardHomeUseCase.kt
+++ features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/domain/PlayCardHomeUseCase.kt
@@ -1,4 +1,25 @@
 package com.tokopedia.dynamicbanner.domain
 
-class PlayCardHomeUseCase {
+import com.tokopedia.dynamicbanner.QUERY_PLAY_CARD
+import com.tokopedia.dynamicbanner.entity.PlayCardHome
+import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.usecase.coroutines.UseCase
+import javax.inject.Inject
+import javax.inject.Named
+
+class PlayCardHomeUseCase @Inject constructor(
+        @Named(QUERY_PLAY_CARD) val query: String,
+        private val graphqlUseCase: MultiRequestGraphqlUseCase
+): UseCase<PlayCardHome>() {
+
+    override suspend fun executeOnBackground(): PlayCardHome {
+        val request = GraphqlRequest(query, PlayCardHome::class.java)
+        graphqlUseCase.clearRequest()
+        graphqlUseCase.addRequest(request)
+
+        val response = graphqlUseCase.executeOnBackground()
+        return response.getData<PlayCardHome>(PlayCardHome::class.java)
+    }
+
 }
