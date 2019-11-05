7153d2b37fb7ef8119a724eebe1286e302b20a3d
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-11-05 14:30:35 +0700

    add iter next

--- libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/repository/TrackingRepository.kt
+++ libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/repository/TrackingRepository.kt
@@ -127,6 +127,7 @@ class TrackingRepository(val context: Context) : ITrackingRepository<TrackingEED
             var count = 0
             while (iter.hasNext() && count < itemCountToAdd) {
                 iter.remove()
+                iter.next()
                 count++
             }
             // replacing old data
@@ -136,15 +137,15 @@ class TrackingRepository(val context: Context) : ITrackingRepository<TrackingEED
 
     }
 
-    fun moveEETrackingToFull(trackingEEDbModel: TrackingEEDbModel,
-                             inputEvent: EventModel, inputCustomDimension: HashMap<String, Any>?,
-                             inputEnhanceECommerceMap: HashMap<String, Any>?) {
+    private fun moveEETrackingToFull(trackingEEDbModel: TrackingEEDbModel,
+                                     inputEvent: EventModel, inputCustomDimension: HashMap<String, Any>?,
+                                     inputEnhanceECommerceMap: HashMap<String, Any>?) {
         sendTrack(trackingEEDbModel)
         directPutEE(inputEvent, inputCustomDimension, inputEnhanceECommerceMap)
     }
 
-    fun directPutEE(inputEvent: EventModel, inputCustomDimensionMap: HashMap<String, Any>?,
-                    inputEnhanceECommerceMap: HashMap<String, Any>?) {
+    private fun directPutEE(inputEvent: EventModel, inputCustomDimensionMap: HashMap<String, Any>?,
+                            inputEnhanceECommerceMap: HashMap<String, Any>?) {
         if (inputEnhanceECommerceMap == null && inputCustomDimensionMap!!.isEmpty()) {
             trackingEEDataSource.delete(inputEvent)
         } else {
