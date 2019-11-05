7dc98c67becadadc6a3433a422d57d5be2748f80
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-11-05 14:44:47 +0700

    fix iter has next

--- libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/repository/TrackingRepository.kt
+++ libraries/tracking_optimizer/src/main/java/com/tokopedia/trackingoptimizer/repository/TrackingRepository.kt
@@ -126,8 +126,8 @@ class TrackingRepository(val context: Context) : ITrackingRepository<TrackingEED
             val iter = inputList.listIterator()
             var count = 0
             while (iter.hasNext() && count < itemCountToAdd) {
-                iter.remove()
                 iter.next()
+                iter.remove()
                 count++
             }
             // replacing old data
