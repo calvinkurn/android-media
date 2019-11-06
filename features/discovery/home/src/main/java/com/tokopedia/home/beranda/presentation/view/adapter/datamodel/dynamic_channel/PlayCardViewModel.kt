77bfcca0426a543bc3671601e810a544cb7b5412
isfaaghyth <isfaaghyth@gmail.com>
2019-11-06 16:48:39 +0700

    create visitable of viewModel of PlayCard

---
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/datamodel/dynamic_channel/PlayCardViewModel.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.home.beranda.presentation.view.adapter.datamodel.dynamic_channel
+
+import com.tokopedia.dynamicbanner.entity.PlayCardHome
+import com.tokopedia.home.beranda.presentation.view.adapter.HomeVisitable
+import com.tokopedia.home.beranda.presentation.view.adapter.factory.HomeTypeFactory
+
+class PlayCardViewModel: HomeVisitable<HomeTypeFactory> {
+    private var isCache: Boolean = false
+    private var trackingData: Map<String, Any>? = null
+    private var isCombined: Boolean = false
+    private var trackingDataForCombination: List<Any> = emptyList()
+    private var playCardHome: PlayCardHome = PlayCardHome()
+
+    override fun isCache(): Boolean {
+        return isCache
+    }
+
+    fun setCache(cache: Boolean) {
+        isCache = cache
+    }
+
+    fun setPlayCardHome(playCardHome: PlayCardHome) {
+        this.playCardHome = playCardHome
+    }
+
+    override fun type(typeFactory: HomeTypeFactory): Int {
+        return typeFactory.type(this)
+    }
+
+    override fun setTrackingData(trackingData: MutableMap<String, Any>) {
+        this.trackingData = trackingData
+    }
+
+    override fun getTrackingData(): Map<String, Any>? = trackingData
+
+    override fun getTrackingDataForCombination(): List<Any> = trackingDataForCombination
+
+    override fun setTrackingDataForCombination(list: List<Any>) {
+        this.trackingDataForCombination = list
+    }
+
+    override fun isTrackingCombined(): Boolean = isCombined
+
+    override fun setTrackingCombined(isCombined: Boolean) {
+        this.isCombined = isCombined
+    }
+}
