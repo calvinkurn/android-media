3d54598cbd03e97f57ddc508c1f9d615cf457f94
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-11-06 17:14:39 +0700

    Add search bar

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/autocomplete/AutoCompleteFragment.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/autocomplete/AutoCompleteFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.design.text.SearchInputView
 import com.tokopedia.logisticaddaddress.R
 
 class AutoCompleteFragment : Fragment() {
 
+    lateinit var searchInput: SearchInputView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_autocomplete, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchInput = view.findViewById(R.id.search_input_autocomplete)
+
+        val rvResults = view.findViewById<RecyclerView>(R.id.rv_autocomplete)
     }
 
     companion object {
