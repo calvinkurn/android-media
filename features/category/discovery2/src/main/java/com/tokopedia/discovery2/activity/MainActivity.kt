3013de6191802170297fa27353c222b65f4ed6ad
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2019-11-06 09:54:05 +0530

    androidx changes

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/activity/MainActivity.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/activity/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.tokopedia.discovery2.activity
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.tokopedia.discovery2.R
 
 class MainActivity : AppCompatActivity() {
