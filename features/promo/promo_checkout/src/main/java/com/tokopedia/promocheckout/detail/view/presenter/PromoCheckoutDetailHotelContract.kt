9d362004cd80dcbe5bd4f6265af3d29ae770eea0
mzennis <meyta.taliti@tokopedia.com>
2019-11-07 20:50:58 +0700

    remove unused import

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/detail/view/presenter/PromoCheckoutDetailHotelContract.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/detail/view/presenter/PromoCheckoutDetailHotelContract.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.promocheckout.detail.view.presenter
 
 import com.tokopedia.abstraction.base.view.presenter.CustomerPresenter
-import com.tokopedia.promocheckout.common.view.uimodel.PromoDigitalModel
 
 interface PromoCheckoutDetailHotelContract {
     interface Presenter : CustomerPresenter<PromoCheckoutDetailContract.View>{
