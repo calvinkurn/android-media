afca23870e5159400247301d70dfcee25f84450f
isfaaghyth <isfaaghyth@gmail.com>
2019-11-07 19:48:42 +0700

    created playCardHome's flag to make sure once request data and make image banner roundedable

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/dynamic_channel/PlayCardViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/dynamic_channel/PlayCardViewHolder.kt
@@ -7,12 +7,13 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.dynamicbanner.entity.PlayCard
+import com.tokopedia.dynamicbanner.entity.PlayCardHome
 import com.tokopedia.home.R
 import com.tokopedia.home.beranda.listener.HomeCategoryListener
 import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.dynamic_channel.PlayCardViewModel
-import com.tokopedia.kotlin.extensions.view.loadImageRounded
 import com.tokopedia.kotlin.extensions.view.showWithCondition
 
 class PlayCardViewHolder(val view: View, val listener: HomeCategoryListener): AbstractViewHolder<PlayCardViewModel>(view) {
@@ -23,19 +24,28 @@ class PlayCardViewHolder(val view: View, val listener: HomeCategoryListener): Ab
     private val chipPlayViewers = view.findViewById<LinearLayout>(R.id.chipPlayViewers)
     private val txtTotalViewers = view.findViewById<TextView>(R.id.txtTotalViewers)
 
+    private var playCardHome: PlayCardHome? = null
+
     override fun bind(element: PlayCardViewModel) {
-        listener.onGetPlayBanner(adapterPosition)
-        bindCard(element.getPlayCardHome().playGetCardHome.data.card)
-        rootBanner.setOnClickListener {
-            element.getChannel()?.enhanceClickPlayBanner
-            RouteManager.route(view.context, element.getPlayCardHome().playGetCardHome.data.card.applink)
+        element.getPlayCardHome()?.let { playCardHome ->
+            this.playCardHome = playCardHome
+
+            bindCard(playCardHome.playGetCardHome.data.card)
+            rootBanner.setOnClickListener {
+                element.getChannel()?.enhanceClickPlayBanner
+                RouteManager.route(view.context, playCardHome.playGetCardHome.data.card.applink)
+            }
+        }
+
+        if (playCardHome == null) {
+            listener.onGetPlayBanner(adapterPosition)
         }
     }
 
     private fun bindCard(card: PlayCard) {
-        imgBanner.loadImageRounded(card.imageUrl, 10f)
         chipLive(card)
         chipViewers(card)
+        ImageHandler.loadImageRounded(view.context, imgBanner, card.imageUrl, ROUNDED_RADIUS)
     }
 
     private fun chipLive(card: PlayCard) {
@@ -49,6 +59,8 @@ class PlayCardViewHolder(val view: View, val listener: HomeCategoryListener): Ab
 
     companion object {
         @LayoutRes val LAYOUT = R.layout.item_home_play_card
+
+        const val ROUNDED_RADIUS = 20f
     }
 
 }
