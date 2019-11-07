884ef5759046c9518ed24610b51fc97de5efeabc
isfaaghyth <isfaaghyth@gmail.com>
2019-11-07 10:44:21 +0700

    change usecase coroutines using observable

--- features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/domain/PlayCardHomeUseCase.kt
+++ features/home/dynamicbanner/src/main/java/com/tokopedia/dynamicbanner/domain/PlayCardHomeUseCase.kt
@@ -2,24 +2,24 @@ package com.tokopedia.dynamicbanner.domain
 
 import com.tokopedia.dynamicbanner.QUERY_PLAY_CARD
 import com.tokopedia.dynamicbanner.entity.PlayCardHome
-import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
-import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.usecase.coroutines.UseCase
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import javax.inject.Inject
 import javax.inject.Named
 
 class PlayCardHomeUseCase @Inject constructor(
         @Named(QUERY_PLAY_CARD) val query: String,
-        private val graphqlUseCase: MultiRequestGraphqlUseCase
-): UseCase<PlayCardHome>() {
+        repository: GraphqlRepository
+): GraphqlUseCase<PlayCardHome>(repository) {
 
-    override suspend fun executeOnBackground(): PlayCardHome {
-        val request = GraphqlRequest(query, PlayCardHome::class.java)
-        graphqlUseCase.clearRequest()
-        graphqlUseCase.addRequest(request)
-
-        val response = graphqlUseCase.executeOnBackground()
-        return response.getData<PlayCardHome>(PlayCardHome::class.java)
+    init {
+        setTypeClass(PlayCardHome::class.java)
+        setGraphqlQuery(query)
+        setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).apply {
+            setSessionIncluded(true)
+        }.build())
     }
 
 }
