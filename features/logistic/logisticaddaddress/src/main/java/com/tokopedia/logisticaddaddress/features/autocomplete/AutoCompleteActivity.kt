f8030470d3274135db32ce4690acce7c3f0d0595
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-11-07 13:43:33 +0700

    Fix toolbar view

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/autocomplete/AutoCompleteActivity.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/autocomplete/AutoCompleteActivity.kt
@@ -1,19 +1,37 @@
 package com.tokopedia.logisticaddaddress.features.autocomplete
 
-import androidx.fragment.app.Fragment
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.logisticaddaddress.R
 
-class AutoCompleteActivity : BaseSimpleActivity() {
+class AutoCompleteActivity : BaseActivity() {
 
-    override fun getNewFragment(): Fragment? {
-        return AutoCompleteFragment.newInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_autocomplete)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        supportActionBar?.setHomeAsUpIndicator(ContextCompat.getDrawable(this,
+                com.tokopedia.design.R.drawable.ic_close_default));
     }
-
-    override fun isShowCloseButton(): Boolean = true
 
     override fun onBackPressed() {
         super.onBackPressed()
-        overridePendingTransition(0,0)
+        overridePendingTransition(0, 0)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
