cb0292bb2b08ca428afd63b3588ba10890c55d82
Calvyn <calvyn.julian@tokopedia.com>
2019-11-09 22:00:59 +0700

    call get notification dashboard service on certain fragment

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/list/view/fragment/ProductManageFragment.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/list/view/fragment/ProductManageFragment.kt
@@ -42,6 +42,7 @@ import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
 import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.coachmark.CoachMarkBuilder
 import com.tokopedia.coachmark.CoachMarkItem
+import com.tokopedia.core.drawer2.service.DrawerGetNotificationService
 import com.tokopedia.design.bottomsheet.CloseableBottomSheetDialog
 import com.tokopedia.design.button.BottomActionView
 import com.tokopedia.design.component.ToasterError
@@ -325,12 +326,17 @@ open class ProductManageFragment : BaseSearchListFragment<ProductManageViewModel
     }
 
     override fun loadData(page: Int) {
-        productManagePresenter.getFreeClaim(GraphqlHelper.loadRawString(resources, R.raw.gql_get_deposit), userSession.shopId)
-        productManagePresenter.getGoldMerchantStatus()
-        productManagePresenter.getProductList(page, searchInputView.searchText,
-                productManageFilterModel.catalogProductOption, productManageFilterModel.conditionProductOption,
-                productManageFilterModel.etalaseProductOption, productManageFilterModel.pictureStatusOption,
-                sortProductOption, productManageFilterModel.categoryId)
+        context?.let {
+            if (GlobalConfig.isSellerApp()) {
+                DrawerGetNotificationService.startService(it, true)
+            }
+            productManagePresenter.getFreeClaim(GraphqlHelper.loadRawString(resources, R.raw.gql_get_deposit), userSession.shopId)
+            productManagePresenter.getGoldMerchantStatus()
+            productManagePresenter.getProductList(page, searchInputView.searchText,
+                    productManageFilterModel.catalogProductOption, productManageFilterModel.conditionProductOption,
+                    productManageFilterModel.etalaseProductOption, productManageFilterModel.pictureStatusOption,
+                    sortProductOption, productManageFilterModel.categoryId)
+        }
     }
 
     override fun renderList(list: MutableList<ProductManageViewModel>, hasNextPage: Boolean) {
