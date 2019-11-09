c2ab3c79665195d4f3fd98f4828550d17089b613
yoasfs <yoas.fs@gmail.com>
2019-11-09 23:03:30 +0700

    add common model

---
+++ features/content/interest_pick_common/src/main/java/com/tokopedia/interest_pick_common/view/viewmodel/InterestPickViewModel.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.interest_pick_common.view.viewmodel
+
+/**
+ * @author by yoasfs on 2019-11-09
+ */
+
+interface InterestPickViewModel {
+    var isEnableOnboarding: Boolean
+    val minimumPick: Int
+    val source: String
+    val titleIntro: String
+    val titleFull: String
+    val instruction: String
+    val buttonCta: String
+    var dataList: List<InterestPickDataViewModel>
+}
