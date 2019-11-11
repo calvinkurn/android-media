4ea6d3d182073a09f26699fba8cea8ab19b5c4be
Henry Priyono <henry.priyono@tokopedia.com>
2019-11-11 13:44:38 +0700

    Revert "Fix Crash Df Settingnotif"
    This reverts commit 69340f95b31c568e10785fff6e7905ed4a71665d.

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/activity/UserNotificationSettingActivity.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/activity/UserNotificationSettingActivity.kt
@@ -45,8 +45,4 @@ class UserNotificationSettingActivity : BaseSimpleActivity(),
                 .commit()
     }
 
-    override fun getParentViewResourceID() = com.tokopedia.abstraction.R.id.parent_view
-
-    override fun getLayoutRes() = com.tokopedia.abstraction.R.layout.activity_base_simple
-
 }
