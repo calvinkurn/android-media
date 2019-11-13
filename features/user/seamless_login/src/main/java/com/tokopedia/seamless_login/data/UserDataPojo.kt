ae556cf4c99309d8f34328b89bc447b83a2348f1
yorispr <yoris.prayogo@tokopedia.com>
2019-11-13 13:58:37 +0700

    create pojo

---
+++ features/user/seamless_login/src/main/java/com/tokopedia/seamless_login/data/UserDataPojo.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.seamless_login.data
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Yoris Prayogo on 2019-11-08.
+ * Copyright (c) 2019 PT. Tokopedia All rights reserved.
+ */
+
+data class UserDataPojo(
+        val date: String = "",
+
+        @SerializedName("content-md5")
+        val content_md5: String = "",
+
+        val authorization: String = "",
+
+        @SerializedName("x-method")
+        val method: String= "",
+
+        @SerializedName("accounts-authorization")
+        val accountAuth: String = ""
+)
