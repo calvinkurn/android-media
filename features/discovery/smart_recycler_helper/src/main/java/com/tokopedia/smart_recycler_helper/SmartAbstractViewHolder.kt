8b261c903bcdcb381588585ce3b8cc27a6c1a5e8
lukaskris12 <lukaskris12@gmail.com>
2019-11-13 10:56:57 +0700

    lib diff util

---
+++ features/discovery/smart_recycler_helper/src/main/java/com/tokopedia/smart_recycler_helper/SmartAbstractViewHolder.kt
@@ -0,0 +1,34 @@
+package com.tokopedia.smart_recycler_helper
+
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+
+abstract class SmartAbstractViewHolder<T : Visitable<*>>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    abstract fun bind(element: T, listener: SmartListener)
+
+    /*
+    This method is used to bind the view holder when only some parts of the model is changed.
+    This way, the view holder won't be fully bind, you can control which part of it to re-bind.
+    You can refer to https://medium.com/livefront/recyclerview-trick-selectively-bind-viewholders-with-payloads-4b28e3d2cce8
+    on how to use it.
+
+    Override this method to do the partial bind
+     */
+    open fun bind(element: T, listener: SmartListener, payloads: List<Any>) {}
+
+    /*
+    Override this to do recycle and clear image resources
+     */
+    fun onViewRecycled() {}
+
+    protected fun getString(@StringRes stringRes: Int): String {
+        return itemView.context.getString(stringRes)
+    }
+
+    protected fun getString(@StringRes stringRes: Int, value: String): String {
+        return itemView.context.getString(stringRes, value)
+    }
+}
