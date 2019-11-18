c4030020388ea651478b9c7f34eb80d77cf2e283
Calvyn <calvyn.julian@tokopedia.com>
2019-11-19 01:09:39 +0700

    Finish change get shop note detail to gql

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/note/di/module/ShopNoteModule.java
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/note/di/module/ShopNoteModule.java
@@ -3,6 +3,8 @@ package com.tokopedia.shop.note.di.module;
 import android.content.Context;
 
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+import com.tokopedia.abstraction.common.utils.GraphqlHelper;
+import com.tokopedia.shop.R;
 import com.tokopedia.shop.note.data.repository.ShopNoteRepositoryImpl;
 import com.tokopedia.shop.note.data.source.ShopNoteDataSource;
 import com.tokopedia.shop.note.di.scope.ShopNoteScope;
@@ -11,8 +13,12 @@ import com.tokopedia.shop.note.view.model.ShopNoteViewModel;
 import com.tokopedia.user.session.UserSession;
 import com.tokopedia.user.session.UserSessionInterface;
 
+import javax.inject.Named;
+
 import dagger.Module;
 import dagger.Provides;
+
+import static com.tokopedia.shop.product.data.GQLQueryConstant.SHOP_NOTES;
 
 @ShopNoteScope
 @Module
@@ -34,6 +40,13 @@ public class ShopNoteModule {
     @Provides
     public UserSessionInterface provideUserSessionInterface(@ApplicationContext Context context) {
         return new UserSession(context);
+    }
+
+    @ShopNoteScope
+    @Provides
+    @Named(SHOP_NOTES)
+    public String provideShopNotesQuery(@ApplicationContext Context context) {
+        return GraphqlHelper.loadRawString(context.getResources(), R.raw.gql_get_shop_notes_by_id);
     }
 }
 
