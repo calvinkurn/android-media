2dc9d783cee36093640d9c84224e3949c20ec7d9
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-11-18 11:06:03 +0700

    Create get chat setting use case

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/di/ChatSettingModule.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/di/ChatSettingModule.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatlist.data.ChatListQueriesConstant
+import com.tokopedia.topchat.chatsetting.data.GetChatSettingResponse
 import com.tokopedia.topchat.chatsetting.usecase.GetChatSettingUseCase
 import dagger.Module
 import dagger.Provides
@@ -29,16 +32,23 @@ class ChatSettingModule {
         return GraphqlHelper.loadRawString(context.resources, R.raw.query_get_chat_settings)
     }
 
+    @ChatSettingScope
     @Provides
-    fun provideMultiRequestGraphQlUseCase(): MultiRequestGraphqlUseCase {
-        return GraphqlInteractor.getInstance().multiRequestGraphqlUseCase
+    fun provideGraphQlRepository(): GraphqlRepository = GraphqlInteractor.getInstance().graphqlRepository
+
+    @ChatSettingScope
+    @Provides
+    fun provideGqlUseCase(graphqlRepository: GraphqlRepository): GraphqlUseCase<GetChatSettingResponse> {
+        return GraphqlUseCase(graphqlRepository)
     }
 
     @ChatSettingScope
     @Provides
-    fun provideFavorite(graphQlUseCase: MultiRequestGraphqlUseCase,
-                        @Named(ChatListQueriesConstant.QUERY_GET_CHAT_SETTING)
-                        gqlQuery: String): GetChatSettingUseCase {
-        return GetChatSettingUseCase(gqlQuery, graphQlUseCase)
+    fun provideFavorite(
+            @Named(ChatListQueriesConstant.QUERY_GET_CHAT_SETTING)
+            gqlQuery: String,
+            gqlUseCase: GraphqlUseCase<GetChatSettingResponse>
+    ): GetChatSettingUseCase {
+        return GetChatSettingUseCase(gqlQuery, gqlUseCase)
     }
 }
--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/viewmodel/ChatSettingViewModel.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/viewmodel/ChatSettingViewModel.kt
@@ -1,17 +1,31 @@
 package com.tokopedia.topchat.chatsetting.viewmodel
 
+import android.util.Log
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.topchat.chatsetting.data.GetChatSettingResponse
 import com.tokopedia.topchat.chatsetting.usecase.GetChatSettingUseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class ChatSettingViewModel @Inject constructor(
-        private val dispatcher: CoroutineDispatcher,
+        private val mainDispatcher: CoroutineDispatcher,
         private val getChatSettingUseCase: GetChatSettingUseCase
-) : BaseViewModel(dispatcher) {
+) : BaseViewModel(mainDispatcher) {
 
     fun loadChatSettings() {
+        getChatSettingUseCase.get(onSuccessGetChatSetting(), onErrorGetChatSetting())
+    }
 
+    private fun onSuccessGetChatSetting(): (GetChatSettingResponse) -> Unit {
+        return { response ->
+            Log.d("RESPONSE", "success")
+        }
+    }
+
+    private fun onErrorGetChatSetting(): (Throwable) -> Unit {
+        return { error ->
+            Log.d("RESPONSE", "error")
+        }
     }
 
 }
