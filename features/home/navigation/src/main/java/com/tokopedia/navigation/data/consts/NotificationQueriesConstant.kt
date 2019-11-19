08975a50889025eee27aedeab5711b5a82d121db
isfaaghyth <isfaaghyth@gmail.com>
2019-11-19 22:21:27 +0700

    moved consts notif queries onto data package

--- features/home/navigation/src/main/java/com/tokopedia/navigation/data/consts/NotificationQueriesConstant.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/data/consts/NotificationQueriesConstant.kt
@@ -1,7 +1,10 @@
-package com.tokopedia.navigation.presentation.di.notification
+package com.tokopedia.navigation.data.consts
 
 object NotificationQueriesConstant {
+    //query
     const val MUTATION_NOTIF_CENTER_PUSH_NOTIF = "notif_center_push_notif"
+    const val DRAWER_PUSH_NOTIFICATION = "drawer_push_notification"
+    const val QUERY_IS_TAB_UPDATE = "query_is_tab_update"
 
     const val PARAM_SECTION_TYPE = "section_type"
     const val PARAM_TEMPLATE_KEY = "template_key"
