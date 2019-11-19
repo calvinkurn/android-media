76b615764dfc4c97400831ad806bbc23ad4b76b8
darianthedytoped <darian.thedy@tokopedia.com>
2019-11-19 17:15:35 +0700

    Fix item decoration

--- features/discovery/similarsearch/src/main/java/com/tokopedia/similarsearch/SimilarSearchItemDecoration.kt
+++ features/discovery/similarsearch/src/main/java/com/tokopedia/similarsearch/SimilarSearchItemDecoration.kt
@@ -2,9 +2,10 @@ package com.tokopedia.similarsearch
 
 import android.graphics.Rect
 import android.view.View
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.tokopedia.productcard.v2.ProductCardView
 import kotlin.math.cos
 import kotlin.math.roundToInt
 
@@ -13,13 +14,13 @@ internal class SimilarSearchItemDecoration(
 ): RecyclerView.ItemDecoration() {
 
     private val allowedViewTypes = listOf(
-            R.layout.similar_search_product_card_layout
+            0
     )
 
     private var horizontalCardViewOffset: Int = 0
     private var verticalCardViewOffset: Int = 0
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State) {
         val position = parent.getChildAdapterPosition(view)
 
         if (isProductItem(parent, position)) {
@@ -29,9 +30,9 @@ internal class SimilarSearchItemDecoration(
             horizontalCardViewOffset = getHorizontalCardViewOffset(view)
             verticalCardViewOffset = getVerticalCardViewOffset(view)
 
-            outRect.left = getLeftOffset()
+            outRect.left = getLeftOffset(relativePosition, totalSpanCount)
             outRect.top = getTopOffset(parent, position, relativePosition, totalSpanCount)
-            outRect.right = getRightOffset()
+            outRect.right = getRightOffset(relativePosition, totalSpanCount)
             outRect.bottom = getBottomOffset()
         }
     }
@@ -45,10 +46,7 @@ internal class SimilarSearchItemDecoration(
         return parent.adapter?.let { getRecyclerViewViewTypeIfAdapterNotNull(viewPosition, it) } ?: -1
     }
 
-    private fun getRecyclerViewViewTypeIfAdapterNotNull(
-            viewPosition: Int,
-            adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
-    ): Int {
+    private fun getRecyclerViewViewTypeIfAdapterNotNull(viewPosition: Int, adapter: Adapter<ViewHolder>): Int {
         return if (viewPosition < 0 || viewPosition > adapter.itemCount - 1) {
             -1
         } else {
@@ -57,6 +55,20 @@ internal class SimilarSearchItemDecoration(
     }
 
     private fun getProductItemRelativePosition(parent: RecyclerView, view: View): Int {
+        return if (view.layoutParams is StaggeredGridLayoutManager.LayoutParams) {
+            getProductItemRelativePositionStaggeredGrid(view)
+        }
+        else {
+            getProductItemRelativePositionNotStaggeredGrid(parent, view)
+        }
+    }
+
+    private fun getProductItemRelativePositionStaggeredGrid(view: View): Int {
+        val staggeredGridLayoutParams = view.layoutParams as StaggeredGridLayoutManager.LayoutParams
+        return staggeredGridLayoutParams.spanIndex
+    }
+
+    private fun getProductItemRelativePositionNotStaggeredGrid(parent: RecyclerView, view: View): Int {
         val absolutePos = parent.getChildAdapterPosition(view)
         var firstProductItemPos = absolutePos
         while (isProductItem(parent, firstProductItemPos - 1)) firstProductItemPos--
@@ -74,9 +86,9 @@ internal class SimilarSearchItemDecoration(
     }
 
     private fun getHorizontalCardViewOffset(view: View): Int {
-        if (view is CardView) {
-            val maxElevation = view.maxCardElevation
-            val radius = view.radius
+        if (view is ProductCardView) {
+            val maxElevation = view.getCardViewMaxElevation()
+            val radius = view.getCardViewRadius()
 
             return (maxElevation + (1 - cos(45.0)) * radius).toFloat().roundToInt() / 2
         }
@@ -85,9 +97,9 @@ internal class SimilarSearchItemDecoration(
     }
 
     private fun getVerticalCardViewOffset(view: View): Int {
-        if (view is CardView) {
-            val maxElevation = view.maxCardElevation
-            val radius = view.radius
+        if (view is ProductCardView) {
+            val maxElevation = view.getCardViewMaxElevation()
+            val radius = view.getCardViewRadius()
 
             return (maxElevation * 1.5 + (1 - cos(45.0)) * radius).toFloat().roundToInt() / 2
         }
@@ -95,24 +107,52 @@ internal class SimilarSearchItemDecoration(
         return 0
     }
 
-    private fun getLeftOffset(): Int {
+    private fun getLeftOffset(relativePos: Int, totalSpanCount: Int): Int {
+        return if (isFirstInRow(relativePos, totalSpanCount)) getLeftOffsetFirstInRow() else getLeftOffsetNotFirstInRow()
+    }
+
+    private fun isFirstInRow(relativePos: Int, spanCount: Int): Boolean {
+        return relativePos % spanCount == 0
+    }
+
+    private fun getLeftOffsetFirstInRow(): Int {
         return margin - horizontalCardViewOffset
     }
 
-    private fun getTopOffset(parent: RecyclerView, absolutePos: Int, relativePos: Int, totalSpanCount: Int): Int {
-        return getTopOffsetWithCardViewOffset(parent, absolutePos, relativePos, totalSpanCount) - verticalCardViewOffset
+    private fun getLeftOffsetNotFirstInRow(): Int {
+        return (margin / 4) - horizontalCardViewOffset
     }
 
-    private fun getTopOffsetWithCardViewOffset(parent: RecyclerView, absolutePos: Int, relativePos: Int, totalSpanCount: Int): Int {
-        return if (isTopProductItem(parent, absolutePos, relativePos, totalSpanCount)) margin else margin / 4
+    private fun getTopOffset(parent: RecyclerView, absolutePos: Int, relativePos: Int, totalSpanCount: Int): Int {
+        return if (isTopProductItem(parent, absolutePos, relativePos, totalSpanCount)) getTopOffsetTopItem() else getTopOffsetNotTopItem()
     }
 
     private fun isTopProductItem(parent: RecyclerView, absolutePos: Int, relativePos: Int, totalSpanCount: Int): Boolean {
         return !isProductItem(parent, absolutePos - relativePos % totalSpanCount - 1)
     }
 
-    private fun getRightOffset(): Int {
+    private fun getTopOffsetTopItem(): Int {
+        return margin - verticalCardViewOffset
+    }
+
+    private fun getTopOffsetNotTopItem(): Int {
+        return margin / 4 - verticalCardViewOffset
+    }
+
+    private fun getRightOffset(relativePos: Int, totalSpanCount: Int): Int {
+        return if (isLastInRow(relativePos, totalSpanCount)) getRightOffsetLastInRow() else getRightOffsetNotLastInRow()
+    }
+
+    private fun isLastInRow(relativePos: Int, spanCount: Int): Boolean {
+        return relativePos % spanCount == spanCount - 1
+    }
+
+    private fun getRightOffsetLastInRow(): Int {
         return margin - horizontalCardViewOffset
+    }
+
+    private fun getRightOffsetNotLastInRow(): Int {
+        return (margin / 4) - horizontalCardViewOffset
     }
 
     private fun getBottomOffset(): Int {
