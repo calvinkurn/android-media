4adb726c13f15ca9c5c3623821e2b5586abbe543
yehezkiel <yehezkiel@tokopedia.com>
2019-11-19 16:19:52 +0700

    add tracker and error page hotfix

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMatcher.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMatcher.kt
@@ -19,6 +19,7 @@ import com.tokopedia.applink.DeepLinkChecker.OTHER
 import com.tokopedia.applink.DeepLinkChecker.PELUANG
 import com.tokopedia.applink.DeepLinkChecker.PLAY
 import com.tokopedia.applink.DeepLinkChecker.PRODUCT
+import com.tokopedia.applink.DeepLinkChecker.PRODUCT_REVIEW
 import com.tokopedia.applink.DeepLinkChecker.PROFILE
 import com.tokopedia.applink.DeepLinkChecker.PROMO_DETAIL
 import com.tokopedia.applink.DeepLinkChecker.PROMO_LIST
@@ -27,7 +28,6 @@ import com.tokopedia.applink.DeepLinkChecker.RECOMMENDATION
 import com.tokopedia.applink.DeepLinkChecker.REFERRAL
 import com.tokopedia.applink.DeepLinkChecker.SALE
 import com.tokopedia.applink.DeepLinkChecker.SHOP
-import com.tokopedia.applink.DeepLinkChecker.SIMILAR_PRODUCT
 import com.tokopedia.applink.DeepLinkChecker.SMCREFERRAL
 import com.tokopedia.applink.DeepLinkChecker.TOKOPOINT
 import com.tokopedia.applink.DeepLinkChecker.TOPPICKS
@@ -83,6 +83,7 @@ class DeeplinkMatcher() {
             add(Pattern(EQ, 1, mapOf(0 to "hotel")) to HOTEL)
             add(Pattern(EQ, 2, mapOf(0 to "rekomendasi")) to RECOMMENDATION)
             add(Pattern(EQ, 1, mapOf(0 to "rekomendasi")) to RECOMMENDATION)
+            add(Pattern(EQ, 4, mapOf(0 to "product-review")) to PRODUCT_REVIEW)
             add(Pattern(EQ, 1, null) to SHOP)
             add(Pattern(EQ, 2, null) to PRODUCT)
         }
