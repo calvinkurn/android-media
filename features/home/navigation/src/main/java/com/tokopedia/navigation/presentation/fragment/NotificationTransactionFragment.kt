b6060994a7b8111ea03beb3461a383b48f05bfa4
isfaaghyth <isfaaghyth@gmail.com>
2019-11-19 22:19:58 +0700

    get notif data with own view model on transaction

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/NotificationTransactionFragment.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/NotificationTransactionFragment.kt
@@ -1,22 +1,62 @@
 package com.tokopedia.navigation.presentation.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.fragment.BaseListFragment
 import com.tokopedia.navigation.R
+import com.tokopedia.navigation.data.consts.buyerMenu
+import com.tokopedia.navigation.data.consts.sellerMenu
+import com.tokopedia.navigation.data.mapper.NotificationMapper
 import com.tokopedia.navigation.presentation.adapter.NotificationTransactionAdapter
 import com.tokopedia.navigation.presentation.adapter.typefactory.NotificationTransactionFactory
 import com.tokopedia.navigation.presentation.adapter.typefactory.NotificationTransactionFactoryImpl
+import com.tokopedia.navigation.presentation.di.notification.DaggerNotificationTransactionComponent
+import com.tokopedia.navigation.presentation.viewmodel.NotificationTransactionViewModel
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
+import javax.inject.Inject
 
 class NotificationTransactionFragment: BaseListFragment<Visitable<*>, BaseAdapterTypeFactory>() {
 
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModelFragmentProvider by lazy {
+        ViewModelProviders.of(this, viewModelFactory)
+    }
+
+    private val viewModel by lazy {
+        viewModelFragmentProvider.get(NotificationTransactionViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_notification_transaction, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getNotification()
+        viewModel.notification.observe(this, Observer {
+            when(it) {
+                is Success -> {
+                    if (NotificationMapper.isHasShop(it.data)) {
+                        adapter.addMoreData(sellerMenu())
+                    }
+                }
+                is Fail -> {
+
+                }
+            }
+        })
     }
 
     override fun getSwipeRefreshLayoutResourceId(): Int = R.id.swipe_refresh
@@ -35,10 +75,16 @@ class NotificationTransactionFragment: BaseListFragment<Visitable<*>, BaseAdapte
         return NotificationTransactionAdapter(typeFactory)
     }
 
-    override fun initInjector() {}
+    override fun initInjector() {
+        DaggerNotificationTransactionComponent.builder()
+                .baseAppComponent((activity?.application as BaseMainApplication).baseAppComponent)
+                .build()
+                .inject(this)
+    }
 
     override fun loadData(page: Int) {
-        renderList(TestData.getData(context), false)
+        renderList(buyerMenu(), false)
+        (adapter as NotificationTransactionAdapter).updateValue()
     }
 
     override fun onItemClicked(t: Visitable<*>?) {}
