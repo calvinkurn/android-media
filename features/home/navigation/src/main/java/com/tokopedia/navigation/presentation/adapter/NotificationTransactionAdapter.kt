60f1a544b1f80fae6766021e524e41f96ef62426
isfaaghyth <isfaaghyth@gmail.com>
2019-11-19 22:16:57 +0700

    test: add update value on adapter

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/NotificationTransactionAdapter.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/NotificationTransactionAdapter.kt
@@ -3,8 +3,27 @@ package com.tokopedia.navigation.presentation.adapter
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.navigation.GlobalNavConstant.MENUNGGU_KONFIRMASI
+import com.tokopedia.navigation.domain.model.DrawerNotification
+import com.tokopedia.navigation.domain.model.PurchaseNotification
 import com.tokopedia.navigation.presentation.adapter.typefactory.NotificationTransactionFactoryImpl
 
 class NotificationTransactionAdapter(
         notificationFactory: NotificationTransactionFactoryImpl
-): BaseListAdapter<Visitable<*>, BaseAdapterTypeFactory>(notificationFactory)
+): BaseListAdapter<Visitable<*>, BaseAdapterTypeFactory>(notificationFactory) {
+
+    fun updateValue() {
+        for (item in data) {
+            if (item is PurchaseNotification) {
+                val childs = item.childs
+                for (child in childs) {
+                    if (child.id == MENUNGGU_KONFIRMASI) {
+                        child.title = "HAHAHA"
+                    }
+                }
+            }
+        }
+        notifyDataSetChanged()
+    }
+
+}
