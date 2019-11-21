297e56572bce847533ee650d01c54e92397856d2
yoasfs <yoas.fs@gmail.com>
2019-11-21 15:15:23 +0700

    fix resources error 3 part 3

--- features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/custom/AffiliateDashboardTab.kt
+++ features/content/affiliate/src/main/java/com/tokopedia/affiliate/feature/dashboard/view/custom/AffiliateDashboardTab.kt
@@ -43,8 +43,8 @@ class AffiliateDashboardTab : LinearLayout {
         tvTitle.setTextColor(
                 MethodChecker.getColor(
                         context,
-                        if (isActive) com.tokopedia.design.R.color.Green_G500
-                        else com.tokopedia.design.R.color.Neutral_N700_44
+                        if (isActive) com.tokopedia.unifyprinciples.R.color.Green_G500
+                        else com.tokopedia.unifyprinciples.R.color.Neutral_N700_44
                 )
         )
     }
