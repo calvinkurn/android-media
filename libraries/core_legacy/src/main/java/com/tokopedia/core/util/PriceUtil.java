04b04111dba256194f293f58d999ba88d35eec08
M normansyah <noiz354@gmail.com>
2019-11-21 12:02:12 +0700

    fix price in analytic.

--- libraries/core_legacy/src/main/java/com/tokopedia/core/util/PriceUtil.java
+++ libraries/core_legacy/src/main/java/com/tokopedia/core/util/PriceUtil.java
@@ -2,14 +2,25 @@ package com.tokopedia.core.util;
 
 import android.text.TextUtils;
 
+import com.tokopedia.design.utils.CurrencyFormatHelper;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PriceUtil {
     public static String from(String price){
+        price = CurrencyFormatHelper.removeCurrencyPrefix(price);
         if (!TextUtils.isEmpty(price)) {
-            price = price.replaceAll("[^\\d]", "");
-            return price;
+            try {
+                long result = new DecimalFormat("#,#", new DecimalFormatSymbols(new Locale("in", "ID"))).parse(price).longValue();
+                return Long.valueOf(result).toString();
+            }catch (ParseException pe){
+                return "0";
+            }
         } else {
             return "0";
         }
