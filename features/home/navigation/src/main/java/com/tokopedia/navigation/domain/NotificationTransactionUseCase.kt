d90596eac6355795ac3e19039d183e54fffa4622
isfaaghyth <isfaaghyth@gmail.com>
2019-11-21 17:49:29 +0700

    created of usecase of notif transaction

--- features/home/navigation/src/main/java/com/tokopedia/navigation/domain/NotificationTransactionUseCase.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/domain/NotificationTransactionUseCase.kt
@@ -2,29 +2,42 @@ package com.tokopedia.navigation.domain
 
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.navigation.data.consts.NotificationQueriesConstant
-import com.tokopedia.navigation.data.entity.NotificationEntity
-import com.tokopedia.usecase.RequestParams
+import com.tokopedia.navigation.domain.pojo.NotificationCenterDetail
 import javax.inject.Inject
 import javax.inject.Named
 
 class NotificationTransactionUseCase @Inject constructor(
-        @Named(NotificationQueriesConstant.DRAWER_PUSH_NOTIFICATION)
+        @Named(NotificationQueriesConstant.TRANSACTION_NOTIFICATION)
         private val query: String,
-        private val useCase: GraphqlUseCase<NotificationEntity>
-) {
+        private val useCase: GraphqlUseCase<NotificationCenterDetail>) {
 
-    private val params = RequestParams.EMPTY
-
-    fun get(onSuccess: (NotificationEntity) -> Unit, onError: (Throwable) -> Unit) {
+    fun get(requestParams: Map<String, Any>,
+            onSuccess: (NotificationCenterDetail) -> Unit,
+            onError: (Throwable) -> Unit) {
         useCase.apply {
-            setTypeClass(NotificationEntity::class.java)
-            setRequestParams(params.parameters)
+            setTypeClass(NotificationCenterDetail::class.java)
+            setRequestParams(requestParams)
             setGraphqlQuery(query)
             execute({ result ->
                 onSuccess(result)
             }, { error ->
                 onError(error)
             })
+        }
+    }
+
+    companion object {
+        private const val PARAM_PAGE = "page"
+        private const val PARAM_LAST_ID = "lastNotifId"
+
+        fun params(
+                page: Int,
+                variables: HashMap<String, Any>,
+                lastNotifId: String
+        ): HashMap<String, Any> {
+            variables[PARAM_PAGE] = page
+            variables[PARAM_LAST_ID] = lastNotifId
+            return variables
         }
     }
 
