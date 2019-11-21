2f09ef3d48cf8c84cb6357c2dd1a323f2bb374fc
isfaaghyth <isfaaghyth@gmail.com>
2019-11-21 17:48:56 +0700

    provider a new one usecase of notif transaction

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/di/notification/module/NotificationTransactionModule.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/di/notification/module/NotificationTransactionModule.kt
@@ -4,7 +4,10 @@ import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.navigation.data.consts.NotificationQueriesConstant
 import com.tokopedia.navigation.data.entity.NotificationEntity
+import com.tokopedia.navigation.domain.NotificationInfoTransactionUseCase
 import com.tokopedia.navigation.domain.NotificationTransactionUseCase
+import com.tokopedia.navigation.domain.model.TransactionItemNotification
+import com.tokopedia.navigation.domain.pojo.NotificationCenterDetail
 import com.tokopedia.navigation.presentation.di.notification.scope.NotificationTransactionScope
 import dagger.Module
 import dagger.Provides
@@ -15,17 +18,33 @@ class NotificationTransactionModule {
 
     @Provides
     @NotificationTransactionScope
-    fun provideGraphqlNotifTransactionUseCase(
+    fun provideGraphqlInfoNotifTransactionUseCase(
             repository: GraphqlRepository): GraphqlUseCase<NotificationEntity> {
         return GraphqlUseCase(repository)
     }
 
     @Provides
     @NotificationTransactionScope
-    fun provideNotificationTransactionUseCase(
+    fun provideGraphqlNotifTransactionUseCase(
+            repository: GraphqlRepository): GraphqlUseCase<NotificationCenterDetail> {
+        return GraphqlUseCase(repository)
+    }
+
+    @Provides
+    @NotificationTransactionScope
+    fun provideInfoNotificationTransactionUseCase(
             @Named(NotificationQueriesConstant.DRAWER_PUSH_NOTIFICATION)
             query: String,
-            useCase: GraphqlUseCase<NotificationEntity>): NotificationTransactionUseCase {
+            useCase: GraphqlUseCase<NotificationEntity>): NotificationInfoTransactionUseCase {
+        return NotificationInfoTransactionUseCase(query, useCase)
+    }
+
+    @Provides
+    @NotificationTransactionScope
+    fun provideNotificationTransactionUseCase(
+            @Named(NotificationQueriesConstant.TRANSACTION_NOTIFICATION)
+            query: String,
+            useCase: GraphqlUseCase<NotificationCenterDetail>): NotificationTransactionUseCase {
         return NotificationTransactionUseCase(query, useCase)
     }
 
