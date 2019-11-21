113abe26971e11c49b2d87e89d1651aca68f3b84
isfaaghyth <isfaaghyth@gmail.com>
2019-11-21 15:15:51 +0700

    created usecase of notification transaction

---
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/domain/NotificationTransactionUseCase.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.navigation.domain
+
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.navigation.data.consts.NotificationQueriesConstant
+import com.tokopedia.navigation.data.entity.NotificationEntity
+import com.tokopedia.usecase.RequestParams
+import javax.inject.Inject
+import javax.inject.Named
+
+class NotificationTransactionUseCase @Inject constructor(
+        @Named(NotificationQueriesConstant.DRAWER_PUSH_NOTIFICATION)
+        private val query: String,
+        private val useCase: GraphqlUseCase<NotificationEntity>
+) {
+
+    private val params = RequestParams.EMPTY
+
+    fun get(onSuccess: (NotificationEntity) -> Unit, onError: (Throwable) -> Unit) {
+        useCase.apply {
+            setTypeClass(NotificationEntity::class.java)
+            setRequestParams(params.parameters)
+            setGraphqlQuery(query)
+            execute({ result ->
+                onSuccess(result)
+            }, { error ->
+                onError(error)
+            })
+        }
+    }
+
+}
