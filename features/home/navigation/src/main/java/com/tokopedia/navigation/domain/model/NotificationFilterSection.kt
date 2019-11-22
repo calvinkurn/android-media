6889dfa9437697f9af2d44a280fbea7a38be43b9
isfaaghyth <isfaaghyth@gmail.com>
2019-11-22 10:55:29 +0700

    created notification filter section model

---
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/domain/model/NotificationFilterSection.kt
@@ -0,0 +1,50 @@
+package com.tokopedia.navigation.domain.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.navigation.presentation.adapter.typefactory.NotificationTransactionFactory
+import com.tokopedia.navigation.presentation.view.viewmodel.NotificationUpdateFilterSectionItemViewModel
+
+class NotificationFilterSection(
+        var filterType: String = "",
+        var title: String = "",
+        var list: List<NotificationUpdateFilterSectionItemViewModel> = arrayListOf()
+) : Visitable<NotificationTransactionFactory>, Parcelable {
+
+    enum class FilterType(val type: String) {
+        TYPE_ID("typeId"),
+        TAG_ID("tagId")
+    }
+
+    override fun type(typeFactory: NotificationTransactionFactory): Int {
+        return typeFactory.type(this)
+    }
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.createTypedArrayList(NotificationUpdateFilterSectionItemViewModel.CREATOR))
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(filterType)
+        parcel.writeString(title)
+        parcel.writeTypedList(list)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<NotificationFilterSection> {
+        override fun createFromParcel(parcel: Parcel): NotificationFilterSection {
+            return NotificationFilterSection(parcel)
+        }
+
+        override fun newArray(size: Int): Array<NotificationFilterSection?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
+}
