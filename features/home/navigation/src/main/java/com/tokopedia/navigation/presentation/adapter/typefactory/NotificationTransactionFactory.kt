889aebe00341948e4ebdec14ce36c8d417d15343
isfaaghyth <isfaaghyth@gmail.com>
2019-11-22 10:56:50 +0700

    added factory for notif filter

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/typefactory/NotificationTransactionFactory.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/typefactory/NotificationTransactionFactory.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.navigation.presentation.adapter.typefactory
 
+import com.tokopedia.navigation.domain.model.NotificationFilterSection
 import com.tokopedia.navigation.domain.model.PurchaseNotification
 import com.tokopedia.navigation.domain.model.SaleNotification
 import com.tokopedia.navigation.domain.model.TransactionItemNotification
@@ -8,4 +9,5 @@ interface NotificationTransactionFactory {
     fun type(purchaseNotification: PurchaseNotification): Int
     fun type(saleNotification: SaleNotification): Int
     fun type(notification: TransactionItemNotification): Int
+    fun type(filter: NotificationFilterSection): Int
 }
--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/typefactory/NotificationTransactionFactoryImpl.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/adapter/typefactory/NotificationTransactionFactoryImpl.kt
@@ -4,18 +4,13 @@ import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.navigation.domain.model.NotificationFilterSection
 import com.tokopedia.navigation.domain.model.PurchaseNotification
 import com.tokopedia.navigation.domain.model.SaleNotification
 import com.tokopedia.navigation.domain.model.TransactionItemNotification
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.BigBannerNotificationViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.ProductRecomNotificationViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.PurchaseViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.SaleViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.SmallBannerNotificationViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.TextNotificationViewHolder
-import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.WishListNotificationViewHolder
+import com.tokopedia.navigation.presentation.adapter.viewholder.transaction.*
 import com.tokopedia.navigation.presentation.view.listener.NotificationTransactionItemListener
-import com.tokopedia.navigation.presentation.view.listener.NotificationUpdateItemListener
+import com.tokopedia.navigation.presentation.view.viewmodel.NotificationUpdateFilterItemViewModel
 import com.tokopedia.navigation.presentation.view.viewmodel.NotificationUpdateItemViewModel
 
 class NotificationTransactionFactoryImpl(
@@ -43,6 +38,10 @@ class NotificationTransactionFactoryImpl(
         }
     }
 
+    override fun type(filter: NotificationFilterSection): Int {
+        return NotificationFilterViewHolder.LAYOUT
+    }
+
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when (type) {
             PurchaseViewHolder.LAYOUT -> PurchaseViewHolder(parent)
@@ -52,6 +51,7 @@ class NotificationTransactionFactoryImpl(
             BigBannerNotificationViewHolder.LAYOUT -> BigBannerNotificationViewHolder(parent, notificationUpdateListener)
             ProductRecomNotificationViewHolder.LAYOUT -> ProductRecomNotificationViewHolder(parent, notificationUpdateListener)
             WishListNotificationViewHolder.LAYOUT -> WishListNotificationViewHolder(parent, notificationUpdateListener)
+            NotificationFilterViewHolder.LAYOUT -> NotificationFilterViewHolder(parent)
             else -> super.createViewHolder(parent, type)
         }
     }
