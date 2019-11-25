a36e7010ccd720e318972b7ee469fbcb870a6253
hansenpw <hansen.wijaya@tokopedia.com>
2019-11-25 10:07:50 +0700

    remove unintended commit

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartFragment.java
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartFragment.java
@@ -126,6 +126,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
