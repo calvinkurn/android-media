b1e5588a61e83fdbaca0aee485b2cd19a6cd6c6b
isfaaghyth <isfaaghyth@gmail.com>
2019-11-26 19:05:28 +0700

    added analytics on common DI module

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/di/notification/module/CommonModule.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/di/notification/module/CommonModule.kt
@@ -3,6 +3,7 @@ package com.tokopedia.navigation.presentation.di.notification.module
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.navigation.analytics.NotificationTransactionAnalytics
 import com.tokopedia.navigation.util.coroutines.AppDispatcherProvider
 import com.tokopedia.navigation.util.coroutines.DispatcherProvider
 import dagger.Module
@@ -20,5 +21,8 @@ import dagger.Provides
 
     @Provides
     fun provideDispatchers(): DispatcherProvider = AppDispatcherProvider()
+
+    @Provides
+    fun provideTransactionAnalytics() = NotificationTransactionAnalytics()
 
 }
