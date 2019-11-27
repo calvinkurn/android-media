c531ae88fe7314b89d3795cf070083d5241b4a0a
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-11-28 00:11:24 +0700

    Create zipcode usecase

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/domain/usecase/GetZipCodeUseCase.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/domain/usecase/GetZipCodeUseCase.kt
@@ -2,28 +2,22 @@ package com.tokopedia.logisticaddaddress.domain.usecase
 
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.domain.GraphqlUseCase
-import com.tokopedia.logisticaddaddress.di.RawQueryConstant
 import com.tokopedia.logisticaddaddress.domain.executor.SchedulerProvider
 import com.tokopedia.logisticaddaddress.domain.model.district_recommendation.DistrictRecommendationResponse
 import com.tokopedia.network.exception.MessageErrorException
 import rx.Observable
 import javax.inject.Inject
 
-class GetDistrictRecommendation @Inject constructor(
-        val queryMap: Map<String, String>,
-        val gql: GraphqlUseCase,
-        val scheduler: SchedulerProvider) {
+class GetZipCodeUseCase
+@Inject constructor(val gql: GraphqlUseCase, val scheduler: SchedulerProvider) {
 
-    private val gqlQuery: String = queryMap[RawQueryConstant.GET_DISTRICT_RECOMMENDATION]
-            ?: throw QueryNotFoundException()
-
-    fun execute(query: String, page: Int): Observable<DistrictRecommendationResponse> {
-        val param: Map<String, String> = mapOf(
-                "query" to query,
-                "page" to page.toString()
+    fun execute(districtId: String): Observable<DistrictRecommendationResponse> {
+        val param = mapOf(
+                "query" to districtId,
+                "page" to "1"
         )
-        val gqlRequest = GraphqlRequest(gqlQuery, DistrictRecommendationResponse::class.java, param)
-
+        val gqlRequest = GraphqlRequest(query_kero_get_district_details,
+                DistrictRecommendationResponse::class.java, param)
         gql.clearRequest()
         gql.addRequest(gqlRequest)
         return gql.getExecuteObservable(null)
@@ -38,8 +32,22 @@ class GetDistrictRecommendation @Inject constructor(
                 .observeOn(scheduler.ui())
     }
 
-    fun unsubscribe() {
-        gql.unsubscribe()
-    }
-
 }
+
+val query_kero_get_district_details = """
+query KeroDistrictQuery(${'$'}query: String, ${'$'}page: String){
+  kero_get_district_details(query: ${'$'}query, page: ${'$'}page) {
+    district {
+      district_id
+      district_name
+      city_id
+      city_name
+      province_id
+      province_name
+      zip_code
+    }
+    next_available
+  }
+}
+""".trimIndent()
+
