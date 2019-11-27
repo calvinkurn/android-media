52b885d46b5d213b96f985d54b92252f6e10510f
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2019-11-27 15:36:25 +0700

    Implement logic in view holder

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/adapter/AddressViewHolder.java
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/adapter/AddressViewHolder.java
@@ -1,9 +1,12 @@
 package com.tokopedia.logisticaddaddress.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.LayoutRes;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,11 +18,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder;
 import com.tokopedia.logisticaddaddress.R;
+import com.tokopedia.unifyprinciples.Typography;
 
 /**
  * Created by Fajar Ulin Nuha on 15/10/18.
  */
-public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> implements OnMapReadyCallback {
+public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> {
 
     private ManageAddressListener listener;
 
@@ -39,12 +43,9 @@ public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> impl
 
     private TextView addressName;
     private TextView addressDetail;
-    private MapView mapView;
-    private View editBtn, deleteBtn, defaultBtn, layoutMap, noLocationLabel, bottomView;
-    private GoogleMap googleMap;
-
-    private String mCurrentLat = DEFAULT_LATITUDE;
-    private String mCurrentLong = DEFAULT_LONGITUDE;
+    private View editBtn, deleteBtn, defaultBtn;
+    private Typography locationDesc;
+    private ImageView locationImg;
 
     @LayoutRes
     public static final int LAYOUT = R.layout.logistic_item_manage_people_address;
@@ -54,19 +55,11 @@ public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> impl
         this.listener = listener;
         addressName = itemView.findViewById(R.id.address_name);
         addressDetail = itemView.findViewById(R.id.address_detail);
-        mapView = itemView.findViewById(R.id.mapView);
         editBtn = itemView.findViewById(R.id.action_edit);
         deleteBtn = itemView.findViewById(R.id.action_delete);
         defaultBtn = itemView.findViewById(R.id.action_default);
-        layoutMap = itemView.findViewById(R.id.layout);
-        noLocationLabel = itemView.findViewById(R.id.label_no_location);
-        bottomView = itemView.findViewById(R.id.view_bottom);
-
-        if (mapView != null) {
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
+        locationDesc = itemView.findViewById(R.id.tv_pinpoint_state);
+        locationImg = itemView.findViewById(R.id.img_location_state);
     }
 
     @Override
@@ -74,16 +67,7 @@ public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> impl
         setVisibility(element);
         addressName.setText(element.getAddressName());
         addressDetail.setText(element.getAddressFull());
-        mCurrentLat = element.getLatitude();
-        mCurrentLong = element.getLongitude();
-        setGoogleMap();
         setListener(element);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-        setGoogleMap();
     }
 
     private void setVisibility(AddressViewModel viewModel) {
@@ -95,9 +79,15 @@ public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> impl
 
         if ((viewModel.getLatitude() == null || viewModel.getLatitude().isEmpty())
                 || (viewModel.getLongitude() == null || viewModel.getLongitude().isEmpty())) {
-            noLocationLabel.setVisibility(View.VISIBLE);
+            Drawable icon = ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_no_pinpoint);
+            locationImg.setImageDrawable(icon);
+            locationDesc.setTextColor(ContextCompat.getColor(itemView.getContext(), R.color.ic_disable_pinpoint));
+            locationDesc.setText(itemView.getContext().getString(R.string.no_pinpoint));
         } else {
-            noLocationLabel.setVisibility(View.INVISIBLE);
+            Drawable icon = ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_pinpoint_green);
+            locationImg.setImageDrawable(icon);
+            locationDesc.setTextColor(ContextCompat.getColor(itemView.getContext(), R.color.green_200));
+            locationDesc.setText(itemView.getContext().getString(R.string.havent_pinpoint));
         }
 
     }
@@ -106,49 +96,5 @@ public class AddressViewHolder extends AbstractViewHolder<AddressViewModel> impl
         editBtn.setOnClickListener(view -> listener.setActionEditButton(viewModel));
         deleteBtn.setOnClickListener(view -> listener.setActionDeleteButton(viewModel));
         defaultBtn.setOnClickListener(view -> listener.setActionDefaultButtonClicked(viewModel));
-    }
-
-    private void setGoogleMap() {
-        if (googleMap != null) {
-            layoutMap.setVisibility(View.VISIBLE);
-            double latitude = getLatitude();
-            double longitude = getLongitude();
-            LatLng latLng = new LatLng(latitude, longitude);
-
-            googleMap.getUiSettings().setMapToolbarEnabled(false);
-            googleMap.addMarker(
-                    new MarkerOptions().position(latLng)
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_icon_pointer_toped))
-            ).setDraggable(false);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                @Override
-                public void onMapClick(LatLng latLng) {
-                    // need this even it's not used
-                    // it's used to override default function of OnMapClickListener
-                    // which is navigate to default Google Map Apps
-                }
-            });
-        } else layoutMap.setVisibility(View.GONE);
-    }
-
-    private Double getLatitude() {
-        double lat = 0.0;
-        try {
-            lat = Double.parseDouble(mCurrentLat);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-        return lat;
-    }
-
-    private Double getLongitude() {
-        double longitude = 0.0;
-        try {
-            longitude = Double.parseDouble(mCurrentLong);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-        return longitude;
     }
 }
