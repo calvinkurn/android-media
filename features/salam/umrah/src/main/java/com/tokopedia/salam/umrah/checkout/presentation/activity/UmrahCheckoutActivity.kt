e83978fcaaf79b65c63531ab454c6222b403e619
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2019-11-28 18:08:59 +0700

    add dialog back

--- features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/checkout/presentation/activity/UmrahCheckoutActivity.kt
+++ features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/checkout/presentation/activity/UmrahCheckoutActivity.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.applink.RouteManager
+import com.tokopedia.dialog.DialogUnify
+import com.tokopedia.salam.umrah.R
 import com.tokopedia.salam.umrah.checkout.di.DaggerUmrahCheckoutComponent
 import com.tokopedia.salam.umrah.checkout.di.UmrahCheckoutComponent
 import com.tokopedia.salam.umrah.checkout.presentation.fragment.UmrahCheckoutFragment
@@ -31,9 +33,9 @@ class UmrahCheckoutActivity : BaseSimpleActivity(), HasComponent<UmrahCheckoutCo
     override fun getNewFragment(): Fragment? = UmrahCheckoutFragment.getInstance(
             intent.getStringExtra(EXTRA_SLUG_NAME),
             intent.getStringExtra(EXTRA_VARIANT),
-            intent.getIntExtra(EXTRA_PRICE,0),
+            intent.getIntExtra(EXTRA_PRICE, 0),
             intent.getIntExtra(EXTRA_TOTAL_PRICE, 0),
-            intent.getIntExtra(EXTRA_TOTAL_PASSENGER,0),
+            intent.getIntExtra(EXTRA_TOTAL_PASSENGER, 0),
             intent.getStringExtra(EXTRA_DEPART_DATE)
     )
 
@@ -46,7 +48,7 @@ class UmrahCheckoutActivity : BaseSimpleActivity(), HasComponent<UmrahCheckoutCo
         super.onCreate(savedInstanceState)
     }
 
-    companion object{
+    companion object {
         const val EXTRA_PRICE = "EXTRA_PRICE"
         const val EXTRA_TOTAL_PRICE = "EXTRA_TOTAL_PRICE"
         const val EXTRA_SLUG_NAME = "EXTRA_SLUG_NAME"
@@ -55,11 +57,11 @@ class UmrahCheckoutActivity : BaseSimpleActivity(), HasComponent<UmrahCheckoutCo
         const val EXTRA_DEPART_DATE = "EXTRA_DEPART_DATE"
 
 
-        fun createIntent(context:Context, slugName: String, variant:String, price: Int,
-                         totalPrice:Int, totalPassenger:Int, departDate: String
-        ):Intent = Intent(context,UmrahCheckoutActivity::class.java)
-                .putExtra(EXTRA_PRICE,price)
-                .putExtra(EXTRA_TOTAL_PRICE,totalPrice)
+        fun createIntent(context: Context, slugName: String, variant: String, price: Int,
+                         totalPrice: Int, totalPassenger: Int, departDate: String
+        ): Intent = Intent(context, UmrahCheckoutActivity::class.java)
+                .putExtra(EXTRA_PRICE, price)
+                .putExtra(EXTRA_TOTAL_PRICE, totalPrice)
                 .putExtra(EXTRA_SLUG_NAME, slugName)
                 .putExtra(EXTRA_VARIANT, variant)
                 .putExtra(EXTRA_TOTAL_PASSENGER, totalPassenger)
@@ -68,7 +70,19 @@ class UmrahCheckoutActivity : BaseSimpleActivity(), HasComponent<UmrahCheckoutCo
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        trackingUmrahUtil.getClickBackCheckoutTracker()
+        val dialog = DialogUnify(this, DialogUnify.HORIZONTAL_ACTION, DialogUnify.NO_IMAGE)
+        dialog.setTitle(getString(R.string.umrah_checkout_dialog_title))
+        dialog.setDescription(getString(R.string.umrah_checkout_dialog_desc))
+        dialog.setSecondaryCTAText(getString(R.string.umrah_checkout_dialog_cancel_desc))
+        dialog.setPrimaryCTAText(getString(R.string.umrah_checkout_dialog_ok_desc))
+        dialog.show()
+        dialog.setPrimaryCTAClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.setSecondaryCTAClickListener {
+            super.onBackPressed()
+            trackingUmrahUtil.getClickBackCheckoutTracker()
+        }
     }
 }
