4d25d42cedb8ba2593ceb0faa4a8cd8cba4a4455
resakemal <resa.saharso@tokopedia.com>
2019-11-28 08:00:35 +0700

    Add dependency injection

---
+++ features/digital/digitalproduct/src/main/java/com/tokopedia/digital/productV2/di/DigitalProductComponent.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.digital.productV2.di
+
+import android.content.Context
+import com.tokopedia.abstraction.common.di.component.BaseAppComponent
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.digital.home.presentation.activity.DigitalHomePageActivity
+import com.tokopedia.digital.home.presentation.fragment.DigitalHomePageFragment
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.user.session.UserSessionInterface
+import dagger.Component
+import kotlinx.coroutines.CoroutineDispatcher
+
+@DigitalProductScope
+@Component(modules = [DigitalProductModule::class, DigitalProductViewModelModule::class], dependencies = [BaseAppComponent::class])
+interface DigitalProductComponent {
+
+    @ApplicationContext
+    fun context(): Context
+
+    fun userSessionInterface(): UserSessionInterface
+
+    fun dispatcher(): CoroutineDispatcher
+
+    fun graphQlRepository(): GraphqlRepository
+
+    fun inject(digitalProductFragment: DigitalProductFragment)
+}
