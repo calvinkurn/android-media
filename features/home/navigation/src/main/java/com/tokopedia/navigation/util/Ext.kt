a7d5dc534b7ca5748e3ad3da060e84fcf83e4d5d
isfaaghyth <isfaaghyth@gmail.com>
2019-11-28 11:38:57 +0700

    added kt extensions for easily providing of view model

---
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/util/Ext.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.navigation.util
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+/**
+ * For Fragments, allows declarations like
+ * ```
+ * val myViewModel = viewModelProvider(myViewModelFactory)
+ * ```
+ */
+inline fun <reified VM : ViewModel> Fragment.viewModelProvider(
+        provider: ViewModelProvider.Factory
+) = ViewModelProviders.of(this, provider).get(VM::class.java)
