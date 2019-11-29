08c0e7a50be735e8ed2b93372714b0b2c5615eef
kuldeepyadav <kuldeep.yadav@tokopedia.com>
2019-11-29 11:03:50 +0530

    implementing parcelable

--- tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/subCategoryModel/SubCategoryItem.kt
+++ tkpddiscovery/src/main/java/com/tokopedia/discovery/categoryrevamp/data/subCategoryModel/SubCategoryItem.kt
@@ -1,10 +1,10 @@
 package com.tokopedia.discovery.categoryrevamp.data.subCategoryModel
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import java.io.Serializable
 
 data class SubCategoryItem(
-
         @field:SerializedName("isAdult")
         val isAdult: Int? = null,
 
@@ -31,4 +31,42 @@ data class SubCategoryItem(
 
         @field:SerializedName("is_default")
         var is_default: Boolean = false
-) : Serializable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readValue(Int::class.java.classLoader) as? Int,
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readValue(Int::class.java.classLoader) as? Int,
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readByte() != 0.toByte()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(isAdult)
+        parcel.writeString(applinks)
+        parcel.writeString(name)
+        parcel.writeValue(id)
+        parcel.writeString(redirectionURL)
+        parcel.writeString(appRedirectionURL)
+        parcel.writeString(thumbnailImage)
+        parcel.writeString(url)
+        parcel.writeByte(if (is_default) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SubCategoryItem> {
+        override fun createFromParcel(parcel: Parcel): SubCategoryItem {
+            return SubCategoryItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SubCategoryItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
