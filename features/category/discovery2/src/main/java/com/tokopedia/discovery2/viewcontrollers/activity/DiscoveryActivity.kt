6af3bd709dc271947101132ff06ad1fb6f5b0c82
Kanishk Sharma-MBP <kanishksharma-mbp@tokopedias-MacBook-Pro-8.local>
2019-11-29 10:54:41 +0530

    base activity remove

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/activity/DiscoveryActivity.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/activity/DiscoveryActivity.kt
@@ -1,12 +1,14 @@
 package com.tokopedia.discovery2.viewcontrollers.activity
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.discovery2.R
 import com.tokopedia.discovery2.viewcontrollers.fragment.DiscoveryFragment
 import com.tokopedia.discovery2.viewmodel.DiscoveryViewModel
+import com.tokopedia.tradein_common.viewcontrollers.BaseViewModelActivity
 import com.tokopedia.tradein_common.viewmodel.BaseViewModel
 
-class DiscoveryActivity : BaseDiscoveryActivity<DiscoveryViewModel>() {
+class DiscoveryActivity : BaseViewModelActivity<DiscoveryViewModel>() {
 
     private lateinit var discoveryViewModel: DiscoveryViewModel
 
@@ -30,4 +32,33 @@ class DiscoveryActivity : BaseDiscoveryActivity<DiscoveryViewModel>() {
     override fun getLayoutRes(): Int {
         return R.layout.activity_discovery
     }
+
+    override fun getTncFragmentInstance(TncResId: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getBottomSheetLayoutRes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun doNeedReattach(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getVMFactory(): ViewModelProvider.AndroidViewModelFactory {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showProgressBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideProgressBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getMenuRes(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
