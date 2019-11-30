d1097290533b34639b5617040d64536f4c731b0e
Jessica Sean <jessica.sean@tokopedia.com>
2019-11-30 20:41:55 +0700

    final yey

--- features/digital/flight/src/main/java/com/tokopedia/flight/bookingV3/presentation/fragment/FlightBookingFragment.kt
+++ features/digital/flight/src/main/java/com/tokopedia/flight/bookingV3/presentation/fragment/FlightBookingFragment.kt
@@ -477,6 +477,7 @@ class FlightBookingFragment : BaseDaggerFragment() {
     }
 
     private fun navigateToPassengerInfoDetail(viewModel: FlightBookingPassengerViewModel, departureDate: String, requestId: String, autofillName: String = "") {
+        passengerAsTraveller = switch_traveller_as_passenger.isChecked
         startActivityForResult(
                 FlightBookingPassengerActivity.getCallingIntent(
                         activity as Activity,
