7307f20d17ffbfde3bd9c4c7dbc1732f007dab81
Jessica Sean <jessica.sean@tokopedia.com>
2019-12-03 12:51:16 +0700

    clean code v3

--- features/digital/flight/src/main/java/com/tokopedia/flight/bookingV3/presentation/fragment/FlightBookingFragment.kt
+++ features/digital/flight/src/main/java/com/tokopedia/flight/bookingV3/presentation/fragment/FlightBookingFragment.kt
@@ -298,7 +298,7 @@ class FlightBookingFragment : BaseDaggerFragment() {
 
     private fun renderErrorToast(resId: Int) {
         view?.let {
-            Toaster.make(it, getString(resId), Snackbar.LENGTH_LONG, Toaster.TYPE_ERROR, "Oke", View.OnClickListener { /* do nothing */ })
+            Toaster.make(it, getString(resId), Snackbar.LENGTH_LONG, Toaster.TYPE_ERROR, getString(R.string.flight_booking_action_okay), View.OnClickListener { /* do nothing */ })
         }
     }
 
