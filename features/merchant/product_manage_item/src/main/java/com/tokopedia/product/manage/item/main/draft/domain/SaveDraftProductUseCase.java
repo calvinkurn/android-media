50fc6baf76a0e05bc9a127961eddd364502a490e
Calvyn <calvyn.julian@tokopedia.com>
2019-12-04 17:24:47 +0700

    add for flow testing

--- features/merchant/product_manage_item/src/main/java/com/tokopedia/product/manage/item/main/draft/domain/SaveDraftProductUseCase.java
+++ features/merchant/product_manage_item/src/main/java/com/tokopedia/product/manage/item/main/draft/domain/SaveDraftProductUseCase.java
@@ -33,7 +33,7 @@ public class SaveDraftProductUseCase extends UseCase<Long> {
 //                    requestParams.getObject(UPLOAD_PRODUCT_INPUT_MODEL);
 //        } else {
             int a = 2/0;
-            throw new MessageErrorException("qwasdsa");
+            return Observable.error(new Exception());
 //        }
 //        long prevDraftId = requestParams.getLong(PREV_DRAFT_ID, 0);
 //        boolean isUploading = requestParams.getBoolean(IS_UPLOADING, false);
