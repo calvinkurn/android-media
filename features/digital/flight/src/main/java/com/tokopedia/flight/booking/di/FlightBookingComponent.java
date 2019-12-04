c91c0c749328e5d963de4e144f8cf6f50432bf03
Muhammad Furqan <nanjihusaha@gmail.com>
2019-12-04 16:58:50 +0700

    remove Flight Booking Activity v1

--- features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingComponent.java
+++ features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingComponent.java
@@ -1,11 +1,11 @@
 package com.tokopedia.flight.booking.di;
 
 import com.tokopedia.common.travel.di.CommonTravelModule;
-import com.tokopedia.flight.booking.view.activity.FlightBookingActivity;
 import com.tokopedia.flight.booking.view.activity.FlightBookingPassengerActivity;
 import com.tokopedia.flight.booking.view.fragment.FlightBookingFragment;
 import com.tokopedia.flight.booking.view.fragment.FlightBookingNationalityFragment;
 import com.tokopedia.flight.booking.view.fragment.FlightBookingPassengerFragment;
+import com.tokopedia.flight.bookingV3.presentation.activity.FlightBookingActivity;
 import com.tokopedia.flight.common.di.component.FlightComponent;
 import com.tokopedia.flight.review.view.fragment.FlightBookingReviewFragment;
 
@@ -31,13 +31,11 @@ public interface FlightBookingComponent {
 
     void inject(FlightBookingPassengerActivity flightBookingPassengerActivity);
 
-    void inject(FlightBookingActivity flightBookingActivity);
-
     void inject(@NotNull com.tokopedia.flight.bookingV2.presentation.activity.FlightBookingActivity flightBookingActivity);
 
     void inject(@NotNull com.tokopedia.flight.bookingV2.presentation.fragment.FlightBookingFragment flightBookingFragment);
 
-    void inject(@NotNull com.tokopedia.flight.bookingV3.presentation.activity.FlightBookingActivity flightBookingActivity);
+    void inject(FlightBookingActivity flightBookingActivity);
 
     void inject(@NotNull com.tokopedia.flight.bookingV3.presentation.fragment.FlightBookingFragment flightBookingFragment);
 
