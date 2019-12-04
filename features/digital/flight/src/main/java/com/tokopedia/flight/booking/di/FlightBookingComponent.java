e29c892c2fa52c06b17bd26174c6f976416a61fc
Muhammad Furqan <nanjihusaha@gmail.com>
2019-12-04 17:01:53 +0700

    remove Flight Booking Fragment

--- features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingComponent.java
+++ features/digital/flight/src/main/java/com/tokopedia/flight/booking/di/FlightBookingComponent.java
@@ -2,10 +2,10 @@ package com.tokopedia.flight.booking.di;
 
 import com.tokopedia.common.travel.di.CommonTravelModule;
 import com.tokopedia.flight.booking.view.activity.FlightBookingPassengerActivity;
-import com.tokopedia.flight.booking.view.fragment.FlightBookingFragment;
 import com.tokopedia.flight.booking.view.fragment.FlightBookingNationalityFragment;
 import com.tokopedia.flight.booking.view.fragment.FlightBookingPassengerFragment;
 import com.tokopedia.flight.bookingV3.presentation.activity.FlightBookingActivity;
+import com.tokopedia.flight.bookingV3.presentation.fragment.FlightBookingFragment;
 import com.tokopedia.flight.common.di.component.FlightComponent;
 import com.tokopedia.flight.review.view.fragment.FlightBookingReviewFragment;
 
@@ -21,8 +21,6 @@ import dagger.Component;
 @Component(modules = {FlightBookingModule.class, CommonTravelModule.class, FlightBookingViewModelModule.class}, dependencies = FlightComponent.class)
 public interface FlightBookingComponent {
 
-    void inject(FlightBookingFragment flightBookingFragment);
-
     void inject(FlightBookingNationalityFragment flightBookingNationalityFragment);
 
     void inject(FlightBookingReviewFragment flightBookingReviewFragment);
@@ -37,6 +35,6 @@ public interface FlightBookingComponent {
 
     void inject(FlightBookingActivity flightBookingActivity);
 
-    void inject(@NotNull com.tokopedia.flight.bookingV3.presentation.fragment.FlightBookingFragment flightBookingFragment);
+    void inject(FlightBookingFragment flightBookingFragment);
 
 }
