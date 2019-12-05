ff3ff0e70e869e411726bb4e9baa23b4d0f12e35
TokoDeepika <deepika.saini@tokopedia.com>
2019-12-05 11:07:27 +0530

    adding empty page filter case

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/common/util/Utils.kt
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderlist/common/util/Utils.kt
@@ -1,26 +1,28 @@
 package com.tokopedia.transaction.orders.orderlist.common.util
 
+import android.content.Context
+import com.tokopedia.transaction.R
 import java.text.ParseException
 import java.text.SimpleDateFormat
 
 object Utils {
 
-    fun convertMonth(num: Int): String {
-
+    fun convertMonth(num: Int, context: Context): String {
+        val entries = context.resources.getStringArray(R.array.month_title)
         var monthString = ""
         when (num) {
-            1 -> monthString = "Jan"
-            2 -> monthString = "Feb"
-            3 -> monthString = "Mar"
-            4 -> monthString = "Apr"
-            5 -> monthString = "Mei"
-            6 -> monthString = "Jun"
-            7 -> monthString = "Jul"
-            8 -> monthString = "Ags"
-            9 -> monthString = "Sep"
-            10 -> monthString = "Okt"
-            11 -> monthString = "Nov"
-            12 -> monthString = "Des"
+            1 -> monthString = entries[0]
+            2 -> monthString = entries[1]
+            3 -> monthString = entries[2]
+            4 -> monthString = entries[3]
+            5 -> monthString = entries[4]
+            6 -> monthString = entries[5]
+            7 -> monthString = entries[6]
+            8 -> monthString = entries[7]
+            9 -> monthString = entries[8]
+            10 -> monthString = entries[9]
+            11 -> monthString = entries[10]
+            12 -> monthString = entries[11]
         }
         return monthString
     }
