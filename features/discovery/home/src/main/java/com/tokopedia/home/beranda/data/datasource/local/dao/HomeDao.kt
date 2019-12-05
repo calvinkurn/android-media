c2c4fa02376613d4dcfc146d8abbd35e1fa8d6d6
lukaskris12 <lukaskris12@gmail.com>
2019-12-05 13:28:09 +0700

    fix wrong table

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/datasource/local/dao/HomeDao.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/datasource/local/dao/HomeDao.kt
@@ -13,6 +13,6 @@ abstract class HomeDao{
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     abstract suspend fun save(item: HomeRoomData)
 
-    @Query("DELETE FROM HomeDbData")
+    @Query("DELETE FROM HomeRoomData")
     abstract fun deleteHomeData()
 }
