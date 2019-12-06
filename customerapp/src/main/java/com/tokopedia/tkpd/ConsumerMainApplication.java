b0698803b0457fbf2dcc87bd558b9e2069a363de
Rahul Lohra <rahul.lohra@tokopedia.com>
2019-12-06 15:29:06 +0530

    1. Update dependencies from release branch 2. Update code from release branch
    for Pdp GamificationView

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -12,12 +12,11 @@ import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.crashlytics.android.Crashlytics;
 import com.facebook.FacebookSdk;
@@ -177,12 +176,6 @@ public class ConsumerMainApplication extends ConsumerRouterApplication implement
 
         GratificationSubscriber subscriber = new GratificationSubscriber(getApplicationContext());
         registerActivityLifecycleCallbacks(subscriber);
-
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            e.printStackTrace();
-            Log.e("NOOB",e.getLocalizedMessage());
-            System.exit(2);
-        });
     }
 
     @Override
@@ -262,7 +255,9 @@ public class ConsumerMainApplication extends ConsumerRouterApplication implement
         }
     }
 
-    protected AbTestPlatform.Listener getRemoteConfigListener() { return null; }
+    protected AbTestPlatform.Listener getRemoteConfigListener() {
+        return null;
+    }
 
     private void setVersionCode() {
         try {
