7cf1d2cbdcb72c63397087f7b40bb27b1e9d38a7
sachin <sachin.bansal@tokopedia.com>
2019-12-06 13:19:03 +0530

    code update

--- tkpddesign/src/main/java/com/tokopedia/design/bottomsheet/BottomSheetViewPager.kt
+++ tkpddesign/src/main/java/com/tokopedia/design/bottomsheet/BottomSheetViewPager.kt
@@ -9,7 +9,7 @@ import java.lang.reflect.Field
 /*
  * This class is added to achieve the nested scrolling behaviour in view pager fragments which are at postion other than 0*/
 
-public class BottomSheetViewPager(context: Context, attrs: AttributeSet?) : ViewPager(context, attrs) {
+class BottomSheetViewPager(context: Context, attrs: AttributeSet?) : ViewPager(context, attrs) {
     constructor(context: Context) : this(context, null)
     private val positionField: Field =
             ViewPager.LayoutParams::class.java.getDeclaredField("position").also {
