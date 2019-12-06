49936e4a4668d66a19ea6b268617ad9921309507
Henry Priyono <henry.priyono@tokopedia.com>
2019-12-06 16:30:05 +0700

    Revert "Temporary Remove isDebug TimberWrapper"
    This reverts commit 4c0219f02964a35687c972ae3606c94f5d62ccc6.

--- customerapp/src/main/java/com/tokopedia/tkpd/timber/TimberWrapper.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/timber/TimberWrapper.java
@@ -38,7 +38,9 @@ public class TimberWrapper {
     public static void initByConfig(@NonNull RemoteConfig remoteConfig){
         Timber.uprootAll();
         boolean isDebug = BuildConfig.DEBUG;
-
+        if (isDebug) {
+            Timber.plant(new TimberDebugTree());
+        } else {
             String logConfigString = remoteConfig.getString(ANDROID_CUSTOMER_APP_LOG_CONFIG);
             if (!TextUtils.isEmpty(logConfigString)) {
                 DataLogConfig dataLogConfig = new Gson().fromJson(logConfigString,
@@ -50,6 +52,6 @@ public class TimberWrapper {
                     }
                 }
             }
-
+        }
     }
 }
