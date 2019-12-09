dcd17ddca722136bb0184532683de55089b08527
mzennis <meyta.taliti@tokopedia.com>
2019-12-09 14:26:13 +0700

    add applink internal for new play

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.play.PLAY_KEY_CHANNEL_ID
 import com.tokopedia.play.R
 import com.tokopedia.play.di.DaggerPlayComponent
 import com.tokopedia.play.view.viewmodel.PlayVideoViewModel
@@ -23,15 +24,15 @@ import javax.inject.Inject
 class PlayFragment : BaseDaggerFragment() {
 
     companion object {
-
-        // TODO retrieve channelId
-        fun newInstance(): PlayFragment {
-            return PlayFragment()
+        fun newInstance(channelId: String): PlayFragment {
+            return PlayFragment().apply {
+                arguments?.putString(PLAY_KEY_CHANNEL_ID, channelId)
+            }
         }
     }
 
-    // TODO remove this hardcoded channelId (1543, 1591, 1387)
-    private var channelId = "1591"
+    // TODO available channelId: 1543, 1591, 1387
+    private var channelId = ""
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -48,6 +49,11 @@ class PlayFragment : BaseDaggerFragment() {
                 )
                 .build()
                 .inject(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        channelId = arguments?.getString(PLAY_KEY_CHANNEL_ID)?:"1387" // TODO remove default value, handle channel_id not found
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
