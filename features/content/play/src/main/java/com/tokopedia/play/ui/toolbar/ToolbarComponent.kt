ad84dc370f9e65a12aa7309703e9ff58a608ae84
kenny <kenny.hadisaputra@tokopedia.com>
2019-12-09 14:39:51 +0700

    - add toolbar

---
+++ features/content/play/src/main/java/com/tokopedia/play/ui/toolbar/ToolbarComponent.kt
@@ -0,0 +1,67 @@
+package com.tokopedia.play.ui.toolbar
+
+import android.view.ViewGroup
+import com.tokopedia.play.component.EventBusFactory
+import com.tokopedia.play.component.UIComponent
+import com.tokopedia.play.ui.toolbar.interaction.PlayToolbarInteractionEvent
+import com.tokopedia.play.view.event.ScreenStateEvent
+import com.tokopedia.play.view.type.PlayVODType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+/**
+ * Created by jegul on 09/12/19
+ */
+class ToolbarComponent(
+        container: ViewGroup,
+        private val bus: EventBusFactory,
+        coroutineScope: CoroutineScope
+) : UIComponent<PlayToolbarInteractionEvent>, ToolbarView.Listener, CoroutineScope by coroutineScope {
+
+    private val uiView = initView(container)
+
+    init {
+        launch {
+            bus.getSafeManagedFlow(ScreenStateEvent::class.java)
+                    .collect {
+                        when (it) {
+                            is ScreenStateEvent.Play ->
+                                uiView.setLiveBadgeVisibility(
+                                        (it.vodType is PlayVODType.Live)
+                                )
+                        }
+                    }
+        }
+    }
+
+    override fun getContainerId(): Int {
+        return uiView.containerId
+    }
+
+    override fun getUserInteractionEvents(): Flow<PlayToolbarInteractionEvent> {
+        return bus.getSafeManagedFlow(PlayToolbarInteractionEvent::class.java)
+    }
+
+    override fun onBackButtonClicked(view: ToolbarView) {
+        launch {
+            bus.emit(PlayToolbarInteractionEvent::class.java, PlayToolbarInteractionEvent.BackButtonClicked)
+        }
+    }
+
+    override fun onMoreButtonClicked(view: ToolbarView) {
+        launch {
+            bus.emit(PlayToolbarInteractionEvent::class.java, PlayToolbarInteractionEvent.MoreButtonClicked)
+        }
+    }
+
+    override fun onFollowButtonClicked(view: ToolbarView) {
+        launch {
+            bus.emit(PlayToolbarInteractionEvent::class.java, PlayToolbarInteractionEvent.FollowButtonClicked)
+        }
+    }
+
+    private fun initView(container: ViewGroup) =
+            ToolbarView(container, this)
+}
