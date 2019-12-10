ecf546e86cc0b046222c886300e78c603eb2039c
yehezkiel <yehezkiel@tokopedia.com>
2019-12-10 17:48:48 +0700

    remove unused code

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/DeviceChecker.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/DeviceChecker.kt
@@ -3,7 +3,6 @@ package com.tokopedia.abstraction.common.utils
 import android.app.ActivityManager
 import android.content.Context
 import android.os.Build
-import android.util.Log
 
 object DeviceChecker {
     private const val MINIMUM_DEVICE_MEMORY = 256
@@ -12,14 +11,9 @@ object DeviceChecker {
     fun isLowPerformingDevice(context: Context): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         val runtime = Runtime.getRuntime()
-        Log.e("deviceChecker", "memory ${activityManager.memoryClass}")
         return Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT ||
                 activityManager.isLowRamDevice ||
                 runtime.availableProcessors() < MINIMUM_DEVICE_PROCESSORS ||
                 activityManager.memoryClass < MINIMUM_DEVICE_MEMORY
-    }
-
-    fun isDeviceLowMemory(context: Context) {
-
     }
 }
