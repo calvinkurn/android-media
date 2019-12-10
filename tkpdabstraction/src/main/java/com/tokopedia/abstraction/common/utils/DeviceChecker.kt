3e51395cb65d7885841057cdef454a0d664e2430
yehezkiel <yehezkiel@tokopedia.com>
2019-12-10 16:18:16 +0700

    add log since kitkat debug lag

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/DeviceChecker.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/DeviceChecker.kt
@@ -3,16 +3,16 @@ package com.tokopedia.abstraction.common.utils
 import android.app.ActivityManager
 import android.content.Context
 import android.os.Build
+import android.util.Log
 
 object DeviceChecker {
-
     private const val MINIMUM_DEVICE_MEMORY = 256
     private const val MINIMUM_DEVICE_PROCESSORS = 4
 
     fun isLowPerformingDevice(context: Context): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         val runtime = Runtime.getRuntime()
-
+        Log.e("deviceChecker", "memory ${activityManager.memoryClass}")
         return Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT ||
                 activityManager.isLowRamDevice ||
                 runtime.availableProcessors() < MINIMUM_DEVICE_PROCESSORS ||
