d4470ea2205e216ed312c17c0eca691d59cb4137
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-12-12 16:39:20 +0700

    use custom empty state

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/view/adapter/ChatSearchTypeFactoryImpl.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/view/adapter/ChatSearchTypeFactoryImpl.kt
@@ -3,10 +3,12 @@ package com.tokopedia.topchat.chatsearch.view.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
 import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.topchat.chatlist.adapter.viewholder.ChatItemListViewHolder
 import com.tokopedia.topchat.chatsearch.data.SearchResult
+import com.tokopedia.topchat.chatsearch.view.adapter.viewholder.EmptySearchChatViewHolder
 import com.tokopedia.topchat.chatsearch.view.adapter.viewholder.ItemSearchChatViewHolder
 import com.tokopedia.topchat.chatsearch.view.adapter.viewholder.LoadingSearchChatViewHolder
 
@@ -22,10 +24,15 @@ class ChatSearchTypeFactoryImpl(
         return LoadingSearchChatViewHolder.LAYOUT
     }
 
+    override fun type(viewModel: EmptyModel?): Int {
+        return EmptySearchChatViewHolder.LAYOUT
+    }
+
     override fun createViewHolder(parent: View?, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when (type) {
             ChatItemListViewHolder.LAYOUT -> ItemSearchChatViewHolder(parent, listener)
             LoadingSearchChatViewHolder.LAYOUT -> LoadingSearchChatViewHolder(parent)
+            EmptySearchChatViewHolder.LAYOUT -> EmptySearchChatViewHolder(parent)
             else -> super.createViewHolder(parent, type)
         }
     }
