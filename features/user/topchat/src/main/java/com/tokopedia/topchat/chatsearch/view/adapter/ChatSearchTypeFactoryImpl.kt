b2f9df60e0d01ea634f15716dc7c300a6cacd6f0
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-12-12 16:20:50 +0700

    Use custom loading model

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/view/adapter/ChatSearchTypeFactoryImpl.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/view/adapter/ChatSearchTypeFactoryImpl.kt
@@ -3,10 +3,12 @@ package com.tokopedia.topchat.chatsearch.view.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.topchat.chatlist.adapter.viewholder.ChatItemListViewHolder
 import com.tokopedia.topchat.chatsearch.data.SearchResult
 import com.tokopedia.topchat.chatsearch.view.adapter.viewholder.ItemSearchChatViewHolder
+import com.tokopedia.topchat.chatsearch.view.adapter.viewholder.LoadingSearchChatViewHolder
 
 class ChatSearchTypeFactoryImpl(
         private val listener: ItemSearchChatViewHolder.Listener
@@ -16,9 +18,14 @@ class ChatSearchTypeFactoryImpl(
         return ChatItemListViewHolder.LAYOUT
     }
 
+    override fun type(viewModel: LoadingModel): Int {
+        return LoadingSearchChatViewHolder.LAYOUT
+    }
+
     override fun createViewHolder(parent: View?, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when (type) {
             ChatItemListViewHolder.LAYOUT -> ItemSearchChatViewHolder(parent, listener)
+            LoadingSearchChatViewHolder.LAYOUT -> LoadingSearchChatViewHolder(parent)
             else -> super.createViewHolder(parent, type)
         }
     }
