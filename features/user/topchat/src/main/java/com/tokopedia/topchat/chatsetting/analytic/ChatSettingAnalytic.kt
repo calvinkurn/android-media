a9e376e3d6a92823d2378acaec9a727e9a7bc292
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-12-13 16:28:56 +0700

    Refactor tracker CL4

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/analytic/ChatSettingAnalytic.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/analytic/ChatSettingAnalytic.kt
@@ -1,19 +1,12 @@
 package com.tokopedia.topchat.chatsetting.analytic
 
 import com.tokopedia.topchat.chatsetting.data.ChatSetting
+import com.tokopedia.topchat.common.analytics.TopChatAnalytics
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
 import javax.inject.Inject
 
 class ChatSettingAnalytic @Inject constructor() {
-
-    object Event {
-        const val CLICK_INBOX_CHAT = "clickInboxChat"
-    }
-
-    object Category {
-        const val CATEGORY_INBOX_CHAT = "inbox-chat"
-    }
 
     object Action {
         const val CLICK_CHAT_SETTING = "click on gear icon setting"
@@ -23,8 +16,8 @@ class ChatSettingAnalytic @Inject constructor() {
     fun eventClickChatSetting(element: ChatSetting) {
         TrackApp.getInstance().gtm.sendGeneralEvent(
                 TrackAppUtils.gtmData(
-                        Event.CLICK_INBOX_CHAT,
-                        Category.CATEGORY_INBOX_CHAT,
+                        TopChatAnalytics.Name.INBOX_CHAT,
+                        TopChatAnalytics.Category.INBOX_CHAT,
                         Action.CLICK_CHAT_SETTING,
                         element.alias
                 )
