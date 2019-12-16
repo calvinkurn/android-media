5b5036217546e0be48cf40b6fbd4dc8c9caf8b97
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2019-12-16 08:56:32 +0700

    done code review

--- features/discovery/carousel_productcard/src/main/java/com/tokopedia/carouselproductcard/model/CarouselProductCardModel.kt
+++ features/discovery/carousel_productcard/src/main/java/com/tokopedia/carouselproductcard/model/CarouselProductCardModel.kt
@@ -6,4 +6,10 @@ import com.tokopedia.productcard.v2.ProductCardModel
 internal data class CarouselProductCardModel(
         val productCardModel: ProductCardModel,
         val carouselProductCardListenerInfo: CarouselProductCardListenerInfo
-)
+) {
+    fun getOnItemClickListener() = carouselProductCardListenerInfo.onItemClickListener
+    fun getOnItemAddToCartListener() = carouselProductCardListenerInfo.onItemAddToCartListener
+    fun getOnItemImpressedListener() = carouselProductCardListenerInfo.onItemImpressedListener
+    fun getOnItemLongClickListener() = carouselProductCardListenerInfo.onItemLongClickListener
+    fun getOnWishlistItemClickListener() = carouselProductCardListenerInfo.onWishlistItemClickListener
+}
