e15868cce54252481f8351e1762b205d80c7845b
hansenpw <hansen.wijaya@tokopedia.com>
2019-12-17 09:32:16 +0700

    revert unintended

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartFragment.java
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartFragment.java
@@ -230,8 +230,6 @@ public class CartFragment extends BaseCheckoutFragment implements ActionListener
         bundle.putString(CartFragment.class.getSimpleName(), args);
         CartFragment fragment = new CartFragment();
         fragment.setArguments(bundle);
-        String a = "asdf";
-        a.contains()
         return fragment;
     }
 
