01ba99f2093bcdbe74bc454f6de796b0b631ba51
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-12-20 18:11:47 +0700

    Add attach invoice unit test 1

--- features/user/attachinvoice/src/test/java/com/tokopedia/attachinvoice/view/viewmodel/AttachInvoiceViewModelTest.kt
+++ features/user/attachinvoice/src/test/java/com/tokopedia/attachinvoice/view/viewmodel/AttachInvoiceViewModelTest.kt
@@ -1,12 +1,31 @@
 package com.tokopedia.attachinvoice.view.viewmodel
 
+import com.tokopedia.attachinvoice.usecase.GetInvoiceUseCase
+import io.mockk.*
 import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 
-object AttachInvoiceViewModelTest: Spek({
-    describe("Some test") {
-        it("Addition") {
-            assert(false)
+object AttachInvoiceViewModelTest : Spek({
+    group("Load Invoice") {
+
+        val exMsgId = "6696"
+
+        val useCase by memoized { mockk<GetInvoiceUseCase>() }
+        val viewmodel by memoized { AttachInvoiceViewModel(useCase) }
+
+        test("Do nothing when messgeId is empty") {
+            viewmodel.messageId = ""
+            viewmodel.loadInvoices(1)
+
+            verify(exactly = 0) { useCase.getInvoices(any(), any(), any(), any()) }
         }
+
+        test("Call the exact same page provided in argument") {
+            viewmodel.messageId = exMsgId
+            every { useCase.getInvoices(any(), any(), any(), any()) } just Runs
+            viewmodel.loadInvoices(1)
+
+            verify(exactly = 1) { useCase.getInvoices(any(), any(), any(), eq(1)) }
+        }
+
     }
 })
