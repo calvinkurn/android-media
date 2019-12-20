22c427e4b19c96dae2d123c574383a271040d5fe
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2019-12-20 13:08:35 +0700

    Add spek dependency

---
+++ features/user/attachinvoice/src/test/java/com/tokopedia/attachinvoice/view/viewmodel/AttachInvoiceViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.attachinvoice.view.viewmodel
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object AttachInvoiceViewModelTest: Spek({
+    describe("Some test") {
+        it("Addition") {
+            assert(false)
+        }
+    }
+})
