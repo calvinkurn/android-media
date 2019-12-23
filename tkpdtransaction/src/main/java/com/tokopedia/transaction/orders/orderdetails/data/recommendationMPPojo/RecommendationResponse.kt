90c01088f1f13dfa48ea20f5c1494def1718d80a
TokoDeepika <deepika.saini@tokopedia.com>
2019-12-23 14:50:47 +0530

    review suggestions

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/recommendationMPPojo/RecommendationResponse.kt
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/recommendationMPPojo/RecommendationResponse.kt
@@ -1,9 +1,7 @@
 package com.tokopedia.transaction.orders.orderdetails.data.recommendationPojo
 
-import javax.annotation.Generated
 import com.google.gson.annotations.SerializedName
 
-@Generated("com.robohorse.robopojogenerator")
 data class RecommendationResponse(
 
 	@field:SerializedName("rechargeFavoriteRecommendationList")
--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/recommendationMPPojo/RecommendationsItem.kt
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/data/recommendationMPPojo/RecommendationsItem.kt
@@ -1,62 +1,60 @@
 package com.tokopedia.transaction.orders.orderdetails.data.recommendationPojo
 
-import javax.annotation.Generated
 import com.google.gson.annotations.SerializedName
 
-@Generated("com.robohorse.robopojogenerator")
 data class RecommendationsItem(
 
-	@field:SerializedName("productId")
-	val productId: Int?,
+        @field:SerializedName("productId")
+        val productId: Int,
 
-	@field:SerializedName("webLink")
-	val webLink: String?,
+        @field:SerializedName("webLink")
+        val webLink: String? = "",
 
-	@field:SerializedName("description")
-	val description: String?,
+        @field:SerializedName("description")
+        val description: String? = "",
 
-	@field:SerializedName("clientNumber")
-	val clientNumber: String?,
+        @field:SerializedName("clientNumber")
+        val clientNumber: String? = "",
 
-	@field:SerializedName("title")
-	val title: String?,
+        @field:SerializedName("title")
+        val title: String? = "",
 
-	@field:SerializedName("type")
-	val type: String?,
+        @field:SerializedName("type")
+        val type: String? = "",
 
-	@field:SerializedName("categoryName")
-	val categoryName: String?,
+        @field:SerializedName("categoryName")
+        val categoryName: String? = "",
 
-	@field:SerializedName("operatorName")
-	val operatorName: String?,
+        @field:SerializedName("operatorName")
+        val operatorName: String? = "",
 
-	@field:SerializedName("productName")
-	val productName: String?,
+        @field:SerializedName("productName")
+        val productName: String? = "",
 
-	@field:SerializedName("appLink")
-	val appLink: String?,
+        @field:SerializedName("appLink")
+        val appLink: String? = "",
 
-	@field:SerializedName("tagType")
-	val tagType: Int?,
+        @field:SerializedName("tagType")
+        val tagType: Int,
 
-	@field:SerializedName("isATC")
-	val isATC: Boolean?,
+        @field:SerializedName("isATC")
+        val isATC: Boolean,
 
-	@field:SerializedName("iconUrl")
-	val iconUrl: String?,
+        @field:SerializedName("iconUrl")
+        val iconUrl: String? = "",
 
-	@field:SerializedName("position")
-	val position: Int?,
+        @field:SerializedName("position")
+        val position: Int,
 
-	@field:SerializedName("tag")
-	val tag: String?,
+        @field:SerializedName("tag")
+        val tag: String? = "",
 
-	@field:SerializedName("operatorID")
-	val operatorID: Int?,
+        @field:SerializedName("operatorID")
+        val operatorID: Int,
 
-	@field:SerializedName("categoryId")
-	val categoryId: Int?,
+        @field:SerializedName("categoryId")
+        val categoryId: Int,
 
-	@field:SerializedName("productPrice")
-	val productPrice: Int?
+        @field:SerializedName("productPrice")
+        val productPrice: Int
 )
