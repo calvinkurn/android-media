5626ec2cfc8a15c865377df6658d08abc538e317
yogieputra8 <yogie.putra8@gmail.com>
2019-12-26 09:36:04 +0700

    Change scale animation to alpha animation

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/widget/OfficialCategoriesTab.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/widget/OfficialCategoriesTab.kt
@@ -131,7 +131,8 @@ class OfficialCategoriesTab(context: Context,
             val icon = findViewByIdFromTab(getTabAt(i), R.id.image_view_category_icon)
             val textCategory = findViewByIdFromTab(getTabAt(i), R.id.text_view_category_title)
             if (icon is ImageView) {
-                icon.animate().scaleX(1.0f).scaleY(1.0f).duration = 200
+                // icon.animate().scaleX(1.0f).scaleY(1.0f).duration = 200
+                icon.animate().alpha(1f).duration = 150
                 textCategory?.animate()?.translationY(0.0f)?.duration = 200
                 // icon.visibility = View.VISIBLE
             }
@@ -143,7 +144,8 @@ class OfficialCategoriesTab(context: Context,
             val icon = findViewByIdFromTab(getTabAt(i), R.id.image_view_category_icon)
             val textCategory = findViewByIdFromTab(getTabAt(i), R.id.text_view_category_title)
             if (icon is ImageView) {
-                icon.animate().scaleX(0.0f).scaleY(0.0f).duration = 200
+                // icon.animate().scaleX(0.0f).scaleY(0.0f).duration = 200
+                icon.animate().alpha(0f).duration = 200
                 textCategory?.animate()?.translationY(-14.0f)?.duration = 200
                 // icon.visibility = View.GONE
             }
