c56539cf5866d79c15fc8277f4b8713f0654384d
irfan khoirul <irfankhoirul@gmail.com>
2019-12-27 16:41:54 +0700

    item decoration improvement

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartItemDecoration.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/cart/view/CartItemDecoration.kt
@@ -6,8 +6,10 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.purchase_platform.R
 import com.tokopedia.purchase_platform.common.feature.promo_global.PromoGlobalViewHolder
-import com.tokopedia.purchase_platform.common.feature.ticker_announcement.TickerAnnouncementViewHolder
-import com.tokopedia.purchase_platform.features.cart.view.viewholder.*
+import com.tokopedia.purchase_platform.features.cart.view.viewholder.CartSectionHeaderViewHolder
+import com.tokopedia.purchase_platform.features.cart.view.viewholder.CartShopViewHolder
+import com.tokopedia.purchase_platform.features.cart.view.viewholder.DisabledItemHeaderViewHolder
+import com.tokopedia.purchase_platform.features.cart.view.viewholder.DisabledShopViewHolder
 
 /**
  * @author anggaprasetiyo on 06/02/18.
@@ -21,36 +23,27 @@ class CartItemDecoration : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         if (context == null) {
             context = parent.context
-            verticalSpaceHeight = context?.resources?.getDimension(R.dimen.dp_8)?.toInt() ?: 0
+            verticalSpaceHeight = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
         }
 
         val viewHolder = parent.getChildViewHolder(view)
 
         when {
-            viewHolder is PromoGlobalViewHolder -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
-            viewHolder is CartTickerErrorViewHolder -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
-            viewHolder is CartRecentViewViewHolder || viewHolder is CartWishlistViewHolder || viewHolder is CartRecommendationViewHolder -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
-            viewHolder is TickerAnnouncementViewHolder -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
-            viewHolder is DisabledCartItemViewHolder -> if (viewHolder.showDivider) {
-                outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
-            } else {
-                outRect.bottom = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
-            }
+            viewHolder is PromoGlobalViewHolder -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
             viewHolder is CartShopViewHolder -> outRect.top = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
+            viewHolder is CartSectionHeaderViewHolder -> outRect.top = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
             viewHolder is DisabledShopViewHolder -> {
                 if (parent.adapter?.getItemViewType(viewHolder.getAdapterPosition() - 1) == DisabledItemHeaderViewHolder.LAYOUT) {
-                    outRect.top = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
+                    outRect.top = verticalSpaceHeight
                 } else {
                     outRect.top = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
                 }
-                outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
+                outRect.bottom = verticalSpaceHeight
             }
             viewHolder is DisabledItemHeaderViewHolder -> {
                 outRect.top = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
-                outRect.bottom = context?.resources?.getDimension(R.dimen.dp_0)?.toInt() ?: 0
+                outRect.bottom = verticalSpaceHeight
             }
-            viewHolder is CartSectionHeaderViewHolder -> outRect.top = context?.resources?.getDimension(R.dimen.dp_6)?.toInt() ?: 0
-            viewHolder.adapterPosition == parent.adapter?.itemCount ?: 0 - 1 -> outRect.bottom = context?.resources?.getDimension(R.dimen.dp_14)?.toInt() ?: 0
             else -> outRect.bottom = verticalSpaceHeight
         }
     }
