6bb67a90d432ebe1696b55de938b1a106497a48a
kenny <kenny.hadisaputra@tokopedia.com>
2019-12-31 11:10:44 +0700

    - handle send chat max characters and trim newline

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -134,8 +134,9 @@ class PlayViewModel @Inject constructor(
         if (!userSessionInterface.isLoggedIn)
             return
 
-        playSocket.send(message, onSuccess = {
-            _observableChatList.value = PlayChat("", "", message,
+        val cleanMessage = message.trimMultipleNewlines()
+        playSocket.send(cleanMessage, onSuccess = {
+            _observableChatList.value = PlayChat("", "", cleanMessage,
                     PlayChat.UserData(userSessionInterface.userId, userSessionInterface.name, userSessionInterface.profilePicture))
         })
     }
@@ -290,4 +291,6 @@ class PlayViewModel @Inject constructor(
                 freezeButtonTitle = channel.freezeChannelState.btnTitle,
                 freezeButtonUrl = channel.freezeChannelState.btnAppLink
         )
+
+    private fun String.trimMultipleNewlines() = trim().replace(Regex("(\\n+)"), "\n")
 }
