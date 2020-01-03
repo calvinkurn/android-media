6cca0d8d3660ba5f9cd94c4fe6143c96568625ac
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2020-01-03 15:27:22 +0530

    dependency of basemvvm module changed to design module for atc_variant module

--- features/merchant/atc_variant/src/main/java/com/tokopedia/atc_variant/view/NormalCheckoutActivity.kt
+++ features/merchant/atc_variant/src/main/java/com/tokopedia/atc_variant/view/NormalCheckoutActivity.kt
@@ -9,7 +9,8 @@ import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.atc_variant.R
 import com.tokopedia.purchase_platform.common.constant.ATC_AND_BUY
 import com.tokopedia.purchase_platform.common.constant.ProductAction
-import com.tokopedia.tradein_common.IAccessRequestListener
+import com.tokopedia.track.TrackApp
+import com.tokopedia.design.dialog.IAccessRequestListener
 import com.tokopedia.common_tradein.model.TradeInParams
 
 /**
@@ -109,13 +110,22 @@ open class NormalCheckoutActivity : BaseSimpleActivity(), IAccessRequestListener
     }
 
     override fun clickAccept() {
+        sendGeneralEvent("setuju")
         normalCheckoutFragment?.run {
             this.goToTradeInHome()
         }
     }
 
     override fun clickDeny() {
+        sendGeneralEvent("batal")
+    }
 
+    private fun sendGeneralEvent(label: String) {
+        val trackApp = TrackApp.getInstance()
+        trackApp.gtm.sendGeneralEvent("clickPDP",
+                "product detail page",
+                "click - asking permission trade in",
+                label)
     }
 
 }
