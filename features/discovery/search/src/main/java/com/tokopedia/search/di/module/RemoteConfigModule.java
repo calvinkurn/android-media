143cfac8179c8954008d0e55fb7e02909dae6117
darianthedytoped <darian.thedy@tokopedia.com>
2020-01-03 11:59:25 +0700

    Commit search shop new empty search UI

--- features/discovery/search/src/main/java/com/tokopedia/search/di/module/RemoteConfigModule.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/di/module/RemoteConfigModule.java
@@ -7,6 +7,8 @@ import com.tokopedia.search.di.scope.SearchScope;
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl;
 import com.tokopedia.remoteconfig.RemoteConfig;
 
+import java.util.Set;
+
 import dagger.Module;
 import dagger.Provides;
 
@@ -17,6 +19,71 @@ public class RemoteConfigModule {
     @SearchScope
     @Provides
     RemoteConfig provideRemoteConfig(@ApplicationContext Context context) {
-        return new FirebaseRemoteConfigImpl(context);
+        return new RemoteConfig() {
+            @Override
+            public Set<String> getKeysByPrefix(String prefix) {
+                return null;
+            }
+
+            @Override
+            public boolean getBoolean(String key) {
+                return true;
+            }
+
+            @Override
+            public boolean getBoolean(String key, boolean defaultValue) {
+                return true;
+            }
+
+            @Override
+            public byte[] getByteArray(String key) {
+                return new byte[0];
+            }
+
+            @Override
+            public byte[] getByteArray(String key, byte[] defaultValue) {
+                return new byte[0];
+            }
+
+            @Override
+            public double getDouble(String key) {
+                return 0;
+            }
+
+            @Override
+            public double getDouble(String key, double defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public long getLong(String key) {
+                return 0;
+            }
+
+            @Override
+            public long getLong(String key, long defaultValue) {
+                return 0;
+            }
+
+            @Override
+            public String getString(String key) {
+                return null;
+            }
+
+            @Override
+            public String getString(String key, String defaultValue) {
+                return null;
+            }
+
+            @Override
+            public void setString(String key, String value) {
+
+            }
+
+            @Override
+            public void fetch(Listener listener) {
+
+            }
+        };
     }
 }
