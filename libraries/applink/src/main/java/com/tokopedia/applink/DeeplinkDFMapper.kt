2c1e6b21c963c66c88447ef12d96c0898cecf154
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-01-06 17:00:34 +0700

    Group DF Topchat

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -188,7 +188,7 @@ object DeeplinkDFMapper {
             add(DFP({ it.startsWith(USER_NOTIFICATION_SETTING) }, DFM_USER, R.string.notif_settings_title))
             add(DFP({ it.startsWith(USER_IDENTIFICATION_FORM) }, DFM_USER, R.string.user_identification_common_title))
             add(DFP({ it.startsWith(ATTACH_INVOICE) }, DFM_USER, R.string.title_module_attachinvoice))
-            add(DFP({it.startsWith(TOPCHAT_IDLESS)}, DFM_USER, R.string.path_chat))
+            add(DFP({ it.startsWith(TOPCHAT_IDLESS)}, DFM_USER, R.string.title_topchat))
             add(DFP({ it.startsWith(SALAM_UMRAH_HOME_PAGE) }, DFM_SALAM, R.string.title_salam))
             add(DFP({ it.startsWith(SALAM_ORDER_DETAIL) }, DFM_SALAM, R.string.title_salam))
         }
