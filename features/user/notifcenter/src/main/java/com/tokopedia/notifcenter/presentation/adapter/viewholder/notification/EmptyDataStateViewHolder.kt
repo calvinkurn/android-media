c6d186ee07fb0524fb9ab7d2f6eb60b925e52e9d
isfaaghyth <isfaaghyth@gmail.com>
2020-01-08 02:06:06 +0700

    handle size of container (based on type of emptySource state)

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/EmptyDataStateViewHolder.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/EmptyDataStateViewHolder.kt
@@ -1,21 +1,45 @@
 package com.tokopedia.notifcenter.presentation.adapter.viewholder.notification
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.notifcenter.R
+import com.tokopedia.notifcenter.data.state.EmptySource
 import com.tokopedia.notifcenter.data.viewbean.NotificationEmptyStateViewBean
+
+typealias LayoutParam = ViewGroup.LayoutParams
 
 class EmptyDataStateViewHolder(view: View): AbstractViewHolder<NotificationEmptyStateViewBean>(view) {
 
-    private val imgState = view.findViewById<ImageView>(R.id.img_state)
+    private val container = view.findViewById<RelativeLayout>(R.id.empty_container)
     private val txtMessage = view.findViewById<TextView>(R.id.txt_message)
+    private val imgState = view.findViewById<ImageView>(R.id.img_state)
+
+    private val _matchParent by lazy { LayoutParam.MATCH_PARENT }
+    private val _wrapContent by lazy { LayoutParam.WRAP_CONTENT }
 
     override fun bind(element: NotificationEmptyStateViewBean) {
         txtMessage.text = if (element.title != 0) getString(element.title) else ""
         imgState.setImageResource(element.icon)
+
+        when (element.type) {
+            is EmptySource.Normal -> {
+                setLayoutParamsContainer()
+            }
+            is EmptySource.Transaction -> {
+                setLayoutParamsContainer(
+                        height = _wrapContent
+                )
+            }
+        }
+    }
+
+    private fun setLayoutParamsContainer(width: Int = _matchParent, height: Int = _matchParent) {
+        container.layoutParams = ViewGroup.LayoutParams(width, height)
     }
 
     companion object {
