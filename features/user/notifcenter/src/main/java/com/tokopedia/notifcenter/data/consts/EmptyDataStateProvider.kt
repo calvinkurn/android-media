a509dc7495ebf9c275e9a96d53ad941453a5664b
isfaaghyth <isfaaghyth@gmail.com>
2020-01-10 01:21:47 +0700

    fix unresolved empty state resources

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/consts/EmptyDataStateProvider.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/consts/EmptyDataStateProvider.kt
@@ -7,7 +7,7 @@ object EmptyDataStateProvider {
 
     fun emptyData(): NotificationEmptyStateViewBean {
         return NotificationEmptyStateViewBean(
-                R.drawable.bg_notif_empty_state_notification,
+                R.drawable.bg_notifcenter_empty_state,
                 R.string.notification_empty_message)
     }
 
