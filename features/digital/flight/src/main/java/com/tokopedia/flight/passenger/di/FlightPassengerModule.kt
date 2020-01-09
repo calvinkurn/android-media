302af905df69df470a11cca16590ccf9c0e93fe3
Muhammad Furqan <nanjihusaha@gmail.com>
2020-01-09 15:35:13 +0700

    remove passenger db

--- features/digital/flight/src/main/java/com/tokopedia/flight/passenger/di/FlightPassengerModule.kt
+++ features/digital/flight/src/main/java/com/tokopedia/flight/passenger/di/FlightPassengerModule.kt
@@ -1,11 +1,8 @@
 package com.tokopedia.flight.passenger.di
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.travel.passenger.data.database.TravelPassengerDb
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
@@ -30,10 +27,5 @@ class FlightPassengerModule {
     @FlightPassengerScope
     @Provides
     fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
-
-    @FlightPassengerScope
-    @Provides
-    fun provideTravelPassengerRoomDb(@ApplicationContext context: Context)
-            : TravelPassengerDb = TravelPassengerDb.getDatabase(context)
 
 }
--- features/digital/travel_passenger/src/main/java/com/tokopedia/travel/passenger/di/TravelPassengerModule.kt
+++ features/digital/travel_passenger/src/main/java/com/tokopedia/travel/passenger/di/TravelPassengerModule.kt
@@ -1,11 +1,8 @@
 package com.tokopedia.travel.passenger.di
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.travel.passenger.data.database.TravelPassengerDb
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
@@ -16,16 +13,6 @@ import kotlinx.coroutines.Dispatchers
  */
 @Module
 class TravelPassengerModule {
-
-    @Provides
-    @TravelPassengerScope
-    fun provideTravelPassengerDb(@ApplicationContext context: Context): TravelPassengerDb =
-            TravelPassengerDb.getDatabase(context)
-
-    @Provides
-    @TravelPassengerScope
-    fun provideTravelPassengerDao(travelPassengerDb: TravelPassengerDb) =
-            travelPassengerDb.travelPassengerDao()
 
     @Provides
     @TravelPassengerScope
