df9a6b4c51ca08cbaf82c4c0a55d7ecb3c85ba0b
yorispr <yoris.prayogo@tokopedia.com>
2020-01-10 11:17:37 +0700

    revert

--- features/user/talk/src/main/java/com/tokopedia/talk/shoptalk/view/activity/ShopTalkActivity.kt
+++ features/user/talk/src/main/java/com/tokopedia/talk/shoptalk/view/activity/ShopTalkActivity.kt
@@ -29,6 +29,36 @@ class ShopTalkActivity : BaseSimpleActivity(), HasComponent<TalkComponent> {
         return ShopTalkFragment.newInstance(bundle)
     }
 
+    companion object {
+        const val EXTRA_SHOP_ID: String = "shopId"
+        const val APP_LINK_EXTRA_SHOP_ID = "shop_id"
+
+        open fun createIntent(context: Context,
+                              shopId: String): Intent {
+            val intent = Intent(context, ShopTalkActivity::class.java)
+            val bundle = Bundle()
+            bundle.putString(EXTRA_SHOP_ID, shopId)
+            intent.putExtras(bundle)
+            return intent
+        }
+
+        object DeepLinkIntents {
+            @JvmStatic
+            @DeepLink(ApplinkConst.SHOP_TALK)
+            fun getCallingIntent(context: Context, extras: Bundle): Intent {
+                val uri = Uri.parse(extras.getString(DeepLink.URI)).buildUpon()
+                val shopId = extras.getString(APP_LINK_EXTRA_SHOP_ID, "")
+                return (context.applicationContext as TalkRouter).getShopTalkIntent(context, shopId)
+                        .setData(uri.build())
+                        .putExtras(extras)
+
+            }
+
+        }
+
+    }
+
+
     override fun getComponent(): TalkComponent {
         return DaggerTalkComponent.builder().baseAppComponent(
                 (application as BaseMainApplication).baseAppComponent).build()
