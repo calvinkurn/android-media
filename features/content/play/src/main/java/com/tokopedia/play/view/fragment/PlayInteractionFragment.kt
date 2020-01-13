3d4b12643a83d9447743d9eeb14fc81407012e72
kenny <kenny.hadisaputra@tokopedia.com>
2020-01-13 17:22:45 +0700

    - fix wrong constraint

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayInteractionFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayInteractionFragment.kt
@@ -521,7 +521,7 @@ class PlayInteractionFragment : BaseDaggerFragment(), CoroutineScope, PlayMoreAc
 
             constraintSet.apply {
                 connect(id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-                connect(id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.START)
+                connect(id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
                 connect(id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM)
                 connect(id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
             }
