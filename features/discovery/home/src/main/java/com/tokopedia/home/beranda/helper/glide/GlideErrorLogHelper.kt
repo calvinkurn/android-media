15ae31e922e1c80045df663e3a3f06cdd81eeb25
Henry Priyono <henry.priyono@tokopedia.com>
2020-01-14 13:45:56 +0700

    Create Extension Style

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/helper/glide/GlideErrorLogHelper.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/helper/glide/GlideErrorLogHelper.kt
@@ -13,39 +13,37 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
-object GlideErrorLogHelper {
 
-    fun logError(context: Context, e: GlideException?, url: String) {
-        GlobalScope.launch {
-            logErrorSync(context, e, url)
-        }
+fun logError(context: Context, e: GlideException?, url: String) {
+    GlobalScope.launch {
+        logErrorSync(context, e, url)
+    }
+}
+
+private suspend fun logErrorSync(context: Context, e: GlideException?, url: String) {
+    val remoteConfig = FirebaseRemoteConfigImpl(context)
+    val traceRouteMinVersion = remoteConfig.getLong(RemoteConfigKey.ENABLE_TRACEROUTE_MIN_VERSION, 999999999)
+    if (GlobalConfig.VERSION_CODE < traceRouteMinVersion) {
+        return
     }
 
-    private suspend fun logErrorSync(context: Context, e: GlideException?, url: String) {
-        val remoteConfig = FirebaseRemoteConfigImpl(context)
-        val traceRouteMinVersion = remoteConfig.getLong(RemoteConfigKey.ENABLE_TRACEROUTE_MIN_VERSION, 999999999)
-        if (GlobalConfig.VERSION_CODE < traceRouteMinVersion) {
-            return
-        }
-
-        if (!isNetworkAvailable(context)) {
-            Timber.w("P2#IMAGE_TRACEROUTE#network not available")
-            return
-        }
-
-        val host = Uri.parse(url).host
-        if (!TextUtils.isEmpty(host)) {
-            val traceResult = TraceRoute.traceRoute(host!!)
-            Timber.w("P2#IMAGE_TRACEROUTE#%s: url= %s message= %s traceroute= %s",
-                    traceResult?.code?.toString() ?: "",
-                    url,
-                    e?.message ?: "",
-                    traceResult?.message ?: "")
-        }
+    if (!isNetworkAvailable(context)) {
+        Timber.w("P2#IMAGE_TRACEROUTE#network not available")
+        return
     }
 
-    private suspend fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager : ConnectivityManager? = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
-        return connectivityManager?.activeNetworkInfo?.isConnected ?: true
+    val host = Uri.parse(url).host
+    if (!TextUtils.isEmpty(host)) {
+        val traceResult = TraceRoute.traceRoute(host!!)
+        Timber.w("P2#IMAGE_TRACEROUTE#%s: url= %s message= %s traceroute= %s",
+                traceResult?.code?.toString() ?: "",
+                url,
+                e?.message ?: "",
+                traceResult?.message ?: "")
     }
+}
+
+private suspend fun isNetworkAvailable(context: Context): Boolean {
+    val connectivityManager : ConnectivityManager? = context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    return connectivityManager?.activeNetworkInfo?.isConnected ?: true
 }
