cd19241b4999d2b8a6fee86634d51dc647c13852
Zishanr <zishan.rashid@tokopedia.com>
2020-01-15 16:07:22 +0530

    Added test module

---
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/di/DiscoveryComponent.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.discovery.categoryrevamp.di
+
+import com.tokopedia.abstraction.common.di.component.BaseAppComponent
+import com.tokopedia.discovery2.di.DiscoveryModule
+import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryActivity
+import com.tokopedia.discovery2.viewcontrollers.fragment.DiscoveryFragment
+import dagger.Component
+
+
+@DiscoveryScope
+@Component(modules = [DiscoveryModule::class,DiscoveryViewModelModule::class], dependencies = [BaseAppComponent::class])
+interface DiscoveryComponent {
+    fun inject(discoveryActivity: DiscoveryActivity)
+}
