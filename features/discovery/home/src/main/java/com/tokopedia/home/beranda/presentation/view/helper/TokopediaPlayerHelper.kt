5334ee56fcc9bb8661ebfa3bf71fc763b36e7a27
lukaskris12 <lukaskris12@gmail.com>
2020-01-16 16:41:14 +0700

    resolve discuss

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/helper/TokopediaPlayerHelper.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/helper/TokopediaPlayerHelper.kt
@@ -4,20 +4,15 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
 import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.mediacodec.MediaCodecRenderer.DecoderInitializationException
-import com.google.android.exoplayer2.mediacodec.MediaCodecUtil.DecoderQueryException
 import com.google.android.exoplayer2.util.Util
-import com.tokopedia.home.R
 import com.tokopedia.home.beranda.presentation.view.customview.TokopediaPlayView
 import com.tokopedia.home.util.ConnectionUtils
 import com.tokopedia.home.util.DimensionUtils
 import com.tokopedia.play_common.player.TokopediaPlayManager
 import kotlinx.coroutines.*
 import timber.log.Timber
-import java.io.IOException
 import kotlin.coroutines.CoroutineContext
 
 @SuppressWarnings("WeakerAccess")
@@ -170,73 +165,6 @@ class TokopediaPlayerHelper(
                 mExoPlayerListener?.onPlayerBuffering(mPlayer?.currentWindowIndex ?: -1)
             }
             else -> Timber.tag(TokopediaPlayerHelper::class.java.name).e("onPlayerStateChanged unknown: $playbackState")
-        }
-    }
-
-    override fun onPlayerError(error: ExoPlaybackException) {
-        var errorString: String? = null
-
-        when (error.type) {
-            ExoPlaybackException.TYPE_SOURCE -> {
-                //https://github.com/google/ExoPlayer/issues/2702
-                val ex: IOException = error.sourceException
-                val msg = ex.message
-                if (msg != null) {
-                    Timber.tag(TokopediaPlayerHelper::class.java.name).e(msg)
-                    errorString = msg
-                }
-            }
-            ExoPlaybackException.TYPE_RENDERER -> {
-                val exception: Exception = error.rendererException
-                if (exception.message != null) {
-                    errorString = exception.message ?: ""
-                    Timber.tag(TokopediaPlayerHelper::class.java.name).e(exception)
-                }
-            }
-            ExoPlaybackException.TYPE_UNEXPECTED -> {
-                val runtimeException: RuntimeException = error.unexpectedException
-                Timber.tag(TokopediaPlayerHelper::class.java.name).e(if (runtimeException.message == null) "Message is null" else runtimeException.message)
-                if (runtimeException.message == null) {
-                    runtimeException.printStackTrace()
-                }
-                errorString = runtimeException.message
-            }
-            ExoPlaybackException.TYPE_OUT_OF_MEMORY -> {
-            }
-            ExoPlaybackException.TYPE_REMOTE -> {
-            }
-        }
-
-        if (error.type == ExoPlaybackException.TYPE_RENDERER) {
-            val cause: Exception = error.rendererException
-            if (cause is DecoderInitializationException) { // Special case for decoder initialization failures.
-                val decoderInitializationException = cause
-                errorString = if (decoderInitializationException.decoderName == null) {
-                    when {
-                        decoderInitializationException.cause is DecoderQueryException -> {
-                            context.getString(R.string.error_querying_decoders)
-                        }
-                        decoderInitializationException.secureDecoderRequired -> {
-                            context.getString(R.string.error_no_secure_decoder,
-                                    decoderInitializationException.mimeType)
-                        }
-                        else -> {
-                            context.getString(R.string.error_no_decoder,
-                                    decoderInitializationException.mimeType)
-                        }
-                    }
-                } else {
-                    context.getString(R.string.error_instantiating_decoder,
-                            decoderInitializationException.decoderName)
-                }
-            }
-        }
-        if (errorString != null) {
-            Timber.tag(TokopediaPlayerHelper::class.java.name).e("$errorString")
-        }
-
-        if (mExoPlayerListener != null) {
-            mExoPlayerListener?.onPlayerError(errorString)
         }
     }
 
