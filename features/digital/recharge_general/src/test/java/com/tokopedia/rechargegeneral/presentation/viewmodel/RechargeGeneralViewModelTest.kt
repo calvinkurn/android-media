a4e7daaef5b79cba886e4f611abcc79908f17ff9
resakemal <resa.saharso@tokopedia.com>
2020-01-16 13:18:16 +0700

    Remove observer after testing in unit test

--- features/digital/recharge_general/src/test/java/com/tokopedia/rechargegeneral/presentation/viewmodel/RechargeGeneralViewModelTest.kt
+++ features/digital/recharge_general/src/test/java/com/tokopedia/rechargegeneral/presentation/viewmodel/RechargeGeneralViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.rechargegeneral.presentation.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.tokopedia.common.topupbills.data.product.CatalogOperator
 import com.tokopedia.common.topupbills.data.product.CatalogProduct
 import com.tokopedia.common.topupbills.data.product.CatalogProductData
@@ -12,16 +13,16 @@ import com.tokopedia.rechargegeneral.model.RechargeGeneralOperatorCluster
 import com.tokopedia.rechargegeneral.model.RechargeGeneralProductData
 import com.tokopedia.rechargegeneral.model.RechargeGeneralProductItemData
 import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.Dispatchers
+import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Test
-
-import org.junit.Assert.*
 import org.junit.Rule
+import org.junit.Test
 
 class RechargeGeneralViewModelTest {
 
@@ -63,21 +64,31 @@ class RechargeGeneralViewModelTest {
                 mapOf(), false)
         coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
 
-        rechargeGeneralViewModel.getOperatorCluster(mapParams)
-
-        rechargeGeneralViewModel.operatorCluster.observeForever {
+        val observer = Observer<Result<RechargeGeneralOperatorCluster>> {
             assert(it is Success)
             assertEquals((it as Success).data.operatorGroups.first().operators.first().id, 1)
+        }
+
+        try {
+            rechargeGeneralViewModel.operatorCluster.observeForever(observer)
+            rechargeGeneralViewModel.getOperatorCluster(mapParams)
+        } finally {
+            rechargeGeneralViewModel.operatorCluster.removeObserver(observer)
         }
     }
 
     @Test
     fun getOperatorCluster_Fail() {
-        rechargeGeneralViewModel.getOperatorCluster(mapParams)
-
-        rechargeGeneralViewModel.operatorCluster.observeForever {
+        val observer = Observer<Result<RechargeGeneralOperatorCluster>> {
             assert(it is Fail)
             assertEquals((it as Fail).throwable.message, errorMessage)
+        }
+
+        try {
+            rechargeGeneralViewModel.operatorCluster.observeForever(observer)
+            rechargeGeneralViewModel.getOperatorCluster(mapParams)
+        } finally {
+            rechargeGeneralViewModel.operatorCluster.removeObserver(observer)
         }
     }
 
@@ -95,21 +106,31 @@ class RechargeGeneralViewModelTest {
                 mapOf(), false)
         coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
 
-        rechargeGeneralViewModel.getProductList(mapParams)
-
-        rechargeGeneralViewModel.productList.observeForever {
+        val observer = Observer<Result<RechargeGeneralProductData>> {
             assert(it is Success)
             assertEquals((it as Success).data.product.dataCollections.first().products.first().id, "1")
+        }
+
+        try {
+            rechargeGeneralViewModel.productList.observeForever(observer)
+            rechargeGeneralViewModel.getProductList(mapParams)
+        } finally {
+            rechargeGeneralViewModel.productList.removeObserver(observer)
         }
     }
 
     @Test
     fun getProductList_Fail() {
-        rechargeGeneralViewModel.getProductList(mapParams)
-
-        rechargeGeneralViewModel.productList.observeForever {
+        val observer = Observer<Result<RechargeGeneralProductData>> {
             assert(it is Fail)
             assertEquals((it as Fail).throwable.message, errorMessage)
+        }
+
+        try {
+            rechargeGeneralViewModel.productList.observeForever(observer)
+            rechargeGeneralViewModel.getProductList(mapParams)
+        } finally {
+            rechargeGeneralViewModel.productList.removeObserver(observer)
         }
     }
 
