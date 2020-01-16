9ad2d65cbdc645dda0213b52f9a727c3d8e53584
Shubham Gupta <shubham.gupta@tokopedia.com>
2020-01-16 12:33:31 +0530

    Revert "POC on Encrypting data in CM Push"
    This reverts commit 8419d3ee

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/database/pushRuleEngine/PushDataStore.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/database/pushRuleEngine/PushDataStore.kt
@@ -1,14 +1,11 @@
 package com.tokopedia.notifications.database.pushRuleEngine
 
-import com.tokopedia.notifications.common.CMPushEncoderDecoder
 import com.tokopedia.notifications.model.BaseNotificationModel
 import com.tokopedia.notifications.model.NotificationStatus
 
 class PushDataStore(private val baseNotificationDao: BaseNotificationDao) : IPushDataStore {
     override suspend fun getNotificationById(notificationId: Int): BaseNotificationModel? {
-        val baseNotificationModel = baseNotificationDao.getNotificationById(notificationId)
-        CMPushEncoderDecoder.decodeBaseNotificationModel(baseNotificationModel)
-        return baseNotificationModel
+        return baseNotificationDao.getNotificationById(notificationId)
     }
 
     override suspend fun deleteNotification(olderThanExpiryMillis: Long, status: NotificationStatus) {
@@ -16,25 +13,23 @@ class PushDataStore(private val baseNotificationDao: BaseNotificationDao) : IPus
     }
 
     override suspend fun insertNotification(baseNotificationModel: BaseNotificationModel) {
-        CMPushEncoderDecoder.encodeBaseNotificationModel(baseNotificationModel)
         return baseNotificationDao.insertNotificationModel(baseNotificationModel)
     }
 
     override suspend fun getPendingNotificationList(currentMillis: Long): List<BaseNotificationModel> {
-        return getDecryptedList(baseNotificationDao.getPendingNotification(currentMillis))
-    }
-
-    private fun getDecryptedList(notificationList: List<BaseNotificationModel>): List<BaseNotificationModel> {
-        notificationList.forEach { CMPushEncoderDecoder.decodeBaseNotificationModel(it) }
-        return notificationList
+        return baseNotificationDao.getPendingNotification(currentMillis)
     }
 
     override suspend fun getNotificationByStatusList(status: NotificationStatus): List<BaseNotificationModel> {
-        return getDecryptedList(baseNotificationDao.getNotificationByStatusList(status))
+        return baseNotificationDao.getNotificationByStatusList(status)
     }
 
     override suspend fun updateNotificationStatusById(notificationId: Int, status: NotificationStatus) {
         baseNotificationDao.updateNotificationStatus(notificationId, status)
     }
+
+
+
+
 
 }
