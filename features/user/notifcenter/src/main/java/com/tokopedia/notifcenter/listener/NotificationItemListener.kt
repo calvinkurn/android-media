3c0a9ea20b52332a583031eaf0b73a435433083d
isfaaghyth <isfaaghyth@gmail.com>
2020-01-20 23:52:56 +0700

    notifcenter: (product-checkout) change listener param from productData into dataNotif

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/listener/NotificationItemListener.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/listener/NotificationItemListener.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.notifcenter.listener
 
 import com.tokopedia.notifcenter.analytics.NotificationUpdateAnalytics
+import com.tokopedia.notifcenter.data.entity.DataNotification
 import com.tokopedia.notifcenter.data.entity.ProductData
 import com.tokopedia.notifcenter.data.viewbean.NotificationItemViewBean
 
@@ -8,7 +9,7 @@ interface NotificationItemListener {
     fun itemClicked(notification: NotificationItemViewBean, adapterPosition: Int)
     fun getAnalytic(): NotificationUpdateAnalytics
     fun addProductToCart(product: ProductData, onSuccessAddToCart: () -> Unit)
-    fun addProductToCheckout(product: ProductData)
+    fun addProductToCheckout(notification: DataNotification)
     fun showTextLonger(element: NotificationItemViewBean)
     fun trackNotificationImpression(element: NotificationItemViewBean)
 }
