ea9018d2db2f9d8b0316d16a26706f9fd78c8d53
Elscione <yusufhendrawan06@gmail.com>
2020-01-20 15:49:56 +0700

    Fix missing imports

--- features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/common/di/module/BroadcastMessageCreateModule.kt
+++ features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/common/di/module/BroadcastMessageCreateModule.kt
@@ -1,8 +1,13 @@
 package com.tokopedia.broadcast.message.common.di.module
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.broadcast.message.common.di.scope.BroadcastMessageCreateScope
 import com.tokopedia.shop.common.di.ShopCommonModule
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
+import dagger.Provides
 
 @BroadcastMessageCreateScope
 @Module(includes = arrayOf(ShopCommonModule::class))
