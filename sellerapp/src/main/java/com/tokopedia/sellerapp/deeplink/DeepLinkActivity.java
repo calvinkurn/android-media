9d397bdbbec6cc48a5bd48b0abc091e71851bb8c
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-01-21 14:39:24 +0700

    remove import Log

--- sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/DeepLinkActivity.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/deeplink/DeepLinkActivity.java
@@ -5,7 +5,6 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.tokopedia.abstraction.base.view.fragment.BaseSessionWebViewFragment;
