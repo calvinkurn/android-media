6c447a9ec770cf786957b707d5901890d360a3f7
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-01-21 11:18:48 +0700

    Add usecase for new rates which is for tradein

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/usecase/GetRatesApiUseCase.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/usecase/GetRatesApiUseCase.kt
@@ -8,28 +8,36 @@ import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.logisticcart.R
 import com.tokopedia.logisticcart.shipping.features.shippingduration.view.ShippingDurationConverter
 import com.tokopedia.logisticcart.shipping.model.RatesParam
+import com.tokopedia.logisticcart.shipping.model.ShippingRecommendationData
 import com.tokopedia.logisticcart.shipping.model.ShopShipment
+import com.tokopedia.logisticdata.data.entity.ratescourierrecommendation.GetRatesCourierRecommendationData
+import com.tokopedia.logisticdata.data.entity.ratescourierrecommendation.GetRatesCourierRecommendationTradeInDropOffData
 import com.tokopedia.remoteconfig.GraphqlHelper
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import javax.inject.Inject
 
-class GetRatesUseCase @Inject constructor(
+// Typealias to make it shorter and have unconfusing meaning
+typealias RatesGqlResponse = GetRatesCourierRecommendationData
+typealias RatesApiGqlResponse = GetRatesCourierRecommendationTradeInDropOffData
+typealias RatesModel = ShippingRecommendationData
+
+class GetRatesApiUseCase @Inject constructor(
         @ApplicationContext val context: Context,
         val converter: ShippingDurationConverter,
         val gql: GraphqlUseCase) {
 
     fun execute(param: RatesParam, selectedSpId: Int, selectedServiceId: Int,
                 shopShipments: List<ShopShipment>): Observable<RatesModel> {
-        val query = GraphqlHelper.loadRawString(context.resources, R.raw.ratesv3)
-        val gqlRequest = GraphqlRequest(query, RatesGqlResponse::class.java, param.toMap())
+        val query = GraphqlHelper.loadRawString(context.resources, R.raw.ratesv3api)
+        val gqlRequest = GraphqlRequest(query, RatesApiGqlResponse::class.java, param.toMap())
 
         gql.clearRequest()
         gql.addRequest(gqlRequest)
         return gql.getExecuteObservable(null)
                 .map { graphqlResponse: GraphqlResponse ->
-                    val response = graphqlResponse.getData<RatesGqlResponse>(RatesGqlResponse::class.java)
+                    val response = graphqlResponse.getData<RatesApiGqlResponse>(RatesApiGqlResponse::class.java)
                     converter.convertModel(response.ratesData, shopShipments, selectedSpId, selectedServiceId)
                 }
                 .subscribeOn(Schedulers.io())
