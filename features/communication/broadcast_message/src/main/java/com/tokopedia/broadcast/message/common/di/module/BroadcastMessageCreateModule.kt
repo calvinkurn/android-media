8ba01ab5547998766e68a2aceb35c22203b8031d
leonard2907 <leonard.gunawan@tokopedia.com>
2020-01-22 11:10:42 +0700

    Fixed DI in broadcast message

--- features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/common/di/module/BroadcastMessageCreateModule.kt
+++ features/communication/broadcast_message/src/main/java/com/tokopedia/broadcast/message/common/di/module/BroadcastMessageCreateModule.kt
@@ -1,9 +1,17 @@
 package com.tokopedia.broadcast.message.common.di.module
 
 import com.tokopedia.broadcast.message.common.di.scope.BroadcastMessageCreateScope
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
 import com.tokopedia.shop.common.di.ShopCommonModule
 import dagger.Module
+import dagger.Provides
 
 @BroadcastMessageCreateScope
 @Module(includes = arrayOf(ShopCommonModule::class))
-class BroadcastMessageCreateModule
+class BroadcastMessageCreateModule {
+    @BroadcastMessageCreateScope
+    @Provides
+    fun provideMultiRequestGraphqlUseCase(): MultiRequestGraphqlUseCase =
+            GraphqlInteractor.getInstance().multiRequestGraphqlUseCase
+}
