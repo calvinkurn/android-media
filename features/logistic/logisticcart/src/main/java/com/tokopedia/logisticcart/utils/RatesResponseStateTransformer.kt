428f04616ad1c5b1f6db13c37ceb815b89e6d2d2
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-01-23 16:01:20 +0700

    Create transformer function to refactor ui variable in converter

---
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/utils/RatesResponseStateTransformer.kt
@@ -0,0 +1,71 @@
+package com.tokopedia.logisticcart.utils
+
+import android.text.TextUtils
+import com.tokopedia.logisticcart.shipping.model.LogisticPromoViewModel
+import com.tokopedia.logisticcart.shipping.model.ShippingRecommendationData
+import com.tokopedia.logisticcart.shipping.model.ShopShipment
+import com.tokopedia.logisticdata.data.entity.ratescourierrecommendation.ProductData
+import rx.functions.Func1
+
+class RatesResponseStateTransformer(private val shopShipments: List<ShopShipment>,
+                                    private val selectedSpId: Int,
+                                    private val selectedServiceId: Int)
+    : Func1<ShippingRecommendationData, ShippingRecommendationData> {
+
+    override fun call(response: ShippingRecommendationData?): ShippingRecommendationData {
+        val isPromoApplied = isPromoStackingApplied(response?.logisticPromo)
+        response?.shippingDurationViewModels?.forEach { duration ->
+            duration.shippingCourierViewModelList?.forEach { courier ->
+                if (selectedSpId != 0 && !isPromoApplied) {
+                    if (selectedSpId == courier.productData.shipperProductId) {
+                        courier.isSelected = true
+                        duration.isSelected = true;
+                    }
+                } else if (selectedServiceId != 0 && !isPromoApplied) {
+                    if (!(duration.serviceData.error != null &&
+                                    !TextUtils.isEmpty(duration.serviceData.error.errorId)) &&
+                            selectedServiceId == duration.serviceData.serviceId) {
+                        duration.isSelected = true;
+                    }
+                } else {
+                    courier.isSelected = courier.productData.isRecommend;
+                    duration.isSelected = false;
+                }
+                courier.additionalFee = getAdditionalFee(courier.productData, shopShipments)
+                courier.isAllowDropshipper = isAllowDropshipper(courier.productData, shopShipments)
+            }
+        }
+        return response!!
+    }
+
+    private fun getAdditionalFee(productData: ProductData, shopShipmentList: List<ShopShipment>)
+            : Int {
+        for (shopShipment in shopShipmentList) {
+            if (shopShipment.shipProds != null) {
+                for (shipProd in shopShipment.shipProds) {
+                    if (shipProd.shipProdId == productData.shipperProductId) {
+                        return shipProd.additionalFee
+                    }
+                }
+            }
+        }
+        return 0
+    }
+
+    private fun isAllowDropshipper(productData: ProductData, shopShipmentList: List<ShopShipment>)
+            : Boolean {
+        for (shopShipment in shopShipmentList) {
+            if (shopShipment.shipId == productData.shipperId) {
+                return shopShipment.isDropshipEnabled
+            }
+        }
+        return false
+    }
+
+    private fun isPromoStackingApplied(logisticPromo: LogisticPromoViewModel?): Boolean =
+            when (logisticPromo) {
+                null -> false
+                else -> logisticPromo.isApplied
+            }
+
+}
