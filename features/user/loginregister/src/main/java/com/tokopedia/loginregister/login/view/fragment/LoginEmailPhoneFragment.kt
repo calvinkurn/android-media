be46901c26ba28a98f2a9131b2524f3dd316bcf5
Ade Fulki Hadian <ade.hadian@tokopedia.com>
2020-01-24 10:52:02 +0700

    add condition before popup pin

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/view/fragment/LoginEmailPhoneFragment.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/view/fragment/LoginEmailPhoneFragment.kt
@@ -1240,7 +1240,10 @@ class LoginEmailPhoneFragment : BaseDaggerFragment(), LoginEmailPhoneContract.Vi
     private fun onSuccessCheckStatusPin(): (StatusPinData) -> Unit {
         return {
             dismissLoadingLogin()
-            if (!it.isRegistered && isFromAccountPage()) {
+            if (!it.isRegistered &&
+                    isFromAccountPage() &&
+                    userSession.phoneNumber.isNotEmpty() &&
+                    userSession.isMsisdnVerified) {
                 val intent = RouteManager.getIntent(context, ApplinkConstInternalGlobal.ADD_PIN_ONBOARDING)
                 intent.putExtra(ApplinkConstInternalGlobal.PARAM_IS_SKIP_OTP, true)
                 startActivityForResult(intent, REQUEST_ADD_PIN)
@@ -1253,7 +1256,10 @@ class LoginEmailPhoneFragment : BaseDaggerFragment(), LoginEmailPhoneContract.Vi
     private fun onSuccessCheckStatusPinAfterSQ(): (StatusPinData) -> Unit {
         return {
             dismissLoadingLogin()
-            if (!it.isRegistered && isFromAccountPage()) {
+            if (!it.isRegistered &&
+                    isFromAccountPage() &&
+                    userSession.phoneNumber.isNotEmpty() &&
+                    userSession.isMsisdnVerified) {
                 val intent = RouteManager.getIntent(context, ApplinkConstInternalGlobal.ADD_PIN_ONBOARDING)
                 intent.putExtra(ApplinkConstInternalGlobal.PARAM_IS_SKIP_OTP, true)
                 startActivityForResult(intent, REQUEST_ADD_PIN_AFTER_SQ)
