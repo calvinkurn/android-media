bdb0c8736d970c9c98b49f42b7aa7e73d9500dd2
yogieputra8 <yogie.putra8@gmail.com>
2020-01-27 14:17:45 +0700

    Implement getCategories viewmodel from fragment

--- features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_category/presentation/fragment/BrandlistContainerFragment.kt
+++ features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_category/presentation/fragment/BrandlistContainerFragment.kt
@@ -1,13 +1,15 @@
 package com.tokopedia.brandlist.brandlist_category.presentation.fragment
 
 import android.os.Bundle
+import android.view.View
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.brandlist.BrandlistInstance
 import com.tokopedia.brandlist.brandlist_category.di.BrandlistCategoryComponent
 import com.tokopedia.brandlist.brandlist_category.di.BrandlistCategoryModule
 import com.tokopedia.brandlist.brandlist_category.di.DaggerBrandlistCategoryComponent
-
+import com.tokopedia.brandlist.brandlist_category.presentation.viewmodel.BrandlistCategoryViewModel
+import javax.inject.Inject
 
 
 class BrandlistContainerFragment : BaseDaggerFragment(),
@@ -20,6 +22,14 @@ class BrandlistContainerFragment : BaseDaggerFragment(),
 //                    putParcelable(PARAM_EXTRA_SHOP_ADDRESS, shopAddressViewModel)
 //                    putBoolean(PARAM_EXTRA_IS_ADD_NEW, isAddNew)
 //                }}
+    }
+
+    @Inject
+    lateinit var viewModel: BrandlistCategoryViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getBrandlistCategories()
     }
 
     override fun getScreenName(): String {
