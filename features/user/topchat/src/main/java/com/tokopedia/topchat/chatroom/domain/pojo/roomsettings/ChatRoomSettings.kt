aae81426a1e672931f7e307898b15295959cad1a
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-01-27 13:49:13 +0700

    Rename class

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/roomsettings/ChatRoomSettings.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/roomsettings/ChatRoomSettings.kt
@@ -4,8 +4,8 @@ package com.tokopedia.topchat.chatroom.domain.pojo.roomsettings
 import com.google.gson.annotations.SerializedName
 
 data class ChatRoomSettings(
-    @SerializedName("banner")
-    val banner: Banner = Banner(),
-    @SerializedName("fraudAlert")
-    val fraudAlert: FraudAlert = FraudAlert()
+        @SerializedName("banner")
+        val banner: RoomSettingBanner = RoomSettingBanner(),
+        @SerializedName("fraudAlert")
+        val fraudAlert: RoomSettingFraudAlert = RoomSettingFraudAlert()
 )
---
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/roomsettings/RoomSettingFraudAlert.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.topchat.chatroom.domain.pojo.roomsettings
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RoomSettingFraudAlert(
+        @SerializedName("enable")
+        val enable: Boolean = false,
+        @SerializedName("imageUrl")
+        val imageUrl: String = "",
+        @SerializedName("text")
+        val text: String = ""
+)
