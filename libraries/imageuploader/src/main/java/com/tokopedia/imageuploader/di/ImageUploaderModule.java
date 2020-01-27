984d9b1da59aa3394d94efb2baddf39a92572f0f
Henry Priyono <henry.priyono@tokopedia.com>
2020-01-27 15:47:15 +0700

    Replace Remaining Chuck with Chucker

--- libraries/imageuploader/src/main/java/com/tokopedia/imageuploader/di/ImageUploaderModule.java
+++ libraries/imageuploader/src/main/java/com/tokopedia/imageuploader/di/ImageUploaderModule.java
@@ -5,7 +5,7 @@ import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.readystatesoftware.chuck.ChuckInterceptor;
+import com.chuckerteam.chucker.api.ChuckerInterceptor;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
 import com.tokopedia.abstraction.common.network.interceptor.ErrorResponseInterceptor;
 import com.tokopedia.abstraction.common.utils.GlobalConfig;
