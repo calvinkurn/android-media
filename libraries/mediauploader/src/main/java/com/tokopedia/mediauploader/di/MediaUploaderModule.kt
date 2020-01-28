92d8e0993b183db82a428127fc8e7b878075392f
isfaaghyth <isfaaghyth@gmail.com>
2020-01-28 22:42:00 +0700

    media: (provide) data policy on dependencies module

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
@@ -1,7 +1,24 @@
 package com.tokopedia.mediauploader.di
 
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.mediauploader.data.entity.DataUploaderPolicy
+import com.tokopedia.mediauploader.domain.DataPolicyUseCase
+import com.tokopedia.usecase.coroutines.UseCase
 import dagger.Module
+import dagger.Provides
 
 @Module class MediaUploaderModule {
+
+    @MediaUploaderQualifier
+    @Provides
+    fun provideDataPolicyUseCase(): UseCase<DataUploaderPolicy> {
+        return DataPolicyUseCase(
+                "",
+                GraphqlCacheStrategy.Builder(CacheType.NONE).build(),
+                GraphqlInteractor.getInstance().graphqlRepository
+        )
+    }
 
 }
