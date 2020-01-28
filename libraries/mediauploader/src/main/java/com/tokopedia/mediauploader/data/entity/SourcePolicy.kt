4f15977e59ddee804e9d9304ed19d9e9e6567a2a
isfaaghyth <isfaaghyth@gmail.com>
2020-01-28 21:39:52 +0700

    media: (added) all of entities required of media uploader

---
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/entity/SourcePolicy.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.mediauploader.data.entity
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class SourcePolicy(
+        @Expose @SerializedName("source_type") val sourceType: String = "",
+        @Expose @SerializedName("image_policy") val imagePolicy: Policy = Policy()
+)
