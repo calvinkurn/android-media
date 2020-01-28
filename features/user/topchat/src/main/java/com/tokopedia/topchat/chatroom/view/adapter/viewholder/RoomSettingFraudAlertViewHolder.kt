9d5e863f0e8e0afdac760a94678a47812dfb05eb
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-01-28 13:40:31 +0700

    Use htmlLink helper from unify

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/RoomSettingFraudAlertViewHolder.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/RoomSettingFraudAlertViewHolder.kt
@@ -1,10 +1,10 @@
 package com.tokopedia.topchat.chatroom.view.adapter.viewholder
 
 import android.view.View
-import androidx.core.text.HtmlCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.domain.pojo.roomsettings.RoomSettingFraudAlert
+import com.tokopedia.unifycomponents.HtmlLinkHelper
 import kotlinx.android.synthetic.main.item_topchat_room_setting_fraud_alert.view.*
 
 class RoomSettingFraudAlertViewHolder(itemView: View?) : AbstractViewHolder<RoomSettingFraudAlert>(itemView) {
@@ -15,8 +15,8 @@ class RoomSettingFraudAlertViewHolder(itemView: View?) : AbstractViewHolder<Room
     }
 
     private fun bindAlertText(alert: RoomSettingFraudAlert) {
-        val htmlText = HtmlCompat.fromHtml(alert.text, HtmlCompat.FROM_HTML_MODE_LEGACY)
-        itemView.tvText?.text = htmlText
+        val htmlText = HtmlLinkHelper(itemView.context, alert.text)
+        itemView.tvText?.text = htmlText.spannedString
     }
 
     companion object {
