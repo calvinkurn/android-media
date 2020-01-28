c60243edcc8fa62350bbd64985e15809730ef992
isfaaghyth <isfaaghyth@gmail.com>
2020-01-28 23:45:50 +0700

    media: (created) data policy graphql query

---
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/consts/MediaUploaderQuery.kt
@@ -0,0 +1,30 @@
+package com.tokopedia.mediauploader.data.consts
+
+object MediaUploaderQuery {
+
+    private const val paramSourceId = "\$source"
+
+    var dataPolicyQuery = """
+        dataPolicyQuery uploader(source: String){
+          uploadpedia_policy(source: $paramSourceId){
+            source_policy{
+              host
+              source_type
+              image_policy{
+                max_file_size
+                max_res {
+                  w
+                  h
+                }
+                min_res {
+                  w
+                  h
+                }
+                allowed_ext
+              }
+            }
+          }
+        }
+    """.trimIndent()
+
+}
