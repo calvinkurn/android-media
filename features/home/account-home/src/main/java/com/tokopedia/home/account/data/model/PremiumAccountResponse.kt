8411d4001afdb479f41f7e1bd0119cbb57c3e61f
lalit.singh <lalit.singh@tokopedia.com>
2020-01-29 13:14:13 +0530

    Unused fields removed from GQL removed commented code

--- features/home/account-home/src/main/java/com/tokopedia/home/account/data/model/PremiumAccountResponse.kt
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/data/model/PremiumAccountResponse.kt
@@ -41,31 +41,3 @@ data class PremiumAccountCopyWriting(
 )
 
 
-
-/*@SerializedName("isPowerWD")
-@Expose
-var isIsPowerWD: Boolean = false,*/
-/*@SerializedName("shopID")
-@Expose
-var shopID: Long = 0,
-@SerializedName("accNum")
-@Expose
-var accNum: String? = null,
-@SerializedName("bankID")
-@Expose
-var bankID: Long = 0,
-@SerializedName("userID")
-@Expose
-var userID: Long = 0,
-@SerializedName("status")
-@Expose
-var status: String? = null,
-@SerializedName("program")
-@Expose
-var program: String? = null,
-@SerializedName("wdPoints")
-@Expose
-var wdPoints: Long = 0,
-@SerializedName("statusInt")
-@Expose
-var statusInt: Int = 0,*/
