1edd1d13e0eeb3e3c2d2d0e9d99df3e1f921c63f
darianthedytoped <darian.thedy@tokopedia.com>
2020-01-30 15:16:47 +0700

    Experimenting instrumentation test of product card

---
+++ features/discovery/productcard/src/androidTest/java/com/tokopedia/productcard/ProductCardSmallGridActivityTest.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.productcard
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProductCardSmallGridActivityTest: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
