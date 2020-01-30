f19d24c48f40656196a5ba033826a0cff896eb1b
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2020-01-30 11:07:01 +0700

    - reduce ovo cache expired time - prevent moengage library to push mainparentactivity layout

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/activity/MainParentActivity.java
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/activity/MainParentActivity.java
@@ -567,6 +567,8 @@ public class MainParentActivity extends BaseActivity implements
         if (!((BaseMainApplication) getApplication()).checkAppSignature()) {
             finish();
         }
+
+        if (currentFragment != null) configureStatusBarBasedOnFragment(currentFragment);
     }
 
     @Override
