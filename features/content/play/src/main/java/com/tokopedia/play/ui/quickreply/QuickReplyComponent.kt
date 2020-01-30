958e796f15c0cadf60b5b96f7e2e226520c3e554
kenny <kenny.hadisaputra@tokopedia.com>
2020-01-30 13:53:52 +0700

    - quick reply component unit test

--- features/content/play/src/main/java/com/tokopedia/play/ui/quickreply/QuickReplyComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/quickreply/QuickReplyComponent.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.play.ui.quickreply
 
 import android.view.ViewGroup
+import androidx.annotation.VisibleForTesting
 import com.tokopedia.play.component.EventBusFactory
 import com.tokopedia.play.component.UIComponent
 import com.tokopedia.play.ui.quickreply.interaction.QuickReplyInteractionEvent
@@ -13,13 +14,14 @@ import kotlinx.coroutines.launch
 /**
  * Created by jegul on 13/12/19
  */
-class QuickReplyComponent(
+open class QuickReplyComponent(
         container: ViewGroup,
         private val bus: EventBusFactory,
         coroutineScope: CoroutineScope
 ) : UIComponent<QuickReplyInteractionEvent>, CoroutineScope by coroutineScope, QuickReplyView.Listener {
 
-    private val uiView = initView(container)
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    val uiView = initView(container)
 
     init {
         uiView.hide()
@@ -50,6 +52,6 @@ class QuickReplyComponent(
         }
     }
 
-    private fun initView(container: ViewGroup) =
+    protected open fun initView(container: ViewGroup) =
             QuickReplyView(container, this)
 }
