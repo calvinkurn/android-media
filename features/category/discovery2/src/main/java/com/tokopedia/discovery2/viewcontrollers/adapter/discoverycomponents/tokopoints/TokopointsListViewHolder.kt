27d1de2a626b7c152e636d664c59c7b4ceed63e6
Zishanr <zishan.rashid@tokopedia.com>
2020-01-31 15:28:20 +0530

    working on Tokopoints

---
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/tokopoints/TokopointsListViewHolder.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.tokopoints
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.discovery2.R
+import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
+import com.tokopedia.discovery2.viewcontrollers.adapter.DiscoveryRecycleAdapter
+import com.tokopedia.discovery2.viewcontrollers.adapter.viewholder.AbstractViewHolder
+
+class TokopointsListViewHolder(itemView: View, private val fragment: Fragment) : AbstractViewHolder(itemView) {
+
+    private lateinit var mTokopointsRecyclerView: RecyclerView
+    private lateinit var mDiscoveryRecycleAdapter: DiscoveryRecycleAdapter
+
+    override fun bindView(lifecycleOwner: LifecycleOwner, discoveryBaseViewModel: DiscoveryBaseViewModel) {
+        initView(itemView)
+    }
+
+    private fun initView(itemView: View) {
+        mTokopointsRecyclerView = itemView.findViewById(R.id.discovery_recyclerView)
+        mTokopointsRecyclerView.layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
+        mDiscoveryRecycleAdapter = DiscoveryRecycleAdapter(fragment)
+        mTokopointsRecyclerView.adapter = mDiscoveryRecycleAdapter
+    }
+}
---
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/tokopoints/TokopointsViewModel.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.tokopoints
+
+import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
+
+class TokopointsViewModel : DiscoveryBaseViewModel() {
+
+    override fun initDaggerInject() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun fetchTokopointsData() {
+
+    }
+}
