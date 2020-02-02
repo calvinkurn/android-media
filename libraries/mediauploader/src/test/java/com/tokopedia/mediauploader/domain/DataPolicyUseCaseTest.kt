d0a7c7e4634dfbf4eea3f1a756572aee35fc8f79
isfaaghyth <isfaaghyth@gmail.com>
2020-02-02 08:46:46 +0700

    uploader: (test) change test case of data policy use case

--- libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/DataPolicyUseCaseTest.kt
+++ libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/DataPolicyUseCaseTest.kt
@@ -1,13 +1,18 @@
 package com.tokopedia.mediauploader.domain
 
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.mediauploader.data.consts.MediaUploaderQuery
 import com.tokopedia.mediauploader.data.entity.DataUploaderPolicy
+import com.tokopedia.mediauploader.data.entity.SourcePolicy
+import com.tokopedia.mediauploader.data.entity.UploaderPolicy
 import com.tokopedia.usecase.coroutines.UseCase
 import io.mockk.MockKStubScope
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
@@ -15,9 +20,11 @@ class DataPolicyUseCaseTest: Spek({
 
     Feature("data policy use case") {
 
-        val cacheStrategy = mockk<GraphqlCacheStrategy>(relaxed = true)
         val repository = mockk<GraphqlRepository>(relaxed = true)
-        val useCase = DataPolicyUseCase(MediaUploaderQuery.dataPolicyQuery, cacheStrategy, repository)
+        val useCase = DataPolicyUseCase(MediaUploaderQuery.dataPolicyQuery, repository)
+        val dataUploaderPolicy = DataUploaderPolicy(
+                UploaderPolicy(SourcePolicy(sourceType = "test"))
+        )
 
         Scenario("create param with source id") {
             var requestParams = mapOf<String, Any>()
@@ -26,33 +33,56 @@ class DataPolicyUseCaseTest: Spek({
             Given("source id") {
                 sourceId = "HUgbQS"
             }
-
             When("create param") {
                 requestParams = DataPolicyUseCase.createParamDataPolicy(sourceId)
             }
-
             Then("it should return source id correctly") {
                 assert(requestParams["source"] == sourceId)
             }
         }
 
         Scenario("request data policy with correct source id") {
-            val dataUploaderPolicy = DataUploaderPolicy()
+            var returnValue: DataUploaderPolicy?= null
 
             Given("request param") {
                 useCase.requestParams = DataPolicyUseCase.createParamDataPolicy("id")
-            }
 
-            When("get data") {
-                useCase.stubExecuteOnBackground().returns(dataUploaderPolicy)
-            }
-
-            Then("should return policy of uploader") {
                 coEvery {
-                    assert(useCase.executeOnBackground() == dataUploaderPolicy)
+                    repository.getReseponse(listOf()).getData<DataUploaderPolicy>(DataUploaderPolicy::class.java)
+                } answers {
+                    GraphqlResponse(
+                            mapOf(DataUploaderPolicy::class.java to DataUploaderPolicy()),
+                            null,
+                            false
+                    ).getData<DataUploaderPolicy>(DataUploaderPolicy::class.java)
+                }
+
+                coEvery {
+                    repository.getReseponse(listOf()).getError(
+                            DataUploaderPolicy::class.java
+                    )
+                } answers {
+                    listOf()
                 }
             }
+            When("get policy uploader") {
+                runBlocking { useCase.executeOnBackground() }
+            }
+            Then("it should return policy of uploader") {
+                useCase.execute({}, {})
+            }
         }
+
+//        Scenario("request data policy with param empty") {
+//            When("get policy uploader") {
+//                graphqlUseCase.stubExecuteOnBackground().returns(dataUploaderPolicy)
+//            }
+//            Then("it should return throw exception of missing param") {
+//                useCase.get({}, {
+//                    assertEquals(it, Throwable(""))
+//                })
+//            }
+//        }
     }
 
 })
