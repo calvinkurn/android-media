7c2e7d5acb8335dcaeddcce75891eb2b4b2bf9eb
darianthedytoped <darian.thedy@tokopedia.com>
2020-02-05 17:07:04 +0700

    Fix auto complete ANR

--- libraries/logger/src/main/java/com/tokopedia/logger/service/ServerJobService.kt
+++ libraries/logger/src/main/java/com/tokopedia/logger/service/ServerJobService.kt
@@ -7,31 +7,33 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Build
 import com.tokopedia.logger.LogManager
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-class ServerJobService : JobService() {
+class ServerJobService : JobService(), CoroutineScope {
+
+    private val masterJob = SupervisorJob()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + masterJob
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        runBlocking {
-            launch(Dispatchers.IO) {
-                when {
-                    // When there is network connection and there is data in DB then we send logs to server
-                    isNetworkAvailable(application) and (LogManager.getCount() > 0) -> {
-                        LogManager.deleteExpiredLogs()
-                        LogManager.sendLogToServer()
-                        jobFinished(params,false)
-                    }
-                    // When there is data in DB but no network connection, we check this data, if its old we delete it
-                    LogManager.loggerRepository.getCount() > 0 -> {
-                        LogManager.deleteExpiredLogs()
-                        jobFinished(params,false)
-                    }
-                    else -> {
-                        jobFinished(params,false)
-                    }
+        launch(Dispatchers.IO) {
+            when {
+                // When there is network connection and there is data in DB then we send logs to server
+                isNetworkAvailable(application) and (LogManager.getCount() > 0) -> {
+                    LogManager.deleteExpiredLogs()
+                    LogManager.sendLogToServer()
+                    jobFinished(params,false)
+                }
+                // When there is data in DB but no network connection, we check this data, if its old we delete it
+                LogManager.loggerRepository.getCount() > 0 -> {
+                    LogManager.deleteExpiredLogs()
+                    jobFinished(params,false)
+                }
+                else -> {
+                    jobFinished(params,false)
                 }
             }
         }
