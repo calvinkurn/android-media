334f2c04fbfdfe923883d2dd8fdfbec12267ad84
isfaaghyth <isfaaghyth@gmail.com>
2020-02-05 11:26:29 +0700

    media-di: (removed) unused module provider

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
@@ -1,23 +1,12 @@
 package com.tokopedia.mediauploader.di
 
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.mediauploader.data.consts.MediaUploaderQuery
-import com.tokopedia.mediauploader.data.entity.DataUploaderPolicy
 import com.tokopedia.mediauploader.domain.DataPolicyUseCase
 import dagger.Module
 import dagger.Provides
 
 @Module class MediaUploaderModule {
-
-//    @MediaUploaderQualifier
-//    @Provides
-//    fun provideGraphqlDataPolicyUseCase(
-//            repository: GraphqlRepository
-//    ): GraphqlUseCase<DataUploaderPolicy> {
-//        return GraphqlUseCase(repository)
-//    }
 
     @MediaUploaderQualifier
     @Provides
