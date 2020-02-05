db8aab102bb7a6b2b2e4cf1be4c140a371063dca
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-02-05 23:11:59 +0700

    add feature reply chat through notification

---
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/service/NotificationChatService.kt
@@ -0,0 +1,67 @@
+package com.tokopedia.topchat.chatroom.service
+
+import android.app.IntentService
+import android.content.Intent
+import androidx.core.app.RemoteInput
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.chat_common.data.ReplyChatViewModel
+import com.tokopedia.topchat.chatroom.di.DaggerChatComponent
+import com.tokopedia.topchat.chatroom.domain.usecase.ReplyChatUseCase
+import rx.Subscriber
+import javax.inject.Inject
+import androidx.core.app.NotificationManagerCompat
+
+class NotificationChatService : IntentService("NotificationChatService") {
+
+    private val REPLY_KEY = "replay_chat_key"
+    private val MESSAGE_ID = "message_chat_id"
+    private val NOTIFICATION_ID = "notification_id"
+
+    @Inject
+    lateinit var replyChatUseCase: ReplyChatUseCase
+
+    override fun onCreate() {
+        super.onCreate()
+        DaggerChatComponent.builder()
+                .baseAppComponent((application as BaseMainApplication).baseAppComponent)
+                .build()
+                .inject(this)
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+
+        if (intent != null) {
+            val messageId = intent.getStringExtra(MESSAGE_ID)
+            val message = remoteInput?.getCharSequence(REPLY_KEY)
+            val notificationId = intent.getIntExtra(NOTIFICATION_ID, 0)
+
+            val requestParam = ReplyChatUseCase.generateParam(messageId, message.toString())
+
+            replyChatUseCase.execute(requestParam, object : Subscriber<ReplyChatViewModel>() {
+                override fun onNext(response: ReplyChatViewModel?) {
+                    if (response != null) {
+                        if (response.isSuccessReplyChat) {
+                            clearNotification(notificationId)
+                        }
+                    }
+                }
+
+                override fun onCompleted() {}
+
+                override fun onError(e: Throwable?) {
+                    clearNotification(notificationId)
+                }
+
+            })
+
+        }
+    }
+
+    private fun clearNotification(notificationId: Int) {
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        notificationManager.cancel(notificationId)
+    }
+
+}
--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/factory/ChatNotificationFactory.java
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/factory/ChatNotificationFactory.java
@@ -1,9 +1,12 @@
 package com.tokopedia.pushnotif.factory;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
+
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
 
 import com.tokopedia.pushnotif.ApplinkNotificationHelper;
 import com.tokopedia.pushnotif.Constant;
@@ -13,7 +16,11 @@ import com.tokopedia.pushnotif.model.ApplinkNotificationModel;
  * @author ricoharisin .
  */
 
-public class ChatNotificationFactory extends BaseNotificationFactory{
+public class ChatNotificationFactory extends BaseNotificationFactory {
+
+    private static String REPLY_KEY = "replay_chat_key";
+    private static String REPLY_LABEL = "Replay";
+
     public ChatNotificationFactory(Context context) {
         super(context);
     }
@@ -39,6 +46,42 @@ public class ChatNotificationFactory extends BaseNotificationFactory{
             if (isAllowVibrate()) builder.setVibrate(getVibratePattern());
         }
 
+        builder.setShowWhen(true);
+        builder.addAction(replyAction(applinkNotificationModel.getApplinks(), notificationId));
+
         return builder.build();
     }
+
+    private NotificationCompat.Action replyAction(String appLinks, int notificationId) {
+
+        return new NotificationCompat.Action.Builder(
+                android.R.drawable.stat_notify_chat, REPLY_LABEL,
+                getReplyChatPendingIntent(getMessageId(appLinks), notificationId))
+                .addRemoteInput(remoteInput())
+                .setAllowGeneratedReplies(true)
+                .build();
+    }
+
+    private RemoteInput remoteInput() {
+        return new RemoteInput.Builder(REPLY_KEY).setLabel(REPLY_LABEL).build();
+    }
+
+    private PendingIntent getReplyChatPendingIntent(String mMessageId, int notificationId) {
+
+        Intent intent = new Intent("com.tokopedia.topchat.chatroom.service.NotificationChatService.REPLY_CHAT");
+        intent.setPackage(context.getPackageName());
+        intent.putExtra("message_chat_id", mMessageId);
+        intent.putExtra("notification_id", notificationId);
+
+        return PendingIntent.getService(context, 100, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private String getMessageId(String appLinks) {
+        String startString = "tokopedia://topchat/";
+        int lengthStart = startString.length();
+        int lastIndex = appLinks.lastIndexOf("?");
+        return appLinks.substring(lengthStart, lastIndex);
+    }
+
 }
