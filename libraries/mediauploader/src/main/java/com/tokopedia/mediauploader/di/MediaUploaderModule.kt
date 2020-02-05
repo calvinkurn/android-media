8529bc3601053f8a4ca9a55b8f7d292d59c1f623
isfaaghyth <isfaaghyth@gmail.com>
2020-02-05 11:33:46 +0700

    media-di: (added) provide media uploader module

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderModule.kt
@@ -1,8 +1,10 @@
 package com.tokopedia.mediauploader.di
 
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.mediauploader.data.UploaderServices
 import com.tokopedia.mediauploader.data.consts.MediaUploaderQuery
 import com.tokopedia.mediauploader.domain.DataPolicyUseCase
+import com.tokopedia.mediauploader.domain.MediaUploaderUseCase
 import dagger.Module
 import dagger.Provides
 
@@ -10,11 +12,19 @@ import dagger.Provides
 
     @MediaUploaderQualifier
     @Provides
-    fun provideDataPolicyUseCase() : DataPolicyUseCase {
+    fun provideDataPolicyUseCase(): DataPolicyUseCase {
         return DataPolicyUseCase(
                 MediaUploaderQuery.dataPolicyQuery,
                 GraphqlInteractor.getInstance().graphqlRepository
         )
+    }
+
+    @MediaUploaderQualifier
+    @Provides
+    fun provideMediaUploaderUseCase(
+            @MediaUploaderQualifier services: UploaderServices
+    ): MediaUploaderUseCase {
+        return MediaUploaderUseCase(services)
     }
 
 }
