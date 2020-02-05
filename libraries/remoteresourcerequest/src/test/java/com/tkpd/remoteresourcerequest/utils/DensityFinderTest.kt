cc3f99fabf5f88ffb3071e420fe5ee969271ed7f
noiz354 <noiz354@gmail.com>
2020-02-05 15:55:31 +0700

    fix to run things.

--- libraries/remoteresourcerequest/src/test/java/com/tkpd/remoteresourcerequest/utils/DensityFinderTest.kt
+++ libraries/remoteresourcerequest/src/test/java/com/tkpd/remoteresourcerequest/utils/DensityFinderTest.kt
@@ -110,7 +110,7 @@ class DensityFinderTest {
     fun findDensityReturnEmpty(){
         every { mockRes.displayMetrics } returns metrics
         every { context.resources } returns mockRes
-        metrics.densityDpi = DisplayMetrics.DENSITY_140
+        metrics.densityDpi = DisplayMetrics.DENSITY_DEFAULT
         val density = DensityFinder.findDensity(context)
 
         assertEquals("", density)
