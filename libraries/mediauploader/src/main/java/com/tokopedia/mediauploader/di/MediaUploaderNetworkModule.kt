1494b7f7b3e3c98ed058db270c865a795ade95bf
isfaaghyth <isfaaghyth@gmail.com>
2020-02-05 11:26:05 +0700

    media-di: (added) media uploader network dependencies

---
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/di/MediaUploaderNetworkModule.kt
@@ -0,0 +1,25 @@
+package com.tokopedia.mediauploader.di
+
+import com.tokopedia.mediauploader.data.UploaderServices
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@Module class MediaUploaderNetworkModule {
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideLiveRetrofit(
+            @MediaUploaderQualifier retrofit: Retrofit.Builder,
+            @MediaUploaderQualifier okHttpClient: OkHttpClient.Builder): Retrofit {
+        return retrofit.client(okHttpClient.build()).build()
+    }
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideLiveServices(retrofit: Retrofit): UploaderServices {
+        return retrofit.create(UploaderServices::class.java)
+    }
+
+}
