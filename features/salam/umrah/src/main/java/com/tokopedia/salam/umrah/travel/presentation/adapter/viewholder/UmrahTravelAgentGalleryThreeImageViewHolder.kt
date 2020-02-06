23d7de328dbbd24ae0c9a6ce2a58257d39479660
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-02-06 19:29:11 +0700

    add foreground di image yang > 3

--- features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/travel/presentation/adapter/viewholder/UmrahTravelAgentGalleryThreeImageViewHolder.kt
+++ features/salam/umrah/src/main/java/com/tokopedia/salam/umrah/travel/presentation/adapter/viewholder/UmrahTravelAgentGalleryThreeImageViewHolder.kt
@@ -7,6 +7,7 @@ import com.tokopedia.applink.RouteManager
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.invisible
 import com.tokopedia.kotlin.extensions.view.loadImage
+import com.tokopedia.kotlin.extensions.view.loadImageRounded
 import com.tokopedia.salam.umrah.R
 import com.tokopedia.salam.umrah.travel.data.UmrahGalleriesEntity
 import com.tokopedia.salam.umrah.travel.data.UmrahGallery
@@ -24,13 +25,13 @@ class UmrahTravelAgentGalleryThreeImageViewHolder(view: View) : AbstractViewHold
             if(element.description.isNotEmpty()) tg_umrah_gallery_three_image_desc.text = element.description
             else tg_umrah_gallery_three_image_desc.gone()
 
-            if(element.medias[0].source.isNotEmpty()) iw_umrah_gallery_three_image_first.loadImage(element.medias[0].source)
+            if(element.medias[0].source.isNotEmpty()) iw_umrah_gallery_three_image_first.loadImageRounded(element.medias[0].source,20.0f)
             else iw_umrah_gallery_three_image_first.invisible()
 
-            if(element.medias[1].source.isNotEmpty()) iw_umrah_gallery_three_image_second.loadImage(element.medias[1].source)
+            if(element.medias[1].source.isNotEmpty()) iw_umrah_gallery_three_image_second.loadImageRounded(element.medias[1].source,20.0f)
             else iw_umrah_gallery_three_image_second.invisible()
 
-            if(element.medias[2].source.isNotEmpty()) iw_umrah_gallery_three_image_third.loadImage(element.medias[0].source)
+            if(element.medias[2].source.isNotEmpty()) iw_umrah_gallery_three_image_third.loadImageRounded(element.medias[0].source,20.0f)
             else iw_umrah_gallery_three_image_third.invisible()
         }
     }
