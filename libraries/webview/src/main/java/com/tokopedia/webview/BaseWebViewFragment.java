3c17193e44948a2a22da34ca270ac6e7aa2e0b8a
mzennis <meyta.taliti@tokopedia.com>
2020-02-07 14:26:01 +0700

    check when web access applink login

--- libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
+++ libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
@@ -65,7 +65,7 @@ public abstract class BaseWebViewFragment extends BaseDaggerFragment {
     public final static int ATTACH_FILE_REQUEST = 1;
     private static final String HCI_CAMERA_KTP = "android-js-call://ktp";
     private static final String HCI_CAMERA_SELFIE = "android-js-call://selfie";
-    private static final String PLAY_LOGIN_URL = "tokopedia://login?ld=https://www.tokopedia.com/play/channels/?is_app=1";
+    private static final String LOGIN_APPLINK = "tokopedia://login";
     String mJsHciCallbackFuncName;
     public static final int HCI_CAMERA_REQUEST_CODE = 978;
     private static final int REQUEST_CODE_LOGIN = 1233;
@@ -479,7 +479,7 @@ public abstract class BaseWebViewFragment extends BaseDaggerFragment {
                 return false;
             }
         }
-        if (url.contains(PLAY_LOGIN_URL)) {
+        if (url.contains(LOGIN_APPLINK)) {
             startActivityForResult(RouteManager.getIntent(getActivity(), url), REQUEST_CODE_LOGIN);
             return true;
         }
