d4b925046ac16587b0d53b2f5c5475bd805e0975
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-02-08 20:10:00 +0700

    Revert "remove module change password no-op"
    This reverts commit 0f74040b

---
+++ features/user/changepassword_no_op/src/main/java/com/tokopedia/changepassword/ChangePasswordRouter.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.changepassword
+
+import android.app.Activity
+
+/**
+ * @author by nisie on 7/25/18.
+ */
+interface ChangePasswordRouter {
+
+    fun logoutToHome(activity: Activity)
+}
