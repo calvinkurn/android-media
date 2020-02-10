d6c49d97681a82a629344107f2027d8b39dfd18d
darianthedytoped <darian.thedy@tokopedia.com>
2020-02-10 10:52:40 +0700

    Refactor tests

--- features/discovery/productcard/src/androidTest/java/com/tokopedia/productcard/test/ProductCardGridActivityTest.kt
+++ features/discovery/productcard/src/androidTest/java/com/tokopedia/productcard/test/ProductCardGridActivityTest.kt
@@ -1,19 +1,16 @@
 package com.tokopedia.productcard.test
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.tokopedia.productcard.test.utils.ProductCardItemDecoration
 import com.tokopedia.productcard.v2.ProductCardModel
 import com.tokopedia.productcard.v3.ProductCardGridView
-import kotlin.math.cos
-import kotlin.math.roundToInt
 
 internal class ProductCardGridActivityTest: AppCompatActivity() {
 
@@ -34,7 +31,7 @@ internal class ProductCardGridActivityTest: AppCompatActivity() {
     }
 
     private fun createItemDecoration(): RecyclerView.ItemDecoration {
-        return ItemDecoration(resources.getDimensionPixelSize(com.tokopedia.design.R.dimen.dp_16))
+        return ProductCardItemDecoration(resources.getDimensionPixelSize(com.tokopedia.design.R.dimen.dp_16))
     }
 
     class Adapter: RecyclerView.Adapter<ViewHolder>() {
@@ -61,114 +58,6 @@ internal class ProductCardGridActivityTest: AppCompatActivity() {
 
             productCardView?.setProductModel(productCardModel)
             productCardView?.setOnClickListener { Toast.makeText(itemView.context, adapterPosition.toString(), Toast.LENGTH_SHORT).show() }
-        }
-    }
-
-    class ItemDecoration(private val spacing: Int): RecyclerView.ItemDecoration() {
-
-        private var verticalCardViewOffset = 0
-        private var horizontalCardViewOffset = 0
-
-        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-            verticalCardViewOffset = getVerticalCardViewOffset(view)
-            horizontalCardViewOffset = getHorizontalCardViewOffset(view)
-
-            val absolutePos = parent.getChildAdapterPosition(view)
-            val relativePos = getProductItemRelativePosition(view)
-            val totalSpanCount = getTotalSpanCount(parent)
-
-            outRect.left = getLeftOffset(relativePos, totalSpanCount)
-            outRect.top = getTopOffset(absolutePos, totalSpanCount)
-            outRect.right = getRightOffset(relativePos, totalSpanCount)
-            outRect.bottom = getBottomOffset()
-        }
-
-        private fun getVerticalCardViewOffset(view: View): Int {
-            if (view is CardView) {
-                val maxElevation = view.maxCardElevation
-                val radius = view.radius
-                return (maxElevation * 1.5 + (1 - cos(45.0)) * radius).toFloat().roundToInt() / 2
-            }
-
-            return 0
-        }
-
-        private fun getHorizontalCardViewOffset(view: View): Int {
-            if (view is CardView) {
-                val maxElevation = view.maxCardElevation
-                val radius = view.radius
-                return (maxElevation + (1 - cos(45.0)) * radius).toFloat().roundToInt() / 2
-            }
-
-            return 0
-        }
-
-        private fun getProductItemRelativePosition(view: View): Int {
-            return if (view.layoutParams is StaggeredGridLayoutManager.LayoutParams) {
-                getProductItemRelativePositionStaggeredGrid(view)
-            } else 0
-        }
-
-        private fun getProductItemRelativePositionStaggeredGrid(view: View): Int {
-            val staggeredGridLayoutParams = view.layoutParams as StaggeredGridLayoutManager.LayoutParams
-            return staggeredGridLayoutParams.spanIndex
-        }
-
-        private fun getTotalSpanCount(parent: RecyclerView): Int {
-            val layoutManager = parent.layoutManager
-            return if (layoutManager is StaggeredGridLayoutManager) layoutManager.spanCount else 0
-        }
-
-        private fun getLeftOffset(relativePos: Int, totalSpanCount: Int): Int {
-            return if (isFirstInRow(relativePos, totalSpanCount)) getLeftOffsetFirstInRow() else getLeftOffsetNotFirstInRow()
-        }
-
-        private fun isFirstInRow(relativePos: Int, spanCount: Int): Boolean {
-            return relativePos % spanCount == 0
-        }
-
-        private fun getLeftOffsetFirstInRow(): Int {
-            return spacing - horizontalCardViewOffset
-        }
-
-        private fun getLeftOffsetNotFirstInRow(): Int {
-            return spacing / 4 - horizontalCardViewOffset
-        }
-
-        private fun getTopOffset(absolutePos: Int, totalSpanCount: Int): Int {
-            return if (isTopProductItem(absolutePos, totalSpanCount)) getTopOffsetTopItem() else getTopOffsetNotTopItem()
-        }
-
-        private fun isTopProductItem(absolutePos: Int, totalSpanCount: Int): Boolean {
-            return absolutePos < totalSpanCount
-        }
-
-        private fun getTopOffsetTopItem(): Int {
-            return spacing - verticalCardViewOffset
-        }
-
-        private fun getTopOffsetNotTopItem(): Int {
-            return spacing / 4 - verticalCardViewOffset
-        }
-
-        private fun getRightOffset(relativePos: Int, totalSpanCount: Int): Int {
-            return if (isLastInRow(relativePos, totalSpanCount)) getRightOffsetLastInRow() else getRightOffsetNotLastInRow()
-        }
-
-        private fun isLastInRow(relativePos: Int, spanCount: Int): Boolean {
-            return relativePos % spanCount == spanCount - 1
-        }
-
-        private fun getRightOffsetLastInRow(): Int {
-            return spacing - horizontalCardViewOffset
-        }
-
-        private fun getRightOffsetNotLastInRow(): Int {
-            return spacing / 4 - horizontalCardViewOffset
-        }
-
-        private fun getBottomOffset(): Int {
-            return spacing / 4 - verticalCardViewOffset
         }
     }
 }
