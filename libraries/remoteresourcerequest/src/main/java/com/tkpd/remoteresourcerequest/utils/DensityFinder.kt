1d366f3d5f30cad0071fc98b7ba8e7108dabd203
lalit.singh <lalit.singh@tokopedia.com>
2020-02-10 11:04:26 +0530

    worker log transmitted through ResourceDownloadManager callback property

--- libraries/remoteresourcerequest/src/main/java/com/tkpd/remoteresourcerequest/utils/DensityFinder.kt
+++ libraries/remoteresourcerequest/src/main/java/com/tkpd/remoteresourcerequest/utils/DensityFinder.kt
@@ -40,13 +40,6 @@ class DensityFinder {
                 DisplayMetrics.DENSITY_XXHIGH, DisplayMetrics.DENSITY_340,
                 DisplayMetrics.DENSITY_360, DisplayMetrics.DENSITY_400,
                 DisplayMetrics.DENSITY_420, DisplayMetrics.DENSITY_440,
-                DisplayMetrics.DENSITY_450 -> {
-                    CallbackDispatcher.dispatchLog(
-                            callback,
-                            "device density = XXHDPI"
-                    )
-                    return "xxhdpi"
-                }
                 DisplayMetrics.DENSITY_XXXHIGH, DisplayMetrics.DENSITY_560 -> {
                     CallbackDispatcher.dispatchLog(
                             callback,
