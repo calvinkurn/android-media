26b653aecf56511330926711f73664fe7c26db42
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-02-11 15:30:02 +0700

    Improve response for autofill

--- features/logistic/logisticdata/src/main/java/com/tokopedia/logisticdata/domain/usecase/RevGeocodeUseCase.kt
+++ features/logistic/logisticdata/src/main/java/com/tokopedia/logisticdata/domain/usecase/RevGeocodeUseCase.kt
@@ -7,6 +7,7 @@ import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.logisticdata.R
 import com.tokopedia.logisticdata.data.entity.response.AutoFillResponse
+import com.tokopedia.logisticdata.data.entity.response.KeroMapsAutofill
 import com.tokopedia.network.exception.MessageErrorException
 import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
@@ -17,7 +18,7 @@ class RevGeocodeUseCase @Inject constructor(
         @ApplicationContext val context: Context,
         val gql: GraphqlUseCase) {
 
-    fun execute(latlng: String): Observable<AutoFillResponse> {
+    fun execute(latlng: String): Observable<KeroMapsAutofill> {
         val param: Map<String, Any> = mapOf(
                 PARAM_LATLNG to latlng,
                 PARAM_ERR to true
@@ -31,7 +32,7 @@ class RevGeocodeUseCase @Inject constructor(
                 .map { gqlResponse ->
                     val response: AutoFillResponse? =
                             gqlResponse.getData(AutoFillResponse::class.java)
-                    response ?: throw MessageErrorException(
+                    response?.keroMapsAutofill ?: throw MessageErrorException(
                             gqlResponse.getError(AutoFillResponse::class.java)[0].message
                     )
                 }
