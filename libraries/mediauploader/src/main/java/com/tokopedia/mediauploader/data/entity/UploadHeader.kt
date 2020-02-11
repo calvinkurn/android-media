1e904c3e1e8793bae5374d41a831f118d6d4b6cc
isfaaghyth <isfaaghyth@gmail.com>
2020-02-11 10:47:54 +0700

    media: (modified) change process_time to float

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/entity/UploadHeader.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/entity/UploadHeader.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class UploadHeader(
-        @Expose @SerializedName("process_time") val processTime: Int = 0,
+        @Expose @SerializedName("process_time") val processTime: Float = 0f,
         @Expose @SerializedName("reason") val reason: String = "",
         @Expose @SerializedName("error_code") val errorCode: String = ""
 )
