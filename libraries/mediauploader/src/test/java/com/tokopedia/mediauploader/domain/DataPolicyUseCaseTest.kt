7dff82ae05b11f6026c8e1eb2e0fe28750d87208
isfaaghyth <isfaaghyth@gmail.com>
2020-02-12 13:10:50 +0700

    media: (clean-up) unit test class of data policy use case

--- libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/DataPolicyUseCaseTest.kt
+++ libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/DataPolicyUseCaseTest.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.mediauploader.domain
 
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.mediauploader.data.consts.MediaUploaderQuery
 import com.tokopedia.mediauploader.data.entity.DataUploaderPolicy
@@ -12,7 +14,6 @@ import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
@@ -21,20 +22,17 @@ class DataPolicyUseCaseTest: Spek({
     Feature("data policy use case") {
 
         val repository = mockk<GraphqlRepository>(relaxed = true)
-        val useCase = DataPolicyUseCase(MediaUploaderQuery.dataPolicyQuery, repository)
+        val useCase = DataPolicyUseCase(repository)
+        val sourceId = "WXjxja"
         val dataUploaderPolicy = DataUploaderPolicy(
                 UploaderPolicy(SourcePolicy(sourceType = "test"))
         )
 
         Scenario("create param with source id") {
             var requestParams = mapOf<String, Any>()
-            var sourceId = ""
 
-            Given("source id") {
-                sourceId = "HUgbQS"
-            }
             When("create param") {
-                requestParams = DataPolicyUseCase.createParamDataPolicy(sourceId)
+                requestParams = DataPolicyUseCase.createParams(sourceId)
             }
             Then("it should return source id correctly") {
                 assert(requestParams["source"] == sourceId)
@@ -42,52 +40,26 @@ class DataPolicyUseCaseTest: Spek({
         }
 
         Scenario("request data policy with correct source id") {
-            var returnValue: DataUploaderPolicy?= null
-
             Given("request param") {
-                useCase.requestParams = DataPolicyUseCase.createParamDataPolicy("id")
-
+                useCase.requestParams = DataPolicyUseCase.createParams("id")
+            }
+            Given("graphql repository") {
                 coEvery {
-                    repository.getReseponse(listOf()).getData<DataUploaderPolicy>(DataUploaderPolicy::class.java)
+                    repository.getReseponse(listOf())
                 } answers {
                     GraphqlResponse(
                             mapOf(DataUploaderPolicy::class.java to DataUploaderPolicy()),
                             null,
                             false
-                    ).getData<DataUploaderPolicy>(DataUploaderPolicy::class.java)
-                }
-
-                coEvery {
-                    repository.getReseponse(listOf()).getError(
-                            DataUploaderPolicy::class.java
                     )
-                } answers {
-                    listOf()
                 }
-            }
-            When("get policy uploader") {
-                runBlocking { useCase.executeOnBackground() }
             }
             Then("it should return policy of uploader") {
-                useCase.execute({}, {})
+                runBlocking {
+                    val result = useCase.executeOnBackground()
+                    assert(dataUploaderPolicy == result)
+                }
             }
         }
-
-//        Scenario("request data policy with param empty") {
-//            When("get policy uploader") {
-//                graphqlUseCase.stubExecuteOnBackground().returns(dataUploaderPolicy)
-//            }
-//            Then("it should return throw exception of missing param") {
-//                useCase.get({}, {
-//                    assertEquals(it, Throwable(""))
-//                })
-//            }
-//        }
     }
-
 })
-
-fun UseCase<*>.stubExecuteOnBackground(): MockKStubScope<Any, Any> {
-    val request = this
-    return coEvery { request.executeOnBackground() }
-}
