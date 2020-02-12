51c76595e6ef2f97e4419febc290f9fc4d1565de
isfaaghyth <isfaaghyth@gmail.com>
2020-02-12 13:03:50 +0700

    media: (removed) plain uploader object wrapper

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/entity/Uploader.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.mediauploader.data.entity
-
-data class Uploader(
-        val isSuccess: Boolean = false,
-        val message: String = "",
-        val uploadId: String = ""
-)
