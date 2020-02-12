edd3bbb45ad5d64a069e13bdf038538eb1bbecfa
kenny <kenny.hadisaputra@tokopedia.com>
2020-02-12 12:03:42 +0700

    - rethrow CancellationException on CoroutineScope.launchCatchError

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/coroutines/CoroutineScopeExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/coroutines/CoroutineScopeExt.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.kotlin.extensions.coroutines
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
@@ -11,10 +12,13 @@ fun CoroutineScope.launchCatchError(context: CoroutineContext = coroutineContext
         try{
             block()
         } catch (t: Throwable){
-            try {
-                onError(t)
-            } catch (e: Throwable){
+            if (t is CancellationException) throw t
+            else {
+                try {
+                    onError(t)
+                } catch (e: Throwable){
 
+                }
             }
         }
     }
