aea2614822d5a3877d1285f0d6797ec4735e064a
isfaaghyth <isfaaghyth@gmail.com>
2020-02-14 20:13:51 +0700

    media-test: (added) network service with mock response

--- libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/MediaUploaderUseCaseTest.kt
+++ libraries/mediauploader/src/test/java/com/tokopedia/mediauploader/domain/MediaUploaderUseCaseTest.kt
@@ -1,27 +1,37 @@
 package com.tokopedia.mediauploader.domain
 
+import com.google.gson.Gson
+import com.tokopedia.mediauploader.MockUploaderResponse
 import com.tokopedia.mediauploader.data.UploaderServices
 import com.tokopedia.mediauploader.data.entity.MediaUploader
-import com.tokopedia.mediauploader.data.entity.UploadData
 import com.tokopedia.usecase.RequestParams
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
+import okhttp3.MultipartBody
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.HttpURLConnection
+import java.util.concurrent.TimeUnit
+import kotlin.test.assert
 import kotlin.test.assertFailsWith
 
 class MediaUploaderUseCaseTest: Spek({
+
+    val mockWebServer = MockWebServer()
+
+    val uploadUrl = "/"
+    val filePath = "image.jpg"
+    var expectedValue = MediaUploader()
+
     Feature("data policy use case") {
+
         val services = mockk<UploaderServices>()
         val useCase = MediaUploaderUseCase(services)
-
-        val uploadUrl = "http://google.com/"
-        val filePath = "/sdcard/DCIM/Camera/test.jpg"
-        val uploadId = "abc-456-xyz-123"
-        val expectedValue = MediaUploader(
-                data = UploadData(uploadId)
-        )
 
         Scenario("create param with upload url and file path") {
             var requestParams = RequestParams()
@@ -45,12 +55,13 @@ class MediaUploaderUseCaseTest: Spek({
             }
         }
 
-        Scenario("request upload image") {
+        Scenario("request upload image from usecase") {
             var requestParams = RequestParams()
 
             Given("set param with upload url and file path") {
                 requestParams = useCase.createParams(uploadUrl, filePath)
             }
+
             Given("upload network services") {
                 coEvery {
                     services.uploadFile(uploadUrl, any())
@@ -58,12 +69,70 @@ class MediaUploaderUseCaseTest: Spek({
                     expectedValue
                 }
             }
+
             Then("it should received upload_id") {
                 runBlocking {
                     val data = useCase(requestParams)
-                    assert(data.data.uploadId == uploadId)
+                    assert(data.data?.uploadId == expectedValue.data?.uploadId)
                 }
             }
+        }
+    }
+
+    Feature("data policy with mock response") {
+
+        mockWebServer.start()
+
+        //retrofit builder
+        val services = Retrofit.Builder()
+                .baseUrl(mockWebServer.url(uploadUrl))
+                .addConverterFactory(GsonConverterFactory.create(Gson()))
+                .build()
+                .create(UploaderServices::class.java)
+
+        Scenario("request upload image with network service") {
+            Given("upload id data response") {
+                val response = MockResponse()
+                        .setResponseCode(HttpURLConnection.HTTP_OK)
+                        .setBody(MockUploaderResponse.success)
+                mockWebServer.enqueue(response)
+            }
+
+            Given("network services") {
+                runBlocking {
+                    val multipart = MultipartBody.Part.createFormData("test", "test")
+                    expectedValue = services.uploadFile(uploadUrl, multipart)
+                }
+            }
+
+            Then("it should received upload_id") {
+                assert(expectedValue.data?.uploadId?.isNotEmpty()?: false)
+            }
+        }
+
+        Scenario("request upload id with invalid resource dimen") {
+            Given("upload id data response") {
+                val response = MockResponse()
+                        .setResponseCode(HttpURLConnection.HTTP_OK)
+                        .setBody(MockUploaderResponse.error)
+                mockWebServer.enqueue(response)
+            }
+
+            Given("network services") {
+                runBlocking {
+                    val multipart = MultipartBody.Part.createFormData("test", "test")
+                    expectedValue = services.uploadFile(uploadUrl, multipart)
+                }
+            }
+
+            Then("it should received null data") {
+                mockWebServer.takeRequest(1, TimeUnit.SECONDS)
+                assert(expectedValue.data == null)
+            }
+        }
+
+        afterGroup {
+            mockWebServer.shutdown()
         }
     }
 })
