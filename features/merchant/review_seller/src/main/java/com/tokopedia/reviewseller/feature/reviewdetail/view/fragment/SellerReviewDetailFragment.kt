4d39c440e93ebe5dc9262ac64576b50c790409c8
milhamj <milham.jamaludin@gmail.com>
2020-02-14 14:39:37 +0700

    Init review_seller :tada:

---
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewdetail/view/fragment/SellerReviewDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.reviewseller.feature.reviewdetail.view.fragment
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+
+/**
+ * @author by milhamj on 2020-02-14.
+ */
+class SellerReviewDetailFragment : BaseDaggerFragment() {
+    override fun getScreenName(): String = ""
+
+    override fun initInjector() {
+    }
+}
