57cc7ebf237b944415e71d546960213d23d42178
isfaaghyth <isfaaghyth@gmail.com>
2020-02-14 10:20:37 +0700

    media: (modified) change base of use case and makes simplified

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/domain/DataPolicyUseCase.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/domain/DataPolicyUseCase.kt
@@ -1,23 +1,20 @@
 package com.tokopedia.mediauploader.domain
 
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.mediauploader.data.consts.MediaUploaderQuery
+import com.tokopedia.mediauploader.BaseUseCase
+import com.tokopedia.mediauploader.MediaRepository
+import com.tokopedia.mediauploader.data.consts.GraphQueryBuilder
 import com.tokopedia.mediauploader.data.entity.DataUploaderPolicy
-import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 
 open class DataPolicyUseCase @Inject constructor(
-        private val repository: GraphqlRepository
-) : UseCase<DataUploaderPolicy>() {
+        private val repository: MediaRepository
+) : BaseUseCase<Map<String, Any>, DataUploaderPolicy>() {
 
-    private val query = MediaUploaderQuery.dataPolicyQuery
-    var requestParams = mapOf<String, Any>()
-
-    override suspend fun executeOnBackground(): DataUploaderPolicy {
-        if (requestParams.isEmpty()) throw Exception("Not param found")
-        val request = GraphqlRequest(query, DataUploaderPolicy::class.java, requestParams)
-        val response = repository.getReseponse(listOf(request))
+    override suspend fun execute(params: Map<String, Any>): DataUploaderPolicy {
+        if (params.isEmpty()) throw Exception("Not param found")
+        val request = GraphqlRequest(QUERY, DataUploaderPolicy::class.java, params)
+        val response = repository.response(listOf(request))
         val error = response.getError(DataUploaderPolicy::class.java)
         if (error == null || error.isEmpty()) {
             return response.getData(DataUploaderPolicy::class.java)
@@ -26,14 +23,24 @@ open class DataPolicyUseCase @Inject constructor(
         }
     }
 
-    companion object {
-        private const val PARAM_SOURCE_ID = "source"
+    fun createParams(sourceId: String): Map<String, Any> {
+        val requestParams = hashMapOf<String, Any>()
+        requestParams[PARAM_SOURCE_ID] = sourceId
+        return requestParams
+    }
 
-        fun createParams(sourceId: String): Map<String, Any> {
-            val requestParams = hashMapOf<String, Any>()
-            requestParams[PARAM_SOURCE_ID] = sourceId
-            return requestParams
-        }
+    companion object {
+        /**
+         * GraphQueryBuilder: query builder,
+         * the query of media policy
+         */
+        private val QUERY = GraphQueryBuilder.mediaPolicy
+
+        /**
+         * keys of params
+         * @param source_id
+         */
+        private const val PARAM_SOURCE_ID = "source"
     }
 
 }
