ade2760b84eed46b2f4c49db92c6ef06e2d4c656
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-02-14 09:11:50 +0700

    Use spek on chatSearchViewModelTest

--- features/user/topchat/src/test/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatSearchViewModelTest.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatSearchViewModelTest.kt
@@ -1,3 +1,10 @@
 package com.tokopedia.topchat.chatsearch.viewmodel
 
-object ChatSearchViewModelTest
+import com.tokopedia.topchat.InstantTaskExecutorRuleSpek
+import org.spekframework.spek2.Spek
+
+object ChatSearchViewModelTest : Spek({
+
+    InstantTaskExecutorRuleSpek(this)
+
+})
