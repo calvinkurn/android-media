76cb3d6a048c7b2f5ac5e89312920360bd5eec8c
Muhammad Furqan <nanjihusaha@gmail.com>
2020-02-15 20:50:43 +0700

    create unit test for TravelDestinationViewModel

---
+++ features/travel/travel_homepage/src/test/java/com/tokopedia/travelhomepage/destination/presentation/viewmodel/TravelDestinationViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.travelhomepage.destination.presentation.viewmodel
+
+import org.spekframework.spek2.Spek
+
+/**
+ * @author by furqan on 15/02/2020
+ */
+class TravelDestinationViewModelTest : Spek({
+
+})
