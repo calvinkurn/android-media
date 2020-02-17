610f73a9107507b127abc332bef33af1a05a3a83
Muhammad Furqan <furqan@tokopedia.com>
2020-02-17 17:26:24 +0700

    temp commit

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
@@ -1,25 +1,34 @@
 package com.tokopedia.flight.filter.presentation.bottomsheets
 
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.flight.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
+import javax.inject.Inject
 
 /**
  * @author by furqan on 17/02/2020
  */
 class FlightFilterAirlineBottomSheet : BottomSheetUnify() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         showCloseIcon = false
-        isFullpage = false
         showKnob = true
         isDragable = true
+        isHideable = true
         setTitle(getString(R.string.airline))
         setAction(getString(R.string.reset)) {
-
+            // TODO: Add Function for reset button
         }
+
+        val view = View.inflate(requireContext(), R.layout.fragment_flight_filter_general, null)
+        setChild(view)
 
     }
 
---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/viewmodel/FlightFilterAirlineViewModel.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.flight.filter.presentation.viewmodel
+
+/**
+ * @author by furqan on 17/02/2020
+ */
+class FlightFilterAirlineViewModel {
+}
