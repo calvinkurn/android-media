10efab5ecef8ed9b7b85ae8614f15b821626dc5b
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-02-17 15:59:43 +0700

    Remove unused classes 2

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/data/source/CloudMessageDataSource.java
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/data/source/CloudMessageDataSource.java
@@ -3,10 +3,7 @@ package com.tokopedia.topchat.chatlist.data.source;
 import com.google.gson.JsonObject;
 import com.tokopedia.topchat.chatlist.data.mapper.DeleteMessageMapper;
 import com.tokopedia.topchat.chatlist.viewmodel.DeleteChatListViewModel;
-import com.tokopedia.topchat.chatlist.viewmodel.InboxChatViewModel;
 import com.tokopedia.topchat.common.chat.api.ChatApi;
-
-import java.util.HashMap;
 
 import rx.Observable;
 
