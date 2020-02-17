0e020bbb7306ff65dc84299413a2b5616ce023dd
hansenpw <hansen.wijaya@tokopedia.com>
2020-02-17 17:12:14 +0700

    add packaging

---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/list/view/PreferenceListActivity.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.list.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokopedia.purchase_platform.R
+
+class PreferenceListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference_list)
+    }
+}
