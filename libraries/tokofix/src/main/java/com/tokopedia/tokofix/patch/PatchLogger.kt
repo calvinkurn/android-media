d11e0f2aa48ac11cc969d55ea4055caf7fb5fd84
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2020-02-17 16:45:40 +0700

    change log to timber

--- libraries/tokofix/src/main/java/com/tokopedia/tokofix/patch/PatchLogger.kt
+++ libraries/tokofix/src/main/java/com/tokopedia/tokofix/patch/PatchLogger.kt
@@ -7,33 +7,34 @@ import android.util.Log
 import android.widget.Toast
 import com.meituan.robust.Patch
 import com.meituan.robust.RobustCallBack
+import timber.log.Timber
 
 /**
  * Author errysuprayogi on 01,February,2020
  */
 class PatchLogger(val context: Context) : RobustCallBack {
     override fun onPatchListFetched(result: Boolean, isNet: Boolean, patches: List<Patch>) {
-        Log.d(TAG, "onPatchListFetched result: $result")
-        Log.d(TAG, "onPatchListFetched isNet: $isNet")
+        Timber.d(TAG, "onPatchListFetched result: $result")
+        Timber.d(TAG, "onPatchListFetched isNet: $isNet")
         for (patch in patches) {
-            Log.d(TAG, "onPatchListFetched patch: " + patch.name)
+            Timber.d(TAG, "onPatchListFetched patch: " + patch.name)
         }
     }
 
     override fun onPatchFetched(result: Boolean, isNet: Boolean, patch: Patch) {
-        Log.d(TAG, "onPatchFetched result: $result")
-        Log.d(TAG, "onPatchFetched isNet: $isNet")
-        Log.d(TAG, "onPatchFetched patch: " + patch.name)
+        Timber.d(TAG, "onPatchFetched result: $result")
+        Timber.d(TAG, "onPatchFetched isNet: $isNet")
+        Timber.d(TAG, "onPatchFetched patch: " + patch.name)
     }
 
     override fun onPatchApplied(result: Boolean, patch: Patch) {
-        Log.d(TAG, "onPatchApplied result: $result")
-        Log.d(TAG, "onPatchApplied patch: " + patch.name)
+        Timber.d(TAG, "onPatchApplied result: $result")
+        Timber.d(TAG, "onPatchApplied patch: " + patch.name)
     }
 
     override fun logNotify(log: String, where: String) {
-        Log.d(TAG, "logNotify log: $log")
-        Log.d(TAG, "logNotify where: $where")
+        Timber.d(TAG, "logNotify log: $log")
+        Timber.d(TAG, "logNotify where: $where")
     }
 
     override fun exceptionNotify(throwable: Throwable, where: String) {
