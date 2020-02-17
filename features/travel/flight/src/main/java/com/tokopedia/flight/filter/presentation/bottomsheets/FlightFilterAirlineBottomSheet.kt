196944c96194f2302e1f777754d1801fe678a2d1
Muhammad Furqan <furqan@tokopedia.com>
2020-02-17 15:46:38 +0700

    first setup for flight airline filter

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
@@ -1,11 +1,21 @@
 package com.tokopedia.flight.filter.presentation.bottomsheets
 
+import android.os.Bundle
+import com.tokopedia.flight.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
 
 /**
  * @author by furqan on 17/02/2020
  */
 class FlightFilterAirlineBottomSheet : BottomSheetUnify() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        clearClose(true)
+        setFullPage(false)
+        setTitle(getString(R.string.airline))
+    }
 
     companion object {
         const val TAG_FILTER_AIRLINE = "TagFilterAirlineBottomSheet"
