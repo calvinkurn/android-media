c83ddb5f46f625ba8d0bfe4443a0601b13761d75
hansenpw <hansen.wijaya@tokopedia.com>
2020-02-17 17:32:32 +0700

    add edit preference package

---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/PreferenceEditActivity.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokopedia.purchase_platform.R
+
+class PreferenceEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference_edit)
+    }
+}
---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/fragment/TempFragment.kt
@@ -0,0 +1,2 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view.fragment
+
