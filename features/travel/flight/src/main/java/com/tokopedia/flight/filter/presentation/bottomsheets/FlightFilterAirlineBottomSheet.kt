8f25f6581ec9837543b36af41c8becaa5a9e330d
Muhammad Furqan <furqan@tokopedia.com>
2020-02-18 14:59:15 +0700

    implement view holder in flight filter airline

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/bottomsheets/FlightFilterAirlineBottomSheet.kt
@@ -3,21 +3,63 @@ package com.tokopedia.flight.filter.presentation.bottomsheets
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListCheckableAdapter
+import com.tokopedia.abstraction.base.view.adapter.factory.BaseListCheckableTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.holder.BaseCheckableViewHolder
 import com.tokopedia.flight.R
+import com.tokopedia.flight.filter.presentation.adapter.FlightFilterAirlineAdapterTypeFactory
+import com.tokopedia.flight.search.presentation.fragment.OnFlightFilterListener
+import com.tokopedia.flight.search.presentation.model.resultstatistics.AirlineStat
 import com.tokopedia.unifycomponents.BottomSheetUnify
+import kotlinx.android.synthetic.main.fragment_flight_filter_general.*
+import java.util.*
 import javax.inject.Inject
 
 /**
  * @author by furqan on 17/02/2020
  */
-class FlightFilterAirlineBottomSheet : BottomSheetUnify() {
+class FlightFilterAirlineBottomSheet : BottomSheetUnify(),
+        BaseCheckableViewHolder.CheckableInteractionListener,
+        BaseListCheckableAdapter.OnCheckableAdapterListener<AirlineStat> {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    lateinit var adapter: BaseListCheckableAdapter<AirlineStat, BaseListCheckableTypeFactory<AirlineStat>>
+    lateinit var listener: OnFlightFilterListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBottomSheet()
+        initAdapter()
+        initRecyclerView()
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        if (::listener.isInitialized && listener.flightSearchStatisticModel != null) {
+            renderList(listener.flightSearchStatisticModel.airlineStatList)
+        }
+    }
+
+    override fun isChecked(position: Int): Boolean = adapter.isChecked(position)
+
+    override fun updateListByCheck(isChecked: Boolean, position: Int) {
+        adapter.updateListByCheck(isChecked, position)
+    }
+
+    override fun onItemChecked(airlineStat: AirlineStat, isChecked: Boolean) {
+//        val flightFilterModel = listener.flightFilterModel
+//        val airlineStatList = adapter.checkedDataList
+//        val airlineList = Observable.from(airlineStatList).map { airlineStat -> airlineStat.airlineDB.id }.toList().toBlocking().first()
+//        flightFilterModel.airlineList = airlineList
+//        listener.onFilterModelChanged(flightFilterModel)
+    }
+
+    private fun initBottomSheet() {
         showCloseIcon = false
         showKnob = true
         isDragable = true
@@ -29,7 +71,46 @@ class FlightFilterAirlineBottomSheet : BottomSheetUnify() {
 
         val view = View.inflate(requireContext(), R.layout.fragment_flight_filter_general, null)
         setChild(view)
+    }
 
+    private fun initAdapter() {
+        val typeFactory = FlightFilterAirlineAdapterTypeFactory(this)
+        adapter = BaseListCheckableAdapter<AirlineStat, BaseListCheckableTypeFactory<AirlineStat>>(typeFactory, this)
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        recycler_view.adapter = adapter
+    }
+
+    private fun renderList(list: List<AirlineStat>) {
+        val flightFilterModel = listener.flightFilterModel
+        val checkedPositionList = HashSet<Int>()
+        if (flightFilterModel != null) {
+            val airlineList = flightFilterModel.airlineList
+            if (airlineList != null) {
+                var i = 0
+                val sizei = airlineList.size
+                while (i < sizei) {
+                    val selectedAirline = airlineList[i]
+                    var j = 0
+                    val sizej = list.size
+                    while (j < sizej) {
+                        val airlineStat = list[j]
+                        if (airlineStat.airlineDB.id == selectedAirline) {
+                            checkedPositionList.add(j)
+                            break
+                        }
+                        j++
+                    }
+                    i++
+                }
+            }
+        }
+
+        adapter.setCheckedPositionList(checkedPositionList)
+        adapter.notifyDataSetChanged()
     }
 
     companion object {
@@ -37,4 +118,5 @@ class FlightFilterAirlineBottomSheet : BottomSheetUnify() {
 
         fun getInstance(): FlightFilterAirlineBottomSheet = FlightFilterAirlineBottomSheet()
     }
+
 }
