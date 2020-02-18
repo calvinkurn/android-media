3a9e234eeddd29ad5c19ebaa7940588a38284380
Felicia Widjaja <fwidjaja@live.com>
2020-02-18 16:29:53 +0700

    add unit test on confirm shipping

---
+++ features/transaction/sellerorder/src/test/java/com/tokopedia/sellerorder/confirmshipping/SomConfirmShippingViewModelTest.kt
@@ -0,0 +1,104 @@
+package com.tokopedia.sellerorder.confirmshipping
+
+import com.google.gson.Gson
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlResponse
+import com.tokopedia.sellerorder.InstantTaskExecutorRuleSpek
+import com.tokopedia.sellerorder.confirmshipping.data.model.SomConfirmShipping
+import com.tokopedia.sellerorder.confirmshipping.data.model.SomCourierList
+import com.tokopedia.sellerorder.confirmshipping.presentation.viewmodel.SomConfirmShippingViewModel
+import com.tokopedia.sellerorder.detail.data.model.*
+import com.tokopedia.sellerorder.detail.presentation.viewmodel.SomDetailViewModel
+import com.tokopedia.user.session.UserSession
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+/**
+ * Created by fwidjaja on 2020-02-18.
+ */
+object SomConfirmShippingViewModelTest: Spek({
+    InstantTaskExecutorRuleSpek(this)
+
+    val dispatcher = Dispatchers.Unconfined
+    val graphqlRepository: GraphqlRepository = mockk(relaxed = true)
+    val viewModel = SomConfirmShippingViewModel(dispatcher, graphqlRepository)
+    val spy = spyk(viewModel)
+    val queryChangeCourier = "query_change_courier"
+    val queryCourierList = "query_courier_list"
+    val queryConfirmShipping = "query_confirm_shipping"
+    val changeCourierSuccessJson = "response_change_courier_success"
+    val courierListSuccessJson = "response_courier_list_success"
+    val confirmShippingSuccessJson = "response_confirm_shipping_success"
+    val gson = Gson()
+
+    Feature("SOM Confirm Shipping") {
+        Scenario("Confirm Shipping") {
+            Given("Return Success Data") {
+                val jsonResponse = this.javaClass.classLoader?.getResourceAsStream(confirmShippingSuccessJson)?.readBytes()?.toString(Charsets.UTF_8)
+                val response = gson.fromJson(jsonResponse, SomConfirmShipping.Data::class.java)
+                val gqlResponseSuccess = GraphqlResponse(
+                        mapOf(SomConfirmShipping.Data::class.java to response),
+                        mapOf(SomConfirmShipping.Data::class.java to listOf()), false)
+                coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
+            }
+
+            When("Load file GQL-raw mutation mpLogisticConfirmShipping") {
+                runBlocking {
+                    spy.doConfirmShipping(queryConfirmShipping)
+                }
+            }
+
+            Then("Verify Func doConfirmShipping works as expected!") {
+                coVerify { spy.doConfirmShipping(queryConfirmShipping) }
+            }
+        }
+
+        Scenario("Get Courier List") {
+            Given("Return Success Data") {
+                val jsonResponse = this.javaClass.classLoader?.getResourceAsStream(courierListSuccessJson)?.readBytes()?.toString(Charsets.UTF_8)
+                val response = gson.fromJson(jsonResponse, SomCourierList.Data::class.java)
+                val gqlResponseSuccess = GraphqlResponse(
+                        mapOf(SomCourierList.Data::class.java to response),
+                        mapOf(SomCourierList.Data::class.java to listOf()), false)
+                coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
+            }
+
+            When("Load file GQL-raw mutation mpLogisticGetEditShippingForm") {
+                runBlocking {
+                    spy.getCourierList(queryCourierList)
+                }
+            }
+
+            Then("Verify Func getCourierList works as expected!") {
+                coVerify { spy.getCourierList(queryCourierList) }
+            }
+        }
+
+        Scenario("Change Courier") {
+            Given("Return Success Data") {
+                val jsonResponse = this.javaClass.classLoader?.getResourceAsStream(changeCourierSuccessJson)?.readBytes()?.toString(Charsets.UTF_8)
+                val response = gson.fromJson(jsonResponse, SomConfirmShipping.Data::class.java)
+                val gqlResponseSuccess = GraphqlResponse(
+                        mapOf(SomConfirmShipping.Data::class.java to response),
+                        mapOf(SomConfirmShipping.Data::class.java to listOf()), false)
+                coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
+            }
+
+            When("Load file GQL-raw mutation mpLogisticChangeCourier") {
+                runBlocking {
+                    spy.doChangeCourier(queryChangeCourier)
+                }
+            }
+
+            Then("Verify Func doChangeCourier works as expected!") {
+                coVerify { spy.doChangeCourier(queryChangeCourier) }
+            }
+        }
+    }
+})
--- features/transaction/sellerorder/src/test/java/com/tokopedia/sellerorder/detail/SomDetailViewModelTest.kt
+++ features/transaction/sellerorder/src/test/java/com/tokopedia/sellerorder/detail/SomDetailViewModelTest.kt
@@ -30,17 +30,27 @@ object SomDetailViewModelTest: Spek({
     val queryOrderDetailPageList = "query_filter_page_list"
     val queryAcceptOrder = "query_accept_order"
     val queryRejectOrder = "query_reject_order"
+    val queryEditAwb = "query_edit_awb"
     val orderDetailPageSuccessJson = "response_order_detail_success"
     val acceptOrderSuccessJson = "response_accept_order_success"
     val rejectReasonsSuccessJson = "response_reject_reasons_success"
     val rejectOrderSuccessJson = "response_reject_order_success"
+    val editAwbSuccessJson = "response_edit_awb_success"
     val shopId = "1479278"
     val orderId = "397293266"
     val orderIdNewOrder = "437997831"
     val gson = Gson()
     val paramRejectReasons = SomReasonRejectParam()
     val paramRejectOrder = SomRejectRequest(
-
+        orderId = orderIdNewOrder,
+        reason = "lalala",
+        rCode = "2",
+        listPrd = "14263814~14263815",
+        closeEnd = "17/05/2017",
+        userId = "12345",
+        mobile = "1",
+        lang = "id",
+        ignorePenalty = "false"
     )
 
     Feature("SOM Detail Order Page") {
@@ -119,12 +129,33 @@ object SomDetailViewModelTest: Spek({
 
             When("Load file GQL-raw mutation RejectOrder") {
                 runBlocking {
-                    spy.doGetRejectReasons(queryRejectOrder, paramRejectReasons)
+                    spy.doRejectOrder(queryRejectOrder, paramRejectOrder)
                 }
             }
 
-            Then("Verify Func doGetRejectReasons works as expected!") {
-                coVerify { spy.doGetRejectReasons(queryAcceptOrder, paramRejectReasons) }
+            Then("Verify Func doRejectOrder works as expected!") {
+                coVerify { spy.doRejectOrder(queryRejectOrder, paramRejectOrder) }
+            }
+        }
+
+        Scenario("Edit AWB") {
+            Given("Return Success Data") {
+                val jsonResponse = this.javaClass.classLoader?.getResourceAsStream(editAwbSuccessJson)?.readBytes()?.toString(Charsets.UTF_8)
+                val response = gson.fromJson(jsonResponse, SomEditAwbResponse.Data::class.java)
+                val gqlResponseSuccess = GraphqlResponse(
+                        mapOf(SomEditAwbResponse.Data::class.java to response),
+                        mapOf(SomEditAwbResponse.Data::class.java to listOf()), false)
+                coEvery { graphqlRepository.getReseponse(any(), any()) } returns gqlResponseSuccess
+            }
+
+            When("Load file GQL-raw mutation Edit Awb") {
+                runBlocking {
+                    spy.doEditAwb(queryEditAwb)
+                }
+            }
+
+            Then("Verify Func doEditAwb works as expected!") {
+                coVerify { spy.doEditAwb(queryEditAwb) }
             }
         }
     }
