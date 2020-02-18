7a50955dc63387deef6ec18c1778331be0283d05
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-02-18 20:15:12 +0700

    Add unit test get district

--- features/logistic/logisticaddaddress/src/test/java/com/tokopedia/logisticaddaddress/features/addnewaddress/pinpoint/PinpointMapPresenterTest.kt
+++ features/logistic/logisticaddaddress/src/test/java/com/tokopedia/logisticaddaddress/features/addnewaddress/pinpoint/PinpointMapPresenterTest.kt
@@ -1,14 +1,19 @@
 package com.tokopedia.logisticaddaddress.features.addnewaddress.pinpoint
 
+import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.logisticaddaddress.domain.mapper.DistrictBoundaryMapper
 import com.tokopedia.logisticaddaddress.domain.mapper.GetDistrictMapper
 import com.tokopedia.logisticaddaddress.domain.usecase.DistrictBoundaryUseCase
 import com.tokopedia.logisticaddaddress.domain.usecase.GetDistrictUseCase
+import com.tokopedia.logisticaddaddress.features.addnewaddress.uimodel.get_district.GetDistrictDataUiModel
+import com.tokopedia.logisticaddaddress.features.addnewaddress.uimodel.get_district.GetDistrictResponseUiModel
 import com.tokopedia.logisticdata.domain.usecase.RevGeocodeUseCase
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
+import rx.Subscriber
 
 object PinpointMapPresenterTest : Spek({
 
@@ -36,6 +41,27 @@ object PinpointMapPresenterTest : Spek({
                     getDistrictUseCase.unsubscribe()
                     revGeoCodeUseCase.unsubscribe()
                     districtBoundUseCase.unsubscribe()
+                }
+            }
+        }
+    }
+
+    Feature("get district") {
+        Scenario("get succcess district") {
+            val gqlSuccess = GraphqlResponse(mapOf(), mapOf(), false)
+            val uiModel = GetDistrictDataUiModel(title = "city")
+            val successModel = GetDistrictResponseUiModel(
+                    data = uiModel
+            )
+            Given("usecase gives success") {
+                every { getDistrictUseCase.execute(any(), any()) } answers {
+                    secondArg<Subscriber<GraphqlResponse>>().onNext(gqlSuccess)
+                }
+                every { getDistrictMapper.map(gqlSuccess) } returns successModel
+            }
+            Then("on success is called") {
+                verify {
+                    view.onSuccessPlaceGetDistrict(uiModel)
                 }
             }
         }
