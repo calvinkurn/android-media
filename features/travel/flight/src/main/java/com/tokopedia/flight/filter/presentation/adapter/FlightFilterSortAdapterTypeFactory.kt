ae2dd9e476637b4d55a43c2902ac42009563b6b8
Jessica Sean <jessica.sean@tokopedia.com>
2020-02-20 16:11:27 +0700

    add widget

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/adapter/FlightFilterSortAdapterTypeFactory.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/adapter/FlightFilterSortAdapterTypeFactory.kt
@@ -1,9 +1,11 @@
-package com.tokopedia.flight.filter.presentation.widget
+package com.tokopedia.flight.filter.presentation.adapter
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.flight.filter.presentation.adapter.viewholder.FlightFilterSortViewHolder
+import com.tokopedia.flight.filter.presentation.widget.TestViewHolder
 
 /**
  * @author by jessica on 2020-02-20
