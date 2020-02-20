ee2fc6d6a79a24c3362b5d1f8a9e04a062fadfba
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2020-02-20 12:26:59 +0530

    dependency added in basemvvm

--- features/category/age_restriction/src/main/java/com/tokopedia/age_restriction/viewmodel/BaseARViewModel.kt
+++ features/category/age_restriction/src/main/java/com/tokopedia/age_restriction/viewmodel/BaseARViewModel.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import com.tokopedia.basemvvm.viewmodel.BaseViewModel
 
 open class BaseARViewModel() : BaseViewModel() {
-    private var progBarVisibility = MutableLiveData<Boolean>()
-    private var warningMessage = MutableLiveData<String>()
-    private var errorMessage = MutableLiveData<String>()
+    protected var progBarVisibility = MutableLiveData<Boolean>()
+    protected var warningMessage = MutableLiveData<String>()
+    protected var errorMessage = MutableLiveData<String>()
 
 
     fun getProgBarVisibility(): LiveData<Boolean> {
