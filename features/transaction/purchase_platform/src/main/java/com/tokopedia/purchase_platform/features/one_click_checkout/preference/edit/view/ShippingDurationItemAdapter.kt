47f50bfa1e0675ba3d1f5d5d4fd11182b69cdc7e
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-02-20 13:37:34 +0700

    Shipping Duration UI

---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/ShippingDurationItemAdapter.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.Preference
+
+class ShippingDurationItemAdapter : androidx.recyclerview.widget.ListAdapter<Preference, ShippingDurationViewHolder>(DIFF_CALLBACK){
+
+    companion object {
+        val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Preference>(){
+            override fun areItemsTheSame(oldItem: Preference, newItem: Preference): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Preference, newItem: Preference): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShippingDurationViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ShippingDurationViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/ShippingDurationViewHolder.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.purchase_platform.R
+
+class ShippingDurationViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+    companion object{
+        val LayoutInflater = R.layout.item_shipping_duration
+    }
+}
