8799878bc198b0562d4fa6057baba2666c71976f
Jessica Sean <jessica.sean@tokopedia.com>
2020-02-20 14:54:16 +0700

    visitable

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/widget/FlightFilterSortAdapterTypeFactory.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/widget/FlightFilterSortAdapterTypeFactory.kt
@@ -3,7 +3,6 @@ package com.tokopedia.flight.filter.presentation.widget
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
-import com.tokopedia.abstraction.base.view.adapter.factory.BaseListCheckableTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 
 /**
@@ -11,8 +10,9 @@ import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolde
  */
 
 class FlightFilterSortAdapterTypeFactory :
-        BaseAdapterTypeFactory(), BaseListCheckableTypeFactory<Visitable<*>> {
-    override fun type(viewModel: Visitable<*>): Int = FlightFilterSortViewHolder.LAYOUT
+        BaseAdapterTypeFactory(){
+
+    fun type(viewModel: Visitable<*>): Int = FlightFilterSortViewHolder.LAYOUT
 
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<out Visitable<*>> {
         return if (type == FlightFilterSortViewHolder.LAYOUT) TestViewHolder(parent) else
