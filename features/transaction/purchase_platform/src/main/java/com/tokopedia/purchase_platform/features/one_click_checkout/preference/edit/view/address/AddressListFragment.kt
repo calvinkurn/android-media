1e17d7cfb2da14c74bdaee63cc5359deabf89ecd
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-02-20 18:03:30 +0700

    AddressList View Model

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListFragment.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListFragment.kt
@@ -4,26 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.purchase_platform.R
+import com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.di.AddressListComponent
 import kotlinx.android.synthetic.main.fragment_choose_address.*
+import javax.inject.Inject
 
 class AddressListFragment : BaseDaggerFragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel: AddressListViewModel by lazy {
+        ViewModelProviders.of(this, viewModelFactory)[AddressListViewModel::class.java]
+    }
 
     val adapter = AddressListItemAdapter()
 
     override fun getScreenName(): String = ""
 
     override fun initInjector() {
+        getComponent(AddressListComponent::class.java).inject(this)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_choose_address, container, false)
     }
 
+    private fun initViewModel(){
+        viewModel.addressList.observe(this, Observer {
+            adapter.submitList(it)
+        })
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViewModel()
 
         btn_save_address.setOnClickListener {
             if(empty_state_order_list.visibility == View.VISIBLE) {
@@ -37,7 +57,6 @@ class AddressListFragment : BaseDaggerFragment() {
 
         address_list_rv.adapter = adapter
         address_list_rv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-
 
     }
 
