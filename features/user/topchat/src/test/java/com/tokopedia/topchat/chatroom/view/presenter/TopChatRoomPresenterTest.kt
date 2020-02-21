86e3f36737c90ae194ce71c1606a850c1fefba7c
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-02-21 09:18:01 +0700

    Add topchat room presenter test file

---
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/view/presenter/TopChatRoomPresenterTest.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.topchat.chatroom.view.presenter
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+
+class TopChatRoomPresenterTest : Spek({
+
+    Feature("Get MessageId") {
+
+    }
+})
