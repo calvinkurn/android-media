efa037df8b703fdd67f5dd836d1f072c3a3fdd11
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-02-21 14:54:32 +0700

    Adding shippingDuration view model

---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/shipping/ShippingDurationViewModel.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view.shipping
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.Preference
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.GetPreferenceEditUseCase
+import javax.inject.Inject
+
+class ShippingDurationViewModel @Inject constructor(val useCase: GetPreferenceEditUseCase): ViewModel(){
+    private val _shippingDuration: MutableLiveData<List<Preference>> = MutableLiveData(listOf(Preference(), Preference(), Preference(), Preference(), Preference(), Preference()))
+    val shippingDuration: LiveData<List<Preference>>
+    get() = _shippingDuration
+}
