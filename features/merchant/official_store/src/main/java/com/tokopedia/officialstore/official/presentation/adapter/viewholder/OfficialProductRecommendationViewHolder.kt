c6c116420723e3cb273566205565f6a58d1cfa93
Darian <darian.thedy@gmail.com>
2020-02-22 18:15:03 +0700

    Implement product card in OS home recom

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialProductRecommendationViewHolder.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/official/presentation/adapter/viewholder/OfficialProductRecommendationViewHolder.kt
@@ -1,20 +1,14 @@
 package com.tokopedia.officialstore.official.presentation.adapter.viewholder
 
-import android.app.Activity
 import android.view.View
-import com.google.android.material.snackbar.Snackbar
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.abstraction.common.utils.network.ErrorHandler
-import com.tokopedia.applink.ApplinkConst
-import com.tokopedia.applink.RouteManager
 import com.tokopedia.kotlin.extensions.view.ViewHintListener
 import com.tokopedia.officialstore.R
 import com.tokopedia.officialstore.official.presentation.adapter.viewmodel.ProductRecommendationViewModel
+import com.tokopedia.productcard.ProductCardGridView
 import com.tokopedia.productcard.v2.ProductCardModel
-import com.tokopedia.productcard.v2.ProductCardView
 import com.tokopedia.recommendation_widget_common.listener.RecommendationListener
 import com.tokopedia.topads.sdk.utils.ImpresionTask
-import com.tokopedia.unifycomponents.Toaster
 
 
 class OfficialProductRecommendationViewHolder(
@@ -22,7 +16,7 @@ class OfficialProductRecommendationViewHolder(
         val recommendationListener: RecommendationListener
 ): AbstractViewHolder<ProductRecommendationViewModel>(view) {
 
-    private val productCardView: ProductCardView by lazy { view.findViewById<ProductCardView>(R.id.product_item) }
+    private val productCardView: ProductCardGridView by lazy { view.findViewById<ProductCardGridView>(R.id.product_item) }
 
     override fun bind(element: ProductRecommendationViewModel) {
         productCardView.run {
@@ -45,7 +39,14 @@ class OfficialProductRecommendationViewHolder(
                             freeOngkir = ProductCardModel.FreeOngkir(
                                     isActive = element.productItem.isFreeOngkirActive,
                                     imageUrl = element.productItem.freeOngkirImageUrl
-                            )
+                            ),
+                            labelGroupList = element.productItem.labelGroupList.map { recommendationLabel ->
+                                ProductCardModel.LabelGroup(
+                                        position = recommendationLabel.position,
+                                        title = recommendationLabel.title,
+                                        type = recommendationLabel.type
+                                )
+                            }
                     )
             )
 
@@ -62,37 +63,7 @@ class OfficialProductRecommendationViewHolder(
                 element.listener.onProductClick(element.productItem, element.productItem.type, adapterPosition)
                 if (element.productItem.isTopAds) ImpresionTask().execute(element.productItem.clickUrl)
             }
-
-            setButtonWishlistOnClickListener {
-                element.listener.onWishlistClick(element.productItem, !element.productItem.isWishlist) { success, throwable ->
-                    if (success) {
-                        element.productItem.isWishlist = !element.productItem.isWishlist
-                        setButtonWishlistImage(element.productItem.isWishlist)
-                        if (element.productItem.isWishlist) {
-                            showSuccessAddWishlist((context as Activity).findViewById(android.R.id.content), getString(R.string.msg_success_add_wishlist))
-                        } else {
-                            showSuccessRemoveWishlist((context as Activity).findViewById(android.R.id.content), getString(R.string.msg_success_remove_wishlist))
-                        }
-                    } else {
-                        showError(rootView, throwable)
-                    }
-                }
-            }
         }
-    }
-
-    private fun showSuccessAddWishlist(view: View, message: String) {
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-                .setAction("Lihat Wishlist") { RouteManager.route(view.context, ApplinkConst.WISHLIST) }
-                .show()
-    }
-
-    private fun showSuccessRemoveWishlist(view: View, message: String) {
-        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show()
-    }
-
-    private fun showError(view: View, throwable: Throwable?) {
-        Toaster.showError(view, ErrorHandler.getErrorMessage(view.context, throwable), Snackbar.LENGTH_LONG)
     }
 
     companion object {
