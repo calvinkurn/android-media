f23a0b14f2ff4bd648ded5495b51ee5abe989950
saidfaisal <said.faisal@tokopedia.com>
2020-02-23 12:44:09 +0700

    fix import

--- features/merchant/reputation/src/main/java/com/tokopedia/tkpd/tkpdreputation/createreputation/ui/fragment/CreateReviewFragment.kt
+++ features/merchant/reputation/src/main/java/com/tokopedia/tkpd/tkpdreputation/createreputation/ui/fragment/CreateReviewFragment.kt
@@ -11,7 +11,9 @@ import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.lifecycle.Observer
