bab6de190ccb6cea79ef006bda8673d4db18aeed
Dari <47311456+darianthedytoped@users.noreply.github.com>
2020-02-24 11:23:48 +0700

    Fix count drawable

--- features/discovery/search/src/main/java/com/tokopedia/search/utils/CountDrawable.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/utils/CountDrawable.kt
@@ -38,9 +38,18 @@ class CountDrawable(context: Context) : Drawable() {
 
     private fun createBadgePaint(context: Context) {
         badgePaint
-        badgePaint.color = ContextCompat.getColor(context.applicationContext, R.color.cart_count_color)
+        badgePaint.color = safeGetColor(context, R.color.cart_count_color)
         badgePaint.isAntiAlias = true
         badgePaint.style = Paint.Style.FILL
+    }
+
+    private fun safeGetColor(context: Context, @ColorRes colorId: Int): Int {
+        return try {
+            ContextCompat.getColor(context, colorId)
+        }
+        catch (throwable: Throwable) {
+            0
+        }
     }
 
     override fun draw(canvas: Canvas) {
