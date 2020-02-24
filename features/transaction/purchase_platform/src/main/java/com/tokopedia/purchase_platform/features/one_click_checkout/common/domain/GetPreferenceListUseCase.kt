1c892420204094e92952d04ec1bbce44360a44aa
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-02-24 17:53:28 +0700

    preferenceListUseCase WIP

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/GetPreferenceListUseCase.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/GetPreferenceListUseCase.kt
@@ -1,9 +1,27 @@
 package com.tokopedia.purchase_platform.features.one_click_checkout.common.domain
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
-import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.Preference
+import com.tokopedia.purchase_platform.R
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.model.response.preference.Response
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.mapper.PreferenceListModelMapper
+import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.PreferenceListResponseModel
+import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 
-class GetPreferenceListUseCase @Inject constructor(val graphql: GraphqlUseCase<List<Preference>>) {
+class GetPreferenceListUseCase @Inject constructor(@ApplicationContext val context: Context, val graphql: GraphqlUseCase<Response>, val preferenceListModelMapper: PreferenceListModelMapper) : UseCase<PreferenceListResponseModel>() {
+    override suspend fun executeOnBackground(): PreferenceListResponseModel {
+        val graphqlRequest = GraphqlHelper.loadRawString(context.resources,
+                R.raw.query_preference_list)
+
+        graphql.setGraphqlQuery(graphqlRequest)
+        graphql.setTypeClass(Response::class.java)
+
+        val result = graphql.executeOnBackground()
+
+       return preferenceListModelMapper.convertToDomainModel(result)
+    }
 
 }
