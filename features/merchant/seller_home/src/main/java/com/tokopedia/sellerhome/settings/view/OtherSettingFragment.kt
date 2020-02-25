a3b8b49038c58138831498e978ce5af8d7c8e623
Reivin <reivin.oktavianus@tokopedia.com>
2020-02-25 18:20:18 +0700

    Initial folder

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/OtherSettingFragment.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.sellerhome.settings.view
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.base.view.fragment.BaseListFragment
+
+class OtherSettingFragment: BaseListFragment<>() {
+
+}
---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/typefactory/OtherSettingTypeFactory.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.sellerhome.settings.view.typefactory
+
+import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+
+class OtherSettingTypeFactory : BaseAdapterTypeFactory() {
+
+
+
+}
