0c02c71b35662ec76cb1fa65e0ccd591180c681d
Rahul Lohra <rahul.lohra@tokopedia.com>
2020-02-25 11:10:07 +0530

    1. Minor code refactor

--- libraries/composite_lib/fakeresponse/src/main/java/com/rahullohra/fakeresponse/data/diProvider/fragments/DownloadFragmentProvider.kt
+++ libraries/composite_lib/fakeresponse/src/main/java/com/rahullohra/fakeresponse/data/diProvider/fragments/DownloadFragmentProvider.kt
@@ -12,19 +12,17 @@ import kotlinx.coroutines.Dispatchers
 class DownloadFragmentProvider :
     DiProvider<DownloadFragment> {
 
-    override fun inject(t: DownloadFragment) {
-        t.activity?.let { activity ->
+    override fun inject(fragment: DownloadFragment) {
+        fragment.activity?.let { activity ->
             val uiDispatcher = Dispatchers.Main
             val workerDispatcher = Dispatchers.IO
 
             val repository = RemoteSqliteRepository()
             val usecase = DownloadSqliteUseCase(repository)
             val list = arrayOf(uiDispatcher, workerDispatcher, usecase)
-            val vmFactory = ViewModelProvider(t,
-                VMFactory(activity.application, list)
-            )
+            val vmFactory = ViewModelProvider(fragment, VMFactory(activity.application, list))
             val vm = vmFactory[DownloadFragmentVM::class.java]
-            t.viewModel = vm
+            fragment.viewModel = vm
         }
     }
 }
