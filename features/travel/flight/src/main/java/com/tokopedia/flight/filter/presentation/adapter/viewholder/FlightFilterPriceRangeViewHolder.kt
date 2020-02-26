498257c07cbee5bca2c02f19c77eccb464b628f4
Muhammad Furqan <furqan@tokopedia.com>
2020-02-26 18:25:50 +0700

    expandable price filter

--- features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/adapter/viewholder/FlightFilterPriceRangeViewHolder.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/filter/presentation/adapter/viewholder/FlightFilterPriceRangeViewHolder.kt
@@ -1,11 +1,14 @@
 package com.tokopedia.flight.filter.presentation.adapter.viewholder
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.flight.R
 import com.tokopedia.flight.common.util.FlightCurrencyFormatUtil
 import com.tokopedia.flight.filter.presentation.FlightFilterSortListener
 import com.tokopedia.flight.filter.presentation.model.PriceRangeModel
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.unifycomponents.RangeSliderUnify
 import kotlinx.android.synthetic.main.item_flight_filter_price_range.view.*
 
@@ -18,6 +21,7 @@ class FlightFilterPriceRangeViewHolder(val view: View,
 
     private var startValue: Int = 0
     private var endValue: Int = 0
+    private var isExpanded: Boolean = true
 
     override fun bind(element: PriceRangeModel) {
         with(view) {
@@ -35,6 +39,20 @@ class FlightFilterPriceRangeViewHolder(val view: View,
                     etFlightLowestPrice.setText(FlightCurrencyFormatUtil.convertToIdrPriceWithoutSymbol(p0.first))
                     etFlightHighestPrice.setText(FlightCurrencyFormatUtil.convertToIdrPriceWithoutSymbol(p0.second))
                     listener.onPriceRangeChanged(p0.first, p0.second)
+                }
+            }
+
+            icFlightFoldableArrow.setOnClickListener {
+                if (isExpanded) {
+                    containerPriceText.hide()
+                    icFlightFoldableArrow.setImageDrawable(ContextCompat.getDrawable(context,
+                            com.tokopedia.resources.common.R.drawable.ic_system_action_arrow_down_gray_24))
+                    isExpanded = false
+                } else {
+                    containerPriceText.show()
+                    icFlightFoldableArrow.setImageDrawable(ContextCompat.getDrawable(context,
+                            com.tokopedia.resources.common.R.drawable.ic_system_action_arrow_up_gray_24))
+                    isExpanded = true
                 }
             }
         }
