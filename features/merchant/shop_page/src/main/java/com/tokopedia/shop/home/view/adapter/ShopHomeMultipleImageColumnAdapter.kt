ed9917fa474ba549cff1bc4f8fb19e14ec3eb4dd
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-02-26 16:28:24 +0700

    Create adapter multiple image column & slider square

---
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/ShopHomeMultipleImageColumnAdapter.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.shop.home.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.tokopedia.shop.R
+import com.tokopedia.shop.home.view.adapter.viewholder.ShopHomeItemImageColumnViewHolder
+import com.tokopedia.shop.home.view.model.WidgetDataModel
+
+class ShopHomeMultipleImageColumnAdapter: ListAdapter<WidgetDataModel, ShopHomeItemImageColumnViewHolder>(AdapterDiffCallback.WidgetDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopHomeItemImageColumnViewHolder {
+        val view = LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.item_shop_home_page_image_column, parent, false)
+        return ShopHomeItemImageColumnViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopHomeItemImageColumnViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
+    }
+
+}
