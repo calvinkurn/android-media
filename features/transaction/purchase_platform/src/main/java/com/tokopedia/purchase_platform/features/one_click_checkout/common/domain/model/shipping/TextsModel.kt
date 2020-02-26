17982122f6964e43a3380ec8b813a036a32ca4d5
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-02-26 09:13:06 +0700

    response shipping

---
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/model/shipping/TextsModel.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.shipping
+
+data class TextsModel (
+    var textEtd: String? = null,
+    var textServiceDesc: String? = null,
+    var textPrice: String? = null
+)
