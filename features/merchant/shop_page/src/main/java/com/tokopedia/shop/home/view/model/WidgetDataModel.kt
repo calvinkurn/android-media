1ac34d5fae673ef7270a447882e810fb9fc6b410
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-02-26 16:32:26 +0700

    create class pojo WidgetDataModel

---
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/model/WidgetDataModel.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.shop.home.view.model
+
+/**
+ * Created by rizqiaryansa on 2020-02-21.
+ */
+
+data class WidgetDataModel(
+        val imageUrl: String? = null,
+        val appLink: String? = null,
+        val webLinkL: String? = null,
+        val videoUrl: String? = null
+)
---
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/model/WidgetModel.kt
@@ -0,0 +1,26 @@
+package com.tokopedia.shop.home.view.model
+
+import com.tokopedia.shop.home.view.adapter.ShopHomeAdapterTypeFactory
+
+/**
+ * Created by rizqiaryansa on 2020-02-21.
+ */
+data class WidgetModel(
+        val widgetID: Int? = 0,
+        val layoutOrder: Int? = 0,
+        val name: String? = null,
+        val type: String? = null,
+        val header: HeaderWidgetModel? = null,
+        val data: MutableList<WidgetDataModel>? = null
+): BaseShopHomeViewModel {
+    override fun type(typeFactory: ShopHomeAdapterTypeFactory): Int {
+        return typeFactory.type(this)
+    }
+}
+
+data class HeaderWidgetModel(
+        val title: String? = null,
+        val ctaText: String? = null,
+        val ctaLink: String? = null,
+        val cover: String? = null
+)
