95389b94db1a093dc2b70b593cf101b345b4edd9
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-02-26 16:27:42 +0700

    create item decoration home shop page & add viewholder to AdapterTypeFactory

---
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/PaddingItemDecorationShopPage.kt
@@ -0,0 +1,36 @@
+package com.tokopedia.shop.home.view.adapter
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.shop.home.WidgetMultipleImageColumn
+import com.tokopedia.shop.home.WidgetSliderSquareBanner
+
+/**
+ * Created by rizqiaryansa on 2020-02-24.
+ */
+
+class PaddingItemDecorationShopPage(val typeWidget: String): RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        val position = parent.getChildAdapterPosition(view)
+
+        when(typeWidget) {
+            WidgetMultipleImageColumn -> {
+                if(position > 0) {
+                    parent.adapter.also {
+                        outRect.left = 3
+                    }
+                }
+            }
+            WidgetSliderSquareBanner -> {
+                if(position > 0) {
+                    parent.adapter.also {
+                        outRect.left = 8
+                    }
+                }
+            }
+        }
+
+    }
+}
--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/ShopHomeAdapterTypeFactory.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/ShopHomeAdapterTypeFactory.kt
@@ -1,17 +1,47 @@
 package com.tokopedia.shop.home.view.adapter
 
-import com.tokopedia.abstraction.base.view.adapter.Visitable
-import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
+import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
-import com.tokopedia.shop.home.view.model.BannerUIModel
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.shop.home.WidgetMultipleImageColumn
+import com.tokopedia.shop.home.WidgetSliderBanner
+import com.tokopedia.shop.home.WidgetSliderSquareBanner
+import com.tokopedia.shop.home.view.adapter.viewholder.*
+import com.tokopedia.shop.home.view.model.*
+import com.tokopedia.shop.home.view.model.WidgetModel
 import com.tokopedia.shop.newproduct.view.datamodel.ShopProductViewModel
 
-class ShopHomeAdapterTypeFactory: BaseAdapterTypeFactory() {
+class ShopHomeAdapterTypeFactory: BaseAdapterTypeFactory(), TypeFactoryShopHome{
+
+    override fun type(widgetModel: WidgetModel): Int {
+        when(widgetModel.name) {
+            WidgetMultipleImageColumn -> return ShopHomeMultipleImageColumnViewHolder.LAYOUT_RES
+            WidgetSliderSquareBanner -> return ShopHomeSliderSquareViewHolder.LAYOUT_RES
+            WidgetSliderBanner -> return ShopHomeSliderBannerViewHolder.LAYOUT_RES
+        }
+        return -1
+    }
+
     fun type(bannerUIModel: BannerUIModel): Int {
         return -1
     }
 
     fun type(shopProductViewModel: ShopProductViewModel): Int {
         return -1
+    }
+
+    override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<*> {
+        return when(type) {
+            ShopHomeMultipleImageColumnViewHolder.LAYOUT_RES -> ShopHomeMultipleImageColumnViewHolder(
+                    parent
+            )
+            ShopHomeSliderSquareViewHolder.LAYOUT_RES -> ShopHomeSliderSquareViewHolder(
+                    parent
+            )
+            ShopHomeSliderBannerViewHolder.LAYOUT_RES -> ShopHomeSliderBannerViewHolder(
+                    parent
+            )
+            else -> return super.createViewHolder(parent, type)
+        }
     }
 }
