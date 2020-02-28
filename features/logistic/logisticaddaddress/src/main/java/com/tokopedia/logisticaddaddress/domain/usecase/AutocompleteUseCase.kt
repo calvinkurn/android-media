6ed1d108659a271e4d94cb90c5eba8c923f9b3f3
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-02-28 11:09:20 +0700

    Refactor auto complete use case

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/domain/usecase/AutocompleteUseCase.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/domain/usecase/AutocompleteUseCase.kt
@@ -1,37 +1,39 @@
 package com.tokopedia.logisticaddaddress.domain.usecase
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.logisticaddaddress.R
+import com.tokopedia.logisticaddaddress.data.query.AutoCompleteQuery
+import com.tokopedia.logisticaddaddress.domain.executor.SchedulerProvider
+import com.tokopedia.logisticaddaddress.domain.mapper.AutoCompleteMapper
 import com.tokopedia.logisticaddaddress.domain.model.autocomplete.AutocompleteResponse
-import com.tokopedia.usecase.RequestParams
+import com.tokopedia.logisticaddaddress.features.autocomplete.model.SuggestedPlace
+import com.tokopedia.network.exception.MessageErrorException
 import rx.Observable
 import javax.inject.Inject
 
-/**
- * Created by fwidjaja on 2019-05-20.
- */
-class AutocompleteUseCase @Inject constructor(@ApplicationContext val context: Context) : GraphqlUseCase() {
-    var queryString: String = ""
+class AutocompleteUseCase
+@Inject constructor(
+        private val gql: GraphqlUseCase,
+        private val scheduler: SchedulerProvider,
+        private val mapper: AutoCompleteMapper) {
 
-    companion object {
-        val PARAM_INPUT = "#keystring"
-    }
-
-    fun setParams(input: String) {
-        queryString = GraphqlHelper.loadRawString(context.resources, R.raw.autocomplete)
-        queryString = queryString.replace(PARAM_INPUT, input, false)
-    }
-
-    override fun createObservable(params: RequestParams?): Observable<GraphqlResponse> {
-        val graphqlRequest = GraphqlRequest(queryString, AutocompleteResponse::class.java)
-        clearRequest()
-        addRequest(graphqlRequest)
-
-        return super.createObservable(params)
+    fun execute(query: String): Observable<List<SuggestedPlace>> {
+        val param = mapOf("param" to query)
+        val gqlRequest = GraphqlRequest(AutoCompleteQuery.keroAutoCompleteGeocode, AutocompleteResponse::class.java, param)
+        gql.clearRequest()
+        gql.addRequest(gqlRequest)
+        return gql.getExecuteObservable(null)
+                .map { gqlResponse ->
+                    val response: AutocompleteResponse? =
+                            gqlResponse.getData(AutocompleteResponse::class.java)
+                    response
+                            ?: throw MessageErrorException(
+                                    gqlResponse.getError(AutocompleteResponse::class.java)[0].message)
+                }
+                .map { mapper.mapAutoComplete(it) }
+                .subscribeOn(scheduler.io())
+                .observeOn(scheduler.ui())
     }
 }
