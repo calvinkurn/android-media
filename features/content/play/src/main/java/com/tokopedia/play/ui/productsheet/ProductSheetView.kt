0f203f1d9e12e317dabcc4fae2f1784f55fff186
kenny <kenny.hadisaputra@tokopedia.com>
2020-03-02 19:01:12 +0700

    - add initial capability for bottom sheet

--- features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/ProductSheetView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/ProductSheetView.kt
@@ -3,6 +3,7 @@ package com.tokopedia.play.ui.productsheet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.tokopedia.play.R
@@ -19,14 +20,21 @@ class ProductSheetView(container: ViewGroup) : UIView(container) {
     private val bottomSheetBehavior = BottomSheetBehavior.from(view)
 
     private val maxHeight : Int
-            get() = (container.height * 0.6).toInt()
+            get() = (container.height * PERCENT_PRODUCT_SHEET_HEIGHT).toInt()
+
+    init {
+        view.findViewById<ImageView>(R.id.iv_close)
+                .setOnClickListener {
+                    hide()
+                }
+    }
 
     override val containerId: Int = view.id
 
     override fun show() {
         if (view.height != maxHeight) {
             val layoutParams = view.layoutParams as CoordinatorLayout.LayoutParams
-            layoutParams.height = (container.height * 0.6).toInt()
+            layoutParams.height = maxHeight
             view.layoutParams = layoutParams
         }
 
@@ -35,5 +43,9 @@ class ProductSheetView(container: ViewGroup) : UIView(container) {
 
     override fun hide() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    companion object {
+        private const val PERCENT_PRODUCT_SHEET_HEIGHT = 0.6
     }
 }
