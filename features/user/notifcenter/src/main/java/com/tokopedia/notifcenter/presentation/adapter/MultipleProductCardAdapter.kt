bcfdfa60e0f010153acc4bf3dc3f4e9f4cfd2bbb
isfaaghyth <isfaaghyth@gmail.com>
2020-03-02 23:35:34 +0700

    notifcenter: (created) adapter of multi product card

---
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/MultipleProductCardAdapter.kt
@@ -0,0 +1,26 @@
+package com.tokopedia.notifcenter.presentation.adapter
+
+import android.view.ViewGroup
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
+import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.notifcenter.data.viewbean.MultipleProductCardViewBean
+import com.tokopedia.notifcenter.presentation.adapter.typefactory.product.MultipleProductCardFactoryImpl
+import com.tokopedia.notifcenter.util.resize
+
+class MultipleProductCardAdapter(
+        private val multipleProductCardFactory: MultipleProductCardFactoryImpl
+): BaseListAdapter<Visitable<*>, BaseAdapterTypeFactory>(multipleProductCardFactory) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AbstractViewHolder<out Visitable<*>> {
+        val containerView = onCreateViewItem(parent, viewType)
+        containerView.resize(80)
+        return multipleProductCardFactory.createViewHolder(containerView, viewType)
+    }
+
+    fun insertData(data: ArrayList<MultipleProductCardViewBean>) {
+        addElement(data)
+    }
+
+}
