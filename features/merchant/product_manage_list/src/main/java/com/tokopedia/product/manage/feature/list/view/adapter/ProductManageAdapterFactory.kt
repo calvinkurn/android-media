828b513b02caf43fed44f960585eab871cfd9db5
rezagama <reza.hidayat@tokopedia.com>
2020-03-02 16:10:09 +0700

    Configure product manage list DI

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/adapter/ProductManageAdapterFactory.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.product.manage.feature.list.view.adapter
+
+import android.view.View
+import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.factory.BaseListCheckableTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.holder.BaseCheckableViewHolder
+import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.product.manage.feature.list.view.adapter.viewholder.ProductViewHolder
+import com.tokopedia.product.manage.feature.list.view.model.ProductViewModel
+import com.tokopedia.product.manage.oldlist.view.adapter.ProductManageEmptyList
+
+class ProductManageAdapterFactory(
+    val listener: BaseCheckableViewHolder.CheckableInteractionListener,
+    private val viewHolderListener: ProductViewHolder.ProductViewHolderView
+) : BaseAdapterTypeFactory(), BaseListCheckableTypeFactory<ProductViewModel> {
+
+    override fun type(postalCodeViewModel: ProductViewModel): Int = ProductViewHolder.LAYOUT
+
+    override fun type(viewModel: EmptyModel?): Int =  ProductManageEmptyList.LAYOUT
+
+    override fun createViewHolder(view: View, type: Int): AbstractViewHolder<*> {
+        return if (type == ProductViewHolder.LAYOUT) {
+            ProductViewHolder(view, listener, viewHolderListener)
+        } else if (type == ProductManageEmptyList.LAYOUT) {
+            ProductManageEmptyList(view)
+        } else {
+            super.createViewHolder(view, type)
+        }
+    }
+}
