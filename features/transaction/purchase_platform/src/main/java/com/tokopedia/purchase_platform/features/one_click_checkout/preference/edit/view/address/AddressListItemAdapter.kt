125d782d4fe37ffba796b4c58bd8157a3678a938
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-03-03 16:51:05 +0700

    hit gql for addressList, search

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListItemAdapter.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListItemAdapter.kt
@@ -1,16 +1,20 @@
 package com.tokopedia.purchase_platform.features.one_click_checkout.preference.edit.view.address
 
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.kotlin.extensions.view.inflateLayout
+import com.tokopedia.logisticcart.shipping.model.RecipientAddressModel
 import com.tokopedia.purchase_platform.R
+import com.tokopedia.purchase_platform.features.checkout.subfeature.address_choice.domain.model.AddressListModel
 import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.Preference
 import kotlinx.android.synthetic.main.card_address_list.view.*
+import kotlin.math.log
 
-class AddressListItemAdapter : ListAdapter<Preference, AddressListItemAdapter.AddressListViewHolder>(DIFF_CALLBACK) {
+class AddressListItemAdapter : RecyclerView.Adapter<AddressListItemAdapter.AddressListViewHolder>() {
 
     companion object {
         val DIFF_CALLBACK = object: DiffUtil.ItemCallback<Preference>(){
@@ -25,34 +29,43 @@ class AddressListItemAdapter : ListAdapter<Preference, AddressListItemAdapter.Ad
         }
     }
 
+    var addressList = mutableListOf<RecipientAddressModel>()
     var lastCheckedPosition = -1
-    private val listAddressList = listOf(Preference(), Preference(), Preference(), Preference(), Preference())
-    private val listener: OnAddressSelected? = null
+//    private val listAddressList = listOf(Preference(), Preference(), Preference(), Preference(), Preference())
+    private val listener: ActionListener? = null
 
-    interface OnAddressSelected{
-        fun onSeelct(selection: Preference)
+    interface ActionListener{
+        fun onSelect(selection: RecipientAddressModel)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressListViewHolder {
         return AddressListViewHolder(parent.inflateLayout(R.layout.card_address_list))
     }
 
-    override fun getItemCount(): Int = 5
-
-    override fun onBindViewHolder(holder: AddressListViewHolder, position: Int) {
-        holder.bind(listAddressList[position])
+    override fun getItemCount(): Int {
+        return addressList.size
     }
 
+    override fun onBindViewHolder(holder: AddressListViewHolder, position: Int) {
+        holder.bind(addressList[position])
+    }
+
+    /*Inner View Holder*/
     inner class AddressListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(data: Preference) {
+        fun bind(data: RecipientAddressModel) {
             with(itemView){
+                address_type.text = data.addressName
+                address_name.text = data.recipientName
+                address_number.text = data.recipientPhoneNumber
+                address_detail.text = data.street + ", " + data.destinationDistrictName + ", " + data.cityName + data.postalCode
+
                 item_address_radio.isChecked = lastCheckedPosition == adapterPosition
 
                 card_address_list.setOnClickListener {
                     lastCheckedPosition = adapterPosition
                     notifyDataSetChanged()
 
-                    listener?.onSeelct(data)
+                    listener?.onSelect(data)
                     item_address_radio.isChecked = !item_address_radio.isChecked
 
                 }
--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListViewModel.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/address/AddressListViewModel.kt
@@ -3,13 +3,78 @@ package com.tokopedia.purchase_platform.features.one_click_checkout.preference.e
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import com.tokopedia.purchase_platform.features.one_click_checkout.common.data.Preference
-import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.GetPreferenceEditUseCase
+import com.tokopedia.purchase_platform.features.checkout.subfeature.address_choice.domain.model.AddressListModel
+import com.tokopedia.purchase_platform.features.checkout.subfeature.address_choice.domain.usecase.GetAddressCornerUseCase
+import com.tokopedia.usecase.coroutines.Success
+import java.util.ArrayList
 import javax.inject.Inject
 
-class AddressListViewModel @Inject constructor(val useCase: GetPreferenceEditUseCase) : ViewModel() {
-    private val _addresslist: MutableLiveData<List<Preference>> = MutableLiveData(listOf(Preference(), Preference(), Preference()))
-    val addressList: LiveData<List<Preference>>
+const val EMPTY_STRING: String = ""
+
+class AddressListViewModel @Inject constructor(val useCase: GetAddressCornerUseCase) : ViewModel() {
+
+    private var currentPage: Int = 1
+    private var query: String = ""
+    private val _addresslist = MutableLiveData<AddressListModel>()
+    val addressList: LiveData<AddressListModel>
     get() = _addresslist
+
+    fun getAddress(){
+        useCase.execute(EMPTY_STRING)
+                .subscribe(object : rx.Observer<AddressListModel> {
+                    override fun onError(e: Throwable?) {
+                        onError(e)
+                    }
+
+                    override fun onNext(addressListModel: AddressListModel) {
+                        _addresslist.value = addressListModel
+                    }
+
+                    override fun onCompleted() {
+                    }
+                })
+    }
+
+    fun loadMore(){
+        useCase.loadMore(query, currentPage + 1)
+                .subscribe(object : rx.Observer<AddressListModel> {
+                    override fun onError(e: Throwable?) {
+                        onError(e)
+                    }
+
+                    override fun onNext(t: AddressListModel) {
+                        val addressList = _addresslist.value
+                        var addressListArray = addressList?.listAddress?.toMutableList()?: ArrayList()
+                        addressListArray.addAll(t.listAddress)
+
+                        addressList?.listAddress = addressListArray
+                        _addresslist.value = addressList
+                        currentPage++
+                    }
+
+                    override fun onCompleted() {
+                    }
+                })
+
+    }
+
+    fun searchAddress(query: String){
+        useCase.execute(query)
+                .subscribe(object : rx.Observer<AddressListModel?> {
+                    override fun onError(e: Throwable?) {
+                        onError(e)
+                    }
+
+                    override fun onNext(t: AddressListModel?) {
+                        _addresslist.value = t
+                    }
+
+                    override fun onCompleted() {
+                    }
+                })
+    }
+
+
 }
