03e664c78547ddf82e99858077275e4d73425c78
isfaaghyth <isfaaghyth@gmail.com>
2020-03-03 18:08:22 +0700

    notifcenter: (created) product card bottom sheet dialog

---
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/NotificationProductCardDialog.kt
@@ -0,0 +1,40 @@
+package com.tokopedia.notifcenter.presentation.fragment
+
+import android.content.Context
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.notifcenter.R
+import com.tokopedia.notifcenter.data.mapper.MultipleProductCardMapper
+import com.tokopedia.notifcenter.data.viewbean.NotificationItemViewBean
+import com.tokopedia.notifcenter.presentation.BaseBottomSheetDialog
+import com.tokopedia.notifcenter.presentation.adapter.MultipleProductCardAdapter
+import com.tokopedia.notifcenter.presentation.adapter.typefactory.product.MultipleProductCardFactoryImpl
+import com.tokopedia.unifyprinciples.Typography
+
+class NotificationProductCardDialog(
+        context: Context,
+        fragmentManager: FragmentManager
+): BaseBottomSheetDialog<NotificationItemViewBean>(context, fragmentManager) {
+
+    private val txtTitle = container?.findViewById<Typography>(R.id.txt_title)
+    private val txtDescription = container?.findViewById<Typography>(R.id.txt_description)
+    private val lstProducts = container?.findViewById<RecyclerView>(R.id.lst_products)
+
+    private val productCardAdapter by lazy {
+        val factory = MultipleProductCardFactoryImpl()
+        MultipleProductCardAdapter(factory)
+    }
+
+    override fun resourceId(): Int {
+        return R.layout.dialog_notification_list_product
+    }
+
+    override fun show(element: NotificationItemViewBean) {
+        txtTitle?.text = element.title
+        txtDescription?.text = element.body
+
+        lstProducts?.adapter = productCardAdapter
+        productCardAdapter.insertData(MultipleProductCardMapper.map(element.products))
+    }
+
+}
