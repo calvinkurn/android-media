835f502f1835be52af99dee7082aceb979507920
Henry Priyono <henry.priyono@tokopedia.com>
2020-03-04 12:35:18 +0700

    Add Fpm Noop

---
+++ libraries/fpm_noop/src/main/java/com/google/firebase/perf/FirebasePerformance.java
@@ -0,0 +1,25 @@
+package com.google.firebase.perf;
+
+import android.content.Context;
+import com.google.firebase.perf.metrics.Trace;
+
+import java.util.Map;
+
+
+public class FirebasePerformance {
+
+    private static FirebasePerformance instance;
+
+    public static FirebasePerformance getInstance() {
+        if (instance == null) {
+            instance = new FirebasePerformance();
+        }
+
+        return instance;
+    }
+
+    public Trace newTrace(@NonNull String var1) {
+        return new Trace();
+    }
+
+}
---
+++ libraries/fpm_noop/src/main/java/com/google/firebase/perf/metrics/Trace.java
@@ -0,0 +1,30 @@
+package com.google.firebase.perf.metrics;
+
+import android.content.Context;
+
+import java.util.Map;
+import androidx.annotation.NonNull;
+
+public class Trace {
+
+    public void start() {
+        //no-op
+    }
+
+    public void stop() {
+        //no-op
+    }
+
+    public void incrementCounter(@NonNull String var1, long var2) {
+        //no-op
+    }
+
+    public void putMetric(@NonNull String var1, long var2) {
+        //no-op
+    }
+
+    public void putAttribute(@NonNull String var1, @NonNull String var2) {
+        //no-op
+    }
+
+}
