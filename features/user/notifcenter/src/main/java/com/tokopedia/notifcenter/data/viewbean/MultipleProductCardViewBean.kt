309e84a6e58610a709e18ce644ee69ff7f4fdaa9
isfaaghyth <isfaaghyth@gmail.com>
2020-03-04 10:53:23 +0700

    notifcenter: (added) template key and notification id on multi product bean

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/viewbean/MultipleProductCardViewBean.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/viewbean/MultipleProductCardViewBean.kt
@@ -6,6 +6,8 @@ import com.tokopedia.notifcenter.data.entity.UserInfo
 import com.tokopedia.notifcenter.presentation.adapter.typefactory.product.MultipleProductCardFactory
 
 class MultipleProductCardViewBean(
+        var templateKey: String = "",
+        var notificationId: String = "",
         var userInfo: UserInfo = UserInfo(),
         var product: ProductData = ProductData()
 ): Visitable<MultipleProductCardFactory> {
