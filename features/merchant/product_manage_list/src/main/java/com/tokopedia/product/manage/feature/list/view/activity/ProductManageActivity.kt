ac0799e7e8446afd75d1a15ae559eaac82972499
rezagama <reza.hidayat@tokopedia.com>
2020-03-04 17:41:47 +0700

    Remove unused import

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/activity/ProductManageActivity.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/activity/ProductManageActivity.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.product.manage.feature.list.view.activity
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.play.core.splitcompat.SplitCompat
