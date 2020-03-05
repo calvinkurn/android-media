e2692dbb4be96cd7831cd23a5caa1874bffe8d42
rezagama <reza.hidayat@tokopedia.com>
2020-03-05 12:53:36 +0700

    Add filter option in view model

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/model/FilterViewModel.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/model/FilterViewModel.kt
@@ -5,16 +5,18 @@ import androidx.annotation.StringRes
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.product.manage.R
 import com.tokopedia.product.manage.feature.list.view.adapter.factory.ProductFilterAdapterFactory
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 
 sealed class FilterViewModel(
     @StringRes val titleId: Int,
     open val count: Int = 0,
+    val option: ProductStatus? = null,
     @DrawableRes val icon: Int? = null
 ) : Visitable<ProductFilterAdapterFactory> {
 
-    data class Active(override val count: Int): FilterViewModel(R.string.product_manage_filter_active, count)
-    data class InActive(override val count: Int): FilterViewModel(R.string.product_manage_filter_inactive, count)
-    data class Banned(override val count: Int): FilterViewModel(R.string.product_manage_filter_banned, count)
+    data class Active(override val count: Int): FilterViewModel(R.string.product_manage_filter_active, count, ProductStatus.ACTIVE)
+    data class InActive(override val count: Int): FilterViewModel(R.string.product_manage_filter_inactive, count, ProductStatus.INACTIVE)
+    data class Banned(override val count: Int): FilterViewModel(R.string.product_manage_filter_banned, count, ProductStatus.BANNED)
 
     object Default: FilterViewModel(R.string.product_manage_filter, icon = R.drawable.unify_filter_ic)
 
