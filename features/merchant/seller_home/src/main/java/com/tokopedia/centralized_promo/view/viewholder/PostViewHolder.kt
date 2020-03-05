e595f798b6c26cdade2e73b346eeace6878adb87
elscione <yusufhendrawan06@gmail.com>
2020-03-05 13:16:56 +0700

    Change post imageview size

--- features/merchant/seller_home/src/main/java/com/tokopedia/centralized_promo/view/viewholder/PostViewHolder.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/centralized_promo/view/viewholder/PostViewHolder.kt
@@ -5,7 +5,9 @@ import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolde
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.centralized_promo.view.model.PostUiModel
+import com.tokopedia.kotlin.extensions.view.dpToPx
 import com.tokopedia.kotlin.extensions.view.parseAsHtml
+import com.tokopedia.sellerhome.R
 import com.tokopedia.sellerhome.R.layout.sah_item_centralized_promo_post
 import kotlinx.android.synthetic.main.sah_item_centralized_promo_post.view.*
 
@@ -17,10 +19,18 @@ class PostViewHolder(view: View?) : AbstractViewHolder<PostUiModel>(view) {
 
     override fun bind(element: PostUiModel) {
         with(itemView) {
-            ImageHandler.loadImageThumbs(context, ivPromotionPost, element.featuredMediaUrl)
             tvPromotionPostTitle.text = element.title.parseAsHtml()
             tvPromotionPostTitleDescription.text = element.subtitle.parseAsHtml()
             setOnClickListener { openApplink(element.applink) }
+            loadImage(element.featuredMediaUrl)
+        }
+    }
+
+    private fun loadImage(featuredMediaURL: String) = with(itemView) {
+        if (featuredMediaURL.isNotBlank()) {
+            ImageHandler.loadImageRounded(context, ivPromotionPost, featuredMediaURL, context.dpToPx(8))
+        } else {
+            ImageHandler.loadImageRounded2(context, ivPromotionPost, R.drawable.error_drawable, context.dpToPx(8))
         }
     }
 
