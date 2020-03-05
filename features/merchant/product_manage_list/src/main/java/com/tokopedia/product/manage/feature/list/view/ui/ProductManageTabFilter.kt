e7526867d69395d30d5fa6ebd09c5c18e77889c6
rezagama <reza.hidayat@tokopedia.com>
2020-03-05 12:50:04 +0700

    Add ProductManageTabFilter

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/ui/ProductManageTabFilter.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.product.manage.feature.list.view.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.orZero
+import com.tokopedia.product.manage.feature.list.view.adapter.decoration.ProductFilterItemDecoration
+import com.tokopedia.product.manage.feature.list.view.adapter.viewholder.FilterViewHolder
+import com.tokopedia.product.manage.feature.list.view.model.FilterViewModel
+
+class ProductManageTabFilter: RecyclerView {
+
+    var selectedFilter: FilterViewModel? = null
+        private set
+
+    init {
+        addItemDecoration(ProductFilterItemDecoration())
+        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        isNestedScrollingEnabled = false
+    }
+
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    fun resetTabFilter(selectedTab: FilterViewHolder) {
+        for(i in 0..adapter?.itemCount.orZero()) {
+            val viewHolder = findViewHolderForAdapterPosition(i)
+            if(viewHolder is FilterViewHolder && viewHolder != selectedTab) {
+                viewHolder.resetFilter()
+            }
+        }
+    }
+
+    fun setSelectedFilter(selectedFilter: FilterViewModel) {
+        this.selectedFilter = selectedFilter
+    }
+
+    fun resetSelectedFilter() {
+        selectedFilter = null
+    }
+
+    fun isActive(): Boolean {
+        return selectedFilter != null
+    }
+}
