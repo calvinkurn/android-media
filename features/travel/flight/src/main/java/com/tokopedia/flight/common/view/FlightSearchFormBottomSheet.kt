712cd12be968ef2dff68da5b586325b03786a73e
Muhammad Furqan <furqan@tokopedia.com>
2020-03-09 16:28:40 +0700

    add listener for search form click

--- features/travel/flight/src/main/java/com/tokopedia/flight/common/view/FlightSearchFormBottomSheet.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/common/view/FlightSearchFormBottomSheet.kt
@@ -1,14 +1,23 @@
 package com.tokopedia.flight.common.view
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.tokopedia.flight.R
+import com.tokopedia.flight.airport.view.activity.FlightAirportPickerActivity
+import com.tokopedia.flight.dashboard.view.activity.FlightClassesActivity
+import com.tokopedia.flight.dashboard.view.activity.FlightSelectPassengerActivity
+import com.tokopedia.flight.dashboard.view.fragment.viewmodel.FlightClassViewModel
+import com.tokopedia.flight.dashboard.view.fragment.viewmodel.FlightPassengerViewModel
 import com.tokopedia.unifycomponents.BottomSheetUnify
+import kotlinx.android.synthetic.main.bottom_sheet_flight_search_form.*
+import java.util.*
 
 /**
  * @author by furqan on 09/03/2020
  */
-class FlightSearchFormBottomSheet : BottomSheetUnify() {
+class FlightSearchFormBottomSheet : BottomSheetUnify(), FlightSearchFormView.FlightSearchFormListener {
 
     private lateinit var mChildView: View
 
@@ -16,6 +25,62 @@ class FlightSearchFormBottomSheet : BottomSheetUnify() {
         super.onCreate(savedInstanceState)
 
         initBottomSheet()
+        initSearchForm()
+    }
+
+    override fun onDepartureAirportClicked() {
+        val intent = FlightAirportPickerActivity.createInstance(requireContext(), getString(R.string.flight_airportpicker_departure_title))
+        startActivityForResult(intent, REQUEST_CODE_AIRPORT_DEPARTURE)
+    }
+
+    override fun onDestinationAirportClicked() {
+        val intent = FlightAirportPickerActivity.createInstance(requireContext(), getString(R.string.flight_airportpicker_arrival_title))
+        startActivityForResult(intent, REQUEST_CODE_AIRPORT_DESTINATION)
+    }
+
+    override fun onDepartureDateClicked(departureDate: Date?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onReturnDateClicked(returnDate: Date?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPassengerClicked(passengerModel: FlightPassengerViewModel?) {
+        passengerModel?.let {
+            val intent = FlightSelectPassengerActivity.getCallingIntent(requireContext(), it)
+            startActivityForResult(intent, REQUEST_CODE_SELECT_PASSENGER)
+        }
+    }
+
+    override fun onClassClicked(classModel: FlightClassViewModel?) {
+        val intent = FlightClassesActivity.getCallingIntent(requireContext(), classModel?.id ?: -1)
+        startActivityForResult(intent, REQUEST_CODE_SELECT_CLASSES)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        flightSearchFormView.removeFocus()
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_AIRPORT_DEPARTURE -> {
+                }
+                REQUEST_CODE_AIRPORT_DESTINATION -> {
+                }
+                REQUEST_CODE_SELECT_PASSENGER -> {
+                    val passengerModel = data?.getParcelableExtra<FlightPassengerViewModel>(FlightSelectPassengerActivity.EXTRA_PASS_DATA)
+                    passengerModel?.let {
+                        flightSearchFormView.setPassengerView(it)
+                    }
+                }
+                REQUEST_CODE_SELECT_CLASSES -> {
+                    val classModel = data?.getParcelableExtra<FlightClassViewModel>(FlightClassesActivity.EXTRA_FLIGHT_CLASS)
+                    classModel?.let {
+                        flightSearchFormView.setClassView(it)
+                    }
+                }
+            }
+        }
     }
 
     private fun initBottomSheet() {
@@ -29,8 +94,17 @@ class FlightSearchFormBottomSheet : BottomSheetUnify() {
         setChild(mChildView)
     }
 
+    private fun initSearchForm() {
+        flightSearchFormView.listener = this
+    }
+
     companion object {
         const val TAG_SEARCH_FORM = "TagFlightSearchFormBottomSheet"
+
+        const val REQUEST_CODE_AIRPORT_DEPARTURE = 1
+        const val REQUEST_CODE_AIRPORT_DESTINATION = 2
+        const val REQUEST_CODE_SELECT_PASSENGER = 3
+        const val REQUEST_CODE_SELECT_CLASSES = 4
 
         fun getInstance(): FlightSearchFormBottomSheet =
                 FlightSearchFormBottomSheet()
--- features/travel/flight/src/main/java/com/tokopedia/flight/common/view/FlightSearchFormView.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/common/view/FlightSearchFormView.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import com.tokopedia.flight.R
+import com.tokopedia.flight.dashboard.view.fragment.cache.FlightDashboardCache
+import com.tokopedia.flight.dashboard.view.fragment.viewmodel.FlightClassViewModel
 import com.tokopedia.flight.dashboard.view.fragment.viewmodel.FlightPassengerViewModel
 import com.tokopedia.unifycomponents.BaseCustomView
+import kotlinx.android.synthetic.main.layout_flight_search_view.view.*
 import java.util.*
 
 /**
@@ -14,27 +17,30 @@ import java.util.*
 class FlightSearchFormView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
     : BaseCustomView(context, attrs, defStyleAttr) {
 
+    var listener: FlightSearchFormListener? = null
+
+    private var flightDashboardCache: FlightDashboardCache = FlightDashboardCache(context)
+
     private var isOneWay: Boolean = true
     private var departureDate: Date? = null
     private var departureDateString: String = ""
     private var returnDate: Date? = null
     private var returnDateString: String = ""
+    private var passengerModel: FlightPassengerViewModel? = null
     private var passengerString: String = ""
-    private var classString: String = ""
+    private var classModel: FlightClassViewModel? = null
 
     init {
         View.inflate(context, R.layout.layout_flight_search_view, this)
 
-        setOneWay()
-        setPassengerView()
+        renderFromCache()
+        setViewClickListener()
     }
 
-    fun setOneWay() {
-        isOneWay = true
-    }
-
-    fun setRoundTrip() {
-        isOneWay = false
+    private fun renderFromCache() {
+        setPassengerView(flightDashboardCache.passengerAdult, flightDashboardCache.passengerChild, flightDashboardCache.passengerInfant)
+        tvFlightPassenger.text = passengerString
+        tvFlightClass.text = getClassTitleById(flightDashboardCache.classCache)
     }
 
     fun isOneWay(): Boolean = isOneWay
@@ -43,7 +49,12 @@ class FlightSearchFormView @JvmOverloads constructor(context: Context, attrs: At
 
     }
 
+    fun setReturnDate(returnDate: Date) {
+
+    }
+
     fun setPassengerView(passengerModel: FlightPassengerViewModel) {
+        this.passengerModel = passengerModel
         var passengerFmt = ""
         if (passengerModel.adult > 0) {
             passengerFmt = passengerModel.adult.toString() + " " + context.getString(R.string.flight_dashboard_adult_passenger)
@@ -56,9 +67,49 @@ class FlightSearchFormView @JvmOverloads constructor(context: Context, attrs: At
         }
 
         passengerString = passengerFmt
+
+        // save passenger to cache
+        flightDashboardCache.putPassengerCount(passengerModel.adult, passengerModel.children, passengerModel.infant)
     }
 
-    fun setPassengerView(adult: Int = 1, children: Int = 0, infant: Int = 0) {
+    fun setClassView(classModel: FlightClassViewModel) {
+        this.classModel = classModel
+        flightDashboardCache.putClassCache(classModel.id)
+    }
+
+    fun removeFocus() {
+        tvFlightOriginLabel.clearFocus()
+        tvFlightOriginAirport.clearFocus()
+        tvFlightDestinationLabel.clearFocus()
+        tvFlightDestinationAirport.clearFocus()
+        tvFlightDepartureDateLabel.clearFocus()
+        tvFlightDepartureDate.clearFocus()
+        tvFlightReturnDateLabel.clearFocus()
+        tvFlightReturnDate.clearFocus()
+        tvFlightPassengerLabel.clearFocus()
+        tvFlightPassenger.clearFocus()
+        tvFlightClassLabel.clearFocus()
+        tvFlightClass.clearFocus()
+    }
+
+    private fun setViewClickListener() {
+        tvFlightOriginLabel.setOnClickListener { listener?.onDepartureAirportClicked() }
+        tvFlightOriginAirport.setOnClickListener { listener?.onDepartureAirportClicked() }
+        tvFlightDestinationLabel.setOnClickListener { listener?.onDestinationAirportClicked() }
+        tvFlightDestinationAirport.setOnClickListener { listener?.onDestinationAirportClicked() }
+        tvFlightDepartureDateLabel.setOnClickListener { listener?.onDepartureDateClicked(returnDate) }
+        tvFlightDepartureDate.setOnClickListener { listener?.onDepartureDateClicked(returnDate) }
+        tvFlightReturnDateLabel.setOnClickListener { listener?.onReturnDateClicked(returnDate) }
+        tvFlightReturnDate.setOnClickListener { listener?.onReturnDateClicked(returnDate) }
+        tvFlightPassengerLabel.setOnClickListener { listener?.onPassengerClicked(passengerModel) }
+        tvFlightPassenger.setOnClickListener { listener?.onPassengerClicked(passengerModel) }
+        tvFlightClassLabel.setOnClickListener { listener?.onClassClicked(classModel) }
+        tvFlightClass.setOnClickListener { listener?.onClassClicked(classModel) }
+    }
+
+    private fun setPassengerView(adult: Int = 1, children: Int = 0, infant: Int = 0) {
+        this.passengerModel = FlightPassengerViewModel(adult, children, infant)
+
         var passengerFmt = ""
         if (adult > 0) {
             passengerFmt = adult.toString() + " " + context.getString(R.string.flight_dashboard_adult_passenger)
@@ -71,10 +122,27 @@ class FlightSearchFormView @JvmOverloads constructor(context: Context, attrs: At
         }
 
         passengerString = passengerFmt
+
+        // save passenger to cache
+        flightDashboardCache.putPassengerCount(adult, children, infant)
     }
 
-    fun setClassView(classString: String) {
-        this.classString = classString
+    private fun getClassTitleById(classId: Int): String {
+        return when (classId) {
+            1 -> "Ekonomi"
+            2 -> "Bisnis"
+            3 -> "Utama"
+            else -> ""
+        }
+    }
+
+    interface FlightSearchFormListener {
+        fun onDepartureAirportClicked()
+        fun onDestinationAirportClicked()
+        fun onDepartureDateClicked(departureDate: Date?)
+        fun onReturnDateClicked(returnDate: Date?)
+        fun onPassengerClicked(passengerModel: FlightPassengerViewModel?)
+        fun onClassClicked(classModel: FlightClassViewModel?)
     }
 
 }
