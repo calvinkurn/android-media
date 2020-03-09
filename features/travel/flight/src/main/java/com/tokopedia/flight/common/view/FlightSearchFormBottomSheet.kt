8aae1deb2fb1a0b7aed854d7aca11fb64e9f6805
Muhammad Furqan <furqan@tokopedia.com>
2020-03-09 14:51:19 +0700

    bottom sheet for flight search form

---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/common/view/FlightSearchFormBottomSheet.kt
@@ -0,0 +1,32 @@
+package com.tokopedia.flight.common.view
+
+import android.os.Bundle
+import android.view.View
+import com.tokopedia.flight.R
+import com.tokopedia.unifycomponents.BottomSheetUnify
+
+/**
+ * @author by furqan on 09/03/2020
+ */
+class FlightSearchFormBottomSheet : BottomSheetUnify() {
+
+    private lateinit var mChildView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBottomSheet()
+    }
+
+    private fun initBottomSheet() {
+        showCloseIcon = false
+        showKnob = true
+        isDragable = true
+        isHideable = true
+        setTitle(getString(R.string.flight_change_search_label))
+
+        mChildView = View.inflate(requireContext(), R.layout.bottom_sheet_flight_search_form, null)
+        setChild(mChildView)
+    }
+
+}
