8e01c38334efdecac7b27c738b179ddbee7b39c1
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-03-10 15:29:19 +0700

    [Payment] adding trim on customerName

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/payment/PaymentMethodFragment.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/preference/edit/view/payment/PaymentMethodFragment.kt
@@ -90,7 +90,7 @@ class PaymentMethodFragment : BaseDaggerFragment() {
 //            WebView.setWebContentsDebuggingEnabled(true)
 //        }
 //        web_view.loadUrl("tokopedia://dummy/payment/listing?callback_url=http%3A%2F%2Flocalhost%3A8080%2Fdummy%2Fpayment%2Flisting&customer_email=davin.kurniawan%40tokopedia.com&customer_msisdn=&customer_name=Davin+Kurniawan&description=Manual+Transfer+%28qweqewqe+-+123123123%29&express_checkout_param=%7B%22account_name%22%3A%22qweqewqe%22%2C%22account_number%22%3A%22123123123%22%2C%22bank_id%22%3A%221%22%7D&express_checkout_url=http%3A%2F%2Flocalhost%3A8080%2Fv2%2Fapi%2Fpayment%2FMANUALTRANSFER&gateway_code=MANUALTRANSFER&image=https%3A%2F%2Fecs7.tokopedia.net%2Fimg%2Ftoppay%2Fpayment-logo%2Ficon-bca.png&merchant_code=tokopediatest&message=Success&profile_code=EXPRESS_SAVE&signature=610d56a2c6d12cc17250145b05aeb445b6c806df&success=true&user_id=34437")
-        val data = "merchant_code=tokopediatest&profile_code=EXPRESS_SAVE&user_id=${UserSession(context).userId}&customer_name=${UserSession(context).name}&customer_email=${UserSession(context).email}&callback_url=https%3A%2F%2Fpay.tokopedia.com%2Fv2%2Fpayment%2Fregister%2Flisting"
+        val data = "merchant_code=tokopediatest&profile_code=EXPRESS_SAVE&user_id=${UserSession(context).userId}&customer_name=${UserSession(context).name.trim()}&customer_email=${UserSession(context).email}&callback_url=https%3A%2F%2Fpay.tokopedia.com%2Fv2%2Fpayment%2Fregister%2Flisting"
         web_view.postUrl("https://pay-staging.tokopedia.com/v2/payment/register/listing", data.toByteArray())
     }
 
