5c98bc567bab7f48d807495625893ff2fd4c2b11
noiz354 <noiz354@gmail.com>
2020-03-10 11:12:19 +0700

    fix testing.

--- libraries/composite_lib/graphql_beta_notif/src/main/java/com/tokopedia/grapqhl/beta/notif/BetaInterceptor.kt
+++ libraries/composite_lib/graphql_beta_notif/src/main/java/com/tokopedia/grapqhl/beta/notif/BetaInterceptor.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.grapqhl.beta.notif
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.tokopedia.graphql.beta.notif.R
 import okhttp3.Interceptor
@@ -22,13 +24,13 @@ class BetaInterceptor(private val context: Context) : Interceptor {
 
         val headers = response.headers()
         if (headers.size() > 0) {
-            val get = headers.get("access-control-allow-origin")
+            var get = headers.get("access-control-allow-origin")
             if (get.equals("https://gql-beta.tokopedia.com")) {
                 context.let {
                     //Get an instance of NotificationManager//
 
                     val mBuilder =
-                            NotificationCompat.Builder(context)
+                            NotificationCompat.Builder(context, "beta")
                                     .setSmallIcon(R.drawable.beta_icon)
                                     .setContentTitle("My notification")
                                     .setContentText("Hello World!")
@@ -36,7 +38,13 @@ class BetaInterceptor(private val context: Context) : Interceptor {
 
                     // Gets an instance of the NotificationManager service//
 
-                    val mNotificationManager = context.getSystemService (Context.NOTIFICATION_SERVICE) as NotificationManager
+                    var mNotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        mNotificationManager.createNotificationChannel(
+                                NotificationChannel("beta",
+                                        context.getString(R.string.chucker_notification_category),
+                                        NotificationManager.IMPORTANCE_LOW))
+                    }
 
                     mNotificationManager.notify(1, mBuilder.build())
                 }
