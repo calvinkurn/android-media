a3c09a4500686a92559599fe5894317cc6571c69
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-03-11 17:03:18 +0700

    change AppCompatImageView to ImageUnify in ViewHolder

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeItemImageColumnViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeItemImageColumnViewHolder.kt
@@ -3,11 +3,11 @@ package com.tokopedia.shop.home.view.adapter.viewholder
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
 import com.tokopedia.shop.R
 import com.tokopedia.shop.home.view.listener.ShopHomeDisplayWidgetListener
 import com.tokopedia.shop.home.view.model.ShopHomeDisplayWidgetUiModel
+import com.tokopedia.unifycomponents.setImage
 
 /**
  * Created by rizqiaryansa on 2020-02-21.
@@ -23,7 +23,8 @@ class ShopHomeItemImageColumnViewHolder(
     var parentPosition: Int = 0
 
     fun bind(data: ShopHomeDisplayWidgetUiModel.DisplayWidgetItem) {
-        ImageHandler.LoadImage(ivMultipleColumn, data.imageUrl)
+        ivMultipleColumn.setImage(data.imageUrl, 0F)
+
         ivMultipleColumn.setOnClickListener {
             listener.onItemClicked(displayWidgetUiModel, data, parentPosition, adapterPosition)
         }
