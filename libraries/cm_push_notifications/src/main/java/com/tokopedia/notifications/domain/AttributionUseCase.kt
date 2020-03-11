71982d670ba0f97b66a34c691742dbdc3252bc97
isfaaghyth <isfaaghyth@gmail.com>
2020-03-11 09:49:23 +0700

    pushnotif: changed query and param builder on attributionUseCase

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/domain/AttributionUseCase.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/domain/AttributionUseCase.kt
@@ -5,7 +5,10 @@ import com.tokopedia.notifications.data.model.AttributionNotifier
 
 typealias UseCase = GraphqlUseCase<AttributionNotifier>
 
-class AttributionUseCase(private val useCase: UseCase) {
+class AttributionUseCase(
+        private val useCase: UseCase,
+        private val query: String
+) {
 
     fun execute(
             setParams: Map<String, Any>,
@@ -15,32 +18,34 @@ class AttributionUseCase(private val useCase: UseCase) {
         useCase.apply {
             setTypeClass(AttributionNotifier::class.java)
             setRequestParams(setParams)
-            setGraphqlQuery(QUERY)
+            setGraphqlQuery(query)
             execute(onSuccess, onError)
         }
     }
 
-    fun params(transactionId: String?, receipientId: String?): Map<String, Any> {
+    fun params(
+            transactionId: String?,
+            userTransId: String?,
+            shopId: String?,
+            blastId: String?
+    ): Map<String, Any> {
         return hashMapOf<String, Any>().apply {
             put(PARAM_TRANSACTION_ID, transactionId?: "")
-            put(PARAM_RECIPIENT_ID, receipientId?: "")
-            put(PARAM_STATUS, "delivered")
+            put(PARAM_USER_TRANSACTION_ID, userTransId?: "")
+            put(PARAM_STATUS, VALUE_STATUS)
+            put(PARAM_SHOP_ID, shopId?: "")
+            put(PARAM_BLAST_ID, blastId?: "")
         }
     }
 
     companion object {
         private const val PARAM_TRANSACTION_ID = "trans_id"
-        private const val PARAM_RECIPIENT_ID = "recipient_id"
+        private const val PARAM_USER_TRANSACTION_ID = "user_trans_id"
         private const val PARAM_STATUS = "status"
+        private const val PARAM_SHOP_ID = "shop_id"
+        private const val PARAM_BLAST_ID = "blast_id"
 
-        private val QUERY = """
-            mutation WebHookPushNotification(${'$'}trans_id: String!, ${'$'}recipient_id: String!, ${'$'}status: String!) {
-                notifier_sendWebhookPushNotification(trans_id:${'$'}trans_id, recipient_id:${'$'}recipient_id, status:${'$'}status){
-                      is_success
-                      error_message
-                }
-            }
-        """.trimIndent()
+        private const val VALUE_STATUS = "clicked"
     }
 
 }
