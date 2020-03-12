f0c1bc286bdc963baa9ec5e44d7621466493ead1
Dari <47311456+darianthedytoped@users.noreply.github.com>
2020-03-12 17:29:39 +0700

    fix compile error

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -15,13 +15,16 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.bugsnag.android.BeforeNotify;
 import com.bugsnag.android.BreadcrumbType;
 import com.bugsnag.android.Bugsnag;
+import com.bugsnag.android.Error;
 import com.chuckerteam.chucker.api.Chucker;
 import com.chuckerteam.chucker.api.ChuckerCollector;
 import com.crashlytics.android.Crashlytics;
@@ -369,15 +372,15 @@ public class ConsumerMainApplication extends ConsumerRouterApplication implement
 
     private void initializeSdk() {
         Bugsnag.beforeNotify(new BeforeNotify() {
-                @Override
-                public boolean run(Error error) {
-                    UserSessionInterface userSession = new UserSession(this);
-                    if (!TextUtils.isEmpty(userSession.getUserId())) {
-                        error.addToTab("account", "userId", userSession.getUserId());
-                        error.addToTab("account", "deviceId", userSession.getDeviceId());
-                    }
-                    return false;
+            @Override
+            public boolean run(@NonNull Error error) {
+                UserSessionInterface userSession = new UserSession(ConsumerMainApplication.this);
+                if (!TextUtils.isEmpty(userSession.getUserId())) {
+                    error.addToTab("account", "userId", userSession.getUserId());
+                    error.addToTab("account", "deviceId", userSession.getDeviceId());
                 }
+                return false;
+            }
         });
         try {
             FirebaseApp.initializeApp(this);
