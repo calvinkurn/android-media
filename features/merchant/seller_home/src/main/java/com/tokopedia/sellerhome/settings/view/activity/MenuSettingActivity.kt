fbbd35641bfa26611e716b13d6c1e5229e1b7b28
Reivin <reivin.oktavianus@tokopedia.com>
2020-03-12 10:03:49 +0700

    Add color for status bar in module resource

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/activity/MenuSettingActivity.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/activity/MenuSettingActivity.kt
@@ -8,6 +8,7 @@ import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.sellerhome.R
 import com.tokopedia.sellerhome.settings.view.fragment.MenuSettingFragment
 
 class MenuSettingActivity : BaseSimpleActivity() {
@@ -20,12 +21,12 @@ class MenuSettingActivity : BaseSimpleActivity() {
         return super.onCreateView(parent, name, context, attrs)
     }
 
-    fun setupDrawerStatusBar() {
+    private fun setupDrawerStatusBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.apply {
                 addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
                 decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                statusBarColor = ContextCompat.getColor(context, com.tokopedia.design.R.color.white_95)
+                statusBarColor = ContextCompat.getColor(context, R.color.white_95)
             }
         }
     }
