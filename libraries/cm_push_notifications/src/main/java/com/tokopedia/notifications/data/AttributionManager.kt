98c60d499b1e71df71f73a537616f46350e0e3c4
isfaaghyth <isfaaghyth@gmail.com>
2020-03-12 21:04:29 +0700

    pushnotif: convert attributionManager from singleton to primitive class

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AttributionManager.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AttributionManager.kt
@@ -1,41 +1,22 @@
 package com.tokopedia.notifications.data
 
-import android.content.Context
-import android.util.Log
-import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
-import com.tokopedia.notifications.R
-import com.tokopedia.notifications.data.model.AttributionNotifier
 import com.tokopedia.notifications.domain.AttributionUseCase
 import com.tokopedia.notifications.model.BaseNotificationModel
-import com.tokopedia.abstraction.common.utils.GraphqlHelper.loadRawString as raw
+import javax.inject.Inject
 
-class AttributionManager(
-        private val useCase: AttributionUseCase,
-        private val notification: BaseNotificationModel?
+class AttributionManager @Inject constructor(
+        private val attributionUseCase: AttributionUseCase
 ) {
 
-    fun postAttribution() {
-        val params = useCase.params(
+    fun post(notification: BaseNotificationModel?) {
+        val params = attributionUseCase.params(
                 transactionId = notification?.transactionId,
                 userTransId = notification?.userTransactionId,
                 recipientId = notification?.userId,
                 shopId = notification?.shopId,
                 blastId = notification?.blastId
         )
-        useCase.execute(params)
-    }
-
-    companion object {
-        fun post(context: Context, notification: BaseNotificationModel?) {
-            val query = raw(context.resources, R.raw.query_notification_attribution)
-            val useCase = GraphqlUseCase<AttributionNotifier>(
-                    GraphqlInteractor.getInstance().graphqlRepository
-            )
-            val attributionUseCase = AttributionUseCase(useCase, query)
-            val manager = AttributionManager(attributionUseCase, notification)
-            manager.postAttribution()
-        }
+        attributionUseCase.execute(params)
     }
 
 }
