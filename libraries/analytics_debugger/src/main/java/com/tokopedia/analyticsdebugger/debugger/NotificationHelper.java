090aab8ad06e2001909212d51507265e2608dd54
Henry Priyono <henry.priyono@tokopedia.com>
2020-03-12 14:21:57 +0700

    Adjust Notif Channel For Low Priority

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/NotificationHelper.java
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/NotificationHelper.java
@@ -1,9 +1,11 @@
 package com.tokopedia.analyticsdebugger.debugger;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
@@ -19,15 +21,18 @@ import com.tokopedia.analyticsdebugger.debugger.ui.activity.FpmDebuggerActivity;
  */
 class NotificationHelper {
     private static final int NOTIFICATION_ID = 89324;
+    private static final String NOTIF_CHANNEL_ID = "DEBUGGING_TOOLS_CHANNEL";
+    private static final String NOTIF_CHANNEL_NAME = "Debugging Tools";
 
     public static void show(Context context, AnalyticsLogData data) {
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        setNotificationChannel(notificationManager);
 
         Intent intent = AnalyticsDebuggerActivity.newInstance(context);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.BigTextStyle inboxStyle = new NotificationCompat.BigTextStyle().bigText(data.getData());
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ANDROID_GENERAL_CHANNEL")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIF_CHANNEL_ID)
                 .setContentIntent(pendingIntent)
                 .setContentTitle("Open Analytics Debugger")
                 .setContentText(data.getName())
@@ -43,11 +48,12 @@ class NotificationHelper {
     public static void show(Context context, PerformanceLogModel data) {
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        setNotificationChannel(notificationManager);
 
         Intent intent = FpmDebuggerActivity.newInstance(context);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.BigTextStyle inboxStyle = new NotificationCompat.BigTextStyle().bigText(data.getData());
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ANDROID_GENERAL_CHANNEL")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIF_CHANNEL_ID)
                 .setContentIntent(pendingIntent)
                 .setContentTitle("Open Performance Debugger")
                 .setContentText(data.getTraceName())
@@ -63,11 +69,12 @@ class NotificationHelper {
     public static void show(Context context, ApplinkLogModel data) {
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        setNotificationChannel(notificationManager);
 
         Intent intent = ApplinkDebuggerActivity.newInstance(context);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.BigTextStyle inboxStyle = new NotificationCompat.BigTextStyle().bigText(data.getData());
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ANDROID_GENERAL_CHANNEL")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIF_CHANNEL_ID)
                 .setContentIntent(pendingIntent)
                 .setContentTitle("Open Applink Debugger")
                 .setContentText(data.getApplink())
@@ -77,5 +84,15 @@ class NotificationHelper {
                 .setStyle(inboxStyle)
                 .setPriority(NotificationCompat.PRIORITY_LOW);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    public static void setNotificationChannel(NotificationManager notificationManager) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel(
+                    NOTIF_CHANNEL_ID,
+                    NOTIF_CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_LOW
+            ));
+        }
     }
 }
