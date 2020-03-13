39dce1183f7aaff043d3af67645dd55b9076c3fa
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-03-13 15:25:57 +0700

    Fix chat not auto up

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/fragment/TopChatRoomFragment.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/fragment/TopChatRoomFragment.kt
@@ -792,10 +792,10 @@ class TopChatRoomFragment : BaseChatFragment(), TopChatContract.View
     override fun onBackPressed(): Boolean {
         if (presenter.isUploading()) {
             showDialogConfirmToAbortUpload()
-            return true
+        } else {
+            finishActivity()
         }
-
-        return super.onBackPressed()
+        return true
     }
 
     private fun finishActivity() {
