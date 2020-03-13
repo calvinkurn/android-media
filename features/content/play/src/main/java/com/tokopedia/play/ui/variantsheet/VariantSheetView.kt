151b83273ff5bee306ce99f5434e7899c8b9c728
kenny <kenny.hadisaputra@tokopedia.com>
2020-03-13 14:56:59 +0700

    - placeholder button product action

--- features/content/play/src/main/java/com/tokopedia/play/ui/variantsheet/VariantSheetView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/variantsheet/VariantSheetView.kt
@@ -1,13 +1,9 @@
 package com.tokopedia.play.ui.variantsheet
 
-import android.graphics.Outline
-import android.graphics.Rect
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewOutlineProvider
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -50,11 +46,12 @@ class VariantSheetView(
 
     private val clVariantContent: ConstraintLayout = view.findViewById(R.id.cl_variant_content)
     private val clProductVariant: ConstraintLayout = view.findViewById(R.id.cl_product_variant)
-    private val clProductVariantPlaceholder: ConstraintLayout = view.findViewById(R.id.cl_product_variant_placeholder)
+    private val phProductVariant: ConstraintLayout = view.findViewById(R.id.ph_product_variant)
     private val tvSheetTitle: TextView = view.findViewById(R.id.tv_sheet_title)
     private val rvVariantList: RecyclerView = view.findViewById(R.id.rv_variant_list)
     private val btnAction: UnifyButton = view.findViewById(R.id.btn_action)
-    private val btnContainer: FrameLayout = view.findViewById(R.id.btn_container)
+    private val phBtnAction: View = view.findViewById(R.id.ph_btn_action)
+    private val btnContainer: ConstraintLayout = view.findViewById(R.id.btn_container)
     private val vBottomOverlay: View = view.findViewById(R.id.v_bottom_overlay)
     private val ivProductImage: ImageView = view.findViewById(R.id.iv_product_image)
     private val tvProductTitle: TextView = view.findViewById(R.id.tv_product_title)
@@ -148,10 +145,16 @@ class VariantSheetView(
 
     internal fun showPlaceholder(isShow: Boolean) {
         if (isShow) {
-            clProductVariantPlaceholder.visible()
+            phProductVariant.visible()
+            phBtnAction.visible()
+
+            btnAction.gone()
             clProductVariant.gone()
         } else {
-            clProductVariantPlaceholder.gone()
+            phProductVariant.gone()
+            phBtnAction.gone()
+
+            btnAction.visible()
             clProductVariant.visible()
         }
     }
