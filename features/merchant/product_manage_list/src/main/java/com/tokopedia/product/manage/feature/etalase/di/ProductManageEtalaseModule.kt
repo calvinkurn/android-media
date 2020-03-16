31f4766a11b513033f0da907b1f9f37c91dac3c4
rezagama <reza.hidayat@tokopedia.com>
2020-03-16 13:30:12 +0700

    Remove provide get shop etalase

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/etalase/di/ProductManageEtalaseModule.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/etalase/di/ProductManageEtalaseModule.kt
@@ -1,19 +1,6 @@
 package com.tokopedia.product.manage.feature.etalase.di
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
-import com.tokopedia.shop.common.graphql.domain.usecase.shopetalase.GetShopEtalaseByShopUseCase
 import dagger.Module
-import dagger.Provides
 
 @Module(includes = [ProductManageEtalaseViewModelModule::class])
-class ProductManageEtalaseModule {
-
-    @ProductManageEtalaseScope
-    @Provides
-    fun provideGetShopeEtalaseByShopUseCase(
-        @ApplicationContext context: Context
-    ): GetShopEtalaseByShopUseCase {
-        return GetShopEtalaseByShopUseCase(context)
-    }
-}
+class ProductManageEtalaseModule
