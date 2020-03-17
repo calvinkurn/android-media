0827d87b22572df82fcbf769d6b263e9bcc6e2f0
leonard2907 <leonard.gunawan@tokopedia.com>
2020-03-17 15:15:09 +0700

    Added unit test for editPrice, editStock and delete

--- features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/list/ProductManageViewModelTest.kt
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/list/ProductManageViewModelTest.kt
@@ -9,17 +9,22 @@ import com.tokopedia.product.manage.feature.quickedit.price.domain.EditPriceUseC
 import com.tokopedia.product.manage.feature.quickedit.stock.domain.EditStockUseCase
 import com.tokopedia.product.manage.oldlist.domain.EditFeaturedProductUseCase
 import com.tokopedia.product.manage.oldlist.domain.PopupManagerAddProductUseCase
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 import com.tokopedia.shop.common.domain.interactor.GQLGetProductListUseCase
 import com.tokopedia.shop.common.domain.interactor.GQLGetShopInfoUseCase
 import com.tokopedia.topads.common.domain.interactor.TopAdsGetShopDepositGraphQLUseCase
 import com.tokopedia.user.session.UserSessionInterface
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockkObject
 import kotlinx.coroutines.Dispatchers
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class ProductManageViewModelTest {
-
-
 
     @RelaxedMockK
     lateinit var editStockUseCase: EditStockUseCase
@@ -72,5 +77,55 @@ class ProductManageViewModelTest {
                 multiEditProductUseCase,
                 dispatcher
         )
+    }
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun `editPrice should execute expected use case`() {
+        mockkObject(editPriceUseCase)
+        coEvery {
+            editPriceUseCase.executeOnBackground()
+        }
+        val productId = "0"
+        val price = "10000"
+        val productName = "Amazing Product"
+        viewModel.editPrice(productId, price, productName)
+        coVerify {
+            editPriceUseCase.executeOnBackground()
+        }
+    }
+
+    @Test
+    fun `editStock should execute expected use case`() {
+        mockkObject(editStockUseCase)
+        coEvery {
+            editStockUseCase.executeOnBackground()
+        }
+        val productId = "0"
+        val stock = 0
+        val productName = "Amazing Product"
+        val status = ProductStatus.ACTIVE
+        viewModel.editStock(productId, stock, productName, status)
+        coVerify {
+            editStockUseCase.executeOnBackground()
+        }
+    }
+
+    @Test
+    fun `deleteProduct should execute expected use case`() {
+        mockkObject(deleteProductUseCase)
+        coEvery {
+            deleteProductUseCase.executeOnBackground()
+        }
+        val productId = "0"
+        val productName = "Amazing Product"
+        viewModel.deleteSingleProduct(productName, productId)
+        coVerify {
+            deleteProductUseCase.executeOnBackground()
+        }
     }
 }
