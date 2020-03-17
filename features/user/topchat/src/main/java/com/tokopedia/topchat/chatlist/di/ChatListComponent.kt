ab33e88875481fd13b2e90de4aa37772fa4fa527
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-03-17 12:00:26 +0700

    Move chat tab list to fragment

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/di/ChatListComponent.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/di/ChatListComponent.kt
@@ -3,6 +3,7 @@ package com.tokopedia.topchat.chatlist.di
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.topchat.chatlist.activity.ChatListActivity
 import com.tokopedia.topchat.chatlist.fragment.ChatListFragment
+import com.tokopedia.topchat.chatlist.fragment.ChatTabListFragment
 import dagger.Component
 
 /**
@@ -26,4 +27,5 @@ import dagger.Component
 interface ChatListComponent {
     fun inject(fragment: ChatListFragment)
     fun inject(activity: ChatListActivity)
+    fun inject(chatTabListFragment: ChatTabListFragment)
 }
