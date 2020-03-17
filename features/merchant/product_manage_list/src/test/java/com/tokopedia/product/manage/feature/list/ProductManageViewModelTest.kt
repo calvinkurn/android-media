a76c545e5a0f71dbe731bb7f63e4026a29fcad13
leonard2907 <leonard.gunawan@tokopedia.com>
2020-03-17 17:26:03 +0700

    Change unit test style

--- features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/list/ProductManageViewModelTest.kt
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/list/ProductManageViewModelTest.kt
@@ -1,131 +1,119 @@
 package com.tokopedia.product.manage.feature.list
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.tokopedia.product.manage.feature.cashback.domain.SetCashbackUseCase
-import com.tokopedia.product.manage.feature.list.view.viewmodel.ProductManageViewModel
-import com.tokopedia.product.manage.feature.multiedit.domain.MultiEditProductUseCase
-import com.tokopedia.product.manage.feature.quickedit.delete.domain.DeleteProductUseCase
-import com.tokopedia.product.manage.feature.quickedit.price.domain.EditPriceUseCase
-import com.tokopedia.product.manage.feature.quickedit.stock.domain.EditStockUseCase
-import com.tokopedia.product.manage.oldlist.domain.EditFeaturedProductUseCase
-import com.tokopedia.product.manage.oldlist.domain.PopupManagerAddProductUseCase
+import com.tokopedia.product.manage.feature.quickedit.common.data.model.ProductUpdateV3Data
+import com.tokopedia.product.manage.feature.quickedit.common.data.model.ProductUpdateV3Response
+import com.tokopedia.product.manage.feature.quickedit.delete.data.model.DeleteProductResult
+import com.tokopedia.product.manage.feature.quickedit.price.data.model.EditPriceResult
+import com.tokopedia.product.manage.feature.quickedit.stock.data.model.EditStockResult
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
-import com.tokopedia.shop.common.domain.interactor.GQLGetProductListUseCase
-import com.tokopedia.shop.common.domain.interactor.GQLGetShopInfoUseCase
-import com.tokopedia.topads.common.domain.interactor.TopAdsGetShopDepositGraphQLUseCase
-import com.tokopedia.user.session.UserSessionInterface
-import io.mockk.MockKAnnotations
+import com.tokopedia.usecase.coroutines.Success
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.mockkObject
-import kotlinx.coroutines.Dispatchers
-import org.junit.Before
-import org.junit.Rule
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
-class ProductManageViewModelTest {
-
-    @RelaxedMockK
-    lateinit var editStockUseCase: EditStockUseCase
-
-    @RelaxedMockK
-    lateinit var editPriceUseCase: EditPriceUseCase
-
-    @RelaxedMockK
-    lateinit var gqlGetShopInfoUseCase: GQLGetShopInfoUseCase
-
-    @RelaxedMockK
-    lateinit var userSessionInterface: UserSessionInterface
-
-    @RelaxedMockK
-    lateinit var topAdsGetShopDepositGraphQLUseCase: TopAdsGetShopDepositGraphQLUseCase
-
-    @RelaxedMockK
-    lateinit var popupManagerAddProductUseCase: PopupManagerAddProductUseCase
-
-    @RelaxedMockK
-    lateinit var getProductListUseCase: GQLGetProductListUseCase
-
-    @RelaxedMockK
-    lateinit var editFeaturedProductUseCase: EditFeaturedProductUseCase
-
-    @RelaxedMockK
-    lateinit var deleteProductUseCase: DeleteProductUseCase
-
-    @RelaxedMockK
-    lateinit var multiEditProductUseCase: MultiEditProductUseCase
-
-    @get:Rule
-    val rule = InstantTaskExecutorRule()
-
-    private val dispatcher by lazy {
-        Dispatchers.Unconfined
-    }
-
-    private val viewModel by lazy {
-        ProductManageViewModel(
-                editPriceUseCase,
-                gqlGetShopInfoUseCase,
-                userSessionInterface,
-                topAdsGetShopDepositGraphQLUseCase,
-                popupManagerAddProductUseCase,
-                getProductListUseCase,
-                editFeaturedProductUseCase,
-                editStockUseCase,
-                deleteProductUseCase,
-                multiEditProductUseCase,
-                dispatcher
-        )
-    }
-
-    @Before
-    fun setup() {
-        MockKAnnotations.init(this)
-    }
+class ProductManageViewModelTest: ProductManageViewModelTestFixture() {
 
     @Test
     fun `editPrice should execute expected use case`() {
-        mockkObject(editPriceUseCase)
-        coEvery {
-            editPriceUseCase.executeOnBackground()
-        }
-        val productId = "0"
-        val price = "10000"
-        val productName = "Amazing Product"
-        viewModel.editPrice(productId, price, productName)
-        coVerify {
-            editPriceUseCase.executeOnBackground()
+        runBlocking {
+            val productId = "0"
+            val price = "10000"
+            val productName = "Amazing Product"
+            val productUpdateV3Response = ProductUpdateV3Response(productUpdateV3Data =
+                    ProductUpdateV3Data(isSuccess = true)
+            )
+
+            onEditPrice_thenReturn(productUpdateV3Response)
+
+            viewModel.editPrice(productId, price, productName)
+
+            val expectedEditPriceResult = Success(EditPriceResult(productName, productId, price))
+
+            verifyEditPriceUseCaseCalled()
+            verifyEditPriceResponseEquals(expectedEditPriceResult)
         }
     }
 
     @Test
     fun `editStock should execute expected use case`() {
-        mockkObject(editStockUseCase)
-        coEvery {
-            editStockUseCase.executeOnBackground()
-        }
-        val productId = "0"
-        val stock = 0
-        val productName = "Amazing Product"
-        val status = ProductStatus.ACTIVE
-        viewModel.editStock(productId, stock, productName, status)
-        coVerify {
-            editStockUseCase.executeOnBackground()
+        runBlocking {
+            val productId = "0"
+            val stock = 0
+            val productName = "Amazing Product"
+            val status = ProductStatus.ACTIVE
+            val productUpdateV3Response = ProductUpdateV3Response(productUpdateV3Data =
+                ProductUpdateV3Data(isSuccess = true)
+            )
+
+            onEditStock_thenReturn(productUpdateV3Response)
+
+            viewModel.editStock(productId, stock, productName, status)
+
+            val expectedEditStockResult = Success(EditStockResult(productName, productId, stock, status))
+
+            verifyEditStockUseCaseCalled()
+            verifyEditStockResponseEquals(expectedEditStockResult)
         }
     }
 
     @Test
     fun `deleteProduct should execute expected use case`() {
-        mockkObject(deleteProductUseCase)
-        coEvery {
-            deleteProductUseCase.executeOnBackground()
+        runBlocking {
+            val productId = "0"
+            val productName = "Amazing Product"
+            val productUpdateV3Response = ProductUpdateV3Response(productUpdateV3Data =
+                ProductUpdateV3Data(isSuccess = true)
+            )
+
+            onDeleteProduct_thenReturn(productUpdateV3Response)
+
+            viewModel.deleteSingleProduct(productName, productId)
+
+            val expectedDeleteProductResult = Success(DeleteProductResult(productName, productId))
+
+            verifyDeleteProductUseCaseCalled()
+            verifyDeleteProductResponseEquals(expectedDeleteProductResult)
         }
-        val productId = "0"
-        val productName = "Amazing Product"
-        viewModel.deleteSingleProduct(productName, productId)
-        coVerify {
-            deleteProductUseCase.executeOnBackground()
-        }
+    }
+
+    private suspend fun onEditPrice_thenReturn(productUpdateV3Response: ProductUpdateV3Response) {
+        coEvery { editPriceUseCase.executeOnBackground() } returns productUpdateV3Response
+    }
+
+    private suspend fun onEditStock_thenReturn(productUpdateV3Response: ProductUpdateV3Response) {
+        coEvery { editStockUseCase.executeOnBackground() } returns productUpdateV3Response
+    }
+
+    private suspend fun onDeleteProduct_thenReturn(productUpdateV3Response: ProductUpdateV3Response) {
+        coEvery { deleteProductUseCase.executeOnBackground() } returns productUpdateV3Response
+    }
+
+    private fun verifyEditPriceUseCaseCalled() {
+        coVerify { editPriceUseCase.executeOnBackground() }
+    }
+
+    private fun verifyEditStockUseCaseCalled() {
+        coVerify { editStockUseCase.executeOnBackground() }
+    }
+
+    private fun verifyDeleteProductUseCaseCalled() {
+        coVerify { deleteProductUseCase.executeOnBackground() }
+    }
+
+    private fun verifyEditPriceResponseEquals(expectedResponse: Success<EditPriceResult>) {
+        val actualEditPriceResult = (viewModel.editPriceResult.value as Success<EditPriceResult>)
+        assertEquals(expectedResponse, actualEditPriceResult)
+    }
+
+    private fun verifyEditStockResponseEquals(expectedResponse: Success<EditStockResult>) {
+        val actualEditStockResult = (viewModel.editStockResult.value as Success<EditStockResult>)
+        assertEquals(expectedResponse, actualEditStockResult)
+    }
+
+    private fun verifyDeleteProductResponseEquals(expectedResponse: Success<DeleteProductResult>) {
+        val actualDeleteStockResult = (viewModel.deleteProductResult.value as Success<DeleteProductResult>)
+        assertEquals(expectedResponse, actualDeleteStockResult)
     }
 }
---
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/list/ProductManageViewModelTestFixture.kt
@@ -0,0 +1,69 @@
+package com.tokopedia.product.manage.feature.list
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.product.manage.feature.list.view.viewmodel.ProductManageViewModel
+import com.tokopedia.product.manage.feature.multiedit.domain.MultiEditProductUseCase
+import com.tokopedia.product.manage.feature.quickedit.delete.domain.DeleteProductUseCase
+import com.tokopedia.product.manage.feature.quickedit.price.domain.EditPriceUseCase
+import com.tokopedia.product.manage.feature.quickedit.stock.domain.EditStockUseCase
+import com.tokopedia.product.manage.oldlist.domain.EditFeaturedProductUseCase
+import com.tokopedia.product.manage.oldlist.domain.PopupManagerAddProductUseCase
+import com.tokopedia.shop.common.domain.interactor.GQLGetProductListUseCase
+import com.tokopedia.shop.common.domain.interactor.GQLGetShopInfoUseCase
+import com.tokopedia.topads.common.domain.interactor.TopAdsGetShopDepositGraphQLUseCase
+import com.tokopedia.user.session.UserSessionInterface
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.Dispatchers
+import org.junit.Before
+import org.junit.Rule
+
+abstract class ProductManageViewModelTestFixture {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
+    lateinit var editStockUseCase: EditStockUseCase
+    @RelaxedMockK
+    lateinit var editPriceUseCase: EditPriceUseCase
+    @RelaxedMockK
+    lateinit var gqlGetShopInfoUseCase: GQLGetShopInfoUseCase
+    @RelaxedMockK
+    lateinit var userSessionInterface: UserSessionInterface
+    @RelaxedMockK
+    lateinit var topAdsGetShopDepositGraphQLUseCase: TopAdsGetShopDepositGraphQLUseCase
+    @RelaxedMockK
+    lateinit var popupManagerAddProductUseCase: PopupManagerAddProductUseCase
+    @RelaxedMockK
+    lateinit var getProductListUseCase: GQLGetProductListUseCase
+    @RelaxedMockK
+    lateinit var editFeaturedProductUseCase: EditFeaturedProductUseCase
+    @RelaxedMockK
+    lateinit var deleteProductUseCase: DeleteProductUseCase
+    @RelaxedMockK
+    lateinit var multiEditProductUseCase: MultiEditProductUseCase
+
+    protected lateinit var viewModel: ProductManageViewModel
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+
+        viewModel = ProductManageViewModel(
+                editPriceUseCase,
+                gqlGetShopInfoUseCase,
+                userSessionInterface,
+                topAdsGetShopDepositGraphQLUseCase,
+                popupManagerAddProductUseCase,
+                getProductListUseCase,
+                editFeaturedProductUseCase,
+                editStockUseCase,
+                deleteProductUseCase,
+                multiEditProductUseCase,
+                Dispatchers.Unconfined
+        )
+    }
+
+
+}
