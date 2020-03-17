5b0bcc31e99f08007243821935d4c572cc994cb6
rezagama <reza.hidayat@tokopedia.com>
2020-03-17 11:55:35 +0700

    Redirect contact cs button to help page

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/constant/ProductManageUrl.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/constant/ProductManageUrl.kt
@@ -5,4 +5,6 @@ object ProductManageUrl {
     const val ILLUSTRATION_MAX_FEATURED_PRODUCT_DOMAIN = "https://ecs7.tokopedia.net/android/others/illustration_max_featured_product.png"
     const val ILLUSTRATION_SPECIAL_FEATURED_PRODUCT_DOMAIN = "https://ecs7.tokopedia.net/android/others/illustration_special_featured_product.png"
     const val ILLUSTRATION_SET_CASHBACK_LIMIT_REACHED = "https://ecs7.tokopedia.net//android/manage_product//product_manage_cashback_limit_exceeded.png"
+
+    const val PRODUCT_VIOLATION_HELP_URL = "https://www.tokopedia.com/help/article/mengapa-produk-saya-dalam-pengawasan"
 }
