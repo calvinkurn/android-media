391d06edae2424b7e5bbdfc0d9cdbfe79ad820fa
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-03-18 16:06:30 +0700

    Done Umrah Unit Test Order Detail

--- features/salam/umrah/src/test/java/com/tokopedia/salam/umrah/orderdetail/presentation/viewmodel/UmrahOrderDetailViewModelTest.kt
+++ features/salam/umrah/src/test/java/com/tokopedia/salam/umrah/orderdetail/presentation/viewmodel/UmrahOrderDetailViewModelTest.kt
@@ -5,6 +5,10 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.salam.umrah.UmrahDispatchersProviderTest
 import com.tokopedia.salam.umrah.common.data.MyUmrahEntity
+import com.tokopedia.salam.umrah.common.data.UmrahValueLabelEntity
+import com.tokopedia.salam.umrah.common.presentation.model.UmrahSimpleDetailModel
+import com.tokopedia.salam.umrah.common.presentation.model.UmrahSimpleModel
+import com.tokopedia.salam.umrah.orderdetail.data.UmrahOrderDetailButtonModel
 import com.tokopedia.salam.umrah.orderdetail.data.UmrahOrderDetailsEntity
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
@@ -79,17 +83,89 @@ class UmrahOrderDetailViewModelTest{
     }
 
     @Test
-    fun `transformToSimpleModel_SuccessTransformToSimpleModel`(){
+    fun `transformToSimpleModel_SuccessTransformToSimpleModel_SuccessDataDetails`(){
+        val dataDetails = listOf(
+                UmrahOrderDetailsEntity.DataDetail("http://tokopedia.com/image",
+                        "http://tokopedia.com/backgroundColor",
+                        "#000","1","Order Detail"))
+        val simpleDetails = listOf(
+                UmrahSimpleModel(
+                        "Order Detail","1","#000"
+                )
+        )
 
+
+        val actual = viewModel.transformToSimpleModel(dataDetails, listOf(), listOf())
+        assert(simpleDetails[0].title.equals(actual[0].title))
+        assert(simpleDetails[0].description.equals(actual[0].description))
+        assert(simpleDetails[0].textColor.equals(actual[0].textColor))
+    }
+
+    @Test
+    fun `transformToSimpleModel_SuccessTransformToSimpleModel_SuccessDataPessanger`(){
+        val pessangers = listOf(
+                UmrahOrderDetailsEntity.Passenger(1,"Firmanda"))
+
+        val simpleDetails = listOf(
+                UmrahSimpleModel(
+                        "1. Firmanda"
+                ))
+
+
+        val actual = viewModel.transformToSimpleModel(listOf(),pessangers, listOf())
+        assert(simpleDetails[0].title.equals(actual[0].title))
+    }
+
+    @Test
+    fun `transformToSimpleModel_SuccessTransformToSimpleModel_SuccessDataValueLabel`(){
+
+        val valueLabelDatas = listOf(UmrahValueLabelEntity(
+                "2000000","Harga Pokok"
+        ))
+
+        val simpleDetails = listOf(
+                UmrahSimpleModel(
+                        "Harga Pokok",  "2000000"
+                )
+        )
+
+
+        val actual = viewModel.transformToSimpleModel(listOf(), listOf(),valueLabelDatas)
+        assert(simpleDetails[0].title.equals(actual[0].title))
+        assert(simpleDetails[0].description.equals(actual[0].description))
     }
 
     @Test
     fun `transformToSimpleDetailModel_SuccessTransformToSimpleDetailModel`(){
+        val dataDetails = listOf(
+                UmrahOrderDetailsEntity.DataDetail("http://tokopedia.com/image",
+                        "http://tokopedia.com/backgroundColor",
+                        "#000","1","Order Detail"))
+        val simpleDetails = listOf(
+                UmrahSimpleDetailModel(
+                        "Order Detail","1","http://tokopedia.com/image"
+                )
+        )
+
+        val actual = viewModel.transformToSimpleDetailModel(dataDetails)
+        assert(simpleDetails[0].icon.equals(actual[0].icon))
+        assert(simpleDetails[0].subtitle.equals(actual[0].subtitle))
+        assert(simpleDetails[0].title.equals(actual[0].title))
 
     }
 
     @Test
     fun `transformToButtonModel_SuccessTransformToButtonModel`(){
+        val umrahOrderDetailButtons =
+                listOf(UmrahOrderDetailButtonModel("1","type","http://tokopedia.com"))
 
+        val actioButtons =
+                listOf(UmrahOrderDetailsEntity.ActionButton("1","type",
+                UmrahOrderDetailsEntity.ActionButton.Body("http://tokopedia.com","http://tokopedia.com")))
+
+        val actual = viewModel.transformToButtonModel(actioButtons)
+        assert(umrahOrderDetailButtons[0].label.equals(actual[0].label))
+        assert(umrahOrderDetailButtons[0].buttonLink.equals(actual[0].buttonLink))
+        assert(umrahOrderDetailButtons[0].buttonType.equals(actual[0].buttonType))
     }
 }
