97680f8b79a554a7f737a6814fc188dd00e8cf05
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-03-18 19:35:36 +0700

    Changed slider square widget to use height ratio and shimmer

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeSliderSquareViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeSliderSquareViewHolder.kt
@@ -30,6 +30,10 @@ class ShopHomeSliderSquareViewHolder(
     override fun bind(element: ShopHomeDisplayWidgetUiModel) {
         val linearLayoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
 
+        val indexZero = getIndexRatio(element, 0).toFloat()
+        val indexOne = getIndexRatio(element,1).toFloat()
+        val heightRatio = (indexOne / indexZero)
+
         itemView.rvCarouselShopPageHome.apply {
             layoutManager = linearLayoutManager
             if (itemDecorationCount == 0) {
@@ -38,8 +42,13 @@ class ShopHomeSliderSquareViewHolder(
             adapter = shopHomeSliderSquareAdapter
         }
         shopHomeSliderSquareAdapter.displayWidgetUiModel = element
+        shopHomeSliderSquareAdapter.heightRatio = heightRatio
         shopHomeSliderSquareAdapter.parentPosition = adapterPosition
         shopHomeSliderSquareAdapter.submitList(element.data)
         itemView.tgHeaderSliderSquare.text = element.header.title
+    }
+
+    private fun getIndexRatio(data: ShopHomeDisplayWidgetUiModel, index: Int): Int {
+        return data.header.ratio.split(":")[index].toInt()
     }
 }
