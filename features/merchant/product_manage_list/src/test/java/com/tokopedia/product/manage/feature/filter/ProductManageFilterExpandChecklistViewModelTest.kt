6f0caf8eba77aedcccdd78ed4e464de0d1b790ca
leonard2907 <leonard.gunawan@tokopedia.com>
2020-03-19 07:49:05 +0700

    Add unit test for clearing all select in checklist

--- features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/filter/ProductManageFilterExpandChecklistViewModelTest.kt
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/feature/filter/ProductManageFilterExpandChecklistViewModelTest.kt
@@ -1,6 +1,25 @@
 package com.tokopedia.product.manage.feature.filter
 
-class ProductManageFilterExpandChecklistViewModelTest: ProductManageFilterViewModelTextFixture() {
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ProductManageFilterExpandChecklistViewModelTest: ProductManageFilterExpandChecklistViewModelTestFixture() {
+
+    @Test
+    fun `when_clear_all_checklist_should_update_select_values_to_false`() {
+        viewModel.clearAllChecklist()
+        verifyAllValuesAreFalse()
+    }
+
+    private fun verifyAllValuesAreFalse() {
+        val actualChecklistData = viewModel.checklistData.value
+        val actualNumberSelected = viewModel.dataSize.value
+        val expectedNumberSelected = 0
+        assertEquals(expectedNumberSelected, actualNumberSelected)
+        actualChecklistData?.forEach {
+            assertEquals(false,it.isSelected)
+        }
+    }
 
 
 }
