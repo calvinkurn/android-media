d9d3928108f36cee821cfcaead4a2e4a83a5ec36
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2020-03-20 16:06:34 +0700

    remove lihat semua card

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/dynamic_channel/MixLeftViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/dynamic_channel/MixLeftViewHolder.kt
@@ -105,9 +105,7 @@ class MixLeftViewHolder (itemView: View, val homeCategoryListener: HomeCategoryL
         listData.add(EmptyDataModel())
         val productDataList = convertDataToProductData(channel)
         listData.addAll(productDataList)
-        if (isHasSeeMoreApplink(channel)) {
-            listData.add(SeeMorePdpDataModel(applink =  channel.header.applink))
-        }
+
         adapter = MixLeftAdapter(listData,typeFactoryImpl)
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
