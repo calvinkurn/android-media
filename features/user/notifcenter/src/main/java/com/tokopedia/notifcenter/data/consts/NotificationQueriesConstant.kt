91984808dbedd5c785962e183641d81d543924c7
isfaaghyth <isfaaghyth@gmail.com>
2020-03-22 00:08:56 +0700

    notifcenter: added const variable for product stock handler

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/consts/NotificationQueriesConstant.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/consts/NotificationQueriesConstant.kt
@@ -5,9 +5,11 @@ object NotificationQueriesConstant {
     const val MUTATION_NOTIF_CENTER_PUSH_NOTIF = "notif_center_push_notif"
     const val TRANSACTION_NOTIFICATION = "transaction_notification"
     const val DRAWER_PUSH_NOTIFICATION = "drawer_push_notification"
+    const val PRODUCT_STOCK_HANDLER = "product_stock_handler"
     const val FILTER_NOTIFICATION = "filter_notification"
     const val QUERY_IS_TAB_UPDATE = "query_is_tab_update"
 
+    //global param
     const val PARAM_SECTION_TYPE = "section_type"
     const val PARAM_TEMPLATE_KEY = "template_key"
     const val PARAM_EXPIRED_TIME = "expired_time"
