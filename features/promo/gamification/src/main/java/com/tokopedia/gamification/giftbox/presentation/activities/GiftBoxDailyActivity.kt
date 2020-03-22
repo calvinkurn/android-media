1f979ee52b395513432cf08fb0d0bf8b1ff5a6d7
Rahul Lohra <rahul.lohra@tokopedia.com>
2020-03-23 01:13:13 +0530

    Add support for non logged user and refactor code

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/presentation/activities/GiftBoxDailyActivity.kt
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/presentation/activities/GiftBoxDailyActivity.kt
@@ -1,32 +1,11 @@
 package com.tokopedia.gamification.giftbox.presentation.activities
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.tokopedia.gamification.giftbox.presentation.fragments.GiftBoxDailyFragment
-import com.tokopedia.gamification.giftbox.presentation.fragments.GiftBoxNonLoginFragment
 import com.tokopedia.gamification.R
+import com.tokopedia.gamification.giftbox.presentation.fragments.GiftBoxDailyFragment
 
-class GiftBoxDailyActivity : AppCompatActivity() {
+class GiftBoxDailyActivity : BaseGiftBoxActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gift_box_daily)
-        if (savedInstanceState == null) {
-            showGiftBoxFragment()
-        }
-    }
+    override fun getLayout() = R.layout.activity_gift_box_daily
+    override fun getDestinationFragment() = GiftBoxDailyFragment()
 
-    fun showGiftBoxFragment() {
-        supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fm, GiftBoxDailyFragment())
-                .commit()
-    }
-
-    fun showNonLoginFragment() {
-        supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fm, GiftBoxNonLoginFragment())
-                .commit()
-    }
 }
--- features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/presentation/activities/GiftBoxTapTapActivity.kt
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/presentation/activities/GiftBoxTapTapActivity.kt
@@ -1,33 +1,11 @@
 package com.tokopedia.gamification.giftbox.presentation.activities
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.tokopedia.gamification.R
-
-import com.tokopedia.gamification.giftbox.presentation.fragments.GiftBoxNonLoginFragment
 import com.tokopedia.gamification.giftbox.presentation.fragments.GiftBoxTapTapFragment
 
-class GiftBoxTapTapActivity : AppCompatActivity() {
+class GiftBoxTapTapActivity : BaseGiftBoxActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gift_box_daily)
-        if (savedInstanceState == null) {
-            showGiftBoxFragment()
-        }
-    }
+    override fun getLayout() = R.layout.activity_gift_box_daily
+    override fun getDestinationFragment() = GiftBoxTapTapFragment()
 
-    fun showGiftBoxFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fm, GiftBoxTapTapFragment())
-            .commit()
-    }
-
-    fun showNonLoginFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fm, GiftBoxNonLoginFragment())
-            .commit()
-    }
 }
