b7cb195be83f8c31e5cb3422b3790194efcef4b7
isfaaghyth <isfaaghyth@gmail.com>
2020-03-23 01:40:39 +0700

    notifcenter: added for base fragment of notification center

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/di/NotificationComponent.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/di/NotificationComponent.kt
@@ -3,6 +3,7 @@ package com.tokopedia.notifcenter.di
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.notifcenter.di.module.*
 import com.tokopedia.notifcenter.di.scope.NotificationScope
+import com.tokopedia.notifcenter.presentation.BaseNotificationFragment
 import com.tokopedia.notifcenter.presentation.activity.NotificationActivity
 import com.tokopedia.notifcenter.presentation.fragment.NotificationTransactionFragment
 import com.tokopedia.notifcenter.presentation.fragment.NotificationUpdateFragment
@@ -23,5 +24,6 @@ import dagger.Component
 interface NotificationComponent {
     fun inject(fragment: NotificationTransactionFragment)
     fun inject(fragment: NotificationUpdateFragment)
+    fun inject(fragment: BaseNotificationFragment)
     fun inject(activity: NotificationActivity)
 }
