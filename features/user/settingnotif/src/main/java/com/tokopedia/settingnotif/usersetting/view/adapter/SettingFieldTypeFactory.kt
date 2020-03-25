3895f15105a75354d76d81313d4a6077f1e60522
isfaaghyth <isfaaghyth@gmail.com>
2020-03-26 01:33:55 +0700

    settingnotif: added activation, seller, and sms section on item factory

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/SettingFieldTypeFactory.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/SettingFieldTypeFactory.kt
@@ -3,19 +3,16 @@ package com.tokopedia.settingnotif.usersetting.view.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.factory.AdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.settingnotif.usersetting.domain.pojo.ChildSetting
-import com.tokopedia.settingnotif.usersetting.domain.pojo.ParentSetting
-import com.tokopedia.settingnotif.usersetting.domain.pojo.SettingSections
+import com.tokopedia.settingnotif.usersetting.domain.pojo.*
 import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.SettingViewHolder
 
 interface SettingFieldTypeFactory : AdapterTypeFactory {
-
+    fun type(settingSections: NotificationActivation): Int
     fun type(settingSections: SettingSections): Int
-
+    fun type(settingSections: SellerSection): Int
     fun type(parentSetting: ParentSetting): Int
-
     fun type(childSetting: ChildSetting): Int
-
+    fun type(parentSetting: SmsSection): Int
     fun createViewHolder(
             parent: View,
             type: Int,
--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/SettingFieldTypeFactoryImpl.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/SettingFieldTypeFactoryImpl.kt
@@ -3,26 +3,25 @@ package com.tokopedia.settingnotif.usersetting.view.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.settingnotif.usersetting.domain.pojo.ChildSetting
-import com.tokopedia.settingnotif.usersetting.domain.pojo.ParentSetting
-import com.tokopedia.settingnotif.usersetting.domain.pojo.SettingSections
-import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.ChildSettingViewHolder
-import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.ParentSettingViewHolder
-import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.SettingSectionViewHolder
-import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.SettingViewHolder
+import com.tokopedia.settingnotif.usersetting.domain.pojo.*
+import com.tokopedia.settingnotif.usersetting.listener.SectionItemListener
+import com.tokopedia.settingnotif.usersetting.view.adapter.viewholder.*
 
-class SettingFieldTypeFactoryImpl : BaseAdapterTypeFactory(), SettingFieldTypeFactory {
-    override fun type(settingSections: SettingSections): Int {
-        return SettingSectionViewHolder.LAYOUT
-    }
+class SettingFieldTypeFactoryImpl(
+        private val listener: SectionItemListener
+) : BaseAdapterTypeFactory(), SettingFieldTypeFactory {
 
-    override fun type(parentSetting: ParentSetting): Int {
-        return ParentSettingViewHolder.LAYOUT
-    }
+    override fun type(settingSections: SettingSections): Int = SettingSectionViewHolder.LAYOUT
 
-    override fun type(childSetting: ChildSetting): Int {
-        return ChildSettingViewHolder.LAYOUT
-    }
+    override fun type(parentSetting: ParentSetting): Int = ParentSettingViewHolder.LAYOUT
+
+    override fun type(childSetting: ChildSetting): Int = ChildSettingViewHolder.LAYOUT
+
+    override fun type(settingSections: NotificationActivation): Int = PushNotifActivationViewHolder.LAYOUT
+
+    override fun type(settingSections: SellerSection): Int = SellerSectionViewHolder.LAYOUT
+
+    override fun type(parentSetting: SmsSection): Int = SmsSectionViewHolder.LAYOUT
 
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<*> {
         return when (type) {
@@ -39,6 +38,9 @@ class SettingFieldTypeFactoryImpl : BaseAdapterTypeFactory(), SettingFieldTypeFa
         return when (type) {
             ParentSettingViewHolder.LAYOUT -> ParentSettingViewHolder(parent, settingListener)
             ChildSettingViewHolder.LAYOUT -> ChildSettingViewHolder(parent, settingListener)
+            PushNotifActivationViewHolder.LAYOUT -> PushNotifActivationViewHolder(parent)
+            SellerSectionViewHolder.LAYOUT -> SellerSectionViewHolder(listener, parent)
+            SmsSectionViewHolder.LAYOUT -> SmsSectionViewHolder(parent)
             else -> createViewHolder(parent, type)
         }
     }
