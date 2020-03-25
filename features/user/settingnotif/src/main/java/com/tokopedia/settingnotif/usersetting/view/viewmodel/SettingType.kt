dd26dbe22167acc2ff93ce93506f5ef9be0a4a6c
isfaaghyth <isfaaghyth@gmail.com>
2020-03-26 01:33:36 +0700

    settingnotif: added icon and seller type for creation setting menu

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/viewmodel/SettingType.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/viewmodel/SettingType.kt
@@ -1,25 +1,44 @@
 package com.tokopedia.settingnotif.usersetting.view.viewmodel
 
-import com.tokopedia.settingnotif.usersetting.view.fragment.EmailFieldFragment
-import com.tokopedia.settingnotif.usersetting.view.fragment.PushNotifFieldFragment
-import com.tokopedia.settingnotif.usersetting.view.fragment.SettingFieldFragment
-import com.tokopedia.settingnotif.usersetting.view.fragment.SmsFieldFragment
+import com.tokopedia.settingnotif.R
+import com.tokopedia.settingnotif.usersetting.view.fragment.*
 
 data class SettingType(
-        val name: String,
-        val settingFieldFragment: Class<out SettingFieldFragment>
+        val icon: Int = 0,
+        val name: String = "",
+        val fragment: Class<out SettingFieldFragment>
 ) {
 
     fun createNewFragmentInstance(): SettingFieldFragment {
-        return settingFieldFragment.newInstance()
+        return fragment.newInstance()
     }
 
     companion object {
         fun createSettingTypes(): List<SettingType> {
             return arrayListOf(
-                    SettingType("Push Notification", PushNotifFieldFragment::class.java),
-                    SettingType("Email", EmailFieldFragment::class.java),
-                    SettingType("SMS", SmsFieldFragment::class.java)
+                    SettingType(
+                            icon = R.drawable.ic_notifsetting_notification,
+                            name = "Push Notification",
+                            fragment = PushNotifFieldFragment::class.java
+                    ),
+                    SettingType(
+                            icon = R.drawable.ic_notifsetting_email,
+                            name = "Email",
+                            fragment = EmailFieldFragment::class.java
+                    ),
+                    SettingType(
+                            icon = R.drawable.ic_notifsetting_sms,
+                            name = "SMS",
+                            fragment = SmsFieldFragment::class.java
+                    )
+            )
+        }
+
+        fun createSellerType(): SettingType {
+            return SettingType(
+                    icon = 0,
+                    name = "Seller",
+                    fragment = SellerFieldFragment::class.java
             )
         }
     }
