1c3480dae58889e76f3c55ba647be84424e279a2
isfaaghyth <isfaaghyth@gmail.com>
2020-03-27 04:28:39 +0700

    settingnotif: set spannable for sms ticker content layout

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/viewholder/SmsSectionViewHolder.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/adapter/viewholder/SmsSectionViewHolder.kt
@@ -1,17 +1,48 @@
 package com.tokopedia.settingnotif.usersetting.view.adapter.viewholder
 
+import android.graphics.Typeface.BOLD
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.settingnotif.R
+import com.tokopedia.settingnotif.usersetting.const.Unify.Green_G500
 import com.tokopedia.settingnotif.usersetting.domain.pojo.SmsSection
+import com.tokopedia.unifycomponents.ticker.Ticker
 
 class SmsSectionViewHolder(itemView: View?): AbstractViewHolder<SmsSection>(itemView) {
 
-    override fun bind(element: SmsSection?) {}
+    private val txtTickerDescription = itemView?.findViewById<Ticker>(R.id.txtTickerDescription)
+
+    override fun bind(element: SmsSection?) {
+        val content = itemView.context.getString(R.string.settingnotif_ticker_sms_desc)
+        val inFull = itemView.context.getString(R.string.settingnotif_ticker_sms_more)
+        val shorten = "$content\n$inFull"
+        val spannable = SpannableString(shorten)
+        val color = ContextCompat.getColor(itemView.context, Green_G500)
+        spannable.setSpan(
+                ForegroundColorSpan(color),
+                shorten.indexOf(inFull),
+                shorten.indexOf(inFull) + inFull.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannable.setSpan(
+                StyleSpan(BOLD),
+                shorten.indexOf(inFull),
+                shorten.indexOf(inFull) + inFull.length,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        txtTickerDescription?.setTextDescription(spannable)
+    }
 
     companion object {
-        @LayoutRes val LAYOUT = R.layout.item_sms_section
+        @LayoutRes val LAYOUT = R.layout.item_sms_ticker_section
     }
 
 }
