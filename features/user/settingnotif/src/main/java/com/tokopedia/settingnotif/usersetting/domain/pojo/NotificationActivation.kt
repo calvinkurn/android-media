0b78f7ff236bcea02d8c36f043fac02102c9a172
isfaaghyth <isfaaghyth@gmail.com>
2020-03-27 03:04:46 +0700

    settingnotif: convert from plain string to resource string of activation

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/NotificationActivation.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/NotificationActivation.kt
@@ -1,12 +1,14 @@
 package com.tokopedia.settingnotif.usersetting.domain.pojo
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.settingnotif.usersetting.state.NotificationActivationState
 import com.tokopedia.settingnotif.usersetting.view.adapter.factory.SettingFieldTypeFactory
 
 data class NotificationActivation(
-        val title: String = "",
-        val description: String = "",
-        val action: String = ""
+        val title: Int = 0,
+        val description: Int = 0,
+        val action: Int = 0,
+        val type: NotificationActivationState
 ): Visitable<SettingFieldTypeFactory> {
 
     override fun type(typeFactory: SettingFieldTypeFactory): Int {
