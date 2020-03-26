3db48953ecd1a90708f1f8ff9b3cbca41e6f61b2
isfaaghyth <isfaaghyth@gmail.com>
2020-03-27 03:09:19 +0700

    settingnotif: showing email activation on first item list

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.settingnotif.usersetting.view.fragment
 
 import com.tokopedia.settingnotif.R
+import com.tokopedia.settingnotif.usersetting.view.dataview.NotificationActivationDataView.activationEmail
+import com.tokopedia.settingnotif.usersetting.view.dataview.UserSettingViewModel
 import com.tokopedia.settingnotif.usersetting.view.fragment.base.SettingFieldFragment
 
 class EmailFieldFragment: SettingFieldFragment() {
@@ -15,6 +17,14 @@ class EmailFieldFragment: SettingFieldFragment() {
 
     override fun getGqlRawQuery(): Int {
         return R.raw.query_email_setting
+    }
+
+    override fun onSuccessGetUserSetting(data: UserSettingViewModel) {
+        val dataSettings = arrayListOf<VisitableSettings>()
+        dataSettings.add(activationEmail())
+        dataSettings.addAll(data.data)
+        data.data = dataSettings.toList()
+        super.onSuccessGetUserSetting(data)
     }
 
 }
