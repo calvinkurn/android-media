c1aef5382ccbce21da7685750e63794d0be722c0
isfaaghyth <isfaaghyth@gmail.com>
2020-03-27 03:04:09 +0700

    settingnotif: added dataview and behaviour state for activation

---
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/dataview/NotificationActivationDataView.kt
@@ -0,0 +1,24 @@
+package com.tokopedia.settingnotif.usersetting.view.dataview
+
+import com.tokopedia.settingnotif.R
+import com.tokopedia.settingnotif.usersetting.domain.pojo.NotificationActivation
+import com.tokopedia.settingnotif.usersetting.state.Email
+import com.tokopedia.settingnotif.usersetting.state.PushNotif
+
+object NotificationActivationDataView {
+
+    fun activationPushNotif() = NotificationActivation(
+            title = R.string.settingnotif_title_activation,
+            description = R.string.settingnotif_desc_activation,
+            action = R.string.settingnotif_activation,
+            type = PushNotif
+    )
+
+    fun activationEmail() = NotificationActivation(
+            title = R.string.settingnotif_title_email,
+            description = R.string.settingnotif_desc_email,
+            action = R.string.settingnotif_activation_email,
+            type = Email
+    )
+
+}
