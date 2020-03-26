2b99dc75a421d2c2c85ce946d559bb9b57f9d0db
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-03-26 18:43:12 +0700

    Parse rfc 3339

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/checkout/view/ShipmentFragment.java
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/checkout/view/ShipmentFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -134,6 +135,7 @@ import com.tokopedia.purchase_platform.features.checkout.view.converter.RatesDat
 import com.tokopedia.purchase_platform.features.checkout.view.di.CheckoutModule;
 import com.tokopedia.purchase_platform.features.checkout.view.di.DaggerCheckoutComponent;
 import com.tokopedia.purchase_platform.features.checkout.view.dialog.ExpiredTimeDialog;
+import com.tokopedia.purchase_platform.features.checkout.view.helper.RfcDateTimeParser;
 import com.tokopedia.purchase_platform.features.checkout.view.uimodel.EgoldAttributeModel;
 import com.tokopedia.purchase_platform.features.checkout.view.uimodel.NotEligiblePromoHolderdata;
 import com.tokopedia.purchase_platform.features.checkout.view.uimodel.ShipmentButtonPaymentModel;
@@ -145,7 +147,9 @@ import com.tokopedia.unifyprinciples.Typography;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -711,6 +715,9 @@ public class ShipmentFragment extends BaseCheckoutFragment implements ShipmentCo
         shipmentAdapter.setShowOnboarding(shipmentPresenter.isShowOnboarding());
         if (shipmentPresenter.getCampaignTimer() != null) {
             CampaignTimerUi timer = shipmentPresenter.getCampaignTimer();
+
+            Date server = RfcDateTimeParser.parseDateString(timer.getTimerServer(), RfcDateTimeParser.RFC_3339);
+            Date expired = RfcDateTimeParser.parseDateString(timer.getTimerExpired(), RfcDateTimeParser.RFC_3339);
 
             cdLayout.setVisibility(View.VISIBLE);
             cdText.setText(timer.getTimerDescription());
