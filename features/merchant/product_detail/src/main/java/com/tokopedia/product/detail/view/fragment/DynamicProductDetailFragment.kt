029832bc6eb52f275650a4d6541003476ac1e510
irfan khoirul <irfankhoirul@gmail.com>
2020-03-27 07:42:38 +0700

    fix import

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/fragment/DynamicProductDetailFragment.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/fragment/DynamicProductDetailFragment.kt
@@ -144,8 +144,6 @@ import com.tokopedia.variant_common.view.ProductVariantListener
 import kotlinx.android.synthetic.main.dynamic_product_detail_fragment.*
 import kotlinx.android.synthetic.main.menu_item_cart.view.*
 import kotlinx.android.synthetic.main.partial_layout_button_action.*
-import com.tokopedia.common_tradein.utils.TradeInUtils
-import com.tokopedia.purchase_platform.common.sharedata.ShipmentFormRequest
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
