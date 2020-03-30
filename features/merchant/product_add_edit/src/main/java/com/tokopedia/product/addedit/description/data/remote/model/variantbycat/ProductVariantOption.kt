7f1d31ba248fddf891d43672564d6fb4aac3a740
Faisal Ramdani <faisal.ramdani@tokopedia.com>
2020-03-30 18:49:17 +0700

    convert variantbycat models to parcelize

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/data/remote/model/variantbycat/ProductVariantOption.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/data/remote/model/variantbycat/ProductVariantOption.kt
@@ -1,34 +1,29 @@
 package com.tokopedia.product.addedit.description.data.remote.model.variantbycat
 
-import android.os.Parcel
 import android.os.Parcelable
-
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by hendry on 8/14/2017.
  */
 
-class ProductVariantOption private constructor(data: Parcel) : Parcelable {
-
-    @SerializedName("value_id")
-    @Expose
-    var valueId: Int = 0
-        private set
-    @SerializedName("value")
-    @Expose
-    var itemId: String? = null
-        private set
-    @SerializedName("hex_code")
-    @Expose
-    var hexCode: String? = null
-        private set
-    @SerializedName("icon")
-    @Expose
-    var icon: String? = null
-        private set
-
+@Parcelize
+class ProductVariantOption(
+        @SerializedName("value_id")
+        @Expose
+        var valueId: Int = 0,
+        @SerializedName("value")
+        @Expose
+        var itemId: String? = null,
+        @SerializedName("hex_code")
+        @Expose
+        var hexCode: String? = null,
+        @SerializedName("icon")
+        @Expose
+        var icon: String? = null
+): Parcelable {
     val id: String
         get() = valueId.toString()
 
@@ -39,38 +34,7 @@ class ProductVariantOption private constructor(data: Parcel) : Parcelable {
         return itemId
     }
 
-    // this is for alias for picker type
-    fun getTitle(): String? {
-        return itemId
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeInt(this.valueId)
-        dest.writeString(this.itemId)
-        dest.writeString(this.hexCode)
-        dest.writeString(this.icon)
-    }
-
-    init {
-        this.valueId = data.readInt()
-        this.itemId = data.readString()
-        this.hexCode = data.readString()
-        this.icon = data.readString()
-    }
-
-    companion object CREATOR : Parcelable.Creator<ProductVariantOption> {
-        val TYPE = 199349
-
-        override fun createFromParcel(parcel: Parcel): ProductVariantOption {
-            return ProductVariantOption(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ProductVariantOption?> {
-            return arrayOfNulls(size)
-        }
+    companion object {
+        const val TYPE = 199349
     }
 }
--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/data/remote/model/variantbycat/ProductVariantUnit.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/data/remote/model/variantbycat/ProductVariantUnit.kt
@@ -1,57 +1,25 @@
 package com.tokopedia.product.addedit.description.data.remote.model.variantbycat
 
-import android.os.Parcel
 import android.os.Parcelable
-
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-
-import java.util.ArrayList
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by hendry on 8/14/2017.
  */
-
-class ProductVariantUnit private constructor(data: Parcel) : Parcelable {
-    @SerializedName("unit_id")
-    @Expose
-    var unitId: Int = 0
-    @SerializedName("name")
-    @Expose
-    var name: String? = null
-    @SerializedName("short_name")
-    @Expose
-    var shortName: String? = null
-    @SerializedName("values")
-    @Expose
-    var productVariantOptionList: List<ProductVariantOption> = emptyList()
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeValue(this.unitId)
-        dest.writeString(this.name)
-        dest.writeString(this.shortName)
-        dest.writeList(this.productVariantOptionList)
-    }
-
-    init {
-        this.unitId = data.readValue(Int::class.java.classLoader) as Int
-        this.name = data.readString()
-        this.shortName = data.readString()
-        this.productVariantOptionList = ArrayList()
-        data.readList(this.productVariantOptionList, ProductVariantOption::class.java.classLoader)
-    }
-
-    companion object CREATOR : Parcelable.Creator<ProductVariantUnit> {
-        override fun createFromParcel(parcel: Parcel): ProductVariantUnit {
-            return ProductVariantUnit(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ProductVariantUnit?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+class ProductVariantUnit (
+        @SerializedName("unit_id")
+        @Expose
+        var unitId: Int = 0,
+        @SerializedName("name")
+        @Expose
+        var name: String? = null,
+        @SerializedName("short_name")
+        @Expose
+        var shortName: String? = null,
+        @SerializedName("values")
+        @Expose
+        var productVariantOptionList: List<ProductVariantOption> = emptyList()
+) : Parcelable
