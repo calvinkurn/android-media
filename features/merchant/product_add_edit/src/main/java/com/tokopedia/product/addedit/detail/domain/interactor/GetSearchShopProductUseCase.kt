5a928c832f4428a5973de98110594565030e2682
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-03-30 11:48:13 +0700

    fix usecase

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/detail/domain/interactor/GetSearchShopProductUseCase.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/detail/domain/interactor/GetSearchShopProductUseCase.kt
@@ -1,11 +1,10 @@
 package com.tokopedia.product.addedit.detail.domain.interactor
 
 import com.tokopedia.abstraction.common.network.exception.MessageErrorException
-import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
-import com.tokopedia.graphql.data.model.CacheType
-import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlError
 import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.product.addedit.detail.domain.UniverseSearchResponse
 import com.tokopedia.product.addedit.detail.presentation.constant.AddEditProductDetailConstants.Companion.UNIVERSE_SEARCH_QUERY
 import com.tokopedia.usecase.coroutines.UseCase
@@ -13,10 +12,10 @@ import javax.inject.Inject
 import javax.inject.Named
 
 class GetSearchShopProductUseCase @Inject constructor(
-        private val graphqlUseCase: MultiRequestGraphqlUseCase,
+        private val graphqlRepository: GraphqlRepository,
         @Named(UNIVERSE_SEARCH_QUERY)
         private val gqlQuery: String
-) : UseCase<UniverseSearchResponse>() {
+): UseCase<UniverseSearchResponse>() {
 
     companion object {
         private const val KEY_SHOP_ID = "shopID"
@@ -35,23 +34,15 @@ class GetSearchShopProductUseCase @Inject constructor(
     var requestParams = mapOf<String, Any>()
 
     override suspend fun executeOnBackground(): UniverseSearchResponse {
-        graphqlUseCase.clearRequest()
-        graphqlUseCase.setCacheStrategy(GraphqlCacheStrategy
-                .Builder(CacheType.CLOUD_THEN_CACHE).build())
 
         val gqlRequest = GraphqlRequest(gqlQuery, UniverseSearchResponse::class.java, requestParams)
-        graphqlUseCase.addRequest(gqlRequest)
-        val gqlResponse = graphqlUseCase.executeOnBackground()
-        val error = gqlResponse.getError(GraphqlError::class.java)
-        if (error == null || error.isEmpty()) {
-            return gqlResponse.getData<UniverseSearchResponse>(UniverseSearchResponse::class.java)
+        val gqlResponse: GraphqlResponse = graphqlRepository.getReseponse(listOf(gqlRequest))
+        ​
+        val errors: List<GraphqlError>? = gqlResponse.getError(UniverseSearchResponse::class.java)
+        if (errors.isNullOrEmpty()) {
+            return gqlResponse.getData(UniverseSearchResponse.UniverseSearch::class.java)
         } else {
-            throw MessageErrorException(error.joinToString(", ") { it.message })
+            throw MessageErrorException(errors.joinToString(", ") { it.message })
         }
-    }
-
-
-    fun clearCache() {
-        graphqlUseCase.clearCache()
     }
 }
