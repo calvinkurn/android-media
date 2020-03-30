386ca407137878f1bbe266a3c04949fb80144fe9
yorispr <yoris.prayogo@tokopedia.com>
2020-03-30 16:53:43 +0700

    change action

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/util/ProductTrackingConstant.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/util/ProductTrackingConstant.kt
@@ -132,10 +132,10 @@ object ProductTrackingConstant {
         const val CLICK_EDIT_PRODUCT = "click - edit product"
         const val CLICK_ATC_NON_LOGIN = "click - tambah ke keranjang on pdp - non login"
 
-        const val CLICK_REQUEST_PERMISSION_IMEI = "click - beli but show popup request IMEI"
-        const val CLICK_ACCEPT_PERMISSION = "click -  berikan akses on access request bottomsheet"
-        const val CLICK_GO_TO_SETTING = "click - ke pengaturan hp on access request pop up"
-        const val CLICK_PERMISSION_LATER = "click - ke nanti saja on access request pop up"
+        const val CLICK_REQUEST_PERMISSION_IMEI = "click - beli then show popup {aktifkan akses telepon yuk/butuh akses ke hp mu} //pop up title"
+        const val CLICK_ACCEPT_PERMISSION = "click -  berikan akses on bottomsheet {aktifkan akses telepon yuk} //pop up title"
+        const val CLICK_GO_TO_SETTING = "click - ke pengaturan hp on popup {butuh akses ke hp mu} //pop up title"
+        const val CLICK_PERMISSION_LATER = "click - nanti saja on popup  {butuh akses ke hp mu} //pop up title"
         const val CLICK_NOTIFY_ME = "click - ingatkan saya on pdp campaign"
     }
 
