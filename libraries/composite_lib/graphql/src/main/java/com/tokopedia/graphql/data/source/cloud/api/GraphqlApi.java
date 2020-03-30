167f29547bb7ea41364e00e30580c318cd1bda7e
isfaaghyth <isfaaghyth@gmail.com>
2020-03-30 12:08:31 +0700

    akamai: change to headerMap for general purposes

--- libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlApi.java
+++ libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlApi.java
@@ -5,10 +5,10 @@ import com.tokopedia.graphql.GraphqlConstant;
 import com.tokopedia.graphql.data.model.GraphqlRequest;
 
 import java.util.List;
+import java.util.Map;
 
-import kotlinx.coroutines.Deferred;
 import retrofit2.http.Body;
-import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import rx.Observable;
@@ -18,12 +18,10 @@ import rx.Observable;
  */
 public interface GraphqlApi {
 
-    static final String AKAMAI_SENSOR_DATA = "X-acf-sensor-data";
-
     @POST("./")
     @Headers(GraphqlConstant.GqlApiKeys.GRAPHQL_HEADER)
     Observable<JsonArray> getResponse(
             @Body List<GraphqlRequest> requestObject,
-            @Header(AKAMAI_SENSOR_DATA) String sensorData
+            @HeaderMap Map<String, String> headerMap
     );
 }
--- libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlApiSuspend.kt
+++ libraries/composite_lib/graphql/src/main/java/com/tokopedia/graphql/data/source/cloud/api/GraphqlApiSuspend.kt
@@ -1,29 +1,23 @@
 package com.tokopedia.graphql.data.source.cloud.api;
 
-import com.google.gson.JsonArray;
-import com.tokopedia.graphql.GraphqlConstant;
-import com.tokopedia.graphql.data.model.GraphqlRequest;
-
-
-import kotlinx.coroutines.Deferred;
-import retrofit2.http.Body;
-import retrofit2.http.Header
-import retrofit2.http.Headers;
-import retrofit2.http.POST;
-import rx.Observable;
+import com.google.gson.JsonArray
+import com.tokopedia.graphql.GraphqlConstant
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import retrofit2.http.Body
+import retrofit2.http.HeaderMap
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 /**
  * Created by Vishal
  */
 interface GraphqlApiSuspend {
+
     @POST("./")
     @Headers(GraphqlConstant.GqlApiKeys.GRAPHQL_HEADER)
     suspend fun getResponseSuspend(
-            @Body requestObject:MutableList<GraphqlRequest>,
-            @Header(AKAMAI_SENSOR_DATA) sensorData: String
+            @Body requestObject: MutableList<GraphqlRequest>,
+            @HeaderMap header: Map<String, String>
     ) : JsonArray
 
-    companion object {
-        private const val AKAMAI_SENSOR_DATA = "X-acf-sensor-data"
-    }
 }
