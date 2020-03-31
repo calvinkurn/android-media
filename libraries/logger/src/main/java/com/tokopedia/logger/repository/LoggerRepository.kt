d1c26298a789cbd8e2a2f3ecc9f5c0929d1e3d16
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2020-03-31 11:41:57 +0700

    add truncate message for logentries

--- libraries/logger/src/main/java/com/tokopedia/logger/repository/LoggerRepository.kt
+++ libraries/logger/src/main/java/com/tokopedia/logger/repository/LoggerRepository.kt
@@ -50,7 +50,7 @@ class LoggerRepository(private val logDao: LoggerDao,
                                          TOKEN: Array<String>,
                                          logger: Logger): Int = coroutineScope {
         val message = encryptor.decrypt(logger.message, secretKey)
-        server.sendLogToServer(serverSeverity, TOKEN, message)
+        server.sendLogToServer(serverSeverity, TOKEN, truncate(message))
     }
 
     override suspend fun sendScalyrLogToServer(logs: List<Logger>) = coroutineScope {
@@ -62,14 +62,17 @@ class LoggerRepository(private val logDao: LoggerDao,
             //to make sure each timestamp in each row is unique
             ts += 1000
             val message = encryptor.decrypt(log.message, secretKey)
-            val truncatedMessage = if (message.length > Constants.MAX_BUFFER) {
-                message.substring(0, Constants.MAX_BUFFER)
-            } else {
-                message
-            }
-            scalyrEventList.add(ScalyrEvent(ts, ScalyrEventAttrs(truncatedMessage)))
+            scalyrEventList.add(ScalyrEvent(ts, ScalyrEventAttrs(truncate(message))))
         }
         scalyrLogger.sendLogToServer(scalyrEventList)
+    }
+
+    fun truncate (str:String):String {
+        return if (str.length > Constants.MAX_BUFFER) {
+            str.substring(0, Constants.MAX_BUFFER)
+        } else {
+            str
+        }
     }
 
     override suspend fun deleteExpiredHighPrio(timeStamp: Long) {
