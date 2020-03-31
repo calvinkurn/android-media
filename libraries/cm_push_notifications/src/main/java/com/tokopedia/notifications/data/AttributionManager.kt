162112febad2a9ce9711de6bb9682381c0173308
isfaaghyth <isfaaghyth@gmail.com>
2020-04-01 01:24:58 +0700

    pushnotif: added ATC module on data manager

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AttributionManager.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AttributionManager.kt
@@ -1,14 +1,26 @@
 package com.tokopedia.notifications.data
 
+import com.tokopedia.atc_common.data.model.request.AddToCartRequestParams
+import com.tokopedia.atc_common.domain.usecase.AddToCartUseCase
+import com.tokopedia.atc_common.domain.usecase.AddToCartUseCase.Companion.REQUEST_PARAM_KEY_ADD_TO_CART_REQUEST
 import com.tokopedia.notifications.domain.AttributionUseCase
 import com.tokopedia.notifications.model.BaseNotificationModel
+import com.tokopedia.notifications.subscriber.DataSubscriber.atcSubscriber
+import com.tokopedia.usecase.RequestParams
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 import javax.inject.Inject
 
 class AttributionManager @Inject constructor(
-        private val attributionUseCase: AttributionUseCase
+        private val attributionUseCase: AttributionUseCase,
+        private val atcProductUseCase: AddToCartUseCase
 ) {
 
-    fun post(notification: BaseNotificationModel?) {
+    /*
+    * Attribution tracker,
+    * Measure push notification order attribution which clicked by user
+    * */
+    fun attribution(notification: BaseNotificationModel?) {
         val params = attributionUseCase.params(
                 transactionId = notification?.transactionId,
                 userTransId = notification?.userTransactionId,
@@ -17,6 +29,38 @@ class AttributionManager @Inject constructor(
                 blastId = notification?.blastId
         )
         attributionUseCase.execute(params)
+    }
+
+    /*
+    * Add to cart of product,
+    * To make a seamless experience for user in
+    * push notification that contains product related
+    * */
+    fun atcProduct(productId: String, shopId: Int?) {
+        val params = atcParams(productId, shopId)
+        atcProductUseCase.createObservable(params)
+                .subscribeOn(Schedulers.io())
+                .unsubscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(atcSubscriber {
+                    println(it.toString())
+                })
+    }
+
+    companion object {
+        
+        fun atcParams(productId: String, shopId: Int?): RequestParams {
+            val addToCartRequestParams = AddToCartRequestParams()
+            addToCartRequestParams.productId = productId.toLongOrNull()?: 0
+            addToCartRequestParams.shopId = shopId?: -1
+            addToCartRequestParams.quantity = 1
+            addToCartRequestParams.notes = ""
+
+            return RequestParams.create().apply {
+                putObject(REQUEST_PARAM_KEY_ADD_TO_CART_REQUEST, addToCartRequestParams)
+            }
+        }
+        
     }
 
 }
