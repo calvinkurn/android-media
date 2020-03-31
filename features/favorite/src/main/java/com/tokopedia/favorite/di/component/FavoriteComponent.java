0903506dce2240ec01fffd49f8efac8b7deaf5cb
Henry Priyono <henry.priyono@tokopedia.com>
2020-03-31 13:13:08 +0700

    Add User Session Module

--- features/favorite/src/main/java/com/tokopedia/favorite/di/component/FavoriteComponent.java
+++ features/favorite/src/main/java/com/tokopedia/favorite/di/component/FavoriteComponent.java
@@ -1,18 +1,15 @@
 package com.tokopedia.favorite.di.component;
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
+import com.tokopedia.favorite.di.modul.UserSessionModule;
 import com.tokopedia.favorite.view.FragmentFavorite;
 import com.tokopedia.favorite.di.modul.FavoriteModule;
 import com.tokopedia.favorite.di.scope.FavoriteScope;
 
 import dagger.Component;
 
-/**
- * @author Kulomady on 1/20/17.
- */
-
 @FavoriteScope
-@Component(modules = FavoriteModule.class, dependencies = BaseAppComponent.class)
+@Component(modules = {FavoriteModule.class, UserSessionModule.class}, dependencies = BaseAppComponent.class)
 public interface FavoriteComponent {
     void inject(FragmentFavorite fragmentFavorite);
 }
