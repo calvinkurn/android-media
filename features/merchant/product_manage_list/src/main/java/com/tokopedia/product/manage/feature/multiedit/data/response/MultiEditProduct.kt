eb6d08d34231f603eeba1600c364c206db4c695e
rezagama <reza.hidayat@tokopedia.com>
2020-04-01 17:18:38 +0700

    Multi edit product to data class

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/multiedit/data/response/MultiEditProduct.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/multiedit/data/response/MultiEditProduct.kt
@@ -2,7 +2,7 @@ package com.tokopedia.product.manage.feature.multiedit.data.response
 
 import com.google.gson.annotations.SerializedName
 
-class MultiEditProduct {
+data class MultiEditProduct(
     @SerializedName("BulkProductEditV3")
     val results: List<MultiEditProductResult>? = null
-}
+)
