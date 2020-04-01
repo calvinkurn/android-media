3856786d82bcd4d4db651876acc28570c5564d7a
isfaaghyth <isfaaghyth@gmail.com>
2020-04-02 02:32:55 +0700

    pushnotif: added addToCart model

---
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/model/AddToCart.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.notifications.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import com.tokopedia.notifications.common.CMConstant
+
+data class AddToCart(
+        @Expose @SerializedName(CMConstant.PayloadKeys.PRODUCT_ID) var productId: Int? = 0,
+        @Expose @SerializedName(CMConstant.PayloadKeys.PRODUCT_NAME) var productName: String? = "",
+        @Expose @SerializedName(CMConstant.PayloadKeys.PRODUCT_BRAND) var productBrand: String? = "",
+        @Expose @SerializedName(CMConstant.PayloadKeys.PRODUCT_VARIANT) var productVariant: String? = "",
+        @Expose @SerializedName(CMConstant.PayloadKeys.PRODUCT_QUANTITY) var productQuantity: String? = "",
+        @Expose @SerializedName(CMConstant.PayloadKeys.SHOP_ID) var shopId: Int? = 0,
+        @Expose @SerializedName(CMConstant.PayloadKeys.SHOP_NAME) var shopName: String? = "",
+        @Expose @SerializedName(CMConstant.PayloadKeys.SHOP_TYPE) var shopType: String? = ""
+)
