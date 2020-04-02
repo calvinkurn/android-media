09232c30b24e985b600caeff6d0aeea022bb8c89
niranjan <niranjan.saxena@tokopedia.com>
2020-04-02 16:29:18 +0530

    lint for our app

---
+++ libraries/lint/src/main/java/com/tokopedia.linter/IssueRegistry.kt
@@ -0,0 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
+package com.tokopedia.linter
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+class IssueRegistry : IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(
+                SetColourDetector.ISSUE,
+                CoreResourcesDetector.ISSUE
+        )
+
+    override val api: Int = CURRENT_API
+
+}
