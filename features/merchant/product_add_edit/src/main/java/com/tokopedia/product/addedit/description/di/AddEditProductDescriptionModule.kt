fa0333db4c443968537aa1dd4e3080ae0af8082d
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-06 18:54:48 +0700

    add interceptor module

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/di/AddEditProductDescriptionModule.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/di/AddEditProductDescriptionModule.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.common.network.coroutines.RestRequestInteractor
 import com.tokopedia.common.network.coroutines.repository.RestRepository
+import com.tokopedia.network.interceptor.CommonErrorResponseInterceptor
 import com.tokopedia.product.addedit.description.data.remote.ProductVariantService
+import com.tokopedia.product.addedit.description.domain.usecase.GetYoutubeVideoUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 @Module(includes = [AddEditProductDescriptionViewModelModule::class])
@@ -39,12 +43,28 @@ class AddEditProductDescriptionModule {
 
     @AddEditProductDescriptionScope
     @Provides
-    fun provideRestRepository(): RestRepository = RestRequestInteractor.getInstance().restRepository
+    fun provideInterceptors(loggingInterceptor: HttpLoggingInterceptor,
+                            commonErrorResponseInterceptor: CommonErrorResponseInterceptor) =
+            mutableListOf(loggingInterceptor, commonErrorResponseInterceptor)
 
-//    @AddEditProductDescriptionScope
-//    @Provides
-//    fun provideGetYoutubeVideoUseCase(restRepository: RestRepository): GetYoutubeVideoUseCase {
-//        return GetYoutubeVideoUseCase(restRepository)
-//    }
+    @AddEditProductDescriptionScope
+    @Provides
+    fun provideRestRepository(interceptors: MutableList<Interceptor>,
+                              @ApplicationContext context: Context): RestRepository =
+            RestRequestInteractor.getInstance().restRepository.apply {
+        updateInterceptors(interceptors, context)
+    }
+
+    @AddEditProductDescriptionScope
+    @Provides
+    fun provideErrorInterceptors(): CommonErrorResponseInterceptor {
+        return CommonErrorResponseInterceptor()
+    }
+
+    @AddEditProductDescriptionScope
+    @Provides
+    fun provideGetYoutubeVideoUseCase(restRepository: RestRepository): GetYoutubeVideoUseCase {
+        return GetYoutubeVideoUseCase(restRepository)
+    }
 
 }
