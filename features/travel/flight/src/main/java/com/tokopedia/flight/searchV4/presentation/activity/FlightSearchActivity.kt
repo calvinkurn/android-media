4d7ede95d327614ad328744850a2f1d55774ed5d
Muhammad Furqan <furqan@tokopedia.com>
2020-04-06 10:57:43 +0700

    create new flight search activity and fragment

---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/searchV4/presentation/activity/FlightSearchActivity.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.flight.searchV4.presentation.activity
+
+import androidx.fragment.app.Fragment
+import com.tokopedia.flight.common.view.BaseFlightActivity
+
+/**
+ * @author by furqan on 06/04/2020
+ */
+open class FlightSearchActivity: BaseFlightActivity() {
+
+    override fun getNewFragment(): Fragment? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
