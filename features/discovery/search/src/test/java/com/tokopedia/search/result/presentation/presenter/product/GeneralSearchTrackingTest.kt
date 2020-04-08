f6978a79831dae606a40ac61fa5a47c7075e5f34
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-08 08:31:41 +0700

    Add General Search Tracking Test JUnit

---
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/GeneralSearchTrackingTest.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.search.result.presentation.presenter.product
+
+import org.junit.Test
+
+internal class GeneralSearchTrackingTest {
+
+    @Test
+    fun `General search tracking with result found`() {
+
+    }
+}
