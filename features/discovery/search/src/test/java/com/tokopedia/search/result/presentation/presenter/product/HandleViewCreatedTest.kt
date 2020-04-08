dee4330915a3d5c03fdf3a58b5316fe8f25e30c1
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-08 11:33:31 +0700

    Migrate handle view created to spek

--- features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/HandleViewCreatedTest.kt
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/HandleViewCreatedTest.kt
@@ -1,98 +1,82 @@
 package com.tokopedia.search.result.presentation.presenter.product
 
-import com.tokopedia.search.result.presentation.ProductListSectionContract
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.verifyOrder
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import org.junit.Test
 
-internal class HandleViewCreatedTest: Spek({
+internal class HandleViewCreatedTest: ProductListPresenterTestFixtures() {
 
-    Feature("Handle view created") {
+    @Test
+    fun `Handle view created when Product is first active tab`() {
+        `Given View is first active tab`()
 
-        
-        createTestInstance()
+        `When handle view created`()
 
-        Scenario("Handle view created when Product is first active tab") {
-            val productListView by memoized<ProductListSectionContract.View>()
-            lateinit var presenter: ProductListPresenter
-
-            Given("Product list presenter") {
-                presenter = createProductListPresenter()
-            }
-
-            Given("View is first active tab") {
-                every { productListView.isFirstActiveTab }.returns(true)
-            }
-
-            When("handle view created for first active tab") {
-                presenter.onViewCreated()
-            }
-
-            Then("Verify view interaction on view created") {
-                verifyOrder {
-                    productListView.isFirstActiveTab
-                    productListView.reloadData()
-                }
-
-                confirmVerified(productListView)
-            }
-        }
-
-        Scenario("Handle view created when product is not first active tab") {
-            val productListView by memoized<ProductListSectionContract.View>()
-            lateinit var presenter: ProductListPresenter
-
-            Given("Product list presenter") {
-                presenter = createProductListPresenter()
-            }
-
-            Given("View is NOT first active tab") {
-                every { productListView.isFirstActiveTab }.returns(false)
-            }
-
-            When("handle view created for first active tab") {
-                presenter.onViewCreated()
-            }
-
-            Then("Verify view interaction on view created") {
-                verifyOrder {
-                    productListView.isFirstActiveTab
-                }
-
-                confirmVerified(productListView)
-            }
-        }
-
-        Scenario("Handle view created multiple times") {
-            val productListView by memoized<ProductListSectionContract.View>()
-            lateinit var presenter: ProductListPresenter
-
-            Given("Product list presenter") {
-                presenter = createProductListPresenter()
-            }
-
-            Given("View is first active tab") {
-                every { productListView.isFirstActiveTab }.returns(true)
-            }
-
-            When("handle view created for first active tab") {
-                presenter.onViewCreated()
-                presenter.onViewCreated()
-                presenter.onViewCreated()
-            }
-
-            Then("Verify view interaction on view created") {
-                verifyOrder {
-                    productListView.isFirstActiveTab
-                    productListView.reloadData()
-                    productListView.isFirstActiveTab
-                    productListView.isFirstActiveTab
-                }
-
-                confirmVerified(productListView)
-            }
-        }
+        `Then verify view interaction when created and is first active tab`()
     }
-})
+
+    private fun `Given View is first active tab`() {
+        every { productListView.isFirstActiveTab }.returns(true)
+    }
+
+    private fun `When handle view created`() {
+        productListPresenter.onViewCreated()
+    }
+
+    private fun `Then verify view interaction when created and is first active tab`() {
+        verifyOrder {
+            productListView.isFirstActiveTab
+            productListView.reloadData()
+        }
+
+        confirmVerified(productListView)
+    }
+
+    @Test
+    fun `Handle view created when product is not first active tab`() {
+        `Given view is NOT first active tab`()
+
+        `When handle view created`()
+
+        `Then verify view interaction when created and not first active tab`()
+    }
+
+    private fun `Given view is NOT first active tab`() {
+        every { productListView.isFirstActiveTab }.returns(false)
+    }
+
+    private fun `Then verify view interaction when created and not first active tab`() {
+        verifyOrder {
+            productListView.isFirstActiveTab
+        }
+
+        confirmVerified(productListView)
+    }
+
+    @Test
+    fun `Handle view created multiple times (edge cases)`() {
+        `Given View is first active tab`()
+
+        `When handle view created multiple times`()
+
+        `Then verify view interaction when created multiple times`()
+    }
+
+    private fun `When handle view created multiple times`() {
+        productListPresenter.onViewCreated()
+        productListPresenter.onViewCreated()
+        productListPresenter.onViewCreated()
+    }
+
+    private fun `Then verify view interaction when created multiple times`() {
+        verifyOrder {
+            productListView.isFirstActiveTab
+            productListView.reloadData()
+            productListView.isFirstActiveTab
+            productListView.isFirstActiveTab
+        }
+
+        confirmVerified(productListView)
+    }
+}
