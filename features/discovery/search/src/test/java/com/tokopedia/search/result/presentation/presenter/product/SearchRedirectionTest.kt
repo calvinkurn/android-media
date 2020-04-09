2f91ea17c31f4c8e4d6fbfb1643fcd60bd29e1da
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-09 14:02:31 +0700

    Search Redirection test spek to junit

--- features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/SearchRedirectionTest.kt
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/SearchRedirectionTest.kt
@@ -1,85 +1,75 @@
 package com.tokopedia.search.result.presentation.presenter.product
 
 import com.tokopedia.discovery.common.constants.SearchApiConst
-import com.tokopedia.filter.common.data.DynamicFilterModel
 import com.tokopedia.search.result.complete
 import com.tokopedia.search.result.domain.model.SearchProductModel
-import com.tokopedia.search.result.presentation.ProductListSectionContract
 import com.tokopedia.search.result.presentation.presenter.product.testinstance.searchProductModelRedirection
 import com.tokopedia.search.shouldBe
 import com.tokopedia.usecase.RequestParams
-import com.tokopedia.usecase.UseCase
 import io.mockk.*
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import org.junit.Test
 import rx.Subscriber
 
-internal class SearchRedirectionTest: Spek({
+internal class SearchRedirectionTest: ProductListPresenterTestFixtures() {
 
-    Feature("Search success with redirection") {
+    private val requestParamsSlot = slot<RequestParams>()
 
-        
-        createTestInstance()
+    @Test
+    fun `Load Data Success With Redirection`() {
+        `Given Search Product API will return SearchProductModel with redirection`()
 
-        Scenario("Load Data Success With Redirection") {
-            val requestParamsSlot = slot<RequestParams>()
-            lateinit var productListPresenter: ProductListPresenter
+        `When Load Data`()
 
-            Given("Search Product API will return SearchProductModel with redirection") {
-                val searchProductFirstPageUseCase by memoized<UseCase<SearchProductModel>>()
-                every { searchProductFirstPageUseCase.execute(capture(requestParamsSlot), any()) }.answers {
-                    secondArg<Subscriber<SearchProductModel>>().complete(searchProductModelRedirection)
-                }
-            }
+        `Then verify use case request params START should be 0`()
+        `Then verify view interaction for redirection`()
+        `Then verify get dynamic filter use case is not executed`()
+        `Then verify start from is incremented`()
+    }
 
-            Given("Product List Presenter") {
-                productListPresenter = createProductListPresenter()
-            }
-
-            When("Load Data") {
-                val searchParameter : Map<String, Any> = mutableMapOf<String, Any>().also {
-                    it[SearchApiConst.Q] = "produk wardyah"
-                    it[SearchApiConst.START] = "0"
-                    it[SearchApiConst.UNIQUE_ID] = "unique_id"
-                    it[SearchApiConst.USER_ID] = productListPresenter.userId
-                }
-
-                productListPresenter.loadData(searchParameter)
-            }
-
-            Then("verify use case request params") {
-                val requestParams = requestParamsSlot.captured
-
-                requestParams.getString(SearchApiConst.START, null) shouldBe "0"
-            }
-
-            Then("verify view interaction") {
-                val productListView by memoized<ProductListSectionContract.View>()
-
-                verifyOrder {
-                    productListView.isAnyFilterActive
-
-                    verifyShowLoading(productListView)
-
-                    productListView.redirectSearchToAnotherPage(searchProductModelRedirection.searchProduct.redirection.redirectApplink)
-
-                    verifyHideLoading(productListView)
-                }
-
-                confirmVerified(productListView)
-            }
-
-            Then("verify get dynamic filter use case is not executed") {
-                val getDynamicFilterUseCase by memoized<UseCase<DynamicFilterModel>>()
-
-                verify(exactly = 1) { getDynamicFilterUseCase.execute(any(), any()) }
-            }
-
-            Then("verify start from is incremented") {
-                val startFrom = productListPresenter.startFrom
-
-                startFrom shouldBe SearchApiConst.DEFAULT_VALUE_OF_PARAMETER_ROWS.toInt()
-            }
+    private fun `Given Search Product API will return SearchProductModel with redirection`() {
+        every { searchProductFirstPageUseCase.execute(capture(requestParamsSlot), any()) }.answers {
+            secondArg<Subscriber<SearchProductModel>>().complete(searchProductModelRedirection)
         }
     }
-})
+
+    private fun `When Load Data`() {
+        val searchParameter : Map<String, Any> = mutableMapOf<String, Any>().also {
+            it[SearchApiConst.Q] = "produk wardyah"
+            it[SearchApiConst.START] = "0"
+            it[SearchApiConst.UNIQUE_ID] = "unique_id"
+            it[SearchApiConst.USER_ID] = productListPresenter.userId
+        }
+
+        productListPresenter.loadData(searchParameter)
+    }
+
+    private fun `Then verify use case request params START should be 0`() {
+        val requestParams = requestParamsSlot.captured
+
+        requestParams.getString(SearchApiConst.START, null) shouldBe "0"
+    }
+
+    private fun `Then verify view interaction for redirection`() {
+        verifyOrder {
+            productListView.isAnyFilterActive
+
+            verifyShowLoading(productListView)
+
+            productListView.redirectSearchToAnotherPage(searchProductModelRedirection.searchProduct.redirection.redirectApplink)
+
+            verifyHideLoading(productListView)
+        }
+
+        confirmVerified(productListView)
+    }
+
+    private fun `Then verify get dynamic filter use case is not executed`() {
+        verify(exactly = 1) { getDynamicFilterUseCase.execute(any(), any()) }
+    }
+
+    private fun `Then verify start from is incremented`() {
+        val startFrom = productListPresenter.startFrom
+
+        startFrom shouldBe SearchApiConst.DEFAULT_VALUE_OF_PARAMETER_ROWS.toInt()
+    }
+}
