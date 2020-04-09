d267643332d47a2d187fa90f1522b5bdd03b8068
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-09 14:38:59 +0700

    Request profile list data spek to junit

--- features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/profile/RequestProfileListDataTest.kt
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/profile/RequestProfileListDataTest.kt
@@ -1,132 +1,103 @@
 package com.tokopedia.search.result.presentation.presenter.profile
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
-import com.tokopedia.search.InstantTaskExecutorRuleSpek
 import com.tokopedia.search.result.domain.model.SearchProfileModel
-import com.tokopedia.search.result.presentation.ProfileListSectionContract
 import com.tokopedia.search.result.presentation.presenter.profile.testinstance.searchProfileModelEmptyResult
 import com.tokopedia.search.result.presentation.presenter.profile.testinstance.searchProfileModelEmptyResultWithRecommendation
 import com.tokopedia.search.shouldBe
-import com.tokopedia.usecase.UseCase
-import io.mockk.confirmVerified
-import io.mockk.every
-import io.mockk.slot
-import io.mockk.verifyOrder
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import io.mockk.*
+import org.junit.Test
 import rx.Subscriber
 
-class RequestProfileListDataTest: Spek({
-    InstantTaskExecutorRuleSpek(this)
+class RequestProfileListDataTest: ProfileListPresenterTestFixtures() {
 
-    Feature("Request Profile List Data Empty Result") {
+    private val query = "dariand"
+    private val slotVisitableList = slot<List<Visitable<*>>>()
 
-        
-        createTestInstance()
+    @Test
+    fun `No Result Empty Profile`() {
+        `Given Search Profile API will return SearchProfileModel`(searchProfileModelEmptyResult)
 
-        Scenario("No Result Empty Profile") {
-            val query = "dariand"
-            val profileListView by memoized<ProfileListSectionContract.View>()
-            val searchProfileUseCase by memoized<UseCase<SearchProfileModel>>()
-            val slotVisitableList = slot<List<Visitable<*>>>()
+        `When Request Profile List Data`()
 
-            lateinit var profileListPresenter: ProfileListPresenter
+        `Then verify total profile count`(searchProfileModelEmptyResult.aceSearchProfile.count)
+        `Then verify has next page`(searchProfileModelEmptyResult.aceSearchProfile.hasNext)
+        `Then verify view interaction for empty search`()
+        `Then verify Visitable List should contain empty search model`()
+    }
 
-            Given("Profile list presenter") {
-                profileListPresenter = createProfileListPresenter()
-            }
-
-            Given("Search Profile API will return empty result") {
-                every { searchProfileUseCase.execute(any(), any()) }.answers {
-                    secondArg<Subscriber<SearchProfileModel>>().onNext(searchProfileModelEmptyResult)
-                }
-            }
-
-            When("Request Profile List Data") {
-                profileListPresenter.requestProfileListData(query, 1)
-            }
-
-            Then("Total profile count should be ${searchProfileModelEmptyResult.aceSearchProfile.count}") {
-                profileListPresenter.getTotalProfileCount() shouldBe searchProfileModelEmptyResult.aceSearchProfile.count
-            }
-
-            Then("Total has next page should be ${searchProfileModelEmptyResult.aceSearchProfile.hasNext}") {
-                profileListPresenter.getHasNextPage() shouldBe searchProfileModelEmptyResult.aceSearchProfile.hasNext
-            }
-
-            Then("Verify view interaction for empty search") {
-                verifyOrder {
-                    profileListView.trackEmptySearchProfile()
-                    profileListView.hideLoading()
-                    profileListView.clearVisitableList()
-                    profileListView.renderVisitableList(capture(slotVisitableList))
-                }
-
-                confirmVerified(profileListView)
-            }
-
-            Then("Visitable List should contain empty search model") {
-                val visitableList = slotVisitableList.captured
-
-                visitableList.shouldOnlyHaveEmptyResult()
-            }
-        }
-
-        Scenario("No Result Empty Profile with Recommendation") {
-            val query = "dariand"
-            val profileListView by memoized<ProfileListSectionContract.View>()
-            val searchProfileUseCase by memoized<UseCase<SearchProfileModel>>()
-            val slotVisitableList = slot<List<Visitable<*>>>()
-            val slotRecommendationProfileTrackingObjectList = slot<List<Any>>()
-
-            lateinit var profileListPresenter: ProfileListPresenter
-
-            Given("Profile list presenter") {
-                profileListPresenter = createProfileListPresenter()
-            }
-
-            Given("Search Profile API will return empty result with recommendation") {
-                every { searchProfileUseCase.execute(any(), any()) }.answers {
-                    secondArg<Subscriber<SearchProfileModel>>().onNext(searchProfileModelEmptyResultWithRecommendation)
-                }
-            }
-
-            When("Request Profile List Data") {
-                profileListPresenter.requestProfileListData(query, 1)
-            }
-
-            Then("Total profile count should be ${searchProfileModelEmptyResultWithRecommendation.aceSearchProfile.count}") {
-                profileListPresenter.getTotalProfileCount() shouldBe searchProfileModelEmptyResultWithRecommendation.aceSearchProfile.count
-            }
-
-            Then("Total has next page should be ${searchProfileModelEmptyResult.aceSearchProfile.hasNext}") {
-                profileListPresenter.getHasNextPage() shouldBe searchProfileModelEmptyResult.aceSearchProfile.hasNext
-            }
-
-            Then("Verify view interaction for empty search") {
-                verifyOrder {
-                    profileListView.trackEmptySearchProfile()
-                    profileListView.trackImpressionRecommendationProfile(capture(slotRecommendationProfileTrackingObjectList))
-                    profileListView.hideLoading()
-                    profileListView.clearVisitableList()
-                    profileListView.renderVisitableList(capture(slotVisitableList))
-                }
-
-                confirmVerified(profileListView)
-            }
-
-            Then("Visitable List should contain empty search model and recommendation profiles") {
-                val visitableList = slotVisitableList.captured
-
-                visitableList.shouldHaveEmptyResultAndRecommendationProfiles()
-            }
-
-            Then("Recommendation profile tracking object list size should be equal to recommendation profile size") {
-                val recommendationProfileTrackingObjectList = slotRecommendationProfileTrackingObjectList.captured
-
-                recommendationProfileTrackingObjectList.size shouldBe
-                        searchProfileModelEmptyResultWithRecommendation.aceSearchProfile.topProfile.size
-            }
+    private fun `Given Search Profile API will return SearchProfileModel`(searchProfileModel: SearchProfileModel) {
+        every { searchProfileUseCase.execute(any(), any()) }.answers {
+            secondArg<Subscriber<SearchProfileModel>>().onNext(searchProfileModel)
         }
     }
-})
+
+    private fun `When Request Profile List Data`() {
+        profileListPresenter.requestProfileListData(query, 1)
+    }
+
+    private fun `Then verify total profile count`(expectedTotalProfileCount: Int) {
+        profileListPresenter.getTotalProfileCount() shouldBe expectedTotalProfileCount
+    }
+
+    private fun `Then verify has next page`(expectedHasNextPage: Boolean) {
+        profileListPresenter.getHasNextPage() shouldBe expectedHasNextPage
+    }
+
+    private fun `Then verify view interaction for empty search`() {
+        verifyOrder {
+            profileListView.trackEmptySearchProfile()
+            profileListView.hideLoading()
+            profileListView.clearVisitableList()
+            profileListView.renderVisitableList(capture(slotVisitableList))
+        }
+
+        confirmVerified(profileListView)
+    }
+
+    private fun `Then verify Visitable List should contain empty search model`() {
+        val visitableList = slotVisitableList.captured
+
+        visitableList.shouldOnlyHaveEmptyResult()
+    }
+
+    @Test
+    fun `No Result Empty Profile with Recommendation`() {
+        val slotRecommendationProfileTrackingObjectList = slot<List<Any>>()
+
+        `Given Search Profile API will return SearchProfileModel`(searchProfileModelEmptyResultWithRecommendation)
+
+        `When Request Profile List Data`()
+
+        `Then verify total profile count`(searchProfileModelEmptyResultWithRecommendation.aceSearchProfile.count)
+        `Then verify has next page`(searchProfileModelEmptyResult.aceSearchProfile.hasNext)
+        `Then verify view interaction for empty search with recommendation`(slotRecommendationProfileTrackingObjectList)
+        `Then Visitable List should contain empty search model and recommendation profiles`()
+        `Then Recommendation profile tracking object list size should be equal to recommendation profile size`(slotRecommendationProfileTrackingObjectList)
+    }
+
+    private fun `Then verify view interaction for empty search with recommendation`(slotRecommendationProfileTrackingObjectList: CapturingSlot<List<Any>>) {
+        verifyOrder {
+            profileListView.trackEmptySearchProfile()
+            profileListView.trackImpressionRecommendationProfile(capture(slotRecommendationProfileTrackingObjectList))
+            profileListView.hideLoading()
+            profileListView.clearVisitableList()
+            profileListView.renderVisitableList(capture(slotVisitableList))
+        }
+
+        confirmVerified(profileListView)
+    }
+
+    private fun `Then Visitable List should contain empty search model and recommendation profiles`() {
+        val visitableList = slotVisitableList.captured
+
+        visitableList.shouldHaveEmptyResultAndRecommendationProfiles()
+    }
+
+    private fun `Then Recommendation profile tracking object list size should be equal to recommendation profile size`(slotRecommendationProfileTrackingObjectList: CapturingSlot<List<Any>>) {
+        val recommendationProfileTrackingObjectList = slotRecommendationProfileTrackingObjectList.captured
+
+        recommendationProfileTrackingObjectList.size shouldBe
+                searchProfileModelEmptyResultWithRecommendation.aceSearchProfile.topProfile.size
+    }
+}
