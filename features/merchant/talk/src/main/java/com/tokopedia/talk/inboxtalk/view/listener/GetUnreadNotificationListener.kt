1dbe191a1a82e1240a8ed853c185930e4d4c8c2f
leonard2907 <leonard.gunawan@tokopedia.com>
2020-04-09 11:40:33 +0700

    Rename old package to talk_old

--- features/merchant/talk/src/main/java/com/tokopedia/talk/inboxtalk/view/listener/GetUnreadNotificationListener.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.talk.inboxtalk.view.listener
-
-/**
- * @author by nisie on 9/7/18.
- */
-interface GetUnreadNotificationListener {
-    fun onGetNotification(notifCount: Int, nav : String)
-}
---
+++ features/merchant/talk/src/main/java/com/tokopedia/talk_old/addtalk/di/AddTalkModule.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.talk_old.addtalk.di
+
+import dagger.Module
+
+/**
+ * @author : Steven 17/09/18
+ */
+@AddTalkScope
+@Module
+class AddTalkModule {
+}
