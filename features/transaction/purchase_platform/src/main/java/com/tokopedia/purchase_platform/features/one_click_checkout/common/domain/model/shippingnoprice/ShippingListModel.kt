c03251b7e8caf44388536fbbf0320531a34db50f
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-04-09 09:20:38 +0700

    fix codes

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/model/shippingnoprice/ShippingListModel.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/model/shippingnoprice/ShippingListModel.kt
@@ -1,9 +1,7 @@
 package com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.shippingnoprice
 
 import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.shippingprice.ServicesItem
-import com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.shippingprice.ServicesItemModel
 
 data class ShippingListModel(
         var services : List<ServicesItem> = emptyList()
-//        var servicesPrice : List<ServicesItemModel> = emptyList()
 )
