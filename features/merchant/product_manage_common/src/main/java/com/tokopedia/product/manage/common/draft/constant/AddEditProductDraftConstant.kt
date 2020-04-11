414ea3e32a6b980078b7dea6c19c3ac87e3ada65
elscione <yusufhendrawan06@gmail.com>
2020-04-11 16:35:40 +0700

    Revert

--- features/merchant/product_manage_common/src/main/java/com/tokopedia/product/manage/common/draft/constant/AddEditProductDraftConstant.kt
+++ features/merchant/product_manage_common/src/main/java/com/tokopedia/product/manage/common/draft/constant/AddEditProductDraftConstant.kt
@@ -1,9 +1,9 @@
 package com.tokopedia.product.manage.common.draft.constant
 
 object AddEditProductDraftConstant {
-    const val DB_NAME = "tkpd_seller_draft.db"
-    const val DB_OLD_VERSION = 6
-    const val DB_VERSION = 7
+    const val DB_NAME = "tkpd_seller.db"
+    const val DB_OLD_VERSION = 7
+    const val DB_VERSION = 8
     const val DB_TABLE = "ProductDraftDataBase"
     const val DB_AUTOGENERATE = true
     const val DB_COLUMN_DATA = "data"
