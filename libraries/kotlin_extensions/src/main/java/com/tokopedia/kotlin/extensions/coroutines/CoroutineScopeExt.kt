84974fa6a0528d9e08d44ece662c10103f120a6e
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-13 01:26:04 +0700

    add asyncCatchError

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/coroutines/CoroutineScopeExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/coroutines/CoroutineScopeExt.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.kotlin.extensions.coroutines
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
@@ -18,3 +20,19 @@ fun CoroutineScope.launchCatchError(context: CoroutineContext = coroutineContext
             }
         }
     }
+
+fun <T> CoroutineScope.asyncCatchError(context: CoroutineContext = coroutineContext,
+                                       block: suspend CoroutineScope.() -> T,
+                                       onError: suspend (Throwable) -> T): Deferred<T?> {
+    return async(context) {
+        try {
+            block()
+        } catch (e: Exception) {
+            try {
+                onError(e)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+}
