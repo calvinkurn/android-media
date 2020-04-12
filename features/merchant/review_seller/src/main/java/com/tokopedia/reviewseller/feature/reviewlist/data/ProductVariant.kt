e82ef49a4e5022fde2b9c0e9c507cc3d7ba25c85
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-12 14:28:34 +0700

    init response data

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/data/ProductVariant.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.reviewseller.feature.reviewlist.data
-
-import com.google.gson.annotations.SerializedName
-
-data class ProductVariant(
-        @SerializedName("variantID") val variantID : Int,
-        @SerializedName("variantName") val variantName : String
-)
--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/data/ReviewListBaseResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.reviewseller.feature.reviewlist.data
-
-import com.google.gson.annotations.SerializedName
-
-data class ReviewListBaseResponse(
-        @SerializedName("data") val ratingData: RatingData
-)
