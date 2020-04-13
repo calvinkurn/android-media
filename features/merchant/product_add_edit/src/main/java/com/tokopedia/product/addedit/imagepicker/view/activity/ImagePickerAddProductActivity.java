20db2b1dcafe367998f3767d39bed412deee9640
saidfaisal <said.faisal@tokopedia.com>
2020-04-13 13:35:15 +0700

    change oncreate placement

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/imagepicker/view/activity/ImagePickerAddProductActivity.java
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/imagepicker/view/activity/ImagePickerAddProductActivity.java
@@ -16,6 +16,13 @@ public class ImagePickerAddProductActivity extends ImagePickerActivity {
     boolean isEditProduct = false;
     public static final String IS_EDIT = "is_edit";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        isEditProduct = getIntent().getBooleanExtra(IS_EDIT, false);
+        super.onCreate(savedInstanceState);
+        userSession = new UserSession(getContext());
+    }
+
     public static Intent getIntent(Context context, ImagePickerBuilder imagePickerBuilder, boolean isEditProduct) {
         Intent intent = new Intent(context, ImagePickerAddProductActivity.class);
         // https://stackoverflow.com/questions/28589509/android-e-parcel-class-not-found-when-unmarshalling-only-on-samsung-tab3
@@ -24,12 +31,6 @@ public class ImagePickerAddProductActivity extends ImagePickerActivity {
         intent.putExtra(EXTRA_IMAGE_PICKER_BUILDER, bundle);
         intent.putExtra(IS_EDIT, isEditProduct);
         return intent;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        userSession = new UserSession(getContext());
     }
 
     @Override
