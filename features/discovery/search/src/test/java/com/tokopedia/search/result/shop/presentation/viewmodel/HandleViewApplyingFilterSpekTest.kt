5759e545c8b3457093a96fad4db8f65037937248
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-13 09:52:04 +0700

    Create search shop apply filter spek to junit

--- features/discovery/search/src/test/java/com/tokopedia/search/result/shop/presentation/viewmodel/HandleViewApplyingFilterSpekTest.kt
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/shop/presentation/viewmodel/HandleViewApplyingFilterSpekTest.kt
@@ -14,7 +14,8 @@ import com.tokopedia.usecase.coroutines.UseCase
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
-internal class HandleViewApplyingFilterTest: Spek({
+@Deprecated("Migrated to JUnit")
+internal class HandleViewApplyingFilterSpekTest: Spek({
 
     InstantTaskExecutorRuleSpek(this)
 
