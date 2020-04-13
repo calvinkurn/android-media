7de23bddaade7faa5a7aa0684ba911cda3161315
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-14 01:25:41 +0700

    refactor module

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/di/module/ReviewSellerViewModelModule.kt
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/di/module/ReviewSellerViewModelModule.kt
@@ -1,20 +1,20 @@
-package com.tokopedia.reviewseller.common.di.module
+package com.tokopedia.reviewseller.feature.reviewlist.di.module
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
-import com.tokopedia.reviewseller.common.di.scope.ReviewSellerScope
+import com.tokopedia.reviewseller.feature.reviewlist.di.scope.ReviewProductListScope
 import com.tokopedia.reviewseller.feature.reviewlist.view.viewmodel.ReviewSellerViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
-@ReviewSellerScope
 @Module
+@ReviewProductListScope
 abstract class ReviewSellerViewModelModule {
 
-    @ReviewSellerScope
+    @ReviewProductListScope
     @Binds
     internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
 
@@ -22,6 +22,4 @@ abstract class ReviewSellerViewModelModule {
     @IntoMap
     @ViewModelKey(ReviewSellerViewModel::class)
     internal abstract fun reviewSellerViewModel(viewModel: ReviewSellerViewModel): ViewModel
-
-
 }
