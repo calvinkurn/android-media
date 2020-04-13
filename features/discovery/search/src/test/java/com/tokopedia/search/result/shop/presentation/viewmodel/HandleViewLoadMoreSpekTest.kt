2decb0cd33c62cbf0bc8cea0d991239fc8c865c7
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-13 12:49:57 +0700

    Search shop load more spek to junit

--- features/discovery/search/src/test/java/com/tokopedia/search/result/shop/presentation/viewmodel/HandleViewLoadMoreSpekTest.kt
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/shop/presentation/viewmodel/HandleViewLoadMoreSpekTest.kt
@@ -14,7 +14,8 @@ import com.tokopedia.usecase.coroutines.UseCase
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.gherkin.Feature
 
-internal class HandleViewLoadMoreTest: Spek({
+@Deprecated("Migrated to JUnit")
+internal class HandleViewLoadMoreSpekTest: Spek({
 
     InstantTaskExecutorRuleSpek(this)
 
