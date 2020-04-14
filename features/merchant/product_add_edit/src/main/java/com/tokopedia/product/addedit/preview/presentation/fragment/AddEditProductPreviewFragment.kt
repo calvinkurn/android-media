0fbeba83816ff8344e9e5cfc987ea237a034851f
Faisal Ramdani <faisal.ramdani@tokopedia.com>
2020-04-15 00:37:29 +0700

    Finish activity preview when submit duplicate

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
@@ -288,6 +288,7 @@ class AddEditProductPreviewFragment : BaseDaggerFragment(), ProductPhotoViewHold
                                 shipmentInputModel = productInputModel.shipmentInputModel,
                                 variantInputModel = productInputModel.variantInputModel
                         )
+                        activity?.finish()
                     }
                 }
             } else if (viewModel.isEditing.value == true) {
