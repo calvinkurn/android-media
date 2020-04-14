8501fbb4b0d66714f2eb19762c55bbb49add9390
Henry Priyono <henry.priyono@tokopedia.com>
2020-04-14 19:48:28 +0700

    Fix Observable Flow

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/data/source/TopAdsVerificationNetworkSource.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/data/source/TopAdsVerificationNetworkSource.kt
@@ -15,19 +15,21 @@ internal constructor(context: Context) {
     }
 
     fun appendVerificationStatus(inputList: List<TopAdsLogDB>): Observable<List<TopAdsLogDB>> {
-        for (input in inputList) {
-            if (input.eventStatus == STATUS_PENDING && System.currentTimeMillis() - input.timestamp > 10000) {
-                input.eventStatus = STATUS_DATA_NOT_FOUND
-                updateItem(input)
-            } else if (input.eventStatus == STATUS_DATA_NOT_FOUND) {
-                input.eventStatus = STATUS_NOT_MATCH
-                updateItem(input)
-            }  else if (input.eventStatus == STATUS_NOT_MATCH) {
-                input.eventStatus = STATUS_MATCH
-                updateItem(input)
+        return Observable.create { subscriber ->
+            for (input in inputList) {
+                if (input.eventStatus == STATUS_PENDING && System.currentTimeMillis() - input.timestamp > 10000) {
+                    input.eventStatus = STATUS_DATA_NOT_FOUND
+                    updateItem(input)
+                } else if (input.eventStatus == STATUS_DATA_NOT_FOUND) {
+                    input.eventStatus = STATUS_NOT_MATCH
+                    updateItem(input)
+                }  else if (input.eventStatus == STATUS_NOT_MATCH) {
+                    input.eventStatus = STATUS_MATCH
+                    updateItem(input)
+                }
             }
+            subscriber.onNext(inputList)
         }
-        return Observable.just(inputList)
     }
 
     fun updateItem(item: TopAdsLogDB) {
