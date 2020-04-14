0b63f6f3715ca9dee67a4b37c44ceb4dd7cb0f84
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-04-14 19:04:49 +0700

    add fade in/fade out on swipe header text onboarding

--- features/merchant/seller_onboarding/src/main/java/com/tokopedia/selleronboarding/utils/OnboardingLayoutManager.kt
+++ features/merchant/seller_onboarding/src/main/java/com/tokopedia/selleronboarding/utils/OnboardingLayoutManager.kt
@@ -17,18 +17,32 @@ class OnboardingLayoutManager(
         reverseLayout: Boolean
 ) : LinearLayoutManager(context, orientation, reverseLayout) {
 
+    private var currentItemPos = 0
+
     override fun scrollHorizontallyBy(dx: Int, recycler: RecyclerView.Recycler?, state: RecyclerView.State?): Int {
-        for (i in 0..childCount) {
-            updateChildrenAlpha(getChildAt(i))
+        println("dx : $dx")
+        println("isMeasuring : ${state?.isMeasuring}")
+        println("currentItemPos : $currentItemPos")
+        val isScrollLeft = dx < 0
+        println("isScrollLeft : $isScrollLeft")
+        if (isScrollLeft) { //scroll to left
+            setLeftAlpha(getChildAt(currentItemPos.minus(1)))
+            setRightAlpha(getChildAt(currentItemPos))
+        } else { //scroll to right
+            setRightAlpha(getChildAt(currentItemPos.plus(1)))
+            setLeftAlpha(getChildAt(currentItemPos))
         }
         return super.scrollHorizontallyBy(dx, recycler, state)
     }
 
     override fun onScrollStateChanged(state: Int) {
         super.onScrollStateChanged(state)
+        if (state == RecyclerView.SCROLL_STATE_IDLE) {
+            currentItemPos = findFirstCompletelyVisibleItemPosition()
+        }
     }
 
-    private fun updateChildrenAlpha(view: View?) = view?.let { v ->
+    private fun setRightAlpha(view: View?) = view?.let { v ->
         val maxDist = (v.width / 3).toFloat()
         val right = getDecoratedRight(v)
         val left = getDecoratedLeft(v)
@@ -39,11 +53,11 @@ class OnboardingLayoutManager(
         mView.alpha = ((abs(center - childCenter) - maxDist) / maxDist)
     }
 
-    private fun updateChildrenAlpha1(view: View?) = view?.let { v ->
+    private fun setLeftAlpha(view: View?) = view?.let { v ->
         val maxDist = (v.width / 3).toFloat()
         val right = getDecoratedRight(v)
         val left = getDecoratedLeft(v)
-        val childCenter = right + (left - right) / 2
+        val childCenter = right + (right - left) / 2
         val center = width / 2
 
         val mView = v.tvHeaderText ?: return@let
