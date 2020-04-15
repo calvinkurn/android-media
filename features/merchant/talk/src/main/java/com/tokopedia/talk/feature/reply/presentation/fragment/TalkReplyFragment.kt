3d9874110e6e0be2d5ef591e226fa18b5cec37a6
leonard2907 <leonard.gunawan@tokopedia.com>
2020-04-15 17:48:10 +0700

    Add methods to show and hide error and loading in reply fragment

--- features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reply/presentation/fragment/TalkReplyFragment.kt
+++ features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reply/presentation/fragment/TalkReplyFragment.kt
@@ -6,11 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
 import com.tokopedia.talk.common.di.TalkComponent
 import com.tokopedia.talk.feature.reply.di.DaggerTalkReplyComponent
 import com.tokopedia.talk.feature.reply.di.TalkReplyComponent
 import com.tokopedia.talk.feature.report.presentation.fragment.TalkReportFragment
 import com.tokopedia.talk_old.R
+import kotlinx.android.synthetic.main.fragment_talk_reading.*
+import kotlinx.android.synthetic.main.partial_talk_connection_error.view.*
 
 class TalkReplyFragment : BaseDaggerFragment(), HasComponent<TalkReplyComponent> {
 
@@ -43,6 +47,28 @@ class TalkReplyFragment : BaseDaggerFragment(), HasComponent<TalkReplyComponent>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_talk_reply, container, false)
+    }
+
+    private fun showPageLoading() {
+        pageLoading.visibility = View.VISIBLE
+    }
+
+    private fun hidePageLoading() {
+        pageLoading.visibility = View.GONE
+    }
+
+    private fun showPageError() {
+        pageError.visibility = View.VISIBLE
+        pageError.readingConnectionErrorRetryButton.setOnClickListener {
+
+        }
+        pageError.readingConnectionErrorGoToSettingsButton.setOnClickListener {
+            RouteManager.route(context, ApplinkConstInternalGlobal.GENERAL_SETTING)
+        }
+    }
+
+    private fun hidePageError() {
+        pageError.visibility = View.GONE
     }
 
     private fun initView() {
