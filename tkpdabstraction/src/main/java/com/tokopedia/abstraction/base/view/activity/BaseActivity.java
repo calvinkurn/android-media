9478e5f145f6854a06e6f229c48a4c8ba2d88e41
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-04-15 19:21:52 +0700

    Set bottom sheet view (2) change peak size and add margin bottom in button

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/activity/BaseActivity.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/activity/BaseActivity.java
@@ -165,7 +165,7 @@ public abstract class BaseActivity extends AppCompatActivity implements
 //                    }
 //                });
         BottomSheetUnify bottomSheetUnify = new BottomSheetUnify();
-        bottomSheetUnify.setCustomPeekHeight(700);
+        bottomSheetUnify.setCustomPeekHeight(1000);
         bottomSheetUnify.setOverlayClickDismiss(false);
 
         View childView = View.inflate(this, R.layout.error_unauthorized, null);
