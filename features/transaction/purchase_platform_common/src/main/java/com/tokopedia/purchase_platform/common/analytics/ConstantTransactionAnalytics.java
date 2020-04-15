c654a2e54409a4b6df7898f841ecf1b6ced663b1
hansenpw <hansen.wijaya@tokopedia.com>
2020-04-15 19:25:37 +0700

    remove word promo in event action

--- features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/analytics/ConstantTransactionAnalytics.java
+++ features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/analytics/ConstantTransactionAnalytics.java
@@ -437,8 +437,8 @@ public interface ConstantTransactionAnalytics {
         String GUNAKAN_PILIHAN_INI_FROM_GANTI_PILIHAN_OSP  = "preference -  click gunakan pilihan ini from ganti pilihan";
         String CLICK_BACK_FROM_OSP = "click back in order summary page";
         String CLICK_BUTTON_INFO_ON_OSP = "click button info on order summary page";
-        String CLICK_PROMO_SECTION_APPLIED_OSP = "promo - click promo section with promo applied";
-        String CLICK_PROMO_SECTION_NOT_APPLIED_OSP = "promo - click promo section with promo not applied";
+        String CLICK_PROMO_SECTION_APPLIED_OSP = "click promo section with promo applied";
+        String CLICK_PROMO_SECTION_NOT_APPLIED_OSP = "click promo section with promo not applied";
         String CLICK_LANJUT_BAYAR_PROMO_ERROR_OSP = "promo - click lanjut bayar on bottom sheet promo error";
         String CLICK_PILIH_PROMO_LAIN_PROMO_ERROR_OSP = "promo - click pilih promo lain on bottom sheet promo error";
 
