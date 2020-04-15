9ca3188a3b17a1e4fdc8187d8d3592a3564ec408
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-04-15 08:59:55 +0700

    cleanup codes

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/common/domain/model/shipping/ShippingDataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.purchase_platform.features.one_click_checkout.common.domain.model.shipping
-
-class ShippingDataModel1 (
-//    val promoStacking: PromoStacking? = null,
-    var id: String? = null,
-    var services: ArrayList<ServicesItemModel?>? = null,
-    var type: String? = null,
-    var ratesId: Long? = null
-)
