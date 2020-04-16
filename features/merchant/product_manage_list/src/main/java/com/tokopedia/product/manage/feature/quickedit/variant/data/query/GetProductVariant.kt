6414c8ebc977adb6490a922d1050fe9ab43854d1
rezagama <reza.hidayat@tokopedia.com>
2020-04-16 19:25:29 +0700

    Add get product variant query

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/data/query/GetProductVariant.kt
@@ -0,0 +1,35 @@
+package com.tokopedia.product.manage.feature.quickedit.variant.data.query
+
+internal object GetProductVariant {
+
+    val QUERY = """
+        query getProductV3(${'$'}productID:String!, ${'$'}options:OptionV3!){
+          getProductV3(productID:${'$'}productID, options:${'$'}options){
+            variant{
+              products{
+                productID
+                status
+                combination
+                isPrimary
+                price
+                sku
+                stock
+                pictures {
+                  picID
+                  description
+                  filePath
+                  fileName
+                  width
+                  height
+                  isFromIG
+                  urlOriginal
+                  urlThumbnail
+                  url300
+                  status
+                }
+              }
+            }
+          }
+        }
+    """.trimIndent()
+}
