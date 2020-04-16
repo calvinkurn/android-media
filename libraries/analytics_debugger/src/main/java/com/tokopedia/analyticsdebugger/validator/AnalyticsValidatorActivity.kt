3bb6148387b5c5c48c5d06f2c1656c3d29d464a1
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-04-16 14:59:46 +0700

    Parsing json query from asset

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/AnalyticsValidatorActivity.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/AnalyticsValidatorActivity.kt
@@ -5,10 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.tokopedia.analyticsdebugger.R
-import com.tokopedia.analyticsdebugger.debugger.ui.fragment.AnalyticsDebuggerFragment
+import timber.log.Timber
 
-class AnalyticsValidatorActivity  : AppCompatActivity() {
+class AnalyticsValidatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +18,13 @@ class AnalyticsValidatorActivity  : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         toolbar.subtitle = "Tokopedia Client Analytics Validator"
+        val jsonTest = Utils.getJsonDataFromAsset(this, "add_address_cvr.json")
+        Timber.d("Json Query \n %s", jsonTest)
+
+        val jsonType = object : TypeToken<Map<String, Any>>() {}.type
+        val testQuery = Gson().fromJson<Map<String, Any>>(jsonTest, jsonType)
+        Timber.d("Test Query Map \n %s", testQuery)
+
 //        if (savedInstanceState == null) {
 //            supportFragmentManager.beginTransaction()
 //                    .add(R.id.container, AnalyticsDebuggerFragment.newInstance(), AnalyticsDebuggerFragment.TAG)
