53e683ba8ea6683e08a5636b677b3283b2b10c9b
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-16 17:39:07 +0700

    Remove unnecessary imports

--- features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridTest.kt
+++ features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridTest.kt
@@ -1,10 +1,8 @@
 package com.tokopedia.carouselproductcard.test
 
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnitRunner
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 // Uncomment to enable, also uncomment build.gradle line 24, and 59-61
 //internal class CarouselProductCardGridTest {
