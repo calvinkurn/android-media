bb5f54a056bcd9048c29f748a70fd4f860175af0
isfaaghyth <isfaaghyth@gmail.com>
2020-04-17 05:27:11 +0700

    settingnotif: make seller section inheritance of base setting

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/SellerSection.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/SellerSection.kt
@@ -4,9 +4,8 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.settingnotif.usersetting.view.adapter.factory.SettingFieldTypeFactory
 
 class SellerSection(
-        val icon: Int = 0,
-        val title: String = ""
-): Visitable<SettingFieldTypeFactory> {
+        var title: String = ""
+) : BaseSetting(), Visitable<SettingFieldTypeFactory> {
 
     override fun type(typeFactory: SettingFieldTypeFactory): Int {
         return typeFactory.type(this)
