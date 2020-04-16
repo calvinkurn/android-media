0901394fe31b929e66eae58d85a35b4bf9838ece
darianthedytoped <darian.thedy@tokopedia.com>
2020-04-16 17:33:04 +0700

    Disable product card instrumentation test

--- features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridTest.kt
+++ features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridTest.kt
@@ -1,15 +1,18 @@
 package com.tokopedia.carouselproductcard.test
 
 import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnitRunner
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-internal class CarouselProductCardGridTest {
-
-    @Rule
-    @JvmField
-    val activityTestRule = ActivityTestRule<CarouselProductCardGridActivityTest>(CarouselProductCardGridActivityTest::class.java)
-
-    @Test
-    fun test() { }
-}
+// Uncomment to enable, also uncomment build.gradle line 24, and 59-61
+//internal class CarouselProductCardGridTest {
+//
+//    @Rule
+//    @JvmField
+//    val activityTestRule = ActivityTestRule<CarouselProductCardGridActivityTest>(CarouselProductCardGridActivityTest::class.java)
+//
+//    @Test
+//    fun test() { }
+//}
