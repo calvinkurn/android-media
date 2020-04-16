34eb8506d1a4658315e9d71e726f37c59ace3ebf
rezagama <reza.hidayat@tokopedia.com>
2020-04-16 19:25:49 +0700

    Add get product variant use case

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/domain/GetProductVariantUseCase.kt
@@ -0,0 +1,44 @@
+package com.tokopedia.product.manage.feature.quickedit.variant.domain
+
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.product.manage.feature.quickedit.variant.data.model.GetProductVariantResponse
+import com.tokopedia.product.manage.feature.quickedit.variant.data.query.GetProductVariant
+import com.tokopedia.usecase.RequestParams
+import javax.inject.Inject
+
+class GetProductVariantUseCase @Inject constructor(
+    graphqlRepository: GraphqlRepository
+): GraphqlUseCase<GetProductVariantResponse>(graphqlRepository) {
+
+    companion object {
+        private const val PARAM_PRODUCT_ID = "productID"
+        private const val PARAM_OPTIONS = "options"
+        private const val PARAM_VARIANT = "variant"
+
+        fun createRequestParams(productId: String): RequestParams {
+            val optionsParam = RequestParams()
+            optionsParam.putBoolean(PARAM_VARIANT, true)
+
+            return RequestParams().apply {
+                putString(PARAM_PRODUCT_ID, productId)
+                putObject(PARAM_OPTIONS, optionsParam.parameters)
+            }
+        }
+    }
+
+    init {
+        val cacheStrategy = GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build()
+        setCacheStrategy(cacheStrategy)
+
+        setGraphqlQuery(GetProductVariant.QUERY)
+        setTypeClass(GetProductVariantResponse::class.java)
+    }
+
+    suspend fun execute(requestParams: RequestParams): GetProductVariantResponse {
+        setRequestParams(requestParams.parameters)
+        return executeOnBackground()
+    }
+}
