3b9d1064363c3c843fe7e93142b8a15a581a82d5
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2020-04-16 18:28:36 +0700

    add replace www

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFFallbackGenerator.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFFallbackGenerator.kt
@@ -11,6 +11,7 @@ object DeeplinkDFFallbackGenerator {
         if (!fallbackUrl.endsWith("/")) {
             fallbackUrl += "/"
         }
+        fallbackUrl.replace("www.", "m.")
         fallbackUrl += "report/"
         return fallbackUrl
     }
