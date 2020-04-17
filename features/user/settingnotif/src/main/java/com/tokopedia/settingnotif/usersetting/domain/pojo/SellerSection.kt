429b99cf3696df96db5129b5768a219d624c9a61
isfaaghyth <isfaaghyth@gmail.com>
2020-04-18 03:02:18 +0700

    settingnotif: clean up seller item section with adding a item creator

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/SellerSection.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/pojo/SellerSection.kt
@@ -1,14 +1,25 @@
 package com.tokopedia.settingnotif.usersetting.domain.pojo
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.settingnotif.R
 import com.tokopedia.settingnotif.usersetting.view.adapter.factory.SettingFieldTypeFactory
 
-class SellerSection(
-        var title: String = ""
+data class SellerSection(
+        var title: Int = 0,
+        var resourceIcon: Int = 0
 ) : BaseSetting(), Visitable<SettingFieldTypeFactory> {
 
     override fun type(typeFactory: SettingFieldTypeFactory): Int {
         return typeFactory.type(this)
+    }
+
+    companion object {
+        fun createSellerItem(): SellerSection {
+            return SellerSection().apply {
+                this.title = R.string.settingnotif_settings_seller_title
+                this.resourceIcon = R.drawable.ic_notifsetting_seller
+            }
+        }
     }
 
 }
