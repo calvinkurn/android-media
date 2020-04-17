73f586acf325223582756ed4f71bd2067044b5bf
rezagama <reza.hidayat@tokopedia.com>
2020-04-17 12:26:36 +0700

    Add selections in variant data model

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/data/model/Variant.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.product.manage.feature.quickedit.variant.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Variant (
+    @SerializedName("products")
+    val products: List<Product>,
+    @SerializedName("selections")
+    val selections: List<Selection>
+)
