68adb1ebc40cc3a834e2018cab3efe05d90ca365
isfaaghyth <isfaaghyth@gmail.com>
2020-04-18 04:26:00 +0700

    settingnotif: handled notification permission of email

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
@@ -1,14 +1,16 @@
 package com.tokopedia.settingnotif.usersetting.view.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProviders
+import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.config.GlobalConfig
 import com.tokopedia.settingnotif.R
-import com.tokopedia.settingnotif.usersetting.domain.pojo.SettingSections
-import com.tokopedia.settingnotif.usersetting.view.adapter.factory.VisitableSettings
-import com.tokopedia.settingnotif.usersetting.view.dataview.ChangeItemDataView.changeEmail
-import com.tokopedia.settingnotif.usersetting.view.dataview.Data
+import com.tokopedia.settingnotif.usersetting.domain.pojo.ParentSetting
 import com.tokopedia.settingnotif.usersetting.view.dataview.NotificationActivationDataView.activationEmail
 import com.tokopedia.settingnotif.usersetting.view.dataview.UserSettingViewModel
 import com.tokopedia.settingnotif.usersetting.view.fragment.base.SettingFieldFragment
+import com.tokopedia.settingnotif.usersetting.view.viewmodel.SettingStateViewModel
 
 class EmailFieldFragment: SettingFieldFragment() {
 
@@ -16,31 +18,63 @@ class EmailFieldFragment: SettingFieldFragment() {
         const val BUYING_TRANSACTION_SECTION_TITLE = "Transaksi Pembelian"
     }
 
+    private lateinit var viewModel: SettingStateViewModel
+
+    private val settingStates by lazy(LazyThreadSafetyMode.NONE) {
+        viewModel.getSettingStates()
+    }
+
     override fun getGqlRawQuery(): Int {
         return R.raw.query_email_setting
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders
+                .of(this, viewModelFactory)
+                .get(SettingStateViewModel::class.java)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (settingStates.isNotEmpty()) {
+            emailValidation()
+        }
+    }
+
     override fun onSuccessGetUserSetting(data: UserSettingViewModel) {
-        val pinnedData = arrayListOf<VisitableSettings>()
-        pinnedData.add(if (userSession.email.isNotEmpty()) {
-            /*
-            * showing change email card
-            * is user has the email
-            * */
-            changeEmail(userSession.email)
-        } else {
-            /*
-            * showing pinned message to
-            * instruction to add a new email
-            * */
-            activationEmail()
-        })
         if (GlobalConfig.isSellerApp()) {
             data.data = removeBuyerNotificationSetting(data.data)
         }
-        pinnedData.addAll(data.data)
-        data.data = pinnedData.toList()
+        viewModel.addPinnedEmailItems(data)
+        data.data = viewModel.getPinnedItems().toList()
         super.onSuccessGetUserSetting(data)
+    }
+
+    override fun renderList(list: MutableList<Visitable<*>>) {
+        super.renderList(list)
+
+        // save state of settings
+        viewModel.saveLastStateAll(list)
+
+        // view validation
+        emailValidation()
+    }
+
+    private fun emailValidation() {
+        permissionValidation(
+                userSession.email.isNotEmpty(),
+                activationEmail(),
+                settingStates
+        )
+    }
+
+    override fun updateSettingState(setting: ParentSetting?) {
+        viewModel.updateSettingState(setting)
     }
 
     override fun getScreenName() = getString(R.string.settingnotif_email)
