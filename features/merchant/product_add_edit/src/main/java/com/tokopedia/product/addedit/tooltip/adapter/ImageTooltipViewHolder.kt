71aa0353c9910ab180989cda2f954e5e2fe39d3a
milhamj <milham.jamaludin@gmail.com>
2020-04-17 19:28:53 +0700

    Fix sparkles :lipstick:

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/tooltip/adapter/ImageTooltipViewHolder.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/tooltip/adapter/ImageTooltipViewHolder.kt
@@ -2,6 +2,7 @@ package com.tokopedia.product.addedit.tooltip.adapter
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.loadImageRounded
 import com.tokopedia.product.addedit.R
 import com.tokopedia.product.addedit.tooltip.model.ImageTooltipModel
 import kotlinx.android.synthetic.main.item_tooltip_image.view.*
@@ -15,7 +16,7 @@ class ImageTooltipViewHolder(val view: View?,
     : AbstractViewHolder<ImageTooltipModel>(view) {
     override fun bind(element: ImageTooltipModel) {
         itemView.textViewContent.text = element.title
-        itemView.imageViewContent.urlSrc = element.imageUrl
+        itemView.imageViewContent.loadImageRounded(element.imageUrl)
         itemView.setOnClickListener { listener?.onClick(element, adapterPosition) }
     }
 
