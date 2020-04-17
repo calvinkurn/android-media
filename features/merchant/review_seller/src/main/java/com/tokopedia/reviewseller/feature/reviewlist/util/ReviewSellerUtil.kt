a0c5c217203d64caa5ed5ae47772bc816a72efca
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-18 00:03:01 +0700

    fix common & util

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/util/ReviewSellerUtil.kt
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/util/ReviewSellerUtil.kt
@@ -3,16 +3,15 @@ package com.tokopedia.reviewseller.feature.reviewlist.util
 import android.widget.ListView
 import com.tokopedia.unifycomponents.list.ListItemUnify
 import com.tokopedia.unifycomponents.list.ListUnify
-import java.util.*
 
-object ListUnifyUtil {
+object ReviewSellerUtil {
 
     fun setSelectedFilterOrSort(listUnify: ListUnify, position: Int) {
         with(listUnify) {
             val clickedItem = this.getItemAtPosition(position) as ListItemUnify
             when (choiceMode) {
                 ListView.CHOICE_MODE_SINGLE -> {
-                    getItems(this).filter { it.listRightRadiobtn?.isChecked ?: false }
+                    (listUnify.getValue("array") as ArrayList<ListItemUnify>).filter { it.listRightRadiobtn?.isChecked ?: false }
                             .filterNot { it == clickedItem }
                             .onEach { it.listRightRadiobtn?.isChecked = false }
 
@@ -21,11 +20,8 @@ object ListUnifyUtil {
             }
         }
     }
+}
 
-    private fun getItems(instance: ListUnify) = with(instance) {
-        this.javaClass.getDeclaredField("array").let {
-            it.isAccessible = true
-            it.get(this) as ArrayList<ListItemUnify>
-        }
-    }
+fun <T : Any> T.getValue(field: String): Any {
+    return this.javaClass.getDeclaredField(field).apply { isAccessible = true }.get(this)
 }
