ee78f731f8e2a57d4fdcb85ecdde0f9f0e6070d2
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-04-18 19:40:10 +0700

    Implement regex compare

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/core/Comparator.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/core/Comparator.kt
@@ -1,12 +1,11 @@
 package com.tokopedia.analyticsdebugger.validator.core
 
 import com.google.gson.internal.LinkedTreeMap
-import java.util.*
-import kotlin.collections.ArrayList
 
 internal fun List<Map<String, Any>>.compare(arr: List<Map<String, Any>>): Boolean {
-    for ((i, map) in this.withIndex()) {
-        if (!map.subSetOf(arr[i])) return false
+    if (this.size > 1) throw Exception("Tracker Query array should only contains one element")
+    for (map in arr) {
+        if (!this[0].subSetOf(map)) return false
     }
     return true
 }
@@ -15,14 +14,25 @@ internal fun Map<String, Any>.subSetOf(obj: Map<String, Any>): Boolean {
     for (entry in this) {
         if (!obj.containsKey(entry.key)) return false
         val objVal = obj[entry.key]
-        if (!entry.value.eqSimilar(objVal!!)) return false
+        if (!entry.value.eqSubSet(objVal!!)) return false
     }
     return true
 }
 
 @Suppress("UNCHECKED_CAST")
-private fun Any.eqSimilar(v: Any): Boolean = when {
+private fun Any.eqSubSet(v: Any): Boolean = when {
     this is LinkedTreeMap<*, *> && v is LinkedTreeMap<*, *> -> (this as Map<String, Any>).subSetOf(v as Map<String, Any>)
     this is ArrayList<*> && v is ArrayList<*> -> (this as List<Map<String, Any>>).compare(v as List<Map<String, Any>>)
+    this is String && v is String && this.contains("\\{\\{.*}}".toRegex()) -> regexCompare(this, v)
     else -> this == v
+}
+
+fun regexCompare(s: String, v: String): Boolean {
+    val syntax = Regex("\\{\\{(.*)}}")
+    val m = syntax.find(s)
+
+    val regex: Regex = m?.groupValues?.get(1)?.toRegex()
+            ?: throw Exception("Syntax not parsed")
+
+    return regex.matchEntire(v) != null
 }
