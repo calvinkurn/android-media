fe6b9e8eae903dc74789f54f13a8033b7a8d5e44
Rahul Lohra <rahul.lohra@tokopedia.com>
2020-04-19 16:31:59 +0530

    1. delete fakereponse activity provider from debug & release folder of developer options 2. Add comments on graphql library

--- features/common/developer_options/src/release/java/com/tokopedia/developer_options/fakeresponse/FakeResponseActivityProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.developer_options.fakeresponse
-
-import android.content.Context
-
-class FakeResponseActivityProvider{
-    fun startActivity(context: Context){
-        //Do nothing
-    }
-}
