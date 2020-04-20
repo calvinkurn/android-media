51bb7258d90a8920d343de795ad41133d24da4c5
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-21 00:38:59 +0700

    fix viewholder & model

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewdetail/view/adapter/viewholder/RatingBarDetailViewHolder.kt
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewdetail/view/adapter/viewholder/RatingBarDetailViewHolder.kt
@@ -1,13 +1,9 @@
 package com.tokopedia.reviewseller.feature.reviewdetail.view.adapter.viewholder
 
-import android.view.View
-import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.reviewseller.feature.reviewdetail.view.model.RatingBarUiModel
-
-class RatingBarDetailViewHolder(val view: View?) : AbstractViewHolder<RatingBarUiModel>(view) {
-
-    companion object {
-    }
-
-    override fun bind(element: RatingBarUiModel?) {}
-}
+//class RatingBarDetailViewHolder(val view: View?) : AbstractViewHolder<RatingBarUiModel>(view) {
+//
+//    companion object {
+//    }
+//
+//    override fun bind(element: RatingBarUiModel?) {}
+//}
