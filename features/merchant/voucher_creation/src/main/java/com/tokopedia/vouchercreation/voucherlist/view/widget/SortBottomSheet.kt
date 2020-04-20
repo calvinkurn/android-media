e83cc149710a388d18aa35fedceaf806c781d68d
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-04-20 21:47:50 +0800

    add class filter and sort bottom sheet

---
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/voucherlist/view/widget/SortBottomSheet.kt
@@ -0,0 +1,19 @@
+package com.tokopedia.vouchercreation.voucherlist.view.widget
+
+import androidx.fragment.app.FragmentManager
+import com.tokopedia.unifycomponents.BottomSheetUnify
+
+/**
+ * Created By @ilhamsuaib on 20/04/20
+ */
+
+class SortBottomSheet : BottomSheetUnify() {
+
+    companion object {
+        val TAG: String = SortBottomSheet::class.java.simpleName
+    }
+
+    fun show(fm: FragmentManager) {
+
+    }
+}
