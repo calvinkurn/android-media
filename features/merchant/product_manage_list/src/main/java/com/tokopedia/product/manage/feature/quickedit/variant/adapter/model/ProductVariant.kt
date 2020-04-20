55f282b18145ec38a171b66d5c02873310a92aba
rezagama <reza.hidayat@tokopedia.com>
2020-04-20 17:31:39 +0700

    Add new fields to ProductVariant view model

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/adapter/model/ProductVariant.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/adapter/model/ProductVariant.kt
@@ -2,11 +2,19 @@ package com.tokopedia.product.manage.feature.quickedit.variant.adapter.model
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.product.manage.feature.quickedit.variant.adapter.factory.ProductVariantAdapterFactory
+import com.tokopedia.product.manage.feature.quickedit.variant.data.model.Picture
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 
 data class ProductVariant(
     val id: String,
     val name: String,
-    val price: Int
+    val status: ProductStatus,
+    val combination: List<Int>,
+    val isPrimary: Boolean,
+    val price: Int,
+    val sku: String,
+    val stock: Int,
+    val pictures: List<Picture>
 ): Visitable<ProductVariantAdapterFactory> {
 
     override fun type(typeFactory: ProductVariantAdapterFactory): Int {
