73c3d822bde1593b3e054f1349647c2e1ca589ab
isfaaghyth <isfaaghyth@gmail.com>
2020-04-20 10:29:57 +0700

    settingnotif: used json converter from common network

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/util/ConverterExt.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/util/ConverterExt.kt
@@ -1,20 +1,9 @@
 package com.tokopedia.settingnotif.usersetting.util
 
-import com.google.gson.Gson
-
-val gson by lazy(LazyThreadSafetyMode.NONE) {
-    Gson()
-}
-
-inline fun <reified T> toJson(src: Any): String {
-    return gson.toJson(src, T::class.java)
-}
-
-inline fun <reified T> fromJson(src: String): T {
-    return gson.fromJson(src, T::class.java)
-}
+import com.tokopedia.graphql.CommonUtils.fromJson
+import com.tokopedia.graphql.CommonUtils.toJson
 
 inline fun <reified T> dataClone(src: Any): T {
-    val json = toJson<T>(src)
-    return fromJson(json)
+    val json = toJson(src)
+    return fromJson(json, T::class.java)
 }
