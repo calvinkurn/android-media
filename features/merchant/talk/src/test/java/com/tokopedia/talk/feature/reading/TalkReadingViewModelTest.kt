366d77bae0a08a244e2456ba9d04dda47e385545
leonard2907 <leonard.gunawan@tokopedia.com>
2020-04-20 18:24:13 +0700

    Add passed test cases for categories and sort options

--- features/merchant/talk/src/test/java/com/tokopedia/talk/feature/reading/TalkReadingViewModelTest.kt
+++ features/merchant/talk/src/test/java/com/tokopedia/talk/feature/reading/TalkReadingViewModelTest.kt
@@ -2,8 +2,11 @@ package com.tokopedia.talk.feature.reading
 
 import com.tokopedia.talk.feature.reading.data.model.DiscussionAggregateResponse
 import com.tokopedia.talk.feature.reading.data.model.DiscussionDataResponseWrapper
+import com.tokopedia.talk.feature.reading.data.model.SortOption
+import com.tokopedia.talk.feature.reading.data.model.TalkReadingCategory
 import io.mockk.coEvery
 import io.mockk.coVerify
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TalkReadingViewModelTest : TalkReadingViewModelTestFixture() {
@@ -30,27 +33,81 @@ class TalkReadingViewModelTest : TalkReadingViewModelTestFixture() {
 
     @Test
     fun `when updateCategories should set _filterCategories with expected value`() {
+        val categories = listOf(
+                TalkReadingCategory("stok", "Stok", false),
+                TalkReadingCategory("lainnya", "Lainnya", true) )
 
+        viewModel.updateCategories(categories)
+
+        verifyCategoriesEqual(categories)
     }
 
     @Test
     fun `when updateSelectedCategory should set element in _filterCategories with expected value`() {
+        val categories = listOf(
+                TalkReadingCategory("stok", "Stok", false),
+                TalkReadingCategory("lainnya", "Lainnya", true) )
 
+        viewModel.updateCategories(categories)
+        viewModel.updateSelectedCategory("Lainnya (5)", false)
+
+        val expectedCategory = TalkReadingCategory("lainnya", "Lainnya", false)
+        verifyCategoryEquals(expectedCategory)
     }
 
     @Test
     fun `when unselectAllCategories should update all elements in _filterCategories to unselected`() {
+        val categories = listOf(
+                TalkReadingCategory("stok", "Stok", false),
+                TalkReadingCategory("lainnya", "Lainnya", true) )
 
+        viewModel.updateCategories(categories)
+        viewModel.unselectAllCategories()
+
+        verifyAllCategoriesAreUnselected()
     }
 
     @Test
     fun `when updateSortOptions should update value in _sortOptions`() {
+        val sortOptions = listOf(SortOption.SortByInformativeness(), SortOption.SortByLike(), SortOption.SortByTime())
 
+        viewModel.updateSortOptions(sortOptions)
+
+        verifySortOptionsEquals(sortOptions)
     }
 
     @Test
     fun `when updateSelectedSort should update element in _sortOptions`() {
+        val sortOptions = listOf(SortOption.SortByInformativeness(), SortOption.SortByLike(), SortOption.SortByTime())
 
+        viewModel.updateSortOptions(sortOptions)
+        viewModel.updateSelectedSort(sortOptions.last())
+
+        val expectedSortOptions = listOf(SortOption.SortByInformativeness(isSelected = false), SortOption.SortByLike(), SortOption.SortByTime(isSelected = true))
+        verifySortOptionsEquals(expectedSortOptions)
+    }
+
+    private fun verifyCategoriesEqual(categories: List<TalkReadingCategory>) {
+        viewModel.filterCategories.value?.forEachIndexed { index, talkReadingCategory ->
+            assertEquals(categories[index], talkReadingCategory)
+        }
+    }
+
+    private fun verifyAllCategoriesAreUnselected() {
+        viewModel.filterCategories.value?.forEach {
+            assertEquals(false, it.isSelected)
+        }
+    }
+
+    private fun verifyCategoryEquals(expectedCategory: TalkReadingCategory) {
+        val actualCategory = viewModel.filterCategories.value?.last()
+        assertEquals(expectedCategory, actualCategory)
+    }
+
+    private fun verifySortOptionsEquals(sortOptions: List<SortOption>) {
+        viewModel.sortOptions.value?.forEachIndexed { index, sortOption ->
+            assertEquals(sortOptions[index], sortOption)
+        }
     }
 
     private fun onGetDiscussionAggregate_thenReturn(discussionAggregateResponse: DiscussionAggregateResponse) {
