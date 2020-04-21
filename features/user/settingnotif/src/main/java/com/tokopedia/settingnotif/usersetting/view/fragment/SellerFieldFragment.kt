0a35d30d5915d42ff4fccdff3ee311e477addbf5
isfaaghyth <isfaaghyth@gmail.com>
2020-04-21 09:26:49 +0700

    settingnotif: use viewModel instead for request update user setting

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/SellerFieldFragment.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/SellerFieldFragment.kt
@@ -3,7 +3,7 @@ package com.tokopedia.settingnotif.usersetting.view.fragment
 import com.tokopedia.settingnotif.R
 import com.tokopedia.settingnotif.usersetting.view.adapter.factory.VisitableSettings
 import com.tokopedia.settingnotif.usersetting.view.dataview.NotificationActivationDataView.activationPushNotif
-import com.tokopedia.settingnotif.usersetting.view.dataview.UserSettingViewModel
+import com.tokopedia.settingnotif.usersetting.view.dataview.UserSettingDataView
 import com.tokopedia.settingnotif.usersetting.view.fragment.base.SettingFieldFragment
 
 class SellerFieldFragment: SettingFieldFragment() {
@@ -12,7 +12,7 @@ class SellerFieldFragment: SettingFieldFragment() {
         return R.raw.query_seller_notif_setting
     }
 
-    override fun onSuccessGetUserSetting(data: UserSettingViewModel) {
+    override fun onSuccessGetUserSetting(data: UserSettingDataView) {
         val pinnedData = arrayListOf<VisitableSettings>()
         /*
         * showing pinned message
@@ -35,8 +35,8 @@ class SellerFieldFragment: SettingFieldFragment() {
             notificationType: String,
             updatedSettingIds: List<Map<String, Any>>
     ) {
-        presenter.requestUpdateUserSetting(TYPE_PUSH_NOTIF, updatedSettingIds)
-        presenter.requestUpdateMoengageUserSetting(updatedSettingIds)
+        settingViewModel.requestUpdateUserSetting(TYPE_PUSH_NOTIF, updatedSettingIds)
+        settingViewModel.requestUpdateMoengageUserSetting(updatedSettingIds)
     }
 
     override fun getScreenName() = getString(R.string.settingnotif_settings_seller_title)
