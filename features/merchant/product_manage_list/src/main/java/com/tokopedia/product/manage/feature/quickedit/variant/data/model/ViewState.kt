f40500d68ee0691c9448a668e8c59600b50c763d
rezagama <reza.hidayat@tokopedia.com>
2020-04-21 17:43:14 +0700

    Add show hide error view

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/data/model/ViewState.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/data/model/ViewState.kt
@@ -3,4 +3,7 @@ package com.tokopedia.product.manage.feature.quickedit.variant.data.model
 sealed class ViewState {
     object ShowProgressBar: ViewState()
     object HideProgressBar: ViewState()
+
+    object ShowErrorView: ViewState()
+    object HideErrorView: ViewState()
 }
