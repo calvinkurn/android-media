e49540485fdf1bc01053dd88fda4e8c2f8241377
rezagama <reza.hidayat@tokopedia.com>
2020-04-21 23:22:50 +0700

    Support null value verification

--- features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/verification/VerificationExt.kt
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/verification/VerificationExt.kt
@@ -3,7 +3,7 @@ package com.tokopedia.product.manage.verification
 import androidx.lifecycle.LiveData
 import junit.framework.TestCase.assertEquals
 
-fun LiveData<*>.verifyValueEquals(expected: Any) {
+fun LiveData<*>.verifyValueEquals(expected: Any?) {
     val actual = value
     assertEquals(expected, actual)
 }
