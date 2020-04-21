c07d1bb5d8ed4f0d4d0ffb1ecfcdabb780fdb1a4
rezagama <reza.hidayat@tokopedia.com>
2020-04-21 22:58:51 +0700

    Add verification extension

---
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/verification/VerificationExt.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.product.manage.verification
+
+import androidx.lifecycle.LiveData
+import junit.framework.TestCase.assertEquals
+
+fun LiveData<*>.verifyValueEquals(expected: Any) {
+    val actual = value
+    assertEquals(expected, actual)
+}
