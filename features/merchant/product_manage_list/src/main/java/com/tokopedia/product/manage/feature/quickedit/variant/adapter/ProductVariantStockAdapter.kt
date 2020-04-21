ef0b07bab7eb234943117f9a87877333181bb0c8
rezagama <reza.hidayat@tokopedia.com>
2020-04-21 17:45:04 +0700

    Remove variant stock adapter

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/adapter/ProductVariantStockAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.product.manage.feature.quickedit.variant.adapter
-
-import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
-import com.tokopedia.product.manage.feature.quickedit.variant.adapter.factory.ProductVariantStockAdapterFactoryImpl
-import com.tokopedia.product.manage.feature.quickedit.variant.adapter.model.ProductVariant
-
-class ProductVariantStockAdapter(
-    adapterFactory: ProductVariantStockAdapterFactoryImpl
-): BaseListAdapter<ProductVariant, ProductVariantStockAdapterFactoryImpl>(adapterFactory)
