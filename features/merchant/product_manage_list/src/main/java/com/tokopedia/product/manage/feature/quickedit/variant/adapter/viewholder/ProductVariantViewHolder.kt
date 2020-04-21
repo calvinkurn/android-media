ccfbf8f833036c3d1770490dacfa7fb4ebe99f3b
rezagama <reza.hidayat@tokopedia.com>
2020-04-21 17:44:56 +0700

    Refactor variant price view holder
    - Rename view ID

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/adapter/viewholder/ProductVariantViewHolder.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/quickedit/variant/adapter/viewholder/ProductVariantViewHolder.kt
@@ -1,17 +1,14 @@
 package com.tokopedia.product.manage.feature.quickedit.variant.adapter.viewholder
 
-import android.app.Activity
 import android.text.Editable
-import android.text.InputFilter
+import android.text.InputFilter.LengthFilter
 import android.text.InputType
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.product.manage.R
 import com.tokopedia.product.manage.feature.quickedit.variant.adapter.model.ProductVariant
-import com.tokopedia.unifycomponents.TextFieldUnify
 import com.tokopedia.utils.text.currency.CurrencyFormatHelper
 import com.tokopedia.utils.text.currency.CurrencyIdrTextWatcher
 import kotlinx.android.synthetic.main.item_product_manage_variant.view.*
@@ -25,85 +22,84 @@ class ProductVariantViewHolder(
         @LayoutRes
         val LAYOUT = R.layout.item_product_manage_variant
 
-        private const val MAXIMUM_EDIT_PRICE_INPUT_LENGTH = 11
+        private const val MAXIMUM_INPUT_LENGTH = 11
     }
 
     override fun bind(variant: ProductVariant) {
-        itemView.name.text = variant.name
-        setupEditPriceTextField(variant)
+        itemView.textProductName.text = variant.name
+        setupEditTextFieldPrice(variant)
     }
 
-    private fun setupEditPriceTextField(variant: ProductVariant) {
-        itemView.priceTextField.apply {
-            setupClearIcon()
-            setPriceTextFieldMaxLength()
-            setPriceTextFieldValue(variant.price)
-            setPriceTextFieldListeners(variant.id)
-            setInputType(InputType.TYPE_CLASS_NUMBER)
-        }
+    private fun setupEditTextFieldPrice(variant: ProductVariant) {
+        setInputType()
+        setupClearIcon()
+        setTextFieldPriceMaxLength()
+        setTextFieldPriceValue(variant.price)
+        setTextFieldPriceListeners(variant.id)
     }
 
-    private fun TextFieldUnify.setupClearIcon() {
-        setFirstIcon(R.drawable.ic_system_action_close_normal_24)
-        getFirstIcon().setOnClickListener {
-            textFieldInput.text.clear()
-            hidePriceTextFieldError()
-        }
-    }
-
-    private fun TextFieldUnify.setPriceTextFieldMaxLength() {
-        textFieldInput.filters = arrayOf(InputFilter.LengthFilter(MAXIMUM_EDIT_PRICE_INPUT_LENGTH))
-    }
-
-    private fun TextFieldUnify.setPriceTextFieldValue(price: Int) {
-        val priceRupiah = CurrencyFormatHelper.convertToRupiah(price.toString())
-        val priceTxt = CurrencyFormatHelper.removeCurrencyPrefix(priceRupiah)
-        val prefixTxt = itemView.context.getString(R.string.product_manage_quick_edit_currency)
-        textFieldInput.setText(priceTxt)
-        prependText(prefixTxt)
-    }
-
-    private fun TextFieldUnify.setPriceTextFieldListeners(productId: String) {
-        val idrTextWatcher = CurrencyIdrTextWatcher(textFieldInput)
-        textFieldInput.addTextChangedListener(idrTextWatcher)
-        textFieldInput.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                val price = CurrencyFormatHelper.convertRupiahToInt(textFieldInput.text.toString())
-                listener.onPriceChanged(productId, price)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-        })
-        setOnFocusChangeListener { v, hasFocus ->
-            if (hasFocus) {
-                showSoftKeyboard()
-            } else {
-                hideSoftKeyboard()
+    private fun setupClearIcon() {
+        itemView.textFieldPrice.apply {
+            setFirstIcon(R.drawable.ic_system_action_close_normal_24)
+            getFirstIcon().setOnClickListener {
+                textFieldInput.text.clear()
+                hideTextFieldPriceError()
             }
         }
     }
 
-    private fun TextFieldUnify.hidePriceTextFieldError() {
-        setError(false)
-        setMessage("")
+    private fun setTextFieldPriceMaxLength() {
+        itemView.textFieldPrice.apply {
+            val maxLength = LengthFilter(MAXIMUM_INPUT_LENGTH)
+            textFieldInput.filters = arrayOf(maxLength)
+        }
     }
 
-    private fun hideSoftKeyboard() {
-        val inputMethodManager = itemView.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        val currentFocus = (itemView.context as? Activity)?.currentFocus ?: return
-        val windowToken = currentFocus.windowToken ?: return
-
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    private fun setTextFieldPriceValue(price: Int) {
+       itemView.textFieldPrice.apply {
+           val priceRupiah = CurrencyFormatHelper.convertToRupiah(price.toString())
+           val priceTxt = CurrencyFormatHelper.removeCurrencyPrefix(priceRupiah)
+           val prefixTxt = itemView.context.getString(R.string.product_manage_quick_edit_currency)
+           textFieldInput.setText(priceTxt)
+           prependText(prefixTxt)
+       }
     }
 
-    private fun showSoftKeyboard() {
-        val inputMethodManager = itemView.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        val currentFocus = (itemView.context as? Activity)?.currentFocus ?: return
-        inputMethodManager.showSoftInput(currentFocus, InputMethodManager.SHOW_IMPLICIT)
+    private fun setTextFieldPriceListeners(productId: String) {
+        itemView.textFieldPrice.apply {
+            val idrTextWatcher = CurrencyIdrTextWatcher(textFieldInput)
+            textFieldInput.addTextChangedListener(idrTextWatcher)
+            textFieldInput.addTextChangedListener(object: TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    val input = textFieldInput.text.toString()
+                    val price = CurrencyFormatHelper.convertRupiahToInt(input)
+                    listener.onPriceChanged(productId, price)
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+            })
+            setOnFocusChangeListener { _, hasFocus ->
+               if(hasFocus) {
+                   val length = textFieldInput.length()
+                   textFieldInput.setSelection(length)
+               }
+            }
+        }
+    }
+
+    private fun setInputType() {
+        itemView.textFieldPrice.setInputType(InputType.TYPE_CLASS_NUMBER)
+    }
+
+    private fun hideTextFieldPriceError() {
+        itemView.textFieldPrice.apply {
+            setError(false)
+            setMessage("")
+        }
     }
 
     interface ProductVariantListener {
