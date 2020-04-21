e5676e876946776c9aca844dfd615c8d2ae5e70d
milhamj <milham.jamaludin@gmail.com>
2020-04-21 16:35:37 +0700

    Revert unwanted changes :rewind:

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/ListUnifyUtil.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/ListUnifyUtil.kt
@@ -9,7 +9,6 @@ import com.tokopedia.unifycomponents.list.ListUnify
 object ListUnifyUtil {
     @Suppress("UNCHECKED_CAST")
     private fun getItems(instance: ListUnify) = with(instance) {
-        instance.arr
         this.javaClass.getDeclaredField("array").let {
             it.isAccessible = true
             it.get(this) as ArrayList<ListItemUnify>
