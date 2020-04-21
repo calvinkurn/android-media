9d33179ae1d4fcdda157d92aab17d3912d805254
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-04-21 14:51:59 +0700

    Use html for product name

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/custom/TransactionOrderProgressLayout.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/custom/TransactionOrderProgressLayout.kt
@@ -14,6 +14,7 @@ import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.common.network.util.CommonUtil
 import com.tokopedia.kotlin.extensions.view.*
@@ -228,7 +229,7 @@ class TransactionOrderProgressLayout : LinearLayout {
     }
 
     private fun renderProductName() {
-        productName?.text = chatOrder.name
+        productName?.text = MethodChecker.fromHtml(chatOrder.name)
     }
 
     private fun renderEstimation() {
