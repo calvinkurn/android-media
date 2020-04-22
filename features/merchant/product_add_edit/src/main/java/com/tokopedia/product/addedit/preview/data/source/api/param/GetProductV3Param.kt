438c55b9d7f2f7a63685b349e46e1d0061cf8dad
Faisal Ramdani <faisal.ramdani@tokopedia.com>
2020-04-22 08:57:34 +0700

    Add serialize name for get product param

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/source/api/param/GetProductV3Param.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/source/api/param/GetProductV3Param.kt
@@ -1,18 +1,31 @@
 package com.tokopedia.product.addedit.preview.data.source.api.param
 
+import com.google.gson.annotations.SerializedName
+
 data class GetProductV3Param(
+        @SerializedName("productID")
         val productID: String,
+        @SerializedName("options")
         val options: OptionV3
 )
 
 data class OptionV3(
+        @SerializedName("edit")
         val edit: Boolean = true,
+        @SerializedName("category")
         val category: Boolean = true,
+        @SerializedName("catalog")
         val catalog: Boolean = true,
+        @SerializedName("wholesale")
         val wholesale: Boolean = true,
+        @SerializedName("preorder")
         val preorder: Boolean = true,
+        @SerializedName("picture")
         val picture: Boolean = true,
+        @SerializedName("sku")
         val sku: Boolean = true,
+        @SerializedName("video")
         val video: Boolean = true,
+        @SerializedName("variant")
         val variant: Boolean = true
 )
