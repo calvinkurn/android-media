86cb1a55928e9829a04c77e93f878cd80783be1d
Jessica Sean <jessica.sean@tokopedia.com>
2020-04-22 19:08:09 +0700

    hotel search phase 2 (part01)

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/search/data/model/params/ParamLocation.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/search/data/model/params/ParamLocation.kt
@@ -22,5 +22,13 @@ data class ParamLocation(
 
         @SerializedName("longitude")
         @Expose
-        var longitude: Float = 0f
+        var longitude: Float = 0f,
+
+        @SerializedName("searchType")
+        @Expose
+        val searchType: String = "",
+
+        @SerializedName("searchID")
+        @Expose
+        val searchId: String= ""
 )
