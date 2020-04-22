9d3ea89c8545669cee0d6afb6d32bd7f05293446
leonard2907 <leonard.gunawan@tokopedia.com>
2020-04-22 17:45:59 +0700

    Add a few test cases for TalkReplyViewModel:

--- features/merchant/talk/src/test/java/com/tokopedia/talk/feature/reply/TalkReplyViewModelTest.kt
+++ features/merchant/talk/src/test/java/com/tokopedia/talk/feature/reply/TalkReplyViewModelTest.kt
@@ -1,6 +1,13 @@
 package com.tokopedia.talk.feature.reply
 
 import android.accounts.NetworkErrorException
+import com.tokopedia.talk.feature.reply.data.model.delete.comment.TalkDeleteComment
+import com.tokopedia.talk.feature.reply.data.model.delete.comment.TalkDeleteCommentResponseWrapper
+import com.tokopedia.talk.feature.reply.data.model.delete.comment.TalkDeleteCommentResultData
+import com.tokopedia.talk.feature.reply.data.model.delete.talk.TalkDeleteTalk
+import com.tokopedia.talk.feature.reply.data.model.delete.talk.TalkDeleteTalkResponseWrapper
+import com.tokopedia.talk.feature.reply.data.model.delete.talk.TalkDeleteTalkResultData
+import com.tokopedia.talk.feature.reply.data.model.discussion.AttachedProduct
 import com.tokopedia.talk.feature.reply.data.model.discussion.DiscussionDataByQuestionIDResponseWrapper
 import com.tokopedia.talk.feature.reply.data.model.follow.TalkFollowUnfollowTalk
 import com.tokopedia.talk.feature.reply.data.model.follow.TalkFollowUnfollowTalkResponseWrapper
@@ -9,6 +16,7 @@ import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.coEvery
 import io.mockk.coVerify
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class TalkReplyViewModelTest : TalkReplyViewModelTestFixture() {
@@ -68,6 +76,7 @@ class TalkReplyViewModelTest : TalkReplyViewModelTestFixture() {
         viewModel.followUnfollowTalk(questionId)
 
         val expectedResponse = Fail(Throwable(message = response.talkFollowUnfollowTalkResponse.messageError.first()))
+
         verifyTalkFollowUnfollowTalkUseCaseExecuted()
         verifyTalkFollowUnfollowTalkErrorEquals(expectedResponse)
     }
@@ -82,8 +91,131 @@ class TalkReplyViewModelTest : TalkReplyViewModelTestFixture() {
         viewModel.followUnfollowTalk(questionId)
 
         val expectedResponse = Fail(exception)
+
         verifyTalkFollowUnfollowTalkUseCaseExecuted()
         verifyTalkFollowUnfollowTalkErrorEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteTalk should execute expected use case and get expected data`() {
+        val response = TalkDeleteTalkResponseWrapper(TalkDeleteTalk(data = TalkDeleteTalkResultData(isSuccess = 1)))
+        val questionId = "148148"
+
+        onTalkDeleteTalk_thenReturn(response)
+
+        viewModel.deleteTalk(questionId)
+
+        val expectedResponse = Success(response)
+
+        verifyTalkDeleteTalkUseCaseExecuted()
+        verifyTalkDeleteTalkDataEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteTalk fails due to backend should execute expected use case and fail with expected error`() {
+        val response = TalkDeleteTalkResponseWrapper(TalkDeleteTalk(messageError = listOf("Some Error"), data = TalkDeleteTalkResultData(isSuccess = 0)))
+        val questionId = "148148"
+
+        onTalkDeleteTalk_thenReturn(response)
+
+        viewModel.deleteTalk(questionId)
+
+        val expectedResponse = Fail(Throwable(response.talkDeleteTalk.messageError.first()))
+
+        verifyTalkDeleteTalkUseCaseExecuted()
+        verifyTalkDeleteTalkErrorEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteTalk fails due to network should execute expected use case and fail with expected error`() {
+        val questionId = "148148"
+        val exception = NetworkErrorException()
+
+        onTalkDeleteTalkFail_thenReturn(exception)
+
+        viewModel.deleteTalk(questionId)
+
+        val expectedResponse = Fail(exception)
+
+        verifyTalkDeleteTalkUseCaseExecuted()
+        verifyTalkDeleteTalkErrorEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteComment should execute expected use case and get expected data`() {
+        val response = TalkDeleteCommentResponseWrapper(TalkDeleteComment(data = TalkDeleteCommentResultData(isSuccess = 1)))
+        val questionId = "148148"
+        val commentId = "19219314"
+
+        onTalkDeleteComment_thenReturn(response)
+
+        viewModel.deleteComment(questionId, commentId)
+
+        val expectedResponse = Success(response)
+
+        verifyTalkDeleteCommentUseCaseExecuted()
+        verifyTalkDeleteCommentDataEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteComment fails due to backend should execute expected use case and fail with expected error`() {
+        val response = TalkDeleteCommentResponseWrapper(TalkDeleteComment(messageError = listOf("Some Error"), data = TalkDeleteCommentResultData(isSuccess = 0)))
+        val questionId = "148148"
+        val commentId = "19219314"
+
+        onTalkDeleteComment_thenReturn(response)
+
+        viewModel.deleteComment(questionId, commentId)
+
+        val expectedResponse = Fail(Throwable(response.talkDeleteComment.messageError.first()))
+
+        verifyTalkDeleteCommentUseCaseExecuted()
+        verifyTalkDeleteCommentErrorEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when deleteComment fails due to network should execute expected use case and fail with expected error`() {
+        val questionId = "148148"
+        val commentId = "19219314"
+        val exception = NetworkErrorException()
+
+        onTalkDeleteCommentFail_thenReturn(exception)
+
+        viewModel.deleteComment(questionId, commentId)
+
+        val expectedResponse = Fail(exception)
+
+        verifyTalkDeleteCommentUseCaseExecuted()
+        verifyTalkDeleteCommentErrorEquals(expectedResponse)
+    }
+
+    @Test
+    fun `when setAttachedProducts should set attachedProducts to expected value`() {
+        val attachedProducts = mutableListOf(AttachedProduct(productId = "1"), AttachedProduct(productId = "2"))
+
+        viewModel.setAttachedProducts(attachedProducts)
+
+        verifyAttachedProductsEqual(attachedProducts)
+    }
+
+    @Test
+    fun `when removeAttachedProduct should remove specified attachedProduct from attachedProducts`() {
+        val attachedProducts = mutableListOf(AttachedProduct(productId = "1"), AttachedProduct(productId = "2"))
+
+        viewModel.setAttachedProducts(attachedProducts)
+        viewModel.removeAttachedProduct("1")
+
+        val expectedAttachedProduct = mutableListOf(AttachedProduct(productId = "2"))
+        verifyAttachedProductsEqual(expectedAttachedProduct)
+    }
+
+    @Test
+    fun `when set IsFollowing should set isFollowing to expected value`() {
+        val expected = true
+
+        viewModel.setIsFollowing(expected)
+
+        verifyIsFollowingEquals(expected)
     }
 
     private fun onGetDiscussionData_thenReturn(discussionDataByQuestionIDResponseWrapper: DiscussionDataByQuestionIDResponseWrapper) {
@@ -100,6 +232,22 @@ class TalkReplyViewModelTest : TalkReplyViewModelTestFixture() {
 
     private fun onTalkFollowUnfollowTalkFail_thenReturn(exception: Exception) {
         coEvery { talkFollowUnfollowTalkUseCase.executeOnBackground() } throws exception
+    }
+
+    private fun onTalkDeleteTalk_thenReturn(talkDeleteTalkResponseWrapper: TalkDeleteTalkResponseWrapper) {
+        coEvery { talkDeleteTalkUseCase.executeOnBackground() } returns talkDeleteTalkResponseWrapper
+    }
+
+    private fun onTalkDeleteTalkFail_thenReturn(exception: Exception) {
+        coEvery { talkDeleteTalkUseCase.executeOnBackground() } throws exception
+    }
+
+    private fun onTalkDeleteComment_thenReturn(talkDeleteCommentResponseWrapper: TalkDeleteCommentResponseWrapper) {
+        coEvery { talkDeleteCommentUseCase.executeOnBackground() } returns talkDeleteCommentResponseWrapper
+    }
+
+    private fun onTalkDeleteCommentFail_thenReturn(exception: Exception) {
+        coEvery { talkDeleteCommentUseCase.executeOnBackground() } throws exception
     }
 
     private fun verifyGetDiscussionDataByQuestionIdUseCaseExecuted() {
@@ -136,5 +284,29 @@ class TalkReplyViewModelTest : TalkReplyViewModelTestFixture() {
 
     private fun verifyTalkFollowUnfollowTalkErrorEquals(expectedResponse: Fail) {
         viewModel.followUnfollowResult.verifyErrorEquals(expectedResponse)
+    }
+
+    private fun verifyTalkDeleteTalkDataEquals(expectedResponse: Success<TalkDeleteTalkResponseWrapper>) {
+        viewModel.deleteTalkResult.verifySuccessEquals(expectedResponse)
+    }
+
+    private fun verifyTalkDeleteTalkErrorEquals(expectedResponse: Fail) {
+        viewModel.deleteTalkResult.verifyErrorEquals(expectedResponse)
+    }
+
+    private fun verifyTalkDeleteCommentDataEquals(expectedResponse: Success<TalkDeleteCommentResponseWrapper>) {
+        viewModel.deleteCommentResult.verifySuccessEquals(expectedResponse)
+    }
+
+    private fun verifyTalkDeleteCommentErrorEquals(expectedResponse: Fail) {
+        viewModel.deleteCommentResult.verifyErrorEquals(expectedResponse)
+    }
+
+    private fun verifyIsFollowingEquals(isFollowing: Boolean) {
+        assertEquals(viewModel.getIsFollowing(), isFollowing)
+    }
+
+    private fun verifyAttachedProductsEqual(attachedProducts: MutableList<AttachedProduct>) {
+        viewModel.attachedProducts.verifyValueEquals(attachedProducts)
     }
 }
