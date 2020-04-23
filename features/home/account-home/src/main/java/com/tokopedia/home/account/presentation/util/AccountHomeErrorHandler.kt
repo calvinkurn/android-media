dbf167105670b7f353cf4fa35209ec2ea15218b4
stevenfredian <steven.fredian@tokopedia.com>
2020-04-23 14:45:00 +0700

    enhance logging account home error

--- features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/util/AccountHomeErrorHandler.kt
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/util/AccountHomeErrorHandler.kt
@@ -3,7 +3,9 @@ package com.tokopedia.home.account.presentation.util
 import android.text.TextUtils
 import com.crashlytics.android.Crashlytics
 import com.tokopedia.home.account.BuildConfig
+import com.tokopedia.kotlin.extensions.view.toEmptyStringIfNull
 import com.tokopedia.network.data.model.response.ResponseV4ErrorException
+import timber.log.Timber
 import java.lang.Exception
 import java.net.URLEncoder
 
@@ -13,8 +15,10 @@ object AccountHomeErrorHandler {
         return try {
             if (t is ResponseV4ErrorException && !TextUtils.isEmpty(t.errorList.firstOrNull())) {
                 t.errorList.firstOrNull() ?: t.localizedMessage
-            } else {
+            } else if(t.localizedMessage != null){
                 t.localizedMessage
+            } else {
+                t.toString()
             }
         }
         catch (e: Exception){
@@ -24,20 +28,24 @@ object AccountHomeErrorHandler {
 
     @JvmStatic
     fun logExceptionToCrashlytics(t: Throwable, userId: String, email:String, errorCode:String) {
-        if (!BuildConfig.DEBUG) {
-            val errorMessage = String.format(
-                    "\"Error account home.\",\"userId: %s\",\"userEmail: %s \",\"errorMessage: %s\",\"%s\"",
-                    userId,
-                    email,
-                    AccountHomeErrorHandler.getExceptionMessage(t),
-                    errorCode)
-            val exception = AccountHomeException(errorMessage, t)
 
+        val errorMessage = String.format(
+                "\"Error account home.\",\"userId: %s\",\"userEmail: %s \",\"errorMessage: %s\",\"%s\"",
+                userId,
+                email,
+                getExceptionMessage(t),
+                errorCode)
+        val exception = AccountHomeException(errorMessage, t)
+
+
+        if (!BuildConfig.DEBUG) {
             try {
                 Crashlytics.logException(exception)
-            } catch (e: IllegalStateException) {
-                e.printStackTrace()
+            } catch (e: Exception) {
+                Timber.w("P2#ACCOUNT_HOME#Failed renderC;$errorMessage $e")
             }
+        } else {
+            Timber.w("P2#ACCOUNT_HOME#Failed render;$errorMessage")
         }
     }
 }
