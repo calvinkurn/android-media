30a879ffb6c820516b16cf28de84743c38e97917
isfaaghyth <isfaaghyth@gmail.com>
2020-04-23 22:08:18 +0700

    settingnotif: use BaseRequestUseCase on getSetting and setSetting user

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/GetUserSettingUseCase.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/domain/GetUserSettingUseCase.kt
@@ -1,31 +1,21 @@
 package com.tokopedia.settingnotif.usersetting.domain
 
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.settingnotif.usersetting.base.BaseRequestUseCase
+import com.tokopedia.settingnotif.usersetting.base.SettingRepository
 import com.tokopedia.settingnotif.usersetting.data.pojo.UserNotificationResponse
 import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 
 open class GetUserSettingUseCase @Inject constructor(
-        private val repository: GraphqlRepository,
+        private val repository: SettingRepository,
         private val graphQuery: String
 ) : UseCase<UserNotificationResponse>() {
 
     override suspend fun executeOnBackground(): UserNotificationResponse {
-        val request = GraphqlRequest(graphQuery, UserNotificationResponse::class.java)
-        val response = repository.getReseponse(listOf(request))
-        val error = response.getError(UserNotificationResponse::class.java)
-
-        if (error == null || error.isEmpty()) {
-            return response.getData(
-                    UserNotificationResponse::class.java
-            ) as UserNotificationResponse
-        } else {
-            throw MessageErrorException(
-                    error.mapNotNull { it.message }.joinToString(separator = ", ")
-            )
-        }
+        return BaseRequestUseCase.execute(
+                query = graphQuery,
+                repository = repository
+        )
     }
 
 }
