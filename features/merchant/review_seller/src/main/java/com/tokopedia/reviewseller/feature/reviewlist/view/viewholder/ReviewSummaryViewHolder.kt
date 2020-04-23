683bbed9205ced46900344d2fad45561d3d86367
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-04-23 10:22:59 +0700

    fix viewholder & viewmodel

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/view/viewholder/ReviewSummaryViewHolder.kt
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewlist/view/viewholder/ReviewSummaryViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.reviewseller.R
+import com.tokopedia.reviewseller.common.util.roundDecimal
 import com.tokopedia.reviewseller.feature.reviewlist.view.model.ProductRatingOverallUiModel
 import com.tokopedia.unifyprinciples.Typography
 
@@ -17,7 +18,7 @@ class ReviewSummaryViewHolder(itemView: View,
 
     override fun bind(element: ProductRatingOverallUiModel?) {
 
-        tgCountRating.text = element?.rating?.toString()
+        tgCountRating.text = element?.rating?.roundDecimal()
         tgCountReview.text = element?.reviewCount?.toString()
         tgFiveReview.text = getString(R.string.rating_overall_product)
         tgPeriodReview.text = "1 Des 2019 - Hari Ini"
