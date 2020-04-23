74d04f8f942611a9ace5a0ba51108ae3a1a0456d
isfaaghyth <isfaaghyth@gmail.com>
2020-04-23 18:00:42 +0700

    settingnotif: fixed useCase provider on DI module

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/di/module/UserSettingModule.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/di/module/UserSettingModule.kt
@@ -2,42 +2,82 @@ package com.tokopedia.settingnotif.usersetting.di.module
 
 import android.content.Context
 import androidx.annotation.RawRes
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.GraphqlUseCase
+import com.tokopedia.settingnotif.R
 import com.tokopedia.settingnotif.usersetting.di.UserSettingScope
 import com.tokopedia.settingnotif.usersetting.domain.GetUserSettingUseCase
 import com.tokopedia.settingnotif.usersetting.domain.SetUserSettingUseCase
+import com.tokopedia.settingnotif.usersetting.util.dispatcher.AppDispatcherProvider
+import com.tokopedia.settingnotif.usersetting.util.dispatcher.DispatcherProvider
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 
 @Module class UserSettingModule(
-        var context: Context?,
-        @RawRes val gqlQueryRaw: Int
+        private val context: Context?,
+        @RawRes private val gqlQueryRaw: Int
 ) {
 
-    @UserSettingScope
     @Provides
-    fun provideUserSession(): UserSessionInterface {
+    @UserSettingScope
+    fun provideContext(): Context? {
+        return context
+    }
+
+    @Provides
+    @UserSettingScope
+    fun provideUserSession(
+            @UserSettingScope context: Context?
+    ): UserSessionInterface {
         return UserSession(context)
     }
 
-    @UserSettingScope
     @Provides
-    fun providesGetUserSettingUseCase(gqlUserCase: GraphqlUseCase): GetUserSettingUseCase {
-        return GetUserSettingUseCase(context, gqlUserCase, gqlQueryRaw)
+    @UserSettingScope
+    fun provideGraphqlRepository(): GraphqlRepository {
+        return GraphqlInteractor.getInstance().graphqlRepository
     }
 
-    @UserSettingScope
     @Provides
-    fun providesSetUserSettingUseCase(gqlUserCase: GraphqlUseCase): SetUserSettingUseCase {
-        return SetUserSettingUseCase(context, gqlUserCase)
+    @UserSettingScope
+    fun provideGetUserSettingUseCase(
+            @UserSettingScope context: Context?,
+            repository: GraphqlRepository
+    ): GetUserSettingUseCase {
+        val query = GraphqlHelper.loadRawString(
+                context?.resources,
+                gqlQueryRaw
+        )
+        return GetUserSettingUseCase(repository, query)
     }
 
-    @UserSettingScope
     @Provides
-    fun providesGqlUseCase(): GraphqlUseCase {
+    @UserSettingScope
+    fun provideSetUserSettingUseCase(
+            @UserSettingScope context: Context?,
+            repository: GraphqlRepository
+    ): SetUserSettingUseCase {
+        val query = GraphqlHelper.loadRawString(
+                context?.resources,
+                R.raw.query_set_user_setting
+        )
+        return SetUserSettingUseCase(repository, query)
+    }
+
+    @Provides
+    @UserSettingScope
+    fun provideGqlUseCase(): GraphqlUseCase {
         return GraphqlUseCase()
+    }
+
+    @Provides
+    @UserSettingScope
+    fun provideDispatcherProvider(): DispatcherProvider {
+        return AppDispatcherProvider()
     }
 
 }
