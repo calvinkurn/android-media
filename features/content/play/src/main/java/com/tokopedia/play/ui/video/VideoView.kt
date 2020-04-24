a153dd5b6d1834dd4c55e3b7a0ead6e5a0d43798
kenny <kenny.hadisaputra@tokopedia.com>
2020-04-24 14:22:18 +0700

    try using player view

--- features/content/play/src/main/java/com/tokopedia/play/ui/video/VideoView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/video/VideoView.kt
@@ -1,11 +1,15 @@
 package com.tokopedia.play.ui.video
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintSet
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.PlayerView
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
@@ -25,7 +29,7 @@ class VideoView(container: ViewGroup) : UIView(container) {
             LayoutInflater.from(container.context).inflate(R.layout.view_video, container, true)
                     .findViewById(R.id.cl_video_view)
 
-    private val pvVideo = view.findViewById<VideoPlayCustom>(R.id.pv_video)
+    private val pvVideo = view.findViewById<PlayerView>(R.id.pv_video)
 
     override val containerId: Int = view.id
 
@@ -39,14 +43,26 @@ class VideoView(container: ViewGroup) : UIView(container) {
 
     internal fun onDestroy() {
         setPlayer(null)
-        pvVideo.release()
     }
 
     internal fun setPlayer(exoPlayer: ExoPlayer?) {
-        pvVideo.setPlayer(exoPlayer)
+        pvVideo.player = exoPlayer
     }
 
     internal fun setOrientation(screenOrientation: ScreenOrientation, videoOrientation: VideoOrientation) {
-        pvVideo.setOrientation(screenOrientation, videoOrientation)
+//        pvVideo.setOrientation(screenOrientation, videoOrientation)
+        pvVideo.resizeMode = if (videoOrientation.isHorizontal) {
+            when {
+                screenOrientation.isLandscape -> AspectRatioFrameLayout.RESIZE_MODE_FIT
+                else -> AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH
+            }
+        } else AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+
+        val lParams = pvVideo.layoutParams as FrameLayout.LayoutParams
+        lParams.gravity =
+                if (videoOrientation.isHorizontal && !screenOrientation.isLandscape) Gravity.NO_GRAVITY
+                else Gravity.CENTER
+
+        pvVideo.layoutParams = lParams
     }
 }
