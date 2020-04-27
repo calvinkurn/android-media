1518b415e1c93a2f1f2f10fc49a9c1e1dc9133e4
sumit-tokopedia <sumit.gupta@tokopedia.com>
2020-04-27 13:14:30 +0530

    bug fix for translator

---
+++ features/common/developer_options/src/release/java/com/tokopedia/translator/ui/TranslatorSettingView.java
@@ -0,0 +1,17 @@
+package com.tokopedia.translator.ui;
+
+import android.widget.*;
+
+public class TranslatorSettingView extends FrameLayout {
+    public TranslatorSettingView(Context context) {
+        super(context);
+    }
+
+    public TranslatorSettingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TranslatorSettingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
