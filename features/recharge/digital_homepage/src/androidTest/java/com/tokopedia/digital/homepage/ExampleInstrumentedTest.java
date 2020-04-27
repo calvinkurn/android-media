f006c033767eca0f36540907ac682c995237158b
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-04-27 19:49:20 +0700

    fix error unit test and instrument test for sonarqube recharge and travel modules

--- features/recharge/digital_homepage/src/androidTest/java/com/tokopedia/digital/homepage/ExampleInstrumentedTest.java
+++ features/recharge/digital_homepage/src/androidTest/java/com/tokopedia/digital/homepage/ExampleInstrumentedTest.java
@@ -1,14 +1,13 @@
-package com.tokopedia.digital.emoney;
+package com.tokopedia.digital.homepage;
 
 import android.content.Context;
-
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.tokopedia.digital.emoney", appContext.getPackageName());
+        assertEquals("com.tokopedia.digital.homepage.test", appContext.getPackageName());
     }
 }
