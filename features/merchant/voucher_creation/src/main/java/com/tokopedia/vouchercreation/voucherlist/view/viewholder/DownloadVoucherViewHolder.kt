1fe66c9ab0be40361f56b140bb922534ab872737
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-04-28 14:33:09 +0800

    rotate chevron

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/voucherlist/view/viewholder/DownloadVoucherViewHolder.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/voucherlist/view/viewholder/DownloadVoucherViewHolder.kt
@@ -1,11 +1,9 @@
 package com.tokopedia.vouchercreation.voucherlist.view.viewholder
 
 import android.view.View
-import androidx.annotation.DrawableRes
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.isVisible
-import com.tokopedia.kotlin.extensions.view.loadImageDrawable
 import com.tokopedia.vouchercreation.R
 import com.tokopedia.vouchercreation.voucherlist.model.DownloadVoucherUiModel
 import kotlinx.android.synthetic.main.item_mvc_download_voucher.view.*
@@ -41,11 +39,7 @@ class DownloadVoucherViewHolder(itemView: View?) : AbstractViewHolder<DownloadVo
     }
 
     private fun rotateChevronIcon(isExpanded: Boolean) = with(itemView) {
-        @DrawableRes val icon = if (isExpanded) {
-            R.drawable.ic_mvc_chevron_down
-        } else {
-            R.drawable.ic_mvc_chevron_up
-        }
-        icMvcChevron.loadImageDrawable(icon)
+        val angle = if (isExpanded) 0 else 180
+        icMvcChevron.rotation = angle.toFloat()
     }
 }
