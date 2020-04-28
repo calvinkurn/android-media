ca243849f23b6173729c8bc006a22b63135620f2
isfaaghyth <isfaaghyth@gmail.com>
2020-04-28 22:10:27 +0700

    fastcompile: provide amplification graphql query on DI module

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/di/module/GraphQueryModule.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/di/module/GraphQueryModule.kt
@@ -3,8 +3,6 @@ package com.tokopedia.notifications.di.module
 import android.content.Context
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.notifications.R
-import com.tokopedia.notifications.di.module.NotificationModule.Companion.ATC_MUTATION_QUERY
-import com.tokopedia.notifications.di.module.NotificationModule.Companion.NOTIFICATION_ATTRIBUTION
 import com.tokopedia.notifications.di.scope.CMNotificationContext
 import dagger.Module
 import dagger.Provides
@@ -32,6 +30,23 @@ import javax.inject.Named
                 context.resources,
                 R.raw.mutation_add_to_cart
         )
+    }
+
+    @Provides
+    @Named(AMPLIFICATION_QUERY)
+    fun provideAmplificationQuery(
+            @CMNotificationContext context: Context
+    ): String {
+        return GraphqlHelper.loadRawString(
+                context.resources,
+                R.raw.query_notification_amplification
+        )
+    }
+
+    companion object {
+        const val NOTIFICATION_ATTRIBUTION = "notification_attribution"
+        const val ATC_MUTATION_QUERY = "atcMutation"
+        const val AMPLIFICATION_QUERY = "atcMutation"
     }
 
 }
