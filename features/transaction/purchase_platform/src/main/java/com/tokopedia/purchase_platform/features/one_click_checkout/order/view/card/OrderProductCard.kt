abd7377aaa68300617e003dc6d8dffe1967995aa
hansenpw <hansen.wijaya@tokopedia.com>
2020-04-28 15:43:14 +0700

    fix notes

--- features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/order/view/card/OrderProductCard.kt
+++ features/transaction/purchase_platform/src/main/java/com/tokopedia/purchase_platform/features/one_click_checkout/order/view/card/OrderProductCard.kt
@@ -27,9 +27,7 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
     private val tvProductName by lazy { view.findViewById<Typography>(R.id.tv_product_name) }
     private val ivProductImage by lazy { view.findViewById<RoundedCornerImageView>(R.id.iv_product_image) }
     private val lblCashback by lazy { view.findViewById<Label>(R.id.lbl_cashback) }
-    private val etNote by lazy { view.findViewById<EditText>(R.id.et_note) }
     private val tfNote by lazy { view.findViewById<TextFieldUnify>(R.id.tf_note) }
-    private val tvNoteCharCounter by lazy { view.findViewById<Typography>(R.id.tv_note_char_counter) }
     private val btnQtyPlus by lazy { view.findViewById<ImageView>(R.id.btn_qty_plus) }
     private val btnQtyMin by lazy { view.findViewById<ImageView>(R.id.btn_qty_min) }
     private val tvQuantityStockAvailable by lazy { view.findViewById<Typography>(R.id.tv_quantity_stock_available) }
@@ -68,22 +66,20 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
 
             tfNote?.textFieldInput?.textSize = 16f
             tfNote?.textFieldInput?.setSingleLine(false)
-            etNote?.filters = arrayOf(InputFilter.LengthFilter(MAX_NOTES_LENGTH))
-            etNote?.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            tfNote?.setCounter(MAX_NOTES_LENGTH)
+            tfNote?.textFieldInput?.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
                 if (hasFocus) {
                     orderSummaryAnalytics.eventClickSellerNotes(product.productId.toString(), shop.shopId.toString())
                 }
             }
             if (noteTextWatcher != null) {
-                etNote?.removeTextChangedListener(noteTextWatcher)
+                tfNote?.textFieldInput?.removeTextChangedListener(noteTextWatcher)
             }
-            etNote?.setText(product.notes)
-            tvNoteCharCounter?.text = view.context.getString(R.string.note_counter_format, product.notes.length, 144)
+            tfNote?.textFieldInput?.setText(product.notes)
             noteTextWatcher = object : TextWatcher {
                 override fun afterTextChanged(s: Editable?) {
                     product.notes = s?.toString() ?: ""
                     listener.onProductChange(product, false)
-                    tvNoteCharCounter?.text = view.context.getString(R.string.note_counter_format, product.notes.length, 144)
                 }
 
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -94,7 +90,7 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
 
                 }
             }
-            etNote?.addTextChangedListener(noteTextWatcher)
+            tfNote?.textFieldInput?.addTextChangedListener(noteTextWatcher)
 
             if (quantityTextWatcher != null) {
                 etQty?.removeTextChangedListener(quantityTextWatcher)
