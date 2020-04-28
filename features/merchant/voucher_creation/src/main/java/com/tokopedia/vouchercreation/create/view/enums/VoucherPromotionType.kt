29efb2f0de64930d763a4f8f335e24f580200957
Reivin <reivin.oktavianus@tokopedia.com>
2020-04-28 20:40:31 +0700

    Add string res for promo type

---
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/enums/VoucherPromotionType.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.vouchercreation.create.view.enums
+
+import androidx.annotation.StringRes
+
+enum class VoucherPromotionType(@StringRes val tickerStringRes: Int) {
+
+}
---
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/viewholder/vouchertarget/widgets/TypeAndBudgetSelectorViewHolder.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.vouchercreation.create.view.viewholder.vouchertarget.widgets
+
+import androidx.annotation.LayoutRes
+import com.tokopedia.vouchercreation.R
+
+class TypeAndBudgetSelectorViewHolder {
+
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.mvc_type_budget_promotion_widget
+    }
+}
