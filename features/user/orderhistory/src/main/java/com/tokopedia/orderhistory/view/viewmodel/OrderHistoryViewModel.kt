dc6c870a77efc74066902d761f95ebfc3de0a147
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-04-29 12:24:13 +0700

    refactor

--- features/user/orderhistory/src/main/java/com/tokopedia/orderhistory/view/viewmodel/OrderHistoryViewModel.kt
+++ features/user/orderhistory/src/main/java/com/tokopedia/orderhistory/view/viewmodel/OrderHistoryViewModel.kt
@@ -14,7 +14,7 @@ import javax.inject.Inject
 
 class OrderHistoryViewModel @Inject constructor(
         private val productHistoryUseCase: GetProductOrderHistoryUseCase,
-        private var addWishListUseCase: AddWishListUseCase
+        private val addWishListUseCase: AddWishListUseCase
 ) : ViewModel() {
 
     private val _product: MutableLiveData<Result<ChatHistoryProductResponse>> = MutableLiveData()
---
+++ features/user/orderhistory/src/test/java/com/tokopedia/orderhistory/view/viewmodel/OrderHistoryViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.tokopedia.orderhistory.view.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+class OrderHistoryViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+
+    @Test
+    fun loadProductHistory() {
+    }
+
+    @Test
+    fun addToWishList() {
+    }
+
+}
