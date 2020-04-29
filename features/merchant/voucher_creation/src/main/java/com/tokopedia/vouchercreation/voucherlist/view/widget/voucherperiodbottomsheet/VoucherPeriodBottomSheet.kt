f05f559c795f7e12f5ab567121c1584fe7a436e4
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-04-29 12:15:01 +0800

    add edit voucher period bottom sheet

---
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/voucherlist/view/widget/voucherperiodbottomsheet/VoucherPeriodBottomSheet.kt
@@ -0,0 +1,72 @@
+package com.tokopedia.vouchercreation.voucherlist.view.widget.voucherperiodbottomsheet
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.tokopedia.kotlin.extensions.view.loadImageDrawable
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.vouchercreation.R
+import com.tokopedia.vouchercreation.voucherlist.model.VoucherUiModel
+import kotlinx.android.synthetic.main.bottomsheet_mvc_voucher_edit_period.view.*
+
+/**
+ * Created By @ilhamsuaib on 29/04/20
+ */
+
+class VoucherPeriodBottomSheet(
+        parent: ViewGroup,
+        private val voucher: VoucherUiModel
+) : BottomSheetUnify() {
+
+    private var onSaveListener: () -> Unit = {}
+
+    init {
+        val child = LayoutInflater.from(parent.context)
+                .inflate(R.layout.bottomsheet_mvc_voucher_edit_period, parent, false)
+
+        setTitle(parent.context.getString(R.string.mvc_edit_shown_period))
+        setChild(child)
+        setupView(child)
+        setAction(parent.context.getString(R.string.mvc_retry)) {
+
+        }
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+    }
+
+    private fun setupView(view: View) = with(view) {
+        imgMvcVoucher.loadImageDrawable(R.drawable.img_mvc_cashback_khusus)
+        tvMvcVoucherName.text = voucher.name
+        tvMvcVoucherDescription.text = voucher.description
+
+        edtMvcStartDate.setOnClickListener {
+            showDatePicker {
+
+            }
+        }
+        edtMvcEndDate.setOnClickListener {
+            showDatePicker {
+
+            }
+        }
+
+        btnMvcSavePeriod.setOnClickListener {
+            onSaveListener()
+            dismiss()
+        }
+    }
+
+    private fun showDatePicker(function: () -> Unit) {
+
+    }
+
+    fun setOnSaveClickListener(callback: () -> Unit): VoucherPeriodBottomSheet {
+        this.onSaveListener = callback
+        return this
+    }
+
+    fun show(fm: FragmentManager) {
+        showNow(fm, this::class.java.simpleName)
+    }
+}
