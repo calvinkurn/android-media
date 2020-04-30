474bbffdd5ae8cf6e3b460d0cb7cf9a0d620bf58
isfaaghyth <isfaaghyth@gmail.com>
2020-04-30 17:23:29 +0700

    pushnotif: created test extension and mock response variable

---
+++ libraries/cm_push_notifications/src/test/java/com/tokopedia/notifications/MockResponse.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.notifications
+
+val generalResponse = """
+    {
+      "icon": "",
+      "sound": "",
+      "notificationId": "1",
+      "source": "toko-cm",
+      "tribe": "13829",
+      "notificationType": "General",
+      "userId": "5513570",
+      "shopId": "",
+      "transId": "MTU4NzEwNTE0MTQyMjI3Njg3OCMxNTY4MyMwIzU1MTM1NzAjdHJ1ZQ==:1",
+      "userTransId": "MTU4NzEwNTE0MTQyMjI3Njg3OCMxNTY4MyMwIzU1MTM1NzAjdHJ1ZQ==",
+      "notifcenterBlastId": "",
+      "channel": "",
+      "title": "Title 3",
+      "desc": "Message 3",
+      "message": "Message 3",
+      "appLink": "tokopedia://referral",
+      "collapsedImg": "",
+      "expandedImg": "",
+      "webhook_params": "{\"nc_notif_id\":\"\",\"nc_type_of_notif\":0}",
+      "campaignUserToken": "MTU4NzEwNTE0MTQyMjI3Njg3OCMxNTY4MyMwIzU1MTM1NzAjdHJ1ZQ==:1",
+      "campaignId": 15683,
+      "parentId": 13829
+    }
+""".trimIndent()
