f36c84f33c6884b357464f4df571a5e052d06511
yogieputra8 <yogie.putra8@gmail.com>
2020-05-05 00:28:16 +0700

    changes due to feedback

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/viewmodel/OfficialStoreCategoryViewModel.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/viewmodel/OfficialStoreCategoryViewModel.kt
@@ -10,8 +10,6 @@ import com.tokopedia.officialstore.category.domain.GetOfficialStoreCategoriesUse
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
-//import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
