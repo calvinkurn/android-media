2ee5d0686fcbe12c40fbf4b2ceba3ebc70168e81
leonard2907 <leonard.gunawan@tokopedia.com>
2020-05-04 12:03:55 +0700

    Add dummy link

--- features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/constants/SellerMigrationConstants.kt
+++ features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/constants/SellerMigrationConstants.kt
@@ -7,7 +7,7 @@ object SellerMigrationConstants {
     const val SELLER_MIGRATION_PROMO_FIRST_IMAGE = "https://ecs7.tokopedia.net/other/seller_migration_top_ads.png"
     const val SELLER_MIGRATION_PROMO_SECOND_IMAGE = "https://ecs7.tokopedia.net/other/seller_migration_statistics.png"
     const val SELLER_MIGRATION_PRODUCT_IMAGE_LINK = "https://ecs7.tokopedia.net/other/seller_migration_product_list.png"
-    const val SELLER_MIGRATION_INFORMATION_LINK = ""
+    const val SELLER_MIGRATION_INFORMATION_LINK = "https://www.tokopedia.com/blog/"
     const val PACKAGE_SELLER_APP = "com.tokopedia.sellerapp"
     const val APPLINK_PLAYSTORE = "market://details?id="
     const val URL_PLAYSTORE = "https://play.google.com/store/apps/details?id="
