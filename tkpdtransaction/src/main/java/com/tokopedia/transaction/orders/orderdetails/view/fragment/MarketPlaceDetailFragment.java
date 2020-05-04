9c2d650a04df5b40e0a1ed8b99afe65e8c43dfc6
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-05-04 14:25:52 +0700

    change handle applink param

--- tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/view/fragment/MarketPlaceDetailFragment.java
+++ tkpdtransaction/src/main/java/com/tokopedia/transaction/orders/orderdetails/view/fragment/MarketPlaceDetailFragment.java
@@ -788,9 +788,9 @@ public class MarketPlaceDetailFragment extends BaseDaggerFragment implements Ref
 
                         String trackingUrl;
                         Uri uri = Uri.parse(actionButton.getUri());
+
                         trackingUrl = uri.getQueryParameter("url");
                         Uri.Builder uriBuilder = new Uri.Builder();
-
                         uriBuilder.appendQueryParameter(ApplinkConst.Query.ORDER_TRACKING_URL_LIVE_TRACKING, trackingUrl);
                         routingAppLink += uriBuilder.toString();
                         RouteManager.route(getContext(), routingAppLink);
