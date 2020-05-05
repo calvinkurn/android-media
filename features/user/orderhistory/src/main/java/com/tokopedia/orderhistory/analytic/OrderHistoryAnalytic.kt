5dd5c72d910152b6087915a8b27de3b46d93ff9a
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-05-05 12:18:08 +0700

    fix review

--- features/user/orderhistory/src/main/java/com/tokopedia/orderhistory/analytic/OrderHistoryAnalytic.kt
+++ features/user/orderhistory/src/main/java/com/tokopedia/orderhistory/analytic/OrderHistoryAnalytic.kt
@@ -1,21 +1,7 @@
 package com.tokopedia.orderhistory.analytic
 
-import com.tokopedia.track.TrackApp
-import com.tokopedia.track.TrackAppUtils
 import javax.inject.Inject
 
 class OrderHistoryAnalytic @Inject constructor() {
-
-    object Name {
-
-    }
-
-    object Category {
-
-    }
-
-    object Action {
-
-    }
 
 }
