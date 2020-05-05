6a429b705eafbd0bc82c671b468cf9c5dbcbbdfb
elscione <yusufhendrawan06@gmail.com>
2020-05-05 15:06:06 +0700

    remove buyer notification setting

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/EmailFieldFragment.kt
@@ -1,13 +1,20 @@
 package com.tokopedia.settingnotif.usersetting.view.fragment
 
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.settingnotif.R
+import com.tokopedia.settingnotif.usersetting.domain.pojo.SettingSections
 import com.tokopedia.settingnotif.usersetting.view.adapter.factory.VisitableSettings
 import com.tokopedia.settingnotif.usersetting.view.dataview.ChangeItemDataView.changeEmail
+import com.tokopedia.settingnotif.usersetting.view.dataview.Data
 import com.tokopedia.settingnotif.usersetting.view.dataview.NotificationActivationDataView.activationEmail
 import com.tokopedia.settingnotif.usersetting.view.dataview.UserSettingViewModel
 import com.tokopedia.settingnotif.usersetting.view.fragment.base.SettingFieldFragment
 
 class EmailFieldFragment: SettingFieldFragment() {
+
+    companion object {
+        const val BUYING_TRANSACTION_SECTION_TITLE = "Transaksi Pembelian"
+    }
 
     override fun getGqlRawQuery(): Int {
         return R.raw.query_email_setting
@@ -28,6 +35,9 @@ class EmailFieldFragment: SettingFieldFragment() {
             * */
             activationEmail()
         })
+        if (GlobalConfig.isSellerApp()) {
+            data.data = removeBuyerNotificationSetting(data.data)
+        }
         pinnedData.addAll(data.data)
         data.data = pinnedData.toList()
         super.onSuccessGetUserSetting(data)
@@ -36,4 +46,15 @@ class EmailFieldFragment: SettingFieldFragment() {
     override fun getScreenName() = getString(R.string.settingnotif_email)
     override fun getNotificationType() = EMAIL_TYPE
 
+    private fun removeBuyerNotificationSetting(items: List<Data>): List<Data> {
+        var include = true
+        return ArrayList(items.filter { setting ->
+            if (setting is SettingSections && setting.title == BUYING_TRANSACTION_SECTION_TITLE) {
+                include = false
+            } else if (setting is SettingSections && !include) {
+                include = true
+            }
+            include
+        })
+    }
 }
