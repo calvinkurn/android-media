ea9a9ef2ff9d4dd4773dadfbd860f2c29c6fb4ee
leonard2907 <leonard.gunawan@tokopedia.com>
2020-05-06 09:40:11 +0700

    rename and fix font

--- features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/widget/SellerMigrationGenericBottomSheet.kt
+++ features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/widget/SellerMigrationGenericBottomSheet.kt
@@ -6,14 +6,14 @@ import android.view.View
 import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.seller_migration_common.R
 import com.tokopedia.seller_migration_common.constants.SellerMigrationConstants.ACCOUNT_SELLER_MIGRATION_IMAGE_LINK
-import kotlinx.android.synthetic.main.widget_seller_migration_account_bottom_sheet.*
+import kotlinx.android.synthetic.main.widget_seller_migration_generic_bottom_sheet.*
 
 class SellerMigrationGenericBottomSheet : SellerMigrationBottomSheet() {
 
     companion object {
         fun createNewInstance(context: Context) : SellerMigrationGenericBottomSheet {
             return SellerMigrationGenericBottomSheet().apply{
-                val view = View.inflate(context, R.layout.widget_seller_migration_account_bottom_sheet,null)
+                val view = View.inflate(context, R.layout.widget_seller_migration_generic_bottom_sheet,null)
                 setChild(view)
             }
         }
