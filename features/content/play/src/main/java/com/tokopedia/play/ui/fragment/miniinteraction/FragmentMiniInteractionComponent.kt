885510253999a3e28450b1f838988f915c81439c
kenny <kenny.hadisaputra@tokopedia.com>
2020-05-06 16:34:47 +0700

    add initial mini interaction

---
+++ features/content/play/src/main/java/com/tokopedia/play/ui/fragment/miniinteraction/FragmentMiniInteractionComponent.kt
@@ -0,0 +1,5 @@
+package com.tokopedia.play.ui.fragment.miniinteraction
+
+/**
+ * Created by jegul on 06/05/20
+ */
---
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayMiniInteractionFragment.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.play.view.fragment
+
+import android.view.ViewGroup
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.play.view.layout.interaction.PlayInteractionViewInitializer
+
+/**
+ * Created by jegul on 06/05/20
+ */
+class PlayMiniInteractionFragment {
+
+}
