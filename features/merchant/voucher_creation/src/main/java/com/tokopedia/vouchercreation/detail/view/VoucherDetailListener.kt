880016037bb0f8cfe324dcdaf2ec4b58f55fe48e
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-05-06 10:48:59 +0800

    add voucher detail listener interface

---
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/detail/view/VoucherDetailListener.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.vouchercreation.detail.view
+
+/**
+ * Created By @ilhamsuaib on 06/05/20
+ */
+
+interface VoucherDetailListener {
+
+    fun onFooterButtonClickListener()
+}
