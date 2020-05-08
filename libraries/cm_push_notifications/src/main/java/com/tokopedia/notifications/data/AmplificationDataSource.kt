86159146cef0db56929a1b9dcf569eb5aa8daf2b
isfaaghyth <isfaaghyth@gmail.com>
2020-05-08 23:51:46 +0700

    pushnotif: sdded notification validation of exisiting row in local db

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AmplificationDataSource.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/data/AmplificationDataSource.kt
@@ -1,12 +1,10 @@
 package com.tokopedia.notifications.data
 
 import android.content.Context
-import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.notifications.PushController
 import com.tokopedia.notifications.R
-import com.tokopedia.notifications.data.converters.JsonBundleConverter.jsonToBundle
 import com.tokopedia.notifications.data.model.AmplificationNotifier
 import com.tokopedia.notifications.domain.AmplificationUseCase
 import com.tokopedia.abstraction.common.utils.GraphqlHelper.loadRawString as loadRaw
@@ -24,8 +22,7 @@ object AmplificationDataSource {
         val amplificationUseCase = AmplificationUseCase(useCase, query)
         amplificationUseCase.execute {
             it.webhookAttributionNotifier.pushData.forEach { data ->
-                val bundle = jsonToBundle(data)
-                PushController(context).handleNotificationBundle(bundle)
+                PushController(context).handleNotificationAmplification(data)
             }
         }
     }
