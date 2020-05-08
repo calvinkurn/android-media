966d5c2fcf9ced1d553bfa93191da9b298c3224c
leonard2907 <leonard.gunawan@tokopedia.com>
2020-05-08 15:03:28 +0700

    Fix toaster duration

--- features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reading/presentation/fragment/TalkReadingFragment.kt
+++ features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reading/presentation/fragment/TalkReadingFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.snackbar.Snackbar
 import com.tokopedia.TalkInstance
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
 import com.tokopedia.abstraction.base.view.fragment.BaseListFragment
@@ -400,7 +401,7 @@ class TalkReadingFragment : BaseListFragment<TalkReadingUiModel,
     }
 
     private fun showErrorToaster() {
-        view?.let { Toaster.make(it, getString(R.string.reading_connection_error_toaster_message), Toaster.LENGTH_LONG, Toaster.TYPE_ERROR, getString(R.string.talk_retry)) }
+        view?.let { Toaster.make(it, getString(R.string.reading_connection_error_toaster_message), Snackbar.LENGTH_LONG, Toaster.TYPE_ERROR, getString(R.string.talk_retry)) }
     }
 
     private fun updateSortHeader(sortOption: SortOption) {
