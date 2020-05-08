4c332e02c5a384804cc20121e1d2500bf64ac2a1
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-05-08 17:20:28 +0700

    change liveness AB

--- features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KYCConstant.java
+++ features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KYCConstant.java
@@ -51,5 +51,5 @@ public interface KYCConstant {
     int USER_EXIT = -9;
 
     String KYC_OVERLAY_COLOR = "#ae000000";
-    String KYC_AB_KEYWORD = "Liveness Detection 1";
+    String KYC_AB_KEYWORD = "Liveness Detection 2";
 }
