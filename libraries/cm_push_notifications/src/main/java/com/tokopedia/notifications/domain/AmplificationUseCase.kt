fbe79cf40953999ad51fdeadbc40b77fa1b3dee9
isfaaghyth <isfaaghyth@gmail.com>
2020-05-08 23:22:32 +0700

    pushnotif: remove requestParams on useCase of amplification

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/domain/AmplificationUseCase.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/domain/AmplificationUseCase.kt
@@ -2,6 +2,7 @@ package com.tokopedia.notifications.domain
 
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.notifications.data.model.AmplificationNotifier
+import com.tokopedia.usecase.RequestParams
 import javax.inject.Inject
 
 class AmplificationUseCase @Inject constructor(
@@ -9,26 +10,13 @@ class AmplificationUseCase @Inject constructor(
         private val rawQuery: String
 ) {
 
-    fun execute(
-            params: Map<String, Any>,
-            onSuccess: (response: AmplificationNotifier) -> Unit
-    ) {
+    fun execute(onSuccess: (response: AmplificationNotifier) -> Unit) {
         useCase.apply {
             setTypeClass(AmplificationNotifier::class.java)
-            setRequestParams(params)
+            setRequestParams(RequestParams.EMPTY.parameters)
             setGraphqlQuery(rawQuery)
             execute(onSuccess, {})
         }
-    }
-
-    fun params(deviceToken: String): Map<String, Any> {
-        return hashMapOf<String, Any>().apply {
-            put(DEVICE_TOKEN, deviceToken)
-        }
-    }
-
-    companion object {
-        private const val DEVICE_TOKEN = "device_token"
     }
 
 }
