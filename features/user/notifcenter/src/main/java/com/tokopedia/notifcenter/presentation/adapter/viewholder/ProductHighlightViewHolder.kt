4c74b34ed756bdee7e30f0005b3c9b0808abe2c0
isfaaghyth <isfaaghyth@gmail.com>
2020-05-10 10:23:22 +0700

    notifcenter: added container click to product detail

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/ProductHighlightViewHolder.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/ProductHighlightViewHolder.kt
@@ -7,12 +7,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.notifcenter.R
+import com.tokopedia.notifcenter.data.entity.ProductData
 import com.tokopedia.notifcenter.data.mapper.ProductHighlightMapper.mapToCampaign
 import com.tokopedia.notifcenter.data.viewbean.ProductHighlightViewBean
 import com.tokopedia.notifcenter.widget.CampaignRedView
+import com.tokopedia.unifycomponents.CardUnify
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
 
@@ -21,6 +25,7 @@ class ProductHighlightViewHolder(
         view: View
 ): RecyclerView.ViewHolder(view) {
 
+    private val container: CardUnify = view.findViewById(R.id.container)
     private val imgProduct: ImageView = view.findViewById(R.id.imgProduct)
     private val txtProductName: TextView = view.findViewById(R.id.txtProductName)
     private val viewCampaignTag: CampaignRedView = view.findViewById(R.id.viewCampaignTag)
@@ -30,7 +35,9 @@ class ProductHighlightViewHolder(
 
     fun bind(element: ProductHighlightViewBean?) {
         if (element == null) return
+        container.setOnClickListener { productDetailClicked(element.id.toString()) }
         btnAddToCart.setOnClickListener { onAtcClick() }
+
         viewCampaignTag.setCampaign(mapToCampaign(element))
 
         imgProduct.loadImage(element.imageUrl)
@@ -41,6 +48,14 @@ class ProductHighlightViewHolder(
             imgCampaign.loadImage(element.freeOngkirIcon)
             imgCampaign.show()
         }
+    }
+
+    private fun productDetailClicked(productId: String) {
+        RouteManager.route(
+                itemView.context,
+                ApplinkConstInternalMarketplace.PRODUCT_DETAIL,
+                productId
+        )
     }
 
     companion object {
