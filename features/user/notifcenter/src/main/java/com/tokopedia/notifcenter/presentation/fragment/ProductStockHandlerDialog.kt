ac49e9f1a09c0e7bd38206665632df8166e721f7
isfaaghyth <isfaaghyth@gmail.com>
2020-05-10 10:17:02 +0700

    notifcenter: added atc product card on highlight product list

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/ProductStockHandlerDialog.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/ProductStockHandlerDialog.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.notifcenter.presentation.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -11,16 +10,14 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import com.facebook.shimmer.ShimmerFrameLayout
-import com.google.android.material.snackbar.Snackbar
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
-import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.analytics.StockHandlerAnalytics
+import com.tokopedia.notifcenter.data.entity.ProductData
 import com.tokopedia.notifcenter.data.viewbean.NotificationItemViewBean
 import com.tokopedia.notifcenter.data.viewbean.ProductHighlightViewBean
 import com.tokopedia.notifcenter.listener.NotificationItemListener
@@ -33,6 +30,9 @@ import com.tokopedia.notifcenter.widget.CampaignRedView
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.unifycomponents.CardUnify
 import com.tokopedia.unifycomponents.Toaster
+import com.tokopedia.unifycomponents.Toaster.TYPE_ERROR
+import com.tokopedia.unifycomponents.Toaster.TYPE_NORMAL
+import com.tokopedia.unifycomponents.Toaster.toasterLength
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
@@ -63,14 +63,17 @@ class ProductStockHandlerDialog(
     private lateinit var viewModel: ProductStockHandlerViewModel
 
     private val productHighlightList = arrayListOf<ProductHighlightViewBean>()
-    private val adapter by lazy { ProductHighlightAdapter(productHighlightList) }
 
-    private val containerView: View by lazy(LazyThreadSafetyMode.NONE) {
-        View.inflate(requireContext(), R.layout.dialog_product_stock_handler, null)
+    private val adapter by lazy(LazyThreadSafetyMode.NONE) {
+        ProductHighlightAdapter(productHighlightList, ::onAtcClicked)
     }
 
     private val analytics by lazy(LazyThreadSafetyMode.NONE) {
         StockHandlerAnalytics()
+    }
+
+    private val containerView: View by lazy(LazyThreadSafetyMode.NONE) {
+        View.inflate(requireContext(), R.layout.dialog_product_stock_handler, null)
     }
 
     private fun initView() {
@@ -100,12 +103,12 @@ class ProductStockHandlerDialog(
 
         initView()
         setChild(containerView)
-        renderView(element)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         analytics.productCardImpression(element, userSession.userId)
+        renderView()
         initObservable()
     }
 
@@ -127,70 +130,100 @@ class ProductStockHandlerDialog(
         })
 
         viewModel.productStockReminder.observe(viewLifecycleOwner, Observer {
-            successReminderProductStock()
+            val successMessage = getString(R.string.product_reminder_success)
+            val actionText = getString(R.string.notifcenter_btn_title_ok)
+            onSuccessListener(successMessage, actionText)
+            btnReminder.isEnabled = false
+        })
+
+        viewModel.addToCart.observe(viewLifecycleOwner, Observer {
+            listener.onSuccessAddToCart(it.data.message.first())
+        })
+
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer {
+            showToastErrorMessage(it)
         })
     }
 
-    private fun renderView(element: NotificationItemViewBean) {
-        txtEmptyMessage.text = "Belum ada produk"
-
-        txtTitle.text = element.title
-        txtDescription.text = element.body
+    private fun renderView() {
+        // content view of bottomSheet container and empty state
+        setContainerContentView()
 
         element.getAtcProduct()?.let { product ->
+            // set product card data
             buttonReminderValidation(product.typeButton)
+            setDataProductCard(product)
+            onClickListener(product)
 
+            // get product highlight
             if (product.shop?.id != null) {
                 viewModel.getHighlightProduct(product.shop.id.toString())
-            }
-
-            ImageHandler.loadImage2(
-                    thumbnail,
-                    product.imageUrl,
-                    R.drawable.ic_notifcenter_loading_toped
-            )
-
-            productName.text = product.name
-            productPrice.text = product.priceFormat
-            productCampaign.setCampaign(product.campaign)
-
-            if (product.shop?.freeShippingIcon != null &&
-                    product.shop.freeShippingIcon.isNotEmpty()) {
-                campaignTag.loadImage(product.shop.freeShippingIcon)
-                campaignTag.show()
-            }
-
-            productCard.setOnClickListener {
-                analytics.productCardClicked(element, userSession.userId)
-                RouteManager.route(
-                        context,
-                        ApplinkConstInternalMarketplace.PRODUCT_DETAIL,
-                        product.productId
-                )
-            }
-
-            btnReminder.setOnClickListener {
-                analytics.stockReminderClicked(element, userSession.userId)
-                if (product.stock < SINGLE_PRODUCT_STOCK) {
-                    viewModel.setProductReminder(product.productId, element.notificationId)
-                } else {
-                    listener.addProductToCheckout(element.userInfo, element)
-                }
             }
         }
     }
 
-    private fun successReminderProductStock() {
-        btnReminder.isEnabled = false
-        dialogWindow()?.let {
-            Toaster.make(
-                    it,
-                    "SUKSES",
-                    Toaster.toasterLength,
-                    Toaster.TYPE_NORMAL,
-                    "LIHAT",
-                    View.OnClickListener { }
+    private fun setContainerContentView() {
+        txtTitle.text = element.title
+        txtDescription.text = element.body
+        txtEmptyMessage.text = getString(R.string.product_highlight_empty)
+    }
+
+    private fun setDataProductCard(product: ProductData) {
+        ImageHandler.loadImage2(
+                thumbnail,
+                product.imageUrl,
+                R.drawable.ic_notifcenter_loading_toped
+        )
+
+        productName.text = product.name
+        productPrice.text = product.priceFormat
+        productCampaign.setCampaign(product.campaign)
+
+        setCampaignTag(product)
+    }
+
+    private fun setCampaignTag(product: ProductData) {
+        if (product.shop?.freeShippingIcon != null &&
+            product.shop.freeShippingIcon.isNotEmpty()) {
+            campaignTag.loadImage(product.shop.freeShippingIcon)
+            campaignTag.show()
+        }
+    }
+
+    private fun onAtcClicked() {
+        viewModel.addProductToCart(element.getAtcProduct())
+    }
+
+    private fun onClickListener(product: ProductData) {
+        productCard.setOnClickListener {
+            analytics.productCardClicked(element, userSession.userId)
+            RouteManager.route(
+                    context,
+                    ApplinkConstInternalMarketplace.PRODUCT_DETAIL,
+                    product.productId
             )
+        }
+
+        btnReminder.setOnClickListener {
+            analytics.stockReminderClicked(element, userSession.userId)
+            if (product.stock < SINGLE_PRODUCT_STOCK) {
+                viewModel.setProductReminder(product.productId, element.notificationId)
+            } else {
+                listener.addProductToCheckout(element.userInfo, element)
+            }
+        }
+    }
+
+    private fun onSuccessListener(title: String, actionText: String, onClick: () -> Unit = {}) {
+        dialogWindow()?.let {
+            val onActionClick = View.OnClickListener { onClick() }
+            Toaster.make(it, title, toasterLength, TYPE_NORMAL, actionText, onActionClick)
+        }
+    }
+
+    private fun showToastErrorMessage(message: String) {
+        dialogWindow()?.let {
+            Toaster.make(it, message, toasterLength, TYPE_ERROR)
         }
     }
 
