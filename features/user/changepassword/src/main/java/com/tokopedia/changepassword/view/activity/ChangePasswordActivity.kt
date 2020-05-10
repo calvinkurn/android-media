4aa48c2eb9ef8a6fbdefc7af3f1f5fd08a24c3eb
stevenfredian <steven.fredian@tokopedia.com>
2020-05-10 14:50:25 +0700

    revert

--- features/user/changepassword/src/main/java/com/tokopedia/changepassword/view/activity/ChangePasswordActivity.kt
+++ features/user/changepassword/src/main/java/com/tokopedia/changepassword/view/activity/ChangePasswordActivity.kt
@@ -1,12 +1,9 @@
 package com.tokopedia.changepassword.view.activity
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.changepassword.view.fragment.ChangePasswordFragment
-import com.tokopedia.user.session.UserSession
 
 class ChangePasswordActivity : BaseSimpleActivity() {
 
@@ -16,18 +13,5 @@ class ChangePasswordActivity : BaseSimpleActivity() {
             bundle.putAll(intent.extras)
         }
         return ChangePasswordFragment()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when(requestCode) {
-            ChangePasswordFragment.REQUEST_LOGIN -> {
-                var userSession = UserSession(this)
-                if (!userSession.isLoggedIn) {
-                    setResult(Activity.RESULT_CANCELED)
-                    finish()
-                }
-            }
-        }
     }
 }
