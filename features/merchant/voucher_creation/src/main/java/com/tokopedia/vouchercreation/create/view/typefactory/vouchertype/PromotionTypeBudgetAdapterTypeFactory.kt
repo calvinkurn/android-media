b278d98b973baa8205c96f10b61e8aca5f1fc68d
Reivin <reivin.oktavianus@tokopedia.com>
2020-05-12 02:16:35 +0700

    Fix error

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/typefactory/vouchertype/PromotionTypeBudgetAdapterTypeFactory.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/typefactory/vouchertype/PromotionTypeBudgetAdapterTypeFactory.kt
@@ -6,8 +6,11 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.vouchercreation.create.view.enums.VoucherImageType
+import com.tokopedia.vouchercreation.create.view.typefactory.voucherimage.VoucherImageTypeFactory
+import com.tokopedia.vouchercreation.create.view.uimodel.voucherimage.BannerVoucherUiModel
 import com.tokopedia.vouchercreation.create.view.uimodel.vouchertype.widget.PromotionTypeInputUiModel
 import com.tokopedia.vouchercreation.create.view.viewholder.NextButtonViewHolder
+import com.tokopedia.vouchercreation.create.view.viewholder.voucherimage.BannerVoucherViewHolder
 import com.tokopedia.vouchercreation.create.view.viewholder.vouchertype.widget.PromotionTypeInputViewHolder
 
 class PromotionTypeBudgetAdapterTypeFactory(private val fragment: Fragment,
@@ -17,6 +20,7 @@ class PromotionTypeBudgetAdapterTypeFactory(private val fragment: Fragment,
 
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when(type) {
+            BannerVoucherViewHolder.LAYOUT -> BannerVoucherViewHolder<PromotionTypeBudgetTypeFactory>(parent)
             PromotionTypeInputViewHolder.LAYOUT -> PromotionTypeInputViewHolder(parent, fragment, onNextStep, onShouldChangeBannerValue)
             NextButtonViewHolder.LAYOUT -> NextButtonViewHolder(parent)
             else -> super.createViewHolder(parent, type)
@@ -24,4 +28,5 @@ class PromotionTypeBudgetAdapterTypeFactory(private val fragment: Fragment,
     }
 
     override fun type(promotionTypeInputUiModel: PromotionTypeInputUiModel): Int = PromotionTypeInputViewHolder.LAYOUT
+    override fun <T : VoucherImageTypeFactory> type(bannerVoucherUiModel: BannerVoucherUiModel<T>): Int = BannerVoucherViewHolder.LAYOUT
 }
--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/typefactory/vouchertype/PromotionTypeBudgetTypeFactory.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/create/view/typefactory/vouchertype/PromotionTypeBudgetTypeFactory.kt
@@ -1,8 +1,9 @@
 package com.tokopedia.vouchercreation.create.view.typefactory.vouchertype
 
 import com.tokopedia.vouchercreation.create.view.typefactory.CreateVoucherTypeFactory
+import com.tokopedia.vouchercreation.create.view.typefactory.voucherimage.VoucherImageTypeFactory
 import com.tokopedia.vouchercreation.create.view.uimodel.vouchertype.widget.PromotionTypeInputUiModel
 
-interface PromotionTypeBudgetTypeFactory : CreateVoucherTypeFactory {
+interface PromotionTypeBudgetTypeFactory : CreateVoucherTypeFactory, VoucherImageTypeFactory {
     fun type(promotionTypeInputUiModel: PromotionTypeInputUiModel): Int
 }
