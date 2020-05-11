0397b248c81dca0297a5ec94d9404e982acbfe67
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-05-11 16:34:11 +0800

    change cons name

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/common/consts/VoucherStatusConst.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/common/consts/VoucherStatusConst.kt
@@ -9,7 +9,7 @@ import androidx.annotation.IntDef
 @MustBeDocumented
 @Retention(AnnotationRetention.SOURCE)
 @IntDef(VoucherStatusConst.DELETED, VoucherStatusConst.PROCESSING,
-        VoucherStatusConst.NOT_STARTED, VoucherStatusConst.ON_GOING,
+        VoucherStatusConst.NOT_STARTED, VoucherStatusConst.ONGOING,
         VoucherStatusConst.ENDED, VoucherStatusConst.STOPPED)
 annotation class VoucherStatusConst {
 
@@ -17,7 +17,7 @@ annotation class VoucherStatusConst {
         const val DELETED = -1
         const val PROCESSING = 0
         const val NOT_STARTED = 1
-        const val ON_GOING = 2
+        const val ONGOING = 2
         const val ENDED = 3
         const val STOPPED = 4
     }
