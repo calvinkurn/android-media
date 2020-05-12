71f86350490ad96175bb4873b739c6838aeea55d
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2020-05-12 11:29:14 +0530

    claim copon repo change

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/repository/claimCoupon/ClaimCouponRestRepository.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/repository/claimCoupon/ClaimCouponRestRepository.kt
@@ -1,12 +1,12 @@
 package com.tokopedia.discovery2.repository.claimCoupon
 
 import com.google.gson.reflect.TypeToken
-import com.tokopedia.abstraction.common.data.model.response.DataResponse
+import com.tokopedia.basemvvm.repository.BaseRepository
 import com.tokopedia.common.network.data.model.RequestType
 import com.tokopedia.discovery2.data.ComponentsItem
 import com.tokopedia.discovery2.data.DiscoveryResponse
 import com.tokopedia.discovery2.discoverymapper.DiscoveryDataMapper
-import com.tokopedia.tradein_common.repository.BaseRepository
+import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.usecase.RequestParams
 import javax.inject.Inject
 
@@ -14,11 +14,11 @@ import javax.inject.Inject
 open class ClaimCouponRestRepository @Inject constructor() : BaseRepository(), IClaimCouponRepository {
 
     override suspend fun getClickCouponData(url: String): ArrayList<ComponentsItem> {
-        val response = getRestData(url,
+        val response = getRestData<DataResponse<DiscoveryResponse>>(url,
                 object : TypeToken<DataResponse<DiscoveryResponse>>() {}.type,
                 RequestType.GET,
                 RequestParams.EMPTY.parameters)
-        val categoryNavigationResponse = response?.getData() as DataResponse<DiscoveryResponse>
+        val categoryNavigationResponse = response
         val discoveryDataMapper = DiscoveryDataMapper()
         return discoveryDataMapper.mapListToComponentList(categoryNavigationResponse.data.component?.data, "claim_coupon_item")
     }
