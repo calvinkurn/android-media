162b0a8037d7b427a3ccc2f9a7f48de15b9d587c
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-05-13 19:57:28 +0700

    add tracker on forgot password

---
+++ tkpdsession/src/main/java/com/tokopedia/session/forgotpassword/analytics/ForgotPasswordAnalytics.kt
@@ -0,0 +1,24 @@
+package com.tokopedia.session.forgotpassword.analytics
+
+import com.tokopedia.track.TrackApp
+
+class ForgotPasswordAnalytics {
+
+    private val tracker = TrackApp.getInstance().gtm
+
+    fun onViewForgotPassword() {
+
+    }
+
+    fun onSubmitClicked() {
+
+    }
+
+    fun onError(message: String) {
+
+    }
+
+    companion object {
+
+    }
+}
