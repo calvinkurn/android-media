5b03702e075dca578f29f21f8a109657b5805c5c
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-05-13 14:47:34 +0700

    refactor on digital_emoney (some codes move to digital_brizzi and common_electronic_money)

--- features/recharge/digital_emoney/src/main/java/com/tokopedia/emoney/viewmodel/EmoneyBalanceViewModel.kt
+++ features/recharge/digital_emoney/src/main/java/com/tokopedia/emoney/viewmodel/EmoneyBalanceViewModel.kt
@@ -2,11 +2,11 @@ package com.tokopedia.emoney.viewmodel
 
 import android.nfc.tech.IsoDep
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
-import com.tokopedia.emoney.data.EmoneyInquiry
-import com.tokopedia.emoney.data.EmoneyInquiryResponse
-import com.tokopedia.emoney.data.NfcCardErrorTypeDef
-import com.tokopedia.emoney.util.NFCUtils
-import com.tokopedia.emoney.util.SingleLiveEvent
+import com.tokopedia.common_electronic_money.data.EmoneyInquiry
+import com.tokopedia.common_electronic_money.data.EmoneyInquiryResponse
+import com.tokopedia.common_electronic_money.util.NFCUtils
+import com.tokopedia.common_electronic_money.util.NfcCardErrorTypeDef
+import com.tokopedia.common_electronic_money.util.SingleLiveEvent
 import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlRequest
