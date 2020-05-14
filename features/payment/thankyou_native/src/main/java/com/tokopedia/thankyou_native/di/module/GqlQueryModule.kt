f588b1cd9ba3d38843218497245a764fe3c7cb81
lalit.singh <lalit.singh@tokopedia.com>
2020-05-14 16:38:21 +0530

    - applink changed for specific order to MarketPlace order List - added todo for future dev

--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/di/module/GqlQueryModule.kt
+++ features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/di/module/GqlQueryModule.kt
@@ -6,19 +6,22 @@ import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.thankyou_native.GQL_CHECK_WHITE_LIST
 import com.tokopedia.thankyou_native.R
 import com.tokopedia.thankyou_native.GQL_THANK_YOU_PAGE_DATA
+import com.tokopedia.thankyou_native.di.scope.ThankYouPageScope
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
 
+@ThankYouPageScope
 @Module
 class GqlQueryModule {
 
+    @ThankYouPageScope
     @Provides
     @Named(GQL_THANK_YOU_PAGE_DATA)
     fun provideRawThankYouPageData(@ApplicationContext context: Context): String =
             GraphqlHelper.loadRawString(context.resources, R.raw.gql_thanks_page_data)
 
-
+    @ThankYouPageScope
     @Provides
     @Named(GQL_CHECK_WHITE_LIST)
     fun provideRawCheckWhitelist(@ApplicationContext context: Context): String =
--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/presentation/fragment/ThankYouBaseFragment.kt
+++ features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/presentation/fragment/ThankYouBaseFragment.kt
@@ -95,13 +95,11 @@ abstract class ThankYouBaseFragment : BaseDaggerFragment(), OnDialogRedirectList
 
     override fun gotoOrderList() {
         try {
+            //todo need multi deeplink once other type of transaction integrated.
+            // ..currently it is for only market place
             getThankPageAnalytics().sendCheckTransactionListEvent()
-            val list: List<String> = getThankPageData().shopOrder.map {
-                it.orderId
-            }
-
             val homeIntent = RouteManager.getIntent(context, ApplinkConst.HOME, "")
-            val orderListListIntent = RouteManager.getIntent(context, ApplinkConst.Transaction.ORDER_MARKETPLACE_DETAIL, list[0])
+            val orderListListIntent = RouteManager.getIntent(context, ApplinkConst.MARKETPLACE_ORDER)
             orderListListIntent?.let {
                 TaskStackBuilder.create(context)
                         .addNextIntent(homeIntent)
