2e00050ca180dd5be06bf6bdf78eb877e5dfe3c6
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-05-15 20:36:51 +0700

    Use sticker imageview

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/StickerViewHolder.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/StickerViewHolder.kt
@@ -1,32 +1,65 @@
 package com.tokopedia.topchat.chatroom.view.adapter.viewholder
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.domain.pojo.sticker.Sticker
+import com.tokopedia.topchat.chatroom.view.custom.StickerImageView
 
 class StickerViewHolder(
         itemView: View,
         private val listener: Listener?
-) : RecyclerView.ViewHolder(itemView) {
+) : RecyclerView.ViewHolder(itemView), RequestListener<Drawable> {
 
-    private var stickerImage: ImageView? = itemView.findViewById(R.id.iv_sticker)
+    private var stickerImage: StickerImageView? = itemView.findViewById(R.id.iv_sticker)
+    private var loader: AnimatedVectorDrawableCompat? = null
 
     interface Listener {
         fun onClickSticker(sticker: Sticker)
     }
 
     fun bind(sticker: Sticker) {
+        initLoader()
         bindStickerImage(sticker)
         bindStickerImageClick(sticker)
     }
 
+    private fun initLoader() {
+        itemView.context?.let {
+            loader = AnimatedVectorDrawableCompat.create(it, com.tokopedia.unifycomponents.R.drawable.unify_loader_shimmer)
+            loader?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable?) {
+                    stickerImage?.handler?.post {
+                        loader?.start()
+                    }
+                }
+            })
+            loader?.start()
+
+        }
+    }
+
     private fun bindStickerImage(sticker: Sticker) {
-        ImageHandler.loadImage(itemView.context, stickerImage, sticker.imageUrl, R.drawable.ic_loading_image)
+        stickerImage?.let {
+            Glide.with(itemView.context)
+                    .load(sticker.imageUrl)
+                    .listener(this)
+                    .dontAnimate()
+                    .placeholder(loader)
+                    .diskCacheStrategy(DiskCacheStrategy.DATA)
+                    .into(it)
+        }
     }
 
     private fun bindStickerImageClick(sticker: Sticker) {
@@ -41,5 +74,15 @@ class StickerViewHolder(
             val view = LayoutInflater.from(parent.context).inflate(LAYOUT, parent, false)
             return StickerViewHolder(view, listener)
         }
+    }
+
+    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+        return true
+    }
+
+    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+        stickerImage?.setImageDrawable(resource)
+        stickerImage?.clipCircle = false
+        return true
     }
 }
