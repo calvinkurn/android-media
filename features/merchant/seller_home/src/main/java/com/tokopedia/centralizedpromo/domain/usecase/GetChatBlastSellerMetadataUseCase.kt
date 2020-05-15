59a52bdea507c535c010371392b5ced5b0c7160d
elscione <yusufhendrawan06@gmail.com>
2020-05-15 11:25:48 +0700

    Create chat blast seller metadata usecase to get free quota status

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/centralizedpromo/domain/usecase/GetChatBlastSellerMetadataUseCase.kt
@@ -0,0 +1,37 @@
+package com.tokopedia.centralizedpromo.domain.usecase
+
+import com.tokopedia.centralizedpromo.domain.mapper.BroadcastChatMetadataMapper
+import com.tokopedia.centralizedpromo.domain.model.ChatBlastSellerMetadataResponse
+import com.tokopedia.centralizedpromo.view.model.ChatBlastSellerMetadataUiModel
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.sellerhome.domain.usecase.BaseGqlUseCase
+import javax.inject.Inject
+
+class GetChatBlastSellerMetadataUseCase @Inject constructor(
+        private val gqlRepository: GraphqlRepository,
+        private val mapper: BroadcastChatMetadataMapper
+) : BaseGqlUseCase<ChatBlastSellerMetadataUiModel>() {
+    override suspend fun executeOnBackground(): ChatBlastSellerMetadataUiModel {
+        val gqlRequest = GraphqlRequest(QUERY, ChatBlastSellerMetadataResponse::class.java)
+        val gqlResponse = gqlRepository.getReseponse(listOf(gqlRequest))
+
+        val errors = gqlResponse.getError(ChatBlastSellerMetadataResponse::class.java)
+        if (errors.isNullOrEmpty()) {
+            val data = gqlResponse.getData<ChatBlastSellerMetadataResponse>()
+            return mapper.mapDomainDataModelToUiDataModel(data)
+        } else {
+            throw MessageErrorException(errors.joinToString(", ") { it.message })
+        }
+    }
+
+    companion object {
+        const val QUERY = "query {\n" +
+                "  chatBlastSellerMetadata {\n" +
+                "    promo\n" +
+                "    promoType\n" +
+                "  }\n" +
+                "}"
+    }
+}
