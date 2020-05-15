d2148fb6cadb40c76f6db9658f5e2b046851d903
rezagama <reza.hidayat@tokopedia.com>
2020-05-15 18:45:18 +0700

    Add power merchant url constants

---
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/constant/PowerMerchantUrl.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.power_merchant.subscribe.view.constant
+
+object PowerMerchantUrl {
+
+    const val URL_FREE_SHIPPING_TERMS_AND_CONDITION = "https://seller.tokopedia.com/edu/bebas-ongkir/"
+    const val URL_BROADCAST_CHAT_TERMS_AND_CONDITION = "https://seller.tokopedia.com/edu/gratis-broadcast-chat/"
+    const val URL_LEARN_MORE_BENEFIT = "https://seller.tokopedia.com/edu/power-merchant/"
+}
