c1d1d7e806f32fd8770d443b8bc74b17dc55afe9
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-05-18 13:46:55 +0700

    remove header hardcoded

--- features/user/managepassword/src/main/java/com/tokopedia/managepassword/common/network/ManagePasswordApi.kt
+++ features/user/managepassword/src/main/java/com/tokopedia/managepassword/common/network/ManagePasswordApi.kt
@@ -10,14 +10,12 @@ import retrofit2.http.POST
 interface ManagePasswordApi {
 
     @FormUrlEncoded
-    @Headers("x-tkpd-path: /${ManagePasswordApiUrl.ADD_PASSWORD}")
     @POST(ManagePasswordApiUrl.ADD_PASSWORD)
     suspend fun createPassword(
             @FieldMap params: HashMap<String, Any>
     ): AddPasswordDataModel
 
     @FormUrlEncoded
-    @Headers("x-tkpd-path: /${ManagePasswordApiUrl.FORGOT_PASSWORD}")
     @POST(ManagePasswordApiUrl.FORGOT_PASSWORD)
     suspend fun resetPassword(
             @FieldMap params: HashMap<String, Any>
