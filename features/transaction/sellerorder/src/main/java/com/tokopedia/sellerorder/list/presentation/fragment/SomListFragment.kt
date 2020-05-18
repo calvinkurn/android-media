4460f5fa6a8aa13483d92a6c67829bc3efe240ff
Felicia Widjaja <fwidjaja@live.com>
2020-05-18 15:22:23 +0700

    delete unused code

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/list/presentation/fragment/SomListFragment.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/list/presentation/fragment/SomListFragment.kt
@@ -5,7 +5,6 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
