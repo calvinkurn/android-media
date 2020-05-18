04b451b85fb302f79357d8bd36e0441348690722
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2020-05-18 14:09:18 +0700

    fix category item spacing decoration

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/CategoryWidgetSpacingItemDecoration.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/CategoryWidgetSpacingItemDecoration.kt
@@ -3,6 +3,7 @@ package com.tokopedia.home.beranda.presentation.view.adapter.itemdecoration
 import android.graphics.Rect
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import com.tokopedia.home.R
 
 /**
  * @author by DevAra on 18/05/20.
@@ -15,7 +16,7 @@ class CategoryWidgetSpacingItemDecoration(private val spanCount: Int, private va
         val column = position % spanCount // item column
 
         if (position == 0 || position == 1) {
-            outRect.left = spacing
+            outRect.left = view.context.resources.getDimensionPixelOffset(R.dimen.dp_14)
         } else {
             outRect.left = spacing/2
         }
