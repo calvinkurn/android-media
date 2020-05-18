2158a4a8fd98fb81c77301ae5ca2dfc2d3d3b9b6
sandeepgupta-xps <sandeep.gupta@tokopedia.com>
2020-05-18 20:13:43 +0530

    reformatted code and comments updated.

--- tools/script_detect_duplicate_res.py
+++ tools/script_detect_duplicate_res.py
@@ -13,9 +13,9 @@ from fnmatch import fnmatch
 
 # name_to_val = dict()
 
-#This function find all the images in the directory pass as a parameter and create the file similar_image_list.txt with duplicate image list
+# This function find all the images in the directory passed as a parameter
 def findDuplicateImages(directory_path, image_to_search_list):
-    #patterns of file to compare
+    # Patterns of file to compare
     patternPng = "*.png"
     patternJpg = "*.jpg"
     patternWebp = "*.webp"
@@ -25,9 +25,12 @@ def findDuplicateImages(directory_path, image_to_search_list):
 
     for path, subdirs, files in os.walk(directory_path):
         for filename in files:
-            if path.find("/build/") == -1 and (fnmatch(filename, patternPng) or fnmatch(filename, patternJpg) or fnmatch(filename, patternWebp) or fnmatch(filename, patternXml)):
+            if path.find("/build/") == -1 and (
+                    fnmatch(filename, patternPng) or
+                    fnmatch(filename, patternJpg) or
+                    fnmatch(filename, patternWebp) or
+                    fnmatch(filename, patternXml)):
                 fln=path+"/"+filename
-                # print(fln)
                 if os.path.isfile(fln):
                     with open(fln, 'rb') as f:
                         file_val = hashlib.md5(f.read()).hexdigest()
@@ -37,14 +40,13 @@ def findDuplicateImages(directory_path, image_to_search_list):
                     else:
                         complete_list[file_val].append(fln)
 
-    #Validate the list passed
+    # Validate the list passed
     for sf in image_to_search_list:
         sf = directory_path + "/" + sf
         if sf in name_to_val :
             find_file_entry = name_to_val[sf]
             if len(complete_list[find_file_entry]) > 1 :
                 print("similar image/s found for file : " + sf)
-                # print(complete_list[find_file_entry])
                 for file_item in complete_list[find_file_entry]:
                     if file_item != sf :
                         print(file_item)
@@ -76,5 +78,5 @@ else:
     image_to_search_list = [item for item in args.list.split(',')]
 
 
-#call this function to find duplicates images in directory
+# Call this function to find duplicates images in directory
 findDuplicateImages(all_images_path, image_to_search_list)
