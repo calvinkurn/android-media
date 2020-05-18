0a2e26a9c067fe11f087bf6f1caf0b2a2ba6cd07
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-05-18 13:44:19 +0700

    clean code

--- features/user/managepassword/src/main/java/com/tokopedia/managepassword/addpassword/view/fragment/AddPasswordFragment.kt
+++ features/user/managepassword/src/main/java/com/tokopedia/managepassword/addpassword/view/fragment/AddPasswordFragment.kt
@@ -68,10 +68,7 @@ class AddPasswordFragment : BaseDaggerFragment() {
 
         txtPassword?.textFieldInput?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-//                viewModel.validatePassword(s)
-            }
-
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable) {
                 viewModel.validatePassword(s)
             }
