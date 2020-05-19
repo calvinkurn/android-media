4ced04d16343ccb49fc22e5b6ced33f839d2f773
isfaaghyth <isfaaghyth@gmail.com>
2020-05-19 11:21:39 +0700

    feat(notifcenter:created): base bottomsheet dialog for notifCenter

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/ProductCardListDialog.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/ProductCardListDialog.kt
@@ -1,23 +1,19 @@
 package com.tokopedia.notifcenter.presentation.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.data.mapper.MultipleProductCardMapper
 import com.tokopedia.notifcenter.data.state.SourceMultipleProductView
 import com.tokopedia.notifcenter.data.viewbean.NotificationItemViewBean
 import com.tokopedia.notifcenter.listener.NotificationItemListener
+import com.tokopedia.notifcenter.presentation.BaseBottomSheet
 import com.tokopedia.notifcenter.presentation.adapter.MultipleProductCardAdapter
 import com.tokopedia.notifcenter.presentation.adapter.typefactory.product.MultipleProductCardFactoryImpl
-import com.tokopedia.unifycomponents.BottomSheetUnify
 import kotlinx.android.synthetic.main.dialog_notification_list_product.*
 
 class ProductCardListDialog(
         private val element: NotificationItemViewBean,
         listener: NotificationItemListener
-): BottomSheetUnify() {
+): BaseBottomSheet() {
 
     private val productCardFactory = MultipleProductCardFactoryImpl(
             sourceView = SourceMultipleProductView.BottomSheetDetail,
@@ -28,31 +24,18 @@ class ProductCardListDialog(
         MultipleProductCardAdapter(productCardFactory)
     }
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val contentView = View.inflate(
-                requireContext(),
-                R.layout.dialog_notification_list_product,
-                null
-        )
-        setChild(contentView)
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun contentView(): Int {
+        return R.layout.dialog_notification_list_product
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun renderView() {
+        // dialog properties
         isFullpage = true
-        renderView()
-    }
 
-    private fun renderView() {
+        // content
         txtTitle?.text = element.title
         txtDescription?.text = element.body
 
-        //product list item
         lstProduct?.adapter = productCardAdapter
         productCardAdapter.insertData(
                 MultipleProductCardMapper.map(element)
