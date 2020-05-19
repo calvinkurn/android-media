b1d937acf89fd7642915f8b0ab305dd4e369baf0
mzennis <meyta.taliti@tokopedia.com>
2020-05-19 11:49:25 +0700

    init module

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayPrepareBroadcastActivity.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.play.broadcaster.view
+
+import android.os.Bundle
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.play.broadcaster.R
+
+
+/**
+ * Created by mzennis on 19/05/20.
+ */
+class PlayPrepareBroadcastActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_prepare_broadcast)
+    }
+}
