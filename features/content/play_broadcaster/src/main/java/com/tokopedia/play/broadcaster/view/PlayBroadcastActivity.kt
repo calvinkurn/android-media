af7feb012580f1fae24f5874643ea55143622e66
mzennis <meyta.taliti@tokopedia.com>
2020-05-20 14:42:23 +0700

    configure init page

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastActivity.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastActivity.kt
@@ -1,10 +1,15 @@
 package com.tokopedia.play.broadcaster.view
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.play.broadcaster.R
+import org.jetbrains.annotations.TestOnly
 
 
 /**
@@ -12,13 +17,39 @@ import com.tokopedia.play.broadcaster.R
  */
 class PlayBroadcastActivity: BaseActivity() {
 
+    companion object {
+
+        @TestOnly
+        fun newIntent(context: Context, channelId: String): Intent {
+            return Intent(context, PlayBroadcastActivity::class.java)
+                    .apply {
+                        // TODO("ask @Rafiando for applink url")
+                        data = Uri.parse("tokopedia://play-broadcaster/$channelId")
+                    }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_broadcaster)
 
-        dummyCheckPermission()
+        dummyCheckPermission() // TODO("testing only")
+
+        val channelId = intent?.data?.lastPathSegment
+        setupView(channelId)
     }
 
+    private fun setupView(channelId: String?) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_fragment, getFragment(channelId))
+                .commit()
+    }
+
+    private fun getFragment(channelId: String?): Fragment {
+        return PlayBroadcastFragment.newInstance(channelId)
+    }
+
+    // TODO("testing only")
     private fun dummyCheckPermission() {
         ActivityCompat.requestPermissions(this,
                 arrayOf(
--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastFragment.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastFragment.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.play.broadcaster.view
 
+import android.os.Bundle
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 
 
@@ -7,6 +8,19 @@ import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
  * Created by mzennis on 19/05/20.
  */
 class PlayBroadcastFragment: BaseDaggerFragment() {
+
+    companion object {
+
+        const val PLAY_KEY_CHANNEL_ID = "channelId"
+
+        fun newInstance(channelId: String?): PlayBroadcastFragment {
+            return PlayBroadcastFragment().apply {
+                val args = Bundle()
+                args.putString(PLAY_KEY_CHANNEL_ID, channelId)
+                arguments = args
+            }
+        }
+    }
 
     override fun getScreenName(): String = "Play Broadcast"
 
