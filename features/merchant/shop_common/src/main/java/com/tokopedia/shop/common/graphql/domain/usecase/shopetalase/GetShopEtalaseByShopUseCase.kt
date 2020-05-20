b7a4c95de5ba901318d26d6b10891641f6b3257f
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2020-05-20 12:55:16 +0700

    change load raw string to string

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/domain/usecase/shopetalase/GetShopEtalaseByShopUseCase.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/domain/usecase/shopetalase/GetShopEtalaseByShopUseCase.kt
@@ -5,7 +5,6 @@ import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.GraphqlConstant
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
-import com.tokopedia.shop.common.R
 import com.tokopedia.shop.common.graphql.data.shopetalase.ShopEtalaseModel
 import com.tokopedia.shop.common.graphql.data.shopetalase.gql.ShopEtalaseByShopQuery
 import com.tokopedia.shop.common.graphql.domain.mapper.GraphQLResultMapper
@@ -20,10 +19,33 @@ class GetShopEtalaseByShopUseCase @Inject
 constructor(@ApplicationContext context: Context) : UseCase<ArrayList<ShopEtalaseModel>>() {
     private val graphQLUseCase: SingleGraphQLUseCase<ShopEtalaseByShopQuery>
 
+    fun getQueryString():String{
+        return """
+            query shopShowcasesByShopID(${'$'}shopId:String!,${'$'}hideNoCount:Boolean,${'$'}hideShowcaseGroup:Boolean,${'$'}isOwner:Boolean) {
+              shopShowcasesByShopID(shopId:${'$'}shopId, hideNoCount:${'$'}hideNoCount, hideShowcaseGroup:${'$'}hideShowcaseGroup, isOwner:${'$'}isOwner) {
+                  result {
+                      id
+                      name
+                      count
+                      type
+                      highlighted
+                      alias
+                      useAce
+                      badge
+                  }
+                  error {
+                      message
+                  }
+              }
+            }
+        """.trimIndent()
+    }
     init {
         graphQLUseCase = object : SingleGraphQLUseCase<ShopEtalaseByShopQuery>(context, ShopEtalaseByShopQuery::class.java) {
-            override val graphQLRawResId: Int
-                get() = R.raw.gql_query_shop_etalase_by_shop
+
+            override fun getRawString(): String {
+                return getQueryString()
+            }
 
             override fun createGraphQLVariable(requestParams: RequestParams): HashMap<String, Any> {
                 val variables = HashMap<String, Any>()
--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/domain/usecase/shopnotes/AddShopNoteUseCase.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/graphql/domain/usecase/shopnotes/AddShopNoteUseCase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 
 import com.google.gson.Gson
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.shop.common.R
 import com.tokopedia.shop.common.graphql.data.shopnote.gql.AddShopNoteMutation
 import com.tokopedia.shop.common.graphql.domain.mapper.GraphQLSuccessMapper
@@ -25,8 +26,9 @@ constructor(@ApplicationContext context: Context) : UseCase<String>() {
 
     init {
         graphQLUseCase = object : SingleGraphQLUseCase<AddShopNoteMutation>(context, AddShopNoteMutation::class.java) {
-            override val graphQLRawResId: Int
-                get() = R.raw.gql_mutation_add_shop_note
+            override fun getRawString(): String {
+                return GraphqlHelper.loadRawString(context.resources, R.raw.gql_mutation_add_shop_note)
+            }
 
             override fun createGraphQLVariable(requestParams: RequestParams): HashMap<String, Any> {
                 val variables = HashMap<String, Any>()
