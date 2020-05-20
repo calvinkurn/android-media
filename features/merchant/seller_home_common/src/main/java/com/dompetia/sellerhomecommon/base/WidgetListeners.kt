3396adedd9f3b33f16c9a22ddc42b2aad731b7a7
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-05-20 10:46:17 +0800

    add widget listener interface

---
+++ features/merchant/seller_home_common/src/main/java/com/dompetia/sellerhomecommon/base/WidgetListeners.kt
@@ -0,0 +1,11 @@
+package com.dompetia.sellerhomecommon.base
+
+import com.dompetia.sellerhomecommon.presentation.view.viewholder.CardViewHolder
+import com.dompetia.sellerhomecommon.presentation.view.viewholder.CarouselViewHolder
+
+/**
+ * Created By @ilhamsuaib on 19/05/20
+ */
+
+interface WidgetListener : CardViewHolder.Listener, CarouselViewHolder.Listener {
+}
