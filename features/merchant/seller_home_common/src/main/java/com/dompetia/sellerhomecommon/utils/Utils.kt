2e33a273390a3fe9f02d95f4b938102efc3f821f
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-05-20 15:11:00 +0800

    add data key util

---
+++ features/merchant/seller_home_common/src/main/java/com/dompetia/sellerhomecommon/utils/Utils.kt
@@ -0,0 +1,14 @@
+package com.dompetia.sellerhomecommon.utils
+
+import com.dompetia.sellerhomecommon.presentation.model.BaseWidgetUiModel
+
+/**
+ * Created By @ilhamsuaib on 20/05/20
+ */
+
+object Utils {
+
+    inline fun <reified T : BaseWidgetUiModel<*>> getWidgetDataKeys(widgets: List<BaseWidgetUiModel<*>>?): List<String> {
+        return widgets.orEmpty().filterIsInstance<T>().map { it.dataKey }
+    }
+}
