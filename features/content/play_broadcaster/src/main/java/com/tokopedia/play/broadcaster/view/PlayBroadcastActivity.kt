da0757697f2690e3c47be850b1a8cdef0c8fd65a
mzennis <meyta.taliti@tokopedia.com>
2020-05-20 14:26:23 +0700

    add permission to access camera, record audio and access storage

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastActivity.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/PlayBroadcastActivity.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.play.broadcaster.view
 
+import android.Manifest
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.play.broadcaster.R
 
@@ -13,5 +15,17 @@ class PlayBroadcastActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_broadcaster)
+
+        dummyCheckPermission()
+    }
+
+    private fun dummyCheckPermission() {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO),
+                2334)
     }
 }
