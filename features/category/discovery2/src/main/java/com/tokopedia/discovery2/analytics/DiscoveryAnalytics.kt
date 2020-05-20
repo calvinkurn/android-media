c7c9038c2505cc212fd67dbddee1299f8dacaf6f
kuldeepyadav <kuldeep.yadav@tokopedia.com>
2020-05-20 11:35:42 +0530

    analytics changes

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/analytics/DiscoveryAnalytics.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/analytics/DiscoveryAnalytics.kt
@@ -1,126 +1,174 @@
 package com.tokopedia.discovery2.analytics
 
-import com.tokopedia.analyticconstant.DataLayer
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.CLAIM_COUPON_CLICK
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.CLAIM_COUPON_IMPRESSION
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.CLICK_BACK_BUTTON_ACTION
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.CLICK_BUTTON_CLAIM_COUPON_ACTION
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.CLICK_SOCIAL_SHARE_ACTION
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.EVENT_CLICK
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.EVENT_CLICK_COUPON
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.EVENT_VIEW
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.KEY_ECOMMERCE
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.KEY_EVENT
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.KEY_EVENT_ACTION
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.KEY_EVENT_CATEGORY
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.KEY_EVENT_LABEL
-import com.tokopedia.discovery2.analytics.DiscoveryAnalyticsConstants.Companion.NONE_OTHER
 import com.tokopedia.discovery2.data.DataItem
 import com.tokopedia.track.TrackApp
+import com.tokopedia.track.interfaces.Analytics
 import com.tokopedia.trackingoptimizer.TrackingQueue
 
-class DiscoveryAnalytics {
+class DiscoveryAnalytics(private val pageType: String = "",
+                         private val pagePath: String = "") {
+
+    private var eventDiscoveryCategory: String = "$VALUE_DISCOVERY_PAGE - $pageType - $pagePath"
+
+    private fun getTracker(): Analytics {
+        return TrackApp.getInstance().gtm
+    }
 
     companion object {
-        val pageType: String = ""
-        val pagePath: String = ""
+        val discoveryAnalytics: DiscoveryAnalytics by lazy { DiscoveryAnalytics() }
+    }
 
-        //20
-        @JvmStatic
-        fun trackBackClick() {
-            val tracker = TrackApp.getInstance().gtm
-            val map = DataLayer.mapOf(
-                    KEY_EVENT, EVENT_CLICK,
-                    KEY_EVENT_CATEGORY, "discovery page - $pageType - $pagePath",
-                    KEY_EVENT_ACTION, CLICK_BACK_BUTTON_ACTION,
-                    KEY_EVENT_LABEL, "")
-            tracker.sendEnhanceEcommerceEvent(map)
-        }
+    private fun createGeneralClickEvent(eventName: String = EVENT_CLICK_DISCOVERY, eventAction: String,
+                                        eventLabel: String = EMPTY_STRING): MutableMap<String, Any> {
+        val map = mutableMapOf<String, Any>(
+                KEY_EVENT to eventName,
+                KEY_EVENT_CATEGORY to eventDiscoveryCategory,
+                KEY_EVENT_ACTION to eventAction,
+                KEY_EVENT_LABEL to eventLabel)
+        return map
+    }
 
-        //21
-        @JvmStatic
-        fun trackShareClick() {
-            val tracker = TrackApp.getInstance().gtm
-            val map = DataLayer.mapOf(
-                    KEY_EVENT, EVENT_CLICK,
-                    KEY_EVENT_CATEGORY, "discovery page - $pageType - $pagePath",
-                    KEY_EVENT_ACTION, CLICK_SOCIAL_SHARE_ACTION,
-                    KEY_EVENT_LABEL, "")
-            tracker.sendEnhanceEcommerceEvent(map)
-        }
+    private fun createGeneralImpressionEvent(eventName: String = EVENT_PROMO_VIEW, eventAction: String,
+                                             eventLabel: String = EMPTY_STRING): MutableMap<String, Any> {
+        val map = mutableMapOf<String, Any>(
+                KEY_EVENT to eventName,
+                KEY_EVENT_CATEGORY to eventDiscoveryCategory,
+                KEY_EVENT_ACTION to eventAction,
+                KEY_EVENT_LABEL to eventLabel)
+        return map
+    }
 
-        //55
-        @JvmStatic
-        fun trackEventImpressionCoupon(trackingQueue: TrackingQueue, coupons: ArrayList<DataItem>?) {
-            if (!coupons.isNullOrEmpty()) {
-                val list = ArrayList<Map<String, Any>>()
-                for (coupon in coupons) {
-                    val map = HashMap<String, Any>()
-                    coupon.let {
-                        map["id"] = it.id.toString()
-                        map["name"] = "${it.minUsageLabel} - p(x) - promo list - mini coupon"
-                        map["creative"] = it.minUsageLabel.toString()
-                        map["creative_url"] = it.imageUrlMobile ?: NONE_OTHER
-                        map["position"] = coupons.indexOf(it) + 1
-                        map["promo_id"] = it.promoId.toString()
-                        map["promo_code"] = it.couponCode.toString()
-                    }
-                    list.add(map)
-                }
+    //20
+    fun trackBackClick() {
+        val map = createGeneralClickEvent(eventAction = CLICK_BACK_BUTTON_ACTION)
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
 
-                val ecommerce: MutableMap<String, Any>? = DataLayer.mapOf(
-                        "promoView", DataLayer.mapOf(
-                        "promotions", list))
+    //21
+    fun trackShareClick() {
+        val map = createGeneralClickEvent(eventAction = CLICK_SOCIAL_SHARE_ACTION)
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
 
-                val map = DataLayer.mapOf(
-                        KEY_EVENT, EVENT_VIEW,
-                        KEY_EVENT_CATEGORY, "discovery page - $pageType - $pagePath",
-                        KEY_EVENT_ACTION, CLAIM_COUPON_IMPRESSION,
-                        KEY_EVENT_LABEL, "",
-                        KEY_ECOMMERCE, ecommerce)
-                trackingQueue.putEETracking(map as HashMap<String, Any>)
-            }
-        }
-
-        //56
-        @JvmStatic
-        fun trackClickClaimCoupon(couponName: String, promoCode: String) {
-            val tracker = TrackApp.getInstance().gtm
-            val map = DataLayer.mapOf(
-                    KEY_EVENT, EVENT_CLICK,
-                    KEY_EVENT_CATEGORY, "discovery page - $pageType - $pagePath",
-                    KEY_EVENT_ACTION, CLICK_BUTTON_CLAIM_COUPON_ACTION,
-                    KEY_EVENT_LABEL, "$couponName - $promoCode")
-            tracker.sendEnhanceEcommerceEvent(map)
-        }
-
-        //57
-        @JvmStatic
-        fun trackEventClickCoupon(trackingQueue: TrackingQueue, coupon: DataItem, position: Int) {
-            val tracker = TrackApp.getInstance().gtm
+    //41
+    fun trackImpressionIconDynamicComponent(headerName: String, headerPosition: String, icons: ArrayList<DataItem>?, trackingQueue: TrackingQueue) {
+        val map = createGeneralImpressionEvent(eventAction = "$IMPRESSION_ICON_DYNAMIC_COMPONENT - $headerName")
+        if (!icons.isNullOrEmpty()) {
             val list = ArrayList<Map<String, Any>>()
-            coupon.let {
-                list.add(DataLayer.mapOf(
-                        "id", it.id.toString(),
-                        "name", "${it.minUsageLabel} - p(x) - promo list - mini coupon",
-                        "creative", it.minUsageLabel.toString(),
-                        "creative_url", it.imageUrlMobile ?: NONE_OTHER,
-                        "position", position.toString(),
-                        "promo_id", it.promoId.toString(),
-                        "promo_code", it.couponCode.toString()
-                ))
+            for (coupon in icons) {
+                val hashMap = HashMap<String, Any>()
+                coupon.let {
+                    hashMap[KEY_ID] = it.id.toString()
+                    hashMap[KEY_NAME] = "$eventDiscoveryCategory - $VALUE_CATEGORY_ICON - $headerName - $headerPosition"
+                    hashMap[KEY_CREATIVE] = it.categoryLabel
+                    hashMap[KEY_POSITION] = icons.indexOf(it) + 1
+                }
+                list.add(hashMap)
+            }
+            val eCommerce: Map<String, Map<String, ArrayList<Map<String, Any>>>> = mapOf(
+                    EVENT_PROMO_VIEW to mapOf(
+                            KEY_PROMOTIONS to list))
+            map[KEY_E_COMMERCE] = eCommerce
+            trackingQueue.putEETracking(map as HashMap<String, Any>)
+        }
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+    //42
+    fun trackClickIconDynamicComponent(headerName: String, headerPosition: String, icons: ArrayList<DataItem>?, trackingQueue: TrackingQueue) {
+        val map = createGeneralClickEvent(eventName = EVENT_PROMO_CLICK, eventAction = "$CLICK_ICON_DYNAMIC_COMPONENT - $headerName")
+        if (!icons.isNullOrEmpty()) {
+            val list = ArrayList<Map<String, Any>>()
+            for (coupon in icons) {
+                val hashMap = HashMap<String, Any>()
+                coupon.let {
+                    hashMap[KEY_ID] = it.id.toString()
+                    hashMap[KEY_NAME] = "$eventDiscoveryCategory - $VALUE_CATEGORY_ICON - $headerName - $headerPosition"
+                    hashMap[KEY_CREATIVE] = it.categoryLabel
+                    hashMap[KEY_POSITION] = icons.indexOf(it) + 1
+                }
+                list.add(hashMap)
+            }
+            val eCommerce: Map<String, Map<String, ArrayList<Map<String, Any>>>> = mapOf(
+                    EVENT_PROMO_CLICK to mapOf(
+                            KEY_PROMOTIONS to list))
+            map[KEY_E_COMMERCE] = eCommerce
+            trackingQueue.putEETracking(map as HashMap<String, Any>)
+        }
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+    //43
+    fun trackClickSeeAllBannerCarousel() {
+        val map = createGeneralClickEvent(eventAction = BANNER_CAROUSEL_LIHAT_SEMUA_CLICK)
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+    //48
+    fun trackClickCustomTopchat() {
+        val map = createGeneralClickEvent(eventAction = CUSTOM_TOP_CHAT_CLICK)
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+    //49
+    fun trackClickChipsFilter(filterName: String) {
+        val map = createGeneralClickEvent(eventAction = CHIPS_FILTER_CLICK, eventLabel = filterName)
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+
+    //55
+    fun trackEventImpressionCoupon(trackingQueue: TrackingQueue, coupons: ArrayList<DataItem>?) {
+        if (!coupons.isNullOrEmpty()) {
+            val list = ArrayList<Map<String, Any>>()
+            for (coupon in coupons) {
+                val map = HashMap<String, Any>()
+                coupon.let {
+                    map[KEY_ID] = it.id.toString()
+                    map[KEY_NAME] = "${it.minUsageLabel} - p(x) - promo list - mini coupon"
+                    map[KEY_CREATIVE] = it.minUsageLabel.toString()
+                    map[KEY_CREATIVE_URL] = it.imageUrlMobile ?: NONE_OTHER
+                    map[KEY_POSITION] = coupons.indexOf(it) + 1
+                    map[KEY_PROMO_ID] = it.promoId.toString()
+                    map[KEY_PROMO_CODE] = it.couponCode.toString()
+                }
+                list.add(map)
             }
 
-            val promotions: MutableMap<String, Any>? = DataLayer.mapOf(
-                    "promotions", list)
-
-            val map = DataLayer.mapOf(
-                    KEY_EVENT, EVENT_CLICK_COUPON,
-                    KEY_EVENT_CATEGORY, "discovery page - $pageType - $pagePath",
-                    KEY_EVENT_ACTION, CLAIM_COUPON_CLICK,
-                    KEY_EVENT_LABEL, "${coupon.name} - ${coupon.couponCode}",
-                    KEY_ECOMMERCE, promotions)
-            tracker.sendEnhanceEcommerceEvent(map)
+            val ecommerce: Map<String, Map<String, ArrayList<Map<String, Any>>>> = mapOf(
+                    EVENT_PROMO_VIEW to mapOf(
+                            KEY_PROMOTIONS to list))
+            val map = createGeneralImpressionEvent(eventAction = CLAIM_COUPON_IMPRESSION)
+            map[KEY_E_COMMERCE] = ecommerce
+            trackingQueue.putEETracking(map as HashMap<String, Any>)
         }
+    }
+
+    //56
+    fun trackClickClaimCoupon(couponName: String, promoCode: String) {
+        val map = createGeneralClickEvent(eventAction = CLICK_BUTTON_CLAIM_COUPON_ACTION, eventLabel = "$couponName - $promoCode")
+        getTracker().sendEnhanceEcommerceEvent(map)
+    }
+
+    //57
+    fun trackEventClickCoupon(trackingQueue: TrackingQueue, coupon: DataItem, position: Int) {
+        val list = ArrayList<Map<String, Any>>()
+        coupon.let {
+            list.add(mapOf(
+                    KEY_ID to it.id.toString(),
+                    KEY_NAME to "${it.minUsageLabel} - p(x) - promo list - mini coupon",
+                    KEY_CREATIVE to it.minUsageLabel.toString(),
+                    KEY_CREATIVE_URL to (it.imageUrlMobile ?: NONE_OTHER),
+                    KEY_POSITION to position.toString(),
+                    KEY_PROMO_ID to it.promoId.toString(),
+                    KEY_PROMO_CODE to it.couponCode.toString()
+            ))
+        }
+        val promotions: Map<String, ArrayList<Map<String, Any>>> = mapOf(
+                KEY_PROMOTIONS to list)
+        val map = createGeneralClickEvent(eventName = EVENT_CLICK_COUPON, eventAction = CLAIM_COUPON_CLICK,
+                eventLabel = "${coupon.name} - ${coupon.couponCode}")
+        map[KEY_E_COMMERCE] = promotions
+        getTracker().sendEnhanceEcommerceEvent(map)
     }
 }
