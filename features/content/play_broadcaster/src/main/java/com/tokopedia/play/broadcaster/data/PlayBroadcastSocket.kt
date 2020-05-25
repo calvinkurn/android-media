640c2f4d20b3e8bc6c91cf8957b76ce98d351240
mzennis <meyta.taliti@tokopedia.com>
2020-05-25 23:56:02 +0700

    support web socket

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/data/PlayBroadcastSocket.kt
@@ -0,0 +1,34 @@
+package com.tokopedia.play.broadcaster.data
+
+import com.tokopedia.abstraction.common.utils.LocalCacheHandler
+import com.tokopedia.user.session.UserSessionInterface
+import com.tokopedia.websocket.WebSocketResponse
+import javax.inject.Inject
+
+
+/**
+ * Created by mzennis on 24/05/20.
+ */
+class PlayBroadcastSocket @Inject constructor(
+        private val userSessionInterface: UserSessionInterface,
+        private val localCacheHandler: LocalCacheHandler
+) {
+
+    var channelId: String = ""
+    var gcToken: String = ""
+
+    fun connect(onMessageReceived: (WebSocketResponse)-> Unit, onReconnect: () -> Unit, onError: (error: Throwable) -> Unit) {
+    }
+
+    fun send(message: String) {
+
+    }
+
+    fun destroy() {
+
+    }
+
+    companion object {
+        const val KEY_GROUPCHAT_PREFERENCES = "com.tokopedia.groupchat.chatroom.view.presenter.GroupChatPresenter"
+    }
+}
