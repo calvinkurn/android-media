35510c60c5b64c4e7bdc43a86a2e09a24b517362
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-05-26 10:15:12 +0800

    update tooltip to seller home common

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/model/TooltipModel.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.sellerhomecommon.domain.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created By @ilhamsuaib on 21/05/20
+ */
+
+data class TooltipModel(
+        @Expose
+        @SerializedName("title")
+        val title: String?,
+        @Expose
+        @SerializedName("content")
+        val content: String?,
+        @Expose
+        @SerializedName("list")
+        val list: List<TooltipListItemModel>?
+)
+
+data class TooltipListItemModel(
+        @Expose
+        @SerializedName("title")
+        val title: String?,
+        @Expose
+        @SerializedName("description")
+        val description: String?
+)
