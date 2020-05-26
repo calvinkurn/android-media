4577102db0e4b3d5949cb0ed3f0045904f6075eb
yorispr <yoris.prayogo@tokopedia.com>
2020-05-26 19:56:42 +0700

    create layout and viewmodel

--- features/user/additional_check/src/main/java/com/tokopedia/additional_check/view/BottomSheetCheck.kt
+++ features/user/additional_check/src/main/java/com/tokopedia/additional_check/view/BottomSheetCheck.kt
@@ -1,6 +1,89 @@
 package com.tokopedia.additional_check.view
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.additional_check.R
+import com.tokopedia.additional_check.data.GetObjectPojo
+import com.tokopedia.additional_check.di.AdditionalCheckModules
+import com.tokopedia.additional_check.di.AdditionalCheckUseCaseModules
+import com.tokopedia.additional_check.di.DaggerAdditionalCheckComponents
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
+import kotlinx.android.synthetic.main.bottom_sheet_layout.view.*
+import javax.inject.Inject
 
-class BottomSheetCheck() {
 
+class BottomSheetCheck(val mActivity: Context?): BottomSheetUnify() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModelProvider by lazy { ViewModelProviders.of(this, viewModelFactory) }
+    private val viewModel by lazy { viewModelProvider.get(BottomSheetCheckViewModel::class.java) }
+
+    companion object {
+        const val TAG = "BottomSheetCheck"
+
+        fun createInstance(activity: FragmentActivity?): BottomSheetCheck = BottomSheetCheck(activity)
+    }
+
+    fun checkStatus(){
+        activity?.run {
+            println("Check status...........")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        DaggerAdditionalCheckComponents
+                .builder()
+                .baseAppComponent((activity?.application as BaseMainApplication).baseAppComponent)
+                .additionalCheckModules(AdditionalCheckModules(context!!))
+                .additionalCheckUseCaseModules(AdditionalCheckUseCaseModules())
+                .build()
+                .inject(this)
+
+        super.onCreate(savedInstanceState)
+        initViews()
+    }
+
+    fun showBottomSheet(fragmentManager: FragmentManager){
+        show(fragmentManager, TAG)
+    }
+
+    private fun initObserver(){
+        viewModel.getDataResponse.observe(this, Observer {
+            when(it){
+                is Success -> onSuccessCheckData(it.data)
+                is Fail -> onFailCheckData(it.throwable)
+            }
+        })
+    }
+
+    private fun onSuccessCheckData(data: GetObjectPojo){
+
+    }
+
+    private fun onFailCheckData(error: Throwable) {
+
+    }
+
+    private fun initViews(){
+        val viewBottomSheetDialog = View.inflate(context, R.layout.bottom_sheet_layout, null)
+
+        viewBottomSheetDialog.bs_layout_button.setOnClickListener {
+
+        }
+
+        setChild(viewBottomSheetDialog)
+        setCloseClickListener {
+            this.dismiss()
+        }
+    }
 }
--- features/user/additional_check/src/main/java/com/tokopedia/additional_check/view/BottomSheetCheckViewModel.kt
+++ features/user/additional_check/src/main/java/com/tokopedia/additional_check/view/BottomSheetCheckViewModel.kt
@@ -1,5 +1,34 @@
 package com.tokopedia.additional_check.view
 
-class BottomSheetCheckViewModel () {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.additional_check.data.BottomSheetModel
+import com.tokopedia.additional_check.data.GetObjectPojo
+import com.tokopedia.additional_check.domain.usecase.AdditionalCheckUseCase
+import com.tokopedia.sessioncommon.data.LoginToken
+import com.tokopedia.sessioncommon.di.SessionModule
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
+import com.tokopedia.user.session.UserSessionInterface
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+import javax.inject.Named
 
+class BottomSheetCheckViewModel @Inject constructor (@Named(SessionModule.SESSION_MODULE) private val userSession: UserSessionInterface,
+                                                     private val additionalCheckUseCase: AdditionalCheckUseCase,
+                                                     dispatcher: CoroutineDispatcher): BaseViewModel(dispatcher) {
+
+    private val mutableGetDataResponse = MutableLiveData<Result<GetObjectPojo>>()
+    val getDataResponse: LiveData<Result<GetObjectPojo>>
+        get() = mutableGetDataResponse
+
+    fun check() {
+        additionalCheckUseCase.getMockBottomSheetSuccess(onSuccess = {
+            mutableGetDataResponse.value = Success(it)
+        }, onError = {
+            Fail(it)
+        })
+    }
 }
