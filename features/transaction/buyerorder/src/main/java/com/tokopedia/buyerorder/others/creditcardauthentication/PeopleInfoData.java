e915fc01df6ec92b57f5357f89a4a91075292a4a
Felicia Widjaja <fwidjaja@live.com>
2020-05-28 11:12:14 +0700

    fix import PeopleInfoData

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/others/creditcardauthentication/PeopleInfoData.java
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/others/creditcardauthentication/PeopleInfoData.java
@@ -1,4 +1,4 @@
-package com.tokopedia.transaction.others.creditcardauthentication;
+package com.tokopedia.buyerorder.others.creditcardauthentication;
 
 import com.google.gson.annotations.SerializedName;
 import com.tokopedia.abstraction.common.utils.view.MethodChecker;
