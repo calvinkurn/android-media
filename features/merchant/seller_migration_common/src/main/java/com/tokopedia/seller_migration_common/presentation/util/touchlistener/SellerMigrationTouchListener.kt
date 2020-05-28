8cc7761de50912ca7652768eea495d4eab9285e9
leonard2907 <leonard.gunawan@tokopedia.com>
2020-05-28 11:43:10 +0700

    Fix margins and touch listener

--- features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/util/touchlistener/SellerMigrationTouchListener.kt
+++ features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/util/touchlistener/SellerMigrationTouchListener.kt
@@ -9,29 +9,28 @@ import android.widget.TextView
 
 class SellerMigrationTouchListener(private val goToInfoPage: (String) -> Boolean) : View.OnTouchListener {
 
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
         val widget = v as TextView
         val text: Any = widget.text
-                if (text is Spanned) {
-                    val buffer: Spanned = text as Spanned
-                    val action = event!!.action
-                    if (action == MotionEvent.ACTION_UP
-                            || action == MotionEvent.ACTION_DOWN) {
-                        var x = event.x.toInt()
-                        var y = event.y.toInt()
-                        x -= widget.totalPaddingLeft
-                        y -= widget.totalPaddingTop
-                        x += widget.scrollX
-                        y += widget.scrollY
-                        val layout: Layout = widget.layout
-                        val line: Int = layout.getLineForVertical(y)
-                        val off: Int = layout.getOffsetForHorizontal(line, x.toFloat())
-                        val link = buffer.getSpans(off, off, URLSpan::class.java)
-                        if (link.isNotEmpty() && action == MotionEvent.ACTION_UP) {
-                            return goToInfoPage(link.first().url.toString())
-                        }
-                    }
+        if (text is Spanned) {
+            val action = event.action
+            if (action == MotionEvent.ACTION_UP
+                    || action == MotionEvent.ACTION_DOWN) {
+                var x = event.x.toInt()
+                var y = event.y.toInt()
+                x -= widget.totalPaddingLeft
+                y -= widget.totalPaddingTop
+                x += widget.scrollX
+                y += widget.scrollY
+                val layout: Layout = widget.layout
+                val line: Int = layout.getLineForVertical(y)
+                val off: Int = layout.getOffsetForHorizontal(line, x.toFloat())
+                val link = text.getSpans(off, off, URLSpan::class.java)
+                if (link.isNotEmpty() && action == MotionEvent.ACTION_UP) {
+                    return goToInfoPage(link.first().url.toString())
                 }
-        return false
+            }
+        }
+        return true
     }
 }
