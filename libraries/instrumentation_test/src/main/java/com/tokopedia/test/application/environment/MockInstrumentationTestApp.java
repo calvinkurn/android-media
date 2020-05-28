77e7d572d6f8eb42959acbdd489aa7fc9af084da
yoasfs <yoas.fs@gmail.com>
2020-05-28 14:20:18 +0700

    revert

--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/MockInstrumentationTestApp.java
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/MockInstrumentationTestApp.java
@@ -151,6 +151,21 @@ public void sendAnalyticsAnomalyResponse(String title,
     }
 
     @Override
+    public Intent getActivitySellingTransactionShippingStatusReal(Context mContext) {
+        return null;
+    }
+
+    @Override
+    public Class getSellingActivityClassReal() {
+        return null;
+    }
+
+    @Override
+    public Intent getActivitySellingTransactionListReal(Context mContext) {
+        return null;
+    }
+
+    @Override
     public Intent getHomeIntent(Context context) {
         return null;
     }
