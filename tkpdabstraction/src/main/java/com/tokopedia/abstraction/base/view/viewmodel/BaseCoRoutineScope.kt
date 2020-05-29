aa1236a6dae7152543d0ef3c96aa0a3b34704e7c
varun0922 <varun.sharma@tokopedia.com>
2020-05-29 18:24:32 +0530

    formatted file

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/viewmodel/BaseCoRoutineScope.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/viewmodel/BaseCoRoutineScope.kt
@@ -6,19 +6,19 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.isActive
 import kotlin.coroutines.CoroutineContext
 
-abstract class BaseCoRoutineScope(private val baseDispatcher: CoroutineDispatcher): CoroutineScope{
-protected val masterJob = SupervisorJob()
+abstract class BaseCoRoutineScope(private val baseDispatcher: CoroutineDispatcher) : CoroutineScope {
+    protected val masterJob = SupervisorJob()
 
-        override val coroutineContext: CoroutineContext
+    override val coroutineContext: CoroutineContext
         get() = baseDispatcher + masterJob
 
-        open fun flush(){
-        if (isActive && !masterJob.isCancelled){
-        masterJob.children.map { it.cancel() }
+    open fun flush() {
+        if (isActive && !masterJob.isCancelled) {
+            masterJob.children.map { it.cancel() }
         }
-        }
+    }
 
-        open fun onCleared() {
+    open fun onCleared() {
         flush()
-        }
-        }
+    }
+}
