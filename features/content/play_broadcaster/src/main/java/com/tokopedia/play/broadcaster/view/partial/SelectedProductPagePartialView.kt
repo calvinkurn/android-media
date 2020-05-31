aa9be26e5136e99f05377758f1b8a9938d6edd50
kenny <kenny.hadisaputra@tokopedia.com>
2020-05-31 21:47:58 +0700

    fix bottom sheet behavior logic

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/partial/SelectedProductPagePartialView.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/partial/SelectedProductPagePartialView.kt
@@ -1,9 +1,12 @@
 package com.tokopedia.play.broadcaster.view.partial
 
 import android.content.Context
+import android.os.Build
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -17,7 +20,7 @@ import com.tokopedia.play.broadcaster.view.uimodel.ProductUiModel
  * Created by jegul on 28/05/20
  */
 class SelectedProductPagePartialView(
-        container: ViewGroup,
+        private val container: ViewGroup,
         listener: Listener
 ) {
 
@@ -25,9 +28,10 @@ class SelectedProductPagePartialView(
         get() = bottomSheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED
 
     private val clSelectedProduct: ConstraintLayout = container.findViewById(R.id.cl_selected_product)
-    private val vDragIndicator: View = container.findViewById(R.id.v_drag_indicator)
+    private val vDragArea: View = container.findViewById(R.id.v_drag_area)
     private val tvSelectedProductTitle: TextView = container.findViewById(R.id.tv_selected_product_title)
     private val rvSelectedProduct: RecyclerView = container.findViewById(R.id.rv_selected_product)
+    private val parentCoordinator = clSelectedProduct.parent as View
 
     private val context: Context
         get() = clSelectedProduct.context
@@ -48,11 +52,38 @@ class SelectedProductPagePartialView(
         rvSelectedProduct.adapter = selectableProductAdapter
         rvSelectedProduct.addItemDecoration(PlayGridTwoItemDecoration(context))
 
+
+        vDragArea.setOnTouchListener(object : View.OnTouchListener {
+
+            @RequiresApi(Build.VERSION_CODES.KITKAT)
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                println("DragTouch: ${MotionEvent.actionToString(event.action)}")
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        parentCoordinator.parent.requestDisallowInterceptTouchEvent(true)
+                        return true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        val currentY = event.rawY.toInt()
+                        bottomSheetBehavior.peekHeight = container.height - currentY
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        parentCoordinator.parent.requestDisallowInterceptTouchEvent(false)
+                        if (bottomSheetBehavior.peekHeight >= 0.6 * parentCoordinator.height) show()
+                        else hide()
+                    }
+                }
+
+                return false
+            }
+        })
+
         hide()
     }
 
     fun show() {
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetBehavior.peekHeight = parentCoordinator.height
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
     fun hide() {
