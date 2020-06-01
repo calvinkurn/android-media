0b6a317fe37adea3eca89649c1e40a6742095273
kuldeepyadav <kuldeep.yadav@tokopedia.com>
2020-06-01 13:41:31 +0530

    youtube fix

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/youtubeview/YoutubeViewViewHolder.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/youtubeview/YoutubeViewViewHolder.kt
@@ -1,27 +1,25 @@
 package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.youtubeview
 
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerSupportFragment
 import com.tokopedia.discovery2.R
 import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
 import com.tokopedia.discovery2.viewcontrollers.adapter.viewholder.AbstractViewHolder
 import com.tokopedia.discovery2.viewcontrollers.fragment.DiscoveryFragment
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.unifycomponents.ImageUnify
-import com.tokopedia.youtubeutils.common.YoutubePlayerConstant
 
 
-class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : AbstractViewHolder(itemView), YouTubePlayer.OnInitializedListener {
+class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : AbstractViewHolder(itemView) {
 
-    private var youTubePlayerSupportFragment: YouTubePlayerSupportFragment =
-            fragment.activity?.supportFragmentManager?.findFragmentById(R.id.youtube_player_fragment) as YouTubePlayerSupportFragment
+    private var youtubeWebview: WebView = itemView.findViewById(R.id.youtube_webview)
     private val shimmerView: ImageUnify = itemView.findViewById(R.id.shimmer_view)
     private lateinit var youTubeViewViewModel: YouTubeViewViewModel
-    private lateinit var youTubePlayer: YouTubePlayer
     private lateinit var videoId: String
 
 
@@ -33,32 +31,30 @@ class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : Ab
     private fun setUpObserver() {
         youTubeViewViewModel.getVideoId().observe(fragment.viewLifecycleOwner, Observer {
             videoId = it.videoId ?: ""
-            youTubePlayerSupportFragment.initialize(YoutubePlayerConstant.GOOGLE_API_KEY, this)
+            showVideoInWebView()
             (fragment as? DiscoveryFragment)?.getDiscoveryAnalytics()?.trackClickVideo(it.videoId
                     ?: "", it.name ?: "", "")
         })
 
     }
 
-
-    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
-
-        if (!wasRestored) {
-            if (player != null) {
-                youTubePlayer = player
+    private fun showVideoInWebView() {
+        youtubeWebview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return false
             }
-            if (::videoId.isInitialized) {
-                shimmerView.hide()
-                youTubePlayer.cueVideo(videoId)
-            }
-            youTubePlayer.setShowFullscreenButton(false)
         }
-
+        val webSettings: WebSettings = youtubeWebview.settings
+        webSettings.javaScriptEnabled = true
+        val mimeType = "text/html"
+        val encoding = "UTF-8"
+        youtubeWebview.webChromeClient = object : WebChromeClient() {}
+        val html: String = getHTML()
+        youtubeWebview.loadData(html, mimeType, encoding)
+        shimmerView.hide()
     }
 
-    override fun onInitializationFailure(provider: YouTubePlayer.Provider?, player: YouTubeInitializationResult?) {
-
+    private fun getHTML(): String {
+        return "<html><body><iframe width=100% height=100% src=\"https://www.youtube.com/embed/$videoId\" frameborder=\"0\"></iframe></body></html>"
     }
-
-
 }
