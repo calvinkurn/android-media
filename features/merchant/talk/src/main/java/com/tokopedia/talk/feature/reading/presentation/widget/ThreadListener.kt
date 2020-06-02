82a33ad938946059ab1525a66279fe8f83a1dfc1
leonard2907 <leonard.gunawan@tokopedia.com>
2020-06-02 12:03:29 +0700

    Redirect to shop page if its seller in reading page

--- features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reading/presentation/widget/ThreadListener.kt
+++ features/merchant/talk/src/main/java/com/tokopedia/talk/feature/reading/presentation/widget/ThreadListener.kt
@@ -2,6 +2,6 @@ package com.tokopedia.talk.feature.reading.presentation.widget
 
 interface ThreadListener {
     fun onThreadClicked(questionID: String)
-    fun onUserDetailsClicked(userId: String)
+    fun onUserDetailsClicked(userId: String, isSeller: Boolean, shopId: String)
     fun onLinkClicked(link: String) : Boolean
 }
