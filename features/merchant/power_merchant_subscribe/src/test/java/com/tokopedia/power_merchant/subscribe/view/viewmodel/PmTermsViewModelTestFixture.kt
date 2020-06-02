5824c4ef824240bc7503ee53e3459311852f27cf
rezagama <reza.hidayat@tokopedia.com>
2020-06-02 10:18:54 +0700

    Import view state

--- features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/PmTermsViewModelTestFixture.kt
+++ features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/PmTermsViewModelTestFixture.kt
@@ -3,8 +3,8 @@ package com.tokopedia.power_merchant.subscribe.view.viewmodel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.gm.common.domain.interactor.ActivatePowerMerchantUseCase
 import com.tokopedia.power_merchant.subscribe.common.coroutine.TestCoroutineDispatchers
-import com.tokopedia.power_merchant.subscribe.view.viewmodel.ViewState.*
 import com.tokopedia.power_merchant.subscribe.verification.verifyValueEquals
+import com.tokopedia.power_merchant.subscribe.view.model.ViewState.HideLoading
 import com.tokopedia.usecase.RequestParams
 import io.mockk.coEvery
 import io.mockk.coVerify
