6120408775bc117e329cae197ca89ec3ddcc5e95
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-06-03 10:42:51 +0700

    remove toast error when fav numbers get null list

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/fragment/DigitalBaseTelcoFragment.kt
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/fragment/DigitalBaseTelcoFragment.kt
@@ -294,7 +294,6 @@ abstract class DigitalBaseTelcoFragment : BaseTopupBillsFragment() {
 
     override fun onFavoriteNumbersError(error: Throwable) {
         errorSetFavNumbers()
-        Toast.makeText(activity, error.message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onCheckVoucherError(error: Throwable) {
