83411d6092b087e855b129afc58e97b948f05bb7
yehezkiel <yehezkiel@tokopedia.com>
2020-06-03 15:31:04 +0700

    revert

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
@@ -1123,7 +1123,7 @@ open class HomeFragment : BaseDaggerFragment(),
     }
 
     private fun injectCouponTimeBased() {
-        if (userSession.isLoggedIn()) viewModel.get().injectCouponTimeBased();
+        if(userSession.isLoggedIn()) viewModel.get().injectCouponTimeBased();
     }
 
     private fun hideLoading() {
