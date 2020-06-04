21b8309ec03bc8b49f98fff10ce6774c6fbb3608
isfaaghyth <isfaaghyth@gmail.com>
2020-06-05 01:21:36 +0700

    feat(media:removed): upload error state

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/state/UploadResult.kt
+++ libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/state/UploadResult.kt
@@ -2,5 +2,5 @@ package com.tokopedia.mediauploader.data.state
 
 sealed class UploadResult {
     class Success(val uploadId: String): UploadResult()
-    class Error(val reason: UploadErrorState, val message: String = ""): UploadResult()
+    class Error(val message: String): UploadResult()
 }
