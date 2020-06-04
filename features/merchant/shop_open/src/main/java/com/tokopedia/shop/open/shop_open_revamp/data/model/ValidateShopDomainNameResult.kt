57509b876d3e837e6137d4e8323d29b9618d6ccb
elscione <yusufhendrawan06@gmail.com>
2020-06-04 20:10:21 +0700

    remove old shop open

--- features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/shop_open_revamp/data/model/ValidateShopDomainNameResult.kt
+++ features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/shop_open_revamp/data/model/ValidateShopDomainNameResult.kt
@@ -2,7 +2,6 @@ package com.tokopedia.shop.open.shop_open_revamp.data.model
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import com.tokopedia.shop.open.data.model.response.ValidateShopDomainNameError
 
 data class ValidateShopDomainNameResult(
 
@@ -18,4 +17,10 @@ data class ValidateDomainShopName(
         @SerializedName("error")
         @Expose
         val error: ValidateShopDomainNameError = ValidateShopDomainNameError()
+)
+
+data class ValidateShopDomainNameError(
+        @SerializedName("message")
+        @Expose
+        val message:String = ""
 )
