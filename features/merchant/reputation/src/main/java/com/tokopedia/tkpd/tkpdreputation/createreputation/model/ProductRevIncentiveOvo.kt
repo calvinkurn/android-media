c90783f4fd7ff0184e4d4b4551a5a28450c0f303
saidfaisal <said.faisal@tokopedia.com>
2020-06-04 07:19:57 +0700

    fix crashlytics for ovo incentive

--- features/merchant/reputation/src/main/java/com/tokopedia/tkpd/tkpdreputation/createreputation/model/ProductRevIncentiveOvo.kt
+++ features/merchant/reputation/src/main/java/com/tokopedia/tkpd/tkpdreputation/createreputation/model/ProductRevIncentiveOvo.kt
@@ -6,5 +6,5 @@ import com.google.gson.annotations.SerializedName
 data class ProductRevIncentiveOvo(
         @SerializedName("productrevIncentiveOvo")
         @Expose
-        val productrevIncentiveOvo: ProductRevIncentiveOvoResponse = ProductRevIncentiveOvoResponse()
+        val productrevIncentiveOvo: ProductRevIncentiveOvoResponse? = ProductRevIncentiveOvoResponse()
 )
