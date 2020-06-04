92e9c3aafab29cc8be01e1debbc12a3dc4fda3e1
Reivin <reivin.oktavianus@tokopedia.com>
2020-06-04 11:44:01 +0700

    Add share capability

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/common/utils/SharingUtil.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/common/utils/SharingUtil.kt
@@ -4,8 +4,15 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.annotation.StringDef
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.tokopedia.kotlin.extensions.view.toBlankOrString
 
 
 @MustBeDocumented
@@ -37,13 +44,35 @@ object SharingUtil {
                            context: Context,
                            urlString: String,
                            messageString: String? = null) {
+        Glide.with(context)
+                .asBitmap()
+                .load(urlString)
+                .listener(object : RequestListener<Bitmap> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        val stringPath = resource?.getSavedImageDirPath(context, System.currentTimeMillis().toString()).toBlankOrString()
+                        goToSocialMedia(packageString, context, stringPath, messageString)
+                        return false
+                    }
+                })
+                .submit()
+
+    }
+
+    private fun goToSocialMedia(@SocmedPackage packageString: String,
+                                context: Context,
+                                pathFile: String,
+                                messageString: String? = null) {
         val intent = context.packageManager.getLaunchIntentForPackage(packageString)
         intent?.run {
             val shareIntent = Intent().apply {
                 action = Intent.ACTION_SEND
                 setPackage(packageString)
                 try {
-                    putExtra(Intent.EXTRA_STREAM, Uri.parse(urlString))
+                    putExtra(Intent.EXTRA_STREAM, Uri.parse(pathFile))
                     messageString?.run {
                         putExtra(Intent.EXTRA_TEXT, this)
                     }
