8624457f717ec2c33838b02817564ed64691e99f
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-06-05 14:04:39 +0700

    remove rupiah format

--- features/entertainment/event_revamp/src/main/java/com/tokopedia/entertainment/pdp/adapter/viewholder/CurrencyFormatter.kt
+++ features/entertainment/event_revamp/src/main/java/com/tokopedia/entertainment/pdp/adapter/viewholder/CurrencyFormatter.kt
@@ -6,11 +6,6 @@ import java.util.*
 object CurrencyFormatter {
     private val localeID = Locale("in", "ID")
     private val rupiahFormat: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
-    fun getRupiahFormat(x: Long): String {
-        rupiahFormat.maximumFractionDigits = 0
-        return if(x!=0L)rupiahFormat.format(x) else "-"
-    }
-
     fun getRupiahAllowZeroFormat(x: Long): String {
         rupiahFormat.maximumFractionDigits = 0
         return rupiahFormat.format(x)
