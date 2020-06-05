c7434b231f34419f4890c2a405669acd589187c8
elscione <yusufhendrawan06@gmail.com>
2020-06-05 16:00:34 +0700

    Make broadcast chat url dynamic

--- features/merchant/seller_home/src/main/java/com/tokopedia/centralizedpromo/view/model/ChatBlastSellerMetadataUiModel.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/centralizedpromo/view/model/ChatBlastSellerMetadataUiModel.kt
@@ -2,5 +2,6 @@ package com.tokopedia.centralizedpromo.view.model
 
 data class ChatBlastSellerMetadataUiModel(
         val promo: Int = 0,
-        val promoType: Int = 0
+        val promoType: Int = 0,
+        val url: String = ""
 )
