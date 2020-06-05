4361efaf0dcb483c4d9542e115de6ab333b8ab92
yogieputra8 <yogie.putra8@gmail.com>
2020-06-05 09:55:27 +0700

    Fix validation

--- features/merchant/shop_showcase/src/main/java/com/tokopedia/shop_showcase/shop_showcase_management/presentation/fragment/ShopShowcaseListReorderFragment.kt
+++ features/merchant/shop_showcase/src/main/java/com/tokopedia/shop_showcase/shop_showcase_management/presentation/fragment/ShopShowcaseListReorderFragment.kt
@@ -168,7 +168,7 @@ class ShopShowcaseListReorderFragment : BaseDaggerFragment(),
         val shopShowcaseList = ArrayList<String>()
         shopShowcaseListReorderAdapter?._showcaseList?.let {
             for (shopShowcaseModel in it) {
-                if (shopShowcaseModel.type == ShowcaseType.GENERATED) {
+                if (shopShowcaseModel.type != ShowcaseType.GENERATED) {
                     shopShowcaseList.add(shopShowcaseModel.id)
                 }
             }
