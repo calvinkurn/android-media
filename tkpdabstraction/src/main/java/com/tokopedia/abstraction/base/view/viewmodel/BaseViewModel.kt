fc578a61eb1c6248c48a0a4e6b7065da8e7358d2
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2020-06-06 20:34:48 +0700

    Change BaseViewModel.kt

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/viewmodel/BaseViewModel.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/viewmodel/BaseViewModel.kt
@@ -2,20 +2,22 @@ package com.tokopedia.abstraction.base.view.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseViewModel(private val baseDispatcher: CoroutineDispatcher): ViewModel(), CoroutineScope{
-    @Deprecated("no longer used")
+    @Deprecated("No longer used")
     protected val masterJob = SupervisorJob()
 
     override val coroutineContext: CoroutineContext
         get() = viewModelScope.coroutineContext + baseDispatcher
 
-    @Deprecated("no longer used")
+    /**
+     * No need to call this on onDestroy activity/fragment
+     * The job is automatically cleared
+     */
+    @Deprecated("Ne need to call this when onDestroy Activity/Fragment")
     open fun flush(){
-        //no-op. already handled in viewModelScope
+        viewModelScope.coroutineContext[Job]?.cancelChildren()
     }
 }
