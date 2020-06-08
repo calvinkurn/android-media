e8dd800c020808935ee485f8c4ac960047f8c228
irfan khoirul <irfankhoirul@gmail.com>
2020-06-08 11:20:31 +0700

    revert hardcoded flag

---
+++ features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/PromoCheckoutActionListener.kt
@@ -0,0 +1,22 @@
+package com.tokopedia.promocheckoutmarketplace.presentation
+
+import com.tokopedia.promocheckoutmarketplace.presentation.uimodel.*
+
+interface PromoCheckoutActionListener {
+
+    fun onClickApplyRecommendedPromo()
+
+    fun onClickApplyManualInputPromo(promoCode: String)
+
+    fun onCLickClearManualInputPromo()
+
+    fun onClickPromoListHeader(element: PromoListHeaderUiModel)
+
+    fun onClickPromoListItem(element: PromoListItemUiModel)
+
+    fun onClickPromoItemDetail(element: PromoListItemUiModel)
+
+    fun onClickPromoEligibilityHeader(element: PromoEligibilityHeaderUiModel)
+
+    fun onClickEmptyStateButton(element: PromoEmptyStateUiModel)
+}
