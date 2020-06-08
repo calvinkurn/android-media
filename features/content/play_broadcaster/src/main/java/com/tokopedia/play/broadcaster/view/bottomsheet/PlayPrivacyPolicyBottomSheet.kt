0147dcff48069aabbd9f2e9ae465d769857bb60c
kenny <kenny.hadisaputra@tokopedia.com>
2020-06-08 17:36:20 +0700

    a little bit adjustment for privacy policy bottom sheet

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/bottomsheet/PlayPrivacyPolicyBottomSheet.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/bottomsheet/PlayPrivacyPolicyBottomSheet.kt
@@ -1,10 +1,10 @@
 package com.tokopedia.play.broadcaster.view.bottomsheet
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.FragmentManager
+import com.tokopedia.kotlin.extensions.view.getScreenHeight
 import com.tokopedia.play.broadcaster.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import javax.inject.Inject
@@ -20,17 +20,37 @@ class PlayPrivacyPolicyBottomSheet @Inject constructor() : BottomSheetUnify() {
         isDragable = false
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        setupView()
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private lateinit var clPrivacyPolicy: ConstraintLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupBottomSheet()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView(view)
     }
 
     fun show(fragmentManager: FragmentManager) {
         show(fragmentManager, TAG)
     }
 
-    private fun setupView() {
+    private fun setupView(view: View) {
+        clPrivacyPolicy.layoutParams = clPrivacyPolicy.layoutParams.apply {
+            height = (getScreenHeight() * 0.6f).toInt()
+        }
+    }
+
+    private fun setupBottomSheet() {
         setTitle(getString(R.string.play_privacy_policy))
+        setChild(getContentView())
+    }
+
+    private fun getContentView(): View {
+        val view = View.inflate(requireContext(), R.layout.bottom_sheet_play_privacy_policy, null)
+        clPrivacyPolicy = view.findViewById(R.id.cl_privacy_policy)
+        return view
     }
 
     companion object {
