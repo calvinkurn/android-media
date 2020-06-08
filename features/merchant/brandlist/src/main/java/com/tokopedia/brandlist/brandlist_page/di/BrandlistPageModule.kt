97e29b283ee643b0c7184f7b5d8516441b365268
yogieputra8 <yogie.putra8@gmail.com>
2020-06-09 03:38:24 +0700

    Remove commented codes

--- features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_page/di/BrandlistPageModule.kt
+++ features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_page/di/BrandlistPageModule.kt
@@ -4,9 +4,6 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.brandlist.R
-//import com.tokopedia.brandlist.common.GQLQueryConstant.QUERY_BRANDLIST_FEATURED_BRAND
-//import com.tokopedia.brandlist.common.GQLQueryConstant.QUERY_BRANDLIST_RECOMMENDATION
-//import com.tokopedia.brandlist.common.GQLQueryConstant.QUERY_BRANDLIST_SEARCH_BRAND
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
