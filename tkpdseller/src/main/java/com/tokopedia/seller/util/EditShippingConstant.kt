c1958a215f6ad9b6aea35b1f6d4c1969263159a0
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-06-08 09:12:35 +0700

    adding temporary link to webview

--- tkpdseller/src/main/java/com/tokopedia/seller/util/EditShippingConstant.kt
+++ tkpdseller/src/main/java/com/tokopedia/seller/util/EditShippingConstant.kt
@@ -6,5 +6,7 @@ import com.tokopedia.url.TokopediaUrl.Companion.getInstance
 object EditShippingConstant {
     val BASE_MOBILE_URL = getInstance().MOBILEWEB
     val URL_TOKOPEDIA_CORNER = BASE_MOBILE_URL + "tokopedia-corner"
+    val URL_BEBAS_ONGKIR = BASE_MOBILE_URL + "bebas-ongkir"
     val APPLINK_TOKOPEDIA_CORNER = String.format("%s?url=%s", ApplinkConst.WEBVIEW, URL_TOKOPEDIA_CORNER)
+    val APPLINK_BEBAS_ONGKIR  = String.format("%s?url=%s", ApplinkConst.WEBVIEW, URL_BEBAS_ONGKIR)
 }
