ded7b4815ba78d9be7792a47925888c05b406b14
rezagama <reza.hidayat@tokopedia.com>
2020-06-09 11:03:14 +0700

    Create free shipping status data class

---
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/ShopFreeShippingStatus.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.shop.common.data.source.cloud.model
+
+import com.google.gson.annotations.SerializedName
+
+class ShopFreeShippingStatus(
+    @SerializedName("EligibleServiceShop")
+    val response: Response
+) {
+
+    data class Response(
+        @SerializedName("shops")
+        val shops: List<Shop>
+    )
+
+    data class Shop(
+        @SerializedName("shopID")
+        val shopId:  Int,
+        @SerializedName("status")
+        val status: Boolean,
+        @SerializedName("statusEligible")
+        val statusEligible: Boolean,
+        @SerializedName("statusSellerApproval")
+        val statusSellerApproval: Boolean
+    ) {
+        fun isEligible(): Boolean {
+            return statusEligible && statusSellerApproval
+        }
+    }
+}
