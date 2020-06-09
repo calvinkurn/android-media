c86c419a26c8dfce4e873ea207216afcec273e26
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-09 16:47:06 +0800

    set activity white status bar

--- features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/activity/StatisticActivity.kt
+++ features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/activity/StatisticActivity.kt
@@ -1,15 +1,19 @@
 package com.tokopedia.statistic.presentation.view.activity
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.kotlin.extensions.view.setLightStatusBar
+import com.tokopedia.kotlin.extensions.view.setStatusBarColor
 import com.tokopedia.statistic.R
 import com.tokopedia.statistic.presentation.view.fragment.StatisticFragment
 
 /**
  * Created By @ilhamsuaib on 08/06/20
- *
- * Internal applink : ApplinkConstInternalMarketplace.MERCHANT_STATISTIC_DASHBOARD
  */
+
+// Internal applink : ApplinkConstInternalMarketplace.MERCHANT_STATISTIC_DASHBOARD
 
 class StatisticActivity : BaseActivity() {
 
@@ -18,11 +22,19 @@ class StatisticActivity : BaseActivity() {
         setContentView(R.layout.activity_stc_statistic)
 
         showFragment()
+        setWhiteStatusBar()
     }
 
     private fun showFragment() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.parent_view_stc, StatisticFragment.newInstance())
                 .commitNowAllowingStateLoss()
+    }
+
+    private fun setWhiteStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setStatusBarColor(Color.WHITE)
+            setLightStatusBar(true)
+        }
     }
 }
