74765544eba891666ef7514db50e17e91b092118
rezagama <reza.hidayat@tokopedia.com>
2020-06-09 11:01:57 +0700

    Add free shipping detail model

---
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/model/PowerMerchantFreeShippingDetail.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.power_merchant.subscribe.view.model
+
+data class PowerMerchantFreeShippingDetail(
+    val isActive: Boolean,
+    val isEligible: Boolean,
+    val isTransitionPeriod: Boolean
+)
