549973cf8807d0ecb8b2e15917949b16f946111c
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-06-09 18:38:00 +0700

    implement 3 dots menu on telco and move voucher game menu to common topup bills module

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/activity/BaseTelcoActivity.kt
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/view/activity/BaseTelcoActivity.kt
@@ -2,11 +2,28 @@ package com.tokopedia.topupbills.telco.view.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.common.topupbills.view.bottomsheet.TopupBillsMenuBottomSheets
 import com.tokopedia.topupbills.R
+import com.tokopedia.topupbills.telco.view.di.DigitalTopupComponent
+import com.tokopedia.user.session.UserSessionInterface
 import timber.log.Timber
+import javax.inject.Inject
 
-open abstract class BaseTelcoActivity : BaseSimpleActivity() {
+open abstract class BaseTelcoActivity : BaseSimpleActivity(), HasComponent<DigitalTopupComponent>,
+        TopupBillsMenuBottomSheets.MenuListener {
+
+    @Inject
+    lateinit var userSession: UserSessionInterface
+
+    private fun initInjector() {
+        component.inject(this)
+    }
 
     override fun getLayoutRes(): Int {
         return R.layout.activity_digital_telco
@@ -22,6 +39,8 @@ open abstract class BaseTelcoActivity : BaseSimpleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initInjector()
         intent?.handleExtra()
 
         //draw background without overdraw GPU
@@ -40,11 +59,52 @@ open abstract class BaseTelcoActivity : BaseSimpleActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_telco, menu)
+        return true
+    }
+
+    override fun onMenuOpened(featureId: Int, menu: Menu?): Boolean {
+        showBottomMenus()
+        return false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId ?: "" == R.id.action_overflow_menu) {
+            showBottomMenus()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onOrderListClicked() {
+        if (userSession.isLoggedIn) {
+        RouteManager.route(this, ApplinkConst.DIGITAL_ORDER)
+        } else {
+            RouteManager.route(this, ApplinkConst.LOGIN)
+        }
+    }
+
+    override fun onPromoClicked() {
+        RouteManager.route(this, ApplinkConst.PROMO_LIST)
+    }
+
+    override fun onHelpClicked() {
+        RouteManager.route(this, ApplinkConst.CONTACT_US_NATIVE)
+    }
+
+    private fun showBottomMenus() {
+        val menuBottomSheet = TopupBillsMenuBottomSheets()
+        menuBottomSheet.listener = this
+        menuBottomSheet.show(supportFragmentManager, TAG_TELCO_MENU)
+    }
+
     companion object {
         const val PARAM_MENU_ID = "menu_id"
         const val PARAM_PRODUCT_ID = "product_id"
         const val PARAM_CLIENT_NUMBER = "client_number"
         const val PARAM_CATEGORY_ID = "category_id"
         const val RECHARGE_PRODUCT_EXTRA = "RECHARGE_PRODUCT_EXTRA"
+        const val TAG_TELCO_MENU = "menu_telco"
     }
 }
