628898f12ff7888a6edd3292ccc4037155872731
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-09 15:35:43 +0800

    add dispatcher in view model

--- features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/viewmodel/StatisticViewModel.kt
+++ features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/viewmodel/StatisticViewModel.kt
@@ -1,8 +1,15 @@
 package com.tokopedia.statistic.presentation.view.viewmodel
 
+import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Named
+
 /**
  * Created By @ilhamsuaib on 08/06/20
  */
 
-class StatisticViewModel {
+class StatisticViewModel(
+        @Named("Main") dispatcher: CoroutineDispatcher
+) : BaseViewModel(dispatcher) {
+
 }
