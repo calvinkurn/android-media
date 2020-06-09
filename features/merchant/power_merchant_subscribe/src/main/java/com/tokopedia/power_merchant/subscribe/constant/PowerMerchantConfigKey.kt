dd65bff0c5067c3d7fd266eca3fe139ab3387094
rezagama <reza.hidayat@tokopedia.com>
2020-06-09 11:00:38 +0700

    Add pm config key

---
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/constant/PowerMerchantConfigKey.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.power_merchant.subscribe.constant
+
+object PowerMerchantConfigKey {
+
+    const val IS_PM_FREE_SHIPPING_IN_TRANSITION_KEY = "is_pm_free_shipping_in_transition"
+}
