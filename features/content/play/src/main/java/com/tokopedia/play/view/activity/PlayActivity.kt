8a99b585cd6220a462c5db242b1a2264acb36818
kenny <kenny.hadisaputra@tokopedia.com>
2020-06-10 10:15:24 +0700

    Fix landscape back

--- features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
@@ -19,10 +19,12 @@ import com.tokopedia.play.*
 import com.tokopedia.play.di.DaggerPlayComponent
 import com.tokopedia.play.di.PlayModule
 import com.tokopedia.play.util.observer.PlayVideoUtilObserver
+import com.tokopedia.play.view.contract.PlayNavigation
 import com.tokopedia.play.view.contract.PlayNewChannelInteractor
 import com.tokopedia.play.view.fragment.PlayFragment
-import com.tokopedia.play_common.util.PlayVideoPlayerObserver
+import com.tokopedia.play.view.type.ScreenOrientation
 import com.tokopedia.play_common.util.PlayProcessLifecycleObserver
+import com.tokopedia.play_common.util.PlayVideoPlayerObserver
 import org.jetbrains.annotations.TestOnly
 import javax.inject.Inject
 
@@ -30,7 +32,7 @@ import javax.inject.Inject
  * Created by jegul on 29/11/19
  * {@link com.tokopedia.applink.internal.ApplinkConstInternalContent#PLAY_DETAIL}
  */
-class PlayActivity : BaseActivity(), PlayNewChannelInteractor {
+class PlayActivity : BaseActivity(), PlayNewChannelInteractor, PlayNavigation {
 
     companion object {
         private const val PLAY_FRAGMENT_TAG = "FRAGMENT_PLAY"
@@ -50,6 +52,9 @@ class PlayActivity : BaseActivity(), PlayNewChannelInteractor {
 
     @Inject
     lateinit var playProcessLifecycleObserver: PlayProcessLifecycleObserver
+
+    private val orientation: ScreenOrientation
+        get() = ScreenOrientation.getByInt(resources.configuration.orientation)
 
     private lateinit var pageMonitoring: PageLoadTimePerformanceInterface
 
@@ -116,20 +121,27 @@ class PlayActivity : BaseActivity(), PlayNewChannelInteractor {
         }
     }
 
-    override fun onBackPressed() {
+    override fun onBackPressed(isSystemBack: Boolean) {
         val fragment = supportFragmentManager.findFragmentByTag(PLAY_FRAGMENT_TAG)
         if (fragment != null && fragment is PlayFragment) {
             if (!fragment.onBackPressed()) {
-                if (isTaskRoot) {
-                    val intent = RouteManager.getIntent(this, ApplinkConst.HOME)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    fragment.setResultBeforeFinish()
-                    supportFinishAfterTransition()
+                if (isSystemBack && orientation.isLandscape) fragment.onOrientationChanged(ScreenOrientation.Portrait, false)
+                else {
+                    if (isTaskRoot) {
+                        val intent = RouteManager.getIntent(this, ApplinkConst.HOME)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        fragment.setResultBeforeFinish()
+                        supportFinishAfterTransition()
+                    }
                 }
             }
         } else super.onBackPressed()
+    }
+
+    override fun onBackPressed() {
+        onBackPressed(true)
     }
 
     override fun onDestroy() {
