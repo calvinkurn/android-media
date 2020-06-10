47cc42c0916202913706fb90ce18a32ac25ae24d
elscione <yusufhendrawan06@gmail.com>
2020-06-10 19:53:29 +0700

    change product list more filter button wording

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/fragment/ProductManageFragment.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/fragment/ProductManageFragment.kt
@@ -1602,6 +1602,7 @@ open class ProductManageFragment : BaseListFragment<ProductViewModel, ProductMan
         if(isLoadingInitialData && showProductEmptyState()) {
             searchBar.showWithCondition(productList.isNotEmpty())
             tabSortFilter.showWithCondition(productList.isNotEmpty())
+            tabSortFilter.textView.text = getString(R.string.product_manage_filter)
         }
     }
 
