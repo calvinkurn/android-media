91e7d3b3bad867d2104ea3dd3a6c573849ed02ac
Rahul Lohra <rahul.lohra@tokopedia.com>
2020-06-10 10:12:13 +0530

    Remove named annotaion, since it was not required

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/data/di/modules/PltModule.kt
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/giftbox/data/di/modules/PltModule.kt
@@ -5,15 +5,12 @@ import com.tokopedia.analytics.performance.util.PageLoadTimePerformanceInterface
 import com.tokopedia.gamification.giftbox.data.di.GAMI_GIFT_DAILY_PLT_NETWORK_METRICS
 import com.tokopedia.gamification.giftbox.data.di.GAMI_GIFT_DAILY_PLT_PREPARE_METRICS
 import com.tokopedia.gamification.giftbox.data.di.GAMI_GIFT_DAILY_PLT_RENDER_METRICS
-import com.tokopedia.gamification.giftbox.data.di.GIFT_BOX_DAILY
 import dagger.Module
 import dagger.Provides
-import javax.inject.Named
 
 @Module
 class PltModule {
 
-    @Named(GIFT_BOX_DAILY)
     @Provides
     fun providePerfInterface(): PageLoadTimePerformanceInterface {
         return PageLoadTimePerformanceCallback(
