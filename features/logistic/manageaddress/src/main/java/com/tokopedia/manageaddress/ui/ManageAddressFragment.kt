20bd8c552f9d27f33dabf841efa2198e34de4353
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-06-10 16:32:06 +0700

    WIP create UI new manage address

--- features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/ui/ManageAddressFragment.kt
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/ui/ManageAddressFragment.kt
@@ -1,15 +1,44 @@
 package com.tokopedia.manageaddress.ui
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.manageaddress.R
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.design.text.SearchInputView
 
-class ManageAddressFragment : BaseDaggerFragment() {
+class ManageAddressFragment : BaseDaggerFragment(), SearchInputView.Listener{
 
-    override fun getScreenName(): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private lateinit var searchAddress: SearchInputView
+    private var swipeRefreshLayout: SwipeRefreshLayout? = null
+
+    override fun getScreenName(): String = ""
 
     override fun initInjector() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_manage_address, container, false)
+        searchAddress = view.findViewById(R.id.search_input_view)
+        return view
+    }
+
+    override fun onSearchSubmitted(text: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSearchTextChanged(text: String?) {
+        openSoftKeyboard()
+    }
+
+    private fun openSoftKeyboard() {
+        (activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.showSoftInput(
+                searchAddress.searchTextView, InputMethodManager.SHOW_IMPLICIT)
     }
 
 }
