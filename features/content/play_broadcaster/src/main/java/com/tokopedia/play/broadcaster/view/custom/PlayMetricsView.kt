0f3f8e8ce8f3578f33f8526923adac403da76c3a
kenny <kenny.hadisaputra@tokopedia.com>
2020-06-10 19:52:13 +0700

    the more performant way

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/PlayMetricsView.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/PlayMetricsView.kt
@@ -11,21 +11,23 @@ import android.text.style.StyleSpan
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.transition.Fade
 import androidx.transition.Slide
 import androidx.transition.TransitionManager
 import androidx.transition.TransitionSet
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.play.broadcaster.R
 import com.tokopedia.play.broadcaster.ui.model.PlayMetricUiModel
-import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by jegul on 10/06/20
  */
-class PlayMetricsView : LinearLayout {
+class PlayMetricsView : RelativeLayout {
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -34,39 +36,44 @@ class PlayMetricsView : LinearLayout {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
-    private var prevView: View? = null
+    private var currentIndex = -1
+    private val textViewList = mutableListOf<TextView>()
 
     init {
         gravity = Gravity.BOTTOM
-        orientation = VERTICAL
+        val view = View.inflate(context, R.layout.item_play_metrics, this)
+        textViewList.add(view.findViewById(R.id.tv_metric_1))
+        textViewList.add(view.findViewById(R.id.tv_metric_2))
     }
 
     fun show(metric: PlayMetricUiModel) {
+        val nextIndex = if (currentIndex == textViewList.size - 1) 0 else currentIndex + 1
+        val currentView = if (currentIndex < 0) null else textViewList[currentIndex]
+        val nextView = textViewList[nextIndex]
+
         val transitionSet = TransitionSet()
-        val previousView = prevView
-        if (previousView != null) {
+        if (currentView != null) {
             transitionSet
                     .addTransition(Slide(Gravity.TOP)
-                            .addTarget(previousView))
+                            .addTarget(currentView))
                     .addTransition(Fade(Fade.OUT)
-                            .addTarget(previousView))
+                            .addTarget(currentView))
         }
 
-        val newView: Typography = View.inflate(context, R.layout.item_play_metrics, null) as Typography
-        newView.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        newView.text = getSpannedMetric(metric)
+        nextView.text = getSpannedMetric(metric)
 
         transitionSet
                 .addTransition(Slide(Gravity.BOTTOM)
-                        .addTarget(newView))
+                        .addTarget(nextView))
                 .addTransition(Fade(Fade.IN)
-                        .addTarget(newView))
+                        .addTarget(nextView))
 
         TransitionManager.beginDelayedTransition(this, transitionSet)
 
-        removeView(previousView)
-        addView(newView)
-        prevView = newView
+        currentView?.gone()
+        nextView.visible()
+
+        currentIndex = nextIndex
     }
 
     private fun getSpannedMetric(metric: PlayMetricUiModel): CharSequence {
