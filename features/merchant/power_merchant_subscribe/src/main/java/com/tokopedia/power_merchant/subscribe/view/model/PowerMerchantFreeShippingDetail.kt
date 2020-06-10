632731a88c69e4ba02b641cdaaa5c1ec8a1ca902
rezagama <reza.hidayat@tokopedia.com>
2020-06-10 21:44:44 +0700

    Add isShopScoreEligible

--- features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/model/PowerMerchantFreeShippingDetail.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.power_merchant.subscribe.view.model
-
-data class PowerMerchantFreeShippingDetail(
-    val isActive: Boolean,
-    val isEligible: Boolean,
-    val isTransitionPeriod: Boolean
-)
