904eb2976ed359ad80b66d19468a031a37da638d
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-06-11 16:38:59 +0700

    fix tracker

--- tkpdseller/src/main/java/com/tokopedia/seller/shopsettings/shipping/analytics/EditShippingAnalytics.kt
+++ tkpdseller/src/main/java/com/tokopedia/seller/shopsettings/shipping/analytics/EditShippingAnalytics.kt
@@ -2,25 +2,28 @@ package com.tokopedia.seller.shopsettings.shipping.analytics
 
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
+import com.tokopedia.track.interfaces.Analytics
 
 object EditShippingAnalytics {
 
-    private const val CLICK_TEXT_DISINI = "clickTextDisini"
+    private const val CLICK_TEXT_DISINI = "clickBebasOngkir"
     private const val BO_SHIPPING_EDITOR = "bo shipping editor"
     private const val CLICK_DISINI = "click disini"
 
-    private fun sendEventCategoryAction(event: String, eventCategory: String,
-                                        eventAction: String) {
-        sendEventCategoryActionLabel(event, eventCategory, eventAction, "")
+    private const val KEY_USER_ID = "userId"
+
+    private fun getTracker(): Analytics {
+        return TrackApp.getInstance().gtm
     }
 
-    private fun sendEventCategoryActionLabel(event: String, eventCategory: String,
-                                             eventAction: String, eventLabel: String) {
-        TrackApp.getInstance().gtm.sendGeneralEvent(TrackAppUtils.gtmData(
-                event, eventCategory, eventAction, eventLabel))
+    fun eventClickonTickerShippingEditor(userId: String) {
+        val map = TrackAppUtils.gtmData(
+                CLICK_TEXT_DISINI,
+                BO_SHIPPING_EDITOR,
+                CLICK_DISINI,
+                "")
+        map[KEY_USER_ID] = userId
+        getTracker().sendGeneralEvent(map)
     }
 
-    fun eventClickonTickerShippingEditor() {
-        sendEventCategoryAction(CLICK_TEXT_DISINI, BO_SHIPPING_EDITOR, CLICK_DISINI)
-    }
 }
