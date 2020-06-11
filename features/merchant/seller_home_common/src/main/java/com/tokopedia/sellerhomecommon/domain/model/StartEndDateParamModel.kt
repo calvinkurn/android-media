75057515d27372d37574b24cbfd6931d60854bad
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-11 18:11:28 +0800

    add data key model and parameter model

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/model/StartEndDateParamModel.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.sellerhomecommon.domain.model
+
+import com.google.gson.Gson
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created By @ilhamsuaib on 11/06/20
+ */
+
+data class StartEndDateParamModel(
+        @Expose
+        @SerializedName("start_date")
+        val startDate: String = "",
+        @Expose
+        @SerializedName("end_date")
+        val endDate: String = "",
+        @Expose
+        @SerializedName("page")
+        val page: Int = 0,
+        @Expose
+        @SerializedName("limit")
+        val limit: Int = 0
+) {
+
+    fun toJsonString(): String {
+        return Gson().toJsonTree(this).toString()
+    }
+}
