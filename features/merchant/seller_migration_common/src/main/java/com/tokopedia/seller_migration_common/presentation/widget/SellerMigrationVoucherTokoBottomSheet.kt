ad5afdb5756460619c62a4cef3135ff83974d4a7
elscione <yusufhendrawan06@gmail.com>
2020-06-12 14:51:16 +0700

    change voucher toko migration image

--- features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/widget/SellerMigrationVoucherTokoBottomSheet.kt
+++ features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/widget/SellerMigrationVoucherTokoBottomSheet.kt
@@ -1,23 +1,27 @@
 package com.tokopedia.seller_migration_common.presentation.widget
 
+import android.content.ContentResolver
 import android.content.Context
+import android.net.Uri
 import android.view.View
 import com.tokopedia.seller_migration_common.R
-import com.tokopedia.seller_migration_common.constants.SellerMigrationConstants
 
 class SellerMigrationVoucherTokoBottomSheet(titles: List<String>,
-                                     contents: List<String>,
-                                     images: ArrayList<String>)
+                                            contents: List<String>,
+                                            images: ArrayList<String>)
     : SellerMigrationBottomSheet(titles, contents, images) {
 
     companion object {
-        fun createNewInstance(context: Context) : SellerMigrationVoucherTokoBottomSheet {
+        fun createNewInstance(context: Context): SellerMigrationVoucherTokoBottomSheet {
             with(context) {
                 val titles = listOf(getString(R.string.seller_migration_voucher_toko_title))
                 val contents = listOf(getString(R.string.seller_migration_voucher_toko_content))
-                val images = arrayListOf(SellerMigrationConstants.SELLER_MIGRATION_PROMO_FIRST_IMAGE)
-                return SellerMigrationVoucherTokoBottomSheet(titles, contents, images).apply{
-                    val view = View.inflate(context, R.layout.widget_seller_migration_bottom_sheet,null)
+                val images = arrayListOf(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                        "://" + context.resources.getResourcePackageName(R.drawable.seller_migration_voucher_toko)
+                        + '/' + context.resources.getResourceTypeName(R.drawable.seller_migration_voucher_toko)
+                        + '/' + context.resources.getResourceEntryName(R.drawable.seller_migration_voucher_toko)).toString())
+                return SellerMigrationVoucherTokoBottomSheet(titles, contents, images).apply {
+                    val view = View.inflate(context, R.layout.widget_seller_migration_bottom_sheet, null)
                     setChild(view)
                 }
             }
