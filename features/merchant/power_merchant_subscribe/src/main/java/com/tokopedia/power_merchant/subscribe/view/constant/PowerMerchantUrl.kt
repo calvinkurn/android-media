9bde26daa40cefa5fe9663434dd9eea035fc2a17
rezagama <reza.hidayat@tokopedia.com>
2020-06-12 18:28:30 +0700

    Add PM url constants

---
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/constant/PowerMerchantUrl.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.power_merchant.subscribe.view.constant
+
+object PowerMerchantUrl {
+
+    const val URL_FREE_SHIPPING_TERMS_AND_CONDITION = "https://seller.tokopedia.com/edu/bebas-ongkir/"
+}
