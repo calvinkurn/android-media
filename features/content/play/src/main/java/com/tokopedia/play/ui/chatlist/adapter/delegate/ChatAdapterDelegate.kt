36e9210e7a5eeeda2023a284e5c4b3e523d43334
kenny <kenny.hadisaputra@tokopedia.com>
2020-06-12 12:58:55 +0700

    remove redundant resource

--- features/content/play/src/main/java/com/tokopedia/play/ui/chatlist/adapter/delegate/ChatAdapterDelegate.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/chatlist/adapter/delegate/ChatAdapterDelegate.kt
@@ -3,14 +3,13 @@ package com.tokopedia.play.ui.chatlist.adapter.delegate
 import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.adapterdelegate.TypedAdapterDelegate
-import com.tokopedia.play.R
 import com.tokopedia.play.ui.chatlist.viewholder.ChatViewHolder
 import com.tokopedia.play.view.uimodel.PlayChatUiModel
 
 /**
  * Created by jegul on 04/12/19
  */
-class ChatAdapterDelegate : TypedAdapterDelegate<PlayChatUiModel, PlayChatUiModel, ChatViewHolder>(R.layout.item_play_chat) {
+class ChatAdapterDelegate : TypedAdapterDelegate<PlayChatUiModel, PlayChatUiModel, ChatViewHolder>(com.tokopedia.play_common.R.layout.item_play_chat) {
 
     override fun onBindViewHolder(item: PlayChatUiModel, holder: ChatViewHolder) {
         holder.bind(item)
