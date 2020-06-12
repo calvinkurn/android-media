026d23d680272bdb7f78d1c553b72b2e7501a393
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-06-12 09:40:29 +0700

    fix error build jenkins

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/data/constant/TelcoComponentName.java
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/data/constant/TelcoComponentName.java
@@ -11,8 +11,8 @@ import java.lang.annotation.RetentionPolicy;
 @StringDef({TelcoComponentName.PRODUCT_PULSA,
         TelcoComponentName.PRODUCT_ROAMING,
         TelcoComponentName.PRODUCT_PAKET_DATA,
-        TelcoComponentName.TELCO_PREPAID,
-        TelcoComponentName.TELCO_POSTPAID})
+        TelcoComponentName.PROMO,
+        TelcoComponentName.RECENTS})
 @Retention(RetentionPolicy.SOURCE)
 public @interface TelcoComponentName {
     String PRODUCT_PULSA = "Pulsa";
