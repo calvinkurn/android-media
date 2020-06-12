d41c166941bbd5a5e2a2427df6667a462f13991a
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-06-12 11:10:20 +0700

    separate usecase for contact load more

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatContactLoadMoreViewModel.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatContactLoadMoreViewModel.kt
@@ -4,26 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.topchat.chatsearch.data.GetChatSearchResponse
-import com.tokopedia.topchat.chatsearch.usecase.GetSearchQueryUseCase
-import com.tokopedia.topchat.chatsearch.view.uimodel.ContactLoadMoreUiModel
+import com.tokopedia.topchat.chatsearch.usecase.GetSearchContactQueryUseCase
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 
 class ChatContactLoadMoreViewModel @Inject constructor(
         dispatcher: CoroutineDispatcher,
-        private val getSearchQueryUseCase: GetSearchQueryUseCase
+        private val searchContactQuery: GetSearchContactQueryUseCase
 ) : BaseViewModel(dispatcher) {
 
     private var _searchResults = MutableLiveData<GetChatSearchResponse>()
     val searchResult: LiveData<GetChatSearchResponse> get() = _searchResults
 
     fun loadSearchResult(page: Int, query: String, firstResponse: GetChatSearchResponse) {
-        getSearchQueryUseCase.doSearch(
+        searchContactQuery.doSearch(
                 ::onSuccessSearchContact, ::onErrorSearchContact, query, page, firstResponse
         )
     }
 
-    private fun onSuccessSearchContact(getChatSearchResponse: GetChatSearchResponse, contactLoadMoreUiModel: ContactLoadMoreUiModel?) {
+    private fun onSuccessSearchContact(getChatSearchResponse: GetChatSearchResponse) {
         _searchResults.value = getChatSearchResponse
     }
 
