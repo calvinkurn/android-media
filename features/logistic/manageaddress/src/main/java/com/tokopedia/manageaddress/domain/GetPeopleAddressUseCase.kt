99c0b85fbeb1611ffe7e5ee0ef8dcafc610a26b1
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-06-15 16:07:39 +0700

    GetPeopleAddressUseCase

---
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/domain/GetPeopleAddressUseCase.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.manageaddress.domain
+
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.manageaddress.data.query.GetPeopleAddressQuery
+import com.tokopedia.manageaddress.domain.response.GetPeopleAddressResponse
+import javax.inject.Inject
+
+class GetPeopleAddressUseCase @Inject constructor(@ApplicationContext val context: Context, val graphqlUseCase: GraphqlUseCase<GetPeopleAddressResponse>) {
+
+    fun execute(query: String, onSuccess: (GetPeopleAddressResponse) -> Unit, onError: (Throwable) -> Unit) {
+        graphqlUseCase.setGraphqlQuery(GetPeopleAddressQuery.keroAddressCorner)
+        graphqlUseCase.setRequestParams(mapOf(
+                "input" to mapOf(
+                        "search_key" to query,
+                        "show_address" to true
+                )
+        ))
+        graphqlUseCase.setTypeClass(GetPeopleAddressResponse::class.java)
+        return graphqlUseCase.execute(
+                { peopleAddressResponse ->
+                    onSuccess(peopleAddressResponse)
+                }, { throwable ->
+            onError(throwable)
+        })
+    }
+
+}
