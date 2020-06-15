da3a5c32122063ee418662b72e6baf04a4d5112f
isfaaghyth <isfaaghyth@gmail.com>
2020-06-16 01:30:55 +0700

    feat(pushnotif:created): viewHolder of action button

---
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/inApp/viewEngine/adapter/viewholder/ActionButtonViewHolder.kt
@@ -0,0 +1,45 @@
+package com.tokopedia.notifications.inApp.viewEngine.adapter.viewholder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.notifications.R
+import com.tokopedia.notifications.inApp.ruleEngine.storage.entities.inappdata.CMButton
+import com.tokopedia.unifycomponents.UnifyButton
+import kotlin.LazyThreadSafetyMode.NONE as NONE
+
+open class ActionButtonViewHolder(
+        view: View,
+        private val onClick: () -> Unit
+): RecyclerView.ViewHolder(view) {
+
+    private val btnAction: UnifyButton = view.findViewById(R.id.btnAction)
+    private val context by lazy(NONE) { view.context }
+
+    fun bind(button: CMButton) {
+        btnAction.text = button.getTxt()
+        btnAction.setOnClickListener {
+            val appLink = RouteManager.getIntent(context, button.getAppLink())
+            context.startActivity(appLink)
+
+            // custom action from dialog
+            onClick()
+        }
+    }
+
+    companion object {
+        @LayoutRes val LAYOUT = R.layout.cm_layout_inapp_action_button
+
+        fun create(parent: ViewGroup, onClick: () -> Unit): ActionButtonViewHolder {
+            val layout = LayoutInflater
+                    .from(parent.context)
+                    .inflate(LAYOUT, parent, false)
+            return ActionButtonViewHolder(layout, onClick)
+        }
+
+    }
+
+}
