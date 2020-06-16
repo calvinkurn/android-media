6ec330661faa6dd2d8ff61884d91219e5e4494c4
rezagama <reza.hidayat@tokopedia.com>
2020-06-16 13:37:02 +0700

    Refactor free shipping status use case

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/ShopFreeShippingStatus.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/ShopFreeShippingStatus.kt
@@ -1,33 +1,6 @@
 package com.tokopedia.shop.common.data.source.cloud.model
 
-import com.google.gson.annotations.SerializedName
-
-class ShopFreeShippingStatus(
-    @SerializedName("EligibleServiceShop")
-    val response: Response
-) {
-
-    data class Response(
-        @SerializedName("shops")
-        val shops: List<Shop>
-    )
-
-    data class Shop(
-        @SerializedName("shopID")
-        val shopId:  Int,
-        @SerializedName("status")
-        val status: Boolean,
-        @SerializedName("statusEligible")
-        val statusEligible: Boolean,
-        @SerializedName("statusSellerApproval")
-        val statusSellerApproval: Int
-    ) {
-        object SellerApproval {
-            const val APPROVED = 3
-        }
-
-        fun isEligible(): Boolean {
-            return statusEligible && statusSellerApproval == SellerApproval.APPROVED
-        }
-    }
-}
+data class ShopFreeShippingStatus(
+    val active: Boolean,
+    val eligible: Boolean
+)
--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/domain/interactor/GetShopFreeShippingStatusUseCase.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/domain/interactor/GetShopFreeShippingStatusUseCase.kt
@@ -1,23 +1,14 @@
 package com.tokopedia.shop.common.domain.interactor
 
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.CacheType
-import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.shop.common.data.source.cloud.model.ShopFreeShippingStatus
-import com.tokopedia.shop.common.data.source.cloud.model.ShopFreeShippingStatus.*
-import com.tokopedia.shop.common.data.source.cloud.query.GetShopFreeShippingStatus
 import com.tokopedia.usecase.RequestParams
-import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
-class GetShopFreeShippingStatusUseCase@Inject constructor(
-    private val userSession: UserSessionInterface,
-    graphqlRepository: GraphqlRepository
-): GraphqlUseCase<ShopFreeShippingStatus>(graphqlRepository) {
-
+class GetShopFreeShippingStatusUseCase @Inject constructor(
+    private val getShopFreeShippingInfoUseCase: GetShopFreeShippingInfoUseCase,
+    private val getShopFreeShippingEligibilityUseCase: GetShopFreeShippingEligibilityUseCase
+) {
     companion object {
-        private const val PARAM_USER_ID = "userID"
         private const val PARAM_SHOP_ID = "shopIDs"
 
         fun createRequestParams(shopIds: List<Int>): RequestParams {
@@ -27,18 +18,9 @@ class GetShopFreeShippingStatusUseCase@Inject constructor(
         }
     }
 
-    init {
-        val cacheStrategy = GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build()
-        setCacheStrategy(cacheStrategy)
-
-        setGraphqlQuery(GetShopFreeShippingStatus.QUERY)
-        setTypeClass(ShopFreeShippingStatus::class.java)
-    }
-
-    suspend fun execute(requestParams: RequestParams): List<Shop> {
-        val userId = userSession.userId.toInt()
-        requestParams.putInt(PARAM_USER_ID, userId)
-        setRequestParams(requestParams.parameters)
-        return executeOnBackground().response.shops
+    suspend fun execute(requestParams: RequestParams): ShopFreeShippingStatus {
+        val active = getShopFreeShippingInfoUseCase.execute(requestParams).first().freeShipping.isActive
+        val eligible = getShopFreeShippingEligibilityUseCase.execute(requestParams).first().status
+        return ShopFreeShippingStatus(active, eligible)
     }
 }
