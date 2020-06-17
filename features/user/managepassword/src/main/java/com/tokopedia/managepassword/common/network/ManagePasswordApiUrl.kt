fa17bfbdae41ca3ca25e2a89bbf79fba0701fa46
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-06-17 11:18:26 +0700

    remove REST API client

--- features/user/managepassword/src/main/java/com/tokopedia/managepassword/common/network/ManagePasswordApiUrl.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.managepassword.common.network
-
-object ManagePasswordApiUrl {
-    const val ADD_PASSWORD = "api/create-password"
-    const val FORGOT_PASSWORD = "api/reset"
-}
