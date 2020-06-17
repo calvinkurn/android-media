89e2939f414c92314ff35e787941cca3a4d640d7
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-17 13:18:42 +0800

    bugfix tooltip bottom sheet

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/bottomsheet/TooltipBottomSheet.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/bottomsheet/TooltipBottomSheet.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.sellerhomecommon.presentation.view.bottomsheet
 
 import android.content.Context
+import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.tokopedia.sellerhomecommon.R
 import com.tokopedia.sellerhomecommon.presentation.model.TooltipUiModel
 import com.tokopedia.unifycomponents.BottomSheetUnify
@@ -19,11 +21,17 @@ class TooltipBottomSheet(
         setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
         setTitle(tooltip.title)
         showCloseIcon = false
+        isDragable = true
         showKnob = true
         isHideable = true
 
-        val bottomSheetContentView = SellerHomeBottomSheetContent(mContext)
-        bottomSheetContentView.setTooltipData(tooltip)
-        setChild(bottomSheetContentView)
+        val child = SellerHomeBottomSheetContent(mContext)
+        child.setTooltipData(tooltip)
+        setChild(child)
+        child.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        customPeekHeight = child.measuredHeight
+        setShowListener {
+            bottomSheet.state = BottomSheetBehavior.STATE_EXPANDED
+        }
     }
 }
