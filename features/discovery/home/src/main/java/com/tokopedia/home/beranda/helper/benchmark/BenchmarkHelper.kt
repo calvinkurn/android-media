fb8295ff4071cc4272bc74e3169f0bfdbf1805ff
devarafikry <devara.akmal@tokopedia.com>
2020-06-17 16:48:07 +0700

    Revert benchmark helper

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/helper/benchmark/BenchmarkHelper.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/helper/benchmark/BenchmarkHelper.kt
@@ -2,17 +2,16 @@ package com.tokopedia.home.beranda.helper.benchmark
 
 import android.os.Build
 import android.os.Trace
-import com.tokopedia.network.BuildConfig
 
 object BenchmarkHelper {
     fun beginSystraceSection(sectionName: String) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && BuildConfig.DEBUG) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             Trace.beginSection(sectionName)
         }
     }
 
     fun endSystraceSection() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && BuildConfig.DEBUG) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             Trace.endSection()
         }
     }
