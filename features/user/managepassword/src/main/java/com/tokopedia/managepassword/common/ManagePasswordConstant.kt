0926075a0f1599bb9bb325f5eff4adebd39a3104
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-06-17 12:18:15 +0700

    clean code

--- features/user/managepassword/src/main/java/com/tokopedia/managepassword/common/ManagePasswordConstant.kt
+++ features/user/managepassword/src/main/java/com/tokopedia/managepassword/common/ManagePasswordConstant.kt
@@ -1,10 +1,6 @@
 package com.tokopedia.managepassword.common
 
 object ManagePasswordConstant {
-    const val QUERY_ADD_PASSWORD = "queryAddPassword"
-    const val QUERY_CHANGE_PASSWORD = "queryChangePassword"
-    const val QUERY_FORGOT_PASSWORD = "queryForgotPassword"
-
     const val PARAM_INPUT = "input"
     const val PARAM_PASSWORD = "password"
     const val PARAM_PASSWORD_CONFIRMATION = "password_confirm"
