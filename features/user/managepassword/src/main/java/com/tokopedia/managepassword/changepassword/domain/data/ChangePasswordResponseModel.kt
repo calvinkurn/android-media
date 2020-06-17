13bf15be6d18dcdbfb6eb2dcaa7e462475b1cce5
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-06-17 11:18:04 +0700

    change password update

--- features/user/managepassword/src/main/java/com/tokopedia/managepassword/changepassword/domain/data/ChangePasswordResponseModel.kt
+++ features/user/managepassword/src/main/java/com/tokopedia/managepassword/changepassword/domain/data/ChangePasswordResponseModel.kt
@@ -5,15 +5,21 @@ import com.google.gson.annotations.SerializedName
 
 data class ChangePasswordResponseModel(
         @Expose
-        @SerializedName("user_id")
-        val userId: Int = 0,
-        @Expose
-        @SerializedName("updated")
-        val isUpdated: Boolean = false,
-        @Expose
-        @SerializedName("type")
-        val type: String = "",
-        @Expose
-        @SerializedName("message")
-        val message: String = ""
-)
+        @SerializedName("SubmitResetPassword")
+        val changePassword: ChangePassword = ChangePassword()
+) {
+    data class ChangePassword(
+            @Expose
+            @SerializedName("user_id")
+            val userId: Int = 0,
+            @Expose
+            @SerializedName("updated")
+            val isUpdated: Boolean = false,
+            @Expose
+            @SerializedName("type")
+            val type: String = "",
+            @Expose
+            @SerializedName("message")
+            val message: String = ""
+    )
+}
