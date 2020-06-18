78e31dea625ab82335ae37a9ef2f1a1dd8e84e6d
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-06-18 11:22:09 +0700

    add unit test

--- features/user/managepassword/src/test/java/com/tokopedia/managepassword/ext/dispacher/DispatcherProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.managepassword.ext.dispacher
-
-import kotlinx.coroutines.CoroutineDispatcher
-
-interface DispatcherProvider {
-    fun ui(): CoroutineDispatcher
-    fun io(): CoroutineDispatcher
-}
--- features/user/managepassword/src/test/java/com/tokopedia/managepassword/ext/ext.kt
+++
@@ -1,8 +0,0 @@
-import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
-import io.mockk.MockKStubScope
-import io.mockk.coEvery
-
-inline fun MultiRequestGraphqlUseCase.stubExecuteOnBackground(): MockKStubScope<Any, Any> {
-    val it = this
-    return coEvery { it.executeOnBackground() }
-}
