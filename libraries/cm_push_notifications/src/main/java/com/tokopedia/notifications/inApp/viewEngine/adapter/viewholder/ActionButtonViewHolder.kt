407714810d6f575ac0773d0f6ff3863f6230fafc
isfaaghyth <isfaaghyth@gmail.com>
2020-06-18 14:35:06 +0700

    feat(inapp:changed): adjust buttons layout manager

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/inApp/viewEngine/adapter/viewholder/ActionButtonViewHolder.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/inApp/viewEngine/adapter/viewholder/ActionButtonViewHolder.kt
@@ -8,29 +8,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.notifications.R
 import com.tokopedia.notifications.inApp.ruleEngine.storage.entities.inappdata.CMButton
+import com.tokopedia.notifications.inApp.ruleEngine.storage.entities.inappdata.CMInApp
 import com.tokopedia.unifycomponents.UnifyButton
 import kotlin.LazyThreadSafetyMode.NONE
 
 open class ActionButtonViewHolder(
         view: View,
-        private val onClick: () -> Unit
+        private val onClick: (CMInApp) -> Unit
 ): RecyclerView.ViewHolder(view) {
 
     private val btnAction: UnifyButton = view.findViewById(R.id.btnAction)
     private val context by lazy(NONE) { view.context }
 
-    fun bind(button: CMButton) {
+    fun bind(cmInApp: CMInApp, button: CMButton?) {
+        if (button == null) return
+
         btnAction.text = button.getTxt()
         btnAction.setOnClickListener {
             RouteManager.route(context, button.getAppLink())
-            onClick()
+            onClick(cmInApp)
         }
     }
 
     companion object {
         @LayoutRes val LAYOUT = R.layout.cm_layout_inapp_action_button
 
-        fun create(parent: ViewGroup, onClick: () -> Unit): ActionButtonViewHolder {
+        fun create(
+                parent: ViewGroup,
+                onClick: (CMInApp) -> Unit
+        ): ActionButtonViewHolder {
             val layout = LayoutInflater
                     .from(parent.context)
                     .inflate(LAYOUT, parent, false)
