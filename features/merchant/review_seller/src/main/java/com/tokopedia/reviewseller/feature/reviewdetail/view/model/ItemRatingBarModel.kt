2ec488f80e5e14e852c15e59d3bef5966402da2f
leonard2907 <leonard.gunawan@tokopedia.com>
2020-06-19 16:24:19 +0700

    Revert "Remove unnecessary changes"
    This reverts commit af26d294e2668036a8326f8e3fab4585ad5b8932.

---
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewdetail/view/model/ItemRatingBarModel.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.reviewseller.feature.reviewdetail.view.model
+
+data class ItemRatingBarModel(
+        var starPosition: String? = "0",
+        var totalReview: String? = "0"
+)
