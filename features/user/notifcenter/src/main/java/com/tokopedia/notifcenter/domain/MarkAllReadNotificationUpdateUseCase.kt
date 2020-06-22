ffe46c55288682e714ddadd912ed63ce9e379f40
elscione <yusufhendrawan06@gmail.com>
2020-06-22 12:06:37 +0700

    revert changes on MarkAllReadNotificationUpdateUseCase

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/domain/MarkAllReadNotificationUpdateUseCase.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/domain/MarkAllReadNotificationUpdateUseCase.kt
@@ -1,11 +1,13 @@
 package com.tokopedia.notifcenter.domain
 
 import android.content.Context
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.graphql.domain.GraphqlUseCase
+import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.data.entity.NotificationUpdateActionResponse
 import com.tokopedia.notifcenter.di.scope.NotificationContext
 import rx.Subscriber
@@ -20,7 +22,9 @@ class MarkAllReadNotificationUpdateUseCase @Inject constructor(
 ) {
     fun execute(requestParams: Map<String, Any>,
                 subscriber: Subscriber<GraphqlResponse>) {
-        val graphqlRequest = GraphqlRequest(QUERY,
+        val query = GraphqlHelper.loadRawString(context.resources
+                , R.raw.mutation_mark_all_read_notification_update)
+        val graphqlRequest = GraphqlRequest(query,
                 NotificationUpdateActionResponse::class.java, requestParams)
 
         val cacheStrategy = GraphqlCacheStrategy.Builder(CacheType.CLOUD_THEN_CACHE)
@@ -32,15 +36,6 @@ class MarkAllReadNotificationUpdateUseCase @Inject constructor(
     }
 
     companion object {
-        private const val QUERY = "mutation markAllReadStatusNotificationUpdate(\$typeOfNotif: Int){\n" +
-                "  notifcenter_markAllReadStatus(type_of_notif: \$typeOfNotif){\n" +
-                "    data {\n" +
-                "        is_success\n" +
-                "    }\n" +
-                "    message_error\n" +
-                "  }\n" +
-                "}"
-
         private const val PARAM_NOTIF_TYPE = "typeOfNotif"
         private const val TYPE_NOTIF_UPDATE = 1 //update
 
