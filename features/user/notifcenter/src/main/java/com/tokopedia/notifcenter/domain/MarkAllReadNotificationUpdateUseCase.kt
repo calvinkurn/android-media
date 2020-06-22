f9b512fb66de1834af1eea7972befecfe724945a
elscione <yusufhendrawan06@gmail.com>
2020-06-22 07:42:29 +0700

    Add type_id parameter for clearing notifications and move the query to a constant

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/domain/MarkAllReadNotificationUpdateUseCase.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/domain/MarkAllReadNotificationUpdateUseCase.kt
@@ -1,13 +1,12 @@
 package com.tokopedia.notifcenter.domain
 
 import android.content.Context
-import com.tokopedia.abstraction.common.utils.GraphqlHelper
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.graphql.domain.GraphqlUseCase
-import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.data.entity.NotificationUpdateActionResponse
 import com.tokopedia.notifcenter.di.scope.NotificationContext
 import rx.Subscriber
@@ -22,9 +21,7 @@ class MarkAllReadNotificationUpdateUseCase @Inject constructor(
 ) {
     fun execute(requestParams: Map<String, Any>,
                 subscriber: Subscriber<GraphqlResponse>) {
-        val query = GraphqlHelper.loadRawString(context.resources
-                , R.raw.mutation_mark_all_read_notification_update)
-        val graphqlRequest = GraphqlRequest(query,
+        val graphqlRequest = GraphqlRequest(QUERY,
                 NotificationUpdateActionResponse::class.java, requestParams)
 
         val cacheStrategy = GraphqlCacheStrategy.Builder(CacheType.CLOUD_THEN_CACHE)
@@ -36,12 +33,25 @@ class MarkAllReadNotificationUpdateUseCase @Inject constructor(
     }
 
     companion object {
+        private const val QUERY = "mutation markAllReadStatusNotificationUpdate(\$typeOfNotif: Int, \$typeId: Int){\n" +
+                "  notifcenter_markAllReadStatus(type_of_notif: \$typeOfNotif, type_id: \$typeId){\n" +
+                "    data {\n" +
+                "        is_success\n" +
+                "    }\n" +
+                "    message_error\n" +
+                "  }\n" +
+                "}"
+
         private const val PARAM_NOTIF_TYPE = "typeOfNotif"
+        private const val PARAM_NOTIF_TYPE_ID = "typeId"
         private const val TYPE_NOTIF_UPDATE = 1 //update
+        private const val TYPE_NOTIF_ID_DEFAULT = 0 //default
+        private const val TYPE_NOTIF_ID_SELLER = 2 //seller
 
         fun params(typeOfNotif: Int = TYPE_NOTIF_UPDATE): HashMap<String, Any> {
             val params = hashMapOf<String, Any>()
             params[PARAM_NOTIF_TYPE] = typeOfNotif
+            params[PARAM_NOTIF_TYPE_ID] = if (GlobalConfig.isSellerApp()) TYPE_NOTIF_ID_SELLER else TYPE_NOTIF_ID_DEFAULT
             return params
         }
     }
