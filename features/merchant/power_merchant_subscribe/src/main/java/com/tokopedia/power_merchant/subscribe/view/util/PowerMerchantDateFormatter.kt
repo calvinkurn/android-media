6cf58901ca7bec662276fbb5315f8c582c7310c8
rezagama <reza.hidayat@tokopedia.com>
2020-06-23 17:49:44 +0700

    Fix cancellation date wording

--- features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/util/PowerMerchantDateFormatter.kt
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/view/util/PowerMerchantDateFormatter.kt
@@ -10,12 +10,16 @@ import com.tokopedia.power_merchant.subscribe.view.util.PowerMerchantSpannableUt
 
 object PowerMerchantDateFormatter {
 
-    fun formatCancellationDate(context: Context, expiredDate: String): SpannableString {
+    fun formatCancellationDate(
+        context: Context,
+        warningTextFormat: Int,
+        expiredDate: String
+    ): SpannableString {
         val cancellationDate = DateFormatUtils.formatDate(PowerMerchantDate.EXPIRED_DATE_FORMAT,
             DateFormatUtils.FORMAT_D_MMMM_YYYY, expiredDate)
         val cancellationHour = DateFormatUtils.formatDate(PowerMerchantDate.EXPIRED_DATE_FORMAT,
             PowerMerchantDate.DATE_FORMAT_HH_MM, expiredDate)
-        val warningText = context.getString(R.string.expired_label, cancellationDate, cancellationHour)
+        val warningText = context.getString(warningTextFormat, cancellationDate, cancellationHour)
         val highlightTextColor = ContextCompat.getColor(context, R.color.light_N700)
 
         return createSpannableString(warningText, cancellationDate, highlightTextColor, true)
