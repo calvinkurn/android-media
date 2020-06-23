5470df050d36b13b7cd10b8fa586e5116117a15a
Muhammad Furqan <furqan@tokopedia.com>
2020-06-23 15:53:34 +0700

    [UnitTest] Flight Fare Calendar View Model

--- features/travel/flight/src/test/java/com/tokopedia/flight/dummy/FlightHomepageDummyData.kt
+++ features/travel/flight/src/test/java/com/tokopedia/flight/dummy/FlightHomepageDummyData.kt
@@ -2,6 +2,9 @@ package com.tokopedia.flight.dummy
 
 import com.tokopedia.common.travel.data.entity.TravelCollectiveBannerModel
 import com.tokopedia.common.travel.ticker.presentation.model.TravelTickerModel
+import com.tokopedia.flight.dashboard.view.model.FlightFare
+import com.tokopedia.flight.dashboard.view.model.FlightFareAttributes
+import com.tokopedia.flight.dashboard.view.model.FlightFareData
 import com.tokopedia.travelcalendar.data.entity.TravelCalendarHoliday
 
 /**
@@ -51,24 +54,33 @@ val TICKER_DATA = TravelTickerModel(
 val HOLIDAY_EMPTY_DATA = TravelCalendarHoliday.HolidayData(arrayListOf())
 
 val HOLIDAY_WITH_DATA = TravelCalendarHoliday.HolidayData(arrayListOf(
-TravelCalendarHoliday("holiday1",
-        TravelCalendarHoliday.HolidayAttribute(
-                "2020-11-11",
-                "Libur 1"
-        )),
+        TravelCalendarHoliday("holiday1",
+                TravelCalendarHoliday.HolidayAttribute(
+                        "2020-11-11",
+                        "Libur 1"
+                )),
         TravelCalendarHoliday("holiday2",
-        TravelCalendarHoliday.HolidayAttribute(
-                "2020-11-12",
-                "Libur 2"
-        )),
+                TravelCalendarHoliday.HolidayAttribute(
+                        "2020-11-12",
+                        "Libur 2"
+                )),
         TravelCalendarHoliday("holiday3",
-        TravelCalendarHoliday.HolidayAttribute(
-                "2020-11-13",
-                "Libur 3"
-        )),
+                TravelCalendarHoliday.HolidayAttribute(
+                        "2020-11-13",
+                        "Libur 3"
+                )),
         TravelCalendarHoliday("holiday4",
-        TravelCalendarHoliday.HolidayAttribute(
-                "2020-11-14",
-                "Libur 4"
-        ))
+                TravelCalendarHoliday.HolidayAttribute(
+                        "2020-11-14",
+                        "Libur 4"
+                ))
 ))
+
+val FARE_CALENDAR_DATA = FlightFareData(
+        FlightFare("dummyId",
+                arrayListOf(
+                        FlightFareAttributes("2020-02-03", 0, "Rp.0", "Rp.200", true),
+                        FlightFareAttributes("2020-02-02", 200, "Rp.200", "Rp.300", false)
+                )
+        )
+)
