e8d0d705bb240fa9b084828309cf0891e7a75206
yorispr <yoris.prayogo@tokopedia.com>
2020-06-24 16:30:13 +0700

    create authenticator to handle 401

---
+++ libraries/composite_lib/network/src/main/java/com/tokopedia/network/interceptor/TkpdAuthenticator.kt
@@ -0,0 +1,66 @@
+package com.tokopedia.network.interceptor
+
+import android.content.Context
+import com.tokopedia.network.NetworkRouter
+import com.tokopedia.network.refreshtoken.AccessTokenRefresh
+import com.tokopedia.user.session.UserSession
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+/**
+ * Created by Yoris Prayogo on 23/06/20.
+ * Copyright (c) 2020 PT. Tokopedia All rights reserved.
+ */
+
+class TkpdAuthenticator(
+        val context: Context,
+        val networkRouter: NetworkRouter,
+        val userSession: UserSession): Authenticator {
+
+    private fun isNeedForceLogout(request: Request): Boolean {
+        if (userSession.isLoggedIn && !userSession.accessToken.isEmpty()) {
+            if (request.header(HEADER_ACCOUNTS_AUTHORIZATION)?.contains(userSession.accessToken) == true) return true
+        }
+        return false
+    }
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+        if(!isNeedForceLogout(response.request())) {
+            return try {
+                val originalRequest = response.request()
+                val accessTokenRefresh = AccessTokenRefresh()
+                val newAccessToken = accessTokenRefresh.refreshToken(context, userSession, networkRouter)
+                networkRouter.doRelogin(newAccessToken)
+                updateRequestWithNewToken(originalRequest)
+            } catch (ex: Exception) {
+                response.request()
+            }
+        }else {
+            networkRouter.showForceLogoutTokenDialog(response.peekBody(BYTE_COUNT).string())
+        }
+        return response.request()
+    }
+
+    private fun updateRequestWithNewToken(request: Request): Request{
+        val newRequest = request.newBuilder()
+        val freshAccessToken = userSession.accessToken
+        val isContainsBearer = request.header(AUTHORIZATION)?.contains(BEARER)
+
+        newRequest.header(HEADER_ACCOUNTS_AUTHORIZATION, "$HEADER_PARAM_BEARER $freshAccessToken")
+        if(isContainsBearer == true){
+            newRequest.header(HEADER_PARAM_AUTHORIZATION, "$HEADER_PARAM_BEARER $freshAccessToken")
+        }
+        return newRequest.build()
+    }
+
+    companion object {
+        private const val HEADER_ACCOUNTS_AUTHORIZATION = "accounts-authorization"
+        private const val HEADER_PARAM_BEARER = "Bearer"
+        private const val AUTHORIZATION = "authorization"
+        private const val BEARER = "Bearer"
+        private const val HEADER_PARAM_AUTHORIZATION = "authorization"
+        const val BYTE_COUNT = 512L
+    }
+}
