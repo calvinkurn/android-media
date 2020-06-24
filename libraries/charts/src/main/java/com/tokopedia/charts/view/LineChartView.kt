b6dc44ba1501278473c83398ec025a2d516b1fc7
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-06-24 17:39:27 +0800

    add line chart view

---
+++ libraries/charts/src/main/java/com/tokopedia/charts/view/LineChartView.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.charts.view
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import com.tokopedia.charts.R
+
+/**
+ * Created By @ilhamsuaib on 24/06/20
+ */
+
+class LineChartView : LinearLayout {
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        View.inflate(context, R.layout.view_line_chart, this)
+    }
+}
