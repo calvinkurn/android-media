db834ef74e1f90b48264688a3363c91335d8cb41
saidfaisal <said.faisal@tokopedia.com>
2020-06-24 14:44:30 +0700

    change resource to be unify resource

--- features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_category/presentation/widget/BrandlistCategoryTabLayout.kt
+++ features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_category/presentation/widget/BrandlistCategoryTabLayout.kt
@@ -120,14 +120,14 @@ class BrandlistCategoryTabLayout(context: Context?, attrs: AttributeSet?) : TabL
 
     private fun setActiveText(categoryView: Typography) {
         categoryView.apply {
-            setTextColor(MethodChecker.getColor(context, R.color.Purple_P600))
+            setTextColor(MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Purple_P600))
             setWeight(Typography.BOLD)
         }
     }
 
     private fun setInactiveText(categoryView: Typography) {
         categoryView.apply {
-            setTextColor(MethodChecker.getColor(context, R.color.Neutral_N700_96))
+            setTextColor(MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Neutral_N700_96))
             setWeight(Typography.REGULAR)
         }
     }
