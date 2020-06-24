76b70925349acaae3cdf8942679a9f1f0600f6ab
yogieputra8 <yogie.putra8@gmail.com>
2020-06-24 17:22:49 +0700

    Add new class GetData to gold merchant and tkpdseller module

--- tkpdseller/src/main/java/com/tokopedia/seller/product/etalase/data/source/cloud/MyEtalaseCloud.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/etalase/data/source/cloud/MyEtalaseCloud.java
@@ -2,15 +2,15 @@ package com.tokopedia.seller.product.etalase.data.source.cloud;
 
 import android.content.Context;
 
-import com.tokopedia.core.base.common.util.GetData;
 import com.tokopedia.core.base.di.qualifier.ApplicationContext;
 import com.tokopedia.core.network.retrofit.utils.AuthUtil;
 import com.tokopedia.core.network.retrofit.utils.TKPDMapParam;
 import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.product.manage.item.main.base.data.source.cloud.model.myetalase.MyEtalaseListServiceModel;
+import com.tokopedia.seller.product.common.utils.GetData;
 import com.tokopedia.seller.product.etalase.data.source.cloud.api.MyEtalaseApi;
 import com.tokopedia.seller.product.etalase.data.source.cloud.model.AddEtalaseServiceModel;
-//import com.tokopedia.seller.shopscore.data.common.GetData;
+
 
 import javax.inject.Inject;
 
