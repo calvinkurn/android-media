f83cc36e5274d308c188c1aec83cc7e4fedff700
glenntuyu <christian.tuyu@tokopedia.com>
2020-06-26 00:56:12 +0700

    fix spacing on inspi card chip

---
+++ features/discovery/search/src/main/java/com/tokopedia/search/utils/ItemDecorationExt.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.search.utils
+
+import androidx.recyclerview.widget.RecyclerView
+
+internal fun RecyclerView.addItemDecorationIfNotExists(itemDecoration: RecyclerView.ItemDecoration) {
+    val hasNoItemDecoration = itemDecorationCount == 0
+    if (hasNoItemDecoration) addItemDecoration(itemDecoration)
+}
