49a635ae00d364824675a644bdc3839a7fc0168f
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2020-06-25 16:57:45 +0700

    remove old submit event voucher

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerRouterApplication.java
@@ -65,10 +65,6 @@ import com.tokopedia.core.util.SessionHandler;
 import com.tokopedia.core.util.SessionRefresh;
 import com.tokopedia.design.component.BottomSheets;
 import com.tokopedia.developer_options.presentation.activity.DeveloperOptionActivity;
-//import com.tokopedia.events.EventModuleRouter;
-//import com.tokopedia.events.di.DaggerEventComponent;
-//import com.tokopedia.events.di.EventComponent;
-//import com.tokopedia.events.di.EventModule;
 import com.tokopedia.feedplus.view.fragment.FeedPlusContainerFragment;
 import com.tokopedia.fingerprint.util.FingerprintConstant;
 import com.tokopedia.flight.orderlist.view.fragment.FlightOrderListFragment;
@@ -193,7 +189,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
         GlobalNavRouter,
         AccountHomeRouter,
         OmsModuleRouter,
-//        EventModuleRouter,
         PhoneVerificationRouter,
         TkpdAppsFlyerRouter,
         UnifiedOrderListRouter,
@@ -208,7 +203,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
     Lazy<ReactUtils> reactUtils;
 
     private DaggerReactNativeComponent.Builder daggerReactNativeBuilder;
-//    private EventComponent eventComponent;
     private OmsComponent omsComponent;
     private DaggerShopComponent.Builder daggerShopBuilder;
     private ShopComponent shopComponent;
@@ -610,12 +604,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
         return PhoneVerificationActivationActivity.getCallingIntent(context);
     }
 
-//    @Override
-//    public Intent tkpdCartCheckoutGetLoyaltyOldCheckoutCouponActiveIntent(
-//            Context context, String platform, String category, String defaultSelectedTab) {
-//        return LoyaltyActivity.newInstanceCouponActive(context, platform, category, defaultSelectedTab);
-//    }
-
     @Override
     public Fragment getReviewFragment(Activity activity, String shopId, String shopDomain) {
         return ReviewShopFragment.createInstance(shopId, shopDomain);
@@ -629,24 +617,6 @@ public abstract class ConsumerRouterApplication extends MainApplication implemen
     @Override
     public void logInvalidGrant(Response response) {
         AnalyticsLog.logInvalidGrant(this, legacyGCMHandler(), legacySessionHandler(), response.request().url().toString());
-    }
-
-    @Override
-    public Observable<TKPDMapParam<String, Object>> verifyEventPromo(com.tokopedia.usecase.RequestParams requestParams) {
-        boolean isEventOMS = remoteConfig.getBoolean("event_oms_android", false);
-//        if(eventComponent == null){
-//            eventComponent = DaggerEventComponent.builder()
-//                    .baseAppComponent((this).getBaseAppComponent())
-//                    .eventModule(new EventModule(this))
-//                    .build();
-//        }
-//        if (!isEventOMS) {
-//            return eventComponent.getVerifyCartWrapper().verifyPromo(requestParams);
-//        } else {
-//            return new PostVerifyCartWrapper(this, eventComponent.getPostVerifyCartUseCase())
-//                    .verifyPromo(requestParams);
-//        }
-        return null;
     }
 
     @Override
