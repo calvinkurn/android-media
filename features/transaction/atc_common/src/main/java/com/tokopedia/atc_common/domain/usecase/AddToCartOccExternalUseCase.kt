9a4cfaa76a7914f56851e871f03d00502cf8a98a
hansenpw <hansen.wijaya@tokopedia.com>
2020-06-25 10:10:06 +0700

    refactor code, add branch io

--- features/transaction/atc_common/src/main/java/com/tokopedia/atc_common/domain/usecase/AddToCartOccExternalUseCase.kt
+++ features/transaction/atc_common/src/main/java/com/tokopedia/atc_common/domain/usecase/AddToCartOccExternalUseCase.kt
@@ -1,22 +1,34 @@
 package com.tokopedia.atc_common.domain.usecase
 
+import android.content.Context
 import com.appsflyer.AFInAppEventParameterName
 import com.appsflyer.AFInAppEventType
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.atc_common.AtcConstant.MUTATION_ATC_OCC_EXTERNAL
 import com.tokopedia.atc_common.data.model.response.AddToCartOccExternalGqlResponse
-import com.tokopedia.atc_common.data.model.response.DataOccResponse
+import com.tokopedia.atc_common.data.model.response.DetailOccResponse
 import com.tokopedia.atc_common.domain.mapper.AddToCartDataMapper
 import com.tokopedia.atc_common.domain.model.response.AddToCartDataModel
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.domain.GraphqlUseCase
+import com.tokopedia.linker.LinkerConstants
+import com.tokopedia.linker.LinkerManager
+import com.tokopedia.linker.LinkerUtils
+import com.tokopedia.linker.model.LinkerData
 import com.tokopedia.track.TrackApp
+import com.tokopedia.track.TrackAppUtils.*
 import com.tokopedia.usecase.RequestParams
 import com.tokopedia.usecase.UseCase
+import com.tokopedia.user.session.UserSession
 import org.json.JSONArray
 import org.json.JSONObject
 import rx.Observable
 import javax.inject.Inject
+import javax.inject.Named
 
-class AddToCartOccExternalUseCase @Inject constructor(private val graphqlUseCase: GraphqlUseCase,
+class AddToCartOccExternalUseCase @Inject constructor(@ApplicationContext private val context: Context,
+                                                      @Named(MUTATION_ATC_OCC_EXTERNAL) private val query: String,
+                                                      private val graphqlUseCase: GraphqlUseCase,
                                                       private val addToCartDataMapper: AddToCartDataMapper) : UseCase<AddToCartDataModel>() {
 
     companion object {
@@ -33,49 +45,21 @@ class AddToCartOccExternalUseCase @Inject constructor(private val graphqlUseCase
 
         private const val VALUE_BEBAS_ONGKIR = "bebas ongkir"
         private const val VALUE_NONE_OTHER = "none / other"
-
-        private val QUERY =
-                """mutation add_to_cart_occ_external(${'$'}param: OneClickCheckoutATCParam) {
-                    add_to_cart_occ_external(param: ${'$'}param) {
-                        error_message
-                        status
-                        data {
-                            message
-                            success
-                            data {
-                                product_id
-                                product_name
-                                quantity
-                                price
-                                category
-                                shop_id
-                                shop_type
-                                shop_name
-                                picture
-                                url
-                                cart_id
-                                brand
-                                category_id
-                                variant
-                                tracker_attribution
-                                is_multi_origin
-                                is_free_ongkir
-                            }
-                        }
-                    }
-                }""".trimIndent()
     }
 
-    override fun createObservable(requestParams: RequestParams?): Observable<AddToCartDataModel> {
-        val productId = requestParams?.getString(REQUEST_PARAM_KEY_PRODUCT_ID, "") ?: ""
-        val graphqlRequest = GraphqlRequest(QUERY, AddToCartOccExternalGqlResponse::class.java, getParams(productId))
+    override fun createObservable(requestParams: RequestParams): Observable<AddToCartDataModel> {
+        val productId = requestParams.getString(REQUEST_PARAM_KEY_PRODUCT_ID, "")
+        val graphqlRequest = GraphqlRequest(query, AddToCartOccExternalGqlResponse::class.java, getParams(productId))
         graphqlUseCase.clearRequest()
         graphqlUseCase.addRequest(graphqlRequest)
         return graphqlUseCase.createObservable(RequestParams.EMPTY).map {
             val addToCartOccGqlResponse = it.getData<AddToCartOccExternalGqlResponse>(AddToCartOccExternalGqlResponse::class.java)
             val result = addToCartDataMapper.mapAddToCartOccResponse(addToCartOccGqlResponse)
-            sendEETracking(addToCartOccGqlResponse.addToCartOccResponse.data)
-            sendAppsFlyerTracking(addToCartOccGqlResponse.addToCartOccResponse.data)
+            if (addToCartOccGqlResponse.addToCartOccResponse.data.success == 1) {
+                sendEETracking(addToCartOccGqlResponse.addToCartOccResponse.data.detail)
+                sendAppsFlyerTracking(addToCartOccGqlResponse.addToCartOccResponse.data.detail)
+                sendBranchIoTracking(addToCartOccGqlResponse.addToCartOccResponse.data.detail)
+            }
             result
         }
 
@@ -85,67 +69,64 @@ class AddToCartOccExternalUseCase @Inject constructor(private val graphqlUseCase
         return mapOf(PARAM to mapOf(PARAM_PRODUCT_ID to productId))
     }
 
-    private fun sendAppsFlyerTracking(response: DataOccResponse) {
-        if (response.success == 1) {
-            TrackApp.getInstance().appsFlyer.sendEvent(AFInAppEventType.ADD_TO_CART,
-                    mutableMapOf(
-                            AFInAppEventParameterName.CONTENT_ID to response.detail.productId,
-                            AFInAppEventParameterName.CONTENT_TYPE to AF_VALUE_CONTENT_TYPE,
-                            AFInAppEventParameterName.DESCRIPTION to response.detail.productName,
-                            AFInAppEventParameterName.CURRENCY to AF_VALUE_CURRENCY,
-                            AFInAppEventParameterName.QUANTITY to response.detail.quantity,
-                            AFInAppEventParameterName.PRICE to response.detail.price,
-                            AF_PARAM_CATEGORY to response.detail.category,
-                            AFInAppEventParameterName.CONTENT to JSONArray().put(JSONObject().put(AF_PARAM_CONTENT_ID, response.detail.productId.toString()).put(AF_PARAM_CONTENT_QUANTITY, response.detail.quantity))
-                    )
-            )
-        }
+    private fun sendAppsFlyerTracking(response: DetailOccResponse) {
+        TrackApp.getInstance().appsFlyer.sendEvent(AFInAppEventType.ADD_TO_CART,
+                mutableMapOf(
+                        AFInAppEventParameterName.CONTENT_ID to response.productId,
+                        AFInAppEventParameterName.CONTENT_TYPE to AF_VALUE_CONTENT_TYPE,
+                        AFInAppEventParameterName.DESCRIPTION to response.productName,
+                        AFInAppEventParameterName.CURRENCY to AF_VALUE_CURRENCY,
+                        AFInAppEventParameterName.QUANTITY to response.quantity,
+                        AFInAppEventParameterName.PRICE to response.price,
+                        AF_PARAM_CATEGORY to response.category,
+                        AFInAppEventParameterName.CONTENT to JSONArray().put(JSONObject().put(AF_PARAM_CONTENT_ID, response.productId.toString()).put(AF_PARAM_CONTENT_QUANTITY, response.quantity))
+                )
+        )
     }
 
-    private fun sendEETracking(response: DataOccResponse) {
-        if (response.success == 1) {
-            TrackApp.getInstance().gtm.sendEnhanceEcommerceEvent(
-                    mutableMapOf<String, Any>(
-                            "event" to "addToCart",
-                            "eventCategory" to "product detail page",
-                            "eventAction" to "click - Beli Langsung on pdp",
-                            "eventLabel" to "fitur : Beli Langsung occ",
-                            "layout" to "layout:${""};catName:${response.detail.category.substringAfterLast("/")};catId:${response.detail.categoryId.substringAfterLast("/")};",
-                            "component" to "",
-                            "productId" to response.detail.productId,
-                            "shopId" to response.detail.shopId,
-                            "shopType" to setDefaultIfEmpty(response.detail.shopType),
-                            "ecommerce" to mutableMapOf(
-                                    "currencyCode" to "IDR",
-                                    "add" to mutableMapOf(
-                                            "products" to arrayListOf(mutableMapOf(
-                                                    "name" to response.detail.productName,
-                                                    "id" to response.detail.productId,
-                                                    "price" to response.detail.price,
-                                                    "brand" to setDefaultIfEmpty(response.detail.brand),
-                                                    "category" to setDefaultIfEmpty(response.detail.category),
-                                                    "variant" to setDefaultIfEmpty(response.detail.variant),
-                                                    "quantity" to response.detail.quantity,
-                                                    "dimension38" to setDefaultIfEmpty(response.detail.trackerAttribution),
-                                                    "dimension40" to "",
-                                                    "dimension45" to response.detail.cartId,
-                                                    "dimension54" to getMultiOriginAttribution(response.detail.isMultiOrigin),
-                                                    "dimension79" to response.detail.shopId,
-                                                    "shop_id" to response.detail.shopId,
-                                                    "dimension80" to setDefaultIfEmpty(response.detail.shopName),
-                                                    "shop_name" to setDefaultIfEmpty(response.detail.shopName),
-                                                    "dimension81" to response.detail.shopType,
-                                                    "shop_type" to response.detail.shopType,
-                                                    "picture" to setDefaultIfEmpty(response.detail.picture),
-                                                    "url" to response.detail.url,
-                                                    "category_id" to response.detail.categoryId,
-                                                    "dimension82" to response.detail.categoryId,
-                                                    "dimension83" to if (response.detail.isFreeOngkir) VALUE_BEBAS_ONGKIR else VALUE_NONE_OTHER
-                                            ))
-                                    )
-                            )
-                    ))
-        }
+    private fun sendEETracking(response: DetailOccResponse) {
+        //same as atc occ in pdp
+        TrackApp.getInstance().gtm.sendEnhanceEcommerceEvent(
+                mutableMapOf<String, Any>(
+                        EVENT to "addToCart",
+                        EVENT_CATEGORY to "product detail page",
+                        EVENT_ACTION to "click - Beli Langsung on pdp",
+                        EVENT_LABEL to "fitur : Beli Langsung occ",
+                        "layout" to "layout:${""};catName:${response.category.substringAfterLast("/")};catId:${response.categoryId.substringAfterLast("/")};",
+                        "component" to "",
+                        "productId" to response.productId,
+                        "shopId" to response.shopId,
+                        "shopType" to setDefaultIfEmpty(response.shopType),
+                        "ecommerce" to mutableMapOf(
+                                "currencyCode" to "IDR",
+                                "add" to mutableMapOf(
+                                        "products" to arrayListOf(mutableMapOf(
+                                                "name" to response.productName,
+                                                "id" to response.productId,
+                                                "price" to response.price,
+                                                "brand" to setDefaultIfEmpty(response.brand),
+                                                "category" to setDefaultIfEmpty(response.category),
+                                                "variant" to setDefaultIfEmpty(response.variant),
+                                                "quantity" to response.quantity,
+                                                "dimension38" to setDefaultIfEmpty(response.trackerAttribution),
+                                                "dimension40" to "",
+                                                "dimension45" to response.cartId,
+                                                "dimension54" to getMultiOriginAttribution(response.isMultiOrigin),
+                                                "dimension79" to response.shopId,
+                                                "shop_id" to response.shopId,
+                                                "dimension80" to setDefaultIfEmpty(response.shopName),
+                                                "shop_name" to setDefaultIfEmpty(response.shopName),
+                                                "dimension81" to response.shopType,
+                                                "shop_type" to response.shopType,
+                                                "picture" to setDefaultIfEmpty(response.picture),
+                                                "url" to response.url,
+                                                "category_id" to response.categoryId,
+                                                "dimension82" to response.categoryId,
+                                                "dimension83" to if (response.isFreeOngkir) VALUE_BEBAS_ONGKIR else VALUE_NONE_OTHER
+                                        ))
+                                )
+                        )
+                ))
     }
 
     private fun setDefaultIfEmpty(value: String?): String {
@@ -160,4 +141,20 @@ class AddToCartOccExternalUseCase @Inject constructor(private val graphqlUseCase
         else -> "regular"
     }
 
+    private fun sendBranchIoTracking(response: DetailOccResponse) {
+        LinkerManager.getInstance().sendEvent(LinkerUtils.createGenericRequest(LinkerConstants.EVENT_ADD_TO_CART, createLinkerData(response)))
+    }
+
+    private fun createLinkerData(response: DetailOccResponse): LinkerData {
+        return LinkerData().apply {
+            id = response.productId.toString()
+            price = response.price.toInt().toString()
+            description = ""
+            shopId = response.shopId.toString()
+            catLvl1 = response.category
+            userId = UserSession(context).userId
+            currency = "IDR"
+            quantity = response.quantity.toString()
+        }
+    }
 }
