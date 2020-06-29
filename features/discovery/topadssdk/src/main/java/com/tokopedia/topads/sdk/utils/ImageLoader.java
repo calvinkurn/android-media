9167587686eca161e7fc8f1d54a709d61d12f145
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2020-06-29 12:00:29 +0700

    remove unused res

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/utils/ImageLoader.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/utils/ImageLoader.java
@@ -188,24 +188,4 @@ public class ImageLoader {
         }
     }
 
-    public static int getRatingDrawable(int param) {
-        switch (param) {
-            case 0:
-                return R.drawable.ic_star_none;
-            case 1:
-                return R.drawable.icon_star_one;
-            case 2:
-                return R.drawable.icon_star_two;
-            case 3:
-                return R.drawable.icon_star_three;
-            case 4:
-                return R.drawable.icon_star_four;
-            case 5:
-                return R.drawable.icon_star_five;
-            default:
-                return R.drawable.ic_star_none;
-        }
-    }
-
-
 }
