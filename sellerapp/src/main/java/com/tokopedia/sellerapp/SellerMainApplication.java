db4911c35ba314404d98b21493e5792e12c97a7e
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-06-29 16:19:45 +0700

    Add user sessio nconstructor
    # Conflicts:
    #	sellerapp/src/main/java/com/tokopedia/sellerapp/fcm/AppNotificationReceiver.java

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
@@ -221,7 +221,7 @@ public class SellerMainApplication extends SellerRouterApplication implements Mo
 
     //Please do not delete this function to keep AppNotificationReceiver
     private void initAppNotificationReceiver() {
-        AppNotificationReceiver appNotificationReceiver = new AppNotificationReceiver();
+        AppNotificationReceiver appNotificationReceiver = new AppNotificationReceiver(getSession());
         String tag = appNotificationReceiver.getClass().getSimpleName();
         Log.d("Init %s", tag);
     }
