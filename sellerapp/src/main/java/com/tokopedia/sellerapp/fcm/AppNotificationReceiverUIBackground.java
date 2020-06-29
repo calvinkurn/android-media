9f8cfdc213a020a57e463139003af67dbe6d4ba0
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-06-29 16:32:47 +0700

    add usersession
    # Conflicts:
    #	sellerapp/src/main/java/com/tokopedia/sellerapp/fcm/AppNotificationReceiverUIBackground.java

--- sellerapp/src/main/java/com/tokopedia/sellerapp/fcm/AppNotificationReceiverUIBackground.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/fcm/AppNotificationReceiverUIBackground.java
@@ -43,7 +43,7 @@ public class AppNotificationReceiverUIBackground extends BaseAppNotificationRece
         super(application);
         remoteConfig = new FirebaseRemoteConfigImpl(application);
         //Hack reflection error don't remove it if reflection is already delete
-        receiver = new AppNotificationReceiver();
+        receiver = new AppNotificationReceiver(new UserSession(application));
     }
 
     public void prepareAndExecuteDedicatedNotification(Bundle data) {
