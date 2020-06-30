2be2c08901c10ce561dc95182a6886145ac25870
kenny <kenny.hadisaputra@tokopedia.com>
2020-06-30 21:35:43 +0700

    handle ui layout when keyboard appear

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/util/PlayBroadcasterExt.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/util/PlayBroadcasterExt.kt
@@ -48,6 +48,35 @@ inline fun View.doOnPreDraw(crossinline action: (view: View) -> Unit) {
     })
 }
 
+inline fun View.doOnNextLayout(crossinline action: (view: View) -> Unit) {
+    addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
+        override fun onLayoutChange(
+                view: View,
+                left: Int,
+                top: Int,
+                right: Int,
+                bottom: Int,
+                oldLeft: Int,
+                oldTop: Int,
+                oldRight: Int,
+                oldBottom: Int
+        ) {
+            view.removeOnLayoutChangeListener(this)
+            action(view)
+        }
+    })
+}
+
+inline fun View.doOnLayout(crossinline action: (view: View) -> Unit) {
+    if (ViewCompat.isLaidOut(this) && !isLayoutRequested) {
+        action(this)
+    } else {
+        doOnNextLayout {
+            action(it)
+        }
+    }
+}
+
 internal var View.compatTransitionName: String?
     get() {
         return ViewCompat.getTransitionName(this)
