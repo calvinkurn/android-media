8befccf0e6a145e9e7238dcfe6b2ca0a85a91365
Henry Priyono <henry.priyono@tokopedia.com>
2020-07-01 16:42:25 +0700

    Revert RouteManager Changes

--- libraries/applink/src/main/java/com/tokopedia/applink/RouteManager.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/RouteManager.java
@@ -160,6 +160,8 @@ public class RouteManager {
      * @return true if successfully routing to activity
      */
     public static boolean route(Context context, String applinkPattern, String... parameter) {
+        Bundle bundle = getBundleFromAppLinkQueryParams(applinkPattern);
+        bundle.putBundle(QUERY_PARAM, bundle);
         return route(context, new Bundle(), applinkPattern, parameter);
     }
 
@@ -180,8 +182,6 @@ public class RouteManager {
             mappedDeeplink = uriString;
         }
         ApplinkLogger.getInstance(context).appendTrace("Mapped Deeplink:\n" + mappedDeeplink);
-
-        queryParamBundle.putAll(getBundleFromAppLinkQueryParams(mappedDeeplink));
 
         Intent intent;
         String dynamicFeatureDeeplink = DeeplinkDFMapper.getDFDeeplinkIfNotInstalled(context, mappedDeeplink);
