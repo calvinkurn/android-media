356d1d42ac8d41d08b233e848c2c805d2f66ea18
mzennis <meyta.taliti@tokopedia.com>
2020-07-01 17:02:58 +0700

    add crashlytics log

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/usecase/BaseUseCase.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/usecase/BaseUseCase.kt
@@ -6,10 +6,10 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.graphql.data.model.GraphqlResponse
-import com.tokopedia.play.broadcaster.domain.model.GetLiveStatisticsResponse
 import com.tokopedia.play.broadcaster.util.error.DefaultErrorThrowable
 import com.tokopedia.play.broadcaster.util.error.DefaultNetworkThrowable
 import com.tokopedia.usecase.coroutines.UseCase
+import java.lang.reflect.ParameterizedType
 import java.net.UnknownHostException
 
 
@@ -25,13 +25,13 @@ abstract class BaseUseCase<out T : Any>: UseCase<T>() {
     ): GraphqlResponse {
         try {
             val gqlResponse =  gqlRepository.getReseponse(listOf(gqlRequest), gqlCacheStrategy)
-            val errors = gqlResponse.getError(GetLiveStatisticsResponse::class.java)
-            if (!errors.isNullOrEmpty()) {
-                if (GlobalConfig.DEBUG) {
-                    throw DefaultErrorThrowable(errors[0].message)
-                }
-                Crashlytics.log(0, TAG, errors[0].message)
-            }
+//            val errors = gqlResponse.getError(clazz)
+//            if (!errors.isNullOrEmpty()) {
+//                if (GlobalConfig.DEBUG) {
+//                    throw DefaultErrorThrowable(errors[0].message)
+//                }
+//                Crashlytics.log(0, TAG, errors[0].message)
+//            }
             return gqlResponse
         } catch (throwable: Throwable) {
             Crashlytics.log(0, TAG, throwable.localizedMessage)
