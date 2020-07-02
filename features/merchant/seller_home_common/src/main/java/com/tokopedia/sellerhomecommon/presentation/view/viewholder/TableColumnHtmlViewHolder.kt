371d69f2ebf4e805e187d3ce4d8cc172c8ed39f1
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-07-02 10:40:00 +0800

    add table item row-column view holder

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/TableColumnHtmlViewHolder.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.sellerhomecommon.presentation.view.viewholder
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.tokopedia.kotlin.extensions.view.parseAsHtml
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.sellerhomecommon.R
+import com.tokopedia.sellerhomecommon.presentation.model.TableRowsUiModel
+import kotlinx.android.synthetic.main.shc_item_table_column_html.view.*
+
+/**
+ * Created By @ilhamsuaib on 01/07/20
+ */
+
+class TableColumnHtmlViewHolder(itemView: View?) : AbstractViewHolder<TableRowsUiModel.RowColumnHtml>(itemView) {
+
+    companion object {
+        @LayoutRes
+        val RES_LAYOUT = R.layout.shc_item_table_column_html
+    }
+
+    override fun bind(element: TableRowsUiModel.RowColumnHtml) {
+        with(itemView) {
+            tvTableColumnHtml.text = element.valueStr.parseAsHtml()
+        }
+    }
+}
