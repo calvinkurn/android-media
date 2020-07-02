197398febb90c189e67f2eb0e2cd53814ebd0420
kenny <kenny.hadisaputra@tokopedia.com>
2020-07-02 12:17:05 +0700

    change comment wording

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/viewmodel/PlayBroadcastViewModel.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/viewmodel/PlayBroadcastViewModel.kt
@@ -198,7 +198,7 @@ class PlayBroadcastViewModel @Inject constructor(
 //            _observableShareInfo.value = PlayBroadcastUiMapper.mapShareInfo(channel)
             _observableShareInfo.value = PlayBroadcastMocker.getMockShare()
 
-            //TODO("Remove this if channel is not mocked anymore")
+            //TODO("Uncomment this if channel is not mocked anymore")
 //            setSelectedProduct(PlayBroadcastUiMapper.mapProductListToData(channel.productTags))
 //            setSelectedCover(PlayBroadcastUiMapper.mapCover(getCurrentSetupDataStore().getSelectedCover(), channel.basic.coverUrl, channel.basic.title))
 
