0352f4af2ef768869abceb8488c3cbc28c871703
Muhammad Furqan <furqan@tokopedia.com>
2020-07-02 12:06:17 +0700

    add ticker in flight booking page

--- features/travel/common_travel/src/main/java/com/tokopedia/common/travel/ticker/TravelTickerUtils.kt
+++ features/travel/common_travel/src/main/java/com/tokopedia/common/travel/ticker/TravelTickerUtils.kt
@@ -3,10 +3,12 @@ package com.tokopedia.common.travel.ticker
 import android.content.Context
 import android.view.View
 import androidx.core.content.ContextCompat
+import com.tokopedia.applink.RouteManager
 import com.tokopedia.common.travel.R
 import com.tokopedia.common.travel.ticker.presentation.model.TravelTickerModel
 import com.tokopedia.design.component.ticker.TickerView
 import com.tokopedia.unifycomponents.ticker.Ticker
+import com.tokopedia.unifycomponents.ticker.TickerCallback
 import java.util.*
 
 /**
@@ -14,6 +16,7 @@ import java.util.*
  */
 object TravelTickerUtils {
     private const val ANNOUNCEMENT_TYPE = 1
+    private const val WARNING_TYPE = 2
     private const val DANGER_TYPE = 2
 
     fun buildTravelTicker(context: Context?, travelTickerModel: TravelTickerModel, tickerView: TickerView) {
@@ -39,12 +42,22 @@ object TravelTickerUtils {
 
     fun buildUnifyTravelTicker(travelTickerModel: TravelTickerModel, tickerView: Ticker) {
         tickerView.setHtmlDescription(travelTickerModel.message)
-        when(travelTickerModel.type) {
+        tickerView.setDescriptionClickEvent(object : TickerCallback {
+            override fun onDescriptionViewClick(linkUrl: CharSequence) {
+                if (linkUrl.isNotEmpty()) {
+                    RouteManager.route(tickerView.context, linkUrl.toString())
+                }
+            }
+
+            override fun onDismiss() {}
+
+        })
+        when (travelTickerModel.type) {
             ANNOUNCEMENT_TYPE -> {
                 tickerView.tickerType = Ticker.TYPE_ANNOUNCEMENT
             }
-            DANGER_TYPE -> {
-                tickerView.tickerType = Ticker.TYPE_ERROR
+            WARNING_TYPE -> {
+                tickerView.tickerType = Ticker.TYPE_WARNING
             }
         }
     }
