b455ea789b699b93e0449f43dc237b1dd07bba1e
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-07-06 15:29:00 +0700

    Handle Null Passenger

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/fragment/OmsDetailFragment.java
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/fragment/OmsDetailFragment.java
@@ -671,7 +671,7 @@ public class OmsDetailFragment extends BaseDaggerFragment implements OrderListDe
                 dividerUserInfo.setVisibility(View.VISIBLE);
                 userInfo.removeAllViews();
                 for (PassengerForm passengerForm : metaDataInfo.getPassengerForms()) {
-                    if(!passengerForm.isEmpty()) {
+                    if(!passengerForm.getPassengerInformations().isEmpty()) {
                         for (PassengerInformation passengerInformation : passengerForm.getPassengerInformations()) {
                             DoubleTextView doubleTextView = new DoubleTextView(getContext(), LinearLayout.VERTICAL);
                             doubleTextView.setTopText(passengerInformation.getTitle());
