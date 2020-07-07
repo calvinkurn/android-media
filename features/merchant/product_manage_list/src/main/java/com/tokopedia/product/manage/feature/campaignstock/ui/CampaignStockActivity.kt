95c85cccb08d9ce918fb50d6b613caaef5b8793f
Reivin <reivin.oktavianus@tokopedia.com>
2020-07-07 18:43:17 +0700

    feat (campaign-stock): Add layout for CampaignStockActivity

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/CampaignStockActivity.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.product.manage.feature.campaignstock.ui
+
+import android.os.Bundle
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.product.manage.R
+
+class CampaignStockActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_campaign_stock)
+    }
+
+
+}
