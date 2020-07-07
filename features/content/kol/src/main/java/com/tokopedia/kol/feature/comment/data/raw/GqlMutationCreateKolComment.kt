9e0935f2943db1b09b6d4d2519185bf52afc7b91
kuldeepyadav <kuldeep.yadav@tokopedia.com>
2020-07-07 17:50:22 +0530

    requested changes

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/data/raw/GqlMutationCreateKolComment.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/data/raw/GqlMutationCreateKolComment.kt
@@ -2,13 +2,10 @@ package com.tokopedia.kol.feature.comment.data.raw
 
 const val GQL_MUTATION_CREATE_KOL_COMMENT: String = """mutation CreateKolComment(${'$'}idPost: Int!, ${'$'}comment: String!) {
  create_comment_kol(idPost: ${'$'}idPost, comment: ${'$'}comment) {	
- __typename 
  error
  data {
-   __typename
    id
    user {
-     __typename
      iskol
      id
      name
--- features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/data/raw/GqlMutationDeleteKolComment.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/data/raw/GqlMutationDeleteKolComment.kt
@@ -2,10 +2,8 @@ package com.tokopedia.kol.feature.comment.data.raw
 
 const val GQL_MUTATION_DELETE_KOL_COMMENT: String = """mutation DeleteKolComment(${'$'}idComment: Int!) {
 	delete_comment_kol(idComment: ${'$'}idComment) {
-		__typename
 		error
 		data {
-			__typename
 			success
 			}
 	}
