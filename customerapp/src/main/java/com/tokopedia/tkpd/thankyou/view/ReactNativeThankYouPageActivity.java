660affe5f4b3f393bd16296b481379f4c1ca18f8
milhamj <ilham.jamaludin@tokopedia.com>
2020-07-07 17:55:21 +0700

    Fix build issue :sparkles:

--- customerapp/src/main/java/com/tokopedia/tkpd/thankyou/view/ReactNativeThankYouPageActivity.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/thankyou/view/ReactNativeThankYouPageActivity.java
@@ -208,11 +208,6 @@ public class ReactNativeThankYouPageActivity extends ReactFragmentActivity<React
     }
 
     @Override
-    public Fragment getReviewSellerFragment() {
-        return null;
-    }
-
-    @Override
     public void showAppFeedbackRatingDialog(FragmentManager fragmentManager, Context context, BottomSheets.BottomSheetDismissListener listener) {
 
     }
