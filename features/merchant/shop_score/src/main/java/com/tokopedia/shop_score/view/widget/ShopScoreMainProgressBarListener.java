b15b036b8775bb52251efce8e872607436d4a49b
yogieputra8 <yogie.putra8@gmail.com>
2020-07-08 11:25:25 +0700

    Rename package

--- features/merchant/shop_score/src/main/java/com/tokopedia/shop_score/view/widget/ShopScoreMainProgressBarListener.java
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.shop_score.view.widget;
-
-/**
- * @author sebastianuskh on 2/24/17.
- */
-public interface ShopScoreMainProgressBarListener {
-    void updateLimitViewOutside(int limit);
-}
