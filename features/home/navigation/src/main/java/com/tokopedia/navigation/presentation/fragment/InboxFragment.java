b9163da6e567492cf504d6de17dbe1930a600b23
lukaskris12 <lukaskris12@gmail.com>
2020-07-08 13:01:04 +0700

    fix recommendation index

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/InboxFragment.java
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/fragment/InboxFragment.java
@@ -119,7 +119,7 @@ public class InboxFragment extends BaseTestableParentFragment<GlobalNavComponent
             boolean wishlistStatusFromPdp = data.getBooleanExtra(WIHSLIST_STATUS_IS_WISHLIST,
                     false);
             int position = data.getIntExtra(PDP_EXTRA_UPDATED_POSITION, -1);
-            if(adapter.getList().size() < position) return;
+            if(position < 0 || adapter.getList().size() < position) return;
 
             if(adapter.getList().get(position) instanceof  Recommendation){
                 Recommendation recommendation = (Recommendation) adapter.getList().get(position);
