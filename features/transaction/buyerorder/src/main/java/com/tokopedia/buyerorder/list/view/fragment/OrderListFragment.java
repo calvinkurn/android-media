bdb05e7f08fa0311e8c0f020e62933f6dab000b7
Felicia Widjaja <fwidjaja@live.com>
2020-07-08 14:43:43 +0700

    add TopAdsUrlHitter onClick ATC

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/list/view/fragment/OrderListFragment.java
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/list/view/fragment/OrderListFragment.java
@@ -68,6 +68,7 @@ import com.tokopedia.design.quickfilter.QuickSingleFilterView;
 import com.tokopedia.design.quickfilter.custom.CustomViewRounderCornerFilterView;
 import com.tokopedia.design.text.SearchInputView;
 import com.tokopedia.topads.sdk.utils.ImpresionTask;
+import com.tokopedia.topads.sdk.utils.TopAdsUrlHitter;
 import com.tokopedia.trackingoptimizer.TrackingQueue;
 import com.tokopedia.transaction.purchase.interactor.TxOrderNetInteractor;
 import com.tokopedia.unifycomponents.Toaster;
@@ -177,6 +178,8 @@ public class OrderListFragment extends BaseDaggerFragment implements
     private SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
     private SimpleDateFormat format1 = new SimpleDateFormat(DATE_FORMAT_1, Locale.getDefault());
     private SimpleDateFormat format2 = new SimpleDateFormat(DATE_FORMAT_2, new Locale("ind", "IND"));
+
+    private String className = "com.tokopedia.buyerorder.list.view.fragment.OrderListFragment";
 
     @Inject
     OrderListAnalytics orderListAnalytics;
@@ -657,9 +660,9 @@ public class OrderListFragment extends BaseDaggerFragment implements
     }
 
     @Override
-    public void sendATCTrackingUrl(String url) {
+    public void sendATCTrackingUrl(String url, String productId, String productName, String imageUrl) {
         String clickUrl = url + "&click_source=ATC_direct_click";
-        new ImpresionTask(getActivity().getClass().getSimpleName(), userSession).execute(clickUrl);
+        new TopAdsUrlHitter(className).hitClickUrl(getContext(), clickUrl, productId, productName, imageUrl);
     }
 
     @Override
