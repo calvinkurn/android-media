ba4796ec7a26d3f28fbcd405291e8fb2d1b6a356
niranjan <niranjan.saxena@tokopedia.com>
2020-07-09 15:23:52 +0530

    added deprecated to topadsurl hitter old one

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/utils/TopAdsUrlHitter.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/utils/TopAdsUrlHitter.java
@@ -15,33 +15,33 @@ public class TopAdsUrlHitter {
     private String sourceClassName;
     private Context context;
 
-    @Deprecated
     public TopAdsUrlHitter(Context context){
         this.context = context;
     }
 
+    @Deprecated
     public TopAdsUrlHitter(String className) {
         impresionTask = new ImpresionTask(className);
         this.sourceClassName = className;
     }
 
+    @Deprecated
     public void hitClickUrl(Context context, String url, String productId, String productName, String imageUrl) {
         impresionTask.execute(url);
         TopAdsLogger.getInstance(context).save(url, TYPE_CLICK, sourceClassName, productId, productName, imageUrl);
     }
 
+    @Deprecated
     public void hitImpressionUrl(Context context, String url, String productId, String productName, String imageUrl) {
         impresionTask.execute(url);
         TopAdsLogger.getInstance(context).save(url, TYPE_IMPRESSION, sourceClassName, productId, productName, imageUrl);
     }
 
-    @Deprecated
     public void hitClickUrl(String className, String url, String productId, String productName, String imageUrl) {
         new ImpresionTask(className).execute(url);
         TopAdsLogger.getInstance(context).save(url, TYPE_CLICK, className, productId, productName, imageUrl);
     }
 
-    @Deprecated
     public void hitImpressionUrl(String className, String url, String productId, String productName, String imageUrl) {
         new ImpresionTask(className).execute(url);
         TopAdsLogger.getInstance(context).save(url, TYPE_IMPRESSION, className, productId, productName, imageUrl);
