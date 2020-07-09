d2bd2891a11e36a9ba007e434b6789cdba562050
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-07-09 17:20:16 +0800

    add bar chart widget and data UI model

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/model/BarChartDataUiModel.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.sellerhomecommon.presentation.model
+
+/**
+ * Created By @ilhamsuaib on 09/07/20
+ */
+
+class BarChartDataUiModel(
+        override val dataKey: String,
+        override var error: String
+) : BaseDataUiModel
