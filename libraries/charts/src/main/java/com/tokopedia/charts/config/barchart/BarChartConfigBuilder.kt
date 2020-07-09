456a05ee72b315222c0e36bda9080b91a38a1172
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-07-09 14:09:18 +0800

    add bar chart config class

---
+++ libraries/charts/src/main/java/com/tokopedia/charts/config/barchart/BarChartConfigBuilder.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.charts.config.barchart
+
+/**
+ * Created By @ilhamsuaib on 09/07/20
+ */
+
+class BarChartConfigBuilder {
+}
---
+++ libraries/charts/src/main/java/com/tokopedia/charts/config/barchart/model/BarChartConfig.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.charts.config.barchart.model
+
+/**
+ * Created By @ilhamsuaib on 09/07/20
+ */
+
+data class BarChartConfig(
+        val isRoundedBar: Boolean = true
+)
