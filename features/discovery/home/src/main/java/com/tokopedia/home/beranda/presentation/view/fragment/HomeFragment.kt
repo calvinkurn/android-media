c88f512c70a1f82f6dbebf8da73fb7365d9be3ae
yoasfs <yoas.fs@gmail.com>
2020-07-10 19:33:40 +0700

    test offest 0 second

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
@@ -1009,7 +1009,7 @@ open class HomeFragment : BaseDaggerFragment(),
 
     private fun setAutoRefreshOnHome(autoRefreshFlag: HomeFlag)  {
         initAutoRefreshHandler()
-        val serverOffsetTime = 0
+        val serverOffsetTime = 0L
         val expiredTime = DateHelper.getExpiredTime(autoRefreshFlag.eventTime)
         autoRefreshRunnable = getAutoRefreshRunnableThread(serverOffsetTime, expiredTime, autoRefreshHandler, this)
         runAutoRefreshJob(autoRefreshHandler, autoRefreshRunnable)
