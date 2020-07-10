72d9c16000329029494601767a91f935fdc7f667
noiz354 <noiz354@gmail.com>
2020-07-10 16:02:55 +0700

    1. add header.

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/data/datasource/CloudTopAdsDataSource.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/data/datasource/CloudTopAdsDataSource.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.tokopedia.authentication.AuthConstant;
+import com.tokopedia.authentication.AuthHelper;
 import com.tokopedia.config.GlobalConfig;
 import com.tokopedia.topads.sdk.base.Config;
 import com.tokopedia.topads.sdk.base.TKPDMapParam;
@@ -49,7 +51,7 @@ public class CloudTopAdsDataSource implements TopAdsDataSource {
                 .setBaseUrl(config.getBaseUrl() + URL_DISPLAY_ADS_V1_3)
                 .addHeader(TKPD_SESSION_ID, config.getSessionId())
                 .addHeader(X_DEVICE, "android-" + GlobalConfig.VERSION_NAME)
-                .header(AuthConstant.HEADER_RELEASE_TRACK, AuthHelper.Companion.getGetReleaseTrack().invoke(GlobalConfig.VERSION_NAME) )
+                .addHeader(AuthConstant.HEADER_RELEASE_TRACK, AuthHelper.Companion.getGetReleaseTrack().invoke(GlobalConfig.VERSION_NAME) )
                 .setMethod(HttpMethod.GET)
                 .addParameters(params)
                 .build();
