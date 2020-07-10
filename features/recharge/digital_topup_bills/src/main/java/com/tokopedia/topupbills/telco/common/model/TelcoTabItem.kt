b7d9838deb1131194c1c83ac27678f20e2677632
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-07-10 18:27:42 +0700

    change model to data class model

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/common/model/TelcoTabItem.kt
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/common/model/TelcoTabItem.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 /**
  * Created by nabillasabbaha on 09/05/19.
  */
-class TelcoTabItem(
+data class TelcoTabItem(
         val bundle: Bundle?,
         val title: String,
         val id: Long
