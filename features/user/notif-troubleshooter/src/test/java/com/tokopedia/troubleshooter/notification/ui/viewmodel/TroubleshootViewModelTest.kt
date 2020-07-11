6df9d37e70a797ac84c2ac29c40f75d64f720e1e
isfaaghyth <isfaaghyth@gmail.com>
2020-07-11 20:13:37 +0700

    feat(troubleshooter:test): added test coverage of troubleshooter

--- features/user/notif-troubleshooter/src/test/java/com/tokopedia/troubleshooter/notification/ui/viewmodel/TroubleshootViewModelTest.kt
+++ features/user/notif-troubleshooter/src/test/java/com/tokopedia/troubleshooter/notification/ui/viewmodel/TroubleshootViewModelTest.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.tokopedia.fcmcommon.FirebaseMessagingManager
-import com.tokopedia.troubleshooter.notification.data.FirebaseInstanceManager
 import com.tokopedia.troubleshooter.notification.data.domain.TroubleshootStatusUseCase
 import com.tokopedia.troubleshooter.notification.data.entity.NotificationSendTroubleshoot
 import com.tokopedia.troubleshooter.notification.data.entity.NotificationTroubleshoot
+import com.tokopedia.troubleshooter.notification.data.service.channel.NotificationChannelManager
+import com.tokopedia.troubleshooter.notification.data.service.fcm.FirebaseInstanceManager
+import com.tokopedia.troubleshooter.notification.data.service.notification.NotificationCompatManager
 import com.tokopedia.troubleshooter.notification.util.dispatchers.TestDispatcherProvider
 import com.tokopedia.troubleshooter.notification.util.isEqualsTo
 import io.mockk.coEvery
@@ -28,23 +30,31 @@ class TroubleshootViewModelTest {
     private val troubleshootUseCase: TroubleshootStatusUseCase = mockk(relaxed = true)
     private val messagingManager: FirebaseMessagingManager = mockk(relaxed = true)
     private val instanceManager: FirebaseInstanceManager = mockk(relaxed = true)
+    private val channelManager: NotificationChannelManager = mockk(relaxed = true)
+    private val compatManager: NotificationCompatManager = mockk(relaxed = true)
 
     private lateinit var viewModel: TroubleshootViewModel
     private val context: Context = mockk(relaxed = true)
 
     private val troubleshootObservable: Observer<NotificationSendTroubleshoot> = mockk(relaxed = true)
+    private val notificationObservable: Observer<Boolean> = mockk(relaxed = true)
     private val errorObservable: Observer<Throwable> = mockk(relaxed = true)
+    private val channelObservable: Observer<Int> = mockk(relaxed = true)
+
     private val dispatcherProvider = TestDispatcherProvider()
 
     @Before fun setUp() {
         viewModel = TroubleshootViewModel(
                 troubleshootUseCase,
+                channelManager,
+                compatManager,
                 messagingManager,
                 instanceManager,
-                context,
                 dispatcherProvider
         )
 
+        viewModel.notificationSetting.observeForever(notificationObservable)
+        viewModel.notificationImportance.observeForever(channelObservable)
         viewModel.troubleshoot.observeForever(troubleshootObservable)
         viewModel.error.observeForever(errorObservable)
     }
@@ -84,7 +94,55 @@ class TroubleshootViewModelTest {
         viewModel.getNewToken()
 
         verify { instanceManager.getNewToken(any()) }
+    }
+
+    @Test fun `it should update new token`() {
+        val token = "123"
+        viewModel.updateToken(token)
         verify { messagingManager.onNewToken(token) }
+    }
+
+    @Test fun `it should get importance notification channel correctly`() {
+        val expectedValue = -1000
+        val isAndroid26 = true
+        every { channelManager.hasNotificationChannel() } returns isAndroid26
+        every { channelManager.getNotificationChannel() } returns expectedValue
+
+        viewModel.getImportanceNotification()
+
+        verify { channelObservable.onChanged(expectedValue) }
+        viewModel.notificationImportance isEqualsTo expectedValue
+    }
+
+    @Test fun `it should cannot get importance notification channel`() {
+        val expectedValue = Int.MAX_VALUE
+        val isAndroid26 = false
+        every { channelManager.hasNotificationChannel() } returns isAndroid26
+
+        viewModel.getImportanceNotification()
+
+        verify { channelObservable.onChanged(expectedValue) }
+        viewModel.notificationImportance isEqualsTo expectedValue
+    }
+
+    @Test fun `it should return notification settings enable state`() {
+        val expectedValue = true
+        every { compatManager.isNotificationEnabled() } returns expectedValue
+
+        viewModel.isNotificationEnabled()
+
+        verify { notificationObservable.onChanged(expectedValue) }
+        viewModel.notificationSetting isEqualsTo expectedValue
+    }
+
+    @Test fun `it should return notification settings disable state`() {
+        val expectedValue = false
+        every { compatManager.isNotificationEnabled() } returns expectedValue
+
+        viewModel.isNotificationEnabled()
+
+        verify { notificationObservable.onChanged(expectedValue) }
+        viewModel.notificationSetting isEqualsTo expectedValue
     }
 
 }
