62deb2b6c687e24a767ce2a97c95e1c8ab6f87f5
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-07-13 15:15:02 +0700

    Improve analytics validator assertion

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/core/ValidatorAssertion.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/core/ValidatorAssertion.kt
@@ -3,6 +3,9 @@ package com.tokopedia.analyticsdebugger.validator.core
 import android.content.Context
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.analyticsdebugger.validator.Utils
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 import rx.Observable
 import rx.schedulers.Schedulers
 
@@ -10,13 +13,44 @@ fun assertAnalyticWithValidator(
         gtmLogDBSource: GtmLogDBSource,
         context: Context,
         queryFileName: String,
-        assertValidator:(Validator) -> Unit
+        assertValidator: (Validator) -> Unit
 ) {
     val testCases = getTestCases(context, queryFileName)
 
     val engine = ValidatorEngine(gtmLogDBSource)
     engine.compute(testCases).test {
         it.forEach { validator -> assertValidator(validator) }
+    }
+}
+
+fun getAnalyticsWithQuery(gtmLogDBSource: GtmLogDBSource,
+                          context: Context,
+                          queryFileName: String): List<Validator> {
+    val testCases = getTestCases(context, queryFileName)
+    return ValidatorEngine(gtmLogDBSource)
+            .compute(testCases)
+            .toBlocking()
+            .first()
+}
+
+fun hasAllSuccess(): Matcher<List<Validator>> {
+    return object : TypeSafeMatcher<List<Validator>>(ArrayList::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has missing anlalytics tracker")
+        }
+
+        override fun describeMismatchSafely(item: List<Validator>?, mismatchDescription: Description?) {
+            mismatchDescription
+                    ?.appendText(" has mismatch status on query number ")
+                    ?.appendValue(item?.indexOfFirst{ it.status != Status.SUCCESS })
+        }
+
+        override fun matchesSafely(result: List<Validator>): Boolean {
+            result.forEach {
+                if (it.status != Status.SUCCESS) return false
+            }
+            return true
+        }
     }
 }
 
