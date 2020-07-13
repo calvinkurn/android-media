d19238ba47f1d7368db1820b8faf65cb50a361da
hansenpw <hansen.wijaya@tokopedia.com>
2020-07-13 09:53:01 +0700

    fix bottomsheet installment ui

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/bottomsheet/InstallmentDetailBottomSheet.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/bottomsheet/InstallmentDetailBottomSheet.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.oneclickcheckout.order.view.bottomsheet
 
 import android.view.View
+import android.widget.LinearLayout
 import com.tokopedia.oneclickcheckout.R
 import com.tokopedia.oneclickcheckout.order.view.OrderSummaryPageFragment
 import com.tokopedia.unifycomponents.BottomSheetUnify
@@ -19,6 +20,13 @@ class InstallmentDetailBottomSheet {
                 setTitle("Pilih Jenis Pembayaran")
 
                 val child = View.inflate(fragment.context, R.layout.bottom_sheet_installment, null)
+                val ll = child.findViewById<LinearLayout>(R.id.main_content)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
+                ll.addView(View.inflate(fragment.context, R.layout.item_installment_detail, null), 0)
                 setChild(child)
                 show(it, null)
             }
