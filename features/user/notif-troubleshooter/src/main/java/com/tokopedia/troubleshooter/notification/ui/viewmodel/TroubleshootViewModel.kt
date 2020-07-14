36e601bca21b7f3f68bcf4bdc8be9cd8c3b2e68a
stevenfredian <steven.fredian@tokopedia.com>
2020-07-14 14:51:57 +0700

    handle log

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/viewmodel/TroubleshootViewModel.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/viewmodel/TroubleshootViewModel.kt
@@ -53,10 +53,38 @@ class TroubleshootViewModel @Inject constructor(
     private val _error = MutableLiveData<Throwable>()
     val error: LiveData<Throwable> get() = _error
 
+    private val _sendLog = MediatorLiveData<Boolean>()
+    val sendLog: LiveData<Boolean> get() = _sendLog
+
+    fun <T1, T2, T3> combineTuple(
+            f1: LiveData<T1>,
+            f2: LiveData<T2>,
+            f3: LiveData<T3>
+    ): LiveData<Triple<T1?, T2?, T3?>>
+            = MediatorLiveData<Triple<T1?, T2?, T3?>>().also { mediator ->
+        mediator.value = Triple(f1.value, f2.value, f3.value)
+
+        mediator.addSource(f1) { t1: T1? ->
+            val (_, t2, t3) = mediator.value!!
+            mediator.value = Triple(t1, t2, t3)
+        }
+
+        mediator.addSource(f2) { t2: T2? ->
+            val (t1, _, t3) = mediator.value!!
+            mediator.value = Triple(t1, t2, t3)
+        }
+
+        mediator.addSource(f3) { t3: T3? ->
+            val (t1, t2, _) = mediator.value!!
+            mediator.value = Triple(t1, t2, t3)
+        }
+    }
+
     init {
         _token.addSource(_troubleshoot) {
             getNewToken()
         }
+
     }
 
     override fun troubleshoot() {
