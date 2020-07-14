f8bc4fbab1e13ae9d4f46cb8a5e985235f09ea5a
elscione <yusufhendrawan06@gmail.com>
2020-07-14 16:34:41 +0700

    implement parcelable on user role data model to enable passing between activities

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/common/domain/model/SomGetUserRoleResponse.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/common/domain/model/SomGetUserRoleResponse.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.sellerorder.common.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -18,8 +20,32 @@ data class GoldGetUserShopInfo(
 data class SomGetUserRoleDataModel(
         @SerializedName("Roles")
         @Expose
-        val roles: List<Roles> = emptyList()
-)
+        var roles: List<Roles> = emptyList()
+): Parcelable {
+    constructor(parcel: Parcel): this() {
+        val rolesString: MutableList<String> = mutableListOf()
+        parcel.readStringList(rolesString)
+        roles = rolesString.map { Roles.valueOf(it) }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeStringList(roles.map { it.name })
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SomGetUserRoleDataModel> {
+        override fun createFromParcel(parcel: Parcel): SomGetUserRoleDataModel {
+            return SomGetUserRoleDataModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SomGetUserRoleDataModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 enum class Roles {
     MANAGE_SHOP,
