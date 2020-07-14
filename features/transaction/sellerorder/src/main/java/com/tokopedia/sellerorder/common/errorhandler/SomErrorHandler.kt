030f4ef8cfa46a7e426af2a4fb1a29d4e01573d0
elscione <yusufhendrawan06@gmail.com>
2020-07-14 13:24:57 +0700

    fix crash in filter fragment and add logging

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/common/errorhandler/SomErrorHandler.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/common/errorhandler/SomErrorHandler.kt
@@ -3,8 +3,25 @@ package com.tokopedia.sellerorder.common.errorhandler
 import com.crashlytics.android.Crashlytics
 import com.tokopedia.sellerorder.BuildConfig
 import com.tokopedia.sellerorder.common.exception.SomException
+import timber.log.Timber
+import java.io.PrintWriter
+import java.io.StringWriter
 
 object SomErrorHandler {
+    fun logMessage(throwable: Throwable, message: String) {
+        try {
+            if (!BuildConfig.DEBUG) {
+                val stringWriter = StringWriter()
+                throwable.printStackTrace(PrintWriter(stringWriter))
+                Crashlytics.log("$message $stringWriter")
+            } else {
+                Timber.e(throwable, message)
+            }
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+    }
+
     fun logExceptionToCrashlytics(throwable: Throwable, message: String) {
         try {
             if (!BuildConfig.DEBUG) {
--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/list/presentation/viewmodel/SomFilterViewModel.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/list/presentation/viewmodel/SomFilterViewModel.kt
@@ -9,6 +9,7 @@ import com.tokopedia.sellerorder.list.data.model.SomListAllFilter
 import com.tokopedia.sellerorder.list.domain.filter.SomGetAllFilterUseCase
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
 import javax.inject.Inject
 
 /**
@@ -29,16 +30,30 @@ class SomFilterViewModel @Inject constructor(dispatcher: SomDispatcherProvider,
     val orderTypeListResult: LiveData<Result<MutableList<SomListAllFilter.Data.OrderType>>>
         get() = _orderTypeListResult
 
+    private val _filterListResult = MutableLiveData<Result<Boolean>>()
+    val filterListResult: LiveData<Result<Boolean>>
+        get() = _filterListResult
+
+    @Suppress("UNCHECKED_CAST")
     fun loadSomFilterData(filterQuery: String) {
         launchCatchError(block = {
             somGetAllFilterUseCase.execute(filterQuery)
-            _shippingListResult.postValue(somGetAllFilterUseCase.getShippingListResult())
-            _statusOrderListResult.postValue(somGetAllFilterUseCase.getStatusOrderListResult())
-            _orderTypeListResult.postValue(somGetAllFilterUseCase.getOrderTypeListResult())
+            val results = mutableListOf<Result<Any>>(
+                    somGetAllFilterUseCase.getShippingListResult(),
+                    somGetAllFilterUseCase.getStatusOrderListResult(),
+                    somGetAllFilterUseCase.getOrderTypeListResult()
+            )
+            _shippingListResult.postValue(results[0] as Result<MutableList<SomListAllFilter.Data.ShippingList>>?)
+            _statusOrderListResult.postValue(results[1] as Result<MutableList<SomListAllFilter.Data.OrderFilterSomSingle.StatusList>>?)
+            _orderTypeListResult.postValue(results[2] as Result<MutableList<SomListAllFilter.Data.OrderType>>?)
+            val failingResult = results.filterIsInstance<Fail>()
+            if (failingResult.isEmpty()) {
+                _filterListResult.postValue(Success(true))
+            } else {
+                _filterListResult.postValue(Fail(failingResult.first().throwable))
+            }
         }, onError = {
-            _shippingListResult.postValue(Fail(it))
-            _statusOrderListResult.postValue(Fail(it))
-            _orderTypeListResult.postValue(Fail(it))
+            _filterListResult.postValue(Fail(it))
         })
     }
 
