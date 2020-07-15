c5338224c5bf0b7b28387306d7a598d8f09ea631
lukaskris12 <lukaskris12@gmail.com>
2020-07-15 12:08:07 +0700

    fix recent view not error

--- features/discovery/recentview/src/main/java/com/tokopedia/recentview/view/activity/RecentViewActivity.kt
+++ features/discovery/recentview/src/main/java/com/tokopedia/recentview/view/activity/RecentViewActivity.kt
@@ -1,11 +1,11 @@
 package com.tokopedia.recentview.view.activity
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.recentview.view.fragment.RecentViewFragment
 
-class RecentViewActivity : BaseSimpleActivity() {
-    override fun getNewFragment(): Fragment? = RecentViewFragment.createInstance(Bundle())
-
+class RecentViewActivity : BaseSimpleActivity(){
+    override fun getNewFragment(): Fragment? {
+        return RecentViewFragment.createInstance()
+    }
 }
