9d2c96eb5f7ee04e7a47a46651775ea136891a7a
Reivin <reivin.oktavianus@tokopedia.com>
2020-07-16 00:06:20 +0700

    feat (campaignstock): Fix quantity editor behaviour

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/SellableStockProductViewHolder.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/SellableStockProductViewHolder.kt
@@ -1,13 +1,16 @@
 package com.tokopedia.product.manage.feature.campaignstock.ui.adapter.viewholder
 
+import android.text.InputFilter
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.afterTextChanged
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.product.manage.R
 import com.tokopedia.product.manage.feature.campaignstock.ui.dataview.SellableStockProductUIModel
 import com.tokopedia.product.manage.feature.quickedit.common.constant.EditProductConstant
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
+import com.tokopedia.unifycomponents.QuantityEditorUnify
 import kotlinx.android.synthetic.main.item_campaign_stock_variant_editor.view.*
 
 class SellableStockProductViewHolder(itemView: View?,
@@ -17,18 +20,14 @@ class SellableStockProductViewHolder(itemView: View?,
     companion object {
         @LayoutRes
         val LAYOUT_RES = R.layout.item_campaign_stock_variant_editor
+
+        private const val MAXIMUM_LENGTH = 7
     }
 
     override fun bind(element: SellableStockProductUIModel) {
         with(itemView) {
             tv_campaign_stock_variant_editor_name?.text = element.productName
-            qte_campaign_stock_variant_editor?.run {
-                maxValue = EditProductConstant.MAXIMUM_STOCK
-                setValue(element.stock.toIntOrZero())
-                setValueChangedListener { stock, _, _ ->
-                    onVariantStockChanged(element.productId, stock)
-                }
-            }
+            qte_campaign_stock_variant_editor?.setElement(element)
             switch_campaign_stock_variant_editor?.run {
                 isChecked = element.isActive
                 setOnCheckedChangeListener { _, isChecked ->
@@ -42,5 +41,37 @@ class SellableStockProductViewHolder(itemView: View?,
                 }
             }
         }
+    }
+
+    private fun QuantityEditorUnify.setElement(element: SellableStockProductUIModel) {
+        val maxLength = InputFilter.LengthFilter(MAXIMUM_LENGTH)
+        editText.filters = arrayOf(maxLength)
+        minValue = EditProductConstant.MINIMUM_STOCK
+        maxValue = EditProductConstant.MAXIMUM_STOCK
+
+        setValue(element.stock.toIntOrZero())
+
+        editText.afterTextChanged {
+            val input = it
+            val stock = if(input.isNotEmpty()) {
+                input.toInt()
+            } else {
+                EditProductConstant.MINIMUM_STOCK
+            }
+            toggleQuantityEditorBtn(stock)
+            onVariantStockChanged(element.productId, stock)
+        }
+    }
+
+    private fun QuantityEditorUnify.toggleQuantityEditorBtn(stock: Int) {
+        val enableAddBtn = stock < EditProductConstant.MAXIMUM_STOCK
+        val enableSubtractBtn = stock > EditProductConstant.MINIMUM_STOCK
+
+        addButton.isEnabled = enableAddBtn
+        subtractButton.isEnabled = enableSubtractBtn
+    }
+
+    private fun String.toInt(): Int {
+        return replace(".", "").toIntOrZero()
     }
 }
--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/TotalStockEditorViewHolder.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/TotalStockEditorViewHolder.kt
@@ -1,11 +1,15 @@
 package com.tokopedia.product.manage.feature.campaignstock.ui.adapter.viewholder
 
+import android.text.InputFilter
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.afterTextChanged
+import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.product.manage.R
 import com.tokopedia.product.manage.feature.campaignstock.ui.dataview.TotalStockEditorUiModel
 import com.tokopedia.product.manage.feature.quickedit.common.constant.EditProductConstant
+import com.tokopedia.unifycomponents.QuantityEditorUnify
 import kotlinx.android.synthetic.main.item_campaign_stock_total_editor.view.*
 
 class TotalStockEditorViewHolder(itemView: View?,
@@ -15,15 +19,43 @@ class TotalStockEditorViewHolder(itemView: View?,
     companion object {
         @LayoutRes
         val LAYOUT_RES = R.layout.item_campaign_stock_total_editor
+
+        private const val MAXIMUM_LENGTH = 7
     }
 
     override fun bind(element: TotalStockEditorUiModel) {
-        itemView.qte_campaign_stock_amount?.run {
-            maxValue = EditProductConstant.MAXIMUM_STOCK
-            setValue(element.totalStock)
-            setValueChangedListener { stock, _, _ ->
-                onTotalStockChanged(stock)
+        itemView.qte_campaign_stock_amount?.setElement(element)
+    }
+
+    private fun QuantityEditorUnify.setElement(element: TotalStockEditorUiModel) {
+        val maxLength = InputFilter.LengthFilter(MAXIMUM_LENGTH)
+        editText.filters = arrayOf(maxLength)
+        minValue = EditProductConstant.MINIMUM_STOCK
+        maxValue = EditProductConstant.MAXIMUM_STOCK
+
+        setValue(element.totalStock)
+
+        editText.afterTextChanged {
+            val input = it
+            val stock = if(input.isNotEmpty()) {
+                input.toInt()
+            } else {
+                EditProductConstant.MINIMUM_STOCK
             }
+            toggleQuantityEditorBtn(stock)
+            onTotalStockChanged(stock)
         }
+    }
+
+    private fun QuantityEditorUnify.toggleQuantityEditorBtn(stock: Int) {
+        val enableAddBtn = stock < EditProductConstant.MAXIMUM_STOCK
+        val enableSubtractBtn = stock > EditProductConstant.MINIMUM_STOCK
+
+        addButton.isEnabled = enableAddBtn
+        subtractButton.isEnabled = enableSubtractBtn
+    }
+
+    private fun String.toInt(): Int {
+        return replace(".", "").toIntOrZero()
     }
 }
