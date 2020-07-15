e5a54765c1cf070c5a7c0fe0b8c531aeeb23a65d
lalit <lalit.singh@tokopedia.com>
2020-07-15 15:58:05 +0530

    - auto withdrawal code structure added

---
+++ features/payment/withdraw/src/main/java/com/tokopedia/withdraw/auto_withdrawal/di/module/GQLQueryModule.kt
@@ -0,0 +1,38 @@
+package com.tokopedia.withdraw.auto_withdrawal.di.module
+
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
+import com.tokopedia.withdraw.R
+import com.tokopedia.withdraw.auto_withdrawal.QUERY_GET_AUTO_WD_STATUS
+import com.tokopedia.withdraw.auto_withdrawal.QUERY_GET_INFO_AUTO_WD
+import com.tokopedia.withdraw.auto_withdrawal.QUERY_GET_TNC_AUTO_WD
+import com.tokopedia.withdraw.auto_withdrawal.QUERY_UPSERT_AUTO_WD_DATA
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+
+@Module
+class GQLQueryModule {
+
+    @Provides
+    @Named(QUERY_GET_AUTO_WD_STATUS)
+    fun provideRawTGetAutoWDStatus(@ApplicationContext context: Context): String =
+            GraphqlHelper.loadRawString(context.resources, R.raw.query_get_auto_wd_status)
+
+    @Provides
+    @Named(QUERY_GET_INFO_AUTO_WD)
+    fun provideRawGetInfoAutoWD(@ApplicationContext context: Context): String =
+            GraphqlHelper.loadRawString(context.resources, R.raw.query_get_info_auto_wd)
+
+    @Provides
+    @Named(QUERY_GET_TNC_AUTO_WD)
+    fun provideRawGetTNCAutoWD(@ApplicationContext context: Context): String =
+            GraphqlHelper.loadRawString(context.resources, R.raw.query_get_tnc_auto_wd)
+
+    @Provides
+    @Named(QUERY_UPSERT_AUTO_WD_DATA)
+    fun provideRawUpsertAutoWDData(@ApplicationContext context: Context): String =
+            GraphqlHelper.loadRawString(context.resources, R.raw.query_upsert_auto_wd_data)
+
+}
---
+++ features/payment/withdraw/src/main/java/com/tokopedia/withdraw/auto_withdrawal/di/module/ViewModelModule.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.withdraw.auto_withdrawal.di.module
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
+import com.tokopedia.withdraw.auto_withdrawal.presentation.viewModel.AutoWDSettingsViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AutoWDSettingsViewModel::class)
+    internal abstract fun autoWDSettingsViewModel(viewModel: AutoWDSettingsViewModel): ViewModel
+
+}
