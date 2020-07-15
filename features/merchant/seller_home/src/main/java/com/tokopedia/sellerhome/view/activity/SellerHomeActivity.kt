995364714ebc52b5129f0bf3a61a2bcfb8f7fe2e
rezagama <reza.hidayat@tokopedia.com>
2020-07-15 13:28:09 +0700

    Early return after start old seller home

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/activity/SellerHomeActivity.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/activity/SellerHomeActivity.kt
@@ -88,7 +88,9 @@ class SellerHomeActivity : BaseActivity(), SellerHomeFragment.Listener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         initInjector()
-        startOldSellerHomeIfEnabled()
+        if(startOldSellerHomeIfEnabled()) {
+            return
+        }
         initPerformanceMonitoring()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sah_seller_home)
@@ -137,12 +139,15 @@ class SellerHomeActivity : BaseActivity(), SellerHomeFragment.Listener {
         performanceMonitoringSellerHomelayout?.stopTrace()
     }
 
-    private fun startOldSellerHomeIfEnabled() {
+    private fun startOldSellerHomeIfEnabled(): Boolean {
         if(remoteConfig.isNewSellerHomeDisabled()) {
-            val intent = com.tokopedia.sellerhome.view.oldactivity.SellerHomeActivity.createIntent(this)
-            startActivity(intent)
+            val oldSellerHome = com.tokopedia.sellerhome.view.oldactivity.SellerHomeActivity.createIntent(this)
+            oldSellerHome.data = intent.data
+            startActivity(oldSellerHome)
             finish()
+            return true
         }
+        return false
     }
 
     private fun setupToolbar() {
