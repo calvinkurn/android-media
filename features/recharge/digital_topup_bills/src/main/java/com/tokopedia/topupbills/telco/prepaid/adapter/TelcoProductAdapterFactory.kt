9ffd88d87d73d3566b1cc58e8ed6e7cea15c48eb
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-07-17 17:00:17 +0700

    remove unused class

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/prepaid/adapter/TelcoProductAdapterFactory.kt
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/prepaid/adapter/TelcoProductAdapterFactory.kt
@@ -3,13 +3,9 @@ package com.tokopedia.topupbills.telco.prepaid.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
-import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
-import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.topupbills.telco.data.TelcoCatalogDataCollection
 import com.tokopedia.topupbills.telco.data.TelcoProduct
-import com.tokopedia.topupbills.telco.prepaid.adapter.viewholder.TelcoProductEmptyViewHolder
-import com.tokopedia.topupbills.telco.prepaid.adapter.viewholder.TelcoProductShimmeringViewHolder
 import com.tokopedia.topupbills.telco.prepaid.adapter.viewholder.TelcoProductTitleViewHolder
 import com.tokopedia.topupbills.telco.prepaid.adapter.viewholder.TelcoProductViewHolder
 
@@ -20,14 +16,8 @@ class TelcoProductAdapterFactory(val productType: Int, val listener: TelcoProduc
         return when (type) {
             TelcoProductTitleViewHolder.LAYOUT -> TelcoProductTitleViewHolder(parent)
             TelcoProductViewHolder.LAYOUT -> TelcoProductViewHolder(parent, productType, listener)
-            TelcoProductEmptyViewHolder.LAYOUT -> TelcoProductEmptyViewHolder(parent)
-            TelcoProductShimmeringViewHolder.LAYOUT -> TelcoProductShimmeringViewHolder(parent)
             else -> super.createViewHolder(parent, type)
         }
-    }
-
-    override fun type(viewModel: EmptyModel?): Int {
-        return TelcoProductEmptyViewHolder.LAYOUT
     }
 
     fun type(telcoProduct: TelcoProduct): Int {
@@ -36,9 +26,5 @@ class TelcoProductAdapterFactory(val productType: Int, val listener: TelcoProduc
 
     fun type(telcoCatalogDataCollection: TelcoCatalogDataCollection): Int {
         return TelcoProductTitleViewHolder.LAYOUT
-    }
-
-    override fun type(viewModel: LoadingModel?): Int {
-        return TelcoProductShimmeringViewHolder.LAYOUT
     }
 }
