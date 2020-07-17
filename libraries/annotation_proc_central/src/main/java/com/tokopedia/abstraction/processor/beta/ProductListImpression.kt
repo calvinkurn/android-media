11be0161a9e84b41f1fbde84462f33a7f9225a92
noiz354 <noiz354@gmail.com>
2020-07-17 10:12:58 +0700

    1. add separate data tracking push for checking the error. 2. add remote config for separate data tracking push. 3. separate production ready rules and beta rules using remote config + different eventAction and eventCategory.

--- libraries/annotation_proc_central/src/main/java/com/tokopedia/abstraction/processor/beta/ProductListImpression.kt
+++ libraries/annotation_proc_central/src/main/java/com/tokopedia/abstraction/processor/beta/ProductListImpression.kt
@@ -1,15 +1,15 @@
-package com.tokopedia.abstraction.processor
+package com.tokopedia.abstraction.processor.beta
 
 
-import com.tokopedia.analytic.annotation.ErrorHandler
-import com.tokopedia.analytic.annotation.Key
-import com.tokopedia.analytic.annotation.Logger
+import com.tokopedia.analytic.annotation.*
 import com.tokopedia.analytic_constant.Event
 import com.tokopedia.analytic_constant.Param
 import com.tokopedia.annotation.AnalyticEvent
 import com.tokopedia.annotation.BundleThis
 import com.tokopedia.annotation.defaultvalues.DefaultValueLong
 import com.tokopedia.annotation.defaultvalues.DefaultValueString
+import com.tokopedia.checkers.ProductListImpressionChecker
+import com.tokopedia.checkers.ProductListImpressionProductChecker
 import com.tokopedia.firebase.analytic.rules.ProductImpressionsRules
 import com.tokopedia.util.GTMErrorHandlerImpl
 import com.tokopedia.util.logger.GTMLoggerImpl
@@ -22,13 +22,16 @@ import com.tokopedia.util.logger.GTMLoggerImpl
 @AnalyticEvent(true, Event.VIEW_ITEM_LIST, ProductImpressionsRules::class)
 data class ProductListImpression(
         val item_list: String,
+        @CustomChecker(ProductListImpressionChecker::class, Level.IGNORE, functionName = ["isSizeBiggerThanOne"])
         val items: List<ProductListImpressionProduct>,
         @DefaultValueString("")
         val currentSite: String?,
+        @CustomChecker(ProductListImpressionChecker::class, Level.IGNORE, functionName = ["isContainValidTerm"])
         @DefaultValueString("")
         val event: String?,
         @DefaultValueString("")
         val eventCategory: String?,
+        @CustomChecker(ProductListImpressionChecker::class, Level.IGNORE, functionName = ["isContainInvalidTerm"])
         @DefaultValueString("")
         val eventAction: String?,
         @DefaultValueString("")
@@ -57,11 +60,13 @@ data class ProductListImpressionProduct(
         val category: String,
         @Key(Param.ITEM_VARIANT)
         val variant: String,
+        @CustomChecker(ProductListImpressionProductChecker::class, Level.IGNORE, functionName = ["isPriceNotZero"])
         @Key(Param.PRICE)
         val price: Double,
         @DefaultValueString("IDR")
         @Key(Param.CURRENCY)
         val currency: String?,
+        @CustomChecker(ProductListImpressionProductChecker::class, Level.IGNORE, functionName = ["isIndexNotZero"])
         @DefaultValueLong(1)
         @Key(Param.INDEX)
         val index: Long,
@@ -70,9 +75,11 @@ data class ProductListImpressionProduct(
         @DefaultValueString("none / other")
         @Key(KEY_DIMENSION_40)
         val dimension40: String?,
+//        @CustomChecker(ProductListImpressionProductChecker::class, Level.IGNORE, "isOnlyMainApp")
         @DefaultValueString("")
         @Key("dimension87")
         val dimension87: String?,
+//        @CustomChecker(ProductListImpressionProductChecker::class, Level.IGNORE, "isOnlyMainApp")
         @DefaultValueString("")
         @Key("dimension88")
         val dimension88: String?
