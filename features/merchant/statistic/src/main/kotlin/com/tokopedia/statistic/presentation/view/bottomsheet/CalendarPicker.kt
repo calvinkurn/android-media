619d897d6f14d3c71b0f600eff5348150782b822
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-07-17 17:52:18 +0800

    show default selected date

--- features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/bottomsheet/CalendarPicker.kt
+++ features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/bottomsheet/CalendarPicker.kt
@@ -1,7 +1,7 @@
 package com.tokopedia.statistic.presentation.view.bottomsheet
 
 import android.content.Context
-import android.os.Handler
+import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -22,8 +22,8 @@ class CalendarPicker(
         private val mContext: Context
 ) : BottomSheetUnify() {
 
+    private var mode: CalendarPickerView.SelectionMode = CalendarPickerView.SelectionMode.SINGLE
     var selectedDates: List<Date> = emptyList()
-        private set
 
     private val calendarView: CalendarPickerView?
 
@@ -36,11 +36,13 @@ class CalendarPicker(
         setStyle(DialogFragment.STYLE_NORMAL, R.style.StcDialogStyle)
     }
 
-    private fun setupView(child: View) = with(child) {
-        edtStcDate.label = context.getString(R.string.stc_date)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setDefaultSelectedDate()
     }
 
     fun init(mode: CalendarPickerView.SelectionMode): CalendarPicker {
+        this.mode = mode
         val days90 = 90L
         val minDate = Date(DateTimeUtil.getNPastDaysTimestamp(days90))
         val maxDate = Date(DateTimeUtil.getNNextDaysTimestamp(1))
@@ -57,7 +59,7 @@ class CalendarPicker(
                         }
                         else -> selectDateRange(cpv)
                     }
-                    showSelectedDate(cpv.selectedDates, mode)
+                    showSelectedDate()
                 }
 
                 override fun onDateUnselected(date: Date) {
@@ -66,6 +68,28 @@ class CalendarPicker(
             })
         }
         return this
+    }
+
+    fun showDatePicker(fm: FragmentManager, tag: String = CalendarPicker::class.java.simpleName) {
+        show(fm, tag)
+    }
+
+    private fun setupView(child: View) = with(child) {
+        edtStcDate.label = context.getString(R.string.stc_date)
+    }
+
+    private fun setDefaultSelectedDate() {
+        calendarView?.let { cpv ->
+            val startDate = selectedDates.firstOrNull()
+            val endDate = selectedDates.lastOrNull()
+            startDate?.let {
+                cpv.selectDate(it)
+            }
+            endDate?.let {
+                cpv.selectDate(it)
+            }
+            showSelectedDate()
+        }
     }
 
     private fun selectDateRange(cpv: CalendarPickerView) {
@@ -82,14 +106,15 @@ class CalendarPicker(
                 cpv.selectDate(today, true)
             }
         }
+        DateTimeUtil
         this.selectedDates = cpv.selectedDates
         dismiss()
     }
 
-    private fun showSelectedDate(dates: List<Date>, mode: CalendarPickerView.SelectionMode) {
+    private fun showSelectedDate() {
         view?.edtStcDate?.run {
-            val startDate = dates.firstOrNull()
-            val endDate = dates.lastOrNull()
+            val startDate = selectedDates.firstOrNull()
+            val endDate = selectedDates.lastOrNull()
             if (startDate != null && endDate != null) {
                 valueStr = if (mode == CalendarPickerView.SelectionMode.SINGLE) {
                     DateTimeUtil.format(startDate.time, "dd MMM yyyy")
@@ -98,9 +123,5 @@ class CalendarPicker(
                 }
             }
         }
-    }
-
-    fun showDatePicker(fm: FragmentManager, tag: String = CalendarPicker::class.java.simpleName) {
-        show(fm, tag)
     }
 }
