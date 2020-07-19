e0ced18f9a671c881be0f7a401373d0ee5b78cb9
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-07-19 18:06:53 +0700

    verify view empty_chat_list_buyer_only

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatlist/activity/ChatListActivityTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatlist/activity/ChatListActivityTest.kt
@@ -2,23 +2,23 @@ package com.tokopedia.topchat.chatlist.activity
 
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.topchat.AndroidFileUtil
+import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatlist.pojo.ChatListPojo
 import com.tokopedia.topchat.stub.chatlist.activity.ChatListActivityStub
 import com.tokopedia.topchat.stub.chatlist.usecase.ChatListGraphqlUseCase
 import com.tokopedia.topchat.stub.common.UserSessionStub
-import io.mockk.*
-import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
-import org.junit.After
+import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -65,7 +65,16 @@ class ChatListActivityTest {
 
         // When
         activity.setupTestFragment(chatListUseCase)
-        Thread.sleep(2500)
+
+        // Then
+        onView(withId(R.id.thumbnail))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.title))
+                .check(matches(withText("Belum ada chat")))
+        onView(withId(R.id.subtitle))
+                .check(matches(withText("Coba ngobrol dengan teman penjual, yuk!")))
+        onView(withId(R.id.btnCta))
+                .check(matches(not(isDisplayed())))
     }
 
     @Test
