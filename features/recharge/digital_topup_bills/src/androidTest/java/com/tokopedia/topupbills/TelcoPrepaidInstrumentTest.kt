b6aee309a4aba63f842b6d5a4f0951991e27b55c
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-07-20 12:00:26 +0700

    fix instrument test telco

--- features/recharge/digital_topup_bills/src/androidTest/java/com/tokopedia/topupbills/TelcoPrepaidInstrumentTest.kt
+++ features/recharge/digital_topup_bills/src/androidTest/java/com/tokopedia/topupbills/TelcoPrepaidInstrumentTest.kt
@@ -1,35 +1,27 @@
 package com.tokopedia.topupbills
 
-import android.util.Log
-import androidx.test.filters.SmallTest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
 import com.tokopedia.topupbills.telco.view.activity.TelcoPrepaidActivity
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-//@RunWith(AndroidJUnit4.class)
-@SmallTest
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class TelcoPrepaidInstrumentTest {
     @get:Rule
     var mActivityRule: ActivityTestRule<TelcoPrepaidActivity> = ActivityTestRule(TelcoPrepaidActivity::class.java)
 
-//    @Throws(Throwable::class)
-//    private fun setContentView(layoutId: Int) {
-//        val activity: Activity = mActivityRule.getActivity()
-//        mActivityRule.runOnUiThread { activity.setContentView(layoutId) }
-//    }
-
     @Test
-    @Throws(Throwable::class)
-    fun inflation() {
-        Log.d("Test", "test")
+    fun testHomeLayout() {
+        onView(withId(R.id.page_container)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.loading_telco_shimmering)).check(ViewAssertions.matches(isDisplayed()))
+        Thread.sleep(1000)
+        onView(withId(R.id.telco_input_number)).check(ViewAssertions.matches(isDisplayed()))
     }
-//
-//    private fun getActivity(): Activity? {
-//        return mActivityRule.getActivity()
-//    }
-//
-//    private fun getInstrumentation(): Instrumentation? {
-//        return InstrumentationRegistry.getInstrumentation()
-//    }
 }
