c5ba5b68bdf68be4da2547515b0ad4bf3eb915ed
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2020-07-21 23:11:55 +0700

    Fix CompileSdk 29 checkout & shop_settings

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/BaseShopEtalaseViewModel.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/BaseShopEtalaseViewModel.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.shop.settings.etalase.data
 
+import android.os.Parcel
 import android.os.Parcelable
+
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.shop.common.constant.ShopEtalaseTypeDef
 import com.tokopedia.shop.common.graphql.data.shopetalase.ShopEtalaseModel
@@ -9,17 +11,20 @@ import com.tokopedia.shop.settings.etalase.view.adapter.factory.BaseShopEtalaseF
 /**
  * Created by hendry on 23/08/18.
  */
-abstract class BaseShopEtalaseViewModel(
-        var id: String = "",
-        var name: String = "",
-        var count: Int = 0,
-        @ShopEtalaseTypeDef
-        var type: Int = 0,
-        var isPrimaryEtalase: Boolean = false
-) : Parcelable, Visitable<BaseShopEtalaseFactory> {
+abstract class BaseShopEtalaseViewModel() : Parcelable, Visitable<BaseShopEtalaseFactory> {
+    var id: String = ""
+        protected set
+    var name: String = ""
+    var count: Int = 0
+        protected set
+    @ShopEtalaseTypeDef
+    var type: Int = 0
+        protected set
+    var isPrimaryEtalase: Boolean = false
+        protected set
 
     constructor(shopEtalaseModel: ShopEtalaseModel,
-                isPrimaryEtalase: Boolean) : this() {
+                isPrimaryEtalase: Boolean): this() {
         this.id = shopEtalaseModel.id
         this.name = shopEtalaseModel.name
         this.count = shopEtalaseModel.count
@@ -30,4 +35,23 @@ abstract class BaseShopEtalaseViewModel(
     abstract override fun type(typeFactory: BaseShopEtalaseFactory): Int
 
 
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(this.id)
+        dest.writeString(this.name)
+        dest.writeInt(this.count)
+        dest.writeInt(this.type)
+        dest.writeByte(if (this.isPrimaryEtalase) 1.toByte() else 0.toByte())
+    }
+
+    protected constructor(`in`: Parcel) : this() {
+        this.id = `in`.readString()?: ""
+        this.name = `in`.readString()?: ""
+        this.count = `in`.readInt()
+        this.type = `in`.readInt()
+        this.isPrimaryEtalase = `in`.readByte().toInt() != 0
+    }
 }
--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/ShopEtalaseTitleViewModel.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/ShopEtalaseTitleViewModel.kt
@@ -1,13 +1,31 @@
 package com.tokopedia.shop.settings.etalase.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.tokopedia.shop.settings.etalase.view.adapter.factory.BaseShopEtalaseFactory
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
-class ShopEtalaseTitleViewModel(var nameTo: String) : BaseShopEtalaseViewModel(name = nameTo) {
+/**
+ * Created by hendry on 20/08/18.
+ */
+class ShopEtalaseTitleViewModel : BaseShopEtalaseViewModel {
+    constructor(nameTo: String) : super() {
+        name = nameTo
+    }
+
+    constructor(`in`: Parcel) : super(`in`) {}
 
     override fun type(typeFactory: BaseShopEtalaseFactory): Int {
         return typeFactory.type(this)
+    }
+
+    companion object CREATOR : Parcelable.Creator<BaseShopEtalaseViewModel> {
+        override fun createFromParcel(source: Parcel): BaseShopEtalaseViewModel {
+            return ShopEtalaseTitleViewModel(source)
+        }
+
+        override fun newArray(size: Int): Array<BaseShopEtalaseViewModel?> {
+            return arrayOfNulls(size)
+        }
     }
 
 }
