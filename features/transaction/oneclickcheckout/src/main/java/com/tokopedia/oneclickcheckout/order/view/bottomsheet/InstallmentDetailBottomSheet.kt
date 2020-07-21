13e9253a3163d446f0187c3d7c6af16c261ca14b
hansenpw <hansen.wijaya@tokopedia.com>
2020-07-21 17:56:17 +0700

    use webview terms

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/bottomsheet/InstallmentDetailBottomSheet.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/bottomsheet/InstallmentDetailBottomSheet.kt
@@ -1,10 +1,10 @@
 package com.tokopedia.oneclickcheckout.order.view.bottomsheet
 
 import android.view.View
+import android.webkit.WebView
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RadioButton
-import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.oneclickcheckout.R
@@ -60,32 +60,76 @@ class InstallmentDetailBottomSheet {
     }
 
     private fun setupTerms(child: View, fragment: OrderSummaryPageFragment) {
-        val body1 = child.findViewById<Typography>(R.id.body1)
+        val webView = child.findViewById<WebView>(R.id.web_view_terms_and_condition)
         val htmlText = """
+<html>
+	<style>
+    body {
+    background: #FAFBFC;
+    }
+     h1 {
+     	font-family: SF Pro Text;
+        font-style: normal;
+        font-weight: bold;
+        font-size: 12px;
+        line-height: 14px;
+        color: #4A4A4A;
+     }
+     ul li {
+     	color: #42B549;
+     }
+     ol li {
+     font-family: SFProText;
+    font-size: 12px;
+    line-height: 18px;
+    color: rgba(0, 0, 0, 0.38);
+    margin-top:8px;
+    }
+    ol {
+    padding-inline-start: 12px;
+    }
+    p {
+        font-family: SFProText;
+    font-size: 12px;
+    line-height: 18px;
+    color: rgba(0, 0, 0, 0.38);
+    }
+    </style>
+    <body>
+        <ul>
+          <li>
+            <h1>Konversi Cicilan 0%</h1>
             <ol>
-            <li> Pihak bank butuh 7-14 hari kerja untuk mengubah tagihanmu dari pembayaran penuh (full payment) jadi cicilan</li>
-            <li> Jika tagihan kartu kreditmu terbit dan ditagih secara penuh, kamu bisa lakukan pembayaran minimum dulu.</li>
+              <li>
+                Pihak bank butuh 7-14 hari kerja untuk mengubah tagihanmu dari pembayaran penuh (full payment) jadi
+                cicilan
+              </li>
+              <li>
+                Jika tagihan kartu kreditmu terbit dan ditagih secara penuh, kamu bisa lakukan pembayaran minimum
+                dulu.
+              </li>
             </ol>
+          </li>
+          <li>
+            <h1>Biaya Layanan</h1>
+            <p>
+              Biaya layanan ini diberlakukan untuk tujuan pemeliharaan sistem dan peningkatan layanan dalam
+              bertransaksi melalui Tokopedia.
+            </p>
+          </li>
+        </ul>
+    </body>
+</html>
         """.trimIndent()
-        body1.text = MethodChecker.fromHtml(htmlText)
+        webView.loadData(htmlText, "text/html", "UTF-8")
         val ivExpandTerms = child.findViewById<ImageView>(R.id.iv_expand_terms)
         ivExpandTerms.setOnClickListener {
             val newRotation = if (ivExpandTerms.rotation == 0f) 180f else 0f
             ivExpandTerms.rotation = newRotation
             if (newRotation == 0f) {
-                child.findViewById<View>(R.id.bullet1).gone()
-                child.findViewById<View>(R.id.header1).gone()
-                child.findViewById<View>(R.id.body1).gone()
-                child.findViewById<View>(R.id.bullet2).gone()
-                child.findViewById<View>(R.id.header2).gone()
-                child.findViewById<View>(R.id.body2).gone()
+                webView.gone()
             } else {
-                child.findViewById<View>(R.id.bullet1).visible()
-                child.findViewById<View>(R.id.header1).visible()
-                child.findViewById<View>(R.id.body1).visible()
-                child.findViewById<View>(R.id.bullet2).visible()
-                child.findViewById<View>(R.id.header2).visible()
-                child.findViewById<View>(R.id.body2).visible()
+                webView.visible()
             }
         }
     }
