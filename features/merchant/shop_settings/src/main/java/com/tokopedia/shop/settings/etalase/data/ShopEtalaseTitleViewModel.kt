00ac0f7488aa116b2c4ba19c025af694fcd800de
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2020-07-21 22:45:00 +0700

    Fix CompileSdk 29 shop_common & shop_settings

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/ShopEtalaseTitleViewModel.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/etalase/data/ShopEtalaseTitleViewModel.kt
@@ -1,33 +1,13 @@
 package com.tokopedia.shop.settings.etalase.data
 
-import android.os.Parcel
-import android.os.Parcelable
-
-import com.tokopedia.shop.common.graphql.data.shopetalase.ShopEtalaseModel
 import com.tokopedia.shop.settings.etalase.view.adapter.factory.BaseShopEtalaseFactory
+import kotlinx.android.parcel.Parcelize
 
-/**
- * Created by hendry on 20/08/18.
- */
-class ShopEtalaseTitleViewModel : BaseShopEtalaseViewModel {
-    constructor(nameTo: String) : super() {
-        name = nameTo
-    }
-
-    constructor(`in`: Parcel) : super(`in`) {}
+@Parcelize
+class ShopEtalaseTitleViewModel(var nameTo: String) : BaseShopEtalaseViewModel(name = nameTo) {
 
     override fun type(typeFactory: BaseShopEtalaseFactory): Int {
         return typeFactory.type(this)
-    }
-
-    companion object CREATOR : Parcelable.Creator<BaseShopEtalaseViewModel> {
-        override fun createFromParcel(source: Parcel): BaseShopEtalaseViewModel {
-            return ShopEtalaseTitleViewModel(source)
-        }
-
-        override fun newArray(size: Int): Array<BaseShopEtalaseViewModel?> {
-            return arrayOfNulls(size)
-        }
     }
 
 }
