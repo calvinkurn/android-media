ab76a7835f49e864d32831bc787c0c8c5ad46113
kenny <kenny.hadisaputra@tokopedia.com>
2020-07-22 22:44:02 +0700

    change yield to check isActive

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -422,7 +422,7 @@ class PlayViewModel @Inject constructor(
                 _observableVideoStream.value = completeInfoUiModel.videoStream
                 _observableEvent.value = completeInfoUiModel.event
 
-                yield()
+                if (!isActive) return@launchCatchError
                 if (completeInfoUiModel.videoPlayer.isGeneral) playGeneralVideoStream(channel)
                 else playVideoManager.release()
 
