5db743629671d59c17100b2e51aed29e21b402ae
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2020-07-23 18:36:10 +0700

    revert SearchParameter.kt

--- features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/model/SearchParameter.kt
+++ features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/model/SearchParameter.kt
@@ -1,16 +1,17 @@
 package com.tokopedia.discovery.common.model
 
+import android.os.Parcel
 import android.os.Parcelable
 import com.tokopedia.discovery.common.constants.SearchApiConst
 import com.tokopedia.discovery.common.utils.URLParser
-import kotlinx.android.parcel.IgnoredOnParcel
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
 
-    @IgnoredOnParcel
     private var searchParameterHashMap = URLParser(deepLinkUri).paramKeyValueMapDecoded
+
+    constructor(parcel: Parcel) : this(parcel.readString() ?: "") {
+        parcel.readMap(searchParameterHashMap as Map<*, *>, String::class.java.classLoader)
+    }
 
     constructor(searchParameter: SearchParameter) : this(searchParameter.deepLinkUri) {
         setSearchParameterHashMap(HashMap(searchParameter.searchParameterHashMap))
@@ -22,19 +23,19 @@ class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
         searchParameterHashMap.values.remove(null)
     }
 
-    private fun setSearchParameterHashMap(searchParameterHashMap : HashMap<String, String>) {
+    private fun setSearchParameterHashMap(searchParameterHashMap: HashMap<String, String>) {
         this.searchParameterHashMap = searchParameterHashMap
     }
 
-    fun getSearchParameterHashMap() : HashMap<String, String> {
+    fun getSearchParameterHashMap(): HashMap<String, String> {
         return searchParameterHashMap
     }
 
-    fun getSearchParameterMap() : Map<String, Any> {
+    fun getSearchParameterMap(): Map<String, Any> {
         return searchParameterHashMap as Map<String, Any>
     }
 
-    fun contains(key: String) : Boolean {
+    fun contains(key: String): Boolean {
         return searchParameterHashMap.contains(key)
     }
 
@@ -42,7 +43,7 @@ class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
         searchParameterHashMap[key] = value
     }
 
-    fun get(key: String) : String {
+    fun get(key: String): String {
         return searchParameterHashMap[key] ?: ""
     }
 
@@ -50,11 +51,11 @@ class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
         searchParameterHashMap.remove(key)
     }
 
-    fun getBoolean(key: String) : Boolean {
+    fun getBoolean(key: String): Boolean {
         return get(key).toBoolean()
     }
 
-    fun getInteger(key: String) : Int {
+    fun getInteger(key: String): Int {
         return try {
             get(key).toInt()
         } catch (e: NumberFormatException) {
@@ -66,7 +67,7 @@ class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
         set(SearchApiConst.Q, query)
     }
 
-    fun getSearchQuery() : String {
+    fun getSearchQuery(): String {
         return when {
             contains(SearchApiConst.Q) -> get(SearchApiConst.Q)
             contains(SearchApiConst.KEYWORD) -> get(SearchApiConst.KEYWORD)
@@ -74,4 +75,22 @@ class SearchParameter(private val deepLinkUri: String = "") : Parcelable {
         }
     }
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(deepLinkUri)
+        parcel.writeMap(searchParameterHashMap as Map<*, *>?)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<SearchParameter> {
+        override fun createFromParcel(parcel: Parcel): SearchParameter {
+            return SearchParameter(parcel)
+        }
+
+        override fun newArray(size: Int): Array<SearchParameter?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
