38a97332d744285e57b8ef8d9a0a5c0c28f2f8f7
M normansyah <noiz354@gmail.com>
2020-07-24 09:20:57 +0700

    1. no real mention R and BuildConfig.

--- customerapp/src/main/java/com/tokopedia/tkpd/FallbackActivity.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/FallbackActivity.java
@@ -5,7 +5,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.tokopedia.intl.R;
 
 /**
  * Created by ricoharisin on 11/22/16.
