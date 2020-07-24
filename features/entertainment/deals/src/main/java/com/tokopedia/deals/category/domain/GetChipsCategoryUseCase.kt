1d9c069168b9d1c3f3029b2ff2e1924c2f3c6a1a
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-07-24 08:33:11 +0700

    fixing cache first

--- features/entertainment/deals/src/main/java/com/tokopedia/deals/category/domain/GetChipsCategoryUseCase.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/category/domain/GetChipsCategoryUseCase.kt
@@ -2,6 +2,7 @@ package com.tokopedia.deals.category.domain
 
 import com.tokopedia.deals.common.domain.DealsGqlQueries
 import com.tokopedia.deals.search.model.response.CuratedData
+import com.tokopedia.graphql.GraphqlConstant
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
@@ -17,7 +18,8 @@ class GetChipsCategoryUseCase @Inject constructor(
     override suspend fun executeOnBackground(): CuratedData {
         val gqlRequest = GraphqlRequest(DealsGqlQueries.getChildCategory(), CuratedData::class.java)
         val gqlResponse = graphqlRepository.getReseponse(listOf(gqlRequest), GraphqlCacheStrategy
-                .Builder(CacheType.CACHE_FIRST).build())
+                .Builder(CacheType.CACHE_FIRST)
+                .setExpiryTime(GraphqlConstant.ExpiryTimes.MINUTE_30.`val`()).build())
         val errors = gqlResponse.getError(CuratedData::class.java)
         if (!errors.isNullOrEmpty()){
             throw  MessageErrorException(errors[0].message)
