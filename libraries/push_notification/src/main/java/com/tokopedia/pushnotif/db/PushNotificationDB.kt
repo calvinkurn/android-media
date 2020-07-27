f2dc5069d8544f99a8252c280a33b76ee6191b30
isfaaghyth <isfaaghyth@gmail.com>
2020-07-27 10:59:13 +0700

    feat: added database migration on notifier pipeline

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/db/PushNotificationDB.kt
@@ -9,18 +9,22 @@ import android.content.Context
 import com.tokopedia.pushnotif.db.dao.HistoryNotificationDao
 import com.tokopedia.pushnotif.db.model.HistoryNotificationDB
 
-
 /**
  * @author okasurya on 5/22/19.
  */
-@Database(entities = [HistoryNotificationDB::class], version = 2)
+@Database(entities = [HistoryNotificationDB::class], version = 3)
 abstract class PushNotificationDB : RoomDatabase() {
     abstract fun historyNotificationDao(): HistoryNotificationDao
 
     companion object {
         // For Singleton instantiation
-        @Volatile
-        private var instance: PushNotificationDB? = null
+        @Volatile private var instance: PushNotificationDB? = null
+
+        private val migration_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE $HISTORY_NOTIFICATION_TABLE ADD COLUMN `trans_id` TEXT")
+            }
+        }
 
         fun getInstance(context: Context): PushNotificationDB {
             return instance ?: synchronized(this) {
@@ -33,6 +37,7 @@ abstract class PushNotificationDB : RoomDatabase() {
                     context,
                     PushNotificationDB::class.java,
                     PUSHNOTIF_DB)
+                    .addMigrations(migration_2_3)
                     .build()
         }
     }
