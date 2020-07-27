38cd9f9777d5c756819eccb7a8175b6b453640aa
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-07-27 11:40:42 +0700

    fix : seller checker

--- features/user/user_identification/src/main/java/com/tokopedia/useridentification/view/fragment/UserIdentificationInfoFragment.java
+++ features/user/user_identification/src/main/java/com/tokopedia/useridentification/view/fragment/UserIdentificationInfoFragment.java
@@ -175,8 +175,8 @@ public class UserIdentificationInfoFragment extends BaseDaggerFragment
                 toggleNotFoundView(true);
                 break;
             default:
-                onErrorGetUserProjectInfo(new MessageErrorException(String.format("%s (%s)", getString(R.string
-                        .default_request_error_unknown), KYCConstant.ERROR_STATUS_UNKNOWN)));
+                onErrorGetUserProjectInfo(new MessageErrorException(String.format("%s (%s)", getString(
+                        com.tokopedia.abstraction.R.string.default_request_error_unknown), KYCConstant.ERROR_STATUS_UNKNOWN)));
                 break;
         }
     }
