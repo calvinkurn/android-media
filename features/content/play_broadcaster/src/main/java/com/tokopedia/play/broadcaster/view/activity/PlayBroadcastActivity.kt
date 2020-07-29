76020d27831a9fdf1bc8b1b36a187b3620743cd2
mzennis <meyta.taliti@tokopedia.com>
2020-07-29 14:26:44 +0700

    replace channel if from get config

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/activity/PlayBroadcastActivity.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/activity/PlayBroadcastActivity.kt
@@ -338,8 +338,8 @@ class PlayBroadcastActivity : BaseActivity(), PlayBroadcastCoordinator, PlayBroa
         if (config.streamAllowed) {
             this.channelType = config.channelType
             if (channelType == ChannelType.Active) {
-                analytic.viewDialogViolation(viewModel.channelId, viewModel.title)
                 showDialogWhenActiveOnOtherDevices()
+                analytic.viewDialogViolation(config.channelId, viewModel.title)
             } else {
                 if (permissionHelper.isAllPermissionsGranted(permissions)) configureChannelType(channelType)
                 else requestPermission()
