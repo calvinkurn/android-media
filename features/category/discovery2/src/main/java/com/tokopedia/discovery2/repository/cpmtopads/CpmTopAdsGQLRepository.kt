126acaaf7e38c03b94ed0058048259a993b1e528
Zishanr <zishanr027@gmail.com>
2020-07-30 19:35:00 +0530

    Reverting Native routing

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/repository/cpmtopads/CpmTopAdsGQLRepository.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/repository/cpmtopads/CpmTopAdsGQLRepository.kt
@@ -1,16 +1,15 @@
 package com.tokopedia.discovery2.repository.cpmtopads
 
 import com.tokopedia.basemvvm.repository.BaseRepository
-import com.tokopedia.discovery2.R
 import com.tokopedia.discovery2.data.cpmtopads.CpmTopAdsResponse
+import com.tokopedia.discovery2.data.gqlraw.GQL_CPM
 import com.tokopedia.topads.sdk.domain.model.CpmModel
-import javax.inject.Inject
 
 
-open class CpmTopAdsGQLRepository @Inject constructor(val getGQLString: (Int) -> String) : BaseRepository(), CpmTopAdsRepository {
+open class CpmTopAdsGQLRepository : BaseRepository(), CpmTopAdsRepository {
 
     override suspend fun getCpmTopAdsData(paramsMobile: String): CpmModel? {
-        return getGQLData(getGQLString(R.raw.query_cpm_topads_gql),
+        return getGQLData(GQL_CPM,
                 CpmTopAdsResponse::class.java, mapOf("params" to paramsMobile)).cpmModelData
     }
 }
