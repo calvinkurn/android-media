9a0da4179882ac62f7ab70a1ede50ad51ef6f6ad
kenny <kenny.hadisaputra@tokopedia.com>
2020-07-31 19:51:45 +0700

    add more handy function in view component base

--- features/content/play_common/src/main/java/com/tokopedia/play_common/viewcomponent/ViewComponent.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/viewcomponent/ViewComponent.kt
@@ -1,8 +1,10 @@
 package com.tokopedia.play_common.viewcomponent
 
+import android.content.res.Resources
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.IdRes
+import androidx.annotation.StringRes
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 
@@ -17,6 +19,17 @@ abstract class ViewComponent(
     override val rootView: View = container.findViewById(rootId)
 
     protected fun <V: View> findViewById(@IdRes id: Int): V = rootView.findViewById(id)
+
+    protected fun getString(@StringRes stringRes: Int): String {
+        return rootView.context.getString(stringRes)
+    }
+
+    protected fun getString(@StringRes stringRes: Int, vararg value: Any): String {
+        return rootView.context.getString(stringRes, *value)
+    }
+
+    protected val resources: Resources
+        get() = rootView.resources
 
     open fun show() {
         rootView.show()
