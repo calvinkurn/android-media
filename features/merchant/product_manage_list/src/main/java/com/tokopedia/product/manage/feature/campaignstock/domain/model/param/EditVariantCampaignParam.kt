4cc15fc6812b3608a6691bd8f256284d549773ac
Reivin <reivin.oktavianus@tokopedia.com>
2020-08-03 15:34:16 +0700

    fix (campaignstock): Add SerializedName annotation

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/domain/model/param/EditVariantCampaignParam.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/domain/model/param/EditVariantCampaignParam.kt
@@ -1,9 +1,13 @@
 package com.tokopedia.product.manage.feature.campaignstock.domain.model.param
 
+import com.google.gson.annotations.SerializedName
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 
 data class EditVariantCampaignProductParam(
+        @SerializedName("productId")
         val productId: String,
+        @SerializedName("status")
         val status: ProductStatus,
+        @SerializedName("stock")
         val stock: Int
 )
