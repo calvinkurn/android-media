138218a27b053fe4c477467eaab4d0a9f17e5b3a
isfaaghyth <isfaaghyth@gmail.com>
2020-08-03 17:55:12 +0700

    feat: (cm) created tracker of CM product notification

---
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/analytics/ProductAnalytics.kt
@@ -0,0 +1,180 @@
+package com.tokopedia.notifications.analytics
+
+import com.tokopedia.notifications.model.BaseNotificationModel
+import com.tokopedia.notifications.model.ProductInfo
+import com.tokopedia.track.TrackApp
+
+object ProductAnalytics {
+
+    private const val KEY_EVENT = "event"
+    private const val KEY_EVENT_CATEGORY = "eventCategory"
+    private const val KEY_EVENT_ACTION = "eventAction"
+    private const val KEY_EVENT_LABEL = "eventLabel"
+    private const val KEY_USER_ID = "userId"
+    private const val KEY_SHOP_ID = "shopId"
+    private const val KEY_ECOMMERCE = "ecommerce"
+    private const val KEY_CURRENCY_CODE = "currencyCode"
+    private const val KEY_IMPRESSIONS = "impressions"
+    private const val KEY_CLICK = "click"
+
+    private const val KEY_ID = "id"
+    private const val KEY_NAME = "name"
+    private const val KEY_PRICE = "price"
+    private const val KEY_BRAND = "brand"
+    private const val KEY_VARIANT = "variant"
+    private const val KEY_LIST = "list"
+    private const val KEY_POSITION = "position"
+    private const val KEY_CATEGORY = "category"
+    private const val KEY_DIMENSION_79 = "dimension79"
+
+    private const val EVENT_VIEW = "viewPushNotifIris"
+    private const val EVENT_CLICK = "clickPushNotif"
+    private const val EVENT_PRODUCT_VIEW = "productView"
+    private const val EVENT_CHECKOUT = "checkout"
+    private const val CATEGORY = "push notif"
+    private const val ACTION_VIEW = "view on push notif"
+    private const val ACTION_VIEW_EXPAND = "view on expanded push"
+    private const val ACTION_CLICK = "click on push body"
+    private const val ACTION_CLICK_EXPAND = "click on expanded push body"
+    private const val ACTION_CLICK_EXPAND_OCC = "click on expanded push occ"
+    private const val ACTION_CLICK_EXPAND_ATC = "click on expanded push atc"
+    private const val IDR = "IDR"
+    private const val LIST = "/notifcenter"
+    private const val POSITION = "1"
+
+    fun impression(
+            element: BaseNotificationModel
+    ) {
+        sendTracker(mapOf(
+                KEY_EVENT to EVENT_VIEW,
+                KEY_EVENT_CATEGORY to CATEGORY,
+                KEY_EVENT_ACTION to ACTION_VIEW,
+                KEY_EVENT_LABEL to element.transactionId.toString(),
+                KEY_USER_ID to element.userId.toString(),
+                KEY_SHOP_ID to element.shopId.toString()
+        ))
+    }
+
+    fun clickBody(
+            element: BaseNotificationModel
+    ) {
+        sendTracker(mapOf(
+                KEY_EVENT to EVENT_CLICK,
+                KEY_EVENT_CATEGORY to CATEGORY,
+                KEY_EVENT_ACTION to ACTION_CLICK_EXPAND,
+                KEY_EVENT_LABEL to element.transactionId.toString(),
+                KEY_USER_ID to element.userId.toString(),
+                KEY_SHOP_ID to element.shopId.toString()
+        ))
+    }
+
+    fun impressionExpanded(
+            element: BaseNotificationModel,
+            product: ProductInfo
+    ) {
+        val impressions = mapOf(
+                KEY_ID to product.element_id.toString(),
+                KEY_NAME to product.productTitle,
+                KEY_PRICE to product.productCurrentPrice,
+                KEY_BRAND to "",
+                KEY_VARIANT to "",
+                KEY_LIST to LIST,
+                KEY_POSITION to POSITION,
+                KEY_CATEGORY to "",
+                KEY_DIMENSION_79 to element.shopId.toString()
+        )
+
+        val ecommerce = mapOf(
+                KEY_CURRENCY_CODE to IDR,
+                KEY_IMPRESSIONS to impressions
+        )
+
+        sendTracker(mapOf(
+                KEY_EVENT to EVENT_PRODUCT_VIEW,
+                KEY_EVENT_CATEGORY to CATEGORY,
+                KEY_EVENT_ACTION to ACTION_VIEW_EXPAND,
+                KEY_EVENT_LABEL to element.transactionId.toString(),
+                KEY_USER_ID to element.userId.toString(),
+                KEY_ECOMMERCE to ecommerce
+        ))
+    }
+
+    fun clickProductCard(
+            element: BaseNotificationModel,
+            product: ProductInfo
+    ) {
+        val productElement = mapOf(
+                KEY_ID to product.element_id.toString(),
+                KEY_NAME to product.productTitle,
+                KEY_PRICE to product.productCurrentPrice,
+                KEY_BRAND to "",
+                KEY_VARIANT to "",
+                KEY_LIST to LIST,
+                KEY_POSITION to POSITION,
+                KEY_CATEGORY to "",
+                KEY_DIMENSION_79 to element.shopId.toString()
+        )
+
+        val ecommerce = mapOf(
+                KEY_CLICK to mapOf(
+                        "actionField" to mapOf(
+                                KEY_LIST to LIST,
+                                "products" to arrayListOf(productElement)
+                        )
+                )
+        )
+
+        sendTracker(mapOf(
+                KEY_EVENT to "productClick",
+                KEY_EVENT_CATEGORY to CATEGORY,
+                KEY_EVENT_ACTION to "click on expanded push product card",
+                KEY_EVENT_LABEL to element.transactionId.toString(),
+                KEY_USER_ID to element.userId.toString(),
+                KEY_ECOMMERCE to ecommerce
+        ))
+    }
+
+    fun occCLickButton(
+            element: BaseNotificationModel,
+            product: ProductInfo
+    ) {
+        val productElement = mapOf(
+                KEY_ID to product.element_id.toString(),
+                KEY_NAME to product.productTitle,
+                KEY_PRICE to product.productCurrentPrice,
+                KEY_BRAND to "",
+                KEY_VARIANT to "",
+                KEY_LIST to LIST,
+                KEY_CATEGORY to "",
+                "quantity" to "1",
+                "dimension79" to element.shopId.toString(),
+                "dimension81" to "",
+                "dimension80" to "",
+                "dimension82" to "",
+                "dimension45" to "",
+                "dimension40" to "/pushnotif"
+        )
+
+        val ecommerce = mapOf(
+                EVENT_CHECKOUT to mapOf(
+                        "actionField" to mapOf(
+                                "products" to arrayListOf(productElement)
+                        )
+                )
+        )
+
+        sendTracker(mapOf(
+                KEY_EVENT to EVENT_CHECKOUT,
+                KEY_EVENT_CATEGORY to CATEGORY,
+                KEY_EVENT_ACTION to ACTION_CLICK_EXPAND_OCC, //TODO by type
+                KEY_EVENT_LABEL to element.transactionId.toString(),
+                KEY_USER_ID to element.userId.toString(),
+                KEY_ECOMMERCE to ecommerce
+        ))
+    }
+
+    private fun sendTracker(dataMap: Map<String, Any>) {
+        TrackApp.getInstance().gtm.sendEnhanceEcommerceEvent(dataMap)
+    }
+
+}
