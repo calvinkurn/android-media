8c6e6021e42b8ae0dfd951257108d3a72577bfbc
Jessica Sean <jessica.sean@tokopedia.com>
2020-08-04 16:53:04 +0700

    initial instrumental test hotel

---
+++ features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/homepage/presentation/activity/HotelHomepageActivityTest.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.hotel.homepage.presentation.activity
+
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * @author by jessica on 04/08/20
+ */
+class HotelHomepageActivityTest {
+    @get:Rule
+    var activityRule: ActivityTestRule<HotelHomepageActivity> = ActivityTestRule(HotelHomepageActivity::class.java)
+
+    @Test
+    fun testHomeLayout() {
+        Thread.sleep(1000)
+    }
+}
