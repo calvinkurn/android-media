4f2d34d427b2126dd5a6941fffe7f01654e13d22
leonard2907 <leonard.gunawan@tokopedia.com>
2020-08-05 21:37:13 +0700

    Add interceptors

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reputationhistory/di/SellerReputationComponent.java
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reputationhistory/di/SellerReputationComponent.java
@@ -1,6 +1,5 @@
 package com.tokopedia.reviewseller.feature.reputationhistory.di;
 
-import com.tokopedia.abstraction.common.di.module.AppModule;
 import com.tokopedia.reviewseller.common.di.component.ReviewSellerComponent;
 import com.tokopedia.reviewseller.feature.reputationhistory.view.fragment.SellerReputationFragment;
 
@@ -10,7 +9,7 @@ import dagger.Component;
  * Created by normansyahputa on 2/13/18.
  */
 @SellerReputationScope
-@Component(modules = {SellerReputationModule.class, AppModule.class}, dependencies = ReviewSellerComponent.class)
+@Component(modules = {SellerReputationModule.class}, dependencies = {ReviewSellerComponent.class})
 public interface SellerReputationComponent {
 
     void inject(SellerReputationFragment sellerReputationFragment);
--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reputationhistory/di/SellerReputationModule.java
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reputationhistory/di/SellerReputationModule.java
@@ -1,9 +1,19 @@
 package com.tokopedia.reviewseller.feature.reputationhistory.di;
 
+import android.content.Context;
+import com.chuckerteam.chucker.api.ChuckerCollector;
+import com.chuckerteam.chucker.api.ChuckerInterceptor;
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
+import com.tokopedia.config.GlobalConfig;
 import com.tokopedia.core.network.constants.TkpdBaseURL;
-import com.tokopedia.core.network.di.qualifier.DefaultAuthWithErrorHandler;
 import com.tokopedia.core.network.di.qualifier.InboxQualifier;
 import com.tokopedia.core.network.di.qualifier.WsV4QualifierWithErrorHander;
+import com.tokopedia.core.network.retrofit.interceptors.TkpdErrorResponseInterceptor;
+import com.tokopedia.core.network.retrofit.response.TkpdV4ResponseError;
+import com.tokopedia.network.NetworkRouter;
+import com.tokopedia.network.interceptor.DebugInterceptor;
+import com.tokopedia.network.interceptor.FingerprintInterceptor;
+import com.tokopedia.network.interceptor.TkpdAuthInterceptor;
 import com.tokopedia.reviewseller.feature.reputationhistory.data.repository.ReputationRepositoryImpl;
 import com.tokopedia.reviewseller.feature.reputationhistory.data.repository.ReputationReviewRepositoryImpl;
 import com.tokopedia.reviewseller.feature.reputationhistory.data.source.cloud.CloudReputationReviewDataSource;
@@ -12,6 +22,7 @@ import com.tokopedia.reviewseller.feature.reputationhistory.domain.ReputationRep
 import com.tokopedia.reviewseller.feature.reputationhistory.domain.ReputationReviewRepository;
 import com.tokopedia.product.manage.item.common.data.source.cloud.ShopApi;
 import com.tokopedia.product.manage.item.common.domain.repository.ShopInfoRepositoryImpl;
+import com.tokopedia.user.session.UserSession;
 
 import dagger.Module;
 import dagger.Provides;
@@ -25,6 +36,69 @@ import retrofit2.Retrofit;
 @Module
 @SellerReputationScope
 public class SellerReputationModule {
+
+    @SellerReputationScope
+    @Provides
+    public FingerprintInterceptor provideFingerprintInterceptor(NetworkRouter networkRouter,
+                                                                UserSession userSession){
+        return new FingerprintInterceptor(networkRouter, userSession);
+    }
+
+    @SellerReputationScope
+    @Provides
+    public TkpdAuthInterceptor provideTkpdAuthInterceptor(@ApplicationContext Context context,
+                                                          NetworkRouter networkRouter,
+                                                          UserSession userSession){
+            return new TkpdAuthInterceptor(context, networkRouter, userSession);
+    }
+
+    @SellerReputationScope
+    @Provides
+    public NetworkRouter provideNetworkRouter(@ApplicationContext Context context) {
+        return (NetworkRouter) context;
+    }
+
+    @SellerReputationScope
+    @Provides
+    public ChuckerInterceptor provideChuckerInterceptor(@ApplicationContext Context context){
+        ChuckerCollector collector = new ChuckerCollector(context, GlobalConfig.isAllowDebuggingTools());
+        return new ChuckerInterceptor(context, collector);
+    }
+
+
+    @SellerReputationScope
+    @Provides
+    public DebugInterceptor provideDebugInterceptor() {
+        return new DebugInterceptor();
+    }
+
+    @SellerReputationScope
+    @Provides
+    TkpdErrorResponseInterceptor provideTkpdErrorResponseInterceptor() {
+        return new TkpdErrorResponseInterceptor(TkpdV4ResponseError.class);
+    }
+
+    @SellerReputationScope
+    @Provides
+    public OkHttpClient provideOkHttpClient(FingerprintInterceptor fingerprintInterceptor,
+                                            TkpdAuthInterceptor tkpdAuthInterceptor,
+                                            ChuckerInterceptor chuckInterceptor,
+                                            DebugInterceptor debugInterceptor,
+                                            TkpdErrorResponseInterceptor errorHandlerInterceptor) {
+        return new OkHttpClient.Builder()
+                .addInterceptor(fingerprintInterceptor)
+                .addInterceptor(tkpdAuthInterceptor)
+                .addInterceptor(chuckInterceptor)
+                .addInterceptor(debugInterceptor)
+                .addInterceptor(errorHandlerInterceptor)
+                .build();
+    }
+
+    @SellerReputationScope
+    @Provides
+    public UserSession provideUserSession(@ApplicationContext Context context) {
+        return new UserSession(context);
+    }
 
     @InboxQualifier
     @SellerReputationScope
