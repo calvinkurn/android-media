c792e3eee4053f9b461e59edfe763793aacbc3e6
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-08-07 10:39:56 +0700

    fix message error & maks character title template

--- features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewreply/view/fragment/SellerReviewReplyFragment.kt
+++ features/merchant/review_seller/src/main/java/com/tokopedia/reviewseller/feature/reviewreply/view/fragment/SellerReviewReplyFragment.kt
@@ -197,7 +197,7 @@ class SellerReviewReplyFragment : BaseDaggerFragment(), ReviewTemplateListViewHo
                 }
                 is Fail -> {
                     view?.let { it1 ->
-                        Toaster.make(it1, getString(R.string.error_message_maximum_template_reply), type = Toaster.TYPE_ERROR, duration = Toaster.LENGTH_LONG)
+                        Toaster.make(it1, it.throwable.message.toString(), type = Toaster.TYPE_ERROR, duration = Toaster.LENGTH_LONG)
                     }
                 }
             }
