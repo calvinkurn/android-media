84af4ca17ad470db0cc9e8b0b5d7fab79d61f183
Reivin <reivin.oktavianus@tokopedia.com>
2020-08-10 19:14:37 +0700

    feat (migration): Add partial account header for seller migration

---
+++ features/merchant/seller_migration_common/src/main/java/com/tokopedia/seller_migration_common/presentation/widget/SellerMigrationAccountCommBottomSheet.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.seller_migration_common.presentation.widget
+
+import android.content.Context
+
+class SellerMigrationAccountCommBottomSheet: SellerMigrationBottomSheet() {
+
+    override fun inflateChildView(context: Context) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trackGoToSellerApp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun trackGoToPlayStore() {
+        TODO("Not yet implemented")
+    }
+
+    override fun trackLearnMore() {
+        TODO("Not yet implemented")
+    }
+
+}
