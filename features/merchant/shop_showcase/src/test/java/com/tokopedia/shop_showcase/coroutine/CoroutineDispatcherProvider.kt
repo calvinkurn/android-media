8c14aa9ab93d56eeece1eb963b0c3273586943ed
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-08-11 16:03:18 +0700

    delete CoroutineDispatcherProvider.kt

--- features/merchant/shop_showcase/src/test/java/com/tokopedia/shop_showcase/coroutine/CoroutineDispatcherProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.shop_showcase.coroutine
-
-import kotlinx.coroutines.CoroutineDispatcher
-
-interface CoroutineDispatcherProvider {
-    fun main(): CoroutineDispatcher
-    fun io(): CoroutineDispatcher
-    fun default(): CoroutineDispatcher
-}
