4bcf747807edb82d78b032d67e3d8d6a4a8265a5
hansenpw <hansen.wijaya@tokopedia.com>
2020-08-11 15:49:44 +0700

    change cc list param

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/preference/edit/view/payment/creditcard/CreditCardPickerFragment.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/preference/edit/view/payment/creditcard/CreditCardPickerFragment.kt
@@ -18,13 +18,11 @@ import android.webkit.WebViewClient
 import android.widget.ProgressBar
 import com.google.gson.Gson
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.config.GlobalConfig
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.oneclickcheckout.R
 import com.tokopedia.oneclickcheckout.order.view.model.OrderPaymentCreditCardAdditionalData
 import com.tokopedia.oneclickcheckout.preference.edit.di.PreferenceEditComponent
-import com.tokopedia.url.TokopediaUrl
 import com.tokopedia.user.session.UserSessionInterface
 import java.net.URLEncoder
 import javax.inject.Inject
@@ -104,8 +102,8 @@ class CreditCardPickerFragment : BaseDaggerFragment() {
                 "customer_name=${getUrlEncoded(additionalData.name.trim())}&" +
                 "customer_email=${getUrlEncoded(additionalData.email)}&" +
                 "customer_msisdn=${getUrlEncoded(additionalData.msisdn)}&" +
-                "version=${getUrlEncoded(GlobalConfig.VERSION_NAME)}&" +
-                "callback_url=${getUrlEncoded("${TokopediaUrl.getInstance().PAY}/v3/cardlist")}"
+                "signature=${getUrlEncoded(additionalData.signature)}&" +
+                "callback_url=${""}"
     }
 
     private fun getUrlEncoded(valueStr: String): String {
