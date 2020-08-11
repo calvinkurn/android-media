b55ce7224bda1b51e5b169699fbbf3beecca791b
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2020-08-11 11:29:32 +0700

    add db count module only for timber

--- libraries/iris/src/main/java/com/tokopedia/iris/data/TrackingRepository.kt
+++ libraries/iris/src/main/java/com/tokopedia/iris/data/TrackingRepository.kt
@@ -67,13 +67,15 @@ class TrackingRepository(
                     trackingDao.flush()
                 } else if (dbCount >= getLineDBSend()) {
                     // if the line is big, send it
-                    if (dbCount % 50 == 0) {
+                    if (dbCount % 5 == 0) {
                         val i = Intent(context, IrisService::class.java)
                         i.putExtra(MAX_ROW, DEFAULT_MAX_ROW)
                         IrisService.enqueueWork(context, i)
 
                         IrisAnalytics.getInstance(context).setAlarm(true, force = true)
-                        Timber.w("P1#IRIS#dbCountSend %d lines", dbCount)
+                        if (dbCount % 50 == 0) {
+                            Timber.w("P1#IRIS#dbCountSend %d lines", dbCount)
+                        }
                     }
                 }
             } catch (e: Throwable) {
