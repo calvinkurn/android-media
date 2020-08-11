a82102f8c36d8cdc974e46e830c022e57692ce46
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-08-11 16:05:27 +0700

    add shopshowcaseproductadd test

---
+++ features/merchant/shop_showcase/src/test/java/com/tokopedia/shop_showcase/viewmodel/shopshowcaseproductadd/ShowcaseProductAddViewModelTestFixture.kt
@@ -0,0 +1,40 @@
+package com.tokopedia.shop_showcase.viewmodel.shopshowcaseproductadd
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.shop_showcase.shop_showcase_product_add.domain.usecase.GetProductListUseCase
+import com.tokopedia.shop_showcase.shop_showcase_product_add.presentation.viewmodel.ShowcaseProductAddViewModel
+import com.tokopedia.user.session.UserSessionInterface
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Before
+import org.junit.Rule
+
+abstract class ShowcaseProductAddViewModelTestFixture {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
+    lateinit var getProductListUseCase: GetProductListUseCase
+
+    @RelaxedMockK
+    lateinit var userSession: UserSessionInterface
+
+
+    protected lateinit var showcaseProductAddViewModel: ShowcaseProductAddViewModel
+
+    protected val testDispatcher by lazy {
+        TestCoroutineDispatcher()
+    }
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        showcaseProductAddViewModel = ShowcaseProductAddViewModel(
+                userSession,
+                getProductListUseCase,
+                testDispatcher
+        )
+    }
+}
