0165e83c3a2c98e6d724bc374052d70e9b5cda31
Calvyn <calvyn.julian@tokopedia.com>
2020-08-11 10:24:54 +0700

    Add stockSoldPercentage to shop home layout query

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/module/ShopPageHomeModule.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/module/ShopPageHomeModule.kt
@@ -152,6 +152,7 @@ class ShopPageHomeModule {
                                         title
                                     }
                                     hideGimmick
+                                    stockSoldPercentage
                                }
                     }
                   }
