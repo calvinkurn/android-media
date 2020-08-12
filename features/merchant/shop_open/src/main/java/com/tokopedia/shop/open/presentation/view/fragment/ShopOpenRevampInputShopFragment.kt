e3d8d68c1d3b9aeb4929d5bf0ef9870fb29a9fdb
yogieputra8 <yogie.putra8@gmail.com>
2020-08-13 00:51:00 +0700

    Fix screenName for open shop feature

--- features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/presentation/view/fragment/ShopOpenRevampInputShopFragment.kt
+++ features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/presentation/view/fragment/ShopOpenRevampInputShopFragment.kt
@@ -30,6 +30,7 @@ import com.tokopedia.network.utils.ErrorHandler
 import com.tokopedia.shop.open.R
 import com.tokopedia.shop.open.analytic.ShopOpenRevampTracking
 import com.tokopedia.shop.open.common.PageNameConstant
+import com.tokopedia.shop.open.common.ScreenNameTracker
 import com.tokopedia.shop.open.common.TermsAndConditionsLink.URL_PRIVACY_POLICY
 import com.tokopedia.shop.open.common.TermsAndConditionsLink.URL_TNC
 import com.tokopedia.shop.open.data.model.ValidateShopDomainSuggestionResult
@@ -150,6 +151,7 @@ class ShopOpenRevampInputShopFragment : BaseDaggerFragment(),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        shopOpenRevampTracking?.sendScreenNameTracker(ScreenNameTracker.SCREEN_SHOP_REGISTRATION)
         txtInputShopName.setMessage(getString(R.string.open_shop_revamp_default_hint_input_shop))
         btnShopRegistration.setOnClickListener {
             activity?.let {
