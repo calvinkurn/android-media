a0939f3f7eb43e8c62041a214003a5ada48e0fa3
mzennis <meyta.taliti@tokopedia.com>
2020-08-13 13:16:47 +0700

    re-new channel id

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
@@ -572,29 +572,29 @@ class PlayFragment : BaseDaggerFragment(), PlayOrientationListener, PlayFragment
     }
 
     private fun stopPrepareMonitoring() {
-        pageMonitoring?.stopPreparePagePerformanceMonitoring()
+        pageMonitoring.stopPreparePagePerformanceMonitoring()
     }
 
     private fun startNetworkMonitoring() {
-        pageMonitoring?.startNetworkRequestPerformanceMonitoring()
+        pageMonitoring.startNetworkRequestPerformanceMonitoring()
     }
 
     private fun stopNetworkMonitoring() {
-        pageMonitoring?.stopNetworkRequestPerformanceMonitoring()
+        pageMonitoring.stopNetworkRequestPerformanceMonitoring()
     }
 
     fun startRenderMonitoring() {
         stopNetworkMonitoring()
-        pageMonitoring?.startRenderPerformanceMonitoring()
+        pageMonitoring.startRenderPerformanceMonitoring()
     }
 
     fun stopRenderMonitoring() {
-        pageMonitoring?.stopRenderPerformanceMonitoring()
+        pageMonitoring.stopRenderPerformanceMonitoring()
         stopPageMonitoring()
     }
 
     private fun stopPageMonitoring() {
-        pageMonitoring?.stopMonitoring()
+        pageMonitoring.stopMonitoring()
     }
 
     private fun hideKeyboard() {
