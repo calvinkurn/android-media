9196ce391573867b90a76aff4c2ceacb677e0fd4
mzennis <meyta.taliti@tokopedia.com>
2020-08-13 14:06:29 +0700

    remove unused import

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayErrorFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayErrorFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
