94b889c94110f653944607e21e2e76accb0c2978
yoasfs <yoas.fs@gmail.com>
2020-08-13 13:33:03 +0700

    add query

---
+++ features/discovery/recentview/src/main/java/com/tokopedia/recentview/data/query/RecentViewQuery.kt
@@ -0,0 +1,44 @@
+package com.tokopedia.recentview.data.query
+
+/**
+ * @author by yoasfs on 12/08/20
+ */
+object RecentViewQuery{
+    private const val userID = "\$userID"
+    private const val count = "\$count"
+    fun getQuery() = """
+        {
+         query getRecentView($userID: String!, $count:Int) {
+          
+                get_recent_view(userID: $userID, count:$count,filter:{
+                     blacklistProductIds:"1,2,3",
+                     source:"any",
+                   }) {
+                     items {
+                       product_id
+                       product_name
+                       product_url
+                       product_image
+                       product_price
+                       product_rating
+                       product_review_count
+                       shop_id
+                       shop_url
+                       shop_location
+                       shop_name
+                       shop_gold_status
+                       wishlist
+                       badges{
+                         title
+                         image_url
+                       }
+                       labels {
+                         title
+                         color
+                       }
+                     }
+                   }
+            }
+        }
+    """.trimIndent()
+}
