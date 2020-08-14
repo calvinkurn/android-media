c7c2e891d7a87e92cd5f2d0843b88f0c6d27a8be
Rizaldi Martaputra <rizaldi.martaputra@tokopedia.com>
2020-08-14 10:08:11 +0700

    Convert classes in com.tokopedia.favorite.view.viewmodel package to kotlin

---
+++ features/merchant/favorite/src/main/java/com/tokopedia/favorite/view/viewmodel/DataFavoriteMapper.kt
@@ -0,0 +1,58 @@
+package com.tokopedia.favorite.view.viewmodel
+
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.favorite.domain.model.FavoriteShop
+import com.tokopedia.favorite.domain.model.FavoriteShopItem
+import com.tokopedia.favorite.domain.model.TopAdsShop
+import java.util.*
+import javax.inject.Inject
+
+/**
+ * @author madi on 4/6/17.
+ */
+class DataFavoriteMapper @Inject constructor() {
+
+    fun prepareDataFavoriteShop(favoriteShop: FavoriteShopItem): FavoriteShopViewModel {
+        val favoriteShopViewModel = FavoriteShopViewModel()
+        favoriteShopViewModel.shopId = favoriteShop.id
+        favoriteShopViewModel.shopName = favoriteShop.name
+        favoriteShopViewModel.shopAvatarImageUrl = favoriteShop.iconUri
+        favoriteShopViewModel.shopLocation = favoriteShop.location
+        favoriteShopViewModel.isFavoriteShop = favoriteShop.isFav
+        favoriteShopViewModel.badgeUrl = favoriteShop.badgeUrl
+        return favoriteShopViewModel
+    }
+
+    fun prepareDataTopAdsShop(adsShop: TopAdsShop): TopAdsShopViewModel {
+        val shopViewModel = TopAdsShopViewModel()
+        val shopItems = ArrayList<TopAdsShopItem>()
+        if (adsShop.topAdsShopItemList != null) {
+            for (item in adsShop.topAdsShopItemList) {
+                val shopItem = TopAdsShopItem()
+                shopItem.shopId = item.shopId
+                shopItem.shopDomain = item.shopDomain
+                shopItem.shopName = item.shopName
+                shopItem.adKey = item.adRefKey
+                shopItem.shopClickUrl = item.shopClickUrl
+                shopItem.shopCoverUrl = item.shopImageCover
+                shopItem.shopCoverEcs = item.shopImageCoverEcs
+                shopItem.shopImageUrl = item.shopImageUrl
+                shopItem.shopImageEcs = item.shopImageEcs
+                shopItem.shopLocation = item.shopLocation
+                shopItem.isFav = item.isSelected
+                shopItems.add(shopItem)
+            }
+        }
+        shopViewModel.adsShopItems = shopItems
+        return shopViewModel
+    }
+
+    fun prepareListFavoriteShop(favoriteShop: FavoriteShop): List<Visitable<*>> {
+        val elementList: MutableList<Visitable<*>> = ArrayList()
+        for (favoriteShopItem in favoriteShop.data) {
+            favoriteShopItem.setIsFav(true)
+            elementList.add(prepareDataFavoriteShop(favoriteShopItem))
+        }
+        return elementList
+    }
+}
---
+++ features/merchant/favorite/src/main/java/com/tokopedia/favorite/view/viewmodel/TopAdsShopItem.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.favorite.view.viewmodel
+
+/**
+ * @author by erry on 30/01/17.
+ */
+class TopAdsShopItem {
+    var shopId: String? = null
+    var shopName: String? = null
+    var shopCoverUrl: String? = null
+    var shopCoverEcs: String? = null
+    var shopImageUrl: String? = null
+    var shopImageEcs: String? = null
+    var shopLocation: String? = null
+    var isFav = false
+    var shopClickUrl: String? = null
+    var adKey: String? = null
+    var shopDomain: String? = null
+}
