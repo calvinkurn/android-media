af7524dfd3a68da4ed27dfa7b51d8b022a8f5c5e
Calvyn <calvyn.julian@tokopedia.com>
2020-08-14 09:41:05 +0700

    remove gql shop favorite from response size checker  on plt ShopOfficialStore

--- features/merchant/shop_page/src/androidTest/java/com/tokopedia/shop/testcase/PltShopPageOfficialStorePerformanceTest.kt
+++ features/merchant/shop_page/src/androidTest/java/com/tokopedia/shop/testcase/PltShopPageOfficialStorePerformanceTest.kt
@@ -55,7 +55,6 @@ class PltShopPageOfficialStorePerformanceTest {
                     KEY_QUERY_SHOP_INFO_HOME_TYPE,
                     KEY_QUERY_SHOP_INFO_GOLD,
                     KEY_QUERY_SHOP_INFO_TOP_CONTENT,
-                    KEY_QUERY_SHOP_INFO_FAVORITE,
                     KEY_QUERY_WHITELIST
             ))
             val intent = Intent()
