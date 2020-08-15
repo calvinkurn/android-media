cdc05c3ed7d90e10ddf94ed2929a8100d25e606f
yogieputra8 <yogie.putra8@gmail.com>
2020-08-15 22:47:11 +0700

    Fix model checker shop is PM

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/basicinfo/data/CheckShopIsPowerMerchantModel.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/basicinfo/data/CheckShopIsPowerMerchantModel.kt
@@ -1,60 +1,36 @@
 package com.tokopedia.shop.settings.basicinfo.data
 
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 
-//data class CheckShopIsPowerMerchantModel(
-//        @SerializedName("power_merchant")
-//        @Expose
-//        val powerMerchant: PowerMerchant = PowerMerchant()
-//)
-//
-////data class CheckShopIsOfficialModel(
-////        @SerializedName("getIsOfficial")
-////        @Expose
-////        val getIsOfficial: GetIsOfficial = GetIsOfficial()
-////)
-////
-////data class GetIsOfficial(
-////        @SerializedName("data")
-////        @Expose
-////        val `data`: Data = Data(),
-////        @SerializedName("message_error")
-////        @Expose
-////        val messageError: String = ""
-////)
-
 data class CheckShopIsPowerMerchantModel(
-        @SerializedName("header")
-        @Expose
-        val header: Header = Header(),
-        @SerializedName("data")
-        @Expose
-        val `data`: DataPowerMerchant = DataPowerMerchant()
+        @SerializedName("goldGetPMOSStatus")
+        val goldGetPMOSStatus: GoldGetPMOSStatus = GoldGetPMOSStatus()
 )
 
-data class Header(
-        @SerializedName("messages")
-        @Expose
-        val messages: String = "",
-        @SerializedName("reason")
-        @Expose
-        val reason: String = ""
+data class GoldGetPMOSStatus(
+        @SerializedName("data")
+        val `data`: DataPowerMerchant = DataPowerMerchant(),
+        @SerializedName("header")
+        val header: Header = Header()
 )
 
 data class DataPowerMerchant(
         @SerializedName("power_merchant")
-        @Expose
         val powerMerchant: PowerMerchant = PowerMerchant()
 )
 
 data class PowerMerchant(
         @SerializedName("expired_time")
-        @Expose
         val expiredTime: String = "",
         @SerializedName("status")
-        @Expose
-        val status: String = "" // There are 3 status: active, "", idle
+        val status: String = ""
+)
+
+data class Header(
+        @SerializedName("messages")
+        val messages: List<Any> = listOf(),
+        @SerializedName("reason")
+        val reason: String = ""
 )
