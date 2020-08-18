8b51a44a1c54bd4fb9098779b652b86ddae0dc70
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-08-18 15:39:00 +0700

    fix app:typographyWeight

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/search/view/adapter/model/ShopSearchProductFixedResultDataModel.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/search/view/adapter/model/ShopSearchProductFixedResultDataModel.kt
@@ -1,12 +1,10 @@
 package com.tokopedia.shop.search.view.adapter.model
 
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import com.tokopedia.shop.search.view.adapter.ShopSearchProductAdapterTypeFactory
 
 class ShopSearchProductFixedResultDataModel(
         var searchQuery: String = "",
-        var searchTypeLabel: Spanned = SpannableStringBuilder(""),
+        var searchTypeLabel: String = "",
         override var type: Type
 ) : ShopSearchProductDataModel() {
 
