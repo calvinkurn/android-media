9d523bbba433ef18ba67b65fe6c39256d8fcb6ee
ashwani tyagi <ashwani.tyagi@tokopedia.com>
2020-08-21 16:10:31 +0530

    change timber scylr logs

--- libraries/linker/src/main/java/com/tokopedia/linker/validation/BranchHelperValidation.kt
+++ libraries/linker/src/main/java/com/tokopedia/linker/validation/BranchHelperValidation.kt
@@ -25,32 +25,32 @@ class BranchHelperValidation() {
 
     private fun validatePaymentId(paymentId: String) {
         if (paymentId.isNullOrBlank()) {
-            logging( "paymentId is blank =$paymentId")
+            logging( "paymentId_blank =$paymentId")
         }
     }
 
     private fun validateOrderId(orderID: String) {
         if (orderID.isNullOrBlank()) {
-            logging( "orderID is blank =$orderID")
+            logging( "orderId_blank =$orderID")
         }
     }
 
     private fun isFromNotNative(isFromNative: Boolean, paymentId: String, orderId:String) {
         if (!isFromNative) {
-            logging( "transaction is not from new thanks page =$paymentId$orderId")
+            logging( "transaction_not__new_thankspage =$paymentId$orderId")
         }
     }
 
     private fun validateRevenue(revenuePrice: Double) {
         if (revenuePrice <= 0) {
-            logging( "revenue <= 0 =$revenuePrice")
+            logging( "revenue_blank =$revenuePrice")
         }
     }
 
 
     private fun validateShipping(shippingPrice: Double) {
         if (shippingPrice <= 0) {
-            logging( "shippingPrice <= 0 =$shippingPrice")
+            logging( "shippingPrice_blank =$shippingPrice")
         }
     }
 
@@ -77,7 +77,7 @@ class BranchHelperValidation() {
 
     fun validateCartQuantity(quantity: String) {
         if (LinkerUtils.convertToDouble(quantity,"cart quantity validation" ) <= 0) {
-            logging("add to cart quantity <= 0 =$quantity")
+            logging("add_to_cart_quantity  =$quantity")
         }
     }
 
