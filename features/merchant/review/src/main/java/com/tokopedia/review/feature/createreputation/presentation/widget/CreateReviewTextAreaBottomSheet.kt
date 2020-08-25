340ae7e85c6a10f99f9f084f8b9c8d0885a3448e
leonard2907 <leonard.gunawan@tokopedia.com>
2020-08-25 11:58:30 +0700

    make bottomsheet header not scrollable in header

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/createreputation/presentation/widget/CreateReviewTextAreaBottomSheet.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/createreputation/presentation/widget/CreateReviewTextAreaBottomSheet.kt
@@ -1,9 +1,13 @@
 package com.tokopedia.review.feature.createreputation.presentation.widget
 
+import android.app.Activity
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.content.ContextCompat
 import com.tokopedia.review.R
@@ -25,10 +29,20 @@ class CreateReviewTextAreaBottomSheet : BottomSheetUnify() {
     private var textAreaListener: TextAreaListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         context?.let {
             val editText = EditText(it)
-            editText.setBackgroundColor(Color.TRANSPARENT)
+            editText.apply {
+                setOnFocusChangeListener { _, hasFocus ->
+                    activity?.run {
+                        if (hasFocus) {
+                            showKeyboard()
+                        } else {
+                            hideKeyboard()
+                        }
+                    }
+                }
+                setBackgroundColor(Color.TRANSPARENT)
+            }
             setChild(editText)
             showCloseIcon = false
             isFullpage = true
@@ -43,10 +57,21 @@ class CreateReviewTextAreaBottomSheet : BottomSheetUnify() {
             setShowListener {
                 Handler().postDelayed( {
                     editText.requestFocus()
+                    this.dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
                 }, 100)
-                this.dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
                 editText.setText(this@CreateReviewTextAreaBottomSheet.text)
             }
         }
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun View.showKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+    }
+
+    private fun Context.hideKeyboard() {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
     }
 }
