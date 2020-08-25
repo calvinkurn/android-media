f3dd6849d05e0c6ba4bca74be78b674e50e2e2c3
leonard2907 <leonard.gunawan@tokopedia.com>
2020-08-25 13:33:36 +0700

    Fix chips padding

--- features/merchant/talk/src/main/java/com/tokopedia/talk/feature/write/presentation/decorator/SpacingItemDecoration.kt
+++ features/merchant/talk/src/main/java/com/tokopedia/talk/feature/write/presentation/decorator/SpacingItemDecoration.kt
@@ -7,8 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 class SpacingItemDecoration(private val dimen: Int) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.right = this.dimen
-        outRect.top = this.dimen
-        outRect.bottom = this.dimen
+        outRect.right = this.dimen / 2
+        outRect.top = this.dimen / 2
+        outRect.bottom = this.dimen / 2
     }
 }
