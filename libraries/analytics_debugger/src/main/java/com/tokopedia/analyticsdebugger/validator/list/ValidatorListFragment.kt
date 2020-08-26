5907042f7ef3e3f9f38e91519890caf860fa21ca
resakemal <resa.saharso@tokopedia.com>
2020-08-26 09:32:51 +0700

    Add search to client analytic validator

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/list/ValidatorListFragment.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/validator/list/ValidatorListFragment.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.analyticsdebugger.validator.list
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +12,17 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.analyticsdebugger.R
 import com.tokopedia.analyticsdebugger.validator.Utils
+import com.tokopedia.kotlin.extensions.view.hideLoading
+import com.tokopedia.unifycomponents.SearchBarUnify
+import kotlinx.coroutines.*
 import timber.log.Timber
 
 class ValidatorListFragment : Fragment() {
 
     private var listener: Listener? = null
+
+    private var searchInputJob: Job? = null
+    private var searchTextChangeDelay: Long = DEFAULT_TEXT_CHANGE_DELAY_MILLIS
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_validator_list, container, false)
@@ -34,6 +42,37 @@ class ValidatorListFragment : Fragment() {
             adapter = listingAdapter
         }
 
+        val searchBar = view.findViewById<SearchBarUnify>(R.id.search_bar)
+//        searchBar.searchBarTextField.setOnEditorActionListener { v, _, _ ->
+//            searchBar.clearFocus()
+//            val filteredListTests = listTests.filter { it.contains(v.text) }
+//            listingAdapter.setItems(filteredListTests)
+//            true
+//        }
+        searchBar.searchBarTextField.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                searchInputJob?.cancel()
+                searchInputJob = CoroutineScope(Dispatchers.Main).launch {
+                    delay(searchTextChangeDelay)
+                    // Filter test cases based on search query
+                    val filteredListTests = listTests.filter { it.contains(s.toString()) }
+                    listingAdapter.setItems(filteredListTests)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
+        searchBar.clearListener = {
+            listingAdapter.setItems(listTests)
+        }
+
         listingAdapter.setOnItemClickListener { listener?.runTest(it) }
         listingAdapter.setItems(listTests)
     }
@@ -48,5 +87,7 @@ class ValidatorListFragment : Fragment() {
 
     companion object {
         fun newInstance(): ValidatorListFragment = ValidatorListFragment()
+
+        const val DEFAULT_TEXT_CHANGE_DELAY_MILLIS: Long = 500
     }
 }
