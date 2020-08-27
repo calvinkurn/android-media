e04b43c4f7b11b40b6de5183d53591c6d3706de1
hansenpw <hansen.wijaya@tokopedia.com>
2020-08-27 10:34:04 +0700

    change to unify quantity editor

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/card/OrderProductCard.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/card/OrderProductCard.kt
@@ -2,24 +2,25 @@ package com.tokopedia.oneclickcheckout.order.view.card
 
 import android.graphics.Paint
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.kotlin.extensions.view.afterTextChanged
 import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.oneclickcheckout.R
 import com.tokopedia.oneclickcheckout.order.analytics.OrderSummaryAnalytics
 import com.tokopedia.oneclickcheckout.order.view.model.OrderProduct
 import com.tokopedia.oneclickcheckout.order.view.model.OrderShop
-import com.tokopedia.oneclickcheckout.order.view.model.QuantityUiModel
 import com.tokopedia.purchase_platform.common.utils.QuantityTextWatcher
 import com.tokopedia.purchase_platform.common.utils.removeDecimalSuffix
 import com.tokopedia.unifycomponents.ImageUnify
 import com.tokopedia.unifycomponents.Label
+import com.tokopedia.unifycomponents.QuantityEditorUnify
 import com.tokopedia.unifycomponents.TextFieldUnify
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.utils.currency.CurrencyFormatUtil
@@ -38,6 +39,7 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
     private val btnQtyMin by lazy { view.findViewById<ImageView>(R.id.btn_qty_min) }
     private val tvQuantityStockAvailable by lazy { view.findViewById<Typography>(R.id.tv_quantity_stock_available) }
     private val tvErrorFormValidation by lazy { view.findViewById<Typography>(R.id.tv_error_form_validation) }
+    private val qtyEditorProduct by lazy { view.findViewById<QuantityEditorUnify>(R.id.qty_editor_product) }
     private val tvShopLocation by lazy { view.findViewById<Typography>(R.id.tv_shop_location) }
     private val tvShopName by lazy { view.findViewById<Typography>(R.id.tv_shop_name) }
     private val tvProductPrice by lazy { view.findViewById<Typography>(R.id.tv_product_price) }
@@ -47,6 +49,8 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
 
     private var quantityTextWatcher: QuantityTextWatcher? = null
     private var noteTextWatcher: TextWatcher? = null
+
+    private var oldQtyValue: Int = 0
 
     fun setProduct(product: OrderProduct) {
         this.product = product
@@ -70,7 +74,7 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
             }
 
             tfNote?.textFieldInput?.textSize = 16f
-            tfNote?.textFieldInput?.setSingleLine(false)
+            tfNote?.textFieldInput?.isSingleLine = false
             tfNote?.setCounter(MAX_NOTES_LENGTH)
             tfNote?.textFieldInput?.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
                 if (hasFocus) {
@@ -97,64 +101,87 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
             }
             tfNote?.textFieldInput?.addTextChangedListener(noteTextWatcher)
 
-            if (quantityTextWatcher != null) {
-                etQty?.removeTextChangedListener(quantityTextWatcher)
-            }
-            etQty?.setText("${product.quantity.orderQuantity}")
-            quantityTextWatcher = QuantityTextWatcher(QuantityTextWatcher.QuantityTextwatcherListener { quantity ->
-                if (quantity.editable.isNotEmpty()) {
-                    var zeroCount = 0
-                    for (element in quantity.editable) {
-                        if (element == '0') {
-                            zeroCount++
-                        } else {
-                            break
-                        }
-                    }
-                    if (zeroCount == quantity.editable.length) {
-                        product.quantity.orderQuantity = 0
-                        validateQuantity()
-                        listener.onProductChange(product)
-                        return@QuantityTextwatcherListener
-                    } else if (quantity.editable[0] == '0') {
-                        etQty?.setText(quantity.editable.toString()
-                                .substring(zeroCount, quantity.editable.toString().length))
-                        etQty?.setSelection(etQty?.length() ?: 0)
-                    }
-                } else if (TextUtils.isEmpty(etQty?.text)) {
-                    product.quantity.orderQuantity = 0
-                    validateQuantity()
-                    listener.onProductChange(product)
-                    return@QuantityTextwatcherListener
-                }
-
-                var qty = 0
-                try {
-                    qty = Integer.parseInt(quantity.editable.toString())
-                } catch (e: NumberFormatException) {
-                }
-
-                product.quantity.orderQuantity = qty
-                validateQuantity()
+            qtyEditorProduct?.minValue = product.quantity.minOrderQuantity
+            qtyEditorProduct?.maxValue = product.quantity.maxOrderStock
+            oldQtyValue = product.quantity.orderQuantity
+            qtyEditorProduct?.setValue(product.quantity.orderQuantity)
+            qtyEditorProduct?.setValueChangedListener { newValue, _, _ ->
+                product.quantity.orderQuantity = newValue
                 listener.onProductChange(product)
-            })
-            etQty?.addTextChangedListener(quantityTextWatcher)
-            btnQtyPlus?.setOnClickListener {
-                if (product.quantity.orderQuantity < product.quantity.maxOrderQuantity && product.quantity.orderQuantity < product.quantity.maxOrderStock) {
-                    product.quantity.orderQuantity++
-                    etQty?.setText("${product.quantity.orderQuantity}")
-                }
+                showPrice()
+            }
+            qtyEditorProduct?.setAddClickListener {
                 orderSummaryAnalytics.eventEditQuantityIncrease(product.productId.toString(), shop.shopId.toString(), product.quantity.orderQuantity.toString())
             }
-            btnQtyMin?.setOnClickListener {
-                if (product.quantity.orderQuantity > product.quantity.minOrderQuantity) {
-                    product.quantity.orderQuantity--
-                    etQty?.setText("${product.quantity.orderQuantity}")
-                }
+            qtyEditorProduct?.setSubstractListener {
                 orderSummaryAnalytics.eventEditQuantityDecrease(product.productId.toString(), shop.shopId.toString(), product.quantity.orderQuantity.toString())
             }
+            qtyEditorProduct?.editText?.afterTextChanged {
+                val newValue = it.replace("[^0-9]".toRegex(), "").toIntOrZero()
+                if (oldQtyValue != newValue) {
+                    oldQtyValue = newValue
+                    qtyEditorProduct?.setValue(newValue)
+                }
+            }
 
-            validateQuantity()
+//            if (quantityTextWatcher != null) {
+//                etQty?.removeTextChangedListener(quantityTextWatcher)
+//            }
+//            etQty?.setText("${product.quantity.orderQuantity}")
+//            quantityTextWatcher = QuantityTextWatcher(QuantityTextWatcher.QuantityTextwatcherListener { quantity ->
+//                if (quantity.editable.isNotEmpty()) {
+//                    var zeroCount = 0
+//                    for (element in quantity.editable) {
+//                        if (element == '0') {
+//                            zeroCount++
+//                        } else {
+//                            break
+//                        }
+//                    }
+//                    if (zeroCount == quantity.editable.length) {
+//                        product.quantity.orderQuantity = 0
+//                        validateQuantity()
+//                        listener.onProductChange(product)
+//                        return@QuantityTextwatcherListener
+//                    } else if (quantity.editable[0] == '0') {
+//                        etQty?.setText(quantity.editable.toString()
+//                                .substring(zeroCount, quantity.editable.toString().length))
+//                        etQty?.setSelection(etQty?.length() ?: 0)
+//                    }
+//                } else if (TextUtils.isEmpty(etQty?.text)) {
+//                    product.quantity.orderQuantity = 0
+//                    validateQuantity()
+//                    listener.onProductChange(product)
+//                    return@QuantityTextwatcherListener
+//                }
+//
+//                var qty = 0
+//                try {
+//                    qty = Integer.parseInt(quantity.editable.toString())
+//                } catch (e: NumberFormatException) {
+//                }
+//
+//                product.quantity.orderQuantity = qty
+//                validateQuantity()
+//                listener.onProductChange(product)
+//            })
+//            etQty?.addTextChangedListener(quantityTextWatcher)
+//            btnQtyPlus?.setOnClickListener {
+//                if (product.quantity.orderQuantity < product.quantity.maxOrderQuantity && product.quantity.orderQuantity < product.quantity.maxOrderStock) {
+//                    product.quantity.orderQuantity++
+//                    etQty?.setText("${product.quantity.orderQuantity}")
+//                }
+//                orderSummaryAnalytics.eventEditQuantityIncrease(product.productId.toString(), shop.shopId.toString(), product.quantity.orderQuantity.toString())
+//            }
+//            btnQtyMin?.setOnClickListener {
+//                if (product.quantity.orderQuantity > product.quantity.minOrderQuantity) {
+//                    product.quantity.orderQuantity--
+//                    etQty?.setText("${product.quantity.orderQuantity}")
+//                }
+//                orderSummaryAnalytics.eventEditQuantityDecrease(product.productId.toString(), shop.shopId.toString(), product.quantity.orderQuantity.toString())
+//            }
+
+//            validateQuantity()
             renderProductTickerMessage()
         }
     }
@@ -171,38 +198,38 @@ class OrderProductCard(private val view: View, private val listener: OrderProduc
         }
     }
 
-    private fun validateQuantity() {
-        var error: String? = null
-        val element = product.quantity
-        btnQtyMin?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_minus_checkout_enabled)
-        btnQtyPlus?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_plus_checkout_enabled)
-
-        if (element.orderQuantity <= 0 || element.orderQuantity < element.minOrderQuantity) {
-            error = element.errorProductMinQuantity.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.minOrderQuantity.toString())
-        } else if (element.orderQuantity > element.maxOrderQuantity) {
-            error = element.errorProductMaxQuantity.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.maxOrderQuantity.toString())
-            orderSummaryAnalytics.eventViewErrorMessage(OrderSummaryAnalytics.ERROR_ID_MAX_QTY)
-        } else if (element.orderQuantity > element.maxOrderStock) {
-            error = element.errorProductAvailableStock.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.maxOrderStock.toString())
-            orderSummaryAnalytics.eventViewErrorMessage(OrderSummaryAnalytics.ERROR_ID_MAX_QTY)
-        }
-        if (element.orderQuantity <= element.minOrderQuantity) {
-            btnQtyMin?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_minus_checkout_disabled)
-        }
-        if (element.orderQuantity >= element.maxOrderQuantity || element.orderQuantity >= element.maxOrderStock) {
-            btnQtyPlus?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_plus_checkout_disabled)
-        }
-
-        if (error != null) {
-            element.isStateError = true
-            tvErrorFormValidation?.text = error
-            tvErrorFormValidation?.visibility = View.VISIBLE
-        } else {
-            element.isStateError = false
-            tvErrorFormValidation?.visibility = View.GONE
-            showPrice()
-        }
-    }
+//    private fun validateQuantity() {
+//        var error: String? = null
+//        val element = product.quantity
+//        btnQtyMin?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_minus_checkout_enabled)
+//        btnQtyPlus?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_plus_checkout_enabled)
+//
+//        if (element.orderQuantity <= 0 || element.orderQuantity < element.minOrderQuantity) {
+//            error = element.errorProductMinQuantity.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.minOrderQuantity.toString())
+//        } else if (element.orderQuantity > element.maxOrderQuantity) {
+//            error = element.errorProductMaxQuantity.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.maxOrderQuantity.toString())
+//            orderSummaryAnalytics.eventViewErrorMessage(OrderSummaryAnalytics.ERROR_ID_MAX_QTY)
+//        } else if (element.orderQuantity > element.maxOrderStock) {
+//            error = element.errorProductAvailableStock.replace(QuantityUiModel.VALUE_REPLACE_STRING, element.maxOrderStock.toString())
+//            orderSummaryAnalytics.eventViewErrorMessage(OrderSummaryAnalytics.ERROR_ID_MAX_QTY)
+//        }
+//        if (element.orderQuantity <= element.minOrderQuantity) {
+//            btnQtyMin?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_minus_checkout_disabled)
+//        }
+//        if (element.orderQuantity >= element.maxOrderQuantity || element.orderQuantity >= element.maxOrderStock) {
+//            btnQtyPlus?.setImageResource(com.tokopedia.purchase_platform.common.R.drawable.bg_button_counter_plus_checkout_disabled)
+//        }
+//
+//        if (error != null) {
+//            element.isStateError = true
+//            tvErrorFormValidation?.text = error
+//            tvErrorFormValidation?.visibility = View.VISIBLE
+//        } else {
+//            element.isStateError = false
+//            tvErrorFormValidation?.visibility = View.GONE
+//            showPrice()
+//        }
+//    }
 
     private fun showPrice() {
         tvProductPrice?.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(product.getPrice(), false).removeDecimalSuffix()
