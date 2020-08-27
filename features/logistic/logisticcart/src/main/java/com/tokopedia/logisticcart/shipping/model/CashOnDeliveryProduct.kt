8e3b6f8a0d1973667b1f2578a170e3fe40e59c48
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-08-27 14:45:35 +0700

    fix codes

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/model/CashOnDeliveryProduct.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/model/CashOnDeliveryProduct.kt
@@ -1,8 +1,9 @@
 package com.tokopedia.logisticcart.shipping.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class CashOnDeliveryProduct(
         var isCodAvailable: Int,
         val codText: String?,
@@ -10,36 +11,4 @@ data class CashOnDeliveryProduct(
         val formattedPrice: String?,
         val tncText: String?,
         val tncLink: String?
-) : Parcelable {
-    constructor(source: Parcel) : this(
-            source.readInt(),
-            source.readString(),
-            source.readInt(),
-            source.readString(),
-            source.readString(),
-            source.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(isCodAvailable)
-        parcel.writeString(codText)
-        parcel.writeInt(codPrice)
-        parcel.writeString(formattedPrice)
-        parcel.writeString(tncText)
-        parcel.writeString(tncLink)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CashOnDeliveryProduct> {
-        override fun createFromParcel(parcel: Parcel): CashOnDeliveryProduct {
-            return CashOnDeliveryProduct(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CashOnDeliveryProduct?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
