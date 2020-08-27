889ab0c3bda27b91f23d30a7a281cc9f6377706c
yogieputra8 <yogie.putra8@gmail.com>
2020-08-27 17:54:40 +0700

    Remove unused changes

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/basicinfo/data/AllowShopNameDomainChanges.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/basicinfo/data/AllowShopNameDomainChanges.kt
@@ -3,33 +3,6 @@ package com.tokopedia.shop.settings.basicinfo.data
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-//data class AllowShopNameDomainChanges(
-//    @SerializedName("allowShopNameDomainChanges")
-//    val data: AllowShopNameDomainChangesData
-//) {
-//
-//    data class AllowShopNameDomainChangesData(
-//        @SerializedName("isDomainAllowed")
-//        val isDomainAllowed: Boolean,
-//        @SerializedName("reasonDomainNotAllowed")
-//        val reasonDomainNotAllowed: String,
-//        @SerializedName("isNameAllowed")
-//        val isNameAllowed: Boolean,
-//        @SerializedName("reasonNameNotAllowed")
-//        val reasonNameNotAllowed: String,
-//        @SerializedName("error")
-//        val error: AllowShopNameDomainChangesError
-//    )
-//
-//    data class AllowShopNameDomainChangesError(
-//        @SerializedName("message")
-//        val message: String
-//    )
-//}
-
-
-
-
 
 data class AllowShopNameDomainChanges(
         @Expose
