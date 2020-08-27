5e37faa417f7df21cb5e1ae00c74afde3e3ec2e3
kuldeepyadav <kuldeep.yadav@tokopedia.com>
2020-08-27 10:05:02 +0530

    implementation of gtm tracking for youtube component

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/youtubeview/YoutubeViewViewHolder.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/youtubeview/YoutubeViewViewHolder.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.Observer
 import com.tokopedia.discovery2.R
 import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
 import com.tokopedia.discovery2.viewcontrollers.adapter.viewholder.AbstractViewHolder
-import com.tokopedia.discovery2.viewcontrollers.fragment.DiscoveryFragment
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.unifycomponents.ImageUnify
 
@@ -20,7 +19,8 @@ class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : Ab
     private var youtubeWebview: WebView = itemView.findViewById(R.id.youtube_webview)
     private val shimmerView: ImageUnify = itemView.findViewById(R.id.shimmer_view)
     private lateinit var youTubeViewViewModel: YouTubeViewViewModel
-    private lateinit var videoId: String
+    private var videoId: String = ""
+    private var videoName: String = ""
 
 
     override fun bindView(discoveryBaseViewModel: DiscoveryBaseViewModel) {
@@ -31,10 +31,9 @@ class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : Ab
     private fun setUpObserver() {
         youTubeViewViewModel.getVideoId().observe(fragment.viewLifecycleOwner, Observer {
             videoId = it.videoId ?: ""
+            videoName = it.name ?: ""
             if (shimmerView.visibility == View.VISIBLE) {
                 showVideoInWebView()
-                (fragment as? DiscoveryFragment)?.getDiscoveryAnalytics()?.trackClickVideo(it.videoId
-                        ?: "", it.name ?: "", "")
             }
         })
     }
@@ -47,6 +46,7 @@ class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : Ab
         }
         val webSettings: WebSettings = youtubeWebview.settings
         webSettings.javaScriptEnabled = true
+        youtubeWebview.addJavascriptInterface(YoutubeWebViewInterface(fragment, videoId, videoName), "jsInterface")
         val mimeType = "text/html"
         val encoding = "UTF-8"
         youtubeWebview.webChromeClient = object : WebChromeClient() {}
@@ -56,8 +56,40 @@ class YoutubeViewViewHolder(itemView: View, private val fragment: Fragment) : Ab
     }
 
     private fun getHTML(): String {
-        return "<html><body><iframe width=100% height=100% src=\"https://www.youtube.com/embed/$videoId\" frameborder=\"0\"></iframe></body></html>"
+        return "<html>\n" +
+                "  <body>\n" +
+                "    <div id=\"player\"></div>\n" +
+                "\n" +
+                "    <script>\n" +
+                "      var tag = document.createElement('script');\n" +
+                "\n" +
+                "      tag.src = \"https://www.youtube.com/iframe_api\";\n" +
+                "      var firstScriptTag = document.getElementsByTagName('script')[0];\n" +
+                "      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);\n" +
+                "\n" +
+                "      var player;\n" +
+                "      function onYouTubeIframeAPIReady() {\n" +
+                "        player = new YT.Player('player', {\n" +
+                "          height: '100%',\n" +
+                "          width: '100%',\n" +
+                "          videoId: '$videoId',\n" +
+                "          events: {\n" +
+                "            'onStateChange': onPlayerStateChange\n" +
+                "          }\n" +
+                "        });\n" +
+                "      }\n" +
+                "\n" +
+                "      function onPlayerStateChange(event) {\n" +
+                "        if (event.data == YT.PlayerState.PLAYING) {\n" +
+                "          jsInterface.onStateChanged(player.getCurrentTime());\n" +
+                "        }\n" +
+                "        else if (event.data == YT.PlayerState.PAUSED) {\n" +
+                "          jsInterface.onStateChanged(player.getCurrentTime());\n" +
+                "        }\n" +
+                "      }\n" +
+                "    </script>\n" +
+                "  </body>\n" +
+                "</html>"
     }
-
 
 }
