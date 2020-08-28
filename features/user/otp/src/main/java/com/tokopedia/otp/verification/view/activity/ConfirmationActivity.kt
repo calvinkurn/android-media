1ca70ddfb12f05e242dbea60d41af8faad066fb5
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-08-28 17:41:29 +0700

    add activity and fragment for confirmation otp

---
+++ features/user/otp/src/main/java/com/tokopedia/otp/verification/view/activity/ConfirmationActivity.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.otp.verification.view.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.otp.verification.view.fragment.ConfirmationFragment
+
+class ConfirmationActivity: BaseSimpleActivity() {
+
+    override fun getNewFragment(): Fragment? {
+        val bundle = Bundle()
+        if (intent.extras != null) {
+            bundle.putAll(intent.extras)
+        }
+        return ConfirmationFragment.createInstance(bundle)
+    }
+
+    companion object {
+        fun getCallingIntent(context: Context?, extras: Bundle?): Intent? {
+            val intent = Intent(context, ConfirmationActivity::class.java)
+            intent.putExtras(extras ?: Bundle())
+            return intent
+        }
+    }
+
+}
