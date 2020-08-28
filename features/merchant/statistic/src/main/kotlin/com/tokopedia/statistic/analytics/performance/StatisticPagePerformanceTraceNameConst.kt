346e70b697a0b99a41fd60c954cdb133e795fde1
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-08-28 11:54:29 +0800

    add statistic trace const

---
+++ features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/analytics/performance/StatisticPagePerformanceTraceNameConst.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.statistic.analytics.performance
+
+/**
+ * Created By @ilhamsuaib on 28/08/20
+ */
+
+object StatisticPagePerformanceTraceNameConst {
+
+    const val CARD_WIDGET_TRACE = "statistic_card_widget"
+    const val LINE_GRAPH_WIDGET_TRACE = "statistic_line_graph_widget"
+    const val PROGRESS_WIDGET_TRACE = "statistic_progress_widget"
+    const val POST_LIST_WIDGET_TRACE = "statistic_post_list_widget"
+    const val CAROUSEL_WIDGET_TRACE = "statistic_carousel_widget"
+    const val TABLE_WIDGET_TRACE = "statistic_table_widget"
+    const val PIE_CHART_WIDGET_TRACE = "statistic_pie_chart_widget"
+    const val BAR_CHART_WIDGET_TRACE = "statistic_bar_chart_widget"
+    const val STATISTIC_LAYOUT_TRACE = "statistic_layout"
+}
