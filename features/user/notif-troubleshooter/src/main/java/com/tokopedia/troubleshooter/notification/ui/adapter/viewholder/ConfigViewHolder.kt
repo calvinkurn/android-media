c04e0b3d54e469aa5177cc123a12e3aa1b073b79
isfaaghyth <isfaaghyth@gmail.com>
2020-08-30 19:39:24 +0700

    changed progressBar to loader and icon status

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/ConfigViewHolder.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/ConfigViewHolder.kt
@@ -3,17 +3,17 @@ package com.tokopedia.troubleshooter.notification.ui.adapter.viewholder
 import android.net.Uri
 import android.view.View
 import android.widget.ImageView
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.kotlin.extensions.view.invisible
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.troubleshooter.notification.R
 import com.tokopedia.troubleshooter.notification.ui.listener.ConfigItemListener
 import com.tokopedia.troubleshooter.notification.ui.uiview.ConfigState
 import com.tokopedia.troubleshooter.notification.ui.uiview.ConfigUIView
 import com.tokopedia.troubleshooter.notification.ui.uiview.StatusState
+import com.tokopedia.unifycomponents.LoaderUnify
 import com.tokopedia.abstraction.common.utils.view.MethodChecker.getDrawable as drawable
 
 open class ConfigViewHolder(
@@ -21,7 +21,7 @@ open class ConfigViewHolder(
         view: View
 ): AbstractViewHolder<ConfigUIView>(view) {
 
-    private val pgLoader = view.findViewById<ProgressBar>(R.id.pgLoader)
+    private val pgLoader = view.findViewById<LoaderUnify>(R.id.pgLoader)
     private val imgStatus = view.findViewById<ImageView>(R.id.imgStatus)
     private val txtTitle = view.findViewById<TextView>(R.id.txtTitle)
     private val txtMessage = view.findViewById<TextView>(R.id.txtMessage)
@@ -57,7 +57,6 @@ open class ConfigViewHolder(
 
     private fun notificationSetting(status: StatusState) {
         troubleshootStatus(status)
-
         itemView.setOnClickListener {
             listener.goToNotificationSettings()
         }
@@ -88,20 +87,16 @@ open class ConfigViewHolder(
 
     private fun troubleshootStatus(status: StatusState) {
         when (status) {
-            is StatusState.Success -> {
-                iconStatusVisibility(R.drawable.ic_status_notif_green_checked)
-            }
-            is StatusState.Error -> {
-                iconStatusVisibility(R.drawable.ic_status_notif_red_error)
-            }
+            is StatusState.Success -> visibility(R.drawable.ic_ts_notif_checked)
+            is StatusState.Error -> visibility(R.drawable.ic_ts_notif_failed)
         }
     }
 
-    private fun iconStatusVisibility(resource: Int) {
+    private fun visibility(resource: Int) {
         val status = drawable(context, resource)
         imgStatus?.setImageDrawable(status)
         imgStatus?.show()
-        pgLoader?.invisible()
+        pgLoader?.hide()
     }
 
     companion object {
