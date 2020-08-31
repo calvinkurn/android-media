bebdfde4fcc1d614e79b74925b62e36cde7305aa
yogieputra8 <yogie.putra8@gmail.com>
2020-08-31 14:20:29 +0700

    Add param to shop common

---
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/constant/ShopShowcaseParamConstant.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.shop.common.constant
+
+object ShopShowcaseParamConstant {
+    val EXTRA_SHOP_ID = "EXTRA_SHOP_ID"
+    val EXTRA_ETALASE_ID = "EXTRA_ETALASE_ID"
+    val EXTRA_IS_SHOW_DEFAULT = "EXTRA_IS_SHOW_DEFAULT"
+    val EXTRA_IS_SHOW_ZERO_PRODUCT = "EXTRA_IS_HIDE_HERO_PRODUCT"
+}
