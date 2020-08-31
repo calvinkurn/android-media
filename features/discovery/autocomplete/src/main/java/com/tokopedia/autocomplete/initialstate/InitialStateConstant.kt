6125fd1955f13f604a414ce566f81651dd9d82cd
glenntuyu <christian.tuyu@tokopedia.com>
2020-08-31 15:52:18 +0700

    add _use_case prefix for the named const

--- features/discovery/autocomplete/src/main/java/com/tokopedia/autocomplete/initialstate/InitialStateConstant.kt
+++ features/discovery/autocomplete/src/main/java/com/tokopedia/autocomplete/initialstate/InitialStateConstant.kt
@@ -1,5 +1,5 @@
 package com.tokopedia.autocomplete.initialstate
 
 const val TYPE_SHOP = "shop"
-const val INITIAL_STATE_USE_CASE = "initial_state"
-const val REFRESH_INITIAL_STATE_USE_CASE = "refresh_initial_state"
+const val INITIAL_STATE_USE_CASE = "initial_state_use_case"
+const val REFRESH_INITIAL_STATE_USE_CASE = "refresh_initial_state_use_case"
