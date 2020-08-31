ab1aeec23f28851c1db6be15990d4abb1e25d8d6
saidfaisal <said.faisal@tokopedia.com>
2020-08-31 13:08:54 +0700

    remove unnecessary resources for drafting in tkpdseller

--- tkpdseller/src/main/java/com/tokopedia/seller/product/draft/domain/interactor/FetchAllDraftProductUseCase.java
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/draft/domain/interactor/FetchAllDraftProductUseCase.java
@@ -1,12 +1,10 @@
 package com.tokopedia.seller.product.draft.domain.interactor;
 
-import com.tokopedia.core.base.domain.RequestParams;
-import com.tokopedia.core.base.domain.UseCase;
-import com.tokopedia.core.base.domain.executor.PostExecutionThread;
-import com.tokopedia.core.base.domain.executor.ThreadExecutor;
 import com.tokopedia.product.manage.common.draft.data.db.repository.AddEditProductDraftRepository;
 import com.tokopedia.seller.manageitem.data.db.ProductDraftViewModel;
 import com.tokopedia.seller.product.draft.domain.mapper.ProductDraftMapper;
+import com.tokopedia.usecase.RequestParams;
+import com.tokopedia.usecase.UseCase;
 
 import java.util.List;
 
@@ -20,23 +18,18 @@ import rx.Observable;
 
 public class FetchAllDraftProductUseCase extends UseCase<List<ProductDraftViewModel>> {
     private AddEditProductDraftRepository addEditProductDraftRepository;
-    private ProductDraftMapper mapper;
 
     @Inject
-    public FetchAllDraftProductUseCase(ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread,
-                                       AddEditProductDraftRepository addEditProductDraftRepository, ProductDraftMapper mapper) {
-        super(threadExecutor, postExecutionThread);
+    public FetchAllDraftProductUseCase(AddEditProductDraftRepository addEditProductDraftRepository) {
         this.addEditProductDraftRepository = addEditProductDraftRepository;
-        this.mapper = mapper;
-    }
-
-    @Override
-    public Observable<List<ProductDraftViewModel>> createObservable(RequestParams requestParams) {
-        return Observable.fromCallable(() -> addEditProductDraftRepository.getAllDrafts())
-                .map(productInputModels -> mapper.mapDomainDataModelToViewModel(productInputModels));
     }
 
     public static RequestParams createRequestParams() {
         return RequestParams.EMPTY;
+    }
+
+    @Override
+    public Observable<List<ProductDraftViewModel>> createObservable(RequestParams requestParams) {
+        return Observable.fromCallable(() -> addEditProductDraftRepository.getAllDrafts()).map(ProductDraftMapper.INSTANCE::mapDomainDataModelToViewModel);
     }
 }
--- tkpdseller/src/main/java/com/tokopedia/seller/product/draft/domain/mapper/ProductDraftMapper.kt
+++ tkpdseller/src/main/java/com/tokopedia/seller/product/draft/domain/mapper/ProductDraftMapper.kt
@@ -3,9 +3,8 @@ package com.tokopedia.seller.product.draft.domain.mapper
 import android.text.TextUtils
 import com.tokopedia.product.manage.common.draft.data.model.ProductDraft
 import com.tokopedia.seller.manageitem.data.db.ProductDraftViewModel
-import javax.inject.Inject
 
-class ProductDraftMapper @Inject constructor() {
+object ProductDraftMapper {
     fun mapDomainDataModelToViewModel(drafts: List<ProductDraft>) = drafts.map { draft ->
         val primaryImageUrl = draft.detailInputModel.imageUrlOrPathList.firstOrNull().orEmpty()
         val productName = draft.detailInputModel.productName
