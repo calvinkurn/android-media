999bb102b134a119e418e14341045b67b634748d
elscione <yusufhendrawan06@gmail.com>
2020-08-31 21:21:12 +0700

    remove unused logger method

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/AddEditProductErrorHandler.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/AddEditProductErrorHandler.kt
@@ -3,32 +3,11 @@ package com.tokopedia.product.addedit.common.util
 import com.crashlytics.android.Crashlytics
 import com.tokopedia.product.addedit.BuildConfig
 import timber.log.Timber
-import java.io.PrintWriter
-import java.io.StringWriter
 
 /**
  * @author by milhamj on 21/04/20.
  */
 object AddEditProductErrorHandler {
-    fun logMessage(throwable: Throwable, message: String) {
-        try {
-            if (!BuildConfig.DEBUG) {
-                val stackTrace = StringBuilder()
-                var cause: Throwable? = throwable
-                while (cause != null) {
-                    val stringWriter = StringWriter()
-                    cause.printStackTrace(PrintWriter(stringWriter))
-                    cause = cause.cause
-                    stackTrace.append(stringWriter)
-                }
-                Crashlytics.log("$message $stackTrace")
-            } else {
-                Timber.e(throwable, message)
-            }
-        } catch (e: IllegalStateException) {
-            e.printStackTrace()
-        }
-    }
 
     fun logMessage(message: String) {
         try {
