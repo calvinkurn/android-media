30626b31f2a9ec8efcc9d7dddfc5deee9fdec8bd
varun0922 <varun.sharma@tokopedia.com>
2020-09-01 13:37:55 +0530

    Changes for internal app Brotli Support

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SellerMainApplication.java
@@ -66,6 +66,7 @@ public class SellerMainApplication extends SellerRouterApplication implements Mo
         InAppManager.InAppMessageListener {
 
     public static final String ANDROID_ROBUST_ENABLE = "android_sellerapp_robust_enable";
+    private static final String ADD_BROTLI_INTERCEPTOR = "android_add_brotli_interceptor";
 
     static {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
@@ -164,7 +165,7 @@ public class SellerMainApplication extends SellerRouterApplication implements Mo
         MoEPushCallBacks.getInstance().setOnMoEPushNavigationAction(this);
         InAppManager.getInstance().setInAppListener(this);
         initCacheApi();
-        GraphqlClient.init(this);
+        GraphqlClient.init(this, remoteConfig.getBoolean(ADD_BROTLI_INTERCEPTOR, false));
         NetworkClient.init(this);
         initializeAbTestVariant();
 
