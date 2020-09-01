f0ffab1d74e9cfc18d9dcc7c9085482b73bdd1a6
isfaaghyth <isfaaghyth@gmail.com>
2020-09-01 09:55:44 +0700

    created an extension of livedata combinators

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/util/Extensions.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/util/Extensions.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.troubleshooter.notification.util
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import java.lang.Exception
 
 fun <T> MutableList<T>.dropFirst() {
@@ -22,4 +24,22 @@ fun String.prefixToken(): String {
 inline fun <T> Iterable<T>.getWithIndex(predicate: (T) -> Boolean): Pair<Int, T>? {
     forEachIndexed { index, element -> if (predicate(element)) return Pair(index, element) }
     return null
+}
+
+fun <T, K, J, R> LiveData<T>.combineWith(
+        f1: LiveData<K>,
+        f2: LiveData<J>,
+        block: (T?, K?, J?) -> R
+): LiveData<R> {
+    val result = MediatorLiveData<R>()
+    result.addSource(this) {
+        result.value = block(this.value, f1.value, f2.value)
+    }
+    result.addSource(f1) {
+        result.value = block(this.value, f1.value, f2.value)
+    }
+    result.addSource(f2) {
+        result.value = block(this.value, f1.value, f2.value)
+    }
+    return result
 }
