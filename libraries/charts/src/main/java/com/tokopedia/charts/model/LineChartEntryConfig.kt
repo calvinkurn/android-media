1cb7f8831d31471e29d7c4fe5d44fbf766a740c0
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-09-01 16:34:48 +0800

    add line chart entry config model

---
+++ libraries/charts/src/main/java/com/tokopedia/charts/model/LineChartEntryConfig.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.charts.model
+
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import com.tokopedia.charts.common.ChartColor
+
+/**
+ * Created By @ilhamsuaib on 01/09/20
+ */
+
+data class LineChartEntryConfig(
+        val lineWidth: Float = 1f,
+        val lineColor: Int = Color.parseColor(ChartColor.DEFAULT_LINE_COLOR),
+        val fillDrawable: Drawable? = null,
+        val fillColor: Int = Color.parseColor(ChartColor.DEFAULT_LINE_CHART_FILL_COLOR),
+        val drawFillEnabled: Boolean = true
+)
