68ba0071e534b04d666c3f5bc53cdce341f98717
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-09-02 22:52:11 +0800

    simplify code in extractUrls method

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/common/SellerHomeCommonUtils.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/common/SellerHomeCommonUtils.kt
@@ -13,10 +13,10 @@ object SellerHomeCommonUtils {
      * Returns list of links contained in given text
      */
     fun extractUrls(text: String): List<String> {
-        val containedUrls: MutableList<String> = ArrayList()
         val urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)"
         val pattern: Pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE)
         val urlMatcher: Matcher = pattern.matcher(text)
+        val containedUrls = mutableListOf<String>()
         while (urlMatcher.find()) {
             containedUrls.add(text.substring(urlMatcher.start(0), urlMatcher.end(0)))
         }
