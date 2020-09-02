26df6b7b8f3842d82f56efebe0f849797a42d6d7
faisalramdani <faisal.ramdani@tokopedia.com>
2020-09-02 09:35:10 +0700

    fix removing variant level

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/variant/presentation/viewmodel/AddEditProductVariantViewModel.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/variant/presentation/viewmodel/AddEditProductVariantViewModel.kt
@@ -346,7 +346,7 @@ class AddEditProductVariantViewModel @Inject constructor(
             // get selected variant detail selected each level
             if (it.value.isNotEmpty()) {
                 variantDetailsSelected.getOrNull(level)?.let { variantDetail ->
-                    val unit = variantUnitMap.getOrElse(level) { Unit() }
+                    val unit = variantUnitMap.getOrElse(it.key) { Unit() }
                     result.add(SelectionInputModel(
                             variantDetail.variantID.toString(),
                             variantDetail.name,
