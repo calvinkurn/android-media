67c00beb7527d5ce646d6da92e5e9646b56b36de
TokoDeepika <deepika.saini@samsung.com>
2020-09-02 12:37:57 +0530

    setting empty view to default manual ads and removing group delete call on product deletion

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsProductIklanFragment.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsProductIklanFragment.kt
@@ -167,7 +167,6 @@ class TopAdsProductIklanFragment : BaseDaggerFragment(), TopAdsDashboardView, Cu
         val view = inflater.inflate(R.layout.topads_dash_fragment_product_iklan, container, false)
         recyclerView = view.findViewById(R.id.auto_ads_list)
         imgBg = view.findViewById(R.id.progressImg)
-        setAutoAdsAdapter()
         return view
     }
 
@@ -233,7 +232,8 @@ class TopAdsProductIklanFragment : BaseDaggerFragment(), TopAdsDashboardView, Cu
         Utils.setSearchListener(context, view, ::fetchData)
     }
 
-    private fun renderViewPager() {
+    private fun renderManualViewPager() {
+        tab_layout.visibility = View.VISIBLE
         view_pager_frag?.adapter = getViewPagerAdapter()
         view_pager_frag.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
         tab_layout.setupWithViewPager(view_pager_frag)
@@ -322,7 +322,7 @@ class TopAdsProductIklanFragment : BaseDaggerFragment(), TopAdsDashboardView, Cu
             app_bar_layout_2.visibility = View.VISIBLE
             autoads_layout.visibility = View.VISIBLE
         } else {
-
+            manualAds()
         }
     }
 
@@ -359,8 +359,8 @@ class TopAdsProductIklanFragment : BaseDaggerFragment(), TopAdsDashboardView, Cu
         } else {
             autoadsDeactivationProgress?.visibility = View.GONE
             autoadsOnboarding.visibility = View.VISIBLE
+            renderManualViewPager()
         }
-        renderViewPager()
     }
 
 
