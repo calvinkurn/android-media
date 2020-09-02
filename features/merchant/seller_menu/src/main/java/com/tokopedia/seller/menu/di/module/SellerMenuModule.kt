d4fac52521268a683295058fea56cb5a2ba6dd82
rezagama <reza.hidayat@tokopedia.com>
2020-09-03 00:39:13 +0700

    Setup seller menu DI

---
+++ features/merchant/seller_menu/src/main/java/com/tokopedia/seller/menu/di/module/SellerMenuModule.kt
@@ -0,0 +1,35 @@
+package com.tokopedia.seller.menu.di.module
+
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.seller.menu.coroutine.CoroutineDispatchers
+import com.tokopedia.seller.menu.coroutine.CoroutineDispatchersProvider
+import com.tokopedia.seller.menu.di.scope.SellerMenuScope
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
+import dagger.Module
+import dagger.Provides
+
+@Module(includes = [ViewModelModule::class])
+class SellerMenuModule {
+
+    @SellerMenuScope
+    @Provides
+    fun provideCoroutineDispatchers(): CoroutineDispatchers {
+        return CoroutineDispatchersProvider
+    }
+
+    @SellerMenuScope
+    @Provides
+    fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface {
+        return UserSession(context)
+    }
+
+    @SellerMenuScope
+    @Provides
+    fun provideUserGrqphqlRepository(): GraphqlRepository {
+        return GraphqlInteractor.getInstance().graphqlRepository
+    }
+}
