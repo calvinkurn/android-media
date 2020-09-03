42a7b8eaa09741f4382e5c9ae63d0d99faeb6ac0
rezagama <reza.hidayat@tokopedia.com>
2020-09-03 11:38:20 +0700

    Revert "Remove centralized promo redirection"
    This reverts commit 57bd8eb6

--- libraries/applink/src/main/java/com/tokopedia/applink/sellermigration/SellerMigrationFeatureName.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/sellermigration/SellerMigrationFeatureName.kt
@@ -50,5 +50,6 @@ annotation class SellerMigrationFeatureName {
         const val FEATURE_PLAY_FEED = "feed"
         const val FEATURE_FINANCIAL_SERVICES = "financial_services"
         const val FEATURE_BROADCAST_CHAT = "broadcast_chat"
+        const val FEATURE_CENTRALIZED_PROMO = "centralized_promo"
     }
 }
