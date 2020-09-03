92c57f66dc266b9f76ce4b5090091e7ec918ae40
yehezkiel <yehezkiel@tokopedia.com>
2020-09-03 09:29:00 +0700

    remove delete database

--- features/merchant/product_detail/src/androidTest/java/com/tokopedia/product/detail/PltProductPerformanceTest.kt
+++ features/merchant/product_detail/src/androidTest/java/com/tokopedia/product/detail/PltProductPerformanceTest.kt
@@ -35,7 +35,6 @@ class PltProductPerformanceTest {
 
         val intent = ProductDetailActivity.createIntent(context, "1061061424")
         activityRule.launchActivity(intent)
-        activityRule.activity.deleteDatabase("tokopedia_graphql")
     }
 
     private fun createMockModelConfig(): MockModelConfig {
