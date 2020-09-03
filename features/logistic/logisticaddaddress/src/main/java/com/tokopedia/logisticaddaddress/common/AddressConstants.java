1a2a480f2e405e4035993368f3bd5be5305dceeb
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-09-03 11:56:16 +0700

    getDistrict circuitBreaker

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/common/AddressConstants.java
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/common/AddressConstants.java
@@ -57,6 +57,9 @@ public class AddressConstants {
     public static final String EXTRA_IS_FULL_FLOW = "EXTRA_IS_FULL_FLOW";
     public static final String EXTRA_ADDRESS_MODEL = "EXTRA_ADDRESS_MODEL";
     public static final String EXTRA_IS_LOGISTIC_LABEL = "EXTRA_IS_LOGISTIC_LABEL";
+    public static final String EXTRA_IS_CIRCUIT_BREAKER = "EXTRA_IS_CIRCUIT_BREAKER";
+
+    public static final Integer CIRCUIT_BREAKER_ON_CODE = 101;
 
     public static final Double DEFAULT_LAT = MONAS_LAT;
     public static final Double DEFAULT_LONG = MONAS_LONG;
