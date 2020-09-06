74864903cd7cacbcb6457cc59ad49fbae531cabc
isfaaghyth <isfaaghyth@gmail.com>
2020-09-06 23:23:51 +0700

    added clear cache dialog on footer item

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/FooterViewHolder.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/FooterViewHolder.kt
@@ -5,10 +5,20 @@ import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.troubleshooter.notification.R
 import com.tokopedia.troubleshooter.notification.ui.uiview.FooterUIView
+import com.tokopedia.troubleshooter.notification.util.ClearCacheUtil
+import com.tokopedia.troubleshooter.notification.util.ClearCacheUtil.showClearCache
+import com.tokopedia.unifycomponents.UnifyButton
 
 class FooterViewHolder(view: View): AbstractViewHolder<FooterUIView>(view) {
 
-    override fun bind(element: FooterUIView?) {}
+    private val btnAction = view.findViewById<UnifyButton>(R.id.btnAction)
+    private val context by lazy { itemView.context }
+
+    override fun bind(element: FooterUIView?) {
+        btnAction?.setOnClickListener {
+            showClearCache(context)
+        }
+    }
 
     companion object {
         @LayoutRes val LAYOUT = R.layout.item_footer_message
