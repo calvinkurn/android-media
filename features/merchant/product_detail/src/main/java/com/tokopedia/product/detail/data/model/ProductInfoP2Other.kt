44a1fda278e5a09a27c5ff809e109fd2f4bc244e
yehezkiel <yehezkiel@tokopedia.com>
2020-09-08 15:33:59 +0700

    remove unused talk

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/ProductInfoP2Other.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/ProductInfoP2Other.kt
@@ -3,7 +3,6 @@ package com.tokopedia.product.detail.data.model
 import com.tokopedia.gallery.viewmodel.ImageReviewItem
 import com.tokopedia.product.detail.data.model.review.Review
 import com.tokopedia.product.detail.data.model.talk.DiscussionMostHelpful
-import com.tokopedia.product.detail.data.model.talk.Talk
 
 /**
  * Created by Yehezkiel on 27/07/20
@@ -11,6 +10,5 @@ import com.tokopedia.product.detail.data.model.talk.Talk
 data class ProductInfoP2Other(
         var imageReviews: List<ImageReviewItem> = listOf(),
         var helpfulReviews: List<Review> = listOf(),
-        var latestTalk: Talk = Talk(),
         var discussionMostHelpful: DiscussionMostHelpful? = null
 )
