768877f09a0c142d184e643130d19cad88e7bad9
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-09-08 16:03:53 +0700

    add reflection

--- libraries/applink/src/test/java/tokopedia/applink/DeepLinkDFMapperTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/DeepLinkDFMapperTest.kt
@@ -1,37 +1,42 @@
 package tokopedia.applink
 
 import android.content.Context
+import android.os.Build
 import com.tokopedia.applink.DeeplinkDFMapper
-import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
-import org.junit.Assert.assertTrue
+import com.tokopedia.applink.internal.ApplinkConstInternalCategory
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockkObject
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
 
 class DeepLinkDFMapperTest {
 
-    @Mock
+    @RelaxedMockK
     lateinit var context: Context
-
-    @Mock
-    lateinit var deepLinkDFMapperTest: DeeplinkDFMapper
 
     @Before
     fun setup() {
-        MockitoAnnotations.initMocks(this)
+        MockKAnnotations.init(this)
+        mockkObject(DeeplinkDFMapper)
+        setFinalStatic(Build.VERSION::class.java.getField("SDK_INT"), 28)
     }
 
     @Test
     fun `check applink onboarding, should return DF_BASE`() {
-        val DF_BASE = "df_base"
-        val actualResult = deepLinkDFMapperTest.getDFDeeplinkIfNotInstalled(context, ApplinkConstInternalMarketplace.ONBOARDING)
-        assertTrue(actualResult?.equals(DF_BASE) ?: false)
+        val DF_BASE = "df_category_trade_in"
+        every { DeeplinkDFMapper.getDFDeeplinkIfNotInstalled(context, ApplinkConstInternalCategory.TRADEIN) }
     }
 
-
-    class DeepLinkDF(applink: String,
-                     moduleId: String
-    )
-
+    @Throws(Exception::class)
+    fun setFinalStatic(field: Field, newValue: Any?) {
+        field.isAccessible = true
+        val modifiersField: Field = Field::class.java.getDeclaredField("modifiers")
+        modifiersField.isAccessible = true
+        modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
+        field.set(null, newValue)
+    }
 }
