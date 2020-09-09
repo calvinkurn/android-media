14edaee113bf0d20456222df6f5bdbe403bb8c7e
rezagama <reza.hidayat@tokopedia.com>
2020-09-09 12:00:31 +0700

    Add missing imports

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/fragment/ProductManageFragment.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/fragment/ProductManageFragment.kt
@@ -125,6 +125,8 @@ import com.tokopedia.shop.common.data.source.cloud.query.param.option.FilterOpti
 import com.tokopedia.topads.common.data.model.DataDeposit
 import com.tokopedia.topads.common.data.model.FreeDeposit.Companion.DEPOSIT_ACTIVE
 import com.tokopedia.topads.freeclaim.data.constant.TOPADS_FREE_CLAIM_URL
+import com.tokopedia.topads.sourcetagging.constant.TopAdsSourceOption
+import com.tokopedia.topads.sourcetagging.constant.TopAdsSourceTaggingConstant
 import com.tokopedia.unifycomponents.Toaster
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
