42c763f360b22a03a2e442c3a2d6b39402f43598
isfaaghyth <isfaaghyth@gmail.com>
2020-09-09 19:28:01 +0700

    remove unused warning title of tickers

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/TickerViewHolder.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/TickerViewHolder.kt
@@ -8,8 +8,6 @@ import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolde
 import com.tokopedia.troubleshooter.notification.R
 import com.tokopedia.troubleshooter.notification.ui.adapter.TickerAdapter
 import com.tokopedia.troubleshooter.notification.ui.adapter.factory.TickerItemFactory
-import com.tokopedia.troubleshooter.notification.ui.state.ConfigState.Ringtone
-import com.tokopedia.troubleshooter.notification.ui.uiview.TickerItemUIView
 import com.tokopedia.troubleshooter.notification.ui.uiview.TickerUIView
 import com.tokopedia.unifyprinciples.Typography
 
@@ -17,34 +15,20 @@ class TickerViewHolder(view: View): AbstractViewHolder<TickerUIView>(view) {
 
     private val txtTitle = view.findViewById<Typography>(R.id.txtTitle)
     private val lstTicker = view.findViewById<RecyclerView>(R.id.lstTicker)
+
     private val context by lazy { itemView.context }
     private var adapter: TickerAdapter? = null
 
     override fun bind(element: TickerUIView?) {
         if (element == null) return
-        warningTitle(element)
-
         if (adapter == null) {
             adapter = TickerAdapter(TickerItemFactory())
             lstTicker?.layoutManager = LinearLayoutManager(context)
             lstTicker?.adapter = adapter
         }
 
+        txtTitle?.text = context.getString(R.string.notif_ticker_title)
         adapter?.addTickers(element.tickers)
-    }
-
-    private fun warningTitle(element: TickerUIView) {
-        if (element.tickers.size == 1) {
-            val ticker = element.tickers.first() as TickerItemUIView
-            txtTitle?.text = when (ticker.type) {
-                is Ringtone -> context.getString(R.string.notif_ticker_ringtone_title)
-                else -> context.getString(R.string.notif_ticker_notification_title)
-            }
-
-            return
-        }
-
-        txtTitle?.text = context.getString(R.string.notif_ticker_common_title)
     }
 
     companion object {
