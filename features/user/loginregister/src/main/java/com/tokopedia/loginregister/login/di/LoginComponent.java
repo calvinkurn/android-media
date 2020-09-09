16b97ebe1067c9f5c529bd8b88edf54eae8e4733
Rizaldi Martaputra <rizaldi.martaputra@tokopedia.com>
2020-09-09 18:45:05 +0700

    Create device fingerprint dagger module in loginregister module

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginComponent.java
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginComponent.java
@@ -1,6 +1,5 @@
 package com.tokopedia.loginregister.login.di;
 
-import com.tokopedia.devicefingerprint.di.DeviceFingerprintModule;
 import com.tokopedia.loginregister.common.di.LoginRegisterComponent;
 import com.tokopedia.loginregister.login.view.activity.LoginActivity;
 import com.tokopedia.loginregister.login.view.fragment.LoginEmailPhoneFragment;
--- libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/di/DeviceFingerprintScope.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.devicefingerprint.di
-
-import javax.inject.Scope
-
-@Scope
-@kotlin.annotation.Retention(AnnotationRetention.BINARY)
-annotation class DeviceFingerprintScope
