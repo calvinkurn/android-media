e0e215c62ac5c23f0ee845421e56330dc3268fc7
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-09-09 19:12:30 +0800

    use base simple activity in statistic page

--- features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/activity/StatisticActivity.kt
+++ features/merchant/statistic/src/main/kotlin/com/tokopedia/statistic/presentation/view/activity/StatisticActivity.kt
@@ -3,7 +3,8 @@ package com.tokopedia.statistic.presentation.view.activity
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.kotlin.extensions.view.setLightStatusBar
 import com.tokopedia.kotlin.extensions.view.setStatusBarColor
 import com.tokopedia.statistic.R
@@ -18,18 +19,28 @@ import com.tokopedia.statistic.presentation.view.fragment.StatisticFragment
 
 // Internal applink : ApplinkConstInternalMechant.MERCHANT_STATISTIC_DASHBOARD
 
-class StatisticActivity : BaseActivity(), StatisticPerformanceMonitoringListener {
+class StatisticActivity : BaseSimpleActivity(), StatisticPerformanceMonitoringListener {
 
     private val performanceMonitoring: StatisticPerformanceMonitoringInterface by lazy {
         StatisticPerformanceMonitoring()
     }
 
+    private val statisticFragment by lazy {
+        StatisticFragment.newInstance()
+    }
+
+    override fun getNewFragment(): Fragment? {
+        return statisticFragment
+    }
+
+    override fun getParentViewResourceID(): Int = R.id.parent_view_stc
+
+    override fun getLayoutRes(): Int = R.layout.activity_stc_statistic
+
     override fun onCreate(savedInstanceState: Bundle?) {
         initPerformanceMonitoring()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stc_statistic)
-
-        showFragment()
         setWhiteStatusBar()
     }
 
@@ -43,12 +54,6 @@ class StatisticActivity : BaseActivity(), StatisticPerformanceMonitoringListener
 
     override fun stopPerformanceMonitoring() {
         performanceMonitoring.stopPerformanceMonitoring()
-    }
-
-    private fun showFragment() {
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.parent_view_stc, StatisticFragment.newInstance())
-                .commitNowAllowingStateLoss()
     }
 
     private fun setWhiteStatusBar() {
