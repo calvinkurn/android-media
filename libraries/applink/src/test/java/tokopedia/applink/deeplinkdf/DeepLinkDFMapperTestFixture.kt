e5bd96bfee4c7c74e6d73166159b97cb0e83a1db
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-09-10 22:52:22 +0700

    add DeepLinkDFMapperTestFixture.kt

---
+++ libraries/applink/src/test/java/tokopedia/applink/deeplinkdf/DeepLinkDFMapperTestFixture.kt
@@ -0,0 +1,58 @@
+package tokopedia.applink.deeplinkdf
+
+import com.tokopedia.applink.DFP
+import com.tokopedia.applink.DeeplinkDFMapper
+import io.mockk.MockKAnnotations
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.After
+import org.junit.Before
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.io.InputStreamReader
+
+abstract class DeepLinkDFMapperTestFixture {
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        mockkObject(DeeplinkDFMapper)
+    }
+
+    @After
+    fun finish() {
+        unmockkAll()
+    }
+
+    protected fun getDeepLinkIdFromDeepLink(dfpList: List<DFP>?, path: InputStream?): List<DFP>? {
+        val turnedOnDfSet = path?.let { getDFFilterMap(it) }
+        if (turnedOnDfSet == null || turnedOnDfSet.isEmpty()) {
+            return listOf()
+        }
+        val resultList = mutableListOf<DFP>()
+        dfpList?.forEach {
+            if (turnedOnDfSet.contains(it.moduleId)) {
+                resultList.add(it)
+            }
+        }
+        return resultList
+    }
+
+    private fun getDFFilterMap(path: InputStream): Set<String>? {
+        return try {
+            val set: HashSet<String> = hashSetOf()
+            val reader = BufferedReader(InputStreamReader(path))
+            var line: String? = reader.readLine()
+            while (line != null) {
+                if (line.isNotEmpty()) {
+                    set.add(line)
+                }
+                line = reader.readLine()
+            }
+            set
+        } catch (e: FileNotFoundException) {
+            null
+        }
+    }
+}
