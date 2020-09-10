26c3e25da606989ad0982f4625c15d03747044c9
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-09-10 19:02:24 +0800

    create seller home coroutine dispatcher

---
+++ features/merchant/seller_home/src/test/java/com/tokopedia/sellerhome/utils/SellerHomeCoroutineDispatcherTest.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.sellerhome.utils
+
+import com.tokopedia.sellerhome.common.coroutine.SellerHomeCoroutineDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+
+/**
+ * Created By @ilhamsuaib on 10/09/20
+ */
+
+object SellerHomeCoroutineDispatcherTest : SellerHomeCoroutineDispatcher {
+
+    override fun io(): CoroutineDispatcher = Dispatchers.Unconfined
+
+    override fun main(): CoroutineDispatcher = Dispatchers.Unconfined
+
+    @ExperimentalCoroutinesApi
+    fun testCoroutineDispatcher(): CoroutineDispatcher = TestCoroutineDispatcher()
+}
