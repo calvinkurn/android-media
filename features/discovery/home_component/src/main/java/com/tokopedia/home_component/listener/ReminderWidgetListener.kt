2547502bdf87a5a75b27677b4412e8e03427e5aa
resakemal <resa.saharso@tokopedia.com>
2020-09-11 10:28:45 +0700

    Revert change

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/listener/ReminderWidgetListener.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/listener/ReminderWidgetListener.kt
@@ -4,7 +4,7 @@ import com.tokopedia.home_component.visitable.ReminderWidgetModel
 
 interface ReminderWidgetListener {
     fun onReminderWidgetClickListener(reminderData: ReminderWidgetModel)
-    fun onReminderWidgetDeclineClickListener(reminderData: ReminderWidgetModel, toggleTracking: Boolean = false)
+    fun onReminderWidgetDeclineClickListener(reminderData: ReminderWidgetModel,toggleTracking: Boolean = false)
     fun onReminderWidgetImpressionListener(reminderData: ReminderWidgetModel)
     fun getReminderWidgetData(reminderData: ReminderWidgetModel)
 }
