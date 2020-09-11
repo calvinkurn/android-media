f459f45c55f6743157a64fa678165399d5158049
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-09-11 10:32:12 +0800

    remove TestCoroutineDispatcher from seller home coroutine test dispatcher

--- features/merchant/seller_home/src/test/java/com/tokopedia/sellerhome/utils/SellerHomeCoroutineTestDispatcher.kt
+++ features/merchant/seller_home/src/test/java/com/tokopedia/sellerhome/utils/SellerHomeCoroutineTestDispatcher.kt
@@ -3,8 +3,6 @@ package com.tokopedia.sellerhome.utils
 import com.tokopedia.sellerhome.common.coroutine.SellerHomeCoroutineDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 
 /**
  * Created By @ilhamsuaib on 10/09/20
@@ -15,7 +13,4 @@ object SellerHomeCoroutineTestDispatcher : SellerHomeCoroutineDispatcher {
     override fun io(): CoroutineDispatcher = Dispatchers.Unconfined
 
     override fun main(): CoroutineDispatcher = Dispatchers.Unconfined
-
-    @ExperimentalCoroutinesApi
-    fun testCoroutineDispatcher(): CoroutineDispatcher = TestCoroutineDispatcher()
 }
