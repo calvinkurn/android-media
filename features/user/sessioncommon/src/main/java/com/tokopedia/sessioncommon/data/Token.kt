bbe0b141e8ee08a428851ef06aa18c82d9e65948
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-09-14 10:26:04 +0700

    change client id

--- features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
@@ -6,7 +6,7 @@ package com.tokopedia.sessioncommon.data
 open class Token {
 
     companion object {
-        const val GOOGLE_API_KEY = "692092518182-rjgh0bja6q41dllpq2dptn134cmhiv9h.apps.googleusercontent.com"
+        const val GOOGLE_API_KEY = "19489735998-bud26sb04dvpsd6sea4g15r22dt1dp8d.apps.googleusercontent.com"
     }
 
 }
