deec43ccd1667e94d831f96064f0855befe861e4
yorispr <yoris.prayogo@tokopedia.com>
2020-09-14 11:34:19 +0700

    change default value to false

--- features/user/loginphone/src/main/java/com/tokopedia/loginphone/chooseaccount/data/AccountListPojo.kt
+++ features/user/loginphone/src/main/java/com/tokopedia/loginphone/chooseaccount/data/AccountListPojo.kt
@@ -48,7 +48,7 @@ data class UserDetail(
         var msisdn_verified: Boolean = false,
         @SerializedName("challenge_2fa")
         @Expose
-        var challenge_2fa: Boolean = true,
+        var challenge_2fa: Boolean = false,
         @SerializedName("user_id_enc")
         @Expose
         var user_id_enc: String = "",
