59eb2c9ab75d2d9f3449705519ddd4294aa0849b
Rifqi M Fahmi <rifqi.fahmi@tokopedia.com>
2020-09-15 16:58:10 +0700

    update label seller migration

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/view/adapter/viewholder/ChatSettingTitleViewHolder.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatsetting/view/adapter/viewholder/ChatSettingTitleViewHolder.kt
@@ -3,8 +3,11 @@ package com.tokopedia.topchat.chatsetting.view.adapter.viewholder
 import android.view.View
 import android.widget.ImageView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.config.GlobalConfig
+import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatsetting.data.uimodel.ChatSettingTitleUiModel
+import com.tokopedia.unifycomponents.Label
 import com.tokopedia.unifyprinciples.Typography
 
 class ChatSettingTitleViewHolder(
@@ -13,10 +16,12 @@ class ChatSettingTitleViewHolder(
 
     private val icon: ImageView? = itemView?.findViewById(R.id.iv_icon)
     private val title: Typography? = itemView?.findViewById(R.id.tv_title)
+    private val label: Label? = itemView?.findViewById(R.id.label_chat_setting)
 
     override fun bind(element: ChatSettingTitleUiModel) {
         bindIcon(element)
         bindTitle(element)
+        initLabel(element)
     }
 
     private fun bindIcon(element: ChatSettingTitleUiModel) {
@@ -25,6 +30,14 @@ class ChatSettingTitleViewHolder(
 
     private fun bindTitle(element: ChatSettingTitleUiModel) {
         title?.setText(element.title)
+    }
+
+    private fun initLabel(element: ChatSettingTitleUiModel) {
+        label?.showWithCondition(!GlobalConfig.isSellerApp() && isTitleSeller(element.title))
+    }
+
+    private fun isTitleSeller(title: Int): Boolean {
+        return title == R.string.title_topchat_as_seller
     }
 
     companion object {
