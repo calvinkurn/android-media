40ccdcfcbd1ddeea4cf5db72df5e076a0f6d72d0
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-16 11:33:15 +0700

    add fun bindDeals to bind data from travelCollectiveBanner, add onItemClickPromo (click deals promo)

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
@@ -2,6 +2,7 @@ package com.tokopedia.promocheckout.list.view.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
 import com.tokopedia.promocheckout.list.model.listlastseen.PromoCheckoutLastSeenModel
 import kotlinx.android.synthetic.main.item_promo_last_seen.view.*
 
@@ -18,7 +19,24 @@ class PromoLastSeenViewHolder(val view : View, private val listenerLastSeen: Lis
         }
     }
 
+    fun bindDeals(data: TravelCollectiveBanner.Banner) {
+        view.setOnClickListener { listenerLastSeen.onClickItemPromo(data) }
+
+        if (data.attributes.promoCode.isNotEmpty()) {
+            view.voucherCode.text = data.attributes.promoCode
+            view.descPromo.text = data.attributes.description.toUpperCase()
+        }
+
+
+        if (data.attributes.promoCode.isEmpty()) {
+            view.layoutPromoLastSeen.visibility = View.GONE
+        } else {
+            view.layoutPromoLastSeen.visibility = View.VISIBLE
+        }
+    }
+
     interface ListenerLastSeen{
         fun onClickItemLastSeen(promoCheckoutLastSeenModel: PromoCheckoutLastSeenModel)
+        fun onClickItemPromo(promoCheckoutDealsPromoCode: TravelCollectiveBanner.Banner)
     }
 }
