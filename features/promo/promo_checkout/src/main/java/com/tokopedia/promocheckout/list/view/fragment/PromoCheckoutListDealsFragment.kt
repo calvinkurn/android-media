a5dcc51cb43f0a0fe9abacbb294e459695d0315b
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-16 09:18:25 +0700

    add key EXTRA_CATEGORY_ID & EXTRA_PRODUCTID, get intent categoryID, load data promo deals, add adapter

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
@@ -1,10 +1,11 @@
 package com.tokopedia.promocheckout.list.view.fragment
 
 import android.os.Bundle
-import android.view.View
 import com.tokopedia.abstraction.constant.IRouterConstant
+import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
 import com.tokopedia.promocheckout.list.di.PromoCheckoutListComponent
+import com.tokopedia.promocheckout.list.view.adapter.PromoDealsAdapter
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListContract
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListDealsPresenter
 import javax.inject.Inject
@@ -18,16 +19,23 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
 
     @Inject
     lateinit var promoCheckoutListDealsPresenter: PromoCheckoutListDealsPresenter
+    private val promoDealsAdapter: PromoDealsAdapter by lazy { PromoDealsAdapter(arrayListOf(), this) }
 
-    override var serviceId: String = IRouterConstant.LoyaltyModule.ExtraLoyaltyActivity.DIGITAL_STRING
+    override var serviceId: String = IRouterConstant.LoyaltyModule.ExtraLoyaltyActivity.DEALS_STRING
+    var categoryID: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        categoryID = arguments?.getInt(EXTRA_CATEGORY_ID, 1) ?: 1
         promoCheckoutListDealsPresenter.attachView(this)
     }
 
     override fun onPromoCodeUse(promoCode: String) {
-        if (promoCode.isNotEmpty()) promoCheckoutListDealsPresenter.dealsCheckoutMapper
+        //
+    }
+
+    override fun onClickItemPromo(promoCheckoutDealsPromoCode: TravelCollectiveBanner.Banner) {
+        //
     }
 
 
@@ -42,26 +50,35 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
 
     override fun loadData(page: Int) {
         if (isCouponActive) {
-            promoCheckoutListPresenter.getListPromo(serviceId, categoryId, page, resources)
+            promoCheckoutListPresenter.getListPromo(serviceId, categoryID, page, resources)
         }
+        promoCheckoutListPresenter.getListTravelCollectiveBanner(resources)
     }
 
     companion object {
 
-        val EXTRA_CART_ID = "EXTRA_CART_ID"
-        val EXTRA_CATEGORY_ID = "EXTRA_CATEGORY_ID"
+        val EXTRA_CATEGORY_ID = "EXTRA_CATEGORYID"
+        val EXTRA_PRODUCTID = "EXTRA_PRODUCTID"
 
-        fun createInstance(isCouponActive: Boolean?, promoCode: String?, cartID: String?, pageTracking: Int?): PromoCheckoutListHotelFragment {
-            val promoCheckoutListMarketplaceFragment = PromoCheckoutListHotelFragment()
+        fun createInstance(isCouponActive: Boolean?, promoCode: String?, categoryId: Int?, pageTracking: Int?, productId: String?): PromoCheckoutListDealsFragment {
+            val promoCheckoutListMarketplaceFragment = PromoCheckoutListDealsFragment()
             val bundle = Bundle()
             bundle.putBoolean(EXTRA_COUPON_ACTIVE, isCouponActive ?: true)
             bundle.putString(EXTRA_PROMO_CODE, promoCode ?: "")
-            bundle.putString(PromoCheckoutListHotelFragment.EXTRA_CART_ID, cartID ?: "")
+            bundle.putInt(EXTRA_CATEGORY_ID, categoryId ?: 1)
             bundle.putInt(PAGE_TRACKING, pageTracking ?: 1)
+            bundle.putString(EXTRA_PRODUCTID, productId ?: "")
             promoCheckoutListMarketplaceFragment.arguments = bundle
             return promoCheckoutListMarketplaceFragment
         }
 
+    }
+
+    override fun renderDealsPromo(data: List<TravelCollectiveBanner.Banner>) {
+        promoDealsAdapter.listData.clear()
+        promoDealsAdapter.listData.addAll(data)
+        promoDealsAdapter.notifyDataSetChanged()
+        populateDealsPromo(data)
     }
 
     override fun onSuccessCheckPromo(data: DataUiModel) {
