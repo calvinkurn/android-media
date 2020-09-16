fc9d6ba0672a3a3bd8fadcb06470905ecb4137be
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-16 09:16:00 +0700

    add getListTravelCollectiveBanner at interface

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListContract.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListContract.kt
@@ -3,6 +3,7 @@ package com.tokopedia.promocheckout.list.view.presenter
 import android.content.res.Resources
 import com.tokopedia.abstraction.base.view.listener.BaseListViewListener
 import com.tokopedia.abstraction.base.view.presenter.CustomerPresenter
+import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
 import com.tokopedia.promocheckout.list.model.listcoupon.PromoCheckoutListModel
 import com.tokopedia.promocheckout.list.model.listlastseen.PromoCheckoutLastSeenModel
@@ -15,6 +16,7 @@ interface PromoCheckoutListContract {
         fun showProgressLoading()
         fun hideProgressLoading()
         fun renderListLastSeen(data: List<PromoCheckoutLastSeenModel>)
+        fun renderDealsPromo(data: List<TravelCollectiveBanner.Banner>)
         fun showGetListLastSeenError(e: Throwable)
         fun onSuccessCheckPromo(data: DataUiModel)
         fun onErrorCheckPromo(e: Throwable)
@@ -24,5 +26,6 @@ interface PromoCheckoutListContract {
     interface Presenter : CustomerPresenter<View> {
         fun getListPromo(serviceId: String, categoryId: Int, page: Int, resources: Resources)
         fun getListLastSeen(categoryIDs: List<Int>, resources: Resources)
+        fun getListTravelCollectiveBanner(resources: Resources)
     }
 }
