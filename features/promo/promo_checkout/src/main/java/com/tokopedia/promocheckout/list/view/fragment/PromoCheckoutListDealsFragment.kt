775fb180e542b9d88040ae030f14eca17c5fa3da
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-17 09:04:58 +0700

    get checkout data, handle onClickItemPromo, handle onPromocodeUse

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
@@ -1,12 +1,16 @@
 package com.tokopedia.promocheckout.list.view.fragment
 
 import android.os.Bundle
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.tokopedia.abstraction.constant.IRouterConstant
 import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
 import com.tokopedia.promocheckout.list.di.PromoCheckoutListComponent
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListContract
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListDealsPresenter
+import kotlinx.android.synthetic.main.fragment_promo_checkout_list.*
 import javax.inject.Inject
 
 
@@ -21,19 +25,26 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
 
     override var serviceId: String = IRouterConstant.LoyaltyModule.ExtraLoyaltyActivity.DEALS_STRING
     var categoryID: Int = 1
+    var checkoutData: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         categoryID = arguments?.getInt(EXTRA_CATEGORY_ID, 1) ?: 1
+        checkoutData = arguments?.getString(EXTRA_CHECKOUT_DATA) ?: ""
         promoCheckoutListDealsPresenter.attachView(this)
     }
 
     override fun onPromoCodeUse(promoCode: String) {
-        //
+        var requestBody: JsonObject? = null
+        if (checkoutData.isNotBlank() || checkoutData.length > 0) {
+            val jsonElement: JsonElement = JsonParser().parse(checkoutData)
+            requestBody = jsonElement.asJsonObject
+            promoCheckoutListDealsPresenter.processCheckDealPromoCode(promoCode, requestBody, false)
+        }
     }
 
     override fun onClickItemPromo(promoCheckoutDealsPromoCode: TravelCollectiveBanner.Banner) {
-        //
+        textInputCoupon.setText(promoCheckoutDealsPromoCode.attributes.promoCode)
     }
 
 
@@ -57,8 +68,9 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
 
         val EXTRA_CATEGORY_ID = "EXTRA_CATEGORYID"
         val EXTRA_PRODUCTID = "EXTRA_PRODUCTID"
+        val EXTRA_CHECKOUT_DATA = "checkoutdata"
 
-        fun createInstance(isCouponActive: Boolean?, promoCode: String?, categoryId: Int?, pageTracking: Int?, productId: String?): PromoCheckoutListDealsFragment {
+        fun createInstance(isCouponActive: Boolean?, promoCode: String?, categoryId: Int?, pageTracking: Int?, productId: String?, checkoutData: String?): PromoCheckoutListDealsFragment {
             val promoCheckoutListMarketplaceFragment = PromoCheckoutListDealsFragment()
             val bundle = Bundle()
             bundle.putBoolean(EXTRA_COUPON_ACTIVE, isCouponActive ?: true)
@@ -66,6 +78,7 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
             bundle.putInt(EXTRA_CATEGORY_ID, categoryId ?: 1)
             bundle.putInt(PAGE_TRACKING, pageTracking ?: 1)
             bundle.putString(EXTRA_PRODUCTID, productId ?: "")
+            bundle.putString(EXTRA_CHECKOUT_DATA, checkoutData ?: "")
             promoCheckoutListMarketplaceFragment.arguments = bundle
             return promoCheckoutListMarketplaceFragment
         }
