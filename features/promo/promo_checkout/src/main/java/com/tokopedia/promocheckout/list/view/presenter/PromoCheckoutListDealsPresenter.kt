e5afbfe38e331dcf8f9d42718a2f4592b3f12423
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-17 09:06:18 +0700

    remove variables constructor, handle processCheckDealPromoCode

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsPresenter.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsPresenter.kt
@@ -1,41 +1,18 @@
 package com.tokopedia.promocheckout.list.view.presenter
 
+import com.google.gson.JsonObject
 import com.tokopedia.abstraction.base.view.presenter.BaseDaggerPresenter
-import com.tokopedia.graphql.data.model.GraphqlResponse
-import com.tokopedia.promocheckout.common.domain.deals.PromoCheckoutListDealsUseCase
-import com.tokopedia.promocheckout.common.domain.deals.Type
-import com.tokopedia.promocheckout.common.domain.mapper.DealsCheckoutMapper
-import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
-import rx.Subscriber
+import com.tokopedia.usecase.RequestParams
 
-class PromoCheckoutListDealsPresenter (private val promoCheckoutListDealsUseCase: PromoCheckoutListDealsUseCase,  val dealsCheckoutMapper: DealsCheckoutMapper) : BaseDaggerPresenter<PromoCheckoutListContract.View>(), DealsCheckoutListContract.Presenter {
 
-    override fun getType(product: Type) {
+class PromoCheckoutListDealsPresenter : BaseDaggerPresenter<PromoCheckoutListContract.View>(), PromoCheckoutListDealsContract.Presenter {
+
+    override fun processCheckDealPromoCode(voucherId: String?, requestBody: JsonObject?, flag: Boolean) {
         view.showProgressLoading()
-        promoCheckoutListDealsUseCase.execute(promoCheckoutListDealsUseCase.createRequestParams(product), object : Subscriber<GraphqlResponse>() {
-            override fun onNext(objects: GraphqlResponse?) {
-                view.hideProgressLoading()
-                val errors = objects?.getError(TravelCollectiveBanner.Response::class.java)
-                if (!errors.isNullOrEmpty()) {
-                    val rawErrorMessage = errors[0].message
-                } else {
-                    val dealsData = objects?.getData<TravelCollectiveBanner.Response>(TravelCollectiveBanner.Response::class.java)?.response
-                }
-            }
+        requestBody!!.addProperty("promocode", voucherId)
+        val requestParams = RequestParams.create()
+        requestParams.putObject("checkoutdata", requestBody)
+        requestParams.putBoolean("ispromocodecase", flag)
 
-            override fun onCompleted() {
-                //do nothing
-            }
-
-            override fun onError(e: Throwable?) {
-                if (isViewAttached) {
-                    view.hideProgressLoading()
-                    view.onErrorCheckPromo(e!!)
-                }
-            }
-
-        })
     }
-
-
 }
