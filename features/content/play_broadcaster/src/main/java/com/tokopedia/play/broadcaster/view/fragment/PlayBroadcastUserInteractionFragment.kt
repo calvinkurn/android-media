ae372d568cb574b1076ee9859b7d315d43637455
kenny <kenny.hadisaputra@tokopedia.com>
2020-09-17 17:28:39 +0700

    ChatListPartialView.kt to ChatListViewComponent.kt

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/fragment/PlayBroadcastUserInteractionFragment.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/fragment/PlayBroadcastUserInteractionFragment.kt
@@ -29,7 +29,7 @@ import com.tokopedia.play.broadcaster.view.custom.PlayStatInfoView
 import com.tokopedia.play.broadcaster.view.custom.PlayTimerCountDown
 import com.tokopedia.play.broadcaster.view.custom.PlayTimerView
 import com.tokopedia.play.broadcaster.view.fragment.base.PlayBaseBroadcastFragment
-import com.tokopedia.play.broadcaster.view.partial.ChatListPartialView
+import com.tokopedia.play.broadcaster.view.partial.ChatListViewComponent
 import com.tokopedia.play.broadcaster.view.state.BroadcastState
 import com.tokopedia.play.broadcaster.view.state.BroadcastTimerState
 import com.tokopedia.play.broadcaster.view.viewmodel.PlayBroadcastViewModel
@@ -38,6 +38,7 @@ import com.tokopedia.play_common.util.event.EventObserver
 import com.tokopedia.play_common.view.doOnApplyWindowInsets
 import com.tokopedia.play_common.view.requestApplyInsetsWhenAttached
 import com.tokopedia.play_common.view.updateMargins
+import com.tokopedia.play_common.viewcomponent.viewComponent
 import com.tokopedia.unifycomponents.Toaster
 import javax.inject.Inject
 
@@ -59,7 +60,8 @@ class PlayBroadcastUserInteractionFragment @Inject constructor(
     private lateinit var countdownTimer: PlayTimerCountDown
     private lateinit var loadingView: FrameLayout
 
-    private lateinit var chatListView: ChatListPartialView
+    private val chatListView by viewComponent { ChatListViewComponent(it) }
+
     private lateinit var productLiveBottomSheet: PlayProductLiveBottomSheet
 
     private lateinit var exitDialog: DialogUnify
@@ -111,8 +113,6 @@ class PlayBroadcastUserInteractionFragment @Inject constructor(
         pmvMetrics = view.findViewById(R.id.pmv_metrics)
         countdownTimer = view.findViewById(R.id.countdown_timer)
         loadingView = view.findViewById(R.id.loading_view)
-
-        chatListView = ChatListPartialView(view as ViewGroup)
     }
 
     private fun setupView() {
