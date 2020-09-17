ac6d5ef3bdeafba6623d52e28f4446635431c937
isfaaghyth <isfaaghyth@gmail.com>
2020-09-17 22:43:31 +0700

    created AdaptiveImage for both of abstraction and utils module

---
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/utils/image/AdaptiveImage.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.utils.image
+
+import android.content.Context
+import com.bumptech.glide.load.model.GlideUrl
+import com.tokopedia.utils.network.NetworkManager
+
+object AdaptiveImage {
+
+    fun glideUrl(context: Context, url: String): GlideUrl {
+        // adaptive network state
+        val networkState = NetworkManager.state(context)
+
+        // get configuration image quality setting
+        val settings = MediaQualityCacheManager(context)
+
+        val connectionType = when(settings.indexQuality()) {
+            0 -> networkState
+            1 -> "2g"
+            2 -> "4g"
+            else -> networkState
+        }
+
+        return GlideUrl(url) {
+            mapOf(Pair("ETC", connectionType))
+        }
+    }
+
+}
---
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/common/utils/AdaptiveImage.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.abstraction.common.utils
+
+import android.content.Context
+import com.bumptech.glide.load.model.GlideUrl
+
+object AdaptiveImage {
+
+    fun glideUrl(context: Context, url: String): GlideUrl {
+        // adaptive network state
+        val networkState = NetworkManager.state(context)
+
+        // get configuration image quality setting
+        val settings = MediaQualityCacheManager(context)
+
+        val connectionType = when(settings.indexQuality()) {
+            0 -> networkState
+            1 -> "2g"
+            2 -> "4g"
+            else -> networkState
+        }
+
+        return GlideUrl(url) {
+            mapOf(Pair("ETC", connectionType))
+        }
+    }
+
+}
