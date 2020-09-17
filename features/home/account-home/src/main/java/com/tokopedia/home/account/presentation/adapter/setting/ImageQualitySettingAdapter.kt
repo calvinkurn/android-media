33fdf8255edec0181bc14e6a022ea4312d266dfe
isfaaghyth <isfaaghyth@gmail.com>
2020-09-17 09:56:08 +0700

    simplified of adapter of imageQualitySetting

--- features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/adapter/setting/ImageQualitySettingAdapter.kt
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/adapter/setting/ImageQualitySettingAdapter.kt
@@ -1,16 +1,15 @@
 package com.tokopedia.home.account.presentation.adapter.setting
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.home.account.R
 import com.tokopedia.home.account.presentation.listener.ImageQualitySettingListener
+import com.tokopedia.home.account.presentation.uimodel.MediaQualityUIModel
 import com.tokopedia.home.account.presentation.viewholder.ImageQualitySettingViewHolder
-import com.tokopedia.home.account.presentation.viewmodel.ImageQualitySettingItemViewModel
 
-class ImageQualitySettingAdapter(private val listener: ImageQualitySettingListener): RecyclerView.Adapter<ImageQualitySettingViewHolder>() {
-
-    var itemList = listOf<ImageQualitySettingItemViewModel>()
+class ImageQualitySettingAdapter(
+        private val itemList: List<MediaQualityUIModel>,
+        private val listener: ImageQualitySettingListener
+): RecyclerView.Adapter<ImageQualitySettingViewHolder>() {
 
     var previousPosition = 0
         set(value) {
@@ -18,16 +17,18 @@ class ImageQualitySettingAdapter(private val listener: ImageQualitySettingListen
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageQualitySettingViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image_quality_option, parent, false)
-        return ImageQualitySettingViewHolder(view)
+    override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+    ): ImageQualitySettingViewHolder {
+        return ImageQualitySettingViewHolder.viewHolder(parent)
     }
 
     override fun getItemCount(): Int = itemList.size
 
     override fun onBindViewHolder(holder: ImageQualitySettingViewHolder, position: Int) {
         val item = itemList[position]
-        holder.bindData(item, position, previousPosition)
+        holder.bind(item, position, previousPosition)
         holder.itemView.setOnClickListener {
             if(previousPosition != position) {
                 previousPosition = position
@@ -36,4 +37,5 @@ class ImageQualitySettingAdapter(private val listener: ImageQualitySettingListen
             }
         }
     }
+
 }
