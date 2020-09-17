a7b048dff6d9606c3ebf04945647eac39423d4fb
isfaaghyth <isfaaghyth@gmail.com>
2020-09-17 09:54:18 +0700

    created MediaQualityCacheManager for handling setting state

---
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/presentation/util/MediaQualityCacheManager.kt
@@ -0,0 +1,30 @@
+package com.tokopedia.home.account.presentation.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+class MediaQualityCacheManager @Inject constructor(val context: Context) {
+
+    private fun pref(): SharedPreferences {
+        return context.getSharedPreferences(MEDIA_QUALITY_PREF, Context.MODE_PRIVATE)
+    }
+
+    fun setQualitySettings(value: Int) {
+        pref().edit().putInt(KEY_QUALITY_SETTING, value).apply()
+    }
+
+    fun indexQuality(): Int {
+        return if (isExist()) pref().getInt(KEY_QUALITY_SETTING, 0) else 0
+    }
+
+    private fun isExist(): Boolean {
+        return pref().contains(KEY_QUALITY_SETTING)
+    }
+
+    companion object {
+        private const val MEDIA_QUALITY_PREF = "media_image_quality"
+        private const val KEY_QUALITY_SETTING = "index_image_quality_setting"
+    }
+
+}
