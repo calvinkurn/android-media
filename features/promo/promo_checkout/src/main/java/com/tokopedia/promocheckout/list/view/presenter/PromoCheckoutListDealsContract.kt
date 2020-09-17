13f7bea2252b07c56718646486031bbf4640810a
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-17 09:03:41 +0700

    add fun processCheckDealPromocode

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsContract.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsContract.kt
@@ -1,9 +1,9 @@
 package com.tokopedia.promocheckout.list.view.presenter
 
+import com.google.gson.JsonObject
 import com.tokopedia.abstraction.base.view.presenter.CustomerPresenter
-import com.tokopedia.promocheckout.common.domain.deals.Type
 
-interface DealsCheckoutListContract {
+interface PromoCheckoutListDealsContract {
 
     interface Presenter : CustomerPresenter<PromoCheckoutListContract.View> {
         fun getType(product: Type)
