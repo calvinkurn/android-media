67177a1762b169d2ab40bb6c1be19370884700e1
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-18 08:58:37 +0700

    implement onSuccessCheckPromo, add navigate to detail

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/fragment/PromoCheckoutListDealsFragment.kt
@@ -1,13 +1,21 @@
 package com.tokopedia.promocheckout.list.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.tokopedia.abstraction.constant.IRouterConstant
+import com.tokopedia.promocheckout.common.data.REQUEST_CODE_PROMO_DETAIL
 import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
+import com.tokopedia.promocheckout.common.domain.model.deals.DealsVerifyBody
+import com.tokopedia.promocheckout.common.util.EXTRA_PROMO_DATA
+import com.tokopedia.promocheckout.common.util.mapToStatePromoCheckout
+import com.tokopedia.promocheckout.common.view.model.PromoData
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
+import com.tokopedia.promocheckout.detail.view.activity.PromoCheckoutDetailDealsActivity
 import com.tokopedia.promocheckout.list.di.PromoCheckoutListComponent
+import com.tokopedia.promocheckout.list.model.listcoupon.PromoCheckoutListModel
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListContract
 import com.tokopedia.promocheckout.list.view.presenter.PromoCheckoutListDealsPresenter
 import kotlinx.android.synthetic.main.fragment_promo_checkout_list.*
@@ -26,6 +34,7 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
     override var serviceId: String = IRouterConstant.LoyaltyModule.ExtraLoyaltyActivity.DEALS_STRING
     var categoryID: Int = 1
     var checkoutData: String = ""
+    var dealsVerify = DealsVerifyBody()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +48,8 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
         if (checkoutData.isNotBlank() || checkoutData.length > 0) {
             val jsonElement: JsonElement = JsonParser().parse(checkoutData)
             requestBody = jsonElement.asJsonObject
-            promoCheckoutListDealsPresenter.processCheckDealPromoCode(promoCode, requestBody, false)
+            dealsVerify.promocode = promoCode
+            promoCheckoutListDealsPresenter.processCheckDealPromoCode(promoCode, false, requestBody)
         }
     }
 
@@ -47,6 +57,31 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
         textInputCoupon.setText(promoCheckoutDealsPromoCode.attributes.promoCode)
     }
 
+    override fun onItemClicked(promoCheckoutListModel: PromoCheckoutListModel?) {
+        super.onItemClicked(promoCheckoutListModel)
+        navigateToPromoDetail(promoCheckoutListModel)
+    }
+
+    open fun navigateToPromoDetail(promoCheckoutListModel: PromoCheckoutListModel?) {
+        var requestBody: JsonObject? = null
+        val jsonElement: JsonElement = JsonParser().parse(checkoutData).asJsonObject
+        requestBody = jsonElement.asJsonObject
+        startActivityForResult(PromoCheckoutDetailDealsActivity.newInstance(
+                activity, promoCheckoutListModel?.code
+                ?: "", false, requestBody), REQUEST_CODE_PROMO_DETAIL)
+    }
+
+    override fun onSuccessCheckPromo(data: DataUiModel) {
+        val intent = Intent()
+        val promoData = PromoData(PromoData.VOUCHER_RESULT_CODE, data.codes[0],
+                data.message.text, data.titleDescription, state = data.message.state.mapToStatePromoCheckout())
+        intent.putExtra(EXTRA_PROMO_DATA, promoData)
+        intent.putExtra(VOUCHER_CODE, data.codes[0])
+        intent.putExtra(VOUCHER_MESSAGE, data.titleDescription)
+        intent.putExtra(VOUCHER_AMOUNT, data.discountAmount)
+        activity?.setResult(PromoData.VOUCHER_RESULT_CODE, intent)
+        activity?.finish()
+    }
 
     override fun initInjector() {
         getComponent(PromoCheckoutListComponent::class.java).inject(this)
@@ -69,6 +104,9 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
         val EXTRA_CATEGORY_ID = "EXTRA_CATEGORYID"
         val EXTRA_PRODUCTID = "EXTRA_PRODUCTID"
         val EXTRA_CHECKOUT_DATA = "checkoutdata"
+        val VOUCHER_CODE = "voucher_code"
+        val VOUCHER_MESSAGE = "voucher_message"
+        val VOUCHER_AMOUNT = "voucher_amount"
 
         fun createInstance(isCouponActive: Boolean?, promoCode: String?, categoryId: Int?, pageTracking: Int?, productId: String?, checkoutData: String?): PromoCheckoutListDealsFragment {
             val promoCheckoutListMarketplaceFragment = PromoCheckoutListDealsFragment()
@@ -82,10 +120,5 @@ class PromoCheckoutListDealsFragment() : BasePromoCheckoutListFragment(), PromoC
             promoCheckoutListMarketplaceFragment.arguments = bundle
             return promoCheckoutListMarketplaceFragment
         }
-
-    }
-
-    override fun onSuccessCheckPromo(data: DataUiModel) {
-        //
     }
 }
