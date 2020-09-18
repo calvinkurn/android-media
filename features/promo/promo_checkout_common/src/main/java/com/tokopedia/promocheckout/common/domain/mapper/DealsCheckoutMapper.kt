fafd8b00f89d04836d4e7c5b5dcfde9b43527ad0
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-18 09:04:57 +0700

    edit deals checkout mapper to object, handle mapData

--- features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/domain/mapper/DealsCheckoutMapper.kt
+++ features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/domain/mapper/DealsCheckoutMapper.kt
@@ -1,14 +1,28 @@
 package com.tokopedia.promocheckout.common.domain.mapper
 
 import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
+import com.tokopedia.promocheckout.common.domain.model.deals.DealsVerifyResponse
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
+import com.tokopedia.promocheckout.common.view.uimodel.MessageUiModel
 import javax.inject.Inject
 
-open class DealsCheckoutMapper @Inject constructor(){
+object DealsCheckoutMapper{
 
-    fun mapData(data: TravelCollectiveBanner): DataUiModel {
+    fun mapData(data: DealsVerifyResponse): DataUiModel {
         return DataUiModel(
-                success = true
+                success = true,
+                message = mapMessageDeals(data.data.cart.promocodeSuccessMessage),
+                codes = listOf(data.data.cart.promocode),
+                discountAmount = data.data.cart.promocodeDiscount,
+                cashbackWalletAmount = data.data.cart.promocodeCashback,
+                titleDescription = data.data.cart.promocodeSuccessMessage
+        )
+    }
+
+    private fun mapMessageDeals(message: String): MessageUiModel {
+        return MessageUiModel(
+                text = message,
+                state = "green"
         )
     }
 }
