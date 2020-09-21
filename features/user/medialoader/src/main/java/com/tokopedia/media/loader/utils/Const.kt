7dccc12104a77e27385b56f1534db1863cf29be2
isfaaghyth <isfaaghyth@gmail.com>
2020-09-21 09:08:27 +0700

    created constants variable of loader

---
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/utils/Const.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.media.loader.utils
+
+/*
+* Client hints: ECT
+* The first client hints that will be used for adaptive is Effective Connection Types (ECT).
+* Beside ECT, there are also other hints that we may take to consideration: Downlink and RTT.
+* But for the first phase, ECT is giving us enough information regarding the client’s
+* network capabilities. See this article to read the details.
+* */
+const val HEADERS_ECT = "ECT"
+
+/*
+* determine object classification in every network capability segmentation
+* only supported for 2 segmentation are slow for 2g / 3g, and fast for 4g and wifi.
+* */
+const val LOW_QUALITY = "2g"
+const val HIGH_QUALITY = "4g"
