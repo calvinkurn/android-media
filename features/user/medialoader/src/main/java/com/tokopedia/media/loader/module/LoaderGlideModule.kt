b3b4f2da76c5beb687d16f65533c733703f1a61d
isfaaghyth <isfaaghyth@gmail.com>
2020-09-21 09:10:13 +0700

    created app glide module

---
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/module/LoaderGlideModule.kt
@@ -0,0 +1,35 @@
+package com.tokopedia.media.loader.module
+
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import com.tokopedia.media.loader.BuildConfig
+import com.tokopedia.media.loader.utils.GlideNetworkInterceptor.okHttpInterceptor
+import java.io.InputStream
+
+@GlideModule
+class LoaderGlideModule: AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        if (BuildConfig.DEBUG) {
+            builder.setLogLevel(Log.VERBOSE)
+        }
+    }
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        super.registerComponents(context, glide, registry)
+        registry.replace(
+                GlideUrl::class.java,
+                InputStream::class.java,
+                OkHttpUrlLoader.Factory(okHttpInterceptor())
+        )
+    }
+
+}
