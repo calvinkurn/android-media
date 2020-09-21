7fa806627e44700768af013188a7ea8f60f02fcf
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-21 18:52:11 +0700

    remove override detach

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsPresenter.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/presenter/PromoCheckoutListDealsPresenter.kt
@@ -46,9 +46,4 @@ class PromoCheckoutListDealsPresenter(
                         })
         )
     }
-
-    override fun detachView() {
-        compositeSubscription.unsubscribe()
-        super.detachView()
-    }
 }
