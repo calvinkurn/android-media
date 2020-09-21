c27ee3a1ee7f68b03b3bd43f74c2c3afc0e64103
irfan khoirul <irfankhoirul@gmail.com>
2020-09-21 18:11:32 +0700

    add static image url

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/TradeInInfoBottomsheetHelper.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/TradeInInfoBottomsheetHelper.kt
@@ -7,8 +7,12 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.checkout.R
+import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import kotlinx.android.synthetic.main.layout_bottomsheet_trade_in_info.view.*
+
+const val TRADE_IN_NORMAL_IMAGE_URL = "https://ecs7.tokopedia.net/timi/desc-timi.png"
+const val TRADE_IN_DROP_OFF_IMAGE_URL = "https://ecs7.tokopedia.net/timi/Ditukar%20Di%20Indomaret@3x.png"
 
 fun showTradeInInfoBottomsheet(fragmentManager: FragmentManager, context: Context) {
 
@@ -24,15 +28,19 @@ fun showTradeInInfoBottomsheet(fragmentManager: FragmentManager, context: Contex
         view.tab_trade_in_info.addNewTab(context.getString(R.string.title_trade_in_drop_off_address))
     }
 
+    view.text_trade_in_info.text = MethodChecker.fromHtml(view.resources.getString(R.string.checkout_trade_in_default_address_info_description))
+    view.img_trade_in_info.loadImage(TRADE_IN_NORMAL_IMAGE_URL)
     view.tab_trade_in_info.tabLayout.getTabAt(0)?.select()
 
     view.tab_trade_in_info.getUnifyTabLayout().addOnTabSelectedListener(object : OnTabSelectedListener {
         override fun onTabSelected(tab: TabLayout.Tab) {
             when (tab.position) {
                 0 -> {
+                    view.img_trade_in_info.loadImage(TRADE_IN_NORMAL_IMAGE_URL)
                     view.text_trade_in_info.text = MethodChecker.fromHtml(view.resources.getString(R.string.checkout_trade_in_default_address_info_description))
                 }
                 1 -> {
+                    view.img_trade_in_info.loadImage(TRADE_IN_DROP_OFF_IMAGE_URL)
                     view.text_trade_in_info.text = MethodChecker.fromHtml(view.resources.getString(R.string.checkout_trade_in_drop_off_address_info_description))
                 }
             }
