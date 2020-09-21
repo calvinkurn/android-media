00881ccff259296f5581c2f4c67ad91ce17c9dee
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-09-21 16:21:42 +0700

    fix codes

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/FeedbackPageFragment.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/FeedbackPageFragment.kt
@@ -190,12 +190,12 @@ class FeedbackPageFragment: Fragment() {
                 //no-op
             }
         }
-
+/*
         email.addTextChangedListener(setWrapperWatcher(et_email_wrapper))
         page.addTextChangedListener(setWrapperWatcher(et_affected_page_wrapper))
         journey.addTextChangedListener(setWrapperWatcher(et_str_wrapper))
         actualResult.addTextChangedListener(setWrapperWatcher(et_actual_result_wrapper))
-        expectedResult.addTextChangedListener(setWrapperWatcher(et_expected_result_wrapper))
+        expectedResult.addTextChangedListener(setWrapperWatcher(et_expected_result_wrapper))*/
 
         submitButton.setOnClickListener {
             val emailText= email.text.toString()
