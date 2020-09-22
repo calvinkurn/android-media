f2ceed451b8b5c0d0ff9aec4106e8e394ded8c11
kenny <kenny.hadisaputra@tokopedia.com>
2020-09-22 14:07:10 +0700

    optimize code on DefaultOnLifecycle.kt

--- features/content/play_common/src/main/java/com/tokopedia/play_common/lifecycle/DefaultOnLifecycle.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/lifecycle/DefaultOnLifecycle.kt
@@ -1,13 +1,9 @@
 package com.tokopedia.play_common.lifecycle
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
-
 /**
  * Created by jegul on 21/09/20
  */
-class DefaultOnLifecycle<T: Any> internal constructor(): LifecycleObserver {
+class DefaultOnLifecycle<T: Any> internal constructor() {
 
     internal var mOnCreate: (T) -> Unit = {}
     internal var mOnStart: (T) -> Unit = {}
@@ -16,32 +12,26 @@ class DefaultOnLifecycle<T: Any> internal constructor(): LifecycleObserver {
     internal var mOnStop: (T) -> Unit = {}
     internal var mOnDestroy: (T) -> Unit = {}
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreate(handler: (T) -> Unit) {
         mOnCreate = handler
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart(handler: (T) -> Unit) {
         mOnStart = handler
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume(handler: (T) -> Unit) {
         mOnResume = handler
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause(handler: (T) -> Unit) {
         mOnPause = handler
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStop(handler: (T) -> Unit) {
         mOnStop = handler
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy(handler: (T) -> Unit) {
         mOnDestroy = handler
     }
