54609ba2d2e6043f550961c06c91e36211dc6d11
mzennis <meyta.taliti@tokopedia.com>
2020-09-22 08:05:51 +0700

    prefix 'broadcaster' in every key

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/PlayBroadcastKey.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/PlayBroadcastKey.kt
@@ -17,18 +17,18 @@ const val PLAY_BROADCASTER_TRACE_PAGE = "plt_play_broadcaster_page"
  * Start: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.onCreate] before super onCreate()
  * Stop: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.getConfiguration]
  */
-const val PLAY_BROADCASTER_TRACE_PREPARE_PAGE = "plt_play_page_prepare_page"
+const val PLAY_BROADCASTER_TRACE_PREPARE_PAGE = "plt_play_broadcaster_page_prepare_page"
 
 /**
  * Monitor duration between network requests until receiving responses
  * Start: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.getConfiguration]
  * Stop: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.observeConfiguration]
  */
-const val PLAY_BROADCASTER_TRACE_REQUEST_NETWORK = "plt_play_page_request_network"
+const val PLAY_BROADCASTER_TRACE_REQUEST_NETWORK = "plt_play_broadcaster_page_request_network"
 
 /**
  * Monitor duration between receive response until the UI is rendered
  * Start: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.observeConfiguration]
  * Stop: [com.tokopedia.play.broadcaster.view.activity.PlayBroadcastActivity.observeConfiguration] after rendering
  */
-const val PLAY_BROADCASTER_TRACE_RENDER_PAGE = "plt_play_page_render_page"
+const val PLAY_BROADCASTER_TRACE_RENDER_PAGE = "plt_play_broadcaster_page_render_page"
