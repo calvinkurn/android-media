0802399e60752268ba0e110fb2250c81e8fdce50
isfaaghyth <isfaaghyth@gmail.com>
2020-09-24 00:03:22 +0700

    created extensions of mediaLoader

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/Loader.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/Loader.kt
@@ -1,7 +1,37 @@
 package com.tokopedia.media.loader
 
-class Loader {
+import android.widget.ImageView
+import com.tokopedia.media.loader.UrlBuilder.urlBuilder
+import com.tokopedia.media.loader.data.Properties
 
+fun ImageView.loadImage(url: Any) = call(url, Properties())
 
+fun ImageView.loadImage(url: Any, configuration: Properties.() -> Unit) = call(url, Properties().apply(configuration))
 
+fun ImageView.loadImageRounded(
+        url: Any,
+        rounded: Float,
+        configuration: Properties.() -> Unit = {
+            Properties().apply {
+                roundedRadius = rounded
+            }
+        }) {
+    call(url, Properties().apply(configuration))
+}
+
+private fun ImageView.call(url: Any, configuration: Properties) {
+    builder(if (url is String) urlBuilder(context, url) else url, configuration)
+}
+
+private fun ImageView.builder(url: Any, configuration: Properties) {
+    val imageView = this
+    with(configuration) {
+        MediaGlide(imageView, url)
+                .isRounded(isRounded, roundedRadius)
+                .showPlaceHolder(placeHolder)
+                .cacheStrategy(cacheStrategy)
+                .isAnimate(animate)
+                .showError(error)
+                .build()
+    }
 }
