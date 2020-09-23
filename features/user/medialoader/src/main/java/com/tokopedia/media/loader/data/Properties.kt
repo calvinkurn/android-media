ab2ca46dc99cffd3aec1e3e6a703a7e79b053714
isfaaghyth <isfaaghyth@gmail.com>
2020-09-24 00:33:22 +0700

    convert properties class into data class

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/data/Properties.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/data/Properties.kt
@@ -6,12 +6,12 @@ import com.bumptech.glide.signature.ObjectKey
 import com.tokopedia.media.loader.R
 import com.tokopedia.media.loader.utils.DEFAULT_ROUNDED
 
-open class Properties {
-    var isRounded: Boolean = false
-    var roundedRadius: Float = DEFAULT_ROUNDED
-    var animate: Boolean = false
-    var signature: Key = ObjectKey(System.currentTimeMillis().toString())
-    var error: Any = R.drawable.ic_media_default_error
-    var placeHolder: Any = R.drawable.ic_media_default_placeholder
-    var cacheStrategy: DiskCacheStrategy? = DiskCacheStrategy.DATA
-}
+data class Properties(
+        var isRounded: Boolean = false,
+        var roundedRadius: Float = DEFAULT_ROUNDED,
+        var animate: Boolean = false,
+        var signature: Key = ObjectKey(System.currentTimeMillis().toString()),
+        var error: Any = R.drawable.ic_media_default_error,
+        var placeHolder: Any = R.drawable.ic_media_default_placeholder,
+        var cacheStrategy: DiskCacheStrategy? = DiskCacheStrategy.DATA
+)
