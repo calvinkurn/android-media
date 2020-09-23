d3c240104d4cbf3240ff7ecb78100cbb9e91d8ed
isfaaghyth <isfaaghyth@gmail.com>
2020-09-23 23:51:19 +0700

    created properties of mediaLoader

---
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/data/Properties.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.media.loader.data
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.Key
+import com.bumptech.glide.signature.ObjectKey
+import com.tokopedia.media.loader.R
+import com.tokopedia.media.loader.utils.DEFAULT_ROUNDED
+
+open class Properties {
+    var isRounded: Boolean = false
+    var rounded: Float = DEFAULT_ROUNDED
+    var animate: Boolean = false
+    var signature: Key = ObjectKey(System.currentTimeMillis().toString())
+    var error: Any = R.drawable.ic_media_default_error
+    var placeHolder: Any = R.drawable.ic_media_default_placeholder
+    var cacheStrategy: DiskCacheStrategy? = DiskCacheStrategy.DATA
+}
