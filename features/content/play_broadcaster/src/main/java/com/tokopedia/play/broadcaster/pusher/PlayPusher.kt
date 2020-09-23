d387aaec21a1fbad8f57311977f28a5cdc6e01a6
mzennis <meyta.taliti@tokopedia.com>
2020-09-23 20:29:13 +0700

    refactor

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/pusher/PlayPusher.kt
@@ -0,0 +1,62 @@
+package com.tokopedia.play.broadcaster.pusher
+
+import android.view.SurfaceView
+
+
+/**
+ * Created by mzennis on 22/09/20.
+ */
+interface PlayPusher {
+
+    fun init()
+
+    fun startPreview(surfaceView: SurfaceView)
+
+    fun stopPreview()
+
+    fun startPush(ingestUrl: String)
+
+    fun restartPush()
+
+    fun stopPush()
+
+    fun switchCamera()
+
+    fun resume()
+
+    fun pause()
+
+    fun destroy()
+
+    /**
+     * add live streaming duration
+     */
+    fun addStreamDuration(durationInMillis: Long)
+
+    /**
+     * add maximum live streaming duration
+     */
+    fun addMaxStreamDuration(durationInMillis: Long)
+
+    /**
+     * restart live streaming duration
+     */
+    fun restartStreamDuration(durationInMillis: Long)
+
+    /**
+     * add maximum pause duration when streaming
+     */
+    fun addMaxPauseDuration(durationInMillis: Long)
+
+    fun getTimeElapsed(): String
+
+    /**
+     * add listener to timer
+     */
+    fun addPlayPusherTimerListener(listener: PlayPusherTimerListener)
+
+    /**
+     * add listener to pusher
+     */
+    fun addPlayPusherInfoListener(listener: PlayPusherInfoListener)
+}
