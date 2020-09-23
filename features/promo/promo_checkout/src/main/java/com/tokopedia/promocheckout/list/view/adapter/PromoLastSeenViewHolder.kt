7b3567ae23f60e0362d29749c2afc813bb3eb54c
achmad.abrar <achmad.abrar@tokopedia.com>
2020-09-23 09:31:27 +0700

    remove unused imported

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoLastSeenViewHolder.kt
@@ -2,11 +2,10 @@ package com.tokopedia.promocheckout.list.view.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.promocheckout.common.domain.model.TravelCollectiveBanner
 import com.tokopedia.promocheckout.list.model.listlastseen.PromoCheckoutLastSeenModel
 import kotlinx.android.synthetic.main.item_promo_last_seen.view.*
 
-class PromoLastSeenViewHolder(val view : View, private val listenerLastSeen: ListenerLastSeen) : RecyclerView.ViewHolder(view) {
+class PromoLastSeenViewHolder(val view: View, private val listenerLastSeen: ListenerLastSeen) : RecyclerView.ViewHolder(view) {
     fun bind(data: PromoCheckoutLastSeenModel) {
         view.setOnClickListener { listenerLastSeen.onClickItemLastSeen(data) }
         view.voucherCode.text = data.promoCode
@@ -18,7 +17,7 @@ class PromoLastSeenViewHolder(val view : View, private val listenerLastSeen: Lis
             view.layoutPromoLastSeen.visibility = View.VISIBLE
         }
     }
-    interface ListenerLastSeen{
+    interface ListenerLastSeen {
         fun onClickItemLastSeen(promoCheckoutLastSeenModel: PromoCheckoutLastSeenModel)
     }
 }
