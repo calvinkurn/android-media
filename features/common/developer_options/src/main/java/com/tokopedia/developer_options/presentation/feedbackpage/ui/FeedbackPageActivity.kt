6f4cf2d7e193cda0a33c1d6da393ad3787637a06
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-09-24 08:19:53 +0700

    add dagger

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/ui/FeedbackPageActivity.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/ui/FeedbackPageActivity.kt
@@ -2,10 +2,14 @@ package com.tokopedia.developer_options.presentation.feedbackpage.ui
 
 import android.net.Uri
 import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.developer_options.presentation.feedbackpage.di.DaggerFeedbackPageComponent
+import com.tokopedia.developer_options.presentation.feedbackpage.di.FeedbackPageComponent
 import com.tokopedia.developer_options.presentation.feedbackpage.utils.EXTRA_URI_IMAGE
 
-class FeedbackPageActivity : BaseSimpleActivity() {
+class FeedbackPageActivity : BaseSimpleActivity(), HasComponent<FeedbackPageComponent> {
 
     override fun getNewFragment(): Fragment? {
         var uriData: Uri?
@@ -16,6 +20,12 @@ class FeedbackPageActivity : BaseSimpleActivity() {
             null
         }
         return FeedbackPageFragment.newInstance(uriData)
+    }
+
+    override fun getComponent(): FeedbackPageComponent {
+        return DaggerFeedbackPageComponent.builder()
+                .baseAppComponent((application as BaseMainApplication).baseAppComponent)
+                .build()
     }
 
 }
