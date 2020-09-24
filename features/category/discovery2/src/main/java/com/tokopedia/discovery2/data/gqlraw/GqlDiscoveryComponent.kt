2bee44bfc88a4d81a18090026edabd146625b436
Zishanr <zishanr027@gmail.com>
2020-09-24 21:36:17 +0530

    Working on Category ID integration in best seller

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/data/gqlraw/GqlDiscoveryComponent.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/data/gqlraw/GqlDiscoveryComponent.kt
@@ -1,7 +1,7 @@
 package com.tokopedia.discovery2.data.gqlraw
 
-const val GQL_COMPONENT: String = """query ComponentInfo(${'$'}device: String!, ${'$'}component_id: String!, ${'$'}identifier: String!) {
-  componentInfo(device: ${'$'}device, component_id: ${'$'}component_id, identifier: ${'$'}identifier) {
+const val GQL_COMPONENT: String = """query ComponentInfo(${'$'}device: String!, ${'$'}component_id: String!, ${'$'}identifier: String!, ${'$'}filters: String) {
+  componentInfo(device: ${'$'}device, component_id: ${'$'}component_id, identifier: ${'$'}identifier, filters: ${'$'}filters) {
   data
   }
 }
