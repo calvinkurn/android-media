145ffad7a5c8d11867c2e78540514692dca77663
Felicia Widjaja <fwidjaja@live.com>
2020-09-24 17:14:55 +0700

    remove unused codes

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/listener/FramePerformanceIndexInterface.java
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.buyerorder.unifiedhistory.list.view.listener;
-
-import com.tokopedia.analytics.performance.fpi.FragmentFramePerformanceIndexMonitoring;
-
-public interface FramePerformanceIndexInterface {
-    FragmentFramePerformanceIndexMonitoring getFramePerformanceIndexData();
-}
