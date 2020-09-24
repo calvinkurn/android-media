cb281bfd27883e79f86168b2e376b4329e4245fd
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-09-24 14:18:43 +0700

    Instrument Checkout + Mock

--- features/entertainment/event/src/androidTest/java/CheckoutEventActivityTest.kt
+++ features/entertainment/event/src/androidTest/java/CheckoutEventActivityTest.kt
@@ -1,5 +1,6 @@
 import android.content.Intent
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -14,7 +15,9 @@ import com.tokopedia.entertainment.R
 import com.tokopedia.entertainment.pdp.activity.EventCheckoutActivity
 import com.tokopedia.entertainment.pdp.adapter.viewholder.EventPDPTextFieldViewHolder
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
+import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import data.MockMetaData
+import mock.CheckoutEventMockResponse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -30,29 +33,48 @@ class CheckoutEventActivityTest {
                 override fun getActivityIntent(): Intent {
                     val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
                     val intent = Intent(targetContext, EventCheckoutActivity::class.java).apply {
-                        putExtra(EventCheckoutActivity.EXTRA_URL_PDP, "doolive-ala-carte-booking-36116")
+                        putExtra(EventCheckoutActivity.EXTRA_URL_PDP, "7-day-jr-all-shikoku-pass-23233")
                         putExtra(EventCheckoutActivity.EXTRA_META_DATA, MockMetaData.getMetaDataResponse())
-                        putExtra(EventCheckoutActivity.EXTRA_PACKAGE_ID, "2300")
+                        putExtra(EventCheckoutActivity.EXTRA_PACKAGE_ID, "2104")
                     }
                     return intent
+                }
+
+                override fun beforeActivityLaunched() {
+                    super.beforeActivityLaunched()
+                    gtmLogDBSource.deleteAll().subscribe()
+                    setupGraphqlMockResponse(CheckoutEventMockResponse())
+
                 }
             }
 
     @Before
     fun setup() {
-        gtmLogDBSource.deleteAll().subscribe()
         InstrumentationAuthHelper.loginInstrumentationTestUser1()
-
     }
 
     @Test
     fun validateCheckoutEvent() {
         Thread.sleep(5000)
-        onView(withId(R.id.widget_event_checkout_pessangers)).perform(click())
+        onView(withId(R.id.tg_event_checkout_widget_pessanger_name)).perform(click())
         Thread.sleep(3000)
         onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<EventPDPTextFieldViewHolder>(0, typeText("Firmanda Mulyawan Nugroho")))
         Thread.sleep(3000)
         onView(withId(R.id.simpanBtn)).perform(click())
+        Thread.sleep(3000)
+        onView(withId(R.id.rv_event_checkout_additional)).perform(RecyclerViewActions.actionOnItemAtPosition<EventPDPTextFieldViewHolder>(0, click()))
+        Thread.sleep(9000)
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<EventPDPTextFieldViewHolder>(0, typeText("Firmanda Mulyawan Nugroho")))
+        Thread.sleep(3000)
+        onView(withId(R.id.simpanBtn)).perform(click())
+        Thread.sleep(3000)
+        onView(withId(R.id.item_checkout_event_data_tambahan_package)).perform(click())
+        Thread.sleep(9000)
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<EventPDPTextFieldViewHolder>(0, typeText("085327499272")))
+        Thread.sleep(3000)
+        onView(withId(R.id.simpanBtn)).perform(click())
+        Thread.sleep(3000)
+        onView(withId(android.R.id.content)).perform(ViewActions.swipeUp())
         Thread.sleep(3000)
         onView(withId(R.id.cb_event_checkout)).perform(click())
         Thread.sleep(2000)
--- features/entertainment/event/src/androidTest/java/mock/CheckoutEventMockResponse.kt
+++ features/entertainment/event/src/androidTest/java/mock/CheckoutEventMockResponse.kt
@@ -5,23 +5,18 @@ import com.tokopedia.entertainment.R
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
 import com.tokopedia.test.application.util.InstrumentationMockHelper.getRawString
 
-class PDPEventMockResponse : MockModelConfig(){
+class CheckoutEventMockResponse : MockModelConfig(){
 
     companion object{
         const val KEY_QUERY_PDP_V3 = "event_product_detail_v3"
-        const val KEY_TRAVEL_HOLIDAY = "TravelGetHoliday"
     }
 
     override fun createMockModel(context: Context): MockModelConfig {
         addMockResponse(
                 KEY_QUERY_PDP_V3,
-                getRawString(context, R.raw.event_pdp),
+                getRawString(context, R.raw.event_checkout),
                 FIND_BY_CONTAINS)
 
-        addMockResponse(
-                KEY_TRAVEL_HOLIDAY,
-                getRawString(context, R.raw.event_travel_holiday),
-                FIND_BY_CONTAINS)
         return this
     }
 }
