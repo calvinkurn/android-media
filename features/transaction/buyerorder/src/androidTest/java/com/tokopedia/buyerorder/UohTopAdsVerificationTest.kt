3ce991a41710b58375bfdf245cafd694e9e3b7fa
Felicia Widjaja <fwidjaja@live.com>
2020-09-24 16:57:33 +0700

    add clickProductRecommItem

--- features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohTopAdsVerificationTest.kt
+++ features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohTopAdsVerificationTest.kt
@@ -1,32 +1,33 @@
 package com.tokopedia.buyerorder
 
 import android.Manifest
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
 import com.tokopedia.buyerorder.unifiedhistory.list.view.activity.UohListActivity
 import com.tokopedia.buyerorder.unifiedhistory.list.view.adapter.viewholder.UohRecommendationItemViewHolder
 import com.tokopedia.test.application.environment.callback.TopAdsVerificatorInterface
-import com.tokopedia.test.application.espresso_component.CommonActions.clickOnEachItemRecyclerView
 import com.tokopedia.test.application.assertion.topads.TopAdsAssertion
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.test.application.util.InstrumentationAuthHelper.loginInstrumentationTestTopAdsUser
 import com.tokopedia.test.application.util.setupTopAdsDetector
 import org.junit.*
 
-/**
- * Created by DevAra
- * Instrumented test, which will execute on an Android device.
- * Verify Topads for HomePage
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
- */
 class UohTopAdsVerificationTest {
     private var topAdsAssertion: TopAdsAssertion? = null
 
     @get:Rule
-    var grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    var grantPermission: GrantPermissionRule = GrantPermissionRule.grant(
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     @get:Rule
     var activityRule = object: ActivityTestRule<UohListActivity>(UohListActivity::class.java) {
@@ -43,6 +44,9 @@ class UohTopAdsVerificationTest {
                 activityRule.activity,
                 activityRule.activity.application as TopAdsVerificatorInterface
         )
+
+        login()
+        waitForData()
     }
 
     @After
@@ -59,16 +63,26 @@ class UohTopAdsVerificationTest {
 
         for (i in 0 until itemCount) {
             scrollUohRecyclerViewToPosition(uohRecyclerView, i)
-            checkProductOnDynamicChannel(uohRecyclerView, i)
+            checkProduct(uohRecyclerView, i)
         }
+        waitForData()
         topAdsAssertion?.assert()
     }
 
-    private fun checkProductOnDynamicChannel(uohRecyclerView: RecyclerView, i: Int) {
-        when (val viewHolder = uohRecyclerView.findViewHolderForAdapterPosition(i)) {
+    private fun checkProduct(uohRecyclerView: RecyclerView, i: Int) {
+        when (uohRecyclerView.findViewHolderForAdapterPosition(i)) {
             is UohRecommendationItemViewHolder -> {
-                clickOnEachItemRecyclerView(viewHolder.itemView, R.id.rv_order_list, 0)
+                clickProductRecommItem(uohRecyclerView, i)
             }
+        }
+    }
+
+    private fun clickProductRecommItem(cartRecyclerView: RecyclerView, i: Int) {
+        try {
+            Espresso.onView(ViewMatchers.withId(cartRecyclerView.id))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(i, ViewActions.click()))
+        } catch (e: PerformException) {
+            e.printStackTrace()
         }
     }
 
@@ -76,8 +90,8 @@ class UohTopAdsVerificationTest {
         Thread.sleep(5000)
     }
 
-    private fun scrollUohRecyclerViewToPosition(homeRecyclerView: RecyclerView, position: Int) {
-        val layoutManager = homeRecyclerView.layoutManager as LinearLayoutManager
+    private fun scrollUohRecyclerViewToPosition(uohRecyclerView: RecyclerView, position: Int) {
+        val layoutManager = uohRecyclerView.layoutManager as GridLayoutManager
         activityRule.runOnUiThread { layoutManager.scrollToPositionWithOffset(position, 0) }
     }
 
