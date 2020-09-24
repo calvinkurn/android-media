7f3b6cde7cee602cc850361cf1eb5007af748902
yehezkiel <yehezkiel@tokopedia.com>
2020-09-24 11:49:03 +0700

    check null context

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/utils/image/ImageUtils.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/utils/image/ImageUtils.kt
@@ -75,15 +75,17 @@ object ImageUtils {
         }
     }
 
-    fun loadImageCircleWithPlaceHolder(context: Context, imageView: ImageView,
+    fun loadImageCircleWithPlaceHolder(context: Context?, imageView: ImageView,
                                        url: String?) {
-        Glide.with(context)
-                .asBitmap()
-                .load(url)
-                .dontAnimate()
-                .placeholder(R.drawable.ic_loading_placeholder)
-                .error(R.drawable.ic_loading_error)
-                .into(getCircleImageViewTarget(imageView))
+        context?.let {
+            Glide.with(context)
+                    .asBitmap()
+                    .load(url)
+                    .dontAnimate()
+                    .placeholder(R.drawable.ic_loading_placeholder)
+                    .error(R.drawable.ic_loading_error)
+                    .into(getCircleImageViewTarget(imageView))
+        }
     }
 
     fun loadImageRounded2(context: Context, imageview: ImageView, url: String?, radius: Float) {
