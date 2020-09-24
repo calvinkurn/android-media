98b2ee71b97bdf6a6242770b6176511bc3c4ce6b
isfaaghyth <isfaaghyth@gmail.com>
2020-09-24 13:12:17 +0700

    fixed issue of firebase/a91457d76bba105f86f27a5caeb86ff1

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/StatusViewHolder.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/StatusViewHolder.kt
@@ -40,7 +40,7 @@ class StatusViewHolder(
 
     private fun success() {
         pgLoader?.hide()
-        imgStatus?.loadImageDrawable(R.drawable.ic_ts_notif_status_checked)
+        imgStatus?.loadImageDrawable(R.drawable.ic_ts_notif_status_passed)
         txtStatus?.text = context.getString(R.string.notif_status_checked)
     }
 
