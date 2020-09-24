38ec453c828d670e0ac27dc295268fa1f452b601
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-09-24 14:27:16 +0700

    fix parcelable class

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/view/model/AttachmentImageModel.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/view/model/AttachmentImageModel.kt
@@ -1,13 +1,16 @@
 package com.tokopedia.kyc_centralized.view.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author by alvinatin on 21/11/18.
  */
-class AttachmentImageModel {
+@Parcelize
+data class AttachmentImageModel (
     @SerializedName("picture_obj")
     @Expose
     val pictureObj: String = ""
-}
+): Parcelable
