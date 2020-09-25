20e4de486719ebfe18f2fc071be2f3407053e7ff
elscione <yusufhendrawan06@gmail.com>
2020-09-25 12:51:35 +0700

    use @Parcelize annotation instead of implement it manually

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/waitingpaymentorder/domain/model/WaitingPaymentOrderRequestParam.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/waitingpaymentorder/domain/model/WaitingPaymentOrderRequestParam.kt
@@ -1,14 +1,15 @@
 package com.tokopedia.sellerorder.waitingpaymentorder.domain.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by yusuf.hendrawan on 2020-09-07.
  */
 
+@Parcelize
 data class WaitingPaymentOrderRequestParam(
         @SerializedName("is_mobile")
         @Expose
@@ -33,35 +34,4 @@ data class WaitingPaymentOrderRequestParam(
         @SerializedName("show_page")
         @Expose
         val showPage: Int = 1
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readByte() != 0.toByte(),
-            parcel.readLong(),
-            parcel.readString().orEmpty(),
-            parcel.readInt(),
-            parcel.readInt(),
-            parcel.readInt())
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeByte(if (isMobile) 1 else 0)
-        parcel.writeLong(nextPaymentDeadline)
-        parcel.writeString(lang)
-        parcel.writeInt(page)
-        parcel.writeInt(batchPage)
-        parcel.writeInt(showPage)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<WaitingPaymentOrderRequestParam> {
-        override fun createFromParcel(parcel: Parcel): WaitingPaymentOrderRequestParam {
-            return WaitingPaymentOrderRequestParam(parcel)
-        }
-
-        override fun newArray(size: Int): Array<WaitingPaymentOrderRequestParam?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
