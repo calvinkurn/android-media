35ef6ac5410564c433c113a0797e4ce738375619
isfaaghyth <isfaaghyth@gmail.com>
2020-09-28 21:13:14 +0700

    adding println of headers on interceptor

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/utils/GlideNetworkInterceptor.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/utils/GlideNetworkInterceptor.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.media.loader.utils
 
+import com.tokopedia.media.loader.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import timber.log.Timber
@@ -12,7 +13,13 @@ object GlideNetworkInterceptor {
             val response = chain?.proceed(chain.request())
             val header = response?.header(HEADER_ECT)
 
+            // send to scalyr
             Timber.d("Connection-Type (ECT): $header")
+
+            // showing into logcat tool
+            if (BuildConfig.DEBUG) {
+                println(response?.headers().toString())
+            }
 
             response
         }
