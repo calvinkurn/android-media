618e252975a8a1847d6f1807c95164e63f5a367c
elscione <yusufhendrawan06@gmail.com>
2020-09-28 21:28:40 +0700

    remove unused codes

--- libraries/applink/src/main/java/com/tokopedia/applink/sellermigration/SellerMigrationUtil.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/sellermigration/SellerMigrationUtil.kt
@@ -2,18 +2,13 @@ package com.tokopedia.applink.sellermigration
 
 import android.app.TaskStackBuilder
 import android.content.Context
-import android.net.Uri
 import com.tokopedia.applink.RouteManager
 
 class SellerMigrationUtil {
     fun startRedirectionActivities(context: Context, appLinks: List<String>) {
         val taskStackBuilder = TaskStackBuilder.create(context)
         appLinks.forEach {
-            val intent = RouteManager.getIntent(context, Uri.parse(it).buildUpon()
-                    .appendQueryParameter(RouteManager.KEY_REDIRECT_TO_SELLER_APP, "true")
-                    .appendQueryParameter(SellerMigrationApplinkConst.QUERY_PARAM_IS_AUTO_LOGIN, "true")
-                    .toString())
-            taskStackBuilder.addNextIntent(intent)
+            taskStackBuilder.addNextIntent(RouteManager.getIntent(context, it))
         }
         taskStackBuilder.startActivities()
     }
