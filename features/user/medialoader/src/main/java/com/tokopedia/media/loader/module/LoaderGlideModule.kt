d74955b9875ccca9b2758e4332b026fc7167d581
isfaaghyth <isfaaghyth@gmail.com>
2020-09-28 21:12:42 +0700

    adding requestOptions for reduce request time

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/module/LoaderGlideModule.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/module/LoaderGlideModule.kt
@@ -7,8 +7,10 @@ import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 import com.tokopedia.media.loader.BuildConfig
 import com.tokopedia.media.loader.utils.GlideNetworkInterceptor.okHttpInterceptor
 import java.io.InputStream
@@ -18,6 +20,12 @@ class LoaderGlideModule: AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         super.applyOptions(context, builder)
+        // optimize image loader on glide
+        builder.setDefaultRequestOptions(
+                RequestOptions().format(DecodeFormat.PREFER_RGB_565)
+        )
+
+        // showing glide log in debug app
         if (BuildConfig.DEBUG) {
             builder.setLogLevel(Log.VERBOSE)
         }
