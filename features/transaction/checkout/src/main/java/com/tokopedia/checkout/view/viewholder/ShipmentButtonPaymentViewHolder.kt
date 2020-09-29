e38767b3cb6b7a3da6efdb2ab5489987987410fd
irfan khoirul <irfankhoirul@gmail.com>
2020-09-29 15:08:02 +0700

    set button not clickable

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentButtonPaymentViewHolder.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentButtonPaymentViewHolder.kt
@@ -45,11 +45,22 @@ class ShipmentButtonPaymentViewHolder(val view: View, val actionListener: Shipme
         }
         itemView.tv_total_payment.text = model.totalPrice
         itemView.btn_select_payment_method.buttonType = model.abTestButton.getUnifyButtonType()
-        itemView.btn_select_payment_method.setOnClickListener {
-            actionListener.onProcessToPayment()
-        }
-        itemView.btn_select_cod.setOnClickListener {
-            actionListener.onProcessToPaymentCod()
+        if (model.isClickable) {
+            itemView.btn_select_payment_method.setOnClickListener {
+                model.isClickable = false
+                actionListener.onProcessToPayment()
+            }
+            itemView.btn_select_cod.setOnClickListener {
+                model.isClickable = false
+                actionListener.onProcessToPaymentCod()
+            }
+        } else {
+            itemView.btn_select_payment_method.setOnClickListener {
+                // No-op
+            }
+            itemView.btn_select_cod.setOnClickListener {
+                // No-op
+            }
         }
     }
 }
