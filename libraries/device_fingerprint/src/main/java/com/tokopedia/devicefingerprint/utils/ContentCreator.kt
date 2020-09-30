d68e911e99ab8b0facdddd7928c9df786a795648
Rizaldi Martaputra <rizaldi.martaputra@tokopedia.com>
2020-09-30 15:07:39 +0700

    Add public key for production

--- libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/utils/ContentCreator.kt
+++ libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/utils/ContentCreator.kt
@@ -5,6 +5,8 @@ import com.google.gson.Gson
 import com.tokopedia.devicefingerprint.payload.DeviceInfoPayload
 import com.tokopedia.encryption.security.AESEncryptorCBC
 import com.tokopedia.encryption.security.RSA
+import com.tokopedia.url.Env
+import com.tokopedia.url.TokopediaUrl
 import javax.inject.Inject
 
 
@@ -13,8 +15,11 @@ class ContentCreator @Inject constructor(
         private val rsa: RSA
 ){
 
-    // TODO Should be moved somewhere else. This key is for staging only.
-    private val publicKeyString = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaDPpCjDLd15gwys1oYW+90ALTYKShRvz3ZzOlYLxfh6uBrWqb05UD+nilZ84z3UmHWZ+LqkE+GhifwI965iTUg4Oz67K52SJ19BXs8SpyLxRmovi539CiRCu6ZMQSAGl9GC/Zv1/tV8UFx2XuprRkwbUJU0oA0xXcP2sqCkN/EwIDAQAB"
+    private val publicKeyString: String = if (TokopediaUrl.getInstance().TYPE == Env.LIVE) {
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCVm5b8oVlwM4LaKKNqt6WHL/fmuM+m+wRev+5ibuS6idviplK24OElYIencibA1T3bX1NBNBX++I6iiVr9D2VLU/RZ809u3TyCCD3jetvDiqQwfzJBiADVY/Q/Nk1zDrKA+2ZhPRTWwH0H0y5WLgju2nq0yKkoLHdCwrKxCQ9pdQIDAQAB"
+    } else {
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaDPpCjDLd15gwys1oYW+90ALTYKShRvz3ZzOlYLxfh6uBrWqb05UD+nilZ84z3UmHWZ+LqkE+GhifwI965iTUg4Oz67K52SJ19BXs8SpyLxRmovi539CiRCu6ZMQSAGl9GC/Zv1/tV8UFx2XuprRkwbUJU0oA0xXcP2sqCkN/EwIDAQAB"
+    }
 
     fun createContent(deviceInfoPayload: DeviceInfoPayload): String {
         // Create key from 32-byte random byte array, encrypted with RSA using publicKey
