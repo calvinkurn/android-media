f8e2ecc276af30ef131126fb83b033a374d828f2
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2020-09-30 09:01:35 +0700

    fix all instrument

--- features/entertainment/event/src/androidTest/java/mock/CheckoutEventMockResponse.kt
+++ features/entertainment/event/src/androidTest/java/mock/CheckoutEventMockResponse.kt
@@ -9,12 +9,18 @@ class CheckoutEventMockResponse : MockModelConfig(){
 
     companion object{
         const val KEY_QUERY_PDP_V3 = "event_product_detail_v3"
+        const val KEY_CONTENT = "event_content_by_id"
     }
 
     override fun createMockModel(context: Context): MockModelConfig {
         addMockResponse(
                 KEY_QUERY_PDP_V3,
                 getRawString(context, R.raw.event_checkout),
+                FIND_BY_CONTAINS)
+
+        addMockResponse(
+                KEY_CONTENT,
+                getRawString(context, R.raw.event_checkout_content),
                 FIND_BY_CONTAINS)
 
         return this
--- features/entertainment/event/src/androidTest/java/mock/PDPTicketEventMockResponse.kt
+++ features/entertainment/event/src/androidTest/java/mock/PDPTicketEventMockResponse.kt
@@ -10,12 +10,19 @@ class PDPTicketEventMockResponse : MockModelConfig(){
     companion object{
         const val KEY_QUERY_PDP_V3 = "event_product_detail_v3"
         const val KEY_TRAVEL_HOLIDAY = "TravelGetHoliday"
+        const val KEY_QUERY_CONTENT = "event_content_by_id"
+
     }
 
     override fun createMockModel(context: Context): MockModelConfig {
         addMockResponse(
                 KEY_QUERY_PDP_V3,
                 getRawString(context, R.raw.event_pdp_ticket),
+                FIND_BY_CONTAINS)
+
+        addMockResponse(
+                KEY_QUERY_CONTENT,
+                getRawString(context, R.raw.event_pdp_content),
                 FIND_BY_CONTAINS)
 
         addMockResponse(
