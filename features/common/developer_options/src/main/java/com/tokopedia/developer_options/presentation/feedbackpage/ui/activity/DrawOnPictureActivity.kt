6f8deca4c589cce326f14f96a2da373adee6d62f
Muhammad Furqan <furqan@tokopedia.com>
2020-09-30 16:33:25 +0700

    [Tokopedia Pro][DoP] Make Draw On Picture Activity FullScreen

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/ui/activity/DrawOnPictureActivity.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/feedbackpage/ui/activity/DrawOnPictureActivity.kt
@@ -1,16 +1,33 @@
 package com.tokopedia.developer_options.presentation.feedbackpage.ui.activity
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.developer_options.presentation.feedbackpage.ui.fragment.DrawOnPictureFragment
 
 class DrawOnPictureActivity : BaseSimpleActivity() {
 
-    override fun getNewFragment(): Fragment = DrawOnPictureFragment()
+    override fun getNewFragment(): Fragment = DrawOnPictureFragment
+            .getInstance(intent.getParcelableExtra<Uri>(EXTRA_IMAGE_URI))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "EXTRA_IMAGE_URI"
+
+        fun getIntent(context: Context, imageUri: Uri?): Intent =
+                Intent(context, DrawOnPictureActivity::class.java)
+                        .putExtra(EXTRA_IMAGE_URI, imageUri)
     }
 
 }
