0f11574f20e339f6e873b38420fa2a2de836d6b3
Reivin <reivin.oktavianus@tokopedia.com>
2020-09-30 16:09:34 +0700

    Fix merchant voucher view crash
    https://tokopedia.atlassian.net/browse/AN-20846

--- features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherList/adapter/viewholder/MerchantVoucherViewHolder.kt
+++ features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/voucherList/adapter/viewholder/MerchantVoucherViewHolder.kt
@@ -7,28 +7,28 @@ import com.tokopedia.merchantvoucher.R
 import com.tokopedia.merchantvoucher.common.model.MerchantVoucherViewModel
 import com.tokopedia.merchantvoucher.common.widget.MerchantVoucherView
 import com.tokopedia.merchantvoucher.voucherList.widget.MerchantVoucherListWidget
-import kotlinx.android.synthetic.main.item_merchant_voucher.view.*
 
 /**
  * Created by hendry on 01/10/18.
  */
 class MerchantVoucherViewHolder(
         itemView: View,
-        onMerchantVoucherViewListener: MerchantVoucherView.OnMerchantVoucherViewListener?,
+        private val merchantVoucherViewListener: MerchantVoucherView.OnMerchantVoucherViewListener?,
         private val onMerchantVoucherListWidgetListener: MerchantVoucherListWidget.OnMerchantVoucherListWidgetListener?
 ) : AbstractViewHolder<MerchantVoucherViewModel>(itemView) {
+
+    private var merchantVoucherView: MerchantVoucherView? = itemView.findViewById(R.id.merchantVoucherView)
 
     companion object {
         val LAYOUT = R.layout.item_merchant_voucher
         val HORIZONTAL_LAYOUT = R.layout.item_merchant_voucher_horizontal
     }
 
-    init {
-        itemView.merchantVoucherView.onMerchantVoucherViewListener = onMerchantVoucherViewListener
-    }
-
     override fun bind(element: MerchantVoucherViewModel?) {
-        itemView.merchantVoucherView.setData(element)
+        merchantVoucherView?.run {
+            setData(element)
+            onMerchantVoucherViewListener = merchantVoucherViewListener
+        }
         element?.let{
             itemView.addOnImpressionListener(it){
                 onMerchantVoucherListWidgetListener?.onVoucherItemImpressed(it, adapterPosition)
