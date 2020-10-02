f6180ab1b70aee0243eaa794f01cab5e169ff52d
samuel-garcia <samuel.garcia@tokopedia.com>
2020-10-02 10:55:03 +0700

    fix concat

--- libraries/utils/src/main/java/com/tokopedia/utils/contentdescription/TextAndContentDescriptionUtil.kt
+++ libraries/utils/src/main/java/com/tokopedia/utils/contentdescription/TextAndContentDescriptionUtil.kt
@@ -7,6 +7,6 @@ object TextAndContentDescriptionUtil {
     @JvmStatic
     fun setTextAndContentDescription(textView :TextView, text :String, contentDescriptionTemplate :String) {
         textView.text = text;
-        textView.contentDescription = contentDescriptionTemplate + text
+        textView.contentDescription = "$contentDescriptionTemplate $text"
     }
 }
