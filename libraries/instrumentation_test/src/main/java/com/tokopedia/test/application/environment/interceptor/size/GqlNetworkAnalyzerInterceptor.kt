f7956a10ffd2f57910b79d49200afc464ada71d0
darianthedytoped <darian.thedy@tokopedia.com>
2020-10-02 13:10:21 +0700

    network analyzer interceptor improvement

--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/interceptor/size/GqlNetworkAnalyzerInterceptor.kt
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/interceptor/size/GqlNetworkAnalyzerInterceptor.kt
@@ -5,6 +5,7 @@ import com.tokopedia.analytics.performance.util.NetworkData
 import com.tokopedia.network.BuildConfig
 import com.tokopedia.test.application.util.parserule.ParserRuleProvider
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import okio.Buffer
 import java.io.IOException
@@ -21,6 +22,7 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
         val sizeInEachRequest = hashMapOf<String, Int>()
         val timeInEachRequest = hashMapOf<String, Long>()
         val queryCounterMap = hashMapOf<String, Int>()
+        val interceptedNetworkDataList = mutableListOf<InterceptedNetworkData>()
 
         var startRequest = 0L
         var endRequest = 0L
@@ -53,7 +55,9 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
             return endRequest - startRequest
         }
 
-        fun getNetworkData(): NetworkData {
+        fun getNetworkData(gqlQueryListToAnalyze: List<String>? = listOf()): NetworkData {
+            processInterceptedNetworkData(gqlQueryListToAnalyze)
+
             return NetworkData(
                     getTotalSize(),
                     getTotalTime(),
@@ -62,18 +66,17 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
                     timeInEachRequest
             )
         }
-    }
 
-    override fun intercept(chain: Interceptor.Chain): Response {
-        if (BuildConfig.DEBUG) {
-            try {
-                val request = chain.request()
-                val buffer = Buffer()
-                request.body()?.writeTo(buffer)
-                val requestString = buffer.readUtf8()
-                val response = chain.proceed(chain.request())
+        private fun processInterceptedNetworkData(gqlQueryListToAnalyze: List<String>?) {
+            Log.d("NetInterceptor", "Processing: ${interceptedNetworkDataList.size}")
 
-                val size = response.peekBody(Long.MAX_VALUE).bytes().size
+            interceptedNetworkDataList.forEach { interceptedNetworkData ->
+                val requestString = interceptedNetworkData.requestString
+                val size = interceptedNetworkData.size
+                val reqTimeStamp = interceptedNetworkData.requestTimeStamp
+                val respTimeStamp = interceptedNetworkData.responseTimeStamp
+                val duration = interceptedNetworkData.duration
+
                 var formattedOperationName = parserRuleProvider.parse(requestString.substringAfter("\"query\": \""))
 
                 var needToAnalyze = true
@@ -81,6 +84,8 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
                 if (gqlFilterList?.isNotEmpty() == true) {
                     needToAnalyze = gqlFilterList.contains(formattedOperationName.toLowerCase(Locale.US))
                 }
+
+                Log.d("NetInterceptor", "Name: $formattedOperationName, needToAnalyze: $needToAnalyze, gqlFilterList: $gqlFilterList")
 
                 if (needToAnalyze) {
                     if (queryCounterMap.containsKey(formattedOperationName)) {
@@ -91,20 +96,30 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
                         queryCounterMap[formattedOperationName] = 1
                     }
                     sizeInEachRequest[formattedOperationName] = size
-                    val reqTimeStamp = response.sentRequestAtMillis()
-                    val respTimeStamp = response.receivedResponseAtMillis()
-                    val duration = respTimeStamp - reqTimeStamp
                     timeInEachRequest[formattedOperationName] = duration
 
                     if (startRequest == 0L) {
                         startRequest = reqTimeStamp
                     }
                     endRequest = respTimeStamp
-
-                    return response
-                } else {
-                    return chain.proceed(chain.request())
                 }
+            }
+        }
+    }
+
+    data class InterceptedNetworkData(
+            val requestString: String,
+            val size: Int,
+            val requestTimeStamp: Long,
+            val responseTimeStamp: Long
+    ) {
+        val duration = responseTimeStamp - requestTimeStamp
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        if (BuildConfig.DEBUG) {
+            try {
+                return analyzeNetwork(chain)
             } catch (e: IOException) {
                 Log.i("SizeInterceptorTag", "did not work" + e.stackTrace)
             }
@@ -114,5 +129,31 @@ class GqlNetworkAnalyzerInterceptor(private var gqlQueryListToAnalyze: List<Stri
                     "bound to be used only with DEBUG mode")
         }
         return chain.proceed(chain.request())
+    }
+
+    private fun analyzeNetwork(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestString = request.getRequestString()
+        val response = chain.proceed(chain.request())
+        val size = response.peekBody(Long.MAX_VALUE).bytes().size
+
+        interceptedNetworkDataList.add(
+                InterceptedNetworkData(
+                        requestString = requestString,
+                        size = size,
+                        requestTimeStamp = response.sentRequestAtMillis(),
+                        responseTimeStamp = response.receivedResponseAtMillis()
+                ).apply {
+                    Log.d("NetInterceptor", "Intercepted Network Data: $this")
+                }
+        )
+
+        return response
+    }
+
+    private fun Request.getRequestString(): String {
+        val buffer = Buffer()
+        this.body()?.writeTo(buffer)
+        return buffer.readUtf8()
     }
 }
