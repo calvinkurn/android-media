dfec4c2918b76d1d247ca159842f581a058d0d96
samuel-garcia <samuel.garcia@tokopedia.com>
2020-10-02 15:47:22 +0700

    change function expect nullable

--- libraries/utils/src/main/java/com/tokopedia/utils/contentdescription/TextAndContentDescriptionUtil.kt
+++ libraries/utils/src/main/java/com/tokopedia/utils/contentdescription/TextAndContentDescriptionUtil.kt
@@ -5,8 +5,12 @@ import android.widget.TextView
 object TextAndContentDescriptionUtil {
 
     @JvmStatic
-    fun setTextAndContentDescription(textView :TextView, text :String, contentDescriptionTemplate :String) {
-        textView.text = text;
-        textView.contentDescription = "$contentDescriptionTemplate $text"
+    fun setTextAndContentDescription(textView :TextView?, text :String, contentDescriptionTemplate :String) {
+        if (textView != null) {
+            textView.text = text
+        }
+        if (textView != null) {
+            textView.contentDescription = "$contentDescriptionTemplate $text"
+        }
     }
 }
