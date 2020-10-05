1d77d55a08361a53b7e9986ac5396deb246debea
Nabilla <nabilla.sabbaha@tokopedia.com>
2020-10-05 16:44:59 +0700

    add implementation reinit retrofit Rest API NetworkClient in instrumentation test module

--- libraries/common_network/src/main/java/com/tokopedia/common/network/util/NetworkClient.java
+++ libraries/common_network/src/main/java/com/tokopedia/common/network/util/NetworkClient.java
@@ -14,6 +14,9 @@ import com.tokopedia.network.interceptor.TkpdAuthInterceptor;
 import com.tokopedia.network.utils.TkpdOkHttpBuilder;
 import com.tokopedia.user.session.UserSession;
 
+import java.util.List;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -21,6 +24,8 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 public class NetworkClient {
     private static Retrofit sRetrofit = null;
     private static RestApi sRestApi = null;
+    private static Retrofit retrofitCustomInterceptor = null;
+    private static RestApi restApiCustomInterceptor = null;
     private static FingerprintManager sFingerprintManager = null;
     private static UserSession sUserSession;
     private static RestDatabase sRestDatabase;
@@ -29,6 +34,7 @@ public class NetworkClient {
 
     }
 
+    // region init default retrofit
     public synchronized static void init(@NonNull Context context) {
         if (sRetrofit == null) {
             UserSession userSession = new UserSession(context.getApplicationContext());
@@ -70,6 +76,7 @@ public class NetworkClient {
         }
         return sRestApi;
     }
+    //endregion
 
     public static UserSession getsUserSession() {
         if (sUserSession != null)
@@ -84,4 +91,48 @@ public class NetworkClient {
         }
         return sFingerprintManager;
     }
+
+    // region init retrofit to support custom interceptor
+    public static void initRetrofitWithInterceptors(@NonNull List<Interceptor> interceptors,
+                                                    @NonNull Context context) {
+        if (retrofitCustomInterceptor == null) reInitRetrofitWithInterceptors(interceptors, context);
+    }
+
+    public static void reInitRetrofitWithInterceptors(@NonNull List<Interceptor> interceptors,
+                                                      @NonNull Context context) {
+        UserSession userSession = new UserSession(context.getApplicationContext());
+        TkpdOkHttpBuilder okkHttpBuilder = new TkpdOkHttpBuilder(context, new OkHttpClient.Builder());
+        if (interceptors != null) {
+            okkHttpBuilder.addInterceptor(new FingerprintInterceptor((NetworkRouter) context.getApplicationContext(), userSession));
+            for (Interceptor interceptor : interceptors) {
+                if (interceptor == null) {
+                    continue;
+                }
+                okkHttpBuilder.addInterceptor(interceptor);
+            }
+        }
+
+        retrofitCustomInterceptor = new Retrofit.Builder()
+                .baseUrl(RestConstant.BASE_URL)
+                .addConverterFactory(new StringResponseConverter())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(okkHttpBuilder.build()).build();
+
+        sUserSession = new UserSession(context);
+    }
+
+    private static Retrofit getRetrofitCustomInterceptor() {
+        if (retrofitCustomInterceptor == null) {
+            throw new RuntimeException("Please call reInitRetrofitWithInterceptors() to start the network library.");
+        }
+        return retrofitCustomInterceptor;
+    }
+
+    public static RestApi getApiInterfaceCustomInterceptor() {
+        if (restApiCustomInterceptor == null) {
+            restApiCustomInterceptor = getRetrofitCustomInterceptor().create(RestApi.class);
+        }
+        return restApiCustomInterceptor;
+    }
+    //endregion
 }
--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
@@ -39,6 +39,7 @@ import com.tokopedia.network.data.model.FingerprintModel;
 import com.tokopedia.remoteconfig.RemoteConfigInstance;
 import com.tokopedia.test.application.environment.callback.TopAdsVerificatorInterface;
 import com.tokopedia.test.application.environment.interceptor.TopAdsDetectorInterceptor;
+import com.tokopedia.test.application.environment.interceptor.mock.MockInterceptor;
 import com.tokopedia.test.application.environment.interceptor.size.GqlNetworkAnalyzerInterceptor;
 import com.tokopedia.test.application.util.DeviceConnectionInfo;
 import com.tokopedia.test.application.util.DeviceInfo;
@@ -87,9 +88,9 @@ public class InstrumentationTestApp extends BaseMainApplication
         initAkamaiBotManager();
         LinkerManager.initLinkerManager(getApplicationContext()).setGAClientId(TrackingUtils.getClientID(getApplicationContext()));
         TrackApp.getInstance().initializeAllApis();
-        NetworkClient.init(this);
         GlobalConfig.DEBUG = true;
         GlobalConfig.VERSION_NAME = "3.90";
+        NetworkClient.init(this);
         GraphqlClient.init(this);
         com.tokopedia.config.GlobalConfig.DEBUG = true;
         RemoteConfigInstance.initAbTestPlatform(this);
@@ -136,6 +137,18 @@ public class InstrumentationTestApp extends BaseMainApplication
             testInterceptors.put(interceptor.getClass().getCanonicalName(), interceptor);
             ArrayList<Interceptor> interceptorList = new ArrayList<Interceptor>(testInterceptors.values());
             GraphqlClient.reInitRetrofitWithInterceptors(interceptorList, this);
+        }
+    }
+
+    /**
+     * this method is just for mock response API with custom interceptor
+     * common_network with use case RestRequestSupportInterceptorUseCase
+     */
+    public void addRestSupportInterceptor(Interceptor interceptor) {
+        if (!testInterceptors.containsKey(interceptor.getClass().getCanonicalName())) {
+            testInterceptors.put(interceptor.getClass().getCanonicalName(), interceptor);
+            ArrayList<Interceptor> interceptorList = new ArrayList<Interceptor>(testInterceptors.values());
+            NetworkClient.reInitRetrofitWithInterceptors(interceptorList, this);
         }
     }
 
