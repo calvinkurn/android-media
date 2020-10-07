71401762bfd497dcae33f2ab83355e1762e54777
mzennis <meyta.taliti@tokopedia.com>
2020-10-07 20:32:14 +0700

    add analytic

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/analytic/PlayWidgetMediumAnalytic.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/analytic/PlayWidgetMediumAnalytic.kt
@@ -1,9 +1,22 @@
 package com.tokopedia.play.widget.analytic
 
+import com.tokopedia.play.widget.PlayWidgetMediumBannerUiModel
+import com.tokopedia.play.widget.PlayWidgetMediumChannelUiModel
+import com.tokopedia.play.widget.PlayWidgetMediumOverlayUiModel
+
 
 /**
  * Created by mzennis on 07/10/20.
  */
 interface PlayWidgetMediumAnalytic {
+
+    fun clickOverlayItem(data: PlayWidgetMediumOverlayUiModel)
+    fun impressionOverlayItem(data: PlayWidgetMediumOverlayUiModel)
+
+    fun clickChannelItem(data: PlayWidgetMediumChannelUiModel, position: Int)
+    fun impressionChannelItem(data: PlayWidgetMediumChannelUiModel, position: Int)
+
+    fun clickBannerItem(data: PlayWidgetMediumBannerUiModel, position: Int)
+    fun impressionBannerItem(data: PlayWidgetMediumBannerUiModel, position: Int)
 
 }
---
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/analytic/PlayWidgetMediumAnalyticImpl.kt
@@ -0,0 +1,34 @@
+package com.tokopedia.play.widget.analytic
+
+import com.tokopedia.play.widget.PlayWidgetMediumBannerUiModel
+import com.tokopedia.play.widget.PlayWidgetMediumChannelUiModel
+import com.tokopedia.play.widget.PlayWidgetMediumOverlayUiModel
+
+
+/**
+ * Created by mzennis on 07/10/20.
+ */
+class PlayWidgetMediumAnalyticImpl : PlayWidgetMediumAnalytic {
+
+    override fun clickOverlayItem(data: PlayWidgetMediumOverlayUiModel) {
+
+    }
+
+    override fun impressionOverlayItem(data: PlayWidgetMediumOverlayUiModel) {
+
+    }
+
+    override fun clickChannelItem(data: PlayWidgetMediumChannelUiModel, position: Int) {
+
+
+    }
+
+    override fun impressionChannelItem(data: PlayWidgetMediumChannelUiModel, position: Int) {
+    }
+
+    override fun clickBannerItem(data: PlayWidgetMediumBannerUiModel, position: Int) {
+    }
+
+    override fun impressionBannerItem(data: PlayWidgetMediumBannerUiModel, position: Int) {
+    }
+}
