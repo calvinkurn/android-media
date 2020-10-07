b572188a6380cb9dcc0e361b95934c53e76f8958
Muhammad Furqan <furqan@tokopedia.com>
2020-10-07 13:24:49 +0700

    [Tokopedia Pro][DoP] Implement Header Unify to Draw On Picture Page

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/drawonpicture/presentation/activity/DrawOnPictureActivity.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/drawonpicture/presentation/activity/DrawOnPictureActivity.kt
@@ -3,12 +3,11 @@ package com.tokopedia.developer_options.drawonpicture.presentation.activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.developer_options.R
 import com.tokopedia.developer_options.drawonpicture.di.DaggerDrawOnPictureComponent
 import com.tokopedia.developer_options.drawonpicture.di.DrawOnPictureComponent
 import com.tokopedia.developer_options.drawonpicture.presentation.fragment.DrawOnPictureFragment
@@ -16,20 +15,18 @@ import com.tokopedia.developer_options.drawonpicture.presentation.fragment.DrawO
 class DrawOnPictureActivity : BaseSimpleActivity(), HasComponent<DrawOnPictureComponent> {
 
     override fun getNewFragment(): Fragment = DrawOnPictureFragment
-            .getInstance(intent.getParcelableExtra<Uri>(EXTRA_IMAGE_URI))
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        actionBar?.hide()
-        supportActionBar?.hide()
-    }
+            .getInstance(intent.getParcelableExtra(EXTRA_IMAGE_URI))
 
     override fun getComponent(): DrawOnPictureComponent =
             DaggerDrawOnPictureComponent.builder()
                     .baseAppComponent((application as BaseMainApplication).baseAppComponent)
                     .build()
+
+    override fun getLayoutRes(): Int = R.layout.activity_draw_on_picture
+
+    override fun getToolbarResourceID(): Int = R.id.dopHeaderUnify
+
+    override fun getParentViewResourceID(): Int = R.id.dopFrameLayout
 
     companion object {
         const val EXTRA_IMAGE_URI = "EXTRA_IMAGE_URI"
