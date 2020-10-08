701c1b70a48b676f47f22664c6a020b4537bf1d6
kelvinsaputra <kelvin.saputra@tokopedia.com>
2020-10-08 14:26:47 +0700

    swap the clientId

--- features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/data/Token.kt
@@ -9,8 +9,8 @@ import com.tokopedia.url.TokopediaUrl
 open class Token {
 
     companion object {
-        private const val GOOGLE_API_KEY = "692092518182-ftki1chbj3oemudv5ud7mdnieqe16u7e.apps.googleusercontent.com"
-        private const val GOOGLE_API_KEY_STAGING = "692092518182-h6a8jufa9bl8mfuvcae95qa92cbmes02.apps.googleusercontent.com"
+        private const val GOOGLE_API_KEY_STAGING = "692092518182-ftki1chbj3oemudv5ud7mdnieqe16u7e.apps.googleusercontent.com"
+        private const val GOOGLE_API_KEY = "692092518182-h6a8jufa9bl8mfuvcae95qa92cbmes02.apps.googleusercontent.com"
 
         fun getGoogleClientId(): String = if (TokopediaUrl.getInstance().TYPE == Env.STAGING) {
             GOOGLE_API_KEY_STAGING
