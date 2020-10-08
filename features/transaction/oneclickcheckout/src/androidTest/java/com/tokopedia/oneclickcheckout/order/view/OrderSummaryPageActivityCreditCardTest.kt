56c470b9ab3c9c750a55bd6e082dac0e5118db6a
hansenpw <hansen.wijaya@tokopedia.com>
2020-10-08 15:32:19 +0700

    add credit card test

---
+++ features/transaction/oneclickcheckout/src/androidTest/java/com/tokopedia/oneclickcheckout/order/view/OrderSummaryPageActivityCreditCardTest.kt
@@ -0,0 +1,64 @@
+package com.tokopedia.oneclickcheckout.order.view
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.oneclickcheckout.common.idling.OccIdlingResource
+import com.tokopedia.oneclickcheckout.common.interceptor.GET_OCC_CART_PAGE_CREDIT_CARD_RESPONSE_PATH
+import com.tokopedia.oneclickcheckout.common.interceptor.OneClickCheckoutInterceptor
+import com.tokopedia.oneclickcheckout.common.robot.orderSummaryPage
+import com.tokopedia.oneclickcheckout.common.rule.FreshIdlingResourceTestRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class OrderSummaryPageActivityCreditCardTest {
+
+    @get:Rule
+    var activityRule = IntentsTestRule(OrderSummaryPageActivity::class.java, false, false)
+
+    @get:Rule
+    val freshIdlingResourceTestRule = FreshIdlingResourceTestRule()
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private var idlingResource: IdlingResource? = null
+
+    private val cartInterceptor = OneClickCheckoutInterceptor.cartInterceptor
+    private val preferenceInterceptor = OneClickCheckoutInterceptor.preferenceInterceptor
+    private val logisticInterceptor = OneClickCheckoutInterceptor.logisticInterceptor
+    private val promoInterceptor = OneClickCheckoutInterceptor.promoInterceptor
+    private val checkoutInterceptor = OneClickCheckoutInterceptor.checkoutInterceptor
+
+    @Before
+    fun setup() {
+        OneClickCheckoutInterceptor.resetAllCustomResponse()
+        OneClickCheckoutInterceptor.setupGraphqlMockResponse(context)
+        idlingResource = OccIdlingResource.getIdlingResource()
+        IdlingRegistry.getInstance().register(idlingResource)
+    }
+
+    @After
+    fun cleanup() {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        activityRule.finishActivity()
+    }
+
+    @Test
+    fun happyFlow_DirectCheckout() {
+        cartInterceptor.customGetOccCartResponsePath = GET_OCC_CART_PAGE_CREDIT_CARD_RESPONSE_PATH
+
+        activityRule.launchActivity(null)
+
+        orderSummaryPage {
+
+        } pay {
+            assertGoToPayment(
+                    redirectUrl = "https://www.tokopedia.com/payment",
+                    queryString = "transaction_id=123",
+                    method = "POST"
+            )
+        }
+    }
+}
