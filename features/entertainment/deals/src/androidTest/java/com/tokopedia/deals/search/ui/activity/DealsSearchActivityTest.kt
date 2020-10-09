63d073b1002f440cb8da736e0bc3128034bb4f17
achmad.abrar <achmad.abrar@tokopedia.com>
2020-10-09 16:16:18 +0700

    add impressionOnSearchResult and impressionOnSearchResulNotFound()

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/DealsSearchActivityTest.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/DealsSearchActivityTest.kt
@@ -5,7 +5,7 @@ import android.app.Instrumentation
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
@@ -53,34 +53,39 @@ class DealsSearchActivityTest {
 
     @Test
     fun testSearchLayout() {
-        clickSearchBar()
-        changeLocation()
+        impressionOnSearchResult()
+        impressionOnSearchResulNotFound()
 
             Assert.assertThat(getAnalyticsWithQuery(gtmLogDbSource, context, ANALYTIC_VALIDATOR_QUERY_DEALS_SEARHPAGE),
                 hasAllSuccess())
     }
 
-    private fun clickSearchBar() {
+    private fun impressionOnSearchResult() {
         Thread.sleep(2000)
-        onView(withId(R.id.searchBarDealsBaseSearch)).check(matches(isDisplayed())).perform(click())
-    }
-
-    private fun changeLocation() {
-        Thread.sleep(2000)
+        onView(withId(R.id.search_bar)).check(matches(isDisplayed())).perform(typeText("tes"))
+        Thread.sleep(3000)
         val recyclerView = activtyRule.activity.findViewById<RecyclerView>(R.id.recycler_view)
-        val viewHolder = recyclerView.findViewHolderForAdapterPosition(CHANGE_LOCATION_POSITION)
+        val viewHolder = recyclerView.findViewHolderForAdapterPosition(BRAND_POSITION)
         viewHolder?.let {
-            CommonActions.clickOnEachItemRecyclerView(it.itemView, R.id.location_layout, 0)
+            CommonActions.clickOnEachItemRecyclerView(it.itemView, R.id.banner_recyclerview, 0)
         }
     }
 
-    private fun impressionOnLayout() {
+    private fun impressionOnSearchResulNotFound() {
+        Thread.sleep(2000)
+        onView(withId(R.id.search_bar)).check(matches(isDisplayed())).perform(typeText("tesasjsagsja"))
         Thread.sleep(3000)
-        //onView(withId)
+        val recyclerView = activtyRule.activity.findViewById<RecyclerView>(R.id.recycler_view)
+        val viewHolder = recyclerView.findViewHolderForAdapterPosition(EMPTY_VIEW_POSITION)
+        viewHolder?.let {
+            CommonActions.clickOnEachItemRecyclerView(it.itemView, R.id.banner_recyclerview, 0)
+        }
     }
+
 
     companion object {
        private const val ANALYTIC_VALIDATOR_QUERY_DEALS_SEARHPAGE = "tracker/entertainment/deals/deals_search_tracking.json"
-        const val CHANGE_LOCATION_POSITION = 0
+        const val BRAND_POSITION = 0
+        const val EMPTY_VIEW_POSITION = 0
     }
 }
