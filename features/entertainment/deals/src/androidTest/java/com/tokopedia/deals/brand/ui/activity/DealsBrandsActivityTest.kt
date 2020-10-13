96fe57cd292d6f81986ed42e1c0ec5a8856d59b0
achmad.abrar <achmad.abrar@tokopedia.com>
2020-10-13 11:40:22 +0700

    edit instrumentest

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
@@ -4,13 +4,16 @@ import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
@@ -19,8 +22,12 @@ import com.tokopedia.cassavatest.getAnalyticsWithQuery
 import com.tokopedia.cassavatest.hasAllSuccess
 import com.tokopedia.deals.R
 import com.tokopedia.deals.brand.ui.activity.mock.DealsBrandsMockResponse
+import com.tokopedia.deals.brand.ui.fragment.DealsBrandFragment.Companion.TAB_NAME
 import com.tokopedia.test.application.espresso_component.CommonActions
+import com.tokopedia.test.application.espresso_component.CommonMatcher
+import com.tokopedia.test.application.espresso_component.CommonMatcher.withTagStringValue
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
+import org.hamcrest.core.AllOf
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -30,6 +37,9 @@ class DealsBrandsActivityTest {
 
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val gtmLogDbSource = GtmLogDBSource(context)
+
+    private val TAB_NAME = "Fashion"
+    private val TYPE = "sadjgh"
 
     @get: Rule
     var activtyRule: IntentsTestRule<DealsBrandActivity> = object : IntentsTestRule<DealsBrandActivity>(DealsBrandActivity::class.java) {
@@ -53,6 +63,8 @@ class DealsBrandsActivityTest {
     fun testSearchLayout() {
         impressionDealsEmptyViewHolder()
         actionOnDealsBrandViewHolder()
+        eventClickSearchBrandPage()
+        clickOnRelaksasiTab()
         eventClickCategoryTabBrandPage()
 
         Assert.assertThat(getAnalyticsWithQuery(gtmLogDbSource, context, ANALYTIC_VALIDATOR_QUERY_DEALS_BRANDPAGE),
@@ -61,7 +73,10 @@ class DealsBrandsActivityTest {
 
     private fun impressionDealsEmptyViewHolder() {
         Thread.sleep(3000)
-        onView(withId(R.id.recycler_view)).check(matches(isDisplayed())).perform(RecyclerViewActions
+        onView(withId(com.tokopedia.unifycomponents.R.id.searchbar_textfield)).perform(typeText(TYPE))
+
+        Thread.sleep(3000)
+        onData(withId(R.id.deals_brand_recycler_view)).check(matches(isDisplayed())).perform(RecyclerViewActions
                 .scrollToPosition<RecyclerView.ViewHolder>(EMPTY_POSITION)).perform(click())
     }
 
@@ -78,9 +93,21 @@ class DealsBrandsActivityTest {
         Thread.sleep(2000)
     }
 
+    private fun eventClickSearchBrandPage() {
+        Thread.sleep(2000)
+        onView(withId(com.tokopedia.unifycomponents.R.id.searchbar_textfield)).perform(click())
+        Thread.sleep(2000)
+        onView(withId(com.tokopedia.unifycomponents.R.id.searchbar_textfield)).perform(click())
+    }
+
+    private fun clickOnRelaksasiTab() {
+        Thread.sleep(5000)
+        onView(AllOf.allOf(withId(R.id.tab_item_text_id), ViewMatchers.withText(TAB_NAME))).perform(click())
+    }
+
     private fun eventClickCategoryTabBrandPage() {
         Thread.sleep(3000)
-        activtyRule.activity.tabAnalytics("Kecantikan", 3)
+        onView(AllOf.allOf(withId(R.id.deals_brand_recycler_view), withTagStringValue(TAB_NAME))).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(2))
     }
 
 
--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchMockResponse.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchMockResponse.kt
@@ -8,13 +8,19 @@ import com.tokopedia.test.application.util.InstrumentationMockHelper
 class DealsSearchMockResponse : MockModelConfig() {
 
     override fun createMockModel(context: Context): MockModelConfig {
-
         addMockResponse(KEY_EVENT_SEARCH,
                 InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_search_brands_product), FIND_BY_CONTAINS)
+
+        addMockResponse(KEY_EVENT_LOCATION_SEARCH,
+                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_nearest_location),
+                FIND_BY_CONTAINS)
+
         return this
     }
 
+
     companion object {
         const val KEY_EVENT_SEARCH = "event_search"
+        const val KEY_EVENT_LOCATION_SEARCH = "event_location_search"
     }
 }
