909a05c4ffd3c3fcc6666f5b53c6faabac37ad46
Henry Priyono <henry.priyono@tokopedia.com>
2020-10-14 10:24:05 +0700

    Fix Conflicting Id carouselProductCardRecyclerView

--- features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridActivityTest.kt
+++ features/discovery/carousel_productcard/src/androidTest/java/com/tokopedia/carouselproductcard/test/CarouselProductCardGridActivityTest.kt
@@ -17,7 +17,7 @@ internal class CarouselProductCardGridActivityTest: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.carousel_product_card_activity_test_layout)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.carouselProductCardRecyclerView)
+        val recyclerView = findViewById<RecyclerView>(R.id.carouselProductCardRecyclerViewTest)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = Adapter()
     }
