fcc462bac2a4e3cea7dba551b2f6c97c1821c021
Reivin <reivin.oktavianus@tokopedia.com>
2020-10-16 13:31:56 +0700

    Fix non updated value when scroll and auto update last item
    https://tokopedia.atlassian.net/browse/AN-21278

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/SellableStockProductViewHolder.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/campaignstock/ui/adapter/viewholder/SellableStockProductViewHolder.kt
@@ -1,10 +1,11 @@
 package com.tokopedia.product.manage.feature.campaignstock.ui.adapter.viewholder
 
+import android.text.Editable
 import android.text.InputFilter
+import android.text.TextWatcher
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.kotlin.extensions.view.afterTextChanged
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.kotlin.extensions.view.visible
@@ -20,11 +21,18 @@ class SellableStockProductViewHolder(itemView: View?,
                                      private val onVariantStockChanged: (productId: String, stock: Int) -> Unit,
                                      private val onVariantStatusChanged: (productId: String, status: ProductStatus) -> Unit): AbstractViewHolder<SellableStockProductUIModel>(itemView) {
 
+    private var stockEditTextWatcher: TextWatcher? = null
+
     companion object {
         @LayoutRes
         val LAYOUT_RES = R.layout.item_campaign_stock_variant_editor
 
         private const val MAXIMUM_LENGTH = 7
+    }
+
+    override fun onViewRecycled() {
+        removeListeners()
+        super.onViewRecycled()
     }
 
     override fun bind(element: SellableStockProductUIModel) {
@@ -63,7 +71,7 @@ class SellableStockProductViewHolder(itemView: View?,
 
         setValue(element.stock.toIntOrZero())
 
-        editText.afterTextChanged {
+        stockEditTextWatcher = getStockTextChangeListener {
             val input = it
             val stock = if(input.isNotEmpty()) {
                 input.toInt()
@@ -71,8 +79,10 @@ class SellableStockProductViewHolder(itemView: View?,
                 EditProductConstant.MINIMUM_STOCK
             }
             toggleQuantityEditorBtn(stock)
+            element.stock = stock.toString()
             onVariantStockChanged(element.productId, stock)
         }
+        editText.addTextChangedListener(stockEditTextWatcher)
 
         editText.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
@@ -94,6 +104,26 @@ class SellableStockProductViewHolder(itemView: View?,
 
         addButton.isEnabled = enableAddBtn
         subtractButton.isEnabled = enableSubtractBtn
+    }
+
+    private fun getStockTextChangeListener(afterTextChanged: (String) -> Unit): TextWatcher =
+            object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(editable: Editable?) {
+                    afterTextChanged(editable?.toString().orEmpty())
+                }
+            }
+
+    private fun removeListeners() {
+        itemView.run {
+            stockEditTextWatcher?.let {
+                qte_campaign_stock_variant_editor?.editText?.removeTextChangedListener(it)
+            }
+            switch_campaign_stock_variant_editor?.setOnCheckedChangeListener(null)
+        }
     }
 
     private fun String.toInt(): Int {
