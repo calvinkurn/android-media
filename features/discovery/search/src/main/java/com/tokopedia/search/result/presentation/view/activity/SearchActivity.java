01986b505afaae1f0cb5d362cc7c124ebe47ff21
lukaskris12 <lukaskris12@gmail.com>
2020-10-19 16:12:43 +0700

    Update Color Search

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.java
@@ -151,7 +151,7 @@ public class SearchActivity extends BaseActivity
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            window.setStatusBarColor(getResources().getColor(R.color.Unify_N0));
+            window.setStatusBarColor(getResources().getColor(com.tokopedia.unifyprinciples.R.color.Unify_N0));
         }
     }
 
