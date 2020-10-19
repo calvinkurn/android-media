dc8d09441228c12438a68cd268d487bd3b34984b
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2020-10-19 15:37:50 +0700

    add exclude class

--- features/user/additional_check/src/main/java/com/tokopedia/additional_check/subscriber/TwoFactorCheckerSubscriber.kt
+++ features/user/additional_check/src/main/java/com/tokopedia/additional_check/subscriber/TwoFactorCheckerSubscriber.kt
@@ -41,7 +41,8 @@ class TwoFactorCheckerSubscriber: Application.ActivityLifecycleCallbacks {
     private val exceptionPageSeller = listOf(
             "SplashScreenActivity", "AddPinActivity", "AddPhoneActivity", "TwoFactorActivity",
             "RegisterFingerprintOnboardingActivity", "VerificationActivity", "PinOnboardingActivity",
-            "LogoutActivity", "LoginActivity","GiftBoxTapTapActivity", "GiftBoxDailyActivity"
+            "LogoutActivity", "LoginActivity","GiftBoxTapTapActivity", "GiftBoxDailyActivity", "RegisterInitialActivity",
+            "RegisterEmailActivity", "ShopOpenRevampActivity" , "PinpointMapActivity"
     )
 
     override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
