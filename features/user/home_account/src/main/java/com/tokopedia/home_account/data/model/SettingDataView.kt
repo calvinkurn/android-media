616271a4e22b9ecdeb1b751e258142d5b0fef229
yorispr <yoris.prayogo@tokopedia.com>
2020-10-19 12:16:56 +0700

    create data views

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/data/model/SettingDataView.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.home_account.data.model
+
+/**
+ * Created by Yoris Prayogo on 16/10/20.
+ * Copyright (c) 2020 PT. Tokopedia All rights reserved.
+ */
+data class SettingDataView(
+        val title: String = "",
+        val items: List<CommonDataView> = listOf()
+)
