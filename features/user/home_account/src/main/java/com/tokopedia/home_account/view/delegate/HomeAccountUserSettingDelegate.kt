e8fc2664ed21643c98432060c75f4dfa74bc09c2
yorispr <yoris.prayogo@tokopedia.com>
2020-10-19 12:17:34 +0700

    create delegate

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/delegate/HomeAccountUserSettingDelegate.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.home_account.view.delegate
+
+import android.view.View
+import android.view.ViewGroup
+import com.tokopedia.adapterdelegate.TypedAdapterDelegate
+import com.tokopedia.home_account.data.model.SettingDataView
+import com.tokopedia.home_account.view.listener.HomeAccountUserListener
+import com.tokopedia.home_account.view.viewholder.SettingViewHolder
+
+/**
+ * Created by Yoris Prayogo on 16/10/20.
+ * Copyright (c) 2020 PT. Tokopedia All rights reserved.
+ */
+
+class HomeAccountUserSettingDelegate (val listener: HomeAccountUserListener):
+        TypedAdapterDelegate<SettingDataView, Any, SettingViewHolder>(
+                SettingViewHolder.LAYOUT
+        ) {
+
+    override fun onBindViewHolder(item: SettingDataView, holder: SettingViewHolder) {
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, basicView: View): SettingViewHolder {
+        return SettingViewHolder(basicView, listener)
+    }
+
+}
