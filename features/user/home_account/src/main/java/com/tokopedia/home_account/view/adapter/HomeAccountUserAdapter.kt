acc3a85d37fd675ce39c27be39d7271a634b1a83
yorispr <yoris.prayogo@tokopedia.com>
2020-10-19 12:17:23 +0700

    create adapters

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/adapter/HomeAccountUserAdapter.kt
@@ -0,0 +1,19 @@
+package com.tokopedia.home_account.view.adapter
+
+import com.tokopedia.adapterdelegate.BaseCommonAdapter
+import com.tokopedia.home_account.view.delegate.HomeAccountUserAdapterDelegate
+import com.tokopedia.home_account.view.delegate.HomeAccountUserSettingDelegate
+import com.tokopedia.home_account.view.listener.HomeAccountUserListener
+
+/**
+ * Created by Yoris Prayogo on 16/10/20.
+ * Copyright (c) 2020 PT. Tokopedia All rights reserved.
+ */
+class HomeAccountUserAdapter(
+        listener: HomeAccountUserListener
+): BaseCommonAdapter() {
+    init {
+        delegatesManager.addDelegate(HomeAccountUserAdapterDelegate(listener))
+        delegatesManager.addDelegate(HomeAccountUserSettingDelegate(listener))
+    }
+}
