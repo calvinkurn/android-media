204dd75638fcf36d205c3b1af02d46f58eeedff4
hansenpw <hansen.wijaya@tokopedia.com>
2020-10-19 14:44:37 +0700

    ui fixes

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingdurationocc/ShippingDurationOccBottomSheet.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingdurationocc/ShippingDurationOccBottomSheet.kt
@@ -26,6 +26,7 @@ class ShippingDurationOccBottomSheet : ShippingDurationAdapterListener {
                 bottomSheetUnify = BottomSheetUnify().apply {
                     isDragable = true
                     isHideable = true
+                    clearContentPadding = true
                     setTitle(context.getString(R.string.title_bottomsheet_shipment))
                     val child = View.inflate(context, R.layout.bottomsheet_shipping_occ, null)
                     setupChild(child, list)
