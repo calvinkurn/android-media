ef4a024bcc6d98b6ddb32fbb848d274af5c34059
yorispr <yoris.prayogo@tokopedia.com>
2020-10-19 12:17:44 +0700

    create listener

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/listener/HomeAccountUserListener.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.home_account.view.listener
+
+import com.tokopedia.home_account.data.model.CommonDataView
+
+/**
+ * Created by Yoris Prayogo on 16/10/20.
+ * Copyright (c) 2020 PT. Tokopedia All rights reserved.
+ */
+interface HomeAccountUserListener {
+    fun onEditProfileClicked()
+    fun onSettingItemClicked(applink: String)
+    fun onSwitchChanged(item: CommonDataView, isActive: Boolean)
+}
