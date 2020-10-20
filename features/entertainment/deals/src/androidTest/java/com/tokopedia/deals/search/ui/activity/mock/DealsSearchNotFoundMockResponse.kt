462bf7c964044052cef5c469124f5882b5f2d251
Jessica Sean <jessica.sean@tokopedia.com>
2020-10-20 14:01:25 +0700

    fix instrumentation test

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchNotFoundMockResponse.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchNotFoundMockResponse.kt
@@ -1,32 +1,32 @@
-package com.tokopedia.deals.category.ui.activity.mock
+package com.tokopedia.deals.search.ui.activity.mock
 
 import android.content.Context
 import com.tokopedia.deals.test.R
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
 import com.tokopedia.test.application.util.InstrumentationMockHelper
 
-/**
- * @author by jessica on 09/10/20
- */
+class DealsSearchNotFoundMockResponse : MockModelConfig() {
 
-class DealsCategoryMockResponse : MockModelConfig() {
     override fun createMockModel(context: Context): MockModelConfig {
-        addMockResponse(KEY_EVENT_BRAND,
-                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_search_brands),
+        addMockResponse(KEY_EVENT_CHILD_CATEGORY,
+                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_recent_search),
                 FIND_BY_CONTAINS)
+
         addMockResponse(KEY_EVENT_LOCATION_SEARCH,
                 InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_nearest_location),
                 FIND_BY_CONTAINS)
-        addMockResponse(KEY_EVENT_CHILD_CATEGORY,
-                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_event_child_category),
+
+        addMockResponse(KEY_EVENT_BRAND,
+                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_search_brands_not_found),
                 FIND_BY_CONTAINS)
+
         return this
     }
+
 
     companion object {
         const val KEY_EVENT_LOCATION_SEARCH = "event_location_search"
         const val KEY_EVENT_CHILD_CATEGORY = "event_child_category"
         const val KEY_EVENT_BRAND = "event_search"
     }
-
 }
