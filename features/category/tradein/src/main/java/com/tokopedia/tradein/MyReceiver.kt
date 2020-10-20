128f48e535d9930891deff0e7c00488d2d988dfa
varun0922 <varun.sharma@tokopedia.com>
2020-10-20 16:36:28 +0530

    sku removed

---
+++ features/category/tradein/src/main/java/com/tokopedia/tradein/MyReceiver.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.tradein
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context,intent.getStringExtra("SEND"),Toast.LENGTH_SHORT).show()
+    }
+}
