6492f100cd98587ed8d037c1c1c2a310ebc5eb09
Reivin <reivin.oktavianus@tokopedia.com>
2020-10-20 15:27:12 +0700

    Remove duplicate coroutinedispatcher
    https://tokopedia.atlassian.net/browse/AN-18879

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/common/di/ProductManageModule.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/common/di/ProductManageModule.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.product.manage.common.coroutine.CoroutineDispatchers
+import com.tokopedia.product.manage.common.coroutine.CoroutineDispatchersProvider
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
@@ -37,5 +39,5 @@ class ProductManageModule  {
 
     @ProductManageScope
     @Provides
-    fun provideCoroutineDispatchers(): com.tokopedia.product.manage.common.coroutine.CoroutineDispatchers = com.tokopedia.product.manage.common.coroutine.CoroutineDispatchersProvider
+    fun provideCoroutineDispatchers(): CoroutineDispatchers = CoroutineDispatchersProvider
 }
