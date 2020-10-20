fa5994d19cde7c51463d64bb69cb32a7ec0ac578
achmad.abrar <achmad.abrar@tokopedia.com>
2020-10-20 10:09:47 +0700

    change mock response

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchMockResponse.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/search/ui/activity/mock/DealsSearchMockResponse.kt
@@ -8,16 +8,12 @@ import com.tokopedia.test.application.util.InstrumentationMockHelper
 class DealsSearchMockResponse : MockModelConfig() {
 
     override fun createMockModel(context: Context): MockModelConfig {
-        addMockResponse(KEY_EVENT_SEARCH,
-                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_search_brands_product),
+        addMockResponse(KEY_EVENT_CHILD_CATEGORY,
+                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_recent_search),
                 FIND_BY_CONTAINS)
 
         addMockResponse(KEY_EVENT_LOCATION_SEARCH,
                 InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_nearest_location),
-                FIND_BY_CONTAINS)
-
-        addMockResponse(KEY_EVENT_CHILD_CATEGORY,
-                InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_get_event_child_category),
                 FIND_BY_CONTAINS)
 
         addMockResponse(KEY_EVENT_BRAND,
@@ -29,7 +25,6 @@ class DealsSearchMockResponse : MockModelConfig() {
 
 
     companion object {
-        const val KEY_EVENT_SEARCH = "event_search"
         const val KEY_EVENT_LOCATION_SEARCH = "event_location_search"
         const val KEY_EVENT_CHILD_CATEGORY = "event_child_category"
         const val KEY_EVENT_BRAND = "event_search"
