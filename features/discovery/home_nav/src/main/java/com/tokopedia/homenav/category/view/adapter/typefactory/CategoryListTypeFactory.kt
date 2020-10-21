960ceb459249f6cf59b2f7e8111acf06bec960cc
yoasfs <yoas.susantio@tokopedia.com>
2020-10-21 10:04:37 +0700

    update for categorylist

--- features/discovery/home_nav/src/main/java/com/tokopedia/homenav/category/view/adapter/typefactory/CategoryListTypeFactory.kt
+++ features/discovery/home_nav/src/main/java/com/tokopedia/homenav/category/view/adapter/typefactory/CategoryListTypeFactory.kt
@@ -6,4 +6,6 @@ import com.tokopedia.homenav.base.diffutil.HomeNavTypeFactory
  * Created by Lukas on 20/10/20.
  */
 
-interface CategoryListTypeFactory : HomeNavTypeFactory
+interface CategoryListTypeFactory {
+
+}
--- features/discovery/home_nav/src/main/java/com/tokopedia/homenav/category/view/adapter/typefactory/CategoryListTypeFactoryImpl.kt
+++ features/discovery/home_nav/src/main/java/com/tokopedia/homenav/category/view/adapter/typefactory/CategoryListTypeFactoryImpl.kt
@@ -3,12 +3,21 @@ package com.tokopedia.homenav.category.view.adapter.typefactory
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.adapter.exception.TypeNotSupportedException
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.homenav.base.diffutil.HomeNavVisitable
+import com.tokopedia.homenav.base.viewmodel.MainNavItemViewModel
 
 /**
  * Created by Lukas on 20/10/20.
  */
 class CategoryListTypeFactoryImpl : CategoryListTypeFactory{
-    override fun createViewHolder(view: View, viewType: Int): RecyclerView.ViewHolder {
-        throw TypeNotSupportedException.create("Layout not supported")
+
+
+    override fun type(mainNavItemViewModel: MainNavItemViewModel): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createViewHolder(view: View, viewType: Int): AbstractViewHolder<HomeNavVisitable> {
+        TODO("Not yet implemented")
     }
 }
