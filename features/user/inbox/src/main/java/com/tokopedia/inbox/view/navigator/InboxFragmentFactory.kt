7a875f8e22e57e41238bf808f2b0f3c52a7f9609
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-10-22 14:51:28 +0700

    create chatlist inbox fragment

--- features/user/inbox/src/main/java/com/tokopedia/inbox/view/navigator/InboxFragmentFactory.kt
+++ features/user/inbox/src/main/java/com/tokopedia/inbox/view/navigator/InboxFragmentFactory.kt
@@ -2,16 +2,16 @@ package com.tokopedia.inbox.view.navigator
 
 import androidx.fragment.app.Fragment
 import com.tokopedia.notifcenter.presentation.fragment.NotificationFragment
-import com.tokopedia.topchat.chatlist.fragment.ChatTabListFragment
+import com.tokopedia.topchat.chatlist.fragment.ChatListInboxFragment
 
 interface InboxFragmentFactory {
     fun createChatListFragment(): Fragment
     fun createNotificationFragment(): Fragment
 }
 
-class InboxFragmentFactoryImpl : InboxFragmentFactory{
+class InboxFragmentFactoryImpl : InboxFragmentFactory {
     override fun createChatListFragment(): Fragment {
-        return ChatTabListFragment.create()
+        return ChatListInboxFragment.createFragment()
     }
 
     override fun createNotificationFragment(): Fragment {
