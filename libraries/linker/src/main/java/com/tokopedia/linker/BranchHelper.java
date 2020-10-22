e01b2b1dbea6e1cd818882c9f3b0484db2fd9663
varun0922 <varun.sharma@tokopedia.com>
2020-10-22 14:04:11 +0530

    Changes for the Branch ATC event data mod sku

--- libraries/linker/src/main/java/com/tokopedia/linker/BranchHelper.java
+++ libraries/linker/src/main/java/com/tokopedia/linker/BranchHelper.java
@@ -156,7 +156,7 @@ public class BranchHelper {
                                 .addCustomMetadata(LinkerConstants.ProductCategory, String.valueOf(linkerData.getCatLvl1())));
         new BranchEvent(BRANCH_STANDARD_EVENT.ADD_TO_CART)
                 .addCustomDataProperty(LinkerConstants.USER_ID, linkerData.getUserId())
-                .addCustomDataProperty(LinkerConstants.PRODUCT_ID, linkerData.getId())
+                .addCustomDataProperty(LinkerConstants.PRODUCT_ID, linkerData.getSku())
                 .addCustomDataProperty(LinkerConstants.CONTENT_TYPE, linkerData.getContentType())
                 .addCustomDataProperty(LinkerConstants.CONTENT, linkerData.getContent())
                 .addCustomDataProperty(LinkerConstants.LEVEL1_NAME, linkerData.getLevel1Name())
