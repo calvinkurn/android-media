ea96e6b77b986eb8c84c7414fb7b348ad1879fe7
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-10-26 15:19:19 +0700

    Optimize testapp

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.chuckerteam.chucker.api.Chucker
 import com.tokopedia.application.MyApplication
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
