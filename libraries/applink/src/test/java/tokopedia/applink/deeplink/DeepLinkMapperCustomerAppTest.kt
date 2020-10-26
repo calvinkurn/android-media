eee44258d4ba0cea077c01d4e1bb5318430b2ce4
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-10-26 18:37:56 +0800

    update unit test deep link mapper

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -3,6 +3,7 @@ package tokopedia.applink.deeplink
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.UriUtil
 import com.tokopedia.applink.constant.DeeplinkConstant
+import com.tokopedia.applink.internal.ApplinkConstInternalMechant
 import com.tokopedia.applink.order.DeeplinkMapperUohOrder
 import io.mockk.every
 import org.junit.Test
@@ -584,7 +585,7 @@ class DeepLinkMapperCustomerAppTest: DeepLinkMapperTestFixture() {
     @Test
     fun `check gold merchant statistic dashboard appLink then should return tokopedia internal gold merchant statistic dashboard in customerapp`() {
         val expectedDeepLink = "${DeeplinkConstant.SCHEME_INTERNAL}://marketplace/gold-merchant-statistic-dashboard"
-        assertEqualsDeepLinkMapper(ApplinkConst.GOLD_MERCHANT_STATISTIC_DASHBOARD, expectedDeepLink)
+        assertEqualsDeepLinkMapper(ApplinkConstInternalMechant.MERCHANT_STATISTIC_DASHBOARD, expectedDeepLink)
     }
 
     @Test
