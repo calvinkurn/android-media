f44c12a58c62a5c85135dbea7a784f5ac280ba1a
Muhammad Furqan <furqan@tokopedia.com>
2020-10-27 22:43:29 +0700

    [Flight][Order Detail Revamp] Render Ticket View and Journey Details

--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailFragment.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailFragment.kt
@@ -144,6 +144,57 @@ class FlightOrderDetailFragment : BaseDaggerFragment() {
     }
 
     private fun renderTicketView(journeys: List<OrderDetailJourneyModel>) {
+        val onwardJourney = journeys[0]
+        val airlineLogo = flightOrderDetailViewModel.getAirlineLogo(onwardJourney)
+        if (airlineLogo != null) {
+            ivFlightOrderDepartureAirlineLogo.loadImage(airlineLogo)
+        } else {
+            ivFlightOrderDepartureAirlineLogo.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.flight_ic_multi_airlines))
+        }
+
+        tgFlightOrderDepartureAirlineName.text = flightOrderDetailViewModel.getAirlineName(onwardJourney)
+
+        if (flightOrderDetailViewModel.getRefundableInfo(onwardJourney)) {
+            tgFlightOrderDepartureTicketRefundableStatus.visibility = View.VISIBLE
+        } else {
+            tgFlightOrderDepartureTicketRefundableStatus.visibility = View.GONE
+        }
+
+        tgFlightOrderDepartureJourneyTrip.text = getString(R.string.flight_order_detail_trip_city,
+                onwardJourney.departureCityName, onwardJourney.departureId,
+                onwardJourney.arrivalCityName, onwardJourney.arrivalId)
+
+        if (onwardJourney.routes.isNotEmpty() && onwardJourney.routes[0].departureTerminal.isNotEmpty()) {
+            tgFlightOrderDepartureAirport.text = getString(R.string.flight_order_detail_airport_with_terminal,
+                    onwardJourney.departureAirportName, onwardJourney.routes[0].departureTerminal)
+        } else {
+            tgFlightOrderDepartureAirport.text = onwardJourney.departureAirportName
+        }
+
+        val departureDateAndTimePair = flightOrderDetailViewModel.getDepartureDateAndTime(onwardJourney)
+        if (onwardJourney.totalTransit > 0) {
+            tgFlightOrderDepartureDetail.text = getString(R.string.flight_order_detail_airport_journey_with_transit,
+                    departureDateAndTimePair.first, departureDateAndTimePair.second, onwardJourney.totalTransit)
+        } else {
+            tgFlightOrderDepartureDetail.text = getString(R.string.flight_order_detail_airport_journey_without_transit,
+                    departureDateAndTimePair.first, departureDateAndTimePair.second)
+        }
+
+        if (onwardJourney.routes.isNotEmpty() && onwardJourney.routes[0].pnr.isNotEmpty()) {
+            tgFlightOrderDepartureBookingCode.visibility = View.VISIBLE
+            tgFlightOrderDepartureBookingCodeLabel.visibility = View.VISIBLE
+            ivFlightOrderDepartureBookingCodeCopy.visibility = View.VISIBLE
+
+            tgFlightOrderDepartureBookingCode.text = onwardJourney.routes[0].pnr
+            ivFlightOrderDepartureBookingCodeCopy.setOnClickListener {
+                copyToClipboard(CLIP_LABEL_DEPARTURE_BOOKING_CODE, onwardJourney.routes[0].pnr)
+            }
+        } else {
+            tgFlightOrderDepartureBookingCode.visibility = View.GONE
+            tgFlightOrderDepartureBookingCodeLabel.visibility = View.GONE
+            ivFlightOrderDepartureBookingCodeCopy.visibility = View.GONE
+        }
+
         if (journeys.size > 1) {
             renderReturnTicketView(journeys[1])
         } else {
@@ -152,8 +203,8 @@ class FlightOrderDetailFragment : BaseDaggerFragment() {
     }
 
     private fun renderReturnTicketView(returnJourney: OrderDetailJourneyModel) {
-        titleFlightOrderReturnTicket.visibility = View.GONE
-        containerFlightOrderReturnTicket.visibility = View.GONE
+        titleFlightOrderReturnTicket.visibility = View.VISIBLE
+        containerFlightOrderReturnTicket.visibility = View.VISIBLE
 
         val airlineLogo = flightOrderDetailViewModel.getAirlineLogo(returnJourney)
         if (airlineLogo != null) {
@@ -189,6 +240,21 @@ class FlightOrderDetailFragment : BaseDaggerFragment() {
             tgFlightOrderReturnDetail.text = getString(R.string.flight_order_detail_airport_journey_without_transit,
                     departureDateAndTimePair.first, departureDateAndTimePair.second)
         }
+
+        if (returnJourney.routes.isNotEmpty() && returnJourney.routes[0].pnr.isNotEmpty()) {
+            tgFlightOrderReturnBookingCode.visibility = View.VISIBLE
+            tgFlightOrderReturnBookingCodeLabel.visibility = View.VISIBLE
+            ivFlightOrderReturnBookingCodeCopy.visibility = View.VISIBLE
+
+            tgFlightOrderReturnBookingCode.text = returnJourney.routes[0].pnr
+            ivFlightOrderReturnBookingCodeCopy.setOnClickListener {
+                copyToClipboard(CLIP_LABEL_RETURN_BOOKING_CODE, returnJourney.routes[0].pnr)
+            }
+        } else {
+            tgFlightOrderReturnBookingCode.visibility = View.GONE
+            tgFlightOrderReturnBookingCodeLabel.visibility = View.GONE
+            ivFlightOrderReturnBookingCodeCopy.visibility = View.GONE
+        }
     }
 
     private fun hideReturnTicketView() {
@@ -214,6 +280,8 @@ class FlightOrderDetailFragment : BaseDaggerFragment() {
         private const val EXTRA_REQUEST_CANCEL = "EXTRA_REQUEST_CANCEL"
 
         private const val CLIP_LABEL_INVOICE_ID = "Flight Invoice Id"
+        private const val CLIP_LABEL_DEPARTURE_BOOKING_CODE = "Flight Departure Booking Code"
+        private const val CLIP_LABEL_RETURN_BOOKING_CODE = "Flight Return Booking Code"
 
         fun createInstance(invoiceId: String,
                            isCancellation: Boolean,
