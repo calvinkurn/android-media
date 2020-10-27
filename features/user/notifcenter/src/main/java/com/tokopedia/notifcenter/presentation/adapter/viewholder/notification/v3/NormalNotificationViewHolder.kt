f753f45b4b9bf1b9b966cea73b5e27d2194d71f8
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-10-27 13:06:49 +0700

    bind several view notifcenter

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/NormalNotificationViewHolder.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/NormalNotificationViewHolder.kt
@@ -1,16 +1,44 @@
 package com.tokopedia.notifcenter.presentation.adapter.viewholder.notification.v3
 
 import android.view.View
+import android.widget.ImageView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.data.uimodel.NotificationUiModel
+import com.tokopedia.unifyprinciples.Typography
 
 class NormalNotificationViewHolder(
         itemView: View?
 ) : AbstractViewHolder<NotificationUiModel>(itemView) {
 
-    override fun bind(element: NotificationUiModel?) {
+    private val icon: ImageView? = itemView?.findViewById(R.id.iv_icon)
+    private val type: Typography? = itemView?.findViewById(R.id.txt_notification_type)
+    private val time: Typography? = itemView?.findViewById(R.id.txt_notification_time)
+    private val title: Typography? = itemView?.findViewById(R.id.txt_notification_title)
+    private val desc: Typography? = itemView?.findViewById(R.id.txt_notification_desc)
 
+    override fun bind(element: NotificationUiModel) {
+        bindTitle(element)
+        bindDesc(element)
+        bindNotificationType(element)
+        bindIcon(element)
+    }
+
+    private fun bindTitle(element: NotificationUiModel) {
+        title?.text = element.title
+    }
+
+    private fun bindDesc(element: NotificationUiModel) {
+        desc?.text = element.shortDescription
+    }
+
+    private fun bindNotificationType(element: NotificationUiModel) {
+        type?.text = element.sectionKey
+    }
+
+    private fun bindIcon(element: NotificationUiModel) {
+        ImageHandler.LoadImage(icon, element.sectionIcon)
     }
 
     companion object {
