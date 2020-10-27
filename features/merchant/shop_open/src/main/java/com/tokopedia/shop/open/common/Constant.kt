4ce5ed0a37589aec3ecbc4ea9c5a0d4e0c2b2123
yogieputra8 <yogie.putra8@gmail.com>
2020-10-27 15:45:23 +0700

    Remove unused constant

--- features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/common/Constant.kt
+++ features/merchant/shop_open/src/main/java/com/tokopedia/shop/open/common/Constant.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.shop.open.common
 
 object PageNameConstant {
-    const val INPUT_SHOP_PAGE = "INPUT_SHOP_PAGE"
     const val SPLASH_SCREEN_PAGE = "GREETING_PAGE"
     const val QUISIONER_PAGE = "QUISIONER_PAGE"
     const val FINISH_SPLASH_SCREEN_PAGE = "FINISH_SPLASH_SCREEN_PAGE"
@@ -10,8 +9,6 @@ object PageNameConstant {
 object GQLQueryConstant {
     const val QUERY_SHOP_OPEN_REVAMP_GET_SURVEY_DATA = "shop_open_revamp_get_survey_data"
     const val QUERY_SHOP_OPEN_REVAMP_SEND_SURVEY_DATA = "shop_open_revamp_send_survey_data"
-    const val QUERY_SHOP_OPEN_REVAMP_VALIDATE_DOMAIN_SHOP_NAME = "validate_domain_name_shop"
-    const val QUERY_SHOP_OPEN_REVAMP_DOMAIN_SHOP_SUGGESTION = "shop_open_revamp_domain_suggestion"
     const val QUERY_SHOP_OPEN_REVAMP_CREATE_SHOP = "shop_open_revamp_create_shop"
     const val QUERY_SHOP_OPEN_REVAMP_SAVE_SHIPMENT_LOCATION = "shop_open_revamp_saves_hipment_location"
 }
