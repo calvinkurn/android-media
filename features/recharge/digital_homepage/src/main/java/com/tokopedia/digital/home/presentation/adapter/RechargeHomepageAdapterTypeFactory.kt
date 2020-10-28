51791c05ab32727b4b9efd53829afa0df50c6e07
resakemal <resa.saharso@tokopedia.com>
2020-10-28 14:16:10 +0700

    Adjust subhome loading view

--- features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/RechargeHomepageAdapterTypeFactory.kt
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/RechargeHomepageAdapterTypeFactory.kt
@@ -3,6 +3,7 @@ package com.tokopedia.digital.home.presentation.adapter
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
+import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.digital.home.model.*
 import com.tokopedia.digital.home.presentation.adapter.viewholder.*
@@ -92,8 +93,13 @@ class RechargeHomepageAdapterTypeFactory(
         return 0
     }
 
+    override fun type(viewModel: LoadingModel): Int {
+        return RechargeHomepageLoadingViewholder.LAYOUT
+    }
+
     override fun createViewHolder(parent: View, type: Int): AbstractViewHolder<out Visitable<*>> {
         return when (type) {
+            RechargeHomepageLoadingViewholder.LAYOUT -> RechargeHomepageLoadingViewholder(parent)
             // Home Components
             ReminderWidgetViewHolder.LAYOUT -> ReminderWidgetViewHolder(parent, reminderWidgetCallback)
             DynamicLegoBannerViewHolder.LAYOUT -> DynamicLegoBannerViewHolder(parent, dynamicLegoBannerCallback, dynamicLegoBannerCallback)
