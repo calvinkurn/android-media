a2b623095274bee61ef3179e2d1eac546eb07131
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2020-10-29 16:02:29 +0530

    option item design improvement

--- features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/viewholder/ChatHelpfullQuestionViewHolder.kt
+++ features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/viewholder/ChatHelpfullQuestionViewHolder.kt
@@ -6,13 +6,13 @@ import androidx.annotation.LayoutRes
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.abstraction.base.view.widget.DividerItemDecoration
 import com.tokopedia.chat_common.view.adapter.viewholder.BaseChatViewHolder
 import com.tokopedia.chatbot.R
 import com.tokopedia.chatbot.data.helpfullquestion.ChatOptionListViewModel
 import com.tokopedia.chatbot.data.helpfullquestion.HelpFullQuestionsViewModel
 import com.tokopedia.chatbot.domain.pojo.helpfullquestion.HelpFullQuestionPojo
 import com.tokopedia.chatbot.util.ChatBotTimeConverter
+import com.tokopedia.chatbot.util.OptionListRecyclerItemDecorator
 import com.tokopedia.chatbot.view.adapter.viewholder.helpfullquestionoptionlist.ChatOptionListAdapter
 import com.tokopedia.chatbot.view.adapter.viewholder.listener.ChatOptionListListener
 import com.tokopedia.kotlin.extensions.view.hide
@@ -34,7 +34,7 @@ class ChatHelpfullQuestionViewHolder(itemView: View, private val chatOptionListL
         chatActionListSelection.layoutManager = LinearLayoutManager(itemView.context,
                 LinearLayoutManager.VERTICAL, false)
         chatActionListSelection.adapter = adapter
-        chatActionListSelection.addItemDecoration(DividerItemDecoration(itemView.context))
+        chatActionListSelection.addItemDecoration(OptionListRecyclerItemDecorator(itemView.context))
 
     }
 
@@ -42,9 +42,9 @@ class ChatHelpfullQuestionViewHolder(itemView: View, private val chatOptionListL
         super.bind(viewModel)
         model = viewModel
         mesage.text = viewModel?.message
-        if (viewModel?.isSubmited == true){
+        if (viewModel?.isSubmited == true) {
             chatActionBubbleSelectionContainer.hide()
-        }else{
+        } else {
             chatActionBubbleSelectionContainer.show()
             val options = getOptionListViewModelList(viewModel?.helpfulQuestion?.helpfulQuestions)
             viewModel?.helpfulQuestion?.helpfulQuestions?.let { adapter.setDataList(options) }
