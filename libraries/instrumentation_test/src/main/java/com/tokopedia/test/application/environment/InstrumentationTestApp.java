4b71d18e705cebe9a08377c735a6ae12f5f24466
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-10-30 00:37:22 +0700

    revert global cache manager

--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
@@ -10,11 +10,9 @@ import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.play.core.splitcompat.SplitCompat;
-import com.google.firebase.FirebaseApp;
 import com.google.gson.Gson;
 import com.tkpd.remoteresourcerequest.task.ResourceDownloadManager;
 import com.tokopedia.abstraction.AbstractionRouter;
-import com.tokopedia.abstraction.base.app.BaseMainApplication;
 import com.tokopedia.abstraction.common.data.model.storage.CacheManager;
 import com.tokopedia.analyticsdebugger.debugger.FpmLogger;
 import com.tokopedia.applink.ApplinkDelegate;
@@ -23,17 +21,15 @@ import com.tokopedia.applink.ApplinkUnsupported;
 import com.tokopedia.cachemanager.PersistentCacheManager;
 import com.tokopedia.common.network.util.NetworkClient;
 import com.tokopedia.config.GlobalConfig;
-import com.tokopedia.core.network.CoreNetworkApplication;
 import com.tokopedia.core.TkpdCoreRouter;
 import com.tokopedia.core.analytics.TrackingUtils;
 import com.tokopedia.core.analytics.container.GTMAnalytics;
 import com.tokopedia.core.analytics.container.MoengageAnalytics;
 import com.tokopedia.core.analytics.fingerprint.LocationCache;
 import com.tokopedia.core.analytics.fingerprint.domain.model.FingerPrint;
-import com.tokopedia.core.database.manager.GlobalCacheManager;
-import com.tokopedia.core.gcm.GCMHandler;
 import com.tokopedia.core.gcm.base.IAppNotificationReceiver;
 import com.tokopedia.core.gcm.model.NotificationPass;
+import com.tokopedia.core.network.CoreNetworkApplication;
 import com.tokopedia.graphql.data.GraphqlClient;
 import com.tokopedia.instrumentation.test.R;
 import com.tokopedia.linker.LinkerManager;
@@ -52,7 +48,6 @@ import com.tokopedia.track.interfaces.ContextAnalytics;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -78,8 +73,6 @@ public class InstrumentationTestApp extends CoreNetworkApplication
     private int topAdsProductCount = 0;
     private Long totalSizeInBytes = 0L;
     private Map<String, Interceptor> testInterceptors = new HashMap<>();
-
-    private CacheManager cacheManager;
 
     @Override
     public void onCreate() {
@@ -426,10 +419,7 @@ public class InstrumentationTestApp extends CoreNetworkApplication
 
     @Override
     public CacheManager getGlobalCacheManager() {
-        if (cacheManager == null) {
-            cacheManager = new GlobalCacheManager();
-        }
-        return cacheManager;
+        return null;
     }
 
     @Override
