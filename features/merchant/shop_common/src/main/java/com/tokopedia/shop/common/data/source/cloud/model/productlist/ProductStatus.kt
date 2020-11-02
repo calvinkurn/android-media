565c2967aa9d36712b1fbd536f83d5d819ad8297
Reivin <reivin.oktavianus@tokopedia.com>
2020-11-03 02:15:02 +0700

    Add Keep Annotation
    https://tokopedia.atlassian.net/browse/AN-18879

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/productlist/ProductStatus.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/productlist/ProductStatus.kt
@@ -1,22 +1,15 @@
 package com.tokopedia.shop.common.data.source.cloud.model.productlist
 
-import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
 
+@Keep
 enum class ProductStatus {
-    @SerializedName("ACTIVE")
     ACTIVE,
-    @SerializedName("INACTIVE")
     INACTIVE,
-    @SerializedName("BANNED")
     BANNED,
-    @SerializedName("EMPTY")
     EMPTY,
-    @SerializedName("MODERATED")
     MODERATED,
-    @SerializedName("DELETED")
     DELETED,
-    @SerializedName("PENDING")
     PENDING,
-    @SerializedName("VIOLATION")
     VIOLATION
 }
