7c3bbfd6edd764c1311c260a8867368ac33cc439
Reivin <reivin.oktavianus@tokopedia.com>
2020-11-02 22:29:14 +0700

    Add SerializedName for enum class ProductStatus.kt
    https://tokopedia.atlassian.net/browse/AN-18879

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/productlist/ProductStatus.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/productlist/ProductStatus.kt
@@ -1,12 +1,22 @@
 package com.tokopedia.shop.common.data.source.cloud.model.productlist
 
+import com.google.gson.annotations.SerializedName
+
 enum class ProductStatus {
+    @SerializedName("ACTIVE")
     ACTIVE,
+    @SerializedName("INACTIVE")
     INACTIVE,
+    @SerializedName("BANNED")
     BANNED,
+    @SerializedName("EMPTY")
     EMPTY,
+    @SerializedName("MODERATED")
     MODERATED,
+    @SerializedName("DELETED")
     DELETED,
+    @SerializedName("PENDING")
     PENDING,
+    @SerializedName("VIOLATION")
     VIOLATION
 }
