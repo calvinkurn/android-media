2df745c58e7a592488951872fc462c7d35cea709
yoasfs <yoas.susantio@tokopedia.com>
2020-11-03 20:39:09 +0700

    rename

--- features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/activity/MainParentActivity.java
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/presentation/activity/MainParentActivity.java
@@ -157,9 +157,9 @@ public class MainParentActivity extends BaseActivity implements
 
     private static final String MAIN_PARENT_PERFORMANCE_MONITORING_KEY = "mp_slow_rendering_perf";
 
-    private static final String ROLLENCE_EXP_NAME = "Navigation Revamp";
-    private static final String ROLLENCE_VARIANT_OLD = "existing navigation";
-    private static final String ROLLENCE_VARIANT_REVAMP = "navigation revamp";
+    private static final String ROLLANCE_EXP_NAME = "Navigation Revamp";
+    private static final String ROLLANCE_VARIANT_OLD = "existing navigation";
+    private static final String ROLLANCE_VARIANT_REVAMP = "navigation revamp";
 
     ArrayList<BottomMenu> menu = new ArrayList<>();
 
@@ -328,7 +328,7 @@ public class MainParentActivity extends BaseActivity implements
         showSelectedPage();
         bottomNavigation = findViewById(R.id.bottom_navbar);
 
-        String rollenceNavType = RemoteConfigInstance.getInstance().getABTestPlatform().getString(ROLLENCE_EXP_NAME, ROLLENCE_VARIANT_OLD);
+        String rollenceNavType = RemoteConfigInstance.getInstance().getABTestPlatform().getString(ROLLANCE_EXP_NAME, ROLLANCE_VARIANT_OLD);
         populateBottomNavigationView(rollenceNavType);
         bottomNavigation.setMenuClickListener(this);
 
@@ -1168,7 +1168,7 @@ public class MainParentActivity extends BaseActivity implements
         menu.add(new BottomMenu(R.id.menu_home, getResources().getString(R.string.home), R.raw.bottom_nav_home, R.raw.bottom_nav_home_to_enabled, R.drawable.ic_bottom_nav_home_active, R.drawable.ic_bottom_nav_home_enabled, com.tokopedia.navigation.R.color.color_active_bottom_nav, true, 1f, 3f));
         menu.add(new BottomMenu(R.id.menu_feed, getResources().getString(R.string.feed), R.raw.bottom_nav_feed, R.raw.bottom_nav_feed_to_enabled,  R.drawable.ic_bottom_nav_feed_active, R.drawable.ic_bottom_nav_feed_enabled,com.tokopedia.navigation.R.color.color_active_bottom_nav, true, 1f, 3f));
         menu.add(new BottomMenu(R.id.menu_os, getResources().getString(R.string.official), R.raw.bottom_nav_official, R.raw.bottom_nav_os_to_enabled,  R.drawable.ic_bottom_nav_os_active, R.drawable.ic_bottom_nav_os_enabled,com.tokopedia.navigation.R.color.color_active_bottom_nav_os, true, 1f, 3f));
-        if (!rollenceNavType.equals(ROLLENCE_VARIANT_REVAMP)) {
+        if (!rollenceNavType.equals(ROLLANCE_VARIANT_REVAMP)) {
             menu.add(new BottomMenu(R.id.menu_cart, getResources().getString(R.string.keranjang), R.raw.bottom_nav_cart, R.raw.bottom_nav_cart_to_enabled, R.drawable.ic_bottom_nav_cart_active, R.drawable.ic_bottom_nav_cart_enabled, com.tokopedia.navigation.R.color.color_active_bottom_nav, true, 1f, 3f));
             if (userSession.get().isLoggedIn()) {
                 menu.add(new BottomMenu(R.id.menu_account, getResources().getString(R.string.akun), R.raw.bottom_nav_account, R.raw.bottom_nav_account_to_enabled, R.drawable.ic_bottom_nav_account_active, R.drawable.ic_bottom_nav_account_enabled, com.tokopedia.navigation.R.color.color_active_bottom_nav, true, 1f, 3f));
