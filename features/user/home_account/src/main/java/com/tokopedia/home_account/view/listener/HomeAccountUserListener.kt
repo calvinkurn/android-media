7e2ce039e177861dcb7fb69e13c62e76de733427
yorispr <yoris.prayogo@tokopedia.com>
2020-11-03 21:55:04 +0700

    use nav toolbar

--- features/user/home_account/src/main/java/com/tokopedia/home_account/view/listener/HomeAccountUserListener.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/listener/HomeAccountUserListener.kt
@@ -1,14 +1,17 @@
 package com.tokopedia.home_account.view.listener
 
+import android.view.View
 import com.tokopedia.home_account.data.model.CommonDataView
 
 /**
  * Created by Yoris Prayogo on 16/10/20.
  * Copyright (c) 2020 PT. Tokopedia All rights reserved.
  */
+
 interface HomeAccountUserListener {
     fun onEditProfileClicked()
     fun onSettingItemClicked(item: CommonDataView)
     fun onMemberItemClicked(applink: String)
     fun onSwitchChanged(item: CommonDataView, isActive: Boolean)
+    fun onItemViewBinded(position: Int, itemView: View)
 }
