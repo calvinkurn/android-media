d53073b94d57ea0ffce703e683983b16cb8a6141
Muhammad Furqan <furqan@tokopedia.com>
2020-11-03 15:18:39 +0700

    [Flight][Video Banner] Create Travel Video Banner Custom View Widget

--- features/travel/common_travel/src/main/java/com/tokopedia/common/travel/widget/TravelVideoBannerWidget.kt
+++ features/travel/common_travel/src/main/java/com/tokopedia/common/travel/widget/TravelVideoBannerWidget.kt
@@ -3,11 +3,13 @@ package com.tokopedia.common.travel.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import com.tokopedia.applink.RouteManager
 import com.tokopedia.common.travel.R
-import com.tokopedia.common.travel.constant.TravelType
-import com.tokopedia.common.travel.viewmodel.TravelVideoBannerViewModel
+import com.tokopedia.common.travel.data.entity.TravelCollectiveBannerModel
+import com.tokopedia.common.travel.presentation.model.TravelVideoBannerModel
+import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.unifycomponents.BaseCustomView
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.widget_travel_video_banner.view.*
 
 /**
  * @author by furqan on 02/11/2020
@@ -15,19 +17,62 @@ import javax.inject.Inject
 class TravelVideoBannerWidget @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
         BaseCustomView(context, attrs, defStyleAttr) {
 
-    @Inject
-
-    private lateinit var travelVideoBannerViewModel: TravelVideoBannerViewModel
-    private lateinit var videoBannerType: TravelType
+    var listener: ActionListener? = null
+    private lateinit var bannerModel: TravelVideoBannerModel
 
     init {
         View.inflate(context, R.layout.widget_travel_video_banner, this)
     }
 
+    fun setData(collectiveBannerModel: TravelCollectiveBannerModel) {
+        bannerModel = TravelVideoBannerModel()
+        collectiveBannerModel.let {
+            bannerModel.title = it.meta.title
+            if (it.banners.isNotEmpty()) {
+                bannerModel.imageUrl = it.banners[0].attribute.imageUrl
+                bannerModel.destinationLink =
+                        if (it.banners[0].attribute.appUrl.isNotEmpty())
+                            it.banners[0].attribute.appUrl
+                        else
+                            it.banners[0].attribute.webUrl
+            }
+        }
+    }
 
+    fun setData(bannerModel: TravelVideoBannerModel) {
+        this.bannerModel = bannerModel
+    }
 
-    fun setTravelType(travelType: TravelType) {
-        videoBannerType = travelType
+    fun build() {
+        if (::bannerModel.isInitialized) {
+            if (bannerModel.title.isNotEmpty() &&
+                    bannerModel.imageUrl.isNotEmpty() &&
+                    bannerModel.destinationLink.isNotEmpty()) {
+                tgTravelVideoBannerTitle.text = bannerModel.title
+                ivTravelVideoBanner.loadImage(bannerModel.imageUrl)
+                ivTravelVideoBanner.setOnClickListener {
+                    listener?.onVideoBannerClicked(bannerModel)
+                    RouteManager.route(context, bannerModel.destinationLink)
+                }
+                showTravelVideoBanner()
+            } else {
+                hideTravelVideoBanner()
+            }
+        } else {
+            hideTravelVideoBanner()
+        }
+    }
+
+    fun showTravelVideoBanner() {
+        containerTravelVideoBanner.visibility = View.VISIBLE
+    }
+
+    fun hideTravelVideoBanner() {
+        containerTravelVideoBanner.visibility = View.GONE
+    }
+
+    interface ActionListener {
+        fun onVideoBannerClicked(bannerData: TravelVideoBannerModel)
     }
 
 }
