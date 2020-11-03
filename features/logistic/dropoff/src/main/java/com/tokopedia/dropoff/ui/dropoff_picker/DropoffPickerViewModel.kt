3a83d90dec1321644254dca311bad4c39c439f74
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2020-11-03 14:01:28 +0700

    Refactor DropoffPickerViewModel.kt coroutine

--- features/logistic/dropoff/src/main/java/com/tokopedia/dropoff/ui/dropoff_picker/DropoffPickerViewModel.kt
+++ features/logistic/dropoff/src/main/java/com/tokopedia/dropoff/ui/dropoff_picker/DropoffPickerViewModel.kt
@@ -2,6 +2,8 @@ package com.tokopedia.dropoff.ui.dropoff_picker
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.dropoff.data.query.LocationQuery
 import com.tokopedia.dropoff.data.response.getStore.GetStoreResponse
@@ -13,34 +15,38 @@ import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class DropoffPickerViewModel
-@Inject constructor(dispatcher: CoroutineDispatcher,
-                    private val gql: GraphqlRepository,
-                    private val mapper: GetStoreMapper) : BaseViewModel(dispatcher) {
+@Inject constructor(private val gql: GraphqlRepository,
+                    private val mapper: GetStoreMapper) : ViewModel(), CoroutineScope {
 
     private val mStoreResponse = MutableLiveData<Result<DropoffUiModel>>()
     val storeData: LiveData<Result<DropoffUiModel>>
         get() = mStoreResponse
 
+    override val coroutineContext: CoroutineContext
+        get() = viewModelScope.coroutineContext + onError
+
     fun getStores(latLng: String) {
         val gqlRequest = GraphqlRequest(LocationQuery.keroAddressStoreLocation,
                 GetStoreResponse::class.java, generateQuery(latLng))
         launch {
-            try {
-                val response = gql.getReseponse(listOf(gqlRequest))
-                        .getData<GetStoreResponse>(GetStoreResponse::class.java)
-                        ?: throw Exception("Can't extract object from Graphql Response")
+            val response = gql.getReseponse(listOf(gqlRequest))
+                    .getData<GetStoreResponse>(GetStoreResponse::class.java)
+                    ?: throw Exception("Can't extract object from Graphql Response")
 
-                val stores = mapper.map(response)
-                mStoreResponse.value = Success(stores)
-            } catch (e: Exception) {
-                mStoreResponse.value = Fail(e)
-            }
+            val stores = mapper.map(response)
+            mStoreResponse.value = Success(stores)
         }
+    }
+
+    private val onError = CoroutineExceptionHandler { _, e ->
+        mStoreResponse.value = Fail(e)
     }
 
     private fun generateQuery(latLng: String): Map<String, Any> = mapOf(
