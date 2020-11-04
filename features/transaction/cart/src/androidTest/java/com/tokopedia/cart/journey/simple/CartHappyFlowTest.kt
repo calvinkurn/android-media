a855aab9564d5b6fa83b040f730bd460ccad1806
irfan khoirul <irfankhoirul@gmail.com>
2020-11-04 13:29:51 +0700

    assert inner rv cart success

--- features/transaction/cart/src/androidTest/java/com/tokopedia/cart/journey/simple/CartHappyFlowTest.kt
+++ features/transaction/cart/src/androidTest/java/com/tokopedia/cart/journey/simple/CartHappyFlowTest.kt
@@ -4,15 +4,12 @@ import android.content.Intent
 import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
 import com.tokopedia.cart.InstrumentTestCartActivity
 import com.tokopedia.cart.robot.CartPageRobot
 import com.tokopedia.cart.robot.cartPage
 import com.tokopedia.cart.test.R
-import com.tokopedia.cart.utils.SimpleIdlingResource
 import com.tokopedia.cart.view.viewholder.*
 import com.tokopedia.purchase_platform.common.feature.sellercashback.ShipmentSellerCashbackViewHolder
 import com.tokopedia.purchase_platform.common.feature.tickerannouncement.TickerAnnouncementViewHolder
@@ -36,7 +33,6 @@ class CartHappyFlowTest {
         setupGraphqlMockResponse {
             addMockResponse(GET_CART_LIST_KEY, InstrumentationMockHelper.getRawString(context, R.raw.cart_happy_flow_response), MockModelConfig.FIND_BY_CONTAINS)
         }
-//        IdlingRegistry.getInstance().register(SimpleIdlingResource.countingIdlingResource)
     }
 
     private fun createIntent(): Intent {
@@ -45,12 +41,8 @@ class CartHappyFlowTest {
 
     @Test
     fun happyFlowTest() {
-//        SimpleIdlingResource.countingIdlingResource.dumpStateToLogs()
-//        val message: StringBuilder = StringBuilder("Resource: ").append(SimpleIdlingResource.countingIdlingResource.name).append(" inflight transaction iddle: ").append(SimpleIdlingResource.countingIdlingResource.isIdleNow)
-//        Log.d("CartHappyFlowTest", message.toString())
-
         activityRule.launchActivity(createIntent())
-        Thread.sleep(1000)
+        Thread.sleep(10000)
 
         Log.d("CartHappyFlowTest", "Start Test")
         val cartRecyclerView = activityRule.activity.findViewById<RecyclerView>(R.id.rv_cart)
@@ -58,20 +50,18 @@ class CartHappyFlowTest {
         Log.d("CartHappyFlowTest", "Item Count : " + itemCount)
 
         cartPage {
+            initData(context)
             assertMainContent()
 
             for (i in 0 until itemCount) {
                 Log.d("CartHappyFlowTest", "Check position : " + i)
                 scrollCartRecyclerViewToPosition(cartRecyclerView, i)
-//                Thread.sleep(1000)
                 val breakLoop = checkItemType(cartRecyclerView, i, this)
                 if (breakLoop) break
             }
         } buy {
 
         }
-
-//        Thread.sleep(60000)
     }
 
     private fun checkItemType(cartRecyclerView: RecyclerView, position: Int, cartPageRobot: CartPageRobot): Boolean {
@@ -89,21 +79,21 @@ class CartHappyFlowTest {
                 cartPageRobot.assertCartTickerErrorViewHolder(position, message)
             }
             is CartShopViewHolder -> {
-                Log.d("CartHappyFlowTest", "CartShopViewHolder")
+                Log.d("CartHappyFlowTest", "CartShopViewHolder, position : " + position)
                 when (position) {
                     POSITION_FIRST_CART_SHOP_VIEW_HOLDER -> {
+                        Log.d("CartHappyFlowTest", "CartShopViewHolder: 1")
                         cartPageRobot.assertFirstCartShopViewHolder(
-                                position = position,
-                                shopName = "Miniso Indonesia",
-                                shopLocation = "Kota Surabaya",
-                                productName = "MINISO Kotak Penyimpanan Storage Box Container Organizer Tempat Baju - Merah Muda, L",
-                                productVariant = "Merah Muda, L"
+                                view = activityRule.activity.findViewById(R.id.parent_view_cart),
+                                position = position
                         )
                     }
                     POSITION_SECOND_CART_SHOP_VIEW_HOLDER -> {
+                        Log.d("CartHappyFlowTest", "CartShopViewHolder: 2")
                         cartPageRobot.assertSecondCartShopViewHolder()
                     }
                     POSITION_THIRD_CART_SHOP_VIEW_HOLDER -> {
+                        Log.d("CartHappyFlowTest", "CartShopViewHolder: 3")
                         cartPageRobot.assertThirdCartShopViewHolder()
                     }
                 }
@@ -140,15 +130,14 @@ class CartHappyFlowTest {
 
     @After
     fun tearDown() {
-//        IdlingRegistry.getInstance().unregister(SimpleIdlingResource.countingIdlingResource)
         activityRule.finishActivity()
     }
 
     companion object {
         const val GET_CART_LIST_KEY = "cart_revamp"
 
-        const val POSITION_FIRST_CART_SHOP_VIEW_HOLDER = 2
-        const val POSITION_SECOND_CART_SHOP_VIEW_HOLDER = 3
-        const val POSITION_THIRD_CART_SHOP_VIEW_HOLDER = 4
+        const val POSITION_FIRST_CART_SHOP_VIEW_HOLDER = 1
+        const val POSITION_SECOND_CART_SHOP_VIEW_HOLDER = 2
+        const val POSITION_THIRD_CART_SHOP_VIEW_HOLDER = 3
     }
 }
--- features/transaction/cart/src/androidTest/java/com/tokopedia/cart/robot/CartPageRobot.kt
+++ features/transaction/cart/src/androidTest/java/com/tokopedia/cart/robot/CartPageRobot.kt
@@ -1,38 +1,58 @@
 package com.tokopedia.cart.robot
 
+import android.content.Context
 import android.util.Log
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.PerformException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.google.gson.Gson
+import com.google.gson.JsonArray
 import com.tokopedia.cart.R
+import com.tokopedia.cart.data.model.response.shopgroupsimplified.ShopGroupSimplifiedGqlResponse
+import com.tokopedia.cart.domain.mapper.CartSimplifiedMapper
+import com.tokopedia.cart.domain.model.cartlist.CartListData
 import com.tokopedia.cart.view.viewholder.CartItemViewHolder
 import com.tokopedia.cart.view.viewholder.CartShopViewHolder
 import com.tokopedia.cart.view.viewholder.CartTickerErrorViewHolder
-import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.graphql.CommonUtils
 import com.tokopedia.purchase_platform.common.feature.tickerannouncement.TickerAnnouncementViewHolder
-import com.tokopedia.test.application.espresso_component.CommonMatcher
+import com.tokopedia.test.application.espresso_component.CommonActions
+import com.tokopedia.test.application.util.InstrumentationMockHelper
 import com.tokopedia.unifycomponents.ImageUnify
 import com.tokopedia.unifycomponents.Label
 import com.tokopedia.unifycomponents.ticker.Ticker
 import com.tokopedia.unifyprinciples.Typography
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.junit.Assert
 
 fun cartPage(func: CartPageRobot.() -> Unit) = CartPageRobot().apply(func)
 
 class CartPageRobot {
+
+    var cartListData: CartListData? = null
+
+    fun initData(context: Context) {
+        val jsonString = InstrumentationMockHelper.getRawString(context, com.tokopedia.cart.test.R.raw.cart_happy_flow_response)
+        val jsonArray: JsonArray = CommonUtils.fromJson(
+                jsonString,
+                JsonArray::class.java
+        )
+        val jsonObject = jsonArray.asJsonArray[0].asJsonObject.getAsJsonObject("data")
+        val tmpData = Gson().fromJson(jsonObject, ShopGroupSimplifiedGqlResponse::class.java)
+        cartListData = CartSimplifiedMapper(context).convertToCartItemDataList(tmpData.shopGroupSimplifiedResponse.data)
+    }
 
     infix fun buy(func: ResultRobot.() -> Unit): ResultRobot {
         Log.d("CartHappyFlowTest", "Do buy")
@@ -69,12 +89,10 @@ class CartPageRobot {
         }))
     }
 
-    fun assertFirstCartShopViewHolder(position: Int,
-                                      shopName: String,
-                                      shopLocation: String,
-                                      productName: String,
-                                      productVariant: String) {
+    fun assertFirstCartShopViewHolder(view: View,
+                                      position: Int) {
         Log.d("CartHappyFlowTest", "Start Test First shop item")
+        val shopIndex = position - 1 // Represent shop index on cartListData
         onView(withId(R.id.rv_cart)).perform(RecyclerViewActions.actionOnItemAtPosition<CartShopViewHolder>(position, object : ViewAction {
             override fun getDescription(): String = "performing assertion action on first CartShopViewHolder"
 
@@ -82,59 +100,21 @@ class CartPageRobot {
 
             override fun perform(uiController: UiController?, view: View) {
                 Log.d("CartHappyFlowTest", "Start Assert First shop item")
-                Assert.assertEquals(shopName, view.findViewById<Typography>(R.id.tv_shop_name).text)
-                Assert.assertEquals(shopLocation, view.findViewById<Typography>(R.id.tv_fulfill_district).text)
+                Assert.assertEquals(cartListData?.shopGroupAvailableDataList?.get(shopIndex)?.shopName ?: "", view.findViewById<Typography>(R.id.tv_shop_name).text)
+                Assert.assertEquals(cartListData?.shopGroupAvailableDataList?.get(shopIndex)?.fulfillmentName ?: "", view.findViewById<Typography>(R.id.tv_fulfill_district).text)
                 Assert.assertEquals(View.VISIBLE, view.findViewById<ImageView>(R.id.img_shop_badge).visibility)
                 Assert.assertEquals(View.VISIBLE, view.findViewById<Label>(R.id.label_fulfillment).visibility)
                 Assert.assertEquals(View.VISIBLE, view.findViewById<ImageUnify>(R.id.img_free_shipping).visibility)
                 Assert.assertEquals(View.VISIBLE, view.findViewById<RecyclerView>(R.id.rv_cart_item).visibility)
                 Assert.assertTrue(view.findViewById<CheckBox>(R.id.cb_select_shop).isChecked)
-
-                assertFirstShopFirstCartItemViewHolder(
-                        view = view,
-                        recyclerViewId = R.id.rv_cart_item,
-                        position = position,
-                        productName = productName,
-                        productVariant = productVariant
-                )
             }
         }))
-    }
 
-    private fun assertFirstShopFirstCartItemViewHolder(view: View,
-                                                       recyclerViewId: Int,
-                                                       position: Int,
-                                                       productName: String,
-                                                       productVariant: String) {
-        val childRecyclerView: RecyclerView = view.findViewById(recyclerViewId)
-        val childItemCount = childRecyclerView.adapter?.itemCount ?: 0
-        Log.d("CartHappyFlowTest", "Inner Item Count " + childItemCount)
-
-        for (i in 0 until childItemCount) {
-            try {
-                Log.d("CartHappyFlowTest", "Start Test Inner RV")
-                onView((withId(recyclerViewId)))
-                        .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(i, ViewActions.click()))
-/*
-                        .perform(RecyclerViewActions.actionOnItemAtPosition<CartItemViewHolder>(position, object : ViewAction {
-                            override fun getDescription(): String = "performing assertion action on first CartItemViewHolder"
-
-                            override fun getConstraints(): Matcher<View>? = null
-
-                            override fun perform(uiController: UiController?, view: View) {
-                                Log.d("CartHappyFlowTest", "Assert Inner RV")
-                                Assert.assertEquals(productName, view.findViewById<Typography>(R.id.text_product_name).text)
-                                Assert.assertEquals(productVariant, view.findViewById<Typography>(R.id.text_product_variant).text)
-                                Assert.assertEquals(View.VISIBLE, view.findViewById<ImageView>(R.id.iv_image_product))
-                                Assert.assertTrue(view.findViewById<CheckBox>(R.id.cb_select_item).isChecked)
-                            }
-                        }))
-*/
-            } catch (e: PerformException) {
-                Log.d("CartHappyFlowTest", "Got Exception")
-                e.printStackTrace()
-            }
-        }
+        assertOnEachCartItem(
+                view = view,
+                recyclerViewId = R.id.rv_cart_item,
+                shopIndex = shopIndex
+        )
 
     }
 
@@ -144,6 +124,35 @@ class CartPageRobot {
 
     fun assertThirdCartShopViewHolder() {
 
+    }
+
+    fun assertOnEachCartItem(view: View, recyclerViewId: Int, shopIndex: Int) {
+        val childRecyclerView: RecyclerView = view.findViewById(recyclerViewId)
+
+        val tempStoreDesc = childRecyclerView.contentDescription
+        childRecyclerView.contentDescription = CommonActions.UNDER_TEST_TAG
+
+        val childItemCount = childRecyclerView.adapter!!.itemCount
+        for (i in 0 until childItemCount) {
+            try {
+                onView(allOf(withId(recyclerViewId), withContentDescription(CommonActions.UNDER_TEST_TAG)))
+                        .perform(RecyclerViewActions.actionOnItemAtPosition<CartItemViewHolder>(i, object : ViewAction {
+                            override fun getDescription(): String = "performing assertion action on first CartShopViewHolder"
+
+                            override fun getConstraints(): Matcher<View>? = null
+
+                            override fun perform(uiController: UiController?, view: View) {
+                                Log.d("CartHappyFlowTest", "Start Assert cart item")
+                                Assert.assertEquals(cartListData?.shopGroupAvailableDataList?.get(shopIndex)?.cartItemDataList?.get(i)?.cartItemData?.originData?.productName, view.findViewById<Typography>(R.id.text_product_name).text.toString())
+                                Log.d("CartHappyFlowTest", "Done Assert cart item")
+                            }
+                        }))
+
+            } catch (e: PerformException) {
+                e.printStackTrace()
+            }
+        }
+        childRecyclerView.contentDescription = tempStoreDesc
     }
 
 }
