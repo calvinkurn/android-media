5d36c5f1b6a14277bc1dd2ebcff7394d8b89d596
irfan khoirul <irfankhoirul@gmail.com>
2020-11-05 13:08:35 +0700

    reset shipping id

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentFragment.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentFragment.java
@@ -2289,11 +2289,17 @@ public class ShipmentFragment extends BaseCheckoutFragment implements ShipmentCo
                             if (shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourierTradeInDropOff() != null) {
                                 ordersItem.setShippingId(shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourierTradeInDropOff().getShipperId());
                                 ordersItem.setSpId(shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourierTradeInDropOff().getShipperProductId());
+                            } else {
+                                ordersItem.setShippingId(0);
+                                ordersItem.setSpId(0);
                             }
                         } else {
                             if (shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourier() != null) {
                                 ordersItem.setShippingId(shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourier().getShipperId());
                                 ordersItem.setSpId(shipmentCartItemModel.getSelectedShipmentDetailData().getSelectedCourier().getShipperProductId());
+                            } else {
+                                ordersItem.setShippingId(0);
+                                ordersItem.setSpId(0);
                             }
                         }
                     }
