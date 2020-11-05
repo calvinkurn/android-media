56a276d04957224e8188db3a007322fc5645d1cf
isfaaghyth <isfaaghyth@gmail.com>
2020-11-05 16:00:19 +0700

    implemented span of help care textview on FooterViewHolder

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/FooterViewHolder.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/ui/adapter/viewholder/FooterViewHolder.kt
@@ -3,13 +3,16 @@ package com.tokopedia.troubleshooter.notification.ui.adapter.viewholder
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.applink.RouteManager
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.troubleshooter.notification.R
 import com.tokopedia.troubleshooter.notification.ui.listener.FooterListener
 import com.tokopedia.troubleshooter.notification.ui.uiview.FooterUIView
 import com.tokopedia.troubleshooter.notification.util.gotoDeviceSettings
+import com.tokopedia.troubleshooter.notification.util.setCustomSpan
 import com.tokopedia.unifycomponents.UnifyButton
 
 class FooterViewHolder(
@@ -27,16 +30,8 @@ class FooterViewHolder(
 
     override fun bind(element: FooterUIView?) {
         if (element == null) return
-
-        if (element.isDndMode) {
-            txtStatus?.show()
-            btnAction?.text = context?.getString(R.string.btn_notif_turnoff_dnd)
-            txtMessage?.text = context?.getString(R.string.notif_footer_dnd_message)
-        } else {
-            txtStatus?.hide()
-            btnAction?.text = context?.getString(R.string.btn_notif_clear_cache)
-            txtMessage?.text = context?.getString(R.string.notif_footer_message)
-        }
+        helpCareLabel()
+        footerVisibilityType(element.isDndMode)
 
         btnAction?.setOnClickListener { onActionClicked(element.isDndMode) }
         txtTitle?.setOnClickListener { listener.onInfoClicked() }
@@ -47,6 +42,29 @@ class FooterViewHolder(
                 R.drawable.ic_ts_notif_info,
                 EMPTY_DRAWABLE
         )
+    }
+
+    private fun footerVisibilityType(isDndMode: Boolean) {
+        if (isDndMode) {
+            txtStatus?.show()
+            btnAction?.text = context?.getString(R.string.btn_notif_turnoff_dnd)
+            txtMessage?.text = context?.getString(R.string.notif_footer_dnd_message)
+        } else {
+            txtStatus?.hide()
+            btnAction?.text = context?.getString(R.string.btn_notif_clear_cache)
+            txtMessage?.text = context?.getString(R.string.notif_footer_message)
+        }
+    }
+
+    private fun helpCareLabel() {
+        val visitCareText = context.getString(R.string.notif_footer_visit_care)
+        val unifyGreenColor = ContextCompat.getColor(context, R.color.Unify_G500)
+
+        txtHelpCare?.setCustomSpan(visitCareText, unifyGreenColor)
+
+        txtHelpCare?.setOnClickListener {
+            context.startActivity(RouteManager.getIntent(context, URL_TOKOPEDIA_CARE))
+        }
     }
 
     private fun onActionClicked(isDndMode: Boolean) {
@@ -60,6 +78,7 @@ class FooterViewHolder(
     companion object {
         @LayoutRes val LAYOUT = R.layout.item_footer_message
 
+        private const val URL_TOKOPEDIA_CARE = "https://www.tokopedia.com/help/article/apa-itu-push-notification"
         private const val EMPTY_DRAWABLE = 0
     }
 
