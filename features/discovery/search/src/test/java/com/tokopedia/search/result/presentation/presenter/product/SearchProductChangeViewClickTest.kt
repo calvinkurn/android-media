86d757262a5942bb508419ad8e2e8c0c4d0e5954
glenntuyu <christian.tuyu@tokopedia.com>
2020-11-06 15:28:53 +0700

    add productCountTitle unit test

--- features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/SearchProductChangeViewClickTest.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.search.result.presentation.presenter.product
-
-import com.tokopedia.abstraction.base.view.adapter.Visitable
-import io.mockk.every
-import io.mockk.just
-
-internal class SearchProductChangeViewClickTest: ProductListPresenterTestFixtures() {
-
-}
