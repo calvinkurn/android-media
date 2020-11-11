cb637e8fdaab321ccde139cd406c6c118b88ba1f
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2020-11-11 15:53:07 +0700

    add setting icon

--- features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/icons/IconList.kt
+++ features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/icons/IconList.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.searchbar.navigation_component.icons
 
 import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.searchbar.R
 
@@ -13,6 +14,7 @@ object IconList {
     const val ID_NAV_GLOBAL = IconUnify.MENU_HAMBURGER
     const val ID_WISHLIST = IconUnify.HEART
     const val ID_SHARE = IconUnify.SHARE_MOBILE
+    const val ID_SETTING = IconUnify.SETTING
 
     const val NAME_MESSAGE = "Inbox"
     const val NAME_NOTIFICATION = "Notif"
@@ -20,6 +22,7 @@ object IconList {
     const val NAME_NAV_GLOBAL = "Global Menu"
     const val NAME_WISHLIST = "Wishlist"
     const val NAME_SHARE = "Share"
+    const val NAME_SETTING = "Setting"
 
     const val ID_NAV_LOTTIE_WISHLIST = 91
 
@@ -100,6 +103,19 @@ object IconList {
                     applink = "",
                     disableRouteManager = disableRouteManager,
                     name = NAME_SHARE
+            ) {
+                onClick.invoke()
+            }
+        }
+    }
+
+    internal object SettingIcon: IconConfigItem {
+        override fun get(disableRouteManager: Boolean, onClick: ()-> Unit): IconToolbar {
+            return IconToolbar(
+                    id = ID_SETTING,
+                    applink = ApplinkConstInternalGlobal.GENERAL_SETTING,
+                    disableRouteManager = disableRouteManager,
+                    name = NAME_SETTING
             ) {
                 onClick.invoke()
             }
