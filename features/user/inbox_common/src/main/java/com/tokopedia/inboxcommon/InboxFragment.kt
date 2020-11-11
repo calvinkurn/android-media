80ef2d733608867c8db626581a36a7483fce181a
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-11-11 15:29:27 +0700

    add doc

--- features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/InboxFragment.kt
+++ features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/InboxFragment.kt
@@ -1,5 +1,9 @@
 package com.tokopedia.inboxcommon
 
+/**
+ * interface to communicate activity to fragment
+ * implement this on fragment
+ */
 interface InboxFragment {
     fun onRoleChanged(@RoleType role: Int)
     fun onPageClickedAgain()
--- features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/InboxFragmentContainer.kt
+++ features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/InboxFragmentContainer.kt
@@ -1,5 +1,9 @@
 package com.tokopedia.inboxcommon
 
+/**
+ * interface to communicate fragment to activity
+ * implement this on activity
+ */
 interface InboxFragmentContainer {
     @RoleType
     val role: Int
