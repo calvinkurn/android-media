79ec5d9663fb68347d4b8674204b641267642610
Felicia Widjaja <fwidjaja@live.com>
2020-11-12 11:22:00 +0700

    provide context

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/di/UohListModule.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/di/UohListModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.buyerorder.unifiedhistory.common.di.UohScope
+import com.tokopedia.buyerorder.unifiedhistory.list.view.fragment.UohListFragment
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
@@ -13,11 +14,16 @@ import javax.inject.Named
  */
 @UohListScope
 @Module
-class UohListModule {
+class UohListModule constructor(val uohListFragment: UohListFragment) {
+
+    @UohListScope
+    @Provides
+    fun provideContext(): Context? = uohListFragment.activity
+
     @UohListScope
     @Provides
     @Named("atcMutation")
-    fun provideAddToCartMutation(context: Context): String {
-        return GraphqlHelper.loadRawString(context.resources, com.tokopedia.atc_common.R.raw.mutation_add_to_cart)
+    fun provideAddToCartMutation(context: Context?): String {
+        return GraphqlHelper.loadRawString(context?.resources, com.tokopedia.atc_common.R.raw.mutation_add_to_cart)
     }
 }
