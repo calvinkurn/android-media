0498e0c285779f7b799dd2eea41daaf49fad0ff9
lukaskris12 <lukaskris12@gmail.com>
2020-11-13 08:15:40 +0700

    Remove commend

--- features/discovery/home_nav/src/main/java/com/tokopedia/homenav/view/activity/HomeNavActivity.kt
+++ features/discovery/home_nav/src/main/java/com/tokopedia/homenav/view/activity/HomeNavActivity.kt
@@ -1,16 +1,10 @@
 package com.tokopedia.homenav.view.activity
 
-import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.transition.Slide
-import android.transition.Transition
-import android.view.Gravity
-import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -23,7 +17,6 @@ class HomeNavActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setAnimation()
         overridePendingTransition(R.anim.slide_top, R.anim.nav_fade_out)
         setContentView(R.layout.activity_main_nav)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -40,41 +33,6 @@ class HomeNavActivity: AppCompatActivity() {
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.nav_fade_in, R.anim.slide_bottom)
-    }
-
-    private fun setAnimation() {
-        if (Build.VERSION.SDK_INT > 20) {
-//            val slide = Slide().apply {
-//                slideEdge = Gravity.LEFT
-//                duration = 200
-//                interpolator = FastOutSlowInInterpolator()
-//                addListener(object : Transition.TransitionListener{
-//                    override fun onTransitionStart(p0: Transition?) {
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                            setStatusBarColor(Color.TRANSPARENT)
-//                        }
-//                    }
-//
-//                    override fun onTransitionEnd(p0: Transition?) {
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                            setStatusBarColor(Color.WHITE)
-//                        }
-//                    }
-//
-//                    override fun onTransitionCancel(p0: Transition?) {
-//                    }
-//
-//                    override fun onTransitionPause(p0: Transition?) {
-//                    }
-//
-//                    override fun onTransitionResume(p0: Transition?) {
-//                    }
-//                })
-//            }
-//            window.exitTransition = slide
-//            window.enterTransition = slide
-
-        }
     }
 
     private fun setupNavigation() {
