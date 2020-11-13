7702b885015b5e3a36fd6de6804cc1d8a26f3dbc
resakemal <resa.saharso@tokopedia.com>
2020-11-13 17:23:28 +0700

    Add copy voucher to order list detail

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/fragment/OrderListDetailFragment.java
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/fragment/OrderListDetailFragment.java
@@ -6,12 +6,6 @@ import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -27,6 +21,11 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment;
 import com.tokopedia.applink.RouteManager;
@@ -53,7 +52,7 @@ import com.tokopedia.buyerorder.detail.data.Title;
 import com.tokopedia.buyerorder.detail.data.recommendationPojo.RechargeWidgetResponse;
 import com.tokopedia.buyerorder.detail.di.OrderDetailsComponent;
 import com.tokopedia.buyerorder.detail.view.adapter.RechargeWidgetAdapter;
-import com.tokopedia.buyerorder.detail.view.customview.DetailItemButtonView;
+import com.tokopedia.buyerorder.detail.view.customview.CopyableDetailItemView;
 import com.tokopedia.buyerorder.detail.view.presenter.OrderListDetailContract;
 import com.tokopedia.buyerorder.detail.view.presenter.OrderListDetailPresenter;
 import com.tokopedia.buyerorder.list.data.ConditionalInfo;
@@ -217,12 +216,12 @@ public class OrderListDetailFragment extends BaseDaggerFragment implements Order
     @Override
     public void setDetail(Detail detail) {
         if (VOUCHER_CODE.equalsIgnoreCase(detail.label())) {
-            DetailItemButtonView itemView = new DetailItemButtonView(getActivity());
+            CopyableDetailItemView itemView = new CopyableDetailItemView(getActivity());
             itemView.setTitle(detail.label());
             itemView.setDescription(detail.value());
-            itemView.setListener(new DetailItemButtonView.Listener() {
+            itemView.setListener(new CopyableDetailItemView.Listener() {
                 @Override
-                public void onButtonClick() {
+                public void onCopyValue() {
                     Utils.copyTextToClipBoard("voucher code", detail.value(), getContext());
                     Utils.vibrate(getContext());
                     Toaster.INSTANCE.showNormal(itemView, getString(R.string.title_voucher_code_copied), Toaster.INSTANCE.getToasterLength());
