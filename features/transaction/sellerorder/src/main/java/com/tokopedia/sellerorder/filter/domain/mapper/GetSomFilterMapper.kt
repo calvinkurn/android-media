e7edb563c5faf5d00ba587d0bfc32c346131441f
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-11-17 16:06:03 +0700

    fix ripple effect, show batas respon, remove deadline section, margin product item

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/filter/domain/mapper/GetSomFilterMapper.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/filter/domain/mapper/GetSomFilterMapper.kt
@@ -6,7 +6,6 @@ import com.tokopedia.sellerorder.common.util.SomConsts.CHIPS_SORT_ASC
 import com.tokopedia.sellerorder.common.util.SomConsts.CHIPS_SORT_DESC
 import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_COURIER
 import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_DATE
-import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_DEADLINE
 import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_LABEL
 import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_SORT
 import com.tokopedia.sellerorder.common.util.SomConsts.FILTER_STATUS_ORDER
@@ -15,12 +14,6 @@ import com.tokopedia.sellerorder.common.util.SomConsts.NOT_YET_PRINTED
 import com.tokopedia.sellerorder.common.util.SomConsts.NOT_YET_PRINTED_LABEL
 import com.tokopedia.sellerorder.common.util.SomConsts.SORT_ASCENDING
 import com.tokopedia.sellerorder.common.util.SomConsts.SORT_DESCENDING
-import com.tokopedia.sellerorder.common.util.SomConsts.TODAY
-import com.tokopedia.sellerorder.common.util.SomConsts.TODAY_LABEL
-import com.tokopedia.sellerorder.common.util.SomConsts.TODAY_TOMORROW
-import com.tokopedia.sellerorder.common.util.SomConsts.TODAY_TOMORROW_LABEL
-import com.tokopedia.sellerorder.common.util.SomConsts.TOMORROW
-import com.tokopedia.sellerorder.common.util.SomConsts.TOMORROW_LABEL
 import com.tokopedia.sellerorder.filter.domain.SomFilterResponse
 import com.tokopedia.sellerorder.filter.presentation.model.BaseSomFilter
 import com.tokopedia.sellerorder.filter.presentation.model.SomFilterChipsUiModel
@@ -43,7 +36,6 @@ object GetSomFilterMapper {
             add(SomFilterUiModel(nameFilter = FILTER_TYPE_ORDER, somFilterData = mapToFilterTypeUiModel(data.orderTypeList), canSelectMany =  true, isDividerVisible = true))
             add(SomFilterUiModel(nameFilter = FILTER_COURIER, somFilterData = mapToFilterCourierUiModel(data.orderFilterSom.shippingList), canSelectMany =  true, isDividerVisible = true))
             add(SomFilterUiModel(nameFilter = FILTER_LABEL, somFilterData = mapToFilterLabelUiModel(), canSelectMany = false, isDividerVisible = true))
-            add(SomFilterUiModel(nameFilter = FILTER_DEADLINE, somFilterData =  mapToFilterDeadlineUiModel(), canSelectMany = false, isDividerVisible = true))
         }
     }
 
@@ -51,14 +43,6 @@ object GetSomFilterMapper {
         return mutableListOf<SomFilterChipsUiModel>().apply {
             add(SomFilterChipsUiModel(name = CHIPS_SORT_DESC, key = CHIPS_SORT_DESC, id = SORT_DESCENDING))
             add(SomFilterChipsUiModel(name = CHIPS_SORT_ASC, key = CHIPS_SORT_ASC, id = SORT_ASCENDING))
-        }
-    }
-
-    private fun mapToFilterDeadlineUiModel(): List<SomFilterChipsUiModel> {
-        return mutableListOf<SomFilterChipsUiModel>().apply {
-            add(SomFilterChipsUiModel(name = TODAY_LABEL, key = TODAY_LABEL, id = TODAY))
-            add(SomFilterChipsUiModel(name = TOMORROW_LABEL, key = TOMORROW_LABEL, id = TOMORROW))
-            add(SomFilterChipsUiModel(name = TODAY_TOMORROW_LABEL, key = TODAY_TOMORROW_LABEL, id = TODAY_TOMORROW))
         }
     }
 
