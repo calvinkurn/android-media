96a2e809f191abe0d8253c67854dd7581a62ab8b
faisalramdani <faisal.ramdani@tokopedia.com>
2020-11-18 15:52:32 +0700

    show notif when edit mode only

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
@@ -112,6 +112,7 @@ import com.tokopedia.seller_migration_common.presentation.widget.SellerFeatureCa
 import com.tokopedia.unifycomponents.DividerUnify
 import com.tokopedia.unifycomponents.Toaster
 import com.tokopedia.unifycomponents.selectioncontrol.SwitchUnify
+import com.tokopedia.unifycomponents.ticker.Ticker
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
@@ -133,6 +134,9 @@ class AddEditProductPreviewFragment:
 
     // action button
     private var doneButton: AppCompatTextView? = null
+
+    // notification
+    private var tickerAddEditProductNotification: Ticker? = null
 
     // photo
     private var addEditProductPhotoButton: Typography? = null
@@ -263,6 +267,9 @@ class AddEditProductPreviewFragment:
 
         // action button
         doneButton = activity?.findViewById(R.id.tv_done)
+
+        // action button
+        tickerAddEditProductNotification = activity?.findViewById(R.id.ticker_add_edit_product_notification)
 
         // photos
         productPhotosView = view.findViewById(R.id.rv_product_photos)
@@ -776,6 +783,8 @@ class AddEditProductPreviewFragment:
     private fun displayEditMode() {
         toolbar?.title = getString(R.string.label_title_edit_product)
         doneButton?.show()
+        tickerAddEditProductNotification?.show()
+
         enablePhotoEdit()
         enableDetailEdit()
         enableDescriptionEdit()
