badb6c07b163818d5d6e70df8d7ed00bd584d9b4
leonard2907 <leonard.gunawan@tokopedia.com>
2020-11-18 14:02:01 +0700

    Remove product status checking for OOS

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/datamodel/ProductTickerInfoDataModel.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/datamodel/ProductTickerInfoDataModel.kt
@@ -37,7 +37,7 @@ data class ProductTickerInfoDataModel(
     }
 
     fun isOos(): Boolean {
-        return isOutOfStock && !isProductInCampaign && isProductWarehouse
+        return isOutOfStock && !isProductInCampaign
     }
 
     fun isProductInactive(): Boolean {
