054d29a0ce97ec228a4b9f5700378b8421926e63
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-11-19 12:23:46 +0700

    use dark mode script module inbox

--- features/user/inbox/src/main/java/com/tokopedia/inbox/view/activity/InboxActivity.kt
+++ features/user/inbox/src/main/java/com/tokopedia/inbox/view/activity/InboxActivity.kt
@@ -132,7 +132,9 @@ class InboxActivity : BaseActivity(), InboxConfig.ConfigListener, InboxFragmentC
     private fun setupNotificationBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            window.statusBarColor = Color.WHITE
+            window.statusBarColor = ContextCompat.getColor(
+                    this, com.tokopedia.unifyprinciples.R.color.Unify_N0
+            )
         }
     }
 
