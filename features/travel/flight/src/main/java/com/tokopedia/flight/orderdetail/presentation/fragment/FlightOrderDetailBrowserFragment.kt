7f0c32dcec9191c0d5cba8ebee31800b92846a2f
Muhammad Furqan <furqan@tokopedia.com>
2020-11-19 20:23:16 +0700

    [Flight][Order Detail Revamp] Fix E-Tiket for Android 10

--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailBrowserFragment.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailBrowserFragment.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.flight.orderdetail.presentation.fragment
 
 import android.os.Bundle
+import android.util.Base64
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
@@ -19,7 +20,12 @@ class FlightOrderDetailBrowserFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         htmlContent = arguments?.getString(EXTRA_HTML_CONTENT) ?: ""
+        savedInstanceState?.let {
+            if (it.containsKey(EXTRA_HTML_CONTENT))
+                htmlContent = it.getString(EXTRA_HTML_CONTENT) ?: ""
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
@@ -31,22 +37,25 @@ class FlightOrderDetailBrowserFragment : Fragment() {
         val displayMetrics = DisplayMetrics()
         activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
 
+        htmlContent = Base64.encodeToString(htmlContent.toByteArray(), Base64.DEFAULT)
+
+        wvFlightOrderDetail.settings.javaScriptEnabled = true
         wvFlightOrderDetail.settings.builtInZoomControls = true
         wvFlightOrderDetail.settings.displayZoomControls = false
         wvFlightOrderDetail.settings.loadWithOverviewMode = true
-        wvFlightOrderDetail.setInitialScale(DEFAULT_SCALE)
         wvFlightOrderDetail.loadData(htmlContent, MIME_TYPE, ENCODING)
-        wvFlightOrderDetail.scrollTo(if (displayMetrics.widthPixels > 0) displayMetrics.widthPixels else DEFAULT_X_POSITION, DEFAULT_Y_POSITION)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EXTRA_HTML_CONTENT, htmlContent)
     }
 
     companion object {
         private const val EXTRA_HTML_CONTENT = "EXTRA_HTML_CONTENT"
 
-        private const val DEFAULT_X_POSITION = 1300
-        private const val DEFAULT_Y_POSITION = 0
-        private const val DEFAULT_SCALE = 130
-        private const val MIME_TYPE = "text/html; charset=utf-8"
-        private const val ENCODING = "UTF-8"
+        private const val MIME_TYPE = "text/html; charset=UTF-8"
+        private const val ENCODING = "base64"
 
         fun getInstance(htmlContent: String): FlightOrderDetailBrowserFragment =
                 FlightOrderDetailBrowserFragment().also {
