b3ea6297ff695d359a39438762fb35989e3afbc5
Felicia Widjaja <fwidjaja@live.com>
2020-11-20 15:26:00 +0700

    pass ui test & cassava for all P1 tracking! yeay!

--- features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohListTrackingTest.kt
+++ features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohListTrackingTest.kt
@@ -3,8 +3,8 @@ package com.tokopedia.buyerorder
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.PerformException
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -16,11 +16,9 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.buyerorder.unifiedhistory.common.util.UohIdlingResource
-import com.tokopedia.buyerorder.unifiedhistory.list.view.adapter.viewholder.UohOrderListViewHolder
 import com.tokopedia.cassavatest.getAnalyticsWithQuery
 import com.tokopedia.cassavatest.hasAllSuccess
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
-import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.test.application.util.InstrumentationMockHelper
 import org.junit.*
 import org.junit.Assert.assertThat
@@ -33,7 +31,10 @@ class UohListTrackingTest {
     companion object {
         private const val QUERY_VIEW_LIST = "tracker/transaction/uoh_view_list.json"
         private const val QUERY_SEARCH = "tracker/transaction/uoh_search.json"
-        private const val QUERY_CLICK_ITEM = "tracker/transaction/uoh_click_item.json"
+        private const val QUERY_CLICK_ITEM_LIST = "tracker/transaction/uoh_click_item_list.json"
+        private const val QUERY_CLICK_PRIMARY_BUTTON = "tracker/transaction/uoh_click_primary_button.json"
+        private const val QUERY_CLICK_THREE_DOTS_MENU = "tracker/transaction/uoh_click_three_dots_menu.json"
+        private const val QUERY_ADD_TO_CART = "tracker/transaction/uoh_add_to_cart.json"
         private const val QUERY_CLICK_FILTER_DATE = "tracker/transaction/uoh_click_filter_date_item.json"
         private const val QUERY_APPLY_FILTER_DATE = "tracker/transaction/uoh_apply_filter_date.json"
         private const val QUERY_CLICK_FILTER_STATUS = "tracker/transaction/uoh_click_filter_status_item.json"
@@ -70,19 +71,34 @@ class UohListTrackingTest {
         waitForData()
 
         // need for order detail
-        // login()
-        // waitForData()
+        runBot { login(activityRule) }
+        waitForData()
 
         val uohRecyclerView = activityRule.activity.findViewById<RecyclerView>(com.tokopedia.buyerorder.R.id.rv_order_list)
-        val itemCount = uohRecyclerView.adapter?.itemCount?:0
 
+        // click primary button
+        onView(withId(R.id.rv_order_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, clickOnViewChild(com.tokopedia.buyerorder.R.id.uoh_btn_action)))
+        assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_CLICK_PRIMARY_BUTTON), hasAllSuccess())
+
+        // click three dots menu
+        onView(withId(R.id.rv_order_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, clickOnViewChild(com.tokopedia.buyerorder.R.id.iv_kebab_menu)))
+        assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_CLICK_THREE_DOTS_MENU), hasAllSuccess())
+
+        // click beli lagi
+        onView(withId(com.tokopedia.buyerorder.R.id.rv_kebab)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_ADD_TO_CART), hasAllSuccess())
+
+        // click order card
+        onView(withId(R.id.rv_order_list)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, clickOnViewChild(com.tokopedia.buyerorder.R.id.cl_data_product)))
+        assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_CLICK_ITEM_LIST), hasAllSuccess())
+        pressBack()
+
+        val itemCount = uohRecyclerView.adapter?.itemCount?:0
         for (i in 0 until itemCount) {
             scrollUohRecyclerViewToPosition(uohRecyclerView, i)
-            // clickOrderItem(uohRecyclerView, i)
         }
         waitForData()
         assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_VIEW_LIST), hasAllSuccess())
-        // assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, QUERY_CLICK_ITEM), hasAllSuccess())
 
         // search
         onView(withId(com.tokopedia.unifycomponents.R.id.searchbar_textfield)).perform(ViewActions.typeText("product 17"))
@@ -127,31 +143,5 @@ class UohListTrackingTest {
     private fun scrollUohRecyclerViewToPosition(uohRecyclerView: RecyclerView, position: Int) {
         val layoutManager = uohRecyclerView.layoutManager as GridLayoutManager
         activityRule.runOnUiThread { layoutManager.scrollToPositionWithOffset(position, 0) }
-    }
-
-    private fun login() {
-        InstrumentationAuthHelper.loginToAnUser(activityRule.activity.application)
-    }
-
-    private fun clickOrderItem(uohRecyclerView: RecyclerView, i: Int) {
-        when (uohRecyclerView.findViewHolderForAdapterPosition(i)) {
-            is UohOrderListViewHolder -> {
-                try {
-                    onView(withId(uohRecyclerView.id))
-                            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(i, ViewActions.click()))
-                } catch (e: PerformException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-    }
-
-    private fun clickFilterOptionItem(uohRecyclerView: RecyclerView, i: Int) {
-        try {
-            onView(withId(uohRecyclerView.id))
-                    .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(i, ViewActions.click()))
-        } catch (e: PerformException) {
-            e.printStackTrace()
-        }
     }
 }
--- features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohRobot.kt
+++ features/transaction/buyerorder/src/androidTest/java/com/tokopedia/buyerorder/UohRobot.kt
@@ -1,26 +1,25 @@
 package com.tokopedia.buyerorder
 
+import android.content.res.Resources
 import android.view.View
 import android.view.ViewGroup
-import android.widget.HorizontalScrollView
-import android.widget.ScrollView
-import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ScrollToAction
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Checks
+import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.buyerorder.unifiedhistory.list.view.activity.UohListActivity
+import com.tokopedia.buyerorder.unifiedhistory.list.view.adapter.viewholder.UohOrderListViewHolder
 import com.tokopedia.cassavatest.getAnalyticsWithQuery
 import com.tokopedia.cassavatest.hasAllSuccess
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
-import org.hamcrest.CoreMatchers.allOf
+import com.tokopedia.unifycomponents.UnifyButton
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert
-import org.hamcrest.Matchers.anyOf
 import org.hamcrest.TypeSafeMatcher
 
 
@@ -39,10 +38,6 @@ class UohRobot {
 
 fun runBot(func: UohRobot.() -> Unit) = UohRobot().apply(func)
 
-fun submit(func: ResultRobot.() -> Unit): ResultRobot {
-    return ResultRobot().apply(func)
-}
-
 fun nthChildOf(parentMatcher: Matcher<View?>, childPosition: Int): Matcher<View?>? {
     return object : TypeSafeMatcher<View?>() {
         override fun describeTo(description: Description) {
@@ -59,26 +54,10 @@ fun nthChildOf(parentMatcher: Matcher<View?>, childPosition: Int): Matcher<View?
     }
 }
 
-var customScrollTo: ViewAction = object : ViewAction {
-    override fun getConstraints(): Matcher<View> {
-        return allOf(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE), isDescendantOfA(anyOf(
-                isAssignableFrom(ScrollView::class.java),
-                isAssignableFrom(HorizontalScrollView::class.java),
-                isAssignableFrom(NestedScrollView::class.java)))
-        )
-    }
+fun clickOnViewChild(viewId: Int) = object : ViewAction {
+    override fun getConstraints() = null
 
-    override fun getDescription(): String? {
-        return null
-    }
+    override fun getDescription() = "Click on a child view with specified id."
 
-    override fun perform(uiController: UiController?, view: View?) {
-        ScrollToAction().perform(uiController, view)
-    }
-}
-
-class ResultRobot {
-    fun hasPassedAnalytics(repository: GtmLogDBSource, queryString: String) {
-        MatcherAssert.assertThat(getAnalyticsWithQuery(repository, queryString), hasAllSuccess())
-    }
+    override fun perform(uiController: UiController, view: View) = click().perform(uiController, view.findViewById<View>(viewId))
 }
