35022aafc1a4ca3511503420fc16f97cb5b12b20
Abhijeet <abhijeet.chauhan@tokopedia.com>
2020-11-23 11:21:13 +0530

    PDP simulation: Paylater module created

---
+++ features/fintech/paylater/src/main/java/com/tokopedia/paylater/presentation/activity/PaylaterActivity.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.paylater.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokopedia.paylater.R
+
+class PaylaterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paylater)
+    }
+}
