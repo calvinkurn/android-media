da852e2554d77a21eaa133b93aa280aea7b3eb5b
isfaaghyth <isfaaghyth@gmail.com>
2020-11-26 00:29:01 +0700

    moved test util into inbox_common module

--- features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/util/FileUtil.kt
+++ features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/util/FileUtil.kt
@@ -1,24 +1,20 @@
-package com.tokopedia.inbox.viewmodel.util
+package com.tokopedia.inboxcommon.util
 
 import java.io.File
 import java.lang.reflect.Type
 
 object FileUtil {
-
     private fun readFileContent(fileName: String): String {
         val fileContent = StringBuilder()
         val filePath = javaClass.getResource(fileName)?.path
-        filePath?.let {
-            File(it).forEachLine { content ->
-                fileContent.append(content)
-            }
+        File(filePath).forEachLine {
+            fileContent.append(it)
         }
         return fileContent.toString()
     }
 
-    fun <T> parse(fileName: String, typeOfT: Type): T? {
+    fun <T> parse(fileName: String, typeOfT: Type): T {
         val stringFile = readFileContent(fileName)
         return CommonUtil.fromJson(stringFile, typeOfT)
     }
-
 }
