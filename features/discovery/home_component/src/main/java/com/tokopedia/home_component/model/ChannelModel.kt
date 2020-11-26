344385eff077f040785c58c86f11075eb24aae83
yoasfs <yoas.susantio@tokopedia.com>
2020-11-26 08:33:10 +0700

    update class 2

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/model/ChannelModel.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/model/ChannelModel.kt
@@ -6,7 +6,6 @@ data class ChannelModel(
         val id: String,
         val groupId: String,
         val type: String = "",
-        val layout: String = "",
         var verticalPosition: Int = 0,
         val contextualInfo: Int = 0,
         val widgetParam: String = "",
