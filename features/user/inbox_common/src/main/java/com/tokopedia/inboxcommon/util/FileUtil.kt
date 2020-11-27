b2bce95354e15de39e79b8a1ab8f63ec7b99be5d
isfaaghyth <isfaaghyth@gmail.com>
2020-11-27 07:58:15 +0700

    fixed nil warning of mock response reader

--- features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/util/FileUtil.kt
+++ features/user/inbox_common/src/main/java/com/tokopedia/inboxcommon/util/FileUtil.kt
@@ -6,9 +6,10 @@ import java.lang.reflect.Type
 object FileUtil {
     private fun readFileContent(fileName: String): String {
         val fileContent = StringBuilder()
-        val filePath = javaClass.getResource(fileName)?.path
-        File(filePath).forEachLine {
-            fileContent.append(it)
+        javaClass.getResource(fileName)?.path?.let { filePath ->
+            File(filePath).forEachLine {
+                fileContent.append(it)
+            }
         }
         return fileContent.toString()
     }
