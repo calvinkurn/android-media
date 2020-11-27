b194405c545e2f4f7245e85ec323933b415a6e0d
Jessica Sean <jessica.sean@tokopedia.com>
2020-11-27 09:04:34 +0700

    add TravelHomepageActivityTest.kt

---
+++ features/travel/travel_homepage/src/androidTest/java/com/tokopedia/travelhomepage/homepage/presentation/activity/TravelHomepageMockResponseConstant.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.travelhomepage.homepage.presentation.activity
+
+/**
+ * @author by jessica on 26/11/20
+ */
+
+object TravelHomepageMockResponseConstant {
+    const val KEY_TRAVEL_SUBHOME_GET_LAYOUT = "travelLayoutSubhomepage"
+    const val KEY_TRAVEL_SUBHOME_GET_ITEM =  "TravelGetDynamicSubhomepage"
+}
