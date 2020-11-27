773ef7b9de10566e773311d41ecd9c94c0664a0e
mzennis <meyta.taliti@tokopedia.com>
2020-11-27 23:43:38 +0700

    enter pip mode when click product detail

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
@@ -273,6 +273,7 @@ class PlayBottomSheetFragment @Inject constructor(
         if (product.applink != null && product.applink.isNotEmpty()) {
             PlayAnalytics.clickProduct(trackingQueue, channelId, product, position, playViewModel.channelType)
             openPageByApplink(product.applink)
+            playViewModel.goPiP()
         }
     }
 
