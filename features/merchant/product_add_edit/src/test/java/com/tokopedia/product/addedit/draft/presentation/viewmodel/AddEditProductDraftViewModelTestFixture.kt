c3b057e5bd67b17ed51a3f3d0e9a4c54937cd7f0
saidfaisal <said.faisal@tokopedia.com>
2020-11-27 07:15:58 +0700

    add ut for AddEditProductDraftViewModel.kt

---
+++ features/merchant/product_add_edit/src/test/java/com/tokopedia/product/addedit/draft/presentation/viewmodel/AddEditProductDraftViewModelTestFixture.kt
@@ -0,0 +1,48 @@
+package com.tokopedia.product.addedit.draft.presentation.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.product.addedit.draft.domain.usecase.*
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Before
+import org.junit.Rule
+import org.junit.jupiter.api.AfterEach
+import kotlin.jvm.Throws
+
+@ExperimentalCoroutinesApi
+abstract class AddEditProductDraftViewModelTestFixture {
+    @get:Rule
+    val instantTaskExcecutorRule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
+    lateinit var deleteAllProductDraftUseCase: DeleteAllProductDraftUseCase
+
+    @RelaxedMockK
+    lateinit var deleteProductDraftUseCase: DeleteProductDraftUseCase
+
+    @RelaxedMockK
+    lateinit var getAllProductDraftUseCase: GetAllProductDraftUseCase
+
+    protected val viewModel: AddEditProductDraftViewModel by lazy {
+        AddEditProductDraftViewModel(
+                TestCoroutineDispatcher(),
+                deleteProductDraftUseCase,
+                deleteAllProductDraftUseCase,
+                getAllProductDraftUseCase
+        )
+    }
+
+    @Before
+    @Throws(Exception::class)
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        clearAllMocks()
+    }
+}
