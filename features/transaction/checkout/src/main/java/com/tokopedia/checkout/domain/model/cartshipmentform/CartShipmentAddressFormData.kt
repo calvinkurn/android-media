a0c3cc9676ea02cf2cbdd3652189dcad64fc0e67
irfan khoirul <irfankhoirul@gmail.com>
2020-11-30 10:03:45 +0700

    change to parcelize & implement parcelable

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/model/cartshipmentform/CartShipmentAddressFormData.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/model/cartshipmentform/CartShipmentAddressFormData.kt
@@ -1,20 +1,19 @@
 package com.tokopedia.checkout.domain.model.cartshipmentform
 
-import android.os.Parcel
 import android.os.Parcelable
-import com.tokopedia.checkout.domain.model.cartshipmentform.AddressesData
-import com.tokopedia.checkout.domain.model.cartshipmentform.DisabledFeaturesDetailData
 import com.tokopedia.checkout.view.uimodel.EgoldAttributeModel
 import com.tokopedia.logisticcart.shipping.model.CodModel
 import com.tokopedia.purchase_platform.common.feature.button.ABTestButton
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.PromoCheckoutErrorDefault
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUiModel
 import com.tokopedia.purchase_platform.common.feature.tickerannouncement.TickerData
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author anggaprasetiyo on 21/02/18.
  */
 
+@Parcelize
 data class CartShipmentAddressFormData(
         var isHasError: Boolean = false,
         var isError: Boolean = false,
@@ -60,76 +59,4 @@ data class CartShipmentAddressFormData(
             }
             return false
         }
-
-    constructor(parcel: Parcel) : this() {
-        isHasError = parcel.readByte() != 0.toByte()
-        isError = parcel.readByte() != 0.toByte()
-        errorMessage = parcel.readString()
-        errorCode = parcel.readInt()
-        isShowOnboarding = parcel.readByte() != 0.toByte()
-        isDropshipperDisable = parcel.readByte() != 0.toByte()
-        isOrderPrioritasDisable = parcel.readByte() != 0.toByte()
-        groupAddress = parcel.createTypedArrayList(GroupAddress.CREATOR) ?: ArrayList()
-        keroToken = parcel.readString()
-        keroDiscomToken = parcel.readString()
-        keroUnixTime = parcel.readInt()
-        donation = parcel.readParcelable(Donation::class.java.classLoader)
-        isUseCourierRecommendation = parcel.readByte() != 0.toByte()
-        isHidingCourier = parcel.readByte() != 0.toByte()
-        isBlackbox = parcel.readByte() != 0.toByte()
-        egoldAttributes = parcel.readParcelable(EgoldAttributeModel::class.java.classLoader)
-        isIneligiblePromoDialogEnabled = parcel.readByte() != 0.toByte()
-        tickerData = parcel.readParcelable(TickerData::class.java.classLoader)
-        addressesData = parcel.readParcelable(AddressesData::class.java.classLoader)
-        disabledFeaturesDetailData = parcel.readParcelable(DisabledFeaturesDetailData::class.java.classLoader)
-        campaignTimerUi = parcel.readParcelable(CampaignTimerUi::class.java.classLoader)
-        lastApplyData = parcel.readParcelable(LastApplyUiModel::class.java.classLoader)
-        promoCheckoutErrorDefault = parcel.readParcelable(PromoCheckoutErrorDefault::class.java.classLoader)
-        isOpenPrerequisiteSite = parcel.readByte() != 0.toByte()
-        isEligibleNewShippingExperience = parcel.readByte() != 0.toByte()
-        abTestButton = parcel.readParcelable(ABTestButton::class.java.classLoader) ?: ABTestButton()
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeByte(if (isHasError) 1 else 0)
-        parcel.writeByte(if (isError) 1 else 0)
-        parcel.writeString(errorMessage)
-        parcel.writeInt(errorCode)
-        parcel.writeByte(if (isShowOnboarding) 1 else 0)
-        parcel.writeByte(if (isDropshipperDisable) 1 else 0)
-        parcel.writeByte(if (isOrderPrioritasDisable) 1 else 0)
-        parcel.writeTypedList(groupAddress)
-        parcel.writeString(keroToken)
-        parcel.writeString(keroDiscomToken)
-        parcel.writeInt(keroUnixTime)
-        parcel.writeParcelable(donation, flags)
-        parcel.writeByte(if (isUseCourierRecommendation) 1 else 0)
-        parcel.writeByte(if (isHidingCourier) 1 else 0)
-        parcel.writeByte(if (isBlackbox) 1 else 0)
-        parcel.writeParcelable(egoldAttributes, flags)
-        parcel.writeByte(if (isIneligiblePromoDialogEnabled) 1 else 0)
-        parcel.writeParcelable(tickerData, flags)
-        parcel.writeParcelable(addressesData, flags)
-        parcel.writeParcelable(disabledFeaturesDetailData, flags)
-        parcel.writeParcelable(campaignTimerUi, flags)
-        parcel.writeParcelable(lastApplyData, flags)
-        parcel.writeParcelable(promoCheckoutErrorDefault, flags)
-        parcel.writeByte(if (isOpenPrerequisiteSite) 1 else 0)
-        parcel.writeByte(if (isEligibleNewShippingExperience) 1 else 0)
-        parcel.writeParcelable(abTestButton, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<CartShipmentAddressFormData> {
-        override fun createFromParcel(parcel: Parcel): CartShipmentAddressFormData {
-            return CartShipmentAddressFormData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CartShipmentAddressFormData?> {
-            return arrayOfNulls(size)
-        }
-    }
 }
