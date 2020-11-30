9d7822983b43d873e6a63b1b49d83ff301e76410
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-11-30 09:56:03 +0700

    add getShopLocation query

---
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/data/query/ShopLocationQuery.kt
@@ -0,0 +1,53 @@
+package com.tokopedia.manageaddress.data.query
+
+object ShopLocationQuery {
+
+    val getShopLocation = """
+        query ShopGetAllLocations(${'$'}shop_id: Int!) {
+          ShopLocGetAllLocations(
+            input: {
+                shop_id: ${'$'}shop_id
+            }
+          ) {
+            status
+            message
+            error {
+              id
+              description
+            }
+            data {
+              warehouses {
+                warehouse_id
+                warehouse_name
+                warehouse_type
+                shop_id {
+                  int64
+                  valid
+                }
+                partner_id {
+                  int64
+                  valid
+                }
+                address_detail
+                postal_code
+                latlon
+                district_id
+                district_name
+                city_id
+                city_name
+                province_id
+                province_name
+                country
+                status
+                is_covered_by_couriers
+                ticker {
+                  text_inactive
+                  text_courier_setting
+                  link_courier_setting
+                }
+              }
+            }
+          }
+        }
+    """.trimIndent()
+}
