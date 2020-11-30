800135785aa422c3c8de13ed03521789b474285f
kenny <kenny.hadisaputra@tokopedia.com>
2020-11-30 16:57:32 +0700

    handle ui model for broadcast schedule

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/partial/BroadcastScheduleViewComponent.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/partial/BroadcastScheduleViewComponent.kt
@@ -1,7 +1,13 @@
 package com.tokopedia.play.broadcaster.view.partial
 
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.IdRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.play.broadcaster.R
+import com.tokopedia.play.broadcaster.ui.model.BroadcastScheduleUiModel
 import com.tokopedia.play_common.viewcomponent.ViewComponent
 
 /**
@@ -12,8 +18,34 @@ class BroadcastScheduleViewComponent(
         @IdRes idRes: Int
 ) : ViewComponent(container, idRes) {
 
-    init {
+    private val clScheduleHeader = findViewById<ConstraintLayout>(R.id.cl_schedule_header)
+    private val clScheduleDesc = findViewById<ConstraintLayout>(R.id.cl_schedule_desc)
 
+    private val tvHeader = findViewById<TextView>(R.id.tv_header)
+    private val iconHeaderEdit = findViewById<IconUnify>(R.id.icon_header_edit)
+
+    private val tvDescSchedule = findViewById<TextView>(R.id.tv_desc_schedule)
+
+    fun setSchedule(scheduleModel: BroadcastScheduleUiModel) {
+        when (scheduleModel) {
+            BroadcastScheduleUiModel.NoSchedule -> setNoSchedule()
+            is BroadcastScheduleUiModel.Scheduled -> setScheduledBroadcast(scheduleModel)
+        }
+    }
+
+    private fun setNoSchedule() {
+        iconHeaderEdit.visibility = View.VISIBLE
+        tvHeader.text = getString(R.string.play_broadcast_add_schedule_info)
+
+        clScheduleDesc.visibility = View.GONE
+    }
+
+    private fun setScheduledBroadcast(model: BroadcastScheduleUiModel.Scheduled) {
+        iconHeaderEdit.visibility = View.GONE
+        tvHeader.text = getString(R.string.play_broadcast_schedule_set_info)
+
+        clScheduleDesc.visibility = View.VISIBLE
+        tvDescSchedule.text = model.formattedTime
     }
 
     interface Listener {
