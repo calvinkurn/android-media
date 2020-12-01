d054acf46039f99739dcfb2409cea4cb7f5e5dd9
faisalramdani <faisal.ramdani@tokopedia.com>
2020-12-01 17:09:54 +0700

    hid done button when loading

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
@@ -1246,9 +1246,11 @@ class AddEditProductPreviewFragment:
 
     private fun showLoading() {
         loadingLayout?.show()
+        doneButton?.show()
     }
 
     private fun hideLoading() {
+        doneButton?.hide()
         loadingLayout?.transitionToEnd()
         loadingLayout?.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
