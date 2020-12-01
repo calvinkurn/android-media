5beb5f19290ec6e067c23f0163a37b08ff80e61f
hansenpw <hansen.wijaya@tokopedia.com>
2020-12-01 14:22:20 +0700

    Add occ revamp analytics test

--- features/transaction/oneclickcheckout/src/androidTest/java/com/tokopedia/oneclickcheckout/common/interceptor/CartTestInterceptor.kt
+++ features/transaction/oneclickcheckout/src/androidTest/java/com/tokopedia/oneclickcheckout/common/interceptor/CartTestInterceptor.kt
@@ -74,5 +74,9 @@ const val GET_OCC_CART_PAGE_OVO_ACTIVATED_RESPONSE_PATH = "cart/get_occ_cart_pag
 const val GET_OCC_CART_PAGE_OVO_LOW_WALLET_RESPONSE_PATH = "cart/get_occ_cart_page_ovo_low_wallet_response.json"
 const val GET_OCC_CART_PAGE_OVO_TOP_UP_RESPONSE_PATH = "cart/get_occ_cart_page_ovo_top_up_response.json"
 
+const val GET_OCC_CART_PAGE_NO_PROFILE_REVAMP_RESPONSE_PATH = "cart/get_occ_cart_page_no_profile_revamp_response.json"
+const val GET_OCC_CART_PAGE_ONE_PROFILE_REVAMP_RESPONSE_PATH = "cart/get_occ_cart_page_one_profile_revamp_response.json"
+const val GET_OCC_CART_PAGE_MANY_PROFILE_REVAMP_RESPONSE_PATH = "cart/get_occ_cart_page_many_profile_revamp_response.json"
+
 const val UPDATE_CART_OCC_SUCCESS_RESPONSE_PATH = "cart/update_cart_occ_success_response.json"
 const val UPDATE_CART_OCC_DIALOG_PROMPT_RESPONSE_PATH = "cart/update_cart_occ_dialog_prompt_response.json"
