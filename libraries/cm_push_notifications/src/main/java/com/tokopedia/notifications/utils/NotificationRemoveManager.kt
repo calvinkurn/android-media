f9c8282f7ca6ccdb12a2a5bb8426fbe8b6ab320f
isfaaghyth <isfaaghyth@gmail.com>
2020-12-01 10:47:55 +0700

    added clear notification manager on removeManager

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/NotificationRemoveManager.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/NotificationRemoveManager.kt
@@ -1,6 +1,9 @@
 package com.tokopedia.notifications.utils
 
+import android.app.NotificationManager
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import androidx.core.app.NotificationManagerCompat
 import com.tokopedia.notifications.model.BaseNotificationModel
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
@@ -15,14 +18,11 @@ open class NotificationRemoveManager(
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + jobs
 
-    fun notificationIdToClear(invoke: (List<BaseNotificationModel>) -> Unit) {
-        launch {
-            val notifications = pushRepository(context)
-                    .getNotification()
-                    .intersect(excludeListByCampaignId) { a, b ->
-                        a.campaignId == b
-                    }
-            withContext(Dispatchers.Main) { invoke(notifications) }
+    fun clearNotification(context: Context) {
+        notificationIdToClear { elements ->
+            elements.forEach {
+                clearNotificationManager(context, it.notificationId)
+            }
         }
     }
 
@@ -30,13 +30,30 @@ open class NotificationRemoveManager(
         jobs.cancelChildren()
     }
 
+    private fun notificationIdToClear(invoke: (List<BaseNotificationModel>) -> Unit) {
+        launch {
+            val notifications = pushRepository(context)
+                    .getNotification()
+                    .intersect(excludeListByCampaignId) { notification, excludedItem ->
+                        notification.campaignId != excludedItem
+                    }
+            withContext(Dispatchers.Main) { invoke(notifications) }
+        }
+    }
+
+    private fun clearNotificationManager(context: Context, notificationId: Int) {
+        (context.getSystemService(NOTIFICATION_SERVICE) as? NotificationManager?)
+                ?.let {
+                    it.cancel(notificationId)
+                }
+
+        NotificationManagerCompat
+                .from(context)
+                .cancel(notificationId)
+    }
+
     companion object {
         private val excludeListByCampaignId = listOf(0L)
-
-        private fun <T, U> List<T>.intersect(
-                uList: List<U>,
-                filterPredicate : (T, U) -> Boolean
-        ) = filter { m -> uList.any { filterPredicate(m, it)} }
     }
 
 }
