bbb0372f97254799886b2836682359376f329a44
isfaaghyth <isfaaghyth@gmail.com>
2020-12-01 10:36:32 +0700

    added intersect extensions to filter two list

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/Extensions.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/Extensions.kt
@@ -4,3 +4,8 @@ fun <T> ArrayList<T>.onlyOne(): Boolean {
     if (size == 1) return true
     return false
 }
+
+fun <T, U> List<T>.intersect(
+        uList: List<U>,
+        filterPredicate : (T, U) -> Boolean
+) = filter { m -> uList.any { filterPredicate(m, it)} }
