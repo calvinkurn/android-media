96e220d22b39b5afc2d0c9da1f01b9176839fc60
isfaaghyth <isfaaghyth@gmail.com>
2020-12-01 10:37:47 +0700

    provided injection of lifecycleModule

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/di/CMNotificationComponent.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/di/CMNotificationComponent.kt
@@ -1,18 +1,20 @@
 package com.tokopedia.notifications.di
 
 import com.tokopedia.notifications.di.module.GraphQueryModule
+import com.tokopedia.notifications.di.module.LifecycleModule
 import com.tokopedia.notifications.di.module.NotificationModule
 import com.tokopedia.notifications.di.scope.CMNotificationScope
-import com.tokopedia.notifications.inApp.CMInAppManager
+import com.tokopedia.notifications.inApp.viewEngine.CMActivityLifeCycle
 import com.tokopedia.notifications.receiver.CMBroadcastReceiver
 import dagger.Component
 
 @CMNotificationScope
 @Component(modules = [
     GraphQueryModule::class,
-    NotificationModule::class
+    NotificationModule::class,
+    LifecycleModule::class
 ])
 interface CMNotificationComponent {
     fun inject(broadcast: CMBroadcastReceiver)
-    fun inject(manager: CMInAppManager)
+    fun inject(lifecycle: CMActivityLifeCycle)
 }
