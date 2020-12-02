4581d1ef9fe66574a40faf1a4306a0ed6b11dc1a
faisalramdani <faisal.ramdani@tokopedia.com>
2020-12-02 10:14:51 +0700

    fix done button visibility

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/presentation/fragment/AddEditProductPreviewFragment.kt
@@ -1246,11 +1246,11 @@ class AddEditProductPreviewFragment:
 
     private fun showLoading() {
         loadingLayout?.show()
-        doneButton?.show()
+        doneButton?.hide()
     }
 
     private fun hideLoading() {
-        doneButton?.hide()
+        doneButton?.show()
         loadingLayout?.transitionToEnd()
         loadingLayout?.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
