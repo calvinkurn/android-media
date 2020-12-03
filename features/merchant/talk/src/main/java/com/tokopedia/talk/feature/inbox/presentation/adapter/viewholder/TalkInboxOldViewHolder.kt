6daf4f0bd8666bd113f6de6b9f2f7a48e989a4dc
leonard2907 <leonard.gunawan@tokopedia.com>
2020-12-03 07:47:03 +0700

    Fix package name

--- features/merchant/talk/src/main/java/com/tokopedia/talk/feature/inbox/presentation/adapter/viewholder/TalkInboxOldViewHolder.kt
+++ features/merchant/talk/src/main/java/com/tokopedia/talk/feature/inbox/presentation/adapter/viewholder/TalkInboxOldViewHolder.kt
@@ -10,7 +10,7 @@ import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.kotlin.model.ImpressHolder
 import com.tokopedia.talk.feature.inbox.presentation.adapter.uimodel.TalkInboxOldUiModel
 import com.tokopedia.talk.feature.inbox.presentation.listener.TalkInboxViewHolderListener
-import com.tokopedia.talk_old.R
+import com.tokopedia.talk.R
 import com.tokopedia.unifyprinciples.Typography
 import kotlinx.android.synthetic.main.item_talk_inbox.view.*
 
