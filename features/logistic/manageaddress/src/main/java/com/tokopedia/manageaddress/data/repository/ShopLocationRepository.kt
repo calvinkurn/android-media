13d25c341046feda827f14a9e865e844db1e2fa5
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2020-12-03 11:51:03 +0700

    add getShopLocation repository

---
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/data/repository/ShopLocationRepository.kt
@@ -0,0 +1,20 @@
+package com.tokopedia.manageaddress.data.repository
+
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.manageaddress.data.query.ShopLocationQuery
+import com.tokopedia.manageaddress.domain.response.shoplocation.GetShopLocationResponse
+import com.tokopedia.manageaddress.util.getResponse
+import javax.inject.Inject
+
+class ShopLocationRepository @Inject constructor(private val gql: GraphqlRepository) {
+
+    suspend fun getShopLocation(shopId: Int): GetShopLocationResponse {
+        val param = mapOf("param" to shopId)
+        val request = GraphqlRequest(ShopLocationQuery.getShopLocation,
+                GetShopLocationResponse::class.java, param)
+        return gql.getResponse(request)
+    }
+
+
+}
---
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/util/GqlExt.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.manageaddress.util
+
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlRequest
+
+internal suspend inline fun <reified T> GraphqlRepository.getResponse(request: GraphqlRequest): T {
+    return getReseponse(listOf(request)).getSuccessData<T>()
+            ?: throw NullPointerException("Data with your type might not exist")
+}
