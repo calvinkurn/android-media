f3903f6b07c60765affff40dcd7ef956d2eab961
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-12-04 15:58:57 +0800

    rename chat worker

---
+++ features/merchant/seller_app_widget/src/main/java/com/tokopedia/sellerappwidget/view/work/GetChatWorker.kt
@@ -0,0 +1,48 @@
+package com.tokopedia.sellerappwidget.view.work
+
+import android.content.Context
+import androidx.work.*
+import com.tokopedia.sellerappwidget.view.service.GetChatService
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created By @ilhamsuaib on 01/12/20
+ */
+
+class GetChatWorker(
+        private val context: Context,
+        workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    companion object {
+        private const val TAG_WORKER = "get_chat_worker"
+        private const val REPEAT_INTERVAL = 900L
+        private var workRequest: OneTimeWorkRequest? = null
+
+        @JvmStatic
+        fun runWorkerPeriodically(context: Context) {
+            if (workRequest == null) {
+                val constraints = Constraints.Builder()
+                        .setRequiresDeviceIdle(false)
+                        .setRequiredNetworkType(NetworkType.CONNECTED)
+                        .build()
+
+                workRequest = OneTimeWorkRequestBuilder<GetChatWorker>()
+                        .setConstraints(constraints)
+                        .setInitialDelay(REPEAT_INTERVAL, TimeUnit.SECONDS)
+                        .build()
+            }
+
+            workRequest?.let {
+                WorkManager.getInstance(context)
+                        .beginUniqueWork(TAG_WORKER, ExistingWorkPolicy.REPLACE, it)
+                        .enqueue()
+            }
+        }
+    }
+
+    override fun doWork(): Result {
+        GetChatService.startService(context)
+        return Result.success()
+    }
+}
