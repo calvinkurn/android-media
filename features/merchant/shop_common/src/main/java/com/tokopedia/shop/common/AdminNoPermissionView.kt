1d6e90825c8359d9e8cf6c82d9d14454ea75266f
Reivin <reivin.oktavianus@tokopedia.com>
2020-12-04 18:39:11 +0700

    Download png instead of using drawable

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/AdminNoPermissionView.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/AdminNoPermissionView.kt
@@ -3,7 +3,9 @@ package com.tokopedia.shop.common
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.shop.common.constant.SellerHomePermissionGroup
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
@@ -12,6 +14,10 @@ class AdminNoPermissionView @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0): ConstraintLayout(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val ERROR_ILLUSTRATION_URL = "https://ecs7.tokopedia.net/android/others/ic_admin_no_permission.png"
+    }
 
     init {
         setupView()
@@ -38,6 +44,10 @@ class AdminNoPermissionView @JvmOverloads constructor(
         titleTextView = findViewById(R.id.tv_admin_no_permission_title)
         descTextView = findViewById(R.id.tv_admin_no_permission_desc)
         actionButton = findViewById(R.id.btn_admin_no_permission)
+
+        findViewById<AppCompatImageView>(R.id.iv_admin_no_permission)?.let { imageView ->
+            ImageHandler.loadImageFitCenter(context, imageView, ERROR_ILLUSTRATION_URL)
+        }
     }
 
 }
