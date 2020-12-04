c565ce3aa1a75198c4257092b060dd27a9067dbf
elscione <yusufhendrawan06@gmail.com>
2020-12-04 17:19:11 +0700

    implement empty state for post, multi trendline and simple table widget

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/TableViewHolder.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/TableViewHolder.kt
@@ -60,6 +60,10 @@ class TableViewHolder(
                     setOnTableEmpty(element)
                 } else {
                     tvShcTableOnEmpty.gone()
+                    imgShcTableEmpty.gone()
+                    tvShcTableEmptyTitle.gone()
+                    tvShcTableEmptyDescription.gone()
+                    btnShcTableEmpty.gone()
                     shcTableView.visible()
                     shcTableView.showTable(element.data?.dataSet.orEmpty())
                     shcTableView.addOnSlideImpressionListener { position, isEmpty ->
@@ -82,7 +86,33 @@ class TableViewHolder(
     }
 
     private fun setOnTableEmpty(element: TableWidgetUiModel) = with(itemView) {
-        tvShcTableOnEmpty.visible()
+        if (element.emptyState.imageUrl.isNotBlank() && element.emptyState.title.isNotBlank() &&
+                element.emptyState.description.isNotBlank() && element.emptyState.ctaText.isNotBlank() &&
+                element.emptyState.appLink.isNotBlank()) {
+            imgShcTableEmpty.apply {
+                visible()
+                ImageHandler.loadImageWithoutPlaceholderAndError(this, element.emptyState.imageUrl.takeIf { it.isNotBlank() })
+            }
+            tvShcTableEmptyTitle.apply {
+                text = element.emptyState.title
+                visible()
+            }
+            tvShcTableEmptyDescription.apply {
+                text = element.emptyState.description
+                visible()
+            }
+            btnShcTableEmpty.apply {
+                text = element.emptyState.ctaText
+                visible()
+                setOnClickListener {
+                    if (RouteManager.route(context, element.emptyState.appLink)) {
+                        listener.sendTableEmptyStateCtaClickEvent(element)
+                    }
+                }
+            }
+        } else {
+            tvShcTableOnEmpty.visible()
+        }
         shcTableView.gone()
         listener.sendTableImpressionEvent(element, 0, true)
     }
@@ -91,6 +121,10 @@ class TableViewHolder(
         shimmerTableWidgetWidget.visible()
         shcTableView.gone()
         tvShcTableOnEmpty.gone()
+        imgShcTableEmpty.gone()
+        tvShcTableEmptyTitle.gone()
+        tvShcTableEmptyDescription.gone()
+        btnShcTableEmpty.gone()
     }
 
     private fun showErrorState() = with(itemView) {
@@ -99,6 +133,10 @@ class TableViewHolder(
         tvTableWidgetTitle.visible()
         shcTableView.gone()
         tvShcTableOnEmpty.gone()
+        imgShcTableEmpty.gone()
+        tvShcTableEmptyTitle.gone()
+        tvShcTableEmptyDescription.gone()
+        btnShcTableEmpty.gone()
 
         ImageHandler.loadImageWithId(imgWidgetOnError, R.drawable.unify_globalerrors_connection)
     }
@@ -143,6 +181,7 @@ class TableViewHolder(
 
         fun sendTableImpressionEvent(model: TableWidgetUiModel, slideNumber: Int, isSlideEmpty: Boolean) {}
         fun sendTableHyperlinkClickEvent(dataKey: String, url: String, isEmpty: Boolean)
+        fun sendTableEmptyStateCtaClickEvent(element: TableWidgetUiModel) {}
 
     }
 }
