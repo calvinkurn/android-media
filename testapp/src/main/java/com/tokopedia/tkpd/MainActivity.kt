6557cad5583f2a22691804489305b510875b17a8
saidfaisal <said.faisal@tokopedia.com>
2020-12-04 07:44:20 +0700

    revert main activity testapp

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.tokopedia.application.MyApplication
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
-import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.applink.internal.ApplinkConstInternalTestApp
 import com.tokopedia.tkpd.helper.logout
 import com.tokopedia.tkpd.network.DataSource
@@ -99,7 +98,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun goTo() {
-        RouteManager.route(this, ApplinkConstInternalMarketplace.SHOP_SETTINGS_ADDRESS)
         /* @example: open groupchat module;
          * startActivity(PlayActivity.getCallingIntent(this, "668", true))
          * or, you can use route like this:
