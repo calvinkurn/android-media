b64331e227531e230c9a46d4140f0ef320bbae41
Felicia Widjaja <fwidjaja@live.com>
2020-12-05 13:08:46 +0700

    request changes by Hansen

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/adapter/UohBottomSheetOptionAdapter.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/adapter/UohBottomSheetOptionAdapter.kt
@@ -20,7 +20,6 @@ import kotlinx.android.synthetic.main.bottomsheet_option_uoh_item.view.*
  * Created by fwidjaja on 05/07/20.
  */
 class UohBottomSheetOptionAdapter(private var listener: ActionListener): RecyclerView.Adapter<UohBottomSheetOptionAdapter.ViewHolder>()  {
-    var uohItemMapKeyList = arrayListOf<HashMap<String, String>>()
     var filterBundleList = arrayListOf<UohFilterBundle>()
     var filterType = -1
     var selectedRadio = -1
@@ -41,79 +40,82 @@ class UohBottomSheetOptionAdapter(private var listener: ActionListener): Recycle
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (filterBundleList[position].type == 0) {
-            holder.itemView.sublabel_option.gone()
-            holder.itemView.divider_sublabel.gone()
-            holder.itemView.label_option.visible()
-            holder.itemView.divider_label.visible()
-            if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundleList[position].value.isEmpty()) {
-                holder.itemView.label_option.text = ALL_STATUS_TRANSACTION
-            } else if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundleList[position].value.equals(TRANSAKSI_BERLANGSUNG, true)) {
-                holder.itemView.label_option.text = SEMUA_TRANSAKSI_BERLANGSUNG
-            } else {
-                holder.itemView.label_option.text = filterBundleList[position].value
-            }
-        } else if (filterBundleList[position].type == 1) {
-            holder.itemView.label_option.gone()
-            holder.itemView.divider_label.gone()
-            holder.itemView.sublabel_option.visible()
-            holder.itemView.divider_sublabel.visible()
-            holder.itemView.sublabel_option.text = filterBundleList[position].value
-        }
-
-        holder.itemView.setOnClickListener {
-            selectItem(position)
-        }
-
-        holder.itemView.rb_option.setOnClickListener {
-            selectItem(position)
-        }
-
-        if (!isReset) {
-            if (selectedKey.isEmpty() && selectedRadio == -1) {
-                if (filterType == UohConsts.TYPE_FILTER_DATE && filterBundleList[position].key == "0") {
-                    holder.itemView.rb_option.isChecked = true
-                } else if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundleList[position].key.isEmpty()) {
-                    holder.itemView.rb_option.isChecked = true
-                } else if (filterType == UohConsts.TYPE_FILTER_CATEGORY && filterBundleList[position].value.equals(ALL_PRODUCTS, true)) {
-                    holder.itemView.rb_option.isChecked = true
-                }
-            } else {
-                if (filterBundleList[position].key.equals(selectedKey, true) && selectedRadio == -1) {
-                    holder.itemView.rb_option.isChecked = true
+        if (position > -1 && position < filterBundleList.size) {
+            val filterBundle = filterBundleList[position]
+            if (filterBundle.type == 0) {
+                holder.itemView.sublabel_option.gone()
+                holder.itemView.divider_sublabel.gone()
+                holder.itemView.label_option.visible()
+                holder.itemView.divider_label.visible()
+                if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundle.value.isEmpty()) {
+                    holder.itemView.label_option.text = ALL_STATUS_TRANSACTION
+                } else if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundle.value.equals(TRANSAKSI_BERLANGSUNG, true)) {
+                    holder.itemView.label_option.text = SEMUA_TRANSAKSI_BERLANGSUNG
                 } else {
-                    holder.itemView.rb_option.isChecked = position == selectedRadio
+                    holder.itemView.label_option.text = filterBundle.value
                 }
+            } else if (filterBundle.type == 1) {
+                holder.itemView.label_option.gone()
+                holder.itemView.divider_label.gone()
+                holder.itemView.sublabel_option.visible()
+                holder.itemView.divider_sublabel.visible()
+                holder.itemView.sublabel_option.text = filterBundle.value
             }
-        } else {
-            if (selectedKey.isEmpty() && selectedRadio == -1) {
-                if (filterType == UohConsts.TYPE_FILTER_DATE) {
-                    if (position == 0) {
+
+            holder.itemView.setOnClickListener {
+                selectItem(position, filterBundle)
+            }
+
+            holder.itemView.rb_option.setOnClickListener {
+                selectItem(position, filterBundle)
+            }
+
+            if (!isReset) {
+                if (selectedKey.isEmpty() && selectedRadio == -1) {
+                    if (filterType == UohConsts.TYPE_FILTER_DATE && filterBundle.key == "0") {
+                        holder.itemView.rb_option.isChecked = true
+                    } else if (filterType == UohConsts.TYPE_FILTER_STATUS && filterBundle.key.isEmpty()) {
+                        holder.itemView.rb_option.isChecked = true
+                    } else if (filterType == UohConsts.TYPE_FILTER_CATEGORY && filterBundle.value.equals(ALL_PRODUCTS, true)) {
                         holder.itemView.rb_option.isChecked = true
                     }
-                } else if (filterType == UohConsts.TYPE_FILTER_STATUS) {
-                    if (filterBundleList[position].value == ALL_STATUS_TRANSACTION) {
+                } else {
+                    if (filterBundle.key.equals(selectedKey, true) && selectedRadio == -1) {
                         holder.itemView.rb_option.isChecked = true
-                    }
-                } else if (filterType == UohConsts.TYPE_FILTER_CATEGORY) {
-                    if (filterBundleList[position].value == ALL_PRODUCTS) {
-                        holder.itemView.rb_option.isChecked = true
+                    } else {
+                        holder.itemView.rb_option.isChecked = position == selectedRadio
                     }
                 }
             } else {
-                if (filterBundleList[position].key.equals(selectedKey, true) && selectedRadio == -1) {
-                    holder.itemView.rb_option.isChecked = true
+                if (selectedKey.isEmpty() && selectedRadio == -1) {
+                    if (filterType == UohConsts.TYPE_FILTER_DATE) {
+                        if (position == 0) {
+                            holder.itemView.rb_option.isChecked = true
+                        }
+                    } else if (filterType == UohConsts.TYPE_FILTER_STATUS) {
+                        if (filterBundle.value == ALL_STATUS_TRANSACTION) {
+                            holder.itemView.rb_option.isChecked = true
+                        }
+                    } else if (filterType == UohConsts.TYPE_FILTER_CATEGORY) {
+                        if (filterBundle.value == ALL_PRODUCTS) {
+                            holder.itemView.rb_option.isChecked = true
+                        }
+                    }
                 } else {
-                    holder.itemView.rb_option.isChecked = position == selectedRadio
+                    if (filterBundle.key.equals(selectedKey, true) && selectedRadio == -1) {
+                        holder.itemView.rb_option.isChecked = true
+                    } else {
+                        holder.itemView.rb_option.isChecked = position == selectedRadio
+                    }
                 }
             }
         }
     }
 
-    private fun selectItem(position: Int) {
+    private fun selectItem(position: Int, filterBundle: UohFilterBundle) {
         isReset = false
         selectedRadio = position
-        listener.onOptionItemClick(filterBundleList[position].key, filterBundleList[position].value, filterType)
+        listener.onOptionItemClick(filterBundle.key, filterBundle.value, filterType)
 
         notifyDataSetChanged()
     }
