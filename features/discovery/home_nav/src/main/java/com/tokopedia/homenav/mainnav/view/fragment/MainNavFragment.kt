c581cc6f80dc0a1ac97c4072d3fcc5638e759607
yorispr <yoris.prayogo@tokopedia.com>
2020-12-08 04:30:07 +0700

    revert

--- features/discovery/home_nav/src/main/java/com/tokopedia/homenav/mainnav/view/fragment/MainNavFragment.kt
+++ features/discovery/home_nav/src/main/java/com/tokopedia/homenav/mainnav/view/fragment/MainNavFragment.kt
@@ -198,7 +198,7 @@ class MainNavFragment : BaseDaggerFragment(), MainNavListener {
     }
 
     override fun onProfileSectionClicked() {
-        val intent = RouteManager.getIntent(context, ApplinkConstInternalGlobal.OLD_HOME_ACCOUNT)
+        val intent = RouteManager.getIntent(context, ApplinkConstInternalGlobal.NEW_HOME_ACCOUNT)
         startActivity(intent)
     }
 
