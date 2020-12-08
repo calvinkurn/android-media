8d68d184aea436626a43c0054fa0b20c309b855b
leonard2907 <leonard.gunawan@tokopedia.com>
2020-12-08 16:53:54 +0700

    Revert to old logic

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/datamodel/ProductTickerInfoDataModel.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/data/model/datamodel/ProductTickerInfoDataModel.kt
@@ -37,7 +37,7 @@ data class ProductTickerInfoDataModel(
     }
 
     fun isOos(): Boolean {
-        return isOutOfStock && !isProductInCampaign
+        return return isOutOfStock && !isProductInCampaign && isProductWarehouse
     }
 
     fun isProductInactive(): Boolean {
