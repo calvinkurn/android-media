727724b819fe216ded810f5c89fbde51b3b48f2c
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-12-14 20:48:03 +0700

    change int to long

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/review/ReviewCard.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/review/ReviewCard.kt
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class ReviewCard(
         @SerializedName("product_id")
-        val productId: Int = 0,
+        val productId: Long = 0,
         @SerializedName("image_url")
         val imageUrl: String = "",
         @SerializedName("product_name")
         val productName: String = "",
         @SerializedName("reputation_id")
-        val reputationId: Int = 0,
+        val reputationId: Long = 0,
         @SerializedName("buyer_id")
-        val buyerId: Int = 0,
+        val buyerId: Long = 0,
         @SerializedName("allow_review")
         val allowReview: Boolean = false,
         @SerializedName("is_reviewed")
