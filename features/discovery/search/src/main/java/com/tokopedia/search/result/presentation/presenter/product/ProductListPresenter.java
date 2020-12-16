a94ad5b462c26abddce857d006b71edf1eacce85
darianthedytoped <darian.thedy@tokopedia.com>
2020-12-16 18:07:38 +0700

    Add TDN

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.java
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.java
@@ -47,6 +47,7 @@ import com.tokopedia.search.result.presentation.model.RelatedViewModel;
 import com.tokopedia.search.result.presentation.model.SearchInTokopediaViewModel;
 import com.tokopedia.search.result.presentation.model.SearchProductCountViewModel;
 import com.tokopedia.search.result.presentation.model.SearchProductTitleViewModel;
+import com.tokopedia.search.result.presentation.model.SearchProductTopAdsImageViewModel;
 import com.tokopedia.search.result.presentation.model.SeparatorViewModel;
 import com.tokopedia.search.result.presentation.model.SuggestionViewModel;
 import com.tokopedia.search.utils.SchedulersProvider;
@@ -62,6 +63,7 @@ import com.tokopedia.topads.sdk.domain.model.CpmModel;
 import com.tokopedia.topads.sdk.domain.model.Data;
 import com.tokopedia.topads.sdk.domain.model.FreeOngkir;
 import com.tokopedia.topads.sdk.domain.model.LabelGroup;
+import com.tokopedia.topads.sdk.domain.model.TopAdsImageViewModel;
 import com.tokopedia.topads.sdk.utils.TopAdsUrlHitter;
 import com.tokopedia.unifycomponents.ChipsUnify;
 import com.tokopedia.usecase.RequestParams;
@@ -153,8 +155,9 @@ final class ProductListPresenter
     private boolean isShowHeadlineAdsBasedOnGlobalNav = false;
 
     private List<Visitable> productList;
-    private List<InspirationCarouselViewModel> inspirationCarouselViewModel;
-    private List<InspirationCardViewModel> inspirationCardViewModel;
+    private List<InspirationCarouselViewModel> inspirationCarouselViewModel = new ArrayList<>();
+    private List<InspirationCardViewModel> inspirationCardViewModel = new ArrayList<>();
+    private List<TopAdsImageViewModel> topAdsImageViewModelList = new ArrayList<>();
     private SuggestionViewModel suggestionViewModel = null;
     private RelatedViewModel relatedViewModel = null;
     private List<Option> quickFilterOptionList = new ArrayList<>();
@@ -497,6 +500,7 @@ final class ProductListPresenter
         List<Visitable> list = new ArrayList<>(createProductItemVisitableList(productViewModel));
         productList.addAll(list);
 
+        processTopAdsImageViewModel(searchParameter, list);
         processHeadlineAds(searchParameter, list);
         processInspirationCardPosition(searchParameter, list);
         processInspirationCarouselPosition(searchParameter, list);
@@ -1089,6 +1093,9 @@ final class ProductListPresenter
         productList = createProductItemVisitableList(productViewModel);
         list.addAll(productList);
 
+        topAdsImageViewModelList = searchProductModel.getTopAdsImageViewModelList();
+        processTopAdsImageViewModel(searchParameter, list);
+
         processHeadlineAds(searchParameter, list);
 
         if (!textIsEmpty(productViewModel.getAdditionalParams())) {
@@ -1385,6 +1392,46 @@ final class ProductListPresenter
         int broadMatchIndex = list.indexOf(productItemAtBroadMatchPosition) + 1;
 
         list.addAll(broadMatchIndex, broadMatchVisitableList);
+    }
+
+    private void processTopAdsImageViewModel(Map<String, Object> searchParameter, List<Visitable> list) {
+        if (topAdsImageViewModelList.size() == 0) return;
+
+        Iterator<TopAdsImageViewModel> topAdsImageViewModelIterator = topAdsImageViewModelList.iterator();
+
+        while(topAdsImageViewModelIterator.hasNext()) {
+            TopAdsImageViewModel data = topAdsImageViewModelIterator.next();
+
+            if (data.getPosition() < 0) {
+                topAdsImageViewModelIterator.remove();
+                continue;
+            }
+
+            if (data.getPosition() <= productList.size()) {
+                try {
+                    processTopAdsImageViewModelInPosition(list, data);
+                    topAdsImageViewModelIterator.remove();
+                }
+                catch (Exception exception) {
+                    exception.printStackTrace();
+                    getView().logWarning(UrlParamUtils.generateUrlParamString(searchParameter), exception);
+                }
+            }
+        }
+    }
+
+    private void processTopAdsImageViewModelInPosition(List<Visitable> list, TopAdsImageViewModel data) {
+        boolean isTopPosition = data.getPosition() == 0 || data.getPosition() == 1;
+        SearchProductTopAdsImageViewModel searchProductTopAdsImageViewModel = new SearchProductTopAdsImageViewModel(data);
+
+        if (isTopPosition) {
+            Visitable product = productList.get(0);
+            list.add(list.indexOf(product), searchProductTopAdsImageViewModel);
+        }
+        else {
+            Visitable product = productList.get(data.getPosition() - 1);
+            list.add(list.indexOf(product) + 1, searchProductTopAdsImageViewModel);
+        }
     }
 
     private boolean isExistsFreeOngkirBadge(List<Visitable> productList) {
---
+++ features/discovery/search/src/test/java/com/tokopedia/search/result/presentation/presenter/product/SearchProductTDNTest.kt
@@ -0,0 +1,131 @@
+package com.tokopedia.search.result.presentation.presenter.product
+
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.search.jsonToObject
+import com.tokopedia.search.result.complete
+import com.tokopedia.search.result.domain.model.SearchProductModel
+import com.tokopedia.search.result.presentation.model.SearchProductTopAdsImageViewModel
+import com.tokopedia.search.shouldBe
+import com.tokopedia.search.shouldBeInstanceOf
+import com.tokopedia.topads.sdk.domain.model.TopAdsImageViewModel
+import io.mockk.every
+import io.mockk.slot
+import org.junit.Test
+import rx.Subscriber
+
+internal class SearchProductTDNTest: ProductListPresenterTestFixtures() {
+
+    private val searchProductModelFirstPage = searchProductCommonResponseJSON.jsonToObject<SearchProductModel>()
+    private val searchProductModelLoadMore = searchProductCommonResponseJSON.jsonToObject<SearchProductModel>()
+    private val tdn0 = TopAdsImageViewModel(position = 0, bannerName = "Position 0")
+    private val tdn1 = TopAdsImageViewModel(position = 1, bannerName = "Position 1")
+    private val tdn4 = TopAdsImageViewModel(position = 4, bannerName = "Position 4")
+    private val tdn8 = TopAdsImageViewModel(position = 8, bannerName = "Position 8")
+    private val tdn12 = TopAdsImageViewModel(position = 12, bannerName = "Position 12")
+    private val tdn16 = TopAdsImageViewModel(position = 16, bannerName = "Position 16")
+
+    private val visitableList = mutableListOf<Visitable<*>>()
+
+    @Test
+    fun `Test TDN on first page`() {
+        val tdnList = listOf(tdn1, tdn4, tdn8)
+        searchProductModelFirstPage.setTopAdsImageViewModelList(tdnList)
+
+        `Given Search Product API will return SearchProductModel`(searchProductModelFirstPage)
+        `Given product list will be captured`()
+
+        `When load data`()
+
+        `Then verify TDN on first page`()
+    }
+
+    private fun `Given Search Product API will return SearchProductModel`(searchProductModel: SearchProductModel) {
+        every { searchProductFirstPageUseCase.execute(any(), any()) }.answers {
+            secondArg<Subscriber<SearchProductModel>>().complete(searchProductModel)
+        }
+    }
+
+    private fun `Given product list will be captured`() {
+        val firstPageVisitableListSlot = slot<List<Visitable<*>>>()
+        every { productListView.setProductList(capture(firstPageVisitableListSlot)) } answers {
+            visitableList.addAll(firstPageVisitableListSlot.captured)
+        }
+
+        val secondPageVisitableListSlot = slot<List<Visitable<*>>>()
+        every { productListView.addProductList(capture(secondPageVisitableListSlot)) } answers {
+            visitableList.addAll(secondPageVisitableListSlot.captured)
+        }
+    }
+
+    private fun `When load data`() {
+        productListPresenter.loadData(mapOf())
+    }
+
+    private fun `Then verify TDN on first page`() {
+        visitableList.size shouldBe 11
+
+        visitableList[0].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[0] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn1
+        visitableList[5].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[5] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn4
+        visitableList[10].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[10] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn8
+    }
+
+    @Test
+    fun `Test TDN on load more`() {
+        val tdnList = listOf(tdn1, tdn4, tdn8, tdn12, tdn16)
+        searchProductModelFirstPage.setTopAdsImageViewModelList(tdnList)
+
+        `Given Search Product API will return SearchProductModel`(searchProductModelFirstPage)
+        `Given Search Product Load More API will return SearchProductModel`(searchProductModelLoadMore)
+        `Given product list will be captured`()
+        `Given view already load first page`()
+
+        `When load more data`()
+
+        `Then verify TDN on second page`()
+    }
+
+    private fun `Given Search Product Load More API will return SearchProductModel`(searchProductModel: SearchProductModel) {
+        every { searchProductLoadMoreUseCase.execute(any(), any()) }.answers {
+            secondArg<Subscriber<SearchProductModel>>().complete(searchProductModel)
+        }
+    }
+
+    private fun `Given view already load first page`() {
+        productListPresenter.loadData(mapOf())
+    }
+
+    private fun `When load more data`() {
+        productListPresenter.loadMoreData(mapOf())
+    }
+
+    private fun `Then verify TDN on second page`() {
+        visitableList.size shouldBe 21
+
+        visitableList[15].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[15] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn12
+        visitableList[20].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[20] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn16
+    }
+
+    @Test
+    fun `Test TDN position 0`() {
+        val tdnList = listOf(tdn0)
+        searchProductModelFirstPage.setTopAdsImageViewModelList(tdnList)
+
+        `Given Search Product API will return SearchProductModel`(searchProductModelFirstPage)
+        `Given product list will be captured`()
+
+        `When load data`()
+
+        `Then verify TDN at position 0`()
+    }
+
+    private fun `Then verify TDN at position 0`() {
+        visitableList.size shouldBe 9
+        visitableList[0].shouldBeInstanceOf<SearchProductTopAdsImageViewModel>()
+        (visitableList[0] as SearchProductTopAdsImageViewModel).topAdsImageViewModel shouldBe tdn0
+    }
+}
