14981db97bc77ef92ac0435cd1d73c60bae4c1ec
ilhamsuaib <ilhamsuaib10@gmail.com>
2020-12-16 15:57:35 +0800

    fix typo data layer tracker app widget

--- features/merchant/seller_app_widget/src/main/java/com/tokopedia/sellerappwidget/analytics/TrackingConstant.kt
+++ features/merchant/seller_app_widget/src/main/java/com/tokopedia/sellerappwidget/analytics/TrackingConstant.kt
@@ -42,7 +42,7 @@ object TrackingConstant {
         const val IMPRESSION_LOADING_STATE = "impression loading state"
         const val IMPRESSION_NO_LOGIN_STATE = "impression no login state"
         const val IMPRESSION_NO_CONNECTION_STATE = "impression no connection state"
-        const val IMPRESSION_ACTIVE_NEW_ORDER = "mpression active new order"
+        const val IMPRESSION_ACTIVE_NEW_ORDER = "impression active new order"
         const val IMPRESSION_ACTIVE_READY_SHIPPING = "impression active ready shipping"
         const val IMPRESSION_NEW_ORDER_SMALL_ORDER = "impression active 4x2 order widget - pesanan baru"
         const val IMPRESSION_READY_TO_SHIP_SMALL_ORDER = "impression active 4x2 order widget - siap dikirim"
