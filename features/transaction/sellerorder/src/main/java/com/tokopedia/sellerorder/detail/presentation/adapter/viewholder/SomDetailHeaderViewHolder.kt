47b2b5cbcb726963391311f68f1863a1932c1569
Felicia Widjaja <fwidjaja@live.com>
2020-12-16 18:46:55 +0700

    change file for invoice seller

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/detail/presentation/adapter/viewholder/SomDetailHeaderViewHolder.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/detail/presentation/adapter/viewholder/SomDetailHeaderViewHolder.kt
@@ -101,7 +101,7 @@ class SomDetailHeaderViewHolder(itemView: View, private val actionListener: SomD
                 }
 
                 header_see_invoice?.setOnClickListener {
-                    actionListener?.onSeeInvoice(item.dataObject.invoiceUrl)
+                    actionListener?.onSeeInvoice(item.dataObject.invoiceUrl, item.dataObject.invoice)
                 }
             }
         }
