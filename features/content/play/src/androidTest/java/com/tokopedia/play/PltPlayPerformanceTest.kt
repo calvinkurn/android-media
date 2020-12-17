0484490cdc89b4a1da566934efef929a707bee5d
mzennis <meyta.taliti@tokopedia.com>
2020-12-17 09:46:16 +0700

    try to hide loader unify (?)

--- features/content/play/src/androidTest/java/com/tokopedia/play/PltPlayPerformanceTest.kt
+++ features/content/play/src/androidTest/java/com/tokopedia/play/PltPlayPerformanceTest.kt
@@ -3,19 +3,28 @@ package com.tokopedia.play
 import android.content.Intent
 import android.net.Uri
 import android.text.TextUtils
+import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.test.espresso.Espresso.onIdle
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analytics.performance.util.PerformanceDataFileUtils
 import com.tokopedia.applink.internal.ApplinkConstInternalContent
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.play.data.PlayMockModelConfig
 import com.tokopedia.play.view.activity.PlayActivity
 import com.tokopedia.test.application.TestRepeatRule
 import com.tokopedia.test.application.util.setupGraphqlMockResponseWithCheck
+import com.tokopedia.unifycomponents.LoaderUnify
+import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -60,6 +69,20 @@ class PltPlayPerformanceTest {
     @Test
     fun testPageLoadTimePerformance() {
         launchActivity()
+
+        onView(withId(R.id.loader_page)).perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(LoaderUnify::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "hide loader unify"
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                view?.hide()
+            }
+        })
 
         IdlingRegistry.getInstance().register(idlingResource)
 
