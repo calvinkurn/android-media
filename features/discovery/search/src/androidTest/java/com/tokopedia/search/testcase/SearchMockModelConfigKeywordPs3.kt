c75fe67e972f92e4495a4fb25da30e392162734f
glenntuyu <christian.tuyu@tokopedia.com>
2020-12-21 11:12:10 +0700

    add instrum test for global nav

---
+++ features/discovery/search/src/androidTest/java/com/tokopedia/search/testcase/SearchMockModelConfigKeywordPs3.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.search.testcase
+
+import android.content.Context
+import com.tokopedia.search.test.R
+import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
+import com.tokopedia.test.application.util.InstrumentationMockHelper.getRawString
+
+internal class SearchMockModelConfigKeywordPs3: MockModelConfig() {
+
+    override fun createMockModel(context: Context): MockModelConfig {
+        val mapMockResponse = createMapOfMockResponse(context)
+
+        for ((key, value) in mapMockResponse.entries) addMockResponse(key, value, FIND_BY_CONTAINS)
+
+        return this
+    }
+
+    private fun createMapOfMockResponse(context: Context) = mapOf(
+            "SearchProduct" to getRawString(context, R.raw.search_product_response_keyword_ps_3)
+    )
+}
