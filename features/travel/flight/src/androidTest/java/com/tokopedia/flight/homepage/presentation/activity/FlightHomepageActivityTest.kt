318d2fa7a161abb89606cc16c034e5940159bd37
Muhammad Furqan <furqan@tokopedia.com>
2020-12-22 10:45:54 +0700

    homepage instrumentation test

--- features/travel/flight/src/androidTest/java/com/tokopedia/flight/homepage/presentation/activity/FlightHomepageActivityTest.kt
+++ features/travel/flight/src/androidTest/java/com/tokopedia/flight/homepage/presentation/activity/FlightHomepageActivityTest.kt
@@ -55,7 +55,8 @@ class FlightHomepageActivityTest {
 
     @Test
     fun validateFlightHomepageP1Tracking() {
-        Thread.sleep(1000)
+        Thread.sleep(2000)
+        onView(withId(R.id.nsvFlightHomepage)).perform(swipeUp())
         Thread.sleep(1000)
         onView(withId(R.id.nsvFlightHomepage)).perform(swipeUp())
 
