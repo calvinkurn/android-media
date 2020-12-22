c5e6dc588e1d0325cd79d984de3954ba1ae67156
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2020-12-22 15:22:39 +0700

    remove unused code

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/activity/ChatListActivity.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatlist/activity/ChatListActivity.kt
@@ -1,8 +1,7 @@
 package com.tokopedia.topchat.chatlist.activity
 
-import android.os.Build
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
@@ -20,7 +19,6 @@ open class ChatListActivity : BaseSimpleActivity(), ChatTabListFragment.Listener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(null)
-        useLightNotificationBar()
         initWindowBackground()
         initTopchatToolbar()
     }
@@ -29,19 +27,16 @@ open class ChatListActivity : BaseSimpleActivity(), ChatTabListFragment.Listener
         return toolbar
     }
 
-    private fun useLightNotificationBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            window.statusBarColor = MethodChecker.getColor(this, com.tokopedia.unifyprinciples.R.color.Unify_N0)
-        }
-    }
-
     private fun initWindowBackground() {
         window.decorView.setBackgroundColor(MethodChecker.getColor(this, com.tokopedia.unifyprinciples.R.color.Unify_N0))
     }
 
     private fun initTopchatToolbar() {
         supportActionBar?.setBackgroundDrawable(null)
+    }
+
+    private fun isDarkMode(): Boolean {
+        return AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
     }
 
     companion object {
