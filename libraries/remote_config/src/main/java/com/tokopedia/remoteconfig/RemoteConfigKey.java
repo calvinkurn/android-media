488d2666846ca528e2de6f2c8b48bec921b27098
Muhammad Furqan <furqan@tokopedia.com>
2020-12-28 21:18:53 +0700

    [Flight] Remove android_customer_flight_booking_new_flow remote config key

--- libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RemoteConfigKey.java
+++ libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RemoteConfigKey.java
@@ -44,7 +44,6 @@ public class RemoteConfigKey {
     public static final String MAINAPP_FLIGHT_TRANSACTION_MENU = "mainapp_flight_transaction_menu";
     public static final String CUSTOMER_HOTEL_SHOW_PROMO = "android_customer_hotel_show_promo";
     public static final String CUSTOMER_HOTEL_BOOK_FOR_TODAY = "android_customer_hotel_enable_booking_for_today";
-    public static final String ANDROID_CUSTOMER_FLIGHT_BOOKING_NEW_FLOW = "android_customer_flight_booking_new_flow";
     public static final String ANDROID_CUSTOMER_FLIGHT_SHOW_FREE_RAPID_TEST = "app_flag_flight_rapid_test";
     public static final String ANDROID_CUSTOMER_FLIGHT_SHOW_SEAT_DISTANCING = "app_flag_flight_seat_distancing";
     public static final String SHOW_IMAGE_SEARCH = "enable_image_search";
