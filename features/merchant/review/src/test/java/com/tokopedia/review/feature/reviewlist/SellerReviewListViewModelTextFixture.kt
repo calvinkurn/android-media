05e29e65f3754f7871b8c1839d849cd7599ff6d2
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2020-12-29 18:26:22 +0700

    remove unused code & increase page load time in review seller

--- features/merchant/review/src/test/java/com/tokopedia/review/feature/reviewlist/SellerReviewListViewModelTextFixture.kt
+++ features/merchant/review/src/test/java/com/tokopedia/review/feature/reviewlist/SellerReviewListViewModelTextFixture.kt
@@ -5,8 +5,11 @@ import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
 import com.tokopedia.review.feature.reviewlist.domain.GetProductRatingOverallUseCase
 import com.tokopedia.review.feature.reviewlist.domain.GetReviewProductListUseCase
 import com.tokopedia.review.feature.reviewlist.view.viewmodel.SellerReviewListViewModel
+import com.tokopedia.unit.test.rule.CoroutineTestRule
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockkObject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 
@@ -28,5 +31,6 @@ abstract class SellerReviewListViewModelTextFixture {
         MockKAnnotations.init(this)
         viewModel = SellerReviewListViewModel(CoroutineTestDispatchersProvider,
                 getProductRatingOverallUse, getReviewProductListUseCase)
+        mockkObject(GetProductRatingOverallUseCase)
     }
 }
