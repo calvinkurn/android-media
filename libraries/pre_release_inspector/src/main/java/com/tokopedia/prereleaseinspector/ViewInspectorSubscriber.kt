35f5e0a9adeb1ef76bea2253fb66948a4624de48
Henry Priyono <henry.priyono@tokopedia.com>
2021-01-04 14:07:17 +0700

    Add Missing Import

--- libraries/pre_release_inspector/src/main/java/com/tokopedia/prereleaseinspector/ViewInspectorSubscriber.kt
+++ libraries/pre_release_inspector/src/main/java/com/tokopedia/prereleaseinspector/ViewInspectorSubscriber.kt
@@ -3,6 +3,7 @@ package com.tokopedia.prereleaseinspector
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
 
 class ViewInspectorSubscriber() : Application.ActivityLifecycleCallbacks {
 
