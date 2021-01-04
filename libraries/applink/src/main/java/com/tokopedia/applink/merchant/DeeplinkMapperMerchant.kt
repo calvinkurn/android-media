6fa7c7e89b8b3dbb048f3680daa9e46ca6f0d991
leonard2907 <leonard.gunawan@tokopedia.com>
2021-01-04 10:48:07 +0700

    Revert removing in deeplink mapper

--- libraries/applink/src/main/java/com/tokopedia/applink/merchant/DeeplinkMapperMerchant.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/merchant/DeeplinkMapperMerchant.kt
@@ -260,6 +260,16 @@ object DeeplinkMapperMerchant {
         return deeplink
     }
 
+    fun isProductDetailPageDeeplink(deeplink: String): Boolean {
+        val uri = Uri.parse(deeplink)
+        return deeplink.startsWithPattern(ApplinkConst.PRODUCT_INFO) && uri.pathSegments.size == 1 && uri.lastPathSegment.toIntOrZero() != 0
+    }
+
+    fun isProductDetailAffiliatePageDeeplink(deeplink: String): Boolean {
+        val uri = Uri.parse(deeplink)
+        return deeplink.startsWithPattern(ApplinkConst.AFFILIATE_PRODUCT) && uri.pathSegments.size == 2
+    }
+
     fun getRegisteredProductDetailAffiliate(deeplink: String) : String {
         val parsedUri = Uri.parse(deeplink)
 
