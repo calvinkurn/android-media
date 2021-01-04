7b391d5149c6b9ed0e2a720548123de26fb61be3
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-01-04 12:54:16 +0700

    update empty state filter

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/NotificationFragment.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/fragment/NotificationFragment.kt
@@ -171,7 +171,7 @@ class NotificationFragment : BaseListFragment<Visitable<*>, NotificationTypeFact
 
     override fun getEmptyDataViewModel(): Visitable<*> {
         if (viewModel.hasFilter() || containerListener?.role == RoleType.SELLER) {
-            return EmptyNotificationUiModel()
+            return EmptyNotificationUiModel(viewModel.hasFilter())
         }
         return super.getEmptyDataViewModel()
     }
