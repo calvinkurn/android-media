018768cde6b8644486af44ae43b7c7fa7b68286f
lukaskris12 <lukaskris12@gmail.com>
2021-01-04 21:15:17 +0700

    remove commend

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/mapper/factory/HomeDynamicChannelVisitableFactoryImpl.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/mapper/factory/HomeDynamicChannelVisitableFactoryImpl.kt
@@ -178,7 +178,6 @@ class HomeDynamicChannelVisitableFactoryImpl(
                 }
             }
         }
-//        createBestSellingWidget(DynamicHomeChannel.Channels(widgetParam = ""))
         if (addLoadingMore) {
             createDynamicChannelLoadingMore()
         }
