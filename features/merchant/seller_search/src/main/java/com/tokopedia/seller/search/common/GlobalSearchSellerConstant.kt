886ba44ad8e9363335dce157b7ffaad17a7de73d
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-01-05 17:17:19 +0700

    revert ktx & replace drawable to ecs

--- features/merchant/seller_search/src/main/java/com/tokopedia/seller/search/common/GlobalSearchSellerConstant.kt
+++ features/merchant/seller_search/src/main/java/com/tokopedia/seller/search/common/GlobalSearchSellerConstant.kt
@@ -13,4 +13,5 @@ object GlobalSearchSellerConstant {
 
     const val WWW_APP_LINK_PREFIX = "https://www.tokopedia.com"
     const val HTTPS_APP_LINK_PREFIX = "https://tokopedia.com"
+    const val IC_NO_HISTORY_URL = "https://ecs7.tokopedia.net/android/sellersearch/ic_no_history_search.png"
 }
