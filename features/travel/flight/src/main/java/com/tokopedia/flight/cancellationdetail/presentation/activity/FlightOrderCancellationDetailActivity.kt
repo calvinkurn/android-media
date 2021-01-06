1d404991f166c829cf7c24da6929b29b1039c54e
Muhammad Furqan <furqan@tokopedia.com>
2021-01-06 15:23:40 +0700

    [Flight] Create new Activity for Order Detail Cancellation List and Cancellation Detail

---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/cancellationdetail/presentation/activity/FlightOrderCancellationDetailActivity.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.flight.cancellationdetail.presentation.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FlightOrderCancellationDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/cancellationdetail/presentation/activity/FlightOrderCancellationListActivity.kt
@@ -0,0 +1,33 @@
+package com.tokopedia.flight.cancellationdetail.presentation.activity
+
+import android.content.Context
+import android.content.Intent
+import android.view.Menu
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.flight.common.view.BaseFlightActivity
+import com.tokopedia.flight.orderdetail.di.DaggerFlightOrderDetailComponent
+import com.tokopedia.flight.orderdetail.di.FlightOrderDetailComponent
+
+class FlightOrderCancellationListActivity : BaseFlightActivity(),
+        HasComponent<FlightOrderDetailComponent> {
+
+    override fun getNewFragment(): Fragment? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getComponent(): FlightOrderDetailComponent =
+            DaggerFlightOrderDetailComponent.builder()
+                    .flightComponent(flightComponent)
+                    .build()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean = true
+
+    companion object {
+        const val EXTRA_INVOICE_ID = "EXTRA_INVOICE_ID"
+
+        fun createIntent(context: Context, invoiceId: String): Intent =
+                Intent(context, FlightOrderCancellationListActivity::class.java)
+                        .putExtra(EXTRA_INVOICE_ID, invoiceId)
+    }
+}
