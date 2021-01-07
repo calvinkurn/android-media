4a9e2b73cdf8e478673065fe6a117ad4c64e58c5
isfaaghyth <isfaaghyth@gmail.com>
2021-01-07 12:52:35 +0700

    fixed missing unify color on kotlin extensions

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/const/Unify.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/const/Unify.kt
@@ -2,6 +2,7 @@ package com.tokopedia.settingnotif.usersetting.const
 
 object Unify {
     var Unify_N700_96 = com.tokopedia.unifyprinciples.R.color.Unify_N700_96
+    var Unify_N700_68 = com.tokopedia.unifyprinciples.R.color.Unify_N700_68
     var Green_G500 = com.tokopedia.unifyprinciples.R.color.Green_G500
     var Unify_N0 = com.tokopedia.unifyprinciples.R.color.Unify_N0
 }
