fdd014c268abbabe9f7ef345532a7c2a53b3bea8
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-01-07 14:34:53 +0700

    update ab test inbox value

--- libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/abtest/AbTestPlatform.kt
+++ libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/abtest/AbTestPlatform.kt
@@ -202,8 +202,8 @@ class AbTestPlatform @JvmOverloads constructor (val context: Context): RemoteCon
         const val NAVIGATION_VARIANT_REVAMP = "Navigation Revamp"
 
         const val KEY_AB_INBOX_REVAMP = "Inbox Revamp"
-        const val VARIANT_OLD_INBOX = "Control Variant"
-        const val VARIANT_NEW_INBOX = "New Inbox"
+        const val VARIANT_OLD_INBOX = ""
+        const val VARIANT_NEW_INBOX = "Inbox Revamp"
     }
 
 }
