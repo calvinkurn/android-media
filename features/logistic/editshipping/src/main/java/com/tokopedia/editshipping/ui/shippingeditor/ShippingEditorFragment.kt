fae4582626d1758aff9d4a072d60d33ba7eb85aa
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-01-07 09:03:08 +0700

    add logic for get shipper list

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shippingeditor/ShippingEditorFragment.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shippingeditor/ShippingEditorFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.editshipping.R
 import com.tokopedia.editshipping.di.shippingeditor.ShippingEditorComponent
+import com.tokopedia.globalerror.GlobalError
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.unifycomponents.ticker.Ticker
 import javax.inject.Inject
 
-class ShippingEditorFragment: BaseDaggerFragment() {
+class ShippingEditorFragment: BaseDaggerFragment(), ShippingEditorItemAdapter.ShippingEditorItemAdapterListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -18,6 +23,16 @@ class ShippingEditorFragment: BaseDaggerFragment() {
     private val viewModel: ShippingEditorViewModel by lazy {
         ViewModelProvider(this, viewModelFactory).get(ShippingEditorViewModel::class.java)
     }
+
+    private var tickerShipperInfo: Ticker? = null
+    private var shipperListOnDemand: RecyclerView? = null
+    private var shipperListConventional: RecyclerView? = null
+    private var btnSaveShipper: UnifyButton? = null
+    private var bottomSheetShipperInfo: BottomSheetUnify? = null
+
+    private var btnShipperBottomSheet: UnifyButton? = null
+
+    private var globalErrorLayout: GlobalError? = null
 
     override fun getScreenName(): String = ""
 
@@ -27,6 +42,44 @@ class ShippingEditorFragment: BaseDaggerFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_shipping_editor_new, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        tickerShipperInfo = view?.findViewById(R.id.ticker_shipper_info)
+        shipperListOnDemand = view?.findViewById(R.id.rv_on_demand)
+        shipperListConventional = view?.findViewById(R.id.rv_conventional)
+        btnSaveShipper = view?.findViewById(R.id.btn_save_shipper)
+        globalErrorLayout = view?.findViewById(R.id.global_error)
+    }
+
+    override fun onShipperInfoClicker() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun openBottomSheetShipperInfo() {
+        bottomSheetShipperInfo = BottomSheetUnify()
+        val viewBottomSheetShipperInfo = View.inflate(context, R.layout.bottomsheet_shipper_info, null)
+        setupBottomSheetShipperInfoChild(viewBottomSheetShipperInfo)
+
+        bottomSheetShipperInfo?.apply {
+            setCloseClickListener { dismiss() }
+            setChild(viewBottomSheetShipperInfo)
+            setOnDismissListener { dismiss() }
+        }
+
+        fragmentManager?.let {
+            bottomSheetShipperInfo?.show(it, "show")
+        }
+    }
+
+    private fun setupBottomSheetShipperInfoChild(child: View) {
+        btnShipperBottomSheet = child.findViewById(R.id.btn_close)
+        btnShipperBottomSheet?.setOnClickListener { bottomSheetShipperInfo?.dismiss() }
     }
 
 
--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shippingeditor/ShippingEditorItemAdapter.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shippingeditor/ShippingEditorItemAdapter.kt
@@ -1,5 +1,29 @@
 package com.tokopedia.editshipping.ui.shippingeditor
 
-class ShippingEditorItemAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ShippingEditorItemAdapter(private val listener: ShippingEditorItemAdapterListener): RecyclerView.Adapter<ShippingEditorItemAdapter.ShippingEditorViewHolder>() {
+
+    interface ShippingEditorItemAdapterListener {
+            fun onShipperInfoClicker()
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShippingEditorViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ShippingEditorViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    inner class ShippingEditorViewHolder(itemView: View, private val listener: ShippingEditorItemAdapterListener): RecyclerView.ViewHolder(itemView) {
+
+    }
 
 }
