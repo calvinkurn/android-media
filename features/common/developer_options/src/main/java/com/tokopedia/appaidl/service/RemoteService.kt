e4247814b5d92fcf9c173f3eb25a8304c33743fb
isfaaghyth <isfaaghyth@gmail.com>
2021-01-12 19:26:58 +0700

    move RemoteService into service package

---
+++ features/common/developer_options/src/main/java/com/tokopedia/appaidl/service/RemoteService.kt
@@ -0,0 +1,46 @@
+package com.tokopedia.appaidl.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.Bundle
+import com.tokopedia.appaidl.AppApi
+import com.tokopedia.appaidl.data.UserKey
+import com.tokopedia.appaidl.data.componentTargetName
+import com.tokopedia.user.session.UserSession
+
+class RemoteService: Service() {
+
+    private val userSession by lazy { UserSession(applicationContext) }
+
+    override fun onBind(intent: Intent) = binder
+    
+    private fun broadcastResult(tag: String, data: Bundle) {
+        sendBroadcast(Intent().apply {
+            `package` = componentTargetName()
+            action = tag
+            putExtras(data)
+        })
+    }
+    
+    private fun getUserSession(tag: String) {
+        val data = Bundle()
+        if (userSession.isLoggedIn) {
+            data.apply {
+                data.putBoolean(UserKey.IS_LOGIN, true)
+                putString(UserKey.NAME, userSession.name)
+                putString(UserKey.EMAIL, userSession.email)
+            }
+        } else {
+            data.putBoolean(UserKey.IS_LOGIN, false)
+        }
+
+        broadcastResult(tag, data)
+    }
+
+    private val binder = object : AppApi.Stub() {
+        override fun send(tag: String) {
+            getUserSession(tag)
+        }
+    }
+
+}
