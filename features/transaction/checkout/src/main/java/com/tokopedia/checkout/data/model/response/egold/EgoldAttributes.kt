f24092c29ff436a9eef73a0fc4944a5992b54ba6
irfan khoirul <irfankhoirul@gmail.com>
2021-01-12 11:10:42 +0700

    convert egold to kotlin

---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/model/response/egold/EgoldAttributes.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.checkout.data.model.response.egold
+
+import com.google.gson.annotations.SerializedName
+
+data class EgoldAttributes(
+    @SerializedName("eligible")
+    var isEligible: Boolean = false,
+    @SerializedName("is_tiering")
+    var isTiering: Boolean = false,
+    @SerializedName("range")
+    var egoldRange: EgoldRange = EgoldRange(),
+    @SerializedName("message")
+    var egoldMessage: EgoldMessage = EgoldMessage(),
+    @SerializedName("tier_data")
+    var egoldTieringDataArrayList: List<EgoldTieringData> = emptyList()
+)
