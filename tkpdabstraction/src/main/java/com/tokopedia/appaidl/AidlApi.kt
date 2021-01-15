6657d52602bd3354d606f93b8c7c535c5a625748
isfaaghyth <isfaaghyth@gmail.com>
2021-01-15 11:55:49 +0700

    use android context instead of application in aidlApi param

--- tkpdabstraction/src/main/java/com/tokopedia/appaidl/AidlApi.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/appaidl/AidlApi.kt
@@ -1,13 +1,12 @@
 package com.tokopedia.appaidl
 
-import android.app.Application
 import android.content.*
 import android.os.Bundle
 import com.tokopedia.appaidl.data.*
 import com.tokopedia.appaidl.ui.ServiceView
 
 open class AidlApi(
-        private val application: Application,
+        private val context: Context,
         private val listener: ReceiverListener
 ) {
 
@@ -15,7 +14,7 @@ open class AidlApi(
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent?.action == CUSTOMER_APP || intent?.action == SELLER_APP) {
                 intent.action?.let {
-                    application.unregisterReceiver(this)
+                    this@AidlApi.context.unregisterReceiver(this)
                     listener.onAidlReceive(it, intent.extras)
                 }
             }
@@ -26,12 +25,12 @@ open class AidlApi(
         // the serviceView is serviceConnection to register the receiver in activity and send the data
         val serviceView = ServiceView(aidlTag()) { tag, service ->
             if (service != null) {
-                application.registerReceiver(broadcastReceiver, IntentFilter().apply { addAction(tag) })
+                context.registerReceiver(broadcastReceiver, IntentFilter().apply { addAction(tag) })
                 service.send(tag)
             }
         }
 
-        val success = application.bindService(Intent().apply {
+        val success = context.bindService(Intent().apply {
             component = ComponentName(componentTargetName(), REMOTE_SERVICE)
         }, serviceView, Context.BIND_AUTO_CREATE)
 
