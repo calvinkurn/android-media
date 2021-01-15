7f434d115ee7c6ff1c21ca20e88b2bfb4f9cee9b
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-01-15 11:12:26 +0700

    provideUserSession

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/di/shippingeditor/ShippingEditorModule.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/di/shippingeditor/ShippingEditorModule.kt
@@ -1,7 +1,11 @@
 package com.tokopedia.editshipping.di.shippingeditor
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 
@@ -13,4 +17,7 @@ class ShippingEditorModule {
             GraphqlInteractor.getInstance().graphqlRepository
 
 
+    @Provides
+    @ShippingEditorScope
+    fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface = UserSession(context)
 }
