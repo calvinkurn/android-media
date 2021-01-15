3e9572cc5e35def0b7a85deb23cd407c02039a19
lalit <lalit.singh@tokopedia.com>
2021-01-15 10:49:33 +0530

    [HWP] Remote Config Added for React How to pay page

--- features/payment/pms/src/main/java/com/tokopedia/pms/howtopay_native/ui/activity/HowToPayActivity.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/howtopay_native/ui/activity/HowToPayActivity.kt
@@ -11,8 +11,11 @@ import com.tokopedia.pms.howtopay_native.di.DaggerHowToPayComponent
 import com.tokopedia.pms.howtopay_native.di.HowToPayComponent
 import com.tokopedia.pms.howtopay_native.di.HowToPayModule
 import com.tokopedia.pms.howtopay_native.ui.fragment.HowToPayFragment
+import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 
 class HowToPayActivity : BaseSimpleActivity(), HasComponent<HowToPayComponent> {
+
+    private val KEY_IS_NEW_HOW_TO_PAY_PAGE = "app_flag_is_native_howtopay"
 
     override fun getNewFragment(): Fragment? = HowToPayFragment.getInstance(this, intent.extras)
 
@@ -25,9 +28,33 @@ class HowToPayActivity : BaseSimpleActivity(), HasComponent<HowToPayComponent> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*val reactIntent = Intent(this, ReactNativeHowToPayActivity::class.java)
-        reactIntent.data = intent.data
-        startActivity(intent)*/
+        if (!isNewHowToPayPageEnabled())
+            startRectHowToPayActivity()
+    }
+
+    private fun startRectHowToPayActivity() {
+        var bundle = intent.extras
+        if (bundle == null) {
+            bundle = Bundle()
+        }
+
+        val newBundle = Bundle()
+        for (key in bundle.keySet()) {
+            newBundle.putString(key, bundle.getString(key))
+        }
+        val reactIntent = Intent(this, ReactNativeHowToPayActivity::class.java)
+        reactIntent.putExtras(newBundle)
+        startActivity(reactIntent)
+        finish()
+    }
+
+    private fun isNewHowToPayPageEnabled(): Boolean {
+        return try {
+            val remoteConfig = FirebaseRemoteConfigImpl(this)
+            return remoteConfig.getBoolean(KEY_IS_NEW_HOW_TO_PAY_PAGE, true)
+        } catch (e: Exception) {
+            true
+        }
     }
 
 }
