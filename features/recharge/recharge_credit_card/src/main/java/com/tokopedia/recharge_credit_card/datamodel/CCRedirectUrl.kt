9105751e4b86b59b5199cb6a24e2448bc501ecbb
Nabilla <nabilla.sabbaha@tokopedia.com>
2021-01-18 13:43:56 +0700

    migrate PCIDSS CC to use common network and remove old implementation

--- features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/datamodel/CCRedirectUrl.kt
+++ features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/datamodel/CCRedirectUrl.kt
@@ -1,7 +1,16 @@
-package com.tokopedia.recharge_credit_card.data
+package com.tokopedia.recharge_credit_card.datamodel
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+
+class CCRedirectUrlResponse(
+        @SerializedName("success")
+        @Expose
+        val success: Boolean = false,
+        @SerializedName("data")
+        @Expose
+        val data: CCRedirectUrl = CCRedirectUrl(),
+)
 
 class CCRedirectUrl(
         @SerializedName("message_error")
