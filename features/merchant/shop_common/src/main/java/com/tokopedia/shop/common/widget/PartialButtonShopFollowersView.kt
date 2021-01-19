92e8f0c09d32017065ede5c08b968b6d02ecdb9b
yehezkiel <yehezkiel@tokopedia.com>
2021-01-19 17:45:22 +0700

    change visibility

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/widget/PartialButtonShopFollowersView.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/widget/PartialButtonShopFollowersView.kt
@@ -96,7 +96,7 @@ class PartialButtonShopFollowersView private constructor(val view: View, private
     }
 
     private fun animateSlideUp() = with(view) {
-        view.visibility = View.VISIBLE
+        visibility = View.VISIBLE
         animate().translationY(0F).setDuration(GONE_ANIMATION_DURATION).setListener(null)
     }
 
@@ -104,7 +104,7 @@ class PartialButtonShopFollowersView private constructor(val view: View, private
         animate().translationY(view.height.toFloat()).setDuration(GONE_ANIMATION_DURATION).setListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator?) {
                 super.onAnimationEnd(animation)
-                view.visibility = View.GONE
+                visibility = View.GONE
                 stopLoading()
             }
         })
