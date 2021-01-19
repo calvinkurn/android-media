60c5bce727b9c6f37518c7a5baff0cf1f918e6e4
isfaaghyth <isfaaghyth@gmail.com>
2021-01-19 21:43:37 +0700

    added note and comment in NotificationValidationManager

--- tkpdabstraction/src/main/java/com/tokopedia/appaidl/manager/NotificationValidationManager.kt
+++ tkpdabstraction/src/main/java/com/tokopedia/appaidl/manager/NotificationValidationManager.kt
@@ -15,29 +15,44 @@ class NotificationValidationManager(
 
     private val userSession by lazy { UserSession(context) }
 
-    private fun validationByMessage(): NotificationPriorityType {
-        val mainAppPriority = priorities[KEY_MAIN_PRIORITY]?: ""
-        val sellerAppPriority = priorities[KEY_SELLER_PRIORITY]?: ""
+    // a mapper to convert prioritiesMap into a sealed class of PriorityType
+    private fun validationByPriorities(): NotificationPriorityType {
+        val mainAppPriority = priorities[KEY_MAIN_PRIORITY]?.toInt()?: 0
+        val sellerAppPriority = priorities[KEY_SELLER_PRIORITY]?.toInt()?: 0
+
+        // render the CM push notification
         return when {
-            mainAppPriority.toInt() > sellerAppPriority.toInt() -> NotificationPriorityType.MainApp
-            mainAppPriority.toInt() < sellerAppPriority.toInt() -> NotificationPriorityType.SellerApp
+            // for mainApp
+            mainAppPriority > sellerAppPriority -> NotificationPriorityType.MainApp
+
+            // for sellerApp
+            mainAppPriority < sellerAppPriority -> NotificationPriorityType.SellerApp
+
+            // both of apps
             else -> NotificationPriorityType.Both
         }
     }
 
+    /*
+    * validate;
+    * to showing the CM push notification based on app priorities
+    * @param(bundle): comes from AIDL services, which is the data gathering from another apps
+    * @param(notify): a callback for a valid type to rendering the notification
+    * */
     fun validate(bundle: Bundle?, notify: () -> Unit) {
-        when (validationByMessage()) {
+        when (validationByPriorities()) {
             is NotificationPriorityType.SellerApp -> {
                 if (GlobalConfig.isSellerApp()) {
                     notify()
                 }
             }
             is NotificationPriorityType.MainApp -> {
+                if (GlobalConfig.isSellerApp()) return
+
                 val isSellerAppInstalled = context.isInstalled(SELLER_APP)
                 val isSellerAppLogged = bundle?.getBoolean(UserKey.IS_LOGIN)?: false
                 val sellerAppUserId = bundle?.getString(UserKey.USER_ID)?: ""
 
-                if (GlobalConfig.isSellerApp()) return
                 if (isSellerAppInstalled && isSellerAppLogged && userSession.userId == sellerAppUserId) {
                     notify()
                 }
@@ -56,7 +71,20 @@ class NotificationValidationManager(
         private const val KEY_MAIN_PRIORITY = "seller_priority"
         private const val KEY_SELLER_PRIORITY = "seller_priority"
 
-        //TODO: please remove this after notification_priority has merged from BE
+        fun setAppPriorities(
+                mainPriority: String,
+                sellerPriority: String
+        ): Map<String, String> {
+            return mutableMapOf<String, String>().also {
+                it[KEY_MAIN_PRIORITY] = mainPriority
+                it[KEY_SELLER_PRIORITY] = sellerPriority
+            }
+        }
+
+        /*
+        * converting from { "message": any() } to appPriorities based on contains character
+        * TODO: please remove this after notification_priority has merged from BE
+        * */
         fun mockPriorities(message: String): Map<String, String> {
             val mockPrioritiesMap = mutableMapOf<String, String>()
             val dataMessage = message.toLowerCase().trim()
@@ -77,16 +105,6 @@ class NotificationValidationManager(
             }
 
             return mockPrioritiesMap
-        }
-
-        fun setAppPriorities(
-                mainPriority: String,
-                sellerPriority: String
-        ): Map<String, String> {
-            return mutableMapOf<String, String>().also {
-                it[KEY_MAIN_PRIORITY] = mainPriority
-                it[KEY_SELLER_PRIORITY] = sellerPriority
-            }
         }
     }
 
