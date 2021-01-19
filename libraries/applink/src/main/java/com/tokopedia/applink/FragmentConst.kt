1a718e99cd72d4ef4ab104c5f762de3b2d621eb3
errysuprayogi <>
2021-01-19 11:55:25 +0700

    add FragmentConst.kt

---
+++ libraries/applink/src/main/java/com/tokopedia/applink/FragmentConst.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.applink
+
+/**
+ * Author errysuprayogi on 19,January,2021
+ */
+object FragmentConst {
+    const val FEED_PLUS_CONTAINER_FRAGMENT = "com.tokopedia.feedplus.view.fragment.FeedPlusContainerFragment"
+}
