a7f47f6b7d3e740a9d582d9ab2e1273f6f15248e
lukaskris12 <lukaskris12@gmail.com>
2021-01-20 11:04:10 +0700

    Fix Margin Dynamic Icon and click

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeFragment.kt
@@ -1245,7 +1245,7 @@ open class HomeFragment : BaseDaggerFragment(),
                 CategoryNavigationCallback(context, this),
                 RechargeBUWidgetCallback(context, this),
                 BannerComponentCallback(context, this),
-                DynamicIconComponentCallback(this)
+                DynamicIconComponentCallback(context, this)
         )
         val asyncDifferConfig = AsyncDifferConfig.Builder(HomeVisitableDiffUtil())
                 .setBackgroundThreadExecutor(Executors.newSingleThreadExecutor())
