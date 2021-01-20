0491dfee1f0fc613e66a29a84271a7f220749070
Muhammad Furqan <furqan@tokopedia.com>
2021-01-20 18:46:48 +0700

    [Flight] Refactor Flight Order Detail Browser Activity to extends Base Download Html Webview

--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/activity/FlightOrderDetailBrowserActivity.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/activity/FlightOrderDetailBrowserActivity.kt
@@ -3,80 +3,39 @@ package com.tokopedia.flight.orderdetail.presentation.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.flight.FlightComponentInstance
-import com.tokopedia.flight.R
 import com.tokopedia.flight.orderdetail.di.DaggerFlightOrderDetailComponent
 import com.tokopedia.flight.orderdetail.di.FlightOrderDetailComponent
 import com.tokopedia.flight.orderdetail.presentation.fragment.FlightOrderDetailBrowserFragment
+import com.tokopedia.webview.download.BaseDownloadHtmlActivity
 
-class FlightOrderDetailBrowserActivity : BaseSimpleActivity(), HasComponent<FlightOrderDetailComponent> {
+class FlightOrderDetailBrowserActivity : BaseDownloadHtmlActivity(), HasComponent<FlightOrderDetailComponent> {
 
-    private lateinit var title: String
-    private lateinit var invoiceId: String
-    private lateinit var htmlContent: String
     private lateinit var orderStatus: String
 
-    private lateinit var orderDetailFragment: FlightOrderDetailBrowserFragment
-
-    override fun getNewFragment(): Fragment? {
-        orderDetailFragment = FlightOrderDetailBrowserFragment.getInstance(invoiceId, htmlContent, orderStatus)
-        return orderDetailFragment
-    }
-
+    override fun getNewFragment(): Fragment =
+            FlightOrderDetailBrowserFragment.getInstance(invoiceId, htmlContent, orderStatus).also {
+                baseDownloadFragment = it
+            }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_flight_order_detail_browser)
-
-        title = intent.getStringExtra(EXTRA_TITLE) ?: ""
-        invoiceId = intent.getStringExtra(EXTRA_INVOICE_ID) ?: ""
-        htmlContent = intent.getStringExtra(EXTRA_HTML_CONTENT) ?: ""
-        orderStatus = intent.getStringExtra(EXTRA_ORDER_STATUS) ?: ""
-
         savedInstanceState?.let {
-            title = it.getString(EXTRA_TITLE, "")
-            invoiceId = it.getString(EXTRA_INVOICE_ID, "")
-            htmlContent = it.getString(EXTRA_HTML_CONTENT, "")
             orderStatus = it.getString(EXTRA_ORDER_STATUS, "")
         }
 
-        super.onCreate(savedInstanceState)
+        intent.getStringExtra(EXTRA_ORDER_STATUS)?.let {
+            orderStatus = it
+        }
 
-        toolbar.title = title
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        super.onCreate(savedInstanceState)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(EXTRA_HTML_CONTENT, htmlContent)
-        outState.putString(EXTRA_TITLE, title)
-        outState.putString(EXTRA_INVOICE_ID, invoiceId)
+
         outState.putString(EXTRA_ORDER_STATUS, orderStatus)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menu?.clear()
-        menuInflater.inflate(R.menu.menu_flight_e_ticket, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_flight_download -> {
-                if (::orderDetailFragment.isInitialized)
-                    orderDetailFragment.printETicket()
-                return true
-            }
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-        }
-        return false
     }
 
     override fun getComponent(): FlightOrderDetailComponent =
@@ -85,9 +44,6 @@ class FlightOrderDetailBrowserActivity : BaseSimpleActivity(), HasComponent<Flig
                     .build()
 
     companion object {
-        private const val EXTRA_TITLE = "EXTRA_TITLE"
-        private const val EXTRA_INVOICE_ID = "EXTRA_INVOICE_ID"
-        private const val EXTRA_HTML_CONTENT = "EXTRA_HTML_CONTENT"
         private const val EXTRA_ORDER_STATUS = "EXTRA_ORDER_STATUS"
 
         fun getIntent(context: Context, title: String,
@@ -95,9 +51,9 @@ class FlightOrderDetailBrowserActivity : BaseSimpleActivity(), HasComponent<Flig
                       htmlContent: String,
                       orderStatus: String): Intent =
                 Intent(context, FlightOrderDetailBrowserActivity::class.java)
-                        .putExtra(EXTRA_TITLE, title)
-                        .putExtra(EXTRA_INVOICE_ID, invoiceId)
-                        .putExtra(EXTRA_HTML_CONTENT, htmlContent)
+                        .putExtra(KEY_WEBVIEW_TITLE, title)
+                        .putExtra(KEY_INVOICE_ID, invoiceId)
+                        .putExtra(KEY_HTML_CONTENT, htmlContent)
                         .putExtra(EXTRA_ORDER_STATUS, orderStatus)
     }
 }
--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailBrowserFragment.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailBrowserFragment.kt
@@ -1,28 +1,18 @@
 package com.tokopedia.flight.orderdetail.presentation.fragment
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.print.PrintAttributes
-import android.print.PrintManager
-import android.util.Base64
-import android.util.DisplayMetrics
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.flight.R
 import com.tokopedia.flight.common.util.FlightAnalytics
 import com.tokopedia.flight.orderdetail.di.FlightOrderDetailComponent
 import com.tokopedia.user.session.UserSessionInterface
-import kotlinx.android.synthetic.main.activity_flight_order_detail_browser.*
+import com.tokopedia.webview.download.BaseDownloadHtmlActivity
+import com.tokopedia.webview.download.BaseDownloadHtmlFragment
 import javax.inject.Inject
 
 
 /**
  * @author by furqan on 16/11/2020
  */
-class FlightOrderDetailBrowserFragment : BaseDaggerFragment() {
+class FlightOrderDetailBrowserFragment : BaseDownloadHtmlFragment() {
 
     @Inject
     lateinit var flightAnalytics: FlightAnalytics
@@ -30,51 +20,27 @@ class FlightOrderDetailBrowserFragment : BaseDaggerFragment() {
     @Inject
     lateinit var userSession: UserSessionInterface
 
-    private lateinit var invoiceId: String
-    private lateinit var htmlContent: String
     private lateinit var orderStatus: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        invoiceId = arguments?.getString(EXTRA_INVOICE_ID) ?: ""
-        htmlContent = arguments?.getString(EXTRA_HTML_CONTENT) ?: ""
-        orderStatus = arguments?.getString(EXTRA_ORDER_STATUS) ?: ""
         savedInstanceState?.let {
-            if (it.containsKey(EXTRA_INVOICE_ID))
-                invoiceId = it.getString(EXTRA_INVOICE_ID) ?: ""
-
-            if (it.containsKey(EXTRA_HTML_CONTENT))
-                htmlContent = it.getString(EXTRA_HTML_CONTENT) ?: ""
-
             if (it.containsKey(EXTRA_ORDER_STATUS))
                 orderStatus = it.getString(EXTRA_ORDER_STATUS) ?: ""
         }
-    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.activity_flight_order_detail_browser, container, false)
+        arguments?.let { args ->
+            args.getString(EXTRA_ORDER_STATUS)?.let {
+                orderStatus = it
+            }
+        }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val displayMetrics = DisplayMetrics()
-        activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
-
-        htmlContent = Base64.encodeToString(htmlContent.toByteArray(), Base64.DEFAULT)
-
-        wvFlightOrderDetail.settings.javaScriptEnabled = true
-        wvFlightOrderDetail.settings.builtInZoomControls = true
-        wvFlightOrderDetail.settings.displayZoomControls = false
-        wvFlightOrderDetail.settings.loadWithOverviewMode = true
-        wvFlightOrderDetail.settings.useWideViewPort = true
-        wvFlightOrderDetail.loadData(htmlContent, MIME_TYPE, ENCODING)
+        super.onCreate(savedInstanceState)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(EXTRA_HTML_CONTENT, htmlContent)
-        outState.putString(EXTRA_INVOICE_ID, invoiceId)
+
         outState.putString(EXTRA_ORDER_STATUS, orderStatus)
     }
 
@@ -84,36 +50,22 @@ class FlightOrderDetailBrowserFragment : BaseDaggerFragment() {
         getComponent(FlightOrderDetailComponent::class.java).inject(this)
     }
 
-    fun printETicket() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            context?.let {
-                val printManager = it.getSystemService(Context.PRINT_SERVICE) as PrintManager
-                val printAdapter = wvFlightOrderDetail.createPrintDocumentAdapter("Tokopedia_Tiket_$invoiceId")
-                val jobName = "Tokopedia_Flight_$invoiceId"
-                printManager.print(jobName, printAdapter, PrintAttributes.Builder().build())
-                wvFlightOrderDetail.setInitialScale(1)
-                flightAnalytics.eventDownloadETicketOrderDetail(
-                        "$orderStatus - $invoiceId",
-                        userSession.userId
-                )
-            }
-        }
+    override fun sendDownloadTrack() {
+        flightAnalytics.eventDownloadETicketOrderDetail(
+                "$orderStatus - $invoiceId",
+                userSession.userId
+        )
     }
 
     companion object {
-        private const val EXTRA_HTML_CONTENT = "EXTRA_HTML_CONTENT"
-        private const val EXTRA_INVOICE_ID = "EXTRA_INVOICE_ID"
         private const val EXTRA_ORDER_STATUS = "EXTRA_ORDER_STATUS"
-
-        private const val MIME_TYPE = "text/html; charset=UTF-8"
-        private const val ENCODING = "base64"
 
         fun getInstance(invoiceId: String, htmlContent: String, orderStatus: String)
                 : FlightOrderDetailBrowserFragment =
                 FlightOrderDetailBrowserFragment().also {
                     it.arguments = Bundle().apply {
-                        putString(EXTRA_INVOICE_ID, invoiceId)
-                        putString(EXTRA_HTML_CONTENT, htmlContent)
+                        putString(BaseDownloadHtmlActivity.KEY_INVOICE_ID, invoiceId)
+                        putString(BaseDownloadHtmlActivity.KEY_HTML_CONTENT, htmlContent)
                         putString(EXTRA_ORDER_STATUS, orderStatus)
                     }
                 }
