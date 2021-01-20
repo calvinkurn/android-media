8f3826b13ec9c8ba1c84290ade0e387b254ec83e
Ade Fulki Hadian <ade.hadian@tokopedia.com>
2021-01-20 11:41:14 +0700

    shop creation unit test
    # Conflicts:
    #	libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/shopcreation/domain/usecase/RegisterCheckUseCase.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/shopcreation/domain/usecase/RegisterCheckUseCase.kt
@@ -6,6 +6,7 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.loginregister.common.DispatcherProvider
 import com.tokopedia.loginregister.shopcreation.di.ShopCreationQueryConstant
 import com.tokopedia.loginregister.shopcreation.domain.param.RegisterCheckParam
 import com.tokopedia.loginregister.shopcreation.domain.pojo.RegisterCheckData
@@ -28,12 +29,11 @@ class RegisterCheckUseCase @Inject constructor(
         @Named(ShopCreationQueryConstant.MUTATION_REGISTER_CHECK)
         private val query: String,
         private val graphqlRepository: GraphqlRepository,
-        @Named(ShopCreationQueryConstant.DISPATCHERS_IO)
-        private val dispatcher: CoroutineDispatcher
+        private val dispatcherProvider: DispatcherProvider
 ) : BaseUseCaseWithParam<RegisterCheckParam, Result<RegisterCheckData>>() {
 
     override suspend fun getData(parameter: RegisterCheckParam): Result<RegisterCheckData> {
-        val response = withContext(dispatcher) {
+        val response = withContext(dispatcherProvider.io()) {
             val cacheStrategy =
                     GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build()
 
--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestApp.java
@@ -463,5 +463,4 @@ public class InstrumentationTestApp extends CoreNetworkApplication
     public void sendAnalyticsAnomalyResponse(String s, String s1, String s2, String s3, String s4) {
 
     }
-
 }
