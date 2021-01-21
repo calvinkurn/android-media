1d7cd2f5cd90d1d46a8cbb17a7c7d5919d1c2f38
Muhammad Furqan <furqan@tokopedia.com>
2021-01-21 11:05:37 +0700

    [Flight] Revert unscoped component

--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/di/FlightOrderDetailComponent.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/di/FlightOrderDetailComponent.kt
@@ -12,6 +12,7 @@ import dagger.Component
 /**
  * @author by furqan on 19/10/2020
  */
+@FlightOrderDetailScope
 @Component(modules = [FlightOrderDetailModule::class, FlightOrderDetailViewModelModule::class],
         dependencies = [FlightComponent::class])
 interface FlightOrderDetailComponent {
