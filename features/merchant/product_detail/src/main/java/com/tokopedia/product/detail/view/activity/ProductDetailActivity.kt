7989d175a00037ddd6352b00a79f36107047157b
yehezkiel <yehezkiel@tokopedia.com>
2021-01-21 16:02:20 +0700

    add import

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/activity/ProductDetailActivity.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/activity/ProductDetailActivity.kt
@@ -17,6 +17,7 @@ import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.product.detail.R
 import com.tokopedia.product.detail.data.util.ProductDetailConstant
 import com.tokopedia.product.detail.data.util.ProductDetailLoadTimeMonitoringListener
+import com.tokopedia.product.detail.view.fragment.DynamicProductDetailFragment
 import com.tokopedia.product.detail.view.fragment.DynamicProductDetailFragmentDiffutil
 import com.tokopedia.product.detail.view.fragment.ProductVideoDetailFragment
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
