c1b4bc2c773db21ceafb22cb2c689fee8359765b
hansenpw <hansen.wijaya@tokopedia.com>
2021-01-21 17:24:49 +0700

    add template

---
+++ features/transaction/promo_checkout_marketplace/src/androidTest/java/com/tokopedia/promocheckoutmarketplace/PromoOccTrackingTest.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.promocheckoutmarketplace
+
+import androidx.test.rule.ActivityTestRule
+import com.tokopedia.promocheckoutmarketplace.presentation.PromoCheckoutActivity
+import org.junit.Rule
+import org.junit.Test
+
+class PromoOccTrackingTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(PromoCheckoutActivity::class.java, false, false)
+
+    @Test
+    fun test() {
+
+    }
+
+}
