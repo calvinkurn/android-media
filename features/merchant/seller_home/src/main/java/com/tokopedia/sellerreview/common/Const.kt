9a8386bdd40188df581646869d78b7861389447b
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-21 17:06:16 +0800

    create rating const

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/Const.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.sellerreview.common
+
+/**
+ * Created By @ilhamsuaib on 21/01/21
+ */
+
+object Const {
+
+    const val LOTTIE_RATING_1 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_sad.json"
+    const val LOTTIE_RATING_2 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_sad.json"
+    const val LOTTIE_RATING_3 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_standard.json"
+    const val LOTTIE_RATING_4 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_happy.json"
+    const val LOTTIE_RATING_5 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_very_happy.json"
+}
