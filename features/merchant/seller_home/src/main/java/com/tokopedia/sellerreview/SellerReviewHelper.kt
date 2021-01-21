c88fd6534d8c5a0556f4538c29c2c0b8f135c525
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-21 17:06:31 +0800

    create review helper

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/SellerReviewHelper.kt
@@ -0,0 +1,22 @@
+package com.tokopedia.sellerreview
+
+import android.content.Context
+import android.os.Handler
+import androidx.fragment.app.FragmentManager
+import com.tokopedia.sellerreview.view.bottomsheet.RatingBottomSheet
+
+/**
+ * Created By @ilhamsuaib on 21/01/21
+ */
+
+object SellerReviewHelper {
+
+    private const val POPUP_DELAY = 2000L
+
+    fun checkForReview(context: Context, fm: FragmentManager) {
+        Handler().postDelayed({
+            RatingBottomSheet.createInstance()
+                    .show(fm)
+        }, POPUP_DELAY)
+    }
+}
