1ea931fe78a6f96efee18fdbdebb571f37ab31c0
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-22 17:08:42 +0800

    create base bottom sheet

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/view/bottomsheet/BaseBottomSheet.kt
@@ -0,0 +1,50 @@
+package com.tokopedia.sellerreview.view.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.tokopedia.sellerhome.R
+import com.tokopedia.unifycomponents.BottomSheetUnify
+
+/**
+ * Created By @ilhamsuaib on 22/01/21
+ */
+
+abstract class BaseBottomSheet : BottomSheetUnify() {
+
+    protected var childView: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setChild(inflater, container)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun dismiss() {
+        view?.post {
+            if (isVisible) {
+                super.dismiss()
+            }
+        }
+    }
+
+    abstract fun show(fm: FragmentManager)
+
+    protected abstract fun getResLayout(): Int
+
+    protected abstract fun setupView(): Unit?
+
+    private fun setChild(inflater: LayoutInflater, container: ViewGroup?) {
+        val child = inflater.inflate(getResLayout(), container, false)
+        childView = child
+        setChild(child)
+        setupView()
+    }
+}
