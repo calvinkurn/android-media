dc3420b955bac78d5fe55b49017be79cc151eb3d
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-22 10:16:38 +0800

    add lottie animation URL constant

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/Const.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/Const.kt
@@ -6,9 +6,9 @@ package com.tokopedia.sellerreview.common
 
 object Const {
 
-    const val LOTTIE_RATING_1 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_sad.json"
-    const val LOTTIE_RATING_2 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_sad.json"
-    const val LOTTIE_RATING_3 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_standard.json"
-    const val LOTTIE_RATING_4 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_happy.json"
-    const val LOTTIE_RATING_5 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_very_happy.json"
+    const val LOTTIE_URL_RATING_1 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_cry.json"
+    const val LOTTIE_URL_RATING_2 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_sad.json"
+    const val LOTTIE_URL_RATING_3 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_standard.json"
+    const val LOTTIE_URL_RATING_4 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_happy.json"
+    const val LOTTIE_URL_RATING_5 = "https://ecs7.tokopedia.net/img/android/lottie/lottie_anim_sir_pedi_very_happy.json"
 }
