cba5b69f9d06f2464aa0abfd2700f1da4474dc68
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-23 21:51:04 +0800

    finishing UI thank you bottom sheet

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/view/bottomsheet/ThankYouBottomSheet.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/view/bottomsheet/ThankYouBottomSheet.kt
@@ -1,7 +1,10 @@
 package com.tokopedia.sellerreview.view.bottomsheet
 
 import androidx.fragment.app.FragmentManager
+import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.sellerhome.R
+import com.tokopedia.sellerreview.common.Const
+import kotlinx.android.synthetic.main.sir_thank_you_bottom_sheet.view.*
 
 /**
  * Created By @ilhamsuaib on 22/01/21
@@ -13,16 +16,14 @@ class ThankYouBottomSheet : BaseBottomSheet() {
         private const val TAG = "ThankYouBottomSheet"
 
         fun createInstance(): ThankYouBottomSheet {
-            return ThankYouBottomSheet().apply {
-                overlayClickDismiss = false
-            }
+            return ThankYouBottomSheet()
         }
     }
 
     override fun getResLayout(): Int = R.layout.sir_thank_you_bottom_sheet
 
     override fun setupView() = childView?.run {
-
+        imgSirTankYou.loadImage(Const.IMG_THANK_YOU)
     }
 
     override fun show(fm: FragmentManager) {
