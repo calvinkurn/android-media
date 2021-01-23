032badbbc0ebe346306515d211c87462e21838cd
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-23 22:11:27 +0800

    fix flow seller review

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/SellerReviewHelper.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/SellerReviewHelper.kt
@@ -5,6 +5,7 @@ import android.os.Handler
 import androidx.fragment.app.FragmentManager
 import com.tokopedia.sellerreview.view.bottomsheet.FeedbackBottomSheet
 import com.tokopedia.sellerreview.view.bottomsheet.RatingBottomSheet
+import com.tokopedia.sellerreview.view.bottomsheet.ThankYouBottomSheet
 
 /**
  * Created By @ilhamsuaib on 21/01/21
@@ -18,7 +19,9 @@ object SellerReviewHelper {
         Handler().postDelayed({
             RatingBottomSheet.createInstance()
                     .setOnSubmittedListener {
-                        showFeedBackBottomSheet(fm)
+                        Handler().postDelayed({
+                            showFeedBackBottomSheet(fm)
+                        }, 500)
                     }
                     .show(fm)
         }, POPUP_DELAY)
@@ -26,6 +29,16 @@ object SellerReviewHelper {
 
     private fun showFeedBackBottomSheet(fm: FragmentManager) {
         FeedbackBottomSheet.createInstance()
+                .setOnSubmittedListener {
+                    Handler().postDelayed({
+                        showTankYouBottomSheet(fm)
+                    }, 500)
+                }
+                .show(fm)
+    }
+
+    private fun showTankYouBottomSheet(fm: FragmentManager) {
+        ThankYouBottomSheet.createInstance()
                 .show(fm)
     }
 }
