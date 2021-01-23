b35102b4316ce4cb877556cc5abfd765f85d0ee7
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-01-23 21:47:41 +0800

    finishing UI feedback form

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/view/bottomsheet/FeedbackBottomSheet.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/view/bottomsheet/FeedbackBottomSheet.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.sellerreview.view.bottomsheet
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.FragmentManager
 import com.tokopedia.sellerhome.R
 import com.tokopedia.sellerreview.common.Const
@@ -27,6 +29,24 @@ class FeedbackBottomSheet : BaseBottomSheet() {
 
     override fun setupView() = childView?.run {
         imgSirFeedback.setImageUrl(Const.IMG_REQUEST_FEEDBACK)
+        tauSirFeedback.textAreaInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                btnSirSubmitFeedback.isEnabled = !s.isNullOrBlank()
+            }
+        })
+        btnSirSubmitFeedback.setOnClickListener {
+            btnSirSubmitFeedback.isLoading = true
+            this@FeedbackBottomSheet.dismiss()
+            onSubmitted?.invoke()
+        }
     }
 
     override fun show(fm: FragmentManager) {
