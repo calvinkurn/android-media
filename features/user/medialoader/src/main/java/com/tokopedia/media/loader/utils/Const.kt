cae1ad1bcfbd2bb6bac5406732785fcc8f1d3bcd
isfaaghyth <isfaaghyth@gmail.com>
2021-01-26 10:37:47 +0700

    remove unused constant variable

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/utils/Const.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/utils/Const.kt
@@ -1,36 +1,16 @@
 package com.tokopedia.media.loader.utils
 
 import android.net.Uri
-import com.bumptech.glide.load.Key
 import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.signature.ObjectKey
 
 // wrapper of GlideException
 typealias MediaException = GlideException
-
-/*
-* Client hints: ECT
-* The first client hints that will be used for adaptive is Effective Connection Types (ECT).
-* Beside ECT, there are also other hints that we may take to consideration: Downlink and RTT.
-* But for the first phase, ECT is giving us enough information regarding the client’s
-* network capabilities. See this article to read the details.
-* */
-const val HEADER_ECT = "ECT"
 
 // url parameter of blur hashing
 const val BLUR_HASH_QUERY = "b"
 
 // default rounded
 const val DEFAULT_ROUNDED = 5.0f
-
-// custom signature
-fun Key?.mediaSignature(glideUrl: GlideUrl): Key {
-    if (this != null) return this
-    val imageUrl = glideUrl.toStringUrl()
-    val connectionType = glideUrl.headers[HEADER_ECT].toString()
-    return ObjectKey("$imageUrl&connection=$connectionType")
-}
 
 fun String.toUri(): Uri? {
     return Uri.parse(this)
