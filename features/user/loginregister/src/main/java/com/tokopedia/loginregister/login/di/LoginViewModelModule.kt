72e727c43e4e8ea29ca36610f69ff16065624601
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-01-26 10:10:45 -0800

    add LoginEmailPhoneViewModel.kt

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginViewModelModule.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginViewModelModule.kt
@@ -4,22 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
+import com.tokopedia.loginregister.login.view.presenter.LoginEmailPhoneViewModel
 import com.tokopedia.loginregister.login.view.viewmodel.SellerSeamlessViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
-/**
- * Created by Yoris Prayogo on 20/04/20.
- * Copyright (c) 2020 PT. Tokopedia All rights reserved.
- */
-
 @Module
-abstract class SeamlessSellerViewModelModule{
-
+abstract class LoginViewModelModule {
     @Binds
     @LoginScope
     internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(LoginEmailPhoneViewModel::class)
+    internal abstract fun provideLoginEmailPhoneViewModel(viewModel: LoginEmailPhoneViewModel): ViewModel
 
     @Binds
     @IntoMap
