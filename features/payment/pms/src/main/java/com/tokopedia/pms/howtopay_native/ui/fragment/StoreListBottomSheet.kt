b44631e75527556480953c51a25713b92a9c0f89
lalit <lalit.singh@tokopedia.com>
2021-01-27 17:01:42 +0530

    - Bottomsheet scroll and hide fix

--- features/payment/pms/src/main/java/com/tokopedia/pms/howtopay_native/ui/fragment/StoreListBottomSheet.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/howtopay_native/ui/fragment/StoreListBottomSheet.kt
@@ -1,8 +1,10 @@
 package com.tokopedia.pms.howtopay_native.ui.fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +14,7 @@ import com.tokopedia.pms.howtopay_native.ui.adapter.StoreListAdapter
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.kotlin.extensions.view.getScreenHeight
 import com.tokopedia.unifycomponents.toDp
+import com.tokopedia.unifycomponents.toPx
 
 class StoreListBottomSheet : BottomSheetUnify() {
 
@@ -19,14 +22,22 @@ class StoreListBottomSheet : BottomSheetUnify() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        showCloseIcon = true
-        isDragable = true
-        customPeekHeight = getScreenHeight().toDp()
+
         setTitle(getString(R.string.pms_hwp_store_list))
         childView = LayoutInflater.from(context).inflate(R.layout.pms_hwp_store_list,
                 null, false)
         setChild(childView)
         addStoreData()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottomSheetWrapper.setPadding(0, bottomSheetWrapper.paddingTop, 0, bottomSheetWrapper.paddingBottom)
+        bottomSheetAction.setMargin(marginRight = 16.toPx())
+        bottomSheetClose.setMargin(marginLeft = 16.toPx(), marginTop = 4.toPx(), marginRight = 12.toPx())
+        isDragable = true
+        isHideable = true
+        customPeekHeight = getScreenHeight().toDp()
     }
 
     private fun addStoreData() {
@@ -41,5 +52,22 @@ class StoreListBottomSheet : BottomSheetUnify() {
         fun showStoreList(supportFragmentManager: FragmentManager) {
             StoreListBottomSheet().show(supportFragmentManager, TAG)
         }
+    }
+}
+
+
+
+internal fun View.setMargin(marginLeft: Int = -1, marginTop: Int = -1, marginRight: Int = -1, marginBottom: Int = -1) {
+    val layoutParams = layoutParams as ViewGroup.MarginLayoutParams
+    val actualMarginLeft = if (marginLeft == -1) layoutParams.leftMargin else marginLeft
+    val actualMarginTop = if (marginTop == -1) layoutParams.topMargin else marginTop
+    val actualMarginRight = if (marginRight == -1) layoutParams.rightMargin else marginRight
+    val actualMarginBottom = if (marginBottom == -1) layoutParams.bottomMargin else marginBottom
+
+    layoutParams.setMargins(actualMarginLeft, actualMarginTop, actualMarginRight, actualMarginBottom)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        layoutParams.marginStart = actualMarginLeft
+        layoutParams.marginEnd = actualMarginRight
     }
 }
