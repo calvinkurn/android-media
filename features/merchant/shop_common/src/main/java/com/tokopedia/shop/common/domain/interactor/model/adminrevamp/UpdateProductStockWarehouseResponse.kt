350660832671fd0516527c8196e6a70266f1f876
rezagama <reza.hidayat@tokopedia.com>
2021-01-28 11:48:52 +0700

    Add getProductStatus

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/domain/interactor/model/adminrevamp/UpdateProductStockWarehouseResponse.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/domain/interactor/model/adminrevamp/UpdateProductStockWarehouseResponse.kt
@@ -3,6 +3,7 @@ package com.tokopedia.shop.common.domain.interactor.model.adminrevamp
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.network.data.model.response.Header
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 
 data class UpdateProductStockWarehouseResponse(
         @SerializedName("IMSUpdateProductWarehouse")
@@ -37,4 +38,17 @@ data class ProductStockWarehouse(
         @SerializedName("status")
         @Expose
         val status: Int? = null
-)
+) {
+        private companion object {
+                const val STATUS_ACTIVE = 1
+                const val STATUS_INACTIVE = 3
+        }
+
+        fun getProductStatus(): ProductStatus? {
+                return when (status) {
+                        STATUS_ACTIVE -> ProductStatus.ACTIVE
+                        STATUS_INACTIVE -> ProductStatus.INACTIVE
+                        else -> null
+                }
+        }
+}
