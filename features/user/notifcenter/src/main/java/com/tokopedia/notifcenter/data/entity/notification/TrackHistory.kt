7e7ace1b6564ca64cfe5e42fad6699d52a24da9b
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-01-29 18:54:03 +0700

    remove track history as data class

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/entity/notification/TrackHistory.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/data/entity/notification/TrackHistory.kt
@@ -3,7 +3,7 @@ package com.tokopedia.notifcenter.data.entity.notification
 
 import com.google.gson.annotations.SerializedName
 
-data class TrackHistory(
+class TrackHistory(
     @SerializedName("create_time_unix")
     val createTimeUnix: Long = 0,
     @SerializedName("title")
