8b7e3b9780f1a9ac3d16814885ed0941ce0bdcf5
Jessica Sean <jessica.sean@tokopedia.com>
2021-02-01 15:34:39 +0700

    fix: loading view on checkout page

--- features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/presentation/fragment/DigitalCartFragment.kt
+++ features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/presentation/fragment/DigitalCartFragment.kt
@@ -142,8 +142,6 @@ class DigitalCartFragment : BaseDaggerFragment(), TickerPromoStackingCheckoutVie
         super.onActivityCreated(savedInstanceState)
 
         viewModel.cartDigitalInfoData.observe(viewLifecycleOwner, Observer {
-            showContent()
-
             digitalSubscriptionParams.isSubscribed = it.crossSellingType == DigitalCartCrossSellingType.SUBSCRIBED.id
             sendGetCartAndCheckoutAnalytics()
             renderCartDigitalInfoData(it)
@@ -202,8 +200,8 @@ class DigitalCartFragment : BaseDaggerFragment(), TickerPromoStackingCheckoutVie
         })
 
         viewModel.showLoading.observe(viewLifecycleOwner, Observer { showLoader ->
-            if (showLoader) showLoading()
-            else showContent()
+            if (showLoader) loaderCheckout.visibility = View.VISIBLE
+            else loaderCheckout.visibility = View.GONE
         })
 
         viewModel.isNeedOtp.observe(viewLifecycleOwner, Observer {
@@ -534,16 +532,6 @@ class DigitalCartFragment : BaseDaggerFragment(), TickerPromoStackingCheckoutVie
     private fun getCategoryName(): String = getCartDigitalInfoData().attributes?.categoryName ?: ""
     private fun getOperatorName(): String = getCartDigitalInfoData().attributes?.operatorName ?: ""
     private fun getProductId(): Int = cartPassData?.productId?.toIntOrNull() ?: 0
-
-    private fun showLoading() {
-        contentCheckout.visibility = View.GONE
-        loaderCheckout.visibility = View.VISIBLE
-    }
-
-    private fun showContent() {
-        contentCheckout.visibility = View.VISIBLE
-        loaderCheckout.visibility = View.GONE
-    }
 
     override fun onClickUsePromo() {
         val attributes = getCartDigitalInfoData().attributes
