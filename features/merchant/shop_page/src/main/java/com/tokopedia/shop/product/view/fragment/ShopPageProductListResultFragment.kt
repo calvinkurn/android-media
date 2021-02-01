49de110e083346d8acbab23bb0996b6be3860f5d
saidfaisal <said.faisal@tokopedia.com>
2021-02-01 20:10:28 +0700

    change old mvc to new mvc in product tab

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/fragment/ShopPageProductListResultFragment.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/view/fragment/ShopPageProductListResultFragment.kt
@@ -177,7 +177,6 @@ class ShopPageProductListResultFragment : BaseListFragment<BaseShopProductViewMo
                 this,
                 null,
                 null,
-                null,
                 this,
                 this,
                 true,
