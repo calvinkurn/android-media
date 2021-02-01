5d99a610c9a75a5d96077d6c6b82bed6a4379083
hansenpw <hansen.wijaya@tokopedia.com>
2021-02-01 14:47:10 +0700

    add eta ui test

--- features/transaction/oneclickcheckout/src/androidTest/java/com/tokopedia/oneclickcheckout/order/view/OrderSummaryPageActivityEtaTest.kt
+++ features/transaction/oneclickcheckout/src/androidTest/java/com/tokopedia/oneclickcheckout/order/view/OrderSummaryPageActivityEtaTest.kt
@@ -8,11 +8,14 @@ import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.oneclickcheckout.R
 import com.tokopedia.oneclickcheckout.common.idling.OccIdlingResource
-import com.tokopedia.oneclickcheckout.common.interceptor.OneClickCheckoutInterceptor
+import com.tokopedia.oneclickcheckout.common.interceptor.*
 import com.tokopedia.oneclickcheckout.common.robot.orderSummaryPage
 import com.tokopedia.oneclickcheckout.common.rule.FreshIdlingResourceTestRule
+import com.tokopedia.unifyprinciples.Typography
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -50,12 +53,47 @@ class OrderSummaryPageActivityEtaTest {
 
     @Test
     fun happyFlow_DirectCheckout() {
+        cartInterceptor.customGetOccCartResponsePath = GET_OCC_CART_PAGE_MANY_PROFILE_REVAMP_RESPONSE_PATH
+        logisticInterceptor.customRatesResponsePath = RATES_ETA_RESPONSE_PATH
+
         activityRule.launchActivity(null)
         intending(anyIntent()).respondWith(ActivityResult(Activity.RESULT_OK, null))
 
         orderSummaryPage {
+            assertShipmentRevamp(
+                    shippingDuration = "Pengiriman Reguler",
+                    shippingCourier = "AnterAja (Rp10.000)",
+                    shippingPrice = null,
+                    shippingEta = "Estimasi tiba besok - 3 Feb")
 
+            assertShipmentPromoRevamp(
+                    hasPromo = true,
+                    promoTitle = "Tersedia Bebas Ongkir",
+                    promoSubtitle = "Estimasi tiba besok - 3 Feb")
 
+            promoInterceptor.customValidateUseResponsePath = VALIDATE_USE_PROMO_REVAMP_BBO_APPLIED_RESPONSE
+            clickApplyShipmentPromoRevamp()
+
+            assertShipmentPromoRevamp(hasPromo = false)
+
+            assertShipmentRevamp(
+                    shippingDuration = null,
+                    shippingCourier = "Pengiriman Bebas Ongkir",
+                    shippingPrice = null,
+                    shippingEta = "Estimasi tiba besok - 3 Feb")
+
+            preferenceInterceptor.customGetPreferenceListResponsePath = GET_PREFERENCE_LIST_WITH_ETA_RESPONSE_PATH
+
+            clickAddOrChangePreferenceRevamp {
+                assertProfile(0) {
+                    val shippingDuration = it.findViewById<Typography>(R.id.tv_new_shipping_duration)
+                    assertEquals("Estimasi tiba besok", shippingDuration.text)
+                }
+                assertProfile(1) {
+                    val shippingDuration = it.findViewById<Typography>(R.id.tv_new_shipping_duration)
+                    assertEquals("Estimasi tiba besok - 3 Feb", shippingDuration.text)
+                }
+            }
         }
     }
 }
