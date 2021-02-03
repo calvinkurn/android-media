749488488f9bac637a874f10238f444df25a92cd
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-02-03 11:58:53 +0800

    bug fix quota check seller app review

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/SellerReviewUtils.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/SellerReviewUtils.kt
@@ -3,6 +3,7 @@ package com.tokopedia.sellerreview.common
 import android.content.Context
 import com.tokopedia.abstraction.common.utils.LocalCacheHandler
 import com.tokopedia.abstraction.constant.TkpdCache
+import java.util.*
 
 /**
  * Created By @ilhamsuaib on 03/02/21
@@ -22,6 +23,7 @@ object SellerReviewUtils {
         context?.let {
             val localCacheHandler = LocalCacheHandler(it, TkpdCache.SellerInAppReview.PREFERENCE_NAME)
             localCacheHandler.putBoolean(TkpdCache.SellerInAppReview.KEY_HAS_OPENED_REVIEW, true)
+            localCacheHandler.putLong(TkpdCache.SellerInAppReview.KEY_LAST_REVIEW_ASKED, Date().time)
             localCacheHandler.applyEditor()
         }
     }
