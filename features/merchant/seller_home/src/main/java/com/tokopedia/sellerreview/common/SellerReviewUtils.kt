efbcddaa4acdf656b571274d9e9c18d73e7fce82
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-02-03 10:55:05 +0800

    create seller review utils for local cache handler

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerreview/common/SellerReviewUtils.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.sellerreview.common
+
+import android.content.Context
+import com.tokopedia.abstraction.common.utils.LocalCacheHandler
+import com.tokopedia.abstraction.constant.TkpdCache
+
+/**
+ * Created By @ilhamsuaib on 03/02/21
+ */
+
+object SellerReviewUtils {
+
+    fun saveFlagHasSubmittedReview(context: Context?) {
+        context?.let {
+            val localCacheHandler = LocalCacheHandler(it, TkpdCache.SellerInAppReview.PREFERENCE_NAME)
+            localCacheHandler.putBoolean(TkpdCache.SellerInAppReview.KEY_HAS_SUBMITTED_REVIEW, true)
+            localCacheHandler.applyEditor()
+        }
+    }
+
+    fun saveFlagHasOpenedReviewApp(context: Context?) {
+        context?.let {
+            val localCacheHandler = LocalCacheHandler(it, TkpdCache.SellerInAppReview.PREFERENCE_NAME)
+            localCacheHandler.putBoolean(TkpdCache.SellerInAppReview.KEY_HAS_OPENED_REVIEW, true)
+            localCacheHandler.applyEditor()
+        }
+    }
+}
