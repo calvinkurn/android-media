5a72d69f0ccd2c7be7309a7461a234e0a80cb90f
Felicia Widjaja <fwidjaja@live.com>
2021-02-04 16:03:29 +0700

    remove unused import

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/adapter/ProductItemAdapter.java
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/adapter/ProductItemAdapter.java
@@ -29,7 +29,6 @@ import com.tokopedia.buyerorder.detail.data.Status;
 import com.tokopedia.buyerorder.detail.view.OrderListAnalytics;
 import com.tokopedia.buyerorder.detail.view.fragment.MarketPlaceDetailFragment;
 import com.tokopedia.buyerorder.detail.view.presenter.OrderListDetailPresenter;
-import com.tokopedia.ordermanagement.snapshot.view.activity.SnapshotActivity;
 
 import java.util.ArrayList;
 import java.util.List;
