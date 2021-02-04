fa44a5aead511863f7bf349ce159af315843177c
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-02-04 15:22:43 +0700

    Optimize noop module to java only

---
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/AnalyticsSource.java
@@ -0,0 +1,20 @@
+package com.tokopedia.analyticsdebugger;
+
+import androidx.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static com.tokopedia.analyticsdebugger.AnalyticsSource.GTM;
+import static com.tokopedia.analyticsdebugger.AnalyticsSource.ALL;
+import static com.tokopedia.analyticsdebugger.AnalyticsSource.APPS_FLYER;
+import static com.tokopedia.analyticsdebugger.AnalyticsSource.BRANCH_IO;
+
+@Retention(RetentionPolicy.SOURCE)
+@StringDef({GTM, BRANCH_IO, APPS_FLYER, ALL})
+public @interface AnalyticsSource {
+    String GTM = "gtm";
+    String BRANCH_IO = "branch_io";
+    String APPS_FLYER = "apps_flyer";
+    String ALL = "";
+}
