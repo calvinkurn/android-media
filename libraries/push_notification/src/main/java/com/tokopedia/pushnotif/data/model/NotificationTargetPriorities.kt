65b20ff8a1da9070135be674858934af32308ad2
isfaaghyth <isfaaghyth@gmail.com>
2021-02-04 14:16:21 +0700

    move validationManager into push-notification-common module

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/data/model/NotificationTargetPriorities.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.pushnotif.data.model
-
-import com.tokopedia.pushnotif.util.NotificationValidationManager
-
-data class NotificationTargetPriorities(
-        var priorityType: NotificationValidationManager.NotificationPriorityType,
-        var isAdvanceTarget: Boolean
-)
