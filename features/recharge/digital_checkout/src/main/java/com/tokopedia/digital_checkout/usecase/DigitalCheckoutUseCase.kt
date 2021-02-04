30e83ce52d69ba7e3456dae2a2b3af41c44d42d7
Jessica Sean <jessica.sean@tokopedia.com>
2021-02-04 19:11:00 +0700

    feat: change the usage of RestRequestSupportInterceptorUseCase to RestRequestUseCase

--- features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/usecase/DigitalCheckoutUseCase.kt
+++ features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/usecase/DigitalCheckoutUseCase.kt
@@ -1,37 +1,36 @@
 package com.tokopedia.digital_checkout.usecase
 
-import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.google.gson.reflect.TypeToken
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.common.network.coroutines.repository.RestRepository
+import com.tokopedia.common.network.coroutines.usecase.RestRequestUseCase
 import com.tokopedia.common.network.data.model.RequestType
 import com.tokopedia.common.network.data.model.RestRequest
-import com.tokopedia.common.network.domain.RestRequestSupportInterceptorUseCase
+import com.tokopedia.common.network.data.model.RestResponse
 import com.tokopedia.common_digital.common.constant.DigitalUrl
 import com.tokopedia.digital_checkout.data.request.RequestBodyCheckout
 import com.tokopedia.digital_checkout.data.response.ResponseCheckout
 import com.tokopedia.digital_checkout.di.DigitalCartCheckoutQualifier
 import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.usecase.RequestParams
-import okhttp3.Interceptor
+import java.lang.reflect.Type
 import javax.inject.Inject
 
-class DigitalCheckoutUseCase @Inject constructor(@DigitalCartCheckoutQualifier val authInterceptor: ArrayList<Interceptor>,
-                                                 @ApplicationContext val context: Context)
-    : RestRequestSupportInterceptorUseCase(authInterceptor, context) {
+class DigitalCheckoutUseCase @Inject constructor(@DigitalCartCheckoutQualifier val repository: RestRepository)
+    : RestRequestUseCase(repository) {
 
-    override fun buildRequest(requestParams: RequestParams): MutableList<RestRequest> {
-        val networkRequest = mutableListOf<RestRequest>()
+    private val url = DigitalUrl.CHECKOUT
+    var requestParams = RequestParams()
+
+    override suspend fun executeOnBackground(): Map<Type, RestResponse?> {
         val token = object : TypeToken<DataResponse<ResponseCheckout>>() {}.type
-
-        val url = DigitalUrl.CHECKOUT
 
         val requestBodyOtpSuccess = requestParams.getObject(PARAM_REQUEST_CHECKOUT) as RequestBodyCheckout
         val jsonElement = JsonParser().parse(Gson().toJson(requestBodyOtpSuccess))
         val requestBody = JsonObject()
-        requestBody.add("data", jsonElement)
+        requestBody.add(PARAM_DATA, jsonElement)
 
         val mapHeader = mutableMapOf<String, String>()
         mapHeader[KEY_CONTENT_TYPE] = VALUE_CONTENT_TYPE
@@ -41,14 +40,16 @@ class DigitalCheckoutUseCase @Inject constructor(@DigitalCartCheckoutQualifier v
                 .setBody(requestBody)
                 .setHeaders(mapHeader)
                 .build()
-        networkRequest.add(restRequest)
-        return networkRequest
+
+        restRequestList.clear()
+        restRequestList.add(restRequest)
+        return repository.getResponses(restRequestList)
     }
 
-    fun createRequestParams(requestBodyCheckout: RequestBodyCheckout): RequestParams {
-        val requestParams = RequestParams.create()
+    fun setRequestParams(requestBodyCheckout: RequestBodyCheckout) {
+        requestParams = RequestParams.create()
         requestParams.putObject(PARAM_REQUEST_CHECKOUT, requestBodyCheckout)
-        return requestParams
+        requestParams
     }
 
     companion object {
@@ -56,5 +57,6 @@ class DigitalCheckoutUseCase @Inject constructor(@DigitalCartCheckoutQualifier v
 
         private const val KEY_CONTENT_TYPE = "Content-Type"
         private const val VALUE_CONTENT_TYPE = "application/json"
+        private const val PARAM_DATA = "data"
     }
 }
--- features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/usecase/DigitalPatchOtpUseCase.kt
+++ features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/usecase/DigitalPatchOtpUseCase.kt
@@ -1,35 +1,31 @@
 package com.tokopedia.digital_checkout.usecase
 
-import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.google.gson.reflect.TypeToken
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
-import com.tokopedia.akamai_bot_lib.map
+import com.tokopedia.common.network.coroutines.repository.RestRepository
+import com.tokopedia.common.network.coroutines.usecase.RestRequestUseCase
 import com.tokopedia.common.network.data.model.RequestType
 import com.tokopedia.common.network.data.model.RestRequest
-import com.tokopedia.common.network.domain.RestRequestSupportInterceptorUseCase
+import com.tokopedia.common.network.data.model.RestResponse
 import com.tokopedia.common_digital.common.constant.DigitalUrl
-import com.tokopedia.common_digital.product.data.response.TkpdDigitalResponse
 import com.tokopedia.digital_checkout.data.request.RequestBodyOtpSuccess
 import com.tokopedia.digital_checkout.data.response.ResponsePatchOtpSuccess
 import com.tokopedia.digital_checkout.di.DigitalCartCheckoutQualifier
-import com.tokopedia.digital_checkout.di.DigitalCartQualifier
 import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.usecase.RequestParams
-import okhttp3.Interceptor
+import java.lang.reflect.Type
 import javax.inject.Inject
 
-class DigitalPatchOtpUseCase @Inject constructor(@DigitalCartCheckoutQualifier val authInterceptor: ArrayList<Interceptor>,
-                                                 @ApplicationContext val context: Context)
-    : RestRequestSupportInterceptorUseCase(authInterceptor, context) {
+class DigitalPatchOtpUseCase @Inject constructor(@DigitalCartCheckoutQualifier val repository: RestRepository)
+    : RestRequestUseCase(repository) {
 
-    override fun buildRequest(requestParams: RequestParams): MutableList<RestRequest> {
-        val networkRequest = mutableListOf<RestRequest>()
+    private val url = DigitalUrl.OTP
+    var requestParams = RequestParams()
+
+    override suspend fun executeOnBackground(): Map<Type, RestResponse?> {
         val token = object : TypeToken<DataResponse<ResponsePatchOtpSuccess>>() {}.type
-
-        val url = DigitalUrl.OTP
 
         val requestBodyOtpSuccess = requestParams.getObject(PARAM_REQUEST_OTP_SUCCESS) as RequestBodyOtpSuccess
         val jsonElement = JsonParser().parse(Gson().toJson(requestBodyOtpSuccess))
@@ -44,14 +40,15 @@ class DigitalPatchOtpUseCase @Inject constructor(@DigitalCartCheckoutQualifier v
                 .setBody(requestBody)
                 .setHeaders(mapHeader)
                 .build()
-        networkRequest.add(restRequest)
-        return networkRequest
+
+        restRequestList.clear()
+        restRequestList.add(restRequest)
+        return repository.getResponses(restRequestList)
     }
 
-    fun createRequestParams(requestBodyOtpSuccess: RequestBodyOtpSuccess): RequestParams {
-        val requestParams = RequestParams.create()
+    fun setRequestParams(requestBodyOtpSuccess: RequestBodyOtpSuccess) {
+        requestParams = RequestParams.create()
         requestParams.putObject(PARAM_REQUEST_OTP_SUCCESS, requestBodyOtpSuccess)
-        return requestParams
     }
 
     companion object {
