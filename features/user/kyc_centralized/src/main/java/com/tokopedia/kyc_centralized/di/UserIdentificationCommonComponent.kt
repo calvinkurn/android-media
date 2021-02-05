c571b777d5bc52693da8923828354a4f1aab2ff9
kelvinsaputra <kelvin.saputra@tokopedia.com>
2021-02-05 14:00:40 +0700

    fix dagger

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/di/UserIdentificationCommonComponent.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/di/UserIdentificationCommonComponent.kt
@@ -1,9 +1,10 @@
 package com.tokopedia.kyc_centralized.di
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
-import com.tokopedia.kyc_centralized.view.fragment.UserIdentificationFormFaceFragment
+import com.tokopedia.kyc_centralized.view.fragment.BaseUserIdentificationStepperFragment
 import com.tokopedia.kyc_centralized.view.fragment.UserIdentificationFormFinalFragment
 import com.tokopedia.kyc_centralized.view.fragment.UserIdentificationInfoFragment
+import com.tokopedia.kyc_centralized.view.model.UserIdentificationStepperModel
 import dagger.Component
 
 /**
@@ -12,7 +13,7 @@ import dagger.Component
 @UserIdentificationCommonScope
 @Component(modules = [UserIdentificationCommonModule::class, KycCentralizedViewModelModule::class, KycUploadImageModule::class], dependencies = [BaseAppComponent::class])
 interface UserIdentificationCommonComponent {
-    fun inject(fragment: UserIdentificationFormFaceFragment?)
+    fun inject(fragment: BaseUserIdentificationStepperFragment<UserIdentificationStepperModel>?)
     fun inject(fragment: UserIdentificationFormFinalFragment?)
     fun inject(fragment: UserIdentificationInfoFragment?)
 }
