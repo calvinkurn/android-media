9fe3f44c216a874c4e5abac25946bd2dbf1e44eb
Rahul Kumar Lohra <rahul.lohra@tokopedia.com>
2021-02-05 16:00:33 +0530

    1. Remove unused file and code

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/cracktoken/util/TokenMarginUtil.java
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/cracktoken/util/TokenMarginUtil.java
@@ -6,9 +6,7 @@ package com.tokopedia.gamification.cracktoken.util;
 
 public class TokenMarginUtil {
     private static final double RATIO_IMAGE_WIDTH = 0.5;
-    private static final double RATIO_IMAGE_MARGIN_BOTTOM = 0.64;
     public static final double RATIO_IMAGE_MARGIN_TOP = 0.795;
-    private static final double RATIO_TIMER_MARGIN_BOTTOM = 0.75;
     public static final float STAGE_PIXEL = 1670f;
     public static final float BASE_DRAWABLE_HEIGHT = 2701f;
 
@@ -17,11 +15,4 @@ public class TokenMarginUtil {
         return (int) (RATIO_IMAGE_WIDTH * Math.min(rootWidth, rootHeight));
     }
 
-    public static int getEggMarginBottom(int rootHeight) {
-        return (int) (RATIO_IMAGE_MARGIN_BOTTOM * (rootHeight));
-    }
-
-    public static int getTimerMarginBottom(int rootHeight) {
-        return (int) (RATIO_TIMER_MARGIN_BOTTOM * (rootHeight));
-    }
 }
