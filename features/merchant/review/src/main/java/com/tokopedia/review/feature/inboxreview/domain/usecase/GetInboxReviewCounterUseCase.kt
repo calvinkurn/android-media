7bf29cd9cfcf345117e590cb6591ca39e5e06163
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-02-08 10:58:16 +0700

    fix gql annotation

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/inboxreview/domain/usecase/GetInboxReviewCounterUseCase.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/inboxreview/domain/usecase/GetInboxReviewCounterUseCase.kt
@@ -1,16 +1,21 @@
 package com.tokopedia.review.feature.inboxreview.domain.usecase
 
+import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.review.feature.inboxreview.domain.response.InboxReviewTabCounterResponse
+import com.tokopedia.review.feature.inboxreview.domain.usecase.GetInboxReviewCounterUseCase.Companion.INBOX_REVIEW_COUNTER_QUERY
+import com.tokopedia.review.feature.inboxreview.domain.usecase.GetInboxReviewCounterUseCase.Companion.INBOX_REVIEW_COUNTER_QUERY_CLASS_NAME
 import javax.inject.Inject
 
+@GqlQuery(INBOX_REVIEW_COUNTER_QUERY_CLASS_NAME, INBOX_REVIEW_COUNTER_QUERY)
 class GetInboxReviewCounterUseCase @Inject constructor(
         graphqlRepository: GraphqlRepository
 ) : GraphqlUseCase<InboxReviewTabCounterResponse>(graphqlRepository) {
 
     companion object {
-        private const val QUERY = """
+        const val INBOX_REVIEW_COUNTER_QUERY_CLASS_NAME = "InboxReviewCounter"
+        const val INBOX_REVIEW_COUNTER_QUERY = """
             query productrevReviewTabCounter {
               productrevReviewTabCounter{
                 list {
@@ -23,7 +28,7 @@ class GetInboxReviewCounterUseCase @Inject constructor(
     }
 
     init {
-        setGraphqlQuery(QUERY)
+        setGraphqlQuery(InboxReviewCounter.GQL_QUERY)
         setTypeClass(InboxReviewTabCounterResponse::class.java)
     }
 }
