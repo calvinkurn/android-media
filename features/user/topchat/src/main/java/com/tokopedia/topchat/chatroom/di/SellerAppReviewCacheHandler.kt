189ea3ebae05b70a14b4f12cb38d9db7179363a7
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-02-08 18:46:47 +0800

    fix code review top chat

---
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/di/SellerAppReviewCacheHandler.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.topchat.chatroom.di
+
+import javax.inject.Qualifier
+
+/**
+ * Created By @ilhamsuaib on 08/02/21
+ */
+
+@Qualifier
+annotation class SellerAppReviewCacheHandler
