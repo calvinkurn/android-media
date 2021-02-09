7daae0038926026490be057f0f13e3ba5fbf1b0e
astidhiyaa <asti.anzaria@tokopedia.com>
2021-02-09 20:15:49 +0800

    - Add login() to VoucherGameDetailActivityTest.kt - Add mock response for REST

--- features/recharge/voucher_game/src/androidTest/java/com/tokopedia/vouchergame/VoucherGameDetailActivityTest.kt
+++ features/recharge/voucher_game/src/androidTest/java/com/tokopedia/vouchergame/VoucherGameDetailActivityTest.kt
@@ -20,9 +20,8 @@ import com.tokopedia.cassavatest.getAnalyticsWithQuery
 import com.tokopedia.cassavatest.hasAllSuccess
 import com.tokopedia.common.topupbills.data.product.CatalogOperatorAttributes
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
-import com.tokopedia.test.application.util.InstrumentationAuthHelper
-import com.tokopedia.test.application.util.InstrumentationMockHelper
-import com.tokopedia.test.application.util.setupGraphqlMockResponseWithCheck
+import com.tokopedia.test.application.util.*
+import com.tokopedia.user.session.UserSession
 import com.tokopedia.vouchergame.common.view.model.VoucherGameExtraParam
 import com.tokopedia.vouchergame.detail.view.activity.VoucherGameDetailActivity
 import com.tokopedia.vouchergame.list.view.adapter.viewholder.VoucherGameListViewHolder
@@ -39,7 +38,7 @@ class VoucherGameDetailActivityTest{
     private val gtmLogDBSource = GtmLogDBSource(targetContext)
 
     @get:Rule
-    var mActivityRule = ActivityTestRule(VoucherGameDetailActivity::class.java,false,false)
+    var mActivityRule = ActivityTestRule(VoucherGameDetailActivity::class.java, false, false)
 
     @Before
     fun setUp(){
@@ -51,22 +50,28 @@ class VoucherGameDetailActivityTest{
                     InstrumentationMockHelper.getRawString(targetContext, R.raw.mock_response_recharge_product_input),
                     MockModelConfig.FIND_BY_CONTAINS)
         }
+        setupRestMockResponse {
+            addMockResponse(
+                    KEY_QUERY_CART,
+                    InstrumentationMockHelper.getRawString(targetContext, R.raw.mock_response_recharge_voucher_game_add_to_cart),
+                    MockModelConfig.FIND_BY_CONTAINS)
+        }
         val intent: Intent = VoucherGameDetailActivity.newInstance(targetContext, VoucherGameExtraParam(menuId = "4", operatorId = "2616"), CatalogOperatorAttributes(description = POKEMON_DETAIL_DESCRIPTION))
         mActivityRule.launchActivity(intent)
-        InstrumentationAuthHelper.loginInstrumentationTestUser1()
+        login()
     }
 
     @Test
     fun validateTracking(){
         clickOnDetail()
+        Thread.sleep(3000)
         ViewMatchers.assertThat(getAnalyticsWithQuery(gtmLogDBSource, targetContext, ANALYTICS_VOUCHER_GAME_DETAIL),
                 hasAllSuccess())
     }
 
     fun clickOnDetail(){
-    intending(IntentMatchers.anyIntent()).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
-        val itemCount = (mActivityRule.activity.findViewById(R.id.recycler_view) as RecyclerView).adapter?.itemCount ?: 0
-        if (itemCount > 0) {
+        intending(IntentMatchers.anyIntent()).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
+        if (itemCount() > 0) {
             Thread.sleep(3000)
             Espresso.onView(ViewMatchers.withId(R.id.recycler_view)).perform(RecyclerViewActions
                     .actionOnItemAtPosition<VoucherGameListViewHolder>(0, ViewActions.click()))
@@ -83,13 +88,37 @@ class VoucherGameDetailActivityTest{
         Espresso.onView(ViewMatchers.withId(R.id.btn_info_icon)).check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(ViewActions.click())
     }
 
+    fun itemCount(): Int {
+        val itemCount: RecyclerView = mActivityRule.activity.findViewById(R.id.recycler_view) as RecyclerView
+        return itemCount.adapter?.itemCount ?: 0
+    }
+
+    fun login() {
+        InstrumentationAuthHelper.loginInstrumentationTestUser1()
+        val userSession = UserSession(targetContext)
+        userSession.setLoginSession(
+                true,
+                userSession.userId,
+                userSession.name,
+                userSession.shopId,
+                true,
+                userSession.shopName,
+                userSession.email,
+                userSession.isGoldMerchant,
+                userSession.phoneNumber,
+        )
+    }
+
     @After
     fun tearDown(){
         Intents.release()
     }
 
+
+
     companion object {
         private const val KEY_QUERY_VOUCHER_DETAIL = "voucherGameProductDetail"
+        const val KEY_QUERY_CART = "{\"data\": {   \"attributes\": {\"device_id\": 5,\"fields\": [],\"identifier\": {\"device_token\":\"cx68b1CtPII:APA91bEV_bdZfq9qPBxHn2z34ccRQ5M8y9c9pfqTbpIy1AlOrJYSFMKzm_GaszoFsYcSeZYbTUbdccqmW8lwPQVli3B1fCjWnASz5ZePCpkh9iEjaWjaPovAZKZenowuo4GMD68hoR\",\"os_type\": \"1\",\"user_id\": \"108956738\"},\"instant_checkout\": false,\"ip_address\": \"10.0.2.15\",\"is_reseller\": false,\"is_thankyou_native\": true,\"is_thankyou_native_new\": true,\"product_id\": 8035,\"show_subscribe_flag\": true,\"user_agent\": \"Android Tokopedia Application/com.tokopedia.tkpd v.3.90 (Google sdk_gphone_x86_arm; Android; API_30; Version11) \",\"user_id\": 108956738},\"type\": \"add_cart\"}}"
         private const val POKEMON_DETAIL_DESCRIPTION = "Pembelian ini dapat digunakan di Google Play Store, app store resmi Android untuk membeli item dalam game di Pokemon Go, serta lebih dari satu juta game dan aplikasi lainnya, dan banyak lagi. Untuk menukarkan, masukkan kode di aplikasi Play Store atau play.google.com. Untuk persyaratan lengkap silahkan lihat"
         private const val ANALYTICS_VOUCHER_GAME_DETAIL = "tracker/recharge/recharge_voucher_game_detail.json"
     }
