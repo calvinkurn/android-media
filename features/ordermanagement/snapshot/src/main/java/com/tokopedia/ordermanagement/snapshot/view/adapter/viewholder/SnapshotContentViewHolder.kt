28e58513e58029c632efbc51b85b9c2fb460aeb2
Felicia Widjaja <fwidjaja@live.com>
2021-02-11 17:22:25 +0700

    fix crash pdp, changes request by yehez (on going)

--- features/ordermanagement/snapshot/src/main/java/com/tokopedia/ordermanagement/snapshot/view/adapter/viewholder/SnapshotContentViewHolder.kt
+++ features/ordermanagement/snapshot/src/main/java/com/tokopedia/ordermanagement/snapshot/view/adapter/viewholder/SnapshotContentViewHolder.kt
@@ -30,15 +30,38 @@ import com.tokopedia.unifyprinciples.Typography
  */
 class SnapshotContentViewHolder(itemView: View, private val actionListener: SnapshotAdapter.ActionListener?) : SnapshotAdapter.BaseViewHolder<SnapshotTypeData>(itemView)  {
     @SuppressLint("SetTextI18n")
+
+    val ivHeader = itemView.findViewById<ImageView>(R.id.snapshot_main_img)
+    val viewPager2 = itemView.findViewById<ViewPager2>(R.id.snapshot_header_view_pager)
+    val indicator = itemView.findViewById<PageControl>(R.id.snapshot_page_indicator)
+    val tickerInfo = itemView.findViewById<Ticker>(R.id.ticker_info)
+    val productPrice = itemView.findViewById<Typography>(R.id.snapshot_main_price)
+    val discLabel = itemView.findViewById<Label>(R.id.snapshot_label_disc)
+    val productName = itemView.findViewById<Typography>(R.id.snapshot_product_name)
+    val shopBadge = itemView.findViewById<ImageView>(R.id.snapshot_shop_badge)
+    val clShop = itemView.findViewById<ConstraintLayout>(R.id.cl_shop)
+    val hargaCoret = itemView.findViewById<Typography>(R.id.snapshot_harga_coret)
+    val shopLogo = itemView.findViewById<ImageView>(R.id.snapshot_shop_logo)
+    val kondisiLabel = itemView.findViewById<Typography>(R.id.snapshot_kondisi_label)
+    val kondisiValue = itemView.findViewById<Typography>(R.id.snapshot_kondisi_value)
+    val dividerKondisi = itemView.findViewById<View>(R.id.divider_kondisi)
+    val beratLabel = itemView.findViewById<Typography>(R.id.snapshot_detail_berat_label)
+    val beratValue = itemView.findViewById<Typography>(R.id.snapshot_berat_value)
+    val dividerBerat = itemView.findViewById<View>(R.id.divider_berat)
+    val shopName = itemView.findViewById<Typography>(R.id.snapshot_shop_name)
+    val labelPo = itemView.findViewById<Typography>(R.id.snapshot_po_label)
+    val valuePo = itemView.findViewById<Typography>(R.id.snapshot_po_value)
+    val dividerPo = itemView.findViewById<View>(R.id.divider_po)
+    val minOrderLabel = itemView.findViewById<Typography>(R.id.snapshot_min_order_label)
+    val minOrderValue = itemView.findViewById<Typography>(R.id.snapshot_min_order_value)
+    val dividerMinOrder = itemView.findViewById<View>(R.id.divider_min_order)
+    val desc = itemView.findViewById<Typography>(R.id.snapshot_desc)
+
     override fun bind(item: SnapshotTypeData, position: Int) {
         if (item.dataObject is SnapshotResponse.Data.GetOrderSnapshot) {
             // header
             val productImages = item.dataObject.productImageSecondary
             if (productImages.isNotEmpty()) {
-                val ivHeader = itemView.findViewById<ImageView>(R.id.snapshot_main_img)
-                val viewPager2 = itemView.findViewById<ViewPager2>(R.id.snapshot_header_view_pager)
-                val indicator = itemView.findViewById<PageControl>(R.id.snapshot_page_indicator)
-
                 if (productImages.size > 1) {
                     ivHeader.gone()
                     viewPager2.visible()
@@ -85,15 +108,12 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
             }
 
             // info
-            val tickerInfo = itemView.findViewById<Ticker>(R.id.ticker_info)
             SnapshotUtils.parseDate(item.dataObject.orderDetail.createTime)?.let {
                 itemView.context.getString(R.string.snapshot_ticker_info).replace(CREATED_TIME, it) }?.let {
                 tickerInfo.setHtmlDescription(it) }
 
-            val productPrice = itemView.findViewById<Typography>(R.id.snapshot_main_price)
             productPrice.text = item.dataObject.productAdditionalData.productPrice
 
-            val discLabel = itemView.findViewById<Label>(R.id.snapshot_label_disc)
             val discPercentage = item.dataObject.campaignData.campaign.discountPercentageText
             if (discPercentage.isEmpty()) {
                 discLabel.gone()
@@ -102,7 +122,6 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 discLabel.text = item.dataObject.campaignData.campaign.discountPercentageText
             }
 
-            val hargaCoret = itemView.findViewById<Typography>(R.id.snapshot_harga_coret)
             val originalPrice = item.dataObject.campaignData.campaign.originalPrice
             if (originalPrice.isEmpty()) {
                 hargaCoret.gone()
@@ -112,32 +131,28 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 hargaCoret.paintFlags = hargaCoret.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG
             }
 
-            val productName = itemView.findViewById<Typography>(R.id.snapshot_product_name)
             productName.text = MethodChecker.fromHtmlPreserveLineBreak(item.dataObject.orderDetail.productName)
 
-            val shopLogo = itemView.findViewById<ImageView>(R.id.snapshot_shop_logo)
             ImageHandler.loadImageCircle2(itemView.context, shopLogo, item.dataObject.shopImagePrimaryUrl)
 
             val drawable = when {
                 item.dataObject.isOs -> {
-                    androidx.core.content.ContextCompat.getDrawable(itemView.context, com.tokopedia.gm.common.R.drawable.ic_official_store_product)
+                    ContextCompat.getDrawable(itemView.context, com.tokopedia.gm.common.R.drawable.ic_official_store_product)
                 }
                 item.dataObject.isPm -> {
-                    androidx.core.content.ContextCompat.getDrawable(itemView.context, com.tokopedia.gm.common.R.drawable.ic_power_merchant)
+                    ContextCompat.getDrawable(itemView.context, com.tokopedia.gm.common.R.drawable.ic_power_merchant)
                 }
                 else -> {
                     null
                 }
             }
 
-            val shopBadge = itemView.findViewById<ImageView>(R.id.snapshot_shop_badge)
             if (drawable == null) shopBadge.gone()
             else {
                 shopBadge.setImageDrawable(drawable)
                 shopBadge.visible()
             }
 
-            val shopName = itemView.findViewById<Typography>(R.id.snapshot_shop_name)
             shopName.text = item.dataObject.shopSummary.shopName
 
             val condition = when {
@@ -152,14 +167,9 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 }
             }
 
-            val clShop = itemView.findViewById<ConstraintLayout>(R.id.cl_shop)
             clShop?.setOnClickListener {
                 actionListener?.onSnapshotShopClicked(item.dataObject.shopSummary.shopId)
             }
-
-            val kondisiLabel = itemView.findViewById<Typography>(R.id.snapshot_kondisi_label)
-            val kondisiValue = itemView.findViewById<Typography>(R.id.snapshot_kondisi_value)
-            val dividerKondisi = itemView.findViewById<View>(R.id.divider_kondisi)
 
             if (condition == null) {
                 kondisiLabel.gone()
@@ -174,10 +184,6 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
             }
 
             val berat = item.dataObject.productTotalWeightFormatted
-            val beratLabel = itemView.findViewById<Typography>(R.id.snapshot_detail_berat_label)
-            val beratValue = itemView.findViewById<Typography>(R.id.snapshot_berat_value)
-            val dividerBerat = itemView.findViewById<View>(R.id.divider_berat)
-
             if (berat.isNotEmpty()) {
                 beratLabel.visible()
                 beratValue.visible()
@@ -189,10 +195,6 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 beratValue.gone()
                 dividerBerat.gone()
             }
-
-            val labelPo = itemView.findViewById<Typography>(R.id.snapshot_po_label)
-            val valuePo = itemView.findViewById<Typography>(R.id.snapshot_po_value)
-            val dividerPo = itemView.findViewById<View>(R.id.divider_po)
 
             if (item.dataObject.preOrder) {
                 if (item.dataObject.preOrderDuration.isNotEmpty()) {
@@ -212,9 +214,6 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 dividerPo.gone()
             }
 
-            val minOrderLabel = itemView.findViewById<Typography>(R.id.snapshot_min_order_label)
-            val minOrderValue = itemView.findViewById<Typography>(R.id.snapshot_min_order_value)
-            val dividerMinOrder = itemView.findViewById<View>(R.id.divider_min_order)
             val minOrder = item.dataObject.orderDetail.minOrder
 
             if (minOrder > 0) {
@@ -228,8 +227,6 @@ class SnapshotContentViewHolder(itemView: View, private val actionListener: Snap
                 minOrderValue.gone()
                 dividerMinOrder.gone()
             }
-
-            val desc = itemView.findViewById<Typography>(R.id.snapshot_desc)
 
             val productDesc = item.dataObject.orderDetail.productDesc
             if (productDesc.isNotEmpty()) {
