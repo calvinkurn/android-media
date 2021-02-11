7b49bb79617df31057a51d446bc811ca8c946af4
astidhiyaa <asti.anzaria@tokopedia.com>
2021-02-11 13:03:49 +0800

    - Migrate AppCompatImageView to IconUnify - Remove unused image

--- features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailFragment.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/orderdetail/presentation/fragment/FlightOrderDetailFragment.kt
@@ -286,14 +286,14 @@ class FlightOrderDetailFragment : BaseDaggerFragment(),
         flightOrderDetailInsurance.setData(
                 getString(R.string.flight_order_detail_insurance_title_label),
                 FlightOrderDetailButtonModel(
-                        MethodChecker.getDrawable(requireContext(), R.drawable.ic_flight_order_detail_insurance),
+                        MethodChecker.getDrawable(requireContext(), R.drawable.iconunify_protection),
                         getString(R.string.flight_order_detail_insurance_trip_label),
                         getString(R.string.flight_order_detail_insurance_trip_description),
                         true,
                         false
                 ),
                 FlightOrderDetailButtonModel(
-                        MethodChecker.getDrawable(requireContext(), R.drawable.ic_flight_order_detail_insurance),
+                        MethodChecker.getDrawable(requireContext(), R.drawable.iconunify_protection),
                         getString(R.string.flight_order_detail_insurance_cancel_label),
                         getString(R.string.flight_order_detail_insurance_trip_description),
                         true,
