a37df8e5fcdf095e995083c52b44e87606fa743d
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-02-15 14:27:07 +0700

    remove crysp worker

--- libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/di/DeviceFingerprintComponent.kt
+++ libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/di/DeviceFingerprintComponent.kt
@@ -2,7 +2,6 @@ package com.tokopedia.devicefingerprint.di
 
 import com.tokopedia.devicefingerprint.appauth.AppAuthWorker
 import com.tokopedia.devicefingerprint.submitdevice.service.SubmitDeviceWorker
-import com.tokopedia.devicefingerprint.crysp.workmanager.CryspWorker
 import com.tokopedia.devicefingerprint.datavisor.workmanager.DataVisorWorker
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import dagger.Component
@@ -12,7 +11,6 @@ import dagger.Component
 interface DeviceFingerprintComponent {
     fun inject(appAuthWorker: AppAuthWorker)
     fun inject(submitDeviceWorker: SubmitDeviceWorker)
-    fun inject(worker: CryspWorker)
     fun inject(worker: DataVisorWorker)
     fun getGraphQlRepo(): GraphqlRepository
 }
