b7b2b4b1a7c11c5093ed18fb0141abda054be6ee
isfaaghyth <isfaaghyth@gmail.com>
2021-02-16 05:16:23 +0700

    convert a few image loader on product detail module

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/adapter/YoutubeThumbnailAdapter.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/adapter/YoutubeThumbnailAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.media.loader.loadImage
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.product.detail.R
 import com.tokopedia.product.detail.common.data.model.product.YoutubeVideo
 import com.tokopedia.product.detail.data.util.thumbnailUrl
