187589c31aee20f7d2a60a08d3a1b8ef5a90b611
yorispr <yoris.prayogo@tokopedia.com>
2021-02-18 16:57:49 +0700

    create usecase

---
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/domain/query/LoginTokenV2Query.kt
@@ -0,0 +1,46 @@
+package com.tokopedia.sessioncommon.domain.query
+
+/**
+ * Created by Yoris Prayogo on 16/02/21.
+ * Copyright (c) 2021 PT. Tokopedia All rights reserved.
+ */
+
+object LoginTokenV2Query {
+
+    private const val grantType = "\$grant_type"
+    private const val username = "\$username"
+    private const val password = "\$password"
+    private const val hash = "\$hash"
+
+    val loginEmailQuery: String = """
+        mutation login_email_v2($grantType: String!, $username: String!, $password: String!, $hash: String!){
+            login_token_v2(
+                input: {
+                    grant_type: $grantType
+                    username: $username
+                    password: $password
+                    hash: $hash
+                }
+            ) {
+                acc_sid
+                access_token
+                expires_in
+                refresh_token
+                sid
+                token_type
+                sq_check
+                action
+                errors {
+                    name
+                    message
+                }
+                event_code
+                popup_error {
+                    header
+                    body
+                    action
+                }
+            }
+        }
+    """.trimIndent()
+}
---
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/domain/usecase/LoginTokenV2UseCase.kt
@@ -0,0 +1,52 @@
+package com.tokopedia.sessioncommon.domain.usecase
+
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.sessioncommon.data.LoginTokenPojo
+import com.tokopedia.sessioncommon.domain.query.LoginTokenV2Query
+import com.tokopedia.sessioncommon.util.TokenGenerator
+import com.tokopedia.usecase.RequestParams
+import com.tokopedia.usecase.coroutines.UseCase
+import javax.inject.Inject
+
+/**
+ * Created by Yoris Prayogo on 16/02/21.
+ * Copyright (c) 2021 PT. Tokopedia All rights reserved.
+ */
+
+class LoginTokenV2UseCase @Inject constructor(private val graphqlUseCase: GraphqlUseCase<LoginTokenPojo>):
+        UseCase<LoginTokenPojo>() {
+
+    init {
+        graphqlUseCase.setGraphqlQuery(LoginTokenV2Query.loginEmailQuery)
+        graphqlUseCase.setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build())
+        graphqlUseCase.setTypeClass(LoginTokenPojo::class.java)
+    }
+
+    private val params = RequestParams.create()
+
+    fun setParams(email: String, password: String, hash: String) {
+        params.putString(PARAM_USERNAME, TokenGenerator().encode(email))
+        params.putString(PARAM_PASSWORD, password)
+        params.putString(PARAM_GRANT_TYPE, TokenGenerator().encode(TYPE_PASSWORD))
+        params.putString(PARAM_HASH, hash)
+    }
+
+    override suspend fun executeOnBackground(): LoginTokenPojo {
+        graphqlUseCase.clearCache()
+        graphqlUseCase.setRequestParams(params.parameters)
+        return graphqlUseCase.executeOnBackground()
+    }
+
+    companion object {
+
+        private val PARAM_GRANT_TYPE: String = "grant_type"
+        private val PARAM_USERNAME: String = "username"
+        private val PARAM_PASSWORD: String = "password"
+        private val PARAM_HASH: String = "hash"
+
+        private val TYPE_PASSWORD:String = "password"
+    }
+
+}
