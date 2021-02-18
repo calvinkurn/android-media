846f1104f7acaf83fdfdd7500b84e8ae85269512
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-02-18 16:46:47 +0700

    use TypeSafeMatcher

--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/matcher/IntentMatcher.kt
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/matcher/IntentMatcher.kt
@@ -3,29 +3,32 @@ package com.tokopedia.test.application.matcher
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.test.platform.app.InstrumentationRegistry
-import org.hamcrest.BaseMatcher
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 
 /**
  * @param activityName - full activity canonical class name for example:
  * com.tokopedia.topchat.chatroom.view.activity.TopChatRoomActivity
  */
 fun isPointingTo(activityName: String): Matcher<Intent> {
-    return object : BaseMatcher<Intent>() {
-        override fun describeMismatch(item: Any?, description: Description?) {
-            super.describeMismatch(item, description)
-            if (item is Intent) {
-                description?.appendText("\nit's pointing to ${item.component?.className}")
-            }
+    return object : TypeSafeMatcher<Intent>() {
+        override fun describeMismatchSafely(
+                item: Intent?,
+                mismatchDescription: Description?
+        ) {
+            super.describeMismatchSafely(item, mismatchDescription)
+            mismatchDescription?.appendText(
+                    "\nit's pointing to ${item?.component?.className}"
+            )
         }
 
         override fun describeTo(description: Description?) {
             description?.appendText(activityName)
         }
 
-        override fun matches(item: Any?): Boolean {
-            val intent = item as? Intent ?: return false
+        override fun matchesSafely(item: Intent?): Boolean {
+            val intent = item ?: return false
             val context = InstrumentationRegistry.getInstrumentation().context
             val resolvedActivities = context.packageManager
                     .queryIntentActivities(intent, PackageManager.MATCH_ALL)
@@ -34,38 +37,33 @@ fun isPointingTo(activityName: String): Matcher<Intent> {
                         it.activityInfo.name == activityName
             }
         }
-
     }
 }
 
 fun hasQueryParameter(key: String, value: String): Matcher<Intent> {
-    return object : BaseMatcher<Intent>() {
+    return object : TypeSafeMatcher<Intent>() {
         override fun describeTo(description: Description?) {
             description?.appendText("intent has query \"$key\" with value \"$value\"")
         }
 
-        override fun matches(item: Any?): Boolean {
-            val intent = item as? Intent ?: return false
-            return intent.data?.getQueryParameter(key) == value
+        override fun matchesSafely(item: Intent?): Boolean {
+            return item?.data?.getQueryParameter(key) == value
         }
     }
 }
 
 fun lastPathSegmentEqualTo(lastPathSegment: String): Matcher<Intent> {
-    return object : BaseMatcher<Intent>() {
-        override fun describeMismatch(item: Any?, description: Description?) {
-            if (item is Intent) {
-                description?.appendText(item.data?.lastPathSegment)
-            }
-        }
-
+    return object : TypeSafeMatcher<Intent>() {
         override fun describeTo(description: Description?) {
             description?.appendText(lastPathSegment)
         }
 
-        override fun matches(item: Any?): Boolean {
-            val intent = item as? Intent ?: return false
-            return intent.data?.lastPathSegment == lastPathSegment
+        override fun describeMismatchSafely(item: Intent?, mismatchDescription: Description?) {
+            mismatchDescription?.appendText(item?.data?.lastPathSegment)
+        }
+
+        override fun matchesSafely(item: Intent?): Boolean {
+            return item?.data?.lastPathSegment == lastPathSegment
         }
     }
 }
