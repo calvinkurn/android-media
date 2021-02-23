3c4fb0db94e34bd08fe7dc09ec1884dc7aad0b71
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-02-23 20:01:28 +0700

    refactor dagger chatComponent to use the same instance between fragments

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/fragment/StickerFragment.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/fragment/StickerFragment.kt
@@ -1,14 +1,18 @@
 package com.tokopedia.topchat.chatroom.view.fragment
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.topchat.R
+import com.tokopedia.topchat.chatroom.di.ChatComponent
 import com.tokopedia.topchat.chatroom.di.ChatRoomContextModule
 import com.tokopedia.topchat.chatroom.di.DaggerChatComponent
 import com.tokopedia.topchat.chatroom.domain.pojo.stickergroup.StickerGroup
@@ -17,14 +21,12 @@ import com.tokopedia.topchat.chatroom.view.custom.StickerRecyclerView
 import com.tokopedia.topchat.chatroom.view.viewmodel.StickerViewModel
 import javax.inject.Inject
 
-class StickerFragment(
-        contentLayoutId: Int,
-        private val stickerListener: StickerViewHolder.Listener?
-) : Fragment(contentLayoutId) {
+class StickerFragment : BaseDaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private var stickerListener: StickerViewHolder.Listener? = null
     private var stickerList: StickerRecyclerView? = null
     private val viewModelFragmentProvider by lazy { ViewModelProviders.of(this, viewModelFactory) }
     private val viewModel by lazy { viewModelFragmentProvider.get(StickerViewModel::class.java) }
@@ -32,11 +34,38 @@ class StickerFragment(
     private var stickerGroupUID = ""
     private var stickerNeedUpdate = false
 
+    interface Listener {
+        fun getStickerViewHolderListener(): StickerViewHolder.Listener?
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initArguments()
         initDagger()
         initObserver()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is Listener) {
+            stickerListener = context.getStickerViewHolderListener()
+        }
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+                LAYOUT, container, false
+        )
+    }
+
+    override fun getScreenName(): String {
+        return "sticker-chatroom"
+    }
+
+    override fun initInjector() {
+        getComponent(ChatComponent::class.java).inject(this)
     }
 
     private fun initObserver() {
@@ -96,13 +125,12 @@ class StickerFragment(
         fun create(
                 stickerGroup: StickerGroup,
                 needUpdate: Boolean,
-                stickerListener: StickerViewHolder.Listener?
         ): StickerFragment {
             val bundle = Bundle().apply {
                 putString(keyStickerGroupUID, stickerGroup.groupUUID)
                 putBoolean(keyStickerNeedUpdate, needUpdate)
             }
-            return StickerFragment(LAYOUT, stickerListener).apply {
+            return StickerFragment().apply {
                 arguments = bundle
             }
         }
