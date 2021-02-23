d7e3b910c343437b239d0a4cde2dde411cb8dbe8
Reivin <reivin.oktavianus@tokopedia.com>
2021-02-24 03:04:46 +0700

    Populate menu setting data according to access

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/viewmodel/MenuSettingViewModel.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/viewmodel/MenuSettingViewModel.kt
@@ -5,38 +5,49 @@ import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
+import com.tokopedia.sellerhome.settings.view.uimodel.menusetting.MenuSettingAccess
 import com.tokopedia.shop.common.constant.AccessId
 import com.tokopedia.shop.common.domain.interactor.AuthorizeAccessUseCase
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MenuSettingViewModel @Inject constructor(
-        private val authorizeAccessUseCase: AuthorizeAccessUseCase,
+        private val authorizeAddressAccessUseCase: AuthorizeAccessUseCase,
+        private val authorizeInfoAccessUseCase: AuthorizeAccessUseCase,
+        private val authorizeNotesAccessUseCase: AuthorizeAccessUseCase,
+        private val authorizeShipmentAccessUseCase: AuthorizeAccessUseCase,
         private val userSession: UserSessionInterface,
         private val dispatchers: CoroutineDispatchers
 ): BaseViewModel(dispatchers.main), LifecycleObserver {
 
-    private val mShopSettingAccessLiveData = MutableLiveData<Result<Boolean>>()
-    val shopSettingAccessLiveData: LiveData<Result<Boolean>>
+    private val mShopSettingAccessLiveData = MutableLiveData<Result<MenuSettingAccess>>()
+    val shopSettingAccessLiveData: LiveData<Result<MenuSettingAccess>>
         get() = mShopSettingAccessLiveData
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun onResume() {
-        checkShopSettingAccess()
-    }
-
-    private fun checkShopSettingAccess() {
+    fun checkShopSettingAccess() {
         launchCatchError(
                 block = {
-                    userSession.shopId.toIntOrZero().let { shopId ->
-                        AuthorizeAccessUseCase.createRequestParams(shopId, AccessId.SHOP_SETTING_INFO).let { requestParams ->
-                            mShopSettingAccessLiveData.postValue(Success(withContext(dispatchers.io) {
-                                authorizeAccessUseCase.execute(requestParams)
-                            }))
+                    if (userSession.isShopOwner) {
+                        mShopSettingAccessLiveData.value = Success(MenuSettingAccess())
+                    } else {
+                        withContext(dispatchers.io) {
+                            userSession.shopId.toIntOrZero().let { shopId ->
+                                val addressRole = async { getAddressAccessRole(shopId) }
+                                val infoRole = async { getInfoAccessRole(shopId) }
+                                val notesRole = async { getNotesAccessRole(shopId) }
+                                val shipmentRole = async { getShipmentAccessRole(shopId) }
+
+                                mShopSettingAccessLiveData.postValue(Success(MenuSettingAccess(
+                                        isAddressAccessAuthorized = addressRole.await(),
+                                        isInfoAccessAuthorized = infoRole.await(),
+                                        isNotesAccessAuthorized = notesRole.await(),
+                                        isShipmentAccessAuthorized = shipmentRole.await())))
+                            }
                         }
                     }
                 },
@@ -44,6 +55,30 @@ class MenuSettingViewModel @Inject constructor(
                     mShopSettingAccessLiveData.value = Fail(it)
                 }
         )
+    }
+
+    private suspend fun getAddressAccessRole(shopId: Int): Boolean {
+        AuthorizeAccessUseCase.createRequestParams(shopId, AccessId.SHOP_SETTING_ADDRESS).let { requestParams ->
+            return authorizeAddressAccessUseCase.execute(requestParams)
+        }
+    }
+
+    private suspend fun getInfoAccessRole(shopId: Int): Boolean {
+        AuthorizeAccessUseCase.createRequestParams(shopId, AccessId.SHOP_SETTING_INFO).let { requestParams ->
+            return authorizeInfoAccessUseCase.execute(requestParams)
+        }
+    }
+
+    private suspend fun getNotesAccessRole(shopId: Int): Boolean {
+        AuthorizeAccessUseCase.createRequestParams(shopId, AccessId.SHOP_SETTING_NOTES).let { requestParams ->
+            return authorizeNotesAccessUseCase.execute(requestParams)
+        }
+    }
+
+    private suspend fun getShipmentAccessRole(shopId: Int): Boolean {
+        AuthorizeAccessUseCase.createRequestParams(shopId, AccessId.SHOP_SETTING_SHIPMENT).let { requestParams ->
+            return authorizeShipmentAccessUseCase.execute(requestParams)
+        }
     }
 
 }
