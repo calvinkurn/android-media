aff74bdef88b606d47b60ef4e436e37d764f2dc1
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-02-23 13:11:41 +0700

    revert scope di

--- features/entertainment/digital_deals/src/main/java/com/tokopedia/digital_deals/domain/getusecase/GetEventContentUseCase.kt
+++ features/entertainment/digital_deals/src/main/java/com/tokopedia/digital_deals/domain/getusecase/GetEventContentUseCase.kt
@@ -1,11 +1,14 @@
 package com.tokopedia.digital_deals.domain.getusecase
 
+import android.content.Context
+import com.tokopedia.digital_deals.di.scope.DealsScope
 import com.tokopedia.digital_deals.domain.util.DealsQuery
 import com.tokopedia.digital_deals.view.model.response.EventContentData
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import javax.inject.Inject
 
 class GetEventContentUseCase @Inject constructor(
+        @DealsScope private val context: Context,
         private val gqlUseCase: GraphqlUseCase<EventContentData>
 ){
     private val paramTypeId = "typeID"
