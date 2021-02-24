81dadfa3dfccc1924aeab2ab215635585b9c2534
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-02-24 19:43:49 +0700

    init timber utils

---
+++ libraries/logger/src/main/java/com/tokopedia/logger/utils/TimberUtils.kt
@@ -0,0 +1,33 @@
+package com.tokopedia.logger.utils
+
+import timber.log.Timber
+
+
+enum class Priority {
+    P1,
+    P2
+}
+
+enum class Type {
+    WARNING,
+    ERROR
+}
+
+// Timber.w("P2#PDP_OPEN_DEEPLINK_ERROR#$weblink;errorCode=$errorCode")
+
+fun Timber.sendLog(priority: Priority, tag: String, data: Map<String, String>, type: Type) {
+    val priorityText = when (priority) {
+        Priority.P1 -> "P1"
+        Priority.P2 -> "P2"
+    }
+
+    val dataBuilder = StringBuilder()
+    val keySet = data.keys
+    keySet.forEachIndexed { index, key ->
+        dataBuilder.append("$key=${data.getValue(key)}")
+        if(index + 1 < keySet.size) {
+            dataBuilder.append(";")
+        }
+    }
+    Timber.w("$priorityText#$tag#$dataBuilder")
+}
