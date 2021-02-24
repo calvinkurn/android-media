d902d0c9515ae88a37b944c7c275d91933fd7a4c
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-02-24 16:05:35 +0700

    refactor test class

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomGeneralTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomGeneralTest.kt
@@ -1,14 +1,13 @@
 package com.tokopedia.topchat.chatroom.view.activity
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
-import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 class TopchatRoomGeneralTest : TopchatRoomTest() {
@@ -50,78 +49,6 @@ class TopchatRoomGeneralTest : TopchatRoomTest() {
 
         // Then
         onView(withId(R.id.new_comment)).check(matches(withText(intentMsg)))
-    }
-
-    /**
-     * The attachment menu should be hidden when user tap sticker menu
-     */
-    @Test
-    fun test_open_attachment_menu_then_open_sticker() {
-        // Given
-        setupChatRoomActivity()
-        getChatUseCase.response = firstPageChatAsSeller
-        chatAttachmentUseCase.response = chatAttachmentResponse
-        stickerGroupUseCase.response = stickerGroupAsBuyer
-        chatListStickerUseCase.response = stickerListAsBuyer
-        inflateTestFragment()
-
-        // WHen
-        clickPlusIconMenu()
-        clickStickerIconMenu()
-
-        // Then
-        onView(withId(R.id.rv_topchat_attachment_menu)).check(
-                matches(not(isDisplayed()))
-        )
-        onView(withId(R.id.ll_sticker_container)).check(
-                matches(isDisplayed())
-        )
-    }
-
-    @Test
-    fun click_back_btn_when_attachment_menu_opened() {
-        // Given
-        setupChatRoomActivity()
-        getChatUseCase.response = firstPageChatAsSeller
-        chatAttachmentUseCase.response = chatAttachmentResponse
-        inflateTestFragment()
-
-        // WHen
-        clickPlusIconMenu()
-        pressBack()
-
-        // Then
-        onView(withId(R.id.fl_chat_menu)).check(
-                matches(not(isDisplayed()))
-        )
-        onView(withId(R.id.rv_topchat_attachment_menu)).check(
-                matches(not(isDisplayed()))
-        )
-    }
-
-    @Test
-    fun click_sticker_icon_show_sticker_container() {
-        // Given
-        setupChatRoomActivity()
-        getChatUseCase.response = firstPageChatAsSeller
-        chatAttachmentUseCase.response = chatAttachmentResponse
-        stickerGroupUseCase.response = stickerGroupAsBuyer
-        chatListStickerUseCase.response = stickerListAsBuyer
-        inflateTestFragment()
-
-        // WHen
-        clickStickerIconMenu()
-
-        // Then
-        onView(withId(R.id.fl_chat_menu)).check(
-                matches(isDisplayed())
-        )
-        onView(withId(R.id.ll_sticker_container)).check(
-                matches(isDisplayed())
-        )
-        onView(withId(R.id.rv_topchat_attachment_menu)).check(
-                matches(not(isDisplayed()))
-        )
     }
 
 }
