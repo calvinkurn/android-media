3cc7d448facf17f8723ac18ca1bad02192a2a22e
lukaskris12 <lukaskris12@gmail.com>
2021-02-24 16:02:06 +0700

    Localization Query Recom

--- features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetRecommendationUseCase.kt
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetRecommendationUseCase.kt
@@ -1,15 +1,16 @@
 package com.tokopedia.recommendation_widget_common.domain.coroutines
 
+import android.content.Context
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-
+import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
 import com.tokopedia.recommendation_widget_common.data.RecommendationEntity
 import com.tokopedia.recommendation_widget_common.data.mapper.RecommendationEntityMapper
 import com.tokopedia.recommendation_widget_common.domain.coroutines.base.UseCase
 import com.tokopedia.recommendation_widget_common.domain.request.GetRecommendationRequestParam
 import com.tokopedia.recommendation_widget_common.domain.request.GetRecommendationUseCaseRequest
+import com.tokopedia.recommendation_widget_common.ext.toQueryParam
 import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationWidget
-
 import javax.inject.Inject
 
 /**
@@ -17,14 +18,14 @@ import javax.inject.Inject
  */
 
 open class GetRecommendationUseCase @Inject
-constructor(private val graphqlRepository: GraphqlRepository)
+constructor(private val context: Context, private val graphqlRepository: GraphqlRepository)
     : UseCase<GetRecommendationRequestParam, List<RecommendationWidget>>() {
 
     override suspend fun getData(inputParameter: GetRecommendationRequestParam): List<RecommendationWidget> {
         val graphqlUseCase = GraphqlUseCase<RecommendationEntity>(graphqlRepository)
-
+        val queryParam = ChooseAddressUtils.getLocalizingAddressData(context)?.toQueryParam(inputParameter.queryParam) ?: inputParameter.queryParam
         graphqlUseCase.setTypeClass(RecommendationEntity::class.java)
-        graphqlUseCase.setRequestParams(inputParameter.toGqlRequest())
+        graphqlUseCase.setRequestParams(inputParameter.copy(queryParam = queryParam).toGqlRequest())
         graphqlUseCase.setGraphqlQuery(GetRecommendationUseCaseRequest.widgetListQuery)
         return RecommendationEntityMapper.mappingToRecommendationModel(
                 graphqlUseCase.executeOnBackground().productRecommendationWidget?.data?: listOf()
--- features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetSingleRecommendationUseCase.kt
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetSingleRecommendationUseCase.kt
@@ -1,13 +1,16 @@
 package com.tokopedia.recommendation_widget_common.domain.coroutines
 
+import android.content.Context
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
 
 import com.tokopedia.recommendation_widget_common.data.SingleProductRecommendationEntity
 import com.tokopedia.recommendation_widget_common.data.mapper.SingleProductRecommendationMapper
 import com.tokopedia.recommendation_widget_common.domain.coroutines.base.UseCase
 import com.tokopedia.recommendation_widget_common.domain.request.GetRecommendationRequestParam
 import com.tokopedia.recommendation_widget_common.domain.request.GetRecommendationUseCaseRequest
+import com.tokopedia.recommendation_widget_common.ext.toQueryParam
 import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationWidget
 import javax.inject.Inject
 
@@ -18,13 +21,13 @@ import rx.Observable
  */
 
 open class GetSingleRecommendationUseCase @Inject
-constructor(private val graphqlRepository: GraphqlRepository)
+constructor(private val context: Context, private val graphqlRepository: GraphqlRepository)
     : UseCase<GetRecommendationRequestParam, RecommendationWidget>() {
     override suspend fun getData(inputParameter: GetRecommendationRequestParam): RecommendationWidget {
         val graphqlUseCase: GraphqlUseCase<SingleProductRecommendationEntity> = GraphqlUseCase(graphqlRepository)
-
+        val queryParam = ChooseAddressUtils.getLocalizingAddressData(context)?.toQueryParam(inputParameter.queryParam) ?: inputParameter.queryParam
         graphqlUseCase.setTypeClass(SingleProductRecommendationEntity::class.java)
-        graphqlUseCase.setRequestParams(inputParameter.toGqlRequest())
+        graphqlUseCase.setRequestParams(inputParameter.copy(queryParam = queryParam).toGqlRequest())
         graphqlUseCase.setGraphqlQuery(GetRecommendationUseCaseRequest.singleQuery)
         return SingleProductRecommendationMapper.convertIntoRecommendationWidget(
             graphqlUseCase.executeOnBackground().productRecommendationWidget?.data
