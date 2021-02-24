a1909ee33e919876d959a333ab49e2b04dc35b80
kenny <kenny.hadisaputra@tokopedia.com>
2021-02-24 14:36:05 +0700

    handle scroll product featured

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ProductFeaturedViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ProductFeaturedViewComponent.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.play.R
 import com.tokopedia.play.ui.product.ProductBasicViewHolder
 import com.tokopedia.play.ui.productfeatured.adapter.ProductFeaturedAdapter
+import com.tokopedia.play.ui.productfeatured.itemdecoration.ProductFeaturedItemDecoration
 import com.tokopedia.play.ui.productsheet.viewholder.ProductLineViewHolder
 import com.tokopedia.play.view.uimodel.PlayProductUiModel
 import com.tokopedia.play_common.viewcomponent.ViewComponent
@@ -14,9 +15,9 @@ import com.tokopedia.play_common.viewcomponent.ViewComponent
  */
 class ProductFeaturedViewComponent(
         container: ViewGroup,
-) : ViewComponent(container, R.id.rv_product_featured) {
+) : ViewComponent(container, R.id.view_product_featured) {
 
-    private val rvProductFeatured = rootView as RecyclerView
+    private val rvProductFeatured: RecyclerView = findViewById(R.id.rv_product_featured)
 
     private val adapter = ProductFeaturedAdapter(object : ProductBasicViewHolder.Listener {
         override fun onClickProductCard(product: PlayProductUiModel.Product, position: Int) {
@@ -26,6 +27,7 @@ class ProductFeaturedViewComponent(
 
     init {
         rvProductFeatured.adapter = adapter
+        rvProductFeatured.addItemDecoration(ProductFeaturedItemDecoration(rvProductFeatured.context))
     }
 
     fun setFeaturedProducts(featuredProducts: List<PlayProductUiModel>) {
