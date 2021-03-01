cb9fa51448eb77dda19c8e84805b2af3dfe11897
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-03-01 14:49:50 +0700

    test coverage

--- features/discovery/home/src/androidTest/java/com/tokopedia/home/mock/MockOvoModel.kt
+++ features/discovery/home/src/androidTest/java/com/tokopedia/home/mock/MockOvoModel.kt
@@ -39,7 +39,7 @@ object MockOvoModel {
                         iconImageURL="https://ecs7.tokopedia.net/img/blog/promo/2020/05/kupon-sayaxxxhdpi.png",
                         sectionContent= listOf(
                                 SectionContentItem(textAttributes= TextAttributes(colour="", text="Kupon Saya", isBold=true), tagAttributes= TagAttributes(text="", backgroundColour=""), type="text"),
-                                SectionContentItem(textAttributes=TextAttributes(colour="#03ac0e", text="Kupon Baru", isBold=true), tagAttributes=TagAttributes(text="", backgroundColour=""), type="text")
+                                SectionContentItem(textAttributes=TextAttributes(colour="", text="Kupon Baru", isBold=true), tagAttributes=TagAttributes(text="", backgroundColour=""), type="text")
                         ),
                         redirectAppLink="tokopedia://rewards/kupon-saya", mainPageTitle=""), cashBackData=null, isPendingTokocashChecked=false, isWalletDataError=false, isTokoPointDataError=false, isUserLogin=true)
         return headerDataModel
