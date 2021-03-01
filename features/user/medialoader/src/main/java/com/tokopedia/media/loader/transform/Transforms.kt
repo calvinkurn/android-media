44fd0515859ac4e47920eca24fac622b921f7054
isfaaghyth <isfaaghyth@gmail.com>
2021-03-01 19:15:19 +0700

    added CenterInside and Rotate tranformation

--- features/user/medialoader/src/main/java/com/tokopedia/media/loader/transform/Transforms.kt
+++ features/user/medialoader/src/main/java/com/tokopedia/media/loader/transform/Transforms.kt
@@ -2,8 +2,10 @@ package com.tokopedia.media.loader.transform
 
 import com.bumptech.glide.load.resource.bitmap.*
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.Rotate
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 abstract class MediaTransformation: BitmapTransformation()
@@ -12,3 +14,5 @@ typealias RoundedCorners = RoundedCorners
 class CenterCrop: CenterCrop()
 class CircleCrop: CircleCrop()
 class FitCenter: FitCenter()
+class CenterInside: CenterInside()
+class Rotate(degreesToRotate: Int): Rotate(degreesToRotate)
