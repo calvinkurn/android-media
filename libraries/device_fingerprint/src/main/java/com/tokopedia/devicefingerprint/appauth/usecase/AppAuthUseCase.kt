e95ebf2cb8fc8a6526be623c23942db26c5e7605
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-03-02 11:04:03 +0700

    fix app auth query

--- libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/appauth/usecase/AppAuthUseCase.kt
+++ libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/appauth/usecase/AppAuthUseCase.kt
@@ -7,21 +7,20 @@ import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
-import com.tokopedia.usecase.RequestParams
 import javax.inject.Inject
 
 class AppAuthUseCase @Inject constructor(
         repository: GraphqlRepository) : GraphqlUseCase<AppAuthResponse>(repository) {
 
     init {
+        setGraphqlQuery(query)
         setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build())
         setTypeClass(AppAuthResponse::class.java)
     }
 
-    private var params: RequestParams = RequestParams.create()
-
-    private val query =
-            """
+    companion object {
+        val query:String =
+                """
             mutation mutationSignDvc(${'$'}input: MutationSignDvcRequest!){
               mutationSignDvc(input: ${'$'}input) {
                 success
@@ -29,6 +28,7 @@ class AppAuthUseCase @Inject constructor(
               }
             }
         """.trimIndent()
+    }
 
     fun setParams(content: String, version: String = "1") {
         val params: Map<String, Any?> = mutableMapOf(
