90eff73eebb5d1fb7684b844002ca4d3f221426b
hansenpw <hansen.wijaya@tokopedia.com>
2021-03-02 14:13:22 +0700

    bottom sheet view binding and ignore activity layout

--- features/transaction/cart/src/main/java/com/tokopedia/cart/view/bottomsheet/SummaryTransactionBottomsheetHelper.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/view/bottomsheet/SummaryTransactionBottomsheetHelper.kt
@@ -1,16 +1,16 @@
 package com.tokopedia.cart.view.bottomsheet
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.FragmentManager
-import com.tokopedia.cart.R
+import com.tokopedia.cart.databinding.LayoutBottomsheetSummaryTransactionBinding
 import com.tokopedia.cart.domain.model.cartlist.CartListData
 import com.tokopedia.design.utils.CurrencyFormatUtil
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.purchase_platform.common.utils.removeDecimalSuffix
 import com.tokopedia.unifycomponents.BottomSheetUnify
-import kotlinx.android.synthetic.main.layout_bottomsheet_summary_transaction.view.*
 
 fun showSummaryTransactionBottomsheet(cartListData: CartListData, fragmentManager: FragmentManager, context: Context) {
 
@@ -19,111 +19,111 @@ fun showSummaryTransactionBottomsheet(cartListData: CartListData, fragmentManage
     bottomSheet.showCloseIcon = false
     bottomSheet.showHeader = false
 
-    val view = View.inflate(context, R.layout.layout_bottomsheet_summary_transaction, null)
+    val binding = LayoutBottomsheetSummaryTransactionBinding.inflate(LayoutInflater.from(context))
 
-    renderPriceTotal(view, cartListData)
-    renderDiscount(cartListData, view)
-    renderPaymentTotal(view, cartListData)
-    renderPromo(cartListData, view)
-    renderSellerCashback(cartListData, view)
-    renderSeparatorBenefit(cartListData, view)
+    renderPriceTotal(binding, cartListData)
+    renderDiscount(cartListData, binding)
+    renderPaymentTotal(binding, cartListData)
+    renderPromo(cartListData, binding)
+    renderSellerCashback(cartListData, binding)
+    renderSeparatorBenefit(cartListData, binding)
 
-    bottomSheet.setChild(view)
+    bottomSheet.setChild(binding.root)
     bottomSheet.show(fragmentManager, "Cart Summary Transaction")
 }
 
-private fun renderSeparatorBenefit(cartListData: CartListData, view: View) {
+private fun renderSeparatorBenefit(cartListData: CartListData, binding: LayoutBottomsheetSummaryTransactionBinding) {
     if (cartListData.shoppingSummaryData.promoValue > 0 ||
             cartListData.shoppingSummaryData.sellerCashbackValue > 0) {
-        view.separator_benefit.show()
+        binding.separatorBenefit.show()
     } else {
-        view.separator_benefit.gone()
+        binding.separatorBenefit.gone()
     }
 }
 
-private fun renderSellerCashback(cartListData: CartListData, view: View) {
+private fun renderSellerCashback(cartListData: CartListData, binding: LayoutBottomsheetSummaryTransactionBinding) {
     if (cartListData.shoppingSummaryData.sellerCashbackValue > 0) {
-        view.text_total_cashback_value?.apply {
+        binding.textTotalCashbackValue.apply {
             text = CurrencyFormatUtil.convertPriceValueToIdrFormat(cartListData.shoppingSummaryData.sellerCashbackValue, false)
                     .replace("Rp","")
                     .removeDecimalSuffix()
             visibility = View.VISIBLE
         }
-        view.text_total_cashback_title?.apply {
+        binding.textTotalCashbackTitle.apply {
             text = cartListData.shoppingSummaryData.sellerCashbackWording
             visibility = View.VISIBLE
         }
     } else {
-        view.text_total_cashback_value?.apply {
+        binding.textTotalCashbackValue.apply {
             visibility = View.GONE
         }
-        view.text_total_cashback_title?.apply {
+        binding.textTotalCashbackTitle.apply {
             visibility = View.GONE
         }
     }
 }
 
-private fun renderPromo(cartListData: CartListData, view: View) {
+private fun renderPromo(cartListData: CartListData, binding: LayoutBottomsheetSummaryTransactionBinding) {
     if (cartListData.shoppingSummaryData.promoValue > 0) {
-        view.text_total_promo_value?.apply {
+        binding.textTotalPromoValue.apply {
             text = CurrencyFormatUtil.convertPriceValueToIdrFormat(cartListData.shoppingSummaryData.promoValue, false)
                     .replace("Rp","")
                     .removeDecimalSuffix()
             visibility = View.VISIBLE
         }
-        view.text_total_promo_title?.apply {
+        binding.textTotalPromoTitle.apply {
             text = cartListData.shoppingSummaryData.promoWording
             visibility = View.VISIBLE
         }
     } else {
-        view.text_total_promo_value?.apply {
+        binding.textTotalPromoValue.apply {
             visibility = View.GONE
         }
-        view.text_total_promo_title?.apply {
+        binding.textTotalPromoTitle.apply {
             visibility = View.GONE
         }
     }
 }
 
-private fun renderPaymentTotal(view: View, cartListData: CartListData) {
-    view.text_total_pay_title?.apply {
+private fun renderPaymentTotal(binding: LayoutBottomsheetSummaryTransactionBinding, cartListData: CartListData) {
+    binding.textTotalPayTitle.apply {
         cartListData.shoppingSummaryData.paymentTotalWording.let {
             text = it
         }
     }
-    view.text_total_pay_value?.apply {
+    binding.textTotalPayValue.apply {
         val totalPay = cartListData.shoppingSummaryData.paymentTotal
         this.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(totalPay, false).removeDecimalSuffix()
     }
 }
 
-private fun renderDiscount(cartListData: CartListData, view: View) {
+private fun renderDiscount(cartListData: CartListData, binding: LayoutBottomsheetSummaryTransactionBinding) {
     if (cartListData.shoppingSummaryData.discountValue > 0) {
-        view.text_discount_total_value?.apply {
+        binding.textDiscountTotalValue.apply {
             text = CurrencyFormatUtil.convertPriceValueToIdrFormat(cartListData.shoppingSummaryData.discountValue * -1, false).removeDecimalSuffix()
             visibility = View.VISIBLE
         }
-        view.text_discount_total_title?.apply {
+        binding.textDiscountTotalTitle.apply {
             text = cartListData.shoppingSummaryData.discountTotalWording
             visibility = View.VISIBLE
         }
     } else {
-        view.text_discount_total_value?.apply {
+        binding.textDiscountTotalValue.apply {
             visibility = View.GONE
         }
-        view.text_discount_total_title?.apply {
+        binding.textDiscountTotalTitle.apply {
             visibility = View.GONE
         }
     }
 }
 
-private fun renderPriceTotal(view: View, cartListData: CartListData) {
-    view.text_price_total_title?.apply {
+private fun renderPriceTotal(binding: LayoutBottomsheetSummaryTransactionBinding, cartListData: CartListData) {
+    binding.textPriceTotalTitle.apply {
         cartListData.shoppingSummaryData.totalWording.let {
             text = it.replace("[0-9]".toRegex(), cartListData.shoppingSummaryData.qty)
         }
     }
-    view.text_price_total_value?.apply {
+    binding.textPriceTotalValue.apply {
         this.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(cartListData.shoppingSummaryData.totalValue, false).removeDecimalSuffix()
     }
 }
