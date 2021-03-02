dd3c694286388c984eddd4b3a89fe9feb616607e
astidhiyaa <asti.anzaria@tokopedia.com>
2021-03-02 12:27:11 +0800

    - Add view model - Set up map - Add layout for container in collapsingToolbar

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/search_map/di/HotelSearchMapViewModelModule.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/search_map/di/HotelSearchMapViewModelModule.kt
@@ -1,10 +1,26 @@
 package com.tokopedia.hotel.search_map.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
+import com.tokopedia.hotel.search.presentation.viewmodel.HotelSearchResultViewModel
+import com.tokopedia.hotel.search_map.presentation.viewmodel.HotelSearchMapViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 /**
  * @author by furqan on 01/03/2021
  */
 @Module
-class HotelSearchMapViewModelModule {
+abstract class HotelSearchMapViewModelModule {
+    @HotelSearchMapScope
+    @Binds
+    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HotelSearchResultViewModel::class)
+    internal abstract fun hotelSearchMapResultViewModel(viewModel: HotelSearchMapViewModel): ViewModel
 }
