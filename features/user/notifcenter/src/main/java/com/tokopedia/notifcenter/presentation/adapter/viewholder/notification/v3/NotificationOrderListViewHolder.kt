19e9400324c9a64afe7bc5c6c2b8ba9b023f2f01
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-03-02 19:09:36 +0700

    add shadow for first card order

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/NotificationOrderListViewHolder.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/NotificationOrderListViewHolder.kt
@@ -1,12 +1,15 @@
 package com.tokopedia.notifcenter.presentation.adapter.viewholder.notification.v3
 
+import android.view.Gravity
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.notifcenter.R
 import com.tokopedia.notifcenter.data.entity.orderlist.NotifOrderListUiModel
 import com.tokopedia.notifcenter.presentation.adapter.viewholder.notification.v3.payload.PayloadOrderList
+import com.tokopedia.notifcenter.util.view.ShadowGenerator
 import com.tokopedia.notifcenter.widget.ItemOrderListLinearLayout
 
 class NotificationOrderListViewHolder(
@@ -15,6 +18,17 @@ class NotificationOrderListViewHolder(
 
     private val firstCard: ItemOrderListLinearLayout? = itemView?.findViewById(R.id.ll_first_card)
     private val secondCard: ItemOrderListLinearLayout? = itemView?.findViewById(R.id.ll_second_card)
+    private val bg = ShadowGenerator.generateBackgroundWithShadow(
+            firstCard,
+            com.tokopedia.unifyprinciples.R.color.Unify_N0,
+            com.tokopedia.unifyprinciples.R.dimen.unify_space_8,
+            com.tokopedia.unifyprinciples.R.dimen.unify_space_8,
+            com.tokopedia.unifyprinciples.R.dimen.unify_space_8,
+            com.tokopedia.unifyprinciples.R.dimen.unify_space_8,
+            com.tokopedia.unifyprinciples.R.color.Unify_N700_20,
+            R.dimen.notif_dp_6,
+            R.dimen.notif_dp_6
+    )
 
     override fun bind(element: NotifOrderListUiModel, payloads: MutableList<Any>) {
         if (payloads.isEmpty()) return
@@ -28,6 +42,7 @@ class NotificationOrderListViewHolder(
 
     override fun bind(element: NotifOrderListUiModel) {
         bindFirstCard(element)
+        bindFirstCardBackground()
     }
 
     private fun bindFirstCard(element: NotifOrderListUiModel) {
@@ -37,6 +52,12 @@ class NotificationOrderListViewHolder(
         } else {
             firstCard?.show()
             firstCard?.bindItem(firstItem)
+        }
+    }
+
+    private fun bindFirstCardBackground() {
+        if (firstCard?.isVisible == true) {
+            firstCard.background = bg
         }
     }
 
