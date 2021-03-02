06ce96d2aa3f114c90243e4a5938301f7d82998e
yoasfs <yoas.susantio@tokopedia.com>
2021-03-02 17:23:25 +0700

    try to render model

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/static_channel/balancewidget/BalanceAdapter.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/static_channel/balancewidget/BalanceAdapter.kt
@@ -20,6 +20,8 @@ import com.tokopedia.home.analytics.v2.OvoWidgetTracking
 import com.tokopedia.home.beranda.data.model.SectionContentItem
 import com.tokopedia.home.beranda.listener.HomeCategoryListener
 import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.BalanceDrawerItemModel
+import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.BalanceTagAttribute
+import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.BalanceTextAttribute
 import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.HomeBalanceModel
 import com.tokopedia.home_component.util.invertIfDarkMode
 
@@ -67,7 +69,6 @@ class BalanceAdapter(val listener: HomeCategoryListener?): RecyclerView.Adapter<
             renderTokoPoint(drawerItem)
         }
 
-
         private fun renderTokoPoint(element: BalanceDrawerItemModel?) {
             val tokoPointHolder = itemView.findViewById<View>(R.id.container_tokopoint)
             val tvBalanceTokoPoint = itemView.findViewById<TextView>(R.id.tv_balance_tokopoint)
@@ -77,11 +78,49 @@ class BalanceAdapter(val listener: HomeCategoryListener?): RecyclerView.Adapter<
             val tokopointActionContainer = itemView.findViewById<View>(R.id.container_action_tokopoint)
             val mTextCouponCount = itemView.findViewById<TextView>(R.id.text_coupon_count)
 
-
+            renderBalanceText(element?.balanceTitleTextAttribute, element?.balanceTitleTagAttribute, tvBalanceTokoPoint)
+            renderBalanceText(element?.balanceSubTitleTextAttribute, element?.balanceSubTitleTagAttribute, tvActionTokopoint)
 
         }
 
-
+        private fun renderBalanceText(textAttr: BalanceTextAttribute?, tagAttr: BalanceTagAttribute?, textView: TextView, textSize: Int = R.dimen.sp_12) {
+            textView.background = null
+            textView.text = null
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemView.context.resources.getDimension(textSize))
+            if (tagAttr != null && !TextUtils.isEmpty(tagAttr.text)) {
+                if (!TextUtils.isEmpty(tagAttr.backgroundColour) && HexValidator.validate(tagAttr.backgroundColour)) {
+                    val drawable = ContextCompat.getDrawable(itemView.context, R.drawable.bg_tokopoints_rounded)
+                    if (drawable is GradientDrawable) {
+                        val shapeDrawable = drawable as GradientDrawable?
+                        shapeDrawable!!.setColorFilter(Color.parseColor(tagAttr.backgroundColour), PorterDuff.Mode.SRC_ATOP)
+                        textView.background = shapeDrawable
+                        val horizontalPadding = itemView.context.resources.getDimensionPixelSize(R.dimen.dp_2)
+                        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, itemView.context.resources.getDimension(R.dimen.sp_8))
+                        textView.setTypeface(null, Typeface.NORMAL)
+                        textView.setPadding(horizontalPadding, 0, horizontalPadding, 0)
+                    }
+                    textView.setTextColor(ContextCompat.getColor(itemView.context, R.color.Unify_N0))
+                } else {
+                    textView.setTextColor(ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_N700_68))
+                }
+                if (!TextUtils.isEmpty(tagAttr.text)) {
+                    textView.text = tagAttr.text
+                }
+            } else if (textAttr != null && !TextUtils.isEmpty(textAttr.text)) {
+                if (!TextUtils.isEmpty(textAttr.colour) && HexValidator.validate(textAttr.colour)) {
+                    textView.setTextColor(Color.parseColor(textAttr.colour).invertIfDarkMode(itemView.context))
+                } else {
+                    textView.setTextColor(ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_N700_68))
+                }
+                if (textAttr.isBold) {
+                    textView.setTypeface(null, Typeface.BOLD)
+                } else {
+                    textView.setTypeface(null, Typeface.NORMAL)
+                }
+                if (!TextUtils.isEmpty(textAttr.text)) {
+                    textView.text = textAttr.text
+                }
+            }
+        }
     }
-
 }
