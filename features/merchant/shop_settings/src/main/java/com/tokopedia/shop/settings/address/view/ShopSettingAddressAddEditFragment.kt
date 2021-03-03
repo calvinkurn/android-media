23ea412548f2bd32f49525de341d32d0bae8a295
saidfaisal <said.faisal@tokopedia.com>
2021-03-03 13:10:57 +0700

    fix address and basicinfo packages

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/address/view/ShopSettingAddressAddEditFragment.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/address/view/ShopSettingAddressAddEditFragment.kt
@@ -39,7 +39,7 @@ class ShopSettingAddressAddEditFragment: BaseDaggerFragment(), ShopSettingAddres
     private var selectedProvinceId = -1
     private val zipCodes: MutableList<String> = mutableListOf()
     private val zipCodesAdapter: ArrayAdapter<String>  by lazy {
-        ArrayAdapter<String>(requireActivity(), R.layout.item_autocomplete_text_double_row, R.id.item, zipCodes)
+        ArrayAdapter<String>(requireActivity(), R.layout.item_auto_complete_text_double_row, R.id.item, zipCodes)
     }
 
     @Inject lateinit var presenter: ShopSettingAddressAddEditPresenter
