7fc62f48b2b233ccd16fde59a32d7c4a0cd66195
misael-jonathan <misael.jonathan@tokopedia.com>
2021-03-05 13:13:03 +0700

    remove additionalinfo const from digitalcheckoutconst

--- features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/data/DigitalCheckoutConst.kt
+++ features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/data/DigitalCheckoutConst.kt
@@ -10,13 +10,6 @@ object DigitalCheckoutConst {
         const val REQUEST_BODY_OTP_CART_TYPE = "cart"
     }
 
-    object AdditionalInfo {
-        const val STRING_PRICE = "Harga"
-        const val STRING_PROMO = "Promo"
-        const val STRING_TOTAL_PAYMENT = "Total Bayar"
-        const val STRING_PAYMENT = "Pembayaran"
-    }
-
     object SummaryInfo {
         const val STRING_SUBTOTAL_TAGIHAN = "Subtotal Tagihan"
         const val STRING_KODE_PROMO = "Kode Promo"
