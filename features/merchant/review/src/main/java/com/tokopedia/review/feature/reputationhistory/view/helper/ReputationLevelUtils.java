59712a5f161df213ac22a57d47961397e7dcc869
leonard2907 <leonard.gunawan@tokopedia.com>
2021-03-05 08:56:15 +0700

    Remove unused code

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/reputationhistory/view/helper/ReputationLevelUtils.java
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/reputationhistory/view/helper/ReputationLevelUtils.java
@@ -1,17 +1,9 @@
 package com.tokopedia.review.feature.reputationhistory.view.helper;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-
-import com.tokopedia.kotlin.extensions.view.ImageViewExtKt;
 import com.tokopedia.review.R;
 
 /**
@@ -19,7 +11,6 @@ import com.tokopedia.review.R;
  */
 public class ReputationLevelUtils {
 
-    public static final String SPEECH_BUBBLE_IMAGE = "https://images.tokopedia.net/img/android/review/review_speechbubble_r90.png";
     private static final int MEDAL_NONE = com.tokopedia.design.R.drawable.ic_icon_repsis_medal;
     private static final int MEDAL_BRONZE = com.tokopedia.design.R.drawable.ic_icon_repsis_medal_bronze;
     private static final int MEDAL_SILVER = com.tokopedia.design.R.drawable.ic_icon_repsis_medal_silver;
@@ -53,31 +44,9 @@ public class ReputationLevelUtils {
         }
     }
 
-    public static void setReputationMedals(Context context, LinearLayout layout, int typeMedal, int levelMedal, String reputationPoints) {
-        int medalType = getTypeMedal(typeMedal);
-
-        layout.removeAllViews();
-        layout.setOrientation(LinearLayout.HORIZONTAL);
-        if(typeMedal == MEDAL_TYPE_0) {
-            createDefaultMedal(context, layout);
-        } else {
-            createMedalBasedOnType(context, layout, medalType, levelMedal, reputationPoints);
-        }
-    }
-
     private static void createDefaultMedal(Context context, LinearLayout layout) {
         View medal = createMedal(context, MEDAL_NONE);
         layout.addView(medal);
-    }
-
-    private static void createMedalBasedOnType(Context context, LinearLayout layout, int medalType, int levelMedal, String reputationPoints) {
-        for( int i = 1 ; i <= levelMedal ; i++){
-            View medal = createMedal(context, medalType);
-            layout.addView(medal);
-            if(i == levelMedal) {
-                layout.setOnClickListener(onReputationClick(context, reputationPoints, medal));
-            }
-        }
     }
 
     private static int getTypeMedal(int type){
@@ -98,30 +67,5 @@ public class ReputationLevelUtils {
         medal.setLayoutParams(param);
         medal.setImageResource(medalType);
         return medal;
-    }
-
-    private static View.OnClickListener onReputationClick(final Context context, final String reputationPoint, final View anchor){
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                View popup = View.inflate(context, R.layout.popup_reputation, null);
-                ImageView popupImage = (ImageView) popup.findViewById(R.id.popup_reputation_speech_bubble);
-                ImageViewExtKt.loadImage(popupImage, SPEECH_BUBBLE_IMAGE, com.tokopedia.kotlin.extensions.R.drawable.ic_loading_placeholder);
-                TextView point = (TextView)popup.findViewById(R.id.point);
-                point.setText(reputationPoint + " Poin");
-                final PopupWindow popWindow = new PopupWindow(popup, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
-                popWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                popWindow.setOutsideTouchable(true);
-                popWindow.setFocusable(false);
-                popWindow.showAsDropDown(v);
-                popWindow.setTouchInterceptor(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent motionEvent) {
-                        popWindow.dismiss();
-                        return true;
-                    }
-                });
-            }
-        };
     }
 }
