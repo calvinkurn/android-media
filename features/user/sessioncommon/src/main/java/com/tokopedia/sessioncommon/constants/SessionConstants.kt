59c4e61e2ee31c0fa30a651fb27c1df9754d67e8
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-03-08 17:55:43 +0700

    create constants file

---
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/constants/SessionConstants.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.sessioncommon.constants
+
+object SessionConstants {
+
+    object Rollout {
+        const val ROLLOUT_REGISTER_ENCRYPTION = "android_user_login_encryption"
+        const val ROLLOUT_LOGIN_ENCRYPTION = "enf_cred_and"
+    }
+
+    object FirebaseConfig {
+        const val CONFIG_REGISTER_ENCRYPTION = "android_user_register_encryption"
+        const val CONFIG_LOGIN_ENCRYPTION = "android_user_login_encryption"
+    }
+}
