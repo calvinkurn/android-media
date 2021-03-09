b7f3abbcf52c048bca203f9c20fa1b7dd703276c
faisalramdani <faisal.ramdani@tokopedia.com>
2021-03-09 17:52:40 +0700

    fix unittest

--- features/merchant/product_add_edit/src/test/java/com/tokopedia/product/addedit/detail/presentation/viewmodel/AddEditProductDetailViewModelTest.kt
+++ features/merchant/product_add_edit/src/test/java/com/tokopedia/product/addedit/detail/presentation/viewmodel/AddEditProductDetailViewModelTest.kt
@@ -11,6 +11,7 @@ import com.tokopedia.product.addedit.detail.domain.model.ProductValidateV3
 import com.tokopedia.product.addedit.detail.domain.model.ValidateProductResponse
 import com.tokopedia.product.addedit.detail.domain.usecase.GetCategoryRecommendationUseCase
 import com.tokopedia.product.addedit.detail.domain.usecase.GetNameRecommendationUseCase
+import com.tokopedia.product.addedit.detail.domain.usecase.PriceSuggestionSuggestedPriceGetUseCase
 import com.tokopedia.product.addedit.detail.domain.usecase.ValidateProductUseCase
 import com.tokopedia.product.addedit.detail.presentation.constant.AddEditProductDetailConstants
 import com.tokopedia.product.addedit.detail.presentation.constant.AddEditProductDetailConstants.Companion.MAX_PRODUCT_STOCK_LIMIT
@@ -63,6 +64,9 @@ class AddEditProductDetailViewModelTest {
     lateinit var annotationCategoryUseCase: AnnotationCategoryUseCase
 
     @RelaxedMockK
+    lateinit var productPriceSuggestionSuggestedPriceGetUseCase: PriceSuggestionSuggestedPriceGetUseCase
+
+    @RelaxedMockK
     lateinit var mIsInputValidObserver: Observer<Boolean>
 
     @get:Rule
@@ -113,7 +117,9 @@ class AddEditProductDetailViewModelTest {
     }
 
     private val viewModel: AddEditProductDetailViewModel by lazy {
-        AddEditProductDetailViewModel(provider, coroutineDispatcher, getNameRecommendationUseCase, getCategoryRecommendationUseCase, validateProductUseCase, getShopEtalaseUseCase, annotationCategoryUseCase)
+        AddEditProductDetailViewModel(provider, coroutineDispatcher, getNameRecommendationUseCase,
+                getCategoryRecommendationUseCase, validateProductUseCase, getShopEtalaseUseCase,
+                annotationCategoryUseCase, productPriceSuggestionSuggestedPriceGetUseCase)
     }
 
     @Test
