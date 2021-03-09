16fae1b83beb06e54b0823430e8ef0c7c5041bea
isfaaghyth <isfaaghyth@gmail.com>
2021-03-09 09:17:02 +0700

    added the timber log on service binding and aidl data receiver (callback)

--- features/communication/push-notification-common/src/main/java/com/tokopedia/notification/common/PushNotificationApi.kt
+++ features/communication/push-notification-common/src/main/java/com/tokopedia/notification/common/PushNotificationApi.kt
@@ -3,7 +3,10 @@ package com.tokopedia.notification.common
 import android.content.Context
 import android.os.Bundle
 import com.tokopedia.appaidl.AidlApi
+import com.tokopedia.notification.common.data.TIMBER_MAX_CHAR_LIMIT
+import com.tokopedia.notification.common.data.TIMBER_TAG
 import com.tokopedia.notification.common.service.PushNotificationService
+import timber.log.Timber
 
 open class PushNotificationApi(
         onAidlReceive: (tag: String, bundle: Bundle?) -> Unit,
@@ -16,7 +19,10 @@ open class PushNotificationApi(
                     context = context,
                     serviceName = PushNotificationService::class.java.name
             )
-        } catch (ignored: Exception) { }
+        } catch (ignored: Exception) {
+            Timber.w("${TIMBER_TAG}bind;reason='cannot_bind_service';data='${ignored.toString().
+            take(TIMBER_MAX_CHAR_LIMIT)}'")
+        }
     }
 
     companion object {
@@ -25,7 +31,10 @@ open class PushNotificationApi(
         @JvmStatic
         fun bindService(
                 context: Context,
-                onAidlReceive: (tag: String, bundle: Bundle?) -> Unit = { _, _ -> },
+                onAidlReceive: (tag: String, bundle: Bundle?) -> Unit = { _, _bundle ->
+                    Timber.w("${TIMBER_TAG}receive;reason='onAidlReceive';data='${_bundle.toString().
+                    take(TIMBER_MAX_CHAR_LIMIT)}'")
+                },
                 onAidlError: () -> Unit = {}
         ) {
             pushNotificationApi = PushNotificationApi(onAidlReceive, onAidlError)
---
+++ features/communication/push-notification-common/src/main/java/com/tokopedia/notification/common/data/constants.kt
@@ -0,0 +1,4 @@
+package com.tokopedia.notification.common.data
+
+const val TIMBER_TAG = "P2#AIDL#"
+const val TIMBER_MAX_CHAR_LIMIT = 1000
