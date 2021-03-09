f3818a908eda8fb7ad44d8d99ef85e427069ff27
faisalramdani <faisal.ramdani@tokopedia.com>
2021-03-09 18:32:07 +0700

    fix variant product condition

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/detail/presentation/fragment/AddEditProductDetailFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/detail/presentation/fragment/AddEditProductDetailFragment.kt
@@ -1458,7 +1458,7 @@ class AddEditProductDetailFragment : BaseDaggerFragment(),
     private fun subscribeToProductPriceRecommendation() {
         //observe only if (1) product has product id, (2) if seller app, (3) has no variant
         if (viewModel.productInputModel.productId == 0L || !GlobalConfig.isSellerApp()
-                || !viewModel.hasVariants) return
+                || viewModel.hasVariants) return
 
         viewModel.productPriceRecommendation.observe(viewLifecycleOwner) {
             if (it.suggestedPrice > 0.0) {
