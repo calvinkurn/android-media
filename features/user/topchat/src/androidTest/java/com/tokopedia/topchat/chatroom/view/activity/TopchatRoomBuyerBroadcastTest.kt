1d59003c2e7b1573852718b149c4c3b98ecfa97a
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-03-09 12:30:04 +0700

    add inst test show_broadcast_spam_handler_when_block_is_false_and_following_is_true

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomBuyerBroadcastTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomBuyerBroadcastTest.kt
@@ -3,15 +3,19 @@ package com.tokopedia.topchat.chatroom.view.activity
 import android.app.Activity
 import android.app.Instrumentation
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.tokopedia.topchat.R
+import com.tokopedia.topchat.assertion.atPositionIsInstanceOf
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
 import com.tokopedia.topchat.chatroom.view.activity.base.blockPromo
 import com.tokopedia.topchat.chatroom.view.activity.base.setFollowing
+import com.tokopedia.topchat.chatroom.view.uimodel.BroadCastUiModel
+import com.tokopedia.topchat.chatroom.view.viewmodel.BroadcastSpamHandlerUiModel
 import org.junit.Test
 
 class TopchatRoomBuyerBroadcastTest : TopchatRoomTest() {
@@ -32,9 +36,37 @@ class TopchatRoomBuyerBroadcastTest : TopchatRoomTest() {
         assertBroadcastSpamHandlerIsVisible()
     }
 
+    @Test
+    fun show_broadcast_spam_handler_when_block_is_false_and_following_is_true() {
+        // Given
+        setupChatRoomActivity()
+        getChatUseCase.response = firstPageChatBroadcastAsBuyer.blockPromo(false)
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        getShopFollowingUseCaseStub.response = getShopFollowingStatus.setFollowing(true)
+        Intents.intending(IntentMatchers.anyIntent()).respondWith(
+                Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+        )
+        inflateTestFragment()
+
+        // Then
+        assertBroadcastSpamHandlerIsHidden()
+    }
+
     private fun assertBroadcastSpamHandlerIsVisible() {
+        onView(withId(R.id.recycler_view)).check(
+                atPositionIsInstanceOf(0, BroadcastSpamHandlerUiModel::class.java)
+        )
         onView(withId(R.id.title_bc_handle)).check(matches(isDisplayed()))
         onView(withId(R.id.btn_stop_promo)).check(matches(isDisplayed()))
         onView(withId(R.id.btn_follow_shop)).check(matches(isDisplayed()))
+    }
+
+    private fun assertBroadcastSpamHandlerIsHidden() {
+        onView(withId(R.id.recycler_view)).check(
+                atPositionIsInstanceOf(0, BroadCastUiModel::class.java)
+        )
+        onView(withId(R.id.title_bc_handle)).check(doesNotExist())
+        onView(withId(R.id.btn_stop_promo)).check(doesNotExist())
+        onView(withId(R.id.btn_follow_shop)).check(doesNotExist())
     }
 }
