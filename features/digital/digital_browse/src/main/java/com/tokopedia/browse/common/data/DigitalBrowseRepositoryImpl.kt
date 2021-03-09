021a94047b2a1333aa38210c2c621047c6c074c9
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2021-03-09 11:24:50 +0700

    remove digital browse module

--- features/digital/digital_browse/src/main/java/com/tokopedia/browse/common/data/DigitalBrowseRepositoryImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.browse.common.data
-
-import com.tokopedia.browse.common.domain.DigitalBrowseRepository
-
-/**
- * @author by furqan on 30/08/18.
- */
-
-class DigitalBrowseRepositoryImpl : DigitalBrowseRepository
