eb798c0c4aca11f8aa1718eef4884763428047f1
Calvyn <calvyn.julian@tokopedia.com>
2021-03-10 18:52:51 +0700

    Add missing import

--- features/merchant/shop_page/src/test/java/com/tokopedia/shop/home/view/viewmodel/ShopHomeViewModelTest.kt
+++ features/merchant/shop_page/src/test/java/com/tokopedia/shop/home/view/viewmodel/ShopHomeViewModelTest.kt
@@ -11,6 +11,7 @@ import com.tokopedia.filter.common.data.DynamicFilterModel
 import com.tokopedia.mvcwidget.TokopointsCatalogMVCSummaryResponse
 import com.tokopedia.mvcwidget.usecases.MVCSummaryUseCase
 import com.tokopedia.localizationchooseaddress.domain.model.LocalCacheMode
+import com.tokopedia.localizationchooseaddress.domain.model.LocalCacheModel
 import com.tokopedia.play.widget.data.*
 import com.tokopedia.play.widget.domain.PlayWidgetUseCase
 import com.tokopedia.play.widget.ui.model.PlayWidgetConfigUiModel
