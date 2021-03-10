8f38f5af502791668569ef5ede39aa0a85041f42
kelvinsaputra <kelvin.saputra@tokopedia.com>
2021-03-10 13:59:36 +0700

    fix session expired when login email with wrong password

--- features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/domain/subscriber/LoginTokenSubscriber.kt
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/domain/subscriber/LoginTokenSubscriber.kt
@@ -34,16 +34,19 @@ class LoginTokenSubscriber(val userSession: UserSessionInterface,
             }
         } else if (shouldGoToActivationPage(pojo)) {
             onGoToActivationPage(MessageErrorException(pojo.loginToken.errors[0].message))
-        } else if (pojo.loginToken.popupError.header.isNotEmpty() &&
-                pojo.loginToken.popupError.body.isNotEmpty() &&
-                pojo.loginToken.popupError.action.isNotEmpty()) {
-            onShowPopupError(pojo)
-        } else if (pojo.loginToken.errors.isNotEmpty()) {
-            onErrorLoginToken(MessageErrorException(pojo.loginToken.errors[0].message))
-        } else if (errors.isNotEmpty()) {
-            onErrorLoginToken(MessageErrorException(errors[0].message))
         } else {
-            onErrorLoginToken(Throwable())
+            userSession.setToken(null, null, null)
+            if (pojo.loginToken.popupError.header.isNotEmpty() &&
+                    pojo.loginToken.popupError.body.isNotEmpty() &&
+                    pojo.loginToken.popupError.action.isNotEmpty()) {
+                onShowPopupError(pojo)
+            } else if (pojo.loginToken.errors.isNotEmpty()) {
+                onErrorLoginToken(MessageErrorException(pojo.loginToken.errors[0].message))
+            } else if (errors.isNotEmpty()) {
+                onErrorLoginToken(MessageErrorException(errors[0].message))
+            } else {
+                onErrorLoginToken(Throwable())
+            }
         }
     }
 
