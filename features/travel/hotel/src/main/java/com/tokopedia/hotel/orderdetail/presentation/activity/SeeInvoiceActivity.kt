2a3da3e813dcfcdc5223d266386eace2749fe5d1
misael-jonathan <misael.jonathan@tokopedia.com>
2021-03-11 23:59:48 +0700

    add SeeInvoiceActivity

---
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/orderdetail/presentation/activity/SeeInvoiceActivity.kt
@@ -0,0 +1,100 @@
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebView
+import androidx.core.content.ContextCompat
+import com.tokopedia.hotel.R
+import io.hansel.pebbletracesdk.presets.UIPresets.findViewById
+
+class SeeInvoiceActivity: BaseSimpleWebViewActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        initWebSettingsConfig()
+        val inflater = menuInflater
+        inflater.inflate(R.menu.hotel_invoice_menu, menu)
+        return true
+    }
+
+    private fun initWebSettingsConfig() {
+        val webView = findViewById<TkpdWebView>(R.id.webview)
+        webView.settings.run {
+            builtInZoomControls = true
+            displayZoomControls = false
+            useWideViewPort = true
+            loadWithOverviewMode = true
+        }
+        webView.setInitialScale(1)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun doWebPrint() {
+        val webView = WebView(this)
+        webView.settings.run {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            builtInZoomControls = true
+            displayZoomControls = true
+        }
+        val data = intent?.extras?.getString(KEY_URL, "defaultKey")
+        webView.loadUrl(data)
+        onPrintClicked(webView)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_download -> {
+                doWebPrint()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun onPrintClicked(webView: WebView?) {
+        webView?.let {
+            val printManager = ContextCompat.getSystemService(this, PrintManager::class.java)
+
+            var lastNoInvoice = ""
+            val invoiceRefNum = intent?.getStringExtra(INVOICE_REF_NUM) ?: ""
+            if (invoiceRefNum.isNotEmpty()) {
+                val splitInvoice = invoiceRefNum.split("/")
+                if (splitInvoice.isNotEmpty()) {
+                    lastNoInvoice = splitInvoice[splitInvoice.size - 1]
+                }
+            }
+            val jobName = "Invoice $lastNoInvoice"
+
+            val printAdapter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                it.createPrintDocumentAdapter(jobName)
+            } else {
+                it.createPrintDocumentAdapter()
+            }
+            val printAttr = PrintAttributes.Builder()
+                    .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
+                    .build()
+            try {
+                printManager?.print(jobName, printAdapter, printAttr)
+            } catch (e: Throwable) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        const val INVOICE_REF_NUM = "invoice_ref_num"
+        fun newInstance(context: Context, invoiceUrl: String, invoiceRefNum: String) =
+                Intent(context, SeeInvoiceActivity::class.java).apply {
+                    putExtra(KEY_URL, invoiceUrl)
+                    putExtra(INVOICE_REF_NUM, invoiceRefNum)
+                }
+    }
+} 
