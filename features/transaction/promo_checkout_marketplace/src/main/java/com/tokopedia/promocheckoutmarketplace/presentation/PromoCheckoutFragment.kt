9e0f39085bf40f6199d15d27df4e75401e3e88f6
Muhammad Furqan <furqan@tokopedia.com>
2021-03-11 15:36:42 +0700

    [Phoenix] Coachmark Initialization

--- features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/PromoCheckoutFragment.kt
+++ features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/PromoCheckoutFragment.kt
@@ -908,7 +908,7 @@ class PromoCheckoutFragment : BaseListFragment<Visitable<*>, PromoCheckoutAdapte
                             promoCoachMark.dismissCoachMark()
                             recyclerView.removeOnScrollListener(coachMarkRecyclerListener)
                         } else if (promoWithCoachMarkIndex != -1 && layoutManager.findLastVisibleItemPosition() == promoWithCoachMarkIndex &&
-                                promoCoachMark.isShowing) {
+                                ::promoCoachMark.isInitialized && promoCoachMark.isShowing) {
                             promoCoachMark.dismissCoachMark()
                             recyclerView.removeOnScrollListener(coachMarkRecyclerListener)
                         }
@@ -935,7 +935,7 @@ class PromoCheckoutFragment : BaseListFragment<Visitable<*>, PromoCheckoutAdapte
                         promoCoachMark = CoachMark2(it)
                         promoCoachMark.showCoachMark(coachMarkItem)
                         recyclerView.addOnScrollListener(coachMarkRecyclerListener)
-                        localCacheHandler.apply{
+                        localCacheHandler.apply {
                             putBoolean(KEY_PROMO_CHECKOUT_COACHMARK_IS_SHOWED, true)
                             applyEditor()
                         }
@@ -1018,7 +1018,8 @@ class PromoCheckoutFragment : BaseListFragment<Visitable<*>, PromoCheckoutAdapte
         renderStickyPromoHeader(recyclerView)
 
         // dismiss coachmark if user click promo with coachmark
-        if (promoWithCoachMarkIndex != -1 && adapter.list[promoWithCoachMarkIndex] == element && promoCoachMark.isShowing) {
+        if (promoWithCoachMarkIndex != -1 && adapter.list[promoWithCoachMarkIndex] == element &&
+                ::promoCoachMark.isInitialized && promoCoachMark.isShowing) {
             promoCoachMark.dismissCoachMark()
         }
     }
