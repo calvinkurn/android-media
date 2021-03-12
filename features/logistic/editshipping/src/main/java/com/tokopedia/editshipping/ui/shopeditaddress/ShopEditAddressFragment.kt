5518bb38886159bafca27fc95fca943c7038bf8a
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-03-12 10:45:49 +0700

    Fix code for requested changes PR

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shopeditaddress/ShopEditAddressFragment.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/shopeditaddress/ShopEditAddressFragment.kt
@@ -20,14 +20,12 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalLogistic
 import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.dialog.DialogUnify
 import com.tokopedia.editshipping.R
 import com.tokopedia.editshipping.di.shopeditaddress.DaggerShopEditAddressComponent
-import com.tokopedia.editshipping.di.shopeditaddress.ShopEditAddressComponent
 import com.tokopedia.editshipping.domain.model.shopeditaddress.ShopEditAddressState
 import com.tokopedia.editshipping.util.EditShippingConstant.DEFAULT_ERROR_MESSAGE
 import com.tokopedia.editshipping.util.EditShippingConstant.DEFAULT_LAT
@@ -37,6 +35,7 @@ import com.tokopedia.editshipping.util.EditShippingConstant.EXTRA_IS_FULL_FLOW
 import com.tokopedia.editshipping.util.EditShippingConstant.EXTRA_LAT
 import com.tokopedia.editshipping.util.EditShippingConstant.EXTRA_LONG
 import com.tokopedia.editshipping.util.EditShippingConstant.EXTRA_WAREHOUSE_DATA
+import com.tokopedia.editshipping.util.ShopEditAddressUtils
 import com.tokopedia.logisticCommon.data.entity.address.DistrictRecommendationAddress
 import com.tokopedia.logisticCommon.data.entity.address.SaveAddressDataModel
 import com.tokopedia.logisticCommon.data.entity.shoplocation.Warehouse
@@ -50,7 +49,6 @@ import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
 import timber.log.Timber
 import javax.inject.Inject
-import kotlin.math.min
 
 class ShopEditAddressFragment : BaseDaggerFragment(), OnMapReadyCallback {
 
@@ -304,9 +302,9 @@ class ShopEditAddressFragment : BaseDaggerFragment(), OnMapReadyCallback {
     }
 
     private fun checkValidateAddressDetail(addressHelper: String, userAddress: String) {
-        val normalizeAddressHelper = normalize(addressHelper)
-        val normalizeUserAddress = normalize(userAddress)
-        if (validatorAddress(normalizeAddressHelper, normalizeUserAddress)) {
+        val normalizeAddressHelper = ShopEditAddressUtils.normalize(addressHelper)
+        val normalizeUserAddress = ShopEditAddressUtils.normalize(userAddress)
+        if (ShopEditAddressUtils.validatorAddress(normalizeAddressHelper, normalizeUserAddress)) {
             validate = true
             helperShopDetail?.text = ""
         } else {
@@ -491,64 +489,6 @@ class ShopEditAddressFragment : BaseDaggerFragment(), OnMapReadyCallback {
         intent.putExtra(EXTRA_WAREHOUSE_DATA, warehouseDataModel)
         intent.putExtra(EXTRA_IS_EDIT_WAREHOUSE, true)
         startActivityForResult(intent, OPEN_MAP_REQUEST_CODE)
-    }
-
-    private fun normalize(address: String): String {
-        var newAddress = address.toLowerCase()
-        newAddress = Regex("[^\\w\\s&]").replace(newAddress, " ")
-        val regex = """(jalan|jln|jl|blok|kavling|kav|nomor|no|nmr|kecamatan|kec|kabupaten|kab|kota|unknown|unnamed|location|[0-9])""".toRegex()
-        newAddress = regex.replace(newAddress, " ")
-        newAddress = Regex("\\r?\\n|\\r").replace(newAddress, " ")
-        newAddress = Regex("  +").replace(newAddress, " ")
-        newAddress = newAddress.trim()
-        return newAddress
-    }
-
-    private fun validatorAddress(addr1: String, addr2: String): Boolean {
-        val matchWord = levenshteinDistance(addr1, addr2)
-        val minWordLen = minLenSentence(addr1, addr2)
-        val verboseTest = matchWord.toDouble()/minWordLen.toDouble()
-        return verboseTest <= 0.4
-    }
-
-    private fun levenshteinDistance(addr1: String, addr2: String): Int {
-        val _addr1 = addr1.split(' ')
-        val _addr2 = addr2.split(' ')
-
-        val lhsLength = _addr1.size
-        val rhsLength = _addr2.size
-
-        var cost = Array(lhsLength) { it }
-        var newCost = Array(lhsLength) { 0 }
-
-        for (i in 1..rhsLength-1) {
-            newCost[0] = i
-
-            for (j in 1..lhsLength-1) {
-                val match = if(addr1[j - 1] == addr2[i - 1]) 0 else 1
-
-                val costReplace = cost[j - 1] + match
-                val costInsert = cost[j] + 1
-                val costDelete = newCost[j - 1] + 1
-
-                newCost[j] = min(min(costInsert, costDelete), costReplace)
-            }
-
-            val swap = cost
-            cost = newCost
-            newCost = swap
-        }
-
-        return cost[lhsLength - 1]
-    }
-
-    private fun minLenSentence(addr1: String, addr2: String): Int {
-        val _addr1 = addr1.split(' ')
-        val _addr2 = addr2.split(' ')
-
-        return if (_addr1.size <= _addr2.size) {
-            _addr1.size
-        } else _addr2.size
     }
 
     companion object {
---
+++ features/logistic/editshipping/src/test/java/com/tokopedia/editshipping/util/ShopEditAddressUtilsTest.kt
@@ -0,0 +1,42 @@
+package com.tokopedia.editshipping.util
+
+import org.junit.Assert
+import org.junit.Test
+
+class ShopEditAddressUtilsTest {
+
+    @Test
+    fun `normalize String Success`() {
+        val address = "Jln. Dr. Satrio no, 364. Jakarta Selatan"
+
+        val expectedResult = "dr satrio jakarta selatan"
+
+        val actualResult = ShopEditAddressUtils.normalize(address)
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Validator Address return true`() {
+        val address1 = "Jln. Dr. Satrio no, 364. Jakarta Selatan"
+        val address2 = "Jalan Dr. Satrio, nomer 355, Setiabudi, Jakarta Selatan"
+
+        val expectedResult = true
+
+        val actualResult = ShopEditAddressUtils.validatorAddress(address1, address2)
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun `Validator Address return false`() {
+        val address1 = "Jln. Dr. Satrio no, 364. Jakarta Selatan"
+        val address2 = "Jalan Karet Kuningan, Setiabudi, jakarta Selatan"
+
+        val expectedResult = false
+
+        val actualResult = ShopEditAddressUtils.validatorAddress(address1, address2)
+
+        Assert.assertEquals(expectedResult, actualResult)
+    }
+}
