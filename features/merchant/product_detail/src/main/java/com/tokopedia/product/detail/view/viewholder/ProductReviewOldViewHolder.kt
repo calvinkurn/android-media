f2f836a11657e1a455517b28e3c2fc5ba826cbe1
yehezkiel <yehezkiel@tokopedia.com>
2021-03-12 11:39:50 +0700

    remove unused hardcoded color change image review slider to static black

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductReviewOldViewHolder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductReviewOldViewHolder.kt
@@ -103,7 +103,7 @@ class ProductReviewOldViewHolder(val view: View, val listener: DynamicProductDet
                 txt_date_user_pdp.text = MethodChecker.fromHtml(
                         view.context.getString(R.string.date_review_pattern, reviewData.reviewCreateTime, "<b>" + reviewData.user.fullName + "</b>"))
                 txt_desc_review_pdp.maxLines = 4
-                val formattingResult = ProductDetailUtil.reviewDescFormatterOld(reviewData.message)
+                val formattingResult = ProductDetailUtil.reviewDescFormatterOld(context, reviewData.message)
                 txt_desc_review_pdp.text = formattingResult.first
 
                 if(formattingResult.second) {
