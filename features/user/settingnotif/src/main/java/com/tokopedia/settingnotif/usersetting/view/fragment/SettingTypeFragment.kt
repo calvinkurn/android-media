a37ab2097313ac43d238c25a727881a46a6392dc
isfaaghyth <isfaaghyth@gmail.com>
2021-03-16 00:31:33 +0700

    convert dark mode of settingnotif module

--- features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/SettingTypeFragment.kt
+++ features/user/settingnotif/src/main/java/com/tokopedia/settingnotif/usersetting/view/fragment/SettingTypeFragment.kt
@@ -2,15 +2,16 @@ package com.tokopedia.settingnotif.usersetting.view.fragment
 
 import android.content.Context
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.settingnotif.R
 import com.tokopedia.settingnotif.usersetting.const.Unify.Unify_N0
+import com.tokopedia.settingnotif.usersetting.const.Unify.Unify_N0_68
 import com.tokopedia.settingnotif.usersetting.const.Unify.Unify_N700_96
 import com.tokopedia.settingnotif.usersetting.view.activity.UserNotificationSettingActivity
 import com.tokopedia.settingnotif.usersetting.view.adapter.SettingTypeAdapter
@@ -94,7 +95,7 @@ class SettingTypeFragment : BaseDaggerFragment() {
     private fun generateShowCaseDialog(): ShowCaseDialog {
         return ShowCaseBuilder()
                 .backgroundContentColorRes(Unify_N700_96)
-                .shadowColorRes(R.color.settingnotif_shadow)
+                .shadowColorRes(Unify_N0_68)
                 .textColorRes(Unify_N0)
                 .textSizeRes(com.tokopedia.unifyprinciples.R.dimen.fontSize_lvl2)
                 .titleTextSizeRes(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl4)
