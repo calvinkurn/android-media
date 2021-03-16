190fdb86b617748d4717b6ea4f435c379ba2f467
faisalramdani <faisal.ramdani@tokopedia.com>
2021-03-16 15:37:59 +0700

    create edit product journey and cassava validation

--- features/merchant/product_add_edit/src/androidTest/java/com/tokopedia/product/addedit/instrumenttest/AddEditProductPreviewAnalyticTest.kt
+++ features/merchant/product_add_edit/src/androidTest/java/com/tokopedia/product/addedit/instrumenttest/AddEditProductPreviewAnalyticTest.kt
@@ -3,12 +3,15 @@ package com.tokopedia.product.addedit.instrumenttest
 import android.content.Intent
 import android.net.Uri
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.applink.internal.ApplinkConstInternalMechant
+import com.tokopedia.cassavatest.getAnalyticsWithQuery
+import com.tokopedia.cassavatest.hasAllSuccess
 import com.tokopedia.config.GlobalConfig
 import com.tokopedia.product.addedit.R
 import com.tokopedia.product.addedit.mock.AddEditProductPreviewMockResponseConfig
@@ -19,6 +22,8 @@ import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.test.application.util.TokopediaGraphqlInstrumentationTestHelper
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import com.tokopedia.trackingoptimizer.constant.Constant
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -53,17 +58,68 @@ class AddEditProductPreviewAnalyticTest {
     }
 
     @Test
-    fun testLayout() {
+    fun testEditProductJourney() {
+        testEditDetail()
+        testEditDescription()
+        testEditShipment()
         Espresso.onView(CommonMatcher
-                .firstView(ViewMatchers.withId(R.id.tv_start_add_edit_product_detail)))
-                .perform(ViewActions.click())
+                .firstView(withId(R.id.tv_done)))
+                .perform(click())
 
-        Espresso.onView(CommonMatcher
-                .firstView(ViewMatchers.withId(R.id.tv_category_picker_button)))
-                .perform(ViewActions.click())
+        //stepper
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_preview_page_click_change_detail.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_preview_page_click_change_description.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_preview_page_click_change_shipment.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_preview_page_click_finish.json")
+        //detail
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_detail_click_choose_categories.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_detail_click_wholesale_toggle.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_detail_click_preorder_toggle.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_detail_page_click_continue.json")
+        //description
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_description_page_click_change_variant.json")
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_description_page_click_continue.json")
+        //shipment
+        doAnalyticDebuggerTest("tracker/merchant/product_add_edit/edit/product_shipping_page_click_insurance_toggle.json")
 
-        Thread.sleep(5000)
         activityRule.activity.finish()
+    }
+
+    private fun testEditShipment() {
+        performScrollAndClick(R.id.tv_start_add_edit_product_shipment)
+        performScrollAndClick(R.id.radio_optional_insurance)
+        performScrollAndClick(R.id.radio_required_insurance)
+        performScrollAndClick(R.id.btn_save)
+    }
+
+    private fun testEditDetail() {
+        performScrollAndClick(R.id.tv_start_add_edit_product_detail)
+        performScrollAndClick(R.id.tv_category_picker_button)
+        pressBack()
+        performScrollAndClick(R.id.su_wholesale)
+        performScrollAndClick(R.id.switch_preorder)
+        performReplaceText(R.id.tfu_duration, "2")
+        performScrollAndClick(R.id.btn_submit)
+    }
+
+    private fun testEditDescription() {
+        performScrollAndClick(R.id.tv_start_add_edit_product_description)
+        performScrollAndClick(R.id.tvAddVariant)
+        pressBack()
+        performDialogSecondaryClick()
+        performScrollAndClick(R.id.btnSave)
+    }
+
+    private fun performDialogSecondaryClick() {
+        Espresso.onView(CommonMatcher
+                .firstView(withId(R.id.dialog_btn_secondary)))
+                .perform(click())
+    }
+
+    private fun performDialogPrimaryClick() {
+        Espresso.onView(CommonMatcher
+                .firstView(withId(R.id.dialog_btn_primary)))
+                .perform(click())
     }
 
     private fun createIntentEditProduct(): Intent {
@@ -76,5 +132,35 @@ class AddEditProductPreviewAnalyticTest {
         return Intent(InstrumentationRegistry.getInstrumentation().targetContext, AddEditProductPreviewActivity::class.java).also {
             it.data = applink
         }
+    }
+
+    private fun performScrollAndClick(id: Int) {
+        Espresso.onView(CommonMatcher
+                .firstView(withId(id)))
+                .perform(scrollTo())
+
+        Espresso.onView(CommonMatcher
+                .firstView(withId(id)))
+                .perform(click())
+    }
+
+    private fun performReplaceText(id: Int, text: String) {
+        Espresso.onView(CommonMatcher
+                .firstView(withId(id)))
+                .perform(scrollTo())
+
+        Espresso.onView(Matchers.allOf(withId(R.id.text_field_input), ViewMatchers.isDescendantOfA(withId(id))))
+                .perform(typeText(text), closeSoftKeyboard())
+    }
+
+    private fun pressBack() {
+        Espresso.pressBackUnconditionally()
+    }
+
+    private fun doAnalyticDebuggerTest(fileName: String) {
+        MatcherAssert.assertThat(
+                getAnalyticsWithQuery(gtmLogDBSource, context, fileName),
+                hasAllSuccess()
+        )
     }
 }
