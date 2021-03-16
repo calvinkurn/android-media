4ebde30e1e630bc619e0a0c3393d073c7b4d2840
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-03-16 15:22:18 +0700

    add divider to recommendation tab

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/HomeRevampRecyclerDecoration.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/HomeRevampRecyclerDecoration.kt
@@ -1,8 +1,8 @@
 package com.tokopedia.home.beranda.presentation.view.adapter.itemdecoration
 
 import android.graphics.Rect
-import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeRevampRecyclerDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
 
