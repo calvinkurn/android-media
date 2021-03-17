3e8c4a068d9c4ff23d09ae95030335b858ce43ad
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2021-03-17 11:45:36 +0800

    implement rollance

--- features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/addname/AddNameRegisterPhoneActivity.kt
+++ features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/addname/AddNameRegisterPhoneActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.play.core.splitcompat.SplitCompat
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.profilecompletion.addname.fragment.AddNameRegisterPhoneCleanViewFragment
 import com.tokopedia.profilecompletion.addname.fragment.AddNameRegisterPhoneFragment
+import com.tokopedia.remoteconfig.RemoteConfigInstance
 
 /**
  * @author by nisie on 22/04/19.
@@ -18,6 +19,8 @@ import com.tokopedia.profilecompletion.addname.fragment.AddNameRegisterPhoneFrag
  * {@link ApplinkConstInternalGlobal.PARAM_UUID}
  */
 class AddNameRegisterPhoneActivity : BaseSimpleActivity() {
+
+    private var rollanceType = ""
 
     override fun getNewFragment(): Fragment {
         val bundle = Bundle()
@@ -32,9 +35,14 @@ class AddNameRegisterPhoneActivity : BaseSimpleActivity() {
         SplitCompat.installActivity(this)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        rollanceType = RemoteConfigInstance.getInstance().abTestPlatform.getString(ROLLANCE_KEY, ROLLANCE_DEFAULT_VALUE)
+        super.onCreate(savedInstanceState)
+    }
+
     private fun checkUri(bundle: Bundle): Fragment {
         val uri = intent?.data
-        return if (uri?.lastPathSegment?.contains(PATH_CLEAN_VIEW) == true) {
+        return if (uri?.lastPathSegment?.contains(PATH_CLEAN_VIEW) == true && !rollanceType.contentEquals(ROLLANCE_DEFAULT_VALUE)) {
             AddNameRegisterPhoneCleanViewFragment.createInstance(bundle)
         } else {
             AddNameRegisterPhoneFragment.createInstance(bundle)
@@ -42,6 +50,8 @@ class AddNameRegisterPhoneActivity : BaseSimpleActivity() {
     }
 
     companion object {
+        private const val ROLLANCE_KEY = "android_defername"
+        private const val ROLLANCE_DEFAULT_VALUE = ""
         private const val PATH_CLEAN_VIEW = "clean-view"
     }
 }
