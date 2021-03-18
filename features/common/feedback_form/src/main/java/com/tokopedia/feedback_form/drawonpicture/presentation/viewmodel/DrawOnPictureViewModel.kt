7e0050990a21675c46f4c9a2154c8719979eb0f1
rezagama <reza.hidayat@tokopedia.com>
2021-03-18 14:35:17 +0700

    [Feedback Form] Refactor coroutine dispatchers
    - Add unitTest lib
    - Use dispatchers from abstraction & unit test lib
    - Remove duplicate dispatchers

--- features/common/feedback_form/src/main/java/com/tokopedia/feedback_form/drawonpicture/presentation/viewmodel/DrawOnPictureViewModel.kt
+++ features/common/feedback_form/src/main/java/com/tokopedia/feedback_form/drawonpicture/presentation/viewmodel/DrawOnPictureViewModel.kt
@@ -3,7 +3,7 @@ package com.tokopedia.feedback_form.drawonpicture.presentation.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
-import com.tokopedia.feedback_form.drawonpicture.DispatcherProvider
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -11,8 +11,8 @@ import javax.inject.Inject
 /**
  * @author by furqan on 01/10/2020
  */
-class DrawOnPictureViewModel @Inject constructor(private val dispatcherProvider: DispatcherProvider)
-    : BaseViewModel(dispatcherProvider.io()) {
+class DrawOnPictureViewModel @Inject constructor(private val dispatcherProvider: CoroutineDispatchers)
+    : BaseViewModel(dispatcherProvider.io) {
 
     private val mutableShowPencilOptions = MutableLiveData<Boolean>()
     val showPencilOptions: LiveData<Boolean>
@@ -22,7 +22,7 @@ class DrawOnPictureViewModel @Inject constructor(private val dispatcherProvider:
 
     fun startDrawing() {
         isOnDrawProcess = true
-        launch(dispatcherProvider.ui()) {
+        launch(dispatcherProvider.main) {
             delay(1000)
             if (isOnDrawProcess) mutableShowPencilOptions.postValue(false)
         }
