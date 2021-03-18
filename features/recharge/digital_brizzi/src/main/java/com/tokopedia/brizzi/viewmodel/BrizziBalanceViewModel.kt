26040afffdac8f2dca9360669db99de8398d2769
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-03-18 11:49:01 +0700

    add balance

--- features/recharge/digital_brizzi/src/main/java/com/tokopedia/brizzi/viewmodel/BrizziBalanceViewModel.kt
+++ features/recharge/digital_brizzi/src/main/java/com/tokopedia/brizzi/viewmodel/BrizziBalanceViewModel.kt
@@ -80,7 +80,7 @@ class BrizziBalanceViewModel @Inject constructor(private val graphqlRepository: 
 
                         balanceInquiry.attributesEmoneyInquiry?.let { attributes ->
                             if (attributes.pendingBalance == 0) {
-                                Timber.d("P2#BRIZZI#SUCCESS_GET_BALANCE#${it.cardNumber}")
+                                Timber.d("P2#BRIZZI#SUCCESS_GET_BALANCE#${it.cardNumber}, ${attributes.lastBalance.toDouble()}")
                                 emoneyInquiry.postValue(balanceInquiry)
                             } else {
                                 writeBalanceToCard(intent, rawLogBrizzi, brizziInstance)
@@ -150,7 +150,7 @@ class BrizziBalanceViewModel @Inject constructor(private val graphqlRepository: 
 
                 if (inquiryIdBrizzi > -1) {
                     balanceInquiry.attributesEmoneyInquiry?.let {
-                        Timber.d("P2#BRIZZI#SUCCESS_GET_BALANCE#${it.cardNumber}")
+                        Timber.d("P2#BRIZZI#SUCCESS_GET_BALANCE#${it.cardNumber}, ${it.lastBalance.toDouble()}")
                         logBrizzi(inquiryIdBrizzi, it.cardNumber, logRawQuery, "success", it.lastBalance.toDouble())
                     }
                 }
