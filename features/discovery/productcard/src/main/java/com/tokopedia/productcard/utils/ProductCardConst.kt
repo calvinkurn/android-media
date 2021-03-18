2c061d02d9a79a992702a1bea55a9fd92ec861a2
glenntuyu <christian.tuyu@tokopedia.com>
2021-03-18 17:10:16 +0700

    improve dark mode coverage in search and productcard

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/ProductCardConst.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/ProductCardConst.kt
@@ -12,9 +12,6 @@ internal const val DARK_RED = "darkRed"
 internal const val DARK_ORANGE = "darkOrange"
 internal const val TRANSPARENT_BLACK = "transparentBlack"
 
-internal const val COLOR_LABEL_DEFAULT = "#FFFFFF"
-internal const val COLOR_LABEL_TRANSPARENT_BLACK = "#AD31353B"
-
 internal const val LABEL_PRODUCT_STATUS = "status"
 internal const val LABEL_PRICE = "price"
 internal const val LABEL_GIMMICK = "gimmick"
@@ -27,11 +24,6 @@ internal const val TEXT_DARK_ORANGE = "textDarkOrange"
 internal const val TEXT_DARK_RED = "textDarkRed"
 internal const val TEXT_DARK_GREY = "textDarkGrey"
 internal const val TEXT_LIGHT_GREY = "textLightGrey"
-
-internal const val COLOR_TEXT_DARK_ORANGE = "#FF8B00"
-internal const val COLOR_TEXT_DARK_RED = "#EF144A"
-internal const val COLOR_TEXT_DARK_GREY = "#AD31353B"
-internal const val COLOR_TEXT_LIGHT_GREY = "#7031353B"
 
 internal const val ROBOTO_BOLD = "RobotoBold.ttf"
 internal const val ROBOTO_REGULAR = "RobotoRegular.ttf"
