2b878786f1337090afa7f2d91eb251d9e5610b35
isfaaghyth <isfaaghyth@gmail.com>
2021-03-22 22:57:47 +0700

    move mediauploader from libraries into features

--- libraries/mediauploader/src/main/java/com/tokopedia/mediauploader/data/state/UploadResult.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.mediauploader.data.state
-
-sealed class UploadResult {
-    class Success(val uploadId: String): UploadResult()
-    class Error(val message: String): UploadResult()
-}
