21b769c960ee122a601b550c1383d7263ea202f7
saidfaisal <said.faisal@tokopedia.com>
2021-03-22 17:31:14 +0700

    Revert "test attach shop note on shop page"
    This reverts commit 39b48e77d5a8741ea81a7e7194439cbb6938339c.

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/notes/view/activity/ShopSettingsNoteBuyerViewActivity.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/notes/view/activity/ShopSettingsNoteBuyerViewActivity.kt
@@ -1,7 +1,5 @@
 package com.tokopedia.shop.settings.notes.view.activity
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.app.BaseMainApplication
@@ -14,25 +12,12 @@ import com.tokopedia.shop.settings.notes.view.fragment.ShopSettingsNoteBuyerView
 
 class ShopSettingsNoteBuyerViewActivity : BaseSimpleActivity(), HasComponent<ShopSettingsComponent> {
 
-    companion object {
-        const val SHOP_ID = "EXTRA_SHOP_ID"
-
-        @JvmStatic
-        fun createIntent(context: Context, shopId: String) = Intent(context, ShopSettingsNoteBuyerViewActivity::class.java)
-                .apply {
-                    putExtra(SHOP_ID, shopId)
-                }
-    }
-
-    private var shopId: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_settings_note_buyer_view)
-        shopId = intent.getStringExtra(SHOP_ID)
     }
 
-    override fun getNewFragment(): Fragment = ShopSettingsNoteBuyerViewFragment.createInstance(shopId)
+    override fun getNewFragment(): Fragment = ShopSettingsNoteBuyerViewFragment.createInstance()
 
     override fun getComponent(): ShopSettingsComponent = DaggerShopSettingsComponent.builder()
                                                             .baseAppComponent((application as BaseMainApplication).baseAppComponent)
