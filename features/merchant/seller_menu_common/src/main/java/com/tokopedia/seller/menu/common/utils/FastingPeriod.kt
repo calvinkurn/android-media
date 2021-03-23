25a83ca0dc4978e5d33340b75c7cac7f22de108d
Reivin <reivin.oktavianus@tokopedia.com>
2021-03-23 11:02:15 +0700

    Add ECS shop status ramadhan thematic URLs

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
@@ -3,8 +3,13 @@ package com.tokopedia.seller.menu.common.utils
 enum class FastingPeriod(val regularMerchantIllustrationUrl: String,
                          val powerMerchantIllustrationUrl: String,
                          val officialStoreIllustrationUrl: String) {
-    // TODO: Set illustration URLs
-    SUHOOR("", "", ""),
-    FASTING("", "", ""),
-    IFTAR("", "", "")
+    SUHOOR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_rm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_pm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_os.png"),
+    FASTING("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_rm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_pm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_os.png"),
+    IFTAR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_rm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_pm.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_os.png")
 }
