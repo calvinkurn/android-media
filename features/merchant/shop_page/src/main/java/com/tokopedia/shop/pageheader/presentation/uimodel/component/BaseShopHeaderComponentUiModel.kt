3a31078741d1e2ad8491952b61b80fc491c1a701
saidfaisal <said.faisal@tokopedia.com>
2021-03-23 12:47:42 +0700

    change component name for shop notes

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/uimodel/component/BaseShopHeaderComponentUiModel.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/uimodel/component/BaseShopHeaderComponentUiModel.kt
@@ -2,7 +2,6 @@ package com.tokopedia.shop.pageheader.presentation.uimodel.component
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
-import com.tokopedia.topads.sdk.domain.model.ImpressHolder
 
 interface BaseShopHeaderComponentUiModel : Visitable<BaseAdapterTypeFactory> {
 
@@ -15,7 +14,7 @@ interface BaseShopHeaderComponentUiModel : Visitable<BaseAdapterTypeFactory> {
     object ComponentName {
         const val SHOP_LOGO = "shop_logo"
         const val SHOP_NAME = "shop_name"
-        const val BUTTON_SHOP_NOTES = "catatan_toko"
+        const val BUTTON_SHOP_NOTES = "shop_notes"
         const val BUTTON_CHAT = "chat"
         const val BUTTON_FOLLOW = "follow_member"
         const val BUTTON_PLAY = "play"
