8661a5677e8bba9452baf300e0da6f9b6e940668
Reivin <reivin.oktavianus@tokopedia.com>
2021-03-23 18:48:24 +0700

    Refactor period of fasting

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriodUtil.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriodUtil.kt
@@ -7,9 +7,11 @@ import java.util.*
 
 object FastingPeriodUtil {
 
-    private const val SUHOOR_START_TIME = 0
+    private const val SUHOOR_START_TIME = 2
     private const val FASTING_START_TIME = 5
-    private const val IFTAR_START_TIME = 17
+    private const val IFTAR_START_TIME = 18
+    private const val MINUTE_HALF_HOUR = 30
+    private const val TWO_AM = 2
 
     /**
      * Get Ramadhan thematic illustration url according to its shop type and current hour of day
@@ -26,11 +28,30 @@ object FastingPeriodUtil {
         }
     }
 
+    /**
+     * Suhoor : 2.30AM - 5PM
+     * Fasting : 5AM - 6PM
+     * Iftar : 6PM - 2.30AM
+     */
     private fun getFastingPeriod(): FastingPeriod {
-        return when(Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
-            in SUHOOR_START_TIME until FASTING_START_TIME -> FastingPeriod.SUHOOR
-            in FASTING_START_TIME until IFTAR_START_TIME -> FastingPeriod.FASTING
-            else -> FastingPeriod.IFTAR
+        Calendar.getInstance().run {
+            return when(val hour = get(Calendar.HOUR_OF_DAY)) {
+                in SUHOOR_START_TIME until FASTING_START_TIME -> {
+                    if (hour == TWO_AM) {
+                        get(Calendar.MINUTE).let { minute ->
+                            if (minute < MINUTE_HALF_HOUR) {
+                                FastingPeriod.IFTAR
+                            } else {
+                                FastingPeriod.SUHOOR
+                            }
+                        }
+                    } else {
+                        FastingPeriod.SUHOOR
+                    }
+                }
+                in FASTING_START_TIME until IFTAR_START_TIME -> FastingPeriod.FASTING
+                else -> FastingPeriod.IFTAR
+            }
         }
     }
 
