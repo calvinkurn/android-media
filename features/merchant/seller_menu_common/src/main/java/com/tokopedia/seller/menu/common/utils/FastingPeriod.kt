7341801a066b91d4698af49ad367ee4abe3eb6a9
Reivin <reivin.oktavianus@tokopedia.com>
2021-03-23 18:34:38 +0700

    Update ramadhan illustration with more resolution

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
@@ -7,13 +7,13 @@ package com.tokopedia.seller.menu.common.utils
 enum class FastingPeriod(val regularMerchantIllustrationUrl: String,
                          val powerMerchantIllustrationUrl: String,
                          val officialStoreIllustrationUrl: String) {
-    SUHOOR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_rm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_os.png"),
-    FASTING("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_rm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_os.png"),
-    IFTAR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_rm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_os.png")
+    SUHOOR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_rm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_pm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_os_2x.png"),
+    FASTING("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_rm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_pm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_os_2x.png"),
+    IFTAR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_rm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_pm_2x.png",
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_os_2x.png")
 }
