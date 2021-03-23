71fb8a53b315536cddd50a36a1b37991a0a6346e
Reivin <reivin.oktavianus@tokopedia.com>
2021-03-23 12:46:45 +0700

    Fix wrong illustration shown

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/utils/FastingPeriod.kt
@@ -1,15 +1,19 @@
 package com.tokopedia.seller.menu.common.utils
 
+/**
+ * Figma: https://www.figma.com/file/5uH3Pi5GbB0jmx7uo3IZJh/UI---Seller-App-Ramadhan?node-id=90%3A4
+ * Note: Illustration links for OS are not quite right with their file naming, but essentially the illustrations are correct
+ */
 enum class FastingPeriod(val regularMerchantIllustrationUrl: String,
                          val powerMerchantIllustrationUrl: String,
                          val officialStoreIllustrationUrl: String) {
     SUHOOR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_rm.png",
             "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_os.png"),
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_os.png"),
     FASTING("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_rm.png",
             "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_puasa_os.png"),
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_os.png"),
     IFTAR("https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_rm.png",
             "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_pm.png",
-            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_buka_os.png")
+            "https://images.tokopedia.net/img/android/seller_home/ic_ramadhan_sahur_os.png")
 }
