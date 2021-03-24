c4e186889987cb56e8eb4e3ea05ffae0df69e2b4
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-03-24 12:58:01 +0700

    Revert: Cleaning code (3d7522de)

---
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/RECOMMENDATION_TYPE.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.recommendation_widget_common
+
+const val TYPE_SCROLL = "Custom-vertical"
+const val TYPE_CUSTOM_HORIZONTAL = "Custom-horizontal"
+const val TYPE_CAROUSEL = "Carousel"
+const val TYPE_INFINITE = "Infinite"
