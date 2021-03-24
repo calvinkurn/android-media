d1c5e6721dd01f1500b0ff750880ebc756eb72d6
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-03-24 23:18:45 +0700

    unused import

--- features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/ItemDetailPerformanceViewHolder.kt
+++ features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/ItemDetailPerformanceViewHolder.kt
@@ -4,9 +4,6 @@ import android.graphics.Color
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.kotlin.extensions.view.hide
-import com.tokopedia.kotlin.extensions.view.setMargin
-import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.shop.score.R
 import com.tokopedia.shop.score.performance.presentation.adapter.ItemShopPerformanceListener
