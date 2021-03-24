2f9048991d5997d42fa859d43fe52cb73aafd24b
irfan khoirul <irfankhoirul@gmail.com>
2021-03-24 15:20:21 +0700

    convert checkout insurance tnc to kotlin

---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/uimodel/ShipmentInsuranceTncModel.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.checkout.view.uimodel
+
+import com.tokopedia.logisticcart.shipping.model.ShipmentData
+
+data class ShipmentInsuranceTncModel (
+    var isVisible: Boolean = false
+) : ShipmentData
