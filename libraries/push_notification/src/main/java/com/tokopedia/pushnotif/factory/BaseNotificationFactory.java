ed30b8200058e9def5f481329a379defb74a7a0e
Reivin <reivin.oktavianus@tokopedia.com>
2021-03-24 18:21:14 +0700

    Change sellerapp push notif large icon
    Also delete old icons

--- libraries/push_notification/src/main/java/com/tokopedia/pushnotif/factory/BaseNotificationFactory.java
+++ libraries/push_notification/src/main/java/com/tokopedia/pushnotif/factory/BaseNotificationFactory.java
@@ -89,7 +89,7 @@ public abstract class BaseNotificationFactory {
 
     protected int getDrawableLargeIcon() {
         if (GlobalConfig.isSellerApp()) {
-            return R.mipmap.ic_big_notif_seller;
+            return com.tokopedia.resources.common.R.mipmap.ic_launcher_sellerapp_ramadhan;
         } else {
             return com.tokopedia.resources.common.R.mipmap.ic_launcher_customerapp;
         }
