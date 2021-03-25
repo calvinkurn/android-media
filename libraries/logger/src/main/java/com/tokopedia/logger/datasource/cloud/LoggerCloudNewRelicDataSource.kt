89c4029b54433bebe566e040af7a3840edb5b8d1
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-03-25 19:41:05 +0700

    fix new relic

--- libraries/logger/src/main/java/com/tokopedia/logger/datasource/cloud/LoggerCloudNewRelicDataSource.kt
+++ libraries/logger/src/main/java/com/tokopedia/logger/datasource/cloud/LoggerCloudNewRelicDataSource.kt
@@ -3,22 +3,24 @@ package com.tokopedia.logger.datasource.cloud
 import com.google.gson.Gson
 import com.tokopedia.keys.Keys
 import com.tokopedia.logger.utils.Constants
+import com.tokopedia.logger.utils.Constants.Companion.HEADER_CONTENT_ENCODING
+import com.tokopedia.logger.utils.Constants.Companion.HEADER_CONTENT_ENCODING_GZIP
+import com.tokopedia.logger.utils.Constants.Companion.HEADER_CONTENT_LENGTH
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
+import java.io.DataOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.zip.GZIPOutputStream
 
 class LoggerCloudNewRelicDataSource : LoggerCloudNewRelicImpl {
 
-    private val gson = Gson()
-
     override suspend fun sendToLogServer(message: List<String>): Boolean {
         var errCode = Constants.LOG_DEFAULT_ERROR_CODE
         withContext(Dispatchers.IO) {
             try {
-                errCode = openURL(message)
+                errCode = openURL(message.firstOrNull())
             } catch (e: Exception) {
                 e.printStackTrace()
             }
@@ -26,22 +28,20 @@ class LoggerCloudNewRelicDataSource : LoggerCloudNewRelicImpl {
         return errCode == Constants.RESPONSE_SUCCESS_CODE
     }
 
-    private fun openURL(message: List<String>): Int {
+    private fun openURL(message: String?): Int {
         var urlConnection: HttpURLConnection? = null
         val url: URL
 
         var responseCode = Constants.LOG_DEFAULT_ERROR_CODE
 
         try {
-            val requestBody = compressRequestBody(message)
             url = URL(Constants.getNewRelicEventURL(Keys.AUTH_NEW_RELIC_USER_ID))
             urlConnection = url.openConnection() as HttpURLConnection
-            urlConnection.setRequestProperty(requestBody.size)
-            urlConnection.outputStream.use {
-                it.write(requestBody)
-                it.flush()
-                it.close()
-            }
+            urlConnection  = urlConnection.setRequestProperty()
+            val wr = DataOutputStream(urlConnection.outputStream)
+            wr.writeBytes(message ?: "")
+            wr.flush()
+            wr.close()
 
             responseCode = urlConnection.responseCode
         } catch (e: Throwable) {
@@ -52,26 +52,12 @@ class LoggerCloudNewRelicDataSource : LoggerCloudNewRelicImpl {
         }
     }
 
-    private fun HttpURLConnection.setRequestProperty(size: Int): HttpURLConnection {
+    private fun HttpURLConnection.setRequestProperty(): HttpURLConnection {
         requestMethod = Constants.METHOD_POST
         doOutput = true
         useCaches = false
         setRequestProperty(Constants.HEADER_CONTENT_TYPE, Constants.HEADER_CONTENT_TYPE_JSON)
         setRequestProperty(Constants.HEADER_NEW_RELIC_KEY, Keys.AUTH_NEW_RELIC_API_KEY)
-        setRequestProperty(Constants.HEADER_CONTENT_ENCODING, Constants.HEADER_CONTENT_ENCODING_GZIP)
-        setRequestProperty(Constants.HEADER_CONTENT_LENGTH, size.toString())
         return this
     }
-
-    private fun compressRequestBody(message: List<String>): ByteArray {
-        val requestBodyInBytes = gson.toJson(message).toByteArray()
-        return ByteArrayOutputStream(requestBodyInBytes.size).use { byteArrayOutputStream ->
-            GZIPOutputStream(byteArrayOutputStream).use { gzipOutputStream ->
-                gzipOutputStream.write(requestBodyInBytes, 0, requestBodyInBytes.size)
-            }
-            byteArrayOutputStream.toByteArray()
-        }
-    }
-
-
 }
