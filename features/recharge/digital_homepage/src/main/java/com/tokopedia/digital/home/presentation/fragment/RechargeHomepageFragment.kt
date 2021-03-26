99faf4e8600e10f288539a74b64a3e123b0259a4
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-03-26 10:10:43 +0700

    fix danger sdk > 19 and content_desc

--- features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/fragment/RechargeHomepageFragment.kt
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/fragment/RechargeHomepageFragment.kt
@@ -160,10 +160,6 @@ class RechargeHomepageFragment : BaseDaggerFragment(),
             activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         }
 
-        if (Build.VERSION.SDK_INT >= 19) {
-            activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-
         if (Build.VERSION.SDK_INT >= 21) {
             activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
             activity?.window?.statusBarColor = Color.TRANSPARENT
