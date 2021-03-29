fadc29fec5db8818e61547e737eb0d79a144fba0
saidfaisal <said.faisal@tokopedia.com>
2021-03-29 23:24:52 +0700

    add log

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/presentation/fragment/AddEditProductDescriptionFragment.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/description/presentation/fragment/AddEditProductDescriptionFragment.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -467,6 +468,7 @@ class AddEditProductDescriptionFragment:
 
     private fun observeDescriptionValidation() {
         descriptionViewModel.descriptionValidationMessage.observe(viewLifecycleOwner) {
+            Log.d("DESCRIPTION_PAGE_TEST", it)
             updateDescriptionFieldErrorMessage(it)
         }
     }
