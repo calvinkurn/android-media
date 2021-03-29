e0b8a7ce5f98fd042a2947346424690d3ba3b90c
Jessica Sean <jessica.sean@tokopedia.com>
2021-03-29 15:57:01 +0700

    feat: initial commit on recharge_pdp_emoney

---
+++ features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/activity/EmoneyPdpActivity.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.recharge_pdp_emoney.presentation.activity
+
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
+class EmoneyPdpActivity : BaseSimpleActivity() {
+    override fun getNewFragment(): Fragment = Fragment()
+}
---
+++ features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/fragment/EmoneyPdpFragment.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.recharge_pdp_emoney.presentation.fragment
+
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+
+/**
+ * @author by jessica on 29/03/21
+ */
+
+class EmoneyPdpFragment : BaseDaggerFragment() {
+    override fun getScreenName(): String = ""
+
+    override fun initInjector() {
+        TODO("Not yet implemented")
+    }
+
+}
