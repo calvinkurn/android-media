47b8050a2337d53671744a8df01c9a488468cf11
Jessica Sean <jessica.sean@tokopedia.com>
2021-03-29 19:33:47 +0700

    feat: add ui for emoney pdp (part 1) - ticker, header and input view

--- features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/activity/EmoneyPdpActivity.kt
+++ features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/activity/EmoneyPdpActivity.kt
@@ -1,8 +1,22 @@
 package com.tokopedia.recharge_pdp_emoney.presentation.activity
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.recharge_pdp_emoney.di.EmoneyPdpComponent
+import com.tokopedia.recharge_pdp_emoney.di.EmoneyPdpComponentInstance
+import com.tokopedia.recharge_pdp_emoney.presentation.fragment.EmoneyPdpFragment
 
-class EmoneyPdpActivity : BaseSimpleActivity() {
-    override fun getNewFragment(): Fragment = Fragment()
+class EmoneyPdpActivity : BaseSimpleActivity(), HasComponent<EmoneyPdpComponent> {
+    override fun getNewFragment(): Fragment = EmoneyPdpFragment()
+
+    override fun getComponent(): EmoneyPdpComponent {
+        return EmoneyPdpComponentInstance.getEmoneyPdpComponent(application)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        toolbar.elevation = 0f
+    }
 }
--- features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/fragment/EmoneyPdpFragment.kt
+++ features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/presentation/fragment/EmoneyPdpFragment.kt
@@ -1,7 +1,13 @@
 package com.tokopedia.recharge_pdp_emoney.presentation.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.recharge_pdp_emoney.R
 import com.tokopedia.recharge_pdp_emoney.di.EmoneyPdpComponent
+import kotlinx.android.synthetic.main.fragment_emoney_pdp.*
 
 /**
  * @author by jessica on 29/03/21
@@ -10,7 +16,22 @@ import com.tokopedia.recharge_pdp_emoney.di.EmoneyPdpComponent
 class EmoneyPdpFragment : BaseDaggerFragment() {
     override fun getScreenName(): String = ""
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_emoney_pdp, container, false)
+    }
+
     override fun initInjector() {
         getComponent(EmoneyPdpComponent::class.java).inject(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //dummmy
+        emoneyPdpHeaderView.titleText = "Cek saldo kamu di sini"
+        emoneyPdpHeaderView.subtitleText = "Yuk, coba cek sisa saldo di kartu e-money kamu."
+        emoneyPdpHeaderView.buttonCtaText = "Cek Saldo"
+
+        emoneyPdpInputCardWidget.initView()
     }
 }
