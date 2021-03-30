6036c263ade4cef4107980a1825e13ac4aca7d8b
mzennis <meyta.taliti@tokopedia.com>
2021-03-30 21:32:30 +0700

    remove unused code

--- features/content/play/src/main/java/com/tokopedia/play/analytic/PlayAnalytic.kt
+++ features/content/play/src/main/java/com/tokopedia/play/analytic/PlayAnalytic.kt
@@ -372,33 +372,7 @@ class PlayAnalytic(
     fun impressionFeaturedProduct(featuredProducts: List<PlayProductUiModel.Product>) {
         /**
          * Data analyst wants us to send multiple enhance-ecommerce at varying times rather than sending them all at once
-         *
-        trackingQueue.putEETracking(
-                EventModel(
-                        "productView",
-                        KEY_TRACK_GROUP_CHAT_ROOM,
-                        "view on featured product",
-                        "$channelId - ${featuredProducts[0].id} - ${channelType.value} - featured product tagging"
-                ),
-                hashMapOf(
-                        "ecommerce" to hashMapOf(
-                                "currencyCode" to "IDR",
-                                "impressions" to convertProductsToListOfObject(
-                                        featuredProducts,
-                                        sourceFrom = "featured product",
-                                        startPosition = 1
-                                )
-                        )
-                ),
-                hashMapOf(
-                        KEY_CURRENT_SITE to KEY_TRACK_CURRENT_SITE,
-                        KEY_SESSION_IRIS to TrackApp.getInstance().gtm.irisSessionId,
-                        KEY_USER_ID to userId,
-                        KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT
-                )
-        )
          */
-
         TrackApp.getInstance().gtm.sendEnhanceEcommerceEvent(
                 mapOf(
                         KEY_EVENT to "productView",
