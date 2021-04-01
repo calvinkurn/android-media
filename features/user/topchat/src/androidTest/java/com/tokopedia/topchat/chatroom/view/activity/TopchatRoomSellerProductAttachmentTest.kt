73356a72b101ed1ef04f00517fca07a367bc9cd7
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-04-01 12:57:30 +0700

    add test assert_normal_product_stock_info

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSellerProductAttachmentTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSellerProductAttachmentTest.kt
@@ -3,10 +3,13 @@ package com.tokopedia.topchat.chatroom.view.activity
 import android.view.Gravity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.view.activity.base.BaseSellerTopchatRoomTest
 import com.tokopedia.topchat.matchers.withLinearLayoutGravity
 import com.tokopedia.topchat.matchers.withRecyclerView
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 class TopchatRoomSellerProductAttachmentTest : BaseSellerTopchatRoomTest() {
@@ -27,8 +30,44 @@ class TopchatRoomSellerProductAttachmentTest : BaseSellerTopchatRoomTest() {
         ).check(matches(withLinearLayoutGravity(Gravity.END)))
         onView(
                 withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        1, R.id.ll_footer
+                )
+        ).check(matches(not(isDisplayed())))
+
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
                         3, R.id.containerProductAttachment
                 )
         ).check(matches(withLinearLayoutGravity(Gravity.START)))
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        3, R.id.ll_footer
+                )
+        ).check(matches(not(isDisplayed())))
     }
+
+    @Test
+    fun assert_normal_product_stock_info() {
+        // Given
+        setupChatRoomActivity()
+        getChatUseCase.response = sellerProductChatReplies
+        chatAttachmentUseCase.response = sellerProductAttachment
+        inflateTestFragment()
+
+        // Then
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        1, R.id.tp_seller_stock_category
+                )
+        ).check(matches(isDisplayed()))
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        1, R.id.tp_seller_stock_category
+                )
+        ).check(matches(withText("Stok:")))
+        assertStockCountVisibilityAt(1, isDisplayed())
+        assertStockCountValueAt(1, 5)
+    }
+
+
 }
--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/base/BaseSellerTopchatRoomTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/base/BaseSellerTopchatRoomTest.kt
@@ -1,8 +1,22 @@
 package com.tokopedia.topchat.chatroom.view.activity.base
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.tokopedia.chat_common.domain.pojo.GetExistingChatPojo
+import com.tokopedia.product.manage.common.feature.list.constant.ProductManageCommonConstant
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 import com.tokopedia.topchat.AndroidFileUtil
+import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.domain.pojo.chatattachment.ChatAttachmentResponse
+import com.tokopedia.topchat.matchers.withRecyclerView
+import org.hamcrest.Matcher
 
 open class BaseSellerTopchatRoomTest : TopchatRoomTest() {
 
@@ -18,6 +32,35 @@ open class BaseSellerTopchatRoomTest : TopchatRoomTest() {
         sellerProductAttachment = AndroidFileUtil.parse(
                 "seller/success_get_chat_attachments_seller.json",
                 ChatAttachmentResponse::class.java
+        )
+    }
+
+    protected fun assertStockCountVisibilityAt(productCardPosition: Int, viewMatcher: Matcher<in View>) {
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        productCardPosition, R.id.tp_seller_stock_count
+                )
+        ).check(matches(viewMatcher))
+    }
+
+    protected fun assertStockCountValueAt(productCardPosition: Int, stockCount: Int) {
+        onView(
+                withRecyclerView(R.id.recycler_view).atPositionOnView(
+                        productCardPosition, R.id.tp_seller_stock_count
+                )
+        ).check(matches(withText("$stockCount")))
+    }
+
+    protected fun createSuccessUpdateStockIntentResult(
+            productId: String, stock: Int, status: ProductStatus
+    ) {
+        val intent = Intent().apply {
+            putExtra(ProductManageCommonConstant.EXTRA_PRODUCT_ID, productId)
+            putExtra(ProductManageCommonConstant.EXTRA_UPDATED_STOCK, stock)
+            putExtra(ProductManageCommonConstant.EXTRA_UPDATED_STATUS, status.name)
+        }
+        intending(anyIntent()).respondWith(
+                Instrumentation.ActivityResult(Activity.RESULT_OK, intent)
         )
     }
 }
