0fb19f7fd7454a391186b2f83f57505cf95abc9c
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-04-05 17:30:28 +0700

    instanceOf BaseWebViewActivity fix

--- libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
+++ libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
@@ -143,7 +143,7 @@ public abstract class BaseWebViewFragment extends BaseDaggerFragment {
     @NonNull
     protected String getUrl() {
         String env = TokopediaUrl.Companion.getInstance().getTYPE().getValue();
-        if (url.contains(JS_TOKOPEDIA)  || (url.contains(JS_STAGING_TOKOPEDIA) && env.equalsIgnoreCase(STAGING))) {
+        if (url.contains(JS_TOKOPEDIA) || (url.contains(JS_STAGING_TOKOPEDIA) && env.equalsIgnoreCase(STAGING))) {
             return url;
         } else if (isTokopediaUrl) {
             String gcmId = userSession.getDeviceId();
@@ -856,9 +856,9 @@ public abstract class BaseWebViewFragment extends BaseDaggerFragment {
     }
 
     public void reloadPage() {
-        BaseSimpleWebViewActivity activity = (BaseSimpleWebViewActivity) getActivity();
-        if (activity!= null) {
-            activity.setWebViewTitle("");
+        Activity activity = getActivity();
+        if (activity instanceof BaseSimpleWebViewActivity) {
+            ((BaseSimpleWebViewActivity) activity).setWebViewTitle("");
         }
         swipeRefreshLayout.setVisibility(View.VISIBLE);
         globalError.setVisibility(View.GONE);
