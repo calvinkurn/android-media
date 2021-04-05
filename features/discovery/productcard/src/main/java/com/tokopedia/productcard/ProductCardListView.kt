3d66ed7a7e4dad9ca1a5204ff127ae8c8d00ffaa
yoasfs <yoas.susantio@tokopedia.com>
2021-04-05 15:08:27 +0700

    remove unused color logic

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/ProductCardListView.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/ProductCardListView.kt
@@ -133,10 +133,7 @@ class ProductCardListView: BaseCustomView, IProductCardView {
     private fun View.renderStockLabel(productCardModel: ProductCardModel) {
         textViewStockLabel?.shouldShowWithAction(productCardModel.stockBarLabel.isNotEmpty()) {
             textViewStockLabel.text = productCardModel.stockBarLabel
-            if (productCardModel.stockBarLabel.equals(WORDING_SEGERA_HABIS, ignoreCase = true)) {
-                    textViewStockLabel.setTextColor(MethodChecker.getColor(context,
-                            com.tokopedia.unifyprinciples.R.color.Unify_R600))
-            } else if (productCardModel.stockBarLabelColor.isNotEmpty()) {
+            if (productCardModel.stockBarLabelColor.isNotEmpty()) {
                 textViewStockLabel.setTextColor(safeParseColor(productCardModel.stockBarLabelColor))
             } else {
                 textViewStockLabel.setTextColor(MethodChecker.getColor(context,
