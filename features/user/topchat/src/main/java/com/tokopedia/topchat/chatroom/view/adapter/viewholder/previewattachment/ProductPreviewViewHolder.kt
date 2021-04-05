4743444cc6ecb87bbc4fff4e94d882f8bdf323e8
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-04-05 16:33:02 +0700

    Revert "remove product price and variant from product preview"
    This reverts commit 8f21644d

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/previewattachment/ProductPreviewViewHolder.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/previewattachment/ProductPreviewViewHolder.kt
@@ -3,10 +3,16 @@ package com.tokopedia.topchat.chatroom.view.adapter.viewholder.previewattachment
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.topchat.R
+import com.tokopedia.topchat.chatroom.view.adapter.viewholder.common.ColorDrawableGenerator
 import com.tokopedia.topchat.chatroom.view.viewmodel.SendableProductPreview
 import com.tokopedia.topchat.common.util.ViewUtil
 import com.tokopedia.unifycomponents.toPx
@@ -16,7 +22,15 @@ class ProductPreviewViewHolder(itemView: View, attachmentItemPreviewListener: At
 
     private val productImage = itemView.findViewById<ImageView>(R.id.iv_product)
     private val productName = itemView.findViewById<TextView>(R.id.tv_product_name)
+    private val productPrice = itemView.findViewById<TextView>(R.id.tv_product_price)
     private val container = itemView.findViewById<ConstraintLayout>(R.id.cl_container)
+
+    private val productVariantContainer = itemView.findViewById<LinearLayout>(R.id.ll_variant)
+    private val productColorVariant = itemView.findViewById<LinearLayout>(R.id.ll_variant_color)
+    private val productColorVariantHex = itemView.findViewById<ImageView>(R.id.iv_variant_color)
+    private val productColorVariantValue = itemView.findViewById<TextView>(R.id.tv_variant_color)
+    private val productSizeVariant = itemView.findViewById<LinearLayout>(R.id.ll_variant_size)
+    private val productSizeVariantValue = itemView.findViewById<TextView>(R.id.tv_variant_size)
 
     private val bg = ViewUtil.generateBackgroundWithShadow(
             container,
@@ -40,10 +54,36 @@ class ProductPreviewViewHolder(itemView: View, attachmentItemPreviewListener: At
         bindBackground()
         bindImageThumbnail(model)
         bindProductName(model)
+        bindProductPrice(model)
+        bindProductVariant(model)
     }
 
     private fun bindProductName(model: SendableProductPreview) {
         productName?.text = model.productPreview.name
+    }
+
+    private fun bindProductPrice(model: SendableProductPreview) {
+        productPrice?.text = model.productPreview.price
+    }
+
+    private fun bindProductVariant(model: SendableProductPreview) {
+        val productPreview = model.productPreview
+        productVariantContainer?.showWithCondition(model.hasVariant())
+        if (model.hasColorVariant()) {
+            productColorVariant.show()
+            val backgroundDrawable = ColorDrawableGenerator.generate(
+                    itemView.context, productPreview.colorHexVariant
+            )
+            productColorVariantHex?.background = backgroundDrawable
+            productColorVariantValue?.text = productPreview.colorVariant
+        } else {
+            productColorVariant?.hide()
+        }
+
+        val productHasSizeVariant = model.hasSizeVariant()
+        productSizeVariant?.shouldShowWithAction(productHasSizeVariant) {
+            productSizeVariantValue?.text = productPreview.sizeVariant
+        }
     }
 
     private fun bindImageThumbnail(model: SendableProductPreview) {
