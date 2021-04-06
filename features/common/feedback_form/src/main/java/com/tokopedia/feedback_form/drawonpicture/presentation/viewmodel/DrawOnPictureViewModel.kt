2e2b8c6c29385cb96db39612be023d0f727ae750
rezagama <reza.hidayat@tokopedia.com>
2021-04-06 11:04:48 +0700

    [Feedback Form] Fix coroutine dispatcher
    - Switch main & io

--- features/common/feedback_form/src/main/java/com/tokopedia/feedback_form/drawonpicture/presentation/viewmodel/DrawOnPictureViewModel.kt
+++ features/common/feedback_form/src/main/java/com/tokopedia/feedback_form/drawonpicture/presentation/viewmodel/DrawOnPictureViewModel.kt
@@ -4,15 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
  * @author by furqan on 01/10/2020
  */
 class DrawOnPictureViewModel @Inject constructor(private val dispatcherProvider: CoroutineDispatchers)
-    : BaseViewModel(dispatcherProvider.io) {
+    : BaseViewModel(dispatcherProvider.main) {
 
     private val mutableShowPencilOptions = MutableLiveData<Boolean>()
     val showPencilOptions: LiveData<Boolean>
@@ -22,9 +22,11 @@ class DrawOnPictureViewModel @Inject constructor(private val dispatcherProvider:
 
     fun startDrawing() {
         isOnDrawProcess = true
-        launch(dispatcherProvider.main) {
+        launchCatchError(dispatcherProvider.io, {
             delay(1000)
             if (isOnDrawProcess) mutableShowPencilOptions.postValue(false)
+        }) {
+            it.printStackTrace()
         }
     }
 
