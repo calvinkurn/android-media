9ed594fc202bdb162dd1c9f8323da73d4a571b53
yoasfs <yoas.susantio@tokopedia.com>
2021-04-06 17:18:45 +0700

    update

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/fragment/RecommendationFragment.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/fragment/RecommendationFragment.kt
@@ -717,9 +717,7 @@ open class RecommendationFragment: BaseListFragment<HomeRecommendationDataModel,
             action = Intent.ACTION_SEND
             type = "text/plain"
             addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                putExtra(Intent.EXTRA_REFERRER, shareUri)
-            }
+            putExtra(Intent.EXTRA_REFERRER, shareUri)
             putExtra(Intent.EXTRA_HTML_TEXT, "$shareContent $shareUri")
             putExtra(Intent.EXTRA_TITLE, title)
             putExtra(Intent.EXTRA_TEXT, "$shareContent $shareUri")
