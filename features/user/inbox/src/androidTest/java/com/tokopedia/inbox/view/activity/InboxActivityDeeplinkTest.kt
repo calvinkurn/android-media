440e24f300744babfb0581e7dbfd691498ddbc75
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-04-12 11:27:10 +0700

    setup deeplink test inbox

---
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/view/activity/InboxActivityDeeplinkTest.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.inbox.view.activity
+
+import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4ClassRunner::class)
+class InboxActivityDeeplinkTest {
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+}
