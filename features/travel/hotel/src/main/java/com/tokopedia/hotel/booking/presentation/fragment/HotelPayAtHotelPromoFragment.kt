52d74b76a65b00c0dd3ca9a59915c5244b451f51
astidhiyaa <asti.anzaria@tokopedia.com>
2021-04-13 14:48:31 +0800

    fix: improvement for page empty promo (pay at hotel)

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/booking/presentation/fragment/HotelPayAtHotelPromoFragment.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/booking/presentation/fragment/HotelPayAtHotelPromoFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.tokopedia.hotel.R
 import com.tokopedia.hotel.booking.di.HotelBookingComponent
 import com.tokopedia.hotel.common.presentation.HotelBaseFragment
+import kotlinx.android.synthetic.main.fragment_hotel_pay_at_hotel_promo.*
 
 class HotelPayAtHotelPromoFragment : HotelBaseFragment() {
     override fun onErrorRetryClicked() {}
@@ -17,6 +18,14 @@ class HotelPayAtHotelPromoFragment : HotelBaseFragment() {
     override fun getScreenName(): String = getString(R.string.hotel_payathotel_header_title)
 
     override fun initInjector() = getComponent(HotelBookingComponent::class.java).inject(this)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnHotelPayAtHotelPromo.setOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
 
     companion object {
         fun newInstance() = HotelPayAtHotelPromoFragment()
