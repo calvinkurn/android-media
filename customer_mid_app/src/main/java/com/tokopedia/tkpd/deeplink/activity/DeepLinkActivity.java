e3dff3225755da5b5d1521aa6571ec89b091da72
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-04-14 17:58:12 +0700

    remove initView content

--- customer_mid_app/src/main/java/com/tokopedia/tkpd/deeplink/activity/DeepLinkActivity.java
+++ customer_mid_app/src/main/java/com/tokopedia/tkpd/deeplink/activity/DeepLinkActivity.java
@@ -103,7 +103,7 @@ public class DeepLinkActivity extends BasePresenterActivity<DeepLinkPresenter> i
 
     @Override
     protected void initView() {
-        View mainView = findViewById(R.id.main_view);
+        
     }
 
     @Override
