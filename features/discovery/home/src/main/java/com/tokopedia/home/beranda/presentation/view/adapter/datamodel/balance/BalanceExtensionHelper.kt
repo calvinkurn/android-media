ac78229ce410b0eeed965be39174a0f2fc0b4923
yoasfs <yoas.susantio@tokopedia.com>
2021-04-14 17:27:12 +0700

    fix text color on ovo

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/datamodel/balance/BalanceExtensionHelper.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/datamodel/balance/BalanceExtensionHelper.kt
@@ -67,7 +67,7 @@ fun HomeHeaderWalletAction.buildWalletSubTitleTextAttribute(): BalanceTextAttrib
     walletBalanceCondition (
             isLinkedCondition = {
                 text = labelActionButton
-                colourRef = R.color.Unify_N700_68
+                colourRef = R.color.Unify_N700_96
             },
             isNotLinkedCondition = {
                 text = labelActionButton
