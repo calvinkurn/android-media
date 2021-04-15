90c0f41fd9c683d9aa06eb5d0921fff3000bd002
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-04-15 13:05:01 +0700

    fix unit test & remove encryption

--- libraries/logger/src/test/java/com/tokopedia/logger/repository/LoggerRepositoryTestFixture.kt
+++ libraries/logger/src/test/java/com/tokopedia/logger/repository/LoggerRepositoryTestFixture.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.logger.repository
 
-import com.tokopedia.encryption.security.AESEncryptorECB
 import com.tokopedia.logger.datasource.cloud.LoggerCloudDataSource
 import com.tokopedia.logger.datasource.cloud.LoggerCloudNewRelicImpl
 import com.tokopedia.logger.datasource.db.LoggerDao
@@ -9,7 +8,6 @@ import com.tokopedia.logger.model.scalyr.ScalyrConfig
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Before
-import javax.crypto.SecretKey
 
 abstract class LoggerRepositoryTestFixture {
 
@@ -36,12 +34,8 @@ abstract class LoggerRepositoryTestFixture {
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        val encryptor = AESEncryptorECB()
-        val ENCRYPTION_KEY = String(charArrayOf(113.toChar(), 40.toChar(), 101.toChar(), 35.toChar(), 37.toChar(), 71.toChar(), 102.toChar(), 64.toChar(), 111.toChar(), 105.toChar(), 62.toChar(), 108.toChar(), 107.toChar(), 66.toChar(), 126.toChar(), 104.toChar()))
-        val secretKey: SecretKey = encryptor.generateKey(ENCRYPTION_KEY)
-
-        encrypt = {s : String -> encryptor.encrypt(s, secretKey)}
-        decrypt = {s : String -> encryptor.decrypt(s, secretKey)}
+        encrypt = {s : String -> s}
+        decrypt = {s : String -> s}
         loggerRepository = LoggerRepository(loggerDao, loggerCloudDataSource, loggerCloudNewRelicImpl,
                 scalyrConfigs, newRelicConfigs, encrypt, decrypt)
     }
