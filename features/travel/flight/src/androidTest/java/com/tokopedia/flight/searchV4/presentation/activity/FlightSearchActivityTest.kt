a3b7330a2bc592427e1d0c81aca1b28dac21073d
astidhiyaa <asti.anzaria@tokopedia.com>
2021-04-16 12:55:04 +0800

    fix id not found

--- features/travel/flight/src/androidTest/java/com/tokopedia/flight/searchV4/presentation/activity/FlightSearchActivityTest.kt
+++ features/travel/flight/src/androidTest/java/com/tokopedia/flight/searchV4/presentation/activity/FlightSearchActivityTest.kt
@@ -156,10 +156,7 @@ class FlightSearchActivityTest {
     fun promoChipsOnClicked(){
         Thread.sleep(2000)
         if (getPromoChipsItemCount() > 0) {
-            val viewInteraction = onView(AllOf.allOf(
-                    AllOf.allOf(withId(R.id.recycler_view_promo_chips), withParent(withId(R.id.layout_widget_flight_promo_chips)),
-                            isDisplayed()))).check(matches(isDisplayed()))
-            viewInteraction.perform(RecyclerViewActions
+            onView(withId(R.id.recycler_view_promo_chips)).perform(RecyclerViewActions
                     .actionOnItemAtPosition<FlightPromoChipsViewHolder>(0, click()))
             assertThat(getAnalyticsWithQuery(gtmLogDBSource, context, ANALYTIC_VALIDATOR_PROMO_CHIPS),
                     hasAllSuccess())
