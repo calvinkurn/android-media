415ba8f09f69e174771c4939247abc85572bca77
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-04-16 16:38:12 +0700

    add access modifier public

--- customer_mid_app/src/main/java/com/tokopedia/tkpd/utils/NewRelicConstants.java
+++ customer_mid_app/src/main/java/com/tokopedia/tkpd/utils/NewRelicConstants.java
@@ -1,5 +1,5 @@
 package com.tokopedia.tkpd.utils;
 
-interface NewRelicConstants {
+public interface NewRelicConstants {
     String ENCRYPTION_KEY = new String(new char[]{113, 40, 101, 35, 37, 71, 102, 64, 111, 105, 62, 108, 107, 66, 126, 104});
 }
