523a0a72f1ce1616782dad40d6f7dda9d692d0ab
misael-jonathan <misael.jonathan@tokopedia.com>
2021-04-19 08:55:41 +0700

    change product sku sent type

--- libraries/linker/src/main/java/com/tokopedia/linker/model/RechargeLinkerData.java
+++ libraries/linker/src/main/java/com/tokopedia/linker/model/RechargeLinkerData.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class RechargeLinkerData {
 
     private LinkerData linkerData;
-    private List<String> groupWiseCategoryId;
+    private List<String> categoryIds;
 
     public LinkerData getLinkerData() {
         return linkerData;
@@ -15,12 +15,12 @@ public class RechargeLinkerData {
         this.linkerData = linkerData;
     }
 
-    public List<String> getCategoryIds() {
-        return groupWiseCategoryId;
+    public String getCategoryIds() {
+        return categoryIds.toString();
     }
 
     public void setCategoryIds(List<String> categoryId) {
-        this.groupWiseCategoryId = categoryId;
+        this.categoryIds = categoryId;
     }
 
 }
