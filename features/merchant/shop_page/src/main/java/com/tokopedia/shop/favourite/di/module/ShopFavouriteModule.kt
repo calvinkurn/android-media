11efdd301c73405b74dcff4110252c0f23fec1fe
shiperus <calvyn.julian@tokopedia.com>
2021-04-20 10:30:17 +0700

    move query string for shop follower

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/favourite/di/module/ShopFavouriteModule.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/favourite/di/module/ShopFavouriteModule.kt
@@ -4,14 +4,11 @@ import android.content.Context
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchersProvider
 import com.tokopedia.shop.common.di.ShopPageContext
-import com.tokopedia.shop.favourite.data.query.GetShopFollowerListQueryProvider.getQuery
 import com.tokopedia.shop.favourite.di.scope.ShopFavouriteScope
-import com.tokopedia.shop.favourite.domain.interactor.GetShopFollowerListUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
-import javax.inject.Named
 
 @Module
 class ShopFavouriteModule {
@@ -19,13 +16,6 @@ class ShopFavouriteModule {
     @Provides
     fun provideUserSessionInterface(@ShopPageContext context: Context?): UserSessionInterface {
         return UserSession(context)
-    }
-
-    @ShopFavouriteScope
-    @Provides
-    @Named(GetShopFollowerListUseCase.QUERY_SHOP_FOLLOWER_LIST)
-    fun providesGetShopFollowerQuery(): String {
-        return getQuery()
     }
 
     @ShopFavouriteScope
