b6a931264df9027331aeda00b4b84393962d0db3
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-04-22 00:01:04 +0800

    add unit test DeactivationViewModel

--- features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/DeactivationViewModelTest.kt
+++ features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/DeactivationViewModelTest.kt
@@ -3,31 +3,39 @@ package com.tokopedia.power_merchant.subscribe.view.viewmodel
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import com.tokopedia.gm.common.data.source.cloud.model.GoldDeactivationSubscription
 import com.tokopedia.gm.common.data.source.cloud.model.GoldGetPmOsStatus
+import com.tokopedia.gm.common.data.source.cloud.model.PMCancellationQuestionnaireAnswerModel
 import com.tokopedia.gm.common.domain.interactor.DeactivatePMUseCase
 import com.tokopedia.gm.common.domain.interactor.GetShopStatusUseCase
-import com.tokopedia.graphql.data.model.GraphqlError
-import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.power_merchant.subscribe.data.model.*
 import com.tokopedia.power_merchant.subscribe.domain.interactor.GetGoldCancellationsQuestionaireUseCase
 import com.tokopedia.power_merchant.subscribe.domain.interactor.GetPMCancellationQuestionnaireDataUseCase
-import com.tokopedia.power_merchant.subscribe.view_old.model.PMCancellationQuestionnaireData
+import com.tokopedia.power_merchant.subscribe.view.model.DeactivationQuestionnaireUiModel
+import com.tokopedia.power_merchant.subscribe.view.model.QuestionnaireOptionUiModel
+import com.tokopedia.power_merchant.subscribe.view.model.QuestionnaireUiModel
 import com.tokopedia.power_merchant.subscribe.view_old.model.PMCancellationQuestionnaireMultipleOptionModel
 import com.tokopedia.power_merchant.subscribe.view_old.model.PMCancellationQuestionnaireQuestionModel
 import com.tokopedia.power_merchant.subscribe.view_old.model.PMCancellationQuestionnaireRateModel
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.unit.test.ext.verifyErrorEquals
+import com.tokopedia.unit.test.ext.verifySuccessEquals
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.*
 import rx.Observable
-import java.lang.reflect.Type
+import rx.Scheduler
+import rx.android.plugins.RxAndroidPlugins
+import rx.android.plugins.RxAndroidSchedulersHook
+import rx.plugins.RxJavaPlugins
+import rx.plugins.RxJavaSchedulersHook
+import rx.schedulers.Schedulers
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
@@ -35,6 +43,7 @@ import java.util.concurrent.TimeUnit
  * Created By @ilhamsuaib on 21/04/21
  */
 
+@ExperimentalCoroutinesApi
 class DeactivationViewModelTest {
 
     @get:Rule
@@ -55,6 +64,7 @@ class DeactivationViewModelTest {
 
     @Before
     fun setup() {
+        registerRxPlugins()
         MockKAnnotations.init(this)
 
         getPMCancellationQuestionnaireDataUseCase = GetPMCancellationQuestionnaireDataUseCase(
@@ -69,8 +79,14 @@ class DeactivationViewModelTest {
         )
     }
 
+    @After
+    fun tearDown() {
+        resetRxPlugins()
+        RxAndroidPlugins.getInstance().reset()
+    }
+
     @Test
-    fun testGetPMCancellationRateQuestionnaireDataSuccess() {
+    fun testGetPMCancellationQuestionnaireDataSuccess() {
         val shopId = "1"
         val questionTitle = "Question A"
         val questionType = PMCancellationQuestionnaireQuestionModel.TYPE_RATE
@@ -78,56 +94,36 @@ class DeactivationViewModelTest {
         val question = Question(question = questionTitle, questionType = questionType)
         val questionnaireData = QuestionnaireData(data = Data(mutableListOf(question)))
 
+        val pmOsStatus = GoldGetPmOsStatus()
         every {
             getShopStatusUseCase.createObservable(any())
-        } returns Observable.just(GoldGetPmOsStatus())
+        } returns Observable.just(pmOsStatus)
 
+        val questionnaireList = GoldCancellationsQuestionaire(questionnaireData)
         every {
             getGoldCancellationsQuestionaireUseCase.createObservable(any())
-        } returns Observable.just(GoldCancellationsQuestionaire(questionnaireData))
+        } returns Observable.just(questionnaireList)
 
         viewModel.getPMCancellationQuestionnaireData(shopId)
 
-        val questionnaireList = mutableListOf<PMCancellationQuestionnaireQuestionModel>(
-                PMCancellationQuestionnaireRateModel(questionType, questionTitle))
+        val numOfQuestions = questionnaireList.result.data.questionList.size
+        val listQuestion = questionnaireList.result.data.questionList.mapIndexed { index, q ->
+            return@mapIndexed when (q.questionType) {
+                PMCancellationQuestionnaireQuestionModel.TYPE_MULTIPLE_OPTION -> {
+                    createMultipleOptionQuestion(q, index != numOfQuestions.minus(1))
+                }
+                else -> QuestionnaireUiModel.QuestionnaireRatingUiModel(q.question)
+            }
+        }
 
-        val expected = Success(PMCancellationQuestionnaireData(listQuestion = questionnaireList))
+        val expected = Success(DeactivationQuestionnaireUiModel(
+                expiredDate = pmOsStatus.result.data.powerMerchant.expiredTime,
+                listQuestion = listQuestion
+        ))
         val actual = viewModel.pmCancellationQuestionnaireData
-                .observeAwaitValue() as? Success<PMCancellationQuestionnaireData>
+                .observeAwaitValue() as? Success<DeactivationQuestionnaireUiModel>
 
-        assertCancellationQuestionnaireData(expected, actual)
-    }
-
-    @Test
-    fun testGetPMCancellationMultipleOptionsQuestionnaireDataSuccess() {
-        val shopId = "2"
-        val optionTitle = "Option 1"
-        val questionTitle = "Question B"
-        val questionType = PMCancellationQuestionnaireQuestionModel.TYPE_MULTIPLE_OPTION
-
-        val options = mutableListOf(Option(value = optionTitle))
-        val question = Question(question = questionTitle, questionType = questionType, option = options)
-        val questionnaireData = QuestionnaireData(data = Data(mutableListOf(question)))
-
-        every {
-            getShopStatusUseCase.createObservable(any())
-        } returns Observable.just(GoldGetPmOsStatus())
-
-        every {
-            getGoldCancellationsQuestionaireUseCase.createObservable(any())
-        } returns Observable.just(GoldCancellationsQuestionaire(questionnaireData))
-
-        viewModel.getPMCancellationQuestionnaireData(shopId)
-
-        val questionOptions = listOf(PMCancellationQuestionnaireMultipleOptionModel.OptionModel(optionTitle))
-        val questionnaireList = mutableListOf<PMCancellationQuestionnaireQuestionModel>(
-                PMCancellationQuestionnaireMultipleOptionModel(questionType, questionTitle, questionOptions))
-
-        val expected = Success(PMCancellationQuestionnaireData(listQuestion = questionnaireList))
-        val actual = viewModel.pmCancellationQuestionnaireData
-                .observeAwaitValue() as? Success<PMCancellationQuestionnaireData>
-
-        assertCancellationQuestionnaireData(expected, actual)
+        assertEquals(expected, actual)
     }
 
     @Test
@@ -139,30 +135,57 @@ class DeactivationViewModelTest {
         every {
             getGoldCancellationsQuestionaireUseCase.createObservable(any())
         } returns Observable.error(Throwable())
+
         viewModel.getPMCancellationQuestionnaireData(shopId)
-        val pmCancellationQuestionnaireData = viewModel
-                .pmCancellationQuestionnaireData.observeAwaitValue()
+
+        val pmCancellationQuestionnaireData = viewModel.pmCancellationQuestionnaireData.observeAwaitValue()
+
         Assert.assertTrue(pmCancellationQuestionnaireData is Fail)
     }
 
     @Test
-    fun testSendQuestionAnswerDataAndTurnOffAutoExtendSuccess() {
+    fun testSendQuestionAnswerDataAndTurnOffAutoExtendSuccess() = runBlocking {
+        val questionsData = mutableListOf<PMCancellationQuestionnaireAnswerModel>()
+        deactivatePMUseCase.params = DeactivatePMUseCase.createRequestParam(questionsData)
 
+        coEvery {
+            deactivatePMUseCase.executeOnBackground()
+        } returns true
+
+        viewModel.submitPmDeactivation(questionsData)
+
+        val expectedResult = Success(true)
+
+        viewModel.isSuccessDeactivate.verifySuccessEquals(expectedResult)
     }
 
     @Test
-    fun testSendQuestionAnswerDataAndTurnOffAutoExtendError() {
+    fun testSendQuestionAnswerDataAndTurnOffAutoExtendError() = runBlocking {
+        val questionsData = mutableListOf<PMCancellationQuestionnaireAnswerModel>()
+        deactivatePMUseCase.params = DeactivatePMUseCase.createRequestParam(questionsData)
 
+        val error = Throwable()
+        coEvery {
+            deactivatePMUseCase.executeOnBackground()
+        } throws error
+
+        viewModel.submitPmDeactivation(questionsData)
+
+        val expectedResult = Fail(error)
+
+        viewModel.isSuccessDeactivate.verifyErrorEquals(expectedResult)
     }
 
-    private fun createMockGraphqlResponseDeactivateMerchant(): GraphqlResponse {
-        val result = HashMap<Type, Any>()
-        val errors = HashMap<Type, List<GraphqlError>>()
-        result[GoldDeactivationSubscription::class.java] = GoldDeactivationSubscription()
-        return GraphqlResponse(
-                result,
-                errors,
-                false
+    private fun createMultipleOptionQuestion(
+            questionData: Question,
+            showItemDivider: Boolean
+    ): QuestionnaireUiModel.QuestionnaireMultipleOptionUiModel {
+        return QuestionnaireUiModel.QuestionnaireMultipleOptionUiModel(
+                question = questionData.question,
+                options = questionData.option.map {
+                    QuestionnaireOptionUiModel(it.value)
+                },
+                showItemDivider = showItemDivider
         )
     }
 
@@ -176,29 +199,6 @@ class DeactivationViewModelTest {
         observeForever(observer)
         latch.await(2, TimeUnit.SECONDS)
         return value
-    }
-
-    private fun assertCancellationQuestionnaireData(
-            expected: Success<PMCancellationQuestionnaireData>,
-            actual: Success<PMCancellationQuestionnaireData>?
-    ) {
-        val expectedQuestionList = expected.data.listQuestion
-        val actualQuestionList = actual?.data?.listQuestion.orEmpty()
-
-        actualQuestionList.forEachIndexed { index, actualQuestion ->
-            val expectedQuestion = expectedQuestionList[index]
-
-            when {
-                actualQuestion is PMCancellationQuestionnaireRateModel &&
-                        expectedQuestion is PMCancellationQuestionnaireRateModel -> {
-                    assertRatingQuestionnaireData(expectedQuestion, actualQuestion)
-                }
-                actualQuestion is PMCancellationQuestionnaireMultipleOptionModel &&
-                        expectedQuestion is PMCancellationQuestionnaireMultipleOptionModel -> {
-                    assertMultipleOptionsQuestionnaireData(expectedQuestion, actualQuestion)
-                }
-            }
-        }
     }
 
     private fun assertRatingQuestionnaireData(
@@ -221,5 +221,29 @@ class DeactivationViewModelTest {
             Assert.assertEquals(expectedOption.value, actualOption.value)
             Assert.assertEquals(expectedOption.isChecked, actualOption.isChecked)
         }
+    }
+
+    private fun registerRxPlugins() {
+        RxJavaPlugins.getInstance().reset()
+        RxAndroidPlugins.getInstance().registerSchedulersHook(object : RxAndroidSchedulersHook() {
+            override fun getMainThreadScheduler(): Scheduler {
+                return Schedulers.immediate()
+            }
+        })
+        RxJavaPlugins.getInstance().registerSchedulersHook(object : RxJavaSchedulersHook() {
+            override fun getIOScheduler(): Scheduler {
+                return Schedulers.immediate()
+            }
+
+            override fun getNewThreadScheduler(): Scheduler {
+                return Schedulers.immediate()
+            }
+        })
+    }
+
+    private fun resetRxPlugins() {
+        @Suppress("UnstableApiUsage")
+        RxAndroidPlugins.getInstance().reset()
+        RxJavaPlugins.getInstance().reset()
     }
 }
--- features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/PowerMerchantSubscriptionViewModelTest.kt
+++ features/merchant/power_merchant_subscribe/src/test/java/com/tokopedia/power_merchant/subscribe/view/viewmodel/PowerMerchantSubscriptionViewModelTest.kt
@@ -1,9 +1,16 @@
 package com.tokopedia.power_merchant.subscribe.view.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
 /**
  * Created By @ilhamsuaib on 21/04/21
  */
 
 class PowerMerchantSubscriptionViewModelTest {
+
+    @get:Rule
+    val ruleForLivaData = InstantTaskExecutorRule()
+
 
 }
