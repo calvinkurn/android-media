5d246344962b2a1b46816e3483520765a2847e1c
shiperus <calvyn.julian@tokopedia.com>
2021-04-22 14:42:56 +0700

    move query string on ShopSearchProductModule.kt

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/search/di/module/ShopSearchProductModule.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/search/di/module/ShopSearchProductModule.kt
@@ -1,19 +1,15 @@
 package com.tokopedia.shop.search.di.module
 
 import android.content.Context
-import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
-import com.tokopedia.shop.R
 import com.tokopedia.shop.analytic.ShopPageTrackingShopSearchProduct
 import com.tokopedia.shop.common.di.ShopPageContext
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchersProvider
-import com.tokopedia.shop.search.ShopSearchProductConstant.UNIVERSE_SEARCH_QUERY
 import com.tokopedia.shop.search.di.scope.ShopSearchProductScope
 import com.tokopedia.trackingoptimizer.TrackingQueue
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
-import javax.inject.Named
 import dagger.Module
 import dagger.Provides
 
@@ -28,16 +24,6 @@ class ShopSearchProductModule {
     fun provideUserSessionInterface(
             @ShopPageContext context: Context
     ): UserSessionInterface = UserSession(context)
-
-    @ShopSearchProductScope
-    @Provides
-    @Named(UNIVERSE_SEARCH_QUERY)
-    fun provideUniverseSearchQuery(
-            @ShopPageContext context: Context
-    ): String = GraphqlHelper.loadRawString(
-            context.resources,
-            R.raw.gql_universe_search
-    )
 
     @ShopSearchProductScope
     @Provides
