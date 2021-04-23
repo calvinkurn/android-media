baeef8f22c712d63a25c51b4b0c5fcb02148872d
faisalramdani <faisal.ramdani@tokopedia.com>
2021-04-23 13:06:52 +0700

    using drawable end programmatically

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/productlimitation/presentation/adapter/ProductLimitationItemViewHolder.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/productlimitation/presentation/adapter/ProductLimitationItemViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.common.utils.image.ImageHandler.loadImageFitCenter
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.product.addedit.R
 import com.tokopedia.product.addedit.productlimitation.presentation.model.ProductLimitationActionItemModel
 import com.tokopedia.unifyprinciples.Typography
@@ -17,10 +18,12 @@ class ProductLimitationItemViewHolder(itemView: View) :
     private val ivCard: ImageView = itemView.findViewById(R.id.iv_card)
 
     fun bindData(data: ProductLimitationActionItemModel) {
+        val drawableIcon = MethodChecker.getDrawable(itemView.context, com.tokopedia.list.R.drawable.listitem_chevron)
         loadImageFitCenter(itemView.context, ivCard, data.imageUrl)
         tvTitle.text = data.title
         tvDescription.text = data.description
         tvAction.text = data.actionText
+        tvAction.setCompoundDrawablesWithIntrinsicBounds(null, null, drawableIcon, null)
     }
 
     fun setOnClickListener(itemOnClick: (Int) -> Unit) {
