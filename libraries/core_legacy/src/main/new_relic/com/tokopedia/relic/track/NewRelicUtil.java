386972bbd5a13e0ee43b7d5eb038cfdf6ff0f207
noiz354 <noiz354@gmail.com>
2021-04-26 05:29:46 +0700

    code review : 1. fix compilation

--- libraries/core_legacy/src/main/new_relic/com/tokopedia/relic/track/NewRelicUtil.java
+++ libraries/core_legacy/src/main/new_relic/com/tokopedia/relic/track/NewRelicUtil.java
@@ -4,7 +4,7 @@ import com.newrelic.agent.android.NewRelic;
 import java.util.Map;
 
 public class NewRelicUtil {
-    public static void sendTrack(String event, Map<String, Object> map) {
+    public static void sendTrack(String eventName, Map<String, Object> map) {
         NewRelic.recordBreadcrumb(eventName, map);
     }
 }
