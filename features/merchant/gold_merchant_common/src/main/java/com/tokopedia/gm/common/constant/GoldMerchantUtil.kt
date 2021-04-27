760b95b7c1cb4e84bf6983973c2b988c60e2fb84
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-04-27 15:10:01 +0700

    fix locale

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/constant/GoldMerchantUtil.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/constant/GoldMerchantUtil.kt
@@ -1,18 +1,16 @@
 package com.tokopedia.gm.common.constant
 
+import com.tokopedia.abstraction.common.utils.view.DateFormatUtils
 import com.tokopedia.kotlin.extensions.view.orZero
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.math.abs
-import kotlin.math.roundToInt
 
 object GoldMerchantUtil {
 
-    private const val LOCALE_ID = "id"
-
     infix fun String.diffDays(days: Int): Boolean {
-        val simpleDateFormat = SimpleDateFormat(PATTERN_DATE_SHOP_INFO, Locale(LOCALE_ID))
+        val simpleDateFormat = SimpleDateFormat(PATTERN_DATE_SHOP_INFO, DateFormatUtils.DEFAULT_LOCALE)
         val joinDate = simpleDateFormat.parse(this)
         val diffInMs: Long = abs(System.currentTimeMillis() - joinDate?.time.orZero())
         val diff = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS)
@@ -27,7 +25,7 @@ object GoldMerchantUtil {
     }
 
     fun String.isTenureNewSeller(): Boolean {
-        val simpleDateFormat = SimpleDateFormat(PATTERN_DATE_SHOP_INFO, Locale(LOCALE_ID))
+        val simpleDateFormat = SimpleDateFormat(PATTERN_DATE_SHOP_INFO, DateFormatUtils.DEFAULT_LOCALE)
         val joinDate = simpleDateFormat.parse(this)
         val diffInMs: Long = abs(System.currentTimeMillis() - joinDate?.time.orZero())
         val diff = TimeUnit.DAYS.convert(diffInMs, TimeUnit.MILLISECONDS)
