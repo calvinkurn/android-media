6a18895dfd7bf71e026efd0a7556a1264f7cfb1f
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-04-27 15:44:22 +0700

    add unit test shop performance

--- features/merchant/shop_score/src/test/java/com/tokopedia/shop/score/performance/presentation/viewmodel/ShopPerformanceViewModelTestFixture.kt
+++ features/merchant/shop_score/src/test/java/com/tokopedia/shop/score/performance/presentation/viewmodel/ShopPerformanceViewModelTestFixture.kt
@@ -1,15 +1,29 @@
 package com.tokopedia.shop.score.performance.presentation.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.gm.common.domain.interactor.GetShopInfoPeriodUseCase
+import com.tokopedia.gm.common.presentation.model.ShopInfoPeriodUiModel
 import com.tokopedia.shop.score.performance.domain.mapper.ShopScoreMapper
+import com.tokopedia.shop.score.performance.domain.model.ShopScoreWrapperResponse
 import com.tokopedia.shop.score.performance.domain.usecase.GetShopPerformanceUseCase
+import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.unit.test.rule.CoroutineTestRule
 import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Before
+import org.junit.Rule
 
 abstract class ShopPerformanceViewModelTestFixture {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineTestRule = CoroutineTestRule()
 
     @RelaxedMockK
     protected lateinit var getShopInfoPeriodUseCase: GetShopInfoPeriodUseCase
@@ -23,7 +37,6 @@ abstract class ShopPerformanceViewModelTestFixture {
     @RelaxedMockK
     protected lateinit var userSession: UserSessionInterface
 
-    @RelaxedMockK
     private lateinit var dispatchers: CoroutineDispatchers
 
     protected lateinit var shopPerformanceViewModel: ShopPerformanceViewModel
@@ -31,6 +44,7 @@ abstract class ShopPerformanceViewModelTestFixture {
     @Before
     fun setup() {
         MockKAnnotations.init(this)
+        dispatchers = CoroutineTestDispatchersProvider
         shopPerformanceViewModel = ShopPerformanceViewModel(
                 dispatchers,
                 shopScoreMapper,
@@ -39,4 +53,30 @@ abstract class ShopPerformanceViewModelTestFixture {
                 getShopPerformanceUseCase
         )
     }
+
+    protected fun onGetShopInfoPeriodUseCase_thenReturn(shopInfoPeriodUiModel: ShopInfoPeriodUiModel) {
+        coEvery { getShopInfoPeriodUseCase.executeOnBackground() } returns shopInfoPeriodUiModel
+    }
+
+    protected fun onGetShopInfoPeriodUseCaseError_thenReturn(exception: Throwable) {
+        coEvery { getShopInfoPeriodUseCase.executeOnBackground() } throws exception
+    }
+
+    protected fun verifyGetShopInfoPeriodUseCaseCalled() {
+        coVerify { getShopInfoPeriodUseCase.executeOnBackground() }
+    }
+
+    protected fun onGetShopPerformanceUseCase_thenReturn(shopScoreWrapperResponse: ShopScoreWrapperResponse) {
+        coEvery { getShopPerformanceUseCase.executeOnBackground() } returns shopScoreWrapperResponse
+    }
+
+    protected fun verifyGetShopPerformanceUseCaseCalled() {
+        coVerify { getShopPerformanceUseCase.executeOnBackground() }
+    }
+
+    protected fun onGetShopPerformanceUseCaseError_thenReturn(exception: Throwable) {
+        coEvery { getShopPerformanceUseCase.executeOnBackground() } throws exception
+    }
+
+
 }
