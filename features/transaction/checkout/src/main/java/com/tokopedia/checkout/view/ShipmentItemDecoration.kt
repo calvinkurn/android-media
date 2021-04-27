ec601753735a0d3e5e3400382453d2ff9cdf3c8e
irfan khoirul <irfankhoirul@gmail.com>
2021-04-27 12:02:26 +0700

    adjust code

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentItemDecoration.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentItemDecoration.kt
@@ -33,18 +33,18 @@ class ShipmentItemDecoration : RecyclerView.ItemDecoration() {
                 viewHolder is ShipmentButtonPaymentViewHolder -> {
                     outRect.bottom = it.resources.getDimension(R.dimen.dp_0).toInt()
                 }
-                viewHolder.adapterPosition == parent.adapter?.itemCount ?: 0 - LAST_POSITION -> {
+                viewHolder.adapterPosition == (parent.adapter?.itemCount ?: 0) - LAST_POSITION -> {
                     outRect.bottom = it.resources.getDimension(R.dimen.dp_14).toInt()
                 }
                 viewHolder is ShipmentCostViewHolder -> {
-                    if (viewHolder.getAdapterPosition() == parent.adapter?.itemCount ?: 0 - SECOND_LAST_POSITION) {
+                    if (viewHolder.adapterPosition == (parent.adapter?.itemCount ?: 0) - SECOND_LAST_POSITION) {
                         outRect.bottom = verticalSpaceHeight
                     } else {
                         outRect.bottom = it.resources.getDimension(R.dimen.dp_0).toInt()
                     }
                 }
                 viewHolder is ShipmentInsuranceTncViewHolder -> {
-                    if (viewHolder.getAdapterPosition() == parent.adapter?.itemCount ?: 0 - SECOND_LAST_POSITION) {
+                    if (viewHolder.adapterPosition == (parent.adapter?.itemCount ?: 0) - SECOND_LAST_POSITION) {
                         outRect.top = verticalSpaceHeight
                     } else {
                         outRect.top = it.resources.getDimension(R.dimen.dp_0).toInt()
