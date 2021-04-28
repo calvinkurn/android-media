c3e329b20c7e713599aee58a825fd94912364f7f
Jessica Sean <jessica.sean@tokopedia.com>
2021-04-28 17:28:35 +0700

    fix: tracking untick called twice

--- features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/presentation/adapter/vh/DigitalMyBillsViewHolder.kt
+++ features/recharge/digital_checkout/src/main/java/com/tokopedia/digital_checkout/presentation/adapter/vh/DigitalMyBillsViewHolder.kt
@@ -27,9 +27,6 @@ class DigitalMyBillsViewHolder(view: View, val listener: MyBillsActionListener) 
                 if (subscription.isChecked) widgetMyBills.setDescription(subscription.bodyContentAfter)
                 else widgetMyBills.setDescription(subscription.bodyContentBefore)
 
-                if (!widgetMyBills.isChecked() && subscription.isChecked) widgetMyBills.setChecked(true)
-                listener.onSubscriptionChecked(subscription, widgetMyBills.isChecked())
-
                 widgetMyBills.actionListener = object : DigitalCartMyBillsWidget.ActionListener {
                     override fun onMoreInfoClicked() {}
 
@@ -38,6 +35,10 @@ class DigitalMyBillsViewHolder(view: View, val listener: MyBillsActionListener) 
                         else widgetMyBills.setDescription(subscription.bodyContentBefore)
                         listener.onSubscriptionChecked(subscription, isChecked)
                     }
+                }
+
+                if (!widgetMyBills.isChecked() && subscription.isChecked) {
+                    widgetMyBills.setChecked(true)
                 }
             }
         }
@@ -55,17 +56,6 @@ class DigitalMyBillsViewHolder(view: View, val listener: MyBillsActionListener) 
                     listener.onTebusMurahImpression(fintechProduct, position)
                 }
 
-                widgetMyBills.visibility = View.VISIBLE
-
-                if (fintechProduct.checkBoxDisabled) {
-                    widgetMyBills.disableCheckBox()
-                } else {
-                    if (!widgetMyBills.isChecked()) {
-                        widgetMyBills.setChecked(fintechProduct.optIn)
-                    }
-                    listener.onFintechProductChecked(fintechProduct, widgetMyBills.isChecked())
-                }
-
                 widgetMyBills.actionListener = object : DigitalCartMyBillsWidget.ActionListener {
                     override fun onMoreInfoClicked() {
                         listener.onFintechMoreInfoChecked(fintechProduct.info)
@@ -77,6 +67,16 @@ class DigitalMyBillsViewHolder(view: View, val listener: MyBillsActionListener) 
                         } else {
                             listener.onFintechProductChecked(fintechProduct, isChecked)
                         }
+                    }
+                }
+
+                widgetMyBills.visibility = View.VISIBLE
+
+                if (fintechProduct.checkBoxDisabled) {
+                    widgetMyBills.disableCheckBox()
+                } else {
+                    if (!widgetMyBills.isChecked() && fintechProduct.optIn) {
+                        widgetMyBills.setChecked(fintechProduct.optIn)
                     }
                 }
             }
