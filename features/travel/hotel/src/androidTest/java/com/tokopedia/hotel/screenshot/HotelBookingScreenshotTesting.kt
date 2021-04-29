8d8df4e032528980129dc68ba65ec748c1404629
astidhiyaa <asti.anzaria@tokopedia.com>
2021-04-29 21:16:41 +0800

    sstesting: hotel booking

--- features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelBookingScreenshotTesting.kt
+++ features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelBookingScreenshotTesting.kt
@@ -5,7 +5,6 @@ import android.app.Instrumentation
 import android.content.Intent
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
-import android.widget.ScrollView
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
@@ -24,7 +23,7 @@ import org.junit.Test
 
 class HotelBookingScreenshotTesting {
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
-    lateinit var hotelBooking : ScrollView
+    lateinit var hotelBooking : RelativeLayout
 
     @get:Rule
     var activityRule: IntentsTestRule<HotelBookingActivity> = object : IntentsTestRule<HotelBookingActivity>(HotelBookingActivity::class.java) {
