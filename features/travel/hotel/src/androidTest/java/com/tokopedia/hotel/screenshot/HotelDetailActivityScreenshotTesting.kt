1561930cab0e963b9cfd3e3e611e089affc7fd2b
astidhiyaa <asti.anzaria@tokopedia.com>
2021-04-29 10:34:47 +0800

    sstesting = hotel detail

--- features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelDetailActivityScreenshotTesting.kt
+++ features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelDetailActivityScreenshotTesting.kt
@@ -3,10 +3,13 @@ package com.tokopedia.hotel.screenshot
 import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
+import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.material.appbar.AppBarLayout
 import com.tokopedia.hotel.hoteldetail.presentation.activity.HotelDetailActivity
 import com.tokopedia.hotel.hoteldetail.presentation.activity.mock.HotelDetailMockResponseConfig
 import com.tokopedia.test.application.espresso_component.CommonActions
@@ -14,9 +17,12 @@ import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import com.tokopedia.hotel.R
 
 class HotelDetailActivityScreenshotTesting {
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    lateinit var appBar: AppBarLayout
+    lateinit var hotelDetail: NestedScrollView
 
     @get:Rule
     var activityRule: IntentsTestRule<HotelDetailActivity> = object : IntentsTestRule<HotelDetailActivity>(HotelDetailActivity::class.java) {
@@ -36,6 +42,8 @@ class HotelDetailActivityScreenshotTesting {
     @Before
     fun setUp() {
         Intents.intending(IntentMatchers.anyIntent()).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
+        appBar = activityRule.activity.findViewById(R.id.app_bar_layout)
+        hotelDetail = activityRule.activity.findViewById(R.id.hotelDetailNestedScrollView)
     }
 
     @Test
@@ -47,7 +55,37 @@ class HotelDetailActivityScreenshotTesting {
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             CommonActions.takeScreenShotVisibleViewInScreen(activity.window.decorView, filePrefix(), "top")
         }
+
+        CommonActions.findViewAndScreenShot(R.id.container_bottom, filePrefix(), "bottom-widget")
+
+        activityRule.runOnUiThread {
+            scrollToMiddle()
+        }
+
+        CommonActions.findViewAndScreenShot(R.id.container_hotel_review, filePrefix(), "review")
+        CommonActions.findViewAndScreenShot(R.id.container_hotel_address, filePrefix(), "address")
+
+        activityRule.runOnUiThread {
+            scrollToBottom()
+        }
+
+        CommonActions.findViewAndScreenShot(R.id.container_facilities, filePrefix(), "facilities")
+        CommonActions.findViewAndScreenShot(R.id.container_hotel_info, filePrefix(), "info")
+        CommonActions.findViewAndScreenShot(R.id.container_important_info, filePrefix(), "important-info")
+        CommonActions.findViewAndScreenShot(R.id.container_hotel_description, filePrefix(), "description")
     }
 
     private fun filePrefix(): String = "hotel-detail"
+
+    private fun scrollToMiddle(){
+        val midPoint = activityRule.activity.findViewById<LinearLayout>(R.id.container_hotel_review)
+        appBar.setExpanded(false)
+        hotelDetail.scrollTo(0, midPoint.y.toInt())
+    }
+
+    private fun scrollToBottom(){
+        val bottomPoint = activityRule.activity.findViewById<LinearLayout>(R.id.container_facilities)
+        appBar.setExpanded(false)
+        hotelDetail.scrollTo(0, bottomPoint.y.toInt())
+    }
 }
