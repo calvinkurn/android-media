e13c9f3b05cbec594082b31e1325c2a7145b92d5
astidhiyaa <asti.anzaria@tokopedia.com>
2021-04-29 16:53:50 +0800

    comment

--- features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelRoomDetailScreenshotTesting.kt
+++ features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/HotelRoomDetailScreenshotTesting.kt
@@ -11,32 +11,32 @@ import org.junit.Test
 import com.tokopedia.hotel.R
 
 class HotelRoomDetailScreenshotTesting {
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
-    lateinit var appBar: AppBarLayout
-    lateinit var roomDetail: NestedScrollView
-
-    @get:Rule
-    var activityRule: IntentsTestRule<HotelRoomDetailActivity> =
-            object : IntentsTestRule<HotelRoomDetailActivity>(HotelRoomDetailActivity::class.java) {
-
-                override fun beforeActivityLaunched() {
-                    super.beforeActivityLaunched()
-                }
-
-                override fun getActivityIntent(): Intent {
-                    return HotelRoomDetailActivity.getCallingIntent(context, "1619672690985", 0)
-                }
-            }
-
-    @Test
-    fun screenShot(){
-
-        activityRule.activity.finishAndRemoveTask()
-    }
-
-    private fun scrollToMiddle(){
-
-    }
-
-    private fun filePrefix(): String = "hotel-room-detail"
+//    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+//    lateinit var appBar: AppBarLayout
+//    lateinit var roomDetail: NestedScrollView
+//
+//    @get:Rule
+//    var activityRule: IntentsTestRule<HotelRoomDetailActivity> =
+//            object : IntentsTestRule<HotelRoomDetailActivity>(HotelRoomDetailActivity::class.java) {
+//
+//                override fun beforeActivityLaunched() {
+//                    super.beforeActivityLaunched()
+//                }
+//
+//                override fun getActivityIntent(): Intent {
+//                    return HotelRoomDetailActivity.getCallingIntent(context, "1619672690985", 0)
+//                }
+//            }
+//
+//    @Test
+//    fun screenShot(){
+//
+//        activityRule.activity.finishAndRemoveTask()
+//    }
+//
+//    private fun scrollToMiddle(){
+//
+//    }
+//
+//    private fun filePrefix(): String = "hotel-room-detail"
 }
