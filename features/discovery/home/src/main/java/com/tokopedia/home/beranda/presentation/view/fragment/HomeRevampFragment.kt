81b3a781259e7c60be19c03a0d7e1613bb3d56c6
yoasfs <yoas.susantio@tokopedia.com>
2021-04-29 19:46:45 +0700

    fix error

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeRevampFragment.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeRevampFragment.kt
@@ -2672,7 +2672,6 @@ open class HomeRevampFragment : BaseDaggerFragment(),
             DynamicChannelViewHolder.TYPE_GIF_BANNER -> putEEToIris(
                     HomePageTracking.getEnhanceImpressionPromoGifBannerDC(channel))
             DynamicChannelViewHolder.TYPE_RECOMMENDATION_LIST -> putEEToIris(RecommendationListTracking.getRecommendationListImpression(channel, true, viewModel.get().getUserId(), position) as HashMap<String, Any>)
-            ) as HashMap<String, Any>)
         }
     }
 
