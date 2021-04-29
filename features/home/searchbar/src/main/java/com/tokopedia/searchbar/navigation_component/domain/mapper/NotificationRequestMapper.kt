3ec7c78db0f7d6510e4e60458b4f37903fd31165
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-04-29 14:21:59 +0700

    - fix notifcation counter showing when user has no shop - fix notification position

--- features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/domain/mapper/NotificationRequestMapper.kt
+++ features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/domain/mapper/NotificationRequestMapper.kt
@@ -10,15 +10,19 @@ import com.tokopedia.searchbar.navigation_component.util.IntegerUtil
 object NotificationRequestMapper {
     const val NOTIF_99 = "99+"
     const val NOTIF_99_NUMBER = 99
+    const val DEFAULT_SHOP_ID_NOT_OPEN = "-1"
+    const val DEFAULT_TOTAL_ORDER_COUNT = 0
 
     fun mapToNotificationModel(notificationResponse: NotificationResponse): TopNavNotificationModel {
+        val hasShop = notificationResponse.userShopInfo.info.shopId != DEFAULT_SHOP_ID_NOT_OPEN
+
         val notification = notificationResponse.notifications
         val totalNewInbox = notification.inboxCounter.all.totalInt
         val totalCart = notification.totalCart
         val totalInbox = buildTotalInbox(notification)
         val totalNotif = buildTotalNotif(notificationResponse)
 
-        val totalSellerOrderCount = buildTotalOrderCount(notificationResponse)
+        val totalSellerOrderCount = buildTotalOrderCount(hasShop, notificationResponse)
         val totalReviewCount = notification.inbox.review
         val totalInboxTicket = notification.inbox.ticket
         val totalUnreadComplain = notification.resolutionAs.buyer
@@ -34,10 +38,12 @@ object NotificationRequestMapper {
         )
     }
 
-    private fun buildTotalOrderCount(data: NotificationResponse): Int {
-        return data.notifications.sellerOrderStatus.newOrder
-                .plus(data.notifications.sellerOrderStatus.readyToShip)
-                .plus(data.notifications.sellerOrderStatus.inResolution)
+    private fun buildTotalOrderCount(hasShop: Boolean, data: NotificationResponse): Int {
+        return if (hasShop) {
+            data.notifications.sellerOrderStatus.newOrder
+                    .plus(data.notifications.sellerOrderStatus.readyToShip)
+                    .plus(data.notifications.sellerOrderStatus.inResolution)
+        } else DEFAULT_TOTAL_ORDER_COUNT
     }
 
     private fun buildTotalNotif(data: NotificationResponse): Int {
