4a75fd2c88ffa420ce3bab0ce1400b8ddb21fd5c
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-04-29 14:07:45 +0700

    fix crash checkout & add accordion to customer_mid

--- features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/fragment/SmartBillsFragment.kt
+++ features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/fragment/SmartBillsFragment.kt
@@ -496,9 +496,9 @@ class SmartBillsFragment : BaseListFragment<RechargeBillsModel, SmartBillsAdapte
 
     override fun onClickNextBuyButton() {
         // Prevent multiple checkout calls when one is already underway
-        if (adapter.checkedDataList.isNotEmpty() && !checkout_loading_view.isVisible) {
+        if (listBills.isNotEmpty() && !checkout_loading_view.isVisible) {
             // Reset error in bill items
-            for ((index, bill) in adapter.data.withIndex()) {
+            for ((index, bill) in listBills.withIndex()) {
                 if (bill.errorMessage.isNotEmpty()) {
                     bill.errorMessage = ""
                     adapter.data[index] = bill
