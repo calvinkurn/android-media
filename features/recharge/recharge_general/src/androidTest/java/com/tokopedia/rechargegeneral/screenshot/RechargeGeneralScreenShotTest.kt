a7446d142fbf1d9fd4f7d8a607b735126fa60ae4
misael-jonathan <misael.jonathan@tokopedia.com>
2021-05-03 16:51:05 +0700

    trial screenshot test

--- features/recharge/recharge_general/src/androidTest/java/com/tokopedia/rechargegeneral/screenshot/RechargeGeneralScreenShotTest.kt
+++ features/recharge/recharge_general/src/androidTest/java/com/tokopedia/rechargegeneral/screenshot/RechargeGeneralScreenShotTest.kt
@@ -1,12 +1,19 @@
 package com.tokopedia.rechargegeneral.screenshot
 
 import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.platform.util.TestOutputEmitter.takeScreenshot
 import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.abstraction.base.view.widget.SwipeToRefresh
+import com.tokopedia.rechargegeneral.R
 import com.tokopedia.rechargegeneral.RechargeGeneralMockResponseConfig
 import com.tokopedia.rechargegeneral.cases.RechargeGeneralProduct
 import com.tokopedia.rechargegeneral.presentation.activity.RechargeGeneralActivity
+import com.tokopedia.rechargegeneral.presentation.fragment.RechargeGeneralFragment
+import com.tokopedia.test.application.espresso_component.CommonActions.findViewAndScreenShot
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import com.tokopedia.test.application.espresso_component.CommonActions.takeScreenShotVisibleViewInScreen
 import org.junit.Rule
@@ -32,7 +39,25 @@ class RechargeGeneralScreenShotTest {
 
     @Test
     fun screenshot() {
-        takeScreenshot("general_template.png")
+        val test = mActivityRule.activity.findViewById<SwipeToRefresh>(R.id.recharge_general_swipe_refresh_layout)
+        takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_pdp")
+        takeScreenShotVisibleViewInScreen(test, filePrefix(), "swipe_to_refresh")
+        takeScreenshot("test.png")
+
+        findViewAndScreenShot(R.id.product_view_pager, filePrefix(), "view_pager")
+
+        onView(withId(R.id.operator_select)).perform(ViewActions.click())
+        takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_operator")
+
     }
+
+//    private fun getPositionViewHolderByName(name: String): Int {
+//        val fragment = mActivityRule.activity.supportFragmentManager.findFragmentByTag(PRODUCT_DETAIL_TAG) as RechargeGeneralFragment
+//        return fragment.productAdapter?.currentList?.indexOfFirst {
+//            it.name() == name
+//        } ?: 0
+//    }
+
+    private fun filePrefix() = "recharge_general_"
 
 }
