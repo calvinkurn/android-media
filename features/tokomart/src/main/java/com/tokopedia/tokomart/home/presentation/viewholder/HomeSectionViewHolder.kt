a1cb338393855fe804dac31ae09d57fff7cfe7ab
rezagama <reza.hidayat@tokopedia.com>
2021-05-05 11:45:51 +0700

    Make tokomart as module
    - Convert home to sub-package

--- features/tokomart/src/main/java/com/tokopedia/tokomart/home/presentation/viewholder/HomeSectionViewHolder.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/home/presentation/viewholder/HomeSectionViewHolder.kt
@@ -3,7 +3,7 @@ package com.tokopedia.tokomart.home.presentation.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.tokomart.home.R
+import com.tokopedia.tokomart.R
 import com.tokopedia.tokomart.home.presentation.uimodel.HomeSectionUiModel
 import kotlinx.android.synthetic.main.item_tokomart_home_section.view.*
 
