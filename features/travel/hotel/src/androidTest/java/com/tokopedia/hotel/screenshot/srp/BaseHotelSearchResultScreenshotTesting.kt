b270fe5de7a34f716692dee58f54286b8f6c26cb
astidhiyaa <asti.anzaria@tokopedia.com>
2021-05-06 16:51:26 +0800

    [hotel old srp] all flow

--- features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/srp/BaseHotelSearchResultScreenshotTesting.kt
+++ features/travel/hotel/src/androidTest/java/com/tokopedia/hotel/screenshot/srp/BaseHotelSearchResultScreenshotTesting.kt
@@ -1,11 +1,19 @@
 package com.tokopedia.hotel.screenshot.srp
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.abstraction.common.utils.LocalCacheHandler
 import com.tokopedia.hotel.R
+import com.tokopedia.hotel.destination.view.activity.HotelDestinationActivity
 import com.tokopedia.hotel.search.data.model.HotelSearchModel
 import com.tokopedia.hotel.search.presentation.activity.HotelSearchResultActivity
 import com.tokopedia.hotel.search.presentation.activity.mock.HotelSearchMockResponseConfig
@@ -13,6 +21,7 @@ import com.tokopedia.hotel.search.presentation.fragment.HotelSearchResultFragmen
 import com.tokopedia.test.application.espresso_component.CommonActions
 import com.tokopedia.test.application.util.setupDarkModeTest
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
+import org.hamcrest.core.AllOf
 import org.junit.Rule
 import org.junit.Test
 
@@ -60,6 +69,21 @@ abstract class BaseHotelSearchResultScreenshotTesting {
                 getHotelResultCount()- 1,
                 "${filePrefix()}-full")
         CommonActions.findViewHolderAndScreenshot(R.id.recycler_view, 0, filePrefix(), "item-hotel")
+
+        //SS testing for filter
+        Espresso.onView(AllOf.allOf(ViewMatchers.withText("Filter"))).perform(ViewActions.click())
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            CommonActions.takeScreenShotVisibleViewInScreen(activityRule.activity.window.decorView, filePrefix(), "filter-bs-page")
+        }
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_sheet_close)).perform(ViewActions.click())
+
+        //SS testing for "Ubah"
+        Espresso.onView(ViewMatchers.withId(R.id.rightContentID)).perform(ViewActions.click())
+        Intents.intending(IntentMatchers.hasComponent(HotelDestinationActivity::class.java.name)).respondWith(createDummyDestination())
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            CommonActions.takeScreenShotVisibleViewInScreen(activityRule.activity.window.decorView, filePrefix(), "ubah-page")
+        }
+
         activityRule.activity.finishAndRemoveTask()
     }
 
@@ -70,5 +94,13 @@ abstract class BaseHotelSearchResultScreenshotTesting {
     private fun getHotelResultCount(): Int {
         val recyclerView: RecyclerView = activityRule.activity.findViewById(R.id.recycler_view) as RecyclerView
         return recyclerView.adapter?.itemCount ?: 0
+    }
+
+    private fun createDummyDestination(): Instrumentation.ActivityResult {
+        val resultData = Intent()
+        resultData.putExtra(HotelDestinationActivity.HOTEL_DESTINATION_NAME, "Jakarta")
+        resultData.putExtra(HotelDestinationActivity.HOTEL_DESTINATION_SEARCH_TYPE, "regionOrigin")
+        resultData.putExtra(HotelDestinationActivity.HOTEL_DESTINATION_SEARCH_ID, "835")
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
     }
 }
