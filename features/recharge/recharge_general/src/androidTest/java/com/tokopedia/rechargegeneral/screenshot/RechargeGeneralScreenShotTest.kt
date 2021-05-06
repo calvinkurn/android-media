75661025b5f33324ae39a742c6a6b6590cff6e46
misael-jonathan <misael.jonathan@tokopedia.com>
2021-05-06 13:46:19 +0700

    trial

--- features/recharge/recharge_general/src/androidTest/java/com/tokopedia/rechargegeneral/screenshot/RechargeGeneralScreenShotTest.kt
+++ features/recharge/recharge_general/src/androidTest/java/com/tokopedia/rechargegeneral/screenshot/RechargeGeneralScreenShotTest.kt
@@ -3,6 +3,7 @@ package com.tokopedia.rechargegeneral.screenshot
 import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
@@ -17,12 +18,15 @@ import androidx.test.internal.platform.util.TestOutputEmitter.takeScreenshot
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.abstraction.base.view.widget.SwipeToRefresh
+import com.tokopedia.common.topupbills.view.adapter.TopupBillsPromoListAdapter
 import com.tokopedia.common.topupbills.widget.TopupBillsInputDropdownWidget
 import com.tokopedia.common.topupbills.widget.TopupBillsInputFieldWidget
 import com.tokopedia.rechargegeneral.R
+import com.tokopedia.rechargegeneral.RechargeGeneralLoginMockResponseConfig
 import com.tokopedia.rechargegeneral.RechargeGeneralMockResponseConfig
 import com.tokopedia.rechargegeneral.cases.RechargeGeneralProduct
 import com.tokopedia.rechargegeneral.presentation.activity.RechargeGeneralActivity
+import com.tokopedia.rechargegeneral.presentation.adapter.viewholder.RechargeGeneralInputViewHolder
 import com.tokopedia.rechargegeneral.presentation.adapter.viewholder.RechargeGeneralProductSelectViewHolder
 import com.tokopedia.rechargegeneral.presentation.fragment.RechargeGeneralFragment
 import com.tokopedia.rechargegeneral.widget.RechargeGeneralProductSelectBottomSheet
@@ -31,6 +35,7 @@ import com.tokopedia.test.application.espresso_component.CommonActions.screenSho
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import com.tokopedia.test.application.espresso_component.CommonActions.takeScreenShotVisibleViewInScreen
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
+import org.hamcrest.core.AllOf
 import org.hamcrest.core.IsNot
 import org.junit.Before
 import org.junit.Rule
@@ -39,29 +44,27 @@ import org.junit.Test
 class RechargeGeneralScreenShotTest {
 
     @get:Rule
-    var mActivityRule: IntentsTestRule<RechargeGeneralActivity> = object : IntentsTestRule<RechargeGeneralActivity>(RechargeGeneralActivity::class.java) {
-        override fun getActivityIntent(): Intent {
-            val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
-            return Intent(targetContext, RechargeGeneralActivity::class.java).apply {
-                putExtra(RechargeGeneralActivity.PARAM_MENU_ID, 113)
-                putExtra(RechargeGeneralActivity.PARAM_CATEGORY_ID, 3)
-            }
-        }
-
-        override fun beforeActivityLaunched() {
-            super.beforeActivityLaunched()
-            setupGraphqlMockResponse(RechargeGeneralMockResponseConfig(RechargeGeneralProduct.LISTRIK))
-            InstrumentationAuthHelper.loginInstrumentationTestUser1()
-        }
-    }
+    var mActivityRule = ActivityTestRule(RechargeGeneralActivity::class.java, false, false)
 
     @Before
     fun stubAllExternalIntents() {
+        Intents.init()
+        setupGraphqlMockResponse(RechargeGeneralLoginMockResponseConfig(RechargeGeneralProduct.LISTRIK))
+
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val intent = Intent(targetContext, RechargeGeneralActivity::class.java).apply {
+            putExtra(RechargeGeneralActivity.PARAM_MENU_ID, 113)
+            putExtra(RechargeGeneralActivity.PARAM_CATEGORY_ID, 3)
+        }
+        mActivityRule.launchActivity(intent)
+        InstrumentationAuthHelper.loginInstrumentationTestUser1()
         Intents.intending(IsNot.not(IntentMatchers.isInternal())).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
     }
 
     @Test
     fun screenshot() {
+        Thread.sleep(3000)
+
         // ss visible screen
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_pdp")
@@ -73,15 +76,7 @@ class RechargeGeneralScreenShotTest {
             takeScreenShotVisibleViewInScreen(test, filePrefix(), "swipe_to_refresh")
         }
 
-        // test
-        takeScreenshot("test.png")
-
-        // ss operator cluster select::: no Cluster in Product Listrik
-//        val operatorClusterView = mActivityRule.activity.findViewById<TopupBillsInputFieldWidget>(R.id.operator_cluster_select)
-//        takeScreenShotVisibleViewInScreen(operatorClusterView, filePrefix(), "operator_cluster_select")
-
         // ss operator select
-
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             val operatorView = mActivityRule.activity.findViewById<TopupBillsInputFieldWidget>(R.id.operator_select)
             takeScreenShotVisibleViewInScreen(operatorView, filePrefix(), "operator_select")
@@ -95,6 +90,7 @@ class RechargeGeneralScreenShotTest {
                 "rv_digital_product")
 
         select_operator()
+        select_phone_number()
         select_product()
         see_promo()
     }
@@ -103,6 +99,7 @@ class RechargeGeneralScreenShotTest {
         // Click "Jenis Produk Listrik"
         onView(withId(R.id.operator_select)).perform(ViewActions.click())
 
+        Thread.sleep(2000)
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "recycler_view_operator_select")
         }
@@ -114,10 +111,27 @@ class RechargeGeneralScreenShotTest {
                         0, ViewActions.click()
                 )
         )
+    }
+
+    private fun select_phone_number() {
+        onView(withId(R.id.rv_digital_product)).check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(
+                RecyclerViewActions.actionOnItemAtPosition<RechargeGeneralInputViewHolder>(
+                        0, ViewActions.click()
+                )
+        )
 
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
-            takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_pdp_2")
+            takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "favorite_number")
         }
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            val frameLayout = mActivityRule.activity.findViewById<FrameLayout>(R.id.parent_view)
+            takeScreenShotVisibleViewInScreen(frameLayout, filePrefix(), "favorite_number")
+        }
+
+        Thread.sleep(1000)
+        onView(ViewMatchers.withText("08121111111"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+                .perform(ViewActions.click())
     }
 
     private fun select_product() {
@@ -128,10 +142,11 @@ class RechargeGeneralScreenShotTest {
                         1, ViewActions.click()
                 )
         )
+        Thread.sleep(2000)
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
             takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "recycler_view_product_item")
         }
-        Thread.sleep(2000)
+
         onView(withId(R.id.rv_product_select_dropdown)).check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(
                 RecyclerViewActions.actionOnItemAtPosition<RechargeGeneralProductSelectBottomSheet.DigitalProductSelectDropdownAdapter.DigitalProductSelectDropdownViewHolder>(
                         1, ViewActions.click()
@@ -139,12 +154,13 @@ class RechargeGeneralScreenShotTest {
         )
 
         InstrumentationRegistry.getInstrumentation().runOnMainSync {
-            takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_pdp_3")
+            takeScreenShotVisibleViewInScreen(mActivityRule.activity.window.decorView, filePrefix(), "visible_screen_pdp_2")
         }
     }
 
     private fun see_promo() {
         findViewAndScreenShot(R.id.product_view_pager, filePrefix(), "view_pager")
+        findViewAndScreenShot(R.id.tab_layout, filePrefix(), "view_pager")
     }
 
     private fun filePrefix() = "recharge_general"
