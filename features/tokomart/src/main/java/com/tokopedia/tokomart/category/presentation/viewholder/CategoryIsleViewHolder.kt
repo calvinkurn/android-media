87270f12dbe7e633604e737905cc5a3a067cc455
darianthedytoped <darian.thedy@tokopedia.com>
2021-05-07 16:31:13 +0700

    Fix layout

--- features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryIsleViewHolder.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryIsleViewHolder.kt
@@ -3,13 +3,14 @@ package com.tokopedia.tokomart.category.presentation.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.tokomart.R
 import com.tokopedia.tokomart.category.presentation.model.CategoryIsleDataView
 
 class CategoryIsleViewHolder(itemView: View): AbstractViewHolder<CategoryIsleDataView>(itemView) {
 
     companion object {
         @LayoutRes
-        val LAYOUT = 0
+        val LAYOUT = R.layout.item_tokomart_category_isle
     }
 
     override fun bind(element: CategoryIsleDataView?) {
