fcbf790bc3e12d34164e5c47fd2df8510ec6476e
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-05-07 16:32:59 +0700

    singleton remote config instance

--- libraries/applink/src/main/java/com/tokopedia/applink/FirebaseRemoteConfigInstance.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/FirebaseRemoteConfigInstance.kt
@@ -4,18 +4,16 @@ import android.content.Context
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 import com.tokopedia.remoteconfig.RemoteConfig
 
-internal class FirebaseRemoteConfigInstance {
-    companion object {
-        var remoteConfig: RemoteConfig? = null
-        fun get(context: Context): RemoteConfig {
-            val rc = remoteConfig
-            return if (rc == null) {
-                val newRc = FirebaseRemoteConfigImpl(context)
-                remoteConfig = newRc
-                newRc
-            } else {
-                rc
-            }
+internal object FirebaseRemoteConfigInstance {
+    var remoteConfig: RemoteConfig? = null
+    fun get(context: Context): RemoteConfig {
+        val rc = remoteConfig
+        return if (rc == null) {
+            val newRc = FirebaseRemoteConfigImpl(context)
+            remoteConfig = newRc
+            newRc
+        } else {
+            rc
         }
     }
 }
