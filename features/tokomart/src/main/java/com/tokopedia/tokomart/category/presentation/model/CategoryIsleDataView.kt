a2e3aa86499de62bd509c73f29ae2bcf1ee1352f
misael-jonathan <misael.jonathan@tokopedia.com>
2021-05-10 17:16:25 +0700

    add aisle placeholder

--- features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/model/CategoryIsleDataView.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/model/CategoryIsleDataView.kt
@@ -3,7 +3,10 @@ package com.tokopedia.tokomart.category.presentation.model
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.tokomart.category.presentation.typefactory.CategoryTypeFactory
 
-class CategoryIsleDataView: Visitable<CategoryTypeFactory> {
+class CategoryIsleDataView(
+        val name: String = "",
+        val imgUrl: String = ""
+): Visitable<CategoryTypeFactory> {
 
     override fun type(typeFactory: CategoryTypeFactory?) =
             typeFactory?.type(this) ?: 0
--- features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryIsleViewHolder.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryIsleViewHolder.kt
@@ -2,9 +2,14 @@ package com.tokopedia.tokomart.category.presentation.viewholder
 
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.tokomart.R
 import com.tokopedia.tokomart.category.presentation.model.CategoryIsleDataView
+import com.tokopedia.tokomart.category.presentation.view.CategoryIsleAdapter
+import kotlinx.android.synthetic.main.item_tokomart_category_isle.view.*
 
 class CategoryIsleViewHolder(itemView: View): AbstractViewHolder<CategoryIsleDataView>(itemView) {
 
@@ -14,6 +19,13 @@ class CategoryIsleViewHolder(itemView: View): AbstractViewHolder<CategoryIsleDat
     }
 
     override fun bind(element: CategoryIsleDataView?) {
-
+        val dummyData = listOf(
+                CategoryIsleDataView("Daging & Seafood"),
+                CategoryIsleDataView("Makanan Kering")
+        )
+        val adapter = CategoryIsleAdapter(dummyData)
+        itemView.rv_category_isle.setHasFixedSize(true)
+        itemView.rv_category_isle.layoutManager = GridLayoutManager(itemView.context, 2)
+        itemView.rv_category_isle.adapter = adapter
     }
 }
