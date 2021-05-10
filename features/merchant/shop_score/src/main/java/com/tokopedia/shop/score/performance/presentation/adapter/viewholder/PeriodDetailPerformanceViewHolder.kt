b1d61a6d922149ef16a2a2b7e28dbcda56ca1cb3
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2021-05-10 11:37:39 +0700

    fix period date new seller

--- features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/PeriodDetailPerformanceViewHolder.kt
+++ features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/PeriodDetailPerformanceViewHolder.kt
@@ -4,6 +4,9 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.shop.score.R
 import com.tokopedia.shop.score.common.rangeTotalDays
 import com.tokopedia.shop.score.performance.presentation.model.PeriodDetailPerformanceUiModel
@@ -25,12 +28,14 @@ class PeriodDetailPerformanceViewHolder(view: View) :
             if (element?.isNewSeller == true) {
                 val totalRangeDays = rangeTotalDays(element.period).toString()
                 tvPerformanceDetailLabel?.text = getString(R.string.title_detail_performance_new_seller)
-                tvPerformanceDetailDate?.text = MethodChecker.fromHtml(
-                        String.format(getString(R.string.title_update_date_new_seller), element.period, totalRangeDays, element.nextUpdate))
+                tvPerformanceDetailDate?.text = getString(R.string.title_update_date_new_seller, totalRangeDays)
+                tvPerformanceDetailDateNewSeller?.text = getString(R.string.title_update_date, element.nextUpdate)
             } else {
                 tvPerformanceDetailLabel?.text = getString(R.string.title_detail_performa, element?.period.orEmpty())
                 tvPerformanceDetailDate?.text = getString(R.string.title_update_date, element?.nextUpdate.orEmpty())
             }
+
+            tvPerformanceDetailDateNewSeller?.showWithCondition(element?.isNewSeller == true)
         }
     }
 }
