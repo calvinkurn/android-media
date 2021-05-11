d560bbf73ad782802d8d325080a69912a52eb137
rezagama <reza.hidayat@tokopedia.com>
2021-05-11 21:03:16 +0700

    Integrate home global component

--- features/tokomart/src/main/java/com/tokopedia/tokomart/home/domain/mapper/HomeLayoutMapper.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/home/domain/mapper/HomeLayoutMapper.kt
@@ -1,25 +1,19 @@
 package com.tokopedia.tokomart.home.domain.mapper
 
-import com.tokopedia.tokomart.home.presentation.uimodel.HomeLayoutUiModel
-import com.tokopedia.tokomart.home.constant.HomeLayoutType
+import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.tokomart.home.domain.model.HomeLayoutResponse
 import com.tokopedia.tokomart.home.presentation.uimodel.HomeAllCategoryUiModel
 import com.tokopedia.tokomart.home.presentation.uimodel.HomeSectionUiModel
 
 object HomeLayoutMapper {
 
-    private val SUPPORTED_TYPE = listOf(
-        HomeLayoutType.SECTION,
-        HomeLayoutType.ALL_CATEGORY,
-        HomeLayoutType.DYNAMIC_CHANNEL
-    )
-
-    fun mapToHomeUiModel(response: List<HomeLayoutResponse>): List<HomeLayoutUiModel> {
-        return response.filter { SUPPORTED_TYPE.contains(it.type) }.map {
-            when(it.type) {
-                HomeLayoutType.ALL_CATEGORY -> HomeAllCategoryUiModel(it.id, it.title)
-                else -> HomeSectionUiModel(it.id, it.title)
-            }
+    fun mapToHomeUiModel(response: List<HomeLayoutResponse>): List<Visitable<*>> {
+        // Temp Dummy Data
+        return response.run {
+            listOf(
+                HomeAllCategoryUiModel("1", "Kategori"),
+                HomeSectionUiModel("2", "Another section")
+            )
         }
     }
 }
--- features/tokomart/src/main/java/com/tokopedia/tokomart/home/presentation/uimodel/HomeAllCategoryUiModel.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/home/presentation/uimodel/HomeAllCategoryUiModel.kt
@@ -5,7 +5,7 @@ import com.tokopedia.tokomart.home.presentation.adapter.TokoMartHomeTypeFactory
 data class HomeAllCategoryUiModel(
     val id: String,
     val title: String
-): HomeLayoutUiModel {
+): TokoMartHomeLayoutUiModel(id) {
     override fun type(typeFactory: TokoMartHomeTypeFactory): Int {
         return typeFactory.type(this)
     }
