10fe49a814051850c712b9b8f6dc124496995681
irfan khoirul <irfankhoirul@gmail.com>
2021-05-17 08:57:40 +0700

    update data and mapper for mini cart simplified

--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/common/domain/data/MiniCartSimplifiedData.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/common/domain/data/MiniCartSimplifiedData.kt
@@ -1,6 +1,17 @@
 package com.tokopedia.minicart.common.domain.data
 
 data class MiniCartSimplifiedData(
+        var miniCartWidgetData: MiniCartWidgetData = MiniCartWidgetData(),
+        var miniCartItems: List<MiniCartItem> = emptyList()
+)
+
+data class MiniCartWidgetData(
+        var totalProductCount: Int = 0,
+        var totalProductError: Int = 0,
+        var totalProductPrice: Long = 0,
+)
+
+data class MiniCartItem(
         var isError: Boolean = false,
         var cartId: String = "",
         var productId: String = "",
--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/common/domain/mapper/MiniCartSimplifiedMapper.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/common/domain/mapper/MiniCartSimplifiedMapper.kt
@@ -1,25 +1,43 @@
 package com.tokopedia.minicart.common.domain.mapper
 
+import com.tokopedia.minicart.common.data.response.minicartlistsimplified.Data
 import com.tokopedia.minicart.common.data.response.minicartlistsimplified.MiniCartData
+import com.tokopedia.minicart.common.domain.data.MiniCartItem
 import com.tokopedia.minicart.common.domain.data.MiniCartSimplifiedData
+import com.tokopedia.minicart.common.domain.data.MiniCartWidgetData
 import javax.inject.Inject
 
 class MiniCartSimplifiedMapper @Inject constructor() {
 
-    fun mapMiniCartSimplifiedData(miniCartData: MiniCartData): List<MiniCartSimplifiedData> {
-        val miniCartSimplifiedDataList = mutableListOf<MiniCartSimplifiedData>()
+    fun mapMiniCartSimplifiedData(miniCartData: MiniCartData): MiniCartSimplifiedData {
+        return MiniCartSimplifiedData().apply {
+            miniCartWidgetData = mapMiniCartWidgetData(miniCartData.data)
+            miniCartItems = mapMiniCartListData(miniCartData)
+        }
+    }
+
+    private fun mapMiniCartWidgetData(data: Data): MiniCartWidgetData {
+        return MiniCartWidgetData().apply {
+            totalProductCount = data.totalProductCount
+            totalProductError = data.totalProductError
+            totalProductPrice = data.totalProductPrice
+        }
+    }
+
+    private fun mapMiniCartListData(miniCartData: MiniCartData): List<MiniCartItem> {
+        val miniCartSimplifiedDataList = mutableListOf<MiniCartItem>()
         miniCartSimplifiedDataList.addAll(getAvailableData(miniCartData))
         miniCartSimplifiedDataList.addAll(getUnavailableData(miniCartData))
         return miniCartSimplifiedDataList
     }
 
-    private fun getAvailableData(miniCartData: MiniCartData): List<MiniCartSimplifiedData> {
-        val miniCartSimplifiedDataList = mutableListOf<MiniCartSimplifiedData>()
+    private fun getAvailableData(miniCartData: MiniCartData): List<MiniCartItem> {
+        val miniCartSimplifiedDataList = mutableListOf<MiniCartItem>()
 
         miniCartData.data.availableSection.availableGroup.forEach { availableGroup ->
             availableGroup.cartDetails.forEach { cartDetail ->
                 miniCartSimplifiedDataList.add(
-                        MiniCartSimplifiedData().apply {
+                        MiniCartItem().apply {
                             isError = false
                             cartId = cartDetail.cartId
                             productId = cartDetail.product.productId
@@ -33,14 +51,14 @@ class MiniCartSimplifiedMapper @Inject constructor() {
         return miniCartSimplifiedDataList
     }
 
-    private fun getUnavailableData(miniCartData: MiniCartData): List<MiniCartSimplifiedData> {
-        val miniCartSimplifiedDataList = mutableListOf<MiniCartSimplifiedData>()
+    private fun getUnavailableData(miniCartData: MiniCartData): List<MiniCartItem> {
+        val miniCartSimplifiedDataList = mutableListOf<MiniCartItem>()
 
         miniCartData.data.unavailableSection.forEach { unavailableSection ->
             unavailableSection.unavailableGroup.forEach { unavailableGroup ->
                 unavailableGroup.cartDetails.forEach { cartDetail ->
                     miniCartSimplifiedDataList.add(
-                            MiniCartSimplifiedData().apply {
+                            MiniCartItem().apply {
                                 isError = true
                                 cartId = cartDetail.cartId
                                 productId = cartDetail.product.productId
