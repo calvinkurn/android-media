673c395a9ea0ac5b1fa668d1f50e989267b3bfc7
irfan khoirul <irfankhoirul@gmail.com>
2021-05-17 08:58:02 +0700

    add example activity

--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleActivity.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleActivity.kt
@@ -1,4 +1,20 @@
 package com.tokopedia.minicart.example
 
-class ExampleActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.tokopedia.minicart.R
+import com.tokopedia.totalamount.TotalAmount
+
+class ExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+        Log.d("MiniCart", "LaunchActivityExample")
+
+        val totalAmount = findViewById<TotalAmount>(R.id.mini_cart_total_amount)
+        totalAmount.let {  }
+    }
+
 }
