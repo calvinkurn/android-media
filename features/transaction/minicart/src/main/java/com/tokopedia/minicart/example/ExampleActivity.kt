90e6e27d629968eb6394543de96f83ad400c2d6c
irfan khoirul <irfankhoirul@gmail.com>
2021-05-18 16:01:32 +0700

    test implementation on activity & fragment

--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleActivity.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleActivity.kt
@@ -1,20 +1,29 @@
 package com.tokopedia.minicart.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.tokopedia.minicart.R
-import com.tokopedia.totalamount.TotalAmount
+import com.tokopedia.minicart.common.domain.data.MiniCartSimplifiedData
+import com.tokopedia.minicart.ui.widget.MiniCartWidget
+import com.tokopedia.minicart.ui.widget.MiniCartWidgetListener
 
-class ExampleActivity : AppCompatActivity() {
+class ExampleActivity : AppCompatActivity(), MiniCartWidgetListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_example)
         Log.d("MiniCart", "LaunchActivityExample")
 
-        val totalAmount = findViewById<TotalAmount>(R.id.mini_cart_total_amount)
-        totalAmount.let {  }
+        val miniCartWidget = findViewById<MiniCartWidget>(R.id.mini_cart_widget)
+        miniCartWidget?.setup(this, this)
+//        miniCartWidget?.updateData(MiniCartWidgetData(totalProductCount = 10, totalProductPrice = 1000000))
+        miniCartWidget?.updateData()
+    }
+
+    override fun onCartItemsUpdated(miniCartSimplifiedData: MiniCartSimplifiedData) {
+        Toast.makeText(this, this.javaClass.name + "  - onCartItemsUpdated", Toast.LENGTH_SHORT).show()
     }
 
 }
--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleFragment.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/example/ExampleFragment.kt
@@ -1,4 +1,21 @@
 package com.tokopedia.minicart.example
 
-class ExampleFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.tokopedia.minicart.common.domain.data.MiniCartSimplifiedData
+import com.tokopedia.minicart.ui.widget.MiniCartWidget
+import com.tokopedia.minicart.ui.widget.MiniCartWidgetListener
+
+class ExampleFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val widget = MiniCartWidget(requireContext())
+        widget.setup(this, object : MiniCartWidgetListener {
+            override fun onCartItemsUpdated(miniCartSimplifiedData: MiniCartSimplifiedData) {
+
+            }
+        })
+    }
 }
