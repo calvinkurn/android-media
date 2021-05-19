a1735b939b31bcebaf13da45f0aab35a1f3f644b
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-05-19 15:31:04 +0700

    executePendingTransactions after oncreate

--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/view/activity/FakeInboxActivity.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/view/activity/FakeInboxActivity.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.inbox.fake.view.activity
 
+import android.os.Bundle
 import com.tokopedia.inbox.di.InboxComponent
 import com.tokopedia.inbox.fake.view.navigator.FakeInboxFragmentFactory
 import com.tokopedia.inbox.view.activity.InboxActivity
@@ -7,6 +8,11 @@ import com.tokopedia.inbox.view.activity.base.InboxTest
 import com.tokopedia.inbox.view.navigator.InboxFragmentFactory
 
 class FakeInboxActivity : InboxActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager.executePendingTransactions()
+    }
 
     override fun createDaggerComponent(): InboxComponent {
         return InboxTest.inboxComponent!!
