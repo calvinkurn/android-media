03b292251bc699efa7694d59fbe44c6569d32a39
noiz354 <noiz354@gmail.com>
2021-05-19 16:49:05 +0700

    change key name accordingly

--- libraries/composite_lib/user_session/src/main/java/com/tokopedia/user/session/Constants.java
+++ libraries/composite_lib/user_session/src/main/java/com/tokopedia/user/session/Constants.java
@@ -8,33 +8,34 @@ public class Constants {
     public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
     public static final String TOKEN_TYPE = "TOKEN_TYPE";
     public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
-    public static final String LOGIN_ID = "LOGIN_ID";
     public static final String IS_LOGIN = "IS_LOGIN";
+    public static final String LOGIN_ID = "LOGIN_ID";
+    public static final String GTM_LOGIN_ID = "GTM_LOGIN_ID";
+
     public static final String LOGIN_SESSION = "LOGIN_SESSION";
     public static final String SHOP_ID = "SHOP_ID";
     public static final String GC_TOKEN = "GC_TOKEN";
     public static final String IS_GOLD_MERCHANT = "IS_GOLD_MERCHANT";
     public static final String FULL_NAME = "FULL_NAME";
     public static final String PROFILE_PICTURE = "PROFILE_PICTURE";
-    public static final String TEMP_USER_ID = "TEMP_USER_ID";
-    public static final String GCM_ID = "GCM_ID";
+    public static final String TEMP_USER_ID = "temp_login_id";
+    public static final String GCM_ID = "gcm_id";
     public static final String TEMP_EMAIL = "TEMP_EMAIL";
     public static final String TEMP_PHONE_NUMBER = "TEMP_PHONE_NUMBER";
     public static final String IS_MSISDN_VERIFIED = "IS_MSISDN_VERIFIED";
-    public static final String IS_AFFILIATE = "IS_AFFILIATE";
+    public static final String IS_AFFILIATE = "is_affiliate";
     public static final String HAS_SHOWN_SALDO_WARNING = "HAS_SHOWN_SALDO_WARNING";
     public static final String HAS_SHOWN_SALDO_INTRO_PAGE = "HAS_SHOWN_SALDO_INTRO_PAGE";
     public static final String PHONE_NUMBER = "PHONE_NUMBER";
     public static final String EMAIL = "EMAIL";
-    public static final String IS_FIRST_TIME_USER_NEW_ONBOARDING = "IS_FIRST_TIME_USER_NEW_ONBOARDING";
+    public static final String IS_FIRST_TIME_USER_NEW_ONBOARDING = "IS_FIRST_TIME_NEW_ONBOARDING";
     public static final String HAS_PASSWORD = "HAS_PASSWORD";
     public static final String SHOP_AVATAR = "SHOP_AVATAR";
     public static final String IS_POWER_MERCHANT_IDLE = "IS_POWER_MERCHANT_IDLE";
     public static final String AUTOFILL_USER_DATA = "AUTOFILL_USER_DATA";
     public static final String LOGIN_METHOD = "LOGIN_METHOD";
-    public static final String UUID_KEY = "UUID_KEY";
+    public static final String UUID_KEY = "uuid";
     public static final String LOGIN_UUID_KEY = "LOGIN_UUID_KEY";
-    public static final String GTM_LOGIN_ID = "GTM_LOGIN_ID";
 
     public static final String TEMP_NAME = "TEMP_NAME";
     public static final String GCM_STORAGE = "GCM_STORAGE";
@@ -42,7 +43,7 @@ public class Constants {
     public static final String REFRESH_TOKEN_KEY = "REFRESH_TOKEN_KEY";
     public static final String IS_FIRST_TIME_USER = "IS_FIRST_TIME_USER";
     public static final String IS_SHOP_OFFICIAL_STORE = "IS_SHOP_OFFICIAL_STORE";
-    public static final String GCM_ID_TIMESTAMP = "GCM_ID_TIMESTAMP";
+    public static final String GCM_ID_TIMESTAMP = "gcm_id_timestamp";
 
     public static final String  ANDROID_ID = "ANDROID_ID";
     public static final String  KEY_ANDROID_ID = "KEY_ANDROID_ID";
