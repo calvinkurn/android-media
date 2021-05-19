d1eaf2784671796ae6dd6b429deb14851af04f19
misael-jonathan <misael.jonathan@tokopedia.com>
2021-05-19 14:57:49 +0700

    check aisle size

--- features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryAisleViewHolder.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/viewholder/CategoryAisleViewHolder.kt
@@ -3,9 +3,12 @@ package com.tokopedia.tokomart.category.presentation.viewholder
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.LayoutRes
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.tokomart.R
 import com.tokopedia.tokomart.category.presentation.model.CategoryAisleDataView
+import com.tokopedia.tokomart.category.presentation.model.CategoryAisleItemDataView
+import com.tokopedia.unifycomponents.CardUnify
 import com.tokopedia.unifycomponents.ImageUnify
 import kotlinx.android.synthetic.main.item_tokomart_category_aisle.view.*
 
@@ -17,16 +20,36 @@ class CategoryAisleViewHolder(itemView: View): AbstractViewHolder<CategoryAisleD
     }
 
     override fun bind(aisle: CategoryAisleDataView) {
-        val txtCategoryNameLeft = itemView.findViewById<TextView>(R.id.tokomartSearchCategoryAisleNameLeft)
-        val txtCategoryNameRight = itemView.findViewById<TextView>(R.id.tokomartSearchCategoryAisleNameRight)
-        val imgCategoryleft = itemView.findViewById<ImageUnify>(R.id.tokomartSearchCategoryAisleImageLeft)
-        val imgCategoryRight = itemView.findViewById<ImageUnify>(R.id.tokomartSearchCategoryAisleImageRight)
-
-        itemView.run {
-            txtCategoryNameLeft.text = aisle.items[0].name
-            txtCategoryNameRight.text = aisle.items[1].name
-            imgCategoryleft.setImageUrl(aisle.items[0].imgUrl)
-            imgCategoryRight.setImageUrl(aisle.items[1].imgUrl)
+        when (aisle.items.size) {
+            0 -> {
+                val aisleContainer = itemView.findViewById<ConstraintLayout?>(R.id.tokomartSearchCategoryAisleContainer)
+                aisleContainer?.visibility = View.GONE
+            }
+            1 -> {
+                val rightAisleCard = itemView.findViewById<CardUnify?>(R.id.tokomartSearchCategoryAisleCardRight)
+                rightAisleCard?.visibility = View.INVISIBLE
+                bindLeftAisle(aisle.items[0])
+            }
+            else -> {
+                bindLeftAisle(aisle.items[0])
+                bindRightAisle(aisle.items[1])
+            }
         }
+    }
+
+    private fun bindLeftAisle(item: CategoryAisleItemDataView) {
+        val txtCategoryNameLeft = itemView.findViewById<TextView?>(R.id.tokomartSearchCategoryAisleNameLeft)
+        val imgCategoryLeft = itemView.findViewById<ImageUnify?>(R.id.tokomartSearchCategoryAisleImageLeft)
+
+        txtCategoryNameLeft?.text = item.name
+        imgCategoryLeft?.setImageUrl(item.imgUrl)
+    }
+
+    private fun bindRightAisle(item: CategoryAisleItemDataView) {
+        val txtCategoryNameRight = itemView.findViewById<TextView?>(R.id.tokomartSearchCategoryAisleNameRight)
+        val imgCategoryRight = itemView.findViewById<ImageUnify?>(R.id.tokomartSearchCategoryAisleImageRight)
+
+        txtCategoryNameRight?.text = item.name
+        imgCategoryRight?.setImageUrl(item.imgUrl)
     }
 }
