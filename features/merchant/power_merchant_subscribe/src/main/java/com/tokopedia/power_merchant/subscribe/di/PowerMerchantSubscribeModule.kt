303fc122900ad641eb1f854ac6b6039ac1f435b1
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-05-20 23:39:58 +0700

    fix resolve conflict

--- features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/di/PowerMerchantSubscribeModule.kt
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/di/PowerMerchantSubscribeModule.kt
@@ -6,6 +6,8 @@ import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.gm.common.constant.GMParamConstant
 import com.tokopedia.gm.common.di.GmCommonModule
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.power_merchant.subscribe.R
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 import com.tokopedia.remoteconfig.RemoteConfig
