90a2917c27d4a88de7743a993a912710cbf520f5
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-05-20 17:00:08 +0700

    adding register check flag

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/case/LoginNegativeCase.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/case/LoginNegativeCase.kt
@@ -2,64 +2,78 @@ package com.tokopedia.loginregister.login.behaviour.case
 
 import com.tokopedia.loginregister.R
 import com.tokopedia.loginregister.login.behaviour.base.LoginBase
-import org.junit.Before
+import com.tokopedia.loginregister.login.domain.pojo.RegisterCheckData
+import com.tokopedia.loginregister.login.domain.pojo.RegisterCheckPojo
 import org.junit.Test
 
 class LoginNegativeCase: LoginBase() {
 
-    @Before
-    fun defaultResponse() {
-        setDefaultDiscover()
-        setRegisterCheckDefaultResponse()
-        launchDefaultFragment()
-    }
-
-//    @Test
-//    /* Show error text when input text is empty */
-//    fun showErrorText_ifEmpty() {
-//        inputEmailOrPhone("yoris.prayogo@tokopedia.com")
-//        deleteEmailOrPhoneInput()
-//        clickSubmit()
-//
-//        shouldBeDisplayed(R.id.tv_error)
-//    }
-
     @Test
     /* Disable button "Selanjutnya" when input text is empty */
     fun disableNextButton_ifEmpty() {
-        inputEmailOrPhone("yoris.prayogo@tokopedia.com")
-        deleteEmailOrPhoneInput()
+        runTest {
+            inputEmailOrPhone("yoris.prayogo@tokopedia.com")
+            deleteEmailOrPhoneInput()
 
-        shouldBeDisabled(R.id.register_btn)
+            shouldBeDisabled(R.id.register_btn)
+        }
     }
 
     @Test
     /* Disable button "Selanjutnya" when input text is not valid email */
     fun disableNextButton_ifNotValidEmail() {
-
-        inputEmailOrPhone("yoris.prayogo")
-
-        shouldBeDisabled(R.id.register_btn)
+        runTest {
+            inputEmailOrPhone("yoris.prayogo")
+            shouldBeDisabled(R.id.register_btn)
+        }
     }
 
     @Test
-    /* Disable button "Selanjutnya" when input text length is not valid for phone number */
-    fun disableNextButton_ifPhoneNumberInvalid() {
+    /* Disable button "Selanjutnya" when input text length is too short for phone number */
+    fun phoneNumberTooShort() {
+        runTest {
+            inputEmailOrPhone("08224")
+            deleteEmailOrPhoneInput()
 
-        inputEmailOrPhone("08224")
-        deleteEmailOrPhoneInput()
+            shouldBeDisabled(R.id.register_btn)
+        }
+    }
 
-        shouldBeDisabled(R.id.register_btn)
+    @Test
+    /* Disable button "Selanjutnya" when input text length is too long for phone number */
+    fun phoneNumberTooLong() {
+        val errorMsg = "Phone too long"
+        isDefaultRegisterCheck = false
+        val data = RegisterCheckData(errors = arrayListOf(errorMsg))
+        registerCheckUseCaseStub.response = RegisterCheckPojo(data = data)
+
+        runTest {
+            inputEmailOrPhone("12345678901234567")
+            clickSubmit()
+            isDisplayingGivenText(R.id.tv_error, errorMsg)
+        }
     }
 
     @Test
     /* Display error text when user click on Masuk button while password is empty */
     fun displayError_IfPasswordEmpty() {
-        inputEmailOrPhone("yoris.prayogo@tokopedia.com")
-        clickSubmit()
-        clickSubmit()
+        runTest {
+            inputEmailOrPhone("yoris.prayogo@tokopedia.com")
+            clickSubmit()
+            clickSubmit()
 
-        isDisplayingGivenText(R.id.textinput_helper_text, "Kata sandi harus diisi")
+            isDisplayingGivenText(R.id.textinput_helper_text, "Kata sandi harus diisi")
+        }
+    }
+
+    @Test
+    /* Disable Email Input if password wrapper visible */
+    fun disableEmailInput_IfPasswordVisible() {
+        runTest {
+            inputEmailOrPhone("yoris.prayogo@tokopedia.com")
+            clickSubmit()
+            shouldBeDisabled(R.id.input_email_phone)
+        }
     }
 
 }
