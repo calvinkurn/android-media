ed87e9a4e1cdc15488bda159eb5907f84d6dc62e
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-05-21 19:10:26 +0700

    add test should_show_pinned_icon_when_pin_status_is_true

--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/common/viewmatcher/RecyclerViewMatcher.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/common/viewmatcher/RecyclerViewMatcher.kt
@@ -3,6 +3,7 @@ package com.tokopedia.inbox.common.viewmatcher
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.matcher.BoundedMatcher
+import com.tokopedia.test.application.matcher.RecyclerViewMatcher
 import org.hamcrest.Description
 
 /**
@@ -20,4 +21,15 @@ fun withTotalItem(expectedCount: Int): BoundedMatcher<View, RecyclerView> {
             return adapter?.itemCount == expectedCount
         }
     }
+}
+
+/**
+ * Example usage:
+ *         onView(
+ *              withRecyclerView(R.id.recycler_view)
+ *              .atPositionOnView(pos, R.id.btn_secondary)
+ *         ).check(matches(ViewMatchers.withText("test")))
+ */
+fun withRecyclerView(recyclerViewId: Int): RecyclerViewMatcher {
+    return RecyclerViewMatcher(recyclerViewId)
 }
