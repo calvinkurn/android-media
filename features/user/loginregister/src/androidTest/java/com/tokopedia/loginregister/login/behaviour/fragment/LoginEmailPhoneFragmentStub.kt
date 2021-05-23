a0eb51f96eeb3f51984639bd56c483494a137d45
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-05-23 23:47:16 +0700

    create fragment stub

---
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/fragment/LoginEmailPhoneFragmentStub.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.loginregister.login.behaviour.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tokopedia.loginregister.login.view.fragment.LoginEmailPhoneFragment
+
+class LoginEmailPhoneFragmentStub: LoginEmailPhoneFragment() {
+
+    companion object {
+        fun createInstance(bundle: Bundle): Fragment {
+            val fragment = LoginEmailPhoneFragmentStub()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    override fun isEnableEncryption(): Boolean = true
+}
