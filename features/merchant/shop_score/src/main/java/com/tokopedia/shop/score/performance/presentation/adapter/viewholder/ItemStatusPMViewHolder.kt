5e3249e3f790cdcbce7d137f97b23eed7cf8b2d8
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-05-25 14:31:20 +0700

    fix background

--- features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/ItemStatusPMViewHolder.kt
+++ features/merchant/shop_score/src/main/java/com/tokopedia/shop/score/performance/presentation/adapter/viewholder/ItemStatusPMViewHolder.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.shop.score.performance.presentation.adapter.viewholder
 
 import android.view.View
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.*
@@ -25,7 +24,6 @@ class ItemStatusPMViewHolder(view: View,
 
     private fun setupItemPowerMerchant(element: ItemStatusPMUiModel?) {
         with(itemView) {
-            potentialPowerMerchantWidget?.background = AppCompatResources.getDrawable(context, R.drawable.bg_header_bronze)
             tv_pm_reputation_value?.text = getString(R.string.title_pm_value)
             element?.descPM?.let {
                 tv_desc_content_pm_section?.setTextMakeHyperlink(it) {
