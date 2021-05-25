6ffff1733929ed80bb5584ce7388a8de98b8169a
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-05-25 14:25:11 +0700

    fix dagger issue

--- features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/di/TrackingPageModule.kt
+++ features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/di/TrackingPageModule.kt
@@ -1,14 +1,23 @@
 package com.tokopedia.logisticorder.di
 
 import android.content.Context
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
+import com.tokopedia.common.network.coroutines.RestRequestInteractor
+import com.tokopedia.common.network.coroutines.repository.RestRepository
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.logisticorder.usecase.GetDeliveryImageUseCase
+import com.tokopedia.network.NetworkRouter
+import com.tokopedia.network.interceptor.FingerprintInterceptor
+import com.tokopedia.network.interceptor.TkpdAuthInterceptor
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
 
 @Module
 object TrackingPageModule {
@@ -16,5 +25,58 @@ object TrackingPageModule {
     @ActivityScope
     fun provideGraphQlRepository(): GraphqlRepository =
             GraphqlInteractor.getInstance().graphqlRepository
+
+    @ActivityScope
+    @Provides
+    fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface = UserSession(context)
+
+    @Provides
+    @ActivityScope
+    internal fun provideNetworkRouter(@ApplicationContext context: Context): NetworkRouter {
+        return context as NetworkRouter
+    }
+
+    @Provides
+    fun provideRestRepository(interceptors: MutableList<Interceptor>,
+                              @ApplicationContext context: Context): RestRepository {
+        return RestRequestInteractor.getInstance().restRepository.apply {
+            updateInterceptors(interceptors, context)
+        }
+    }
+
+    @Provides
+    @ActivityScope
+    internal fun provideFingerprintInterceptor(networkRouter: NetworkRouter, userSession: UserSessionInterface): FingerprintInterceptor {
+        return FingerprintInterceptor(networkRouter, userSession)
+    }
+
+    @Provides
+    @ActivityScope
+    fun provideChuckerInterceptor(@ApplicationContext context: Context): ChuckerInterceptor {
+        return ChuckerInterceptor(context)
+    }
+
+    @ActivityScope
+    @Provides
+    fun provideAuthInterceptors(@ApplicationContext context: Context,
+                                userSession: UserSessionInterface): TkpdAuthInterceptor {
+        return TkpdAuthInterceptor(context, context as NetworkRouter, userSession)
+    }
+
+
+    @Provides
+    @ActivityScope
+    fun provideInterceptors(tkpdAuthInterceptor: TkpdAuthInterceptor,
+                            fingerprintInterceptor: FingerprintInterceptor,
+                            httpLoggingInterceptor: HttpLoggingInterceptor,
+                            chuckerInterceptor: ChuckerInterceptor): MutableList<Interceptor> {
+        return mutableListOf(tkpdAuthInterceptor, fingerprintInterceptor, httpLoggingInterceptor, chuckerInterceptor)
+    }
+
+    @Provides
+    @ActivityScope
+    fun provideGetDeliveryImageUseCase(repository: RestRepository): GetDeliveryImageUseCase {
+        return GetDeliveryImageUseCase(repository)
+    }
 
 }
