0e1b99cf676e4e51ab438c8742b06e48c08492ed
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-05-27 21:20:51 +0700

    fix pm pro

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/constant/PMProURL.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/constant/PMProURL.kt
@@ -1,7 +1,7 @@
 package com.tokopedia.seller.menu.common.constant
 
 object PMProURL {
-    const val BG_ULTIMATE = "https://images.tokopedia.net/img/android/shop_score/ic_pmpro_ultimate.png"
-    const val BG_EXPERT =  "https://images.tokopedia.net/img/android/shop_score/ic_pmpro_expert.png"
-    const val BG_ADVANCE = "https://images.tokopedia.net/img/android/shop_score/ic_pmpro_advance.png"
+    const val BG_ULTIMATE = "https://images.tokopedia.net/img/android/seller_menu_common/ic_pmpro_ultimate.png"
+    const val BG_EXPERT =  "https://images.tokopedia.net/img/android/seller_menu_common/ic_pmpro_expert.png"
+    const val BG_ADVANCE = "https://images.tokopedia.net/img/android/seller_menu_common/ic_pmpro_advance.png"
 }
