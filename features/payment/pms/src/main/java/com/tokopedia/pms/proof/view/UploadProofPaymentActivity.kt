c245277b5eb4c1075dcc0afcb95ef7273e8396b7
Abhijeet <abhijeet.chauhan@tokopedia.com>
2021-05-27 11:24:39 +0530

    [PMS Revamp] Upload proof refactor completed

--- features/payment/pms/src/main/java/com/tokopedia/pms/proof/view/UploadProofPaymentActivity.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/proof/view/UploadProofPaymentActivity.kt
@@ -1,55 +1,46 @@
-package com.tokopedia.pms.proof.view;
+package com.tokopedia.pms.proof.view
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-
-import androidx.fragment.app.Fragment;
-
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
-import com.tokopedia.config.GlobalConfig;
-import com.tokopedia.pms.common.Constant;
-import com.tokopedia.pms.paymentlist.domain.data.BasePaymentModel;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.config.GlobalConfig
+import com.tokopedia.pms.common.Constant
+import com.tokopedia.pms.paymentlist.domain.data.BasePaymentModel
 
 /**
  * Created by zulfikarrahman on 7/6/18.
  */
-
-public class UploadProofPaymentActivity extends BaseSimpleActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setSecureWindowFlag();
+class UploadProofPaymentActivity : BaseSimpleActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSecureWindowFlag()
     }
 
-    private void setSecureWindowFlag() {
+    private fun setSecureWindowFlag() {
         if (GlobalConfig.APPLICATION_TYPE == GlobalConfig.CONSUMER_APPLICATION || GlobalConfig.APPLICATION_TYPE == GlobalConfig.SELLER_APPLICATION) {
-            runOnUiThread(() -> {
-                Window window = getWindow();
-                if (window != null) {
-                    window.addFlags(WindowManager.LayoutParams.FLAG_SECURE);
-                }
-            });
+            runOnUiThread {
+                val window = window
+                window?.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+            }
         }
     }
 
-    public static Intent createIntent(Context context, BasePaymentModel paymentListModel) {
-        Intent intent = new Intent(context, UploadProofPaymentActivity.class);
-        intent.putExtra(Constant.PAYMENT_LIST_MODEL_EXTRA, paymentListModel);
-        return intent;
+    override fun isShowCloseButton() = true
+
+    override fun getNewFragment(): Fragment {
+        val paymentListModel: BasePaymentModel =
+            intent.getParcelableExtra(Constant.PAYMENT_LIST_MODEL_EXTRA)
+        return UploadProofPaymentFragment.createInstance(paymentListModel)
     }
 
-    @Override
-    protected boolean isShowCloseButton() {
-        return true;
-    }
-
-    @Override
-    protected Fragment getNewFragment() {
-        BasePaymentModel paymentListModel = getIntent().getParcelableExtra(Constant.PAYMENT_LIST_MODEL_EXTRA);
-        return UploadProofPaymentFragment.createInstance(paymentListModel);
+    companion object {
+        fun createIntent(context: Context?, paymentListModel: BasePaymentModel?): Intent {
+            val intent = Intent(context, UploadProofPaymentActivity::class.java)
+            intent.putExtra(Constant.PAYMENT_LIST_MODEL_EXTRA, paymentListModel)
+            return intent
+        }
     }
 }
--- features/payment/pms/src/main/java/com/tokopedia/pms/proof/view/UploadProofPaymentFragment.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/proof/view/UploadProofPaymentFragment.kt
@@ -1,234 +1,202 @@
-package com.tokopedia.pms.proof.view;
+package com.tokopedia.pms.proof.view
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.tokopedia.abstraction.base.app.BaseMainApplication;
-import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment;
-import com.tokopedia.abstraction.common.utils.image.ImageHandler;
-import com.tokopedia.abstraction.common.utils.network.ErrorHandler;
-import com.tokopedia.abstraction.common.utils.snackbar.NetworkErrorHelper;
-import com.tokopedia.abstraction.common.utils.view.MethodChecker;
-import com.tokopedia.applink.RouteManager;
-import com.tokopedia.applink.internal.ApplinkConstInternalGlobal;
-import com.tokopedia.imagepicker.common.ImagePickerBuilder;
-import com.tokopedia.imagepicker.common.ImagePickerResultExtractor;
-import com.tokopedia.imagepicker.common.ImagePickerRouterKt;
-import com.tokopedia.pms.R;
-import com.tokopedia.pms.common.Constant;
-import com.tokopedia.pms.paymentlist.domain.data.BasePaymentModel;
-import com.tokopedia.pms.paymentlist.domain.data.ParentPaymentModelKt;
-import com.tokopedia.pms.proof.di.DaggerUploadProofPaymentComponent;
-import com.tokopedia.pms.proof.di.UploadProofPaymentModule;
-import com.tokopedia.pms.proof.model.PaymentProofResponse;
-import com.tokopedia.unifycomponents.Toaster;
-
-import java.util.List;
-
-import javax.inject.Inject;
-
-import kotlin.Pair;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
+import com.tokopedia.iconunify.getIconUnifyDrawable
+import com.tokopedia.imagepicker.common.ImagePickerBuilder.Companion.getOriginalImageBuilder
+import com.tokopedia.imagepicker.common.ImagePickerResultExtractor.extract
+import com.tokopedia.imagepicker.common.putImagePickerBuilder
+import com.tokopedia.loaderdialog.LoaderDialog
+import com.tokopedia.pms.R
+import com.tokopedia.pms.common.Constant
+import com.tokopedia.pms.paymentlist.domain.data.BasePaymentModel
+import com.tokopedia.pms.paymentlist.domain.data.extractValues
+import com.tokopedia.pms.proof.di.DaggerUploadProofPaymentComponent
+import com.tokopedia.pms.proof.di.UploadProofPaymentModule
+import com.tokopedia.pms.proof.model.PaymentProofResponse
+import com.tokopedia.unifycomponents.Toaster
+import kotlinx.android.synthetic.main.fragment_upload_proof_payment.*
+import javax.inject.Inject
 
 /**
  * Created by zulfikarrahman on 7/6/18.
  */
-
-public class UploadProofPaymentFragment extends BaseDaggerFragment implements UploadProofPaymentContract.View {
-
-    private static final int REQUEST_CODE_IMAGE_PROOF = 845;
-    public static final int MAX_FILE_SIZE_IN_KB = 10240;
-
+class UploadProofPaymentFragment : BaseDaggerFragment(), UploadProofPaymentContract.View {
     @Inject
-    UploadProofPaymentPresenter uploadProofPaymentPresenter;
+    lateinit var uploadProofPaymentPresenter: UploadProofPaymentPresenter
 
-    private BasePaymentModel paymentListModel;
-    private View containerHelpUploadProof;
-    private View containerImageUpload;
-    private ImageView buttonActionCloseImage;
-    private ImageView imageViewProof;
-    private Button buttonSave;
-    private Button buttonChooseAnotherImage;
-    private TextView titleUploadImage;
-    private ProgressDialog progressDialog;
-    private String imageUrl = "";
-    private boolean isUploaded;
+    private var paymentListModel: BasePaymentModel? = null
+    private var progressDialog: LoaderDialog? = null
+    private var imageUrl = ""
+    private var isUploaded = false
+    override fun getScreenName() = null
 
-    @Override
-    protected String getScreenName() {
-        return null;
-    }
-
-    @Override
-    protected void initInjector() {
+    override fun initInjector() {
         DaggerUploadProofPaymentComponent.builder()
-                .uploadProofPaymentModule(new UploadProofPaymentModule())
-                .baseAppComponent(((BaseMainApplication) getActivity().getApplication()).getBaseAppComponent())
-                .build()
-                .inject(this);
-        uploadProofPaymentPresenter.attachView(this);
+            .uploadProofPaymentModule(UploadProofPaymentModule())
+            .baseAppComponent((activity?.application as BaseMainApplication).baseAppComponent)
+            .build()
+            .inject(this)
+        uploadProofPaymentPresenter.attachView(this)
     }
 
-    @Override
-    public void onDestroy() {
-        uploadProofPaymentPresenter.detachView();
-        super.onDestroy();
+    override fun onDestroy() {
+        uploadProofPaymentPresenter.detachView()
+        super.onDestroy()
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        paymentListModel = getArguments().getParcelable(Constant.PAYMENT_LIST_MODEL_EXTRA);
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        arguments?.let {
+            paymentListModel = it.getParcelable(Constant.PAYMENT_LIST_MODEL_EXTRA)
+        }
+        super.onCreate(savedInstanceState)
     }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_upload_proof_payment, container, false);
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage(getString(com.tokopedia.abstraction.R.string.title_loading));
-        containerHelpUploadProof = view.findViewById(R.id.container_helper);
-        containerImageUpload = view.findViewById(R.id.container_image_helper);
-        buttonActionCloseImage = view.findViewById(R.id.iv_action_image);
-        buttonChooseAnotherImage = view.findViewById(R.id.button_save_choose_another_image);
-        titleUploadImage = view.findViewById(R.id.text_confirmation);
-        imageViewProof = view.findViewById(R.id.image_payment);
-        buttonSave = view.findViewById(R.id.button_save);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                actionButtonUpload();
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_upload_proof_payment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_save.setOnClickListener(View.OnClickListener { actionButtonUpload() })
+        iv_action_image.setOnClickListener(View.OnClickListener {
+            if (!isUploaded) {
+                resetImageUrl()
+                invalidateView()
             }
-        });
-        buttonActionCloseImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!isUploaded) {
-                    resetImageUrl();
-                    invalidateView();
-                }
-            }
-        });
-        buttonChooseAnotherImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openImagePicker();
-            }
-        });
-        return view;
+        })
+        button_save_choose_another_image.setOnClickListener(View.OnClickListener { openImagePicker() })
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    private fun resetImageUrl() {
+        imageUrl = ""
     }
 
-    public void resetImageUrl() {
-        imageUrl = "";
-    }
-
-    private void invalidateView() {
+    private fun invalidateView() {
         if (!TextUtils.isEmpty(imageUrl)) {
-            ImageHandler.LoadImage(imageViewProof, imageUrl);
-            containerImageUpload.setVisibility(View.VISIBLE);
-            containerHelpUploadProof.setVisibility(View.GONE);
+            ImageHandler.LoadImage(image_payment, imageUrl)
+            container_image_helper!!.visibility = View.VISIBLE
+            container_helper!!.visibility = View.GONE
             if (isUploaded) {
-                buttonActionCloseImage.setImageDrawable(MethodChecker.getDrawable(getActivity(), R.drawable.ic_check_green_payment));
-                buttonSave.setText(R.string.payment_label_finish);
-                buttonChooseAnotherImage.setVisibility(View.VISIBLE);
-                titleUploadImage.setText(R.string.payment_label_succes_upload_proof);
+                val drawable = getIconUnifyDrawable(
+                    requireContext(),
+                    com.tokopedia.iconunify.R.drawable.iconunify_check,
+                    com.tokopedia.unifyprinciples.R.color.Unify_G500
+                )
+
+                iv_action_image?.setImageDrawable(drawable)
+                button_save.setText(R.string.payment_label_finish)
+                button_save_choose_another_image!!.visibility = View.VISIBLE
+                text_confirmation.setText(R.string.payment_label_succes_upload_proof)
             } else {
-                buttonActionCloseImage.setImageDrawable(MethodChecker.getDrawable(getActivity(), com.tokopedia.design.R.drawable.ic_close_default));
-                buttonSave.setText(R.string.payment_label_save_image);
-                buttonChooseAnotherImage.setVisibility(View.GONE);
-                titleUploadImage.setText(R.string.payment_label_confirmation_upload_image);
+                val drawable = getIconUnifyDrawable(
+                    requireContext(),
+                    com.tokopedia.iconunify.R.drawable.iconunify_close)
+                iv_action_image.setImageDrawable(drawable)
+                button_save!!.setText(R.string.payment_label_save_image)
+                button_save_choose_another_image!!.visibility = View.GONE
+                text_confirmation.setText(R.string.payment_label_confirmation_upload_image)
             }
         } else {
-            buttonSave.setText(R.string.payment_label_choose_image);
-            buttonChooseAnotherImage.setVisibility(View.GONE);
-            containerHelpUploadProof.setVisibility(View.VISIBLE);
-            containerImageUpload.setVisibility(View.GONE);
+            button_save!!.setText(R.string.payment_label_choose_image)
+            button_save_choose_another_image!!.visibility = View.GONE
+            container_helper!!.visibility = View.VISIBLE
+            container_image_helper!!.visibility = View.GONE
         }
     }
 
-    public void actionButtonUpload() {
-        if (buttonSave.getText().toString().equals(getString(R.string.payment_label_choose_image))) {
-            openImagePicker();
-        } else if (buttonSave.getText().toString().equals(getString(R.string.payment_label_finish))) {
-            getActivity().finish();
+    private fun actionButtonUpload() {
+        if (button_save.text.toString() == getString(R.string.payment_label_choose_image)) {
+            openImagePicker()
+        } else if (button_save.text.toString() == getString(R.string.payment_label_finish)) {
+            activity?.finish()
         } else {
-            Pair<String, String> idPair = ParentPaymentModelKt.extractValues(paymentListModel);
-            uploadProofPaymentPresenter.uploadProofPayment(idPair.getFirst(), idPair.getSecond(), imageUrl);
+            if (paymentListModel != null) {
+                val (first, second) = paymentListModel!!.extractValues()
+                uploadProofPaymentPresenter.uploadProofPayment(first, second, imageUrl)
+            }
         }
     }
 
-    private void openImagePicker() {
-        ImagePickerBuilder builder = ImagePickerBuilder.getOriginalImageBuilder(requireContext());
-        Intent intent = RouteManager.getIntent(requireContext(), ApplinkConstInternalGlobal.IMAGE_PICKER);
-        ImagePickerRouterKt.putImagePickerBuilder(intent, builder);
-        startActivityForResult(intent, REQUEST_CODE_IMAGE_PROOF);
+    private fun openImagePicker() {
+        val builder = getOriginalImageBuilder(requireContext())
+        val intent =
+            RouteManager.getIntent(requireContext(), ApplinkConstInternalGlobal.IMAGE_PICKER)
+        intent.putImagePickerBuilder(builder)
+        startActivityForResult(intent, REQUEST_CODE_IMAGE_PROOF)
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_IMAGE_PROOF && resultCode == Activity.RESULT_OK && data != null) {
-            List<String> imageUrlOrPathList = ImagePickerResultExtractor.extract(data).getImageUrlOrPathList();
-            if (imageUrlOrPathList.size() > 0) {
-                imageUrl = imageUrlOrPathList.get(0);
+            val imageUrlOrPathList: List<String> = extract(data).imageUrlOrPathList
+            if (imageUrlOrPathList.isNotEmpty()) {
+                imageUrl = imageUrlOrPathList[0]
             }
-            isUploaded = false;
-            invalidateView();
+            isUploaded = false
+            invalidateView()
         }
     }
 
-    public static Fragment createInstance(BasePaymentModel paymentListModel) {
-        UploadProofPaymentFragment uploadProofPaymentFragment = new UploadProofPaymentFragment();
-        Bundle bundle = new Bundle();
-        bundle.putParcelable(Constant.PAYMENT_LIST_MODEL_EXTRA, paymentListModel);
-        uploadProofPaymentFragment.setArguments(bundle);
-        return uploadProofPaymentFragment;
+    override fun showLoadingDialog() {
+        context?.let {
+            progressDialog = LoaderDialog(it)
+            progressDialog?.show()
+        }
     }
 
-    @Override
-    public void showLoadingDialog() {
-        progressDialog.show();
+    override fun hideLoadingDialog() {
+       progressDialog?.dialog?.dismiss()
+        progressDialog = null
     }
 
-    @Override
-    public void hideLoadingDialog() {
-        progressDialog.hide();
+    override fun onErrorUploadProof(e: Throwable) {
+        showToast(com.tokopedia.network.utils.ErrorHandler.getErrorMessage(context, e), Toaster.TYPE_ERROR)
     }
 
-
-    @Override
-    public void onErrorUploadProof(Throwable e) {
-        NetworkErrorHelper.showRedCloseSnackbar(getActivity(), ErrorHandler.getErrorMessage(getActivity(), e));
-    }
-
-    @Override
-    public void onResultUploadProof(PaymentProofResponse paymentProofResponse) {
-        if (paymentProofResponse.getStatus() != null
-                && paymentProofResponse.getStatus().equalsIgnoreCase("OK")) {
-            isUploaded = true;
-            invalidateView();
-            getActivity().setResult(Activity.RESULT_OK);
+    override fun onResultUploadProof(paymentProofResponse: PaymentProofResponse) {
+        if (paymentProofResponse.status != null
+            && paymentProofResponse.status.equals("OK", ignoreCase = true)
+        ) {
+            isUploaded = true
+            invalidateView()
+            activity?.setResult(Activity.RESULT_OK)
         } else {
-            if (getView() != null && paymentProofResponse.getMessageError() != null)
-                Toaster.make(getView(), paymentProofResponse.getMessageError(),
-                        Toaster.LENGTH_SHORT, Toaster.TYPE_ERROR);
+            if (view != null && paymentProofResponse.messageError != null)
+                showToast(paymentProofResponse.messageError!!, Toaster.TYPE_ERROR)
+        }
+    }
+
+    private fun showToast(message: String, toastType: Int) {
+        view?.let {
+            Toaster.make(it, message, Toaster.LENGTH_SHORT, toastType)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_IMAGE_PROOF = 845
+        const val MAX_FILE_SIZE_IN_KB = 10240
+        fun createInstance(paymentListModel: BasePaymentModel?): Fragment {
+            val uploadProofPaymentFragment = UploadProofPaymentFragment()
+            val bundle = Bundle()
+            bundle.putParcelable(Constant.PAYMENT_LIST_MODEL_EXTRA, paymentListModel)
+            uploadProofPaymentFragment.arguments = bundle
+            return uploadProofPaymentFragment
         }
     }
 }
