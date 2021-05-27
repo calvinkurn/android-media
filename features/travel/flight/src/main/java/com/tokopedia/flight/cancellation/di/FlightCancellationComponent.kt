a6f538f9364412316b39fb0c92d906d8684b167e
Muhammad Furqan <furqan@tokopedia.com>
2021-05-27 21:08:46 +0700

    [Flight][Navigation] Implement Flight Cancellation Choose Reason

--- features/travel/flight/src/main/java/com/tokopedia/flight/cancellation/di/FlightCancellationComponent.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/cancellation/di/FlightCancellationComponent.kt
@@ -4,6 +4,7 @@ import com.tokopedia.flight.cancellation.presentation.fragment.FlightCancellatio
 import com.tokopedia.flight.cancellation.presentation.fragment.FlightCancellationPassengerFragment
 import com.tokopedia.flight.cancellation.presentation.fragment.FlightCancellationReasonFragment
 import com.tokopedia.flight.cancellation.presentation.fragment.FlightCancellationReviewFragment
+import com.tokopedia.flight.cancellation_navigation.presentation.bottomsheet.FlightCancellationChooseReasonBottomSheet
 import com.tokopedia.flight.common.di.component.FlightComponent
 import dagger.Component
 
@@ -21,4 +22,6 @@ interface FlightCancellationComponent {
 
     // Inject for cancellation navigation fragment
     fun inject(flightCancellationPassengerFragment: com.tokopedia.flight.cancellation_navigation.presentation.fragment.FlightCancellationPassengerFragment)
+    fun inject(flightCancellationReasonFragment: com.tokopedia.flight.cancellation_navigation.presentation.fragment.FlightCancellationReasonFragment)
+    fun inject(flightCancellationChooseReasonBottomSheet: FlightCancellationChooseReasonBottomSheet)
 }
