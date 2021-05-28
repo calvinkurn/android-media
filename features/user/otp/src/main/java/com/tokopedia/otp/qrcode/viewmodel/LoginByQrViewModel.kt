b1baea81badda146a7f94c65b017d641a1abe95e
Ade Fulki Hadian <ade.hadian@tokopedia.com>
2021-05-28 09:46:54 +0700

    fix unit test

--- features/user/otp/src/main/java/com/tokopedia/otp/qrcode/viewmodel/LoginByQrViewModel.kt
+++ features/user/otp/src/main/java/com/tokopedia/otp/qrcode/viewmodel/LoginByQrViewModel.kt
@@ -51,7 +51,7 @@ class LoginByQrViewModel @Inject constructor(
                 }
             }
         }, {
-            _verifyQrResult.postValue(Fail(Throwable()))
+            _verifyQrResult.postValue(Fail(it))
         })
     }
 
