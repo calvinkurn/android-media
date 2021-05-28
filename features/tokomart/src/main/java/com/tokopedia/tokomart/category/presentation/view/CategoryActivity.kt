dbde66c643b7f8440a412605c9377b1bcbf8a3c4
darianthedytoped <darian.thedy@tokopedia.com>
2021-05-28 14:06:46 +0700

    process category id

--- features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/view/CategoryActivity.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/category/presentation/view/CategoryActivity.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.tokomart.category.presentation.view
 
+import android.net.Uri
 import com.tokopedia.abstraction.common.di.component.HasComponent
 import com.tokopedia.discovery.common.utils.URLParser
+import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.tokomart.category.di.CategoryComponent
 import com.tokopedia.tokomart.category.di.CategoryContextModule
 import com.tokopedia.tokomart.category.di.CategoryParamModule
@@ -15,12 +17,20 @@ class CategoryActivity: BaseSearchCategoryActivity(), HasComponent<CategoryCompo
 
     override fun getComponent(): CategoryComponent {
         val uri = intent.data
-        val categoryId = 0 // Get category id from applink intent
+        val categoryId = uri.getCategoryId()
         val queryParamMap = URLParser(uri.toString()).paramKeyValueMapDecoded
+
         return DaggerCategoryComponent.builder()
                 .baseAppComponent(getBaseAppComponent())
                 .categoryContextModule(CategoryContextModule(this))
                 .categoryParamModule(CategoryParamModule(categoryId, queryParamMap))
                 .build()
+    }
+
+    private fun Uri?.getCategoryId(): Int =
+            this?.pathSegments?.getOrNull(PATH_SEGMENT_INDEX_CATEGORY_ID).toIntOrZero()
+
+    companion object {
+        private const val PATH_SEGMENT_INDEX_CATEGORY_ID = 1
     }
 }
