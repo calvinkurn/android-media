d23ed04a56b0294ad4f49756ccc8fa257fdf03c4
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-05-31 01:28:59 +0700

    create dialog helper

---
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/view/dialog/FingerprintDialogHelper.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.loginfingerprint.view.dialog
+
+import android.content.Context
+import com.tokopedia.dialog.DialogUnify
+import com.tokopedia.loginfingerprint.R
+
+object FingerprintDialogHelper {
+    fun showInvalidFingerprintDialog(context: Context?, onPositiveButtonClick: () -> Unit? = {}) {
+        context?.run {
+            val dialog = DialogUnify(this, DialogUnify.SINGLE_ACTION, DialogUnify.NO_IMAGE)
+            dialog.setTitle(getString(R.string.error_fingerprint_invalid_title))
+            dialog.setDescription(getString(R.string.error_fingerprint_invalid_description))
+            dialog.setPrimaryCTAText(getString(R.string.error_fingerprint_invalid_primary_btn_text))
+            dialog.setPrimaryCTAClickListener {
+                dialog.dismiss()
+                onPositiveButtonClick()
+            }
+            dialog.show()
+        }
+    }
+}
