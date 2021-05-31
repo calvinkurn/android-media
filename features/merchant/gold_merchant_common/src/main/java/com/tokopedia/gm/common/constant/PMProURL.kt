5c45acac44b361f28b4376d729841995c6b890a4
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-05-31 21:47:16 +0700

    revert

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/constant/PMProURL.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/constant/PMProURL.kt
@@ -4,6 +4,4 @@ object PMProURL {
     const val BG_ULTIMATE = "https://images.tokopedia.net/img/android/gold_merchant_common/ic_pmpro_ultimate.png"
     const val BG_EXPERT =  "https://images.tokopedia.net/img/android/gold_merchant_common/ic_pmpro_expert.png"
     const val BG_ADVANCE = "https://images.tokopedia.net/img/android/gold_merchant_common/ic_pmpro_advance.png"
-
-    const val ICON_URL = "https://images.tokopedia.net/img/goldmerchant/pm_activation/badge/Power%20Merchant%20Pro@3x.png"
 }
