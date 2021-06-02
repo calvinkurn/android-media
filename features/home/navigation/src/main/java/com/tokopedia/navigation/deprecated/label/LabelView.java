e3467a5ab1d7f58a49b9b22f361a43bd89cd2977
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-06-02 16:07:13 +0700

    use dimen unify

--- features/home/navigation/src/main/java/com/tokopedia/navigation/deprecated/label/LabelView.java
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/deprecated/label/LabelView.java
@@ -84,9 +84,9 @@ public class LabelView extends BaseCustomView {
             if (drawableId >= 0) {
                 imageDrawable = AppCompatResources.getDrawable(getContext(), drawableId);
             }
-            imageWidth = (int) styledAttributes.getDimension(R.styleable.LabelView_nav_lv_image_width, getResources().getDimension(R.dimen.dp_32));
+            imageWidth = (int) styledAttributes.getDimension(R.styleable.LabelView_nav_lv_image_width, getResources().getDimension(R.dimen.unify_space_32));
             imageHeight = (int) styledAttributes.getDimension(R.styleable.LabelView_nav_lv_image_height, imageWidth);
-            imageMarginRight = (int) styledAttributes.getDimension(R.styleable.LabelView_nav_lv_image_margin_right, getResources().getDimension(R.dimen.dp_8));
+            imageMarginRight = (int) styledAttributes.getDimension(R.styleable.LabelView_nav_lv_image_margin_right, getResources().getDimension(R.dimen.unify_space_8));
             titleText = styledAttributes.getString(R.styleable.LabelView_nav_lv_title);
             titleColorValue = styledAttributes.getColor(R.styleable.LabelView_nav_lv_title_color, ContextCompat.getColor(getContext(), com.tokopedia.unifyprinciples.R.color.Unify_N700_68));
             subtitleColorValue = styledAttributes.getColor(R.styleable.LabelView_nav_lv_sub_title_color, ContextCompat.getColor(getContext(), com.tokopedia.unifyprinciples.R.color.Unify_N700_32));
@@ -97,10 +97,10 @@ public class LabelView extends BaseCustomView {
             contentTextStyleValue = styledAttributes.getInt(R.styleable.LabelView_nav_lv_content_text_style, Typeface.NORMAL);
             titleTextStyleValue = styledAttributes.getInt(R.styleable.LabelView_nav_lv_title_text_style, Typeface.NORMAL);
             maxLines = styledAttributes.getInt(R.styleable.LabelView_nav_lv_content_max_lines, 1);
-            contentTextSize = styledAttributes.getDimension(R.styleable.LabelView_nav_lv_content_text_size, getResources().getDimension(R.dimen.sp_16));
+            contentTextSize = styledAttributes.getDimension(R.styleable.LabelView_nav_lv_content_text_size, getResources().getDimension(R.dimen.body_1));
             contentMaxWidthPercentage = styledAttributes.getFloat(R.styleable.LabelView_nav_lv_content_max_width_percentage, MAX_WIDTH_PERCENT_CONTENT);
             titleTextSize = styledAttributes.getDimension(R.styleable.LabelView_nav_lv_title_text_size, contentTextSize);
-            subTitleTextSize = styledAttributes.getDimension(R.styleable.LabelView_nav_lv_sub_title_text_size, getResources().getDimension(R.dimen.sp_12));
+            subTitleTextSize = styledAttributes.getDimension(R.styleable.LabelView_nav_lv_sub_title_text_size, getResources().getDimension(R.dimen.body_3));
             minTitleWidth = styledAttributes.getDimensionPixelSize(R.styleable.LabelView_nav_lv_title_min_width, 0);
             isArrowShown = styledAttributes.getBoolean(R.styleable.LabelView_nav_lv_show_arrow, false);
         } finally {
