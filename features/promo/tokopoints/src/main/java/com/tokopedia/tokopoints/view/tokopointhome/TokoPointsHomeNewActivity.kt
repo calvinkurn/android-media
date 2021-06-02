2145a1146564325e9b7cdf115da8b436468db1a8
kamalkumarmajhi <kamal.majhi@tokopedia.com>
2021-06-02 14:45:27 +0530

    login check

--- features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/tokopointhome/TokoPointsHomeNewActivity.kt
+++ features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/tokopointhome/TokoPointsHomeNewActivity.kt
@@ -9,23 +9,35 @@ import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.RouteManager
 import com.tokopedia.tokopoints.R
 import com.tokopedia.tokopoints.di.DaggerTokopointBundleComponent
 import com.tokopedia.tokopoints.di.TokopointBundleComponent
 import com.tokopedia.tokopoints.di.TokopointsQueryModule
 import com.tokopedia.tokopoints.view.interfaces.onAppBarCollapseListener
 import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 
 class TokoPointsHomeNewActivity : BaseSimpleActivity(), HasComponent<TokopointBundleComponent>, onAppBarCollapseListener {
     private val tokoPointComponent: TokopointBundleComponent by lazy { initInjector() }
+    private var mUserSession: UserSessionInterface? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        mUserSession = UserSession(this)
         super.onCreate(savedInstanceState)
         toolbar.visibility = View.GONE
         updateTitle(getString(R.string.tp_title_tokopoints))
     }
 
     override fun getNewFragment(): Fragment? {
-        return TokoPointsHomeFragmentNew.newInstance()
+        val tokoPointsHomeFragmentNew = TokoPointsHomeFragmentNew.newInstance()
+        return if (mUserSession?.isLoggedIn == true) {
+            tokoPointsHomeFragmentNew
+        } else {
+            startActivityForResult(RouteManager.getIntent(this, ApplinkConst.LOGIN), REQUEST_CODE_LOGIN)
+            null
+        }
     }
 
     override fun getComponent(): TokopointBundleComponent {
@@ -51,7 +63,15 @@ class TokoPointsHomeNewActivity : BaseSimpleActivity(), HasComponent<TokopointBu
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_LOGIN) {
+            inflateFragment()
+        }
+    }
+
     companion object {
+        private const val REQUEST_CODE_LOGIN = 1
         fun getCallingIntent(context: Context?): Intent {
             return Intent(context, TokoPointsHomeNewActivity::class.java)
         }
