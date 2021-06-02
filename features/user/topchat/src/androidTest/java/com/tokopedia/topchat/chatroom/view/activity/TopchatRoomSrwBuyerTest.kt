0737808cc0ab96a1d7524969a5bcf74df3a0c5ce
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-02 12:02:06 +0700

    fix priority

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
@@ -5,26 +5,30 @@ class TopchatRoomSrwBuyerTest {
     // TODO: SRW bubble should be displayed after click SRW preview
     // TODO: SRW bubble always stays at the bottom when receive response msg from ws
     // TODO: SRW bubble always stays at the bottom when receive response msg from ws from different day
+    // TODO: SRW bubble should be removed when user sent invoice
+    // TODO: SRW bubble should be removed when user receive invoice event from himself.
+    // TODO: SRW preview should not be sent when user attach image.
+    // TODO: SRW preview should not be sent when user receive attach image event from himself.
+    // TODO: What if user receive event invoice & image from seller and SRW bubble still visible?.
+
     // TODO: SRW bubble should maintain state (expand/collapse) if scrolled far top and back at it again
     // TODO: SRW should hide broadcast handler if visible
     // TODO: Template chat should be hidden when SRW bubble exist
-    // TODO: SRW bubble should be send delayed when user is in the middle of the page (from chat search)
     // TODO: SRW bubble & preview should collapsed when user open sticker
     // TODO: SRW bubble & preview should expanded when user close sticker
     // TODO: SRW bubble should be displayed when user click send sticker
-    // TODO-Alert: what if SRW in error state and user send msg/sticker
     // TODO: SRW bubble should be removed when user re-attach product
     // TODO: SRW should be collapsed/expanded when user open/close keyboard
     // TODO: SRW bubble & preview should collapsed when user open chat menu
     // TODO: SRW bubble & preview should expanded when user close chat menu
     // TODO: SRW preview should be removed when user re-attach preview with invoice
-    // TODO-Alert: SRW bubble should be removed when user sent invoice. what happened if user receive invoice event from other platform??
     // TODO: SRW Bubble should be collapsed when preview attachment is visible other than product
     // TODO: SRW Bubble should be expanded when preview attachment is not visible
-    // TODO: SRW preview should not be sent when user attach image. what happened if user receive image event from other platform??
     // TODO: SRW Bubble should be removed when user attach image
     // TODO: SRW Bubble should be displayed when user manually type msg instead of click SRW question
-    // TODO-Note: What if BE create new ws event to indicate closing of SRW if it's no longer relevant
+    // TODO: SRW bubble should be send delayed when user is in the middle of the page (from chat search)
+    // TODO-Alert: what if SRW in error state and user send msg/sticker
 
+    // TODO-Note: What if BE create new ws event to indicate closing of SRW if it's no longer relevant
     // TODO: CTA on left msg
 }
