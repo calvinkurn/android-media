9a6dafc5da1d67519b3e841b061c31dbcba88f79
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-04 03:14:26 +0700

    create choose account page for login fingerprint

--- features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/view/fragment/ChooseAccountFingerprintFragment.kt
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/view/fragment/ChooseAccountFingerprintFragment.kt
@@ -1,38 +1,153 @@
 package com.tokopedia.loginfingerprint.view.fragment
 
+import android.app.Activity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.loginfingerprint.R
-import com.tokopedia.loginfingerprint.di.LoginFingerprintComponent
-import com.tokopedia.loginphone.chooseaccount.view.fragment.ChooseAccountFragment
+import androidx.lifecycle.ViewModelProvider
+import com.tokopedia.abstraction.common.utils.snackbar.NetworkErrorHelper
+import com.tokopedia.loginfingerprint.viewmodel.ChooseAccountFingerprintViewModel
+import com.tokopedia.loginphone.chooseaccount.data.AccountList
+import com.tokopedia.loginphone.chooseaccount.data.UserDetail
+import com.tokopedia.loginphone.chooseaccount.view.fragment.BaseChooseAccountFragment
+import com.tokopedia.network.utils.ErrorHandler
+import com.tokopedia.sessioncommon.di.SessionModule
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
+import com.tokopedia.user.session.UserSessionInterface
+import javax.inject.Inject
+import javax.inject.Named
 
-class ChooseAccountFingerprintFragment: ChooseAccountFragment() {
+class ChooseAccountFingerprintFragment(val validateToken: String): BaseChooseAccountFragment() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    override fun getAccountList() {
-//        chooseAccountViewModel.getAccountListFingerprint()
+    @Named(SessionModule.SESSION_MODULE)
+    @Inject
+    lateinit var userSessionInterface: UserSessionInterface
+
+    private val viewModelProvider by lazy {
+        ViewModelProvider(this, viewModelFactory)
     }
 
-    fun onSuccessGetFingerprintAccounts() {
-
+    private val viewModel by lazy {
+        viewModelProvider.get(ChooseAccountFingerprintViewModel::class.java)
     }
 
-    fun onErrorGetFingerprintAccounts() {
+    override fun getScreenName(): String {
+        return ""
+    }
 
+    override fun onSelectedAccount(account: UserDetail, phone: String) {
+        loginBiometric(account.email, validateToken)
+    }
+
+    override fun initObserver() {
+        viewModel.getAccountListResponse.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            when (it) {
+                is Success -> onSuccessGetFingerprintAccounts(it.data)
+                is Fail -> onErrorGetFingerprintAccounts(it.throwable)
+            }
+        })
+
+        viewModel.loginBiometricResponse.observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is Success -> onSuccessLoginBiometric()
+                is Fail -> onErrorLoginBiometric(it.throwable)
+            }
+        })
+
+        viewModel.showPopup.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if (it != null) {
+                showPopupError(it.header, it.body, it.action)
+            }
+        })
+
+        viewModel.getUserInfoResponse.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            when (it) {
+                is Success -> onSuccessGetUserInfo(it.data)
+                is Fail -> {
+                    dismissLoadingProgress()
+                    onErrorGetUserInfo(it.throwable)
+                }
+            }
+        })
+
+        viewModel.showPopup.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            if (it != null) {
+                showPopupError(it.header, it.body, it.action)
+            }
+        })
+
+        viewModel.goToActivationPage.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            onGoToActivationPage()
+        })
+
+        viewModel.goToSecurityQuestion.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            onGoToSecurityQuestion()
+        })
+
+        viewModel.showAdminLocationPopUp.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            when(it) {
+                is Success -> showLocationAdminPopUp(userSessionInterface)
+                is Fail -> showLocationAdminError(it.throwable)
+            }
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getFingerprintAccounts()
+    }
+
+    private fun getFingerprintAccounts() {
+        viewModel.getAccountListFingerprint(validateToken)
+    }
+
+    private fun loginBiometric(email: String, validateToken: String) {
+        viewModel.loginTokenBiometric(email, validateToken)
+    }
+
+    private fun onSuccessGetFingerprintAccounts(accountList: AccountList) {
+        if (accountList.userDetails.size == 1 && accountList.msisdn.isNotEmpty()) {
+            adapter.setList(accountList.userDetails, accountList.msisdn)
+            val userDetail = accountList.userDetails[0]
+            loginBiometric(userDetail.email, validateToken)
+        } else {
+            dismissLoadingProgress()
+            adapter.setList(accountList.userDetails, accountList.msisdn)
+        }
+    }
+
+    private fun onErrorGetFingerprintAccounts(throwable: Throwable) {
+        dismissLoadingProgress()
+        val errorMessage = ErrorHandler.getErrorMessage(context, throwable)
+        NetworkErrorHelper.showEmptyState(context, view, errorMessage) {
+            showLoadingProgress()
+            getFingerprintAccounts()
+        }
+    }
+
+    private fun onSuccessLoginBiometric() {
+        viewModel.getUserInfo()
+    }
+
+    private fun onErrorLoginBiometric(throwable: Throwable) {
+        checkExceptionType(throwable)
+        logUnknownError(Throwable("Login Fingerprint is not success"))
+    }
+
+    override fun onSuccessLogin(userId: String) {
+        setFCM(userSessionInterface.deviceId)
+        activity?.setResult(Activity.RESULT_OK)
+        activity?.finish()
     }
 
     companion object {
-        fun createInstance(bundle: Bundle): Fragment {
-            val fragment = ChooseAccountFingerprintFragment()
-            fragment.arguments = bundle
-            return fragment
+        fun createInstance(validateToken: String): Fragment {
+            return ChooseAccountFingerprintFragment(validateToken)
         }
     }
 }
--- features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/viewmodel/ChooseAccountFingerprintViewModel.kt
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/viewmodel/ChooseAccountFingerprintViewModel.kt
@@ -2,29 +2,112 @@ package com.tokopedia.loginfingerprint.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
-import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.loginfingerprint.data.preference.FingerprintSetting
-import com.tokopedia.loginfingerprint.domain.usecase.RegisterFingerprintUseCase
-import com.tokopedia.loginfingerprint.utils.crypto.Cryptography
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.loginfingerprint.domain.usecase.LoginFingerprintUseCase
 import com.tokopedia.loginphone.chooseaccount.data.AccountList
+import com.tokopedia.loginphone.chooseaccount.data.AccountListPojo
+import com.tokopedia.loginphone.chooseaccount.di.ChooseAccountQueryConstant
+import com.tokopedia.loginphone.chooseaccount.viewmodel.BaseChooseAccountViewModel
+import com.tokopedia.loginphone.chooseaccount.viewmodel.ChooseAccountViewModel
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.sessioncommon.data.LoginToken
+import com.tokopedia.sessioncommon.di.SessionModule
+import com.tokopedia.sessioncommon.domain.usecase.GetAdminTypeUseCase
+import com.tokopedia.sessioncommon.domain.usecase.GetProfileUseCase
+import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
+import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
+import javax.inject.Named
 
-class ChooseAccountFingerprintViewModel @Inject constructor(dispatcher: CoroutineDispatchers,
-                                                      private val userSession: UserSessionInterface,
-                                                      private val cryptographyUtils: Cryptography?,
-                                                      private val fingerprintSetting: FingerprintSetting,
-                                                      private val registerFingerprintUseCase: RegisterFingerprintUseCase
-)
-    : BaseViewModel(dispatcher.io){
+class ChooseAccountFingerprintViewModel @Inject constructor(
+    private val loginFingerprintUseCase: LoginFingerprintUseCase,
+    private val getAccountsListPojoUseCase: GraphqlUseCase<AccountListPojo>,
+    @param:Named(SessionModule.SESSION_MODULE) private val userSessionInterface: UserSessionInterface,
+    getProfileUseCase: GetProfileUseCase,
+    getAdminTypeUseCase: GetAdminTypeUseCase,
+    private val rawQueries: Map<String, String>,
+    dispatcher: CoroutineDispatcher
+): BaseChooseAccountViewModel(userSessionInterface, getProfileUseCase, getAdminTypeUseCase, dispatcher) {
 
     private val mutableGetAccountListResponse = MutableLiveData<Result<AccountList>>()
     val getAccountListResponse: LiveData<Result<AccountList>>
         get() = mutableGetAccountListResponse
 
-    fun getFingerprintUserList() {
+    private val mutableLoginBiometricResponse = MutableLiveData<Result<LoginToken>>()
+    val loginBiometricResponse: LiveData<Result<LoginToken>>
+        get() = mutableLoginBiometricResponse
 
+    fun getAccountListFingerprint(validateToken: String) {
+        rawQueries[ChooseAccountQueryConstant.QUERY_GET_ACCOUNT_LIST]?.let { query ->
+            val params = mapOf(
+                ChooseAccountQueryConstant.PARAM_VALIDATE_TOKEN to validateToken,
+                ChooseAccountQueryConstant.PARAM_PHONE to "",
+                ChooseAccountQueryConstant.PARAM_LOGIN_TYPE to ChooseAccountViewModel.LOGIN_TYPE_BIOMETRIC
+            )
+
+            getAccountsListPojoUseCase.apply {
+                setTypeClass(AccountListPojo::class.java)
+                setRequestParams(params)
+                setGraphqlQuery(query)
+                execute(
+                    onSuccessGetAccountListBiometric(),
+                    onFailedGetAccountListBiometric()
+                )
+            }
+        }
+    }
+
+    private fun onSuccessGetAccountListBiometric(): (AccountListPojo) -> Unit {
+        return {
+            if (it.accountList.errors.isEmpty()) {
+                mutableGetAccountListResponse.value = Success(it.accountList)
+            } else if (it.accountList.errors[0].message.isNotEmpty()) {
+                mutableGetAccountListResponse.value =
+                    Fail(MessageErrorException(it.accountList.errors[0].message))
+            } else {
+                mutableGetAccountListResponse.value = Fail(RuntimeException())
+            }
+        }
+    }
+
+    private fun onFailedGetAccountListBiometric(): (Throwable) -> Unit {
+        return {
+            mutableGetAccountListResponse.value = Fail(it)
+        }
+    }
+
+    fun loginTokenBiometric(email: String, validateToken: String) {
+        loginFingerprintUseCase.loginBiometric(email, validateToken,
+            onSuccessLoginBiometric(),
+            onFailedLoginBiometric(),
+            { showPopup().invoke(it.popupError) },
+            onGoToActivationPage(),
+            onGoToSecurityQuestion("")
+        )
+    }
+
+    private fun onSuccessLoginBiometric(): (LoginToken) -> Unit {
+        return {
+            if (it.accessToken.isNotEmpty() &&
+                it.refreshToken.isNotEmpty() &&
+                it.tokenType.isNotEmpty()) {
+                mutableLoginBiometricResponse.value = Success(it)
+            } else if (it.errors.isNotEmpty() &&
+                it.errors[0].message.isNotEmpty()) {
+                mutableLoginBiometricResponse.value = Fail(MessageErrorException(it.errors[0].message))
+            } else {
+                mutableLoginBiometricResponse.value = Fail(RuntimeException())
+            }
+        }
+    }
+
+    private fun onFailedLoginBiometric(): (Throwable) -> Unit {
+        return {
+            userSessionInterface.clearToken()
+            mutableLoginBiometricResponse.value = Fail(it)
+        }
     }
 }
