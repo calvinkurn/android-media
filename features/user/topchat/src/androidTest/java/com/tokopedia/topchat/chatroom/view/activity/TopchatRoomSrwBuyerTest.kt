1548ab46b56765bad9023956c9b06b57ff541c02
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-03 14:08:38 +0700

    move SRW preview tests to TopchatRoomSrwBuyerTest

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
@@ -1,6 +1,28 @@
 package com.tokopedia.topchat.chatroom.view.activity
 
-class TopchatRoomSrwBuyerTest {
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.ApplinkConst.AttachProduct.TOKOPEDIA_ATTACH_PRODUCT_SOURCE_KEY
+import com.tokopedia.chat_common.data.preview.ProductPreview
+import com.tokopedia.common.network.util.CommonUtil
+import com.tokopedia.topchat.R
+import com.tokopedia.topchat.chatroom.view.activity.base.BaseBuyerTopchatRoomTest
+import com.tokopedia.topchat.chatroom.view.activity.base.hasQuestion
+import com.tokopedia.topchat.common.TopChatInternalRouter.Companion.SOURCE_TOPCHAT
+import org.hamcrest.Matchers.not
+import org.junit.Before
+import org.junit.Test
+
+class TopchatRoomSrwBuyerTest : BaseBuyerTopchatRoomTest() {
 
     // TODO: SRW bubble should displayed after click SRW preview
     // TODO: SRW bubble should always stays at the bottom when receive response msg from ws
@@ -32,4 +54,386 @@ class TopchatRoomSrwBuyerTest {
 
     // TODO-Note: What if BE create new ws event to indicate closing of SRW if it's no longer relevant
     // TODO: CTA on left msg
+
+    lateinit var productPreview: ProductPreview
+
+    @Before
+    override fun before() {
+        super.before()
+        productPreview = ProductPreview(
+            "1111",
+            ProductPreviewAttribute.productThumbnail,
+            ProductPreviewAttribute.productName,
+            "Rp 23.000.000",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "tokopedia://product/1111",
+            false,
+            "",
+            "Rp 50.000.000",
+            500000,
+            "50%",
+            false
+        )
+    }
+
+
+    @Test
+    fun srw_displayed_if_buyer_attach_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwTitle(chatSrwResponse.chatSmartReplyQuestion.title)
+        assertSrwTotalQuestion(1)
+    }
+
+    @Test
+    fun srw_displayed_with_multiple_questions_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponseMultipleQuestion
+        inflateTestFragment()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwTitle(chatSrwResponse.chatSmartReplyQuestion.title)
+        assertSrwTotalQuestion(3)
+    }
+
+    @Test
+    fun srw_displayed_if_buyer_attach_from_attach_product() {
+        // Given
+        setupChatRoomActivity()
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+        intendingAttachProduct(1)
+
+        // When
+        clickPlusIconMenu()
+        clickAttachProductMenu()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwTitle(chatSrwResponse.chatSmartReplyQuestion.title)
+        assertSrwTotalQuestion(1)
+    }
+
+    @Test
+    fun srw_no_question_not_displayed_if_buyer_attach_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse.hasQuestion(false)
+        inflateTestFragment()
+
+        // Then
+        assertSrwPreviewContentIsHidden()
+        assertTemplateChatVisibility(isDisplayed())
+    }
+
+    @Test
+    fun template_chat_shown_if_srw_load_finish_first_with_no_question() {
+        // Given
+        val templateChats = listOf(
+            "Hi barang ini ready gk?", "Lorem Ipsum"
+        )
+        val templateResponse = generateTemplateResponse(templates = templateChats)
+        val templateDelay = 500L
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse.hasQuestion(false)
+        getTemplateChatRoomUseCase.setResponse(templateResponse, templateDelay)
+        inflateTestFragment()
+
+        // Then
+        // Simulate template chat load last after SRW
+        waitForIt(templateDelay + 10)
+
+        // Then
+        assertSrwPreviewContentIsHidden()
+        assertTemplateChatVisibility(isDisplayed())
+    }
+
+    @Test
+    fun template_chat_remain_hidden_if_user_click_send_btn_when_srw_shown() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // Then
+        clickSendBtn()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+    }
+
+    @Test
+    fun srw_no_question_not_displayed_if_buyer_attach_from_attach_product() {
+        // Given
+        setupChatRoomActivity()
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse.hasQuestion(false)
+        inflateTestFragment()
+        intendingAttachProduct(1)
+
+        // When
+        clickPlusIconMenu()
+        clickAttachProductMenu()
+
+        // Then
+        assertSrwPreviewContentIsHidden()
+        assertTemplateChatVisibility(isDisplayed())
+    }
+
+    @Test
+    fun srw_loading_state_displayed_if_buyer_attach_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.setResponseWithDelay(
+            chatSrwResponse, 1500
+        )
+        inflateTestFragment()
+
+        // Then
+        assertTemplateChatVisibility(not(isDisplayed()))
+        assertSrwPreviewContentIsLoading()
+    }
+
+    @Test
+    fun srw_error_state_displayed_if_buyer_attach_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.isError = true
+        inflateTestFragment()
+
+        // Then
+        assertSrwPreviewContentIsError()
+    }
+
+    @Test
+    fun load_srw_from_error_state_if_buyer_attach_from_start_intent() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.isError = true
+        inflateTestFragment()
+
+        // When
+        chatSrwUseCase.isError = false
+        chatSrwUseCase.response = chatSrwResponse
+        onView(withId(com.tokopedia.unifycomponents.R.id.refreshID))
+            .perform(click())
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+    }
+
+    @Test
+    fun assert_srw_expand_collapse_interaction() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwExpanded()
+
+        // When
+        onView(withId(R.id.tp_srw_container_partial)).perform(click())
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwCollapsed()
+
+        // When
+        onView(withId(R.id.tp_srw_container_partial)).perform(click())
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwExpanded()
+    }
+
+    @Test
+    fun srw_content_hidden_if_content_is_clicked() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // When
+        clickSrwPreviewItemAt(0)
+
+        // Then
+        assertSrwPreviewContentIsHidden()
+        assertTemplateChatVisibility(not(isDisplayed()))
+    }
+
+    @Test
+    fun collapse_srw_when_user_open_keyboard() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // When
+        clickComposeArea()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwCollapsed()
+    }
+
+    @Test
+    fun expand_srw_when_user_hide_keyboard() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // When
+        clickComposeArea()
+        pressBack()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwExpanded()
+    }
+
+    @Test
+    fun expand_srw_when_keyboard_opened() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // When
+        clickComposeArea()
+        onView(withId(R.id.tp_srw_container_partial)).perform(click())
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwExpanded()
+        assertKeyboardIsNotVisible()
+    }
+
+    @Test
+    fun collapse_srw_when_keyboard_opened() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+
+        // When
+        clickComposeArea()
+        onView(withId(R.id.tp_srw_container_partial)).perform(click())
+        onView(withId(R.id.tp_srw_container_partial)).perform(click())
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwCollapsed()
+    }
+
+    /**
+     * SRW should expanded when reattach product
+     * when previous product preview is collapsed and canceled
+     */
+    @Test
+    fun srw_should_expanded_when_reattach_product() {
+        // Given
+        setupChatRoomActivity {
+            putProductAttachmentIntent(it)
+        }
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        chatSrwUseCase.response = chatSrwResponse
+        inflateTestFragment()
+        intending(hasExtra(TOKOPEDIA_ATTACH_PRODUCT_SOURCE_KEY, SOURCE_TOPCHAT))
+            .respondWith(
+                Instrumentation.ActivityResult(
+                    Activity.RESULT_OK, getAttachProductData(1)
+                )
+            )
+
+        // When
+        clickComposeArea()
+        clickCloseAttachmentPreview(0)
+        pressBack()
+        clickPlusIconMenu()
+        clickAttachProductMenu()
+
+        // Then
+        assertSrwPreviewContentIsVisible()
+        assertSrwExpanded()
+    }
+
+    private fun putProductAttachmentIntent(intent: Intent) {
+        val productPreviews = listOf(productPreview)
+        val stringProductPreviews = CommonUtil.toJson(productPreviews)
+        intent.putExtra(ApplinkConst.Chat.PRODUCT_PREVIEWS, stringProductPreviews)
+    }
 }
