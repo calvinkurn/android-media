df24c880c134cdd4173d2333b7e228f9a9248461
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-04 03:12:37 +0700

    extend base account

--- features/user/loginphone/src/main/java/com/tokopedia/loginphone/chooseaccount/viewmodel/ChooseAccountViewModel.kt
+++ features/user/loginphone/src/main/java/com/tokopedia/loginphone/chooseaccount/viewmodel/ChooseAccountViewModel.kt
@@ -2,7 +2,6 @@ package com.tokopedia.loginphone.chooseaccount.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.loginphone.chooseaccount.data.AccountList
 import com.tokopedia.loginphone.chooseaccount.data.AccountListPojo
@@ -14,11 +13,7 @@ import com.tokopedia.loginphone.chooseaccount.domain.subscriber.LoginFacebookSub
 import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.sessioncommon.data.LoginToken
 import com.tokopedia.sessioncommon.data.LoginTokenPojo
-import com.tokopedia.sessioncommon.data.PopupError
-import com.tokopedia.sessioncommon.data.profile.ProfileInfo
-import com.tokopedia.sessioncommon.data.profile.ProfilePojo
 import com.tokopedia.sessioncommon.di.SessionModule
-import com.tokopedia.sessioncommon.domain.subscriber.GetProfileSubscriber
 import com.tokopedia.sessioncommon.domain.subscriber.LoginTokenSubscriber
 import com.tokopedia.sessioncommon.domain.usecase.GetAdminTypeUseCase
 import com.tokopedia.sessioncommon.domain.usecase.GetProfileUseCase
@@ -36,15 +31,15 @@ import javax.inject.Named
  * ade.hadian@tokopedia.com
  */
 
-class ChooseAccountViewModel @Inject constructor(
-        private val getAccountsListPojoUseCase: GraphqlUseCase<AccountListPojo>,
-        @param:Named(SessionModule.SESSION_MODULE) private val userSessionInterface: UserSessionInterface,
-        private val loginTokenUseCase: LoginTokenUseCase,
-        private val getProfileUseCase: GetProfileUseCase,
-        private val getAdminTypeUseCase: GetAdminTypeUseCase,
-        private val rawQueries: Map<String, String>,
-        dispatcher: CoroutineDispatcher
-) : BaseViewModel(dispatcher) {
+open class ChooseAccountViewModel @Inject constructor(
+    private val getAccountsListPojoUseCase: GraphqlUseCase<AccountListPojo>,
+    @param:Named(SessionModule.SESSION_MODULE) private val userSessionInterface: UserSessionInterface,
+    private val loginTokenUseCase: LoginTokenUseCase,
+    getProfileUseCase: GetProfileUseCase,
+    getAdminTypeUseCase: GetAdminTypeUseCase,
+    private val rawQueries: Map<String, String>,
+    dispatcher: CoroutineDispatcher
+) : BaseChooseAccountViewModel(userSessionInterface, getProfileUseCase, getAdminTypeUseCase, dispatcher) {
 
     private val mutableGetAccountListFBResponse = MutableLiveData<Result<AccountList>>()
     val getAccountListFBResponse: LiveData<Result<AccountList>>
@@ -54,33 +49,9 @@ class ChooseAccountViewModel @Inject constructor(
     val getAccountListPhoneResponse: LiveData<Result<AccountList>>
         get() = mutableGetAccountListPhoneResponse
 
-    private val mutableGetAccountListFingerprint = MutableLiveData<Result<AccountList>>()
-    val getAccountListFingerprintResponse: LiveData<Result<AccountList>>
-        get() = mutableGetAccountListFingerprint
-
     private val mutableLoginPhoneNumberResponse = MutableLiveData<Result<LoginToken>>()
     val loginPhoneNumberResponse: LiveData<Result<LoginToken>>
         get() = mutableLoginPhoneNumberResponse
-
-    private val mutableGetUserInfoResponse = MutableLiveData<Result<ProfileInfo>>()
-    val getUserInfoResponse: LiveData<Result<ProfileInfo>>
-        get() = mutableGetUserInfoResponse
-
-    private val mutableShowPopup = MutableLiveData<PopupError>()
-    val showPopup: LiveData<PopupError>
-        get() = mutableShowPopup
-
-    private val mutableGoToActivationPage = MutableLiveData<MessageErrorException>()
-    val goToActivationPage: LiveData<MessageErrorException>
-        get() = mutableGoToActivationPage
-
-    private val mutableGoToSecurityQuestion = MutableLiveData<String>()
-    val goToSecurityQuestion: LiveData<String>
-        get() = mutableGoToSecurityQuestion
-
-    private val mutableShowAdminLocationPopUp = MutableLiveData<Result<Boolean>>()
-    val showAdminLocationPopUp: LiveData<Result<Boolean>>
-        get() = mutableShowAdminLocationPopUp
 
     fun loginTokenPhone(key: String, email: String, phoneNumber: String) {
         loginTokenUseCase.executeLoginPhoneNumber(LoginTokenUseCase.generateParamLoginPhone(
@@ -145,33 +116,13 @@ class ChooseAccountViewModel @Inject constructor(
                 setRequestParams(params)
                 setGraphqlQuery(query)
                 execute(
-                        onSuccessGetAccountListFacebook(),
-                        onFailedGetAccountListFacebook()
+                    onSuccessGetAccountListFacebook(),
+                    onFailedGetAccountListFacebook()
                 )
             }
         }
     }
 
-    fun getAccountListFingerprint() {
-
-    }
-
-    fun getUserInfo() {
-        getProfileUseCase.execute(GetProfileSubscriber(userSessionInterface,
-                onSuccessGetUserInfo(),
-                onFailedGetUserInfo(),
-                getAdminTypeUseCase,
-                showLocationAdminPopUp(),
-                showGetAdminTypeError()))
-    }
-
-    private fun showLocationAdminPopUp(): (() -> Unit) = {
-        mutableShowAdminLocationPopUp.value = Success(true)
-    }
-
-    private fun showGetAdminTypeError(): ((e: Throwable) -> Unit) = {
-        mutableShowAdminLocationPopUp.value = Fail(it)
-    }
 
     private fun onSuccessLoginToken(): (LoginTokenPojo) -> Unit {
         return {
@@ -233,44 +184,14 @@ class ChooseAccountViewModel @Inject constructor(
         }
     }
 
-    private fun onSuccessGetUserInfo(): (ProfilePojo) -> Unit {
-        return {
-            mutableGetUserInfoResponse.value = Success(it.profileInfo)
-        }
-    }
-
-    private fun onFailedGetUserInfo(): (Throwable) -> Unit {
-        return {
-            mutableGetUserInfoResponse.value = Fail(it)
-        }
-    }
-
-    private fun showPopup(): (PopupError) -> Unit {
-        return {
-            mutableShowPopup.value = it
-        }
-    }
-
-    private fun onGoToActivationPage(): (MessageErrorException) -> Unit {
-        return {
-            mutableGoToActivationPage.value = it
-        }
-    }
-
-    private fun onGoToSecurityQuestion(phone: String): () -> Unit {
-        return {
-            mutableGoToSecurityQuestion.value = phone
-        }
-    }
-
     override fun onCleared() {
         super.onCleared()
         getAccountsListPojoUseCase.cancelJobs()
         loginTokenUseCase.unsubscribe()
-        getProfileUseCase.unsubscribe()
     }
 
     companion object {
         const val LOGIN_TYPE_FACEBOOK = "fb"
+        const val LOGIN_TYPE_BIOMETRIC = "biometric"
     }
 }
