e5d9b998584b97d8f8835d01a21e48e04320da7b
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-07 01:55:44 +0700

    inject

--- features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/di/LoginFingerprintComponent.kt
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/di/LoginFingerprintComponent.kt
@@ -4,6 +4,7 @@ import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.loginfingerprint.view.dialog.ScanFingerprintDialog
 import com.tokopedia.loginfingerprint.view.fragment.ChooseAccountFingerprintFragment
 import com.tokopedia.loginfingerprint.view.fragment.RegisterFingerprintOnboardingFragment
+import com.tokopedia.loginfingerprint.view.fragment.SettingFingerprintFragment
 import com.tokopedia.sessioncommon.di.SessionCommonScope
 import com.tokopedia.sessioncommon.di.SessionModule
 import dagger.Component
@@ -21,4 +22,5 @@ interface LoginFingerprintComponent {
     fun inject(fragment: ScanFingerprintDialog)
     fun inject(fragment: RegisterFingerprintOnboardingFragment)
     fun inject(fragment: ChooseAccountFingerprintFragment)
+    fun inject(fingerprintFragment: SettingFingerprintFragment)
 }
