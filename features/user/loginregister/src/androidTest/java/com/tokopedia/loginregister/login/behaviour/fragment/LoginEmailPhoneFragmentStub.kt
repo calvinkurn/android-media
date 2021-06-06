92f8478d01cbb590c4051bedd748928265e26f8c
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-06 23:27:26 +0700

    override routing

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/fragment/LoginEmailPhoneFragmentStub.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/fragment/LoginEmailPhoneFragmentStub.kt
@@ -1,7 +1,11 @@
 package com.tokopedia.loginregister.login.behaviour.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.tokopedia.loginregister.login.behaviour.activity.ChangeNameActivityStub
+import com.tokopedia.loginregister.login.behaviour.activity.ChooseAccountActivityStub
+import com.tokopedia.loginregister.login.behaviour.activity.VerificationActivityStub
 import com.tokopedia.loginregister.login.view.fragment.LoginEmailPhoneFragment
 
 class LoginEmailPhoneFragmentStub: LoginEmailPhoneFragment() {
@@ -12,6 +16,19 @@ class LoginEmailPhoneFragmentStub: LoginEmailPhoneFragment() {
             fragment.arguments = bundle
             return fragment
         }
+    }
+
+    override fun goToChooseAccountPage(accessToken: String, phoneNumber: String) {
+        val intent = Intent(activity, ChooseAccountActivityStub::class.java)
+        startActivityForResult(intent, REQUEST_CHOOSE_ACCOUNT)
+    }
+
+    override fun goToLoginPhoneVerifyPage(phoneNumber: String) {
+        startActivityForResult(Intent(activity, VerificationActivityStub::class.java), REQUEST_LOGIN_PHONE)
+    }
+
+    override fun onGoToChangeName() {
+        startActivity(Intent(activity, ChangeNameActivityStub::class.java))
     }
 
     override fun refreshRolloutVariant() {
