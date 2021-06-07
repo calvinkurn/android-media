6a27f6c9bc323206d2571df02de9924f6f637489
kenny <kenny.hadisaputra@tokopedia.com>
2021-06-07 17:25:05 +0700

    handle youtube freeze

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -328,7 +328,7 @@ class PlayViewModel @Inject constructor(
     }
 
     private val videoStateProcessor = videoStateProcessorFactory.create(playVideoPlayer, viewModelScope) { channelType }
-    private val channelStateProcessor = channelStateProcessorFactory.create(playVideoPlayer, viewModelScope) { channelType }
+    private val channelStateProcessor = channelStateProcessorFactory.create(playVideoPlayer, viewModelScope, { channelType }, { videoPlayer })
     private val videoBufferGovernor = videoBufferGovernorFactory.create(playVideoPlayer, viewModelScope)
 
     init {
