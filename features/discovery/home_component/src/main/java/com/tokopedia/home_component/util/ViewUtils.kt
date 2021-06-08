4d32e962344f88eb8425cc7c868d6873071c1432
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-06-08 09:18:48 +0700

    hotfix ui feedback for margin

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/ViewUtils.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/ViewUtils.kt
@@ -2,19 +2,10 @@ package com.tokopedia.home_component.util
 
 import android.content.Context
 import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
 import android.graphics.drawable.*
-import android.graphics.drawable.shapes.RoundRectShape
 import android.util.TypedValue
-import android.view.Gravity
 import android.view.View
-import android.view.View.LAYER_TYPE_SOFTWARE
-import androidx.annotation.ColorRes
-import androidx.annotation.DimenRes
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.home_component.R
 import com.tokopedia.home_component.model.ChannelConfig
 import com.tokopedia.home_component.model.ChannelModel
 import com.tokopedia.kotlin.extensions.view.gone
@@ -30,19 +21,6 @@ object ChannelWidgetUtil {
         dividerTop: DividerUnify?,
         dividerBottom: DividerUnify?
     ) {
-//        dividerBottom?.let {
-//            it.background =
-//                ColorDrawable(
-//                    ContextCompat.getColor(it.context, R.color.Blue_B500)
-//                )
-//        }
-//
-//        dividerTop?.let {
-//            it.background =
-//                ColorDrawable(
-//                    ContextCompat.getColor(it.context, R.color.Unify_G500_96)
-//                )
-//        }
         when(channelModel?.channelConfig?.dividerType) {
             ChannelConfig.DIVIDER_NO_DIVIDER -> {
                 dividerTop?.gone()
