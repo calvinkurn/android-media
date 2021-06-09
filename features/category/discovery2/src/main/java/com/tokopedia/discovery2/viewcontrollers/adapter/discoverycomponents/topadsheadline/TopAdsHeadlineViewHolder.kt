20fd3ff0deb2b0c07227ff55badefe2267aa8f17
niranjan <niranjan.saxena@tokopedia.com>
2021-06-09 17:57:42 +0530

    gtm added for topads headline

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/topadsheadline/TopAdsHeadlineViewHolder.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/topadsheadline/TopAdsHeadlineViewHolder.kt
@@ -6,16 +6,26 @@ import com.tokopedia.discovery2.R
 import com.tokopedia.discovery2.di.getSubComponent
 import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
 import com.tokopedia.discovery2.viewcontrollers.adapter.viewholder.AbstractViewHolder
+import com.tokopedia.discovery2.viewcontrollers.fragment.DiscoveryFragment
+import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.model.ImpressHolder
+import com.tokopedia.topads.sdk.domain.model.CpmData
 import com.tokopedia.topads.sdk.domain.model.CpmModel
+import com.tokopedia.topads.sdk.domain.model.Product
+import com.tokopedia.topads.sdk.listener.TopAdsBannerClickListener
+import com.tokopedia.topads.sdk.listener.TopAdsItemImpressionListener
 import com.tokopedia.topads.sdk.widget.TopAdsHeadlineView
 
-class TopAdsHeadlineViewHolder (itemView: View, private val fragment: Fragment) : AbstractViewHolder(itemView, fragment.viewLifecycleOwner) {
+class TopAdsHeadlineViewHolder (itemView: View, private val fragment: Fragment) : AbstractViewHolder(itemView, fragment.viewLifecycleOwner), TopAdsBannerClickListener {
 
     private lateinit var topAdsHeadlineViewModel: TopAdsHeadlineViewModel
     private val topadsHeadlineView: TopAdsHeadlineView = itemView.findViewById(R.id.topads_headline_view)
 
+    init {
+//        topadsHeadlineView.setTopAdsBannerClickListener(this)
+    }
 
     override fun bindView(discoveryBaseViewModel: DiscoveryBaseViewModel) {
         topAdsHeadlineViewModel = discoveryBaseViewModel as TopAdsHeadlineViewModel
@@ -42,5 +52,16 @@ class TopAdsHeadlineViewHolder (itemView: View, private val fragment: Fragment) 
         topadsHeadlineView.hideShimmerView()
         topadsHeadlineView.show()
         topadsHeadlineView.displayAds(cpmModel)
+        cpmModel.let {
+            topadsHeadlineView.addOnImpressionListener(ImpressHolder()) {
+                (fragment as DiscoveryFragment).getDiscoveryAnalytics().onTopadsHeadlineImpression(it, adapterPosition)
+            }
+        }
+    }
+
+    override fun onBannerAdsClicked(position: Int, applink: String?, data: CpmData?) {
+        data?.let {
+            (fragment as DiscoveryFragment).getDiscoveryAnalytics().onTopAdsHeadlineAdsClick(position, applink, it)
+        }
     }
 }
