592d9cb69d7ca8960a8a2b9928cf76b1552facd1
Muhammad Furqan <furqan@tokopedia.com>
2021-06-09 20:37:35 +0700

    [Flight] Convert Files inside package Common Data from Java to Kotlin

---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/common/data/source/FlightAuthInterceptor.kt
@@ -0,0 +1,75 @@
+package com.tokopedia.flight.common.data.source
+
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.utils.network.AuthUtil
+import com.tokopedia.network.NetworkRouter
+import com.tokopedia.network.constant.ErrorNetMessage
+import com.tokopedia.network.interceptor.TkpdAuthInterceptor
+import com.tokopedia.user.session.UserSessionInterface
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
+import okio.GzipSource
+import okio.buffer
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+/**
+ * @author by furqan on 09/06/2021
+ */
+class FlightAuthInterceptor @Inject constructor(@ApplicationContext context: Context,
+                                                networkRouter: NetworkRouter,
+                                                userSessionInterface: UserSessionInterface)
+    : TkpdAuthInterceptor(context, networkRouter, userSessionInterface) {
+
+    init {
+        maxRetryAttempt = 0
+    }
+
+    override fun getHeaderMap(path: String?, strParam: String?, method: String?, authKey: String?, contentTypeHeader: String?): MutableMap<String, String> {
+        val newPath = path?.replace("/travel", "") ?: ""
+        this.authKey = AuthUtil.KEY.KEY_WSV4
+        return AuthUtil.generateHeadersWithXUserId(newPath, strParam, method, this.authKey,
+                contentTypeHeader, userSession.userId, userSession.deviceId, userSession)
+    }
+
+    override fun getResponse(chain: Interceptor.Chain?, request: Request?): Response {
+        val oldResponse = super.getResponse(chain, request)
+        val contentEncoding = oldResponse.header(PARAM_CONTENT_ENCODING)
+        contentEncoding?.let {
+            if (it.equals(KEY_ZIP_ENCODING, true)) {
+                oldResponse.body()?.let { oldBody ->
+                    val source: GzipSource = GzipSource(oldBody.source())
+                    val bodyString = source.buffer().readUtf8()
+                    print(bodyString)
+
+                    val responseBody = ResponseBody.create(oldBody.contentType(), bodyString)
+
+                    val builder = Response.Builder()
+                    builder.body(responseBody)
+                            .headers(oldResponse.headers())
+                            .message(oldResponse.message())
+                            .handshake(oldResponse.handshake())
+                            .protocol(oldResponse.protocol())
+                            .cacheResponse(oldResponse.cacheResponse())
+                            .priorResponse(oldResponse.priorResponse())
+                            .code(oldResponse.code())
+                            .request(oldResponse.request())
+                            .networkResponse(oldResponse.networkResponse())
+
+                    return builder.build()
+                }
+                throw UnknownHostException(ErrorNetMessage.MESSAGE_ERROR_DEFAULT)
+            }
+        }
+        return oldResponse
+    }
+
+    companion object {
+        private const val PARAM_CONTENT_ENCODING = "Content-Encoding"
+        private const val KEY_ZIP_ENCODING = "gzip"
+    }
+
+}
--- features/travel/flight/src/main/java/com/tokopedia/flight/common/data/source/cloud/api/retrofit/StringResponseConverter.kt
+++ features/travel/flight/src/main/java/com/tokopedia/flight/common/data/source/cloud/api/retrofit/StringResponseConverter.kt
@@ -1,44 +1,26 @@
-package com.tokopedia.flight.common.data.source.cloud.api.retrofit;
+package com.tokopedia.flight.common.data.source.cloud.api.retrofit
 
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import okhttp3.ResponseBody
+import retrofit2.Converter
+import retrofit2.Retrofit
+import java.lang.reflect.Type
 
-import okhttp3.MediaType;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Converter;
-import retrofit2.Retrofit;
-
-public class StringResponseConverter extends Converter.Factory {
-    private static final MediaType MEDIA_TYPE = MediaType.parse("text/plain");
-
-    @Override
-    public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
-        if (String.class == type) {
-            return new Converter<ResponseBody, String>() {
-                @Override
-                public String convert(ResponseBody value) throws IOException {
-                    return value.string();
-                }
-            };
-        }
-        return null;
+class StringResponseConverter : Converter.Factory() {
+    override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, *>? {
+        return if (String::class.java == type) {
+            Converter { value -> value.string() }
+        } else null
     }
 
-    @Override
-    public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
-        if (String.class == type) {
-            return new Converter<String, RequestBody>() {
-                @Override
-                public RequestBody convert(String value) throws IOException {
-                    return RequestBody.create(MEDIA_TYPE, value);
-                }
-            };
-        }
-        return null;
+    override fun requestBodyConverter(type: Type, parameterAnnotations: Array<Annotation>, methodAnnotations: Array<Annotation>, retrofit: Retrofit): Converter<*, RequestBody>? {
+        return if (String::class.java == type) {
+            Converter<String?, RequestBody> { value -> RequestBody.create(MEDIA_TYPE, value) }
+        } else null
     }
 
-
+    companion object {
+        private val MEDIA_TYPE = MediaType.parse("text/plain")
+    }
 }
-
