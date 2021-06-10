3fe427386dfb014655b00b29be244da0ca0f950d
yoasfs <yoas.susantio@tokopedia.com>
2021-06-10 20:03:26 +0700

    remove hardcode

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/fragment/OfficialHomeContainerFragment.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/category/presentation/fragment/OfficialHomeContainerFragment.kt
@@ -467,8 +467,7 @@ class OfficialHomeContainerFragment
     }
 
     private fun isChooseAddressRollenceActive(): Boolean {
-        return true
-//        return ChooseAddressUtils.isRollOutUser(requireContext())
+        return ChooseAddressUtils.isRollOutUser(requireContext())
     }
 
     private fun getChooseAddressWidget(): ChooseAddressWidget? {
