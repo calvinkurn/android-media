3afd872f8f06e5b6cd5af0d4e9dfcba2f6df61bc
Felicia Widjaja <fwidjaja@live.com>
2021-06-10 16:44:50 +0700

    change shop_id from int to Long

--- features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/data/response/GetCouponListResponse.kt
+++ features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/data/response/GetCouponListResponse.kt
@@ -166,7 +166,7 @@ data class Coupon(
         @SerializedName("unique_id")
         val uniqueId: String = "",
         @SerializedName("shop_id")
-        val shopId: Int = 0,
+        val shopId: Long = 0,
         @SerializedName("tag_image_urls")
         val tagImageUrls: List<String> = emptyList(),
         @SerializedName("benefit_amount")
