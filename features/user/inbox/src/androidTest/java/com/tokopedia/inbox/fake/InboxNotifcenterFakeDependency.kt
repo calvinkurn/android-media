0eed8ec1f450ddf56485efb3f088ff9c5937678f
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-10 13:14:40 +0700

    create FakeNotifcenterDetailUseCase.kt

--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/InboxNotifcenterFakeDependency.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/InboxNotifcenterFakeDependency.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.inbox.fake
 
 import com.tokopedia.inbox.fake.common.FakeUserSession
+import com.tokopedia.inbox.fake.domain.usecase.notifcenter.FakeNotifOrderListUseCase
 import com.tokopedia.inbox.fake.domain.usecase.notifcenter.FakeNotifcenterCacheManager
+import com.tokopedia.inbox.fake.domain.usecase.notifcenter.FakeNotifcenterDetailUseCase
 import javax.inject.Inject
 
 class InboxNotifcenterFakeDependency {
@@ -11,6 +13,12 @@ class InboxNotifcenterFakeDependency {
 
     @Inject
     lateinit var cacheManager: FakeNotifcenterCacheManager
+
+    @Inject
+    lateinit var notifOrderListUseCase: FakeNotifOrderListUseCase
+
+    @Inject
+    lateinit var notifcenterDetailUseCase: FakeNotifcenterDetailUseCase
 
     fun init() {
 //        initResponse()
--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/di/notifcenter/FakeNotifcenterUsecase.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/di/notifcenter/FakeNotifcenterUsecase.kt
@@ -1,11 +1,16 @@
 package com.tokopedia.inbox.fake.di.notifcenter
 
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.inbox.fake.common.FakeGraphqlUseCase
 import com.tokopedia.inbox.fake.domain.usecase.notifcenter.FakeNotifOrderListUseCase
+import com.tokopedia.inbox.fake.domain.usecase.notifcenter.FakeNotifcenterDetailUseCase
 import com.tokopedia.notifcenter.common.network.NotifcenterCacheManager
+import com.tokopedia.notifcenter.data.entity.notification.NotifcenterDetailResponse
 import com.tokopedia.notifcenter.data.entity.orderlist.NotifOrderListResponse
+import com.tokopedia.notifcenter.data.mapper.NotifcenterDetailMapper
 import com.tokopedia.notifcenter.di.scope.NotificationScope
 import com.tokopedia.notifcenter.domain.NotifOrderListUseCase
+import com.tokopedia.notifcenter.domain.NotifcenterDetailUseCase
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
@@ -36,4 +41,27 @@ class FakeNotifcenterUsecase {
         )
     }
 
+    // -- separator -- //
+
+    @Provides
+    @NotificationScope
+    fun provideNotifcenterDetailUseCase(
+        fake: FakeNotifcenterDetailUseCase
+    ): NotifcenterDetailUseCase {
+        return fake
+    }
+
+    @Provides
+    @NotificationScope
+    fun provideFakeNotifcenterDetailUseCase(
+        @Named(NotifcenterDetailUseCase.QUERY_NOTIFCENTER_DETAIL_V3)
+        query: String,
+        gqlUseCase: FakeGraphqlUseCase<NotifcenterDetailResponse>,
+        mapper: NotifcenterDetailMapper,
+        dispatchers: CoroutineDispatchers
+    ): FakeNotifcenterDetailUseCase {
+        return FakeNotifcenterDetailUseCase(
+            query, gqlUseCase, mapper, dispatchers
+        )
+    }
 }
