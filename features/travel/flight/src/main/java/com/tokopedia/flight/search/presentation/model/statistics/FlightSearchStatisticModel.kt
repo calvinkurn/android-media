1334368b87094f3a243154a9b8593b29878652ed
Muhammad Furqan <furqan@tokopedia.com>
2021-06-11 01:25:00 +0700

    [Flight][Tech Debt] Convert Flight Search files from Java to Kotlin

---
+++ features/travel/flight/src/main/java/com/tokopedia/flight/search/presentation/model/statistics/FlightSearchStatisticModel.kt
@@ -0,0 +1,39 @@
+package com.tokopedia.flight.search.presentation.model.statistics
+
+import com.tokopedia.flight.search.presentation.model.FlightAirlineModel
+
+/**
+ * Created by User on 10/30/2017.
+ */
+class FlightSearchStatisticModel(val minPrice: Int,
+                                 val maxPrice: Int,
+                                 val minDuration: Int,
+                                 val maxDuration: Int,
+                                 val transitTypeStatList: List<TransitStat>,
+                                 val airlineStatList: List<AirlineStat>,
+                                 val departureTimeStatList: List<DepartureStat>,
+                                 val arrivalTimeStatList: List<DepartureStat>,
+                                 val refundableTypeStatList: List<RefundableStat>,
+                                 val isHaveSpecialPrice: Boolean,
+                                 val isHaveBaggage: Boolean,
+                                 val isHaveInFlightMeal: Boolean,
+                                 val isHasFreeRapidTest: Boolean,
+                                 val isSeatDistancing: Boolean) {
+
+    fun getAirline(airlineID: String): FlightAirlineModel {
+        val airlineStatList: List<AirlineStat>? = airlineStatList
+        if (airlineStatList != null) {
+            var i = 0
+            val sizei = airlineStatList.size
+            while (i < sizei) {
+                val flightAirlineDB = airlineStatList[i].airlineDB
+                if (airlineID == flightAirlineDB.id) {
+                    return flightAirlineDB
+                }
+                i++
+            }
+        }
+        return FlightAirlineModel(airlineID, "", "", "")
+    }
+
+}
