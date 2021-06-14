8c0091f0fdae2eb5c77dec2309f2b6c181dd90cd
astidhiyaa <asti.anzaria@tokopedia.com>
2021-06-14 09:59:50 +0800

    fix applink

--- libraries/applink/src/main/java/com/tokopedia/applink/travel/DeeplinkMapperTravel.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/travel/DeeplinkMapperTravel.kt
@@ -5,6 +5,7 @@ import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.FirebaseRemoteConfigInstance
 import com.tokopedia.applink.internal.ApplinkConstInternalTravel
 import com.tokopedia.applink.order.DeeplinkMapperUohOrder
+import com.tokopedia.applink.startsWithPattern
 import com.tokopedia.remoteconfig.RemoteConfigInstance
 import com.tokopedia.remoteconfig.RemoteConfigKey
 
@@ -22,10 +23,10 @@ object DeeplinkMapperTravel {
             deeplink.equals(ApplinkConst.HOTEL_ORDER, true) -> {
                 DeeplinkMapperUohOrder.getRegisteredNavigationUohOrder(context, deeplink)
             }
-            deeplink.equals(ApplinkConst.HOTEL, true) -> {
+            deeplink.startsWith(ApplinkConst.HOTEL, true) -> {
                 ApplinkConstInternalTravel.DASHBOARD_HOTEL
             }
-            deeplink.equals(ApplinkConst.HOTEL_DETAIL, true) -> {
+            deeplink.startsWithPattern(ApplinkConst.HOTEL_DETAIL) -> {
                 ApplinkConstInternalTravel.HOTEL_DETAIL
             }
             else -> deeplink
