1f98a8f2ed3064b935ac2f877a348b100d920368
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-15 18:36:28 +0700

    change to mock

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/di/RegisterInitialComponentStub.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/di/RegisterInitialComponentStub.kt
@@ -2,13 +2,17 @@ package com.tokopedia.loginregister.login.behaviour.di
 
 import com.tokopedia.loginregister.login.behaviour.base.RegisterInitialBase
 import com.tokopedia.loginregister.login.behaviour.di.modules.MockLoginRegisterComponent
+import com.tokopedia.loginregister.login.behaviour.di.modules.registerinitial.MockRegisterInitialModule
 import com.tokopedia.loginregister.login.behaviour.di.modules.registerinitial.MockRegisterInitialuseCaseModule
-import com.tokopedia.loginregister.registerinitial.di.*
+import com.tokopedia.loginregister.registerinitial.di.RegisterInitialComponent
+import com.tokopedia.loginregister.registerinitial.di.RegisterInitialQueryModule
+import com.tokopedia.loginregister.registerinitial.di.RegisterInitialScope
+import com.tokopedia.loginregister.registerinitial.di.RegisterInitialViewModelModule
 import dagger.Component
 
 @RegisterInitialScope
 @Component(modules = [
-    RegisterInitialModule::class,
+    MockRegisterInitialModule::class,
     RegisterInitialQueryModule::class,
     MockRegisterInitialuseCaseModule::class,
     RegisterInitialViewModelModule::class
