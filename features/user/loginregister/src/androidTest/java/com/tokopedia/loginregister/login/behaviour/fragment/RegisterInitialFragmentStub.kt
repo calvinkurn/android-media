12f76262e54f1c9e08aeaadcb7a7030136bf3799
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-15 18:37:00 +0700

    mock redirection

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/fragment/RegisterInitialFragmentStub.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/fragment/RegisterInitialFragmentStub.kt
@@ -1,12 +1,31 @@
 package com.tokopedia.loginregister.login.behaviour.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.tokopedia.loginregister.login.behaviour.activity.LoginActivityStub
+import com.tokopedia.loginregister.login.behaviour.activity.VerificationActivityStub
 import com.tokopedia.loginregister.registerinitial.view.fragment.RegisterInitialFragment
 
 class RegisterInitialFragmentStub: RegisterInitialFragment() {
     override fun fetchRemoteConfig() {
         // DO NOTHING
+    }
+
+    override fun goToLoginPage() {
+        startActivity(Intent(context, LoginActivityStub::class.java))
+    }
+
+    override fun goToOTPRegisterEmail(email: String) {
+        startActivity(Intent(context, VerificationActivityStub::class.java))
+    }
+
+    override fun gotoLoginEmailPage(email: String) {
+        startActivity(Intent(context, LoginActivityStub::class.java))
+    }
+
+    override fun goToLoginRegisteredPhoneNumber(phone: String) {
+        startActivity(Intent(context, LoginActivityStub::class.java))
     }
 
     companion object {
