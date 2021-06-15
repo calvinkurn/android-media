204e39a22c24fd03bca6df5fd805e0baab67769e
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-15 19:29:28 +0700

    mock gotologinpage function

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/stub/RegisterInitialRouterHelperStub.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/stub/RegisterInitialRouterHelperStub.kt
@@ -1,6 +1,13 @@
 package com.tokopedia.loginregister.login.stub
 
+import android.app.Activity
+import android.content.Intent
+import com.tokopedia.loginregister.login.behaviour.activity.LoginActivityStub
 import com.tokopedia.loginregister.registerinitial.view.util.RegisterInitialRouterHelper
 
 class RegisterInitialRouterHelperStub: RegisterInitialRouterHelper() {
+
+    override fun goToLoginPage(context: Activity) {
+        context.startActivity(Intent(context, LoginActivityStub::class.java))
+    }
 }
