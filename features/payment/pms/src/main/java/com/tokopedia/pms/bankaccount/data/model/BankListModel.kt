2d046eb62a8773e8f1849754eefdfa3c96663356
Abhijeet <abhijeet.chauhan@tokopedia.com>
2021-06-15 16:33:49 +0530

    [PMS Revamp] Refactor bankList

--- features/payment/pms/src/main/java/com/tokopedia/pms/bankaccount/data/model/BankListModel.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/bankaccount/data/model/BankListModel.kt
@@ -1,62 +1,14 @@
-package com.tokopedia.pms.bankaccount.data.model;
+package com.tokopedia.pms.bankaccount.data.model
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by zulfikarrahman on 7/5/18.
  */
-
-public class BankListModel implements Parcelable {
-
-    private String id;
-    private String bankName;
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getBankName() {
-        return bankName;
-    }
-
-    public void setBankName(String bankName) {
-        this.bankName = bankName;
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(this.id);
-        dest.writeString(this.bankName);
-    }
-
-    public BankListModel() {
-    }
-
-    protected BankListModel(Parcel in) {
-        this.id = in.readString();
-        this.bankName = in.readString();
-    }
-
-    public static final Parcelable.Creator<BankListModel> CREATOR = new Parcelable.Creator<BankListModel>() {
-        @Override
-        public BankListModel createFromParcel(Parcel source) {
-            return new BankListModel(source);
-        }
-
-        @Override
-        public BankListModel[] newArray(int size) {
-            return new BankListModel[size];
-        }
-    };
-}
+@Parcelize
+data class BankListModel(
+    val id: String?,
+    val bankName: String?
+) : Parcelable
--- features/payment/pms/src/main/java/com/tokopedia/pms/bankaccount/domain/BankListDataUseCase.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/bankaccount/domain/BankListDataUseCase.kt
@@ -1,39 +1,31 @@
 package com.tokopedia.pms.bankaccount.domain
 
 import com.tokopedia.pms.bankaccount.data.model.BankListModel
-import java.util.*
 
 /**
  * Created by zulfikarrahman on 7/5/18.
  */
 class BankListDataUseCase {
-    val bankList: List<BankListModel>
-        get() {
-            val listOfBank = listOf("BCA", "MANDIRI", "BNI", "BRI", "CIMB")
-            val listOfBankCode = listOf("1", "2", "3", "4", "5")
-            val bankListModels: MutableList<BankListModel> = ArrayList()
-            for (i in listOfBank.indices) {
-                val bankListModel =
-                    BankListModel()
-                bankListModel.bankName = listOfBank[i]
-                bankListModel.id = listOfBankCode[i]
-                bankListModels.add(bankListModel)
-            }
-            return bankListModels
-        }
+
+    val bankList: List<BankListModel> by lazy {
+        arrayListOf(
+            BankListModel("1", "BCA"),
+            BankListModel("2", "MANDIRI"),
+            BankListModel("3", "BNI"),
+            BankListModel("4", "BRI"),
+            BankListModel("5", "CIMB"),
+        )
+    }
 
     fun getBankNameFromBankId(bankId: String?): String {
-        for (bankModel in bankList) {
-            if (bankModel.id == bankId) return bankModel.bankName
-        }
+        for (bankModel in bankList)
+            if (bankModel.id == bankId) return bankModel.bankName ?: ""
         return bankList.getOrNull(0)?.bankName ?: ""
     }
 
     fun getBankIdFromBankName(bankName: String): String {
-        for (bankModel in bankList) {
-            if (bankModel.bankName == bankName) return bankModel.id
-        }
+        for (bankModel in bankList)
+            if (bankModel.bankName == bankName) return bankModel.id ?: ""
         return bankList.getOrNull(0)?.id ?: "1"
     }
-
 }
