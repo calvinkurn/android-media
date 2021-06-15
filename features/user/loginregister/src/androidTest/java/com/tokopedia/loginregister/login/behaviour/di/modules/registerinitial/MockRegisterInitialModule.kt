ba7849af02af1f7db47e648da911129ad2b878c5
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-06-15 19:28:59 +0700

    add @module anotation

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/di/modules/registerinitial/MockRegisterInitialModule.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/login/behaviour/di/modules/registerinitial/MockRegisterInitialModule.kt
@@ -8,10 +8,12 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.loginregister.login.stub.RegisterInitialRouterHelperStub
 import com.tokopedia.loginregister.registerinitial.di.RegisterInitialScope
 import com.tokopedia.loginregister.registerinitial.view.util.RegisterInitialRouterHelper
+import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+@Module
 class MockRegisterInitialModule {
     @Provides
     fun providesContext(@ApplicationContext context: Context): Context = context
@@ -41,7 +43,7 @@ class MockRegisterInitialModule {
 
     @Provides
     @RegisterInitialScope
-    fun provideRegisterInitialRouter(): RegisterInitialRouterHelperStub {
+    fun provideRegisterInitialRouterStub(): RegisterInitialRouterHelperStub {
         return RegisterInitialRouterHelperStub()
     }
 }
