ff673ba4e58a4520f2a5dc1e2e5ec3eb7e7ab7a7
irfan khoirul <irfankhoirul@gmail.com>
2021-06-15 10:24:08 +0700

    add unit test get mini cart list simplified

--- features/transaction/minicart/src/test/java/com/tokopedia/minicart/common/widget/viewmodel/dataprovider/GetMiniCartListSimplifiedDataProvider.kt
+++ features/transaction/minicart/src/test/java/com/tokopedia/minicart/common/widget/viewmodel/dataprovider/GetMiniCartListSimplifiedDataProvider.kt
@@ -1,4 +1,20 @@
 package com.tokopedia.minicart.common.widget.viewmodel.dataprovider
 
+import com.google.gson.Gson
+import com.tokopedia.minicart.common.data.response.minicartlist.MiniCartData
+import com.tokopedia.minicart.common.data.response.minicartlist.MiniCartGqlResponse
+import com.tokopedia.minicart.common.domain.data.MiniCartSimplifiedData
+import com.tokopedia.minicart.common.domain.mapper.MiniCartSimplifiedMapper
+
 object GetMiniCartListSimplifiedDataProvider {
+
+    private val gson = Gson()
+    private val dataMapper = MiniCartSimplifiedMapper()
+    private val fileUtil = UnitTestFileUtils
+
+    fun provideGetMiniCartSimplifiedSuccessAllAvailable(): MiniCartSimplifiedData {
+        val json = gson.fromJson(fileUtil.getJsonFromAsset("assets/get_mini_cart_simplified_success_all_available"), MiniCartGqlResponse::class.java)
+        return dataMapper.mapMiniCartSimplifiedData(json.miniCart)
+    }
+
 }
