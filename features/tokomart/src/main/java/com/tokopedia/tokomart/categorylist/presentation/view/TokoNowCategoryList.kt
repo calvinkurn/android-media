7cd2eb03bcb21ba5a9e5f4857dee1f491cfe824f
rezagama <reza.hidayat@tokopedia.com>
2021-06-16 19:08:11 +0700

    Convert category list to custom view

--- features/tokomart/src/main/java/com/tokopedia/tokomart/categorylist/presentation/view/TokoNowCategoryList.kt
+++ features/tokomart/src/main/java/com/tokopedia/tokomart/categorylist/presentation/view/TokoNowCategoryList.kt
@@ -2,36 +2,99 @@ package com.tokopedia.tokomart.categorylist.presentation.view
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.tokomart.categorylist.presentation.adapter.TokoMartCategoryListAdapter
-import com.tokopedia.tokomart.categorylist.presentation.adapter.TokoMartCategoryListAdapterTypeFactory
-import com.tokopedia.tokomart.categorylist.presentation.adapter.decoration.TokoNowCategoryListDecoration
-import com.tokopedia.tokomart.categorylist.presentation.adapter.differ.TokoMartCategoryListDiffer
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.kotlin.extensions.view.setMargin
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.tokomart.R
+import com.tokopedia.tokomart.categorylist.analytic.CategoryListAnalytics
 import com.tokopedia.tokomart.categorylist.presentation.uimodel.CategoryListChildUiModel
-import com.tokopedia.tokomart.categorylist.presentation.viewholder.CategoryListItemViewHolder
-import com.tokopedia.tokomart.categorylist.presentation.viewholder.CategoryListItemViewHolder.CategoryListListener
+import com.tokopedia.tokomart.categorylist.presentation.uimodel.CategoryListChildUiModel.*
+import com.tokopedia.tokomart.categorylist.presentation.uimodel.CategoryListItemUiModel
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifyprinciples.Typography
 
-class TokoNowCategoryList : RecyclerView {
+class TokoNowCategoryList : LinearLayout {
 
-    private lateinit var categoryAdapter: TokoMartCategoryListAdapter
+    private lateinit var analytics: CategoryListAnalytics
+    private lateinit var listener: CategoryListListener
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-    constructor(context: Context, listener: CategoryListListener, getCategoryLevelOneListener:
-    CategoryListItemViewHolder.GetCategoryLevelOneListener) : super(context) {
-        categoryAdapter = TokoMartCategoryListAdapter(
-            TokoMartCategoryListAdapterTypeFactory(listener, getCategoryLevelOneListener),
-            TokoMartCategoryListDiffer()
+    constructor(context: Context, analytics: CategoryListAnalytics, listener: CategoryListListener) : super(context) {
+        this.analytics = analytics
+        this.listener = listener
+    }
+
+    init {
+        orientation = VERTICAL
+        layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
         )
     }
 
-    fun setupCategoryList(data: List<CategoryListChildUiModel>) {
-        adapter = categoryAdapter
-        isNestedScrollingEnabled = false
-        layoutManager = LinearLayoutManager(context)
-        addItemDecoration(TokoNowCategoryListDecoration())
-        categoryAdapter.submitList(data)
+    fun setup(category: CategoryListItemUiModel) {
+        category.childList.forEachIndexed { index, categoryL2 ->
+            val itemView = if (categoryL2.type == CategoryType.CHILD_CATEGORY_ITEM) {
+                createCategoryItemView(category, categoryL2).apply {
+                    if(index == 0) setMargin(0, 0, 0, 0)
+                }
+            } else {
+                createCategoryTextView(category, categoryL2)
+            }
+            addView(itemView)
+        }
+    }
+
+    private fun createCategoryItemView(categoryLevel1: CategoryListItemUiModel, categoryL2: CategoryListChildUiModel): View {
+        return LayoutInflater.from(context).inflate(R.layout.item_tokomart_category_list, this, false).apply {
+            val textTitle = findViewById<Typography>(R.id.textTitle)
+            val imageCategory = findViewById<ImageUnify>(R.id.imageCategory)
+
+            textTitle.text = categoryL2.name
+            textTitle.setWeight(categoryL2.textWeight)
+            textTitle.setTextColor(ContextCompat.getColor(context, categoryL2.textColorId))
+
+            categoryL2.imageUrl?.let {
+                imageCategory.loadImage(it)
+                imageCategory.show()
+            }
+
+            setOnClickListener {
+                analytics.onClickLevelTwoCategory(categoryLevel1.name, categoryL2.name)
+                RouteManager.route(context, categoryL2.appLink)
+                listener.onClickCategoryItem()
+            }
+        }
+    }
+
+    private fun createCategoryTextView(categoryLevel1: CategoryListItemUiModel, categoryL2: CategoryListChildUiModel): View {
+        return LayoutInflater.from(context).inflate(R.layout.item_tokomart_category_list, this, false).apply {
+            val textTitle = findViewById<Typography>(R.id.textTitle)
+            val verticalMargin = context.resources.getDimensionPixelSize(
+                com.tokopedia.unifyprinciples.R.dimen.spacing_lvl3)
+
+            textTitle.text = context.getString(R.string.tokomart_all_category_text_format, categoryL2.name)
+            textTitle.setWeight(categoryL2.textWeight)
+            textTitle.setTextColor(ContextCompat.getColor(context, categoryL2.textColorId))
+
+            setOnClickListener {
+                analytics.onClickLihatSemuaCategory(categoryLevel1.name)
+                RouteManager.route(context, categoryL2.appLink)
+                listener.onClickCategoryItem()
+            }
+
+            setMargin(0, verticalMargin, 0, verticalMargin)
+        }
+    }
+
+    interface CategoryListListener {
+        fun onClickCategoryItem()
     }
 }
