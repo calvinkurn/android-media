df8f61f8c4b7ea37b4cf2f9e2584f7844a22ec11
irfan khoirul <irfankhoirul@gmail.com>
2021-06-21 20:53:48 +0700

    adjust ui & code to show seller cashback

--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/subpage/summarytransaction/SummaryTransactionBottomSheet.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/subpage/summarytransaction/SummaryTransactionBottomSheet.kt
@@ -3,6 +3,8 @@ package com.tokopedia.minicart.cartlist.subpage.summarytransaction
 import android.content.Context
 import android.view.View
 import androidx.fragment.app.FragmentManager
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.minicart.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.unifyprinciples.Typography
@@ -17,6 +19,9 @@ class SummaryTransactionBottomSheet @Inject constructor() {
     private var textDiscountTotalValue: Typography? = null
     private var textTotalPayTitle: Typography? = null
     private var textTotalPayValue: Typography? = null
+    private var separatorSellerCashback: View? = null
+    private var textTotalCashbackTitle: Typography? = null
+    private var textTotalCashbackValue: Typography? = null
 
     fun show(data: MiniCartSummaryTransactionUiModel, fragmentManager: FragmentManager, context: Context) {
 
@@ -27,62 +32,59 @@ class SummaryTransactionBottomSheet @Inject constructor() {
         bottomSheet.setTitle(context.getString(R.string.mini_cart_title_summary_transaction))
 
         val view = View.inflate(context, R.layout.layout_bottomsheet_mini_cart_summary_transaction, null)
+        initializeView(view)
+
+        renderPriceTotal(data)
+        renderDiscount(data)
+        renderPaymentTotal(data)
+        renderSellerCashback(data)
+
+        bottomSheet.setChild(view)
+        bottomSheet.show(fragmentManager, "Mini Cart Summary Transaction")
+    }
+
+    private fun initializeView(view: View) {
         textPriceTotalTitle = view.findViewById(R.id.text_price_total_title)
         textPriceTotalValue = view.findViewById(R.id.text_price_total_value)
         textDiscountTotalTitle = view.findViewById(R.id.text_discount_total_title)
         textDiscountTotalValue = view.findViewById(R.id.text_discount_total_value)
         textTotalPayTitle = view.findViewById(R.id.text_total_pay_title)
         textTotalPayValue = view.findViewById(R.id.text_total_pay_value)
-
-        renderPriceTotal(data)
-        renderDiscount(data)
-        renderPaymentTotal(data)
-
-        bottomSheet.setChild(view)
-        bottomSheet.show(fragmentManager, "Mini Cart Summary Transaction")
+        separatorSellerCashback = view.findViewById(R.id.separator_seller_cashback)
+        textTotalCashbackTitle = view.findViewById(R.id.text_total_cashback_title)
+        textTotalCashbackValue = view.findViewById(R.id.text_total_cashback_value)
     }
 
     private fun renderPaymentTotal(data: MiniCartSummaryTransactionUiModel) {
-        textTotalPayTitle?.apply {
-            data.paymentTotalWording.let {
-                text = it
-            }
-        }
-        textTotalPayValue?.apply {
-            val totalPay = data.paymentTotal
-            this.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(totalPay, false)
-        }
+        textTotalPayTitle?.text = data.paymentTotalWording
+        textTotalPayValue?.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.paymentTotal, false)
     }
 
     private fun renderDiscount(data: MiniCartSummaryTransactionUiModel) {
         if (data.discountValue > 0) {
-            textDiscountTotalValue?.apply {
-                text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.discountValue * -1, false)
-                visibility = View.VISIBLE
-            }
-            textDiscountTotalTitle?.apply {
-                text = data.discountTotalWording
-                visibility = View.VISIBLE
-            }
+            textDiscountTotalValue?.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.discountValue * -1, false)
+            textDiscountTotalValue?.show()
+            textDiscountTotalTitle?.text = data.discountTotalWording
+            textDiscountTotalTitle?.show()
         } else {
-            textDiscountTotalValue?.apply {
-                visibility = View.GONE
-            }
-            textDiscountTotalTitle?.apply {
-                visibility = View.GONE
-            }
+            textDiscountTotalValue?.gone()
+            textDiscountTotalTitle?.gone()
         }
     }
 
     private fun renderPriceTotal(data: MiniCartSummaryTransactionUiModel) {
-        textPriceTotalTitle?.apply {
-            data.totalWording.let {
-                text = it.replace("[0-9]".toRegex(), data.qty.toString())
-            }
-        }
-        textPriceTotalValue?.apply {
-            this.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.totalValue, false)
-        }
+        textPriceTotalTitle?.text = data.totalWording.replace("[0-9]+".toRegex(), data.qty.toString())
+        textPriceTotalValue?.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.totalValue, false)
     }
 
+    private fun renderSellerCashback(data: MiniCartSummaryTransactionUiModel) {
+        if (data.sellerCashbackValue > 0) {
+            textTotalCashbackTitle?.text = CurrencyFormatUtil.convertPriceValueToIdrFormat(data.sellerCashbackValue, false).replace("Rp", "")
+            textTotalCashbackTitle?.text = data.sellerCashbackWording
+        } else {
+            separatorSellerCashback?.gone()
+            textTotalCashbackTitle?.gone()
+            textTotalCashbackValue?.gone()
+        }
+    }
 }
