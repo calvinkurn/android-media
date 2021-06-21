a365abfffffa72f2efbe8abdcef2924cc90e16fd
misael-jonathan <misael.jonathan@tokopedia.com>
2021-06-21 16:37:12 +0700

    postpaid login ss test

--- features/recharge/digital_topup_bills/src/androidTest/java/com/tokopedia/topupbills/screenshot/postpaid/BaseTelcoPostpaidLoginScreenShotTest.kt
+++ features/recharge/digital_topup_bills/src/androidTest/java/com/tokopedia/topupbills/screenshot/postpaid/BaseTelcoPostpaidLoginScreenShotTest.kt
@@ -6,28 +6,39 @@ import android.app.Instrumentation
 import android.content.Intent
 import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.ComponentNameMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
-import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.common.topupbills.data.TopupBillsFavNumberItem
 import com.tokopedia.common.topupbills.view.activity.TopupBillsSearchNumberActivity
+import com.tokopedia.common.topupbills.view.adapter.TopupBillsPromoListAdapter
 import com.tokopedia.common.topupbills.view.fragment.TopupBillsSearchNumberFragment
 import com.tokopedia.graphql.GraphqlCacheManager
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
 import com.tokopedia.test.application.espresso_component.CommonActions
+import com.tokopedia.test.application.espresso_component.CommonActions.findViewAndScreenShot
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import com.tokopedia.topupbills.R
-import com.tokopedia.topupbills.postpaid.TelcoPostpaidLoginInstrumentTest
 import com.tokopedia.topupbills.telco.common.activity.BaseTelcoActivity
 import com.tokopedia.topupbills.telco.data.constant.TelcoCategoryType
 import com.tokopedia.topupbills.telco.data.constant.TelcoComponentType
 import com.tokopedia.topupbills.telco.postpaid.activity.TelcoPostpaidActivity
 import com.tokopedia.topupbills.utils.ResourceUtils
+import org.hamcrest.CoreMatchers
+import org.hamcrest.core.AllOf
 import org.hamcrest.core.IsNot
 import org.junit.After
 import org.junit.Before
@@ -80,8 +91,12 @@ class BaseTelcoPostpaidLoginScreenShotTest {
     @Test
     fun screenshot() {
         stubSearchNumber()
-        take_screenshot_visible_screen()
-
+//        take_screenshot_visible_screen()
+//        take_screenshot_interaction_menu()
+//        take_screenshot_input_number()
+//        take_screenshot_enquiry_phone_number()
+        take_screenshot_interaction_view_pager()
+        take_screenshot_interaction_interactive_header()
     }
 
     fun take_screenshot_visible_screen() {
@@ -105,6 +120,110 @@ class BaseTelcoPostpaidLoginScreenShotTest {
         }
     }
 
+    fun take_screenshot_interaction_menu() {
+        Thread.sleep(2000)
+
+        Espresso.onView(ViewMatchers.withId(R.id.action_overflow_menu)).perform(ViewActions.click())
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.container_menu,
+                filePrefix(),
+                "interaction_menu"
+        )
+        Thread.sleep(2000)
+        Espresso.onView(ViewMatchers.withId(R.id.bottom_sheet_close)).perform(ViewActions.click())
+    }
+
+    fun take_screenshot_input_number() {
+        Thread.sleep(2000)
+        Espresso.onView(ViewMatchers.withId(R.id.telco_ac_input_number)).perform(ViewActions.click())
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.container_search_number_telco,
+                filePrefix(),
+                "search_number"
+        )
+
+        Espresso.onView(ViewMatchers.withId(R.id.searchbar_textfield)).perform(ViewActions.typeText(VALID_PHONE_NUMBER))
+        Espresso.closeSoftKeyboard()
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.container_search_number_telco,
+                filePrefix(),
+                "search_number_filled"
+        )
+        Espresso.onView(ViewMatchers.withId(R.id.searchbar_textfield)).perform(ViewActions.click(), ViewActions.pressImeActionButton())
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.telco_input_number_layout,
+                filePrefix(),
+                "input_number_widget_filled"
+        )
+    }
+
+    fun take_screenshot_enquiry_phone_number() {
+        Thread.sleep(2000)
+        Espresso.onView(ViewMatchers.withId(R.id.telco_buy_widget)).check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+        Espresso.onView(ViewMatchers.withId(R.id.telco_enquiry_btn))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+                .perform(ViewActions.click())
+
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            val test = mActivityRule.activity.findViewById<RelativeLayout>(R.id.telco_page_container)
+            CommonActions.takeScreenShotVisibleViewInScreen(test, filePrefix(), "full_layout_enquiry")
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.telco_clear_input_number_btn)).perform(ViewActions.click())
+        Thread.sleep(2000)
+    }
+
+    fun take_screenshot_interaction_view_pager() {
+        findViewAndScreenShot(R.id.telco_tab_layout, filePrefix(), "tab_layout")
+
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.layout_widget,
+                filePrefix(),
+                "pager_recent"
+        )
+
+        Espresso.onView(withText("Promo")).perform(ViewActions.click())
+
+        Thread.sleep(2000)
+        val viewInteraction = Espresso.onView(AllOf.allOf(
+                CoreMatchers.allOf(ViewMatchers.withId(R.id.recycler_view_menu_component), ViewMatchers.withParent(ViewMatchers.withId(R.id.layout_widget)),
+                        ViewMatchers.isDisplayed()))).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.layout_widget,
+                filePrefix(),
+                "pager_promo"
+        )
+
+        viewInteraction.perform(RecyclerViewActions.actionOnItemAtPosition<TopupBillsPromoListAdapter.PromoItemViewHolder>(0,
+                CommonActions.clickChildViewWithId(R.id.btn_copy_promo)))
+
+        Thread.sleep(2000)
+        findViewAndScreenShot(
+                R.id.layout_widget,
+                filePrefix(),
+                "pager_promo_chosen"
+        )
+    }
+
+    fun take_screenshot_interaction_interactive_header() {
+        Thread.sleep(2000)
+        val recyclerView = mActivityRule.activity.findViewById<RecyclerView>(R.id.recycler_view_menu_component)
+        scrollRecyclerViewToPosition(recyclerView, 5)
+
+        Thread.sleep(2000)
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            val test = mActivityRule.activity.findViewById<RelativeLayout>(R.id.telco_page_container)
+            CommonActions.takeScreenShotVisibleViewInScreen(test, filePrefix(), "full_layout_header_disappear")
+        }
+    }
+
     private fun createOrderNumberTypeManual(): Instrumentation.ActivityResult {
         val orderClientNumber = TopupBillsFavNumberItem(clientNumber = VALID_PHONE_NUMBER)
         val resultData = Intent()
@@ -118,6 +237,11 @@ class BaseTelcoPostpaidLoginScreenShotTest {
         Intents.intending(IntentMatchers.hasComponent(
                 ComponentNameMatchers.hasShortClassName(".DigitalSearchNumberActivity")))
                 .respondWith(createOrderNumberTypeManual())
+    }
+
+    private fun scrollRecyclerViewToPosition(recyclerView: RecyclerView, position: Int) {
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        mActivityRule.runOnUiThread { layoutManager.scrollToPositionWithOffset(position, 0) }
     }
 
     fun filePrefix(): String {
