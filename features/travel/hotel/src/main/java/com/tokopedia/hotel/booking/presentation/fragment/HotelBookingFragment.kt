93ce044ec96037842cf9707c1c1562e930875cd4
astidhiyaa <asti.anzaria@tokopedia.com>
2021-06-22 19:35:25 +0800

    test: add description for UiSelector

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/booking/presentation/fragment/HotelBookingFragment.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/booking/presentation/fragment/HotelBookingFragment.kt
@@ -252,6 +252,7 @@ class HotelBookingFragment : HotelBaseFragment() {
     }
 
     private fun initView() {
+        hotel_booking_container.contentDescription = getString(R.string.hotel_order_detail_title)
         hideLoadingBar()
 
         setupHotelInfo(hotelCart.property)
