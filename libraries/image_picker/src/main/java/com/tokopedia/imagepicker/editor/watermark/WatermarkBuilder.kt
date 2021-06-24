2ecb2300165daf8ae40130a824f3ea69626db338
isfaaghyth <isfaaghyth@gmail.com>
2021-06-24 15:01:05 +0700

    increase the size of empty watermark container

--- libraries/image_picker/src/main/java/com/tokopedia/imagepicker/editor/watermark/WatermarkBuilder.kt
+++ libraries/image_picker/src/main/java/com/tokopedia/imagepicker/editor/watermark/WatermarkBuilder.kt
@@ -129,7 +129,7 @@ open class WatermarkBuilder {
 
         @JvmStatic
         fun createWithSize(context: Context): WatermarkBuilder {
-            val bitmap = createBitmap(2320, 2320, ARGB_8888)
+            val bitmap = createBitmap(2300, 2300, ARGB_8888)
             return WatermarkBuilder(context, bitmap)
         }
 
