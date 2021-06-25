9ac4b96a24c9d7b4f9da8220fbcb6f6373d70455
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-06-25 17:57:15 +0700

    support 3 startup mode in 1 test

--- macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/test/home/HomeStartupBenchmark.kt
+++ macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/test/home/HomeStartupBenchmark.kt
@@ -1,5 +1,6 @@
 package com.tkpd.macrobenchmark.test.home
 
+import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -13,6 +14,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 /**
  * Created by DevAra
@@ -23,8 +25,8 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @SdkSuppress(minSdkVersion = 29)
-@RunWith(AndroidJUnit4::class)
-class HomeStartupBenchmark {
+@RunWith(Parameterized::class)
+class HomeStartupBenchmark(private val startupMode: StartupMode) {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
@@ -37,9 +39,18 @@ class HomeStartupBenchmark {
     fun macroBenchmarkHomeLaunchTime() {
         benchmarkRule.measureStartup(
             profileCompiled = false,
-            startupMode = MacroArgs.getStartupMode(InstrumentationRegistry.getArguments()),
+            startupMode = startupMode,
             iterations = MacroArgs.getIterations(InstrumentationRegistry.getArguments())) {
             MacroIntent.Home.getHomeIntent()
+        }
+    }
+
+    companion object {
+        @Parameterized.Parameters(name = "mode={0}")
+        @JvmStatic
+        fun parameters(): List<Array<Any>> {
+            return listOf(StartupMode.COLD, StartupMode.WARM)
+                .map { arrayOf(it) }
         }
     }
 }
