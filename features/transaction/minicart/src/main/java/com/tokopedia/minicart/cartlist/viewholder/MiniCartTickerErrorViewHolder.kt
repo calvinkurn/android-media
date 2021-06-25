317925e3e2feeb411f41c649e399f7048ed61e19
irfan khoirul <irfankhoirul@gmail.com>
2021-06-25 10:37:37 +0700

    implement view binding

--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/viewholder/MiniCartTickerErrorViewHolder.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/viewholder/MiniCartTickerErrorViewHolder.kt
@@ -1,40 +1,34 @@
 package com.tokopedia.minicart.cartlist.viewholder
 
-import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.minicart.R
 import com.tokopedia.minicart.cartlist.MiniCartListActionListener
 import com.tokopedia.minicart.cartlist.uimodel.MiniCartTickerErrorUiModel
-import com.tokopedia.unifycomponents.ticker.Ticker
-import com.tokopedia.unifyprinciples.Typography
+import com.tokopedia.minicart.databinding.ItemMiniCartTickerErrorBinding
 
-class MiniCartTickerErrorViewHolder(private val view: View,
+class MiniCartTickerErrorViewHolder(private val viewBinding: ItemMiniCartTickerErrorBinding,
                                     private val listener: MiniCartListActionListener)
-    : AbstractViewHolder<MiniCartTickerErrorUiModel>(view) {
+    : AbstractViewHolder<MiniCartTickerErrorUiModel>(viewBinding.root) {
 
     companion object {
         val LAYOUT = R.layout.item_mini_cart_ticker_error
     }
 
-    private val tickerError: Ticker? by lazy {
-        view.findViewById(R.id.ticker_error)
-    }
-    private val textShow: Typography? by lazy {
-        view.findViewById(R.id.text_show)
-    }
-
     override fun bind(element: MiniCartTickerErrorUiModel) {
-        val message = tickerError?.context?.getString(R.string.mini_cart_message_ticker_error, element.unavailableItemCount) ?: ""
-        tickerError?.setHtmlDescription(message)
-        if (element.isShowErrorActionLabel) {
-            textShow?.show()
-        } else {
-            textShow?.gone()
-        }
-        textShow?.setOnClickListener {
-            listener.onShowUnavailableItemsCLicked()
+        with(viewBinding) {
+            val message = tickerError.context?.getString(R.string.mini_cart_message_ticker_error, element.unavailableItemCount)
+                    ?: ""
+            tickerError.setHtmlDescription(message)
+            if (element.isShowErrorActionLabel) {
+                textShow.show()
+            } else {
+                textShow.gone()
+            }
+            textShow.setOnClickListener {
+                listener.onShowUnavailableItemsCLicked()
+            }
         }
     }
 
--- features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/viewholder/MiniCartUnavailableHeaderViewHolder.kt
+++ features/transaction/minicart/src/main/java/com/tokopedia/minicart/cartlist/viewholder/MiniCartUnavailableHeaderViewHolder.kt
@@ -1,32 +1,27 @@
 package com.tokopedia.minicart.cartlist.viewholder
 
-import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.minicart.R
 import com.tokopedia.minicart.cartlist.MiniCartListActionListener
 import com.tokopedia.minicart.cartlist.uimodel.MiniCartUnavailableHeaderUiModel
-import com.tokopedia.unifyprinciples.Typography
+import com.tokopedia.minicart.databinding.ItemMiniCartUnavailableHeaderBinding
 
-class MiniCartUnavailableHeaderViewHolder(private val view: View,
+class MiniCartUnavailableHeaderViewHolder(private val viewBinding: ItemMiniCartUnavailableHeaderBinding,
                                           private val listener: MiniCartListActionListener)
-    : AbstractViewHolder<MiniCartUnavailableHeaderUiModel>(view) {
+    : AbstractViewHolder<MiniCartUnavailableHeaderUiModel>(viewBinding.root) {
 
     companion object {
         val LAYOUT = R.layout.item_mini_cart_unavailable_header
     }
 
-    private val textDisabledItemCount: Typography? by lazy {
-        view.findViewById(R.id.text_disabled_item_count)
-    }
-    private val textDelete: Typography? by lazy {
-        view.findViewById(R.id.text_delete)
-    }
-
     override fun bind(element: MiniCartUnavailableHeaderUiModel) {
-        val message = textDisabledItemCount?.context?.getString(R.string.mini_cart_title_unavailable_header, element.unavailableItemCount) ?: ""
-        textDisabledItemCount?.text = message
-        textDelete?.setOnClickListener {
-            listener.onBulkDeleteUnavailableItems()
+        with(viewBinding) {
+            val message = textDisabledItemCount.context?.getString(R.string.mini_cart_title_unavailable_header, element.unavailableItemCount)
+                    ?: ""
+            textDisabledItemCount.text = message
+            textDelete.setOnClickListener {
+                listener.onBulkDeleteUnavailableItems()
+            }
         }
     }
 
