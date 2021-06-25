8cc19bbf6ec5b65647840acf028f4677f8776f4f
Felicia Widjaja <fwidjaja@live.com>
2021-06-25 21:08:34 +0700

    change int to String

--- features/discovery/home_wishlist/src/main/java/com/tokopedia/home_wishlist/viewmodel/WishlistViewModel.kt
+++ features/discovery/home_wishlist/src/main/java/com/tokopedia/home_wishlist/viewmodel/WishlistViewModel.kt
@@ -786,7 +786,7 @@ open class WishlistViewModel @Inject constructor(
                                         RemoveWishlistActionData(
                                                 message = "",
                                                 isSuccess = true,
-                                                productId = productId?.toInt() ?: 0
+                                                productId = productId ?: ""
                                         )
                                 )
                                 if (updatedList.isEmpty()) updatedList.add(EmptyWishlistDataModel())
