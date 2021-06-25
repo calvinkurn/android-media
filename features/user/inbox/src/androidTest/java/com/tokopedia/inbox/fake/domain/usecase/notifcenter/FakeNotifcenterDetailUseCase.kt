696b2dc9df34465a7ffb34e1a95bc8d4e217f485
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-25 15:46:08 +0700

    add IT should_render_widget_message

--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/FakeNotifcenterDetailUseCase.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/FakeNotifcenterDetailUseCase.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.inbox.fake.domain.usecase.notifcenter
 
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.inbox.common.AndroidFileUtil
 import com.tokopedia.inbox.fake.common.FakeGraphqlUseCase
+import com.tokopedia.inbox.test.R
 import com.tokopedia.notifcenter.data.entity.notification.NotifcenterDetailResponse
 import com.tokopedia.notifcenter.data.mapper.NotifcenterDetailMapper
 import com.tokopedia.notifcenter.domain.NotifcenterDetailUseCase
@@ -21,7 +23,25 @@ class FakeNotifcenterDetailUseCase(
             gqlUseCase.response = value
         }
 
+    val noTrackHistoryWidget: NotifcenterDetailResponse
+        get() = AndroidFileUtil.parseRaw(
+            R.raw.notifcenter_detail_v3_no_track_history_widget,
+            NotifcenterDetailResponse::class.java
+        )
+
+    val defaultResponse: NotifcenterDetailResponse
+        get() = AndroidFileUtil.parseRaw(
+            R.raw.notifcenter_detail_v3,
+            NotifcenterDetailResponse::class.java
+        )
+
     init {
         response = response
     }
+
+    fun initialize() {
+        this.response = defaultResponse
+    }
+
+
 }
---
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/view/activity/notifcenter/buyer/NotifcenterWidgetTest.kt
@@ -0,0 +1,25 @@
+package com.tokopedia.inbox.view.activity.notifcenter.buyer
+
+import com.tokopedia.inbox.view.activity.base.notifcenter.InboxNotifcenterTest
+import com.tokopedia.inbox.view.activity.base.notifcenter.NotifcenterAssertion
+import org.junit.Test
+
+class NotifcenterWidgetTest: InboxNotifcenterTest() {
+
+    @Test
+    fun should_render_widget_message() {
+        // Given
+        val msg = inboxNotifcenterDep.notifcenterDetailUseCase
+            .noTrackHistoryWidget.notifcenterDetail.newList[0].widget.message
+        inboxNotifcenterDep.notifcenterDetailUseCase.apply {
+            response = noTrackHistoryWidget
+        }
+        startInboxActivity()
+
+        // Then
+        NotifcenterAssertion.assertNotifWidgetMsg(2, msg)
+    }
+
+    // TODO: should_render_short_desc_on_notif_widget
+
+}
