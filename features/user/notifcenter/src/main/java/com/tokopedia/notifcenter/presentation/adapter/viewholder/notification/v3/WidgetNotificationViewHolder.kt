ebf173fafdc2f342ad4f736440b8d1b666131591
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-25 14:27:44 +0700

    fix cta margin

--- features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/WidgetNotificationViewHolder.kt
+++ features/user/notifcenter/src/main/java/com/tokopedia/notifcenter/presentation/adapter/viewholder/notification/v3/WidgetNotificationViewHolder.kt
@@ -226,7 +226,19 @@ class WidgetNotificationViewHolder constructor(
     private fun bindWidgetDescSingle(element: NotificationUiModel) {
         widgetDescSingle?.shouldShowWithAction(element.hasSingleLineDesc()) {
             widgetDescSingle.text = element.widgetDescHtml
+            bindWidgetDescSingleMargin(element)
         }
+    }
+
+    private fun bindWidgetDescSingleMargin(element: NotificationUiModel) {
+        val lp = widgetDescSingle?.layoutParams
+                as? ViewGroup.MarginLayoutParams ?: return
+        if (element.widget.hasCta()) {
+            lp.rightMargin = 0
+        } else {
+            lp.rightMargin = padding_12?.toInt() ?: 0
+        }
+        widgetDescSingle.layoutParams = lp
     }
 
     private fun bindWidgetDesc(element: NotificationUiModel) {
@@ -236,7 +248,7 @@ class WidgetNotificationViewHolder constructor(
     }
 
     private fun bindWidgetCta(element: NotificationUiModel) {
-        widgetCta?.shouldShowWithAction(element.widget.buttonText.isNotEmpty()) {
+        widgetCta?.shouldShowWithAction(element.widget.hasCta()) {
             widgetCta.text = element.widget.buttonText
             widgetCta.setOnClickListener {
                 listener?.trackClickCtaWidget(element)
