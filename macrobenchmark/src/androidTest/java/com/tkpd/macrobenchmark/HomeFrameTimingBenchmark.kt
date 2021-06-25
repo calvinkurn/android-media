8b96148fb29d63efacd1339dd68eec08758564ed
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-06-25 11:10:29 +0700

    test using developer options

--- macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/HomeFrameTimingBenchmark.kt
+++ macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/HomeFrameTimingBenchmark.kt
@@ -1,5 +1,6 @@
 package com.tkpd.macrobenchmark
 
+import android.content.Context
 import android.content.Intent
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
@@ -7,9 +8,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Direction
 import androidx.test.uiautomator.UiDevice
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,40 +28,46 @@ import org.junit.runner.RunWith
 @SdkSuppress(minSdkVersion = 29)
 @RunWith(AndroidJUnit4::class)
 class HomeFrameTimingBenchmark {
-    @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
+//    @get:Rule
+//    val benchmarkRule = MacrobenchmarkRule()
+
+    @Before
+    fun startDeveloperOptions() {
+        val instrumentation = InstrumentationRegistry.getInstrumentation()
+        instrumentation.targetContext.startActivity(MacroIntent.getHomeMacroSetupIntent())
+        Thread.sleep(5000)
+    }
 
     @Test
     fun start() {
         val instrumentation = InstrumentationRegistry.getInstrumentation()
         val device = UiDevice.getInstance(instrumentation)
 
-        instrumentation.context.startActivity(MacroIntent.getHomeMacroSetupIntent())
-        Thread.sleep(5000)
-        /**
-         * Read the docs
-         * https://developer.android.com/studio/profile/macrobenchmark
-         */
-        benchmarkRule.measureRepeated(
-            packageName = PACKAGE_NAME,
-            metrics = listOf(FrameTimingMetric()),
-            // Try switching to different compilation modes to see the effect
-            // it has on frame timing metrics.
-            compilationMode = MacroArgs.getCompilationMode(InstrumentationRegistry.getArguments()),
-            iterations = MacroArgs.getIterations(InstrumentationRegistry.getArguments()),
-            setupBlock = {
-                MacroIntent.getHomeIntent()
-            }
-        ) {
-            val recycler = device.findObject(By.res(PACKAGE_NAME, RESOURCE_ID))
-            // Set gesture margin to avoid triggering gesture navigation
-            // with input events from automation.
-            recycler.setGestureMargin(device.displayWidth / 5)
-            for (i in 1..(MacroArgs.getRecyclerViewScrollIterations(InstrumentationRegistry.getArguments()))) {
-                recycler.scroll(Direction.DOWN, 2f)
-                device.waitForIdle()
-            }
-        }
+
+//        /**
+//         * Read the docs
+//         * https://developer.android.com/studio/profile/macrobenchmark
+//         */
+//        benchmarkRule.measureRepeated(
+//            packageName = PACKAGE_NAME,
+//            metrics = listOf(FrameTimingMetric()),
+//            // Try switching to different compilation modes to see the effect
+//            // it has on frame timing metrics.
+//            compilationMode = MacroArgs.getCompilationMode(InstrumentationRegistry.getArguments()),
+//            iterations = MacroArgs.getIterations(InstrumentationRegistry.getArguments()),
+//            setupBlock = {
+//                MacroIntent.getHomeIntent()
+//            }
+//        ) {
+//            val recycler = device.findObject(By.res(PACKAGE_NAME, RESOURCE_ID))
+//            // Set gesture margin to avoid triggering gesture navigation
+//            // with input events from automation.
+//            recycler.setGestureMargin(device.displayWidth / 5)
+//            for (i in 1..(MacroArgs.getRecyclerViewScrollIterations(InstrumentationRegistry.getArguments()))) {
+//                recycler.scroll(Direction.DOWN, 2f)
+//                device.waitForIdle()
+//            }
+//        }
     }
 
     companion object {
--- macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/MacroIntent.kt
+++ macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/MacroIntent.kt
@@ -13,7 +13,7 @@ object MacroIntent {
 
     fun getHomeMacroSetupIntent(): Intent {
         val intent = Intent()
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         intent.setData(Uri.parse("tokopedia://setting/dev-opts/home-macrobenchmark/setup"))
         return intent
     }
