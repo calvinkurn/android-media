3bdd9c5a4bc66ef222ace90fee25b64a44085fdc
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-06-28 06:40:24 +0700

    Convert unit test from spek to junit

--- features/logistic/logisticaddaddress/src/test/java/com/tokopedia/logisticaddaddress/features/district_recommendation/DiscomPresenterTest.kt
+++ features/logistic/logisticaddaddress/src/test/java/com/tokopedia/logisticaddaddress/features/district_recommendation/DiscomPresenterTest.kt
@@ -1,205 +1,163 @@
 package com.tokopedia.logisticaddaddress.features.district_recommendation
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.logisticCommon.data.entity.address.Token
+import com.tokopedia.logisticCommon.domain.usecase.RevGeocodeUseCase
 import com.tokopedia.logisticaddaddress.domain.mapper.DistrictRecommendationMapper
 import com.tokopedia.logisticaddaddress.domain.model.Address
 import com.tokopedia.logisticaddaddress.domain.model.AddressResponse
 import com.tokopedia.logisticaddaddress.domain.usecase.GetDistrictRecommendation
 import com.tokopedia.logisticaddaddress.domain.usecase.GetDistrictRequestUseCase
 import com.tokopedia.logisticaddaddress.helper.DiscomDummyProvider
-import com.tokopedia.logisticCommon.data.entity.address.Token
-import com.tokopedia.logisticCommon.domain.usecase.RevGeocodeUseCase
+import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import io.mockk.verifyOrder
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.gherkin.Feature
+import org.hamcrest.MatcherAssert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
 import rx.Observable
 import rx.Subscriber
 
-object DiscomPresenterTest: Spek({
-    val first_page = 1
-    val view: DiscomContract.View = mockk(relaxed = true)
-    val rest: GetDistrictRequestUseCase = mockk(relaxUnitFun = true)
-    val gql: GetDistrictRecommendation = mockk(relaxUnitFun = true)
-    val revGeocodeUseCase: RevGeocodeUseCase = mockk(relaxed = true)
-    val mapper = DistrictRecommendationMapper()
+class DiscomPresenterTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val firstPage = 1
+
+    private val view: DiscomContract.View = mockk(relaxed = true)
+    private val getDistrictRequestUseCase: GetDistrictRequestUseCase = mockk(relaxUnitFun = true)
+    private val getDistrictRecommendation: GetDistrictRecommendation = mockk(relaxUnitFun = true)
+    private val revGeocodeUseCase: RevGeocodeUseCase = mockk(relaxed = true)
+    private val mapper = DistrictRecommendationMapper()
+
     lateinit var presenter: DiscomPresenter
 
-    beforeEachTest {
-        presenter = DiscomPresenter(rest, revGeocodeUseCase, gql, mapper)
+    private val throwable = Throwable()
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        presenter = DiscomPresenter(getDistrictRequestUseCase, revGeocodeUseCase, getDistrictRecommendation, mapper)
         presenter.attach(view)
     }
 
-    Feature("load data with data") {
-        Scenario("load data with data return success") {
-            val query = "jak"
-            val expected = DiscomDummyProvider.getSuccessResponse()
-            Given("data") {
-                every { gql.execute(any(), any())
-                } answers {
-                    Observable.just(expected)
-                }
-            }
+    @Test
+    fun `load data with data`() {
+        val expected = DiscomDummyProvider.getSuccessResponse()
 
-            When("load data") {
-                presenter.loadData(query, first_page)
-            }
-
-            Then("return success") {
-                verify {
-                    view.renderData(withArg { org.assertj.core.api.Assertions.assertThat(it).isNotEmpty }, expected.keroDistrictRecommendation.nextAvailable)
-                }
-
-                verifyOrder {
-                    view.setLoadingState(true)
-                    view.setLoadingState(false)
-                }
-            }
+        every { getDistrictRecommendation.execute(any(), any())
+        } answers  {
+            Observable.just(expected)
         }
 
-        Scenario("load data with data return error") {
-            val query = "jak"
-            val throwable = Throwable()
-            Given("data") {
-                every { gql.execute(any(), any())
-                } answers {
-                    Observable.error(throwable)
-                }
-            }
+        presenter.loadData("jak", firstPage)
 
-            When("load data") {
-                presenter.loadData(query, first_page)
-            }
-
-            Then("return error"){
-                verify {
-                    view.showGetListError(throwable)
-                }
-
-                verifyOrder {
-                    view.setLoadingState(true)
-                    view.setLoadingState(false)
-                }
-            }
-        }
-
-        Scenario("load data with data return empty") {
-            val query = "qwr"
-            val datum = DiscomDummyProvider.getEmptyResponse()
-            Given("data") {
-                every { gql.execute(any(), any())
-                } answers {
-                    Observable.just(datum)
-                }
-            }
-
-            When("load data") {
-                presenter.loadData(query, first_page)
-            }
-
-            Then("return empty") {
-                verify {
-                    view.showEmpty()
-                }
-
-                verifyOrder {
-                    view.setLoadingState(true)
-                    view.setLoadingState(false)
-                }
-            }
+        verifyOrder {
+            view.setLoadingState(true)
+            view.setLoadingState(false)
+            view.renderData(withArg { org.assertj.core.api.Assertions.assertThat(it).isNotEmpty }, expected.keroDistrictRecommendation.nextAvailable)
         }
     }
 
-    Feature("load data with token data") {
-        Scenario("load data with token return success"){
-            val query = "jak"
-            val expected = AddressResponse().apply {
-                addresses = arrayListOf(
-                        Address().apply { cityName = "Jakarta" }
-                )
-                isNextAvailable = false
-            }
-            Given("token data") {
-                every { rest.execute(any(), any())
-                } answers {
-                    secondArg<Subscriber<AddressResponse>>().onNext(expected)
-                }
-            }
-
-            When("load data") {
-                presenter.loadData(query, first_page, Token())
-            }
-
-            Then("return success") {
-                verify {
-                    view.renderData(expected.addresses, expected.isNextAvailable)
-                }
-            }
+    @Test
+    fun `load data with data return error`() {
+        every { getDistrictRecommendation.execute(any(), any())
+        } answers  {
+            Observable.error(throwable)
         }
 
-        Scenario("load data with token return error") {
-            val query = "jak"
-            val throwable = Throwable()
-            every { rest.execute(any(), any()) } answers {
-                secondArg<Subscriber<AddressResponse>>().onError(throwable)
-            }
-            Given("token data") {
-               every { rest.execute(any(), any())
-               } answers {
-                   secondArg<Subscriber<AddressResponse>>().onError(throwable)
-               }
-            }
+        presenter.loadData("jak", firstPage)
 
-            When("load data") {
-                presenter.loadData(query, first_page, Token())
-            }
-
-            Then("return error") {
-                verify {
-                    view.showGetListError(throwable)
-                }
-            }
-        }
-
-        Scenario("load data with token return empty") {
-            val query = "qwr"
-            val expected = AddressResponse().apply {
-                addresses = arrayListOf()
-                isNextAvailable = false
-            }
-            Given("token data") {
-                every { rest.execute(any(), any())
-                } answers {
-                    secondArg<Subscriber<AddressResponse>>().onNext(expected)
-                }
-            }
-
-            When("load data") {
-                presenter.loadData(query, first_page, Token())
-            }
-
-            Then("return empty") {
-                verify {
-                    view.showEmpty()
-                }
-            }
+        verifyOrder {
+            view.setLoadingState(true)
+            view.setLoadingState(false)
+            view.showGetListError(throwable)
         }
     }
 
-    Feature("onDetach") {
-        Scenario("presenter onDetach") {
-            When("presenter onDetach") {
-                presenter.detach()
-            }
+    @Test
+    fun `load data with data return empty`() {
+        val expected = DiscomDummyProvider.getEmptyResponse()
 
-            Then("gql unsubscribed") {
-                gql.unsubscribe()
-            }
+        every { getDistrictRecommendation.execute(any(), any())
+        } answers  {
+            Observable.just(expected)
+        }
 
-            Then("rest unsubscrbed") {
-                rest.unsubscribe()
-            }
+        presenter.loadData("jak", firstPage)
+
+        verifyOrder {
+            view.setLoadingState(true)
+            view.setLoadingState(false)
+            view.showEmpty()
         }
     }
-})
+
+    @Test
+    fun `load data with token data`() {
+        val expected = AddressResponse().apply {
+            addresses = arrayListOf(
+                    Address().apply { cityName = "Jakarta" }
+            )
+            isNextAvailable = false
+        }
+        every { getDistrictRequestUseCase.execute(any(), any())
+        } answers {
+            secondArg<Subscriber<AddressResponse>>().onNext(expected)
+        }
+
+        presenter.loadData("jak", firstPage, Token())
+
+        verifyOrder {
+            view.renderData(expected.addresses, expected.isNextAvailable)
+        }
+    }
+
+    @Test
+    fun `load data with token data return error`() {
+
+        every { getDistrictRequestUseCase.execute(any(), any())
+        } answers {
+            secondArg<Subscriber<AddressResponse>>().onError(throwable)
+        }
+
+        presenter.loadData("jak", firstPage, Token())
+
+        verifyOrder {
+            view.showGetListError(throwable)
+        }
+    }
+
+    @Test
+    fun `load data with token data return empty`() {
+        val expected = AddressResponse().apply {
+            addresses = arrayListOf()
+            isNextAvailable = false
+        }
+
+        every { getDistrictRequestUseCase.execute(any(), any())
+        } answers {
+            secondArg<Subscriber<AddressResponse>>().onNext(expected)
+        }
+
+        presenter.loadData("jak", firstPage, Token())
+
+        verifyOrder {
+            view.showEmpty()
+        }
+    }
+
+    @Test
+    fun `onDetach`() {
+        presenter.detach()
+
+        verifyOrder {
+            getDistrictRequestUseCase.unsubscribe()
+            getDistrictRecommendation.unsubscribe()
+        }
+    }
+}
