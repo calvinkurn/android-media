f43116163d6b2215b6d837f52c0fd97810cd8666
misael-jonathan <misael.jonathan@tokopedia.com>
2021-06-28 16:48:11 +0700

    on progress

--- features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/view/adapter/TopupBillsFavoriteNumberListAdapter.kt
+++ features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/view/adapter/TopupBillsFavoriteNumberListAdapter.kt
@@ -3,25 +3,43 @@ package com.tokopedia.common.topupbills.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.common.topupbills.data.TopupBillsSeamlessFavNumberItem
 import com.tokopedia.common.topupbills.databinding.ItemTopupBillsFavoritNumberBinding
+import com.tokopedia.common.topupbills.databinding.ItemTopupBillsFavoriteNumberEmptyStateBinding
+import com.tokopedia.common.topupbills.view.listener.FavoriteNumberEmptyStateListener
 import com.tokopedia.common.topupbills.view.listener.OnFavoriteNumberClickListener
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 
 class TopupBillsFavoriteNumberListAdapter (
-        val listener: OnFavoriteNumberClickListener,
+        val favoriteNumberListener: OnFavoriteNumberClickListener,
+        val emptyStateListener: FavoriteNumberEmptyStateListener,
         var clientNumbers: List<TopupBillsSeamlessFavNumberItem>
-): RecyclerView.Adapter<TopupBillsFavoriteNumberListAdapter.FavoriteNumberViewHolder>() {
+): RecyclerView.Adapter<BaseFavoriteNumberViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopupBillsFavoriteNumberListAdapter.FavoriteNumberViewHolder {
-        val binding = ItemTopupBillsFavoritNumberBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false)
-        return FavoriteNumberViewHolder(binding)
+        if (viewType == VIEW_TYPE_FAVORITE_NUMBER) {
+            val binding = ItemTopupBillsFavoritNumberBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false)
+            return FavoriteNumberViewHolder(binding)
+        } else if (viewType == VIEW_TYPE_EMPTY_STATE) {
+            val binding = ItemTopupBillsFavoriteNumberEmptyStateBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false)
+            return FavoriteNumberEmptyViewHolder(binding)
+        }
     }
 
     override fun onBindViewHolder(holder: TopupBillsFavoriteNumberListAdapter.FavoriteNumberViewHolder, position: Int) {
         holder.bind(clientNumbers[position])
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (clientNumbers.isEmpty()) {
+            VIEW_TYPE_EMPTY_STATE
+        } else {
+            VIEW_TYPE_FAVORITE_NUMBER
+        }
     }
 
     override fun getItemCount(): Int = clientNumbers.size
@@ -29,6 +47,10 @@ class TopupBillsFavoriteNumberListAdapter (
     fun setNumbers(clientNumbers: List<TopupBillsSeamlessFavNumberItem>) {
         this.clientNumbers = clientNumbers
         notifyDataSetChanged()
+    }
+
+    abstract inner class BaseFavoriteNumberViewHolder {
+        abstract fun bind(item: Visitable)
     }
 
     inner class FavoriteNumberViewHolder(
@@ -53,9 +75,27 @@ class TopupBillsFavoriteNumberListAdapter (
                     commonTopupbillsFavoriteNumberIcon.setImageUrl(favClientNumber.iconUrl)
                 }
                 commonTopupbillsFavoriteNumberContainer.setOnClickListener {
-                    listener.onFavoriteNumberClick(favClientNumber)
+                    favoriteNumberListener.onFavoriteNumberClick(favClientNumber)
                 }
             }
         }
+    }
+
+    inner class FavoriteNumberEmptyViewHolder(
+            private val binding: ItemTopupBillsFavoriteNumberEmptyStateBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind() {
+            binding.run {
+                commonTopupbillsEmptyStateButton.setOnClickListener {
+                    emptyStateListener.onContinueClicked()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_FAVORITE_NUMBER = 1
+        private const val VIEW_TYPE_EMPTY_STATE = 2
     }
 }
