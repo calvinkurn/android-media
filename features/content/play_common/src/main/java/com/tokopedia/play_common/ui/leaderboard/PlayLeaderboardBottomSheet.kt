9268150e3cbb4d41ee0da8f8d2c8a257e1c1be2c
Meyta <meytajennist@gmail.com>
2021-06-29 16:47:37 +0700

    initial leaderboard bottomsheet for engagement tools

---
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/ui/leaderboard/PlayLeaderboardBottomSheet.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.play_common.ui.leaderboard
+
+import com.tokopedia.unifycomponents.BottomSheetUnify
+
+
+/**
+ * Created by mzennis on 29/06/21.
+ */
+class PlayLeaderboardBottomSheet : BottomSheetUnify() {
+
+
+}
