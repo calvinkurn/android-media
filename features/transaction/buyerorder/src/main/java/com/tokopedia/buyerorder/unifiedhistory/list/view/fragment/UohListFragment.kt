eb7483fdbd90e506a7cac5b56ba8c51b2fdc4bfb
dhaba.widhikari <wikodhawi@gmail.com>
2021-06-29 11:24:27 +0700

    - remove delay when MIN_KEYWORD_CHARACTER_COUNT then activated after MIN_KEYWORD_CHARACTER_COUNT - Add icon with false animation

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/fragment/UohListFragment.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/fragment/UohListFragment.kt
@@ -455,29 +455,36 @@ class UohListFragment : BaseDaggerFragment(), RefreshHandler.OnRefreshHandlerLis
 
         statusbar.layoutParams.height = ViewHelper.getStatusBarHeight(activity)
         uoh_navtoolbar?.let {
-            var timer: CountDownTimer? = null
+            var timerAction: CountDownTimer? = null
             it.setupSearchbar(searchbarType = NavToolbar.Companion.SearchBarType.TYPE_EDITABLE, hints = arrayListOf(
                 HintData(getString(R.string.hint_cari_transaksi) )),
-                navSearchbarInterface = { query,_,count,_ ->
-                    if (timer != null) timer!!.cancel()
-                    timer = object : CountDownTimer(800, 1000) {
-                        override fun onTick(l: Long) {}
-                        override fun onFinish() {
-                            searchQuery = query.toString()
-                            when (searchQuery.length) {
-                                0 -> {
-                                    view?.let { context?.let { it1 -> UohUtils.hideKeyBoard(it1, it) } }
-                                    triggerSearch()
-                                }
-                                in 1 until MIN_KEYWORD_CHARACTER_COUNT -> {
-                                    showToaster(getString(R.string.error_message_minimum_search_keyword), Toaster.TYPE_ERROR)
-                                }
-                                else -> {
-                                    triggerSearch()
+                navSearchbarInterface = { query,_,_,_ ->
+                    if (timerAction != null) timerAction?.cancel()
+                    if (query.toString().length in 1 until MIN_KEYWORD_CHARACTER_COUNT) {
+
+                        showToaster(getString(R.string.error_message_minimum_search_keyword), Toaster.TYPE_ERROR)
+                    }
+                    else
+                    {
+                        timerAction = object : CountDownTimer(800, 1000) {
+                            override fun onTick(l: Long) {}
+                            override fun onFinish() {
+                                searchQuery = query.toString()
+                                when {
+                                    searchQuery.isBlank() -> {
+                                        view?.let { context?.let { it1 -> UohUtils.hideKeyBoard(it1, it) } }
+                                        triggerSearch()
+                                    }
+                                    searchQuery.length in 1 until MIN_KEYWORD_CHARACTER_COUNT -> {
+                                        showToaster(getString(R.string.error_message_minimum_search_keyword), Toaster.TYPE_ERROR)
+                                    }
+                                    else -> {
+                                        triggerSearch()
+                                    }
                                 }
                             }
-                        }
-                    }.start()
+                        }.start()
+                    }
                 }
             )
             val icons = IconBuilder(
