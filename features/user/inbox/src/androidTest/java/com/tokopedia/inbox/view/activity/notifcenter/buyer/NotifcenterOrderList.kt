e5edcc72376e89e443bb1568c9fb904ee9dc204f
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-30 15:47:37 +0700

    add TODO IT notif order on notifcenter

---
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/view/activity/notifcenter/buyer/NotifcenterOrderList.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.inbox.view.activity.notifcenter.buyer
+
+class NotifcenterOrderList {
+
+    // TODO: should show order list when success load order list with no cache
+    // TODO: should hide order list when user has notification filter
+    // TODO: should show cached version order list when cache data is exist
+    // TODO: should update currently visible cached order list with counter when finished loading remote data
+    // TODO: should placed first when notification rendered first
+    // TODO: should placed first when rendered first instead of notification
+    // TODO: should render notification empty state when notification remote data is empty
+
+}
