5ad0d2a4256ca214cab82b47a5e903c7d7d05b9c
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-06-30 18:47:13 +0700

    add TODO IT NotifcenterNotificationList

---
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/view/activity/notifcenter/buyer/NotifcenterNotificationList.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.inbox.view.activity.notifcenter.buyer
+
+class NotifcenterNotificationList {
+
+    // TODO: should show new list title
+    // TODO: should show new list section load more button when new list section has next true
+    // TODO: should hide new list section load more button when new list section has next false
+    // TODO: should show new list section next page when load more button success clicked
+    // TODO: load more button on new list should not clickable when loading new list
+    // TODO: should render notifications only when load more new list
+
+    // TODO: should show earlier title
+    // TODO: should show earlier section load more button when earlier section has next true
+    // TODO: should hide earlier section load more button when earlier section has next false
+    // TODO: should show earlier section next page when load more button success clicked
+    // TODO: load more button on earlier should not clickable when loading earlier notifications
+    // TODO: should render notifications only when load more earlier
+
+    // TODO: should show new list title and earlier title when both response is not empty
+    // TODO: should show empty state with just text when notifications is empty
+    // TODO: should show empty state with illustration when notifications is empty with filter
+
+}
