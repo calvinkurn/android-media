ffb88bb19f4d8a8f658faf0b976f080808c24e13
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-06-30 14:44:28 +0700

    feat: add error handler in homeevent

---
+++ features/entertainment/event/src/main/java/com/tokopedia/entertainment/common/util/EventGlobalError.kt
@@ -0,0 +1,48 @@
+package com.tokopedia.entertainment.common.util
+
+import android.content.Context
+import android.view.Gravity
+import android.view.ViewGroup
+import com.tokopedia.globalerror.GlobalError
+import com.tokopedia.globalerror.showUnifyError
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.network.utils.ErrorHandler
+
+object EventGlobalError {
+
+    fun errorEventHandlerGlobalError(context: Context,
+                                     throwable: Throwable,
+                                     containerError: ViewGroup,
+                                     globalError: GlobalError,
+                                     defaultAction: () -> Unit = { }
+    ) {
+        containerError.apply {
+            show()
+            showUnifyError(throwable, {
+                defaultAction()
+            }, {
+                globalError.show()
+                showUnexpectedError(context, globalError, throwable)
+            })
+
+            findViewById<GlobalError>(com.tokopedia.globalerror.R.id.globalerror_view)?.let {
+                it.gravity = Gravity.CENTER
+            }
+        }
+    }
+
+    private fun showUnexpectedError(context: Context,
+                                    globalError: GlobalError,
+                                    throwable: Throwable,
+                                    defaultAction: () -> Unit = { }
+    ) {
+        globalError.apply {
+            errorTitle.text = ErrorHandler.getErrorMessage(context, throwable)
+            setActionClickListener {
+                defaultAction()
+            }
+            errorSecondaryAction.hide()
+        }
+    }
+}
