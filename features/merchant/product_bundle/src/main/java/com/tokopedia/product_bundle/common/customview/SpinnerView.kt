b1f824085d0159194de444f29f7ae3a274487ba9
faisalramdani <faisal.ramdani@tokopedia.com>
2021-07-01 09:47:34 +0700

    fix click behavior to SpinnerView

--- features/merchant/product_bundle/src/main/java/com/tokopedia/product_bundle/common/customview/SpinnerView.kt
+++ features/merchant/product_bundle/src/main/java/com/tokopedia/product_bundle/common/customview/SpinnerView.kt
@@ -1,12 +1,17 @@
 package com.tokopedia.product_bundle.common.customview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import com.tokopedia.product_bundle.R
 import com.tokopedia.unifycomponents.BaseCustomView
+import com.tokopedia.unifycomponents.CardUnify
+import com.tokopedia.unifycomponents.CardUnify.Companion.TYPE_BORDER
+import com.tokopedia.unifycomponents.CardUnify.Companion.TYPE_BORDER_ACTIVE
 import com.tokopedia.unifyprinciples.Typography
 
 class SpinnerView : BaseCustomView {
@@ -23,6 +28,7 @@ class SpinnerView : BaseCustomView {
             refreshViews()
         }
 
+    var cardSpinner: CardUnify? = null
     var layoutSpinner: LinearLayout? = null
     var tvSpinner: Typography? = null
     private var onClickListener: () -> Unit = {}
@@ -41,13 +47,27 @@ class SpinnerView : BaseCustomView {
 
     private fun init(attrs: AttributeSet?) {
         val view = View.inflate(context, R.layout.customview_spinner, this)
+        cardSpinner = view.findViewById(R.id.card_spinner)
         layoutSpinner = view.findViewById(R.id.layout_spinner)
         tvSpinner = view.findViewById(R.id.tv_spinner)
 
-        layoutSpinner?.setOnClickListener { onClickListener.invoke() }
+        setupLayoutOnClick(layoutSpinner, cardSpinner)
 
         defineCustomAttributes(attrs)
         refreshViews()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupLayoutOnClick(layoutSpinner: LinearLayout?, cardSpinner: CardUnify?) {
+        layoutSpinner?.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                cardSpinner?.cardType = (TYPE_BORDER_ACTIVE)
+            } else if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_CANCEL) {
+                cardSpinner?.cardType = (TYPE_BORDER)
+                onClickListener.invoke()
+            }
+            return@setOnTouchListener true
+        }
     }
 
     private fun defineCustomAttributes(attrs: AttributeSet?) {
