d8606035bbc108674afd3df2e80801e7ca869593
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-07-01 10:37:14 +0700

    refactor default response initialization

--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/FakeNotifOrderListUseCase.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/FakeNotifOrderListUseCase.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.inbox.fake.domain.usecase.notifcenter
 
+import com.tokopedia.inbox.common.AndroidFileUtil
 import com.tokopedia.inbox.fake.common.FakeGraphqlUseCase
+import com.tokopedia.inbox.test.R
 import com.tokopedia.notifcenter.common.network.NotifcenterCacheManager
 import com.tokopedia.notifcenter.data.entity.orderlist.NotifOrderListResponse
 import com.tokopedia.notifcenter.domain.NotifOrderListUseCase
@@ -16,15 +18,23 @@ class FakeNotifOrderListUseCase(
 ) : NotifOrderListUseCase(
     query, gqlUseCase, cacheManager, userSession
 ) {
-
     var response = NotifOrderListResponse()
         set(value) {
             field = value
             gqlUseCase.response = value
         }
 
+    val defaultResponse: NotifOrderListResponse
+        get() = AndroidFileUtil.parseRaw(
+            R.raw.notifcenter_notif_order_list, NotifOrderListResponse::class.java
+        )
+
     init {
         response = response
+    }
+
+    fun initialize() {
+        this.response = defaultResponse
     }
 
 }
--- features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/topads/FakeTopAdsRepository.kt
+++ features/user/inbox/src/androidTest/java/com/tokopedia/inbox/fake/domain/usecase/notifcenter/topads/FakeTopAdsRepository.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.inbox.fake.domain.usecase.notifcenter.topads
 
 import com.tokopedia.common.network.coroutines.repository.RestRepository
+import com.tokopedia.inbox.common.AndroidFileUtil
+import com.tokopedia.inbox.test.R
 import com.tokopedia.topads.sdk.domain.model.TopAdsmageViewResponse
 import com.tokopedia.topads.sdk.repository.TopAdsRepository
 
@@ -12,8 +14,17 @@ class FakeTopAdsRepository : TopAdsRepository() {
             repository.response = value
         }
 
+    val defaultResponse: TopAdsmageViewResponse
+        get() = AndroidFileUtil.parseRaw(
+            R.raw.notifcenter_tdn, TopAdsmageViewResponse::class.java
+        )
+
     private val repository = FakeTopAdsRestRepository()
 
     override val restRepository: RestRepository
         get() = repository
+
+    fun initialize() {
+        this.response = defaultResponse
+    }
 }
