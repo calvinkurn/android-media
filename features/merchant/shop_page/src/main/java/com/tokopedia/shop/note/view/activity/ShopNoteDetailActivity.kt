a3d193d97fca53b745108975ac259c88a438f930
saidfaisal <said.faisal@tokopedia.com>
2021-07-01 08:58:50 +0700

    fix linter and fix background on shop note detail

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/note/view/activity/ShopNoteDetailActivity.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/note/view/activity/ShopNoteDetailActivity.kt
@@ -2,18 +2,18 @@ package com.tokopedia.shop.note.view.activity
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.shop.R
 import com.tokopedia.shop.common.constant.ShopParamConstant
 import com.tokopedia.shop.note.view.fragment.ShopNoteDetailFragment.Companion.newInstance
 
 /**
  * Created by normansyahputa on 2/8/18.
  */
-class ShopNoteDetailActivity : BaseSimpleActivity() {
+class ShopNoteDetailActivity :BaseSimpleActivity() {
     private var shopNoteId: String? = null
     private var shopId: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +21,15 @@ class ShopNoteDetailActivity : BaseSimpleActivity() {
         shopNoteId = intent.getStringExtra(ShopParamConstant.EXTRA_SHOP_NOTE_ID)
         super.onCreate(savedInstanceState)
         window?.decorView?.setBackgroundColor(MethodChecker.getColor(this, com.tokopedia.unifyprinciples.R.color.Unify_Background))
-        toolbar.setTitleTextColor(MethodChecker.getColor(this, com.tokopedia.unifyprinciples.R.color.Unify_N700_96))
     }
 
-    override fun getNewFragment(): Fragment? {
-        return newInstance(shopId!!, shopNoteId!!)
-    }
+    override fun getLayoutRes() = R.layout.activity_shop_info
+
+    override fun getNewFragment(): Fragment = newInstance(shopId!!, shopNoteId!!)
+
+    override fun getParentViewResourceID(): Int = R.id.parent_view
+
+    override fun getToolbarResourceID(): Int = R.id.toolbar
 
     companion object {
         fun createIntent(context: Context?, shopId: String?, shopNoteId: String?): Intent {
