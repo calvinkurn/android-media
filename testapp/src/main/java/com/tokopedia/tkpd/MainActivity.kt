629742cfd58a18238ba60cdb06c80b53a2b331ad
yehezkiel <yehezkiel@tokopedia.com>
2021-07-06 15:33:43 +0700

    revert

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -14,6 +14,7 @@ import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalTestApp
 import com.tokopedia.tkpd.helper.logout
+import com.tokopedia.tkpd.network.DataSource
 import com.tokopedia.tkpd.testgql.TestGqlUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
