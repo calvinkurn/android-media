fa12f0812ac7ab4efc6f5a795c6abd22c3d93418
Felicia Widjaja <fwidjaja@live.com>
2021-07-07 14:44:08 +0700

    dismiss bottomSheetKebabMenu

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/fragment/UohListFragment.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/unifiedhistory/list/view/fragment/UohListFragment.kt
@@ -1530,6 +1530,7 @@ class UohListFragment : BaseDaggerFragment(), RefreshHandler.OnRefreshHandlerLis
             }
         } else if (dotMenu.actionType.equals(TYPE_ACTION_CANCEL_ORDER, true)) {
             if (dotMenu.appURL.contains(APPLINK_BASE)) {
+                bottomSheetKebabMenu?.dismiss()
                 var helpLinkUrl = ""
                 orderData.metadata.dotMenus.forEach {
                     if (it.label.equals(LABEL_HELP_LINK)) {
