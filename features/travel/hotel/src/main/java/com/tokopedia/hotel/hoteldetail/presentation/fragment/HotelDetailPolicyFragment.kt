1c3115ab5f727d4c4eb8ffde85bf2b9d281bb59e
astidhiyaa <asti.anzaria@tokopedia.com>
2021-07-08 19:13:17 +0800

    feat: add binding in HotelDetailPolicyFragment.kt.kt

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/hoteldetail/presentation/fragment/HotelDetailPolicyFragment.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/hoteldetail/presentation/fragment/HotelDetailPolicyFragment.kt
@@ -7,10 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.abstraction.base.view.fragment.BaseListFragment
 import com.tokopedia.hotel.R
+import com.tokopedia.hotel.databinding.FragmentHotelDetailPolicyBinding
 import com.tokopedia.hotel.hoteldetail.data.entity.PropertyPolicyData
 import com.tokopedia.hotel.hoteldetail.presentation.adapter.HotelDetailFacilityAdapterTypeFactory
 import com.tokopedia.hotel.hoteldetail.presentation.model.HotelDetailPolicyModel
-import kotlinx.android.synthetic.main.fragment_hotel_detail_policy.*
+import com.tokopedia.utils.lifecycle.autoClearedNullable
 
 /**
  * @author by furqan on 07/05/19
@@ -18,9 +19,12 @@ import kotlinx.android.synthetic.main.fragment_hotel_detail_policy.*
 class HotelDetailPolicyFragment : BaseListFragment<PropertyPolicyData, HotelDetailFacilityAdapterTypeFactory>() {
 
     lateinit var connector: Connector
+    private var binding by autoClearedNullable<FragmentHotelDetailPolicyBinding>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_hotel_detail_policy, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHotelDetailPolicyBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
 
     override fun getSwipeRefreshLayoutResourceId() = 0
 
@@ -31,21 +35,21 @@ class HotelDetailPolicyFragment : BaseListFragment<PropertyPolicyData, HotelDeta
 
         if (::connector.isInitialized) {
             setupPolicySwitcher()
-            recycler_view.isFocusable = false
+            binding?.recyclerView?.isFocusable = false
         }
     }
 
     private fun setupPolicySwitcher() {
         if (connector.getPolicyData().checkInTo.isNotEmpty()) {
-            scv_hotel_date.setLeftTitleText(getString(R.string.hotel_detail_check_from_to, connector.getPolicyData().checkInFrom, connector.getPolicyData().checkInTo))
+            binding?.scvHotelDate?.setLeftTitleText(getString(R.string.hotel_detail_check_from_to, connector.getPolicyData().checkInFrom, connector.getPolicyData().checkInTo))
         } else {
-            scv_hotel_date.setLeftTitleText(getString(R.string.hotel_detail_check_start_from, connector.getPolicyData().checkInFrom))
+            binding?.scvHotelDate?.setLeftTitleText(getString(R.string.hotel_detail_check_start_from, connector.getPolicyData().checkInFrom))
         }
 
         if (connector.getPolicyData().checkOutFrom.isNotEmpty()) {
-            scv_hotel_date.setRightTitleText(getString(R.string.hotel_detail_check_from_to, connector.getPolicyData().checkOutFrom, connector.getPolicyData().checkOutTo))
+            binding?.scvHotelDate?.setRightTitleText(getString(R.string.hotel_detail_check_from_to, connector.getPolicyData().checkOutFrom, connector.getPolicyData().checkOutTo))
         } else {
-            scv_hotel_date.setRightTitleText(getString(R.string.hotel_detail_check_to, connector.getPolicyData().checkOutTo))
+            binding?.scvHotelDate?.setRightTitleText(getString(R.string.hotel_detail_check_to, connector.getPolicyData().checkOutTo))
         }
     }
 
