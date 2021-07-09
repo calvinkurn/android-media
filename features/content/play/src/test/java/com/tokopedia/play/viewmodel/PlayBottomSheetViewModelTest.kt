98cea8ea7d5c41610ff9809d6074915e89ae9b9c
yehezkiel <yehezkiel@tokopedia.com>
2021-07-09 19:03:17 +0700

    fix play unit test

--- features/content/play/src/test/java/com/tokopedia/play/viewmodel/PlayBottomSheetViewModelTest.kt
+++ features/content/play/src/test/java/com/tokopedia/play/viewmodel/PlayBottomSheetViewModelTest.kt
@@ -15,6 +15,7 @@ import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
 import com.tokopedia.play_common.util.event.Event
 import com.tokopedia.user.session.UserSessionInterface
+import com.tokopedia.variant_common.model.GetProductVariantResponse
 import com.tokopedia.variant_common.model.ProductDetailVariantCommonResponse
 import com.tokopedia.variant_common.use_case.GetProductVariantUseCase
 import com.tokopedia.variant_common.util.VariantCommonMapper
@@ -41,7 +42,7 @@ class PlayBottomSheetViewModelTest {
 
     private val modelBuilder = ModelBuilder()
     private val productModelBuilder = PlayProductTagsModelBuilder()
-    private val mockProductVariantResponse: ProductDetailVariantCommonResponse = modelBuilder.buildProductVariant()
+    private val mockProductVariantResponse: GetProductVariantResponse = modelBuilder.buildProductVariant()
 
     private lateinit var playBottomSheetViewModel: PlayBottomSheetViewModel
 
