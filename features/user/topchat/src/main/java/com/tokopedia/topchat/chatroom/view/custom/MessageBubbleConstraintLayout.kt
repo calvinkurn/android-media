59188bc852ae16b91613c0f92c2b06d2a93fd0b0
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-07-12 19:04:34 +0700

    put reply bubble place holder at the top of bubble msg

---
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/custom/MessageBubbleConstraintLayout.kt
@@ -0,0 +1,37 @@
+package com.tokopedia.topchat.chatroom.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.topchat.R
+
+// TODO: add support for `showCheckMark` flexbox
+class MessageBubbleConstraintLayout: ConstraintLayout {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        initViewLayout()
+    }
+
+    private fun initViewLayout() {
+        View.inflate(context, LAYOUT, this)
+    }
+
+    companion object {
+        val LAYOUT = R.layout.partial_chat_messsage_bubble
+    }
+}
