18c1265aa260ccbb6cdf002ce575a9c4ff44c2f2
Muhammad Furqan <furqan@tokopedia.com>
2021-07-16 17:22:10 +0700

    [Embrace] Add Embrace to Logger

---
+++ libraries/logger/src/main/java/com/tokopedia/logger/datasource/cloud/LoggerCloudEmbraceImpl.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.logger.datasource.cloud
+
+import com.tokopedia.logger.model.EmbraceBody
+
+interface LoggerCloudEmbraceImpl {
+    suspend fun sendToLogServer(embraceBodyList: List<EmbraceBody>): Boolean
