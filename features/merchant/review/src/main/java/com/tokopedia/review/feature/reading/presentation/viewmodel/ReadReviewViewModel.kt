2c035da135400b4ee02b39d9ac5f589fcaf20b64
leonard2907 <leonard.gunawan@tokopedia.com>
2021-07-16 10:04:48 +0700

    Fix fab visibility

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/presentation/viewmodel/ReadReviewViewModel.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/presentation/viewmodel/ReadReviewViewModel.kt
@@ -50,6 +50,9 @@ class ReadReviewViewModel @Inject constructor(
         get() = _toggleLikeReview
 
     private val currentPage = MutableLiveData<Int>()
+    val page: LiveData<Int>
+        get() = currentPage
+
     private var productId: MutableLiveData<String> = MutableLiveData()
     private var sort: String = SortTypeConstants.MOST_HELPFUL_PARAM
     private var filter: SelectedFilters = SelectedFilters()
