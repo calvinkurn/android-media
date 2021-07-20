7b1319437e82b151e9a2cf0a79c218ae371d1504
Mayank Gera <mayank.gera@tokopedia.com>
2021-07-20 17:14:23 +0530

    remove code in goto

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -109,6 +109,5 @@ class MainActivity : AppCompatActivity() {
          * RouteManager.route(this, ApplinkConstInternalMarketplace.SHOP_SETTINGS)
          * LEAVE THIS EMPTY AS DEFAULT!!
          * */
-        RouteManager.route(this,"tokopedia-android-internal://global/discovery/goto")
     }
 }
