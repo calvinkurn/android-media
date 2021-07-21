788c96bb80ab588de3ff7e9e5450902247505074
hansenpw <hansen.wijaya@tokopedia.com>
2021-07-21 14:01:03 +0700

    adjust update cart id

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
@@ -87,7 +87,7 @@ class OrderSummaryPageCartProcessor @Inject constructor(private val atcOccMultiE
                 if (!it.isError) {
                     cart.add(
                             UpdateCartOccCartRequest(
-                                    if (it.cartId.isEmpty() || it.cartId == "0") null else it.cartId,
+                                    it.cartId,
                                     it.quantity.orderQuantity,
                                     it.notes,
                                     it.productId.toString()
