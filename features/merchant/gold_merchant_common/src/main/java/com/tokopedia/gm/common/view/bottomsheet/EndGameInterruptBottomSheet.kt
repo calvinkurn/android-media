24e7577fb4714cda83ef45ad8737f3c1d4868688
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-07-21 20:39:41 +0700

    create bottom sheet end game

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/view/bottomsheet/EndGameInterruptBottomSheet.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/view/bottomsheet/EndGameInterruptBottomSheet.kt
@@ -1,12 +1,13 @@
 package com.tokopedia.gm.common.view.bottomsheet
 
+import androidx.core.content.ContextCompat
+import com.tokopedia.gm.common.R
+
 class EndGameInterruptBottomSheet: BaseBottomSheet() {
 
-    override fun getResLayout(): Int {
-
-    }
+    override fun getResLayout(): Int = R.layout.bottom_sheet_end_game_interrupt_shop_score
 
     override fun setupView() = childView?.run {
-
+        setBackgroundColor(ContextCompat.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_Background))
     }
 }
