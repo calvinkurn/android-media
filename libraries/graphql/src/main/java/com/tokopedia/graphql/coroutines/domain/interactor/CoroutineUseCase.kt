79e07dfa6bd6443795a3b44b5edf2a27ebef073f
isfaaghyth <isfaaghyth@gmail.com>
2021-07-21 19:14:06 +0700

    created base coroutines use case

---
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/CoroutineUseCase.kt
@@ -0,0 +1,49 @@
+package com.tokopedia.graphql.coroutines.domain.interactor
+
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import com.tokopedia.graphql.util.LoggingUtils.logGqlErrorNetwork as logGqlErrorNetwork
+
+abstract class CoroutineUseCase<P, out R: Any> constructor(
+    private val dispatcher: CoroutineDispatcher
+) {
+
+    /*
+    * override this to set the code to be executed
+    * */
+    @Throws(RuntimeException::class)
+    protected abstract suspend fun execute(params: P): R
+
+    /*
+    * Executes the use case synchronously
+    *
+    * @param params the input parameters to run the use case with
+    * @return an generic class comes from P
+    * */
+    suspend operator fun invoke(params: P): Result<R> {
+        return try {
+            withContext(dispatcher) {
+                Success(execute(params))
+            }
+        } catch (e: Exception) {
+            logGqlErrorNetwork("CoroutineUseCase", "", e)
+            Fail(e)
+        }
+    }
+
+    protected suspend inline fun <reified T> GraphqlRepository.request(
+        graphqlQuery: String,
+        params: Map<String, Any>
+    ): T {
+        val request = GraphqlRequest(graphqlQuery, T::class.java, params)
+        val response = getReseponse(listOf(request))
+        return response.getSuccessData()
+    }
+
+}
