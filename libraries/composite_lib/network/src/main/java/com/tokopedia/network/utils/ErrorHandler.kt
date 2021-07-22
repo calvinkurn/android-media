8131a47f6408928b892516bb00bab15a14189f76
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2021-07-22 23:32:13 +0700

    add capability error handler hit Scalyr

--- libraries/composite_lib/network/src/main/java/com/tokopedia/network/utils/ErrorHandler.kt
+++ libraries/composite_lib/network/src/main/java/com/tokopedia/network/utils/ErrorHandler.kt
@@ -1,87 +1,117 @@
-package com.tokopedia.network.utils;
+package com.tokopedia.network.utils
 
-import android.content.Context;
-import android.text.TextUtils;
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import com.tokopedia.logger.LogManager
+import com.tokopedia.logger.utils.Priority
+import com.tokopedia.network.R
+import com.tokopedia.network.constant.ResponseStatus
+import com.tokopedia.network.data.model.response.ResponseV4ErrorException
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.network.utils.ExceptionDictionary.Companion.getErrorCodeSimple
+import com.tokopedia.network.utils.ExceptionDictionary.Companion.getRandomString
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
-import com.tokopedia.network.R;
-import com.tokopedia.network.constant.ResponseStatus;
-import com.tokopedia.network.data.model.response.ResponseV4ErrorException;
-import com.tokopedia.network.exception.MessageErrorException;
+object ErrorHandler {
 
-import java.io.IOException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
+    fun getErrorMessage(context: Context, e: Throwable): String {
+        return getErrorMessage(context, e, Builder())
+    }
 
-public class ErrorHandler {
-
-    public static String getErrorMessage(final Context context, Throwable e) {
-        String errorMessageString = getErrorMessageString(context, e);
-        String errorCode;
-        if(e instanceof MessageErrorException) {
+    fun getErrorMessage(
+        context: Context,
+        e: Throwable,
+        builder: Builder
+    ): String {
+        val errorMessageString =
+            getErrorMessageString(context, e)
+        val errorCode: String = if (e is MessageErrorException) {
             //http
-            errorCode = getErrorMessageHTTP(context, e);
+            getErrorMessageHTTP(context, e)
         } else {
             // native
-            errorCode = ExceptionDictionary.Companion.getErrorCodeSimple(e);
+            getErrorCodeSimple(e)
         }
-
-        String errorIdentifier = ExceptionDictionary.Companion.getRandomString(4);
-        String errorMessage = errorMessageString + " <" + errorCode +"-"+ errorIdentifier+">";
-        return errorMessage;
-
+        val errorIdentifier = getRandomString(4)
+        val mapParam = mapOf(
+            "identifier" to errorIdentifier,
+            "class" to context.javaClass.name,
+            "e" to Log.getStackTraceString(e)
+        )
+        if (builder.errorCode) {
+            mapParam.plus("error_code" to errorCode)
+        }
+        if (builder.sendToScalyr) {
+            LogManager.log(Priority.P2, "ERROR_HANDLER", mapParam)
+        }
+        return "$errorMessageString <$errorCode-$errorIdentifier>"
     }
 
-    public static String getErrorMessageString(final Context context, Throwable e) {
+    fun getErrorMessageString(context: Context?, e: Throwable?): String? {
         if (context == null || e == null) {
-            return "Terjadi kesalahan. Ulangi beberapa saat lagi";
+            return "Terjadi kesalahan. Ulangi beberapa saat lagi"
         }
-
-        if (e instanceof ResponseV4ErrorException) {
-            return ((ResponseV4ErrorException) e).getErrorList().get(0);
-        } else if (e instanceof UnknownHostException) {
-            return context.getString(R.string.msg_no_connection);
-        } else if (e instanceof SocketTimeoutException) {
-            return context.getString(R.string.default_request_error_timeout);
-        } else if (e instanceof RuntimeException &&
-                e.getLocalizedMessage() != null &&
-                !e.getLocalizedMessage().equals("") &&
-                e.getLocalizedMessage().length() <= 3) {
+        return if (e is ResponseV4ErrorException) {
+            e.errorList[0]
+        } else if (e is UnknownHostException) {
+            context.getString(R.string.msg_no_connection)
+        } else if (e is SocketTimeoutException) {
+            context.getString(R.string.default_request_error_timeout)
+        } else if (e is RuntimeException && e.getLocalizedMessage() != null &&
+            e.getLocalizedMessage() != "" && e.getLocalizedMessage().length <= 3
+        ) {
             try {
-                int code = Integer.parseInt(e.getLocalizedMessage());
-                switch (code) {
-                    case ResponseStatus.SC_REQUEST_TIMEOUT:
-                    case ResponseStatus.SC_GATEWAY_TIMEOUT:
-                        return context.getString(R.string.default_request_error_timeout);
-                    case ResponseStatus.SC_INTERNAL_SERVER_ERROR:
-                        return context.getString(R.string.default_request_error_internal_server);
-                    case ResponseStatus.SC_FORBIDDEN:
-                        return context.getString(R.string.default_request_error_forbidden_auth);
-                    case ResponseStatus.SC_BAD_GATEWAY:
-                        return context.getString(R.string.default_request_error_bad_request);
-                    case ResponseStatus.SC_BAD_REQUEST:
-                        return context.getString(R.string.default_request_error_bad_request);
-                    case ResponseStatus.SC_UNAUTHORIZED:
-                        return context.getString(R.string.msg_expired_session_or_unauthorized);
-                    default:
-                        return context.getString(R.string.default_request_error_unknown);
+                when (e.getLocalizedMessage().toInt()) {
+                    ResponseStatus.SC_REQUEST_TIMEOUT, ResponseStatus.SC_GATEWAY_TIMEOUT -> context.getString(
+                        R.string.default_request_error_timeout
+                    )
+                    ResponseStatus.SC_INTERNAL_SERVER_ERROR -> context.getString(R.string.default_request_error_internal_server)
+                    ResponseStatus.SC_FORBIDDEN -> context.getString(R.string.default_request_error_forbidden_auth)
+                    ResponseStatus.SC_BAD_GATEWAY -> context.getString(R.string.default_request_error_bad_request)
+                    ResponseStatus.SC_BAD_REQUEST -> context.getString(R.string.default_request_error_bad_request)
+                    ResponseStatus.SC_UNAUTHORIZED -> context.getString(R.string.msg_expired_session_or_unauthorized)
+                    else -> context.getString(R.string.default_request_error_unknown)
                 }
-            } catch (NumberFormatException e1) {
-                return context.getString(R.string.default_request_error_unknown);
+            } catch (e1: NumberFormatException) {
+                context.getString(R.string.default_request_error_unknown)
             }
-        } else if (e instanceof MessageErrorException && !TextUtils.isEmpty(e.getMessage())) {
-            return e.getMessage();
-        } else if (e instanceof IOException) {
-            return context.getString(R.string.default_request_error_internal_server);
+        } else if (e is MessageErrorException && !TextUtils.isEmpty(e.message)) {
+            e.message
+        } else if (e is IOException) {
+            context.getString(R.string.default_request_error_internal_server)
         } else {
-            return context.getString(R.string.default_request_error_unknown);
+            context.getString(R.string.default_request_error_unknown)
         }
     }
 
-    public static String getErrorMessageHTTP(final Context context, Throwable e) {
-        if (e instanceof MessageErrorException && !TextUtils.isEmpty(e.getMessage())) {
-            return ((MessageErrorException) e).getErrorCode();
+    fun getErrorMessageHTTP(context: Context?, e: Throwable): String {
+        return if (e is MessageErrorException && !TextUtils.isEmpty(e.message)) {
+            e.errorCode
         } else {
-            return "000";
+            "000"
         }
+    }
+
+    class Builder {
+        var sendToScalyr = true
+        var errorCode = true
+
+        fun sendToScalyr(value: Boolean): Builder {
+            sendToScalyr = value
+            return this
+        }
+
+        fun withErrorCode(value: Boolean): Builder {
+            errorCode = value
+            return this
+        }
+
+        fun build(): Builder {
+            return this
+        }
+
     }
 }
