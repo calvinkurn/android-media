5fb720cdfa65b50f2c097b037b30380166d4f86d
irfan khoirul <irfankhoirul@gmail.com>
2021-07-22 09:56:28 +0700

    unintended changes

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.tokopedia.application.MyApplication
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
-import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.applink.internal.ApplinkConstInternalTestApp
 import com.tokopedia.tkpd.helper.logout
 import com.tokopedia.tkpd.network.DataSource
@@ -110,7 +109,5 @@ class MainActivity : AppCompatActivity() {
          * RouteManager.route(this, ApplinkConstInternalMarketplace.SHOP_SETTINGS)
          * LEAVE THIS EMPTY AS DEFAULT!!
          * */
-
-        RouteManager.route(this, ApplinkConstInternalMarketplace.CART)
     }
 }
