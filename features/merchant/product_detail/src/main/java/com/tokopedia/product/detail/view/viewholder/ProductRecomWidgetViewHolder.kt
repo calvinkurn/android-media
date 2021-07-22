d2390b9e22502c5143acb4e61bbc83fe360dd310
yoasfs <yoas.susantio@tokopedia.com>
2021-07-22 20:33:35 +0700

    fix tracker and fix topads product

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductRecomWidgetViewHolder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductRecomWidgetViewHolder.kt
@@ -28,7 +28,6 @@ class ProductRecomWidgetViewHolder (
     }
     private var productRecom: ProductRecomWidgetDataModel? = null
 
-    private var componentTrackDataModel: ComponentTrackDataModel? = null
     private val recomWidget : RecommendationCarouselWidgetView = itemView.findViewById(R.id.widget_recom)
 
     override fun bind(element: ProductRecomWidgetDataModel) {
@@ -38,7 +37,6 @@ class ProductRecomWidgetViewHolder (
             recomWidget.bindTemporaryHeader(itemView.context.getString(R.string.title_other_product))
         } else {
             element.recomWidgetData?.let {
-                componentTrackDataModel = getComponentTrackData(element = element)
                 recomWidget.bind(
                         carouselData = RecommendationCarouselData(it, RecommendationCarouselData.STATE_READY),
                         adapterPosition = adapterPosition,
@@ -51,6 +49,7 @@ class ProductRecomWidgetViewHolder (
     }
 
     override fun onSeeAllBannerClicked(data: RecommendationCarouselData, applink: String) {
+        listener.onSeeAllRecomClicked(data.recommendationData.pageName, applink, getComponentTrackData(productRecom))
         listener.goToApplink(applink)
     }
 
@@ -69,7 +68,7 @@ class ProductRecomWidgetViewHolder (
                 itemPosition,
                 data.recommendationData.pageName,
                 data.recommendationData.title,
-                componentTrackDataModel?: ComponentTrackDataModel())
+                getComponentTrackData(productRecom))
     }
 
     override fun onRecomProductCardClicked(data: RecommendationCarouselData, recomItem: RecommendationItem, applink: String, itemPosition: Int, adapterPosition: Int) {
@@ -84,7 +83,7 @@ class ProductRecomWidgetViewHolder (
                 itemPosition,
                 data.recommendationData.pageName,
                 data.recommendationData.title,
-                componentTrackDataModel ?: ComponentTrackDataModel())
+                getComponentTrackData(productRecom))
 
         view.context?.run {
             RouteManager.route(this,
