266ed5a5667b30c0de121b963707612891bf031e
astidhiyaa <asti.anzaria@tokopedia.com>
2021-07-22 14:36:28 +0800

    feat: migrate hotel filter to viewBinding

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/search_map/presentation/fragment/HotelSearchFilterFragment.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/search_map/presentation/fragment/HotelSearchFilterFragment.kt
@@ -14,6 +14,7 @@ import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.cachemanager.SaveInstanceCacheManager
 import com.tokopedia.hotel.R
 import com.tokopedia.hotel.common.presentation.widget.SpanningLinearLayoutManager
+import com.tokopedia.hotel.databinding.FragmentHotelSearchFilterBinding
 import com.tokopedia.hotel.search_map.data.model.Filter
 import com.tokopedia.hotel.search_map.data.model.params.ParamFilter
 import com.tokopedia.hotel.search_map.data.util.CommonParam
@@ -21,12 +22,14 @@ import com.tokopedia.hotel.search_map.presentation.adapter.HotelSearchResultFilt
 import com.tokopedia.hotel.search_map.presentation.adapter.viewholder.SpaceItemDecoration
 import com.tokopedia.hotel.search_map.presentation.widget.HotelFilterPriceRangeSlider
 import com.tokopedia.kotlin.extensions.view.setMargin
-import kotlinx.android.synthetic.main.fragment_hotel_search_filter.*
+import com.tokopedia.utils.lifecycle.autoClearedNullable
 import kotlin.math.max
 
 class HotelSearchFilterFragment: BaseDaggerFragment() {
     var filter: Filter = Filter()
     var selectedFilter: ParamFilter = ParamFilter()
+
+    private var binding by autoClearedNullable<FragmentHotelSearchFilterBinding>()
 
     lateinit var manager: SaveInstanceCacheManager
     private val starAdapter: HotelSearchResultFilterAdapter by lazy {
@@ -40,7 +43,8 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
     override fun initInjector() {}
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_hotel_search_filter, container, false)
+        binding = FragmentHotelSearchFilterBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -57,10 +61,10 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
         setupAccomodationType(filter.accomodation)
         setupRating(filter.filterReview)
         setupPayAtHotel()
-        save_filter.setOnClickListener {
+        binding?.saveFilter?.setOnClickListener {
             selectedFilter.star = starAdapter.selectedItems.mapNotNull { it.toIntOrNull() }
             selectedFilter.propertyType = propertyTypeAdapter.selectedItems.mapNotNull { it.toIntOrNull() }
-            selectedFilter.paymentType = if (switch_pay_at_hotel.isChecked) PAYMENT_TYPE_PAY_AT_HOTEL else 0
+            selectedFilter.paymentType = if (binding?.switchPayAtHotel?.isChecked == true) PAYMENT_TYPE_PAY_AT_HOTEL else 0
             activity?.run {
                 setResult(Activity.RESULT_OK, Intent().apply {
                     val cacheManager = SaveInstanceCacheManager(this@run, true).also {
@@ -75,14 +79,14 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
             selectedFilter = ParamFilter()
             starAdapter.clearSelection()
             propertyTypeAdapter.clearSelection()
-            switch_pay_at_hotel.isChecked = false
-            price_range_input_view.initView(filter.price.minPrice, filter.price.maxPrice, filter.price.maxPrice)
-            rating_seekbar.progress = rating_seekbar.max
+            binding?.switchPayAtHotel?.isChecked = false
+            binding?.priceRangeInputView?.initView(filter.price.minPrice, filter.price.maxPrice, filter.price.maxPrice)
+            binding?.ratingSeekbar?.progress = binding?.ratingSeekbar?.max ?: 0
         }
     }
 
     private fun setupPayAtHotel() {
-        switch_pay_at_hotel.isChecked = selectedFilter.paymentType == PAYMENT_TYPE_PAY_AT_HOTEL
+        binding?.switchPayAtHotel?.isChecked = selectedFilter.paymentType == PAYMENT_TYPE_PAY_AT_HOTEL
 
         // need to hide pay at hotel filter for now
         hidePayAtHotelFilter()
@@ -91,16 +95,16 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
     private fun setupAccomodationType(accomodation: List<Filter.FilterAccomodation>) {
         val filterAccomodation = accomodation.map {
             HotelSearchResultFilterAdapter.HotelFilterItem(it.id.toString(), it.displayName, false) }
-        filter_accomodation_type.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        filter_accomodation_type.addItemDecoration(SpaceItemDecoration(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl1),
+        binding?.filterAccomodationType?.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding?.filterAccomodationType?.addItemDecoration(SpaceItemDecoration(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl1),
                 LinearLayoutManager.HORIZONTAL))
-        filter_accomodation_type.adapter = propertyTypeAdapter
+        binding?.filterAccomodationType?.adapter = propertyTypeAdapter
         propertyTypeAdapter.updateItems(filterAccomodation, selectedFilter.propertyType.map { it.toString() }.toSet())
     }
 
     private fun setupPriceFilter(price: Filter.FilterPrice) {
-        price_range_input_view.initView(selectedFilter.minPrice, selectedFilter.maxPrice, price.maxPrice)
-        price_range_input_view.onValueChangedListener = object: HotelFilterPriceRangeSlider.OnValueChangedListener{
+        binding?.priceRangeInputView?.initView(selectedFilter.minPrice, selectedFilter.maxPrice, price.maxPrice)
+        binding?.priceRangeInputView?.onValueChangedListener = object: HotelFilterPriceRangeSlider.OnValueChangedListener{
             override fun onValueChanged(startValue: Int, endValue: Int) {
                 onFilterPriceValueChangedListener(startValue, endValue)
             }
@@ -117,10 +121,10 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
         ratingStep.add(0, 0)
 
         selectedFilter.reviewScore = if (ratingStep.first() != 0 && selectedFilter.reviewScore == 0) ratingStep.first() else selectedFilter.reviewScore
-        rating_seekbar.max = ratingStep.size - 1
+        binding?.ratingSeekbar?.max = ratingStep.size - 1
 
-        if (selectedFilter.reviewScore == 0) rating_seekbar.progress = rating_seekbar.max
-        else rating_seekbar.progress = filterReview.maxReview.toInt() - max(selectedFilter.reviewScore, 5)
+        if (selectedFilter.reviewScore == 0) binding?.ratingSeekbar?.progress = binding?.ratingSeekbar?.max ?: 0
+        else binding?.ratingSeekbar?.progress = filterReview.maxReview.toInt() - max(selectedFilter.reviewScore, 5)
 
         ratingStep.forEachIndexed { index, item ->
             val stepView = LayoutInflater.from(context).inflate(R.layout.item_hotel_filter_rating_step, null)
@@ -130,17 +134,17 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
                 else -> stepView.findViewById<TextView>(R.id.title_step).text = getString(R.string.hotel_search_filter_rating_all)
             }
 
-            base_rating_step.addView(stepView)
+            binding?.baseRatingStep?.addView(stepView)
             if (index < ratingStep.size - 1){
                 val separator = View(context)
                 val lp = LinearLayout.LayoutParams(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl0),
                         ViewGroup.LayoutParams.WRAP_CONTENT, 1f)
                 separator.layoutParams = lp
-                base_rating_step.addView(separator)
+                binding?.baseRatingStep?.addView(separator)
             }
         }
 
-        rating_seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        binding?.ratingSeekbar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 if (p1 == ratingStep.size - 1) selectedFilter.reviewScore = 0
                 else selectedFilter.reviewScore = ratingStep[ratingStep.size - p1 - 1]
@@ -156,18 +160,20 @@ class HotelSearchFilterFragment: BaseDaggerFragment() {
     private fun setupStarFilter(filterStar: Filter.FilterStar) {
         val filterStars
                 = filterStar.stars.map { HotelSearchResultFilterAdapter.HotelFilterItem(it.toString(), it.toString(), true) }
-        filter_star.layoutManager = SpanningLinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,
+        binding?.filterStar?.layoutManager = SpanningLinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,
                 false, resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl1))
-        filter_star.addItemDecoration(SpaceItemDecoration(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl1),
+        binding?.filterStar?.addItemDecoration(SpaceItemDecoration(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.layout_lvl1),
                 LinearLayoutManager.HORIZONTAL))
-        filter_star.adapter = starAdapter
+        binding?.filterStar?.adapter = starAdapter
         starAdapter.updateItems(filterStars, selectedFilter.star.map { it.toString() }.toSet())
     }
 
     private fun hidePayAtHotelFilter() {
-        layout_filter_pay_at_hotel.visibility = View.GONE
-        divider_layout_pay_at_hotel.visibility = View.GONE
-        hotel_filter_rating_title.setMargin(0,0,0,0)
+        binding?.let {
+            it.layoutFilterPayAtHotel.visibility = View.GONE
+            it.dividerLayoutPayAtHotel.visibility = View.GONE
+            it.hotelFilterRatingTitle.setMargin(0,0,0,0)
+        }
     }
 
     companion object {
