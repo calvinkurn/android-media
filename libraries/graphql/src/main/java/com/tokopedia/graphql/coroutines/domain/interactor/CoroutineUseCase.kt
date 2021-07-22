23d5e8067fe1c78272253e6d6ae9ae5080108bf2
isfaaghyth <isfaaghyth@gmail.com>
2021-07-22 16:35:11 +0700

    nullable param on base use case

--- libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/CoroutineUseCase.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/CoroutineUseCase.kt
@@ -23,7 +23,7 @@ abstract class CoroutineUseCase<P, out R: Any> constructor(
     * override this to set the code to be executed
     * */
     @Throws(RuntimeException::class)
-    protected abstract suspend fun execute(params: P): R
+    protected abstract suspend fun execute(params: P?): R
 
     /*
     * Executes the use case synchronously
@@ -31,7 +31,7 @@ abstract class CoroutineUseCase<P, out R: Any> constructor(
     * @param params the input parameters to run the use case with
     * @return an generic class comes from P
     * */
-    suspend operator fun invoke(params: P): Result<R> {
+    suspend operator fun invoke(params: P? = null): Result<R> {
         return try {
             withContext(dispatcher) {
                 Success(execute(params))
@@ -43,9 +43,9 @@ abstract class CoroutineUseCase<P, out R: Any> constructor(
     }
 
     protected suspend inline fun <reified T> GraphqlRepository.request(
-        params: Map<String, Any>
+        params: Map<String, Any>?
     ): T {
-        val request = GraphqlRequest(graphqlQuery(), T::class.java, params)
+        val request = GraphqlRequest(graphqlQuery(), T::class.java, params?: mapOf())
         val response = getReseponse(listOf(request))
         return response.getSuccessData()
     }
