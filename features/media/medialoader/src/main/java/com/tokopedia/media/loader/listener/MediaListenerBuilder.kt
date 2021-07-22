12bb2a6f4d9051bf231b275a6999b2c1ce524353
isfaaghyth <isfaaghyth@gmail.com>
2021-07-22 18:11:58 +0700

    save bitmap size accumulative on glide callback listener

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/listener/MediaListenerBuilder.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/listener/MediaListenerBuilder.kt
@@ -8,6 +8,7 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.tokopedia.analytics.performance.PerformanceMonitoring
+import com.tokopedia.media.common.Loader
 import com.tokopedia.media.loader.common.Properties
 import com.tokopedia.media.loader.tracker.PerformanceTracker.postRender as trackPerformancePostRender
 import com.tokopedia.media.loader.wrapper.MediaDataSource.Companion.mapTo as dataSource
@@ -41,6 +42,9 @@ object MediaListenerBuilder {
             val pageName = context.javaClass.name.split(".").last()
             val fileSize = resource?.let { BitmapCompat.getAllocationByteCount(it).toString() }?: "0"
             val loadTime = (System.currentTimeMillis() - startTime).toString()
+
+            // save for an accumulative bitmap size in local
+            Loader.bitmapSize()?.save(fileSize.toInt())
 
             // only track if the URL from CDN service
             trackPerformancePostRender(
