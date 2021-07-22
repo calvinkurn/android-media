96db6ec98da1082c830b194d3190fe1b82b7e6b8
astidhiyaa <asti.anzaria@tokopedia.com>
2021-07-22 15:37:58 +0800

    fix: change to kotlin annotation

--- libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/di/TravelCalendarScope.kt
+++ libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/di/TravelCalendarScope.kt
@@ -1,13 +1,10 @@
 package com.tokopedia.travelcalendar.di
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
 import javax.inject.Scope
 
 /**
  * Created by nabillasabbaha on 14/05/18.
  */
 @Scope
-@Retention(RetentionPolicy.CLASS)
+@kotlin.annotation.Retention(AnnotationRetention.BINARY)
 annotation class TravelCalendarScope
