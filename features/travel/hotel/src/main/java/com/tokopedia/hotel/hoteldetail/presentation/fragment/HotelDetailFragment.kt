2d9880e333927619d21f1b74951613b2eac07ec8
astidhiyaa <asti.anzaria@tokopedia.com>
2021-07-22 10:16:03 +0800

    fix: specified import

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/hoteldetail/presentation/fragment/HotelDetailFragment.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/hoteldetail/presentation/fragment/HotelDetailFragment.kt
@@ -48,7 +48,9 @@ import com.tokopedia.hotel.hoteldetail.util.HotelShare
 import com.tokopedia.hotel.roomlist.data.model.HotelRoom
 import com.tokopedia.hotel.roomlist.presentation.activity.HotelRoomListActivity
 import com.tokopedia.imagepreviewslider.presentation.util.ImagePreviewSlider
-import com.tokopedia.kotlin.extensions.view.*
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.loadImage
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.mapviewer.activity.MapViewerActivity
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifycomponents.ticker.Ticker
