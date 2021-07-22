8faaf3a2118d567dbae90a36ae88dfdb51a68bfb
astidhiyaa <asti.anzaria@tokopedia.com>
2021-07-22 18:50:58 +0800

    fix: revert retention to java style

--- libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/di/TravelCalendarScope.kt
+++ libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/di/TravelCalendarScope.kt
@@ -1,10 +1,13 @@
 package com.tokopedia.travelcalendar.di
 
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
 import javax.inject.Scope
 
 /**
  * Created by nabillasabbaha on 14/05/18.
  */
 @Scope
-@kotlin.annotation.Retention(AnnotationRetention.BINARY)
+@Retention(RetentionPolicy.CLASS)
 annotation class TravelCalendarScope
