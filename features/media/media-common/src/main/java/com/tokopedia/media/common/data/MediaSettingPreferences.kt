e3c1c38c696d038b5462c68d5f26bdaa891b5b9f
isfaaghyth <isfaaghyth@gmail.com>
2021-07-23 12:42:07 +0700

    fixed minor bug on MediaSettingPreferences

--- features/media/media-common/src/main/java/com/tokopedia/media/common/data/MediaSettingPreferences.kt
+++ features/media/media-common/src/main/java/com/tokopedia/media/common/data/MediaSettingPreferences.kt
@@ -5,17 +5,41 @@ import android.content.SharedPreferences
 
 class MediaSettingPreferences(context: Context?) : MediaPreferences(context) {
 
-    fun qualitySettings() = getInt(KEY_QUALITY_SETTING)
+    fun qualitySettings(): Int {
+        return if (isExist(KEY_QUALITY_SETTING)) {
+            getInt(KEY_QUALITY_SETTING)
+        } else {
+            0
+        }
+    }
 
-    fun toasterVisibility() = getBoolean(KEY_MEDIA_TOASTER)
+    fun toasterVisibility(): Boolean {
+        return if (isExist(KEY_MEDIA_TOASTER)) {
+            getBoolean(KEY_MEDIA_TOASTER)
+        } else {
+            false
+        }
+    }
 
-    fun glideMigration() = getBoolean(KEY_GLIDE_CLEAR_CACHE)
+    fun glideMigration(): Boolean {
+        return if (isExist(KEY_GLIDE_CLEAR_CACHE)) {
+            getBoolean(KEY_GLIDE_CLEAR_CACHE)
+        } else {
+            false
+        }
+    }
 
-    fun setQualitySettings(value: Int) = insert(KEY_QUALITY_SETTING, value)
+    fun setQualitySettings(value: Int) {
+        insert(KEY_QUALITY_SETTING, value)
+    }
 
-    fun setToasterVisibilityFlag(value: Boolean) = insert(KEY_MEDIA_TOASTER, value)
+    fun setToasterVisibilityFlag(value: Boolean) {
+        insert(KEY_MEDIA_TOASTER, value)
+    }
 
-    fun setGlideMigration(value: Boolean) = insert(KEY_GLIDE_CLEAR_CACHE, value)
+    fun setGlideMigration(value: Boolean) {
+        insert(KEY_GLIDE_CLEAR_CACHE, value)
+    }
 
     fun getQualitySetting(index: Int): String {
         return when(index) {
