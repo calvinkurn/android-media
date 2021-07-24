1eb18fdfda870606d99a4dfa19bb5445dff9655c
isfaaghyth <isfaaghyth@gmail.com>
2021-07-25 00:36:19 +0700

    convert request helper from extension to injectable of repository by param

--- libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/UseCase.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/coroutines/domain/interactor/UseCase.kt
@@ -11,12 +11,18 @@ abstract class UseCase {
     * */
     protected abstract fun graphqlQuery(): String
 
+    /*
+    * this is helper function to request network using graphql repository
+    * */
     @Suppress("UNCHECKED_CAST")
-    protected suspend inline fun <reified T, reified P> GraphqlRepository.request(params: P): T {
+    protected suspend inline fun <reified T, reified P> request(
+        repository: GraphqlRepository,
+        params: P
+    ): T {
         val parameters = if (params is Map<*, *>) params as Map<String, Any> else mapOf()
 
         val request = GraphqlRequest(graphqlQuery(), T::class.java, parameters)
-        val response = getReseponse(listOf(request))
+        val response = repository.getReseponse(listOf(request))
 
         return response.getSuccessData()
     }
