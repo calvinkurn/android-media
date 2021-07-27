cc46ce6947e941d0c689de7dbf83419191188798
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-07-27 12:11:51 +0700

    Feat[Add Bills]: add ticker from menu detail

--- features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/fragment/SmartBillsAddTelcoFragment.kt
+++ features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/fragment/SmartBillsAddTelcoFragment.kt
@@ -5,11 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.common.topupbills.data.TopupBillsTicker
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.observe
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.smartbills.R
 import com.tokopedia.smartbills.di.SmartBillsComponent
 import com.tokopedia.smartbills.presentation.activity.SmartBillsAddTelcoActivity
 import com.tokopedia.smartbills.presentation.viewmodel.SmartBillsAddTelcoViewModel
+import com.tokopedia.smartbills.presentation.viewmodel.SmartBillsViewModel
+import com.tokopedia.unifycomponents.ticker.Ticker
+import com.tokopedia.unifycomponents.ticker.TickerCallback
+import com.tokopedia.unifycomponents.ticker.TickerData
+import com.tokopedia.unifycomponents.ticker.TickerPagerAdapter
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
 import kotlinx.android.synthetic.main.fragment_smart_bills_add_telco.*
 import javax.inject.Inject
 
@@ -38,10 +52,91 @@ class SmartBillsAddTelcoFragment: BaseDaggerFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        activity?.let {
+            val viewModelProvider = ViewModelProviders.of(it, viewModelFactory)
+            viewModel = viewModelProvider.get(SmartBillsAddTelcoViewModel::class.java)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showLoader()
+        getMenuDetailTicker()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        observeTicker()
+    }
+
+    private fun getMenuDetailTicker(){
+        viewModel.getMenuDetailAddTelco(viewModel.createMenuDetailAddTelcoParams(getMenuID()))
+    }
+
+    private fun getMenuID(): Int{
+        return menuId.toIntOrZero()
+    }
+
+    private fun observeTicker(){
+        observe(viewModel.listTicker){
+            when(it){
+                is Success -> {
+                    if (!it.data.isNullOrEmpty()){
+                        showTicker(it.data)
+                    } else {
+                        hideTicker()
+                    }
+                }
+
+                is Fail -> {
+                    hideTicker()
+                }
+            }
+            hideLoader()
+        }
+    }
+
+    private fun hideTicker(){
+        ticker_sbm_add_telco.hide()
+    }
+
+    private fun showTicker(listTicker: List<TopupBillsTicker>){
+        ticker_sbm_add_telco.show()
+        val messages = ArrayList<TickerData>()
+        for (item in listTicker) {
+            messages.add(TickerData(item.name, item.content,
+                    when (item.type) {
+                        "warning" -> Ticker.TYPE_WARNING
+                        "info" -> Ticker.TYPE_INFORMATION
+                        "success" -> Ticker.TYPE_ANNOUNCEMENT
+                        "error" -> Ticker.TYPE_ERROR
+                        else -> Ticker.TYPE_INFORMATION
+                    }, isFromHtml = true))
+        }
+        context?.run {
+            val tickerAdapter = TickerPagerAdapter(this, messages)
+            tickerAdapter.setDescriptionClickEvent(object: TickerCallback{
+                override fun onDescriptionViewClick(linkUrl: CharSequence) {
+                    RouteManager.route(context, linkUrl.toString())
+                }
+
+                override fun onDismiss() {
+
+                }
+            })
+
+            ticker_sbm_add_telco.addPagerView(tickerAdapter, messages)
+        }
+    }
+
+    private fun showLoader(){
+        loader_sbm_add_telco.show()
+        hideTicker()
+    }
+
+    private fun hideLoader(){
+        loader_sbm_add_telco.hide()
     }
 
     companion object {
--- features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/viewmodel/SmartBillsAddTelcoViewModel.kt
+++ features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/presentation/viewmodel/SmartBillsAddTelcoViewModel.kt
@@ -1,13 +1,54 @@
 package com.tokopedia.smartbills.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.common.topupbills.data.TelcoCatalogMenuDetailData
+import com.tokopedia.common.topupbills.data.TopupBillsTicker
+import com.tokopedia.common.topupbills.utils.CommonTopupBillsGqlQuery
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SmartBillsAddTelcoViewModel @Inject constructor(
         private val graphqlRepository: GraphqlRepository,
         private val dispatcher: CoroutineDispatchers): BaseViewModel(dispatcher.io){
 
+    private val mutableListTicker = MutableLiveData<Result<List<TopupBillsTicker>>>()
+    val listTicker: LiveData<Result<List<TopupBillsTicker>>>
+        get() = mutableListTicker
+
+    fun getMenuDetailAddTelco(mapParam: Map<String, Any>) {
+        launchCatchError(block = {
+            val data = withContext(dispatcher.io) {
+                val graphqlRequest = GraphqlRequest(CommonTopupBillsGqlQuery.catalogMenuDetail,
+                        TelcoCatalogMenuDetailData::class.java, mapParam)
+                graphqlRepository.getReseponse(listOf(graphqlRequest),
+                        GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build())
+            }.getSuccessData<TelcoCatalogMenuDetailData>()
+
+            mutableListTicker.postValue(Success(data.catalogMenuDetailData.tickers))
+        }) {
+            mutableListTicker.postValue(Fail(it))
+        }
+    }
+
+    fun createMenuDetailAddTelcoParams(menuId: Int): Map<String, Any> {
+        return mapOf(PARAM_MENU_ID to menuId)
+    }
+
+    companion object{
+        const val PARAM_MENU_ID = "menuID"
+        const val PARAM_CATEGORY_ID = "categoryID"
+    }
 
 }
