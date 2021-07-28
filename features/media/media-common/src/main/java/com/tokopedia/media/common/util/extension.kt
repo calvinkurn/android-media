8b284371dfef71c06eeab3c0f461bc9aa0b8ba4a
isfaaghyth <isfaaghyth@gmail.com>
2021-07-28 12:45:29 +0700

    use kotlin extension walkTopDown to get sum of dir

--- features/media/media-common/src/main/java/com/tokopedia/media/common/util/extension.kt
+++ features/media/media-common/src/main/java/com/tokopedia/media/common/util/extension.kt
@@ -5,17 +5,9 @@ package com.tokopedia.media.common.util
 import java.io.File
 
 fun getDirSize(file: File?): Long {
-    var dirSize = 0L
-
-    if (file == null) return dirSize
-
-    for (_file: File in file.listFiles()) {
-        if (_file.isDirectory) {
-            dirSize += getDirSize(_file)
-        } else if (file.isFile) {
-            dirSize += file.length()
-        }
-    }
-
-    return dirSize
+    return file?.walkTopDown()
+        ?.filter { it.isFile }
+        ?.map { it.length() }
+        ?.sum()
+        ?: 0L
 }
