e47ab399b2ea2927e9b0fbf85299545996d4e4a6
isfaaghyth <isfaaghyth@gmail.com>
2021-07-28 12:08:06 +0700

    created separate use case for stateless and with state

--- libraries/graphql/src/main/java/com/tokopedia/graphql/domain/flow/FlowStateUseCase.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/domain/flow/FlowStateUseCase.kt
@@ -1,5 +1,6 @@
-package com.tokopedia.graphql.coroutines.domain.interactor
+package com.tokopedia.graphql.domain.flow
 
+import com.tokopedia.graphql.domain.GqlUseCase
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import kotlinx.coroutines.CoroutineDispatcher
@@ -7,21 +8,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOn
 
-abstract class FlowUseCase<P, out R: Any> constructor(
+abstract class FlowStateUseCase<P, out R: Any> constructor(
     private val dispatcher: CoroutineDispatcher
-) : UseCase() {
+) : GqlUseCase<P, Flow<Result<R>>>() {
 
     /*
-    * override this to set the code to be executed
-    * */
-    @Throws(RuntimeException::class)
-    protected abstract suspend fun execute(params: P): Flow<Result<R>>
-
-    /*
-    * Executes the use case based on dispatcher's flow
+    * Executes the use case based on dispatcher's flow with state
     *
     * @param params the input parameters to run the use case with
-    * @return an generic class comes from P
+    * @return an generic class with state and flowable comes from R
     * */
     suspend operator fun invoke(params: P): Flow<Result<R>> {
         return execute(params)
