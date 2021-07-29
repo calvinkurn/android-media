277ca408570882d7d976a861e107b5fc483bb541
leonard2907 <leonard.gunawan@tokopedia.com>
2021-07-29 13:43:19 +0700

    Add full path

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/presentation/widget/ReadReviewAttachedImages.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/presentation/widget/ReadReviewAttachedImages.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.review.R
 import com.tokopedia.review.feature.reading.data.ProductReview
 import com.tokopedia.review.feature.reading.data.ProductReviewAttachments
 import com.tokopedia.review.feature.reading.presentation.adapter.ReadReviewAttachedImagesAdapter
@@ -30,7 +29,7 @@ class ReadReviewAttachedImages : BaseCustomView {
 
     private fun init() {
         View.inflate(context, com.tokopedia.review.inbox.R.layout.widget_review_attached_images, this)
-        reviewAttachedImages = findViewById(R.id.reviewAttachedImages)
+        reviewAttachedImages = findViewById(com.tokopedia.review.inbox.R.id.reviewAttachedImages)
     }
 
     fun setImages(attachedImages: List<ProductReviewAttachments>, listener: ReadReviewAttachedImagesListener, productReview: ProductReview, shopId: String) {
