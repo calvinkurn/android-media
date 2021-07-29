f90a99556e11b3f439836a3e8922139d8da29834
Henry Priyono <henry.priyono@tokopedia.com>
2021-07-29 13:18:54 +0700

    Add record mic option

--- libraries/screen_recorder/src/main/java/com/tokopedia/screenrecorder/ScreenRecorderActivity.kt
+++ libraries/screen_recorder/src/main/java/com/tokopedia/screenrecorder/ScreenRecorderActivity.kt
@@ -12,6 +12,7 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -21,15 +22,16 @@ class ScreenRecorderActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_PERMISSION_RECORD_SCREEN = 1;
-        private val PERMISSIONS = arrayOf(
+        private val PERMISSIONS = mutableSetOf(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.RECORD_AUDIO
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
 
         private const val REQUEST_MEDIA_PROJECTION = 123
         private const val REQUEST_MANAGE_OVERLAY_PERMISSION = 2323
     }
+
+    var isRecordMic = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +40,10 @@ class ScreenRecorderActivity : AppCompatActivity() {
     }
 
     private fun activateScreenRecorder() {
+        isRecordMic = findViewById<CheckBox>(R.id.checkboxRecordMic)?.isChecked ?: false
+
+        if (isRecordMic) PERMISSIONS.add(Manifest.permission.RECORD_AUDIO)
+
         var allPermissionsGranted = true
         for (permission in PERMISSIONS) {
             val granted = ActivityCompat.checkSelfPermission(
@@ -122,6 +128,7 @@ class ScreenRecorderActivity : AppCompatActivity() {
         serviceIntent
                 .putExtra(ScreenRecordService.EXTRA_MEDIA_PROEJECTION_RESULT_CODE, resultCode)
                 .putExtra(ScreenRecordService.EXTRA_MEDIA_PROEJECTION_RESULT_DATA, data)
+                .putExtra(ScreenRecordService.EXTRA_RECORD_MIC, isRecordMic)
         ContextCompat.startForegroundService(this, serviceIntent)
     }
 
