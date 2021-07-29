32d77266b1bf360bd0c7e23476d6767a8219b762
yehezkiel <yehezkiel@tokopedia.com>
2021-07-29 17:48:33 +0700

    complete ui test add custom interceptor instead of use MockInterceptor (so response can be modify in the midle of test)

--- features/merchant/product_detail/src/androidTest/java/com/tokopedia/product/detail/analytics/ProductDetailMockResponse.kt
+++ features/merchant/product_detail/src/androidTest/java/com/tokopedia/product/detail/analytics/ProductDetailMockResponse.kt
@@ -2,6 +2,7 @@ package com.tokopedia.product.detail.analytics
 
 import android.content.Context
 import com.tokopedia.instrumentation.test.R
+import com.tokopedia.product.detail.util.ResponseUtil.getJsonFromResource
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
 import com.tokopedia.test.application.util.InstrumentationMockHelper
 
@@ -9,7 +10,7 @@ class ProductDetailMockResponse: MockModelConfig() {
 
     override fun createMockModel(context: Context): MockModelConfig {
         addMockResponse(KEY_CONTAINS_PDP_GET_LAYOUT, InstrumentationMockHelper.getRawString(context, R.raw.response_mock_data_pdp_get_layout), FIND_BY_CONTAINS)
-        addMockResponse(KEY_CONTAINS_DISCUSSION_MOST_HELPFUL, InstrumentationMockHelper.getRawString(context, com.tokopedia.product.detail.test.R.raw.response_mock_discussion_most_helpful), FIND_BY_CONTAINS)
+        addMockResponse(KEY_CONTAINS_DISCUSSION_MOST_HELPFUL, getJsonFromResource("raw/response_mock_discussion_most_helpful.json"), FIND_BY_CONTAINS)
         return this
     }
 
