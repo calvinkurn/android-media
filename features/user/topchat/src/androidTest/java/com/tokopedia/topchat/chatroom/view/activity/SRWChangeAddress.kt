13b1843d48c5ba5e805257caf9a46b40071d991a
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-07-29 16:36:28 +0700

    create IT todos change address at chat

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/SRWChangeAddress.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+class SRWChangeAddress : TopchatRoomTest() {
+    // TODO: should show cta enabled state when attachment has visible cta
+    // TODO: should show cta disabled state when attachment status disabled
+    // TODO: should show attachment body text as bubble message
+    // TODO: should hide cta when attachment does not have visible cta
+    // TODO: should resend SRW when user success change address
+    // TODO: should hide msg header when attachment is null
+}
