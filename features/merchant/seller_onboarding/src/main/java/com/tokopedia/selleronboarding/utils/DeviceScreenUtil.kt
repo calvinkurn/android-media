b8e5bb2ad10d2aced1a3fde83f3e61e91be11af2
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-07-30 21:54:46 +0700

    add low end devices

--- features/merchant/seller_onboarding/src/main/java/com/tokopedia/selleronboarding/utils/DeviceScreenUtil.kt
+++ features/merchant/seller_onboarding/src/main/java/com/tokopedia/selleronboarding/utils/DeviceScreenUtil.kt
@@ -7,7 +7,7 @@ const val IMG_DEVICE_SCREEN_PERCENT = 0.45F
 
 fun View.setupMarginTitleSob(setMarginTitleSmallScreen:() -> Unit) {
     when (resources.displayMetrics.densityDpi) {
-        DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_260,
+        DisplayMetrics.DENSITY_LOW, DisplayMetrics.DENSITY_MEDIUM, DisplayMetrics.DENSITY_HIGH, DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_260,
         DisplayMetrics.DENSITY_280, DisplayMetrics.DENSITY_300, DisplayMetrics.DENSITY_340,
         DisplayMetrics.DENSITY_360 -> {
             setMarginTitleSmallScreen()
