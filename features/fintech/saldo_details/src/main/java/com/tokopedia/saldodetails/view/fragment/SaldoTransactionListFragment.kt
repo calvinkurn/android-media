cb695e74ddf03464437b210b68effa03ff410438
lalit <lalit.singh@tokopedia.com>
2021-07-30 10:28:36 +0530

    [Saldo REVAMP] Sale item view click added

--- features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/view/fragment/SaldoTransactionListFragment.kt
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/view/fragment/SaldoTransactionListFragment.kt
@@ -14,6 +14,7 @@ import com.tokopedia.saldodetails.adapter.SaldoDetailTransactionFactory
 import com.tokopedia.saldodetails.adapter.SaldoTransactionAdapter
 import com.tokopedia.saldodetails.adapter.listener.DataEndLessScrollListener
 import com.tokopedia.saldodetails.di.SaldoDetailsComponent
+import com.tokopedia.saldodetails.domain.model.SalesTransactionDetail
 import com.tokopedia.saldodetails.response.model.DepositHistoryList
 import com.tokopedia.saldodetails.view.fragment.new.*
 import com.tokopedia.saldodetails.view.viewmodel.TransactionHistoryViewModel
@@ -27,7 +28,9 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
     @Inject
     lateinit var viewModelFactory: dagger.Lazy<ViewModelProvider.Factory>
 
-    private val transactionHistoryViewModel: TransactionHistoryViewModel? by lazy(LazyThreadSafetyMode.NONE) {
+    private val transactionHistoryViewModel: TransactionHistoryViewModel? by lazy(
+        LazyThreadSafetyMode.NONE
+    ) {
         parentFragment?.let {
             val viewModelProvider = ViewModelProvider(it, viewModelFactory.get())
             viewModelProvider.get(TransactionHistoryViewModel::class.java)
@@ -40,17 +43,17 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
 
     private lateinit var transactionType: TransactionType
 
-    private val adapter : SaldoTransactionAdapter by lazy {
+    private val adapter: SaldoTransactionAdapter by lazy {
         SaldoTransactionAdapter(getAdapterTypeFactory())
     }
 
-    private var endlessRecyclerViewScrollListener : EndlessRecyclerViewScrollListener? = null
+    private var endlessRecyclerViewScrollListener: EndlessRecyclerViewScrollListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val transactionTitleStr = arguments?.getString(PARAM_TRANSACTION_TYPE, null)
         val transactionType = TransactionTypeMapper.getTransactionListType(transactionTitleStr)
-        if(transactionType != null)
+        if (transactionType != null)
             this.transactionType = transactionType
     }
 
@@ -58,21 +61,25 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
         getComponent(SaldoDetailsComponent::class.java).inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.saldo_fragment_transaction_list,
-                container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.saldo_fragment_transaction_list,
+            container, false
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if(::transactionType.isInitialized){
+        if (::transactionType.isInitialized) {
             initRecyclerView()
             initObservers()
         }
     }
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         transactionRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         transactionRecyclerView.adapter = adapter
         endlessRecyclerViewScrollListener = createEndlessRecyclerViewListener()
@@ -90,7 +97,7 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
 
     private fun initObservers() {
         transactionHistoryViewModel?.getLiveDataByTransactionType(transactionType)?.observe(
-                viewLifecycleOwner
+            viewLifecycleOwner
         ) {
             when (it) {
                 is InitialLoadingError -> {
@@ -99,7 +106,7 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
                 is LoadMoreError -> {
                     //todo try to show error...
                 }
-                LoadingMoreState-> adapter.showLoadingInAdapter()
+                LoadingMoreState -> adapter.showLoadingInAdapter()
                 InitialLoadingState -> {
                     endlessRecyclerViewScrollListener?.resetState()
                     adapter.clearAllElements()
@@ -116,11 +123,14 @@ class SaldoTransactionListFragment : BaseDaggerFragment() {
         endlessRecyclerViewScrollListener?.setHasNextPage(hasMore)
     }
 
-    private fun getAdapterTypeFactory(): SaldoDetailTransactionFactory  = SaldoDetailTransactionFactory(::openDetailPage)
+    private fun getAdapterTypeFactory(): SaldoDetailTransactionFactory =
+        SaldoDetailTransactionFactory(::openDetailPage)
 
-    private fun openDetailPage(visitable: Visitable<*>){
-        if(visitable is DepositHistoryList){
+    private fun openDetailPage(visitable: Visitable<*>) {
+        if (visitable is DepositHistoryList) {
             //todo Abhijeet
+        } else if (visitable is SalesTransactionDetail){
+            //todo Abhijeet penjualan
         }
     }
 
--- features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/viewholder/SalesSaldoTransactionViewHolder.kt
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/viewholder/SalesSaldoTransactionViewHolder.kt
@@ -3,19 +3,13 @@ package com.tokopedia.saldodetails.viewholder
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.iconunify.IconUnify
-import com.tokopedia.kotlin.extensions.view.gone
-import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.saldodetails.R
 import com.tokopedia.saldodetails.domain.model.SalesTransactionDetail
-import com.tokopedia.saldodetails.utils.SaldoDateUtil
 import com.tokopedia.saldodetails.utils.SaldoDateUtil.DATE_PATTERN_FOR_UI
 import com.tokopedia.saldodetails.utils.SaldoDateUtil.DATE_PATTERN_FROM_SERVER
-import com.tokopedia.unifycomponents.Label
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.utils.currency.CurrencyFormatUtil
 import com.tokopedia.utils.date.DateUtil
-import java.util.*
 import kotlin.math.abs
 
 class SalesSaldoTransactionViewHolder (itemView: View, val onClick: (Visitable<*>) -> Unit) :
@@ -25,9 +19,6 @@ class SalesSaldoTransactionViewHolder (itemView: View, val onClick: (Visitable<*
     private val tvSaldoAmount = itemView.findViewById<Typography>(R.id.tvSaldoAmount)
     private val tvNote = itemView.findViewById<Typography>(R.id.tvSaldoNote)
     private val tvTransactionDate = itemView.findViewById<Typography>(R.id.tvSaldoTransactionDate)
-    private val labelTransactionStatus =
-        itemView.findViewById<Label>(R.id.labelSaldoTransactionStatus)
-    private val iconNext = itemView.findViewById<IconUnify>(R.id.iconNextButton)
 
 
     override fun bind(element: SalesTransactionDetail) {
@@ -46,14 +37,11 @@ class SalesSaldoTransactionViewHolder (itemView: View, val onClick: (Visitable<*
                 CurrencyFormatUtil.convertPriceValueToIdrFormat(abs(element.amount), false)
             )
         }
-
         val transDateStr = DateUtil.formatDate(
             DATE_PATTERN_FROM_SERVER,
             DATE_PATTERN_FOR_UI,
             element.createTime,
         )
-
-        iconNext.visible()
         itemView.setOnClickListener { openDetailPage(element) }
         tvTransactionDate.text = context.resources.getString(R.string.sp_date_time_view, transDateStr)
 
