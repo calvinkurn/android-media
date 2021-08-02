ccc2f0130aa313e18a45b29dd05e2dad87bf5947
yehezkiel <yehezkiel@tokopedia.com>
2021-08-02 11:32:05 +0700

    add clear flag

--- features/merchant/atc_variant/src/main/java/com/tkpd/atcvariant/view/bottomsheet/AtcVariantBottomSheet.kt
+++ features/merchant/atc_variant/src/main/java/com/tkpd/atcvariant/view/bottomsheet/AtcVariantBottomSheet.kt
@@ -604,7 +604,7 @@ class AtcVariantBottomSheet : BottomSheetUnify(), AtcVariantListener, PartialAtc
 
     private fun goToHomePage(){
         val intent = RouteManager.getIntent(context, ApplinkConst.HOME)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
         activity?.finish()
     }
