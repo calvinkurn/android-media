b5f966b532e25e70c81b8ead062392b458272f3d
astidhiyaa <asti.anzaria@tokopedia.com>
2021-08-02 17:08:11 +0800

    fix: change to usecase

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/viewmodel/PromoCheckoutListHotelViewModel.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/viewmodel/PromoCheckoutListHotelViewModel.kt
@@ -4,28 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.GraphqlRequest
-import com.tokopedia.network.exception.MessageErrorException
-import com.tokopedia.promocheckout.common.domain.mapper.HotelCheckVoucherMapper
-import com.tokopedia.promocheckout.common.domain.model.HotelCheckVoucher
-import com.tokopedia.promocheckout.common.util.PromoQuery
+import com.tokopedia.promocheckout.list.domain.HotelCheckVoucherUseCase
 import com.tokopedia.promocheckout.common.view.uimodel.DataUiModel
-import com.tokopedia.usecase.RequestParams
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
-import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.usecase.launch_cache_error.launchCatchError
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
  * @author: astidhiyaa on 31/07/21.
  */
 class PromoCheckoutListHotelViewModel @Inject constructor(private val dispatcher: CoroutineDispatchers,
-                                                         private val graphqlRepository: GraphqlRepository,
-                                                         val checkVoucherMapper: HotelCheckVoucherMapper
+                                                          val hotelCheckVoucherUseCase: HotelCheckVoucherUseCase
 ): BaseViewModel(dispatcher.io) {
 
     private val _hotelCheckVoucherResult = MutableLiveData<Result<DataUiModel>>()
@@ -37,35 +27,13 @@ class PromoCheckoutListHotelViewModel @Inject constructor(private val dispatcher
     fun checkPromoCode(cartID: String, promoCode: String, hexColor: String){
         showLoadingPromoHotel.postValue(true)
         launchCatchError(block = {
-            val data =  withContext(dispatcher.io){
-                graphqlRepository.getReseponse(listOf(createRequest(cartID, promoCode)))
-            }.getSuccessData<HotelCheckVoucher.Response>().response
-            data.messageColor = hexColor
             showLoadingPromoHotel.postValue( false)
-            if(data.isSuccess){
-                _hotelCheckVoucherResult.postValue(Success(checkVoucherMapper.mapData(data)))
-            }else{
-                _hotelCheckVoucherResult.postValue(Fail(MessageErrorException(data.errorMessage)))
-            }
+            _hotelCheckVoucherResult.postValue(hotelCheckVoucherUseCase.
+                execute(hotelCheckVoucherUseCase.createRequestParams(promoCode,cartID), onMessageColorChange = { hexColor })
+            )
         }){
             showLoadingPromoHotel.postValue( false)
             _hotelCheckVoucherResult.postValue(Fail(it))
         }
-    }
-
-    private fun createRequest(cartID: String, promoCode: String): GraphqlRequest{
-        val requestParams = RequestParams.create()
-        requestParams.putString(PARAMNAME_VOUCHER_CODE, promoCode)
-        requestParams.putString(PARAMNAME_CART_ID, cartID)
-        val variables = mapOf(PARAMNAME_DATA to requestParams.parameters)
-        return GraphqlRequest(
-            PromoQuery.promoCheckoutHotelCheckVoucher(),
-            HotelCheckVoucher.Response::class.java, variables, false)
-    }
-
-    companion object{
-        const val PARAMNAME_CART_ID = "cartID"
-        const val PARAMNAME_VOUCHER_CODE = "voucherCode"
-        const val PARAMNAME_DATA = "data"
     }
 }
