fbd5af2d3fa073b74bae063681e71974f5bc20e9
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-02 12:52:11 +0700

    add spam like view component

---
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/SpamLikeViewComponent.kt
@@ -0,0 +1,30 @@
+package com.tokopedia.play.view.viewcomponent
+
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import com.tokopedia.play.R
+import com.tokopedia.play.animation.spamlike.PlaySpamLikeAnimation
+import com.tokopedia.play_common.viewcomponent.ViewComponent
+
+/**
+ * Created By : Jonathan Darwin on August 02, 2021
+ */
+class SpamLikeViewComponent(
+    container: ViewGroup,
+    @IdRes idRes: Int
+) : ViewComponent(container, idRes) {
+
+    private val spamLike = findViewById<PlaySpamLikeAnimation>(R.id.spam_like_animation)
+
+    init {
+        spamLike.setParentView(container)
+    }
+
+    fun shot() {
+        spamLike.shot()
+    }
+
+    interface Listener {
+
+    }
+}
