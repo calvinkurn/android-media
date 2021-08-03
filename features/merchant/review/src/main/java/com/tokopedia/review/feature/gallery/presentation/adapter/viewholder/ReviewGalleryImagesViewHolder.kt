d02344a0911081a46fa0ed313aea7dedfac6185e
leonard2907 <leonard.gunawan@tokopedia.com>
2021-08-03 13:39:46 +0700

    Change image to use imagepreview component to support zoom

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/gallery/presentation/adapter/viewholder/ReviewGalleryImagesViewHolder.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/gallery/presentation/adapter/viewholder/ReviewGalleryImagesViewHolder.kt
@@ -4,14 +4,20 @@ import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.image_gallery.ImagePreview
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.media.loader.loadImage
 import com.tokopedia.review.R
 import com.tokopedia.review.feature.gallery.presentation.listener.ReviewGalleryImageListener
-import com.tokopedia.unifycomponents.ImageUnify
 
 class ReviewGalleryImagesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    private var image: ImageUnify? = null
+    companion object {
+        const val ZOOM_SCALE_FACTOR = 2f
+        const val UNZOOM_SCALE_FACTOR = 1f
+    }
+
+    private var image: ImagePreview? = null
     private var background: ConstraintLayout? = null
 
     init {
@@ -21,24 +27,59 @@ class ReviewGalleryImagesViewHolder(view: View) : RecyclerView.ViewHolder(view) 
 
     fun bind(imageUrl: String, imageListener: ReviewGalleryImageListener) {
         image?.apply {
-            setOnClickListener {
+            mLoaderView.hide()
+            mImageView.apply {
+                image?.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        com.tokopedia.unifycomponents.R.color.Unify_N75
+                    )
+                )
+                loadImage(imageUrl) {
+                    listener(
+                        onSuccess = { _, _ ->
+                            mLoaderView.hide()
+                            this@ReviewGalleryImagesViewHolder.background?.setBackgroundColor(
+                                ContextCompat.getColor(
+                                    context,
+                                    android.R.color.transparent
+                                )
+                            )
+                        },
+                        onError = {
+                            mLoaderView.hide()
+                            imageListener.onImageLoadFailed(adapterPosition)
+                        }
+                    )
+                }
+            }
+            onImageClickListener = {
                 imageListener.onImageClicked()
             }
-            this@ReviewGalleryImagesViewHolder.background?.setBackgroundColor(ContextCompat.getColor(context, com.tokopedia.unifycomponents.R.color.Unify_N75))
-            loadImage(imageUrl) {
-                listener(
-                    onSuccess = { _, _ ->
-                        this@ReviewGalleryImagesViewHolder.background?.setBackgroundColor(
-                            ContextCompat.getColor(
-                                context,
-                                android.R.color.transparent
-                            )
-                        )
-                    },
-                    onError = {
-                        imageListener.onImageLoadFailed(adapterPosition)
+            onImageDoubleClickListener = {
+                if (mScaleFactor == UNZOOM_SCALE_FACTOR) {
+                    setScaleFactor(ZOOM_SCALE_FACTOR)
+                } else {
+                    setScaleFactor(UNZOOM_SCALE_FACTOR)
+                }
+            }
+            imagePreviewUnifyListener = object : ImagePreview.ImagePreviewUnifyListener {
+                override fun onZoomStart(scaleFactor: Float) {
+
+                }
+
+                override fun onZoom(scaleFactor: Float) {
+
+                }
+
+                override fun onZoomEnd(scaleFactor: Float) {
+                    if (scaleFactor > UNZOOM_SCALE_FACTOR) {
+                        imageListener.disableScroll()
+                    } else {
+                        imageListener.enableScroll()
+                        this@ReviewGalleryImagesViewHolder.background?.setBackgroundColor(ContextCompat.getColor(context, com.tokopedia.unifycomponents.R.color.Unify_N75))
                     }
-                )
+                }
             }
         }
     }
