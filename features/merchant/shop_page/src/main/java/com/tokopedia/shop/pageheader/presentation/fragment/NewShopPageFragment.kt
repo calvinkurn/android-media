4ce4392a4d999e25785769cfbf1f4c5669140213
shiperus <calvyn.julian@tokopedia.com>
2021-08-03 18:26:09 +0700

    process onActivityResult from MvcView on shop page

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/fragment/NewShopPageFragment.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/fragment/NewShopPageFragment.kt
@@ -50,6 +50,7 @@ import com.tokopedia.localizationchooseaddress.domain.model.LocalCacheModel
 import com.tokopedia.localizationchooseaddress.ui.widget.ChooseAddressWidget
 import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
 import com.tokopedia.mvcwidget.MvcSource
+import com.tokopedia.mvcwidget.views.MvcView
 import com.tokopedia.mvcwidget.views.activities.TransParentActivity
 import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.network.exception.UserNotLoginException
@@ -2235,5 +2236,14 @@ class NewShopPageFragment :
             scrollToTopButton?.hide()
             scrollToTopButton?.gone()
         }
+    }
+
+    fun refreshShopHomeMvcWidget() {
+        refreshData()
+//        val fragmentHomePosition = viewPagerAdapter?.getFragmentPosition(
+//                ShopPageHomeFragment::class.java
+//        ).orZero()
+//        val shopHomeFragment = viewPagerAdapter?.getRegisteredFragment(fragmentHomePosition) as? ShopPageHomeFragment
+//        shopHomeFragment?.onVoucherReloaded()
     }
 }
