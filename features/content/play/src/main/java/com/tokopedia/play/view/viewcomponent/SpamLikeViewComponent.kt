0307547d6a7a66a49e602a7aa410b30cb9bd2463
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-03 10:38:15 +0700

    add argument in shot() for specify totalShot at one time: amount

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/SpamLikeViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/SpamLikeViewComponent.kt
@@ -7,6 +7,8 @@ import androidx.lifecycle.OnLifecycleEvent
 import com.tokopedia.play.R
 import com.tokopedia.play.animation.spamlike.PlaySpamLikeAnimation
 import com.tokopedia.play_common.viewcomponent.ViewComponent
+import kotlinx.coroutines.*
+import okhttp3.Dispatcher
 
 /**
  * Created By : Jonathan Darwin on August 02, 2021
@@ -16,18 +18,27 @@ class SpamLikeViewComponent(
     @IdRes idRes: Int
 ) : ViewComponent(container, idRes) {
 
+    private val job = SupervisorJob()
     private val spamLike = findViewById<PlaySpamLikeAnimation>(R.id.spam_like_animation)
 
     init {
         spamLike.setParentView(container)
     }
 
-    fun shot() {
-        spamLike.shot()
+    fun shot(amount: Int = 1) {
+        CoroutineScope(Dispatchers.IO + job).launch {
+            for(i in 1..amount) {
+                delay(100)
+                withContext(Dispatchers.Main) {
+                    spamLike.shot()
+                }
+            }
+        }
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
+        job.cancel()
         spamLike.clear()
     }
 }
