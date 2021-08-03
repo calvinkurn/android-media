96747e8655d80fbb6df1c7216d5ed4d929729688
Ivan Fadhila <36855469+ivanfadh@users.noreply.github.com>
2021-08-04 06:23:52 +0700

    fix dagger

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/di/customproductlogistic/CustomProductLogisticModule.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/di/customproductlogistic/CustomProductLogisticModule.kt
@@ -1,7 +1,17 @@
 package com.tokopedia.editshipping.di.customproductlogistic
 
+import com.tokopedia.editshipping.di.shippingeditor.ShippingEditorScope
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import dagger.Module
+import dagger.Provides
 
 @Module
 class CustomProductLogisticModule {
+
+    @Provides
+    @ShippingEditorScope
+    fun provideGraphQlRepository(): GraphqlRepository =
+        GraphqlInteractor.getInstance().graphqlRepository
+
 }
