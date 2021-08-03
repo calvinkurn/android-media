d28dc67ad4a0d7a547b5e502ce7538ba4f4eb3fd
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-03 17:37:30 +0700

    remove logging on fragment

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
@@ -8,7 +8,6 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.Observer
@@ -694,7 +693,6 @@ class PlayUserInteractionFragment @Inject constructor(
             private var isFirstTime = true
 
             override fun onChanged(it: PlayLikeStatusInfoUiModel) {
-                Log.d("<LIKE>", it.toString())
                 val shotAmount: Long
                 if (isFirstTime) {
                     shotAmount = minOf(it.totalLike, spamLikeView.maxShot)
