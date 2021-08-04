f02c2f468923883b6c9af138d0800fdf901ebe17
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-08-04 13:14:37 +0700

    use parcelize

--- features/user/attachcommon/src/main/java/com/tokopedia/attachcommon/data/ResultProduct.kt
+++ features/user/attachcommon/src/main/java/com/tokopedia/attachcommon/data/ResultProduct.kt
@@ -1,11 +1,12 @@
 package com.tokopedia.attachcommon.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /**
  * Created by Hendri on 19/02/18.
  */
+@Parcelize
 class ResultProduct constructor(
     val productId: String = "",
     val productUrl: String = "",
@@ -14,39 +15,4 @@ class ResultProduct constructor(
     val name: String = "",
     val priceBefore: String = "",
     val dropPercentage: String = ""
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(productId)
-        parcel.writeString(productUrl)
-        parcel.writeString(productImageThumbnail)
-        parcel.writeString(price)
-        parcel.writeString(name)
-        parcel.writeString(priceBefore)
-        parcel.writeString(dropPercentage)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<ResultProduct> {
-        override fun createFromParcel(parcel: Parcel): ResultProduct {
-            return ResultProduct(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ResultProduct?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
