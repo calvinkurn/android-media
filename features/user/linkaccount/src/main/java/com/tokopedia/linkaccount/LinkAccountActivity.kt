c1c906469167085b30d636ad30896099430414eb
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-08-04 21:32:14 +0700

    base page

---
+++ features/user/linkaccount/src/main/java/com/tokopedia/linkaccount/LinkAccountActivity.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.linkaccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinkAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
