c57622b868afa60fe590891da68bd732ebcad167
hansenpw <hansen.wijaya@tokopedia.com>
2021-08-06 14:35:00 +0700

    comment param cart validation

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
@@ -96,9 +96,9 @@ class OrderSummaryPageCartProcessor @Inject constructor(private val atcOccMultiE
                     )
                 }
             }
-            if (cart.isEmpty()) {
-                return null
-            }
+//            if (cart.isEmpty()) {
+//                return null
+//            }
             var metadata = orderProfile.payment.metadata
             val selectedTerm = orderPayment.creditCard.selectedTerm
             if (selectedTerm != null) {
