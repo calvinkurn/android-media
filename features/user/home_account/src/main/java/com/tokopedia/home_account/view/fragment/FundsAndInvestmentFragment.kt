29a49b465d7ebc75667cd15d39d75fbe67ce5d7e
Ade Fulki Hadian <ade.hadian@tokopedia.com>
2021-08-06 13:34:23 +0700

    setup fragment binding

--- features/user/home_account/src/main/java/com/tokopedia/home_account/view/fragment/FundsAndInvestmentFragment.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/fragment/FundsAndInvestmentFragment.kt
@@ -7,12 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.home_account.AccountConstants
+import com.tokopedia.home_account.R
+import com.tokopedia.home_account.databinding.FundsAndInvestmentFragmentBinding
 import com.tokopedia.home_account.di.HomeAccountUserComponents
 import com.tokopedia.home_account.view.HomeAccountUserViewModel
 import com.tokopedia.home_account.view.adapter.HomeAccountFundsAndInvestmentAdapter
 import com.tokopedia.home_account.view.listener.WalletListener
+import com.tokopedia.utils.view.binding.noreflection.viewBinding
 import javax.inject.Inject
 
 class FundsAndInvestmentFragment : BaseDaggerFragment(), WalletListener {
@@ -23,6 +27,7 @@ class FundsAndInvestmentFragment : BaseDaggerFragment(), WalletListener {
     private val viewModelFragmentProvider by lazy { ViewModelProviders.of(this, viewModelFactory) }
     private val viewModel by lazy { viewModelFragmentProvider.get(HomeAccountUserViewModel::class.java) }
 
+    private val binding by viewBinding(FundsAndInvestmentFragmentBinding::bind)
     private var adapter: HomeAccountFundsAndInvestmentAdapter? = null
 
     override fun getScreenName(): String = ""
@@ -35,9 +40,11 @@ class FundsAndInvestmentFragment : BaseDaggerFragment(), WalletListener {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        adapter.getItemViewType()
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View? = inflater.inflate(R.layout.funds_and_investment_fragment, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
     }
 
     override fun onClickWallet(type: String) {
@@ -45,6 +52,11 @@ class FundsAndInvestmentFragment : BaseDaggerFragment(), WalletListener {
             AccountConstants.WALLET.OVO -> {}
             AccountConstants.WALLET.GOPAY -> {}
         }
+    }
+
+    private fun initView() {
+        binding?.fundsAndInvestmentRv?.layoutManager = LinearLayoutManager(context)
+        binding?.fundsAndInvestmentRv?.adapter = adapter
     }
 
     companion object {
