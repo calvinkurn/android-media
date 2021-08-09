d02195ad88a33d6ba187ff1024e3c295996f9c14
astidhiyaa <asti.anzaria@tokopedia.com>
2021-08-09 16:18:33 +0800

    Revert "feat: remove deprecated class"
    This reverts commit 73d451e646a2649bafd043274ee7f7b1a6ca2184.
    
    # Conflicts:
    #	features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/detail/di/PromoCheckoutDetailModule.kt

---
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/detail/domain/DetailCouponMarkeplaceModel.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.promocheckout.detail.domain
+
+import com.tokopedia.graphql.data.model.GraphqlResponse
+import com.tokopedia.promocheckout.common.domain.model.DataVoucher
+import com.tokopedia.promocheckout.detail.model.DataPromoCheckoutDetail
+
+@Deprecated("Not needed anymore.")
+data class DetailCouponMarkeplaceModel(var dataPromoCheckoutDetail: DataPromoCheckoutDetail? = null,
+                                       var dataVoucher: GraphqlResponse? = null)
--- features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/di/PromoCheckoutModule.kt
+++ features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/di/PromoCheckoutModule.kt
@@ -10,6 +10,9 @@ import com.tokopedia.promocheckout.common.domain.CancelPromoUseCase
 import com.tokopedia.promocheckout.common.domain.CheckPromoCodeUseCase
 import com.tokopedia.promocheckout.common.domain.CheckPromoStackingCodeFinalUseCase
 import com.tokopedia.promocheckout.common.domain.CheckPromoStackingCodeUseCase
+import com.tokopedia.promocheckout.common.domain.digital.DigitalCheckVoucherUseCase
+import com.tokopedia.promocheckout.common.domain.flight.FlightCancelVoucherUseCase
+import com.tokopedia.promocheckout.common.domain.flight.FlightCheckVoucherUseCase
 import com.tokopedia.promocheckout.common.domain.mapper.CheckPromoStackingCodeMapper
 import com.tokopedia.promocheckout.common.domain.umroh.UmrahCheckPromoUseCase
 import com.tokopedia.user.session.UserSession
@@ -45,6 +48,24 @@ class PromoCheckoutModule {
     @Provides
     fun provideCheckPromoStackingCodeFinalUseCase(@ApplicationContext context: Context): CheckPromoStackingCodeFinalUseCase {
         return CheckPromoStackingCodeFinalUseCase(context)
+    }
+
+    @PromoCheckoutQualifier
+    @Provides
+    fun provideCheckVoucherDigitalUseCase(@ApplicationContext context: Context): DigitalCheckVoucherUseCase {
+        return DigitalCheckVoucherUseCase(context, GraphqlUseCase())
+    }
+
+    @PromoCheckoutQualifier
+    @Provides
+    fun provideCheckVoucherFlightUseCase(): FlightCheckVoucherUseCase {
+        return FlightCheckVoucherUseCase(GraphqlUseCase())
+    }
+
+    @PromoCheckoutQualifier
+    @Provides
+    fun provideCancelVoucherDigitalUseCase(): FlightCancelVoucherUseCase {
+        return FlightCancelVoucherUseCase(GraphqlUseCase())
     }
 
     @PromoCheckoutQualifier
