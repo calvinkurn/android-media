cd9db82f6ce18f5cc900b691b3ebcd4a8c48ca6c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-10 17:28:58 +0700

    add new line

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/mapper/PlayWidgetPromoLabelMapper.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/mapper/PlayWidgetPromoLabelMapper.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 /**
  * Created by jegul on 07/04/21
  */
+
 class PlayWidgetPromoLabelMapper @Inject constructor() {
 
     fun mapWidgetPromoType(promoLabels: List<PlayWidgetPromoLabel>): PlayWidgetPromoType {
