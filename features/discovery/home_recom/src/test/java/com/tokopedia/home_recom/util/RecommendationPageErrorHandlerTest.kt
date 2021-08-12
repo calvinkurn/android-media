8d163f85237e2d1330449728bd8843faca6a02a6
errysuprayogi <erry.suprayogi+1@tokopedia.com>
2021-08-12 15:47:12 +0700

    change assert UT

--- features/discovery/home_recom/src/test/java/com/tokopedia/home_recom/util/RecommendationPageErrorHandlerTest.kt
+++ features/discovery/home_recom/src/test/java/com/tokopedia/home_recom/util/RecommendationPageErrorHandlerTest.kt
@@ -5,8 +5,7 @@ import com.tokopedia.abstraction.common.network.exception.MessageErrorException
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import java.io.IOException
@@ -51,7 +50,7 @@ class RecommendationPageErrorHandlerTest{
         val defaultError = "Koneksi timeout. Silakan coba beberapa saat lagi"
         every { context.getString(any()) } returns defaultError
         val throwable = SocketTimeoutException()
-//        assertEquals(RecommendationPageErrorHandler.getErrorMessage(context, throwable), "Koneksi timeout. Silakan coba beberapa saat lagi")
+        assertTrue(RecommendationPageErrorHandler.getErrorMessage(context, throwable).startsWith("Koneksi timeout. Silakan coba beberapa saat lagi"))
     }
 
     @Test
@@ -59,6 +58,6 @@ class RecommendationPageErrorHandlerTest{
         val defaultError = "Tidak ada koneksi internet"
         every { context.getString(any()) } returns defaultError
         val throwable = UnknownHostException()
-//        assertEquals(RecommendationPageErrorHandler.getErrorMessage(context, throwable), "Tidak ada koneksi internet")
+        assertTrue(RecommendationPageErrorHandler.getErrorMessage(context, throwable).startsWith("Tidak ada koneksi internet"))
     }
 }
