2393b2bc6a1fe01a6104e3a51059442ffc506481
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2021-08-13 10:23:23 +0700

    update viewstub

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductBundlingViewHolder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductBundlingViewHolder.kt
@@ -3,7 +3,7 @@ package com.tokopedia.product.detail.view.viewholder
 import android.content.Context
 import android.graphics.Paint
 import android.view.View
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.ViewStub
 import androidx.constraintlayout.widget.Group
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
@@ -35,76 +35,35 @@ class ProductBundlingViewHolder(
         private const val BUNDLE_ITEM_MINIMUM_COUNT_MULTIPLE = 2
     }
 
-    private val weakContext: WeakReference<Context> = WeakReference(view.context)
-
-    private val component: View = view.findViewById(R.id.product_bundling_component)
-
     private var componentTrackDataModel: ComponentTrackDataModel? = null
 
-    private val title: Typography = view.findViewById(R.id.product_bundling_title)
-    private val info: Typography = view.findViewById(R.id.product_bundling_info)
-    private val slash: Typography = view.findViewById(R.id.product_bundling_total_slash)
-    private val saving: Typography = view.findViewById(R.id.product_bundling_total_saving)
-    private val price: Typography = view.findViewById(R.id.product_bundling_total_price)
-    private val quantity: Typography = view.findViewById(R.id.product_bundling_total_quantity)
-    private val buttonCheck: UnifyButton = view.findViewById(R.id.product_bundling_button_check)
+    private val bundleViewDelegate = lazy { BundleView(view) }
+    private val bundleComponent: BundleView by bundleViewDelegate
 
-    private val singleView: View = view.findViewById(R.id.product_bundling_content_single)
-    private val singleImage: ImageUnify by lazy { view.findViewById(R.id.product_bundling_single_image) }
-    private val singleLabel: Label by lazy { view.findViewById(R.id.product_bundling_single_label) }
-    private val singleName: Typography by lazy { view.findViewById(R.id.product_bundling_single_name) }
-    private val singlePrice: Typography by lazy { view.findViewById(R.id.product_bundling_single_price) }
-    private val singleDiscount: Label by lazy { view.findViewById(R.id.product_bundling_single_discount) }
-    private val singleSlash: Typography by lazy { view.findViewById(R.id.product_bundling_single_slash) }
+    private val multiBundleDelegate = lazy { MultiBundle(view) }
+    private val multiBundle: MultiBundle by multiBundleDelegate
 
-    private val multiView = view.findViewById<View>(R.id.product_bundling_content_multi)
-    private val multiImage1: ImageUnify by lazy { view.findViewById(R.id.product_bundling_image_1) }
-    private val multiImage2: ImageUnify by lazy { view.findViewById(R.id.product_bundling_image_2) }
-    private val multiImage3: ImageUnify by lazy { view.findViewById(R.id.product_bundling_image_3) }
-    private val multiPrice1: Typography by lazy { view.findViewById(R.id.product_bundling_price_1) }
-    private val multiPrice2: Typography by lazy { view.findViewById(R.id.product_bundling_price_2) }
-    private val multiPrice3: Typography by lazy { view.findViewById(R.id.product_bundling_price_3) }
-    private val multiDiscount1: Label by lazy { view.findViewById(R.id.product_bundling_discount_1) }
-    private val multiDiscount2: Label by lazy { view.findViewById(R.id.product_bundling_discount_2) }
-    private val multiDiscount3: Label by lazy { view.findViewById(R.id.product_bundling_discount_3) }
-    private val multiSlash1: Typography by lazy { view.findViewById(R.id.product_bundling_slash_1) }
-    private val multiSlash2: Typography by lazy { view.findViewById(R.id.product_bundling_slash_2) }
-    private val multiSlash3: Typography by lazy { view.findViewById(R.id.product_bundling_slash_3) }
-    private val multiGroup1: Group by lazy { view.findViewById(R.id.product_bundling_group_1) }
-    private val multiGroup2: Group by lazy { view.findViewById(R.id.product_bundling_group_2) }
-    private val multiGroup3: Group by lazy { view.findViewById(R.id.product_bundling_group_3) }
+    private val singleBundleDelegate = lazy { SingleBundle(view) }
+    private val singleBundle: SingleBundle by singleBundleDelegate
 
     override fun bind(element: ProductBundlingDataModel) {
 
-        val bundle = element.bundleInfo ?: return showComponent(false)
+        val bundle = element.bundleInfo ?: return hideComponent()
 
         val bundleItems = bundle.bundleItems
         val bundleType = bundle.type
-        if (checkBundleItems(bundleType, bundleItems)) showComponent(true)
-        else return showComponent(false)
+        if (!checkBundleItems(bundleType, bundleItems)) return hideComponent()
 
         componentTrackDataModel = getComponentTrackData(element)
 
         val bundleId = bundle.bundleId
-        when (bundleType) {
-            BUNDLE_TYPE_SINGLE -> showSingleBundle(bundleItems.firstOrNull())
-            BUNDLE_TYPE_MULTIPLE -> showMultiBundle(bundle)
+        bundleComponent.process(bundle) {
+            listener.onClickCheckBundling(bundleId, bundleType, componentTrackDataModel)
         }
 
-        title.text = bundle.titleComponent
-        info.text = bundle.preorderString
-        slash.apply {
-            text = bundle.originalPriceBundling
-            paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-        }
-        saving.text = bundle.savingPriceBundling
-        price.text = bundle.finalPriceBundling
-        buttonCheck.setOnClickListener {
-            listener.onClickCheckBundling(
-                bundleId,
-                bundleType,
-                componentTrackDataModel
-            )
+        when (bundleType) {
+            BUNDLE_TYPE_SINGLE -> showSingleBundle(bundleItems.first())
+            BUNDLE_TYPE_MULTIPLE -> showMultiBundle(bundle, bundleId)
         }
 
         view.addOnImpressionListener(element.impressHolder) {
@@ -112,89 +71,16 @@ class ProductBundlingViewHolder(
         }
     }
 
-    private fun showSingleBundle(item: BundleInfo.BundleItem?) {
-        if (item == null) return
-
-        singleView.show()
-        multiView.hide()
-
-        singleImage.urlSrc = item.picURL
-        singleLabel.text = item.quantity
-        singleName.text = item.name
-        singlePrice.text = item.bundlePrice
-        singleDiscount.text = item.discountPercentage
-        singleSlash.apply {
-            text = item.originalPrice
-            paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-        }
-
-        quantity.hide()
+    private fun showSingleBundle(item: BundleInfo.BundleItem) {
+        if (multiBundleDelegate.isInitialized()) multiBundle.hide()
+        singleBundle.process(item)
     }
 
-    private fun showMultiBundle(bundle: BundleInfo) {
-        singleView.hide()
-        multiView.show()
-
-        val items = bundle.bundleItems
-
-        val viewImages = listOf(multiImage1, multiImage2, multiImage3)
-        val viewPrices = listOf(multiPrice1, multiPrice2, multiPrice3)
-        val viewDiscounts = listOf(multiDiscount1, multiDiscount2, multiDiscount3)
-        val viewSlashes = listOf(multiSlash1, multiSlash2, multiSlash3)
-        val viewGroups = listOf(multiGroup1, multiGroup2, multiGroup3)
-
-        val unusedGroups = viewGroups.toMutableList()
-        items.forEachIndexed { index, item ->
-            val viewImage = viewImages[index]
-            val viewPrice = viewPrices[index]
-            val viewDiscount = viewDiscounts[index]
-            val viewSlash = viewSlashes[index]
-
-            viewGroups[index].apply {
-                unusedGroups -= this
-                show()
-            }
-
-            viewImage.urlSrc = item.picURL
-
-            val discount = item.discountPercentage
-            if (discount.isBlank()) {
-                viewSlash.hide()
-                viewDiscount.hide()
-                viewPrice.text = item.originalPrice
-            } else {
-                viewDiscount.show()
-                viewSlash.show()
-                viewPrice.text = item.bundlePrice
-                viewDiscount.text = discount
-                viewSlash.apply {
-                    text = item.originalPrice
-                    paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-                }
-            }
-
-            val itemProductId = item.productId
-            if (itemProductId != bundle.productId) {
-                setItemClickListener(listOf(viewImage, viewPrice, viewDiscount, viewSlash)) {
-                    listener.onClickProductInBundling(
-                        bundle.bundleId,
-                        itemProductId,
-                        componentTrackDataModel
-                    )
-                }
-            }
+    private fun showMultiBundle(bundle: BundleInfo, bundleId: String) {
+        if (singleBundleDelegate.isInitialized()) singleBundle.hide()
+        multiBundle.process(bundle) { productId ->
+            listener.onClickProductInBundling(bundleId, productId, componentTrackDataModel)
         }
-
-        unusedGroups.forEach { group ->
-            group.invisible()
-        }
-
-        val quantityText = weakContext.get()?.getString(
-            R.string.pdp_bundling_quantity,
-            items.size.toString()
-        ) ?: items.size.toString()
-        quantity.text = quantityText
-        quantity.show()
     }
 
     private fun checkBundleItems(
@@ -213,14 +99,10 @@ class ProductBundlingViewHolder(
         return true
     }
 
-    private fun showComponent(isShow: Boolean) {
-        val params = component.layoutParams
-        params.height = if (isShow) WRAP_CONTENT else 0
-        component.layoutParams = params
-    }
-
-    private fun setItemClickListener(views: List<View>, onClick: () -> Unit) {
-        views.forEach { it.setOnClickListener { onClick() } }
+    private fun hideComponent() {
+        if (bundleViewDelegate.isInitialized()) {
+            bundleComponent.hide()
+        }
     }
 
     private fun getComponentTrackData(element: ProductBundlingDataModel) = ComponentTrackDataModel(
@@ -228,5 +110,160 @@ class ProductBundlingViewHolder(
         componentName = element.name(),
         adapterPosition = adapterPosition + 1
     )
+
+    class MultiBundle(parent: View) {
+
+        private val viewStub: ViewStub = parent.findViewById(R.id.product_bundling_stub_multi)
+        private val view: View = viewStub.inflate()
+
+        private val weakContext: WeakReference<Context> = WeakReference(view.context)
+
+        private val image1: ImageUnify = view.findViewById(R.id.product_bundling_image_1)
+        private val image2: ImageUnify = view.findViewById(R.id.product_bundling_image_2)
+        private val image3: ImageUnify = view.findViewById(R.id.product_bundling_image_3)
+        private val price1: Typography = view.findViewById(R.id.product_bundling_price_1)
+        private val price2: Typography = view.findViewById(R.id.product_bundling_price_2)
+        private val price3: Typography = view.findViewById(R.id.product_bundling_price_3)
+        private val discount1: Label = view.findViewById(R.id.product_bundling_discount_1)
+        private val discount2: Label = view.findViewById(R.id.product_bundling_discount_2)
+        private val discount3: Label = view.findViewById(R.id.product_bundling_discount_3)
+        private val slash1: Typography = view.findViewById(R.id.product_bundling_slash_1)
+        private val slash2: Typography = view.findViewById(R.id.product_bundling_slash_2)
+        private val slash3: Typography = view.findViewById(R.id.product_bundling_slash_3)
+        private val group1: Group = view.findViewById(R.id.product_bundling_group_1)
+        private val group2: Group = view.findViewById(R.id.product_bundling_group_2)
+        private val group3: Group = view.findViewById(R.id.product_bundling_group_3)
+
+        private val quantity: Typography = parent.findViewById(R.id.product_bundling_total_quantity)
+
+        private val images = listOf(image1, image2, image3)
+        private val prices = listOf(price1, price2, price3)
+        private val discounts = listOf(discount1, discount2, discount3)
+        private val slashes = listOf(slash1, slash2, slash3)
+        private val groups = listOf(group1, group2, group3)
+
+        fun process(
+            bundle: BundleInfo,
+            setOnClickItem: (productId: String) -> Unit
+        ) {
+            view.show()
+
+            val items = bundle.bundleItems
+            val unusedGroups = groups.toMutableList()
+
+            items.forEachIndexed { index, item ->
+                val viewImage = images[index]
+                val viewPrice = prices[index]
+                val viewDiscount = discounts[index]
+                val viewSlash = slashes[index]
+
+                groups[index].apply {
+                    unusedGroups -= this
+                    show()
+                }
+
+                viewImage.urlSrc = item.picURL
+
+                val discount = item.discountPercentage
+                if (discount.isBlank()) {
+                    viewSlash.hide()
+                    viewDiscount.hide()
+                    viewPrice.text = item.originalPrice
+                } else {
+                    viewDiscount.show()
+                    viewSlash.show()
+                    viewPrice.text = item.bundlePrice
+                    viewDiscount.text = discount
+                    viewSlash.apply {
+                        text = item.originalPrice
+                        paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    }
+                }
+
+                val itemProductId = item.productId
+                if (itemProductId != bundle.productId) {
+                    setItemClickListener(listOf(viewImage, viewPrice, viewDiscount, viewSlash)) {
+                        setOnClickItem(itemProductId)
+                    }
+                }
+            }
+
+            unusedGroups.forEach { group ->
+                group.invisible()
+            }
+
+            val quantityText = weakContext.get()?.getString(
+                R.string.pdp_bundling_quantity,
+                items.size.toString()
+            ) ?: items.size.toString()
+            quantity.text = quantityText
+            quantity.show()
+        }
+
+        fun hide() {
+            view.hide()
+            quantity.hide()
+        }
+
+        private fun setItemClickListener(views: List<View>, onClick: () -> Unit) {
+            views.forEach { it.setOnClickListener { onClick() } }
+        }
+    }
+
+    class SingleBundle(parent: View) {
+        private val viewStub: ViewStub = parent.findViewById(R.id.product_bundling_stub_single)
+        private val view: View = viewStub.inflate()
+
+        private val image: ImageUnify = view.findViewById(R.id.product_bundling_single_image)
+        private val label: Label = view.findViewById(R.id.product_bundling_single_label)
+        private val name: Typography = view.findViewById(R.id.product_bundling_single_name)
+        private val price: Typography = view.findViewById(R.id.product_bundling_single_price)
+        private val discount: Label = view.findViewById(R.id.product_bundling_single_discount)
+        private val slash: Typography = view.findViewById(R.id.product_bundling_single_slash)
+
+        fun process(item: BundleInfo.BundleItem) {
+            view.show()
+
+            image.urlSrc = item.picURL
+            label.text = item.quantity
+            name.text = item.name
+            price.text = item.bundlePrice
+            discount.text = item.discountPercentage
+            slash.apply {
+                text = item.originalPrice
+                paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            }
+        }
+
+        fun hide() = view.hide()
+    }
+
+    class BundleView(parent: View) {
+        private val viewStub: ViewStub = parent.findViewById(R.id.product_bundling_stub_component)
+        private val view: View = viewStub.inflate()
+
+        private val title: Typography = view.findViewById(R.id.product_bundling_title)
+        private val info: Typography = view.findViewById(R.id.product_bundling_info)
+        private val slash: Typography = view.findViewById(R.id.product_bundling_total_slash)
+        private val saving: Typography = view.findViewById(R.id.product_bundling_total_saving)
+        private val price: Typography = view.findViewById(R.id.product_bundling_total_price)
+        private val buttonCheck: UnifyButton = view.findViewById(R.id.product_bundling_button_check)
+
+        fun process(bundle: BundleInfo, onClickCheck: () -> Unit) {
+            view.show()
+
+            title.text = bundle.titleComponent
+            info.text = bundle.preorderString
+            slash.apply {
+                text = bundle.originalPriceBundling
+                paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            }
+            saving.text = bundle.savingPriceBundling
+            price.text = bundle.finalPriceBundling
+            buttonCheck.setOnClickListener { onClickCheck() }
+        }
+
+        fun hide() = view.hide()
+    }
 
 }
