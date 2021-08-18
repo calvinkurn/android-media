9de477e44cd9bc16a80fb21a822fecceacec393e
isfaaghyth <isfaaghyth@gmail.com>
2021-08-18 12:42:37 +0700

    created presenter (viewmodel) of chucker network log

---
+++ features/media/live-broadcaster/src/main/java/com/tokopedia/broadcaster/chucker/ui/viewmodel/NetworkChuckerViewModel.kt
@@ -0,0 +1,44 @@
+package com.tokopedia.broadcaster.chucker.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.broadcaster.chucker.data.mapper.mapToData
+import com.tokopedia.broadcaster.chucker.data.mapper.mapToUI
+import com.tokopedia.broadcaster.chucker.data.repository.ChuckerLogRepository
+import com.tokopedia.broadcaster.chucker.ui.uimodel.ChuckerLogUIModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class NetworkChuckerViewModel @Inject constructor(
+    private val repository: ChuckerLogRepository,
+    dispatcher: CoroutineDispatcher
+) : BaseViewModel(dispatcher) {
+
+    private var _broadcasterLog = MutableLiveData<List<ChuckerLogUIModel>>()
+    val broadcasterLog: LiveData<List<ChuckerLogUIModel>> get() = _broadcasterLog
+
+    init {
+        chucker()
+    }
+
+    fun chucker() {
+        launch {
+            val result = repository.chuckers()
+            withContext(Dispatchers.Main) {
+                _broadcasterLog.value = result.mapToUI()
+            }
+        }
+    }
+
+    fun log(model: ChuckerLogUIModel) {
+        launch {
+            repository.logChucker(model.mapToData())
+        }
+    }
+
+}
