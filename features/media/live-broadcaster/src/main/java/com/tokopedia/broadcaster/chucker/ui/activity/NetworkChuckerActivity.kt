9dee385ac71a4bfce21cf1ceddcf147c89fe30c0
isfaaghyth <isfaaghyth@gmail.com>
2021-08-18 12:43:25 +0700

    register the activity to manifest

--- features/media/live-broadcaster/src/main/java/com/tokopedia/broadcaster/chucker/ui/activity/NetworkChuckerActivity.kt
+++ features/media/live-broadcaster/src/main/java/com/tokopedia/broadcaster/chucker/ui/activity/NetworkChuckerActivity.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.broadcaster.chucker.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.broadcaster.chucker.ui.fragment.NetworkChuckerFragment
@@ -8,6 +10,12 @@ class NetworkChuckerActivity : BaseSimpleActivity() {
 
     override fun getNewFragment(): Fragment {
         return NetworkChuckerFragment()
+    }
+
+    companion object {
+        fun route(context: Context) {
+            context.startActivity(Intent(context, NetworkChuckerActivity::class.java))
+        }
     }
 
 }
--- features/media/live-broadcaster/src/main/java/com/tokopedia/broadcaster/chucker/ui/fragment/NetworkChuckerFragment.kt
+++ features/media/live-broadcaster/src/main/java/com/tokopedia/broadcaster/chucker/ui/fragment/NetworkChuckerFragment.kt
@@ -1,30 +1,76 @@
 package com.tokopedia.broadcaster.chucker.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.broadcaster.R
+import com.tokopedia.broadcaster.chucker.di.DaggerChuckerComponent
+import com.tokopedia.broadcaster.chucker.di.module.ChuckerModule
+import com.tokopedia.broadcaster.chucker.ui.uimodel.ChuckerLogUIModel
+import com.tokopedia.broadcaster.chucker.ui.viewmodel.NetworkChuckerViewModel
+import com.tokopedia.broadcaster.databinding.FragmentChuckerBroadcasterBinding
+import com.tokopedia.utils.view.binding.viewBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class NetworkChuckerFragment : BaseDaggerFragment() {
+
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    private var binding by viewBinding<FragmentChuckerBroadcasterBinding>()
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            viewModelFactory
+        ).get(NetworkChuckerViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(
-        R.layout.fragment_chucker_broadcaster,
-        container,
-        false
-    )
+    ): View = FragmentChuckerBroadcasterBinding
+        .inflate(inflater)
+        .also {
+            binding = it
+        }.root
 
-    override fun getScreenName(): String {
-        TODO("Not yet implemented")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initObservable()
+    }
+
+    private fun initObservable() {
+        viewModel.broadcasterLog.observe(viewLifecycleOwner, {
+            it.forEach { data ->
+                Log.d("BROADCASTER_LOG", data.userId)
+            }
+        })
+
+        GlobalScope.launch {
+            repeat(20) {
+                viewModel.log(ChuckerLogUIModel(
+                    userId = "abcdefg1234".random().toString()
+                ))
+            }
+        }
     }
 
     override fun initInjector() {
-        TODO("Not yet implemented")
+        DaggerChuckerComponent.builder()
+            .chuckerModule(ChuckerModule(requireContext()))
+            .build()
+            .inject(this)
+    }
+
+    override fun getScreenName() = SCREEN_NAME
+
+    companion object {
+        private const val SCREEN_NAME = "Broadcaster Chucker"
     }
 
 }
