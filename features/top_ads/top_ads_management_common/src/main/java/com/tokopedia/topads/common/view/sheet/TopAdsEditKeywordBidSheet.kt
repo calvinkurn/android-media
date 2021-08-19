540aaeeebec316a32cddec5c7ab8d26dbe8e0034
naveen goyal <naveen.goyal@tokopedia.com>
2021-08-19 15:57:16 +0530

    changes for Split bid bugs

--- features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/view/sheet/TopAdsEditKeywordBidSheet.kt
+++ features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/view/sheet/TopAdsEditKeywordBidSheet.kt
@@ -55,7 +55,9 @@ class TopAdsEditKeywordBidSheet : BottomSheetUnify() {
                         setMessageErrorField(getString(R.string.min_bid_error_new), minBid, true)
                     }
                     (result >= minBid.toDouble() && result < suggestedBid.toDouble()) -> {
-                        setMessageErrorField("", "", true)
+                        budget.setError(false)
+                        budget.setMessage("")
+                        btnSave.isEnabled = true
                         min_suggested_bid.visibility = View.VISIBLE
                     }
                     result > maxBid.toDouble() -> {
