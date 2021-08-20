00f5b65a1a97ad67b6cf359738a69fc0f2de6263
mohit-toko <mohit.singh@tokopedia.com>
2021-08-20 10:00:59 +0530

    Affiliate MVP - Added Bottom Navigation

--- features/category/affiliate_toko/src/main/java/com/tokopedia/exploreCategory/ui/activity/AffiliateActivity.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/exploreCategory/ui/activity/AffiliateActivity.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import com.tokopedia.affiliate_toko.R
 import com.tokopedia.basemvvm.viewcontrollers.BaseViewModelActivity
 import com.tokopedia.basemvvm.viewmodel.BaseViewModel
+import com.tokopedia.exploreCategory.ui.bottomnav.BottomMenu
+import com.tokopedia.exploreCategory.ui.bottomnav.IBottomClickListener
+import com.tokopedia.exploreCategory.ui.bottomnav.LottieBottomNavbar
 import com.tokopedia.exploreCategory.viewmodel.AffiliateViewModel
 
-class AffiliateActivity : BaseViewModelActivity<AffiliateViewModel>() {
+class AffiliateActivity : BaseViewModelActivity<AffiliateViewModel>() , IBottomClickListener{
 
     private lateinit var affiliateVM: AffiliateViewModel
+
+    var menu: ArrayList<BottomMenu> = ArrayList()
+    private val isNewNavigation = false
+    private var bottomNavigation: LottieBottomNavbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initBottomNavigationView()
     }
 
     override fun getLayoutRes(): Int = R.layout.affiliate_layout
@@ -28,5 +36,26 @@ class AffiliateActivity : BaseViewModelActivity<AffiliateViewModel>() {
 
     override fun getNewFragment(): Fragment? {
         return null
+    }
+
+    private fun initBottomNavigationView() {
+        bottomNavigation = findViewById(R.id.bottom_navbar)
+        populateBottomNavigationView()
+        bottomNavigation?.setMenuClickListener(this)
+    }
+
+    private fun populateBottomNavigationView(){
+        menu.add(BottomMenu(R.id.menu_home, resources.getString(R.string.home), R.raw.bottom_nav_home, R.raw.bottom_nav_home_to_enabled, R.drawable.ic_bottom_nav_home_active, R.drawable.ic_bottom_nav_home_enabled, com.tokopedia.unifyprinciples.R.color.Unify_G500, true, 1f, 3f))
+        menu.add(BottomMenu(R.id.menu_feed, resources.getString(R.string.feed), R.raw.bottom_nav_feed, R.raw.bottom_nav_feed_to_enabled, R.drawable.ic_bottom_nav_feed_active, R.drawable.ic_bottom_nav_feed_enabled, com.tokopedia.unifyprinciples.R.color.Unify_G500, true, 1f, 3f))
+        menu.add(BottomMenu(R.id.menu_os, resources.getString(R.string.official), R.raw.bottom_nav_official, R.raw.bottom_nav_os_to_enabled, R.drawable.ic_bottom_nav_os_active, R.drawable.ic_bottom_nav_os_enabled, com.tokopedia.unifyprinciples.R.color.Unify_P500, true, 1f, 3f))
+        bottomNavigation?.setMenu(menu, isNewNavigation)
+    }
+
+    override fun menuClicked(position: Int, id: Int): Boolean {
+        return true
+    }
+
+    override fun menuReselected(position: Int, id: Int) {
+
     }
 }
