8b4d171f75b67c10b2b4ee153f9f64e4bbe0f0cf
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-23 10:23:03 +0700

    add compoundview product icon

--- features/content/play/src/main/java/com/tokopedia/play/view/custom/ProductIconView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/custom/ProductIconView.kt
@@ -2,14 +2,34 @@ package com.tokopedia.play.view.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.play.R
 
 /**
  * Created By : Jonathan Darwin on August 23, 2021
  */
 class ProductIconView(context: Context, attributeSet: AttributeSet): ConstraintLayout(context, attributeSet) {
 
-    init {
+    private val tvTotal: TextView
 
+    init {
+        val view = View.inflate(context, R.layout.view_product_icon, this)
+        tvTotal = view.findViewById(R.id.tv_total)
+
+        val type = context.obtainStyledAttributes(attributeSet, R.styleable.ProductIconView)
+        setTotalProduct(type.getInt(R.styleable.ProductIconView_totalProduct, -1))
+        type.recycle()
+    }
+
+    fun setTotalProduct(total: Int) {
+        if(total != -1) {
+            tvTotal.text = total.toString()
+            tvTotal.visible()
+        }
+        else tvTotal.gone()
     }
 }
