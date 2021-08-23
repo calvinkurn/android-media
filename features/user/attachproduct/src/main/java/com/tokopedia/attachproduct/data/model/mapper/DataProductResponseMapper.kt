952f19b2e35e8ac651b58e4c9f2d370cde7303de
ramawidragama <rama.putra@tokopedia.com>
2021-08-23 14:58:23 +0700

    add image 700

--- features/user/attachproduct/src/main/java/com/tokopedia/attachproduct/data/model/mapper/DataProductResponseMapper.kt
+++ features/user/attachproduct/src/main/java/com/tokopedia/attachproduct/data/model/mapper/DataProductResponseMapper.kt
@@ -4,10 +4,9 @@ import com.tokopedia.attachproduct.data.model.NewDataProductResponse
 import com.tokopedia.attachproduct.view.uimodel.NewAttachProductItemUiModel
 
 fun NewDataProductResponse.mapToAttachUiModel(): NewAttachProductItemUiModel {
-    return NewAttachProductItemUiModel(this.productUrl ?: "",
-        this.productName ?: "", this.productId ?: ""
-        , this.productImage ?: "", this.productImage ?: ""
-        , this.productPrice ?: "", this.shop?.shopName ?: ""
-
+    return NewAttachProductItemUiModel(this.productUrl,
+        this.productName, this.productId
+        , this.productImageFull, this.productImage
+        , this.productPrice, this.shop.shopName
     )
 }
