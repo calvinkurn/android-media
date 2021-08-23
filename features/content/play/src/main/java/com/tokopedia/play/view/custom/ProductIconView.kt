cc05936818b0798645f5ebc2be3fd793773ed8a9
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-23 09:56:28 +0700

    generate working branch

---
+++ features/content/play/src/main/java/com/tokopedia/play/view/custom/ProductIconView.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.play.view.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+/**
+ * Created By : Jonathan Darwin on August 23, 2021
+ */
+class ProductIconView(context: Context, attributeSet: AttributeSet): ConstraintLayout(context, attributeSet) {
+
+    init {
+
+    }
+}
