ed2269a215d56207f3f0b23a80e4f2a7dd25abaa
isfaaghyth <isfaaghyth@gmail.com>
2021-08-23 13:25:00 +0700

    fixed by code review

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/SizeFormatExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/SizeFormatExt.kt
@@ -6,12 +6,13 @@ private const val MEGA_BYTE_SIZE = 1000000L
 
 fun Long?.formattedToMB(): String {
     if (this == 0L || this == null) return "0"
-    val sizeFormat = "%.2f"
 
-    try {
-        val sizeInMB = this.toFloat().div(MEGA_BYTE_SIZE)
-        return String.format(Locale.ENGLISH, sizeFormat, sizeInMB)
+    val sizeFormat = "%.2f"
+    val sizeInMB = this.toFloat().div(MEGA_BYTE_SIZE)
+
+    return try {
+        String.format(Locale.ENGLISH, sizeFormat, sizeInMB)
     } catch (e: Exception) {
-        return "0"
+        sizeInMB.toString()
     }
 }
