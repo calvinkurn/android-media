eacd0a8317a767665b02a78f6df5796c4c6e4885
isfaaghyth <isfaaghyth@gmail.com>
2021-08-23 11:33:53 +0700

    fixed crash on https://console.firebase.google.com/u/0/project/tkpd-tokopedia-2014/crashlytics/app/android:com.tokopedia.sellerapp/issues/6992b4ab8a7206507eda61f480d8c323?time=last-seven-days&versions=2.68%20(206206800)&sessionEventKey=612268D502DA0001421B9B134589A279_1577717659297915028

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/SizeFormatExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/SizeFormatExt.kt
@@ -4,9 +4,14 @@ import java.util.Locale
 
 private const val MEGA_BYTE_SIZE = 1000000L
 
-fun Long.formattedToMB(): String {
+fun Long?.formattedToMB(): String {
+    if (this == 0L || this == null) return "0"
     val sizeFormat = "%.2f"
 
-    val sizeInMB = this.toFloat() / MEGA_BYTE_SIZE
-    return String.format(Locale.ENGLISH, sizeFormat, sizeInMB)
+    try {
+        val sizeInMB = this.toFloat().div(MEGA_BYTE_SIZE)
+        return String.format(Locale.ENGLISH, sizeFormat, sizeInMB)
+    } catch (e: Exception) {
+        return "0"
+    }
 }
