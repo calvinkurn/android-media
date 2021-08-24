7d7236eb9684b5f4787b46e5829b03ce74580559
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2021-08-24 11:23:32 +0700

    fix error handler message for analytics

---
+++ features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/common/ProfileCompletionUtils.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.profilecompletion.common
+
+object ProfileCompletionUtils {
+
+    private const val DELIMITER_ERROR_CODE = "Kode Error:"
+    fun String.getMessageWithoutErrorCode() : String {
+        var msg = this
+        if (this.contains(DELIMITER_ERROR_CODE)) {
+            msg = this.substringBefore(DELIMITER_ERROR_CODE).trimEnd(' ')
+        }
+        return  msg
+    }
+}
