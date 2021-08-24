9d506b87cda9622567073847a3935b7f8a0e2958
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-08-24 14:21:57 +0700

    Clean up

--- libraries/graphql/src/test/java/com/tokopedia/graphql/domain/coroutine/CoroutineUseCaseTest.kt
+++ libraries/graphql/src/test/java/com/tokopedia/graphql/domain/coroutine/CoroutineUseCaseTest.kt
@@ -41,11 +41,4 @@ class CoroutineUseCaseTest {
     }
 
 
-    @Test
-    fun `a a`() {
-        dispatcher.runBlockingTest {
-            val result = repository.getReseponse(listOf())
-            print(result)
-        }
-    }
 }
