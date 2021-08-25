ad80b081439035978e76f97a5b2bc95dec01c39d
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-08-25 14:34:25 +0700

    add IT todo's TopchatRoomBroadcastCampaignLabel

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomBroadcastCampaignLabel.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+class TopchatRoomBroadcastCampaignLabel: TopchatRoomTest() {
+
+    // TODO: should show starting state when campaign status is started
+    // TODO: should show ongoing state when campaign status is started
+    // TODO: should show ended state when campaign status is started
+    // TODO: should show ended state when ongoing campaign finished countdown
+    // TODO: should show ended state when end date is invalid or has passed
+
+}
