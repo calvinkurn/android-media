ba13eb9b678b2a6b64bc9896051dddf73296466c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-08-25 09:52:27 +0700

    fixing naming variable

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/util/PlayWidgetDateFormatter.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/util/PlayWidgetDateFormatter.kt
@@ -10,30 +10,31 @@ object PlayWidgetDateFormatter {
 
     private const val COUNTRY_ID = "ID"
     private const val LANGUAGE_ID = "id"
-    private const val yyyymmddThhmmss = "yyyy-MM-dd'T'HH:mm:ss"
-    private const val ddmmmmyyyy_hhmm = "dd MMMM yyyy - HH:mm"
     private const val GMT = "GMT"
     private const val Z = "Z"
     private const val GMT07 = "+0700"
+
+    private const val yyyymmddThhmmss = "yyyy-MM-dd'T'HH:mm:ss"
+    private const val ddmmmmyyyy_hhmm = "dd MMMM yyyy - HH:mm"
 
     private val locale = Locale(LANGUAGE_ID, COUNTRY_ID)
 
     fun formatDate(
         raw: String,
-        inputFormat: String = yyyymmddThhmmss,
-        outputFormat: String = ddmmmmyyyy_hhmm
+        inputPattern: String = yyyymmddThhmmss,
+        outputPattern: String = ddmmmmyyyy_hhmm
     ): String {
         getDeviceGMT()
         return try {
-            val input = SimpleDateFormat(inputFormat, locale)
-            val output = SimpleDateFormat(outputFormat, locale)
-            val date = input.parse(raw)
+            val inputFormat = SimpleDateFormat(inputPattern, locale)
+            val outputFormat = SimpleDateFormat(outputPattern, locale)
+            val date = inputFormat.parse(raw)
 
             date?.let {
                 val diff = (getDeviceGMT().toInt() - GMT07.toInt()) / 100
                 val time = it.time + (diff * 60 * 60 * 1000)
 
-                return@let output.format(Date(time))
+                return@let outputFormat.format(Date(time))
             } ?: raw
         }
         catch (e: Exception){
