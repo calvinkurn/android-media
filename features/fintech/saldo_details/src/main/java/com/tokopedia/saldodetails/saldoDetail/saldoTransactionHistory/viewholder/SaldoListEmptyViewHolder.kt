1e580ea48bf85dc13d007442ef929388b8ba6bae
Abhijeet <abhijeet.chauhan@tokopedia.com>
2021-08-26 15:39:23 +0530

    [Saldo Revamp] Deferred Image view complete url method

--- features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/saldoDetail/saldoTransactionHistory/viewholder/SaldoListEmptyViewHolder.kt
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/saldoDetail/saldoTransactionHistory/viewholder/SaldoListEmptyViewHolder.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.saldodetails.saldoDetail.saldoTransactionHistory.viewholder
 
 import android.view.View
+import com.tkpd.remoteresourcerequest.view.DeferredImageView
+import com.tkpd.remoteresourcerequest.view.ImageDensityType
 
 import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
@@ -14,9 +16,11 @@ class SaldoListEmptyViewHolder(itemView: View, private val isSalesTab: Boolean =
 
     private val emptyViewTitle: Typography = itemView.findViewById(R.id.text_view_empty_title_text)
     private val emptyViewDescription: Typography = itemView.findViewById(R.id.text_view_empty_content_text)
-
+    private val deferredImageView: DeferredImageView = itemView.findViewById(R.id.no_result_image)
 
     override fun bind(element: EmptyModel) {
+        deferredImageView.loadRemoteImageDrawable(EMPTY_IMAGE,  ImageDensityType.SUPPORT_SINGLE_DPI)
+
         if (isSalesTab) {
             emptyViewTitle.text = getString(R.string.saldo_empty_sales_tab_title)
             emptyViewDescription.text = getString(R.string.saldo_empty_sales_tab_desc)
@@ -29,5 +33,6 @@ class SaldoListEmptyViewHolder(itemView: View, private val isSalesTab: Boolean =
 
     companion object {
         val LAYOUT = R.layout.saldo_transaction_list_empty
+        const val EMPTY_IMAGE = "saldo_img_empty_list"
     }
 }
