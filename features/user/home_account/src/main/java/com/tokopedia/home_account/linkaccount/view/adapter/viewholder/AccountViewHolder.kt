24bfbbe143ea6fbe84984c45dc32ca2a4f5729e2
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-08-26 07:06:50 +0700

    fix layout

--- features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/adapter/viewholder/AccountViewHolder.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/adapter/viewholder/AccountViewHolder.kt
@@ -26,7 +26,6 @@ open class AccountViewHolder(
         binding?.itemAccountPartnerName?.text = item.partnerName
         if(item.isLinked) {
             binding?.itemAccountLinkBtn?.hide()
-            binding?.itemAccountViewBtn?.show()
             binding?.itemAccountLinkedDate?.show()
             binding?.itemAccountContainer?.setOnClickListener { listener.onViewAccountClicked() }
             binding?.itemAccountLinkedDate?.text = item.linkDate
@@ -34,7 +33,6 @@ open class AccountViewHolder(
         else  {
             binding?.itemAccountLinkedDate?.hide()
             binding?.itemAccountLinkBtn?.show()
-            binding?.itemAccountViewBtn?.hide()
             binding?.itemAccountContainer?.setOnClickListener { listener.onLinkAccountClicked() }
         }
 
