7af6487b6d559dd9ba6bac0dade6f96efca0a919
shiperus <calvyn.julian@tokopedia.com>
2021-08-27 14:50:11 +0700

    change module name resource id

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -361,7 +361,7 @@ object DeeplinkDFMapper : CoroutineScope {
             add(DFP({
                 it.startsWithPattern(ApplinkConstInternalMarketplace.SHOP_REVIEW) ||
                         it.startsWithPattern(ApplinkConstInternalMarketplace.PRODUCT_REVIEW)
-            }, DF_MERCHANT_REVIEW, R.string.title_review_inbox))
+            }, DF_MERCHANT_REVIEW, R.string.title_product_review))
 
             add(DFP({
                 it.startsWith(ApplinkConstInternalMarketplace.PRODUCT_DETAIL) ||
