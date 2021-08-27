7c95a2726883a58291ec1ebe4c8e85198154de9c
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-08-27 09:51:02 +0700

    Add toJsonMap

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/cassava/AnalyticsMapParser.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/cassava/AnalyticsMapParser.kt
@@ -2,7 +2,7 @@ package com.tokopedia.analyticsdebugger.cassava
 
 import com.google.gson.*
 import com.google.gson.reflect.TypeToken
-import java.lang.ArithmeticException
+import com.tokopedia.analyticsdebugger.cassava.validator.core.JsonMap
 import java.lang.reflect.Type
 import java.math.BigDecimal
 import java.net.URLDecoder
@@ -10,28 +10,42 @@ import java.net.URLDecoder
 class AnalyticsMapParser {
 
     private val gson: Gson = GsonBuilder()
-        .registerTypeAdapter(object : TypeToken<Double>() {}.type, object : JsonSerializer<Double> {
-            override fun serialize(
-                src: Double?,
-                typeOfSrc: Type?,
-                context: JsonSerializationContext?
-            ): JsonElement {
-                var bigDecimal = BigDecimal.valueOf(src ?: 0.0)
-                try {
-                    bigDecimal = BigDecimal(bigDecimal.toBigIntegerExact())
-                } catch (e: ArithmeticException) {
-                    // ignore
-                }
-                return JsonPrimitive(bigDecimal)
-            }
-        }).disableHtmlEscaping().setPrettyPrinting().create()
+        .registerTypeAdapter(object : TypeToken<Double>() {}.type, BigDoubleAdapter())
+        .disableHtmlEscaping()
+        .setPrettyPrinting()
+        .create()
 
-    fun parse(data: Map<String, Any>): String {
+    fun parse(data: JsonMap): String {
         return URLDecoder.decode(
             gson.toJson(data)
                 .replace("%(?![0-9a-fA-F]{2})".toRegex(), "%25")
                 .replace("\\+".toRegex(), "%2B"), "UTF-8"
         )
+    }
+
+    fun toJsonMap(str: String): JsonMap {
+        val jsonType = object : TypeToken<Map<String, Any>>() {}.type
+        return try {
+            gson.fromJson(str, jsonType)
+        } catch (e: JsonSyntaxException) {
+            emptyMap()
+        }
+    }
+
+    class BigDoubleAdapter : JsonSerializer<Double> {
+        override fun serialize(
+            src: Double?,
+            typeOfSrc: Type?,
+            context: JsonSerializationContext?
+        ): JsonElement {
+            var bigDecimal = BigDecimal.valueOf(src ?: 0.0)
+            try {
+                bigDecimal = BigDecimal(bigDecimal.toBigIntegerExact())
+            } catch (e: ArithmeticException) {
+                // ignore
+            }
+            return JsonPrimitive(bigDecimal)
+        }
     }
 
 }
--- libraries/analytics_debugger/src/test/java/com/tokopedia/analyticsdebugger/cassava/AnalyticsMapParserTest.kt
+++ libraries/analytics_debugger/src/test/java/com/tokopedia/analyticsdebugger/cassava/AnalyticsMapParserTest.kt
@@ -1,19 +1,50 @@
 package com.tokopedia.analyticsdebugger.cassava
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.math.BigDecimal
 
 class AnalyticsMapParserTest {
 
+    private val uut = AnalyticsMapParser()
+
     @Test
-    fun `parse test`() {
+    fun `given double when parsed should return json primitive`() {
+        val case = mapOf<String, Any>("doubleVal" to 25.34)
+        val expected = """
+            {
+              "doubleVal": 25.34
+            }
+        """.trimIndent()
+        val actual = uut.parse(case)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `given big double when parsed should return exact json primitive`() {
         val priceTest = 2000000000.0
-        println("toString:\n${BigDecimal.valueOf(priceTest).toPlainString()}")
+        val expected = """
+            {
+              "price": 2000000000
+            }
+        """.trimIndent()
         val case = mapOf<String, Any>(
             "price" to priceTest
         )
-        val uut = AnalyticsMapParser()
         val actual = uut.parse(case)
-        println("Parsed:\n$actual")
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `given json string when called toJsonMap converts to map object`() {
+        val case = """
+            {
+              "doubleVal": 25.34
+            }
+        """.trimIndent()
+
+        val actual = uut.toJsonMap(case)
+        val expected = mapOf<String, Any>("doubleVal" to 25.34)
+
+        assertEquals(expected, actual)
     }
 }
