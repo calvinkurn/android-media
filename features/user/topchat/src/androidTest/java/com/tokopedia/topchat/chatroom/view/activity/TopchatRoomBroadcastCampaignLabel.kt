f4c2d204483503994ded1c338bfc54e29b169683
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-08-27 10:25:11 +0700

    fix swapped assertion

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomBroadcastCampaignLabel.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomBroadcastCampaignLabel.kt
@@ -23,12 +23,11 @@ class TopchatRoomBroadcastCampaignLabel : TopchatRoomTest() {
                 R.string.desc_topchat_broadcast_campaign_started
             )
         )
-        assertBroadcastCampaignLabelCountdownAt(1, isDisplayed())
-        assertBroadcastCampaignLabelStartDateIconAt(1, not(isDisplayed()))
-        assertBroadcastCampaignLabelStartDateTextAt(1, not(isDisplayed()))
+        assertBroadcastCampaignLabelCountdownAt(1, not(isDisplayed()))
+        assertBroadcastCampaignLabelStartDateIconAt(1, isDisplayed())
+        assertBroadcastCampaignLabelStartDateTextAt(1, isDisplayed())
     }
 
-    // TODO: should show ongoing state when campaign status is started
     // TODO: should show ended state when campaign status is started
     // TODO: should show ended state when ongoing campaign finished countdown
     // TODO: should show ended state when end date is invalid or has passed
