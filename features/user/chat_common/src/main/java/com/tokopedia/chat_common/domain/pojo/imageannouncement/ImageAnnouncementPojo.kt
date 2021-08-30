d515da52b685fab16fed4efdac073fe3f9c5d975
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-08-30 15:23:52 +0700

    remove expose

--- features/user/chat_common/src/main/java/com/tokopedia/chat_common/domain/pojo/imageannouncement/ImageAnnouncementPojo.kt
+++ features/user/chat_common/src/main/java/com/tokopedia/chat_common/domain/pojo/imageannouncement/ImageAnnouncementPojo.kt
@@ -10,28 +10,19 @@ import com.google.gson.annotations.SerializedName
 class ImageAnnouncementPojo {
 
     @SerializedName("image_url")
-    @Expose
     var imageUrl: String = ""
     @SerializedName("url")
-    @Expose
     var url: String = ""
     @SerializedName("is_hide_banner")
-    @Expose
     var isHideBanner: Boolean = false
-
     @SerializedName("wording_end_state")
-    @Expose
     var endStateWording: String = ""
     @SerializedName("is_campaign")
-    @Expose
     var isCampaign: Boolean = false
     @SerializedName("status_campaign")
-    @Expose
     var statusCampaign: Int = 1
     @SerializedName("start_date")
-    @Expose
     var startDate: String = ""
     @SerializedName("end_date")
-    @Expose
     var endDate: String = ""
 }
