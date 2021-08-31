8266de77f919b779af8f978515203290b085d88a
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-08-31 17:28:48 +0700

    refactor UploadImageChatServiceStub

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatroom/view/service/UploadImageChatServiceStub.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatroom/view/service/UploadImageChatServiceStub.kt
@@ -2,23 +2,15 @@ package com.tokopedia.topchat.stub.chatroom.view.service
 
 import android.content.Context
 import android.content.Intent
-import com.tokopedia.chat_common.domain.pojo.ChatReplyPojo
-import com.tokopedia.topchat.AndroidFileUtil
 import com.tokopedia.topchat.chatroom.data.ImageUploadServiceModel
 import com.tokopedia.topchat.chatroom.service.UploadImageChatService
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
-import com.tokopedia.topchat.stub.chatroom.usecase.ReplyChatGQLUseCaseStub
 
 class UploadImageChatServiceStub : UploadImageChatService() {
+
     override fun inject() {
         TopchatRoomTest.chatComponentStub?.inject(this)
-        (replyChatGQLUseCase as ReplyChatGQLUseCaseStub).response = uploadImageReplyResponse
     }
-
-    private var uploadImageReplyResponse: ChatReplyPojo = AndroidFileUtil.parse(
-            "success_upload_image_reply.json",
-            ChatReplyPojo::class.java
-    )
 
     companion object {
         var dummyMap = UploadImageChatService.dummyMap
