cfbcc7b9bc193ddd4ec7f49fde49acadb38eca9b
Zishanr <zishan.rashid@tokopedia.com>
2021-09-01 13:18:14 +0530

    Handle auto scroll for empty screen

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewmodel/DiscoveryViewModel.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewmodel/DiscoveryViewModel.kt
@@ -61,7 +61,6 @@ class DiscoveryViewModel @Inject constructor(private val discoveryDataUseCase: D
     private val discoveryFabLiveData = MutableLiveData<Result<ComponentsItem>>()
     private val discoveryResponseList = MutableLiveData<Result<List<ComponentsItem>>>()
     private val discoveryLiveStateData = MutableLiveData<DiscoveryLiveState>()
-    private val wishlistUpdateLiveData = MutableLiveData<ProductCardOptionsModel>()
     private val discoveryBottomNavLiveData = MutableLiveData<Result<ComponentsItem>>()
     var pageIdentifier: String = ""
     var pageType: String = ""
@@ -273,7 +272,7 @@ class DiscoveryViewModel @Inject constructor(private val discoveryDataUseCase: D
     }
 
     fun getScrollDepth(offset: Int, extent: Int, range: Int): Int {
-        return SCROLL_DEPTH * (offset + extent) / range
+        return if(range > 0) SCROLL_DEPTH * (offset + extent) / range else 0
     }
 
     fun getShareUTM(data:PageInfo) : String{
