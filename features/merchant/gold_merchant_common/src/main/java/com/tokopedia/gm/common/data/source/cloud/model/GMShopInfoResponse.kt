df4ebfe0c57701e03a71b08859afd2239dd64391
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-09-01 11:21:11 +0700

    add expose

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/cloud/model/GMShopInfoResponse.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/cloud/model/GMShopInfoResponse.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.gm.common.data.source.cloud.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -14,6 +15,7 @@ data class GMShopInfoResponse(
 data class GoldGetPMShopInfoDataModel(
         @SerializedName("is_new_seller")
         val isNewSeller: Boolean? = true,
+        @Expose
         @SerializedName("is_30_days_first_monday")
         val is30DaysFirstMonday: Boolean? = false,
         @SerializedName("shop_age")
