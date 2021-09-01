ce7c1290ffd09e1d0550d6eba9ed05118212678f
leonard2907 <leonard.gunawan@tokopedia.com>
2021-09-01 13:45:57 +0700

    Fix productId tracker positon

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/analytics/ReadReviewTracking.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/reading/analytics/ReadReviewTracking.kt
@@ -42,6 +42,7 @@ object ReadReviewTracking {
                     countRating,
                     countReview
                 ),
+                ReadReviewTrackingConstants.KEY_PRODUCT_ID to productId,
                 ReviewTrackingConstant.EVENT_CATEGORY to ReadReviewTrackingConstants.EVENT_CATEGORY,
                 ReadReviewTrackingConstants.KEY_USER_ID to userId,
                 ReadReviewTrackingConstants.KEY_BUSINESS_UNIT to ReadReviewTrackingConstants.BUSINESS_UNIT,
@@ -58,7 +59,6 @@ object ReadReviewTracking {
                                     imageCount
                                 ),
                                 ReadReviewTrackingConstants.KEY_POSITION to position.toString(),
-                                ReadReviewTrackingConstants.KEY_PRODUCT_ID to productId
                             )
                         )
                     )
