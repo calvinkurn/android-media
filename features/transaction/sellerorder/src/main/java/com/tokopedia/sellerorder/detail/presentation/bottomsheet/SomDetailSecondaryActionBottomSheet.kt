39900a8c712297fc9549b9c40178dceeb0dac5dc
elscione <yusufhendrawan06@gmail.com>
2021-09-01 17:29:10 +0700

    Make request cancel bottomsheets in SOM scrollable

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/detail/presentation/bottomsheet/SomDetailSecondaryActionBottomSheet.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/detail/presentation/bottomsheet/SomDetailSecondaryActionBottomSheet.kt
@@ -2,10 +2,9 @@ package com.tokopedia.sellerorder.detail.presentation.bottomsheet
 
 import android.content.Context
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.tokopedia.kotlin.extensions.view.gone
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.sellerorder.R
 import com.tokopedia.sellerorder.common.presenter.SomBottomSheet
-import kotlinx.android.synthetic.main.bottomsheet_secondary.view.*
 
 class SomDetailSecondaryActionBottomSheet(
         context: Context,
@@ -13,20 +12,16 @@ class SomDetailSecondaryActionBottomSheet(
 ) : SomBottomSheet(LAYOUT, true, true, false, context.getString(R.string.som_detail_other_bottomsheet_title), context, true) {
 
     companion object {
-        private val LAYOUT = R.layout.bottomsheet_secondary
+        private val LAYOUT = R.layout.bottomsheet_secondary_action_list
     }
 
     private var secondaryActionAdapter: SomBottomSheetRejectOrderAdapter? = null
 
     override fun setupChildView() {
         secondaryActionAdapter = SomBottomSheetRejectOrderAdapter(listener, false)
-        childViews?.run {
-            rv_bottomsheet_secondary?.apply {
-                layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
-                adapter = secondaryActionAdapter
-            }
-            fl_btn_primary?.gone()
-            tf_extra_notes?.gone()
+        childViews?.findViewById<RecyclerView>(R.id.rvBottomSheetActionList)?.run {
+            layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+            adapter = secondaryActionAdapter
         }
     }
 
