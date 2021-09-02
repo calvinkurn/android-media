af832b31b4df73d550c4abbf76a8633782f970ad
dhaba.widhikari <wikodhawi@gmail.com>
2021-09-02 20:44:22 +0700

    on progress view all

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/productcardgridcarousel/viewHolder/CarouselViewAllCardViewHolder.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/productcardgridcarousel/viewHolder/CarouselViewAllCardViewHolder.kt
@@ -2,11 +2,12 @@ package com.tokopedia.home_component.productcardgridcarousel.viewHolder
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.home_component.R
 import com.tokopedia.home_component.model.ChannelModel
 import com.tokopedia.home_component.productcardgridcarousel.dataModel.CarouselViewAllCardDataModel
-import com.tokopedia.media.loader.loadImageWithoutPlaceholder
 
 /**
  * created by Dhaba
@@ -16,13 +17,13 @@ class CarouselViewAllCardViewHolder(view: View,
 )
     : AbstractViewHolder<CarouselViewAllCardDataModel>(view){
 
-    private val container: View by lazy { view.findViewById<View>(R.id.container_banner_mix_more) }
-    private val bannerBackgroundImage: ImageView by lazy { view.findViewById<ImageView>(R.id.background_banner_mix_more)}
+    private val container: View by lazy { view.findViewById<ConstraintLayout>(R.id.container_banner_view_all) }
+    private val ivHeadingViewAll: ImageView by lazy { view.findViewById<ImageView>(R.id.iv_chevron_down_view_all) }
+    private val tvDcMixViewAll: TextView by lazy { view.findViewById<TextView>(R.id.tv_dc_mix_view_all) }
+    private val tvTitleViewAll: TextView by lazy { view.findViewById<TextView>(R.id.tv_title_view_all) }
+    private val tvDescriptionViewAll: TextView by lazy { view.findViewById<TextView>(R.id.tv_description_view_all) }
 
     override fun bind(element: CarouselViewAllCardDataModel) {
-        bannerBackgroundImage.setOnClickListener {
-            element.listener.onSeeMoreCardClicked(applink = element.applink, channel = channels)
-        }
         container.setOnClickListener {
             element.listener.onSeeMoreCardClicked(applink = element.applink, channel = channels)
         }
