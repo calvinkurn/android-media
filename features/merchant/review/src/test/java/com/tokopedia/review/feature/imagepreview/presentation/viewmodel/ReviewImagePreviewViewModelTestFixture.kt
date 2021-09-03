1fe0bebc30ae35624c7cdfc61605f98ee970cf44
leonard2907 <leonard.gunawan@tokopedia.com>
2021-09-03 09:11:02 +0700

    Fix unit test

--- features/merchant/review/src/test/java/com/tokopedia/review/feature/imagepreview/presentation/viewmodel/ReviewImagePreviewViewModelTestFixture.kt
+++ features/merchant/review/src/test/java/com/tokopedia/review/feature/imagepreview/presentation/viewmodel/ReviewImagePreviewViewModelTestFixture.kt
@@ -4,6 +4,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.review.common.domain.usecase.ToggleLikeReviewUseCase
 import com.tokopedia.review.feature.gallery.domain.usecase.GetReviewImagesUseCase
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Before
@@ -17,6 +18,9 @@ abstract class ReviewImagePreviewViewModelTestFixture {
     @RelaxedMockK
     lateinit var getReviewImagesUseCase: GetReviewImagesUseCase
 
+    @RelaxedMockK
+    lateinit var userSessionInterface: UserSessionInterface
+
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
@@ -25,7 +29,7 @@ abstract class ReviewImagePreviewViewModelTestFixture {
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        viewModel = ReviewImagePreviewViewModel(toggleLikeReviewUseCase, getReviewImagesUseCase, CoroutineTestDispatchersProvider)
+        viewModel = ReviewImagePreviewViewModel(toggleLikeReviewUseCase, getReviewImagesUseCase, userSessionInterface, CoroutineTestDispatchersProvider)
         viewModel.reviewImages.observeForever {  }
     }
 }
