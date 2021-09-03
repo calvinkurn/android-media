c989e65fd7966f0d3f91cf69fbb021033d5bf092
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-09-03 17:08:09 +0700

    Get troubleshoot adjustment

--- features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/data/domain/GetTroubleshootStatusUseCase.kt
+++ features/user/notif-troubleshooter/src/main/java/com/tokopedia/troubleshooter/notification/data/domain/GetTroubleshootStatusUseCase.kt
@@ -1,22 +1,23 @@
 package com.tokopedia.troubleshooter.notification.data.domain
 
+import com.tokopedia.graphql.coroutines.data.extensions.request
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.troubleshooter.notification.data.entity.NotificationTroubleshoot
 import kotlinx.coroutines.CoroutineDispatcher
 
 open class GetTroubleshootStatusUseCase constructor(
-        repository: GraphqlRepository,
+        private val repository: GraphqlRepository,
         private val graphQuery: String,
         dispatcher: CoroutineDispatcher
-) : CoroutineUseCase<Unit, NotificationTroubleshoot>(repository, dispatcher) {
+) : CoroutineUseCase<Unit, NotificationTroubleshoot>(dispatcher) {
 
     override fun graphqlQuery(): String {
         return graphQuery
     }
 
     override suspend fun execute(params: Unit): NotificationTroubleshoot {
-        return request(emptyMap())
+        return repository.request(graphqlQuery(), params)
     }
 
 }
