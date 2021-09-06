dc13c1a6ea693e020fee717ab2fabc30db5a42e3
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-06 14:53:07 +0700

    remove unnused import

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUpcomingFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUpcomingFragment.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.ViewModelProvider
