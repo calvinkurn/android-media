33bc1f0731e77509356ce394ddca49423ffef7c0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-06 13:21:38 +0700

    add toolbar

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUpcomingFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUpcomingFragment.kt
@@ -1,11 +1,15 @@
 package com.tokopedia.play.view.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.tokopedia.abstraction.base.view.fragment.TkpdBaseV4Fragment
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
@@ -13,8 +17,15 @@ import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.play.R
 import com.tokopedia.play.analytic.PlayAnalytic
+import com.tokopedia.play.util.withCache
+import com.tokopedia.play.view.uimodel.recom.PlayPartnerFollowStatus
+import com.tokopedia.play.view.uimodel.state.PlayViewerNewUiState
+import com.tokopedia.play.view.viewcomponent.ToolbarViewComponent
 import com.tokopedia.play.view.viewmodel.PlayViewModel
+import com.tokopedia.play_common.viewcomponent.viewComponent
+import com.tokopedia.unifycomponents.Toaster
 import com.tokopedia.unifycomponents.UnifyButton
+import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
 
 /**
@@ -24,8 +35,11 @@ class PlayUpcomingFragment @Inject constructor(
     private val viewModelFactory: ViewModelProvider.Factory,
     private val dispatchers: CoroutineDispatchers,
     private val analytic: PlayAnalytic
-): TkpdBaseV4Fragment() {
+): TkpdBaseV4Fragment(),
+    ToolbarViewComponent.Listener
+{
 
+    private val toolbarView by viewComponent { ToolbarViewComponent(it, R.id.view_toolbar, this) }
     private lateinit var ivUpcomingCover: AppCompatImageView
     private lateinit var btnAction: UnifyButton
 
@@ -50,6 +64,7 @@ class PlayUpcomingFragment @Inject constructor(
         super.onViewCreated(view, savedInstanceState)
         initView(view)
         setupView(view)
+        setupObserver()
     }
 
     private fun initView(view: View) {
@@ -60,6 +75,7 @@ class PlayUpcomingFragment @Inject constructor(
     private fun setupView(view: View) {
         setCover(view)
         setButtonAction()
+        setToolbar()
     }
 
     private fun setCover(view: View) {
@@ -84,5 +100,75 @@ class PlayUpcomingFragment @Inject constructor(
         btnAction.setOnClickListener {
             btnAction.isLoading = true
         }
+    }
+
+    private fun setToolbar() {
+        toolbarView.setIsShareable(true)
+    }
+
+    private fun setupObserver() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            playViewModel.uiState.withCache().collectLatest { cachedState ->
+                val state = cachedState.value
+                renderToolbarView(state.followStatus, state.partnerName)
+            }
+        }
+    }
+
+    private fun renderToolbarView(
+        followStatus: PlayPartnerFollowStatus,
+        partnerName: String
+    ) {
+        toolbarView.setFollowStatus(followStatus)
+        toolbarView.setPartnerName(partnerName)
+    }
+
+    override fun onBackButtonClicked(view: ToolbarViewComponent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMoreButtonClicked(view: ToolbarViewComponent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFollowButtonClicked(view: ToolbarViewComponent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPartnerNameClicked(view: ToolbarViewComponent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCartButtonClicked(view: ToolbarViewComponent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCopyButtonClicked(view: ToolbarViewComponent, content: String) {
+        copyToClipboard(content)
+        showLinkCopiedToaster()
+
+        analytic.clickCopyLink()
+    }
+
+    private fun copyToClipboard(content: String) {
+        (requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager)
+            .setPrimaryClip(ClipData.newPlainText("play-room", content))
+    }
+
+    private fun showLinkCopiedToaster() {
+        doShowToaster(message = getString(R.string.play_link_copied))
+    }
+
+    private fun doShowToaster(
+        toasterType: Int = Toaster.TYPE_NORMAL,
+        actionText: String = "",
+        message: String,
+    ) {
+        Toaster.build(
+            requireView(),
+            message,
+            type = toasterType,
+            actionText = actionText
+        ).show()
     }
 }
