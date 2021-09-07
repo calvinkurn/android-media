1e70f6a4dae93a8aba68967df32ca765972d7ee0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-07 13:04:22 +0700

    adding tv upcoming date

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/UpcomingTimerViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/UpcomingTimerViewComponent.kt
@@ -2,6 +2,9 @@ package com.tokopedia.play.view.viewcomponent
 
 import android.view.ViewGroup
 import androidx.annotation.IdRes
+import androidx.appcompat.widget.AppCompatTextView
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.play.R
 import com.tokopedia.play_common.viewcomponent.ViewComponent
 import com.tokopedia.unifycomponents.timer.TimerUnifyHighlight
@@ -11,13 +14,25 @@ import java.util.*
  * Created By : Jonathan Darwin on September 06, 2021
  */
 class UpcomingTimerViewComponent(
-    private val container: ViewGroup,
+    container: ViewGroup,
     @IdRes idRes: Int
 ): ViewComponent(container, idRes) {
 
     private val timerUpcoming = findViewById<TimerUnifyHighlight>(R.id.timer_upcoming)
+    private val tvTimerLabel = findViewById<AppCompatTextView>(R.id.tv_timer_label)
+    private val tvUpcomingDate = findViewById<AppCompatTextView>(R.id.tv_upcoming_date)
 
     fun setTimer(calendar: Calendar) {
+        timerUpcoming.show()
+        tvTimerLabel.show()
+        tvUpcomingDate.hide()
         timerUpcoming.targetDate = calendar
+    }
+
+    fun setDate(date: String) {
+        tvUpcomingDate.show()
+        timerUpcoming.hide()
+        tvTimerLabel.hide()
+        tvUpcomingDate.text = date
     }
 }
