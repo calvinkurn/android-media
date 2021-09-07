8f60ae8de3b20a9b3968b96b655333062e279fc1
yohann.prananta <yohann.prananta@BA-00004624.local>
2021-09-07 22:23:00 +0700

    fix tagged bundle detail

--- features/merchant/product_bundle/src/main/java/com/tokopedia/product_bundle/multiple/presentation/viewholder/ProductBundleDetailViewHolder.kt
+++ features/merchant/product_bundle/src/main/java/com/tokopedia/product_bundle/multiple/presentation/viewholder/ProductBundleDetailViewHolder.kt
@@ -6,11 +6,10 @@ import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.kotlin.extensions.view.visible
-import com.tokopedia.product.detail.common.data.model.variant.ProductVariant
 import com.tokopedia.product_bundle.R
 import com.tokopedia.product_bundle.common.customview.DiscountPriceView
-import com.tokopedia.product_bundle.common.customview.SpinnerView
 import com.tokopedia.product_bundle.common.customview.RoundedCornerImageView
+import com.tokopedia.product_bundle.common.customview.SpinnerView
 import com.tokopedia.product_bundle.multiple.presentation.adapter.ProductBundleDetailAdapter.ProductBundleDetailItemClickListener
 import com.tokopedia.product_bundle.multiple.presentation.model.ProductBundleDetail
 import com.tokopedia.unifyprinciples.Typography
@@ -31,7 +30,6 @@ class ProductBundleDetailViewHolder(itemView: View, clickListener: ProductBundle
         this.tvVariantEmpty = itemView.findViewById(R.id.tv_variant_empty)
         this.productPriceView = itemView.findViewById(R.id.dpv_product_price)
         this.productVariantsView = itemView.findViewById(R.id.sv_product_variants)
-
         this.productNameView?.setOnClickListener {
             val productBundleDetailObj = productVariantsView?.getTag(R.id.product_bundle_detail_tag)
             productBundleDetailObj?.let { obj ->
@@ -39,26 +37,21 @@ class ProductBundleDetailViewHolder(itemView: View, clickListener: ProductBundle
                 clickListener.onProductNameViewClicked(productBundleDetail)
             }
         }
-
         this.productVariantsView?.setOnClickListener {
             val productBundleDetailObj = productVariantsView?.getTag(R.id.product_bundle_detail_tag)
             productBundleDetailObj?.let { obj ->
                 val productBundleDetail = obj as ProductBundleDetail
                 clickListener.onProductVariantSpinnerClicked(productBundleDetail)
             }
-//            val productVariantObj = productVariantsView?.getTag(R.id.product_variant_tag)
-//            productVariantObj?.let { obj ->
-//                val productVariant = obj as ProductVariant
-//                clickListener.onProductVariantSpinnerClicked(productVariant)
-//            }
         }
     }
 
     fun bindData(bundleDetail: ProductBundleDetail, isVariantStockEmpty: Boolean) {
         productImageView?.loadImage(bundleDetail.productImageUrl)
         productNameView?.text = bundleDetail.productName
-        // tag product variant to productVariantsView
-        productVariantsView?.setTag(R.id.product_variant_tag, bundleDetail.productVariant)
+        // tag product bundle detail to product variant spinner and product name view
+        productNameView?.setTag(R.id.product_bundle_detail_tag, bundleDetail)
+        productVariantsView?.setTag(R.id.product_bundle_detail_tag, bundleDetail)
         // hide product variant dropbox when there is no selection
         if (bundleDetail.hasVariant) productVariantsView?.visible()
         else productVariantsView?.gone()
