c41ae25d2818a710ac3a69efa7614aee7d247480
yoasfs <yoas.susantio@tokopedia.com>
2021-09-09 16:14:55 +0700

    update

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/domain/usecases/GetPrimaryProductUseCase.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/domain/usecases/GetPrimaryProductUseCase.kt
@@ -1,16 +1,21 @@
 package com.tokopedia.home_recom.domain.usecases
 
+import android.content.Context
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.home_recom.*
 import com.tokopedia.home_recom.model.entity.PrimaryProductEntity
+import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
+import com.tokopedia.recommendation_widget_common.ext.toQueryParam
 import com.tokopedia.usecase.RequestParams
 import com.tokopedia.usecase.coroutines.UseCase
+import javax.inject.Inject
 
-class GetPrimaryProductUseCase (
+class GetPrimaryProductUseCase @Inject constructor(
+        private val context: Context,
         private val graphqlUseCase: GraphqlUseCase<PrimaryProductEntity>
-): UseCase<PrimaryProductEntity>(){
+) : UseCase<PrimaryProductEntity>() {
 
     init {
         graphqlUseCase.setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.CLOUD_THEN_CACHE).build())
@@ -28,7 +33,8 @@ class GetPrimaryProductUseCase (
     fun setParameter(productId: String, queryParam: String){
         parameter.parameters.clear()
         parameter.putString(PARAM_PRODUCT_ID_STRING, productId)
-        parameter.putString(PARAM_QUERY_PARAM, queryParam)
+        parameter.putString(PARAM_QUERY_PARAM, ChooseAddressUtils.getLocalizingAddressData(context)?.toQueryParam(queryParam)
+                ?: queryParam)
         parameter.putString(PARAM_X_SOURCE, DEFAULT_X_SOURCE)
     }
 
