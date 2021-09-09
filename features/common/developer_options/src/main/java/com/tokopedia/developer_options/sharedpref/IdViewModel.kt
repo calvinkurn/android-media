1d7b718bd6d59a3321f0f92c2d53cc9e3f2ba39d
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-09-09 17:02:25 +0700

    remove parcelize

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/sharedpref/IdViewModel.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/sharedpref/IdViewModel.kt
@@ -1,13 +1,10 @@
 package com.tokopedia.developer_options.sharedpref
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class IdViewModel(var id: String = "") : Visitable<SimpleTypeFactory> {
 
     override fun type(typeFactory: SimpleTypeFactory): Int {
         return typeFactory.type(this)
     }
-
 }
