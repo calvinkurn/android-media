390a9c28f4af1389545850b7be7d44e1a55a3fec
hansenpw <hansen.wijaya@tokopedia.com>
2021-09-10 09:31:25 +0700

    adjust android test

--- features/transaction/checkout/src/androidTest/java/com/tokopedia/checkout/journey/simple/CheckoutTokoNowTest.kt
+++ features/transaction/checkout/src/androidTest/java/com/tokopedia/checkout/journey/simple/CheckoutTokoNowTest.kt
@@ -1,11 +1,11 @@
-package com.tokopedia.checkout.bundle.journey.simple
+package com.tokopedia.checkout.journey.simple
 
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
-import com.tokopedia.checkout.bundle.robot.checkoutPage
-import com.tokopedia.checkout.bundle.test.R
-import com.tokopedia.checkout.bundle.view.ShipmentActivity
+import com.tokopedia.checkout.robot.checkoutPage
+import com.tokopedia.checkout.test.R
+import com.tokopedia.checkout.old.OldShipmentActivity
 import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.test.application.util.InstrumentationMockHelper
@@ -17,7 +17,7 @@ import org.junit.Test
 class CheckoutTokoNowTest {
 
     @get:Rule
-    var activityRule = object : IntentsTestRule<ShipmentActivity>(ShipmentActivity::class.java, false, false) {
+    var activityRule = object : IntentsTestRule<OldShipmentActivity>(OldShipmentActivity::class.java, false, false) {
         override fun beforeActivityLaunched() {
             super.beforeActivityLaunched()
             InstrumentationAuthHelper.loginInstrumentationTestUser1()
