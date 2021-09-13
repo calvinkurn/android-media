b6e53b83d0d653caa86bbd8f346502d0d734293f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-13 09:24:38 +0700

    change hardcoded sse url to tokopediaurl

--- features/content/play_common/src/main/java/com/tokopedia/play_common/sse/PlayChannelSSEImpl.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/sse/PlayChannelSSEImpl.kt
@@ -27,7 +27,7 @@ class PlayChannelSSEImpl @Inject constructor(
     private var sseFlow = MutableSharedFlow<SSEAction>(extraBufferCapacity = 100)
 
     override fun connect(channelId: String, pageSource: String, gcToken: String) {
-        var url = "https://sse-staging.tokopedia.com/${PLAY_SSE}?page=$pageSource&channel_id=$channelId"
+        var url = "${TokopediaUrl.getInstance().SSE}${PLAY_SSE}?page=$pageSource&channel_id=$channelId"
         if(gcToken.isNotEmpty()) url += "&token=${gcToken}"
 
         val request = Request.Builder().get().url(url)
