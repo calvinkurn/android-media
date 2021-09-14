57ba3ffa9b6dde0109ab3cd16bcae106bcf6f45f
leonard2907 <leonard.gunawan@tokopedia.com>
2021-09-14 13:23:29 +0700

    Fix unit test

---
+++ features/merchant/gallery/src/test/java/com/tokopedia/gallery/viewmodel/GalleryViewModelTest.kt
@@ -0,0 +1,63 @@
+package com.tokopedia.gallery.viewmodel
+
+import com.tokopedia.gallery.networkmodel.ProductrevGetReviewImage
+import com.tokopedia.gallery.networkmodel.ProductrevGetReviewImageResponse
+import com.tokopedia.unit.test.ext.verifyErrorEquals
+import com.tokopedia.unit.test.ext.verifySuccessEquals
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
+import io.mockk.coEvery
+import io.mockk.coVerify
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+
+class GalleryViewModelTest : GalleryViewModelTestFixture() {
+
+    @Test
+    fun `when setPage should call getProductReviews and return expected results`() {
+        val productId = ArgumentMatchers.anyString()
+        val page = ArgumentMatchers.anyInt()
+        val expectedResponse = ProductrevGetReviewImageResponse()
+
+        onGetReviewImagesSuccess_thenReturn(expectedResponse)
+
+        viewModel.setPage(productId, page)
+
+        verifyGetReviewImagesUseCaseExecuted()
+        verifyReviewImagesSuccessEquals(Success(expectedResponse.productrevGetReviewImage))
+    }
+
+    @Test
+    fun `when setPage should call getReviewImages and return expected error`() {
+        val productId = ArgumentMatchers.anyString()
+        val page = ArgumentMatchers.anyInt()
+        val expectedResponse = Throwable()
+
+        onGetReviewsImagesFail_thenReturn(expectedResponse)
+
+        viewModel.setPage(productId, page)
+
+        verifyGetReviewImagesUseCaseExecuted()
+        verifyReviewImagesErrorEquals(Fail(expectedResponse))
+    }
+
+    private fun onGetReviewImagesSuccess_thenReturn(expectedResponse: ProductrevGetReviewImageResponse) {
+        coEvery { getReviewImagesUseCase.executeOnBackground() } returns expectedResponse
+    }
+
+    private fun onGetReviewsImagesFail_thenReturn(throwable: Throwable) {
+        coEvery { getReviewImagesUseCase.executeOnBackground() } throws throwable
+    }
+
+    private fun verifyGetReviewImagesUseCaseExecuted() {
+        coVerify { getReviewImagesUseCase.executeOnBackground() }
+    }
+
+    private fun verifyReviewImagesSuccessEquals(expectedSuccessValue: Success<ProductrevGetReviewImage>) {
+        viewModel.reviewImages.verifySuccessEquals(expectedSuccessValue)
+    }
+
+    private fun verifyReviewImagesErrorEquals(expectedErrorValue: Fail) {
+        viewModel.reviewImages.verifyErrorEquals(expectedErrorValue)
+    }
+}
