7b67e022fa8240f5dc4c2751ece4caba33b550d2
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-15 15:05:48 +0700

    handle edge case where seller is late starting upcoming channel and user open that channel

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/UpcomingTimerViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/UpcomingTimerViewComponent.kt
@@ -9,6 +9,7 @@ import com.tokopedia.play.R
 import com.tokopedia.play_common.util.datetime.PlayDateTimeFormatter
 import com.tokopedia.play_common.viewcomponent.ViewComponent
 import com.tokopedia.unifycomponents.timer.TimerUnifyHighlight
+import java.util.*
 
 /**
  * Created By : Jonathan Darwin on September 06, 2021
@@ -22,8 +23,9 @@ class UpcomingTimerViewComponent(
 
     fun setupTimer(startTime: String) {
         val targetCalendar = PlayDateTimeFormatter.convertToCalendar(startTime)
-        targetCalendar?.let { calendar ->
-            timerUpcoming.targetDate = calendar
+        targetCalendar?.let { target ->
+            if(target.timeInMillis > Calendar.getInstance().timeInMillis)
+                timerUpcoming.targetDate = target
         } ?: invisible()
     }
 
