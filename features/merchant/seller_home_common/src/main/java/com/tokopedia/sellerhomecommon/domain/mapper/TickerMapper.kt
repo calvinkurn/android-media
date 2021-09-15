cdf14aee4f316cbba53793763182de5da487412e
elscione <yusufhendrawan06@gmail.com>
2021-09-15 10:55:13 +0700

    Remove trailing spaces from seller home ticker title and description/message by manually removing paragraph tag

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/TickerMapper.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/TickerMapper.kt
@@ -3,6 +3,7 @@ package com.tokopedia.sellerhomecommon.domain.mapper
 import com.tokopedia.sellerhomecommon.common.SellerHomeCommonUtils
 import com.tokopedia.sellerhomecommon.domain.model.GetTickerResponse
 import com.tokopedia.sellerhomecommon.presentation.model.TickerItemUiModel
+import com.tokopedia.sellerhomecommon.utils.Utils
 import javax.inject.Inject
 
 /**
@@ -16,8 +17,8 @@ class TickerMapper @Inject constructor() : BaseResponseMapper<GetTickerResponse,
             TickerItemUiModel(
                     color = it.color,
                     id = it.id.orEmpty(),
-                    message = it.message.orEmpty(),
-                    title = it.title.orEmpty(),
+                    message = Utils.fromHtmlWithoutExtraSpace(it.message.orEmpty()),
+                    title = Utils.fromHtmlWithoutExtraSpace(it.title.orEmpty()),
                     type = it.tickerType ?: 0,
                     redirectUrl = SellerHomeCommonUtils.extractUrls(it.message.orEmpty())
                             .getOrNull(0)
