07c94fb219c1c88a59846ca09f439f61193b9e84
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-09-15 18:28:57 +0700

    fix logic isRM

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/local/model/PMStatusUiModel.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/local/model/PMStatusUiModel.kt
@@ -3,6 +3,7 @@ package com.tokopedia.gm.common.data.source.local.model
 import com.tokopedia.abstraction.common.utils.view.DateFormatUtils
 import com.tokopedia.gm.common.constant.PMConstant
 import com.tokopedia.gm.common.constant.PMStatusConst
+import com.tokopedia.gm.common.constant.PMTier
 
 /**
  * Created By @ilhamsuaib on 16/03/21
@@ -30,7 +31,7 @@ data class PMStatusUiModel(
     }
 
     fun isRegularMerchant(): Boolean =
-        status == PMStatusConst.INACTIVE || status == PMStatusConst.INACTIVE
+        status == PMStatusConst.INACTIVE || pmTier == PMTier.REGULAR
 
     fun isPowerMerchant(): Boolean = status == PMStatusConst.ACTIVE || status == PMStatusConst.IDLE
 }
