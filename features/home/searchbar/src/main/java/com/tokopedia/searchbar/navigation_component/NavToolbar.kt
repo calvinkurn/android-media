1e0d199ab976bca1a3b5918cf4a16efae87e083c
yoasfs <yoas.susantio@tokopedia.com>
2021-09-17 16:20:11 +0700

    fix ui

--- features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/NavToolbar.kt
+++ features/home/searchbar/src/main/java/com/tokopedia/searchbar/navigation_component/NavToolbar.kt
@@ -224,7 +224,7 @@ class NavToolbar: Toolbar, LifecycleObserver, TopNavComponentListener {
     /**
      * Call this function to let the NavToolbar manage your status bar transparency
      */
-    fun setupToolbarWithStatusBar(activity: Activity, statusBarTheme: Int? = null, applyPadding: Boolean = true) {
+    fun setupToolbarWithStatusBar(activity: Activity, statusBarTheme: Int? = null, applyPadding: Boolean = true, applyPaddingNegative: Boolean = false) {
         statusBarUtil = StatusBarUtil(WeakReference(activity))
 
         statusBarTheme?.let {
@@ -236,6 +236,7 @@ class NavToolbar: Toolbar, LifecycleObserver, TopNavComponentListener {
         }
 
         if (applyPadding) applyStatusBarPadding()
+        if (applyPaddingNegative) applyStatusBarNegativePadding()
     }
 
     /**
@@ -503,7 +504,7 @@ class NavToolbar: Toolbar, LifecycleObserver, TopNavComponentListener {
     }
 
     fun getCurrentSearchbarText(): String {
-        return navSearchBarController.etSearch?.text?.toString()?:""
+        return navSearchBarController.etSearch?.text?.toString() ?: ""
     }
 
     private fun applyStatusBarPadding() {
@@ -512,8 +513,14 @@ class NavToolbar: Toolbar, LifecycleObserver, TopNavComponentListener {
         navToolbar?.updatePadding(top = pT)
     }
 
+    private fun applyStatusBarNegativePadding() {
+        var pT = 0
+//        pT = ViewHelper.getStatusBarHeight(context)
+        navToolbar?.updatePadding(top = pT)
+    }
+
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    private fun onStartListener(owner: LifecycleOwner){
+    private fun onStartListener(owner: LifecycleOwner) {
         this.lifecycleOwner = owner
         observeLiveData()
     }
