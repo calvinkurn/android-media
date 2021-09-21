86aa478add7362fccd7b1538c595442dbd956d24
hansenpw <hansen.wijaya@tokopedia.com>
2021-09-21 12:50:53 +0700

    set page source dimension key

--- features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/analytics/enhanced_ecommerce_data/EnhancedECommerceProductCartMapData.kt
+++ features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/analytics/enhanced_ecommerce_data/EnhancedECommerceProductCartMapData.kt
@@ -226,7 +226,7 @@ class EnhancedECommerceProductCartMapData {
         private const val KEY_PICTURE = "picture"
         private const val KEY_URL = "url"
         private const val KEY_CAMPAIGN_ID = "dimension104"
-        private const val KEY_PAGE_SOURCE = "pageSource"
+        private const val KEY_PAGE_SOURCE = "dimension90"
         const val DEFAULT_VALUE_NONE_OTHER = "none/other"
         const val VALUE_BEBAS_ONGKIR = "bebas ongkir"
         const val VALUE_BEBAS_ONGKIR_EXTRA = "bebas ongkir extra"
