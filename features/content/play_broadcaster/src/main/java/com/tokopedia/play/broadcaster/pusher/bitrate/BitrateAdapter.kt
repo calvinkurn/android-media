38bf20897b51f786a6735c1e89237a32089dc14f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-09-21 17:14:07 +0700

    wrap with try catch on runtask bitrate adapter

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/pusher/bitrate/BitrateAdapter.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/pusher/bitrate/BitrateAdapter.kt
@@ -94,11 +94,16 @@ abstract class BitrateAdapter(context: Context) {
         if (checkDelay() == 0L || checkInterval() == 0L) return
         val checkNetwork: TimerTask = object : TimerTask() {
             override fun run() {
-                if (mStreamer == null || mConnectionId == null) return
-                val audioLost: Long = mStreamer!!.getAudioPacketsLost(mConnectionId!!)
-                var videoLost: Long = mStreamer!!.getVideoPacketsLost(mConnectionId!!)
-                videoLost += mStreamer!!.getUdpPacketsLost(mConnectionId!!)
-                check(audioLost, videoLost)
+                try {
+                    if (mStreamer == null && mConnectionId == null) return
+                    val audioLost: Long = mStreamer!!.getAudioPacketsLost(mConnectionId!!)
+                    var videoLost: Long = mStreamer!!.getVideoPacketsLost(mConnectionId!!)
+                    videoLost += mStreamer!!.getUdpPacketsLost(mConnectionId!!)
+                    check(audioLost, videoLost)
+                }
+                catch (e: Exception) {
+
+                }
             }
         }
         mCheckTimer = Timer()
