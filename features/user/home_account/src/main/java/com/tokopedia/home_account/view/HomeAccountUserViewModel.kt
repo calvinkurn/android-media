0852f5e4fe32595f24d9ddcdc837613b417fb758
adefulki <ade.hadian@tokopedia.com>
2021-09-23 12:52:12 +0700

    code review home account, asset page

--- features/user/home_account/src/main/java/com/tokopedia/home_account/view/HomeAccountUserViewModel.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/HomeAccountUserViewModel.kt
@@ -185,7 +185,7 @@ class HomeAccountUserViewModel @Inject constructor(
     }
 
     fun getCentralizedUserAssetConfig(entryPoint: String) {
-        launchCatchError(context = dispatcher.main, block = {
+        launchCatchError(block = {
             val result = getCentralizedUserAssetConfigUseCase(entryPoint)
             _centralizedUserAssetConfig.value = Success(result.data)
         }, onError = {
@@ -194,7 +194,7 @@ class HomeAccountUserViewModel @Inject constructor(
     }
 
     fun getBalanceAndPoint(walletId: String, assetConfig: AssetConfig? = null) {
-        launchCatchError(context=dispatcher.main, block = {
+        launchCatchError(block = {
             if(walletId == AccountConstants.WALLET.TOKOPOINT) {
                 val result = getTokopointsBalanceAndPointUseCase(Unit)
                 setBalanceAndPointValue(result.data, walletId)
@@ -249,7 +249,7 @@ class HomeAccountUserViewModel @Inject constructor(
     }
 
     fun getGopayWalletEligible() {
-        launchCatchError(context=dispatcher.main, block = {
+        launchCatchError(block = {
             val params = getWalletEligibleUseCase.getParams(GOPAY_PARTNER_CODE, GOPAY_WALLET_CODE)
             val result = getWalletEligibleUseCase(params)
 
