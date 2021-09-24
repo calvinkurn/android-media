734fd2801fe2cdd0becc760d1ea927288e0346ab
Devara Fikry Akmal <devara.akmal@tokopedia.com>
2021-09-24 09:40:25 +0700

    Simplify test file

--- libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/test/home/HomeStartupBenchmark.kt
+++ libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/test/home/HomeStartupBenchmark.kt
@@ -1,17 +1,12 @@
 package com.tkpd.macrobenchmark.test.home
 
 import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.filters.LargeTest
 import androidx.test.filters.SdkSuppress
-import androidx.test.platform.app.InstrumentationRegistry
-import com.tkpd.macrobenchmark.util.MacroArgs
+import com.tkpd.macrobenchmark.base.BaseStartupBenchmark
 import com.tkpd.macrobenchmark.util.MacroDevOps
 import com.tkpd.macrobenchmark.util.MacroIntent
-import com.tkpd.macrobenchmark.util.measureStartup
 import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
@@ -25,30 +20,10 @@ import org.junit.runners.Parameterized
 @LargeTest
 @SdkSuppress(minSdkVersion = 29)
 @RunWith(Parameterized::class)
-class HomeStartupBenchmark(private val startupMode: StartupMode) {
-    @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
-
+class HomeStartupBenchmark(startupMode: StartupMode): BaseStartupBenchmark(startupMode) {
     @Before
     fun setupEnvironment() {
         MacroDevOps.setupHomeEnvironment()
     }
-
-    @Test
-    fun macroBenchmarkHomeLaunchTime() {
-        benchmarkRule.measureStartup(
-            startupMode = startupMode,
-            iterations = MacroArgs.getIterations(InstrumentationRegistry.getArguments())) {
-            MacroIntent.Home.getHomeIntent()
-        }
-    }
-
-    companion object {
-        @Parameterized.Parameters(name = "mode={0}")
-        @JvmStatic
-        fun parameters(): List<Array<Any>> {
-            return listOf(StartupMode.COLD, StartupMode.WARM, StartupMode.HOT)
-                .map { arrayOf(it) }
-        }
-    }
+    override fun getIntent() = MacroIntent.Home.getHomeIntent()
 }
