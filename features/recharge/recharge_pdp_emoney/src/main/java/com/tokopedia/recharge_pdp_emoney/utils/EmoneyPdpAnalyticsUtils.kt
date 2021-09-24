00ee60194602a9e1701a0ba70df4954c3056e31f
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-09-24 13:21:13 +0700

    add tracker number 12,13,14,15,16

--- features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/utils/EmoneyPdpAnalyticsUtils.kt
+++ features/recharge/recharge_pdp_emoney/src/main/java/com/tokopedia/recharge_pdp_emoney/utils/EmoneyPdpAnalyticsUtils.kt
@@ -9,7 +9,7 @@ import com.tokopedia.common_digital.common.constant.DigitalTrackingConst.Value.R
 import com.tokopedia.recharge_pdp_emoney.utils.EmoneyPdpAnalyticsConst.Category.EMONEY_PDP_SCREEN_NAME
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
-import java.util.HashMap
+import java.util.*
 
 /**
  * @author by jessica on 06/05/21
@@ -23,61 +23,61 @@ object EmoneyPdpAnalyticsUtils {
         TrackApp.getInstance().gtm.sendScreenAuthenticated(EMONEY_PDP_SCREEN_NAME, customDimension)
     }
 
-    fun clickCheckSaldoButton(userId: String) {
+    fun clickCheckSaldoButton(userId: String, issuerName: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(DataLayer.mapOf(
-                TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.DIGITAL_GENERAL_EVENT,
+                TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_DIGITAL_EVENT,
                 TrackAppUtils.EVENT_CATEGORY, EmoneyPdpAnalyticsConst.Category.DIGITAL_HOMEPAGE,
                 TrackAppUtils.EVENT_ACTION, EmoneyPdpAnalyticsConst.Action.CLICK_UPDATE_SALDO,
-                TrackAppUtils.EVENT_LABEL, "",
+                TrackAppUtils.EVENT_LABEL, issuerName,
                 BUSINESS_UNIT, RECHARGE_BU,
                 CURRENTSITE, RECHARGE_SITE,
                 USER_ID, userId
         ))
     }
 
-    fun clickSeeProductDetail(price: String, userId: String) {
+    fun clickSeeProductDetail(issuerName: String, price: String, userId: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(DataLayer.mapOf(
                 TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_EMONEY,
                 TrackAppUtils.EVENT_CATEGORY, EmoneyPdpAnalyticsConst.Category.DIGITAL_UANG_ELEKTRONIK,
                 TrackAppUtils.EVENT_ACTION, EmoneyPdpAnalyticsConst.Action.CLICK_LIHAT_DETAIL,
-                TrackAppUtils.EVENT_LABEL, price,
+                TrackAppUtils.EVENT_LABEL, "$issuerName - $price",
                 BUSINESS_UNIT, RECHARGE_BU,
                 CURRENTSITE, RECHARGE_SITE,
                 USER_ID, userId
         ))
     }
 
-    fun clickClearCardNumber(userId: String) {
+    fun clickClearCardNumber(userId: String, issuerName: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(DataLayer.mapOf(
                 TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_EMONEY,
                 TrackAppUtils.EVENT_CATEGORY, EmoneyPdpAnalyticsConst.Category.DIGITAL_UANG_ELEKTRONIK,
                 TrackAppUtils.EVENT_ACTION, EmoneyPdpAnalyticsConst.Action.CLICK_X_BUTTON_CARD_NUMBER,
-                TrackAppUtils.EVENT_LABEL, "",
+                TrackAppUtils.EVENT_LABEL, issuerName,
                 BUSINESS_UNIT, RECHARGE_BU,
                 CURRENTSITE, RECHARGE_SITE,
                 USER_ID, userId
         ))
     }
 
-    fun clickCameraIcon(userId: String) {
+    fun clickCameraIcon(userId: String, issuerName: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(DataLayer.mapOf(
-                TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_DIGITAL_NATIVE,
+                TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_EMONEY,
                 TrackAppUtils.EVENT_CATEGORY, EmoneyPdpAnalyticsConst.Category.DIGITAL_NATIVE,
                 TrackAppUtils.EVENT_ACTION, EmoneyPdpAnalyticsConst.Action.CLICK_CAMERA_ICON,
-                TrackAppUtils.EVENT_LABEL, "",
+                TrackAppUtils.EVENT_LABEL, issuerName,
                 BUSINESS_UNIT, RECHARGE_BU,
                 CURRENTSITE, RECHARGE_SITE,
                 USER_ID, userId
         ))
     }
 
-    fun clickChangeCardNumber(cardNumber: String, userId: String) {
+    fun clickChangeCardNumber(cardNumber: String, userId: String, issuerName: String) {
         if (cardNumber.isNotEmpty()) {
             TrackApp.getInstance().gtm.sendGeneralEvent(DataLayer.mapOf(
                     TrackAppUtils.EVENT, EmoneyPdpAnalyticsConst.Event.CLICK_EMONEY,
                     TrackAppUtils.EVENT_CATEGORY, EmoneyPdpAnalyticsConst.Category.DIGITAL_UANG_ELEKTRONIK,
                     TrackAppUtils.EVENT_ACTION, EmoneyPdpAnalyticsConst.Action.INPUT_CARD_NUMBER,
-                    TrackAppUtils.EVENT_LABEL, cardNumber,
+                    TrackAppUtils.EVENT_LABEL, "$issuerName - $cardNumber",
                     BUSINESS_UNIT, RECHARGE_BU,
                     CURRENTSITE, RECHARGE_SITE,
                     USER_ID, userId
