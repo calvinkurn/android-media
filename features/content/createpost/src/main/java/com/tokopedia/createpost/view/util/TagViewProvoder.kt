cc43e05d63bc8c7f914bdf71acd533ac482f879d
Lavekush Agrawal <lavekush.agrawal@tokopedia.com>
2021-09-27 20:48:37 +0530

    Added coordinates for GA

--- features/content/createpost/src/main/java/com/tokopedia/createpost/view/util/TagViewProvoder.kt
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/view/util/TagViewProvoder.kt
@@ -16,43 +16,44 @@ import com.tokopedia.feedcomponent.data.feedrevamp.FeedXMediaTagging
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.imagepicker_insta.toPx
 import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.unifyprinciples.Typography
+import java.util.logging.Handler
 
-class TagViewProvider
-  {
-     var dX = 0f
-     var dY = 0f
-     var listener: CreateContentPostCOmmonLIstener? = null
+class TagViewProvider {
+    var dX = 0f
+    var dY = 0f
+    var listener: CreateContentPostCOmmonLIstener? = null
 
 
-     /*
+    /*
 
-     val tagImg = findViewById<ConstraintLayout>(R.id.tag)
+    val tagImg = findViewById<ConstraintLayout>(R.id.tag)
 
-         tagImg.setOnTouchListener(object : View.OnTouchListener {
-             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                 when (event?.action) {
-                     MotionEvent.ACTION_DOWN -> {
-                         return true
-                     }
+        tagImg.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        return true
+                    }
 
-                     MotionEvent.ACTION_UP -> {
-                         val vv = TagViewProvider.getTagView(this@MainActivity)
- //                        vv.x = event.x
- //                        vv.y = event.y
- //                        tagImg.addView(vv)
-                         TagViewProvider.addViewToParent(vv, v as ConstraintLayout, event)
-                     }
+                    MotionEvent.ACTION_UP -> {
+                        val vv = TagViewProvider.getTagView(this@MainActivity)
+//                        vv.x = event.x
+//                        vv.y = event.y
+//                        tagImg.addView(vv)
+                        TagViewProvider.addViewToParent(vv, v as ConstraintLayout, event)
+                    }
 
-                 }
+                }
 
-                 return v?.onTouchEvent(event) ?: true
-             }
-         })
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
 
-      */
+     */
 
     /*
 
@@ -106,10 +107,12 @@ class TagViewProvider
             productViewPrice.text = price
         }
         productTagViewDelete.setOnClickListener {
-            listener?.deleteItemFromProductTagList(feedXTag.tagIndex,
+            listener?.deleteItemFromProductTagList(
+                feedXTag.tagIndex,
                 id,
                 true,
-                MediaType.IMAGE)
+                MediaType.IMAGE
+            )
             view.gone()
         }
         return view
@@ -122,11 +125,7 @@ class TagViewProvider
         feedXMediaTagging: FeedXMediaTagging,
     ) {
         child.visibility = View.INVISIBLE
-
-        val location = IntArray(2)
-        parent.getLocationOnScreen(location)
-        val x = location[0]
-        val y = location[1]
+        parent.addView(child)
 
         Log.d("Lavekush", "X=$x ,Y=$y")
 
@@ -135,20 +134,17 @@ class TagViewProvider
             view.parent.requestDisallowInterceptTouchEvent(true)
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    if (!productTagViewDelete.isVisible)
-                        productTagViewDelete.visible()
-                    else
-                        productTagViewDelete.gone()
                     dX = view.x - motionEvent.rawX
                     dY = view.y - motionEvent.rawY
                     return@OnTouchListener true
                 }
+
                 MotionEvent.ACTION_MOVE -> {
                     val gotoX: Float = when {
-                        (motionEvent.rawX?.plus(dX) ?: 0f) + pxFromDp( view.context, 145f) > parent.right -> {
-                            parent.right - pxFromDp(view.context, 145f) /*Blocking right on drag*/
+                        (motionEvent.rawX.plus(dX) ?: 0f) + view.width > parent.right -> {
+                            parent.right - view.width.toFloat() /*Blocking right on drag*/
                         }
-                        motionEvent.rawX?.plus(dX) ?: 0f < 0 -> {
+                        motionEvent.rawX.plus(dX) ?: 0f < 0 -> {
                             0f /*Blocking left on drag*/
                         }
                         else -> {
@@ -163,8 +159,8 @@ class TagViewProvider
                         motionEvent.rawY.plus(dY) < 0 -> {
                             0f /*Blocking top on drag*/
                         }
-                        (motionEvent.rawY?.plus(dY) ?: 0f) + pxFromDp(child.context, 68f) > parent.bottom -> {
-                            parent.bottom - pxFromDp(view.context, 68f) /*Blocking bottom on drag*/
+                        (motionEvent.rawY?.plus(dY) ?: 0f) + view.height > parent.bottom -> {
+                            parent.bottom - view.height.toFloat() /*Blocking bottom on drag*/
                         }
                         else -> {
                             motionEvent.rawY.plus(dY)  /*Normal vertical drag*/
@@ -193,6 +189,7 @@ class TagViewProvider
                         .y(gotoY)
                         .setDuration(0)
                         .start()
+
                     if (view.y < parent.height * 0.75) {
                         view.findViewById<View>(R.id.topNotch).visibility = View.VISIBLE
                         view.findViewById<View>(R.id.bottomNotch).visibility = View.GONE
@@ -203,44 +200,61 @@ class TagViewProvider
 
                     return@OnTouchListener true
                 }
+
+                MotionEvent.ACTION_UP -> {
+                    if (productTagViewDelete.isVisible) {
+                        productTagViewDelete.gone()
+                    } else {
+                        productTagViewDelete.hide()
+                    }
+
+                    val location = IntArray(2)
+                    view.findViewById<View>(R.id.topNotch).getLocationOnScreen(location)
+                    feedXMediaTagging.X =  location[0].toFloat()
+                    feedXMediaTagging.Y =  location[1].toFloat()
+                }
             }
+
             false
         })
 
-        /*Handling for X position*/
-        var xTapped: Float =
-            feedXMediaTagging.X?.minus(pxFromDp(child.context, 145f) / 2) ?: 0f
-        val x2Want: Float = xTapped + pxFromDp(child.context, 145f)
-        val x2Diff = parent.right - x2Want
-        if (x2Diff < 0) {
-            xTapped += x2Diff
-        }
+        android.os.Handler().postDelayed(Runnable {
+            /*Handling for X position*/
+            var xTapped: Float =
+                feedXMediaTagging.X?.minus(child.width / 2) ?: 0f
+            val x2Want: Float = xTapped + child.width.toFloat()
+            val x2Diff = parent.right - x2Want
+            if (x2Diff < 0) {
+                xTapped += x2Diff
+            }
 
+            /*Handling for Y position*/
+            var yTapped = feedXMediaTagging.Y!!
+            val y2Want: Float = yTapped + child.height.toFloat()
+            val y2Diff = parent.bottom - y2Want
+            if (y2Diff < 0) {
+                yTapped += y2Diff
+            }
 
-        /*Handling for Y position*/
-        var yTapped = feedXMediaTagging.Y!!
-        val y2Want: Float = yTapped + pxFromDp(child.context, 68f)
-        val y2Diff = parent.bottom - y2Want
-        if (y2Diff < 0) {
-            yTapped += y2Diff
-        }
+            //Handling for negative X axis
+            if (xTapped < 0) {
+                child.x = 0f
+            } else {
+                child.x = xTapped
+            }
 
-        //Handling for negative X axis
-        if (xTapped < 0) {
-            child.x = 0f
-        } else {
-            child.x = xTapped
-        }
-        child.y = yTapped
-        child.visibility = View.VISIBLE
-        parent.addView(child)
-        if (child.y < parent.height * 0.75) {
-            child.findViewById<View>(R.id.topNotch).visibility = View.VISIBLE
-            child.findViewById<View>(R.id.bottomNotch).visibility = View.GONE
-        } else {
-            child.findViewById<View>(R.id.topNotch).visibility = View.GONE
-            child.findViewById<View>(R.id.bottomNotch).visibility = View.VISIBLE
-        }
+            child.y = yTapped
+            child.visibility = View.VISIBLE
+
+            if (child.y < parent.height * 0.75) {
+                child.findViewById<View>(R.id.topNotch).visibility = View.VISIBLE
+                child.findViewById<View>(R.id.bottomNotch).visibility = View.GONE
+            } else {
+                child.findViewById<View>(R.id.topNotch).visibility = View.GONE
+                child.findViewById<View>(R.id.bottomNotch).visibility = View.VISIBLE
+            }
+
+        }, 50)
 
 //        new Handler().post(new Runnable() {
 //            @Override
