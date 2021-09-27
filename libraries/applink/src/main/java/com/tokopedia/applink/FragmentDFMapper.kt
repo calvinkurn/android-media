a5f00ad94b6e2fd231d9c69be5f43bb51fadcb09
shiperus <calvyn.julian@tokopedia.com>
2021-09-27 18:37:32 +0700

    fix FragmentDFMapper for sellerapp

--- libraries/applink/src/main/java/com/tokopedia/applink/FragmentDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/FragmentDFMapper.kt
@@ -8,6 +8,7 @@ import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.tokopedia.applink.FragmentConst.DF_INSTALLER_FRAGMENT_CLASS_PATH
 import com.tokopedia.applink.FragmentConst.REVIEW_SHOP_FRAGMENT
+import com.tokopedia.config.GlobalConfig
 import tokopedia.applink.R
 
 /**
@@ -22,7 +23,7 @@ object FragmentDFMapper {
     private const val BUNDLE_ARGUMENTS_KEY_EXTRAS = "BUNDLE_ARGUMENTS_EXTRAS"
     private const val BUNDLE_KEY_CLASS_NAME = "CLASS_NAME"
     private var manager: SplitInstallManager? = null
-    val fragmentDfModuleMapper: List<FragmentDFPattern> by lazy {
+    val fragmentDfModuleMapperCustomerApp: List<FragmentDFPattern> by lazy {
         mutableListOf<FragmentDFPattern>().apply {
             /**
              * You can add the map of the fragment of a specific module here
@@ -41,6 +42,25 @@ object FragmentDFMapper {
         }
     }
 
+    val fragmentDfModuleMapperSellerApp: List<FragmentDFPattern> by lazy {
+        mutableListOf<FragmentDFPattern>().apply {
+            /**
+             * You can add the map of the fragment of a specific module here
+             * For example
+             * add(FragmentDFPattern(
+            logic to match the given className E.g.: { it == FRAGMENT_CLASS_PATH },
+            MODULE_ID,
+            MODULE_NAME_RESOURCE_ID
+            ))
+             */
+            add(FragmentDFPattern(
+                    {it == REVIEW_SHOP_FRAGMENT},
+                    DeeplinkDFMapper.DF_BASE_SELLER_APP,
+                    R.string.title_review_shop
+            ))
+        }
+    }
+
     private fun getSplitManager(context: Context): SplitInstallManager? {
         if (manager === null) {
             manager = SplitInstallManagerFactory.create(context.applicationContext)
@@ -50,29 +70,40 @@ object FragmentDFMapper {
 
     @JvmStatic
     fun checkIfFragmentIsInstalled(context: Context, className: String): Boolean {
-        val moduleId = fragmentDfModuleMapper.firstOrNull {
-            it.logic(className)
-        }?.moduleId.orEmpty()
+        val matchedFragmentDFPattern = getMatchedFragmentDFPattern(className)
+        val moduleId = matchedFragmentDFPattern?.moduleId.orEmpty()
         return getSplitManager(context)?.installedModules?.contains(moduleId) ?: false
     }
 
+    private fun getMatchedFragmentDFPattern(
+            className: String,
+    ): FragmentDFPattern? {
+        val fragmentDfModuleMapper = if (GlobalConfig.isSellerApp()) {
+            fragmentDfModuleMapperSellerApp
+        } else {
+            fragmentDfModuleMapperCustomerApp
+        }
+        return fragmentDfModuleMapper.firstOrNull {
+            it.logic(className)
+        }
+    }
+
     @JvmStatic
-    fun getFragmentDFDownloader(activity: AppCompatActivity, classPathName: String, extras: Bundle): Fragment? {
-        getSplitManager(activity)?.let {
-            fragmentDfModuleMapper.firstOrNull {
-                it.logic(classPathName)
-            }?.let{ fragmentDfMapper ->
+    fun getFragmentDFDownloader(activity: AppCompatActivity, className: String, extras: Bundle): Fragment? {
+        return getSplitManager(activity)?.let {
+            val matchedFragmentDFPattern = getMatchedFragmentDFPattern(className)
+            matchedFragmentDFPattern?.let { fragmentDfMapper ->
                 val moduleId = fragmentDfMapper.moduleId
                 val moduleName = activity.getString(fragmentDfMapper.moduleNameResourceId)
                 val bundle = Bundle().apply {
                     putString(BUNDLE_KEY_MODULE_ID, moduleId)
                     putString(BUNDLE_KEY_MODULE_NAME, moduleName)
-                    putString(BUNDLE_KEY_CLASS_NAME, classPathName)
+                    putString(BUNDLE_KEY_CLASS_NAME, className)
                     putBundle(BUNDLE_ARGUMENTS_KEY_EXTRAS, extras)
                 }
-                return RouteManager.instantiateFragment(activity, DF_INSTALLER_FRAGMENT_CLASS_PATH, bundle)
+                RouteManager.instantiateFragment(activity, DF_INSTALLER_FRAGMENT_CLASS_PATH, bundle)
             }
-        } ?: return null
+        }
     }
 
 }
