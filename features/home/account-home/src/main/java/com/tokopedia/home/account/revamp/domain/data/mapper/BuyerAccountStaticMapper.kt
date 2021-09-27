f15d37cdc386090f8c11ed8865a868e0aae17cc9
ramawidragama <rama.putra@tokopedia.com>
2021-09-27 14:50:21 +0700

    add affiliates when error

--- features/home/account-home/src/main/java/com/tokopedia/home/account/revamp/domain/data/mapper/BuyerAccountStaticMapper.kt
+++ features/home/account-home/src/main/java/com/tokopedia/home/account/revamp/domain/data/mapper/BuyerAccountStaticMapper.kt
@@ -96,6 +96,15 @@ class BuyerAccountStaticMapper @Inject constructor(
             title = context.getString(R.string.title_menu_favorites)
         })
 
+        viewItems.add(MenuListViewModel().apply {
+            menu = context.getString(R.string.title_menu_tokopedia_affiliates)
+            menuDescription = context.getString(R.string.label_menu_tokopedia_affiliates)
+            titleTrack = AccountConstants.Analytics.PEMBELI
+            sectionTrack = context.getString(R.string.title_menu_favorites)
+            applink = ApplinkConst.AFFILIATE
+            isBeta = true
+        })
+
         if (remoteConfig.getBoolean("mainapp_enable_interest_pick", true)) {
             viewItems.add(MenuListViewModel().apply {
                 menu = context.getString(R.string.title_menu_favorite_topic)
