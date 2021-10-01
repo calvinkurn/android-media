2cd102add15b7878b34a7820ca939ec1eae9f109
Reivin <reivin.oktavianus@tokopedia.com>
2021-10-01 11:25:06 +0700

    Update query to use trimIndent

--- features/merchant/seller_home/src/main/java/com/tokopedia/centralizedpromo/domain/usecase/VoucherCashbackEligibleUseCase.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/centralizedpromo/domain/usecase/VoucherCashbackEligibleUseCase.kt
@@ -15,22 +15,25 @@ class VoucherCashbackEligibleUseCase @Inject constructor(
 ) : GraphqlUseCase<MerchantPromotionGetPromoListResponse>(repository) {
 
     companion object {
-        private const val QUERY = "query GetPromoList(\$shopId: Int!) {\n" +
-                "  merchantPromotionGetPromoList(shop_id: \$shopId, tab_id: 0, search: \"\") {\n" +
-                "    header {\n" +
-                "      process_time\n" +
-                "      message\n" +
-                "      reason\n" +
-                "      error_code\n" +
-                "    }\n" +
-                "    data {\n" +
-                "      pages {\n" +
-                "        page_id\n" +
-                "        is_eligible\n" +
-                "      }\n" +
-                "    }\n" +
-                "  }\n" +
-                "}"
+
+        val QUERY = """
+            query GetPromoList(${'$'}shopId: Int!) {
+              merchantPromotionGetPromoList(shop_id: ${'$'}shopId, tab_id: 0, search: "") {
+                header {
+                  process_time
+                  message
+                  reason
+                  error_code
+                }
+                data {
+                  pages {
+                    page_id
+                    is_eligible
+                  }
+                }
+              }
+            }
+        """.trimIndent()
 
         private const val SHOP_ID_KEY = "shopId"
 
