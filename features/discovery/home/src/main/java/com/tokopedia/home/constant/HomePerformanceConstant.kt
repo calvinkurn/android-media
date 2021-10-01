81cde43c2339e3990a70c148ea60a594933d1f59
dhaba.widhikari <wikodhawi@gmail.com>
2021-10-01 13:32:32 +0700

    removed unused code

--- features/discovery/home/src/main/java/com/tokopedia/home/constant/HomePerformanceConstant.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/constant/HomePerformanceConstant.kt
@@ -5,6 +5,5 @@ package com.tokopedia.home.constant
  */
 object HomePerformanceConstant {
     const val KEY_PERFORMANCE_ON_CREATE_HOME = "mp_home_fragment_on_create_metrics"
-    const val KEY_PERFORMANCE_ON_START_HOME = "mp_home_fragment_on_start_metrics"
     const val KEY_PERFORMANCE_ON_RESUME_HOME = "mp_home_fragment_on_resume_metrics"
 }
