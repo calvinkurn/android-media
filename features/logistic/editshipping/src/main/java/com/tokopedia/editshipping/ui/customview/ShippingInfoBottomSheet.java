5b0d62ed031738c08d9bf9d8b4a847f99fdfcc03
hiradevina <fakhira.devina@tokopedia.com>
2021-10-01 18:42:05 +0700

    refactor: shippinginfobottomsheet

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/customview/ShippingInfoBottomSheet.java
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/customview/ShippingInfoBottomSheet.java
@@ -1,25 +1,25 @@
 package com.tokopedia.editshipping.ui.customview;
 
 import android.content.Context;
-import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.tokopedia.editshipping.R;
+import com.tokopedia.unifyprinciples.Typography;
 
 /**
  * Created by kris on 8/25/16. Tokopedia
  */
 public class ShippingInfoBottomSheet {
 
-    TextView courierInformation;
-    TextView serviceNameTextView;
+    Typography courierInformation;
+    Typography serviceNameTextView;
 
     public ShippingInfoBottomSheet(String information, String serviceName, Context activity) {
         BottomSheetDialog dialog = new BottomSheetDialog(activity);
         dialog.setContentView(R.layout.shipping_info_bottom_sheet);
 
-        courierInformation = (TextView) dialog.findViewById(R.id.courier_information);
-        serviceNameTextView =(TextView) dialog.findViewById(R.id.courier_name_service);
+        courierInformation = (Typography) dialog.findViewById(R.id.courier_information);
+        serviceNameTextView =(Typography) dialog.findViewById(R.id.courier_name_service);
 
         courierInformation.setText(information);
         serviceNameTextView.setText(serviceName);
