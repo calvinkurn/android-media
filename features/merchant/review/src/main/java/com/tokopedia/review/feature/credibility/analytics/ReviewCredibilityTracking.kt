f29fb493923c9187f5a5cbcce267ff2b037c7c03
leonard2907 <leonard.gunawan@tokopedia.com>
2021-10-05 17:19:59 +0700

    Fix click area in image preview, fix tracker and button size

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/credibility/analytics/ReviewCredibilityTracking.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/credibility/analytics/ReviewCredibilityTracking.kt
@@ -1,16 +1,18 @@
 package com.tokopedia.review.feature.credibility.analytics
 
 import com.tokopedia.review.common.analytics.ReviewTrackingConstant
+import com.tokopedia.review.feature.inbox.pending.presentation.fragment.ReviewPendingFragment
+import com.tokopedia.review.feature.reading.presentation.fragment.ReadReviewFragment
 import com.tokopedia.track.TrackApp
 
 object ReviewCredibilityTracking {
 
-    fun trackOnClickCTASelfCredibility(ctaValue: String, userId: String) {
+    fun trackOnClickCTASelfCredibility(ctaValue: String, userId: String, source: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(
             mapOf(
                 ReviewTrackingConstant.EVENT to ReviewCredibilityTrackingConstant.EVENT_CLICK_INBOX_REVIEW,
                 ReviewTrackingConstant.EVENT_ACTION to ReviewCredibilityTrackingConstant.EVENT_ACTION_CLICK_CTA_SELF,
-                ReviewTrackingConstant.EVENT_CATEGORY to ReviewCredibilityTrackingConstant.EVENT_CATEGORY_INBOX,
+                ReviewTrackingConstant.EVENT_CATEGORY to getEventCategoryBasedOnSource(source),
                 ReviewTrackingConstant.EVENT_LABEL to String.format(
                     ReviewCredibilityTrackingConstant.EVENT_LABEL_VALUE,
                     ctaValue
@@ -22,12 +24,12 @@ object ReviewCredibilityTracking {
         )
     }
 
-    fun trackOnClickCTAOtherUserCredibility(ctaValue: String, userId: String, productId: String) {
+    fun trackOnClickCTAOtherUserCredibility(ctaValue: String, userId: String, productId: String, source: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(
             mapOf(
                 ReviewTrackingConstant.EVENT to ReviewCredibilityTrackingConstant.EVENT_CLICK_PDP,
                 ReviewTrackingConstant.EVENT_ACTION to ReviewCredibilityTrackingConstant.EVENT_ACTION_CLICK_CTA_OTHER_USER,
-                ReviewTrackingConstant.EVENT_CATEGORY to ReviewCredibilityTrackingConstant.EVENT_CATEGORY_READING,
+                ReviewTrackingConstant.EVENT_CATEGORY to getEventCategoryBasedOnSource(source),
                 ReviewTrackingConstant.EVENT_LABEL to String.format(
                     ReviewCredibilityTrackingConstant.EVENT_LABEL_VALUE,
                     ctaValue
@@ -38,5 +40,13 @@ object ReviewCredibilityTracking {
                 ReviewTrackingConstant.KEY_PRODUCT_ID to productId
             )
         )
+    }
+
+    private fun getEventCategoryBasedOnSource(source: String): String {
+        return if (source == ReadReviewFragment.READING_SOURCE) {
+            ReviewCredibilityTrackingConstant.EVENT_CATEGORY_READING
+        } else {
+            ReviewPendingFragment.INBOX_SOURCE
+        }
     }
 }
