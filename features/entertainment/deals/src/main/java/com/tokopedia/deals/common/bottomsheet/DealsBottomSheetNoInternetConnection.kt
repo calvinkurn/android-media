4266e909396967acec4d5d684f0f409f7be53c44
Firmanda Mulyawan Nugroho <firmanda.nugroho@tokopedia.com>
2021-10-05 17:08:25 +0700

    fix dismiss error

--- features/entertainment/deals/src/main/java/com/tokopedia/deals/common/bottomsheet/DealsBottomSheetNoInternetConnection.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/common/bottomsheet/DealsBottomSheetNoInternetConnection.kt
@@ -24,7 +24,6 @@ class DealsBottomSheetNoInternetConnection {
             }
         }
         bottomSheet.setOnDismissListener {
-            bottomSheet.dismiss()
             listener.onDismissBottomsheet()
         }
         bottomSheet.setChild(binding.root)
