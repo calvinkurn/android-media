b213cd1fa277b3b6c553eaee5a24a6bdd1355af8
Reivin <reivin.oktavianus@tokopedia.com>
2021-10-06 01:57:57 +0700

    Remove ktx in seller_menu_common module

--- features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/view/viewholder/SellerSettingsTitleViewHolder.kt
+++ features/merchant/seller_menu_common/src/main/java/com/tokopedia/seller/menu/common/view/viewholder/SellerSettingsTitleViewHolder.kt
@@ -3,10 +3,9 @@ package com.tokopedia.seller.menu.common.view.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.seller.menu.common.R
+import com.tokopedia.seller.menu.common.databinding.SellerMenuSettingsTitleBinding
 import com.tokopedia.seller.menu.common.view.uimodel.SellerSettingsTitleUiModel
-import com.tokopedia.unifyprinciples.Typography
 
 class SellerSettingsTitleViewHolder(itemView: View) : AbstractViewHolder<SellerSettingsTitleUiModel>(itemView) {
 
@@ -15,11 +14,12 @@ class SellerSettingsTitleViewHolder(itemView: View) : AbstractViewHolder<SellerS
         val LAYOUT = R.layout.seller_menu_settings_title
     }
 
-    private val settingTitleIcon: IconUnify? = itemView.findViewById(R.id.settingTitleIcon)
-    private val settingTitleText: Typography? = itemView.findViewById(R.id.settingTitleText)
+    private val binding = SellerMenuSettingsTitleBinding.bind(itemView)
 
     override fun bind(data: SellerSettingsTitleUiModel) {
-        data.iconUnify?.let { settingTitleIcon?.setImage(it) }
-        settingTitleText?.text = data.settingTitle
+        data.iconUnify?.let {
+            binding.settingTitleIcon.setImage(it)
+        }
+        binding.settingTitleText.text = data.settingTitle
     }
 }
