c299bc485ce1ad1b90ecce58d86289492177450b
irfan khoirul <irfankhoirul@gmail.com>
2021-10-06 12:26:55 +0700

    change to string and set default id value

--- features/transaction/atc_common/src/main/java/com/tokopedia/atc_common/data/model/request/AddToCartBundleRequestParams.kt
+++ features/transaction/atc_common/src/main/java/com/tokopedia/atc_common/data/model/request/AddToCartBundleRequestParams.kt
@@ -1,36 +1,33 @@
 package com.tokopedia.atc_common.data.model.request
 
-import android.annotation.SuppressLint
 import com.google.gson.annotations.SerializedName
-import java.math.BigInteger
 
 data class AddToCartBundleRequestParams(
         @SerializedName("shop_id")
-        var shopId: String = "",
+        var shopId: String = "0",
         @SerializedName("bundle_id")
-        var bundleId: String = "",
+        var bundleId: String = "0",
         @SerializedName("bundle_qty")
         var bundleQty: Int = 0,
         @SerializedName("selected_product_pdp")
-        var selectedProductPdp: String = "",
+        var selectedProductPdp: String = "0",
         @SerializedName("product_detail")
         var productDetails: List<ProductDetail> = emptyList()
 )
 
 data class ProductDetail(
         @SerializedName("product_id")
-        var productId: String = "",
+        var productId: String = "0",
         @SerializedName("quantity")
         var quantity: Int = 0,
         @SerializedName("notes")
         var notes: String = "",
         @SerializedName("shop_id")
-        val shopId: String = "",
-        @SuppressLint("Invalid Data Type")
+        val shopId: String = "0",
         @SerializedName("warehouse_id")
-        val warehouseId: BigInteger = 0.toBigInteger(),
+        val warehouseId: String = "0",
         @SerializedName("is_product_parent")
         val isProductParent: Boolean = false,
         @SerializedName("customer_id")
-        val customerId: String = ""
+        val customerId: String = "0"
 )
