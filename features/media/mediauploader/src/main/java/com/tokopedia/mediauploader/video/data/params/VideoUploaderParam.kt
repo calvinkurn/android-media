b3224fc2f330a2b3e458e989b8dc7e39433d27c6
isfaaghyth <isfaaghyth@gmail.com>
2021-10-06 07:47:04 +0700

    simplified param builder

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/VideoUploaderParam.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/VideoUploaderParam.kt
@@ -1,11 +1,10 @@
 package com.tokopedia.mediauploader.video.data.params
 
-import com.tokopedia.mediauploader.common.data.entity.SourcePolicy
 import com.tokopedia.mediauploader.common.data.params.CommonParam
 import com.tokopedia.mediauploader.common.data.params.ParamValidator
 import com.tokopedia.mediauploader.common.data.params.SourceIdParam
-import com.tokopedia.mediauploader.data.state.ProgressCallback
 import com.tokopedia.mediauploader.common.util.UploadRequestBody
+import com.tokopedia.mediauploader.data.state.ProgressCallback
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import java.io.File
@@ -40,13 +39,14 @@ data class VideoUploaderParam(
         private const val SUPPORTED_CONTENT_TYPE = "video/*"
 
         fun create(
-            fileToUpload: File,
-            policy: SourcePolicy,
-            sourceId: String
+            fileToUpload: String,
+            timeOutUpload: String,
+            sourceIdUpload: String
         ) = VideoUploaderParam().apply {
-            uploadUrl = "https://vod.tokopedia.com/v2/video/upload/simple/$sourceId"
-            timeOut = policy.timeOut.toString()
-            filePath = fileToUpload.path
+            uploadUrl = "https://vod.tokopedia.com/v2/video/upload/simple/$sourceIdUpload"
+            timeOut = timeOutUpload
+            filePath = fileToUpload
+            sourceId = sourceIdUpload
         }
 
     }
