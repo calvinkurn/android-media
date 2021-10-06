a21ea07c92e45023e3f4da6826d828ef407c6e9e
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2021-10-06 19:57:14 +0700

    make unifyorderhistory DF ready

--- libraries/applink/src/main/java/com/tokopedia/applink/FragmentConst.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/FragmentConst.kt
@@ -8,4 +8,5 @@ object FragmentConst {
     const val FEED_PLUS_CONTAINER_FRAGMENT = "com.tokopedia.feedplus.view.fragment.FeedPlusContainerFragment"
     const val CART_FRAGMENT = "com.tokopedia.cart.view.CartFragment"
     const val DF_INSTALLER_FRAGMENT_CLASS_PATH = "com.tokopedia.dynamicfeatures.DFInstallerFragment"
+    const val UOH_LIST_FRAGMENT = "com.tokopedia.unifyorderhistory.view.fragment.UohListFragment"
 }
