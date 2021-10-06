440a6c1e699ceddb5f277ae8e1245e54a50d30b2
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-10-06 18:38:03 +0700

    update parent reply parse contract

--- features/user/chat_common/src/main/java/com/tokopedia/chat_common/data/parentreply/ParentReply.kt
+++ features/user/chat_common/src/main/java/com/tokopedia/chat_common/data/parentreply/ParentReply.kt
@@ -4,27 +4,27 @@ import com.google.gson.annotations.SerializedName
 import com.tokopedia.kotlin.extensions.view.toLongOrZero
 
 data class ParentReply(
-    @SerializedName("attachment_id")
+    @SerializedName("attachmentID", alternate = ["attachment_id"])
     val attachmentId: String = "",
-    @SerializedName("attachment_type")
+    @SerializedName("attachmentType", alternate = ["attachment_type"])
     val attachmentType: String = "",
     @SerializedName("name")
     val name: String = "",
-    @SerializedName("sender_id")
+    @SerializedName("senderID", alternate = ["sender_id"])
     val senderId: String = "",
-    @SerializedName("reply_time")
+    @SerializedName("replyTimeUnixNano", alternate = ["reply_time"])
     val replyTime: String = "",
-    @SerializedName("fraud_status")
+    @SerializedName("fraudStatus", alternate = ["fraud_status"])
     val fraudStatus: Int = 0,
-    @SerializedName("main_text")
+    @SerializedName("mainText", alternate = ["main_text"])
     val mainText: String = "",
-    @SerializedName("sub_text")
+    @SerializedName("subText", alternate = ["sub_text"])
     val subText: String = "",
-    @SerializedName("image_url")
+    @SerializedName("imageURL", alternate = ["image_url"])
     val imageUrl: String = "",
-    @SerializedName("local_id")
+    @SerializedName("localID", alternate = ["local_id"])
     val localId: String = "",
-    @SerializedName("is_expired")
+    @SerializedName("isExpired", alternate = ["is_expired"])
     val isExpired: Boolean = false,
     @SerializedName("source")
     val source: String = "",
