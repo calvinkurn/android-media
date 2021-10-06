b588e93f81e74d967e8d71e61cac10866b1960dd
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-10-06 17:36:36 +0700

    fix pop up issue

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/utils/GoldMerchantUtil.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/utils/GoldMerchantUtil.kt
@@ -48,10 +48,14 @@ object GoldMerchantUtil {
             val simpleDateFormat =
                 SimpleDateFormat(PATTERN_DATE_SHOP_INFO, DateFormatUtils.DEFAULT_LOCALE)
             val calendar = Calendar.getInstance(DateFormatUtils.DEFAULT_LOCALE)
+            val calendarExistingSeller = Calendar.getInstance(DateFormatUtils.DEFAULT_LOCALE)
             val shopAge = totalDays(dateString).toInt()
             simpleDateFormat.parse(dateString)?.let { calendar.time = it }
             calendar.add(Calendar.DATE, shopAge)
-            return if (shopAge in NEW_SELLER_DAYS..SHOP_AGE_NINETY_FOUR) {
+            calendarExistingSeller.add(Calendar.DATE, NEW_SELLER_DAYS)
+            return if (shopAge in NEW_SELLER_DAYS..SHOP_AGE_NINETY_FOUR &&
+                calendarExistingSeller.get(Calendar.DAY_OF_WEEK) > Calendar.MONDAY
+            ) {
                 calendar.get(Calendar.DAY_OF_WEEK) > Calendar.MONDAY
             } else {
                 false
