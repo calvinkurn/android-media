20ce701d8a4760dc52a9d6893c3cb8268ca546c1
hendrysetiadi89 <hendry.setiadi@tokopedia.com>
2021-10-08 12:14:31 +0700

    fix redirect create shop applink

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -465,7 +465,6 @@ public interface ApplinkConst {
     interface SellerApp {
         String PRODUCT_ADD = "sellerapp://product/add";
         String SHOP_PAGE_PRODUCTS_CREATE_SHOWCASE = "sellerapp://shop/showcase-create";
-        String SALES = "sellerapp://sales";
         String TOPADS_CREDIT = "sellerapp://topads/buy";
         String TOPADS_AUTO_TOPUP = "sellerapp://topads/auto-topup";
         String TOPADS_PRODUCT_CREATE = "sellerapp://topads/create";
