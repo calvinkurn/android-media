174f1255bc50213f235e8d76be68adc934932cf5
Rohan <rohan.ghosh@tokopedia.com>
2021-10-11 10:41:41 +0530

    Added the visibility for the recommended text

--- features/fintech/layanan_finansial/src/main/java/com/tokopedia/layanan_finansial/view/viewHolder/TopAdsViewHolder.kt
+++ features/fintech/layanan_finansial/src/main/java/com/tokopedia/layanan_finansial/view/viewHolder/TopAdsViewHolder.kt
@@ -2,9 +2,8 @@ package com.tokopedia.layanan_finansial.view.viewHolder
 
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SnapHelper
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.applink.RouteManager
@@ -14,6 +13,7 @@ import com.tokopedia.layanan_finansial.view.models.TopAdsImageModel
 import com.tokopedia.topads.sdk.domain.model.TopAdsImageViewModel
 import com.tokopedia.topads.sdk.listener.TopAdsImageVieWApiResponseListener
 import com.tokopedia.topads.sdk.widget.TopAdsImageView
+import com.tokopedia.unifyprinciples.Typography
 
 class TopAdsViewHolder(val view: View): AbstractViewHolder<Visitable<*>>(view) {
 
@@ -24,6 +24,7 @@ class TopAdsViewHolder(val view: View): AbstractViewHolder<Visitable<*>>(view) {
             R.id.topAdsImagesRecycler
         )
     }
+    private val recommendedText: Typography by lazy { itemView.findViewById(R.id.recommendedText) }
 
     companion object{
         val LAYOUT:Int = R.layout.layout_topads_view
@@ -41,6 +42,7 @@ class TopAdsViewHolder(val view: View): AbstractViewHolder<Visitable<*>>(view) {
                 val imageList= imageDataList as ArrayList<TopAdsImageModel>
                 if (imageDataList.isNotEmpty())
                 {
+                    recommendedText.visibility = View.VISIBLE
                     generateAdsCarousal(imageList)
                 }
             }
@@ -58,8 +60,7 @@ class TopAdsViewHolder(val view: View): AbstractViewHolder<Visitable<*>>(view) {
             LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false)
         val topAdsAdapter = TopAdsAdapter(imageList, ::onClick)
         displayRecycler.adapter = topAdsAdapter
-        val snapHelper = LinearSnapHelper()
-        snapHelper.attachToRecyclerView(displayRecycler)
+        PagerSnapHelper().attachToRecyclerView(displayRecycler)
     }
 
     private fun onClick(appLink: String) {
