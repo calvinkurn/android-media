5ef78a1dcde0fe4740cc4ad3ad8e8f7b4ba9dffb
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-10-12 01:17:15 +0800

    use binding

--- features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/view/fragment/SettingFingerprintFragment.kt
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/view/fragment/SettingFingerprintFragment.kt
@@ -18,6 +18,7 @@ import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.loginfingerprint.R
 import com.tokopedia.loginfingerprint.data.model.CheckFingerprintResult
 import com.tokopedia.loginfingerprint.data.model.RegisterFingerprintResult
+import com.tokopedia.loginfingerprint.databinding.FragmentSettingFingerprintBinding
 import com.tokopedia.loginfingerprint.di.LoginFingerprintComponent
 import com.tokopedia.loginfingerprint.listener.AuthenticationFingerprintCallback
 import com.tokopedia.loginfingerprint.tracker.BiometricTracker
@@ -28,7 +29,7 @@ import com.tokopedia.unifycomponents.Toaster
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
-import kotlinx.android.synthetic.main.fragment_setting_fingerprint.*
+import com.tokopedia.utils.view.binding.viewBinding
 import javax.inject.Inject
 
 
@@ -44,13 +45,12 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
     @Inject
     lateinit var userSession: UserSessionInterface
 
-//    @Inject
-//    lateinit var fingerprintPreference: FingerprintPreference
-//
     @Inject
     lateinit var tracker: BiometricTracker
 
     private var enableSwitch = true
+
+    private val binding: FragmentSettingFingerprintBinding? by viewBinding()
 
     override fun getScreenName(): String = TAG
 
@@ -69,8 +69,7 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
             initObserver()
             loading()
             viewModel.getFingerprintStatus()
-
-            fragment_fingerprint_setting_switch?.setOnCheckedChangeListener { switch, isEnable ->
+            binding?.fragmentFingerprintSettingSwitch?.setOnCheckedChangeListener { switch, isEnable ->
                 if (enableSwitch) {
                     if (isEnable) {
                         enableSwitch = false
@@ -102,12 +101,12 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
             when(it) {
                 is Success -> {
                     enableSwitch = false
-                    fragment_fingerprint_setting_switch?.isChecked = false
+                    binding?.fragmentFingerprintSettingSwitch?.isChecked = false
                     tracker.trackRemoveFingerprintSuccess()
                 }
                 is Fail -> {
                     enableSwitch = false
-                    fragment_fingerprint_setting_switch?.isChecked = true
+                    binding?.fragmentFingerprintSettingSwitch?.isChecked = true
                     tracker.trackRemoveFingerprintFailed(it.throwable.message ?: "")
                     showToaster(it.throwable.message)
                 }
@@ -120,7 +119,7 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
                 is Success -> onSuccessRegisterFingerprint(it.data)
                 is Fail -> {
                     enableSwitch = false
-                    fragment_fingerprint_setting_switch?.isChecked = false
+                    binding?.fragmentFingerprintSettingSwitch?.isChecked = false
                     onErrorRegisterFingerprint(throwable = it.throwable)
                 }
             }
@@ -169,7 +168,7 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
         if (data.success) {
             tracker.trackRegisterFpSuccess()
             enableSwitch = false
-            fragment_fingerprint_setting_switch?.isChecked = true
+            binding?.fragmentFingerprintSettingSwitch?.isChecked = true
             view?.let {
                 Toaster.build(it, getString(R.string.fingerprint_success_login_toaster), Toaster.LENGTH_LONG).show()
             }
@@ -187,7 +186,7 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
 
     fun onSuccessGetFingerprintStatus(checkFingerprintResponse: CheckFingerprintResult) {
         enableSwitch = false
-        fragment_fingerprint_setting_switch?.isChecked = checkFingerprintResponse.isRegistered
+        binding?.fragmentFingerprintSettingSwitch?.isChecked = checkFingerprintResponse.isRegistered
         enableSwitch = true
     }
 
@@ -196,13 +195,13 @@ class SettingFingerprintFragment(val listener: AuthenticationFingerprintCallback
     }
 
     fun loading() {
-        fragment_fingerprint_setting_container?.alpha = LESS_ALPHA
-        fragment_fingerprint_setting_loader.show()
+        binding?.fragmentFingerprintSettingContainer?.alpha = LESS_ALPHA
+        binding?.fragmentFingerprintSettingLoader?.show()
     }
 
     fun hideLoading() {
-        fragment_fingerprint_setting_container?.alpha = NORMAL_ALPHA
-        fragment_fingerprint_setting_loader.hide()
+        binding?.fragmentFingerprintSettingContainer?.alpha = NORMAL_ALPHA
+        binding?.fragmentFingerprintSettingLoader?.hide()
     }
 
     private fun onSuccessAuthentication() {
