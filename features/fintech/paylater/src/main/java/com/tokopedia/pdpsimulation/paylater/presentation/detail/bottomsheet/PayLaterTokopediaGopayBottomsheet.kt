14521ecdf65722465ac3f900587f3904ed04342d
Rohan <rohan.ghosh@tokopedia.com>
2021-10-12 16:32:04 +0530

    Added the bottom sheet UI and class

---
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/presentation/detail/bottomsheet/PayLaterTokopediaGopayBottomsheet.kt
@@ -0,0 +1,55 @@
+package com.tokopedia.pdpsimulation.paylater.presentation.detail.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.tokopedia.kotlin.extensions.view.getScreenHeight
+import com.tokopedia.pdpsimulation.R
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.unifycomponents.toDp
+
+class PayLaterTokopediaGopayBottomsheet : BottomSheetUnify() {
+
+    init {
+        setShowListener {
+            bottomSheet.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(view: View, slideOffset: Float) {
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                        dismiss()
+                    }
+                }
+            })
+        }
+    }
+
+    private val childLayoutRes = R.layout.paylater_card_activation_purchase
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setDefaultParams()
+        initBottomSheet()
+
+    }
+
+    private fun initBottomSheet() {
+        val childView = LayoutInflater.from(context).inflate(
+            childLayoutRes,
+            null, false
+        )
+        setChild(childView)
+    }
+
+    private fun setDefaultParams() {
+        isDragable = true
+        isHideable = true
+        showCloseIcon = true
+        showHeader = true
+        customPeekHeight = (getScreenHeight()).toDp()
+    }
+
+}
