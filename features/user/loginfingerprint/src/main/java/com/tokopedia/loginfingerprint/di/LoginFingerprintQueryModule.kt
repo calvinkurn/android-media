e545be9e5fd7463caf0627d5e4ea114e7a500b42
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-10-12 13:42:19 +0800

    remove unused code

--- features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/di/LoginFingerprintQueryModule.kt
+++ features/user/loginfingerprint/src/main/java/com/tokopedia/loginfingerprint/di/LoginFingerprintQueryModule.kt
@@ -9,37 +9,9 @@ import dagger.Provides
 @Module
 class LoginFingerprintQueryModule {
 
-//    @LoginFingerprintSettingScope
-//    @Provides
-//    fun provideRegisterFingerprintUsecase(graphqlRepository: GraphqlRepository, dispatchers: CoroutineDispatchers, fingerprintPreference: FingerprintPreference): RegisterFingerprintUseCase {
-//        val useCase = GraphqlUseCase<RegisterFingerprintPojo>(graphqlRepository)
-//        return RegisterFingerprintUseCase(useCase, dispatchers, fingerprintPreference)
-//    }
-//
-//    @LoginFingerprintSettingScope
-//    @Provides
-//    fun provideVerifyFingerprintUsecase(graphqlRepository: GraphqlRepository, dispatchers: CoroutineDispatchers, fingerprintPreference: FingerprintPreference): VerifyFingerprintUseCase {
-//        val useCase = GraphqlUseCase<VerifyFingerprintPojo>(graphqlRepository)
-//        return VerifyFingerprintUseCase(useCase, dispatchers, fingerprintPreference)
-//    }
-
-//    @LoginFingerprintSettingScope
-//    @Provides
-//    fun provideCheckFingerprintToggleUsecase(graphqlRepository: GraphqlRepository, dispatchers: CoroutineDispatchers, fingerprintPreference: FingerprintPreference): CheckFingerprintToggleStatusUseCase {
-//        val useCase = GraphqlUseCase<CheckFingerprintPojo>(graphqlRepository)
-//        return CheckFingerprintToggleStatusUseCase(useCase, dispatchers, fingerprintPreference)
-//    }
-
     @LoginFingerprintSettingScope
     @Provides
     fun provideLoginTokenUseCase(graphqlRepository: GraphqlRepository)
             : GraphqlUseCase<LoginTokenPojo> = GraphqlUseCase(graphqlRepository)
-
-//    @LoginFingerprintSettingScope
-//    @Provides
-//    fun provideRemoveFingerprintUseCase(graphqlRepository: GraphqlRepository, dispatchers: CoroutineDispatchers, fingerprintPreference: FingerprintPreference): RemoveFingerprintUsecase {
-//        val useCase = GraphqlUseCase<RemoveFingerprintPojo>(graphqlRepository)
-//        return RemoveFingerprintUsecase(useCase, dispatchers, fingerprintPreference)
-//    }
 
 }
