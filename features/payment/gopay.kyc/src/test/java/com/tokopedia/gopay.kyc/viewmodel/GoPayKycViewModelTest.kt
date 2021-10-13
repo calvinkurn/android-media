e860dcaaad8b4ff55764a94dc5c294b7c358f1c9
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2021-10-13 16:38:02 +0530

    Fixing unit test

--- features/payment/gopay.kyc/src/test/java/com/tokopedia/gopay.kyc/viewmodel/GoPayKycViewModelTest.kt
+++ features/payment/gopay.kyc/src/test/java/com/tokopedia/gopay.kyc/viewmodel/GoPayKycViewModelTest.kt
@@ -1,15 +1,12 @@
 package com.tokopedia.gopay.kyc.viewmodel
 
-import android.graphics.Bitmap
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.otaliastudios.cameraview.BitmapCallback
-import com.otaliastudios.cameraview.CameraUtils
+import com.tokopedia.gopay.kyc.domain.data.CameraImageResult
 import com.tokopedia.gopay.kyc.domain.data.KycStatusData
 import com.tokopedia.gopay.kyc.domain.data.KycStatusResponse
 import com.tokopedia.gopay.kyc.domain.usecase.CheckKycStatusUseCase
 import com.tokopedia.gopay.kyc.domain.usecase.SaveCaptureImageUseCase
-import com.tokopedia.usecase.coroutines.Result
 import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -74,6 +71,34 @@ class GoPayKycViewModelTest {
         }
         viewModel.checkKycStatus()
         assert(viewModel.kycEligibilityStatus.value?.isEligible == data.kycStatusData.isEligible)
+    }
+
+    @Test
+    fun `Execute processAndSaveImage error Exception`() {
+        coEvery { saveCaptureImageUseCase.parseAndSaveCapture(any(), any(), any()) } coAnswers {
+            secondArg<(Throwable) -> Unit>().invoke(mockThrowable)
+        }
+        viewModel.processAndSaveImage(ByteArray(1))
+        assert(viewModel.captureErrorLiveData.value is Throwable)
+    }
+
+    @Test
+    fun `Execute processAndSaveImage bytearray null`() {
+        viewModel.processAndSaveImage(null)
+        assert((viewModel.captureErrorLiveData.value as Throwable).message == "Empty Data byte")
+    }
+
+    @Test
+    fun `Execute processAndSaveImage code Success`() {
+        val cameraImageResult = CameraImageResult(1080,1080, "/path", listOf())
+        coEvery { saveCaptureImageUseCase.parseAndSaveCapture(any(), any(), any()) } coAnswers {
+            firstArg<(CameraImageResult) -> Unit>().invoke(cameraImageResult)
+        }
+        viewModel.processAndSaveImage(ByteArray(1))
+        assert((viewModel.cameraImageResultLiveData.value as CameraImageResult).bitmapHeight == 1080)
+        assert((viewModel.cameraImageResultLiveData.value as CameraImageResult).bitmapWidth == 1080)
+        assert((viewModel.cameraImageResultLiveData.value as CameraImageResult).finalCameraResultPath == "/path")
+
     }
 
 }
