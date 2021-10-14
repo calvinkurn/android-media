94c5309a47b8f1cb960a4c35e9278f9e683398d2
isfaaghyth <isfaaghyth@gmail.com>
2021-10-15 04:08:10 +0700

    improved image uploader param

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/image/data/params/ImageUploaderParam.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/image/data/params/ImageUploaderParam.kt
@@ -1,47 +1,29 @@
 package com.tokopedia.mediauploader.image.data.params
 
-import com.tokopedia.mediauploader.common.data.entity.SourcePolicy
-import com.tokopedia.mediauploader.common.data.params.CommonParam
-import com.tokopedia.mediauploader.common.data.params.ParamValidator
 import com.tokopedia.mediauploader.common.state.ProgressCallback
-import com.tokopedia.mediauploader.common.util.UploadRequestBody
-import okhttp3.MediaType
-import okhttp3.MultipartBody
+import com.tokopedia.mediauploader.common.util.fileBody
 import java.io.File
 
 data class ImageUploaderParam(
-    override var uploadUrl: String = "",
-    override var filePath: String = "",
-    override var timeOut: String = ""
-) : CommonParam, ParamValidator() {
+    var hostUrl: String,
+    var sourceId: String,
+    var file: File,
+    var timeOut: String
+) {
 
-    fun imageBody(
-        progressCallback: ProgressCallback?
-    ): MultipartBody.Part {
-        val file = File(filePath)
-        val contentType = MediaType.parse(SUPPORTED_CONTENT_TYPE)
-        val requestBody = UploadRequestBody(file, contentType, progressCallback)
-        return MultipartBody.Part.createFormData(BODY_FILE_UPLOAD, file.name, requestBody)
+    fun hasNoParams(): Boolean {
+        return hostUrl.isEmpty() && !file.exists() && timeOut.isEmpty()
     }
+
+    fun fileBody(progressCallback: ProgressCallback?) = file.fileBody(
+        type = SUPPORTED_CONTENT_TYPE,
+        bodyName = BODY_FILE_UPLOAD,
+        progressCallback = progressCallback
+    )
 
     companion object {
         private const val BODY_FILE_UPLOAD = "file_upload"
         private const val SUPPORTED_CONTENT_TYPE = "image/*"
-
-        fun imageUploadUrl(baseUrl: String, sourceId: String): String {
-            return "$baseUrl/v1/upload/image/$sourceId"
-        }
-
-        fun create(
-            fileToUpload: File,
-            policy: SourcePolicy,
-            sourceId: String
-        ) = ImageUploaderParam().apply {
-            uploadUrl = imageUploadUrl(policy.host, sourceId)
-            timeOut = policy.timeOut.toString()
-            filePath = fileToUpload.path
-        }
-
     }
 
 }
