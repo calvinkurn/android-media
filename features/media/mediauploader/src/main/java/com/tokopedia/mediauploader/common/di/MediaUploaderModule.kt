ce7feb7426bb720a2ed9e4d12cb547fff7f479a3
isfaaghyth <isfaaghyth@gmail.com>
2021-10-15 02:03:57 +0700

    combined image and video di module

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/common/di/MediaUploaderModule.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/common/di/MediaUploaderModule.kt
@@ -2,23 +2,20 @@ package com.tokopedia.mediauploader.common.di
 
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.mediauploader.UploaderUseCase
-import com.tokopedia.mediauploader.image.di.ImageUploaderModule
+import com.tokopedia.mediauploader.image.data.ImageUploadServices
 import com.tokopedia.mediauploader.image.domain.GetImagePolicyUseCase
 import com.tokopedia.mediauploader.image.domain.GetImageUploaderUseCase
-import com.tokopedia.mediauploader.video.di.VideoUploaderModule
+import com.tokopedia.mediauploader.video.data.VideoUploadServices
+import com.tokopedia.mediauploader.video.domain.GetSingleVideoUploaderUseCase
+import com.tokopedia.mediauploader.video.domain.GetVideoPolicyUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 
-@Module(
-    includes = [
-        MediaUploaderNetworkModule::class,
-        ImageUploaderModule::class,
-        VideoUploaderModule::class
-    ]
-)
+@Module(includes = [MediaUploaderNetworkModule::class])
 class MediaUploaderModule {
 
     @Provides
@@ -36,6 +33,42 @@ class MediaUploaderModule {
         imageUploaderUseCase: GetImageUploaderUseCase
     ): UploaderUseCase {
         return UploaderUseCase(imagePolicyUseCase, imageUploaderUseCase)
+    }
+
+    // --- image ---
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideGetImagePolicyUseCase(
+        repository: GraphqlRepository
+    ): GetImagePolicyUseCase {
+        return GetImagePolicyUseCase(repository)
+    }
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideGetImageUploaderUseCase(
+        services: ImageUploadServices
+    ): GetImageUploaderUseCase {
+        return GetImageUploaderUseCase(services)
+    }
+
+    // --- video ---
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideGetVideoPolicyUseCase(
+        repository: GraphqlRepository
+    ): GetVideoPolicyUseCase {
+        return GetVideoPolicyUseCase(repository)
+    }
+
+    @Provides
+    @MediaUploaderQualifier
+    fun provideGetSingleVideoUploaderUseCase(
+        services: VideoUploadServices
+    ): GetSingleVideoUploaderUseCase {
+        return GetSingleVideoUploaderUseCase(services)
     }
 
 }
