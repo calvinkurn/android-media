a531967ca1598d1612bded4f8534e113c2a30509
isfaaghyth <isfaaghyth@gmail.com>
2021-10-15 04:06:31 +0700

    created request body util in common

---
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/common/util/requestbody.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.mediauploader.common.util
+
+import com.tokopedia.mediauploader.common.state.ProgressCallback
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
+
+fun String.requestBody(): RequestBody {
+    return RequestBody.create(MultipartBody.FORM, this)
+}
+
+fun File.fileBody(
+    type: String,
+    bodyName: String,
+    progressCallback: ProgressCallback? = null
+): MultipartBody.Part {
+    val contentType = MediaType.parse(type)
+    val requestBody = UploadRequestBody(this, contentType, progressCallback)
+    return MultipartBody.Part.createFormData(bodyName, this.name, requestBody)
+}
