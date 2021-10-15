1e8a281088383788c0548b17ae38b69105cb617a
misael-jonathan <misael.jonathan@tokopedia.com>
2021-10-15 16:15:08 +0700

    reconfig recharge homepage instrumentation test

--- features/recharge/digital_homepage/src/androidTest/java/com/tokopedia/digital/home/RechargeHomepageInstrumentTest.kt
+++ features/recharge/digital_homepage/src/androidTest/java/com/tokopedia/digital/home/RechargeHomepageInstrumentTest.kt
@@ -69,12 +69,12 @@ class RechargeHomepageInstrumentTest {
         check_favorite_section()
         check_category_section()
         check_trustmark_section()
-        check_reminder_section()
         check_dual_banners_section()
         check_lego_banners_section()
         check_product_cards_section()
         check_single_banner_section()
         check_product_banner_section()
+        check_reminder_section()
 
         assertThat(
                 getAnalyticsWithQuery(gtmLogDBSource, context, SUBHOME_ANALYTIC_VALIDATOR_QUERY),
@@ -156,9 +156,9 @@ class RechargeHomepageInstrumentTest {
 
     private fun check_dual_banners_section() {
         onView(withId(R.id.recycler_view)).perform(
-                RecyclerViewActions.scrollToPosition<RechargeHomepageDualBannersViewHolder>(8)
+                RecyclerViewActions.scrollToPosition<RechargeHomepageDualBannersViewHolder>(9)
         )
-        Thread.sleep(1000)
+        Thread.sleep(3000)
         onView(withId(R.id.view_recharge_home_dual_banners_container)).check(matches(isDisplayed()))
         onView(withId(R.id.view_recharge_home_dual_banners_image_1)).check(matches(isDisplayed())).perform(click())
         Thread.sleep(1000)
