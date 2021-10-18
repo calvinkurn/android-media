3a22a80c5874abb69703ec313c653bf9f5ff688a
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2021-10-18 15:27:44 +0530

    enable screenshot on saldo and withdrawal page

--- features/payment/withdraw/src/main/java/com/tokopedia/withdraw/saldowithdrawal/presentation/activity/WithdrawActivity.kt
+++ features/payment/withdraw/src/main/java/com/tokopedia/withdraw/saldowithdrawal/presentation/activity/WithdrawActivity.kt
@@ -41,15 +41,8 @@ class WithdrawActivity : BaseSimpleActivity(), WithdrawalFragmentCallback,
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setSecureWindowFlag()
         updateHeaderTitle(getString(R.string.swd_activity_withdraw))
         initInjector()
-    }
-
-    private fun setSecureWindowFlag() {
-        if (GlobalConfig.APPLICATION_TYPE == GlobalConfig.CONSUMER_APPLICATION || GlobalConfig.APPLICATION_TYPE == GlobalConfig.SELLER_APPLICATION) {
-            runOnUiThread { window.addFlags(WindowManager.LayoutParams.FLAG_SECURE) }
-        }
     }
 
     private fun initInjector() {
