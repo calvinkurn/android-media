f32f1169a49acb7e5ca54170aceb0cf1fc17ee38
lalittokopedia <lalit.singh@tokopedia.com>
2021-10-22 11:44:14 +0530

    Fix import issue

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/inApp/external/CmEventListener.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/inApp/external/CmEventListener.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.notifications.inApp.external
 
 import com.tokopedia.abstraction.base.view.fragment.lifecycle.FragmentLifecycleCallback
-import com.tokopedia.notifications.inApp.PushIntentContract
 
 object CmEventListener {
     val pushIntentContractList = arrayListOf<PushIntentContract>()
