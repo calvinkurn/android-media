2d9df37a93778e37b0dc80b8b67460d8508b07d4
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2021-10-26 08:48:04 +0700

    add cross sell model ui

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/bundle/domain/model/cartshipmentform/CrossSell.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.checkout.bundle.domain.model.cartshipmentform
-
-/**
- * Created by fwidjaja on 26/10/21.
- */
-data class CrossSell()
---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/bundle/view/uimodel/CrossSellModel.kt
@@ -0,0 +1,20 @@
+package com.tokopedia.checkout.bundle.view.uimodel
+
+import android.os.Parcelable
+import com.tokopedia.checkout.bundle.data.model.response.shipmentaddressform.CrossSellBottomSheet
+import com.tokopedia.checkout.bundle.data.model.response.shipmentaddressform.CrossSellInfoData
+import com.tokopedia.checkout.bundle.data.model.response.shipmentaddressform.CrossSellOrderSummary
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class CrossSellModel(
+        var id: String = "",
+        var checkboxDisabled: Boolean = false,
+        var isChecked: Boolean = false,
+        var price: Long = 0,
+        var info: CrossSellInfoModel = CrossSellInfoModel(),
+        var orderSummary: CrossSellOrderSummaryModel = CrossSellOrderSummaryModel(),
+        var bottomSheet: CrossSellBottomSheet = CrossSellBottomSheet(),
+        var additionalVerticalId: Long = 0,
+        var transactionType: String = ""
+) : Parcelable
