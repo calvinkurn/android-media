318c331d0f325649cb3d299640e15c209b7e237e
stevenfredian <steven.fredian@tokopedia.com>
2021-10-27 17:37:39 +0700

    handling device id null when init abtest

--- libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/abtest/AbTestPlatform.kt
+++ libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/abtest/AbTestPlatform.kt
@@ -137,9 +137,7 @@ class AbTestPlatform @JvmOverloads constructor (val context: Context): RemoteCon
         if (userSession.isLoggedIn) {
             payloads[ID] = userSession.userId
         } else {
-            if(userSession.deviceId == null){
-                return
-            }
+            if(handleDeviceIdless()) return
             payloads[ID] = userSession.deviceId
         }
         payloads[IRIS_SESSION_ID] = irisSession.getSessionId()
@@ -171,6 +169,12 @@ class AbTestPlatform @JvmOverloads constructor (val context: Context): RemoteCon
                     override fun onError(e: Throwable?) { }
 
                 }}
+    }
+
+    private fun handleDeviceIdless() :Boolean {
+        if(userSession.deviceId == null){
+            return true
+        }
     }
 
     private fun gqlResponseHandler(graphqlResponse: GraphqlResponse): RolloutFeatureVariants {
