a41006005ba97ef0a0217f6976e02daf6b0195ae
hiradevina <fakhira.devina@tokopedia.com>
2021-10-27 14:06:15 +0700

    refactor: move hardcoded constants

--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlist/view/viewmodel/WishlistV2ViewModel.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlist/view/viewmodel/WishlistV2ViewModel.kt
@@ -13,6 +13,7 @@ import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.wishlist.data.model.WishlistV2Params
 import com.tokopedia.wishlist.data.model.WishlistV2Response
 import com.tokopedia.wishlist.domain.WishlistV2UseCase
+import com.tokopedia.wishlist.util.WishlistV2Consts
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,8 +21,8 @@ import javax.inject.Inject
  * Created by fwidjaja on 15/10/21.
  */
 class WishlistV2ViewModel @Inject constructor(dispatcher: CoroutineDispatchers,
-                                             private val wishlistV2V2UseCase: WishlistV2UseCase,
-private val recommendationUseCase: GetRecommendationUseCase) : BaseViewModel(dispatcher.main) {
+                                              private val wishlistV2V2UseCase: WishlistV2UseCase,
+                                              private val recommendationUseCase: GetRecommendationUseCase) : BaseViewModel(dispatcher.main) {
 
     private val _wishlistV2Result = MutableLiveData<Result<WishlistV2Response.Data.WishlistV2>>()
     val wishlistV2Result: LiveData<Result<WishlistV2Response.Data.WishlistV2>>
@@ -43,8 +44,7 @@ private val recommendationUseCase: GetRecommendationUseCase) : BaseViewModel(dis
                 val data = recommendationUseCase.getData(
                     GetRecommendationRequestParam(
                         pageNumber = pageNumber,
-                        // todo move to constant
-                        pageName = "wishlist"
+                        pageName = WishlistV2Consts.PAGE_NAME
                     )
                 )
                 _recommendationResult.value = Success(data)
