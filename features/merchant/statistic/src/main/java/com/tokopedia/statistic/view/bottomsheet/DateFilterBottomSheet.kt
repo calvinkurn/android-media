f251586084aafdd010450bccf61fc6d18c58385d
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-10-27 14:11:30 +0800

    add exclusive feature tag on date filter

--- features/merchant/statistic/src/main/java/com/tokopedia/statistic/view/bottomsheet/DateFilterBottomSheet.kt
+++ features/merchant/statistic/src/main/java/com/tokopedia/statistic/view/bottomsheet/DateFilterBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.statistic.view.bottomsheet
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -10,7 +11,9 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.dpToPx
 import com.tokopedia.kotlin.extensions.view.isVisible
+import com.tokopedia.kotlin.extensions.view.setMargin
 import com.tokopedia.statistic.R
 import com.tokopedia.statistic.common.StatisticPageHelper
 import com.tokopedia.statistic.databinding.BottomsheetStcSelectDateRangeBinding
@@ -42,6 +45,8 @@ class DateFilterBottomSheet : BaseBottomSheet<BottomsheetStcSelectDateRangeBindi
         ): DateFilterBottomSheet {
             return DateFilterBottomSheet().apply {
                 clearContentPadding = true
+                showHeader = false
+                showCloseIcon = false
                 arguments = Bundle().apply {
                     putParcelableArrayList(KEY_DATE_FILTERS, ArrayList(dateFilters))
                     putString(KEY_IDENTIFIER_DESCRIPTION, identifierDescription)
@@ -66,7 +71,6 @@ class DateFilterBottomSheet : BaseBottomSheet<BottomsheetStcSelectDateRangeBindi
     ): View? {
         binding = BottomsheetStcSelectDateRangeBinding.inflate(inflater).apply {
             setChild(root)
-            setTitle(root.context.getString(R.string.stc_change_date_range))
         }
         return super.onCreateView(inflater, container, savedInstanceState)
     }
@@ -77,45 +81,15 @@ class DateFilterBottomSheet : BaseBottomSheet<BottomsheetStcSelectDateRangeBindi
     }
 
     override fun setupView() = binding?.run {
-        rvStcDateRage.run {
-            layoutManager = LinearLayoutManager(context)
-            adapter = mAdapter
-        }
+        root.setBackgroundColor(Color.TRANSPARENT)
+        view?.setBackgroundColor(Color.TRANSPARENT)
+        showFilterItems()
 
-        mAdapter?.clearAllElements()
-        mAdapter?.addElement(items)
+        icStcCloseDateFilter.setOnClickListener {
+            dismiss()
+        }
 
         showExclusiveIdentifier()
-    }
-
-    private fun showExclusiveIdentifier() {
-        activity?.let {
-            val userSession: UserSessionInterface = UserSession(it.applicationContext)
-            val isRegularMerchant = StatisticPageHelper.getRegularMerchantStatus(userSession)
-            binding?.rvStcDateRage?.addOnItemTouchListener(object :
-                RecyclerView.OnItemTouchListener {
-                override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
-                    return isRegularMerchant
-                }
-
-                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
-
-                }
-
-                override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
-
-                }
-            })
-            binding?.stcFilterExclusiveIdentifier?.isVisible = isRegularMerchant
-            binding?.stcFilterExclusiveIdentifier?.setOnCtaClickListener {
-                dismiss()
-            }
-            if (isRegularMerchant) {
-                val identifierDescription = arguments
-                    ?.getString(KEY_IDENTIFIER_DESCRIPTION).orEmpty()
-                binding?.stcFilterExclusiveIdentifier?.setDescription(identifierDescription)
-            }
-        }
     }
 
     override fun onItemDateRangeClick(model: DateFilterItem) {
@@ -162,6 +136,57 @@ class DateFilterBottomSheet : BaseBottomSheet<BottomsheetStcSelectDateRangeBindi
     fun show() {
         fm?.let {
             show(it, TAG)
+        }
+    }
+
+
+    private fun showFilterItems() {
+        binding?.run {
+            rvStcDateRage.run {
+                layoutManager = LinearLayoutManager(context)
+                adapter = mAdapter
+            }
+
+            mAdapter?.clearAllElements()
+            mAdapter?.addElement(items)
+        }
+    }
+
+    private fun showExclusiveIdentifier() = binding?.run {
+        activity?.let {
+            val userSession: UserSessionInterface = UserSession(it.applicationContext)
+            val isRegularMerchant = StatisticPageHelper.getRegularMerchantStatus(userSession)
+            rvStcDateRage.addOnItemTouchListener(object :
+                RecyclerView.OnItemTouchListener {
+                override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+                    return isRegularMerchant
+                }
+
+                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+
+                }
+
+                override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+
+                }
+            })
+            tvStcExclusiveFeature.isVisible = !isRegularMerchant
+            icStcExclusiveFeature.isVisible = !isRegularMerchant
+            stcFilterExclusiveIdentifier.isVisible = isRegularMerchant
+            stcFilterExclusiveIdentifier.setOnCtaClickListener {
+                dismiss()
+            }
+            if (isRegularMerchant) {
+                val identifierDescription = arguments
+                    ?.getString(KEY_IDENTIFIER_DESCRIPTION).orEmpty()
+                stcFilterExclusiveIdentifier.setDescription(identifierDescription)
+                viewStcFilterHeader.setMargin(0, 0, 0, 0)
+                viewStcFilterHeader.setBackgroundResource(R.drawable.bg_stc_filter_header_rm)
+            } else {
+                val topMargin = it.dpToPx(36).toInt()
+                viewStcFilterHeader.setMargin(0, topMargin, 0, 0)
+                viewStcFilterHeader.setBackgroundResource(R.drawable.bg_stc_filter_header)
+            }
         }
     }
 
