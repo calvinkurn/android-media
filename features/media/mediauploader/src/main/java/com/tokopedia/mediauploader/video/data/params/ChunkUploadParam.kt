4b03d6fe70c6c851d7f3c1cc98e6056378a81c24
isfaaghyth <isfaaghyth@gmail.com>
2021-10-28 19:25:51 +0700

    adjusted the large video upload param

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/ChunkUploadParam.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/ChunkUploadParam.kt
@@ -1,20 +1,45 @@
 package com.tokopedia.mediauploader.video.data.params
 
 import com.tokopedia.mediauploader.common.util.fileBody
-import java.io.File
 
 data class ChunkUploadParam(
     var sourceId: String,
     var uploadId: String,
     var partNumber: String,
-    var file: File,
+    var fileName: String,
+    var byteArray: ByteArray,
     var timeOut: String,
 ) {
 
-    fun fileBody() = file.fileBody(
+    fun fileBody() = byteArray.fileBody(
+        fileName = fileName,
         type = TYPE_FILE,
         bodyName = BODY_FILE
     )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ChunkUploadParam
+
+        if (sourceId != other.sourceId) return false
+        if (uploadId != other.uploadId) return false
+        if (partNumber != other.partNumber) return false
+        if (!byteArray.contentEquals(other.byteArray)) return false
+        if (timeOut != other.timeOut) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sourceId.hashCode()
+        result = 31 * result + uploadId.hashCode()
+        result = 31 * result + partNumber.hashCode()
+        result = 31 * result + byteArray.contentHashCode()
+        result = 31 * result + timeOut.hashCode()
+        return result
+    }
 
     companion object {
         private const val BODY_FILE = "file"
--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/CompleteParam.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/params/CompleteParam.kt
@@ -2,6 +2,5 @@ package com.tokopedia.mediauploader.video.data.params
 
 data class CompleteParam(
     var uploadId: String,
-    var fileName: String,
     var accessToken: String,
 )
