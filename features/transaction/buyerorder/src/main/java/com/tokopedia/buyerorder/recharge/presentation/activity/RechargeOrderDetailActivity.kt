3b0873ebce33505eaaa0c66f567d05c2bd0340c4
Muhammad Furqan <furqan@tokopedia.com>
2021-10-28 13:28:44 +0700

    [Recharge] Setup Recharge Order Detail Activity

---
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/recharge/presentation/activity/RechargeOrderDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.buyerorder.recharge.presentation.activity
+
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
+/**
+ * @author by furqan on 28/10/2021
+ */
+class RechargeOrderDetailActivity : BaseSimpleActivity() {
+    override fun getNewFragment(): Fragment? {
+//        TODO("Not yet implemented")
+        return null
+    }
+}
