3c20f1a8fce8791dcc48af3a9a29bba26ef914ed
isfaaghyth <isfaaghyth@gmail.com>
2021-10-28 19:28:59 +0700

    adjusted complete use-case

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/domain/SetCompleteUploaderUseCase.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/domain/SetCompleteUploaderUseCase.kt
@@ -6,19 +6,17 @@ import com.tokopedia.mediauploader.video.data.VideoUploadServices
 import com.tokopedia.mediauploader.video.data.entity.LargeUploader
 import com.tokopedia.mediauploader.video.data.params.CompleteParam
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class SetCompleteUploaderUseCase constructor(
+class SetCompleteUploaderUseCase @Inject constructor(
     private val services: VideoUploadServices
 ) : CoroutineUseCase<CompleteParam, LargeUploader>(Dispatchers.IO) {
 
     override suspend fun execute(params: CompleteParam): LargeUploader {
-        val (uploadId, fileName, accessToken) = params
-
         return services.completeLargeUpload(
             urlToUpload = UrlBuilder.completeUrl(),
-            fileName = fileName,
-            uploadId = uploadId,
-            accessToken = accessToken
+            uploadId = params.uploadId,
+            accessToken = params.accessToken
         )
     }
 
