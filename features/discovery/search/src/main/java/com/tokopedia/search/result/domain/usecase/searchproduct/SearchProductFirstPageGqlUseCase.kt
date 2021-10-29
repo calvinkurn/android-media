bec410fbbe80d8280c338e8fa28781c336399019
darianthedytoped <darian.thedy@tokopedia.com>
2021-10-29 11:06:22 +0700

    GQL Annotation Processing

--- features/discovery/search/src/main/java/com/tokopedia/search/result/domain/usecase/searchproduct/SearchProductFirstPageGqlUseCase.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/domain/usecase/searchproduct/SearchProductFirstPageGqlUseCase.kt
@@ -92,9 +92,10 @@ class SearchProductFirstPageGqlUseCase(
         add(createQuickFilterRequest(query = query, params = params))
     }
 
+    @GqlQuery("QuickFilter", QUICK_FILTER_QUERY)
     private fun createQuickFilterRequest(query: String, params: String) =
             GraphqlRequest(
-                    QUICK_FILTER_QUERY,
+                    QuickFilter.GQL_QUERY,
                     QuickFilterModel::class.java,
                     mapOf(GQL.KEY_QUERY to query, GQL.KEY_PARAMS to params)
             )
@@ -105,9 +106,10 @@ class SearchProductFirstPageGqlUseCase(
         }
     }
 
+    @GqlQuery("GlobalNav", GLOBAL_NAV_GQL_QUERY)
     private fun createGlobalSearchNavigationRequest(query: String, params: String) =
             GraphqlRequest(
-                    GLOBAL_NAV_GQL_QUERY,
+                    GlobalNav.GQL_QUERY,
                     GlobalSearchNavigationModel::class.java,
                     mapOf(GQL.KEY_QUERY to query, GQL.KEY_PARAMS to params)
             )
@@ -118,9 +120,10 @@ class SearchProductFirstPageGqlUseCase(
         }
     }
 
+    @GqlQuery("InspirationCarousel", SEARCH_INSPIRATION_CAROUSEL_QUERY)
     private fun createSearchInspirationCarouselRequest(params: String) =
             GraphqlRequest(
-                    SEARCH_INSPIRATION_CAROUSEL_QUERY,
+                    InspirationCarousel.GQL_QUERY,
                     SearchInspirationCarouselModel::class.java,
                     mapOf(GQL.KEY_PARAMS to params)
             )
@@ -131,9 +134,10 @@ class SearchProductFirstPageGqlUseCase(
         }
     }
 
+    @GqlQuery("InspirationWidget", SEARCH_INSPIRATION_WIDGET_QUERY)
     private fun createSearchInspirationWidgetRequest(params: String) =
             GraphqlRequest(
-                    SEARCH_INSPIRATION_WIDGET_QUERY,
+                    InspirationWidget.GQL_QUERY,
                     SearchInspirationWidgetModel::class.java,
                     mapOf(GQL.KEY_PARAMS to params)
             )
