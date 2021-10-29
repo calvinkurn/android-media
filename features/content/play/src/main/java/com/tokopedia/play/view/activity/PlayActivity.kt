66b446d164588f4d5c20388d14dc1b5d362bcaf0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-10-29 14:04:26 +0700

    remove unnecessary log

--- features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
@@ -1,12 +1,9 @@
 package com.tokopedia.play.view.activity
 
-import android.app.ActivityManager
-import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.media.AudioManager
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
