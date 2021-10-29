5db27f166a0bde8b347796ca7990ed5ef50c9d74
irfankhoirul <irfan.muhlishin@tokopedia.com>
2021-10-29 20:34:26 +0700

    remove old cart

--- features/transaction/cart/src/main/java/com/tokopedia/cart/old/view/uimodel/CartSelectAllHolderData.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.cart.old.view.uimodel
-
-data class CartSelectAllHolderData(
-        var isCheked: Boolean = false,
-        var isCheckUncheckDirectAction: Boolean = true,
-        var isShowDeleteButton: Boolean = false
-)
