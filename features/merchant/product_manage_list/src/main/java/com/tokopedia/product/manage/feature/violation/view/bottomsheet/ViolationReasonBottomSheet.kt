2596a11be5d386438ea11466bf566f4352634647
Reivin <reivin.oktavianus@tokopedia.com>
2021-11-01 13:36:39 +0700

    Add violation bottom sheet layouts + item

---
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/violation/view/bottomsheet/ViolationReasonBottomSheet.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.product.manage.feature.violation.view.bottomsheet
+
+import com.tokopedia.unifycomponents.BottomSheetUnify
+
+class ViolationReasonBottomSheet(): BottomSheetUnify() {
+
+    companion object {
+        fun createInstance(): ViolationReasonBottomSheet {
+            return ViolationReasonBottomSheet()
+        }
+    }
+
+}
