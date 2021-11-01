b8363c9966833db6b4c8d6ff88e545d1da56efac
saidfaisal <saidfaisal777@gmail.com>
2021-11-01 10:46:46 +0700

    review TokoNowView Implementation

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/view/TokoNowView.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/view/TokoNowView.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
 interface TokoNowView {
-    fun getFragmentPage(): Fragment?
-    fun getFragmentManagerPage(): FragmentManager?
+    fun getFragmentPage(): Fragment
+    fun getFragmentManagerPage(): FragmentManager
     fun refreshLayoutPage()
     fun getScrollState(adapterPosition: Int): Parcelable?
     fun saveScrollState(adapterPosition: Int, scrollState: Parcelable?)
