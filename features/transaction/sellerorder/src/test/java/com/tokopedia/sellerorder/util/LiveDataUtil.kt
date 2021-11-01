a754e419470a542b25afdccb7be5a3e3dd5aa2d2
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2021-11-02 00:48:10 +0700

    Improve unit test coverage

--- features/transaction/sellerorder/src/test/java/com/tokopedia/sellerorder/util/LiveDataUtil.kt
+++ features/transaction/sellerorder/src/test/java/com/tokopedia/sellerorder/util/LiveDataUtil.kt
@@ -2,17 +2,38 @@ package com.tokopedia.sellerorder.util
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 fun <T> LiveData<T>.observeAwaitValue(
-        time: Long = 2,
-        timeUnit: TimeUnit = TimeUnit.SECONDS): T? {
+    time: Long = 5,
+    timeUnit: TimeUnit = TimeUnit.SECONDS
+): T? {
     var value: T? = null
     val latch = CountDownLatch(1)
     val observer = Observer<T> { t ->
         value = t
         latch.countDown()
+    }
+    observeForever(observer)
+    latch.await(time, timeUnit)
+    return value
+}
+
+fun <T> LiveData<T>.observeAwaitSpecificValue(
+    time: Long = 5,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    expected: T
+): T? {
+    var value: T? = null
+    val latch = CountDownLatch(1)
+    val observer = Observer<T> { t ->
+        if (t == expected) {
+            value = t
+            latch.countDown()
+        }
     }
     observeForever(observer)
     latch.await(time, timeUnit)
