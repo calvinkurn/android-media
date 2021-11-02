8de6c4f64f4afc2f06e0e50164a3c2391193728e
isfaaghyth <isfaaghyth@gmail.com>
2021-11-03 00:49:18 +0700

    fixed build time issue on transcoding data model

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/entity/Transcoding.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/data/entity/Transcoding.kt
@@ -2,7 +2,6 @@ package com.tokopedia.mediauploader.video.data.entity
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import com.tokopedia.mediauploader.video.data.state.TranscodingState
 
 data class Transcoding(
     @Expose @SerializedName("status") val status: String? = ""
