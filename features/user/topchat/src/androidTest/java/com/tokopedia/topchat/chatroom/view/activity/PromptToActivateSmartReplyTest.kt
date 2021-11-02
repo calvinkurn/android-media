a8c8acf4acef9ecb99dcd470fa73131483a70bfa
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-11-02 17:29:42 +0700

    create empty test class

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/PromptToActivateSmartReplyTest.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+class PromptToActivateSmartReplyTest: TopchatRoomTest() {
+}
