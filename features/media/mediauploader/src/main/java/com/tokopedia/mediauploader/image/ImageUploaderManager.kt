b266424b42e9e76d291bf718abd9d80de594b22b
isfaaghyth <isfaaghyth@gmail.com>
2021-11-03 00:39:08 +0700

    adjusted the image uploader manager

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/image/ImageUploaderManager.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/image/ImageUploaderManager.kt
@@ -3,6 +3,7 @@ package com.tokopedia.mediauploader.image
 import com.tokopedia.mediauploader.UploaderManager
 import com.tokopedia.mediauploader.common.data.consts.*
 import com.tokopedia.mediauploader.common.data.entity.SourcePolicy
+import com.tokopedia.mediauploader.common.data.mapper.PolicyMapper
 import com.tokopedia.mediauploader.common.state.ProgressCallback
 import com.tokopedia.mediauploader.common.state.UploadResult
 import com.tokopedia.mediauploader.common.util.fileExtension
@@ -14,7 +15,6 @@ import com.tokopedia.mediauploader.image.domain.GetImagePolicyUseCase
 import com.tokopedia.mediauploader.image.domain.GetImageUploaderUseCase
 import java.io.File
 import javax.inject.Inject
-import com.tokopedia.mediauploader.common.data.mapper.ImagePolicyMapper.mapToSourcePolicy
 
 class ImageUploaderManager @Inject constructor(
     private val imagePolicyUseCase: GetImagePolicyUseCase,
@@ -26,36 +26,36 @@ class ImageUploaderManager @Inject constructor(
 
         val filePath = file.path
         val policyData = imagePolicyUseCase(sourceId)
-        val sourcePolicy = mapToSourcePolicy(policyData.dataPolicy)
+        val sourcePolicy = PolicyMapper.map(policyData.dataPolicy)
 
-        val onError = if (sourcePolicy.imagePolicy != null) {
+        if (sourcePolicy.imagePolicy != null) {
             val extensions = sourcePolicy.imagePolicy.extension.split(",")
             val maxFileSize = sourcePolicy.imagePolicy.maxFileSize
             val maxRes = sourcePolicy.imagePolicy.maximumRes
             val minRes = sourcePolicy.imagePolicy.minimumRes
 
-            setError(listOf(
-                when {
-                    !file.exists() -> FILE_NOT_FOUND
-                    !extensions.contains(filePath.fileExtension()) ->
-                        formatNotAllowedMessage(sourcePolicy.imagePolicy.extension)
-                    file.isMaxFileSize(maxFileSize) ->
-                        maxFileSizeMessage(maxFileSize)
-                    filePath.isMaxBitmapResolution(maxRes.width, maxRes.height) ->
-                        maxResBitmapMessage(maxRes.width, maxRes.height)
-                    filePath.isMinBitmapResolution(minRes.width, minRes.height) ->
-                        minResBitmapMessage(minRes.width, minRes.height)
-                    else -> ""
+            return when {
+                !file.exists() -> {
+                    UploadResult.Error(FILE_NOT_FOUND)
                 }
-            ), sourceId, file) as UploadResult.Error
+                file.isMaxFileSize(maxFileSize) -> {
+                    UploadResult.Error(maxFileSizeMessage(maxFileSize))
+                }
+                !extensions.contains(filePath.fileExtension()) -> {
+                    UploadResult.Error(formatNotAllowedMessage(sourcePolicy.imagePolicy.extension))
+                }
+                filePath.isMaxBitmapResolution(maxRes.width, maxRes.height) -> {
+                    UploadResult.Error(maxResBitmapMessage(maxRes.width, maxRes.height))
+                }
+                filePath.isMinBitmapResolution(minRes.width, minRes.height) -> {
+                    UploadResult.Error(minResBitmapMessage(minRes.width, minRes.height))
+                }
+                else -> {
+                    upload(file, sourceId, sourcePolicy)
+                }
+            }
         } else {
-            UploadResult.Error(UNKNOWN_ERROR)
-        }
-
-        return if (onError.message.isNotEmpty()) {
-            onError
-        } else {
-            upload(file, sourceId, sourcePolicy)
+            return UploadResult.Error(UNKNOWN_ERROR)
         }
     }
 
@@ -68,14 +68,14 @@ class ImageUploaderManager @Inject constructor(
         ))
 
         val error = if (upload.header.messages.isNotEmpty()) {
-            upload.header.messages
+            upload.header.messages.first()
         } else {
-            listOf(UNKNOWN_ERROR)
+            UNKNOWN_ERROR
         }
 
         return upload.data?.let {
             UploadResult.Success(uploadId = it.uploadId)
-        }?: setError(error, sourceId, file)
+        }?: UploadResult.Error(error)
     }
 
     override fun setProgressUploader(progress: ProgressCallback?) {
