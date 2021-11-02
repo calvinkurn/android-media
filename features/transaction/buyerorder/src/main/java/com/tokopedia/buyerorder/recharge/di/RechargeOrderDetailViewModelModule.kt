4d89c660e23b0805fffa8227bae0831306536b61
Muhammad Furqan <furqan@tokopedia.com>
2021-11-02 09:57:11 +0700

    [Recharge] Fix Include Digital Recommendation View Model Module

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/recharge/di/RechargeOrderDetailViewModelModule.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/recharge/di/RechargeOrderDetailViewModelModule.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
 import com.tokopedia.buyerorder.recharge.presentation.viewmodel.RechargeOrderDetailViewModel
+import com.tokopedia.digital.digital_recommendation.di.DigitalRecommendationViewModelModule
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -12,7 +13,7 @@ import dagger.multibindings.IntoMap
 /**
  * @author by furqan on 28/10/2021
  */
-@Module
+@Module(includes = [DigitalRecommendationViewModelModule::class])
 abstract class RechargeOrderDetailViewModelModule {
     @Binds
     @RechargeOrderDetailScope
