d087e607976839f1670d69095a38e4e13254eef5
jegul <kenny.hadisaputra@tokopedia.com>
2021-11-02 19:32:35 +0700

    adjust to new ui for pinned

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/pinnedmessage/PinnedMessageView.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/pinnedmessage/PinnedMessageView.kt
@@ -1,12 +1,21 @@
 package com.tokopedia.play.broadcaster.view.custom.pinnedmessage
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ImageSpan
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.iconunify.getIconUnifyDrawable
+import com.tokopedia.play.broadcaster.R
 import com.tokopedia.play.broadcaster.databinding.ViewPlayBroPinnedMsgBinding
 import com.tokopedia.play.broadcaster.databinding.ViewPlayBroPinnedMsgEmptyBinding
 import com.tokopedia.play.broadcaster.databinding.ViewPlayBroPinnedMsgFilledBinding
+import com.tokopedia.unifyprinciples.R as unifyR
 
 /**
  * Created by jegul on 11/10/21
@@ -33,6 +42,20 @@ class PinnedMessageView : FrameLayout {
         this,
         true,
     )
+
+    private val offset14 by lazy { resources.getDimensionPixelSize(R.dimen.play_dp_14) }
+
+    private val editDrawable: Drawable? by lazy {
+        getIconUnifyDrawable(
+            context,
+            IconUnify.EDIT,
+            MethodChecker.getColor(context, unifyR.color.Unify_Static_White)
+        )?.apply drawable@ {
+            this@drawable.setBounds(
+                0, 0, offset14, offset14,
+            )
+        }
+    }
 
     private var mOnPinnedClickedListener: OnPinnedClickedListener? = null
 
@@ -81,7 +104,14 @@ class PinnedMessageView : FrameLayout {
         layout.root.setOnClickListener {
             mOnPinnedClickedListener?.onPinnedClicked(this@PinnedMessageView, message)
         }
-        layout.tvPinnedMsg.text = message
+        layout.tvPinnedMsg.text = SpannableStringBuilder().apply {
+            append(message)
+            val editDrawable = this@PinnedMessageView.editDrawable
+            if (editDrawable != null) {
+                append(' ')
+                append(" ", ImageSpan(editDrawable), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+        }
 
         addView(layout.root)
     }
