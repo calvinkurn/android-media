97a10d262e2b1cb9c954faf517f609bf201cec7c
isfaaghyth <isfaaghyth@gmail.com>
2021-11-03 00:41:38 +0700

    get the transcoding pure result instead of as transcoding state

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/domain/GetTranscodingStatusUseCase.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/domain/GetTranscodingStatusUseCase.kt
@@ -3,22 +3,18 @@ package com.tokopedia.mediauploader.video.domain
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import com.tokopedia.mediauploader.common.data.consts.UrlBuilder
 import com.tokopedia.mediauploader.video.data.VideoUploadServices
-import com.tokopedia.mediauploader.video.data.state.TranscodingState
+import com.tokopedia.mediauploader.video.data.entity.Transcoding
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class GetTranscodingStatusUseCase @Inject constructor(
     private val services: VideoUploadServices
-) : CoroutineUseCase<String, TranscodingState>(Dispatchers.IO) {
+) : CoroutineUseCase<String, Transcoding>(Dispatchers.IO) {
 
-    override suspend fun execute(params: String): TranscodingState {
-        if (params.isEmpty()) return TranscodingState.UNKNOWN
-
-        val transcoding = services.checkTranscodingStatus(
+    override suspend fun execute(params: String): Transcoding {
+        return services.checkTranscodingStatus(
             url = UrlBuilder.isTranscodeUrl(params)
         )
-
-        return transcoding.status()
     }
 
     override fun graphqlQuery() = ""
