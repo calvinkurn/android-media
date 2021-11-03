639a65ebaf7cbe2cae04cdd075b555cc66ac0030
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-11-03 11:12:53 +0700

    add TODO PromptToActivateSmartReplyTest

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/PromptToActivateSmartReplyTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/PromptToActivateSmartReplyTest.kt
@@ -3,4 +3,11 @@ package com.tokopedia.topchat.chatroom.view.activity
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
 
 class PromptToActivateSmartReplyTest: TopchatRoomTest() {
+    // TODO: Should show ticker below matched bubble if previous msg is carousel products
+    // TODO: Should show ticker below matched bubble if previous msg is single products
+    // TODO: Should show ticker below matched bubble if success get GetReminderTicker and matched bubble is on page two or more
+    // TODO: Should close ticker when close btn closed
+    // TODO: Should not show ticker if user is in the middle of the page
+    // TODO: Should not show ticker if no ticker available to exist from GetReminderTicker
+    // TODO: Should go to chat setting if clicked from hamburger menu item
 }
