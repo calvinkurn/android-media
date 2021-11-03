b4580e300967e4f19256e57a41dc8ca07284285a
isfaaghyth <isfaaghyth@gmail.com>
2021-11-03 15:09:01 +0700

    added transcoding step and transcode max retry count on simple uploader

--- features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/SimpleUploaderManager.kt
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/video/SimpleUploaderManager.kt
@@ -1,16 +1,25 @@
 package com.tokopedia.mediauploader.video
 
+import com.tokopedia.mediauploader.common.data.consts.TRANSCODING_FAILED
 import com.tokopedia.mediauploader.common.data.consts.UNKNOWN_ERROR
 import com.tokopedia.mediauploader.common.data.entity.SourcePolicy
 import com.tokopedia.mediauploader.common.state.UploadResult
 import com.tokopedia.mediauploader.video.data.params.SimpleUploadParam
 import com.tokopedia.mediauploader.video.domain.GetSimpleUploaderUseCase
+import com.tokopedia.mediauploader.video.domain.GetTranscodingStatusUseCase
+import kotlinx.coroutines.delay
 import java.io.File
 import javax.inject.Inject
 
-class SimpleUploaderManager @Inject constructor(val simpleUploaderUseCase: GetSimpleUploaderUseCase) {
+class SimpleUploaderManager @Inject constructor(
+    val simpleUploaderUseCase: GetSimpleUploaderUseCase,
+    val transcodingUseCase: GetTranscodingStatusUseCase,
+) {
 
-    suspend operator fun invoke(file: File, sourceId: String, policy: SourcePolicy): UploadResult {
+    // set max retry of transcoding checker
+    private var maxRetryTranscoding = 0
+
+    suspend operator fun invoke(file: File, sourceId: String, policy: SourcePolicy, withTranscode: Boolean): UploadResult {
         val uploader = simpleUploaderUseCase(SimpleUploadParam(
             timeOut = policy.timeOut.toString(),
             sourceId = sourceId,
@@ -23,11 +32,41 @@ class SimpleUploaderManager @Inject constructor(val simpleUploaderUseCase: GetSi
             UNKNOWN_ERROR
         }
 
+        if (withTranscode) {
+            while(true) {
+                if (maxRetryTranscoding >= MAX_RETRY_TRANSCODING) {
+                    resetUpload()
+
+                    return UploadResult.Error(TRANSCODING_FAILED)
+                }
+
+                if (uploader.uploadId != null) {
+                    if (transcodingUseCase(uploader.uploadId).isCompleted()) {
+                        break
+                    }
+                }
+
+                maxRetryTranscoding++
+                delay(5000)
+            }
+        }
+
         return if (!uploader.videoUrl.isNullOrBlank()) {
-            UploadResult.Success(videoUrl = uploader.videoUrl)
+            UploadResult.Success(
+                videoUrl = uploader.videoUrl,
+                uploadId = uploader.uploadId.toString()
+            )
         } else {
             UploadResult.Error(error)
         }
+    }
+
+    private fun resetUpload() {
+        maxRetryTranscoding = 0
+    }
+
+    companion object {
+        private const val MAX_RETRY_TRANSCODING = 24
     }
 
 }
