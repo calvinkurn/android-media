59295e629ab778adc50b01c00056205bff5d872f
yohann.prananta <yohann.prananta@BA-00004624.local>
2021-11-03 10:13:03 +0700

    revert broken product card mapping

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeFlashSaleProductCardGridViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeFlashSaleProductCardGridViewHolder.kt
@@ -21,7 +21,7 @@ class ShopHomeFlashSaleProductCardGridViewHolder(
     fun bindData(uiModel: ShopHomeProductUiModel) {
         this.uiModel = uiModel
         productCardGrid?.setProductModel(
-            ShopPageHomeMapper.mapToProductCardModel(
+            ShopPageHomeMapper.mapToProductCardCampaignModel(
                 isHasAddToCartButton = false,
                 hasThreeDots = false,
                 shopHomeProductViewModel = uiModel
