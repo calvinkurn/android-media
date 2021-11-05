50bc158967d8ffa192da1cb3611273b1d72e5d53
meyta.taliti <meyta.taliti@tokopedia.com>
2021-11-05 07:59:12 +0700

    remove unused import

--- testing/joint/dg_transaction/src/main/java/com/tokopedia/dg_transaction/testing/digital_checkout/robot/DigitalCartRobot.kt
+++ testing/joint/dg_transaction/src/main/java/com/tokopedia/dg_transaction/testing/digital_checkout/robot/DigitalCartRobot.kt
@@ -1,11 +1,9 @@
 package com.tokopedia.dg_transaction.testing.digital_checkout.robot
 
-import android.app.Activity
 import android.app.Instrumentation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.ComponentNameMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.tokopedia.dg_transaction.testing.R
