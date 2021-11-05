9767921a98175f0a70a8be38c0b5c26b8d63816f
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-11-05 14:19:24 +0700

    fix uncommented code

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomGeneralTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomGeneralTest.kt
@@ -15,39 +15,39 @@ class TopchatRoomGeneralTest : TopchatRoomTest() {
     private val exShopId = "1231"
     private val exUserId = "1232"
 
-//    @Test
-//    fun test_intent_ask_seller_with_custom_msg() {
-//        // Given
-//        val intentMsg = "Hi seller"
-//        getChatUseCase.response = firstPageChatAsBuyer
-//        chatAttachmentUseCase.response = chatAttachmentResponse
-//        launchChatRoomActivity {
-//            val intent = RouteManager.getIntent(
-//                context, ApplinkConst.TOPCHAT_ROOM_ASKSELLER_WITH_MSG, exShopId, intentMsg
-//            )
-//            it.putExtras(intent)
-//        }
-//
-//        // Then
-//        onView(withId(R.id.new_comment)).check(matches(withText(intentMsg)))
-//    }
-//
-//    @Test
-//    fun test_intent_ask_buyer_with_custom_msg() {
-//        // Given
-//        val intentMsg = "Hi buyer"
-//        getChatUseCase.response = firstPageChatAsSeller
-//        chatAttachmentUseCase.response = chatAttachmentResponse
-//        launchChatRoomActivity {
-//            val intent = RouteManager.getIntent(
-//                context, ApplinkConst.TOPCHAT_ROOM_ASKBUYER_WITH_MSG, exUserId, intentMsg
-//            )
-//            it.putExtras(intent)
-//        }
-//
-//        // Then
-//        onView(withId(R.id.new_comment)).check(matches(withText(intentMsg)))
-//    }
+    @Test
+    fun test_intent_ask_seller_with_custom_msg() {
+        // Given
+        val intentMsg = "Hi seller"
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        launchChatRoomActivity {
+            val intent = RouteManager.getIntent(
+                context, ApplinkConst.TOPCHAT_ROOM_ASKSELLER_WITH_MSG, exShopId, intentMsg
+            )
+            it.putExtras(intent)
+        }
+
+        // Then
+        onView(withId(R.id.new_comment)).check(matches(withText(intentMsg)))
+    }
+
+    @Test
+    fun test_intent_ask_buyer_with_custom_msg() {
+        // Given
+        val intentMsg = "Hi buyer"
+        getChatUseCase.response = firstPageChatAsSeller
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        launchChatRoomActivity {
+            val intent = RouteManager.getIntent(
+                context, ApplinkConst.TOPCHAT_ROOM_ASKBUYER_WITH_MSG, exUserId, intentMsg
+            )
+            it.putExtras(intent)
+        }
+
+        // Then
+        onView(withId(R.id.new_comment)).check(matches(withText(intentMsg)))
+    }
 
     @Test
     fun should_show_dummy_toolbar_when_using_chatroom_askseller_deeplink() {
