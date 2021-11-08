40599a52d359bf010f0b87c0b00832768b7c4d41
Steven <steven@BA-00003246.local>
2021-11-08 09:04:54 +0700

    improve unify coverage on productcard option and similarsearch module

--- features/discovery/productcard_options/src/main/java/com/tokopedia/productcard/options/ProductCardOptionsActivity.kt
+++ features/discovery/productcard_options/src/main/java/com/tokopedia/productcard/options/ProductCardOptionsActivity.kt
@@ -1,17 +1,20 @@
 package com.tokopedia.productcard.options
 
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.annotation.AnimRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.discovery.common.manager.PRODUCT_CARD_OPTIONS_MODEL
 import com.tokopedia.discovery.common.model.ProductCardOptionsModel
 import com.tokopedia.productcard.options.di.ProductCardOptionsContextModule
+import com.tokopedia.unifycomponents.BottomSheetUnify
 import kotlinx.android.synthetic.main.product_card_options_activity_layout.*
 import javax.inject.Inject
 import javax.inject.Named
@@ -21,31 +24,17 @@ internal class ProductCardOptionsActivity : BaseSimpleActivity() {
 
     @field:[Inject Named(PRODUCT_CARD_OPTIONS_VIEW_MODEL_FACTORY)]
     lateinit var productCardOptionsViewModelFactory: ViewModelProvider.Factory
+    private var bottomSheetProductCardOptions = BottomSheetUnify()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        animateTransparentBackground()
-        setTransparentBackgroundOnClick()
+        configBottomsheet()
+        bottomSheetProductCardOptions.show(supportFragmentManager,tagFragment)
     }
 
     override fun getLayoutRes(): Int {
         return R.layout.product_card_options_activity_layout
-    }
-
-    private fun animateTransparentBackground() {
-        val animation = loadAnimation(R.anim.product_card_options_fade_in)
-        imageBackgroundTransparent?.startAnimation(animation)
-    }
-
-    private fun loadAnimation(@AnimRes animationResource: Int): Animation {
-        return AnimationUtils.loadAnimation(this, animationResource)
-    }
-
-    private fun setTransparentBackgroundOnClick() {
-        imageBackgroundTransparent?.setOnClickListener {
-            finish()
-        }
     }
 
     // setupStatusBar overriden as empty to disable status bar
@@ -83,41 +72,14 @@ internal class ProductCardOptionsActivity : BaseSimpleActivity() {
         return ProductCardOptionsFragment()
     }
 
-    override fun inflateFragment() {
-        supportFragmentManager
-                .beginTransaction()
-                .setCustomAnimations(R.anim.product_card_options_slide_up, R.anim.product_card_options_slide_down)
-                .replace(R.id.parentView, newFragment, tagFragment)
-                .commit()
-    }
+    override fun inflateFragment() { }
 
     override fun finish() {
-        fragment?.let { animateCloseBottomSheet(it) }
-    }
-
-    private fun animateCloseBottomSheet(fragment: Fragment) {
-        val animation = loadAnimation(R.anim.product_card_options_slide_down)
-        animation.setAnimationListener(createBottomSheetAnimationListener(fragment))
-
-        fragment.view?.startAnimation(animation)
-    }
-
-    private fun createBottomSheetAnimationListener(fragment: Fragment): Animation.AnimationListener {
-        return object: Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) { }
-
-            override fun onAnimationStart(animation: Animation?) { }
-
-            override fun onAnimationEnd(animation: Animation?) { onBottomSheetAnimationEnd(fragment) }
+        fragment?.let {
+            removeFragment(it)
+            super.finish()
+            overridePendingTransition(0, 0)
         }
-    }
-
-    private fun onBottomSheetAnimationEnd(fragment: Fragment) {
-        removeFragment(fragment)
-        cancelBottomSheetAnimation(fragment)
-        overridePendingTransition(0, 0)
-
-        super.finish()
     }
 
     private fun removeFragment(fragment: Fragment) {
@@ -130,5 +92,36 @@ internal class ProductCardOptionsActivity : BaseSimpleActivity() {
     private fun cancelBottomSheetAnimation(fragment: Fragment) {
         fragment.view?.clearAnimation()
         fragment.view?.animate()?.cancel()
+    }
+
+    private fun configBottomsheet() {
+        val childView = View.inflate(this.baseContext, R.layout.product_card_options_activity_layout, null)
+        bottomSheetProductCardOptions.apply {
+            setChild(childView)
+            showKnob = true
+            showHeader = false
+            showCloseIcon = false
+            isHideable = true
+            setOnDismissListener {
+                finish()
+            }
+            setShowListener {
+                childFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.parentView, newFragment, tagFragment)
+                        .commit()
+
+                bottomSheetProductCardOptions.bottomSheet.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                    override fun onSlide(p0: View, p1: Float) {
+                        println("hahaha x $p1")
+                    }
+
+                    override fun onStateChanged(p0: View, p1: Int) {
+                        println("hahaha $p1")
+                    }
+                })
+            }
+        }
+
     }
 }
