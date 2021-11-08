45c3a8e9c9db0cb49170a20d761e9e0266f95a37
Rohan <rohan.ghosh@tokopedia.com>
2021-11-08 17:36:50 +0530

    Removed the mainApplication white space

--- testapp/src/main/java/com/tokopedia/application/MyApplication.java
+++ testapp/src/main/java/com/tokopedia/application/MyApplication.java
@@ -24,6 +24,7 @@ import com.tokopedia.common.network.util.NetworkClient;
 import com.tokopedia.config.GlobalConfig;
 import com.tokopedia.core.TkpdCoreRouter;
 import com.tokopedia.core.analytics.TrackingUtils;
+import com.tokopedia.core.analytics.container.AppsflyerAnalytics;
 import com.tokopedia.core.analytics.container.GTMAnalytics;
 import com.tokopedia.core.analytics.container.MoengageAnalytics;
 import com.tokopedia.core.gcm.base.IAppNotificationReceiver;
@@ -355,13 +356,12 @@ public class MyApplication extends BaseMainApplication
         }
     }
 
-    public void initFileDirConfig() {
+    public void initFileDirConfig(){
         GlobalConfig.INTERNAL_CACHE_DIR = this.getCacheDir().getAbsolutePath();
         GlobalConfig.INTERNAL_FILE_DIR = this.getFilesDir().getAbsolutePath();
         GlobalConfig.EXTERNAL_CACHE_DIR = this.getExternalCacheDir() != null ? this.getExternalCacheDir().getAbsolutePath() : "";
         GlobalConfig.EXTERNAL_FILE_DIR = this.getExternalFilesDir(null) != null ? this.getExternalFilesDir(null).getAbsolutePath() : "";
     }
-
 
     public static class AppsflyerAnalytics extends DummyAnalytics {
 
