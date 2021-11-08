0f02e2c5ae2dab64b164531d221149bfdf30cba2
hansenpw <hansen.wijaya@tokopedia.com>
2021-11-08 17:42:26 +0700

    fix unit test

--- features/transaction/cart/src/test/java/com/tokopedia/cart/bundle/view/presenter/CartListPresenterCalculateSubTotalTest.kt
+++ features/transaction/cart/src/test/java/com/tokopedia/cart/bundle/view/presenter/CartListPresenterCalculateSubTotalTest.kt
@@ -12,7 +12,7 @@ import com.tokopedia.cart.bundle.view.uimodel.CartShopHolderData
 import com.tokopedia.cartcommon.domain.usecase.DeleteCartUseCase
 import com.tokopedia.cartcommon.domain.usecase.UndoDeleteCartUseCase
 import com.tokopedia.cartcommon.domain.usecase.UpdateCartUseCase
-import com.tokopedia.promocheckout.common.domain.ClearCacheAutoApplyStackUseCase
+import com.tokopedia.purchase_platform.common.feature.promo.domain.usecase.ClearCacheAutoApplyStackUseCase
 import com.tokopedia.purchase_platform.common.feature.promo.domain.usecase.ValidateUsePromoRevampUseCase
 import com.tokopedia.purchase_platform.common.schedulers.TestSchedulers
 import com.tokopedia.recommendation_widget_common.domain.GetRecommendationUseCase
