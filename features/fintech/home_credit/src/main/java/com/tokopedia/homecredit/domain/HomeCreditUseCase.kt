fb1cbe37387851d3588d0536180a60912207f2b4
Rohan <rohan.ghosh@tokopedia.com>
2021-11-09 16:24:39 +0530

    Added the usecase and its dependency

--- features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/domain/HomeCreditUseCase.kt
+++ features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/domain/HomeCreditUseCase.kt
@@ -2,7 +2,32 @@ package com.tokopedia.homecredit.domain
 
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 
+
 class HomeCreditUseCase @Inject constructor(
-     @ApplicationContext val context: Context)
+     @ApplicationContext val context: Context): UseCase<OutputState>() {
+
+
+     fun saveDetail(success : (OutputState)-> Unit, onFail:(OutputState)-> Unit){
+          execute({
+               success(OutputState.SuccessState)
+          },{
+               onFail(OutputState.FailState)
+          })
+     }
+
+     override suspend fun executeOnBackground(): OutputState {
+          return OutputState.SuccessState
+     }
+
+
+}
+
+
+sealed class OutputState()
+{
+     object SuccessState: OutputState()
+     object FailState: OutputState()
+}
--- features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/viewModel/HomeCreditViewModel.kt
+++ features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/viewModel/HomeCreditViewModel.kt
@@ -1,14 +1,14 @@
 package com.tokopedia.homecredit.viewModel
 
-import androidx.lifecycle.ViewModel
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.homecredit.domain.HomeCreditUseCase
 import javax.inject.Inject
 
 class HomeCreditViewModel @Inject constructor(
-    homeCreditUseCase: HomeCreditUseCase
-) : ViewModel()
+    private val homeCreditUseCase: HomeCreditUseCase,
+    private val dispatcher: CoroutineDispatchers
+) : BaseViewModel(dispatcher.io)
 {
-
-
+    
 }
