9e1bf479b103a18c4b6aec1bb7e8287002980713
Rohan <rohan.ghosh@tokopedia.com>
2021-11-09 11:33:52 +0530

    Added the kotlin dependency

--- features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/di/module/HomeCreditViewModelModule.java
+++ features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/di/module/HomeCreditViewModelModule.java
@@ -1,17 +1,26 @@
 package com.tokopedia.homecredit.di.module;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory;
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey;
+import com.tokopedia.homecredit.viewModel.HomeCreditViewModel;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
 @Module
 public abstract class HomeCreditViewModelModule {
 
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory viewModelFactory);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeCreditViewModel.class)
+    abstract ViewModel bindsPayLaterViewModel(HomeCreditViewModel homeCreditViewModel);
 
 
 }
