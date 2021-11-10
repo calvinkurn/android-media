10032f0486b737d038c478ef2211c74b947a7f74
astidhiyaa <asti.anzaria@tokopedia.com>
2021-11-11 01:37:15 +0800

    fix: change swipe banner behaviour

--- features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/viewholder/RechargeHomepageSwipeBannerViewHolder.kt
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/viewholder/RechargeHomepageSwipeBannerViewHolder.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.digital.home.presentation.adapter.viewholder
 
+import android.app.Activity
+import android.util.DisplayMetrics
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
@@ -41,11 +43,10 @@ class RechargeHomepageSwipeBannerViewHolder(itemView: View,
 
     private fun initBanner(bind: ViewRechargeHomeSwipeBannerBinding){
         bind.rechargeHomeSwipeBanner.apply {
-            freeMode = false
             centerMode = true
-            slideToScroll = 1
+            slideToScroll = SLIDE_TO_SCROLL
             indicatorPosition = CarouselUnify.INDICATOR_HIDDEN
-            slideToShow = 1.2f
+            slideToShow = SLIDE_TO_SHOW
 
             if(slidesList.size == 1){
                 autoplay = false
@@ -62,6 +63,11 @@ class RechargeHomepageSwipeBannerViewHolder(itemView: View,
                 data as BannerData
 
                 val img = view.findViewById<ImageUnify>(R.id.recharge_img_swipe_banner)
+
+                this.post {
+                    img.layoutParams.width = getBannerWidth()
+                }
+
                 img.setImageUrl(data.mediaUrl)
 
                 val index = urlArr.indexOf(data)
@@ -83,12 +89,23 @@ class RechargeHomepageSwipeBannerViewHolder(itemView: View,
         }
     }
 
+    private fun getBannerWidth(): Int{
+        val displayMetrics = DisplayMetrics()
+        (itemView.context as Activity).windowManager?.defaultDisplay?.getMetrics(displayMetrics)
+        var bannerWidth = (displayMetrics.widthPixels / SLIDE_TO_SHOW).toInt()
+        bannerWidth -= MARGIN_SIZE
+        return bannerWidth
+    }
+
     class BannerData(var mediaUrl: String)
 
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.view_recharge_home_swipe_banner
 
-        const val AUTOPLAY_DURATION = 5000L
+        private const val AUTOPLAY_DURATION = 5000L
+        private const val SLIDE_TO_SHOW = 1.2f
+        private const val SLIDE_TO_SCROLL = 1
+        private const val MARGIN_SIZE = 16
     }
 }
