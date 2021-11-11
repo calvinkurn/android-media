caaa2a0bda1a4e576a2ca08079c550ca01bf8aa5
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-11-11 13:08:00 +0700

    revert to old key

--- customer_mid_app/src/main/java/com/tokopedia/tkpd/app/ConsumerMainApplication.java
+++ customer_mid_app/src/main/java/com/tokopedia/tkpd/app/ConsumerMainApplication.java
@@ -119,7 +119,7 @@ public abstract class ConsumerMainApplication extends ConsumerRouterApplication 
     private final String NOTIFICATION_CHANNEL_DESC_BTS_ONE = "notification channel for custom sound with BTS tone";
     private final String NOTIFICATION_CHANNEL_DESC_BTS_TWO = "notification channel for custom sound with different BTS tone";
     private static final String REMOTE_CONFIG_SCALYR_KEY_LOG = "android_customerapp_log_config_scalyr";
-    private static final String REMOTE_CONFIG_NEW_RELIC_KEY_LOG = "android_customerapp_log_config_new_relic_V2";
+    private static final String REMOTE_CONFIG_NEW_RELIC_KEY_LOG = "android_customerapp_log_config_new_relic";
     private static final String REMOTE_CONFIG_EMBRACE_KEY_LOG = "android_customerapp_log_config_embrace";
     private static final String PARSER_SCALYR_MA = "android-main-app-p%s";
     private static final String ENABLE_ASYNC_AB_TEST = "android_enable_async_abtest";
