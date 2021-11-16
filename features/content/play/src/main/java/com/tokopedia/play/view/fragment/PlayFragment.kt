39acc43a0e52d3ac92ccdfa5ba7f00d51054d733
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-11-16 14:15:29 +0700

    remove upcoming page on PlayFragment and PlayViewModel

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayFragment.kt
@@ -86,9 +86,6 @@ class PlayFragment @Inject constructor(
     private val fragmentYouTubeView by viewComponent {
         FragmentYouTubeViewComponent(channelId, it, R.id.fl_youtube, childFragmentManager, this)
     }
-    private val fragmentUpcomingView by viewComponent {
-        FragmentUpcomingViewComponent(it, R.id.fl_upcoming, childFragmentManager)
-    }
 
     private lateinit var playParentViewModel: PlayParentViewModel
     private lateinit var playViewModel: PlayViewModel
@@ -231,16 +228,6 @@ class PlayFragment @Inject constructor(
         isFirstTopBoundsCalculated = true
         viewLifecycleOwner.lifecycleScope.launchWhenResumed {
             when {
-                playViewModel.upcomingInfo?.isUpcoming == true -> {
-                    fragmentVideoView.safeRelease()
-                    fragmentVideoView.hide()
-                    fragmentYouTubeView.safeRelease()
-                    fragmentYouTubeView.hide()
-                    fragmentUserInteractionView.safeRelease()
-                    fragmentUserInteractionView.hide()
-
-                    fragmentUpcomingView.safeInit("")
-                }
                 playViewModel.videoPlayer.isYouTube -> {
                     fragmentYouTubeView.safeInit()
                     fragmentYouTubeView.show()
@@ -268,9 +255,6 @@ class PlayFragment @Inject constructor(
             val totalView = playViewModel.totalView
             if (totalView.isNotEmpty()) putExtra(EXTRA_TOTAL_VIEW, totalView)
             if (channelId.isNotEmpty()) putExtra(EXTRA_CHANNEL_ID, channelId)
-            playViewModel.upcomingInfo?.let {
-                putExtra(EXTRA_IS_REMINDER, it.isReminderSet)
-            }
         })
     }
 
