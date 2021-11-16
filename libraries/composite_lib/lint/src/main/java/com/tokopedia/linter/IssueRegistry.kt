8ace6e3347a1d8bd7269bf8a4e03572972fb4f7f
rezagama <rezagamahidayat@gmail.com>
2021-11-16 16:56:40 +0700

    Add tkpddesign resource lint

--- libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/IssueRegistry.kt
+++ libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/IssueRegistry.kt
@@ -7,6 +7,7 @@ import com.tokopedia.linter.detectors.DimenResourceValueDetector
 import com.tokopedia.linter.detectors.ResourcePackageDetector
 import com.tokopedia.linter.detectors.UnsupportedColorDetector
 import com.tokopedia.linter.detectors.VectorDrawableDetector
+import com.tokopedia.linter.detectors.DeprecatedResourceDetector
 import com.tokopedia.linter.detectors.gradle.BannedDependencyDetector.DEPENDENCY_BANNED
 import com.tokopedia.linter.detectors.gradle.DeprecatedDependencyDetector.DEPENDENCY_DEPRECATED
 import com.tokopedia.linter.detectors.gradle.HANSEL_REQUIRED
@@ -31,6 +32,7 @@ class IssueRegistry : IssueRegistry() {
                 UnsupportedColorDetector.JAVA_ISSUE,
                 ResourcePackageDetector.JAVA_ISSUE,
                 DimenResourceValueDetector.ISSUE,
+                DeprecatedResourceDetector.ISSUE,
                 HANSEL_REQUIRED
         )
 
