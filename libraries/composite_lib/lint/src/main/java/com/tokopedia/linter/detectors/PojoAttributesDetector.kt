2fec5ab317298dc550b69c8eb00a9f490fb48e92
reivinoktavianus <reivin.oktavianus@tokopedia.com>
2021-11-17 15:11:30 +0700

    Filter lint check usages to certain directories and file names only

--- libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/detectors/PojoAttributesDetector.kt
+++ libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/detectors/PojoAttributesDetector.kt
@@ -6,18 +6,10 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.asJava.elements.KtLightField
 import org.jetbrains.uast.UClass
 import org.jetbrains.uast.UElement
-import org.jetbrains.uast.UField
 import org.jetbrains.uast.kotlin.KotlinUClass
-import org.jetbrains.uast.kotlin.KotlinUField
 
 
 class PojoAttributesDetector: Detector(), Detector.UastScanner {
-
-    override fun getApplicableUastTypes(): List<java.lang.Class<out UElement>> {
-        return listOf(UClass::class.java)
-    }
-
-    override fun createUastHandler(context: JavaContext): DataClassVisitor = DataClassVisitor(context)
 
     companion object {
         private const val ERROR_MESSAGE = "Attributes in response or payload POJO should be annotated with @SerializedName"
@@ -25,7 +17,8 @@ class PojoAttributesDetector: Detector(), Detector.UastScanner {
         val JAVA_ISSUE = Issue.create(
             id = "PojoAttributes",
             briefDescription = ERROR_MESSAGE,
-            explanation = "@SerializedName annotation will prevent the attribute for being obfuscated by proguard",
+            explanation = "@SerializedName annotation will prevent the attribute for being obfuscated by proguard" +
+                "Please add the @SerializedName annotation on this field/attribute. " ,
             category = Category.CORRECTNESS,
             priority = 10,
             severity = Severity.WARNING,
@@ -38,34 +31,75 @@ class PojoAttributesDetector: Detector(), Detector.UastScanner {
         private const val SERIALIZED_NAME = "com.google.gson.annotations.SerializedName"
     }
 
+    override fun getApplicableUastTypes(): List<java.lang.Class<out UElement>> {
+        return listOf(UClass::class.java)
+    }
+
+    override fun createUastHandler(context: JavaContext): DataClassVisitor = DataClassVisitor(context)
+
     class DataClassVisitor(private val context: JavaContext): UElementHandler() {
+
+        private val parentDirectoryList = listOf("/domain", "/data")
+        private val classNameList = listOf("Response", "Param", "Data")
+
         override fun visitClass(node: UClass) {
             if (node !is KotlinUClass) return
             val isDataClass = node.methods
                 .map { it.name }
                 .containsAll(listOf("equals", "hashCode"))
-            if (isDataClass) {
+            if (isDataClass && isResponsePojo(node)) {
                 val fields = node
                     .allFields
                     .filterIsInstance<KtLightField>()
-                checkFields(fields)
+                checkFields(fields, ERROR_MESSAGE)
             }
         }
 
-        private fun checkFields(fields: List<KtLightField>) {
+        private fun isResponsePojo(node: UClass): Boolean {
+            val containingDir = node.context?.getContainingFile()?.containingDirectory?.toString()
+            val fileName = node.context?.containingFile?.originalFile?.toString()
+
+            val isInsideDesignatedDirectory = containingDir?.let { dir ->
+                checkForDirectory(dir)
+            } ?: false
+            val isFileNameContainsDesignatedKeyword = fileName?.let { name ->
+                checkForClassName(name)
+            } ?: false
+            return isInsideDesignatedDirectory && isFileNameContainsDesignatedKeyword
+        }
+
+        private fun checkForDirectory(containingDir: String): Boolean {
+            parentDirectoryList.forEach { dir ->
+                if (containingDir.contains(dir)) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        private fun checkForClassName(className: String): Boolean {
+            classNameList.forEach { name ->
+                if (className.contains(name)) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        private fun checkFields(fields: List<KtLightField>, message: String) {
             fields.forEach { field ->
                 if (field.getAnnotation(SERIALIZED_NAME) == null) {
-                    report(field)
+                    report(field, message)
                 }
             }
         }
 
-        private fun report(node: PsiElement) {
+        private fun report(node: PsiElement, message: String) {
             context.report(
                 issue = JAVA_ISSUE,
                 scope = node,
                 location = context.getNameLocation(node),
-                message = ERROR_MESSAGE
+                message = message
             )
         }
     }
