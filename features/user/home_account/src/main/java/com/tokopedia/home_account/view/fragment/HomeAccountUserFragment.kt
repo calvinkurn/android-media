9b7253f6d23e4ed56f76d20b84a7058d8d6c6d98
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-11-18 18:17:07 +0700

    remove wallet elligibility

--- features/user/home_account/src/main/java/com/tokopedia/home_account/view/fragment/HomeAccountUserFragment.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/fragment/HomeAccountUserFragment.kt
@@ -585,16 +585,6 @@ open class HomeAccountUserFragment : BaseDaggerFragment(), HomeAccountUserListen
             }
         })
 
-        viewModel.walletEligible.observe(viewLifecycleOwner, Observer {
-            when (it) {
-                is Success -> {
-                    onSuccessGetWalletEligible(it.data)
-                }
-                is Fail -> {
-                    onFailedGetWalletEligible()
-                }
-            }
-        })
 
         viewModel.phoneNo.observe(viewLifecycleOwner, Observer {
             if(it.isNotEmpty()) {
@@ -643,24 +633,6 @@ open class HomeAccountUserFragment : BaseDaggerFragment(), HomeAccountUserListen
 
     private fun onFailedGetBalanceAndPoint(walletId: String) {
         balanceAndPointAdapter?.changeItemToFailedById(walletId)
-        adapter?.notifyItemChanged(0)
-    }
-
-    private fun onSuccessGetWalletEligible(walletappWalletEligibility: WalletappWalletEligibility) {
-        val eligibility = walletappWalletEligibility.data
-        if (eligibility.isNotEmpty()) {
-            if (eligibility[0].isEligible) {
-                viewModel.getBalanceAndPoint(AccountConstants.WALLET.GOPAY)
-                balanceAndPointAdapter?.removeById(AccountConstants.WALLET.TOKOPOINT)
-            } else {
-                balanceAndPointAdapter?.removeById(AccountConstants.WALLET.GOPAY)
-            }
-            adapter?.notifyItemChanged(0)
-        }
-    }
-
-    private fun onFailedGetWalletEligible() {
-        balanceAndPointAdapter?.removeById(AccountConstants.WALLET.GOPAY)
         adapter?.notifyItemChanged(0)
     }
 
