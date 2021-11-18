3345b0fcd9883d2b6561be280293ec8d5bedc172
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-11-18 13:52:54 +0700

    fixing getInstance

--- libraries/analytics_debugger_noop/src/main/java/com/tokopedia/analyticsdebugger/debugger/SSELogger.java
+++ libraries/analytics_debugger_noop/src/main/java/com/tokopedia/analyticsdebugger/debugger/SSELogger.java
@@ -5,24 +5,30 @@ import android.content.Context;
 /**
  * Created By : Jonathan Darwin on November 17, 2021
  */
-public class SSELogger implements RealtimeNetworkLoggerInterface {
+public class SSELogger {
 
-    @Override
-    public void init(String generalInfo) {
+    private static RealtimeNetworkLoggerInterface instance;
 
+    public static RealtimeNetworkLoggerInterface getInstance(Context context) {
+        if(instance == null) {
+            instance = new RealtimeNetworkLoggerInterface() {
+                @Override
+                public void init(String generalInfo) {
+
+                }
+
+                @Override
+                public void send(String event, String message) {
+
+                }
+
+                @Override
+                public void send(String event) {
+
+                }
+            };
+        }
+        return instance;
     }
 
-    @Override
-    public void send(String event, String message) {
-
-    }
-
-    @Override
-    public void send(String event) {
-
-    }
-
-    public RealtimeNetworkLoggerInterface getInstance(Context context) {
-        return new SSELogger();
-    }
 }
