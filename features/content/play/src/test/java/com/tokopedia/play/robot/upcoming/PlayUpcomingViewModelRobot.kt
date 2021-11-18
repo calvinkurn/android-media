80a768340ca8a134252acdf6f987d86cc5fd31ab
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-11-18 08:15:30 +0700

    add upcoming viewmodel robot

---
+++ features/content/play/src/test/java/com/tokopedia/play/robot/upcoming/PlayUpcomingViewModelRobot.kt
@@ -0,0 +1,133 @@
+package com.tokopedia.play.robot.upcoming
+
+import androidx.lifecycle.viewModelScope
+import com.tokopedia.play.analytic.PlayNewAnalytic
+import com.tokopedia.play.domain.GetChannelStatusUseCase
+import com.tokopedia.play.domain.GetSocketCredentialUseCase
+import com.tokopedia.play.domain.PlayChannelReminderUseCase
+import com.tokopedia.play.domain.repository.PlayViewerRepository
+import com.tokopedia.play.robot.play.PlayViewModelRobot2
+import com.tokopedia.play.view.uimodel.event.PlayUpcomingUiEvent
+import com.tokopedia.play.view.uimodel.mapper.PlayUiModelMapper
+import com.tokopedia.play.view.uimodel.state.PlayUpcomingUiState
+import com.tokopedia.play.view.viewmodel.PlayUpcomingViewModel
+import com.tokopedia.play_common.sse.PlayChannelSSE
+import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchers
+import com.tokopedia.user.session.UserSessionInterface
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
+import java.io.Closeable
+
+/**
+ * Created By : Jonathan Darwin on November 18, 2021
+ */
+class PlayUpcomingViewModelRobot(
+    private val getChannelStatusUseCase: GetChannelStatusUseCase = mockk(relaxed = true),
+    private val playChannelReminderUseCase: PlayChannelReminderUseCase = mockk(relaxed = true),
+    private val getSocketCredentialUseCase: GetSocketCredentialUseCase = mockk(relaxed = true),
+    private val dispatchers: CoroutineTestDispatchers = CoroutineTestDispatchers,
+    private val userSession: UserSessionInterface = mockk(relaxed = true),
+    private val playUiModelMapper: PlayUiModelMapper = mockk(relaxed = true),
+    private val playAnalytic: PlayNewAnalytic = mockk(relaxed = true),
+    private val playChannelSSE: PlayChannelSSE = mockk(relaxed = true),
+    private val repo: PlayViewerRepository = mockk(relaxed = true),
+): Closeable {
+
+    val viewModel = PlayUpcomingViewModel(
+        getChannelStatusUseCase= getChannelStatusUseCase,
+        playChannelReminderUseCase = playChannelReminderUseCase,
+        getSocketCredentialUseCase = getSocketCredentialUseCase,
+        dispatchers = dispatchers,
+        userSession = userSession,
+        playUiModelMapper = playUiModelMapper,
+        playAnalytic = playAnalytic,
+        playChannelSSE = playChannelSSE,
+        repo = repo,
+    )
+
+    fun recordState(fn: suspend PlayViewModelRobot2.() -> Unit): PlayUpcomingUiState {
+        val scope = CoroutineScope(dispatchers.coroutineDispatcher)
+        lateinit var uiState: PlayUpcomingUiState
+        scope.launch {
+            viewModel.uiState.collect {
+                uiState = it
+            }
+        }
+        dispatchers.coroutineDispatcher.runBlockingTest { fn() }
+        dispatchers.coroutineDispatcher.advanceUntilIdle()
+        scope.cancel()
+        return uiState
+    }
+
+    fun recordEvent(fn: suspend PlayViewModelRobot2.() -> Unit): List<PlayUpcomingUiEvent> {
+        val scope = CoroutineScope(dispatchers.coroutineDispatcher)
+        val uiEvents = mutableListOf<PlayUpcomingUiEvent>()
+        scope.launch {
+            viewModel.uiEvent.collect {
+                uiEvents.add(it)
+            }
+        }
+        dispatchers.coroutineDispatcher.runBlockingTest { fn() }
+        dispatchers.coroutineDispatcher.advanceUntilIdle()
+        scope.cancel()
+        return uiEvents
+    }
+
+    fun recordStateAndEvent(fn: suspend PlayViewModelRobot2.() -> Unit): Pair<PlayUpcomingUiState, List<PlayUpcomingUiEvent>> {
+        val scope = CoroutineScope(dispatchers.coroutineDispatcher)
+        lateinit var uiState: PlayUpcomingUiState
+        val uiEvents = mutableListOf<PlayUpcomingUiEvent>()
+        scope.launch {
+            viewModel.uiState.collect {
+                uiState = it
+            }
+        }
+        scope.launch {
+            viewModel.uiEvent.collect {
+                uiEvents.add(it)
+            }
+        }
+        dispatchers.coroutineDispatcher.runBlockingTest { fn() }
+        dispatchers.coroutineDispatcher.advanceUntilIdle()
+        scope.cancel()
+        return uiState to uiEvents
+    }
+
+    fun cancelRemainingTasks() {
+        viewModel.viewModelScope.coroutineContext.cancelChildren()
+    }
+
+    override fun close() {
+        cancelRemainingTasks()
+    }
+}
+
+fun createPlayUpcomingViewModelRobot(
+    dispatchers: CoroutineTestDispatchers = CoroutineTestDispatchers,
+    getChannelStatusUseCase: GetChannelStatusUseCase  = mockk(relaxed = true),
+    playChannelReminderUseCase: PlayChannelReminderUseCase  = mockk(relaxed = true),
+    getSocketCredentialUseCase: GetSocketCredentialUseCase  = mockk(relaxed = true),
+    userSession: UserSessionInterface  = mockk(relaxed = true),
+    playUiModelMapper: PlayUiModelMapper  = mockk(relaxed = true),
+    playAnalytic: PlayNewAnalytic  = mockk(relaxed = true),
+    playChannelSSE: PlayChannelSSE  = mockk(relaxed = true),
+    repo: PlayViewerRepository  = mockk(relaxed = true),
+    fn: PlayUpcomingViewModelRobot.() -> Unit
+): PlayUpcomingViewModelRobot {
+    return PlayUpcomingViewModelRobot(
+        getChannelStatusUseCase = getChannelStatusUseCase,
+        playChannelReminderUseCase = playChannelReminderUseCase,
+        getSocketCredentialUseCase = getSocketCredentialUseCase,
+        dispatchers = dispatchers,
+        userSession = userSession,
+        playUiModelMapper = playUiModelMapper,
+        playAnalytic = playAnalytic,
+        playChannelSSE = playChannelSSE,
+        repo = repo,
+    ).apply(fn)
+}
