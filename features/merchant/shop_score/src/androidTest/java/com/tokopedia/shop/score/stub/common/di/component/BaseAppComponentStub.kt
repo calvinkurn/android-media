e160db59fa8ae9bf4b049ee048a01e10c4c5046f
Rizqi Aryansa <rizqi.aryansa@tokopedia.com>
2021-11-19 15:12:50 +0700

    setup instrumented test and add some stub

---
+++ features/merchant/shop_score/src/androidTest/java/com/tokopedia/shop/score/stub/common/di/component/BaseAppComponentStub.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.shop.score.stub.common.di.component
+
+import com.tokopedia.abstraction.common.di.scope.ApplicationScope
+
+@ApplicationScope
+interface BaseAppComponentStub {
+}
