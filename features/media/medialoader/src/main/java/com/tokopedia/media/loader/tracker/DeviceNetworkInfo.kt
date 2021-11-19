ad8d1111eb851a2d2745b2fa0c14a67a43dc66de
isfaaghyth <isfaaghyth@gmail.com>
2021-11-19 19:07:54 +0700

    defined the 4G type as const variable

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/tracker/DeviceNetworkInfo.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/tracker/DeviceNetworkInfo.kt
@@ -19,6 +19,8 @@ object DeviceNetworkInfo {
     private const val CONN_4G = "4g"
     private const val CONN_5G = "5g"
 
+    private const val NETWORK_TYPE_LTE_CA = 19
+
     private fun getConnectionTransportType(context: Context): Int {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -52,37 +54,25 @@ object DeviceNetworkInfo {
                     val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
                     if (cm.activeNetworkInfo?.subtype == null) return CONN_UNKNOWN
                     return when (cm.activeNetworkInfo?.subtype) {
-                        // 2G:
-                        TelephonyManager.NETWORK_TYPE_GPRS, // ~ 100 kbps
-                        TelephonyManager.NETWORK_TYPE_1xRTT, // ~ 50-100 kbps
-                        TelephonyManager.NETWORK_TYPE_CDMA, // ~ 14-64 kbps
-                        TelephonyManager.NETWORK_TYPE_EDGE, // ~ 50-100 kbps
-                        TelephonyManager.NETWORK_TYPE_IDEN, // ~25 kbps
+                        TelephonyManager.NETWORK_TYPE_GPRS, // 2G ~ 100 kbps
+                        TelephonyManager.NETWORK_TYPE_1xRTT, // 2G ~ 50-100 kbps
+                        TelephonyManager.NETWORK_TYPE_CDMA, // 2G ~ 14-64 kbps
+                        TelephonyManager.NETWORK_TYPE_EDGE, // 2G ~ 50-100 kbps
+                        TelephonyManager.NETWORK_TYPE_IDEN, // 2G ~ 25 kbps
                         TelephonyManager.NETWORK_TYPE_GSM -> CONN_2G
-
-                        // 3G
-                        TelephonyManager.NETWORK_TYPE_UMTS, // ~ 400-7000 kbps
-                        TelephonyManager.NETWORK_TYPE_EVDO_0, // ~ 400-1000 kbps
-                        TelephonyManager.NETWORK_TYPE_EVDO_A, // ~ 600-1400 kbps
-                        TelephonyManager.NETWORK_TYPE_HSDPA, // ~ 2-14 Mbps
-                        TelephonyManager.NETWORK_TYPE_HSPA, // ~ 700-1700 kbps
-                        TelephonyManager.NETWORK_TYPE_HSUPA, // ~ 1-23 Mbps
-                        TelephonyManager.NETWORK_TYPE_EHRPD, // ~ 1-2 Mbps
-                        TelephonyManager.NETWORK_TYPE_EVDO_B, // ~ 5 Mbps
-                        TelephonyManager.NETWORK_TYPE_HSPAP, // ~ 10-20 Mbps
+                        TelephonyManager.NETWORK_TYPE_UMTS, // 3G ~ 400-7000 kbps
+                        TelephonyManager.NETWORK_TYPE_EVDO_0, // 3G ~ 400-1000 kbps
+                        TelephonyManager.NETWORK_TYPE_EVDO_A, // 3G ~ 600-1400 kbps
+                        TelephonyManager.NETWORK_TYPE_HSDPA, // 3G ~ 2-14 Mbps
+                        TelephonyManager.NETWORK_TYPE_HSPA, // 3G ~ 700-1700 kbps
+                        TelephonyManager.NETWORK_TYPE_HSUPA, // 3G ~ 1-23 Mbps
+                        TelephonyManager.NETWORK_TYPE_EHRPD, // 3G ~ 1-2 Mbps
+                        TelephonyManager.NETWORK_TYPE_EVDO_B, // 3G ~ 5 Mbps
+                        TelephonyManager.NETWORK_TYPE_HSPAP, // 3G ~ 10-20 Mbps
                         TelephonyManager.NETWORK_TYPE_TD_SCDMA -> CONN_3G
-
-                        //4G
-                        TelephonyManager.NETWORK_TYPE_LTE -> CONN_4G // ~ 10+ Mbps
-
-                        // LTE CA
-                        19 -> CONN_4G
-
-                        TelephonyManager.NETWORK_TYPE_NR -> CONN_5G
-
-                        //5G
-                        20 -> CONN_5G
-
+                        TelephonyManager.NETWORK_TYPE_LTE -> CONN_4G // 4G ~ 10+ Mbps
+                        NETWORK_TYPE_LTE_CA -> CONN_4G // LTE CA
+                        TelephonyManager.NETWORK_TYPE_NR -> CONN_5G  //5G
                         TelephonyManager.NETWORK_TYPE_UNKNOWN -> CONN_UNKNOWN
                         else -> CONN_UNKNOWN
                     }
