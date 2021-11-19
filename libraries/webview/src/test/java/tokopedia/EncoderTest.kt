61a01390fa9ad7465eb12ca1c740e6acfdb19f46
hendry.setiadi <hendry.setiadi@tokopedia.com>
2021-11-19 12:01:46 +0700

    add unit test for webview Helper

--- libraries/webview/src/test/java/tokopedia/EncoderTest.kt
+++ libraries/webview/src/test/java/tokopedia/EncoderTest.kt
@@ -1,44 +1,42 @@
-package com.tokopedia.webview
+package tokopedia
 
-
-import androidx.test.runner.AndroidJUnit4
+import android.os.Build
 import com.tokopedia.webview.ext.decode
 import com.tokopedia.webview.ext.encodeOnce
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
-@RunWith(AndroidJUnit4::class)
-class EncoderUtilTest {
-
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+class EncoderTest {
     @Test
     fun testEncodeOnce() {
         val url = "https://www.tokopedia.com/help"
         val encodedUrl = url.encodeOnce()
-        assertEquals("https%3A%2F%2Fwww.tokopedia.com%2Fhelp", encodedUrl)
+        Assert.assertEquals("https%3A%2F%2Fwww.tokopedia.com%2Fhelp", encodedUrl)
     }
 
     @Test
     fun testEncodeOnceTwice() {
         val url = "https://www.tokopedia.com/help"
         val encodedUrl = url.encodeOnce().encodeOnce()
-        assertEquals("https%3A%2F%2Fwww.tokopedia.com%2Fhelp", encodedUrl)
+        Assert.assertEquals("https%3A%2F%2Fwww.tokopedia.com%2Fhelp", encodedUrl)
     }
 
     @Test
     fun testDecode() {
         val url = "https%3A%2F%2Fwww.tokopedia.com%2Fhelp"
         val decoded = url.decode()
-        assertEquals("https://www.tokopedia.com/help", decoded)
+        Assert.assertEquals("https://www.tokopedia.com/help", decoded)
     }
 
     @Test
     fun testDecodeTwice() {
         val url = "https%3A%2F%2Fwww.tokopedia.com%2Fhelp"
         val decoded = url.decode().decode()
-        assertEquals("https://www.tokopedia.com/help", decoded)
+        Assert.assertEquals("https://www.tokopedia.com/help", decoded)
     }
-
-
-
 }
