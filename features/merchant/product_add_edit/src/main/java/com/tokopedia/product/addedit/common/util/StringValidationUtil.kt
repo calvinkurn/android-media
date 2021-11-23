3052c683f3516699e35c087edb972a08caf35f2d
faisalramd <faisal.ramdani@tokopedia.com>
2021-11-24 02:51:19 +0700

    change string validation

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/StringValidationUtil.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/StringValidationUtil.kt
@@ -3,7 +3,8 @@ package com.tokopedia.product.addedit.common.util
 object StringValidationUtil {
 
     fun isAllowedString(string: String): Boolean {
-        return string.all { it.isLetterOrDigit() || it.isAllowedSymbol()}
+        return string.any { it.isLetter() } &&
+                string.all { it.isLetterOrDigit() || it.isAllowedSymbol()}
     }
 
     fun Char.isAllowedSymbol(): Boolean {
