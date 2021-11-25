20e91ac99a92f053180b4fe42046a8098dd231f0
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-11-25 23:19:08 +0700

    remove expose and use core-module

--- libraries/interceptors/src/main/java/com/tokopedia/interceptors/refreshtoken/RefreshTokenModel.kt
+++ libraries/interceptors/src/main/java/com/tokopedia/interceptors/refreshtoken/RefreshTokenModel.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.interceptors.refreshtoken
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
@@ -9,37 +8,27 @@ import com.google.gson.annotations.SerializedName
 
 data class RefreshTokenModel(
     @SerializedName("login_token")
-    @Expose
     var loginToken: RefreshToken = RefreshToken()
 )
 
 
 data class RefreshToken(
     @SerializedName("acc_sid")
-    @Expose
     var accountSid: String = "",
     @SerializedName("access_token")
-    @Expose
     var accessToken: String = "",
     @SerializedName("refresh_token")
-    @Expose
     var refreshToken: String = "",
     @SerializedName("expires_in")
-    @Expose
     var expiresIn: Int = 0,
     @SerializedName("token_type")
-    @Expose
     var tokenType: String = "",
     @SerializedName("sq_check")
-    @Expose
     var sqCheck: Boolean = false,
     @SerializedName("event_code")
-    @Expose
     var eventCode: String = "",
     @SerializedName("action")
-    @Expose
     var action: Int = 0,
     @SerializedName("errors")
-    @Expose
     var errors: ArrayList<Error> = arrayListOf()
 )
