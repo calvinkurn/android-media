2ee2e5fb00f0cbb241e8a585b32df48be567c0b2
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-11-25 11:30:29 +0700

    add placeholder viewholder and delegate

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/viewholder/ProductTagPlaceholderViewHolder.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.play.broadcaster.ui.viewholder
+
+import android.view.View
+import com.tokopedia.adapterdelegate.BaseViewHolder
+import com.tokopedia.play.broadcaster.R
+
+/**
+ * Created By : Jonathan Darwin on November 25, 2021
+ */
+class ProductTagPlaceholderViewHolder(
+    itemView: View
+): BaseViewHolder(itemView) {
+
+    companion object {
+        val LAYOUT = R.layout.item_play_product_tag_placeholder
+    }
+}
--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/adapter/PlayProductTagAdapter.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/adapter/PlayProductTagAdapter.kt
@@ -2,29 +2,27 @@ package com.tokopedia.play.broadcaster.view.adapter
 
 import com.tokopedia.adapterdelegate.BaseDiffUtilAdapter
 import com.tokopedia.play.broadcaster.ui.model.ProductContentUiModel
+import com.tokopedia.play.broadcaster.ui.model.ProductUiModel
 import com.tokopedia.play.broadcaster.view.adapter.delegate.ProductTagAdapterDelegate
+import com.tokopedia.play.broadcaster.view.adapter.delegate.ProductTagPlaceholderAdapterDelegate
 
 /**
  * Created By : Jonathan Darwin on November 24, 2021
  */
-class PlayProductTagAdapter: BaseDiffUtilAdapter<ProductContentUiModel>() {
+class PlayProductTagAdapter: BaseDiffUtilAdapter<ProductUiModel>() {
 
     init {
         delegatesManager
             .addDelegate(ProductTagAdapterDelegate())
+            .addDelegate(ProductTagPlaceholderAdapterDelegate())
     }
 
-    override fun areItemsTheSame(
-        oldItem: ProductContentUiModel,
-        newItem: ProductContentUiModel
-    ): Boolean {
-        return oldItem.id == newItem.id
+    override fun areItemsTheSame(oldItem: ProductUiModel, newItem: ProductUiModel): Boolean {
+        return if(oldItem is ProductContentUiModel && newItem is ProductContentUiModel) oldItem.id == newItem.id
+        else oldItem == newItem
     }
 
-    override fun areContentsTheSame(
-        oldItem: ProductContentUiModel,
-        newItem: ProductContentUiModel
-    ): Boolean {
+    override fun areContentsTheSame(oldItem: ProductUiModel, newItem: ProductUiModel): Boolean {
         return oldItem == newItem
     }
 }
