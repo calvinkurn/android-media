df2c95eca32b82f1acad7a74b49df855cdb3df75
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-11-25 15:13:06 +0700

    fixing cast button not showing up

--- features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/activity/PlayActivity.kt
@@ -20,6 +20,7 @@ import com.tokopedia.play.R
 import com.tokopedia.play.cast.PlayCastNotificationAction
 import com.tokopedia.play.di.DaggerPlayComponent
 import com.tokopedia.play.di.PlayModule
+import com.tokopedia.play.util.PlayCastHelper
 import com.tokopedia.play.util.PlayFullScreenHelper
 import com.tokopedia.play.util.PlaySensorOrientationManager
 import com.tokopedia.play.view.contract.PlayFullscreenManager
@@ -118,6 +119,8 @@ class PlayActivity : BaseActivity(),
         startPageMonitoring()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
+
+        PlayCastHelper.getCastContext(this)
 
         removePip()
 
