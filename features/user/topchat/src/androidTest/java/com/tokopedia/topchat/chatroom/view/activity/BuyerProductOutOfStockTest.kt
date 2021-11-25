2f5c4cc08866810e3f5c73f1212d3afad471094d
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-11-25 14:37:15 +0700

    add IT BuyerProductOutOfStockTest

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/BuyerProductOutOfStockTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.test.application.annotations.UiTest
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+@UiTest
+class BuyerProductOutOfStockTest: TopchatRoomTest() {
+
+    // TODO: should show remind me button and hide empty stock label if product is upcoming campaign
+    // TODO: should show remind me button and show empty stock label if product is not upcoming campaign and inactive
+    // TODO: should show buy and atc button and hide empty stock label if product is not upcoming campaign and active
+
+}
