c2af469a70d2039908f240aea2054afcdbf03147
irfankhoirul <irfan.muhlishin@tokopedia.com>
2021-11-25 21:11:37 +0700

    render input

--- features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/viewholder/PromoInputViewHolder.kt
+++ features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/viewholder/PromoInputViewHolder.kt
@@ -2,15 +2,14 @@ package com.tokopedia.promocheckoutmarketplace.presentation.viewholder
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.promocheckoutmarketplace.R
 import com.tokopedia.promocheckoutmarketplace.databinding.PromoCheckoutMarketplaceModuleItemPromoInputBinding
 import com.tokopedia.promocheckoutmarketplace.presentation.listener.PromoCheckoutActionListener
 import com.tokopedia.promocheckoutmarketplace.presentation.uimodel.PromoInputUiModel
-import com.tokopedia.unifycomponents.TextFieldUnify
-import com.tokopedia.unifycomponents.UnifyButton
 
 class PromoInputViewHolder(private val viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding,
                            private val listener: PromoCheckoutActionListener
@@ -22,34 +21,22 @@ class PromoInputViewHolder(private val viewBinding: PromoCheckoutMarketplaceModu
 
     override fun bind(element: PromoInputUiModel) {
         with(viewBinding) {
-            if (element.uiState.isError) {
-                renderPromoInputError(element)
-            } else {
-                renderPromoInputNotError(element)
-            }
+            renderErrorState(viewBinding, element)
+            renderLoadingState(viewBinding, element)
+            renderCloseIcon(viewBinding, element)
+            renderTextField(viewBinding, element)
 
-            if (element.uiState.isLoading) {
-                buttonApplyPromo.isLoading = true
-                buttonApplyPromo.isClickable = false
-            } else {
-                buttonApplyPromo.isLoading = false
-                buttonApplyPromo.isClickable = true
-            }
+            setOnClickListeners(viewBinding, element)
 
-            if (element.uiData.promoCode.isNotBlank()) {
-                buttonApplyPromo.isEnabled = true
-                textFieldInputPromo.setFirstIcon(com.tokopedia.unifycomponents.R.drawable.unify_chips_ic_close)
-            } else {
-                buttonApplyPromo.isEnabled = false
-                textFieldInputPromo.setFirstIcon(com.tokopedia.unifyprinciples.R.color.Unify_N0)
-            }
+            element.uiState.viewHeight = itemView.height
+        }
+    }
 
-            textFieldInputPromo.textFieldInput.setOnClickListener {
-                listener.onClickPromoManualInputTextField()
-            }
-            textFieldInputPromo.textFieldInput.imeOptions = EditorInfo.IME_ACTION_DONE
-            textFieldInputPromo.textFieldInput.setText(element.uiData.promoCode)
-            textFieldInputPromo.textFieldInput.addTextChangedListener(object : TextWatcher {
+    private fun renderTextField(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
+        with(viewBinding) {
+            textFieldInputPromo.editText.imeOptions = EditorInfo.IME_ACTION_DONE
+            textFieldInputPromo.editText.setText(element.uiData.promoCode)
+            textFieldInputPromo.editText.addTextChangedListener(object : TextWatcher {
                 override fun afterTextChanged(p0: Editable?) {
                 }
 
@@ -63,76 +50,100 @@ class PromoInputViewHolder(private val viewBinding: PromoCheckoutMarketplaceModu
                     element.uiData.promoCode = text?.toString() ?: ""
                     if (text?.isNotEmpty() == true) {
                         element.uiState.isButtonSelectEnabled = true
-                        buttonApplyPromo.isEnabled = true
+                        buttonApplyPromo.show()
                         textFieldInputPromo.setFirstIcon(com.tokopedia.unifycomponents.R.drawable.unify_chips_ic_close)
                     } else {
                         element.uiState.isButtonSelectEnabled = false
-                        buttonApplyPromo.isEnabled = false
+                        buttonApplyPromo.gone()
                         textFieldInputPromo.setFirstIcon(com.tokopedia.unifyprinciples.R.color.Unify_N0)
                     }
                 }
             })
+        }
+    }
 
-            textFieldInputPromo.getFirstIcon().setOnClickListener {
-                textFieldInputPromo.textFieldInput.text.clear()
+    private fun setOnClickListeners(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
+        with(viewBinding) {
+            textFieldInputPromo.editText.setOnClickListener {
+                listener.onClickPromoManualInputTextField()
+            }
+
+            textFieldInputPromo.icon1.setOnClickListener {
+                textFieldInputPromo.editText.text.clear()
                 listener.onCLickClearManualInputPromo()
             }
 
             buttonApplyPromo.setOnClickListener {
-                val promoCode = textFieldInputPromo.textFieldInput.text.toString()
+                val promoCode = textFieldInputPromo.editText.text.toString()
                 if (promoCode.isNotEmpty()) {
                     listener.onClickApplyManualInputPromo(promoCode, element.uiState.isValidLastSeenPromo)
                 }
             }
-
-            element.uiState.viewHeight = itemView.height
         }
     }
 
-    private fun renderPromoInputError(element: PromoInputUiModel) {
+    private fun renderCloseIcon(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
         with(viewBinding) {
-            textFieldInputPromo.setError(true)
-            if (element.uiData.exception != null) {
-                textFieldInputPromo.setMessage(element.uiData.exception?.message
-                        ?: "Terjadi kesalahan. Ulangi beberapa saat lagi")
-                setPaddingViewHasError()
+            if (element.uiData.promoCode.isNotBlank()) {
+                buttonApplyPromo.show()
+                textFieldInputPromo.setFirstIcon(com.tokopedia.unifycomponents.R.drawable.unify_chips_ic_close)
             } else {
-                setPaddingViewHasNoError()
+                buttonApplyPromo.gone()
+                textFieldInputPromo.setFirstIcon(com.tokopedia.unifyprinciples.R.color.Unify_N0)
             }
         }
     }
 
-    private fun setPaddingViewHasNoError() {
-        viewBinding.root.setPadding(
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_0)?.toInt()
-                        ?: 0
-        )
-    }
-
-    private fun setPaddingViewHasError() {
-        viewBinding.root.setPadding(
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0,
-                itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
-                        ?: 0
-        )
-    }
-
-    private fun renderPromoInputNotError(element: PromoInputUiModel) {
+    private fun renderLoadingState(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
         with(viewBinding) {
-            textFieldInputPromo.setError(false)
+            if (element.uiState.isLoading) {
+                buttonApplyPromo.isLoading = true
+                buttonApplyPromo.isClickable = false
+            } else {
+                buttonApplyPromo.isLoading = false
+                buttonApplyPromo.isClickable = true
+            }
+        }
+    }
+
+    private fun renderErrorState(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
+        if (element.uiState.isError) {
+            renderPromoInputError(viewBinding, element)
+        } else {
+            renderPromoInputNotError(viewBinding)
+        }
+    }
+
+    private fun renderPromoInputError(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding, element: PromoInputUiModel) {
+        with(viewBinding) {
+            textFieldInputPromo.isInputError = true
+            if (element.uiData.exception != null) {
+                textFieldInputPromo.setMessage(element.uiData.exception?.message
+                        ?: "Terjadi kesalahan. Ulangi beberapa saat lagi")
+                setPaddingViewHasError(viewBinding)
+            } else {
+                setPaddingViewHasNoError(viewBinding)
+            }
+        }
+    }
+
+    private fun setPaddingViewHasNoError(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding) {
+        val padding16 = itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
+                ?: 0
+        viewBinding.root.setPadding(padding16, padding16, padding16, 0)
+    }
+
+    private fun setPaddingViewHasError(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding) {
+        val padding16 = itemView.context?.resources?.getDimension(com.tokopedia.abstraction.R.dimen.dp_16)?.toInt()
+                ?: 0
+        viewBinding.root.setPadding(padding16, padding16, padding16, padding16)
+    }
+
+    private fun renderPromoInputNotError(viewBinding: PromoCheckoutMarketplaceModuleItemPromoInputBinding) {
+        with(viewBinding) {
+            textFieldInputPromo.isInputError = false
             textFieldInputPromo.setMessage("")
-            setPaddingViewHasNoError()
+            setPaddingViewHasNoError(viewBinding)
         }
     }
 
