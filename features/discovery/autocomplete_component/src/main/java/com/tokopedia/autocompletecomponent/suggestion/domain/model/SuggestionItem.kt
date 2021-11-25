70eefef170aca2d2d7ba510044b08676de9adee0
Darian Thedy <darian.thedy@tokopedia.com>
2021-11-25 15:49:57 +0700

    Add expose

--- features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/domain/model/SuggestionItem.kt
+++ features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/domain/model/SuggestionItem.kt
@@ -1,59 +1,78 @@
 package com.tokopedia.autocompletecomponent.suggestion.domain.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class SuggestionItem(
         @SerializedName("template")
+        @Expose
         val template: String = "",
 
         @SerializedName("type")
+        @Expose
         val type: String = "",
 
         @SerializedName("applink")
+        @Expose
         val applink: String = "",
 
         @SerializedName("url")
+        @Expose
         val url: String = "",
 
         @SerializedName("title")
+        @Expose
         val title: String = "",
 
         @SerializedName("subtitle")
+        @Expose
         val subtitle: String = "",
 
         @SerializedName("icon_title")
+        @Expose
         val iconTitle: String = "",
 
         @SerializedName("icon_subtitle")
+        @Expose
         val iconSubtitle: String = "",
 
         @SerializedName("shortcut_url")
+        @Expose
         val shortcutUrl: String = "",
 
         @SerializedName("shortcut_image")
+        @Expose
         val shortcutImage: String = "",
 
         @SerializedName("image_url")
+        @Expose
         val imageUrl: String = "",
 
         @SerializedName("label")
+        @Expose
         val label: String = "",
 
         @SerializedName("label_type")
+        @Expose
         val labelType: String = "",
 
         @SerializedName("url_tracker")
+        @Expose
         val urlTracker: String = "",
 
         @SerializedName("tracking")
+        @Expose
         val tracking: SuggestionTracking = SuggestionTracking(),
 
         @SerializedName("discount_percentage")
+        @Expose
         val discountPercentage: String = "",
 
         @SerializedName("original_price")
+        @Expose
         val originalPrice: String = "",
 
         @SerializedName("child_items")
+        @Expose
         val suggestionChildItems: List<SuggestionChildItem> = listOf()
 )
--- features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/domain/model/SuggestionUniverse.kt
+++ features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/domain/model/SuggestionUniverse.kt
@@ -1,11 +1,14 @@
 package com.tokopedia.autocompletecomponent.suggestion.domain.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class SuggestionUniverse(
         @SerializedName("data")
+        @Expose
         val data: SuggestionData = SuggestionData(),
 
         @SerializedName("top_shops")
+        @Expose
         val topShop: List<SuggestionTopShop> = listOf()
 )
