f7918f1ed4e45613c04ba16082624e1dc7e85bb9
isfaaghyth <isfaaghyth@gmail.com>
2021-11-26 05:00:07 +0700

    created pusher mediator for both of livePusher and liveBroadcaster

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/pusher/mediator/PusherMediator.kt
@@ -0,0 +1,35 @@
+package com.tokopedia.play.broadcaster.pusher.mediator
+
+import android.content.Context
+import android.os.Handler
+import com.tokopedia.broadcaster.mediator.LivePusherConfig
+import com.tokopedia.broadcaster.widget.SurfaceAspectRatioView
+import com.tokopedia.play.broadcaster.pusher.PlayLivePusherMediatorListener
+import com.tokopedia.play.broadcaster.pusher.PlayLivePusherState
+
+interface PusherMediator {
+    val remainingDurationInMillis: Long
+    val config: LivePusherConfig
+    val ingestUrl: String
+
+    fun getLivePusherState(): PlayLivePusherState
+
+    fun init(context: Context, handler: Handler)
+    fun switchCamera()
+    fun start(url: String)
+    fun resume()
+    fun pause()
+    fun stop()
+    fun release()
+    fun setLiveStreamingDuration(durationInMillis: Long)
+    fun setLiveStreamingPauseDuration(durationInMillis: Long)
+    fun startLiveStreaming(ingestUrl: String, withTimer: Boolean)
+    fun stopLiveStreaming()
+    fun startLiveCountDownTimer()
+    fun restartLiveCountDownTimer(duration: Long)
+    fun destroy()
+    fun addListener(listener: PlayLivePusherMediatorListener)
+    fun removeListener(listener: PlayLivePusherMediatorListener)
+    fun onCameraChanged(surfaceView: SurfaceAspectRatioView)
+    fun onCameraDestroyed()
+}
