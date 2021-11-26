286e4c1b53bceace1a022f9d912fc4b25fa02444
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2021-11-26 17:49:40 +0530

    [Saldo Phase 2] Code data class cleanup done

--- features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/saldoDetail/coachmark/SaldoCoachMark.kt
+++ features/fintech/saldo_details/src/main/java/com/tokopedia/saldodetails/saldoDetail/coachmark/SaldoCoachMark.kt
@@ -2,7 +2,6 @@ package com.tokopedia.saldodetails.saldoDetail.coachmark
 
 import android.content.Context
 import android.view.View
-import androidx.annotation.StringRes
 import com.tokopedia.coachmark.CoachMark2Item
 import com.tokopedia.coachmark.CoachMarkContentPosition
 import com.tokopedia.saldodetails.R
@@ -24,11 +23,10 @@ data class SaldoCoachMark(
             anchorViewList.getOrNull(0)?.let {
                 val item = SaldoCoachMark(
                     KEY_CAN_SHOW_REFUND_COACHMARK,
-                    getCoachMarkInstance(
-                        context,
+                    CoachMark2Item(
                         it,
-                        R.string.saldo_total_balance_buyer,
-                        R.string.saldo_balance_buyer_desc
+                        context.getString(R.string.saldo_total_balance_buyer),
+                        context.getString(R.string.saldo_balance_buyer_desc)
                     )
                 )
                 list.add(item)
@@ -37,11 +35,10 @@ data class SaldoCoachMark(
             anchorViewList.getOrNull(1)?.let {
                 val item = SaldoCoachMark(
                     KEY_CAN_SHOW_INCOME_COACHMARK,
-                    getCoachMarkInstance(
-                        context,
+                    CoachMark2Item(
                         it,
-                        R.string.saldo_total_balance_seller,
-                        R.string.saldo_intro_description_seller
+                        context.getString(R.string.saldo_total_balance_seller),
+                        context.getString(R.string.saldo_intro_description_seller)
                     )
                 )
                 list.add(item)
@@ -50,11 +47,10 @@ data class SaldoCoachMark(
             anchorViewList.getOrNull(2)?.let {
                 val item = SaldoCoachMark(
                     KEY_CAN_SHOW_PENJUALAN_COACHMARK,
-                    getCoachMarkInstance(
-                        context,
+                    CoachMark2Item(
                         it,
-                        R.string.saldo_penjualan_coachmark_title,
-                        R.string.saldo_penjualan_coachmark_desc,
+                        context.getString(R.string.saldo_penjualan_coachmark_title),
+                        context.getString(R.string.saldo_penjualan_coachmark_desc),
                         CoachMarkContentPosition.BOTTOM.position
                     )
                 )
@@ -62,19 +58,6 @@ data class SaldoCoachMark(
             }
             return list
         }
-
-        private fun getCoachMarkInstance(
-            context: Context,
-            anchorView: View,
-            @StringRes titleId: Int,
-            @StringRes descriptionId: Int,
-            position: Int = -1
-        ) = CoachMark2Item(
-            anchorView,
-            context.getString(titleId),
-            context.getString(descriptionId),
-            position
-        )
     }
 
 }
