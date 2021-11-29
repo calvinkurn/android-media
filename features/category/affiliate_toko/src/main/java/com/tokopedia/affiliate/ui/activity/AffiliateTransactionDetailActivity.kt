20e8feb2f5d7df14b1ce2171d5d6d9f4cafa2610
Piyush Sinha <piyush.sinha@tokopedia.com>
2021-11-29 10:08:48 +0530

    Error State Handle

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/activity/AffiliateTransactionDetailActivity.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/activity/AffiliateTransactionDetailActivity.kt
@@ -3,6 +3,7 @@ package com.tokopedia.affiliate.ui.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,8 +18,15 @@ import com.tokopedia.affiliate.viewmodel.AffiliateTransactionDetailViewModel
 import com.tokopedia.affiliate_toko.R
 import com.tokopedia.basemvvm.viewcontrollers.BaseViewModelActivity
 import com.tokopedia.basemvvm.viewmodel.BaseViewModel
+import com.tokopedia.globalerror.GlobalError
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.LoaderUnify
 import com.tokopedia.unifyprinciples.Typography
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class AffiliateTransactionDetailActivity : BaseViewModelActivity<AffiliateTransactionDetailViewModel>() {
@@ -48,10 +56,15 @@ class AffiliateTransactionDetailActivity : BaseViewModelActivity<AffiliateTransa
     private fun afterViewCreated() {
         initRv()
         initObserver()
+        getData()
+    }
+
+    private fun getData() {
         intent?.getStringExtra(PARAM_TRANSACTION)?.let { transactionID ->
             affiliateVM.affiliateCommission(transactionID)
         }
     }
+
     private val adapter: AffiliateAdapter = AffiliateAdapter(AffiliateAdapterFactory())
     lateinit var detailsRV : RecyclerView
     private fun initRv() {
@@ -63,7 +76,8 @@ class AffiliateTransactionDetailActivity : BaseViewModelActivity<AffiliateTransa
 
     private fun initObserver() {
         affiliateVM.getErrorMessage().observe(this , {
-
+            hideView()
+            showError(it)
         })
         affiliateVM.getCommissionData().observe(this,{
             setData(it)
@@ -71,6 +85,47 @@ class AffiliateTransactionDetailActivity : BaseViewModelActivity<AffiliateTransa
         affiliateVM.getDetailList().observe(this,{
             adapter.addMoreData(it)
         })
+        affiliateVM.progressBar().observe(this, { visibility ->
+            if (visibility != null) {
+                if (visibility) {
+                   findViewById<LoaderUnify>(R.id.affiliate_progress_bar)?.show()
+                    hideView()
+                } else {
+                    findViewById<LoaderUnify>(R.id.affiliate_progress_bar)?.gone()
+                    showView()
+                }
+            }
+        })
+    }
+
+    private fun showError(it: Throwable?) {
+        findViewById<GlobalError>(R.id.commision_global_error).run {
+            when (it) {
+                is UnknownHostException, is SocketTimeoutException -> {
+                    setType(GlobalError.NO_CONNECTION)
+                }
+                is IllegalStateException -> {
+                    setType(GlobalError.PAGE_FULL)
+                }
+                else -> {
+                    setType(GlobalError.SERVER_ERROR)
+                }
+            }
+            show()
+            setActionClickListener {
+                hide()
+                getData()
+            }
+        }
+    }
+
+
+    private fun hideView() {
+        findViewById<Group>(R.id.details_view).hide()
+    }
+    private fun showView(){
+        findViewById<Group>(R.id.details_view).show()
+        findViewById<GlobalError>(R.id.commision_global_error).hide()
     }
 
     private fun setData(commissionData: AffiliateCommissionDetailsData.GetAffiliateCommissionDetail?) {
--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/viewmodel/AffiliateTransactionDetailViewModel.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/viewmodel/AffiliateTransactionDetailViewModel.kt
@@ -19,14 +19,18 @@ class AffiliateTransactionDetailViewModel  @Inject constructor(
     private var commssionData = MutableLiveData<AffiliateCommissionDetailsData.GetAffiliateCommissionDetail>()
     private var detailList = MutableLiveData<ArrayList<Visitable<AffiliateAdapterTypeFactory>>>()
     private var errorMessage = MutableLiveData<Throwable>()
+    private var progressBar = MutableLiveData<Boolean>()
     private val TYPE_DIVIDER = "divider"
     fun affiliateCommission(transactionID:String) {
         launchCatchError(block = {
+            progressBar.value = true
            affiliateCommissionDetailUserCase.affiliateCommissionDetails(transactionID).getAffiliateCommissionDetail?.let {
                detailList.value = getDetailListOrganize(it?.data?.detail)
                commssionData.value = it
            }
+            progressBar.value = false
         }, onError = {
+            progressBar.value = false
             it.printStackTrace()
             errorMessage.value = it
         })
@@ -46,4 +50,5 @@ class AffiliateTransactionDetailViewModel  @Inject constructor(
     fun getErrorMessage(): LiveData<Throwable> = errorMessage
     fun getCommissionData() : LiveData<AffiliateCommissionDetailsData.GetAffiliateCommissionDetail> = commssionData
     fun getDetailList(): LiveData<ArrayList<Visitable<AffiliateAdapterTypeFactory>>> = detailList
+    fun progressBar(): LiveData<Boolean> = progressBar
 }
