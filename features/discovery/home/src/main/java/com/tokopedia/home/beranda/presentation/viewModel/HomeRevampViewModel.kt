206be42b705efae78b3c2a869f3b7ee3682b73b8
jayantkapila <JayKay@11798>
2021-11-29 14:26:16 +0530

    build fail issue

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/viewModel/HomeRevampViewModel.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/viewModel/HomeRevampViewModel.kt
@@ -131,9 +131,7 @@ open class HomeRevampViewModel @Inject constructor(
     private val homeDispatcher: Lazy<CoroutineDispatchers>,
     private val playWidgetTools: Lazy<PlayWidgetTools>,
     private val getWalletAppBalanceUseCase: Lazy<GetWalletAppBalanceUseCase>,
-    private val getWalletEligibilityUseCase: Lazy<GetWalletEligibilityUseCase>,
-    private val getQuestWidgetUsecase : Lazy<QuestWidgetUseCase>
-) : BaseCoRoutineScope(homeDispatcher.get().io) {
+    private val getWalletEligibilityUseCase: Lazy<GetWalletEligibilityUseCase>) : BaseCoRoutineScope(homeDispatcher.get().io) {
 
     companion object {
         private const val HOME_LIMITER_KEY = "HOME_LIMITER_KEY"
@@ -1681,29 +1679,29 @@ open class HomeRevampViewModel @Inject constructor(
     }
 
     private fun getQuestWidgetData() {
-        if(getQuestWidgetDataJob?.isActive == true) return
-        findWidget<QuestWidgetModel> { questModel, index ->
-            getQuestWidgetDataJob = launchCatchError(coroutineContext, {
-                val results = getQuestWidgetUsecase.get().getQuestData(
-                    getQuestWidgetUsecase.get().getQueryParams(
-                        0,
-                        "",
-                        QuestWidgetLocations.MY_REWARD
-                )
-                )
-                if (results.widgetData?.questWidgetList?.questWidgetList?.isNotEmpty() == true) {
-                    val newQuestModel = questModel.copy(questData = results)
-                    updateWidget(newQuestModel, index)
-                } else {
-                    if (userSession.get().isLoggedIn) {
-                        deleteWidget(questModel, index)
-                    }
-                }
-            }){
-                it.printStackTrace()
-                deleteWidget(questModel, index)
-            }
-        }
+//        if(getQuestWidgetDataJob?.isActive == true) return
+//        findWidget<QuestWidgetModel> { questModel, index ->
+//            getQuestWidgetDataJob = launchCatchError(coroutineContext, {
+//                val results = getQuestWidgetUsecase.get().getQuestData(
+//                    getQuestWidgetUsecase.get().getQueryParams(
+//                        0,
+//                        "",
+//                        QuestWidgetLocations.MY_REWARD
+//                )
+//                )
+//                if (results.widgetData?.questWidgetList?.questWidgetList?.isNotEmpty() == true) {
+//                    val newQuestModel = questModel.copy(questData = results)
+//                    updateWidget(newQuestModel, index)
+//                } else {
+//                    if (userSession.get().isLoggedIn) {
+//                        deleteWidget(questModel, index)
+//                    }
+//                }
+//            }){
+//                it.printStackTrace()
+//                deleteWidget(questModel, index)
+//            }
+//        }
     }
 
     fun getBeautyFest(data: List<Visitable<*>>) {
