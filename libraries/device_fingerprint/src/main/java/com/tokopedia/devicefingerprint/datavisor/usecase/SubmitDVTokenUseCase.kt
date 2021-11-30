66475ff5cb18e30c76f661b575634f7279cb80f8
hendry.setiadi <hendry.setiadi@tokopedia.com>
2021-11-30 11:18:30 +0700

    use gqlQueryInterface for SubmitDVTokenUseCase.kt

--- libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/datavisor/usecase/SubmitDVTokenUseCase.kt
+++ libraries/device_fingerprint/src/main/java/com/tokopedia/devicefingerprint/datavisor/usecase/SubmitDVTokenUseCase.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.devicefingerprint.datavisor.usecase
 
 import com.tokopedia.devicefingerprint.datavisor.response.SubmitDeviceInitResponse
+import com.tokopedia.gql_query_annotation.GqlQueryInterface
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import javax.inject.Inject
@@ -16,23 +17,13 @@ class SubmitDVTokenUseCase @Inject constructor(val repository: dagger.Lazy<Graph
         private const val PARAM_DEVICE_TYPE = "device_type"
         private const val PARAM_CHECK_FORCE = "check_force_initialize"
         private const val ANDROID = "android"
-        private val query = """
-            mutation subDvcIntlEvent(${'$'}input: SubDvcIntlEventRequest!){
-              subDvcIntlEvent(input: ${'$'}input) {
-                is_error
-                data {
-                  is_expire
-                }
-              }
-            }
-        """.trimIndent()
     }
 
     private fun getOrCreateUseCase(): GraphqlUseCase<SubmitDeviceInitResponse> {
         val useCaseTemp = useCase
         if (useCaseTemp == null) {
             val newUseCase = GraphqlUseCase<SubmitDeviceInitResponse>(repository.get())
-            newUseCase.setGraphqlQuery(query)
+            newUseCase.setGraphqlQuery(SubDvcIntlEventQuery())
             newUseCase.setTypeClass(SubmitDeviceInitResponse::class.java)
             useCase = newUseCase
             return newUseCase
@@ -41,18 +32,34 @@ class SubmitDVTokenUseCase @Inject constructor(val repository: dagger.Lazy<Graph
         }
     }
 
-    suspend fun execute(key: String, retryCount: Int, errorMessage: String,
-                        deviceType: String = ANDROID,
-                        checkForce: Boolean = false): SubmitDeviceInitResponse {
+    suspend fun execute(
+        key: String, retryCount: Int, errorMessage: String,
+        deviceType: String = ANDROID,
+        checkForce: Boolean = false
+    ): SubmitDeviceInitResponse {
         val useCase = getOrCreateUseCase()
         val params = mapOf(
-                PARAM_KEY to key,
-                PARAM_RETRY_COUNT to retryCount,
-                PARAM_ERROR_MESSAGE to errorMessage,
-                PARAM_DEVICE_TYPE to deviceType,
-                PARAM_CHECK_FORCE to checkForce
+            PARAM_KEY to key,
+            PARAM_RETRY_COUNT to retryCount,
+            PARAM_ERROR_MESSAGE to errorMessage,
+            PARAM_DEVICE_TYPE to deviceType,
+            PARAM_CHECK_FORCE to checkForce
         )
         useCase.setRequestParams(params)
         return useCase.executeOnBackground()
+    }
+}
+
+class SubDvcIntlEventQuery : GqlQueryInterface {
+
+    override fun getOperationNameList(): List<String> {
+        return listOf("subDvcIntlEvent")
+    }
+
+    override fun getQuery() =
+        "mutation subDvcIntlEvent(\$input: SubDvcIntlEventRequest!){ subDvcIntlEvent(input: \$input) { is_error data { is_expire } } }"
+
+    override fun getTopOperationName(): String {
+        return "subDvcIntlEvent"
     }
 }
