324ffce722c09131982444af40cf333c8c193ad4
Muhammad Furqan <furqan@tokopedia.com>
2021-12-01 02:07:52 +0700

    Create enumeration for Digital Goods Unification Card

---
+++ features/recharge/recharge_component/src/main/java/com/tokopedia/recharge_component/digital_card/presentation/model/DigitalCardEnum.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.recharge_component.digital_card.presentation.model
+
+enum class MediaType(val value: String) {
+    FULL("full"),
+    FRAME("frame")
+}
+
+enum class RatingType(val value: String) {
+    SQUARE("square"),
+    STAR("star")
+}
