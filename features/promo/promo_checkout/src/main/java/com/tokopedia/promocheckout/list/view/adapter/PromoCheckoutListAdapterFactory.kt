e94d0ba220dc9c8496b5dcd0053cc72b59fb5499
astidhiyaa <asti.anzaria@tokopedia.com>
2021-12-01 19:25:52 +0800

    fix: change to abstarction

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoCheckoutListAdapterFactory.kt
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoCheckoutListAdapterFactory.kt
@@ -5,7 +5,6 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.factory.BaseAdapterTypeFactory
 import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyViewHolder
 import com.tokopedia.promocheckout.list.model.listcoupon.PromoCheckoutListModel
 
 class PromoCheckoutListAdapterFactory(val listenerTrackingCoupon: PromoCheckoutListViewHolder.ListenerTrackingCoupon) : BaseAdapterTypeFactory() {
@@ -15,14 +14,14 @@ class PromoCheckoutListAdapterFactory(val listenerTrackingCoupon: PromoCheckoutL
     }
 
     override fun type(viewModel: EmptyModel): Int {
-        return EmptyViewHolder.LAYOUT
+        return PromoCheckoutListEmptyViewHolder.LAYOUT
     }
 
     override fun createViewHolder(parent: View?, type: Int): AbstractViewHolder<out Visitable<*>> {
         if(type == PromoCheckoutListViewHolder.LAYOUT){
             return PromoCheckoutListViewHolder(parent, listenerTrackingCoupon)
-        } else if (type == EmptyViewHolder.LAYOUT) {
-            return EmptyViewHolder(parent)
+        } else if (type == PromoCheckoutListEmptyViewHolder.LAYOUT) {
+            return PromoCheckoutListEmptyViewHolder(parent)
         }
         return super.createViewHolder(parent, type)
     }
