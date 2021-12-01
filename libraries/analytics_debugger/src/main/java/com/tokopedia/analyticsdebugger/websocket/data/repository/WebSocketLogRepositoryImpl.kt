1dbd655afe8ce5bc2a011b260987967734e39c1b
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-12-01 13:07:00 +0700

    add repository

---
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/websocket/data/repository/WebSocketLogRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.analyticsdebugger.websocket.data.repository
+
+/**
+* Created By : Jonathan Darwin on December 01, 2021
+*/class WebSocketLogRepositoryImpl {
+}
---
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/websocket/domain/repository/WebSocketLogRespository.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.analyticsdebugger.websocket.domain.repository
+
+/**
+* Created By : Jonathan Darwin on December 01, 2021
+*/class WebSocketLogRespository {
+}
