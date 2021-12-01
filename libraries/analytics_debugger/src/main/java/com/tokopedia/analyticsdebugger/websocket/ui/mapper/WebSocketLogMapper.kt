5d94ef922d9b1df886a53f30bdce66442902a3b5
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-12-01 13:14:26 +0700

    add uimodel and mapper

---
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/websocket/ui/mapper/WebSocketLogMapper.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.analyticsdebugger.websocket.ui.mapper
+
+/**
+* Created By : Jonathan Darwin on December 01, 2021
+*/class WebSocketLogMapper {
+}
---
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/websocket/ui/uimodel/WebSocketLogGeneralInfoUiModel.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.analyticsdebugger.websocket.ui.uimodel
+
+/**
+* Created By : Jonathan Darwin on December 01, 2021
+*/class WebSocketLogGeneralInfoUiModel {
+}
