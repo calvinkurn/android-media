1552ef50bd73bab080a7b89c4242cc9511a767aa
astidhiyaa <asti.anzaria@tokopedia.com>
2021-12-01 19:41:33 +0800

    Revert "fix: change to abstarction"
    This reverts commit e94d0ba2

--- features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoCheckoutListEmptyViewHolder.java
+++ features/promo/promo_checkout/src/main/java/com/tokopedia/promocheckout/list/view/adapter/PromoCheckoutListEmptyViewHolder.java
@@ -4,14 +4,15 @@ import android.view.View;
 
 import androidx.annotation.LayoutRes;
 
-import com.tokopedia.abstraction.base.view.adapter.viewholders.EmptyViewHolder;
+import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel;
+import com.tokopedia.abstraction.base.view.adapter.viewholders.BaseEmptyViewHolder;
 import com.tokopedia.promocheckout.R;
 
 /**
  * @author kulomady on 1/24/17.
  */
 
-public class PromoCheckoutListEmptyViewHolder extends EmptyViewHolder {
+public class PromoCheckoutListEmptyViewHolder extends BaseEmptyViewHolder<EmptyModel> {
 
     @LayoutRes
     public static final int LAYOUT = R.layout.item_list_promo_checkout_empty;
@@ -22,5 +23,10 @@ public class PromoCheckoutListEmptyViewHolder extends EmptyViewHolder {
 
     public PromoCheckoutListEmptyViewHolder(View itemView, Callback callback) {
         super(itemView, callback);
+    }
+
+    @Override
+    public void bind(EmptyModel element) {
+        super.bind(element);
     }
 }
