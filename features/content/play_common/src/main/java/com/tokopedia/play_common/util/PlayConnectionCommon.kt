78851107a20b0bb2c46dbd0d2b5435d32c47790d
astidhiyaa <asti.anzaria@tokopedia.com>
2021-12-02 18:39:58 +0800

    fix: add try catch

--- features/content/play_common/src/main/java/com/tokopedia/play_common/util/PlayConnectionCommon.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/util/PlayConnectionCommon.kt
@@ -67,7 +67,7 @@ object PlayConnectionCommon {
                     }
                 }
             }
-            return result
+            result
         } catch (e: Exception){
             false
         }
