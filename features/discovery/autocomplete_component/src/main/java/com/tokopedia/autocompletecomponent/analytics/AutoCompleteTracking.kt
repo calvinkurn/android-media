10bab356d8eaca629903dbaf566cae261e21b847
Darian Thedy <darian.thedy@tokopedia.com>
2021-12-02 17:02:50 +0700

    Fix auto complete manual enter

--- features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/analytics/AutoCompleteTracking.kt
+++ features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/analytics/AutoCompleteTracking.kt
@@ -82,6 +82,42 @@ open class AutoCompleteTracking(
         )
     }
 
+    open fun eventClickSubmitInitialState(
+        keyword: String,
+        pageSource: String,
+        searchResultApplink: String,
+        fallback: () -> Unit,
+    ) {
+        SearchComponentTrackingRollence.click(
+            searchComponentTracking(
+                keyword = keyword,
+                componentId = INITIAL_STATE_MANUAL_ENTER,
+                dimension90 = pageSource,
+                applink = searchResultApplink,
+            ),
+            AUTOCOMPLETE_INITIAL_STATE_COMPONENT_TRACKING,
+            fallback,
+        )
+    }
+
+    open fun eventClickSubmitAutoComplete(
+        keyword: String,
+        pageSource: String,
+        searchResultApplink: String,
+        fallback: () -> Unit,
+    ) {
+        SearchComponentTrackingRollence.click(
+            searchComponentTracking(
+                keyword = keyword,
+                componentId = AUTO_COMPLETE_MANUAL_ENTER,
+                dimension90 = pageSource,
+                applink = searchResultApplink,
+            ),
+            AUTOCOMPLETE_SUGGESTION_COMPONENT_TRACKING,
+            fallback,
+        )
+    }
+
     open fun eventClickSubmitTokoNow(label: String) {
         TrackApp.getInstance().gtm.sendGeneralEvent(
             DataLayer.mapOf(
@@ -93,42 +129,6 @@ open class AutoCompleteTracking(
                 CURRENT_SITE, TOKOPEDIA_MARKETPLACE,
             )
         )
-    }
-
-    open fun eventClickSubmitInitialState(
-        keyword: String,
-        pageSource: String,
-        searchResultApplink: String,
-    ) {
-        SearchComponentTrackingRollence.click(
-            searchComponentTracking(
-                keyword = keyword,
-                componentId = INITIAL_STATE_MANUAL_ENTER,
-                dimension90 = pageSource,
-                applink = searchResultApplink,
-            ),
-            AUTOCOMPLETE_INITIAL_STATE_COMPONENT_TRACKING
-        ) {
-            eventClickSubmit(keyword)
-        }
-    }
-
-    open fun eventClickSubmitAutoComplete(
-        keyword: String,
-        pageSource: String,
-        searchResultApplink: String,
-    ) {
-        SearchComponentTrackingRollence.click(
-            searchComponentTracking(
-                keyword = keyword,
-                componentId = AUTO_COMPLETE_MANUAL_ENTER,
-                dimension90 = pageSource,
-                applink = searchResultApplink,
-            ),
-            AUTOCOMPLETE_SUGGESTION_COMPONENT_TRACKING
-        ) {
-            eventClickSubmit(keyword)
-        }
     }
 
     open fun eventClickSubmit(label: String) {
