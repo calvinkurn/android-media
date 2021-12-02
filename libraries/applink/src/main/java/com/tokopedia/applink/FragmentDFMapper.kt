25bf09118a99f9ebf6c5f7df7c93cbdb3bc62271
CJ <calvyn.julian@tokopedia.com>
2021-12-02 16:35:29 +0700

    add config for testing

--- libraries/applink/src/main/java/com/tokopedia/applink/FragmentDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/FragmentDFMapper.kt
@@ -35,7 +35,7 @@ object FragmentDFMapper {
              */
             add(FragmentDFPattern(
                     {it == FragmentConst.SHOP_SHOWCASE_TAB_FRAGMENT_CLASS_PATH},
-                    DeeplinkDFMapper.DF_BASE,
+                    DeeplinkDFMapper.DF_MERCHANT_LOGIN,
                     R.string.shop_showcase_tab
             ))
         }
