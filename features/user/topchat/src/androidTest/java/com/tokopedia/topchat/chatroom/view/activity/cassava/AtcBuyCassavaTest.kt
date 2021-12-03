4e4043e51ae59dedcfa95699289f3078b4f16423
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-12-03 14:33:26 +0700

    add CassavaTest annotation

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/cassava/AtcBuyCassavaTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/cassava/AtcBuyCassavaTest.kt
@@ -3,12 +3,14 @@ package com.tokopedia.topchat.chatroom.view.activity.cassava
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import com.tokopedia.cassavatest.CassavaTestRule
 import com.tokopedia.cassavatest.hasAllSuccess
+import com.tokopedia.test.application.annotations.CassavaTest
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
 import com.tokopedia.topchat.chatroom.view.activity.robot.product.ProductCardRobot.clickATCButtonAt
 import com.tokopedia.topchat.chatroom.view.activity.robot.product.ProductCardRobot.clickBuyButtonAt
 import org.junit.Rule
 import org.junit.Test
 
+@CassavaTest
 class AtcBuyCassavaTest: TopchatRoomTest() {
 
     @get:Rule
