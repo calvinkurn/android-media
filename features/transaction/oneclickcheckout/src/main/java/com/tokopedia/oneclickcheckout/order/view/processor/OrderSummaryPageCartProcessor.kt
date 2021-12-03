c58bd5648d902ed16d5841b5e348c00ded85a590
irfankhoirul <irfan.muhlishin@tokopedia.com>
2021-12-03 14:34:42 +0700

    revert unused import

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/view/processor/OrderSummaryPageCartProcessor.kt
@@ -14,7 +14,6 @@ import com.tokopedia.oneclickcheckout.order.data.update.UpdateCartOccRequest.Com
 import com.tokopedia.oneclickcheckout.order.domain.GetOccCartUseCase
 import com.tokopedia.oneclickcheckout.order.domain.UpdateCartOccUseCase
 import com.tokopedia.oneclickcheckout.order.view.model.*
-import com.tokopedia.purchase_platform.common.utils.Utils
 import dagger.Lazy
 import kotlinx.coroutines.withContext
 import timber.log.Timber
