4cc5db95dd77d78914f581e68b5abed0a361c51a
yorisprayogo <yoris.prayogo@tokopedia.com>
2021-12-03 11:22:02 +0700

    normalize phone number

--- features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/util/AuthenticityUtils.kt
+++ features/user/sessioncommon/src/main/java/com/tokopedia/sessioncommon/util/AuthenticityUtils.kt
@@ -5,9 +5,14 @@ import com.tkpd.util.Base64
 import com.tokopedia.encryption.security.sha256
 
 object AuthenticityUtils {
+    fun normalizePhoneNumber(phoneNum: String): String {
+        return if (phoneNum.isNotEmpty()) phoneNum.replaceFirst("^0(?!$)".toRegex(), "62") else ""
+    }
+
     fun generateAuthenticity(msisdn: String, timeUnix: String, context: Context): String {
+        val normalizedMsisdn = normalizePhoneNumber(msisdn)
         val encoder = Base64.GetDecoder(context).trim()
-        val content = (msisdn + timeUnix + encoder)
+        val content = (normalizedMsisdn + timeUnix + encoder)
         return content.sha256()
     }
 }
