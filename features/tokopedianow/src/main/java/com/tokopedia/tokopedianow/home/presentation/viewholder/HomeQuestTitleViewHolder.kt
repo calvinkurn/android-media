aad6395e4b3b2a39eb7bb788e939af32c96a4185
saidfaisal <saidfaisal777@gmail.com>
2021-12-06 14:02:29 +0700

    add error state quest title widget

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/viewholder/HomeQuestTitleViewHolder.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/viewholder/HomeQuestTitleViewHolder.kt
@@ -2,6 +2,9 @@ package com.tokopedia.tokopedianow.home.presentation.viewholder
 
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.view.hide
@@ -25,19 +28,32 @@ class HomeQuestTitleViewHolder(
     override fun bind(element: HomeQuestTitleUiModel) {
         hideShimmering()
         if (element.isErrorState) {
-            // not yet
+            setErrorState()
         } else {
-            binding?.iuGift?.setImage(newIconId = IconUnify.GIFT)
-            binding?.tpCounter?.text = String.format("${element.currentQuestFinished}/${element.totalQuestTarget}")
+            setCounter(element)
         }
     }
 
     private fun hideShimmering() {
         binding?.questTitleWidgetShimmering?.root?.hide()
-        showQuestWidget()
     }
 
-    private fun showQuestWidget() {
+    private fun setCounter(element: HomeQuestTitleUiModel) {
+        binding?.questTitleWidgetErrorState?.root?.hide()
         binding?.questTitleWidget?.show()
+        binding?.iuGift?.setImage(newIconId = IconUnify.GIFT)
+        binding?.tpCounter?.text = String.format("${element.currentQuestFinished}/${element.totalQuestTarget}")
+    }
+
+    private fun setErrorState() {
+        binding?.questTitleWidgetErrorState?.root?.show()
+        binding?.questTitleWidget?.hide()
+        binding?.questTitleWidgetErrorState?.apply {
+            val unifyColor = ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_YN500)
+            sivRefresh.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(unifyColor, BlendModeCompat.SRC_ATOP)
+            sivRefresh.setOnClickListener {
+
+            }
+        }
     }
 }
