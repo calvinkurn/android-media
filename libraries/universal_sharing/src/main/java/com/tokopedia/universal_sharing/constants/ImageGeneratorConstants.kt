9d12e2b1bcb3310f507e4d58b652fbe09e053a03
varun0922 <varun.sharma@tokopedia.com>
2021-12-06 13:45:15 +0530

    adding the keys for FS toko and Rilisan Spesial template

--- libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/constants/ImageGeneratorConstants.kt
+++ libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/constants/ImageGeneratorConstants.kt
@@ -2,7 +2,9 @@ package com.tokopedia.universal_sharing.constants
 
 class ImageGeneratorConstants {
     object ImageGeneratorSourceId{
-        const val PDP_SOURCE_ID = "gFZoml"
+        const val PDP = "gFZoml"
+        const val FS_TOKO = "tJHYeb"
+        const val RILISAN_SPESIAL = "aaGBeS"
     }
     object ImageGeneratorKeys{
         const val PRODUCT_IMAGE_URL = "product_image_url"
@@ -11,6 +13,32 @@ class ImageGeneratorConstants {
         const val IS_BEBAS_ONGKIR = "is_bebas_ongkir"
         const val PLATFORM = "platform"
         const val PRODUCT_RATING = "product_rating"
+        const val SHOP_LOGO = "shop-logo"
+        const val SHOP_NAME = "shop-name"
+        const val BADGE = "badge"
+        const val DATE = "date"
+        const val DISCOUNT = "discount"
+        const val ONGOING = "ongoing"
+        const val PRODUCTS_COUNT = "products-count"
+        const val PRODUCTS_OVERLOAD = "products-overload"
+        const val PRODUCT_1 = "product-1"
+        const val PRODUCT_1_PRICE_BEFORE = "product-1_price-before"
+        const val PRODUCT_1_PRICE_AFTER = "product-1_price-after"
+        const val PRODUCT_1_DISCOUNT = "product-1_discount"
+        const val PRODUCT_2 = "product-2"
+        const val PRODUCT_2_PRICE_BEFORE = "product-2_price-before"
+        const val PRODUCT_2_PRICE_AFTER = "product-2_price-after"
+        const val PRODUCT_2_DISCOUNT = "product-2_discount"
+        const val PRODUCT_3 = "product-3"
+        const val PRODUCT_3_PRICE_BEFORE = "product-3_price-before"
+        const val PRODUCT_3_PRICE_AFTER = "product-3_price-after"
+        const val PRODUCT_3_DISCOUNT = "product-3_discount"
+        const val PRODUCT_4 = "product-4"
+        const val PRODUCT_4_PRICE_BEFORE = "product-4_price-before"
+        const val PRODUCT_4_PRICE_AFTER = "product-4_price-after"
+        const val PRODUCT_4_DISCOUNT = "product-4_discount"
+        const val CAMPAIGN_NAME = "campaign-name"
+        const val CAMPAIGN_INFO = "campaign-info"
     }
     object ImageGeneratorPlatforms{
         const val WHATSAPP = "wa"
