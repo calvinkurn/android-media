0b62fd5e8f8354e1c5621eaf7d48f3b652ff94c3
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2021-12-07 16:17:58 +0700

    fixing home analytics and cassava feed

--- features/discovery/home/src/main/java/com/tokopedia/home/analytics/v2/HomePlayWidgetAnalyticListener.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/analytics/v2/HomePlayWidgetAnalyticListener.kt
@@ -90,6 +90,7 @@ class HomePlayWidgetAnalyticListener(
 
     override fun onImpressChannelCard(view: PlayWidgetMediumView, item: PlayWidgetMediumChannelUiModel, channelPositionInList: Int, isAutoPlay: Boolean, recommendationType: String) {
         val finalChannelPositionInList = channelPositionInList + 1
+        val promoText = if (item.promoType.promoText.isNotBlank()) item.promoType.promoText else "no promo"
         val trackerMap = BaseTrackerBuilder().constructBasicPromotionView(
                 event = Event.PROMO_VIEW,
                 eventCategory = "homepage-cmp",
@@ -100,7 +101,7 @@ class HomePlayWidgetAnalyticListener(
                         "$mBusinessWidgetPosition - " +
                         "$isAutoPlay - " +
                         "${item.poolType} - " +
-                        if (item.promoType.promoText.isNotBlank()) item.promoType.promoText else "no promo" + " - " +
+                        "$promoText - " +
                         recommendationType,
                 promotions = listOf(
                         BaseTrackerConst.Promotion(
@@ -122,6 +123,7 @@ class HomePlayWidgetAnalyticListener(
 
     override fun onClickChannelCard(view: PlayWidgetMediumView, item: PlayWidgetMediumChannelUiModel, channelPositionInList: Int, isAutoPlay: Boolean, recommendationType: String) {
         val finalChannelPositionInList = channelPositionInList + 1
+        val promoText = if (item.promoType.promoText.isNotBlank()) item.promoType.promoText else "no promo"
         val trackerMap = BaseTrackerBuilder().constructBasicPromotionClick(
                 event = Event.PROMO_CLICK,
                 eventCategory = "homepage-cmp",
@@ -133,7 +135,7 @@ class HomePlayWidgetAnalyticListener(
                         "$mBusinessWidgetPosition - " +
                         "$isAutoPlay - " +
                         "${item.poolType} - " +
-                        if (item.promoType.promoText.isNotBlank()) item.promoType.promoText else "no promo" + " - " +
+                        "$promoText - " +
                         recommendationType,
                 promotions = listOf(
                         BaseTrackerConst.Promotion(
