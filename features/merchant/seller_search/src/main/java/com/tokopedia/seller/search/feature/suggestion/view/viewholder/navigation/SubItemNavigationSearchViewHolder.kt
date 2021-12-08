99615db5b560f2f37c3e320ff9d090ffed290335
ilhamsuaib <ilhamsuaib10@gmail.com>
2021-12-08 14:58:52 +0800

    seller search for statistic

---
+++ features/merchant/seller_search/src/main/java/com/tokopedia/seller/search/feature/suggestion/view/viewholder/navigation/SubItemNavigationSearchViewHolder.kt
@@ -0,0 +1,25 @@
+package com.tokopedia.seller.search.feature.suggestion.view.viewholder.navigation
+
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.seller.search.databinding.ItemSubSearchResultNavigationBinding
+import com.tokopedia.seller.search.feature.suggestion.view.model.sellersearch.NavigationSellerSearchSubItemUiModel
+
+/**
+ * Created by @ilhamsuaib on 08/12/21.
+ */
+
+class SubItemNavigationSearchViewHolder(
+    private val binding: ItemSubSearchResultNavigationBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(model: NavigationSellerSearchSubItemUiModel) {
+        with(binding) {
+            chipSearchSubItem.chipText = model.title
+            //chipSearchSubItem.chip_right_icon =
+            chipSearchSubItem.setOnClickListener {
+                RouteManager.route(root.context, model.appLink)
+            }
+        }
+    }
+}
