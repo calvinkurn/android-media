5f5c0a46a9a63446ce73e616fc09193dfff80af8
Muhammad Furqan <furqan@tokopedia.com>
2021-12-09 02:04:51 +0700

    [Recharge][DGU] Rename Files

--- features/recharge/recharge_component/src/main/java/com/tokopedia/recharge_component/digital_card/presentation/adapter/DigitalUnificationCardTypeFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.recharge_component.digital_card.presentation.adapter
-
-import com.tokopedia.abstraction.base.view.adapter.factory.AdapterTypeFactory
-import com.tokopedia.recharge_component.digital_card.presentation.model.DigitalUnificationModel
-
-interface DigitalUnificationCardTypeFactory : AdapterTypeFactory {
-    fun type(digitalUnificationModel: DigitalUnificationModel): Int
-}
