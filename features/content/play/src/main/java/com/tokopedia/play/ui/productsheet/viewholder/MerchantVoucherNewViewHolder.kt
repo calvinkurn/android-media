185c64f5a9d6c1951c622f5d301b4e4b465916fe
astidhiyaa <asti.anzaria@tokopedia.com>
2021-12-08 10:51:27 +0800

    fix: from feedback

--- features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/viewholder/MerchantVoucherNewViewHolder.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/viewholder/MerchantVoucherNewViewHolder.kt
@@ -4,8 +4,9 @@ import android.view.View
 import android.widget.TextView
 import com.tokopedia.adapterdelegate.BaseViewHolder
 import com.tokopedia.iconunify.IconUnify
-import com.tokopedia.kotlin.extensions.view.isMoreThanZero
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.play.R
 import com.tokopedia.play.view.uimodel.MerchantVoucherUiModel
 import com.tokopedia.utils.date.DateUtil
@@ -18,18 +19,23 @@ class MerchantVoucherNewViewHolder(
     private val listener: Listener
 ) : BaseViewHolder(itemView) {
 
-    private val tvVoucherTitle: TextView = itemView.findViewById(R.id.tvCouponTitle)
-    private val tvVoucherDescription: TextView = itemView.findViewById(R.id.tvMinTrax)
-    private val tvVoucherStock: TextView = itemView.findViewById(R.id.tvCouponStock)
-    private val tvVoucherExpiredDate: TextView = itemView.findViewById(R.id.tvExpiredDate)
-    private val ivCopyVoucher: IconUnify = itemView.findViewById(R.id.ivCopyVoucher)
+    private val tvVoucherTitle: TextView = itemView.findViewById(R.id.tv_coupon_title)
+    private val tvVoucherDescription: TextView = itemView.findViewById(R.id.tv_min_tranx)
+    private val tvVoucherStock: TextView = itemView.findViewById(R.id.tv_coupon_stock)
+    private val tvVoucherExpiredDate: TextView = itemView.findViewById(R.id.tv_expired_date)
+    private val ivCopyVoucher: IconUnify = itemView.findViewById(R.id.iv_copy_voucher)
 
     fun bind(item: MerchantVoucherUiModel) {
         tvVoucherTitle.text = item.title
         tvVoucherDescription.text = item.description
-        tvVoucherStock.shouldShowWithAction(item.voucherStock.isMoreThanZero()){
+
+        if (item.voucherStock > 0){
+            tvVoucherStock.show()
             tvVoucherStock.text = getString(R.string.play_voucher_sheet_coupon_stock, item.voucherStock.toString())
+        }else{
+            tvVoucherStock.hide()
         }
+
         tvVoucherExpiredDate.shouldShowWithAction(item.expiredDate.isNotEmpty()){
             tvVoucherExpiredDate.text = getString(R.string.play_voucher_sheet_coupon_expired, countDays(item.expiredDate).toString())
         }
--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ShopCouponSheetViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ShopCouponSheetViewComponent.kt
@@ -22,7 +22,7 @@ import com.tokopedia.play_common.viewcomponent.ViewComponent
 /**
  * @author by astidhiyaa on 25/11/21
  */
-class ShopCouponSheetViewComponent (container: ViewGroup,
+class ShopCouponSheetViewComponent(container: ViewGroup,
                                     private val listener: Listener
 ) : ViewComponent(container, R.id.cl_shop_coupon_sheet){
 
