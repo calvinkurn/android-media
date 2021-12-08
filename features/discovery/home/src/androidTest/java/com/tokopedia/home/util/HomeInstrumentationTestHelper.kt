a0f7a665a4ac42118409b8cc22987587e75cef96
devarafikry <devara.akmal@tokopedia.com>
2021-12-08 12:21:49 +0700

    move delete database before activity launch

--- features/discovery/home/src/androidTest/java/com/tokopedia/home/util/HomeInstrumentationTestHelper.kt
+++ features/discovery/home/src/androidTest/java/com/tokopedia/home/util/HomeInstrumentationTestHelper.kt
@@ -1,9 +1,14 @@
 package com.tokopedia.home.util
 
+import android.content.Context
 import androidx.test.rule.ActivityTestRule
 
 object HomeInstrumentationTestHelper {
     fun ActivityTestRule<*>.deleteHomeDatabase() {
         this.activity.deleteDatabase("HomeCache.db")
+    }
+
+    fun Context.deleteHomeDatabase() {
+        this.deleteDatabase("HomeCache.db")
     }
 }
