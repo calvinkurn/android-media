5eb1bd12422fbd00a7ab58a15c09bc196cf38bc6
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-12-09 11:59:16 +0700

    add IT todo

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/UnsendImageTest.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+class UnsendImageTest: TopchatRoomTest() {
+
+    // TODO: Should show bottomsheet menu before deletion
+    // TODO: Should show dialog confirmation before deletion
+    // TODO: Should not show bottomsheet on long click opposite image bubble
+    // TODO: Should replace image bubble to deleted type bubble after success delete message
+    // TODO: Should render deleted type bubble when status equal to 5 (deleted) from GQL
+    // TODO: Should replace bubble to deleted type bubble when receive 104 event ws
+    // TODO: Should prevent long click when msg bubble is deleted
+    // TODO: Should show success toaster when success delete message
+    // TODO: Should show failed toaster when error delete message
+
+}
