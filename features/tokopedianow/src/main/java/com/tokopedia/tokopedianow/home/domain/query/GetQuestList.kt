93bbc7b69986331e59ec643dd043cfb3570710cc
saidfaisal <saidfaisal777@gmail.com>
2021-12-10 08:02:19 +0700

    add value to param channelSlug

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/domain/query/GetQuestList.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/domain/query/GetQuestList.kt
@@ -1,9 +1,11 @@
 package com.tokopedia.tokopedianow.home.domain.query
 
+import com.tokopedia.tokopedianow.home.domain.usecase.GetQuestWidgetListUseCase.Companion.CHANNEL_SLUG
+
 internal object GetQuestList {
     val QUERY = """
-        query quest_list(){
-          questList(input: {limit: 0, nextQuestID: 0, channel: 0, channelSlug: ""}) {
+        query quest_list(${'$'}${CHANNEL_SLUG}: String){
+          questList(input: {limit: 0, nextQuestID: 0, channel: 0, channelSlug: ${'$'}${CHANNEL_SLUG}}) {
             questList {
               id
               title
