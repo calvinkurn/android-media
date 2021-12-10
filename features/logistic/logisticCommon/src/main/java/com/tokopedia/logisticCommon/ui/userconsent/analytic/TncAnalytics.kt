e22e9a034832dfee3121ff83cf957d52a8c3737f
hiradevina <fakhira.devina@tokopedia.com>
2021-12-10 16:39:02 +0700

    create analytics object

---
+++ features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/ui/userconsent/analytic/TncAnalytics.kt
@@ -0,0 +1,34 @@
+package com.tokopedia.logisticCommon.ui.userconsent.analytic
+
+import com.tokopedia.track.builder.BaseTrackerBuilder
+import com.tokopedia.track.builder.util.BaseTrackerConst
+
+object TncAnalytics : BaseTrackerConst() {
+    private const val BUSINESS_UNIT_LOGISTIC = "logistics & fulfillment"
+    private const val CLICK_ADDRESS = "clickAddress"
+    private const val CLICK_TNC = "click syarat dan ketentuan"
+    private const val VIEW_ADDRESS = "viewAddressIris"
+    private const val VIEW_TNC = "view syarat dan ketentuan"
+
+    fun onClickTnC(category: String) {
+        getTracker().sendGeneralEvent(BaseTrackerBuilder()
+                .appendEvent(CLICK_ADDRESS)
+                .appendEventCategory(category)
+                .appendEventAction(CLICK_TNC)
+                .appendEventLabel("")
+                .appendBusinessUnit(BUSINESS_UNIT_LOGISTIC)
+                .appendCurrentSite(CurrentSite.DEFAULT)
+                .build())
+    }
+
+    fun onViewTnC(category: String) {
+        getTracker().sendGeneralEvent(BaseTrackerBuilder()
+                .appendEvent(VIEW_ADDRESS)
+                .appendEventCategory(category)
+                .appendEventAction(VIEW_TNC)
+                .appendEventLabel("")
+                .appendBusinessUnit(BUSINESS_UNIT_LOGISTIC)
+                .appendCurrentSite(CurrentSite.DEFAULT)
+                .build())
+    }
+}
