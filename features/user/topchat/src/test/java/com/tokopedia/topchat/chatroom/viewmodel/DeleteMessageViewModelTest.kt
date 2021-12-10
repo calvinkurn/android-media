1ea698869a29787f68c724fb170e1e17a5c348ad
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-12-10 13:02:48 +0700

    add UT should_update_live_data_value_if_failed

--- features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/DeleteMessageViewModelTest.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/DeleteMessageViewModelTest.kt
@@ -3,6 +3,7 @@ package com.tokopedia.topchat.chatroom.viewmodel
 import com.tokopedia.topchat.chatroom.domain.pojo.unsendreply.UnsendReply
 import com.tokopedia.topchat.chatroom.domain.pojo.unsendreply.UnsendReplyResponse
 import com.tokopedia.topchat.chatroom.viewmodel.base.BaseTopChatViewModelTest
+import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.coEvery
@@ -30,6 +31,23 @@ class DeleteMessageViewModelTest : BaseTopChatViewModelTest() {
         val data = (result as? Success)?.data
         assertThat(result, IsInstanceOf(Success::class.java))
         assertThat(data, `is`(replyTime))
+    }
+
+    @Test
+    fun should_update_live_data_value_if_failed() {
+        // Given
+        val successResponse = UnsendReplyResponse(UnsendReply(isSuccess = false))
+        val replyTime = "11"
+        coEvery {
+            unsendReplyUseCase.invoke(any())
+        } returns successResponse
+
+        // When
+        viewModel.deleteMsg("0", replyTime)
+
+        // Then
+        val result: Result<String>? = viewModel.deleteBubble.value
+        assertThat(result, IsInstanceOf(Fail::class.java))
     }
 
 }
