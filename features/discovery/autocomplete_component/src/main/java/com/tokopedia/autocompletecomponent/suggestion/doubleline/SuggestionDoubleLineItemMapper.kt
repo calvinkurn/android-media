64e3b0bafe4d8c216daca8dbfbc5a2af724e5a43
Darian Thedy <darian.thedy@tokopedia.com>
2021-12-14 15:43:01 +0700

    Shop Ads in Auto Complete

--- features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/doubleline/SuggestionDoubleLineItemMapper.kt
+++ features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/doubleline/SuggestionDoubleLineItemMapper.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.autocompletecomponent.suggestion.doubleline
 
 import com.tokopedia.autocompletecomponent.suggestion.convertToBaseSuggestion
+import com.tokopedia.autocompletecomponent.suggestion.convertToBaseSuggestionShopAds
 import com.tokopedia.autocompletecomponent.suggestion.domain.model.SuggestionItem
+import com.tokopedia.topads.sdk.domain.model.CpmData
 
 fun SuggestionItem.convertToDoubleLineVisitableList(
     searchTerm: String,
@@ -13,6 +15,36 @@ fun SuggestionItem.convertToDoubleLineVisitableList(
             searchTerm,
             position,
             dimension90,
+        )
+    )
+}
+
+fun SuggestionItem.convertToDoubleLineWithoutImageVisitableList(
+    searchTerm: String,
+    position: Int,
+    dimension90: String
+): SuggestionDoubleLineWithoutImageDataDataView {
+    return SuggestionDoubleLineWithoutImageDataDataView(
+        data = convertToBaseSuggestion(
+            searchTerm,
+            position,
+            dimension90,
+        )
+    )
+}
+
+fun SuggestionItem.convertToDoubleLineShopAds(
+    searchTerm: String,
+    position: Int,
+    dimension90: String,
+    cpmData: CpmData,
+): SuggestionDoubleLineDataDataView {
+    return SuggestionDoubleLineDataDataView(
+        data = convertToBaseSuggestionShopAds(
+            searchTerm,
+            position,
+            dimension90,
+            cpmData,
         )
     )
 }
