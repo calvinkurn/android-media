5afd4cdc77ff715513cc20586368e79ad6889345
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2021-12-14 10:27:38 +0700

    update response contract

--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/unsendreply/UnsendReply.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/unsendreply/UnsendReply.kt
@@ -4,6 +4,6 @@ package com.tokopedia.topchat.chatroom.domain.pojo.unsendreply
 import com.google.gson.annotations.SerializedName
 
 data class UnsendReply(
-    @SerializedName("isSuccess")
+    @SerializedName("success")
     var isSuccess: Boolean = false
 )
--- features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/unsendreply/UnsendReplyResponse.kt
+++ features/user/topchat/src/main/java/com/tokopedia/topchat/chatroom/domain/pojo/unsendreply/UnsendReplyResponse.kt
@@ -4,6 +4,6 @@ package com.tokopedia.topchat.chatroom.domain.pojo.unsendreply
 import com.google.gson.annotations.SerializedName
 
 data class UnsendReplyResponse(
-    @SerializedName("unsendReply")
+    @SerializedName("chatUnsendReply")
     var unsendReply: UnsendReply = UnsendReply()
 )
