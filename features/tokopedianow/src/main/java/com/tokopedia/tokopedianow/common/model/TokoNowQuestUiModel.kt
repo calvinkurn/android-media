b618dcd2740097fd1aa0c903722b1c659f35785d
saidfaisal <saidfaisal777@gmail.com>
2021-12-15 08:59:03 +0700

    remove unused ui model

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/model/TokoNowQuestUiModel.kt
+++
@@ -1,3 +0,0 @@
-package com.tokopedia.tokopedianow.common.model
-
-data class TokoNowQuestUiModel (val progress: Float, val title: String, val desc: String)
--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/model/TokoNowQuestWidetUiModel.kt
+++
@@ -1,3 +0,0 @@
-package com.tokopedia.tokopedianow.common.model
-
-data class TokoNowQuestWidetUiModel (val progress: Float, val title: String, val desc: String)
