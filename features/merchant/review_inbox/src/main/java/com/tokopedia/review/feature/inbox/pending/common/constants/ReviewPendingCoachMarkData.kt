4321d87db3298537be06de128db45c29c3aeeea6
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2021-12-16 15:33:11 +0700

    Remove carousel credibility CoachMark

--- features/merchant/review_inbox/src/main/java/com/tokopedia/review/feature/inbox/pending/common/constants/ReviewPendingCoachMarkData.kt
+++
@@ -1,5 +0,0 @@
-package com.tokopedia.review.feature.inbox.pending.common.constants
-
-object ReviewPendingCoachMarkData {
-    const val CREDIBILITY_CAROUSEL_COACH_MARK_KEY = "credibilityCarouselCoachMarkKey"
-}
