b41126de0f12df9b24d4922f736f56eb904a3b66
Dhaba Widhikari <wikodhawi@gmail.com>
2021-12-20 09:19:26 +0700

    Migrate Recharge BU Widget Query using gql annotations

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/GetRechargeBUWidgetUseCase.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/GetRechargeBUWidgetUseCase.kt
@@ -3,6 +3,7 @@ package com.tokopedia.home.beranda.domain.interactor
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.home.beranda.di.module.query.RechargeBuWidgetQuery
 import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.recharge_component.model.RechargePerso
 import com.tokopedia.recharge_component.model.WidgetSource
@@ -30,63 +31,9 @@ class GetRechargeBUWidgetUseCase @Inject constructor(
 
     private var params: RequestParams = RequestParams.create()
 
-    private val query by lazy {
-        val widgetSource = "\$widgetSource"
-
-        """
-            query getBUWidget($widgetSource: WidgetSource!) {
-                getBUWidget(widgetSource: $widgetSource) {
-                  title
-                  media_url
-                  app_link
-                  web_link
-                  banner_app_link
-                  banner_web_link
-                  text_link
-                  option_1
-                  option_2
-                  option_3
-                  tracking{
-                    action
-                    data
-                  }
-                  items {
-                    id
-                    title
-                    media_url
-                    media_url_type
-                    background_color
-                    subtitle
-                    subtitle_mode
-                    label_1
-                    label_1_mode
-                    label_2
-                    label_3
-                    app_link
-                    web_link
-                    tracking{
-                      action
-                      data
-                    }
-                    tracking_data{
-                      product_id
-                      operator_id
-                      business_unit
-                      item_type
-                      item_label
-                      client_number
-                      category_id
-                      category_name
-                    }
-                  }
-                }
-            }
-        """.trimIndent()
-    }
-
     override suspend fun executeOnBackground(): RechargePerso {
         graphqlUseCase.clearCache()
-        graphqlUseCase.setGraphqlQuery(query)
+        graphqlUseCase.setGraphqlQuery(RechargeBuWidgetQuery())
         graphqlUseCase.setRequestParams(params.parameters)
         val response = graphqlUseCase.executeOnBackground().response
 
