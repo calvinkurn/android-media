74337a9af34128c68627d3f4bda1865e4a4c5224
isfaaghyth <isfaaghyth@gmail.com>
2021-12-22 00:31:05 +0700

    setup the picker DI scope, module and component

--- features/media/mediapicker/src/main/java/com/tokopedia/picker/di/PickerComponent.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/picker/di/PickerComponent.kt
@@ -1,4 +1,16 @@
 package com.tokopedia.picker.di
 
+import com.tokopedia.picker.di.module.PickerModule
+import com.tokopedia.picker.di.module.PickerViewModelModule
+import com.tokopedia.picker.di.scope.PickerScope
+import com.tokopedia.picker.ui.fragment.gallery.GalleryFragment
+import dagger.Component
+
+@PickerScope
+@Component(modules = [
+    PickerModule::class,
+    PickerViewModelModule::class
+])
 interface PickerComponent {
+    fun inject(fragment: GalleryFragment)
 }
--- features/media/mediapicker/src/main/java/com/tokopedia/picker/di/module/PickerModule.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/picker/di/module/PickerModule.kt
@@ -1,4 +1,21 @@
 package com.tokopedia.picker.di.module
 
-class PickerModule {
+import android.content.Context
+import com.tokopedia.picker.data.repository.FileLoaderRepository
+import com.tokopedia.picker.data.repository.FileLoaderRepositoryImpl
+import com.tokopedia.picker.di.scope.PickerScope
+import dagger.Module
+import dagger.Provides
+
+@Module
+class PickerModule constructor(
+    private val context: Context
+) {
+
+    @Provides
+    @PickerScope
+    fun provideFilesLoaderRepository(): FileLoaderRepository {
+        return FileLoaderRepositoryImpl(context)
+    }
+
 }
