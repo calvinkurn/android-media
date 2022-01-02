55d98cc78258a691321004ec67006f36c16da0a3
Dhaba Widhikari <wikodhawi@gmail.com>
2022-01-02 20:57:00 +0700

    add repository and use case for business unit

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/usecase/HomeBusinessUnitUseCase.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/usecase/HomeBusinessUnitUseCase.kt
@@ -1,5 +1,35 @@
 package com.tokopedia.home.beranda.domain.interactor.usecase
 
-interface HomeBusinessUnitUseCase {
-    fun onClickBusinessUnitTab()
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.home.beranda.data.repository.HomeRevampRepository
+import com.tokopedia.home.beranda.domain.interactor.repository.HomeBusinessUnitRepository
+import com.tokopedia.home.beranda.helper.Result
+import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.HomeDynamicChannelModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class HomeBusinessUnitUseCase @Inject constructor(
+    private val homeBusinessUnitRepository: HomeBusinessUnitRepository
+) : HomeRevampRepository {
+
+    fun getBuUnitTab(position: Int) {
+
+    }
+
+    override fun getHomeDataFlow(): Flow<HomeDynamicChannelModel?> {
+        return flowOf()
+    }
+
+    override fun updateHomeData(): Flow<Result<Any>> {
+        return flowOf()
+    }
+
+    override suspend fun onDynamicChannelExpired(groupId: String): List<Visitable<*>> {
+        return listOf()
+    }
+
+    override fun deleteHomeData() {
+
+    }
 }
