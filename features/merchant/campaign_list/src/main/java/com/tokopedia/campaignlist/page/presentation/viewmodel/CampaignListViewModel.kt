090e4b93d91f4012b879fc50df2e298a811534e0
Fajar Chaeril Azhar <fajar.azhar@tokopedia.com>
2022-01-03 12:23:51 +0700

    Fix incorrect wording

--- features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/viewmodel/CampaignListViewModel.kt
+++ features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/viewmodel/CampaignListViewModel.kt
@@ -202,16 +202,16 @@ class CampaignListViewModel @Inject constructor(
     ): String {
         return when (campaignStatusId ?: "") {
             ONGOING_STATUS_ID -> {
-                val template = resourceProvider.getShareCampaignDescriptionWording() ?: ""
-                val startDate = merchantBannerData.formattedStartDate.split(", ").first()
-                val startTime = merchantBannerData.formattedStartDate.split(", ").last()
-                String.format(template, campaignData.name, shopData.name, startDate, startTime).plus(" $shareUri")
-            }
-            else -> {
                 val template = resourceProvider.getShareOngoingCampaignDescriptionWording() ?: ""
                 val endDate = merchantBannerData.formattedEndDate.split(", ").first()
                 val endTime = merchantBannerData.formattedEndDate.split(", ").last()
                 String.format(template, campaignData.name, shopData.name, endDate, endTime).plus(" $shareUri")
+            }
+            else -> {
+                val template = resourceProvider.getShareCampaignDescriptionWording() ?: ""
+                val startDate = merchantBannerData.formattedStartDate.split(", ").first()
+                val startTime = merchantBannerData.formattedStartDate.split(", ").last()
+                String.format(template, campaignData.name, shopData.name, startDate, startTime).plus(" $shareUri")
             }
         }
     }
