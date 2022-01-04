ec2afa6ddaeb5fdde5c377c2defb7e4b5e6e8349
Rohan <rohan.ghosh@tokopedia.com>
2022-01-04 18:43:15 +0530

    added the adapter and demo ui

---
+++ features/fintech/pdp_fintech_widget/src/main/java/com/tokopedia/pdp/fintech/adapter/FintechWidgetAdapter.kt
@@ -0,0 +1,24 @@
+package com.tokopedia.pdp.fintech.adapter
+
+import com.tokopedia.pdp.fintech.adapter.FintechWidgetAdapter.MyViewHolder
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.pdp_fintech.R
+
+class FintechWidgetAdapter : RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.fintech_invidual_chip, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {}
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
---
+++ features/fintech/pdp_fintech_widget/src/main/java/com/tokopedia/pdp/fintech/view/PdpFintechWidget.kt
@@ -0,0 +1,38 @@
+package com.tokopedia.pdp.fintech.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.pdp.fintech.adapter.FintechWidgetAdapter
+import com.tokopedia.pdp_fintech.R
+import com.tokopedia.unifycomponents.BaseCustomView
+
+class PdpFintechWidget @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : BaseCustomView(context, attrs, defStyleAttr) {
+
+    private lateinit var baseView: View
+
+
+    init {
+        initView()
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        val recyclerView = baseView.findViewById<RecyclerView>(R.id.recycler_items)
+        recyclerView.layoutManager = LinearLayoutManager(context,RecyclerView.HORIZONTAL,false)
+        recyclerView.adapter = FintechWidgetAdapter()
+    }
+
+    private fun initView() {
+       baseView =  inflate(context, R.layout.pdp_fintech_widget_layout,this)
+    }
+
+
+}
