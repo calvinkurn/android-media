638e6d0a2901f2518c27c756f5ba5f74c22b1242
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2022-01-04 15:33:28 +0700

    implement AccelerometerOrientationListener

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/cashback/presentation/activity/ProductManageSetCashbackActivity.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/cashback/presentation/activity/ProductManageSetCashbackActivity.kt
@@ -2,9 +2,12 @@ package com.tokopedia.product.manage.feature.cashback.presentation.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.device.info.DeviceScreenInfo
 import com.tokopedia.kotlin.extensions.view.toBlankOrString
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.product.manage.feature.cashback.presentation.fragment.ProductManageSetCashbackFragment
@@ -12,6 +15,7 @@ import com.tokopedia.product.manage.feature.cashback.presentation.fragment.Produ
 import com.tokopedia.product.manage.feature.cashback.presentation.fragment.ProductManageSetCashbackFragment.Companion.EXTRA_CASHBACK_SHOP_ID
 import com.tokopedia.product.manage.feature.cashback.presentation.fragment.ProductManageSetCashbackFragment.Companion.PARAM_SET_CASHBACK_PRODUCT_PRICE
 import com.tokopedia.product.manage.feature.cashback.presentation.fragment.ProductManageSetCashbackFragment.Companion.PARAM_SET_CASHBACK_VALUE
+import com.tokopedia.utils.accelerometer.orientation.AccelerometerOrientationListener
 
 class ProductManageSetCashbackActivity: BaseSimpleActivity() {
 
@@ -30,6 +34,12 @@ class ProductManageSetCashbackActivity: BaseSimpleActivity() {
             bundle.putString(PARAM_PRICE, price)
             intent.putExtras(bundle)
             return intent
+        }
+    }
+
+    private val accelerometerOrientationListener: AccelerometerOrientationListener by lazy {
+        AccelerometerOrientationListener(contentResolver) {
+            onAccelerometerOrientationSettingChange(it)
         }
     }
 
@@ -63,6 +73,7 @@ class ProductManageSetCashbackActivity: BaseSimpleActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setActivityOrientation()
         supportActionBar?.hide()
     }
 
@@ -71,6 +82,45 @@ class ProductManageSetCashbackActivity: BaseSimpleActivity() {
         val f = fragment
         if(f != null && f is ProductManageSetCashbackFragment) {
             f.setTrackerOnBackPressed()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (DeviceScreenInfo.isTablet(this)) {
+            accelerometerOrientationListener.register()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (DeviceScreenInfo.isTablet(this)) {
+            accelerometerOrientationListener.unregister()
+        }
+    }
+
+    private fun onAccelerometerOrientationSettingChange(isEnabled: Boolean) {
+        if (DeviceScreenInfo.isTablet(this)) {
+            requestedOrientation = if (isEnabled)
+                ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+            else
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        }
+    }
+
+    private fun setActivityOrientation() {
+        if (DeviceScreenInfo.isTablet(this)) {
+            val isAccelerometerRotationEnabled = Settings.System.getInt(
+                contentResolver,
+                Settings.System.ACCELEROMETER_ROTATION,
+                0
+            ) == 1
+
+            requestedOrientation = if (isAccelerometerRotationEnabled)
+                ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+            else
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         }
     }
 
