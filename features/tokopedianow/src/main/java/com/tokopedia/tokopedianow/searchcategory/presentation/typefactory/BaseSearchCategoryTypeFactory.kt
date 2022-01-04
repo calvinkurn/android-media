21e036cbc09098df3474c784229207700031a522
saidfaisal <saidfaisal777@gmail.com>
2022-01-04 09:33:05 +0700

    fix missing type

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/searchcategory/presentation/typefactory/BaseSearchCategoryTypeFactory.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/searchcategory/presentation/typefactory/BaseSearchCategoryTypeFactory.kt
@@ -1,14 +1,7 @@
 package com.tokopedia.tokopedianow.searchcategory.presentation.typefactory
 
 import com.tokopedia.abstraction.base.view.adapter.factory.AdapterTypeFactory
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.BannerDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.CategoryFilterDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.ChooseAddressDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.ProductCountDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.ProductItemDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.QuickFilterDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.TitleDataView
-import com.tokopedia.tokopedianow.searchcategory.presentation.model.ProgressBarDataView
+import com.tokopedia.tokopedianow.searchcategory.presentation.model.*
 
 interface BaseSearchCategoryTypeFactory: AdapterTypeFactory {
 
@@ -20,4 +13,5 @@ interface BaseSearchCategoryTypeFactory: AdapterTypeFactory {
     fun type(productCountDataView: ProductCountDataView): Int
     fun type(productItemDataView: ProductItemDataView): Int
     fun type(progressBarDataView: ProgressBarDataView): Int
+    fun type(switcherWidgetDataView: SwitcherWidgetDataView): Int
 }
