1cea4ded4faa910f8e7352b9a697b5250890aec7
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-01-05 09:44:37 +0700

    init RechargeClientNumberWidget class

---
+++ features/recharge/recharge_component/src/main/java/com/tokopedia/recharge_component/widget/RechargeClientNumberWidget.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.recharge_component.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import org.jetbrains.annotations.NotNull
+
+class RechargeClientNumberWidget @JvmOverloads constructor(@NotNull context: Context, attrs: AttributeSet? = null,
+                                                           defStyleAttr: Int = 0)
+    : FrameLayout(context, attrs, defStyleAttr) {
+
+
+}
