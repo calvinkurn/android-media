77841c9bc7e77e9932773b06b4fde4593c45b11b
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2022-01-06 14:16:33 +0700

    refactor test class

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/ComposeMsgTest.kt
@@ -0,0 +1,54 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import android.app.Activity
+import android.app.Instrumentation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.tokopedia.test.application.annotations.UiTest
+import com.tokopedia.topchat.R
+import com.tokopedia.topchat.chatroom.view.activity.TopchatRoomInvoiceAttachmentTest.Companion.getInvoiceAttachmentIntent
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+import org.junit.Test
+
+@UiTest
+class ComposeMsgTest : TopchatRoomTest() {
+
+    @Test
+    fun user_can_not_sent_preview_invoice_when_text_is_empty() {
+        // Given
+        getChatUseCase.response = firstPageChatAsBuyer
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        launchChatRoomActivity()
+
+        // When
+        Intents.intending(anyIntent()).respondWith(
+            Instrumentation.ActivityResult(
+                Activity.RESULT_OK, getInvoiceAttachmentIntent(true)
+            )
+        )
+        clickPlusIconMenu()
+        clickAttachInvoiceMenu()
+        clickSendBtn()
+
+        // Then
+        assertSnackbarText(context.getString(R.string.topchat_desc_empty_text_box))
+    }
+
+    @Test
+    fun test_msg_sent_error_empty_text_click() {
+        //Given
+        getChatUseCase.response = firstPageChatAsSeller
+        chatAttachmentUseCase.response = chatAttachmentResponse
+        launchChatRoomActivity()
+
+        //When
+        onView(withId(R.id.send_but)).perform(ViewActions.click())
+
+        //Then
+        assertSnackbarText(context.getString(R.string.topchat_desc_empty_text_box))
+    }
+
+}
--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomInvoiceAttachmentTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomInvoiceAttachmentTest.kt
@@ -21,9 +21,6 @@ import org.junit.Test
 @UiTest
 class TopchatRoomInvoiceAttachmentTest : BaseBuyerTopchatRoomTest() {
 
-    private val goodId = "770851031"
-    private val badId = "INV_123"
-
     private val expectedIntent = Matchers.allOf(
         IntentMatchers.hasExtra(ApplinkConst.AttachInvoice.PARAM_OPPONENT_NAME, "Seller Testing"),
         IntentMatchers.hasExtra(ApplinkConst.AttachInvoice.PARAM_MESSAGE_ID, "66961")
@@ -73,27 +70,6 @@ class TopchatRoomInvoiceAttachmentTest : BaseBuyerTopchatRoomTest() {
 
         // Then
         onView(withId(R.id.rv_attachment_preview)).check(matches(not(isDisplayed())))
-    }
-
-    @Test
-    fun user_can_not_sent_preview_invoice_when_text_is_empty() {
-        // Given
-        getChatUseCase.response = firstPageChatAsBuyer
-        chatAttachmentUseCase.response = chatAttachmentResponse
-        launchChatRoomActivity()
-
-        // When
-        Intents.intending(expectedIntent).respondWith(
-            Instrumentation.ActivityResult(
-                Activity.RESULT_OK, getInvoiceAttachmentIntent(true)
-            )
-        )
-        clickPlusIconMenu()
-        clickAttachInvoiceMenu()
-        clickSendBtn()
-
-        // Then
-        assertSnackbarText(context.getString(R.string.topchat_desc_empty_text_box))
     }
 
     @Test
@@ -158,22 +134,26 @@ class TopchatRoomInvoiceAttachmentTest : BaseBuyerTopchatRoomTest() {
         assertSnackbarText(context.getString(R.string.topchat_desc_empty_text_box))
     }
 
-    private fun getInvoiceAttachmentIntent(isValidInvoice: Boolean): Intent {
-        val intent = Intent()
-        val id: String = if(isValidInvoice) goodId else badId
-        intent.putExtra(ApplinkConst.Chat.INVOICE_ID, id)
-        intent.putExtra(ApplinkConst.Chat.INVOICE_CODE, "INV/20210422/MPL/1190494783")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_TITLE,
-            "sendal jepit 999^^\u0026%#^%#$%$%*\u0026^%")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_DATE, "22 Apr 2021")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_IMAGE_URL,
-            "https://ecs7.tokopedia.net/img/cache/100-square/VqbcmM/2021/2/16/61e5d2f7-f411-4b14-94fe-c5c401636952.jpg")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_URL,
-            "https://www.tokopedia.com/invoice.pl?id\u003d770851031\u0026pdf\u003dInvoice-136513670-10825582-20210422132008-eHh4eHh4eHg3")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_STATUS_ID, "700")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_STATUS, "Pesanan Selesai")
-        intent.putExtra(ApplinkConst.Chat.INVOICE_TOTAL_AMOUNT, "Rp 200")
+    companion object {
+        private val goodId = "770851031"
+        private val badId = "INV_123"
+        fun getInvoiceAttachmentIntent(isValidInvoice: Boolean): Intent {
+            val intent = Intent()
+            val id: String = if(isValidInvoice) goodId else badId
+            intent.putExtra(ApplinkConst.Chat.INVOICE_ID, id)
+            intent.putExtra(ApplinkConst.Chat.INVOICE_CODE, "INV/20210422/MPL/1190494783")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_TITLE,
+                "sendal jepit 999^^\u0026%#^%#$%$%*\u0026^%")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_DATE, "22 Apr 2021")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_IMAGE_URL,
+                "https://ecs7.tokopedia.net/img/cache/100-square/VqbcmM/2021/2/16/61e5d2f7-f411-4b14-94fe-c5c401636952.jpg")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_URL,
+                "https://www.tokopedia.com/invoice.pl?id\u003d770851031\u0026pdf\u003dInvoice-136513670-10825582-20210422132008-eHh4eHh4eHg3")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_STATUS_ID, "700")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_STATUS, "Pesanan Selesai")
+            intent.putExtra(ApplinkConst.Chat.INVOICE_TOTAL_AMOUNT, "Rp 200")
 
-        return intent
+            return intent
+        }
     }
 }
