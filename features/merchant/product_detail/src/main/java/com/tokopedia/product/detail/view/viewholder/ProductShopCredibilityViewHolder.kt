29d4fe30b348d0ccf77cf04bd8676a545b4fe79d
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2022-01-07 18:22:46 +0700

    update UI side.

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductShopCredibilityViewHolder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/ProductShopCredibilityViewHolder.kt
@@ -8,6 +8,7 @@ import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.showIfWithBlock
 import com.tokopedia.media.loader.loadImage
 import com.tokopedia.media.loader.loadImageCircle
 import com.tokopedia.product.detail.R
@@ -18,6 +19,7 @@ import com.tokopedia.product.detail.databinding.ItemShopCredibilityBinding
 import com.tokopedia.product.detail.databinding.ViewShopCredibilityBinding
 import com.tokopedia.product.detail.databinding.ViewShopCredibilityShimmeringBinding
 import com.tokopedia.product.detail.view.listener.DynamicProductDetailListener
+import com.tokopedia.shop.common.graphql.data.shopinfo.ShopInfo
 import com.tokopedia.unifycomponents.HtmlLinkHelper
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
@@ -87,9 +89,22 @@ class ProductShopCredibilityViewHolder(
                 listener.gotoShopDetail(componentTracker)
             }
 
+            setupTicker(element.tickerDataResponse, this)
+
             view.addOnImpressionListener(element.impressHolder) {
                 listener.onImpressComponent(componentTracker)
             }
+        }
+    }
+
+    private fun setupTicker(
+        tickerDataResponse: List<ShopInfo.Ticker.TickerDataResponse>,
+        binding: ViewShopCredibilityBinding
+    ) {
+        binding.shopCredibilityInfoTicker.showIfWithBlock(tickerDataResponse.isNotEmpty()) {
+            val data = tickerDataResponse.first()
+            setHtmlDescription(generateHtml(data.message, data.link))
+            tickerTitle = data.title
         }
     }
 
@@ -259,6 +274,10 @@ class ProductShopCredibilityViewHolder(
 
     private fun enableButton() = mainBinding?.apply {
         shopCredibilityButtonFollow.isClickable = true
+    }
+
+    private fun generateHtml(message: String, link: String): String = with(itemView) {
+        return message.replace("{link}", context.getString(R.string.ticker_href_builder, link))
     }
 
     private fun getComponentTrackData(
