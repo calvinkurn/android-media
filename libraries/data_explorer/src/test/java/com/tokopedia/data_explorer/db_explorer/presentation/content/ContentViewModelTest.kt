5e54cf440a05d6f0e12ad1f77f9aef931aa6d055
chauhan-abhi-toko <abhijeet.chauhan@tokopedia.com>
2022-01-10 17:51:45 +0530

    [Data Explorer] Adding unit test cases

---
+++ libraries/data_explorer/src/test/java/com/tokopedia/data_explorer/db_explorer/presentation/content/ContentViewModelTest.kt
@@ -0,0 +1,33 @@
+package com.tokopedia.data_explorer.db_explorer.presentation.content
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class ContentViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getTableInfo() {
+    }
+
+    @Test
+    fun getTableRowsCount() {
+    }
+
+    @Test
+    fun getTableContent() {
+    }
+
+    @Test
+    fun dropTable() {
+    }
+
+    @Test
+    fun updateHeader() {
+    }
+}
---
+++ libraries/data_explorer/src/test/java/com/tokopedia/data_explorer/db_explorer/presentation/schema/SchemaViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.data_explorer.db_explorer.presentation.schema
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class SchemaViewModelTest {
+
+    @Test
+    fun getTables() {
+    }
+}
