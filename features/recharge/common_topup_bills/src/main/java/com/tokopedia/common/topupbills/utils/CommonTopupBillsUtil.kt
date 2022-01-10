dbe5ebedd4ce8f4c6dfaa2a9e9be35e760dd0fa2
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-01-10 13:22:14 +0700

    extract some string to const

--- features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/utils/CommonTopupBillsUtil.kt
+++ features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/utils/CommonTopupBillsUtil.kt
@@ -9,6 +9,11 @@ class CommonTopupBillsUtil {
     companion object {
         const val REMOTE_CONFIG_MAINAPP_DIGITAL_FAVORITE_NUMBER = "android_customer_enable_digital_favorite_number"
 
+        const val NUMBER_CODE = "62"
+        const val FORMATTED_NUMBER_CODE = "+62"
+        const val NORMALIZED_NUMBER_PREFIX = "0"
+        const val NUMBER_REGEX = "[^0-9]+"
+
         fun isSeamlessFavoriteNumber(context: Context): Boolean {
             val remoteConfig = FirebaseRemoteConfigImpl(context)
             return remoteConfig.getBoolean(REMOTE_CONFIG_MAINAPP_DIGITAL_FAVORITE_NUMBER, true)
@@ -35,16 +40,15 @@ class CommonTopupBillsUtil {
 
         private fun validatePrefixClientNumber(phoneNumber: String): String {
             var phoneNumber = phoneNumber
-            if (phoneNumber.startsWith("62")) {
-                phoneNumber = phoneNumber.replaceFirst("62".toRegex(), "0")
+            if (phoneNumber.startsWith(NUMBER_CODE)) {
+                phoneNumber = phoneNumber.replaceFirst(NUMBER_CODE.toRegex(), NORMALIZED_NUMBER_PREFIX)
             }
-            if (phoneNumber.startsWith("+62")) {
-                phoneNumber = phoneNumber.replace("+62", "0")
+            if (phoneNumber.startsWith(FORMATTED_NUMBER_CODE)) {
+                phoneNumber = phoneNumber.replace(FORMATTED_NUMBER_CODE, NORMALIZED_NUMBER_PREFIX)
             }
             phoneNumber = phoneNumber.replace(".", "")
 
-            return phoneNumber.replace("[^0-9]+".toRegex(), "")
+            return phoneNumber.replace(NUMBER_REGEX.toRegex(), "")
         }
-
     }
 }
