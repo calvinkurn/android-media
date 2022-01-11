79c6aaebca36dabfa48de9c95d0a45748e5b3233
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-01-11 11:33:48 +0700

    fix after resolve conflicts

--- features/transaction/cart/src/main/java/com/tokopedia/cart/view/viewholder/CartSelectAllViewHolder.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/view/viewholder/CartSelectAllViewHolder.kt
@@ -4,13 +4,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.cart.R
 import com.tokopedia.cart.view.ActionListener
 import com.tokopedia.cart.view.uimodel.CartSelectAllHolderData
-import com.tokopedia.cart.databinding.ItemSelectAllBundleBinding
+import com.tokopedia.cart.databinding.ItemSelectAllBinding
 import rx.subscriptions.CompositeSubscription
 
-class CartSelectAllViewHolder(private val binding: ItemSelectAllBundleBinding, val listener: ActionListener?, val compositeSubscription: CompositeSubscription) : RecyclerView.ViewHolder(binding.root) {
+class CartSelectAllViewHolder(private val binding: ItemSelectAllBinding, val listener: ActionListener?, val compositeSubscription: CompositeSubscription) : RecyclerView.ViewHolder(binding.root) {
 
     companion object {
-        val LAYOUT = R.layout.item_select_all_bundle
+        val LAYOUT = R.layout.item_select_all
     }
 
     fun bind(data: CartSelectAllHolderData) {
