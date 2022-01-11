7bfba1c4d54e4d17dca5eae7d2a732c94f325e27
reivinoktavianus <oktavianusreivin@gmail.com>
2022-01-11 17:39:06 +0700

    Add menu item for social media links and add 'new' TAG. Also, remove feedback tag checking

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/bottomsheet/SocialMediaLinksBottomSheet.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/bottomsheet/SocialMediaLinksBottomSheet.kt
@@ -1,6 +1,14 @@
 package com.tokopedia.sellerhome.settings.view.bottomsheet
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tokopedia.sellerhome.R
 import com.tokopedia.sellerhome.databinding.BottomSheetSahSocialMediaBinding
+import com.tokopedia.sellerhome.settings.view.adapter.socialmedialinks.SocialMediaLinksAdapter
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 
@@ -11,10 +19,39 @@ class SocialMediaLinksBottomSheet: BottomSheetUnify() {
         fun createInstance(): SocialMediaLinksBottomSheet {
             return SocialMediaLinksBottomSheet()
         }
+
+        private const val TAG = "social_media_links"
     }
 
     private var binding by autoClearedNullable<BottomSheetSahSocialMediaBinding>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = BottomSheetSahSocialMediaBinding.inflate(inflater, container, false)
+        setChild(binding?.root)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    fun show(fm: FragmentManager?) {
+        fm?.let { show(it, TAG) }
+    }
+
+    private fun setupView() {
+        context?.let {
+            setTitle(it.getString(R.string.sah_social_title))
+            binding?.rvSahSocial?.run {
+                layoutManager = LinearLayoutManager(it)
+                adapter = SocialMediaLinksAdapter(it)
+            }
+        }
+    }
 
 }
