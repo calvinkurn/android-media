589aa3dc58da3461c789288a0cc5d9d0e0f51578
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2022-01-12 14:44:58 +0700

    fix IT srw_bubble_should_removed_when_user_receive_invoice_event_from_ws

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSrwBuyerTest.kt
@@ -472,9 +472,9 @@ class TopchatRoomSrwBuyerTest : BaseBuyerTopchatRoomTest() {
 
         // When
         clickSrwPreviewItemAt(0)
-        websocket.simulateResponseFromRequestQueue(getChatUseCase.response)
-        websocket.simulateResponse(wsSellerInvoiceResponse.changeTimeStampTo(today()))
-        websocket.simulateResponse(wsSellerResponseText.changeTimeStampTo(today()))
+        websocket2.simulateResponseFromRequestQueue(getChatUseCase.response)
+        websocket2.simulateResponse(wsSellerInvoiceResponse.changeTimeStampTo(today()))
+        websocket2.simulateResponse(wsSellerResponseText.changeTimeStampTo(today()))
 
         // Then
         assertSrwBubbleDoesNotExist()
