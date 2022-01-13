5d1d830d5af1812d6660f3733bd24aa5b4c2fa65
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2022-01-13 13:31:24 +0700

    replace value with available constant.

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/tracking/ShopCredibilityTracking.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/tracking/ShopCredibilityTracking.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.product.detail.tracking
 
+import com.tokopedia.product.detail.common.ProductTrackingConstant
 import com.tokopedia.track.TrackApp
 import com.tokopedia.trackingoptimizer.TrackingQueue
 
@@ -7,8 +8,6 @@ object ShopCredibilityTracking {
 
     private const val ACTION_IMPRESSION_SHOP_TICKER = "impression - shop component ticker"
     private const val ACTION_CLICK_SHOP_TICKER = "click - button text on shop component ticker"
-    private const val VALUE_PRODUCT_DETAIL_PAGE = "product detail page"
-    private const val VALUE_CURRENT_SITE = "tokopediamarketplace"
 
     fun impressShopTicker(
         data: ShopCredibilityTracker.ImpressionShopTicker,
@@ -18,13 +17,13 @@ object ShopCredibilityTracking {
         val shopId = data.shopId
 
         val mapEvent = hashMapOf(
-            "event" to "promoView",
-            "eventAction" to "impression - shop component ticker",
-            "eventCategory" to VALUE_PRODUCT_DETAIL_PAGE,
+            "event" to ProductTrackingConstant.Tracking.PROMO_VIEW,
+            "eventAction" to ACTION_IMPRESSION_SHOP_TICKER,
+            "eventCategory" to ProductTrackingConstant.Category.PDP,
             "eventLabel" to "shop_id:$shopId;",
-            "businessUnit" to VALUE_PRODUCT_DETAIL_PAGE,
+            "businessUnit" to ProductTrackingConstant.Tracking.BUSINESS_UNIT_PDP,
             "component" to "comp:${data.componentName};temp:${data.componentType};elem:$ACTION_IMPRESSION_SHOP_TICKER;cpos:${data.componentPosition};",
-            "currentSite" to VALUE_CURRENT_SITE,
+            "currentSite" to ProductTrackingConstant.Tracking.CURRENT_SITE,
             "layout" to "layout:${data.layoutName};catName:${data.categoryName};catId:${data.categoryId};",
             "productId" to data.productId,
             "ecommerce" to mapOf(
@@ -54,11 +53,11 @@ object ShopCredibilityTracking {
         val mapEvent = hashMapOf<String, Any>(
             "event" to "clickPG",
             "eventAction" to ACTION_CLICK_SHOP_TICKER,
-            "eventCategory" to VALUE_PRODUCT_DETAIL_PAGE,
+            "eventCategory" to ProductTrackingConstant.Category.PDP,
             "eventLabel" to "shop_id:$shopId;title:${data.title};ticker_type:${data.tickerType};message:${data.message};button_text:${data.buttonText};",
-            "businessUnit" to VALUE_PRODUCT_DETAIL_PAGE,
+            "businessUnit" to ProductTrackingConstant.Tracking.BUSINESS_UNIT_PDP,
             "component" to "comp:${data.componentName};temp:${data.componentType};elem:$ACTION_CLICK_SHOP_TICKER;cpos:${data.componentPosition};",
-            "currentSite" to VALUE_CURRENT_SITE,
+            "currentSite" to ProductTrackingConstant.Tracking.CURRENT_SITE,
             "layout" to "layout:${data.layoutName};catName:${data.categoryName};catId:${data.categoryId};",
             "productId" to data.productId,
             "shopId" to "$shopId;",
