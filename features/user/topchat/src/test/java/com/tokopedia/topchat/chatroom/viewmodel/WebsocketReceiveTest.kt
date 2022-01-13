086d4bbf1ac72d84af2b6976264f2a998d56d051
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2022-01-13 13:01:24 +0700

    add UT should_not_retry_connect_websocket_when_ws_closed_normally

--- features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/WebsocketReceiveTest.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/WebsocketReceiveTest.kt
@@ -2,6 +2,7 @@ package com.tokopedia.topchat.chatroom.viewmodel
 
 import com.tokopedia.topchat.chatroom.viewmodel.base.BaseTopChatViewModelTest
 import com.tokopedia.topchat.common.websocket.DefaultTopChatWebSocket
+import io.mockk.coVerify
 import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -47,5 +48,40 @@ class WebsocketReceiveTest : BaseTopChatViewModelTest() {
 
         // When
         viewModel.connectWebSocket()
+    }
+
+    @Test
+    fun should_retry_connect_websocket_when_ws_closed_abnormally() {
+        // Given
+        onConnectWebsocket {
+            it.onClosed(websocket, 500, "")
+        }
+
+        // When
+        viewModel.connectWebSocket()
+
+        // Then
+        assertEquals(viewModel.isWebsocketError.value, true)
+        coVerify {
+            chatWebSocket.close()
+            webSocketStateHandler.scheduleForRetry(any())
+        }
+    }
+
+    @Test
+    fun should_not_retry_connect_websocket_when_ws_closed_normally() {
+        // Given
+        onConnectWebsocket {
+            it.onClosed(websocket, DefaultTopChatWebSocket.CODE_NORMAL_CLOSURE, "")
+        }
+
+        // When
+        viewModel.connectWebSocket()
+
+        // Then
+        coVerify(exactly = 0) {
+            chatWebSocket.close()
+            webSocketStateHandler.scheduleForRetry(any())
+        }
     }
 }
