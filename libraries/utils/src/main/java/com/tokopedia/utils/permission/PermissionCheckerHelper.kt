de70e2e583c2a3c19a3fb2ac60b711b0d37e8330
hiradevina <fakhira.devina@tokopedia.com>
2022-01-14 08:08:42 +0700

    remove unused import

--- libraries/utils/src/main/java/com/tokopedia/utils/permission/PermissionCheckerHelper.kt
+++ libraries/utils/src/main/java/com/tokopedia/utils/permission/PermissionCheckerHelper.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
