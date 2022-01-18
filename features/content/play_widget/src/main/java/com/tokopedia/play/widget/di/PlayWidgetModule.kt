2937f4257ffe4dea9f5fe7661b40f033e077881f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-01-18 12:07:30 +0700

    get autoplay from preference in config mapper play widget

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/di/PlayWidgetModule.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/di/PlayWidgetModule.kt
@@ -1,5 +1,9 @@
 package com.tokopedia.play.widget.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.play.widget.domain.PlayWidgetUpdateChannelUseCase
 import dagger.Module
@@ -14,5 +18,11 @@ class PlayWidgetModule {
     @Provides
     fun providePlayWidgetUpdateChannelUseCase(graphqlRepository: GraphqlRepository): PlayWidgetUpdateChannelUseCase {
         return PlayWidgetUpdateChannelUseCase(graphqlRepository)
+    }
+
+    @Provides
+    fun providePlayWidgetConfigMapper(@ApplicationContext context: Context): SharedPreferences {
+        /** TODO("dont use deprecated way") */
+        return PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
     }
 }
--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/mapper/PlayWidgetConfigMapper.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/mapper/PlayWidgetConfigMapper.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.play.widget.ui.mapper
 
+import android.content.SharedPreferences
+import android.util.Log
 import com.tokopedia.play.widget.data.PlayWidget
 import com.tokopedia.play.widget.ui.model.PlayWidgetConfigUiModel
 import javax.inject.Inject
@@ -7,15 +9,23 @@ import javax.inject.Inject
 /**
  * Created by jegul on 07/10/20
  */
-class PlayWidgetConfigMapper @Inject constructor() {
+class PlayWidgetConfigMapper @Inject constructor(
+    private val pref: SharedPreferences,
+) {
 
-    fun mapWidgetConfig(data: PlayWidget): PlayWidgetConfigUiModel = PlayWidgetConfigUiModel(
-            autoPlay = data.meta.autoplay,
+    fun mapWidgetConfig(data: PlayWidget): PlayWidgetConfigUiModel {
+        /** TODO("key and default value make it cleaner") */
+        val autoPlayPref = pref.getBoolean("play_widget_autoplay", true)
+        Log.d("<LOG>", "autoPlayPref: $autoPlayPref")
+
+        return PlayWidgetConfigUiModel(
+            autoPlay = if(autoPlayPref) data.meta.autoplay else false,
             autoPlayAmount = data.meta.autoplayAmount,
             autoRefresh = data.meta.autoRefresh,
             autoRefreshTimer = data.meta.autoRefreshTimer,
             maxAutoPlayCellularDuration = data.meta.maxAutoplayCell,
             maxAutoPlayWifiDuration = data.meta.maxAutoplayWifi,
             businessWidgetPosition = data.meta.businessWidgetPosition
-    )
+        )
+    }
 }
