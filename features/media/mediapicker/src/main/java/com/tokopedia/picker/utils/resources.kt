bdf502d5ee10477e2fe82942ba5c572297228cf6
isfaaghyth <isfaaghyth@gmail.com>
2022-01-18 23:58:29 +0700

    added unify_n700_96 on resources utils

--- features/media/mediapicker/src/main/java/com/tokopedia/picker/utils/resources.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/picker/utils/resources.kt
@@ -3,7 +3,7 @@ package com.tokopedia.picker.utils
 val Unify_N0 = com.tokopedia.unifyprinciples.R.color.Unify_N0
 val Unify_G500 = com.tokopedia.unifyprinciples.R.color.Unify_G500
 val Unify_N600 = com.tokopedia.unifyprinciples.R.color.Unify_N600
-
+val Unify_N700_96 = com.tokopedia.unifyprinciples.R.color.Unify_N700_96
 val Unify_Static_White = com.tokopedia.unifyprinciples.R.color.Unify_Static_White
 
 val DP_72 = com.tokopedia.abstraction.R.dimen.dp_72
