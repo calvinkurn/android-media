8ade81e51ab2421e9195858c0ff47725aef62596
isfaaghyth <isfaaghyth@gmail.com>
2022-01-19 01:37:32 +0700

    removed unused logger on MediaLoaderActivityLifecycle

--- features/media/media-common/src/main/java/com/tokopedia/media/common/common/MediaLoaderActivityLifecycle.kt
+++ features/media/media-common/src/main/java/com/tokopedia/media/common/common/MediaLoaderActivityLifecycle.kt
@@ -5,22 +5,19 @@ import android.app.Application.ActivityLifecycleCallbacks
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.ViewGroup
-import com.bumptech.glide.Glide
 import com.tokopedia.applink.RouteManager.getIntent
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal.MEDIA_QUALITY_SETTING
-import com.tokopedia.config.GlobalConfig
 import com.tokopedia.dev_monitoring_tools.session.SessionDataUsageLogger
 import com.tokopedia.device.info.DeviceConnectionInfo.getConnectionType
 import com.tokopedia.kotlin.extensions.view.formattedToMB
 import com.tokopedia.logger.utils.Priority
 import com.tokopedia.media.common.R
 import com.tokopedia.media.common.data.HIGH_QUALITY
-import com.tokopedia.media.common.data.MediaBitmapSize
 import com.tokopedia.media.common.data.MediaSettingPreferences
 import com.tokopedia.media.common.util.getDirSize
 import com.tokopedia.unifycomponents.Toaster
-import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 import com.bumptech.glide.Glide.getPhotoCacheDir as getGlidePhotoCacheDir
 import com.tokopedia.media.common.util.NetworkManager.state as networkManagerState
@@ -29,10 +26,7 @@ class MediaLoaderActivityLifecycle(
         private val context: Context
 ) : ActivityLifecycleCallbacks {
 
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
-
     private val preferences by lazy { MediaSettingPreferences(context) }
-    private val bitmapSize by lazy { MediaBitmapSize(context) }
 
     private val logger by lazy {
         SessionDataUsageLogger(
@@ -44,11 +38,13 @@ class MediaLoaderActivityLifecycle(
     }
 
     override fun onActivityStarted(activity: Activity) {
-        if (!WHITELIST.singleOrNull {
-                    it == activity.javaClass.canonicalName
-                }.isNullOrEmpty()
-                && !preferences.toasterVisibility()
-                && networkManagerState(context) != HIGH_QUALITY) {
+        val className = activity.javaClass.canonicalName ?: return
+
+        val isFromWhiteList = !WHITELIST.singleOrNull { it == className }.isNullOrEmpty()
+        val toasterVisibility = !preferences.toasterVisibility()
+        val isQualitySettingHigh = networkManagerState(context) != HIGH_QUALITY
+
+        if (isFromWhiteList && toasterVisibility && isQualitySettingHigh) {
             try {
                 showToaster(activity)
             } catch (ignored: Exception) {}
@@ -59,9 +55,7 @@ class MediaLoaderActivityLifecycle(
         logger.returnFromOtherActivity = true
     }
 
-    override fun onActivityDestroyed(activity: Activity) {
-        scope.cancel()
-    }
+    override fun onActivityDestroyed(activity: Activity) {}
 
     override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
 
@@ -70,9 +64,8 @@ class MediaLoaderActivityLifecycle(
     override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
         logger.openedPageCount++
         logger.openedPageCountTotal++
-        logger.addJourney(activity)
 
-        glideMigration()
+        logger.addJourney(activity)
     }
 
     override fun onActivityResumed(activity: Activity) {
@@ -80,7 +73,6 @@ class MediaLoaderActivityLifecycle(
         if (logger.running) return
 
         val mediaAdditionalData = mapOf(
-            KEY_ACCUMULATIVE_SIZE to bitmapSize.getSize().formattedToMB(),
             KEY_INTERNAL_CACHE_SIZE to activity.cacheDir.getDirSize().formattedToMB(),
             KEY_GLIDE_CACHE_SIZE to getGlidePhotoCacheDir(context).getDirSize().formattedToMB()
         )
@@ -96,7 +88,7 @@ class MediaLoaderActivityLifecycle(
     }
 
     private fun showToaster(activity: Activity) {
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             with(activity) {
                 window.decorView.findViewById<ViewGroup>(android.R.id.content)?.let {
                     preferences.hasShowToaster(true) // set flag
@@ -117,20 +109,7 @@ class MediaLoaderActivityLifecycle(
         }, DELAY_PRE_SHOW_TOAST)
     }
 
-    private fun glideMigration() {
-        if (!preferences.glideMigration() && GlobalConfig.VERSION_NAME == MIGRATION_VERSION_SUPPORT) {
-            scope.launch {
-                Glide.get(context).clearDiskCache()
-
-                withContext(Dispatchers.Main) {
-                    preferences.hasGlideMigration(true)
-                }
-            }
-        }
-    }
-
     companion object {
-        private const val MIGRATION_VERSION_SUPPORT = "3.138"
         private const val DELAY_PRE_SHOW_TOAST = 2500L
 
         // for data usage logger
