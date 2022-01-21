8d6b2aade6e6d0740d59a88a83404a20c0222822
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-01-21 13:50:09 +0700

    remove log

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ShareExperienceViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/ShareExperienceViewComponent.kt
@@ -2,7 +2,6 @@ package com.tokopedia.play.view.viewcomponent
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.util.Log
 import android.view.ViewGroup
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
