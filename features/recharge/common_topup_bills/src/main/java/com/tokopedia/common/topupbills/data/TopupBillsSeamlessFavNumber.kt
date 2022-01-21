422cef2d14c283fc8a43d6b3a7db5c2054fbc230
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-01-21 13:56:21 +0700

    remove unused import

--- features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/TopupBillsSeamlessFavNumber.kt
+++ features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/TopupBillsSeamlessFavNumber.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.common.topupbills.data
 
 import android.os.Parcelable
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
