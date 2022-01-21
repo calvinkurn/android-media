1579059bd6fc1b5da0f5279b37ba6cec2c07200d
Steven <steven@BA-00003246.local>
2022-01-21 10:08:16 +0700

    specific imports on ProductListSectionContract.kt

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/ProductListSectionContract.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/ProductListSectionContract.kt
@@ -13,7 +13,13 @@ import com.tokopedia.filter.common.data.SavedOption
 import com.tokopedia.localizationchooseaddress.domain.model.LocalCacheModel
 import com.tokopedia.remoteconfig.RemoteConfig
 import com.tokopedia.search.analytics.GeneralSearchTrackingModel
-import com.tokopedia.search.result.presentation.model.*
+import com.tokopedia.search.result.presentation.model.BroadMatchDataView
+import com.tokopedia.search.result.presentation.model.BroadMatchItemDataView
+import com.tokopedia.search.result.presentation.model.EmptySearchProductDataView
+import com.tokopedia.search.result.presentation.model.GlobalNavDataView
+import com.tokopedia.search.result.presentation.model.InspirationCarouselDataView
+import com.tokopedia.search.result.presentation.model.ProductItemDataView
+import com.tokopedia.search.result.presentation.model.InspirationSizeDataView
 import com.tokopedia.sortfilter.SortFilterItem
 import org.json.JSONArray
 import java.util.*
