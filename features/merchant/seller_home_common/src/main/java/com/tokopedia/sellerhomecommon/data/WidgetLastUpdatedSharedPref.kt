0074c6defff23bd814902882a3147ea8e50158e8
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-01-21 11:24:52 +0800

    create widget last updated info shared preference

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/data/WidgetLastUpdatedSharedPref.kt
@@ -0,0 +1,32 @@
+package com.tokopedia.sellerhomecommon.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+ * Created by @ilhamsuaib on 21/01/22.
+ */
+
+class WidgetLastUpdatedSharedPref(
+    private val context: Context
+) : WidgetLastUpdatedSharedPrefInterface {
+
+    companion object {
+        private const val PREF_NAME = "shc_widget_last_updated_info_pref"
+        private const val DEFAULT = 0L
+    }
+
+    private val sharedPref: SharedPreferences by lazy {
+        context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+    }
+
+    override fun saveLastUpdateInfo(dataKey: String, timeInMillis: Long) {
+        val spe = sharedPref.edit()
+        spe.putLong(dataKey, timeInMillis)
+        spe.apply()
+    }
+
+    override fun getLastUpdateInfoInMillis(dataKey: String): Long {
+        return sharedPref.getLong(dataKey, DEFAULT)
+    }
+}
