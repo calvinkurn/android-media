5d1b3379021d32dd72ac3da35f7c6d1870539794
Steven <steven@BA-00003246.local>
2022-01-21 11:26:01 +0700

    revert testapp settings

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -125,6 +125,6 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return ApplinkConst.DISCOVERY_SEARCH_AUTOCOMPLETE
+        return ""
     }
 }
