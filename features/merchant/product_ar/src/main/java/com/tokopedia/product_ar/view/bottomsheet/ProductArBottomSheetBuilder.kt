c0ab00ef1f33c72237d36fd553719ac16fad918f
yehezkiel <yehezkiel@tokopedia.com>
2022-01-24 12:44:37 +0700

    fix some ui, add shadow in typography

--- features/merchant/product_ar/src/main/java/com/tokopedia/product_ar/view/bottomsheet/ProductArBottomSheetBuilder.kt
+++ features/merchant/product_ar/src/main/java/com/tokopedia/product_ar/view/bottomsheet/ProductArBottomSheetBuilder.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import com.tokopedia.product_ar.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
 
 object ProductArBottomSheetBuilder {
@@ -18,7 +19,11 @@ object ProductArBottomSheetBuilder {
             setTitle(context.getString(R.string.txt_bs_info_title))
             setChild(view)
             val txtInfo = view.findViewById<Typography>(R.id.txt_bs_ar_info)
+            val btnInfo = view.findViewById<UnifyButton>(R.id.btn_bs_ar_info)
             txtInfo.text = context.getString(R.string.txt_bs_info_desc)
+            btnInfo.setOnClickListener {
+                dismiss()
+            }
         }
 
         return bottomSheetUnify
