b035716f0da1ead4fe4b916c3e96755a0a688170
yehezkiel <yehezkiel@tokopedia.com>
2022-01-24 15:30:40 +0700

    add drop shadow image

--- features/merchant/product_ar/src/main/java/com/tokopedia/product_ar/util/AnimatedTextIcon.kt
+++ features/merchant/product_ar/src/main/java/com/tokopedia/product_ar/util/AnimatedTextIcon.kt
@@ -5,14 +5,21 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.widget.ImageView
 import android.widget.LinearLayout
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.media.loader.loadImageWithoutPlaceholder
 import com.tokopedia.product_ar.R
 import com.tokopedia.unifyprinciples.Typography
 
 class AnimatedTextIcon : LinearLayout {
+
+    companion object {
+        private const val GALERY_IMAGE = "https://images.tokopedia.net/img/android/others/ic_gallery_ar_small.png"
+        private const val CAMERA_IMAGE = "https://images.tokopedia.net/img/android/others/ic_camera_ar_small.png"
+    }
 
     constructor(context: Context) : super(context)
 
@@ -21,7 +28,7 @@ class AnimatedTextIcon : LinearLayout {
     constructor(context: Context, attrSet: AttributeSet, defStyleAttr: Int) : super(context, attrSet, defStyleAttr)
 
     private var txtTitle: Typography? = null
-    private var icImg: IconUnify? = null
+    private var icImg: ImageView? = null
     private val fadeOutAnimation = AlphaAnimation(1.0f, 0.0f).apply {
         duration = 500
         startOffset = 5000
@@ -55,8 +62,16 @@ class AnimatedTextIcon : LinearLayout {
     fun renderText(text: String, iconUnify: Int) {
         txtTitle?.show()
         txtTitle?.text = text
-        icImg?.setImage(newIconId = iconUnify)
+        loadImage(iconUnify)
         txtTitle?.startAnimation(fadeOutAnimation)
         invalidate()
+    }
+
+    private fun loadImage(iconUnify: Int) {
+        if (iconUnify == IconUnify.IMAGE) {
+            icImg?.loadImageWithoutPlaceholder(GALERY_IMAGE)
+        } else {
+            icImg?.loadImageWithoutPlaceholder(CAMERA_IMAGE)
+        }
     }
 }
