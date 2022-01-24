42976ff15a6631a8c44d33a610be7c00742bbf9f
shrutiagarwal <shruti.agarwal@tokopedia.com>
2022-01-24 14:08:08 +0530

    Crash fix

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/PostDynamicViewNew.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/PostDynamicViewNew.kt
@@ -687,7 +687,7 @@ class PostDynamicViewNew @JvmOverloads constructor(
                         spannableString.setSpan(
                             cs,
                             0,
-                            caption.author.name.length - 1 ,
+                            MethodChecker.fromHtml(caption.author.name).length - 1 ,
                             Spannable.SPAN_INCLUSIVE_INCLUSIVE
                         )
                         captionText.text = spannableString
@@ -717,7 +717,7 @@ class PostDynamicViewNew @JvmOverloads constructor(
             spannableString.setSpan(
                 cs,
                 0,
-                caption.author.name.length - 1,
+                MethodChecker.fromHtml(caption.author.name).length - 1,
                 Spannable.SPAN_INCLUSIVE_INCLUSIVE
             )
             captionText.text = spannableString
