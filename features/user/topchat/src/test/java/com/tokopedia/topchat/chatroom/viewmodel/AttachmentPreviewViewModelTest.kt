8bc290119e055d594f99cb27a8f6411fdec85e61
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2022-01-25 14:40:30 +0700

    fix unit test

--- features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/AttachmentPreviewViewModelTest.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatroom/viewmodel/AttachmentPreviewViewModelTest.kt
@@ -1,31 +1,14 @@
 package com.tokopedia.topchat.chatroom.viewmodel
 
-import com.tokopedia.attachcommon.data.ResultProduct
-import com.tokopedia.attachcommon.preview.ProductPreview
-import com.tokopedia.topchat.chatroom.view.viewmodel.SendableProductPreview
+import com.tokopedia.topchat.chatroom.view.viewmodel.TopchatProductAttachmentPreviewUiModel
 import com.tokopedia.topchat.chatroom.viewmodel.base.BaseTopChatViewModelTest
-import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AttachmentPreviewViewModelTest : BaseTopChatViewModelTest() {
 
     val resultProduct = arrayListOf(
-        ResultProduct(
-            name = "a",
-            productImageThumbnail = "h",
-            price = "1",
-            productId = "1"
-        )
-    )
-
-    val notEnoughRequiredProductData = arrayListOf(
-        ResultProduct(
-            name = "",
-            productImageThumbnail = "",
-            price = "",
-            productId = ""
-        )
+            "1"
     )
 
     @Test
@@ -39,28 +22,20 @@ class AttachmentPreviewViewModelTest : BaseTopChatViewModelTest() {
     }
 
     @Test
-    fun should_not_add_product_attachment_preview_on_initialization_when_not_enough_required_data() {
-        // When
-        viewModel.loadProductPreview(notEnoughRequiredProductData)
-
-        // Then
-        assertEquals(viewModel.getAttachmentsPreview().size, 0)
-    }
-
-    @Test
     fun should_map_product_preview_product_id() {
         // When
         viewModel.loadProductPreview(resultProduct)
         val productIds = viewModel.getProductIdPreview()
 
         // Then
-        assertEquals(productIds[0], resultProduct[0].productId)
+        assertEquals(productIds[0], resultProduct[0])
     }
 
     @Test
     fun should_get_attachment_preview_when_init() {
         //Given
-        val sendablePreview = SendableProductPreview(ProductPreview())
+        val sendablePreview = TopchatProductAttachmentPreviewUiModel.Builder()
+                .build()
 
         //When
         viewModel.addAttachmentPreview(sendablePreview)
