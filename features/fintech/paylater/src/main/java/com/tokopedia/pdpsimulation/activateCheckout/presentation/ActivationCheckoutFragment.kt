01087cb127771dc8a54c802703a8617b78a9b9a6
Rohan <rohan.ghosh@tokopedia.com>
2022-01-25 18:21:30 +0530

    completed Ui component

--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/activateCheckout/presentation/ActivationCheckoutFragment.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/activateCheckout/presentation/ActivationCheckoutFragment.kt
@@ -4,30 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.viewpager.widget.ViewPager
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
-import com.tokopedia.globalerror.GlobalError
-import com.tokopedia.kotlin.extensions.view.*
 import com.tokopedia.pdpsimulation.R
-import com.tokopedia.pdpsimulation.common.analytics.PdpSimulationEvent
-import com.tokopedia.pdpsimulation.common.constants.PRODUCT_PRICE
-import com.tokopedia.pdpsimulation.common.di.component.PdpSimulationComponent
-import com.tokopedia.pdpsimulation.common.listener.PdpSimulationCallback
-import com.tokopedia.pdpsimulation.paylater.domain.model.Detail
-import com.tokopedia.pdpsimulation.paylater.domain.model.PayLaterAllData
-import com.tokopedia.pdpsimulation.paylater.domain.model.PayLaterGetSimulation
-import com.tokopedia.pdpsimulation.paylater.presentation.detail.adapter.PayLaterOfferPagerAdapter
-import com.tokopedia.pdpsimulation.paylater.viewModel.PayLaterViewModel
-import com.tokopedia.sortfilter.SortFilterItem
-import com.tokopedia.unifycomponents.ChipsUnify
-import com.tokopedia.usecase.coroutines.Fail
-import com.tokopedia.usecase.coroutines.Success
-import kotlinx.android.synthetic.main.fragment_paylater_offers.*
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
-import javax.inject.Inject
+
 
 class ActivationCheckoutFragment : BaseDaggerFragment() {
 
