9477150d4a230d9e08a1c4904f97183869c34a1f
meyta.taliti <meyta.taliti@tokopedia.com>
2022-01-26 14:55:20 +0700

    remove unnecessary Exposed

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/campaign/GetCampaignProductResponse.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/campaign/GetCampaignProductResponse.kt
@@ -1,25 +1,21 @@
 package com.tokopedia.play.broadcaster.domain.model.campaign
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by meyta.taliti on 25/01/22.
  */
 data class GetCampaignProductResponse(
-    @Expose
     @SerializedName("getCampaignProduct")
     val getCampaignProduct: GetCampaignProduct = GetCampaignProduct()
 ) {
 
     data class GetCampaignProduct(
-        @Expose
         @SerializedName("Products")
         val products: List<Product> = listOf()
     )
 
     data class Product(
-        @Expose
         @SerializedName("ID")
         val id: Long = 0
     )
