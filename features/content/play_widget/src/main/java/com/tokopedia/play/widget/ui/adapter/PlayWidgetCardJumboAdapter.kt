9a945edda35095af9a7e4268c7ed27ff816a5f9b
astidhiyaa <asti.anzaria@tokopedia.com>
2022-01-26 12:05:46 +0800

    fix: remove transcode

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/adapter/PlayWidgetCardJumboAdapter.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/adapter/PlayWidgetCardJumboAdapter.kt
@@ -3,22 +3,17 @@ package com.tokopedia.play.widget.ui.adapter
 import com.tokopedia.adapterdelegate.BaseDiffUtilAdapter
 import com.tokopedia.play.widget.ui.adapter.delegate.jumbo.PlayWidgetCardJumboBannerAdapterDelegate
 import com.tokopedia.play.widget.ui.adapter.delegate.jumbo.PlayWidgetCardJumboChannelAdapterDelegate
-import com.tokopedia.play.widget.ui.adapter.delegate.jumbo.PlayWidgetCardJumboTranscodeAdapterDelegate
 import com.tokopedia.play.widget.ui.adapter.viewholder.jumbo.PlayWidgetCardJumboBannerViewHolder
 import com.tokopedia.play.widget.ui.adapter.viewholder.jumbo.PlayWidgetCardJumboChannelViewHolder
-import com.tokopedia.play.widget.ui.adapter.viewholder.jumbo.PlayWidgetCardJumboTranscodeViewHolder
 import com.tokopedia.play.widget.ui.model.PlayWidgetChannelUiModel
 import com.tokopedia.play.widget.ui.model.PlayWidgetItemUiModel
-import com.tokopedia.play_common.util.blur.ImageBlurUtil
 
 /**
  * @author by astidhiyaa on 12/01/22
  */
 class PlayWidgetCardJumboAdapter(
-    imageBlurUtil: ImageBlurUtil,
     channelCardListener: PlayWidgetCardJumboChannelViewHolder.Listener,
     bannerCardListener: PlayWidgetCardJumboBannerViewHolder.Listener,
-    transcodeListener: PlayWidgetCardJumboTranscodeViewHolder.Listener
 ) :
     BaseDiffUtilAdapter<PlayWidgetItemUiModel>() {
 
@@ -26,7 +21,6 @@ class PlayWidgetCardJumboAdapter(
         delegatesManager
             .addDelegate(PlayWidgetCardJumboBannerAdapterDelegate(bannerCardListener))
             .addDelegate(PlayWidgetCardJumboChannelAdapterDelegate(channelCardListener))
-            .addDelegate(PlayWidgetCardJumboTranscodeAdapterDelegate(imageBlurUtil, transcodeListener))
     }
 
     override fun areItemsTheSame(
--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/adapter/PlayWidgetCardLargeAdapter.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/adapter/PlayWidgetCardLargeAdapter.kt
@@ -3,22 +3,17 @@ package com.tokopedia.play.widget.ui.adapter
 import com.tokopedia.adapterdelegate.BaseDiffUtilAdapter
 import com.tokopedia.play.widget.ui.adapter.delegate.large.PlayWidgetCardLargeBannerAdapterDelegate
 import com.tokopedia.play.widget.ui.adapter.delegate.large.PlayWidgetCardLargeChannelAdapterDelegate
-import com.tokopedia.play.widget.ui.adapter.delegate.large.PlayWidgetCardLargeTranscodeAdapterDelegate
 import com.tokopedia.play.widget.ui.adapter.viewholder.large.PlayWidgetCardLargeBannerViewHolder
 import com.tokopedia.play.widget.ui.adapter.viewholder.large.PlayWidgetCardLargeChannelViewHolder
-import com.tokopedia.play.widget.ui.adapter.viewholder.large.PlayWidgetCardLargeTranscodeViewHolder
-import com.tokopedia.play.widget.ui.model.PlayWidgetItemUiModel
 import com.tokopedia.play.widget.ui.model.PlayWidgetChannelUiModel
-import com.tokopedia.play_common.util.blur.ImageBlurUtil
+import com.tokopedia.play.widget.ui.model.PlayWidgetItemUiModel
 
 /**
  * @author by astidhiyaa on 11/01/22
  */
 class PlayWidgetCardLargeAdapter(
-    imageBlurUtil: ImageBlurUtil,
     channelCardListener: PlayWidgetCardLargeChannelViewHolder.Listener,
     bannerCardListener: PlayWidgetCardLargeBannerViewHolder.Listener,
-    transcodeCardListener: PlayWidgetCardLargeTranscodeViewHolder.Listener
 ) :
     BaseDiffUtilAdapter<PlayWidgetItemUiModel>() {
 
@@ -26,7 +21,6 @@ class PlayWidgetCardLargeAdapter(
         delegatesManager
             .addDelegate(PlayWidgetCardLargeBannerAdapterDelegate(bannerCardListener))
             .addDelegate(PlayWidgetCardLargeChannelAdapterDelegate(channelCardListener))
-            .addDelegate(PlayWidgetCardLargeTranscodeAdapterDelegate(imageBlurUtil, transcodeCardListener))
     }
 
     override fun areItemsTheSame(
