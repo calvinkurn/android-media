490ad1dd504198a20cd4dd2566250e36c4d5f4d4
astidhiyaa <asti.anzaria@tokopedia.com>
2022-01-27 18:35:57 +0800

    feat - show stock

--- features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/viewholder/ProductLineViewHolder.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/viewholder/ProductLineViewHolder.kt
@@ -1,10 +1,12 @@
 package com.tokopedia.play.ui.productsheet.viewholder
 
 import android.view.View
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.iconunify.getIconUnifyDrawable
 import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.play.R
 import com.tokopedia.play.ui.product.ProductBasicViewHolder
@@ -23,6 +25,7 @@ class ProductLineViewHolder(itemView: View, private val listener: Listener) : Pr
     private val btnProductAtc: UnifyButton = itemView.findViewById(R.id.btn_product_atc)
     private val lblOutOfStock: Label = itemView.findViewById(R.id.label_out_of_stock)
     private val shadowOutOfStock: View = itemView.findViewById(R.id.shadow_out_of_stock)
+    private val tvOutOfStock: TextView = itemView.findViewById(R.id.tv_product_out_of_stock)
 
     override fun bind(item: PlayProductUiModel.Product) {
         super.bind(item)
@@ -30,6 +33,7 @@ class ProductLineViewHolder(itemView: View, private val listener: Listener) : Pr
             OutOfStock -> {
                 shadowOutOfStock.show()
                 lblOutOfStock.show()
+                tvOutOfStock.gone()
                 btnProductAtc.setDrawable(
                     getIconUnifyDrawable(itemView.context, IconUnify.ADD, ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_NN100))
                 )
@@ -40,6 +44,9 @@ class ProductLineViewHolder(itemView: View, private val listener: Listener) : Pr
             is StockAvailable -> {
                 shadowOutOfStock.gone()
                 lblOutOfStock.gone()
+                tvOutOfStock.shouldShowWithAction(item.stock.stock <= MIN_STOCK){
+                    tvOutOfStock.text = getString(R.string.play_product_item_stock, item.stock.stock)
+                }
                 btnProductBuy.isEnabled = true
                 btnProductAtc.isEnabled = true
                 btnProductAtc.setDrawable(
@@ -59,6 +66,8 @@ class ProductLineViewHolder(itemView: View, private val listener: Listener) : Pr
 
     companion object {
         val LAYOUT = R.layout.item_play_product_line
+
+        private const val MIN_STOCK: Int = 5
     }
 
     interface Listener : ProductBasicViewHolder.Listener {
