03f840cdca12c9d3eadcaf064c7228eb117b885e
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-01-27 19:13:50 +0700

    revert unneede changes

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -12,8 +12,6 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
-import com.tokopedia.digital_product_detail.presentation.activity.DigitalPDPPulsaActivity
-import com.tokopedia.digital_product_detail.presentation.activity.DigitalPDPDataPlanActivity
 import com.tokopedia.tkpd.testgql.TestGqlUseCase
 import com.tokopedia.url.TokopediaUrl
 import com.tokopedia.user.session.UserSession
@@ -117,13 +115,10 @@ class MainActivity : AppCompatActivity() {
          * RouteManager.route(this, ApplinkConstInternalMarketplace.SHOP_SETTINGS)
          * LEAVE THIS EMPTY AS DEFAULT!!
          * */
-//        startActivity(Intent(this, DigitalPDPPulsaActivity::class.java))
         val appLink = etAppLink.text.toString()
         if(appLink.isNotBlank())
             RouteManager.route(this, appLink)
-        else
-//            startActivity(Intent(this, DigitalPDPPulsaActivity::class.java))
-            startActivity(Intent(this, DigitalPDPDataPlanActivity::class.java))
+        else Toast.makeText(this, "Please input appLink / webLink", Toast.LENGTH_SHORT).show()
     }
 
     private fun getDefaultAppLink(): String {
