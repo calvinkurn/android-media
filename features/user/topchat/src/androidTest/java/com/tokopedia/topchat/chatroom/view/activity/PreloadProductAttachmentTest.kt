44087479d9d9c90c1d924bdba07b62cf205473d5
rifqimfahmi <rifqi.fahmi@tokopedia.com>
2022-01-27 08:59:32 +0700

    create PreloadProductAttachmentTest

---
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/PreloadProductAttachmentTest.kt
@@ -0,0 +1,20 @@
+package com.tokopedia.topchat.chatroom.view.activity
+
+import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
+
+class PreloadProductAttachmentTest: TopchatRoomTest() {
+
+    // TODO: should show loading product preview
+    // TODO: should render actual product data for product preview when success pre attach product payload
+    // TODO: should show error product preview when error pre attach product payload
+    // TODO: should retry preload product attachment when user click retry
+    // TODO: should send text only when attach product preview is loading
+    // TODO: should send sticker only when attach product preview is loading
+    // TODO: should send text only when attach product preview is error
+    // TODO: should send sticker only when attach product preview is loading
+    // TODO: should show srw preview when attach product preview is success
+    // TODO: should hide srw preview when attach product preview is loading
+    // TODO: should hide srw preview when attach product preview is error
+    // TODO: should show srw preview when user retry attach product preview is success
+
+}
