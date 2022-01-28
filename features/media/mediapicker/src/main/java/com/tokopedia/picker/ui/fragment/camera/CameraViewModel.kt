7eb111e8ff9a6645abd3966f985788f0e10241c7
isfaaghyth <isfaaghyth@gmail.com>
2022-01-29 00:45:52 +0700

    set as state instead of media ui model on CameraViewModel

--- features/media/mediapicker/src/main/java/com/tokopedia/picker/ui/fragment/camera/CameraViewModel.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/picker/ui/fragment/camera/CameraViewModel.kt
@@ -1,9 +1,9 @@
 package com.tokopedia.picker.ui.fragment.camera
 
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.picker.ui.uimodel.MediaUiModel
 import com.tokopedia.picker.utils.EventBusFactory
 import com.tokopedia.picker.utils.EventState
 import kotlinx.coroutines.flow.flowOn
@@ -11,14 +11,14 @@ import javax.inject.Inject
 
 class CameraViewModel @Inject constructor(
     dispatchers: CoroutineDispatchers
-): ViewModel() {
+): ViewModel(), LifecycleObserver {
 
     val uiEvent = EventBusFactory
         .subscriber(viewModelScope)
         .flowOn(dispatchers.computation)
 
-    fun send(mediaUiModel: MediaUiModel) {
-        EventBusFactory.emit(EventState.CameraCaptured(mediaUiModel))
+    fun send(eventState: EventState) {
+        EventBusFactory.emit(eventState)
     }
 
 }
