627bdfb6c3b0280aa4094b5911948e2c764af6c5
yorisprayogo <yoris.prayogo@tokopedia.com>
2022-01-29 01:13:45 +0700

    fix hashmap

--- libraries/composite_lib/user_session/src/main/java/com/tokopedia/user/session/UserSessionMap.java
+++ libraries/composite_lib/user_session/src/main/java/com/tokopedia/user/session/UserSessionMap.java
@@ -3,8 +3,7 @@ package com.tokopedia.user.session;
 import android.util.Pair;
 
 import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class UserSessionMap {
-    public static ConcurrentHashMap<Pair<String, String>, Object> map = new ConcurrentHashMap<>();
+    public static HashMap<Pair<String, String>, Object> map = new HashMap<>();
 }
