337c18bfba884db9f2279f089a6a84d73f9d31c4
yohann.prananta <yohann.prananta@BA-00019887.local>
2022-02-02 09:51:34 +0700

    fix productlist navigation issue

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/view/fragment/ProductCouponPreviewFragment.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/view/fragment/ProductCouponPreviewFragment.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.vouchercreation.product.create.view.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -40,7 +39,6 @@ import com.tokopedia.vouchercreation.product.create.view.bottomsheet.TermAndCond
 import com.tokopedia.vouchercreation.product.create.view.dialog.CreateProductCouponFailedDialog
 import com.tokopedia.vouchercreation.product.create.view.dialog.UpdateProductCouponFailedDialog
 import com.tokopedia.vouchercreation.product.create.view.viewmodel.ProductCouponPreviewViewModel
-import com.tokopedia.vouchercreation.product.list.view.activity.ProductListActivity
 import com.tokopedia.vouchercreation.shop.create.view.enums.VoucherCreationStep
 import java.net.URLEncoder
 import java.util.*
@@ -98,9 +96,7 @@ class ProductCouponPreviewFragment private constructor(): BaseDaggerFragment() {
 
     private var onNavigateToCouponInformationPage: () -> Unit = {}
     private var onNavigateToCouponSettingsPage: () -> Unit = {}
-    private var onNavigateToProductListPage: () -> Unit = {
-        startActivity(Intent(context,ProductListActivity::class.java))
-    }
+    private var onNavigateToProductListPage: () -> Unit = {}
     private var onUpdateCouponSuccess: () -> Unit = {}
     private var onCreateCouponSuccess: (Coupon) -> Unit = {}
     private var couponSettings: CouponSettings? = null
