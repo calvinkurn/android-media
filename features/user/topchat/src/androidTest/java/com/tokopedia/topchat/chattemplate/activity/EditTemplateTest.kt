357732e55581eb75762202d4d2bc0d26e3cca7af
kelvinsaputra <kelvin.saputra@tokopedia.com>
2022-02-02 20:46:19 +0700

    instrument test edit chat template

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chattemplate/activity/EditTemplateTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chattemplate/activity/EditTemplateTest.kt
@@ -1,11 +1,144 @@
 package com.tokopedia.topchat.chattemplate.activity
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.tokopedia.test.application.annotations.UiTest
+import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chattemplate.activity.base.BaseEditTemplateTest
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
+@UiTest
 class EditTemplateTest: BaseEditTemplateTest() {
 
     @Test
-    fun a() {
+    fun should_open_with_text_when_edit() {
+        //When
+        launchActivity()
+
+        //Then
+        onView(withId(R.id.edittext)).check(matches(withText(testTemplateList.first())))
+    }
+
+    @Test
+    fun should_open_with_empty_text_when_create() {
+        //When
+        launchActivity(
+            message = null,
+            position = -1,
+            sizeList = 1,
+            arrayListTemplate = arrayListOf()
+        )
+
+        //Then
+        onView(withId(R.id.edittext))
+            .check(matches(withHint(context.getString(R.string.hint_template_chat))))
+    }
+
+    @Test
+    fun should_not_able_to_save_when_less_than_5_chars() {
+        //Given
+        val expectedText = "Test"
+
+        //When
+        launchActivity()
+        onView(withId(R.id.edittext)).perform(replaceText(expectedText))
+
+        //Then
+        onView(withId(R.id.edittext)).check(matches(withText(expectedText)))
+        onView(withId(R.id.error)).check(matches(isDisplayed()))
+        onView(withId(R.id.error)).check(matches(withText(R.string.minimal_char_template)))
+    }
+
+    @Test
+    fun should_able_to_save_when_more_than_5_chars() {
+        //Given
+        val expectedText = "Test 123 Test 456"
+
+        //When
+        launchActivity()
+        onView(withId(R.id.edittext)).perform(replaceText(expectedText))
+
+        //Then
+        onView(withId(R.id.edittext)).check(matches(withText(expectedText)))
+        onView(withId(R.id.error)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun should_not_able_to_save_when_more_than_200_chars() {
+        //Given
+        val maxChar = 200
+        val expectedText = getLongTestText()
+        val errorText = context.getString(R.string.maximal_char_template, maxChar)
+
+        //When
+        launchActivity()
+        onView(withId(R.id.edittext)).perform(replaceText(expectedText))
+
+        //Then
+        onView(withId(R.id.edittext)).check(matches(withText(expectedText)))
+        onView(withId(R.id.error)).check(matches(withText(errorText)))
+    }
+
+    @Test
+    fun should_not_show_delete_button_when_create_template() {
+        //When
+        launchActivity(
+            message = null,
+            position = -1,
+            sizeList = 1,
+            arrayListTemplate = arrayListOf()
+        )
+
+        //Then
+        onView(withId(R.id.action_organize)).check(doesNotExist())
+    }
+
+    @Test
+    fun should_show_pop_up_when_delete_template() {
+        //When
+        launchActivity()
+        onView(withId(R.id.action_organize)).perform(click())
+
+        //Then
+        assertWithSubText(context.getString(R.string.delete_chat_template))
+        assertWithSubText(context.getString(R.string.forever_deleted_template))
+    }
+
+    @Test
+    fun should_show_snackBar_when_delete_last_template() {
+        //Given
+        val list = arrayListOf("Test")
+
+        //When
+        launchActivity(sizeList = 2, arrayListTemplate = list)
+        onView(withId(R.id.action_organize)).perform(click())
+
+        //Then
+        assertWithSubText(context.getString(R.string.minimum_template_chat_warning))
+    }
+
+    private fun getLongTestText(): String {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor ex mi, " +
+                "eu egestas lacus consectetur et. In eleifend luctus ligula, eu efficitur tellus " +
+                "elementum quis. Mauris dapibus auctor nisl, sit amet suscipit erat sagittis vitae. " +
+                "Ut id blandit lacus, in pharetra quam. Ut sed massa tristique, " +
+                "ornare lacus et, mollis quam. Aenean eu iaculis urna. Suspendisse laoreet semper " +
+                "erat sed tristique. Nam sagittis nibh ligula, vel varius neque pretium a. " +
+                "Sed eget luctus tortor. Quisque enim tellus, condimentum eget porta in, consectetur ac neque. " +
+                "Ut lacus diam, lobortis et tortor non, porttitor vestibulum ipsum. Mauris vel aliquam dui.\n" +
+                "Nunc eu arcu eu mauris ornare lacinia. Vivamus ultricies, dui eu pretium varius, " +
+                "urna purus ornare nulla, non dapibus eros elit a urna. Morbi consectetur risus libero, " +
+                "in pellentesque enim viverra vel. Curabitur mollis consequat justo vitae elementum. " +
+                "Morbi non quam eget tellus blandit aliquet a in turpis. Proin nec neque accumsan massa " +
+                "hendrerit venenatis consequat eget neque. Morbi venenatis venenatis erat eget fermentum. " +
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis placerat enim, " +
+                "in gravida mauris. Duis ac eros finibus, semper mauris eget, sagittis metus. " +
+                "Pellentesque rhoncus rhoncus nunc, sit amet fringilla ipsum dictum eget. Morbi quis sapien." +
+                "Test Test Test Test Test"
     }
 }
--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chattemplate/activity/base/BaseEditTemplateTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chattemplate/activity/base/BaseEditTemplateTest.kt
@@ -1,13 +1,22 @@
 package com.tokopedia.topchat.chattemplate.activity.base
 
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withSubstring
 import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.config.GlobalConfig
+import com.tokopedia.topchat.chattemplate.view.activity.TemplateChatActivity
+import com.tokopedia.topchat.chattemplate.view.fragment.TemplateChatFragment
+import com.tokopedia.topchat.common.InboxMessageConstant
 import com.tokopedia.topchat.stub.chattemplate.di.DaggerTemplateChatComponentStub
 import com.tokopedia.topchat.stub.chattemplate.di.TemplateChatComponentStub
 import com.tokopedia.topchat.stub.chattemplate.usecase.*
 import com.tokopedia.topchat.stub.chattemplate.view.activity.EditTemplateChatActivityStub
-import com.tokopedia.topchat.stub.chattemplate.view.activity.TemplateChatActivityStub
 import com.tokopedia.topchat.stub.common.di.DaggerFakeBaseAppComponent
 import com.tokopedia.topchat.stub.common.di.module.FakeAppModule
 import org.junit.Before
@@ -25,6 +34,7 @@ abstract class BaseEditTemplateTest {
     protected val applicationContext: Context
         get() = InstrumentationRegistry
             .getInstrumentation().context.applicationContext
+    protected open lateinit var activity: EditTemplateChatActivityStub
 
     @Inject
     lateinit var createTemplateUseCase: CreateTemplateUseCaseStub
@@ -34,6 +44,14 @@ abstract class BaseEditTemplateTest {
 
     @Inject
     lateinit var deleteTemplateUseCase: DeleteTemplateUseCaseStub
+
+    protected val testTemplateList = arrayListOf(
+        "Test template 1",
+        "Test template 2",
+        "Test template 3",
+        "Test template 4",
+        "Test template 5"
+    )
 
     @Before
     open fun before() {
@@ -48,6 +66,48 @@ abstract class BaseEditTemplateTest {
             .fakeBaseAppComponent(baseComponent)
             .build()
         chatTemplateComponentStub!!.inject(this)
+    }
+
+    protected fun launchActivity(
+        isSellerApp: Boolean = false,
+        message: String? = testTemplateList.first(),
+        position: Int = 0,
+        sizeList: Int = 5,
+        arrayListTemplate: ArrayList<String> = testTemplateList
+    ) {
+        if (isSellerApp) {
+            GlobalConfig.APPLICATION_TYPE = GlobalConfig.SELLER_APPLICATION
+        }
+        val intent = Intent().apply {
+            this.putExtras(createBundleData(message, position, sizeList, arrayListTemplate, isSellerApp))
+        }
+        activityTestRule.launchActivity(intent)
+        activity = activityTestRule.activity
+    }
+
+    private fun createBundleData(
+        message: String?,
+        position: Int,
+        sizeList: Int,
+        listTemplate: ArrayList<String>,
+        isSellerApp: Boolean
+    ): Bundle {
+        val bundle = Bundle()
+        bundle.putString(InboxMessageConstant.PARAM_MESSAGE, message)
+        bundle.putInt(InboxMessageConstant.PARAM_POSITION, position)
+        bundle.putInt(InboxMessageConstant.PARAM_NAV, sizeList - 1) //Minus last button
+        bundle.putStringArrayList(InboxMessageConstant.PARAM_ALL, listTemplate)
+        if (message == null) {
+            bundle.putInt(InboxMessageConstant.PARAM_MODE, TemplateChatFragment.CREATE)
+        } else {
+            bundle.putInt(InboxMessageConstant.PARAM_MODE, TemplateChatFragment.EDIT)
+        }
+        bundle.putBoolean(TemplateChatActivity.PARAM_IS_SELLER, isSellerApp)
+        return bundle
+    }
+
+    protected fun assertWithSubText(msg: String) {
+        onView(withSubstring(msg)).check(matches(isDisplayed()))
     }
 
     companion object {
