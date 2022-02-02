6fd3a075385c6589558b1e7296584b71cb8fd3a9
mohit-toko <mohit.singh@tokopedia.com>
2022-02-02 11:50:38 +0530

    Url changes

--- features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogDetailPageFragment.kt
+++ features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogDetailPageFragment.kt
@@ -454,7 +454,7 @@ class CatalogDetailPageFragment : Fragment(),
         linkerData.id = catalogId
         linkerData.type = LinkerData.CATALOG_TYPE
         linkerData.name = getString(com.tokopedia.catalog.R.string.catalog_share_link_name,catalogName)
-        linkerData.uri  = CatalogUtil.getShareUrl(catalogId)
+        linkerData.uri  = CatalogUtil.getShareURI(catalogUrl)
         linkerData.description = getString(com.tokopedia.catalog.R.string.catalog_share_link_description)
         linkerData.isThrowOnError = true
         val linkerShareData = LinkerShareData()
