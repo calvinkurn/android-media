1bd126f7cfe1a800e251c0635d8efa31ad0907f7
yehezkiel <yehezkiel@tokopedia.com>
2022-02-03 14:03:07 +0700

    add now15 warehouse id tracker

--- features/merchant/atc_variant/src/main/java/com/tkpd/atcvariant/view/bottomsheet/AtcVariantBottomSheet.kt
+++ features/merchant/atc_variant/src/main/java/com/tkpd/atcvariant/view/bottomsheet/AtcVariantBottomSheet.kt
@@ -95,6 +95,12 @@ class AtcVariantBottomSheet : BottomSheetUnify(),
         ViewModelProvider(requireActivity()).get(AtcVariantSharedViewModel::class.java)
     }
 
+    private val localizationChooseAddressData by lazy(LazyThreadSafetyMode.NONE) {
+        context?.let {
+            ChooseAddressUtils.getLocalizingAddressData(it)
+        }
+    }
+
     private var loadingProgressDialog: ProgressDialog? = null
 
     private val compositeSubscription by lazy { CompositeSubscription() }
@@ -525,7 +531,8 @@ class AtcVariantBottomSheet : BottomSheetUnify(),
                 isCod = variantAggregatorData?.isCod ?: false,
                 ratesEstimateData = ratesEstimateData,
                 buyerDistrictId = buyerDistrictId,
-                sellerDistrictId = sellerDistrictId
+                sellerDistrictId = sellerDistrictId,
+                lcaWarehouseId = localizationChooseAddressData?.warehouse_id ?: ""
         )
     }
 
