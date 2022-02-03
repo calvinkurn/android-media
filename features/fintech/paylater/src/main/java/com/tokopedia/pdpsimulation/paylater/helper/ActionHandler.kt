000b337f1ebc3f298c2c02bda61d56cd02f4076d
Rohan <rohan.ghosh@tokopedia.com>
2022-02-03 17:04:11 +0530

    resolved cherry pick

--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/helper/ActionHandler.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/helper/ActionHandler.kt
@@ -6,10 +6,17 @@ import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
 import com.tokopedia.pdpsimulation.paylater.domain.model.Cta
 import com.tokopedia.pdpsimulation.paylater.domain.model.Detail
+import com.tokopedia.pdpsimulation.paylater.domain.model.InstallmentDetails
 import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterActionStepsBottomSheet
+import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterInstallmentFeeInfo
 import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterTokopediaGopayBottomsheet
 
 object ActionHandler {
+
+    private const val TYPE_APP_LINK = 1
+    private const val TYPE_WEB_VIEW = 2
+    private const val TYPE_HOW_TO_USE = 3
+    private const val TYPE_HOW_TO_USE_II = 4
 
     fun handleClickNavigation(
         context: Context?,
@@ -17,37 +24,36 @@ object ActionHandler {
         openHowToUse: (Bundle) -> Unit,
         openGoPay: (Bundle) -> Unit
     ) {
-
         when (detail.cta.cta_type) {
-            1 -> {
-                if (!detail.cta.android_url.isNullOrEmpty())
-                    try {
-                        RouteManager.route(
-                            context,
-                            detail.cta.android_url
-                        )
-                    } catch (e: Exception) {
-                    }
+            TYPE_APP_LINK -> routeToAppLink(context, detail.cta.android_url)
+            TYPE_WEB_VIEW -> {
+                if (shouldShowGoPayBottomSheet(detail))
+                    openGoPay(getGoPayBundle(detail.cta))
+                else routeToWebView(context, detail.cta.android_url)
             }
-            2 -> {
-                if (!detail.cta.android_url.isNullOrEmpty() && detail.cta.bottomSheet != null && detail.cta.bottomSheet.isShow == true) {
-                    val bundle = getGoPayBundle(detail.cta)
-                    openGoPay(bundle)
-                } else if (!detail.cta.android_url.isNullOrEmpty())
-                    RouteManager.route(
-                        context,
-                        ApplinkConstInternalGlobal.WEBVIEW,
-                        detail.cta.android_url
-                    )
-            }
-            3, 4 -> {
+            TYPE_HOW_TO_USE, TYPE_HOW_TO_USE_II -> {
                 if (detail.gatewayDetail?.how_toUse != null) {
-                    val bundle = getHowToUseBundle(detail)
-                    openHowToUse(bundle)
+                    openHowToUse(getHowToUseBundle(detail))
                 }
             }
         }
     }
+
+    private fun routeToAppLink(context: Context?, appLink: String?) {
+        if (!appLink.isNullOrEmpty())
+            try {
+                RouteManager.route(context, appLink)
+            } catch (e: Exception) {
+            }
+    }
+
+    private fun routeToWebView(context: Context?, webLink: String?) {
+        if (!webLink.isNullOrEmpty())
+            RouteManager.route(context, ApplinkConstInternalGlobal.WEBVIEW, webLink)
+    }
+
+    private fun shouldShowGoPayBottomSheet(detail: Detail) = !detail.cta.android_url.isNullOrEmpty()
+            && detail.cta.bottomSheet != null && detail.cta.bottomSheet.isShow == true
 
     private fun getHowToUseBundle(detail: Detail) = Bundle().apply {
         putParcelable(PayLaterActionStepsBottomSheet.STEPS_DATA, detail)
@@ -56,6 +62,11 @@ object ActionHandler {
     private fun getGoPayBundle(cta: Cta) =
         Bundle().apply {
             putParcelable(PayLaterTokopediaGopayBottomsheet.GOPAY_BOTTOMSHEET_DETAIL, cta)
+        }
+
+    fun getInstallmentBundle(installment: InstallmentDetails) =
+        Bundle().apply {
+            putParcelable(PayLaterInstallmentFeeInfo.INSTALLMENT_DETAIL, installment)
         }
 
 }
