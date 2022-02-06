665e13264c9a0983ed76f8c52a68b03f26d49eec
Muhammad Furqan <furqan@tokopedia.com>
2022-02-07 03:33:40 +0700

    [Shop Page][Donation] Fix Donation model to data class

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/model/ShopHomeCardDonationUiModel.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/model/ShopHomeCardDonationUiModel.kt
@@ -2,7 +2,7 @@ package com.tokopedia.shop.home.view.model
 
 import com.tokopedia.shop.home.view.adapter.ShopHomeAdapterTypeFactory
 
-class ShopHomeCardDonationUiModel(
+data class ShopHomeCardDonationUiModel(
     override val widgetId: String,
     override val layoutOrder: Int,
     override val name: String,
@@ -12,24 +12,5 @@ class ShopHomeCardDonationUiModel(
 
     override fun type(typeFactory: ShopHomeAdapterTypeFactory): Int =
         typeFactory.type(this)
-
-    override fun equals(other: Any?): Boolean {
-        return other?.let {
-            it is ShopHomeCardDonationUiModel &&
-                    this.widgetId == it.widgetId &&
-                    this.type == it.type &&
-                    !this.isNewData &&
-                    this.widgetState == it.widgetState
-        } ?: false
-    }
-
-    override fun hashCode(): Int {
-        var result = widgetId.hashCode()
-        result = 31 * result + layoutOrder
-        result = 31 * result + name.hashCode()
-        result = 31 * result + type.hashCode()
-        result = 31 * result + header.hashCode()
-        return result
-    }
 
 }
