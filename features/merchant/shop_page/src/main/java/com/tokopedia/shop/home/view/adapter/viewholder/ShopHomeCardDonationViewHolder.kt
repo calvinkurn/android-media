95a61c60abf284eb14feec3b4af8d18064e4a9cb
Muhammad Furqan <furqan@tokopedia.com>
2022-02-07 05:50:02 +0700

    [Shop Page][Donation] Fix Donation Card

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeCardDonationViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeCardDonationViewHolder.kt
@@ -7,69 +7,65 @@ import android.text.TextUtils
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import android.text.style.StyleSpan
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.media.loader.loadImage
 import com.tokopedia.shop.R
-import com.tokopedia.shop.databinding.ItemShopHomeCardDonationBinding
 import com.tokopedia.shop.home.view.listener.ShopHomeCardDonationListener
 import com.tokopedia.shop.home.view.model.ShopHomeCardDonationUiModel
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifyprinciples.Typography
 
 class ShopHomeCardDonationViewHolder(
     itemView: View,
     private val listener: ShopHomeCardDonationListener
 ) : AbstractViewHolder<ShopHomeCardDonationUiModel>(itemView) {
 
-    val binding = ItemShopHomeCardDonationBinding.inflate(
-        LayoutInflater.from(itemView.context),
-        itemView as ViewGroup,
-        false
-    )
+    private val shopHomeCardDonationImage: ImageUnify =
+        itemView.findViewById(R.id.shop_home_card_donation_image)
+    private val shopHomeCardDonationDescription: Typography =
+        itemView.findViewById(R.id.shop_home_card_donation_description)
 
     override fun bind(element: ShopHomeCardDonationUiModel) {
-        with(binding) {
-            // render image
-            shopHomeCardDonationImage.loadImage(element.header.cover)
+        // render image
+        shopHomeCardDonationImage.loadImage(element.header.cover)
 
-            val ctaDescription = SpannableString(element.header.ctaText)
-            // set cta color to green
-            ctaDescription.setSpan(
-                ForegroundColorSpan(
-                    MethodChecker.getColor(
-                        root.context,
-                        com.tokopedia.unifyprinciples.R.color.Unify_GN500
-                    )
-                ),
-                0,
-                ctaDescription.length,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            // set cta typeface to bold
-            ctaDescription.setSpan(
-                StyleSpan(Typeface.BOLD),
-                0,
-                ctaDescription.length,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            // set cta action
-            ctaDescription.setSpan(
-                object : ClickableSpan() {
-                    override fun onClick(widget: View) {
-                        listener.onCardDonationClick(element.header.ctaLink)
-                    }
-                },
-                0,
-                ctaDescription.length,
-                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
-            // render description
-            shopHomeCardDonationDescription.text =
-                TextUtils.concat(element.header.title, " ", ctaDescription)
-        }
+        val ctaDescription = SpannableString(element.header.ctaText)
+        // set cta color to green
+        ctaDescription.setSpan(
+            ForegroundColorSpan(
+                MethodChecker.getColor(
+                    itemView.context,
+                    com.tokopedia.unifyprinciples.R.color.Unify_GN500
+                )
+            ),
+            0,
+            ctaDescription.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        // set cta typeface to bold
+        ctaDescription.setSpan(
+            StyleSpan(Typeface.BOLD),
+            0,
+            ctaDescription.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        // set cta action
+        ctaDescription.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    listener.onCardDonationClick(element.header.ctaLink)
+                }
+            },
+            0,
+            ctaDescription.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        // render description
+        shopHomeCardDonationDescription.text =
+            TextUtils.concat(element.header.title, " ", ctaDescription)
     }
 
     companion object {
