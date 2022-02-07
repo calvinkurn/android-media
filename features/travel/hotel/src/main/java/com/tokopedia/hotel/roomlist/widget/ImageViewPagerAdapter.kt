0b56a9e4e127ec0ece8f23b8311f43a5a5064f53
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-02-07 23:10:20 +0800

    adjust size image placeholder

--- features/travel/hotel/src/main/java/com/tokopedia/hotel/roomlist/widget/ImageViewPagerAdapter.kt
+++ features/travel/hotel/src/main/java/com/tokopedia/hotel/roomlist/widget/ImageViewPagerAdapter.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.hotel.R
-import com.tokopedia.media.loader.data.PLACEHOLDER_RES_UNIFY
 import com.tokopedia.media.loader.loadImage
 
 /**
@@ -25,7 +24,7 @@ class ImageViewPagerAdapter(private val images: MutableList<String>, private val
             if (clickListener != null) holder.bannerImage.setOnClickListener { clickListener.onImageClicked(position) }
         }
         holder.bannerImage.loadImage(images[position]){
-            setPlaceHolder(PLACEHOLDER_RES_UNIFY)
+            setPlaceHolder(R.drawable.ic_hotel_loading_image)
         }
     }
 
