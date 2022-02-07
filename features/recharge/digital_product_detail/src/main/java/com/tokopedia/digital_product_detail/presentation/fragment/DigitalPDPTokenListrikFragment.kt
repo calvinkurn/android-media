d9fee7fc0da448a06f17d92ff4f755a2fffd5ac0
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-02-07 14:41:45 +0700

    [Recharge][PDP Revamp][Token Listrik] Remove dynamic spacer setup

--- features/recharge/digital_product_detail/src/main/java/com/tokopedia/digital_product_detail/presentation/fragment/DigitalPDPTokenListrikFragment.kt
+++ features/recharge/digital_product_detail/src/main/java/com/tokopedia/digital_product_detail/presentation/fragment/DigitalPDPTokenListrikFragment.kt
@@ -46,13 +46,11 @@ import com.tokopedia.digital_product_detail.di.DigitalPDPComponent
 import com.tokopedia.digital_product_detail.presentation.bottomsheet.SummaryTelcoBottomSheet
 import com.tokopedia.digital_product_detail.presentation.utils.DigitalPDPTelcoAnalytics
 import com.tokopedia.digital_product_detail.presentation.utils.DigitalPDPCategoryUtil
-import com.tokopedia.digital_product_detail.presentation.utils.setupDynamicAppBar
 import com.tokopedia.digital_product_detail.presentation.viewmodel.DigitalPDPTokenListrikViewModel
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.isLessThanZero
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.kotlin.extensions.view.show
-import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.network.utils.ErrorHandler
 import com.tokopedia.recharge_component.listener.RechargeBuyWidgetListener
 import com.tokopedia.recharge_component.listener.RechargeDenomGridListener
@@ -97,7 +95,6 @@ class DigitalPDPTokenListrikFragment: BaseDaggerFragment(),
 
     private var binding by autoClearedNullable<FragmentDigitalPdpTokenListrikBinding>()
 
-    private var dynamicSpacerHeightRes = R.dimen.dynamic_banner_space
     private var loyaltyStatus = ""
     private var clientNumber = ""
     private var productId =  0
@@ -135,7 +132,6 @@ class DigitalPDPTokenListrikFragment: BaseDaggerFragment(),
         getDataFromBundle()
         initClientNumberWidget()
         initEmptyState()
-        setAnimationAppBarLayout()
         observeData()
 
         getCatalogMenuDetail()
@@ -169,43 +165,6 @@ class DigitalPDPTokenListrikFragment: BaseDaggerFragment(),
         binding?.rechargePdpTokenListrikEmptyStateWidget?.setImageUrl(
             "https://images.tokopedia.net/img/ULHhFV/2022/1/7/8324919c-fa15-46d9-84f7-426adb6994e0.jpg"
         )
-    }
-
-    private fun setAnimationAppBarLayout() {
-        binding?.run {
-            rechargePdpTokenListrikAppbar.setupDynamicAppBar(
-                { !viewModel.isEligibleToBuy },
-                { rechargePdpTokenListrikClientNumberWidget.getInputNumber().isEmpty() },
-                { onCollapseAppBar() },
-                { onExpandAppBar() }
-            )
-        }
-    }
-
-    private fun showDynamicSpacer() {
-        binding?.rechargePdpTokenListrikDynamicBannerSpacer?.layoutParams?.height =
-            context?.resources?.getDimensionPixelSize(dynamicSpacerHeightRes)
-                ?: DigitalPDPConstant.DEFAULT_SPACE_HEIGHT
-        binding?.rechargePdpTokenListrikDynamicBannerSpacer?.requestLayout()
-    }
-
-    private fun hideDynamicSpacer() {
-        binding?.rechargePdpTokenListrikDynamicBannerSpacer?.layoutParams?.height = 0
-        binding?.rechargePdpTokenListrikDynamicBannerSpacer?.requestLayout()
-    }
-
-    private fun onCollapseAppBar() {
-        binding?.run {
-            rechargePdpTokenListrikClientNumberWidget.setVisibleSimplifiedLayout(true)
-            showDynamicSpacer()
-        }
-    }
-
-    private fun onExpandAppBar() {
-        binding?.run {
-            rechargePdpTokenListrikClientNumberWidget.setVisibleSimplifiedLayout(false)
-            hideDynamicSpacer()
-        }
     }
 
     private fun observeData() {
@@ -444,7 +403,6 @@ class DigitalPDPTokenListrikFragment: BaseDaggerFragment(),
                 setFilterChipShimmer(false, favoriteNumber.isEmpty())
                 setFavoriteNumber(favoriteNumber)
                 setAutoCompleteList(favoriteNumber)
-                dynamicSpacerHeightRes = R.dimen.dynamic_banner_space_extended
             }
         }
     }
