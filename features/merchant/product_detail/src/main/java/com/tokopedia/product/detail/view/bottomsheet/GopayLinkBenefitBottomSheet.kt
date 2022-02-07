6cf42fa743e8c3edc5ea11d638ec30769eb0e974
Rohan <rohan.ghosh@tokopedia.com>
2022-02-07 20:04:18 +0530

    refactored some code

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/bottomsheet/GopayLinkBenefitBottomSheet.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/bottomsheet/GopayLinkBenefitBottomSheet.kt
@@ -1,16 +1,16 @@
-package com.tokopedia.pdpsimulation.activteGopay.presentation.bottomsheet
+package com.tokopedia.product.detail.view.bottomsheet
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.FragmentManager
-import com.tokopedia.pdpsimulation.R
+import com.tokopedia.product.detail.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
 
 class GopayLinkBenefitBottomSheet:BottomSheetUnify() {
 
     private var parentView: View? = null
-    private val childLayoutRes = R.layout.pdpwidget_gopay_activation_bottomsheet
+    private val childLayoutRes = R.layout.bottom_sheet_pdp_widget_gopay_activation
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
