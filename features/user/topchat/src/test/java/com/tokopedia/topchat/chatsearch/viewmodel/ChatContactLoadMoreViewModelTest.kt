e096577c645fe11fd19c1aa19121ad22f612a7da
kelvinsaputra <kelvin.saputra@tokopedia.com>
2022-02-08 20:10:08 +0700

    increase unit test coverage for chatlist

--- features/user/topchat/src/test/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatContactLoadMoreViewModelTest.kt
+++ features/user/topchat/src/test/java/com/tokopedia/topchat/chatsearch/viewmodel/ChatContactLoadMoreViewModelTest.kt
@@ -1,4 +1,4 @@
-package com.tokopedia.topchat.chatlist.viewmodel
+package com.tokopedia.topchat.chatsearch.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.topchat.chatsearch.data.ChatSearch
