314485e10b332d06b8cc6ce05582a1fe6c73fd25
faisalramd <faisal.ramdani@tokopedia.com>
2022-02-08 15:26:51 +0700

    implement bottomsheet layout

--- features/merchant/product_service_widget/src/main/java/com/tokopedia/gifting/presentation/bottomsheet/GiftingBottomSheet.kt
+++ features/merchant/product_service_widget/src/main/java/com/tokopedia/gifting/presentation/bottomsheet/GiftingBottomSheet.kt
@@ -1,10 +1,13 @@
 package com.tokopedia.gifting.presentation.bottomsheet
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.gifting.di.DaggerGiftingComponent
 import com.tokopedia.gifting.presentation.viewmodel.GiftingViewModel
+import com.tokopedia.product_service_widget.R
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import javax.inject.Inject
 
@@ -13,8 +16,10 @@ class GiftingBottomSheet(private val productId: Long) : BottomSheetUnify() {
     @Inject
     lateinit var viewModel: GiftingViewModel
 
-    init {
-        setTitle("werwer $productId")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setTitle("Cara atur bingkisan")
+        initChildLayout()
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,5 +33,11 @@ class GiftingBottomSheet(private val productId: Long) : BottomSheetUnify() {
             .baseAppComponent((requireActivity().applicationContext as BaseMainApplication).baseAppComponent)
             .build()
             .inject(this)
+    }
+
+    private fun initChildLayout() {
+        overlayClickDismiss = true
+        val contentView: View? = View.inflate(context, R.layout.bottomsheet_gifting, null)
+        setChild(contentView)
     }
 }
