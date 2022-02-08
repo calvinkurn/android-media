27af6ef1eb1d6f0d95577e31ec571e11b211f699
kamal <kamal.majhi@tokopedia.com>
2022-02-08 13:56:46 +0530

    add membershipCardId

--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/data/mapper/TokomemberMapper.kt
+++ features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/data/mapper/TokomemberMapper.kt
@@ -1,8 +1,6 @@
 package com.tokopedia.thankyou_native.data.mapper
 
-import com.tokopedia.thankyou_native.presentation.adapter.model.GyroTokomemberItem
-import com.tokopedia.thankyou_native.presentation.adapter.model.TokoMemberRequestParam
-import com.tokopedia.thankyou_native.presentation.adapter.model.TokomemberModel
+import com.tokopedia.thankyou_native.presentation.adapter.model.*
 import com.tokopedia.tokomember.model.BottomSheetContentItem
 import com.tokopedia.tokomember.model.MembershipGetShopRegistrationWidget
 import com.tokopedia.tokomember.model.WidgetContentItem
@@ -13,10 +11,11 @@ object TokomemberMapper {
         tokomemberRequestParam: TokoMemberRequestParam
     ): TokomemberModel {
         val membershipType = shopRegisterResponse?.membershipType ?: 0
-        val waitingWidgetData = shopRegisterResponse?.widgetContent?.get(0) ?: WidgetContentItem()
-        val instantWidgetData = shopRegisterResponse?.widgetContent?.get(1) ?: WidgetContentItem()
-        val successWidgetData = shopRegisterResponse?.widgetContent?.get(2) ?: WidgetContentItem()
-        val bottomSheetContentItem = shopRegisterResponse?.bottomSheetContent?.get(0) ?: BottomSheetContentItem()
+        val cardId = shopRegisterResponse?.cardID ?: 0
+        val waitingWidgetData = shopRegisterResponse?.widgetContent?.get(TOKOMEMBER_WAITING_WIDGET) ?: WidgetContentItem()
+        val instantWidgetData = shopRegisterResponse?.widgetContent?.get(TOKOMEMBER_INSTANT_WIDGET) ?: WidgetContentItem()
+        val successWidgetData = shopRegisterResponse?.widgetContent?.get(TOKOMEMBER_SUCCESS_WIDGET) ?: WidgetContentItem()
+        val bottomSheetContentItem = shopRegisterResponse?.bottomSheetContent?.get(TOKOMEMBER_SUCCESS_BOTTOMSHEET) ?: BottomSheetContentItem()
 
         bottomSheetContentItem.source = tokomemberRequestParam.source
         bottomSheetContentItem.shopID = tokomemberRequestParam.shopID
@@ -29,16 +28,19 @@ object TokomemberMapper {
                 getWidgetData(
                     waitingWidgetData,
                     membershipType,
-                    bottomSheetContentItem
+                    bottomSheetContentItem,
+                    cardId
                 ),
                 getWidgetData(
                     instantWidgetData,
                     membershipType,
-                    bottomSheetContentItem
+                    bottomSheetContentItem,
+                    cardId
                 ),
                 getWidgetData(
                     successWidgetData,
-                    membershipType
+                    membershipType,
+                    cardId = cardId
                 )
             )
         )
@@ -47,7 +49,8 @@ object TokomemberMapper {
     private fun getWidgetData(
         widgetContentItem: WidgetContentItem,
         membershipType: Int,
-        bottomSheetContentItem: BottomSheetContentItem = BottomSheetContentItem()
+        bottomSheetContentItem: BottomSheetContentItem = BottomSheetContentItem(),
+        cardId: Int
     ): GyroTokomemberItem {
         return GyroTokomemberItem(
             title = widgetContentItem.title,
@@ -59,7 +62,8 @@ object TokomemberMapper {
             urlApp = widgetContentItem.appLink,
             usecase = widgetContentItem.usecase,
             membershipType = membershipType,
-            listOfBottomSheetContent = bottomSheetContentItem
+            listOfBottomSheetContent = bottomSheetContentItem,
+            membershipCardId = cardId.toString()
         )
     }
 }
