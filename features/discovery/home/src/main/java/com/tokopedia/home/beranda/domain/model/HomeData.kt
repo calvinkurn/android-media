cf00ff558e71b458a14b8b0f73c97196441bd469
devarafikry <devara.akmal@tokopedia.com>
2022-02-09 14:29:37 +0700

    add expose to homedata field increase database version

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/model/HomeData.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/model/HomeData.kt
@@ -1,23 +1,34 @@
 package com.tokopedia.home.beranda.domain.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.home.beranda.data.model.HomeAtfData
 import com.tokopedia.home.beranda.domain.model.banner.BannerDataModel
 
 data class HomeData(
     @SerializedName("dynamicHomeChannel")
+    @Expose
     var dynamicHomeChannel: DynamicHomeChannel = DynamicHomeChannel(),
     @SerializedName("slides")
+    @Expose
     var banner: BannerDataModel = BannerDataModel(),
     @SerializedName("ticker")
+    @Expose
     var ticker: Ticker = Ticker(),
     @SerializedName("dynamicHomeIcon")
+    @Expose
     var dynamicHomeIcon: DynamicHomeIcon = DynamicHomeIcon(),
     @SerializedName("spotlight")
+    @Expose
     val spotlight: Spotlight = Spotlight(),
     @SerializedName("homeFlag")
+    @Expose
     var homeFlag: HomeFlag = HomeFlag(),
+    @SerializedName("token")
+    @Expose
     var token: String = "",
+    @SerializedName("homeAtfData")
+    @Expose
     var atfData: HomeAtfData? = HomeAtfData(),
     /**
      * is processing dynamic channel means home still getting dynamic channel from repository
@@ -26,5 +37,7 @@ data class HomeData(
      * Note that dynamic channel is different with ATF data
      *
      */
+    @SerializedName("isProcessingDynamicChannel")
+    @Expose
     var isProcessingDynamicChannel: Boolean = false
 )
