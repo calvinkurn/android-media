db251034e5ea2911659499f95d462cc9edc8f92c
kamal <kamal.majhi@tokopedia.com>
2022-02-09 10:34:57 +0530

    multiple ticker

--- features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/tokopointhome/ticker/SectionTickerViewHolder.kt
+++ features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/tokopointhome/ticker/SectionTickerViewHolder.kt
@@ -7,64 +7,65 @@ import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.kotlin.extensions.view.setMargin
 import com.tokopedia.tokopoints.R
+import com.tokopedia.tokopoints.view.model.TickerMetadata
 import com.tokopedia.tokopoints.view.model.section.SectionContent
 import com.tokopedia.tokopoints.view.util.AnalyticsTrackerUtil
 import com.tokopedia.tokopoints.view.util.CommonConstant
 import com.tokopedia.unifycomponents.ticker.Ticker
-import com.tokopedia.unifycomponents.ticker.TickerCallback
+import com.tokopedia.unifycomponents.ticker.TickerData
+import com.tokopedia.unifycomponents.ticker.TickerPagerAdapter
+import com.tokopedia.unifycomponents.ticker.TickerPagerCallback
 
 class SectionTickerViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun bind(content: SectionContent) {
 
         val tickerContainer = view.findViewById<View>(R.id.cons_ticker_container)
-
-        if (view == null || content == null || content.layoutTickerAttr == null || content.layoutTickerAttr.tickerList == null || content.layoutTickerAttr.tickerList.isEmpty()) {
+        val pager: Ticker? = view.findViewById(R.id.ticker_new)
+        val tickerDataList= arrayListOf<TickerData>()
+        var link = ""
+        var desc = ""
+        var linkDesc = ""
+        if (content.layoutTickerAttr == null || content.layoutTickerAttr.tickerList == null || content.layoutTickerAttr.tickerList.isEmpty()) {
             tickerContainer.visibility = View.GONE
             return
         }
 
-        val pager: Ticker? = view?.findViewById(R.id.ticker_new)
-        var link = ""
-        var desc = ""
-        var linkDesc = ""
         if (!content.layoutTickerAttr.tickerList.isNullOrEmpty()) {
-            for (tickerItem in content.layoutTickerAttr.tickerList[0].metadata) {
-                link = if (tickerItem.link[CommonConstant.TickerMapKeys.APP_LINK]?.length != 0) {
-                    tickerItem.link[CommonConstant.TickerMapKeys.APP_LINK].toString()
-                } else {
-                    tickerItem.link[CommonConstant.TickerMapKeys.URL].toString()
-                }
-
-                if (link.isNotEmpty()) {
-                    linkDesc = tickerItem.text[CommonConstant.TickerMapKeys.CONTENT].toString()
-                } else {
-                    desc = tickerItem.text[CommonConstant.TickerMapKeys.CONTENT].toString()
+            content.layoutTickerAttr.tickerList?.let { tickerData ->
+                tickerData.forEachIndexed { index, tickerContainer ->
+                   val tickerItem =  tickerContainer.metadata?.get(0)?: TickerMetadata()
+                    link = if (tickerItem.link[CommonConstant.TickerMapKeys.APP_LINK]?.length != 0) {
+                        tickerItem.link[CommonConstant.TickerMapKeys.APP_LINK].toString()
+                    } else {
+                        tickerItem.link[CommonConstant.TickerMapKeys.URL].toString()
+                    }
+                    if (link.isNotEmpty()) {
+                        linkDesc = tickerItem.text[CommonConstant.TickerMapKeys.CONTENT].toString()
+                    } else {
+                        desc = tickerItem.text[CommonConstant.TickerMapKeys.CONTENT].toString()
+                    }
+                    val descriptionText = desc + "<a href=\"${link}\">" + ". " + linkDesc + "</a>"
+                    tickerDataList.add(index,TickerData(descriptionText,Ticker.TYPE_ANNOUNCEMENT))
                 }
             }
         }
-        val descriptionText = desc + "<a href=\"${link}\">" + ". " + linkDesc + "</a>"
-        pager?.setHtmlDescription(descriptionText)
+        val tickerViewPager = TickerPagerAdapter(view.context, tickerDataList)
+        pager?.addPagerView(tickerViewPager,tickerDataList)
         pager?.findViewById<TextView>(com.tokopedia.unifycomponents.R.id.ticker_description)?.setMargin(16, 8, 22, 8)
-        pager?.setDescriptionClickEvent(object : TickerCallback {
-            override fun onDescriptionViewClick(linkUrl: CharSequence) {
+        tickerViewPager.setPagerDescriptionClickEvent(object : TickerPagerCallback {
+            override fun onPageDescriptionViewClick(linkUrl: CharSequence, itemData: Any?) {
                 if (link.startsWith(CommonConstant.TickerMapKeys.TOKOPEDIA)) {
                     RouteManager.route(view.context, link)
                 } else {
                     RouteManager.route(view.context, String.format(view.context.resources.getString(R.string.tp_webview_format), ApplinkConst.WEBVIEW, link))
                 }
                 AnalyticsTrackerUtil.sendEvent(view.context,
-                        AnalyticsTrackerUtil.EventKeys.EVENT_TOKOPOINT,
-                        AnalyticsTrackerUtil.CategoryKeys.TOKOPOINTS,
-                        AnalyticsTrackerUtil.ActionKeys.CLICK_TICKER,
-                        "$desc $linkDesc")
-            }
-
-            override fun onDismiss() {
+                    AnalyticsTrackerUtil.EventKeys.EVENT_TOKOPOINT,
+                    AnalyticsTrackerUtil.CategoryKeys.TOKOPOINTS,
+                    AnalyticsTrackerUtil.ActionKeys.CLICK_TICKER,
+                    "$desc $linkDesc")
             }
         })
-
         tickerContainer.visibility = View.VISIBLE
-
     }
-
 }
