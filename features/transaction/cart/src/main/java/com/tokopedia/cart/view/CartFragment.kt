9797129c8e572100f8066c1edbbda2b0e45f8b8c
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-02-10 14:12:38 +0700

    comment temporary add on navigation

--- features/transaction/cart/src/main/java/com/tokopedia/cart/view/CartFragment.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/view/CartFragment.kt
@@ -1708,7 +1708,7 @@ class CartFragment : BaseCheckoutFragment(), ICartListView, ActionListener, Cart
             binding?.goToCourierPageButton?.isEnabled = true
             binding?.goToCourierPageButton?.setOnClickListener { checkGoToShipment("") }
         }
-        tempInitializeAddOnNavigation()
+//        tempInitializeAddOnNavigation()
     }
 
     override fun onCartDataDisableToCheckout() {
