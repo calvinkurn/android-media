bc3b6d478ca7bef06d8523c61551b4f5aaec2710
kelvinsaputra <kelvin.saputra@tokopedia.com>
2022-02-11 17:51:05 +0700

    instrument test MVC lock to product

--- features/user/attachvoucher/src/androidTest/java/com/tokopedia/attachvoucher/test/AttachVoucherListTest.kt
+++ features/user/attachvoucher/src/androidTest/java/com/tokopedia/attachvoucher/test/AttachVoucherListTest.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import com.tokopedia.attachvoucher.common.matchers.withRecyclerView
 import com.tokopedia.attachvoucher.data.voucherv2.GetMerchantPromotionGetMVListResponse
 import com.tokopedia.attachvoucher.test.base.AttachVoucherTest
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 class AttachVoucherListTest: AttachVoucherTest() {
@@ -92,17 +93,32 @@ class AttachVoucherListTest: AttachVoucherTest() {
     }
 
     @Test
-    fun should_show_additional_text_for_private_voucher() {
+    fun should_not_show_additional_text_for_non_product_voucher() {
         //Given
         getVoucherUseCase.response = successGetMerchantPromotionGetMVListResponse
         launchAttachVoucherActivity()
 
         //When
-        scrollListToPosition(1)
+        scrollListToPosition(0)
 
         //Then
         onView(withRecyclerView(R.id.recycler_view)
-            .atPositionOnView(1, com.tokopedia.merchantvoucher.R.id.tvVoucherDesc))
+            .atPositionOnView(0, com.tokopedia.merchantvoucher.R.id.tvVoucherDesc))
+            .check(matches(not(withSubstring("untuk produk tertentu"))))
+    }
+
+    @Test
+    fun should_show_additional_text_for_product_voucher() {
+        //Given
+        getVoucherUseCase.response = successGetMerchantPromotionGetMVListResponse
+        launchAttachVoucherActivity()
+
+        //When
+        scrollListToPosition(2)
+
+        //Then
+        onView(withRecyclerView(R.id.recycler_view)
+            .atPositionOnView(2, com.tokopedia.merchantvoucher.R.id.tvVoucherDesc))
             .check(matches(withSubstring("untuk produk tertentu")))
     }
 }
--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSellerAttachVoucherTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/TopchatRoomSellerAttachVoucherTest.kt
@@ -19,6 +19,7 @@ import com.tokopedia.topchat.chatroom.view.activity.base.BaseSellerTopchatRoomTe
 import com.tokopedia.topchat.chatroom.view.activity.robot.general.GeneralResult
 import com.tokopedia.topchat.chatroom.view.activity.robot.general.GeneralRobot
 import com.tokopedia.topchat.matchers.withRecyclerView
+import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 class TopchatRoomSellerAttachVoucherTest: BaseSellerTopchatRoomTest() {
@@ -42,7 +43,22 @@ class TopchatRoomSellerAttachVoucherTest: BaseSellerTopchatRoomTest() {
     }
 
     @Test
-    fun should_show_additional_text_in_private_voucher() {
+    fun should_show_additional_text_in_lock_to_product_voucher() {
+        // Given
+        getChatUseCase.response = getChatUseCase.voucherAttachmentChatWithSellerResponse
+        launchChatRoomActivity(isSellerApp = true)
+
+        //When
+        GeneralRobot.doScrollChatToPosition(0)
+
+        // Then
+        onView(withRecyclerView(R.id.recycler_view_chatroom)
+            .atPositionOnView(1, com.tokopedia.merchantvoucher.R.id.tvVoucherDesc))
+            .check(matches(withSubstring("untuk produk tertentu")))
+    }
+
+    @Test
+    fun should_not_show_additional_text_in_regular_voucher() {
         // Given
         getChatUseCase.response = getChatUseCase.voucherAttachmentChatWithSellerResponse
         launchChatRoomActivity(isSellerApp = true)
@@ -53,14 +69,14 @@ class TopchatRoomSellerAttachVoucherTest: BaseSellerTopchatRoomTest() {
         // Then
         onView(withRecyclerView(R.id.recycler_view_chatroom)
             .atPositionOnView(0, com.tokopedia.merchantvoucher.R.id.tvVoucherDesc))
-            .check(matches(withSubstring("untuk produk tertentu")))
+            .check(matches(not(withSubstring("untuk produk tertentu"))))
     }
 
     @Test
     fun should_show_additional_text_in_preview_private_voucher() {
         // Given
         getChatUseCase.response = getChatUseCase.voucherAttachmentChatWithSellerResponse
-        val voucherData = "{\"amount\":20000,\"amountType\":1,\"desktopUrl\":\"https://images.tokopedia.net/img/BTJGre/2022/1/25/a5dad590-e6d3-466b-9c8f-6ea81ad1632d.jpg\",\"identifier\":\"1\",\"isPublic\":0,\"minimumSpend\":100000,\"mobileUrl\":\"https://images.tokopedia.net/img/BTJGre/2022/1/25/a5dad590-e6d3-466b-9c8f-6ea81ad1632d.jpg\",\"source\":\"inbox\",\"tnc\":\"\\u003col\\u003e\\u003cli\\u003eVoucher Gratis Ongkir dapat digunakan dengan membuka halaman promo di Keranjang.\\u003c/li\\u003e\\u003cli\\u003eVoucher Gratis Ongkir hanya berlaku apabila pembelian Pengguna sudah memenuhi syarat dan ketentuan yang tertera pada voucher.\\u003c/li\\u003e\\u003cli\\u003eNominal Gratis Ongkir yang bisa didapatkan sebesar Rp 20.000.\\u003c/li\\u003e\\u003cli\\u003eVoucher Gratis Ongkir berlaku untuk transaksi minimal Rp 100.000 (tidak termasuk ongkos kirim dan biaya tambahan lainnya).\\u003c/li\\u003e\\u003cli\\u003eVoucher Gratis Ongkir hanya berlaku untuk pembelanjaan di 10973651br04dc4st-s4tu.\\u003c/li\\u003e\\u003cli\\u003e1 (satu) Pengguna Tokopedia hanya boleh menggunakan 1 (satu) akun Tokopedia untuk menggunakan Voucher Gratis Ongkir ini.\\u003c/li\\u003e\\u003cli\\u003ePromo tidak berlaku untuk produk Logam Mulia, Emas Batangan, Voucher, dan Paket Data.\\u003c/li\\u003e\\u003cli\\u003eTokopedia berhak melakukan tindakan yang diperlukan apabila diduga terjadi tindakan kecurangan yang dilakukan oleh Pengguna dan/atau melanggar Syarat dan Ketentuan Situs dan/atau merugikan pihak Tokopedia.\\u003c/li\\u003e\\u003cli\\u003eDengan menggunakan Voucher Gratis Ongkir, Pengguna dianggap telah memahami dan menyetujui semua Syarat dan Ketentuan yang berlaku.\\u003c/li\\u003e\\u003c/ol\\u003e\",\"validThru\":1645576200,\"voucherCode\":\"1097122921\",\"voucherId\":7297979,\"voucherName\":\"gratis ongkir gratis ongkir gr\",\"voucherType\":1}"
+        val voucherData = "{\"amount\":20000,\"amountType\":1,\"desktopUrl\":\"https://images.tokopedia.net/img/BTJGre/2022/1/25/a5dad590-e6d3-466b-9c8f-6ea81ad1632d.jpg\",\"identifier\":\"1\",\"isPublic\":0,\"minimumSpend\":100000,\"mobileUrl\":\"https://images.tokopedia.net/img/BTJGre/2022/1/25/a5dad590-e6d3-466b-9c8f-6ea81ad1632d.jpg\",\"source\":\"inbox\",\"tnc\":\"tnc panjang\",\"validThru\":1645576200,\"voucherCode\":\"1097122921\",\"voucherId\":7297979,\"voucherName\":\"gratis ongkir gratis ongkir gr\",\"voucherType\":1, \"isLockToProduct\":1}"
         val resultIntent = Intent().apply {
             this.putExtra(ApplinkConst.AttachVoucher.PARAM_VOUCHER_PREVIEW, voucherData)
         }
