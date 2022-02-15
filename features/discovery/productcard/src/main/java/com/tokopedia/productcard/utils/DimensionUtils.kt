f23295c5ecd72054f12535c31e94d504e8653630
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2022-02-15 16:21:44 +0700

    limit util visibility

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/DimensionUtils.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/utils/DimensionUtils.kt
@@ -2,7 +2,7 @@ package com.tokopedia.productcard.utils
 
 import android.content.Context
 
-object DimensionUtils {
+internal object DimensionUtils {
     fun getDensityMatrix(context: Context): Float {
         return context.resources.displayMetrics.density
     }
