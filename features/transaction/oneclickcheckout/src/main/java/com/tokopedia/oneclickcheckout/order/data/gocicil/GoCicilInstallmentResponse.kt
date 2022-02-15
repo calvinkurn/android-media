39124fed2d247c47c7715dbfa245da65afeef63f
hansenpw <hansen.wijaya@tokopedia.com>
2022-02-15 08:17:09 +0700

    add installment option response

---
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/data/gocicil/GoCicilInstallmentResponse.kt
@@ -0,0 +1,48 @@
+package com.tokopedia.oneclickcheckout.order.data.gocicil
+
+import com.google.gson.annotations.SerializedName
+
+class GoCicilInstallmentGqlResponse(
+        @SerializedName("getInstallmentInfo")
+        val response: GoCicilInstallmentResponse = GoCicilInstallmentResponse()
+)
+
+class GoCicilInstallmentResponse(
+        @SerializedName("success")
+        val success: Boolean = false,
+        @SerializedName("installment_options")
+        val installmentOptions: List<GoCicilInstallmentOption> = emptyList(),
+)
+
+class GoCicilInstallmentOption(
+        @SerializedName("installment_term")
+        val installmentTerm: Int = 0,
+        @SerializedName("option_id")
+        val optionId: String = "",
+        @SerializedName("installment_period")
+        val installmentPeriod: String = "",
+        @SerializedName("first_installment_time")
+        val firstInstallmentTime: String = "",
+        @SerializedName("est_installment_end")
+        val estInstallmentEnd: String = "",
+        @SerializedName("first_due_message")
+        val firstDueMessage: String = "",
+        @SerializedName("principal_amount")
+        val principalAmount: Double = 0.0,
+        @SerializedName("interest_amount")
+        val interestAmount: Double = 0.0,
+        @SerializedName("fee_amount")
+        val feeAmount: Double = 0.0,
+        @SerializedName("total_amount")
+        val totalAmount: Double = 0.0,
+        @SerializedName("installment_amount_per_period")
+        val installmentAmountPerPeriod: Double = 0.0,
+        @SerializedName("label_type")
+        val labelType: String = "",
+        @SerializedName("label_message")
+        val labelMessage: String = "",
+        @SerializedName("is_active")
+        val isActive: Boolean = false,
+        @SerializedName("description")
+        val description: String = "",
+)
--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/domain/GoCicilInstallmentOptionUseCase.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/domain/GoCicilInstallmentOptionUseCase.kt
@@ -3,12 +3,9 @@ package com.tokopedia.oneclickcheckout.order.domain
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.oneclickcheckout.order.data.creditcard.CreditCardTenorList
 import com.tokopedia.oneclickcheckout.order.data.creditcard.CreditCardTenorListRequest
-import com.tokopedia.oneclickcheckout.order.data.creditcard.TenorListItem
-import com.tokopedia.oneclickcheckout.order.view.model.CreditCardTenorListData
+import com.tokopedia.oneclickcheckout.order.data.gocicil.GoCicilInstallmentOption
 import com.tokopedia.oneclickcheckout.order.view.model.OrderPaymentGoCicilTerms
-import com.tokopedia.oneclickcheckout.order.view.model.TenorListData
 import kotlinx.coroutines.delay
 import javax.inject.Inject
 
@@ -30,51 +27,49 @@ class GoCicilInstallmentOptionUseCase @Inject constructor(@ApplicationContext pr
         return mapOf(INPUT to input)
     }
 
-    private fun mapCreditCardTenorListData(ccTenorListGqlResponse: CreditCardTenorList): CreditCardTenorListData {
-        return CreditCardTenorListData(
-                processTime = ccTenorListGqlResponse.processTime,
-                errorCode = ccTenorListGqlResponse.errorCode,
-                errorMsg = ccTenorListGqlResponse.errorMsg,
-                tenorList = mapTenorList(ccTenorListGqlResponse.tenorList)
-        )
-
-    }
-
-    private fun mapTenorList(listTenorGql: List<TenorListItem>): List<TenorListData> {
-        val arrayListTenor = arrayListOf<TenorListData>()
-        listTenorGql.forEach {
-            val tenor = TenorListData(
-                    type = it.type,
-                    bank = it.bank,
-                    desc = it.description,
-                    amount = it.amount,
-                    fee = it.fee,
-                    rate = it.rate,
-                    disable = it.disabled
+    private fun mapInstallmentOptions(installmentOptions: List<GoCicilInstallmentOption>): List<OrderPaymentGoCicilTerms> {
+        return installmentOptions.map {
+            OrderPaymentGoCicilTerms(
+                    installmentTerm = it.installmentTerm,
+                    optionId = it.optionId,
+                    firstInstallmentDate = it.firstInstallmentTime,
+                    lastInstallmentDate = it.estInstallmentEnd,
+                    firstDueMessage = it.firstDueMessage,
+                    interestAmount = it.interestAmount,
+                    feeAmount = it.feeAmount,
+                    installmentAmountPerPeriod = it.installmentAmountPerPeriod,
+                    labelType = it.labelType,
+                    labelMessage = it.labelMessage,
+                    isActive = it.isActive,
+                    description = it.description
             )
-            arrayListTenor.add(tenor)
         }
-        return arrayListTenor
     }
 
     companion object {
-        const val INPUT = "input"
+        private const val INPUT = "input"
 
-        const val GoCicilInstallmentOptionQuery = "GoCicilInstallmentOptionQuery"
-        const val QUERY = """
-            mutation creditCardTenorList(${'$'}input: CreditCardTenorListRequest!) {
-            creditCardTenorList(input:${'$'}input) {
-                process_time
-                error_code
-                error_msg
-                tenor_list {
-                    type
-                    bank
-                    description
-                    amount
-                    fee
-                    rate
-                    disabled
+        private const val GoCicilInstallmentOptionQuery = "GoCicilInstallmentOptionQuery"
+        private const val QUERY = """
+            query getInstallmentInfo(${'$'}gatewayCode: String!, ${'$'}merchantCode: String!, ${'$'}profileCode: String!, ${'$'}userDefinedValue: String!, ${'$'}paymentAmount: Float!, ${'$'}signature: String, ${'$'}orderMetadata: String) {
+                getInstallmentInfo(gatewayCode: "PEMUDACICIL", merchantCode: "tokopedia", profileCode: "TKPD_DEFAULT", userDefinedValue: "%7B%22user_id%22%3A8970876%2C%22vcc_id%22%3A0%2C%22vcc_user_id%22%3A0%2C%22is_instant%22%3Afalse%2C%22device%22%3A1%2C%22msisdn_verified%22%3A621234567890%2C%22va_code%22%3A%22081234567890%22%2C%22is_qa_account%22%3A%220%22%2C%22callback_url%22%3A%22%22%2C%22subsidized_amount%22%3A0%2C%22is_stacking%22%3Afalse%2C%22payment_level_additional_detail%22%3A%7B%22total_order_price%22%3A0%2C%22donation_amount%22%3A0%2C%22egold_amount%22%3A0%2C%22final_promo_benefit_discount%22%3A0%2C%22final_promo_benefit_cashback%22%3A0%7D%2C%22is_mitra%22%3Afalse%7D", paymentAmount: 500000, orderMetadata: "%7B%22order_data%22%3A%5B%7B%22order_id%22%3A1%2C%22merchant_type%22%3A%22RM%22%2C%22order_amount%22%3A10000%7D%2C%7B%22order_id%22%3A2%2C%22merchant_type%22%3A%22OS%22%2C%22order_amount%22%3A30000%7D%5D%7D") {
+                    success
+                    installment_options {
+                        installment_term
+                        option_id
+                        installment_period
+                        first_installment_time
+                        est_installment_end
+                        first_due_message
+                        principal_amount
+                        interest_amount
+                        fee_amount
+                        total_amount
+                        installment_amount_per_period
+                        label_type
+                        label_message
+                        is_active
+                        description
                     }
                 }
             }
