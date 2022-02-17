c2cc33869ead81a1a9767e61d8023e0dd73fcb27
Rohan <rohan.ghosh@tokopedia.com>
2022-02-17 14:24:05 +0530

    added a demo activity

---
+++ features/fintech/fintech_home_widget/src/main/java/com/tokopedia/gopayhomewidget/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.gopayhomewidget.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokopedia.gopayhomewidget.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
