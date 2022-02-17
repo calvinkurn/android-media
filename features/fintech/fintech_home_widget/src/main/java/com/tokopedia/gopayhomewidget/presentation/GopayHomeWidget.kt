f17e39ff4c3aeb50ab7e9f45517c0ec5a2dc65e9
Rohan <rohan.ghosh@tokopedia.com>
2022-02-17 11:41:40 +0530

    added a new module and the basic ui
    :wq

---
+++ features/fintech/fintech_home_widget/src/main/java/com/tokopedia/gopayhomewidget/presentation/GopayHomeWidget.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.gopayhomewidget.presentation
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.AttrRes
+import com.tokopedia.gopayhomewidget.R
+import com.tokopedia.unifycomponents.BaseCustomView
+
+class GopayHomeWidget @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0,
+) : BaseCustomView(context, attrs, defStyleAttr) {
+
+    private lateinit var baseView: View
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        baseView = inflate(context, R.layout.layout_gopay_home_widget, this)
+    }
+
+
+}
