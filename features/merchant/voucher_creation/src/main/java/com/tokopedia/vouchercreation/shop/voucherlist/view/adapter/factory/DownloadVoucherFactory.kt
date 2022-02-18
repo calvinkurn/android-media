2830b349dfa07ef083767c4972fa38cb5a5d7a09
Fajar Chaeril Azhar <fajar.azhar@tokopedia.com>
2022-02-18 08:00:24 +0700

    Disable button when no selection of downloaded image

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/shop/voucherlist/view/adapter/factory/DownloadVoucherFactory.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/shop/voucherlist/view/adapter/factory/DownloadVoucherFactory.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.vouchercreation.shop.voucherlist.view.adapter.factory
 
 import com.tokopedia.vouchercreation.common.bottmsheet.downloadvoucher.DownloadVoucherUiModel
-import com.tokopedia.vouchercreation.product.download.CouponImageUiModel
 
 /**
  * Created By @ilhamsuaib on 28/04/20
@@ -10,5 +9,4 @@ import com.tokopedia.vouchercreation.product.download.CouponImageUiModel
 interface DownloadVoucherFactory {
 
     fun type(model: DownloadVoucherUiModel): Int
-    fun type(model: CouponImageUiModel) : Int
 }
