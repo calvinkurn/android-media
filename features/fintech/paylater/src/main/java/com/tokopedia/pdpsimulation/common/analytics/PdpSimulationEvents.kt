15447c2cee7b6a031552024707ae41c59de774b9
chauhan-abhi <chauhanabhi1245@gmail.com>
2022-02-18 12:49:17 +0530

    [Paylater v3] Add linking status in gql and analytics

--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/common/analytics/PdpSimulationEvents.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/common/analytics/PdpSimulationEvents.kt
@@ -1,17 +1,15 @@
 package com.tokopedia.pdpsimulation.common.analytics
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
 open class PayLaterAnalyticsBase {
     var productId: String = ""
     var userStatus: String = ""
     var tenureOption: Int = 0
     var payLaterPartnerName: String = ""
+    var linkingStatus: String = ""
     var action: String = ""
 }
 
-class PayLaterCtaClick: PayLaterAnalyticsBase(){
+class PayLaterCtaClick : PayLaterAnalyticsBase() {
     var emiAmount: String = ""
     var limit: String = ""
     var redirectLink: String = ""
--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/helper/PayLaterBundleGenerator.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/helper/PayLaterBundleGenerator.kt
@@ -1,9 +1,9 @@
 package com.tokopedia.pdpsimulation.paylater.helper
 
 import android.os.Bundle
-import com.tokopedia.pdpsimulation.paylater.domain.model.Cta
-import com.tokopedia.pdpsimulation.paylater.domain.model.Detail
-import com.tokopedia.pdpsimulation.paylater.domain.model.InstallmentDetails
+import com.tokopedia.pdpsimulation.common.analytics.PayLaterAnalyticsBase
+import com.tokopedia.pdpsimulation.common.analytics.PdpSimulationAnalytics
+import com.tokopedia.pdpsimulation.paylater.domain.model.*
 import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterActionStepsBottomSheet
 import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterInstallmentFeeInfo
 import com.tokopedia.pdpsimulation.paylater.presentation.bottomsheet.PayLaterTokopediaGopayBottomsheet
@@ -20,6 +20,25 @@ object PayLaterBundleGenerator {
 
     fun getInstallmentBundle(installmentDetails: InstallmentDetails?) = Bundle().apply {
         putParcelable(PayLaterInstallmentFeeInfo.INSTALLMENT_DETAIL, installmentDetails)
+    }
+
+    fun getPayLaterImpressionEvent(data: ArrayList<SimulationUiModel>, defaultSelectedSimulation:Int): PayLaterAnalyticsBase {
+        val simulationList = data[defaultSelectedSimulation].simulationList
+        val firstDetail: BasePayLaterWidgetUiModel? = simulationList?.getOrNull(0)
+        val (userState,linkStatus) = if(firstDetail is Detail)
+            Pair(firstDetail.userState ?: "", firstDetail.linkingStatus ?: "")
+        else Pair("", "")
+
+        val partnerList = if (data.isNotEmpty()) PayLaterHelper.getProductNameList(simulationList) else ""
+
+        return PayLaterAnalyticsBase().apply {
+            tenureOption = defaultSelectedSimulation
+            action = PdpSimulationAnalytics.IMPRESSION_PAYLATER
+            userStatus = userState
+            payLaterPartnerName = partnerList
+            linkingStatus = linkStatus
+        }
+
     }
 
 }
