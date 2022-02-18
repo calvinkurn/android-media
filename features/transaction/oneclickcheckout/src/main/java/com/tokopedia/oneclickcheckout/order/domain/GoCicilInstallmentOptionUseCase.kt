e53f8fa136b96a666806998b5c81fdb7724abeb4
hansenpw <hansen.wijaya@tokopedia.com>
2022-02-18 11:50:34 +0700

    add response

--- features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/domain/GoCicilInstallmentOptionUseCase.kt
+++ features/transaction/oneclickcheckout/src/main/java/com/tokopedia/oneclickcheckout/order/domain/GoCicilInstallmentOptionUseCase.kt
@@ -56,8 +56,10 @@ class GoCicilInstallmentOptionUseCase @Inject constructor(@ApplicationContext pr
                             first_installment_time
                             est_installment_end
                             first_due_message
+                            principle_amount
                             interest_amount
                             fee_amount
+                            total_amount
                             installment_amount_per_period
                             label_type
                             label_message
