d0c2406c84306aecdb3d022aba9d72c1ab38b564
reivinoktavianus <oktavianusreivin@gmail.com>
2022-02-21 16:48:35 +0700

    Add click product and filter test cases

--- features/merchant/product_manage_list/src/androidTest/java/com/tokopedia/product/manage/cassava/testcase/ProductManageTrackerValidationTest.kt
+++ features/merchant/product_manage_list/src/androidTest/java/com/tokopedia/product/manage/cassava/testcase/ProductManageTrackerValidationTest.kt
@@ -10,6 +10,10 @@ class ProductManageTrackerValidationTest : ProductManageTrackerValidationTestFix
             "tracker/merchant/product_manage_list/list/pml_click_edit_price_button.json"
         private const val CLICK_EDIT_STOCK_BUTTON_PATH =
             "tracker/merchant/product_manage_list/list/pml_click_edit_stock_button.json"
+        private const val CLICK_ON_PRODUCT_PATH =
+            "tracker/merchant/product_manage_list/list/pml_click_on_product.json"
+        private const val CLICK_TAB_FILTER_PATH =
+            "tracker/merchant/product_manage_list/list/pml_click_tab_filter.json"
     }
 
     @Test
@@ -25,6 +29,23 @@ class ProductManageTrackerValidationTest : ProductManageTrackerValidationTestFix
         actionTest {
             clickEditPriceButton(activityRule.activity)
             validate(CLICK_EDIT_PRICE_BUTTON_PATH)
+        }
+    }
+
+    @Test
+    fun validateProductClicked() {
+        actionTest {
+            blockAllIntent()
+            clickProduct()
+            validate(CLICK_ON_PRODUCT_PATH)
+        }
+    }
+
+    @Test
+    fun validateFilterTabClicked() {
+        actionTest {
+            clickFilterTab()
+            validate(CLICK_TAB_FILTER_PATH)
         }
     }
 
--- features/merchant/product_manage_list/src/androidTest/java/com/tokopedia/product/manage/cassava/testcase/ProductManageTrackerValidationTestFixture.kt
+++ features/merchant/product_manage_list/src/androidTest/java/com/tokopedia/product/manage/cassava/testcase/ProductManageTrackerValidationTestFixture.kt
@@ -1,46 +1,19 @@
 package com.tokopedia.product.manage.cassava.testcase
 
-import android.app.Application
 import android.content.Context
 import android.content.Intent
-import android.view.View
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.IdlingResource
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
-import com.tokopedia.applink.ApplinkConst
-import com.tokopedia.applink.RouteManager
-import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
 import com.tokopedia.cassavatest.CassavaTestRule
 import com.tokopedia.cassavatest.hasAllSuccess
-import com.tokopedia.product.manage.R
-import com.tokopedia.product.manage.cassava.robot.ProductManageRobot
-import com.tokopedia.product.manage.cassava.robot.actionTest
-import com.tokopedia.product.manage.feature.cashback.presentation.activity.ProductManageSetCashbackActivity
-import com.tokopedia.product.manage.feature.list.view.activity.ProductManageActivity
-import com.tokopedia.product.manage.feature.list.view.adapter.viewholder.ProductViewHolder
 import com.tokopedia.product.manage.mock.ProductManageMockResponseConfig
-import com.tokopedia.product.manage.stub.common.GraphqlRepositoryStub
-import com.tokopedia.product.manage.stub.common.di.component.BaseAppComponentStubInstance
-import com.tokopedia.product.manage.stub.feature.ProductManageStubInstance
-import com.tokopedia.product.manage.stub.feature.list.domain.usecase.GetProductManageAccessUseCaseStub
 import com.tokopedia.product.manage.stub.feature.list.view.activity.ProductManageActivityStub
-import com.tokopedia.shop.common.domain.interactor.GQLGetProductListUseCase
-import com.tokopedia.test.application.espresso_component.CommonActions
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
-import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
@@ -70,13 +43,13 @@ abstract class ProductManageTrackerValidationTestFixture {
         startActivity()
     }
 
-    protected fun startActivity() {
-        activityRule.launchActivity(getIntent())
-    }
-
     protected fun validate(fileName: String) {
         waitForTrackerSent()
         MatcherAssert.assertThat(cassavaTestRule.validate(fileName), hasAllSuccess())
+    }
+
+    private fun startActivity() {
+        activityRule.launchActivity(getIntent())
     }
 
     private fun getIntent(): Intent {
