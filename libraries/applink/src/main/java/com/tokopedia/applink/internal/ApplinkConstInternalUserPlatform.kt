300e4b6d9caacae7f2da3b0e0ba83418fd31bf79
yorisprayogo <yoris.prayogo@tokopedia.com>
2022-02-22 02:16:40 +0700

    create internal applink

--- libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalUserPlatform.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalUserPlatform.kt
@@ -5,7 +5,11 @@ import com.tokopedia.applink.constant.DeeplinkConstant
 
 object ApplinkConstInternalUserPlatform {
 
+    private const val HOST_USER = "user"
+
     private const val INTERNAL_USER = "${DeeplinkConstant.SCHEME_INTERNAL}://${ApplinkConstInternalGlobal.HOST_GLOBAL}"
+
+    const val NEW_INTERNAL_HOME = "${DeeplinkConstant.SCHEME_INTERNAL}://${HOST_USER}"
 
     const val METHOD_LOGIN_EMAIL = "email"
     const val METHOD_LOGIN_PHONE = "phone"
@@ -18,6 +22,8 @@ object ApplinkConstInternalUserPlatform {
     const val LOGIN_EMAIL = "${LOGIN}?method=$METHOD_LOGIN_EMAIL&e={email}&source={source}"
     const val LOGIN_PHONE = "${LOGIN}?method=$METHOD_LOGIN_PHONE&p={phone}&source={source}"
     const val LOGIN_THIRD_PARTY = "${LOGIN}?method={method}&source={source}"
+
+    const val NEW_PROFILE_INFO = "${NEW_INTERNAL_HOME}/profile-info"
 
     fun getInactivePhoneInternalDeepLink(deepLink: String): String {
         return deepLink.replace(ApplinkConst.CHANGE_INACTIVE_PHONE, ApplinkConstInternalGlobal.CHANGE_INACTIVE_PHONE)
