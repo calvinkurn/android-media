558d5237a2bc90044bf5e726f73eba3de2491898
Fajar Chaeril Azhar <fajar.azhar@tokopedia.com>
2022-02-21 16:03:15 +0700

    Disable free shipping voucher

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/view/fragment/CouponSettingFragment.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/view/fragment/CouponSettingFragment.kt
@@ -482,17 +482,9 @@ class CouponSettingFragment private constructor(): BaseDaggerFragment() {
             }
         }
 
-        binding.chipFreeShipping.selectedChangeListener = { isActive ->
-            if (isActive) {
-                binding.groupFreeShipping.visible()
-            } else {
-                binding.groupFreeShipping.gone()
-            }
-        }
-
         binding.chipCashback.chip_container.setOnClickListener {
             binding.chipCashback.chipType = ChipsUnify.TYPE_SELECTED
-            binding.chipFreeShipping.chipType = ChipsUnify.TYPE_NORMAL
+            binding.chipFreeShipping.chipType = ChipsUnify.TYPE_DISABLE
 
             binding.chipDiscountTypeNominal.chipType = ChipsUnify.TYPE_SELECTED
             binding.chipMinimumPurchaseNominal.chipType = ChipsUnify.TYPE_SELECTED
@@ -504,20 +496,6 @@ class CouponSettingFragment private constructor(): BaseDaggerFragment() {
             viewModel.couponTypeChanged(selectedCouponType)
             calculateMaxExpenseEstimation()
             showCashbackCouponTypeWidget()
-        }
-
-        binding.chipFreeShipping.chip_container.setOnClickListener {
-            binding.chipCashback.chipType = ChipsUnify.TYPE_NORMAL
-            binding.chipFreeShipping.chipType = ChipsUnify.TYPE_SELECTED
-
-            selectedCouponType = CouponType.FREE_SHIPPING
-            selectedDiscountType = DiscountType.NONE
-            selectedMinimumPurchaseType = MinimumPurchaseType.NONE
-
-            viewModel.couponTypeChanged(selectedCouponType)
-
-            calculateMaxExpenseEstimation()
-            showFreeShippingCouponTypeWidget()
         }
     }
 
@@ -877,7 +855,7 @@ class CouponSettingFragment private constructor(): BaseDaggerFragment() {
     private fun displayFreeShippingDiscountTypeData(coupon: CouponSettings) {
         with(binding) {
             showFreeShippingCouponTypeWidget()
-            chipFreeShipping.chipType = ChipsUnify.TYPE_SELECTED
+            chipFreeShipping.chipType = ChipsUnify.TYPE_DISABLE
 
             textAreaFreeShippingDiscountAmount.textAreaInput.setText(coupon.discountAmount.splitByThousand())
             textAreaFreeShippingMinimumPurchase.textAreaInput.setText(coupon.minimumPurchase.splitByThousand())
