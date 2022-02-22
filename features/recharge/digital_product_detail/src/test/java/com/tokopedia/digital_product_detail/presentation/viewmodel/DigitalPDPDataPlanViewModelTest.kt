58401a75b65c4c8b3ed20e5455812b08bd9dc64c
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-02-22 15:12:47 +0700

    [Recharge] update dataFactory usage ini dataplan viewmodel

--- features/recharge/digital_product_detail/src/test/java/com/tokopedia/digital_product_detail/presentation/viewmodel/DigitalPDPDataPlanViewModelTest.kt
+++ features/recharge/digital_product_detail/src/test/java/com/tokopedia/digital_product_detail/presentation/viewmodel/DigitalPDPDataPlanViewModelTest.kt
@@ -1,12 +1,15 @@
 package com.tokopedia.digital_product_detail.presentation.viewmodel
 
 import com.tokopedia.digital_product_detail.data.mapper.DigitalDenomMapper
+import com.tokopedia.digital_product_detail.presentation.data.DataPlanDataFactory
 import com.tokopedia.digital_product_detail.presentation.data.PulsaDataFactory
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 class DigitalPDPDataPlanViewModelTest: DigitalPDPDataPlanViewModelTestFixture() {
 
-    private val dataFactory = PulsaDataFactory()
+    private val dataFactory = DataPlanDataFactory()
     private val mapperFactory = DigitalDenomMapper()
+
+
 }
