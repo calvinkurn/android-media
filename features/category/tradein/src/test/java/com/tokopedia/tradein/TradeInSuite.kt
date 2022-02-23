c2070c165a81e7b2ae08778fa075ca13b8f39ec4
niranjansaxena <niranjan.saxena@tokopedia.com>
2022-02-23 10:45:29 +0530

    test cases

--- features/category/tradein/src/test/java/com/tokopedia/tradein/TradeInSuite.kt
+++ features/category/tradein/src/test/java/com/tokopedia/tradein/TradeInSuite.kt
@@ -1,12 +1,22 @@
 package com.tokopedia.tradein
 
 import com.tokopedia.tradein.usecase.*
+import com.tokopedia.tradein.viewmodel.*
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
+    PromoUseCaseTest::class,
+    TNCInfoUseCaseTest::class,
+    TradeInDetailUseCaseTest::class,
+    TradeInValidateImeiUseCaseTest::class,
+    TradeInHomePageFragmentVMTest::class,
+    TradeInHomePageVMTest::class,
+    TradeInImeiBSViewModelTest::class,
+    TradeInInfoViewModelTest::class,
+    TradeInPromoDetailPageVMTest::class
 )
 class TradeInSuite {
 }
---
+++ features/category/tradein/src/test/java/com/tokopedia/tradein/usecase/PromoUseCaseTest.kt
@@ -0,0 +1,32 @@
+package com.tokopedia.tradein.usecase
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.MockKAnnotations
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+class PromoUseCaseTest {
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        MockKAnnotations.init(this)
+        Dispatchers.setMain(TestCoroutineDispatcher())
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+}
