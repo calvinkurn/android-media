f43c4006df74eca8179ec6080b5ce4746e345740
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-02-23 13:42:54 +0700

    add create interactive unittest

--- features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/interactive/PlayBroInteractiveViewModel.kt
+++ features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/interactive/PlayBroInteractiveViewModel.kt
@@ -5,10 +5,12 @@ import androidx.lifecycle.Observer
 import com.tokopedia.play.broadcaster.domain.repository.PlayBroadcastRepository
 import com.tokopedia.play.broadcaster.model.UiModelBuilder
 import com.tokopedia.play.broadcaster.model.interactive.InteractiveUiModelBuilder
+import com.tokopedia.play.broadcaster.pusher.mediator.PusherMediator
 import com.tokopedia.play.broadcaster.robot.PlayBroadcastViewModelRobot
 import com.tokopedia.play.broadcaster.ui.model.interactive.BroadcastInteractiveCoachMark
 import com.tokopedia.play.broadcaster.ui.model.interactive.BroadcastInteractiveInitState
 import com.tokopedia.play.broadcaster.ui.model.interactive.BroadcastInteractiveState
+import com.tokopedia.play.broadcaster.ui.model.interactive.InteractiveSessionUiModel
 import com.tokopedia.play.broadcaster.util.assertEqualTo
 import com.tokopedia.play.broadcaster.util.assertType
 import com.tokopedia.play.broadcaster.util.getOrAwaitValue
@@ -18,6 +20,7 @@ import com.tokopedia.play_common.model.result.NetworkResult
 import com.tokopedia.unit.test.rule.CoroutineTestRule
 import io.mockk.*
 import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions
 import org.junit.Assert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Rule
@@ -35,6 +38,7 @@ class PlayBroInteractiveViewModel {
 
     private val testDispatcher = rule.dispatchers
 
+    private val mockLivePusher: PusherMediator = mockk(relaxed = true)
     private val mockRepo: PlayBroadcastRepository = mockk(relaxed = true)
     private val mockSharedPref: HydraSharedPreferences = mockk(relaxed = true)
 
@@ -239,51 +243,76 @@ class PlayBroInteractiveViewModel {
         }
     }
 
-//    @Test
-//    fun `when user starts livestreaming, get interactive config and interactive config is active & finished, it should emit interactive state finished`() {
-//
-//        val mockInteractive = interactiveUiModelBuilder.buildLeaderboardInfoModel(
-//            leaderboardWinners = emptyList()
-//        )
-//        val mockCurrentInteractive = interactiveUiModelBuilder.buildCurrentInteractiveModel(
-//            timeStatus = PlayInteractiveTimeStatus.Finished
-//        )
-//
-//        val mockExpectedStateLoading = BroadcastInteractiveState.Allowed.Init(
-//            state = BroadcastInteractiveInitState.Loading
-//        )
-//        val mockExpectedStateCoachMark = BroadcastInteractiveState.Allowed.Init(
-//            state = BroadcastInteractiveInitState.HasPrevious(
-//                coachMark = BroadcastInteractiveCoachMark.NoCoachMark
-//            )
-//        )
-//
-//        val stateObserver: Observer<BroadcastInteractiveState> = mockk()
-//
-//        rule.dispatchers.coroutineDispatcher.advanceUntilIdle()
-//
-//        coEvery { mockRepo.getInteractiveConfig() } returns mockInteractiveConfigResponse
-//        coEvery { mockRepo.getCurrentInteractive(any()) } returns mockCurrentInteractive
-//        coEvery { mockRepo.getInteractiveLeaderboard(any(), any()) } returns mockInteractive
-//        every { stateObserver.onChanged(any()) }.just(Runs)
-//
-//        val robot = PlayBroadcastViewModelRobot(
-//            dispatchers = testDispatcher,
-//            channelRepo = mockRepo
-//        )
-//
-//        runBlockingTest {
-//            robot.getViewModel().observableInteractiveState.observeForever(stateObserver)
-//
-//            robot.getViewModel().startLiveStream(true)
-//
-//            // val configResult = robot.getViewModel().observableInteractiveConfig.getOrAwaitValue()
-//
-//            // configResult.assertEqualTo(mockInteractiveConfigResponse)
-//            verifySequence {
-//                stateObserver.onChanged(mockExpectedStateLoading)
-//                stateObserver.onChanged(mockExpectedStateCoachMark)
-//            }
-//        }
-//    }
+    @Test
+    fun `when user successfully create new interactive session, it should return interactive model`() {
+
+        val mockTitle = "Giveaway"
+        val mockDurationInMs = 1000L
+        val mockInteractiveResponse = InteractiveSessionUiModel(
+            id = "1",
+            title = mockTitle,
+            durationInMs = mockDurationInMs,
+        )
+
+        every { mockLivePusher.remainingDurationInMillis } returns 10_000
+        coEvery { mockRepo.createInteractiveSession(any(), any(), any()) } returns mockInteractiveResponse
+
+        val robot = PlayBroadcastViewModelRobot(
+            livePusherMediator = mockLivePusher,
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+            sharedPref = mockSharedPref,
+        )
+
+        robot.getViewModel().createInteractiveSession(mockTitle, mockDurationInMs)
+
+        val result = robot.getViewModel().observableCreateInteractiveSession.getOrAwaitValue()
+
+        Assertions
+            .assertThat(result)
+            .isInstanceOf(NetworkResult.Success::class.java)
+    }
+
+    @Test
+    fun `when user creates new interactive session but the remaining duration is not enough, it should return fail state`() {
+
+        every { mockLivePusher.remainingDurationInMillis } returns 500
+
+        val robot = PlayBroadcastViewModelRobot(
+            livePusherMediator = mockLivePusher,
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+            sharedPref = mockSharedPref,
+        )
+
+        robot.getViewModel().createInteractiveSession("Giveaway", 1000L)
+
+        val result = robot.getViewModel().observableCreateInteractiveSession.getOrAwaitValue()
+
+        Assertions
+            .assertThat(result)
+            .isInstanceOf(NetworkResult.Fail::class.java)
+    }
+
+    @Test
+    fun `when user failed create new interactive session, it should return fail state`() {
+
+        every { mockLivePusher.remainingDurationInMillis } returns 10_000
+        coEvery { mockRepo.createInteractiveSession(any(), any(), any()) } throws mockException
+
+        val robot = PlayBroadcastViewModelRobot(
+            livePusherMediator = mockLivePusher,
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+            sharedPref = mockSharedPref,
+        )
+
+        robot.getViewModel().createInteractiveSession("Giveaway", 1000L)
+
+        val result = robot.getViewModel().observableCreateInteractiveSession.getOrAwaitValue()
+
+        Assertions
+            .assertThat(result)
+            .isInstanceOf(NetworkResult.Fail::class.java)
+    }
 }
