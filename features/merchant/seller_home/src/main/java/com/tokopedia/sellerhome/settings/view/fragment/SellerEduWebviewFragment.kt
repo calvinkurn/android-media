6e3aeb9575bfefc339dd52d818e97785246e2a96
reivinoktavianus <oktavianusreivin@gmail.com>
2022-02-24 15:00:47 +0700

    Check for invalid url when downloading pdf in Seller Edu Webview
    - Append url if it doesn't contain correct schemes
    - Check for valid URL
    - Send error log if invalid

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/fragment/SellerEduWebviewFragment.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/fragment/SellerEduWebviewFragment.kt
@@ -3,17 +3,30 @@ package com.tokopedia.sellerhome.settings.view.fragment
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
 import android.webkit.WebView
+import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.kotlin.util.DownloadHelper
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.sellerhome.R
+import com.tokopedia.sellerhome.common.errorhandler.SellerHomeErrorHandler
+import com.tokopedia.sellerhome.di.component.DaggerSellerHomeComponent
+import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.permission.PermissionCheckerHelper
 import com.tokopedia.webview.BaseSessionWebViewFragment
 import com.tokopedia.webview.KEY_URL
+import javax.inject.Inject
 
 class SellerEduWebviewFragment : BaseSessionWebViewFragment() {
 
     companion object {
         private const val GOOGLE_DOCS_LINK = "https://docs.google.com/viewer?url="
         private const val PDF_EXT = "pdf"
+
+        private const val HTTP_SCHEME = "http://"
+        private const val HTTPS_SCHEME = "https://"
+
+        private const val
 
         fun newInstance(url: String): SellerEduWebviewFragment {
             val fragment = SellerEduWebviewFragment()
@@ -25,7 +38,17 @@ class SellerEduWebviewFragment : BaseSessionWebViewFragment() {
         }
     }
 
+    @Inject
+    lateinit var userSession: UserSessionInterface
+
     private var permissionCheckerHelper: PermissionCheckerHelper? = null
+
+    override fun initInjector() {
+        DaggerSellerHomeComponent.builder()
+            .baseAppComponent((requireContext().applicationContext as BaseMainApplication).baseAppComponent)
+            .build()
+            .inject(this)
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -46,7 +69,16 @@ class SellerEduWebviewFragment : BaseSessionWebViewFragment() {
 
     override fun shouldOverrideUrlLoading(webview: WebView?, url: String): Boolean {
         if (isPdf(url)) {
-            checkPermissionAndDownload(url.ejectFromGoogleDocs())
+            try {
+                val correctedUrl = url.appendUrlIfInvalid()
+                if (correctedUrl.isValidUrl()) {
+                    checkPermissionAndDownload(correctedUrl.ejectFromGoogleDocs())
+                } else {
+                    sendErrorLog(url)
+                }
+            } catch (ex: Exception) {
+                sendErrorLog(url, ex)
+            }
             return true
         }
         return super.shouldOverrideUrlLoading(webView, url)
@@ -86,6 +118,21 @@ class SellerEduWebviewFragment : BaseSessionWebViewFragment() {
         downloadHelper.downloadFile(::isPdf)
     }
 
+    private fun sendErrorLog(url: String,
+                             exception: Exception? = null) {
+        val errorMessage = context?.getString(
+            R.string.sah_seller_edu_error_message,
+            url
+        ).orEmpty()
+        val throwable = exception ?: MessageErrorException(errorMessage)
+        SellerHomeErrorHandler.logExceptionToServer(
+            errorTag = SellerHomeErrorHandler.SELLER_EDU_TAG,
+            throwable = throwable,
+            errorType = errorMessage,
+            deviceId = userSession.deviceId.orEmpty()
+        )
+    }
+
     private fun isPdf(uri: String): Boolean =
         uri.lowercase().endsWith(PDF_EXT)
 
@@ -95,5 +142,18 @@ class SellerEduWebviewFragment : BaseSessionWebViewFragment() {
 
     private fun String.ejectFromGoogleDocs(): String =
         this.replace(GOOGLE_DOCS_LINK, "")
+
+    private fun String.appendUrlIfInvalid(): String {
+        val isUrlHasScheme = startsWith(HTTP_SCHEME) || startsWith(HTTPS_SCHEME)
+        var appendedUrl = this
+        if (!isUrlHasScheme) {
+            appendedUrl = HTTPS_SCHEME + this
+        }
+        return appendedUrl
+    }
+
+    private fun String.isValidUrl(): Boolean {
+        return Patterns.WEB_URL.matcher(this).matches()
+    }
 
 }
