b6646fa00ccd8cadb51ae6c7df5132eef63f25f7
Fajar Chaeril Azhar <fajar.azhar@tokopedia.com>
2022-02-24 15:28:49 +0700

    Add expose annotation

--- features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/data/response/CreateCouponResponse.kt
+++ features/merchant/voucher_creation/src/main/java/com/tokopedia/vouchercreation/product/create/data/response/CreateCouponResponse.kt
@@ -1,28 +1,37 @@
 package com.tokopedia.vouchercreation.product.create.data.response
 
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class CreateCouponResponse(
     @SerializedName("merchantPromotionCreateMV")
+    @Expose
     val merchantPromotionCreateMV: MerchantPromotionCreateMV = MerchantPromotionCreateMV()
 )
 
 data class Data(
     @SerializedName("redirect_url")
+    @Expose
     val redirectUrl: String = "",
     @SerializedName("status")
+    @Expose
     val status: String = "",
     @SerializedName("voucher_id")
+    @Expose
     val voucherId: Int = 0
 )
 data class MerchantPromotionCreateMV(
     @SerializedName("data")
+    @Expose
     val data: Data = Data(),
     @SerializedName("message")
+    @Expose
     val message: String = "",
     @SerializedName("process_time")
+    @Expose
     val processTime: Double = 0.0,
     @SerializedName("status")
+    @Expose
     val status: Int = 0
 )
