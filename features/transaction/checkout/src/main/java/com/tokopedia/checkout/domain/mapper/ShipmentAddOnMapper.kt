f43f519817519dea66c12d4c151284d50ac34ee9
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-03-02 02:02:35 +0700

    add mapper and fix ineligible product in bottomsheet unavailable product

---
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/mapper/ShipmentAddOnMapper.kt
@@ -0,0 +1,190 @@
+package com.tokopedia.checkout.domain.mapper
+
+import com.tokopedia.logisticcart.shipping.model.CartItemModel
+import com.tokopedia.logisticcart.shipping.model.ShipmentCartItemModel
+import com.tokopedia.purchase_platform.common.feature.gifting.data.model.*
+import com.tokopedia.purchase_platform.common.feature.gifting.domain.model.*
+
+object ShipmentAddOnMapper {
+    private const val qty = "{{qty}}"
+
+    fun mapAddOnBottomSheetParam(addOnsDataModel: AddOnsDataModel,
+                                 availableBottomSheetData: AvailableBottomSheetData,
+                                 unavailableBottomSheetData: UnavailableBottomSheetData): AddOnProductData {
+        return AddOnProductData(
+                bottomSheetType = addOnsDataModel.addOnsButtonModel.action,
+                bottomSheetTitle = addOnsDataModel.addOnsBottomSheetModel.headerTitle,
+                source = AddOnProductData.SOURCE_NORMAL_CHECKOUT,
+                availableBottomSheetData = availableBottomSheetData,
+                unavailableBottomSheetData = unavailableBottomSheetData
+        )
+    }
+
+    fun mapAvailableBottomSheetOrderLevelData(addOnWordingModel: AddOnWordingModel, shipmentCartItemModel: ShipmentCartItemModel): AvailableBottomSheetData {
+        val addOnsDataModel = shipmentCartItemModel.addOnsOrderLevelModel
+        val products = addOnsDataModel?.addOnsBottomSheetModel?.products
+
+        val addOnWordingData = AddOnWordingData()
+        addOnWordingData.onlyGreetingCard = addOnWordingModel.onlyGreetingCard
+        addOnWordingData.packagingAndGreetingCard = addOnWordingModel.packagingAndGreetingCard
+        addOnWordingData.invoiceNotSendToRecipient = addOnWordingModel.invoiceNotSendToRecipient
+
+        val listProductId = arrayListOf<String>()
+        val listProduct = arrayListOf<Product>()
+        if (products != null) {
+            for ((_, productName) in products) {
+                for ((cartId, _, _, productId, _, name, price, _, _, _, variantParentId, _, _, _, _, quantity, _, imageUrl) in shipmentCartItemModel.cartItemModels) {
+                    if (productName.equals(name, ignoreCase = true)) {
+                        val product = Product()
+                        product.cartId = cartId.toString()
+                        product.productId = productId.toString()
+                        product.productName = name
+                        product.productPrice = price.toLong()
+                        product.productQuantity = quantity
+                        product.productImageUrl = imageUrl
+                        product.productParentId = variantParentId
+                        listProduct.add(product)
+                        listProductId.add(productId.toString())
+                        break
+                    }
+                }
+            }
+        }
+
+        val addOnDataList = arrayListOf<AddOnData>()
+        if (addOnsDataModel?.addOnsDataItemModelList?.isNotEmpty() == true) {
+            for ((addOnPrice, addOnId, addOnMetadata1, addOnQty) in addOnsDataModel.addOnsDataItemModelList) {
+                val addOnData = AddOnData()
+                addOnData.addOnId = addOnId
+                addOnData.addOnPrice = addOnPrice
+                addOnData.addOnQty = addOnQty.toInt()
+                val addOnNote = AddOnNote()
+                val addOnNoteItemModel = addOnMetadata1.addOnNoteItemModel
+                addOnNote.isCustomNote = addOnNoteItemModel.isCustomNote
+                addOnNote.notes = addOnNoteItemModel.notes
+                addOnNote.from = addOnNoteItemModel.from
+                addOnNote.to = addOnNoteItemModel.to
+                val addOnMetadata = AddOnMetadata()
+                addOnMetadata.addOnNote = addOnNote
+                addOnData.addOnMetadata = addOnMetadata
+                addOnDataList.add(addOnData)
+            }
+        }
+
+        return AvailableBottomSheetData(
+                addOnInfoWording = addOnWordingData,
+                shopName = shipmentCartItemModel.shopName ?: "",
+                products = listProduct,
+                addOnSavedStates = addOnDataList,
+                cartString = shipmentCartItemModel.cartString ?: "",
+                isTokoCabang = shipmentCartItemModel.isFulfillment,
+                warehouseId = shipmentCartItemModel.fulfillmentId.toString()
+        )
+    }
+
+    fun mapUnavailableBottomSheetOrderLevelData(addOnBottomSheetModel: AddOnBottomSheetModel, shipmentCartItemModel: ShipmentCartItemModel): UnavailableBottomSheetData {
+        val listUnavailableProduct: MutableList<Product> = arrayListOf()
+        for ((_, productName) in addOnBottomSheetModel.products) {
+            for ((cartId, _, _, productId, _, name, price, _, _, _, variantParentId, _, _, _, _, quantity, _, imageUrl) in shipmentCartItemModel.cartItemModels) {
+                if (productName.equals(name, ignoreCase = true)) {
+                    val product = Product()
+                    product.cartId = cartId.toString()
+                    product.productId = productId.toString()
+                    product.productPrice = price.toLong()
+                    product.productQuantity = quantity
+                    product.productName = name
+                    product.productImageUrl = imageUrl
+                    product.productParentId = variantParentId
+                    listUnavailableProduct.add(product)
+                    break
+                }
+            }
+        }
+
+        return UnavailableBottomSheetData(
+                description = addOnBottomSheetModel.description,
+                tickerMessage = addOnBottomSheetModel.ticker.text,
+                unavailableProducts = listUnavailableProduct
+        )
+    }
+
+    fun mapAvailableBottomSheetProductLevelData(addOnWordingModel: AddOnWordingModel, cartItemModel: CartItemModel): AvailableBottomSheetData {
+        val addOnWordingData = AddOnWordingData()
+        var onlyGreetingCard: String = addOnWordingModel.onlyGreetingCard
+        if (onlyGreetingCard.contains(qty)) {
+            onlyGreetingCard = onlyGreetingCard.replace(qty, cartItemModel.quantity.toString())
+        }
+        addOnWordingData.onlyGreetingCard = onlyGreetingCard
+
+        var packageAndGreetingCard: String = addOnWordingModel.packagingAndGreetingCard
+        if (packageAndGreetingCard.contains(qty)) {
+            packageAndGreetingCard = packageAndGreetingCard.replace(qty, cartItemModel.quantity.toString())
+        }
+
+        addOnWordingData.packagingAndGreetingCard = packageAndGreetingCard
+        addOnWordingData.invoiceNotSendToRecipient = addOnWordingModel.invoiceNotSendToRecipient
+
+        val listProduct = arrayListOf<Product>()
+        val product = Product()
+        product.cartId = cartItemModel.cartId.toString()
+        product.productId = cartItemModel.productId.toString()
+        product.productName = cartItemModel.name
+        product.productPrice = cartItemModel.price.toLong()
+        product.productQuantity = cartItemModel.quantity
+        product.productImageUrl = cartItemModel.imageUrl
+        product.productParentId = cartItemModel.variantParentId
+        listProduct.add(product)
+
+        val addOnDataList = arrayListOf<AddOnData>()
+        val (_, addOnsDataItemModelList) = cartItemModel.addOnProductLevelModel
+        if (cartItemModel.addOnProductLevelModel.addOnsDataItemModelList.isNotEmpty()) {
+            for ((addOnPrice, addOnId, addOnMetadata1, addOnQty) in addOnsDataItemModelList) {
+                val addOnData = AddOnData()
+                addOnData.addOnId = addOnId
+                addOnData.addOnPrice = addOnPrice
+                addOnData.addOnQty = addOnQty.toInt()
+                val addOnNote = AddOnNote()
+                val addOnNoteItemModel = addOnMetadata1.addOnNoteItemModel
+                addOnNote.isCustomNote = addOnNoteItemModel.isCustomNote
+                addOnNote.notes = addOnNoteItemModel.notes
+                addOnNote.from = addOnNoteItemModel.from
+                addOnNote.to = addOnNoteItemModel.to
+                val addOnMetadata = AddOnMetadata()
+                addOnMetadata.addOnNote = addOnNote
+                addOnData.addOnMetadata = addOnMetadata
+                addOnDataList.add(addOnData)
+            }
+        }
+
+        return AvailableBottomSheetData(
+                addOnInfoWording = addOnWordingData,
+                shopName = cartItemModel.shopName,
+                products = listProduct,
+                addOnSavedStates = addOnDataList,
+                cartString = cartItemModel.cartString,
+                isTokoCabang = cartItemModel.isTokoCabang,
+                warehouseId = cartItemModel.warehouseId
+        )
+    }
+
+    fun mapUnavailableBottomSheetProductLevelData(addOnBottomSheetModel: AddOnBottomSheetModel, cartItemModel: CartItemModel): UnavailableBottomSheetData {
+        val listUnavailableProduct = arrayListOf<Product>()
+        for ((productImageUrl, productName) in addOnBottomSheetModel.products) {
+            val product = Product()
+            product.cartId = cartItemModel.cartId.toString()
+            product.productId = cartItemModel.productId.toString()
+            product.productPrice = cartItemModel.price.toLong()
+            product.productQuantity = cartItemModel.quantity
+            product.productName = productName
+            product.productImageUrl = productImageUrl
+            product.productParentId = cartItemModel.variantParentId
+            listUnavailableProduct.add(product)
+        }
+
+        return UnavailableBottomSheetData(
+                description = addOnBottomSheetModel.description,
+                tickerMessage = addOnBottomSheetModel.ticker.text,
+                unavailableProducts = listUnavailableProduct
+        )
+    }
+}
