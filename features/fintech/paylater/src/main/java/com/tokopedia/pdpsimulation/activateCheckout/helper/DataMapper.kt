623627f3c796c9760b026a9ce622591e208e9566
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-03-01 12:20:07 +0530

    added the kotlin map in data mapper https://github.com/tokopedia/android-tokopedia-core/pull/24298#discussion_r815681299

--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/activateCheckout/helper/DataMapper.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/activateCheckout/helper/DataMapper.kt
@@ -13,14 +13,9 @@ object DataMapper {
         val priceText = tenureDetail.monthly_installment
         val tenure = tenureDetail.tenure.toString()
         val contentList: MutableList<Content> = ArrayList()
-        for (i in 0 until tenureDetail.installment_details.detailContent.size) {
-            val content = Content(
-                tenureDetail.installment_details.detailContent[i].title,
-                tenureDetail.installment_details.detailContent[i].value,
-                tenureDetail.installment_details.detailContent[i].type
-            )
-            contentList.add(content)
-        }
+         tenureDetail.installment_details.detailContent.map {
+            Content(it.title,it.value,it.type)
+        }.toCollection(contentList)
         val installmentDetails =
             InstallmentDetails(tenureDetail.installment_details.header, contentList)
         return TenureSelectedModel(priceText, tenure, installmentDetails)
