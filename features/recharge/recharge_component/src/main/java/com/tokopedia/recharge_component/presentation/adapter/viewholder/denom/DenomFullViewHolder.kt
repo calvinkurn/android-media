f513bb10829fda45047a6a9f1e614ef34859a25a
firmankagami <firmanda.nugroho@tokopedia.com>
2022-03-02 16:32:15 +0700

    [Feature][Outofstock] update layout out of stock

--- features/recharge/recharge_component/src/main/java/com/tokopedia/recharge_component/presentation/adapter/viewholder/denom/DenomFullViewHolder.kt
+++ features/recharge/recharge_component/src/main/java/com/tokopedia/recharge_component/presentation/adapter/viewholder/denom/DenomFullViewHolder.kt
@@ -2,6 +2,7 @@ package com.tokopedia.recharge_component.presentation.adapter.viewholder.denom
 
 import android.graphics.Paint
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
@@ -12,6 +13,8 @@ import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.recharge_component.R
 import com.tokopedia.recharge_component.databinding.ViewRechargeDenomFullBinding
 import com.tokopedia.recharge_component.listener.RechargeDenomFullListener
+import com.tokopedia.recharge_component.model.denom.DenomConst
+import com.tokopedia.recharge_component.model.denom.DenomConst.setStatusOutOfStockColor
 import com.tokopedia.recharge_component.model.denom.DenomData
 import com.tokopedia.recharge_component.model.denom.DenomWidgetEnum
 import com.tokopedia.unifycomponents.CardUnify
@@ -29,6 +32,7 @@ class DenomFullViewHolder(
             tgDenomFullTitle.run {
                 if (!denomFull.title.isNullOrEmpty()){
                     show()
+                    setStatusOutOfStockColor(denomFull.status, context)
                     text = denomFull.title
                 } else hide()
             }
@@ -36,6 +40,7 @@ class DenomFullViewHolder(
             tgDenomFullDesc.run {
                 if (!denomFull.description.isNullOrEmpty()){
                     show()
+                    setStatusOutOfStockColor(denomFull.status, context)
                     text = denomFull.description
                 } else hide()
             }
@@ -43,6 +48,7 @@ class DenomFullViewHolder(
             tgDenomFullQuota.run {
                 if (!denomFull.quotaInfo.isNullOrEmpty()){
                     show()
+                    setStatusOutOfStockColor(denomFull.status, context)
                     text = denomFull.quotaInfo
                 } else hide()
             }
@@ -56,12 +62,13 @@ class DenomFullViewHolder(
             tgDenomFullExpired.run {
                 if (!denomFull.expiredDays.isNullOrEmpty()){
                     show()
+                    setStatusOutOfStockColor(denomFull.status, context)
                     text = denomFull.expiredDays
                 } else hide()
             }
 
             labelDenomFullSpecial.run {
-                if (!denomFull.specialLabel.isNullOrEmpty()){
+                if (!denomFull.specialLabel.isNullOrEmpty() && denomFull.status != DenomConst.DENOM_STATUS_OUT_OF_STOCK){
                     show()
                     text = denomFull.specialLabel
                 } else hide()
@@ -70,19 +77,55 @@ class DenomFullViewHolder(
             tgDenomFullPrice.run {
                 if (!denomFull.price.isNullOrEmpty()){
                     show()
+                    setStatusOutOfStockColor(denomFull.status, context)
                     text = denomFull.price
                 } else hide()
             }
 
             labelDenomFullDiscount.run {
-                if (!denomFull.discountLabel.isNullOrEmpty()){
+                if (denomFull.status == DenomConst.DENOM_STATUS_OUT_OF_STOCK) {
                     show()
+                    setStatusOutOfStockColor(resources.getString(com.tokopedia.recharge_component.R.string.out_of_stock_label_denom_digital))
+
+                    val labelParams = this.layoutParams as ConstraintLayout.LayoutParams
+                    labelParams.topToTop = ConstraintLayout.LayoutParams.UNSET
+                    labelParams.topToBottom = tgDenomFullPrice.id
+                    labelParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+                    labelParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
+                    layoutParams = labelParams
+
+                    setMargin(
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl2)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt())
+                } else if (!denomFull.discountLabel.isNullOrEmpty()){
+                    show()
+                    val labelParams = this.layoutParams as ConstraintLayout.LayoutParams
+                    labelParams.topToTop = tgDenomFullPrice.id
+                    labelParams.bottomToBottom = tgDenomFullPrice.id
+                    labelParams.leftToRight = tgDenomFullPrice.id
+                    layoutParams = labelParams
+
+                    setMargin(
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl2)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt(),
+                        resources.getDimension(com.tokopedia.unifyprinciples.R.dimen.unify_space_0)
+                            .toInt())
                     text = denomFull.discountLabel
                 } else hide()
             }
 
             tgDenomFullSlashPrice.run {
-                if (!denomFull.slashPrice.isNullOrEmpty()) {
+                if (!denomFull.slashPrice.isNullOrEmpty() && denomFull.status != DenomConst.DENOM_STATUS_OUT_OF_STOCK) {
                     show()
                     text = denomFull.slashPrice
                     paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
@@ -91,14 +134,14 @@ class DenomFullViewHolder(
 
 
             tgDenomFullSoldPercentageLabel.run {
-                if (!denomFull.flashSaleLabel.isNullOrEmpty()){
+                if (!denomFull.flashSaleLabel.isNullOrEmpty() && denomFull.status != DenomConst.DENOM_STATUS_OUT_OF_STOCK){
                     show()
                     text = denomFull.flashSaleLabel
                 } else hide()
             }
 
             pgDenomFullFlashSale.run {
-                if (denomFull.flashSalePercentage.isMoreThanZero()) {
+                if (denomFull.flashSalePercentage.isMoreThanZero() && denomFull.status != DenomConst.DENOM_STATUS_OUT_OF_STOCK) {
                     show()
                     setProgressIcon(
                         icon = ContextCompat.getDrawable(
@@ -138,8 +181,9 @@ class DenomFullViewHolder(
                     }
                 } else ViewGroup.LayoutParams.WRAP_CONTENT
 
-                cardType = if (isSelectedItem) CardUnify.TYPE_BORDER_ACTIVE else
-                    if (denomType == DenomWidgetEnum.MCCM_FULL_TYPE) CardUnify.TYPE_SHADOW
+                cardType = if(denomFull.status == DenomConst.DENOM_STATUS_OUT_OF_STOCK) CardUnify.TYPE_BORDER_DISABLED
+                    else if (isSelectedItem) CardUnify.TYPE_BORDER_ACTIVE
+                    else if (denomType == DenomWidgetEnum.MCCM_FULL_TYPE) CardUnify.TYPE_SHADOW
                     else CardUnify.TYPE_BORDER
 
                 if (denomType == DenomWidgetEnum.MCCM_FULL_TYPE && isOnlyOneSize){
@@ -149,7 +193,9 @@ class DenomFullViewHolder(
             }
 
             root.setOnClickListener {
-                denomFullListener.onDenomFullClicked(denomFull, denomType, position, "",true)
+                if(denomFull.status != DenomConst.DENOM_STATUS_OUT_OF_STOCK) {
+                    denomFullListener.onDenomFullClicked(denomFull, denomType, position, "", true)
+                }
             }
 
             root.addOnImpressionListener(denomFull) {
