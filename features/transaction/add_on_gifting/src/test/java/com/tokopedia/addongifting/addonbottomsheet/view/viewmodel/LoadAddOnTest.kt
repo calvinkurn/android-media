88e06fc74fedec511d30a55796fce62429e76107
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-03-02 20:06:49 +0700

    unit test

--- features/transaction/add_on_gifting/src/test/java/com/tokopedia/addongifting/addonbottomsheet/view/viewmodel/LoadAddOnTest.kt
+++ features/transaction/add_on_gifting/src/test/java/com/tokopedia/addongifting/addonbottomsheet/view/viewmodel/LoadAddOnTest.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.addongifting.addonbottomsheet.view.viewmodel
 
+import com.tokopedia.abstraction.common.network.exception.ResponseErrorException
 import com.tokopedia.addongifting.addonbottomsheet.data.getaddonbyproduct.GetAddOnByProductResponse
 import com.tokopedia.addongifting.addonbottomsheet.view.DataProvider
+import com.tokopedia.addongifting.addonbottomsheet.view.UiEvent
 import com.tokopedia.purchase_platform.common.feature.gifting.domain.model.AddOnProductData
 import io.mockk.Runs
 import io.mockk.coEvery
@@ -11,20 +13,19 @@ import org.junit.Test
 class LoadAddOnTest: BaseAddOnTest() {
 
     @Test
-    fun `WHEN load add on data success THEN should `() {
+    fun `WHEN load add on data error THEN should ui state should be STATE_FAILED_LOAD_ADD_ON_DATA`() {
         // GIVEN
         val addOnProductData = AddOnProductData()
-        val mockResponse = DataProvider.provideLoadAddOnDataSuccess()
         coEvery { getAddOnByProductUseCase.setParams(any()) } just Runs
         coEvery { getAddOnByProductUseCase.execute(any(), any()) } answers {
-            firstArg<(GetAddOnByProductResponse) -> Unit>().invoke(mockResponse)
+            secondArg<(Throwable) -> Unit>().invoke(ResponseErrorException())
         }
 
         // WHEN
         viewModel.loadAddOnData(addOnProductData)
 
         // THEN
-        assert(viewModel.uiEvent.replayCache.firstOrNull()?.)
+        assert(viewModel.uiEvent.replayCache.firstOrNull()?.state == UiEvent.STATE_FAILED_LOAD_ADD_ON_DATA)
     }
 
 }
