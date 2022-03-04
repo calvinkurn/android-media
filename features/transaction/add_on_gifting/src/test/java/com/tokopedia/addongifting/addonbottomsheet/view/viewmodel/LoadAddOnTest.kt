372213c1629c019f8079d2c1262a0de79eb33b08
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-03-04 11:30:49 +0700

    unit test

--- features/transaction/add_on_gifting/src/test/java/com/tokopedia/addongifting/addonbottomsheet/view/viewmodel/LoadAddOnTest.kt
+++ features/transaction/add_on_gifting/src/test/java/com/tokopedia/addongifting/addonbottomsheet/view/viewmodel/LoadAddOnTest.kt
@@ -2,18 +2,54 @@ package com.tokopedia.addongifting.addonbottomsheet.view.viewmodel
 
 import com.tokopedia.abstraction.common.network.exception.ResponseErrorException
 import com.tokopedia.addongifting.addonbottomsheet.data.getaddonbyproduct.GetAddOnByProductResponse
+import com.tokopedia.addongifting.addonbottomsheet.data.getaddonsavedstate.GetAddOnSavedStateResponse
 import com.tokopedia.addongifting.addonbottomsheet.view.DataProvider
 import com.tokopedia.addongifting.addonbottomsheet.view.UiEvent
 import com.tokopedia.purchase_platform.common.feature.gifting.domain.model.AddOnProductData
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.just
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class LoadAddOnTest: BaseAddOnTest() {
 
     @Test
-    fun `WHEN load add on data error THEN should ui state should be STATE_FAILED_LOAD_ADD_ON_DATA`() {
+    fun `WHEN load add on data and ad on saved state success THEN ui state should be STATE_SUCCESS_LOAD_ADD_ON_DATA`() = runBlockingTest {
+        // GIVEN
+        val addOnProductData = AddOnProductData()
+        val addOnSuccessResponse = DataProvider.provideLoadAddOnDataSuccess()
+        coEvery { getAddOnByProductUseCase.setParams(any()) } just Runs
+        coEvery { getAddOnByProductUseCase.execute(any(), any()) } answers {
+            firstArg<(GetAddOnByProductResponse) -> Unit>().invoke(addOnSuccessResponse)
+        }
+
+        val addOnSavedStateSuccessResponse = DataProvider.provideLoadAddOnSavedStateDataSuccess()
+        coEvery { getAddOnSavedStateUseCase.setParams(any()) } just Runs
+        coEvery { getAddOnSavedStateUseCase.execute(any(), any()) } answers {
+            firstArg<(GetAddOnSavedStateResponse) -> Unit>().invoke(addOnSavedStateSuccessResponse)
+        }
+
+        // store the result
+        val result = mutableListOf<UiEvent>()
+        val job = launch {
+            viewModel.uiEvent.toCollection(result)
+        }
+
+        // WHEN
+        viewModel.loadAddOnData(addOnProductData)
+
+        // THEN
+        assert(result.first().state == UiEvent.STATE_SUCCESS_LOAD_ADD_ON_DATA)
+        job.cancel()
+    }
+
+    @Test
+    fun `WHEN load add on data error THEN ui state should be STATE_FAILED_LOAD_ADD_ON_DATA`() = runBlockingTest {
         // GIVEN
         val addOnProductData = AddOnProductData()
         coEvery { getAddOnByProductUseCase.setParams(any()) } just Runs
@@ -21,11 +57,18 @@ class LoadAddOnTest: BaseAddOnTest() {
             secondArg<(Throwable) -> Unit>().invoke(ResponseErrorException())
         }
 
+        // store the result
+        val result = mutableListOf<UiEvent>()
+        val job = launch {
+            viewModel.uiEvent.toCollection(result)
+        }
+
         // WHEN
         viewModel.loadAddOnData(addOnProductData)
 
         // THEN
-        assert(viewModel.uiEvent.replayCache.firstOrNull()?.state == UiEvent.STATE_FAILED_LOAD_ADD_ON_DATA)
+        assert(result.first().state == UiEvent.STATE_FAILED_LOAD_ADD_ON_DATA)
+        job.cancel()
     }
 
 }
