bc5829e99d672f63a5bc44f4296fb080be80e07c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-03-04 10:39:18 +0700

    fix channel name on upcoming page

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayUpcomingViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayUpcomingViewModel.kt
@@ -408,7 +408,7 @@ class PlayUpcomingViewModel @Inject constructor(
 
     private fun handleSharingOption(shareModel: ShareModel) {
         viewModelScope.launch {
-            playAnalytic.clickSharingOption(mChannelId, partnerId, channelType.value, shareModel.socialMediaName, playShareExperience.isScreenshotBottomSheet())
+            playAnalytic.clickSharingOption(mChannelId, partnerId, channelType.value, shareModel.channel, playShareExperience.isScreenshotBottomSheet())
 
             val playShareExperienceData = getPlayShareExperienceData()
 
