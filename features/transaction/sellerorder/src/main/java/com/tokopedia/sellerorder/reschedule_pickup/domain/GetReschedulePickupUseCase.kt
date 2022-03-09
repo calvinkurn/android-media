b06523175af016bd67d70b76d0ea0b2018aaa9a1
hiradevina <fakhira.devina@tokopedia.com>
2022-03-09 14:32:06 +0700

    to be reverted: set testing env

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/reschedule_pickup/domain/GetReschedulePickupUseCase.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/reschedule_pickup/domain/GetReschedulePickupUseCase.kt
@@ -14,9 +14,73 @@ class GetReschedulePickupUseCase @Inject constructor(private val useCase: Graphq
     }
 
     suspend fun execute(param: GetReschedulePickupParam): GetReschedulePickupResponse.Data {
-        useCase.setGraphqlQuery(GetReschedulePickupQuery)
-        useCase.setRequestParams(generateParam(param))
-        return useCase.executeOnBackground()
+//        useCase.setGraphqlQuery(GetReschedulePickupQuery)
+//        useCase.setRequestParams(generateParam(param))
+//        return useCase.executeOnBackground()
+        return GetReschedulePickupResponse.Data(
+            mpLogisticGetReschedulePickup = GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup(
+                data = listOf(
+                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem(
+                        orderData = listOf(
+                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData(
+                                chooseDay = listOf(
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption(
+                                        day = "Kamis, 9 Desember 2021",
+                                        chooseTime = listOf(
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "08:00 WIB"
+                                            ),
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "09:00 WIB"
+                                            ),
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "10:00 WIB"
+                                            )
+                                        )
+                                    ),
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption(
+                                        day = "Jumat, 10 Desember 2021",
+                                        chooseTime = listOf(
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "11:00 WIB"
+                                            ),
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "12:00 WIB"
+                                            ),
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "13:00 WIB"
+                                            )
+                                        )
+                                    ),
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption(
+                                        day = "Senin, 13 Desember 2021",
+                                        chooseTime = listOf(
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "14:00 WIB"
+                                            ),
+                                            GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.DayOption.TimeOption(
+                                                time = "15:00 WIB"
+                                            ),
+                                        )
+                                    )
+                                ),
+                                chooseReason = listOf(
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.ReasonOption(
+                                        reason = "Toko Tutup"
+                                    ),
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.ReasonOption(
+                                        reason = "Pembeli Tidak Ditempat"
+                                    ),
+                                    GetReschedulePickupResponse.Data.MpLogisticGetReschedulePickup.DataItem.OrderData.ReasonOption(
+                                        reason = "Lainnya (Isi Sendiri)"
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
     }
 
     private fun generateParam(param: GetReschedulePickupParam): Map<String, Any?> {
