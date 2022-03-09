9ad07e999b8bc4d23368c6ab57d9d95693e022ef
niranjansaxena <niranjan.saxena@tokopedia.com>
2022-03-09 16:44:44 +0530

    di changes

--- features/category/tradein/src/main/java/com/tokopedia/tradein/di/TradeInUseCaseModule.kt
+++ features/category/tradein/src/main/java/com/tokopedia/tradein/di/TradeInUseCaseModule.kt
@@ -2,10 +2,14 @@ package com.tokopedia.tradein.di
 
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.utils.GraphqlHelper
+import com.tokopedia.graphql.coroutines.data.Interactor
+import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class TradeInUseCaseModule {
@@ -20,5 +24,19 @@ class TradeInUseCaseModule {
     @Provides
     fun provideUserSessionInterface(@ApplicationContext context: Context): UserSessionInterface {
         return UserSession(context)
+    }
+
+    @TradeInScope
+    @Provides
+    fun provideGraphqlUseCase(): GraphqlUseCase = GraphqlUseCase()
+
+    @Provides
+    fun provideGraphQlRepository() = Interactor.getInstance().graphqlRepository
+
+    @TradeInScope
+    @Provides
+    @Named("atcOcsMutation")
+    fun provideAddToCartOcsMutation(@ApplicationContext context: Context): String {
+        return GraphqlHelper.loadRawString(context.resources, com.tokopedia.atc_common.R.raw.mutation_add_to_cart_one_click_shipment)
     }
 }
