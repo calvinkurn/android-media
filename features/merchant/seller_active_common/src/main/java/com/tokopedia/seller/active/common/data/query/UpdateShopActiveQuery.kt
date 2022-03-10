a4df8f786afdf45fb97d4482b9adc1290d1bea10
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-03-10 15:01:50 +0800

    update query code style

--- features/merchant/seller_active_common/src/main/java/com/tokopedia/seller/active/common/data/query/UpdateShopActiveQuery.kt
+++ features/merchant/seller_active_common/src/main/java/com/tokopedia/seller/active/common/data/query/UpdateShopActiveQuery.kt
@@ -5,18 +5,19 @@ import com.tokopedia.gql_query_annotation.GqlQueryInterface
 object UpdateShopActiveQuery : GqlQueryInterface {
 
     private const val OPERATION_NAME = "updateShopActive"
-    const val QUERY = "mutation updateShopActive(\$input: ParamUpdateLastActive!){\n" +
-            "  updateShopActive(input: \$input) {\n" +
-            "    success\n" +
-            "    message\n" +
-            "  }\n" +
-            "}"
 
     override fun getOperationNameList(): List<String> {
         return listOf(OPERATION_NAME)
     }
 
-    override fun getQuery(): String = QUERY
+    override fun getQuery(): String = """
+        mutation $OPERATION_NAME(${'$'}input: ParamUpdateLastActive!){
+          $OPERATION_NAME(input: ${'$'}input){
+            success
+            message
+          }
+        }
+    """.trimIndent()
 
     override fun getTopOperationName(): String = OPERATION_NAME
 }
