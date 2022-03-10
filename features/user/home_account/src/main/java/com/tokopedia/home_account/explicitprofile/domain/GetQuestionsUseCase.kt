98a1fff0e6630fb64bce2d2fdce416cd1df28927
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2022-03-11 00:29:20 +0800

    Explicit Profile + add explicit profile useCase classes

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/explicitprofile/domain/GetQuestionsUseCase.kt
@@ -0,0 +1,76 @@
+package com.tokopedia.home_account.explicitprofile.domain
+
+import com.google.gson.annotations.SerializedName
+import com.tokopedia.graphql.coroutines.data.extensions.request
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.GqlParam
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
+import com.tokopedia.home_account.explicitprofile.data.ExplicitprofileGetQuestion
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class GetQuestionsUseCase @Inject constructor(
+    private val repository: GraphqlRepository
+) : CoroutineUseCase<GetQuestionsUseCase.QuestionParams, ExplicitprofileGetQuestion>(Dispatchers.IO) {
+
+    override suspend fun execute(params: QuestionParams): ExplicitprofileGetQuestion {
+        return repository.request(graphqlQuery(), params.toMapParam())
+    }
+
+    override fun graphqlQuery(): String {
+        return """
+            query explicitprofileGetQuestion(${'$'}templateName: String) {
+              explicitprofileGetQuestion(templateName: ${'$'}templateName) {
+                activeConfig {
+                  value
+                  detail {
+                    configActive
+                    templateActive
+                  }
+                }
+                template {
+                  id
+                  name
+                  description
+                  property {
+                    title
+                    image
+                  }
+                  sections {
+                    sectionID
+                    layout
+                    property {
+                      title
+                      infoHeader
+                    }
+                    questions {
+                      questionId
+                      answerId
+                      questionType
+                      answerValue
+                      property {
+                        name
+                        title
+                        subtitle
+                        infoContent
+                        options {
+                          value
+                          caption
+                          message
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+        """.trimIndent()
+    }
+
+    data class QuestionParams(
+        @SerializedName("checkActive")
+        var checkActive: Boolean = false,
+        @SerializedName("templateName")
+        var templateName: String = "",
+    ) : GqlParam
+}
---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/explicitprofile/domain/SaveMultipleAnswersUseCase.kt
@@ -0,0 +1,69 @@
+package com.tokopedia.home_account.explicitprofile.domain
+
+import com.tokopedia.gql_query_annotation.GqlQueryInterface
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
+import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
+import com.tokopedia.graphql.data.model.CacheType
+import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
+import com.tokopedia.home_account.explicitprofile.data.ExplicitProfileSaveMultiAnswers
+import com.tokopedia.home_account.explicitprofile.data.SaveMultipleAnswersParam
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class SaveMultipleAnswersUseCase @Inject constructor(
+    private val repository: MultiRequestGraphqlUseCase
+) : CoroutineUseCase<
+        MutableList<SaveMultipleAnswersParam>,
+        ExplicitProfileSaveMultiAnswers>(Dispatchers.IO)
+{
+
+    override suspend fun execute(
+        params: MutableList<SaveMultipleAnswersParam>
+    ): ExplicitProfileSaveMultiAnswers {
+        repository.clearRequest()
+        repository.clearCache()
+        repository.setCacheStrategy(
+            GraphqlCacheStrategy
+                .Builder(CacheType.ALWAYS_CLOUD)
+                .build()
+        )
+
+        params.forEach {
+            repository.addRequest(
+                GraphqlRequest(
+                    SaveMultipleAnswerGqlQuery(),
+                    ExplicitProfileSaveMultiAnswers::class.java,
+                    it.toMapParam()
+                )
+            )
+        }
+
+        return repository.executeOnBackground().getSuccessData()
+    }
+
+    override fun graphqlQuery(): String = ""
+
+    internal class SaveMultipleAnswerGqlQuery: GqlQueryInterface {
+        override fun getOperationNameList(): List<String> {
+            return listOf(
+                "explicitprofileSaveMultiAnswers"
+            )
+        }
+
+        override fun getQuery(): String {
+            return """
+                mutation explicitprofileSaveMultiAnswers(${'$'}input: SaveMultiAnswersRequest!) {
+                  explicitprofileSaveMultiAnswers(input: ${'$'}input) {
+                    message
+                  }
+                }
+            """.trimIndent()
+        }
+
+        override fun getTopOperationName(): String {
+            return "explicitprofileSaveMultiAnswers"
+        }
+    }
+}
