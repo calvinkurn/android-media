a77d3080ffef2f9016dcdbd3baf3f406676f6b89
devarafikry <devara.akmal@tokopedia.com>
2022-03-11 15:12:20 +0700

    adjust see all card

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/productcardgridcarousel/viewHolder/CarouselViewAllCardViewHolder.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/productcardgridcarousel/viewHolder/CarouselViewAllCardViewHolder.kt
@@ -92,23 +92,12 @@ class CarouselViewAllCardViewHolder(
                 card.mode = MODE_INVERT
                 card.backgroundView.setImageResource(R.drawable.ic_graphic_element_white)
             }
-        } else if (element.layoutType == DynamicChannelLayout.LAYOUT_CAMPAIGN_FEATURING) {
-            if (element.imageUrl.isEmpty() || getGradientBackgroundViewAllWhite(
-                    element.gradientColor, itemView.context
-                )
-            ) {
-                card.mode = MODE_INVERT
-                card.backgroundView.setImageResource(R.drawable.ic_graphic_element_green)
-            } else {
-                card.mode = MODE_INVERT
-                card.backgroundView.setImageResource(R.drawable.ic_graphic_element_white)
-            }
         } else {
             if (isGradientWhite || getGradientBackgroundViewAllWhite(element.gradientColor, itemView.context)) {
                 card.mode = MODE_NORMAL
                 card.backgroundView.setImageResource(R.drawable.ic_graphic_element_green)
             } else {
-                card.mode = MODE_COLOR
+                card.mode = MODE_INVERT
                 card.containerView.setGradientBackground(element.channelViewAllCard.gradientColor)
                 card.backgroundView.setImageResource(R.drawable.ic_graphic_element_white)
             }
