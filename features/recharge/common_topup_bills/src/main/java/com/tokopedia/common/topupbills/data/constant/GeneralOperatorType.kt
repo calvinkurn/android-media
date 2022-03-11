0ccab5437ed88ba87a4b61d989e06d0a5322ad38
firmankagami <firmanda.nugroho@tokopedia.com>
2022-03-11 14:41:04 +0700

    [Feedback] Feedback General operator and category

--- features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/constant/GeneralOperatorType.kt
+++ features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/constant/GeneralOperatorType.kt
@@ -2,8 +2,8 @@ package com.tokopedia.common.topupbills.data.constant
 
 class GeneralOperatorType {
     companion object {
-        val OPERATOR_TOKEN_LISTRIK = "6"
-        val OPERATOR_TAGLIS = "18"
-        val OPERATOR_NON_TAGLIS = "2315"
+        const val OPERATOR_TOKEN_LISTRIK = "6"
+        const val OPERATOR_TAGLIS = "18"
+        const val OPERATOR_NON_TAGLIS = "2315"
     }
 }
