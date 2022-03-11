e6828fd6b0c7e836943e6fc98b56fec829108949
rama.putra <rama.putra@BA-00021232.local>
2022-03-11 13:19:56 +0700

    fix unit test

--- features/user/profilecompletion/src/test/java/com/tokopedia/profilecompletion/profileinfo/ProfileViewModelTest.kt
+++ features/user/profilecompletion/src/test/java/com/tokopedia/profilecompletion/profileinfo/ProfileViewModelTest.kt
@@ -3,6 +3,7 @@ package com.tokopedia.profilecompletion.profileinfo
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.tokopedia.mediauploader.UploaderUseCase
+import com.tokopedia.mediauploader.common.state.UploadResult
 import com.tokopedia.profilecompletion.profileinfo.data.ProfileFeedResponse
 import com.tokopedia.profilecompletion.profileinfo.data.ProfileInfoResponse
 import com.tokopedia.profilecompletion.profileinfo.data.ProfileInfoUiModel
@@ -12,15 +13,20 @@ import com.tokopedia.profilecompletion.profileinfo.usecase.ProfileInfoUseCase
 import com.tokopedia.profilecompletion.profileinfo.usecase.ProfileRoleUseCase
 import com.tokopedia.profilecompletion.profileinfo.usecase.SaveProfilePictureUseCase
 import com.tokopedia.profilecompletion.profileinfo.viewmodel.ProfileViewModel
+import com.tokopedia.profilecompletion.settingprofile.data.SaveProfilePictureData
+import com.tokopedia.profilecompletion.settingprofile.data.SaveProfilePictureInnerData
+import com.tokopedia.profilecompletion.settingprofile.data.SaveProfilePictureResponse
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
 import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
+import net.bytebuddy.implementation.bytecode.Throw
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.io.File
 
 class ProfileViewModelTest {
 
@@ -56,11 +62,24 @@ class ProfileViewModelTest {
 
     private lateinit var viewModel: ProfileViewModel
 
-    private val dummyUserId = "123"
 
+    private val dummyError = Exception("")
+    private val dummyImageUrl = "dummy_image_url"
+    private val dummyErrorMsg = "failed_save_picture"
     private val dummyProfileInfo = ProfileInfoResponse()
     private val dummyProfilerole = ProfileRoleResponse()
     private val dummyProfileFeed = ProfileFeedResponse()
+    private val dummyUploadResultSuccess = UploadResult.Success("123", "")
+    private val dummyUploadResultFailed = UploadResult.Error("")
+    private val dummySaveProfilePictureSuccess = SaveProfilePictureResponse(SaveProfilePictureData(
+        "", emptyList(), SaveProfilePictureInnerData(imageUrl = dummyImageUrl, isSuccess = 1)))
+    private val dummySaveProfilePictureFailed = SaveProfilePictureResponse(SaveProfilePictureData(
+        "", listOf(dummyErrorMsg), SaveProfilePictureInnerData(imageUrl = "", isSuccess = 0)))
+    private val dummyProfileUIModel = ProfileInfoUiModel(
+        profileInfoData = dummyProfileInfo.profileInfoData,
+        profileRoleData = dummyProfilerole.profileRole,
+        profileFeedData = dummyProfileFeed.profileFeedData
+    )
 
         @Before
     fun before() {
@@ -75,8 +94,8 @@ class ProfileViewModelTest {
     @Test
     fun `success get profile info`() {
         //Given
-        coEvery { profileInfoUsecase(any()) } returns dummyProfileInfo
-        coEvery { profileRoleUsecase(any()) } returns dummyProfilerole
+        coEvery { profileInfoUsecase(Unit) } returns dummyProfileInfo
+        coEvery { profileRoleUsecase(Unit) } returns dummyProfilerole
         coEvery { profileFeedInfoUsecase(any()) } returns dummyProfileFeed
 
         //When
@@ -84,10 +103,143 @@ class ProfileViewModelTest {
 
         //Then
         coVerify(exactly = 1) {
-            profileInfoUsecase(any())
-            profileRoleUsecase(any())
+            profileInfoUsecase(Unit)
+            profileRoleUsecase(Unit)
             profileFeedInfoUsecase(any())
+            profileInfoObserver.onChanged(dummyProfileUIModel)
         }
+    }
 
+    @Test
+    fun `failed get profile info`() {
+        //Given
+        coEvery { profileInfoUsecase(Unit) } throws dummyError
+        coEvery { profileRoleUsecase(Unit) } returns dummyProfilerole
+        coEvery { profileFeedInfoUsecase(any()) } returns dummyProfileFeed
+
+        //When
+        viewModel.getProfileInfo()
+
+        //Then
+        coVerify(exactly = 1) {
+            profileInfoUsecase(Unit)
+            errorMessageObserver.onChanged(dummyError.message)
+        }
+    }
+
+    @Test
+    fun `failed get profile role`() {
+        //Given
+        coEvery { profileInfoUsecase(Unit) } returns dummyProfileInfo
+        coEvery { profileRoleUsecase(Unit) } throws dummyError
+        coEvery { profileFeedInfoUsecase(any()) } returns dummyProfileFeed
+
+        //When
+        viewModel.getProfileInfo()
+
+        //Then
+        coVerify(exactly = 1) {
+            profileRoleUsecase(Unit)
+            errorMessageObserver.onChanged(viewModel.errorMessage.value)
+        }
+    }
+
+    @Test
+    fun `failed get profile feed`() {
+        //Given
+        coEvery { profileInfoUsecase(Unit) } returns dummyProfileInfo
+        coEvery { profileRoleUsecase(Unit) } returns dummyProfilerole
+        coEvery { profileFeedInfoUsecase(any()) } throws dummyError
+
+        //When
+        viewModel.getProfileInfo()
+
+        //Then
+        coVerify(exactly = 1) {
+            profileFeedInfoUsecase(any())
+            errorMessageObserver.onChanged(viewModel.errorMessage.value)
+        }
+    }
+
+    @Test
+    fun `upload picture success and success save picture `() {
+        //Given
+        coEvery { uploaderUsecase(any()) } returns dummyUploadResultSuccess
+        coEvery { saveProfilePictureUsecase(any()) } returns dummySaveProfilePictureSuccess
+
+        //When
+        viewModel.uploadPicture(File(""))
+
+        //Then
+        coVerify {
+            uploaderUsecase(any())
+            saveProfilePictureUsecase(any())
+            savePhotoObserver.onChanged(dummyImageUrl)
+        }
+    }
+
+    @Test
+    fun `upload picture success and failed save picture`() {
+        //Given
+        coEvery { uploaderUsecase(any()) } returns dummyUploadResultSuccess
+        coEvery { saveProfilePictureUsecase(any()) } returns dummySaveProfilePictureFailed
+
+        //When
+        viewModel.uploadPicture(File(""))
+
+        //Then
+        coVerify {
+            uploaderUsecase(any())
+            saveProfilePictureUsecase(any())
+            errorMessageObserver.onChanged(dummyErrorMsg)
+        }
+    }
+
+    @Test
+    fun `upload picture success and throws error`() {
+        //Given
+        coEvery { uploaderUsecase(any()) } returns dummyUploadResultSuccess
+        coEvery { saveProfilePictureUsecase(any()) } throws dummyError
+
+        //When
+        viewModel.uploadPicture(File(""))
+
+        //Then
+        coVerify {
+            uploaderUsecase(any())
+            saveProfilePictureUsecase(any())
+            errorMessageObserver.onChanged(dummyError.message)
+        }
+    }
+
+
+    @Test
+    fun `upload picture failed`() {
+        coEvery { uploaderUsecase(any()) } returns dummyUploadResultFailed
+
+        //When
+        viewModel.uploadPicture(File(""))
+
+        //Then
+        coVerify {
+            uploaderUsecase(any())
+            errorMessageObserver.onChanged(dummyUploadResultFailed.message)
+        }
+    }
+
+    @Test
+    fun `upload picture throws error`() {
+        val dummyErrors = Exception("")
+
+        coEvery { uploaderUsecase(any()) } throws dummyErrors
+
+        //When
+        viewModel.uploadPicture(File(""))
+
+        //Then
+        coVerify {
+            uploaderUsecase(any())
+            errorMessageObserver.onChanged(dummyErrors.message)
+        }
     }
 }
