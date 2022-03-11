62982a3128458c8e60947c5afa560c8edc69212a
rama.putra <rama.putra@BA-00021232.local>
2022-03-11 09:30:21 +0700

    remove log

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -605,12 +605,8 @@ object DeeplinkDFMapper : CoroutineScope {
             add(DFP({ it.startsWith(ApplinkConstInternalMarketplace.REVIEW_CREDIBILITY)}, DF_MERCHANT_REVIEW, R.string.title_review_credibility))
 
             //Feedback Form
-            add(DFP({
-                Log.d("Feedbackform", it)
-                Log.d("Feedbackform", ApplinkConstInternalGlobal.FEEDBACK_FORM)
-                Log.d("Feedbackform", "apakah true ${it.startsWith(ApplinkConstInternalGlobal.FEEDBACK_FORM) }")
-                it.startsWith(ApplinkConstInternalGlobal.FEEDBACK_FORM) || it == ApplinkConstInternalGlobal.FEEDBACK_FORM
-                    }, DF_FEEDBACKFORM, R.string.internal_feedback))
+            add(DFP({ it.startsWith(ApplinkConstInternalGlobal.FEEDBACK_FORM) ||
+                    it == ApplinkConstInternalGlobal.FEEDBACK_FORM }, DF_FEEDBACKFORM, R.string.internal_feedback))
         }
     }
 
