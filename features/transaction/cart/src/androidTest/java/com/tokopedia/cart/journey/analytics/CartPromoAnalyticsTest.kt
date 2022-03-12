57ee057e00f459f07cde3f4989800cf0a0f16425
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-03-12 09:32:42 +0700

    revert

---
+++ features/transaction/cart/src/androidTest/java/com/tokopedia/cart/journey/analytics/CartPromoAnalyticsTest.kt
@@ -0,0 +1,82 @@
+package com.tokopedia.cart.journey.analytics
+
+import android.app.Activity
+import android.app.Instrumentation
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.cart.view.CartActivity
+import com.tokopedia.cart.view.CartIdlingResource
+import com.tokopedia.cart.robot.cartPage
+import com.tokopedia.cart.test.R
+import com.tokopedia.cassavatest.CassavaTestRule
+import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
+import com.tokopedia.test.application.util.InstrumentationAuthHelper
+import com.tokopedia.test.application.util.InstrumentationMockHelper
+import com.tokopedia.test.application.util.setupGraphqlMockResponse
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CartPromoAnalyticsTest {
+
+    @get:Rule
+    var activityRule = object : IntentsTestRule<CartActivity>(CartActivity::class.java, false, false) {
+        override fun beforeActivityLaunched() {
+            super.beforeActivityLaunched()
+            InstrumentationAuthHelper.loginInstrumentationTestUser1()
+        }
+    }
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private var idlingResource: IdlingResource? = null
+
+    @get:Rule
+    var cassavaTestRule = CassavaTestRule()
+
+    @Before
+    fun setup() {
+        idlingResource = CartIdlingResource.getIdlingResource()
+        IdlingRegistry.getInstance().register(idlingResource)
+        setupGraphqlMockResponse {
+            addMockResponse(GET_CART_LIST_KEY, InstrumentationMockHelper.getRawString(context, R.raw.cart_bundle_analytics_promo_response), MockModelConfig.FIND_BY_CONTAINS)
+            addMockResponse(UPDATE_CART_KEY, InstrumentationMockHelper.getRawString(context, R.raw.update_cart_response), MockModelConfig.FIND_BY_CONTAINS)
+            addMockResponse(VALIDATE_USE_KEY, InstrumentationMockHelper.getRawString(context, R.raw.validate_use_analytics_default_response), MockModelConfig.FIND_BY_CONTAINS)
+        }
+    }
+
+    @Test
+    fun cartPromoJourney_PassedAnalyticsTest() {
+        activityRule.launchActivity(null)
+        Intents.intending(IntentMatchers.anyIntent()).respondWith(Instrumentation.ActivityResult(Activity.RESULT_CANCELED, null))
+
+        cartPage {
+            clickPromoButton()
+        } validateAnalytics {
+            hasPassedAnalytics(cassavaTestRule, ANALYTIC_VALIDATOR_QUERY_FILE_NAME)
+        }
+
+        // Prevent glide crash
+        Thread.sleep(2000)
+    }
+
+    @After
+    fun cleanup() {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        if (activityRule.activity?.isDestroyed == false) activityRule.finishActivity()
+    }
+
+    companion object {
+        private const val GET_CART_LIST_KEY = "cart_revamp_v3"
+        private const val UPDATE_CART_KEY = "update_cart_v2"
+        private const val VALIDATE_USE_KEY = "validate_use_promo_revamp"
+
+        private const val ANALYTIC_VALIDATOR_QUERY_FILE_NAME = "tracker/transaction/cart_promo.json"
+    }
+
+}
