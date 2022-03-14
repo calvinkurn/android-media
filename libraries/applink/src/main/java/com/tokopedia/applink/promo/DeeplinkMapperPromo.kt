76d78f7675630249df1161bdeca799b0f64cbb9f
kamal <kamal.majhi@tokopedia.com>
2022-03-14 23:37:17 +0530

    applink fix

--- libraries/applink/src/main/java/com/tokopedia/applink/promo/DeeplinkMapperPromo.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/promo/DeeplinkMapperPromo.kt
@@ -1,43 +1,56 @@
 package com.tokopedia.applink.promo
 
-import android.content.Context
 import android.net.Uri
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.internal.ApplinkConstInternalPromo
 
+fun getRegisteredNavigationTokopoints(deeplink: String) =
+    when (deeplink) {
+        ApplinkConst.TOKOPEDIA_REWARD -> ApplinkConstInternalPromo.TOKOPOINTS_HOME
+        else ->
+            getDynamicDeeplinkForTokopoints(deeplink)
+    }
 
-fun getRegisteredNavigationTokopoints(context: Context, deeplink: String) =
-        when (deeplink) {
-            ApplinkConst.TokoPoints.HOMEPAGE, ApplinkConst.TokoPoints.HOMEPAGE2, ApplinkConst.TokoPoints.HOMEPAGE_REWARD1,
-            ApplinkConst.TokoPoints.HOMEPAGE_REWARD2 -> ApplinkConstInternalPromo.TOKOPOINTS_HOME
-            ApplinkConst.TokoPoints.HISTORY -> ApplinkConstInternalPromo.TOKOPOINTS_HOME
-            else ->
-                getDynamicDeeplinkForTokopoints(context, deeplink)
+fun getDynamicDeeplinkForTokopoints(deeplink: String): String {
+
+    val deepLinkInternal: Any
+    val uri = Uri.parse(deeplink)
+    when {
+        deeplink.contains(ApplinkConst.TOKOPEDIA_REWARD) && uri.pathSegments.isEmpty() -> {
+            return deeplink.replace(
+                ApplinkConst.TOKOPEDIA_REWARD,
+                ApplinkConstInternalPromo.TOKOPOINTS_HOME
+            )
         }
-
-
-fun getDynamicDeeplinkForTokopoints(context: Context, deeplink: String): String {
-
-    var deepLinkInternal = ""
-    val path = Uri.parse(deeplink)
-
-    if (deeplink.contains(ApplinkConst.TOKOPOINTS)) {
-        deepLinkInternal = deeplink.replace(ApplinkConst.TOKOPOINTS, ApplinkConstInternalPromo.INTERNAL_TOKOPOINTS)
-    } else if (deeplink.contains(ApplinkConst.TOKOPEDIA_REWARD)) {
-        deepLinkInternal = deeplink.replace(ApplinkConst.TOKOPEDIA_REWARD, ApplinkConstInternalPromo.INTERNAL_TOKOPOINTS)
+        else -> {
+            deepLinkInternal = deeplink.replace(
+                ApplinkConst.TOKOPEDIA_REWARD,
+                ApplinkConstInternalPromo.INTERNAL_TOKOPOINTS
+            )
+            when {
+                deepLinkInternal.contains(ApplinkConst.TokoPoints.COUPON_DETAIL) -> {
+                    return deepLinkInternal.replace(
+                        ApplinkConst.TokoPoints.COUPON_DETAIL,
+                        ApplinkConst.TokoPoints.COUPON_DETAIL_VALUE
+                    )
+                }
+                deepLinkInternal.contains(ApplinkConst.TokoPoints.CATALOG_DETAIL_NEW) -> {
+                    return deepLinkInternal.replace(
+                        ApplinkConst.TokoPoints.CATALOG_DETAIL_NEW,
+                        ApplinkConst.TokoPoints.CATALOG_DETAIL_VALUE
+                    )
+                }
+                deepLinkInternal.contains(ApplinkConst.TokoPoints.CATALOG_LIST_NEW) &&
+                        uri.pathSegments[0] == ApplinkConst.TokoPoints.CATALOG_LIST_NEW -> {
+                    return deepLinkInternal.replace(
+                        ApplinkConst.TokoPoints.CATALOG_LIST_NEW,
+                        ApplinkConst.TokoPoints.CATALOG_LIST_VALUE
+                    )
+                }
+                else ->{}
+            }
+        }
     }
-    if (deepLinkInternal.contains(ApplinkConst.TokoPoints.COUPON_DETAIL)) {
-        return deepLinkInternal.replace(ApplinkConst.TokoPoints.COUPON_DETAIL, ApplinkConst.TokoPoints.COUPON_DETAIL_VALUE)
-    }
-    if (deepLinkInternal.contains(ApplinkConst.TokoPoints.CATALOG_DETAIL)) {
-        return deepLinkInternal.replace(ApplinkConst.TokoPoints.CATALOG_DETAIL, ApplinkConst.TokoPoints.CATALOG_DETAIL_VALUE)
-    }
-    if (deepLinkInternal.contains(ApplinkConst.TokoPoints.CATALOG_DETAIL_NEW)) {
-        return deepLinkInternal.replace(ApplinkConst.TokoPoints.CATALOG_DETAIL_NEW, ApplinkConst.TokoPoints.CATALOG_DETAIL_VALUE)
-    }
-    if (deepLinkInternal.contains(ApplinkConst.TokoPoints.CATALOG_LIST_NEW) && path.pathSegments[0] == ApplinkConst.TokoPoints.CATALOG_LIST_NEW) {
-        return deepLinkInternal.replaceFirst(ApplinkConst.TokoPoints.CATALOG_LIST_NEW, ApplinkConst.TokoPoints.CATALOG_LIST_VALUE)
-    }
-
     return deepLinkInternal
 }
+
