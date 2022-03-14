dc6767865afba3e28889f3f6336770a61e649a41
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-03-14 08:47:02 +0800

    Revert "Revert: bug fix empty state multi trend line (c490785a)"
    This reverts commit 41d93707

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/MultiLineGraphMapper.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/MultiLineGraphMapper.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.sellerhomecommon.domain.mapper
 
 import com.tokopedia.kotlin.extensions.orFalse
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.kotlin.extensions.view.orZero
 import com.tokopedia.sellerhomecommon.common.const.MetricsType
 import com.tokopedia.sellerhomecommon.domain.model.GetMultiLineGraphResponse
 import com.tokopedia.sellerhomecommon.domain.model.LineModel
@@ -92,8 +94,8 @@ class MultiLineGraphMapper @Inject constructor(): BaseResponseMapper<GetMultiLin
     }
 
     private fun isMetricEmpty(line: LineModel?): Boolean {
-        val isCurrentPeriodEmpty = line?.currentPeriode?.sumBy { (it.yVal ?: 0f).toInt() } == 0
-        val isLastPeriodEmpty = line?.lastPeriode?.sumBy { (it.yVal ?: 0f).toInt() } == 0
+        val isCurrentPeriodEmpty = line?.currentPeriode?.sumOf { (it.yVal.orZero()).toInt() } == Int.ZERO
+        val isLastPeriodEmpty = line?.lastPeriode?.sumOf { (it.yVal.orZero()).toInt() } == Int.ZERO
         return isCurrentPeriodEmpty && isLastPeriodEmpty
     }
 
