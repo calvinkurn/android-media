6ace3aa03d7895ef4c34b37ef9528ce760e50132
devarafikry <devara.akmal@tokopedia.com>
2022-03-15 13:24:33 +0700

    add impression log for topads

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/util/RecomServerLogger.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/util/RecomServerLogger.kt
@@ -20,6 +20,7 @@ object RecomServerLogger {
     const val TOPADS_RECOM_PAGE_TIMEOUT_EXCEEDED = "topads_recom_page_timeout"
     const val TOPADS_RECOM_PAGE_HIT_DYNAMIC_SLOTTING = "topads_recom_page_hit_dynamic_slotting"
     const val TOPADS_RECOM_PAGE_HIT_ADS_TRACKER = "topads_recom_page_hit_ads_tracker"
+    const val TOPADS_RECOM_PAGE_HIT_ADS_TRACKER_IMPRESSION = "topads_recom_page_hit_ads_tracker_impression"
     const val TOPADS_RECOM_PAGE_IS_NOT_ADS = "topads_recom_page_is_not_ads"
     const val TOPADS_RECOM_PAGE_BE_ERROR = "topads_recom_page_be_error"
     const val TOPADS_RECOM_PAGE_GENERAL_ERROR = "topads_recom_page_general_error"
