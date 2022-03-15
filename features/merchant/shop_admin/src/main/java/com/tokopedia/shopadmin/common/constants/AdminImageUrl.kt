2cb0537415e3cc288f0dbf33d48e0e6aaeee13ce
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-03-15 11:59:43 +0700

    add imageUrl for admin mobile

--- features/merchant/shop_admin/src/main/java/com/tokopedia/shopadmin/common/constants/AdminImageUrl.kt
+++ features/merchant/shop_admin/src/main/java/com/tokopedia/shopadmin/common/constants/AdminImageUrl.kt
@@ -1,5 +1,8 @@
 package com.tokopedia.shopadmin.common.constants
 
 object AdminImageUrl {
-    const val INVITATION_EXPIRES = ""
+    const val IL_CONFIRMATION_INVITATION = "https://images.tokopedia.net/img/android/shop_admin/il_confirmation_invitation.png"
+    const val IL_INVITATION_EXPIRES = "https://images.tokopedia.net/img/android/shop_admin/il_invitation_expires.png"
+    const val IL_INVITATION_REJECTED = "https://images.tokopedia.net/img/android/shop_admin/il_invitation_rejected.png"
+    const val IL_WELCOME_INVITATION_ACCEPTED = "https://images.tokopedia.net/img/android/shop_admin/il_welcome_invitation_accepted.png"
 }
