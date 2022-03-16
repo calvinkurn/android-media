d2c640d53ae602a263ca0a414837b267726d1cc7
astidhiyaa <asti.anzaria@tokopedia.com>
2022-03-16 16:13:17 +0800

    fix: conflict

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -1957,7 +1957,7 @@ class PlayViewModel @AssistedInject constructor(
 
     private fun handleSharingOption(shareModel: ShareModel) {
         viewModelScope.launch {
-            playAnalytic.clickSharingOption(channelId, partnerId, channelType.value, shareModel.socialMediaName, playShareExperience.isScreenshotBottomSheet())
+            playAnalytic.clickSharingOption(channelId, partnerId, channelType.value, shareModel.channel, playShareExperience.isScreenshotBottomSheet())
 
             val playShareExperienceData = getPlayShareExperienceData()
 
