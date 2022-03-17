bfb6943ab08d99668db0c8d5f7448c7d1120a23c
Ankit <ankit.kumar1@tokopedia.com>
2022-03-17 15:56:46 +0530

    missing files added

---
+++ features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/view/TopadsAutoBidSwitchPartialLayout.kt
@@ -0,0 +1,84 @@
+package com.tokopedia.topads.common.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.dialog.DialogUnify
+import com.tokopedia.topads.common.R
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.selectioncontrol.SwitchUnify
+import com.tokopedia.unifyprinciples.Typography
+
+class TopadsAutoBidSwitchPartialLayout(
+    context: Context, attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+
+    var switchBidEditKeyword: SwitchUnify? = null
+        private set
+    var txtBidTitleEditKeyword: Typography? = null
+        private set
+    var ivBidInfoEditKeyword: ImageUnify? = null
+        private set
+    var onInfoClicked: (() -> Unit)? = null
+    var onCheckBoxStateChanged: ((Boolean) -> Unit)? = null     //true for automatic
+    fun isBidAutomatic() = switchBidEditKeyword?.isChecked ?: false
+
+    init {
+        inflate(context, R.layout.topads_common_auto_bid_switch_partial_layout, this)
+        initView()
+        setClick()
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setClick() {
+        ivBidInfoEditKeyword?.setOnClickListener {
+            onInfoClicked?.invoke()
+        }
+
+        switchBidEditKeyword?.let {
+            it.setOnTouchListener { view, motionEvent ->
+                if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                    showChangeBidTypeConfirmationDialog(isBidAutomatic()) {
+                        it.isChecked = !it.isChecked
+                        onCheckBoxStateChanged?.invoke(it.isChecked)
+                    }
+                }
+                false
+            }
+        }
+    }
+
+    private fun initView() {
+        switchBidEditKeyword = findViewById(R.id.switchBidEditKeyword) //checked for otomatis
+        txtBidTitleEditKeyword = findViewById(R.id.txtBidTitleEditKeyword)
+        ivBidInfoEditKeyword = findViewById(R.id.ivBidInfoEditKeyword)
+        switchBidEditKeyword?.isClickable = false
+    }
+
+    private fun showChangeBidTypeConfirmationDialog(
+        switchToAutomatic: Boolean, positiveClick: () -> Unit,
+    ) {
+        DialogUnify(context, DialogUnify.HORIZONTAL_ACTION, DialogUnify.NO_IMAGE).apply {
+            if (switchToAutomatic) {
+                setTitle(context.resources.getString(R.string.topads_common_automatic_dialog_title))
+                setDescription(context.resources.getString(R.string.topads_common_automatic_dialog_description))
+            } else {
+                setTitle(context.resources.getString(R.string.topads_common_manual_dialog_title))
+                setDescription(context.resources.getString(R.string.topads_common_manual_dialog_description))
+            }
+            setPrimaryCTAText(context.resources.getString(R.string.topads_common_dialog_cta_text))
+            setSecondaryCTAText(context.resources.getString(R.string.topads_common_batal))
+            setSecondaryCTAClickListener {
+                dismiss()
+            }
+            setPrimaryCTAClickListener {
+                positiveClick()
+                dismiss()
+            }
+        }.show()
+    }
+}
