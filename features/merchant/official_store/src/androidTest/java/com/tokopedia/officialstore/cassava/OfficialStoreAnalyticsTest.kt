7dca1d2dd7a4fdbbaa36e01741395030c388099d
Dhaba Widhikari <wikodhawi@gmail.com>
2022-03-17 19:37:38 +0700

    remove unused code

--- features/merchant/official_store/src/androidTest/java/com/tokopedia/officialstore/cassava/OfficialStoreAnalyticsTest.kt
+++ features/merchant/official_store/src/androidTest/java/com/tokopedia/officialstore/cassava/OfficialStoreAnalyticsTest.kt
@@ -16,9 +16,12 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.cassavatest.CassavaTestRule
-import com.tokopedia.home_component.viewholders.*
+import com.tokopedia.home_component.viewholders.DynamicLegoBannerViewHolder
+import com.tokopedia.home_component.viewholders.MixLeftComponentViewHolder
+import com.tokopedia.home_component.viewholders.MixTopComponentViewHolder
 import com.tokopedia.home_component.viewholders.FeaturedShopViewHolder
 import com.tokopedia.home_component.visitable.MerchantVoucherDataModel
+import com.tokopedia.home_component.viewholders.FeaturedBrandViewHolder
 import com.tokopedia.localizationchooseaddress.domain.model.LocalWarehouseModel
 import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
 import com.tokopedia.officialstore.R
