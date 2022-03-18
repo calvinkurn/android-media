c18169d6b65f5ac55a3689263a9f4fe0b3a30724
astidhiyaa <asti.anzaria@tokopedia.com>
2022-03-18 18:02:33 +0800

    feat: add inet connection

--- features/content/play_common/src/main/java/com/tokopedia/play_common/util/PlayConnectionCommon.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/util/PlayConnectionCommon.kt
@@ -75,4 +75,26 @@ object PlayConnectionCommon {
             false
         }
     }
+
+    /***
+     * Get user's avg internet connection speed, in Kbps
+     */
+    @JvmStatic
+    fun getInetSpeed(context: Context): Int {
+        return try {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val networkCapabilities = connectivityManager.activeNetwork
+                val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities)
+                val avgSpeed = (actNw?.linkDownstreamBandwidthKbps?.plus(actNw.linkUpstreamBandwidthKbps))?.div(2) ?: 0
+                avgSpeed
+            }else{
+                0
+            }
+        } catch (e: Exception) {
+            0
+        }
+    }
+
 }
