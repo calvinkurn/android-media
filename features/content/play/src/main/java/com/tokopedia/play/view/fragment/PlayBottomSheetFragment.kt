1302c05629c8c69e031c0019bf831dbea548b1bc
astidhiyaa <asti.anzaria@tokopedia.com>
2022-03-22 14:49:01 +0800

    fix: ut change default param

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
@@ -223,12 +223,12 @@ class PlayBottomSheetFragment @Inject constructor(
         closeVariantSheet()
     }
 
-    override fun onAddToCartClicked(view: VariantSheetViewComponent, productModel: PlayProductUiModel.Product) {
-        shouldDoActionProduct(product = productModel, action = ProductAction.AddToCart, type = BottomInsetsType.VariantSheet)
+    override fun onAddToCartClicked(view: VariantSheetViewComponent, productModel: PlayProductUiModel.Product, sectionUiModel: ProductSectionUiModel.Section) {
+        shouldDoActionProduct(product = productModel, action = ProductAction.AddToCart, type = BottomInsetsType.VariantSheet, sectionInfo = sectionUiModel)
     }
 
-    override fun onBuyClicked(view: VariantSheetViewComponent, productModel: PlayProductUiModel.Product) {
-        shouldDoActionProduct(product = productModel, action = ProductAction.Buy, type = BottomInsetsType.VariantSheet)
+    override fun onBuyClicked(view: VariantSheetViewComponent, productModel: PlayProductUiModel.Product, sectionUiModel: ProductSectionUiModel.Section) {
+        shouldDoActionProduct(product = productModel, action = ProductAction.Buy, type = BottomInsetsType.VariantSheet, sectionInfo = sectionUiModel)
     }
 
     /**
@@ -406,16 +406,16 @@ class PlayBottomSheetFragment @Inject constructor(
 
     private fun shouldCheckProductVariant(product: PlayProductUiModel.Product, sectionInfo: ProductSectionUiModel.Section, action: ProductAction) {
         if (product.isVariantAvailable) {
-            openVariantSheet(product, action)
+            openVariantSheet(product, action, sectionInfo)
             analytic.clickActionProductWithVariant(product.id, action, sectionInfo)
         } else {
             shouldDoActionProduct(product, sectionInfo,action,BottomInsetsType.ProductSheet)
         }
     }
 
-    private fun openVariantSheet(product: PlayProductUiModel.Product, action: ProductAction) {
+    private fun openVariantSheet(product: PlayProductUiModel.Product, action: ProductAction, sectionInfo: ProductSectionUiModel.Section) {
         playViewModel.onShowVariantSheet(variantSheetMaxHeight, product, action)
-        viewModel.getProductVariant(product, action)
+        viewModel.getProductVariant(product, action, sectionInfo)
     }
 
     private fun closeVariantSheet() {
@@ -433,7 +433,7 @@ class PlayBottomSheetFragment @Inject constructor(
         if (::loadingDialog.isInitialized) loadingDialog.dismiss()
     }
 
-    private fun shouldDoActionProduct(product: PlayProductUiModel.Product, sectionInfo: ProductSectionUiModel.Section = ProductSectionUiModel.Section.Empty, action: ProductAction, type: BottomInsetsType) {
+    private fun shouldDoActionProduct(product: PlayProductUiModel.Product, sectionInfo: ProductSectionUiModel.Section, action: ProductAction, type: BottomInsetsType) {
         viewModel.doInteractionEvent(InteractionEvent.DoActionProduct(product, sectionInfo, action, type))
     }
 
