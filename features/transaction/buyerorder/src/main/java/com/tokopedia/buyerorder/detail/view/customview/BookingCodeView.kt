bc10f277b539724dea45d6c7486ddf78ba380759
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-03-23 14:53:48 +0800

    `refactor` : update booking code ui

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/customview/BookingCodeView.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/view/customview/BookingCodeView.kt
@@ -1,80 +1,85 @@
-package com.tokopedia.buyerorder.detail.view.customview;
+package com.tokopedia.buyerorder.detail.view.customview
 
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View.OnClickListener
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.tokopedia.buyerorder.R
+import com.tokopedia.unifycomponents.Toaster.LENGTH_LONG
+import com.tokopedia.unifycomponents.Toaster.TYPE_NORMAL
+import com.tokopedia.unifycomponents.Toaster.build
 
-import com.tokopedia.buyerorder.R;
-import com.tokopedia.unifycomponents.Toaster;
+class BookingCodeView : RelativeLayout {
 
-import static android.content.Context.CLIPBOARD_SERVICE;
+    private var bookingCode: String? = null
+    private var position = 0
+    private var totalItems = 0
+    private var bookingCodeTitle: String? = null
+    private lateinit var bookingCodeText: TextView
+    private lateinit var copyBookingCode: TextView
+    private lateinit var bookingCodeTitleView: TextView
 
-public class BookingCodeView extends RelativeLayout {
-
-    private static final String TEXT_COPIED = "Kode booking telah disalin";
-    private static final String TEXT = "text";
-    private Context context;
-    private String bookingCode;
-    private int position, totalItems;
-    private String bookingCodeTitle;
-    private TextView bookingCodeText, copyBookingCode, bookingCodeTitleView;
-    private ImageView dividerBookingCode;
-    private boolean isQRCodeAvailable;
-
-    public BookingCodeView(Context context) {
-        super(context);
-        initView();
+    constructor(context: Context?) : super(context) {
+        initView()
     }
 
-    public BookingCodeView (Context context, String  bookingCode, int position, String bookingCodeTitle, int totalItems) {
-        super(context);
-        this.context = context;
-        this.bookingCode = bookingCode;
-        this.position = position;
-        this.bookingCodeTitle = bookingCodeTitle;
-        this.totalItems = totalItems;
-        initView();
+    constructor(
+        context: Context,
+        bookingCode: String,
+        position: Int,
+        bookingCodeTitle: String,
+        totalItems: Int
+    ) : super(context) {
+        this.bookingCode = bookingCode
+        this.position = position
+        this.bookingCodeTitle = bookingCodeTitle
+        this.totalItems = totalItems
+        initView()
     }
 
-    public BookingCodeView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initView();
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        initView()
     }
 
-    public BookingCodeView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initView();
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        initView()
     }
 
-    private void initView() {
-        View view = LayoutInflater.from(context).inflate(R.layout.booking_code_layout, this);
-        bookingCodeText = view.findViewById(R.id.booking_code);
-        bookingCodeTitleView = view.findViewById(R.id.booking_code_title);
-        copyBookingCode = view.findViewById(R.id.copyCode);
-        dividerBookingCode = view.findViewById(R.id.divider_booking_code);
-        bookingCodeTitleView.setText(bookingCodeTitle);
-        bookingCodeText.setText(bookingCode);
-        copyBookingCode.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ClipData myClip;
-                ClipboardManager myClipboard = (ClipboardManager) getContext().getSystemService(CLIPBOARD_SERVICE);
-                myClip = ClipData.newPlainText(TEXT, bookingCode);
-                myClipboard.setPrimaryClip(myClip);
-                Toaster.build(view, TEXT_COPIED, Toaster.LENGTH_LONG, Toaster.TYPE_NORMAL, "Ok", v1 -> {
-                }).show();
+    private fun initView() {
+        val view = LayoutInflater.from(context).inflate(R.layout.booking_code_layout, this)
 
-            }
-        });
+        bookingCodeText = view.findViewById(R.id.booking_code)
+        bookingCodeTitleView = view.findViewById(R.id.booking_code_title)
+        copyBookingCode = view.findViewById(R.id.copyCode)
+        bookingCodeTitleView.text = bookingCodeTitle
+        bookingCodeText.text = bookingCode
 
-        if (position == totalItems - 1) {
-            dividerBookingCode.setVisibility(GONE);
+        copyBookingCode.setOnClickListener {
+            val myClipboard =
+                context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val myClip: ClipData = ClipData.newPlainText(TEXT, bookingCode)
+            myClipboard.setPrimaryClip(myClip)
+            build(
+                view,
+                TEXT_COPIED,
+                LENGTH_LONG,
+                TYPE_NORMAL,
+                "Ok",
+                OnClickListener { }
+            ).show()
         }
+    }
+
+    companion object {
+        private const val TEXT_COPIED = "Kode booking telah disalin"
+        private const val TEXT = "text"
     }
 }
