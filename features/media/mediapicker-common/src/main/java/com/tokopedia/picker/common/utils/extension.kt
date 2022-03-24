25f7ff98efa450132d32b7fe20a495eefe05c935
Calvin <calvin.kurniawan@tokopedia.com>
2022-03-24 15:12:57 +0700

    delete function from mediapicker-common (will move to mediapicker)

--- features/media/mediapicker-common/src/main/java/com/tokopedia/picker/common/utils/extension.kt
+++ features/media/mediapicker-common/src/main/java/com/tokopedia/picker/common/utils/extension.kt
@@ -1,8 +1,5 @@
 package com.tokopedia.picker.common.utils
 
-import android.content.Context
-import com.tokopedia.picker.common.R.string as stringRes
-
 private const val BYTES_TO_MB = 1000000
 private const val MILLIS_TO_SEC = 1000
 
@@ -16,12 +13,4 @@ fun Int.toSec(): Int {
 
 fun Long.toSec(): Long {
     return this / MILLIS_TO_SEC
-}
-
-fun Long.toReadableTime(context: Context): String{
-    return if(this < 60){
-        "$this ${context.resources.getString(stringRes.picker_video_duration_max_limit_sec)}"
-    } else {
-        "${this / 60} ${context.resources.getString(stringRes.picker_video_duration_max_limit_min)}"
-    }
 }
