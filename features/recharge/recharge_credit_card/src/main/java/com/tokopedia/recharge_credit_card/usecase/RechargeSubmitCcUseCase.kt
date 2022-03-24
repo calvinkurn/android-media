0d79495f16ed36ab5cfac029dcd5a6c3d798337e
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-03-24 13:56:27 +0700

    [Recharge] add custom x-app-version headers to pcidss cc

--- features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/usecase/RechargeSubmitCcUseCase.kt
+++ features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/usecase/RechargeSubmitCcUseCase.kt
@@ -5,6 +5,8 @@ import com.tokopedia.common.network.coroutines.usecase.RestRequestUseCase
 import com.tokopedia.common.network.data.model.RequestType
 import com.tokopedia.common.network.data.model.RestRequest
 import com.tokopedia.common.network.data.model.RestResponse
+import com.tokopedia.config.GlobalConfig
+import com.tokopedia.network.authentication.HEADER_X_APP_VERSION
 import com.tokopedia.recharge_credit_card.datamodel.CCRedirectUrlResponse
 import com.tokopedia.url.TokopediaUrl
 import java.lang.reflect.Type
@@ -13,6 +15,7 @@ import javax.inject.Inject
 class RechargeSubmitCcUseCase @Inject constructor(val repository: RestRepository) : RestRequestUseCase(repository) {
 
     var mapParam = mutableMapOf<String, String>()
+
     fun setMapParam(map: HashMap<String, String>) {
         this.mapParam = map
     }
@@ -22,11 +25,18 @@ class RechargeSubmitCcUseCase @Inject constructor(val repository: RestRepository
     override suspend fun executeOnBackground(): Map<Type, RestResponse?> {
         val restRequest = RestRequest.Builder(url, CCRedirectUrlResponse::class.java)
                 .setRequestType(RequestType.POST)
+                .setHeaders(getPcidssCustomHeaders())
                 .setBody(mapParam)
                 .build()
         restRequestList.clear()
         restRequestList.add(restRequest)
         return repository.getResponses(restRequestList)
+    }
+
+    private fun getPcidssCustomHeaders(): HashMap<String, String> {
+        val headers = HashMap<String, String>()
+        headers[HEADER_X_APP_VERSION] = GlobalConfig.VERSION_NAME
+        return headers
     }
 
     companion object {
