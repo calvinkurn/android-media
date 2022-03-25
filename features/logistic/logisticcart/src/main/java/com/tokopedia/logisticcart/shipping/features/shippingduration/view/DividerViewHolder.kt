17c31484ebacd5f80f7fe81ff3670e846b4208fe
hiradevina <fakhira.devina@tokopedia.com>
2022-03-25 09:09:35 +0700

    rename layout

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingduration/view/DividerViewHolder.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingduration/view/DividerViewHolder.kt
@@ -3,10 +3,9 @@ package com.tokopedia.logisticcart.shipping.features.shippingduration.view
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.logisticcart.R
-import com.tokopedia.unifycomponents.ticker.Ticker
 
 class DividerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     companion object {
-        @JvmStatic val LAYOUT = R.layout.item_divider
+        @JvmStatic val LAYOUT = R.layout.item_divider_logistic_cart
     }
 }
