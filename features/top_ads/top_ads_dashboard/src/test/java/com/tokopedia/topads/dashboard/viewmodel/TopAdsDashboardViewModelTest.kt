f98ee664a520fbda9fd0916b9a757db29eda5231
Ankit <ankit.kumar1@tokopedia.com>
2022-03-28 14:15:13 +0530

    dashboard viewmodel test added

--- features/top_ads/top_ads_dashboard/src/test/java/com/tokopedia/topads/dashboard/viewmodel/TopAdsDashboardViewModelTest.kt
+++ features/top_ads/top_ads_dashboard/src/test/java/com/tokopedia/topads/dashboard/viewmodel/TopAdsDashboardViewModelTest.kt
@@ -1,12 +1,15 @@
 package com.tokopedia.topads.dashboard.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.gson.Gson
 import com.tokopedia.topads.common.data.response.Deposit
 import com.tokopedia.topads.common.data.response.DepositAmount
 import com.tokopedia.topads.common.data.response.TopadsDashboardDeposits
 import com.tokopedia.topads.common.domain.usecase.TopAdsGetDepositUseCase
 import com.tokopedia.topads.dashboard.data.model.beranda.RecommendationStatistics
+import com.tokopedia.topads.dashboard.data.model.beranda.TopAdsLatestReading
 import com.tokopedia.topads.dashboard.data.model.beranda.TopadsWidgetSummaryStatisticsModel
+import com.tokopedia.topads.dashboard.data.raw.topAdsHomepageLatestReadingJson
 import com.tokopedia.topads.dashboard.domain.interactor.TopAdsWidgetSummaryStatisticsUseCase
 import com.tokopedia.topads.dashboard.domain.interactor.TopadsRecommendationStatisticsUseCase
 import com.tokopedia.unit.test.rule.CoroutineTestRule
@@ -52,6 +55,26 @@ class TopAdsDashboardViewModelTest {
     }
 
     @Test
+    fun `getLatestReadings should parse topAdsHomepageLatestReadingJson and invoke 0th article as success`() {
+        val expected = Gson().fromJson(topAdsHomepageLatestReadingJson, TopAdsLatestReading::class.java)
+        viewModel.getLatestReadings()
+
+        assertEquals(expected[0].articles , (viewModel.latestReadingLiveData.value as Success).data)
+    }
+
+    @Test
+    fun `fetchSummaryStatistics failure check`() {
+        val expected : TopadsWidgetSummaryStatisticsModel? = null
+
+        coEvery {
+            summaryStatisticsUseCase.getSummaryStatistics(any(), any(), any())
+        } returns expected
+
+        viewModel.fetchSummaryStatistics("", "", "")
+        assert(viewModel.summaryStatisticsLiveData.value is Fail)
+    }
+
+    @Test
     fun `fetchSummaryStatistics success check`() {
         val expected =
             TopadsWidgetSummaryStatisticsModel(
@@ -82,9 +105,8 @@ class TopAdsDashboardViewModelTest {
     @Test
     fun `fetchRecommendationStatistics success check`() {
         val expected =
-            RecommendationStatistics(RecommendationStatistics.Statistics(mockk(),
-                listOf(),
-                mockk()))
+            RecommendationStatistics(RecommendationStatistics.Statistics(
+                mockk(), listOf(), mockk()))
 
         coEvery { recommendationStatisticsUseCase.fetchRecommendationStatistics() } returns expected
 
