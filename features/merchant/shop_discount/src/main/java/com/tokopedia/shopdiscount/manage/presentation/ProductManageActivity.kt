f484ec60701ffaf648b3c24fe5c0e8fc3fbbe653
Fajar Chaeril Azhar <fajar.azhar@tokopedia.com>
2022-03-28 12:53:18 +0700

    Add entry point to product manage page

---
+++ features/merchant/shop_discount/src/main/java/com/tokopedia/shopdiscount/manage/presentation/ProductManageActivity.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.shopdiscount.manage.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokopedia.shopdiscount.R
+
+class ProductManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_manage)
+    }
+}
