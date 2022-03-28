12c4a8719a911cb1e57116c48d95a04ababc86e8
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-03-28 11:29:30 +0800

    `feat` : create redeem adapter

---
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/detail/data/RedeemVoucherModel.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.buyerorder.detail.data
+
+/*
+ * created by @bayazidnasir on 28/3/2022
+ */
+
+data class RedeemVoucherModel(
+    val poweredBy: String,
+    val qrCodeUrl: String,
+    val voucherCode: String,
+    val isRedeem: Boolean
+)
