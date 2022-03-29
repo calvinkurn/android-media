f6258687958c4f37c49635fb7b75f92cfccdeff5
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2022-03-29 13:06:45 +0530

    tdn gql migration

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/repository/TopAdsRepository.kt
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/repository/TopAdsRepository.kt
@@ -6,68 +6,148 @@ import com.tokopedia.common.network.coroutines.repository.RestRepository
 import com.tokopedia.common.network.data.model.RequestType
 import com.tokopedia.common.network.data.model.RestCacheStrategy
 import com.tokopedia.common.network.data.model.RestRequest
+import com.tokopedia.gql_query_annotation.GqlQuery
+import com.tokopedia.graphql.data.model.GraphqlRequest
+import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.topads.sdk.UrlTopAdsSdk.getTopAdsImageViewUrl
 import com.tokopedia.topads.sdk.domain.interactor.DIMEN_ID
+import com.tokopedia.topads.sdk.domain.model.TopAdsBannerResponse
 import com.tokopedia.topads.sdk.domain.model.TopAdsImageViewModel
-import com.tokopedia.topads.sdk.domain.model.TopAdsmageViewResponse
 import com.tokopedia.usecase.RequestParams
 import java.lang.reflect.Type
 
 private const val KEY_IRIS_SESSION_ID = "iris_session_id"
+private const val DISPLAY_BANNER_PARAMS = "displayBannnerParams"
+private const val TOP_ADS_BANNER_QUERY =
+    """query topadsDisplayBannerAdsV3(${'$'}displayBannnerParams: String) {
+  topadsDisplayBannerAdsV3(displayBannnerParams:${'$'}displayBannnerParams){
+    data {
+      id
+      ad_ref_key
+      ad_view_url
+      ad_click_url
+      banner {
+        Name
+        Position
+        Images{
+          Dimension {
+            Id
+            Height
+            Width
+          }
+          Url
+        }
+        CategoryIDs
+        Shop
+        {
+          ShopID
+          ShopName
+          ShopImage {
+            cover
+            cover_ecs
+            s_ecs
+            s_url
+            xs_ecs
+            xs_url
+          }
+          ShopDomain
+          ShopTagline
+          ShopCityName
+          ShopIsOfficial
+          IsPowerMerchant
+          IsPowerMerchantBadge
+        }
+      } 
+    }
+    header {
+      total_data
+      pagination {
+        kind
+        next_page_token
+        current_page
+      }
+    }
+  }
+}"""
+
+@GqlQuery("TopAdsBannerQuery", TOP_ADS_BANNER_QUERY)
 open class TopAdsRepository {
 
     protected open val restRepository: RestRepository by lazy { RestRequestInteractor.getInstance().restRepository }
 
-    suspend fun getImageData(queryParams: MutableMap<String, Any>, sessionId: String): ArrayList<TopAdsImageViewModel> {
+    suspend fun getImageData(
+        queryParams: MutableMap<String, Any>,
+        sessionId: String
+    ): ArrayList<TopAdsImageViewModel> {
 
-        val response = this.getRestData<TopAdsmageViewResponse>(getTopAdsImageViewUrl(),
-                object : TypeToken<TopAdsmageViewResponse>() {}.type,
-                queryMap = queryParams,
-                sessionId = sessionId
+        val response = this.postData<TopAdsBannerResponse>(
+            getTopAdsImageViewUrl(),
+            object : TypeToken<DataResponse<TopAdsBannerResponse>>() {}.type,
+            queryMap = queryParams,
+            sessionId = sessionId
         )
 
         return mapToListOfTopAdsImageViewModel(response, queryParams)
 
     }
 
-    private suspend fun <T : Any> getRestData(url: String,
-                                              typeOf: Type,
-                                              requestType: RequestType = RequestType.GET,
-                                              queryMap: MutableMap<String, Any> = RequestParams.EMPTY.parameters,
-                                              sessionId: String,
-                                              cacheType: com.tokopedia.common.network.data.model.CacheType = com.tokopedia.common.network.data.model.CacheType.ALWAYS_CLOUD): T {
+    private suspend fun <T : Any> postData(
+        url: String,
+        typeOf: Type,
+        requestType: RequestType = RequestType.POST,
+        queryMap: MutableMap<String, Any> = RequestParams.EMPTY.parameters,
+        sessionId: String,
+        cacheType: com.tokopedia.common.network.data.model.CacheType = com.tokopedia.common.network.data.model.CacheType.ALWAYS_CLOUD
+    ): T {
         try {
             val restRequest = RestRequest.Builder(url, typeOf)
-                    .setRequestType(requestType)
-                    .setQueryParams(queryMap)
-                    .setHeaders(mapOf(KEY_IRIS_SESSION_ID to sessionId))
-                    .setCacheStrategy(RestCacheStrategy.Builder(cacheType).build())
-                    .build()
-            return restRepository.getResponse(restRequest).getData()
+                .setRequestType(requestType)
+                .setBody(
+                    GraphqlRequest(
+                        TopAdsBannerQuery.GQL_QUERY,
+                        TopAdsBannerResponse::class.java,
+                        mapOf(DISPLAY_BANNER_PARAMS to getQueryMapAsString(queryMap))
+                    )
+                )
+
+                .setHeaders(mapOf(KEY_IRIS_SESSION_ID to sessionId))
+                .setCacheStrategy(RestCacheStrategy.Builder(cacheType).build())
+                .build()
+            val r = restRepository.getResponse(restRequest)
+            return r.getData<DataResponse<TopAdsBannerResponse>>().data as T
         } catch (t: Throwable) {
             throw t
         }
     }
 
-    private fun mapToListOfTopAdsImageViewModel(response: TopAdsmageViewResponse, queryParams: MutableMap<String, Any>): ArrayList<TopAdsImageViewModel> {
+    private fun getQueryMapAsString(queryMap: MutableMap<String, Any>): String {
+        return queryMap.entries.joinToString("&")
+    }
+
+    private fun mapToListOfTopAdsImageViewModel(
+        responseBanner: TopAdsBannerResponse,
+        queryParams: MutableMap<String, Any>
+    ): ArrayList<TopAdsImageViewModel> {
         val list = ArrayList<TopAdsImageViewModel>()
-        response.data?.forEach{ data ->
+        responseBanner.topadsDisplayBannerAdsV3.bannerListData?.forEach { data ->
             val model = TopAdsImageViewModel()
-            val image = getImageById(data?.banner?.images, queryParams[DIMEN_ID] as? Int)
+            val image = getImageById(data.banner?.images, queryParams[DIMEN_ID].toString())
             with(model) {
-                bannerId = data?.id
-                bannerName = data?.banner?.name ?: ""
-                position = data?.banner?.position ?: 0
-                adClickUrl = data?.adClickUrl ?: ""
-                adViewUrl = data?.adViewUrl ?: ""
-                applink = data?.applinks ?: ""
+                bannerId = data.id
+                bannerName = data.banner?.name ?: ""
+                position = data.banner?.position ?: 0
+                adClickUrl = data.adClickUrl ?: ""
+                adViewUrl = data.adViewUrl ?: ""
+                applink = data.applinks
                 imageUrl = image.first
                 imageWidth = image.second
                 imageHeight = image.third
-                nextPageToken = response.header?.pagination?.nextPageToken
-                shopId = data?.banner?.shop?.id?.toString()?:""
-                currentPage = response.header?.pagination?.currentPage ?: ""
-                kind = response.header?.pagination?.kind ?: ""
+                nextPageToken =
+                    responseBanner.topadsDisplayBannerAdsV3.header?.pagination?.nextPageToken
+                shopId = data.banner?.shop?.shopID?.toString() ?: ""
+                currentPage =
+                    responseBanner.topadsDisplayBannerAdsV3.header?.pagination?.currentPage ?: ""
+                kind = responseBanner.topadsDisplayBannerAdsV3.header?.pagination?.kind ?: ""
             }
             list.add(model)
         }
@@ -75,17 +155,20 @@ open class TopAdsRepository {
         return list
     }
 
-    private fun getImageById(images: List<TopAdsmageViewResponse.Data.Banner.Image?>?, dimenId: Int?): Triple<String, Int, Int> {
+    private fun getImageById(
+        images: List<TopAdsBannerResponse.TopadsDisplayBannerAdsV3.BannerListData.Banner.Image>?,
+        dimenId: String?
+    ): Triple<String, Int, Int> {
         var imageUrl = ""
         var imageWidth = 0
         var imageHeight = 0
 
         images?.let {
             for (image in it) {
-                if (image?.imageDimension?.dimenId == dimenId) {
-                    imageUrl = image?.fullEcs ?: ""
-                    imageWidth = image?.imageDimension?.width ?: 0
-                    imageHeight = image?.imageDimension?.height ?: 0
+                if (image.dimension?.id == dimenId) {
+                    imageUrl = image.url ?: ""
+                    imageWidth = image.dimension?.width ?: 0
+                    imageHeight = image.dimension?.height ?: 0
                     break
                 }
             }
