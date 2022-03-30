99a776e6d80c2b6d63ac0d21c21288363900818a
faisalramd <faisal.ramdani@tokopedia.com>
2022-03-30 16:03:01 +0700

    Fix list input and header variant layout

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/variant/presentation/adapter/viewholder/VariantDetailHeaderViewHolder.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/variant/presentation/adapter/viewholder/VariantDetailHeaderViewHolder.kt
@@ -5,17 +5,22 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.kotlin.extensions.view.isVisible
+import com.tokopedia.product.addedit.R
 import com.tokopedia.product.addedit.variant.presentation.adapter.uimodel.VariantDetailHeaderUiModel
 
 class VariantDetailHeaderViewHolder(itemView: View?, listener: OnCollapsibleHeaderClickListener) : AbstractViewHolder<VariantDetailHeaderUiModel>(itemView) {
 
     private var unitValueHeader: AppCompatTextView? = null
     private var accordionIndicator: AppCompatImageView? = null
+    private var divTop: View? = null
     private var headerPosition = 0
 
     init {
-        unitValueHeader = itemView?.findViewById(com.tokopedia.product.addedit.R.id.tv_unit_value_header)
-        accordionIndicator = itemView?.findViewById(com.tokopedia.product.addedit.R.id.iv_accordion_indicator)
+        unitValueHeader = itemView?.findViewById(R.id.tv_unit_value_header)
+        accordionIndicator = itemView?.findViewById(R.id.iv_accordion_indicator)
+        divTop = itemView?.findViewById(R.id.div_top_item)
 
         itemView?.setOnClickListener {
             val isCollapsed = listener.onHeaderClicked(headerPosition)
@@ -28,21 +33,22 @@ class VariantDetailHeaderViewHolder(itemView: View?, listener: OnCollapsibleHead
             headerPosition = element.position
             unitValueHeader?.text = element.headerTitle
             changeIndicatorIcon(element.isCollapsed)
+            divTop?.isVisible = adapterPosition != Int.ZERO
         }
     }
 
     private fun changeIndicatorIcon(isCollapsed: Boolean) {
         accordionIndicator?.setImageResource(
             if (isCollapsed)
-                com.tokopedia.product.addedit.R.drawable.product_add_edit_ic_chevron_up
+                R.drawable.product_add_edit_ic_chevron_down
             else
-                com.tokopedia.product.addedit.R.drawable.product_add_edit_ic_chevron_down
+                R.drawable.product_add_edit_ic_chevron_up
         )
     }
 
     companion object {
         @LayoutRes
-        val LAYOUT = com.tokopedia.product.addedit.R.layout.product_variant_detail_header_layout
+        val LAYOUT = R.layout.product_variant_detail_header_layout
     }
 
     interface OnCollapsibleHeaderClickListener {
