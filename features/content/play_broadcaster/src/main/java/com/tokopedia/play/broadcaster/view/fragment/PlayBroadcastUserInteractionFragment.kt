dbc498511fc3e1757b30df9c709d8fa7710c9523
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-03-31 10:56:04 +0700

    fix bug back button on quiz

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/fragment/PlayBroadcastUserInteractionFragment.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/fragment/PlayBroadcastUserInteractionFragment.kt
@@ -383,7 +383,7 @@ class PlayBroadcastUserInteractionFragment @Inject constructor(
                 true
             }
             isQuizFormVisible() -> {
-                showQuizForm(false)
+                parentViewModel.submitAction(PlayBroadcastAction.ClickBackOnQuiz)
                 true
             }
             /** TODO: gonna delete this */
