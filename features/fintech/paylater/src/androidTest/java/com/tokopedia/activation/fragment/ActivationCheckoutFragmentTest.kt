1b1da622e433d35e4e1001ed634b0a0ba8d8e809
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-03-31 12:25:04 +0530

    addded the test for the 1 st page impression

--- features/fintech/paylater/src/androidTest/java/com/tokopedia/activation/fragment/ActivationCheckoutFragmentTest.kt
+++ features/fintech/paylater/src/androidTest/java/com/tokopedia/activation/fragment/ActivationCheckoutFragmentTest.kt
@@ -8,12 +8,12 @@ import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.activation.ActivationCheckoutMockResponseConfig
+import com.tokopedia.activation.analytics.actionTest
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.cassavatest.CassavaTestRule
 import com.tokopedia.pdpsimulation.TkpdIdlingResource
 import com.tokopedia.pdpsimulation.TkpdIdlingResourceProvider
 import com.tokopedia.pdpsimulation.activateCheckout.presentation.activity.OptimizedCheckoutActivity
-import com.tokopedia.pdpsimulation.analytics.actionTest
 import com.tokopedia.pdpsimulation.common.constants.PARAM_GATEWAY_CODE
 import com.tokopedia.pdpsimulation.common.constants.PARAM_GATEWAY_ID
 import com.tokopedia.pdpsimulation.common.constants.PARAM_PRODUCT_ID
@@ -60,7 +60,7 @@ class ActivationCheckoutFragmentTest {
     @Test
     fun check_simulation_test() {
         actionTest {
-
+            waitForData()
         } assertTest {
             hasPassedAnalytics(cassavaTestRule, PAY_LATER_PARTNER_BUTTON_CLICK)
             clearData()
@@ -78,7 +78,7 @@ class ActivationCheckoutFragmentTest {
 
     private fun launchActivity() {
         val bundle = Bundle()
-        bundle.putString(PARAM_GATEWAY_ID, "1000000")
+        bundle.putString(PARAM_GATEWAY_ID, "7")
         bundle.putString(PARAM_PRODUCT_ID, "2147954780")
         bundle.putString(PARAM_PRODUCT_TENURE,"3")
         bundle.putString(PARAM_GATEWAY_CODE,"GOCICIL")
@@ -102,7 +102,7 @@ class ActivationCheckoutFragmentTest {
     companion object {
 
         const val PAY_LATER_PARTNER_BUTTON_CLICK =
-            "tracker/fintech/simulation_paylater.json"
+            "tracker/fintech/activation_paylater.json"
 
 
     }
