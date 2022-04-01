142ea6453dfa8019d8d950c67208d1b4d5026459
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-04-01 14:00:08 +0800

    `refactor` : remove unused component

--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/recommendationdigital/presentation/adapter/listener/DigitalRecommendationViewListener.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.thankyou_native.recommendationdigital.presentation.adapter.listener
-
-import com.tokopedia.thankyou_native.recommendationdigital.model.RecommendationItem
-
-interface DigitalRecommendationViewListener  {
-    fun onDigitalProductClick(item: RecommendationItem, position: Int)
-    fun onDigitalProductImpression(item: RecommendationItem, position: Int)
-
-}
