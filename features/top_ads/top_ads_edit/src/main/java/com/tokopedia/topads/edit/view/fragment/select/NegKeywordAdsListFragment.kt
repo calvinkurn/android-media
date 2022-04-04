46e01950c2c70dacb06894a2bca98a80ec17007a
Ankit <ankit.kumar1@tokopedia.com>
2022-04-04 16:08:54 +0530

    synthetics removed from topads_edit - NegKeywordAdsListFragment

--- features/top_ads/top_ads_edit/src/main/java/com/tokopedia/topads/edit/view/fragment/select/NegKeywordAdsListFragment.kt
+++ features/top_ads/top_ads_edit/src/main/java/com/tokopedia/topads/edit/view/fragment/select/NegKeywordAdsListFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.common.utils.snackbar.SnackbarManager
@@ -21,13 +22,20 @@ import com.tokopedia.topads.edit.utils.Constants.CURRENTLIST
 import com.tokopedia.topads.edit.utils.Constants.RESTORED_DATA
 import com.tokopedia.topads.edit.utils.Constants.SELECTED_KEYWORD
 import com.tokopedia.topads.edit.view.adapter.neg_keyword.NegKeywordListAdapter
-import kotlinx.android.synthetic.main.topads_edit_select_negative_keyword_list_layout.*
+import com.tokopedia.unifycomponents.TextFieldUnify
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Pika on 13/4/20.
  */
 class NegKeywordAdsListFragment : BaseDaggerFragment() {
 
+    private var editText: TextFieldUnify? = null
+    private var addBtn: UnifyButton? = null
+    private var keywordList: RecyclerView? = null
+    private var btnNext: UnifyButton? = null
+    private var selectedInfo: Typography? = null
 
     private lateinit var adapter: NegKeywordListAdapter
     private var currentList: ArrayList<String> = arrayListOf()
@@ -38,7 +46,6 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
             val fragment = NegKeywordAdsListFragment()
             fragment.arguments = extras
             return fragment
-
         }
     }
 
@@ -50,8 +57,18 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
         getComponent(TopAdsEditComponent::class.java).inject(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(resources.getLayout(R.layout.topads_edit_select_negative_keyword_list_layout), container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+        val view =
+            inflater.inflate(resources.getLayout(R.layout.topads_edit_select_negative_keyword_list_layout),
+                container, false)
+        editText = view.findViewById(R.id.editText)
+        addBtn = view.findViewById(R.id.add_btn)
+        keywordList = view.findViewById(R.id.keyword_list)
+        btnNext = view.findViewById(R.id.btn_next)
+        selectedInfo = view.findViewById(R.id.selected_info)
+        return view
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,21 +84,21 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        add_btn.isEnabled = false
+        addBtn?.isEnabled = false
         val data = arguments?.getParcelableArrayList<GetKeywordResponse.KeywordsItem>(RESTORED_DATA)
         currentList = arguments?.getStringArrayList(CURRENTLIST)!!
         formatRestoredData(data)
 
-        add_btn.setOnClickListener {
-            if (keywordValidation(editText.textFieldInput.text.toString().trim())) {
-                adapter.addKeyword(editText.textFieldInput.text.toString().trim())
+        addBtn?.setOnClickListener {
+            if (keywordValidation(editText?.textFieldInput?.text.toString().trim())) {
+                adapter.addKeyword(editText?.textFieldInput?.text.toString().trim())
                 onCheckedItem()
             }
-            add_btn?.isEnabled = false
-            editText.textFieldInput.text.clear()
+            addBtn?.isEnabled = false
+            editText?.textFieldInput?.text?.clear()
         }
 
-        btn_next.setOnClickListener {
+        btnNext?.setOnClickListener {
             val intent = Intent()
             intent.putParcelableArrayListExtra(SELECTED_KEYWORD, getSelectedList())
             intent.putParcelableArrayListExtra(RESTORED_DATA, getData())
@@ -89,13 +106,13 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
             activity?.finish()
         }
 
-        keyword_list.adapter = adapter
-        keyword_list.layoutManager = LinearLayoutManager(context)
-        editText.textFieldInput?.addTextChangedListener(object : TextWatcher {
+        keywordList?.adapter = adapter
+        keywordList?.layoutManager = LinearLayoutManager(context)
+        editText?.textFieldInput?.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val text = validateKeyword(s)
                 if (s.toString().trim().isEmpty()) {
-                    add_btn.isEnabled = false
+                    addBtn?.isEnabled = false
                 } else if (!text.isNullOrBlank()) {
                     setValues(false, text)
                 } else {
@@ -109,10 +126,10 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
         })
-        editText.textFieldInput?.setOnEditorActionListener { v, actionId, _ ->
+        editText?.textFieldInput?.setOnEditorActionListener { v, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                if (keywordValidation(editText.textFieldInput.text.toString().trim())) {
-                    adapter.addKeyword(editText.textFieldInput.text.toString().trim())
+                if (keywordValidation(editText?.textFieldInput?.text.toString().trim())) {
+                    adapter.addKeyword(editText?.textFieldInput?.text.toString().trim())
                     onCheckedItem()
                 }
                 Utils.dismissKeyboard(context, v)
@@ -134,25 +151,26 @@ class NegKeywordAdsListFragment : BaseDaggerFragment() {
 
     private fun onCheckedItem() {
         val count = adapter.getSelectedList().size
-        selected_info.text = String.format(getString(R.string.format_selected_keyword), count)
+        selectedInfo?.text = String.format(getString(R.string.format_selected_keyword), count)
     }
 
     private fun setValues(flag: Boolean, text: CharSequence) {
-        add_btn.isEnabled = flag
-        editText.textFieldInput?.imeOptions = if (flag) EditorInfo.IME_ACTION_NEXT else EditorInfo.IME_ACTION_NONE
-        if(flag) {
-            editText.setError(false)
-            editText.setMessage(text)
+        addBtn?.isEnabled = flag
+        editText?.textFieldInput?.imeOptions =
+            if (flag) EditorInfo.IME_ACTION_NEXT else EditorInfo.IME_ACTION_NONE
+        if (flag) {
+            editText?.setError(false)
+            editText?.setMessage(text)
         } else {
-            editText.setError(true)
-            editText.setMessage(text)
+            editText?.setError(true)
+            editText?.setMessage(text)
         }
     }
 
     private fun makeToast(s: String) {
         SnackbarManager.make(activity, s,
-                Snackbar.LENGTH_LONG)
-                .show()
+            Snackbar.LENGTH_LONG)
+            .show()
     }
 
     private fun validateKeyword(text: CharSequence?): CharSequence? {
