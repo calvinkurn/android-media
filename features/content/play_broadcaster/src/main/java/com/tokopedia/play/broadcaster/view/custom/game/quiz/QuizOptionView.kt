28c35e3824773960a66e37fbc1b000a8fdf3d5ff
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-04-04 13:47:59 +0700

    add attribute for quiz option

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/game/quiz/QuizOptionView.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/game/quiz/QuizOptionView.kt
@@ -1,10 +1,15 @@
 package com.tokopedia.play.broadcaster.view.custom.game.quiz
 
 import android.content.Context
+import android.text.InputFilter
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.tokopedia.kotlin.extensions.view.showWithCondition
+import com.tokopedia.play.broadcaster.R
 import com.tokopedia.play.broadcaster.databinding.ViewQuizOptionBinding
+import com.tokopedia.play.broadcaster.ui.model.game.quiz.QuizFormDataUiModel
 
 /**
  * Created By : Jonathan Darwin on April 04, 2022
@@ -30,4 +35,56 @@ class QuizOptionView : ConstraintLayout {
         this,
         true,
     )
+
+    var maxLength: Int = 0
+        set(value) {
+            if(field != value) {
+                field = value
+
+                binding.etQuizOption.filters = arrayOf(InputFilter.LengthFilter(maxLength))
+            }
+        }
+
+    var isEditable: Boolean = true
+        set(value) {
+            field = value
+
+            binding.etQuizOption.apply {
+                isFocusable = value
+                isFocusableInTouchMode = value
+                isEnabled = value
+            }
+        }
+
+    var isCorrect: Boolean = false
+        set(value) {
+            if(field != value) {
+                field = value
+                binding.apply {
+                    tvQuizOptionChoice.showWithCondition(!value)
+                    icQuizOptionChecked.showWithCondition(value)
+
+                    root.background = ContextCompat.getDrawable(
+                        context,
+                        if(value) R.drawable.bg_quiz_option
+                        else R.drawable.bg_quiz_option_selected
+                    )
+
+                    etQuizOption.setTextColor(
+                        ContextCompat.getColor(
+                            context,
+                            if(value) R.color.Unify_Static_White
+                            else R.color.Unify_NN950
+                        )
+                    )
+                }
+
+            }
+        }
+
+    var text: String = ""
+        set(value) {
+            field = value
+            binding.etQuizOption.setText(value)
+        }
 }
