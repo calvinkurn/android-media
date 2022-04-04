0ad4643312385f3f400926d7c072dd39c57a1037
Ankit <ankit.kumar1@tokopedia.com>
2022-04-04 15:28:10 +0530

    synthetics removed from topads_edit module

--- features/top_ads/top_ads_edit/src/main/java/com/tokopedia/topads/edit/view/adapter/keyword/KeywordSelectedAdapter.kt
+++ features/top_ads/top_ads_edit/src/main/java/com/tokopedia/topads/edit/view/adapter/keyword/KeywordSelectedAdapter.kt
@@ -9,20 +9,21 @@ import com.tokopedia.topads.common.data.util.Utils
 import com.tokopedia.topads.edit.R
 import com.tokopedia.topads.edit.view.adapter.keyword.viewholder.KeywordItemViewHolder
 import com.tokopedia.unifycomponents.Label
-import kotlinx.android.synthetic.main.topads_edit_layout_keyword_list_item.view.*
+import com.tokopedia.unifycomponents.selectioncontrol.CheckboxUnify
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Pika on 23/8/20.
  */
 
-class KeywordSelectedAdapter(private val onChecked: ((position: Int) -> Unit)) : RecyclerView.Adapter<KeywordSelectedAdapter.ViewHolder>() {
+class KeywordSelectedAdapter(private val onChecked: ((position: Int) -> Unit)) :
+    RecyclerView.Adapter<KeywordSelectedAdapter.ViewHolder>() {
 
     var items: MutableList<KeywordDataItem> = mutableListOf()
 
-    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.topads_edit_layout_keyword_list_item, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.topads_edit_layout_keyword_list_item, parent, false)
         return ViewHolder(v)
     }
 
@@ -30,55 +31,61 @@ class KeywordSelectedAdapter(private val onChecked: ((position: Int) -> Unit)) :
         return items.count()
     }
 
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.view.keyword_name.text = items[holder.adapterPosition].keyword
+        holder.keywordName.text = items[holder.adapterPosition].keyword
         try {
             if (items[holder.adapterPosition].totalSearch == "-1") {
-                holder.view.keyword_count.text = "  -  "
+                holder.keywordCount.text = "  -  "
 
             } else
-                holder.view.keyword_count.text = Utils.convertToCurrencyString(items[position].totalSearch.toLong())
+                holder.keywordCount.text =
+                    Utils.convertToCurrencyString(items[position].totalSearch.toLong())
         } catch (e: Exception) {
-            holder.view.keyword_count.text = items[holder.adapterPosition].totalSearch.toString()
+            holder.keywordCount.text = items[holder.adapterPosition].totalSearch.toString()
         }
-        holder.view.checkBox.setOnCheckedChangeListener(null)
-        holder.view.checkBox.isChecked = true
+        holder.checkBox.setOnCheckedChangeListener(null)
+        holder.checkBox.isChecked = true
         holder.view.setOnClickListener {
-            holder.view.checkBox.isChecked = false
+            holder.checkBox.isChecked = false
         }
-        holder.view.checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+        holder.checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
             if (holder.adapterPosition != RecyclerView.NO_POSITION) {
                 items[holder.adapterPosition].onChecked = isChecked
                 onChecked.invoke(holder.adapterPosition)
             }
-
         }
         if (items[holder.adapterPosition].competition.isEmpty()) {
-            holder.view.keywordCompetition.setLabelType(Label.GENERAL_DARK_GREY)
-            holder.view.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_unknown))
+            holder.keywordCompetition.setLabelType(Label.GENERAL_DARK_GREY)
+            holder.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_unknown))
 
         } else {
-            holder.view.keywordCompetition.visibility = View.VISIBLE
+            holder.keywordCompetition.visibility = View.VISIBLE
             when (items[holder.adapterPosition].competition) {
                 KeywordItemViewHolder.LOW -> {
-                    holder.view.keywordCompetition.setLabelType(Label.GENERAL_DARK_GREEN)
-                    holder.view.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_low))
+                    holder.keywordCompetition.setLabelType(Label.GENERAL_DARK_GREEN)
+                    holder.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_low))
                 }
 
                 KeywordItemViewHolder.MEDIUM -> {
-                    holder.view.keywordCompetition.setLabelType(Label.GENERAL_DARK_ORANGE)
-                    holder.view.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_moderation))
+                    holder.keywordCompetition.setLabelType(Label.GENERAL_DARK_ORANGE)
+                    holder.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_moderation))
                 }
 
                 KeywordItemViewHolder.HIGH -> {
-                    holder.view.keywordCompetition.setLabelType(Label.GENERAL_DARK_RED)
-                    holder.view.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_high))
+                    holder.keywordCompetition.setLabelType(Label.GENERAL_DARK_RED)
+                    holder.keywordCompetition.setLabel(holder.view.resources.getString(R.string.topads_common_keyword_competition_high))
                 }
 
             }
         }
+    }
+
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val keywordName: Typography = view.findViewById(R.id.keyword_name)
+        val keywordCount: Typography = view.findViewById(R.id.keyword_count)
+        val keywordCompetition: Label = view.findViewById(R.id.keywordCompetition)
+        val checkBox: CheckboxUnify = view.findViewById(R.id.checkBox)
     }
 
 }
