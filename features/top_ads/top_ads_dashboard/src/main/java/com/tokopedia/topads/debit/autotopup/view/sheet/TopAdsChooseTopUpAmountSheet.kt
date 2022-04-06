2662636fb2aa6aa6b16d8bf1490f456820ef204c
Ankit <ankit.kumar1@tokopedia.com>
2022-04-06 16:49:13 +0530

    synthetic removed : topads dashboard - TopAdsChooseTopUpAmountSheet

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/debit/autotopup/view/sheet/TopAdsChooseTopUpAmountSheet.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/debit/autotopup/view/sheet/TopAdsChooseTopUpAmountSheet.kt
@@ -5,20 +5,29 @@ import android.text.Html
 import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.topads.common.data.util.Utils
 import com.tokopedia.topads.dashboard.R
 import com.tokopedia.topads.dashboard.data.utils.Utils.calculatePercentage
 import com.tokopedia.topads.debit.autotopup.data.model.AutoTopUpStatus
 import com.tokopedia.topads.debit.autotopup.view.adapter.TopAdsAutoTopUpChipsAdapter
 import com.tokopedia.unifycomponents.BottomSheetUnify
-import kotlinx.android.synthetic.main.topads_dash_choose_auto_topup_sheet_layout.*
-import kotlinx.android.synthetic.main.topads_dash_choose_topup_layout.*
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Pika on 23/9/20.
  */
 
 class TopAdsChooseTopUpAmountSheet : BottomSheetUnify() {
+
+    private var saveBtn: UnifyButton? = null
+    private var cancelBtn: UnifyButton? = null
+    private var creditOptionsRV: RecyclerView? = null
+    private var bonusTxt: Typography? = null
+    private var tooltip: ImageUnify? = null
+    private var dedAmount: Typography? = null
 
     private var data: AutoTopUpStatus? = null
     var onCancel: (() -> Unit)? = null
@@ -39,14 +48,24 @@ class TopAdsChooseTopUpAmountSheet : BottomSheetUnify() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val childView = View.inflate(context, R.layout.topads_dash_choose_auto_topup_sheet_layout, null)
+        val childView =
+            View.inflate(context, R.layout.topads_dash_choose_auto_topup_sheet_layout, null)
         setChild(childView)
+        initView(childView)
         setTitle(getString(R.string.topads_dash_topads_auto_topup_bottomsheet_title))
         showCloseIcon = false
     }
 
-    fun show(fragmentManager: FragmentManager,
-             data: AutoTopUpStatus?, bonus: Double) {
+    private fun initView(childView: View) {
+        saveBtn = childView.findViewById(R.id.saveBtn)
+        cancelBtn = childView.findViewById(R.id.cancelBtn)
+        creditOptionsRV = childView.findViewById(R.id.creditOptionsRV)
+        bonusTxt = childView.findViewById(R.id.bonusTxt)
+        tooltip = childView.findViewById(R.id.tooltip)
+        dedAmount = childView.findViewById(R.id.dedAmount)
+    }
+
+    fun show(fragmentManager: FragmentManager, data: AutoTopUpStatus?, bonus: Double) {
         this.data = data
         this.bonus = bonus
         show(fragmentManager, TOPADS_BOTTOM_SHEET_TAG)
@@ -76,7 +95,11 @@ class TopAdsChooseTopUpAmountSheet : BottomSheetUnify() {
         adapter?.setListener(object : TopAdsAutoTopUpChipsAdapter.OnCreditOptionItemClicked {
             override fun onItemClicked(position: Int) {
                 data?.let {
-                    bonusTxt.text = Html.fromHtml(String.format(getString(R.string.topads_dash_bonus_String_bottomsheet), "$bonus%", Utils.convertToCurrency(calculatePercentage(it?.availableNominals[position].priceFmt, bonus).toLong())))
+                    bonusTxt?.text =
+                        Html.fromHtml(String.format(getString(R.string.topads_dash_bonus_String_bottomsheet),
+                            "$bonus%",
+                            Utils.convertToCurrency(calculatePercentage(it?.availableNominals[position].priceFmt,
+                                bonus).toLong())))
                     dedAmount?.text = it.availableNominals[position].minCreditFmt
                 }
             }
@@ -94,9 +117,12 @@ class TopAdsChooseTopUpAmountSheet : BottomSheetUnify() {
     private fun setInitialState() {
         /*def should be 200k*/
         adapter?.setSelected()
-        bonusTxt.text = Html.fromHtml(String.format(getString(R.string.topads_dash_bonus_String_bottomsheet), "$bonus%", Utils.convertToCurrency(calculatePercentage(data?.availableNominals?.get(defIndex)?.priceFmt
-                ?: "1", bonus)
-                .toLong())))
+        bonusTxt?.text =
+            Html.fromHtml(String.format(getString(R.string.topads_dash_bonus_String_bottomsheet),
+                "$bonus%",
+                Utils.convertToCurrency(calculatePercentage(data?.availableNominals?.get(defIndex)?.priceFmt
+                    ?: "1", bonus)
+                    .toLong())))
         dedAmount?.text = data?.availableNominals?.get(defIndex)?.minCreditFmt
 
     }
