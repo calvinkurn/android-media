61dd7492a932fd63cf8d7bab8f5b26f697d3d88e
hansenpw <hansen.wijaya@tokopedia.com>
2022-04-06 17:31:07 +0700

    fix weird checkbox checked state issue

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/TypographySwitcherViewHolder.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/TypographySwitcherViewHolder.kt
@@ -2,7 +2,6 @@ package com.tokopedia.developer_options.presentation.viewholder
 
 import android.app.Activity
 import android.view.View
-import android.widget.CheckBox
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.developer_options.R
@@ -12,7 +11,7 @@ import com.tokopedia.unifyprinciples.Typography
 
 class TypographySwitcherViewHolder (view: View): AbstractViewHolder<TypographySwitchUiModel>(view) {
 
-    private val checkbox = itemView.findViewById<CheckBox>(R.id.dev_opt_new_typography)
+    private val checkbox = itemView.findViewById<CheckboxUnify>(R.id.dev_opt_new_typography)
 
     override fun bind(element: TypographySwitchUiModel?) {
         checkbox.isChecked = Typography.isFontTypeOpenSauceOne
