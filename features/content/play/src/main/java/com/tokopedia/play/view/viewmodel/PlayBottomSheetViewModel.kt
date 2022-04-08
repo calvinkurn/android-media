fbc47ff3c918b0e9110a2c46b88c69026ab00dc6
astidhiyaa <asti.anzaria@tokopedia.com>
2022-04-08 12:14:25 +0800

    fix: remove unused code

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayBottomSheetViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayBottomSheetViewModel.kt
@@ -4,22 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
-import com.tokopedia.play.data.CartFeedbackResponseModel
-import com.tokopedia.play.domain.repository.PlayViewerRepository
-import com.tokopedia.play.view.type.*
-import com.tokopedia.play.view.uimodel.CartFeedbackUiModel
-import com.tokopedia.play.view.uimodel.PlayProductUiModel
-import com.tokopedia.play.view.uimodel.VariantSheetUiModel
-import com.tokopedia.play.view.uimodel.recom.tagitem.ProductSectionUiModel
 import com.tokopedia.play.view.wrapper.InteractionEvent
 import com.tokopedia.play.view.wrapper.LoginStateEvent
-import com.tokopedia.play.view.wrapper.PlayResult
 import com.tokopedia.play_common.util.event.Event
 import com.tokopedia.user.session.UserSessionInterface
 import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -27,12 +16,7 @@ import javax.inject.Inject
  */
 class PlayBottomSheetViewModel @Inject constructor(
     private val userSession: UserSessionInterface,
-    private val dispatchers: CoroutineDispatchers,
-    private val repo: PlayViewerRepository,
 ) : ViewModel() {
-
-    private val _observableAddToCart = MutableLiveData<PlayResult<Pair<Event<CartFeedbackUiModel>, ProductSectionUiModel.Section>>>()
-    private val _observableProductVariant = MutableLiveData<PlayResult<VariantSheetUiModel>>()
 
     private val _observableLoggedInInteractionEvent = MutableLiveData<Event<LoginStateEvent>>()
     val observableLoggedInInteractionEvent: LiveData<Event<LoginStateEvent>> = _observableLoggedInInteractionEvent
@@ -47,17 +31,4 @@ class PlayBottomSheetViewModel @Inject constructor(
     fun onFreezeBan() {
         viewModelScope.coroutineContext.cancelChildren()
     }
-
-    private fun mappingResponseCart(response: CartFeedbackResponseModel,
-                                    product: PlayProductUiModel.Product,
-                                    action: ProductAction,
-                                    bottomInsetsType: BottomInsetsType) =
-            CartFeedbackUiModel(
-                    isSuccess = response.isSuccess,
-                    errorMessage = response.errorMessage,
-                    cartId = response.cartId,
-                    product = product,
-                    action = action,
-                    bottomInsetsType = bottomInsetsType
-            )
 }
