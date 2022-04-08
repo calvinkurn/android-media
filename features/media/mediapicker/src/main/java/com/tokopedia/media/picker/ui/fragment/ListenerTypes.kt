429fb38afe83fec5f32bed468e4455e79e5677f4
isfaaghyth <isfaaghyth@gmail.com>
2022-04-09 01:37:07 +0700

    restricted visibilities of callback (code review from: https://github.com/tokopedia/android-tokopedia-core/pull/23834#discussion_r844895006)

--- features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/fragment/ListenerTypes.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/fragment/ListenerTypes.kt
@@ -3,6 +3,6 @@ package com.tokopedia.media.picker.ui.fragment
 import com.tokopedia.picker.common.uimodel.AlbumUiModel
 import com.tokopedia.picker.common.uimodel.MediaUiModel
 
-typealias OnAlbumClickListener = (AlbumUiModel) -> Unit
-typealias OnMediaClickListener = (MediaUiModel, Boolean) -> Boolean
-typealias OnMediaSelectedListener = (List<MediaUiModel>) -> Unit
+internal typealias OnAlbumClickListener = (AlbumUiModel) -> Unit
+internal typealias OnMediaClickListener = (MediaUiModel, Boolean) -> Boolean
+internal typealias OnMediaSelectedListener = (List<MediaUiModel>) -> Unit
