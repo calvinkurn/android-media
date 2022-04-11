4b5bb357732dc3a85756580a07cc015b529daee5
XORByte29 <rivaldy.firmansyah@tokopedia.com>
2022-04-11 12:46:36 +0700

    merge base class

--- features/user/home_account/src/androidTest/java/com/tokopedia/home_account/explicitprofile/ExplicitProfileUiTest.kt
+++ features/user/home_account/src/androidTest/java/com/tokopedia/home_account/explicitprofile/ExplicitProfileUiTest.kt
@@ -1,13 +1,47 @@
 package com.tokopedia.home_account.explicitprofile
 
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.cassavatest.CassavaTestRule
+import com.tokopedia.graphql.data.GraphqlClient
+import com.tokopedia.home_account.explicitprofile.fakes.ExplicitProfileInterceptor
+import com.tokopedia.home_account.explicitprofile.features.ExplicitProfileActivity
 import com.tokopedia.test.application.annotations.CassavaTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @CassavaTest
 @RunWith(AndroidJUnit4::class)
-class ExplicitProfileUiTest : BaseExplicitProfileTest() {
+class ExplicitProfileUiTest {
+
+    @get:Rule
+    var activityRule = IntentsTestRule(
+        ExplicitProfileActivity::class.java, false, false
+    )
+
+    @get:Rule
+    var cassavaRule = CassavaTestRule()
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Before
+    fun setup() {
+        GraphqlClient.reInitRetrofitWithInterceptors(
+            listOf(ExplicitProfileInterceptor(context)),
+            context
+        )
+
+        activityRule.launchActivity(null)
+    }
+
+    @After
+    fun tearDown() {
+        activityRule.finishActivity()
+    }
 
     @Test
     fun selectTabKategoriLain() {
