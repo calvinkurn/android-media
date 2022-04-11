aaa7d33dd33cdc0aa0dccc86cd84cb99718f775e
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-04-11 10:57:39 +0700

    address comment : fix GameSmallWidgetView.kt

--- features/content/play_common/src/main/java/com/tokopedia/play_common/view/game/GameSmallWidgetView.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/view/game/GameSmallWidgetView.kt
@@ -41,41 +41,6 @@ class GameSmallWidgetView : FrameLayout {
             binding.tvEngagementDesc.text = value
         }
 
-    private fun setBackground(){
-        val drawableValue = when(type){
-            PlayGameType.Quiz -> R.drawable.bg_play_quiz_header
-            PlayGameType.TapTap -> R.drawable.bg_play_quiz_header
-            else -> R.drawable.bg_play_quiz_header
-        }
-        val drawableType = ContextCompat.getDrawable(context, drawableValue)
-        drawableType?.alpha = 178
-
-        binding.root.background = drawableType
-    }
-
-    private fun setQuizIcon(){
-        val (icon, color) = when(type){
-            PlayGameType.Quiz -> Pair(IconUnify.QUIZ, R.color.play_bro_quiz_icon_color)
-            PlayGameType.TapTap -> Pair(IconUnify.GIFT, R.color.play_bro_taptap_icon_color)
-            else -> Pair(IconUnify.QUIZ, R.color.play_bro_quiz_icon_color)
-        }
-
-        val iconType = getIconUnifyDrawable(context = context, iconId = icon, assetColor = color)
-
-        binding.ivEngagementTools.setImageDrawable(iconType)
-    }
-
-    private fun setTimerType(){
-        val (timerVariant, timerInfo) = when(type){
-            PlayGameType.Quiz -> Pair(TimerUnifySingle.VARIANT_MAIN, R.string.play_common_widget_interactive_end)
-            PlayGameType.TapTap -> Pair(TimerUnifySingle.VARIANT_GENERAL, R.string.play_common_widget_interactive_start)
-            else -> Pair(TimerUnifySingle.VARIANT_MAIN, R.string.play_common_widget_interactive_end)
-        }
-
-        binding.timerEngagementTools.timerVariant = timerVariant
-        binding.tvEngagementTimerInfo.text = context.getString(timerInfo)
-    }
-
     fun setTimer(duration: Long, onFinished: () -> Unit){
         val calendar = Calendar.getInstance()
         calendar.add(Calendar.MILLISECOND, duration.toInt())
