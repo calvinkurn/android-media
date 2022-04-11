3aebad618f5fe286df96ec8b5042ba45f90511a4
firmankagami <firmanda.nugroho@tokopedia.com>
2022-04-11 11:03:17 +0700

    [Feature] Add TokoFood Icons

--- features/tokofood/src/main/java/com/tokopedia/tokofood/home/presentation/adapter/viewholder/TokoFoodIconsViewHolder.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/home/presentation/adapter/viewholder/TokoFoodIconsViewHolder.kt
@@ -1,21 +1,86 @@
 package com.tokopedia.tokofood.home.presentation.adapter.viewholder
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.media.loader.loadImage
 import com.tokopedia.tokofood.R
-import com.tokopedia.tokofood.home.presentation.uimodel.TokoFoodFakeTabUiModel
+import com.tokopedia.tokofood.home.presentation.uimodel.TokoFoodIcon
+import com.tokopedia.tokofood.home.presentation.uimodel.TokoFoodIconsUiModel
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifyprinciples.Typography
 
 class TokoFoodIconsViewHolder(
     itemView: View
-): AbstractViewHolder<TokoFoodFakeTabUiModel>(itemView) {
+): AbstractViewHolder<TokoFoodIconsUiModel>(itemView) {
 
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.item_tokofood_icons
+
+        private const val GRID_ITEM = 4
     }
 
-    override fun bind(element: TokoFoodFakeTabUiModel) {
+    private val adapter = TokoFoodIconAdapter()
+    private var iconRecyclerView: RecyclerView? = null
 
+    override fun bind(element: TokoFoodIconsUiModel) {
+        setupTokoFoodIcon(element)
+    }
+
+    private fun setupTokoFoodIcon(element: TokoFoodIconsUiModel){
+        val icons = element.listIcons
+        iconRecyclerView = itemView.findViewById(R.id.tokofood_icon_recycler_view)
+        if (icons.isNotEmpty()){
+            adapter.submitList(element)
+        }
+        iconRecyclerView?.adapter = adapter
+        iconRecyclerView?.layoutManager = GridLayoutManager(itemView.context, GRID_ITEM, GridLayoutManager.VERTICAL, false)
+    }
+
+    internal inner class TokoFoodIconAdapter: RecyclerView.Adapter<TokoFoodIconViewHolder>() {
+        private val iconList = mutableListOf<TokoFoodIcon>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TokoFoodIconViewHolder {
+            return TokoFoodIconViewHolder(
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_tokofood_icon, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: TokoFoodIconViewHolder, position: Int) {
+            iconList.getOrNull(position)?.let {
+                holder.bind(it)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return iconList.size
+        }
+
+        fun submitList(list: TokoFoodIconsUiModel) {
+            iconList.clear()
+            iconList.addAll(list.listIcons)
+        }
+    }
+
+    internal inner class TokoFoodIconViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var imgIcon : ImageUnify? = null
+        var tgIcon : Typography? = null
+
+        fun bind(item: TokoFoodIcon){
+            imgIcon = itemView.findViewById(R.id.img_icon_tokofood_home)
+            tgIcon = itemView.findViewById(R.id.tg_icon_tokofood_home)
+
+            imgIcon?.loadImage(item.imageUrl)
+            tgIcon?.text = item.textIcon
+
+            itemView?.setOnClickListener {
+
+            }
+        }
     }
 }
--- features/tokofood/src/main/java/com/tokopedia/tokofood/home/presentation/uimodel/TokoFoodIconsUiModel.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/home/presentation/uimodel/TokoFoodIconsUiModel.kt
@@ -2,10 +2,18 @@ package com.tokopedia.tokofood.home.presentation.uimodel
 
 import com.tokopedia.tokofood.home.presentation.adapter.TokoFoodHomeTypeFactory
 
-class TokoFoodIconsUiModel(
-    id: String
+data class TokoFoodIconsUiModel(
+    val id: String,
+    val listIcons: List<TokoFoodIcon>
 ): TokoFoodHomeLayoutUiModel(id) {
     override fun type(typeFactory: TokoFoodHomeTypeFactory): Int {
         return typeFactory.type(this)
     }
 }
+
+data class TokoFoodIcon (
+    val id : String = "",
+    val imageUrl: String = "",
+    val textIcon: String = "",
+    val applinkUrl: String = ""
+)
