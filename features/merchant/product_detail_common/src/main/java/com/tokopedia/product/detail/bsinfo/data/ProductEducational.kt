541c9943516b99bee748137934290be8ffcb48c0
yehezkiel <yehezkiel@tokopedia.com>
2022-04-11 15:53:40 +0700

    change key

--- features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/bsinfo/data/ProductEducational.kt
+++ features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/bsinfo/data/ProductEducational.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class ProductEducational(
-        @SerializedName("pdpGetEducationalBottomsheet")
+        @SerializedName("getPDPEducationalBottomsheet")
         @Expose
         val response: ProductEducationalResponse = ProductEducationalResponse()
 )
