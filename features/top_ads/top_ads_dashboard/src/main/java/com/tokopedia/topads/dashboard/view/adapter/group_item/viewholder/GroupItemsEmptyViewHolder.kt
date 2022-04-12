2d2115809e4e901f444d3a963300fa0b31df1990
Ankit <ankit.kumar1@tokopedia.com>
2022-04-12 15:50:55 +0530

    synthetics removed topads dashboard :  GroupItemsEmptyViewHolder

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/adapter/group_item/viewholder/GroupItemsEmptyViewHolder.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/adapter/group_item/viewholder/GroupItemsEmptyViewHolder.kt
@@ -9,13 +9,20 @@ import com.tokopedia.topads.common.data.response.groupitem.DataItem
 import com.tokopedia.topads.dashboard.R
 import com.tokopedia.topads.dashboard.data.model.CountDataItem
 import com.tokopedia.topads.dashboard.view.adapter.group_item.viewmodel.GroupItemsEmptyModel
-import kotlinx.android.synthetic.main.topads_dash_group_empty_state.view.*
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Pika on 2/6/20.
  */
 
 class GroupItemsEmptyViewHolder(val view: View) : GroupItemsViewHolder<GroupItemsEmptyModel>(view) {
+
+    private val imageEmpty: ImageUnify = view.findViewById(R.id.image_empty)
+    private val textTitle: Typography = view.findViewById(R.id.text_title)
+    private val textDesc: Typography = view.findViewById(R.id.text_desc)
+    private val btnSubmit: UnifyButton = view.findViewById(R.id.btn_submit)
 
     companion object {
         @LayoutRes
@@ -24,17 +31,17 @@ class GroupItemsEmptyViewHolder(val view: View) : GroupItemsViewHolder<GroupItem
 
     override fun bind(item: GroupItemsEmptyModel, selectedMode: Boolean, fromSearch: Boolean, statsData: MutableList<DataItem>, countList: MutableList<CountDataItem>) {
         if (!fromSearch) {
-            view.text_title.text = view.context.getString(R.string.topads_dash_empty_group_title)
-            view.text_desc.text = view.context.getString(R.string.topads_dash_empty_group_desc)
-            view.btn_submit.visibility = View.VISIBLE
-            view.btn_submit.setOnClickListener {
+            textTitle.text = view.context.getString(R.string.topads_dash_empty_group_title)
+            textDesc.text = view.context.getString(R.string.topads_dash_empty_group_desc)
+            btnSubmit.visibility = View.VISIBLE
+            btnSubmit.setOnClickListener {
                 RouteManager.route(view.context, ApplinkConstInternalTopAds.TOPADS_CREATE_ADS)
             }
         } else {
-            view.text_title.text = view.context.getString(R.string.topads_dash_group_no_search_result_title)
-            view.text_desc.text = view.context.getString(R.string.topads_empty_on_search_desc)
-            view.btn_submit.visibility = View.GONE
+            textTitle.text = view.context.getString(R.string.topads_dash_group_no_search_result_title)
+            textDesc.text = view.context.getString(R.string.topads_empty_on_search_desc)
+            btnSubmit.visibility = View.GONE
         }
-        view.image_empty.setImageDrawable(view.context.getResDrawable(com.tokopedia.topads.common.R.drawable.no_products))
+        imageEmpty.setImageDrawable(view.context.getResDrawable(com.tokopedia.topads.common.R.drawable.no_products))
     }
 }
