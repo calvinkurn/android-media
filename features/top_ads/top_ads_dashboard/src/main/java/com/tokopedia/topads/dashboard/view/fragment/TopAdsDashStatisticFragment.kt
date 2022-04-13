34e979185a497ef5a1276c36f94a41404e136519
Ankit <ankit.kumar1@tokopedia.com>
2022-04-13 18:14:37 +0530

    synthetics removed: TopAdsDashStatisticFragment

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashStatisticFragment.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashStatisticFragment.kt
@@ -11,6 +11,7 @@ import com.tokopedia.abstraction.base.view.fragment.TkpdBaseV4Fragment
 import com.tokopedia.charts.common.ChartTooltip
 import com.tokopedia.charts.config.LineChartConfig
 import com.tokopedia.charts.model.*
+import com.tokopedia.charts.view.LineChartView
 import com.tokopedia.topads.dashboard.R
 import com.tokopedia.topads.dashboard.data.constant.TopAdsDashboardConstant.CONST_0
 import com.tokopedia.topads.dashboard.data.constant.TopAdsDashboardConstant.CONST_1
@@ -23,8 +24,7 @@ import com.tokopedia.topads.dashboard.data.model.Cell
 import com.tokopedia.topads.dashboard.data.model.DataStatistic
 import com.tokopedia.topads.dashboard.data.model.Summary
 import com.tokopedia.topads.dashboard.data.utils.ChartXAxisLabelFormatter
-import kotlinx.android.synthetic.main.topads_graph_tooltip.view.*
-import kotlinx.android.synthetic.main.topads_statistics_graph_fragment.*
+import com.tokopedia.unifyprinciples.Typography
 import java.text.SimpleDateFormat
 
 /**
@@ -43,7 +43,11 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
 
     override fun getScreenName(): String = javaClass.simpleName
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
         return inflater.inflate(R.layout.topads_statistics_graph_fragment, container, false)
     }
 
@@ -55,7 +59,7 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
             return
         }
         this.cells = dataStatistic.cells
-        with(lineGraphView) {
+        view?.findViewById<LineChartView>(R.id.lineGraphView)?.apply {
             init(getLineChartConfig())
             setDataSets(getLineChartData())
             invalidateChart()
@@ -65,12 +69,20 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
     private fun getCellData(): List<StatsData> {
         return cells.map {
             when {
-                getIndex() == CONST_0 -> StatsData(it.impressionSum.toFloat(), it.impressionSum.toString(), getDate(it))
-                getIndex() == CONST_1 -> StatsData(it.clickSum.toFloat(), it.clickSumFmt, getDate(it))
+                getIndex() == CONST_0 -> StatsData(it.impressionSum.toFloat(),
+                    it.impressionSum.toString(),
+                    getDate(it))
+                getIndex() == CONST_1 -> StatsData(it.clickSum.toFloat(),
+                    it.clickSumFmt,
+                    getDate(it))
                 getIndex() == CONST_2 -> StatsData(it.costSum, it.costSumFmt, getDate(it))
                 getIndex() == CONST_3 -> StatsData(it.grossProfit, it.grossProfitFmt, getDate(it))
-                getIndex() == CONST_4 -> StatsData(it.ctrPercentage, it.ctrPercentageFmt, getDate(it))
-                getIndex() == CONST_5 -> StatsData(it.conversionSum.toFloat(), it.conversionSumFmt, getDate(it))
+                getIndex() == CONST_4 -> StatsData(it.ctrPercentage,
+                    it.ctrPercentageFmt,
+                    getDate(it))
+                getIndex() == CONST_5 -> StatsData(it.conversionSum.toFloat(),
+                    it.conversionSumFmt,
+                    getDate(it))
                 getIndex() == CONST_6 -> StatsData(it.costAvg, it.costAvgFmt, getDate(it))
                 else -> StatsData(it.soldSum, it.soldSumFmt, getDate(it))
             }
@@ -86,11 +98,11 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
         val yAxisLabel = getYAxisLabel()
 
         return LineChartData(
-                chartEntry = chartEntry,
-                yAxisLabel = yAxisLabel,
-                config = LineChartEntryConfigModel(
-                        lineWidth = 1.8f
-                )
+            chartEntry = chartEntry,
+            yAxisLabel = yAxisLabel,
+            config = LineChartEntryConfigModel(
+                lineWidth = 1.8f
+            )
         )
     }
 
@@ -113,7 +125,10 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
                 val xAxisLabels = lineChartData.chartEntry.map { it.xLabel }
                 gridEnabled { false }
                 context?.let {
-                    textColor { ContextCompat.getColor(it, (com.tokopedia.unifyprinciples.R.color.Neutral_N700_96)) }
+                    textColor {
+                        ContextCompat.getColor(it,
+                            (com.tokopedia.unifyprinciples.R.color.Neutral_N700_96))
+                    }
                 }
                 labelFormatter {
                     ChartXAxisLabelFormatter(xAxisLabels)
@@ -124,12 +139,12 @@ abstract class TopAdsDashStatisticFragment : TkpdBaseV4Fragment() {
 
     private fun getLineGraphTooltip(): ChartTooltip {
         return ChartTooltip(context, TOOLTIP_RES_LAYOUT)
-                .setOnDisplayContent { view, data, x, y ->
-                    (data as? LineChartEntry)?.let {
-                        view.tooltip_title.text = it.xLabel
-                        view.tooltip_value.text = it.yLabel
-                    }
+            .setOnDisplayContent { view, data, x, y ->
+                (data as? LineChartEntry)?.let {
+                    view.findViewById<Typography>(R.id.tooltip_title)?.text = it.xLabel
+                    view.findViewById<Typography>(R.id.tooltip_value)?.text = it.yLabel
                 }
+            }
     }
 
 
