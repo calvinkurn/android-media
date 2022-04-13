22372ef0ece9b7696c4ea095c6befaeb109fec44
Ankit <ankit.kumar1@tokopedia.com>
2022-04-13 18:21:33 +0530

    synthetics removed: TopAdsInsightKeyBidFragment

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/insight/TopAdsInsightKeyBidFragment.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/insight/TopAdsInsightKeyBidFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.kotlin.extensions.view.getResDrawable
 import com.tokopedia.topads.dashboard.R
@@ -21,12 +22,20 @@ import com.tokopedia.topads.dashboard.data.utils.Utils
 import com.tokopedia.topads.dashboard.di.TopAdsDashboardComponent
 import com.tokopedia.topads.dashboard.view.adapter.insight.TopAdsInsightBidKeyAdapter
 import com.tokopedia.topads.dashboard.view.sheet.InsightKeyBottomSheet
-import kotlinx.android.synthetic.main.topads_dash_fragment_pos_key_insight.*
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Pika on 21/7/20.
  */
 class TopAdsInsightKeyBidFragment : BaseDaggerFragment() {
+
+    private var insightTitle: Typography? = null
+    private var toolTip: ImageUnify? = null
+    private var insightDesc: Typography? = null
+    private var btnTambah: UnifyButton? = null
+    private var insightListPosKey: RecyclerView? = null
 
     private lateinit var adapter: TopAdsInsightBidKeyAdapter
     private var itemCountCallBack: OnKeywordBidAdded? = null
@@ -52,21 +61,29 @@ class TopAdsInsightKeyBidFragment : BaseDaggerFragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.topads_dash_fragment_pos_key_insight, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+        val view = inflater.inflate(R.layout.topads_dash_fragment_pos_key_insight, container, false)
+        insightTitle = view.findViewById(R.id.insight_title)
+        toolTip = view.findViewById(R.id.toolTip)
+        insightDesc = view.findViewById(R.id.insight_desc)
+        btnTambah = view.findViewById(R.id.btnTambah)
+        insightListPosKey = view.findViewById(R.id.insightListPosKey)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter = TopAdsInsightBidKeyAdapter(this::butttonClicked)
         setView()
-        toolTip.setImageDrawable(context?.getResDrawable(R.drawable.topads_dash_info_tooltip))
-        toolTip.setOnClickListener {
+        toolTip?.setImageDrawable(context?.getResDrawable(R.drawable.topads_dash_info_tooltip))
+        toolTip?.setOnClickListener {
             val sheet = InsightKeyBottomSheet.createInstance(2)
             sheet.show(fragmentManager!!, "")
         }
-        insightListPosKey.adapter = adapter
-        insightListPosKey.layoutManager = LinearLayoutManager(context)
+        insightListPosKey?.adapter = adapter
+        insightListPosKey?.layoutManager = LinearLayoutManager(context)
     }
 
     private fun butttonClicked(data: MutationData) {
@@ -83,27 +100,30 @@ class TopAdsInsightKeyBidFragment : BaseDaggerFragment() {
             totalPotential += (it.data?.get(TopAdsDashboardConstant.INDEX_4)?.value) as Double
         }
         setHeader(totalPotential)
-        btnTambah.setOnClickListener {
+        btnTambah?.setOnClickListener {
             itemCountCallBack?.onButtonClickedBid(mutationList, key
-                    ?: "", dataInsight?.get(key)?.bid?.size ?: 0, true)
+                ?: "", dataInsight?.get(key)?.bid?.size ?: 0, true)
         }
         adapter.notifyDataSetChanged()
     }
 
     private fun setHeader(totalPotential: Double) {
-        insight_title.text = data?.bid?.box?.title
+        insightTitle?.text = data?.bid?.box?.title
         val text = data?.keyword?.box?.desc
-        val withValue = text?.replace(COUNT, dataInsight?.get(key)?.bid?.size.toString())?.replace(VALUE, "+" + Utils.convertToCurrencyString(totalPotential.toLong()))
-        insight_desc.text = Html.fromHtml(withValue)
-        btnTambah.text = data?.bid?.box?.button?.title?.replace(COUNT, dataInsight?.get(key)?.bid?.size.toString())
+        val withValue = text?.replace(COUNT, dataInsight?.get(key)?.bid?.size.toString())
+            ?.replace(VALUE, "+" + Utils.convertToCurrencyString(totalPotential.toLong()))
+        insightDesc?.text = Html.fromHtml(withValue)
+        btnTambah?.text = data?.bid?.box?.button?.title?.replace(COUNT,
+            dataInsight?.get(key)?.bid?.size.toString())
         if (dataInsight?.get(key)?.bid?.size == 0)
-            btnTambah.isEnabled = false
+            btnTambah?.isEnabled = false
     }
 
     private fun getfromArguments() {
         key = arguments?.getString(KEY_INSIGHT)
         data = arguments?.getParcelable(INSIGHT_DATA_HEADER)
-        dataInsight = arguments?.getSerializable(DATA_INSIGHT) as HashMap<String, KeywordInsightDataMain>
+        dataInsight =
+            arguments?.getSerializable(DATA_INSIGHT) as HashMap<String, KeywordInsightDataMain>
     }
 
     override fun onAttach(context: Context) {
@@ -119,7 +139,12 @@ class TopAdsInsightKeyBidFragment : BaseDaggerFragment() {
     }
 
     interface OnKeywordBidAdded {
-        fun onButtonClickedBid(data: List<MutationData>, groupId: String, countToAdd: Int, forAllButton: Boolean)
+        fun onButtonClickedBid(
+            data: List<MutationData>,
+            groupId: String,
+            countToAdd: Int,
+            forAllButton: Boolean,
+        )
     }
 
 }
