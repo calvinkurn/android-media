191013c9e03710f8410add01a598573f48769825
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-04-13 16:38:56 +0800

    avoid wildcard import

--- libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/IssueRegistry.kt
+++ libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/IssueRegistry.kt
@@ -2,7 +2,18 @@ package com.tokopedia.linter
 
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
-import com.tokopedia.linter.detectors.*
+import com.tokopedia.linter.detectors.AnimatedVectorDetector
+import com.tokopedia.linter.detectors.ResourcePackageDetector
+import com.tokopedia.linter.detectors.UnsupportedColorDetector
+import com.tokopedia.linter.detectors.VectorDrawableDetector
+import com.tokopedia.linter.detectors.DimenResourceValueDetector
+import com.tokopedia.linter.detectors.DeprecatedResourceDetector
+import com.tokopedia.linter.detectors.SerializedNameAnnotationDetector
+import com.tokopedia.linter.detectors.AndroidExportedDetector
+import com.tokopedia.linter.detectors.UnifyBackgroundDetector
+import com.tokopedia.linter.detectors.DeprecatedMethodDetector
+import com.tokopedia.linter.detectors.GradientVectorDetector
+import com.tokopedia.linter.detectors.DimenUsageDetector
 import com.tokopedia.linter.detectors.gradle.BannedDependencyDetector.DEPENDENCY_BANNED
 import com.tokopedia.linter.detectors.gradle.DeprecatedDependencyDetector.DEPENDENCY_DEPRECATED
 import com.tokopedia.linter.detectors.gradle.HANSEL_REQUIRED
