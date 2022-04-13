955670529ecf5085053e1284cde5a6be4d9f733e
Abhijeet <72848818+chauhan-abhi-toko@users.noreply.github.com>
2022-04-13 13:09:01 +0530

    Remove unnecessary changes

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/webview/CommonWebViewClient.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/base/view/webview/CommonWebViewClient.java
@@ -1,19 +1,18 @@
 package com.tokopedia.abstraction.base.view.webview;
 
-import static android.app.Activity.RESULT_OK;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import androidx.fragment.app.Fragment;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
-import androidx.fragment.app.Fragment;
+import static android.app.Activity.RESULT_OK;
 @Deprecated
 public class CommonWebViewClient extends WebChromeClient {
 
