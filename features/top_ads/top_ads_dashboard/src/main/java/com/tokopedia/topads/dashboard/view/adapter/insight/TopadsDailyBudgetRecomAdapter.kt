37d4e246605a036f20b25c3b023800b5e4d31914
Ankit <ankit.kumar1@tokopedia.com>
2022-04-13 18:06:57 +0530

    synthetics removed: TopadsDailyBudgetRecomAdapter

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/adapter/insight/TopadsDailyBudgetRecomAdapter.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/adapter/insight/TopadsDailyBudgetRecomAdapter.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.topads.dashboard.view.adapter.insight
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,18 +13,31 @@ import com.tokopedia.topads.dashboard.R
 import com.tokopedia.topads.dashboard.data.constant.TopAdsDashboardConstant.BUDGET_MULTIPLE_FACTOR
 import com.tokopedia.topads.dashboard.data.constant.TopAdsDashboardConstant.RECOMMENDATION_DAILY_MAX_BUDGET
 import com.tokopedia.topads.dashboard.data.model.DataBudget
+import com.tokopedia.unifycomponents.TextFieldUnify
+import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.text.currency.NumberTextWatcher
-import kotlinx.android.synthetic.main.topads_dash_recon_daily_budget_item.view.*
+import com.tokopedia.unifyprinciples.Typography
 
-class TopadsDailyBudgetRecomAdapter(private val userSession: UserSessionInterface, private val onBudgetClicked: ((pos: Int) -> Unit)) : RecyclerView.Adapter<TopadsDailyBudgetRecomAdapter.ViewHolder>() {
+class TopadsDailyBudgetRecomAdapter(
+    private val userSession: UserSessionInterface,
+    private val onBudgetClicked: ((pos: Int) -> Unit),
+) : RecyclerView.Adapter<TopadsDailyBudgetRecomAdapter.ViewHolder>() {
     var items: MutableList<DataBudget> = mutableListOf()
     private var dailyRecommendationModel = mutableListOf<InsightDailyBudgetModel>()
 
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val groupName: Typography = view.findViewById(R.id.groupName)
+        val dailyBudgetValue: Typography = view.findViewById(R.id.daily_budget_value)
+        val recomBudget: Typography = view.findViewById(R.id.recom_budget)
+        val potentialClick: Typography = view.findViewById(R.id.potentialClick)
+        val editBudget: TextFieldUnify = view.findViewById(R.id.editBudget)
+        val buttonSubmitEdit: UnifyButton = view.findViewById(R.id.buttonSubmitEdit)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.topads_dash_recon_daily_budget_item, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.topads_dash_recon_daily_budget_item, parent, false)
         return ViewHolder(v)
     }
 
@@ -37,7 +49,8 @@ class TopadsDailyBudgetRecomAdapter(private val userSession: UserSessionInterfac
         val budgetModel = items[holder.adapterPosition]
         var totalPotentialClick = 0.0
         totalPotentialClick += if (budgetModel.setCurrentBid
-                >= budgetModel.priceDaily) {
+            >= budgetModel.priceDaily
+        ) {
             ((budgetModel.setCurrentBid
                     - budgetModel.priceDaily) / budgetModel.avgBid.toDouble())
         } else {
@@ -48,62 +61,79 @@ class TopadsDailyBudgetRecomAdapter(private val userSession: UserSessionInterfac
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(items[holder.adapterPosition]) {
-            holder.view.buttonSubmitEdit.isLoading = false
-            holder.view.groupName.text = groupName
-            val dailyBudget = convertToCurrency(priceDaily.toLong()) + holder.view.context.getString(com.tokopedia.topads.common.R.string.topads_common_hari_)
-            holder.view.daily_budget_value.text = dailyBudget
-            val recommendationBid = "Rp" + convertToCurrency(suggestedPriceDaily.toLong()) + holder.view.context.getString(com.tokopedia.topads.common.R.string.topads_common_hari_)
-            holder.view.recom_budget.text = recommendationBid
-            holder.view.editBudget?.textFieldInput?.setText(convertToCurrency(suggestedPriceDaily.toLong()))
+            holder.buttonSubmitEdit.isLoading = false
+            holder.groupName.text = groupName
+            val dailyBudget =
+                convertToCurrency(priceDaily.toLong()) + holder.view.context.getString(com.tokopedia.topads.common.R.string.topads_common_hari_)
+            holder.dailyBudgetValue.text = dailyBudget
+            val recommendationBid =
+                "Rp" + convertToCurrency(suggestedPriceDaily.toLong()) + holder.view.context.getString(
+                    com.tokopedia.topads.common.R.string.topads_common_hari_)
+            holder.recomBudget.text = recommendationBid
+            holder.editBudget?.textFieldInput?.setText(convertToCurrency(suggestedPriceDaily.toLong()))
             setCurrentBid = suggestedPriceDaily
-            holder.view.potentialClick.text = String.format(holder.view.context.getString(R.string.topads_dash_potential_click_text), calculatePotentialClick(holder).thousandFormatted())
-            holder.view.buttonSubmitEdit.setOnClickListener {
-                holder.view.buttonSubmitEdit.isLoading = true
+            holder.potentialClick.text =
+                String.format(holder.view.context.getString(R.string.topads_dash_potential_click_text),
+                    calculatePotentialClick(holder).thousandFormatted())
+            holder.buttonSubmitEdit.setOnClickListener {
+                holder.buttonSubmitEdit.isLoading = true
                 onBudgetClicked(holder.adapterPosition)
             }
             setPotensiKlik = calculatePotentialClick(holder).toLong()
             holder.view.addOnImpressionListener(impressHolder) {
                 dailyRecommendationModel.clear()
-                    var dailyBudgetModel = InsightDailyBudgetModel().apply {
-                        id = groupId
-                        name = groupName
-                        dailySuggestedPrice = suggestedPriceDaily
-                        potentialClick = calculatePotentialClick(holder).toLong()
-                    }
-                    dailyRecommendationModel.add(dailyBudgetModel)
-                TopAdsCreateAnalytics.topAdsCreateAnalytics.sendInsightSightDailyProductEcommerceViewEvent(VIEW_DAILY_RECOMMENDATION_PRODUKS, "", dailyRecommendationModel, holder.adapterPosition, userSession.userId)
+                val dailyBudgetModel = InsightDailyBudgetModel().apply {
+                    id = groupId
+                    name = groupName
+                    dailySuggestedPrice = suggestedPriceDaily
+                    potentialClick = calculatePotentialClick(holder).toLong()
+                }
+                dailyRecommendationModel.add(dailyBudgetModel)
+                TopAdsCreateAnalytics.topAdsCreateAnalytics.sendInsightSightDailyProductEcommerceViewEvent(
+                    VIEW_DAILY_RECOMMENDATION_PRODUKS,
+                    "",
+                    dailyRecommendationModel,
+                    holder.adapterPosition,
+                    userSession.userId)
             }
-            holder.view.editBudget?.textFieldInput?.addTextChangedListener(object : NumberTextWatcher(holder.view.editBudget.textFieldInput, "0") {
+            holder.editBudget?.textFieldInput?.addTextChangedListener(object :
+                NumberTextWatcher(holder.editBudget.textFieldInput, "0") {
                 override fun onNumberChanged(number: Double) {
                     super.onNumberChanged(number)
                     items[holder.adapterPosition].setCurrentBid = number
-                    holder.view.potentialClick.text = String.format(holder.view.context.getString(R.string.topads_dash_potential_click_text), calculatePotentialClick(holder).thousandFormatted())
+                    holder.potentialClick.text =
+                        String.format(holder.view.context.getString(R.string.topads_dash_potential_click_text),
+                            calculatePotentialClick(holder).thousandFormatted())
                     setPotensiKlik = calculatePotentialClick(holder).toLong()
                     when {
                         number < items[holder.adapterPosition].suggestedPriceDaily && number > items[holder.adapterPosition].priceDaily -> {
-                            holder.view.buttonSubmitEdit.isEnabled = true
-                            holder.view.editBudget?.setError(false)
-                            holder.view.editBudget?.setMessage(String.format(holder.view.context.getString(R.string.topads_dash_budget_recom_error), suggestedPriceDaily.toInt()))
+                            holder.buttonSubmitEdit.isEnabled = true
+                            holder.editBudget?.setError(false)
+                            holder.editBudget?.setMessage(String.format(holder.view.context.getString(
+                                R.string.topads_dash_budget_recom_error),
+                                suggestedPriceDaily.toInt()))
                         }
                         number < items[holder.adapterPosition].priceDaily -> {
-                            holder.view.editBudget?.setError(true)
-                            holder.view.editBudget?.setMessage(holder.view.context.getString(R.string.topads_dash_product_recomm_min_budget_error))
-                            holder.view.buttonSubmitEdit.isEnabled = false
+                            holder.editBudget?.setError(true)
+                            holder.editBudget?.setMessage(holder.view.context.getString(R.string.topads_dash_product_recomm_min_budget_error))
+                            holder.buttonSubmitEdit.isEnabled = false
                         }
                         number > RECOMMENDATION_DAILY_MAX_BUDGET.toDouble() -> {
-                            holder.view.editBudget?.setError(true)
-                            holder.view.editBudget?.setMessage(holder.view.context.getString(R.string.topads_dash_product_recomm_max_budget_error))
-                            holder.view.buttonSubmitEdit.isEnabled = false
+                            holder.editBudget?.setError(true)
+                            holder.editBudget?.setMessage(holder.view.context.getString(R.string.topads_dash_product_recomm_max_budget_error))
+                            holder.buttonSubmitEdit.isEnabled = false
                         }
                         number.toInt() % BUDGET_MULTIPLE_FACTOR != 0 -> {
-                            holder.view.editBudget?.setError(true)
-                            holder.view.editBudget?.setMessage(String.format(holder.view.context.getString(com.tokopedia.topads.common.R.string.topads_common_error_multiple_50), BUDGET_MULTIPLE_FACTOR))
-                            holder.view.buttonSubmitEdit.isEnabled = false
+                            holder.editBudget?.setError(true)
+                            holder.editBudget?.setMessage(String.format(holder.view.context.getString(
+                                com.tokopedia.topads.common.R.string.topads_common_error_multiple_50),
+                                BUDGET_MULTIPLE_FACTOR))
+                            holder.buttonSubmitEdit.isEnabled = false
                         }
                         else -> {
-                            holder.view.editBudget?.setError(false)
-                            holder.view.editBudget?.setMessage("")
-                            holder.view.buttonSubmitEdit.isEnabled = true
+                            holder.editBudget?.setError(false)
+                            holder.editBudget?.setMessage("")
+                            holder.buttonSubmitEdit.isEnabled = true
                         }
                     }
                 }
