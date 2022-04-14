8966f8757b6d661edff3d8e8ce54e1bff73d3444
Calvin <calvin.kurniawan@tokopedia.com>
2022-04-14 16:05:27 +0700

    update dispatcher & mock data on AlbumViewModelTest

--- features/media/mediapicker/src/test/java/com/tokopedia/media/picker/ui/activity/album/AlbumViewModelTest.kt
+++ features/media/mediapicker/src/test/java/com/tokopedia/media/picker/ui/activity/album/AlbumViewModelTest.kt
@@ -3,46 +3,30 @@ package com.tokopedia.media.picker.ui.activity.album
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.media.picker.data.entity.Album
 import com.tokopedia.media.picker.data.repository.AlbumRepository
-import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchers
+import com.tokopedia.unit.test.rule.CoroutineTestRule
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert.*
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class AlbumViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
-    private val testDispatcher = CoroutineTestDispatchers
+    @get:Rule
+    val coroutineScopeRule = CoroutineTestRule()
+
     private val albumRepository = mockk<AlbumRepository>()
     private val viewModel: AlbumViewModel = AlbumViewModel(
         albumRepository,
-        testDispatcher
+        coroutineScopeRule.dispatchers
     )
-
-    @ExperimentalCoroutinesApi
-    @Before
-    fun setup() {
-        Dispatchers.setMain(CoroutineTestDispatchers.main)
-    }
-
-    @ExperimentalCoroutinesApi
-    @After
-    fun reset() {
-        Dispatchers.resetMain()
-    }
 
     @Test
     fun `get device album list`() {
-        // Given
-
         // When
         coEvery { albumRepository.invoke(Unit) } returns albumCollection
 
@@ -50,14 +34,12 @@ class AlbumViewModelTest {
         val result = viewModel.albums.value
 
         // Then
-        assertEquals(albumCollection[3].name, result?.get(3)?.name)
+        assertEquals(albumCollection.first().name, result?.first()?.name)
         assertEquals(albumCollection.size, result?.size)
     }
 
     @Test
     fun `check loading state flow`() {
-        // Given
-
         // When
         coEvery { albumRepository.invoke(Unit) } returns albumCollection
         viewModel.fetch()
@@ -68,11 +50,11 @@ class AlbumViewModelTest {
 
     companion object{
         val albumCollection = listOf(
-            Album(12, "album name 1", count = 2),
-            Album(13, "album name 2", count = 3),
-            Album(14, "album name 3", count = 4),
-            Album(15, "album name 4", count = 5),
-            Album(16, "album name 5", count = 6)
+            Album(0, "album name 0", count = 0),
+            Album(1, "album name 1", count = 1),
+            Album(2, "album name 2", count = 2),
+            Album(3, "album name 3", count = 3),
+            Album(4, "album name 4", count = 4)
         )
     }
 }
