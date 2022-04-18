f8be30284fc118e8416ef04009119bd46133f671
jayantkapila <JayKay@11798>
2022-04-18 21:51:56 +0530

    Edit program flow

--- features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/fragment/TokomemberDashCreateProgramFragment.kt
+++ features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/fragment/TokomemberDashCreateProgramFragment.kt
@@ -1,30 +1,36 @@
 package com.tokopedia.tokomember_seller_dashboard.view.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.google.gson.Gson
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.datepicker.LocaleUtils
 import com.tokopedia.datepicker.datetimepicker.DateTimePickerUnify
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.tokomember_seller_dashboard.R
 import com.tokopedia.tokomember_seller_dashboard.di.component.DaggerTokomemberDashComponent
 import com.tokopedia.tokomember_seller_dashboard.domain.requestparam.ProgramUpdateDataInput
+import com.tokopedia.tokomember_seller_dashboard.domain.requestparam.TimeWindow
 import com.tokopedia.tokomember_seller_dashboard.model.MembershipGetProgramForm
+import com.tokopedia.tokomember_seller_dashboard.model.ProgramSellerListItem
+import com.tokopedia.tokomember_seller_dashboard.util.BUNDLE_EDIT_PROGRAM
 import com.tokopedia.tokomember_seller_dashboard.view.viewmodel.TokomemberDashProgramViewmodel
 import com.tokopedia.unifycomponents.ChipsUnify
 import com.tokopedia.unifycomponents.ProgressBarUnify
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
-import kotlinx.android.synthetic.main.tm_dash_create_card.*
-import kotlinx.android.synthetic.main.tm_dash_create_card.progressCard
 import kotlinx.android.synthetic.main.tm_dash_progrm_form.*
 import java.util.*
 import javax.inject.Inject
 
 class TokomemberDashCreateProgramFragment : BaseDaggerFragment() {
+
+    private var fromEdit = false
+    private var editItem: ProgramSellerListItem? = null
 
     @Inject
     lateinit var viewModelFactory: dagger.Lazy<ViewModelProvider.Factory>
@@ -35,6 +41,16 @@ class TokomemberDashCreateProgramFragment : BaseDaggerFragment() {
         viewModelProvider.get(TokomemberDashProgramViewmodel::class.java)
     }
     private var selectedCalendar: Calendar? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        arguments?.getBoolean(BUNDLE_EDIT_PROGRAM, false)?.let {
+            fromEdit = it
+        }
+        arguments?.getString(BUNDLE_EDIT_PROGRAM, "")?.let {
+            editItem = Gson().fromJson(it, ProgramSellerListItem::class.java)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -71,15 +87,26 @@ class TokomemberDashCreateProgramFragment : BaseDaggerFragment() {
     }
 
     private fun renderHeader() {
-        headerProgram?.apply {
-            title = "Buat Program"
-            subtitle = "Langkah 2 dari 4"
-            isShowBackButton = true
+        if(fromEdit) {
+            headerProgram?.apply {
+                title = "Ubah Program"
+                isShowBackButton = true
+            }
+            progressProgram?.hide()
+            btnCreateProgram.text = "Simpan"
         }
-        progressProgram?.apply {
-            progressBarColorType = ProgressBarUnify.COLOR_GREEN
-            progressBarHeight = ProgressBarUnify.SIZE_SMALL
-            setValue(50, false)
+        else{
+            btnCreateProgram.text = "Buat Program"
+            headerProgram?.apply {
+                title = "Buat Program"
+                subtitle = "Langkah 2 dari 4"
+                isShowBackButton = true
+            }
+            progressProgram?.apply {
+                progressBarColorType = ProgressBarUnify.COLOR_GREEN
+                progressBarHeight = ProgressBarUnify.SIZE_SMALL
+                setValue(50, false)
+            }
         }
     }
 
@@ -102,14 +129,26 @@ class TokomemberDashCreateProgramFragment : BaseDaggerFragment() {
         }
         //TODO 1. get program name
         btnCreateProgram?.setOnClickListener {
-            tokomemberDashProgramViewmodel.updateProgram(
-                ProgramUpdateDataInput(
-                    actionType = "create",
-                    apiVersion = "3.0",
-                    name = "name",
-
+            if(fromEdit){
+                tokomemberDashProgramViewmodel.updateProgram(
+                    ProgramUpdateDataInput(
+                        actionType = "edit",
+                        apiVersion = "3.0",
+                        name = editItem?.name,
+                        cardID = editItem?.cardID,
+                        timeWindow = TimeWindow(startTime = editItem?.timeWindow?.startTime, endTime = editItem?.timeWindow?.endTime)
+                    )
                 )
-            )
+            }
+            else {
+                tokomemberDashProgramViewmodel.updateProgram(
+                    ProgramUpdateDataInput(
+                        actionType = "create",
+                        apiVersion = "3.0",
+                        name = "name",
+                        )
+                )
+            }
         }
     }
 
@@ -166,8 +205,8 @@ class TokomemberDashCreateProgramFragment : BaseDaggerFragment() {
     companion object {
         const val MAX_YEAR = 10
         const val MIN_YEAR = -90
-        fun newInstance(): TokomemberDashCreateProgramFragment {
-            return TokomemberDashCreateProgramFragment()
+        fun newInstance(extras: Bundle?) = TokomemberDashCreateProgramFragment().apply {
+            arguments = extras
         }
 
     }
