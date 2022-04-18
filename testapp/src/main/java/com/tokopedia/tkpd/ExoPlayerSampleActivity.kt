399db502a532f33951862fa4d787502070bc478c
mayankgera-toko <mayank.gera@tokopedia.com>
2022-04-18 19:27:10 +0530

    Exo Player Sample 1
    Signed-off-by: mayankgera-toko <mayank.gera@tokopedia.com>

--- testapp/src/main/java/com/tokopedia/tkpd/ExoPlayerSampleActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/ExoPlayerSampleActivity.kt
@@ -102,7 +102,7 @@ class ExoPlayerSampleActivity : AppCompatActivity(), FoldableSupportManager.Fold
 
     @SuppressLint("InlinedApi")
     private fun hideSystemUi() {
-        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+        constraintLayout.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
