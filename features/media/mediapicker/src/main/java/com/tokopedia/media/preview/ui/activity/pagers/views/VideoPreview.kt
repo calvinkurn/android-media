3fb36ec824e3fe1ac9a1afd75bcb68e0a01bbcd7
Calvin <calvin.kurniawan@tokopedia.com>
2022-04-18 17:22:57 +0700

    add handler behavior for schedule hide the control

--- features/media/mediapicker/src/main/java/com/tokopedia/media/preview/ui/activity/pagers/views/VideoPreview.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/media/preview/ui/activity/pagers/views/VideoPreview.kt
@@ -1,29 +1,30 @@
 package com.tokopedia.media.preview.ui.activity.pagers.views
 
 import android.content.Context
-import android.view.TextureView
+import android.os.Handler
 import android.view.View
-import android.widget.ImageView
-import com.google.android.exoplayer2.ui.PlayerControlView
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.video.VideoListener
-import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.media.R
 import com.tokopedia.media.preview.ui.player.PickerVideoPlayer
 import com.tokopedia.media.preview.ui.uimodel.PreviewUiModel
+import android.os.Looper
 
 class VideoPreview(
     private val context: Context,
     val videoPlayer: PickerVideoPlayer
-) : BasePagerPreview {
+) : BasePagerPreview, VideoControlView.Listener {
 
     override val layout: Int
         get() = R.layout.view_item_preview_video
 
+    private val handler = Handler(Looper.getMainLooper())
+
+    private lateinit var videoControl: VideoControlView
+
     override fun setupView(media: PreviewUiModel): View {
         return rootLayoutView(context).also {
             val viewPlayer = it.findViewById<PlayerView>(R.id.video_preview)
-            val videoControl = it.findViewById<PlayerControlView>(R.id.video_control)
+            videoControl = it.findViewById(R.id.video_control)
 
             viewPlayer.player = videoPlayer.player()
             videoControl.player = videoPlayer.player()
@@ -31,16 +32,58 @@ class VideoPreview(
             videoPlayer.videoUrl = media.data.path
 
             it.setOnClickListener {
-                if(videoPlayer.player().isPlaying){
-                    if(!videoControl.isVisible) videoControl.show() else videoControl.hide()
+                if(!videoControl.isVisible) {
+                    videoControl.show()
+                    hideJob()
+                } else {
+                    videoControl.hide()
+                    cleanHideJob()
                 }
             }
 
             videoPlayer.listener = object : PickerVideoPlayer.Listener {
                 override fun onPlayStateChanged(isPlaying: Boolean) {
-                    videoControl.showWithCondition(!videoPlayer.player().isPlaying)
+                    videoControl.updateCenterButton(!videoPlayer.player().isPlaying)
+
+                    if(videoPlayer.player().isPlaying){
+                        hideJob()
+                    } else {
+                        cleanHideJob()
+                    }
                 }
             }
+
+            videoControl.setListener(this)
         }
+    }
+
+    fun cleanHideJob(){
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    fun hideJob(){
+        handler.postDelayed({
+            videoControl.hide()
+        }, 3000)
+    }
+
+    override fun onCenterControlButtonClicked() {
+        if(videoPlayer.isPlaying()) videoPlayer.pause() else videoPlayer.resume()
+    }
+
+    override fun onScrubStart() {
+        videoPlayer.pause()
+        cleanHideJob()
+    }
+
+    override fun onScrubStop() {
+        hideJob()
+        Handler(Looper.getMainLooper()).postDelayed({
+            videoPlayer.resume()
+        },1000)
+    }
+
+    override fun onScrubMove(position: Long) {
+        videoPlayer.player().seekTo(position)
     }
 }
