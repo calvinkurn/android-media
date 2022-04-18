2251acb01a319dfcee989a3e807990698bc0188c
Ankit <ankit.kumar1@tokopedia.com>
2022-04-18 10:31:32 +0530

    unncessary lazy removed from TopAdsDashboardBerandaFragment.kt

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashboardBerandaFragment.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/fragment/TopAdsDashboardBerandaFragment.kt
@@ -44,7 +44,7 @@ open class TopAdsDashboardBerandaFragment : BaseDaggerFragment() {
 
     private lateinit var binding: FragmentTopadsDashboardBerandaBaseBinding
 
-    private val graphLayoutFragment by lazy(LazyThreadSafetyMode.NONE) { TopAdsMultiLineGraphFragment() }
+    private val graphLayoutFragment = TopAdsMultiLineGraphFragment()
 
     private val summaryAdTypeList by lazy(LazyThreadSafetyMode.NONE) {
         context?.resources?.getSummaryAdTypes() ?: listOf()
@@ -56,14 +56,13 @@ open class TopAdsDashboardBerandaFragment : BaseDaggerFragment() {
     }
     private val summaryInformationBottomSheet by lazy(LazyThreadSafetyMode.NONE) { SummaryInformationBottomSheet() }
 
-    private val kataKunciChipsDetailRvAdapter by lazy(LazyThreadSafetyMode.NONE) { TopAdsBerandsKataKunciChipsDetailRvAdapter() }
-    private val kataKunciChipsRvAdapter by lazy(LazyThreadSafetyMode.NONE) {
+    private val kataKunciChipsDetailRvAdapter = TopAdsBerandsKataKunciChipsDetailRvAdapter()
+    private val kataKunciChipsRvAdapter =
         TopAdsBerandsKataKunciChipsRvAdapter(::kataKunciItemSelected)
-    }
-    private val anggarnHarianAdapter by lazy(LazyThreadSafetyMode.NONE) { TopAdsBerandaAnggarnHarianAdapter() }
-    private val produkBerpotensiAdapter by lazy(LazyThreadSafetyMode.NONE) { TopadsImageRvAdapter() }
-    private val summaryRvAdapter by lazy(LazyThreadSafetyMode.NONE) { TopAdsBerandaSummaryRvAdapter() }
-    private val latestReadingRvAdapter by lazy(LazyThreadSafetyMode.NONE) { LatestReadingTopAdsDashboardRvAdapter() }
+    private val anggarnHarianAdapter = TopAdsBerandaAnggarnHarianAdapter()
+    private val produkBerpotensiAdapter = TopadsImageRvAdapter()
+    private val summaryRvAdapter = TopAdsBerandaSummaryRvAdapter()
+    private val latestReadingRvAdapter = LatestReadingTopAdsDashboardRvAdapter()
 
     companion object {
         private const val REQUEST_CODE_SET_AUTO_TOPUP = 6
@@ -313,7 +312,9 @@ open class TopAdsDashboardBerandaFragment : BaseDaggerFragment() {
         topAdsDashboardViewModel.latestReadingLiveData.observe(viewLifecycleOwner) { data ->
             when (data) {
                 is Success -> latestReadingRvAdapter.addItems(data.data)
-                else -> { binding.layoutLatestReading.root.hide() }
+                else -> {
+                    binding.layoutLatestReading.root.hide()
+                }
             }
         }
     }
