2edba047642d832aa182570df26d0753902d8d69
firmankagami <firmanda.nugroho@tokopedia.com>
2022-04-19 17:17:28 +0700

    [Feature] Get Data USP from staging

--- features/tokofood/src/main/java/com/tokopedia/tokofood/home/domain/usecase/TokoFoodHomeUSPUseCase.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/home/domain/usecase/TokoFoodHomeUSPUseCase.kt
@@ -1,12 +1,9 @@
 package com.tokopedia.tokofood.home.domain.usecase
 
-import com.google.gson.Gson
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.tokofood.home.domain.data.TokoFoodHomeUSPResponse
 import com.tokopedia.tokofood.home.domain.query.TokoFoodHomeUSPQuery
-import com.tokopedia.tokofood.home.presentation.viewmodel.DummyData
-import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class TokoFoodHomeUSPUseCase @Inject constructor(graphqlRepository: GraphqlRepository):
@@ -18,7 +15,6 @@ class TokoFoodHomeUSPUseCase @Inject constructor(graphqlRepository: GraphqlRepos
      }
 
     suspend fun execute(): TokoFoodHomeUSPResponse {
-        delay(1000)
-        return Gson().fromJson(DummyData.dummyUSP, TokoFoodHomeUSPResponse::class.java)
+        return executeOnBackground()
     }
 }
