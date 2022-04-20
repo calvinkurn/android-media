f099f95ff348140112f9b4ad04150bd8712ffb76
niranjansaxena <niranjan.saxena@tokopedia.com>
2022-04-20 11:43:12 +0530

    fix constructor crash

--- testapp/src/main/java/com/tokopedia/tkpd/FoldableFragment1.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/FoldableFragment1.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FoldableFragment1 : Fragment(){
+class FoldableFragment1(): Fragment(){
     var listener : Listener? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_container_foldable_1, container, false)
