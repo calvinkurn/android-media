27a45e1b90b15808324ff4409c90ea4a43652777
Ankit <ankit.kumar1@tokopedia.com>
2022-04-20 18:54:45 +0530

    migration rxjava to coroutines : TopAdsEditKeywordUseCase

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/presenter/TopAdsInsightPresenter.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/view/presenter/TopAdsInsightPresenter.kt
@@ -7,6 +7,7 @@ import com.google.gson.reflect.TypeToken
 import com.tokopedia.abstraction.base.view.presenter.BaseDaggerPresenter
 import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.common.network.data.model.RestResponse
+import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.topads.common.data.response.FinalAdResponse
 import com.tokopedia.topads.dashboard.R
@@ -16,17 +17,24 @@ import com.tokopedia.topads.dashboard.data.model.insightkey.MutationData
 import com.tokopedia.topads.dashboard.domain.interactor.TopAdsEditKeywordUseCase
 import com.tokopedia.topads.dashboard.domain.interactor.TopAdsInsightUseCase
 import com.tokopedia.topads.dashboard.view.listener.TopAdsInsightView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import rx.Subscriber
 import java.lang.reflect.Type
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 /**
  * Created by Pika on 22/7/20.
  */
 
-class TopAdsInsightPresenter @Inject constructor(private val topAdsInsightUseCase: TopAdsInsightUseCase,
-                                                 private val topAdsEditKeywordUseCase: TopAdsEditKeywordUseCase
-) : BaseDaggerPresenter<TopAdsInsightView>() {
+class TopAdsInsightPresenter @Inject constructor(
+    private val topAdsInsightUseCase: TopAdsInsightUseCase,
+    private val topAdsEditKeywordUseCase: TopAdsEditKeywordUseCase
+) : BaseDaggerPresenter<TopAdsInsightView>() , CoroutineScope{
+
+    private val job = SupervisorJob()
 
     fun getInsight(resources: Resources) {
         topAdsInsightUseCase.setQuery(GraphqlHelper.loadRawString(resources, R.raw.gql_query_insights_keyword))
@@ -49,31 +57,24 @@ class TopAdsInsightPresenter @Inject constructor(private val topAdsInsightUseCas
     }
 
     fun topAdsCreated(groupId: String, query: String, data: List<MutationData>) {
-        topAdsEditKeywordUseCase.setQuery(query)
-        val requestParams = topAdsEditKeywordUseCase.setParam(groupId, data)
-        topAdsEditKeywordUseCase.execute(requestParams, object : Subscriber<Map<Type, RestResponse>>() {
-            override fun onCompleted() {}
-
-            override fun onError(e: Throwable?) {
-                e?.printStackTrace()
-            }
-
-            override fun onNext(typeResponse: Map<Type, RestResponse>) {
-                val token = object : TypeToken<DataResponse<FinalAdResponse?>>() {}.type
-                val restResponse: RestResponse? = typeResponse[token]
-                val response = restResponse?.getData() as DataResponse<FinalAdResponse>
-                if (response.data.topadsManageGroupAds.keywordResponse.errors?.isEmpty()!!)
-                    view?.onSuccessEditKeywords(response.data)
-                else
-                    view?.onErrorEditKeyword(response.data.topadsManageGroupAds.keywordResponse.errors)
-            }
+        launchCatchError(block = {
+            val requestParams = topAdsEditKeywordUseCase.setParam(groupId, data)
+            val response = topAdsEditKeywordUseCase.execute(query, requestParams)
+            if (response.topadsManageGroupAds.keywordResponse.errors?.isEmpty()!!)
+                view?.onSuccessEditKeywords(response)
+            else
+                view?.onErrorEditKeyword(response.topadsManageGroupAds.keywordResponse.errors)
+        }, onError = {
+            it.printStackTrace()
         })
     }
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun detachView() {
         super.detachView()
         topAdsInsightUseCase.unsubscribe()
-        topAdsEditKeywordUseCase.unsubscribe()
+        job.cancel()
     }
-
 }
