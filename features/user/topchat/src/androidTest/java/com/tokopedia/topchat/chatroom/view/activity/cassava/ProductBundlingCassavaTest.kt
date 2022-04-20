c12eec87ff6cf9c1006e15b4dddb6cdf71d2c65c
kelvinsaputra <kelvin.saputra@tokopedia.com>
2022-04-20 19:50:19 +0700

    Cassava test for SRW & Product Bundling

--- features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/cassava/ProductBundlingCassavaTest.kt
+++ features/user/topchat/src/androidTest/java/com/tokopedia/topchat/chatroom/view/activity/cassava/ProductBundlingCassavaTest.kt
@@ -4,44 +4,55 @@ import android.app.Activity
 import android.app.Instrumentation
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import com.tokopedia.cassavatest.CassavaTestRule
 import com.tokopedia.cassavatest.hasAllSuccess
+import com.tokopedia.test.application.annotations.CassavaTest
 import com.tokopedia.topchat.chatroom.view.activity.base.TopchatRoomTest
 import com.tokopedia.topchat.chatroom.view.activity.robot.general.GeneralRobot.doScrollChatToPosition
 import com.tokopedia.topchat.chatroom.view.activity.robot.product_bundling.ProductBundlingRobot.clickCtaProductBundling
-import org.hamcrest.MatcherAssert
+import org.junit.Rule
 import org.junit.Test
 
+@CassavaTest
 class ProductBundlingCassavaTest: TopchatRoomTest() {
 
-    var cassavaTestRule = CassavaTestRule(false, false)
+    @get:Rule
+    var cassavaTestRule = CassavaTestRule(true, true)
 
     @Test
-    fun track_multiple_product_bundling() {
+    fun test_product_bundling() {
+        val journeyId = "234"
+
+        // Test
+        track_multiple_product_bundling()
+        track_single_product_bundling()
+        track_click_product_bundling()
+
+        // Then
+        assertThat(cassavaTestRule.validate(journeyId), hasAllSuccess())
+    }
+
+    private fun track_multiple_product_bundling() {
         // Given
-        val journeyId = ""
         getChatUseCase.response = getChatUseCase.productBundlingAttachmentMultipleChat
         launchChatRoomActivity()
 
-        // Then
-        MatcherAssert.assertThat(cassavaTestRule.validate(journeyId), hasAllSuccess())
+        //Close
+        finishChatRoomActivity()
     }
 
-    @Test
-    fun track_single_product_bundling() {
+    private fun track_single_product_bundling() {
         // Given
-        val journeyId = ""
         getChatUseCase.response = getChatUseCase.productBundlingAttachmentSingleChat
         launchChatRoomActivity()
 
-        // Then
-        MatcherAssert.assertThat(cassavaTestRule.validate(journeyId), hasAllSuccess())
+        //Close
+        finishChatRoomActivity()
     }
 
-    @Test
-    fun track_click_product_bundling() {
+    private fun track_click_product_bundling() {
         // Given
-        val journeyId = ""
         getChatUseCase.response = getChatUseCase.productBundlingAttachmentMultipleChat
         launchChatRoomActivity()
 
@@ -51,7 +62,7 @@ class ProductBundlingCassavaTest: TopchatRoomTest() {
         doScrollChatToPosition(0)
         clickCtaProductBundling(0)
 
-        //Then
-        MatcherAssert.assertThat(cassavaTestRule.validate(journeyId), hasAllSuccess())
+        //Close
+        finishChatRoomActivity()
     }
 }
