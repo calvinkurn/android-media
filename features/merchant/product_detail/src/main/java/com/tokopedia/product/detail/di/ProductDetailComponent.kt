ef829691e1836aa9c7b496a0a6de056f6698b9f4
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-04-20 16:36:28 +0530

    remove unwanted component

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/di/ProductDetailComponent.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/di/ProductDetailComponent.kt
@@ -3,7 +3,6 @@ package com.tokopedia.product.detail.di
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.product.detail.view.fragment.DynamicProductDetailFragment
 import com.tokopedia.product.detail.view.widget.AddToCartDoneBottomSheet
-import com.tokopedia.product.detail.view.widget.FtPDPInsuranceBottomSheet
 import com.tokopedia.product.info.view.bottomsheet.ProductDetailInfoBottomSheet
 import dagger.Component
 
@@ -20,5 +19,4 @@ interface ProductDetailComponent {
     fun inject(fragment: DynamicProductDetailFragment)
     fun inject(bottomSheetFragment: AddToCartDoneBottomSheet)
     fun inject(productDetailBottomSheet: ProductDetailInfoBottomSheet)
-    fun inject (ftPDPInsuranceBottomSheet:FtPDPInsuranceBottomSheet)
 }
