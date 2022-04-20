9c4770235340fe9dd62aee75aa97e55dc85f7cab
Dhaba Widhikari <wikodhawi@gmail.com>
2022-04-20 12:05:40 +0700

    revert https://github.com/tokopedia/android-tokopedia-core/pull/25135/files

--- features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_page/presentation/fragment/BrandlistPageFragment.kt
+++ features/merchant/brandlist/src/main/java/com/tokopedia/brandlist/brandlist_page/presentation/fragment/BrandlistPageFragment.kt
@@ -315,7 +315,7 @@ class BrandlistPageFragment :
                     val totalBrandsFiltered = if (stateLoadBrands == LoadAllBrandState.LOAD_ALL_BRAND ||
                             stateLoadBrands == LoadAllBrandState.LOAD_INITIAL_ALL_BRAND) totalBrandsNumber else it.data.totalBrands
                     if (recyclerView?.isComputingLayout == false) {
-                        BrandlistPageMapper.resetAllBrandData(adapter)
+                        adapter?.hideLoading()
                     }
 
                     swipeRefreshLayout?.isRefreshing = false
