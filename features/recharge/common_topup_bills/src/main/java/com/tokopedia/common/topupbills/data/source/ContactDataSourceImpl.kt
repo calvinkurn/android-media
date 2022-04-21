2604c76d5d6f8b3982b88335254782dfccfb0726
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-04-21 09:11:37 +0700

    [Recharge] add elvis check on ContactDataSourceImpl on phoneNumber

--- features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/source/ContactDataSourceImpl.kt
+++ features/recharge/common_topup_bills/src/main/java/com/tokopedia/common/topupbills/data/source/ContactDataSourceImpl.kt
@@ -25,8 +25,10 @@ class ContactDataSourceImpl(
                 val name = cursor.getString(cursor.getColumnIndex(
                     ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)) ?: ""
                 val phoneNumber = cursor.getString(cursor.getColumnIndex(
-                    ContactsContract.CommonDataKinds.Phone.NUMBER))
-                contacts.add(TopupBillsContact(name, formatPrefixClientNumber(phoneNumber)))
+                    ContactsContract.CommonDataKinds.Phone.NUMBER)) ?: ""
+                if (phoneNumber.isNotEmpty()) {
+                    contacts.add(TopupBillsContact(name, formatPrefixClientNumber(phoneNumber)))
+                }
             }
         }
         cursor?.close()
