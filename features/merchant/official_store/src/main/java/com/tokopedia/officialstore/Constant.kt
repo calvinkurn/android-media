15cdbf88f8d3c21f580bf971bda869e30aa82535
Dhaba Widhikari <wikodhawi@gmail.com>
2022-04-21 17:49:17 +0700

    remove unused query product recommendation

--- features/merchant/official_store/src/main/java/com/tokopedia/officialstore/Constant.kt
+++ features/merchant/official_store/src/main/java/com/tokopedia/officialstore/Constant.kt
@@ -7,7 +7,6 @@ object GQLQueryConstant {
     const val QUERY_OFFICIAL_STORE_BANNERS = "gql_query_official_store_banners"
     const val QUERY_OFFICIAL_STORE_BENEFITS = "gql_query_official_store_benefits"
     const val QUERY_OFFICIAL_STORE_FEATURED_SHOPS = "gql_query_official_store_featured"
-    const val QUERY_OFFICIAL_STORE_PRODUCT_RECOMMENDATION = "gql_query_official_store_product_recommendation"
 }
 
 object DynamicChannelIdentifiers {
