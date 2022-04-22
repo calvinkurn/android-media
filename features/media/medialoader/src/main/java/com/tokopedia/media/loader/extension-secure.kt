a1cf279510277aa4909de9c77fb6bf7c8e2d7606
isfaaghyth <isfaaghyth@gmail.com>
2022-04-22 13:02:08 +0700

    used userSession instead of particular param on secure image loader

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/extension-secure.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/extension-secure.kt
@@ -1,16 +1,26 @@
 package com.tokopedia.media.loader
 
 import android.widget.ImageView
+import android.widget.Toast
 import com.tokopedia.media.loader.common.Properties
+import com.tokopedia.user.session.UserSessionInterface
 
 inline fun ImageView.loadSecureImage(
     url: String,
-    accessToken: String,
+    userSession: UserSessionInterface,
     crossinline properties: Properties.() -> Unit = {}
-) = call(
-    url,
-    Properties()
-        .apply(properties)
-        .userSessionAccessToken(accessToken),
-    isSecure = true
-)
+) {
+    if (userSession.accessToken.isEmpty()) {
+        Toast.makeText(context, "(Dev) - access token not found", Toast.LENGTH_SHORT).show()
+        return
+    }
+
+    call(
+        url,
+        Properties()
+            .apply(properties)
+            .userId(userSession.userId)
+            .userSessionAccessToken(userSession.accessToken),
+        isSecure = true
+    )
+}
