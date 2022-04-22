dd5397d208586953082e6cd7f0e8c1e49aa4dc47
isfaaghyth <isfaaghyth@gmail.com>
2022-04-22 13:00:56 +0700

    created requestHeader extension for header builder on glide

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/MediaLoaderApi.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/MediaLoaderApi.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.LazyHeaders
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.media.common.Loader
 import com.tokopedia.media.loader.common.Properties
 import com.tokopedia.media.loader.common.factory.BitmapFactory
@@ -15,6 +16,8 @@ import com.tokopedia.media.loader.data.PLACEHOLDER_RES_UNIFY
 import com.tokopedia.media.loader.module.GlideApp
 import com.tokopedia.media.loader.transform.TopRightCrop
 import com.tokopedia.media.loader.utils.HEADER_KEY_AUTH
+import com.tokopedia.media.loader.utils.HEADER_USER_ID
+import com.tokopedia.media.loader.utils.HEADER_X_DEVICE
 import com.tokopedia.media.loader.utils.PREFIX_BEARER
 
 internal object MediaLoaderApi {
@@ -23,6 +26,17 @@ internal object MediaLoaderApi {
 
     private val bitmap by lazy { BitmapFactory() }
     private val gif by lazy { GifFactory() }
+
+    private fun LazyHeaders.Builder.requestHeaders(accessToken: String, userId: String) {
+        // Accounts-Authorization
+        addHeader(HEADER_KEY_AUTH, "$PREFIX_BEARER %s".format(accessToken))
+
+        // X-Device
+        addHeader(HEADER_X_DEVICE, "android-${GlobalConfig.VERSION_NAME}")
+
+        // Tkpd-UserId
+        addHeader(HEADER_USER_ID, userId)
+    }
 
     fun loadImage(imageView: ImageView, properties: Properties, isSecure: Boolean) {
         val context = imageView.context
@@ -52,10 +66,10 @@ internal object MediaLoaderApi {
                     val url = GlideUrl(source, LazyHeaders.Builder()
                         .apply {
                             if (isSecure) {
-                                val auth = "$PREFIX_BEARER %s".format(properties.accessToken)
-
-                                // add accounts auth for secure image loader
-                                addHeader(HEADER_KEY_AUTH, auth)
+                                this.requestHeaders(
+                                    accessToken = properties.accessToken,
+                                    userId = properties.userId
+                                )
                             }
                         }
                         .build()
