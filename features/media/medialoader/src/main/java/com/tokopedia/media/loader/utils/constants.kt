6223c68b80e01886419c76a23a88540b86d5dea0
isfaaghyth <isfaaghyth@gmail.com>
2022-04-22 13:02:49 +0700

    defined headers key on const

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/constants.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/constants.kt
@@ -10,4 +10,6 @@ const val DEFAULT_ROUNDED = 5.0f
 
 // uses for secure image loader
 const val HEADER_KEY_AUTH = "Accounts-Authorization"
+const val HEADER_USER_ID = "Tkpd-UserId"
+const val HEADER_X_DEVICE = "X-Device"
 const val PREFIX_BEARER = "Bearer"
