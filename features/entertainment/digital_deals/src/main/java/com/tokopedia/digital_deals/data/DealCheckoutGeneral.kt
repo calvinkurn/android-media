617f8dd5ae58900cd4d5eb7576dfb494b7e2760c
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-04-25 11:09:52 +0800

    refactor : add function overloading that handle no promo

--- features/entertainment/digital_deals/src/main/java/com/tokopedia/digital_deals/data/DealCheckoutGeneral.kt
+++ features/entertainment/digital_deals/src/main/java/com/tokopedia/digital_deals/data/DealCheckoutGeneral.kt
@@ -3,22 +3,49 @@ package com.tokopedia.digital_deals.data
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+open class DealsGeneral
+open class DealsInstant
+
 data class DealCheckoutGeneral(
         @SerializedName("carts")
         @Expose
-        val carts : Carts = Carts()
-)
+        val carts : CartsPromo = CartsPromo()
+): DealsGeneral()
 
 data class DealCheckoutGeneralInstant(
+        @SerializedName("carts")
+        @Expose
+        val carts : CartsPromo = CartsPromo(),
+        @SerializedName("gateway_code")
+        @Expose
+        var gatewayCode : String = ""
+): DealsInstant()
+
+data class DealCheckoutGeneralNoPromo(
+        @SerializedName("carts")
+        @Expose
+        val carts : Carts = Carts()
+): DealsGeneral()
+
+data class DealCheckoutGeneralInstantNoPromo(
         @SerializedName("carts")
         @Expose
         val carts : Carts = Carts(),
         @SerializedName("gateway_code")
         @Expose
         var gatewayCode : String = ""
-)
+): DealsInstant()
 
 data class Carts(
+        @SerializedName("business_type")
+        @Expose
+        var businessType: Int = 0,
+        @SerializedName("cart_info")
+        @Expose
+        var cartInfo: MutableList<CartInfo> = arrayListOf()
+)
+
+data class CartsPromo(
         @SerializedName("business_type")
         @Expose
         var businessType: Int = 0,
@@ -28,15 +55,6 @@ data class Carts(
         @SerializedName("promo_codes")
         @Expose
         var promoCodes: List<String> = emptyList()
-)
-
-data class CartsNoPromo(
-        @SerializedName("business_type")
-        @Expose
-        var businessType: Int = 0,
-        @SerializedName("cart_info")
-        @Expose
-        var cartInfo: MutableList<CartInfo> = arrayListOf(),
 )
 
 data class CartInfo(
