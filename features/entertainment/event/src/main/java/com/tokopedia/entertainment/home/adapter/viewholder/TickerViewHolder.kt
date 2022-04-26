133ef98167cb4a6a2a4fbff49c146367bb8f2f96
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-04-26 14:52:19 +0800

    feat : create viewHolder

---
+++ features/entertainment/event/src/main/java/com/tokopedia/entertainment/home/adapter/viewholder/TickerViewHolder.kt
@@ -0,0 +1,25 @@
+package com.tokopedia.entertainment.home.adapter.viewholder
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.entertainment.R
+import com.tokopedia.entertainment.home.adapter.viewmodel.TickerModel
+import com.tokopedia.unifycomponents.ticker.Ticker
+
+/**
+ * created by @bayazidnasir on 26/4/2022
+ */
+
+class TickerViewHolder(itemView: View): AbstractViewHolder<TickerModel>(itemView) {
+
+    companion object{
+        @LayoutRes
+        val LAYOUT = R.layout.ent_layout_viewholder_event_ticker
+    }
+
+    override fun bind(element: TickerModel) {
+        val ticker = itemView.findViewById<Ticker>(R.id.ticker_view)
+        ticker.setTextDescription(element.message)
+    }
+}
