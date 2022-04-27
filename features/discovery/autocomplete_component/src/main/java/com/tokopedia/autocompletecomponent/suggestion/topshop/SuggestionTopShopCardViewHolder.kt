9dc749da0a5c0bfa72f839c9cb507c6fde087c91
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2022-04-27 15:26:13 +0700

    remove kotlin synthetic in `autocomplete_component` module

--- features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/topshop/SuggestionTopShopCardViewHolder.kt
+++ features/discovery/autocomplete_component/src/main/java/com/tokopedia/autocompletecomponent/suggestion/topshop/SuggestionTopShopCardViewHolder.kt
@@ -4,9 +4,10 @@ import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.abstraction.common.utils.image.ImageHandler
 import com.tokopedia.autocompletecomponent.R
+import com.tokopedia.autocompletecomponent.databinding.SuggestionTopShopCardLayoutBinding
 import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.unifycomponents.toDp
-import kotlinx.android.synthetic.main.suggestion_top_shop_card_layout.view.*
+import com.tokopedia.utils.view.binding.viewBinding
 
 class SuggestionTopShopCardViewHolder(
         itemView: View,
@@ -16,6 +17,7 @@ class SuggestionTopShopCardViewHolder(
     companion object {
         val LAYOUT = R.layout.suggestion_top_shop_card_layout
     }
+    private var binding: SuggestionTopShopCardLayoutBinding? by viewBinding()
 
     override fun bind(element: SuggestionTopShopCardDataView) {
         bindImage(element)
@@ -27,26 +29,26 @@ class SuggestionTopShopCardViewHolder(
     }
 
     private fun bindImage(element: SuggestionTopShopCardDataView) {
-        itemView.suggestionTopShopCardImage?.let {
+        binding?.suggestionTopShopCardImage?.let {
             ImageHandler.loadImageCircle2(itemView.context, it, element.imageUrl)
         }
     }
 
     private fun bindIconTitle(item: SuggestionTopShopCardDataView) {
-        itemView.suggestionTopShopCardIconTitle?.shouldShowWithAction(item.iconTitle.isNotEmpty()) {
-            ImageHandler.loadImageWithoutPlaceholderAndError(itemView.suggestionTopShopCardIconTitle, item.iconTitle)
+        binding?.suggestionTopShopCardIconTitle?.shouldShowWithAction(item.iconTitle.isNotEmpty()) {
+            ImageHandler.loadImageWithoutPlaceholderAndError(binding?.suggestionTopShopCardIconTitle, item.iconTitle)
         }
     }
 
     private fun bindTitle(element: SuggestionTopShopCardDataView) {
-        itemView.suggestionTopShopCardTitle?.shouldShowWithAction(element.title.isNotEmpty()) {
-            itemView.suggestionTopShopCardTitle?.text = element.title
+        binding?.suggestionTopShopCardTitle?.shouldShowWithAction(element.title.isNotEmpty()) {
+            binding?.suggestionTopShopCardTitle?.text = element.title
         }
     }
 
     private fun bindSubtitle(element: SuggestionTopShopCardDataView) {
-        itemView.suggestionTopShopCardSubtitle?.shouldShowWithAction(element.title.isNotEmpty()) {
-            itemView.suggestionTopShopCardSubtitle?.text = element.subtitle
+        binding?.suggestionTopShopCardSubtitle?.shouldShowWithAction(element.title.isNotEmpty()) {
+            binding?.suggestionTopShopCardSubtitle?.text = element.subtitle
         }
     }
 
@@ -59,25 +61,25 @@ class SuggestionTopShopCardViewHolder(
     }
 
     private fun bindProductImage1(imageUrl: String) {
-        itemView.suggestionTopShopCardProduct1?.let {
+        binding?.suggestionTopShopCardProduct1?.let {
             ImageHandler.loadImageRounded(itemView.context, it, imageUrl, 6.toDp().toFloat())
         }
     }
 
     private fun bindProductImage2(imageUrl: String) {
-        itemView.suggestionTopShopCardProduct2?.let {
+        binding?.suggestionTopShopCardProduct2?.let {
             ImageHandler.loadImageRounded(itemView.context, it, imageUrl, 6.toDp().toFloat())
         }
     }
 
     private fun bindProductImage3(imageUrl: String) {
-        itemView.suggestionTopShopCardProduct3?.let {
+        binding?.suggestionTopShopCardProduct3?.let {
             ImageHandler.loadImageRounded(itemView.context, it, imageUrl, 6.toDp().toFloat())
         }
     }
 
     private fun bindListener(element: SuggestionTopShopCardDataView) {
-        itemView.suggestionTopShopCard?.setOnClickListener {
+        binding?.suggestionTopShopCard?.setOnClickListener {
             suggestionTopShopListener.onTopShopCardClicked(element)
         }
     }
