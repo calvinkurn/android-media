8c2535b32f82299e4087bf359d82996627927d00
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-04-27 11:36:22 +0700

    Add new ApplicationScope in features/common/app_common

--- features/common/app_common/src/main/java/com/tokopedia/app/common/di/CommonAppComponent.java
+++ features/common/app_common/src/main/java/com/tokopedia/app/common/di/CommonAppComponent.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext;
-import com.tokopedia.abstraction.common.di.scope.ApplicationScope;
+import com.tokopedia.app.common.scope.ApplicationScope;
 import com.tokopedia.app.common.MainApplication;
 
 import dagger.Component;
