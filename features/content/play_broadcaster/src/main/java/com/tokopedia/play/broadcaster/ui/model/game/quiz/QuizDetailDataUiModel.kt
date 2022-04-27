e879eafd75e384dd4becffe3334a6bfb40022f7b
mohamad.andriyan <andriyan@tokopedia.com>
2022-04-27 10:52:35 +0700

    Fix QuizDetailDataUiModel.kt default value

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/model/game/quiz/QuizDetailDataUiModel.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/model/game/quiz/QuizDetailDataUiModel.kt
@@ -1,15 +1,15 @@
 package com.tokopedia.play.broadcaster.ui.model.game.quiz
 
 data class QuizDetailDataUiModel(
-    val question: String,
-    val reward:String,
-    val countDownEnd: Int,
-    val choices: List<Choice> = listOf(),
+    val question: String = "",
+    val reward:String = "",
+    val countDownEnd: Int = 0,
+    val choices: List<Choice> = emptyList(),
 ) {
     data class Choice(
-        val id: String,
-        val text: String,
-        val isCorrectAnswer: Boolean,
-        val participantCount: Int,
+        val id: String = "",
+        val text: String = "",
+        val isCorrectAnswer: Boolean = false,
+        val participantCount: Int = 0,
     )
 }
