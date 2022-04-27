5d5d4173d1fbc78a0998baba52bf88b60fd70542
souravsaikia <sourav.saikia@tokopedia.com>
2022-04-27 15:42:59 +0530

    Left View Holder Fixed

---
+++ features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/viewholder/chatbubble/LeftChatMessageUnifyViewHolder.kt
@@ -0,0 +1,141 @@
+package com.tokopedia.chatbot.view.adapter.viewholder.chatbubble
+
+import android.view.Gravity
+import android.view.View
+import com.google.gson.GsonBuilder
+import com.tokopedia.abstraction.common.utils.image.ImageHandler
+import com.tokopedia.chat_common.data.MessageUiModel
+import com.tokopedia.chat_common.view.adapter.viewholder.listener.ChatLinkHandlerListener
+import com.tokopedia.chatbot.R
+import com.tokopedia.chatbot.domain.pojo.senderinfo.SenderInfoData
+import com.tokopedia.chatbot.util.ViewUtil
+import com.tokopedia.chatbot.view.adapter.viewholder.binder.ChatbotMessageViewHolderBinder
+import com.tokopedia.chatbot.view.adapter.viewholder.binder.ChatbotMessageViewHolderBinder2
+import com.tokopedia.chatbot.view.adapter.viewholder.listener.ChatbotAdapterListener
+import com.tokopedia.chatbot.view.customview.reply.ReplyBubbleAreaMessage
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.toDp
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifyprinciples.Typography
+
+class LeftChatMessageUnifyViewHolder(
+    itemView: View?,
+    listener: ChatLinkHandlerListener,
+    private val chatbotAdapterListener: ChatbotAdapterListener,
+    replyBubbleListener: ReplyBubbleAreaMessage.Listener
+) : ChatbotMessageUnifyViewHolder(itemView, listener, replyBubbleListener) {
+
+    private val senderAvatar = itemView?.findViewById<ImageUnify>(R.id.senderAvatar)
+    private val senderName = itemView?.findViewById<Typography>(R.id.senderName)
+    //   private val replyBubbleArea = itemView?.findViewById<ReplyBubbleAreaMessage>(R.id.reply)
+
+    private val bg = ViewUtil.generateBackgroundWithShadow(
+        customChatLayout,
+        com.tokopedia.unifyprinciples.R.color.Unify_N0,
+        R.dimen.dp_chatbot_0,
+        R.dimen.dp_chatbot_20,
+        R.dimen.dp_chatbot_20,
+        R.dimen.dp_chatbot_20,
+        com.tokopedia.unifyprinciples.R.color.Unify_N700_20,
+        R.dimen.dp_chatbot_2,
+        R.dimen.dp_chatbot_1,
+        Gravity.CENTER
+    )
+
+    override fun bind(message: MessageUiModel) {
+        super.bind(message)
+        hideSenderInfo()
+        val senderInfoData = convertToSenderInfo(message.source)
+        bindBackground(message)
+        if (chatbotAdapterListener.isPreviousItemSender(adapterPosition)) {
+            senderInfoData?.let { bindSenderInfo(it) }
+        }
+
+
+        customChatLayout?.background = bg
+
+//        if (message.parentReply!=null){
+        //    replyBubbleArea?.composeMsg(message.parentReply?.name, message.parentReply?.mainText)
+        //       replyBubbleArea?.background = bg
+        //   customChatLayout?.fxChat?.background = bgForChatReplyBubble
+        customChatLayout?.replyBubbleContainer?.updateReplyButtonState(true)
+        customChatLayout?.replyBubbleContainer?.updateBackground(true)
+        customChatLayout?.replyBubbleContainer?.updateCloseButtonState(false)
+        customChatLayout?.replyBubbleContainer?.show()
+//        }else{
+
+//        if (message.parentReply!=null){
+        //    replyBubbleArea?.composeMsg(message.parentReply?.name, message.parentReply?.mainText)
+        //       replyBubbleArea?.background = bg
+        //   customChatLayout?.fxChat?.background = bgForChatReplyBubble
+//        customChatLayout?.replyBubbleContainer?.updateReplyButtonState(true)
+//        customChatLayout?.replyBubbleContainer?.updateBackground(false)
+//        customChatLayout?.replyBubbleContainer?.updateCloseButtonState(false)
+//        customChatLayout?.replyBubbleContainer?.show()
+
+
+//        if (message.parentReply!=null){
+//            replyBubbleArea?.composeMsg(message.parentReply?.name, message.parentReply?.mainText)
+//            replyBubbleArea?.show()
+//        }else{
+//            replyBubbleArea?.hide()
+//        }
+
+
+//        replyBubbleArea?.updateReplyButtonState(true)
+//        replyBubbleArea?.updateBackground(true)
+//        replyBubbleArea?.updateCloseButtonState(false)
+//        replyBubbleArea?.show()
+
+    }
+
+//    private fun bindReplyBubbleListener() {
+//        replyBubbleArea?.setReplyListener(replyBubbleListener)
+//    }
+//
+//    private fun bindReplyReference(msg: MessageUiModel) {
+//        replyBubbleArea?.bindReplyData(msg)
+//    }
+
+    private fun hideSenderInfo() {
+        senderAvatar?.hide()
+        senderName?.hide()
+    }
+
+    private fun bindSenderInfo(senderInfoData: SenderInfoData) {
+        senderAvatar?.show()
+        senderName?.show()
+        ImageHandler.loadImageCircle2(itemView.context, senderAvatar, senderInfoData.iconUrl)
+        senderName?.text = senderInfoData.name
+    }
+
+    private fun convertToSenderInfo(source: String): SenderInfoData? {
+        val senderInfoPrefix = itemView.context.getString(R.string.chatbot_sender_info_prefix)
+        if (source.isNotEmpty() && source.startsWith(senderInfoPrefix)) {
+            val s = source.substring(senderInfoPrefix.length, source.length)
+            return GsonBuilder().create()
+                .fromJson<SenderInfoData>(s,
+                    SenderInfoData::class.java)
+        } else return null
+    }
+
+    private fun bindBackground(message: MessageUiModel) {
+        customChatLayout?.background = bg
+        //     replyBubbleArea?.updateBackground(true)
+    }
+
+    private fun bindMessageInfo(message: MessageUiModel) {
+        if (!message.isSender) {
+            customChatLayout?.fxChat?.showReadMoreView()
+        } else {
+            customChatLayout?.fxChat?.hideReadMoreView()
+        }
+    }
+
+    override fun getDateContainerId(): Int = R.id.dateContainer
+
+    companion object {
+        val LAYOUT = R.layout.item_chatbot_chat_left
+    }
+}
