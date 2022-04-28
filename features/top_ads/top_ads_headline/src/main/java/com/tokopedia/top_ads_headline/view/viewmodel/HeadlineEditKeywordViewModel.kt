e8d581d856bea419b92be6c2b9a8613d61659aa6
Ankit <ankit.kumar1@tokopedia.com>
2022-04-28 14:22:45 +0530

    Revert "HeadlineEditKeywordViewModel.kt refractored"
    This reverts commit 493a2f3e

--- features/top_ads/top_ads_headline/src/main/java/com/tokopedia/top_ads_headline/view/viewmodel/HeadlineEditKeywordViewModel.kt
+++ features/top_ads/top_ads_headline/src/main/java/com/tokopedia/top_ads_headline/view/viewmodel/HeadlineEditKeywordViewModel.kt
@@ -12,56 +12,40 @@ import com.tokopedia.topads.common.domain.usecase.GetAdKeywordUseCase
 import javax.inject.Inject
 
 class HeadlineEditKeywordViewModel @Inject constructor(
-    private val getAdKeywordUseCase: GetAdKeywordUseCase,
+        private val getAdKeywordUseCase: GetAdKeywordUseCase
 ) : ViewModel() {
 
     private var selectedKeywords: ArrayList<GetKeywordResponse.KeywordsItem> = ArrayList()
 
-    fun getAdKeyword(
-        shopId: String,
-        groupId: Int,
-        cursor: String,
-        onSuccess: (List<GetKeywordResponse.KeywordsItem>, cursor: String) -> Unit,
-        keywordType: String,
-    ) {
-        getAdKeywordUseCase.setParams(groupId,
-            cursor,
-            shopId,
-            EDIT_HEADLINE_PAGE,
-            keywordStatus = listOf(1, 3))
+    fun getAdKeyword(shopId: String, groupId: Int, cursor: String, onSuccess: (List<GetKeywordResponse.KeywordsItem>, cursor: String) -> Unit, keywordType: String) {
+        getAdKeywordUseCase.setParams(groupId, cursor, shopId, EDIT_HEADLINE_PAGE, keywordStatus = listOf(1, 3))
         getAdKeywordUseCase.executeQuerySafeMode(
-            {
-                if (cursor.isBlank()) {
-                    selectedKeywords.clear()
-                }
-                addSelectedKeywords(keywordType, it.topAdsListKeyword.data.keywords)
-                onSuccess(getDeepCopyOfSelectedKeywords(),
-                    it.topAdsListKeyword.data.pagination.cursor)
-            },
-            { throwable ->
-                throwable.printStackTrace()
-            })
+                {
+                    if (cursor.isBlank()) {
+                        selectedKeywords.clear()
+                    }
+                    addSelectedKeywords(keywordType, it.topAdsListKeyword.data.keywords)
+                    onSuccess(getDeepCopyOfSelectedKeywords(), it.topAdsListKeyword.data.pagination.cursor)
+                },
+                { throwable ->
+                    throwable.printStackTrace()
+                })
     }
 
     private fun getDeepCopyOfSelectedKeywords(): List<GetKeywordResponse.KeywordsItem> {
-        val list = ArrayList<GetKeywordResponse.KeywordsItem>()
+        val list  = ArrayList<GetKeywordResponse.KeywordsItem>()
         selectedKeywords.forEach {
             list.add(it.copy())
         }
         return list
     }
 
-    private fun addSelectedKeywords(
-        keywordType: String,
-        keywords: List<GetKeywordResponse.KeywordsItem>,
-    ) {
+    private fun addSelectedKeywords(keywordType: String, keywords: List<GetKeywordResponse.KeywordsItem>) {
         if (keywordType == KEYWORD_POSITIVE) {
-            val positiveKeywordList =
-                keywords.filter { it.type == KEYWORD_TYPE_PHRASE || it.type == KEYWORD_TYPE_EXACT }
+            val positiveKeywordList = keywords.filter { it.type == KEYWORD_TYPE_PHRASE || it.type == KEYWORD_TYPE_EXACT }
             selectedKeywords.addAll(positiveKeywordList)
         } else {
-            val positiveKeywordList =
-                keywords.filter { it.type == KEYWORD_TYPE_NEGATIVE_PHRASE || it.type == KEYWORD_TYPE_NEGATIVE_EXACT }
+            val positiveKeywordList = keywords.filter { it.type == KEYWORD_TYPE_NEGATIVE_PHRASE || it.type == KEYWORD_TYPE_NEGATIVE_EXACT }
             selectedKeywords.addAll(positiveKeywordList)
         }
     }
