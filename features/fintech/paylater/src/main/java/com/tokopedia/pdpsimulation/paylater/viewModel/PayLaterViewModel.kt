f26cade0d8c37a081d598d91e9334b4494455e0d
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-05-04 09:54:16 +0530

    added usecase

--- features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/viewModel/PayLaterViewModel.kt
+++ features/fintech/paylater/src/main/java/com/tokopedia/pdpsimulation/paylater/viewModel/PayLaterViewModel.kt
@@ -3,6 +3,13 @@ package com.tokopedia.pdpsimulation.paylater.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.applink.UriUtil
+import com.tokopedia.applink.internal.ApplinkConstInternalMarketplace
+import com.tokopedia.atc_common.data.model.request.AddToCartOccMultiCartParam
+import com.tokopedia.atc_common.data.model.request.AddToCartOccMultiRequestParams
+import com.tokopedia.atc_common.domain.model.response.AddToCartOccMultiDataModel
+import com.tokopedia.atc_common.domain.usecase.coroutine.AddToCartOccMultiUseCase
+import com.tokopedia.pdpsimulation.activateCheckout.viewmodel.ShowToasterException
 import com.tokopedia.pdpsimulation.common.di.qualifier.CoroutineMainDispatcher
 import com.tokopedia.pdpsimulation.common.domain.model.BaseProductDetailClass
 import com.tokopedia.pdpsimulation.common.domain.model.GetProductV3
@@ -21,6 +28,7 @@ class PayLaterViewModel @Inject constructor(
     private val paylaterGetSimulationV3UseCase: PayLaterSimulationV3UseCase,
     private val productDetailUseCase: ProductDetailUseCase,
     private val mapperUseCase: PayLaterUiMapperUseCase,
+    private val addToCartUseCase: AddToCartOccMultiUseCase,
     @CoroutineMainDispatcher dispatcher: CoroutineDispatcher,
 ) : BaseViewModel(dispatcher) {
 
@@ -32,6 +40,12 @@ class PayLaterViewModel @Inject constructor(
     private val _productDetailLiveData = MutableLiveData<Result<GetProductV3>>()
     val productDetailLiveData: LiveData<Result<GetProductV3>> = _productDetailLiveData
 
+
+    private val _addToCartLiveData = MutableLiveData<Result<AddToCartOccMultiDataModel>>()
+    val addToCartLiveData: LiveData<Result<AddToCartOccMultiDataModel>> = _addToCartLiveData
+
+
+
     // tenure to be auto-selected by default coming from applink
     var defaultTenure = 0
 
@@ -41,6 +55,7 @@ class PayLaterViewModel @Inject constructor(
     // if tenure not found -> then max simulation
     var defaultSelectedSimulation: Int = 0
     var finalProductPrice:Double = 0.0
+     var shopId:String? = null
 
     fun getPayLaterAvailableDetail(price: Double, productId: String) {
         finalProductPrice = price
@@ -68,7 +83,10 @@ class PayLaterViewModel @Inject constructor(
                 ) == true) && data.price?.equals(0.0) == true)
             )
                 onFailProductDetail(IllegalStateException("Data invalid"))
-            else _productDetailLiveData.postValue(Success(data))
+            else {
+                shopId = data.shopDetail?.shopId
+                _productDetailLiveData.postValue(Success(data))
+            }
         }
     }
 
@@ -95,11 +113,61 @@ class PayLaterViewModel @Inject constructor(
         )
     }
 
+
+
+    fun addProductToCart(productId: String) {
+        shopId?.let {
+            addToCartUseCase.setParams(
+                AddToCartOccMultiRequestParams(
+                    carts = arrayListOf(
+                        AddToCartOccMultiCartParam(
+                            productId = productId,
+                            shopId = it,
+                            quantity = PRODUCT_QUANTITY,
+                        )
+                    ),
+                    source = AddToCartOccMultiRequestParams.SOURCE_FINTECH
+                )
+            )
+            addToCartUseCase.execute(
+                onSuccess = {
+                    onSuccessAddToCartForCheckout(it)
+                },
+                onError = {
+                    onErrorAddToCartForCheckout(it)
+                }
+            )
+        }
+    }
+
+    private fun onErrorAddToCartForCheckout(throwable: Throwable) {
+        _addToCartLiveData.value = Fail(throwable)
+    }
+
+    private fun onSuccessAddToCartForCheckout(addToCartOcc: AddToCartOccMultiDataModel) {
+        if (addToCartOcc.isStatusError())
+            _addToCartLiveData.value = Fail(
+                ShowToasterException(
+                    addToCartOcc.getAtcErrorMessage()
+                        ?: ""
+                )
+            )
+        else {
+            _addToCartLiveData.value = Success(addToCartOcc)
+
+        }
+    }
+
+
     override fun onCleared() {
         paylaterGetSimulationV3UseCase.cancelJobs()
         productDetailUseCase.cancelJobs()
         mapperUseCase.cancelJobs()
         super.onCleared()
+    }
+
+    companion object{
+        const val PRODUCT_QUANTITY = "1"
     }
 
 }
