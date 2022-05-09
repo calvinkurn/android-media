98c5d6195bc4c3dd3714666a8c518fef42069f5c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-05-09 18:22:14 +0700

    fix : remove unused import

--- features/content/createpost_common/src/main/java/com/tokopedia/createpost/common/data/pojo/getcontentform/FeedContentForm.kt
+++ features/content/createpost_common/src/main/java/com/tokopedia/createpost/common/data/pojo/getcontentform/FeedContentForm.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.createpost.common.data.pojo.getcontentform
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class FeedContentForm(
