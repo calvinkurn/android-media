a995c3b2065af566ba03b1d88ed8f412cc436f21
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-05-09 17:59:27 +0700

    address comment : make product tag source as enum

--- features/content/createpost/src/main/java/com/tokopedia/createpost/producttag/view/uimodel/ProductTagSource.kt
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/producttag/view/uimodel/ProductTagSource.kt
@@ -3,28 +3,23 @@ package com.tokopedia.createpost.producttag.view.uimodel
 /**
  * Created By : Jonathan Darwin on April 25, 2022
  */
-sealed class ProductTagSource {
-    object GlobalSearch: ProductTagSource()
-    object Wishlist: ProductTagSource()
-    object LastTagProduct: ProductTagSource()
-    object LastPurchase: ProductTagSource()
-    object MyShop: ProductTagSource()
-    object Unknown: ProductTagSource()
+enum class ProductTagSource(val tag: String) {
+    GlobalSearch("global_search"),
+    Wishlist("wishlist"),
+    MyShop("own_shop"),
+    LastPurchase("last_purchase"),
+    LastTagProduct("last_tag_product"),
+    Unknown("");
 
     companion object {
-        private const val GLOBAL_SEARCH = "global_search"
-        private const val WISHLIST = "wishlist"
-        private const val OWN_SHOP = "own_shop"
-        private const val LAST_PURCHASE = "last_purchase"
-        private const val LAST_TAG_PRODUCT = "last_tag_product"
 
         fun mapFromString(s: String): ProductTagSource {
             return when(s) {
-                GLOBAL_SEARCH -> GlobalSearch
-                WISHLIST -> Wishlist
-                OWN_SHOP -> MyShop
-                LAST_TAG_PRODUCT -> LastTagProduct
-                LAST_PURCHASE -> LastPurchase
+                GlobalSearch.tag -> GlobalSearch
+                Wishlist.tag -> Wishlist
+                MyShop.tag -> MyShop
+                LastTagProduct.tag -> LastTagProduct
+                LastPurchase.tag -> LastPurchase
                 else -> Unknown
             }
         }
