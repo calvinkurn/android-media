41b6b0bc4c37c1afc62e60e59b3abb93a73c3793
astidhiyaa <asti.anzaria@tokopedia.com>
2022-05-10 15:37:27 +0800

    fix: add delay

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -1778,6 +1778,8 @@ class PlayViewModel @AssistedInject constructor(
 
             val isRewardAvailable: Boolean = (_interactive.value.interactive as InteractiveUiModel.Quiz).reward.isNotEmpty()
 
+            delay(INTERACTIVE_FINISH_MESSAGE_DELAY)
+
             suspend fun checkWinnerStatus(): Boolean {
                 val winnerStatus = _winnerStatus.value
                 return if (winnerStatus != null) {
