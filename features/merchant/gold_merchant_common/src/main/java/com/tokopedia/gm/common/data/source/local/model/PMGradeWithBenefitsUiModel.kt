94fe1b1ac58a5af9bddf3f0d7817f6f22ebd643b
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-05-11 15:21:54 +0800

    remove hard code in PMGradeWithBenefitsUiModel.kt

--- features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/local/model/PMGradeWithBenefitsUiModel.kt
+++ features/merchant/gold_merchant_common/src/main/java/com/tokopedia/gm/common/data/source/local/model/PMGradeWithBenefitsUiModel.kt
@@ -1,45 +1,48 @@
 package com.tokopedia.gm.common.data.source.local.model
 
+import com.tokopedia.kotlin.extensions.view.EMPTY
+import com.tokopedia.kotlin.extensions.view.ZERO
+
 /**
  * Created By @ilhamsuaib on 10/03/21
  */
 
 sealed class PMGradeWithBenefitsUiModel(
-    open val gradeName: String = "",
+    open val gradeName: String = String.EMPTY,
     open val isTabActive: Boolean = false,
-    open val tabLabel: String = "",
-    open val tabResIcon: Int = 0,
+    open val tabLabel: String = String.EMPTY,
+    open val tabResIcon: Int = Int.ZERO,
     open val benefitList: List<PMBenefitItemUiModel> = emptyList()
 ) {
     data class PM(
-        override val gradeName: String = "",
+        override val gradeName: String = String.EMPTY,
         override val isTabActive: Boolean = false,
-        override val tabLabel: String = "",
-        override val tabResIcon: Int = 0,
+        override val tabLabel: String = String.EMPTY,
+        override val tabResIcon: Int = Int.ZERO,
         override val benefitList: List<PMBenefitItemUiModel> = emptyList()
     ) : PMGradeWithBenefitsUiModel(gradeName, isTabActive, tabLabel, tabResIcon, benefitList)
 
     data class PMProAdvance(
-        override val gradeName: String = "",
+        override val gradeName: String = String.EMPTY,
         override val isTabActive: Boolean = false,
-        override val tabLabel: String = "",
-        override val tabResIcon: Int = 0,
+        override val tabLabel: String = String.EMPTY,
+        override val tabResIcon: Int = Int.ZERO,
         override val benefitList: List<PMBenefitItemUiModel> = emptyList()
     ) : PMGradeWithBenefitsUiModel(gradeName, isTabActive, tabLabel, tabResIcon, benefitList)
 
     data class PMProExpert(
-        override val gradeName: String = "",
+        override val gradeName: String = String.EMPTY,
         override val isTabActive: Boolean = false,
-        override val tabLabel: String = "",
-        override val tabResIcon: Int = 0,
+        override val tabLabel: String = String.EMPTY,
+        override val tabResIcon: Int = Int.ZERO,
         override val benefitList: List<PMBenefitItemUiModel> = emptyList()
     ) : PMGradeWithBenefitsUiModel(gradeName, isTabActive, tabLabel, tabResIcon, benefitList)
 
     data class PMProUltimate(
-        override val gradeName: String = "",
+        override val gradeName: String = String.EMPTY,
         override val isTabActive: Boolean = false,
-        override val tabLabel: String = "",
-        override val tabResIcon: Int = 0,
+        override val tabLabel: String = String.EMPTY,
+        override val tabResIcon: Int = Int.ZERO,
         override val benefitList: List<PMBenefitItemUiModel> = emptyList()
     ) : PMGradeWithBenefitsUiModel(gradeName, isTabActive, tabLabel, tabResIcon, benefitList)
 }
