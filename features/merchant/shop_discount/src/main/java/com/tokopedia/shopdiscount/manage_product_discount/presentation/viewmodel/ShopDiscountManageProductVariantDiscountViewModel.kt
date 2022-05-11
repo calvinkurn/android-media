384a0de712f42deec4b0655f84fc7c4f42bfbbbe
CJ <calvyn.julian@tokopedia.com>
2022-05-11 11:13:30 +0700

    fix end date for create flow on seller vps membership

--- features/merchant/shop_discount/src/main/java/com/tokopedia/shopdiscount/manage_product_discount/presentation/viewmodel/ShopDiscountManageProductVariantDiscountViewModel.kt
+++ features/merchant/shop_discount/src/main/java/com/tokopedia/shopdiscount/manage_product_discount/presentation/viewmodel/ShopDiscountManageProductVariantDiscountViewModel.kt
@@ -21,6 +21,7 @@ import com.tokopedia.shopdiscount.utils.constant.ShopDiscountManageProductDiscou
 import com.tokopedia.shopdiscount.utils.constant.ShopDiscountManageProductDiscountErrorValidation.Companion.NONE
 import com.tokopedia.shopdiscount.utils.constant.SlashPriceStatusId
 import com.tokopedia.shopdiscount.utils.extension.toCalendar
+import com.tokopedia.shopdiscount.utils.extension.unixToMs
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
@@ -124,7 +125,7 @@ class ShopDiscountManageProductVariantDiscountViewModel @Inject constructor(
         val vpsPackageData = slashPriceBenefitData.listSlashPriceBenefitData.firstOrNull {
             it.packageId.toIntOrZero() != -1
         }
-        return Date(vpsPackageData?.expiredAtUnix.orZero())
+        return Date(vpsPackageData?.expiredAtUnix?.unixToMs().orZero())
     }
 
     fun getMembershipDefaultEndDate(): Date {
