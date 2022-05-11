a53df815c3b19adc7cd67888ec9b9401869bbf07
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-05-11 11:16:17 +0700

    feature : add adapter & xml UI

---
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/producttag/view/adapter/viewholder/ShopCardViewHolder.kt
@@ -0,0 +1,67 @@
+package com.tokopedia.createpost.producttag.view.adapter.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.tokopedia.createpost.createpost.databinding.ItemProductTagLoadingListBinding
+import com.tokopedia.createpost.createpost.databinding.ItemProductTagShopListBinding
+import com.tokopedia.createpost.producttag.view.adapter.ShopCardAdapter
+import com.tokopedia.createpost.producttag.view.uimodel.ShopUiModel
+
+/**
+ * Created By : Jonathan Darwin on May 11, 2022
+ */
+internal class ShopCardViewHolder private constructor() {
+
+    internal class Shop(
+        private val binding: ItemProductTagShopListBinding,
+        private val onSelected: (ShopUiModel) -> Unit,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: ShopCardAdapter.Model.Shop) {
+            binding.shopCardView.apply {
+//                setProductModel(item.product.toProductCard())
+//                setOnClickListener { onSelected(item.product) }
+            }
+        }
+
+        companion object {
+
+            fun create(
+                parent: ViewGroup,
+                onSelected: (ShopUiModel) -> Unit
+            ) = Shop(
+                binding = ItemProductTagShopListBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                ),
+                onSelected = onSelected,
+            )
+        }
+    }
+
+    internal class Loading(
+        binding: ItemProductTagLoadingListBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            val layoutParams = itemView.layoutParams
+            if (layoutParams is StaggeredGridLayoutManager.LayoutParams) {
+                layoutParams.isFullSpan = true
+            }
+        }
+
+        companion object {
+
+            fun create(parent: ViewGroup) = Loading(
+                ItemProductTagLoadingListBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false,
+                ),
+            )
+        }
+    }
+}
