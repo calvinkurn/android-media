2b178976932df3177e7c7f6c8a610a1b66f3e3ec
isfaaghyth <isfaaghyth@gmail.com>
2022-05-12 18:44:40 +0700

    makes microphone permission request at the second position

--- features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/uimodel/PermissionUiModel.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/uimodel/PermissionUiModel.kt
@@ -39,7 +39,7 @@ data class PermissionUiModel(
                     }
                 }
                 PageType.GALLERY -> listOf(storage())
-                else -> listOf(microphone(), camera(), storage())
+                else -> listOf(camera(), microphone(), storage())
             }
         }
     }
