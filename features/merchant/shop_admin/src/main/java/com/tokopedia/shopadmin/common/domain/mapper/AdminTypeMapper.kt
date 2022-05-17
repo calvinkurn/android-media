5cde16060b07b4071543c3011af89d214afee4eb
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-05-17 18:24:32 +0700

    fix admin type mapper

--- features/merchant/shop_admin/src/main/java/com/tokopedia/shopadmin/common/domain/mapper/AdminTypeMapper.kt
+++ features/merchant/shop_admin/src/main/java/com/tokopedia/shopadmin/common/domain/mapper/AdminTypeMapper.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.shopadmin.common.domain.mapper
 
-import com.tokopedia.shopadmin.common.constants.AdminStatus
 import com.tokopedia.shopadmin.common.domain.model.GetAdminTypeResponse
 import com.tokopedia.shopadmin.common.presentation.uimodel.AdminTypeUiModel
 import javax.inject.Inject
@@ -10,9 +9,8 @@ class AdminTypeMapper @Inject constructor() {
     fun mapToAdminTypeUiModel(adminInfoResponse: GetAdminTypeResponse): AdminTypeUiModel {
         val adminType = adminInfoResponse.getAdminType
         val adminRoleType = adminType.adminData.detailInformation.adminRoleType
-//        return AdminTypeUiModel(adminType.adminData.status, adminType.shopID)
         return AdminTypeUiModel(
-            AdminStatus.WAITING_CONFIRMATION,
+            adminType.adminData.status,
             adminType.shopID,
             adminRoleType.isShopAdmin
         )
