2fe05876fa16796301cc4f6fbaa3efea8df15b92
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-05-18 14:04:17 +0700

    Fix Mapper

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMapper.kt
@@ -479,7 +479,7 @@ object DeeplinkMapper {
             DLP.exact(ApplinkConst.MediaPicker.MEDIA_PICKER, ApplinkConstInternalMedia.INTERNAL_MEDIA_PICKER),
             DLP.exact(ApplinkConst.MediaPicker.MEDIA_PICKER_PREVIEW, ApplinkConstInternalMedia.INTERNAL_MEDIA_PICKER_PREVIEW),
             DLP.host(ApplinkConst.WEB_HOST) {_, _, deeplink, _ -> getWebHostWebViewLink(deeplink)},
-            DLP.startWith(ApplinkConst.INPUT_INACTIVE_NUMBER) { ctx, _, deeplink, _ -> DeeplinkMapperUser.getRegisteredNavigationUser(ctx, deeplink) },
+            DLP.exact(ApplinkConst.INPUT_INACTIVE_NUMBER) { ctx, _, deeplink, _ -> DeeplinkMapperUser.getRegisteredNavigationUser(ctx, deeplink) },
         )
 
     fun getTokopediaSchemeList():List<DLP>{
