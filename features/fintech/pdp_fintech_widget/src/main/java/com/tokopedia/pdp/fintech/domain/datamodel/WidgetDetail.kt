a800b122fd1f4ce4761bba8ed910ed04e31fcbb8
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-05-20 00:51:23 +0530

    removed kotlin extension

--- features/fintech/pdp_fintech_widget/src/main/java/com/tokopedia/pdp/fintech/domain/datamodel/WidgetDetail.kt
+++ features/fintech/pdp_fintech_widget/src/main/java/com/tokopedia/pdp/fintech/domain/datamodel/WidgetDetail.kt
@@ -2,7 +2,7 @@ package com.tokopedia.pdp.fintech.domain.datamodel
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 data class WidgetDetail(
     @SerializedName("paylater_getPDPWidgetV2") var baseWidgetResponse: BaseDataResponse? = null
