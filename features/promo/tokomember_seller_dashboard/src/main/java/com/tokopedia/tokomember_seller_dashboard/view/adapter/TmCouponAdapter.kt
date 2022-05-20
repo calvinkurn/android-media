27dae1a31b01b58330e3268275fd82313395af8e
jayantkapila <JayKay@11798>
2022-05-20 17:56:52 +0530

    Filters complete

--- features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/adapter/TmCouponAdapter.kt
+++ features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/adapter/TmCouponAdapter.kt
@@ -1,6 +1,6 @@
 package com.tokopedia.tokomember_seller_dashboard.view.adapter
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,12 +18,7 @@ class TmCouponAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TmCouponVh {
         return TmCouponVh(
-            View.inflate(
-                parent.context,
-                R.layout.tm_coupon_list_item,
-                null
-            ),
-            fragmentManager
+            LayoutInflater.from(parent.context).inflate(R.layout.tm_coupon_list_item,parent,false), fragmentManager
         )
     }
 
