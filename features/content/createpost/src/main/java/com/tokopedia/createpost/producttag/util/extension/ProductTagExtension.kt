19e9260c8f063413c732ff18d751dadec9932584
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-05-20 17:22:24 +0700

    feature : add error state on last purchased

--- features/content/createpost/src/main/java/com/tokopedia/createpost/producttag/util/extension/ProductTagExtension.kt
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/producttag/util/extension/ProductTagExtension.kt
@@ -1,6 +1,9 @@
 package com.tokopedia.createpost.producttag.util.extension
 
 import com.tokopedia.createpost.producttag.view.uimodel.ProductTagSource
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 /**
  * Created By : Jonathan Darwin on May 11, 2022
@@ -13,3 +16,8 @@ fun Set<ProductTagSource>.removeLast(): Set<ProductTagSource> {
         lastOrNull()?.let { remove(it) }
     }
 }
+
+internal val Throwable.isNetworkError: Boolean
+    get() = this is ConnectException ||
+            this is SocketTimeoutException ||
+            this is UnknownHostException
