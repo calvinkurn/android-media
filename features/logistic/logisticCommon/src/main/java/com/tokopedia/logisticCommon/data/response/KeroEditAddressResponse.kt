b6941f2a8d1240182202d1d01c7105984b2e452c
hiradevina <fakhira.devina@tokopedia.com>
2022-05-20 14:57:07 +0700

    remove expose annotation

--- features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/data/response/KeroEditAddressResponse.kt
+++ features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/data/response/KeroEditAddressResponse.kt
@@ -1,45 +1,36 @@
 package com.tokopedia.logisticCommon.data.response
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class KeroEditAddressResponse(
-
-    @Expose
+    
     @SerializedName("data")
     val data: Data = Data()
 ) {
     data class Data(
-
-        @Expose
+        
         @SerializedName("kero_edit_address")
         val keroEditAddress: KeroEditAddress = KeroEditAddress(),
     ) {
         data class KeroEditAddress(
 
-            @Expose
             @SerializedName("data")
             val data: KeroEditAddressSuccessResponse = KeroEditAddressSuccessResponse(),
 
-            @Expose
             @SerializedName("server_process_time")
             val serverProcessTime: String = "",
 
-            @Expose
             @SerializedName("config")
             val config: String = "",
 
-            @Expose
             @SerializedName("status")
             val status: String = ""
         ) {
             data class KeroEditAddressSuccessResponse(
 
-                @Expose
                 @SerializedName("is_success")
                 val isSuccess: Int = 0,
 
-                @Expose
                 @SerializedName("is_state_chosen_address_changed")
                 val isStateChosenAddressChanged: Boolean = false
             )
