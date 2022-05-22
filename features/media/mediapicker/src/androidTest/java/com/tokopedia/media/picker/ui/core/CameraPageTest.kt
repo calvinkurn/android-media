957884de6e25c8eb6ae3ecb95ef04377531dea8d
isfaaghyth <isfaaghyth@gmail.com>
2022-05-23 01:20:11 +0700

    optimize cameraPageTest import class

--- features/media/mediapicker/src/androidTest/java/com/tokopedia/media/picker/ui/core/CameraPageTest.kt
+++ features/media/mediapicker/src/androidTest/java/com/tokopedia/media/picker/ui/core/CameraPageTest.kt
@@ -1,23 +1,22 @@
 package com.tokopedia.media.picker.ui.core
 
-import android.net.Uri
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.otaliastudios.cameraview.CameraView
-import com.tokopedia.media.picker.ui.PickerTest
 import com.tokopedia.media.R
 import com.tokopedia.media.picker.common.ui.activity.TestPreviewActivity
 import com.tokopedia.media.picker.helper.utils.PickerCameraViewActions
+import com.tokopedia.media.picker.ui.PickerTest
 import com.tokopedia.media.picker.ui.activity.main.component.BottomNavComponent
 import org.hamcrest.CoreMatchers.not
 
-open class CameraPageTest : PickerTest() {
-
-    override fun createAndAppendUri(builder: Uri.Builder) {}
+abstract class CameraPageTest : PickerTest() {
 
     object Robot {
         fun clickCaptureButton() {
