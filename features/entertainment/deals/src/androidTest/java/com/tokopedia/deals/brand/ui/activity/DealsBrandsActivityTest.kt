79c1a8f16fb58669a015c033e5c6ce5edbed4889
firmankagami <firmanda.nugroho@tokopedia.com>
2022-05-25 14:46:54 +0700

    [fix] try fix deals by change structure

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
@@ -11,6 +11,7 @@ import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
 import com.tokopedia.cassavatest.CassavaTestRule
 import com.tokopedia.cassavatest.getAnalyticsWithQuery
@@ -19,12 +20,16 @@ import com.tokopedia.deals.DealsDummyResponseString
 import com.tokopedia.deals.DealsDummyResponseString.DUMMY_RESPONSE_SECOND_CATEGORY_TITLE
 import com.tokopedia.deals.DealsDummyResponseString.DUMMY_USER_TYPE_STRING
 import com.tokopedia.deals.R
+import com.tokopedia.deals.brand_detail.ui.activity.DealsBrandDetailActivity
 import com.tokopedia.deals.category.ui.activity.mock.DealsCategoryMockResponse
+import com.tokopedia.graphql.GraphqlCacheManager
 import com.tokopedia.test.application.espresso_component.CommonMatcher
 import com.tokopedia.test.application.espresso_component.CommonMatcher.getElementFromMatchAtPosition
 import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import org.hamcrest.core.AllOf
+import org.junit.After
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -32,22 +37,25 @@ class DealsBrandsActivityTest {
 
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
     private val gtmLogDbSource = GtmLogDBSource(context)
+    private val graphqlCacheManager = GraphqlCacheManager()
 
     @get:Rule
     var cassavaTestRule = CassavaTestRule()
 
-    @get: Rule
-    var activityRule: IntentsTestRule<DealsBrandActivity> = object : IntentsTestRule<DealsBrandActivity>(DealsBrandActivity::class.java) {
-        override fun beforeActivityLaunched() {
-            super.beforeActivityLaunched()
-            gtmLogDbSource.deleteAll().subscribe()
-            setupGraphqlMockResponse(DealsCategoryMockResponse())
-        }
+    @get:Rule
+    var activityRule =
+        ActivityTestRule(DealsBrandActivity::class.java, false, false)
 
-        override fun getActivityIntent(): Intent {
-            return DealsBrandActivity.getCallingIntent(context, "")
-        }
+    @Before
+    fun setup(){
+        Intents.init()
+        graphqlCacheManager.deleteAll()
+        gtmLogDbSource.deleteAll().subscribe()
+        setupGraphqlMockResponse(DealsCategoryMockResponse())
+        val intent = DealsBrandActivity.getCallingIntent(context, "")
+        activityRule.launchActivity(intent)
     }
+
 
     @Test
     fun testBrandLayout() {
@@ -59,6 +67,7 @@ class DealsBrandsActivityTest {
 
         Assert.assertThat(cassavaTestRule.validate(ANALYTIC_VALIDATOR_QUERY_DEALS_BRANDPAGE),
                 hasAllSuccess())
+        activityRule.finishActivity()
     }
 
     private fun actionOnDealsBrandViewHolder() {
@@ -83,6 +92,12 @@ class DealsBrandsActivityTest {
         Thread.sleep(2000)
         onView(CommonMatcher.firstView(withText(DealsDummyResponseString.DUMMY_LOCATION_TWO_STRING))).perform(click())
         Thread.sleep(2000)
+    }
+
+    @After
+    fun tearDown() {
+        gtmLogDbSource.deleteAll().subscribe()
+        Intents.release()
     }
 
     companion object {
