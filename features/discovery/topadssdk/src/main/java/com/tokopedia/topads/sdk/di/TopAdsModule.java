ba1a43252e43de27341947a5b8bec32ec76298bc
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-05-25 15:16:41 +0700

    revert back topadssdk module - because unit & instrumentation test is failed

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/di/TopAdsModule.java
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/di/TopAdsModule.java
@@ -9,6 +9,8 @@ import com.tokopedia.topads.sdk.presenter.BannerAdsPresenter;
 import com.tokopedia.topads.sdk.utils.CacheHandler;
 import com.tokopedia.user.session.UserSession;
 import com.tokopedia.user.session.UserSessionInterface;
+import com.tokopedia.wishlist.common.usecase.AddWishListUseCase;
+import com.tokopedia.wishlist.common.usecase.RemoveWishListUseCase;
 
 import dagger.Module;
 import dagger.Provides;
@@ -43,5 +45,17 @@ public class TopAdsModule {
     @Provides
     CacheHandler provideCacheHandler(@ApplicationContext Context context){
         return new CacheHandler(context, CacheHandler.TOP_ADS_CACHE);
+    }
+
+    @TopAdsScope
+    @Provides
+    AddWishListUseCase addWishListUseCase(@ApplicationContext Context context){
+        return new AddWishListUseCase(context);
+    }
+
+    @TopAdsScope
+    @Provides
+    RemoveWishListUseCase removeWishListUseCase(@ApplicationContext Context context){
+        return new RemoveWishListUseCase(context);
     }
 }
