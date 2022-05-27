219a6db3b441db071b5ce6fab6494a525ede2926
astidhiyaa <asti.anzaria@tokopedia.com>
2022-05-27 17:02:34 +0800

    unittest

--- features/content/play/src/test/java/com/tokopedia/play/viewmodel/interactive/PlayQuizInteractiveTest.kt
+++ features/content/play/src/test/java/com/tokopedia/play/viewmodel/interactive/PlayQuizInteractiveTest.kt
@@ -1,24 +1,36 @@
 package com.tokopedia.play.viewmodel.interactive
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.play.data.interactive.AnswerQuizResponse
+import com.tokopedia.play.data.interactive.PostInteractiveTapResponse
 import com.tokopedia.play.data.repository.PlayViewerInteractiveRepositoryImpl
 import com.tokopedia.play.domain.interactive.AnswerQuizUseCase
 import com.tokopedia.play.domain.repository.PlayViewerInteractiveRepository
-import com.tokopedia.play.model.PlayChannelDataModelBuilder
-import com.tokopedia.play.model.PlayChannelInfoModelBuilder
-import com.tokopedia.play.model.PlaySocketResponseBuilder
-import com.tokopedia.play.model.PlayVideoModelBuilder
+import com.tokopedia.play.model.*
 import com.tokopedia.play.repo.PlayViewerMockRepository
+import com.tokopedia.play.robot.andThen
+import com.tokopedia.play.robot.play.createPlayViewModelRobot
+import com.tokopedia.play.robot.play.givenPlayViewModelRobot
+import com.tokopedia.play.robot.thenVerify
+import com.tokopedia.play.util.*
 import com.tokopedia.play.view.storage.interactive.PlayInteractiveStorage
 import com.tokopedia.play.view.type.PlayChannelType
+import com.tokopedia.play.view.uimodel.action.PlayViewerNewAction
+import com.tokopedia.play.view.uimodel.event.QuizAnsweredEvent
+import com.tokopedia.play.view.uimodel.event.ShowErrorEvent
 import com.tokopedia.play.view.uimodel.mapper.PlayUiModelMapper
+import com.tokopedia.play_common.domain.model.interactive.GetCurrentInteractiveResponse
 import com.tokopedia.play_common.domain.usecase.interactive.GetCurrentInteractiveUseCase
 import com.tokopedia.play_common.model.dto.interactive.InteractiveUiModel
 import com.tokopedia.play_common.model.dto.interactive.PlayCurrentInteractiveModel
+import com.tokopedia.play_common.model.ui.QuizChoicesUiModel
+import com.tokopedia.play_common.view.game.quiz.PlayQuizOptionState
 import com.tokopedia.play_common.websocket.PlayWebSocket
 import com.tokopedia.play_common.websocket.WebSocketAction
 import com.tokopedia.remoteconfig.RemoteConfig
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchers
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -37,7 +49,8 @@ class PlayQuizInteractiveTest {
 
     private val testDispatcher = CoroutineTestDispatchers
 
-    private val interactiveId = "abc"
+    private val interactiveId = "12398"
+    private val title = "Quiz Ikan Koi"
 
     private val socketResponseBuilder = PlaySocketResponseBuilder()
     private val channelDataBuilder = PlayChannelDataModelBuilder()
@@ -59,6 +72,7 @@ class PlayQuizInteractiveTest {
     )
     private val socket: PlayWebSocket = mockk(relaxed = true)
     private val mockMapper: PlayUiModelMapper = mockk(relaxed = true)
+    private val modelBuilder = UiModelBuilder.get()
     private val mockInteractiveStorage = object : PlayInteractiveStorage {
         private var hasJoined = false
         override fun setDetail(interactiveId: String, model: PlayCurrentInteractiveModel) {}
@@ -76,12 +90,15 @@ class PlayQuizInteractiveTest {
     }
 
     private val mockCurrentInteractiveUseCase: GetCurrentInteractiveUseCase = mockk(relaxed = true)
-    private val mockAnswerQUizUseCase: AnswerQuizUseCase = mockk(relaxed = true)
+    private val mockAnswerQuizUseCase: AnswerQuizUseCase = mockk(relaxed = true)
+
+    private val duration = 3000L.millisFromNow()
+    private val waitingDuration = 6000L
 
     private val interactiveRepo: PlayViewerInteractiveRepository =
         PlayViewerInteractiveRepositoryImpl(
             getCurrentInteractiveUseCase = mockCurrentInteractiveUseCase,
-            answerQuizUseCase = mockAnswerQUizUseCase,
+            answerQuizUseCase = mockAnswerQuizUseCase,
             getInteractiveViewerLeaderboardUseCase = mockk(relaxed = true),
             mapper = mockMapper,
             dispatchers = testDispatcher,
@@ -96,9 +113,162 @@ class PlayQuizInteractiveTest {
     @Before
     fun setUp(){
         every { mockRemoteConfig.getBoolean(any(), any()) } returns true
+        every { socket.listenAsFlow() } returns socketFlow
+        coEvery { mockCurrentInteractiveUseCase.executeOnBackground() } returns GetCurrentInteractiveResponse()
     }
 
     @Test
     fun `given quiz is active and answer quiz is success, when click option, then user should have joined the game`() {
+        coEvery { mockMapper.mapInteractive(any<GetCurrentInteractiveResponse.Data>()) } returns InteractiveUiModel.Quiz(
+            id = interactiveId,
+            title = title,
+            waitingDuration = waitingDuration,
+            status = InteractiveUiModel.Quiz.Status.Ongoing(
+                endTime = duration,
+            ),
+            reward = "Sepeda",
+            listOfChoices = listOf(modelBuilder.buildQuizChoices(id = "3", text = "25 June", type = PlayQuizOptionState.Default('a')))
+        )
+
+        coEvery { mockAnswerQuizUseCase.executeOnBackground() } returns AnswerQuizResponse(data = AnswerQuizResponse.Data(correctAnswerID = "3"))
+
+        createPlayViewModelRobot (
+            playChannelWebSocket = socket,
+            repo = mockRepo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+            it.setLoggedIn(true)
+            it.setUserId("1")
+
+            print(it.viewModel.interactiveData)
+
+            mockInteractiveStorage.hasJoined(interactiveId).assertFalse()
+            it.recordEvent {
+                it.viewModel.submitAction(PlayViewerNewAction.ClickQuizOptionAction(item = QuizChoicesUiModel(id = "3", text = "25 June", type = PlayQuizOptionState.Default('a'))))
+            }.last().assertEqualTo(QuizAnsweredEvent)
+            mockInteractiveStorage.hasJoined(interactiveId).assertTrue()
+        }
+    }
+
+    @Test
+    fun `given quiz is active and answer quiz is error, when click option, then user should not have joined the game`() {
+        coEvery { mockMapper.mapInteractive(any<GetCurrentInteractiveResponse.Data>()) } returns InteractiveUiModel.Quiz(
+            id = interactiveId,
+            title = title,
+            waitingDuration = waitingDuration,
+            status = InteractiveUiModel.Quiz.Status.Ongoing(
+                endTime = duration,
+            ),
+            reward = "Sepeda",
+            listOfChoices = listOf(modelBuilder.buildQuizChoices(id = "3", text = "25 June", type = PlayQuizOptionState.Default('a')))
+        )
+        val err = MessageErrorException("Error gk bs jawab y")
+
+        coEvery { mockAnswerQuizUseCase.executeOnBackground() } throws err
+
+        createPlayViewModelRobot (
+            playChannelWebSocket = socket,
+            repo = mockRepo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+            it.setLoggedIn(true)
+            it.setUserId("1")
+
+            mockInteractiveStorage.hasJoined(interactiveId).assertFalse()
+            it.recordEvent {
+                it.viewModel.submitAction(PlayViewerNewAction.ClickQuizOptionAction(item = QuizChoicesUiModel(id = "3", text = "25 June", type = PlayQuizOptionState.Default('a'))))
+            }.last().assertEqualTo(ShowErrorEvent(err))
+            mockInteractiveStorage.hasJoined(interactiveId).assertFalse()
+        }
+    }
+
+    @Test
+    fun `given quiz is active and user has not answer question, then click answer, quiz options state needs to change`() {
+        val selectedId = "3"
+        coEvery { mockMapper.mapInteractive(any<GetCurrentInteractiveResponse.Data>()) } returns InteractiveUiModel.Quiz(
+            id = interactiveId,
+            title = title,
+            waitingDuration = waitingDuration,
+            status = InteractiveUiModel.Quiz.Status.Ongoing(
+                endTime = duration,
+            ),
+            reward = "Sepeda",
+            listOfChoices = listOf(
+                modelBuilder.buildQuizChoices(id = selectedId, text = "25 June", type = PlayQuizOptionState.Default('a')),
+                modelBuilder.buildQuizChoices(id = "31", text = "25 Juky", type = PlayQuizOptionState.Default('b')),
+                modelBuilder.buildQuizChoices(id = "32", text = "25 Juky", type = PlayQuizOptionState.Default('b')),
+            )
+        )
+
+        coEvery { mockAnswerQuizUseCase.executeOnBackground() } returns AnswerQuizResponse(data = AnswerQuizResponse.Data(correctAnswerID = "31"))
+
+        createPlayViewModelRobot (
+            playChannelWebSocket = socket,
+            repo = mockRepo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+        ).use {
+            val eventAndState = it.recordStateAndEvent {
+                createPage(mockChannelData)
+                focusPage(mockChannelData)
+                setLoggedIn(true)
+                setUserId("1")
+
+                mockInteractiveStorage.hasJoined(interactiveId).assertFalse()
+                viewModel.submitAction(PlayViewerNewAction.ClickQuizOptionAction(item = QuizChoicesUiModel(id = selectedId, text = "25 June", type = PlayQuizOptionState.Default('a'))))
+                mockInteractiveStorage.hasJoined(interactiveId).assertTrue()
+            }
+            eventAndState.second.last().assertEqualTo(QuizAnsweredEvent)
+            (eventAndState.first.interactive.interactive as InteractiveUiModel.Quiz).listOfChoices.forEach { quizChoice ->
+                quizChoice.assertType<QuizChoicesUiModel> { choice ->
+                    if(choice.id == selectedId) choice.type is PlayQuizOptionState.Answered
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `given quiz is active and user has already answered question, user can't click any options - not in default state`() {
+        coEvery { mockMapper.mapInteractive(any<GetCurrentInteractiveResponse.Data>()) } returns InteractiveUiModel.Quiz(
+            id = interactiveId,
+            title = title,
+            waitingDuration = waitingDuration,
+            status = InteractiveUiModel.Quiz.Status.Ongoing(
+                endTime = duration,
+            ),
+            reward = "Sepeda",
+            listOfChoices = listOf(
+                modelBuilder.buildQuizChoices(id = "3", text = "25 June", type = PlayQuizOptionState.Answered(false)),
+                modelBuilder.buildQuizChoices(id = "31", text = "25 Juky", type = PlayQuizOptionState.Other(true)),
+                modelBuilder.buildQuizChoices(id = "32", text = "25 Juky", type = PlayQuizOptionState.Other(true)),
+            )
+        )
+
+        coEvery { mockAnswerQuizUseCase.executeOnBackground() } returns AnswerQuizResponse(data = AnswerQuizResponse.Data(correctAnswerID = "31"))
+
+        createPlayViewModelRobot (
+            playChannelWebSocket = socket,
+            repo = mockRepo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+        ).use {
+            val eventAndState = it.recordStateAndEvent {
+                createPage(mockChannelData)
+                focusPage(mockChannelData)
+                setLoggedIn(true)
+                setUserId("1")
+            }
+            (eventAndState.first.interactive.interactive as InteractiveUiModel.Quiz).listOfChoices.forEach { quizChoice ->
+                quizChoice.assertType<QuizChoicesUiModel> { choice ->
+                    choice.type !is PlayQuizOptionState.Default
+                }
+            }
+        }
     }
 }
