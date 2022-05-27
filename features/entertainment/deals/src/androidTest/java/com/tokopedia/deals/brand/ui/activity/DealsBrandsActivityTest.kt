fb54f6ddc300ef454204478e882a63faef881730
firmankagami <firmanda.nugroho@tokopedia.com>
2022-05-27 12:01:49 +0700

    [revert] revert change

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/brand/ui/activity/DealsBrandsActivityTest.kt
@@ -68,7 +68,6 @@ class DealsBrandsActivityTest {
     private fun clickOnRelaksasiTab() {
         Thread.sleep(5000)
         onView(AllOf.allOf(withId(R.id.tab_item_text_id), withText(DUMMY_RESPONSE_SECOND_CATEGORY_TITLE))).perform(click())
-
     }
 
     private fun changeLocationBrandPage() {
