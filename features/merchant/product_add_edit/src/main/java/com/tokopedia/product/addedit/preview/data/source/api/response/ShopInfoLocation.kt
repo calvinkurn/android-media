c3412094712d8a75738909c641c838a79f426a76
faisalramd <faisal.ramdani@tokopedia.com>
2022-05-27 09:26:04 +0700

    fix invalid datatype

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/source/api/response/ShopInfoLocation.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/source/api/response/ShopInfoLocation.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.product.addedit.preview.data.source.api.response
 
+import android.annotation.SuppressLint
 import com.google.gson.annotations.SerializedName
 
 data class ShopInfoById(
@@ -13,6 +14,7 @@ data class Shipping(
 )
 
 data class ShippingLoc(
+        @SuppressLint("Invalid Data Type") // provinceId currently using Integer at server
         @SerializedName("provinceID")
         val provinceId: Int
 )
