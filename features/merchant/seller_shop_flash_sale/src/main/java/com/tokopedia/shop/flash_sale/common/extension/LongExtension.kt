e8cdabaf4f3d5c84fba07c55f50242e00cab5df2
fajarca <fajar.azhar@tokopedia.com>
2022-05-31 12:32:37 +0700

    Optimize code

--- features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flash_sale/common/extension/LongExtension.kt
+++ features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flash_sale/common/extension/LongExtension.kt
@@ -1,42 +1,23 @@
 package com.tokopedia.shop.flash_sale.common.extension
 
-import com.tokopedia.shop.flash_sale.common.constant.DateConstant
 import com.tokopedia.shop.flash_sale.common.constant.LocaleConstant
 import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.OffsetDateTime
 import java.util.*
 
 private const val EPOCH_TO_MILLIS_MULTIPLIER = 1000
+private const val OFFSET_WIB = 7
 
 fun Long.toDate(): Date {
     return Date(this)
 }
 
 fun Long.epochToDate(): Date {
-    return try {
-        val originalDate = Date(this * EPOCH_TO_MILLIS_MULTIPLIER)
-        val simpleDateFormat = SimpleDateFormat(DateConstant.DATE_TIME, LocaleConstant.INDONESIA)
-        simpleDateFormat.timeZone = TimeZone.getTimeZone("GMT")
-        val stringifyOriginalDate = simpleDateFormat.format(originalDate)
-
-        val outputFormat = SimpleDateFormat(DateConstant.DATE_TIME, LocaleConstant.INDONESIA)
-        outputFormat.timeZone = TimeZone.getTimeZone("GMT")
-        val date = outputFormat.parse(stringifyOriginalDate)
-
-
-
-        val adjusted = Calendar.getInstance()
-        adjusted.time = date
-        val hour = adjusted.get(Calendar.HOUR_OF_DAY)
-        val decreased = hour - 7
-        adjusted.set(Calendar.HOUR_OF_DAY, decreased)
-
-        val time = adjusted.time
-        return  time ?: Date() //Converted to local time (WIB)
-    }catch (e:Exception) {
-        Date(this * EPOCH_TO_MILLIS_MULTIPLIER)
-    }
+    val calendar = Calendar.getInstance()
+    calendar.time = Date(this * EPOCH_TO_MILLIS_MULTIPLIER)
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val decreased = hour - OFFSET_WIB
+    calendar.set(Calendar.HOUR_OF_DAY, decreased)
+    return calendar.time
 }
 
 fun Long.epochTo(desiredOutputFormat: String): String {
