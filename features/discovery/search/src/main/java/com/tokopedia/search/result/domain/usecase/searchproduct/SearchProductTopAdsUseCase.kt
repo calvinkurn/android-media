ffb9f5e8fbf37526d4d29bf9c05265aecfe84fc3
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2022-06-02 13:55:48 +0700

    [AN-36223] fix invalid params due to previous params already flattened

--- features/discovery/search/src/main/java/com/tokopedia/search/result/domain/usecase/searchproduct/SearchProductTopAdsUseCase.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/domain/usecase/searchproduct/SearchProductTopAdsUseCase.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.search.result.domain.usecase.searchproduct
 
-import com.tokopedia.discovery.common.constants.SearchConstant.SearchProduct.SEARCH_PRODUCT_PARAMS
 import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.graphql.domain.GraphqlUseCase
 import com.tokopedia.search.result.domain.model.ProductTopAdsModel
@@ -16,9 +15,7 @@ class SearchProductTopAdsUseCase(
     override fun createObservable(
         requestParams: RequestParams
     ): Observable<TopAdsModel> {
-        val searchProductParams = requestParams.parameters[SEARCH_PRODUCT_PARAMS] as Map<String?, Any?>
-
-        val params = UrlParamUtils.generateUrlParamString(searchProductParams)
+        val params = UrlParamUtils.generateUrlParamString(requestParams.parameters)
 
         val graphqlRequestList = graphqlRequests {
             addProductAdsRequest(requestParams, params)
