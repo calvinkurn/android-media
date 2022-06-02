6237f331131964a81285a908dd3cd79f6eeb3968
mayankgera-toko <mayank.gera@tokopedia.com>
2022-06-02 14:47:14 +0530

    UT Coverage Increase
    Signed-off-by: mayankgera-toko <mayank.gera@tokopedia.com>

--- features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/topquest/TopQuestViewModelTest.kt
+++ features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/topquest/TopQuestViewModelTest.kt
@@ -3,15 +3,17 @@ package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.top
 import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.discovery2.data.ComponentsItem
-import com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.tdnbanner.DiscoveryTDNBannerViewModel
+import com.tokopedia.discovery2.data.HideSectionResponse
+import com.tokopedia.discovery2.usecase.HideSectionUseCase
 import com.tokopedia.quest_widget.constants.QuestUrls
-import io.mockk.*
-import junit.framework.TestCase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.*
+import com.tokopedia.quest_widget.util.LiveDataResult
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 class TopQuestViewModelTest {
 
@@ -22,6 +24,9 @@ class TopQuestViewModelTest {
 
     private val viewModel: TopQuestViewModel by lazy {
         spyk(TopQuestViewModel(application, componentsItem, 99))
+    }
+    private val useCase: HideSectionUseCase by lazy {
+        spyk()
     }
 
     @Before
@@ -49,10 +54,10 @@ class TopQuestViewModelTest {
     @Test
     fun `test for onResume when shouldUpdate is true`() {
         viewModel.shouldUpdate = true
-        viewModel.loggedInUpdate = true
         viewModel.onResume()
 
         assert(viewModel.updateQuestData.value == true)
+        assert(!viewModel.shouldUpdate)
     }
 
     @Test
@@ -61,15 +66,70 @@ class TopQuestViewModelTest {
         viewModel.onResume()
 
         assert(!viewModel.loggedInUpdate)
+        assert(viewModel.shouldUpdate)
     }
     /**************************** end of onResume() *******************************************/
 
     /**************************** test for loggedInCallback() *******************************************/
     @Test
-    fun `test for loggedInCallback`(){
+    fun `test for loggedInCallback navigation data`(){
         viewModel.loggedInCallback()
 
         assert(viewModel.navigateData.value == QuestUrls.QUEST_URL)
 
     }
+    @Test
+    fun `test for loggedInCallback loggedInUpdate set`(){
+        viewModel.loggedInUpdate = false
+        viewModel.loggedInCallback()
+        assert(viewModel.loggedInUpdate)
+    }
+    /**************************** end for loggedInCallback() *******************************************/
+
+    /**************************** test for handleWidgetStatus() *******************************************/
+    @Test
+    fun `test for handleWidgetStatus Error should hide widget`(){
+        viewModel.hideSectionUseCase  = useCase
+        every { useCase.checkForHideSectionHandling(componentsItem) } returns HideSectionResponse(false,"")
+        viewModel.handleWidgetStatus(LiveDataResult.STATUS.ERROR)
+        assert(viewModel.shouldHideWidget.value == true)
+    }
+
+    @Test
+    fun `test for handleWidgetStatus Empty State`(){
+        viewModel.hideSectionUseCase  = useCase
+        every { useCase.checkForHideSectionHandling(componentsItem) } returns HideSectionResponse(false,"")
+        viewModel.handleWidgetStatus(LiveDataResult.STATUS.EMPTY_DATA)
+        assert(viewModel.shouldHideWidget.value == true)
+    }
+
+    @Test
+    fun `test for handleWidgetStatus Success`(){
+        viewModel.hideSectionUseCase  = useCase
+        every { useCase.checkForHideSectionHandling(componentsItem) } returns HideSectionResponse(false,"")
+        viewModel.handleWidgetStatus(LiveDataResult.STATUS.SUCCESS)
+        assert(viewModel.shouldHideWidget.value == false)
+    }
+
+    @Test
+    fun `test for handleWidgetStatus Error hide section not required`(){
+        viewModel.hideSectionUseCase  = useCase
+        every { useCase.checkForHideSectionHandling(componentsItem) } returns HideSectionResponse(false,"")
+        viewModel.handleWidgetStatus(LiveDataResult.STATUS.ERROR)
+        assert(viewModel.getSyncPageLiveData().value != true)
+        assert(viewModel.hideSectionLD.value.isNullOrEmpty())
+    }
+    @Test
+    fun `test for handleWidgetStatus Error hide section required`(){
+        viewModel.hideSectionUseCase  = useCase
+        every { useCase.checkForHideSectionHandling(componentsItem) } returns HideSectionResponse(true,"5")
+        viewModel.handleWidgetStatus(LiveDataResult.STATUS.ERROR)
+        assert(viewModel.getSyncPageLiveData().value == true)
+        assert(viewModel.hideSectionLD.value == "5")
+    }
+
+    /**************************** end for handleWidgetStatus() *******************************************/
+
+
+
 }
