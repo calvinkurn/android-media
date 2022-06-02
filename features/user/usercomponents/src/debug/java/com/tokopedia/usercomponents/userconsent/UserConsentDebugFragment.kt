f07c1a52ad1a2acbcf38f7d035abb77edafcaedf
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-06-02 09:38:58 +0700

    user consent add debug page for testing purpose

---
+++ features/user/usercomponents/src/debug/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugFragment.kt
@@ -0,0 +1,70 @@
+package com.tokopedia.usercomponents.userconsent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.usercomponents.databinding.FragmentDebugUserConsentBinding
+import com.tokopedia.usercomponents.userconsent.common.UserConsentPayload
+import com.tokopedia.usercomponents.userconsent.domain.ConsentCollectionParam
+import com.tokopedia.usercomponents.userconsent.ui.UserConsentActionClickListener
+import com.tokopedia.utils.lifecycle.autoClearedNullable
+
+class UserConsentDebugFragment: BaseDaggerFragment() {
+
+    var viewBinding by autoClearedNullable<FragmentDebugUserConsentBinding>()
+
+    override fun getScreenName(): String {
+        return "User Consent Debug Fragment"
+    }
+
+    override fun initInjector() { }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewBinding = FragmentDebugUserConsentBinding.inflate(inflater, container, false)
+        return viewBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding?.textCollextionId?.editText?.setText("6d45e8ce-d46d-4f0e-bf0c-a93f82f75e36")
+        viewBinding?.buttonLoadConsent?.setOnClickListener {
+            loadConsentComponent(
+                viewBinding?.textCollextionId?.editText?.text.toString(),
+                "1"
+            )
+            viewBinding?.sampleUserConsent?.show()
+            viewBinding?.textPayloadData?.hide()
+        }
+    }
+
+    private fun loadConsentComponent(collectionId: String, version: String) {
+        val consentParam = ConsentCollectionParam(collectionId,version)
+        viewBinding?.sampleUserConsent?.apply {
+            actionText = viewBinding?.textActionButton?.editText?.text.toString()
+        }?.load(viewLifecycleOwner, consentParam, object : UserConsentActionClickListener {
+            override fun onChecklistClicked(isChecked: Boolean) {
+
+            }
+
+            override fun onActionClicked(payload: UserConsentPayload) {
+                viewBinding?.textPayloadData?.apply {
+                    text = payload.toString()
+                }?.show()
+            }
+
+            override fun onFailed(throwable: Throwable) {
+                Toast.makeText(context, throwable.message.orEmpty(), Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        viewBinding?.sampleUserConsent?.onDetach()
+    }
+}
