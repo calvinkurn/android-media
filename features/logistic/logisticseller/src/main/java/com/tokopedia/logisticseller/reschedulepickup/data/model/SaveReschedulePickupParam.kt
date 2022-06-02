3cf4b460d67f84225c1477517f212e36197d0117
hiradevina <fakhira.devina@tokopedia.com>
2022-06-02 12:53:16 +0700

    move reschedule pickup to new module

---
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/reschedulepickup/data/model/SaveReschedulePickupParam.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.logisticseller.reschedulepickup.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SaveReschedulePickupParam(
+    @SerializedName("order_ids")
+    val orderIds: List<String> = listOf(),
+    @SerializedName("date")
+    val date: String = "",
+    @SerializedName("time")
+    val time: String = "",
+    @SerializedName("reason")
+    val reason: String = "",
+)
---
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/reschedulepickup/domain/SaveReschedulePickupUseCase.kt
@@ -0,0 +1,39 @@
+package com.tokopedia.logisticseller.reschedulepickup.domain
+
+import com.tokopedia.gql_query_annotation.GqlQuery
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.logisticseller.common.LogisticSellerConst
+import com.tokopedia.logisticseller.reschedulepickup.data.model.SaveReschedulePickupParam
+import com.tokopedia.logisticseller.reschedulepickup.data.model.SaveReschedulePickupResponse
+import javax.inject.Inject
+
+class SaveReschedulePickupUseCase @Inject constructor(private val useCase: GraphqlUseCase<SaveReschedulePickupResponse.Data>) {
+
+    init {
+        useCase.setTypeClass(SaveReschedulePickupResponse.Data::class.java)
+    }
+
+    @GqlQuery(SaveReschedulePickupMutation, MP_LOGISTIC_INSERT_RESCHEDULE_PICKUP)
+    suspend fun execute(param: SaveReschedulePickupParam): SaveReschedulePickupResponse.Data {
+        useCase.setGraphqlQuery(SaveReschedulePickupMutation())
+        useCase.setRequestParams(generateParam(param))
+        return useCase.executeOnBackground()
+    }
+
+    private fun generateParam(param: SaveReschedulePickupParam): Map<String, Any?> {
+        return mapOf(LogisticSellerConst.PARAM_INPUT to param)
+    }
+
+    companion object {
+        const val SaveReschedulePickupMutation = "SaveReschedulePickupMutation"
+        const val MP_LOGISTIC_INSERT_RESCHEDULE_PICKUP = """
+            mutation saveReschedulePickup(${'$'}input:MpLogisticInsertReschedulePickupInputs!){
+                mpLogisticInsertReschedulePickup(input:${'$'}input){
+                    status
+                    message
+                    errors
+                }
+            }
+        """
+    }
+}
