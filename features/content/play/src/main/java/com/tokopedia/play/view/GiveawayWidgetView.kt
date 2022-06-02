dcaf35922f1472c60f232a5c7da233c25946b5cb
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-02 16:52:56 +0800

    scale anim revert

--- features/content/play/src/main/java/com/tokopedia/play/view/GiveawayWidgetView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/GiveawayWidgetView.kt
@@ -4,23 +4,26 @@ import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
-import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
-import com.airbnb.lottie.LottieDrawable
-import com.tokopedia.play.R
 import com.tokopedia.play.databinding.ViewGiveawayWidgetBinding
-import com.tokopedia.play.view.custom.interactive.InteractiveTapView
 import com.tokopedia.play_common.view.game.GameHeaderView
 import com.tokopedia.play_common.view.game.setupGiveaway
-import java.net.UnknownHostException
 import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.atomic.AtomicInteger
+import android.content.res.ColorStateList
+import android.graphics.Color
+
+import android.graphics.drawable.RippleDrawable
+
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.AnticipateOvershootInterpolator
+import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.iconunify.getIconUnifyDrawable
+
 
 /**
  * Created by kenny.hadisaputra on 12/04/22
@@ -47,32 +50,10 @@ class GiveawayWidgetView : ConstraintLayout {
         this,
     )
 
-    private val lottieConfettiListener: Animator.AnimatorListener
-
     private var mListener: Listener? = null
-
-    private val isButtonLottieLoaded = AtomicBoolean(false)
-    private val isConfettiLottieLoaded = AtomicBoolean(false)
-
-    private val buttonLottieRetryCount = AtomicInteger(0)
-    private val confettiLottieRetryCount = AtomicInteger(0)
 
     init {
         setupView()
-
-        lottieConfettiListener = object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator) {}
-
-            override fun onAnimationEnd(animation: Animator) {
-                binding.lottieConfettiTap.progress = 0f
-            }
-
-            override fun onAnimationCancel(animation: Animator) {
-                binding.lottieConfettiTap.progress = 0f
-            }
-
-            override fun onAnimationRepeat(animation: Animator) {}
-        }
     }
 
     override fun onDetachedFromWindow() {
@@ -106,73 +87,47 @@ class GiveawayWidgetView : ConstraintLayout {
 
     private fun setupView() {
         setTitle("")
-        binding.lottieButtonTap.setOnClickListener {
+        binding.flTap.setOnClickListener {
             it.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
 
+            animateTap()
+
             mListener?.onTapTapClicked(this)
-
-            animateView()
         }
     }
 
-    private fun animateView(){
-        fun setupLottieButton() {
-            binding.lottieButtonTap.setFailureListener {
-                if (it is UnknownHostException && buttonLottieRetryCount.getAndIncrement() < 3) loadButtonTapLottie()
-            }
-            binding.lottieButtonTap.addLottieOnCompositionLoadedListener {
-                isButtonLottieLoaded.compareAndSet(false, true)
-                invalidateLottieState()
-            }
-            binding.lottieButtonTap.repeatCount = LottieDrawable.INFINITE
-            binding.lottieButtonTap.playAnimation()
-            loadButtonTapLottie()
+    private fun animateTap() {
+        answerTrueAnimator.addListener(animationListener)
+        answerTrueAnimator.duration = 100L
+        answerTrueAnimator.start()
+    }
+
+    private val clickScaleXAnimation = ObjectAnimator.ofFloat(
+        binding.flTap, View.SCALE_X, 1f, 0.8f
+    )
+    private val clickScaleYAnimation = ObjectAnimator.ofFloat(
+        binding.flTap, View.SCALE_Y, 1f, 0.8f
+    )
+
+    private val answerTrueAnimator = AnimatorSet().apply {
+        playTogether(clickScaleXAnimation, clickScaleYAnimation)
+    }
+
+    private val animationListener = object : Animator.AnimatorListener {
+        override fun onAnimationStart(animation: Animator?) {}
+
+        override fun onAnimationEnd(animation: Animator?) {
+            binding.flTap.scaleX = 1f
+            binding.flTap.scaleY = 1f
         }
 
-        fun setupLottieConfetti() {
-            binding.lottieConfettiTap.setFailureListener {
-                if (it is UnknownHostException && confettiLottieRetryCount.getAndIncrement() < 3) loadConfettiTapLottie()
-            }
-            binding.lottieConfettiTap.addLottieOnCompositionLoadedListener {
-                isConfettiLottieLoaded.compareAndSet(false, true)
-                invalidateLottieState()
-            }
-            binding.lottieConfettiTap.addAnimatorListener(lottieConfettiListener)
-            binding.lottieConfettiTap.setMinProgress(0.3f)
-            loadConfettiTapLottie()
+        override fun onAnimationCancel(animation: Animator?) {
         }
 
-        setupLottieButton()
-        setupLottieConfetti()
-    }
-
-    private fun loadButtonTapLottie() {
-        binding.lottieButtonTap.setAnimationFromUrl(context.getString(R.string.lottie_button_tap),
-            LOTTIE_BUTTON_CACHE_KEY
-        )
-    }
-
-    private fun loadConfettiTapLottie() {
-        binding.lottieConfettiTap.setAnimationFromUrl(context.getString(R.string.lottie_confetti_tap),
-            LOTTIE_CONFETTI_CACHE_KEY
-        )
-    }
-
-    private fun invalidateLottieState() = synchronized(this) {
-        if (isButtonLottieLoaded.get() && isConfettiLottieLoaded.get()) {
-            binding.lottieButtonTap.visibility = View.VISIBLE
-            binding.lottieConfettiTap.visibility = View.VISIBLE
-        } else {
-            binding.lottieButtonTap.visibility = View.GONE
-            binding.lottieConfettiTap.visibility = View.GONE
+        override fun onAnimationRepeat(animation: Animator?) {
         }
+
     }
-
-
-    companion object {
-       private const val LOTTIE_CONFETTI_CACHE_KEY = "CONFETTI_INTERACTIVE_TAP"
-       private const val LOTTIE_BUTTON_CACHE_KEY = "BUTTON_INTERACTIVE_TAP"
-   }
 
     interface Listener {
 
