485a3ac831656b0a4952316390a3be02cee243e8
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-06-02 11:32:32 +0700

    feature : remove plist module

--- features/content/createpost/src/main/java/com/tokopedia/createpost/di/CreatePostComponent.kt
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/di/CreatePostComponent.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.createpost.di
 
-import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.affiliatecommon.analytics.AffiliateAnalytics
 import com.tokopedia.createpost.common.analyics.CreatePostAnalytics
 import com.tokopedia.createpost.common.di.CreatePostScope
@@ -9,7 +8,6 @@ import com.tokopedia.createpost.view.activity.CreatePostActivityNew
 import com.tokopedia.createpost.view.activity.ProductTagActivity
 import com.tokopedia.createpost.view.fragment.BaseCreatePostFragment
 import com.tokopedia.createpost.view.fragment.BaseCreatePostFragmentNew
-import com.tokopedia.createpost.view.plist.*
 import dagger.Component
 
 /**
@@ -32,9 +30,5 @@ interface CreatePostComponent {
 
     fun inject(baseCreatePostFragmentNew: BaseCreatePostFragmentNew)
     fun inject(createPostActivityNew: CreatePostActivityNew)
-    fun inject(shopProductListFragment: ShopProductListFragment)
-    fun inject(shopProductSearchPageListParentFragment: ShopProductSearchPageListParentFragment)
-    fun inject(barangSearchResultFragment: BarangSearchResultFragment)
-    fun inject(tokoSearchResultFragment: TokoSearchResultFragment)
     fun inject(productTagActivity: ProductTagActivity)
 }
