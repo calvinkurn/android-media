75d79a9233154b1f0a722b728f1c4e6680092209
hansenpw <hansen.wijaya@tokopedia.com>
2022-06-03 10:06:27 +0700

    fix notify multiple remove item, fix update is_selected in shop state

--- features/transaction/cart/src/main/java/com/tokopedia/cart/view/adapter/cart/CartAdapter.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/view/adapter/cart/CartAdapter.kt
@@ -1044,12 +1044,15 @@ class CartAdapter @Inject constructor(private val actionListener: ActionListener
                 data is CartShopHolderData -> {
                     val toBeDeletedProducts = mutableListOf<CartItemHolderData>()
                     var hasSelectDeletedProducts = false
+                    var selectedNonDeletedProducts = 0
                     data.productUiModelList.forEach { cartItemHolderData ->
                         if (cartIds.contains(cartItemHolderData.cartId)) {
                             toBeDeletedProducts.add(cartItemHolderData)
                             if (cartItemHolderData.isSelected) {
                                 hasSelectDeletedProducts = true
                             }
+                        } else if (cartItemHolderData.isSelected) {
+                            selectedNonDeletedProducts += 1
                         }
                     }
                     if (toBeDeletedProducts.isNotEmpty()) {
@@ -1066,7 +1069,10 @@ class CartAdapter @Inject constructor(private val actionListener: ActionListener
                             toBeRemovedItems.add(data)
                             toBeRemovedIndices.add(index)
                         } else {
-                            if (!needRefresh && (isFromGlobalCheckbox || hasSelectDeletedProducts)) {
+                            // update selection
+                            data.isAllSelected = selectedNonDeletedProducts > 0 && data.productUiModelList.size == selectedNonDeletedProducts
+                            data.isPartialSelected = selectedNonDeletedProducts > 0 && data.productUiModelList.size > selectedNonDeletedProducts
+                            if (!needRefresh && (isFromGlobalCheckbox || hasSelectDeletedProducts) && selectedNonDeletedProducts > 0) {
                                 actionListener.checkBoAffordability(data)
                             }
                             toBeUpdatedIndices.add(index)
