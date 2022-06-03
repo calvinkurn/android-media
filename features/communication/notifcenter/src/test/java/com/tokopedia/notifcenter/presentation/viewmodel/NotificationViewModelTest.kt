f7f80757e687ad867b92f91044f6078a06123df4
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-06-03 20:11:51 +0700

    fix unit test error on NotificationViewModelTest.kt

--- features/communication/notifcenter/src/test/java/com/tokopedia/notifcenter/presentation/viewmodel/NotificationViewModelTest.kt
+++ features/communication/notifcenter/src/test/java/com/tokopedia/notifcenter/presentation/viewmodel/NotificationViewModelTest.kt
@@ -679,15 +679,13 @@ class NotificationViewModelTest {
         val resultWishlistAddV2 = AddToWishlistV2Response.Data.WishlistAddV2(success = true)
 
         every { addToWishlistV2UseCase.setParams(any(), any()) } just Runs
-        coEvery { addToWishlistV2UseCase.execute(any(), any()) } answers {
-            firstArg<(Success<AddToWishlistV2Response.Data.WishlistAddV2>) -> Unit>().invoke(Success(resultWishlistAddV2))
-        }
+        coEvery { addToWishlistV2UseCase.executeOnBackground() } returns Success(resultWishlistAddV2)
 
         val mockListener: WishlistV2ActionListener = mockk(relaxed = true)
         viewModel.addWishlistV2(recommItem, mockListener)
 
         verify { addToWishlistV2UseCase.setParams(recommItem.productId.toString(), userSessionInterface.userId) }
-        coVerify { addToWishlistV2UseCase.execute(any(), any()) }
+        coVerify { addToWishlistV2UseCase.executeOnBackground() }
     }
 
     @Test
@@ -696,15 +694,13 @@ class NotificationViewModelTest {
         val resultWishlistRemoveV2 = DeleteWishlistV2Response.Data.WishlistRemoveV2(success = true)
 
         every { deleteWishlistV2UseCase.setParams(any(), any()) } just Runs
-        coEvery { deleteWishlistV2UseCase.execute(any(), any()) } answers {
-            firstArg<(Success<DeleteWishlistV2Response.Data.WishlistRemoveV2>) -> Unit>().invoke(Success(resultWishlistRemoveV2))
-        }
+        coEvery { deleteWishlistV2UseCase.executeOnBackground() } returns Success(resultWishlistRemoveV2)
 
         val mockListener: WishlistV2ActionListener = mockk(relaxed = true)
         viewModel.removeWishlistV2(recommItem, mockListener)
 
         verify { deleteWishlistV2UseCase.setParams(recommItem.productId.toString(), userSessionInterface.userId) }
-        coVerify { deleteWishlistV2UseCase.execute(any(), any()) }
+        coVerify { deleteWishlistV2UseCase.executeOnBackground() }
     }
 
     @Test
