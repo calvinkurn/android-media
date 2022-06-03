1e153d52a82d403354593a8182669f279730e21d
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-06-03 16:04:49 +0700

    add cancelJobs on ImagePreviewPdpViewModel.kt

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/imagepreview/view/viewmodel/ImagePreviewPdpViewModel.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/imagepreview/view/viewmodel/ImagePreviewPdpViewModel.kt
@@ -7,7 +7,6 @@ import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.wishlist.common.listener.WishListActionListener
 import com.tokopedia.wishlist.common.usecase.AddWishListUseCase
 import com.tokopedia.wishlist.common.usecase.RemoveWishListUseCase
-import com.tokopedia.wishlistcommon.data.response.AddToWishlistV2Response
 import com.tokopedia.wishlistcommon.domain.AddToWishlistV2UseCase
 import com.tokopedia.wishlistcommon.domain.DeleteWishlistV2UseCase
 import com.tokopedia.wishlistcommon.listener.WishlistV2ActionListener
@@ -104,5 +103,11 @@ class ImagePreviewPdpViewModel @Inject constructor(
     companion object {
         /** Regex pattern is only accepted when product id is numeric, example : "12331213" */
         const val PATTERN_REGEX = "^(?![a-zA-Z])([0-9])*\$"
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        addToWishlistV2UseCase.cancelJobs()
+        deleteWishlistV2UseCase.cancelJobs()
     }
 }
