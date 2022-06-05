ad185f3489518d104147b042ebbb6532df52281b
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-06-05 21:08:53 +0800

    bug fix next monthly refresh

--- features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/domain/usecase/GetMembershipDetailUseCase.kt
+++ features/merchant/power_merchant_subscribe/src/main/java/com/tokopedia/power_merchant/subscribe/domain/usecase/GetMembershipDetailUseCase.kt
@@ -56,6 +56,7 @@ class GetMembershipDetailUseCase @Inject constructor(
                 }
               }
               goldGetPMGradeBenefitInfo(shop_id: ${'$'}shopId, source: "android-goldmerchant", lang: "id", device: "android", fields: ["current_pm_grade", "next_level_benefit_package_list"]) {
+                next_monthly_refresh_date
                 current_pm_grade {
                   grade_name
                 }
