eaee0ce74330436db1fa391de54a3cfbc5aafa75
Dhaba Widhikari <wikodhawi@gmail.com>
2022-06-06 13:11:32 +0700

    on progress cassava test home nav

--- features/discovery/home_nav/src/androidTest/java/com/tokopedia/homenav/environment/InstrumentationHomeNavTestActivity.kt
+++ features/discovery/home_nav/src/androidTest/java/com/tokopedia/homenav/environment/InstrumentationHomeNavTestActivity.kt
@@ -7,11 +7,15 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.analytics.performance.PerformanceMonitoring
 import com.tokopedia.analytics.performance.util.PageLoadTimePerformanceCallback
 import com.tokopedia.analytics.performance.util.PageLoadTimePerformanceInterface
 import com.tokopedia.homenav.view.activity.HomeNavPerformanceInterface
+import com.tokopedia.test.application.annotations.CassavaTest
+import com.tokopedia.cassavatest.CassavaTestRule
+import com.tokopedia.test.application.util.setupGraphqlMockResponse
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -19,6 +23,7 @@ import org.junit.Rule
 /**
  * Created by dhaba
  */
+@CassavaTest
 class InstrumentationHomeNavTestActivity : AppCompatActivity(), HomeNavPerformanceInterface {
     private val navPerformanceMonitoring = PerformanceMonitoring()
     private val navPerformanceCallback = PageLoadTimePerformanceCallback(
@@ -40,6 +45,14 @@ class InstrumentationHomeNavTestActivity : AppCompatActivity(), HomeNavPerforman
 
     override fun getNavPerformanceInterface(): PageLoadTimePerformanceInterface {
         return navPerformanceCallback
+    }
+
+    @get:Rule
+    var activityRule = object: IntentsTestRule<InstrumentationHomeNavTestActivity>(InstrumentationHomeNavTestActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            super.beforeActivityLaunched()
+            setupGraphqlMockResponse(HomeNavMockResponseConfig())
+        }
     }
 
     @get:Rule
