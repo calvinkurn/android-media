008c9729c6ff6a48551c8ef407992e3778b24968
Yuda <yuda.putra@tokopedia.com>
2022-06-07 17:05:08 +0700

    add ViewBinding at campaign teaser info bottom sheet

--- features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flash_sale/presentation/creation/campaign_information/bottomsheet/CampaignTeaserInformationBottomSheet.kt
+++ features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flash_sale/presentation/creation/campaign_information/bottomsheet/CampaignTeaserInformationBottomSheet.kt
@@ -1,39 +1,38 @@
 package com.tokopedia.shop.flash_sale.presentation.creation.campaign_information.bottomsheet
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.text.SpannableString
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import com.tokopedia.seller_shop_flash_sale.R
 import androidx.fragment.app.FragmentManager
+import com.tokopedia.seller_shop_flash_sale.databinding.SsfsBottomSheetInformationCampaignTeaserBinding
 import com.tokopedia.shop.flash_sale.common.extension.toBulletSpan
-
 import com.tokopedia.unifycomponents.BottomSheetUnify
-import com.tokopedia.unifyprinciples.Typography
+import com.tokopedia.utils.lifecycle.autoClearedNullable
 
 class CampaignTeaserInformationBottomSheet : BottomSheetUnify() {
 
     companion object {
-        @JvmStatic
-        fun createInstance(context: Context): CampaignTeaserInformationBottomSheet =
-            CampaignTeaserInformationBottomSheet().apply {
-                val view = View.inflate(
-                    context,
-                    R.layout.ssfs_bottom_sheet_informasi_campaign_teaser,
-                    null
-                )
-                setChild(view)
-            }
-
         private const val TAG = "CampaignTeaserInformationBottomSheet"
     }
 
-    private var campaignTeaserInfoPoint1: Typography? = null
-    private var campaignTeaserInfoPoint2: Typography? = null
+    private var binding by autoClearedNullable<SsfsBottomSheetInformationCampaignTeaserBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = SsfsBottomSheetInformationCampaignTeaserBinding.inflate(inflater, container, false)
+        setChild(binding?.root)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setupView(view)
+        setupView()
         setupContent()
         super.onViewCreated(view, savedInstanceState)
     }
@@ -43,20 +42,19 @@ class CampaignTeaserInformationBottomSheet : BottomSheetUnify() {
     }
 
     @SuppressLint("ResourcePackage")
-    private fun setupView(view: View) {
+    private fun setupView() {
         setTitle(getString(R.string.campaign_teaser_info_title))
-
-        campaignTeaserInfoPoint1 = view.findViewById(R.id.tg_point_1_campaign_info)
-        campaignTeaserInfoPoint2 = view.findViewById(R.id.tg_point_2_campaign_info)
         showCloseIcon = true
     }
 
     private fun setupContent() {
-        campaignTeaserInfoPoint1?.run {
-            text = SpannableString(getString(R.string.campaign_teaser_info_point_1)).toBulletSpan()
-        }
-        campaignTeaserInfoPoint2?.run {
-            text = SpannableString(getString(R.string.campaign_teaser_info_point_2)).toBulletSpan()
+        binding?.run {
+            tgPoint1CampaignInfo.run {
+                text = SpannableString(getString(R.string.campaign_teaser_info_point_1)).toBulletSpan()
+            }
+            tgPoint2CampaignInfo.run {
+                text = SpannableString(getString(R.string.campaign_teaser_info_point_2)).toBulletSpan()
+            }
         }
     }
 }
