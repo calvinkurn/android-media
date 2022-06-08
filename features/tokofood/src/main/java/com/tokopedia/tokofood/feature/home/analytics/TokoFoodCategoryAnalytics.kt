525e797dcacb77e771d1e92886f4791fe5586011
firmankagami <firmanda.nugroho@tokopedia.com>
2022-06-08 11:05:11 +0700

    [Feature] Optimize reusable method

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/home/analytics/TokoFoodCategoryAnalytics.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/home/analytics/TokoFoodCategoryAnalytics.kt
@@ -1,15 +1,16 @@
 package com.tokopedia.tokofood.feature.home.analytics
 
 import android.os.Bundle
-import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.tokofood.common.analytics.TokoFoodAnalytics
 import com.tokopedia.tokofood.common.analytics.TokoFoodAnalyticsConstants
 import com.tokopedia.tokofood.common.domain.response.CheckoutTokoFoodData
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.addGeneralTracker
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.getItemATC
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.getPromotionMerchant
 import com.tokopedia.tokofood.feature.home.domain.data.Merchant
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
 import com.tokopedia.track.builder.util.BaseTrackerConst
-import com.tokopedia.track.constant.TrackerConstant
 
 /**
  * Category
@@ -51,49 +52,12 @@ class TokoFoodCategoryAnalytics: BaseTrackerConst() {
         TrackApp.getInstance().gtm.sendEnhanceEcommerceEvent(TokoFoodAnalyticsConstants.ADD_TO_CART, eventDataLayer)
     }
 
-    private fun getItemATC(data: CheckoutTokoFoodData): ArrayList<Bundle> {
-        val itemBundles = arrayListOf<Bundle>()
-        itemBundles.addAll(
-            data.availableSection.products.map {
-                Bundle().apply {
-                    putString(TokoFoodAnalytics.KEY_CATEGORY_ID, "//TODO_CATEGORY_ID")
-                    putString(TokoFoodAnalytics.KEY_DIMENSION_45, it.cartId)
-                    putString(Items.ITEM_BRAND, "//TODO_BRAND_ID")
-                    putString(Items.ITEM_CATEGORY, "//TODO_ITEM_CATEGORY")
-                    putString(Items.ITEM_ID, it.productId)
-                    putString(Items.ITEM_NAME, it.productName)
-                    putString(Items.ITEM_VARIANT, "//TODO_ITEM_VARIANT")
-                    putDouble(Items.PRICE, it.price)
-                    putInt(Items.QUANTITY, it.quantity)
-                    putString(TrackerConstant.SHOP_ID, data.shop.shopId)
-                    putString(TokoFoodAnalytics.KEY_SHOP_NAME, data.shop.name)
-                    putString(TokoFoodAnalytics.KEY_SHOP_TYPE, "//TODO_SHOP_TYPE")
-                }
-            }
-        )
-        return itemBundles
-    }
-
-    private fun getPromotionMerchant(merchant: Merchant, horizontalPosition: Int): ArrayList<Bundle> {
-        val promotionBundle = arrayListOf<Bundle>()
-        promotionBundle.add(
-            Bundle().apply {
-                putString(Promotion.CREATIVE_NAME, "")
-                putString(Promotion.CREATIVE_SLOT, (horizontalPosition + Int.ONE).toString())
-                putString(Promotion.ITEM_ID, "${merchant.id} - ${merchant.name}")
-                putString(Promotion.ITEM_NAME, "//TODO_MERCHANT_LOCATION - ${merchant.etaFmt} - ${merchant.distanceFmt} - ${merchant.ratingFmt}")
-            }
-        )
-        return promotionBundle
-    }
-
     private fun Bundle.addToCart(userId: String?, destinationId: String?): Bundle {
         addGeneralTracker(userId, destinationId)
         this.putString(TrackAppUtils.EVENT, TokoFoodAnalyticsConstants.ADD_TO_CART)
         this.putString(TrackAppUtils.EVENT_CATEGORY, TokoFoodAnalytics.EVENT_CATEGORY_CATEGORY_PAGE)
         return this
     }
-
 
     private fun Bundle.viewItem(userId: String?, destinationId: String?): Bundle {
         addGeneralTracker(userId, destinationId)
@@ -106,14 +70,6 @@ class TokoFoodCategoryAnalytics: BaseTrackerConst() {
         addGeneralTracker(userId, destinationId)
         this.putString(TrackAppUtils.EVENT, Event.SELECT_CONTENT)
         this.putString(TrackAppUtils.EVENT_CATEGORY, TokoFoodAnalytics.EVENT_CATEGORY_CATEGORY_PAGE)
-        return this
-    }
-
-    private fun Bundle.addGeneralTracker(userId: String?, destinationId: String?): Bundle {
-        this.putString(TokoFoodAnalyticsConstants.BUSSINESS_UNIT, TokoFoodAnalyticsConstants.PHYSICAL_GOODS)
-        this.putString(TokoFoodAnalyticsConstants.CURRENT_SITE, TokoFoodAnalyticsConstants.TOKOPEDIA_MARKETPLACE)
-        this.putString(TokoFoodAnalyticsConstants.USER_ID, userId ?: TokoFoodAnalyticsConstants.EMPTY_DATA)
-        this.putString(TokoFoodAnalyticsConstants.DESTINATION_ID, destinationId ?: TokoFoodAnalyticsConstants.EMPTY_DATA)
         return this
     }
 }
--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/home/analytics/TokoFoodHomeAnalytics.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/home/analytics/TokoFoodHomeAnalytics.kt
@@ -28,6 +28,9 @@ import com.tokopedia.tokofood.common.analytics.TokoFoodAnalyticsConstants.OUT_OF
 import com.tokopedia.tokofood.common.analytics.TokoFoodAnalyticsConstants.SCREEN_NAME
 import com.tokopedia.tokofood.common.analytics.TokoFoodAnalyticsConstants.VIEW_ITEM
 import com.tokopedia.tokofood.common.domain.response.CheckoutTokoFoodData
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.addGeneralTracker
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.getItemATC
+import com.tokopedia.tokofood.feature.home.analytics.TokoFoodHomeCategoryCommonAnalytics.getPromotionMerchant
 import com.tokopedia.tokofood.feature.home.domain.constanta.TokoFoodHomeLayoutType
 import com.tokopedia.tokofood.feature.home.domain.data.DynamicIcon
 import com.tokopedia.tokofood.feature.home.domain.data.Merchant
@@ -35,7 +38,6 @@ import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
 import com.tokopedia.track.builder.util.BaseTrackerConst
 import com.tokopedia.track.builder.util.BaseTrackerConst.Event.SELECT_CONTENT
-import com.tokopedia.track.constant.TrackerConstant
 
 /**
  * Home
@@ -260,42 +262,6 @@ class TokoFoodHomeAnalytics: BaseTrackerConst() {
         return promotionBundle
     }
 
-    private fun getPromotionMerchant(merchant: Merchant, horizontalPosition: Int): ArrayList<Bundle> {
-        val promotionBundle = arrayListOf<Bundle>()
-        promotionBundle.add(
-                Bundle().apply {
-                    putString(Promotion.CREATIVE_NAME, "")
-                    putString(Promotion.CREATIVE_SLOT, (horizontalPosition + Int.ONE).toString())
-                    putString(Promotion.ITEM_ID, "${merchant.id} - ${merchant.name}")
-                    putString(Promotion.ITEM_NAME, "//TODO_MERCHANT_LOCATION - ${merchant.etaFmt} - ${merchant.distanceFmt} - ${merchant.ratingFmt}")
-            }
-        )
-        return promotionBundle
-    }
-
-    private fun getItemATC(data: CheckoutTokoFoodData): ArrayList<Bundle> {
-        val itemBundles = arrayListOf<Bundle>()
-        itemBundles.addAll(
-            data.availableSection.products.map {
-                Bundle().apply {
-                    putString(TokoFoodAnalytics.KEY_CATEGORY_ID, "//TODO_CATEGORY_ID")
-                    putString(TokoFoodAnalytics.KEY_DIMENSION_45, it.cartId)
-                    putString(Items.ITEM_BRAND, "//TODO_BRAND_ID")
-                    putString(Items.ITEM_CATEGORY, "//TODO_ITEM_CATEGORY")
-                    putString(Items.ITEM_ID, it.productId)
-                    putString(Items.ITEM_NAME, it.productName)
-                    putString(Items.ITEM_VARIANT, "//TODO_ITEM_VARIANT")
-                    putDouble(Items.PRICE, it.price)
-                    putInt(Items.QUANTITY, it.quantity)
-                    putString(TrackerConstant.SHOP_ID, data.shop.shopId)
-                    putString(TokoFoodAnalytics.KEY_SHOP_NAME, data.shop.name)
-                    putString(TokoFoodAnalytics.KEY_SHOP_TYPE, "//TODO_SHOP_TYPE")
-                }
-            }
-        )
-        return itemBundles
-    }
-
     private fun Bundle.openScreen(userId: String?, destinationId: String?, isLoggenInStatus: Boolean): Bundle {
         addGeneralTracker(userId, destinationId)
         this.putString(TrackAppUtils.EVENT, OPEN_SCREEN)
@@ -328,14 +294,6 @@ class TokoFoodHomeAnalytics: BaseTrackerConst() {
         addGeneralTracker(userId, destinationId)
         this.putString(TrackAppUtils.EVENT, SELECT_CONTENT)
         this.putString(TrackAppUtils.EVENT_CATEGORY, TokoFoodAnalytics.EVENT_CATEGORY_HOME_PAGE)
-        return this
-    }
-
-    private fun Bundle.addGeneralTracker(userId: String?, destinationId: String?): Bundle {
-        this.putString(TokoFoodAnalyticsConstants.BUSSINESS_UNIT, TokoFoodAnalyticsConstants.PHYSICAL_GOODS)
-        this.putString(TokoFoodAnalyticsConstants.CURRENT_SITE, TokoFoodAnalyticsConstants.TOKOPEDIA_MARKETPLACE)
-        this.putString(TokoFoodAnalyticsConstants.USER_ID, userId ?: EMPTY_DATA)
-        this.putString(TokoFoodAnalyticsConstants.DESTINATION_ID, destinationId ?: EMPTY_DATA)
         return this
     }
 }
