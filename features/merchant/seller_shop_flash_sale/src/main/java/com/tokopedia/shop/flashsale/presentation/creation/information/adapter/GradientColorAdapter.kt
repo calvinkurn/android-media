ebf82397ef6c8f0db81629ae143d099d740b829b
fajarca <fajar.azhar@tokopedia.com>
2022-06-09 22:19:07 +0700

    Use diff util on color adapter

--- features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/presentation/creation/information/adapter/GradientColorAdapter.kt
+++ features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/presentation/creation/information/adapter/GradientColorAdapter.kt
@@ -2,10 +2,10 @@ package com.tokopedia.shop.flashsale.presentation.creation.information.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.seller_shop_flash_sale.databinding.SsfsItemColorBinding
-import com.tokopedia.shop.flashsale.common.constant.Constant
 import com.tokopedia.shop.flashsale.common.extension.setBackgroundFromGradient
 import com.tokopedia.shop.flashsale.domain.entity.Gradient
 
@@ -28,13 +28,21 @@ class GradientColorAdapter : RecyclerView.Adapter<GradientColorAdapter.GradientV
         holder.bind(gradients[position])
     }
 
-    fun submit(items: List<Gradient>) {
-        this.gradients = items.toMutableList()
-        notifyItemRangeChanged(Constant.ZERO, items.size)
+    fun submit(newGradients: List<Gradient>) {
+        val callback = DiffUtilCallback(gradients, newGradients)
+        val diffResult = DiffUtil.calculateDiff(callback)
+
+        diffResult.dispatchUpdatesTo(this)
+        gradients.clear()
+        gradients.addAll(newGradients)
     }
 
     fun getItems(): List<Gradient> {
         return gradients
+    }
+
+    fun setOnGradientClicked(onGradientClicked: (Gradient) -> Unit) {
+        this.onGradientClicked = onGradientClicked
     }
 
     inner class GradientViewHolder(
@@ -48,7 +56,24 @@ class GradientColorAdapter : RecyclerView.Adapter<GradientColorAdapter.GradientV
         }
     }
 
-    fun setOnGradientClicked(onGradientClicked: (Gradient) -> Unit) {
-        this.onGradientClicked = onGradientClicked
+    inner class DiffUtilCallback(
+        private val oldList: List<Gradient>,
+        private val newList: List<Gradient>
+    ) : DiffUtil.Callback() {
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            val oldItem = oldList[oldItemPosition]
+            val newItem = newList[newItemPosition]
+            return oldItem.isSelected == newItem.isSelected
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
     }
+
 }
