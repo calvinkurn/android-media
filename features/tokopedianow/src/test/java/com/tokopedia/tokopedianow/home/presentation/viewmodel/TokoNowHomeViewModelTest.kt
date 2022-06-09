ad82ab4b37900576ce718bd014e81f93c9585125
saidfaisal <saidfaisal777@gmail.com>
2022-06-09 16:27:55 +0700

    Revert "fix switcher name of event label tracker"
    This reverts commit f9d59bd87ba843973691f35aff766e679706b48d.

--- features/tokopedianow/src/test/java/com/tokopedia/tokopedianow/home/presentation/viewmodel/TokoNowHomeViewModelTest.kt
+++ features/tokopedianow/src/test/java/com/tokopedia/tokopedianow/home/presentation/viewmodel/TokoNowHomeViewModelTest.kt
@@ -3304,7 +3304,7 @@ class TokoNowHomeViewModelTest: TokoNowHomeViewModelTestFixture() {
             userId = "",
             whIdOrigin = "111111",
             whIdDestination = "222222",
-            is20mSwitcher = true,
+            isNow15 = false,
             isImpressionTracker = isImpressionTracker
         ))
     }
@@ -3343,7 +3343,7 @@ class TokoNowHomeViewModelTest: TokoNowHomeViewModelTestFixture() {
             userId = "",
             whIdOrigin = "222222",
             whIdDestination = "111111",
-            is20mSwitcher = false,
+            isNow15 = true,
             isImpressionTracker = isImpressionTracker
         ))
     }
@@ -3367,7 +3367,7 @@ class TokoNowHomeViewModelTest: TokoNowHomeViewModelTestFixture() {
             userId = "",
             whIdOrigin = "222222",
             whIdDestination = "0",
-            is20mSwitcher = false,
+            isNow15 = true,
             isImpressionTracker = isImpressionTracker
         ))
     }
