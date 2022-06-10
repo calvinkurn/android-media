de9d658ab7713c96e79bbf57ab44455f34a6d19f
Steven <steven@BA-00003246.local>
2022-06-10 12:10:31 +0700

    rever sample

--- features/discovery/global_nav_widget_samples/src/main/java/com/tokopedia/globalnavwidgetsamples/GlobalNavWidgetSamplesActivity.kt
+++ features/discovery/global_nav_widget_samples/src/main/java/com/tokopedia/globalnavwidgetsamples/GlobalNavWidgetSamplesActivity.kt
@@ -25,7 +25,6 @@ class GlobalNavWidgetSamplesActivity : AppCompatActivity(), GlobalNavWidgetListe
         binding.singleGlobalNavWidgetCardIconWithNoSubtitleInfo.setData(singleGlobalNavWidgetModelCardIconWithNoSubtitleInfo, this)
         binding.singleGlobalNavWidgetCardIcon.setData(singleGlobalNavWidgetModelCardIcon, this)
         binding.singleGlobalNavWidgetCardImage.setData(singleGlobalNavWidgetModelCardImage, this)
-        binding.globalNavWidgetCatalogWidgetModel.setData(globalNavWidgetCatalogWidgetModel, this)
     }
 
     override fun onClickItem(item: GlobalNavWidgetModel.Item) {
