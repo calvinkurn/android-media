102eba39dfba5569403a7156fa2927243e8db094
devarafikry <devara.akmal@tokopedia.com>
2022-06-10 15:55:41 +0700

    redirect recom from external to discovery page

--- libraries/applink/src/main/java/com/tokopedia/applink/recommendation/DeeplinkMapperRecommendation.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/recommendation/DeeplinkMapperRecommendation.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.applink.recommendation
 
 import android.net.Uri
+import com.tokopedia.applink.constant.DeeplinkConstant
 import com.tokopedia.applink.internal.ApplinkConsInternalHome
 
 fun getRegisteredNavigationRecommendation(deeplink: String): String {
@@ -11,9 +12,19 @@ fun getRegisteredNavigationRecommendation(deeplink: String): String {
     }
 }
 
-fun getRegisteredNavigationRecommendationFromHttp(uri: Uri): String {
+private const val RECOM_PRODUCT_ID = "recomProductId"
+fun getRegisteredNavigationRecommendationFromHttp(deeplink: String): String {
+    val uri = Uri.parse(deeplink)
     return when {
-        uri.pathSegments.size > 0 -> ApplinkConsInternalHome.DISCOVERY_HOME_RECOMMENDATION + uri.path
+        uri.pathSegments.size > 0 -> {
+            ApplinkConsInternalHome.DISCOVERY_HOME_RECOMMENDATION + uri.path
+            Uri.Builder()
+                .scheme(DeeplinkConstant.SCHEME_TOKOPEDIA)
+                .appendPath(ApplinkConsInternalHome.DISCOVERY_HOME_RECOMMENDATION)
+                .appendQueryParameter(RECOM_PRODUCT_ID, uri.pathSegments[1])
+                .build()
+                .toString() + "?${uri.query}"
+        }
         else -> ApplinkConsInternalHome.DISCOVERY_HOME_RECOMMENDATION
     }
 }
