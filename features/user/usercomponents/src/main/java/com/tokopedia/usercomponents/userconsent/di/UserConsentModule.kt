0dc500bd9f282b860ac76043ca789e730e5a011d
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-06-13 13:15:49 +0700

    remove unused code

--- features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/di/UserConsentModule.kt
+++ features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/di/UserConsentModule.kt
@@ -1,9 +1,6 @@
 package com.tokopedia.usercomponents.userconsent.di
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
@@ -14,16 +11,5 @@ class UserConsentModule {
 
     @Provides
     @ActivityScope
-    @ApplicationContext
-    fun provideContext(@ApplicationContext context: Context): Context = context
-
-    @Provides
-    @ActivityScope
     fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.Main
-
-    @Provides
-    @ActivityScope
-    fun provideGraphQlRepository(@ApplicationContext repository: GraphqlRepository): GraphqlRepository {
-        return repository
-    }
 }
