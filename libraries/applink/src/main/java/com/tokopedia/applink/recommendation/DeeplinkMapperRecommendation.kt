ba7b3d40fcd1aaae65835dfb65105418ddb4b2d1
devarafikry <devara.akmal@tokopedia.com>
2022-06-13 12:13:50 +0700

    redirect to disco page

--- libraries/applink/src/main/java/com/tokopedia/applink/recommendation/DeeplinkMapperRecommendation.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/recommendation/DeeplinkMapperRecommendation.kt
@@ -3,6 +3,9 @@ package com.tokopedia.applink.recommendation
 import android.net.Uri
 import com.tokopedia.applink.constant.DeeplinkConstant
 import com.tokopedia.applink.internal.ApplinkConsInternalHome
+import com.tokopedia.applink.order.DeeplinkMapperOrder
+import com.tokopedia.kotlin.extensions.view.EMPTY
+import java.net.URLDecoder
 
 fun getRegisteredNavigationRecommendation(deeplink: String): String {
     val uri = Uri.parse(deeplink)
@@ -14,17 +17,31 @@ fun getRegisteredNavigationRecommendation(deeplink: String): String {
 
 private const val RECOM_PRODUCT_ID = "recomProdId"
 fun getRegisteredNavigationRecommendationFromHttp(uri: Uri): String {
-    return when {
-        uri.pathSegments.size > 0 -> {
-            Uri.Builder()
-                .scheme(DeeplinkConstant.SCHEME_TOKOPEDIA)
-                .authority(ApplinkConsInternalHome.AUTHORITY_DISCOVERY)
-                .appendPath(ApplinkConsInternalHome.PATH_REKOMENDASI)
-                .appendQueryParameter(RECOM_PRODUCT_ID, uri.pathSegments[1])
-                .build()
-                .toString() + "?${uri.query}"
+    try {
+        val decodedUrl = URLDecoder.decode(uri.toString(), Charsets.UTF_8.name())
+        val pattern = "^https://www\\.tokopedia\\.com(/(rekomendasi))".toRegex()
+        val result = pattern.find(decodedUrl)
+        val matches = result?.groupValues
+        if (!matches.isNullOrEmpty()) {
+            return redirectToDiscoveryRecom(uri)
         }
-        else -> Uri.Builder()
+    } catch (e: Exception) {
+        return String.EMPTY
+    }
+    return String.EMPTY
+}
+
+fun redirectToDiscoveryRecom(uri: Uri): String {
+    if (uri.pathSegments.size > 0) {
+        Uri.Builder()
+            .scheme(DeeplinkConstant.SCHEME_TOKOPEDIA)
+            .authority(ApplinkConsInternalHome.AUTHORITY_DISCOVERY)
+            .appendPath(ApplinkConsInternalHome.PATH_REKOMENDASI)
+            .appendQueryParameter(RECOM_PRODUCT_ID, uri.pathSegments[1])
+            .build()
+            .toString() + "?${uri.query}"
+    } else {
+        Uri.Builder()
             .scheme(DeeplinkConstant.SCHEME_TOKOPEDIA)
             .authority(ApplinkConsInternalHome.AUTHORITY_DISCOVERY)
             .appendPath(ApplinkConsInternalHome.PATH_REKOMENDASI).toString()
