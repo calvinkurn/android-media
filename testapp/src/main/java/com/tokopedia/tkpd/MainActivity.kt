fb15594fc469a3d50df28d1d143526c186a2c2dc
Irpan - <irpan@tokopedia.com>
2022-06-13 10:51:04 +0800

    revert main Activity

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -13,7 +13,6 @@ import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalGlobal
 import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform
-import com.tokopedia.manageaddress.ui.manageaddress.ManageAddressActivity
 import com.tokopedia.tkpd.testgql.TestGqlUseCase
 import com.tokopedia.url.TokopediaUrl
 import com.tokopedia.user.session.UserSession
@@ -121,7 +120,6 @@ class MainActivity : AppCompatActivity() {
         if(appLink.isNotBlank())
             RouteManager.route(this, appLink)
         else Toast.makeText(this, "Please input appLink / webLink", Toast.LENGTH_SHORT).show()
-        startActivity(Intent(this,ManageAddressActivity::class.java))
     }
 
     private fun getDefaultAppLink(): String {
