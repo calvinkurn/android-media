dec591e6ca09732f9155ef5a723ee6908283523b
Dhaba Widhikari <wikodhawi@gmail.com>
2022-06-13 09:51:28 +0700

    Add type and location when refresh mission widget

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/usecase/HomeMissionWidgetUseCase.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/interactor/usecase/HomeMissionWidgetUseCase.kt
@@ -1,22 +1,32 @@
 package com.tokopedia.home.beranda.domain.interactor.usecase
 
+import android.os.Bundle
+import com.tokopedia.home.beranda.domain.interactor.repository.HomeChooseAddressRepository
 import com.tokopedia.home.beranda.domain.interactor.repository.HomeMissionWidgetRepository
 import com.tokopedia.home.beranda.helper.MissionWidgetHelper
 import com.tokopedia.home.constant.ConstantKey
 import com.tokopedia.home_component.visitable.MissionWidgetListDataModel
+import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils.convertToLocationParams
 import javax.inject.Inject
 
 /**
  * Created by dhaba
  */
 class HomeMissionWidgetUseCase @Inject constructor(
-        private val missionWidgetRepository: HomeMissionWidgetRepository
+        private val missionWidgetRepository: HomeMissionWidgetRepository,
+        private val homeChooseAddressRepository: HomeChooseAddressRepository
         ) {
 
     suspend fun onMissionWidgetRefresh(currentMissionWidgetListDataModel: MissionWidgetListDataModel): MissionWidgetListDataModel {
         return try {
-            val results = missionWidgetRepository.executeOnBackground()
-            val resultList = MissionWidgetHelper.convertMissionWidgetDataList(results.getHomeMissionWidget.missions)
+            val results = missionWidgetRepository.getRemoteData(Bundle().apply {
+                putString(
+                    HomeMissionWidgetRepository.BANNER_LOCATION_PARAM,
+                    homeChooseAddressRepository.getRemoteData()?.convertToLocationParams()
+                )
+            })
+            val resultList =
+                MissionWidgetHelper.convertMissionWidgetDataList(results.getHomeMissionWidget.missions)
             currentMissionWidgetListDataModel.copy(
                 missionWidgetList = resultList,
                 status = ConstantKey.LoadDataKey.STATUS_SUCCESS
