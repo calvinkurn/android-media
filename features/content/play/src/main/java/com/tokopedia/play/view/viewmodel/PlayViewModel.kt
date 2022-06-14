6136d526dece8738d89f0e8bdf363b8d8c7a1efa
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-14 22:05:07 +0700

    `fix` code

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -1742,7 +1742,7 @@ class PlayViewModel @AssistedInject constructor(
 
     private fun handleQuizEnded() {
         viewModelScope.launchCatchError(dispatchers.computation, block = {
-            val interactive = _interactive.getAndUpdate {
+            val interactive = _interactive.updateAndGet {
                 if (it.interactive !is InteractiveUiModel.Quiz) error("Error")
                 val newInteractive = it.interactive.copy(
                     status = InteractiveUiModel.Quiz.Status.Finished
