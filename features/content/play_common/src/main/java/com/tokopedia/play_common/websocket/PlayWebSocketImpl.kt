9a35091a5dd7152484fa2b7c08891b99c4aa383d
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-14 13:44:14 +0700

    `testing purpose` add logger

--- features/content/play_common/src/main/java/com/tokopedia/play_common/websocket/PlayWebSocketImpl.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/websocket/PlayWebSocketImpl.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.play_common.websocket
 
 import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.abstraction.common.utils.LocalCacheHandler
@@ -75,6 +76,7 @@ class PlayWebSocketImpl(
     override fun connect(channelId: String, warehouseId: String, gcToken: String, source: String) {
         close()
         val url = generateUrl(channelId, warehouseId, gcToken)
+        Log.d("sukses socket", url)
         mWebSocket = client.newWebSocket(getRequest(url, userSession.accessToken), webSocketListener)
         WebSocketLogger.getInstance(context).init(buildGeneralInfo(channelId, warehouseId, gcToken, source).toString())
     }
