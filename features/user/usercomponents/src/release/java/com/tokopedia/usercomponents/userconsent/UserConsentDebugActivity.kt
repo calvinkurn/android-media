19bdaf8b32bc788101f57ff49d90e263675d2cd0
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-06-14 14:11:02 +0700

    add shadow class

---
+++ features/user/usercomponents/src/release/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugActivity.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.usercomponents.userconsent
+
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
+class UserConsentDebugActivity: BaseSimpleActivity() {
+
+    override fun getNewFragment(): Fragment? {
+        return null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
