44f536bf55f5b0fbdd0b48f2c7e219b2c4cb7319
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-06-15 10:40:43 +0700

    rollback changes MainActivity.kt

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -125,6 +125,6 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return ApplinkConstInternalUserPlatform.NEW_PROFILE_INFO
+        return ""
     }
 }
