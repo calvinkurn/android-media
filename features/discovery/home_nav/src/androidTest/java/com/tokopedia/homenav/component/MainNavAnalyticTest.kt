6765ee0fee3df808e42dbe61495055a3d7f203b1
Dhaba Widhikari <wikodhawi@gmail.com>
2022-06-16 14:11:35 +0700

    Show activity cassava test

---
+++ features/discovery/home_nav/src/androidTest/java/com/tokopedia/homenav/component/MainNavAnalyticTest.kt
@@ -0,0 +1,86 @@
+package com.tokopedia.homenav.component
+
+import android.app.Activity
+import android.app.Instrumentation
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.cassavatest.CassavaTestRule
+import com.tokopedia.homenav.util.MainNavRecyclerViewIdlingResource
+import com.tokopedia.homenav.view.activity.HomeNavActivity
+import com.tokopedia.remoteconfig.RemoteConfigInstance
+import com.tokopedia.remoteconfig.RollenceKey
+import com.tokopedia.test.application.annotations.CassavaTest
+import com.tokopedia.test.application.util.InstrumentationAuthHelper
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Created by dhaba
+ */
+private const val TAG = "HomeNavAnalyticsTest"
+@CassavaTest
+class HomeNavAnalyticTest {
+    @get:Rule
+    var activityRule = object: IntentsTestRule<HomeNavActivity>(
+        HomeNavActivity::class.java) {
+        override fun beforeActivityLaunched() {
+            super.beforeActivityLaunched()
+            setupAbTestRemoteConfig()
+        }
+    }
+
+    @get:Rule
+    var cassavaTestRule = CassavaTestRule()
+
+    private var mainNavRecyclerViewIdlingResource: MainNavRecyclerViewIdlingResource? = null
+
+    @Before
+    fun resetAll() {
+        Intents.intending(IntentMatchers.isInternal()).respondWith(
+            Instrumentation.ActivityResult(
+                Activity.RESULT_OK,
+                null
+            )
+        )
+        val recyclerView: RecyclerView =
+                activityRule.activity.findViewById(com.tokopedia.homenav.R.id.recycler_view)
+        mainNavRecyclerViewIdlingResource = MainNavRecyclerViewIdlingResource(
+            recyclerView = recyclerView
+        )
+        IdlingRegistry.getInstance().register(mainNavRecyclerViewIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(mainNavRecyclerViewIdlingResource)
+    }
+
+    private fun initTest() {
+        InstrumentationAuthHelper.clearUserSession()
+        login()
+        waitForData()
+    }
+
+    @Test
+    fun testComponentProductHighlight() {
+        initTest()
+    }
+
+    private fun login() {
+        InstrumentationAuthHelper.loginInstrumentationTestTopAdsUser()
+        InstrumentationAuthHelper.loginToAnUser(activityRule.activity.application)
+    }
+
+    private fun setupAbTestRemoteConfig() {
+        RemoteConfigInstance.getInstance().abTestPlatform.setString(
+            RollenceKey.ME_PAGE_REVAMP,
+            RollenceKey.ME_PAGE_REVAMP_VARIANT
+        )
+    }
+}
--- features/discovery/home_nav/src/androidTest/java/com/tokopedia/homenav/component/MainNavInstrumentHelper.kt
+++ features/discovery/home_nav/src/androidTest/java/com/tokopedia/homenav/component/MainNavInstrumentHelper.kt
@@ -5,3 +5,7 @@ package com.tokopedia.homenav.component
  */
 
 const val ANALYTIC_VALIDATOR_QUERY_FILE_NAME_SHOP_AFFILIATE = "tracker/home_nav/shop_affiliate.json"
+
+fun waitForData() {
+    Thread.sleep(4000)
+}
