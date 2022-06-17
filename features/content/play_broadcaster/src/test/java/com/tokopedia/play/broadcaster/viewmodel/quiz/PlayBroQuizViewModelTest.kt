f5cd60537994d3455015548ac4eeae18a4e00f9a
mohamad.andriyan <andriyan@tokopedia.com>
2022-06-17 16:32:08 +0700

    fixing some failed test on create quiz

--- features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/quiz/PlayBroQuizViewModelTest.kt
+++ features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/quiz/PlayBroQuizViewModelTest.kt
@@ -8,10 +8,15 @@ import com.tokopedia.play.broadcaster.pusher.mediator.PusherMediator
 import com.tokopedia.play.broadcaster.robot.PlayBroadcastViewModelRobot
 import com.tokopedia.play.broadcaster.ui.action.PlayBroadcastAction
 import com.tokopedia.play.broadcaster.ui.event.PlayBroadcastEvent
+import com.tokopedia.play.broadcaster.ui.model.game.quiz.QuizChoiceDetailUiModel
+import com.tokopedia.play.broadcaster.ui.model.game.quiz.QuizDetailDataUiModel
+import com.tokopedia.play.broadcaster.ui.model.game.quiz.QuizDetailStateUiModel
 import com.tokopedia.play.broadcaster.ui.model.game.quiz.QuizFormDataUiModel
 import com.tokopedia.play.broadcaster.util.assertEqualTo
 import com.tokopedia.play.broadcaster.util.preference.HydraSharedPreferences
 import com.tokopedia.play_common.model.dto.interactive.InteractiveUiModel
+import com.tokopedia.play_common.model.ui.QuizChoicesUiModel
+import com.tokopedia.play_common.view.game.quiz.PlayQuizOptionState
 import com.tokopedia.unit.test.rule.CoroutineTestRule
 import io.mockk.coEvery
 import io.mockk.coJustRun
@@ -33,28 +38,27 @@ class PlayBroQuizViewModelTest {
 
     private val testDispatcher = rule.dispatchers
 
-    private val mockLivePusher: PusherMediator = mockk(relaxed = true)
     private val mockRepo: PlayBroadcastRepository = mockk(relaxed = true)
     private val mockSharedPref: HydraSharedPreferences = mockk(relaxed = true)
 
     private val uiModelBuilder = UiModelBuilder()
 
+    private val mockConfig = uiModelBuilder.buildConfigurationUiModel(
+        streamAllowed = true,
+        channelId = "123"
+    )
+
     private val mockException = uiModelBuilder.buildException()
 
-    private val mockQuizUiModel = InteractiveUiModel.Quiz(
-        "1",
-        "quiz",
-        100,
-        InteractiveUiModel.Quiz.Status.Ongoing(Calendar.getInstance().apply {
-            add(Calendar.SECOND, 1800)
-        }),
-        emptyList(),
-        ""
+    private val mockQuizUiModel = uiModelBuilder.buildQuizModel(
+        id = "1",
+        title = "quiz",
+        waitingDuration = 15,
+        duration = 180,
+        choices = emptyList()
     )
     @Test
     fun `when user successfully create new quiz, it should return quiz model`() {
-
-        every { mockLivePusher.remainingDurationInMillis } returns 10_000
         coJustRun {
             mockRepo.createInteractiveQuiz(
                 any(),
@@ -65,74 +69,51 @@ class PlayBroQuizViewModelTest {
             )
         }
         coEvery { mockRepo.getCurrentInteractive(any()) } returns mockQuizUiModel
+        coEvery { mockRepo.getChannelConfiguration() } returns mockConfig
         val robot = PlayBroadcastViewModelRobot(
-            livePusherMediator = mockLivePusher,
             dispatchers = testDispatcher,
             channelRepo = mockRepo,
-            sharedPref = mockSharedPref,
         )
 
         robot.use {
             val state = robot.recordState {
-                it.getViewModel().submitAction(PlayBroadcastAction.SubmitQuizForm)
+                getConfig()
+                getViewModel().submitAction(PlayBroadcastAction.SubmitQuizForm)
             }
-
-            //please check don't know why interactive type didn't changed to quiz, it still in type unknown
             Assertions.assertThat(state.interactive)
                 .isInstanceOf(InteractiveUiModel.Quiz::class.java)
         }
     }
 
-
     @Test
-    fun `when user click on ongoing quiz widget should trigger event to display bottom sheet quiz detail`() {
-
-        coEvery { mockRepo.getCurrentInteractive(any()) } returns mockQuizUiModel
+    fun `when ongoing quiz ended, it should return unknown interactive model`(){
+        coEvery { mockRepo.getChannelConfiguration() } returns mockConfig
         val robot = PlayBroadcastViewModelRobot(
-            livePusherMediator = mockLivePusher,
             dispatchers = testDispatcher,
             channelRepo = mockRepo,
         )
-
-
-        robot.use {
-            val events = robot.recordEvent {
-                it.getViewModel().submitAction(PlayBroadcastAction.ClickOngoingWidget)
-            }
-//            Assertions.assertThat(events.last())
-//                .isInstanceOf(PlayBroadcastEvent.ShowQuizDetailBottomSheet::class.java)
-        }
-    }
-
-    @Test
-    fun `when ongoing quiz ended, it should return unknown interactive model`(){
-        val robot = PlayBroadcastViewModelRobot(
-            livePusherMediator =  mockLivePusher,
-            dispatchers = testDispatcher,
-        )
-
         robot.use {
             val state = robot.recordState {
+                getConfig()
                 it.getViewModel().submitAction(PlayBroadcastAction.QuizEnded)
             }
             Assertions.assertThat(state.interactive).isInstanceOf(InteractiveUiModel.Unknown::class.java)
         }
     }
 
-
     @Test
-    fun `when user fill quiz question`(){
-
+    fun `when user fill quiz question state form data title must be same as user input`() {
+        coEvery { mockRepo.getChannelConfiguration() } returns mockConfig
         val question = "pertanyaan"
         val robot = PlayBroadcastViewModelRobot(
             dispatchers = testDispatcher,
+            channelRepo = mockRepo,
         )
-
         robot.use {
-            val state = robot.recordState {
-                it.getViewModel().submitAction(PlayBroadcastAction.InputQuizTitle(question))
+            val state = it.recordState {
+                getConfig()
+                getViewModel().submitAction(PlayBroadcastAction.InputQuizTitle(question))
             }
-            // please check why state didn't change
             state.quizForm.quizFormData.title.assertEqualTo(question)
         }
     }
