ab03508c35e5c98e6d191b7e98eb5faa4373d20d
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-17 13:18:35 +0700

    `feat add analytics`

--- features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalytic.kt
+++ features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalytic.kt
@@ -4,4 +4,22 @@ package com.tokopedia.play.analytic.tokonow
  * @author by astidhiyaa on 17/06/22
  */
 interface PlayTokonowAnalytic {
+    fun impressAddressWidget()
+    fun impressChooseAddress()
+    fun clickChooseAddress()
+    fun clickInfoAddressWidget()
+    fun impressInfoNow()
+    fun clickInfoNow()
+
+    /**
+     * Can be merged with the existing
+     */
+    fun impressCarouselNow()
+    fun clickCarouselNow()
+    fun impressProductNow()
+    fun clickProductNow()
+    fun clickATNow()
+    fun clickBuyNow()
+    fun impressNowToaster()
+    fun clickLihatNowToaster()
 }
