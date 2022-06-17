43ab51e96daecadc9ec182ab58900a9c7d54cfe6
souravsaikia <sourav.saikia@tokopedia.com>
2022-06-17 12:48:59 +0530

    removed unused code

--- features/user/chatbot/src/main/java/com/tokopedia/chatbot/domain/usecase/GetTopBotNewSessionUseCase.kt
+++ features/user/chatbot/src/main/java/com/tokopedia/chatbot/domain/usecase/GetTopBotNewSessionUseCase.kt
@@ -2,6 +2,7 @@ package com.tokopedia.chatbot.domain.usecase
 
 import com.tokopedia.basemvvm.repository.BaseRepository
 import com.tokopedia.chatbot.data.newsession.TopBotNewSessionResponse
+import com.tokopedia.chatbot.domain.gqlqueries.GQL_GET_TOP_BOT_NEW_SESSION
 import com.tokopedia.chatbot.domain.gqlqueries.GetTopBotNewSessionQuery
 import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
@@ -10,29 +11,18 @@ import com.tokopedia.usecase.RequestParams
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
-const val GET_NEW_SESSION_QUERY = """query topbotGetNewSession(${'$'}msgId :String!, ${'$'}userId :String!){
-    topbotGetNewSession(msgID: ${'$'}msgId, userID: ${'$'}userId) {
-    isNewSession
-    isTypingBlocked
-  }
-  }"""
-
-private const val USER_ID = "userId"
-private const val MSG_ID = "msgId"
-
-@GqlQuery("GetNewSessionQuery", GET_NEW_SESSION_QUERY)
+@GqlQuery("GetNewSessionQuery", GQL_GET_TOP_BOT_NEW_SESSION)
 class GetTopBotNewSessionUseCase @Inject constructor(
     private val userSession: UserSessionInterface,
     graphqlRepository: GraphqlRepository
-) : GraphqlUseCase<TopBotNewSessionResponse>(graphqlRepository)
-{
+) : GraphqlUseCase<TopBotNewSessionResponse>(graphqlRepository) {
     @Inject
     lateinit var baseRepository: BaseRepository
 
     fun getTopBotUserSession(
-        onSuccess : (TopBotNewSessionResponse) -> Unit,
-        onError : (Throwable) -> Unit,
-        msgId : String
+        onSuccess: (TopBotNewSessionResponse) -> Unit,
+        onError: (Throwable) -> Unit,
+        msgId: String
     ) {
         try {
             this.setTypeClass(TopBotNewSessionResponse::class.java)
@@ -40,29 +30,18 @@ class GetTopBotNewSessionUseCase @Inject constructor(
             this.setGraphqlQuery(GetTopBotNewSessionQuery())
 
             this.execute(
-                {
-                    result ->
-                        onSuccess(result)
-                }, {
-                    error ->
-                        onError(error)
+                { result ->
+                    onSuccess(result)
+                }, { error ->
+                    onError(error)
                 }
             )
-        } catch (throwable : Throwable) {
+        } catch (throwable: Throwable) {
             onError(throwable)
         }
     }
 
-
-    suspend fun getTobBotUserSession(params: RequestParams): TopBotNewSessionResponse {
-        return baseRepository.getGQLData(
-            GetNewSessionQuery.GQL_QUERY,
-            TopBotNewSessionResponse::class.java,
-            params.parameters
-        )
-    }
-
-    private fun createParams(msgId : String) : Map<String,Any> {
+    private fun createParams(msgId: String): Map<String, Any> {
         return mapOf(
             MSG_ID to msgId,
             USER_ID to userSession.userId
@@ -70,10 +49,9 @@ class GetTopBotNewSessionUseCase @Inject constructor(
 
     }
 
-    fun createRequestParams(msgId: String): RequestParams {
-        val requestParams = RequestParams.create()
-        requestParams.putString(MSG_ID, msgId)
-        requestParams.putString(USER_ID, userSession.userId)
-        return requestParams
+    companion object {
+        private const val USER_ID = "userId"
+        private const val MSG_ID = "msgId"
     }
+
 }
