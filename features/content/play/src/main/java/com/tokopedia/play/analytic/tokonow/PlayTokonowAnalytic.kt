0d68d3f1e5b5d45a6d86ee01c3e937a790277d90
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-17 13:45:51 +0700

    `feat add analytics`

--- features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalytic.kt
+++ features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalytic.kt
@@ -1,19 +1,21 @@
 package com.tokopedia.play.analytic.tokonow
 
+import com.tokopedia.play.view.type.PlayChannelType
+
 /**
  * @author by astidhiyaa on 17/06/22
  */
 interface PlayTokonowAnalytic {
-    fun impressAddressWidget()
-    fun impressChooseAddress()
-    fun clickChooseAddress()
-    fun clickInfoAddressWidget()
-    fun impressInfoNow()
-    fun clickInfoNow()
+    fun impressAddressWidget(channelId: String, channelType: PlayChannelType)
+    fun impressChooseAddress(channelId: String, channelType: PlayChannelType)
+    fun clickChooseAddress(channelId: String, channelType: PlayChannelType)
+    fun clickInfoAddressWidget(channelId: String, channelType: PlayChannelType)
+    fun impressInfoNow(channelId: String, channelType: PlayChannelType)
+    fun clickInfoNow(channelId: String, channelType: PlayChannelType)
 
     /**
      * Can be merged with the existing
-     */
+
     fun impressCarouselNow()
     fun clickCarouselNow()
     fun impressProductNow()
@@ -22,4 +24,6 @@ interface PlayTokonowAnalytic {
     fun clickBuyNow()
     fun impressNowToaster()
     fun clickLihatNowToaster()
+
+     */
 }
--- features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalyticImpl.kt
+++ features/content/play/src/main/java/com/tokopedia/play/analytic/tokonow/PlayTokonowAnalyticImpl.kt
@@ -1,9 +1,95 @@
 package com.tokopedia.play.analytic.tokonow
 
+import com.tokopedia.play.analytic.*
+import com.tokopedia.play.analytic.KEY_TRACK_GROUP_CHAT_ROOM
+import com.tokopedia.play.analytic.KEY_TRACK_VIEW_CONTENT_IRIS
+import com.tokopedia.play.view.type.PlayChannelType
+import com.tokopedia.track.TrackApp
+import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
 /**
  * @author by astidhiyaa on 17/06/22
  */
-class PlayTokonowAnalyticImpl @Inject constructor(): PlayTokonowAnalytic {
+class PlayTokonowAnalyticImpl @Inject constructor(private val userSession: UserSessionInterface): PlayTokonowAnalytic {
+    private val userId: String
+        get() = userSession.userId
+
+    override fun impressAddressWidget(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "view - out of now coverage",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32235"
+            )
+        )
+    }
+
+    override fun impressChooseAddress(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "view - ganti alamat",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32237"
+            )
+        )
+    }
+
+    override fun clickChooseAddress(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "click - ganti alamat",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32239"
+            )
+        )
+    }
+
+    override fun clickInfoAddressWidget(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "click - now info selengkapnya",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32238"
+            )
+        )
+    }
+
+    override fun impressInfoNow(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "view - now info bottomsheet",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32252"
+            )
+        )
+    }
+
+    override fun clickInfoNow(channelId: String, channelType: PlayChannelType) {
+        TrackApp.getInstance().gtm.sendGeneralEvent(
+            mapOf(
+                KEY_EVENT to KEY_TRACK_VIEW_CONTENT_IRIS,
+                KEY_EVENT_CATEGORY to KEY_TRACK_GROUP_CHAT_ROOM,
+                KEY_EVENT_ACTION to "click - now info bottomsheet\"",
+                KEY_EVENT_LABEL to "$channelId - ${channelType.value}",
+                KEY_BUSINESS_UNIT to KEY_TRACK_BUSINESS_UNIT,
+                KEY_TRACKER_ID to "32253"
+            )
+        )
+    }
 }
