55a9d507c5a9c3acdf00e2c7627c14710ffe59aa
fajarca <fajar.azhar@tokopedia.com>
2022-06-20 18:07:24 +0700

    Add sort by

--- features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/data/request/GetSellerCampaignListRequest.kt
+++ features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/data/request/GetSellerCampaignListRequest.kt
@@ -13,17 +13,23 @@ data class GetSellerCampaignListRequest(
     @SerializedName("pagination")
     val pagination: Pagination,
     @SerializedName("filter")
-    val filter: Filter
+    val filter: Filter,
+    @SerializedName("sort")
+    val sort: Sort
 ) {
     data class Pagination(
         @SerializedName("rows") val rows: Int,
         @SerializedName("offset") val offset: Int
     )
 
+    data class Sort(
+        @SerializedName("order_by") val orderBy: Int,
+        @SerializedName("order_rule") val orderRule: Int
+    )
+
     data class Filter(
         @SuppressLint("Invalid Data Type") @SerializedName("status_id") val status_id: List<Int>,
         @SuppressLint("Invalid Data Type") @SerializedName("campaign_id") val campaignId: Long,
-        @SerializedName("campaign_name") val campaignName: String,
-        @SerializedName("thematic_participation") val thematicParticipation: Boolean
+        @SerializedName("campaign_name") val campaignName: String
     )
 }
