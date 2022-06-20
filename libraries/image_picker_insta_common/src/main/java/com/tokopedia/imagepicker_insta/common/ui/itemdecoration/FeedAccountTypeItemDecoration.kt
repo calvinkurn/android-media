531acac70459bf203bd98930089b23bc6caa8892
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-06-20 17:31:12 +0700

    fix : adjust margin top and bottom on feed account type selection bottomsheet

--- libraries/image_picker_insta_common/src/main/java/com/tokopedia/imagepicker_insta/common/ui/itemdecoration/FeedAccountTypeItemDecoration.kt
+++ libraries/image_picker_insta_common/src/main/java/com/tokopedia/imagepicker_insta/common/ui/itemdecoration/FeedAccountTypeItemDecoration.kt
@@ -4,15 +4,17 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.imagepicker_insta.common.R
 
 /**
  * Created By : Jonathan Darwin on April 13, 2022
  */
 class FeedAccountTypeItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
 
-    private val offset16 = context.resources.getDimensionPixelOffset(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl4)
+    private val offset12 = context.resources.getDimensionPixelOffset(R.dimen.dp_12)
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-        outRect.bottom = offset16
+        outRect.bottom = offset12
+        outRect.top = offset12
     }
 }
