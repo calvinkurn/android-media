ecc04ea6e2506b11d5bfaa6e134a89e3cbf38c14
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-06-20 18:05:59 +0700

    `address comment` : use 12dp unify

--- libraries/image_picker_insta_common/src/main/java/com/tokopedia/imagepicker_insta/common/ui/itemdecoration/FeedAccountTypeItemDecoration.kt
+++ libraries/image_picker_insta_common/src/main/java/com/tokopedia/imagepicker_insta/common/ui/itemdecoration/FeedAccountTypeItemDecoration.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.imagepicker_insta.common.R
+import com.tokopedia.unifyprinciples.R as unifyR
 
 /**
  * Created By : Jonathan Darwin on April 13, 2022
  */
 class FeedAccountTypeItemDecoration(context: Context) : RecyclerView.ItemDecoration() {
 
-    private val offset12 = context.resources.getDimensionPixelOffset(R.dimen.dp_12)
+    private val offset12 = context.resources.getDimensionPixelOffset(unifyR.dimen.unify_space_12)
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         outRect.bottom = offset12
