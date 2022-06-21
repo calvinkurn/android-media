9caba25bc782b04005a8021d5b45bd229ebe9ad1
isfaaghyth <isfaaghyth@gmail.com>
2022-06-21 18:04:57 +0700

    cerry-pick from https://github.com/tokopedia/android-tokopedia-core/pull/26933

--- features/media/mediapicker/src/main/java/com/tokopedia/media/preview/ui/activity/PickerPreviewActivity.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/media/preview/ui/activity/PickerPreviewActivity.kt
@@ -3,6 +3,7 @@ package com.tokopedia.media.preview.ui.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.tokopedia.abstraction.base.app.BaseMainApplication
@@ -12,6 +13,7 @@ import com.tokopedia.loaderdialog.LoaderDialog
 import com.tokopedia.media.R
 import com.tokopedia.media.common.utils.ParamCacheManager
 import com.tokopedia.media.databinding.ActivityPreviewBinding
+import com.tokopedia.media.picker.ui.activity.picker.PickerActivity
 import com.tokopedia.media.picker.ui.widget.drawerselector.DrawerActionType
 import com.tokopedia.media.picker.ui.widget.drawerselector.DrawerSelectionWidget
 import com.tokopedia.media.preview.analytics.PREVIEW_PAGE_LANJUT
@@ -38,8 +40,9 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
 import javax.inject.Inject
 
-class PickerPreviewActivity : BaseActivity(), NavToolbarComponent.Listener,
-    DrawerSelectionWidget.Listener {
+class PickerPreviewActivity : BaseActivity()
+    , NavToolbarComponent.Listener
+    , DrawerSelectionWidget.Listener {
 
     @Inject
     lateinit var param: ParamCacheManager
@@ -311,6 +314,15 @@ class PickerPreviewActivity : BaseActivity(), NavToolbarComponent.Listener,
 
     companion object {
         private const val CACHE_LAST_SELECTION = "cache_last_selection"
+
+        fun start(activity: ComponentActivity, medias: ArrayList<MediaUiModel>, reqCode: Int) {
+            activity.startActivityForResult(
+                Intent(activity.applicationContext, PickerPreviewActivity::class.java).apply {
+                    putExtra(EXTRA_INTENT_PREVIEW, medias)
+                },
+                reqCode
+            )
+        }
     }
 
 }
