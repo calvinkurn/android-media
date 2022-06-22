fccb78f523c2a99865d4b51234c585bfba48f1fc
astidhiyaa <asti.anzaria@tokopedia.com>
2022-06-22 16:00:04 +0700

    Revert "revert: logs"
    This reverts commit 40de42ca

--- features/content/play/src/main/java/com/tokopedia/play/analytic/interactive/PlayInteractiveAnalyticImpl.kt
+++ features/content/play/src/main/java/com/tokopedia/play/analytic/interactive/PlayInteractiveAnalyticImpl.kt
@@ -28,8 +28,9 @@ class PlayInteractiveAnalyticImpl @Inject constructor(
         channelType: PlayChannelType,
         ) {
         val (eventAction, eventLabel) = when(interactiveType){
+            is InteractiveUiModel.Quiz -> Pair("click - follow quiz popup","$shopId - $channelId - $userId - $interactiveId")
             is InteractiveUiModel.Giveaway -> Pair("click follow from engagement tools widget","$channelId - $channelType - $interactiveId")
-            else -> Pair("click - follow quiz popup","$shopId - $channelId - $userId - $interactiveId")
+            else -> Pair("","")
         }
         sendCompleteGeneralEvent(
             event = if(interactiveType is InteractiveUiModel.Quiz) KEY_TRACK_CLICK_CONTENT else KEY_TRACK_CLICK_GROUP_CHAT,
