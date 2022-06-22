2540650098604f4556226cb4fa3951bc3beac7a8
devarafikry <devara.akmal@tokopedia.com>
2022-06-22 14:42:18 +0700

    create macrobenchmark test for app startup time

---
+++ libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/test/app/AppStartupBenchmark.kt
@@ -0,0 +1,40 @@
+package com.tkpd.macrobenchmark.test.app
+
+import androidx.benchmark.macro.StartupMode
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SdkSuppress
+import com.tkpd.macrobenchmark.base.BaseStartupBenchmark
+import com.tkpd.macrobenchmark.util.MacroDevOps
+import com.tkpd.macrobenchmark.util.MacroIntent
+import com.tkpd.macrobenchmark.util.MacroInteration
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * Created by DevAra
+ * This test will measure App Startup Time launch time with macro benchmark
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+
+@LargeTest
+@SdkSuppress(minSdkVersion = 29)
+@RunWith(Parameterized::class)
+class AppStartupBenchmark(startupMode: StartupMode): BaseStartupBenchmark(startupMode) {
+    /**
+     * To disable introduction page
+     */
+    @Before
+    fun setupEnvironment() {
+        MacroDevOps.setupEnvironment(MacroIntent.App.getAppMacroSetupIntent())
+    }
+    override fun getIntent() = MacroIntent.App.getAppLauncherIntent()
+
+    override fun waitUntil() {
+        MacroInteration.waitForRecyclerViewContent(
+                MacroIntent.TKPD_PACKAGE_NAME,
+                MacroIntent.App.RV_RESOURCE_ID
+        )
+    }
+}
