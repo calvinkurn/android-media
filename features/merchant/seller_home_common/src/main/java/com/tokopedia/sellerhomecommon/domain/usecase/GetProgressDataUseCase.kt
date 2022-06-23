95e8d54cdb665a21e53542a9581509ff7962a679
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-06-23 14:05:46 +0800

    remove unused GqlGetProgressData.kt

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/usecase/GetProgressDataUseCase.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/usecase/GetProgressDataUseCase.kt
@@ -62,6 +62,7 @@ class GetProgressDataUseCase constructor(
                   error
                   errorMsg
                   showWidget
+                  updateInfo
                 }
               }
             }
