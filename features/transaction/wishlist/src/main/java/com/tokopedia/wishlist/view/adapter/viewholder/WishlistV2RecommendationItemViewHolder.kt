61bdf9689ab54c27d7a111f8c93c0bc508e19d6c
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-06-23 17:38:20 +0700

    reformat code

--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlist/view/adapter/viewholder/WishlistV2RecommendationItemViewHolder.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlist/view/adapter/viewholder/WishlistV2RecommendationItemViewHolder.kt
@@ -8,7 +8,10 @@ import com.tokopedia.wishlist.data.model.WishlistV2TypeLayoutData
 import com.tokopedia.wishlist.databinding.WishlistV2RecommendationItemBinding
 import com.tokopedia.wishlist.view.adapter.WishlistV2Adapter
 
-class WishlistV2RecommendationItemViewHolder(private val binding: WishlistV2RecommendationItemBinding, private val actionListener: WishlistV2Adapter.ActionListener?) : RecyclerView.ViewHolder(binding.root) {
+class WishlistV2RecommendationItemViewHolder(
+    private val binding: WishlistV2RecommendationItemBinding,
+    private val actionListener: WishlistV2Adapter.ActionListener?
+) : RecyclerView.ViewHolder(binding.root) {
     private val cardView: ProductCardGridView by lazy { binding.wishlistProductItem }
 
     fun bind(item: WishlistV2TypeLayoutData, adapterPosition: Int) {
@@ -22,12 +25,15 @@ class WishlistV2RecommendationItemViewHolder(private val binding: WishlistV2Reco
                 }
 
                 setImageProductViewHintListener(
-                        item.recommItem,
-                        object : ViewHintListener {
-                            override fun onViewHint() {
-                                actionListener?.onRecommendationItemImpression(item.recommItem, adapterPosition)
-                            }
+                    item.recommItem,
+                    object : ViewHintListener {
+                        override fun onViewHint() {
+                            actionListener?.onRecommendationItemImpression(
+                                item.recommItem,
+                                adapterPosition
+                            )
                         }
+                    }
                 )
             }
         }
