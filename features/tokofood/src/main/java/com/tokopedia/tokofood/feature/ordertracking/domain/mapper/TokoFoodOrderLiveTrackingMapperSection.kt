17750e669a66cf06bbb3bb6d7104e7f1bdd66c4c
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-06-27 14:29:40 +0700

    fix divider double

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/ordertracking/domain/mapper/TokoFoodOrderLiveTrackingMapperSection.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/ordertracking/domain/mapper/TokoFoodOrderLiveTrackingMapperSection.kt
@@ -16,7 +16,6 @@ class TokoFoodOrderLiveTrackingMapperSection @Inject constructor() : BaseOrderDe
             addOrderTrackingStatusInfo(tokoFoodOrderDetail.orderStatus)
             addEstimationUiModel(tokoFoodOrderDetail.eta)
             addDriverSectionUiModel(tokoFoodOrderDetail.driverDetails)
-            addThinDividerUiModel(MARGIN_TOP_TWENTY)
             addRestaurantUserAddress(tokoFoodOrderDetail.merchant, tokoFoodOrderDetail.destination)
             addThickDividerUiModel()
             addOrderDetailHeaderUiModel()
