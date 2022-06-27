6a94092fb5dd7c2affa68aefb2233fa99e348aa5
reivinoktavianus <oktavianusreivin@gmail.com>
2022-06-27 10:16:37 +0700

    Fix agree consent use case

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/purchase/purchasepage/domain/model/response/AgreeConsentResponse.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/purchase/purchasepage/domain/model/response/AgreeConsentResponse.kt
@@ -1,26 +1,15 @@
 package com.tokopedia.tokofood.feature.purchase.purchasepage.domain.model.response
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import com.tokopedia.tokofood.common.domain.TokoFoodCartUtil
 
 data class AgreeConsentResponse(
-    @SerializedName("data")
-    @Expose
-    val data: AgreeConsent = AgreeConsent()
-)
-
-data class AgreeConsent(
     @SerializedName("tokofoodSubmitUserConsent")
-    @Expose
     val tokofoodSubmitUserConsent: AgreeConsentData = AgreeConsentData()
 )
 
 data class AgreeConsentData(
     @SerializedName("success")
-    @Expose
     val isSuccess: Boolean = false,
     @SerializedName("message")
-    @Expose
     val message: String = ""
 )
