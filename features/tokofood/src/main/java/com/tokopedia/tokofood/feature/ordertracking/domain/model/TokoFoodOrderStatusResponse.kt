cc1fbbc576ffed368b6de93ac4ce61196ba53c2f
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-06-27 12:26:10 +0700

    remove @expose unused

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/ordertracking/domain/model/TokoFoodOrderStatusResponse.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/ordertracking/domain/model/TokoFoodOrderStatusResponse.kt
@@ -1,28 +1,21 @@
 package com.tokopedia.tokofood.feature.ordertracking.domain.model
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class TokoFoodOrderStatusResponse(
     @SerializedName("tokofoodOrderDetail")
-    @Expose
     val tokofoodOrderDetail: TokofoodOrderDetail = TokofoodOrderDetail()
 ) {
     data class TokofoodOrderDetail(
         @SerializedName("additionalTickerInfo")
-        @Expose
         val additionalTickerInfo: List<TokoFoodOrderDetailResponse.TokofoodOrderDetail.AdditionalTickerInfo>? = listOf(),
         @SerializedName("eta")
-        @Expose
         val eta: TokoFoodOrderDetailResponse.TokofoodOrderDetail.Eta? = TokoFoodOrderDetailResponse.TokofoodOrderDetail.Eta(),
         @SerializedName("invoice")
-        @Expose
         val invoice: TokoFoodOrderDetailResponse.TokofoodOrderDetail.Invoice = TokoFoodOrderDetailResponse.TokofoodOrderDetail.Invoice(),
         @SerializedName("orderStatus")
-        @Expose
         val orderStatus: TokoFoodOrderDetailResponse.TokofoodOrderDetail.OrderStatus = TokoFoodOrderDetailResponse.TokofoodOrderDetail.OrderStatus(),
         @SerializedName("payment")
-        @Expose
         val payment: TokoFoodOrderDetailResponse.TokofoodOrderDetail.Payment = TokoFoodOrderDetailResponse.TokofoodOrderDetail.Payment()
     )
 }
