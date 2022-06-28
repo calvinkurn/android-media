64bd43675770dbb0e07cdae9499b6a9645fcec9f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-06-28 14:24:43 +0700

    `refactor` : implement repo

--- features/content/people/src/main/java/com/tokopedia/people/data/UserProfileRepositoryImpl.kt
+++ features/content/people/src/main/java/com/tokopedia/people/data/UserProfileRepositoryImpl.kt
@@ -5,6 +5,12 @@ import com.tokopedia.people.domains.*
 import com.tokopedia.people.domains.repository.UserProfileRepository
 import com.tokopedia.feedcomponent.domain.usecase.GetWhitelistUseCase
 import com.tokopedia.feedcomponent.domain.usecase.GetWhitelistUseCase.Companion.WHITELIST_ENTRY_POINT
+import com.tokopedia.people.model.ProfileHeaderBase
+import com.tokopedia.people.views.uimodel.mapper.UserProfileUiMapper
+import com.tokopedia.people.views.uimodel.profile.FollowInfoUiModel
+import com.tokopedia.people.views.uimodel.profile.ProfileUiModel
+import com.tokopedia.people.views.uimodel.profile.ProfileWhitelistUiModel
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -12,6 +18,7 @@ import javax.inject.Inject
  */
 class UserProfileRepositoryImpl @Inject constructor(
     private val dispatcher: CoroutineDispatchers,
+    private val mapper: UserProfileUiMapper,
     private val userDetailsUseCase: UserDetailsUseCase,
     private val playVodUseCase: PlayPostContentUseCase,
     private val useCaseDoFollow: ProfileFollowUseCase,
@@ -21,4 +28,26 @@ class UserProfileRepositoryImpl @Inject constructor(
     private val getWhitelistUseCase: GetWhitelistUseCase,
 ) : UserProfileRepository {
 
+    override suspend fun getProfile(username: String): ProfileUiModel {
+        return withContext(dispatcher.io) {
+            val result = userDetailsUseCase.getUserProfileDetail(username)
+
+            mapper.mapUserProfile(result.getData(ProfileHeaderBase::class.java))
+        }
+    }
+
+    override suspend fun getFollowInfo(profileIdList: List<String>): FollowInfoUiModel {
+        return withContext(dispatcher.io) {
+            val result = profileIsFollowing.profileIsFollowing(profileIdList)
+
+            mapper.mapFollowInfo(result)
+        }
+    }
+
+    override suspend fun getWhitelist(): ProfileWhitelistUiModel {
+        TODO("will be done later")
+//        return withContext(dispatcher.io) {
+//            val result =
+//        }
+    }
 }
--- features/content/people/src/main/java/com/tokopedia/people/domains/repository/UserProfileRepository.kt
+++ features/content/people/src/main/java/com/tokopedia/people/domains/repository/UserProfileRepository.kt
@@ -1,7 +1,17 @@
 package com.tokopedia.people.domains.repository
 
+import com.tokopedia.people.views.uimodel.profile.FollowInfoUiModel
+import com.tokopedia.people.views.uimodel.profile.ProfileUiModel
+import com.tokopedia.people.views.uimodel.profile.ProfileWhitelistUiModel
+
 /**
  * Created By : Jonathan Darwin on June 28, 2022
  */
 interface UserProfileRepository {
+
+    suspend fun getProfile(username: String): ProfileUiModel
+
+    suspend fun getFollowInfo(profileIdList: List<String>): FollowInfoUiModel
+
+    suspend fun getWhitelist(): ProfileWhitelistUiModel
 }
