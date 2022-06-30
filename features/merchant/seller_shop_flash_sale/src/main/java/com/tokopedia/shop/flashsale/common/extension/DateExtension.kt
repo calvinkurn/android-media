c31291fbe2f415e3e653b89078081a247c575ac5
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2022-06-30 13:54:32 +0700

    code refactor + add some comment

--- features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/common/extension/DateExtension.kt
+++ features/merchant/seller_shop_flash_sale/src/main/java/com/tokopedia/shop/flashsale/common/extension/DateExtension.kt
@@ -130,13 +130,22 @@ fun Date.localFormatTo(
 
 fun Date.removeTimeZone(): Date {
     val calendar = Calendar.getInstance()
-    calendar.timeInMillis = this.time + calendar.getTimeZoneOffset()
+    calendar.timeInMillis = this.time + calendar.timeZoneOffsetInMillis
     return calendar.time
 }
 
-private fun Calendar.getTimeZoneOffset(): Int {
-    return -(get(Calendar.ZONE_OFFSET) + get(Calendar.DST_OFFSET))
-}
+/**
+ * This method will return time-zone offset in millis,
+ * calculation based on recommended formula from now deprecated [Date.getTimezoneOffset]:
+ *
+ * > replaced by -(Calendar.get(Calendar.ZONE_OFFSET) + Calendar.get(Calendar.DST_OFFSET)) / (60 * 1000)
+ *
+ * However we need it in millis, so no need to divide with (60 * 1000) anymore.
+ *
+ * @return the time-zone offset, in millis, for the current time zone.
+ */
+private val Calendar.timeZoneOffsetInMillis: Int
+    get() = -(get(Calendar.ZONE_OFFSET) + get(Calendar.DST_OFFSET))
 
 fun Date.minuteDifference(): Long {
     val calendar = Calendar.getInstance()
