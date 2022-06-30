0e6e724e1c309e2b012451a6f6d55af2c6b38852
reivinoktavianus <oktavianusreivin@gmail.com>
2022-06-30 21:54:17 +0700

    Fix single radio button option

--- features/tokofood/src/main/java/com/tokopedia/tokofood/common/presentation/mapper/CustomOrderDetailsMapper.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/common/presentation/mapper/CustomOrderDetailsMapper.kt
@@ -5,6 +5,7 @@ import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.getCurrencyFormatted
 import com.tokopedia.tokofood.common.domain.response.CheckoutTokoFoodProduct
 import com.tokopedia.tokofood.common.domain.response.CheckoutTokoFoodProductVariant
+import com.tokopedia.tokofood.common.domain.response.CheckoutTokoFoodProductVariantSelectionRules
 import com.tokopedia.tokofood.feature.merchant.presentation.enums.CustomListItemType
 import com.tokopedia.tokofood.feature.merchant.presentation.enums.SelectionControlType
 import com.tokopedia.tokofood.feature.merchant.presentation.model.AddOnUiModel
@@ -52,7 +53,7 @@ object CustomOrderDetailsMapper {
                     name = option.name,
                     price = option.price,
                     priceFmt = option.priceFmt,
-                    selectionControlType = if (variant.rules.selectionRules.maxQuantity > Int.ONE) SelectionControlType.MULTIPLE_SELECTION else SelectionControlType.SINGLE_SELECTION
+                    selectionControlType = mapSelectionRulesToSelectionControlType(variant.rules.selectionRules)
                 )
             }
             CustomListItem(
@@ -73,6 +74,15 @@ object CustomOrderDetailsMapper {
         customListItems.addAll(addOns)
         customListItems.add(noteInput)
         return customListItems.toList()
+    }
+
+    private fun mapSelectionRulesToSelectionControlType(selectionRules: CheckoutTokoFoodProductVariantSelectionRules): SelectionControlType {
+        return when {
+            selectionRules.type == CheckoutTokoFoodProductVariantSelectionRules.SELECT_MANY -> SelectionControlType.MULTIPLE_SELECTION
+            selectionRules.type == CheckoutTokoFoodProductVariantSelectionRules.SELECT_ONE -> SelectionControlType.SINGLE_SELECTION
+            selectionRules.maxQuantity > Int.ONE -> SelectionControlType.MULTIPLE_SELECTION
+            else -> SelectionControlType.SINGLE_SELECTION
+        }
     }
 
 }
