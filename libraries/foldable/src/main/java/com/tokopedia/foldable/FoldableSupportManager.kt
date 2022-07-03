b08d8b9e73d1ad258699e18edf01f9e02e6aebdb
mayankgera-toko <mayank.gera@tokopedia.com>
2022-07-03 22:17:56 +0530

    Why we are not removing observer for lifecycle
    Signed-off-by: mayankgera-toko <mayank.gera@tokopedia.com>

--- libraries/foldable/src/main/java/com/tokopedia/foldable/FoldableSupportManager.kt
+++ libraries/foldable/src/main/java/com/tokopedia/foldable/FoldableSupportManager.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.foldable
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -27,11 +28,12 @@ class FoldableSupportManager(
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     open fun onCreate() {
-
+        Log.e("Activity Callback", "OnCreate")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     open fun onStart() {
+        Log.e("Activity Callback", "OnStart")
         layoutUpdatesJob = CoroutineScope(Dispatchers.Main).launch {
             windowInfoRepo.windowLayoutInfo()
                 .collect { newLayoutInfo ->
@@ -42,21 +44,26 @@ class FoldableSupportManager(
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     open fun onStop() {
+        Log.e("Activity Callback", "OnStop")
         layoutUpdatesJob?.cancel()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     open fun onResume() {
+        Log.e("Activity Callback", "OnResume")
 
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     open fun onPause() {
+        Log.e("Activity Callback", "OnPause")
 
     }
 
+//    https://stackoverflow.com/questions/47656728/is-it-mandatory-to-remove-yourself-as-an-observer-from-android-lifecycle
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     open fun onDestroy() {
+    Log.e("Activity Callback", "OnDestroy")
 
     }
 
