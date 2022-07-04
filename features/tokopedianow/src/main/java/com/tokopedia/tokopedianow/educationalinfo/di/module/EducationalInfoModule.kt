a359e2e309c9610f0fdaf23fa2e296c2f48995f4
saidfaisal <saidfaisal777@gmail.com>
2022-07-04 10:55:12 +0700

    add userId to visit now tracker

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/educationalinfo/di/module/EducationalInfoModule.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/educationalinfo/di/module/EducationalInfoModule.kt
@@ -1,7 +1,11 @@
 package com.tokopedia.tokopedianow.educationalinfo.di.module
 
+import android.content.Context
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.tokopedianow.educationalinfo.analytics.EducationalInfoAnalytics
 import com.tokopedia.tokopedianow.educationalinfo.di.scope.EducationalInfoScope
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
 
@@ -10,8 +14,14 @@ class EducationalInfoModule {
 
     @EducationalInfoScope
     @Provides
-    fun provideEducationalInfoAnalytic(): EducationalInfoAnalytics {
-        return EducationalInfoAnalytics()
+    fun provideEducationalInfoAnalytic(userSession: UserSessionInterface): EducationalInfoAnalytics {
+        return EducationalInfoAnalytics(userSession)
+    }
+
+    @EducationalInfoScope
+    @Provides
+    fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface {
+        return UserSession(context)
     }
 
 }
