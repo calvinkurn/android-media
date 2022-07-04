d87e9258d84abe010c449d58c8a9337c203c53b5
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-07-04 16:04:09 +0800

    toko plus badge on lain nya page

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/uimodel/secondaryinfo/widget/TokoPlusWidgetUiModel.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/settings/view/uimodel/secondaryinfo/widget/TokoPlusWidgetUiModel.kt
@@ -5,7 +5,7 @@ import com.tokopedia.seller.menu.common.view.uimodel.base.SettingResponseState
 import com.tokopedia.sellerhome.settings.view.adapter.ShopSecondaryInfoAdapterFactory
 
 class TokoPlusWidgetUiModel(
-    override var state: SettingResponseState<String>,
+    override var state: SettingResponseState<String> = SettingResponseState.SettingLoading,
     val impressHolder: ImpressHolder = ImpressHolder()
 ) : ShopSecondaryInfoWidget<String> {
 
