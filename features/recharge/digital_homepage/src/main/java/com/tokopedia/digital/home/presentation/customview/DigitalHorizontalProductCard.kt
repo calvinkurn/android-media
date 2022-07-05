9f927accd652ff740a3b227b94b00e906a80865e
Muhammad Furqan <furqan@tokopedia.com>
2022-07-05 14:29:02 +0700

    [SHP] Create Horizontal Product Card Widget

---
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/customview/DigitalHorizontalProductCard.kt
@@ -0,0 +1,97 @@
+package com.tokopedia.digital.home.presentation.customview
+
+import android.content.Context
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.tokopedia.digital.home.databinding.LayoutDigitalHorizontalProductCardBinding
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.unifycomponents.BaseCustomView
+
+class DigitalHorizontalProductCard @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : BaseCustomView(context, attrs, defStyleAttr) {
+
+    val binding: LayoutDigitalHorizontalProductCardBinding
+
+    var imageUrl: String = ""
+    var productCategory: String = ""
+    var productDetail: String = ""
+    var productPrice: String = ""
+    var productSlashPrice: String = ""
+
+    init {
+        binding = LayoutDigitalHorizontalProductCardBinding
+            .inflate(LayoutInflater.from(context), this, true)
+
+        with(binding.tgHorizontalCardProductSlashPrice) {
+            paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }
+    }
+
+    fun buildView() {
+        setupProductImage()
+        setupProductCategory()
+        setupProductDetail()
+        setupProductPrice()
+        setupProductSlashPrice()
+    }
+
+    private fun setupProductImage() {
+        with(binding.tgHorizontalCardProductImage) {
+            if (imageUrl.isNotEmpty()) {
+                loadImage(imageUrl)
+            }
+        }
+    }
+
+    private fun setupProductCategory() {
+        with(binding.tgHorizontalCardProductCategory) {
+            if (productCategory.isNotEmpty()) {
+                text = productCategory
+                show()
+            } else {
+                hide()
+            }
+        }
+    }
+
+    private fun setupProductDetail() {
+        with(binding.tgHorizontalCardProductDetail) {
+            if (productDetail.isNotEmpty()) {
+                text = productDetail
+                show()
+            } else {
+                hide()
+            }
+        }
+    }
+
+    private fun setupProductPrice() {
+        with(binding.tgHorizontalCardProductPrice) {
+            if (productPrice.isNotEmpty()) {
+                text = productPrice
+                show()
+            } else {
+                hide()
+            }
+        }
+    }
+
+    private fun setupProductSlashPrice() {
+        with(binding.tgHorizontalCardProductSlashPrice) {
+            if (productSlashPrice.isNotEmpty()) {
+                text = productSlashPrice
+                paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                show()
+            } else {
+                hide()
+            }
+        }
+    }
+
+}
