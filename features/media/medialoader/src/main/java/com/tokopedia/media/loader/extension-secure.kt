044472440660a32a47fc201c1dc27140371f2e3b
isfaaghyth <isfaaghyth@gmail.com>
2022-07-05 09:58:53 +0800

    adjust

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/extension-secure.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/extension-secure.kt
@@ -1,17 +1,20 @@
 package com.tokopedia.media.loader
 
 import android.widget.ImageView
-import android.widget.Toast
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.media.loader.common.Properties
+import com.tokopedia.media.loader.tracker.MediaLoaderTracker
 import com.tokopedia.user.session.UserSessionInterface
+import timber.log.Timber
 
 inline fun ImageView.loadSecureImage(
     url: String,
     userSession: UserSessionInterface,
     crossinline properties: Properties.() -> Unit = {}
 ) {
-    if (userSession.accessToken.isEmpty()) {
-        Toast.makeText(context, "(Dev) - access token not found", Toast.LENGTH_SHORT).show()
+    if (userSession.accessToken.isEmpty() && GlobalConfig.isAllowDebuggingTools()) {
+        Timber.e("MediaLoader: Access token not found")
+        MediaLoaderTracker.simpleTrack(context, url)
         return
     }
 
