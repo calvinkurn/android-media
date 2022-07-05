7330ead8fcc3c126d52e96653a0eec78721aef9b
saidfaisal <saidfaisal777@gmail.com>
2022-07-05 10:10:45 +0700

    add expose and using common class header

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/MaxStockThresholdResponse.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/source/cloud/model/MaxStockThresholdResponse.kt
@@ -1,30 +1,26 @@
 package com.tokopedia.shop.common.data.source.cloud.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import com.tokopedia.network.data.model.response.Header
 
 data class MaxStockThresholdResponse(
+    @Expose
     @SerializedName("GetIMSMeta")
     val getIMSMeta: GetIMSMeta
 ) {
     data class GetIMSMeta(
+        @Expose
         @SerializedName("data")
         val data: Data,
+        @Expose
         @SerializedName("header")
         val header: Header
     ) {
         data class Data(
+            @Expose
             @SerializedName("max_stock_threshold")
             val maxStockThreshold: String
-        )
-        data class Header(
-            @SerializedName("error_code")
-            val errorCode: String,
-            @SerializedName("messages")
-            val messages: List<Any>,
-            @SerializedName("process_time")
-            val processTime: Double,
-            @SerializedName("reason")
-            val reason: String
         )
     }
 }
