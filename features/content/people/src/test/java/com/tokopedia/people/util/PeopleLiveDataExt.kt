eb3648c58ba74bbadab0ba61c09e787a438f53b6
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-07-06 16:39:18 +0700

    `fix` : forgot to push getOrNullValue()

--- features/content/people/src/test/java/com/tokopedia/people/util/PeopleLiveDataExt.kt
+++ features/content/people/src/test/java/com/tokopedia/people/util/PeopleLiveDataExt.kt
@@ -32,3 +32,27 @@ fun <T> LiveData<T>.getOrAwaitValue(
     @Suppress("UNCHECKED_CAST")
     return data as T
 }
+
+fun <T> LiveData<T>.getOrNullValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS
+): T? {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrNullValue.removeObserver(this)
+        }
+    }
+
+    this.observeForever(observer)
+
+    // Don't wait indefinitely if the LiveData is not set.
+    if (!latch.await(time, timeUnit)) {
+        return null
+    }
+
+    return data
+}
