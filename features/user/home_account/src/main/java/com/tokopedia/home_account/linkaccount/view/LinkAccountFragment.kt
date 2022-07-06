23f76125b73138fcc2e588443d21d484c5fca3f9
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-07-06 19:02:55 +0700

    fix ld

--- features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/LinkAccountFragment.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/LinkAccountFragment.kt
@@ -171,7 +171,7 @@ class LinkAccountFragment: BaseDaggerFragment(), AccountItemListener {
 
     override fun onViewAccountClicked() {
         homeAccountAnalytics.trackClickViewStatusLinkAccountPage()
-        LinkAccountWebViewActivity.gotoSuccessPage(activity, LinkAccountWebviewFragment.BACK_BTN_APPLINK)
+        LinkAccountWebViewActivity.gotoSuccessPage(activity, ApplinkConst.HOME)
     }
 
     private fun LinkStatus.toUserAccountDataView(): UserAccountDataView {
--- features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/PrivacyAccountFragment.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/linkaccount/view/PrivacyAccountFragment.kt
@@ -298,7 +298,7 @@ class PrivacyAccountFragment : BaseDaggerFragment() {
 
     private fun onViewAccountClicked() {
         homeAccountAnalytics.trackClickViewStatusLinkAccountPage()
-        LinkAccountWebViewActivity.gotoSuccessPage(activity, LinkAccountWebviewFragment.BACK_BTN_APPLINK)
+        LinkAccountWebViewActivity.gotoSuccessPage(activity, ApplinkConst.HOME)
     }
 
     private fun switchListener() {
