5fbd5aa37d4676c558f6cf4b018d391f591479df
mohit-toko <mohit.singh@tokopedia.com>
2022-07-06 09:31:42 +0530

    Test Cases

--- features/category/epharmacy/src/test/java/com/tokopedia/epharmacy/UploadPrescriptionViewModelTest.kt
+++ features/category/epharmacy/src/test/java/com/tokopedia/epharmacy/UploadPrescriptionViewModelTest.kt
@@ -95,6 +95,22 @@ class UploadPrescriptionViewModelTest {
     }
 
     @Test
+    fun `orderDetail fetch failed throws exception`()
+    {
+        coEvery {
+            getEPharmacyOrderDetailUseCase.getEPharmacyOrderDetail(any(), any(), "")
+        } coAnswers {
+            secondArg<(Throwable) -> Unit>().invoke(mockThrowable)
+        }
+        viewModel.getEPharmacyOrderDetail("")
+        Assert.assertEquals(
+            (viewModel.productDetailLiveDataResponse.value as Fail).throwable,
+            mockThrowable
+        )
+    }
+
+
+    @Test
     fun checkoutDetailSuccessTest()
     {
         val product = mockk<EPharmacyProduct>(relaxed = true)
@@ -134,6 +150,22 @@ class UploadPrescriptionViewModelTest {
             "Data invalid"
         )
     }
+
+    @Test
+    fun `checkoutDetail fetch failed throws exception`()
+    {
+        coEvery {
+            getEPharmacyCheckoutDetailUseCase.getEPharmacyCheckoutDetail(any(), any(), "")
+        } coAnswers {
+            secondArg<(Throwable) -> Unit>().invoke(mockThrowable)
+        }
+        viewModel.getEPharmacyCheckoutDetail("")
+        Assert.assertEquals(
+            (viewModel.productDetailLiveDataResponse.value as Fail).throwable,
+            mockThrowable
+        )
+    }
+
     @Test
     fun uploadIdsSuccessTest()
     {
@@ -159,122 +191,6 @@ class UploadPrescriptionViewModelTest {
         viewModel.uploadPrescriptionIds(UPLOAD_CHECKOUT_ID_KEY,"")
         assert(viewModel.uploadPrescriptionIdsData.value is Fail)
     }
-
-//    @Test
-//    fun `productDetail fetch failed throws exception`()
-//    {
-//        coEvery {
-//            productDetailUseCase.getProductDetail(any(), any(), "")
-//        } coAnswers {
-//            secondArg<(Throwable) -> Unit>().invoke(mockThrowable)
-//        }
-//        viewModel.getProductDetail("")
-//        Assert.assertEquals(
-//                (viewModel.productDetailLiveData.value as Fail).throwable,
-//                mockThrowable
-//        )
-//    }
-//
-//    @Test
-//    fun `productDetail data invalid `()
-//    {
-//        val baseProductDetail = mockk<BaseProductDetailClass>(relaxed = true)
-//        coEvery {
-//            productDetailUseCase.getProductDetail(any(), any(), "")
-//        } coAnswers {
-//            firstArg<(BaseProductDetailClass) -> Unit>().invoke(baseProductDetail)
-//        }
-//        viewModel.getProductDetail("")
-//        assert(viewModel.productDetailLiveData.value is Fail)
-//    }
-//
-//
-//    @Test
-//    fun successPayLaterOptions()
-//    {
-//        val payLaterGetSimulation = PayLaterGetSimulation(listOf(PayLaterAllData(1,"", "", listOf())))
-//        val list = arrayListOf(SimulationUiModel(
-//                1,
-//                "",
-//                "",
-//                false,
-//                arrayListOf(SupervisorUiModel)))
-//        mockMapperResponse(list)
-//        coVerify(exactly = 0) { payLaterUiMapperUseCase.mapResponseToUi(any(), any(), any()) }
-//        coEvery {
-//            payLaterSimulationData.getPayLaterSimulationDetails(any(), any(), 10.0, "0")
-//        } coAnswers {
-//            firstArg<(PayLaterGetSimulation) -> Unit>().invoke(payLaterGetSimulation)
-//        }
-//
-//        viewModel.getPayLaterAvailableDetail(10.0, "0")
-//        coVerify(exactly = 1) { payLaterUiMapperUseCase.mapResponseToUi(any(), any(), any()) }
-//        Assert.assertEquals((viewModel.payLaterOptionsDetailLiveData.value as Success).data, list)
-//    }
-//
-//    @Test
-//    fun `successPayLaterOptions With Default Selected`()
-//    {
-//        val payLaterGetSimulation = PayLaterGetSimulation(listOf(PayLaterAllData(1,"", "", listOf())))
-//        val list = arrayListOf(SimulationUiModel(
-//            1,
-//            "",
-//            "",
-//            true,
-//            arrayListOf(SupervisorUiModel)))
-//        mockMapperResponse(list)
-//
-//        coEvery {
-//            payLaterSimulationData.getPayLaterSimulationDetails(any(), any(), 10.0, "0")
-//        } coAnswers {
-//            firstArg<(PayLaterGetSimulation) -> Unit>().invoke(payLaterGetSimulation)
-//        }
-//
-//        viewModel.getPayLaterAvailableDetail(10.0, "0")
-//        coVerify(exactly = 1) { payLaterUiMapperUseCase.mapResponseToUi(any(), any(), any()) }
-//        Assert.assertEquals((viewModel.payLaterOptionsDetailLiveData.value as Success).data, list)
-//    }
-//
-//
-//    private fun mockMapperResponse(list: ArrayList<SimulationUiModel>) {
-//        coEvery {
-//            payLaterUiMapperUseCase.mapResponseToUi(any(), any(), any())
-//        } coAnswers {
-//            firstArg<(ArrayList<SimulationUiModel>) -> Unit>().invoke(list)
-//        }
-//    }
-//
-//    @Test
-//    fun failPayLaterOptions()
-//    {
-//        coEvery {
-//            payLaterSimulationData.getPayLaterSimulationDetails(any(), any(), 0.0, "0")
-//        } coAnswers {
-//            secondArg<(Throwable) -> Unit>().invoke(mockThrowable)
-//        }
-//        viewModel.getPayLaterAvailableDetail(0.0, "0")
-//        coVerify(exactly = 0) { payLaterUiMapperUseCase.mapResponseToUi(any(), any(), any()) }
-//        Assert.assertEquals((viewModel.payLaterOptionsDetailLiveData.value as Fail).throwable,mockThrowable)
-//    }
-//
-//
-//    @Test
-//    fun successAddToCart() {
-//        val addToCartMultiDataModel = mockk<AddToCartOccMultiDataModel>(relaxed = true)
-//        val detail = mockk<Detail>(relaxed = true)
-//        coEvery {
-//            addToCartUseCase.execute(captureLambda(), any())
-//        } coAnswers {
-//            val onSuccess = lambda<(AddToCartOccMultiDataModel) -> Unit>()
-//            onSuccess.invoke(addToCartMultiDataModel)
-//        }
-//        viewModel.shopId = ""
-//        viewModel.addProductToCart(detail,"")
-//        verify {
-//            addToCartUseCase.execute(any(), any())
-//        }
-//    }
-
 
     @Test
     fun removePrescriptionImageAtIndex() {
