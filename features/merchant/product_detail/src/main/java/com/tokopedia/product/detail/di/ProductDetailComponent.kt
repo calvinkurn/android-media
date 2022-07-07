f06fa6d602e1d945d4177e9045d6e3714eb56c8a
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2022-07-07 15:15:28 +0700

    replace old query affiliate with new sdk

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/di/ProductDetailComponent.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/di/ProductDetailComponent.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.product.detail.di
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
+import com.tokopedia.common_sdk_affiliate_toko.di.AffiliateCommonSdkModule
 import com.tokopedia.product.detail.view.fragment.DynamicProductDetailFragment
 import com.tokopedia.product.detail.view.widget.AddToCartDoneBottomSheet
 import com.tokopedia.product.info.view.bottomsheet.ProductDetailInfoBottomSheet
@@ -13,7 +14,8 @@ import dagger.Component
     GqlRawQueryModule::class,
     ProductWishlistModule::class,
     ProductDetailUserModule::class,
-    ProductDetailDevModule::class],
+    ProductDetailDevModule::class,
+    AffiliateCommonSdkModule::class],
         dependencies = [BaseAppComponent::class])
 interface ProductDetailComponent {
     fun inject(fragment: DynamicProductDetailFragment)
