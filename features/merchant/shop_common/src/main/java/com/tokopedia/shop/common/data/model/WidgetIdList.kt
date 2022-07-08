23fe3b3b8355ab41f25045126231339424686e38
CJ <calvyn.julian@tokopedia.com>
2022-07-08 10:42:44 +0700

    remove expose

--- features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/model/WidgetIdList.kt
+++ features/merchant/shop_common/src/main/java/com/tokopedia/shop/common/data/model/WidgetIdList.kt
@@ -1,22 +1,17 @@
 package com.tokopedia.shop.common.data.model
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class WidgetIdList(
     @SerializedName("widgetID")
-    @Expose
     val widgetId: String = "",
 
     @SerializedName("widgetMasterID")
-    @Expose
     val widgetMasterId: String = "",
 
     @SerializedName("widgetType")
-    @Expose
     val widgetType: String = "",
 
     @SerializedName("widgetName")
-    @Expose
     val widgetName: String = ""
 )
