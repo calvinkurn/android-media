1e2ca556dafa31b20dadeafb572259cafe010291
Dhaba Widhikari <wikodhawi@gmail.com>
2022-07-09 09:32:02 +0700

    set progress state loading error

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/static_channel/BalanceWidgetFailedViewHolder.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/viewholder/static_channel/BalanceWidgetFailedViewHolder.kt
@@ -5,16 +5,24 @@ import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolde
 import com.tokopedia.home.R
 import com.tokopedia.home.beranda.listener.HomeCategoryListener
 import com.tokopedia.home.beranda.presentation.view.adapter.datamodel.balance.BalanceWidgetFailedModel
+import com.tokopedia.home.databinding.LayoutBalanceWidgetFailedBinding
+import com.tokopedia.utils.view.binding.viewBinding
 
 /**
  * Created by dhaba
  */
 class BalanceWidgetFailedViewHolder (itemView: View, val listener: HomeCategoryListener?) :
     AbstractViewHolder<BalanceWidgetFailedModel>(itemView) {
+    private var binding: LayoutBalanceWidgetFailedBinding? by viewBinding()
 
     companion object {
         var LAYOUT = R.layout.layout_balance_widget_failed
     }
 
-    override fun bind(element: BalanceWidgetFailedModel) {}
+    override fun bind(element: BalanceWidgetFailedModel) {
+        binding?.localloadErrorBalanceWidget?.refreshBtn?.setOnClickListener {
+            binding?.localloadErrorBalanceWidget?.progressState = true
+            val x = 1
+        }
+    }
 }
