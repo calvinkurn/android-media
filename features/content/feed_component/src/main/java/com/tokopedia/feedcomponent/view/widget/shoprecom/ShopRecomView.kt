d4f332e0cdd971576e92cffa9d7b609ca4fef8ec
fachrizalmrsln-tkpd <fachrizal.mursalin@tokopedia.com>
2022-07-11 12:21:10 +0800

    'refactor': implement viewBinding at ShopRecomView.kt

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/shoprecom/ShopRecomView.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/view/widget/shoprecom/ShopRecomView.kt
@@ -2,20 +2,16 @@ package com.tokopedia.feedcomponent.view.widget.shoprecom
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.view.LayoutInflater
 import android.widget.FrameLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.tokopedia.feedcomponent.R
 import com.tokopedia.feedcomponent.R.string.up_btn_text_follow
 import com.tokopedia.feedcomponent.R.string.up_btn_text_following
 import com.tokopedia.feedcomponent.data.pojo.people.ShopRecomItemUI
-import com.tokopedia.unifycomponents.ImageUnify
-import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.feedcomponent.databinding.ItemShopRecommendationBinding
 import com.tokopedia.unifycomponents.UnifyButton.Type.ALTERNATE
 import com.tokopedia.unifycomponents.UnifyButton.Type.MAIN
 import com.tokopedia.unifycomponents.UnifyButton.Variant.FILLED
 import com.tokopedia.unifycomponents.UnifyButton.Variant.GHOST
-import com.tokopedia.unifyprinciples.Typography
 
 /**
  * created by fachrizalmrsln on 07/07/22
@@ -31,37 +27,24 @@ class ShopRecomView : FrameLayout {
     )
 
     interface Listener {
-        fun onCloseClicked(item: ShopRecomItemUI)
-        fun onFollowClicked(item: ShopRecomItemUI)
-        fun onShopItemClicked(appLink: String)
+        fun onShopRecomCloseClicked(item: ShopRecomItemUI)
+        fun onShopRecomFollowClicked(item: ShopRecomItemUI)
+        fun onShopRecomItemClicked(appLink: String)
     }
 
     private var mListener: Listener? = null
 
-    private val clItemShopContainer: ConstraintLayout
-    private val txtItemShopName: Typography
-    private val txtItemShopUsername: Typography
-    private val imgItemShopImage: ImageUnify
-    private val imgItemShopBadge: ImageUnify
-    private val imgItemShopClose: ImageUnify
-    private val btnItemShopCta: UnifyButton
-
-    init {
-        val view = View.inflate(context, R.layout.item_shop_recommendation, this)
-        clItemShopContainer = view.findViewById(R.id.cl_item_shop_container)
-        txtItemShopName = view.findViewById(R.id.txt_item_shop_name)
-        txtItemShopUsername = view.findViewById(R.id.txt_item_shop_username)
-        imgItemShopImage = view.findViewById(R.id.img_item_shop_image)
-        imgItemShopBadge = view.findViewById(R.id.img_item_shop_badge)
-        imgItemShopClose = view.findViewById(R.id.img_item_shop_close)
-        btnItemShopCta = view.findViewById(R.id.btn_item_shop_cta)
-    }
+    private var binding = ItemShopRecommendationBinding.inflate(
+        LayoutInflater.from(this.context),
+        this,
+        true
+    )
 
     fun setListener(listener: Listener?) {
         mListener = listener
     }
 
-    fun setData(data: ShopRecomItemUI) {
+    fun setData(data: ShopRecomItemUI) = with(binding) {
         txtItemShopName.text = data.name
         txtItemShopUsername.text = data.nickname
         imgItemShopImage.setImageUrl(data.logoImageURL)
@@ -71,7 +54,7 @@ class ShopRecomView : FrameLayout {
         onClickListener(data)
     }
 
-    private fun buttonFollowState(isFollow: Boolean) {
+    private fun buttonFollowState(isFollow: Boolean) = with(binding) {
         btnItemShopCta.apply {
             text = context.getString(if (isFollow) up_btn_text_following else up_btn_text_follow)
             buttonVariant = if (isFollow) GHOST else FILLED
@@ -79,10 +62,10 @@ class ShopRecomView : FrameLayout {
         }
     }
 
-    private fun onClickListener(data: ShopRecomItemUI) {
-        clItemShopContainer.setOnClickListener { mListener?.onShopItemClicked(data.applink) }
-        imgItemShopClose.setOnClickListener { mListener?.onCloseClicked(data) }
-        btnItemShopCta.setOnClickListener { mListener?.onFollowClicked(data) }
+    private fun onClickListener(data: ShopRecomItemUI) = with(binding) {
+        clItemShopContainer.setOnClickListener { mListener?.onShopRecomItemClicked(data.applink) }
+        imgItemShopClose.setOnClickListener { mListener?.onShopRecomCloseClicked(data) }
+        btnItemShopCta.setOnClickListener { mListener?.onShopRecomFollowClicked(data) }
     }
 
 }
