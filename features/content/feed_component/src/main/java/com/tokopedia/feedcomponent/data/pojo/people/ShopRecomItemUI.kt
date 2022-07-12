f3d8a19d719ca749c1af4329d79ef67ca2435493
fachrizalmrsln-tkpd <fachrizal.mursalin@tokopedia.com>
2022-07-12 16:57:51 +0800

    `refactor`: remove unused serialize

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/data/pojo/people/ShopRecomItemUI.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/data/pojo/people/ShopRecomItemUI.kt
@@ -1,24 +1,13 @@
 package com.tokopedia.feedcomponent.data.pojo.people
 
-import com.google.gson.annotations.SerializedName
-
 data class ShopRecomItemUI(
-    @SerializedName("badgeImageURL")
     val badgeImageURL: String = "",
-    @SerializedName("encryptedID")
     val encryptedID: String = "",
-    @SerializedName("id")
     val id: Long = 0,
-    @SerializedName("logoImageURL")
     val logoImageURL: String = "",
-    @SerializedName("name")
     val name: String = "",
-    @SerializedName("nickname")
     val nickname: String = "",
-    @SerializedName("type")
     val type: Int = 0,
-    @SerializedName("applink")
     val applink: String = "",
-    @SerializedName("isFollow")
     val isFollow: Boolean = false,
 )
--- features/content/people/src/main/java/com/tokopedia/people/views/uimodel/shoprecom/ShopRecomUiModel.kt
+++ features/content/people/src/main/java/com/tokopedia/people/views/uimodel/shoprecom/ShopRecomUiModel.kt
@@ -1,36 +1,22 @@
 package com.tokopedia.people.views.uimodel.shoprecom
 
-import com.google.gson.annotations.SerializedName
 import com.tokopedia.library.baseadapter.BaseItem
 
 data class ShopRecomUiModel(
-    @SerializedName("isShown")
     val isShown: Boolean = false,
-    @SerializedName("items")
     val items: List<ShopRecomUiModelItem> = emptyList(),
-    @SerializedName("nextCursor")
     val nextCursor: String = "",
-    @SerializedName("title")
     val title: String = "",
 )
 
 data class ShopRecomUiModelItem(
-    @SerializedName("badgeImageURL")
     val badgeImageURL: String = "",
-    @SerializedName("encryptedID")
     val encryptedID: String = "",
-    @SerializedName("id")
     val id: Long = 0,
-    @SerializedName("logoImageURL")
     val logoImageURL: String = "",
-    @SerializedName("name")
     val name: String = "",
-    @SerializedName("nickname")
     val nickname: String = "",
-    @SerializedName("type")
     val type: Int = 0,
-    @SerializedName("applink")
     val applink: String = "",
-    @SerializedName("isFollow")
     var isFollow: Boolean = false,
-): BaseItem()
+) : BaseItem()
