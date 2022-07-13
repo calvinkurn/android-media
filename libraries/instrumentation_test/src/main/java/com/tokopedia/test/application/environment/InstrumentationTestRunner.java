40230f4536e0b507b3f05056acf986fe0b352969
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2022-07-13 09:21:12 +0700

    Revert "Add dexopener"
    This reverts commit 517bbecefdee832bfc2092fa899c4d523602fe7a.

--- libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestRunner.java
+++ libraries/instrumentation_test/src/main/java/com/tokopedia/test/application/environment/InstrumentationTestRunner.java
@@ -2,21 +2,14 @@ package com.tokopedia.test.application.environment;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Build;
 
 import androidx.test.runner.AndroidJUnitRunner;
 
-import com.github.tmurakami.dexopener.DexOpener;
 import com.tokopedia.test.application.environment.InstrumentationTestApp;
 
 public class InstrumentationTestRunner extends AndroidJUnitRunner {
     @Override
     public Application newApplication(ClassLoader cl, String className, Context context) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        // We only need to enable DexOpener for < 28
-        // MockK supports for mocking final classes on Android 9+.
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-            DexOpener.install(this);
-        }
         return super.newApplication(cl, InstrumentationTestApp.class.getName(), context);
     }
 }
