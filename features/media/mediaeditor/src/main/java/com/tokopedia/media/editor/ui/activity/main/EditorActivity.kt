e0d1a33c9aa0e550e88cc10e1de0cadfd9755d01
Calvin <calvin.kurniawan@tokopedia.com>
2022-07-14 12:07:29 +0700

    add source image

--- features/media/mediaeditor/src/main/java/com/tokopedia/media/editor/ui/activity/main/EditorActivity.kt
+++ features/media/mediaeditor/src/main/java/com/tokopedia/media/editor/ui/activity/main/EditorActivity.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.media.editor.ui.activity.main
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
@@ -62,7 +63,9 @@ class EditorActivity : BaseEditorActivity() {
             arrayListOf(
                 "https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2FtcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
                 "https://images.unsplash.com/photo-1561336313-0bd5e0b27ec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FtcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1531361171768-37170e369163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNhbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+                "https://images.unsplash.com/photo-1531361171768-37170e369163?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNhbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+                "https://thumbs.dreamstime.com/b/sign-displaying-small-business-concept-individualowned-known-its-limited-size-stack-sample-cube-conceptual-caption-222695840.jpg",
+                "https://staticg.sportskeeda.com/editor/2022/03/206cb-16473460338144-1920.jpg"
             )
         )
     }
