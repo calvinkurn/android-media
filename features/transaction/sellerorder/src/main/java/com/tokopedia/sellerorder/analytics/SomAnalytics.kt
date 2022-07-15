9362f8c2032ddb9d878887003758a633565dcfa8
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2022-07-15 11:37:06 +0700

    Fix wrong CoachMark trackers id on MainApp

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/analytics/SomAnalytics.kt
+++ features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/analytics/SomAnalytics.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.sellerorder.analytics
 
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
 
@@ -57,9 +58,12 @@ object SomAnalytics {
     private const val EVENT_ACTION_CLICK_ONBOARD_CONTINUE = "click onboard info - lanjut"
     private const val EVENT_ACTION_CLICK_ONBOARD_OK = "click onboard info - oke"
 
-    private const val TRACKER_ID_CLICK_ONBOARD_CONTINUE = "33451"
-    private const val TRACKER_ID_CLICK_ONBOARD_BACK = "33452"
-    private const val TRACKER_ID_CLICK_ONBOARD_OK = "33453"
+    private const val TRACKER_ID_CLICK_ONBOARD_CONTINUE_SA = "33451"
+    private const val TRACKER_ID_CLICK_ONBOARD_BACK_SA = "33452"
+    private const val TRACKER_ID_CLICK_ONBOARD_OK_SA = "33453"
+    private const val TRACKER_ID_CLICK_ONBOARD_CONTINUE_MA = "33446"
+    private const val TRACKER_ID_CLICK_ONBOARD_BACK_MA = "33447"
+    private const val TRACKER_ID_CLICK_ONBOARD_OK_MA = "33448"
 
     @JvmStatic
     fun sendScreenName(screenName: String) {
@@ -303,7 +307,11 @@ object SomAnalytics {
             TrackAppUtils.EVENT_LABEL to userId,
             CUSTOM_DIMENSION_BUSINESS_UNIT to BUSINESS_UNIT_PHYSICAL_GOODS_CAPITALIZE,
             CUSTOM_DIMENSION_CURRENT_SITE to TOKOPEDIA_MARKETPLACE,
-            CUSTOM_DIMENSION_TRACKER_ID to TRACKER_ID_CLICK_ONBOARD_CONTINUE
+            CUSTOM_DIMENSION_TRACKER_ID to if (GlobalConfig.isSellerApp()) {
+                TRACKER_ID_CLICK_ONBOARD_CONTINUE_SA
+            } else {
+                TRACKER_ID_CLICK_ONBOARD_CONTINUE_MA
+            }
         )
         TrackApp.getInstance().gtm.sendGeneralEvent(data)
     }
@@ -316,7 +324,11 @@ object SomAnalytics {
             TrackAppUtils.EVENT_LABEL to userId,
             CUSTOM_DIMENSION_BUSINESS_UNIT to BUSINESS_UNIT_PHYSICAL_GOODS_CAPITALIZE,
             CUSTOM_DIMENSION_CURRENT_SITE to TOKOPEDIA_MARKETPLACE,
-            CUSTOM_DIMENSION_TRACKER_ID to TRACKER_ID_CLICK_ONBOARD_BACK
+            CUSTOM_DIMENSION_TRACKER_ID to if (GlobalConfig.isSellerApp()) {
+                TRACKER_ID_CLICK_ONBOARD_BACK_SA
+            } else {
+                TRACKER_ID_CLICK_ONBOARD_BACK_MA
+            }
         )
         TrackApp.getInstance().gtm.sendGeneralEvent(data)
     }
@@ -329,7 +341,11 @@ object SomAnalytics {
             TrackAppUtils.EVENT_LABEL to userId,
             CUSTOM_DIMENSION_BUSINESS_UNIT to BUSINESS_UNIT_PHYSICAL_GOODS_CAPITALIZE,
             CUSTOM_DIMENSION_CURRENT_SITE to TOKOPEDIA_MARKETPLACE,
-            CUSTOM_DIMENSION_TRACKER_ID to TRACKER_ID_CLICK_ONBOARD_OK
+            CUSTOM_DIMENSION_TRACKER_ID to if (GlobalConfig.isSellerApp()) {
+                TRACKER_ID_CLICK_ONBOARD_OK_SA
+            } else {
+                TRACKER_ID_CLICK_ONBOARD_OK_MA
+            }
         )
         TrackApp.getInstance().gtm.sendGeneralEvent(data)
     }
