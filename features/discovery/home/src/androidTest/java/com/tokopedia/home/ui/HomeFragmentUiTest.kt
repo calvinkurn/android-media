a4dbbe33645686ee2b05253d40a374afe95f46fc
Dhaba Widhikari <wikodhawi@gmail.com>
2022-07-15 21:26:04 +0700

    Fix import HomeFragmentUiTest.kt

--- features/discovery/home/src/androidTest/java/com/tokopedia/home/ui/HomeFragmentUiTest.kt
+++ features/discovery/home/src/androidTest/java/com/tokopedia/home/ui/HomeFragmentUiTest.kt
@@ -7,7 +7,10 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.swipeUp
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analyticsdebugger.debugger.data.source.GtmLogDBSource
