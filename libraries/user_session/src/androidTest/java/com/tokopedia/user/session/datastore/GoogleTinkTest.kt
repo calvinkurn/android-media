963fc668b96266604bbc3f8b3c999b13041e2ec6
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2022-07-15 08:47:29 +0700

    Add recovery test

---
+++ libraries/user_session/src/androidTest/java/com/tokopedia/user/session/datastore/GoogleTinkTest.kt
@@ -0,0 +1,45 @@
+package com.tokopedia.user.session.datastore
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.crypto.tink.Aead
+import com.tokopedia.utils.GoogleTinkExplorerLab
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class GoogleTinkTest {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext<Context>()
+    }
+
+    @Test
+    fun test_recover_from_android_keystore() {
+        val aead = GoogleTinkExplorerLab.generateKey(context, withKeystore = true)
+
+        encryptDecryptTest(aead)
+
+        /*
+        * When we are recovering from corrupted Keyset using Android Keystore, we have to delete the
+        * key associated with Android Keystore before generating a new AEAD with the same name.
+        * issue on Android Keystore instability: https://github.com/google/tink/issues/504
+        * */
+        GoogleTinkExplorerLab.delete(context)
+
+        val recoverAead = GoogleTinkExplorerLab.generateKey(context, withKeystore = false)
+        encryptDecryptTest(recoverAead)
+    }
+
+    private fun encryptDecryptTest(aead: Aead, secret: String = "top-secret") {
+        with(aead) {
+            val cipher = encrypt(secret.toByteArray(), null)
+            val decipher = decrypt(cipher, null)
+            assertThat(secret, `is`(String(decipher)))
+        }
+    }
+}
