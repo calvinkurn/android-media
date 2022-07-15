aac2749f97aed5b7337ad3f68457205f8f5746cb
CJ <calvyn.julian@tokopedia.com>
2022-07-15 16:09:50 +0700

    fix tracker impression shop banner widget campaign tab

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/analytic/ShopPageTrackingConstant.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/analytic/ShopPageTrackingConstant.kt
@@ -157,7 +157,7 @@ object ShopPageTrackingConstant {
     const val MULTIPLE_TYPE = "multiple"
     const val SINGLE_TYPE = "single"
     const val BUNDLE_ADD_TO_CART = "add to cart"
-    const val IMPRESSION_SHOP_BANNER = "shop banner impression"
+    const val IMPRESSION_SHOP_BANNER = "impression shop banner"
     const val CLICK_SHOP_BANNER = "click shop banner"
 
     //label
