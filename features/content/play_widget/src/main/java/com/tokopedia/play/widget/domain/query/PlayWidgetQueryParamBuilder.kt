a51e212b485836ae58ca966246daaa8b2445df9c
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-07-18 04:38:10 +0700

    Merge branch 'release' of github.com:tokopedia/android-tokopedia-core into feature/xorbyte29/user_consent_ui_test
    # Conflicts:
    #	features/user/usercomponents/build.gradle
    #	features/user/usercomponents/src/androidTest/AndroidManifest.xml

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/domain/query/PlayWidgetQueryParamBuilder.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/domain/query/PlayWidgetQueryParamBuilder.kt
@@ -81,5 +81,6 @@ class PlayWidgetQueryParamBuilder {
         const val PARAM_IS_WIFI = "isWifi"
         const val PARAM_PRODUCT_ID = "productIDs"
         const val PARAM_CATEGORY_ID = "categoryIDs"
+        const val PARAM_CHANNEL_TAG = "channelTag"
     }
 }
