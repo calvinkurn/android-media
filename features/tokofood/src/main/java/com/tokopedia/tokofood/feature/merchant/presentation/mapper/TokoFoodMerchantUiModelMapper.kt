7dc7caa9801844bcd9ec3aa53dc0f3a6df74ed38
yohann.prananta <yohann.prananta@BA-00019887.local>
2022-07-20 17:44:49 +0700

    fix double appended product ui param

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/merchant/presentation/mapper/TokoFoodMerchantUiModelMapper.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/merchant/presentation/mapper/TokoFoodMerchantUiModelMapper.kt
@@ -183,8 +183,8 @@ object TokoFoodMerchantUiModelMapper {
     }
 
     fun getMerchantFoodAppLink(merchantId: String, productId: String): String {
-        return UriUtil.buildUri(ApplinkConst.TokoFood.MERCHANT, mapOf("{"+ShareComponentConstants.Merchant.MERCHANT_ID+"}" to merchantId)).apply {
-            UriUtil.buildUriAppendParam(this, mapOf(ShareComponentConstants.Merchant.PRODUCT_ID to productId))
-        }
+        var merchantFoodAppLink = UriUtil.buildUri(ApplinkConst.TokoFood.MERCHANT, mapOf("{"+ShareComponentConstants.Merchant.MERCHANT_ID+"}" to merchantId))
+        merchantFoodAppLink = UriUtil.buildUri(merchantFoodAppLink, mapOf("{"+ShareComponentConstants.Merchant.PRODUCT_ID+"}" to productId))
+        return merchantFoodAppLink
     }
 }
