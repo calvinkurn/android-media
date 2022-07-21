d5919125bc75978f74f7327e4bd84ba7ab190052
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-07-21 11:22:45 +0700

    update KYC TnC page url

--- features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KycCommonUrl.kt
+++ features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KycCommonUrl.kt
@@ -7,9 +7,7 @@ import com.tokopedia.url.TokopediaUrl.Companion.getInstance
  * @author by nisie on 16/11/18.
  */
 object KycCommonUrl {
-    const val TERMS_CONDITION = "https://m.tokopedia.com/myshop/verify/terms"
-    private var BASE_URL = getInstance().MOBILEWEB
-    private val URL_TERMS_AND_CONDITION = BASE_URL + "terms/merchantkyc"
+    private val URL_TERMS_AND_CONDITION = "https://www.tokopedia.com/help/article/syarat-dan-ketentuan-verifikasi-pengguna"
     @JvmField
     val APPLINK_TERMS_AND_CONDITION = String.format("%s?url=%s", ApplinkConst.WEBVIEW, URL_TERMS_AND_CONDITION)
 }
