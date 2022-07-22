43045971f18ba0a73a18d8ec1cc65168cffdc2a6
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-07-22 13:57:07 +0700

    [Recharge] update nullability check on diffutil calculatediff

--- features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/postpaid/fragment/DigitalSignalFragment.kt
+++ features/recharge/digital_topup_bills/src/main/java/com/tokopedia/topupbills/telco/postpaid/fragment/DigitalSignalFragment.kt
@@ -193,21 +193,23 @@ class DigitalSignalFragment: DigitalBaseTelcoFragment() {
         val oldItems = telcoTabViewModel.createIdSnapshot()
         performChange()
         val newItems = telcoTabViewModel.createIdSnapshot()
-        DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                oldItems[oldItemPosition] == newItems[newItemPosition]
+        viewPager.adapter?.let {
+            DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                    oldItems[oldItemPosition] == newItems[newItemPosition]
 
-            override fun getOldListSize(): Int {
-                return oldItems.size
-            }
+                override fun getOldListSize(): Int {
+                    return oldItems.size
+                }
 
-            override fun getNewListSize(): Int {
-                return newItems.size
-            }
+                override fun getNewListSize(): Int {
+                    return newItems.size
+                }
 
-            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-                areItemsTheSame(oldItemPosition, newItemPosition)
-        }, true).dispatchUpdatesTo(viewPager.adapter!!)
+                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+                    areItemsTheSame(oldItemPosition, newItemPosition)
+            }, true).dispatchUpdatesTo(it)
+        }
     }
 
     override fun setupCheckoutData() {
