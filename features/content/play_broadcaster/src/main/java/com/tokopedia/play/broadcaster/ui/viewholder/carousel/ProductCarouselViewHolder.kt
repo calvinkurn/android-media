68e9cb738487cca67a6ff34e585f8cb5997438c0
astidhiyaa <asti.anzaria@tokopedia.com>
2022-07-24 21:46:59 +0800

    `feat` add new view holder

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/viewholder/carousel/ProductCarouselViewHolder.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/viewholder/carousel/ProductCarouselViewHolder.kt
@@ -3,21 +3,26 @@ package com.tokopedia.play.broadcaster.ui.viewholder.carousel
 import android.content.Context
 import android.graphics.Paint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.iconunify.IconUnify
-import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.showWithCondition
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.invisible
 import com.tokopedia.kotlin.extensions.view.show
-import com.tokopedia.kotlin.extensions.view.showWithCondition
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.play.broadcaster.R
+import com.tokopedia.play.broadcaster.databinding.ItemPlayBroPinnedProductCarouselBinding
 import com.tokopedia.play.broadcaster.databinding.ItemPlayBroPlaceholderCarouselBinding
 import com.tokopedia.play.broadcaster.databinding.ItemPlayBroProductCarouselBinding
 import com.tokopedia.play.broadcaster.type.DiscountedPrice
 import com.tokopedia.play.broadcaster.type.OriginalPrice
 import com.tokopedia.play.broadcaster.ui.model.pinnedproduct.PinStatus
 import com.tokopedia.play.broadcaster.ui.model.product.ProductUiModel
+import com.tokopedia.play_common.view.loadImage
 import com.tokopedia.unifyprinciples.R as unifyR
 
 /**
@@ -53,11 +58,14 @@ class ProductCarouselViewHolder private constructor() {
                 binding.ivProductTagCover.show()
             }
 
-            when(item.price) {
+            when (item.price) {
                 is DiscountedPrice -> {
                     binding.tvProductTagNormalPrice.show()
                     binding.tvProductTagDiscount.show()
-                    binding.tvProductTagDiscount.text = context.getString(R.string.play_bro_product_discount_template, item.price.discountPercent)
+                    binding.tvProductTagDiscount.text = context.getString(
+                        R.string.play_bro_product_discount_template,
+                        item.price.discountPercent
+                    )
                     binding.tvProductTagPrice.text = item.price.discountedPrice
                     binding.tvProductTagNormalPrice.text = item.price.originalPrice
                 }
@@ -82,16 +90,42 @@ class ProductCarouselViewHolder private constructor() {
                 listener.onPinProductClicked(item)
             }
 
-            when(item.pinStatus.pinStatus) {
+            when (item.pinStatus.pinStatus) {
                 PinStatus.Pinned -> {
-                    binding.viewPinProduct.ivPin.setImage(newIconId = IconUnify.PUSH_PIN, newDarkEnable = MethodChecker.getColor(context, unifyR.color.Unify_RN400), newLightEnable = MethodChecker.getColor(context, unifyR.color.Unify_RN400))
-                    binding.viewPinProduct.tvPin.text = context.resources.getString(R.string.play_bro_unpin)
-                    binding.viewPinProduct.tvPin.setTextColor(MethodChecker.getColor(context, unifyR.color.Unify_RN400))
+                    binding.viewPinProduct.ivPin.setImage(
+                        newIconId = IconUnify.PUSH_PIN,
+                        newDarkEnable = MethodChecker.getColor(context, unifyR.color.Unify_RN400),
+                        newLightEnable = MethodChecker.getColor(context, unifyR.color.Unify_RN400)
+                    )
+                    binding.viewPinProduct.tvPin.text =
+                        context.resources.getString(R.string.play_bro_unpin)
+                    binding.viewPinProduct.tvPin.setTextColor(
+                        MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_RN400
+                        )
+                    )
                 }
                 PinStatus.Unpin -> {
-                    binding.viewPinProduct.ivPin.setImage(newIconId = IconUnify.PUSH_PIN, newDarkEnable = MethodChecker.getColor(context, unifyR.color.Unify_Static_White), newLightEnable = MethodChecker.getColor(context, unifyR.color.Unify_Static_White))
-                    binding.viewPinProduct.tvPin.text = context.resources.getString(R.string.play_bro_pin)
-                    binding.viewPinProduct.tvPin.setTextColor(MethodChecker.getColor(context, unifyR.color.Unify_Static_White))
+                    binding.viewPinProduct.ivPin.setImage(
+                        newIconId = IconUnify.PUSH_PIN,
+                        newDarkEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        ),
+                        newLightEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        )
+                    )
+                    binding.viewPinProduct.tvPin.text =
+                        context.resources.getString(R.string.play_bro_pin)
+                    binding.viewPinProduct.tvPin.setTextColor(
+                        MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        )
+                    )
                 }
             }
         }
@@ -107,6 +141,134 @@ class ProductCarouselViewHolder private constructor() {
                     listener
                 )
             }
+        }
+
+        interface Listener {
+            fun onPinProductClicked(product: ProductUiModel)
+        }
+    }
+
+    class PinnedProduct(
+        private val binding: ItemPlayBroPinnedProductCarouselBinding,
+        private val listener: Listener,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.tvPinnedProductCarouselOriginalPrice.paintFlags =
+                binding.tvPinnedProductCarouselOriginalPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }
+
+        private val context: Context
+            get() = itemView.context
+
+
+        fun bind(item: ProductUiModel) {
+            binding.ivPinnedProductCarousel.loadImage(item.imageUrl)
+            binding.tvPinnedProductCarouselName.text = item.name
+
+            if (item.stock > 0) {
+                binding.tvPinnedProductCarouselStock.apply {
+                    text = itemView.context.getString(
+                        R.string.play_bro_product_chooser_stock,
+                        item.stock
+                    )
+                    visibility = View.VISIBLE
+                }
+                binding.tvProductSummaryEmptyStock.gone()
+                binding.ivPinnedProductCarouselOos.gone()
+            } else {
+                binding.tvPinnedProductCarouselStock.gone()
+                binding.tvProductSummaryEmptyStock.visible()
+                binding.ivPinnedProductCarouselOos.visible()
+            }
+
+            when (item.price) {
+                is OriginalPrice -> {
+                    binding.tvPinnedProductCarouselPrice.text = item.price.price
+                    binding.labelPinnedProductCarouselDiscount.visibility = View.GONE
+                    binding.tvPinnedProductCarouselOriginalPrice.visibility = View.GONE
+                }
+                is DiscountedPrice -> {
+                    binding.tvPinnedProductCarouselPrice.text = item.price.discountedPrice
+                    binding.tvPinnedProductCarouselOriginalPrice.text =
+                        item.price.originalPrice
+                    binding.labelPinnedProductCarouselDiscount.text = itemView.context.getString(
+                        R.string.play_bro_product_discount_template,
+                        item.price.discountPercent
+                    )
+                    binding.labelPinnedProductCarouselDiscount.visible()
+                    binding.tvPinnedProductCarouselOriginalPrice.visible()
+                }
+                else -> {
+                    binding.tvPinnedProductCarouselPrice.text = ""
+                    binding.labelPinnedProductCarouselDiscount.visible()
+                    binding.tvPinnedProductCarouselOriginalPrice.visible()
+                }
+            }
+
+            binding.viewPinProduct.root.showWithCondition(item.pinStatus.canPin)
+            binding.viewPinProduct.ivLoaderPin.showWithCondition(item.pinStatus.isLoading)
+            binding.viewPinProduct.ivPin.showWithCondition(!item.pinStatus.isLoading)
+            binding.viewPinProduct.root.setOnClickListener {
+                listener.onPinProductClicked(item)
+            }
+
+            when (item.pinStatus.pinStatus) {
+                PinStatus.Pinned -> {
+                    binding.viewPinProduct.ivPin.setImage(
+                        newIconId = IconUnify.PUSH_PIN,
+                        newDarkEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_RN400
+                        ),
+                        newLightEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_RN400
+                        )
+                    )
+                    binding.viewPinProduct.tvPin.text =
+                        context.resources.getString(R.string.play_bro_unpin)
+                    binding.viewPinProduct.tvPin.setTextColor(
+                        MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_RN400
+                        )
+                    )
+                }
+                PinStatus.Unpin -> {
+                    binding.viewPinProduct.ivPin.setImage(
+                        newIconId = IconUnify.PUSH_PIN,
+                        newDarkEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        ),
+                        newLightEnable = MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        )
+                    )
+                    binding.viewPinProduct.tvPin.text =
+                        context.resources.getString(R.string.play_bro_pin)
+                    binding.viewPinProduct.tvPin.setTextColor(
+                        MethodChecker.getColor(
+                            context,
+                            unifyR.color.Unify_Static_White
+                        )
+                    )
+                }
+            }
+        }
+
+        companion object {
+            fun create(parent: ViewGroup, listener: Listener): PinnedProduct =
+                PinnedProduct(
+                    ItemPlayBroPinnedProductCarouselBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false,
+                    ),
+                    listener
+                )
         }
 
         interface Listener {
--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/adapter/delegate/ProductCarouselAdapterDelegate.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/adapter/delegate/ProductCarouselAdapterDelegate.kt
@@ -3,6 +3,7 @@ package com.tokopedia.play.broadcaster.view.adapter.delegate
 import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.adapterdelegate.TypedAdapterDelegate
+import com.tokopedia.play.broadcaster.ui.model.pinnedproduct.PinStatus
 import com.tokopedia.play_common.R as commonR
 import com.tokopedia.play.broadcaster.ui.model.product.ProductUiModel
 import com.tokopedia.play.broadcaster.ui.viewholder.carousel.ProductCarouselViewHolder
@@ -12,8 +13,17 @@ import com.tokopedia.play.broadcaster.ui.viewholder.carousel.ProductCarouselView
  */
 internal class ProductCarouselAdapterDelegate private constructor() {
 
-    class Product(private  val listener: ProductCarouselViewHolder.Product.Listener, ) : TypedAdapterDelegate<
+    class Product(private val listener: ProductCarouselViewHolder.Product.Listener) : TypedAdapterDelegate<
             ProductUiModel, Any, ProductCarouselViewHolder.Product>(commonR.layout.view_play_empty) {
+
+        override fun isForViewType(
+            itemList: List<Any>,
+            position: Int,
+            isFlexibleType: Boolean
+        ): Boolean {
+            val product = itemList.filterIsInstance<ProductUiModel>()[position]
+            return product.pinStatus.pinStatus != PinStatus.Pinned
+        }
 
         override fun onBindViewHolder(
             item: ProductUiModel,
@@ -28,6 +38,32 @@ internal class ProductCarouselAdapterDelegate private constructor() {
         ): ProductCarouselViewHolder.Product {
             return ProductCarouselViewHolder.Product.create(parent, listener)
         }
+    }
+
+    class PinnedProduct(private val pinnedProductListener: ProductCarouselViewHolder.PinnedProduct.Listener): TypedAdapterDelegate<
+            ProductUiModel, Any, ProductCarouselViewHolder.PinnedProduct>(commonR.layout.view_play_empty){
+
+        override fun isForViewType(
+            itemList: List<Any>,
+            position: Int,
+            isFlexibleType: Boolean
+        ): Boolean {
+            val product = itemList.filterIsInstance<ProductUiModel>()[position]
+            return product.pinStatus.pinStatus == PinStatus.Pinned
+        }
+
+        override fun onBindViewHolder(
+            item: ProductUiModel,
+            holder: ProductCarouselViewHolder.PinnedProduct
+        ) {
+            holder.bind(item)
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            basicView: View
+        ): ProductCarouselViewHolder.PinnedProduct = ProductCarouselViewHolder.PinnedProduct.create(parent, listener = pinnedProductListener)
+
     }
 
     class Loading : TypedAdapterDelegate<
