2fc2060310048f5c2b0783c6aeb9ea712ca24d7e
yohann.prananta <yohann.prananta@BA-00019887.local>
2022-07-25 13:28:20 +0700

    code clean up - remove unused resource, function

--- features/tokofood/src/test/java/com/tokopedia/tokofood/merchant/MerchantPageViewModelTestFixture.kt
+++ features/tokofood/src/test/java/com/tokopedia/tokofood/merchant/MerchantPageViewModelTestFixture.kt
@@ -36,11 +36,4 @@ abstract class MerchantPageViewModelTestFixture {
                 getChooseAddressWarehouseLocUseCase = getChooseAddressWarehouseLocUseCase
         )
     }
-
-    inline fun <reified T>Any.getPrivateField(name: String): T {
-        return this::class.java.getDeclaredField(name).let {
-            it.isAccessible = true
-            return@let it.get(this) as T
-        }
-    }
 }
