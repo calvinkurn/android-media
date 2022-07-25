7d669dabb0d6dde7169f093e85bfb2fba165f05f
fajarca <fajar.azhar@tokopedia.com>
2022-07-25 15:15:30 +0700

    Add date constant

---
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/constant/DateConstant.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.campaign.utils.constant
+
+object DateConstant {
+    //region Date Time
+    const val DATE_TIME_SECOND_PRECISION = "yyyy-MM-dd HH:mm:ss"
+    //endregion
+
+    //region Date
+    const val DATE_YEAR_PRECISION = "dd MMM yyyy"
+    //endregion
+}
