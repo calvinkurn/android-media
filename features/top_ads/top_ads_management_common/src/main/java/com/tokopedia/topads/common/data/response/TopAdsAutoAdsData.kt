096f156a0c43293feba61484366726c723fe66d1
Ankit <ankit.kumar1@tokopedia.com>
2022-07-25 11:09:57 +0530

    TopAdsAutoAdsData reverted

--- features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/response/TopAdsAutoAdsData.kt
+++ features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/response/TopAdsAutoAdsData.kt
@@ -6,24 +6,26 @@ import com.google.gson.annotations.SerializedName
  * Author errysuprayogi on 15,May,2019
  */
 data class TopAdsAutoAdsData(
-    @SerializedName("shopID")
-    val shopId: String? = "",
-    @SerializedName("status")
-    val status: Int = 0,
-    @SerializedName("statusDesc")
-    val statusDesc: String? = "",
-    @SerializedName("dailyBudget")
-    val dailyBudget: Int = 0,
-    @SerializedName("dailyUsage")
-    val dailyUsage: Int = 0,
-    @SerializedName("info")
-    val adsInfo: TopAdsAutoAdsInfo? = TopAdsAutoAdsInfo(),
+
+        @SerializedName("shop_id")
+        val shopId: Int = 0,
+        @SerializedName("status")
+        val status: Int = 0,
+        @SerializedName("status_desc")
+        val statusDesc: String = "",
+        @SerializedName("daily_budget")
+        val dailyBudget: Int = 0,
+        @SerializedName("daily_usage")
+        val dailyUsage: Int = 0,
+        @SerializedName("info")
+        val adsInfo: TopAdsAutoAdsInfo = TopAdsAutoAdsInfo()
 ) {
     data class TopAdsAutoAdsInfo(
 
-        @SerializedName("reason")
-        val reason: String? = "",
-        @SerializedName("message")
-        val message: String? = "",
+            @SerializedName("reason")
+            val reason: String = "",
+            @SerializedName("message")
+            val message: String = ""
     )
+
 }
