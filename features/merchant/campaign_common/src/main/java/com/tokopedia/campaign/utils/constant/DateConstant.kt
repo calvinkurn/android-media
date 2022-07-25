0aa382d36e8a36cc60b340eecc0d46a9547ea203
fajarca <fajar.azhar@tokopedia.com>
2022-07-25 15:57:04 +0700

    Add new date constant

--- features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/constant/DateConstant.kt
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/constant/DateConstant.kt
@@ -4,6 +4,7 @@ object DateConstant {
     //region Date Time
     const val DATE_TIME_MINUTE_PRECISION = "yyyy-MM-dd HH:mm"
     const val DATE_TIME_SECOND_PRECISION = "yyyy-MM-dd HH:mm:ss"
+    const val DATE_WITH_SECOND_PRECISION_WITH_TIMEZONE = "yyyy-MM-dd'T'HH:mm:ssZ"
     //endregion
 
     //region Date
--- features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/extension/LongExtension.kt
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/extension/LongExtension.kt
@@ -4,15 +4,26 @@ import com.tokopedia.campaign.utils.constant.LocaleConstant
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Date
+import java.util.Locale
 
-const val DECIMAL_FORMAT_PATTERN = "#,###,###"
+private const val DECIMAL_FORMAT_PATTERN = "#,###,###"
 
+/**
+ * Convert millis to Date object
+ */
 fun Long.toDate(): Date {
     return Date(this)
 }
 
-fun Long.splitByThousand() : String {
-    val symbol = DecimalFormatSymbols(LocaleConstant.INDONESIA)
-    val formatter = DecimalFormat(DECIMAL_FORMAT_PATTERN, symbol)
+/**
+ * Input: 1_000_000
+ * Output: Rp 1.000.000 (if locale is Indonesia)
+ */
+fun Long.splitByThousand(
+    desiredOutputFormat: String = DECIMAL_FORMAT_PATTERN,
+    locale: Locale = LocaleConstant.INDONESIA
+): String {
+    val symbol = DecimalFormatSymbols(locale)
+    val formatter = DecimalFormat(desiredOutputFormat, symbol)
     return formatter.format(this)
 }
