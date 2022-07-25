3467ad525d943a5c8eda71d57df5ba7c313c0ae4
fajarca <fajar.azhar@tokopedia.com>
2022-07-25 15:28:09 +0700

    Add text color text view ext

--- features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/extension/TextViewExtension.kt
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/utils/extension/TextViewExtension.kt
@@ -1,8 +1,15 @@
 package com.tokopedia.campaign.utils.extension
 
 import android.graphics.Paint
-import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 
-fun TextView.strikethrough() {
+fun AppCompatTextView.strikethrough() {
     this.paintFlags = this.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+}
+
+fun AppCompatTextView.textColor(@ColorRes resourceId: Int) {
+    val color = ContextCompat.getColor(this.context, resourceId)
+    this.setTextColor(color)
 }
