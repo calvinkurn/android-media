0ba93cb34c09f7c1f62f0f57b5633d2842c35421
mohit-toko <mohit.singh@tokopedia.com>
2022-07-25 15:12:30 +0530

    Case fix and GTM fix

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/analytics/EPharmacyAnalytics.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/analytics/EPharmacyAnalytics.kt
@@ -2,7 +2,7 @@ package com.tokopedia.checkout.analytics
 
 import com.tokopedia.purchase_platform.common.analytics.TransactionAnalytics
 
-class EPharmacyAnalytics constructor(val userId: String) : TransactionAnalytics() {
+class EPharmacyAnalytics constructor(val userId: String, val isLoggedIn : Boolean) : TransactionAnalytics() {
     companion object {
         const val KEY_BUSINESS_UNIT = "businessUnit"
         const val VALUE_BUSINESS_UNIT = "Physical Goods"
@@ -10,6 +10,7 @@ class EPharmacyAnalytics constructor(val userId: String) : TransactionAnalytics(
         const val VALUE_CURRENT_SITE = "tokopediamarketplace"
         const val KEY_USER_ID = "userId"
         const val KEY_TRACKER_ID = "trackerId"
+        const val KEY_IS_LOGGED_IN = "isLoggedInStatus"
     }
 
     interface Events {
@@ -44,6 +45,7 @@ class EPharmacyAnalytics constructor(val userId: String) : TransactionAnalytics(
         gtmData[KEY_CURRENT_SITE] = VALUE_CURRENT_SITE
         gtmData[KEY_TRACKER_ID] = TrackerId.CLICK_UPLOAD_PRESCRIPTION_TRACKER_ID
         gtmData[KEY_USER_ID] = userId
+        gtmData[KEY_IS_LOGGED_IN] = isLoggedIn
         sendGeneralEvent(gtmData)
     }
 }
