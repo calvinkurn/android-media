758bcbc4817aaff44f119834014860ea0c55ee67
hiradevina <fakhira.devina@tokopedia.com>
2022-07-27 14:56:19 +0700

    fix: hide service when all products ui_rates_hidden == true

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingduration/view/ShippingDurationConverter.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/shipping/features/shippingduration/view/ShippingDurationConverter.kt
@@ -65,7 +65,7 @@ class ShippingDurationConverter @Inject constructor() {
             shippingDurationUiModel.etaErrorCode = serviceData.texts.errorCode
             val shippingCourierUiModels = convertToShippingCourierViewModel(shippingDurationUiModel,
                     serviceData.products, ratesId, blackboxInfo, convertToPreOrderModel(ratesDetailData.preOrder))
-            shippingDurationUiModel.serviceData.isUiRatesHidden = shippingDurationUiModel.serviceData.isUiRatesHidden && shippingCourierUiModels.all { it.productData.isUiRatesHidden }
+            shippingDurationUiModel.serviceData.isUiRatesHidden = shippingDurationUiModel.serviceData.isUiRatesHidden || shippingCourierUiModels.all { it.productData.isUiRatesHidden }
             shippingDurationUiModel.shippingCourierViewModelList = shippingCourierUiModels
             if (shippingCourierUiModels.isNotEmpty()) {
                 shippingDurationUiModels.add(shippingDurationUiModel)
