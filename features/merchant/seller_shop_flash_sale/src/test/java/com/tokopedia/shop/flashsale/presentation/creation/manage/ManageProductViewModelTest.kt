7efedd4f93eaa688623982cca5137bd046b25bd9
Yuda <yuda.putra@tokopedia.com>
2022-07-28 09:42:20 +0700

    add ManageProductViewModelTestr File

---
+++ features/merchant/seller_shop_flash_sale/src/test/java/com/tokopedia/shop/flashsale/presentation/creation/manage/ManageProductViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.shop.flashsale.presentation.creation.manage
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class ManageProductViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
