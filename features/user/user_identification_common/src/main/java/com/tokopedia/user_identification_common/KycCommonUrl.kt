cdd87047e888e40b5d8a0aa343ca1ca27bf0a606
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-07-29 09:12:28 +0800

    update link

--- features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KycCommonUrl.kt
+++ features/user/user_identification_common/src/main/java/com/tokopedia/user_identification_common/KycCommonUrl.kt
@@ -1,13 +1,13 @@
 package com.tokopedia.user_identification_common
 
 import com.tokopedia.applink.ApplinkConst
-import com.tokopedia.url.TokopediaUrl.Companion.getInstance
+import com.tokopedia.url.TokopediaUrl
 
 /**
  * @author by nisie on 16/11/18.
  */
 object KycCommonUrl {
-    private val URL_TERMS_AND_CONDITION = "https://www.tokopedia.com/help/article/syarat-dan-ketentuan-verifikasi-pengguna"
+    private val URL_TERMS_AND_CONDITION = "${TokopediaUrl.getInstance().WEB}/help/article/syarat-dan-ketentuan-verifikasi-pengguna"
     @JvmField
     val APPLINK_TERMS_AND_CONDITION = String.format("%s?url=%s", ApplinkConst.WEBVIEW, URL_TERMS_AND_CONDITION)
 }
