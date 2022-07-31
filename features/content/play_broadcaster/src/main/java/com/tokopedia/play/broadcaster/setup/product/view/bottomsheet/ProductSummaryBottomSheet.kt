32ac0e5701b5b4966ee320defb40176b2362277b
fachrizalmrsln-tkpd <fachrizal.mursalin@tokopedia.com>
2022-07-31 17:18:38 +0800

    `refactor`: change implementation of fail pin-unpin

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/setup/product/view/bottomsheet/ProductSummaryBottomSheet.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/setup/product/view/bottomsheet/ProductSummaryBottomSheet.kt
@@ -63,10 +63,15 @@ class ProductSummaryBottomSheet @Inject constructor(
     }
 
     private fun checkPinProduct(pinStatus: Boolean, ifTimerIsOn: () -> Unit) {
-        if (!viewModel.getCoolDownStatus() || pinStatus) ifTimerIsOn()
+        if (!viewModel.getCoolDownStatus()) ifTimerIsOn()
         else {
+            if (pinStatus) analytic.onImpressFailUnPinProductBottomSheet(viewModel.channelId)
+            else analytic.onImpressFailPinProductBottomSheet(viewModel.channelId)
             toaster.showToaster(
-                message = getString(R.string.play_bro_pin_product_failed),
+                message = getString(
+                    if (pinStatus) R.string.play_bro_pin_product_un_pin_failed
+                    else R.string.play_bro_pin_product_pin_failed
+                ),
                 type = Toaster.TYPE_ERROR
             )
         }
