94c3e1374b108027ea27c713e3fbade4d292aa93
Satyam Tewari <satyam.tewari@tokopedia.com>
2022-08-02 15:11:59 +0530

    Color mapping added for credit status.

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/credit/history/view/adapter/viewholder/CreditHistoryViewHolder.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/credit/history/view/adapter/viewholder/CreditHistoryViewHolder.kt
@@ -13,18 +13,46 @@ class CreditHistoryViewHolder(val view: View) : AbstractViewHolder<CreditHistory
     private val txtAmount: Typography? = view.findViewById(R.id.txt_amount)
     private val txtTitle: Typography? = view.findViewById(R.id.txt_title)
     private val txtDate: Typography? = view.findViewById(R.id.txt_date)
+    private val txtStatus: Typography? = view.findViewById(R.id.txt_status)
 
     override fun bind(element: CreditHistory) {
         with(itemView) {
             txtDate?.text = element.formatedDate
             txtAmount?.text = ("${if (element.isReduction) "-" else "+"} ${element.amountFmt}")
             if (!element.isReduction)
-                txtAmount?.setTextColor(ContextCompat.getColor(context,
-                    com.tokopedia.topads.common.R.color.topads_common_green_tab))
+                txtAmount?.setTextColor(
+                    ContextCompat.getColor(
+                        context,
+                        com.tokopedia.topads.common.R.color.topads_common_green_tab
+                    )
+                )
             else
-                txtAmount?.setTextColor(ContextCompat.getColor(context,
-                    com.tokopedia.topads.common.R.color.topads_heading_color))
+                txtAmount?.setTextColor(
+                    ContextCompat.getColor(
+                        context,
+                        com.tokopedia.topads.common.R.color.topads_heading_color
+                    )
+                )
             txtTitle?.text = element.description
+
+            txtStatus?.text = element.status
+            txtStatus?.setTextColor(
+                ContextCompat.getColor(
+                    context,
+                    when (element.status) {
+                        getString(R.string.topads_credit_status_berhasil) ->
+                            com.tokopedia.unifyprinciples.R.color.Unify_GN500
+                        getString(R.string.topads_credit_status_digunakan) ->
+                            com.tokopedia.unifyprinciples.R.color.Unify_NN500
+                        getString(R.string.topads_credit_status_menunggu) ->
+                            com.tokopedia.unifyprinciples.R.color.Unify_YN500
+                        getString(R.string.topads_credit_status_kedaluwarsa) ->
+                            com.tokopedia.unifyprinciples.R.color.Unify_RN500
+                        else ->
+                            com.tokopedia.unifyprinciples.R.color.Unify_NN500
+                    }
+                )
+            )
         }
     }
 
