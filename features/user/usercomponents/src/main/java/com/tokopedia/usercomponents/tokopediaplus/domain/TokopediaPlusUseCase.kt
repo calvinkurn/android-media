da4fc612eb07f6478347bf8d81c25bfde24a5535
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-08-03 15:50:55 +0800

    adjust & add ui login for specific page

--- features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/tokopediaplus/domain/TokopediaPlusUseCase.kt
+++ features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/tokopediaplus/domain/TokopediaPlusUseCase.kt
@@ -2,10 +2,10 @@ package com.tokopedia.usercomponents.tokopediaplus.domain
 
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.graphql.coroutines.data.extensions.request
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import javax.inject.Inject
+import kotlin.random.Random
 
 
 /**
@@ -21,7 +21,16 @@ class TokopediaPlusUseCase @Inject constructor(
 ) : CoroutineUseCase<Map<String, String>, TokopediaPlusResponseDataModel>(dispatchers.io) {
 
     override suspend fun execute(params: Map<String, String>): TokopediaPlusResponseDataModel {
-        return repository.request(graphqlQuery(), params)
+//        return repository.request(graphqlQuery(), params)
+        return TokopediaPlusResponseDataModel(
+            TokopediaPlusDataModel(
+                isShown = true,
+                isSubscriber = Random.nextBoolean(),
+                iconImageURL = "https://images.tokopedia.net/img/plus/logo/default_logo.png",
+                title = "Tokopedia Plus!",
+                subtitle = "Temukan banyak ke untungan"
+            )
+        )
     }
 
     override fun graphqlQuery(): String {
