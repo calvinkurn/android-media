f62e3d485f983c12a79b1f9e6c9f9c9621708cb0
isfaaghyth <isfaaghyth@gmail.com>
2022-08-03 13:41:08 +0700

    [Explicit] Add ExplisitModule on Discovery Home module

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/di/BerandaComponent.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/di/BerandaComponent.kt
@@ -6,10 +6,18 @@ import com.tokopedia.home.beranda.di.module.ViewModelModule
 import com.tokopedia.home.beranda.presentation.view.fragment.HomeRecommendationFragment
 import com.tokopedia.home.beranda.presentation.view.fragment.HomeRevampFragment
 import dagger.Component
-import com.tokopedia.home.beranda.di.HomeScope
+import com.tokopedia.usercomponents.explicit.di.ExplicitViewModule
 
 @HomeScope
-@Component(modules = [HomeModule::class, ViewModelModule::class], dependencies = [BaseAppComponent::class])
+@Component(
+    modules = [
+        HomeModule::class,
+        ViewModelModule::class,
+        ExplicitViewModule::class
+    ], dependencies = [
+        BaseAppComponent::class
+    ]
+)
 interface BerandaComponent {
     fun inject(homeRevampFragment: HomeRevampFragment?)
     fun inject(homeRecommendationFragment: HomeRecommendationFragment)
