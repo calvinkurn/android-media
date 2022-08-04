a6ad0552bf9a3108b5ea63f361e3d64e9ed4960e
rama.putra <rama.putra@tokopedia.com>
2022-08-04 16:58:53 +0700

    add fragment

--- features/operational/resolution/src/main/java/com/tokopedia/resolution/ResolutionSuccessActivity.kt
+++ features/operational/resolution/src/main/java/com/tokopedia/resolution/ResolutionSuccessActivity.kt
@@ -1,58 +1,13 @@
 package com.tokopedia.resolution
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.tokopedia.applink.ApplinkConst
-import com.tokopedia.applink.RouteManager
-import com.tokopedia.applink.internal.ApplinkConstInternalOperational
-import com.tokopedia.resolution.databinding.ActivityResolutionSuccessBinding
-import com.tokopedia.utils.view.binding.viewBinding
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.webview.KEY_URL
 
-class ResolutionSuccessActivity : AppCompatActivity() {
+class ResolutionSuccessActivity : BaseSimpleActivity() {
 
-    private val binding: ActivityResolutionSuccessBinding? by viewBinding()
-    private var url: String = ""
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_resolution_success)
-        initData()
-        setToolbar()
-
-        binding?.btnGoToDetail?.setOnClickListener {
-            routeToDetail()
-        }
-
-        binding?.btnGoToHome?.setOnClickListener {
-            routeToHome()
-        }
+    override fun getNewFragment(): Fragment {
+        val url = intent.data?.getQueryParameter(KEY_URL) ?: ""
+        return ResolutionSuccessFragment.createNewInstance(url)
     }
-
-    private fun setToolbar() {
-        supportActionBar?.title = getString(R.string.title_toolbar_resolution)
-    }
-
-    private fun initData() {
-        url = intent.data?.getQueryParameter(KEY_URL) ?: ""
-    }
-
-    private fun routeToDetail() {
-        if (url.isNotEmpty()) {
-            RouteManager.route(this, String.format("%s?url=%s", ApplinkConst.WEBVIEW, url))
-            finish()
-        }
-    }
-
-    private fun routeToHome() {
-        this.let {
-            val intent = RouteManager.getIntent(it, ApplinkConst.HOME)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            it.startActivity(intent)
-        }
-    }
-
-
 }
---
+++ features/operational/resolution/src/main/java/com/tokopedia/resolution/ResolutionSuccessFragment.kt
@@ -0,0 +1,84 @@
+package com.tokopedia.resolution
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.fragment.TkpdBaseV4Fragment
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.resolution.databinding.FragmentResolutionSuccessBinding
+import com.tokopedia.utils.view.binding.viewBinding
+import com.tokopedia.webview.KEY_URL
+
+class ResolutionSuccessFragment : TkpdBaseV4Fragment() {
+
+    private val binding: FragmentResolutionSuccessBinding? by viewBinding()
+    private var url: String = ""
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_resolution_success, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initData()
+        initView()
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        activity?.let {
+            (it as ResolutionSuccessActivity).supportActionBar?.title = getString(R.string.title_toolbar_resolution)
+        }
+    }
+
+    private fun initView() {
+        binding?.btnGoToDetail?.setOnClickListener {
+            routeToDetail()
+        }
+
+        binding?.btnGoToHome?.setOnClickListener {
+            routeToHome()
+        }
+    }
+
+    private fun initData() {
+        url = arguments?.getString(KEY_URL) ?: ""
+    }
+
+    private fun routeToDetail() {
+        if (url.isNotEmpty()) {
+            activity?.let {
+                RouteManager.route(it, String.format("%s?url=%s", ApplinkConst.WEBVIEW, url))
+                it.finish()
+            }
+        }
+    }
+
+    private fun routeToHome() {
+        activity?.let {
+            val intent = RouteManager.getIntent(it, ApplinkConst.HOME)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            it.startActivity(intent)
+        }
+    }
+
+    override fun getScreenName(): String {
+        return getString(com.tokopedia.resolution.R.string.screen_name_resolution) ?: ""
+    }
+
+    companion object {
+        fun createNewInstance(url: String): Fragment {
+            val fragment = ResolutionSuccessFragment()
+            val bundle = Bundle()
+            bundle.apply {
+                putString(KEY_URL, url)
+            }
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
