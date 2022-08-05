c6e4c3d1a901e8421f84cf5557db3c4f4a5b29de
meyta.taliti <meyta.taliti@tokopedia.com>
2022-08-05 12:47:36 +0700

    deprecating old usecase, and add comment for future reference

--- features/content/people/src/main/java/com/tokopedia/people/domains/PlayPostContentUseCase.kt
+++ features/content/people/src/main/java/com/tokopedia/people/domains/PlayPostContentUseCase.kt
@@ -6,6 +6,9 @@ import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.people.model.UserPostModel
 import javax.inject.Inject
 
+/**
+ * Duplicate with: com.tokopedia.videoTabComponent.domain.usecase.GetPlayContentUseCase
+ */
 const val PLAY_VIDEO_QUERY = """
     query playGetContentSlot(${'$'}group: String, ${'$'}cursor: String, ${'$'}source_type: String, ${'$'}source_id: String) {
                 playGetContentSlot(req: {group:${'$'}group, cursor: ${'$'}cursor, source_type: ${'$'}source_type, source_id: \${'$'}source_id}) {
