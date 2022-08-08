d86f4328c5d864dd326ecdade2228ca03941a5bf
fajarca <fajar.azhar@tokopedia.com>
2022-08-08 13:54:55 +0700

    Move some func to global module #2

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
@@ -3,6 +3,9 @@ package com.tokopedia.kotlin.extensions.view
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 /**
  * @author by milhamj on 30/11/18.
@@ -18,4 +21,37 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
 
         override fun onTextChanged(char: CharSequence?, p1: Int, p2: Int, p3: Int) {}
     })
+}
+
+inline fun EditText.doOnTextChanged(crossinline onTextChangeEvent: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            onTextChangeEvent(s.toString())
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+    })
+}
+
+fun EditText.textChangesAsFlow(): Flow<String> {
+    return Channel<String>(capacity = Channel.UNLIMITED).also { channel ->
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s == null) return
+                channel.trySend(this@textChangesAsFlow.text.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }.receiveAsFlow()
 }
