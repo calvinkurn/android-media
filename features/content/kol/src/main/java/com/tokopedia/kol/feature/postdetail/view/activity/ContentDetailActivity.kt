a27530a27e949ba04981cfcb234b918ff5b6796f
shrutiagarwal <shruti.agarwal@tokopedia.com>
2022-08-08 11:48:46 +0530

    Fix: back press when opened from sharelink

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/activity/ContentDetailActivity.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/activity/ContentDetailActivity.kt
@@ -56,6 +56,7 @@ class ContentDetailActivity : BaseSimpleActivity() {
             val intent = RouteManager.getIntent(it, ApplinkConst.HOME_FEED)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
+            finishAffinity()
         }
     }
 
