eb38d1f10268d790d84e2c26b604e6db29a66df5
fajarca <fajar.azhar@tokopedia.com>
2022-08-08 14:32:42 +0700

    Move doOnTextchanged change to campaign common

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
@@ -23,22 +23,6 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
     })
 }
 
-inline fun EditText.doOnTextChanged(crossinline onTextChangeEvent: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            onTextChangeEvent(s.toString())
-        }
-
-        override fun afterTextChanged(s: Editable?) {
-
-        }
-    })
-}
-
 fun EditText.textChangesAsFlow(): Flow<String> {
     return Channel<String>(capacity = Channel.UNLIMITED).also { channel ->
         addTextChangedListener(object : TextWatcher {
