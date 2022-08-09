8e0e9548bc803361603be0151532c570b1f7eeba
fajarca <fajar.azhar@tokopedia.com>
2022-08-09 16:04:14 +0700

    Use sharedflow instead of channel

--- libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
+++ libraries/kotlin_extensions/src/main/java/com/tokopedia/kotlin/extensions/view/EditTextExt.kt
@@ -3,13 +3,14 @@ package com.tokopedia.kotlin.extensions.view
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 
 /**
  * @author by milhamj on 30/11/18.
  */
+
+private const val DEFAULT_REPLAY_COUNT = 1
 
 fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
@@ -24,18 +25,20 @@ fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
 }
 
 fun EditText.textChangesAsFlow(): Flow<String> {
-    return Channel<String>(capacity = Channel.UNLIMITED).also { channel ->
-        addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+    val flow = MutableSharedFlow<String>(replay = DEFAULT_REPLAY_COUNT)
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s == null) return
-                channel.trySend(this@textChangesAsFlow.text.toString())
-            }
+    addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
 
-            override fun afterTextChanged(s: Editable?) {
-            }
-        })
-    }.receiveAsFlow()
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            if (s == null) return
+            flow.tryEmit(s.toString())
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
+    })
+
+    return flow
 }
