875ac12aefb3e6a7ef86a888999579106b0a6637
fajarca <fajar.azhar@tokopedia.com>
2022-08-10 18:46:32 +0700

    Move query as internal object

--- features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/domain/usecase/GetFlashSaleListForSellerMetaUseCase.kt
+++ features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/domain/usecase/GetFlashSaleListForSellerMetaUseCase.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.tkpd.flashsale.presentation.domain.usecase
 
+import com.tokopedia.gql_query_annotation.GqlQueryInterface
 import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
@@ -7,7 +8,6 @@ import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.tkpd.flashsale.presentation.data.mapper.GetFlashSaleListForSellerMetaMapper
-import com.tokopedia.tkpd.flashsale.presentation.data.query.GetFlashSaleListForSellerMetaQuery
 import com.tokopedia.tkpd.flashsale.presentation.data.request.GetFlashSaleListForSellerMetaRequest
 import com.tokopedia.tkpd.flashsale.presentation.data.response.GetFlashSaleListForSellerMetaResponse
 import com.tokopedia.tkpd.flashsale.presentation.domain.entity.TabMetadata
@@ -36,14 +36,37 @@ class GetFlashSaleListForSellerMetaUseCase @Inject constructor(
     }
 
     private fun buildRequest(): GraphqlRequest {
-        val requestHeader = GetFlashSaleListForSellerMetaRequest.CampaignParticipationRequestHeader(usecase = "campaign_list")
+        val requestHeader =
+            GetFlashSaleListForSellerMetaRequest.CampaignParticipationRequestHeader(usecase = "campaign_list")
         val payload = GetFlashSaleListForSellerMetaRequest(requestHeader)
         val params = mapOf(REQUEST_PARAM_KEY to payload)
 
         return GraphqlRequest(
-            GetFlashSaleListForSellerMetaQuery,
+            query,
             GetFlashSaleListForSellerMetaResponse::class.java,
             params
         )
+    }
+
+    private val query = object : GqlQueryInterface {
+
+        private val OPERATION_NAME = "getFlashSaleListForSellerMeta"
+        private val QUERY = """
+        query $OPERATION_NAME(${'$'}params: GetFlashSaleListForSellerMeta!) {
+             $OPERATION_NAME(params: ${'$'}params) {
+                   tab_list {
+                     tab_id
+                     tab_name
+                     total_campaign
+                     display_name
+                   }
+             }
+       }
+
+    """.trimIndent()
+
+        override fun getOperationNameList(): List<String> = listOf(OPERATION_NAME)
+        override fun getQuery(): String = QUERY
+        override fun getTopOperationName(): String = OPERATION_NAME
     }
 }
