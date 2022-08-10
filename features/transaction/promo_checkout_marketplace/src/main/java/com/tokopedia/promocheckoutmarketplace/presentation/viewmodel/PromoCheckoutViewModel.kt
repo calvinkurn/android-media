4d703d0acdee51f551437e0a2124b3dabec2dfbe
hiradevina <fakhira.devina@tokopedia.com>
2022-08-10 14:30:09 +0700

    fix: remove previous bebas ongkir selected before set param

--- features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/viewmodel/PromoCheckoutViewModel.kt
+++ features/transaction/promo_checkout_marketplace/src/main/java/com/tokopedia/promocheckoutmarketplace/presentation/viewmodel/PromoCheckoutViewModel.kt
@@ -577,6 +577,8 @@ class PromoCheckoutViewModel @Inject constructor(dispatcher: CoroutineDispatcher
     //------------------------------------//
 
     fun applyPromo(validateUsePromoRequest: ValidateUsePromoRequest, bboPromoCodes: ArrayList<String>) {
+        // remove previous bbo promo
+        removeBebasOngkirPromoFromLastRequestData(validateUsePromoRequest)
         // Set request data
         setApplyPromoRequestData(validateUsePromoRequest)
 
@@ -692,10 +694,26 @@ class PromoCheckoutViewModel @Inject constructor(dispatcher: CoroutineDispatcher
         }
     }
 
+    private fun removeBebasOngkirPromoFromLastRequestData(validateUsePromoRequest: ValidateUsePromoRequest) {
+        validateUsePromoRequest.orders.forEach { order ->
+            promoListUiModel.value?.forEach { visitable ->
+                if (visitable is PromoListItemUiModel && visitable.uiState.isBebasOngkir && !visitable.uiState.isSelected) {
+                    val boData = visitable.uiData.boAdditionalData.firstOrNull { order.uniqueId == it.uniqueId }
+                    boData?.let {
+                        if (order.codes.contains(it.code)) {
+                            order.shippingId = 0
+                            order.spId = 0
+                            order.codes.remove(it.code)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private fun setApplyPromoRequestDataFromSelectedPromo(promoListItemUiModel: PromoListItemUiModel,
                                                           order: OrdersItem?,
                                                           validateUsePromoRequest: ValidateUsePromoRequest) {
-        // for each order, refresh every promo list ui model
         if (promoListItemUiModel.uiState.isSelected && !promoListItemUiModel.uiState.isDisabled &&
                 promoListItemUiModel.uiData.currentClashingPromo.isNullOrEmpty()) {
             // If coupon is selected, not disabled, and not clashing, add to request param
@@ -731,21 +749,23 @@ class PromoCheckoutViewModel @Inject constructor(dispatcher: CoroutineDispatcher
             if (promoListItemUiModel.uiData.uniqueId == order?.uniqueId &&
                     order.codes.contains(promoListItemUiModel.uiData.promoCode)) {
                 order.codes.remove(promoListItemUiModel.uiData.promoCode)
-            } else if (promoListItemUiModel.uiState.isBebasOngkir) {
-                // if coupon is bebas ongkir promo, then reset shipping id, sp id, and code
-                val boData = promoListItemUiModel.uiData.boAdditionalData.firstOrNull { order?.uniqueId == it.uniqueId }
-                if (boData != null) {
-                    // todo what happen if user check bo reg, and uncheck bo plus for the same order
-                    order?.let {
-                        // todo this logic does not support address change from other page
-                        if (it.codes.contains(boData.code) && it.shippingId == boData.shippingId && it.spId == boData.shipperProductId) {
-                            it.shippingId = 0
-                            it.spId = 0
-                            it.codes.remove(boData.code)
-                        }
-                    }
-                }
-            } else if (promoListItemUiModel.uiData.shopId == 0 &&
+            }
+//            else if (promoListItemUiModel.uiState.isBebasOngkir) {
+//                // if coupon is bebas ongkir promo, then reset shipping id, sp id, and code
+//                val boData = promoListItemUiModel.uiData.boAdditionalData.firstOrNull { order?.uniqueId == it.uniqueId }
+//                if (boData != null) {
+//                    // todo what happen if user check bo reg, and uncheck bo plus for the same order
+//                    order?.let {
+//                        // todo this logic does not support address change from other page
+//                        if (it.codes.contains(boData.code) && it.shippingId == boData.shippingId && it.spId == boData.shipperProductId) {
+//                            it.shippingId = 0
+//                            it.spId = 0
+//                            it.codes.remove(boData.code)
+//                        }
+//                    }
+//                }
+//            }
+            else if (promoListItemUiModel.uiData.shopId == 0 &&
                     validateUsePromoRequest.codes.contains(promoListItemUiModel.uiData.promoCode)) {
                 validateUsePromoRequest.codes.remove(promoListItemUiModel.uiData.promoCode)
             }
