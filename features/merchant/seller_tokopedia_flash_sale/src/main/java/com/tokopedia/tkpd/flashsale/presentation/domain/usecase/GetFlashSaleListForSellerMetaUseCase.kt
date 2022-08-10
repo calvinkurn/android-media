9463d88557ec4a1f59ea596aa669374035176e7d
fajarca <fajar.azhar@tokopedia.com>
2022-08-10 19:49:56 +0700

    Adjust code position

--- features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/domain/usecase/GetFlashSaleListForSellerMetaUseCase.kt
+++ features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/domain/usecase/GetFlashSaleListForSellerMetaUseCase.kt
@@ -20,32 +20,12 @@ class GetFlashSaleListForSellerMetaUseCase @Inject constructor(
     private val mapper: GetFlashSaleListForSellerMetaMapper
 ) : GraphqlUseCase<List<TabMetadata>>(repository) {
 
-
-    companion object {
-        private const val REQUEST_PARAM_KEY = "params"
-    }
-
     init {
         setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build())
     }
 
-    suspend fun execute(): List<TabMetadata> {
-        val request = buildRequest()
-        val response = repository.response(listOf(request))
-        val data = response.getSuccessData<GetFlashSaleListForSellerMetaResponse>()
-        return mapper.map(data)
-    }
-
-    private fun buildRequest(): GraphqlRequest {
-        val requestHeader = CampaignParticipationRequestHeader(usecase = "campaign_list")
-        val payload = GetFlashSaleListForSellerMetaRequest(requestHeader)
-        val params = mapOf(REQUEST_PARAM_KEY to payload)
-
-        return GraphqlRequest(
-            query,
-            GetFlashSaleListForSellerMetaResponse::class.java,
-            params
-        )
+    companion object {
+        private const val REQUEST_PARAM_KEY = "params"
     }
 
     private val query = object : GqlQueryInterface {
@@ -69,4 +49,24 @@ class GetFlashSaleListForSellerMetaUseCase @Inject constructor(
         override fun getQuery(): String = QUERY
         override fun getTopOperationName(): String = OPERATION_NAME
     }
+
+    suspend fun execute(): List<TabMetadata> {
+        val request = buildRequest()
+        val response = repository.response(listOf(request))
+        val data = response.getSuccessData<GetFlashSaleListForSellerMetaResponse>()
+        return mapper.map(data)
+    }
+
+    private fun buildRequest(): GraphqlRequest {
+        val requestHeader = CampaignParticipationRequestHeader(usecase = "campaign_list")
+        val payload = GetFlashSaleListForSellerMetaRequest(requestHeader)
+        val params = mapOf(REQUEST_PARAM_KEY to payload)
+
+        return GraphqlRequest(
+            query,
+            GetFlashSaleListForSellerMetaResponse::class.java,
+            params
+        )
+    }
+
 }
