331dcccddf40a0f8764f17663c0dbdab3f23d4eb
saidfaisal <saidfaisal777@gmail.com>
2022-08-12 00:40:27 +0700

    create search ingredient bottomsheet

---
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/recipesearch/searchingredient/presentation/viewholder/RecipeSearchIngredientViewHolder.kt
@@ -0,0 +1,42 @@
+package com.tokopedia.tokopedianow.recipesearch.searchingredient.presentation.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.tokopedianow.databinding.ItemTokopedianowRecipeSearchIngredientBinding
+import com.tokopedia.tokopedianow.recipesearch.searchingredient.presentation.uimodel.RecipeSearchIngredientUiModel
+
+class RecipeSearchIngredientViewHolder(
+    private val binding: ItemTokopedianowRecipeSearchIngredientBinding,
+    private val listener: RecipeSearchIngredientListener? = null
+): RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+        private const val ALL_CORNER_SIZES = 20f
+    }
+
+    fun bind(model: RecipeSearchIngredientUiModel) {
+        binding.apply {
+            tpTitle.text = model.title
+            cbOption.isChecked = model.isChecked
+            sivPic.loadImage(model.imgUrl)
+            sivPic.shapeAppearanceModel = sivPic.shapeAppearanceModel
+                .toBuilder()
+                .setAllCornerSizes(ALL_CORNER_SIZES)
+                .build()
+            root.setOnClickListener {
+                val isChecked = !cbOption.isChecked
+                cbOption.isChecked = isChecked
+                listener?.onCheckRecipe(isChecked)
+            }
+            cbOption.setOnClickListener {
+                val isChecked = !cbOption.isChecked
+                listener?.onCheckRecipe(isChecked)
+            }
+        }
+    }
+
+    interface RecipeSearchIngredientListener {
+        fun onCheckRecipe(isChecked: Boolean)
+    }
+
+}
