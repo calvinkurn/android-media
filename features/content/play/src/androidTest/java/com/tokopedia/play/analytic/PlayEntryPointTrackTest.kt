3eeebf6b36da323058323498a90f97903abbd06a
astidhiyaa <asti.anzaria@tokopedia.com>
2022-08-12 10:43:14 +0800

    `fix` assert

--- features/content/play/src/androidTest/java/com/tokopedia/play/analytic/PlayEntryPointTrackTest.kt
+++ features/content/play/src/androidTest/java/com/tokopedia/play/analytic/PlayEntryPointTrackTest.kt
@@ -1,7 +1,12 @@
 package com.tokopedia.play.analytic
 
+import android.content.Intent
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.play.PLAY_KEY_SOURCE_ID
+import com.tokopedia.play.PLAY_KEY_SOURCE_TYPE
 import com.tokopedia.play.view.activity.PlayActivity
+import com.tokopedia.play.view.type.PlaySource
 import org.junit.Rule
 import org.junit.Test
 
@@ -9,11 +14,25 @@ import org.junit.Test
  * @author by astidhiyaa on 11/08/22
  */
 class PlayEntryPointTrackTest {
+    private val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    private val playSource: PlaySource = PlaySource(id = "112", type = "FEEDS")
+    private val channelId: String = "12665"
+
     @get:Rule
-    val intentsTestRule = IntentsTestRule(PlayActivity::class.java, false, false)
+    val activityRule : IntentsTestRule<PlayActivity> = object : IntentsTestRule<PlayActivity>(PlayActivity::class.java) {
+        override fun getActivityIntent(): Intent {
+            return Intent(targetContext, PlayActivity::class.java).apply {
+                putExtra(PLAY_KEY_SOURCE_TYPE, playSource.type)
+                putExtra(PLAY_KEY_SOURCE_ID, playSource.id)
+            }
+        }
+    }
 
     @Test
     fun validate() {
-
+        val extra = activityRule.activity.intent.extras
+        assert(extra?.get(PLAY_KEY_SOURCE_ID) == playSource.id)
+        assert(extra?.get(PLAY_KEY_SOURCE_TYPE) == playSource.type)
     }
 }
