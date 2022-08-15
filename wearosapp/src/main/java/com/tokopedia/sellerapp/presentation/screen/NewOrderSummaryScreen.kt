0d042d42a68fd82e80ca86689a1536ad42cc1c23
CJ <calvyn.julian@tokopedia.com>
2022-08-15 17:10:44 +0700

    change to typography

--- wearosapp/src/main/java/com/tokopedia/sellerapp/presentation/screen/NewOrderSummaryScreen.kt
+++ wearosapp/src/main/java/com/tokopedia/sellerapp/presentation/screen/NewOrderSummaryScreen.kt
@@ -1,24 +1,20 @@
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.wear.compose.material.*
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
 import com.tokopedia.sellerapp.presentation.theme.NestDarkN0
 import com.tokopedia.sellerapp.presentation.theme.NestLightBlue
-import com.tokopedia.sellerapp.presentation.theme.NestLightG500
-import com.tokopedia.sellerapp.presentation.theme.NestLightN0
 import com.tokopedia.tkpd.R
+import com.tokopedia.unifyprinciples.Typography
+import com.tokopedia.unifyprinciples.Typography.Companion.DISPLAY_1
 
 @Composable
 fun NewOrderSummaryScreen() {
@@ -53,19 +49,17 @@ fun CreateOpenOrder() {
     Button(
         modifier = Modifier
             .height(32.dp)
-            .padding(PaddingValues(top = 4.dp)),
-        onClick = {redirectToNewOrderListScreen()},
+            .padding(PaddingValues(top = 6.dp)),
+        onClick = { redirectToNewOrderListScreen() },
         colors = ButtonDefaults.buttonColors(backgroundColor = NestLightBlue)
     ) {
-        Text(
-            text = stringResource(id = R.string.new_order_summary_text_open_order),
-            fontSize = 14.sp,
-            color = Color.Black,
-            fontWeight = FontWeight.Normal,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(PaddingValues(12.dp, 0.dp, 12.dp, 0.dp)),
-        )
+        AndroidView(factory = {
+            Typography(it).apply {
+                text = it.getString(R.string.new_order_summary_text_open_order)
+                setTextColor(it.getColor(com.tokopedia.unifyprinciples.R.color.Unify_N0))
+                fontType = DISPLAY_1
+            }
+        })
     }
 }
 
@@ -75,44 +69,51 @@ fun redirectToNewOrderListScreen() {
 
 @Composable
 fun CreateOrderPotential(orderPotential: String) {
-    Text(
-        text = buildAnnotatedString {
-            withStyle(style = SpanStyle(color = Color.White)) {
-                append(stringResource(id = R.string.new_order_summary_text_potency))
-            }
-            withStyle(style = SpanStyle(color = NestLightG500)) {
-                append(stringResource(id = R.string.new_order_summary_text_potency_format, orderPotential))
-            }
-        },
-        color = NestLightN0,
-        fontSize = 14.sp,
-        overflow = TextOverflow.Ellipsis,
-        fontWeight = FontWeight.Normal,
-        maxLines = 1
-    )
+    AndroidView(factory = {
+        Typography(it).apply {
+            val textLabelPotency = it.getString(R.string.new_order_summary_text_potency)
+            val textTotalPotency = String.format(
+                it.getString(R.string.new_order_summary_text_potency_format),
+                orderPotential
+            )
+            setTextColor(it.getColor(com.tokopedia.unifyprinciples.R.color.Unify_N0))
+            val textPotencySpannable = SpannableString("$textLabelPotency $textTotalPotency")
+            textPotencySpannable.setSpan(
+                ForegroundColorSpan(it.getColor(com.tokopedia.unifyprinciples.R.color.Unify_G500)),
+                textPotencySpannable.indexOf(textTotalPotency),
+                textPotencySpannable.indexOf(textTotalPotency) + textTotalPotency.length,
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+            )
+            text = textPotencySpannable
+            fontType = DISPLAY_1
+        }
+    })
 }
 
 @Composable
 fun CreateOrderQuantity(totalOrder: Int) {
-    Text(
-        text = totalOrder.toString(),
-        color = NestLightN0,
-        fontSize = 30.sp,
-        fontWeight = FontWeight.Normal,
+    AndroidView(
         modifier = Modifier.padding(
             paddingValues = PaddingValues(
                 vertical = 8.dp
             )
-        )
-    )
+        ),
+        factory = {
+            Typography(it).apply {
+                setTextColor(it.getColor(com.tokopedia.unifyprinciples.R.color.Unify_N0))
+                text = totalOrder.toString()
+                textSize = 30.0f
+            }
+        })
 }
 
 @Composable
 private fun CreateOrderTitle() {
-    Text(
-        text = stringResource(id = R.string.new_order_summary_text_title),
-        color = NestLightN0,
-        fontSize = 14.sp,
-        fontWeight = FontWeight.Normal,
-    )
+    AndroidView(
+        factory = {
+            Typography(it).apply {
+                setTextColor(it.getColor(com.tokopedia.unifyprinciples.R.color.Unify_N0))
+                text = it.getString(R.string.new_order_summary_text_title)
+            }
+        })
 }
