33464b7ad201a82515b23fca591ecbe0bbf89af4
firmankagami <firmanda.nugroho@tokopedia.com>
2022-08-16 11:13:19 +0700

    [feat] add feature copy in applink debugger

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/ui/fragment/ApplinkDebuggerDetailFragment.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/ui/fragment/ApplinkDebuggerDetailFragment.kt
@@ -1,22 +1,29 @@
 package com.tokopedia.analyticsdebugger.debugger.ui.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 
 import com.tokopedia.abstraction.base.view.fragment.TkpdBaseV4Fragment
 import com.tokopedia.analyticsdebugger.R
 import com.tokopedia.analyticsdebugger.debugger.ui.model.ApplinkDebuggerViewModel
 
 import com.tokopedia.analyticsdebugger.debugger.AnalyticsDebuggerConst.DATA_DETAIL
+import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.unifycomponents.Toaster
 
 class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
     private var textApplink: TextView? = null
     private var textTimestamp: TextView? = null
     private var textTraces: TextView? = null
+    private var iconMainApplink: IconUnify? = null
     private var viewModel: ApplinkDebuggerViewModel? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -24,6 +31,7 @@ class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
         textApplink = view.findViewById(R.id.applink_text_name)
         textTimestamp = view.findViewById(R.id.applink_text_timestamp)
         textTraces = view.findViewById(R.id.applink_text_traces)
+        iconMainApplink = view.findViewById(R.id.icon_copy_main_applink)
 
         return view
     }
@@ -34,6 +42,11 @@ class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
             textApplink!!.text = viewModel!!.applink
             textTimestamp!!.text = viewModel!!.timestamp
             textTraces!!.text = viewModel!!.trace
+            iconMainApplink?.setOnClickListener {
+                viewModel?.let {
+                    copyToClipBoard(it.applink)
+                }
+            }
         }
     }
 
@@ -41,7 +54,25 @@ class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
         return ApplinkDebuggerDetailFragment::class.java.simpleName
     }
 
+    private fun copyToClipBoard(applink: String?) {
+        activity?.let {
+            val clipboard = it.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText(
+                CLIP_DATA_APPLINK, applink
+            )
+
+            clipboard.setPrimaryClip(clip)
+            view?.run {
+                Toaster.build(this,
+                    getString(com.tokopedia.analyticsdebugger.R.string.applink_already_copied),
+                    Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+
     companion object {
+
+        const val CLIP_DATA_APPLINK = "clip_data_applink"
 
         fun newInstance(extras: Bundle): Fragment {
             val fragment = ApplinkDebuggerDetailFragment()
