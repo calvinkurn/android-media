b60fb830d83e71c6d9c38744356943cb8ff57587
firmankagami <firmanda.nugroho@tokopedia.com>
2022-08-16 12:21:27 +0700

    [feat] add feature copy mapped applink debugger

--- libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/ui/fragment/ApplinkDebuggerDetailFragment.kt
+++ libraries/analytics_debugger/src/main/java/com/tokopedia/analyticsdebugger/debugger/ui/fragment/ApplinkDebuggerDetailFragment.kt
@@ -17,21 +17,32 @@ import com.tokopedia.analyticsdebugger.debugger.ui.model.ApplinkDebuggerViewMode
 
 import com.tokopedia.analyticsdebugger.debugger.AnalyticsDebuggerConst.DATA_DETAIL
 import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.unifycomponents.Toaster
 
 class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
     private var textApplink: TextView? = null
+    private var textMappedDeeplink: TextView? = null
+    private var textTitleMappedDeeplink: TextView? = null
     private var textTimestamp: TextView? = null
     private var textTraces: TextView? = null
-    private var iconMainApplink: IconUnify? = null
+    private var iconApplink: IconUnify? = null
+    private var iconMappedDeeplink: IconUnify? = null
     private var viewModel: ApplinkDebuggerViewModel? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_applink_debugger_detail, container, false)
         textApplink = view.findViewById(R.id.applink_text_name)
+        textMappedDeeplink = view.findViewById(R.id.mapped_deeplink)
+        textTitleMappedDeeplink= view.findViewById(R.id.title_mapped_deeplink)
         textTimestamp = view.findViewById(R.id.applink_text_timestamp)
         textTraces = view.findViewById(R.id.applink_text_traces)
-        iconMainApplink = view.findViewById(R.id.icon_copy_main_applink)
+        iconApplink = view.findViewById(R.id.icon_copy_main_applink)
+        iconMappedDeeplink = view.findViewById(R.id.icon_copy_mapped_deeplink)
 
         return view
     }
@@ -42,10 +53,26 @@ class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
             textApplink!!.text = viewModel!!.applink
             textTimestamp!!.text = viewModel!!.timestamp
             textTraces!!.text = viewModel!!.trace
-            iconMainApplink?.setOnClickListener {
+
+            val mappedDeeplink = getMappedDeeplink(viewModel!!.trace)
+            textMappedDeeplink?.let { view ->
+                if (!mappedDeeplink.isNullOrEmpty()) {
+                    view.text = mappedDeeplink
+                } else {
+                    iconMappedDeeplink?.hide()
+                    textMappedDeeplink?.hide()
+                    textTitleMappedDeeplink?.hide()
+                }
+            }
+
+            iconApplink?.setOnClickListener {
                 viewModel?.let {
                     copyToClipBoard(it.applink)
                 }
+            }
+
+            iconMappedDeeplink?.setOnClickListener {
+                copyToClipBoard(mappedDeeplink)
             }
         }
     }
@@ -55,24 +82,51 @@ class ApplinkDebuggerDetailFragment : TkpdBaseV4Fragment() {
     }
 
     private fun copyToClipBoard(applink: String?) {
-        activity?.let {
-            val clipboard = it.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText(
-                CLIP_DATA_APPLINK, applink
-            )
+        if (!applink.isNullOrEmpty()) {
+            activity?.let {
+                val clipboard = it.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText(
+                    CLIP_DATA_APPLINK, applink
+                )
 
-            clipboard.setPrimaryClip(clip)
+                clipboard.setPrimaryClip(clip)
+                view?.run {
+                    Toaster.build(
+                        this,
+                        getString(com.tokopedia.analyticsdebugger.R.string.applink_already_copied),
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+        } else {
             view?.run {
-                Toaster.build(this,
-                    getString(com.tokopedia.analyticsdebugger.R.string.applink_already_copied),
-                    Snackbar.LENGTH_LONG).show()
+                Toaster.build(
+                    this,
+                    getString(com.tokopedia.analyticsdebugger.R.string.applink_not_copied),
+                    Snackbar.LENGTH_LONG,
+                    type = Toaster.TYPE_ERROR
+                ).show()
             }
         }
     }
 
+    private fun getMappedDeeplink(trace: String?): String? {
+        var mappedApplink: String? = null
+        try {
+            mappedApplink = trace?.substringAfterLast(MAPPED_APPLINK_DIVIDER_START)?.substringBeforeLast(
+                MAPPED_APPLINK_DIVIDER_END)
+        } catch (e: Exception) {
+            mappedApplink = null
+        }
+
+        return mappedApplink
+    }
+
     companion object {
 
-        const val CLIP_DATA_APPLINK = "clip_data_applink"
+        private const val CLIP_DATA_APPLINK = "clip_data_applink"
+        private const val MAPPED_APPLINK_DIVIDER_START = "Mapped Deeplink:\n"
+        private const val MAPPED_APPLINK_DIVIDER_END = "\n\nDF Deeplink:"
 
         fun newInstance(extras: Bundle): Fragment {
             val fragment = ApplinkDebuggerDetailFragment()
