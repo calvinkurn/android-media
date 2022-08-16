a1ddc45561eb141a9808ab56069ee6854cdcb39f
faisalramd <faisal.ramdani@tokopedia.com>
2022-08-17 01:43:33 +0700

    fix danger

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/productlimitation/presentation/dialog/ProductLimitationBottomSheet.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/productlimitation/presentation/dialog/ProductLimitationBottomSheet.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.device.info.DeviceScreenInfo
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.orFalse
+import com.tokopedia.kotlin.extensions.view.orZero
 import com.tokopedia.product.addedit.R
 import com.tokopedia.product.addedit.common.customview.TabletAdaptiveBottomSheet
 import com.tokopedia.product.addedit.common.util.HorizontalItemDecoration
@@ -85,9 +86,10 @@ class ProductLimitationBottomSheet(
         val ticker = contentView?.findViewById<Ticker>(R.id.ticker_product_limitation)
         val iconTitle = contentView?.findViewById<IconUnify>(R.id.icon_product_limitation)
         val tvTitle = contentView?.findViewById<TextView>(R.id.tv_title_product_limitation)
+        val itemDecorPadding = context?.resources?.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl3)
 
         rvItems?.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        rvItems?.addItemDecoration(HorizontalItemDecoration(resources.getDimensionPixelSize(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl3)))
+        rvItems?.addItemDecoration(HorizontalItemDecoration(itemDecorPadding.orZero()))
         rvItems?.adapter = ProductLimitationItemAdapter().apply {
             setData(actionItems)
             setOnItemClick { category, title: String, url: String ->
