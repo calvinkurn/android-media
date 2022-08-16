ca143fdb430b09a4af5a03bc13ff9997f147da70
firmankagami <firmanda.nugroho@tokopedia.com>
2022-08-16 17:07:32 +0700

    [fix] fix emoney category id

--- features/recharge/recharge_pdp_emoney/src/test/java/com/tokopedia/recharge_pdp_emoney/presentation/viewmodel/EmoneyPdpViewModelTest.kt
+++ features/recharge/recharge_pdp_emoney/src/test/java/com/tokopedia/recharge_pdp_emoney/presentation/viewmodel/EmoneyPdpViewModelTest.kt
@@ -223,10 +223,11 @@ class EmoneyPdpViewModelTest {
         val productId = "1222"
         val operatorId = "14141414"
         val userId = "12345"
+        val categoryId = "34"
         coEvery { userSession.userId } coAnswers { userId }
 
         //when
-        emoneyPdpViewModel.generateCheckoutPassData(copiedPromo, clientNumber, productId, operatorId)
+        emoneyPdpViewModel.generateCheckoutPassData(copiedPromo, clientNumber, productId, operatorId, categoryId)
 
         //then
         assert(emoneyPdpViewModel.digitalCheckoutPassData.idemPotencyKey?.isNotEmpty() ?: false)
@@ -243,12 +244,13 @@ class EmoneyPdpViewModelTest {
         val copiedPromo = "QATOPED"
         val clientNumber = "1234566789"
         val userId = "12345"
+        val categoryId = "34"
         coEvery { userSession.userId } coAnswers { userId }
         getSelectedOperator_onInputNumberValid_shouldUpdateSelectedOperator()
         setSelectedProduct_shouldUpdateSelectedProduct()
 
         //when
-        emoneyPdpViewModel.generateCheckoutPassData(copiedPromo, clientNumber)
+        emoneyPdpViewModel.generateCheckoutPassData(copiedPromo, clientNumber, categoryIdFromPDP = categoryId)
 
         //then
         assert(emoneyPdpViewModel.digitalCheckoutPassData.idemPotencyKey?.isNotEmpty() ?: false)
