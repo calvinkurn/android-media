87afbef14f805fa833d65d5188d1503c3fba9828
deepesh tiwari <deepesh.tiwari@tokopedia.com>
2022-08-18 12:01:34 +0530

    removed aff_trx_history usage

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/AffiliateConstants.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/AffiliateConstants.kt
@@ -38,8 +38,6 @@ const val AFFILIATE_SPLASH_TIME = 4000L
 
 const val AFFILIATE_WALLET_TRANSACTION_ENABLE = "app_affiliate_wallet_transaction_enable"
 
-const val AFFILIATE_TRX_ENABLED = "aff_trx_history"
-
 const val AFFILIATE_WITHDRAWAL = "Aff_withdrawal"
 
 const val AFFILIATE_TRAFFIC_ADB = "traffic_att_adp"
--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/custom/AffiliateBottomNavbar.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/custom/AffiliateBottomNavbar.kt
@@ -3,15 +3,13 @@ package com.tokopedia.affiliate.ui.custom
 import android.content.Context
 import com.tokopedia.affiliate_toko.R
 import com.tokopedia.affiliate.ui.activity.AffiliateActivity
-import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 
 class AffiliateBottomNavbar(private val bottomNavigation: LottieBottomNavbar?,
                             private val menuListener : IBottomClickListener,
-                            private val context : Context,
-                            private val isAffiliateWalletEnabled : Boolean) {
+                            private val context : Context) {
 
-    var menu: ArrayList<BottomMenu> = ArrayList()
+    private var menu: ArrayList<BottomMenu> = ArrayList()
     private val isNewNavigation = false
 
     fun populateBottomNavigationView(){
@@ -19,10 +17,8 @@ class AffiliateBottomNavbar(private val bottomNavigation: LottieBottomNavbar?,
                 com.tokopedia.unifyprinciples.R.color.Unify_G500,com.tokopedia.unifyprinciples.R.color.Unify_NN600, true, 1f, 3f))
         menu.add(BottomMenu(R.id.menu_promo_affiliate, context.resources.getString(R.string.affiliate_promo), null, null, R.drawable.ic_bottom_nav_promo_active_affiliate, R.drawable.ic_bottom_nav_promo_inactive_affiliate,
                 com.tokopedia.unifyprinciples.R.color.Unify_G500,com.tokopedia.unifyprinciples.R.color.Unify_NN600, true, 1f, 3f))
-        if(isAffiliateWalletEnabled) {
-            menu.add(BottomMenu(R.id.menu_withdrawal_affiliate, context.resources.getString(R.string.affiliate_withdrawal), null, null, R.drawable.ic_bottom_nav_finance_active_affiliate, R.drawable.ic_bottom_nav_finance_inactive_affiliate,
+        menu.add(BottomMenu(R.id.menu_withdrawal_affiliate, context.resources.getString(R.string.affiliate_withdrawal), null, null, R.drawable.ic_bottom_nav_finance_active_affiliate, R.drawable.ic_bottom_nav_finance_inactive_affiliate,
                     com.tokopedia.unifyprinciples.R.color.Unify_G500, com.tokopedia.unifyprinciples.R.color.Unify_NN600, true, 1f, 3f))
-        }
         menu.add(BottomMenu(R.id.menu_help_affiliate, context.resources.getString(R.string.affiliate_bantuan), null , null , R.drawable.ic_bottom_nav_help_active_affiliate, R.drawable.ic_bottom_nav_help_inactive_affiliate,
                 com.tokopedia.unifyprinciples.R.color.Unify_G500,com.tokopedia.unifyprinciples.R.color.Unify_NN600, true, 1f, 3f))
         bottomNavigation?.setMenu(menu, isNewNavigation)
@@ -36,10 +32,6 @@ class AffiliateBottomNavbar(private val bottomNavigation: LottieBottomNavbar?,
 
     fun showBottomNav() {
         bottomNavigation?.show()
-    }
-
-    fun hideBottomNav(){
-        bottomNavigation?.hide()
     }
 
     fun selectBottomTab(position : Int){
