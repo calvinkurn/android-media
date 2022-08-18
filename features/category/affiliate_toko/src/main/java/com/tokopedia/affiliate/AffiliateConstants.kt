e06b1cef3c8fcfa6791df828a7784b37aaa55427
deepesh tiwari <deepesh.tiwari@tokopedia.com>
2022-08-18 15:50:51 +0530

    removed traffic_att_adp usage

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/AffiliateConstants.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/AffiliateConstants.kt
@@ -38,9 +38,6 @@ const val AFFILIATE_SPLASH_TIME = 4000L
 
 const val AFFILIATE_WALLET_TRANSACTION_ENABLE = "app_affiliate_wallet_transaction_enable"
 
-
-const val AFFILIATE_TRAFFIC_ADB = "traffic_att_adp"
-
 const val DEFAULT_VALUE = false
 
 const val PROJECT_ID = 20
