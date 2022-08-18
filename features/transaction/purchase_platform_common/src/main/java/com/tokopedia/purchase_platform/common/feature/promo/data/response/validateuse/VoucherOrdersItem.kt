886f334d912036ab04d244efa0fa18ccbf236adc
hiradevina <fakhira.devina@tokopedia.com>
2022-08-18 10:40:27 +0700

    remove unused changes

--- features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/promo/data/response/validateuse/VoucherOrdersItem.kt
+++ features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/promo/data/response/validateuse/VoucherOrdersItem.kt
@@ -11,12 +11,6 @@ data class VoucherOrdersItem(
         @field:SerializedName("unique_id")
         val uniqueId: String = "",
 
-        @field:SerializedName("title_description")
-        val titleDescription: String = "",
-
-        @field:SerializedName("is_po")
-        val isPo: Int = 0,
-
         @SuppressLint("Invalid Data Type")
         @field:SerializedName("sp_id")
         val spId: Int = 0,
@@ -24,6 +18,12 @@ data class VoucherOrdersItem(
         @SuppressLint("Invalid Data Type")
         @field:SerializedName("shipping_id")
         val shippingId: Int = 0,
+
+        @field:SerializedName("title_description")
+        val titleDescription: String = "",
+
+        @field:SerializedName("is_po")
+        val isPo: Int = 0,
 
         @field:SerializedName("type")
         val type: String = "",
