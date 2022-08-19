48930a13bdb2ea3514139aed2b084bc0bfee4b1c
astidhiyaa <asti.anzaria@tokopedia.com>
2022-08-19 21:32:36 +0800

    `fix` remove unused mapper - PLAY

--- features/content/play_common/src/main/java/com/tokopedia/play_common/model/mapper/PlayInteractiveLeaderboardMapper.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/model/mapper/PlayInteractiveLeaderboardMapper.kt
@@ -12,15 +12,6 @@ import javax.inject.Inject
  * Created by jegul on 02/07/21
  */
 class PlayInteractiveLeaderboardMapper @Inject constructor(private val decodeHtml: HtmlTextTransformer) {
-
-    /***
-     * New leaderboard
-     */
-    fun mapNewLeaderboard(response: GetLeaderboardSlotResponse, isChatAllowed: () -> Boolean) =
-        PlayLeaderboardInfoUiModel(
-            leaderboardWinners = mapNewLeaderboardInteractive(response.data.slots, isChatAllowed),
-        )
-
     @ExperimentalStdlibApi
     fun mapNewPlayLeaderboard(
         response: GetLeaderboardSlotResponse,
@@ -64,26 +55,6 @@ class PlayInteractiveLeaderboardMapper @Inject constructor(private val decodeHtm
         }
     }
 
-    private fun mapNewLeaderboardInteractive(
-        leaderboardsResponse: List<GetLeaderboardSlotResponse.SlotData>,
-        isChatAllowed: () -> Boolean
-    ): List<PlayLeaderboardUiModel> = leaderboardsResponse.map {
-        PlayLeaderboardUiModel(
-            title = if (getLeaderboardType(it) == LeadeboardType.Quiz) decodeHtml.transform(it.question) else it.title,
-            winners = mapInteractiveWinner(
-                title = it.title,
-                winnersResponse = it.winner,
-                isChatAllowed = isChatAllowed
-            ),
-            otherParticipantText = it.otherParticipantCountText,
-            otherParticipant = it.otherParticipantCount.toLong(),
-            choices = mapChoicesOld(it.choices, it.userChoice),
-            leaderBoardType = getLeaderboardType(it),
-            emptyLeaderBoardCopyText = it.emptyLeaderboardCopyText,
-            reward = decodeHtml.transform(it.reward),
-            id = it.interactiveId
-        )
-    }
 
     /***
      * Change to typename to make sure
@@ -96,7 +67,7 @@ class PlayInteractiveLeaderboardMapper @Inject constructor(private val decodeHtm
         }
     }
 
-    fun mapChoices(
+    private fun mapChoices(
         choices: List<QuizResponse.Choice>,
         userPicksId: String
     ): List<LeaderboardGameUiModel.QuizOption> {
@@ -116,70 +87,6 @@ class PlayInteractiveLeaderboardMapper @Inject constructor(private val decodeHtm
         }
     }
 
-
-    fun mapChoicesOld(
-        choices: List<QuizResponse.Choice>,
-        userPicksId: String
-    ): List<QuizChoicesUiModel> {
-        return choices.mapIndexed { index, item: QuizResponse.Choice ->
-            QuizChoicesUiModel(
-                index,
-                item.id,
-                decodeHtml.transform(item.text),
-                if (item.id == userPicksId)
-                    PlayQuizOptionState.Answered(isCorrect = item.isCorrect ?: false)
-                else
-                    PlayQuizOptionState.Other(isCorrect = item.isCorrect ?: false)
-            )
-        }
-    }
-
-    /***
-     * Old leaderboard
-     */
-    fun mapLeaderboard(response: GetInteractiveLeaderboardResponse, isChatAllowed: () -> Boolean) =
-        PlayLeaderboardInfoUiModel(
-            leaderboardWinners = mapLeaderboardWinner(
-                response.data.data,
-                response.data.config.topChatMessage,
-                isChatAllowed
-            ),
-            totalParticipant = response.data.summary.totalParticipant.toString(),
-            config = mapLeaderboardConfig(response),
-        )
-
-    private fun mapLeaderboardWinner(
-        leaderboardsResponse: List<GetInteractiveLeaderboardResponse.Data>,
-        topChatResponseRaw: String,
-        isChatAllowed: () -> Boolean
-    ): List<PlayLeaderboardUiModel> = leaderboardsResponse.map {
-        PlayLeaderboardUiModel(
-            title = it.title,
-            winners = mapInteractiveWinner(it.title, it.winner, topChatResponseRaw, isChatAllowed),
-            otherParticipantText = it.otherParticipantCountText,
-            otherParticipant = it.otherParticipantCount.toLong(),
-            id = "" //temp for old leaderboard, removed if bro doesnt use it anymore
-        )
-    }
-
-    private fun mapInteractiveWinner(
-        title: String,
-        winnersResponse: List<GetInteractiveLeaderboardResponse.Winner>,
-        topChatResponseRaw: String = "",
-        isChatAllowed: () -> Boolean,
-    ) = winnersResponse.mapIndexed { index, winner ->
-        PlayWinnerUiModel(
-            rank = index + 1,
-            id = winner.userID.toString(),
-            name = winner.userName,
-            imageUrl = winner.imageUrl,
-            allowChat = isChatAllowed,
-            topChatMessage = topChatResponseRaw
-                .replace(FORMAT_FIRST_NAME, winner.userName)
-                .replace(FORMAT_TITLE, title)
-        )
-    }
-
     private fun mapInteractiveWinnerNew(
         title: String,
         winnersResponse: List<GetInteractiveLeaderboardResponse.Winner>,
@@ -195,27 +102,6 @@ class PlayInteractiveLeaderboardMapper @Inject constructor(private val decodeHtm
             topChatMessage = topChatResponseRaw
                 .replace(FORMAT_FIRST_NAME, winner.userName)
                 .replace(FORMAT_TITLE, title)
-        )
-    }
-
-    private fun mapLeaderboardConfig(response: GetInteractiveLeaderboardResponse): PlayLeaderboardConfigUiModel {
-        val configResponse = response.data.config
-        val currentInteractive = response.data.data.firstOrNull()
-        val currentInteractiveWinner = currentInteractive?.winner?.firstOrNull()
-        val winnerName = currentInteractiveWinner?.userName.orEmpty()
-
-        return PlayLeaderboardConfigUiModel(
-            sellerMessage = configResponse.sellerMessage
-                .replace(FORMAT_WINNER_NAME, winnerName)
-                .replace(
-                    FORMAT_TOTAL_PARTICIPANT,
-                    response.data.summary.totalParticipant.toString()
-                ),
-            winnerMessage = configResponse.winnerMessage,
-            winnerDetail = configResponse.winnerDetail,
-            loserMessage = configResponse.loserMessage
-                .replace(FORMAT_WINNER_NAME, winnerName),
-            loserDetail = configResponse.loserDetail
         )
     }
 
