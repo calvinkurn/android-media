6078e110d63f5c9ea73fcb8a1ec1053e4bea141d
hiradevina <fakhira.devina@tokopedia.com>
2022-08-19 11:17:40 +0700

    fix conflict

--- features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/promo/data/response/validateuse/VoucherOrdersItem.kt
+++ features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/promo/data/response/validateuse/VoucherOrdersItem.kt
@@ -25,14 +25,6 @@ data class VoucherOrdersItem(
         @field:SerializedName("is_po")
         val isPo: Int = 0,
 
-        @SuppressLint("Invalid Data Type")
-        @field:SerializedName("sp_id")
-        val spId: Int = 0,
-
-        @SuppressLint("Invalid Data Type")
-        @field:SerializedName("shipping_id")
-        val shippingId: Int = 0,
-
         @field:SerializedName("type")
         val type: String = "",
 
