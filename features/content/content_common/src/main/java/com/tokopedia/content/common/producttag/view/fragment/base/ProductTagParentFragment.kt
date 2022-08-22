360bacf3e1f48dbe3e48cc1965bd992e1f42627b
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-08-22 16:06:34 +0700

    `fix` product tag backstack

--- features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/fragment/base/ProductTagParentFragment.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/fragment/base/ProductTagParentFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -274,7 +275,13 @@ class ProductTagParentFragment @Inject constructor(
                 .commit()
         }
         else {
-            childFragmentManager.popBackStack()
+            repeat(currStack.size - prevStack.size) {
+                childFragmentManager.popBackStack()
+            }
+
+            childFragmentManager.beginTransaction()
+                .replace(binding.flCcProductTagContainer.id, fragment, tag)
+                .commit()
         }
     }
 
