1ef46686a958cce2ec990fe3732077682dfb4234
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-08-22 17:55:24 +0700

    feat(shipment_info) bottom sheet of shipment content adjustment

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/util/ProductSeparatorItemDecoration.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/util/ProductSeparatorItemDecoration.kt
@@ -5,6 +5,5 @@ import android.content.Context
 /**
  * Created by Yehezkiel on 26/01/21
  */
-class ProductSeparatorItemDecoration(context: Context) : com.tokopedia.abstraction.base.view.widget.DividerItemDecoration(context) {
-    override fun getDimenPaddingLeft(): Int = com.tokopedia.unifyprinciples.R.dimen.unify_space_0
-}
+class ProductSeparatorItemDecoration(context: Context) :
+    com.tokopedia.abstraction.base.view.widget.DividerItemDecoration(context)
