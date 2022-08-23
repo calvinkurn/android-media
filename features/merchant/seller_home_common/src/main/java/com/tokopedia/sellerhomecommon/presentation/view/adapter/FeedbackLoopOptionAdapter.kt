cc47ce3ed6e2990d5b18eafb4af90633c3244c0e
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-08-23 13:59:40 +0800

    add feedback option bottom sheet

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/adapter/FeedbackLoopOptionAdapter.kt
@@ -0,0 +1,92 @@
+package com.tokopedia.sellerhomecommon.presentation.view.adapter
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.EMPTY
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.sellerhomecommon.databinding.ShcItemFeedbackLoopOptionBinding
+import com.tokopedia.sellerhomecommon.presentation.model.FeedbackLoopOptionUiModel
+
+/**
+ * Created by @ilhamsuaib on 22/08/22.
+ */
+
+class FeedbackLoopOptionAdapter(
+    private val items: List<FeedbackLoopOptionUiModel>,
+    private val onCheckedListener: (FeedbackLoopOptionUiModel) -> Unit
+) : RecyclerView.Adapter<FeedbackLoopOptionAdapter.ViewHolder>() {
+
+    companion object {
+        private const val MAX_CHAR = 250
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ShcItemFeedbackLoopOptionBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return ViewHolder(binding, onCheckedListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    inner class ViewHolder(
+        private val binding: ShcItemFeedbackLoopOptionBinding,
+        private val onCheckedListener: (FeedbackLoopOptionUiModel) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: FeedbackLoopOptionUiModel) = binding.run {
+            cbShcFeedbackOption.isChecked = item.isSelected
+            tvShcFeedbackOption.text = item.title
+
+            root.setOnClickListener {
+                item.isSelected = !item.isSelected
+                if (item is FeedbackLoopOptionUiModel.Other) {
+                    setOnOtherClicked(item)
+                }
+                onCheckedListener(item)
+            }
+            cbShcFeedbackOption.setOnCheckedChangeListener { _, _ ->
+                root.performClick()
+            }
+        }
+
+        private fun setOnOtherClicked(item: FeedbackLoopOptionUiModel.Other) {
+            binding.run {
+                if (item.isSelected) {
+                    edtShcFeedbackOption.visible()
+                    edtShcFeedbackOption.setCounter(MAX_CHAR)
+                    edtShcFeedbackOption.textFieldInput.addTextChangedListener(object :
+                        TextWatcher {
+                        override fun beforeTextChanged(
+                            p0: CharSequence?,
+                            p1: Int,
+                            p2: Int,
+                            p3: Int
+                        ) {
+                        }
+
+                        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        }
+
+                        override fun afterTextChanged(text: Editable) {
+                            item.value = text.toString()
+                            onCheckedListener(item)
+                        }
+                    })
+                } else {
+                    edtShcFeedbackOption.gone()
+                    item.value = String.EMPTY
+                }
+            }
+        }
+    }
+}
