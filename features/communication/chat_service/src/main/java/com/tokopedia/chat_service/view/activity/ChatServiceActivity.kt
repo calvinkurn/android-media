dfafac93003c9a88e015ff3fc69eb879f4a7428c
kelvinsaputra <kelvin.saputra@tokopedia.com>
2022-08-24 17:55:35 +0700

    Add proguard-rules for moshi & change artifactory variable name
    # Conflicts:
    #	customerapp/proguard-rules.txt
    #	customerapp_pro/proguard-rules.txt

--- features/communication/chat_service/src/main/java/com/tokopedia/chat_service/view/activity/ChatServiceActivity.kt
+++ features/communication/chat_service/src/main/java/com/tokopedia/chat_service/view/activity/ChatServiceActivity.kt
@@ -1,14 +1,19 @@
-package com.tokopedia.chat_service
+package com.tokopedia.chat_service.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gojek.conversations.logging.ConversationsLogger
+import com.tokopedia.chat_service.R
 
 class ChatServiceActivity : AppCompatActivity(), ConversationsLogger.ILog {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_service)
+    }
+
+    private fun initConversationRepository() {
+
     }
 
     override fun d(tag: String, message: String) {
