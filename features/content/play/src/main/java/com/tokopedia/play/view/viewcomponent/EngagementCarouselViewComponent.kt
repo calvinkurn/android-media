c7df049d327e7bd2f2197e0a9d1b76256f998546
astidhiyaa <asti.anzaria@tokopedia.com>
2022-08-24 16:50:58 +0800

    feat: add view for promo

--- features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/EngagementCarouselViewComponent.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewcomponent/EngagementCarouselViewComponent.kt
@@ -1,7 +1,21 @@
 package com.tokopedia.play.view.viewcomponent
 
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import com.tokopedia.play_common.viewcomponent.ViewComponent
+import kotlinx.coroutines.CoroutineScope
+
 /**
  * @author by astidhiyaa on 24/08/22
  */
-class EngagementCarouselViewComponent {
+class EngagementCarouselViewComponent(
+    container: ViewGroup,
+    @IdRes resId: Int,
+    private val listener: Listener,
+    private val scope: CoroutineScope
+) : ViewComponent(container, resId) {
+
+    interface Listener {
+
+    }
 }
