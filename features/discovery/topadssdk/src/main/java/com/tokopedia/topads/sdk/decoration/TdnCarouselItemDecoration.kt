3969eba65c79d0188aa145b1068e8517ff843283
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2022-08-24 11:03:07 +0530

    quick fix margins

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/decoration/TdnCarouselItemDecoration.kt
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/decoration/TdnCarouselItemDecoration.kt
@@ -3,6 +3,7 @@ package com.tokopedia.topads.sdk.decoration
 import android.graphics.Rect
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
+import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.toPx
 import com.tokopedia.topads.sdk.R
@@ -17,7 +18,10 @@ class TdnCarouselItemDecoration : RecyclerView.ItemDecoration() {
         if (parent.getChildAdapterPosition(view) == Int.ZERO) {
             outRect.left = view.context.resources.getDimensionPixelSize(R.dimen.sdk_dp_16)
         }
-
-        outRect.right = 8f.toPx().toInt()
+        if (parent.getChildAdapterPosition(view) == parent.adapter?.itemCount ?: Int.ZERO - Int.ONE) {
+            outRect.right = view.context.resources.getDimensionPixelSize(R.dimen.sdk_dp_16)
+        } else {
+            outRect.right = 8f.toPx().toInt()
+        }
     }
 }
