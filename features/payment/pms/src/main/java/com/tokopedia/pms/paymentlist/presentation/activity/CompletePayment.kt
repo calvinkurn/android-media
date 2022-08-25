d5bf5d0f7d680a3f3daca99c563217ae2513d927
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-08-26 00:37:34 +0530

    added the activity for complete payment

---
+++ features/payment/pms/src/main/java/com/tokopedia/pms/paymentlist/presentation/activity/CompletePayment.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.pms.paymentlist.presentation.activity
+
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+
+class CompletePayment : BaseSimpleActivity() {
+
+
+    override fun getNewFragment(): Fragment? {
+        TODO("Not yet implemented")
+    }
+}
