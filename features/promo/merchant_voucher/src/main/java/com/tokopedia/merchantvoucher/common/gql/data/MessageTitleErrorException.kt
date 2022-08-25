bc7458468e3277f0ad3441aa6daa35c726174477
Harshit Jain <harshit.jain@tokopedia.com>
2022-08-25 17:48:58 +0530

    Changed String type to nullable in MessageTitleErrorException.kt and changed imports in MerchantVoucherView.kt

--- features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/common/gql/data/MessageTitleErrorException.kt
+++ features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/common/gql/data/MessageTitleErrorException.kt
@@ -2,4 +2,4 @@ package com.tokopedia.merchantvoucher.common.gql.data
 
 import com.tokopedia.abstraction.common.network.exception.MessageErrorException
 
-class MessageTitleErrorException(val errorMessageTitle:String,message:String) : MessageErrorException(message)
+class MessageTitleErrorException(val errorMessageTitle:String?,message:String?) : MessageErrorException(message)
