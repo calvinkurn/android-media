c6e5ef9aa47189f7c90eac897fda1b1f3d2dd30d
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2022-08-25 17:51:09 +0700

    Improvment of importing

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/report/activity/ReportShopWebViewActivity.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/report/activity/ReportShopWebViewActivity.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.fragment.app.Fragment
 import com.tokopedia.shop.report.fragment.ReportStoreReportWebViewFragment
-import com.tokopedia.webview.*
+import com.tokopedia.webview.BaseSimpleWebViewActivity
+import com.tokopedia.webview.KEY_URL
+import com.tokopedia.webview.KEY_TITLEBAR
+import com.tokopedia.webview.KEY_ALLOW_OVERRIDE
+import com.tokopedia.webview.KEY_NEED_LOGIN
+import com.tokopedia.webview.KEY_TITLE
 import com.tokopedia.webview.ext.encodeOnce
 
 
@@ -17,18 +22,14 @@ class ReportShopWebViewActivity : BaseSimpleWebViewActivity() {
     companion object {
         fun getStartIntent(
             context: Context,
-            url: String,
-            showToolbar: Boolean = true,
-            allowOverride: Boolean = true,
-            needLogin: Boolean = false,
-            title: String = ""
+            url: String
         ): Intent {
             return Intent(context, ReportShopWebViewActivity::class.java).apply {
                 putExtra(KEY_URL, url.encodeOnce())
-                putExtra(KEY_TITLEBAR, showToolbar)
-                putExtra(KEY_ALLOW_OVERRIDE, allowOverride)
-                putExtra(KEY_NEED_LOGIN, needLogin)
-                putExtra(KEY_TITLE, title)
+                putExtra(KEY_TITLEBAR, true)
+                putExtra(KEY_ALLOW_OVERRIDE, true)
+                putExtra(KEY_NEED_LOGIN, true)
+                putExtra(KEY_TITLE, "")
             }
         }
     }
