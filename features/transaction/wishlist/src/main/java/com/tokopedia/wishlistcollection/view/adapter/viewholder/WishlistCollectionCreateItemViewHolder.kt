213356ef723dd27ee085a621fb752258b3e9251f
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-08-25 16:26:41 +0700

    fix layout collection items

--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/adapter/viewholder/WishlistCollectionCreateItemViewHolder.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/adapter/viewholder/WishlistCollectionCreateItemViewHolder.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.wishlistcollection.view.adapter.viewholder
 
-import android.widget.GridLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.media.loader.loadImage
 import com.tokopedia.media.loader.wrapper.MediaCacheStrategy
@@ -8,7 +7,6 @@ import com.tokopedia.wishlist.R
 import com.tokopedia.wishlistcollection.data.model.WishlistCollectionTypeLayoutData
 import com.tokopedia.wishlist.databinding.CollectionWishlistCreateItemBinding
 import com.tokopedia.wishlist.util.WishlistV2Consts.CREATE_NEW_COLLECTION_BG_IMAGE
-import com.tokopedia.wishlist.util.WishlistV2Utils
 import com.tokopedia.wishlistcollection.analytics.WishlistCollectionAnalytics
 import com.tokopedia.wishlistcollection.data.response.GetWishlistCollectionResponse
 import com.tokopedia.wishlistcollection.view.adapter.WishlistCollectionAdapter
@@ -19,13 +17,7 @@ class WishlistCollectionCreateItemViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: WishlistCollectionTypeLayoutData) {
             if (item.dataObject is GetWishlistCollectionResponse.GetWishlistCollections.WishlistCollectionResponseData.Placeholder) {
-                val params: GridLayout.LayoutParams = GridLayout.LayoutParams(binding.rlCreateWishlistCollection.layoutParams)
-                params.width = WishlistV2Utils.toDp(154)
-                params.height = WishlistV2Utils.toDp(154)
-
                 binding.run {
-                    rlCreateWishlistCollection.layoutParams = params
-
                     labelNewCollection.text = item.dataObject.text
                     if (item.dataObject.action == CREATE_COLLECTION) {
                         wishlistCollectionCreateNew.apply {
@@ -34,7 +26,7 @@ class WishlistCollectionCreateItemViewHolder(
                                 setPlaceHolder(R.drawable.placeholder_img)
                             }
                         }
-                        rlCreateWishlistCollection.setOnClickListener {
+                        clCreateCollection.setOnClickListener {
                             actionListener?.onCreateNewCollectionClicked()
                             WishlistCollectionAnalytics.sendCreateNewCollectionOnWishlistPageEvent()
                         }
--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/adapter/viewholder/WishlistCollectionRecommendationTitleViewHolder.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/adapter/viewholder/WishlistCollectionRecommendationTitleViewHolder.kt
@@ -2,13 +2,20 @@ package com.tokopedia.wishlistcollection.view.adapter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.unifycomponents.toPx
 import com.tokopedia.wishlist.databinding.WishlistV2RecommendationTitleItemBinding
 import com.tokopedia.wishlistcollection.data.model.WishlistCollectionTypeLayoutData
 
 class WishlistCollectionRecommendationTitleViewHolder(private val binding: WishlistV2RecommendationTitleItemBinding, private val isWithMargin: Boolean) : RecyclerView.ViewHolder(binding.root) {
 
+    private companion object {
+        private const val PADDING_20 = 20
+        private const val PADDING_12 = 12
+    }
+    
     fun bind(data: WishlistCollectionTypeLayoutData) {
         binding.root.visible()
+        binding.root.setPadding(PADDING_20.toPx(), PADDING_20.toPx(), 0, PADDING_12.toPx())
         binding.tvRvTitleWishlistV2.text = data.dataObject as String
     }
 }
