f2baee5ca4f63be0628be77df18ae90de1d6990e
Dhaba Widhikari <wikodhawi@gmail.com>
2022-08-26 10:33:33 +0700

    add wait for data

--- features/merchant/brandlist/src/androidTest/java/com/tokopedia/brandlist/cassava/BrandListPageCassava.kt
+++ features/merchant/brandlist/src/androidTest/java/com/tokopedia/brandlist/cassava/BrandListPageCassava.kt
@@ -91,6 +91,7 @@ class BrandListPageCassava {
 //            Espresso.onView(firstView(withId(R.id.chip_alphabet_header))).perform(click())
             CommonActions.clickOnEachItemRecyclerView(holder.itemView, R.id.rv_groups_chip, 4)
         }
+        waitForData()
         doActivityTestByModelClass(delayBeforeRender = 5000, dataModelClass = AllBrandUiModel::class) { holder, position ->
             logTestMessage("Captured is AllBrandViewHolder")
             InstrumentationRegistry.getInstrumentation().runOnMainSync {
