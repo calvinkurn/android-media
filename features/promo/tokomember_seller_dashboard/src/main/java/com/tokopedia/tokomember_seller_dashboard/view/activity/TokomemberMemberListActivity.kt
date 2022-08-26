9abe88c48f98c2cb10427ef731f3fe67b8f79c00
Harshit Jain <harshit.jain@tokopedia.com>
2022-08-26 23:44:40 +0530

    added fragment and xml layouts for member list screen

---
+++ features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/activity/TokomemberMemberListActivity.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.tokomember_seller_dashboard.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tokopedia.tokomember_seller_dashboard.R
+
+class TokomemberMemberListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun addFragment(){
+
+    }
+
+}
---
+++ features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/fragment/TokomemberMemberListFragment.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.tokomember_seller_dashboard.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+
+class TokomemberMemberListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
