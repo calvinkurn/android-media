a9b4879fe9dc115aef10933b13e928ae259e99fa
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-08-26 14:22:01 +0800

    more option bottom sheet

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/bottomsheet/PostMoreOptionBottomSheet.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/bottomsheet/PostMoreOptionBottomSheet.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tokopedia.sellerhomecommon.R
 import com.tokopedia.sellerhomecommon.databinding.ShcBottomSheetPostMoreOptionBinding
+import com.tokopedia.sellerhomecommon.presentation.view.adapter.PostMoreOptionAdapter
 
 /**
  * Created by @ilhamsuaib on 23/08/22.
  */
 
 class PostMoreOptionBottomSheet : BaseBottomSheet<ShcBottomSheetPostMoreOptionBinding>() {
+
+    companion object {
+        private const val TAG = "PostMoreOptionBottomSheet"
+    }
+
+    private var onRemoveArticleOptionCallback: () -> Unit = {}
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +34,31 @@ class PostMoreOptionBottomSheet : BaseBottomSheet<ShcBottomSheetPostMoreOptionBi
     }
 
     override fun setupView() = binding?.run {
+        setupOptions()
+    }
 
+    fun setOnRemoveArticleOptionClicked(onRemoveArticleOptionCallback: () -> Unit) {
+        this.onRemoveArticleOptionCallback = onRemoveArticleOptionCallback
+    }
+
+    fun show(fm: FragmentManager) {
+        if (fm.isStateSaved || isVisible) {
+            return
+        }
+
+        show(fm, TAG)
+    }
+
+    private fun setupOptions() {
+        binding?.rvShcPostMoreOption?.run {
+            val options = listOf(
+                context.getString(R.string.shc_post_more_option_1)
+            )
+
+            val optionAdapter = PostMoreOptionAdapter(options, onRemoveArticleOptionCallback)
+
+            adapter = optionAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 }
