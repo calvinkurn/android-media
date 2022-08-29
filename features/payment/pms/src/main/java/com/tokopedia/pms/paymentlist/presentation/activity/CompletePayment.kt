a8540fb176694c7c22095d05be0ffc3ec9ef1a85
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-08-29 12:52:47 +0530

    added basic layout in webview

--- features/payment/pms/src/main/java/com/tokopedia/pms/paymentlist/presentation/activity/CompletePayment.kt
+++ features/payment/pms/src/main/java/com/tokopedia/pms/paymentlist/presentation/activity/CompletePayment.kt
@@ -1,12 +1,35 @@
 package com.tokopedia.pms.paymentlist.presentation.activity
 
-import androidx.fragment.app.Fragment
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class CompletePayment : BaseSimpleActivity() {
+
+class CompletePayment : AppCompatActivity() {
 
 
-    override fun getNewFragment(): Fragment? {
-        TODO("Not yet implemented")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window?.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window?.statusBarColor = ContextCompat.getColor(this,com.tokopedia.unifyprinciples.R.color.Unify_G500)
+            } else {
+                window?.statusBarColor = ContextCompat.getColor(this,com.tokopedia.unifyprinciples.R.color.Unify_G500)
+            }
+        }
+
+        initView()
+
     }
+
+    private fun initView() {
+
+    }
+
 }
