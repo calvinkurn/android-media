6681380a7774d2d0f7504cb0559f93f3c19baf0d
stevenwijaya <steven.wijaya@tokopedia.com>
2022-08-30 03:09:53 +0700

    add seller view model

--- wearosapp/src/main/java/com/tokopedia/sellerapp/presentation/SellerAppActivity.kt
+++ wearosapp/src/main/java/com/tokopedia/sellerapp/presentation/SellerAppActivity.kt
@@ -5,13 +5,16 @@ import WearAppTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavHostController
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
-import com.tokopedia.sellerapp.util.WearMessageHelper
+import com.tokopedia.sellerapp.presentation.viewmodel.SellerAppViewModel
+import com.tokopedia.sellerapp.presentation.viewmodel.SellerAppViewModelFactory
 
 class SellerAppActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
+    private lateinit var sellerAppViewModel: SellerAppViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,24 @@ class SellerAppActivity : ComponentActivity() {
             }
         }
 
-        WearMessageHelper.sendAction(this, WearMessageHelper.Action.GET_ORDER_LIST, ::onOrderListDataReceived)
+        initViewModel()
+        observeOrderListData()
+        loadData()
     }
 
-    private fun onOrderListDataReceived(data: String) {
+    private fun initViewModel() {
+        sellerAppViewModel =
+            ViewModelProvider(this, SellerAppViewModelFactory(this))
+                .get(SellerAppViewModel::class.java)
+    }
 
+    private fun observeOrderListData() {
+        sellerAppViewModel.getOrderListDataState().observe(this) {
+
+        }
+    }
+
+    private fun loadData() {
+        sellerAppViewModel.getOrderListData(SellerAppViewModel.Action.GET_ORDER_LIST)
     }
 }
