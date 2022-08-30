acce0391259c60735fe16994639b388f7e457279
astidhiyaa <asti.anzaria@tokopedia.com>
2022-08-30 16:48:13 +0800

    `feat` setup item dec

--- features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/itemdecoration/MerchantVoucherItemDecoration.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/productsheet/itemdecoration/MerchantVoucherItemDecoration.kt
@@ -29,7 +29,7 @@ class MerchantVoucherItemDecoration(context: Context) : RecyclerView.ItemDecorat
     }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        for (index in 0 until parent.childCount) {
+        for (index in 0 until parent.childCount - 1) {
             val child = parent.getChildAt(index)
 
             if (index != 0) {
