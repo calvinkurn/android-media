423b6d19ed34da6134e8205368205ebf5db243d1
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-08-30 11:28:32 +0700

    `revert` MainActivity.kt

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -118,7 +118,7 @@ class MainActivity : AppCompatActivity() {
         val appLink = etAppLink.text.toString()
         if(appLink.isNotBlank())
             RouteManager.route(this, appLink)
-        else startActivity(Intent(this, ContentProductTagSampleActivity::class.java))
+        else Toast.makeText(this, "Please input appLink / webLink", Toast.LENGTH_SHORT).show()
     }
 
     private fun getDefaultAppLink(): String {
