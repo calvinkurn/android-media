4bde00d2abe22d13d7def882bec51a40bd72db2b
Harshit Jain <harshit.jain@tokopedia.com>
2022-08-30 11:46:38 +0530

    Fixed top margin of Info icon and removed hardcoded Program string

--- features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/viewholder/TokomemberDashProgramVh.kt
+++ features/promo/tokomember_seller_dashboard/src/main/java/com/tokopedia/tokomember_seller_dashboard/view/viewholder/TokomemberDashProgramVh.kt
@@ -112,7 +112,6 @@ class TokomemberDashProgramVh(itemView: View, val fragmentManager: FragmentManag
                 btn_edit.show()
             }
             ACTIVE ->{
-                programStatus.text = itemView.context.getString(R.string.tm_program_status_value,item.statusStr ?: "")
                 programStatus.setTextColor(ColorStateList.valueOf(ContextCompat.getColor(itemView.context, R.color.Unify_GN500)))
                 view_status.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(itemView.context, R.color.Unify_GN500))
                 btn_edit.hide()
