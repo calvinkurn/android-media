34c8ea7585d212a4e31eb99fe63df238b4bf226f
stevenwijaya <steven.wijaya@tokopedia.com>
2022-08-31 08:49:05 +0700

    unit test update

--- features/discovery/autocomplete_component/src/test/java/com/tokopedia/autocompletecomponent/universal/presenter/viewmodel/LoadUniversalSearchTest.kt
+++ features/discovery/autocomplete_component/src/test/java/com/tokopedia/autocompletecomponent/universal/presenter/viewmodel/LoadUniversalSearchTest.kt
@@ -6,6 +6,7 @@ import com.tokopedia.autocompletecomponent.shouldBeInstanceOf
 import com.tokopedia.autocompletecomponent.universal.domain.model.UniversalSearchModel
 import com.tokopedia.autocompletecomponent.universal.presentation.widget.carousel.CarouselDataView
 import com.tokopedia.autocompletecomponent.universal.presentation.widget.doubleline.DoubleLineDataView
+import com.tokopedia.autocompletecomponent.universal.presentation.widget.errorstate.ErrorStateDataView
 import com.tokopedia.autocompletecomponent.universal.presentation.widget.listgrid.ListGridDataView
 import com.tokopedia.autocompletecomponent.universal.presenter.viewmodel.testinstance.universalSearchItems
 import com.tokopedia.discovery.common.State
@@ -23,7 +24,7 @@ internal class LoadUniversalSearchTest: UniversalSearchDataViewTestFixtures() {
         `When universal search view created`()
 
         `Then assert universal search is successful`()
-        `Then assert visitable data is correct`()
+        `Then assert visitable data`()
     }
 
     private fun `When universal search view created`() {
@@ -36,7 +37,7 @@ internal class LoadUniversalSearchTest: UniversalSearchDataViewTestFixtures() {
         universalSearchState.value.shouldBeInstanceOf<State.Success<*>>()
     }
 
-    private fun `Then assert visitable data is correct`() {
+    private fun `Then assert visitable data`() {
         assert(universalSearchState.value?.data?.size == expectedDataCount)
         universalSearchState.value?.data?.assertVisitableList()
     }
@@ -47,5 +48,26 @@ internal class LoadUniversalSearchTest: UniversalSearchDataViewTestFixtures() {
         get(2).shouldBeInstanceOf<ListGridDataView>()
 
         assertVisitableListData(universalSearchItems, keyword, dimension90)
+    }
+
+    @Test
+    fun `Universal Search Error`() {
+        `Given universal search API will be fail`()
+
+        `When universal search view created`()
+
+        `Then assert universal search is failed`()
+        `Then assert error visitable data`()
+    }
+
+    private fun `Then assert universal search is failed`() {
+        universalSearchState = universalSearchViewModel.getUniversalSearchState()
+
+        universalSearchState.value.shouldBeInstanceOf<State.Error<*>>()
+    }
+
+    private fun `Then assert error visitable data`() {
+        assert(universalSearchState.value?.data?.size == 1)
+        universalSearchState.value?.data?.get(0).shouldBeInstanceOf<ErrorStateDataView>()
     }
 }
