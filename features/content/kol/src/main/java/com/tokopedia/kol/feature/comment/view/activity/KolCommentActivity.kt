784ab058bcd01f3d05dd8214bc4fefa4a609d875
sahiti-chilakala <sahiti_chilakala@tokopedia.com>
2022-09-01 22:28:38 +0530

    update_comment_page_same_as_shop_comment_page

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/view/activity/KolCommentActivity.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/comment/view/activity/KolCommentActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.kol.analytics.KolEventTracking
-import com.tokopedia.kol.feature.comment.view.fragment.KolCommentFragment
+import com.tokopedia.kol.feature.comment.view.fragment.KolCommentNewFragment
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.track.TrackApp
 import com.tokopedia.track.TrackAppUtils
@@ -54,7 +54,7 @@ class KolCommentActivity : BaseSimpleActivity() {
         val colPosition = bundle[ARGS_POSITION_COLUMN]
         if (colPosition is String) bundle.putInt(ARGS_POSITION_COLUMN, colPosition.toInt())
 
-        return KolCommentFragment.createInstance(bundle)
+        return KolCommentNewFragment.createInstance(bundle)
     }
 
     override fun onBackPressed() {
