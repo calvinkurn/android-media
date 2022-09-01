eaaa021b4d95eb650748a71dff9c4fce9b178e07
saidfaisal <saidfaisal777@gmail.com>
2022-09-01 18:49:26 +0700

    remove unnecessary import

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/recipesearch/presentation/fragment/TokoNowRecipeSearchFragment.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/recipesearch/presentation/fragment/TokoNowRecipeSearchFragment.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.tokopedianow.recipesearch.presentation.fragment
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
