87c58137cbfc28ced54191eef3862291e1e02a17
fajarca <fajar.azhar@tokopedia.com>
2022-09-02 14:59:52 +0700

    Guard required params

--- features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/delegates/HasPaginatedListImpl.kt
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/delegates/HasPaginatedListImpl.kt
@@ -13,6 +13,9 @@ class HasPaginatedListImpl : HasPaginatedList {
         config: HasPaginatedList.Config,
         loadNextPage: (Int, Int) -> Unit
     ) {
+        if (recyclerView.adapter == null) throw RuntimeException("RecyclerView adapter must not be null. Please initialize recyclerView adapter first")
+        if (recyclerView.layoutManager == null) throw RuntimeException("RecyclerView layout manager must not be null. Please initialize layout manager first")
+
         this.config = config
         enablePaging(recyclerView, config, loadNextPage)
         resetPaging()
