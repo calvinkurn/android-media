6d76dea7505484a13ec06a65cb170c34b0b27f31
astidhiyaa <asti.anzaria@tokopedia.com>
2022-09-02 10:15:48 +0800

    `fix` remove view component from fragment

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
@@ -134,7 +134,6 @@ class PlayUserInteractionFragment @Inject constructor(
         PiPViewComponent.Listener,
         CastViewComponent.Listener,
         ProductSeeMoreViewComponent.Listener,
-        InteractiveActiveViewComponent.Listener,
         InteractiveGameResultViewComponent.Listener,
         ChooseAddressViewComponent.Listener,
         EngagementCarouselViewComponent.Listener
@@ -174,8 +173,6 @@ class PlayUserInteractionFragment @Inject constructor(
     /**
      * Interactive
      */
-    private val interactiveActiveView by viewComponentOrNull { InteractiveActiveViewComponent(it, this) }
-    private val interactiveFinishView by viewComponentOrNull { InteractiveFinishViewComponent(it) }
     private val interactiveResultView by viewComponentOrNull(isEagerInit = true) { InteractiveGameResultViewComponent(it, this) }
 
     private val offset8 by lazy { requireContext().resources.getDimensionPixelOffset(com.tokopedia.unifyprinciples.R.dimen.spacing_lvl3) }
@@ -566,11 +563,9 @@ class PlayUserInteractionFragment @Inject constructor(
     /**
      * Interactive Active Listener
      */
-    override fun onWidgetClicked(view: InteractiveActiveViewComponent) {
-        playViewModel.submitAction(
-            PlayViewerNewAction.StartPlayingInteractive)
-            newAnalytic.clickActiveInteractive(interactiveId = playViewModel.interactiveData.id, shopId = playViewModel.partnerId.toString(), interactiveType = playViewModel.interactiveData, channelId = playViewModel.channelId)
-    }
+//        playViewModel.submitAction(
+//            PlayViewerNewAction.StartPlayingInteractive)
+//            newAnalytic.clickActiveInteractive(interactiveId = playViewModel.interactiveData.id, shopId = playViewModel.partnerId.toString(), interactiveType = playViewModel.interactiveData, channelId = playViewModel.channelId)
     //endregion
 
     fun maxTopOnChatMode(maxTopPosition: Int) {
@@ -866,12 +861,6 @@ class PlayUserInteractionFragment @Inject constructor(
                 val state = cachedState.value
                 val prevState = cachedState.prevValue
 
-                renderInteractiveView(
-                    prevState?.interactive,
-                    state.interactive,
-                    prevState?.bottomInsets,
-                    state.bottomInsets
-                )
                 renderToolbarView(state.title)
                 renderShareView(state.channel, state.bottomInsets, state.status)
                 renderPartnerInfoView(prevState?.partner, state.partner)
@@ -1499,120 +1488,6 @@ class PlayUserInteractionFragment @Inject constructor(
 
         if (!bottomInsets.isAnyShown) pipView?.show()
         else pipView?.hide()
-    }
-
-    private fun renderInteractiveView(
-        prevState: InteractiveStateUiModel?,
-        state: InteractiveStateUiModel,
-        prevBottomInsets: Map<BottomInsetsType, BottomInsetsState>?,
-        bottomInsets: Map<BottomInsetsType, BottomInsetsState>,
-    ) {
-        if (state.isPlaying) {
-            interactiveActiveView?.hide()
-            interactiveFinishView?.hide()
-            return
-        }
-
-        val isAnyInsetsShown = bottomInsets.isAnyShown
-        /**
-         * Invisible to reduce the amount of conditions
-         * for square [com.tokopedia.unifycomponents.timer.TimerUnifySingle] to appear
-         */
-        if (isAnyInsetsShown) {
-            interactiveActiveView?.invisible()
-            interactiveFinishView?.invisible()
-            return
-        }
-
-        val prevIsAnyInsetsShown = prevBottomInsets?.isAnyShown
-
-        /**
-         * Render:
-         * - if interactive has changed <b>or</b>
-         * - if isPlaying state has changed to not playing
-         * - if any bottom insets shown state has changed to false
-         */
-        if (prevState?.interactive != state.interactive ||
-            ((prevState.isPlaying != state.isPlaying) && !state.isPlaying) ||
-            (prevIsAnyInsetsShown != isAnyInsetsShown) && !isAnyInsetsShown) {
-
-            when (state.interactive) {
-                is InteractiveUiModel.Giveaway -> renderGiveawayView(state.interactive)
-                is InteractiveUiModel.Quiz -> renderQuizView(state.interactive)
-                InteractiveUiModel.Unknown -> {
-                    interactiveActiveView?.hide()
-                    interactiveFinishView?.hide()
-                }
-            }
-        }
-    }
-
-    private fun renderGiveawayView(
-        state: InteractiveUiModel.Giveaway,
-    ) {
-        when (val status = state.status) {
-            is InteractiveUiModel.Giveaway.Status.Upcoming -> {
-                interactiveActiveView?.setUpcomingGiveaway(
-                    desc = state.title,
-                    targetTime = status.startTime,
-                    onDurationEnd = {
-                        playViewModel.submitAction(PlayViewerNewAction.GiveawayUpcomingEnded)
-                    }
-                )
-                interactiveActiveView?.show()
-                interactiveFinishView?.hide()
-            }
-            is InteractiveUiModel.Giveaway.Status.Ongoing -> {
-                interactiveActiveView?.setOngoingGiveaway(
-                    desc = state.title,
-                    targetTime = status.endTime,
-                    onDurationEnd = {
-                        playViewModel.submitAction(PlayViewerNewAction.GiveawayOngoingEnded)
-                    }
-                )
-                interactiveActiveView?.show()
-                interactiveFinishView?.hide()
-
-                playViewModel.submitAction(PlayViewerNewAction.AutoOpenInteractive)
-            }
-            InteractiveUiModel.Giveaway.Status.Finished -> {
-                interactiveActiveView?.hide()
-
-                interactiveFinishView?.setupGiveaway()
-                interactiveFinishView?.show()
-            }
-            InteractiveUiModel.Giveaway.Status.Unknown -> {
-                interactiveActiveView?.hide()
-                interactiveFinishView?.hide()
-            }
-        }
-    }
-
-    private fun renderQuizView(state: InteractiveUiModel.Quiz) {
-        when (val status = state.status) {
-            is InteractiveUiModel.Quiz.Status.Ongoing -> {
-                interactiveActiveView?.setQuiz(
-                    question = state.title,
-                    targetTime = status.endTime,
-                    onDurationEnd = {
-                        playViewModel.submitAction(PlayViewerNewAction.QuizEnded)
-                    }
-                )
-                interactiveActiveView?.show()
-                interactiveFinishView?.hide()
-                newAnalytic.impressActiveInteractive(shopId = playViewModel.partnerId.toString(), interactiveId = state.id, channelId = playViewModel.channelId)
-            }
-            InteractiveUiModel.Quiz.Status.Finished -> {
-                interactiveActiveView?.hide()
-
-                interactiveFinishView?.setupQuiz()
-                interactiveFinishView?.show()
-            }
-            InteractiveUiModel.Quiz.Status.Unknown -> {
-                interactiveActiveView?.hide()
-                interactiveFinishView?.hide()
-            }
-        }
     }
 
     private fun renderInteractiveDialog(
