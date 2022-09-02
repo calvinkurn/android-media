d2785661cd835e774ed83aadc1f2d6cb24e227bc
fajarca <fajar.azhar@tokopedia.com>
2022-09-02 11:28:36 +0700

    Some optimization

--- features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/delegates/HasPaginatedListImpl.kt
+++ features/merchant/campaign_common/src/main/java/com/tokopedia/campaign/delegates/HasPaginatedListImpl.kt
@@ -3,11 +3,9 @@ package com.tokopedia.campaign.delegates
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.listener.EndlessLayoutManagerListener
 import com.tokopedia.abstraction.base.view.recyclerview.EndlessRecyclerViewScrollListener
-import com.tokopedia.kotlin.extensions.view.orZero
 
 class HasPaginatedListImpl : HasPaginatedList {
 
-    private var recyclerView: RecyclerView? = null
     private var config: HasPaginatedList.Config? = null
     private var endlessRecyclerViewScrollListener: EndlessRecyclerViewScrollListener? = null
     private var endlessLayoutManagerListener: EndlessLayoutManagerListener? = null
@@ -17,40 +15,26 @@ class HasPaginatedListImpl : HasPaginatedList {
         config: HasPaginatedList.Config,
         loadNextPage: (Int, Int) -> Unit
     ) {
-        this.recyclerView = recyclerView
         this.config = config
-
-        enableLoadMore(config, loadNextPage)
-
-        loadInitialData(config, loadNextPage)
+        enablePaging(recyclerView, config, loadNextPage)
+        resetPaging()
     }
 
-    private fun loadInitialData(config: HasPaginatedList.Config, loadNextPage: (Int, Int) -> Unit) {
-        clearAllData()
-        config.onLoadNextPage()
-
-        //loadNextPage(1, 0)
-    }
-
-    private fun clearAllData() {
-        endlessRecyclerViewScrollListener?.resetState()
-    }
-
-    private fun enableLoadMore(config: HasPaginatedList.Config, loadNextPage: (Int, Int) -> Unit) {
+    private fun enablePaging(recyclerView: RecyclerView, config: HasPaginatedList.Config, loadNextPage: (Int, Int) -> Unit) {
         if (endlessRecyclerViewScrollListener == null) {
-            endlessRecyclerViewScrollListener = createEndlessRecyclerViewListener(config, loadNextPage)
+            endlessRecyclerViewScrollListener = createEndlessRecyclerViewListener(recyclerView,config, loadNextPage)
             endlessRecyclerViewScrollListener?.setEndlessLayoutManagerListener(
                 endlessLayoutManagerListener
             )
 
         }
         endlessRecyclerViewScrollListener?.apply {
-            recyclerView?.addOnScrollListener(this)
+            recyclerView.addOnScrollListener(this)
         }
     }
 
-    private fun createEndlessRecyclerViewListener(config: HasPaginatedList.Config, loadNextPage: (Int, Int) -> Unit): EndlessRecyclerViewScrollListener {
-        return object : EndlessRecyclerViewScrollListener(recyclerView?.layoutManager) {
+    private fun createEndlessRecyclerViewListener(recyclerView: RecyclerView, config: HasPaginatedList.Config, loadNextPage: (Int, Int) -> Unit): EndlessRecyclerViewScrollListener {
+        return object : EndlessRecyclerViewScrollListener(recyclerView.layoutManager) {
             override fun onLoadMore(page: Int, totalItemsCount: Int) {
                 config.onLoadNextPage()
                 loadNextPage(page, totalItemsCount)
@@ -62,6 +46,7 @@ class HasPaginatedListImpl : HasPaginatedList {
         val config = this.config ?: return
 
         config.onLoadNextPageFinished()
+
         endlessRecyclerViewScrollListener?.updateStateAfterGetData()
         endlessRecyclerViewScrollListener?.setHasNextPage(hasNextPage)
     }
