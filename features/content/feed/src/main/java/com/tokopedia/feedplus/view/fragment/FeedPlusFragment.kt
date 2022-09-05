f1b083c9764e9771a2517c6f33ad1ca27b6a84e1
shrutiagarwal <shruti.agarwal@tokopedia.com>
2022-09-05 11:18:23 +0530

    merge parent branch, fix ui

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.kt
@@ -20,7 +20,6 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.google.android.material.snackbar.Snackbar
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.model.EmptyModel
