6712eeaaa0c4dabe55ad4d55d107cf2458afa1da
astidhiyaa <asti.anzaria@tokopedia.com>
2022-09-05 14:03:28 +0800

    feat: add sisa kupon

--- features/content/play/src/main/java/com/tokopedia/play/ui/promosheet/viewholder/MerchantVoucherNewViewHolder.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/promosheet/viewholder/MerchantVoucherNewViewHolder.kt
@@ -1,8 +1,11 @@
 package com.tokopedia.play.ui.promosheet.viewholder
 
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.adapterdelegate.BaseViewHolder
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
@@ -12,6 +15,7 @@ import com.tokopedia.play.view.type.MerchantVoucherType
 import com.tokopedia.play.view.uimodel.PlayVoucherUiModel
 import com.tokopedia.play_common.util.extension.buildSpannedString
 import com.tokopedia.utils.date.DateUtil
+import com.tokopedia.unifyprinciples.R as unifyR
 
 /**
  * @author by astidhiyaa on 30/11/21
@@ -28,27 +32,57 @@ class MerchantVoucherNewViewHolder(
     private val ivCopyVoucher: IconUnify = itemView.findViewById(R.id.iv_play_voucher_copy)
     private val tvVoucherCode: TextView = itemView.findViewById(R.id.tv_play_voucher_code)
 
+    private val baseColor: ForegroundColorSpan by lazy {
+        ForegroundColorSpan(MethodChecker.getColor(itemView.context, unifyR.color.Unify_RN500))
+    }
+
+    private val fgColor: ForegroundColorSpan by lazy {
+        ForegroundColorSpan(
+            MethodChecker.getColor(
+                itemView.context,
+                unifyR.color.Unify_NN950
+            )
+        )
+    }
+
     fun bind(item: PlayVoucherUiModel.MerchantVoucherUiModel) {
         val isPrivate = item.type == MerchantVoucherType.Private
         tvVoucherTitle.text = item.title
         tvVoucherDescription.text = item.description
 
         tvVoucherExpiredDate.shouldShowWithAction(item.expiredDate.isNotEmpty()) {
-            tvVoucherExpiredDate.text = getString(
-                R.string.play_voucher_sheet_coupon_expired,
-                countDays(item.expiredDate).toString()
-            )
+            tvVoucherExpiredDate.text = buildSpannedString {
+                append(
+                    getString(
+                        R.string.play_voucher_sheet_coupon_expired,
+                        countDays(item.expiredDate).toString()
+                    ),
+                    fgColor, Spanned.SPAN_EXCLUSIVE_INCLUSIVE
+                )
+                if (item.voucherStock <= 20) {
+                    append(
+                        " " + getString(R.string.play_product_pinned_info_separator) + " ", baseColor,
+                        Spanned.SPAN_EXCLUSIVE_INCLUSIVE
+                    )
+                    append(
+                        getString(
+                            R.string.play_voucher_widget_low_quantity,
+                            item.voucherStock, ""
+                        ), baseColor, Spanned.SPAN_EXCLUSIVE_INCLUSIVE
+                    )
+                }
+            }
         }
 
         viewCopyable.showWithCondition(item.copyable)
         tvVoucherCode.text = item.code
 
         ivCopyVoucher.setOnClickListener {
-            if(!isPrivate) return@setOnClickListener
+            if (!isPrivate) return@setOnClickListener
             listener.onCopyItemVoucherClicked(item)
         }
         itemView.setOnClickListener {
-            if(isPrivate) return@setOnClickListener
+            if (isPrivate) return@setOnClickListener
             listener.onVoucherItemClicked(item)
         }
     }
