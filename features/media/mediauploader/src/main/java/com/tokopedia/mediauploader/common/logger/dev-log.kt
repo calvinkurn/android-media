f64d1b990b9935e133d5d5aab96f4a2f280b3643
isfaaghyth <isfaaghyth@gmail.com>
2022-09-05 15:48:02 +0700

    Fix: cache issue on uploader with delegate the gson on SourcePolicyManager

---
+++ features/media/mediauploader/src/main/java/com/tokopedia/mediauploader/common/logger/dev-log.kt
@@ -0,0 +1,28 @@
+package com.tokopedia.mediauploader.common.logger
+
+import com.tokopedia.config.GlobalConfig
+import com.tokopedia.mediauploader.common.data.entity.SourcePolicy
+
+data class DebugLog(
+    val sourceId: String,
+    val sourceFile: String,
+    val url: String = "",
+    val uploadId: String,
+    val sourcePolicy: SourcePolicy?
+)
+
+fun onShowDebugLogcat(param: DebugLog) {
+    if (!GlobalConfig.isAllowDebuggingTools()) return
+
+    val moduleName = "DevMediaUploader"
+
+    val message = """
+        sourceId: ${param.sourceId} |
+        file: ${param.sourceFile} |
+        resultUrl: ${param.url} |
+        uploadId: ${param.uploadId} |
+        policy: ${param.sourcePolicy}
+    """
+
+    println("$moduleName: $message")
+}
