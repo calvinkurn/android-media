670800e1ab39b096ce5d05d520ec4140ae62dfdd
Irpan - <irpan@tokopedia.com>
2022-09-07 04:42:30 +0800

    add base activity to modify app bar and add fragment home

--- features/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/DtHomeActivity.kt
+++ features/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/DtHomeActivity.kt
@@ -1,11 +1,17 @@
 package com.tokopedia.dilayanitokopedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tokopedia.dilayanitokopedia.common.base.activity.BaseDtActivity
+import com.tokopedia.dilayanitokopedia.home.presentation.fragment.DtHomeFragment
 
-class DtHomeActivity : AppCompatActivity() {
+class DtHomeActivity : BaseDtActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dthome)
     }
+
+    override fun getFragment(): Fragment {
+        return DtHomeFragment()
+    }
+
 }
