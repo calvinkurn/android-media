deeba3f7753a8aed1576d0479857cf6402eec741
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2022-09-06 11:40:31 +0700

    enable top divider only

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/ViewUtils.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/ViewUtils.kt
@@ -29,26 +29,25 @@ object ChannelWidgetUtil {
     ) {
         dividerTop?.layoutParams?.height = DIVIDER_HEIGHT.dpToPx().toInt()
         dividerBottom?.layoutParams?.height = DIVIDER_HEIGHT.dpToPx().toInt()
-        dividerTop?.invisible()
-        dividerBottom?.visible()
 //        when(channelModel?.channelConfig?.dividerType) {
-//            ChannelConfig.DIVIDER_NO_DIVIDER -> {
-//                dividerTop?.invisible()
-//                dividerBottom?.gone()
-//            }
-//            ChannelConfig.DIVIDER_TOP -> {
-//                dividerTop?.visible()
-//                dividerBottom?.gone()
-//            }
-//            ChannelConfig.DIVIDER_BOTTOM -> {
-//                dividerTop?.invisible()
-//                dividerBottom?.visible()
-//            }
-//            ChannelConfig.DIVIDER_TOP_AND_BOTTOM -> {
-//                dividerTop?.visible()
-//                dividerBottom?.visible()
-//            }
-//        }
+        when(ChannelConfig.DIVIDER_TOP) {
+            ChannelConfig.DIVIDER_NO_DIVIDER -> {
+                dividerTop?.invisible()
+                dividerBottom?.gone()
+            }
+            ChannelConfig.DIVIDER_TOP -> {
+                dividerTop?.visible()
+                dividerBottom?.gone()
+            }
+            ChannelConfig.DIVIDER_BOTTOM -> {
+                dividerTop?.invisible()
+                dividerBottom?.visible()
+            }
+            ChannelConfig.DIVIDER_TOP_AND_BOTTOM -> {
+                dividerTop?.visible()
+                dividerBottom?.visible()
+            }
+        }
     }
 }
 
