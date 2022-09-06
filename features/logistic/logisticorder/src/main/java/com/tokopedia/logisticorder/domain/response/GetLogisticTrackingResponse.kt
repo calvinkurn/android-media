7152036762fc939978fe6e5b7aaa39ea9d146c5f
hiradevina <fakhira.devina@tokopedia.com>
2022-09-06 11:48:18 +0700

    deprecate tippingdata.lastDriver

--- features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/domain/response/GetLogisticTrackingResponse.kt
+++ features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/domain/response/GetLogisticTrackingResponse.kt
@@ -220,7 +220,7 @@ data class Tipping(
         val statusSubtitle: String = "",
         @SerializedName("last_driver")
         @Expose
-        @Deprecated("Use LastDriver instead")
+        @Deprecated("Use TrackingData.lastDriver instead")
         val lastDriver: LastDriver = LastDriver()
 )
 
