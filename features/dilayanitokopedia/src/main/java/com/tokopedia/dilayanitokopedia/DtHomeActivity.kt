9699cc3e93c74434847062bed5d7018793dfabcb
Irpan - <irpan@tokopedia.com>
2022-09-07 04:07:39 +0800

    add new module

---
+++ features/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/DtHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.dilayanitokopedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DtHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dthome)
+    }
+}
