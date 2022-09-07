34673eb145cfe0e6d2f3f80dde371d02fca3d60a
fajarca <fajar.azhar@tokopedia.com>
2022-09-07 13:47:43 +0700

    Add test case for get finished flash sale items

--- features/merchant/seller_tokopedia_flash_sale/src/test/java/com/tokopedia/tkpd/flashsale/presentation/list/child/FlashSaleListViewModelTest.kt
+++ features/merchant/seller_tokopedia_flash_sale/src/test/java/com/tokopedia/tkpd/flashsale/presentation/list/child/FlashSaleListViewModelTest.kt
@@ -6,6 +6,7 @@ import com.tokopedia.tkpd.flashsale.domain.entity.FlashSaleData
 import com.tokopedia.tkpd.flashsale.domain.entity.enums.FlashSaleStatus
 import com.tokopedia.tkpd.flashsale.domain.usecase.GetFlashSaleListForSellerCategoryUseCase
 import com.tokopedia.tkpd.flashsale.domain.usecase.GetFlashSaleListForSellerUseCase
+import com.tokopedia.tkpd.flashsale.presentation.list.child.adapter.item.FinishedFlashSaleItem
 import com.tokopedia.tkpd.flashsale.presentation.list.child.adapter.item.UpcomingFlashSaleItem
 import com.tokopedia.tkpd.flashsale.presentation.list.child.uimodel.FlashSaleListUiEvent
 import com.tokopedia.tkpd.flashsale.presentation.list.child.uimodel.FlashSaleListUiState
@@ -20,7 +21,6 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import java.util.Date
 import java.util.GregorianCalendar
 
 class FlashSaleListViewModelTest {
@@ -69,9 +69,9 @@ class FlashSaleListViewModelTest {
     }
 
     @Test
-    fun `When fetch flash sale from remote success, should successfully receive the data`() = runBlockingTest {
-
-        val tabId = 1
+    fun `When fetch upcoming flash sale from remote success, should successfully receive the data`() = runBlockingTest {
+        //Given
+        val tabId = TabConstant.TAB_ID_UPCOMING
         val tabName = "upcoming"
         val offset = 0
 
@@ -140,8 +140,97 @@ class FlashSaleListViewModelTest {
         }
 
         //When
-        viewModel.processEvent(FlashSaleListUiEvent.LoadPage(tabId, "upcoming", offset, currentDate))
+        viewModel.processEvent(FlashSaleListUiEvent.LoadPage(tabId, tabName, offset, currentDate))
 
+        //Then
+        val actualTabName = emittedValues.last().tabName
+        val actualOffset = emittedValues.last().offset
+        val actualTabId = emittedValues.last().tabId
+        val actualItems = emittedValues.last().allItems
+
+        assertEquals(tabName, actualTabName)
+        assertEquals(offset, actualOffset)
+        assertEquals(tabId, actualTabId)
+        assertEquals(expected, actualItems)
+
+        job.cancel()
+    }
+
+    @Test
+    fun `When fetch finished flash sale from remote success, should successfully receive the data`() = runBlockingTest {
+        //Given
+        val tabId =  TabConstant.TAB_ID_FINISHED
+        val tabName = "finished"
+        val offset = 0
+
+        val params = GetFlashSaleListForSellerUseCase.Param(
+            tabName,
+            offset,
+            categoryIds = listOf(),
+            statusIds = listOf(),
+            sortOrderBy = "DEFAULT_VALUE_PLACEHOLDER",
+            sortOrderRule = "ASC",
+            requestProductMetaData = true
+        )
+
+        val productMeta = FlashSale.ProductMeta(5, 5, 10, 5, 5, 0)
+        val currentDate = GregorianCalendar(2022, 10,1, 0,0,0).time
+        val flashSaleStartDate = GregorianCalendar(2022, 10,10, 0,0,0).time
+        val flashSaleEndDate = GregorianCalendar(2022, 10,20, 0,0,0).time
+        val flashSaleReview = GregorianCalendar(2022, 10,5, 0,0,0).time
+        val flashSaleSubmission = GregorianCalendar(2022, 10,1, 7,0,0).time
+
+        val flashSale = FlashSale(
+            1,
+            "",
+            "",
+            "",
+            flashSaleEndDate,
+            1,
+            "Flash Sale 1",
+            true,
+            productMeta,
+            1,
+            flashSaleReview,
+            flashSaleReview,
+            "",
+            flashSaleStartDate,
+            TabConstant.TAB_ID_FINISHED,
+            "Selesai",
+            flashSaleSubmission,
+            flashSaleSubmission,
+            false,
+            FlashSale.FormattedDate("", ""),
+            FlashSaleStatus.FINISHED
+        )
+        val expected = listOf(
+            FinishedFlashSaleItem(
+                1,
+                "Flash Sale 1",
+                "",
+                "",
+                "",
+                FlashSaleStatus.FINISHED,
+                "Selesai",
+                productMeta,
+                "",
+                100
+            )
+
+        )
+        val response = FlashSaleData(1, listOf(flashSale))
+
+        coEvery { getFlashSaleListForSellerUseCase.execute(params) } returns response
+
+        val emittedValues = arrayListOf<FlashSaleListUiState>()
+        val job = launch {
+            viewModel.uiState.toList(emittedValues)
+        }
+
+        //When
+        viewModel.processEvent(FlashSaleListUiEvent.LoadPage(tabId, tabName, offset, currentDate))
+
+        //Then
         val actualTabName = emittedValues.last().tabName
         val actualOffset = emittedValues.last().offset
         val actualTabId = emittedValues.last().tabId
