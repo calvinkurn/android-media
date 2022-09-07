09c6c64d14a6f49d8424e2fa564c5a7d6c128c62
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2022-09-08 00:58:19 +0700

    remove unnecessary code

--- wearosapp/src/main/java/com/tokopedia/sellerapp/data/repository/WearCacheAction.kt
+++ wearosapp/src/main/java/com/tokopedia/sellerapp/data/repository/WearCacheAction.kt
@@ -1,7 +1,5 @@
 package com.tokopedia.sellerapp.data.repository
 
-import android.util.Log
-
 interface WearCacheAction {
     fun saveOrderListToCache(message: String)
     fun saveOrderSummaryToCache(message: String)
