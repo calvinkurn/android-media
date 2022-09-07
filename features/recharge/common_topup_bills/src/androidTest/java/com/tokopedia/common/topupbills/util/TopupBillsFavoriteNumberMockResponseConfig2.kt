6b4cb8a7c6d0d894cec8f52c409d9a4b64dd29eb
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-09-07 17:35:33 +0700

    [Recharge] favorite number instrumentation test update, applink reroute to perso activity

---
+++ features/recharge/common_topup_bills/src/androidTest/java/com/tokopedia/common/topupbills/util/TopupBillsFavoriteNumberMockResponseConfig2.kt
@@ -0,0 +1,53 @@
+package com.tokopedia.common.topupbills.util
+
+import android.content.Context
+import com.tokopedia.common.topupbills.test.R
+import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
+import com.tokopedia.test.application.util.InstrumentationMockHelper
+
+class TopupBillsFavoriteNumberMockResponseConfig2(
+    private val isMockFilledFavoriteNumber: Boolean,
+    private val isMockUpdateFavoriteDetail: Boolean,
+): MockModelConfig() {
+
+    companion object {
+        const val KEY_QUERY_MODIFY_FAV_NUMBER = "updateFavoriteDetail"
+        const val KEY_QUERY_FETCH_FAV_NUMBER = "digiPersoGetPersonalizedItems"
+    }
+
+    override fun createMockModel(context: Context): MockModelConfig {
+        if (isMockUpdateFavoriteDetail) {
+            addMockUpdateFavoriteDetail(context)
+        }
+        if (isMockFilledFavoriteNumber) {
+            addHappyMockModel(context)
+        } else {
+            addUnhappyMockModel(context)
+        }
+        return this
+    }
+
+    private fun addHappyMockModel(context: Context) {
+        addMockResponse(
+            KEY_QUERY_FETCH_FAV_NUMBER,
+            InstrumentationMockHelper.getRawString(context, R.raw.response_mock_data_favorite_number_fetch),
+            FIND_BY_CONTAINS
+        )
+    }
+
+    private fun addUnhappyMockModel(context: Context) {
+        addMockResponse(
+            KEY_QUERY_FETCH_FAV_NUMBER,
+            InstrumentationMockHelper.getRawString(context, R.raw.response_mock_data_favorite_number_fetch_zero),
+            FIND_BY_CONTAINS
+        )
+    }
+
+    private fun addMockUpdateFavoriteDetail(context: Context) {
+        addMockResponse(
+            KEY_QUERY_MODIFY_FAV_NUMBER,
+            InstrumentationMockHelper.getRawString(context, R.raw.response_mock_data_favorite_number_update),
+            FIND_BY_CONTAINS
+        )
+    }
+}
