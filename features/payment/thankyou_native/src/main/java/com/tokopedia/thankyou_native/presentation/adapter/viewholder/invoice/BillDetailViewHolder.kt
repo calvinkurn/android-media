b9afe09ac27cd233bd8358f90060a7fc1a93f67d
deepika <deepika.tripathi@tokopedia.com>
2022-09-07 11:54:42 +0530

    tooltip bottom sheet removed graphic

--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/presentation/adapter/viewholder/invoice/BillDetailViewHolder.kt
+++ features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/presentation/adapter/viewholder/invoice/BillDetailViewHolder.kt
@@ -7,13 +7,13 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.thankyou_native.R
 import com.tokopedia.thankyou_native.presentation.adapter.model.FeeDetail
 import com.tokopedia.thankyou_native.presentation.adapter.model.TotalFee
 import com.tokopedia.thankyou_native.presentation.fragment.ToolTipInfoBottomSheet.Companion.openTooltipInfoBottomSheet
+import com.tokopedia.unifycomponents.ImageUnify
 import kotlinx.android.synthetic.main.thank_widget_bill_detail.view.*
 
 class BillDetailViewHolder(val view: View) : AbstractViewHolder<TotalFee>(view) {
@@ -45,13 +45,13 @@ class BillDetailViewHolder(val view: View) : AbstractViewHolder<TotalFee>(view) 
             feeDetailTitle.text = feeDetail.feeTitle
             feeDetailView.findViewById<TextView>(R.id.tvInvoicePaidWithModeValue)
                     .text = feeDetail.feeAmountStr
-            val tooltipInfoIcon = feeDetailView.findViewById<IconUnify>(R.id.infoToolTip)
+            val tooltipInfoIcon = feeDetailView.findViewById<ImageUnify>(R.id.infoToolTip)
             setUpToolTipInfoIcon(context,tooltipInfoIcon,feeDetail)
             llContainer.addView(feeDetailView)
         }
     }
 
-    private fun setUpToolTipInfoIcon(context: Context,toolTipIconView:IconUnify,feeDetail: FeeDetail){
+    private fun setUpToolTipInfoIcon(context: Context,toolTipIconView:ImageUnify,feeDetail: FeeDetail){
         if(feeDetail.showToolTip.toBoolean()){
             toolTipIconView.visible()
             toolTipIconView.setOnClickListener {
