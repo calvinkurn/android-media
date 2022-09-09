d1b388d582f6f96a48dc3a3b5237040172f95459
Muhammad Furqan <furqan@tokopedia.com>
2022-09-09 11:28:21 +0700

    resolve danger error

--- features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/addpin/view/activity/PinOnboardingActivity.kt
+++ features/user/profilecompletion/src/main/java/com/tokopedia/profilecompletion/addpin/view/activity/PinOnboardingActivity.kt
@@ -83,11 +83,6 @@ class PinOnboardingActivity : BaseSimpleActivity(),
             setWindowFlag(true)
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            window.decorView.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setWindowFlag(false)
             window.statusBarColor =
