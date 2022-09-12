29fc570bdd0d0ee107483585ae5472e57fe92628
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-09-12 14:17:40 +0700

    Revert "rename for InitialSearchStateFragment.kt"
    This reverts commit b5d01fad2a150801277c69c592573d4eb8563168.

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -125,6 +125,6 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return ApplinkConst.TokoFood.HOME
+        return ""
     }
 }
