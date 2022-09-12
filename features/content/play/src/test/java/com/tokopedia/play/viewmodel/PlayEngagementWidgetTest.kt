85c4708467e0d1c1ef9baa3708ebd39b149c137e
astidhiyaa <asti.anzaria@tokopedia.com>
2022-09-12 14:47:26 +0800

    feat: add ut

---
+++ features/content/play/src/test/java/com/tokopedia/play/viewmodel/PlayEngagementWidgetTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.play.viewmodel
+
+/**
+ * @author by astidhiyaa on 12/09/22
+ */
+class PlayEngagementWidgetTest {
+    //if only game exist
+    //if only voucher exist
+    //add each test for game
+    //check if voucher list for coupon not empty
+    //click voucher open coupon sheet
+    //close voucher
+}
