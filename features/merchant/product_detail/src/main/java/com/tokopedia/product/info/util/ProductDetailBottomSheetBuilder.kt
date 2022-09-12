72882d508d6981c92931c5948fa73a78f78838c3
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-09-12 13:41:41 +0700

    chore:pdp(catalog) shop note adjustment

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/info/util/ProductDetailBottomSheetBuilder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/info/util/ProductDetailBottomSheetBuilder.kt
@@ -5,10 +5,10 @@ import android.view.View
 import androidx.fragment.app.FragmentManager
 import com.tokopedia.localizationchooseaddress.ui.bottomsheet.ChooseAddressBottomSheet
 import com.tokopedia.product.detail.R
+import com.tokopedia.product.detail.databinding.BsProductShopNotesInfoBinding
 import com.tokopedia.product.detail.view.util.toDateId
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.unifycomponents.HtmlLinkHelper
-import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by Yehezkiel on 21/10/20
@@ -16,19 +16,26 @@ import com.tokopedia.unifyprinciples.Typography
 object ProductDetailBottomSheetBuilder {
     private const val PDP_TIME_PUKUL = "pukul"
     private const val SHIPPING_CHOOSE_ADDRESS_TAG = "SHIPPING_CHOOSE_ADDRESS_TAG"
+    private const val DATE_FORMATTER = "dd MMM yyyy, '${PDP_TIME_PUKUL}' HH:mm"
+    private val DESC_REPLACE_CHAR_REGEX = "(\r\n|\n)".toRegex()
+    private const val DESC_REPLACE_TO = "<br />"
 
     fun getShopNotesBottomSheet(context: Context, dateValue: String, descValue: String, titleValue: String): BottomSheetUnify {
         val bottomSheetUnify = BottomSheetUnify()
         val view = View.inflate(context, R.layout.bs_product_shop_notes_info, null)
+        val binding = BsProductShopNotesInfoBinding.bind(view)
 
         bottomSheetUnify.apply {
+            setChild(binding.root)
             setTitle(titleValue)
-            setChild(view)
-            val date = view.findViewById<Typography>(R.id.product_shop_notes_date)
-            val desc = view.findViewById<Typography>(R.id.product_shop_notes_desc)
+        }
 
-            date.text = dateValue toDateId ("dd MMM yyyy, '${PDP_TIME_PUKUL}' HH:mm")
-            desc.text = HtmlLinkHelper(context, descValue.replace("(\r\n|\n)".toRegex(), "<br />")).spannedString
+        binding.apply {
+            productShopNotesDate.text = dateValue toDateId DATE_FORMATTER
+            productShopNotesDesc.text = HtmlLinkHelper(
+                context,
+                descValue.replace(DESC_REPLACE_CHAR_REGEX, DESC_REPLACE_TO)
+            ).spannedString
         }
 
         return bottomSheetUnify
