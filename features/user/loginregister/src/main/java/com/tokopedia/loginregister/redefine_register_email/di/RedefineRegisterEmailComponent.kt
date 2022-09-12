ee8e98a69f9ce26c69873bafff2c38bc191023d2
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-09-12 09:56:36 +0700

    clean some code regarding migrate to navigation component

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/redefine_register_email/di/RedefineRegisterEmailComponent.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/redefine_register_email/di/RedefineRegisterEmailComponent.kt
@@ -2,10 +2,9 @@ package com.tokopedia.loginregister.redefine_register_email.di
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
-import com.tokopedia.loginregister.redefine_register_email.input_phone.view.activity.RedefineRegisterInputPhoneActivity
 import com.tokopedia.loginregister.redefine_register_email.register_email.view.fragment.RedefineRegisterEmailFragment
 import com.tokopedia.loginregister.redefine_register_email.input_phone.view.fragment.RedefineRegisterInputPhoneFragment
-import com.tokopedia.loginregister.redefine_register_email.register_email.view.activity.RedefineRegisterEmailActivity
+import com.tokopedia.loginregister.redefine_register_email.view.activity.RedefineRegisterEmailActivity
 import dagger.Component
 
 @ActivityScope
@@ -18,7 +17,6 @@ import dagger.Component
 )
 interface RedefineRegisterEmailComponent {
     fun inject(activity: RedefineRegisterEmailActivity)
-    fun inject(activity: RedefineRegisterInputPhoneActivity)
     fun inject(fragment: RedefineRegisterEmailFragment)
     fun inject(fragment: RedefineRegisterInputPhoneFragment)
 }
--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/redefine_register_email/view/activity/RedefineRegisterEmailActivity.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/redefine_register_email/view/activity/RedefineRegisterEmailActivity.kt
@@ -1,4 +1,4 @@
-package com.tokopedia.loginregister.redefine_register_email.input_phone.view.activity
+package com.tokopedia.loginregister.redefine_register_email.view.activity
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,15 +6,13 @@ import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
-import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.loginregister.R
 import com.tokopedia.loginregister.redefine_register_email.di.DaggerRedefineRegisterEmailComponent
 import com.tokopedia.loginregister.redefine_register_email.di.RedefineRegisterEmailComponent
-import com.tokopedia.loginregister.redefine_register_email.input_phone.view.fragment.RedefineRegisterInputPhoneFragment
 import com.tokopedia.loginregister.redefine_register_email.input_phone.view.viewmodel.RedefineRegisterViewModel
 import javax.inject.Inject
 
-class RedefineRegisterInputPhoneActivity : BaseSimpleActivity(),
-    HasComponent<RedefineRegisterEmailComponent> {
+class RedefineRegisterEmailActivity : BaseSimpleActivity(), HasComponent<RedefineRegisterEmailComponent> {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -27,14 +25,14 @@ class RedefineRegisterInputPhoneActivity : BaseSimpleActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         component.inject(this)
-        toolbar.hide()
+        supportActionBar?.elevation = 0f
     }
 
-    override fun getNewFragment(): Fragment{
-        val bundle = Bundle()
-        bundle.putAll(intent.extras)
-        return RedefineRegisterInputPhoneFragment.newInstance(bundle)
-    }
+    override fun getLayoutRes(): Int = R.layout.activity_redefine_register_email
+
+    override fun getParentViewResourceID(): Int = R.id.parent_view
+
+    override fun getNewFragment(): Fragment? = null
 
     override fun getComponent(): RedefineRegisterEmailComponent {
         return DaggerRedefineRegisterEmailComponent.builder()
