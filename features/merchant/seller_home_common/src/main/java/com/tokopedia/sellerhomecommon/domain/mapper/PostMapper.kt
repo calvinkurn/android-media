3bf91b2d7678282aa8fcc18619293e30f0638d9f
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-09-12 14:56:18 +0700

    bug fix post list widget

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/PostMapper.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/mapper/PostMapper.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.sellerhomecommon.domain.mapper
 
-import com.tokopedia.kotlin.extensions.orFalse
+import com.tokopedia.kotlin.extensions.view.ONE
+import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.sellerhomecommon.data.WidgetLastUpdatedSharedPrefInterface
 import com.tokopedia.sellerhomecommon.domain.model.DataKeyModel
 import com.tokopedia.sellerhomecommon.domain.model.GetPostDataResponse
@@ -31,20 +32,20 @@ class PostMapper @Inject constructor(
         response: GetPostDataResponse,
         isFromCache: Boolean
     ): List<PostListDataUiModel> {
-        return response.getPostWidgetData?.data.orEmpty().mapIndexed { i, post ->
+        return response.getPostWidgetData.data.mapIndexed { i, post ->
             val maxDisplay = dataKeys.getOrNull(i)?.maxDisplay ?: MAX_ITEM_PER_PAGE
             PostListDataUiModel(
-                dataKey = post.dataKey.orEmpty(),
-                postPagers = getPostPagers(post.list.orEmpty(), post.emphasizeType, maxDisplay),
+                dataKey = post.dataKey,
+                postPagers = getPostPagers(post.list, post.emphasizeType, maxDisplay),
                 cta = PostCtaDataUiModel(
-                    text = post.cta?.text.orEmpty(),
-                    appLink = post.cta?.appLink.orEmpty()
+                    text = post.cta.text,
+                    appLink = post.cta.appLink
                 ),
-                error = post.error.orEmpty(),
+                error = post.error,
                 isFromCache = isFromCache,
-                showWidget = post.showWidget.orFalse(),
+                showWidget = post.showWidget,
                 emphasizeType = post.emphasizeType ?: PostListDataUiModel.IMAGE_EMPHASIZED,
-                lastUpdated = getLastUpdatedMillis(post.dataKey.orEmpty(), isFromCache)
+                lastUpdated = getLastUpdatedMillis(post.dataKey, isFromCache)
             )
         }
     }
@@ -58,7 +59,7 @@ class PostMapper @Inject constructor(
         emphasizeType: Int?,
         maxDisplay: Int
     ): List<PostListPagerUiModel> {
-        val maxItemPerPage = if (maxDisplay == 0) {
+        val maxItemPerPage = if (maxDisplay == Int.ZERO) {
             MAX_ITEM_PER_PAGE
         } else {
             maxDisplay
@@ -78,25 +79,26 @@ class PostMapper @Inject constructor(
                 when (emphasizeType) {
                     PostListDataUiModel.TEXT_EMPHASIZED -> {
                         PostItemUiModel.PostTextEmphasizedUiModel(
-                            title = postItem.title.orEmpty(),
-                            appLink = postItem.appLink.orEmpty(),
-                            url = postItem.url.orEmpty(),
-                            featuredMediaUrl = postItem.featuredMediaURL.orEmpty(),
-                            subtitle = postItem.subtitle.orEmpty(),
+                            title = postItem.title,
+                            appLink = postItem.appLink,
+                            url = postItem.url,
+                            featuredMediaUrl = postItem.featuredMediaURL,
+                            subtitle = postItem.subtitle,
                             textEmphasizeType = PostListDataUiModel.TEXT_EMPHASIZED,
-                            stateText = postItem.stateText.orEmpty(),
-                            stateMediaUrl = postItem.stateMediaUrl.orEmpty(),
-                            shouldShowUnderLine = index != postList.size.minus(1),
-                            isPinned = postItem.isPinned
+                            stateText = postItem.stateText,
+                            stateMediaUrl = postItem.stateMediaUrl,
+                            shouldShowUnderLine = index != postList.size.minus(Int.ONE),
+                            isPinned = postItem.isPinned,
+                            postItemId = postItem.postItemID
                         )
                     }
                     else -> {
                         PostItemUiModel.PostImageEmphasizedUiModel(
-                            title = postItem.title.orEmpty(),
-                            appLink = postItem.appLink.orEmpty(),
-                            url = postItem.url.orEmpty(),
-                            featuredMediaUrl = postItem.featuredMediaURL.orEmpty(),
-                            subtitle = postItem.subtitle.orEmpty(),
+                            title = postItem.title,
+                            appLink = postItem.appLink,
+                            url = postItem.url,
+                            featuredMediaUrl = postItem.featuredMediaURL,
+                            subtitle = postItem.subtitle,
                             textEmphasizeType = PostListDataUiModel.IMAGE_EMPHASIZED,
                             isPinned = postItem.isPinned
                         )
--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/usecase/GetPostDataUseCase.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/domain/usecase/GetPostDataUseCase.kt
@@ -70,6 +70,7 @@ class GetPostDataUseCase(
                     stateMediaURL
                     stateText
                     pinned
+                    postItemID
                   }
                   cta{
                     text
