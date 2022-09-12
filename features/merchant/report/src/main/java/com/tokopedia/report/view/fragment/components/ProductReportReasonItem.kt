b7f44984d066998bf7aa405c8f96c186a6cfd93f
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-09-12 22:52:32 +0700

    improve reason item compose

--- features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonItem.kt
+++ features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonItem.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,58 +37,72 @@ fun ProductReportReasonItem(
     reason: ProductReportReason,
     onClick: (ProductReportReason) -> Unit
 ) {
-    Row(
+    Column(
         modifier = Modifier
-            .wrapContentHeight()
             .clickable {
                 onClick.invoke(reason)
             }
     ) {
-        Column(
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            TextUnify(
+            Column(
                 modifier = Modifier
-                    .padding(16.dp),
-                type = TextUnifyType.Body2,
-                weight = TextUnifyWeight.Bold,
-                properties = { context ->
-                    val color = ContextCompat.getColor(
-                        context,
-                        com.tokopedia.unifyprinciples.R.color.Unify_N700_96
-                    )
-                    setTextColor(color)
-                    text = reason.value
-                }
-            )
-
-            if (reason.detail.isNotBlank()) {
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(2.dp)
+            ) {
                 TextUnify(
-                    modifier = Modifier
-                        .padding(16.dp),
-                    type = TextUnifyType.Body3,
+                    modifier = Modifier,
+                    type = TextUnifyType.Body2,
+                    weight = TextUnifyWeight.Bold,
                     properties = { context ->
                         val color = ContextCompat.getColor(
                             context,
-                            com.tokopedia.unifyprinciples.R.color.Unify_N700_68
+                            com.tokopedia.unifyprinciples.R.color.Unify_N700_96
                         )
                         setTextColor(color)
-                        text = reason.detail
-                        maxLines = 2
-                        ellipsize = TextUtils.TruncateAt.END
+                        text = reason.value
                     }
                 )
+
+                if (reason.detail.isNotBlank()) {
+                    TextUnify(
+                        modifier = Modifier,
+                        type = TextUnifyType.Body3,
+                        properties = { context ->
+                            val color = ContextCompat.getColor(
+                                context,
+                                com.tokopedia.unifyprinciples.R.color.Unify_N700_68
+                            )
+                            setTextColor(color)
+                            text = reason.detail
+                            maxLines = 2
+                            ellipsize = TextUtils.TruncateAt.END
+                        }
+                    )
+                }
             }
+            
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Icon(
+                modifier = Modifier
+                    .width(16.dp)
+                    .wrapContentHeight(),
+                painter = painterResource(id = com.tokopedia.design.R.drawable.ic_arrow_right_grey),
+                tint = Color(com.tokopedia.design.R.color.divider_color),
+                contentDescription = "ic_button_${reason.strLabel}"
+            )
         }
 
-        Icon(
+        Divider(
             modifier = Modifier
-                .width(24.dp)
-                .height(24.dp),
-            painter = painterResource(id = com.tokopedia.design.R.drawable.ic_arrow_right_grey),
-            contentDescription = "icn_button_${reason.strLabel}"
+                .padding(horizontal = 16.dp),
+            color = colorResource(id = com.tokopedia.design.R.color.dividerunify_background)
         )
     }
 }
@@ -97,7 +116,7 @@ fun ProductReportReasonItem() {
             children = emptyList(),
             additionalInfo = emptyList(),
             detail = "adsf",
-            value = "adf"
+            value = "vakue ajajajjaaaa"
         )
     ) {
 
