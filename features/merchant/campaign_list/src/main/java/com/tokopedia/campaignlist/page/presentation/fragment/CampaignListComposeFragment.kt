c174c8110f1e81ac6afc3dbf2336c5677728423c
fajarca <fajar.azhar@tokopedia.com>
2022-09-14 16:18:33 +0700

    Display data

--- features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/fragment/CampaignListComposeFragment.kt
+++ features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/fragment/CampaignListComposeFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.lifecycle.ViewTreeViewModelStoreOwner
@@ -19,8 +25,10 @@ import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.campaignlist.R
 import com.tokopedia.campaignlist.common.analytics.CampaignListTracker
+import com.tokopedia.campaignlist.common.data.model.response.GetCampaignListV2Response
 import com.tokopedia.campaignlist.common.di.DaggerCampaignListComponent
 import com.tokopedia.campaignlist.page.presentation.viewmodel.CampaignListViewModel
+import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
@@ -63,19 +71,45 @@ class CampaignListComposeFragment : BaseDaggerFragment() {
                               savedInstanceState: Bundle?): View {
         initViewTreeOwners()
 
+        viewModel.getCampaignList()
         return ComposeView(requireContext()).apply {
             setContent {
-
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    Greeting(name = "Hi")
-                }
+                CampaignListScreen()
             }
         }
     }
 
     @Composable
+    fun CampaignListScreen() {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            List(viewModel = viewModel)
+        }
+    }
+
+    @Composable
+    fun List(viewModel: CampaignListViewModel) {
+        val data = viewModel.getCampaignListResult.observeAsState()
+        if (data.value is Success) {
+            val items = (data.value as Success<GetCampaignListV2Response>).data.getCampaignListV2.campaignList
+            LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)){
+                items(items) {
+                    CampaignItem(name = it.campaignName)
+                }
+            }
+        }
+
+    }
+
+    @Composable
     fun Greeting(name: String) {
         Text(text = "Hello $name!")
+    }
+
+    @Composable
+    fun CampaignItem(name: String) {
+        Card {
+            Text(text = "Hello $name!")
+        }
     }
 
     private fun initViewTreeOwners() {
