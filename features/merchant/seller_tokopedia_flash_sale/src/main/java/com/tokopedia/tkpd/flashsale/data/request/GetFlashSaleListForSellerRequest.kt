c9afff915d452f515243f5f2ed458c52abcb05f0
faisalramd <faisal.ramdani@tokopedia.com>
2022-09-15 02:54:09 +0700

    fix request

--- features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/data/request/GetFlashSaleListForSellerRequest.kt
+++ features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/data/request/GetFlashSaleListForSellerRequest.kt
@@ -14,7 +14,11 @@ data class GetFlashSaleListForSellerRequest(
     @SerializedName("sort")
     val sort: Sort,
     @SerializedName("additional_params")
-    val additionalParams: AdditionalParam
+    val additionalParams: AdditionalParam,
+    @SerializedName("check_product_eligibility")
+    val checkProductEligibility: Boolean = true,
+    @SerializedName("matched_product_per_criteria")
+    val matchedProductPerCriteria: Boolean = true
 ) {
     data class Pagination(
         @SerializedName("rows")
@@ -42,11 +46,7 @@ data class GetFlashSaleListForSellerRequest(
     data class AdditionalParam(
         @SerializedName("highlight_recommendation")
         val highlightRecommendation: Boolean = false,
-        @SerializedName("matched_product_per_criteria")
-        val matchedProductPerCriteria: Boolean = false,
         @SerializedName("product_meta")
-        val productMeta: Boolean = false,
-        @SerializedName("check_product_eligibility")
-        val checkProductEligibility: Boolean = false
+        val productMeta: Boolean = false
     )
 }
