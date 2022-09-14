13410b055da2c2225d84ff0d7d6eb7f81a9e65c5
faisalramd <faisal.ramdani@tokopedia.com>
2022-09-14 10:41:21 +0700

    fix checking checkbox product list issue

---
+++ features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/manageproduct/mapper/BulkApplyMapper.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.tkpd.flashsale.presentation.manageproduct.mapper
+
+import com.tokopedia.campaign.components.bottomsheet.bulkapply.data.uimodel.ProductBulkApplyResult
+import com.tokopedia.campaign.components.bottomsheet.bulkapply.data.uimodel.ProductBulkApplyUiModel
+import com.tokopedia.tkpd.flashsale.domain.entity.ReservedProduct
+
+object BulkApplyMapper {
+
+    fun mapBulkResultToProduct(result: ProductBulkApplyResult): ReservedProduct.Product {
+        // TODO: @Darren complete this
+    }
+
+    fun mapProductToBulkParam(product: ReservedProduct.Product): ProductBulkApplyUiModel {
+        // TODO: @Darren complete this
+    }
+
+}
