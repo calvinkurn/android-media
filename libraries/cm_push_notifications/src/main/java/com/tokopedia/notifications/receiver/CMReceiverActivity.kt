2c3649acf7ed974a60768353baa3d259c1790561
architpandey <archit.pandey@tokopedia.com>
2022-09-15 10:55:46 +0530

    Notification trampoline changes.

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/receiver/CMReceiverActivity.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/receiver/CMReceiverActivity.kt
@@ -9,5 +9,6 @@ class CMReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cmreceiver)
+        CMNotificationHandler.instance.handleIntent(this, intent)
     }
 }
