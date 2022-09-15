a6b7aa96b40ac0afc78ba48bec2a6287fc34caf8
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2022-09-15 22:25:39 +0700

    Implement partial binding on BOM detail resolution widget

--- features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/OrderResolutionViewHolder.kt
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/OrderResolutionViewHolder.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.buyerorderdetail.presentation.adapter.viewholder
 
+import android.animation.LayoutTransition
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.buyerorderdetail.R
@@ -30,8 +31,56 @@ class OrderResolutionViewHolder(
             bindResolutionTitle(uiModel.title)
             bindResolutionStatus(uiModel.status, uiModel.resolutionStatusFontColor)
             bindResolutionDescription(uiModel.description.parseAsHtml())
-            bindResolutionDeadline(uiModel.deadlineDateTime, uiModel.backgroundColor, uiModel.showDeadline)
+            bindResolutionDeadline(
+                uiModel.deadlineDateTime,
+                uiModel.backgroundColor,
+                uiModel.showDeadline
+            )
             bindListener(uiModel.redirectPath)
+        }
+    }
+
+    override fun bind(element: OrderResolutionUIModel, payloads: MutableList<Any>) {
+        binding?.run {
+            payloads.firstOrNull()?.let {
+                if (it is Pair<*, *>) {
+                    val (oldItem, newItem) = it
+                    if (oldItem is OrderResolutionUIModel && newItem is OrderResolutionUIModel) {
+                        root.layoutTransition?.enableTransitionType(LayoutTransition.CHANGING)
+                        if (oldItem.picture != newItem.picture) {
+                            bindResolutionIcon(newItem.picture)
+                        }
+                        if (oldItem.title != newItem.title) {
+                            bindResolutionTitle(newItem.title)
+                        }
+                        if (
+                            oldItem.status != newItem.status ||
+                            oldItem.resolutionStatusFontColor != newItem.resolutionStatusFontColor
+                        ) {
+                            bindResolutionStatus(newItem.status, newItem.resolutionStatusFontColor)
+                        }
+                        if (oldItem.description != newItem.description) {
+                            bindResolutionDescription(newItem.description.parseAsHtml())
+                        }
+                        if (
+                            oldItem.deadlineDateTime != newItem.deadlineDateTime ||
+                            oldItem.backgroundColor != newItem.backgroundColor ||
+                            oldItem.showDeadline != newItem.showDeadline
+                        ) {
+                            bindResolutionDeadline(
+                                newItem.deadlineDateTime,
+                                newItem.backgroundColor,
+                                newItem.showDeadline
+                            )
+                        }
+                        if (oldItem.redirectPath != newItem.redirectPath) {
+                            bindListener(newItem.redirectPath)
+                        }
+                        root.layoutTransition?.disableTransitionType(LayoutTransition.CHANGING)
+                        return
+                    }
+                }
+            }
         }
     }
 
@@ -43,7 +92,10 @@ class OrderResolutionViewHolder(
         tvTitle.text = title
     }
 
-    private fun ItemBuyerOrderDetailResolutionBinding.bindResolutionStatus(status: String, fontColor: String) {
+    private fun ItemBuyerOrderDetailResolutionBinding.bindResolutionStatus(
+        status: String,
+        fontColor: String
+    ) {
         val unifyColor = stringToUnifyColor(root.context, fontColor)
         tvStatus.text = status
         tvStatus.setTextColor(unifyColor.unifyColor ?: unifyColor.defaultColor)
