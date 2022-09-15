901679b57756e35902d305d3cc3ecc7dd2349ae1
shivansh <shivansh.srivastava@tokopedia.com>
2022-09-16 00:19:16 +0530

    Design Modifications

--- features/category/catalog/src/main/java/com/tokopedia/catalog/adapter/components/CatalogComparisonAccordionAdapter.kt
+++ features/category/catalog/src/main/java/com/tokopedia/catalog/adapter/components/CatalogComparisonAccordionAdapter.kt
@@ -2,10 +2,12 @@ package com.tokopedia.catalog.adapter.components
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.catalog.R
 import com.tokopedia.catalog.model.raw.ComponentData
 import com.tokopedia.catalog.viewholder.components.CatalogAccordionItemViewHolder
+import com.tokopedia.unifyprinciples.Typography
 
 class CatalogComparisonAccordionAdapter(var subcard: ArrayList<ComponentData.SpecList.Subcard>):
     RecyclerView.Adapter<CatalogAccordionItemViewHolder>() {
@@ -21,6 +23,19 @@ class CatalogComparisonAccordionAdapter(var subcard: ArrayList<ComponentData.Spe
 
     override fun onBindViewHolder(holder: CatalogAccordionItemViewHolder, position: Int) {
         holder.bind(subcard[position])
+
+        when {
+            subcard.size == 1 -> {
+                holder.view.findViewById<Typography>(R.id.subcategory_title).setBackgroundResource(R.drawable.catalog_subtitle_background)
+                holder.view.findViewById<LinearLayout>(R.id.comparison_content_layout).setBackgroundResource(R.drawable.catalog_content_border)
+            }
+            position == 0 -> {
+                holder.view.findViewById<Typography>(R.id.subcategory_title).setBackgroundResource(R.drawable.catalog_subtitle_background)
+            }
+            position == subcard.size-1 -> {
+                holder.view.findViewById<LinearLayout>(R.id.comparison_content_layout).setBackgroundResource(R.drawable.catalog_content_border)
+            }
+        }
     }
 
     override fun getItemCount() = subcard.size
