7109f50ae513c39656ee172f1be99340bb718894
edwinnrw <101694276+edwinnrw@users.noreply.github.com>
2022-09-16 10:30:48 +0700

    Update features/merchant/seller_menu/src/main/java/com/tokopedia/seller/menu/presentation/viewmodel/SellerSettingViewModel.kt
    Co-authored-by: sonoda-bot <57702427+sonoda-bot@users.noreply.github.com>

--- features/merchant/seller_menu/src/main/java/com/tokopedia/seller/menu/presentation/viewmodel/SellerSettingViewModel.kt
+++ features/merchant/seller_menu/src/main/java/com/tokopedia/seller/menu/presentation/viewmodel/SellerSettingViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
-import com.tokopedia.kotlin.extensions.view.toLongOrZero
 import com.tokopedia.logisticCommon.domain.usecase.ShopMultilocWhitelistUseCase
 import com.tokopedia.seller.menu.domain.usecase.GetShopScoreLevelUseCase
 import com.tokopedia.usecase.coroutines.Fail
