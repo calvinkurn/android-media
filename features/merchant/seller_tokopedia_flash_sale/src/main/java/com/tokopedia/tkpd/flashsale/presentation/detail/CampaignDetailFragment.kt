acf4b79ec52c7835002cf99d1e2307bfb421f7b0
Yuda <yuda.putra@tokopedia.com>
2022-09-16 10:36:26 +0700

    Fix CDP findings about date and wording

--- features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/detail/CampaignDetailFragment.kt
+++ features/merchant/seller_tokopedia_flash_sale/src/main/java/com/tokopedia/tkpd/flashsale/presentation/detail/CampaignDetailFragment.kt
@@ -12,14 +12,12 @@ import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.campaign.components.adapter.CompositeAdapter
-import com.tokopedia.campaign.delegates.HasPaginatedList
 import com.tokopedia.campaign.utils.constant.DateConstant.DATE_MONTH_ONLY
 import com.tokopedia.campaign.utils.constant.DateConstant.DATE_TIME_SECOND_PRECISION_WITH_TIMEZONE_ID_FORMAT
+import com.tokopedia.campaign.utils.constant.DateConstant.DATE_YEAR_PRECISION
 import com.tokopedia.campaign.utils.constant.DateConstant.TIME_MINUTE_PRECISION_WITH_TIMEZONE
 import com.tokopedia.campaign.utils.constant.ImageUrlConstant
 import com.tokopedia.campaign.utils.extension.applyPaddingToLastItem
-import com.tokopedia.campaign.utils.extension.routeToUrl
-import com.tokopedia.header.HeaderUnify
 import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.tkpd.flashsale.common.extension.enablePaging
 import com.tokopedia.kotlin.extensions.view.*
@@ -43,10 +41,7 @@ import com.tokopedia.tkpd.flashsale.presentation.detail.adapter.registered.OnSel
 import com.tokopedia.tkpd.flashsale.presentation.detail.adapter.registered.WaitingForSelectionDelegateAdapter
 import com.tokopedia.tkpd.flashsale.presentation.detail.adapter.registered.item.WaitingForSelectionItem
 import com.tokopedia.tkpd.flashsale.presentation.detail.bottomsheet.CampaignDetailBottomSheet
-import com.tokopedia.tkpd.flashsale.presentation.detail.uimodel.TimelineStepModel
 import com.tokopedia.tkpd.flashsale.presentation.list.child.adapter.LoadingDelegateAdapter
-import com.tokopedia.tkpd.flashsale.presentation.list.child.adapter.item.LoadingItem
-import com.tokopedia.tkpd.flashsale.presentation.list.container.FlashSaleContainerFragment
 import com.tokopedia.unifycomponents.timer.TimerUnifySingle
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
@@ -293,7 +288,7 @@ class CampaignDetailFragment : BaseDaggerFragment() {
             val startSubmissionDate = flashSale.submissionStartDateUnix.formatTo(
                 DATE_MONTH_ONLY
             )
-            val endSubmissionDate = flashSale.submissionEndDateUnix.formatTo(DATE_MONTH_ONLY)
+            val endSubmissionDate = flashSale.submissionEndDateUnix.formatTo(DATE_YEAR_PRECISION)
             tgRegisterPeriod.text = getString(
                 R.string.register_period_value_placeholder_2,
                 startSubmissionDate,
@@ -372,6 +367,7 @@ class CampaignDetailFragment : BaseDaggerFragment() {
                 getString(R.string.stfs_title_ticker_upcoming_cdp_registration_close_state)
             tickerHeader.setTextDescription(getString(R.string.stfs_description_ticker_upcoming_cdp_registration_close_state))
             tgCampaignStatus.text = getString(R.string.registration_closed_in_label)
+            timer.gone()
         }
     }
 
@@ -539,10 +535,10 @@ class CampaignDetailFragment : BaseDaggerFragment() {
 
     private fun setupChooseProductRedirection() {
         binding?.btnRegister?.setOnClickListener {
-            ChooseProductActivity.start(context?: return@setOnClickListener, flashSaleId)
+            ChooseProductActivity.start(context ?: return@setOnClickListener, flashSaleId)
         }
         upcomingCdpMidBinding?.btnCheckReason?.setOnClickListener {
-            ChooseProductActivity.start(context?: return@setOnClickListener, flashSaleId)
+            ChooseProductActivity.start(context ?: return@setOnClickListener, flashSaleId)
         }
     }
 
@@ -578,7 +574,10 @@ class CampaignDetailFragment : BaseDaggerFragment() {
                 getString(
                     R.string.product_eligible_for_selection_end_date_placeholder,
                     flashSale.reviewEndDateUnix.formatTo(
-                        DATE_TIME_SECOND_PRECISION_WITH_TIMEZONE_ID_FORMAT
+                        DATE_YEAR_PRECISION
+                    ),
+                    flashSale.reviewEndDateUnix.formatTo(
+                        TIME_MINUTE_PRECISION_WITH_TIMEZONE
                     )
                 )
             )
@@ -607,7 +606,10 @@ class CampaignDetailFragment : BaseDaggerFragment() {
                 getString(
                     R.string.product_eligible_start_date_placeholder,
                     flashSale.startDateUnix.formatTo(
-                        DATE_TIME_SECOND_PRECISION_WITH_TIMEZONE_ID_FORMAT
+                        DATE_YEAR_PRECISION
+                    ),
+                    flashSale.startDateUnix.formatTo(
+                        TIME_MINUTE_PRECISION_WITH_TIMEZONE
                     )
                 )
             )
