78a65217b8fb0086e34fd34f202d4459a12475d0
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-09-20 05:13:28 +0800

    `refactor`: change `sealed class` to `enum class`

--- features/content/content_common/src/main/java/com/tokopedia/content/common/ui/model/AccountStateInfo.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/ui/model/AccountStateInfo.kt
@@ -9,10 +9,6 @@ data class AccountStateInfo(
     val tnc: List<TermsAndConditionUiModel> = emptyList(),
 )
 
-sealed class AccountStateInfoType {
-    object Live : AccountStateInfoType()
-    object NoUsername : AccountStateInfoType()
-    object NotAcceptTNC : AccountStateInfoType()
-    object Banned : AccountStateInfoType()
-    object Unknown : AccountStateInfoType()
+enum class AccountStateInfoType {
+    Live, NoUsername, NotAcceptTNC, Banned, Unknown
 }
