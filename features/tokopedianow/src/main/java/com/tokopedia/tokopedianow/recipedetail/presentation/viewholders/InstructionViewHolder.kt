5c39af4d88eb2aef1a07255f5c73c1eec0af0fbe
rezagama <rezagamahidayat@gmail.com>
2022-09-20 15:03:48 +0700

    Fix format html

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/recipedetail/presentation/viewholders/InstructionViewHolder.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/recipedetail/presentation/viewholders/InstructionViewHolder.kt
@@ -2,10 +2,10 @@ package com.tokopedia.tokopedianow.recipedetail.presentation.viewholders
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.tokopedianow.R
 import com.tokopedia.tokopedianow.databinding.ItemTokopedianowRecipeInstructionBinding
 import com.tokopedia.tokopedianow.recipedetail.presentation.uimodel.InstructionUiModel
+import com.tokopedia.utils.htmltags.HtmlUtil
 import com.tokopedia.utils.view.binding.viewBinding
 
 class InstructionViewHolder(itemView: View) : AbstractViewHolder<InstructionUiModel>(itemView) {
@@ -17,6 +17,6 @@ class InstructionViewHolder(itemView: View) : AbstractViewHolder<InstructionUiMo
     private var binding: ItemTokopedianowRecipeInstructionBinding? by viewBinding()
 
     override fun bind(instruction: InstructionUiModel) {
-        binding?.textInstruction?.text = MethodChecker.fromHtml(instruction.htmlText)
+        binding?.textInstruction?.text = HtmlUtil.fromHtml(instruction.htmlText)
     }
 }
