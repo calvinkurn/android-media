9297aee2af5b3b725753b2ed53de8cd4fc2ad9bf
deepika <deepika.tripathi@tokopedia.com>
2022-09-20 13:58:37 +0530

    added chevron right icon , changed masking of card number format , subheading aligned with heading in success page

--- features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/helper/ThankExtension.kt
+++ features/payment/thankyou_native/src/main/java/com/tokopedia/thankyou_native/helper/ThankExtension.kt
@@ -4,6 +4,6 @@ import android.widget.LinearLayout
 
 fun String.getMaskedNumberSubStringPayment(): String {
     val LAST_NUMBERS = 4
-    val FOUR_CROSS = "XXXX "
+    val FOUR_CROSS = "\u00b7\u00b7\u00b7\u00b7 "
     return FOUR_CROSS + this.substring(this.length - LAST_NUMBERS)
 }
