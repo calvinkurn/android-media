49e3ca6584aff648a719970d0372b7395dfa9dfb
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-09-21 14:42:07 +0700

    `fix` remove unnecessary comment

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusContainerFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusContainerFragment.kt
@@ -694,7 +694,6 @@ class FeedPlusContainerFragment : BaseDaggerFragment(), FragmentListener, AllNot
 
         ivFeedUser.onUrlLoaded = { isSuccess ->
             if(!isSuccess) {
-                // Load Default Image
                 ivFeedUser.post {
                     ivFeedUser.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_user_profile_default))
                 }
