c2d0829b2fbf8395485d18aba7f3ec5801023368
reivinoktavianus <oktavianusreivin@gmail.com>
2022-09-21 16:34:37 +0700

    Add template_name

--- features/tokofood/src/main/java/com/tokopedia/tokofood/feature/search/searchresult/domain/usecase/TokofoodFilterSortUseCase.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/search/searchresult/domain/usecase/TokofoodFilterSortUseCase.kt
@@ -16,6 +16,7 @@ private const val QUERY = """
             filter {
               title
               subtitle
+              template_name
               options {
                 name
                 description
