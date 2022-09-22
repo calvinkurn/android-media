49f87be1f966f253f1245236e68ceabe44c5dcd5
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-09-22 12:10:25 +0800

    `refactor`: revert adding new `combine`

--- features/content/play_common/src/main/java/com/tokopedia/play_common/util/extension/Combine.kt
+++ features/content/play_common/src/main/java/com/tokopedia/play_common/util/extension/Combine.kt
@@ -6,23 +6,6 @@ import kotlinx.coroutines.flow.Flow
  * Created by kenny.hadisaputra on 15/02/22
  */
 @Suppress("MagicNumber")
-fun <T1, T2, T3, T4, R> combine(
-    flow: Flow<T1>,
-    flow2: Flow<T2>,
-    flow3: Flow<T3>,
-    flow4: Flow<T4>,
-    transform: suspend (T1, T2, T3, T4) -> R
-): Flow<R> =
-    kotlinx.coroutines.flow.combine(flow, flow2, flow3, flow4) { args: Array<*> ->
-        transform(
-            args[0] as T1,
-            args[1] as T2,
-            args[2] as T3,
-            args[3] as T4,
-        )
-    }
-
-@Suppress("MagicNumber")
 fun <T1, T2, T3, T4, T5, T6, R> combine(
     flow: Flow<T1>,
     flow2: Flow<T2>,
