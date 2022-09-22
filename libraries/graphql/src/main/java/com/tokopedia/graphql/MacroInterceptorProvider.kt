e1805f4b0213fe3c08077d055d92bb8407d7c9d2
devarafikry <devara.akmal@tokopedia.com>
2022-09-22 19:50:44 +0700

    prevent spread operator

--- libraries/graphql/src/main/java/com/tokopedia/graphql/MacroInterceptorProvider.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/MacroInterceptorProvider.kt
@@ -8,11 +8,7 @@ object MacroInterceptorProvider {
         val constructor = kClass.constructors
             .minByOrNull { it.parameters.size }
         constructor?.let {
-            val arguments = constructor?.parameters
-                .map { context as Context }
-                .toTypedArray()
-            return it.call(*arguments)
-
+            return it.call(context)
         }
         return null
     }
