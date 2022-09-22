9bb64c4db5d4e308a70f43af8bb25660ff2ceb05
devarafikry <devara.akmal@tokopedia.com>
2022-09-22 15:38:03 +0700

    fix macrobenchmark app can't be compiled

--- libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/util/MacroIntent.kt
+++ libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/util/MacroIntent.kt
@@ -3,8 +3,10 @@ package com.tkpd.macrobenchmark.util
 import android.content.Intent
 import android.net.Uri
 import androidx.test.platform.app.InstrumentationRegistry
+import com.tokopedia.macrobenchmark_util.env.mock.InstrumentationMockHelper.getRawString
 import com.tokopedia.macrobenchmark_util.env.mock.config.HomeMockResponseConfig
 import com.tokopedia.macrobenchmark_util.env.mock.config.SearchMockResponseConfig
+import com.tkpd.macrobenchmark.test.R
 
 fun Intent.putMockData(key: String, res: Int) {
     this.putExtra(key, getRawString(
