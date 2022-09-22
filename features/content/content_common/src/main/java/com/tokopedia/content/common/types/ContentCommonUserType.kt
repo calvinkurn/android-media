a36ba04fb5232bb9e1e0bdac040198c7dd72484d
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-09-22 22:27:18 +0800

    `feature`: add TYPE_NAME_SELLER and TYPE_NAME_USER

--- features/content/content_common/src/main/java/com/tokopedia/content/common/types/ContentCommonUserType.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/types/ContentCommonUserType.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.content.common.types
 
 object ContentCommonUserType {
+    const val TYPE_NAME_SELLER = "seller"
+    const val TYPE_NAME_USER = "user"
     const val TYPE_USER = "content-user"
     const val TYPE_SHOP = "content-shop"
     const val VALUE_TYPE_ID_SHOP = 2
