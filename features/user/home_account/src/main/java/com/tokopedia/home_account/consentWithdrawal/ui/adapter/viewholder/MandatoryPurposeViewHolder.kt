912e2140b6cabd4a53671dff481c31385dcf642c
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-09-23 10:15:19 +0800

    add new module package for consent withdrawal page

---
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/consentWithdrawal/ui/adapter/viewholder/MandatoryPurposeViewHolder.kt
@@ -0,0 +1,39 @@
+package com.tokopedia.home_account.consentWithdrawal.ui.adapter.viewholder
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.tokopedia.adapterdelegate.BaseViewHolder
+import com.tokopedia.home_account.R
+import com.tokopedia.home_account.consentWithdrawal.common.TransactionType
+import com.tokopedia.home_account.consentWithdrawal.data.ConsentPurposeItemDataModel
+import com.tokopedia.home_account.consentWithdrawal.ui.ConsentWithdrawalListener
+import com.tokopedia.home_account.databinding.ViewItemConsentWithdrawalPurposeMandatoryBinding
+import com.tokopedia.utils.view.binding.noreflection.viewBinding
+
+class MandatoryPurposeViewHolder(
+    itemView: View,
+    private val listener: ConsentWithdrawalListener.Mandatory
+) : BaseViewHolder(itemView) {
+
+    private val itemViewBinding by viewBinding(ViewItemConsentWithdrawalPurposeMandatoryBinding::bind)
+
+    fun onBind(item: ConsentPurposeItemDataModel) {
+        itemViewBinding?.apply {
+            val isActive = item.consentStatus == TransactionType.OPT_IN.alias
+            itemTitle.text = item.consentTitle
+            itemDesc.text = item.consentSubtitle
+            itemTextButton.text = if (isActive) TEXT_ACTIVE else TEXT_NON_ACTIVE
+            itemButtonLayout.setOnClickListener {
+                listener.onActivationButtonClicked(layoutPosition, isActive, item)
+            }
+        }
+    }
+
+    companion object {
+        private const val TEXT_ACTIVE = "Aktif"
+        private const val TEXT_NON_ACTIVE = "Nonaktif"
+
+        @LayoutRes
+        val LAYOUT = R.layout.view_item_consent_withdrawal_purpose_mandatory
+    }
+}
