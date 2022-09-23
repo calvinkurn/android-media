fdacc7057389e15df9af09012b5fa6418d51b037
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-09-23 10:16:27 +0800

    change old consent menu to the new one

--- features/user/home_account/src/main/java/com/tokopedia/home_account/privacy_account/viewmodel/PrivacyAccountViewModel.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/privacy_account/viewmodel/PrivacyAccountViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.home_account.consentWithdrawal.data.ConsentGroupListDataModel
+import com.tokopedia.home_account.consentWithdrawal.domain.GetConsentGroupListUseCase
 import com.tokopedia.home_account.privacy_account.data.DataSetConsent
 import com.tokopedia.home_account.privacy_account.data.LinkStatusResponse
 import com.tokopedia.home_account.privacy_account.domain.GetConsentSocialNetworkUseCase
@@ -12,6 +14,7 @@ import com.tokopedia.home_account.privacy_account.domain.GetLinkStatusUseCase
 import com.tokopedia.home_account.privacy_account.domain.GetUserProfile
 import com.tokopedia.home_account.privacy_account.domain.SetConsentSocialNetworkUseCase
 import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
+import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Result
 import com.tokopedia.usecase.coroutines.Success
@@ -27,6 +30,7 @@ class PrivacyAccountViewModel @Inject constructor(
     private val getProfileUseCase: GetUserProfile,
     private val getConsentSocialNetworkUseCase: GetConsentSocialNetworkUseCase,
     private val setConsentSocialNetworkUseCase: SetConsentSocialNetworkUseCase,
+    private val getConsentGroupListUseCase: GetConsentGroupListUseCase,
     private val userSession: UserSessionInterface,
     dispatcher: CoroutineDispatchers
 ): BaseViewModel(dispatcher.main), LifecycleObserver {
@@ -39,6 +43,10 @@ class PrivacyAccountViewModel @Inject constructor(
 
     private val _setConsentSocialNetwork = MutableLiveData<Result<DataSetConsent>>()
     val setConsentSocialNetwork: LiveData<Result<DataSetConsent>> get() = _setConsentSocialNetwork
+
+    private val _getConsentGroupList = MutableLiveData<Result<ConsentGroupListDataModel>>()
+    val getConsentGroupList: LiveData<Result<ConsentGroupListDataModel>>
+        get() = _getConsentGroupList
 
     fun getLinkStatus(isGetProfile: Boolean = false) {
         launchCatchError(block = {
@@ -73,6 +81,19 @@ class PrivacyAccountViewModel @Inject constructor(
             _setConsentSocialNetwork.value = Success(response.socialNetworkSetConsent.data)
         }) {
             _setConsentSocialNetwork.value = Fail(it)
+        }
+    }
+
+    fun getConsentGroupList() {
+        launchCatchError(coroutineContext, {
+            val response = getConsentGroupListUseCase(Unit)
+            if (response.consentGroupList.success) {
+                _getConsentGroupList.value = Success(response.consentGroupList)
+            } else {
+                _getConsentGroupList.value = Fail(MessageErrorException(response.consentGroupList.errorMessages.toString()))
+            }
+        }) {
+            _getConsentGroupList.value = Fail(it)
         }
     }
 }
