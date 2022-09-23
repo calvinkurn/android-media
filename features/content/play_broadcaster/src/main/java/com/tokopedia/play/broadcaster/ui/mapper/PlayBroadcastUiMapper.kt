c71a21d04e65bab3073fa4788ab482572583974f
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-09-23 17:28:10 +0800

    add check for quiz isActive

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/mapper/PlayBroadcastUiMapper.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/mapper/PlayBroadcastUiMapper.kt
@@ -330,7 +330,7 @@ class PlayBroadcastUiMapper @Inject constructor(
                 },
             ),
             quizConfig = QuizConfigUiModel(
-                isActive = response.interactiveConfig.quizConfig.isActive,
+                isActive = if (authorType == TYPE_SHOP) response.interactiveConfig.tapTapConfig.isActive else false,
                 isGiftActive = authorType == TYPE_SHOP,
                 maxTitleLength = response.interactiveConfig.quizConfig.maxTitleLength,
                 maxChoicesCount = response.interactiveConfig.quizConfig.maxChoicesCount,
