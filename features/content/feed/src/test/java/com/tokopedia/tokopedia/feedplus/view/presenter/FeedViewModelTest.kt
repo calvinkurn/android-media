b82edcaf7b1f112f7fdb4e471390c3043b8b245b
astidhiyaa <asti.anzaria@tokopedia.com>
2022-09-24 14:26:54 +0800

    test: feedvm

--- features/content/feed/src/test/java/com/tokopedia/tokopedia/feedplus/view/presenter/FeedViewModelTest.kt
+++ features/content/feed/src/test/java/com/tokopedia/tokopedia/feedplus/view/presenter/FeedViewModelTest.kt
@@ -1,9 +1,24 @@
 package com.tokopedia.tokopedia.feedplus.view.presenter
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.affiliatecommon.data.pojo.submitpost.response.SubmitPostData
+import com.tokopedia.feedcomponent.analytics.topadstracker.SendTopAdsUseCase
+import com.tokopedia.feedcomponent.data.pojo.FeedXTrackViewerResponse
+import com.tokopedia.feedcomponent.data.pojo.VisitChannelTracking
+import com.tokopedia.feedcomponent.domain.usecase.FeedBroadcastTrackerUseCase
+import com.tokopedia.feedcomponent.domain.usecase.FeedXTrackViewerUseCase
+import com.tokopedia.feedcomponent.util.CustomUiMessageThrowable
 import com.tokopedia.feedcomponent.view.viewmodel.responsemodel.DeletePostViewModel
+import com.tokopedia.feedplus.R
+import com.tokopedia.kolcommon.data.SubmitActionContentResponse
 import com.tokopedia.kolcommon.data.SubmitReportContentResponse
+import com.tokopedia.kolcommon.data.pojo.like.LikeKolPostData
+import com.tokopedia.kolcommon.domain.interactor.SubmitActionContentUseCase
+import com.tokopedia.kolcommon.domain.interactor.SubmitLikeContentUseCase
 import com.tokopedia.kolcommon.domain.interactor.SubmitReportContentUseCase
+import com.tokopedia.kolcommon.view.viewmodel.LikeKolViewModel
+import com.tokopedia.kolcommon.view.viewmodel.ViewsKolModel
+import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.tokopedia.feedplus.helper.assertEqualTo
 import com.tokopedia.tokopedia.feedplus.helper.assertTrue
 import com.tokopedia.tokopedia.feedplus.helper.assertType
@@ -14,6 +29,7 @@ import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.coEvery
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Rule
 import org.junit.Test
@@ -31,7 +47,17 @@ class FeedViewModelTest {
     private val testDispatcher = coroutineTestRule.dispatchers
 
     private val mockReport: SubmitReportContentUseCase = mockk(relaxed = true)
+    private val mockTrackChannel: FeedBroadcastTrackerUseCase = mockk(relaxed = true)
+    private val mockTrackViewer: FeedXTrackViewerUseCase = mockk(relaxed = true)
+    private val mockLike: SubmitLikeContentUseCase = mockk(relaxed = true)
+    private val mockDelete: SubmitActionContentUseCase = mockk(relaxed = true)
+    private val mockTopAdsTracker: SendTopAdsUseCase = mockk(relaxed = true)
 
+    private val gqlFailed = MessageErrorException("ooPs")
+
+    /**
+     * Send Report
+     */
     @Test
     fun `send report - success`() {
         val expected = SubmitReportContentResponse(
@@ -45,15 +71,16 @@ class FeedViewModelTest {
         create(dispatcher = testDispatcher, sendReportUseCase = mockReport)
             .use {
                 it.vm.sendReport(1, 1, "", "")
-                it.vm.reportResponse.getOrAwaitValue().assertType<Success<DeletePostViewModel>> { dt ->
-                    dt.data.errorMessage.assertEqualTo(expected.content.errorMessage)
-                    dt.data.isSuccess.assertTrue()
-                }
+                it.vm.reportResponse.getOrAwaitValue()
+                    .assertType<Success<DeletePostViewModel>> { dt ->
+                        dt.data.errorMessage.assertEqualTo(expected.content.errorMessage)
+                        dt.data.isSuccess.assertTrue()
+                    }
             }
     }
 
     @Test
-    fun `send report - failed`() {
+    fun `send report - failed from response`() {
         val expected = SubmitReportContentResponse(
             content = SubmitReportContentResponse.FeedReportSubmit(
                 data = SubmitReportContentResponse.FeedReportData(success = 200),
@@ -70,5 +97,292 @@ class FeedViewModelTest {
                     dt.throwable.message.assertEqualTo(expected.content.errorMessage)
                 }
             }
+    }
+
+    @Test
+    fun `send report - failed throw from gql`() {
+        coEvery { mockReport.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, sendReportUseCase = mockReport)
+            .use {
+                it.vm.sendReport(1, 1, "", "")
+                it.vm.reportResponse.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * Track Visit Channel
+     */
+
+    @Test
+    fun `track visit channel - success`() {
+        val expected =
+            VisitChannelTracking.Response(reportVisitChannelTracking = VisitChannelTracking(success = true))
+
+        coEvery { mockTrackChannel.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, trackVisitChannelBroadcasterUseCase = mockTrackChannel)
+            .use {
+                it.vm.trackVisitChannel("", 1)
+                it.vm.viewTrackResponse.getOrAwaitValue().assertType<Success<ViewsKolModel>> { dt ->
+                    dt.data.isSuccess.assertTrue()
+                }
+            }
+    }
+
+    @Test
+    fun `track visit channel - failed throw from gql`() {
+        coEvery { mockTrackChannel.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, trackVisitChannelBroadcasterUseCase = mockTrackChannel)
+            .use {
+                it.vm.trackVisitChannel("", 1)
+                it.vm.viewTrackResponse.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * Track Long Video
+     */
+
+    @Test
+    fun `track long video - success`() {
+        val expected = FeedXTrackViewerResponse.Response(
+            feedXTrackViewerResponse = FeedXTrackViewerResponse(success = true)
+        )
+
+        coEvery { mockTrackViewer.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, feedXTrackViewerUseCase = mockTrackViewer)
+            .use {
+                it.vm.trackLongVideoView("", 1)
+                it.vm.longVideoViewTrackResponse.getOrAwaitValue()
+                    .assertType<Success<ViewsKolModel>> { dt ->
+                        dt.data.isSuccess.assertTrue()
+                    }
+            }
+    }
+
+    @Test
+    fun `track long video - failed throw from gql`() {
+        coEvery { mockTrackViewer.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, feedXTrackViewerUseCase = mockTrackViewer)
+            .use {
+                it.vm.trackLongVideoView("", 1)
+                it.vm.longVideoViewTrackResponse.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * Like KOL
+     */
+
+    @Test
+    fun `like kol - success`() {
+        val expected = LikeKolPostData(
+            doLikeKolPost = LikeKolPostData.DoLikeKolPost(
+                data = LikeKolPostData.LikeKolPostSuccessData(success = 1)
+            )
+        )
+
+        coEvery { mockLike.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doLikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue()
+                    .assertType<Success<LikeKolViewModel>> { dt ->
+                        dt.data.isSuccess.assertEqualTo(expected.doLikeKolPost.data.success == SubmitLikeContentUseCase.SUCCESS)
+                        dt.data.isSuccess.assertTrue()
+                    }
+            }
+    }
+
+    @Test
+    fun `like kol - failed from response`() {
+        val expected = LikeKolPostData(
+            doLikeKolPost = LikeKolPostData.DoLikeKolPost(
+                data = LikeKolPostData.LikeKolPostSuccessData(success = 200)
+            )
+        )
+
+        coEvery { mockLike.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doLikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertType<CustomUiMessageThrowable> { msg ->
+                        msg.errorMessageId.assertEqualTo(R.string.feed_like_error_message)
+                    }
+                }
+            }
+    }
+
+    @Test
+    fun `like kol - failed throw from gql`() {
+        coEvery { mockLike.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doLikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * UnLike KOL
+     */
+
+    @Test
+    fun `Unlike kol - success`() {
+        val expected = LikeKolPostData(
+            doLikeKolPost = LikeKolPostData.DoLikeKolPost(
+                data = LikeKolPostData.LikeKolPostSuccessData(success = 1)
+            )
+        )
+
+        coEvery { mockLike.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doUnlikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue()
+                    .assertType<Success<LikeKolViewModel>> { dt ->
+                        dt.data.isSuccess.assertEqualTo(expected.doLikeKolPost.data.success == SubmitLikeContentUseCase.SUCCESS)
+                        dt.data.isSuccess.assertTrue()
+                    }
+            }
+    }
+
+    @Test
+    fun `Unlike kol - failed from response`() {
+        val expected = LikeKolPostData(
+            doLikeKolPost = LikeKolPostData.DoLikeKolPost(
+                data = LikeKolPostData.LikeKolPostSuccessData(success = 200)
+            )
+        )
+
+        coEvery { mockLike.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doUnlikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertType<MessageErrorException> { }
+                }
+            }
+    }
+
+    @Test
+    fun `Unlike kol - failed throw from gql`() {
+        coEvery { mockLike.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, likeKolPostUseCase = mockLike)
+            .use {
+                it.vm.doUnlikeKol(1, 1)
+                it.vm.likeKolResp.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * Delete Post
+     */
+
+    @Test
+    fun `delete post - success`() {
+        val expected = SubmitActionContentResponse(
+            content = SubmitActionContentResponse.FeedContentSubmit(success = 1)
+        )
+
+        coEvery { mockDelete.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, deletePostUseCase = mockDelete)
+            .use {
+                it.vm.doDeletePost(1, 1)
+                it.vm.deletePostResp.getOrAwaitValue()
+                    .assertType<Success<DeletePostViewModel>> { dt ->
+                        dt.data.isSuccess.assertEqualTo(expected.content.success == SubmitPostData.SUCCESS)
+                        dt.data.isSuccess.assertTrue()
+                    }
+            }
+    }
+
+    @Test
+    fun `delete post - failed from response`() {
+        val expected = SubmitActionContentResponse(
+            content = SubmitActionContentResponse.FeedContentSubmit(success = 200)
+        )
+
+        coEvery { mockDelete.executeOnBackground() } returns expected
+
+        create(dispatcher = testDispatcher, deletePostUseCase = mockDelete)
+            .use {
+                it.vm.doDeletePost(1, 1)
+                it.vm.deletePostResp.getOrAwaitValue()
+                    .assertType<Fail> { dt ->
+                        dt.throwable.assertType<MessageErrorException> { }
+                    }
+            }
+    }
+
+    @Test
+    fun `delete post - failed throw from gql`() {
+        coEvery { mockDelete.executeOnBackground() } throws gqlFailed
+
+        create(dispatcher = testDispatcher, deletePostUseCase = mockDelete)
+            .use {
+                it.vm.doDeletePost(1, 1)
+                it.vm.deletePostResp.getOrAwaitValue().assertType<Fail> { dt ->
+                    dt.throwable.assertEqualTo(gqlFailed)
+                }
+            }
+    }
+
+    /**
+     * TopAds tracker
+     */
+
+    @Test
+    fun `top ads - when topads is clicked hit tracker hitClick`() {
+        create(dispatcher = testDispatcher, sendTopAdsUseCase = mockTopAdsTracker)
+            .use {
+                it.vm.doTopAdsTracker(
+                    url = "",
+                    shopId = "",
+                    shopName = "",
+                    imageUrl = "",
+                    isClick = true
+                )
+                verify { mockTopAdsTracker.hitClick(any(), any(), any(), any()) }
+            }
+
+    }
+
+    @Test
+    fun `top ads - when topads is not clicked hit tracker impression`() {
+        create(dispatcher = testDispatcher, sendTopAdsUseCase = mockTopAdsTracker)
+            .use {
+                it.vm.doTopAdsTracker(
+                    url = "",
+                    shopId = "",
+                    shopName = "",
+                    imageUrl = "",
+                    isClick = false
+                )
+                verify { mockTopAdsTracker.hitImpressions(any(), any(), any(), any()) }
+            }
+
     }
 }
