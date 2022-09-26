2f7eee3c96a518db3ccd0b5225210290fe3bda13
Irpan - <irpan@tokopedia.com>
2022-09-26 12:06:10 +0800

    add applink and search

--- features/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/common/constant/ConstantKey.kt
+++ features/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/common/constant/ConstantKey.kt
@@ -2,4 +2,7 @@ package com.tokopedia.dilayanitokopedia.common.constant
 
 object ConstantKey {
 
+
+    const val PARAM_APPLINK_AUTOCOMPLETE = "?navsource={source}&placeholder={placeholder}&first_install={first_install}"
+
 }
