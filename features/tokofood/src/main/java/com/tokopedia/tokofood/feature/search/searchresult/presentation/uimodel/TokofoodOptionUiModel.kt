7a51a4dfec1d31e70992e38eb58705ff1020318f
reivinoktavianus <oktavianusreivin@gmail.com>
2022-09-28 11:18:48 +0700

    Copy quick price range classess to separate module (tokofood)

---
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/feature/search/searchresult/presentation/uimodel/TokofoodOptionUiModel.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.tokofood.feature.search.searchresult.presentation.uimodel
+
+import com.tokopedia.filter.common.data.Option
+
+class TokofoodOptionUiModel(
+    val option: Option
+) {
+    var isSelected = false
+}
