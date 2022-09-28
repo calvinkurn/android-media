3d911ab8f8d49340143c6c2c11497af19256b16c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-09-28 12:29:55 +0700

    `feature` add DevOpsNotificationManager.java on developer_options_noop

---
+++ features/common/developer_options_noop/src/main/java/com/tokopedia/developer_options/notification/DevOpsNotificationManager.java
@@ -0,0 +1,17 @@
+package com.tokopedia.developer_options.notification;
+
+import android.app.Application;
+
+/**
+ * Created By : Jonathan Darwin on September 28, 2022
+ */
+public class DevOpsNotificationManager {
+
+    public DevOpsNotificationManager(Application application) {
+
+    }
+
+    public void start() {
+
+    }
+}
