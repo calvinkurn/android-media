cf2557a72496d0ff9bf91c27dac999115fa0bbe2
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-09-29 11:30:07 +0700

    `feature` add logic on DeveloperOptionsOnNotificationViewHolder.kt bind()

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
@@ -8,8 +8,26 @@ public class DevOptConfig {
     public static final String CHUCK_ENABLED = "CHUCK_ENABLED";
     public static final String IS_CHUCK_ENABLED = "is_enable";
 
+    public static final String DEV_OPT_ON_NOTIF_ENABLED = "DEV_OPT_ON_NOTIF_ENABLED";
+    public static final String IS_DEV_OPT_ON_NOTIF_ENABLED = "IS_DEV_OPT_ON_NOTIF_ENABLED";
+
     public static boolean isChuckNotifEnabled(Context context) {
         SharedPreferences cache = context.getSharedPreferences(CHUCK_ENABLED, Context.MODE_PRIVATE);
         return cache.getBoolean(IS_CHUCK_ENABLED, false);
+    }
+
+    public static boolean isDevOptOnNotifEnabled(Context context) {
+        SharedPreferences cache = context.getSharedPreferences(DEV_OPT_ON_NOTIF_ENABLED, Context.MODE_PRIVATE);
+        return cache.getBoolean(IS_DEV_OPT_ON_NOTIF_ENABLED, false);
+    }
+
+    public static void setDevOptOnNotifEnabled(Context context, boolean isDevOptOnNotifEnabled) {
+        context.getSharedPreferences(DEV_OPT_ON_NOTIF_ENABLED, Context.MODE_PRIVATE)
+            .edit()
+            .putBoolean(
+                DevOptConfig.IS_DEV_OPT_ON_NOTIF_ENABLED,
+                isDevOptOnNotifEnabled
+            )
+            .apply();
     }
 }
--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/DeveloperOptionsOnNotificationViewHolder.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/DeveloperOptionsOnNotificationViewHolder.kt
@@ -3,12 +3,10 @@ package com.tokopedia.developer_options.presentation.viewholder
 import android.view.View
 import android.widget.CompoundButton
 import androidx.annotation.LayoutRes
-import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.developer_options.R
 import com.tokopedia.developer_options.config.DevOptConfig
 import com.tokopedia.developer_options.presentation.model.DeveloperOptionsOnNotificationUiModel
-import com.tokopedia.developer_options.presentation.model.NetworkLogOnNotificationUiModel
 import com.tokopedia.unifycomponents.selectioncontrol.CheckboxUnify
 
 /**
@@ -24,18 +22,10 @@ class DeveloperOptionsOnNotificationViewHolder(
     }
 
     override fun bind(element: DeveloperOptionsOnNotificationUiModel) {
-//        val sharedPref = itemView.context.getSharedPreferences(
-//            DevOptConfig.CHUCK_ENABLED,
-//            BaseActivity.MODE_PRIVATE
-//        )
-//        val cb = itemView.findViewById<CheckboxUnify>(R.id.network_log_on_notification_cb)
-//        cb.isChecked = sharedPref.getBoolean(DevOptConfig.IS_CHUCK_ENABLED, false)
-//        cb.setOnCheckedChangeListener { _: CompoundButton, state: Boolean ->
-//            val editor = sharedPref.edit().putBoolean(
-//                DevOptConfig.IS_CHUCK_ENABLED,
-//                state
-//            )
-//            editor.apply()
-//        }
+        val cb = itemView.findViewById<CheckboxUnify>(R.id.cbx_developer_options_on_notificaion)
+        cb.isChecked = DevOptConfig.isDevOptOnNotifEnabled(itemView.context)
+        cb.setOnCheckedChangeListener { _: CompoundButton, state: Boolean ->
+            DevOptConfig.setDevOptOnNotifEnabled(itemView.context, state)
+        }
     }
 }
