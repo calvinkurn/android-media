7a8991f28379cd57f5a255904785f4b1d97dfd7d
Dhaba Widhikari <wikodhawi@gmail.com>
2022-10-03 10:38:14 +0700

    migrate get order history

---
+++ features/discovery/home_nav/src/main/java/com/tokopedia/homenav/mainnav/domain/usecases/query/QueryGetOrderHistory.kt
@@ -0,0 +1,46 @@
+package com.tokopedia.homenav.mainnav.domain.usecases.query
+
+import com.tokopedia.gql_query_annotation.GqlQuery
+import com.tokopedia.homenav.mainnav.domain.usecases.query.QueryGetOrderHistory.GET_ORDER_HISTORY_QUERY
+import com.tokopedia.homenav.mainnav.domain.usecases.query.QueryGetOrderHistory.GET_ORDER_HISTORY_QUERY_NAME
+
+/**
+ * Created by dhaba
+ */
+@GqlQuery(GET_ORDER_HISTORY_QUERY_NAME, GET_ORDER_HISTORY_QUERY)
+internal object QueryGetOrderHistory {
+    const val GET_ORDER_HISTORY_QUERY_NAME = "GetOrderHistoryQuery"
+    const val GET_ORDER_HISTORY_QUERY = "" +
+        "query GetOrderHistory(${'$'}input:UOHOrdersRequest!){\n" +
+        "              uohOrders(input:${'$'}input) {\n" +
+        "                orders {\n" +
+        "                          orderUUID\n" +
+        "                          status\n" +
+        "                          metadata {\n" +
+        "                            detailURL {\n" +
+        "                              appURL\n" +
+        "                            }\n" +
+        "                            status {\n" +
+        "                              label\n" +
+        "                              textColor\n" +
+        "                              bgColor\n" +
+        "                            }\n" +
+        "                            products {\n" +
+        "                              title\n" +
+        "                              imageURL\n" +
+        "                              inline1 {\n" +
+        "                                label\n" +
+        "                                textColor\n" +
+        "                                bgColor\n" +
+        "                              }\n" +
+        "                              inline2 {\n" +
+        "                                label\n" +
+        "                                textColor\n" +
+        "                                bgColor\n" +
+        "                                }\n" +
+        "                            }\n" +
+        "                        }\n" +
+        "                    }\n" +
+        "                }\n" +
+        "            }"
+}
