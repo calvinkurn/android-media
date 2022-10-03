0620bd83afe60ac9a807cc23fad9e1af76b7ee4c
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-10-03 15:23:32 +0700

    fix(pdp_ims) rollback MainActivity.kt

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -125,6 +125,6 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return "tokopedia://product/15418162"
+        return ""
     }
 }
