60a256556d84100e1bdeb885538c699f37dbaa2d
architpandey <archit.pandey@tokopedia.com>
2022-10-03 17:32:43 +0530

    On click allow GQL call implementation.

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/NotificationSettingsUtils.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/utils/NotificationSettingsUtils.kt
@@ -6,8 +6,11 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.notifications.common.CMConstant
 import com.tokopedia.notifications.common.NotificationSettingsGtmEvents
+import com.tokopedia.notifications.domain.NotificationSettingTrackerUseCase
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 
@@ -20,6 +23,13 @@ class NotificationSettingsUtils(private val context: Context) {
     private val postNotificationPermission = "android.permission.POST_NOTIFICATIONS"
     private val userSession: UserSessionInterface = UserSession(context)
     private val sdkLevel33 = 33
+    private val graphRepository: GraphqlRepository by lazy {
+        GraphqlInteractor.getInstance().graphqlRepository
+    }
+
+    private fun getSettingTrackerUseCase() : NotificationSettingTrackerUseCase {
+        return NotificationSettingTrackerUseCase(graphRepository)
+    }
 
     fun checkNotificationsModeForSpecificChannel(channel: String?): NotificationMode {
         return if (NotificationManagerCompat.from(context).areNotificationsEnabled()) {
@@ -60,6 +70,7 @@ class NotificationSettingsUtils(private val context: Context) {
                 ) == PackageManager.PERMISSION_GRANTED
             ) {
                 try {
+                    getSettingTrackerUseCase().sendTrackerUserSettings({}, {})
                     NotificationSettingsGtmEvents(userSession, context).sendActionAllowEvent(context)
                 } catch (_: Exception) {
                 }
