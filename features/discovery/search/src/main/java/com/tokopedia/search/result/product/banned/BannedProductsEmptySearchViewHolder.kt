b9bbb4afd167e82c0e4c9523acaf92046dddc30b
Darian Thedy <darian.thedy@tokopedia.com>
2022-10-04 17:07:58 +0700

    [AN-35873][Search] Banned Products Refactor

--- features/discovery/search/src/main/java/com/tokopedia/search/result/product/banned/BannedProductsEmptySearchViewHolder.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/product/banned/BannedProductsEmptySearchViewHolder.kt
@@ -1,11 +1,11 @@
-package com.tokopedia.search.result.presentation.view.adapter.viewholder.product
+package com.tokopedia.search.result.product.banned
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.search.R
 import com.tokopedia.search.databinding.SearchResultBannedProductsEmptySearchLayoutBinding
-import com.tokopedia.search.result.presentation.model.BannedProductsEmptySearchDataView
+import com.tokopedia.search.result.product.banned.BannedProductsEmptySearchDataView
 import com.tokopedia.utils.view.binding.viewBinding
 
 class BannedProductsEmptySearchViewHolder(itemView: View): AbstractViewHolder<BannedProductsEmptySearchDataView>(itemView) {
