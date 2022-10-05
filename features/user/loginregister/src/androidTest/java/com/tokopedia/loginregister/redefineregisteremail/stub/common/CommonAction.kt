562874f23ad44cdd0fc264d7aff64e05c911c8c5
habibitkp <101687909+habibitkp@users.noreply.github.com>
2022-10-05 12:38:33 +0700

    remove unused function - input email

--- features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/redefineregisteremail/stub/common/CommonAction.kt
+++ features/user/loginregister/src/androidTest/java/com/tokopedia/loginregister/redefineregisteremail/stub/common/CommonAction.kt
@@ -4,26 +4,20 @@ import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.GONE
 import androidx.test.espresso.matcher.ViewMatchers.isClickable
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isEnabled
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withInputType
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
 import org.hamcrest.core.AllOf
-
 
 fun isDisplayed(vararg resIds: Int) {
     resIds.forEach {
@@ -37,31 +31,6 @@ fun isTextDisplayed(vararg texts: String) {
         onView(withText(it))
             .check(matches(isDisplayed()))
     }
-}
-
-fun isTextNotDisplayed(vararg texts: String) {
-    texts.forEach {
-        onView(withText(it))
-            .check(matches(isDisplayed()))
-    }
-}
-
-fun isGoneView(resId: Int) {
-    onView(withId(resId))
-        .check(matches(withEffectiveVisibility(GONE)))
-}
-
-fun isNotDisplayed(vararg resIds: Int) {
-    resIds.forEach {
-        onView(withId(it))
-            .check(doesNotExist())
-    }
-}
-
-fun clickOnText(text: String) {
-    onView(withText(text))
-        .check(matches(isDisplayed()))
-        .perform(click())
 }
 
 fun clickOnView(resId: Int) {
@@ -97,11 +66,6 @@ fun isFieldEnable(isEnabled: Boolean, inputType: Int) {
         )
 }
 
-fun scrollToView(resId: Int) {
-    onView(withId(resId))
-        .perform(ViewActions.scrollTo())
-}
-
 fun inputText(inputType: Int, text: String) {
     onView(withInputType(inputType))
         .perform(typeText(text), closeSoftKeyboard())
@@ -112,13 +76,13 @@ fun clearText(inputType: Int) {
         .perform(clearText(), closeSoftKeyboard())
 }
 
-fun clickOnButtonDialog(text: String) {
-    onView(withText(text))
+fun clickOnButtonDialog(resId: Int) {
+    onView(withId(resId))
         .check(matches(isDisplayed()))
         .perform(forceClick())
 }
 
-private fun forceClick(): ViewAction? {
+private fun forceClick(): ViewAction {
     return object : ViewAction {
         override fun getConstraints(): Matcher<View> {
             return AllOf.allOf(isClickable(), isEnabled(), isDisplayed())
