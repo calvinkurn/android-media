e2995da4675409dd8a58d77ed4b0da12f981e076
rama.putra <rama.putra@tokopedia.com>
2022-10-05 23:21:18 +0700

    remove unused changes

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMapper.kt
@@ -520,8 +520,7 @@ object DeeplinkMapper {
             }),
             DLP.startWith(ApplinkConst.TokoFood.MAIN_PATH) { _, uri, _, _ -> DeeplinkMapperTokoFood.mapperInternalApplinkTokoFood(uri) },
             DLP.matchPattern(ApplinkConst.RESOLUTION_SUCCESS) { _, uri, _, _ -> ApplinkConstInternalOperational.buildApplinkResolution(uri)},
-            DLP.exact(ApplinkConst.DISCOVERY_SEARCH_UNIVERSAL) { _, _, deeplink, _ -> getRegisteredNavigationSearch(deeplink) },
-            DLP.matchPattern(ApplinkConst.RESOLUTION_SUCCESS) { _, uri, _, _ -> ApplinkConstInternalOperational.buildApplinkResolution(uri)},
+            DLP.exact(ApplinkConst.DISCOVERY_SEARCH_UNIVERSAL) { _, _, deeplink, _ -> getRegisteredNavigationSearch(deeplink) }
         )
 
     fun getTokopediaSchemeList():List<DLP>{
