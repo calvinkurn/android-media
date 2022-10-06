ad034b31960a8c473ae594178faa3a9861967303
fajarca <fajar.azhar@tokopedia.com>
2022-10-06 12:45:06 +0700

    Remove unused import

--- features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/fragment/CampaignListScreen.kt
+++ features/merchant/campaign_list/src/main/java/com/tokopedia/campaignlist/page/presentation/fragment/CampaignListScreen.kt
@@ -12,10 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
