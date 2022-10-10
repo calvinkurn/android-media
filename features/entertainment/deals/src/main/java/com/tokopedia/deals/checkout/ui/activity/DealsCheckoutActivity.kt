9fd76f9c2e9dbf3f52c4d843c778fac960578fda
firmankagami <firmanda.nugroho@tokopedia.com>
2022-10-10 15:26:09 +0700

    feat: create initial layout data implementation

--- features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/activity/DealsCheckoutActivity.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/activity/DealsCheckoutActivity.kt
@@ -1,30 +1,20 @@
 package com.tokopedia.deals.checkout.ui.activity
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.abstraction.common.di.component.HasComponent
-import com.tokopedia.common_entertainment.data.EventVerifyResponse
-import com.tokopedia.common_entertainment.data.ItemMapResponse
 import com.tokopedia.deals.DealsComponentInstance
 import com.tokopedia.deals.checkout.di.DaggerDealsCheckoutComponent
 import com.tokopedia.deals.checkout.di.DealsCheckoutComponent
 import com.tokopedia.deals.checkout.ui.fragment.DealsCheckoutFragment
-import com.tokopedia.deals.pdp.data.ProductDetailData
 
 class DealsCheckoutActivity: BaseSimpleActivity(), HasComponent<DealsCheckoutComponent> {
 
-    private var dealsDetail: ProductDetailData? = null
-    private var dealsVerify: EventVerifyResponse? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        dealsDetail = intent.getParcelableExtra(EXTRA_DEAL_DETAIL)
-        dealsVerify = intent.getParcelableExtra(EXTRA_DEAL_VERIFY)
-    }
-
     override fun getNewFragment(): Fragment {
-        return DealsCheckoutFragment.createInstance(dealsDetail, dealsVerify)
+        return DealsCheckoutFragment.createInstance(
+            intent.getParcelableExtra(EXTRA_DEAL_DETAIL),
+            intent.getParcelableExtra(EXTRA_DEAL_VERIFY)
+        )
     }
 
     override fun getComponent(): DealsCheckoutComponent {
--- features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/fragment/DealsCheckoutFragment.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/fragment/DealsCheckoutFragment.kt
@@ -8,14 +8,19 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.common_entertainment.data.EventVerifyResponse
-import com.tokopedia.common_entertainment.data.ItemMap
 import com.tokopedia.common_entertainment.data.ItemMapResponse
+import com.tokopedia.deals.R
 import com.tokopedia.deals.checkout.di.DealsCheckoutComponent
 import com.tokopedia.deals.checkout.ui.activity.DealsCheckoutActivity.Companion.EXTRA_DEAL_DETAIL
 import com.tokopedia.deals.checkout.ui.activity.DealsCheckoutActivity.Companion.EXTRA_DEAL_VERIFY
 import com.tokopedia.deals.checkout.ui.viewmodel.DealsCheckoutViewModel
+import com.tokopedia.deals.common.utils.DealsUtils
 import com.tokopedia.deals.databinding.FragmentDealsCheckoutBinding
 import com.tokopedia.deals.pdp.data.ProductDetailData
+import com.tokopedia.kotlin.extensions.view.toIntSafely
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 import javax.inject.Inject
@@ -33,6 +38,11 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
     private var dealsDetail: ProductDetailData? = null
     private var dealsVerify: EventVerifyResponse? = null
     private var dealsItemMap: ItemMapResponse? = null
+
+    private var imgBrand: ImageUnify? = null
+    private var tgBrandName: Typography? = null
+    private var tgTitle: Typography? = null
+    private var tgExpiredDate: Typography? = null
 
     override fun getScreenName(): String = ""
 
@@ -56,6 +66,36 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
     ): View? {
         binding = FragmentDealsCheckoutBinding.inflate(inflater)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+        showUI()
+    }
+
+    private fun setupUI() {
+        view?.apply {
+            imgBrand = binding?.imageViewBrand
+            tgBrandName = binding?.tgBrandName
+            tgTitle = binding?.tgDealDetails
+            tgExpiredDate = binding?.tgExpiryDate
+        }
+    }
+
+    private fun showUI() {
+        dealsDetail?.let {
+            imgBrand?.loadImage(it.imageApp)
+            tgBrandName?.text = it.brand.title
+            tgTitle?.text = it.displayName
+            tgExpiredDate?.text = String.format(
+                getString(
+                    R.string.deals_pdp_valid_through,
+                    DealsUtils.convertEpochToString(it.saleEndDate.toIntSafely()
+                    )
+                )
+            )
+        }
     }
 
     companion object {
