57219aba11de2428a6263638826cee4435e81025
souravsaikia <sourav.saikia@tokopedia.com>
2022-10-11 12:50:06 +0530

    Fixing Bottom Sheet Issue

--- features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/MediaRetryBottomSheetAdapter.kt
+++ features/user/chatbot/src/main/java/com/tokopedia/chatbot/view/adapter/MediaRetryBottomSheetAdapter.kt
@@ -3,15 +3,15 @@ package com.tokopedia.chatbot.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.tokopedia.chatbot.databinding.BottomSheetMediaUploadBinding
+import com.tokopedia.chatbot.databinding.RetryUploadMediaBottomSheetItemBinding
 import com.tokopedia.unifyprinciples.Typography
 
 class MediaRetryBottomSheetAdapter(private val onBottomSheetItemClicked: (position: Int) -> Unit) : RecyclerView.Adapter<MediaRetryBottomSheetAdapter.MediaRetryBottomSheetViewHoder>() {
 
     val list = mutableListOf<String>()
 
-    inner class MediaRetryBottomSheetViewHoder(itemView: BottomSheetMediaUploadBinding) : RecyclerView.ViewHolder(itemView.root) {
-        val item: Typography = itemView.retry
+    inner class MediaRetryBottomSheetViewHoder(itemView: RetryUploadMediaBottomSheetItemBinding) : RecyclerView.ViewHolder(itemView.root) {
+        val item: Typography = itemView.retryImageUploadBottomSheetItem
         fun bind(item: String, position: Int) {
             this.item.text = item
             itemView.setOnClickListener { onBottomSheetItemClicked(position) }
@@ -19,7 +19,7 @@ class MediaRetryBottomSheetAdapter(private val onBottomSheetItemClicked: (positi
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaRetryBottomSheetViewHoder {
-        val view = BottomSheetMediaUploadBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        val view = RetryUploadMediaBottomSheetItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return MediaRetryBottomSheetViewHoder(view)
 
     }
