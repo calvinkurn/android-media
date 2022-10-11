eb5e454adfcc5ab0a733c203f66a04477dc0b2ea
firmankagami <firmanda.nugroho@tokopedia.com>
2022-10-11 10:14:42 +0700

    feat: section 2 done

--- features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/fragment/DealsCheckoutFragment.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/checkout/ui/fragment/DealsCheckoutFragment.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.deals.checkout.ui.fragment
 
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,10 +18,15 @@ import com.tokopedia.deals.checkout.ui.viewmodel.DealsCheckoutViewModel
 import com.tokopedia.deals.common.utils.DealsUtils
 import com.tokopedia.deals.databinding.FragmentDealsCheckoutBinding
 import com.tokopedia.deals.pdp.data.ProductDetailData
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.extensions.view.toIntSafely
 import com.tokopedia.media.loader.loadImage
+import com.tokopedia.promocheckout.common.view.widget.TickerPromoStackingCheckoutView
 import com.tokopedia.unifycomponents.ImageUnify
 import com.tokopedia.unifycomponents.TextFieldUnify
+import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.lifecycle.autoClearedNullable
@@ -47,6 +53,15 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
     private var tgNumbersLocation: Typography? = null
     private var tgAllLocation: Typography? = null
     private var etEmail: TextFieldUnify? = null
+    private var tgMrpPerQuantity: Typography? = null
+    private var tgSalesPricePerQty: Typography? = null
+    private var tgSalesPriceAllQty: Typography? = null
+    private var tgServiceFee: Typography? = null
+    private var tgServiceFeeAmount: Typography? = null
+    private var tgTotalAmount: Typography? = null
+    private var tgNumberVoucher: Typography? = null
+    private var btnPayment: UnifyButton? = null
+    private var tickerPromoCode: TickerPromoStackingCheckoutView? = null
 
     override fun getScreenName(): String = ""
 
@@ -87,6 +102,13 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
             tgNumbersLocation = binding?.tgNoLocations
             tgAllLocation = binding?.tgAvailableLocations
             etEmail = binding?.etEmail
+            tgMrpPerQuantity = binding?.tgMrpPerQuantity
+            tgSalesPricePerQty = binding?.tgSalesPricePerQuantity
+            tgSalesPriceAllQty = binding?.tgSalesPriceAllQuantity
+            tgServiceFee = binding?.tgServiceFee
+            tgServiceFeeAmount = binding?.tgServiceFeeAmount
+            tgTotalAmount = binding?.tgTotalAmount
+            tgNumberVoucher = binding?.tgNumberVouchers
         }
     }
 
@@ -104,7 +126,7 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
             )
 
             if (it.outlets.isNullOrEmpty()) {
-                tgAllLocation?.text = getString(
+                tgAllLocation?.text = context?.resources?.getString(
                     com.tokopedia.deals.R.string.deals_checkout_all_indonesia
                 )
             }
@@ -120,6 +142,31 @@ class DealsCheckoutFragment: BaseDaggerFragment() {
 
             etEmail?.textFieldInput?.setText(userSession.email)
             etEmail?.textFieldInput?.setKeyListener(null)
+
+            if (it.mrp.toIntSafely() != 0 && it.mrp != it.salesPrice) {
+                tgMrpPerQuantity?.apply {
+                    show()
+                    text = DealsUtils.convertToCurrencyString(it.mrp.toIntSafely().toLong())
+                    paintFlags = this.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                }
+            } else {
+                tgMrpPerQuantity?.hide()
+            }
+
+            dealsItemMap?.let { itemMap ->
+                tgSalesPricePerQty?.text = DealsUtils.convertToCurrencyString(itemMap.price)
+                tgSalesPriceAllQty?.text = DealsUtils.convertToCurrencyString(itemMap.price * itemMap.quantity)
+
+                if (itemMap.commission <= 0) {
+                    tgServiceFee?.gone()
+                    tgServiceFeeAmount?.gone()
+                } else {
+                    tgServiceFeeAmount?.text = DealsUtils.convertToCurrencyString(itemMap.commission.toLong())
+                }
+
+                tgTotalAmount?.text = DealsUtils.convertToCurrencyString(itemMap.price.toLong() * itemMap.quantity.toLong() + itemMap.commission.toLong())
+                tgNumberVoucher?.text = context?.resources?.getString(com.tokopedia.deals.R.string.deals_checkout_number_of_vouchers, itemMap.quantity)
+            }
         }
     }
 
