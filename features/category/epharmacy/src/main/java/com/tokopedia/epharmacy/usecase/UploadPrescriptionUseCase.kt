8237eb306376bc155f6b55b54c67a3c7111b1095
mohit-toko <mohit.singh@tokopedia.com>
2022-10-11 19:40:40 +0530

    Removed Logs

--- features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/usecase/UploadPrescriptionUseCase.kt
+++ features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/usecase/UploadPrescriptionUseCase.kt
@@ -3,7 +3,6 @@ package com.tokopedia.epharmacy.usecase
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
-import android.util.Log
 import com.tokopedia.analytics.performance.util.EmbraceMonitoring
 import com.tokopedia.common.network.coroutines.repository.RestRepository
 import com.tokopedia.common.network.coroutines.usecase.RestRequestUseCase
