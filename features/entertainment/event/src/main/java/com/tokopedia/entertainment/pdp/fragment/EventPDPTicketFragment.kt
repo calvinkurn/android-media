5ecc18df3ec69147fc8fa21931cf566f93a03417
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-10-12 15:07:35 +0800

    fix : fix wildcard imports

--- features/entertainment/event/src/main/java/com/tokopedia/entertainment/pdp/fragment/EventPDPTicketFragment.kt
+++ features/entertainment/event/src/main/java/com/tokopedia/entertainment/pdp/fragment/EventPDPTicketFragment.kt
@@ -38,7 +38,13 @@ import com.tokopedia.entertainment.pdp.adapter.factory.PackageTypeFactory
 import com.tokopedia.entertainment.pdp.adapter.factory.PackageTypeFactoryImpl
 import com.tokopedia.entertainment.pdp.analytic.EventPDPTracking
 import com.tokopedia.entertainment.pdp.common.util.CurrencyFormatter.getRupiahFormat
-import com.tokopedia.entertainment.pdp.data.*
+import com.tokopedia.entertainment.pdp.data.EventPDPTicket
+import com.tokopedia.entertainment.pdp.data.EventPDPTicketBanner
+import com.tokopedia.entertainment.pdp.data.EventPDPTicketGroup
+import com.tokopedia.entertainment.pdp.data.EventPDPTicketModel
+import com.tokopedia.entertainment.pdp.data.PackageItem
+import com.tokopedia.entertainment.pdp.data.PackageV3
+import com.tokopedia.entertainment.pdp.data.ProductDetailData
 import com.tokopedia.entertainment.pdp.data.pdp.ItemMap
 import com.tokopedia.entertainment.pdp.data.pdp.MetaDataResponse
 import com.tokopedia.entertainment.pdp.data.pdp.VerifyRequest
@@ -60,15 +66,27 @@ import com.tokopedia.network.utils.ErrorHandler
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.user.session.UserSessionInterface
-import kotlinx.android.synthetic.main.ent_ticket_listing_activity.*
-import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.*
-import kotlinx.android.synthetic.main.item_event_pdp_parent_ticket.*
-import kotlinx.android.synthetic.main.item_event_pdp_parent_ticket_banner.*
-import kotlinx.android.synthetic.main.widget_event_pdp_calendar.view.*
+import kotlinx.android.synthetic.main.ent_ticket_listing_activity.loaderUbah
+import kotlinx.android.synthetic.main.ent_ticket_listing_activity.txtDate
+import kotlinx.android.synthetic.main.ent_ticket_listing_activity.txtUbah
+import kotlinx.android.synthetic.main.ent_ticket_listing_activity.txtPlaceHolderTglKunjungan
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.txtTotalHarga
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.rvEventTicketList
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.swipe_refresh_layout
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.containerEventBottom
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.pilihTicketBtn
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.scroll_ticket_pdp
+import kotlinx.android.synthetic.main.ent_ticket_listing_fragment.viewBottom
+import kotlinx.android.synthetic.main.item_event_pdp_parent_ticket.accordionEventPDPTicket
+import kotlinx.android.synthetic.main.item_event_pdp_parent_ticket_banner.tgEventTicketRecommendationTitle
+import kotlinx.android.synthetic.main.widget_event_pdp_calendar.view.bottom_sheet_calendar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.*
+import java.util.TimeZone
+import java.util.Date
 import javax.inject.Inject
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class EventPDPTicketFragment : BaseListFragment<EventPDPTicket, PackageTypeFactory>(),
         OnBindItemTicketListener, OnCoachmarkListener {
@@ -371,7 +389,7 @@ class EventPDPTicketFragment : BaseListFragment<EventPDPTicket, PackageTypeFacto
     private fun showViewBottom(state: Boolean) {
         viewBottom?.visibility = if (state) View.VISIBLE else View.GONE
         containerEventBottom?.visibility = if (state) View.VISIBLE else View.GONE
-        if (!state) txtTotalHarga.text = String.format(resources.getString(R.string.ent_default_totalPrice))
+        if (!state) txtTotalHarga.text = String.format(context?.resources?.getString(R.string.ent_default_totalPrice) ?: "")
     }
 
     private fun setupUbahButton() {
