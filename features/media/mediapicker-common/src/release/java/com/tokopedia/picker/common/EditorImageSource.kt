d1769cfb99ec43349f18aa877208ba8c41b702f8
Calvin <calvin.kurniawan@tokopedia.com>
2022-10-14 17:00:47 +0700

    delete duplicate dir

--- features/media/mediapicker-common/src/release/java/com/tokopedia/picker/common/EditorImageSource.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.picker.common
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class EditorImageSource(
-    val originalPaths: List<String> = emptyList()
-) : Parcelable
