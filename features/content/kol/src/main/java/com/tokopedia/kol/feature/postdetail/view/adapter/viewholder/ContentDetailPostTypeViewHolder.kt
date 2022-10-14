90d0d026b05b75aea3c96f9c4f648f53b9a13729
Shru-2212 <83584976+Shru-2212@users.noreply.github.com>
2022-10-14 14:01:27 +0530

    Update features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/adapter/viewholder/ContentDetailPostTypeViewHolder.kt
    Co-authored-by: sonoda-bots[bot] <113779905+sonoda-bots[bot]@users.noreply.github.com>

--- features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/adapter/viewholder/ContentDetailPostTypeViewHolder.kt
+++ features/content/kol/src/main/java/com/tokopedia/kol/feature/postdetail/view/adapter/viewholder/ContentDetailPostTypeViewHolder.kt
@@ -374,8 +374,8 @@ class ContentDetailPostTypeViewHolder  @JvmOverloads constructor(
                 ASGC_RESTOCK_PRODUCTS -> context.getString(
                     feedComponentR.string.feeds_asgc_restock_text)
                 ASGC_DISCOUNT_TOKO -> context.getString(feedComponentR.string.feed_asgc_diskon_toko)
-                ASGC_FLASH_SALE_TOKO ->  context.getString(feedComponentR.string.feed_asgc_flash_sale_toko)
-                ASGC_RILISAN_SPECIAL ->  context.getString(feedComponentR.string.feed_asgc_rilisan_special)
+                ASGC_FLASH_SALE_TOKO -> context.getString(feedComponentR.string.feed_asgc_flash_sale_toko)
+                ASGC_RILISAN_SPECIAL -> context.getString(feedComponentR.string.feed_asgc_rilisan_special)
                 else -> String.EMPTY
             }
         } else {
