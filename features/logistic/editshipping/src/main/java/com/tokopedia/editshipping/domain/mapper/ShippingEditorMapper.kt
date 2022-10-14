0e2e71daa963c92884f2e19c2ef53a8dd30fb00c
hiradevina <fakhira.devina@tokopedia.com>
2022-10-14 14:55:41 +0700

    feat: description for whitelabel service

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/domain/mapper/ShippingEditorMapper.kt
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/domain/mapper/ShippingEditorMapper.kt
@@ -9,8 +9,8 @@ class ShippingEditorMapper @Inject constructor() {
 
     fun mapShipperList(response: GetShipperListResponse): ShipperListModel {
         return ShipperListModel().apply {
-             shippers = mapShipper(response.ongkirShippingEditor.data)
-             ticker = mapTickerShipperList(response)
+            shippers = mapShipper(response.ongkirShippingEditor.data)
+            ticker = mapTickerShipperList(response)
         }
     }
 
@@ -30,11 +30,12 @@ class ShippingEditorMapper @Inject constructor() {
     }
 
     private fun mapShipperOnDemand(response: List<OnDemand>): List<OnDemandModel> {
-        val onDemandModelList =  ArrayList<OnDemandModel>()
+        val onDemandModelList = ArrayList<OnDemandModel>()
         response.forEach { data ->
             val isWhitelabelShipper = isWhitelabelShipper(data.shipperId)
-            // todo get whitelabel description from ?
-            val shipperDescription = if (isWhitelabelShipper) "" else data.shipperProduct.joinToString(" | ") { it.shipperProductName }
+            val shipperDescription =
+                if (isWhitelabelShipper) data.shipperProduct.firstOrNull()?.description
+                    ?: "" else data.shipperProduct.joinToString(" | ") { it.shipperProductName }
             val onDemandUiModel = OnDemandModel().apply {
                 shipperId = data.shipperId
                 shipperName = data.shipperName
@@ -55,8 +56,9 @@ class ShippingEditorMapper @Inject constructor() {
         val conventionalModelList = ArrayList<ConventionalModel>()
         response.forEach { data ->
             val isWhitelabelShipper = isWhitelabelShipper(data.shipperId)
-            // todo get whitelabel description from ?
-            val shipperDescription = if (isWhitelabelShipper) "" else data.shipperProduct.joinToString(" | ") { it.shipperProductName }
+            val shipperDescription =
+                if (isWhitelabelShipper) data.shipperProduct.firstOrNull()?.description
+                    ?: "" else data.shipperProduct.joinToString(" | ") { it.shipperProductName }
             val conventionalUiModel = ConventionalModel().apply {
                 shipperId = data.shipperId
                 shipperName = data.shipperName
@@ -73,25 +75,25 @@ class ShippingEditorMapper @Inject constructor() {
         return conventionalModelList
     }
 
-    private fun isWhitelabelShipper(shipperId: Long) : Boolean {
+    private fun isWhitelabelShipper(shipperId: Long): Boolean {
         return WHITELABEL_SHIPPER_ID.contains(shipperId)
     }
 
     private fun mapFeatureInfo(response: List<FeatureInfo>): List<FeatureInfoModel> {
-       return response.map {
-           FeatureInfoModel(
-                   it.header,
-                   it.body
-           )
-       }
+        return response.map {
+            FeatureInfoModel(
+                it.header,
+                it.body
+            )
+        }
     }
 
     private fun mapShipperProduct(response: List<ShipperProduct>): List<ShipperProductModel> {
         return response.map {
             ShipperProductModel(
-                    it.shipperProductId,
-                    it.shipperProductName,
-                    it.isActive
+                it.shipperProductId,
+                it.shipperProductName,
+                it.isActive
             )
         }
     }
@@ -100,15 +102,18 @@ class ShippingEditorMapper @Inject constructor() {
         val data = response.ongkirShippingEditor.data.ticker
         return data.map {
             TickerModel(
-                    it.header,
-                    it.body,
-                    it.textLink,
-                    it.urlLink
+                it.header,
+                it.body,
+                it.textLink,
+                it.urlLink
             )
         }
     }
 
-    private fun mapHeaderTicker(response: HeaderTicker, warehouses: List<Warehouses>): HeaderTickerModel {
+    private fun mapHeaderTicker(
+        response: HeaderTicker,
+        warehouses: List<Warehouses>
+    ): HeaderTickerModel {
         return HeaderTickerModel().apply {
             header = response.header
             body = response.body
@@ -119,19 +124,25 @@ class ShippingEditorMapper @Inject constructor() {
         }
     }
 
-    private fun mapCourierTicker(response: List<CourierTicker>, warehouses: List<Warehouses>): List<CourierTickerModel> {
+    private fun mapCourierTicker(
+        response: List<CourierTicker>,
+        warehouses: List<Warehouses>
+    ): List<CourierTickerModel> {
         return response.map {
             CourierTickerModel(
-                    it.shipperId,
-                    mapWarehouseModelBasedOnWarehouseId(it.warehouseIds?: emptyList(), warehouses),
-                    it.tickerState,
-                    it.isAvailable,
-                    mapShipperProductTicker(it.shipperProduct)
+                it.shipperId,
+                mapWarehouseModelBasedOnWarehouseId(it.warehouseIds ?: emptyList(), warehouses),
+                it.tickerState,
+                it.isAvailable,
+                mapShipperProductTicker(it.shipperProduct)
             )
         }
     }
 
-    private fun mapWarehouseModelBasedOnWarehouseId(response: List<Long>, warehouses: List<Warehouses>): List<WarehousesModel> {
+    private fun mapWarehouseModelBasedOnWarehouseId(
+        response: List<Long>,
+        warehouses: List<Warehouses>
+    ): List<WarehousesModel> {
         return warehouses.filter {
             response.any { id ->
                 it.warehouseId == id
@@ -153,40 +164,40 @@ class ShippingEditorMapper @Inject constructor() {
         return shipperProductTickerList
     }
 
-    private fun mapWarehousesTicker(it: Warehouses) : WarehousesModel {
+    private fun mapWarehousesTicker(it: Warehouses): WarehousesModel {
         return WarehousesModel(
-                    it.warehouseId,
-                    it.warehouseName,
-                    it.districtId,
-                    it.districtName,
-                    it.cityId,
-                    it.cityName,
-                    it.provinceId,
-                    it.provinceName,
-                    it.status,
-                    it.postalCode,
-                    it.isDefault,
-                    it.latLon,
-                    it.latitude,
-                    it.longitude,
-                    it.addressDetail,
-                    it.country,
-                    it.isFulfillment,
-                    it.warehouseType,
-                    it.email,
-                    mapShopId(it.shopId),
-                    mapPartnerId(it.partnerId)
+            it.warehouseId,
+            it.warehouseName,
+            it.districtId,
+            it.districtName,
+            it.cityId,
+            it.cityName,
+            it.provinceId,
+            it.provinceName,
+            it.status,
+            it.postalCode,
+            it.isDefault,
+            it.latLon,
+            it.latitude,
+            it.longitude,
+            it.addressDetail,
+            it.country,
+            it.isFulfillment,
+            it.warehouseType,
+            it.email,
+            mapShopId(it.shopId),
+            mapPartnerId(it.partnerId)
         )
     }
 
-    private fun mapShopId(response: ShopId) : ShopIdModel {
+    private fun mapShopId(response: ShopId): ShopIdModel {
         return ShopIdModel().apply {
             int64 = response.int64
             valid = response.valid
         }
     }
 
-    private fun mapPartnerId(response: PartnerId) : PartnerIdModel {
+    private fun mapPartnerId(response: PartnerId): PartnerIdModel {
         return PartnerIdModel().apply {
             int64 = response.int64
             valid = response.valid
--- features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/data/response/shippingeditor/GetShipperListResponse.kt
+++ features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/data/response/shippingeditor/GetShipperListResponse.kt
@@ -79,6 +79,8 @@ data class ShipperProduct(
         var shipperProductId: String = "",
         @SerializedName("shipper_product_name")
         var shipperProductName: String = "",
+        @SerializedName("description")
+        var description: String = "",
         @SerializedName("is_active")
         var isActive: Boolean = false
 )
