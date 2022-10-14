c18bc839ddb01e55e263ad5593c0e09d58fd9a17
astidhiyaa <asti.anzaria@tokopedia.com>
2022-10-14 16:37:35 +0800

    fix: adjust job

--- features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/viewmodel/PlayViewModel.kt
@@ -1296,14 +1296,13 @@ class PlayViewModel @AssistedInject constructor(
     private fun handleOnboarding(videoMetaInfo: PlayVideoMetaInfoUiModel) {
         val isShown = playPreference.isCoachMark(userId = userId)
         playAnalytic.screenWithSwipeCoachMark(isShown = isShown, channelId = channelId, channelType = channelType, isLoggedIn = userSession.isLoggedIn, userId = userId)
+        coachmarkJob?.cancel()
         if (isShown && !videoMetaInfo.videoPlayer.isYouTube) {
             coachmarkJob?.cancel()
             coachmarkJob = viewModelScope.launch(dispatchers.main) {
                 delay(ONBOARDING_DELAY)
                 _observableOnboarding.value = Event(Unit)
             }
-        } else {
-            coachmarkJob?.cancel()
         }
     }
 
