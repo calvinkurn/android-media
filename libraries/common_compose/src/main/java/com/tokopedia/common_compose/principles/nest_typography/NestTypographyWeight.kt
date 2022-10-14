2cbd5d191c939f5061b21d29be497a7aea7f92cb
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-10-14 10:14:27 +0700

    fix(compose) fix code review 13/10/2022

--- libraries/common_compose/src/main/java/com/tokopedia/common_compose/principles/nest_typography/NestTypographyWeight.kt
+++ libraries/common_compose/src/main/java/com/tokopedia/common_compose/principles/nest_typography/NestTypographyWeight.kt
@@ -1,11 +1,11 @@
-package com.tokopedia.common_compose.principles.nest_text
+package com.tokopedia.common_compose.principles.nest_typography
 
 /**
  * Created by yovi.putra on 07/09/22"
  * Project name: android-tokopedia-core
  **/
 
-enum class NestTextWeight(val value: Int) {
+enum class NestTypographyWeight(val value: Int) {
     Regular(value = 1),
     Bold(value = 2)
 }
