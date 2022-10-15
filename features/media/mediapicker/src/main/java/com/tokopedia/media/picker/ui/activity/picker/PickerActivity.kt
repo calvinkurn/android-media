ee44c9cee6a4d23a465239ce033bb8017778a4fa
varun0922 <varun.sharma@tokopedia.com>
2022-10-15 15:55:14 +0530

    changes for nullability MotionEvent

--- features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/activity/picker/PickerActivity.kt
+++ features/media/mediapicker/src/main/java/com/tokopedia/media/picker/ui/activity/picker/PickerActivity.kt
@@ -294,7 +294,7 @@ open class PickerActivity : BaseActivity()
         }
     }
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         if (!param.get().isIncludeVideoFile()) {
             return super.dispatchTouchEvent(ev)
         }
