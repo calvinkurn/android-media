386154578f35f7e06e5adc4c8c5ce2b7d6cd5d5d
hiradevina <fakhira.devina@tokopedia.com>
2022-10-17 11:53:05 +0700

    feat: adjust layout in shipper single loc

--- features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/customview/CourierView.java
+++ features/logistic/editshipping/src/main/java/com/tokopedia/editshipping/ui/customview/CourierView.java
@@ -2,6 +2,8 @@ package com.tokopedia.editshipping.ui.customview;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -10,7 +12,9 @@ import androidx.annotation.NonNull;
 import com.tokopedia.abstraction.common.utils.image.ImageHandler;
 import com.tokopedia.editshipping.R;
 import com.tokopedia.editshipping.domain.model.editshipping.Courier;
+import com.tokopedia.editshipping.domain.model.editshipping.Service;
 import com.tokopedia.editshipping.ui.EditShippingViewListener;
+import com.tokopedia.editshipping.util.EditShippingConstant;
 import com.tokopedia.unifyprinciples.Typography;
 
 /**
@@ -27,6 +31,7 @@ public class CourierView extends EditShippingCourierView<Courier,
     PackageView packageView;
     LinearLayout packageViewHolder;
     Typography courierUnavailableWarning;
+    CheckBox checkBoxWhitelabelService;
     private EditShippingViewListener mainView;
 
     public CourierView(Context context) {
@@ -44,6 +49,7 @@ public class CourierView extends EditShippingCourierView<Courier,
         courierNameText = (Typography) view.findViewById(R.id.name);
         courierImageHolder = (ImageView) view.findViewById(R.id.img_courier);
         shipmentSettings = (LinearLayout) view.findViewById(R.id.shipping_settings);
+        checkBoxWhitelabelService = (CheckBox) view.findViewById(R.id.checkbox_whitelabel_service);
 
         packageView = (PackageView) view.findViewById(R.id.children_layout);
         packageViewHolder = (LinearLayout) view.findViewById(R.id.package_view_holder);
@@ -55,10 +61,21 @@ public class CourierView extends EditShippingCourierView<Courier,
     @Override
     public void renderData(@NonNull final Courier courier, final int courierIndex) {
         courierNameText.setText(courier.name);
-        ImageHandler.LoadImage(courierImageHolder, courier.logo);
-        packageView.setViewListener(mainView);
+        if (isWhitelabelService(courier)) {
+            courierImageHolder.setVisibility(View.GONE);
+            checkBoxWhitelabelService.setVisibility(View.VISIBLE);
+            setWhitelabelInitialState(courier);
+            // set service condition
+            checkBoxWhitelabelService.setOnCheckedChangeListener(onWhitelabelServiceChecked(courier, courierIndex));
+            packageView.setVisibility(View.GONE);
+        } else {
+            ImageHandler.LoadImage(courierImageHolder, courier.logo);
+            courierImageHolder.setVisibility(View.VISIBLE);
+            checkBoxWhitelabelService.setVisibility(View.GONE);
+            packageView.setViewListener(mainView);
+            setPackageAvailability(courierIndex, courier);
+        }
         setCourierWeightPolicy(courier);
-        setPackageAvailability(courierIndex, courier);
         shipmentSettings.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,6 +104,19 @@ public class CourierView extends EditShippingCourierView<Courier,
         }
     }
 
+    private void setWhitelabelInitialState(Courier courier) {
+        if (courier.available.equals("1") && !courier.services.isEmpty()) {
+            checkBoxWhitelabelService.setEnabled(true);
+            Service service = courier.services.get(0);
+            checkBoxWhitelabelService.setChecked(service.getActive());
+        }
+        else {
+            checkBoxWhitelabelService.setEnabled(false);
+            courierUnavailableWarning.setVisibility(VISIBLE);
+            shipmentSettings.setVisibility(View.GONE);
+        }
+    }
+
     private int weightInformationVisibility(String information){
         if(information == null
                 || information.isEmpty()
@@ -101,6 +131,21 @@ public class CourierView extends EditShippingCourierView<Courier,
                     weightInformationVisibility(currentCourier.weightPolicy));
         } else setCourierInformation(mainView.getMainContext().getString(R.string.info_shipping_unavailable),
                 currentCourier.name, View.GONE);
+    }
+
+    private CompoundButton.OnCheckedChangeListener onWhitelabelServiceChecked(Courier courier, final int courierIndex) {
+        return new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                for(int serviceIndex = 0; serviceIndex < courier.services.size(); serviceIndex++){
+                    mainView.setServiceCondition(isChecked, serviceIndex, courierIndex);
+                }
+            }
+        };
+    }
+
+    private boolean isWhitelabelService(Courier courier) {
+        return EditShippingConstant.INSTANCE.getWHITELABEL_SHIPPER_ID().contains(Long.parseLong(courier.id));
     }
 
     private void setCourierInformation(final String information, final String courierName,
