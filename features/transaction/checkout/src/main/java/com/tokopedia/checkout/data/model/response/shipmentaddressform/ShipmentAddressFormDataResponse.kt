0e85d52ec6418f0b8e1f3043b3995d4ff1df195b
mohamad.andriyan <andriyan@tokopedia.com>
2022-10-17 17:53:41 +0700

    formatting some spacing

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/model/response/shipmentaddressform/ShipmentAddressFormDataResponse.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/model/response/shipmentaddressform/ShipmentAddressFormDataResponse.kt
@@ -8,64 +8,64 @@ import com.tokopedia.purchase_platform.common.feature.promo.domain.model.PromoSA
 import com.tokopedia.purchase_platform.common.feature.tickerannouncement.Ticker
 
 data class ShipmentAddressFormDataResponse(
-    @SerializedName("errors")
+        @SerializedName("errors")
         val errors: List<String> = emptyList(),
-    @SerializedName("error_code")
+        @SerializedName("error_code")
         val errorCode: Int = 0,
-    @SerializedName("group_address")
+        @SerializedName("group_address")
         val groupAddress: List<GroupAddress> = emptyList(),
-    @SerializedName("kero_token")
+        @SerializedName("kero_token")
         val keroToken: String = "",
-    @SerializedName("kero_discom_token")
+        @SerializedName("kero_discom_token")
         val keroDiscomToken: String = "",
-    @SerializedName("kero_unix_time")
+        @SerializedName("kero_unix_time")
         val keroUnixTime: Int = 0,
-    @SerializedName("pop_up")
+        @SerializedName("pop_up")
         val popup: PopUp = PopUp( ),
-    @SerializedName("add_on_wording")
+        @SerializedName("add_on_wording")
         val addOnWording: AddOnWording = AddOnWording( ),
-    @SerializedName("donation")
+        @SerializedName("donation")
         val donation: Donation = Donation(),
-    @SerializedName("cod")
+        @SerializedName("cod")
         val cod: Cod = Cod(),
-    @SerializedName("is_hide_courier_name")
+        @SerializedName("is_hide_courier_name")
         val hideCourier: Boolean = false,
-    @SerializedName("is_blackbox")
+        @SerializedName("is_blackbox")
         val isBlackbox: Int = 0,
-    @SerializedName("egold_attributes")
+        @SerializedName("egold_attributes")
         val egoldAttributes: EgoldAttributes = EgoldAttributes(),
-    @SerializedName("is_show_onboarding")
+        @SerializedName("is_show_onboarding")
         val isShowOnboarding: Boolean = false,
-    @SerializedName("is_ineligible_promo_dialog_enabled")
+        @SerializedName("is_ineligible_promo_dialog_enabled")
         val isIneligiblePromoDialogEnabled: Boolean = false,
-    @SerializedName("disabled_features")
+        @SerializedName("disabled_features")
         val disabledFeatures: List<String> = emptyList(),
-    @SerializedName("tickers")
+        @SerializedName("tickers")
         val tickers: List<Ticker> = emptyList(),
-    @SerializedName("donation_checkbox_status")
+        @SerializedName("donation_checkbox_status")
         val isDonationCheckboxStatus: Boolean = false,
-    @SerializedName("campaign_timer")
+        @SerializedName("campaign_timer")
         val campaignTimer: CampaignTimer = CampaignTimer(),
-    @SerializedName("addresses")
+        @SerializedName("addresses")
         val addresses: Addresses = Addresses(),
-    @SerializedName("disabled_features_detail")
+        @SerializedName("disabled_features_detail")
         val disabledFeaturesDetail: DisabledFeaturesDetail = DisabledFeaturesDetail(),
-    @SerializedName("promo")
+        @SerializedName("promo")
         val promoSAFResponse: PromoSAFResponse = PromoSAFResponse(),
-    @SerializedName("open_prerequisite_site")
+        @SerializedName("open_prerequisite_site")
         val isOpenPrerequisiteSite: Boolean = false,
-    @SerializedName("eligible_new_shipping_experience")
+        @SerializedName("eligible_new_shipping_experience")
         val isEligibleNewShippingExperience: Boolean = false,
-    @SerializedName("pop_up_message")
+        @SerializedName("pop_up_message")
         val popUpMessage: String = "",
-    @SerializedName("error_ticker")
+        @SerializedName("error_ticker")
         val errorTicker: String = "",
-    @SerializedName("cross_sell")
+        @SerializedName("cross_sell")
         val crossSell: List<CrossSellResponse> = emptyList(),
-    @SerializedName("image_upload")
+        @SerializedName("image_upload")
         val imageUpload: ImageUploadResponse = ImageUploadResponse(),
-    @SerializedName("upsell")
+        @SerializedName("upsell")
         val upsell: Upsell = Upsell(),
-    @SerializedName("cart_data")
+        @SerializedName("cart_data")
         val cartData: String = "",
 )
