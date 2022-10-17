53b4a30428878e965b8b562667b88b48513b85c0
Muhammad Fiqri Muthohar <crescenthikari@users.noreply.github.com>
2022-10-17 09:47:22 +0700

    Update features/discovery/video_widget/src/main/java/com/tokopedia/video_widget/VideoPlayerController.kt
    Co-authored-by: sonoda-bots[bot] <113779905+sonoda-bots[bot]@users.noreply.github.com>

--- features/discovery/video_widget/src/main/java/com/tokopedia/video_widget/VideoPlayerController.kt
+++ features/discovery/video_widget/src/main/java/com/tokopedia/video_widget/VideoPlayerController.kt
@@ -2,7 +2,6 @@ package com.tokopedia.video_widget
 
 import android.app.Activity
 import android.app.Application
-import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
