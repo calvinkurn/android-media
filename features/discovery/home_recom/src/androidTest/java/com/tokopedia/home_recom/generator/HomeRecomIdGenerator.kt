723267bb85450e74be28a77623ee48e3f1d039ef
Dhaba Widhikari <wikodhawi@gmail.com>
2022-10-17 16:19:05 +0700

    add class HomeRecomIdGenerator.kt

---
+++ features/discovery/home_recom/src/androidTest/java/com/tokopedia/home_recom/generator/HomeRecomIdGenerator.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.home_recom.generator
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.runner.RunWith
+
+/**
+ * Created by dhaba
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HomeRecomIdGenerator {
+}
