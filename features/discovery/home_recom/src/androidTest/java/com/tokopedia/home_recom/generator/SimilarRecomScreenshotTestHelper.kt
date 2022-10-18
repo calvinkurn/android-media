fe9379e052555bea30cb445da0d220440c872e4d
Dhaba Widhikari <wikodhawi@gmail.com>
2022-10-18 20:44:29 +0700

    add data to 3 product recom

--- features/discovery/home_recom/src/androidTest/java/com/tokopedia/home_recom/generator/SimilarRecomScreenshotTestHelper.kt
+++ features/discovery/home_recom/src/androidTest/java/com/tokopedia/home_recom/generator/SimilarRecomScreenshotTestHelper.kt
@@ -6,10 +6,9 @@ package com.tokopedia.home_recom.generator
 object SimilarRecomScreenshotTestHelper {
     fun getWidgetScreenshotList(): List<ScreenshotModel> {
         return listOf(
-            ScreenshotModel(name = "ProductInfo"),
-            ScreenshotModel(name = "TitleCarouselVertical"),
-            ScreenshotModel(name = "ProductRecommendationVertical"),
-            ScreenshotModel(name = "RecommendationCarousel"),
+            ScreenshotModel(name = "ProductRecom1"),
+            ScreenshotModel(name = "ProductRecom2"),
+            ScreenshotModel(name = "ProductRecom3"),
         )
     }
 }
