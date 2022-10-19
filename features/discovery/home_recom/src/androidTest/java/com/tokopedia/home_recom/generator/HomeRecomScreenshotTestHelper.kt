20d6ef3667f17f9107cfb0b119913d8254364a68
Dhaba Widhikari <wikodhawi@gmail.com>
2022-10-19 13:34:17 +0700

    change list data recom page

--- features/discovery/home_recom/src/androidTest/java/com/tokopedia/home_recom/generator/HomeRecomScreenshotTestHelper.kt
+++ features/discovery/home_recom/src/androidTest/java/com/tokopedia/home_recom/generator/HomeRecomScreenshotTestHelper.kt
@@ -6,7 +6,10 @@ package com.tokopedia.home_recom.generator
 object HomeRecomScreenshotTestHelper {
     fun getWidgetScreenshotList(): List<ScreenshotModel> {
         return listOf(
-            ScreenshotModel(name = "SimilarRecom"),
+            ScreenshotModel(name = "ProductInfo"),
+            ScreenshotModel(name = "TitleCarouselVertical"),
+            ScreenshotModel(name = "ProductRecommendationVertical"),
+            ScreenshotModel(name = "RecommendationCarousel"),
         )
     }
 }
