f818230ffebde92f946c8a55b3543d6fe9210565
edwin91 <edwin.nurwansyah@tokopedia.com>
2022-10-25 17:54:10 +0700

    Fixing Unit Testing

--- features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/data/Variant_DataFactory.kt
+++ features/merchant/product_manage_list/src/test/java/com/tokopedia/product/manage/data/Variant_DataFactory.kt
@@ -11,6 +11,7 @@ import com.tokopedia.product.manage.common.feature.variant.data.model.Selection
 import com.tokopedia.product.manage.common.feature.variant.data.model.Variant
 import com.tokopedia.product.manage.common.feature.variant.data.model.response.GetProductVariantResponse
 import com.tokopedia.product.manage.common.feature.variant.presentation.data.EditVariantResult
+import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductCampaignType
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
 
 fun createProductVariantResponse(
@@ -24,7 +25,11 @@ fun createProductVariantResponse(
     stockAlertStatus: String = "2",
     stockAlertCount: Int = 5,
     pictures: List<Picture> = emptyList(),
-    isCampaign: Boolean = false
+    isCampaign: Boolean = false,
+    hasStockAlert: Boolean = false,
+    notifyMeOOSCount: Int = 0,
+    isBelowStockAlert: Boolean = false,
+    isEmptyStock: Boolean = false
 ): Product {
     return Product(
         productID,
@@ -37,7 +42,11 @@ fun createProductVariantResponse(
         stock,
         stockAlertStatus,
         stockAlertCount,
-        pictures
+        pictures,
+        hasStockAlert = hasStockAlert,
+        isEmptyStock = isEmptyStock,
+        isBelowStockAlert = isBelowStockAlert,
+        notifymeCount = notifyMeOOSCount
     )
 }
 
@@ -74,7 +83,11 @@ fun createProductVariant(
     isAllStockEmpty: Boolean = true,
     access: ProductManageAccess = createShopOwnerAccess(),
     campaignTypeList: List<CampaignType>? = emptyList(),
-    maxStock: Int? = null
+    maxStock: Int? = null,
+    notifyMeOOSCount: Int = 0,
+    stockAlertStatus: Int = 0,
+    stockAlertCount: Int = 0,
+    isBelowStockAlert: Boolean = false
 ): ProductVariant {
     return ProductVariant(
         id,
@@ -90,7 +103,11 @@ fun createProductVariant(
         isAllStockEmpty,
         access,
         campaignTypeList,
-        maxStock
+        maxStock,
+        notifyMeOOSCount,
+        stockAlertStatus,
+        stockAlertCount,
+        isBelowStockAlert
     )
 }
 
@@ -102,7 +119,8 @@ fun createGetVariantResponse(
     stock: Int = 10,
     products: List<Product> = emptyList(),
     selections: List<Selection> = emptyList(),
-    sizeCharts: List<Picture> = emptyList()
+    sizeCharts: List<Picture> = emptyList(),
+    notifyMeOOSCount: Int = 0
 ): GetProductVariantResponse {
     val variantResponse = Variant(products, selections, sizeCharts)
     val getProductV3Response = GetProductV3(
@@ -111,6 +129,7 @@ fun createGetVariantResponse(
         stockAlertCount,
         stockAlertStatus,
         stock,
+        notifyMeOOSCount,
         variantResponse
     )
     return GetProductVariantResponse(getProductV3Response)
