6156843679ed3889c36c23ad6dcd9425a8acf486
steven.fredian <steven.fredian@tokopedia.com>
2022-10-25 11:36:03 +0700

    blind fix parcelize

--- features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/domain/model/MetaData.kt
+++ features/discovery/topadssdk/src/main/java/com/tokopedia/topads/sdk/domain/model/MetaData.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.topads.sdk.domain.model
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
@@ -7,4 +8,4 @@ import kotlinx.android.parcel.Parcelize
 data class MetaData(
     @SerializedName("display")
     var display: String? = null
-)
+): Parcelable
