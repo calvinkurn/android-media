17a2f04f1363768f3e49ab8abe1f26be7a0f6320
edwinnrw <101694276+edwinnrw@users.noreply.github.com>
2022-10-25 18:15:02 +0700

    Update features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/adapter/TableItemAdapter.kt
    Co-authored-by: sonoda-bots[bot] <113779905+sonoda-bots[bot]@users.noreply.github.com>

--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/adapter/TableItemAdapter.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/adapter/TableItemAdapter.kt
@@ -4,7 +4,6 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseAdapter
 import com.tokopedia.sellerhomecommon.presentation.adapter.factory.TableItemFactoryImpl
 import com.tokopedia.sellerhomecommon.presentation.view.viewholder.TableColumnHtmlViewHolder
-import com.tokopedia.sellerhomecommon.presentation.view.viewholder.TableColumnHtmlWithIconViewHolder
 
 /**
  * Created By @ilhamsuaib on 01/07/20
