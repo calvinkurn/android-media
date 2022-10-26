96799ce301d5075dfc2aac6dde71244870954ea3
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-10-26 22:38:56 +0700

    fix: remove supresslint

--- libraries/bubbles/src/main/java/com/tokopedia/bubbles/factory/BubblesFactoryImpl.kt
+++ libraries/bubbles/src/main/java/com/tokopedia/bubbles/factory/BubblesFactoryImpl.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.bubbles.factory
 
-import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.PendingIntent
 import android.content.ComponentName
