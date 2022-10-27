1379f87b16249b989c5a1b57c368e3eb7fb7819d
astidhiyaa <asti.anzaria@tokopedia.com>
2022-10-27 18:01:12 +0800

    feat: add bottom sheet

---
+++ features/content/play/src/main/java/com/tokopedia/play/view/bottomsheet/PlayFollowBottomSheet.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.play.view.bottomsheet
+
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import javax.inject.Inject
+
+/**
+ * @author by astidhiyaa on 27/10/22
+ */
+class PlayFollowBottomSheet @Inject constructor() : BottomSheetUnify () {
+    companion object {
+
+    }
+}
