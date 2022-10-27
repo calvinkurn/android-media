eb39dc74cb5b3d68bace1dc9e9ca2976488d2fe3
fajarca <fajar.azhar@tokopedia.com>
2022-10-27 16:33:24 +0700

    Add status bar color adjustment

--- libraries/common_compose/src/main/java/com/tokopedia/common_compose/ui/Theme.kt
+++ libraries/common_compose/src/main/java/com/tokopedia/common_compose/ui/Theme.kt
@@ -1,6 +1,8 @@
 package com.tokopedia.common_compose.ui
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
@@ -8,7 +10,11 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 
 
 private val NestThemeLight = lightColors(
@@ -35,13 +41,11 @@ fun NestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val themeColors = if (darkTheme) {
-        NestThemeDark
-    } else {
-        NestThemeLight
-    }
+    val themeColors = if (darkTheme) NestThemeDark else NestThemeLight
     val elevation = if (darkTheme) DarkElevation else LightElevation
     val colors = populateColor(darkTheme)
+
+    AdaptiveStatusBarColor(themeColors = themeColors)
 
     CompositionLocalProvider(
         LocalElevations provides elevation,
@@ -53,6 +57,22 @@ fun NestTheme(
             typography = OpenSauceTypography,
             content = content
         )
+    }
+}
+
+@Composable
+private fun AdaptiveStatusBarColor(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    themeColors: Colors
+) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = themeColors.primary.toArgb()
+
+            WindowCompat.getInsetsController(window, view)?.isAppearanceLightStatusBars = !darkTheme
+        }
     }
 }
 
