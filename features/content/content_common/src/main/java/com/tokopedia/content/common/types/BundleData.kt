83600e3bd63300e3a3610997c474bc40832a58f6
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-10-27 15:43:21 +0800

    add BundleData.kt

---
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/types/BundleData.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.content.common.types
+
+object BundleData {
+    const val KEY_PLAY_BROADCASTER_ENTRY_POINT = "key_play_broadcaster_entry_point"
+
+    const val VALUE_PLAY_BROADCASTER_ENTRY_POINT_FEED_PAGE = 1
+    const val VALUE_PLAY_BROADCASTER_ENTRY_POINT_USER_PROFILE = 2
+}
