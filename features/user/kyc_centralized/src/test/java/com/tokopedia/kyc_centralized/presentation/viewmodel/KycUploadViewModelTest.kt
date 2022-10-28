01530a73d5f2bbf7f2ddb4c4da023e9114056c5b
XORByte29 <rivaldyfirmansyah@gmail.com>
2022-10-28 09:09:08 +0800

    increase UT

--- features/user/kyc_centralized/src/test/java/com/tokopedia/kyc_centralized/presentation/viewmodel/KycUploadViewModelTest.kt
+++ features/user/kyc_centralized/src/test/java/com/tokopedia/kyc_centralized/presentation/viewmodel/KycUploadViewModelTest.kt
@@ -195,7 +195,10 @@ class KycUploadViewModelTest {
     fun `Register - Failed and get error header response`() {
         val kycResponse = KycResponse().apply {
             header?.errorCode = "9999"
-            header?.message = mutableListOf("Error message on header")
+            header?.message = mutableListOf(
+                "Error message on header 1",
+                "Error message on header 2"
+            )
         }
 
         provideEveryUseCase(kycResponse)
@@ -463,5 +466,36 @@ class KycUploadViewModelTest {
         assert(viewModel.kycResponseLiveData.value is Fail)
         val result = viewModel.kycResponseLiveData.value as Fail
         assert(result.throwable.message?.contains(KycUploadErrorCodeUtil.FAILED_ENCRYPTION) == true)
+    }
+
+    @Test
+    fun `test sharedPref null`() {
+        coEvery {
+            sharedPreference.getByteArrayCache(any())
+        } returns null
+
+        uploadWithEncrypt()
+        assert(viewModel.kycResponseLiveData.value is Fail)
+    }
+
+    @Test
+    fun `error on header but empty message`() {
+        val kycResponse = KycResponse().apply {
+            header?.message = mutableListOf("")
+            header?.errorCode = null
+        }
+
+        provideEveryUseCase(kycResponse)
+        provideEverySuccessEncrypt()
+        provideEverySuccessDecrypted(originalImagePath)
+
+        coEvery {
+            sharedPreference.getByteArrayCache(any())
+        } answers {
+            encryptedImagePath.encodeToByteArray()
+        }
+
+        uploadWithEncrypt()
+        assert(viewModel.kycResponseLiveData.value is Fail)
     }
 }
