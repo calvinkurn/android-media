cb2ca8ac8fbdb25bf156c471070f6a3baf366306
astidhiyaa <asti.anzaria@tokopedia.com>
2022-10-31 13:35:56 +0800

    ut (2)

--- features/content/play/src/test/java/com/tokopedia/play/viewmodel/PlayEngagementWidgetTest.kt
+++ features/content/play/src/test/java/com/tokopedia/play/viewmodel/PlayEngagementWidgetTest.kt
@@ -9,8 +9,11 @@ import com.tokopedia.play.ui.engagement.model.EngagementUiModel
 import com.tokopedia.play.util.*
 import com.tokopedia.play.view.type.PlayChannelType
 import com.tokopedia.play.view.type.VideoOrientation
+import com.tokopedia.play.view.uimodel.action.PlayViewerNewAction
 import com.tokopedia.play.websocket.response.PlayMerchantVoucherSocketResponse
 import com.tokopedia.play_common.model.dto.interactive.InteractiveUiModel
+import com.tokopedia.play_common.model.ui.QuizChoicesUiModel
+import com.tokopedia.play_common.view.game.quiz.PlayQuizOptionState
 import com.tokopedia.play_common.websocket.PlayWebSocket
 import com.tokopedia.play_common.websocket.WebSocketAction
 import com.tokopedia.play_common.websocket.WebSocketResponse
@@ -75,11 +78,6 @@ class PlayEngagementWidgetTest {
         every { mockRemoteConfig.getBoolean(any(), any()) } returns true
     }
 
-    //add each test for game
-    //check if voucher list for coupon not empty
-    //click voucher open coupon sheet
-    //close voucher
-
     /**
      * Voucher
      */
@@ -122,7 +120,7 @@ class PlayEngagementWidgetTest {
     }
 
     @Test
-    fun `voucher is exist but no highlighted voucher` () {
+    fun `voucher is exist but no highlighted voucher`() {
         coEvery { repo.getTagItem(any(), any(), any()) } returns uiModelBuilder.buildTagItem(
             voucher = uiModelBuilder.buildVoucherModel(
                 voucherList = listOf(uiModelBuilder.buildMerchantVoucher())
@@ -141,6 +139,81 @@ class PlayEngagementWidgetTest {
             state.engagement.data.assertEmpty()
             state.engagement.shouldShow.assertFalse()
 
+        }
+    }
+
+    @Test
+    fun `voucher is exist but no highlighted voucher but socket return highlighted voucher`() {
+        coEvery { repo.getTagItem(any(), any(), any()) } returns uiModelBuilder.buildTagItem(
+            voucher = uiModelBuilder.buildVoucherModel(
+                voucherList = listOf(uiModelBuilder.buildMerchantVoucher())
+            )
+        )
+
+        every { socket.listenAsFlow() } returns socketFlow
+
+        createPlayViewModelRobot(
+            repo = repo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+            playChannelWebSocket = socket,
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+
+            val state = it.recordState {}
+            state.engagement.data.assertEmpty()
+            state.engagement.shouldShow.assertFalse()
+
+            val state2 = it.recordState {
+                socketFlow.emit(WebSocketAction.NewMessage(mockVoucherSocketResponse))
+            }
+            state2.engagement.data.first().assertType<EngagementUiModel.Promo> { }
+            state2.engagement.data.size.assertEqualTo(1)
+            state2.engagement.shouldShow.assertTrue()
+        }
+    }
+
+    @Test
+    fun `voucher clicked - open shop coupon sheet`() {
+        every { socket.listenAsFlow() } returns socketFlow
+
+        createPlayViewModelRobot(
+            repo = repo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+            playChannelWebSocket = socket
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+
+            val state = it.recordState {
+                socketFlow.emit(WebSocketAction.NewMessage(mockVoucherSocketResponse))
+                it.viewModel.showCouponSheet(100)
+            }
+            state.engagement.shouldShow.assertFalse()
+        }
+    }
+
+    @Test
+    fun `voucher clicked - open shop coupon sheet then close show widget`() {
+        every { socket.listenAsFlow() } returns socketFlow
+
+        createPlayViewModelRobot(
+            repo = repo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+            playChannelWebSocket = socket
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+
+            val state = it.recordState {
+                socketFlow.emit(WebSocketAction.NewMessage(mockVoucherSocketResponse))
+                it.viewModel.showCouponSheet(100)
+                it.viewModel.hideCouponSheet()
+            }
+            state.engagement.shouldShow.assertTrue()
         }
     }
 
@@ -189,6 +262,55 @@ class PlayEngagementWidgetTest {
             }
             state.engagement.data.size.assertEqualTo(1)
             state.engagement.shouldShow.assertTrue()
+        }
+    }
+
+    @Test
+    fun `game quiz - when only game exist - when user open game, hide`() {
+        every { socket.listenAsFlow() } returns socketFlow
+
+        coEvery { repo.getCurrentInteractive(any()) } returns uiModelBuilder.buildQuiz(
+            status = InteractiveUiModel.Quiz.Status.Ongoing(
+                200L.millisFromNow()
+            )
+        )
+
+        createPlayViewModelRobot(
+            repo = repo,
+            dispatchers = testDispatcher,
+            remoteConfig = mockRemoteConfig,
+            playChannelWebSocket = socket
+        ).use {
+            it.createPage(mockChannelData)
+            it.focusPage(mockChannelData)
+
+            val state = it.recordState {}
+            state.interactive.interactive.assertInstanceOf<InteractiveUiModel.Quiz>()
+            state.engagement.data.first().assertType<EngagementUiModel.Game> { dt ->
+                dt.interactive.assertType<InteractiveUiModel.Quiz> { }
+            }
+            state.engagement.data.size.assertEqualTo(1)
+            state.engagement.shouldShow.assertTrue()
+
+            val state2 = it.recordState {
+                it.submitAction(PlayViewerNewAction.StartPlayingInteractive)
+                it.submitAction(
+                    PlayViewerNewAction.ClickQuizOptionAction(
+                        QuizChoicesUiModel(
+                            id = "1",
+                            index = 0,
+                            type = PlayQuizOptionState.Participant('A', false, "100", false),
+                            text = "jawaban a"
+                        )
+                    )
+                )
+            } //answer / open game
+
+            state2.interactive.isPlaying.assertTrue()
+            state2.engagement.shouldShow.assertFalse()
+            state.engagement.data.first().assertType<EngagementUiModel.Game> { dt ->
+                dt.interactive.assertType<InteractiveUiModel.Quiz> {  }
+            }
         }
     }
 
