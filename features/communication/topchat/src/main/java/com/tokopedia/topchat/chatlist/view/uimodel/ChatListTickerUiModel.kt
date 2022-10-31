5b046a9b20c91eceb0c126fea4b808ab146dc747
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-10-31 14:06:39 +0700

    fix: change var to val

--- features/communication/topchat/src/main/java/com/tokopedia/topchat/chatlist/view/uimodel/ChatListTickerUiModel.kt
+++ features/communication/topchat/src/main/java/com/tokopedia/topchat/chatlist/view/uimodel/ChatListTickerUiModel.kt
@@ -4,7 +4,7 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.topchat.chatlist.view.adapter.typefactory.ChatListTypeFactory
 
 data class ChatListTickerUiModel(
-    var message: String,
+    val message: String,
     val tickerType: Int,
     val appLink: String
 ): Visitable<ChatListTypeFactory> {
