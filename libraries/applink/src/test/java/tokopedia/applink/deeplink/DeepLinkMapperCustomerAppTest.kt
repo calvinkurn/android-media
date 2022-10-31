fd6da0bdcbf0912bc6db9cd51ffa8dab48051204
hiradevina <fakhira.devina@tokopedia.com>
2022-10-31 16:23:28 +0700

    wip: unit test

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -34,7 +34,7 @@ import org.robolectric.RobolectricTestRunner
 class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
 
     companion object {
-        const val SIZE_MAPPER = 205
+        const val SIZE_MAPPER = 206
     }
 
     override fun setup() {
@@ -1571,6 +1571,14 @@ class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
         val expectedDeepLink =
             "${DeeplinkConstant.SCHEME_INTERNAL}://logistic/reschedulepickup?order_id=1234567890"
         val appLink = "${ApplinkConst.LOGISTIC_SELLER_RESCHEDULE}?order_id=1234567890"
+        assertEqualsDeepLinkMapper(appLink, expectedDeepLink)
+    }
+
+    @Test
+    fun `check edit address applink then should return tokopedia internal edit address revamp in customerapp`() {
+        val expectedDeepLink =
+            "${DeeplinkConstant.SCHEME_INTERNAL}://logistic/editaddressrevamp/1234567890"
+        val appLink = "${ApplinkConst.SETTING_EDIT_ADDRESS}?id=1234567890"
         assertEqualsDeepLinkMapper(appLink, expectedDeepLink)
     }
 
