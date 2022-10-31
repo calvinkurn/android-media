0aba4c34d78b00ddd6761c6619ffdc0d6d027098
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2022-10-31 11:07:22 +0700

    [BOM] Change PoD info key

--- features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/common/constants/BuyerOrderDetailLogisticSectionInfoID.kt
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/common/constants/BuyerOrderDetailLogisticSectionInfoID.kt
@@ -2,5 +2,5 @@ package com.tokopedia.buyerorderdetail.common.constants
 
 object BuyerOrderDetailLogisticSectionInfoID {
     const val DRIVER_TIPPING_INFO = "drivertip"
-    const val POD_INFO = "pod"
+    const val POD_INFO = "podafterfinish"
 }
