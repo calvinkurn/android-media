281ac12e84795e505d309d4a475e1ea609b08f75
reivinoktavianus <oktavianusreivin@gmail.com>
2022-10-31 13:16:08 +0700

    feat: set correct image url

--- features/communication/topchat/src/main/java/com/tokopedia/topchat/common/util/BubbleChat.kt
+++ features/communication/topchat/src/main/java/com/tokopedia/topchat/common/util/BubbleChat.kt
@@ -4,9 +4,8 @@ object BubbleChat {
 
     object Url {
         const val BUBBLE_ACTIVATION_INTRO_IMAGE = "https://images.tokopedia.net/img/android/topchat/iv_bubble_chat_onboarding_url.png"
-        // TODO: set actual image url
-        const val BUBBLE_ACTIVATION_INTRO_1 = "https://images.tokopedia.net/img/android/topchat/iv_bubble_activation_guide_1_url.png"
-        const val BUBBLE_ACTIVATION_INTRO_2 = "https://images.tokopedia.net/img/android/topchat/iv_bubble_activation_guide_2_url.png"
+        const val BUBBLE_ACTIVATION_INTRO_1 = "https://images.tokopedia.net/img/android/topchat/ic_bubble_chat_shop.png"
+        const val BUBBLE_ACTIVATION_INTRO_2 = "https://images.tokopedia.net/img/android/topchat/ic_bubble_chat_handphone.png"
         const val BUBBLE_ACTIVATION_GUIDE_1 = "https://images.tokopedia.net/img/android/topchat/iv_bubble_activation_guide_1_url.png"
         const val BUBBLE_ACTIVATION_GUIDE_2 = "https://images.tokopedia.net/img/android/topchat/iv_bubble_activation_guide_2_url.png"
         const val BUBBLE_ACTIVATION_GUIDE_3 = "https://images.tokopedia.net/img/android/topchat/iv_bubble_activation_guide_3_url.png"
