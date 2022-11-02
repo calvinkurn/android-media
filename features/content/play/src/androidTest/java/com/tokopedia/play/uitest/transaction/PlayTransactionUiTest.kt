7d5a70b10741442a273ccea6eb54c6933824f668
astidhiyaa <asti.anzaria@tokopedia.com>
2022-11-02 15:27:08 +0800

    feat: add class

---
+++ features/content/play/src/androidTest/java/com/tokopedia/play/uitest/transaction/PlayTransactionUiTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.play.uitest.transaction
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.tokopedia.test.application.annotations.UiTest
+import org.junit.runner.RunWith
+
+/**
+ * @author by astidhiyaa on 02/11/22
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+@UiTest
+class PlayTransactionUiTest {
+}
