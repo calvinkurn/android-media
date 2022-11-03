08ae2a270b63d42ebea8e07fb5bebb70fa80aefc
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-11-03 18:26:30 +0800

    creating loading view holder

---
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/shoprecom/adapter/ShopRecomLoadingViewHolder.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.feedcomponent.shoprecom.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.feedcomponent.databinding.ItemShopRecommendationLoadingBinding
+
+/**
+ * created by fachrizalmrsln on 03/11/22
+ **/
+class ShopRecomLoadingViewHolder(
+    binding: ItemShopRecommendationLoadingBinding,
+) : RecyclerView.ViewHolder(binding.root)
