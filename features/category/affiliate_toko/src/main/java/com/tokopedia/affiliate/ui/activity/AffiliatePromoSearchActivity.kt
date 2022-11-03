3186589d5326a877e42f5ecf7bd25ba933cdfee9
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2022-11-03 15:14:07 +0530

    removed unnecessary viewmodel instantiating

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/activity/AffiliatePromoSearchActivity.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/activity/AffiliatePromoSearchActivity.kt
@@ -2,44 +2,26 @@ package com.tokopedia.affiliate.ui.activity
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.affiliate.di.AffiliateComponent
 import com.tokopedia.affiliate.di.DaggerAffiliateComponent
 import com.tokopedia.affiliate.ui.fragment.AffiliatePromoSearchFragment
-import com.tokopedia.affiliate.viewmodel.AffiliatePromoViewModel
-import com.tokopedia.basemvvm.viewcontrollers.BaseViewModelActivity
-import com.tokopedia.basemvvm.viewmodel.BaseViewModel
-import javax.inject.Inject
 
-class AffiliatePromoSearchActivity : BaseViewModelActivity<AffiliatePromoViewModel>() {
-    @Inject
-    lateinit var viewModelProvider: ViewModelProvider.Factory
+class AffiliatePromoSearchActivity : BaseSimpleActivity() {
 
-    private lateinit var affiliateVM: AffiliatePromoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initInject()
         supportActionBar?.hide()
-    }
-
-    override fun getVMFactory(): ViewModelProvider.Factory {
-        return viewModelProvider
-    }
-
-    override fun getViewModelType(): Class<AffiliatePromoViewModel> {
-        return AffiliatePromoViewModel::class.java
-    }
-
-    override fun setViewModel(viewModel: BaseViewModel) {
-        affiliateVM = viewModel as AffiliatePromoViewModel
     }
 
     override fun getNewFragment(): Fragment {
         return AffiliatePromoSearchFragment.newInstance()
     }
 
-    override fun initInject() {
+    private fun initInject() {
         getComponent().injectPromoSearchActivity(this)
     }
 
