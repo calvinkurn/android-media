b16b3aa010778d5da4d08feb492bf24c33e9c243
Misael Jonathan <misael.jonathan@tokopedia.com>
2022-11-04 22:31:54 +0700

    [Recharge] remove unused imports and logs

--- features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/RechargeCCFragment.kt
+++ features/recharge/recharge_credit_card/src/main/java/com/tokopedia/recharge_credit_card/RechargeCCFragment.kt
@@ -8,7 +8,6 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -519,8 +518,6 @@ class RechargeCCFragment :
     override fun onClearInput() {
         operatorIdSelected = ""
         productIdSelected = ""
-
-        // TODO: [Misael] check this
     }
 
     override fun onClickNavigationIcon() {
