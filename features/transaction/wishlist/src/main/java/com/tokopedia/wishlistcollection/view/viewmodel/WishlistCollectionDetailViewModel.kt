00bc998cb440f399ee1c525ff816c0115ae2a8d1
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2022-11-07 17:35:08 +0700

    change page_name when hit recom, adjust width wishlist card on cart page

--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/viewmodel/WishlistCollectionDetailViewModel.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/view/viewmodel/WishlistCollectionDetailViewModel.kt
@@ -299,7 +299,7 @@ class WishlistCollectionDetailViewModel @Inject constructor(
     }
 
     companion object {
-        private const val WISHLIST_PAGE_NAME = "wishlist"
-        private const val EMPTY_WISHLIST_PAGE_NAME = "empty_wishlist"
+        private const val WISHLIST_PAGE_NAME = "wlcollection"
+        private const val EMPTY_WISHLIST_PAGE_NAME = "wlcollection_empty"
     }
 }
