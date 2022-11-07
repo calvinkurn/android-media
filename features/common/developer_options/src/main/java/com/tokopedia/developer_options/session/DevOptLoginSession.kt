2898b5ad7ed80928171b28e5e6a7fb2f82db94c4
rezagama <rezagamahidayat@gmail.com>
2022-11-07 15:57:35 +0700

    Developer Option Login Session

---
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/session/DevOptLoginSession.kt
@@ -0,0 +1,63 @@
+package com.tokopedia.developer_options.session
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
+import com.tokopedia.remoteconfig.RemoteConfigKey
+import java.util.concurrent.TimeUnit
+
+class DevOptLoginSession(private val context: Context) {
+
+    companion object {
+        private const val LOGIN_SESSION_SHARED_PREF = "dev_opt_login_session"
+        private const val KEY_PASSWORD = "password_hash"
+        private const val KEY_LAST_UPDATED = "last_updated"
+
+        private const val SESSION_EXPIRED_DAYS = 14 // 2 weeks
+    }
+
+    private val sharedPref by lazy { createSharedPreference() }
+    private val remoteConfig by lazy { FirebaseRemoteConfigImpl(context) }
+
+    fun isLoggedIn(): Boolean {
+        return isPasswordValid() && !isSessionExpired()
+    }
+
+    fun setPassword(password: String) {
+        sharedPref.edit().putString(KEY_PASSWORD, password).apply()
+        sharedPref.edit().putLong(KEY_LAST_UPDATED, System.currentTimeMillis()).apply()
+    }
+
+    fun clear() {
+        sharedPref.edit().clear().apply()
+    }
+
+    private fun getPassword(): String? {
+        return sharedPref.getString(KEY_PASSWORD, "")
+    }
+
+    private fun getLastUpdated(): Long {
+        return sharedPref.getLong(KEY_LAST_UPDATED, 0)
+    }
+
+    private fun isPasswordValid(): Boolean {
+        val currentPassword = getPassword()
+        val serverPassword = getServerPassword()
+        return currentPassword == serverPassword
+    }
+
+    private fun isSessionExpired(): Boolean {
+        val lastUpdated = TimeUnit.MILLISECONDS.toDays(getLastUpdated())
+        val currentTime = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())
+        val diff = currentTime - lastUpdated
+        return diff >= SESSION_EXPIRED_DAYS
+    }
+
+    private fun getServerPassword(): String? {
+        return remoteConfig.getString(RemoteConfigKey.DEV_OPTS_AUTHORIZATION, "")
+    }
+
+    private fun createSharedPreference(): SharedPreferences {
+        return context.getSharedPreferences(LOGIN_SESSION_SHARED_PREF, Context.MODE_PRIVATE)
+    }
+}
