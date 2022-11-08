d0475e615ab6abf50b54392a75d1924744bf8b30
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2022-11-08 19:28:57 +0700

    Refactor Compose UI

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -1,34 +1,19 @@
 package com.tokopedia.tkpd
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.service.autofill.Validators.not
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.graphics.toColorInt
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.tokopedia.applink.ApplinkConst
 import com.tokopedia.applink.RouteManager
 import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform
-import com.tokopedia.common_compose.principles.NestButton
-import com.tokopedia.common_compose.principles.NestHeader
 import com.tokopedia.common_compose.ui.NestTheme
 import com.tokopedia.url.TokopediaUrl
 import com.tokopedia.user.session.UserSession
@@ -172,110 +157,15 @@ class MainActivity : AppCompatActivity() {
 
     data class Model(
         val applink: String = "",
-        val urlState: String = "live",
+        val urlState: String = "LIVE",
         val isDarkModeChecked: Boolean = false,
-        val loginText: String = "Logged in as fjlasdkjflkdsfeafesafdsaf"
+        val loginText: String = "Login"
     )
-}
 
-sealed interface HomeDestination {
-    object LOGIN : HomeDestination
-    object LOGOUT : HomeDestination
-    object DEVELOPER_OPTION : HomeDestination
-    object APPLINK : HomeDestination
-}
-
-@SuppressLint("UnsupportedDarkModeColor")
-@Composable
-fun TestAppHome(
-    model: MainActivity.Model = MainActivity.Model(),
-    onDarkModeChanged: () -> Unit = {},
-    onApplinkChanged: (String) -> Unit = {},
-    onNavigateTo: (HomeDestination) -> Unit = {}
-) {
-    Surface {
-        Column {
-            NestHeader(title = "Tokopedia Test App", showBackNav = false)
-            val urlBgColor =
-                (if (model.urlState.contains("live", true)) "#27ae60" else "#e67e22").toColorInt()
-            Text(
-                text = model.urlState,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 4.dp)
-                    .background(Color(urlBgColor)),
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colors.onPrimary,
-                style = NestTheme.typography.heading5
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 72.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = { onNavigateTo(HomeDestination.LOGIN) }
-                ) {
-                    Text(
-                        text = model.loginText,
-                        textAlign = TextAlign.Center,
-                        style = NestTheme.typography.heading6,
-                        color = Color.White
-                    )
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                NestButton(
-                    text = "Logout",
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = { onNavigateTo(HomeDestination.LOGOUT) }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                NestButton(
-                    text = "Developer Option",
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = { onNavigateTo(HomeDestination.DEVELOPER_OPTION) }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    value = model.applink,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 4.dp),
-                    onValueChange = onApplinkChanged,
-                    textStyle = NestTheme.typography.body3,
-                    label = {
-                        Text(text = "applink")
-                    }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                NestButton(
-                    text = "Open",
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = { onNavigateTo(HomeDestination.APPLINK) }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .clickable { onDarkModeChanged() }
-                        .padding(8.dp)
-                ) {
-                    Checkbox(checked = model.isDarkModeChecked, onCheckedChange = null)
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = "Force Dark Mode")
-                }
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun TestAppHomePreview() {
-    NestTheme {
-        TestAppHome()
+    sealed interface HomeDestination {
+        object LOGIN : HomeDestination
+        object LOGOUT : HomeDestination
+        object DEVELOPER_OPTION : HomeDestination
+        object APPLINK : HomeDestination
     }
 }
