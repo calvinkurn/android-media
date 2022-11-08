1c4cf73d85c0980d925999701254d2548ffb5558
yohann.prananta <yohann.prananta@BA-00019887.local>
2022-11-08 09:27:28 +0700

    apply ktlint format to shop page module

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/domain/interactor/NewGetShopPageP1DataUseCase.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/domain/interactor/NewGetShopPageP1DataUseCase.kt
@@ -28,9 +28,9 @@ import javax.inject.Inject
 import javax.inject.Named
 
 class NewGetShopPageP1DataUseCase @Inject constructor(
-        @Named(GQLQueryNamedConstant.SHOP_PAGE_P1_QUERIES)
-        private var mapQuery: Map<String, String>,
-        private val gqlUseCase: MultiRequestGraphqlUseCase
+    @Named(GQLQueryNamedConstant.SHOP_PAGE_P1_QUERIES)
+    private var mapQuery: Map<String, String>,
+    private val gqlUseCase: MultiRequestGraphqlUseCase
 ) : UseCase<NewShopPageHeaderP1>() {
 
     companion object {
@@ -44,10 +44,10 @@ class NewGetShopPageP1DataUseCase @Inject constructor(
 
         @JvmStatic
         fun createParams(
-                shopId: String,
-                shopDomain: String,
-                extParam: String,
-                widgetUserAddressLocalData: LocalCacheModel
+            shopId: String,
+            shopDomain: String,
+            extParam: String,
+            widgetUserAddressLocalData: LocalCacheModel
         ): RequestParams = RequestParams.create().apply {
             putObject(PARAM_SHOP_ID, shopId)
             putObject(PARAM_SHOP_DOMAIN, shopDomain)
@@ -88,11 +88,13 @@ class NewGetShopPageP1DataUseCase @Inject constructor(
             add(getFeedWhitelistRequest(shopId))
         }
         gqlUseCase.clearRequest()
-        gqlUseCase.setCacheStrategy(GraphqlCacheStrategy.Builder(
+        gqlUseCase.setCacheStrategy(
+            GraphqlCacheStrategy.Builder(
                 if (isFromCacheFirst)
                     CacheType.CACHE_FIRST
                 else CacheType.ALWAYS_CLOUD
-        ).build())
+            ).build()
+        )
         gqlUseCase.addRequests(listRequest)
         val gqlResponse = gqlUseCase.executeOnBackground()
         val getIsOfficialData = getResponseData<GetIsShopOfficialStore.Response>(gqlResponse).result
@@ -102,41 +104,41 @@ class NewGetShopPageP1DataUseCase @Inject constructor(
         val getShopInfoCoreAndAssetsData = getResponseData<ShopInfo.Response>(gqlResponse).result.data.first()
         val getFeedWhitelist = getResponseData<WhitelistQuery>(gqlResponse).whitelist
         return NewShopPageHeaderP1(
-                getIsOfficialData,
-                getIsShopPowerMerchant,
-                getShopInfoTopContentData,
-                getShopDynamicTabData,
-                getShopInfoCoreAndAssetsData,
-                getFeedWhitelist
+            getIsOfficialData,
+            getIsShopPowerMerchant,
+            getShopInfoTopContentData,
+            getShopDynamicTabData,
+            getShopInfoCoreAndAssetsData,
+            getFeedWhitelist
         )
     }
 
     private fun getIsShopOfficialRequest(shopId: String): GraphqlRequest {
         val params = GqlGetIsShopOsUseCase.createParams(shopId.toIntOrZero())
         return createGraphqlRequest<GetIsShopOfficialStore.Response>(
-                GqlGetIsShopOsUseCase.QUERY,
-                params.parameters
+            GqlGetIsShopOsUseCase.QUERY,
+            params.parameters
         )
     }
 
     private fun getIsShopPowerMerchantRequest(shopId: String): GraphqlRequest {
         val params = GqlGetIsShopPmUseCase.createParams(shopId.toIntOrZero())
         return createGraphqlRequest<GetIsShopPowerMerchant.Response>(
-                GqlGetIsShopPmUseCase.QUERY,
-                params.parameters
+            GqlGetIsShopPmUseCase.QUERY,
+            params.parameters
         )
     }
 
     private fun getShopInfoTopContentDataRequest(shopId: String, shopDomain: String): GraphqlRequest {
         val params = GQLGetShopInfoUseCase.createParams(
-                if (shopId.toIntOrZero() == 0) listOf() else listOf(shopId.toIntOrZero()),
-                shopDomain,
-                source = SHOP_PAGE_SOURCE,
-                fields = listOf(GQLGetShopInfoUseCase.FIELD_TOP_CONTENT)
+            if (shopId.toIntOrZero() == 0) listOf() else listOf(shopId.toIntOrZero()),
+            shopDomain,
+            source = SHOP_PAGE_SOURCE,
+            fields = listOf(GQLGetShopInfoUseCase.FIELD_TOP_CONTENT)
         )
         return createGraphqlRequest<ShopInfo.Response>(
-                mapQuery[GQLQueryNamedConstant.SHOP_INFO_FOR_TOP_CONTENT].orEmpty(),
-                params.parameters
+            mapQuery[GQLQueryNamedConstant.SHOP_INFO_FOR_TOP_CONTENT].orEmpty(),
+            params.parameters
         )
     }
 
@@ -157,38 +159,38 @@ class NewGetShopPageP1DataUseCase @Inject constructor(
             longitude
         )
         return createGraphqlRequest<ShopPageGetDynamicTabResponse>(
-                GqlShopPageGetDynamicTabUseCase.QUERY,
-                params.parameters
+            GqlShopPageGetDynamicTabUseCase.QUERY,
+            params.parameters
         )
     }
 
     private fun getShopInfoCoreAndAssetsDataRequest(shopId: String, shopDomain: String): GraphqlRequest {
         val params = GQLGetShopInfoUseCase.createParams(
-                if (shopId.toIntOrZero() == 0) listOf() else listOf(shopId.toIntOrZero()),
-                shopDomain,
-                source = SHOP_PAGE_SOURCE,
-                fields = listOf(GQLGetShopInfoUseCase.FIELD_CORE, GQLGetShopInfoUseCase.FIELD_ASSETS)
+            if (shopId.toIntOrZero() == 0) listOf() else listOf(shopId.toIntOrZero()),
+            shopDomain,
+            source = SHOP_PAGE_SOURCE,
+            fields = listOf(GQLGetShopInfoUseCase.FIELD_CORE, GQLGetShopInfoUseCase.FIELD_ASSETS)
         )
         return createGraphqlRequest<ShopInfo.Response>(
-                mapQuery[GQLQueryNamedConstant.SHOP_INFO_FOR_CORE_AND_ASSETS].orEmpty(),
-                params.parameters
+            mapQuery[GQLQueryNamedConstant.SHOP_INFO_FOR_CORE_AND_ASSETS].orEmpty(),
+            params.parameters
         )
     }
 
     private fun getFeedWhitelistRequest(shopId: String): GraphqlRequest {
         val params = GetWhitelistUseCase.createRequestParams(
-                GetWhitelistUseCase.WHITELIST_SHOP,
-                shopId
+            GetWhitelistUseCase.WHITELIST_SHOP,
+            shopId
         )
         return createGraphqlRequest<WhitelistQuery>(
-                mapQuery[ShopPageHeaderConstant.SHOP_PAGE_FEED_WHITELIST].orEmpty(),
-                params
+            mapQuery[ShopPageHeaderConstant.SHOP_PAGE_FEED_WHITELIST].orEmpty(),
+            params
         )
     }
 
     private inline fun <reified T> createGraphqlRequest(
-            query: String,
-            parameters: Map<String, Any>
+        query: String,
+        parameters: Map<String, Any>
     ): GraphqlRequest {
         return GraphqlRequest(query, T::class.java, parameters)
     }
