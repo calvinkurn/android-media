1e9bddfef4e8cca8b5c6230d5a1840873232ab8e
astidhiyaa <asti.anzaria@tokopedia.com>
2022-11-09 17:43:58 +0800

    fix: conflict

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayUserInteractionFragment.kt
@@ -1221,7 +1221,6 @@ class PlayUserInteractionFragment @Inject constructor(
     private fun openProductSheet() {
         interactiveResultView?.hideCoachMark()
         playViewModel.onShowProductSheet(bottomSheetMaxHeight)
-        productAnalyticHelper.sendImpressedProductSheets()
     }
 
     private fun pushParentPlayByKeyboardHeight(estimatedKeyboardHeight: Int) {
