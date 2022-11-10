d5650ab634e8b83f36fff62080bd01f74f2d5bff
Yovi Eka Putra <yovi.putra@tokopedia.com>
2022-11-10 17:25:25 +0700

    [compose] annotation string for reason footer

--- features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonFooter.kt
+++ features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonFooter.kt
@@ -1,18 +1,16 @@
 package com.tokopedia.report.view.fragment.components
 
-import android.content.Context
-import android.text.method.LinkMovementMethod
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tokopedia.common_compose.principles.NestTypography
 import com.tokopedia.common_compose.ui.NestTheme
-import com.tokopedia.unifycomponents.HtmlLinkHelper
-import com.tokopedia.unifyprinciples.Typography
 
 /**
  * Created by yovi.putra on 07/09/22"
@@ -21,7 +19,6 @@ import com.tokopedia.unifyprinciples.Typography
 
 @Composable
 fun ProductReportReasonFooter(
-    context: Context = LocalContext.current,
     text: String,
     onClick: (String) -> Unit
 ) {
@@ -29,35 +26,29 @@ fun ProductReportReasonFooter(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
-        text = text,
+        text = composeSpannable(text = text, onClick = onClick),
         textStyle = NestTheme.typography.body3
     )
 }
 
+@Composable
 private fun composeSpannable(
-    typography: Typography,
     text: String,
     onClick: (String) -> Unit
-) = with(typography) {
-    movementMethod = LinkMovementMethod.getInstance()
-    val helper = HtmlLinkHelper(
-        context = context,
-        htmlString = text
-    )
-    helper.urlList.forEach {
-        it.onClick = {
-            onClick.invoke(it.linkUrl)
-        }
+) = buildAnnotatedString {
+    withStyle(style = SpanStyle(color = NestTheme.colors.GN500)) {
+        append("Pelajari Lebih Lanjut")
     }
-    this.text = helper.spannedString
+    append(" ")
+    append("tipe-tipe pelanggaran produk di Tokopedia")
 }
 
 @Preview
 @Composable
 fun ProductReportReasonFooterPreview() {
-    /*ProductReportReasonFooter(
-        stringResource(id = com.tokopedia.report.R.string.product_report_see_all_types)
+    ProductReportReasonFooter(
+        text = ""
     ) {
 
-    }*/
+    }
 }
