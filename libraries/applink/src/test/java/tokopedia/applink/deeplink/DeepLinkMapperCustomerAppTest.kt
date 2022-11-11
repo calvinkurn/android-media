4d6561aa0d65737ce4b1df78768306e68222a482
hiradevina <fakhira.devina@tokopedia.com>
2022-11-11 10:02:34 +0700

    fix: deeplink size mapper

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -34,7 +34,7 @@ import org.robolectric.RobolectricTestRunner
 class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
 
     companion object {
-        const val SIZE_MAPPER = 206
+        const val SIZE_MAPPER = 207
     }
 
     override fun setup() {
