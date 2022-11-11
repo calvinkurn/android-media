d0e69c1733cf7cc573f02e00396d6bbba08f4126
Rohan Ghosh <rohan.ghosh@tokopedia.com>
2022-11-11 13:06:07 +0530

    removed unwanted changes

--- libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
+++ libraries/webview/src/main/java/com/tokopedia/webview/BaseWebViewFragment.java
@@ -31,7 +31,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
