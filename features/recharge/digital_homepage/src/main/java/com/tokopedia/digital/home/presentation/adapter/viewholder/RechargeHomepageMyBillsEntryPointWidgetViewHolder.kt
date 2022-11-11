b1c5dc06bc51de55e869699cdbf68b9718adc848
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-11-11 14:01:48 +0800

    feat : create content shimmering

--- features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/viewholder/RechargeHomepageMyBillsEntryPointWidgetViewHolder.kt
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/viewholder/RechargeHomepageMyBillsEntryPointWidgetViewHolder.kt
@@ -5,6 +5,10 @@ import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.digital.home.model.RechargeHomepageMyBillsEntryPointModel
 import com.tokopedia.digital.home.R
+import com.tokopedia.digital.home.databinding.ViewRechargeHomeMyBillsEntrypointBinding
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.media.loader.loadImage
 
 /**
  * created by @bayazidnasir on 7/11/2022
@@ -19,7 +23,22 @@ class RechargeHomepageMyBillsEntryPointWidgetViewHolder(
         val LAYOUT = R.layout.view_recharge_home_my_bills_entrypoint
     }
 
-    override fun bind(element: RechargeHomepageMyBillsEntryPointModel?) {
+    override fun bind(element: RechargeHomepageMyBillsEntryPointModel) {
+        val binding = ViewRechargeHomeMyBillsEntrypointBinding.bind(itemView)
 
+        if (element.section.items.isNotEmpty()) {
+            binding.containerContent.visible()
+            binding.containerShimmer.gone()
+            val sectionItem = element.section.items.first()
+            with(binding) {
+                tvTitle.text = sectionItem.title
+                tvSubtitle.text = sectionItem.subtitle
+                labelWidget.text = sectionItem.label1
+                ivProductIcon.loadImage(sectionItem.mediaUrl)
+            }
+        } else {
+            binding.containerContent.gone()
+            binding.containerShimmer.visible()
+        }
     }
 }
