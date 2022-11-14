f2ab0f79f545623b0708a6c46ea8b5cbc1b7dc4c
ilhamsuaib <ilhamsuaib10@gmail.com>
2022-11-14 19:58:22 +0800

    new seller experience first order dialog

---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/dialog/NewSellerFirstOrderDialog.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.sellerhome.view.dialog
+
+/**
+ * Created by @ilhamsuaib on 14/11/22.
+ */
+
+class NewSellerFirstOrderDialog {
+}
---
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/dialog/NewSellerWelcomingDialog.kt
@@ -0,0 +1,37 @@
+package com.tokopedia.sellerhome.view.dialog
+
+import android.content.Context
+import com.tokopedia.dialog.DialogUnify
+import com.tokopedia.sellerhome.R
+
+/**
+ * Created by @ilhamsuaib on 14/11/22.
+ */
+
+object NewSellerWelcomingDialog {
+
+    private const val IMG_NEW_ORDER_DIALOG = ""
+
+    fun show(context: Context) {
+        val dialog = DialogUnify(
+            context,
+            DialogUnify.VERTICAL_ACTION,
+            DialogUnify.WITH_ILLUSTRATION
+        )
+
+        with(dialog) {
+            setImageUrl(IMG_NEW_ORDER_DIALOG)
+            setTitle(context.getString(R.string.sah_new_seller_dialog_first_order_title))
+            setDescription(context.getString(R.string.sah_new_seller_dialog_first_order_description))
+            setPrimaryCTAText(context.getString(R.string.sah_process_order))
+            setSecondaryCTAText(context.getString(R.string.sah_later))
+            setPrimaryCTAClickListener {
+                dismiss()
+            }
+            setSecondaryCTAClickListener {
+                dismiss()
+            }
+            show()
+        }
+    }
+}
