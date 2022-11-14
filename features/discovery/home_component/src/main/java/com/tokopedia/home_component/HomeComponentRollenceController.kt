5d7acfda51684521547968ba1278bf30ae5360cf
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2022-11-14 11:22:10 +0700

    remove lego rollence

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/HomeComponentRollenceController.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/HomeComponentRollenceController.kt
@@ -8,17 +8,7 @@ import com.tokopedia.remoteconfig.RollenceKey
  */
 object HomeComponentRollenceController {
 
-    private var rollenceLego24BannerValue: String = ""
-
     fun fetchHomeComponentRollenceValue() {
-        rollenceLego24BannerValue = RemoteConfigInstance.getInstance().abTestPlatform.getString(RollenceKey.HOME_COMPONENT_LEGO24BANNER_EXP, RollenceKey.HOME_COMPONENT_LEGO24BANNER_OLD)
-    }
 
-    private fun getRollenceValueLego24Banner(): String {
-        return if (rollenceLego24BannerValue.isNotEmpty()) rollenceLego24BannerValue else RollenceKey.HOME_COMPONENT_LEGO24BANNER_OLD
-    }
-
-    fun isHomeComponentLego24BannerUsingRollenceVariant(): Boolean {
-        return getRollenceValueLego24Banner() == RollenceKey.HOME_COMPONENT_LEGO24BANNER_VARIANT
     }
 }
