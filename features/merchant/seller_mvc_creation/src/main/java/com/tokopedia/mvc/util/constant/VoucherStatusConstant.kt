372a0cb7bcf528ed9a18ee3b417ec93d3ddfb4ee
Yuda <yuda.putra@tokopedia.com>
2022-11-16 21:10:05 +0700

    implement broadcast chat component

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/VoucherStatusConstant.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/VoucherStatusConstant.kt
@@ -4,7 +4,7 @@ object VoucherStatusConstant {
     const val DELETED = -1
     const val PROCESSING = 0
     const val NOT_STARTED = 1
-    const val ONGOING = 1
-    const val ENDED = 2
-    const val STOPPED = 3
+    const val ONGOING = 2
+    const val ENDED = 3
+    const val STOPPED = 4
 }
