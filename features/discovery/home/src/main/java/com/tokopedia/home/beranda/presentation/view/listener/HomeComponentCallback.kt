0d66205eaa3566772100b655904998b72bda2241
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2022-11-16 16:27:55 +0700

    Remove ExperimentalCoroutinesApi

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/listener/HomeComponentCallback.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/listener/HomeComponentCallback.kt
@@ -4,10 +4,8 @@ import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.home.beranda.listener.HomeCategoryListener
 import com.tokopedia.home_component.listener.HomeComponentListener
 import com.tokopedia.home_component.model.ChannelModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class HomeComponentCallback(val homeCategoryListener: HomeCategoryListener): HomeComponentListener {
-    @ExperimentalCoroutinesApi
     override fun onChannelExpired(channelModel: ChannelModel, channelPosition: Int, visitable: Visitable<*>) {
         if (channelModel.channelConfig.isAutoRefreshAfterExpired) {
             homeCategoryListener.getDynamicChannelData(visitable, channelModel, channelPosition)
