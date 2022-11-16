a13067ec787e1589c941a4b5f847c07ea1bf2153
fajarca <fajar.azhar@tokopedia.com>
2022-11-16 14:20:46 +0700

    Simplify code

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/data/mapper/GetInitiateVoucherPageMapper.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/data/mapper/GetInitiateVoucherPageMapper.kt
@@ -8,15 +8,16 @@ import javax.inject.Inject
 class GetInitiateVoucherPageMapper @Inject constructor() {
 
     fun map(response: GetInitiateVoucherPageResponse): VoucherCreationMetadata {
-        return VoucherCreationMetadata(
-            response.getInitiateVoucherPage.data.accessToken,
-            response.getInitiateVoucherPage.data.isEligible,
-            response.getInitiateVoucherPage.data.maxProduct,
-            response.getInitiateVoucherPage.data.prefixVoucherCode,
-            response.getInitiateVoucherPage.data.shopId.toLongOrZero(),
-            response.getInitiateVoucherPage.data.token,
-            response.getInitiateVoucherPage.data.userId.toLongOrZero()
-        )
-
+        return with(response.getInitiateVoucherPage.data) {
+            VoucherCreationMetadata(
+                accessToken,
+                isEligible,
+                maxProduct,
+                prefixVoucherCode,
+                shopId.toLongOrZero(),
+                token,
+                userId.toLongOrZero()
+            )
+        }
     }
 }
