b6f0674aff20ae86705620b2747867100a7f729d
yorisprayogo <yoris.prayogo@tokopedia.com>
2022-11-22 17:42:35 +0700

    revert to base url

--- features/user/privacycenter/src/main/java/com/tokopedia/privacycenter/main/section/privacypolicy/PrivacyPolicyConst.kt
+++ features/user/privacycenter/src/main/java/com/tokopedia/privacycenter/main/section/privacypolicy/PrivacyPolicyConst.kt
@@ -1,22 +1,14 @@
 package com.tokopedia.privacycenter.main.section.privacypolicy
 
-import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.url.TokopediaUrl
 
 object PrivacyPolicyConst {
 
-//    private val BASE_URL = "${TokopediaUrl.getInstance().API}ent-tnc/v1/privacy/history"
-
-    // for testing purpose
-    private val BASE_URL = "https://api-staging.tokopedia.com/ent-tnc/v1/privacy/history"
+    private val BASE_URL = "${TokopediaUrl.getInstance().API}ent-tnc/v1/privacy/history"
 
     val GET_LIST_URL = "$BASE_URL?language=id&showAll=true"
 
     val WEBVIEW_URL = "${TokopediaUrl.getInstance().MOBILEWEB}privacy/history/"
-
-    fun getPrivacyPolicyDetailUrl(sectionId: String): String {
-        return "$BASE_URL/${sectionId.toIntOrZero()}"
-    }
 
     const val RESPONSE_OK = "200"
     const val RESPONSE_SUCCESS = "success"
