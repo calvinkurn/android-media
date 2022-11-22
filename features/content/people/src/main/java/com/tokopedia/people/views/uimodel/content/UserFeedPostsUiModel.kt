2454aa800df41ae23ccacb71f7b1123beb6d05bf
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2022-11-22 16:29:27 +0800

    add default value

--- features/content/people/src/main/java/com/tokopedia/people/views/uimodel/content/UserFeedPostsUiModel.kt
+++ features/content/people/src/main/java/com/tokopedia/people/views/uimodel/content/UserFeedPostsUiModel.kt
@@ -1,13 +1,12 @@
 package com.tokopedia.people.views.uimodel.content
 
-import com.tokopedia.library.baseadapter.BaseItem
 
 /**
  * Created by fachrizalmrsln at 21/11/2022
  */
 data class UserFeedPostsUiModel(
-    val pagination: PaginationUiModel,
-    val posts: List<PostUiModel>,
+    val pagination: PaginationUiModel = PaginationUiModel(),
+    val posts: List<PostUiModel> = emptyList(),
 )
 
 data class PaginationUiModel(
@@ -20,7 +19,7 @@ data class PostUiModel(
     val id: String = "",
     val appLink: String = "",
     val media: List<MediaUiModel> = emptyList(),
-) : BaseItem()
+)
 
 data class MediaUiModel(
     val appLink: String = "",
