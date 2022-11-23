456ed617d584ad2153ac693636665dc919c530ef
faisalramd <faisal.ramdani@tokopedia.com>
2022-11-23 11:47:05 +0700

    other period placeholder layout

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/bottomsheet/OtherPeriodBottomSheet.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/bottomsheet/OtherPeriodBottomSheet.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tokopedia.mvc.R
-import com.tokopedia.mvc.databinding.SmvcBottomsheetFilterVoucherBinding
+import com.tokopedia.mvc.databinding.SmvcBottomsheetOtherPeriodVoucherBinding
 import com.tokopedia.unifycomponents.BottomSheetUnify
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 
 class OtherPeriodBottomSheet: BottomSheetUnify() {
 
-    private var binding by autoClearedNullable<SmvcBottomsheetFilterVoucherBinding>()
+    private var binding by autoClearedNullable<SmvcBottomsheetOtherPeriodVoucherBinding>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +24,7 @@ class OtherPeriodBottomSheet: BottomSheetUnify() {
     }
 
     private fun setupBottomSheet(inflater: LayoutInflater, container: ViewGroup?) {
-        binding = SmvcBottomsheetFilterVoucherBinding.inflate(inflater, container, false)
+        binding = SmvcBottomsheetOtherPeriodVoucherBinding.inflate(inflater, container, false)
         isKeyboardOverlap = false
         clearContentPadding = true
         setChild(binding?.root)
