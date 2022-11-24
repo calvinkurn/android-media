0f50d0f12d9cb3dc27052191668684e0fe7af7ba
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-11-24 17:49:38 +0700

    `feature` wrap shopName with MethodChecker.fromHtml() in summary page

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/view/fragment/PlayShortsSummaryFragment.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/view/fragment/PlayShortsSummaryFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.play.broadcaster.databinding.FragmentPlayShortsSummaryBinding
 import com.tokopedia.play.broadcaster.shorts.ui.model.action.PlayShortsAction
 import com.tokopedia.play.broadcaster.shorts.ui.model.event.PlayShortsUiEvent
@@ -148,7 +149,7 @@ class PlayShortsSummaryFragment @Inject constructor(
         }
 
         if (prev?.selectedAccount != curr.selectedAccount) {
-            binding.tvName.text = curr.selectedAccount.name
+            binding.tvName.text = MethodChecker.fromHtml(curr.selectedAccount.name)
             binding.ivProfile.setImageUrl(curr.selectedAccount.iconUrl)
         }
     }
