b87e5831217c8b1ef02d9fc6192d310bb1f9a11b
hansenpw <hansen.wijaya@tokopedia.com>
2022-11-24 09:57:30 +0700

    fix validation

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/adapter/ShipmentAdapter.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/adapter/ShipmentAdapter.java
@@ -651,26 +651,27 @@ public class ShipmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             for (int i = 0; i < shipmentDataList.size(); i++) {
                 Object shipmentData = shipmentDataList.get(i);
                 if (shipmentData instanceof ShipmentCartItemModel) {
-                    if (((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData() != null &&
-                            ((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().getUseDropshipper() != null &&
-                            ((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().getUseDropshipper()) {
-                        if (TextUtils.isEmpty(((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().getDropshipperName()) ||
-                                TextUtils.isEmpty(((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().getDropshipperPhone()) ||
-                                !((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().isDropshipperNameValid() ||
-                                !((ShipmentCartItemModel) shipmentData).getSelectedShipmentDetailData().isDropshipperPhoneValid()) {
+                    ShipmentCartItemModel shipmentCartItemModel = (ShipmentCartItemModel) shipmentData;
+                    if (shipmentCartItemModel.getSelectedShipmentDetailData() != null &&
+                            shipmentCartItemModel.getSelectedShipmentDetailData().getUseDropshipper() != null &&
+                            shipmentCartItemModel.getSelectedShipmentDetailData().getUseDropshipper()) {
+                        if (TextUtils.isEmpty(shipmentCartItemModel.getSelectedShipmentDetailData().getDropshipperName()) ||
+                                TextUtils.isEmpty(shipmentCartItemModel.getSelectedShipmentDetailData().getDropshipperPhone()) ||
+                                !shipmentCartItemModel.getSelectedShipmentDetailData().isDropshipperNameValid() ||
+                                !shipmentCartItemModel.getSelectedShipmentDetailData().isDropshipperPhoneValid()) {
                             availableCheckout = false;
                             errorPosition = i;
                             errorSelectedShipmentData = shipmentData;
                             break;
                         }
                     }
-                    if (((ShipmentCartItemModel) shipmentData).getHasEthicalProducts()) {
-                        boolean prescriptionIdsEmpty = ((ShipmentCartItemModel) shipmentData).getPrescriptionIds().isEmpty();
-                        boolean consultationEmpty = (TextUtils.isEmpty(((ShipmentCartItemModel) shipmentData).getTokoConsultationId()) ||
-                                TextUtils.isEmpty(((ShipmentCartItemModel) shipmentData).getPartnerConsultationId()) ||
-                                ((ShipmentCartItemModel) shipmentData).getTokoConsultationId().equals("0") ||
-                                ((ShipmentCartItemModel) shipmentData).getPartnerConsultationId().equals("0") ||
-                                TextUtils.isEmpty(((ShipmentCartItemModel) shipmentData).getConsultationDataString()));
+                    if (shipmentCartItemModel.getHasEthicalProducts() && !shipmentCartItemModel.isError()) {
+                        boolean prescriptionIdsEmpty = shipmentCartItemModel.getPrescriptionIds().isEmpty();
+                        boolean consultationEmpty = (TextUtils.isEmpty(shipmentCartItemModel.getTokoConsultationId()) ||
+                                TextUtils.isEmpty(shipmentCartItemModel.getPartnerConsultationId()) ||
+                                shipmentCartItemModel.getTokoConsultationId().equals("0") ||
+                                shipmentCartItemModel.getPartnerConsultationId().equals("0") ||
+                                TextUtils.isEmpty(shipmentCartItemModel.getConsultationDataString()));
                         if (prescriptionIdsEmpty && consultationEmpty) {
                             isPrescriptionFrontEndValidationError = true;
                             availableCheckout = false;
@@ -678,29 +679,7 @@ public class ShipmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                     }
                 }
             }
-
-//            if (availableCheckout) {
-//                for (int i = 0; i < shipmentDataList.size(); i++) {
-//                    Object shipmentData = shipmentDataList.get(i);
-//                    if (shipmentData instanceof UploadPrescriptionUiModel) {
-//                        if (((UploadPrescriptionUiModel) shipmentData).getFrontEndValidation()
-//                                && ((UploadPrescriptionUiModel) shipmentData).getUploadedImageCount() != null
-//                                && ((UploadPrescriptionUiModel) shipmentData).getUploadedImageCount() == 0) {
-//                            isPrescriptionFrontEndValidationError = true;
-//                            errorPosition = i;
-//                            errorSelectedShipmentData = shipmentData;
-//                        } else {
-//                            isPrescriptionFrontEndValidationError = false;
-//                        }
-//                    }
-//                }
-//            }
-
-//            if (isPrescriptionFrontEndValidationError) {
-//                shipmentAdapterActionListener.onCheckoutValidationResult(false, null, errorPosition);
-//            } else {
             shipmentAdapterActionListener.onCheckoutValidationResult(availableCheckout, errorSelectedShipmentData, errorPosition, isPrescriptionFrontEndValidationError);
-//            }
         } else {
             int errorPosition = 0;
             if (shipmentCartItemModelList != null && shipmentDataList != null) {
