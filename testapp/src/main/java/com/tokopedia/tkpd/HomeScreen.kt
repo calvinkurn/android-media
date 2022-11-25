3f90a49016ba7f0cc30b05e567cfc4042af43662
fajarca <fajar.azhar@tokopedia.com>
2022-11-25 13:26:58 +0700

    Use nest button

--- testapp/src/main/java/com/tokopedia/tkpd/HomeScreen.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/HomeScreen.kt
@@ -3,12 +3,23 @@ package com.tokopedia.tkpd
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Checkbox
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,17 +61,12 @@ fun HomeScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Button(
+                NestButton(
+                    text = model.loginText,
                     modifier = Modifier.fillMaxWidth(),
                     onClick = { onNavigateTo(MainActivity.HomeDestination.LOGIN) }
-                ) {
-                    Text(
-                        text = model.loginText,
-                        textAlign = TextAlign.Center,
-                        style = NestTheme.typography.heading6,
-                        color = Color.White
-                    )
-                }
+                )
+
                 Spacer(modifier = Modifier.height(16.dp))
                 NestButton(
                     text = "Logout",
