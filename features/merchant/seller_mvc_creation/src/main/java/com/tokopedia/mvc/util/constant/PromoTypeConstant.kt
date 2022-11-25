934cf9949bb58e269980d20e8e405564eb067f70
Yuda <yuda.putra@tokopedia.com>
2022-11-25 16:04:28 +0700

    Fix code feedback from review

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/PromoTypeConstant.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.mvc.util.constant
-
-object PromoTypeConstant {
-    const val SHIPPING = 1
-    const val DISCOUNT = 2
-    const val CASHBACK = 3
-}
--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/VoucherTypeConstant.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.mvc.util.constant
-
-object VoucherTypeConstant {
-    const val VOUCHER_PRODUCT = 1
-    const val VOUCHER_STORE = 2
-}
