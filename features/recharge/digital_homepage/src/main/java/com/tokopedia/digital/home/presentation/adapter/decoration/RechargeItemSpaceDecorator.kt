98bd60823fe6f60e653ceeb964696f11085fc142
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-11-28 15:09:36 +0700

    fix : card layout

--- features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/decoration/RechargeItemSpaceDecorator.kt
+++ features/recharge/digital_homepage/src/main/java/com/tokopedia/digital/home/presentation/adapter/decoration/RechargeItemSpaceDecorator.kt
@@ -11,18 +11,36 @@ import com.tokopedia.kotlin.extensions.view.ZERO
  */
 
 class RechargeItemSpaceDecorator(
-    private val space: Int
-): RecyclerView.ItemDecoration() {
+    space: Int
+) : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+    companion object {
+        const val TWO_MULTIPLIER = 2
+    }
+
+    private val fullPadding = space
+    private val halfPadding = space / TWO_MULTIPLIER
+    private val verticalPadding = space / TWO_MULTIPLIER / TWO_MULTIPLIER
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
         val position = parent.getChildLayoutPosition(view)
         val lastPosition = parent.adapter?.itemCount?.minus(Int.ONE) ?: Int.ZERO
         if (position == Int.ZERO) {
-            outRect.left = space
+            outRect.left = fullPadding * TWO_MULTIPLIER
+            outRect.right = halfPadding
+        } else if (position == lastPosition) {
+            outRect.right = fullPadding * TWO_MULTIPLIER
+            outRect.left = halfPadding
+        } else {
+            outRect.right = halfPadding
+            outRect.left = halfPadding
         }
-        if (position == lastPosition) {
-            outRect.right = space
-        }
-        outRect.bottom = space
+        outRect.top = verticalPadding
+        outRect.bottom = verticalPadding
     }
 }
