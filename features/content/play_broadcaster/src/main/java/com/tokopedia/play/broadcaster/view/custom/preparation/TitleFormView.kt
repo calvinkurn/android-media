2fe7c7559a5f332f4e332604361728fb1dc1db2e
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2022-11-28 14:50:51 +0700

    `feature` add setupInsets in TitleFormView.kt

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/preparation/TitleFormView.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/custom/preparation/TitleFormView.kt
@@ -7,6 +7,7 @@ import android.util.AttributeSet
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchersProvider
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
@@ -49,7 +50,19 @@ class TitleFormView : ConstraintLayout {
     private val scope = CoroutineScope(dispatchers.io)
 
     init {
+        setupInsets()
         setupView()
+    }
+
+    private fun setupInsets() {
+        binding.btnSave.doOnApplyWindowInsets { v, insets, _, margin ->
+            val marginLayoutParams = v.layoutParams as ViewGroup.MarginLayoutParams
+            val newBottomMargin = margin.bottom + insets.systemWindowInsetBottom
+            if (marginLayoutParams.bottomMargin != newBottomMargin) {
+                marginLayoutParams.updateMargins(bottom = newBottomMargin)
+                v.parent.requestLayout()
+            }
+        }
     }
 
     private fun setupView() {
