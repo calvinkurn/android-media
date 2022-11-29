ec67062920bfdbd4eeedb1751c3476fabffabd7c
said.faisal <said.faisal@tokopedia.com>
2022-11-29 22:40:01 +0700

    change wildcart import

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/util/TimerTaskImpl.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/util/TimerTaskImpl.kt
@@ -1,6 +1,6 @@
 package com.tokopedia.tokopedianow.common.util
 
-import java.util.*
+import java.util.TimerTask
 
 class TimerTaskImpl(private val run : () -> Unit): TimerTask() {
     override fun run() {
