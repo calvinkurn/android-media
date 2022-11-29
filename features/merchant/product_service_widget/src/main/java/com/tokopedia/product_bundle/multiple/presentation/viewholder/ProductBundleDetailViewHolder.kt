acc7822ad7cc9e4ebcaa257a0f7250eaf83cde5e
faisalramd <faisal.ramdani@tokopedia.com>
2022-11-29 11:12:56 +0700

    fix currency formatting

--- features/merchant/product_service_widget/src/main/java/com/tokopedia/product_bundle/multiple/presentation/viewholder/ProductBundleDetailViewHolder.kt
+++ features/merchant/product_service_widget/src/main/java/com/tokopedia/product_bundle/multiple/presentation/viewholder/ProductBundleDetailViewHolder.kt
@@ -2,6 +2,7 @@ package com.tokopedia.product_bundle.multiple.presentation.viewholder
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.getCurrencyFormatted
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.kotlin.extensions.view.visible
@@ -13,7 +14,6 @@ import com.tokopedia.product_bundle.multiple.presentation.model.ProductBundleDet
 import com.tokopedia.product_service_widget.R
 import com.tokopedia.unifycomponents.ImageUnify
 import com.tokopedia.unifyprinciples.Typography
-import com.tokopedia.utils.currency.CurrencyFormatUtil
 
 class ProductBundleDetailViewHolder(itemView: View, clickListener: ProductBundleDetailItemClickListener)
     : RecyclerView.ViewHolder(itemView) {
@@ -59,8 +59,8 @@ class ProductBundleDetailViewHolder(itemView: View, clickListener: ProductBundle
         productVariantsView?.text = bundleDetail.selectedVariantText
         // set product price
         productPriceView?.apply {
-            price = CurrencyFormatUtil.convertPriceValueToIdrFormat(bundleDetail.bundlePrice, false)
-            slashPrice = CurrencyFormatUtil.convertPriceValueToIdrFormat(bundleDetail.originalPrice, false)
+            price = bundleDetail.bundlePrice.getCurrencyFormatted()
+            slashPrice = bundleDetail.originalPrice.getCurrencyFormatted()
             context?.run {
                 discountAmount = String.format(this.getString(R.string.text_discount_in_percentage), bundleDetail.discountAmount)
             }
