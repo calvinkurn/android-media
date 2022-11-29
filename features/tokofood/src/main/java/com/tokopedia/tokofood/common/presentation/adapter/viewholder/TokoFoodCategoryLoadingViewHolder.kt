072257277ff0c4f541447a048088994886c95d6c
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2022-11-29 13:08:01 +0700

    fix: revert unused background color

--- features/tokofood/src/main/java/com/tokopedia/tokofood/common/presentation/adapter/viewholder/TokoFoodCategoryLoadingViewHolder.kt
+++ features/tokofood/src/main/java/com/tokopedia/tokofood/common/presentation/adapter/viewholder/TokoFoodCategoryLoadingViewHolder.kt
@@ -3,9 +3,7 @@ package com.tokopedia.tokofood.common.presentation.adapter.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.tokofood.R
-import com.tokopedia.tokofood.databinding.ItemTokofoodCategoryLoadingStateBinding
 import com.tokopedia.tokofood.feature.home.presentation.uimodel.TokoFoodCategoryLoadingStateUiModel
 
 class TokoFoodCategoryLoadingViewHolder(
@@ -15,19 +13,6 @@ class TokoFoodCategoryLoadingViewHolder(
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.item_tokofood_category_loading_state
-    }
-
-    private val binding = ItemTokofoodCategoryLoadingStateBinding.bind(itemView)
-
-    init {
-        with(binding.root) {
-            setBackgroundColor(
-                MethodChecker.getColor(
-                    context,
-                    com.tokopedia.unifyprinciples.R.color.Unify_Background
-                )
-            )
-        }
     }
 
     override fun bind(element: TokoFoodCategoryLoadingStateUiModel) {}
