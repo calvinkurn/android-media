a6429ddf12f468008931cceb0a5c771f1367794b
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2022-11-30 11:08:26 +0700

    Update features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/tracking/GeneralInfoTracker.kt
    Co-authored-by: sonoda-bots[bot] <113779905+sonoda-bots[bot]@users.noreply.github.com>

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/tracking/GeneralInfoTracker.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/tracking/GeneralInfoTracker.kt
@@ -5,7 +5,7 @@ import com.tokopedia.product.detail.data.model.datamodel.ComponentTrackDataModel
 data class GeneralInfoTracker(
     val isTokoNow: Boolean,
     private val componentData: ComponentTrackDataModel
-){
+) {
     val componentName = componentData.componentName
     val componentType = componentData.componentType
     val componentPosition = componentData.adapterPosition
