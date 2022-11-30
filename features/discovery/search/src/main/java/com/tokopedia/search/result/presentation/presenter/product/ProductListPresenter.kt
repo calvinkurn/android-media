f9a3328015e96172d7bf84cff03f36277698e86b
stevenwijaya <steven.wijaya@tokopedia.com>
2022-11-30 15:07:39 +0700

    add ut

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.kt
@@ -890,7 +890,6 @@ class ProductListPresenter @Inject constructor(
         )
     }
 
-
     private fun getViewToShowProductList(
             searchParameter: Map<String, Any>,
             searchProductModel: SearchProductModel,
