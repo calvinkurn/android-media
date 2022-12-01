312ff04536f93b2d4a350f855b53d803a4d29a42
jayantkapila <JayKay@11798>
2022-12-01 17:37:05 +0530

    condition revert

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/view/productcard/TokoNowProductCardView.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/common/view/productcard/TokoNowProductCardView.kt
@@ -226,7 +226,7 @@ class TokoNowProductCardView @JvmOverloads constructor(
         isShown: Boolean,
         productId: String
     ) {
-        similarProductTypography.showIfWithBlock(true) {
+        similarProductTypography.showIfWithBlock(isOos && isShown) {
             adjustChevronIcon(
                 drawable = getIconUnifyDrawable(
                     context = context,
