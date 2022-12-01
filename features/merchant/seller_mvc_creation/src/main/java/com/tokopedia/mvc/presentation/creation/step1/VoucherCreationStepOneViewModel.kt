a6528a8ed57bec03de46455c7355a5e20f05445c
Yuda <yuda.putra@tokopedia.com>
2022-12-01 10:53:21 +0700

    add use case dependency

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/creation/step1/VoucherCreationStepOneViewModel.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/creation/step1/VoucherCreationStepOneViewModel.kt
@@ -2,10 +2,12 @@ package com.tokopedia.mvc.presentation.creation.step1
 
 import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.mvc.domain.usecase.GetInitiateVoucherPageUseCase
+import com.tokopedia.mvc.domain.usecase.VoucherValidationPartialUseCase
 import javax.inject.Inject
 
 class VoucherCreationStepOneViewModel @Inject constructor(
-    private val dispatchers: CoroutineDispatchers
-) : BaseViewModel(dispatchers.main) {
-
-}
+    private val dispatchers: CoroutineDispatchers,
+    private val getInitiateVoucherPageUseCase: GetInitiateVoucherPageUseCase,
+    private val voucherValidationPartialUseCase: VoucherValidationPartialUseCase
+) : BaseViewModel(dispatchers.main)
