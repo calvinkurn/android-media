9e9c1292df8bb9e91c132723b07b9e2c0c077964
Yuda <yuda.putra@tokopedia.com>
2022-12-01 20:03:24 +0700

    Implement layout for voucher creation step 2

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/common/customview/VoucherTargetSelectionView.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/common/customview/VoucherTargetSelectionView.kt
@@ -7,10 +7,9 @@ import com.tokopedia.mvc.R
 import com.tokopedia.unifycomponents.BaseCustomView
 import com.tokopedia.unifycomponents.CardUnify2
 import com.tokopedia.unifycomponents.ImageUnify
-import com.tokopedia.unifycomponents.selectioncontrol.RadioButtonUnify
 import com.tokopedia.unifyprinciples.Typography
 
-class VoucherTypeSelectionView: BaseCustomView {
+class VoucherTargetSelectionView: BaseCustomView {
     var isActive: Boolean = false
         set(value) {
             field = value
@@ -24,15 +23,14 @@ class VoucherTypeSelectionView: BaseCustomView {
         }
 
     var descriptionText: String = ""
-    set(value) {
-        field = value
-        refreshViews()
-    }
+        set(value) {
+            field = value
+            refreshViews()
+        }
 
     var tpgTitle: Typography? = null
     var tpgDescription: Typography? = null
-    var imgVoucherType: ImageUnify? = null
-    var radioButton: RadioButtonUnify? = null
+    var imgVoucherTarget: ImageUnify? = null
     var cardParent: CardUnify2? = null
 
     constructor(context: Context) : super(context) {
@@ -48,27 +46,26 @@ class VoucherTypeSelectionView: BaseCustomView {
     }
 
     private fun init(attrs: AttributeSet?) {
-        val view = View.inflate(context, R.layout.smvc_custom_view_voucher_type_selection, this)
+        val view = View.inflate(context, R.layout.smvc_custom_view_voucher_creation_voucher_target, this)
         setupViews(view)
         defineCustomAttributes(attrs)
         refreshViews()
     }
 
     private fun setupViews(view: View) {
-        tpgTitle = view.findViewById(R.id.tpg_title)
-        tpgDescription = view.findViewById(R.id.tpg_description)
-        imgVoucherType = view.findViewById(R.id.image_voucher)
-        radioButton = view.findViewById(R.id.radio_btn_voucher)
-        cardParent = view.findViewById(R.id.card_parent)
+        tpgTitle = view.findViewById(R.id.tpg_title_voucher_target)
+        tpgDescription = view.findViewById(R.id.tpg_description_voucher_target)
+        imgVoucherTarget = view.findViewById(R.id.icon_voucher_target)
+        cardParent = view.findViewById(R.id.card_parent_voucher_target)
     }
 
     private fun defineCustomAttributes(attrs: AttributeSet?) {
         if (attrs != null) {
-            val styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.VoucherTypeSelectionView, 0, 0)
+            val styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.VoucherTargetSelectionView, 0, 0)
             try {
-                titleText = styledAttributes.getString(R.styleable.VoucherTypeSelectionView_voucher_type_title_text).orEmpty()
-                descriptionText = styledAttributes.getString(R.styleable.VoucherTypeSelectionView_voucher_type_description_text).orEmpty()
-                isActive = styledAttributes.getBoolean(R.styleable.VoucherTypeSelectionView_is_voucher_type_active, false)
+                titleText = styledAttributes.getString(R.styleable.VoucherTargetSelectionView_voucher_target_title_text).orEmpty()
+                descriptionText = styledAttributes.getString(R.styleable.VoucherTargetSelectionView_voucher_target_description_text).orEmpty()
+                isActive = styledAttributes.getBoolean(R.styleable.VoucherTargetSelectionView_voucher_target_description_text, false)
             } finally {
                 styledAttributes.recycle()
             }
@@ -79,7 +76,6 @@ class VoucherTypeSelectionView: BaseCustomView {
         try {
             tpgTitle?.text = titleText
             tpgDescription?.text = descriptionText
-            radioButton?.isChecked = isActive
             if (isActive) {
                 cardParent?.cardType = CardUnify2.TYPE_BORDER_ACTIVE
             } else {
--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/creation/step2/VoucherCreationStepTwoFragment.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/creation/step2/VoucherCreationStepTwoFragment.kt
@@ -10,6 +10,10 @@ import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
 import com.tokopedia.mvc.R
 import com.tokopedia.mvc.databinding.SmvcFragmentVoucherCreationStepTwoBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherCreationStepTwoVoucherCodeSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherCreationStepTwoVoucherNameSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherCreationStepTwoVoucherPeriodSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherCreationStepTwoVoucherTargetSectionBinding
 import com.tokopedia.mvc.di.component.DaggerMerchantVoucherCreationComponent
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 import javax.inject.Inject
@@ -22,10 +26,14 @@ class VoucherCreationStepTwoFragment : BaseDaggerFragment() {
 
     // binding
     private var binding by autoClearedNullable<SmvcFragmentVoucherCreationStepTwoBinding>()
+    private var voucherTargetSectionBinding by autoClearedNullable<SmvcVoucherCreationStepTwoVoucherTargetSectionBinding>()
+    private var voucherNameSectionBinding by autoClearedNullable<SmvcVoucherCreationStepTwoVoucherNameSectionBinding>()
+    private var voucherCodeSectionBinding by autoClearedNullable<SmvcVoucherCreationStepTwoVoucherCodeSectionBinding>()
+    private var voucherPeriodSectionBinding by autoClearedNullable<SmvcVoucherCreationStepTwoVoucherPeriodSectionBinding>()
 
+    //viewModel
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
-
     private val viewModelProvider by lazy { ViewModelProvider(this, viewModelFactory) }
     private val viewModel by lazy { viewModelProvider.get(VoucherCreationStepTwoViewModel::class.java) }
 
