ee228571c06e71b21f1dc59d54258b6e53e6781d
hansenpw <hansen.wijaya@tokopedia.com>
2022-12-02 15:15:25 +0700

    refactor method

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentFragment.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/ShipmentFragment.java
@@ -711,25 +711,34 @@ public class ShipmentFragment extends BaseCheckoutFragment implements ShipmentCo
                         if (!isUnsubscribed() && ShipmentFragment.this.getActivityContext() != null) {
                             if (UploadPrescriptionUiModel1.getConsultationFlow()) {
                                 shipmentPresenter.fetchEpharmacyData();
-                                if (!CoachMarkPreference.INSTANCE.hasShown(ShipmentFragment.this.getActivityContext(), KEY_PREFERENCE_COACHMARK_EPHARMACY)) {
-                                    int uploadPrescriptionPosition = shipmentAdapter.getUploadPrescriptionPosition();
-                                    rvShipment.scrollToPosition(uploadPrescriptionPosition);
-                                    rvShipment.post(() -> {
-                                        RecyclerView.ViewHolder viewHolder = rvShipment.findViewHolderForAdapterPosition(uploadPrescriptionPosition);
-                                        if (viewHolder instanceof UploadPrescriptionViewHolder) {
-                                            CoachMark2Item item = new CoachMark2Item(viewHolder.itemView, ShipmentFragment.this.getActivityContext().getString(R.string.checkout_epharmacy_coachmark_title), ShipmentFragment.this.getActivityContext().getString(R.string.checkout_epharmacy_coachmark_description), CoachMark2.POSITION_TOP);
-                                            ArrayList<CoachMark2Item> list = new ArrayList<>();
-                                            list.add(item);
-                                            CoachMark2 coachMark = new CoachMark2(requireContext());
-                                            coachMark.showCoachMark(list, null, 0);
-                                            CoachMarkPreference.INSTANCE.setShown(ShipmentFragment.this.getActivityContext(), KEY_PREFERENCE_COACHMARK_EPHARMACY, true);
-                                        }
-                                    });
-                                }
+                                showCoachMarkEpharmacy();
                             }
                         }
                     }
                 });
+    }
+
+    private void showCoachMarkEpharmacy() {
+        if (getActivityContext() != null && !CoachMarkPreference.INSTANCE.hasShown(getActivityContext(), KEY_PREFERENCE_COACHMARK_EPHARMACY)) {
+            int uploadPrescriptionPosition = shipmentAdapter.getUploadPrescriptionPosition();
+            rvShipment.scrollToPosition(uploadPrescriptionPosition);
+            rvShipment.post(() -> {
+                RecyclerView.ViewHolder viewHolder = rvShipment.findViewHolderForAdapterPosition(uploadPrescriptionPosition);
+                if (viewHolder instanceof UploadPrescriptionViewHolder) {
+                    CoachMark2Item item = new CoachMark2Item(
+                            viewHolder.itemView,
+                            getActivityContext().getString(R.string.checkout_epharmacy_coachmark_title),
+                            getActivityContext().getString(R.string.checkout_epharmacy_coachmark_description),
+                            CoachMark2.POSITION_TOP
+                    );
+                    ArrayList<CoachMark2Item> list = new ArrayList<>();
+                    list.add(item);
+                    CoachMark2 coachMark = new CoachMark2(requireContext());
+                    coachMark.showCoachMark(list, null, 0);
+                    CoachMarkPreference.INSTANCE.setShown(getActivityContext(), KEY_PREFERENCE_COACHMARK_EPHARMACY, true);
+                }
+            });
+        }
     }
 
     private ArrayList<Long> getCrossSellChildCategoryId(List<ShipmentCartItemModel> shipmentCartItemModelList) {
