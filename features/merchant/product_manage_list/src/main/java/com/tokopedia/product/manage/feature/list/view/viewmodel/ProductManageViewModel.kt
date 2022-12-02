ebacf72fab1a662e86baa1a81dd902fa34095d60
edwin91 <edwin.nurwansyah@tokopedia.com>
2022-12-02 15:01:54 +0700

    fixing unit test

--- features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/viewmodel/ProductManageViewModel.kt
+++ features/merchant/product_manage_list/src/main/java/com/tokopedia/product/manage/feature/list/view/viewmodel/ProductManageViewModel.kt
@@ -49,7 +49,6 @@ import com.tokopedia.product.manage.feature.quickedit.delete.domain.DeleteProduc
 import com.tokopedia.product.manage.feature.quickedit.price.data.model.EditPriceResult
 import com.tokopedia.product.manage.feature.quickedit.price.domain.EditPriceUseCase
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
-import com.tokopedia.remoteconfig.RemoteConfigKey.ENABLE_TICKER_NOTIFY_ME
 import com.tokopedia.shop.common.data.model.ProductStock
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.Product
 import com.tokopedia.shop.common.data.source.cloud.model.productlist.ProductStatus
@@ -398,7 +397,7 @@ class ProductManageViewModel @Inject constructor(
         })
     }
 
-    fun getTickerData(enableStockAvailable: Boolean) {
+    fun getTickerData(enableStockAvailable: Boolean= true) {
         val isMultiLocationShop = userSessionInterface.isMultiLocationShop
         _tickerData.value =
             tickerStaticDataProvider.getTickers(
