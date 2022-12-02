fa8fdf5c5b9a02423087a1ed68e78092a03ea8f5
faisalramd <faisal.ramdani@tokopedia.com>
2022-12-02 18:14:20 +0700

    implement layout setup

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/summary/fragment/SummaryFragment.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/summary/fragment/SummaryFragment.kt
@@ -1,23 +1,29 @@
 package com.tokopedia.mvc.presentation.summary.fragment
 
- import android.graphics.drawable.GradientDrawable
- import android.os.Bundle
- import android.view.LayoutInflater
- import android.view.View
- import android.view.ViewGroup
- import com.tokopedia.abstraction.base.app.BaseMainApplication
- import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
- import com.tokopedia.abstraction.common.utils.view.MethodChecker
- import com.tokopedia.header.HeaderUnify
- import com.tokopedia.kotlin.extensions.view.applyUnifyBackgroundColor
- import com.tokopedia.mvc.R
- import com.tokopedia.mvc.databinding.SmvcFragmentSummaryBinding
- import com.tokopedia.mvc.di.component.DaggerMerchantVoucherCreationComponent
- import com.tokopedia.mvc.presentation.summary.viewmodel.SummaryViewModel
- import com.tokopedia.unifycomponents.toPx
- import com.tokopedia.utils.lifecycle.autoClearedNullable
- import javax.inject.Inject
-
+import android.graphics.drawable.GradientDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.header.HeaderUnify
+import com.tokopedia.kotlin.extensions.view.applyUnifyBackgroundColor
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.mvc.R
+import com.tokopedia.mvc.databinding.SmvcFragmentSummaryBinding
+import com.tokopedia.mvc.databinding.SmvcFragmentSummaryPreviewBinding
+import com.tokopedia.mvc.databinding.SmvcFragmentSummarySubmissionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherDetailProductSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherDetailVoucherInfoSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherDetailVoucherSettingSectionBinding
+import com.tokopedia.mvc.databinding.SmvcVoucherDetailVoucherTypeSectionBinding
+import com.tokopedia.mvc.di.component.DaggerMerchantVoucherCreationComponent
+import com.tokopedia.mvc.presentation.summary.viewmodel.SummaryViewModel
+import com.tokopedia.unifycomponents.toPx
+import com.tokopedia.utils.lifecycle.autoClearedNullable
+import javax.inject.Inject
 
 class SummaryFragment: BaseDaggerFragment() {
 
@@ -50,19 +56,6 @@ class SummaryFragment: BaseDaggerFragment() {
         binding?.setupView()
         setupObservables()
         viewModel.aaa()
-
-        val green2 = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_GN500)
-        val green = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_GN200)
-        val gd = GradientDrawable(
-            GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(green2, green)
-        )
-        val corner = 16.toPx().toFloat()
-        gd.cornerRadii = floatArrayOf(0f, 0f, 0f, 0f, corner, corner, corner, corner)
-
-        binding?.layoutPreview?.viewBg?.background = gd
-        binding?.layoutSubmission?.cbTnc?.text = MethodChecker.fromHtml(
-            getString(R.string.smvc_summary_page_tnc_text)
-        )
     }
 
     private fun setupObservables() {
@@ -71,6 +64,12 @@ class SummaryFragment: BaseDaggerFragment() {
 
     private fun SmvcFragmentSummaryBinding.setupView() {
         header.setupHeader()
+        layoutPreview.setupLayoutPreview()
+        layoutType.setupLayoutType()
+        layoutInfo.setupLayoutInfo()
+        layoutSetting.setupLayoutSetting()
+        layoutProducts.setupLayoutProducts()
+        layoutSubmission.setupLayoutSubmission()
     }
 
     private fun HeaderUnify.setupHeader() {
@@ -78,5 +77,36 @@ class SummaryFragment: BaseDaggerFragment() {
         setNavigationOnClickListener {
             activity?.finish()
         }
+    }
+
+    private fun SmvcFragmentSummaryPreviewBinding.setupLayoutPreview() {
+        val green2 = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_GN500)
+        val green = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_GN200)
+        val gd = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(green2, green)
+        )
+        val corner = 16.toPx().toFloat()
+        gd.cornerRadii = floatArrayOf(0f, 0f, 0f, 0f, corner, corner, corner, corner)
+        viewBg.background = gd
+    }
+
+    private fun SmvcVoucherDetailVoucherTypeSectionBinding.setupLayoutType() {
+        tpgEditAction.visible()
+    }
+
+    private fun SmvcVoucherDetailVoucherInfoSectionBinding.setupLayoutInfo() {
+        tpgEditAction.visible()
+    }
+
+    private fun SmvcVoucherDetailVoucherSettingSectionBinding.setupLayoutSetting() {
+        tpgEditAction.visible()
+    }
+
+    private fun SmvcVoucherDetailProductSectionBinding.setupLayoutProducts() {
+        tpgEditAction.visible()
+    }
+
+    private fun SmvcFragmentSummarySubmissionBinding.setupLayoutSubmission() {
+        cbTnc.text = MethodChecker.fromHtml(getString(R.string.smvc_summary_page_tnc_text))
     }
 }
