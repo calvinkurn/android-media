8faf3e891f4a7f0788d1c735645a70b61aba73f2
rama.putra <rama.putra@tokopedia.com>
2022-12-05 09:25:07 +0700

    remove unused changes

--- features/category/basemvvm/src/main/java/com/tokopedia/basemvvm/viewcontrollers/BaseViewModelActivity.kt
+++ features/category/basemvvm/src/main/java/com/tokopedia/basemvvm/viewcontrollers/BaseViewModelActivity.kt
@@ -20,7 +20,7 @@ abstract class BaseViewModelActivity<T : BaseViewModel> : BaseSimpleActivity(), 
     }
 
     private fun setViewModel() {
-        bVM = ViewModelProvider(this, getVMFactory()).get(getViewModelType())
+        bVM = ViewModelProviders.of(this, getVMFactory()).get(getViewModelType())
         setViewModel(bVM)
         lifecycle.addObserver(getLifeCycleObserver(bVM))
     }
