ab09accde82115b3bc958fb9079783dc566f5680
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2022-12-08 20:15:36 +0700

    Add url article to edu center menu adapter

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/bottomsheet/educenterbottomsheet/EduCenterMenuAdapter.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/presentation/bottomsheet/educenterbottomsheet/EduCenterMenuAdapter.kt
@@ -10,9 +10,12 @@ import com.tokopedia.mvc.R
 class EduCenterMenuAdapter(val context: Context,
                            val listener: EduCenterClickListener) : RecyclerView.Adapter<EduCenterMenuViewHolder>()  {
 
+    companion object{
+        private const val url_main_article_about_coupon = "https://seller.tokopedia.com/edu/kupon-toko-saya/"
+    }
     private var educationCenterMenuList = arrayListOf(
         EduCenterMenuModel(context.resources?.getString(R.string.smvc_edu_center_about_my_coupon)),
-        EduCenterMenuModel(context.resources?.getString(R.string.smvc_edu_center_read_article))
+        EduCenterMenuModel(context.resources?.getString(R.string.smvc_edu_center_read_article), url_main_article_about_coupon)
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EduCenterMenuViewHolder {
