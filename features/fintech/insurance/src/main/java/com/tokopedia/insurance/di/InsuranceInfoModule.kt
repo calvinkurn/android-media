818ebf6077c828e05699b232d5857bb984224976
irfankhoirul <irfan.muhlishin@tokopedia.com>
2022-12-15 11:07:52 +0700

    adjust DI

--- features/fintech/insurance/src/main/java/com/tokopedia/insurance/di/InsuranceInfoModule.kt
+++ features/fintech/insurance/src/main/java/com/tokopedia/insurance/di/InsuranceInfoModule.kt
@@ -2,6 +2,7 @@ package com.tokopedia.insurance.di
 
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.di.scope.ActivityScope
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
@@ -10,8 +11,7 @@ import dagger.Provides
 @Module
 class InsuranceInfoModule {
 
-    @InsuranceInfoScope
+    @ActivityScope
     @Provides
     fun provideUserSession(@ApplicationContext context: Context): UserSessionInterface = UserSession(context)
-
 }
