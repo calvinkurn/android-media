88c6cc5efa32b44ef969293b1fa0f13e60736a4f
yehezkiel <yehezkiel@tokopedia.com>
2022-12-16 15:08:35 +0700

    move hardcoded to constant

--- features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/EpharmacyInfoViewHolder.kt
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/EpharmacyInfoViewHolder.kt
@@ -14,6 +14,12 @@ class EpharmacyInfoViewHolder(
 
     companion object {
         val LAYOUT = R.layout.item_buyer_order_detail_epharmacy_info
+        private const val CONSULTATION_NAME_KEY = "Konsultasi"
+        private const val DOCTOR_KEY = "Dokter"
+        private const val PRESCRIPTION_NUMBER_KEY = "No. Resep"
+        private const val CONSULTATION_DATE_KEY = "Tanggal Dikeluarkan"
+        private const val CONSULTATION_EXPIRY_DATE_KEY = "Masa Berlaku"
+        private const val CONSULTATION_PATIENT_NAME_KEY = "Nama Pasien"
     }
 
     private val binding = ItemBuyerOrderDetailEpharmacyInfoBinding.bind(itemView)
@@ -23,12 +29,12 @@ class EpharmacyInfoViewHolder(
         binding.run {
             containerBuyerOrderDetailEphar.removeAllViews()
 
-            element.consultationName.addViewIfNotBlank("Konsultasi")
-            element.consultationDoctorName.addViewIfNotBlank("Dokter")
-            element.consultationPrescriptionNumber.addViewIfNotBlank("No. Resep")
-            element.consultationDate.addViewIfNotBlank("Tanggal Dikeluarkan")
-            element.consultationExpiryDate.addViewIfNotBlank("Masa Berlaku")
-            element.consultationPatientName.addViewIfNotBlank("Nama Pasien")
+            element.consultationName.addViewIfNotBlank(CONSULTATION_NAME_KEY)
+            element.consultationDoctorName.addViewIfNotBlank(DOCTOR_KEY)
+            element.consultationPrescriptionNumber.addViewIfNotBlank(PRESCRIPTION_NUMBER_KEY)
+            element.consultationDate.addViewIfNotBlank(CONSULTATION_DATE_KEY)
+            element.consultationExpiryDate.addViewIfNotBlank(CONSULTATION_EXPIRY_DATE_KEY)
+            element.consultationPatientName.addViewIfNotBlank(CONSULTATION_PATIENT_NAME_KEY)
         }
     }
 
