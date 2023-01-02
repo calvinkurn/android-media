78acca6609eb5f6113cc009de46431f6be138312
yogieputra8 <yogie.putra@tokopedia.com>
2023-01-02 17:58:36 +0700

    Remove unused casava: click back on main page

--- features/merchant/product_add_edit/src/androidTest/java/com/tokopedia/product/addedit/instrumenttest/AddEditProductAddingAnalyticTest.kt
+++ features/merchant/product_add_edit/src/androidTest/java/com/tokopedia/product/addedit/instrumenttest/AddEditProductAddingAnalyticTest.kt
@@ -40,8 +40,6 @@ class AddEditProductAddingAnalyticTest {
         private const val PRODUCT_IMAGE_PICKER_OPEN = "tracker/merchant/product_add_edit/add/product_image_picker_open.json"
 
         private const val PRODUCT_DETAIL_PAGE_OPEN = "tracker/merchant/product_add_edit/add/product_detail_page_open.json"
-        private const val PRODUCT_DETAIL_PAGE_CLICK_BACK = "tracker/merchant/product_add_edit/add/product_detail_page_click_back.json"
-
 
         private const val PRODUCT_DETAIL_PAGE_CLICK_CONTINUE = "tracker/merchant/product_add_edit/add/product_detail_page_click_continue.json"
 
@@ -98,7 +96,6 @@ class AddEditProductAddingAnalyticTest {
         doAnalyticDebuggerTest(PRODUCT_IMAGE_PICKER_OPEN)
 
         doAnalyticDebuggerTest(PRODUCT_DETAIL_PAGE_OPEN)
-        doAnalyticDebuggerTest(PRODUCT_DETAIL_PAGE_CLICK_BACK)
 
         activityRule.activity.finish()
     }
