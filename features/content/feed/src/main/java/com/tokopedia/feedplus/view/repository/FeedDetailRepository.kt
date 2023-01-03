f5e63692d4fab54c55a10f2c427a61641e54493f
meyta.taliti <meyta.taliti@tokopedia.com>
2023-01-03 14:58:52 +0700

    fix: conflicted when merging with release branch

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/repository/FeedDetailRepository.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/repository/FeedDetailRepository.kt
@@ -35,7 +35,8 @@ class FeedDetailRepository @Inject constructor(
         const val FEED_GET_ACTIVITY_PRODUCT_QUERY = """
             query FeedXGetActivityProducts(${'$'}req: FeedXGetActivityProductsRequest!){
               feedXGetActivityProducts(req:${'$'}req){
-                hasVoucherproducts{
+                hasVoucher
+                products{
                       id
                       shopID
                       name
