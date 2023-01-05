72a6780620aeadfa848151926589461caea79e99
Nabilla <nabilla.sabbaha@tokopedia.com>
2023-01-06 00:10:39 +0700

    create util for user consent temporary since create shop hasn't integrated with user consent platform

---
+++ libraries/utils/src/main/java/com/tokopedia/utils/text/style/UserConsentTncPolicyUtil.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.utils.text.style
+
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.view.View
+
+object UserConsentTncPolicyUtil {
+
+    fun generateText(
+        message: String,
+        tncTitle: String,
+        policyTitle: String,
+        actionTnc: () -> Unit,
+        actionPolicy: () -> Unit,
+        colorSpan: Int
+    ): SpannableString {
+        return SpannableString(String.format(message, tncTitle, policyTitle)).apply {
+            setSpan(
+                createClickableSpannable(actionTnc, colorSpan),
+                this.indexOf(tncTitle),
+                this.indexOf(tncTitle) + tncTitle.length,
+                0
+            )
+
+            setSpan(
+                createClickableSpannable(actionPolicy, colorSpan),
+                this.indexOf(policyTitle),
+                this.indexOf(policyTitle) + policyTitle.length,
+                0
+            )
+        }
+    }
+
+    private fun createClickableSpannable(action: () -> Unit, colorSpan: Int): ClickableSpan {
+        return object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                action.invoke()
+            }
+            override fun updateDrawState(ds: TextPaint) {
+                ds.typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+                ds.color =  colorSpan
+            }
+        }
+    }
+}
