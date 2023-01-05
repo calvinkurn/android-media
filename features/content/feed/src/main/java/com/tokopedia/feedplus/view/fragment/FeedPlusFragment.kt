ad0f20f115a1ffb1f040718e38fdea2d8cc7ae84
shrutiagarwal <shruti.agarwal@tokopedia.com>
2023-01-05 12:07:55 +0530

    fix string when unfollow

--- features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/view/fragment/FeedPlusFragment.kt
@@ -477,7 +477,7 @@ class FeedPlusFragment :
                                     }
                                 } else {
                                     val messageRes =
-                                        if (data.isFollow) com.tokopedia.feedcomponent.R.string.feed_component_follow_success_toast else com.tokopedia.feedcomponent.R.string.feed_component_follow_success_toast
+                                        if (data.isFollow) com.tokopedia.feedcomponent.R.string.feed_component_follow_success_toast else com.tokopedia.feedcomponent.R.string.feed_component_unfollow_success_toast
                                     showToast(
                                         getString(messageRes),
                                         Toaster.TYPE_NORMAL
