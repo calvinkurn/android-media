97e2a3753771931439bdb6cc63828b283536460d
Eka D <106505791+ekadesyan@users.noreply.github.com>
2023-01-09 18:01:17 +0700

    [Logistic - Schelly] Fix linter error (#31109)

--- features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/scheduledelivery/view/adapter/ScheduleSlotAdapter.kt
+++ features/logistic/logisticcart/src/main/java/com/tokopedia/logisticcart/scheduledelivery/view/adapter/ScheduleSlotAdapter.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.logisticcart.scheduledelivery.view.adapter
 
+import android.annotation.SuppressLint
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.adapter.adapter.BaseListAdapter
 import com.tokopedia.logisticcart.scheduledelivery.view.uimodel.BaseScheduleSlotUiModel
@@ -9,12 +10,14 @@ import com.tokopedia.logisticcart.scheduledelivery.utils.DividerType
 class ScheduleSlotAdapter(private val factory: ScheduleSlotTypeFactory)
     : BaseListAdapter<Visitable<*>, ScheduleSlotTypeFactory>(factory) {
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setData(data: List<BaseScheduleSlotUiModel<out Any>>) {
         visitables?.clear()
         visitables?.addAll(data)
         notifyDataSetChanged()
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setData(data: BottomSheetUiModel) {
         visitables?.clear()
         visitables.add(data.date)
