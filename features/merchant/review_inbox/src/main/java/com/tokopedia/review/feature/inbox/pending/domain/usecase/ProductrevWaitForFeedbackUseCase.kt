6e412c159bcdb93fe9a1bdf0e8f5db8842a4c968
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-01-09 13:05:13 +0700

    Revert "[Review][BulkReview] For testing bulk review form"
    This reverts commit e82badf4a71bc215b51336ccdecdfc8fda53271e.

--- features/merchant/review_inbox/src/main/java/com/tokopedia/review/feature/inbox/pending/domain/usecase/ProductrevWaitForFeedbackUseCase.kt
+++ features/merchant/review_inbox/src/main/java/com/tokopedia/review/feature/inbox/pending/domain/usecase/ProductrevWaitForFeedbackUseCase.kt
@@ -4,7 +4,6 @@ import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.review.common.ReviewInboxConstants
-import com.tokopedia.review.feature.inbox.pending.data.ProductrevWaitForFeedbackLabelAndImage
 import com.tokopedia.review.feature.inbox.pending.data.ProductrevWaitForFeedbackResponseWrapper
 import com.tokopedia.usecase.RequestParams
 import javax.inject.Inject
@@ -56,23 +55,6 @@ class ProductrevWaitForFeedbackUseCase @Inject constructor(graphqlRepository: Gr
                     }
                 }
             """
-    }
-
-    override suspend fun executeOnBackground(): ProductrevWaitForFeedbackResponseWrapper {
-        return super.executeOnBackground().run {
-            copy(
-                productrevWaitForFeedbackWaitForFeedback = productrevWaitForFeedbackWaitForFeedback.copy(
-                    banners = productrevWaitForFeedbackWaitForFeedback.banners.plus(
-                        ProductrevWaitForFeedbackLabelAndImage(
-                            labelTitle = "Lebih cepat ulas sekaligus ✨",
-                            labelSubtitle = "Yuk, lihat 10 barang ini & mulai ulas!",
-                            imageURL = "https://thumb.viva.co.id/media/frontend/thumbs3/2022/07/06/62c503d49d9ef-thor-love-and-thunder_1265_711.jpg",
-                            appLink = "tokopedia://product-review/bulk-create/"
-                        )
-                    )
-                )
-            )
-        }
     }
 
     @GqlQuery(WAIT_FOR_FEEDBACK_QUERY_CLASS_NAME, WAIT_FOR_FEEDBACK_QUERY)
