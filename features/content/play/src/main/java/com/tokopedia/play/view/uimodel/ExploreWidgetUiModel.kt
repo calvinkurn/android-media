10781759d5042beb270af00a01e34859d2c5732d
astidhiyaa <asti.anzaria@tokopedia.com>
2023-01-11 15:55:19 +0700

    fix: unmerge list

--- features/content/play/src/main/java/com/tokopedia/play/view/uimodel/ExploreWidgetUiModel.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/uimodel/ExploreWidgetUiModel.kt
@@ -98,7 +98,13 @@ val WidgetParamUiModel.hasNextPage: Boolean
 
 val List<WidgetUiModel>.getChannelBlock: WidgetItemUiModel
     get() {
-        return this.filterIsInstance<WidgetItemUiModel>().firstOrNull() ?: WidgetItemUiModel.Empty
+        val newList = mutableListOf<List<PlayWidgetItemUiModel>>()
+        val filteredList =  this.filterIsInstance<WidgetItemUiModel>()
+        filteredList.forEach {
+            newList.add(it.item.items)
+        }
+        val config = filteredList.firstOrNull() ?: WidgetItemUiModel.Empty
+        return WidgetItemUiModel(item = PlayWidgetUiModel(items = newList.flatten(), title = config.item.title, config = config.item.config, actionTitle = config.item.actionTitle, actionAppLink = config.item.actionAppLink, isActionVisible = config.item.isActionVisible, background = config.item.background))
     }
 
 val List<WidgetUiModel>.getChannelBlocks: List<WidgetItemUiModel>
