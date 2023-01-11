f9406386dce940c0aba13af43d78b512ead9ea7b
CJ <calvyn.julian@tokopedia.com>
2023-01-11 11:52:28 +0700

    fix comment

--- libraries/graphql/src/main/java/com/tokopedia/graphql/util/RemoteConfigHelper.kt
+++ libraries/graphql/src/main/java/com/tokopedia/graphql/util/RemoteConfigHelper.kt
@@ -3,7 +3,7 @@ package com.tokopedia.graphql.util
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
 object RemoteConfigHelper {
-    //need to define remote config key here, because we can't include remote_config module inside graphql module, because of circular dependency
+    //need to define remote config key here, since we can't include remote_config module inside graphql module, because of circular dependency
     private const val KEY_ENABLE_GQL_PARSE_ERROR_LOGGING_IMPROVEMENT = "android_enable_gql_parse_logging_improvement"
     fun isEnableGqlParseErrorLoggingImprovement(): Boolean {
         return try {
