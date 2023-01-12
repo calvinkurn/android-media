af63c4ff39a113cf3602e30551fe56737439555b
hiradevina <fakhira.devina@tokopedia.com>
2023-01-12 14:58:24 +0700

    feat: create reschedule pickup screen

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/uimodel/ReschedulePickupScreen.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/uimodel/ReschedulePickupScreen.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.logisticseller.ui.reschedulepickup.uimodel
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,13 +8,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tokopedia.common_compose.principles.NestButton
+import com.tokopedia.common_compose.principles.NestTicker
 import com.tokopedia.common_compose.principles.NestTypography
 import com.tokopedia.common_compose.ui.NestTheme
 import com.tokopedia.logisticseller.R
@@ -34,7 +42,7 @@ fun ReschedulePickupScreen() {
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
-            Column {
+            Column(modifier = Modifier.padding(16.dp)) {
                 Row {
                     Column {
                         NestTypography(
@@ -72,10 +80,63 @@ fun ReschedulePickupScreen() {
             textStyle = NestTheme.typography.body3,
             modifier = Modifier.padding(horizontal = 16.dp)
         )
+        TipsUnifyCompose(
+            title = "title",
+            description = "description",
+            modifier = Modifier.padding(16.dp)
+        )
         TextFieldUnifyCompose(
-            value = "test",
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-            label = stringResource(id = R.string.label_day_reschedule_pick_up)
+            value = "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = {
+                NestTypography(
+                    text =
+                    stringResource(id = R.string.label_day_reschedule_pick_up)
+                )
+            },
+            placeholder = {
+                NestTypography(text = stringResource(id = R.string.placeholder_day_reschedule_pick_up))
+            },
+            trailingIcon = { TrailingIconTextField() }
+        )
+        TextFieldUnifyCompose(
+            value = "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = {
+                NestTypography(text = stringResource(id = R.string.label_time_reschedule_pick_up))
+            },
+            placeholder = {
+                NestTypography(text = stringResource(id = R.string.placeholder_time_reschedule_pick_up))
+            },
+            trailingIcon = { TrailingIconTextField() }
+        )
+        TextFieldUnifyCompose(
+            value = "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = {
+                NestTypography(text = stringResource(id = R.string.label_reason_reschedule_pickup))
+            },
+            trailingIcon = { TrailingIconTextField() }
+        )
+        NestTicker(
+            text = "ticker",
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+        )
+        TextFieldUnifyCompose(
+            value = "",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = {
+                NestTypography(text = stringResource(id = R.string.label_detail_reason_reschedule_pickup))
+            }
         )
         NestButton(
             modifier = Modifier
@@ -89,21 +150,64 @@ fun ReschedulePickupScreen() {
 }
 
 @Composable
+fun TrailingIconTextField() {
+    IconButton(
+        onClick = {}
+    ) {
+        Icon(
+            imageVector = Icons.Filled.ArrowDropDown,
+            contentDescription = "drop down"
+        )
+    }
+}
+
+@Composable
 fun TextFieldUnifyCompose(
     value: String,
     onValueChanged: (TextFieldValue) -> Unit = {},
-    label: String,
-    modifier: Modifier = Modifier
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier,
+    trailingIcon: @Composable (() -> Unit)? = null
 ) {
     OutlinedTextField(
         value = TextFieldValue(text = value),
         onValueChange = onValueChanged,
         modifier = modifier,
-        label = {
-            NestTypography(
-                text = label
-            )
-        }
+        placeholder = placeholder,
+        trailingIcon = trailingIcon,
+        label = label
 
     )
+}
+
+@Composable
+fun TipsUnifyCompose(
+    title: String? = null,
+    description: String? = null,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        border = BorderStroke(width = 1.dp, color = NestTheme.colors.NN._200),
+        backgroundColor = NestTheme.colors.NN._50
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            title?.run {
+                NestTypography(
+                    text = this,
+                    textStyle = NestTheme.typography.paragraph3.copy(fontWeight = FontWeight.Bold)
+                )
+            }
+            description?.run {
+                NestTypography(text = this, textStyle = NestTheme.typography.small)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ReschedulePickupScreenPreview() {
+    ReschedulePickupScreen()
 }
