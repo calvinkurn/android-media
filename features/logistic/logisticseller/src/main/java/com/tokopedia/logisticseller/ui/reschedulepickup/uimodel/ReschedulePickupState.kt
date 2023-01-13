de6823471c759466fbd4a46ff112157ad67e9ed7
hiradevina <fakhira.devina@tokopedia.com>
2023-01-13 15:58:01 +0700

    feat: create state

---
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/uimodel/ReschedulePickupState.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.logisticseller.ui.reschedulepickup.uimodel
+
+import com.tokopedia.logisticseller.data.model.RescheduleDayOptionModel
+import com.tokopedia.logisticseller.data.model.RescheduleReasonOptionModel
+import com.tokopedia.logisticseller.data.model.RescheduleTimeOptionModel
+
+data class ReschedulePickupState(
+    val info: ReschedulePickupInfo = ReschedulePickupInfo(),
+    val options: ReschedulePickupOptions = ReschedulePickupOptions(),
+    val error: String = ""
+)
+
+data class ReschedulePickupInput(
+    val day: String = "",
+    val time: String = "",
+    val reason: String = ""
+)
+
+data class ReschedulePickupOptions(
+    val dayOptions: List<RescheduleDayOptionModel> = listOf(),
+    val timeOptions: List<RescheduleTimeOptionModel> = listOf(),
+    val reasonOptions: List<RescheduleReasonOptionModel> = listOf()
+)
+
+data class ReschedulePickupInfo(
+    val invoice: String = "",
+    val courier: String = "",
+    val guide: String = "",
+    val applink: String = "",
+    val summary: String = ""
+)
