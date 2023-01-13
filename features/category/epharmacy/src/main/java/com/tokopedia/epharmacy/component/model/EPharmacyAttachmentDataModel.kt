c295ac9abc66f7e8483f142f09b2c392ee93f11d
mohit-toko <mohit.singh@tokopedia.com>
2023-01-13 12:37:02 +0530

    Import Alias

--- features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/component/model/EPharmacyAttachmentDataModel.kt
+++ features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/component/model/EPharmacyAttachmentDataModel.kt
@@ -1,9 +1,9 @@
 package com.tokopedia.epharmacy.component.model
 
 import android.os.Bundle
-import com.tokopedia.common_epharmacy.network.response.EPharmacyPrepareProductsGroupResponse
 import com.tokopedia.epharmacy.adapters.factory.EPharmacyAdapterFactory
 import com.tokopedia.epharmacy.component.BaseEPharmacyDataModel
+import com.tokopedia.common_epharmacy.network.response.EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup as EG
 
 data class EPharmacyAttachmentDataModel(
     val name: String = "",
@@ -11,18 +11,18 @@ data class EPharmacyAttachmentDataModel(
     val epharmacyGroupId: String?,
     val enablerName: String?,
     val enablerLogo: String?,
-    val shopInfo: EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.ProductsInfo?,
+    val shopInfo: EG.ProductsInfo?,
     val consultationStatus: Int?,
     val consultationString: String?,
-    val prescription: List<EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.ConsultationData.Prescription?>?,
+    val prescription: List<EG.ConsultationData.Prescription?>?,
     val partnerConsultationId: String?,
     val tokoConsultationId: Long?,
-    var prescriptionImages: List<EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.PrescriptionImage?>?,
+    var prescriptionImages: List<EG.PrescriptionImage?>?,
     val prescriptionSource: List<String?>?,
-    val consultationSource: EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.ConsultationSource?,
-    val consultationData: EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.ConsultationData?,
+    val consultationSource: EG.ConsultationSource?,
+    val consultationData: EG.ConsultationData?,
     var productsIsExpanded: Boolean = false,
-    var prescriptionCTA: EPharmacyPrepareProductsGroupResponse.EPharmacyPrepareProductsGroupData.GroupData.EpharmacyGroup.PrescriptionCTA?,
+    var prescriptionCTA: EG.PrescriptionCTA?,
     val showUploadWidget: Boolean = false,
     val showDivider: Boolean = true,
     var isError: Boolean = false,
