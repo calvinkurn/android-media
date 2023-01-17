2adc71987bef7aedd6034e33e2a8aec7163fc440
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-01-18 02:54:26 +0530

    added gql annotation

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/model/raw/GQLAffiliate_SSE_Token.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.affiliate.model.raw
-
-val GQL_Affiliate_SSE_Token: String = """query GetAffiliateToken(){
-  getAffiliateToken(){
-    Token
-  }
-}
-""".trimIndent()
--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/usecase/AffiliateSSEAuthTokenUseCase.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/usecase/AffiliateSSEAuthTokenUseCase.kt
@@ -1,16 +1,28 @@
 package com.tokopedia.affiliate.usecase
 
-import com.tokopedia.affiliate.model.raw.GQL_Affiliate_SSE_Token
 import com.tokopedia.affiliate.model.response.AffiliateSSEToken
 import com.tokopedia.affiliate.repository.AffiliateRepository
+import com.tokopedia.affiliate.usecase.AffiliateSSEAuthTokenUseCase.Companion.GQL_Affiliate_SSE_Token
+import com.tokopedia.gql_query_annotation.GqlQuery
 import javax.inject.Inject
 
+@GqlQuery("GetAffiliateToken", GQL_Affiliate_SSE_Token)
 class AffiliateSSEAuthTokenUseCase @Inject constructor(
     private val repository: AffiliateRepository
 ) {
+    companion object {
+        const val GQL_Affiliate_SSE_Token: String = """
+            query GetAffiliateToken(){
+                getAffiliateToken(){
+                    Token
+                }
+            }
+            """
+    }
+
     suspend fun getAffiliateToken(): AffiliateSSEToken {
         return repository.getGQLData(
-            GQL_Affiliate_SSE_Token,
+            GetAffiliateToken.GQL_QUERY,
             AffiliateSSEToken::class.java,
             mapOf()
         )
