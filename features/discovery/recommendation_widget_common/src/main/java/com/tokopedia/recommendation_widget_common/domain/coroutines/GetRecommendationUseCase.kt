14cae609897f063bcf78f8862ce2d1b1186e2f09
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2023-01-17 20:17:28 +0700

    revert usecase changes

--- features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetRecommendationUseCase.kt
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/domain/coroutines/GetRecommendationUseCase.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.recommendation_widget_common.domain.coroutines
 
 import android.content.Context
-import com.tokopedia.gql_query_annotation.GqlQueryInterface
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.localizationchooseaddress.util.ChooseAddressUtils
@@ -17,7 +16,6 @@ import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 import com.tokopedia.remoteconfig.RemoteConfig
 import com.tokopedia.remoteconfig.RemoteConfigKey
 import com.tokopedia.user.session.UserSession
-import java.lang.ref.WeakReference
 import javax.inject.Inject
 
 /**
@@ -25,36 +23,30 @@ import javax.inject.Inject
  */
 
 // [GetRecommendationUseCase] already sent choose address location params
-open class GetRecommendationUseCase @Inject constructor(
-    context: Context,
-    graphqlRepository: GraphqlRepository,
-) : UseCase<GetRecommendationRequestParam, List<RecommendationWidget>>() {
-    private val contextReference: WeakReference<Context> = WeakReference(context)
-    private val context: Context?
-        get() = contextReference.get()
+open class GetRecommendationUseCase @Inject
+constructor(private val context: Context, private val graphqlRepository: GraphqlRepository)
+    : UseCase<GetRecommendationRequestParam, List<RecommendationWidget>>() {
 
     private val graphqlUseCase = GraphqlUseCase<RecommendationEntity>(graphqlRepository)
-    private val getRecommendationQuery: GqlQueryInterface
-        get() {
-            val remoteConfig: RemoteConfig = FirebaseRemoteConfigImpl(context)
-            val isUseGqlFedQuery = remoteConfig.getBoolean(
-                RemoteConfigKey.RECOM_USE_GQL_FED_QUERY,
-                true
-            )
-            return if (isUseGqlFedQuery) {
+    init {
+        graphqlUseCase.setTypeClass(RecommendationEntity::class.java)
+        val remoteConfig: RemoteConfig = FirebaseRemoteConfigImpl(context)
+        graphqlUseCase.setGraphqlQuery(
+            if (remoteConfig.getBoolean(
+                    RemoteConfigKey.RECOM_USE_GQL_FED_QUERY,
+                    true
+                )
+            ) {
                 ListProductRecommendationQueryV2()
             } else {
                 ListProductRecommendationQuery()
             }
-        }
-    init {
-        graphqlUseCase.setTypeClass(RecommendationEntity::class.java)
-        graphqlUseCase.setGraphqlQuery(getRecommendationQuery)
+        )
     }
     override suspend fun getData(inputParameter: GetRecommendationRequestParam): List<RecommendationWidget> {
         val userSession = UserSession(context)
         inputParameter.userId = userSession.userId.toIntOrNull() ?: 0
-        val queryParam = context?.let { ChooseAddressUtils.getLocalizingAddressData(it).toQueryParam(inputParameter.queryParam) } ?: inputParameter.queryParam
+        val queryParam = ChooseAddressUtils.getLocalizingAddressData(context)?.toQueryParam(inputParameter.queryParam) ?: inputParameter.queryParam
         graphqlUseCase.setRequestParams(inputParameter.copy(queryParam = queryParam).toGqlRequest())
         return graphqlUseCase.executeOnBackground().productRecommendationWidget.data.mappingToRecommendationModel()
     }
