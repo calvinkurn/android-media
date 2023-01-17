14923f393b6d93ba3c489e1a8971110e974068ef
hiradevina <fakhira.devina@tokopedia.com>
2023-01-17 11:17:48 +0700

    fix: tips icon layout

--- libraries/common_compose/src/main/java/com/tokopedia/common_compose/principles/NestTips.kt
+++ libraries/common_compose/src/main/java/com/tokopedia/common_compose/principles/NestTips.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
@@ -16,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tokopedia.common_compose.ui.NestTheme
 
@@ -31,31 +35,42 @@ fun NestTips(
         backgroundColor = if (isSystemInDarkTheme()) NestTheme.colors.NN._200 else NestTheme.colors.NN._50
     ) {
         Box {
-            Box(
-                modifier = Modifier
-                    .background(NestTheme.colors.NN._100, shape = CircleShape)
-                    .align(
-                        Alignment.TopEnd
-                    )
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.Lightbulb,
-                    contentDescription = "tips logo",
-                    modifier = Modifier.padding(10.dp),
-                    tint = NestTheme.colors.NN._300
+            Box(modifier = Modifier.align(Alignment.TopEnd).offset(x = (6).dp, y = (-8).dp)) {
+                Box(
+                    modifier = Modifier
+                        .width(40.dp).height(40.dp)
+                        .background(NestTheme.colors.NN._100, shape = CircleShape)
                 )
             }
+            Icon(
+                imageVector = Icons.Outlined.Lightbulb,
+                contentDescription = "tips icon",
+                modifier = Modifier.align(Alignment.TopEnd).padding(end = 4.dp, top = 4.dp),
+                tint = NestTheme.colors.NN._300
+            )
             Column(modifier = Modifier.padding(16.dp)) {
                 title?.run {
                     NestTypography(
                         text = this,
-                        textStyle = NestTheme.typography.paragraph3.copy(fontWeight = FontWeight.Bold)
+                        textStyle = NestTheme.typography.paragraph3.copy(
+                            fontWeight = FontWeight.Bold,
+                            color = NestTheme.colors.NN._950
+                        )
                     )
                 }
                 description?.run {
-                    NestTypography(text = this, textStyle = NestTheme.typography.small)
+                    NestTypography(
+                        text = this,
+                        textStyle = NestTheme.typography.small.copy(color = NestTheme.colors.NN._950)
+                    )
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun NestTipsPreview() {
+    NestTips(title = "tips title", description = "tips description")
 }
