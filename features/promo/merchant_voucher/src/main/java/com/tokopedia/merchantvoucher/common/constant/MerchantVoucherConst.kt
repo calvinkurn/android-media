7fa6dcd79294473ab6ba362db64218fd369dcd48
isfaaghyth <isfaaghyth@gmail.com>
2023-01-18 10:26:17 +0700

    Fix: discount background image with a new asset

--- features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/common/constant/MerchantVoucherConst.kt
+++ features/promo/merchant_voucher/src/main/java/com/tokopedia/merchantvoucher/common/constant/MerchantVoucherConst.kt
@@ -2,6 +2,6 @@ package com.tokopedia.merchantvoucher.common.constant
 
 object MerchantVoucherConst {
     const val DELIVERY_VOUCHER_IMAGE_URL = "https://images.tokopedia.net/img/android/merchant_voucher/delivery_voucher_image.png"
-    const val DISCOUNT_VOUCHER_IMAGE_URL = "https://images.tokopedia.net/img/android/merchant_voucher/test_bg_topchat_mvc_discount.png"
+    const val DISCOUNT_VOUCHER_IMAGE_URL = "https://images.tokopedia.net/img/android/merchant_voucher/discount_voucher_image.png"
     const val DISCOUNT_OR_CASHBACK_VOUCHER_IMAGE_URL = "https://images.tokopedia.net/img/android/merchant_voucher/discount_or_cashback_voucher_image.png"
 }
