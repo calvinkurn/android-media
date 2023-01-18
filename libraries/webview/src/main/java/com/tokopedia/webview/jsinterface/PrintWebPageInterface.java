756841a3d7b602f318b6e899c30994cd537b5097
Misael Jonathan <misael.jonathan@tokopedia.com>
2023-01-18 17:35:24 +0700

    [Mark] move print function to dedicated func

--- libraries/webview/src/main/java/com/tokopedia/webview/jsinterface/PrintWebPageInterface.java
+++ libraries/webview/src/main/java/com/tokopedia/webview/jsinterface/PrintWebPageInterface.java
@@ -42,43 +42,47 @@ public final class PrintWebPageInterface {
         mRunnable = new Runnable() {
             @Override
             public void run() {
-                try {
-                    if (mContextRef.get() == null || mWebViewRef.get() == null) {
-                        return;
-                    }
-                    long currentTimestampCalled = System.currentTimeMillis();
-                    if (lastTimestampCalled + DELAY >= currentTimestampCalled) {
-                        return;
-                    }
-                    lastTimestampCalled = System.currentTimeMillis();
-                    PrintManager printManager = ContextCompat.getSystemService(mContextRef.get(), PrintManager.class);
-                    PrintDocumentAdapter adapter;
-                    PrintAttributes.Builder printAttrBuilder = new PrintAttributes.Builder();
-                    PrintAttributes printAttr = printAttrBuilder.setMediaSize(
-                            PrintAttributes.MediaSize.ISO_A4
-                    ).build();
-
-                    String jobName = "";
-                    if (filename.isEmpty()) {
-                        jobName = DEFAULT_JOB_NAME_PREFIX + currentTimestampCalled;
-                    } else {
-                        jobName = filename;
-                    }
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        adapter = mWebViewRef.get().createPrintDocumentAdapter(jobName);
-                    } else {
-                        adapter = mWebViewRef.get().createPrintDocumentAdapter();
-                    }
-
-                    if (printManager != null) {
-                        printManager.print(jobName, adapter, printAttr);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                executePrint(filename);
             }
         };
         mHandler.post(mRunnable);
+    }
+
+    private void executePrint(String filename) {
+        try {
+            if (mContextRef.get() == null || mWebViewRef.get() == null) {
+                return;
+            }
+            long currentTimestampCalled = System.currentTimeMillis();
+            if (lastTimestampCalled + DELAY >= currentTimestampCalled) {
+                return;
+            }
+            lastTimestampCalled = System.currentTimeMillis();
+            PrintManager printManager = ContextCompat.getSystemService(mContextRef.get(), PrintManager.class);
+            PrintDocumentAdapter adapter;
+            PrintAttributes.Builder printAttrBuilder = new PrintAttributes.Builder();
+            PrintAttributes printAttr = printAttrBuilder.setMediaSize(
+                    PrintAttributes.MediaSize.ISO_A4
+            ).build();
+
+            String jobName = "";
+            if (filename.isEmpty()) {
+                jobName = DEFAULT_JOB_NAME_PREFIX + currentTimestampCalled;
+            } else {
+                jobName = filename;
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                adapter = mWebViewRef.get().createPrintDocumentAdapter(jobName);
+            } else {
+                adapter = mWebViewRef.get().createPrintDocumentAdapter();
+            }
+
+            if (printManager != null) {
+                printManager.print(jobName, adapter, printAttr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
