3a84e5604bdc4bbd6161ed29d17574e5669d5e6d
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-01-19 20:57:45 +0700

    Delete ReviewAdapter.kt

--- features/discovery/home_nav/src/main/java/com/tokopedia/homenav/mainnav/view/adapter/viewholder/review/ReviewAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.homenav.mainnav.view.adapter.viewholder.review
-
-import com.tokopedia.abstraction.base.view.adapter.adapter.BaseAdapter
-import com.tokopedia.homenav.mainnav.view.adapter.typefactory.ReviewTypeFactoryImpl
-
-class ReviewAdapter(typeFactory: ReviewTypeFactoryImpl)
-    : BaseAdapter<ReviewTypeFactoryImpl>(typeFactory) {
-}
-
