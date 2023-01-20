8b5548289cebea05f35c983099f678af5cb0e1e9
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-01-20 21:39:04 +0800

    improve SSE connecting scheme

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
@@ -340,11 +340,6 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
         context?.let { UpdateShopActiveWorker.execute(it) }
     }
 
-    override fun onResume() {
-        super.onResume()
-        startWidgetSse()
-    }
-
     override fun onPause() {
         super.onPause()
         stopWidgetSse()
@@ -382,6 +377,7 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
             if (!isFirstLoad) {
                 getShopStateInfoIfEligible()
             }
+            startWidgetSse()
         }
     }
 
