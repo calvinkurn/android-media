c8ed625c44d0ae5074aa773760d5d24ba89509ed
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-01-24 14:49:26 +0700

    remove unused import

--- features/user/usercomponents/src/debug/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugFragment.kt
+++ features/user/usercomponents/src/debug/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
