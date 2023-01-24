837c0d64bb74f816ffcf90f02fe9e697e60bf0a5
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-01-24 20:16:41 +0800

    handle navigation default destination

--- features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/data/local/PersonaSharedPreference.kt
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/data/local/PersonaSharedPreference.kt
@@ -15,11 +15,20 @@ class PersonaSharedPreference @Inject constructor(
 
     companion object {
         private const val SHARED_PREF = "SellerPersonaSharedPref"
+        private const val KEY_IS_FIRST_VISIT = "keyIsFirstVisit"
     }
 
     private val sharedPref: SharedPreferences by lazy {
         context.getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
     }
 
+    fun setHasFirstVisit() {
+        val spe = sharedPref.edit()
+        spe.putBoolean(KEY_IS_FIRST_VISIT, false)
+        spe.apply()
+    }
 
+    fun isFirstVisit(): Boolean {
+        return sharedPref.getBoolean(KEY_IS_FIRST_VISIT, true)
+    }
 }
--- features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/view/activity/SellerPersonaActivity.kt
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/view/activity/SellerPersonaActivity.kt
@@ -1,21 +1,51 @@
 package com.tokopedia.sellerpersona.view.activity
 
+import android.os.Build
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.kotlin.extensions.view.getResColor
+import com.tokopedia.kotlin.extensions.view.setLightStatusBar
+import com.tokopedia.kotlin.extensions.view.setStatusBarColor
+import com.tokopedia.sellerpersona.R
+import com.tokopedia.sellerpersona.data.local.PersonaSharedPreference
 import com.tokopedia.sellerpersona.databinding.ActivitySellerPersonaBinding
+import com.tokopedia.sellerpersona.di.DaggerSellerPersonaComponent
+import com.tokopedia.sellerpersona.di.SellerPersonaComponent
+import javax.inject.Inject
 
 /**
  * Created by @ilhamsuaib on 17/01/23.
  */
 
-class SellerPersonaActivity : BaseActivity() {
+class SellerPersonaActivity : BaseActivity(), HasComponent<SellerPersonaComponent> {
+
+    @Inject
+    lateinit var sharedPref: PersonaSharedPreference
 
     private var binding: ActivitySellerPersonaBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initInjector()
         setContentView()
-        setupView()
+        setNavigationStartDestination()
+        setWhiteStatusBar()
+
+    }
+
+    override fun getComponent(): SellerPersonaComponent {
+        return DaggerSellerPersonaComponent.builder()
+            .baseAppComponent((applicationContext as BaseMainApplication).baseAppComponent)
+            .build()
+    }
+
+    private fun initInjector() {
+        component.inject(this)
     }
 
     private fun setContentView() {
@@ -24,9 +54,27 @@ class SellerPersonaActivity : BaseActivity() {
         }
     }
 
-    private fun setupView() {
-        binding?.headerSellerPersona?.let {
-            setSupportActionBar(it)
+    private fun setNavigationStartDestination() {
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val inflater = navController.navInflater
+        val graph = inflater.inflate(R.navigation.nav_graph)
+
+        val defaultDestination = if (sharedPref.isFirstVisit()) {
+            R.id.openingFragment
+        } else {
+            R.id.resultFragment
+        }
+        graph.startDestination = defaultDestination
+
+        navController.graph = graph
+    }
+
+    private fun setWhiteStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setStatusBarColor(getResColor(com.tokopedia.unifyprinciples.R.color.Unify_N0))
+            setLightStatusBar(true)
         }
     }
 }
