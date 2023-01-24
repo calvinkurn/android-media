04eecd68653dd9a43553a3797049bd1dce177113
fajarca <fajar.azhar@tokopedia.com>
2023-01-24 12:33:00 +0700

    Add event label

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/AddProductTracker.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/AddProductTracker.kt
@@ -1,18 +1,19 @@
 package com.tokopedia.mvc.util.tracker
 
+import com.tokopedia.mvc.domain.entity.enums.PageMode
 import com.tokopedia.mvc.util.constant.TrackerConstant
 import com.tokopedia.track.builder.Tracker
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
 class AddProductTracker @Inject constructor(private val userSession: UserSessionInterface) {
-    
-    fun sendClickButtonBackToPreviousPageEvent() {
+
+    fun sendClickButtonBackToPreviousPageEvent(pageMode: PageMode) {
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click kembali button - third step")
             .setEventCategory("kupon toko saya - creation pengaturan kupon")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39416")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
@@ -21,12 +22,12 @@ class AddProductTracker @Inject constructor(private val userSession: UserSession
             .send()
     }
     
-    fun sendClickToolbarBackButtonEvent() {
+    fun sendClickToolbarBackButtonEvent(pageMode: PageMode) {
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click kembali arrow - third step")
             .setEventCategory("kupon toko saya - creation pengaturan kupon")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39417")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
@@ -35,12 +36,12 @@ class AddProductTracker @Inject constructor(private val userSession: UserSession
             .send()
     }
     
-    fun sendClickAddProductButtonEvent() {
+    fun sendClickAddProductButtonEvent(pageMode: PageMode) {
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click tambah produk")
             .setEventCategory("kupon toko saya - creation daftar produk")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39418")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
@@ -48,5 +49,14 @@ class AddProductTracker @Inject constructor(private val userSession: UserSession
             .build()
             .send()
     }
-    
+
+    private fun PageMode.asEventLabel(): String {
+        val mode = if (this == PageMode.CREATE) {
+            "create"
+        } else {
+            "edit"
+        }
+
+        return "voucher_step: $mode"
+    }
 }
--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/ProductListPageTracker.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/ProductListPageTracker.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.mvc.util.tracker
 
+import com.tokopedia.mvc.domain.entity.enums.PageMode
 import com.tokopedia.mvc.util.constant.TrackerConstant
 import com.tokopedia.track.builder.Tracker
 import com.tokopedia.user.session.UserSessionInterface
@@ -7,12 +8,12 @@ import javax.inject.Inject
 
 class ProductListPageTracker @Inject constructor(private val userSession: UserSessionInterface) {
 
-    fun sendClickToolbarBackButtonEvent() {
+    fun sendClickToolbarBackButtonEvent(pageMode: PageMode) {
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click kembali arrow - fourth step")
             .setEventCategory("kupon toko saya - creation daftar produk")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39419")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
@@ -22,12 +23,12 @@ class ProductListPageTracker @Inject constructor(private val userSession: UserSe
     }
 
 
-    fun sendButtonContinueClickEvent() {
+    fun sendButtonContinueClickEvent(pageMode: PageMode) {
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click lanjut - fourth step")
             .setEventCategory("kupon toko saya - creation daftar produk")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39420")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
@@ -36,17 +37,29 @@ class ProductListPageTracker @Inject constructor(private val userSession: UserSe
             .send()
     }
 
-    fun sendClickToolbarBackButtonWithProductSelectedEvent() {
+    fun sendClickToolbarBackButtonWithProductSelectedEvent(pageMode: PageMode) {
+
+
         Tracker.Builder()
             .setEvent(TrackerConstant.EVENT)
             .setEventAction("click kembali arrow - product selected - fourth step")
             .setEventCategory("kupon toko saya - creation daftar produk")
-            .setEventLabel("")
+            .setEventLabel(pageMode.asEventLabel())
             .setCustomProperty(TrackerConstant.TRACKER_ID, "39421")
             .setBusinessUnit(TrackerConstant.BUSINESS_UNIT)
             .setCurrentSite(TrackerConstant.CURRENT_SITE)
             .setShopId(userSession.shopId)
             .build()
             .send()
+    }
+
+    private fun PageMode.asEventLabel(): String {
+        val mode = if (this == PageMode.CREATE) {
+            "create"
+        } else {
+            "edit"
+        }
+
+        return "voucher_step: $mode"
     }
 }
