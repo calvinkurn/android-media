47e49ae59c9d36c215b0520396bca6423771ac61
Satyam Tewari <satyam.tewari@tokopedia.com>
2023-01-24 18:41:21 +0530

    buat iklan grup page bug fixes

--- features/top_ads/top_ads_create/src/main/java/com/tokopedia/topads/view/fragment/MpCreateAdGroupFragment.kt
+++ features/top_ads/top_ads_create/src/main/java/com/tokopedia/topads/view/fragment/MpCreateAdGroupFragment.kt
@@ -3,6 +3,7 @@ package com.tokopedia.topads.view.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -29,9 +30,13 @@ private const val GROUP_DETAIL_PAGE = "android.group_detail"
 class MpCreateAdGroupFragment : BaseDaggerFragment() {
 
     private lateinit var binding: FragmentMpCreateAdGroupBinding
-    private var minDailyBudget: Int? = null
-    private var maxDailyBudget: Int? = null
+    private var minDailyBudget: Int = 0
+    private var maxDailyBudget: Int = 0
     private var createdGroupId: String? = null
+    private var counter : Int = 1
+    private var autofillGroupName: String? = null
+    private var validGroupName : Boolean = false
+    private var validBudget: Boolean = false
 
     private val successImageUrl = "https://images.tokopedia.net/img/android/topads/createads_success/mp_group_creation_success_dialog.png"
     private var productId: String? = null
@@ -65,7 +70,6 @@ class MpCreateAdGroupFragment : BaseDaggerFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init()
-        setupToolbar()
         attachClickListeners()
     }
 
@@ -76,10 +80,96 @@ class MpCreateAdGroupFragment : BaseDaggerFragment() {
     }
 
     private fun init() {
+        binding.headerToolbar.setNavigationOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        binding.groupName.isLoading = true
+        binding.btnSubmit.isEnabled = false
+
         val suggestions = java.util.ArrayList<DataSuggestions>()
         suggestions.add(DataSuggestions("", listOf()))
         createGroupViewModel.getBidInfo(suggestions, GROUP_DETAIL_PAGE, this::onSuccessBidSuggestion)
-        createGroupViewModel.getProduct(productId, this::onSuccessNameSuggestion)
+        createGroupViewModel.getProduct("2150857075", this::onSuccessNameSuggestion)
+    }
+
+    private fun attachClickListeners() {
+        binding.dailyBudget.iconContainer.setOnClickListener {
+            MpCreateGroupBudgetHelpSheet().show(childFragmentManager, MpCreateAdGroupFragment::class.java.name)
+        }
+
+        binding.groupName.editText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                binding.groupName.setMessage("")
+                if(p0 != null && !p0.isEmpty()) {
+                    if(p0.toString().length > 70){
+                        binding.groupName.setMessage(getString(R.string.ad_groups_character_limit_error_msg))
+                        binding.groupName.isInputError = true
+                        validGroupName = false
+                        checkAllFieldsValidations()
+                    } else {
+                        binding.groupName.isInputError = false
+                        createGroupViewModel.validateGroup(
+                            p0.toString(),
+                            ::checkGroupNameValidation
+                        )
+                        binding.groupName.isLoading = true
+                    }
+                } else {
+                    binding.groupName.isInputError = true
+                    validGroupName = false
+                    checkAllFieldsValidations()
+                }
+            }
+        })
+
+        binding.dailyBudget.editText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                if(p0 != null && !p0.toString().isEmpty() && p0.toString().toDouble() < minDailyBudget){
+                    binding.dailyBudget.isInputError = true
+                    binding.dailyBudget.setMessage("${getString(R.string.min_budget_rp)} $minDailyBudget")
+                    validBudget = false
+                    checkAllFieldsValidations()
+                } else if (p0 != null && !p0.toString().isEmpty() && p0.toString().toDouble() > maxDailyBudget) {
+                    binding.dailyBudget.isInputError = true
+                    binding.dailyBudget.setMessage("${getString(R.string.max_budget_rp)} $maxDailyBudget")
+                    validBudget = false
+                    checkAllFieldsValidations()
+                } else if (p0 == null){
+                    binding.dailyBudget.isInputError = true
+                    binding.dailyBudget.setMessage("")
+                    validBudget = false
+                    checkAllFieldsValidations()
+                } else {
+                    binding.dailyBudget.setMessage("")
+                    binding.dailyBudget.isInputError = false
+                    validBudget = true
+                    checkAllFieldsValidations()
+                }
+            }
+        })
+
+        binding.btnSubmit.setOnClickListener {
+            if (!binding.btnSubmit.isLoading) {
+                binding.btnSubmit.isLoading = true
+                createGroupViewModel.topAdsCreate(
+                    listOf(productId ?: ""),
+                    binding.groupName.editText.text.toString(),
+                    binding.dailyBudget.editText.text.toString().toDouble(),
+                    this::onSuccessGroupCreate,
+                    this::onErrorGroupCreate
+                )
+            }
+        }
     }
 
     private fun onSuccessBidSuggestion(data: List<TopadsBidInfo.DataItem>) {
@@ -90,26 +180,26 @@ class MpCreateAdGroupFragment : BaseDaggerFragment() {
         }
     }
 
-    private fun onSuccessGroupValidation(data: ResponseGroupValidateName.TopAdsGroupValidateNameV2) {
-        if (data.errors.isEmpty()) {
-            createGroupViewModel.topAdsCreate(
-                listOf(productId ?: ""),
-                binding.groupName.editText.text.toString(),
-                binding.dailyBudget.editText.text.toString().toDouble(),
-                this::onSuccessGroupCreate,
-                this::onErrorGroupCreate
-            )
-        } else {
-            binding.groupName.isInputError = true
-            binding.groupName.setMessage(getString(R.string.the_group_name_is_already_in_use))
-            binding.btnSubmit.isLoading = false
+    private fun onSuccessNameSuggestion(response: TopAdsProductResponse) {
+        response.product.let {
+            var groupName : String = (it.category?.name ?: getString(R.string.group)) + " " + DateUtil.getCurrentDate().formatTo("dd/MM/yy")
+            autofillGroupName = groupName
+            createGroupViewModel.validateGroup(groupName, this::checkAutofillGroupNameValidation)
         }
     }
 
-    private fun onSuccessNameSuggestion(response: TopAdsProductResponse) {
-        response.product.let {
-            var currentDate = DateUtil.getCurrentDate().formatTo("dd/MM/yy")
-            binding.groupName.editText.text = Editable.Factory().newEditable("${it.productName} $currentDate")
+    private fun checkAutofillGroupNameValidation(data: ResponseGroupValidateName.TopAdsGroupValidateNameV2){
+        if (data.errors.isEmpty()) {
+            binding.groupName.editText.text = Editable.Factory().newEditable(data.data.groupName)
+            binding.groupName.isLoading = false
+            validGroupName = true
+            checkAllFieldsValidations()
+        } else {
+            validGroupName = false
+            checkAllFieldsValidations()
+            createGroupViewModel.validateGroup("$autofillGroupName ($counter)", this::checkAutofillGroupNameValidation)
+            counter++
+            binding.groupName.isLoading = true
         }
     }
 
@@ -129,30 +219,25 @@ class MpCreateAdGroupFragment : BaseDaggerFragment() {
         if (isEnoughDeposit) {
             openSuccessDialog()
         } else {
-            openInsufficientCreditsDialog()
+            openInsufficientCreditsDialog(data)
         }
     }
 
-    private fun setupToolbar() {
-        binding.headerToolbar.setNavigationOnClickListener {
-            activity?.onBackPressed()
+    private fun checkGroupNameValidation(data: ResponseGroupValidateName.TopAdsGroupValidateNameV2){
+        if (data.errors.isEmpty()) {
+            validGroupName = true
+            checkAllFieldsValidations()
+        } else {
+            binding.groupName.isInputError = true
+            binding.groupName.setMessage(getString(R.string.the_group_name_is_already_in_use))
+            validGroupName = false
+            checkAllFieldsValidations()
         }
+        binding.groupName.isLoading = false
     }
 
-    private fun attachClickListeners() {
-        binding.dailyBudget.iconContainer.setOnClickListener {
-            MpCreateGroupBudgetHelpSheet().show(childFragmentManager, MpCreateAdGroupFragment::class.java.name)
-        }
-
-        binding.btnSubmit.setOnClickListener {
-            if (!binding.btnSubmit.isLoading) {
-                binding.btnSubmit.isLoading = true
-                createGroupViewModel.validateGroup(
-                    binding.groupName.editText.text.toString(),
-                    this::onSuccessGroupValidation
-                )
-            }
-        }
+    private fun checkAllFieldsValidations(){
+        binding.btnSubmit.isEnabled = validGroupName && validBudget
     }
 
     private fun openSuccessDialog() {
@@ -175,9 +260,9 @@ class MpCreateAdGroupFragment : BaseDaggerFragment() {
         dialog.show()
     }
 
-    private fun openInsufficientCreditsDialog() {
+    private fun openInsufficientCreditsDialog(data: DepositAmount) {
         var dialog = DialogUnify(requireContext(), DialogUnify.VERTICAL_ACTION, DialogUnify.NO_IMAGE)
-        dialog.setDescription(getString(R.string.success_group_creation_insufficient_credits_text))
+        dialog.setDescription(getString(R.string.success_group_creation_insufficient_credits_text).replace("Rpx.xxx","Rp ${data.amount}"))
         dialog.setTitle(getString(R.string.ads_created_successfully_but_cant_appear_yet))
         dialog.setPrimaryCTAText(getString(R.string.add_credit))
         dialog.setSecondaryCTAText(getString(R.string.later))
--- features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/raw/TopAdsProductGQL.kt
+++ features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/raw/TopAdsProductGQL.kt
@@ -2,10 +2,15 @@ package com.tokopedia.topads.common.data.raw
 
 const val TOP_ADS_PRODUCT_GQL = """query getProductV3(${'$'}productID:String!, ${'$'}options:OptionV3!){
       getProductV3(productID:${'$'}productID, options: ${'$'}options){
-          productID
-          productName
-          price
-          description
+            productID
+            productName
+            price
+            description
+            category {
+                id
+                name
+                title
+            }
       }
 }
 """
