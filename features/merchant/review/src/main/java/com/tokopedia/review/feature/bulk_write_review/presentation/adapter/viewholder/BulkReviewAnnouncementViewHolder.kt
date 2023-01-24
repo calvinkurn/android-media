05307751275e9bd31f0cc28ed7fe006a59bf8201
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-01-24 16:02:51 +0700

    [Review][BulkReview] Use ImageUnify xml attribute to set image and remove unnecessary view binding class

--- features/merchant/review/src/main/java/com/tokopedia/review/feature/bulk_write_review/presentation/adapter/viewholder/BulkReviewAnnouncementViewHolder.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/bulk_write_review/presentation/adapter/viewholder/BulkReviewAnnouncementViewHolder.kt
@@ -2,7 +2,6 @@ package com.tokopedia.review.feature.bulk_write_review.presentation.adapter.view
 
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.media.loader.loadImage
 import com.tokopedia.review.R
 import com.tokopedia.review.databinding.ItemBulkReviewAnnouncementCardBinding
 import com.tokopedia.review.feature.bulk_write_review.presentation.uimodel.BulkReviewAnnouncementUiModel
@@ -13,20 +12,11 @@ class BulkReviewAnnouncementViewHolder(
 
     companion object {
         val LAYOUT = R.layout.item_bulk_review_announcement_card
-        private const val IMAGE_URL = "https://images.tokopedia.net/img/android/review/ic_bulk_review_announcement.png"
     }
 
     private val binding = ItemBulkReviewAnnouncementCardBinding.bind(itemView)
 
-    init {
-        initViews()
-    }
-
     override fun bind(element: BulkReviewAnnouncementUiModel) {
         binding.tvBulkReviewAnnouncement.text = element.text
-    }
-
-    private fun initViews() {
-        binding.icBulkReviewAnnouncement.loadImage(IMAGE_URL)
     }
 }
--- features/merchant/review/src/main/java/com/tokopedia/review/feature/bulk_write_review/presentation/widget/WidgetBulkReviewSubmitLoader.kt
+++ features/merchant/review/src/main/java/com/tokopedia/review/feature/bulk_write_review/presentation/widget/WidgetBulkReviewSubmitLoader.kt
@@ -2,27 +2,20 @@ package com.tokopedia.review.feature.bulk_write_review.presentation.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
+import android.view.View
 import com.tokopedia.kotlin.extensions.view.ZERO
-import com.tokopedia.media.loader.loadImage
-import com.tokopedia.review.databinding.WidgetBulkReviewSubmitLoaderBinding
+import com.tokopedia.review.R
 import com.tokopedia.unifycomponents.BaseCustomView
 
 class WidgetBulkReviewSubmitLoader @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = Int.ZERO
 ) : BaseCustomView(context, attrs, defStyleAttr) {
 
-    companion object {
-        private const val IMAGE_URL = "https://images.tokopedia.net/img/android/review/ic_bulk_review_submit_loader_illustration.png"
-    }
-
     init {
-        WidgetBulkReviewSubmitLoaderBinding.inflate(
-            LayoutInflater.from(context), this, true
-        ).initViews()
+        inflateWidget()
     }
 
-    private fun WidgetBulkReviewSubmitLoaderBinding.initViews() {
-        ivBulkReviewSubmitLoader.loadImage(IMAGE_URL)
+    private fun inflateWidget() {
+        View.inflate(context, R.layout.widget_bulk_review_submit_loader, this)
     }
 }
