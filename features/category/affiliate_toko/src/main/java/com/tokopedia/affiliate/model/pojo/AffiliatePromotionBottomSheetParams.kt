9f86750fbb28de1f5d5f6703274a8cc4f4bb3548
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-01-25 13:47:19 +0530

    added support for ssa in promotion bottom sheet

---
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/model/pojo/AffiliatePromotionBottomSheetParams.kt
@@ -0,0 +1,30 @@
+package com.tokopedia.affiliate.model.pojo
+
+import java.io.Serializable
+
+data class AffiliatePromotionBottomSheetParams(
+    val idArray: ArrayList<Int>?,
+    val itemId: String,
+    val itemName: String,
+    val itemImage: String,
+    val itemUrl: String,
+    val productIdentifier: String,
+    val origin: Int = 1, // default origin promosikan = 1
+    val isLinkGenerationEnabled: Boolean = true,
+    val commission: String = "",
+    val status: String = "",
+    val type: String? = "pdp",
+    val ssaInfo: SSAInfo? = null
+) : Serializable {
+    data class SSAInfo(
+        val ssaStatus: Boolean,
+        val ssaMessage: String,
+        val message: String,
+        val label: Label
+    ) {
+        data class Label(
+            val labelType: String,
+            val labelMessage: String
+        )
+    }
+}
