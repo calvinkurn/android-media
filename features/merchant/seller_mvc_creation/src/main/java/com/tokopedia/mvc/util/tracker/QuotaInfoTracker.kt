4c125c648680e085d783638668dd39926a4df27b
faisalramd <faisal.ramdani@tokopedia.com>
2023-01-25 14:06:07 +0700

    review changes

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/QuotaInfoTracker.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/tracker/QuotaInfoTracker.kt
@@ -3,19 +3,25 @@ package com.tokopedia.mvc.util.tracker
 import com.tokopedia.mvc.util.constant.MvcTrackerConstant.MVC_BUSINESS_UNIT
 import com.tokopedia.mvc.util.constant.MvcTrackerConstant.MVC_CURRENT_SITE
 import com.tokopedia.mvc.util.constant.MvcTrackerConstant.MVC_EVENT
+import com.tokopedia.mvc.util.constant.MvcTrackerConstant.TRACKER_ID
 import com.tokopedia.track.builder.Tracker
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
 class QuotaInfoTracker @Inject constructor(private val userSession: UserSessionInterface) {
+    companion object {
+        private const val EA_CLICK_CLOSE = "click close"
+        private const val EA_CLICK_BUTTON_UPGRADE = "click button upgrade"
+        private const val EC_QUOTA_INFO_PAGE = "kupon toko saya - sumber kuota"
+    }
 
     fun sendClickCloseEvent() {
         Tracker.Builder()
             .setEvent(MVC_EVENT)
-            .setEventAction("click close")
-            .setEventCategory("kupon toko saya - sumber kuota")
+            .setEventAction(EA_CLICK_CLOSE)
+            .setEventCategory(EC_QUOTA_INFO_PAGE)
             .setEventLabel("")
-            .setCustomProperty("trackerId", "39433")
+            .setCustomProperty(TRACKER_ID, "39433")
             .setBusinessUnit(MVC_BUSINESS_UNIT)
             .setCurrentSite(MVC_CURRENT_SITE)
             .setShopId(userSession.shopId)
@@ -26,10 +32,10 @@ class QuotaInfoTracker @Inject constructor(private val userSession: UserSessionI
     fun sendClickButtonUpgradeEvent(buttonName: String) {
         Tracker.Builder()
             .setEvent(MVC_EVENT)
-            .setEventAction("click button upgrade")
-            .setEventCategory("kupon toko saya - sumber kuota")
+            .setEventAction(EA_CLICK_BUTTON_UPGRADE)
+            .setEventCategory(EC_QUOTA_INFO_PAGE)
             .setEventLabel(buttonName)
-            .setCustomProperty("trackerId", "39434")
+            .setCustomProperty(TRACKER_ID, "39434")
             .setBusinessUnit(MVC_BUSINESS_UNIT)
             .setCurrentSite(MVC_CURRENT_SITE)
             .setShopId(userSession.shopId)
