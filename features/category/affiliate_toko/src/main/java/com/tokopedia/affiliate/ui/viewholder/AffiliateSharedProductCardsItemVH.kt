d0f0f72a53cf080dcd0d02336f5620cf2558e451
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-01-27 14:41:22 +0530

    added ssa label

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/viewholder/AffiliateSharedProductCardsItemVH.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/viewholder/AffiliateSharedProductCardsItemVH.kt
@@ -9,14 +9,21 @@ import com.tokopedia.affiliate.PAGE_TYPE_PDP
 import com.tokopedia.affiliate.PAGE_TYPE_SHOP
 import com.tokopedia.affiliate.interfaces.ProductClickInterface
 import com.tokopedia.affiliate.model.response.AffiliatePerformanceData
-import com.tokopedia.affiliate_toko.R
 import com.tokopedia.affiliate.ui.viewholder.viewmodel.AffiliateSharedProductCardsModel
+import com.tokopedia.affiliate_toko.R
+import com.tokopedia.kotlin.extensions.orFalse
+import com.tokopedia.kotlin.extensions.view.isVisible
 import com.tokopedia.unifycomponents.ImageUnify
+import com.tokopedia.unifycomponents.Label
+import com.tokopedia.unifycomponents.Label.Companion.HIGHLIGHT_LIGHT_GREEN
+import com.tokopedia.unifycomponents.Label.Companion.HIGHLIGHT_LIGHT_GREY
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.user.session.UserSession
 
-class AffiliateSharedProductCardsItemVH(itemView: View, private val productClickInterface: ProductClickInterface?)
-    : AbstractViewHolder<AffiliateSharedProductCardsModel>(itemView) {
+class AffiliateSharedProductCardsItemVH(
+    itemView: View,
+    private val productClickInterface: ProductClickInterface?
+) : AbstractViewHolder<AffiliateSharedProductCardsModel>(itemView) {
 
     companion object {
         @JvmField
@@ -29,39 +36,90 @@ class AffiliateSharedProductCardsItemVH(itemView: View, private val productClick
 
     override fun bind(element: AffiliateSharedProductCardsModel?) {
         element?.product?.let { product ->
-            itemView.findViewById<ImageUnify>(R.id.product_image)?.setImageUrl(product.image?.androidURL ?: "")
+            itemView.findViewById<ImageUnify>(R.id.product_image)
+                ?.setImageUrl(product.image?.androidURL ?: "")
             itemView.findViewById<Typography>(R.id.product_name)?.text = product.itemTitle
             if (product.status == PRODUCT_ACTIVE) {
-                itemView.findViewById<ImageUnify>(R.id.status_bullet)?.setImageDrawable(MethodChecker.getDrawable(itemView.context, R.drawable.affiliate_circle_active))
+                itemView.findViewById<ImageUnify>(R.id.status_bullet)?.setImageDrawable(
+                    MethodChecker.getDrawable(
+                        itemView.context,
+                        R.drawable.affiliate_circle_active
+                    )
+                )
                 itemView.findViewById<Typography>(R.id.product_status)?.run {
-                    setTextColor(MethodChecker.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_G500))
+                    setTextColor(
+                        MethodChecker.getColor(
+                            itemView.context,
+                            com.tokopedia.unifyprinciples.R.color.Unify_G500
+                        )
+                    )
                     text = getString(R.string.affiliate_active)
                 }
+                itemView.findViewById<Label>(R.id.ssa_label)?.apply {
+                    isVisible = element.product.ssaStatus.orFalse()
+                    text = element.product.label?.labelText
+                    setLabelType(HIGHLIGHT_LIGHT_GREEN)
+                }
             } else {
-                itemView.findViewById<ImageUnify>(R.id.status_bullet)?.setImageDrawable(MethodChecker.getDrawable(itemView.context, R.drawable.affiliate_circle_inactive))
+                itemView.findViewById<ImageUnify>(R.id.status_bullet)?.setImageDrawable(
+                    MethodChecker.getDrawable(
+                        itemView.context,
+                        R.drawable.affiliate_circle_inactive
+                    )
+                )
                 itemView.findViewById<Typography>(R.id.product_status)?.run {
-                    setTextColor(MethodChecker.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_NN500))
+                    setTextColor(
+                        MethodChecker.getColor(
+                            itemView.context,
+                            com.tokopedia.unifyprinciples.R.color.Unify_NN500
+                        )
+                    )
                     text = getString(R.string.affiliate_inactive)
                 }
+                itemView.findViewById<Label>(R.id.ssa_label)?.apply {
+                    isVisible = element.product.ssaStatus.orFalse()
+                    text = element.product.label?.labelText
+                    setLabelType(HIGHLIGHT_LIGHT_GREY)
+                }
             }
-            itemView.findViewById<Typography>(R.id.shop_name)?.text = product.footer?.firstOrNull()?.footerText
+            itemView.findViewById<Typography>(R.id.shop_name)?.text =
+                product.footer?.firstOrNull()?.footerText
             itemView.setOnClickListener {
                 sendSelectContentEvent(product)
-                val type = if(product.itemType == TYPE_PRODUCT) PAGE_TYPE_PDP else PAGE_TYPE_SHOP
-                productClickInterface?.onProductClick(product.itemID, product.itemTitle ?: "", product.image?.androidURL
-                        ?: "", product.defaultLinkURL ?: "", product.itemID, product.status ?: 0, type =  type )
+                val type = if (product.itemType == TYPE_PRODUCT) PAGE_TYPE_PDP else PAGE_TYPE_SHOP
+                productClickInterface?.onProductClick(
+                    product.itemID,
+                    product.itemTitle ?: "",
+                    product.image?.androidURL
+                        ?: "",
+                    product.defaultLinkURL ?: "",
+                    product.itemID,
+                    product.status ?: 0,
+                    type = type
+                )
             }
         }
     }
 
-    private fun sendSelectContentEvent(product: AffiliatePerformanceData.GetAffiliateItemsPerformanceList.Data.SectionData.Item) {
-        val label = if(product.status == AffiliatePerformaSharedProductCardsItemVH.PRODUCT_ACTIVE) AffiliateAnalytics.LabelKeys.ACTIVE else AffiliateAnalytics.LabelKeys.INACTIVE
+    private fun sendSelectContentEvent(
+        product: AffiliatePerformanceData.GetAffiliateItemsPerformanceList.Data.SectionData.Item
+    ) {
+        val label =
+            if (product.status == AffiliatePerformaSharedProductCardsItemVH.PRODUCT_ACTIVE) {
+                AffiliateAnalytics.LabelKeys.ACTIVE
+            } else {
+                AffiliateAnalytics.LabelKeys.INACTIVE
+            }
         AffiliateAnalytics.trackEventImpression(
             AffiliateAnalytics.EventKeys.SELECT_CONTENT,
             AffiliateAnalytics.ActionKeys.CLICK_PRODUCT_DAFTAR_LINK_PRODUK,
             AffiliateAnalytics.CategoryKeys.AFFILIATE_HOME_PAGE_LINK_HISTORY,
-            UserSession(itemView.context).userId,product.itemID,adapterPosition+1,product.itemTitle,"${product.itemID} - $label",
-            AffiliateAnalytics.ItemKeys.AFFILIATE_DAFTAR_LINK_PRODUK)
-
+            UserSession(itemView.context).userId,
+            product.itemID,
+            bindingAdapterPosition + 1,
+            product.itemTitle,
+            "${product.itemID} - $label",
+            AffiliateAnalytics.ItemKeys.AFFILIATE_DAFTAR_LINK_PRODUK
+        )
     }
 }
