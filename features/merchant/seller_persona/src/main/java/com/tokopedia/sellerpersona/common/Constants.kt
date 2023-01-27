e598af32f903501e34c5c95036889ca318737af5
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-01-27 14:02:52 +0800

    add image constants

---
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/common/Constants.kt
@@ -0,0 +1,16 @@
+package com.tokopedia.sellerpersona.common
+
+/**
+ * Created by @ilhamsuaib on 26/01/23.
+ */
+
+object Constants {
+
+    const val IMG_INTRODUCTION = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_introduction-min.png"
+    const val IMG_ENTRY_POINT = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_entry_point-min.png"
+    const val IMG_BACKGROUND_RUMAHAN = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_background_rumahan-min.png"
+    const val IMG_BACKGROUND_GEDONGAN = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_background_gedongan-min.png"
+    const val IMG_AVATAR_RUMAHAN = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_avatar_rumahan-min.png"
+    const val IMG_AVATAR_GEDONGAN = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_avatar_gedongan-min.png"
+    const val IMG_AVATAR_GEDONGAN_ADMIN = "https://images.tokopedia.net/img/android/sellerapp/seller_persona/img_persona_avatar_gedongan_admin-min.png"
+}
