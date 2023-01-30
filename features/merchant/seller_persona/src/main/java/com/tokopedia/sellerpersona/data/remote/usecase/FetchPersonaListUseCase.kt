2dec37a36f147b8a77fcc7e10e513643be4dcc18
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-01-30 18:02:34 +0800

    remove persona list dummy data

--- features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/data/remote/usecase/FetchPersonaListUseCase.kt
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/data/remote/usecase/FetchPersonaListUseCase.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.sellerpersona.data.remote.usecase
 
-import com.google.gson.Gson
 import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
@@ -27,8 +26,7 @@ class FetchPersonaListUseCase @Inject constructor(
 
     suspend fun execute(): List<PersonaUiModel> {
         try {
-            val response = Gson().fromJson(dummy, FetchPersonaListResponse::class.java)
-            //val response = executeOnBackground()
+            val response = executeOnBackground()
             if (response.error) {
                 throw MessageErrorException(response.errorMsg)
             }
@@ -41,68 +39,6 @@ class FetchPersonaListUseCase @Inject constructor(
     companion object {
         const val QUERY = """
             
-        """
-        const val dummy = """
-            {
-                "error": false,
-                "errorMsg": "",
-                "data": [
-                    {
-                        "name": "mom-and-pop",
-                        "header": {
-                            "title": "Rumahan",
-                            "subtitle": "Pemilik Toko",
-                            "image": "",
-                            "backgroundImage": ""
-                        },
-                        "body": {
-                            "title": "Pilih tipe ini jika kamu:",
-                            "itemList": [
-                                "Menerima min. 2 pesanan per minggu",
-                                "Belum punya toko fisik (offline)",
-                                "Mengurus sendiri operasional toko",
-                                "Mengakses Tokopedia Seller di desktop"
-                            ]
-                        }
-                    },
-                    {
-                        "name": "corporate-supervisor-owner",
-                        "header": {
-                            "title": "Gedongan",
-                            "subtitle": "Pemilik Toko",
-                            "image": "",
-                            "backgroundImage": ""
-                        },
-                        "body": {
-                            "title": "Pilih tipe ini jika kamu:",
-                            "itemList": [
-                                "Menerima 1-10 pesanan per hari",
-                                "Punya toko fisik (offline)",
-                                "Punya pegawai yang mengurus operasional toko",
-                                "Sering mencari peluang untuk strategi baru bisnismu"
-                            ]
-                        }
-                    },
-                    {
-                        "name": "corporate-employee",
-                        "header": {
-                            "title": "Gedongan",
-                            "subtitle": "Admin Toko",
-                            "image": "",
-                            "backgroundImage": ""
-                        },
-                        "body": {
-                            "title": "Pilih tipe ini jika kamu:",
-                            "itemList": [
-                                "Menerima 1-10 pesanan per hari",
-                                "Punya toko fisik (offline)",
-                                "Mengurus operasional toko",
-                                "Mengakses Tokopedia Seller di desktop dan aplikasi HP"
-                            ]
-                        }
-                    }
-                ]
-            }
         """
     }
 }
