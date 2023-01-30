f8020d376ff997dbf1d21ab19307a87bbc6004a4
astidhiyaa <asti.anzaria@tokopedia.com>
2023-01-30 19:27:46 +0800

    fix: revert card adjustment - autoplay

--- features/content/play/src/main/java/com/tokopedia/play/ui/explorewidget/PlayExploreWidgetCoordinator.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/explorewidget/PlayExploreWidgetCoordinator.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import com.tokopedia.play.widget.ui.PlayWidgetLargeView
 import com.tokopedia.play.widget.ui.listener.PlayWidgetListener
+import com.tokopedia.play.widget.ui.model.PlayWidgetType
 import com.tokopedia.play.widget.ui.model.PlayWidgetUiModel
 import kotlinx.coroutines.*
 
@@ -13,14 +14,16 @@ import kotlinx.coroutines.*
 
 class PlayExploreWidgetCoordinator(
     lifecycleOwner: LifecycleOwner? = null,
-    mainCoroutineDispatcher: CoroutineDispatcher = Dispatchers.Main.immediate,
+    mainCoroutineDispatcher: CoroutineDispatcher = Dispatchers.Main.immediate
 ) : LifecycleObserver {
 
     private val scope = CoroutineScope(mainCoroutineDispatcher)
 
     private var mListener: PlayWidgetListener? = null
 
-    //autoPlay//autoRefresh//applylistener
+    private val autoplayCoordinator = PlayExploreWidgetAutoPlay(scope, mainCoroutineDispatcher)
+
+    // autoPlay//autoRefresh//applylistener
 
     init {
         lifecycleOwner?.let { configureLifecycle(it) }
@@ -32,31 +35,37 @@ class PlayExploreWidgetCoordinator(
 
     fun connect(widget: PlayWidgetLargeView, model: PlayWidgetUiModel) {
         widget.setData(model)
+        autoplayCoordinator.configureAutoPlay(widget, model.config, PlayWidgetType.Medium)
     }
 
     fun setListener(listener: PlayWidgetListener?) {
         mListener = listener
     }
 
-
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
+        autoplayCoordinator.onPause()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
+        autoplayCoordinator.onResume()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
+        autoplayCoordinator.onDestroy()
         scope.coroutineContext.cancelChildren()
     }
 
     private fun configureLifecycle(lifecycleOwner: LifecycleOwner) {
         if (lifecycleOwner is Fragment) {
-            lifecycleOwner.viewLifecycleOwnerLiveData.observe(lifecycleOwner, Observer {
-                it.lifecycle.addObserver(this)
-            })
+            lifecycleOwner.viewLifecycleOwnerLiveData.observe(
+                lifecycleOwner,
+                Observer {
+                    it.lifecycle.addObserver(this)
+                }
+            )
         } else {
             lifecycleOwner.lifecycle.addObserver(this)
         }
