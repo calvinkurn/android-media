1c60e233dda19481332809a047e76b83a65b588e
Harshit Jain <harshit.jain@tokopedia.com>
2023-01-31 10:43:14 +0530

    removed reset btn from ad sort bottomsheet

--- features/top_ads/top_ads_create/src/main/java/com/tokopedia/topads/view/customviews/AdGroupFilterBottomSheet.kt
+++ features/top_ads/top_ads_create/src/main/java/com/tokopedia/topads/view/customviews/AdGroupFilterBottomSheet.kt
@@ -1,11 +1,19 @@
 package com.tokopedia.topads.view.customviews
 
+import android.os.Bundle
+import android.view.View
 import com.tokopedia.filter.bottomsheet.filtergeneraldetail.FilterGeneralDetailBottomSheet
 import com.tokopedia.filter.common.data.Option
 
 class AdGroupFilterBottomSheet(private val callback:((option: Option,isChecked: Boolean) -> Unit)?=null) : FilterGeneralDetailBottomSheet() {
     override fun onOptionClick(option: Option, isChecked: Boolean, position: Int) {
-        callback?.invoke(option, isChecked)
         super.onOptionClick(option, isChecked, position)
+        callback?.invoke(option, isChecked)
+        clearAction()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        clearAction()
+        super.onViewCreated(view, savedInstanceState)
     }
 }
