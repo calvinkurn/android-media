e5dc5b85362be94b5be15445d4e11eaf6fbaa2b8
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-01-31 11:23:57 +0700

    feat: add isBanned field, but not implemented yet

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/ui/mapper/PlayShortsUiMapper.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/ui/mapper/PlayShortsUiMapper.kt
@@ -43,6 +43,7 @@ class PlayShortsUiMapper @Inject constructor(
         return PlayShortsConfigUiModel(
             shortsId = if (config.draftShortsId == 0) "" else config.draftShortsId.toString(),
             shortsAllowed = response.authorConfig.shortVideoAllowed,
+            isBanned = response.authorConfig.isBanned,
             tncList = response.authorConfig.tnc.map {
                 TermsAndConditionUiModel(desc = it.description)
             },
