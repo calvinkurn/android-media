0d585e4f3a224fe54bec8ef277e002fed42029d7
yohann.prananta <yohann.prananta@BA-00019887.local>
2023-01-31 18:07:46 +0700

    refactor findviewbyid in aep product draft page to view binding

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/draft/presentation/viewholder/ProductDraftListViewHolder.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/draft/presentation/viewholder/ProductDraftListViewHolder.kt
@@ -1,48 +1,40 @@
 package com.tokopedia.product.addedit.draft.presentation.viewholder
 
 import android.graphics.Typeface
-import android.view.View
-import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar
 import com.tokopedia.kotlin.extensions.view.loadImage
 import com.tokopedia.product.addedit.R
+import com.tokopedia.product.addedit.databinding.ItemProductDraftListBinding
 import com.tokopedia.product.addedit.draft.presentation.listener.ProductDraftListListener
 import com.tokopedia.product.addedit.draft.presentation.model.ProductDraftUiModel
-import com.tokopedia.unifyprinciples.Typography
 
-class ProductDraftListViewHolder(itemView: View, private val listener: ProductDraftListListener)
-    :  RecyclerView.ViewHolder(itemView) {
+class ProductDraftListViewHolder(
+    private val binding: ItemProductDraftListBinding,
+    private val listener: ProductDraftListListener
+) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bindData(draft: ProductDraftUiModel)  {
+    fun bindData(draft: ProductDraftUiModel) {
         itemView.run {
-            val ivProduct: AppCompatImageView? = findViewById(R.id.ivProduct)
-            val ivTrashCan: AppCompatImageView? = findViewById(R.id.ivTrashCan)
-            val tvCompletionPercentage: Typography? = findViewById(R.id.tvCompletionPercentage)
-            val tvProductName: Typography? = findViewById(R.id.tvProductName)
-            val pbCompletion: RoundCornerProgressBar? = findViewById(R.id.pbCompletion)
-
-            ivProduct?.loadImage(draft.imageUrl, R.drawable.ic_image_not_available)
+            binding.ivProduct.loadImage(draft.imageUrl, R.drawable.ic_image_not_available)
 
             if (draft.productName.isBlank()) {
-                tvProductName?.text = itemView.context.getString(R.string.label_draft_has_not_have_product_name_yet)
-                tvProductName?.setTypeface(tvProductName.typeface, Typeface.ITALIC)
+                binding.tvProductName.text = itemView.context.getString(R.string.label_draft_has_not_have_product_name_yet)
+                binding.tvProductName.setTypeface(binding.tvProductName.typeface, Typeface.ITALIC)
             } else {
-                tvProductName?.text = draft.productName
-                tvProductName?.setTypeface(tvProductName.typeface, Typeface.BOLD)
+                binding.tvProductName.text = draft.productName
+                binding.tvProductName.setTypeface(binding.tvProductName.typeface, Typeface.BOLD)
             }
 
-            tvCompletionPercentage?.text = itemView.context.getString(R.string.label_draft_item_percent_complete, draft.completionPercent)
-            pbCompletion?.progress = draft.completionPercent.toFloat()
+            binding.tvCompletionPercentage.text = itemView.context.getString(R.string.label_draft_item_percent_complete, draft.completionPercent)
+            binding.pbCompletion.progress = draft.completionPercent.toFloat()
 
             setOnClickListener {
                 listener.onDraftClickListener(draft.draftId)
             }
 
-            ivTrashCan?.setOnClickListener {
+            binding.ivTrashCan.setOnClickListener {
                 listener.onDraftDeleteListener(draft.draftId, adapterPosition, draft.productName)
             }
         }
     }
-
 }
