abc60fc8efdffa2680055a90491f9ce78b16870c
isfaaghyth <isfaaghyth@gmail.com>
2023-02-01 13:27:40 +0700

    Fix: missing GlobalConfig import class in Play

--- features/content/play/src/main/java/com/tokopedia/play/di/PlayModule.kt
+++ features/content/play/src/main/java/com/tokopedia/play/di/PlayModule.kt
@@ -13,6 +13,7 @@ import com.tokopedia.analyticsdebugger.debugger.ws.PlayWebSocketLogger
 import com.tokopedia.atc_common.AtcConstant
 import com.tokopedia.atc_common.domain.mapper.AddToCartDataMapper
 import com.tokopedia.atc_common.domain.usecase.AddToCartUseCase
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.GraphqlUseCase
