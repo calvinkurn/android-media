6174d359c1c46d6eb4009c1a52d676d7cecd6061
isfaaghyth <isfaaghyth@gmail.com>
2023-02-01 12:03:25 +0700

    Feat: add topchat websocket logger in developer options

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/TopchatWebSocketLoggingViewHolder.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/TopchatWebSocketLoggingViewHolder.kt
@@ -3,37 +3,30 @@ package com.tokopedia.developer_options.presentation.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.analyticsdebugger.sse.ui.activity.SSELoggingActivity
 import com.tokopedia.analyticsdebugger.websocket.ui.activity.WebSocketLoggingActivity
 import com.tokopedia.analyticsdebugger.websocket.ui.uimodel.WebSocketLogPageSource
 import com.tokopedia.developer_options.R
-import com.tokopedia.developer_options.presentation.model.PlayWebSocketSseLoggingUiModel
+import com.tokopedia.developer_options.presentation.model.TopchatWebSocketLoggingUiModel
 import com.tokopedia.unifycomponents.UnifyButton
 
-class PlayWebSocketSseLoggingViewHolder(
+class TopchatWebSocketLoggingViewHolder(
     itemView: View
-): AbstractViewHolder<PlayWebSocketSseLoggingUiModel>(itemView)
+): AbstractViewHolder<TopchatWebSocketLoggingUiModel>(itemView)
 {
-    private val btnViewSSELogging = itemView.findViewById<UnifyButton>(R.id.view_sse_logging_btn)
     private val btnViewWebSocketLogging = itemView.findViewById<UnifyButton>(R.id.view_websocket_logging_btn)
 
     init {
-        btnViewSSELogging.setOnClickListener {
-            itemView.context.apply {
-                startActivity(SSELoggingActivity.newInstance(this))
-            }
-        }
         btnViewWebSocketLogging.setOnClickListener {
             itemView.context.apply {
-                startActivity(WebSocketLoggingActivity.newInstance(this, WebSocketLogPageSource.PLAY))
+                startActivity(WebSocketLoggingActivity.newInstance(this, WebSocketLogPageSource.TOPCHAT))
             }
         }
     }
     companion object {
         @LayoutRes
-        val LAYOUT = R.layout.item_play_web_socket_sse_logging
+        val LAYOUT = R.layout.item_topchat_websocket_logging
     }
 
-    override fun bind(element: PlayWebSocketSseLoggingUiModel) {
-    }
+    override fun bind(element: TopchatWebSocketLoggingUiModel) = Unit
 }
+
