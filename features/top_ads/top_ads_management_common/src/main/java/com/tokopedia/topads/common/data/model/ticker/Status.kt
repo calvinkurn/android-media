d774328045fbd8a73db57cfe854ea97a92087bbe
steven.fredian <steven.fredian@tokopedia.com>
2023-02-02 17:16:54 +0700

    topads dashboard ticker

--- features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/model/ticker/Status.kt
+++ features/top_ads/top_ads_management_common/src/main/java/com/tokopedia/topads/common/data/model/ticker/Status.kt
@@ -1,4 +1,4 @@
-package com.tokopedia.topads.dashboard.data.model.ticker
+package com.tokopedia.topads.common.data.model.ticker
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
