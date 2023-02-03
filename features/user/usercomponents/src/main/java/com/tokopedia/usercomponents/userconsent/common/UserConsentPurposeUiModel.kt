921cadc4cb80611198e4e17112953dfaed762c0b
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-02-03 17:31:05 +0700

    implement user consent customize wording

--- features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/common/UserConsentPurposeUiModel.kt
+++ features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/common/UserConsentPurposeUiModel.kt
@@ -1,7 +1,5 @@
 package com.tokopedia.usercomponents.userconsent.common
 
-import com.tokopedia.usercomponents.userconsent.common.UserConsentCollectionDataModel.CollectionPointDataModel.PurposeDataModel
-
 data class UserConsentPurposeUiModel(
     var purposes: PurposeDataModel = PurposeDataModel()
 )
--- features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/ui/UserConsentDescription.kt
+++ features/user/usercomponents/src/main/java/com/tokopedia/usercomponents/userconsent/ui/UserConsentDescription.kt
@@ -5,10 +5,10 @@ import android.text.SpannableString
 import android.text.TextPaint
 import android.text.style.ClickableSpan
 import android.view.View
-import com.tokopedia.url.TokopediaUrl
 import com.tokopedia.usercomponents.userconsent.analytics.UserConsentAnalytics
-import com.tokopedia.usercomponents.userconsent.common.UserConsentCollectionDataModel
-import com.tokopedia.usercomponents.userconsent.common.UserConsentConst.PRIVACY
+import com.tokopedia.usercomponents.userconsent.common.AttributesItem
+import com.tokopedia.usercomponents.userconsent.common.CollectionPointDataModel
+import com.tokopedia.usercomponents.userconsent.common.StatementWording
 
 interface UserConsentDescriptionDelegate {
     val textTermCondition: String
@@ -24,200 +24,64 @@ interface UserConsentDescriptionDelegate {
     val textAgreementDefaultTncOptional: String
     val textAgreementDefaultTncPolicyOptional: String
     val unifyG500: Int
-
-    fun openConsentPageDetail(pageId: String, type: String = "1", tab: String = "")
-    fun openDefaultConsentPageDetail(url: String)
-    fun openPrivacyPolicyPage()
+    fun openWebview(url: String)
 }
 
 class UserConsentDescription constructor(
     private val delegate: UserConsentDescriptionDelegate,
-    private val collectionDataModel: UserConsentCollectionDataModel.CollectionPointDataModel = UserConsentCollectionDataModel.CollectionPointDataModel()
+    private val collectionDataModel: CollectionPointDataModel = CollectionPointDataModel()
 ) {
 
     private val userConsentAnalytics = UserConsentAnalytics()
 
-    fun generateTermConditionSinglePurposeText(
-        isChecklist: Boolean,
-        purposeText: String
+    fun generateDescriptionSpannableText(
+        statementWording: StatementWording
     ): SpannableString {
-        val message = if(isChecklist) {
-            delegate.textAgreementSingleTncOptional
-        } else {
-            delegate.textAgreementSingleTncMandatory
-        }
-
-        return SpannableString(String.format(message, delegate.textTermCondition, purposeText)).apply {
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnTnCHyperLinkClicked(it)
-                        }
-
-                        delegate.openConsentPageDetail(attributes.policyNoticeTnCPageID)
-                    }
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-        }
+        val customText = replaceText(statementWording)
+        return setUpSpannable(customText, statementWording.attributes)
     }
 
-    fun generateTermConditionPolicySinglePurposeText(
-        isChecklist: Boolean,
-        purposeText: String
-    ): SpannableString {
-        val message = if (isChecklist) {
-            delegate.textAgreementSingleTncPolicyOptional
-        } else {
-            delegate.textAgreementSingleTncPolicyMandatory
+    private fun replaceText(statementWording: StatementWording): String {
+        var textResult = statementWording.template
+
+        statementWording.attributes.forEach {
+            textResult = textResult.replace(it.key, it.text)
         }
 
-        return SpannableString(String.format(message, delegate.textTermCondition, delegate.textPolicy, purposeText)).apply {
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnTnCHyperLinkClicked(it)
-                        }
-
-                        delegate.openConsentPageDetail(attributes.policyNoticeTnCPageID)
-                    }
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnPolicyHyperLinkClicked(it)
-                        }
-
-                        delegate.openPrivacyPolicyPage()
-                    }
-                },
-                this.indexOf(delegate.textPolicy),
-                this.indexOf(delegate.textPolicy) + delegate.textPolicy.length,
-                0
-            )
-        }
+        return textResult
     }
 
-    fun generateTermConditionMultipleOptionalPurposeText(): SpannableString {
-        val message = delegate.textAgreementMultiTnc
-        return SpannableString(String.format(message, delegate.textTermCondition)).apply {
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnTnCHyperLinkClicked(it)
+    private fun setUpSpannable(text: String, listSpannable: List<AttributesItem>): SpannableString {
+        val spannable = SpannableString(text)
+
+        listSpannable.forEach {
+            if (it.link.isNotEmpty()) {
+                spannable.setSpan(
+                    createClickableSpannable {
+                        with(collectionDataModel) {
+                            purposes.let { purposes ->
+                                when(it.key) {
+                                    KEY_TNC -> {
+                                        userConsentAnalytics.trackOnTnCHyperLinkClicked(purposes)
+                                    }
+                                    KEY_PRIVACY -> {
+                                        userConsentAnalytics.trackOnPolicyHyperLinkClicked(purposes)
+                                    }
+                                }
+
+                            }
+
+                            delegate.openWebview(it.link)
                         }
-
-                        delegate.openConsentPageDetail(attributes.policyNoticeTnCPageID)
-                    }
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-        }
-    }
-
-    fun generateTermConditionPolicyMultipleOptionalPurposeText(): SpannableString {
-        val message = delegate.textAgreementMultiTncPolicy
-        return SpannableString(String.format(message, delegate.textTermCondition, delegate.textPolicy)).apply {
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnTnCHyperLinkClicked(it)
-                        }
-
-                        delegate.openConsentPageDetail(attributes.policyNoticeTnCPageID)
-                    }
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-
-            setSpan(
-                createClickableSpannable {
-                    with(collectionDataModel) {
-                        purposes.let {
-                            userConsentAnalytics.trackOnPolicyHyperLinkClicked(it)
-                        }
-
-                        delegate.openPrivacyPolicyPage()
-                    }
-                },
-                this.indexOf(delegate.textPolicy),
-                this.indexOf(delegate.textPolicy) + delegate.textPolicy.length,
-                0
-            )
-        }
-    }
-
-    fun generateDefaultTemplateTnc(isMandatory: Boolean, tncPage: String = ""): SpannableString {
-        val message = if (isMandatory) {
-            delegate.textAgreementDefaultTncMandatory
-        } else {
-            delegate.textAgreementDefaultTncOptional
+                    },
+                    spannable.indexOf(it.text),
+                    spannable.indexOf(it.text) + it.text.length,
+                    0
+                )
+            }
         }
 
-        return SpannableString(String.format(message, delegate.textTermCondition)).apply {
-            setSpan(
-                createClickableSpannable {
-                    delegate.openDefaultConsentPageDetail(
-                        tncPage.ifEmpty { DEFAULT_TNC_PAGE }
-                    )
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-        }
-    }
-
-    fun generateDefaultTemplateTncPolicy(
-        isMandatory: Boolean,
-        tncPage: String = "",
-        policyPage: String = ""
-    ): SpannableString {
-        val message = if (isMandatory) {
-            delegate.textAgreementDefaultTncPolicyMandatory
-        } else {
-            delegate.textAgreementDefaultTncPolicyOptional
-        }
-
-        return SpannableString(String.format(message, delegate.textTermCondition, delegate.textPolicy)).apply {
-            setSpan(
-                createClickableSpannable {
-                    delegate.openDefaultConsentPageDetail(
-                        tncPage.ifEmpty { DEFAULT_TNC_PAGE }
-                    )
-                },
-                this.indexOf(delegate.textTermCondition),
-                this.indexOf(delegate.textTermCondition) + delegate.textTermCondition.length,
-                0
-            )
-
-            setSpan(
-                createClickableSpannable {
-                    delegate.openDefaultConsentPageDetail(
-                        policyPage.ifEmpty { DEFAULT_PRIVACY_PAGE }
-                    )
-                },
-                this.indexOf(delegate.textPolicy),
-                this.indexOf(delegate.textPolicy) + delegate.textPolicy.length,
-                0
-            )
-        }
+        return spannable
     }
 
     private fun createClickableSpannable(action: () -> Unit): ClickableSpan {
@@ -233,7 +97,7 @@ class UserConsentDescription constructor(
     }
 
     companion object {
-        private val DEFAULT_TNC_PAGE = "${TokopediaUrl.getInstance().WEB}terms?lang=id"
-        private val DEFAULT_PRIVACY_PAGE = "${TokopediaUrl.getInstance().WEB}privacy?lang=id"
+        private const val KEY_TNC = "#tnc"
+        private const val KEY_PRIVACY = "#privacy"
     }
 }
