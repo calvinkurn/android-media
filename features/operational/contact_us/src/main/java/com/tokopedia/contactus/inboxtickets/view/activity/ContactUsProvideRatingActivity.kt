c3e080bc64ac2babfe04456dee928d78b8764a5b
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2023-02-06 15:41:55 +0700

    Remove unused code on testing

--- features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/activity/ContactUsProvideRatingActivity.kt
+++ features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/activity/ContactUsProvideRatingActivity.kt
@@ -3,8 +3,6 @@ package com.tokopedia.contactus.inboxtickets.view.activity
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
