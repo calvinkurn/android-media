93251ed7fd21113fc371c3a96313356d77f407c0
astidhiyaa <asti.anzaria@tokopedia.com>
2023-02-07 16:30:52 +0800

    feat: add visibility config

---
+++ features/content/play/src/main/java/com/tokopedia/play/channel/ui/component/CommentIconUiComponent.kt
@@ -0,0 +1,36 @@
+package com.tokopedia.play.channel.ui.component
+
+import com.tokopedia.play.databinding.ViewVodCommentBinding
+import com.tokopedia.play.ui.component.UiComponent
+import com.tokopedia.play.ui.view.comment.CommentIconUiView
+import com.tokopedia.play.util.CachedState
+import com.tokopedia.play.util.isChanged
+import com.tokopedia.play.view.uimodel.state.PlayViewerNewUiState
+import com.tokopedia.play_common.eventbus.EventBus
+
+/**
+ * @author by astidhiyaa on 07/02/23
+ */
+class CommentIconUiComponent(
+    bus: EventBus<in Event>,
+    binding: ViewVodCommentBinding,
+) : UiComponent<PlayViewerNewUiState> {
+
+    private val view = CommentIconUiView(binding, object : CommentIconUiView.Listener {
+        override fun onCommentClicked(view: CommentIconUiView) {
+            bus.emit(Event.OnCommentClicked)
+        }
+    })
+
+    override fun render(state: CachedState<PlayViewerNewUiState>) {
+        //ToDo: add comment counter value
+        if (state.isChanged { it.channel }) {
+            view.show(state.value.channel.channelInfo.channelType.isVod)
+            // view.setCounter("")
+        }
+    }
+
+    sealed interface Event {
+        object OnCommentClicked : Event
+    }
+}
