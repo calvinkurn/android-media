703656a17e31ea49bb382c6112058cb9d14ea830
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-02-08 17:59:40 +0700

    Fix: get parcelable applink

--- sellerapp/src/main/java/com/tokopedia/sellerapp/SplashScreenActivity.java
+++ sellerapp/src/main/java/com/tokopedia/sellerapp/SplashScreenActivity.java
@@ -64,7 +64,7 @@ public class SplashScreenActivity extends SplashScreen {
      * handle/forward app link redirection from customer app to seller app
      */
     private boolean handleAppLink(UserSessionInterface userSession) {
-        Uri uri = getIntent().getParcelableExtra(SELLER_APP_APPLINK, Uri.class);
+        Uri uri = getIntent().getParcelableExtra(SELLER_APP_APPLINK);
         if (null != uri) {
             boolean isFromMainApp = uri.getBooleanQueryParameter(RouteManager.KEY_REDIRECT_TO_SELLER_APP, false);
             boolean isAutoLogin = uri.getBooleanQueryParameter(KEY_AUTO_LOGIN, false);
