bb001cb93ce57fbcbeacba662588f140dbeacc64
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-02-09 10:10:59 +0700

    implement cassava test - new add phone number

---
+++ features/user/profilecompletion/src/androidTest/java/com/tokopedia/profilecompletion/addphone/NewAddPhoneCassavaTest.kt
@@ -0,0 +1,75 @@
+package com.tokopedia.profilecompletion.addphone
+
+import android.content.Intent
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers
+import com.tokopedia.analyticsdebugger.cassava.cassavatest.CassavaTestRule
+import com.tokopedia.analyticsdebugger.cassava.cassavatest.hasAllSuccess
+import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform
+import com.tokopedia.profilecompletion.addphone.view.activity.NewAddPhoneActivity
+import com.tokopedia.profilecompletion.common.helper.respondWithOk
+import com.tokopedia.profilecompletion.common.stub.di.TestComponentActivityFactory
+import com.tokopedia.profilecompletion.di.ActivityComponentFactory
+import com.tokopedia.test.application.annotations.UiTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@UiTest
+class NewAddPhoneCassavaTest {
+
+    private val testComponentFactory = TestComponentActivityFactory()
+
+    @get:Rule
+    var cassavaRule = CassavaTestRule(isFromNetwork = true, sendValidationResult = true)
+
+    @get:Rule
+    var activityTestRule = IntentsTestRule(
+        NewAddPhoneActivity::class.java,
+        false,
+        false
+    )
+
+    @Before
+    fun before() {
+        ActivityComponentFactory.instance = testComponentFactory
+    }
+
+    @After
+    fun after() {
+        activityTestRule.finishActivity()
+    }
+
+    @Test
+    fun input_invalid_phone_and_submit_then_error_is_displayed() {
+        activityTestRule.launchActivity(Intent())
+
+        inputInvalidValidatePhoneThenShowError()
+
+        ViewMatchers.assertThat(
+            cassavaRule.validate(TEST_CASE_ID_564),
+            hasAllSuccess()
+        )
+    }
+
+    @Test
+    fun input_valid_phone_and_submit_then_success_update() {
+        activityTestRule.launchActivity(Intent())
+
+        Intents.intending(IntentMatchers.hasData(ApplinkConstInternalUserPlatform.COTP)).respondWithOk()
+
+        inputValidUpdatePhoneThenSuccess()
+
+        ViewMatchers.assertThat(
+            cassavaRule.validate(TEST_CASE_ID_564),
+            hasAllSuccess()
+        )
+    }
+
+    companion object {
+        private const val TEST_CASE_ID_564 = "564"
+    }
+}
