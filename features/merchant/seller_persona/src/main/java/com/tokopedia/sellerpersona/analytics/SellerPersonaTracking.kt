4b4a325dca63cd7e65ce9f34948ac91e530dd964
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-02-09 13:48:58 +0800

    create persona tracker

--- features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/analytics/SellerPersonaTracking.kt
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/analytics/SellerPersonaTracking.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.sellerpersona.analytics
 
+import com.tokopedia.kotlin.extensions.view.EMPTY
 import com.tokopedia.track.builder.Tracker
 
 /**
@@ -8,120 +9,119 @@ import com.tokopedia.track.builder.Tracker
 
 object SellerPersonaTracking {
 
-
     fun sendSettingsClickSellerPersonaEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("settings - click seller persona")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40032")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.SETTINGS_CLICK_SELLER_PERSONA)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40032")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendImpressionSellerPersonaEvent() {
         Tracker.Builder()
-            .setEvent("viewPGIris")
-            .setEventAction("impression seller persona")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40033")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.VIEW_PG_IRIS)
+            .setEventAction(TrackingConst.IMPRESSION_SELLER_PERSONA)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40033")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
-    fun sendClickSellerPersonaNantiSajaEvent() {
+    fun sendClickSellerPersonaLaterEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona - nanti saja")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40034")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_LATER)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40034")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
-    fun sendClickSellerPersonaMulaiKuisEvent() {
+    fun sendClickSellerPersonaStartQuizEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona - mulai kuis")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40035")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_START_QUIZ)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40035")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendImpressionSellerPersonaResultEvent() {
         Tracker.Builder()
-            .setEvent("viewPGIris")
-            .setEventAction("impression seller persona result")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40036")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.VIEW_PG_IRIS)
+            .setEventAction(TrackingConst.IMPRESSION_SELLER_PERSONA_RESULT)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40036")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendClickSellerPersonaResultToggleActiveEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona result - toggle active")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40037")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_TOGGLE_ACTIVE)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40037")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendClickSellerPersonaResultSelectPersonaEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona result - select persona")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40038")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_SELECT_PERSONA)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40038")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendClickSellerPersonaResultRetakeQuizEvent() {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona result - retake quiz")
-            .setEventCategory("others tab")
-            .setEventLabel("")
-            .setCustomProperty("trackerId", "40039")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_TAKE_QUIZ)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
+            .setEventLabel(String.EMPTY)
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40039")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
 
     fun sendClickSellerPersonaResultSavePersonaEvent(eventLabel: String) {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("click seller persona result - save persona")
-            .setEventCategory("others tab")
+            .setEvent(TrackingConst.Event.CLICK_PG)
+            .setEventAction(TrackingConst.CLICK_SELLER_PERSONA_SAVE_PERSONA)
+            .setEventCategory(TrackingConst.Category.OTHER_TAB)
             .setEventLabel(eventLabel)
-            .setCustomProperty("trackerId", "40040")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(TrackingConst.TRACKER_ID, "40040")
+            .setBusinessUnit(TrackingConst.BUSINESS_UNIT)
+            .setCurrentSite(TrackingConst.TOKOPEDIA_MARKET_PLACE)
             .build()
             .send()
     }
--- features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/analytics/TrackingConst.kt
+++ features/merchant/seller_persona/src/main/java/com/tokopedia/sellerpersona/analytics/TrackingConst.kt
@@ -6,5 +6,25 @@ package com.tokopedia.sellerpersona.analytics
 
 object TrackingConst {
 
+    object Event {
+        const val CLICK_PG = "clickPG"
+        const val VIEW_PG_IRIS = "viewPGIris"
+    }
 
+    object Category {
+        const val OTHER_TAB = "others tab"
+    }
+
+    const val CLICK_SELLER_PERSONA_SAVE_PERSONA = "click seller persona result - save persona"
+    const val CLICK_SELLER_PERSONA_TAKE_QUIZ = "click seller persona result - retake quiz"
+    const val CLICK_SELLER_PERSONA_SELECT_PERSONA = "click seller persona result - select persona"
+    const val CLICK_SELLER_PERSONA_TOGGLE_ACTIVE = "click seller persona result - toggle active"
+    const val IMPRESSION_SELLER_PERSONA_RESULT = "impression seller persona result"
+    const val CLICK_SELLER_PERSONA_START_QUIZ = "click seller persona - mulai kuis"
+    const val CLICK_SELLER_PERSONA_LATER = "click seller persona - nanti saja"
+    const val IMPRESSION_SELLER_PERSONA = "impression seller persona"
+    const val SETTINGS_CLICK_SELLER_PERSONA = "settings - click seller persona"
+    const val TRACKER_ID = "trackerId"
+    const val BUSINESS_UNIT = "Physical Goods"
+    const val TOKOPEDIA_MARKET_PLACE = "tokopediamarketplace"
 }
