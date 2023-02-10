1ab7a92d876ae287b6183520fbd3ee3e5da206c9
rezagama <rezagamahidayat@gmail.com>
2023-02-10 16:18:04 +0700

    Change chucker config key

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
@@ -8,22 +8,22 @@ import android.content.SharedPreferences;
  */
 public class DevOptConfig {
 
-    private static final String CHUCK_ENABLED = "CHUCK_ENABLED";
-    private static final String IS_CHUCK_ENABLED = "is_enable";
+    private static final String CHUCKER_CONFIG = "chucker_config";
+    private static final String IS_CHUCKER_NORIFICATION_ENABLED = "is_chucker_notification_enabled";
 
     private static final String DEV_OPT_ON_NOTIF_ENABLED = "DEV_OPT_ON_NOTIF_ENABLED";
     private static final String IS_DEV_OPT_ON_NOTIF_ENABLED = "IS_DEV_OPT_ON_NOTIF_ENABLED";
 
     public static boolean isChuckNotifEnabled(Context context) {
-        SharedPreferences cache = context.getSharedPreferences(CHUCK_ENABLED, Context.MODE_PRIVATE);
-        return cache.getBoolean(IS_CHUCK_ENABLED, false);
+        SharedPreferences cache = context.getSharedPreferences(CHUCKER_CONFIG, Context.MODE_PRIVATE);
+        return cache.getBoolean(IS_CHUCKER_NORIFICATION_ENABLED, false);
     }
 
     public static void setChuckNotifEnabled(Context context, boolean isChuckNotifEnabled) {
-        context.getSharedPreferences(CHUCK_ENABLED, Context.MODE_PRIVATE)
+        context.getSharedPreferences(CHUCKER_CONFIG, Context.MODE_PRIVATE)
                 .edit()
                 .putBoolean(
-                        IS_CHUCK_ENABLED,
+                    IS_CHUCKER_NORIFICATION_ENABLED,
                         isChuckNotifEnabled
                 )
                 .apply();
