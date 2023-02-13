ba531ff985bd83df96b9439db382ea8cc2f3865e
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-02-13 09:13:13 +0700

    add todo widget mapping

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/model/DynamicHomeChannel.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/domain/model/DynamicHomeChannel.kt
@@ -308,6 +308,7 @@ data class DynamicHomeChannel(
             const val LAYOUT_MISSION_WIDGET: String = "mission_widget"
             const val LAYOUT_VPS_WIDGET: String = "4_banners_auto_vps_v2"
             const val LAYOUT_LEGO_4_PRODUCT: String = "lego_4_product"
+            const val LAYOUT_TODO_WIDGET_REVAMP: String = "todo_widget"
             const val channelId: String = "channelId"
             const val campaignCodeLabel: String = "campaignCode"
             const val DIVIDER_NO_DIVIDER = 0
