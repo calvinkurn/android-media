82bb5b5491f2d5b3445bcea5329720ae1ad74101
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-02-13 11:46:26 +0700

    revert unused changes

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/push/PushController.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/push/PushController.kt
@@ -97,23 +97,23 @@ class PushController(val context: Context) : CoroutineScope {
 
     private fun handleNotificationBundle(model: BaseNotificationModel) {
         launchCatchError(
-            block = {
-                if(model.isAmplification){
-                    if(!isAmpNotificationValid(model.notificationId))
-                        return@launchCatchError
-                }
-                if (model.notificationMode == NotificationMode.OFFLINE) {
-                    if (isOfflinePushEnabled)
-                        onOfflinePushPayloadReceived(model)
-                } else {
-                    onLivePushPayloadReceived(model)
-                }
+                block = {
+                    if(model.isAmplification){
+                        if(!isAmpNotificationValid(model.notificationId))
+                            return@launchCatchError
+                    }
+                    if (model.notificationMode == NotificationMode.OFFLINE) {
+                        if (isOfflinePushEnabled)
+                            onOfflinePushPayloadReceived(model)
+                    } else {
+                        onLivePushPayloadReceived(model)
+                    }
             }, onError = {
-                ServerLogger.log(Priority.P2, "CM_VALIDATION",
+            ServerLogger.log(Priority.P2, "CM_VALIDATION",
                     mapOf("type" to "exception",
                         "err" to Log.getStackTraceString(it).take(CMConstant.TimberTags.MAX_LIMIT),
                         "data" to model.toString().take(CMConstant.TimberTags.MAX_LIMIT)))
-            })
+        })
     }
 
     private suspend fun isAmpNotificationValid(notificationID: Int): Boolean {
