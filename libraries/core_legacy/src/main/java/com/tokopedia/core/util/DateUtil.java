c3e82a6ca8e08e624a53e5ad822ff9e814185f8d
rama.putra <rama.putra@tokopedia.com>
2023-02-13 10:40:40 +0700

    change to java

---
+++ libraries/core_legacy/src/main/java/com/tokopedia/core/util/DateUtil.java
@@ -0,0 +1,15 @@
+package com.tokopedia.core.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtil {
+    private static final String TIME_FORMAT = "ddmmyyhhmm";
+
+    public static String getCurrentTime() {
+        Date currentSystemTime = Calendar.getInstance().getTime();
+        return new SimpleDateFormat(TIME_FORMAT, Locale.getDefault()).format(currentSystemTime);
+    }
+}
