d1c725804b825754c39e6b5a7fc4af1e45ec341f
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-02-14 13:42:40 +0700

    fix: unused variables

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/common/SellerHomeConst.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/common/SellerHomeConst.kt
@@ -12,8 +12,6 @@ object SellerHomeConst {
     const val SHOP_SHARE_GQL_TRACKER_ACTION = "ShopSharing"
     const val SHOP_SHARE_GQL_TRACKER_SOURCE = "seller-home"
     const val HOME_BACKGROUND_RATIO = 0.75f
-    const val OTHER_BACKGROUND_RATIO = 0.7f
-
 
     object Images {
         const val IMG_ERROR_500 = "https://images.tokopedia.net/img/android/unify/unify_globalerrors_500.png"
@@ -23,9 +21,6 @@ object SellerHomeConst {
         const val SAH_SHOP_STATE_BG_RM_THEMATIC = "https://images.tokopedia.net/img/android/seller_home/sah_shop_state_bg_rm_thematic.png"
         const val SAH_SHOP_STATE_BG_PM_THEMATIC = "https://images.tokopedia.net/img/android/seller_home/sah_shop_state_bg_pm_thematic.png"
         const val SAH_SHOP_STATE_BG_OS_THEMATIC = "https://images.tokopedia.net/img/android/seller_home/sah_shop_state_bg_os_thematic.png"
-        const val BG_SAH_OTHER_CURVED_HEADER_THEMATIC_RM = "https://images.tokopedia.net/img/android/seller_home/bg_sah_other_curved_header_thematic_rm.png"
-        const val BG_SAH_OTHER_CURVED_HEADER_THEMATIC_PM = "https://images.tokopedia.net/img/android/seller_home/bg_sah_other_curved_header_thematic_pm.png"
-        const val BG_SAH_OTHER_CURVED_HEADER_THEMATIC_OS = "https://images.tokopedia.net/img/android/seller_home/bg_sah_other_curved_header_thematic_os.png"
 
         const val BG_BASE_SAH_OTHER_CURVED_HEADER_THEMATIC_RM = "https://images.tokopedia.net/img/android/seller_home/bg_base_sah_other_curved_header_thematic_rm@3x.png"
         const val BG_BASE_SAH_OTHER_CURVED_HEADER_THEMATIC_PM = "https://images.tokopedia.net/img/android/seller_home/bg_base_sah_other_curved_header_thematic_pm@3x.png"
