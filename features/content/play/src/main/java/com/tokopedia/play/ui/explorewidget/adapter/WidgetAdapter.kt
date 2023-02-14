ce790dbe3ee5af02031b550d2e12a1ca0e977b6a
astidhiyaa <asti.anzaria@tokopedia.com>
2023-02-14 13:54:46 +0800

    fix: revert widgetadapter

--- features/content/play/src/main/java/com/tokopedia/play/ui/explorewidget/adapter/WidgetAdapter.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/explorewidget/adapter/WidgetAdapter.kt
@@ -3,8 +3,6 @@ package com.tokopedia.play.ui.explorewidget.adapter
 import com.tokopedia.adapterdelegate.BaseDiffUtilAdapter
 import com.tokopedia.play.ui.explorewidget.PlayExploreWidgetCoordinator
 import com.tokopedia.play.ui.explorewidget.adapter.delegate.WidgetAdapterDelegate
-import com.tokopedia.play.view.uimodel.TabMenuUiModel
-import com.tokopedia.play.view.uimodel.WidgetItemUiModel
 import com.tokopedia.play.view.uimodel.WidgetUiModel
 
 /**
@@ -18,9 +16,7 @@ class WidgetAdapter(coordinator: PlayExploreWidgetCoordinator) :
     }
 
     override fun areItemsTheSame(oldItem: WidgetUiModel, newItem: WidgetUiModel): Boolean {
-        return if (oldItem is WidgetItemUiModel && newItem is WidgetItemUiModel) oldItem.item == newItem.item
-        else if (oldItem is TabMenuUiModel && newItem is TabMenuUiModel) oldItem.items == newItem.items
-        else oldItem == newItem
+        return oldItem == newItem
     }
 
     override fun areContentsTheSame(oldItem: WidgetUiModel, newItem: WidgetUiModel): Boolean =
