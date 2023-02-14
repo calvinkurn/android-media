77e2687c694e15efc5b90551d4a48dca8ed8be44
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-02-14 16:42:57 +0700

    run ktlint on wishlist_common module

--- features/transaction/wishlist_common/src/main/java/com/tokopedia/wishlistcommon/data/response/DeleteWishlistV2Response.kt
+++ features/transaction/wishlist_common/src/main/java/com/tokopedia/wishlistcommon/data/response/DeleteWishlistV2Response.kt
@@ -3,40 +3,39 @@ package com.tokopedia.wishlistcommon.data.response
 import com.google.gson.annotations.SerializedName
 
 data class DeleteWishlistV2Response(
-        @SerializedName("data")
-        val data: Data = Data()
+    @SerializedName("data")
+    val data: Data = Data()
 ) {
     data class Data(
-            @SerializedName("wishlist_remove_v2")
-            val wishlistRemoveV2: WishlistRemoveV2 = WishlistRemoveV2()
+        @SerializedName("wishlist_remove_v2")
+        val wishlistRemoveV2: WishlistRemoveV2 = WishlistRemoveV2()
     ) {
         data class WishlistRemoveV2(
-                @SerializedName("button")
-                val button: Button = Button(),
+            @SerializedName("button")
+            val button: Button = Button(),
 
-                @SerializedName("success")
-                val success: Boolean = false,
+            @SerializedName("success")
+            val success: Boolean = false,
 
-                @SerializedName("id")
-                val id: String = "",
+            @SerializedName("id")
+            val id: String = "",
 
-                @SerializedName("message")
-                val message: String = "",
+            @SerializedName("message")
+            val message: String = "",
 
-                @SerializedName("toaster_color")
-                val toasterColor: String = ""
+            @SerializedName("toaster_color")
+            val toasterColor: String = ""
         ) {
             data class Button(
-                    @SerializedName("action")
-                    val action: String = "",
+                @SerializedName("action")
+                val action: String = "",
 
-                    @SerializedName("text")
-                    val text: String = "",
+                @SerializedName("text")
+                val text: String = "",
 
-                    @SerializedName("url")
-                    val url: String = ""
+                @SerializedName("url")
+                val url: String = ""
             )
         }
     }
 }
-
