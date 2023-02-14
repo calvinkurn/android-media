a0b61a4c463a3ae1932d077a1560475b8b7593c8
Muhammad Furqan <furqan@tokopedia.com>
2023-02-14 11:05:24 +0700

    [Feed Immersive] Convert RecyclerView to ViewPager2

--- features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/adapter/viewholder/FeedFollowingViewHolder.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/adapter/viewholder/FeedFollowingViewHolder.kt
@@ -1,10 +1,7 @@
 package com.tokopedia.feedplus.presentation.adapter.viewholder
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.feedplus.databinding.ItemFeedFollowingBinding
-import com.tokopedia.feedplus.presentation.fragment.FeedFragment
-import com.tokopedia.feedplus.presentation.model.FeedTabType
 
 /**
  * Created By : Muhammad Furqan on 02/02/23
@@ -13,18 +10,18 @@ class FeedFollowingViewHolder(private val binding: ItemFeedFollowingBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind() {
-        val fragment = FeedFragment.createFeedFragment(FeedTabType.FOLLOWING)
-        val tag = TAG_FOLLOWING
-
-        val activity = binding.root.context as AppCompatActivity
-
-        if (activity.supportFragmentManager.findFragmentByTag(tag) == null) {
-            activity
-                .supportFragmentManager
-                .beginTransaction()
-                .replace(binding.containerFeedFollowingFragment.id, fragment, tag)
-                .commit()
-        }
+//        val fragment = FeedFragment.createFeedFragment(FeedTabType.FOLLOWING)
+//        val tag = TAG_FOLLOWING
+//
+//        val activity = binding.root.context as AppCompatActivity
+//
+//        if (activity.supportFragmentManager.findFragmentByTag(tag) == null) {
+//            activity
+//                .supportFragmentManager
+//                .beginTransaction()
+//                .replace(binding.containerFeedFollowingFragment.id, fragment, tag)
+//                .commit()
+//        }
     }
 
     companion object {
--- features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/adapter/viewholder/FeedForYouViewHolder.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/adapter/viewholder/FeedForYouViewHolder.kt
@@ -1,10 +1,7 @@
 package com.tokopedia.feedplus.presentation.adapter.viewholder
 
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.feedplus.databinding.ItemFeedForYouBinding
-import com.tokopedia.feedplus.presentation.fragment.FeedFragment
-import com.tokopedia.feedplus.presentation.model.FeedTabType
 
 /**
  * Created By : Muhammad Furqan on 02/02/23
@@ -13,17 +10,17 @@ class FeedForYouViewHolder(private val binding: ItemFeedForYouBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind() {
-        val fragment = FeedFragment.createFeedFragment(FeedTabType.FOR_YOU)
-
-        val activity = binding.root.context as AppCompatActivity
-
-        if (activity.supportFragmentManager.findFragmentByTag(TAG_FOR_YOU) == null) {
-            activity
-                .supportFragmentManager
-                .beginTransaction()
-                .replace(binding.containerFeedForYouFragment.id, fragment, TAG_FOR_YOU)
-                .commit()
-        }
+//        val fragment = FeedFragment.createFeedFragment(FeedTabType.FOR_YOU)
+//
+//        val activity = binding.root.context as AppCompatActivity
+//
+//        if (activity.supportFragmentManager.findFragmentByTag(TAG_FOR_YOU) == null) {
+//            activity
+//                .supportFragmentManager
+//                .beginTransaction()
+//                .replace(binding.containerFeedForYouFragment.id, fragment, TAG_FOR_YOU)
+//                .commit()
+//        }
     }
 
     companion object {
