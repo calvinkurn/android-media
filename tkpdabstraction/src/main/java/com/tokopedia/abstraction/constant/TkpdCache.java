95cbeb54e56f2ad91e0157f13d98843d0aec8f34
yoris.prayogo <yoris.prayogo@tokopedia.com>
2023-02-15 17:05:21 +0700

    revert changes

--- tkpdabstraction/src/main/java/com/tokopedia/abstraction/constant/TkpdCache.java
+++ tkpdabstraction/src/main/java/com/tokopedia/abstraction/constant/TkpdCache.java
@@ -4,10 +4,20 @@ package com.tokopedia.abstraction.constant;
  * Created by Nisie on 11/11/15.
  */
 public class TkpdCache {
+    public static final String STATUS_UPDATE = "STATUS_UPDATE";
+    public static final String DEALS_LOCATION = "DEALS_LOCATION";
+    public static final String ADVERTISINGID = "ADVERTISINGID";
     public static final String MAINTENANCE = "MAINTENANCE";
 
     public class Key {
+        public static final String STATUS = "status";
         public static final String URL = "URL";
+        public static final String KEY_DEALS_LOCATION = "KEY_DEALS_LOCATION";
+        public static final String KEY_ADVERTISINGID = "KEY_ADVERTISINGID";
+        public static final String KEY_LOCATION = "KEY_FP_LOCATION";
+        public static final String KEY_LOCATION_LAT = "KEY_FP_LOCATION_LAT";
+        public static final String KEY_LOCATION_LONG = "KEY_FP_LOCATION_LONG";
+        public static final String ETALASE_ADD_PROD = "ETALASE_ADD_PROD";
         public static final String STATUS2 = "STATUS";
         public static final String MESSAGE = "message";
         public static final String KEY_DARK_MODE = "KEY_DARK_MODE";
