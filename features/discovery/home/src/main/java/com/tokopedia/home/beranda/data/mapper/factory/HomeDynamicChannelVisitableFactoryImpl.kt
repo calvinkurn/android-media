ff47c1dc505637e2d268a0479340f9fdad9d1295
firmankagami <firmanda.nugroho@tokopedia.com>
2023-02-15 15:32:29 +0700

    tweak dummy dg bills widget

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/mapper/factory/HomeDynamicChannelVisitableFactoryImpl.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/data/mapper/factory/HomeDynamicChannelVisitableFactoryImpl.kt
@@ -80,6 +80,11 @@ class HomeDynamicChannelVisitableFactoryImpl(
         } else if (homeChannelData?.dynamicHomeChannel?.channels?.isNotEmpty() == true) {
             dynamicChannelList = homeChannelData?.dynamicHomeChannel?.channels as MutableList<DynamicHomeChannel.Channels>
         }
+        dynamicChannelList.add(DynamicHomeChannel.Channels(
+            id = "1",
+            name = "dg_bills",
+            layout = "dg_bills"
+        ))
         dynamicChannelList.forEachIndexed { index, channel ->
             val position = index + startPosition
             setDynamicChannelPromoName(position, channel)
