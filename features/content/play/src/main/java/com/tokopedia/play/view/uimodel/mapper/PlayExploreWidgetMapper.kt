0ec2d528c955f906802307690d6e23c6370b8391
astidhiyaa <asti.anzaria@tokopedia.com>
2023-02-16 10:44:29 +0800

    fix: special character

--- features/content/play/src/main/java/com/tokopedia/play/view/uimodel/mapper/PlayExploreWidgetMapper.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/uimodel/mapper/PlayExploreWidgetMapper.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.play.view.uimodel.mapper
 
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.content.common.model.Content
 import com.tokopedia.content.common.model.WidgetSlot
 import com.tokopedia.play.di.PlayScope
@@ -66,6 +67,7 @@ class PlayExploreWidgetMapper @Inject constructor() {
                 background = PlayWidgetBackgroundUiModel(overlayImageAppLink = "", overlayImageUrl = "", overlayImageWebLink = "", backgroundUrl = "", gradientColors = emptyList()),
                 items = content.items.map {
                     val channelType = PlayWidgetChannelType.getByValue(it.airTime)
+                    val partnerName = MethodChecker.fromHtml(it.partner.name).toString()
                     PlayWidgetChannelUiModel(
                         channelId = it.id,
                         title = it.title,
@@ -74,7 +76,7 @@ class PlayExploreWidgetMapper @Inject constructor() {
                         totalView = PlayWidgetTotalView(totalViewFmt = it.stats.view.formatted, isVisible = channelType != PlayWidgetChannelType.Upcoming),
                         promoType = PlayWidgetPromoType.getByType(it.configurations.promoLabels.firstOrNull()?.type.orEmpty(), it.configurations.promoLabels.firstOrNull()?.text.orEmpty()),
                         reminderType = getReminderType(it.configurations.reminder.isSet),
-                        partner = PlayWidgetPartnerUiModel(it.partner.id, it.partner.name),
+                        partner = PlayWidgetPartnerUiModel(it.partner.id, partnerName),
                         video = PlayWidgetVideoUiModel(it.video.id, it.isLive, it.coverUrl, it.video.streamUrl),
                         channelType = channelType,
                         hasGame = it.configurations.promoLabels.firstOrNull { it.type == GIVEAWAY } != null,
