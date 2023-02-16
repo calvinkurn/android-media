4b84a55e50eca58be38f1298c484c0233ff40518
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-02-16 17:21:19 +0700

    fix(minicart): rename MiniCartTestActivity

--- features/transaction/minicart/src/androidTest/java/com/tokopedia/minicart/MiniCartTestActivity.kt
+++ features/transaction/minicart/src/androidTest/java/com/tokopedia/minicart/MiniCartTestActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.tokopedia.minicart.test.R
 
-class ExampleActivity : AppCompatActivity() {
+class MiniCartTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
