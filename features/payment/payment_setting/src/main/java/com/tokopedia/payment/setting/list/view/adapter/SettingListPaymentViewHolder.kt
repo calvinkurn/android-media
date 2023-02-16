fae4a9d02d2e806d51780339b1409832c94f1b57
stevenwijaya <steven.wijaya@tokopedia.com>
2023-02-16 19:24:44 +0700

    update card info string

--- features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/view/adapter/SettingListPaymentViewHolder.kt
+++ features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/view/adapter/SettingListPaymentViewHolder.kt
@@ -17,12 +17,17 @@ class SettingListPaymentViewHolder(itemView : View?) : AbstractViewHolder<Settin
         itemView.cardBankName.text = element.bank
         itemView.cardNumber.text = itemView.context.getString(
             R.string.payment_label_card_info_setting,
+            getCardType(element.isDebitOnline),
             element.maskedNumber?.takeLast(CARD_NUMBER_SHOWN_DIGIT),
         )
     }
 
+    private fun getCardType(isDebit: Boolean): String = if (isDebit) DEBIT_STRING else CREDIT_STRING
+
     companion object {
         val LAYOUT = R.layout.item_setting_list_payment
         private const val CARD_NUMBER_SHOWN_DIGIT = 4
+        private const val DEBIT_STRING = "debit"
+        private const val CREDIT_STRING = "kredit"
     }
 }
