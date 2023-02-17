4ee1be96e66ea91f1d7adadea73158f6216c8b76
Nabilla <nabilla.sabbaha@tokopedia.com>
2023-02-17 20:26:00 +0700

    feat: clean up code on liveness & kyc_centralized

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/tokoKyc/info/UserIdentificationInfoActivity.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/tokoKyc/info/UserIdentificationInfoActivity.kt
@@ -3,7 +3,6 @@ package com.tokopedia.kyc_centralized.ui.tokoKyc.info
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
-import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform.PARAM_CALL_BACK
 import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform.PARAM_KYC_TYPE
 import com.tokopedia.applink.internal.ApplinkConstInternalUserPlatform.PARAM_PROJECT_ID
@@ -40,7 +39,6 @@ class UserIdentificationInfoActivity : BaseSimpleActivity() {
         if (intent != null && intent.extras != null) {
             isSourceSeller = intent.extras?.getBoolean(KYCConstant.EXTRA_IS_SOURCE_SELLER)?: false
         }
-        toolbar.setTitleTextColor(MethodChecker.getColor(this, com.tokopedia.unifyprinciples.R.color.Unify_N700_96))
     }
 
     override fun getNewFragment(): Fragment? {
