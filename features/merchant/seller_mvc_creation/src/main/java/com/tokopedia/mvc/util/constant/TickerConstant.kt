1667bbbb4d5fde3df2924f11e155c04e5ac3fb93
fajarca <fajar.azhar@tokopedia.com>
2023-02-20 13:59:39 +0700

    Update ticker request params

--- features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/TickerConstant.kt
+++ features/merchant/seller_mvc_creation/src/main/java/com/tokopedia/mvc/util/constant/TickerConstant.kt
@@ -1,6 +1,6 @@
 package com.tokopedia.mvc.util.constant
 
 object TickerConstant {
-    const val REMOTE_TICKER_KEY_VOUCHER_LIST_PAGE = "icarus.seller-coupon-list"
-    const val REMOTE_TICKER_KEY_VOUCHER_CREATION_PAGE = "icarus.seller-coupon-creation"
+    const val REMOTE_TICKER_KEY_VOUCHER_LIST_PAGE = "seller.coupon-list"
+    const val REMOTE_TICKER_KEY_VOUCHER_CREATION_PAGE = "seller.coupon-creation"
 }
