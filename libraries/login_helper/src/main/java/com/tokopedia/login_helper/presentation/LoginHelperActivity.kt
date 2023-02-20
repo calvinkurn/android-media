3160cfcca3ac234e4ea41d954fb12a18b7de3c09
souravsaikia <sourav.saikia@tokopedia.com>
2023-02-20 15:12:09 +0530

    Added Applink support

---
+++ libraries/login_helper/src/main/java/com/tokopedia/login_helper/presentation/LoginHelperActivity.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.login_helper.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginHelperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.tokopedia.login_helper.R.layout.activity_login_helper)
+    }
+}
