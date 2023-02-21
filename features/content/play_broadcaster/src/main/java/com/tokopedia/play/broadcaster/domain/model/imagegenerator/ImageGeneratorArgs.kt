123dd5f945e8d71ab32ababe53d0796e2d20218b
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-02-21 17:33:44 +0800

    `feat`: implement save cover

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/imagegenerator/ImageGeneratorArgs.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/imagegenerator/ImageGeneratorArgs.kt
@@ -1,21 +1,45 @@
 package com.tokopedia.play.broadcaster.domain.model.imagegenerator
 
-data class ImageGeneratorArgs(
-    val backgroundColor: String,
-    val logo: String,
-    val productUrl1: String,
-    val productUrl2: String,
-    val productAmount: String,
-) {
+data class ImageGeneratorArgs(private val imagePreviewUrl: String) {
 
     fun getArg(): List<Map<String, String>> {
         val args = mutableListOf<Map<String, String>>()
-        args.add(mapOf("key" to "background_color", "value" to backgroundColor))
-        args.add(mapOf("key" to "shop_logo", "value" to logo))
-        args.add(mapOf("key" to "product_image_1", "value" to productUrl1))
-        args.add(mapOf("key" to "product_image_2", "value" to productUrl2))
-        args.add(mapOf("key" to "product_amount", "value" to productAmount))
+        args.add(mapOf(KEY to KEY_BACKGROUND_COLOR, VALUE to getBackgroundColor()))
+        args.add(mapOf(KEY to KEY_SHOP_LOGO, VALUE to getLogo()))
+        args.add(mapOf(KEY to KEY_PRODUCT_AMOUNT, VALUE to getProductAmount()))
+        args.add(mapOf(KEY to KEY_PRODUCT_IMAGE_1, VALUE to getProductImage1()))
+        if (getProductAmount() == "2") args.add(mapOf(KEY to KEY_PRODUCT_IMAGE_2, VALUE to getProductImage2()))
         return args
+    }
+
+    private fun getBackgroundColor(): String {
+        return imagePreviewUrl.substringAfter("$KEY_BACKGROUND_COLOR=").substringBefore("&")
+    }
+
+    private fun getLogo(): String {
+        return imagePreviewUrl.substringAfter("$KEY_SHOP_LOGO=").substringBefore("&")
+    }
+
+    private fun getProductAmount(): String {
+        return imagePreviewUrl.substringAfter("$KEY_PRODUCT_AMOUNT=").substringBefore("&")
+    }
+
+    private fun getProductImage1(): String {
+        return imagePreviewUrl.substringAfter("$KEY_PRODUCT_IMAGE_1=").substringBefore("&")
+    }
+
+    private fun getProductImage2(): String {
+        return imagePreviewUrl.substringAfter("$KEY_PRODUCT_IMAGE_2=").substringBefore("&")
+    }
+
+    companion object {
+        private const val KEY = "key"
+        private const val VALUE = "value"
+        private const val KEY_BACKGROUND_COLOR = "background_color"
+        private const val KEY_SHOP_LOGO = "shop_logo"
+        private const val KEY_PRODUCT_IMAGE_1 = "product_image_1"
+        private const val KEY_PRODUCT_IMAGE_2 = "product_image_2"
+        private const val KEY_PRODUCT_AMOUNT = "product_amount"
     }
 
 }
