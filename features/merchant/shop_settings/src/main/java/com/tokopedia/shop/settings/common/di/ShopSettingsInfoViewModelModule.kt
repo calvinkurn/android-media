0a01ee4c104229e1fc2c47b05ee3dc8ee96f8f66
yohann.prananta <yohann.prananta@BA-00019887.local>
2023-02-22 11:20:09 +0700

    revert di changes

--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/common/di/ShopSettingsInfoViewModelModule.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/common/di/ShopSettingsInfoViewModelModule.kt
@@ -1,6 +1,13 @@
 package com.tokopedia.shop.settings.common.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
+import com.tokopedia.shop.settings.basicinfo.view.viewmodel.ShopSettingsInfoViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class ShopSettingsInfoViewModelModule {
--- features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/common/di/ViewModelModule.kt
+++ features/merchant/shop_settings/src/main/java/com/tokopedia/shop/settings/common/di/ViewModelModule.kt
@@ -1,9 +1,28 @@
 package com.tokopedia.shop.settings.common.di
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelKey
+import com.tokopedia.shop.settings.basicinfo.view.viewmodel.ShopEditBasicInfoViewModel
+import com.tokopedia.shop.settings.basicinfo.view.viewmodel.ShopSetOperationalHoursViewModel
+import com.tokopedia.shop.settings.basicinfo.view.viewmodel.ShopSettingsOperationalHoursViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
-abstract class ViewModelModule
+abstract class ViewModelModule {
+
+    @Binds
+    @ShopSettingsScope
+    internal abstract fun bindViewModelFactory(viewModelFactory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ShopSettingsScope
+    @ViewModelKey(ShopEditBasicInfoViewModel::class)
+    internal abstract fun shopEditBasicInfoViewModel(viewModel: ShopEditBasicInfoViewModel): ViewModel
 
     @Binds
     @IntoMap
