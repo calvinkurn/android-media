c654e3dff7c9bee531533bff82e7d045336a1bb4
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-02-23 14:34:27 +0800

    change seller home toaster param name

--- libraries/applink/src/main/java/com/tokopedia/applink/sellerhome/SellerHomeApplinkConst.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/sellerhome/SellerHomeApplinkConst.kt
@@ -6,6 +6,6 @@ object SellerHomeApplinkConst {
     const val TYPE_VOUCHER_CASHBACK = "cashback"
     const val TYPE_VOUCHER_PRODUCT = "product"
     const val TYPE_TOKOPEDIA_PLAY = "play"
-    const val TOASTER_MESSAGE = "toaster_message"
-    const val TOASTER_CTA = "toaster_cta"
+    const val TOASTER_MESSAGE = "toast_msg"
+    const val TOASTER_CTA = "toast_cta"
 }
