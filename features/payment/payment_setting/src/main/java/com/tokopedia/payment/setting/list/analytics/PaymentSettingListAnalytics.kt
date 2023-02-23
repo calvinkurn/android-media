cafaed10157c29e5e6d076b430a95104ebb31a99
irfankhoirul <irfan.muhlishin@tokopedia.com>
2023-02-23 17:21:54 +0700

    add tracker

---
+++ features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/analytics/PaymentSettingListAnalytics.kt
@@ -0,0 +1,96 @@
+package com.tokopedia.payment.setting.list.analytics
+
+import com.tokopedia.payment.setting.list.analytics.PaymentSettingListAnalyticsConstant.Action
+import com.tokopedia.payment.setting.list.analytics.PaymentSettingListAnalyticsConstant.Category
+import com.tokopedia.payment.setting.list.analytics.PaymentSettingListAnalyticsConstant.Event
+import com.tokopedia.payment.setting.list.analytics.PaymentSettingListAnalyticsConstant.Key
+import com.tokopedia.payment.setting.list.analytics.PaymentSettingListAnalyticsConstant.Value
+import com.tokopedia.track.TrackApp
+import com.tokopedia.track.TrackAppUtils
+
+object PaymentSettingListAnalytics {
+
+    private fun sendGeneralEvent(gtmData: MutableMap<String, Any>) {
+        gtmData[Key.BUSINESS_UNIT] = Value.PAYMENT
+        gtmData[Key.CURRENT_SITE] = Value.TOKOPEDIAMARKETPLACE
+
+        TrackApp.getInstance().gtm.sendGeneralEvent(gtmData)
+    }
+
+    fun sendEventViewBannerUserNotYetHaveCobrand() {
+        val map = TrackAppUtils.gtmData(
+            Event.VIEW_ITEM,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.VIEW_BANNER_USER_NOT_YET_HAVE_COBRAND,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41438
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventClickBannerUserNotYetHaveCobrand() {
+        val map = TrackAppUtils.gtmData(
+            Event.SELECT_CONTENT,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.CLICK_BANNER_USER_NOT_YET_HAVE_COBRAND,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41439
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventViewBannerUserCobrandOnProgress() {
+        val map = TrackAppUtils.gtmData(
+            Event.VIEW_ITEM,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.VIEW_BANNER_USER_COBRAND_ON_PROGRESS,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41440
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventClickBannerUserCobrandOnProgress() {
+        val map = TrackAppUtils.gtmData(
+            Event.SELECT_CONTENT,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.CLICK_BANNER_USER_COBRAND_ON_PROGRESS,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41441
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventViewBannerUserCobrandActivate() {
+        val map = TrackAppUtils.gtmData(
+            Event.VIEW_ITEM,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.VIEW_BANNER_USER_COBRAND_ACTIVATE,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41442
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventClickBannerUserCobrandActivate() {
+        val map = TrackAppUtils.gtmData(
+            Event.SELECT_CONTENT,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.CLICK_BANNER_USER_COBRAND_ACTIVATE,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41443
+        sendGeneralEvent(map)
+    }
+
+    fun sendEventClickBannerUserCobrandAdd() {
+        val map = TrackAppUtils.gtmData(
+            Event.SELECT_CONTENT,
+            Category.CREDIT_CARD_SETTING_PAGE_COBRAND,
+            Action.CLICK_BANNER_USER_COBRAND_ADD,
+            ""
+        )
+        map[Key.TRACKER_ID] = Value.TRACKER_ID_41444
+        sendGeneralEvent(map)
+    }
+}
---
+++ features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/analytics/PaymentSettingListAnalyticsConstant.kt
@@ -0,0 +1,41 @@
+package com.tokopedia.payment.setting.list.analytics
+
+object PaymentSettingListAnalyticsConstant {
+
+    object Key {
+        const val TRACKER_ID = "trackerId"
+        const val BUSINESS_UNIT = "businessUnit"
+        const val CURRENT_SITE = "currentSite"
+    }
+
+    object Value {
+        const val TRACKER_ID_41438 = "41438"
+        const val TRACKER_ID_41439 = "41439"
+        const val TRACKER_ID_41440 = "41440"
+        const val TRACKER_ID_41441 = "41441"
+        const val TRACKER_ID_41442 = "41442"
+        const val TRACKER_ID_41443 = "41443"
+        const val TRACKER_ID_41444 = "41444"
+        const val PAYMENT = "payment"
+        const val TOKOPEDIAMARKETPLACE = "tokopediamarketplace"
+    }
+
+    object Event {
+        const val VIEW_ITEM = "view_item"
+        const val SELECT_CONTENT = "select_content"
+    }
+
+    object Category {
+        const val CREDIT_CARD_SETTING_PAGE_COBRAND = "credit card setting page cobrand"
+    }
+
+    object Action {
+        const val VIEW_BANNER_USER_NOT_YET_HAVE_COBRAND = "view banner user not yet have cobrand"
+        const val CLICK_BANNER_USER_NOT_YET_HAVE_COBRAND = "click banner user not yet have cobrand"
+        const val VIEW_BANNER_USER_COBRAND_ON_PROGRESS = "view banner user cobrand on progress"
+        const val CLICK_BANNER_USER_COBRAND_ON_PROGRESS = "click banner user cobrand on progress"
+        const val VIEW_BANNER_USER_COBRAND_ACTIVATE = "view banner user cobrand activate"
+        const val CLICK_BANNER_USER_COBRAND_ACTIVATE = "click banner user cobrand activate"
+        const val CLICK_BANNER_USER_COBRAND_ADD = "click banner user cobrand add"
+    }
+}
