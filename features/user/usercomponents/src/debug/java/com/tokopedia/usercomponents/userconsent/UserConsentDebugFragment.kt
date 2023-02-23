a9292fbb4461810d1461960532bc41e284c0810e
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-02-23 14:38:40 +0700

    rename parameter

--- features/user/usercomponents/src/debug/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugFragment.kt
+++ features/user/usercomponents/src/debug/java/com/tokopedia/usercomponents/userconsent/UserConsentDebugFragment.kt
@@ -41,7 +41,7 @@ open class UserConsentDebugFragment: BaseDaggerFragment() {
         viewBinding?.buttonAction?.setOnClickListener {
             viewBinding?.sampleUserConsent?.submitConsent()
         }
-        viewBinding?.sampleUserConsent?.setOnDetailConsentListener { needConsent, _ ->
+        viewBinding?.sampleUserConsent?.setOnDetailConsentListener { needConsent, consentType ->
             viewBinding?.buttonAction?.showWithCondition(needConsent)
         }
     }
