359e75897359688c48ba9af1c4ffccb73bcec300
yogieputra8 <yogie.putra@tokopedia.com>
2023-02-24 01:53:25 +0700

    Remove unused imports

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/response/GqlShopSortProductResponse.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/response/GqlShopSortProductResponse.kt
@@ -4,8 +4,6 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.shop.common.graphql.data.shopsort.ShopProductSort
 
-// import com.tokopedia.shop.sort.data.source.cloud.model.ShopProductSort
-
 data class GqlShopSortProductResponse(
     @SerializedName("shopSort")
     @Expose
--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/source/cloud/api/ShopApi.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/source/cloud/api/ShopApi.kt
@@ -4,7 +4,6 @@ import com.tokopedia.abstraction.common.data.model.response.PagingList
 import com.tokopedia.network.data.model.response.DataResponse
 import com.tokopedia.shop.common.graphql.data.shopsort.ShopProductSortList
 import com.tokopedia.shop.product.data.source.cloud.model.ShopProduct
-// import com.tokopedia.shop.sort.data.source.cloud.model.ShopProductSortList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.QueryMap
