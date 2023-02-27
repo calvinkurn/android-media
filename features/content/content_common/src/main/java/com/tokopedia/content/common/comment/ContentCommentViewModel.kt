cf3cdabf06e5284d7819abdae74e817c24007852
astidhiyaa <asti.anzaria@tokopedia.com>
2023-02-27 14:40:20 +0800

    fix: comment from PR

--- features/content/content_common/src/main/java/com/tokopedia/content/common/comment/ContentCommentViewModel.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/comment/ContentCommentViewModel.kt
@@ -246,7 +246,7 @@ class ContentCommentViewModel @AssistedInject constructor(
     }
 
     private fun undoComment() {
-        _comments.getAndUpdate {
+        _comments.update {
             val newList = it.list.toMutableList()
             newList.add(_selectedComment.value.second, _selectedComment.value.first)
             it.copy(list = newList)
