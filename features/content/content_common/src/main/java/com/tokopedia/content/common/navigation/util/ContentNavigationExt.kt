9bebce0ebb9951a8b8227ea9b6eb14badc2d6b3f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-02-27 10:15:21 +0700

    address comment: adjust appendDivider function

--- features/content/content_common/src/main/java/com/tokopedia/content/common/navigation/util/ContentNavigationExt.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/navigation/util/ContentNavigationExt.kt
@@ -7,12 +7,12 @@ internal fun StringBuilder.appendQuery(key: String, value: Any) {
     append("$key=$value")
 }
 
-internal fun StringBuilder.appendDivider() {
-    append(DIVIDER)
+internal fun StringBuilder.appendDivider(divider: String = AND_DIVIDER) {
+    append(divider)
 }
 
 internal fun buildAppLink(baseAppLink: String, query: String): String {
     return "$baseAppLink?$query"
 }
 
-private const val DIVIDER = "&"
+private const val AND_DIVIDER = "&"
