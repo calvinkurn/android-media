2cc0d61244c0ba12a6c7132a13ee9d2a64f03019
stevenwijaya <steven.wijaya@tokopedia.com>
2023-02-27 10:48:04 +0700

    code review update

--- features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/domain/GetSettingBannerUseCase.kt
+++ features/payment/payment_setting/src/main/java/com/tokopedia/payment/setting/list/domain/GetSettingBannerUseCase.kt
@@ -6,6 +6,8 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.payment.setting.list.model.GQLSettingBannerResponse
 import com.tokopedia.payment.setting.list.model.SettingBannerModel
 import com.tokopedia.payment.setting.util.GQL_GET_SETTING_BANNER
+import com.tokopedia.usecase.RequestParams
+import java.util.HashMap
 import javax.inject.Inject
 
 @GqlQuery("GetSettingBannerQuery", GQL_GET_SETTING_BANNER)
@@ -20,14 +22,7 @@ class GetSettingBannerUseCase @Inject constructor(
         try {
             this.setTypeClass(GQLSettingBannerResponse::class.java)
             this.setGraphqlQuery(GetSettingBannerQuery.GQL_QUERY)
-            this.setRequestParams(
-                mapOf(
-                    "req" to mapOf<String, Any>(
-                        "cobrand_bank_id" to 1,
-                        "device_type" to "ANDROID",
-                    ),
-                )
-            )
+            this.setRequestParams(getRequestParams())
             this.execute(
                 { result ->
                     onSuccess(result.settingBannerResponse)
@@ -38,5 +33,25 @@ class GetSettingBannerUseCase @Inject constructor(
         } catch (throwable: Throwable) {
             onError(throwable)
         }
+    }
+
+    private fun getRequestParams(): HashMap<String, Any> {
+        val requestParam = RequestParams.create()
+        requestParam.putObject(
+            REQ_PARAM, mapOf<String, Any>(
+                COBRAND_BANK_ID_PARAM to COBRAND_BANK_ID_VALUE_1,
+                DEVICE_TYPE_PARAM to DEVICE_TYPE_ANDROID,
+            ),
+        )
+
+        return requestParam.parameters
+    }
+
+    companion object {
+        const val REQ_PARAM = "req"
+        const val COBRAND_BANK_ID_PARAM = "cobrand_bank_id"
+        const val COBRAND_BANK_ID_VALUE_1 = 1
+        const val DEVICE_TYPE_PARAM = "device_type"
+        const val DEVICE_TYPE_ANDROID = "ANDROID"
     }
 }
