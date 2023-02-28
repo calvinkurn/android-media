a4e79ad3ff9f665e5bee613e8ab5c5c1b0117494
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-02-28 11:57:05 +0700

    feat: add face filter option adapter & viewholder

---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/model/FaceFilterUiModel.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.play.broadcaster.ui.model
+
+/**
+ * Created By : Jonathan Darwin on February 28, 2023
+ */
+data class FaceFilterUiModel(
+    val name: String,
+    val minValue: Double,
+    val maxValue: Double,
+    val defaultValue: Double,
+    val value: Double,
+    val iconUrl: String,
+    val assetLink: String,
+    val isSelected: Boolean,
+    val assetStatus: AssetStatus,
+) {
+
+    val isChecked: Boolean
+        get() = value != defaultValue
+
+    enum class AssetStatus {
+        Available,
+        Downloading,
+        NotDownloaded,
+        Unknown
+    }
+}
---
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/viewholder/FaceFilterOptionViewHolder.kt
@@ -0,0 +1,75 @@
+package com.tokopedia.play.broadcaster.ui.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.kotlin.extensions.view.showWithCondition
+import com.tokopedia.play.broadcaster.databinding.ItemFaceFilterOptionBinding
+import com.tokopedia.play.broadcaster.ui.model.FaceFilterUiModel
+import com.tokopedia.play.broadcaster.R
+
+/**
+ * Created By : Jonathan Darwin on February 28, 2023
+ */
+class FaceFilterOptionViewHolder(
+    private val binding: ItemFaceFilterOptionBinding,
+    private val listener: Listener,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: FaceFilterUiModel) {
+        binding.apply {
+            tvFaceFilter.text = item.name
+            imgFaceFilter.setImageUrl(item.iconUrl)
+            icChecked.showWithCondition(item.isChecked)
+
+            clIcon.setBackgroundResource(
+                if(item.isSelected) R.drawable.bg_face_filter_option
+                else R.drawable.bg_face_filter_option_selected
+            )
+
+            setAssetStatus(item.assetStatus)
+
+            root.setOnClickListener {
+                listener.onClick(item)
+            }
+        }
+    }
+
+    private fun setAssetStatus(assetStatus: FaceFilterUiModel.AssetStatus) {
+        when(assetStatus) {
+            FaceFilterUiModel.AssetStatus.Downloading -> {
+                binding.icLoader.show()
+                binding.icDownload.hide()
+            }
+            FaceFilterUiModel.AssetStatus.NotDownloaded -> {
+                binding.icLoader.hide()
+                binding.icDownload.show()
+            }
+            else -> {
+                binding.icLoader.hide()
+                binding.icDownload.hide()
+            }
+        }
+    }
+
+    interface Listener {
+        fun onClick(item: FaceFilterUiModel)
+    }
+
+    companion object {
+
+        fun create(
+            parent: ViewGroup,
+            listener: Listener,
+        ) = FaceFilterOptionViewHolder(
+            ItemFaceFilterOptionBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false,
+            ),
+            listener
+        )
+    }
+}
