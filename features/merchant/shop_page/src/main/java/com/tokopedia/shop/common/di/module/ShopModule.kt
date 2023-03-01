c5e9dfb012282e4a19a4fa899d4c6d05a65377fa
yogieputra8 <yogie.putra@tokopedia.com>
2023-03-01 13:49:00 +0700

    Remove unused provideShopApi

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/di/module/ShopModule.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/di/module/ShopModule.kt
@@ -8,18 +8,18 @@ import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUse
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.network.NetworkRouter
 import com.tokopedia.network.interceptor.TkpdAuthInterceptor
-import com.tokopedia.shop.common.constant.GQLQueryNamedConstant
+// import com.tokopedia.shop.common.constant.GQLQueryNamedConstant
 import com.tokopedia.shop.common.constant.ShopPageConstant
-import com.tokopedia.shop.common.data.source.cloud.api.ShopApi
+// import com.tokopedia.shop.common.data.source.cloud.api.ShopApi
 import com.tokopedia.shop.common.di.ShopCommonModule
 import com.tokopedia.shop.common.di.ShopPageContext
-import com.tokopedia.shop.common.di.ShopQualifier
-import com.tokopedia.shop.common.di.scope.ShopScope
+// import com.tokopedia.shop.common.di.ShopQualifier
+// import com.tokopedia.shop.common.di.scope.ShopScope
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
-import retrofit2.Retrofit
+// import retrofit2.Retrofit
 
 /**
  * @author sebastianuskh on 4/13/17.
@@ -31,11 +31,11 @@ class ShopModule(val context: Context) {
     @ShopPageContext
     fun provideActivityContext() = context
 
-    @ShopScope
-    @Provides
-    fun provideShopApi(@ShopQualifier retrofit: Retrofit): ShopApi {
-        return retrofit.create(ShopApi::class.java)
-    }
+//    @ShopScope
+//    @Provides
+//    fun provideShopApi(@ShopQualifier retrofit: Retrofit): ShopApi {
+//        return retrofit.create(ShopApi::class.java)
+//    }
 
     @Provides
     fun provideGqlRepository(): GraphqlRepository {
