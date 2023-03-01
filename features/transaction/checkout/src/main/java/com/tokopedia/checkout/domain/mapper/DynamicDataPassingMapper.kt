af9170f6df766023fb0a77aa62b0c43d7dff0f7d
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-03-01 09:32:14 +0700

    clean up code

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/mapper/DynamicDataPassingMapper.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/domain/mapper/DynamicDataPassingMapper.kt
@@ -1,39 +1,12 @@
 package com.tokopedia.checkout.domain.mapper
 
 import com.tokopedia.kotlin.extensions.view.toLongOrZero
-import com.tokopedia.logisticcart.shipping.model.CartItemModel
 import com.tokopedia.logisticcart.shipping.model.ShipmentCartItemModel
-import com.tokopedia.purchase_platform.common.feature.dynamicdatapassing.data.model.UpdateDynamicDataPassingUiModel
 import com.tokopedia.purchase_platform.common.feature.dynamicdatapassing.data.request.DynamicDataPassingParamRequest
 import com.tokopedia.purchase_platform.common.feature.gifting.data.model.AddOnsDataModel
 import com.tokopedia.purchase_platform.common.feature.gifting.domain.model.AddOnResult
 
 object DynamicDataPassingMapper {
-
-    private val dynamicDataParamList: List<DynamicDataPassingParamRequest>? = null
-
-    fun mapResponseToUiModel(dynamicData: String): UpdateDynamicDataPassingUiModel {
-        return UpdateDynamicDataPassingUiModel(dynamicData = dynamicData)
-    }
-
-    fun getCartStringCartId(addOnResult: AddOnResult, listCartItemModel: List<CartItemModel>): String {
-        var cartStringCartId = ""
-        if (addOnResult.addOnData.isNotEmpty()) {
-            addOnResult.addOnData.forEach { addOnData ->
-                run loop@{
-                    listCartItemModel.forEach { cartItemModel ->
-                        val keyProductLevel = cartItemModel.cartString + "-" + cartItemModel.cartId
-                        if (addOnResult.addOnKey.equals(keyProductLevel, ignoreCase = true)) {
-                            cartStringCartId = keyProductLevel
-                            return@loop
-                        }
-                    }
-                }
-            }
-        }
-        return cartStringCartId
-    }
-
     fun getCartString(addOnResult: AddOnResult, shipmentCartItemModel: ShipmentCartItemModel): String {
         var cartString = ""
         if (addOnResult.addOnData.isNotEmpty()) {
@@ -80,7 +53,7 @@ object DynamicDataPassingMapper {
         isOcs: Boolean
     ): DynamicDataPassingParamRequest.AddOn {
         val listAddOnData = arrayListOf<DynamicDataPassingParamRequest.AddOn.AddOnDataParam>()
-        addOnResult.addOnsDataItemModelList.forEach { dataItem ->
+        addOnResult.addOnsDataItemModelList.map { dataItem ->
             val addOnData = DynamicDataPassingParamRequest.AddOn.AddOnDataParam(
                 addOnId = dataItem.addOnId.toLongOrZero(),
                 addOnQty = dataItem.addOnQty.toInt(),
@@ -107,14 +80,6 @@ object DynamicDataPassingMapper {
     const val PAYMENT_LEVEL = "payment_level"
     const val ATTRIBUTE_ADDON_DETAILS = "addon_details"
     const val ATTRIBUTE_DONATION = "donation"
-    const val KEY_IS_ADD_ON = "is_addon"
-    const val KEY_ADD_ON_NOTES_TO = "to"
-    const val KEY_ADD_ON_NOTES_FROM = "from"
-    const val KEY_ADD_ON_NOTES = "notes"
     const val SOURCE_OCS = "OCS"
     const val SOURCE_NORMAL = "normal"
-    const val KEY_LEVEL = "level"
-    const val KEY_PARENT_UNIQUE_ID = "parent_unique_id"
-    const val KEY_UNIQUE_ID = "unique_id"
-    const val KEY_ATTRIBUTE = "attribute"
 }
