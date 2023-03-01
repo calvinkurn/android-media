12b6c35e88073fcdcb9203d65408851428f9cc89
jegul <kenny.hadisaputra@tokopedia.com>
2023-03-01 14:49:47 +0700

    fix: prevent app crash when PlayWidgetCardMediumChannelView failed to inflate

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/widget/medium/adapter/PlayWidgetMediumAdapterDelegate.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/widget/medium/adapter/PlayWidgetMediumAdapterDelegate.kt
@@ -2,6 +2,7 @@ package com.tokopedia.play.widget.ui.widget.medium.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.tokopedia.adapterdelegate.BaseAdapterDelegate
 import com.tokopedia.adapterdelegate.TypedAdapterDelegate
 import com.tokopedia.play.widget.ui.model.PlayWidgetBannerUiModel
@@ -56,7 +57,7 @@ internal class PlayWidgetMediumAdapterDelegate private constructor() {
 
     internal class Channel(
         private val cardChannelListener: PlayWidgetMediumViewHolder.Channel.Listener
-    ) : BaseAdapterDelegate<PlayWidgetChannelUiModel, Any, PlayWidgetMediumViewHolder.Channel>(commonR.layout.view_play_empty) {
+    ) : BaseAdapterDelegate<PlayWidgetChannelUiModel, Any, ViewHolder>(commonR.layout.view_play_empty) {
 
         private val allowedTypes = listOf(
             PlayWidgetChannelType.Live,
@@ -68,16 +69,20 @@ internal class PlayWidgetMediumAdapterDelegate private constructor() {
 
         override fun onBindViewHolder(
             item: PlayWidgetChannelUiModel,
-            holder: PlayWidgetMediumViewHolder.Channel
+            holder: ViewHolder
         ) {
-            holder.bind(item)
+            if (holder is PlayWidgetMediumViewHolder.Channel) holder.bind(item)
         }
 
         override fun onCreateViewHolder(
             parent: ViewGroup,
             basicView: View
-        ): PlayWidgetMediumViewHolder.Channel {
-            return PlayWidgetMediumViewHolder.Channel.create(parent, cardChannelListener)
+        ): ViewHolder {
+            return try {
+                PlayWidgetMediumViewHolder.Channel.create(parent, cardChannelListener)
+            } catch (e: Throwable) {
+                object : ViewHolder(basicView) {}
+            }
         }
 
         override fun isForViewType(
