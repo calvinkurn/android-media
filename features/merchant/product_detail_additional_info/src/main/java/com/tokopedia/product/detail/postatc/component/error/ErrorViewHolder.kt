49b91d838f5ac84454a10fc4ae18f2c4db63516a
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2023-03-02 15:40:03 +0700

    cleanup code

--- features/merchant/product_detail_additional_info/src/main/java/com/tokopedia/product/detail/postatc/component/error/ErrorViewHolder.kt
+++ features/merchant/product_detail_additional_info/src/main/java/com/tokopedia/product/detail/postatc/component/error/ErrorViewHolder.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.product.detail.postatc.component.error
 
-import com.tokopedia.globalerror.GlobalError
 import com.tokopedia.product.detail.databinding.ItemPostAtcErrorBinding
 import com.tokopedia.product.detail.postatc.base.PostAtcListener
 import com.tokopedia.product.detail.postatc.base.PostAtcViewHolder
@@ -11,14 +10,9 @@ class ErrorViewHolder(
 ) : PostAtcViewHolder<ErrorUiModel>(binding.root) {
     override fun bind(element: ErrorUiModel) {
         binding.apply {
-            val errorType = element.errorType
-            if(errorType == GlobalError.SERVER_ERROR){
-
-            } else{
-                postAtcError.setType(element.errorType)
-                postAtcError.setActionClickListener {
-                    listener.refreshPage()
-                }
+            postAtcError.setType(element.errorType)
+            postAtcError.setActionClickListener {
+                listener.refreshPage()
             }
         }
     }
