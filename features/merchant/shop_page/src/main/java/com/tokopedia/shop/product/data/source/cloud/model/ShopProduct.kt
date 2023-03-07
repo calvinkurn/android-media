a378d11a40c0bdab21be7db6c629b36b6bac6dd8
yogieputra8 <yogie.putra@tokopedia.com>
2023-03-07 09:17:45 +0700

    Remove unused class & get_shop_product API Endpoint

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/data/source/cloud/model/ShopProduct.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/product/data/source/cloud/model/ShopProduct.kt
@@ -3,7 +3,6 @@ package com.tokopedia.shop.product.data.source.cloud.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-//
 class ShopProduct {
     @SerializedName("shop_id")
     @Expose
