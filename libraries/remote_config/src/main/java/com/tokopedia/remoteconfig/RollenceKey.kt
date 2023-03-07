620d99706c456a604c039d8c1f610e932ee2a2fe
reivinoktavianus <oktavianusreivin@gmail.com>
2023-03-07 18:32:12 +0700

    feat: Remove bubble chat rollence

--- libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RollenceKey.kt
+++ libraries/remote_config/src/main/java/com/tokopedia/remoteconfig/RollenceKey.kt
@@ -70,9 +70,6 @@ object RollenceKey {
     // Review
     const val CREATE_REVIEW_REVIEW_INSPIRATION_EXPERIMENT_NAME = "review_inspiration"
 
-    // Bubbles
-    const val KEY_ROLLENCE_BUBBLE_CHAT = "seller_bubble_chat_2"
-
     // Logistic
     const val KEY_SHARE_ADDRESS_LOGI = "share_address_logi"
 }
