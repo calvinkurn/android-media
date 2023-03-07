3973c010af2cfaf5125941ecd7e278ec27bcb941
said.faisal <said.faisal@tokopedia.com>
2023-03-07 17:43:55 +0700

    call extension value zero

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/model/HomeClaimCouponDataModel.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/model/HomeClaimCouponDataModel.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.tokopedianow.home.presentation.model
 
 import com.tokopedia.kotlin.extensions.view.EMPTY
+import com.tokopedia.kotlin.extensions.view.ZERO
 
 data class HomeClaimCouponDataModel(
     val appLink: String = String.EMPTY,
@@ -10,7 +11,7 @@ data class HomeClaimCouponDataModel(
     val widgetId: String = String.EMPTY,
     val catalogId: String = String.EMPTY,
     val couponStatus: String = String.EMPTY,
-    val position: Int = 0,
+    val position: Int = Int.ZERO,
     val slugText: String = String.EMPTY,
     val couponName: String = String.EMPTY,
     val warehouseId: String = String.EMPTY
