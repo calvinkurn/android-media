5ac3a6ba302cbd2af94d931c6763f0e85a0624fa
firmankagami <firmanda.nugroho@tokopedia.com>
2023-03-08 18:21:39 +0700

    test: update unit test

--- features/recharge/digital_emoney/src/test/java/com/tokopedia/emoney/viewmodel/EmoneyBalanceViewModelTest.kt
+++ features/recharge/digital_emoney/src/test/java/com/tokopedia/emoney/viewmodel/EmoneyBalanceViewModelTest.kt
@@ -243,7 +243,7 @@ class EmoneyBalanceViewModelTest {
         coEvery { graphqlRepository.response(any(), any()) } returnsMany listOf(gqlResponseGetInquirySuccess, gqlResponseWriteBalanceSuccess)
         every { isoDep.transceive(NFCUtils.hexStringToByteArray("1")) } answers { throw IOException() }
         //when
-        emoneyBalanceViewModel.writeBalanceToCard("", "", 0, mapParam)
+        emoneyBalanceViewModel.writeBalanceToCard("1", "", 0, mapParam)
 
         //then
         assertNotNull(emoneyBalanceViewModel.errorCardMessage.value)
@@ -275,7 +275,7 @@ class EmoneyBalanceViewModelTest {
         coEvery { graphqlRepository.response(any(), any()) } returnsMany listOf(gqlResponseGetInquirySuccess, gqlResponseWriteBalanceSuccess)
         every { isoDep.transceive(NFCUtils.hexStringToByteArray("1")) } answers { null }
         //when
-        emoneyBalanceViewModel.writeBalanceToCard("", "", 0, mapParam)
+        emoneyBalanceViewModel.writeBalanceToCard("1", "", 0, mapParam)
 
         //then
         assertNotNull(emoneyBalanceViewModel.errorCardMessage.value)
@@ -307,7 +307,7 @@ class EmoneyBalanceViewModelTest {
         coEvery { graphqlRepository.response(any(), any()) } returnsMany listOf(gqlResponseGetInquirySuccess, gqlResponseWriteBalanceSuccess)
         every { isoDep.transceive(NFCUtils.hexStringToByteArray("1")) } answers { null }
         //when
-        emoneyBalanceViewModel.writeBalanceToCard("", "", 0, mapParam)
+        emoneyBalanceViewModel.writeBalanceToCard("1", "", 0, mapParam)
 
         //then
         assertNotNull(emoneyBalanceViewModel.errorCardMessage.value)
@@ -316,6 +316,21 @@ class EmoneyBalanceViewModelTest {
 
     @Test
     fun processTagIntent_WriteSaldoEmoneyErrorIO_ISONotConnected() {
+        //given
+        var mapParam = HashMap<String, Any>()
+        mapParam.put(EmoneyBalanceViewModel.TYPE_CARD, "")
+        mapParam.put(EmoneyBalanceViewModel.ID_CARD, "")
+        mapParam.put(EmoneyBalanceViewModel.ATTRIBUTES_CARD, "")
+        //when
+        emoneyBalanceViewModel.writeBalanceToCard("1", "", 0, mapParam)
+
+        //then
+        assertNotNull(emoneyBalanceViewModel.errorCardMessage.value)
+        assertEquals(NfcCardErrorTypeDef.FAILED_READ_CARD, (emoneyBalanceViewModel.errorCardMessage.value as Throwable).message)
+    }
+
+    @Test
+    fun processTagIntent_WriteSaldoEmoneyErrorPayload_PayloadIsEmpty() {
         //given
         var mapParam = HashMap<String, Any>()
         mapParam.put(EmoneyBalanceViewModel.TYPE_CARD, "")
