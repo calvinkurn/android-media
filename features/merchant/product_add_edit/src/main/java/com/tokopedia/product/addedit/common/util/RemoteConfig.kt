68d0530db72ce31fc68b3d4aa874944c5ab9f667
Calvin <calvin.kurniawan@tokopedia.com>
2023-03-09 16:03:12 +0700

    simplify method call

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/RemoteConfig.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/common/util/RemoteConfig.kt
@@ -6,7 +6,6 @@ import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 object RemoteConfig {
     private const val ENABLE_NEW_MEDIA_PICKER_AEP = "android_enable_new_media_picker"
     fun getImagePickerRemoteConfig(context: Context): Boolean {
-        val remoteConfig = FirebaseRemoteConfigImpl(context)
-        return remoteConfig.getBoolean(ENABLE_NEW_MEDIA_PICKER_AEP, false)
+        return FirebaseRemoteConfigImpl(context).getBoolean(ENABLE_NEW_MEDIA_PICKER_AEP, false)
     }
 }
