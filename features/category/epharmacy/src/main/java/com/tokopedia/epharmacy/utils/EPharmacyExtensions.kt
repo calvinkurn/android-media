b076c2cbd508b6959950d76161a194f815ef5616
mohit-toko <mohit.singh@tokopedia.com>
2023-03-15 14:31:33 +0530

    Optimization

--- features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/utils/EPharmacyExtensions.kt
+++ features/category/epharmacy/src/main/java/com/tokopedia/epharmacy/utils/EPharmacyExtensions.kt
@@ -4,15 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import com.tokopedia.applink.RouteManager
-import com.tokopedia.webview.ext.decode
+import com.tokopedia.webview.ext.encode
 import java.lang.Exception
-import java.net.URLEncoder
 
 fun Context.openDocument(url: String) {
     val uri = Uri.parse(url)
     if (uri.path?.endsWith(".pdf") == true && url.startsWith("http")) {
         val intent = Intent(Intent.ACTION_VIEW)
-        intent.setDataAndType(Uri.parse(uri.toString().replace(getGoogleDocEmbeddedUrl(), "").decode()), "application/pdf")
+        intent.setDataAndType(uri, "application/pdf")
         try {
             startActivity(intent)
         } catch (e: Exception) {
@@ -25,14 +24,9 @@ fun Context.openDocument(url: String) {
 }
 
 fun getPdfInWebViewUrl(url: String): String {
-    val encodeUrl = URLEncoder.encode(url, getEncodingType())
-    return URLEncoder.encode("${getGoogleDocEmbeddedUrl()}$encodeUrl", getEncodingType())
+    return "${getGoogleDocEmbeddedUrl()}${url.encode()}".encode()
 }
 
 fun getGoogleDocEmbeddedUrl(): String {
     return "https://docs.google.com/viewer?url="
-}
-
-fun getEncodingType(): String {
-    return "UTF-8"
 }
