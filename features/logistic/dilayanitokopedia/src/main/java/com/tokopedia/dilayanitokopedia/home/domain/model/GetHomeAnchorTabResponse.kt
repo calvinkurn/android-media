b4c36305b7ccf341567fa093f956f154491fd130
Irpan - <irpan@tokopedia.com>
2023-03-17 18:59:57 +0800

    remove annotation Expose

--- features/logistic/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/home/domain/model/GetHomeAnchorTabResponse.kt
+++ features/logistic/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/home/domain/model/GetHomeAnchorTabResponse.kt
@@ -15,46 +15,32 @@ data class GetHomeAnchorTabResponse(
     ) {
         data class Icon(
             @SerializedName("applinks")
-            @Expose
             val applinks: String = "",
             @SerializedName("brandID")
-            @Expose
             val brandID: String = "",
             @SerializedName("buIdentifier")
-            @Expose
             val buIdentifier: String = "",
             @SerializedName("campaignCode")
-            @Expose
             val campaignCode: String = "",
             @SerializedName("categoryPersona")
-            @Expose
             val categoryPersona: String = "",
             @SerializedName("galaxyAttribution")
-            @Expose
             val galaxyAttribution: String = "",
             @SerializedName("feParam")
-            @Expose
             val feParam: String = "",
             @SerializedName("id")
-            @Expose
             val id: Long = 0,
             @SerializedName("imageUrl")
-            @Expose
             val imageUrl: String = "",
             @SerializedName("name")
-            @Expose
             val name: String = "",
             @SerializedName("page")
-            @Expose
             val page: String = "",
             @SerializedName("persona")
-            @Expose
             val persona: String = "",
             @SerializedName("url")
-            @Expose
             val url: String = "",
             @SerializedName("withBackground")
-            @Expose
             val withBackground: Boolean = false
         )
     }
