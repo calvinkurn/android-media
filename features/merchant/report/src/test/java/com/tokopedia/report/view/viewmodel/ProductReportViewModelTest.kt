f080d7753e5561e2cdd8b6b3e3599b17e8134fad
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-03-20 14:25:20 +0700

    fix[product_report_compose] update ut 76%

--- features/merchant/report/src/test/java/com/tokopedia/report/view/viewmodel/ProductReportViewModelTest.kt
+++ features/merchant/report/src/test/java/com/tokopedia/report/view/viewmodel/ProductReportViewModelTest.kt
@@ -1,110 +1,112 @@
 package com.tokopedia.report.view.viewmodel
 
-import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.data.model.GraphqlError
-import com.tokopedia.graphql.data.model.GraphqlResponse
 import com.tokopedia.report.data.model.ProductReportReason
 import com.tokopedia.report.view.fragment.models.ProductReportUiEvent
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
-import io.mockk.coEvery
-import io.mockk.coVerify
 import junit.framework.Assert
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.last
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import java.lang.reflect.Type
 
 class ProductReportViewModelTest : ProductReportViewModelTestFixture() {
     private val dataReasonEmpty = listOf(ProductReportReason())
-    private val dataReasonHaveChildren = listOf(ProductReportReason(
-        children = listOf(ProductReportReason())
-    ))
+    private val dataReasonHaveChildren = listOf(
+        ProductReportReason(
+            children = listOf(ProductReportReason())
+        )
+    )
 
     @Test
     fun `when getReportReason success should return expected result`() = runBlockingTest {
-        val expectedResponse = `get response success`(dataReasonEmpty)
-        val expected = `get success`(expectedResponse)
+        val expectedResponse = `get gql response success`(dataReasonEmpty)
+        val expected = `get response success`(expectedResponse)
         val test = viewModel.uiState.first()
         Assert.assertEquals(expected, test.data)
     }
 
-    private fun `get response success`(data: List<ProductReportReason>) = GraphqlResponse(
-        mapOf(ProductReportReason.Response::class.java to ProductReportReason.Response(data)) as MutableMap<Type, Any>,
-        HashMap<Type, List<GraphqlError>>(),
-        false
-    )
-
-    private fun `get success`(response: GraphqlResponse): List<ProductReportReason> {
-        val expected = response.getSuccessData<ProductReportReason.Response>().data
-        onGetReportReasonSuccess_thenReturn(response)
-        viewModel = ProductReportViewModel(graphqlRepository, CoroutineTestDispatchersProvider)
-        verifyUseCaseCalled()
-        return expected
-    }
-
     @Test
     fun `when getReportReason error should return expected throwable`() = runBlockingTest {
+        // given
         val errorGql = GraphqlError()
-        val errorMessage = "Error getReportReason"
-        errorGql.message = errorMessage
+        errorGql.message = "Error getReportReason"
         onGetReportReasonError_thenReturn(errorGql)
+
+        // when get init reason
         viewModel = ProductReportViewModel(graphqlRepository, CoroutineTestDispatchersProvider)
         verifyUseCaseCalled()
-        val test = viewModel.uiState.first()
-        Assert.assertEquals(errorMessage, test.error.orEmpty())
+
+        runCollectingUiEvent {
+            assertTrue(it.last() is ProductReportUiEvent.OnToasterError)
+        }
     }
 
     @Test
     fun `event on item clicked with children is not empty`() = runBlockingTest {
-        val response = `get response success`(dataReasonHaveChildren)
-        `get success`(response)
-        viewModel.onEvent(ProductReportUiEvent.OnItemClicked(
-            reason = dataReasonHaveChildren.first()
-        ))
+        // given
+        val response = `get gql response success`(dataReasonHaveChildren)
+        `get response success`(response)
 
-        val result = viewModel.uiEvent.last()
-        Assert.assertTrue(result is ProductReportUiEvent.OnScrollTop)
+        // when
+        viewModel.onEvent(
+            ProductReportUiEvent.OnItemClicked(
+                reason = dataReasonHaveChildren.first()
+            )
+        )
+
+        // then
+        runCollectingUiEvent {
+            Assert.assertTrue(it.last() is ProductReportUiEvent.OnScrollTop)
+        }
     }
 
     @Test
     fun `event on item clicked with children is empty`() = runBlockingTest {
-        val response = `get response success`(dataReasonEmpty)
-        `get success`(response)
-        viewModel.onEvent(ProductReportUiEvent.OnItemClicked(
-            reason = dataReasonEmpty.first()
-        ))
-        val result = viewModel.uiEvent.last()
-        Assert.assertTrue(result is ProductReportUiEvent.OnGoToForm)
+        // given
+        val response = `get gql response success`(dataReasonEmpty)
+        `get response success`(response)
+
+        // when
+        viewModel.onEvent(
+            ProductReportUiEvent.OnItemClicked(
+                reason = dataReasonEmpty.first()
+            )
+        )
+
+        // then
+        runCollectingUiEvent {
+            Assert.assertTrue(it.last() is ProductReportUiEvent.OnGoToForm)
+        }
     }
 
     @Test
     fun `event on back pressed`() {
+        // given
+        val response = `get gql response success`(dataReasonEmpty)
+        `get response success`(response)
 
+        // when
+        viewModel.onEvent(ProductReportUiEvent.OnBackPressed)
+
+        // then
+        runCollectingUiEvent {
+            Assert.assertTrue(it.last() is ProductReportUiEvent.OnBackPressed)
+        }
     }
 
     @Test
     fun `event on footer click`() {
+        // given
+        val response = `get gql response success`(dataReasonEmpty)
+        `get response success`(response)
 
-    }
+        // when
+        viewModel.onEvent(ProductReportUiEvent.OnFooterClicked(""))
 
-    private fun onGetReportReasonSuccess_thenReturn(graphqlResponse: GraphqlResponse) {
-        coEvery { graphqlRepository.response(any(), any()) } coAnswers { graphqlResponse }
-    }
-
-    private fun onGetReportReasonError_thenReturn(errorGql: GraphqlError) {
-        val errors = HashMap<Type, List<GraphqlError>>()
-        errors[ProductReportReason.Response::class.java] = listOf(errorGql)
-        coEvery {
-            graphqlRepository.response(
-                any(),
-                any()
-            )
-        } coAnswers { GraphqlResponse(HashMap<Type, Any?>(), errors, false) }
-    }
-
-    private fun verifyUseCaseCalled() {
-        coVerify { graphqlRepository.response(any(), any()) }
+        // then
+        runCollectingUiEvent {
+            Assert.assertTrue(it.last() is ProductReportUiEvent.OnFooterClicked)
+        }
     }
 }
--- features/merchant/report/src/test/java/com/tokopedia/report/view/viewmodel/ProductReportViewModelTestFixture.kt
+++ features/merchant/report/src/test/java/com/tokopedia/report/view/viewmodel/ProductReportViewModelTestFixture.kt
@@ -1,17 +1,29 @@
 package com.tokopedia.report.view.viewmodel
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.graphql.coroutines.data.extensions.getSuccessData
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.model.GraphqlError
+import com.tokopedia.graphql.data.model.GraphqlResponse
+import com.tokopedia.report.data.model.ProductReportReason
+import com.tokopedia.report.view.fragment.models.ProductReportUiEvent
+import com.tokopedia.report.view.fragment.models.ProductReportUiState
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.unit.test.rule.CoroutineTestRule
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import java.lang.reflect.Type
 
 abstract class ProductReportViewModelTestFixture {
 
@@ -21,7 +33,7 @@ abstract class ProductReportViewModelTestFixture {
     protected lateinit var viewModel: ProductReportViewModel
 
     @get:Rule
-    val rule = InstantTaskExecutorRule()
+    val rule = CoroutineTestRule()
 
     @Before
     fun setup() {
@@ -32,5 +44,58 @@ abstract class ProductReportViewModelTestFixture {
     @After
     fun cleanup() {
         Dispatchers.resetMain()
+    }
+
+    protected fun runCollectingUiState(block: (List<ProductReportUiState>) -> Unit) {
+        val scope = CoroutineScope(rule.dispatchers.coroutineDispatcher)
+        val uiEvent = mutableListOf<ProductReportUiState>()
+        val uiEventCollectorJob = scope.launch {
+            viewModel.uiState.toList(uiEvent)
+        }
+        block.invoke(uiEvent)
+        uiEventCollectorJob.cancel()
+    }
+
+    protected fun runCollectingUiEvent(block: (List<ProductReportUiEvent>) -> Unit) {
+        val scope = CoroutineScope(rule.dispatchers.coroutineDispatcher)
+        val uiEvent = mutableListOf<ProductReportUiEvent>()
+        val uiEventCollectorJob = scope.launch {
+            viewModel.uiEvent.toList(uiEvent)
+        }
+        block.invoke(uiEvent)
+        uiEventCollectorJob.cancel()
+    }
+
+    private fun onGetReportReasonSuccess_thenReturn(graphqlResponse: GraphqlResponse) {
+        coEvery { graphqlRepository.response(any(), any()) } coAnswers { graphqlResponse }
+    }
+
+    protected fun `get gql response success`(data: List<ProductReportReason>) = GraphqlResponse(
+        mapOf(ProductReportReason.Response::class.java to ProductReportReason.Response(data)) as MutableMap<Type, Any>,
+        HashMap<Type, List<GraphqlError>>(),
+        false
+    )
+
+    protected fun `get response success`(response: GraphqlResponse): List<ProductReportReason> {
+        val expected = response.getSuccessData<ProductReportReason.Response>().data
+        onGetReportReasonSuccess_thenReturn(response)
+        viewModel = ProductReportViewModel(graphqlRepository, CoroutineTestDispatchersProvider)
+        verifyUseCaseCalled()
+        return expected
+    }
+
+    protected fun onGetReportReasonError_thenReturn(errorGql: GraphqlError) {
+        val errors = HashMap<Type, List<GraphqlError>>()
+        errors[ProductReportReason.Response::class.java] = listOf(errorGql)
+        coEvery {
+            graphqlRepository.response(
+                any(),
+                any()
+            )
+        } coAnswers { GraphqlResponse(HashMap<Type, Any?>(), errors, false) }
+    }
+
+    protected fun verifyUseCaseCalled() {
+        coVerify { graphqlRepository.response(any(), any()) }
     }
 }
