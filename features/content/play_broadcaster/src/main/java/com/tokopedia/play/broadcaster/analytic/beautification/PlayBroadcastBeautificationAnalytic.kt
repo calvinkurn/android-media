4eb8cd9791982e2f1de3a197a55df1efec5c3eb0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-03-21 16:05:52 +0700

    feat: fix row 10 analytic & remove comment

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/beautification/PlayBroadcastBeautificationAnalytic.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/beautification/PlayBroadcastBeautificationAnalytic.kt
@@ -29,7 +29,7 @@ interface PlayBroadcastBeautificationAnalytic {
     fun openScreenBeautificationBottomSheet()
 
     /** Row 6 */
-    fun clickCustomFace(account: ContentAccountUiModel, page: Page, customFace: String) /** TODO: which one is shaping/buffing/toning? */
+    fun clickCustomFace(account: ContentAccountUiModel, page: Page, customFace: String)
 
     /** Row 7 */
     fun clickNoneCustomFace(account: ContentAccountUiModel, page: Page)
@@ -41,7 +41,7 @@ interface PlayBroadcastBeautificationAnalytic {
     fun clickBeautyFilterReset(account: ContentAccountUiModel, page: Page)
 
     /** Row 10 */
-    fun clickSliderBeautyFilter(account: ContentAccountUiModel, page: Page, tab: Tab, customFace: String) /** TODO: if user choose makeup, what is the value for shaping/buffing/toning? */
+    fun clickSliderBeautyFilter(account: ContentAccountUiModel, page: Page, tab: Tab, filterName: String)
 
     /** Row 11 */
     fun viewResetFilterPopup(account: ContentAccountUiModel, page: Page, tab: Tab)
