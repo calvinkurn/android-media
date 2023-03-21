aa55536ee625850421402d882794c32ef1968774
varun0922 <varun.sharma@tokopedia.com>
2023-03-21 17:40:50 +0530

    making object first check for creating sharedpref

--- libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/common/CMNotificationUtils.kt
+++ libraries/cm_push_notifications/src/main/java/com/tokopedia/notifications/common/CMNotificationUtils.kt
@@ -339,10 +339,12 @@ object CMNotificationUtils {
         return gson.fromJson(pageReminderPromptFreqRecordStr, ReminderPromptAppDataObj::class.java)
     }
 
-    private fun updateReminderPromptFreqRecord(context: Context, pageName: String): Boolean{
+    private fun updateReminderPromptFreqRecord(context: Context,
+                                               pageName: String,
+                                               reminderPromptFreqRemoteConfObj: ReminderPromptFreqRemoteConfObj?): Boolean{
         val reminderPromptAppDataObj = readReminderPromptFreqRecord(context,
             pageName+SUFFIX_REMINDER_PROMPT_PAGE_DATA)
-        val reminderPromptFreqRemoteConfObj = getRemoteConfigReminderPromptFreqCapping(context, pageName)
+//        val reminderPromptFreqRemoteConfObj = getRemoteConfigReminderPromptFreqCapping(context, pageName)
         //check if reset
         if(reminderPromptFreqRemoteConfObj?.reset == true){
             deleteReminderPromptFreqRecord(context, pageName+ SUFFIX_REMINDER_PROMPT_PAGE_DATA)
@@ -382,11 +384,14 @@ object CMNotificationUtils {
 
     fun validateReminderPromptDisplay(context: Context, pageName: String): Boolean{
         //create record if does not exist
+        val reminderPromptFreqRemoteConfObj =
+            getRemoteConfigReminderPromptFreqCapping(context, pageName)
+                ?: return false
         initReminderPromptSharedPref(context)
         if(sharedPreference?.contains(pageName+ SUFFIX_REMINDER_PROMPT_PAGE_DATA) == false){
             createReminderPromptFreqRecord(context, pageName)
         }
-        return updateReminderPromptFreqRecord(context, pageName)
+        return updateReminderPromptFreqRecord(context, pageName, reminderPromptFreqRemoteConfObj)
     }
 
     private fun initReminderPromptSharedPref(context: Context){
