3da4566ff1ccdbd891755f7dbeb6f9077b13a415
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-03-24 11:02:21 +0700

    feat: handle edge case & emit progress as integer

--- features/content/createpost_common/src/main/java/com/tokopedia/createpost/common/view/util/PostUpdateProgressManager.kt
+++ features/content/createpost_common/src/main/java/com/tokopedia/createpost/common/view/util/PostUpdateProgressManager.kt
@@ -73,7 +73,12 @@ abstract class PostUpdateProgressManager(
     }
 
     private fun sendBroadcast(currentProgress: Int, imageUrl: String) {
-        val progressPercentage = currentProgress / maxCount.toDouble() * PERCENTAGE
+
+        val progressPercentage = if(maxCount == 0) {
+            0
+        } else {
+            (currentProgress / maxCount.toDouble() * PERCENTAGE).toInt()
+        }
 
         val intent = Intent(UPLOAD_POST_NEW)
         intent.putExtra(UPLOAD_POST_SUCCESS_NEW, true)
