7cd7543df4bec742f9f77f32b436ea9bf212d4c3
rezagama <rezagamahidayat@gmail.com>
2023-03-27 11:33:50 +0700

    Increase category affiliate UT coverage

--- features/tokopedianow/src/test/java/com/tokopedia/tokopedianow/searchcategory/CategoryAffiliateTest.kt
+++ features/tokopedianow/src/test/java/com/tokopedia/tokopedianow/searchcategory/CategoryAffiliateTest.kt
@@ -63,6 +63,13 @@ class CategoryAffiliateTest: CategoryTestFixtures(), Callback {
     }
 
     @Test
+    fun `when check atc affiliate cookie error should do nothing`() {
+        `given check atc affiliate cookie error`()
+
+        addToCartTestHelper.`test add to cart success`()
+    }
+
+    @Test
     fun `given init affiliate cookie error when add to cart should not nothing`() {
         `given init affiliate cookie error`()
 
@@ -82,6 +89,10 @@ class CategoryAffiliateTest: CategoryTestFixtures(), Callback {
 
     private fun `given init affiliate cookie error`() {
         coEvery { affiliateService.initAffiliateCookie(anyString(), anyString()) } throws NullPointerException()
+    }
+
+    private fun `given check atc affiliate cookie error`() {
+        coEvery { affiliateService.checkAtcAffiliateCookie(any()) } throws NullPointerException()
     }
 
     private fun `when create affiliate link`(url: String) {
