e98575c80a0b5072c4d6299d99dc041515518881
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-03-27 13:44:32 +0700

    chore[bottom_sheet_edu] remove isReroute,eduLink in shopMultilocation, move to bottomSheetEdu

--- features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/pdplayout/ProductMultilocation.kt
+++ features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/pdplayout/ProductMultilocation.kt
@@ -4,9 +4,6 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class ProductMultilocation(
-    @SerializedName("isReroute")
-    @Expose
-    val isReroute: Boolean = false,
     @SerializedName("cityName")
     @Expose
     val cityName: String = ""
