8f7521499f6fa3dc8530dbf1cd92b3e798fdf2b6
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-03-27 16:31:14 +0700

    address comment: migrate from GraphqlUseCase to CoroutineUseCase

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/usecase/GetConfigurationUseCase.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/usecase/GetConfigurationUseCase.kt
@@ -2,15 +2,15 @@ package com.tokopedia.play.broadcaster.domain.usecase
 
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.content.common.types.ContentCommonUserType.TYPE_SHOP
 import com.tokopedia.content.common.types.ContentCommonUserType.TYPE_USER
 import com.tokopedia.content.common.types.ContentCommonUserType.VALUE_TYPE_ID_SHOP
 import com.tokopedia.content.common.types.ContentCommonUserType.VALUE_TYPE_ID_USER
-import com.tokopedia.gql_query_annotation.GqlQuery
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.coroutines.data.extensions.request
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.graphql.data.model.CacheType
-import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import com.tokopedia.play.broadcaster.domain.model.GetBroadcasterAuthorConfigResponse
 import com.tokopedia.play_common.util.device.PlayDeviceSpec
 import javax.inject.Inject
@@ -18,23 +18,19 @@ import javax.inject.Inject
 /**
  * Created by mzennis on 14/06/20.
  */
-@GqlQuery(GetConfigurationUseCase.QUERY_NAME, GetConfigurationUseCase.QUERY_BROADCASTER_GET_AUTHOR_CONFIG)
 class GetConfigurationUseCase @Inject constructor(
-    graphqlRepository: GraphqlRepository,
+    @ApplicationContext private val repository: GraphqlRepository,
+    dispatchers: CoroutineDispatchers,
     private val deviceSpec: PlayDeviceSpec,
     private val gson: Gson,
-) : GraphqlUseCase<GetBroadcasterAuthorConfigResponse>(graphqlRepository) {
+) : CoroutineUseCase<GetConfigurationUseCase.RequestParam, GetBroadcasterAuthorConfigResponse>(dispatchers.io) {
 
-    init {
-        setGraphqlQuery(GetConfigurationUseCaseQuery())
-        setCacheStrategy(GraphqlCacheStrategy.Builder(CacheType.ALWAYS_CLOUD).build())
-        setTypeClass(GetBroadcasterAuthorConfigResponse::class.java)
-    }
+    override fun graphqlQuery(): String = QUERY_BROADCASTER_GET_AUTHOR_CONFIG
 
-    private fun createRequestParams(authorId: String, authorType: String) {
-        val request = mapOf(
-            PARAMS_AUTHOR_ID to authorId.toLong(),
-            PARAMS_AUTHOR_TYPE to when (authorType) {
+    override suspend fun execute(params: RequestParam): GetBroadcasterAuthorConfigResponse {
+        val param = mapOf(
+            PARAMS_AUTHOR_ID to params.authorId.toLong(),
+            PARAMS_AUTHOR_TYPE to when (params.authorType) {
                 TYPE_USER -> VALUE_TYPE_ID_USER
                 TYPE_SHOP -> VALUE_TYPE_ID_SHOP
                 else -> 0
@@ -42,12 +38,8 @@ class GetConfigurationUseCase @Inject constructor(
             PARAMS_WITH_CHANNEL_STATE to VALUE_WITH_CHANNEL_STATE,
             PARAMS_DEVICE_SPECS to generateDeviceSpecJson()
         )
-        setRequestParams(request)
-    }
 
-    suspend fun execute(authorId: String, authorType: String): GetBroadcasterAuthorConfigResponse {
-        this.createRequestParams(authorId, authorType)
-        return executeOnBackground()
+        return repository.request(graphqlQuery(), param)
     }
 
     private fun generateDeviceSpecJson(): String {
@@ -63,6 +55,11 @@ class GetConfigurationUseCase @Inject constructor(
 
         return gson.toJson(deviceSpec)
     }
+
+    data class RequestParam(
+        val authorId: String,
+        val authorType: String,
+    )
 
     data class DeviceSpecModel(
         @SerializedName("device_type")
@@ -90,10 +87,10 @@ class GetConfigurationUseCase @Inject constructor(
 
         private const val VALUE_WITH_CHANNEL_STATE = true
 
-        const val QUERY_NAME = "GetConfigurationUseCaseQuery"
+        private const val QUERY_NAME = "GetConfigurationUseCaseQuery"
         /** TODO: dont forget to add PARAMS_DEVICE_SPECS in this QUERY PARAM */
         /** TODO: dont forget to add beautificationConfig in this QUERY BODY */
-        const val QUERY_BROADCASTER_GET_AUTHOR_CONFIG = """
+        private const val QUERY_BROADCASTER_GET_AUTHOR_CONFIG = """
             query BroadcasterGetAuthorConfig(
                 ${"$$PARAMS_AUTHOR_ID"}: Int64!, 
                 ${"$$PARAMS_AUTHOR_TYPE"}: Int!, 
