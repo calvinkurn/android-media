945a6136f6313277908b158da3b5d9b0c3ed1d56
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2023-03-28 08:52:16 +0700

    Remove comment code

--- features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/inbox/InboxContactUsFragment.kt
+++ features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/inbox/InboxContactUsFragment.kt
@@ -310,9 +310,6 @@ class InboxContactUsFragment :
                 }
             })
             adapter = mAdapter
-            /*attachPaging(this, pagingConfig) { _, _ ->
-                viewModel.getTicketItems()
-            }*/
             addOnScrollListener(rvOnScrollListener)
         }
     }
