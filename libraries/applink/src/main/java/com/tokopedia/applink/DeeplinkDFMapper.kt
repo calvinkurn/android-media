ac6e04ac092b79cc6f45f5f43b893fd0a50dcd63
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-03-28 09:28:57 +0800

    fix df seller persona

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFMapper.kt
@@ -230,7 +230,7 @@ object DeeplinkDFMapper : CoroutineScope {
     const val DF_ALPHA_TESTING = "df_alpha_testing"
     const val DF_DIGITAL = "df_digital"
     const val DF_TOKOCHAT = "df_comm_tokochat"
-    const val DF_SELLER_PERSONA = "df_seller_persona"
+    const val DF_SELLER_PERSONA = "seller_persona"
 
     const val SHARED_PREF_TRACK_DF_USAGE = "pref_track_df_usage"
     var dfUsageList = mutableListOf<String>()
