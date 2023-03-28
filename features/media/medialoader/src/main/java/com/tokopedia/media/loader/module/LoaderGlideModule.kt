9da8b4b0f0f711a6d01e4215a9f96ddd01221115
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-03-28 15:55:38 +0700

    [Review] Adjust after pull release

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/module/LoaderGlideModule.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/module/LoaderGlideModule.kt
@@ -1,8 +1,8 @@
 package com.tokopedia.media.loader.module
 
 import android.content.Context
-import android.util.Log
 import android.graphics.Bitmap
+import android.util.Log
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
@@ -10,17 +10,20 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
 import com.bumptech.glide.module.AppGlideModule
 import com.tokopedia.media.loader.isImageLoaderV2
+import com.tokopedia.media.loader.modelloader.M3U8ModelLoaderFactory
 import com.tokopedia.media.loader.utils.RemoteConfig
 import java.io.InputStream
 
 @GlideModule
-class LoaderGlideModule: AppGlideModule() {
+class LoaderGlideModule : AppGlideModule() {
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {
-        builder.setDiskCache(DiskLruCacheFactory(
-            Glide.getPhotoCacheDir(context)?.absolutePath,
-            (LIMIT_CACHE_SIZE_IN_MB * SIZE_IN_MB * SIZE_IN_MB).toLong()
-        ))
+        builder.setDiskCache(
+            DiskLruCacheFactory(
+                Glide.getPhotoCacheDir(context)?.absolutePath,
+                (LIMIT_CACHE_SIZE_IN_MB * SIZE_IN_MB * SIZE_IN_MB).toLong()
+            )
+        )
         builder.setLogLevel(Log.VERBOSE)
         super.applyOptions(context, builder)
     }
@@ -52,5 +55,4 @@ class LoaderGlideModule: AppGlideModule() {
         private const val LIMIT_CACHE_SIZE_IN_MB = 50
         private const val SIZE_IN_MB = 1024
     }
-
 }
