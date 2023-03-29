ec50a20c968926ec6245c38ea083124d554d4e26
astidhiyaa <asti.anzaria@tokopedia.com>
2023-03-29 10:55:02 +0700

    fix: add ui test - cassava

---
+++ features/content/play/src/androidTest/java/com/tokopedia/play/uitest/comment/PlayCommentVodAnalyticTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.play.uitest.comment
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.tokopedia.test.application.annotations.CassavaTest
+import org.junit.runner.RunWith
+
+/**
+ * @author by astidhiyaa on 29/03/23
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+@CassavaTest
+class PlayCommentVodAnalyticTest {
+}
