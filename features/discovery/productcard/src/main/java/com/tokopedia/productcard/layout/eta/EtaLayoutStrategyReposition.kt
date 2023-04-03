46296585fffb54233ac7bff02c36f221c48622f4
Muhammad Fiqri Muthohar <crescenthikari@users.noreply.github.com>
2023-04-03 10:04:57 +0700

    Update features/discovery/productcard/src/main/java/com/tokopedia/productcard/layout/eta/EtaLayoutStrategyReposition.kt
    Co-authored-by: sonoda-bots[bot] <113779905+sonoda-bots[bot]@users.noreply.github.com>

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/layout/eta/EtaLayoutStrategyReposition.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/layout/eta/EtaLayoutStrategyReposition.kt
@@ -1,8 +1,6 @@
 package com.tokopedia.productcard.layout.eta
 
 import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import com.tokopedia.productcard.ProductCardModel
 import com.tokopedia.productcard.R
 import com.tokopedia.productcard.utils.applyConstraintSet
