3dc3b5c1052d97dec7126207afaf55ee9d2a1f08
victor <victor@tokopedia.com>
2023-04-03 09:04:01 +0700

    convert UpdateAndReloadCartUseCase to CoroutineUseCase

--- features/transaction/cart/src/main/java/com/tokopedia/cart/domain/usecase/UpdateAndReloadCartUseCase.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/domain/usecase/UpdateAndReloadCartUseCase.kt
@@ -1,22 +1,19 @@
 package com.tokopedia.cart.domain.usecase
 
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.cart.data.model.request.UpdateCartWrapperRequest
 import com.tokopedia.cart.domain.model.updatecart.UpdateAndReloadCartListData
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 
 class UpdateAndReloadCartUseCase @Inject constructor(
     private val updateCartUseCase: UpdateCartUseCase,
-) : UseCase<UpdateAndReloadCartListData>() {
+    dispatcher: CoroutineDispatchers
+) : CoroutineUseCase<UpdateCartWrapperRequest, UpdateAndReloadCartListData>(dispatcher.io) {
+    override fun graphqlQuery(): String = ""
 
-    private var params: UpdateCartWrapperRequest = UpdateCartWrapperRequest()
-    
-    fun setParams(request: UpdateCartWrapperRequest): UpdateAndReloadCartUseCase {
-        this.params = request
-        return this
-    }
-    
-    override suspend fun executeOnBackground(): UpdateAndReloadCartListData {
+    override suspend fun execute(params: UpdateCartWrapperRequest): UpdateAndReloadCartListData {
         val updateAndReloadCartListData = UpdateAndReloadCartListData()
         val updateCartData = updateCartUseCase.setParams(params).executeOnBackground()
         updateAndReloadCartListData.updateCartData = updateCartData
