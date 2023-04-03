c80c9ac5fc63f135c9fa0a9a026d5fa858e85601
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-04-03 14:19:14 +0700

    fix: remove old preset first before applying the new preset (2)

--- libraries/effect/src/main/java/com/tokopedia/effect/model/SavedComposeNode.kt
+++ libraries/effect/src/main/java/com/tokopedia/effect/model/SavedComposeNode.kt
@@ -6,19 +6,17 @@ package com.tokopedia.effect.model
 data class SavedComposeNode(
     val faceFilterComposeNodeApplied: Boolean,
     val faceFilters: List<FaceFilter>,
-    val presetComposeNodeApplied: Boolean,
     val preset: Preset
 ) {
     fun clearFaceFilter(): SavedComposeNode = copy(faceFilterComposeNodeApplied = false, faceFilters = emptyList())
 
-    fun clearPreset(): SavedComposeNode = copy(presetComposeNodeApplied = false, preset = Preset.Unknown)
+    fun clearPreset(): SavedComposeNode = copy(preset = Preset.Unknown)
 
     companion object {
         val Empty: SavedComposeNode
             get() = SavedComposeNode(
                 faceFilterComposeNodeApplied = false,
                 faceFilters = emptyList(),
-                presetComposeNodeApplied = false,
                 preset = Preset.Unknown
             )
     }
