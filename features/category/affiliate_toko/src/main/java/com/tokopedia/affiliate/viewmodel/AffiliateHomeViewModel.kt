7735684f2a6c5a9b3909ba28f49b0c4013b5fa00
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-04-04 20:28:30 +0530

    fixed default count

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/viewmodel/AffiliateHomeViewModel.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/viewmodel/AffiliateHomeViewModel.kt
@@ -88,7 +88,7 @@ class AffiliateHomeViewModel @Inject constructor(
     private var itemTypes = emptyList<ItemTypesItem>()
     private var sseJob: Job? = null
 
-    private val _unreadNotificationCount = MutableLiveData<Int>(5)
+    private val _unreadNotificationCount = MutableLiveData<Int>()
 
     fun getUnreadNotificationCount(): LiveData<Int> = _unreadNotificationCount
 
