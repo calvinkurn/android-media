97201ca1f4ec8252f8f7afe8d8dba91ace705413
fajarca <fajar.azhar@tokopedia.com>
2023-04-05 08:11:35 +0700

    Detect pdf file

--- features/merchant/seller_shop_nib/src/main/java/com/tokopedia/shop_nib/presentation/submission/NibSubmissionFragment.kt
+++ features/merchant/seller_shop_nib/src/main/java/com/tokopedia/shop_nib/presentation/submission/NibSubmissionFragment.kt
@@ -3,6 +3,7 @@ package com.tokopedia.shop_nib.presentation.submission
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,18 @@ import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.visible
+import com.tokopedia.media.loader.loadImage
 import com.tokopedia.shop_nib.databinding.SsnFragmentNibSubmissionBinding
 import com.tokopedia.shop_nib.di.component.DaggerShopNibComponent
 import com.tokopedia.shop_nib.util.FileHelper
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 import javax.inject.Inject
-
+import com.tokopedia.shop_nib.R
+import com.tokopedia.shop_nib.util.extension.toMb
+import java.io.File
 
 class NibSubmissionFragment : BaseDaggerFragment() {
 
@@ -70,21 +76,20 @@ class NibSubmissionFragment : BaseDaggerFragment() {
     }
 
     private fun setupView() {
-        binding?.layoutFilePicker?.setOnClickListener {
+        binding?.layoutFilePickerDefault?.setOnClickListener {
             showFilePicker()
         }
+        binding?.iconClose?.setOnClickListener { removeSelectedFile() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE_SELECT_FILE) {
             if (resultCode == Activity.RESULT_OK && data != null) {
-                val uri = data.data
-                val file = fileHelper.getFileFromUri( uri ?: return)
-                val fileSize = fileHelper.getFileSizeInBytes(uri)
-                print(file)
+                showSelectedFile(data)
             }
         }
     }
+
 
     private fun showFilePicker() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
@@ -98,4 +103,40 @@ class NibSubmissionFragment : BaseDaggerFragment() {
         } catch (_: ActivityNotFoundException) {}
 
     }
+
+    private fun showSelectedFile(intent: Intent) {
+        val fileUri = intent.data ?: return
+
+        val fileName = fileHelper.getFileName(fileUri)
+        val fileExtension = fileHelper.getFileExtension(fileUri)
+
+        val fileSizeInMb = fileHelper.getFileSizeInBytes(fileUri).toMb()
+
+        val isPdf = fileExtension == "pdf"
+
+        binding?.run {
+            if (isPdf) {
+                val bitmap = fileHelper.generatePdfThumbnail(fileUri)
+                imgFile.loadImage(bitmap)
+            } else {
+                imgFile.loadImage(fileUri)
+            }
+
+            layoutFilePickerDefault.gone()
+            layoutFilePickerSelected.visible()
+            tpgFileName.text = fileName
+
+            val roundedFileSizeInMb = String.format("%.2f", fileSizeInMb)
+            tpgFileSize.text = context?.getString(R.string.ssn_placeholder_file_size, roundedFileSizeInMb)
+        }
+    }
+
+    private fun removeSelectedFile() {
+        binding?.run {
+            layoutFilePickerDefault.visible()
+            layoutFilePickerSelected.gone()
+        }
+    }
+
+
 }
--- features/merchant/seller_shop_nib/src/main/java/com/tokopedia/shop_nib/util/FileHelper.kt
+++ features/merchant/seller_shop_nib/src/main/java/com/tokopedia/shop_nib/util/FileHelper.kt
@@ -1,8 +1,14 @@
 package com.tokopedia.shop_nib.util
 
 import android.content.Context
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.graphics.pdf.PdfRenderer
 import android.net.Uri
+import android.os.ParcelFileDescriptor
 import android.provider.MediaStore
+import android.provider.OpenableColumns
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import java.io.File
 import java.io.IOException
@@ -10,11 +16,6 @@ import java.io.InputStream
 import javax.inject.Inject
 
 class FileHelper @Inject constructor(@ApplicationContext private val context: Context) {
-
-    fun getFileFromUri(uri: Uri): File? {
-        val filePath = getRealPathFromUri(context, uri) ?: return null
-        return File(filePath)
-    }
 
     fun getFileSizeInBytes(uri: Uri): Long {
         val contentResolver = context.contentResolver
@@ -34,6 +35,55 @@ class FileHelper @Inject constructor(@ApplicationContext private val context: Co
         }
 
         return fileSize
+    }
+
+    fun getFileName(uri: Uri): String {
+        val cursor: Cursor? = context.contentResolver.query(
+            uri, null, null, null, null, null
+        )
+
+        return try {
+            if (cursor != null && cursor.moveToFirst()) {
+                val displayNameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                cursor.getString(displayNameIndex)
+            } else {
+                ""
+            }
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+    fun generatePdfThumbnail(uri: Uri): Bitmap? {
+        val contentResolver = context.contentResolver
+        var renderer: PdfRenderer? = null
+        var bitmap: Bitmap? = null
+        try {
+            val fileDescriptor = contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor
+            val parcelFileDescriptor = ParcelFileDescriptor.dup(fileDescriptor)
+            renderer = PdfRenderer(parcelFileDescriptor)
+            if (renderer.pageCount > 0) {
+                val page = renderer.openPage(0)
+                val width = 80
+                val height = 80
+                bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                val matrix = Matrix()
+                val scale = width.toFloat() / page.width
+                matrix.setScale(scale, scale)
+                page.render(bitmap, null, matrix, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                page.close()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            renderer?.close()
+        }
+        return bitmap
+    }
+
+    fun getFileExtension(uri: Uri): String {
+        val fileName = getFileName(uri)
+        return fileName.substringAfterLast(".")
     }
 
     private fun getRealPathFromUri(context: Context, uri: Uri): String? {
