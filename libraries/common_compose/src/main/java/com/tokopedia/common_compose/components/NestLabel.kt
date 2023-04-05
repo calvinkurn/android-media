b247f6810d68db2aeabe0b268c87303601e0fa71
fajarca <fajar.azhar@tokopedia.com>
2023-04-05 13:44:56 +0700

    Add variant and preview

--- libraries/common_compose/src/main/java/com/tokopedia/common_compose/components/NestLabel.kt
+++ libraries/common_compose/src/main/java/com/tokopedia/common_compose/components/NestLabel.kt
@@ -1,11 +1,19 @@
 package com.tokopedia.common_compose.components
 
+import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,18 +31,24 @@ fun NestLabel(
         NestLabelType.HIGHLIGHT_LIGHT_ORANGE -> NestTheme.colors.YN._100
         NestLabelType.HIGHLIGHT_LIGHT_GREY -> NestTheme.colors.NN._100
         NestLabelType.HIGHLIGHT_LIGHT_RED -> NestTheme.colors.RN._100
+        NestLabelType.HIGHLIGHT_LIGHT_BLUE -> NestTheme.colors.BN._100
+        NestLabelType.HIGHLIGHT_LIGHT_TEAL -> NestTheme.colors.TN._100
+        NestLabelType.HIGHLIGHT_LIGHT_IMAGE_LABEL -> if (isSystemInDarkTheme()) ImageLabelColorDark else ImageLabelColorLight
     }
 
     val textColor = when (nestLabelType) {
-        NestLabelType.HIGHLIGHT_LIGHT_GREEN -> NestTheme.colors.GN._500
-        NestLabelType.HIGHLIGHT_LIGHT_ORANGE -> NestTheme.colors.YN._500
-        NestLabelType.HIGHLIGHT_LIGHT_GREY -> NestTheme.colors.NN._600
-        NestLabelType.HIGHLIGHT_LIGHT_RED -> NestTheme.colors.RN._500
+        NestLabelType.HIGHLIGHT_LIGHT_GREEN -> if (isSystemInDarkTheme()) NestTheme.colors.GN._500 else NestTheme.colors.GN._800
+        NestLabelType.HIGHLIGHT_LIGHT_ORANGE -> if (isSystemInDarkTheme()) NestTheme.colors.YN._500 else NestTheme.colors.YN._800
+        NestLabelType.HIGHLIGHT_LIGHT_GREY -> if (isSystemInDarkTheme()) NestTheme.colors.NN._600 else NestTheme.colors.NN._800
+        NestLabelType.HIGHLIGHT_LIGHT_RED -> if (isSystemInDarkTheme()) NestTheme.colors.RN._500 else NestTheme.colors.RN._800
+        NestLabelType.HIGHLIGHT_LIGHT_BLUE -> if (isSystemInDarkTheme()) NestTheme.colors.BN._500 else NestTheme.colors.BN._800
+        NestLabelType.HIGHLIGHT_LIGHT_TEAL -> if (isSystemInDarkTheme()) NestTheme.colors.TN._500 else NestTheme.colors.TN._800
+        NestLabelType.HIGHLIGHT_LIGHT_IMAGE_LABEL -> if (isSystemInDarkTheme()) NestTheme.colors.NN._0 else NestTheme.colors.NN._1000
     }
 
     Surface(
-        modifier = modifier,
-        shape = RoundedCornerShape(4.dp),
+        modifier = modifier.height(20.dp),
+        shape = RoundedCornerShape(3.dp),
         color = backgroundColor
     ) {
         Text(
@@ -42,34 +56,165 @@ fun NestLabel(
             modifier = Modifier.padding(horizontal = 4.dp, vertical = 3.dp),
             color = textColor,
             fontSize = 10.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            maxLines = 1
         )
     }
 }
 
 enum class NestLabelType {
     HIGHLIGHT_LIGHT_GREEN,
+    HIGHLIGHT_LIGHT_BLUE,
     HIGHLIGHT_LIGHT_ORANGE,
+    HIGHLIGHT_LIGHT_RED,
+    HIGHLIGHT_LIGHT_TEAL,
     HIGHLIGHT_LIGHT_GREY,
-    HIGHLIGHT_LIGHT_RED
+    HIGHLIGHT_LIGHT_IMAGE_LABEL,
 }
 
-@Preview(name = "Label (Green)")
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun NestLabelPreview() {
-    NestLabel(
-        Modifier,
-        labelText = "Berlangsung",
-        nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREEN
-    )
+private fun NestLabelGreenPreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Green",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREEN
+        )
+    }
+}
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelOrangePreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Orange",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_ORANGE
+        )
+    }
 }
 
-@Preview(name = "Label (Red)")
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
-fun NestLabelRedPreview() {
-    NestLabel(
-        Modifier,
-        labelText = "Dibatalkan",
-        nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_RED
-    )
+private fun NestLabelGreyPreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Grey",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREY
+        )
+    }
 }
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelRedPreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Red",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_RED
+        )
+    }
+}
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelBluePreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Blue",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_BLUE
+        )
+    }
+}
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelTealPreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Teal",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_TEAL
+        )
+    }
+}
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelOnImagePreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Label on Image",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_IMAGE_LABEL
+        )
+    }
+}
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun NestLabelGreenEllipsisPreview() {
+    NestTheme {
+        NestLabel(
+            labelText = "Green label with ellipsis",
+            nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREEN
+        )
+    }
+}
+@Preview(
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    device = "spec:width=411dp,height=891dp"
+)
+@Preview(
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    backgroundColor = 0xFF222329,
+    device = "spec:width=411dp,height=891dp"
+)
+@Composable
+private fun NestLabelOnDevicesPreview() {
+    NestTheme {
+        Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                NestLabel(labelText = "Green", nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREEN)
+                NestLabel(
+                    labelText = "Orange",
+                    nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_ORANGE
+                )
+                NestLabel(labelText = "Grey", nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_GREY)
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                NestLabel(labelText = "Red", nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_RED)
+                NestLabel(labelText = "Blue", nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_BLUE)
+                NestLabel(labelText = "Teal", nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_TEAL)
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                NestLabel(
+                    labelText = "Label on Image",
+                    nestLabelType = NestLabelType.HIGHLIGHT_LIGHT_IMAGE_LABEL
+                )
+            }
+        }
+    }
+}
+//NN100 70%
+val ImageLabelColorDark = Color(0xFF2E2F36)
+
+//NN900 70%
+val ImageLabelColorLight = Color(0xB32E3137)
