01ac58dcf940ce2b70cbae393907b7023530e621
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-04-06 15:18:27 +0700

    feat: don't change value to 0 if user choose none face filter

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/viewmodel/PlayBroadcastViewModel.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/view/viewmodel/PlayBroadcastViewModel.kt
@@ -1747,7 +1747,6 @@ class PlayBroadcastViewModel @AssistedInject constructor(
                     faceFilters = it.faceFilters.map { item ->
                         item.copy(
                             isSelected = item.id == faceFilter.id,
-                            value = if(faceFilter.isRemoveEffect) 0.0 else item.value
                         )
                     }
                 )
