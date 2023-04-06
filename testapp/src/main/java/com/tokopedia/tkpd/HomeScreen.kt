23cb70c6937ddf00fecbbab1cda31da3c2eaeeb0
M. Fiqri Muthohar <fiqri.muthohar@tokopedia.com>
2023-04-06 09:27:42 +0700

    prevent applink text edit to be displayed under soft keyboard in small devices

--- testapp/src/main/java/com/tokopedia/tkpd/HomeScreen.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/HomeScreen.kt
@@ -1,22 +1,35 @@
 package com.tokopedia.tkpd
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.constrain
 import androidx.compose.ui.unit.dp
 import com.tokopedia.common_compose.components.NestButton
 import com.tokopedia.common_compose.principles.NestHeader
 import com.tokopedia.common_compose.ui.NestTheme
+import com.tokopedia.content.test.cassava.contains
 
+@OptIn(ExperimentalFoundationApi::class)
 @SuppressLint("UnsupportedDarkModeColor")
 @Composable
 fun HomeScreen(
@@ -25,8 +38,16 @@ fun HomeScreen(
     onApplinkChanged: (String) -> Unit = {},
     onNavigateTo: (MainActivity.HomeDestination) -> Unit = {}
 ) {
-    Surface {
-        Column {
+    val scrollState = rememberScrollState()
+    val constraints = remember { mutableStateOf(Constraints()) }
+    val bringIntoViewRequester =  BringIntoViewRequester()
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .onMeasureConstraints { constraints.value = it }
+            .verticalScroll(scrollState)
+        ) {
             NestHeader(title = "Tokopedia Test App", showBackIcon = false)
             val urlBgColor = if (model.urlState.contains("live", true)) {
                 NestTheme.colors.GN._600
@@ -45,7 +66,7 @@ fun HomeScreen(
             )
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .constrainSize { constraints.value }
                     .padding(horizontal = 72.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -89,7 +110,8 @@ fun HomeScreen(
                     value = model.applink,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 4.dp),
+                        .padding(vertical = 4.dp)
+                        .bringIntoViewRequester(bringIntoViewRequester),
                     onValueChange = onApplinkChanged,
                     textStyle = NestTheme.typography.body3,
                     label = {
@@ -115,6 +137,25 @@ fun HomeScreen(
                 }
             }
         }
+    }
+}
+private fun Modifier.onMeasureConstraints(
+    block: (Constraints) -> Unit
+) = layout { measurable, constraints ->
+    // record the constraints *before* measuring so that they're available during recursive measurement
+    block(constraints)
+    val placeable = measurable.measure(constraints)
+    layout(placeable.width, placeable.height) {
+        placeable.place(0, 0)
+    }
+}
+
+private fun Modifier.constrainSize(
+    getConstraints: () -> Constraints
+) = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints.constrain(getConstraints()))
+    layout(placeable.width, placeable.height) {
+        placeable.place(0, 0)
     }
 }
 
