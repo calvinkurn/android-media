a5516649888a90b98a03db9e64324b6829ee774f
rezagama <rezagamahidayat@gmail.com>
2023-04-06 09:36:15 +0700

    Fix constant name

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/config/DevOptConfig.java
@@ -9,24 +9,24 @@ import android.content.SharedPreferences;
 public class DevOptConfig {
 
     private static final String CHUCKER_CONFIG = "chucker_config";
-    private static final String IS_CHUCKER_NORIFICATION_ENABLED = "is_chucker_notification_enabled";
+    private static final String IS_CHUCKER_NOTIFICATION_ENABLED = "is_chucker_notification_enabled";
 
     private static final String DEV_OPT_ON_NOTIF_ENABLED = "DEV_OPT_ON_NOTIF_ENABLED";
     private static final String IS_DEV_OPT_ON_NOTIF_ENABLED = "IS_DEV_OPT_ON_NOTIF_ENABLED";
 
     public static boolean isChuckNotifEnabled(Context context) {
         SharedPreferences cache = context.getSharedPreferences(CHUCKER_CONFIG, Context.MODE_PRIVATE);
-        return cache.getBoolean(IS_CHUCKER_NORIFICATION_ENABLED, false);
+        return cache.getBoolean(IS_CHUCKER_NOTIFICATION_ENABLED, false);
     }
 
     public static void setChuckNotifEnabled(Context context, boolean isChuckNotifEnabled) {
         context.getSharedPreferences(CHUCKER_CONFIG, Context.MODE_PRIVATE)
-                .edit()
-                .putBoolean(
-                    IS_CHUCKER_NORIFICATION_ENABLED,
-                        isChuckNotifEnabled
-                )
-                .apply();
+            .edit()
+            .putBoolean(
+                IS_CHUCKER_NOTIFICATION_ENABLED,
+                isChuckNotifEnabled
+            )
+            .apply();
     }
 
     public static boolean isDevOptOnNotifEnabled(Context context) {
