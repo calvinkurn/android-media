a9ae74f5f4b74faf81d83ee4ec47c7850e8cbf97
stevenwijaya <steven.wijaya@tokopedia.com>
2023-04-10 03:04:26 +0700

    build with home credit camera

--- features/fintech/indodana/src/main/java/com/tokopedia/indodana/IndodanaActivity.kt
+++ features/fintech/indodana/src/main/java/com/tokopedia/indodana/IndodanaActivity.kt
@@ -2,10 +2,15 @@ package com.tokopedia.indodana
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Base64
 import android.util.DisplayMetrics
 import android.util.Log
 import android.util.Pair
@@ -13,12 +18,19 @@ import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.indodana.whitelabelsdk.camera.CameraController
 import com.indodana.whitelabelsdk.camera.WhitelabelCameraView
 import com.indodana.whitelabelsdk.webview.jsinterface.WhitelabelCameraJsInterface
+import com.tokopedia.applink.ApplinkConst
+import com.tokopedia.applink.RouteManager
+import com.tokopedia.homecredit.view.activity.HomeCreditRegisterActivity
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
 import java.io.UnsupportedEncodingException
 import java.net.URLEncoder
 
@@ -97,28 +109,30 @@ class IndodanaActivity : AppCompatActivity() {
 
     //Handler to display camera view
     private fun takePictureFromCamera(docType: String?, lang: String?) {
-        runOnUiThread {
-            if (cameraController == null) {
-                cameraController = CameraController(
-                    this,
-                    this,
-                    getRealScreenSize(),
-                    cameraview,
-                    docType
-                ) { docsTyp, imageBase64 ->
-                    finishTakePicture(docsTyp, imageBase64)
-                }
-            }
-            cameraview.initialize(docType, lang, "", {
-                cameraController?.takePicture()
-            }, { toggled: Boolean ->
-                cameraController?.flashToggled(toggled)
-            }, {
-                dismissTakeImageView()
-            })
-            cameraview.visibility = View.VISIBLE
-            cameraController?.startTakingPicture()
-        }
+        val intent = RouteManager.getIntent(this, ApplinkConst.HOME_CREDIT_SELFIE_WITHOUT_TYPE)
+        startActivityForResult(intent, REQUEST_CODE_IMAGE)
+//        runOnUiThread {
+//            if (cameraController == null) {
+//                cameraController = CameraController(
+//                    this,
+//                    this,
+//                    getRealScreenSize(),
+//                    cameraview,
+//                    docType
+//                ) { docsTyp, imageBase64 ->
+//                    finishTakePicture(docsTyp, imageBase64)
+//                }
+//            }
+//            cameraview.initialize(docType, lang, "", {
+//                cameraController?.takePicture()
+//            }, { toggled: Boolean ->
+//                cameraController?.flashToggled(toggled)
+//            }, {
+//                dismissTakeImageView()
+//            })
+//            cameraview.visibility = View.VISIBLE
+//            cameraController?.startTakingPicture()
+//        }
     }
 
     //Handler to inject image back to webview
@@ -177,5 +191,96 @@ class IndodanaActivity : AppCompatActivity() {
         webview.onPause()
         cameraController?.onPause()
         super.onPause()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == Activity.RESULT_OK) {
+            val imageFilePath = data?.getStringExtra(HomeCreditRegisterActivity.HCI_KTP_IMAGE_PATH)
+
+            val imageBase64 = getBase64FromImagePath(imageFilePath ?: "")?.replace("\n", "")
+
+            finishTakePicture(DOCTYPE_KYC, imageBase64)
+        }
+    }
+
+    private fun getBase64OfPrescriptionImage(localFilePath: String, compress : Boolean = false, compressCounter : Int = 0): String {
+        var prescriptionImageBitmap: Bitmap? = null
+        val options: BitmapFactory.Options
+        var finalEncodedString = ""
+        return try {
+            prescriptionImageBitmap = if(compress){
+                options = BitmapFactory.Options()
+                options.inSampleSize = 2 + compressCounter
+                BitmapFactory.decodeFile(localFilePath, options)
+            }else {
+                BitmapFactory.decodeFile(localFilePath)
+            }
+            val prescriptionByteArrayOutputStream = ByteArrayOutputStream()
+            prescriptionImageBitmap?.compress(
+                Bitmap.CompressFormat.JPEG,
+                QUALITY_SAFE_FIX,
+                prescriptionByteArrayOutputStream
+            )
+            val byteArrayImage = prescriptionByteArrayOutputStream.toByteArray()
+            prescriptionImageBitmap?.recycle()
+
+            val encodedString = Base64.encodeToString(byteArrayImage, Base64.DEFAULT)
+            finalEncodedString = encodedString
+            finalEncodedString
+        }catch (e : Exception){
+            prescriptionImageBitmap?.recycle()
+            when(e){
+                is NullPointerException -> {
+                    if((!compress) || (compress && (compressCounter < MAX_COMPRESSIONS))){
+                        finalEncodedString = if (compress && localFilePath.isNotBlank()){
+                            getBase64OfPrescriptionImage(localFilePath, true,compressCounter + 1)
+                        }else {
+                            getBase64OfPrescriptionImage(localFilePath, true,compressCounter +  1)
+                        }
+                    }
+                }
+                else -> {
+                }
+            }
+            finalEncodedString
+        }
+    }
+
+    fun getBase64FromImagePath(imagePath: String): String? {
+        try {
+            val file = File(imagePath)
+            val inputStream = FileInputStream(file)
+            val buffer = ByteArray(1024)
+            val outputStream = ByteArrayOutputStream()
+            var length: Int
+            while (inputStream.read(buffer).also { length = it } != -1) {
+                outputStream.write(buffer, 0, length)
+            }
+            inputStream.close()
+            val imageBytes = outputStream.toByteArray()
+            return Base64.encodeToString(imageBytes, Base64.DEFAULT)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    fun getBitmapFromBase64(base64String: String): Bitmap? {
+        try {
+            val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    companion object {
+        private const val REQUEST_CODE_IMAGE = 123
+        private const val DOCTYPE_KYC = "kyc"
+        private const val MAX_COMPRESSIONS = 5
+        private const val QUALITY_SAFE_FIX = 60
     }
 }
