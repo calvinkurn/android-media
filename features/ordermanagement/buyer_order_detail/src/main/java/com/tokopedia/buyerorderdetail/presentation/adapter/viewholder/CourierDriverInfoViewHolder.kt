c8514875261ad2d7231a70ec6f7010754df08b58
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-04-10 12:05:37 +0700

    refactor: change itemview to non-null & add layoutmanager to rv button courier driver

--- features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/CourierDriverInfoViewHolder.kt
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/adapter/viewholder/CourierDriverInfoViewHolder.kt
@@ -2,30 +2,32 @@ package com.tokopedia.buyerorderdetail.presentation.adapter.viewholder
 
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.buyerorderdetail.R
 import com.tokopedia.buyerorderdetail.common.utils.BuyerOrderDetailNavigator
 import com.tokopedia.buyerorderdetail.presentation.adapter.CourierActionButtonAdapter
 import com.tokopedia.buyerorderdetail.presentation.model.ShipmentInfoUiModel
+import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.unifycomponents.ImageUnify
 import com.tokopedia.unifyprinciples.Typography
 
 open class CourierDriverInfoViewHolder(
-        itemView: View?,
-        private val navigator: BuyerOrderDetailNavigator
+    private val itemView: View,
+    private val navigator: BuyerOrderDetailNavigator
 ) : AbstractViewHolder<ShipmentInfoUiModel.CourierDriverInfoUiModel>(itemView) {
 
     companion object {
         val LAYOUT = R.layout.item_buyer_order_detail_courier_driver_info
     }
 
-    private val container = itemView?.findViewById<ConstraintLayout>(R.id.container)
-    private val ivBuyerOrderDetailCourierDriverPhoto = itemView?.findViewById<ImageUnify>(R.id.ivBuyerOrderDetailCourierDriverPhoto)
-    private val tvBuyerOrderDetailCourierDriverName = itemView?.findViewById<Typography>(R.id.tvBuyerOrderDetailCourierDriverName)
-    private val tvBuyerOrderDetailCourierDriverPlateNumber = itemView?.findViewById<Typography>(R.id.tvBuyerOrderDetailCourierDriverPlateNumber)
-    private val rvBuyerOrderDetailCourierDriverButton = itemView?.findViewById<RecyclerView>(R.id.rv_courier_driver_btn)
+    private val container = itemView.findViewById<ConstraintLayout>(R.id.container)
+    private val ivBuyerOrderDetailCourierDriverPhoto = itemView.findViewById<ImageUnify>(R.id.ivBuyerOrderDetailCourierDriverPhoto)
+    private val tvBuyerOrderDetailCourierDriverName = itemView.findViewById<Typography>(R.id.tvBuyerOrderDetailCourierDriverName)
+    private val tvBuyerOrderDetailCourierDriverPlateNumber = itemView.findViewById<Typography>(R.id.tvBuyerOrderDetailCourierDriverPlateNumber)
+    private val rvBuyerOrderDetailCourierDriverButton = itemView.findViewById<RecyclerView>(R.id.rv_courier_driver_btn)
 
     private var element: ShipmentInfoUiModel.CourierDriverInfoUiModel? = null
 
@@ -65,6 +67,11 @@ open class CourierDriverInfoViewHolder(
         CourierActionButtonAdapter(element, navigator).apply {
             this.driverActionButtonList = element.buttonList
             rvBuyerOrderDetailCourierDriverButton?.adapter = this
+            rvBuyerOrderDetailCourierDriverButton.layoutManager = LinearLayoutManager(
+                itemView.context,
+                LinearLayoutManager.HORIZONTAL,
+                element.buttonList.size == Int.ONE
+            ) // Reverse layout if only one
         }
     }
 }
