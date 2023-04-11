fea2dedcaa9b7aac08f0a454ef0e4970533475eb
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-04-11 12:03:01 +0700

    feat[bs_product_detail_info] update graphql query pdp get layout

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/usecase/GetPdpLayoutUseCase.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/usecase/GetPdpLayoutUseCase.kt
@@ -24,8 +24,10 @@ import com.tokopedia.usecase.coroutines.UseCase
 import javax.inject.Inject
 import javax.inject.Named
 
-open class GetPdpLayoutUseCase @Inject constructor(private val gqlUseCase: MultiRequestGraphqlUseCase,
-                                                   @Named(NAME_LAYOUT_ID_DAGGER) private val layoutIdTest: String) : UseCase<ProductDetailDataModel>() {
+open class GetPdpLayoutUseCase @Inject constructor(
+    private val gqlUseCase: MultiRequestGraphqlUseCase,
+    @Named(NAME_LAYOUT_ID_DAGGER) private val layoutIdTest: String
+) : UseCase<ProductDetailDataModel>() {
 
     companion object {
         const val QUERY = """
@@ -199,6 +201,8 @@ open class GetPdpLayoutUseCase @Inject constructor(private val gqlUseCase: Multi
                     ... on pdpDataProductDetail {
                       title
                       content {
+                        key
+                        extParam
                         title
                         subtitle
                         applink
