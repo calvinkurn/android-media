bf2fda2d788a64c4d0651c408765fd8ba0d8853f
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-04-11 13:16:53 +0700

    feat: implement owoc viewmodel

---
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/presentation/viewmodel/OwocViewModel.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.buyerorderdetail.presentation.viewmodel
+
+import com.tokopedia.abstraction.base.view.viewmodel.BaseViewModel
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+
+class OwocViewModel(
+    dispatcher: CoroutineDispatchers
+) : BaseViewModel(dispatcher.main)
