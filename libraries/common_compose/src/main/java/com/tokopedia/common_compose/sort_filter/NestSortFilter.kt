aeb2a0ce7ced691fda7c560879678308b8b7e0ba
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-04-13 07:24:39 +0700

    Implement Advanced NestSortFilter

--- libraries/common_compose/src/main/java/com/tokopedia/common_compose/sort_filter/NestSortFilter.kt
+++ libraries/common_compose/src/main/java/com/tokopedia/common_compose/sort_filter/NestSortFilter.kt
@@ -2,27 +2,19 @@ package com.tokopedia.common_compose.sort_filter
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
-import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.tokopedia.common_compose.components.Color
 import com.tokopedia.common_compose.components.NestChips
-import com.tokopedia.common_compose.components.NestNotification
-import com.tokopedia.common_compose.ui.LocalNestColor
 import com.tokopedia.common_compose.ui.NestTheme
 import com.tokopedia.iconunify.R
 
@@ -41,7 +33,7 @@ fun NestSortFilter(
     }
     Row(modifier = modifier) {
         val closeVisible by remember(items) {
-            derivedStateOf {  showClearFilterIcon && items.any { it.isSelected } }
+            derivedStateOf { showClearFilterIcon && items.any { it.isSelected } }
         }
         AnimatedVisibility(closeVisible) {
             PrefixFilterItem(
@@ -71,11 +63,52 @@ fun NestSortFilter(
     }
 }
 
+@Composable
+fun NestSortFilterAdvanced(
+    modifier: Modifier = Modifier,
+    size: Size = Size.DEFAULT,
+    items: List<SortFilter>,
+    onPrefixClicked: () -> Unit = {},
+    onItemClicked: (SortFilter) -> Unit
+) {
+    val chipSize = when (size) {
+        Size.DEFAULT -> com.tokopedia.common_compose.components.Size.SMALL
+        Size.LARGE -> com.tokopedia.common_compose.components.Size.LARGE
+    }
+    val selectedSize by remember(items) { derivedStateOf { items.filter { it.isSelected }.size } }
+    Row(modifier = modifier) {
+        PrefixFilterItem(
+            modifier = Modifier.padding(end = 4.dp),
+            size = size,
+            text = "Filter",
+            selectedSize = selectedSize,
+            onClick = onPrefixClicked
+        )
+        LazyRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(items) {
+                NestChips(
+                    text = it.title,
+                    isSelected = it.isSelected,
+                    size = chipSize,
+                    showChevron = it.showChevron,
+                    onClick = {
+                        onItemClicked(it)
+                        it.onClick()
+                    }
+                )
+            }
+        }
+    }
+}
+
 data class SortFilter(
     val title: String,
     val isSelected: Boolean,
     val showChevron: Boolean = false,
-    val onClick: () -> Unit
+    val onClick: () -> Unit = {}
 )
 
 enum class Size { DEFAULT, LARGE }
@@ -84,17 +117,21 @@ enum class Size { DEFAULT, LARGE }
 @Preview(name = "Sort Filter (Dark)", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun NestSortFilterPreview() {
-    var items by remember {
-        mutableStateOf(
-            listOf(
-                SortFilter("Terrible", true, showChevron = true, onClick = {}),
-                SortFilter("Bad", false, onClick = {}),
-                SortFilter("Medium", false, onClick = {}),
-                SortFilter("Good", false, onClick = {}),
-                SortFilter("Impressive", false, onClick = {}),
-            )
-        )
-    }
+    var items by remember { mutableStateOf(listOf(
+        SortFilter("Terrible", true, showChevron = true),
+        SortFilter("Bad", false),
+        SortFilter("Medium", false),
+        SortFilter("Good", false),
+        SortFilter("Impressive", false),
+    )) }
+    var advItems by remember { mutableStateOf(listOf(
+        SortFilter("Micro", false),
+        SortFilter("Tiny", false),
+        SortFilter("Small", true),
+        SortFilter("Medium", false),
+        SortFilter("Big", false, showChevron = true),
+        SortFilter("Enormous", false),
+    )) }
 
     var size by remember { mutableStateOf(Size.DEFAULT) }
     NestTheme {
@@ -123,6 +160,14 @@ fun NestSortFilterPreview() {
                         }
                     },
                     onClearFilter = { items = items.map { it.copy(isSelected = false) } })
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Advanced Filter")
+                NestSortFilterAdvanced(items = advItems, size = size, onItemClicked = { sf ->
+                    advItems = advItems.map {
+                        if (it == sf) it.copy(isSelected = it.isSelected.not())
+                        else it
+                    }
+                })
             }
         }
     }
