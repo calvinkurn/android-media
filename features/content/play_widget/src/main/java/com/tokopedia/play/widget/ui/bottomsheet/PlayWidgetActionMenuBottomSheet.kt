15edda06d2725f362d398d429bb3bba02ca5264d
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-04-13 10:09:08 +0700

    `fix`: moved impression listener to `show` function

--- features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/bottomsheet/PlayWidgetActionMenuBottomSheet.kt
+++ features/content/play_widget/src/main/java/com/tokopedia/play/widget/ui/bottomsheet/PlayWidgetActionMenuBottomSheet.kt
@@ -54,7 +54,6 @@ class PlayWidgetActionMenuBottomSheet : BottomSheetUnify() {
     private fun setupBottomSheet() {
         viewBinding = BottomsheetPlayWidgetSellerActionListBinding.inflate(LayoutInflater.from(context))
         setChild(viewBinding?.root)
-        mListener?.onImpressed()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -73,6 +72,7 @@ class PlayWidgetActionMenuBottomSheet : BottomSheetUnify() {
     }
 
     fun show(fragmentManager: FragmentManager) {
+        mListener?.onImpressed()
         if(!isAdded) showNow(fragmentManager, TAG)
     }
 
