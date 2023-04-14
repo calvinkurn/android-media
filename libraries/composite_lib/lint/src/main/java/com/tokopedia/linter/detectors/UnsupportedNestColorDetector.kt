8d325d3dd85a9885f5f3e9113f10f1c9de14ac57
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-04-14 15:16:30 +0700

    chore[nest_color_linter] update code

--- libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/detectors/UnsupportedNestColorDetector.kt
+++ libraries/composite_lib/lint/src/main/java/com/tokopedia/linter/detectors/UnsupportedNestColorDetector.kt
@@ -1,10 +1,11 @@
+@file:Suppress("UnstableApiUsage")
+
 package com.tokopedia.linter.detectors
 
 import com.android.SdkConstants
 import com.android.resources.ResourceFolderType
 import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.Category
-import com.android.tools.lint.detector.api.Context
 import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Implementation
 import com.android.tools.lint.detector.api.Issue
@@ -12,23 +13,20 @@ import com.android.tools.lint.detector.api.JavaContext
 import com.android.tools.lint.detector.api.LintFix
 import com.android.tools.lint.detector.api.Scope
 import com.android.tools.lint.detector.api.Severity
-import com.android.tools.lint.detector.api.SourceCodeScanner
 import com.android.tools.lint.detector.api.XmlContext
 import com.android.tools.lint.detector.api.XmlScanner
-import com.android.tools.lint.detector.api.getBaseName
-import com.google.common.collect.Sets
 import com.tokopedia.linter.Priority
 import com.tokopedia.linter.unify.UnifyComponentsList
 import org.jetbrains.uast.UElement
 import org.jetbrains.uast.ULiteralExpression
 import org.w3c.dom.Attr
-import java.io.File
 
-class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
+class UnsupportedNestColorDetector : Detector(), XmlScanner {
     companion object {
         private const val ISSUE_ID = "UnsupportedNestColor"
+        private const val BRIEF_DESC = "Deprecated color should not be used."
         private const val ERROR_MESSAGE = "Color has deprecated. " +
-            "Please use color with token contain 'N' before color number. Example:" +
+            "Please use color with token contains 'N' before color number. Example:" +
             "\n❌ : Unify_N100 " +
             "\n✅ : Unify_NN100" +
             "\n❌ : Unify_G500 " +
@@ -38,11 +36,11 @@ class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
         private val ISSUE_CATEGORY = Category.CORRECTNESS
         private const val NEST_INDEX = 7
         private const val NEST_CHARACTER = "N"
-        private const val REGEX_OLD_COLOR = "Unify_[A-Z]\\d{1,4}"
+        val REGEX_OLD_COLOR = "(Unify_[A-Z]\\d{1,4}_\\d{1,2})|(Unify_[A-Z]\\d{1,4})".toRegex()
 
         val JAVA_ISSUE = Issue.create(
             id = ISSUE_ID,
-            briefDescription = "Deprecated color should not be used.",
+            briefDescription = BRIEF_DESC,
             explanation = ERROR_MESSAGE,
             category = ISSUE_CATEGORY,
             priority = ISSUE_PRIORITY.value,
@@ -54,7 +52,7 @@ class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
         )
         val XML_ISSUE = Issue.create(
             id = ISSUE_ID,
-            briefDescription = "Deprecated color should not be used.",
+            briefDescription = BRIEF_DESC,
             explanation = ERROR_MESSAGE,
             category = ISSUE_CATEGORY,
             priority = ISSUE_PRIORITY.value,
@@ -66,64 +64,18 @@ class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
         )
     }
 
-    private val deprecatedResourceIds = Sets.newHashSet<String>()
-
+    // region XML detector
     override fun appliesTo(folderType: ResourceFolderType): Boolean {
         return folderType == ResourceFolderType.LAYOUT ||
             folderType == ResourceFolderType.VALUES ||
-            folderType == ResourceFolderType.DRAWABLE ||
-            folderType == ResourceFolderType.COLOR
+            folderType == ResourceFolderType.COLOR ||
+            folderType == ResourceFolderType.DRAWABLE
     }
 
-    /*override fun beforeCheckEachProject(context: Context) {
-        context.project.resourceFolders.forEach { resFolder ->
-            findDeprecatedResourceIds(resFolder = resFolder)
-        }
-    }*/
-
-    private fun findDeprecatedResourceIds(resFolder: File?) {
-        val layoutDirs = resFolder?.listFiles()
-            ?.filter { it.name.contains(SdkConstants.FD_RES_LAYOUT) }
-
-        val drawableDirs = resFolder?.listFiles()
-            ?.filter { it.name.contains(SdkConstants.FD_RES_DRAWABLE) }
-
-        val valueDirs = resFolder?.listFiles()
-            ?.filter { it.name.contains(SdkConstants.FD_RES_VALUES) }
-
-        layoutDirs?.forEach { file ->
-            val files = file.listFiles().orEmpty()
-            val drawable = files.map { getBaseName(it.name) }
-            deprecatedResourceIds.addAll(drawable)
-        }
-
-        drawableDirs?.forEach { file ->
-            val files = file.listFiles().orEmpty()
-            val drawable = files.map { getBaseName(it.name) }
-            deprecatedResourceIds.addAll(drawable)
-        }
-
-        valueDirs?.forEach {
-            val files = it.listFiles()
-            findValueResources(files)
-        }
-    }
-
-    private fun findValueResources(files: Array<File>?) {
-        files?.forEach { file ->
-            val valueIds = file.readLines().filter {
-                it.contains(SdkConstants.ATTR_NAME) && !it.contains(SdkConstants.TAG_ITEM)
-            }.map {
-                it.substringAfter("=\"").substringBefore("\"")
-            }.filter { it.isNotEmpty() }
-            deprecatedResourceIds.addAll(valueIds)
-        }
-    }
-
-    override fun getApplicableElements(): Collection<String> {
+    override fun getApplicableAttributes(): Collection<String> {
         return listOf(
-            SdkConstants.COLOR_RESOURCE_PREFIX,
             SdkConstants.ATTR_COLOR,
+            SdkConstants.ATTR_BACKGROUND,
             SdkConstants.ATTR_TEXT_COLOR,
             SdkConstants.ATTR_AM_PM_BACKGROUND_COLOR,
             SdkConstants.ATTR_AM_PM_TEXT_COLOR,
@@ -156,48 +108,18 @@ class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
             SdkConstants.ATTR_TAB_TEXT_COLOR,
             SdkConstants.ATTR_TITLE_TEXT_COLOR,
             SdkConstants.ATTR_YEAR_LIST_SELECTOR_COLOR,
-            SdkConstants.FD_RES_COLOR,
-            SdkConstants.RESOURCE_CLZ_COLOR,
-            SdkConstants.TAG_COLOR,
-            SdkConstants.TRANSPARENT_COLOR,
             SdkConstants.ATTR_TEXT_COLOR_LINK,
-            SdkConstants.ANDROID_COLOR_RESOURCE_PREFIX,
             SdkConstants.ATTR_CACHE_COLOR_HINT,
             SdkConstants.ATTR_TEXT_COLOR_HIGHLIGHT,
             SdkConstants.ATTR_TEXT_COLOR_HINT
+
         )
     }
 
     override fun visitAttribute(context: XmlContext, attribute: Attr) {
-        runCatching {
-            if (attribute.value.contains(Regex(REGEX_OLD_COLOR))) {
-                reportXmlError(context, attribute)
-            }
+        if (attribute.value.contains(REGEX_OLD_COLOR)) {
+            reportXmlError(context, attribute)
         }
-    }
-
-    override fun getApplicableUastTypes(): List<Class<out UElement>> {
-        return listOf(ULiteralExpression::class.java)
-    }
-
-    override fun createUastHandler(context: JavaContext): UElementHandler {
-        return object : UElementHandler() {
-            override fun visitLiteralExpression(node: ULiteralExpression) {
-                val value = node.value.toString()
-                if (value.matches(Regex(REGEX_OLD_COLOR))) {
-                    reportJavaError(context, node)
-                }
-            }
-        }
-    }
-
-    private fun reportJavaError(context: JavaContext, node: UElement) {
-        context.report(
-            JAVA_ISSUE,
-            node,
-            context.getLocation(node),
-            ERROR_MESSAGE
-        )
     }
 
     private fun reportXmlError(context: XmlContext, attribute: Attr) {
@@ -219,4 +141,32 @@ class UnsupportedNestColorDetector : Detector(), SourceCodeScanner, XmlScanner {
             quickFix
         )
     }
+    // endregion XML detector
+
+    // region JAVA detector
+    override fun getApplicableUastTypes(): List<Class<out UElement>> {
+        return listOf(ULiteralExpression::class.java)
+    }
+
+    override fun createUastHandler(context: JavaContext): UElementHandler {
+        return object : UElementHandler() {
+            override fun visitLiteralExpression(node: ULiteralExpression) {
+                val value = node.value.toString()
+                if (value.contains(REGEX_OLD_COLOR)) {
+                    reportJavaError(context, node)
+                }
+            }
+        }
+    }
+
+    private fun reportJavaError(context: JavaContext, node: UElement) {
+        context.report(
+            JAVA_ISSUE,
+            node,
+            context.getLocation(node),
+            ERROR_MESSAGE
+        )
+    }
+
+    // endregion JAVA detector
 }
--- libraries/composite_lib/lint/src/test/java/UnsupportedNestColorDetectorTest.kt
+++ libraries/composite_lib/lint/src/test/java/UnsupportedNestColorDetectorTest.kt
@@ -1,3 +1,4 @@
+
 import com.android.tools.lint.checks.infrastructure.LintDetectorTest
 import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Issue
@@ -19,8 +20,29 @@ class UnsupportedNestColorDetectorTest : LintDetectorTest() {
 
     override fun getIssues(): MutableList<Issue> {
         return mutableListOf(
-            UnsupportedNestColorDetector.XML_ISSUE
+            UnsupportedNestColorDetector.XML_ISSUE,
+            UnsupportedNestColorDetector.JAVA_ISSUE
         )
+    }
+
+    @Test
+    fun `test`() {
+        val regex = UnsupportedNestColorDetector.REGEX_OLD_COLOR
+
+        assertTrue(regex.containsMatchIn("Unify_N100_70"))
+        assertTrue("Unify_N100_70".contains(regex))
+        assertTrue("Unify_N50".contains(regex))
+        assertTrue(
+            """
+            android:textColor="@color/Unify_N700"
+            """.trimMargin().contains(regex)
+        )
+        assertTrue(
+            """
+            android:color="@color/Unify_N700_10"
+            """.trimMargin().contains(regex)
+        )
+        assertFalse("Unify_NN50".contains(regex))
     }
 
     @Test
@@ -35,7 +57,8 @@ class UnsupportedNestColorDetectorTest : LintDetectorTest() {
                         xmlns:tools="http://schemas.android.com/tools"
                         android:layout_width="wrap_content"
                         android:layout_height="wrap_content"
-                        android:background="@drawable/bg_border_pdp"
+                        android:background="@color/Unify_N700"
+                        android:color="@color/Unify_N700"
                         android:gravity="center_vertical"
                         android:orientation="horizontal">
                     
@@ -46,12 +69,20 @@ class UnsupportedNestColorDetectorTest : LintDetectorTest() {
                             android:textColor="@color/Unify_N700"
                             app:typographyType="display_3"
                             tools:text="Diskusi" />
+                            
+                        <com.tokopedia.unifyprinciples.Typography
+                            android:id="@+id/socialProofChipTitle"
+                            android:layout_width="wrap_content"
+                            android:layout_height="wrap_content"
+                            android:color="@color/Unify_N700_50"
+                            app:typographyType="display_3"
+                            tools:text="Diskusi" />
                     
                     </LinearLayout>
                 """.trimIndent()
             )
         ).allowMissingSdk()
             .run()
-            .expectWarningCount(0)
+            .expectWarningCount(3)
     }
 }
