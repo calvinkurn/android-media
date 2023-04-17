4e29b3d4f7380700d58a2e67369c42d39182185a
souravsaikia <sourav.saikia@tokopedia.com>
2023-04-18 00:11:28 +0530

    Set up recycler view for Search Page

---
+++ libraries/login_helper/src/main/java/com/tokopedia/loginHelper/presentation/searchAccount/adapter/LoginHelperSearchAdapter.kt
@@ -0,0 +1,46 @@
+package com.tokopedia.loginHelper.presentation.searchAccount.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.loginHelper.databinding.ItemLoginSearchBinding
+import com.tokopedia.loginHelper.domain.uiModel.UserDataUiModel
+import com.tokopedia.loginHelper.presentation.searchAccount.adapter.listener.LoginHelperSearchListener
+import com.tokopedia.loginHelper.presentation.searchAccount.adapter.viewholder.LoginHelperSearchViewHolder
+
+class LoginHelperSearchAdapter(
+    private val listener: LoginHelperSearchListener
+) : RecyclerView.Adapter<LoginHelperSearchViewHolder>() {
+
+    private var data: MutableList<UserDataUiModel> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LoginHelperSearchViewHolder {
+        val binding = ItemLoginSearchBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return LoginHelperSearchViewHolder(binding, listener)
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: LoginHelperSearchViewHolder, position: Int) {
+        data.getOrNull(position)?.let {
+            holder.bind(it)
+        }
+    }
+
+    fun clearDataList() {
+        val oldDataSize = data.size
+        data = mutableListOf()
+        notifyItemRangeRemoved(Int.ZERO, oldDataSize)
+    }
+
+    fun addDataList(newData: List<UserDataUiModel>) {
+        val oldDataSize = data.size
+        data.addAll(newData)
+        notifyItemRangeInserted(oldDataSize, oldDataSize + newData.size)
+    }
+}
---
+++ libraries/login_helper/src/main/java/com/tokopedia/loginHelper/presentation/searchAccount/adapter/viewholder/LoginHelperSearchViewHolder.kt
@@ -0,0 +1,30 @@
+package com.tokopedia.loginHelper.presentation.searchAccount.adapter.viewholder
+
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.loginHelper.databinding.ItemLoginSearchBinding
+import com.tokopedia.loginHelper.domain.uiModel.UserDataUiModel
+import com.tokopedia.loginHelper.presentation.searchAccount.adapter.listener.LoginHelperSearchListener
+
+class LoginHelperSearchViewHolder(
+    private val binding: ItemLoginSearchBinding,
+    private val listener: LoginHelperSearchListener
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(user: UserDataUiModel) {
+        binding.apply {
+            userEmail.text = user.email
+            editAccount.setOnClickListener {
+                listener.onEditAccount(user)
+            }
+            deleteAccount.setOnClickListener {
+                listener.onDeleteAccount(user)
+            }
+        }
+    }
+
+    companion object {
+        @LayoutRes
+        val RES_LAYOUT = com.tokopedia.loginHelper.R.layout.item_login_search
+    }
+}
