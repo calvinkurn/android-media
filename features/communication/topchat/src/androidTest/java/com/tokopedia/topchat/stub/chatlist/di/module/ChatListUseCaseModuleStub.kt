1f3497b32a87a4d0bce78cf7035db5a45ccde864
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-04-23 12:39:03 +0700

    Adjust androidTest source

--- features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/di/module/ChatListUseCaseModuleStub.kt
+++ features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/di/module/ChatListUseCaseModuleStub.kt
@@ -1,12 +1,12 @@
 package com.tokopedia.topchat.stub.chatlist.di.module
 
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.topchat.chatlist.domain.mapper.GetChatListMessageMapper
 import com.tokopedia.topchat.chatlist.domain.pojo.ChatListPojo
-import com.tokopedia.topchat.chatlist.domain.pojo.NotificationsPojo
 import com.tokopedia.topchat.chatlist.domain.pojo.whitelist.ChatWhitelistFeatureResponse
 import com.tokopedia.topchat.chatlist.domain.usecase.GetChatListMessageUseCase
 import com.tokopedia.topchat.chatlist.domain.usecase.GetChatNotificationUseCase
@@ -47,8 +47,8 @@ object ChatListUseCaseModuleStub {
     @Provides
     @ActivityScope
     fun provideChatNotificationUseCaseStub(
-        graphqlUseCase: GraphqlUseCase<NotificationsPojo>
-    ): GetChatNotificationUseCaseStub = GetChatNotificationUseCaseStub(graphqlUseCase)
+        @ApplicationContext graphqlRepository: GraphqlRepository
+    ): GetChatNotificationUseCaseStub = GetChatNotificationUseCaseStub(graphqlRepository)
 
     @Provides
     @ActivityScope
--- features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/usecase/GetChatNotificationUseCaseStub.kt
+++ features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/usecase/GetChatNotificationUseCaseStub.kt
@@ -1,22 +1,17 @@
 package com.tokopedia.topchat.stub.chatlist.usecase
 
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.test.application.dispatcher.CoroutineTestDispatchersProvider
 import com.tokopedia.topchat.chatlist.domain.pojo.NotificationsPojo
 import com.tokopedia.topchat.chatlist.domain.usecase.GetChatNotificationUseCase
 import javax.inject.Inject
 
 class GetChatNotificationUseCaseStub @Inject constructor(
-        gqlUseCase: GraphqlUseCase<NotificationsPojo>
-) : GetChatNotificationUseCase(gqlUseCase, CoroutineTestDispatchersProvider) {
-
+    @ApplicationContext gqlRepo: GraphqlRepository
+) : GetChatNotificationUseCase(gqlRepo, CoroutineTestDispatchersProvider) {
     var response = NotificationsPojo()
-
-    override fun getChatNotification(
-        shopId: String,
-        onSuccess: (NotificationsPojo) -> Unit,
-        onError: (Throwable) -> Unit
-    ) {
-        onSuccess(response)
+    override suspend fun execute(params: String): NotificationsPojo {
+        return response
     }
 }
