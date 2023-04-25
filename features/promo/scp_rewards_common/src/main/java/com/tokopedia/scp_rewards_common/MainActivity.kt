d7639b7773b30a00fa13cb8a7d53a5bd6c6d22a6
jayantkapila <JayKay@11798>
2023-04-25 11:41:38 +0530

    scp rewards common module setup

---
+++ features/promo/scp_rewards_common/src/main/java/com/tokopedia/scp_rewards_common/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.scp_rewards_common
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
