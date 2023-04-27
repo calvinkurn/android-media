8b5f37818e42da6123e6499a8e5437b9ef08dfa6
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-04-27 15:39:42 +0700

    fix[product_report_compose] remove blank line

--- features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonFooter.kt
+++ features/merchant/report/src/main/java/com/tokopedia/report/view/fragment/components/ProductReportReasonFooter.kt
@@ -1,14 +1,11 @@
 package com.tokopedia.report.view.fragment.components
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -35,16 +32,13 @@ fun ProductReportReasonFooter(
             .clickable {
                 onClick.invoke(text)
             },
-        text = composeSpannable(text = text, onClick = onClick),
+        text = composeSpannable(),
         textStyle = NestTheme.typography.body3
     )
 }
 
 @Composable
-private fun composeSpannable(
-    text: String,
-    onClick: (String) -> Unit
-) = buildAnnotatedString {
+private fun composeSpannable() = buildAnnotatedString {
     withStyle(
         style = SpanStyle(
             color = NestTheme.colors.GN._500,
@@ -57,55 +51,13 @@ private fun composeSpannable(
     append("tipe-tipe pelanggaran produk di Tokopedia")
 }
 
-@Composable
-fun AnnotatedClickableText() {
-    val annotatedText = buildAnnotatedString {
-        append("Click ")
-
-        // We attach this *URL* annotation to the following content
-        // until `pop()` is called
-        pushStringAnnotation(
-            tag = "URL",
-            annotation = "https://developer.android.com"
-        )
-        withStyle(
-            style = SpanStyle(
-                color = Color.Blue,
-                fontWeight = FontWeight.Bold
-            )
-        ) {
-            append("here")
-        }
-
-        pop()
-    }
-
-    ClickableText(
-        text = annotatedText,
-        onClick = { offset ->
-            // We check if there is an *URL* annotation attached to the text
-            // at the clicked position
-            annotatedText.getStringAnnotations(
-                tag = "URL",
-                start = offset,
-                end = offset
-            ).firstOrNull()?.let { annotation ->
-                // If yes, we log its value
-                Log.d("Clicked URL", annotation.item)
-            }
-        }
-    )
-}
-
 @Preview
 @Composable
 fun ProductReportReasonFooterPreview() {
     Column {
-        AnnotatedClickableText()
         ProductReportReasonFooter(
             text = ""
         ) {
-
         }
     }
 }
