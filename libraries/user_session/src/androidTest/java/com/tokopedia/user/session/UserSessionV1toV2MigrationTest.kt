977a601cf214ab2ecd6e2c574df31e8417276a21
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-04-30 08:20:23 +0700

    Write more test involving empty value

--- libraries/user_session/src/androidTest/java/com/tokopedia/user/session/UserSessionV1toV2MigrationTest.kt
+++ libraries/user_session/src/androidTest/java/com/tokopedia/user/session/UserSessionV1toV2MigrationTest.kt
@@ -3,9 +3,9 @@ package com.tokopedia.user.session
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.tokopedia.user.session.Constants.EMAIL
+import com.tokopedia.user.session.Constants.LOGIN_METHOD
 import com.tokopedia.utils.MockTimber
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -32,18 +32,23 @@ class UserSessionV1toV2MigrationTest {
 
     @Test
     fun whenGettingValue_oldPreferenceShouldBeRemoved() {
-        // save using old key and value : for example login_session
-        val v = "noiz354@gmail.com"
-        val sharedPrefs = context.getSharedPreferences(LEGACY_SESSION, Context.MODE_PRIVATE)
-        val editor = sharedPrefs.edit()
-        editor.putString(EMAIL, v)
-        editor.apply()
-        val email = sut.email
-        assertEquals(v, email)
+        testV1PreferenceShouldBeRemoved(EMAIL, "noiz@gmail.com") {
+            assertEquals(it, sut.email)
+        }
+    }
 
-        // old value should be removed by now
-        val oldValue = sharedPrefs.getString("EMAIL", "none")
-        assertEquals("none", oldValue)
+    @Test
+    fun whenGettingLoginMethod_oldPreferenceShouldBeRemoved() {
+        testV1PreferenceShouldBeRemoved(LOGIN_METHOD, "google") {
+            assertEquals(it, sut.loginMethod)
+        }
+    }
+
+    @Test
+    fun whenGettingLoginMethodWithEmptyValue_oldPreferenceShouldBeRemoved() {
+        testV1PreferenceShouldBeRemoved(LOGIN_METHOD, "") {
+            assertEquals(it, sut.loginMethod)
+        }
     }
 
     @Test
@@ -90,6 +95,20 @@ class UserSessionV1toV2MigrationTest {
         UserSessionMap.map.clear()
         userId = sut.tokenType
         assertThat(userId, equalTo(empty))
+    }
+
+    private fun testV1PreferenceShouldBeRemoved(KEY: String, VALUE: String, exec: (String) -> Unit) {
+        val sharedPrefs = context.getSharedPreferences(LEGACY_SESSION, Context.MODE_PRIVATE)
+        val editor = sharedPrefs.edit()
+        editor.putString(KEY, VALUE)
+        editor.apply()
+
+        // when
+        exec(VALUE)
+
+        // old value should be removed by now
+        val oldValue = sharedPrefs.getString(KEY, "none")
+        assertEquals("none", oldValue)
     }
 
     private fun cleanUp() {
