0bd5252fda3dafb91d1fb933d87e95f9dd646034
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-04-30 09:24:27 +0700

    Remove unused inject

--- features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/di/ManageAddressComponent.kt
+++ features/logistic/manageaddress/src/main/java/com/tokopedia/manageaddress/di/ManageAddressComponent.kt
@@ -4,7 +4,6 @@ import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
 import com.tokopedia.manageaddress.di.module.ManageAddressModule
 import com.tokopedia.manageaddress.di.module.ManageAddressViewModelModule
-import com.tokopedia.manageaddress.ui.manageaddress.ManageAddressActivity
 import com.tokopedia.manageaddress.ui.manageaddress.ManageAddressFragment
 import com.tokopedia.manageaddress.ui.manageaddress.fromfriend.FromFriendFragment
 import com.tokopedia.manageaddress.ui.manageaddress.mainaddress.MainAddressFragment
@@ -15,7 +14,6 @@ import dagger.Component
 @ActivityScope
 @Component(modules = [ManageAddressModule::class, ManageAddressViewModelModule::class], dependencies = [BaseAppComponent::class])
 interface ManageAddressComponent {
-    fun inject(manageAddressActivity: ManageAddressActivity)
     fun inject(manageAddressFragment: ManageAddressFragment)
     fun inject(mainAddressFragment: MainAddressFragment)
     fun inject(fromFriendFragment: FromFriendFragment)
