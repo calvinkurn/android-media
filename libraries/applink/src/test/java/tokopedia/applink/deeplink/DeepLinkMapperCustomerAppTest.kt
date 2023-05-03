60c7e27005cf8bdf81f9e6b8958217d3aee69533
CJ <calvyn.julian@tokopedia.com>
2023-05-03 12:38:47 +0700

    change SIZE_MAPPER value

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -25,7 +25,7 @@ import org.robolectric.RobolectricTestRunner
 class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
 
     companion object {
-        const val SIZE_MAPPER = 223
+        const val SIZE_MAPPER = 224
     }
 
     override fun setup() {
