b094b534bd7b030bc6fc05d265d331e0cff3a093
astidhiyaa <asti.anzaria@tokopedia.com>
2023-05-04 09:26:00 +0700

    fix: only call gql when item doesnt have variant

--- features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
+++ features/content/play/src/main/java/com/tokopedia/play/view/fragment/PlayBottomSheetFragment.kt
@@ -318,17 +318,15 @@ class PlayBottomSheetFragment @Inject constructor(
         if (product.isVariantAvailable) {
             playViewModel.submitAction(ShowVariantAction(product, false))
             analytic.clickActionProductWithVariant(product.id, action)
+        } else {
+            playViewModel.submitAction(
+                when (action) {
+                    ProductAction.Buy -> BuyProductAction(sectionInfo, product)
+                    ProductAction.AddToCart -> AtcProductAction(sectionInfo, product)
+                    ProductAction.OCC -> OCCProductAction(sectionInfo, product)
+                }
+            )
         }
-
-        //harus atc dlu walaupun mau buka variant?
-
-        playViewModel.submitAction(
-            when (action) {
-                ProductAction.Buy -> BuyProductAction(sectionInfo, product)
-                ProductAction.AddToCart -> AtcProductAction(sectionInfo, product)
-                ProductAction.OCC -> OCCProductAction(sectionInfo, product)
-            }
-        )
     }
 
     private fun showLoadingView() {
