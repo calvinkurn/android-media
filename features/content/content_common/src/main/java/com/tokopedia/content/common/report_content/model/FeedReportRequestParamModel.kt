24e2fe9ee2affcf9a10b5743aa7ad984c54f54fe
astidhiyaa <asti.anzaria@tokopedia.com>
2023-05-10 15:27:39 +0700

    fix: mostly comments in Report page

--- features/content/content_common/src/main/java/com/tokopedia/content/common/report_content/model/FeedReportRequestParamModel.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/report_content/model/FeedReportRequestParamModel.kt
@@ -1,10 +1,15 @@
 package com.tokopedia.content.common.report_content.model
 
+import com.google.gson.annotations.SerializedName
 import com.tokopedia.content.common.usecase.FeedComplaintSubmitReportUseCase
 
 data class FeedReportRequestParamModel(
+    @SerializedName("reportType")
     val reportType: String = FeedComplaintSubmitReportUseCase.VALUE_REPORT_TYPE_POST,
+    @SerializedName("contentID")
     val contentId: String,
+    @SerializedName("reason")
     val reason: String,
+    @SerializedName("reasonDetails")
     val reasonDetails: String
 )
