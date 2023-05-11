0028d9f0f7560bea6ce89aca089228bbfcf710b4
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-05-11 17:18:05 +0700

    refactor: update remote config function in deeplink internal mapper communication

--- libraries/applink/src/main/java/com/tokopedia/applink/communication/DeeplinkMapperCommunication.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/communication/DeeplinkMapperCommunication.kt
@@ -1,7 +1,6 @@
 package com.tokopedia.applink.communication
 
 import android.content.Context
-import androidx.annotation.VisibleForTesting
 import com.tokopedia.applink.FirebaseRemoteConfigInstance
 import com.tokopedia.applink.constant.DeeplinkConstant
 import com.tokopedia.applink.internal.ApplinkConstInternalCommunication
@@ -11,31 +10,35 @@ import com.tokopedia.applink.internal.ApplinkConstInternalOrder
 object DeeplinkMapperCommunication {
 
     private const val CHAT_SETTINGS = "chatsettings"
+
     const val TOKOCHAT_REMOTE_CONFIG = "android_enable_tokochat"
+
+    /**
+     * Remote Config util
+     */
+    private fun isRemoteConfigActive(
+        context: Context,
+        key: String,
+        default: Boolean = true
+    ): Boolean {
+        return try {
+            FirebaseRemoteConfigInstance.get(context).getBoolean(key, default)
+        } catch (throwable: Throwable) {
+            true
+        }
+    }
 
     /**
      * Tokochat mapper with remote config
      */
     fun getRegisteredNavigationTokoChat(context: Context, deeplink: String): String {
-        return if (isTokoChatRollenceActive(context)) {
+        return if (isRemoteConfigActive(context, TOKOCHAT_REMOTE_CONFIG)) {
             deeplink.replace(
                 DeeplinkConstant.SCHEME_TOKOPEDIA_SLASH,
                 ApplinkConstInternalCommunication.INTERNAL_COMMUNICATION + "/"
             )
         } else {
             ApplinkConstInternalOrder.UNIFY_ORDER_TOKOFOOD
-        }
-    }
-
-    @VisibleForTesting
-    fun isTokoChatRollenceActive(context: Context): Boolean {
-        return try {
-            FirebaseRemoteConfigInstance.get(context).getBoolean(
-                TOKOCHAT_REMOTE_CONFIG,
-                true
-            )
-        } catch (throwable: Throwable) {
-            true
         }
     }
 
