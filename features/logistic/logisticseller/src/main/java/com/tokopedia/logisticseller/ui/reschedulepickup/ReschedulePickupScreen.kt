82197295d4f6f760454cd819896a257d58c917e5
hiradevina <fakhira.devina@tokopedia.com>
2023-05-11 14:50:16 +0700

    fix: set bottom sheet content, revert button style

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/ReschedulePickupScreen.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/ReschedulePickupScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -81,8 +80,8 @@ fun ReschedulePickupScreen(
                     sheetState.show()
                 }
             } else {
-                setRescheduleBottomSheetState(bottomSheetState)
                 sheetState.hide()
+                setRescheduleBottomSheetState(bottomSheetState)
             }
         }
     }
@@ -172,10 +171,9 @@ private fun ReschedulePickupScreenLayout(
         NestButton(
             modifier = Modifier
                 .padding(end = 16.dp, start = 16.dp, bottom = 16.dp)
-                .fillMaxWidth()
-                .defaultMinSize(minHeight = 48.dp),
+                .fillMaxWidth(),
             text = stringResource(id = R.string.title_button_reschedule_pickup),
-            enabled = state.valid
+            isEnabled = state.valid
         ) {
             onEvent(ReschedulePickupUiEvent.SaveReschedule)
         }
