e2a50d3036fb16de3d569204b735ba2ec1c04bb0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-05-12 10:04:06 +0700

    feat: remove compose dependency on people module

--- features/content/people/src/main/java/com/tokopedia/people/views/activity/ProfileSettingsActivity.kt
+++ features/content/people/src/main/java/com/tokopedia/people/views/activity/ProfileSettingsActivity.kt
@@ -2,41 +2,22 @@ package com.tokopedia.people.views.activity
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
 import com.tokopedia.abstraction.base.app.BaseMainApplication
-import com.tokopedia.common_compose.principles.NestTypography
-import com.tokopedia.common_compose.ui.NestTheme
-import com.tokopedia.header.HeaderUnify
-import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.kotlin.extensions.orFalse
 import com.tokopedia.people.di.DaggerUserProfileComponent
 import com.tokopedia.people.di.UserProfileModule
 import com.tokopedia.people.viewmodels.UserProfileSettingsViewModel
 import com.tokopedia.people.viewmodels.factory.UserProfileSettingsViewModelFactory
-import com.tokopedia.people.views.uimodel.ProfileSettingsUiModel
 import com.tokopedia.people.views.uimodel.action.UserProfileSettingsAction
 import javax.inject.Inject
 import com.tokopedia.people.R
 import com.tokopedia.people.databinding.ActivityUserProfileSettingsBinding
 import com.tokopedia.people.utils.showErrorToast
 import com.tokopedia.people.views.uimodel.event.UserProfileSettingsEvent
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 
 /**
@@ -65,26 +46,6 @@ class ProfileSettingsActivity : AppCompatActivity() {
         setupView()
         setupListener()
         setupObserver()
-//        setContent {
-//            NestTheme {
-//                Column(
-//                    modifier = Modifier
-//                        .background(
-//                            color = colorResource(id = com.tokopedia.unifyprinciples.R.color.Unify_Background)
-//                        )
-//                        .fillMaxHeight()
-//                ) {
-//                    NestHeader(
-//                        title = "Pengaturan Profil",
-//                        onNavigationClickListener = {
-//                            finish()
-//                        }
-//                    )
-//
-//                    ReviewSettingRow()
-//                }
-//            }
-//        }
     }
 
     private fun inject() {
@@ -141,149 +102,6 @@ class ProfileSettingsActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    @Composable
-    private fun ReviewSettingRow() {
-
-        val state by viewModel.reviewSettings.collectAsState(initial = ProfileSettingsUiModel.Empty)
-
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            NestIcon(icon = IconUnify.STAR)
-
-            NestTypography(
-                state.settingID,
-                textStyle = NestTheme.typography.display3,
-            )
-
-            NestSwitch(
-                isEnabled = state.isEnabled,
-                onChanged = {
-                    viewModel.submitAction(UserProfileSettingsAction.SetShowReview(isShow = it))
-                }
-            )
-        }
-    }
-
-    @Composable
-    private fun NestIcon(
-        modifier: Modifier = Modifier,
-        icon: Int,
-    ) {
-        AndroidView(
-            factory = { context ->
-                IconUnify(context)
-            },
-            update = {
-                it.setImage(icon)
-            },
-        )
-    }
-
-    @Composable
-    private fun NestHeader(
-        modifier: Modifier = Modifier,
-        title: String,
-        onNavigationClickListener: () -> Unit,
-    ) {
-        AndroidView(
-            modifier = modifier.fillMaxWidth(),
-            factory = { context ->
-                HeaderUnify(context)
-            },
-            update = {
-                it.title = title
-                it.isShowBackButton = true
-                it.setNavigationOnClickListener {
-                    onNavigationClickListener()
-                }
-            }
-        )
-    }
-
-    @Composable
-    private fun NestSwitch(
-        isEnabled: Boolean,
-        onChanged: (isEnabled: Boolean) -> Unit
-    ) {
-//        AndroidView(
-//            factory = { context ->
-//                SwitchUnify(this, object : AttributeSet {
-//                    override fun getAttributeCount(): Int = 0
-//
-//                    override fun getAttributeName(p0: Int): String = ""
-//
-//                    override fun getAttributeValue(p0: Int): String = ""
-//
-//                    override fun getAttributeValue(p0: String?, p1: String?): String = ""
-//
-//                    override fun getPositionDescription(): String = ""
-//
-//                    override fun getAttributeNameResource(p0: Int): Int = 0
-//
-//                    override fun getAttributeListValue(
-//                        p0: String?,
-//                        p1: String?,
-//                        p2: Array<out String>?,
-//                        p3: Int
-//                    ): Int = 0
-//
-//                    override fun getAttributeListValue(
-//                        p0: Int,
-//                        p1: Array<out String>?,
-//                        p2: Int
-//                    ): Int = 0
-//
-//                    override fun getAttributeBooleanValue(
-//                        p0: String?,
-//                        p1: String?,
-//                        p2: Boolean
-//                    ): Boolean = false
-//
-//                    override fun getAttributeBooleanValue(p0: Int, p1: Boolean): Boolean = false
-//
-//                    override fun getAttributeResourceValue(p0: String?, p1: String?, p2: Int): Int = 0
-//
-//                    override fun getAttributeResourceValue(p0: Int, p1: Int): Int = 0
-//
-//                    override fun getAttributeIntValue(p0: String?, p1: String?, p2: Int): Int = 0
-//
-//                    override fun getAttributeIntValue(p0: Int, p1: Int): Int = 0
-//
-//                    override fun getAttributeUnsignedIntValue(
-//                        p0: String?,
-//                        p1: String?,
-//                        p2: Int
-//                    ): Int = 0
-//
-//                    override fun getAttributeUnsignedIntValue(p0: Int, p1: Int): Int = 0
-//
-//                    override fun getAttributeFloatValue(
-//                        p0: String?,
-//                        p1: String?,
-//                        p2: Float
-//                    ): Float = 0f
-//
-//                    override fun getAttributeFloatValue(p0: Int, p1: Float): Float = 0f
-//
-//                    override fun getIdAttribute(): String = ""
-//
-//                    override fun getClassAttribute(): String = ""
-//
-//                    override fun getIdAttributeResourceValue(p0: Int): Int = 0
-//
-//                    override fun getStyleAttribute(): Int = 0
-//                })
-//            },
-//            update = {
-//                it.isEnabled = isEnabled
-//                it.setOnCheckedChangeListener { compoundButton, isEnable ->
-//                    onChanged(isEnabled)
-//                }
-//            }
-//        )
     }
 
     companion object {
