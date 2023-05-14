ab8e69f2706f11500408108fc3ece8fdd565abd1
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-05-14 16:39:58 +0700

    Clean HomeAccountTest.kt

--- features/user/home_account/src/androidTest/java/com/tokopedia/home_account/base/HomeAccountTest.kt
+++ features/user/home_account/src/androidTest/java/com/tokopedia/home_account/base/HomeAccountTest.kt
@@ -7,17 +7,14 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.analyticsdebugger.cassava.cassavatest.CassavaTestRule
 import com.tokopedia.analyticsdebugger.cassava.cassavatest.hasAllSuccess
-import com.tokopedia.home_account.common.idling.FragmentTransactionIdle
 import com.tokopedia.home_account.di.ActivityComponentFactory
 import com.tokopedia.home_account.stub.data.GraphqlRepositoryStub
 import com.tokopedia.home_account.stub.di.ActivityComponentFactoryStub
 import com.tokopedia.home_account.stub.di.user.HomeAccountUserComponentsStub
 import com.tokopedia.home_account.stub.view.activity.HomeAccountUserActivityStub
 import com.tokopedia.home_account.view.activity.HomeAccountUserActivity
-import com.tokopedia.home_account.view.fragment.HomeAccountUserFragment
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
 import com.tokopedia.user.session.UserSessionInterface
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -42,31 +39,13 @@ abstract class HomeAccountTest {
     lateinit var userSession: UserSessionInterface
 
     protected open lateinit var activity: HomeAccountUserActivity
-    protected open lateinit var fragmentTransactionIdling: FragmentTransactionIdle
-
-    lateinit var fragment: HomeAccountUserFragment
-
     protected val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
-    private val applicationContext: Context
-        get() = InstrumentationRegistry
-            .getInstrumentation().context.applicationContext
 
-    @ExperimentalCoroutinesApi
     @Before
     open fun before() {
-//        val appComponent = DaggerFakeBaseAppComponent.builder().fakeAppModule(FakeAppModule(applicationContext)).build()
-//        repo = appComponent.graphqlRepository() as GraphqlRepositoryStub
-//        homeAccountUserComponents = DaggerHomeAccountUserComponentsStub.builder()
-//            .fakeBaseAppComponent(appComponent)
-//            .fakeHomeAccountUserModules(FakeHomeAccountUserModules(context))
-//            .homeAccountUserUsecaseModules(HomeAccountUserUsecaseModules())
-//            .build()
-//        userSession = homeAccountUserComponents.userSession()
-
         val stub = ActivityComponentFactoryStub()
         ActivityComponentFactory.instance = stub
         stub.component.inject(this)
-//        ApplicationProvider.getApplicationContext<BaseMainApplication>().setComponent(appComponent)
         InstrumentationAuthHelper.loginInstrumentationTestUser1()
     }
 
@@ -81,7 +60,7 @@ abstract class HomeAccountTest {
     }
 
     fun Unit.validate(query: List<Map<String, String>>) {
-        Thread.sleep(5000)
+        Thread.sleep(3000)
         val queryMatcher = cassavaTestRule.validate(
             query,
             CassavaTestRule.MODE_SUBSET
@@ -100,14 +79,7 @@ abstract class HomeAccountTest {
     ) {
         val intent = Intent()
         intentModifier(intent)
-        activityTestRule.activity
         activityTestRule.launchActivity(intent)
-        activity = activityTestRule.activity
-        fragmentTransactionIdling = FragmentTransactionIdle(
-            activity.supportFragmentManager,
-            HomeAccountUserActivity.TAG
-        )
-        fragment = activity.fragment
     }
 
     companion object {
