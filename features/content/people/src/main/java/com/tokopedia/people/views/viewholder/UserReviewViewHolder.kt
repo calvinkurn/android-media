73ea16a261d0476a75b43bce51e73723201e5945
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-05-15 18:17:47 +0700

    feat: bind data to user review viewholder (not done yet)

--- features/content/people/src/main/java/com/tokopedia/people/views/viewholder/UserReviewViewHolder.kt
+++ features/content/people/src/main/java/com/tokopedia/people/views/viewholder/UserReviewViewHolder.kt
@@ -3,9 +3,13 @@ package com.tokopedia.people.views.viewholder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.people.databinding.ItemUserReviewBinding
 import com.tokopedia.people.databinding.UpItemUserPostLoadingBinding
 import com.tokopedia.people.views.uimodel.UserReviewUiModel
+import com.tokopedia.people.R
+import com.tokopedia.unifyprinciples.R as unifyR
 
 /**
  * Created By : Jonathan Darwin on May 15, 2023
@@ -19,8 +23,41 @@ class UserReviewViewHolder private constructor() {
 
         fun bind(item: UserReviewUiModel.Review) {
             binding.apply {
+                imgProduct.setImageUrl(item.product.productImageURL)
+                tvProduct.text = item.product.productName
 
+                if(item.product.productVariant.variantName.isNotEmpty()) {
+                    tvVariant.text = itemView.context.getString(
+                        R.string.up_profile_user_review_product_variant_template,
+                        item.product.productVariant.variantName
+                    )
+                }
+
+                setupStar(icStar1, item.rating >= 1)
+                setupStar(icStar2, item.rating >= 2)
+                setupStar(icStar3, item.rating >= 3)
+                setupStar(icStar4, item.rating >= 4)
+                setupStar(icStar5, item.rating >= 5)
+
+                /** TODO: handle Selengkapnya */
+                tvReview.text = item.reviewText
+
+                tvLikeCounter.text = item.likeDislike.totalLike.toString()
             }
+        }
+
+        private fun setupStar(icon: IconUnify, isFilled: Boolean) {
+            val color = if (isFilled) {
+                unifyR.color.Unify_YN300
+            } else {
+                unifyR.color.Unify_NN300
+            }
+
+            icon.setImage(
+                IconUnify.STAR,
+                newLightEnable = MethodChecker.getColor(itemView.context, color),
+                newDarkEnable = MethodChecker.getColor(itemView.context, color),
+            )
         }
 
         companion object {
