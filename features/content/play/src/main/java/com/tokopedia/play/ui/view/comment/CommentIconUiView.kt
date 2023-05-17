fbe60060dac7f029d9f2a0b1eb950acf05129a1d
astidhiyaa <asti.anzaria@tokopedia.com>
2023-05-17 18:02:46 +0700

    fix: crash

--- features/content/play/src/main/java/com/tokopedia/play/ui/view/comment/CommentIconUiView.kt
+++ features/content/play/src/main/java/com/tokopedia/play/ui/view/comment/CommentIconUiView.kt
@@ -26,8 +26,7 @@ class CommentIconUiView(private val group: Group, listener: Listener) {
     }
 
     fun setCounter(value: String) {
-        view.findViewById<com.tokopedia.unifyprinciples.Typography>(R.id.view_vod_comment_counter)
-            .text = value
+        view.findViewById<com.tokopedia.unifyprinciples.Typography>(R.id.view_vod_comment_counter)?.text = value
     }
 
     interface Listener {
