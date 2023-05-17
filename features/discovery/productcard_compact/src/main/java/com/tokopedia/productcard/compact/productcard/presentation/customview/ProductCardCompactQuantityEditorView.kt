24949433f9a5f8fa0acaeb627120d5c50a925daf
rezagama <rezagamahidayat@gmail.com>
2023-05-17 13:49:47 +0700

    Fix show hide shimmer and checkmark

--- features/discovery/productcard_compact/src/main/java/com/tokopedia/productcard/compact/productcard/presentation/customview/ProductCardCompactQuantityEditorView.kt
+++ features/discovery/productcard_compact/src/main/java/com/tokopedia/productcard/compact/productcard/presentation/customview/ProductCardCompactQuantityEditorView.kt
@@ -9,9 +9,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
-import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.orZero
-import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.productcard.compact.R
 import com.tokopedia.productcard.compact.common.util.ContextUtil.getActivityFromContext
@@ -38,6 +36,7 @@ class ProductCardCompactQuantityEditorView @JvmOverloads constructor(
         private const val DEFAULT_NUMBER = 0
         private const val DEFAULT_DP = 0
         private const val NO_PROGRESS = 0F
+        private const val FINISHED_PROGRESS = 100F
         private const val HIDE_SOFT_INPUT_KEYBOARD_FLAG = 0
         private const val MAX_ALPHA = 1f
         private const val MIN_ALPHA = 0f
@@ -245,17 +244,6 @@ class ProductCardCompactQuantityEditorView @JvmOverloads constructor(
             binding.root.getTransition(animScene)?.setEnable(false)
             binding.root.loadLayoutDescription(scene)
         }
-
-    }
-
-    private fun LayoutProductCardCompactQuantityEditorViewBinding.showCheckMarkIcon() {
-        imageCheckMark.show()
-        background.hide()
-        editText.hide()
-        subButton.hide()
-        addButton.hide()
-        addButton.alpha = MIN_ALPHA
-        addToCartBtnShimmer.alpha = MIN_ALPHA
     }
 
     private fun LayoutProductCardCompactQuantityEditorViewBinding.onClickAddNonVariant() {
@@ -293,10 +281,8 @@ class ProductCardCompactQuantityEditorView @JvmOverloads constructor(
     }
 
     private fun LayoutProductCardCompactQuantityEditorViewBinding.onClickAddToCartWithoutAnimation() {
-        background.hide()
-        editText.hide()
-        subButton.hide()
-        addButton.alpha = MIN_ALPHA
+        root.setTransition(R.id.end, R.id.end)
+        root.progress = FINISHED_PROGRESS
         addToCartBtnShimmer.alpha = MAX_ALPHA
         onQuantityChangedListener?.invoke(minQuantity)
     }
@@ -455,13 +441,15 @@ class ProductCardCompactQuantityEditorView @JvmOverloads constructor(
         }
     }
 
+    private fun LayoutProductCardCompactQuantityEditorViewBinding.showCheckMarkIcon() {
+        root.setTransition(R.id.startWithValue, R.id.startWithValue)
+        root.progress = NO_PROGRESS
+        addToCartBtnShimmer.alpha = MIN_ALPHA
+    }
+
     private fun LayoutProductCardCompactQuantityEditorViewBinding.showAddButton() {
-        background.show()
-        addButton.show()
-        editText.hide()
-        subButton.hide()
-        imageCheckMark.hide()
-        addButton.alpha = MAX_ALPHA
+        root.setTransition(R.id.start, R.id.start)
+        root.progress = NO_PROGRESS
         addToCartBtnShimmer.alpha = MIN_ALPHA
     }
 
