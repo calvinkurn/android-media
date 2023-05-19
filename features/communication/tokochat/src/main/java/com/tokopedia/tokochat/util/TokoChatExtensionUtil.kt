d938f64cfc8a3861ae7f829295467ffad2ec4d14
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-05-19 14:23:07 +0700

    Merge branch 'feature/android/tokochat_bubbles' of https://github.com/tokopedia/android-tokopedia-core into feature/kelvinsaputra/bubble_awareness_ticker
    # Conflicts:
    #	features/communication/tokochat/src/main/java/com/tokopedia/tokochat/analytics/TokoChatAnalytics.kt
    #	features/communication/tokochat/src/main/java/com/tokopedia/tokochat/analytics/TokoChatAnalyticsConstants.kt
    #	features/communication/tokochat_common/src/main/res/values/strings.xml

--- features/communication/tokochat/src/main/java/com/tokopedia/tokochat/util/TokoChatExtensionUtil.kt
+++ features/communication/tokochat/src/main/java/com/tokopedia/tokochat/util/TokoChatExtensionUtil.kt
@@ -1,24 +1,13 @@
 package com.tokopedia.tokochat.util
 
 import android.app.Activity
-import android.os.Build
-import android.view.Display
+import androidx.core.app.ActivityCompat
 
 fun Activity?.isFromBubble(): Boolean {
     return try {
         this?.let {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                isLaunchedFromBubble
-            } else {
-                val displayId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    display?.displayId
-                } else {
-                    @Suppress("DEPRECATION")
-                    windowManager.defaultDisplay.displayId
-                }
-                displayId != Display.DEFAULT_DISPLAY
-            }
-        } ?: false
+            ActivityCompat.isLaunchedFromBubble(it)
+        } == true
     } catch (ignored: Throwable) {
         false
     }
