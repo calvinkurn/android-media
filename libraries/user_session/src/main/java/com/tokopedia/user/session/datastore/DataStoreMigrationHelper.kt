cbb2b736593c8f3582e56e71be33d147712557a3
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-05-19 13:16:26 +0700

    Refactor checkDataSync for testability

--- libraries/user_session/src/main/java/com/tokopedia/user/session/datastore/DataStoreMigrationHelper.kt
+++ libraries/user_session/src/main/java/com/tokopedia/user/session/datastore/DataStoreMigrationHelper.kt
@@ -1,10 +1,8 @@
 package com.tokopedia.user.session.datastore
 
-import android.content.Context
 import android.util.Log
 import com.tokopedia.logger.ServerLogger
 import com.tokopedia.logger.utils.Priority
-import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.user.session.datastore.workmanager.DataStoreMigrationWorker
 import kotlinx.coroutines.flow.first
@@ -56,11 +54,11 @@ object DataStoreMigrationHelper {
         }
     }
 
-    suspend fun checkDataSync(applicationContext: Context): List<String> {
+    suspend fun checkDataSync(
+        dataStore: UserSessionDataStore,
+        userSession: UserSessionInterface
+    ): List<String> {
         val result = mutableListOf<String>()
-        val dataStore = UserSessionDataStoreClient.getInstance(applicationContext)
-        val userSession = UserSession(applicationContext)
-
         if (dataStore.getName().first().trim() != userSession.name.trim()) {
             result.add("name")
         }
