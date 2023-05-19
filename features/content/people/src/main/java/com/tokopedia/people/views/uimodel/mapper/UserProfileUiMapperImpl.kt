2f1b77b4ffe452e59bf693d2bb2cbaedbdb1a23e
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-05-19 16:09:32 +0700

    feat: only take 5 medias per review

--- features/content/people/src/main/java/com/tokopedia/people/views/uimodel/mapper/UserProfileUiMapperImpl.kt
+++ features/content/people/src/main/java/com/tokopedia/people/views/uimodel/mapper/UserProfileUiMapperImpl.kt
@@ -191,7 +191,7 @@ class UserProfileUiMapperImpl @Inject constructor(
                             mediaUrl = attachment.thumbnailURL,
                             type = UserReviewUiModel.Attachment.Type.Image,
                         )
-                    },
+                    }.take(MAX_REVIEW_MEDIA),
                     likeDislike = UserReviewUiModel.LikeDislike(
                         totalLike = it.likeDislike.totalLike,
                         likeStatus = it.likeDislike.likeStatus,
@@ -223,5 +223,6 @@ class UserProfileUiMapperImpl @Inject constructor(
         private const val SUCCESS_UPDATE_REMINDER_CODE = 200
         private const val SUCCESS_UNFOLLOW_CODE = "1"
         private const val EXTRA_STATS_REVIEWS = "reviews"
+        private const val MAX_REVIEW_MEDIA = 5
     }
 }
