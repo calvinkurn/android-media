8148d6c679e8f9058432581384d9d39cc0b11bd0
hiradevina <fakhira.devina@tokopedia.com>
2023-05-22 10:17:14 +0700

    fix: double code after solve conflict

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/ReschedulePickupScreen.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/reschedulepickup/ReschedulePickupScreen.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import com.tokopedia.common_compose.components.ButtonSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
@@ -36,6 +34,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.tokopedia.common_compose.components.ButtonSize
 import com.tokopedia.common_compose.components.NestBottomSheetShape
 import com.tokopedia.common_compose.components.NestButton
 import com.tokopedia.common_compose.components.NestTextField
@@ -83,7 +82,6 @@ fun ReschedulePickupScreen(
                     sheetState.show()
                 }
             } else {
-                setRescheduleBottomSheetState(bottomSheetState)
                 sheetState.hide()
                 setRescheduleBottomSheetState(bottomSheetState)
             }
