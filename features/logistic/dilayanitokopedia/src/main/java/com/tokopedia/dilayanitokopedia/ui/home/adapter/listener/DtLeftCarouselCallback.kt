ab037075e524234b620b683afccdbd21261ace92
Irpan - <irpan@tokopedia.com>
2023-05-23 08:57:55 +0700

    adjust tracker dt to include items

--- features/logistic/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/ui/home/adapter/listener/DtLeftCarouselCallback.kt
+++ features/logistic/dilayanitokopedia/src/main/java/com/tokopedia/dilayanitokopedia/ui/home/adapter/listener/DtLeftCarouselCallback.kt
@@ -1,13 +1,18 @@
 package com.tokopedia.dilayanitokopedia.ui.home.adapter.listener
 
 import com.tokopedia.dilayanitokopedia.data.analytics.DtHomepageAnalytics
+import com.tokopedia.dilayanitokopedia.data.analytics.ProductCardAnalyticsMapper
 import com.tokopedia.home_component.listener.MixLeftComponentListener
 import com.tokopedia.home_component.model.ChannelGrid
 import com.tokopedia.home_component.model.ChannelModel
+import com.tokopedia.user.session.UserSessionInterface
 
 object DtLeftCarouselCallback {
 
-    fun createLeftCarouselCallback(onActionLinkClicked: (String) -> Unit): MixLeftComponentListener {
+    fun createLeftCarouselCallback(
+        userSession: UserSessionInterface,
+        onActionLinkClicked: (String) -> Unit
+    ): MixLeftComponentListener {
         return object : MixLeftComponentListener {
 
             override fun onMixLeftImpressed(channel: ChannelModel, parentPos: Int) {
@@ -32,7 +37,10 @@ object DtLeftCarouselCallback {
                 adapterPosition: Int,
                 position: Int
             ) {
-                DtHomepageAnalytics.sendImpressionProductCardsDtEvent()
+                DtHomepageAnalytics.sendImpressionProductCardsDtEvent(
+                    userSession,
+                    ProductCardAnalyticsMapper.fromGridChannel(position, channelGrid)
+                )
             }
 
             override fun onProductCardClicked(
@@ -43,7 +51,10 @@ object DtLeftCarouselCallback {
                 applink: String
             ) {
                 onActionLinkClicked(applink)
-                DtHomepageAnalytics.sendClickProductCardsDtEvent()
+                DtHomepageAnalytics.sendClickProductCardsDtEvent(
+                    userSession,
+                    ProductCardAnalyticsMapper.fromGridChannel(position, channelGrid)
+                )
             }
 
             override fun onSeeMoreCardClicked(channel: ChannelModel, applink: String) {
