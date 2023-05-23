26111f90ad59aa24e37738d3da29f4f99293cb98
said.faisal <said.faisal@tokopedia.com>
2023-05-23 07:36:51 +0700

    fix failed fetching tokonow detail in old category

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/oldcategory/domain/model/TokonowCategoryDetail.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/oldcategory/domain/model/TokonowCategoryDetail.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class TokonowCategoryDetail(
-        @SerializedName("TokonowCategoryDetailResponse")
+        @SerializedName("TokonowCategoryDetail")
         @Expose
         val categoryDetail: CategoryDetail = CategoryDetail(),
 ) {
