3b183aa192bdeae0803a628ff57a204d0f4466b6
Muhammad Furqan <furqan@tokopedia.com>
2023-05-23 13:05:13 +0700

    [Feed Immersive] Create Unit Test for FeedPostViewModel

---
+++ features/content/feed/src/test/java/com/tokopedia/feedplus/presentation/viewmodel/FeedPostViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.tokopedia.feedplus.presentation.viewmodel
+
+import org.junit.Before
+
+/**
+ * Created By : Muhammad Furqan on 22/05/23
+ */
+class FeedPostViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+}
