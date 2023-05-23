21304c69c5bec060cff089624d5f8db8701384ac
said.faisal <said.faisal@tokopedia.com>
2023-05-23 15:27:31 +0700

    add rounded corner add the bottom of category title

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/category/di/module/CategoryParamModule.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/category/di/module/CategoryParamModule.kt
@@ -8,14 +8,10 @@ import javax.inject.Named
 @Module
 class CategoryParamModule(
         private val categoryL1: String,
-        private val categoryL2: String,
-        private val serviceType: String,
         private val queryParamMap: Map<String, String>,
 ) {
     companion object {
         const val NOW_CATEGORY_L1 = "now_category_l1"
-        const val NOW_CATEGORY_L2 = "now_category_l2"
-        const val NOW_CATEGORY_SERVICE_TYPE = "now_category_service_type"
         const val NOW_CATEGORY_QUERY_PARAM_MAP = "now_category_query_param_map"
     }
 
@@ -23,16 +19,6 @@ class CategoryParamModule(
     @Provides
     @Named(NOW_CATEGORY_L1)
     fun provideCategoryL1() = categoryL1
-
-    @CategoryScope
-    @Provides
-    @Named(NOW_CATEGORY_L2)
-    fun provideCategoryL2() = categoryL2
-
-    @CategoryScope
-    @Provides
-    @Named(NOW_CATEGORY_SERVICE_TYPE)
-    fun provideServiceType() = serviceType
 
     @CategoryScope
     @Provides
