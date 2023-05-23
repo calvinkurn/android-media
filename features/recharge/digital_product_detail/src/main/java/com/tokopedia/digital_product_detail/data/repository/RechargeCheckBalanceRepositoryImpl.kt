749c48bf50ceb95602ed7ded0418e744922b6d9b
Misael Jonathan <misael.jonathan@tokopedia.com>
2023-05-23 14:54:08 +0700

    [Recharge] add save access token use case

--- features/recharge/digital_product_detail/src/main/java/com/tokopedia/digital_product_detail/data/repository/RechargeCheckBalanceRepositoryImpl.kt
+++ features/recharge/digital_product_detail/src/main/java/com/tokopedia/digital_product_detail/data/repository/RechargeCheckBalanceRepositoryImpl.kt
@@ -3,22 +3,25 @@ package com.tokopedia.digital_product_detail.data.repository
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.digital_product_detail.data.mapper.DigitalPersoMapper
 import com.tokopedia.digital_product_detail.domain.model.DigitalCheckBalanceModel
+import com.tokopedia.digital_product_detail.domain.model.DigitalSaveAccessTokenResultModel
 import com.tokopedia.digital_product_detail.domain.repository.RechargeCheckBalanceRepository
 import com.tokopedia.digital_product_detail.domain.usecase.GetRechargeCheckBalanceUseCase
+import com.tokopedia.digital_product_detail.domain.usecase.SaveRechargeUserBalanceAccessTokenUseCase
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RechargeCheckBalanceRepositoryImpl @Inject constructor(
     private val getRechargeCheckBalanceUseCase: GetRechargeCheckBalanceUseCase,
+    private val saveRechargeUserBalanceAccessTokenUseCase: SaveRechargeUserBalanceAccessTokenUseCase,
     private val digitalPersoMapper: DigitalPersoMapper,
-    private val dispatchers: CoroutineDispatchers,
-): RechargeCheckBalanceRepository {
+    private val dispatchers: CoroutineDispatchers
+) : RechargeCheckBalanceRepository {
 
     override suspend fun getRechargeCheckBalance(
         clientNumbers: List<String>,
         dgCategoryIds: List<Int>,
         dgOperatorIds: List<Int>,
-        channelName: String,
+        channelName: String
     ): DigitalCheckBalanceModel =
         withContext(dispatchers.io) {
             val data = getRechargeCheckBalanceUseCase.apply {
@@ -26,5 +29,19 @@ class RechargeCheckBalanceRepositoryImpl @Inject constructor(
             }.executeOnBackground()
 
             return@withContext digitalPersoMapper.mapDigiPersoToCheckBalanceOTPModel(data.digitalPersoData)
+        }
+
+    override suspend fun saveRechargeUserBalanceAccessToken(
+        msisdn: String,
+        accessToken: String
+    ): DigitalSaveAccessTokenResultModel =
+        withContext(dispatchers.io) {
+            val data = saveRechargeUserBalanceAccessTokenUseCase.apply {
+                setParams(msisdn, accessToken)
+            }.executeOnBackground()
+
+            return@withContext digitalPersoMapper.mapSaveAccessTokenToAccessTokenResultModel(
+                data.rechargeSaveTelcoUserBalanceAccessToken
+            )
         }
 }
