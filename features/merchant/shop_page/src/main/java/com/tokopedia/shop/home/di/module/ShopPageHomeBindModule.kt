d74fd7bea08cc3d1935a4154bb82f58260413074
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-05-29 14:49:50 +0700

    fix: duplicate provides contentcoachmarksharedpref on shoppage

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/module/ShopPageHomeBindModule.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/module/ShopPageHomeBindModule.kt
@@ -2,8 +2,6 @@ package com.tokopedia.shop.home.di.module
 
 import com.tokopedia.content.common.analytic.entrypoint.PlayPerformanceDashboardEntryPointAnalytic
 import com.tokopedia.content.common.analytic.entrypoint.PlayPerformanceDashboardEntryPointAnalyticImpl
-import com.tokopedia.content.common.util.coachmark.ContentCoachMarkSharedPref
-import com.tokopedia.content.common.util.coachmark.ContentCoachMarkSharedPrefImpl
 import com.tokopedia.shop.home.di.scope.ShopPageHomeScope
 import dagger.Binds
 import dagger.Module
@@ -14,8 +12,4 @@ abstract class ShopPageHomeBindModule {
     @ShopPageHomeScope
     @Binds
     abstract fun bindPlayPerformanceDashboardAnalytic(analytic: PlayPerformanceDashboardEntryPointAnalyticImpl): PlayPerformanceDashboardEntryPointAnalytic
-
-    @ShopPageHomeScope
-    @Binds
-    abstract fun bindContentCoachMarkSharedPref(contentCoachMarkSharedPref: ContentCoachMarkSharedPrefImpl): ContentCoachMarkSharedPref
 }
