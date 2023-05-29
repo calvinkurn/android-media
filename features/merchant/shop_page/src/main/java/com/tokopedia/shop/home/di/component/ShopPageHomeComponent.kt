6c795f1c205f8622e15ca18b584e37edb6a325a3
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-05-29 09:44:12 +0700

    fix: add contentcoachmarksharedpref module dependency on ShopPageHomeComponent

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/component/ShopPageHomeComponent.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/di/component/ShopPageHomeComponent.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.shop.home.di.component
 
+import com.tokopedia.content.common.di.ContentCoachMarkSharedPrefModule
 import com.tokopedia.shop.campaign.view.fragment.ShopPageCampaignFragment
 import com.tokopedia.shop.common.di.component.ShopComponent
 import com.tokopedia.shop.home.di.module.ShopPageHomeBindModule
@@ -14,7 +15,11 @@ import dagger.Component
  * Created by hendry on 18/01/18.
  */
 @ShopPageHomeScope
-@Component(modules = [ShopPageHomeModule::class, ShopPageHomeBindModule::class], dependencies = [ShopComponent::class])
+@Component(modules = [
+    ShopPageHomeModule::class,
+    ShopPageHomeBindModule::class,
+    ContentCoachMarkSharedPrefModule::class,
+ ], dependencies = [ShopComponent::class])
 interface ShopPageHomeComponent {
     fun inject(fragment: ShopPageHomeFragment?)
     fun inject(fragment: ShopPageCampaignFragment?)
