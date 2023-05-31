5a46cbb43b00927ec3b7df937ecd30ee57a7f0c5
varun0922 <varun.sharma@tokopedia.com>
2023-05-31 14:00:55 +0530

    adding checks for Android 8 and above

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/utils/PushRefreshWorker.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/utils/PushRefreshWorker.kt
@@ -2,15 +2,21 @@ package com.tokopedia.developer_options.utils
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.tokopedia.developer_options.presentation.service.DeleteFirebaseTokenService
+
 
 class PushRefreshWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
     override suspend fun doWork(): Result {
         val intent = Intent(this.applicationContext, DeleteFirebaseTokenService::class.java)
-        this.applicationContext.startService(intent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.applicationContext.startForegroundService(intent)
+        } else {
+            this.applicationContext.startService(intent)
+        }
         return Result.success()
     }
 }
