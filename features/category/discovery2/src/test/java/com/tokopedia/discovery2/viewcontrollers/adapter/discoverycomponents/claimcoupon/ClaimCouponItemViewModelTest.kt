afaa940e7481e3d7c2c9ac3a31155e2748ff1d75
rachit2598 <rachit.panwar@tokopedia.com>
2023-06-01 20:13:02 +0530

    detekt part -2

--- features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/claimcoupon/ClaimCouponItemViewModelTest.kt
+++ features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/claimcoupon/ClaimCouponItemViewModelTest.kt
@@ -3,12 +3,10 @@ package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.cla
 import android.app.Application
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.tokopedia.discovery2.Constant
 import com.tokopedia.discovery2.Constant.ClaimCouponConstant.DOUBLE_COLUMNS
 import com.tokopedia.discovery2.Constant.ClaimCouponConstant.HABIS
 import com.tokopedia.discovery2.Constant.ClaimCouponConstant.NOT_LOGGEDIN
 import com.tokopedia.discovery2.data.ComponentsItem
-import com.tokopedia.discovery2.data.DataItem
 import com.tokopedia.discovery2.data.claim_coupon.CatalogWithCouponList
 import com.tokopedia.discovery2.data.claimcoupon.RedeemCouponResponse
 import com.tokopedia.discovery2.usecase.ClaimCouponClickUseCase
@@ -138,7 +136,6 @@ class ClaimCouponItemViewModelTest {
 
     /**************************** getIsDouble() *******************************************/
 
-
     /**************************** redeemCoupon() *******************************************/
 
     @Test
@@ -158,11 +155,18 @@ class ClaimCouponItemViewModelTest {
         viewModel.claimCouponClickUseCase = claimCouponClickUseCase
         val applink = "applink"
         val code = "code"
-        val data = RedeemCouponResponse(hachikoRedeem =
-        RedeemCouponResponse.HachikoRedeem(coupons =
-        arrayListOf(RedeemCouponResponse.HachikoRedeem.Coupon(
-            appLink = applink, code = code
-        ))))
+        val data = RedeemCouponResponse(
+            hachikoRedeem =
+            RedeemCouponResponse.HachikoRedeem(
+                coupons =
+                arrayListOf(
+                    RedeemCouponResponse.HachikoRedeem.Coupon(
+                        appLink = applink,
+                        code = code
+                    )
+                )
+            )
+        )
         val list = ArrayList<CatalogWithCouponList>()
         val dataItem = CatalogWithCouponList(id = 12)
         list.add(dataItem)
@@ -181,11 +185,18 @@ class ClaimCouponItemViewModelTest {
         viewModel.claimCouponClickUseCase = claimCouponClickUseCase
         val applink = "applink"
         val code = "code"
-        val data = RedeemCouponResponse(hachikoRedeem =
-        RedeemCouponResponse.HachikoRedeem(coupons =
-        arrayListOf(RedeemCouponResponse.HachikoRedeem.Coupon(
-            appLink = applink, code = code
-        ))))
+        val data = RedeemCouponResponse(
+            hachikoRedeem =
+            RedeemCouponResponse.HachikoRedeem(
+                coupons =
+                arrayListOf(
+                    RedeemCouponResponse.HachikoRedeem.Coupon(
+                        appLink = applink,
+                        code = code
+                    )
+                )
+            )
+        )
         every { componentsItem.claimCouponList } returns null
         coEvery { viewModel.userSession.isLoggedIn } returns true
         coEvery { viewModel.claimCouponClickUseCase.redeemCoupon(any()) } returns data
@@ -201,11 +212,18 @@ class ClaimCouponItemViewModelTest {
         viewModel.claimCouponClickUseCase = claimCouponClickUseCase
         val applink = "applink"
         val code = "code"
-        val data = RedeemCouponResponse(hachikoRedeem =
-        RedeemCouponResponse.HachikoRedeem(coupons =
-        arrayListOf(RedeemCouponResponse.HachikoRedeem.Coupon(
-            appLink = applink, code = code
-        ))))
+        val data = RedeemCouponResponse(
+            hachikoRedeem =
+            RedeemCouponResponse.HachikoRedeem(
+                coupons =
+                arrayListOf(
+                    RedeemCouponResponse.HachikoRedeem.Coupon(
+                        appLink = applink,
+                        code = code
+                    )
+                )
+            )
+        )
         every { componentsItem.claimCouponList } returns arrayListOf()
         coEvery { viewModel.userSession.isLoggedIn } returns true
         coEvery { viewModel.claimCouponClickUseCase.redeemCoupon(any()) } returns data
@@ -232,8 +250,10 @@ class ClaimCouponItemViewModelTest {
     fun `redeemCoupon when isLoggedIn is true and coupons list empty`() {
         viewModel.userSession = userSession
         viewModel.claimCouponClickUseCase = claimCouponClickUseCase
-        val data = RedeemCouponResponse(hachikoRedeem =
-        RedeemCouponResponse.HachikoRedeem(coupons = arrayListOf()))
+        val data = RedeemCouponResponse(
+            hachikoRedeem =
+            RedeemCouponResponse.HachikoRedeem(coupons = arrayListOf())
+        )
         coEvery { userSession.isLoggedIn } returns true
         coEvery { claimCouponClickUseCase.redeemCoupon(any()) } returns data
 
@@ -275,7 +295,7 @@ class ClaimCouponItemViewModelTest {
         val applink = "tokopedia://discovery/deals"
         val data = arrayListOf(CatalogWithCouponList(appLink = applink))
         every { componentsItem.claimCouponList } returns data
-        every { viewModel.navigate(any(),any()) } just runs
+        every { viewModel.navigate(any(), any()) } just runs
 
         viewModel.setClick(mockk(), "")
 
@@ -283,7 +303,6 @@ class ClaimCouponItemViewModelTest {
     }
 
     /**************************** setClick() *******************************************/
-
 
     /**************************** getCouponAppLink() *******************************************/
 
@@ -309,11 +328,9 @@ class ClaimCouponItemViewModelTest {
         every { componentsItem.claimCouponList } returns null
 
         assertEquals(viewModel.getCouponAppLink(), "")
-
     }
 
     /**************************** getCouponAppLink() *******************************************/
-
 
     @After
     fun shutDown() {
