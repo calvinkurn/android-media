3abe54081f523569da49323c42ff923415a921a3
Vishnu Singhal <vishanu.singhal@tokopedia.com>
2023-06-01 15:40:19 +0530

    commit checck

--- features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/recommendation/common/RecommendationConstants.kt
+++ features/top_ads/top_ads_dashboard/src/main/java/com/tokopedia/topads/dashboard/recommendation/common/RecommendationConstants.kt
@@ -23,4 +23,5 @@ object RecommendationConstants {
     const val KEYWORD_TYPE_POSITIVE_PHRASE = "positive_phrase"
     const val KEYWORD_TYPE_POSITIVE_EXACT = "positive_exact"
     const val INVALID_INSIGHT_TYPE = -1
+    const val INVALID_INSIGHT_TYPE2 = -1
 }
