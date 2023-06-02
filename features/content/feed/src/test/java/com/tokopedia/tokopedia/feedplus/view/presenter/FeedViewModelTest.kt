49a8c41b3cce7aac4ce89f6816210c1d9b0691d2
meyta.taliti <meyta.taliti@tokopedia.com>
2023-06-02 17:16:05 +0700

    refactor: moving TrackVisitChannelBroadcasterUseCase to content/common module.

--- features/content/feed/src/test/java/com/tokopedia/tokopedia/feedplus/view/presenter/FeedViewModelTest.kt
+++ features/content/feed/src/test/java/com/tokopedia/tokopedia/feedplus/view/presenter/FeedViewModelTest.kt
@@ -6,6 +6,8 @@ import com.tokopedia.abstraction.base.view.adapter.model.LoadingModel
 import com.tokopedia.atc_common.domain.model.response.AddToCartDataModel
 import com.tokopedia.atc_common.domain.model.response.DataModel
 import com.tokopedia.atc_common.domain.usecase.coroutine.AddToCartUseCase
+import com.tokopedia.content.common.model.TrackVisitChannelResponse
+import com.tokopedia.content.common.usecase.TrackVisitChannelBroadcasterUseCase
 import com.tokopedia.createpost.common.domain.entity.SubmitPostData
 import com.tokopedia.feedcomponent.analytics.topadstracker.SendTopAdsUseCase
 import com.tokopedia.feedcomponent.data.feedrevamp.FeedXAuthor
@@ -19,7 +21,6 @@ import com.tokopedia.feedcomponent.data.feedrevamp.FeedXProduct
 import com.tokopedia.feedcomponent.data.pojo.FeedXTrackViewerResponse
 import com.tokopedia.feedcomponent.data.pojo.PostUpcomingCampaign
 import com.tokopedia.feedcomponent.data.pojo.UpcomingCampaignResponse
-import com.tokopedia.feedcomponent.data.pojo.VisitChannelTracking
 import com.tokopedia.feedcomponent.data.pojo.feed.contentitem.FollowCta
 import com.tokopedia.feedcomponent.data.pojo.feed.contentitem.Header
 import com.tokopedia.feedcomponent.data.pojo.shopmutation.FollowShop
@@ -31,7 +32,6 @@ import com.tokopedia.feedcomponent.domain.model.ResultItem
 import com.tokopedia.feedcomponent.domain.model.ShopCore
 import com.tokopedia.feedcomponent.domain.model.ShopFollowingEntity
 import com.tokopedia.feedcomponent.domain.model.ShopInfoById
-import com.tokopedia.feedcomponent.domain.usecase.FeedBroadcastTrackerUseCase
 import com.tokopedia.feedcomponent.domain.usecase.FeedXTrackViewerUseCase
 import com.tokopedia.feedcomponent.domain.usecase.GetDynamicFeedNewUseCase
 import com.tokopedia.feedcomponent.domain.usecase.GetFollowingUseCase
@@ -112,7 +112,7 @@ class FeedViewModelTest {
     private val testDispatcher = coroutineTestRule.dispatchers
 
     private val mockReport: SubmitReportContentUseCase = mockk(relaxed = true)
-    private val mockTrackChannel: FeedBroadcastTrackerUseCase = mockk(relaxed = true)
+    private val mockTrackChannel: TrackVisitChannelBroadcasterUseCase = mockk(relaxed = true)
     private val mockTrackViewer: FeedXTrackViewerUseCase = mockk(relaxed = true)
     private val mockLike: SubmitLikeContentUseCase = mockk(relaxed = true)
     private val mockDelete: SubmitActionContentUseCase = mockk(relaxed = true)
@@ -194,7 +194,7 @@ class FeedViewModelTest {
     @Test
     fun `track visit channel - success`() {
         val expected =
-            VisitChannelTracking.Response(reportVisitChannelTracking = VisitChannelTracking(success = true))
+            TrackVisitChannelResponse.Response(model = TrackVisitChannelResponse(success = true))
 
         coEvery { mockTrackChannel.executeOnBackground() } returns expected
 
@@ -1112,7 +1112,7 @@ class FeedViewModelTest {
                 dataList = mutableListOf(
                     Data(
                         shop = Shop(
-                            id = "12",
+                            id = "12"
                         ),
                         isFavorit = true
                     ),
@@ -1139,7 +1139,7 @@ class FeedViewModelTest {
                             )
                         )
                     )
-                ),
+                )
             )
         )
 
@@ -1281,7 +1281,7 @@ class FeedViewModelTest {
                 dataList = mutableListOf(
                     Data(
                         shop = Shop(
-                            id = "67890",
+                            id = "67890"
                         ),
                         isFavorit = true
                     ),
@@ -1313,7 +1313,7 @@ class FeedViewModelTest {
                             )
                         )
                     )
-                ),
+                )
             )
         )
 
@@ -1347,11 +1347,12 @@ class FeedViewModelTest {
                 assert((newListResult[3] as DynamicPostUiModel).feedXCard.followers.isFollowed)
                 assert(newListResult[4] == currentDataList[4])
                 assert((newListResult[5] as ShopRecomWidgetModel).shopRecomUiModel.items[0] == (currentDataList[5] as ShopRecomWidgetModel).shopRecomUiModel.items[0])
-                assert((newListResult[5] as ShopRecomWidgetModel).shopRecomUiModel.items[1].state == ShopRecomFollowState.UNFOLLOW )
+                assert((newListResult[5] as ShopRecomWidgetModel).shopRecomUiModel.items[1].state == ShopRecomFollowState.UNFOLLOW)
                 assert(newListResult[6] == currentDataList[6])
                 assert((newListResult[7] as TopadsHeadLineV2Model).feedXCard.followers.isFollowed)
                 assert(
-                    (newListResult[7] as TopadsHeadLineV2Model).cpmModel!!.data[0].cpm.cpmShop.isFollowed)
+                    (newListResult[7] as TopadsHeadLineV2Model).cpmModel!!.data[0].cpm.cpmShop.isFollowed
+                )
                 assert((newListResult[8] as TopadsHeadLineV2Model).feedXCard.followers.isFollowed)
                 assert(!(newListResult[9] as TopadsShopUiModel).dataList[0].isFavorit)
                 assert((newListResult[9] as TopadsShopUiModel).dataList[1] == (currentDataList[9] as TopadsShopUiModel).dataList[1])
@@ -1375,7 +1376,6 @@ class FeedViewModelTest {
                 )
             )
         )
-
 
         coEvery { mockFollowingUsecase(any()) } coAnswers { throw MessageErrorException("Failed") }
 
