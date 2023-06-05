7b63cdab8499b2a14ff01b74d1162dbfb62b5732
victor <victor@tokopedia.com>
2023-06-05 08:20:44 +0700

    enable affiliate commission in wishlist share utils

--- libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/tracker/PageType.kt
+++ libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/tracker/PageType.kt
@@ -3,6 +3,7 @@ package com.tokopedia.universal_sharing.tracker
 enum class PageType(val value: String) {
     PDP("pdp"),
     SHOP("shop"),
+    WISHLIST("wishlist"),
     OTHERS("others"),
     CAMPAIGN("campaign")
 }
