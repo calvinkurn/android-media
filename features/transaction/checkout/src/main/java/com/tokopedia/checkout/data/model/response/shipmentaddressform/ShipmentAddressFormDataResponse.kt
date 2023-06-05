7f69bf162f01256b8711b1d2a28349acc8a56989
hansenpw <hansen.wijaya@tokopedia.com>
2023-06-05 13:53:48 +0700

    fix platform fee conflict

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/model/response/shipmentaddressform/ShipmentAddressFormDataResponse.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/data/model/response/shipmentaddressform/ShipmentAddressFormDataResponse.kt
@@ -3,7 +3,6 @@ package com.tokopedia.checkout.data.model.response.shipmentaddressform
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.checkout.data.model.response.dynamicdata.ShipmentDynamicDataPassing
 import com.tokopedia.checkout.data.model.response.egold.EgoldAttributes
-import com.tokopedia.checkout.data.model.response.platformfee.ShipmentPlatformFee
 import com.tokopedia.purchase_platform.common.feature.coachmarkplus.CoachmarkPlusResponse
 import com.tokopedia.purchase_platform.common.feature.ethicaldrug.data.response.ImageUploadResponse
 import com.tokopedia.purchase_platform.common.feature.gifting.data.response.PopUp
