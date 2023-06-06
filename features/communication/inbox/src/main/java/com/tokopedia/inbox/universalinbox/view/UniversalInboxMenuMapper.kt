0b636d031466840d73aaa82528183eeecee045b4
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-06-06 14:43:41 +0700

    feat: add ab test variant for with section and without section

--- features/communication/inbox/src/main/java/com/tokopedia/inbox/universalinbox/view/UniversalInboxMenuMapper.kt
+++ features/communication/inbox/src/main/java/com/tokopedia/inbox/universalinbox/view/UniversalInboxMenuMapper.kt
@@ -11,6 +11,9 @@ import com.tokopedia.inbox.universalinbox.util.UniversalInboxResourceProvider
 import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.CHATBOT_TYPE
 import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.PAGE_SOURCE_KEY
 import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.PAGE_SOURCE_REVIEW_INBOX
+import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.ROLLENCE_KEY
+import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.ROLLENCE_TYPE_A
+import com.tokopedia.inbox.universalinbox.util.UniversalInboxValueUtil.ROLLENCE_TYPE_B
 import com.tokopedia.inbox.universalinbox.view.uimodel.MenuItemType
 import com.tokopedia.inbox.universalinbox.view.uimodel.UniversalInboxMenuSectionUiModel
 import com.tokopedia.inbox.universalinbox.view.uimodel.UniversalInboxMenuSeparatorUiModel
@@ -22,58 +25,55 @@ import com.tokopedia.inbox.universalinbox.view.uimodel.UniversalInboxWidgetUiMod
 import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
+import com.tokopedia.remoteconfig.abtest.AbTestPlatform
 import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
 class UniversalInboxMenuMapper @Inject constructor(
-    private val resourceProvider: UniversalInboxResourceProvider
+    private val resourceProvider: UniversalInboxResourceProvider,
+    private val abTestPlatform: AbTestPlatform
 ) {
-
     fun getStaticMenu(userSession: UserSessionInterface): List<Any> {
-        val staticMenuList = arrayListOf(
-            UniversalInboxMenuSectionUiModel(resourceProvider.getSectionChatTitle()),
+        val chatSectionList = getChatSectionList(userSession)
+        val othersSectionList = getOthersSectionList()
+        return chatSectionList + othersSectionList
+    }
+
+    // Generate Chat Section
+    private fun getChatSectionList(userSession: UserSessionInterface): List<Any> {
+        val chatSectionList = arrayListOf<Any>()
+        if (isVariantA()) {
+            chatSectionList.add(
+                UniversalInboxMenuSectionUiModel(
+                    resourceProvider.getSectionChatTitle()
+                )
+            )
+        }
+        chatSectionList.add(
             UniversalInboxMenuUiModel(
                 type = MenuItemType.CHAT_BUYER,
                 title = resourceProvider.getMenuChatBuyerTitle(),
                 icon = IconUnify.CHAT,
                 counter = Int.ZERO,
                 applink = getChatBuyerApplink()
-            ),
-            UniversalInboxMenuSectionUiModel(resourceProvider.getSectionOthersTitle()),
-            UniversalInboxMenuUiModel(
-                type = MenuItemType.DISCUSSION,
-                title = resourceProvider.getMenuDiscussionTitle(),
-                icon = IconUnify.DISCUSSION,
-                counter = Int.ZERO,
-                applink = getDiscussionApplink()
-            ),
-            UniversalInboxMenuUiModel(
-                type = MenuItemType.REVIEW,
-                title = resourceProvider.getMenuReviewTitle(),
-                icon = IconUnify.STAR,
-                counter = Int.ZERO,
-                applink = getReviewApplink()
-            ),
-            UniversalInboxMenuSeparatorUiModel(),
-            UniversalInboxTopAdsBannerUiModel()
-        ).also {
-            if (userSession.hasShop()) {
-                it.add(2,
-                    UniversalInboxMenuUiModel(
-                        type = MenuItemType.CHAT_SELLER,
-                        title = resourceProvider.getMenuChatSellerTitle(),
-                        icon = IconUnify.SHOP,
-                        counter = Int.ZERO,
-                        applink = getChatSellerApplink(),
-                        additionalInfo = UniversalInboxShopInfoUiModel(
-                            avatar = userSession.shopAvatar,
-                            shopName = userSession.shopName
-                        )
+            )
+        )
+        if (userSession.hasShop()) {
+            chatSectionList.add(
+                UniversalInboxMenuUiModel(
+                    type = MenuItemType.CHAT_SELLER,
+                    title = resourceProvider.getMenuChatSellerTitle(),
+                    icon = IconUnify.SHOP,
+                    counter = Int.ZERO,
+                    applink = getChatSellerApplink(),
+                    additionalInfo = UniversalInboxShopInfoUiModel(
+                        avatar = userSession.shopAvatar,
+                        shopName = userSession.shopName
                     )
                 )
-            }
+            )
         }
-        return staticMenuList
+        return chatSectionList
     }
 
     private fun getChatBuyerApplink(): String {
@@ -84,13 +84,48 @@ class UniversalInboxMenuMapper @Inject constructor(
         return "${ApplinkConst.TOP_CHAT}?${ApplinkConst.Inbox.PARAM_ROLE}=${ApplinkConst.Inbox.VALUE_ROLE_SELLER}"
     }
 
+    private fun getOthersSectionList(): List<Any> {
+        val othersSectionList = arrayListOf<Any>()
+        if (isVariantA()) {
+            othersSectionList.add(
+                UniversalInboxMenuSectionUiModel(
+                    resourceProvider.getSectionOthersTitle()
+                )
+            )
+        }
+        othersSectionList.apply {
+            add(
+                UniversalInboxMenuUiModel(
+                    type = MenuItemType.DISCUSSION,
+                    title = resourceProvider.getMenuDiscussionTitle(),
+                    icon = IconUnify.DISCUSSION,
+                    counter = Int.ZERO,
+                    applink = getDiscussionApplink()
+                )
+            )
+            add(
+                UniversalInboxMenuUiModel(
+                    type = MenuItemType.REVIEW,
+                    title = resourceProvider.getMenuReviewTitle(),
+                    icon = IconUnify.STAR,
+                    counter = Int.ZERO,
+                    applink = getReviewApplink()
+                )
+            )
+            add(UniversalInboxMenuSeparatorUiModel())
+            add(UniversalInboxTopAdsBannerUiModel())
+        }
+        return othersSectionList
+    }
+
     private fun getDiscussionApplink(): String {
         return ApplinkConstInternalGlobal.INBOX_TALK
     }
 
     private fun getReviewApplink(): String {
         return Uri.parse(ApplinkConst.REPUTATION).buildUpon().appendQueryParameter(
-            PAGE_SOURCE_KEY, PAGE_SOURCE_REVIEW_INBOX
+            PAGE_SOURCE_KEY,
+            PAGE_SOURCE_REVIEW_INBOX
         ).build().toString()
     }
 
@@ -147,5 +182,23 @@ class UniversalInboxMenuMapper @Inject constructor(
                 type = this.type
             )
         }
+    }
+
+    private fun getVariant(): VariantType {
+        val variantAB = abTestPlatform.getString(ROLLENCE_KEY, ROLLENCE_TYPE_A)
+        return if (variantAB == ROLLENCE_TYPE_A) {
+            VariantType.INBOX_VAR_A
+        } else {
+            VariantType.INBOX_VAR_B
+        }
+    }
+
+    private fun isVariantA(): Boolean {
+        return getVariant() == VariantType.INBOX_VAR_A
+    }
+
+    enum class VariantType(val type: String) {
+        INBOX_VAR_A(ROLLENCE_TYPE_A),
+        INBOX_VAR_B(ROLLENCE_TYPE_B)
     }
 }
