3a0e11390a47f41c2e531a85376149c9fbe0381d
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-06-07 15:42:24 +0700

    `revert`: MainActivity.kt

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return "tokopedia://play-shorts"
+        return ""
     }
 
     data class Model(
