0098c25d0f1ba8bd0d7d5fbe0f8c39cf71fd653b
varun0922 <varun.sharma@tokopedia.com>
2023-06-07 12:28:53 +0530

    adding the code for the starting the service

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/service/DeleteFirebaseTokenService.java
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/service/DeleteFirebaseTokenService.java
@@ -2,11 +2,17 @@ package com.tokopedia.developer_options.presentation.service;
 
 import android.annotation.SuppressLint;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -34,6 +40,20 @@ public class DeleteFirebaseTokenService extends IntentService {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "Service created");
+        if (Build.VERSION.SDK_INT >= 26) {
+            String CHANNEL_ID = "my_channel_01";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+
+            startForeground(1, notification);
+        }
     }
 
     @Override
