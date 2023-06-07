7897a5b666ef3bba7df71638478d7820c994dcef
fajarca <fajar.azhar@tokopedia.com>
2023-06-07 15:18:59 +0700

    Add test case

--- features/merchant/shop_page/src/test/java/com/tokopedia/shop/campaign/view/viewmodel/VoucherDetailViewModelTest.kt
+++ features/merchant/shop_page/src/test/java/com/tokopedia/shop/campaign/view/viewmodel/VoucherDetailViewModelTest.kt
@@ -2,6 +2,7 @@ package com.tokopedia.shop.campaign.view.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.shop.campaign.domain.entity.RedeemPromoVoucherResult
 import com.tokopedia.shop.campaign.domain.entity.VoucherDetail
 import com.tokopedia.shop.campaign.domain.usecase.GetVoucherDetailUseCase
 import com.tokopedia.shop.campaign.domain.usecase.RedeemPromoVoucherUseCase
@@ -12,6 +13,7 @@ import com.tokopedia.usecase.coroutines.Fail
 import com.tokopedia.usecase.coroutines.Success
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.impl.annotations.RelaxedMockK
 import org.junit.Assert.*
 import org.junit.Before
@@ -81,9 +83,89 @@ class VoucherDetailViewModelTest {
     }
     //endregion
 
+    //region claimPromoVoucher
+    @Test
+    fun `When claim voucher to remote success, observer should successfully receive data`() {
+        //Given
+        val voucherId : Long = 1
+        val isGift = 0
+
+        val voucherDetail = buildVoucherDetail().copy(id = voucherId, isGift = isGift)
+
+        val slug = "JUNCASHBACKSUPER"
+        coEvery { getPromoVoucherDetailUseCase.execute(slug) } returns voucherDetail
+
+        val param = RedeemPromoVoucherUseCase.Param(voucherId, isGift)
+        val mockedResponse = RedeemPromoVoucherResult(
+            promoId = 1,
+            voucherCode = "AABBCC",
+            redeemMessage = "Sukses di klaim"
+        )
+        coEvery { redeemPromoVoucherUseCase.execute(param) } returns mockedResponse
+
+        //When
+        viewModel.getVoucherDetail(slug)
+        viewModel.claimPromoVoucher()
+
+        //Then
+        val actual = viewModel.redeemResult.getOrAwaitValue()
+        assertEquals(Success(mockedResponse), actual)
+    }
+
+    @Test
+    fun `When claiming voucher but voucher detail is null, should not invoke remote call to claim voucher`() {
+        //Given
+        val voucherId : Long = 1
+        val isGift = 0
+        val error = MessageErrorException("Server error")
+
+        val slug = "JUNCASHBACKSUPER"
+        coEvery { getPromoVoucherDetailUseCase.execute(slug) } throws error
+
+        val param = RedeemPromoVoucherUseCase.Param(voucherId, isGift)
+        val mockedResponse = RedeemPromoVoucherResult(
+            promoId = 1,
+            voucherCode = "AABBCC",
+            redeemMessage = "Sukses di klaim"
+        )
+        coEvery { redeemPromoVoucherUseCase.execute(param) } returns mockedResponse
+
+        //When
+        viewModel.getVoucherDetail(slug)
+        viewModel.claimPromoVoucher()
+
+        //Then
+        coVerify(exactly = 0) { redeemPromoVoucherUseCase.execute(param) }
+    }
+
+    @Test
+    fun `When claim voucher to remote failed, observer should receive error`() {
+        //Given
+        val voucherId : Long = 1
+        val isGift = 0
+        val error = MessageErrorException("Server error")
+
+        val voucherDetail = buildVoucherDetail().copy(id = voucherId, isGift = isGift)
+
+        val slug = "JUNCASHBACKSUPER"
+        coEvery { getPromoVoucherDetailUseCase.execute(slug) } returns voucherDetail
+
+        val param = RedeemPromoVoucherUseCase.Param(voucherId, isGift)
+        coEvery { redeemPromoVoucherUseCase.execute(param) } throws error
+
+        //When
+        viewModel.getVoucherDetail(slug)
+        viewModel.claimPromoVoucher()
+
+        //Then
+        val actual = viewModel.redeemResult.getOrAwaitValue()
+        assertEquals(Fail(error), actual)
+    }
+    //endregion
+
     //region usePromoVoucher
     @Test
-    fun `When use voucher from remote success, observer should successfully receive data`() {
+    fun `When use voucher from remote success, observer should successfully receive true as a result`() {
         //Given
         val shopId = "11334455"
         val voucherCode = "voucher-code"
@@ -100,6 +182,26 @@ class VoucherDetailViewModelTest {
         val actual = viewModel.useVoucherResult.getOrAwaitValue()
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun `When use voucher from remote failed, observer should successfully receive false as a result`() {
+        //Given
+        val shopId = "11334455"
+        val voucherCode = "voucher-code"
+        val param = UsePromoVoucherUseCase.Param(shopId, voucherCode)
+        val mockedResponse = false
+        val expected = Success(mockedResponse)
+
+        coEvery { usePromoVoucherUseCase.execute(param) } returns mockedResponse
+
+        //When
+        viewModel.usePromoVoucher(shopId, voucherCode)
+
+        //Then
+        val actual = viewModel.useVoucherResult.getOrAwaitValue()
+        assertEquals(expected, actual)
+    }
+
 
     @Test
     fun `When use voucher from remote failed, observer should receive error`() {
