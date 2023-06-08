4b8ccb596a40737dd8f69d9a45710d31042308b0
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-06-08 09:49:05 +0700

    move onNeedUpdateRequestData(); onInsuranceChecked

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentItemViewHolder.java
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentItemViewHolder.java
@@ -1573,7 +1573,6 @@ public class ShipmentItemViewHolder extends RecyclerView.ViewHolder implements S
                         mActionListener.onInsuranceCheckedForTrackingAnalytics();
                     }
                     mActionListener.onInsuranceChecked(getAdapterPosition());
-                    mActionListener.onNeedUpdateRequestData();
                     if (saveStateDebounceListener != null) {
                         saveStateDebounceListener.onNeedToSaveState(shipmentCartItemModel);
                     }
