c9f7dec553b73f7d5523165f0f084c3ed4f66d8c
rama.putra <rama.putra@tokopedia.com>
2023-06-08 14:18:15 +0700

    remove unused model

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/branchlink/data/model/BranchLinkResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.universal_sharing.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class BranchLinkResponse(
-        @SerializedName("\$android_deeplink_path")
-        val android_deeplink: String
-)
