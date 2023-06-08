a6f13af225ab20a14fafcec1a0d5099d7bc5372c
reivinoktavianus <oktavianusreivin@gmail.com>
2023-06-08 14:22:28 +0700

    feat: Remove updated code

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return ApplinkConst.SHOP_PENALTY
+        return ""
     }
 
     data class Model(
