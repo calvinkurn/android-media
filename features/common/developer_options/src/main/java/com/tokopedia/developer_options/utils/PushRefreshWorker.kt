0c364307b171b7bc9e8436553c631aa772213801
varun0922 <varun.sharma@tokopedia.com>
2023-06-09 10:52:23 +0530

    adding the code to worker from service

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/utils/PushRefreshWorker.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/utils/PushRefreshWorker.kt
@@ -1,22 +1,45 @@
 package com.tokopedia.developer_options.utils
 
 import android.content.Context
-import android.content.Intent
-import android.os.Build
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
 import com.tokopedia.developer_options.presentation.service.DeleteFirebaseTokenService
+import com.tokopedia.fcmcommon.service.SyncFcmTokenService.Companion.startService
 
 
 class PushRefreshWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
+    private val TAG = "DeleteFcmTokenWorker"
+
     override suspend fun doWork(): Result {
-        val intent = Intent(this.applicationContext, DeleteFirebaseTokenService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            this.applicationContext.startForegroundService(intent)
-        } else {
-            this.applicationContext.startService(intent)
-        }
+//        val intent = Intent(this.applicationContext, DeleteFirebaseTokenService::class.java)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            this.applicationContext.startForegroundService(intent)
+//        } else {
+//            this.applicationContext.startService(intent)
+//        }
+        Log.d(TAG, "Worker started")
+        FirebaseMessaging.getInstance().deleteToken()
+            .addOnCompleteListener { p0 ->
+                Log.d(TAG, "Task success: " + p0.isSuccessful)
+                Log.d(TAG, "Token deleted")
+                retrieveNewToken()
+            }
         return Result.success()
+    }
+
+    private fun retrieveNewToken() {
+        try {
+            FirebaseMessaging.getInstance().token.addOnCompleteListener { p0 ->
+                Log.d(TAG, "Task success: " + p0.isSuccessful)
+                startService(applicationContext)
+            }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 }
