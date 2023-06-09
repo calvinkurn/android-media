096dfce10ff67da41c89a0b1e03b96d194271fea
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-06-09 21:26:51 +0700

    adjust bottom sheet implementation

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/oneKycSdk/GotoKycSdkBottomSheet.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/oneKycSdk/GotoKycSdkBottomSheet.kt
@@ -1,47 +1,35 @@
 package com.tokopedia.kyc_centralized.ui.gotoKyc.oneKycSdk
 
 import android.app.Activity
-import android.view.View
 import com.gojek.kyc.plus.card.KycPlusCard
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.tokopedia.kotlin.extensions.view.showWithCondition
-import com.tokopedia.kyc_centralized.R
-import com.tokopedia.kyc_centralized.databinding.LayoutGotoKycBottomSheetBinding
 
 class GotoKycSdkBottomSheet(
     private val activity: Activity,
-    private val view: View,
-    private val onDismiss: (() -> Unit)?,
-    private val showCloseIcon : Boolean = true
+    private val bottomSheetDialog: BottomSheetDialog,
+    private val onDismiss: (() -> Unit)?
 ) : KycPlusCard {
 
-    val bottomSheet = BottomSheetDialog(activity, R.style.BottomSheetDialogStyle)
-
     override fun dismiss() {
-        bottomSheet.dismiss()
+        if (!activity.isFinishing) {
+            bottomSheetDialog.dismiss()
+        }
     }
 
     override fun isShowing(): Boolean {
-        return bottomSheet.isShowing
+        return if (!activity.isFinishing) {
+            bottomSheetDialog.isShowing
+        } else {
+            false
+        }
     }
 
     override fun show() {
-        val binding = LayoutGotoKycBottomSheetBinding.inflate(activity.layoutInflater)
-
-        binding.root.addView(view)
-
-        binding.icDismiss.setOnClickListener {
-            bottomSheet.dismiss()
-        }
-        binding.spacing.showWithCondition(!showCloseIcon)
-        binding.icDismiss.showWithCondition(showCloseIcon)
-
-        bottomSheet.setContentView(binding.root)
-
-        bottomSheet.show()
-
-        bottomSheet.setOnDismissListener {
-            onDismiss?.invoke()
+        if (!activity.isFinishing) {
+            bottomSheetDialog.show()
+            bottomSheetDialog.setOnDismissListener {
+                onDismiss?.invoke()
+            }
         }
     }
 
