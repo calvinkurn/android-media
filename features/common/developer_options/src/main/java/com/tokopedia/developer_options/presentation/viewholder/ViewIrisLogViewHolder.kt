3ac2ea95e459af1bcae23a84098525a8b3b23abf
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-06-09 22:35:58 +0700

    Add more trackers

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/ViewIrisLogViewHolder.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/ViewIrisLogViewHolder.kt
@@ -5,14 +5,14 @@ import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.analyticsdebugger.debugger.IrisLogger
 import com.tokopedia.developer_options.R
-import com.tokopedia.developer_options.presentation.model.SystemNonSystemAppsUiModel
 import com.tokopedia.developer_options.presentation.model.ViewIrisLogUiModel
+import com.tokopedia.developer_options.tracker.DevOpsTracker
+import com.tokopedia.developer_options.tracker.DevopsFeature
 import com.tokopedia.unifycomponents.UnifyButton
 
 class ViewIrisLogViewHolder(
     itemView: View
-): AbstractViewHolder<ViewIrisLogUiModel>(itemView)
-{
+) : AbstractViewHolder<ViewIrisLogUiModel>(itemView) {
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.item_view_iris_log
@@ -23,9 +23,11 @@ class ViewIrisLogViewHolder(
         val btnViewIrisSendLog = itemView.findViewById<UnifyButton>(R.id.view_iris_send_log_btn)
         itemView.context.apply {
             btnViewIrisSaveLog.setOnClickListener {
+                DevOpsTracker.trackEntryEvent(DevopsFeature.VIEW_IRIS_SAVE_LOG)
                 IrisLogger.getInstance(this).openSaveActivity()
             }
             btnViewIrisSendLog.setOnClickListener {
+                DevOpsTracker.trackEntryEvent(DevopsFeature.VIEW_IRIS_SEND_LOG)
                 IrisLogger.getInstance(this).openSendActivity()
             }
         }
