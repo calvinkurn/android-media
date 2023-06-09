8115263f541c53cc9e309254faac4122a7083177
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-06-09 21:41:25 +0700

    only add necessary header

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/oneKycSdk/GotoKycInterceptor.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/oneKycSdk/GotoKycInterceptor.kt
@@ -1,7 +1,9 @@
 package com.tokopedia.kyc_centralized.ui.gotoKyc.oneKycSdk
 
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
+import com.tokopedia.config.GlobalConfig
 import com.tokopedia.kyc_centralized.util.KycSharedPreference
+import com.tokopedia.user.session.UserSessionInterface
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -9,19 +11,26 @@ import javax.inject.Inject
 
 @ActivityScope
 class GotoKycInterceptor @Inject constructor(
-    private val kycSharedPreference: KycSharedPreference
+    private val kycSharedPreference: KycSharedPreference,
+    private val userSessionInterface: UserSessionInterface
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request()
         val requestBuilder: Request.Builder = request.newBuilder()
-        if (request.header(KEY_X_PROJECT_ID) == null) {
-            requestBuilder.addHeader(KEY_X_PROJECT_ID, kycSharedPreference.getProjectId())
+        requestBuilder.apply {
+            addHeader(KEY_X_PROJECT_ID, kycSharedPreference.getProjectId())
+            addHeader(KEY_X_DEVICE, "$VALUE_ANDROID-${GlobalConfig.VERSION_NAME}")
+            addHeader(KEY_ACCOUNTS_AUTHORIZATION, "$VALUE_BEARER ${userSessionInterface.accessToken}")
         }
         return chain.proceed(requestBuilder.build())
     }
 
     companion object {
+        private const val VALUE_ANDROID = "android"
+        private const val VALUE_BEARER = "Bearer"
+        private const val KEY_ACCOUNTS_AUTHORIZATION = "Accounts-Authorization"
+        private const val KEY_X_DEVICE = "X-Device"
         private const val KEY_X_PROJECT_ID = "x-project-id"
     }
 }
