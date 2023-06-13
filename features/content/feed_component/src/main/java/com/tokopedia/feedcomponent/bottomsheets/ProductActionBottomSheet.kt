9602f0d8f8795622e0c391d13378fdd9fcaa425a
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-06-13 13:57:21 +0700

    `refactor`: migrate ProductActionBottomSheet.kt to using viewBinding

--- features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/bottomsheets/ProductActionBottomSheet.kt
+++ features/content/feed_component/src/main/java/com/tokopedia/feedcomponent/bottomsheets/ProductActionBottomSheet.kt
@@ -4,20 +4,65 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tokopedia.feedcomponent.R
+import com.tokopedia.feedcomponent.databinding.ProductTagMenuOptionsBinding
+import com.tokopedia.kotlin.extensions.orTrue
 import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.unifycomponents.BottomSheetUnify
-import kotlinx.android.synthetic.main.product_tag_menu_options.*
 
 class ProductActionBottomSheet : BottomSheetUnify() {
+
+    private var _binding: ProductTagMenuOptionsBinding? = null
+    private val binding: ProductTagMenuOptionsBinding
+        get() = _binding!!
 
     var addToCartCB: (() -> Unit)? = null
     var addToWIshListCB: (() -> Unit)? = null
     var shareProductCB: (() -> Unit)? = null
     var isLogin = false
 
-    companion object {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ProductTagMenuOptionsBinding.inflate(layoutInflater)
+        setChild(binding.root)
 
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        isLogin = arguments?.getBoolean(KEY_IS_LOGIN).orTrue()
+
+        with(binding) {
+            addToWishList.showWithCondition(isLogin)
+            div1.showWithCondition(isLogin)
+            div2.showWithCondition(isLogin)
+            addToCart.showWithCondition(isLogin)
+
+            addToWishList.setOnClickListener {
+                addToWIshListCB?.invoke()
+                dismiss()
+            }
+            addToCart.setOnClickListener {
+                addToCartCB?.invoke()
+                dismiss()
+            }
+            shareProduct.setOnClickListener {
+                shareProductCB?.invoke()
+                dismiss()
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val KEY_IS_LOGIN = "isLogin"
         fun newInstance(login: Bundle): ProductActionBottomSheet {
             val frag = ProductActionBottomSheet()
             frag.arguments = login
@@ -25,36 +70,4 @@ class ProductActionBottomSheet : BottomSheetUnify() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val contentView = View.inflate(context, R.layout.product_tag_menu_options, null)
-        setChild(contentView)
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        isLogin = arguments?.getBoolean("isLogin")?:true
-        addToWishList?.showWithCondition(isLogin)
-        div1?.showWithCondition(isLogin)
-        div2?.showWithCondition(isLogin)
-        addToCart?.showWithCondition(isLogin)
-
-        addToWishList?.setOnClickListener {
-            addToWIshListCB?.invoke()
-            dismiss()
-        }
-        addToCart?.setOnClickListener {
-            addToCartCB?.invoke()
-            dismiss()
-        }
-        shareProduct?.setOnClickListener {
-            shareProductCB?.invoke()
-            dismiss()
-        }
-
-    }
 }
