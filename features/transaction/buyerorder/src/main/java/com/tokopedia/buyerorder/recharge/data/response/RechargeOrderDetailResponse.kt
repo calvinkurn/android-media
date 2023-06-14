1bfcbe11ba5b17322d2ed4997bae85c17aab6b3d
firmankagami <firmanda.nugroho@tokopedia.com>
2023-06-14 11:38:48 +0700

    feat: remove export

--- features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/recharge/data/response/RechargeOrderDetailResponse.kt
+++ features/transaction/buyerorder/src/main/java/com/tokopedia/buyerorder/recharge/data/response/RechargeOrderDetailResponse.kt
@@ -1,10 +1,7 @@
 package com.tokopedia.buyerorder.recharge.data.response
 
 import android.annotation.SuppressLint
-import android.os.Parcelable
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
 /**
  * @author by furqan on 26/10/2021
