0b76baef98693da318d6dbccf771ad64a9c44beb
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-06-15 14:10:07 +0700

    feat: remove unused instrumentation test class

--- features/content/people/src/androidTest/java/com/tokopedia/people/activity/UserProfileEmptyActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.people.activity
-
-import androidx.appcompat.app.AppCompatActivity
-
-/**
- * Created By : Jonathan Darwin on June 14, 2023
- */
-class UserProfileEmptyActivity : AppCompatActivity()
