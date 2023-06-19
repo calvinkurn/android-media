94432e60342392ee9446541f18c5ca0895e075d1
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-06-19 12:17:57 +0700

    `refactor`: remove kotlin-synthetic `RelatedProductAdapter.kt`

--- features/content/createpost/src/main/java/com/tokopedia/createpost/view/adapter/RelatedProductAdapter.kt
+++ features/content/createpost/src/main/java/com/tokopedia/createpost/view/adapter/RelatedProductAdapter.kt
@@ -1,21 +1,26 @@
 package com.tokopedia.createpost.view.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.createpost.common.TYPE_AFFILIATE
 import com.tokopedia.createpost.common.view.viewmodel.RelatedProductItem
 import com.tokopedia.createpost.createpost.R
 import com.tokopedia.kotlin.extensions.view.*
-import kotlinx.android.synthetic.main.item_af_related_product.view.*
+import com.tokopedia.unifyprinciples.Typography
 
 /**
  * @author by milhamj on 21/02/19.
  */
-class RelatedProductAdapter(val listener: RelatedProductListener? = null, val type: String = "") :
+class RelatedProductAdapter(
+    private val listener: RelatedProductListener? = null,
+    val type: String = ""
+) :
     RecyclerView.Adapter<RelatedProductAdapter.ViewHolder>() {
 
     private val emptyItem: RelatedProductItem = RelatedProductItem(EMPTY_ITEM_ID)
@@ -38,7 +43,7 @@ class RelatedProductAdapter(val listener: RelatedProductListener? = null, val ty
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_af_related_product, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, listener)
     }
 
     override fun getItemCount(): Int = list.size
@@ -48,39 +53,11 @@ class RelatedProductAdapter(val listener: RelatedProductListener? = null, val ty
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val element = list[position]
+        holder.bindData(list[position], type)
 
-        if (element.id == EMPTY_ITEM_ID) {
-            holder.itemView.thumbnail.loadImageDrawable(com.tokopedia.resources.common.R.drawable.ic_system_action_addimage_grayscale_62)
-            holder.itemView.delete.hide()
-            holder.itemView.separatorBottom.hide()
-            holder.itemView.separatorBottomEmpty.show()
-            holder.itemView.setOnClickListener {
-                listener?.onEmptyProductClick()
-            }
-        } else {
-            holder.itemView.thumbnail.loadImageRounded(element.image, IMAGE_RADIUS)
-            holder.itemView.delete.showWithCondition(type != TYPE_PREVIEW)
-            holder.itemView.separatorBottom.show()
-            holder.itemView.separatorBottomEmpty.hide()
-            holder.itemView.setOnClickListener { }
-        }
-        holder.itemView.name.text = element.name
-        holder.itemView.price.text = element.price
-        holder.itemView.price.setTextColor(
-            MethodChecker.getColor(
-                holder.itemView.context,
-                if (element.type == TYPE_AFFILIATE) com.tokopedia.unifyprinciples.R.color.Unify_B500
-                else com.tokopedia.unifyprinciples.R.color.Unify_Y500
-            )
-        )
-        holder.itemView.delete.setOnClickListener {
-            listener?.onItemDeleted(holder.adapterPosition)
-
-            if (list.isEmpty() && shouldAddEmpty()) {
-                list.add(emptyItem)
-                notifyItemInserted(list.size)
-            }
+        if (list.isEmpty() && type != TYPE_PREVIEW) {
+            list.add(emptyItem)
+            notifyItemInserted(list.size)
         }
     }
 
@@ -102,7 +79,53 @@ class RelatedProductAdapter(val listener: RelatedProductListener? = null, val ty
 
     private fun shouldAddEmpty() = type != TYPE_PREVIEW
 
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v)
+    class ViewHolder(
+        v: View,
+        private val listener: RelatedProductListener?
+    ) : RecyclerView.ViewHolder(v) {
+
+        private val rootView: View = v
+        private val context: Context = v.context
+        private val thumbnail: ImageView = v.findViewById(R.id.thumbnail)
+        private val delete: ImageView = v.findViewById(R.id.delete)
+        private val separatorBottom: View = v.findViewById(R.id.separatorBottom)
+        private val separatorBottomEmpty: View = v.findViewById(R.id.separatorBottomEmpty)
+        private val name: Typography = v.findViewById(R.id.name)
+        private val price: Typography = v.findViewById(R.id.price)
+
+        fun bindData(
+            element: RelatedProductItem,
+            type: String,
+        ) {
+            if (element.id == EMPTY_ITEM_ID) {
+                thumbnail.loadImageDrawable(com.tokopedia.resources.common.R.drawable.ic_system_action_addimage_grayscale_62)
+                delete.hide()
+                separatorBottom.hide()
+                separatorBottomEmpty.show()
+                rootView.setOnClickListener {
+                    listener?.onEmptyProductClick()
+                }
+            } else {
+                thumbnail.loadImageRounded(element.image, IMAGE_RADIUS)
+                delete.showWithCondition(type != TYPE_PREVIEW)
+                separatorBottom.show()
+                separatorBottomEmpty.hide()
+                rootView.setOnClickListener { }
+            }
+            name.text = element.name
+            price.text = element.price
+            price.setTextColor(
+                MethodChecker.getColor(
+                    context,
+                    if (element.type == TYPE_AFFILIATE) com.tokopedia.unifyprinciples.R.color.Unify_B500
+                    else com.tokopedia.unifyprinciples.R.color.Unify_Y500
+                )
+            )
+            delete.setOnClickListener {
+                listener?.onItemDeleted(adapterPosition)
+            }
+        }
+    }
 
     interface RelatedProductListener {
         fun onEmptyProductClick()
