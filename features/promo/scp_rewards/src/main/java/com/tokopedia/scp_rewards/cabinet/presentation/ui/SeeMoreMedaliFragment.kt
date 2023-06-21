68e783895fcefe082dd391cd2991a08e2006723f
Harshit Jain <harshit.jain@tokopedia.com>
2023-06-21 11:01:22 +0530

    created see more medali page

---
+++ features/promo/scp_rewards/src/main/java/com/tokopedia/scp_rewards/cabinet/presentation/ui/SeeMoreMedaliFragment.kt
@@ -0,0 +1,202 @@
+package com.tokopedia.scp_rewards.cabinet.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.tokopedia.abstraction.base.view.adapter.Visitable
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.base.view.recyclerview.EndlessRecyclerViewScrollListener
+import com.tokopedia.abstraction.base.view.viewmodel.ViewModelFactory
+import com.tokopedia.applink.internal.ApplinkConstInternalPromo.MEDALI_QUERY_PARAM
+import com.tokopedia.scp_rewards.R
+import com.tokopedia.scp_rewards.cabinet.di.MedalCabinetComponent
+import com.tokopedia.scp_rewards.cabinet.domain.model.ScpRewardsGetUserMedalisResponse
+import com.tokopedia.scp_rewards.cabinet.mappers.MedaliListMapper
+import com.tokopedia.scp_rewards.cabinet.presentation.adapter.SeeMoreMedalAdapter
+import com.tokopedia.scp_rewards_widgets.common.model.LoadingModel
+import com.tokopedia.scp_rewards_widgets.common.model.LoadingMoreModel
+import com.tokopedia.scp_rewards_widgets.medal.SeeMoreMedalTypeFactory
+import com.tokopedia.scp_rewards.cabinet.presentation.viewmodel.SeeMoreMedaliViewModel
+import com.tokopedia.scp_rewards.common.data.InfiniteLoading
+import com.tokopedia.scp_rewards.common.data.Loading
+import com.tokopedia.scp_rewards.common.data.Success
+import com.tokopedia.scp_rewards.common.utils.EARNED_BADGE
+import com.tokopedia.scp_rewards.databinding.SeeMoreMedaliFragmentBinding
+import com.tokopedia.scp_rewards_widgets.common.GridSpacing
+import com.tokopedia.scp_rewards_widgets.medal.MedalViewHolder
+import javax.inject.Inject
+
+class SeeMoreMedaliFragment : BaseDaggerFragment() {
+
+    companion object {
+        private const val FULL_COLUMN = 1
+        private const val ONE_THIRD_COLUMN = 3
+        private const val GRID_VERTICAL_SPACING = 22
+    }
+
+    private var binding: SeeMoreMedaliFragmentBinding? = null
+
+    private val medalAdapter: SeeMoreMedalAdapter by lazy {
+        SeeMoreMedalAdapter(SeeMoreMedalTypeFactory())
+    }
+    private var rvLayoutManager: LayoutManager? = null
+
+    private var badgeType = EARNED_BADGE
+
+    private var rvScrollListener: EndlessRecyclerViewScrollListener? = null
+
+    @Inject
+    @JvmField
+    var viewModelFactory: ViewModelFactory? = null
+
+    private val viewModel: SeeMoreMedaliViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory!!)[SeeMoreMedaliViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        badgeType = activity?.intent?.data?.getQueryParameter(MEDALI_QUERY_PARAM) ?: EARNED_BADGE
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = SeeMoreMedaliFragmentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getUserMedalis()
+        setupHeader()
+        setupRecyclerView()
+        setupViewModelObservers()
+    }
+
+    private fun setupHeader() {
+        binding?.header?.apply {
+            title = getHeaderTitle()
+            setNavigationOnClickListener {
+                activity?.onBackPressed()
+            }
+        }
+    }
+
+    private fun getHeaderTitle(): String {
+        return if (badgeType == EARNED_BADGE)
+            context?.getString(R.string.earned_medali_title) ?: ""
+        else
+            context?.getString(R.string.progress_medali_title) ?: ""
+    }
+
+    private fun setupViewModelObservers() {
+        viewModel.medalLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is Success<*> -> {
+                    onSuccessResponse(it.data as ScpRewardsGetUserMedalisResponse)
+                }
+
+                is Loading -> {
+                    onLoadingState()
+                }
+
+                is InfiniteLoading -> {
+                    onLoadingMoreState()
+                }
+
+                else -> {}
+            }
+        }
+        viewModel.hasNextLiveData.observe(viewLifecycleOwner) {
+            if (!it) {
+                removeRecyclerViewScrollListener()
+            }
+        }
+    }
+
+    private fun onSuccessResponse(response: ScpRewardsGetUserMedalisResponse) {
+        viewModel.apply {
+            if (pageCount == 1) {
+                visitableList.clear()
+            } else {
+                if (visitableList.last() is LoadingMoreModel) {
+                    visitableList.removeLast()
+                }
+            }
+            visitableList.addAll(
+                MedaliListMapper.getMedalList(response, badgeType)
+            )
+            submitAdapterList(visitableList)
+            rvScrollListener?.updateStateAfterGetData()
+        }
+    }
+
+    private fun onLoadingState() {
+        viewModel.apply {
+            visitableList.clear()
+            visitableList.add(LoadingModel())
+            submitAdapterList(visitableList)
+        }
+    }
+
+    private fun onLoadingMoreState() {
+        viewModel.visitableList.add(LoadingMoreModel())
+        submitAdapterList(viewModel.visitableList)
+    }
+
+    private fun submitAdapterList(list: List<Visitable<*>>) {
+        medalAdapter.submitList(list)
+    }
+
+    private fun setupRecyclerView() {
+        binding?.badgeRv?.apply {
+            setupLayoutManager()
+            setupRecyclerViewScrollListener()
+            adapter = medalAdapter
+            layoutManager = rvLayoutManager
+            addOnScrollListener(rvScrollListener!!)
+            addItemDecoration(GridSpacing(0, GRID_VERTICAL_SPACING))
+        }
+    }
+
+    private fun setupLayoutManager() {
+        rvLayoutManager = GridLayoutManager(context, 3).apply {
+            spanSizeLookup = getSpanLookup()
+        }
+    }
+
+    private fun setupRecyclerViewScrollListener() {
+        rvScrollListener = object : EndlessRecyclerViewScrollListener(rvLayoutManager) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int) {
+                viewModel.getUserMedalis(page)
+            }
+        }
+    }
+
+    private fun getSpanLookup() = object : GridLayoutManager.SpanSizeLookup() {
+        override fun getSpanSize(position: Int): Int {
+            return when (medalAdapter.getItemViewType(position)) {
+                MedalViewHolder.LAYOUT -> FULL_COLUMN
+                else -> ONE_THIRD_COLUMN
+            }
+        }
+    }
+
+    private fun removeRecyclerViewScrollListener() {
+        if (rvScrollListener != null) {
+            binding?.badgeRv?.removeOnScrollListener(rvScrollListener!!)
+        }
+    }
+
+    override fun initInjector() {
+        getComponent(MedalCabinetComponent::class.java).inject(this)
+    }
+
+    override fun getScreenName() = ""
+}
--- features/promo/scp_rewards/src/main/java/com/tokopedia/scp_rewards/common/data/ScpResult.kt
+++ features/promo/scp_rewards/src/main/java/com/tokopedia/scp_rewards/common/data/ScpResult.kt
@@ -5,3 +5,4 @@ sealed interface ScpResult
 class Success<T>(val data:T) : ScpResult
 class Error(val error:Throwable) : ScpResult
 object Loading : ScpResult
+object InfiniteLoading: ScpResult
