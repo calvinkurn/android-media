9259944f01b364cf0ccdd33d717e5ebf1d2d26c3
devarafikry <devara.akmal@tokopedia.com>
2023-06-21 21:38:36 +0700

    require baseline

--- libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/util/MacroTestExt.kt
+++ libraries/macrobenchmark/src/androidTest/java/com/tkpd/macrobenchmark/util/MacroTestExt.kt
@@ -12,7 +12,7 @@ fun MacrobenchmarkRule.measureTokopediaApps(
 ) = measureRepeated(
     packageName = packageName,
     metrics = metrics,
-    compilationMode = CompilationMode.Partial(BaselineProfileMode.UseIfAvailable),
+    compilationMode = CompilationMode.Partial(BaselineProfileMode.Require),
     iterations = MacroArgs.getIterations(InstrumentationRegistry.getArguments()),
     startupMode = startupMode,
     setupBlock = {
