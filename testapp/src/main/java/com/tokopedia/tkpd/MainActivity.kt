05417a7caeb7d535fd1adcb2c6b47938667a4b8d
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-06-22 15:41:35 +0700

    remove unused changes

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return "tokopedia-android-internal://communication/universal-inbox"
+        return ""
     }
 
     data class Model(
