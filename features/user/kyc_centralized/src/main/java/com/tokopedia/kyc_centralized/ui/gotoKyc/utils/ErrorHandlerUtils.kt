ff4be1e4e8ac1d0ce5b88309520c2c676cbe92bc
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-06-23 08:43:28 +0700

    remove filtering exception

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
@@ -2,20 +2,19 @@ package com.tokopedia.kyc_centralized.ui.gotoKyc.utils
 
 import android.content.Context
 import com.tokopedia.kyc_centralized.R
-import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.network.utils.ErrorHandler
 
 fun Throwable?.getGotoKycErrorMessage(context: Context): String {
     val message = context.getString(R.string.goto_kyc_error_from_be)
-    return if (this == null || this is MessageErrorException) {
-        message
-    } else {
+    val errorCode = if (this != null) {
         val errorHandler = ErrorHandler.getErrorMessagePair(
             context = context,
             e = this,
             builder = ErrorHandler.Builder().build()
         )
-        val errorCode = context.getString(R.string.error_code, errorHandler.second)
-        message.plus(" $errorCode")
+        context.getString(R.string.error_code, errorHandler.second)
+    } else {
+        ""
     }
+    return message.plus(" $errorCode")
 }
