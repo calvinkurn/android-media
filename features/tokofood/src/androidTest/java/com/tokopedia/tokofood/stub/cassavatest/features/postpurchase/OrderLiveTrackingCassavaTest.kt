17ebca7302706141fe277ff9e9ac667c268ea445
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-06-30 14:01:13 +0700

    feat: remove unused comment

--- features/tokofood/src/androidTest/java/com/tokopedia/tokofood/stub/cassavatest/features/postpurchase/OrderLiveTrackingCassavaTest.kt
+++ features/tokofood/src/androidTest/java/com/tokopedia/tokofood/stub/cassavatest/features/postpurchase/OrderLiveTrackingCassavaTest.kt
@@ -1,11 +1,9 @@
 package com.tokopedia.tokofood.stub.cassavatest.features.postpurchase
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tokopedia.tokofood.stub.cassavatest.base.TokoFoodPostPurchaseCassavaTest
 import org.junit.Test
-import org.junit.runner.RunWith
 
-class OrderLiveTrackingCassavaTest: TokoFoodPostPurchaseCassavaTest() {
+class OrderLiveTrackingCassavaTest : TokoFoodPostPurchaseCassavaTest() {
 
     override fun setup() {
         super.setup()
@@ -32,9 +30,6 @@ class OrderLiveTrackingCassavaTest: TokoFoodPostPurchaseCassavaTest() {
 
     @Test
     fun validateClickCallDriverBottomSheet() {
-//        launchActivity {
-//            clickCallDriverBottomSheet(it)
-//        }
         clickCallDriverBottomSheet()
     }
 }
