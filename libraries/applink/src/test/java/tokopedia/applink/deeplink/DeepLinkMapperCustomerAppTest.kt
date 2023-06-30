b166fe96ba5a72d7e8ac3131b582cd571322b714
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-06-30 16:02:30 +0700

    fix: unittest deeplinkPatternTokopediaSchemeList check

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -27,7 +27,7 @@ class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
         // This a reminder to developer.
         // If this size is modified, please also add unit test for the added deeplink.
         const val SIZE_HOST = 155
-        const val SIZE_PATH = 254
+        const val SIZE_PATH = 255
     }
 
     override fun setup() {
