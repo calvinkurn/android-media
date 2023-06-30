c4ef47089f0f22e56683d8772efa3ba3af922de4
astidhiyaa <asti.anzaria@tokopedia.com>
2023-06-30 11:36:59 +0700

    fix; revert to Java7

--- features/content/content_common/src/main/java/com/tokopedia/content/common/util/ContentDateConverter.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/util/ContentDateConverter.kt
@@ -1,7 +1,11 @@
 package com.tokopedia.content.common.util
 
+import android.os.Build
+import com.tokopedia.utils.date.DateUtil
+import com.tokopedia.utils.date.toDate
 import java.time.Duration
 import java.time.ZonedDateTime
+import java.util.concurrent.TimeUnit
 
 /**
  * @author by astidhiyaa on 10/05/23
@@ -13,7 +17,7 @@ object ContentDateConverter {
     private const val DEFAULT_WORDING = "Beberapa detik yang lalu"
 
     fun convertTime(date: String): String {
-        return try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val now = ZonedDateTime.now()
             val convert = ZonedDateTime.parse(date)
             val diff = Duration.between(convert, now)
@@ -32,8 +36,23 @@ object ContentDateConverter {
             } else {
                 DEFAULT_WORDING
             }
-        } catch (e: Exception) {
-            DEFAULT_WORDING
+        } else {
+            val convert = date.toDate(DateUtil.YYYY_MM_DD_T_HH_MM_SS)
+            val diff = DateUtil.getCurrentCalendar().time.time - convert.time
+            val minute = TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS)
+            val hour = TimeUnit.HOURS.convert(diff, TimeUnit.MILLISECONDS)
+            val day = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
+            return if (day in 1..90) {
+                "$day $DAY"
+            } else if (day > 90) {
+                "${convert.month} ${convert.year}"
+            } else if (hour in 1..24) {
+                "$hour $HOUR"
+            } else if (minute in 1..60) {
+                "$minute $MINUTE"
+            } else {
+                DEFAULT_WORDING
+            }
         }
     }
 
