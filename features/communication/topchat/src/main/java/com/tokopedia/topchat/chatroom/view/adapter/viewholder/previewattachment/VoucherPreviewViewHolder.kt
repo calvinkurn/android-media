bb1535e122ce937a8c85aec146bc2cb2a4325227
isfaaghyth <isfaaghyth@gmail.com>
2023-07-03 15:57:25 +0700

    feat: remove kotlin synthetic APIs on topchat module

--- features/communication/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/previewattachment/VoucherPreviewViewHolder.kt
+++ features/communication/topchat/src/main/java/com/tokopedia/topchat/chatroom/view/adapter/viewholder/previewattachment/VoucherPreviewViewHolder.kt
@@ -4,13 +4,21 @@ import android.view.View
 import android.widget.ImageView
 import com.tokopedia.topchat.R
 import com.tokopedia.topchat.chatroom.view.uimodel.SendableVoucherPreviewUiModel
-import kotlinx.android.synthetic.main.item_voucher_preview.view.*
+import com.tokopedia.topchat.databinding.ItemVoucherPreviewBinding
+import com.tokopedia.utils.view.binding.viewBinding
 
-class VoucherPreviewViewHolder(itemView: View, attachmentItemPreviewListener: AttachmentItemPreviewListener)
-    : AttachmentPreviewViewHolder<SendableVoucherPreviewUiModel>(itemView, attachmentItemPreviewListener) {
+class VoucherPreviewViewHolder(
+    itemView: View,
+    attachmentItemPreviewListener: AttachmentItemPreviewListener
+) : AttachmentPreviewViewHolder<SendableVoucherPreviewUiModel>(
+    itemView,
+    attachmentItemPreviewListener
+) {
+
+    private val binding: ItemVoucherPreviewBinding? by viewBinding()
 
     override fun getButtonView(itemView: View): ImageView? {
-        return itemView.iv_close
+        return binding?.ivClose
     }
 
     override fun bind(model: SendableVoucherPreviewUiModel) {
@@ -20,7 +28,7 @@ class VoucherPreviewViewHolder(itemView: View, attachmentItemPreviewListener: At
 
     private fun bindVoucherView(model: SendableVoucherPreviewUiModel) {
         val voucherModel = model.merchantVoucherViewModel
-        itemView.voucher?.setData(voucherModel, false)
+        binding?.voucher?.setData(voucherModel, false)
     }
 
     companion object {
