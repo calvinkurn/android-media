06eb0df305ac2d80a0b53a701056456fe8c627dd
isfaaghyth <isfaaghyth@gmail.com>
2023-07-04 15:38:27 +0700

    feat: render the label based on failure type

--- features/media/medialoader/src/debug/java/com/tokopedia/media/loader/DebugMediaLoaderActivity.kt
+++ features/media/medialoader/src/debug/java/com/tokopedia/media/loader/DebugMediaLoaderActivity.kt
@@ -5,11 +5,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
-import com.tokopedia.media.loader.data.Properties
+import com.tokopedia.kotlin.extensions.view.showWithCondition
+import com.tokopedia.unifycomponents.Label
 
 /** @suppress */
 open class DebugMediaLoaderActivity : AppCompatActivity() {
@@ -19,6 +18,7 @@ open class DebugMediaLoaderActivity : AppCompatActivity() {
     private val edtUrl by lazy { findViewById<EditText>(R.id.edt_url) }
     private val edtProperties by lazy { findViewById<EditText>(R.id.edt_properties) }
     private val imgSample by lazy { findViewById<ImageView>(R.id.img_sample) }
+    private val label by lazy { findViewById<Label>(R.id.label) }
 
     private var isCustomPropertiesVisible = false
 
@@ -26,14 +26,8 @@ open class DebugMediaLoaderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug_medialoader)
 
-        edtUrl.setText("https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg")
-
-        edtProperties.setText(
-            GsonBuilder()
-                .setPrettyPrinting()
-                .create()
-                .toJson(Properties())
-        )
+        edtUrl.setText("")
+        edtProperties.setText("")
 
         btnEdit.setOnClickListener {
             isCustomPropertiesVisible = if (!isCustomPropertiesVisible) {
@@ -47,15 +41,18 @@ open class DebugMediaLoaderActivity : AppCompatActivity() {
 
         btnShow.setOnClickListener {
             val url = edtUrl.text.toString().trim()
-            val json = edtProperties.text.toString().trim()
 
-            val properties = Gson().fromJson(json, Properties::class.java)
-            imgSample.debugLoadImage(url, properties)
+            imgSample.loadImage(url) {
+                shouldTrackNetworkResponse(true)
+
+                networkResponse { _, failureType ->
+                    label.showWithCondition(failureType != null)
+
+                    if (failureType != null) {
+                        label.setLabel(failureType.toString())
+                    }
+                }
+            }
         }
     }
-
-    private fun ImageView.debugLoadImage(
-        url: String? = "",
-        properties: Properties = Properties()
-    ) = call(url, properties)
 }
