37100a66d03d4e0c37b267a98a9a94693938d045
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-07-05 13:09:36 +0700

    [Recom] Remove unused code

--- features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/widget/vertical/RecommendationVerticalMapper.kt
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/widget/vertical/RecommendationVerticalMapper.kt
@@ -1,26 +1,12 @@
 package com.tokopedia.recommendation_widget_common.widget.vertical
 
-import com.tokopedia.recommendation_widget_common.data.RecommendationFilterChipsEntity
 import com.tokopedia.recommendation_widget_common.extension.toProductCardModel
-import com.tokopedia.recommendation_widget_common.presentation.model.AnnotationChip
 import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationWidget
 import com.tokopedia.recommendation_widget_common.widget.vertical.model.RecommendationVerticalProductCardModel
 import com.tokopedia.recommendation_widget_common.widget.vertical.model.RecommendationVerticalSeeMoreModel
 import com.tokopedia.recommendation_widget_common.widget.vertical.model.RecommendationVerticalVisitable
 
 object RecommendationVerticalMapper {
-    fun mapToAnnotateChip(
-        recommendationFilterChips: List<RecommendationFilterChipsEntity.RecommendationFilterChip>,
-        selectedRecommendationFilterChips: RecommendationFilterChipsEntity.RecommendationFilterChip?
-    ): List<AnnotationChip> {
-        return recommendationFilterChips.map {
-            AnnotationChip(
-                recommendationFilterChip = if (it.name == selectedRecommendationFilterChips?.name) {
-                    it.copy(isActivated = true)
-                } else it
-            )
-        }
-    }
 
     fun mapVisitableList(
         widget: RecommendationWidget
