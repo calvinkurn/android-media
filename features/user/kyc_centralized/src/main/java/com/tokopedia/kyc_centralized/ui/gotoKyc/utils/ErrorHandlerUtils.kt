cce171587dcb8eb5c444cc9b82b83ac4cc5831d0
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-07-06 12:02:06 +0700

    adjust message error for no connection and timeout

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
@@ -3,9 +3,11 @@ package com.tokopedia.kyc_centralized.ui.gotoKyc.utils
 import android.content.Context
 import com.tokopedia.kyc_centralized.R
 import com.tokopedia.network.utils.ErrorHandler
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 fun Throwable?.getGotoKycErrorMessage(context: Context): String {
-    val message = context.getString(R.string.goto_kyc_error_from_be)
     val errorCode = if (this != null) {
         val errorHandler = ErrorHandler.getErrorMessagePair(
             context = context,
@@ -15,6 +17,17 @@ fun Throwable?.getGotoKycErrorMessage(context: Context): String {
         context.getString(R.string.error_code, errorHandler.second)
     } else {
         ""
+    }
+    val message = when (this) {
+        is UnknownHostException, is ConnectException -> {
+            context.getString(R.string.goto_kyc_error_no_connection)
+        }
+        is SocketTimeoutException -> {
+            context.getString(R.string.goto_kyc_error_timeout)
+        }
+        else -> {
+            context.getString(R.string.goto_kyc_error_from_be)
+        }
     }
     return message.plus(" $errorCode")
 }
