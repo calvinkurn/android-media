b3c388a63d1461328e9e5a3e81a505524cc054e8
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-07-07 11:13:00 +0700

    remove errorCode for throwable no connection and timeout

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
@@ -8,16 +8,7 @@ import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
 fun Throwable?.getGotoKycErrorMessage(context: Context): String {
-    val errorCode = if (this != null) {
-        val errorHandler = ErrorHandler.getErrorMessagePair(
-            context = context,
-            e = this,
-            builder = ErrorHandler.Builder().build()
-        )
-        context.getString(R.string.error_code, errorHandler.second)
-    } else {
-        ""
-    }
+    var errorCode = ""
     val message = when (this) {
         is UnknownHostException, is ConnectException -> {
             context.getString(R.string.goto_kyc_error_no_connection)
@@ -26,6 +17,15 @@ fun Throwable?.getGotoKycErrorMessage(context: Context): String {
             context.getString(R.string.goto_kyc_error_timeout)
         }
         else -> {
+            if (this != null) {
+                val errorHandler = ErrorHandler.getErrorMessagePair(
+                    context = context,
+                    e = this,
+                    builder = ErrorHandler.Builder().build()
+                )
+                errorCode = context.getString(R.string.error_code, errorHandler.second)
+            }
+
             context.getString(R.string.goto_kyc_error_from_be)
         }
     }
