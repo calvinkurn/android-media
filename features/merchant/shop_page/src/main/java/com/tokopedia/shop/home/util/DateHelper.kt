4e0ff82467a67d39bd44d4356a368f69061edb10
CJ <calvyn.julian@tokopedia.com>
2023-07-07 11:31:12 +0700

    fix shop home banner timer finish banner ui

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/util/DateHelper.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/util/DateHelper.kt
@@ -10,7 +10,7 @@ object DateHelper {
     private const val DEFAULT_SERVER_TIMEZONE = "Asia/Jakarta"
     const val SHOP_NPL_CAMPAIGN_WIDGET_MORE_THAT_1_DAY_DATE_FORMAT = "dd MMM yyyy  HH : mm"
     const val SHOP_CAMPAIGN_BANNER_TIMER_MORE_THAN_1_DAY_DATE_FORMAT = "dd MMM yyyy | HH : mm"
-
+    const val SHOP_CAMPAIGN_BANNER_TIMER_MORE_THAN_1_DAY_DATE_FORMAT_ENDED = "dd MMM yyyy | HH:mm"
     fun getDateFromString(dateString: String): Date {
         val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         format.timeZone = TimeZone.getTimeZone(DEFAULT_SERVER_TIMEZONE)
