0eaf5ed157b4476c27df077debdbaed078414c97
reivinoktavianus <oktavianusreivin@gmail.com>
2023-07-10 17:32:05 +0700

    feat: Revert change logics

--- features/tokofood/src/androidTest/java/com/tokopedia/tokofood/cassavatest/features/postpurchase/OrderCompletedCassavaTest.kt
+++ features/tokofood/src/androidTest/java/com/tokopedia/tokofood/cassavatest/features/postpurchase/OrderCompletedCassavaTest.kt
@@ -1,9 +1,5 @@
 package com.tokopedia.tokofood.cassavatest.features.postpurchase
 
-import android.app.Activity
-import android.app.Instrumentation
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
 import com.tokopedia.tokofood.cassavatest.base.TokoFoodPostPurchaseCassavaTest
 import org.junit.Test
 
@@ -18,9 +14,6 @@ class OrderCompletedCassavaTest: TokoFoodPostPurchaseCassavaTest() {
 
     @Test
     fun validateClickAddToCartCta() {
-        Intents.intending(IntentMatchers.anyIntent())
-            .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
-
-        Thread.sleep(3000)
+        clickAddToCartCta()
     }
 }
