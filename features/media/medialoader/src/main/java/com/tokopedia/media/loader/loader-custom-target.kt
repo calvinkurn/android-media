b7edf5fa41d43165d8c78c9c3b746c6fff7457b0
isfaaghyth <isfaaghyth@gmail.com>
2023-07-10 16:58:15 +0700

    feat: change the deprecation message

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/loader-custom-target.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/loader-custom-target.kt
@@ -85,7 +85,7 @@ fun <T : View> loadImageWithTarget(
 
 @Deprecated(
     message = "This function is too verbose, please use getBitmapImageUrl() extension instead",
-    replaceWith = ReplaceWith("getBitmapImageUrl")
+    replaceWith = ReplaceWith("imageUrl.getBitmapImageUrl(context, properties)")
 )
 fun loadImageWithEmptyTarget(
     context: Context,
