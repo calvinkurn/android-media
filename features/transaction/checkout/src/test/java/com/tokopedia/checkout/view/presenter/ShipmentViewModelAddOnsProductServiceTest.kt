978fa269f7870ac3a62bb7d3a8a3d511354fd9ea
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-07-11 15:16:56 +0700

    adding unit test for save addons before checkout page

--- features/transaction/checkout/src/test/java/com/tokopedia/checkout/view/presenter/ShipmentViewModelAddOnsProductServiceTest.kt
+++ features/transaction/checkout/src/test/java/com/tokopedia/checkout/view/presenter/ShipmentViewModelAddOnsProductServiceTest.kt
@@ -9,6 +9,7 @@ import com.tokopedia.purchase_platform.common.feature.addons.data.response.SaveA
 import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.just
+import io.mockk.verify
 import org.junit.Test
 
 class ShipmentViewModelAddOnsProductServiceTest : BaseShipmentViewModelTest() {
@@ -112,5 +113,86 @@ class ShipmentViewModelAddOnsProductServiceTest : BaseShipmentViewModelTest() {
 
         // Then
         assert(viewModel.listSummaryAddOnModel.isNotEmpty())
+    }
+
+    @Test
+    fun verifySaveAddonsBeforeCheckoutReturnSuccess() {
+        val response = DataProvider.provideShipmentAddressFormWithAddOnsProductEnabledResponse()
+        val cartShipmentAddressFormData = shipmentMapper
+            .convertToShipmentAddressFormData(response.shipmentAddressFormResponse.data)
+        cartShipmentAddressFormData.groupAddress.forEach { groupAddress ->
+            groupAddress.groupShop.forEach { groupShop ->
+                groupShop.groupShopData.forEach { groupShopV2 ->
+                    groupShopV2.products.forEach {
+                    }
+                }
+            }
+        }
+        val cartItemModel = CartItemModel(cartStringGroup = "111-111-111")
+
+        coEvery { getShipmentAddressFormV4UseCase(any()) } returns cartShipmentAddressFormData
+
+        coEvery { saveAddOnStateUseCase.setParams(any(), false) } just Runs
+        coEvery { saveAddOnStateUseCase.execute(any(), any()) } answers {
+            firstArg<(SaveAddOnStateResponse) -> Unit>().invoke(
+                SaveAddOnStateResponse(
+                    saveAddOns = SaveAddOnsResponse(
+                        status = "OK"
+                    )
+                )
+            )
+        }
+
+        // When
+        viewModel.processInitialLoadCheckoutPage(
+            false,
+            false,
+            false
+        )
+        viewModel.saveAddOnsProductBeforeCheckout()
+
+        // Then
+        assert(viewModel.listSummaryAddOnModel.isNotEmpty())
+        verify {
+            view.handleOnSuccessSaveAddOnProduct()
+        }
+    }
+
+    @Test
+    fun verifySaveAddonsBeforeCheckoutReturnFailed() {
+        val response = DataProvider.provideShipmentAddressFormWithAddOnsProductEnabledResponse()
+        val cartShipmentAddressFormData = shipmentMapper
+            .convertToShipmentAddressFormData(response.shipmentAddressFormResponse.data)
+        cartShipmentAddressFormData.groupAddress.forEach { groupAddress ->
+            groupAddress.groupShop.forEach { groupShop ->
+                groupShop.groupShopData.forEach { groupShopV2 ->
+                    groupShopV2.products.forEach {
+                    }
+                }
+            }
+        }
+        val cartItemModel = CartItemModel(cartStringGroup = "111-111-111")
+        val throwable = ResponseErrorException("fail testing delete")
+
+        coEvery { getShipmentAddressFormV4UseCase(any()) } returns cartShipmentAddressFormData
+
+        coEvery { saveAddOnStateUseCase.setParams(any(), false) } just Runs
+        coEvery { saveAddOnStateUseCase.execute(any(), any()) } answers {
+            secondArg<(Throwable) -> Unit>().invoke(throwable)
+        }
+
+        // When
+        viewModel.processInitialLoadCheckoutPage(
+            false,
+            false,
+            false
+        )
+        viewModel.saveAddOnsProductBeforeCheckout()
+
+        // Then
+        assert(viewModel.listSummaryAddOnModel.isNotEmpty())
+        verify {
+            view.showToastError(any())
+        }
     }
 }
