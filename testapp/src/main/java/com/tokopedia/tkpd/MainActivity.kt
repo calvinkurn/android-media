8d8be9eb49bc3049f41830e527ab603f04a19069
reivinoktavianus <oktavianusreivin@gmail.com>
2023-07-11 09:56:41 +0700

    feat: Revert sellerapp

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return ApplinkConst.TokoFood.HOME
+        return ""
     }
 
     data class Model(
