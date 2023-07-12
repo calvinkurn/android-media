ea36d21cdf338dea07e893ef0aa35c531378a88c
reivinoktavianus <oktavianusreivin@gmail.com>
2023-07-12 13:47:11 +0700

    feat: Add tokofood promo cassava test
    https://tokopedia.atlassian.net/browse/AN-46543

--- features/tokofood/src/androidTest/java/com/tokopedia/tokofood/stub/base/presentation/activity/BaseTokofoodActivityStub.kt
+++ features/tokofood/src/androidTest/java/com/tokopedia/tokofood/stub/base/presentation/activity/BaseTokofoodActivityStub.kt
@@ -5,16 +5,23 @@ import android.content.Intent
 import androidx.fragment.app.Fragment
 import com.tokopedia.tokofood.common.presentation.view.BaseTokofoodActivity
 import com.tokopedia.tokofood.stub.purchase.presentation.fragment.TokoFoodPurchaseFragmentStub
+import com.tokopedia.tokofood.stub.purchase.promo.presentation.fragment.TokoFoodPromoFragmentStub
 
 class BaseTokofoodActivityStub: BaseTokofoodActivity() {
 
     override fun getRootFragment(): Fragment {
-        return TokoFoodPurchaseFragmentStub.createInstance()
+        return when (intent?.getStringExtra(PAGE_KEY)) {
+            PURCHASE_PAGE -> TokoFoodPurchaseFragmentStub.createInstance()
+            else -> TokoFoodPromoFragmentStub.createInstance()
+        }
     }
 
     companion object {
 
         private const val PAGE_KEY = "page_key"
+
+        const val PURCHASE_PAGE = "purchase_page"
+        const val PROMO_PAGE = "promo_page"
 
         @JvmStatic
         fun createIntent(
