e861ee4dae07122fca8912defb7a1e207886859e
hansenpw <hansen.wijaya@tokopedia.com>
2023-07-14 13:38:38 +0700

    promo in checkout

--- features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/view/widget/PromoEntryPointWidget.kt
+++ features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/view/widget/PromoEntryPointWidget.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.View
+import android.widget.TextSwitcher
 import android.widget.ViewSwitcher
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.get
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
+import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.promocheckout.common.R
 import com.tokopedia.unifycomponents.BaseCustomView
+import com.tokopedia.unifycomponents.ImageUnify
 
 open class PromoEntryPointWidget @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
@@ -21,7 +25,13 @@ open class PromoEntryPointWidget @JvmOverloads constructor(
     private var loadingView: View? = null
     private var switcherView: ViewSwitcher? = null
     private var activeView: View? = null
+    private var activeViewRightIcon: IconUnify? = null
+    private var activeViewLeftImage: ImageUnify? = null
+    private var activeViewWording: TextSwitcher? = null
     private var inActiveView: View? = null
+    private var inActiveViewLeftImage: ImageUnify? = null
+    private var inActiveViewWording: TextSwitcher? = null
+    private var inActiveViewRightIcon: IconUnify? = null
     private var errorView: View? = null
 
     init {
@@ -34,7 +44,13 @@ open class PromoEntryPointWidget @JvmOverloads constructor(
         errorView = findViewById(R.id.error_promo_checkout)
         switcherView = findViewById(R.id.switcher_promo_checkout)
         activeView = switcherView?.get(0)
+        activeViewLeftImage = activeView?.findViewById(R.id.iv_promo_checkout_left)
+        activeViewWording = activeView?.findViewById(R.id.tv_promo_checkout_title)
+        activeViewRightIcon = activeView?.findViewById(R.id.ic_promo_checkout_right)
         inActiveView = switcherView?.get(1)
+        inActiveViewLeftImage = inActiveView?.findViewById(R.id.iv_promo_checkout_left)
+        inActiveViewWording = inActiveView?.findViewById(R.id.tv_promo_checkout_title)
+        inActiveViewRightIcon = inActiveView?.findViewById(R.id.ic_promo_checkout_right)
 
         val loadingBackground = ResourcesCompat.getDrawable(resources, R.drawable.background_promo_checkout_teal_gradient, null)
 //        val n0Color = ResourcesCompat.getColor(resources, com.tokopedia.unifyprinciples.R.color.Unify_NN1000, null)
@@ -65,11 +81,68 @@ open class PromoEntryPointWidget @JvmOverloads constructor(
         errorView?.visibility = View.GONE
     }
 
-    fun showError() {
+    fun showError(onReloadClickListener: () -> Unit) {
         errorView?.visibility = View.VISIBLE
         loadingView?.visibility = View.GONE
         switcherView?.visibility = View.GONE
+        errorView?.setOnClickListener {
+            onReloadClickListener.invoke()
+        }
     }
 
+    fun showInactive(leftImageUrl: String, wording: String, animate: Boolean = false, onClickListener: () -> Unit = {}) {
+        if (animate && switcherView?.visibility == View.VISIBLE) {
+            switcherView?.visibility = View.VISIBLE
+            inActiveViewLeftImage?.setImageUrl(leftImageUrl)
+            inActiveViewWording?.setCurrentText(MethodChecker.fromHtml(wording))
+            inActiveViewRightIcon?.visibility = View.GONE
+            if (switcherView?.displayedChild != 1) {
+                // only trigger animation if currently showing different view
+                switcherView?.displayedChild = 1
+            }
+            errorView?.visibility = View.GONE
+            loadingView?.visibility = View.GONE
+        } else {
+            switcherView?.reset()
+            inActiveViewLeftImage?.setImageUrl(leftImageUrl)
+            inActiveViewWording?.setCurrentText(MethodChecker.fromHtml(wording))
+            inActiveViewRightIcon?.visibility = View.GONE
+            switcherView?.displayedChild = 1
+            switcherView?.visibility = View.VISIBLE
+            errorView?.visibility = View.GONE
+            loadingView?.visibility = View.GONE
+        }
+        inActiveView?.setOnClickListener {
+            onClickListener.invoke()
+        }
+    }
 
+    fun showActive(leftImageUrl: String, wording: String, rightIcon: Int, animate: Boolean = false, animateWording: Boolean = false, onClickListener: () -> Unit = {}) {
+        if (animate && switcherView?.visibility == View.VISIBLE) {
+            switcherView?.visibility = View.VISIBLE
+            activeViewLeftImage?.setImageUrl(leftImageUrl)
+            activeViewRightIcon?.setImage(rightIcon)
+            if (switcherView?.displayedChild != 0) {
+                // only trigger view switch animation if currently showing different view
+                activeViewWording?.setCurrentText(MethodChecker.fromHtml(wording))
+                switcherView?.displayedChild = 0
+            } else if (animateWording) {
+                activeViewWording?.setText(MethodChecker.fromHtml(wording))
+            }
+            errorView?.visibility = View.GONE
+            loadingView?.visibility = View.GONE
+        } else {
+            switcherView?.reset()
+            activeViewLeftImage?.setImageUrl(leftImageUrl)
+            activeViewWording?.setCurrentText(MethodChecker.fromHtml(wording))
+            activeViewRightIcon?.setImage(rightIcon)
+            switcherView?.displayedChild = 0
+            switcherView?.visibility = View.VISIBLE
+            errorView?.visibility = View.GONE
+            loadingView?.visibility = View.GONE
+        }
+        activeView?.setOnClickListener {
+            onClickListener.invoke()
+        }
+    }
 }
--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/view/viewholder/CheckoutPromoViewHolder.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/view/viewholder/CheckoutPromoViewHolder.kt
@@ -3,11 +3,29 @@ package com.tokopedia.checkout.revamp.view.viewholder
 import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.checkout.R
 import com.tokopedia.checkout.databinding.ItemCheckoutPromoBinding
+import com.tokopedia.iconunify.IconUnify
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUiModel
 
 class CheckoutPromoViewHolder(private val binding: ItemCheckoutPromoBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(promo: LastApplyUiModel) {
+        binding.btnCheckoutPromo.showActive("https://static.vecteezy.com/system/resources/previews/004/495/473/original/sales-promotion-line-icon-logo-free-vector.jpg", "gak bisa nih", IconUnify.CHEVRON_DOWN) {
+            binding.btnCheckoutPromo.showError {
+                binding.btnCheckoutPromo.showInactive("https://static.vecteezy.com/system/resources/previews/004/495/473/original/sales-promotion-line-icon-logo-free-vector.jpg", "errr") {
+                    binding.btnCheckoutPromo.showActive("https://static.vecteezy.com/system/resources/previews/004/495/473/original/sales-promotion-line-icon-logo-free-vector.jpg", "gak bisa nih", IconUnify.CHEVRON_DOWN, true, true) {
+                        binding.btnCheckoutPromo.showActive("https://static.vecteezy.com/system/resources/previews/004/495/473/original/sales-promotion-line-icon-logo-free-vector.jpg", "gak bisa nih 2", IconUnify.CHEVRON_DOWN, true, true) {
+                            binding.btnCheckoutPromo.showInactive(
+                                "https://static.vecteezy.com/system/resources/previews/004/495/473/original/sales-promotion-line-icon-logo-free-vector.jpg",
+                                "errr",
+                                true
+                            ) {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     companion object {
