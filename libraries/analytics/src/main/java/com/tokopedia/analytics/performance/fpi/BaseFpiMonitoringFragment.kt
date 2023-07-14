5bdcb95915c96f07d377c3d777a8c63cca51fc9e
Yovi Eka Putra <yovi.putra@tokopedia.com>
2023-07-14 11:33:09 +0700

    feat[frame_metric] add menu enable fpi monitoring into developer option

--- libraries/analytics/src/main/java/com/tokopedia/analytics/performance/fpi/BaseFpiMonitoringFragment.kt
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/performance/fpi/BaseFpiMonitoringFragment.kt
@@ -1,8 +1,11 @@
 package com.tokopedia.analytics.performance.fpi
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.config.GlobalConfig
 
 /**
  * Created by yovi.putra on 14/07/23"
@@ -14,17 +17,25 @@ abstract class BaseFpiMonitoringFragment : BaseDaggerFragment() {
 
     private val fpiMonitoring: FpiMonitoringDelegate by lazy { FpiMonitoringDelegateImpl() }
 
+    // ref to DeveloperOptionActivity.PREF_KEY_FPI_MONITORING_POPUP
+    private val prefKey = "fpi_monitoring_popup"
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fpiMonitoring.onViewCreated(this)
+        if(isActive()) fpiMonitoring.onViewCreated(this)
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
-        fpiMonitoring.onHiddenChanged(hidden)
+        if (isActive()) fpiMonitoring.onHiddenChanged(hidden)
     }
 
     private fun isActive(): Boolean {
-        return true
+        val context = context ?: return false
+        return GlobalConfig.DEBUG && context.isFpiMonitoringEnable()
     }
+
+    private fun Context.isFpiMonitoringEnable(): Boolean = getSharedPreferences(
+        prefKey, BaseActivity.MODE_PRIVATE
+    ).getBoolean(prefKey, false)
 }
