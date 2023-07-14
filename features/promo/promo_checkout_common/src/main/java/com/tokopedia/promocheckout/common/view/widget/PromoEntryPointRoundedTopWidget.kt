9f3e59418afd69b921f3b361943a579be225afa5
hansenpw <hansen.wijaya@tokopedia.com>
2023-07-14 11:03:14 +0700

    temp error promo

--- features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/view/widget/PromoEntryPointRoundedTopWidget.kt
+++ features/promo/promo_checkout_common/src/main/java/com/tokopedia/promocheckout/common/view/widget/PromoEntryPointRoundedTopWidget.kt
@@ -1,10 +1,12 @@
 package com.tokopedia.promocheckout.common.view.widget
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.View
 import android.widget.ViewSwitcher
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.get
 import com.tokopedia.promocheckout.common.R
 import com.tokopedia.unifycomponents.BaseCustomView
 
@@ -45,44 +47,65 @@ class PromoEntryPointRoundedTopWidget @JvmOverloads constructor(
 
     private var loadingView: View? = null
     private var switcherView: ViewSwitcher? = null
+    private var activeView: View? = null
+    private var inActiveView: View? = null
+    private var errorView: View? = null
 
     init {
         inflate(context, getLayout(), this)
         setupViews()
-        val styledAttributes =
-            context.obtainStyledAttributes(attrs, R.styleable.PromoCheckoutButtonView)
-        try {
-            state = ButtonPromoCheckoutView.State.fromId(
-                styledAttributes.getInteger(
-                    R.styleable.PromoCheckoutButtonView_stateButton,
-                    1
-                )
-            )
-            title = styledAttributes.getString(R.styleable.PromoCheckoutButtonView_title) ?: ""
-            desc = styledAttributes.getString(R.styleable.PromoCheckoutButtonView_desc) ?: ""
-            chevronIcon = styledAttributes.getInt(R.styleable.PromoCheckoutButtonView_icon_right, 0)
-//            margin = ButtonPromoCheckoutView.Margin.fromId(
+//        val styledAttributes =
+//            context.obtainStyledAttributes(attrs, R.styleable.PromoCheckoutButtonView)
+//        try {
+//            state = ButtonPromoCheckoutView.State.fromId(
 //                styledAttributes.getInteger(
-//                    R.styleable.PromoCheckoutButtonView_marginButton,
-//                    0
+//                    R.styleable.PromoCheckoutButtonView_stateButton,
+//                    1
 //                )
 //            )
-
-        } finally {
-            styledAttributes.recycle()
-        }
+//            title = styledAttributes.getString(R.styleable.PromoCheckoutButtonView_title) ?: ""
+//            desc = styledAttributes.getString(R.styleable.PromoCheckoutButtonView_desc) ?: ""
+//            chevronIcon = styledAttributes.getInt(R.styleable.PromoCheckoutButtonView_icon_right, 0)
+////            margin = ButtonPromoCheckoutView.Margin.fromId(
+////                styledAttributes.getInteger(
+////                    R.styleable.PromoCheckoutButtonView_marginButton,
+////                    0
+////                )
+////            )
+//
+//        } finally {
+//            styledAttributes.recycle()
+//        }
     }
 
     private fun setupViews() {
         loadingView = findViewById(R.id.loader_promo_checkout)
+        errorView = findViewById(R.id.error_promo_checkout)
         switcherView = findViewById(R.id.switcher_promo_checkout)
+        activeView = switcherView?.get(0)
+        inActiveView = switcherView?.get(1)
 
         loadingView?.background = ResourcesCompat.getDrawable(resources, R.drawable.background_promo_checkout_teal_rounded, null)
+        activeView?.background = ResourcesCompat.getDrawable(resources, R.drawable.background_promo_checkout_active_teal_rounded, null)
+        val inActiveBackground = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.background_promo_checkout_teal_rounded,
+            null
+        )
+        (inActiveBackground as? GradientDrawable)?.setColor(ResourcesCompat.getColor(resources, com.tokopedia.unifyprinciples.R.color.Unify_NN50, null))
+        inActiveView?.background = inActiveBackground
+        val errorBackground = ResourcesCompat.getDrawable(
+            resources,
+            R.drawable.background_promo_checkout_teal_rounded,
+            null
+        )
+        (errorBackground as? GradientDrawable)?.setColor(ResourcesCompat.getColor(resources, com.tokopedia.unifyprinciples.R.color.Unify_YN50, null))
+        errorView?.background = errorBackground
     }
 
     private fun initView() {
         when (state) {
-            ButtonPromoCheckoutView.State.LOADING -> setViewLoading()
+            ButtonPromoCheckoutView.State.LOADING -> showLoading()
             ButtonPromoCheckoutView.State.ACTIVE -> setViewActive()
             ButtonPromoCheckoutView.State.INACTIVE -> setViewInactive()
         }
@@ -111,9 +134,10 @@ class PromoEntryPointRoundedTopWidget @JvmOverloads constructor(
 //        }
     }
 
-    private fun setViewLoading() {
+    fun showLoading() {
         loadingView?.visibility = View.VISIBLE
         switcherView?.visibility = View.GONE
+        errorView?.visibility = View.GONE
     }
 
     private fun setViewActive() {
@@ -132,5 +156,4 @@ class PromoEntryPointRoundedTopWidget @JvmOverloads constructor(
 
     private fun setViewWIthNoMarginBottom() {
     }
-
 }
