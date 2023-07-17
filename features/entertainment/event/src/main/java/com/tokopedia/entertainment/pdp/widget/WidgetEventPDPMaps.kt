0f4eb6a5e9451186502cab0cb9ba363295a4ce72
firmankagami <firmanda.nugroho@tokopedia.com>
2023-07-17 15:06:37 +0700

    refactor(event): event pdp view binding part five

--- features/entertainment/event/src/main/java/com/tokopedia/entertainment/pdp/widget/WidgetEventPDPMaps.kt
+++ features/entertainment/event/src/main/java/com/tokopedia/entertainment/pdp/widget/WidgetEventPDPMaps.kt
@@ -2,20 +2,19 @@ package com.tokopedia.entertainment.pdp.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import com.tokopedia.entertainment.R
+import android.view.LayoutInflater
+import com.tokopedia.entertainment.databinding.WidgetEventPdpDetailLokasiBinding
 import com.tokopedia.unifycomponents.BaseCustomView
-import kotlinx.android.synthetic.main.widget_event_pdp_detail_lokasi.view.*
 
 class WidgetEventPDPMaps @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
         BaseCustomView(context, attrs, defStyleAttr){
 
-
-    init {
-        View.inflate(context, R.layout.widget_event_pdp_detail_lokasi, this)
-    }
+    private val binding = WidgetEventPdpDetailLokasiBinding.inflate(
+        LayoutInflater.from(context),
+        this, true
+    )
 
     fun setLocationTitle(title : String){
-        tg_event_pdp_detail_lokasi_map_title.text = title
+        binding.tgEventPdpDetailLokasiMapTitle.text = title
     }
 }
