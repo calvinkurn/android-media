baf192911df18e31aaac465a432d49842fe39fd3
isfaaghyth <isfaaghyth@gmail.com>
2023-07-17 12:36:48 +0700

    feat: init universal media editor

---
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/MainEditorActivity.kt
@@ -0,0 +1,11 @@
+package com.tokopedia.editor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainEditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_editor)
+    }
+}
