8811b7e54074174e6f1bc13006332513ac199cde
hendry.setiadi <hendry.setiadi@tokopedia.com>
2023-07-17 13:42:49 +0700

    fix lint

--- libraries/iris/src/main/java/com/tokopedia/iris/data/TrackingRepository.kt
+++ libraries/iris/src/main/java/com/tokopedia/iris/data/TrackingRepository.kt
@@ -58,60 +58,99 @@ class TrackingRepository private constructor(
         }
     }
 
-    private fun getLineDBFlush()= getRemoteConfig().getLong(REMOTE_CONFIG_IRIS_DB_FLUSH, 5000)
+    private fun getLineDBFlush() = getRemoteConfig().getLong(REMOTE_CONFIG_IRIS_DB_FLUSH, 5000)
     private fun getLineDBSend() = getRemoteConfig().getLong(REMOTE_CONFIG_IRIS_DB_SEND, 400)
-    private fun getBatchPerPeriod()= getRemoteConfig().getLong(REMOTE_CONFIG_IRIS_BATCH_SEND, 5)
+    private fun getBatchPerPeriod() = getRemoteConfig().getLong(REMOTE_CONFIG_IRIS_BATCH_SEND, 5)
 
     suspend fun saveEvent(data: String) =
         withContext(Dispatchers.IO) {
             try {
-                val tracking = Tracking(data, userSession.userId, userSession.deviceId,
-                    Calendar.getInstance().timeInMillis, GlobalConfig.VERSION_NAME)
+                val tracking = Tracking(
+                    data, userSession.userId, userSession.deviceId,
+                    Calendar.getInstance().timeInMillis, GlobalConfig.VERSION_NAME
+                )
                 trackingDao.insert(tracking)
                 IrisLogger.getInstance(context).putSaveIrisEvent(tracking.toString())
                 setRelicLog("getCount", "total count")
                 val dbCount = trackingDao.getCount()
                 if (dbCount >= getLineDBFlush()) {
-                    ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "dbCountFlush", "no" to dbCount.toString()))
+                    ServerLogger.log(
+                        Priority.P1,
+                        "IRIS",
+                        mapOf("type" to "dbCountFlush", "no" to dbCount.toString())
+                    )
                     trackingDao.flush()
                 } else if (dbCount >= getLineDBSend()) {
                     // if the line is big, send it
                     if (dbCount % 5 == 0) {
                         IrisAnalytics.getInstance(context).setAlarm(true, force = true)
                         if (dbCount % 50 == 0) {
-                            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("dbCountSend %d lines", dbCount)))
+                            ServerLogger.log(
+                                Priority.P1,
+                                "IRIS",
+                                mapOf(
+                                    "type" to String.format(
+                                        Locale.US,
+                                        "dbCountSend %d lines",
+                                        dbCount
+                                    )
+                                )
+                            )
                         }
                     }
                 }
             } catch (e: Throwable) {
-                ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "saveEvent", "err" to e.toString()))
+                ServerLogger.log(
+                    Priority.P1,
+                    "IRIS",
+                    mapOf("type" to "saveEvent", "err" to e.toString())
+                )
             }
         }
 
-    @SuppressLint("DefaultLocale")
     suspend fun savePerformanceEvent(data: String) =
         withContext(Dispatchers.IO) {
             try {
-                val tracking = PerformanceTracking(data, userSession.userId, userSession.deviceId,
+                val tracking = PerformanceTracking(
+                    data, userSession.userId, userSession.deviceId,
                     Calendar.getInstance().timeInMillis, GlobalConfig.VERSION_NAME,
-                    DeviceConnectionInfo.getCarrierName(context), isPowerSaveMode(context))
+                    DeviceConnectionInfo.getCarrierName(context), isPowerSaveMode(context)
+                )
                 trackingPerfDao.insert(tracking)
                 IrisLogger.getInstance(context).putSaveIrisEvent(tracking.toString())
                 val dbCount = trackingDao.getCount()
                 if (dbCount >= getLineDBFlush()) {
-                    ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "dbCountFlushPerf", "no" to dbCount.toString()))
+                    ServerLogger.log(
+                        Priority.P1,
+                        "IRIS",
+                        mapOf("type" to "dbCountFlushPerf", "no" to dbCount.toString())
+                    )
                     trackingDao.flush()
                 } else if (dbCount >= getLineDBSend()) {
                     // if the line is big, send it
                     if (dbCount % 5 == 0) {
                         IrisAnalytics.getInstance(context).setAlarm(true, force = true)
                         if (dbCount % 50 == 0) {
-                            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("dbCountSendPerf %d lines", dbCount)))
+                            ServerLogger.log(
+                                Priority.P1,
+                                "IRIS",
+                                mapOf(
+                                    "type" to String.format(
+                                        Locale.US,
+                                        "dbCountSendPerf %d lines",
+                                        dbCount
+                                    )
+                                )
+                            )
                         }
                     }
                 }
             } catch (e: Throwable) {
-                ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "saveEventPerf", "err" to e.toString()))
+                ServerLogger.log(
+                    Priority.P1,
+                    "IRIS",
+                    mapOf("type" to "saveEventPerf", "err" to e.toString())
+                )
             }
         }
 
@@ -120,17 +159,24 @@ class TrackingRepository private constructor(
             setRelicLog("getFromOldest", maxRow.toString())
             trackingDao.getFromOldest(maxRow)
         } catch (e: Throwable) {
-            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("getFromOldest %s", e.toString())))
+            ServerLogger.log(
+                Priority.P1,
+                "IRIS",
+                mapOf("type" to String.format(Locale.US, "getFromOldest %s", e.toString()))
+            )
             ArrayList()
         }
     }
 
-    @SuppressLint("DefaultLocale")
     private fun getFromOldestPerf(maxRow: Int): List<PerformanceTracking> {
         return try {
             trackingPerfDao.getFromOldest(maxRow)
         } catch (e: Throwable) {
-            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("getFromOldestPerf %s", e.toString())))
+            ServerLogger.log(
+                Priority.P1,
+                "IRIS",
+                mapOf("type" to String.format(Locale.US, "getFromOldestPerf %s", e.toString()))
+            )
             ArrayList()
         }
     }
@@ -149,52 +195,74 @@ class TrackingRepository private constructor(
         try {
             trackingDao.delete(data)
         } catch (ignored: Throwable) {
-            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("deletingData %s", ignored.toString())))
+            ServerLogger.log(
+                Priority.P1,
+                "IRIS",
+                mapOf("type" to String.format(Locale.US, "deletingData %s", ignored.toString()))
+            )
         }
     }
 
-    @SuppressLint("DefaultLocale")
     fun deletePerf(data: List<PerformanceTracking>) {
         try {
             trackingPerfDao.delete(data)
         } catch (ignored: Throwable) {
-            ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to String.format("deletingDataPerf %s", ignored.toString())))
+            ServerLogger.log(
+                Priority.P1,
+                "IRIS",
+                mapOf("type" to String.format(Locale.US, "deletingDataPerf %s", ignored.toString()))
+            )
         }
     }
 
     suspend fun sendSingleEvent(data: String, session: Session, eventName: String?): Boolean {
         try {
-            val dataRequest = TrackingMapper().transformSingleEvent(data, session.getSessionId(),
-                    userSession.userId, userSession.deviceId)
+            val dataRequest = TrackingMapper().transformSingleEvent(
+                data, session.getSessionId(),
+                userSession.userId, userSession.deviceId
+            )
             val requestBody = ApiService.parse(dataRequest)
             val response = apiService.sendSingleEventAsync(requestBody)
             val isSuccessFul = response.isSuccessful
             if (!isSuccessFul) {
-                ServerLogger.log(Priority.P1, "IRIS_REALTIME_ERROR", mapOf("type" to "not_success", "data" to data.take(ERROR_MAX_LENGTH).trim()))
+                ServerLogger.log(
+                    Priority.P1,
+                    "IRIS_REALTIME_ERROR",
+                    mapOf("type" to "not_success", "data" to data.take(ERROR_MAX_LENGTH).trim())
+                )
                 saveRealTimeCmData(eventName, data, session)
             }
             return isSuccessFul
         } catch (e: Exception) {
-            ServerLogger.log(Priority.P1, "IRIS_REALTIME_ERROR", mapOf("type" to "exception",
+            ServerLogger.log(
+                Priority.P1, "IRIS_REALTIME_ERROR", mapOf(
+                    "type" to "exception",
                     "data" to data.take(ERROR_MAX_LENGTH).trim(),
-                    "err" to Log.getStackTraceString(e).take(ERROR_MAX_LENGTH).trim()))
+                    "err" to Log.getStackTraceString(e).take(ERROR_MAX_LENGTH).trim()
+                )
+            )
             saveRealTimeCmData(eventName, data, session)
             return false
         }
     }
 
-    private suspend fun saveRealTimeCmData(eventName: String?, data: String, session: Session){
+    private suspend fun saveRealTimeCmData(eventName: String?, data: String, session: Session) {
         try {
             eventName?.let {
                 if (CM_REALTIME_EVENT_LIST.contains(it)) {
-                    val transformedEvent = TrackingMapper.reformatEvent(data, session.getSessionId()).toString()
+                    val transformedEvent =
+                        TrackingMapper.reformatEvent(data, session.getSessionId()).toString()
                     saveEvent(transformedEvent)
                 }
             }
-        }catch (e:Exception){
-            ServerLogger.log(Priority.P1, "IRIS_REALTIME_ERROR", mapOf("type" to "transform_exception",
+        } catch (e: Exception) {
+            ServerLogger.log(
+                Priority.P1, "IRIS_REALTIME_ERROR", mapOf(
+                    "type" to "transform_exception",
                     "data" to data.take(ERROR_MAX_LENGTH).trim(),
-                    "err" to Log.getStackTraceString(e).take(ERROR_MAX_LENGTH).trim()))
+                    "err" to Log.getStackTraceString(e).take(ERROR_MAX_LENGTH).trim()
+                )
+            )
         }
     }
 
@@ -240,8 +308,12 @@ class TrackingRepository private constructor(
                 }
             } else {
                 lastSuccessSent = false
-                ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "failedSendData",
-                    "data" to request.take(ERROR_MAX_LENGTH).trim()))
+                ServerLogger.log(
+                    Priority.P1, "IRIS", mapOf(
+                        "type" to "failedSendData",
+                        "data" to request.take(ERROR_MAX_LENGTH).trim()
+                    )
+                )
                 break
             }
             counterLoop++
@@ -288,8 +360,12 @@ class TrackingRepository private constructor(
                 }
             } else {
                 lastSuccessSent = false
-                ServerLogger.log(Priority.P1, "IRIS", mapOf("type" to "failedSendDataPerf",
-                    "data" to request.take(ERROR_MAX_LENGTH).trim()))
+                ServerLogger.log(
+                    Priority.P1, "IRIS", mapOf(
+                        "type" to "failedSendDataPerf",
+                        "data" to request.take(ERROR_MAX_LENGTH).trim()
+                    )
+                )
                 break
             }
             counterLoop++
@@ -305,7 +381,8 @@ class TrackingRepository private constructor(
         const val NEW_RELIC_CUSTOMER_TAG = "CURSOR_INDEX_OUTOFBOUND"
 
         fun isNetworkAvailable(context: Context): Boolean {
-            val connectivityManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val connectivityManager =
+                context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetworkInfo = connectivityManager.activeNetworkInfo
             return activeNetworkInfo != null && activeNetworkInfo.isConnected
         }
