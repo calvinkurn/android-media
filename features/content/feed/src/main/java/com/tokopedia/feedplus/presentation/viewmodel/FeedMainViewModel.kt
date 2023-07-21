31e32e1c77f900e964f746830b5f877aeccce3a5
Muhammad Furqan <furqan@tokopedia.com>
2023-07-21 10:59:12 +0700

    revert unintended changes

--- features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/viewmodel/FeedMainViewModel.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/presentation/viewmodel/FeedMainViewModel.kt
@@ -96,8 +96,8 @@ class FeedMainViewModel @Inject constructor(
 
             return feedCreateContentData is Success &&
                 feedCreateContentData.data.find {
-                it.type == CreateContentType.CREATE_SHORT_VIDEO
-            } != null
+                    it.type == CreateContentType.CREATE_SHORT_VIDEO
+                } != null
         }
 
     init {
