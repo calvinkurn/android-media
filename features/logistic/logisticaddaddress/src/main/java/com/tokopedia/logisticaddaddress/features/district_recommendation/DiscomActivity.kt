adf78cd3549a9745f058788fa2635625990f70be
Irpan - <irpan@tokopedia.com>
2023-07-25 12:36:29 +0700

    update to kotlin

--- features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/district_recommendation/DiscomActivity.kt
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/district_recommendation/DiscomActivity.kt
@@ -1,89 +1,75 @@
-package com.tokopedia.logisticaddaddress.features.district_recommendation;
+package com.tokopedia.logisticaddaddress.features.district_recommendation
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.tokopedia.abstraction.base.app.BaseMainApplication;
-import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity;
-import com.tokopedia.abstraction.common.di.component.BaseAppComponent;
-import com.tokopedia.abstraction.common.di.component.HasComponent;
-import com.tokopedia.localizationchooseaddress.analytics.ChooseAddressTracking;
-import com.tokopedia.logisticCommon.data.entity.address.Token;
-import com.tokopedia.purchase_platform.common.analytics.CheckoutAnalyticsChangeAddress;
-import com.tokopedia.user.session.UserSession;
-import com.tokopedia.user.session.UserSessionInterface;
-
-import static com.tokopedia.logisticaddaddress.features.district_recommendation.DiscomContract.Constant.ARGUMENT_DATA_TOKEN;
-import static com.tokopedia.logisticaddaddress.features.district_recommendation.DiscomContract.Constant.IS_LOCALIZATION;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.tokopedia.abstraction.base.app.BaseMainApplication
+import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
+import com.tokopedia.abstraction.common.di.component.BaseAppComponent
+import com.tokopedia.abstraction.common.di.component.HasComponent
+import com.tokopedia.design.R
+import com.tokopedia.localizationchooseaddress.analytics.ChooseAddressTracking.onClickCloseKotaKecamatan
+import com.tokopedia.logisticCommon.data.entity.address.Token
+import com.tokopedia.logisticaddaddress.features.district_recommendation.DiscomFragment.Companion.newInstance
+import com.tokopedia.purchase_platform.common.analytics.CheckoutAnalyticsChangeAddress
+import com.tokopedia.user.session.UserSession
+import com.tokopedia.user.session.UserSessionInterface
 
 /**
  * Created by Irfan Khoirul on 17/11/18.
  * Deeplink: DISTRICT_RECOMMENDATION_SHOP_SETTINGS
  */
-
-public class DiscomActivity extends BaseSimpleActivity
-        implements HasComponent, DiscomFragment.ActionListener {
-
-    private CheckoutAnalyticsChangeAddress analytics;
-    private Boolean isLocalization = false;
-
-    public static Intent newInstance(Activity activity, Token token, Boolean isLocalization) {
-        Intent intent = new Intent(activity, DiscomActivity.class);
-        intent.putExtra(ARGUMENT_DATA_TOKEN, token);
-        intent.putExtra(IS_LOCALIZATION, isLocalization);
-        return intent;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        analytics = new CheckoutAnalyticsChangeAddress();
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setElevation(0);
-            toolbar.setNavigationIcon(com.tokopedia.design.R.drawable.ic_close_thin);
+class DiscomActivity : BaseSimpleActivity(), HasComponent<Any?>, DiscomFragment.ActionListener {
+    private var analytics: CheckoutAnalyticsChangeAddress? = null
+    private var isLocalization = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        analytics = CheckoutAnalyticsChangeAddress()
+        if (supportActionBar != null) {
+            supportActionBar!!.elevation = 0f
+            toolbar.setNavigationIcon(R.drawable.ic_close_thin)
         }
     }
 
-    @Override
-    protected Fragment getNewFragment() {
-        Token  token = getIntent().getParcelableExtra(ARGUMENT_DATA_TOKEN);
-        isLocalization = getIntent().getBooleanExtra(IS_LOCALIZATION, false);
-        if (token == null) {
-            return DiscomFragment.newInstance(isLocalization);
+    override fun getNewFragment(): Fragment? {
+        val token = intent.getParcelableExtra<Token>(DiscomContract.Constant.ARGUMENT_DATA_TOKEN)
+        isLocalization = intent.getBooleanExtra(DiscomContract.Constant.IS_LOCALIZATION, false)
+        return if (token == null) {
+            newInstance(isLocalization)
         } else {
-            return DiscomFragment.newInstance(token, isLocalization);
+            newInstance(token, isLocalization)
         }
     }
 
-    @Override
-    public BaseAppComponent getComponent() {
-        return ((BaseMainApplication) getApplication()).getBaseAppComponent();
+    override fun getComponent(): BaseAppComponent {
+        return (application as BaseMainApplication).baseAppComponent
     }
 
-    @Override
-    public void onBackPressed() {
-        UserSessionInterface userSession = new UserSession(this);
-        if (isLocalization) ChooseAddressTracking.INSTANCE.onClickCloseKotaKecamatan(userSession.getUserId());
-        else gtmOnBackPressClicked();
-        super.onBackPressed();
+    override fun onBackPressed() {
+        val userSession: UserSessionInterface = UserSession(this)
+        if (isLocalization) onClickCloseKotaKecamatan(userSession.userId) else gtmOnBackPressClicked()
+        super.onBackPressed()
     }
 
-    @Override
-    public void gtmOnBackPressClicked() {
-        analytics.eventClickShippingCartChangeAddressClickXPojokKiriKotaAtauKecamatanPadaTambahAddress();
+    override fun gtmOnBackPressClicked() {
+        analytics!!.eventClickShippingCartChangeAddressClickXPojokKiriKotaAtauKecamatanPadaTambahAddress()
     }
 
-    @Override
-    public void gtmOnDistrictDropdownSelectionItemClicked(String districtName) {
-        analytics.eventClickShippingCartChangeAddressClickChecklistKotaAtauKecamatanPadaTambahAddress(districtName);
+    override fun gtmOnDistrictDropdownSelectionItemClicked(districtName: String) {
+        analytics!!.eventClickShippingCartChangeAddressClickChecklistKotaAtauKecamatanPadaTambahAddress(districtName)
     }
 
-    @Override
-    public void gtmOnClearTextDistrictRecommendationInput() {
-        analytics.eventClickShippingCartChangeAddressClickXPojokKananKotaAtauKecamatanPadaTambahAddress();
+    override fun gtmOnClearTextDistrictRecommendationInput() {
+        analytics!!.eventClickShippingCartChangeAddressClickXPojokKananKotaAtauKecamatanPadaTambahAddress()
+    }
+
+    companion object {
+        fun newInstance(activity: Activity?, token: Token?, isLocalization: Boolean?): Intent {
+            val intent = Intent(activity, DiscomActivity::class.java)
+            intent.putExtra(DiscomContract.Constant.ARGUMENT_DATA_TOKEN, token)
+            intent.putExtra(DiscomContract.Constant.IS_LOCALIZATION, isLocalization)
+            return intent
+        }
     }
 }
