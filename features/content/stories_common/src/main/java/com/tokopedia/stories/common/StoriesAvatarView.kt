8cb14bb36e46f04dcee4118655469342ac5adb3d
jegul <kenny.hadisaputra@tokopedia.com>
2023-07-25 11:39:51 +0700

    feat: support seen stories border color

--- features/content/stories_common/src/main/java/com/tokopedia/stories/common/StoriesAvatarView.kt
+++ features/content/stories_common/src/main/java/com/tokopedia/stories/common/StoriesAvatarView.kt
@@ -1,8 +1,9 @@
 package com.tokopedia.stories.common
 
 import android.content.Context
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.util.AttributeSet
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,8 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.AbstractComposeView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.tokopedia.nest.components.NestImage
 import com.tokopedia.nest.principles.ui.NestTheme
@@ -28,7 +31,7 @@ import com.tokopedia.nest.principles.ui.NestTheme
 class StoriesAvatarView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
+    defStyleAttr: Int = 0
 ) : AbstractComposeView(
     context,
     attrs,
@@ -36,6 +39,7 @@ class StoriesAvatarView @JvmOverloads constructor(
 ) {
 
     private var mImageUrl by mutableStateOf("")
+    private var hasUnseenStories by mutableStateOf(false)
 
     fun setImageUrl(imageUrl: String) {
         mImageUrl = imageUrl
@@ -43,51 +47,64 @@ class StoriesAvatarView @JvmOverloads constructor(
 
     @Composable
     override fun Content() {
-        StoriesAvatarContent(mImageUrl)
+        StoriesAvatarContent(mImageUrl, hasUnseenStories)
     }
 }
 
 @Composable
 private fun StoriesAvatarContent(
     imageUrl: String,
+    hasUnseenStories: Boolean,
     modifier: Modifier = Modifier,
+    imageToBorderPadding: Dp = 8.dp
 ) {
     NestTheme {
         Box(
             modifier
                 .fillMaxSize()
                 .clip(CircleShape)
-                .background(
-                    Brush.linearGradient(
-                        0f to Color(0xFF69F2E2),
-                        1f to Color(0xFF00AA5B),
-                    )
+                .border(
+                    width = if (hasUnseenStories) 2.dp else 1.dp,
+                    brush = if (hasUnseenStories) {
+                        Brush.linearGradient(
+                            0f to Color(0xFF69F2E2),
+                            1f to Color(0xFF00AA5B)
+                        )
+                    } else {
+                        SolidColor(Color(0xFFD6DFEB))
+                    },
+                    shape = CircleShape
                 )
         ) {
-            Box(
+            NestImage(
+                imageUrl = imageUrl,
                 Modifier
-                    .padding(6.dp)
+                    .matchParentSize()
+                    .padding(imageToBorderPadding)
                     .clip(CircleShape)
-                    .background(NestTheme.colors.NN._0)
-                    .fillMaxSize()
-            ) {
-                NestImage(
-                    imageUrl = imageUrl,
-                    Modifier
-                        .matchParentSize()
-                        .padding(2.dp)
-                        .clip(CircleShape),
-                )
-            }
+            )
         }
     }
 }
 
 @Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
-private fun StoriesAvatarPreview() {
+private fun NoUnseenStoriesAvatarPreview() {
     StoriesAvatarContent(
         "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg",
+        false,
+        Modifier.size(200.dp)
+    )
+}
+
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun HasUnseenStoriesAvatarPreview() {
+    StoriesAvatarContent(
+        "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg",
+        true,
         Modifier.size(200.dp)
     )
 }
