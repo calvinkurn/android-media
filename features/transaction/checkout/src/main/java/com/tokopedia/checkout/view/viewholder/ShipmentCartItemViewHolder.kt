bb8e842e9ca829d78033adf97c6165016ef01508
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-07-26 15:31:38 +0700

    fix mandatory checkbox layout

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentCartItemViewHolder.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/view/viewholder/ShipmentCartItemViewHolder.kt
@@ -21,6 +21,7 @@ import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.extensions.view.visible
 import com.tokopedia.logisticcart.shipping.model.CartItemModel
 import com.tokopedia.purchase_platform.common.constant.AddOnConstant.ADD_ON_PRODUCT_STATUS_CHECK
+import com.tokopedia.purchase_platform.common.constant.AddOnConstant.ADD_ON_PRODUCT_STATUS_MANDATORY
 import com.tokopedia.purchase_platform.common.databinding.ItemProductInfoAddOnBinding
 import com.tokopedia.purchase_platform.common.databinding.ItemShipmentAddonProductItemBinding
 import com.tokopedia.purchase_platform.common.feature.addons.data.model.AddOnProductDataItemModel
@@ -459,7 +460,16 @@ class ShipmentCartItemViewHolder(
                         tvShipmentAddOnPrice.text = CurrencyFormatUtil
                             .convertPriceValueToIdrFormat(addon.price.toLong(), false)
                             .removeDecimalSuffix()
-                        cbAddonItem.isChecked = (addon.status == ADD_ON_PRODUCT_STATUS_CHECK)
+                        if (addon.status == ADD_ON_PRODUCT_STATUS_CHECK) {
+                            cbAddonItem.isChecked = true
+                            cbAddonItem.isEnabled = true
+                        } else if (addon.status == ADD_ON_PRODUCT_STATUS_MANDATORY) {
+                            cbAddonItem.isChecked = true
+                            cbAddonItem.isEnabled = false
+                        } else {
+                            cbAddonItem.isChecked = false
+                            cbAddonItem.isEnabled = true
+                        }
                         cbAddonItem.setOnCheckedChangeListener { compoundButton, isChecked ->
                             delayChangeCheckboxAddOnState?.cancel()
                             delayChangeCheckboxAddOnState = GlobalScope.launch(Dispatchers.Main) {
