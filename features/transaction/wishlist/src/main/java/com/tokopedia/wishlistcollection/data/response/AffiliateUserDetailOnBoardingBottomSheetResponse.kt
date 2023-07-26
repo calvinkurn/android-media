c359171bb73b446c7275cbda20ae1c0ddc79f0d2
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-07-26 18:03:49 +0530

    feat: fixed response key

--- features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/data/response/AffiliateUserDetailOnBoardingBottomSheetResponse.kt
+++ features/transaction/wishlist/src/main/java/com/tokopedia/wishlistcollection/data/response/AffiliateUserDetailOnBoardingBottomSheetResponse.kt
@@ -3,11 +3,11 @@ package com.tokopedia.wishlistcollection.data.response
 import com.google.gson.annotations.SerializedName
 
 data class AffiliateUserDetailOnBoardingBottomSheetResponse(
-    @SerializedName("data")
+    @SerializedName("affiliateUserDetail")
     val affiliateUserDetail: AffiliateUserDetail
 ) {
     data class AffiliateUserDetail(
         @SerializedName("IsRegistered")
-        val isRegistered: Boolean = false,
+        val isRegistered: Boolean = false
     )
 }
