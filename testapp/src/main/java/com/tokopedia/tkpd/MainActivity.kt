a96186bab934462973f9519d970e0ca323a96721
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-07-31 10:15:49 +0700

    fix: undo changes for testapp main activity

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return "tokopedia://cart"
+        return ""
     }
 
     data class Model(
