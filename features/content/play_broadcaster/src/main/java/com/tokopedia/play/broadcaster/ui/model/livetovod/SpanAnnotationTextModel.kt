d219180c89231b655e94cf1911a6fcff9d0df915
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-07-31 09:22:52 +0700

    fix: remove unused class

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/ui/model/livetovod/SpanAnnotationTextModel.kt
+++
@@ -1,8 +0,0 @@
-package com.tokopedia.play.broadcaster.ui.model.livetovod
-
-data class SpanAnnotationTextModel(
-    val item: String,
-    val start: Int,
-    val end: Int,
-    val link: String,
-)
