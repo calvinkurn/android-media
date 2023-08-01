4e45c484dd5293227b7f8c403111622c31c36ca8
Muhammad Furqan <furqan@tokopedia.com>
2023-08-01 10:49:14 +0700

    Resolve Reviews from Kenny

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -22,8 +22,6 @@ public interface ApplinkConst {
     String AMP_FIND = "tokopedia://amp/find";
     String FEED_HASHTAG = "tokopedia://feed/hashtag/{hashtag}";
     String FEED_DETAILS = "tokopedia://feedcommunicationdetail";
-
-    String FEED_UNIFIED_DETAILS_PATTERN = "tokopedia://content/detail/{post_id}";
     String FEED_PlAY_LIVE_DETAIL = "tokopedia://feedplaylivedetail";
     String HOME_CATEGORY = "tokopedia://home/category";
     String HOME_HOT_HOST = "hot";
