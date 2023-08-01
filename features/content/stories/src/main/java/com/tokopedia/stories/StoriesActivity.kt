db68a6f90d904873b06dc4c6aa1db67531d00305
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-08-01 10:28:10 +0700

    `feat`: add applink for Stories

--- features/content/stories/src/main/java/com/tokopedia/stories/StoriesActivity.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/StoriesActivity.kt
@@ -1,20 +1,29 @@
 package com.tokopedia.stories
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.stories.databinding.ActivityStoriesBinding
 
-class StoriesActivity : AppCompatActivity() {
+class StoriesActivity : BaseActivity() {
 
     private var binding: ActivityStoriesBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupView()
+        getData()
     }
 
     private fun setupView() {
         binding = ActivityStoriesBinding.inflate(layoutInflater)
         setContentView(binding?.root)
     }
+
+    private fun getData() {
+        val data = intent.data ?: return
+        val pathSegment = data.pathSegments
+        val shopId = pathSegment[1]
+        val storiesId = pathSegment[2]
+    }
+
 }
