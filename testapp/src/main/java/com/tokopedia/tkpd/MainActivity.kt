249221f84f415d8bcc9bcfdddcfc4893c5aa4eed
Yuda <yuda.putra@tokopedia.com>
2023-08-04 13:54:26 +0700

    Fix quota at voucher list

--- testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
+++ testapp/src/main/java/com/tokopedia/tkpd/MainActivity.kt
@@ -162,7 +162,7 @@ class MainActivity : AppCompatActivity() {
         /*
          * Put your default applink here
          */
-        return "sellerapp://seller-mvc/list"
+        return ""
     }
 
     data class Model(
