1d68f45b4df8762487a6113e4ff9774a5131b228
isfaaghyth <isfaaghyth@gmail.com>
2023-08-04 08:50:58 +0700

    feat: set visiblity of guideline and create a snap effect

--- features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/gesture/java/MultiTouchListener.java
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/gesture/java/MultiTouchListener.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.tokopedia.editor.ui.gesture.listener.OnGestureControl;
 import com.tokopedia.editor.ui.gesture.listener.OnMultiTouchListener;
 import com.tokopedia.editor.ui.model.AddTextModel;
+import com.tokopedia.editor.ui.widget.GridGuidelineView;
 
 public class MultiTouchListener implements View.OnTouchListener {
 
@@ -20,16 +21,20 @@ public class MultiTouchListener implements View.OnTouchListener {
     float maximumScale = 10.0f;
     private int activePointerId = INVALID_POINTER_ID;
     private float prevX, prevY;
-    private ScaleGestureDetector scaleGestureDetector;
 
+    private ScaleGestureDetector scaleGestureDetector;
     private OnMultiTouchListener onMultiTouchListener;
     private OnGestureControl onGestureControl;
     boolean isTextPinchZoomable;
 
-    public MultiTouchListener(Context context) {
+    private GridGuidelineView gridGuidelineView;
+
+    public MultiTouchListener(Context context, GridGuidelineView guideline) {
         isTextPinchZoomable = true;
         scaleGestureDetector = new ScaleGestureDetector(new ScaleGestureListener(this));
         gestureListener = new GestureDetector(context, new GestureListener());
+
+        gridGuidelineView = guideline;
     }
 
     private float adjustAngle(float degrees) {
@@ -113,6 +118,34 @@ public class MultiTouchListener implements View.OnTouchListener {
                     if (!scaleGestureDetector.isInProgress()) {
                         adjustTranslation(view, currX - prevX, currY - prevY);
                     }
+
+                    // Calculate distances from the center of the parent
+                    float centerX = view.getX() + view.getWidth() / 2f;
+                    float centerY = view.getY() + view.getHeight() / 2f;
+                    float parentCenterX = ((View) view.getParent()).getWidth() / 2f;
+                    float parentCenterY = ((View) view.getParent()).getHeight() / 2f;
+
+                    // Check if the TextView is close to the vertical center of the parent
+                    boolean isAlignedWithCenterX = Math.abs(centerX - parentCenterX) <= 20f;
+
+                    // Check if the TextView is close to the horizontal center of the parent
+                    boolean isAlignedWithCenterY = Math.abs(centerY - parentCenterY) <= 20f;
+
+                    // Snap effect for horizontal alignment
+                    if (isAlignedWithCenterX) {
+                        float newX = parentCenterX - view.getWidth() / 2f;
+                        view.setX(newX);
+                    }
+
+                    // Snap effect for vertical alignment
+                    if (isAlignedWithCenterY) {
+                        float newY = parentCenterY - view.getHeight() / 2f;
+                        view.setY(newY);
+                    }
+
+                    // Show/hide vertical and horizontal guidelines based on alignment
+                    gridGuidelineView.setShowVerticalLine(isAlignedWithCenterX);
+                    gridGuidelineView.setShowHorizontalLine(isAlignedWithCenterY);
                 }
                 break;
             case MotionEvent.ACTION_CANCEL:
@@ -120,6 +153,17 @@ public class MultiTouchListener implements View.OnTouchListener {
                 break;
             case MotionEvent.ACTION_UP:
                 activePointerId = INVALID_POINTER_ID;
+
+                if (isAlignedWithCenterX(view)) {
+                    view.setX(((View) view.getParent()).getWidth() / 2f - view.getWidth() / 2f);
+                }
+
+                if (isAlignedWithCenterY(view)) {
+                    view.setY(((View) view.getParent()).getHeight() / 2f - view.getHeight() / 2f);
+                }
+
+                gridGuidelineView.setShowVerticalLine(false);
+                gridGuidelineView.setShowHorizontalLine(false);
                 break;
             case MotionEvent.ACTION_POINTER_UP:
                 int pointerIndexPointerUp = (action & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
@@ -133,6 +177,20 @@ public class MultiTouchListener implements View.OnTouchListener {
                 break;
         }
         return true;
+    }
+
+    private boolean isAlignedWithCenterX(View view) {
+        float parentWidth = ((View) view.getParent()).getWidth();
+        float centerX = parentWidth / 2;
+        float viewX = (view.getX() + view.getWidth()) / 2;
+        return Math.abs(centerX - viewX) <= 20;
+    }
+
+    private boolean isAlignedWithCenterY(View view) {
+        float parentHeight = ((View) view.getParent()).getHeight();
+        float centerY = parentHeight / 2;
+        float viewY = (view.getY() + view.getHeight()) / 2;
+        return Math.abs(centerY - viewY) <= 20;
     }
 
     private void setOnMultiTouchListener(OnMultiTouchListener onMultiTouchListener) {
