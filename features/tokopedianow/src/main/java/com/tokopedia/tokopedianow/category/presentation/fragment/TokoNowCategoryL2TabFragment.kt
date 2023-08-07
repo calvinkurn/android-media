987c3b8ab989e9e17af04db9bb38248b8f62a35a
rezagama <rezagamahidayat@gmail.com>
2023-08-07 16:17:24 +0700

    Remove set itemAnimator null

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/category/presentation/fragment/TokoNowCategoryL2TabFragment.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/category/presentation/fragment/TokoNowCategoryL2TabFragment.kt
@@ -231,7 +231,6 @@ open class TokoNowCategoryL2TabFragment : Fragment() {
                 spanSizeLookup = createSpanSizeLookup()
             }
             adapter = categoryAdapter
-            itemAnimator = null
             addProductItemDecoration()
         }
     }
