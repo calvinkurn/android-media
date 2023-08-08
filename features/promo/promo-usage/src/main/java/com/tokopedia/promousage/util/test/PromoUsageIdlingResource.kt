619ea8bb722175074f9a9baff08648741069c256
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-08-08 08:50:16 +0700

    feat: add dummy idling resource

---
+++ features/promo/promo-usage/src/main/java/com/tokopedia/promousage/util/test/PromoUsageIdlingResource.kt
@@ -0,0 +1,14 @@
+package com.tokopedia.promousage.util.test
+
+object PromoUsageIdlingResource {
+
+    private const val RESOURCE_NAME = "PromoUsage"
+
+    fun increment() {
+        // TODO: Implement increment idling resource for testing
+    }
+
+    fun decrement() {
+        // TODO: Implement increment idling resource for testing
+    }
+}
