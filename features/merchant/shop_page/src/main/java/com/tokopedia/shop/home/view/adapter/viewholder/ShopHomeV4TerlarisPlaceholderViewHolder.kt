99542abcad1a63e9d5d02b8ee329882bd11fc9c6
yogieputra8 <yogie.putra@tokopedia.com>
2023-08-09 08:36:31 +0700

    Add placeholder viewholder for terlaris widget

---
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeV4TerlarisPlaceholderViewHolder.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.shop.home.view.adapter.viewholder
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.shop.R
+import com.tokopedia.shop.home.view.model.BaseShopHomeWidgetUiModel
+
+class ShopHomeV4TerlarisPlaceholderViewHolder(
+    itemView: View
+): AbstractViewHolder<BaseShopHomeWidgetUiModel>(itemView) {
+
+    companion object {
+        @LayoutRes
+        val LAYOUT = R.layout.item_shop_home_v4_terlaris_placeholder
+    }
+
+    override fun bind(element: BaseShopHomeWidgetUiModel?) {
+        TODO("Not yet implemented")
+    }
+}
