c313142b773465b627d11fae54f07e641fd02513
faisalramd <faisal.ramdani@tokopedia.com>
2023-08-10 03:23:23 +0700

    fix save addon empty

--- features/merchant/product_service_widget/src/main/java/com/tokopedia/addon/presentation/viewmodel/AddOnViewModel.kt
+++ features/merchant/product_service_widget/src/main/java/com/tokopedia/addon/presentation/viewmodel/AddOnViewModel.kt
@@ -110,7 +110,6 @@ class AddOnViewModel @Inject constructor(
     }
 
     fun saveAddOnState(cartId: Long, source: String) {
-        if (AddOnMapper.flatmapToChangedAddonSelection(modifiedAddOnGroups.value).isEmpty()) return
         mSaveSelectionResult.value = Success(emptyList())
         saveAddOnStateUseCase.setParams(
             AddOnMapper.mapToSaveAddOnStateRequest(
