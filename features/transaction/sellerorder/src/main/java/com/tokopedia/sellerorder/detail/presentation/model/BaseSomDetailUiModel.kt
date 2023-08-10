2dfa9c031fbcc18be2a71240b5da3543557acebc
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-08-10 16:38:37 +0700

    feat: delete unused class

--- features/transaction/sellerorder/src/main/java/com/tokopedia/sellerorder/detail/presentation/model/BaseSomDetailUiModel.kt
+++
@@ -1,6 +0,0 @@
-package com.tokopedia.sellerorder.detail.presentation.model
-
-import com.tokopedia.abstraction.base.view.adapter.Visitable
-import com.tokopedia.sellerorder.detail.presentation.adapter.factory.SomDetailAdapterFactoryImpl
-
-interface BaseSomDetailUiModel: Visitable<SomDetailAdapterFactoryImpl>
