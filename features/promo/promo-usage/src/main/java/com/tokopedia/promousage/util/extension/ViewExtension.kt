60a57d4d46dbea0990ae1a2ffec9c4e4adb9f9db
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-08-11 12:09:08 +0700

    fix: fix extensions

---
+++ features/promo/promo-usage/src/main/java/com/tokopedia/promousage/util/extension/ViewExtension.kt
@@ -0,0 +1,17 @@
+package com.tokopedia.promousage.util.extension
+
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.widget.ImageView
+
+internal fun ImageView.grayscale() {
+    val colorMatrix = ColorMatrix().apply {
+        setSaturation(0f)
+    }
+    val colorFilter = ColorMatrixColorFilter(colorMatrix)
+    this.colorFilter = colorFilter
+}
+
+internal fun ImageView.removeGrayscale() {
+    this.colorFilter = null
+}
