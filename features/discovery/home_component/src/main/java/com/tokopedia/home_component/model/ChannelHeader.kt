4568699be1c86cff4d9a121c637ddba193b53c0f
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-08-11 08:55:00 +0700

    move layout strategy to channel header model

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/model/ChannelHeader.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/model/ChannelHeader.kt
@@ -1,5 +1,8 @@
 package com.tokopedia.home_component.model
 
+import com.tokopedia.home_component.customview.header.HeaderLayoutStrategy
+import com.tokopedia.home_component.customview.header.HeaderLayoutStrategyFactory
+
 data class ChannelHeader(
         val id: String = "",
         val name: String = "",
@@ -10,8 +13,10 @@ data class ChannelHeader(
         val url: String = "",
         val backColor: String = "",
         val backImage: String = "",
-        val textColor: String = ""
+        val textColor: String = "",
+        val useHeaderRevamp: Boolean = false,
 ) {
+    internal val layoutStrategy: HeaderLayoutStrategy = HeaderLayoutStrategyFactory.create(useHeaderRevamp)
 
     fun getTitleSubtitle(defaultTitle: String, defaultSubtitle: String): Pair<String, String> {
         return if (name.isEmpty()) defaultTitle to defaultSubtitle
--- features/discovery/home_component_header/src/main/java/com/tokopedia/home_component_header/model/ChannelHeader.kt
+++ features/discovery/home_component_header/src/main/java/com/tokopedia/home_component_header/model/ChannelHeader.kt
@@ -1,5 +1,8 @@
 package com.tokopedia.home_component_header.model
 
+import com.tokopedia.home_component_header.view.HeaderLayoutStrategy
+import com.tokopedia.home_component_header.view.HeaderLayoutStrategyFactory
+
 data class ChannelHeader(
         val id: String = "",
         val name: String = "",
@@ -13,5 +16,8 @@ data class ChannelHeader(
         val textColor: String = "",
         //  Data from channel level
         val channelId: String = "",
-        var serverTimeOffset: Long = 0,
-)
+        val serverTimeOffset: Long = 0,
+        val useHeaderRevamp: Boolean = false,
+) {
+    internal val layoutStrategy: HeaderLayoutStrategy = HeaderLayoutStrategyFactory.create(useHeaderRevamp)
+}
