3f1f3dbaad002940f416c6920ce2b07bef54e217
meyta.taliti <meyta.taliti@tokopedia.com>
2023-08-14 16:20:18 +0700

    feat: add spacing before colon

--- features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/uimodel/NetworkResult.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/uimodel/NetworkResult.kt
@@ -4,8 +4,8 @@ package com.tokopedia.content.common.producttag.view.uimodel
  * Created By : Jonathan Darwin on May 18, 2022
  */
 sealed class NetworkResult<out T> {
-    data class Success<T>(val data: T): NetworkResult<T>()
-    data class Error<Nothing>(val error: Throwable): NetworkResult<Nothing>()
-    object Loading: NetworkResult<Nothing>()
-    object Unknown: NetworkResult<Nothing>()
+    data class Success<T>(val data: T) : NetworkResult<T>()
+    data class Error<Nothing>(val error: Throwable) : NetworkResult<Nothing>()
+    object Loading : NetworkResult<Nothing>()
+    object Unknown : NetworkResult<Nothing>()
 }
