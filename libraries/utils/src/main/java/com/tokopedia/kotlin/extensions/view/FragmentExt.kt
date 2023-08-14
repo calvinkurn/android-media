85c6868891502536d87f878406f66b142644b404
Kelvindo Sutan <kelvindo.sutan@tokopedia.com>
2023-08-14 11:20:04 +0700

    remove unused changes

--- libraries/utils/src/main/java/com/tokopedia/kotlin/extensions/view/FragmentExt.kt
+++ libraries/utils/src/main/java/com/tokopedia/kotlin/extensions/view/FragmentExt.kt
@@ -26,10 +26,6 @@ fun Fragment.getStringArg(key: String, defValue: String = ""): Lazy<String> = la
     arguments?.getString(key, defValue) ?: defValue
 }
 
-fun <T>Fragment.getParcelableArg(key: String, defValue: T): Lazy<T> = lazy {
-    arguments?.getParcelable(key) ?: defValue
-}
-
 fun Fragment.getIntArg(key: String, defValue: Int = 0): Lazy<Int> = lazy {
     arguments?.getInt(key, defValue) ?: defValue
 }
