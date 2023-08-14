8f151912bdd1be0b14c5705dc78c2e3ada082fb3
meyta.taliti <meyta.taliti@tokopedia.com>
2023-08-14 16:22:36 +0700

    feat: reverting sealed class to interface

--- features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/uimodel/NetworkResult.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/producttag/view/uimodel/NetworkResult.kt
@@ -3,9 +3,9 @@ package com.tokopedia.content.common.producttag.view.uimodel
 /**
  * Created By : Jonathan Darwin on May 18, 2022
  */
-sealed class NetworkResult<out T> {
-    data class Success<T>(val data: T) : NetworkResult<T>()
-    data class Error<Nothing>(val error: Throwable) : NetworkResult<Nothing>()
-    object Loading : NetworkResult<Nothing>()
-    object Unknown : NetworkResult<Nothing>()
+sealed interface NetworkResult<out T> {
+    data class Success<T>(val data: T) : NetworkResult<T>
+    data class Error<Nothing>(val error: Throwable) : NetworkResult<Nothing>
+    object Loading : NetworkResult<Nothing>
+    object Unknown : NetworkResult<Nothing>
 }
