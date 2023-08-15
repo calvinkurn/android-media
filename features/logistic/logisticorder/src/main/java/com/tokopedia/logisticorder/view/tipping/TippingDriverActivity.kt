197b9f08853e5485c86d91bd13971f174a88f8a8
hiradevina <fakhira.devina@tokopedia.com>
2023-08-15 12:17:40 +0700

    feat: hide toolbat

--- features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/view/tipping/TippingDriverActivity.kt
+++ features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/view/tipping/TippingDriverActivity.kt
@@ -1,10 +1,17 @@
 package com.tokopedia.logisticorder.view.tipping
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
 import com.tokopedia.abstraction.base.view.activity.BaseSimpleActivity
 import com.tokopedia.logisticorder.utils.TippingConstant.PARAM_ORDER_ID
 
 class TippingDriverActivity : BaseSimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        supportActionBar?.hide()
+    }
 
     override fun getNewFragment(): Fragment? {
         val orderId = intent?.data?.getQueryParameter(PARAM_ORDER_ID).orEmpty()
