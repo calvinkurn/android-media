0d1ddc954703b7869c27810da1b7da02ff610e9f
hansenpw <hansen.wijaya@tokopedia.com>
2023-08-15 08:40:01 +0700

    temp ut calculate total

---
+++ features/transaction/checkout/src/test/java/com/tokopedia/checkout/revamp/view/CheckoutViewModelCalculatorTest.kt
@@ -0,0 +1,10 @@
+package com.tokopedia.checkout.revamp.view
+
+import org.junit.Test
+
+class CheckoutViewModelCalculatorTest : BaseCheckoutViewModelTest() {
+
+    @Test
+    fun calculate_total() {
+    }
+}
