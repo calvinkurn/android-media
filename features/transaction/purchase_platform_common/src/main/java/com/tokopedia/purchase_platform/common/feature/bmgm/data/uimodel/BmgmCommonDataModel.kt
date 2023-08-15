3852f744974779ec7b10df881b4b4f66befa4eac
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-08-15 15:38:15 +0700

    bmgm common data

--- features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/bmgm/data/uimodel/BmgmCommonDataModel.kt
+++ features/transaction/purchase_platform_common/src/main/java/com/tokopedia/purchase_platform/common/feature/bmgm/data/uimodel/BmgmCommonDataModel.kt
@@ -1,10 +1,10 @@
-package com.tokopedia.purchase_platform.common.feature.bmgm.uimodel
+package com.tokopedia.purchase_platform.common.feature.bmgm.data.uimodel
 
 /**
  * Created by @ilhamsuaib on 07/08/23.
  */
 
-class BmgmCommonDataUiModel(
+class BmgmCommonDataModel(
     val offerId: Long = 0L,
     val offerName: String = "",
     val offerMessage: String = "",
@@ -13,29 +13,33 @@ class BmgmCommonDataUiModel(
     val priceBeforeBenefit: Double = 0.0,
     val finalPrice: Double = 0.0,
     val showMiniCartFooter: Boolean = false,
-    val tiersApplied: List<TierUiModel> = emptyList()
+    val tiersApplied: List<TierModel> = emptyList()
 ) {
 
     companion object {
         const val PARAM_KEY_BMGM_DATA = "bmgm_data"
+
+        const val NON_DISCOUNT_TIER_ID = 0L
     }
 
-    data class TierUiModel(
-        val tierId: Long = 0L,
+    data class TierModel(
+        val tierId: Long = NON_DISCOUNT_TIER_ID,
         val tierMessage: String = "",
         val tierDiscountStr: String = "",
         val priceBeforeBenefit: Double = 0.0,
         val priceAfterBenefit: Double = 0.0,
-        val products: List<ProductUiModel> = emptyList()
+        val products: List<ProductModel> = emptyList()
     ) {
 
         /**
          * tier_id = 0 means the product is not in discount group
          * */
-        fun isNonDiscountProducts(): Boolean = tierId == 0L
+        fun isNonDiscountProducts(): Boolean {
+            return tierId == NON_DISCOUNT_TIER_ID
+        }
     }
 
-    data class ProductUiModel(
+    data class ProductModel(
         val productId: String = "",
         val warehouseId: String = "",
         val productName: String = "",
