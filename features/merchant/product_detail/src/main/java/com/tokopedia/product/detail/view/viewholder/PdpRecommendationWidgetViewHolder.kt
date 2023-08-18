a30b4f6a6a1412e0f747b3583b83b56bf2d8031b
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-08-18 21:37:51 +0700

    [PDP] Adjust recom global implementation on PDP to handle edge cases when refreshing the data

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/PdpRecommendationWidgetViewHolder.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/PdpRecommendationWidgetViewHolder.kt
@@ -3,7 +3,6 @@ package com.tokopedia.product.detail.view.viewholder
 import android.view.View
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
 import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
-import com.tokopedia.kotlin.extensions.view.showWithCondition
 import com.tokopedia.product.detail.R
 import com.tokopedia.product.detail.data.model.datamodel.ComponentTrackDataModel
 import com.tokopedia.product.detail.data.model.datamodel.PdpRecommendationWidgetDataModel
@@ -26,24 +25,22 @@ class PdpRecommendationWidgetViewHolder(
     private val binding: ItemRecomViewHolderBinding? by viewBinding()
 
     override fun bind(element: PdpRecommendationWidgetDataModel) {
-        // when `PdpRecommendationWidgetDataModel` initially created it don't have productID
-        // so we need to wait until it get productID data from the P1 data
-        val hasValidProductID = element.recommendationWidgetModel.metadata.productIds.isNotEmpty()
-        if (hasValidProductID) {
-            element.recommendationWidgetModel.let {
-                binding?.recomWidget?.bind(it)
-                binding?.root?.addOnImpressionListener(element.impressHolder) {
-                    listener.onImpressComponent(
-                        ComponentTrackDataModel(
-                            componentType = element.type(),
-                            componentName = element.name(),
-                            adapterPosition = bindingAdapterPosition
-                        )
+        element.recommendationWidgetModel.let {
+            binding?.recomWidget?.bind(it)
+            binding?.root?.addOnImpressionListener(element.impressHolder) {
+                listener.onImpressComponent(
+                    ComponentTrackDataModel(
+                        componentType = element.type(),
+                        componentName = element.name(),
+                        adapterPosition = bindingAdapterPosition
                     )
-                }
+                )
             }
         }
-        binding?.root?.showWithCondition(hasValidProductID)
+    }
+
+    override fun bind(element: PdpRecommendationWidgetDataModel, payloads: MutableList<Any>) {
+        bind(element)
     }
 
     override fun onViewRecycled() {
