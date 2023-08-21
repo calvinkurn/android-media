c9c32286770593a9fc4f0445833d45e885bf587a
astidhiyaa <asti.anzaria@tokopedia.com>
2023-08-21 10:39:43 +0700

    fat: add swipe up - animation

--- features/content/stories/src/main/java/com/tokopedia/stories/view/animation/StoriesProductNotch.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/view/animation/StoriesProductNotch.kt
@@ -1,14 +1,21 @@
 package com.tokopedia.stories.view.animation
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,10 +37,23 @@ import com.tokopedia.unifyprinciples.R as unifyR
 fun StoriesProductNotch(productCount: Int, onClick: () -> Unit) {
     NestTheme {
         val ctx = LocalContext.current
+
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val y by infiniteTransition.animateFloat(
+            initialValue = 30f,
+            targetValue = 10f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 300),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .wrapContentSize()
+                .offset(y = y.dp)
                 .background(color = Color(0x80000000), shape = RoundedCornerShape(20.dp))
                 .padding(4.dp)
                 .clickable { onClick() }
