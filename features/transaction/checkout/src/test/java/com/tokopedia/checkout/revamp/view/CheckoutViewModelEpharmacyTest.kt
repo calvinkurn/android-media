a77459db67810de2d23815e86fe99916c9922a9b
hansenpw <hansen.wijaya@tokopedia.com>
2023-08-21 08:35:07 +0700

    add more epharm test

--- features/transaction/checkout/src/test/java/com/tokopedia/checkout/revamp/view/CheckoutViewModelEpharmacyTest.kt
+++ features/transaction/checkout/src/test/java/com/tokopedia/checkout/revamp/view/CheckoutViewModelEpharmacyTest.kt
@@ -6,9 +6,26 @@ import com.tokopedia.checkout.domain.model.cartshipmentform.GroupAddress
 import com.tokopedia.checkout.domain.model.cartshipmentform.GroupShop
 import com.tokopedia.checkout.domain.model.cartshipmentform.GroupShopV2
 import com.tokopedia.checkout.domain.model.cartshipmentform.Product
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutAddressModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutButtonPaymentModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutCostModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutCrossSellGroupModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutEpharmacyModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutOrderModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutProductModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutPromoModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutTickerErrorModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutTickerModel
+import com.tokopedia.checkout.revamp.view.uimodel.CheckoutUpsellModel
+import com.tokopedia.checkout.view.uimodel.ShipmentNewUpsellModel
+import com.tokopedia.logisticCommon.data.entity.address.RecipientAddressModel
 import com.tokopedia.purchase_platform.common.feature.ethicaldrug.data.response.GetPrescriptionIdsResponse
+import com.tokopedia.purchase_platform.common.feature.ethicaldrug.domain.model.UploadPrescriptionUiModel
+import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUiModel
+import com.tokopedia.purchase_platform.common.feature.tickerannouncement.TickerAnnouncementHolderData
 import io.mockk.coEvery
 import io.mockk.coVerify
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class CheckoutViewModelEpharmacyTest : BaseCheckoutViewModelTest() {
@@ -184,5 +201,43 @@ class CheckoutViewModelEpharmacyTest : BaseCheckoutViewModelTest() {
 
         // Then
         coVerify(inverse = true) { prescriptionIdsUseCase.setParams(any()).executeOnBackground() }
+    }
+
+    @Test
+    fun `WHEN set prescription ids THEN should set upload prescription image count`() {
+        // Given
+        viewModel.listData.value = listOf(
+            CheckoutTickerErrorModel(errorMessage = ""),
+            CheckoutTickerModel(ticker = TickerAnnouncementHolderData()),
+            CheckoutAddressModel(
+                recipientAddressModel = RecipientAddressModel().apply {
+                    id = "1"
+                    destinationDistrictId = "1"
+                    addressName = "jakarta"
+                    postalCode = "123"
+                    latitude = "123"
+                    longitude = "321"
+                }
+            ),
+            CheckoutUpsellModel(upsell = ShipmentNewUpsellModel()),
+            CheckoutProductModel("123"),
+            CheckoutOrderModel("123"),
+            CheckoutEpharmacyModel(epharmacy = UploadPrescriptionUiModel()),
+            CheckoutPromoModel(promo = LastApplyUiModel()),
+            CheckoutCostModel(),
+            CheckoutCrossSellGroupModel(),
+            CheckoutButtonPaymentModel()
+        )
+
+        val prescriptions = arrayListOf("123", "456")
+
+        // When
+        viewModel.setPrescriptionIds(prescriptions)
+
+        // Then
+        assertEquals(
+            prescriptions.size,
+            viewModel.listData.value.epharmacy()?.epharmacy?.uploadedImageCount
+        )
     }
 }
