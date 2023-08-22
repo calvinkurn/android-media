48fb1e5debf9bd76c27059ec1f922640d1722ab8
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-08-22 11:33:04 +0700

    handling utils error message for mapping error

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/utils/ErrorHandlerUtils.kt
@@ -2,6 +2,8 @@ package com.tokopedia.kyc_centralized.ui.gotoKyc.utils
 
 import android.content.Context
 import com.tokopedia.kyc_centralized.R
+import com.tokopedia.kyc_centralized.common.KYCConstant
+import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.network.utils.ErrorHandler
 import java.net.ConnectException
 import java.net.SocketTimeoutException
@@ -17,16 +19,23 @@ fun Throwable?.getGotoKycErrorMessage(context: Context): String {
             context.getString(R.string.goto_kyc_error_timeout)
         }
         else -> {
+            var messageError = context.getString(R.string.goto_kyc_error_from_be)
             if (this != null) {
-                val errorHandler = ErrorHandler.getErrorMessagePair(
-                    context = context,
-                    e = this,
-                    builder = ErrorHandler.Builder().build()
-                )
-                errorCode = context.getString(R.string.error_code, errorHandler.second)
+                if (this is MessageErrorException && this.errorCode == KYCConstant.KEY_KNOW_ERROR_CODE) {
+                    // if errorCode is KYCConstant.KEY_KNOW_ERROR_CODE that mean the message already have valid errorCode
+                    // link of errorCode: https://docs.google.com/spreadsheets/d/1pk5DOc1wAxUMUNNxeRAqZxPFGUHbLwBkmLijNI6cH8g/edit?usp=sharing
+                    messageError = this.message.toString()
+                } else {
+                    val errorHandler = ErrorHandler.getErrorMessagePair(
+                        context = context,
+                        e = this,
+                        builder = ErrorHandler.Builder().build()
+                    )
+                    errorCode = context.getString(R.string.error_code, errorHandler.second)
+                }
             }
 
-            context.getString(R.string.goto_kyc_error_from_be)
+            messageError
         }
     }
     return message.plus(" $errorCode")
