d943c6b0bbf66c5928537d87a52591e5d224e272
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-08-23 06:02:09 +0700

    feat: update recom animation

--- features/promo/promo-usage/src/main/java/com/tokopedia/promousage/view/adapter/PromoRecommendationDelegateAdapter.kt
+++ features/promo/promo-usage/src/main/java/com/tokopedia/promousage/view/adapter/PromoRecommendationDelegateAdapter.kt
@@ -2,6 +2,7 @@ package com.tokopedia.promousage.view.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
@@ -12,6 +13,7 @@ import com.tokopedia.promousage.R
 import com.tokopedia.promousage.databinding.PromoUsageItemVoucherRecommendationBinding
 import com.tokopedia.promousage.domain.entity.list.PromoRecommendationItem
 import com.tokopedia.promousage.util.composite.DelegateAdapter
+import com.tokopedia.unifyprinciples.UnifyMotion
 
 class PromoRecommendationDelegateAdapter(
     private val onClickUsePromoRecommendation: () -> Unit
@@ -37,11 +39,10 @@ class PromoRecommendationDelegateAdapter(
             val selectedRecommendedPromoCount = item.selectedCodes.size
             val recommendedPromoCount = item.codes.size
             binding.btnRecommendationUseVoucher.setOnClickListener {
-                binding.btnRecommendationUseVoucher.invisible()
-                binding.ivCheckmark.visible()
-                binding.ivCheckmarkOutline.visible()
-                onClickUsePromoRecommendation()
-                startButtonUseVoucherAnimation()
+                hideMessage {
+                    showMessage(item.messageSelected)
+                }
+                startButtonAnimation()
             }
             if (item.selectedCodes.containsAll(item.codes)) {
                 binding.tpgRecommendationTitle.text = item.messageSelected
@@ -59,23 +60,83 @@ class PromoRecommendationDelegateAdapter(
             }
         }
 
-        private fun startButtonUseVoucherAnimation() {
-            val shrinkOutAnimation =
-                AnimationUtils.loadAnimation(binding.ivCheckmarkOutline.context, R.anim.shrink_out)
-            shrinkOutAnimation.setAnimationListener(object : AnimationListener {
-                override fun onAnimationStart(p0: Animation?) {
+        private fun hideMessage(onCompleted: (() -> Unit)?) {
+            val fadeOutAnimation = AlphaAnimation(1f, 0f)
+            fadeOutAnimation.interpolator = UnifyMotion.EASE_IN_OUT
+            fadeOutAnimation.duration = UnifyMotion.T2
+            fadeOutAnimation.setAnimationListener(object : AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
                     // no-op
                 }
 
+                override fun onAnimationEnd(animation: Animation?) {
+                    binding.tpgRecommendationTitle.invisible()
+                    onCompleted?.invoke()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                    // no-op
+                }
+            })
+            binding.tpgRecommendationTitle.startAnimation(fadeOutAnimation)
+        }
+
+        private fun showMessage(message: String) {
+            val fadeInAnimation = AlphaAnimation(0f, 1f)
+            fadeInAnimation.interpolator = UnifyMotion.EASE_IN_OUT
+            fadeInAnimation.duration = UnifyMotion.T2
+            fadeInAnimation.setAnimationListener(object : AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                    // no-op
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    binding.tpgRecommendationTitle.text = message
+                    binding.tpgRecommendationTitle.visible()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                    // no-op
+                }
+            })
+            binding.tpgRecommendationTitle.startAnimation(fadeInAnimation)
+        }
+
+        private fun startButtonAnimation() {
+            val shrinkLeftAnimation =
+                AnimationUtils.loadAnimation(binding.root.context, R.anim.shrink_left)
+            val zoomOutAnimation =
+                AnimationUtils.loadAnimation(binding.root.context, R.anim.zoom_out)
+            shrinkLeftAnimation.setAnimationListener(object : AnimationListener {
+                override fun onAnimationStart(p0: Animation?) {
+                    binding.btnRecommendationUseVoucher.visible()
+                }
+
                 override fun onAnimationEnd(p0: Animation?) {
-                    binding.ivCheckmarkOutline.invisible()
+                    binding.btnRecommendationUseVoucher.invisible()
                 }
 
                 override fun onAnimationRepeat(p0: Animation?) {
                     // no-op
                 }
             })
-            binding.ivCheckmarkOutline.startAnimation(shrinkOutAnimation)
+            zoomOutAnimation.setAnimationListener(object : AnimationListener {
+                override fun onAnimationStart(p0: Animation?) {
+                    binding.ivCheckmark.visible()
+                    binding.ivCheckmarkOutline.visible()
+                }
+
+                override fun onAnimationEnd(p0: Animation?) {
+                    binding.ivCheckmarkOutline.invisible()
+                    onClickUsePromoRecommendation()
+                }
+
+                override fun onAnimationRepeat(p0: Animation?) {
+                    // no-op
+                }
+            })
+            binding.btnRecommendationUseVoucher.startAnimation(shrinkLeftAnimation)
+            binding.ivCheckmarkOutline.startAnimation(zoomOutAnimation)
         }
     }
 }
