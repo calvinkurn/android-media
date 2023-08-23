e778cfd7580c7589768e0e8c9bbdd1557f64a420
yogieputra8 <yogie.putra@tokopedia.com>
2023-08-23 15:44:45 +0700

    Setup color resources for widget text description

--- features/category/catalog_common/src/main/java/com/tokopedia/catalogcommon/viewholder/TextDescriptionViewHolder.kt
+++ features/category/catalog_common/src/main/java/com/tokopedia/catalogcommon/viewholder/TextDescriptionViewHolder.kt
@@ -15,6 +15,9 @@ class TextDescriptionViewHolder(itemView: View):
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.item_text_description
+
+        private val TEXT_HIGH_EMPHASIS = R.color.dms_static_Unify_NN950_light
+        private val TEXT_LOW_EMPHASIS = R.color.dms_static_Unify_NN600_light
     }
 
     private val binding by viewBinding<ItemTextDescriptionBinding>()
@@ -31,9 +34,9 @@ class TextDescriptionViewHolder(itemView: View):
     private fun overrideWidgetTheme(element: TextDescriptionUiModel) {
         binding?.let {
             if (element.isDarkMode) {
-                it.tgpTextWidgetHighlight.setTextColor(ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_NN600))
-                it.tgpTextWidgetTitle.setTextColor(ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_NN950))
-                it.tgpTextWidgetDescription.setTextColor(ContextCompat.getColor(itemView.context, com.tokopedia.unifyprinciples.R.color.Unify_NN600))
+                it.tgpTextWidgetHighlight.setTextColor(ContextCompat.getColor(itemView.context, TEXT_LOW_EMPHASIS))
+                it.tgpTextWidgetTitle.setTextColor(ContextCompat.getColor(itemView.context, TEXT_HIGH_EMPHASIS))
+                it.tgpTextWidgetDescription.setTextColor(ContextCompat.getColor(itemView.context, TEXT_LOW_EMPHASIS))
             }
         }
     }
