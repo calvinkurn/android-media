5a0c63f5f863957d633d497c6a9e57cc61c81ce1
faisalramd <faisal.ramdani@tokopedia.com>
2023-08-23 11:56:21 +0700

    implement layout and behavior of navigation bar

--- features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogDetailPageFragment.kt
+++ features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogDetailPageFragment.kt
@@ -1,24 +1,36 @@
 package com.tokopedia.catalog.ui.fragment
 
+import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.catalog.databinding.FragmentCatalogReimagineDetailPageBinding
 import com.tokopedia.catalog.di.DaggerCatalogComponent
 import com.tokopedia.catalog.ui.viewmodel.CatalogDetailPageViewModel
 import com.tokopedia.catalogcommon.adapter.CatalogAdapterFactoryImpl
 import com.tokopedia.catalogcommon.adapter.WidgetCatalogAdapter
+import com.tokopedia.catalogcommon.customview.CatalogToolbar
 import com.tokopedia.catalogcommon.listener.HeroBannerListener
 import com.tokopedia.catalogcommon.uimodel.DummyUiModel
 import com.tokopedia.catalogcommon.uimodel.HeroBannerUiModel
 import com.tokopedia.catalogcommon.uimodel.PanelImageUiModel
 import com.tokopedia.catalogcommon.uimodel.TopFeaturesUiModel
 import com.tokopedia.catalogcommon.uimodel.TrustMakerUiModel
+import com.tokopedia.catalogcommon.util.DrawableExtension
+import com.tokopedia.catalogcommon.util.stringHexColorParseToInt
+import com.tokopedia.kotlin.extensions.view.ONE
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.kotlin.extensions.view.isMoreThanZero
+import com.tokopedia.kotlin.extensions.view.isVisible
+import com.tokopedia.kotlin.extensions.view.orZero
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 import javax.inject.Inject
 
@@ -43,6 +55,7 @@ class CatalogDetailPageFragment : BaseDaggerFragment(), HeroBannerListener {
 
     companion object {
         private const val ARG_EXTRA_CATALOG_ID = "ARG_EXTRA_CATALOG_ID"
+        private const val COLOR_VALUE_MAX = 255
         const val CATALOG_DETAIL_PAGE_FRAGMENT_TAG = "CATALOG_DETAIL_PAGE_FRAGMENT_TAG"
         fun newInstance(catalogId: String): CatalogDetailPageFragment {
             val fragment = CatalogDetailPageFragment()
@@ -75,10 +88,8 @@ class CatalogDetailPageFragment : BaseDaggerFragment(), HeroBannerListener {
         super.onViewCreated(view, savedInstanceState)
         catalogDetailPageViewModel.getA()
         context?.let {
-            binding?.rvContent?.apply {
-                layoutManager = LinearLayoutManager(it)
-                adapter = widgetAdapter
-            }
+            binding?.setupRvWidgets(false, true)
+            binding?.setupToolbar(false, true, "#ffffff")
             widgets.add(
                 HeroBannerUiModel(
                     "bannercoy",
@@ -88,7 +99,7 @@ class CatalogDetailPageFragment : BaseDaggerFragment(), HeroBannerListener {
                     widgetTextColor = null,
                     brandTitle = "Samsung",
                     brandDesc = "360 RTrfvds Dewsign",
-                    brandIconUrl = "https://www.samsung.com/etc.clientlibs/samsung/clientlibs/consumer/global/clientlib-common/resources/images/gnb-desktop-120x32.png",
+                    brandIconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Samsung_wordmark.svg/440px-Samsung_wordmark.svg.png",
                     //brandIconUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UNIQLO_logo.svg/1200px-UNIQLO_logo.svg.png",
                     brandImageUrls = listOf(
                         "https://placekitten.com/200/300",
@@ -120,6 +131,26 @@ class CatalogDetailPageFragment : BaseDaggerFragment(), HeroBannerListener {
                     )
                 )
             )
+            widgets.add(
+                PanelImageUiModel("1", "2", "2",
+                    content = listOf(
+                        PanelImageUiModel.PanelImageItemData(imageUrl = "https://images.tokopedia.net/android/shop_page/image_product_empty_state_buyer.png", highlight = "",
+                            title = "asd", description = "ad"),
+                        PanelImageUiModel.PanelImageItemData(imageUrl = "https://placekitten.com/200/300", highlight = "",
+                            title = "asd22", description = "ad22"),
+                    )
+                )
+            )
+            widgets.add(
+                PanelImageUiModel("1", "2", "2",
+                    content = listOf(
+                        PanelImageUiModel.PanelImageItemData(imageUrl = "https://images.tokopedia.net/android/shop_page/image_product_empty_state_buyer.png", highlight = "",
+                            title = "asd", description = "ad"),
+                        PanelImageUiModel.PanelImageItemData(imageUrl = "https://placekitten.com/200/300", highlight = "",
+                            title = "asd22", description = "ad22"),
+                    )
+                )
+            )
             widgetAdapter.addWidget(widgets)
         }
 
@@ -135,5 +166,60 @@ class CatalogDetailPageFragment : BaseDaggerFragment(), HeroBannerListener {
 
     override fun onNavMoreMenuClicked() {
         // no-op
+    }
+
+    private fun FragmentCatalogReimagineDetailPageBinding.setupRvWidgets(
+        isDarkMode: Boolean,
+        isPremium: Boolean
+    ) {
+        val layoutManager = LinearLayoutManager(context)
+        rvContent.layoutManager = layoutManager
+        rvContent.adapter = widgetAdapter
+        rvContent.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val bannerHeight = layoutManager.findViewByPosition(Int.ZERO)?.height.orZero()
+                val bannerRect = Rect()
+                layoutManager.findViewByPosition(Int.ZERO)?.getGlobalVisibleRect(bannerRect)
+                val scrollProgress = Int.ONE - if (bannerRect.height().isMoreThanZero() && bannerHeight.isMoreThanZero()) {
+                    bannerRect.height() / bannerHeight.toFloat()
+                } else {
+                    Int.ZERO.toFloat()
+                }
+                toolbar.updateToolbarAppearance(scrollProgress, isDarkMode, isPremium)
+            }
+        })
+    }
+
+    private fun FragmentCatalogReimagineDetailPageBinding.setupToolbar(
+        isDarkMode: Boolean,
+        isPremium: Boolean,
+        colorString: String
+    ) {
+        val colorBgGradient = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_Static_Black_44)
+        val colorFontDark = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_Static_White)
+        val colorFontLight = MethodChecker.getColor(context, com.tokopedia.unifyprinciples.R.color.Unify_Static_White)
+        val colorFont = if (isDarkMode) colorFontDark else colorFontLight
+
+        toolbarShadow.background = DrawableExtension.createGradientDrawable(colorTop = colorBgGradient)
+        toolbar.setColors(colorFont)
+        toolbarShadow.isVisible = !isPremium
+        toolbarBg.setBackgroundColor(colorString.stringHexColorParseToInt())
+    }
+
+    private fun CatalogToolbar.updateToolbarAppearance(
+        scrollProgress: Float,
+        isDarkMode: Boolean,
+        isPremium: Boolean
+    ) {
+        if (!isDarkMode) {
+            val colorProgress: Int = COLOR_VALUE_MAX - (COLOR_VALUE_MAX * scrollProgress).toInt()
+            setColors(Color.rgb(colorProgress, colorProgress, colorProgress))
+        }
+
+        if (isPremium){
+            alpha = scrollProgress
+        }
+        binding?.toolbarBg?.alpha = scrollProgress
     }
 }
