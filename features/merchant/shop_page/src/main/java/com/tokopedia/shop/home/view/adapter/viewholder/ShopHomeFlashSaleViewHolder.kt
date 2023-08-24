3722b48bfa2a80cd300555f480c1d1013260d448
CJ <calvyn.julian@tokopedia.com>
2023-08-24 15:14:41 +0700

    adjust flash sale margin logic

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeFlashSaleViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeFlashSaleViewHolder.kt
@@ -168,7 +168,6 @@ class ShopHomeFlashSaleViewHolder(
         doubleBackGroundView?.hide()
         multipleBackGroundView?.hide()
         configMarginFestivity()
-        setShopReimaginedContainerMargin()
     }
 
     private fun configMarginFestivity(){
