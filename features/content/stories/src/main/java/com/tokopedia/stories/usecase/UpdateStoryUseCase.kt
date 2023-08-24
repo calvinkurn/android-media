871dd7beb8c7e4e206f88e1c9e8fc5f31b984add
astidhiyaa <asti.anzaria@tokopedia.com>
2023-08-24 11:25:43 +0700

    fix: remove non-mandatory param in update use case

--- features/content/stories/src/main/java/com/tokopedia/stories/usecase/UpdateStoryUseCase.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/usecase/UpdateStoryUseCase.kt
@@ -14,7 +14,7 @@ import javax.inject.Inject
 class UpdateStoryUseCase @Inject constructor(
     private val repo: GraphqlRepository,
     dispatchers: CoroutineDispatchers
-) : CoroutineUseCase<Param, UpdateStoryResponse>(dispatchers.io) {
+) : CoroutineUseCase<UpdateStoryUseCase.Param, UpdateStoryResponse>(dispatchers.io) {
 
     override fun graphqlQuery(): String = QUERY
 
@@ -22,28 +22,31 @@ class UpdateStoryUseCase @Inject constructor(
         return repo.request(graphqlQuery(), params.convertToMap())
     }
 
-    companion object {
-        const val REQ_PARAM = "req"
-        const val STORY_ID_PARAM = "storyID"
-        const val STORY_TYPE_PARAM = "storyType"
-        const val AUTHOR_ID_PARAM = "authorID"
-        const val AUTHOR_TYPE_PARAM = "authorType"
-        const val STATUS_PARAM = "status"
-        const val MEDIA_ID_PARAM = "activeMediaID"
-        const val PUBLISHED_AT_PARAM = "publishedAt"
+    data class Param(
+        val storyId: String,
+        val action: StoryActionType,
+    ) {
+        fun convertToMap(): Map<String, Any> {
+            return mapOf(
+                REQ_PARAM to mapOf<String, Any>(
+                    STORY_ID_PARAM to storyId,
+                    STATUS_PARAM to action.value,
+                )
+            )
+        }
+    }
 
-        //TODO() wait finalized contract
+    companion object {
+        private const val REQ_PARAM = "req"
+        private const val STORY_ID_PARAM = "storyID"
+        private const val STATUS_PARAM = "status"
+
         private const val QUERY = """
             mutation updateStory(${'$'} storyId: String!, ${'$'}storyType: Int!, ${'$'}authorId: String!, ${'$'}authorType: Int!, ${'$'}action: Int!, ${'$'}mediaId: String!, ${'$'}publishedAt: String!){
                 contentStoryUpdateStory (
                    $REQ_PARAM: {
                        $STORY_ID_PARAM: ${'$'}storyId,
-                       $STORY_TYPE_PARAM :${'$'}storyType,
-                       $AUTHOR_ID_PARAM : ${'$'}authorId,
-                       $AUTHOR_TYPE_PARAM: ${'$'}authorType,
                        $STATUS_PARAM :${'$'}action,
-                       $MEDIA_ID_PARAM : ${'$'}mediaId,
-                       $PUBLISHED_AT_PARAM : ${'$'}publishedAt
                    }
                 ) {
                     storyID
@@ -51,17 +54,4 @@ class UpdateStoryUseCase @Inject constructor(
             }
         """
     }
-}
-
-data class Param(
-    val storyId: String,
-    val action: StoryActionType,
-) {
-    fun convertToMap(): Map<String, Any> =
-        mapOf(
-            UpdateStoryUseCase.REQ_PARAM to mapOf(
-                UpdateStoryUseCase.STORY_ID_PARAM to storyId,
-                UpdateStoryUseCase.STATUS_PARAM to action.value,
-            )
-        )
 }
