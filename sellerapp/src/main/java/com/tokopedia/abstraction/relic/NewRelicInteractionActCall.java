bdb81c2448ab9e33333608dda85a41706bf8819f
milhamj <milham.jamaludin@gmail.com>
2023-08-30 17:31:59 +0700

    Trying more logs :loud_sound:

--- sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
+++ sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
@@ -3,10 +3,12 @@ package com.tokopedia.abstraction.relic;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
-import android.util.Log;
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.newrelic.agent.android.NewRelic;
 import com.tokopedia.user.session.UserSessionInterface;
+
+import timber.log.Timber;
 
 public class NewRelicInteractionActCall implements Application.ActivityLifecycleCallbacks {
 
@@ -50,10 +52,22 @@ public class NewRelicInteractionActCall implements Application.ActivityLifecycle
 
     private void setNewRelicAttribute(Activity activity) {
         NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
-        Log.d("milhamj", "Class simple name: " + activity.getClass().getSimpleName());
-        Log.d("milhamj", "Class name: " + activity.getClass().getName());
-        Log.d("milhamj", "Local class name: " + activity.getLocalClassName());
-        Log.d("milhamj", "Component class name: " + activity.getComponentName().getClassName());
-        Log.d("milhamj", "Component short class name: " + activity.getComponentName().getShortClassName());
+        Timber.tag("milhamj").e("Class simple name: " + activity.getClass().getSimpleName());
+        Timber.tag("milhamj").e("Class name: " + activity.getClass().getName());
+        Timber.tag("milhamj").e("Local class name: " + activity.getLocalClassName());
+        Timber.tag("milhamj").e("Component class name: " + activity.getComponentName().getClassName());
+        Timber.tag("milhamj").e("Component short class name: " + activity.getComponentName().getShortClassName());
+        String eMessage = "Class simple name: " + activity.getClass().getSimpleName()
+                + "Class name: " + activity.getClass().getName()
+                + "Local class name: " + activity.getLocalClassName()
+                + "Component class name: " + activity.getComponentName().getClassName()
+                + "Component short class name: " + activity.getComponentName().getShortClassName();
+        IllegalStateException exception = new IllegalStateException(eMessage);
+        exception.printStackTrace();
+        try {
+            FirebaseCrashlytics.getInstance().recordException(exception);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
