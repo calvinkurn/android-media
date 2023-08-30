1ce764cb4cdef78082b8579be675b676f28fce67
milhamj <milham.jamaludin@gmail.com>
2023-08-30 14:38:47 +0700

    Set newrelic attribute :tada:

--- customer_mid_app/src/main/java/com/tokopedia/abstraction/newrelic/NewRelicInteractionActCall.kt
+++ customer_mid_app/src/main/java/com/tokopedia/abstraction/newrelic/NewRelicInteractionActCall.kt
@@ -8,14 +8,21 @@ import com.tokopedia.user.session.UserSessionInterface
 
 class NewRelicInteractionActCall(private val userSession: UserSessionInterface?) : Application.ActivityLifecycleCallbacks {
 
+    companion object {
+        private const val ATTRIBUTE_ACTIVITY = "activity"
+    }
+
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
         NewRelic.setUserId(userSession?.userId.orEmpty())
         NewRelic.startInteraction(activity.localClassName)
         NewRelic.setInteractionName(activity.localClassName)
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.localClassName)
     }
 
     override fun onActivityStarted(activity: Activity) {}
-    override fun onActivityResumed(activity: Activity) {}
+    override fun onActivityResumed(activity: Activity) {
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.localClassName)
+    }
     override fun onActivityPaused(activity: Activity) {}
     override fun onActivityStopped(activity: Activity) {}
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
--- sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
+++ sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
@@ -5,18 +5,34 @@ import android.app.Application;
 import android.os.Bundle;
 
 import com.newrelic.agent.android.NewRelic;
+import com.tokopedia.user.session.UserSessionInterface;
 
 public class NewRelicInteractionActCall implements Application.ActivityLifecycleCallbacks {
 
+    public NewRelicInteractionActCall(UserSessionInterface userSession) {
+        this.userSession = userSession;
+    }
+
+    private UserSessionInterface userSession;
+
+    private static final String ATTRIBUTE_ACTIVITY = "activity";
+
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        if (userSession != null) {
+            NewRelic.setUserId(userSession.getUserId());
+        } else {
+            NewRelic.setUserId("");
+        }
         NewRelic.startInteraction(activity.getLocalClassName());
         NewRelic.setInteractionName(activity.getLocalClassName());
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
     }
 
     public void onActivityStarted(Activity activity) {
     }
 
     public void onActivityResumed(Activity activity) {
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
     }
 
     public void onActivityPaused(Activity activity) {
