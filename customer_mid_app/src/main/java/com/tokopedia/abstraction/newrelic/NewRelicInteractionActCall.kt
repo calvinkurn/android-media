48f908df53c79d04196ba380b8066f6a75e36207
milhamj <milham.jamaludin@gmail.com>
2023-08-30 16:09:13 +0700

    Test log :loud_sound:

--- customer_mid_app/src/main/java/com/tokopedia/abstraction/newrelic/NewRelicInteractionActCall.kt
+++ customer_mid_app/src/main/java/com/tokopedia/abstraction/newrelic/NewRelicInteractionActCall.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import com.newrelic.agent.android.NewRelic
 import com.tokopedia.user.session.UserSessionInterface
 
-class NewRelicInteractionActCall(private val userSession: UserSessionInterface?) : Application.ActivityLifecycleCallbacks {
+class NewRelicInteractionActCall(
+    private val userSession: UserSessionInterface?
+) : Application.ActivityLifecycleCallbacks {
 
     companion object {
         private const val ATTRIBUTE_ACTIVITY = "activity"
@@ -16,15 +18,20 @@ class NewRelicInteractionActCall(private val userSession: UserSessionInterface?)
         NewRelic.setUserId(userSession?.userId.orEmpty())
         NewRelic.startInteraction(activity.localClassName)
         NewRelic.setInteractionName(activity.localClassName)
-        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.localClassName)
+        setNewRelicAttribute(activity)
     }
 
     override fun onActivityStarted(activity: Activity) {}
     override fun onActivityResumed(activity: Activity) {
-        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.localClassName)
+        setNewRelicAttribute(activity)
     }
+
     override fun onActivityPaused(activity: Activity) {}
     override fun onActivityStopped(activity: Activity) {}
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
     override fun onActivityDestroyed(activity: Activity) {}
+
+    private fun setNewRelicAttribute(activity: Activity) {
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.localClassName)
+    }
 }
--- sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
+++ sellerapp/src/main/java/com/tokopedia/abstraction/relic/NewRelicInteractionActCall.java
@@ -3,6 +3,7 @@ package com.tokopedia.abstraction.relic;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.newrelic.agent.android.NewRelic;
 import com.tokopedia.user.session.UserSessionInterface;
@@ -13,9 +14,9 @@ public class NewRelicInteractionActCall implements Application.ActivityLifecycle
         this.userSession = userSession;
     }
 
-    private UserSessionInterface userSession;
-
     private static final String ATTRIBUTE_ACTIVITY = "activity";
+
+    private final UserSessionInterface userSession;
 
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
         if (userSession != null) {
@@ -25,14 +26,14 @@ public class NewRelicInteractionActCall implements Application.ActivityLifecycle
         }
         NewRelic.startInteraction(activity.getLocalClassName());
         NewRelic.setInteractionName(activity.getLocalClassName());
-        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
+        setNewRelicAttribute(activity);
     }
 
     public void onActivityStarted(Activity activity) {
     }
 
     public void onActivityResumed(Activity activity) {
-        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
+        setNewRelicAttribute(activity);
     }
 
     public void onActivityPaused(Activity activity) {
@@ -45,5 +46,14 @@ public class NewRelicInteractionActCall implements Application.ActivityLifecycle
     }
 
     public void onActivityDestroyed(Activity activity) {
+    }
+
+    private void setNewRelicAttribute(Activity activity) {
+        NewRelic.setAttribute(ATTRIBUTE_ACTIVITY, activity.getLocalClassName());
+        Log.d("milhamj", "Class simple name: " + activity.getClass().getSimpleName());
+        Log.d("milhamj", "Class name: " + activity.getClass().getName());
+        Log.d("milhamj", "Local class name: " + activity.getLocalClassName());
+        Log.d("milhamj", "Component class name: " + activity.getComponentName().getClassName());
+        Log.d("milhamj", "Component short class name: " + activity.getComponentName().getShortClassName());
     }
 }
