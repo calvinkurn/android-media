bf01ccf951aba432ec1900857a13c1e487b1b17d
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2023-08-31 09:35:42 +0700

    [Search MPS Pre Release] Animation on + icon stay for 1month after first success use

--- features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/utils/SharedPrefsMpsLocalCache.kt
+++ features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/utils/SharedPrefsMpsLocalCache.kt
@@ -20,16 +20,34 @@ class SharedPrefsMpsLocalCache(
         val sharedPref = sharedPref ?: return
         sharedPref.edit()
             .putBoolean(KEY_FIRST_MPS_SUCCESS, true)
+            .putLong(TIME_FIRST_MPS_SUCCESS, createdTimeMils())
             .apply()
 
     }
 
     override fun shouldAnimatePlusIcon(): Boolean {
-        return !isFirstMpsSuccess()
+        return !isTimeFirstSuccessAlreadyOneMonth()
+    }
+
+    private fun createdTimeMils(): Long {
+        return System.currentTimeMillis()
+    }
+
+    private fun getTimeFirstSuccess() : Long {
+        val defaultTime = createdTimeMils()
+        val sharedPref = sharedPref ?: return defaultTime
+        return sharedPref.getLong(TIME_FIRST_MPS_SUCCESS, defaultTime)
+    }
+
+    private fun isTimeFirstSuccessAlreadyOneMonth(): Boolean {
+        val timePassed = createdTimeMils() - getTimeFirstSuccess()
+        return timePassed > THIRTY_DAYS
     }
 
     companion object {
         private const val PREF_NAME = "MPSSharedPref"
         const val KEY_FIRST_MPS_SUCCESS = "KEY_FIRST_MPS_SUCCESS"
+        const val TIME_FIRST_MPS_SUCCESS = "TIME_FIRST_MPS_SUCCESS"
+        const val THIRTY_DAYS = 2592000000L
     }
 }
