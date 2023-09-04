fe7f1f5d0184b362cda3a4c0e64e32a39380065d
stevenwijaya <steven.wijaya@tokopedia.com>
2023-09-04 13:00:48 +0700

    checkpoint

--- features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/utils/BitmapCropping.kt
+++ features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/utils/BitmapCropping.kt
@@ -26,7 +26,8 @@ interface BitmapProcessingListener {
 }
 
 class BitmapCropping constructor(
-    private val context: Context
+    private val context: Context,
+    private val camera: CameraView
 ) : CoroutineScope {
 
     private val parentJob = SupervisorJob()
@@ -38,7 +39,7 @@ class BitmapCropping constructor(
 
     fun doCropping(bitmap: Bitmap, frame: View, listener: BitmapProcessingListener) {
         launchCatchError(coroutineContext, block = {
-            val bitmapCropped = doCroppingWithFrame(bitmap, frame)
+            val bitmapCropped = doCroppingWithFrame(bitmap, frame, 0)
 
             withContext(coroutineDispatchers.main) {
                 listener.onBitmapReady(bitmapCropped)
@@ -56,25 +57,67 @@ class BitmapCropping constructor(
      * @param padding to add padding between frame and final cropping result
      * @return cropped bitmap
      */
-    private fun doCroppingWithFrame(bitmap: Bitmap, frame: View): Bitmap {
-        val framePos = IntArray(2)
-        frame.getLocationOnScreen(framePos)
+    private fun doCroppingWithFrame(bitmap: Bitmap, frame: View, padding: Int = DEFAULT_PADDING): Bitmap {
+        val framePadding = padding.dpToPx(context.resources.displayMetrics)
 
-        val x = ((bitmap.width - getScreenWidth()) / 2) + framePos[0]
-        val y = ((bitmap.height - getScreenHeight()) / 2) + framePos[1] - DisplayMetricUtils.getStatusBarHeight(context)
-        val w = getScreenWidth() - FRAME_MARGIN.toPx()
-        val h = w * 2 / 3
+        val bitmapW = bitmap.width.toFloat()
+        val bitmapH = bitmap.height.toFloat()
+
+        val diffScaleW = camera.width.toFloat() / bitmapW
+        val diffScaleH = camera.height.toFloat() / bitmapH
+
+        val frameW = if (frame.width.toFloat() > bitmapW) bitmapW else frame.width.toFloat()
+        val frameH = if (frame.height.toFloat() > bitmapH) bitmapH else frame.height.toFloat()
+        val frameLeft = if (frame.left.toFloat() < 0) 0F else frame.left.toFloat()
+        val frameTop = if (frame.top.toFloat() < 0) 0F else frame.top.toFloat()
+
+        var offsetX: Float = (frameLeft / diffScaleW) - (camera.left.toFloat() / diffScaleW) - framePadding
+        offsetX = if (offsetX > bitmapW) frameLeft else if (offsetX < 0) 0F else offsetX
+        var offsetY: Float = (frameTop / diffScaleH) - (camera.top.toFloat() / diffScaleH) - framePadding
+        offsetY = if (offsetY > bitmapH) frameTop else if (offsetY < 0) 0F else offsetY
+
+        var newW: Float = (frameW / diffScaleW) + (framePadding * 2).toFloat() // 2 for padding on 2 side (left & right)
+        newW = when {
+            newW > bitmapW -> bitmapW
+            newW + offsetX > bitmapW -> newW - offsetX
+            else -> newW
+        }
+
+        var newH: Float = (frameH / diffScaleH) + (framePadding * 2).toFloat() // 2 for padding on 2 side (top & bottom)
+        newH = when {
+            newH > bitmapH -> bitmapH
+            newH + offsetY > bitmapH -> newH - offsetY
+            else -> newH
+        }
+
+        val a = bitmapW / camera.width.toFloat()
 
         return Bitmap.createBitmap(
             bitmap,
-            x,
-            y,
-            w,
-            h
+            offsetX.roundToInt(),
+            offsetY.roundToInt(),
+            (newH * 3 / 2).roundToInt(),
+            newH.roundToInt()
         )
     }
 
+    /**
+     * @param bitmap
+     * @param quality bitmap compress to X quality of original source
+     * @return compressed bitmap
+     */
+    private fun doCompression(bitmap: Bitmap, quality: Float): Bitmap {
+        val reduceQualityPercentage = quality / 100
+        val width = (bitmap.width * reduceQualityPercentage).roundToInt()
+        val height = (bitmap.height * reduceQualityPercentage).roundToInt()
+
+        if (width <= 0) throw Throwable("Please make sure final width > 0")
+        if (height <= 0) throw Throwable("Please make sure final height > 0")
+
+        return Bitmap.createScaledBitmap(bitmap, width, height, false)
+    }
+
     companion object {
-        const val FRAME_MARGIN = 48
+        const val DEFAULT_PADDING = 8
     }
 }
