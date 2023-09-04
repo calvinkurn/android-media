1f7f604bba2dff5bae82d3dc95e9af27e7ae2d81
stevenwijaya <steven.wijaya@tokopedia.com>
2023-09-04 10:43:41 +0700

    fix crop logic

--- features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/utils/BitmapCropping.kt
+++ features/fintech/home_credit/src/main/java/com/tokopedia/homecredit/utils/BitmapCropping.kt
@@ -5,10 +5,13 @@ import android.graphics.Bitmap
 import android.view.View
 import com.otaliastudios.cameraview.CameraView
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchersProvider
+import com.tokopedia.abstraction.common.utils.DisplayMetricUtils
 import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
 import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.dpToPx
+import com.tokopedia.kotlin.extensions.view.getScreenHeight
+import com.tokopedia.kotlin.extensions.view.getScreenWidth
 import com.tokopedia.unifycomponents.toPx
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
@@ -23,8 +26,7 @@ interface BitmapProcessingListener {
 }
 
 class BitmapCropping constructor(
-    private val context: Context,
-    private val camera: CameraView
+    private val context: Context
 ) : CoroutineScope {
 
     private val parentJob = SupervisorJob()
@@ -36,7 +38,7 @@ class BitmapCropping constructor(
 
     fun doCropping(bitmap: Bitmap, frame: View, listener: BitmapProcessingListener) {
         launchCatchError(coroutineContext, block = {
-            val bitmapCropped = doCroppingWithFrame(bitmap, frame, 0)
+            val bitmapCropped = doCroppingWithFrame(bitmap, frame)
 
             withContext(coroutineDispatchers.main) {
                 listener.onBitmapReady(bitmapCropped)
@@ -54,79 +56,25 @@ class BitmapCropping constructor(
      * @param padding to add padding between frame and final cropping result
      * @return cropped bitmap
      */
-    private fun doCroppingWithFrame(bitmap: Bitmap, frame: View, padding: Int = DEFAULT_PADDING): Bitmap {
-        val framePadding = padding.dpToPx(context.resources.displayMetrics)
-
-        val bitmapW = bitmap.width.toFloat()
-        val bitmapH = bitmap.height.toFloat()
-
-        val diffScaleW = camera.width.toFloat() / bitmapW
-        val diffScaleH = camera.height.toFloat() / bitmapH
-
-        val frameW = if (frame.width.toFloat() > bitmapW) bitmapW else frame.width.toFloat()
-        val frameH = if (frame.height.toFloat() > bitmapH) bitmapH else frame.height.toFloat()
-        val frameLeft = if (frame.left.toFloat() < 0) 0F else frame.left.toFloat()
-        val frameTop = if (frame.top.toFloat() < 0) 0F else frame.top.toFloat()
-
-        var offsetX: Float = (frameLeft / diffScaleW) - (camera.left.toFloat() / diffScaleW) - framePadding
-        offsetX = if (offsetX > bitmapW) frameLeft else if (offsetX < 0) 0F else offsetX
-        var offsetY: Float = (frameTop / diffScaleH) - (camera.top.toFloat() / diffScaleH) - framePadding
-        offsetY = if (offsetY > bitmapH) frameTop else if (offsetY < 0) 0F else offsetY
-
-        var newW: Float = (frameW / diffScaleW) + (framePadding * 2).toFloat() // 2 for padding on 2 side (left & right)
-        newW = when {
-            newW > bitmapW -> bitmapW
-            newW + offsetX > bitmapW -> newW - offsetX
-            else -> newW
-        }
-
-        var newH: Float = (frameH / diffScaleH) + (framePadding * 2).toFloat() // 2 for padding on 2 side (top & bottom)
-        newH = when {
-            newH > bitmapH -> bitmapH
-            newH + offsetY > bitmapH -> newH - offsetY
-            else -> newH
-        }
-
-//        return Bitmap.createBitmap(
-//            bitmap,
-//            offsetX.roundToInt(),
-//            offsetY.roundToInt(),
-//            newW.roundToInt(),
-//            newH.roundToInt()
-//        )
-
-        val ratioW = bitmap.width / camera.width
-        val ratioH = bitmap.height / camera.height
-
+    private fun doCroppingWithFrame(bitmap: Bitmap, frame: View): Bitmap {
         val framePos = IntArray(2)
-        frame.getLocationInWindow(framePos)
+        frame.getLocationOnScreen(framePos)
+
+        val x = ((bitmap.width - getScreenWidth()) / 2) + framePos[0]
+        val y = ((bitmap.height - getScreenHeight()) / 2) + framePos[1] - DisplayMetricUtils.getStatusBarHeight(context)
+        val w = getScreenWidth() - FRAME_MARGIN.toPx()
+        val h = w * 2 / 3
 
         return Bitmap.createBitmap(
             bitmap,
-            (bitmap.width / 2) - (frame.width * ratioW),
-            (bitmap.height / 2) - (frame.height * ratioH),
-            frame.width * ratioW,
-            frame.height * ratioH
+            x,
+            y,
+            w,
+            h
         )
     }
 
-    /**
-     * @param bitmap
-     * @param quality bitmap compress to X quality of original source
-     * @return compressed bitmap
-     */
-    private fun doCompression(bitmap: Bitmap, quality: Float): Bitmap {
-        val reduceQualityPercentage = quality / 100
-        val width = (bitmap.width * reduceQualityPercentage).roundToInt()
-        val height = (bitmap.height * reduceQualityPercentage).roundToInt()
-
-        if (width <= 0) throw Throwable("Please make sure final width > 0")
-        if (height <= 0) throw Throwable("Please make sure final height > 0")
-
-        return Bitmap.createScaledBitmap(bitmap, width, height, false)
-    }
-
     companion object {
-        const val DEFAULT_PADDING = 8
+        const val FRAME_MARGIN = 48
     }
 }
