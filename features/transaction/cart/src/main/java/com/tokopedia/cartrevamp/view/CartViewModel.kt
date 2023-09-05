0b7cd7cbdf1c78973e6dddc675c980ec3abd94d2
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-09-05 09:18:10 +0700

    fix conflict on build.gradle product_detail, and fix import on CartViewModel.kt

--- features/transaction/cart/src/main/java/com/tokopedia/cartrevamp/view/CartViewModel.kt
+++ features/transaction/cart/src/main/java/com/tokopedia/cartrevamp/view/CartViewModel.kt
@@ -75,6 +75,7 @@ import com.tokopedia.cartrevamp.view.uimodel.DisabledCollapsedHolderData
 import com.tokopedia.cartrevamp.view.uimodel.DisabledItemHeaderHolderData
 import com.tokopedia.cartrevamp.view.uimodel.DisabledReasonHolderData
 import com.tokopedia.cartrevamp.view.uimodel.FollowShopEvent
+import com.tokopedia.cartrevamp.view.uimodel.GetBmGmGroupProductTickerState
 import com.tokopedia.cartrevamp.view.uimodel.LoadRecentReviewState
 import com.tokopedia.cartrevamp.view.uimodel.LoadRecommendationState
 import com.tokopedia.cartrevamp.view.uimodel.LoadWishlistV2State
