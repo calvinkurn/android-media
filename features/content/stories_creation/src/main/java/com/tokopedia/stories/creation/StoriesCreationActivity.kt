6b13c6db498183c91c79645daf1892fa2729375c
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-09-05 14:35:21 +0700

    feat: add new module `stories_creation` for stories creation main flow

---
+++ features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/StoriesCreationActivity.kt
@@ -0,0 +1,9 @@
+package com.tokopedia.stories.creation
+
+import com.tokopedia.abstraction.base.view.activity.BaseActivity
+
+/**
+ * Created By : Jonathan Darwin on September 05, 2023
+ */
+class StoriesCreationActivity : BaseActivity() {
+}
