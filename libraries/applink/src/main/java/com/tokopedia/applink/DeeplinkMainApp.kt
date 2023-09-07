a90814278889c1b10c6e96a58ec6eb6bb8cfcbc1
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-09-07 18:45:55 +0700

    fix alphabetically applink webview-kyc

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMainApp.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMainApp.kt
@@ -1266,13 +1266,13 @@ object DeeplinkMainApp {
                 DeeplinkMapperUser.getRegisteredUserNavigation(deeplink)
             }
         ),
+        "webview" to mutableListOf(
+            DLP.goToLink { ApplinkConstInternalGlobal.WEBVIEW_BASE }
+        ),
         "webview-kyc" to mutableListOf(
             DLP.goTo { deeplink: String ->
                 DeeplinkMapperUser.getRegisteredNavigationUser(deeplink)
             }
-        ),
-        "webview" to mutableListOf(
-            DLP.goToLink { ApplinkConstInternalGlobal.WEBVIEW_BASE }
         ),
         "webviewbackhome" to mutableListOf(
             DLP.goToLink { ApplinkConstInternalGlobal.WEBVIEW_BACK_HOME }
