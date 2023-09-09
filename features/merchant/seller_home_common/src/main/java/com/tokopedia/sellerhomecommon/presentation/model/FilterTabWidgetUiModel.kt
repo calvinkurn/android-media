aa9a83eab3be256a2f46b2e13195258114be5440
reivinoktavianus <oktavianusreivin@gmail.com>
2023-09-09 18:51:09 +0700

    feat: Add layout for filter tab

---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/model/FilterTabWidgetUiModel.kt
@@ -0,0 +1,43 @@
+package com.tokopedia.sellerhomecommon.presentation.model
+
+import com.tokopedia.kotlin.model.ImpressHolder
+import com.tokopedia.sellerhomecommon.domain.model.TabModel
+import com.tokopedia.sellerhomecommon.presentation.adapter.WidgetAdapterFactory
+
+data class FilterTabWidgetUiModel(
+    override val id: String,
+    override val widgetType: String,
+    override val title: String,
+    override val subtitle: String,
+    override val tooltip: TooltipUiModel?,
+    override val tag: String,
+    override val appLink: String,
+    override val dataKey: String,
+    override val ctaText: String,
+    override val gridSize: Int,
+    override val isShowEmpty: Boolean,
+    override var data: BaseDataUiModel?,
+    override var impressHolder: ImpressHolder = ImpressHolder(),
+    override var isLoaded: Boolean,
+    override var isLoading: Boolean,
+    override var isFromCache: Boolean,
+    override var isNeedToBeRemoved: Boolean = false,
+    override var showLoadingState: Boolean = false,
+    override var emptyState: WidgetEmptyStateUiModel,
+    override var useRealtime: Boolean = false,
+    val filterTabs: List<TabModel>? = null,
+    val selectedFilterPage: String? = null
+): BaseWidgetUiModel<BaseDataUiModel> {
+
+    override fun copyWidget(): BaseWidgetUiModel<BaseDataUiModel> {
+        return this.copy()
+    }
+
+    override fun needToRefreshData(other: BaseWidgetUiModel<BaseDataUiModel>): Boolean {
+        return dataKey != other.dataKey
+    }
+
+    override fun type(typeFactory: WidgetAdapterFactory): Int {
+        return typeFactory.type(this)
+    }
+}
---
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/FilterTabViewHolder.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.sellerhomecommon.presentation.view.viewholder
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.sellerhomecommon.R
+import com.tokopedia.sellerhomecommon.databinding.ShcFilterTabWidgetBinding
+import com.tokopedia.sellerhomecommon.domain.model.TabModel
+import com.tokopedia.sellerhomecommon.presentation.model.FilterTabWidgetUiModel
+
+class FilterTabViewHolder(
+    view: View?,
+    private val listener: Listener
+): AbstractViewHolder<FilterTabWidgetUiModel>(view) {
+
+    private val binding by lazy { ShcFilterTabWidgetBinding.bind(itemView) }
+
+    override fun bind(element: FilterTabWidgetUiModel) {
+        with(binding) {
+            val selectedTab = element.filterTabs?.firstOrNull { it.page == element.selectedFilterPage }
+            selectedTab?.let { tab ->
+                tvFilterTabTitle.text = tab.tabTitle
+                tvFilterTabDesc.text = tab.tabTitle
+
+                root.setOnClickListener {
+                    listener.onFilterClicked(
+                        element.filterTabs,
+                        tab.page
+                    )
+                }
+            }
+        }
+    }
+
+    interface Listener: BaseViewHolderListener {
+        fun onFilterClicked(
+            tabs: List<TabModel>?,
+            selectedPage: String?
+        )
+    }
+
+    companion object {
+        @LayoutRes
+        val RES_LAYOUT = R.layout.shc_filter_tab_widget
+    }
+
+}
