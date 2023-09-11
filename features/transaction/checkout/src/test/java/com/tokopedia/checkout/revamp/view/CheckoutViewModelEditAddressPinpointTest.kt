ab8b307f30b9916ef090a6847936a8fa2e404001
hansenpw <hansen.wijaya@tokopedia.com>
2023-09-11 15:27:20 +0700

    fix unit test

--- features/transaction/checkout/src/test/java/com/tokopedia/checkout/revamp/view/CheckoutViewModelEditAddressPinpointTest.kt
+++ features/transaction/checkout/src/test/java/com/tokopedia/checkout/revamp/view/CheckoutViewModelEditAddressPinpointTest.kt
@@ -16,16 +16,15 @@ import com.tokopedia.checkout.revamp.view.uimodel.CheckoutUpsellModel
 import com.tokopedia.checkout.view.uimodel.ShipmentNewUpsellModel
 import com.tokopedia.logisticCommon.data.entity.address.RecipientAddressModel
 import com.tokopedia.logisticCommon.data.entity.geolocation.autocomplete.LocationPass
+import com.tokopedia.logisticCommon.data.response.KeroEditAddressResponse
 import com.tokopedia.purchase_platform.common.feature.ethicaldrug.domain.model.UploadPrescriptionUiModel
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUiModel
 import com.tokopedia.purchase_platform.common.feature.tickerannouncement.TickerAnnouncementHolderData
 import com.tokopedia.unifycomponents.Toaster
 import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.every
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import rx.Observable
 import java.io.IOException
 
 class CheckoutViewModelEditAddressPinpointTest : BaseCheckoutViewModelTest() {
@@ -62,14 +61,12 @@ class CheckoutViewModelEditAddressPinpointTest : BaseCheckoutViewModelTest() {
         val latitude = "123"
         val longitude = "456"
 
-        every { editAddressUseCase.createObservable(any()) } returns Observable.just(
-            """
-            {
-                "data": {
-                    "is_success": 1
-                }
-            }
-            """.trimIndent()
+        coEvery { pinpointUseCase(any()) } returns KeroEditAddressResponse.Data(
+            KeroEditAddressResponse.Data.KeroEditAddress(
+                KeroEditAddressResponse.Data.KeroEditAddress.KeroEditAddressSuccessResponse(
+                    isSuccess = 1
+                )
+            )
         )
 
         coEvery { getShipmentAddressFormV4UseCase(any()) } returns CartShipmentAddressFormData()
@@ -84,66 +81,6 @@ class CheckoutViewModelEditAddressPinpointTest : BaseCheckoutViewModelTest() {
 
         // Then
         coVerify { getShipmentAddressFormV4UseCase(any()) }
-    }
-
-    @Test
-    fun pinpointFailed_ShouldNavigateToSetPinpointWithErrorMessage() {
-        // Given
-        viewModel.listData.value = listOf(
-            CheckoutTickerErrorModel(errorMessage = ""),
-            CheckoutTickerModel(ticker = TickerAnnouncementHolderData()),
-            CheckoutAddressModel(
-                recipientAddressModel = RecipientAddressModel().apply {
-                    id = "1"
-                    addressName = "address 1"
-                    street = "street 1"
-                    postalCode = "12345"
-                    destinationDistrictId = "1"
-                    cityId = "1"
-                    provinceId = "1"
-                    recipientName = "user 1"
-                    recipientPhoneNumber = "1234567890"
-                }
-            ),
-            CheckoutUpsellModel(upsell = ShipmentNewUpsellModel()),
-            CheckoutProductModel("123"),
-            CheckoutOrderModel("123"),
-            CheckoutEpharmacyModel(epharmacy = UploadPrescriptionUiModel()),
-            CheckoutPromoModel(promo = LastApplyUiModel()),
-            CheckoutCostModel(),
-            CheckoutCrossSellGroupModel(),
-            CheckoutButtonPaymentModel()
-        )
-
-        val latitude = "123"
-        val longitude = "456"
-        val locationPass = LocationPass()
-
-        val errorMessage = "error"
-
-        every { editAddressUseCase.createObservable(any()) } returns Observable.just(
-            """
-            {
-                "data": {
-                    "is_success": 0
-                },
-                "message_error": ["$errorMessage"]
-            }
-            """.trimIndent()
-        )
-        var resultErrorMessage = ""
-
-        // When
-        viewModel.editAddressPinpoint(
-            latitude,
-            longitude,
-            locationPass
-        ) { errorMessage, locationPass ->
-            resultErrorMessage = errorMessage
-        }
-
-        // Then
-        assertEquals(errorMessage, resultErrorMessage)
     }
 
     @Test
@@ -179,17 +116,12 @@ class CheckoutViewModelEditAddressPinpointTest : BaseCheckoutViewModelTest() {
         val longitude = "456"
         val locationPass = LocationPass()
 
-        val errorMessage = "error"
-
-        every { editAddressUseCase.createObservable(any()) } returns Observable.just(
-            """
-            {
-                "data": {
-                    "is_success": 0
-                },
-                "message_error": []
-            }
-            """.trimIndent()
+        coEvery { pinpointUseCase(any()) } returns KeroEditAddressResponse.Data(
+            KeroEditAddressResponse.Data.KeroEditAddress(
+                KeroEditAddressResponse.Data.KeroEditAddress.KeroEditAddressSuccessResponse(
+                    isSuccess = 0
+                )
+            )
         )
 
         var resultErrorMessage = ""
@@ -240,7 +172,7 @@ class CheckoutViewModelEditAddressPinpointTest : BaseCheckoutViewModelTest() {
         val longitude = "456"
 
         val throwable = IOException("testing")
-        every { editAddressUseCase.createObservable(any()) } returns Observable.error(throwable)
+        coEvery { pinpointUseCase(any()) } throws throwable
 
         var resultErrorMessage = ""
 
