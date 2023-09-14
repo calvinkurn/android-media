e67e0fda0ced1c63d4e79b73cecfd15a7180b662
Muhammad Furqan <furqan@tokopedia.com>
2023-09-14 12:27:13 +0700

    [Stories] Add View Model Module to Dagger Component Shop

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/di/component/ShopPageHeaderComponent.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/di/component/ShopPageHeaderComponent.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.shop.pageheader.di.component
 
+import com.tokopedia.creation.common.di.ContentCreationViewModelModule
 import com.tokopedia.shop.common.di.component.ShopComponent
 import com.tokopedia.shop.pageheader.di.module.ShopPageBindModule
 import com.tokopedia.shop.pageheader.di.module.ShopPageHeaderModule
@@ -11,7 +12,14 @@ import dagger.Component
  * Created by hendry on 18/01/18.
  */
 @ShopPageHeaderScope
-@Component(modules = [ShopPageHeaderModule::class, ShopPageBindModule::class], dependencies = [ShopComponent::class])
+@Component(
+    modules = [
+        ShopPageHeaderModule::class,
+        ShopPageBindModule::class,
+        ContentCreationViewModelModule::class
+    ],
+    dependencies = [ShopComponent::class]
+)
 interface ShopPageHeaderComponent {
     fun inject(headerFragment: ShopPageHeaderFragment?)
 }
