215e102d3f45e8c28891891607f6257b0c176b82
Darian Thedy <darian.thedy@tokopedia.com>
2023-09-14 22:33:45 +0700

    [Product Card] Fix add to cart drawable color

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/reimagine/ProductCardAddToCart.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/reimagine/ProductCardAddToCart.kt
@@ -1,17 +1,22 @@
 package com.tokopedia.productcard.reimagine
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams
 import androidx.constraintlayout.widget.ConstraintSet.BOTTOM
 import androidx.constraintlayout.widget.ConstraintSet.END
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import androidx.core.view.setPadding
 import com.tokopedia.productcard.R
 import com.tokopedia.unifycomponents.UnifyButton
 import com.tokopedia.unifycomponents.toPx
 import com.tokopedia.iconunify.R as iconunifyR
+import com.tokopedia.unifyprinciples.R as unifyprinciplesR
 
 private const val ADD_TO_CART_PADDING_DP = 8
 private const val ADD_TO_CART_TO_IMAGE_MARGIN_END = 6
@@ -36,7 +41,7 @@ private fun AddToCartButton(context: Context) = UnifyButton(context).apply {
     buttonSize = UnifyButton.Size.MICRO
     buttonVariant = UnifyButton.Variant.GHOST
 
-    setDrawable(AppCompatResources.getDrawable(context, iconunifyR.drawable.iconunify_add))
+    setDrawable(addToCartDrawable(context))
 
     setPadding(ADD_TO_CART_PADDING_DP.toPx())
 }
@@ -49,3 +54,12 @@ private fun layoutParams() = LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
         ADD_TO_CART_TO_IMAGE_MARGIN_BOTTOM.toPx()
     )
 }
+
+private fun addToCartDrawable(context: Context): Drawable? =
+    AppCompatResources.getDrawable(context, iconunifyR.drawable.iconunify_add)?.apply {
+        mutate()
+        colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+            ContextCompat.getColor(context, unifyprinciplesR.color.Unify_GN500),
+            BlendModeCompat.SRC_ATOP
+        )
+    }
