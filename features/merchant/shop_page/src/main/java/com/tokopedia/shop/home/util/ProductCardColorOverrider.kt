32781dc3bbe926540083b0cd2f4cf7124a7f9eb3
fajarca <fajar.azhar@tokopedia.com>
2023-09-14 15:14:54 +0700

    Persist label color

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/util/ProductCardColorOverrider.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/util/ProductCardColorOverrider.kt
@@ -1,14 +1,22 @@
 package com.tokopedia.shop.home.util
 
+import android.graphics.PorterDuff
+import android.util.TypedValue
 import androidx.core.content.ContextCompat
 import com.tokopedia.productcard.ProductCardGridView
 import com.tokopedia.productcard.ProductCardListView
 import com.tokopedia.productcard.R as productcardR
 import com.tokopedia.unifycomponents.CardUnify2
+import com.tokopedia.unifycomponents.Label
+import com.tokopedia.unifycomponents.R as unifycomponentsR
+import com.tokopedia.unifycomponents.toPx
 import com.tokopedia.unifyprinciples.Typography
 import com.tokopedia.shop.R as shopR
 
 class ProductCardColorOverrider {
+
+    private val horizontalPaddingPx by lazy { 4.toPx() }
+    private val verticalPaddingPx by lazy { 3.toPx() }
 
     fun forceLightModeColor(productCard: ProductCardGridView?) {
         productCard?.let {
@@ -20,7 +28,36 @@ class ProductCardColorOverrider {
 
             val productPrice = productCard.findViewById<Typography>(productcardR.id.textViewPrice)
             productPrice.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_Unify_Unify_NN950_96_light))
+
+            val labelDiscount = productCard.findViewById<Label>(productcardR.id.labelDiscount)
+            labelDiscount.lightRed()
+
+            val productSlashPrice = productCard.findViewById<Typography>(productcardR.id.textViewSlashedPrice)
+            productSlashPrice.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_44))
+
+            val soldCount = productCard.findViewById<Typography>(productcardR.id.textViewSales)
+            soldCount.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_68))
+
+            val rating = productCard.findViewById<Typography>(productcardR.id.salesRatingFloat)
+            rating.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_68))
+
+            val gimmick = productCard.findViewById<Typography>(productcardR.id.textViewGimmick)
+            gimmick.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_YN400))
         }
+    }
+
+    private fun Label.lightRed() {
+        val textSize = 10.toPx()
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize.toFloat())
+        typeface = Typography.getFontType(context, true, Typography.SMALL)
+        setTextColor(ContextCompat.getColor(context, shopR.color.dms_static_Unify_RN500_light))
+
+        setPadding(horizontalPaddingPx, verticalPaddingPx, horizontalPaddingPx, verticalPaddingPx)
+
+        val drawable = ContextCompat.getDrawable(context, unifycomponentsR.drawable.label_bg)
+        drawable?.setColorFilter(context.resources.getColor(shopR.color.dms_static_Unify_RN100_light), PorterDuff.Mode.SRC_ATOP)
+
+        setBackgroundDrawable(drawable)
     }
 
     fun forceLightModeColor(productCard: ProductCardListView?) {
@@ -33,6 +70,21 @@ class ProductCardColorOverrider {
 
             val productPrice = productCard.findViewById<Typography>(productcardR.id.textViewPrice)
             productPrice.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_Unify_Unify_NN950_96_light))
+
+            val labelDiscount = productCard.findViewById<Label>(productcardR.id.labelDiscount)
+            labelDiscount.lightRed()
+
+            val productSlashPrice = productCard.findViewById<Typography>(productcardR.id.textViewSlashedPrice)
+            productSlashPrice.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_44))
+
+            val soldCount = productCard.findViewById<Typography>(productcardR.id.textViewSales)
+            soldCount.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_68))
+
+            val rating = productCard.findViewById<Typography>(productcardR.id.salesRatingFloat)
+            rating.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_NN950_68))
+
+            val gimmick = productCard.findViewById<Typography>(productcardR.id.textViewGimmick)
+            gimmick.setTextColor(ContextCompat.getColor(productName.context, shopR.color.dms_static_light_YN400))
         }
     }
 
