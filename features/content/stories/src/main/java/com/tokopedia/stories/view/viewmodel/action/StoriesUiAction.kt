a42439d82d9caa67c5b56b71678e515bb8d7dc2c
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-09-15 13:20:36 +0700

    `feat`: clean up code

--- features/content/stories/src/main/java/com/tokopedia/stories/view/viewmodel/action/StoriesUiAction.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/view/viewmodel/action/StoriesUiAction.kt
@@ -3,15 +3,15 @@ package com.tokopedia.stories.view.viewmodel.action
 import android.os.Bundle
 
 sealed interface StoriesUiAction {
-    data class SetInitialData(val bundle: Bundle?): StoriesUiAction
-    data class SaveInstanceStateData(val bundle: Bundle?): StoriesUiAction
-    data class GetSavedInstanceStateData(val bundle: Bundle?): StoriesUiAction
-    data class SetMainData(val selectedGroup: Int): StoriesUiAction
-    data class SelectGroup(val selectedGroup: Int, val showAnimation: Boolean): StoriesUiAction
+    data class SetInitialData(val bundle: Bundle?) : StoriesUiAction
+    data class SaveInstanceStateData(val bundle: Bundle?) : StoriesUiAction
+    data class GetSavedInstanceStateData(val bundle: Bundle?) : StoriesUiAction
+    data class SetMainData(val selectedGroup: Int) : StoriesUiAction
+    data class SelectGroup(val selectedGroup: Int, val showAnimation: Boolean) : StoriesUiAction
 
-    object NextDetail: StoriesUiAction
-    object PreviousDetail: StoriesUiAction
-    object PauseStories: StoriesUiAction
-    object ResumeStories: StoriesUiAction
-    object ContentIsLoaded: StoriesUiAction
+    object NextDetail : StoriesUiAction
+    object PreviousDetail : StoriesUiAction
+    object PauseStories : StoriesUiAction
+    object ResumeStories : StoriesUiAction
+    object ContentIsLoaded : StoriesUiAction
 }
--- features/content/stories/src/main/java/com/tokopedia/stories/view/viewmodel/event/StoriesUiEvent.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/view/viewmodel/event/StoriesUiEvent.kt
@@ -2,8 +2,8 @@ package com.tokopedia.stories.view.viewmodel.event
 
 sealed interface StoriesUiEvent {
     data class SelectGroup(val position: Int, val showAnimation: Boolean) : StoriesUiEvent
-    data class ErrorGroupPage(val throwable: Throwable): StoriesUiEvent
-    data class ErrorDetailPage(val throwable: Throwable): StoriesUiEvent
+    data class ErrorGroupPage(val throwable: Throwable) : StoriesUiEvent
+    data class ErrorDetailPage(val throwable: Throwable) : StoriesUiEvent
 
-    object FinishedAllStories: StoriesUiEvent
+    object FinishedAllStories : StoriesUiEvent
 }
