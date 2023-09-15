23aef2872b67ea2e258ac5248e5a1efb09860f3f
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-09-15 15:31:48 +0700

    feat `stories creation`: add more database component and model

--- features/content/creation_common/src/main/java/com/tokopedia/creation/common/data/entity/CreationUploadQueueEntity.kt
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/data/entity/CreationUploadQueueEntity.kt
@@ -11,6 +11,7 @@ import androidx.room.PrimaryKey
 const val CREATION_UPLOAD_QUEUE = "creation_upload_queue"
 @Entity(tableName = CREATION_UPLOAD_QUEUE)
 data class CreationUploadQueueEntity(
+
     @PrimaryKey
     @ColumnInfo(name = "id")
     val id: String = "",
@@ -24,4 +25,21 @@ data class CreationUploadQueueEntity(
     @ColumnInfo(name = "media_uri")
     val mediaUri: String = "",
 
+    @ColumnInfo(name = "cover_uri")
+    val cover_uri: String = "",
+
+    @ColumnInfo(name = "source_id")
+    val sourceId: String = "",
+
+    @ColumnInfo(name = "author_id")
+    val authorId: String = "",
+
+    @ColumnInfo(name = "author_type")
+    val authorType: String = "",
+
+    @ColumnInfo(name = "queue_status")
+    val queueStatus: String = "",
+
+    @ColumnInfo(name = "timestamp")
+    val timestamp: Long = 0L,
 )
--- features/content/creation_common/src/main/java/com/tokopedia/creation/common/di/CreationUploaderModule.kt
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/di/CreationUploaderModule.kt
@@ -3,6 +3,7 @@ package com.tokopedia.creation.common.di
 import android.content.Context
 import androidx.work.WorkManager
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.creation.common.data.database.CreationUploadQueueDatabase
 import dagger.Module
 import dagger.Provides
 
@@ -15,5 +16,10 @@ class CreationUploaderModule {
     @Provides
     fun provideWorkManager(@ApplicationContext context: Context): WorkManager {
         return WorkManager.getInstance(context)
+    }
+
+    @Provides
+    fun provideCreationUploadQueueDatabase(@ApplicationContext context: Context): CreationUploadQueueDatabase {
+        return CreationUploadQueueDatabase.getInstance(context)
     }
 }
