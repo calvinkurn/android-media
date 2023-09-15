22c5ab9fea9445b4b3b59331ba3921d74f1272c6
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2023-09-15 11:08:24 +0700

    `feat`: change request implementation to be parallel

--- features/content/stories/src/main/java/com/tokopedia/stories/data/repository/StoriesRepositoryImpl.kt
+++ features/content/stories/src/main/java/com/tokopedia/stories/data/repository/StoriesRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.tokopedia.stories.data.repository
 
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.kotlin.extensions.coroutines.asyncCatchError
 import com.tokopedia.stories.data.mapper.StoriesMapperImpl
 import com.tokopedia.stories.domain.model.StoriesRequestModel
 import com.tokopedia.stories.domain.model.StoriesTrackActivityRequestModel
@@ -15,25 +16,27 @@ import javax.inject.Inject
 class StoriesRepositoryImpl @Inject constructor(
     private val dispatchers: CoroutineDispatchers,
     private val storiesGroupsUseCase: StoriesGroupsUseCase,
-    private val storiesDetailsUSeCase: StoriesDetailsUseCase,
+    private val storiesDetailsUseCase: StoriesDetailsUseCase,
     private val storiesTrackActivityUseCase: StoriesTrackActivityUseCase,
     private val mapper: StoriesMapperImpl,
 ) : StoriesRepository {
 
     override suspend fun getStoriesInitialData(data: StoriesRequestModel): StoriesUiModel = withContext(dispatchers.io) {
-        val storiesGroupData = storiesGroupsUseCase(data)
-        val storiesDetailData = storiesDetailsUSeCase(data)
-        return@withContext mapper.mapStoriesInitialData(storiesGroupData, storiesDetailData)
+        val groupRequest = asyncCatchError(block = { storiesGroupsUseCase(data) }) { throw it }
+        val detailRequest = asyncCatchError(block = { storiesDetailsUseCase(data) }) { throw it }
+        val groupResult = groupRequest.await() ?: throw Throwable("Data Group is null")
+        val detailResult = detailRequest.await() ?: throw Throwable("Data Detail is null")
+        return@withContext mapper.mapStoriesInitialData(groupResult, detailResult)
     }
 
     override suspend fun getStoriesDetailData(data: StoriesRequestModel): StoriesDetail = withContext(dispatchers.io) {
-        val storiesDetailData = storiesDetailsUSeCase(data)
-        return@withContext mapper.mapStoriesDetailRequest("", storiesDetailData)
+        val detailRequest = storiesDetailsUseCase(data)
+        return@withContext mapper.mapStoriesDetailRequest("", detailRequest)
     }
 
     override suspend fun setStoriesTrackActivity(data: StoriesTrackActivityRequestModel): Boolean = withContext(dispatchers.io) {
-        val storiesTrackActivity = storiesTrackActivityUseCase(data)
-        return@withContext storiesTrackActivity.data.isSuccess
+        val trackActivityRequest = storiesTrackActivityUseCase(data)
+        return@withContext trackActivityRequest.data.isSuccess
     }
 
 }
