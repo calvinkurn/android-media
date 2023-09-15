e62cec78e4b6d6a6c07a31518106da41219be2bc
stevenwijaya <steven.wijaya@tokopedia.com>
2023-09-15 16:36:32 +0700

    add serialized name on additional info model

--- libraries/device_info/src/main/java/com/tokopedia/device/info/model/AdditionalInfoModel.kt
+++ libraries/device_info/src/main/java/com/tokopedia/device/info/model/AdditionalInfoModel.kt
@@ -7,20 +7,48 @@ import android.os.Build
 import android.os.Build.VERSION.SDK_INT
 import android.util.Base64
 import com.google.gson.Gson
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import com.tokopedia.config.GlobalConfig
 import com.tokopedia.device.info.DeviceInfo
 import timber.log.Timber
 import java.util.UUID
 
 data class AdditionalInfoModel(
+    @SerializedName("time")
+    @Expose
     val time: String,
+
+    @SerializedName("brand")
+    @Expose
     val brand: String,
+
+    @SerializedName("product")
+    @Expose
     val product: String,
+
+    @SerializedName("board")
+    @Expose
     val board: String,
+
+    @SerializedName("cpuAbi")
+    @Expose
     val cpuAbi: String,
+
+    @SerializedName("device")
+    @Expose
     val device: String,
+
+    @SerializedName("versionName")
+    @Expose
     val versionName: String,
+
+    @SerializedName("advertisingId")
+    @Expose
     val advertisingId: String,
+
+    @SerializedName("wideVineId")
+    @Expose
     val wideVineId: String
 ) {
 
