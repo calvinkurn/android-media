df65c5febc00d59e29fd20391e06b2108559234d
Muhammad Furqan <furqan@tokopedia.com>
2023-09-18 15:28:49 +0700

    [Stories] Implement Error View

--- features/content/creation_common/src/main/java/com/tokopedia/creation/common/presentation/bottomsheet/ContentCreationBottomSheet.kt
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/presentation/bottomsheet/ContentCreationBottomSheet.kt
@@ -51,7 +51,7 @@ class ContentCreationBottomSheet : BottomSheetUnify() {
                 val creationList = viewModel?.creationConfig?.collectAsStateWithLifecycle()
 
                 ContentCreationComponent(
-                    creationItemList = creationList?.value,
+                    creationConfig = creationList?.value,
                     selectedItem = selectedCreation?.value,
                     onSelectItem = {
                         viewModel?.selectCreationItem(it)
