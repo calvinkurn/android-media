2c0e39562476d1b8584a04ef1a531a59cba6ecd1
yusuf.hendrawan <yusuf.hendrawan@tokopedia.com>
2023-09-18 14:35:56 +0700

    Revert SellerApp thematic 14 anniversary

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
@@ -14,6 +14,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
 import androidx.lifecycle.LiveData
@@ -36,10 +37,8 @@ import com.tokopedia.coachmark.CoachMark2Item
 import com.tokopedia.empty_state.EmptyStateUnify
 import com.tokopedia.globalerror.GlobalError
 import com.tokopedia.gm.common.utils.CoachMarkPrefHelper
-import com.tokopedia.imageassets.TokopediaImageUrl
 import com.tokopedia.kotlin.extensions.orFalse
 import com.tokopedia.kotlin.extensions.view.EMPTY
-import com.tokopedia.kotlin.extensions.view.ONE
 import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.addOnImpressionListener
 import com.tokopedia.kotlin.extensions.view.addOneTimeGlobalLayoutListener
@@ -1912,6 +1911,7 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
             if (it is Success) {
                 val info = it.data
                 this.isNewSellerState = info.isNewSellerState
+                setViewBackgroundNewSeller()
                 if (info.subtitle.isBlank()) return@observe
 
                 if (info.subType == ShopStateInfoUiModel.SubType.TOAST) {
@@ -1925,46 +1925,86 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
         }
     }
 
+    private fun setViewBackgroundNewSeller() {
+        binding?.run {
+            if (isNewSellerState) {
+                viewBgShopStatus.visible()
+                viewBgShopStatus.layoutParams.height = LayoutParams.MATCH_PARENT
+                viewBgShopStatus.setImageResource(R.drawable.sah_shop_state_bg_new_seller)
+                viewBgShopStatus.requestLayout()
+                imgSahNewSellerLeft.loadImage(SellerHomeConst.Images.IMG_NEW_SELLER_LEFT) {
+                    useCache(true)
+                    listener(onSuccess = { _, _ ->
+                        imgSahNewSellerLeft.visible()
+                    })
+                }
+
+                imgSahNewSellerRight.loadImage(SellerHomeConst.Images.IMG_NEW_SELLER_RIGHT) {
+                    useCache(true)
+                    listener(onSuccess = { _, _ ->
+                        imgSahNewSellerRight.visible()
+                    })
+                }
+            } else {
+                imgSahNewSellerLeft.gone()
+                imgSahNewSellerRight.gone()
+                setViewBackground()
+            }
+            setSectionWidgetTextColor()
+        }
+    }
+
+    private fun setSectionWidgetTextColor() {
+        recyclerView?.post {
+            val widgets = adapter.data.map {
+                if (it is SectionWidgetUiModel) {
+                    val titleTextColor: Int
+                    val subTitleTextColor: Int
+                    if (isNewSellerState) {
+                        titleTextColor = com.tokopedia.unifyprinciples.R.color.Unify_NN0
+                        subTitleTextColor = com.tokopedia.unifyprinciples.R.color.Unify_NN0
+                    } else {
+                        titleTextColor = com.tokopedia.unifyprinciples.R.color.Unify_N700_96
+                        subTitleTextColor = com.tokopedia.unifyprinciples.R.color.Unify_N700_68
+                    }
+                    return@map it.copy(
+                        titleTextColorId = titleTextColor, subTitleTextColorId = subTitleTextColor
+                    )
+                }
+                return@map it
+            }
+
+            notifyWidgetWithSdkChecking {
+                updateWidgets(widgets)
+            }
+        }
+    }
+
     private fun setViewBackground() = binding?.run {
         val isOfficialStore = userSession.isShopOfficialStore
         val isPowerMerchant = userSession.isPowerMerchantIdle || userSession.isGoldMerchant
         when {
-            isOfficialStore -> showHomeBackground(
-                backgroundResource = R.drawable.sah_shop_state_bg_official_store,
-                effectUrl = TokopediaImageUrl.IMG_14TH_ANNIV_HOME_OS
-            )
-            isPowerMerchant -> showHomeBackground(
-                backgroundResource = R.drawable.sah_shop_state_bg_power_merchant,
-                effectUrl = TokopediaImageUrl.IMG_14TH_ANNIV_HOME_PM
-            )
-            else -> showHomeBackground(
-                backgroundResource = R.drawable.sah_shop_state_bg_regular_merchant,
-                effectUrl = TokopediaImageUrl.IMG_14TH_ANNIV_HOME_RM
-            )
+            isOfficialStore -> {
+                showRegularHomeBackground(R.drawable.sah_shop_state_bg_official_store)
+            }
+
+            isPowerMerchant -> {
+                showRegularHomeBackground(R.drawable.sah_shop_state_bg_power_merchant)
+            }
+
+            else -> {
+                viewBgShopStatus.gone()
+            }
         }
-        show14thIllustration()
     }
 
-    private fun show14thIllustration() {
-        binding?.imgSahNewSellerRight?.visible()
-        binding?.imgSahNewSellerRight?.loadImage(TokopediaImageUrl.IMG_14TH_ANNIV)
-    }
-
-    private fun showHomeBackground(
-        backgroundResource: Int,
-        effectUrl: String
-    ) {
+    private fun showRegularHomeBackground(backgroundResource: Int) {
         binding?.run {
             val height = requireActivity().resources.getDimensionPixelSize(R.dimen.sah_dimen_280dp)
             viewBgShopStatus.layoutParams.height = height
             viewBgShopStatus.visible()
             viewBgShopStatus.setImageResource(backgroundResource)
             viewBgShopStatus.requestLayout()
-
-            imgBackgroundEffect.loadImage(effectUrl) {
-                setPlaceHolder(Int.ONE.inv())
-                setErrorDrawable(Int.ZERO)
-            }
         }
     }
 
@@ -2740,6 +2780,7 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
         if (shopState == ShopStateUiModel.NewRegisteredShop) {
             showNewSellerDialog()
             isNewSellerState = true
+            setViewBackgroundNewSeller()
         } else if (shouldGetShopStateInfo) {
             getShopStateInfoIfEligible()
         }
@@ -2750,6 +2791,7 @@ class SellerHomeFragment : BaseListFragment<BaseWidgetUiModel<*>, WidgetAdapterF
             sellerHomeViewModel.getShopStateInfo()
         } else {
             isNewSellerState = false
+            setViewBackgroundNewSeller()
         }
     }
 
--- features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/SectionViewHolder.kt
+++ features/merchant/seller_home_common/src/main/java/com/tokopedia/sellerhomecommon/presentation/view/viewholder/SectionViewHolder.kt
@@ -43,7 +43,7 @@ class SectionViewHolder(
                 if (shouldShowTooltip) {
                     tvSectionTitle.setUnifyDrawableEnd(
                         iconId = IconUnify.INFORMATION,
-                        colorIcon = root.context.getResColor(com.tokopedia.unifyprinciples.R.color.Unify_NN900)
+                        colorIcon = root.context.getResColor(com.tokopedia.unifyprinciples.R.color.Unify_NN500)
                     )
                     tvSectionTitle.setOnClickListener {
                         showSectionTooltip(element, tooltip)
