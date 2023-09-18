2327d953666745b14fcd78549156bbe6589abfa2
Irpan - <irpan@tokopedia.com>
2023-09-18 14:15:05 +0700

    remove @expose

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomConfirmShipping.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomConfirmShipping.kt
@@ -2,27 +2,24 @@ package com.tokopedia.logisticseller.ui.confirmshipping.data.model
 
 import android.os.Parcel
 import android.os.Parcelable
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by fwidjaja on 2019-11-12.
  */
-data class SomConfirmShipping (
+data class SomConfirmShipping(
     @SerializedName("data")
-    @Expose
     val data: Data = Data()
 ) {
-        data class Data(
-            @SerializedName("mpLogisticConfirmShipping")
-            @Expose
-            val mpLogisticConfirmShipping: MpLogisticConfirmShipping = MpLogisticConfirmShipping()
-        ) {
+    data class Data(
+        @SerializedName("mpLogisticConfirmShipping")
+        val mpLogisticConfirmShipping: MpLogisticConfirmShipping = MpLogisticConfirmShipping()
+    ) {
 
         data class MpLogisticConfirmShipping(
-                @SerializedName("message")
-                @Expose
-                val listMessage: List<String> = listOf()) : Parcelable {
+            @SerializedName("message")
+            val listMessage: List<String> = listOf()
+        ) : Parcelable {
             constructor(parcel: Parcel) : this(parcel.createStringArrayList() ?: arrayListOf())
 
             override fun writeToParcel(parcel: Parcel, flags: Int) {
