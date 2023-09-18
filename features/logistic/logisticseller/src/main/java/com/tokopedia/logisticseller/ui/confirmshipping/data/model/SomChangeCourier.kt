25ee1a1058e3d7614420468d43c5c1c696563169
Irpan - <irpan@tokopedia.com>
2023-09-18 14:14:20 +0700

    remove @expose

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomChangeCourier.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomChangeCourier.kt
@@ -2,30 +2,27 @@ package com.tokopedia.logisticseller.ui.confirmshipping.data.model
 
 import android.os.Parcel
 import android.os.Parcelable
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by fwidjaja on 2019-11-23.
  */
-data class SomChangeCourier (
+data class SomChangeCourier(
     @SerializedName("data")
-    @Expose
     val data: Data = Data(),
 
     @SerializedName("errors")
-    @Expose
-    val errorList: List<Error> = listOf()) {
-        data class Data(
-            @SerializedName("mpLogisticChangeCourier")
-            @Expose
-            val mpLogisticChangeCourier: MpLogisticChangeCourier = MpLogisticChangeCourier()
-        ) {
+    val errorList: List<Error> = listOf()
+) {
+    data class Data(
+        @SerializedName("mpLogisticChangeCourier")
+        val mpLogisticChangeCourier: MpLogisticChangeCourier = MpLogisticChangeCourier()
+    ) {
 
         data class MpLogisticChangeCourier(
-                @SerializedName("message")
-                @Expose
-                val listMessage: List<String> = listOf()) : Parcelable {
+            @SerializedName("message")
+            val listMessage: List<String> = listOf()
+        ) : Parcelable {
             constructor(parcel: Parcel) : this(parcel.createStringArrayList() ?: arrayListOf())
 
             override fun writeToParcel(parcel: Parcel, flags: Int) {
@@ -49,8 +46,7 @@ data class SomChangeCourier (
     }
 
     data class Error(
-            @SerializedName("message")
-            @Expose
-            val errorMessage: String = ""
+        @SerializedName("message")
+        val errorMessage: String = ""
     )
 }
