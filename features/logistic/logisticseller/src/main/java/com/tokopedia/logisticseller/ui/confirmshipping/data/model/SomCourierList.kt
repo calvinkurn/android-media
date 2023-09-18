5e08d4a1766522a67137ff715700f3e789d56e20
Irpan - <irpan@tokopedia.com>
2023-09-18 14:13:30 +0700

    remove @expose

--- features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomCourierList.kt
+++ features/logistic/logisticseller/src/main/java/com/tokopedia/logisticseller/ui/confirmshipping/data/model/SomCourierList.kt
@@ -1,78 +1,66 @@
 package com.tokopedia.logisticseller.ui.confirmshipping.data.model
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by fwidjaja on 2019-11-22.
  */
-data class SomCourierList (
-        @SerializedName("data")
-        @Expose
-        val data: Data = Data()
+data class SomCourierList(
+    @SerializedName("data")
+    val data: Data = Data()
 ) {
 
     data class Data(
-            @SerializedName("mpLogisticGetEditShippingForm")
-            @Expose
-            val mpLogisticGetEditShippingForm: MpLogisticGetEditShippingForm = MpLogisticGetEditShippingForm()
+        @SerializedName("mpLogisticGetEditShippingForm")
+        val mpLogisticGetEditShippingForm: MpLogisticGetEditShippingForm = MpLogisticGetEditShippingForm()
     ) {
 
         data class MpLogisticGetEditShippingForm(
             @SerializedName("data")
-                @Expose
-                val dataShipment: DataShipment = DataShipment(),
+            val dataShipment: DataShipment = DataShipment(),
 
             @SerializedName("status")
-                @Expose
-                val status: String = "") {
+            val status: String = ""
+        ) {
 
             data class DataShipment(
-                    @SerializedName("shipment")
-                    @Expose
-                    val listShipment: List<Shipment> = listOf()) {
+                @SerializedName("shipment")
+                val listShipment: List<Shipment> = listOf()
+            ) {
 
                 data class Shipment(
-                        @SerializedName("shipping_max_add_fee")
-                        @Expose
-                        val shippingMaxAddFee: String = "0",
+                    @SerializedName("shipping_max_add_fee")
+                    val shippingMaxAddFee: String = "0",
 
-                        @SerializedName("shipment_id")
-                        @Expose
-                        val shipmentId: String = "0",
+                    @SerializedName("shipment_id")
+                    val shipmentId: String = "0",
 
-                        @SerializedName("shipment_available")
-                        @Expose
-                        val shipmentAvailable: Int = 0,
+                    @SerializedName("shipment_available")
+                    val shipmentAvailable: Int = 0,
 
-                        @SerializedName("shipment_image")
-                        @Expose
-                        val shipmentImage: String = "",
+                    @SerializedName("shipment_image")
+                    val shipmentImage: String = "",
 
-                        @SerializedName("shipment_name")
-                        @Expose
-                        val shipmentName: String = "",
+                    @SerializedName("shipment_name")
+                    val shipmentName: String = "",
 
-                        @SerializedName("shipment_package")
-                        @Expose
-                        val listShipmentPackage: List<ShipmentPackage> = listOf()) {
+                    @SerializedName("shipment_package")
+                    val listShipmentPackage: List<ShipmentPackage> = listOf()
+                ) {
 
                     data class ShipmentPackage(
-                            @SerializedName("desc")
-                            @Expose
-                            val desc: String = "",
+                        @SerializedName("desc")
+                        val desc: String = "",
 
-                            @SerializedName("active")
-                            @Expose
-                            val active: Int = 0,
+                        @SerializedName("active")
+                        val active: Int = 0,
 
-                            @SerializedName("name")
-                            @Expose
-                            val name: String = "",
+                        @SerializedName("name")
+                        val name: String = "",
 
-                            @SerializedName("sp_id")
-                            @Expose
-                            val spId: String = "")
+                        @SerializedName("sp_id")
+                        val spId: String = ""
+                    )
                 }
             }
         }
