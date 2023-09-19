2ef2fab18c1c052ae264685dd3532571b76a4e78
Misael Jonathan <misael.jonathan@tokopedia.com>
2023-09-19 13:00:57 +0700

    feat(travel_calendar): remove kotlin synthetic and migrate to view binding

--- libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/singlepickcalendar/SinglePickCalendarWidget.kt
+++ libraries/travel_calendar/src/main/java/com/tokopedia/travelcalendar/singlepickcalendar/SinglePickCalendarWidget.kt
@@ -1,20 +1,20 @@
 package com.tokopedia.travelcalendar.singlecalendar
 
 import android.app.Application
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.tokopedia.calendar.CalendarPickerView
 import com.tokopedia.calendar.Legend
 import com.tokopedia.travelcalendar.*
+import com.tokopedia.travelcalendar.databinding.DialogCalendarSinglePickBinding
 import com.tokopedia.travelcalendar.selectionrangecalendar.SelectionRangeCalendarWidget
 import com.tokopedia.travelcalendar.viewmodel.TravelHolidayCalendarViewModel
 import com.tokopedia.unifycomponents.BottomSheetUnify
-import kotlinx.android.synthetic.main.dialog_calendar_single_pick.loading_progress_bar
-import kotlinx.android.synthetic.main.dialog_calendar_single_pick.view.*
+import com.tokopedia.utils.lifecycle.autoClearedNullable
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -22,8 +22,6 @@ import java.util.*
 import javax.inject.Inject
 
 open class SinglePickCalendarWidget : BottomSheetUnify() {
-    lateinit var calendar: CalendarPickerView
-
     private lateinit var listenerCalendar: ActionListener
 
     private lateinit var holidayCalendarViewModel: TravelHolidayCalendarViewModel
@@ -33,6 +31,8 @@ open class SinglePickCalendarWidget : BottomSheetUnify() {
     var selectedDate: Date = Date()
 
     var isFirstTime: Boolean = true
+
+    private var binding by autoClearedNullable<DialogCalendarSinglePickBinding>()
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -78,7 +78,7 @@ open class SinglePickCalendarWidget : BottomSheetUnify() {
 
     private fun initInjector() {
         val component = TravelCalendarComponentInstance
-                .getComponent(activity?.application as Application)
+            .getComponent(activity?.application as Application)
         component.inject(this)
     }
 
@@ -87,54 +87,56 @@ open class SinglePickCalendarWidget : BottomSheetUnify() {
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-
-    private fun initChildLayout(){
-        val childView = View.inflate(context, R.layout.dialog_calendar_single_pick, null)
-        calendar = childView.calendar_unify.calendarPickerView as CalendarPickerView
-        setChild(childView)
+    private fun initChildLayout() {
+        binding = DialogCalendarSinglePickBinding.inflate(LayoutInflater.from(context))
+        setChild(binding?.root)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loading_progress_bar.visibility = View.VISIBLE
+        binding?.loadingProgressBar?.visibility = View.VISIBLE
         holidayCalendarViewModel.getCalendarHoliday()
-        holidayCalendarViewModel.holidayCalendarData.observe(this, androidx.lifecycle.Observer {
-            loading_progress_bar.visibility = View.GONE
-            it?.let {
-                if (isFirstTime) {
-                    renderSinglePickCalendar(it)
-                    isFirstTime = false
+        holidayCalendarViewModel.holidayCalendarData.observe(
+            this,
+            androidx.lifecycle.Observer {
+                binding?.loadingProgressBar?.visibility = View.GONE
+                it?.let {
+                    if (isFirstTime) {
+                        renderSinglePickCalendar(it)
+                        isFirstTime = false
+                    }
                 }
             }
-        })
-
+        )
     }
 
     open fun renderSinglePickCalendar(holidayArrayList: ArrayList<Legend>) {
         val nextYear = Calendar.getInstance()
-        nextYear.add(Calendar.YEAR,1)
+        nextYear.add(Calendar.YEAR, 1)
 
-        calendar?.init(minDate, nextYear.time, holidayArrayList)
+        binding?.let {
+            val calendar = it.calendarUnify.calendarPickerView
+            calendar?.init(minDate, nextYear.time, holidayArrayList)
                 ?.inMode(CalendarPickerView.SelectionMode.SINGLE)
                 ?.withSelectedDate(selectedDate)
 
-        calendar?.setOnDateSelectedListener(object : CalendarPickerView.OnDateSelectedListener {
-            override fun onDateSelected(date: Date) {
-                if (::listenerCalendar.isInitialized) {
-                    listenerCalendar?.onDateSelected(date)
+            calendar?.setOnDateSelectedListener(object : CalendarPickerView.OnDateSelectedListener {
+                override fun onDateSelected(date: Date) {
+                    if (::listenerCalendar.isInitialized) {
+                        listenerCalendar.onDateSelected(date)
 
-                    GlobalScope.launch {
-                        delay(300)
-                        dismissAllowingStateLoss()
+                        GlobalScope.launch {
+                            delay(300)
+                            dismissAllowingStateLoss()
+                        }
                     }
                 }
-            }
 
-            override fun onDateUnselected(date: Date) {
-
-            }
-        })
+                override fun onDateUnselected(date: Date) {
+                }
+            })
+        }
     }
 
     fun setListener(listener: ActionListener) {
@@ -150,15 +152,17 @@ open class SinglePickCalendarWidget : BottomSheetUnify() {
         const val MAX_DATE = "max_date"
         const val SELECTED_DATE = "selected_date"
 
-        fun newInstance(minDateString: String, maxDateString: String,
-                        selectedDate: String): SinglePickCalendarWidget =
-                SinglePickCalendarWidget().also {
-                    it.arguments = Bundle().apply {
-                        putString(MIN_DATE, minDateString)
-                        putString(MAX_DATE, maxDateString)
-                        putString(SELECTED_DATE, selectedDate)
-                    }
+        fun newInstance(
+            minDateString: String,
+            maxDateString: String,
+            selectedDate: String
+        ): SinglePickCalendarWidget =
+            SinglePickCalendarWidget().also {
+                it.arguments = Bundle().apply {
+                    putString(MIN_DATE, minDateString)
+                    putString(MAX_DATE, maxDateString)
+                    putString(SELECTED_DATE, selectedDate)
                 }
+            }
     }
-
 }
