3c6b067cf78e531ad20adc7d657de846e7bc83d6
fajarca <fajar.azhar@tokopedia.com>
2023-09-19 13:37:05 +0700

    Add force light mode on flash sale widget

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeProductItemListViewHolder.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/home/view/adapter/viewholder/ShopHomeProductItemListViewHolder.kt
@@ -25,7 +25,7 @@ open class ShopHomeProductItemListViewHolder(
     itemView: View,
     private val shopHomeEndlessProductListener: ShopHomeEndlessProductListener?,
     private val isShowTripleDot: Boolean,
-    shopHomeListener: ShopHomeListener
+    private val shopHomeListener: ShopHomeListener
 ) : AbstractViewHolder<ShopHomeProductUiModel>(itemView) {
     private val viewBinding: ItemShopHomeProductCardListBinding? by viewBinding()
     private var productCard: ProductCardListView? = null
@@ -55,7 +55,10 @@ open class ShopHomeProductItemListViewHolder(
         )
         productCard?.setProductModel(productCardModel)
         setListener(productCardModel)
-        productCard?.forceLightModeColor(productCard)
+
+        if (shopHomeListener.isOverrideTheme()) {
+            productCard?.forceLightModeColor(productCard)
+        }
     }
 
     protected open fun setListener(productCardModel: ProductCardModel) {
