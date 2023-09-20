cba969aaa9d4732ce9bc0c4e9ac190db09d4f277
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-09-20 14:45:38 +0700

    change applink name account linking

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMainApp.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkMainApp.kt
@@ -379,7 +379,7 @@ object DeeplinkMainApp {
         ),
         "gojek-account-link" to mutableListOf(
             DLP.matchPattern("") { _: String ->
-                ApplinkConstInternalUserPlatform.ACCOUNT_LINKING_WEBVIEW
+                ApplinkConstInternalUserPlatform.LINK_ACCOUNT_WEBVIEW
             }
         ),
         "gold-merchant-statistic-dashboard" to mutableListOf(
