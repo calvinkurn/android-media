4277380bd3a967e8a7d1e6be4f61f81bf7db7b29
hiradevina <fakhira.devina@tokopedia.com>
2023-09-21 14:14:33 +0700

    feat: remove KeroRepository.kt

--- features/logistic/logisticCommon/src/main/java/com/tokopedia/logisticCommon/data/repository/KeroRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.logisticCommon.data.repository
-
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
-import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import javax.inject.Inject
-
-class KeroRepository @Inject constructor(@ApplicationContext private val gql: GraphqlRepository)
