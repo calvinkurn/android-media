69c9b673028387dd76ae1235536fb415501284cd
kelvinsaputra <kelvin.saputra@tokopedia.com>
2023-09-21 13:21:29 +0700

    feat: separate the bottomsheet

--- libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/view/activity/UniversalSharingUniversalSharingPostPurchaseSharingActivity.kt
+++ libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/view/activity/UniversalSharingUniversalSharingPostPurchaseSharingActivity.kt
@@ -1,108 +1,23 @@
 package com.tokopedia.universal_sharing.view.activity
 
 import android.os.Bundle
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.tokopedia.abstraction.base.view.activity.BaseActivity
 import com.tokopedia.kotlin.extensions.view.hideKeyboard
-import com.tokopedia.kotlin.extensions.view.show
-import com.tokopedia.unifycomponents.BottomSheetUnify
-import com.tokopedia.universal_sharing.R
-import com.tokopedia.universal_sharing.view.bottomsheet.adapter.UniversalSharingPostPurchaseAdapter
-import com.tokopedia.universal_sharing.view.bottomsheet.listener.UniversalSharingPostPurchaseBottomSheetListener
-import com.tokopedia.universal_sharing.view.bottomsheet.typefactory.UniversalSharingTypeFactoryImpl
-import com.tokopedia.universal_sharing.view.model.UniversalSharingPostPurchaseProductUiModel
-import com.tokopedia.universal_sharing.view.model.UniversalSharingPostPurchaseShopTitleUiModel
+import com.tokopedia.universal_sharing.view.bottomsheet.UniversalSharingPostPurchaseBottomSheet
 
-class UniversalSharingUniversalSharingPostPurchaseSharingActivity:
-    BaseActivity(),
-    UniversalSharingPostPurchaseBottomSheetListener {
-
-    private var bottomSheet: BottomSheetUnify? = null
-    private var loaderLayout: ConstraintLayout? = null
+class UniversalSharingUniversalSharingPostPurchaseSharingActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setBottomSheet()
         showBottomSheet()
-    }
-
-    private fun setBottomSheet() {
-        bottomSheet = BottomSheetUnify()
-        setupBottomSheet()
     }
 
     private fun showBottomSheet() {
         currentFocus?.hideKeyboard()
-        bottomSheet?.show(
-            supportFragmentManager, ::UniversalSharingUniversalSharingPostPurchaseSharingActivity.name)
-    }
-
-    private fun setupBottomSheet() {
-        setupBottomSheetConfig()
-        bottomSheet?.setTitle(
-            getString(R.string.universal_sharing_post_purchase_bottomsheet_title))
-        setBottomSheetChildView()
-        setBottomSheetListener()
-    }
-
-    private fun setupBottomSheetConfig() {
-        bottomSheet?.apply {
-            showHeader = true // show title
-            showCloseIcon = true // show close button
-            clearContentPadding = true // remove default margin
-        }
-    }
-
-    private fun setBottomSheetChildView() {
-        val view = View.inflate(
-            this,
-            R.layout.universal_sharing_post_purchase_bottomsheet,
-            null
+        val bottomSheet = UniversalSharingPostPurchaseBottomSheet()
+        bottomSheet.show(
+            supportFragmentManager,
+            ::UniversalSharingUniversalSharingPostPurchaseSharingActivity.name
         )
-        loaderLayout = view.findViewById(R.id.universal_sharing_layout_loading)
-        setRecyclerView(view)
-        bottomSheet?.setChild(view)
-    }
-
-    private fun setBottomSheetListener() {
-        bottomSheet?.setCloseClickListener {
-            finish()
-        }
-    }
-
-    private fun setRecyclerView(view: View) {
-        val typeFactory = UniversalSharingTypeFactoryImpl(this)
-        val adapter = UniversalSharingPostPurchaseAdapter(typeFactory)
-        val recyclerView: RecyclerView? = view.findViewById(R.id.universal_sharing_post_purchase_rv)
-        recyclerView?.apply {
-            this.adapter = adapter
-            this.layoutManager = LinearLayoutManager(context)
-        }
-        adapter.addElement(
-            UniversalSharingPostPurchaseShopTitleUiModel(
-                iconUrl = "https://images.tokopedia.net/img/official_store/badge_os.png",
-                name = "Xiaomi Official Store"
-            )
-        )
-        adapter.addElement(
-            UniversalSharingPostPurchaseProductUiModel(
-                productId = "123123",
-                name = "Xiaomi 12 Pro 256 GB - Pink",
-                price = "300.000",
-                imageUrl = "https://images.tokopedia.net/img/cache/215-square/GAnVPX/2022/6/29/e6e99027-a32b-4cc9-b9ae-0eea6ef2e6f3.png",
-                isLoading = false
-            )
-        )
-    }
-
-    override fun onClickShare(productId: String) {
-        if (productId.isNotBlank()) {
-            loaderLayout?.show()
-        } else {
-            // todo: Handle
-        }
     }
 }
---
+++ libraries/universal_sharing/src/main/java/com/tokopedia/universal_sharing/view/bottomsheet/UniversalSharingPostPurchaseBottomSheet.kt
@@ -0,0 +1,93 @@
+package com.tokopedia.universal_sharing.view.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.unifycomponents.BottomSheetUnify
+import com.tokopedia.universal_sharing.R
+import com.tokopedia.universal_sharing.view.bottomsheet.adapter.UniversalSharingPostPurchaseAdapter
+import com.tokopedia.universal_sharing.view.bottomsheet.listener.UniversalSharingPostPurchaseBottomSheetListener
+import com.tokopedia.universal_sharing.view.bottomsheet.typefactory.UniversalSharingTypeFactoryImpl
+import com.tokopedia.universal_sharing.view.model.UniversalSharingPostPurchaseProductUiModel
+import com.tokopedia.universal_sharing.view.model.UniversalSharingPostPurchaseShopTitleUiModel
+
+class UniversalSharingPostPurchaseBottomSheet :
+    BottomSheetUnify(),
+    UniversalSharingPostPurchaseBottomSheetListener {
+
+    private var loaderLayout: ConstraintLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupBottomSheetConfig()
+        setBottomSheetChildView()
+    }
+
+    private fun setupBottomSheetConfig() {
+        showHeader = true // show title
+        showCloseIcon = true // show close button
+        clearContentPadding = true // remove default margin
+        isDragable = false // should be not draggable
+        // set max peek height
+        customPeekHeight = 100
+    }
+
+    private fun setBottomSheetChildView() {
+        val view = LayoutInflater.from(context).inflate(
+            R.layout.universal_sharing_post_purchase_bottomsheet,
+            null,
+            false
+        )
+        loaderLayout = view.findViewById(R.id.universal_sharing_layout_loading)
+        setChild(view)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBottomSheetTitle()
+        setRecyclerView(view)
+    }
+
+    private fun setBottomSheetTitle() {
+        setTitle(getString(R.string.universal_sharing_post_purchase_bottomsheet_title))
+    }
+
+    private fun setRecyclerView(view: View) {
+        val typeFactory = UniversalSharingTypeFactoryImpl(this)
+        val adapter = UniversalSharingPostPurchaseAdapter(typeFactory)
+        val recyclerView: RecyclerView? = view.findViewById(R.id.universal_sharing_post_purchase_rv)
+        recyclerView?.apply {
+            this.adapter = adapter
+            this.layoutManager = LinearLayoutManager(context)
+        }
+        adapter.addElement(
+            UniversalSharingPostPurchaseShopTitleUiModel(
+                iconUrl = "https://images.tokopedia.net/img/official_store/badge_os.png",
+                name = "Xiaomi Official Store"
+            )
+        )
+        for (i in 1..20) {
+            adapter.addElement(
+                UniversalSharingPostPurchaseProductUiModel(
+                    productId = "123123",
+                    name = "Xiaomi 12 Pro 256 GB - Pink",
+                    price = "300.000",
+                    imageUrl = "https://images.tokopedia.net/img/cache/215-square/GAnVPX/2022/6/29/e6e99027-a32b-4cc9-b9ae-0eea6ef2e6f3.png",
+                    isLoading = false
+                )
+            )
+        }
+    }
+
+    override fun onClickShare(productId: String) {
+        if (productId.isNotBlank()) {
+            loaderLayout?.show()
+        } else {
+            // todo: Handle
+        }
+    }
+}
