bf53b4c0c7035f90762ac338b28d79a4cb52d1be
Misael Jonathan <misael.jonathan@tokopedia.com>
2023-09-25 13:00:34 +0700

    feat(checkout): iris session usage removal for tracker

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/di/CheckoutModule.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/di/CheckoutModule.kt
@@ -1,10 +1,7 @@
 package com.tokopedia.checkout.revamp.di
 
-import android.content.Context
-import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
 import com.tokopedia.checkout.analytics.CheckoutTradeInAnalytics
-import com.tokopedia.iris.util.IrisSession
 import com.tokopedia.purchase_platform.common.analytics.EPharmacyAnalytics
 import com.tokopedia.purchase_platform.common.di.PurchasePlatformBaseModule
 import com.tokopedia.user.session.UserSessionInterface
@@ -28,11 +25,5 @@ class CheckoutModule {
     @ActivityScope
     fun provideEPharmacyAnalytics(userSession: UserSessionInterface): EPharmacyAnalytics {
         return EPharmacyAnalytics(userSession.userId)
-    }
-
-    @Provides
-    @ActivityScope
-    fun provideIris(@ApplicationContext context: Context): IrisSession {
-        return IrisSession(context)
     }
 }
