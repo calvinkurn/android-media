eac43960f90f85b19dee7426a13e9bba1d8c32ea
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-09-25 11:09:40 +0700

    fix `stories creation`: getTopQueue query

--- features/content/creation_common/src/main/java/com/tokopedia/creation/common/upload/data/local/dao/CreationUploadQueueDao.kt
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/upload/data/local/dao/CreationUploadQueueDao.kt
@@ -13,13 +13,13 @@ import com.tokopedia.creation.common.upload.data.local.entity.CreationUploadQueu
 @Dao
 interface CreationUploadQueueDao {
 
-    @Query("SELECT * FROM $CREATION_UPLOAD_QUEUE ORDER BY timestamp DESC LIMIT 1")
+    @Query("SELECT * FROM $CREATION_UPLOAD_QUEUE ORDER BY timestamp ASC LIMIT 1")
     suspend fun getTopQueue(): CreationUploadQueueEntity?
 
     @Insert
     suspend fun insert(entity: CreationUploadQueueEntity)
 
-    @Query("DELETE FROM $CREATION_UPLOAD_QUEUE WHERE queue_id IN (SELECT queue_id FROM $CREATION_UPLOAD_QUEUE ORDER BY timestamp DESC LIMIT 1)")
+    @Query("DELETE FROM $CREATION_UPLOAD_QUEUE WHERE queue_id IN (SELECT queue_id FROM $CREATION_UPLOAD_QUEUE ORDER BY timestamp ASC LIMIT 1)")
     suspend fun deleteTopQueue()
 
     @Query("DELETE FROM $CREATION_UPLOAD_QUEUE WHERE queue_id = :queueId")
