04d6bc2f244c08005ed5594ebf85f35c5c8d3d93
isfaaghyth <isfaaghyth@gmail.com>
2023-09-26 18:23:18 +0700

    feat: adjust the saveBitmap usage in flatten and placement

--- features/media/universal-editor/src/main/java/com/tokopedia/editor/data/repository/ImageFlattenRepository.kt
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/data/repository/ImageFlattenRepository.kt
@@ -4,11 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import com.tokopedia.editor.util.getEditorCacheFolderPath
-import com.tokopedia.utils.file.FileUtil
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 interface ImageFlattenRepository {
@@ -20,7 +17,7 @@ class ImageFlattenRepositoryImpl @Inject constructor(
 ) : ImageFlattenRepository {
 
     override fun flattenImage(imageBitmap: Bitmap, textBitmap: Bitmap?): Flow<String> {
-        return callbackFlow {
+        return flow {
             val flattenBitmap = Bitmap.createBitmap(imageBitmap.width, imageBitmap.height, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(flattenBitmap)
 
@@ -31,9 +28,7 @@ class ImageFlattenRepositoryImpl @Inject constructor(
                 canvas.drawBitmap(it, 0f, 0f, null)
             }
 
-            val path = getEditorCacheFolderPath() + FileUtil.generateUniqueFileName() + FILENAME_SUFFIX
-            trySend(imageSaveRepository.saveBitmap(path, flattenBitmap))
-            awaitClose { channel.close() }
+            emit(imageSaveRepository.saveBitmap(flattenBitmap))
         }
     }
 
