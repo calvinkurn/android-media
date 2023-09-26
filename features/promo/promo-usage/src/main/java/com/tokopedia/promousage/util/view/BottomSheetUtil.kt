9fc9a763665290625f869a9d431d3feb6078727b
Bobby Ingkirama <bobby.ingkirama@tokopedia.com>
2023-09-27 06:26:56 +0700

    fix(promo-usage)(lint): use import alias for R class

--- features/promo/promo-usage/src/main/java/com/tokopedia/promousage/util/view/BottomSheetUtil.kt
+++ features/promo/promo-usage/src/main/java/com/tokopedia/promousage/util/view/BottomSheetUtil.kt
@@ -6,20 +6,22 @@ import android.graphics.drawable.Drawable
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import com.tokopedia.unifycomponents.R as unifycomponentsR
 
 internal object BottomSheetUtil {
 
     fun generateBackgroundDrawableWithColor(context: Context, hexColor: String): Drawable? {
         val bgDrawable = ContextCompat
-            .getDrawable(context, com.tokopedia.unifycomponents.R.drawable.bottomsheet_background)
+            .getDrawable(context, unifycomponentsR.drawable.bottomsheet_background)
         bgDrawable?.let {
             if (hexColor.isNotBlank()) {
                 DrawableCompat.setTint(DrawableCompat.wrap(it), Color.parseColor(hexColor))
             } else {
                 DrawableCompat.setTint(
-                    DrawableCompat.wrap(it), ContextCompat.getColor(
+                    DrawableCompat.wrap(it),
+                    ContextCompat.getColor(
                         context,
-                        com.tokopedia.unifyprinciples.R.color.Unify_Background
+                        unifycomponentsR.color.Unify_Background
                     )
                 )
             }
@@ -29,7 +31,7 @@ internal object BottomSheetUtil {
 
     fun generateBackgroundDrawableWithColor(context: Context, @ColorRes colorRes: Int): Drawable? {
         val bgDrawable = ContextCompat
-            .getDrawable(context, com.tokopedia.unifycomponents.R.drawable.bottomsheet_background)
+            .getDrawable(context, unifycomponentsR.drawable.bottomsheet_background)
         bgDrawable?.let {
             DrawableCompat.setTint(
                 DrawableCompat.wrap(it),
