1d98366e56ad436f34e659c1cf36a96c47310e7e
hiradevina <fakhira.devina@tokopedia.com>
2023-09-26 09:30:39 +0700

    feat: remove get chosen address from repo

--- features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/data/query/ChooseAddressQuery.kt
+++ features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/data/query/ChooseAddressQuery.kt
@@ -2,44 +2,6 @@ package com.tokopedia.localizationchooseaddress.data.query
 
 object ChooseAddressQuery {
 
-    val getStateChosenAddress = """
-        query KeroAddrGetStateChosenAddress(${'$'}source: String!, ${'$'}is_tokonow_request: Boolean!){
-          keroAddrGetStateChosenAddress(source: ${'$'}source, is_tokonow_request: ${'$'}is_tokonow_request) {
-            data {
-              addr_id
-              receiver_name
-              addr_name
-              district
-              city
-              city_name
-              district_name
-              status
-              latitude
-              longitude
-              postal_code
-            }
-            tokonow {
-              shop_id
-              warehouse_id
-              warehouses {
-                warehouse_id
-                service_type
-              }
-              service_type
-              tokonow_last_update
-            }
-            kero_addr_error {
-              code
-              detail
-            }
-            status
-            server_process_time
-            config
-          }
-        }
-
-    """.trimIndent()
-
     val getDefaultChosenAddress = """
         query KeroAddrGetDefaultChosenAddress(${'$'}lat_long: String!,  ${'$'}source: String!,  ${'$'}is_tokonow_request: Boolean!){
           keroAddrGetDefaultChosenAddress(input: {lat_long: ${'$'}lat_long, source: ${'$'}source, is_tokonow_request: ${'$'}is_tokonow_request}) {
--- features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/data/repository/ChooseAddressRepository.kt
+++ features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/data/repository/ChooseAddressRepository.kt
@@ -5,21 +5,10 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.data.model.GraphqlRequest
 import com.tokopedia.localizationchooseaddress.data.query.ChooseAddressQuery
 import com.tokopedia.localizationchooseaddress.domain.response.GetDefaultChosenAddressGqlResponse
-import com.tokopedia.localizationchooseaddress.domain.response.GetStateChosenAddressQglResponse
 import com.tokopedia.localizationchooseaddress.util.getResponse
 import javax.inject.Inject
 
 class ChooseAddressRepository @Inject constructor(@ApplicationContext private val gql: GraphqlRepository) {
-
-    suspend fun getStateChosenAddress(source: String, isTokonow: Boolean): GetStateChosenAddressQglResponse {
-        val param = mapOf("source" to source, "is_tokonow_request" to isTokonow)
-        val request = GraphqlRequest(
-            ChooseAddressQuery.getStateChosenAddress,
-            GetStateChosenAddressQglResponse::class.java,
-            param
-        )
-        return gql.getResponse(request)
-    }
 
     suspend fun getDefaultChosenAddress(latLong: String?, source: String, isTokonow: Boolean): GetDefaultChosenAddressGqlResponse {
         val param = mapOf(
