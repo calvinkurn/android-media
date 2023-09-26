8b8bfed6bd17e0d3070d92eec62b6e1e1136768a
hiradevina <fakhira.devina@tokopedia.com>
2023-09-26 11:19:49 +0700

    fix: serialize state choose address param

--- features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/domain/model/StateChooseAddressParam.kt
+++ features/logistic/localizationchooseaddress/src/main/java/com/tokopedia/localizationchooseaddress/domain/model/StateChooseAddressParam.kt
@@ -4,14 +4,23 @@ import com.google.gson.annotations.SerializedName
 import com.tokopedia.graphql.data.GqlParam
 
 data class StateChooseAddressParam(
+    @SerializedName("status")
     val status: Int,
+    @SerializedName("addr_id")
     val addressId: Long?,
+    @SerializedName("receiver_name")
     val receiverName: String,
+    @SerializedName("addr_name")
     val addressName: String,
+    @SerializedName("latitude")
     val latitude: String,
+    @SerializedName("longitude")
     val longitude: String,
+    @SerializedName("district")
     val districtId: Long,
+    @SerializedName("postal_code")
     val postalCode: String,
+    @SerializedName("is_tokonow_request")
     val isTokonow: Boolean
 ) : GqlParam {
 
@@ -31,4 +40,4 @@ data class StateChooseAddressParam(
 data class KeroAddrSetStateChosenAddressInput(
     @SerializedName("input")
     val input: StateChooseAddressParam
-)
+) : GqlParam
