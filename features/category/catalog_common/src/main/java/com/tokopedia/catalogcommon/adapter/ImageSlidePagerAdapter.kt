a4c060cd44e65af5d3720c6e889f173c06c38b9f
edwin91 <edwin.nurwansyah@tokopedia.com>
2023-09-27 08:59:37 +0700

    bug fixing

--- features/category/catalog_common/src/main/java/com/tokopedia/catalogcommon/adapter/ImageSlidePagerAdapter.kt
+++ features/category/catalog_common/src/main/java/com/tokopedia/catalogcommon/adapter/ImageSlidePagerAdapter.kt
@@ -3,26 +3,20 @@ package com.tokopedia.catalogcommon.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.tokopedia.catalogcommon.databinding.ItemSliderTextImageBinding
 import com.tokopedia.catalogcommon.uimodel.SliderImageTextUiModel
-import com.tokopedia.kotlin.extensions.view.ONE
-import com.tokopedia.kotlin.extensions.view.ZERO
-import com.tokopedia.kotlin.extensions.view.dpToPx
+
 import com.tokopedia.kotlin.extensions.view.loadImageRounded
 
 class ImageSlidePagerAdapter(private var imageList: List<SliderImageTextUiModel.ItemSliderImageText>) :
     PagerAdapter() {
 
     companion object{
-        private const val THIRTY_PERCENT_TRANSPARANT = 0.3f
         private const val SEVENTY_FIVE_PERCENT_TRANSPARANT = 0.75f
-        private const val NOT_TRANSPARANT = 1f
 
     }
-    private val views = mutableListOf<View>()
+    private var currentPosition = -1
 
     override fun getCount(): Int {
         return imageList.size
@@ -40,6 +34,7 @@ class ImageSlidePagerAdapter(private var imageList: List<SliderImageTextUiModel.
         )
 
         imageList[position].let { item ->
+
             binding.ivImage.loadImageRounded(item.image)
             binding.tvHighlight.text = item.textHighlight
             binding.tvTitle.text = item.textTitle
@@ -48,6 +43,12 @@ class ImageSlidePagerAdapter(private var imageList: List<SliderImageTextUiModel.
             binding.tvHighlight.setTextColor(item.textHighlightColor)
             binding.tvTitle.setTextColor(item.textTitleColor)
             binding.tvDescription.setTextColor(item.textDescriptionColor)
+
+            val lineHeight = binding.tvDescription.lineHeight
+
+            val totalHeight = lineHeight * binding.tvDescription.maxLines
+
+            binding.tvDescription.layoutParams.height = totalHeight + 15
         }
         container.addView(binding.root, 0)
         return binding.root
