9aaedd39776100eccaef4d2912e940520947f556
said.faisal <said.faisal@tokopedia.com>
2023-09-27 09:50:37 +0700

    improve unit test

--- features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/productcardcolumnlist/ProductCardColumnListViewModelTest.kt
+++ features/category/discovery2/src/test/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/productcardcolumnlist/ProductCardColumnListViewModelTest.kt
@@ -3,6 +3,7 @@ package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.pro
 import android.app.Application
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.firebase.crashlytics.internal.model.CrashlyticsReport.Session.User
 import com.tokopedia.discovery.common.utils.URLParser
 import com.tokopedia.discovery2.Constant
 import com.tokopedia.discovery2.data.ComponentsItem
@@ -15,6 +16,7 @@ import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.toIntSafely
 import com.tokopedia.unit.test.ext.getOrAwaitValue
 import com.tokopedia.unit.test.ext.verifyValueEquals
+import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.every
@@ -38,6 +40,7 @@ class ProductCardColumnListViewModelTest {
     val rule = InstantTaskExecutorRule()
     private val componentsItem: ComponentsItem = mockk(relaxed = true)
     private val productCardsUseCase: ProductCardsUseCase = mockk(relaxed = true)
+    private val userSession: UserSessionInterface = mockk(relaxed = true)
     private val componentItemPosition = 99
     private val application: Application = mockk()
     private var viewModel: ProductCardColumnListViewModel =
@@ -60,99 +63,43 @@ class ProductCardColumnListViewModelTest {
     }
 
     @Test
-    fun `test 1`() {
-        val components: List<ComponentsItem> = mutableListOf(
-            ComponentsItem(
-                data = listOf(
-                    DataItem(
-                        id = "22222",
-                        name = "Product Name 1"
-                    )
-                ),
-                id = "12313"
-            ),
-            ComponentsItem(
-                data = listOf(
-                    DataItem(
-                        id = "33333",
-                        name = "Product Name 2"
-                    )
-                ),
-                id = "33122"
-            ),
-            ComponentsItem(
-                data = listOf(
-                    DataItem(
-                        id = "44444",
-                        name = "Product Name 3"
-                    )
-                ),
-                id = "42133"
-            )
-        )
-        every { componentsItem.getComponentsItem() } returns components
-
-        components.forEachIndexed { index, item ->
-            Assert.assertEquals(item.data?.firstOrNull(), viewModel.getProduct(index))
-        }
-    }
-
-    @Test
     fun `test 2`() {
-        every { componentsItem.getComponentsItem() } returns null
-        Assert.assertEquals(viewModel.getProduct(0), null)
+        every { componentsItem.getComponentItem(0) } returns null
+        Assert.assertEquals(null, viewModel.getProduct(0))
 
-        val components1: List<ComponentsItem> = mutableListOf()
-        every { componentsItem.getComponentsItem() } returns components1
-        Assert.assertEquals(viewModel.getProduct(0), null)
-
-        val components2: List<ComponentsItem> = mutableListOf(
-            ComponentsItem(
-                data = listOf(),
-                id = "12313"
-            )
+        //
+        val dataItem2 = ComponentsItem(
+            data = listOf(),
+            id = "12313"
         )
-        every { componentsItem.getComponentsItem() } returns components2
-        Assert.assertEquals(viewModel.getProduct(0), null)
+        every { componentsItem.getComponentItem(0) } returns dataItem2
+        Assert.assertEquals(null, viewModel.getProduct(0))
 
-        val components3: List<ComponentsItem> = mutableListOf(
-            ComponentsItem(
-                data = null,
-                id = "12313"
-            )
+        //
+        val dataItem = ComponentsItem(
+            data = null,
+            id = "12313"
         )
-        every { componentsItem.getComponentsItem() } returns components3
-        Assert.assertEquals(viewModel.getProduct(0), null)
-    }
+        every { componentsItem.getComponentItem(0) } returns dataItem
+        Assert.assertEquals(null, viewModel.getProduct(0))
 
-    @Test
-    fun `test for component type`() {
-        // properties has shop card comp type
-        val propertiesShopCardCompType = Properties(rows = "4")
-        every { componentsItem.properties } returns propertiesShopCardCompType
-        Assert.assertEquals(4, viewModel.getPropertyRows())
+        //
+        val dataItem4 = ComponentsItem(
+            data = listOf(
+                DataItem(
+                    id = "22222",
+                    name = "Product Name 1"
+                )
+            ),
+            id = "12313"
+        )
 
-        // properties has empty comp type
-        val propertiesEmptyCompType = Properties(rows = null)
-        every { componentsItem.properties } returns propertiesEmptyCompType
-        Assert.assertEquals(Int.ZERO, viewModel.getPropertyRows())
-
-        // properties has null comp type
-        val propertiesNullCompType = Properties(rows = String.EMPTY)
-        every { componentsItem.properties } returns propertiesNullCompType
-        Assert.assertEquals(Int.ZERO, viewModel.getPropertyRows())
-
-        // properties is null
-        every { componentsItem.properties } returns null
-        Assert.assertEquals(Int.ZERO, viewModel.getPropertyRows())
+        every { componentsItem.getComponentItem(0) } returns dataItem4
+        Assert.assertEquals(dataItem4.data?.firstOrNull(), viewModel.getProduct(0))
     }
 
     @Test
     fun `test again`() {
-        coEvery {
-            productCardsUseCase.loadFirstPageComponents(any(), any(), any())
-        } returns true
-
         val components2: List<ComponentsItem> = mutableListOf(
             ComponentsItem(
                 data = listOf(
@@ -179,7 +126,7 @@ class ProductCardColumnListViewModelTest {
         viewModel.productCardsUseCase = productCardsUseCase
 
         coEvery {
-            viewModel.productCardsUseCase?.loadFirstPageComponents(any(), any(), any())
+            productCardsUseCase.loadFirstPageComponents(any(), any(), any())
         } returns true
 
         val components2: List<ComponentsItem> = listOf()
@@ -195,11 +142,29 @@ class ProductCardColumnListViewModelTest {
     }
 
     @Test
+    fun `test again 5`() {
+        viewModel.productCardsUseCase = productCardsUseCase
+
+        coEvery {
+            productCardsUseCase.loadFirstPageComponents(any(), any(), any())
+        } returns true
+
+        every { componentsItem.getComponentsItem() } returns null
+
+        viewModel.onAttachToViewHolder()
+
+        viewModel.errorState.getOrAwaitValue()
+
+        viewModel.errorState
+            .verifyValueEquals(Unit)
+    }
+
+    @Test
     fun `test again 2`() {
         viewModel.productCardsUseCase = productCardsUseCase
 
         coEvery {
-            viewModel.productCardsUseCase?.loadFirstPageComponents(any(), any(), any())
+            productCardsUseCase.loadFirstPageComponents(any(), any(), any())
         } throws Throwable()
 
         viewModel.onAttachToViewHolder()
@@ -217,5 +182,18 @@ class ProductCardColumnListViewModelTest {
         Assert.assertEquals(componentsItem, viewModel.components)
 
         Assert.assertEquals(componentItemPosition, viewModel.position)
+    }
+
+    @Test
+    fun `login`() {
+        Assert.assertEquals(false, viewModel.isLoggedIn())
+
+        viewModel.userSession = userSession
+
+        every {
+            userSession.isLoggedIn
+        } returns true
+
+        Assert.assertEquals(true, viewModel.isLoggedIn())
     }
 }
