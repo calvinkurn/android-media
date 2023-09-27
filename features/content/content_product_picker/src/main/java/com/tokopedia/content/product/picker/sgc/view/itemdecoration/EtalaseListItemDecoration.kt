605f3a40333fbea6eca53d18107a351665035ba4
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-09-27 16:28:49 +0700

    feat: remove "play bro" keywords on dimens

--- features/content/content_product_picker/src/main/java/com/tokopedia/content/product/picker/sgc/view/itemdecoration/EtalaseListItemDecoration.kt
+++ features/content/content_product_picker/src/main/java/com/tokopedia/content/product/picker/sgc/view/itemdecoration/EtalaseListItemDecoration.kt
@@ -17,7 +17,7 @@ class EtalaseListItemDecoration(
     context: Context,
 ) : RecyclerView.ItemDecoration() {
 
-    private val dividerHeight = context.resources.getDimensionPixelOffset(R.dimen.play_bro_etalase_list_divider_height)
+    private val dividerHeight = context.resources.getDimensionPixelOffset(R.dimen.content_product_etalase_list_divider_height)
 
     private val mPaint = Paint().apply {
         color = MethodChecker.getColor(context, unifyprinciplesR.color.Unify_NN50)
