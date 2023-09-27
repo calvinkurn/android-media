8642574a0f183bcec9e814484bc5ff39c5b48b9c
faisalramd <faisal.ramdani@tokopedia.com>
2023-09-27 16:02:42 +0700

    create test for landing page viewmodel

--- features/category/catalog/src/test/java/com/tokopedia/catalog/viewmodel/CatalogLandingPageViewModelTest.kt
+++ features/category/catalog/src/test/java/com/tokopedia/catalog/viewmodel/CatalogLandingPageViewModelTest.kt
@@ -2,10 +2,13 @@ package com.tokopedia.catalog.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.catalog.ui.viewmodel.CatalogLandingPageViewModel
+import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.oldcatalog.usecase.detail.CatalogDetailUseCase
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.unit.test.ext.getOrAwaitValue
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
+import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.spyk
 import org.junit.Before
@@ -44,7 +47,33 @@ class CatalogLandingPageViewModelTest {
 
     // Testing scope
     @Test
-    fun `this is dummy test`() {
+    fun `When getProductCatalogVersion is layout 4, Then should invoke usingV4AboveLayout true`() {
+        coEvery {
+            catalogDetailUseCase.initializeGetCatalogDetail(any())
+        } returns true
         viewModel.getProductCatalogVersion("41")
+        val result = viewModel.usingV4AboveLayout.getOrAwaitValue()
+        assert(result)
+    }
+
+    @Test
+    fun `When getProductCatalogVersion is layout below 3, Then should invoke usingV4AboveLayout false`() {
+        coEvery {
+            catalogDetailUseCase.initializeGetCatalogDetail(any())
+        } returns false
+        viewModel.getProductCatalogVersion("41")
+        val result = viewModel.usingV4AboveLayout.getOrAwaitValue()
+        assert(!result)
+    }
+
+    @Test
+    fun `When getProductCatalogVersion is error, Then should invoke errorPage`() {
+        val errorMessage = "error message"
+        coEvery {
+            catalogDetailUseCase.initializeGetCatalogDetail(any())
+        } throws MessageErrorException(errorMessage)
+        viewModel.getProductCatalogVersion("41")
+        val result = viewModel.errorPage.getOrAwaitValue()
+        assert(result.message == errorMessage)
     }
 }
