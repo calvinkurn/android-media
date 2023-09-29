4f3f45d5af1f57f79ace22d6fc81cd4564476fed
fajarca <fajar.azhar@tokopedia.com>
2023-09-29 09:18:26 +0700

    Revert padding

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/fragment/ShopPageHeaderFragmentTabContentWrapper.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/pageheader/presentation/fragment/ShopPageHeaderFragmentTabContentWrapper.kt
@@ -638,7 +638,6 @@ class ShopPageHeaderFragmentTabContentWrapper :
                 }
 
                 ShopPageHeaderTabName.PRODUCT -> {
-                    removePaddingTop()
                     val shopPageProductFragment = ShopPageProductListFragment.createInstance(
                         shopId = shopId,
                         shopName = shopPageHeaderDataModel?.shopName.orEmpty(),
@@ -655,7 +654,6 @@ class ShopPageHeaderFragmentTabContentWrapper :
                 }
 
                 ShopPageHeaderTabName.SHOWCASE -> {
-                    removePaddingTop()
                     val shopShowcaseTabFragment = RouteManager.instantiateFragmentDF(
                         activity as AppCompatActivity,
                         FragmentConst.SHOP_SHOWCASE_TAB_FRAGMENT_CLASS_PATH,
@@ -681,7 +679,6 @@ class ShopPageHeaderFragmentTabContentWrapper :
                 }
 
                 ShopPageHeaderTabName.FEED -> {
-                    removePaddingTop()
                     if (isShowFeed) {
                         val feedFragment = RouteManager.instantiateFragmentDF(
                             activity as AppCompatActivity,
@@ -698,7 +695,6 @@ class ShopPageHeaderFragmentTabContentWrapper :
                 }
 
                 ShopPageHeaderTabName.REVIEW -> {
-                    removePaddingTop()
                     val reviewTabFragment = RouteManager.instantiateFragmentDF(
                         activity as AppCompatActivity,
                         SHOP_REVIEW_FRAGMENT,
@@ -710,12 +706,10 @@ class ShopPageHeaderFragmentTabContentWrapper :
                 }
 
                 ShopPageHeaderTabName.CAMPAIGN -> {
-                    removePaddingTop()
                     createCampaignTabFragment(it)
                 }
 
                 else -> {
-                    removePaddingTop()
                     null
                 }
             }
@@ -852,13 +846,5 @@ class ShopPageHeaderFragmentTabContentWrapper :
 
     fun setInitialShopLayoutData(initialShopLayoutData: HomeLayoutData?) {
         this.initialShopLayoutData = initialShopLayoutData
-    }
-    
-    private fun applyPaddingTop() {
-        viewBinding?.tabFragment?.setPadding(0, PADDING_TOP_IN_DP.toPx(), 0, 0)
-    }
-    
-    private fun removePaddingTop() {
-        viewBinding?.tabFragment?.setPadding(0, 0, 0, 0)
     }
 }
