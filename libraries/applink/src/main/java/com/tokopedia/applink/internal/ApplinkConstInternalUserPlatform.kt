f11e6e6b5b36dcabb8614709e95a86384f3b1ab2
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-09-29 13:29:46 +0700

    change name applink consent withdrawal

--- libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalUserPlatform.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/internal/ApplinkConstInternalUserPlatform.kt
@@ -351,7 +351,7 @@ object ApplinkConstInternalUserPlatform {
      * @param : [GROUP_ID] data type Int
      * */
     const val GROUP_ID = "groupId"
-    const val CONSENT_WITHDRAWAL_NEW = "$NEW_INTERNAL_USER/consent/withdrawal/new?$GROUP_ID={groupId}"
+    const val CONSENT_WITHDRAWAL = "$NEW_INTERNAL_USER/consent/withdrawal?$GROUP_ID={groupId}"
 
     /**
      * com.tokopedia.troubleshooter.notification.ui.activity.TroubleshootActivity
