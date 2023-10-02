9788d238217906ba0fdd6b93ec97e4e793fa80cb
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-10-02 16:11:48 +0700

    feat `stories creation`: add domain for stories add media

---
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/upload/domain/usecase/stories/StoriesAddMediaUseCase.kt
@@ -0,0 +1,41 @@
+package com.tokopedia.creation.common.upload.domain.usecase.stories
+
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.creation.common.upload.model.dto.stories.StoriesAddMediaRequest
+import com.tokopedia.creation.common.upload.model.dto.stories.StoriesAddMediaResponse
+import com.tokopedia.graphql.coroutines.data.extensions.request
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
+import javax.inject.Inject
+
+/**
+ * Created By : Jonathan Darwin on October 02, 2023
+ */
+class StoriesAddMediaUseCase @Inject constructor(
+    dispatchers: CoroutineDispatchers,
+    @ApplicationContext private val repository: GraphqlRepository,
+) : CoroutineUseCase<StoriesAddMediaRequest, StoriesAddMediaResponse>(dispatchers.io) {
+
+    override fun graphqlQuery(): String = QUERY
+
+    override suspend fun execute(params: StoriesAddMediaRequest): StoriesAddMediaResponse {
+        return repository.request(graphqlQuery(), params.buildRequestParam())
+    }
+
+    companion object {
+        private const val PARAM_REQ = "req"
+
+        private const val QUERY = """
+            mutation contentCreatorStoryAddMedia(
+                ${"$$PARAM_REQ"}: ContentCreatorStoryAddMediaRequest!,
+            ) {
+                contentCreatorStoryAddMedia(
+                    $PARAM_REQ: ${"$$PARAM_REQ"}
+                ) {
+                    mediaID
+                }
+            }
+        """
+    }
+}
---
+++ features/content/creation_common/src/main/java/com/tokopedia/creation/common/upload/model/dto/stories/StoriesAddMediaRequest.kt
@@ -0,0 +1,27 @@
+package com.tokopedia.creation.common.upload.model.dto.stories
+
+/**
+ * Created By : Jonathan Darwin on October 02, 2023
+ */
+data class StoriesAddMediaRequest(
+    val storyId: String,
+    val type: Int,
+    val mediaUrl: String,
+    val uploadId: String,
+    val status: Int = 1,
+    val orientation: Int = 0,
+) {
+
+    fun buildRequestParam(): Map<String, Any> {
+        return mapOf(
+            "req" to mapOf(
+                "storyID" to storyId,
+                "type" to type,
+                "mediaURL" to mediaUrl,
+                "uploadID" to uploadId,
+                "status" to status,
+                "orientation" to orientation,
+            )
+        )
+    }
+}
