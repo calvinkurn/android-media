43aa543a858bedab964e97e9a50b4a47f2e4234e
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-10-03 15:55:15 +0700

    fix: android_seller_feedback_kmp

--- libraries/applink/src/main/java/com/tokopedia/applink/sellerfeedback/DeeplinkMapperSellerFeedback.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/sellerfeedback/DeeplinkMapperSellerFeedback.kt
@@ -3,12 +3,13 @@ package com.tokopedia.applink.sellerfeedback
 import android.content.Context
 import com.tokopedia.applink.FirebaseRemoteConfigInstance
 import com.tokopedia.applink.internal.ApplinkConstInternalSellerapp
+import com.tokopedia.remoteconfig.RemoteConfigKey
 
 object DeeplinkMapperSellerFeedback {
 
     fun getSellerFeedbackInternalAppLink(context: Context): String {
         val remoteConfigImpl = FirebaseRemoteConfigInstance.get(context)
-        val isSellerFeedbackKmp = remoteConfigImpl.getBoolean(ApplinkConstInternalSellerapp.SELLER_FEEDBACK_KMP)
+        val isSellerFeedbackKmp = remoteConfigImpl.getBoolean(RemoteConfigKey.SELLER_FEEDBACK_KMP)
         return if (isSellerFeedbackKmp) {
             ApplinkConstInternalSellerapp.SELLER_FEEDBACK_KMP
         } else {
