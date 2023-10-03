46a734b552796fbd5fbf11d8751a8c9873afdb2e
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2023-10-03 15:29:49 +0700

    adjust main editor vm test and remove unused code

--- features/media/universal-editor/src/test/java/com/tokopedia/editor/faker/FakeImageFlattenRepository.kt
+++ features/media/universal-editor/src/test/java/com/tokopedia/editor/faker/FakeImageFlattenRepository.kt
@@ -2,15 +2,16 @@ package com.tokopedia.editor.faker
 
 import android.graphics.Bitmap
 import com.tokopedia.editor.data.repository.FlattenParam
+import com.tokopedia.editor.data.repository.ImageFlattenRepository
 import com.tokopedia.editor.data.repository.VideoFlattenRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
-class FakeVideoFlattenRepository : VideoFlattenRepository {
+class FakeImageFlattenRepository : ImageFlattenRepository {
 
     private val flow = MutableSharedFlow<String>()
 
     suspend fun emit(value: String) = flow.emit(value)
 
-    override fun flatten(param: FlattenParam) = flow
+    override fun flattenImage(imageBitmap: Bitmap, textBitmap: Bitmap?) = flow
 }
