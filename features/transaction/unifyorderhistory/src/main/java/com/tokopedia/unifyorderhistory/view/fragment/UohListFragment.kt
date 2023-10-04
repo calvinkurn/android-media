e3830def0eb4372822420d23183936a65e623fc5
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-10-04 14:22:17 +0700

    add validation for paymentStatus > 0

--- features/transaction/unifyorderhistory/src/main/java/com/tokopedia/unifyorderhistory/view/fragment/UohListFragment.kt
+++ features/transaction/unifyorderhistory/src/main/java/com/tokopedia/unifyorderhistory/view/fragment/UohListFragment.kt
@@ -574,7 +574,7 @@ open class UohListFragment : BaseDaggerFragment(), RefreshHandler.OnRefreshHandl
     private fun observeUohPmsCounter() {
         uohListViewModel.getUohPmsCounterResult.observe(viewLifecycleOwner) {
             if (it is Success) {
-                if (!paramUohOrder.hasActiveFilter()) {
+                if (!paramUohOrder.hasActiveFilter() && it.data.notifications.buyerOrderStatus.paymentStatus > 0) {
                     val data = UohTypeData(
                         dataObject = it.data,
                         typeLayout = UohConsts.TYPE_PMS_BUTTON
