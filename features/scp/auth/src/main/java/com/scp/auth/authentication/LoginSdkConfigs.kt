99334a67bb892f80cd1a7334f08ecd4adfdf0524
yorisprayogo <yoris.prayogo@tokopedia.com>
2023-10-06 23:58:58 +0700

    update ver

--- features/scp/auth/src/main/java/com/scp/auth/authentication/LoginSdkConfigs.kt
+++ features/scp/auth/src/main/java/com/scp/auth/authentication/LoginSdkConfigs.kt
@@ -21,6 +21,6 @@ class LoginSdkConfigs(val context: Context) : LSdkConfig {
     }
 
     override fun getAuthConfigs(): LSdkAuthConfig {
-        return LSdkAuthConfig(clientID = "tokopedia:consumer:android", clientSecret = "uPu4ieJOyPnf7sAS6ENCrBSvRMhF1g", gotoPinclientID = "uPu4ieJOyPnf7sAS6ENCrBSvRMhF1g")
+        return LSdkAuthConfig(clientID = "tokopedia:consumer:android", clientSecret = "uPu4ieJOyPnf7sAS6ENCrBSvRMhF1g", gotoPinclientID = "a8533f47-040b-49a1-b0c7-d818b9499a2f-ICP")
     }
 }
