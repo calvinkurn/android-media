af9f3e1635bb93c2a40bfdd4b2bc9976554e0f6d
hiradevina <fakhira.devina@tokopedia.com>
2023-10-06 13:48:38 +0700

    refactor: get district location from place id

---
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/pinpoint/pinpointnew/PinpointViewModel.kt
@@ -0,0 +1,328 @@
+package com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tokopedia.imageassets.TokopediaImageUrl
+import com.tokopedia.kotlin.extensions.coroutines.launchCatchError
+import com.tokopedia.kotlin.extensions.view.toDoubleOrZero
+import com.tokopedia.logisticCommon.data.entity.address.SaveAddressDataModel
+import com.tokopedia.logisticCommon.data.entity.response.KeroMapsAutofill
+import com.tokopedia.logisticCommon.data.response.KeroAddrGetDistrictCenterResponse
+import com.tokopedia.logisticCommon.domain.param.GetDistrictGeoCodeParam
+import com.tokopedia.logisticCommon.domain.param.GetDistrictParam
+import com.tokopedia.logisticCommon.domain.usecase.GetDistrictBoundariesUseCase
+import com.tokopedia.logisticCommon.domain.usecase.GetDistrictCenterUseCase
+import com.tokopedia.logisticCommon.domain.usecase.GetDistrictGeoCodeUseCase
+import com.tokopedia.logisticCommon.domain.usecase.GetDistrictUseCase
+import com.tokopedia.logisticCommon.uimodel.AddressUiState
+import com.tokopedia.logisticCommon.uimodel.isAdd
+import com.tokopedia.logisticCommon.uimodel.isEdit
+import com.tokopedia.logisticCommon.uimodel.isPinpointOnly
+import com.tokopedia.logisticaddaddress.domain.mapper.DistrictBoundaryMapper
+import com.tokopedia.logisticaddaddress.domain.mapper.GetDistrictMapper
+import com.tokopedia.logisticaddaddress.domain.mapper.SaveAddressMapper.map
+import com.tokopedia.logisticaddaddress.domain.model.mapsgeocode.MapsGeocodeParam
+import com.tokopedia.logisticaddaddress.domain.usecase.MapsGeocodeUseCase
+import com.tokopedia.logisticaddaddress.features.addnewaddressrevamp.uimodel.DistrictBoundaryResponseUiModel
+import com.tokopedia.logisticaddaddress.features.addnewaddressrevamp.uimodel.DistrictCenterUiModel
+import com.tokopedia.logisticaddaddress.features.addnewaddressrevamp.uimodel.GetDistrictDataUiModel
+import com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.PinpointNewPageFragment.Companion.BOTTOMSHEET_NOT_FOUND_LOC
+import com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.PinpointNewPageFragment.Companion.LOCATION_NOT_FOUND_MESSAGE
+import com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.uimodel.MapsGeocodeState
+import com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.uimodel.PinpointAction
+import com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.uimodel.PinpointUiModel
+import com.tokopedia.unifycomponents.UnifyButton
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Result
+import com.tokopedia.usecase.coroutines.Success
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class PinpointViewModel @Inject constructor(
+    private val getDistrict: GetDistrictUseCase,
+    private val getDistrictBoundaries: GetDistrictBoundariesUseCase,
+    private val getDistrictCenter: GetDistrictCenterUseCase,
+    private val getDistrictGeoCode: GetDistrictGeoCodeUseCase,
+    private val getDistrictMapper: GetDistrictMapper,
+    private val districtBoundaryMapper: DistrictBoundaryMapper,
+    private val mapsGeocodeUseCase: MapsGeocodeUseCase
+) : ViewModel() {
+
+    // only set this for pinpoint webview
+    private var saveAddressDataModel: SaveAddressDataModel? = null
+    var uiModel = PinpointUiModel()
+    var addressId = ""
+    var whDistrictId = 0L
+    var uiState: AddressUiState? = null
+
+    private val _autofillDistrictData = MutableLiveData<Result<KeroMapsAutofill>>()
+    val autofillDistrictData: LiveData<Result<KeroMapsAutofill>>
+        get() = _autofillDistrictData
+
+    private val _districtLocation = MutableLiveData<Result<GetDistrictDataUiModel>>()
+    val districtLocation: LiveData<Result<GetDistrictDataUiModel>>
+        get() = _districtLocation
+
+    private val _districtBoundary = MutableLiveData<Result<DistrictBoundaryResponseUiModel>>()
+    val districtBoundary: LiveData<Result<DistrictBoundaryResponseUiModel>>
+        get() = _districtBoundary
+
+    private val _districtCenter = MutableLiveData<Result<DistrictCenterUiModel>>()
+    val districtCenter: LiveData<Result<DistrictCenterUiModel>>
+        get() = _districtCenter
+
+    private val _mapsGeocodeState = MutableLiveData<MapsGeocodeState>()
+    val mapsGeocodeState: LiveData<MapsGeocodeState>
+        get() = _mapsGeocodeState
+
+    private val _action = MutableLiveData<PinpointAction>()
+    val action: LiveData<PinpointAction>
+        get() = _action
+
+    fun onViewCreated(
+        districtName: String = "",
+        cityName: String = "",
+        lat: Double = 0.0,
+        long: Double = 0.0,
+        placeId: String = "",
+        districtId: Long = 0L,
+        whDistrictId: Long = 0L,
+        addressId: String = "",
+        uiState: AddressUiState
+    ) {
+        this.uiModel = this.uiModel.copy(
+            districtName = districtName,
+            cityName = cityName,
+            lat = lat,
+            long = long,
+            placeId = placeId,
+            districtId = districtId
+        )
+        this.whDistrictId = whDistrictId
+        this.addressId = addressId
+        this.uiState = uiState
+    }
+
+    fun fetchData(addressUiState: AddressUiState) {
+        if (uiModel.placeId.isNotEmpty()) {
+            getDistrictLocation(uiModel.placeId)
+        } else if (uiModel.hasPinpoint()) {
+            getLocationFromLatLong()
+        } else if (uiModel.districtId != 0L) {
+            getDistrictCenter()
+        } else if (uiModel.hasDistrictAndCityName) {
+            getGeocodeByDistrictAndCityName()
+        } else if (addressUiState.isPinpointOnly()) {
+            _action.value = PinpointAction.GetCurrentLocation
+        }
+    }
+
+    // batas suci
+
+    fun setPinpointUiModel(
+        districtName: String = "",
+        cityName: String = "",
+        lat: Double = 0.0,
+        long: Double = 0.0,
+        placeId: String = "",
+        districtId: Long = 0L
+    ) {
+        this.uiModel = this.uiModel.copy(
+            districtName = districtName,
+            cityName = cityName,
+            lat = lat,
+            long = long,
+            placeId = placeId,
+            districtId = districtId
+        )
+    }
+
+//    fun setDistrictAndCityName(
+//        districtName: String?,
+//        cityName: String?
+//    ) {
+//        if (districtName?.isNotBlank() == true && cityName?.isNotBlank() == true) {
+//            saveAddressDataModel.districtName = districtName
+//            saveAddressDataModel.cityName = cityName
+//        }
+//    }
+
+    fun getDistrictData(lat: Double, long: Double) {
+        val param = "$lat,$long"
+        viewModelScope.launch {
+            try {
+                val districtData = getDistrictGeoCode(
+                    GetDistrictGeoCodeParam(
+                        latLng = param,
+                        isManageAddressFlow = true
+                    )
+                )
+                _autofillDistrictData.value = Success(districtData.keroMapsAutofill)
+            } catch (e: Throwable) {
+                _autofillDistrictData.value = Fail(e)
+            }
+        }
+    }
+
+    private fun locationNotFound() {
+        val invalidLocationDetail =
+            if (uiModel.hasPinpoint() || uiState.isPinpointOnly()) {
+                "Tenang, kamu bisa pilih ulang lokasimu atau pakai lokasimu sebelumnya."
+            } else {
+                "Tenang, kamu bisa pilih ulang lokasimu atau tetap menyimpan alamat tanpa pinpoint."
+            }
+        val buttonState: PinpointAction.LocationInvalid.ButtonState? = if (uiState.isPinpointOnly()) {
+            null // gone
+        } else if (uiState.isEdit()) {
+            if (uiModel.hasPinpoint()) {
+                null // gone
+            } else {
+                PinpointAction.LocationInvalid.ButtonState(
+                    variant = UnifyButton.Variant.GHOST,
+                    type = UnifyButton.Type.MAIN,
+                    text = "Mengerti"
+                )
+            }
+        } else {
+            if (uiState.isAdd()) {
+                PinpointAction.LocationInvalid.ButtonState()
+            } else {
+                null
+            }
+        }
+        _action.value = PinpointAction.LocationInvalid(
+            type = BOTTOMSHEET_NOT_FOUND_LOC,
+            title = "Yaah, lokasimu tidak terdeteksi",
+            detail = invalidLocationDetail,
+            imageUrl = TokopediaImageUrl.LOCATION_NOT_FOUND,
+            showMapIllustration = false,
+            buttonState = buttonState
+        )
+    }
+
+    fun getDistrictLocation(placeId: String) {
+        viewModelScope.launch {
+            try {
+                val districtLoc =
+                    getDistrict(GetDistrictParam(placeId = placeId, isManageAddressFlow = true))
+                val responseModel = getDistrictMapper.map(districtLoc)
+                val latitude = responseModel.latitude.toDoubleOrZero()
+                val longitude = responseModel.longitude.toDoubleOrZero()
+                if (latitude != 0.0 && longitude != 0.0) {
+                    uiModel = uiModel.copy(
+                        lat = responseModel.latitude.toDoubleOrZero(),
+                        long = responseModel.longitude.toDoubleOrZero()
+                    )
+                    _action.value = PinpointAction.MoveMap(latitude, longitude)
+                }
+                if ((responseModel.postalCode.isEmpty() && uiModel.postalCode.isEmpty()) || responseModel.districtId == 0L) {
+                    locationNotFound()
+                } else {
+                    uiModel = uiModel.map(responseModel, null)
+                    if (responseModel.errMessage.isNullOrEmpty()) {
+                        _action.value = PinpointAction.UpdatePinpointDetail(uiModel.title, uiModel.formattedAddress)
+                    } else if (responseModel.errMessage?.contains(LOCATION_NOT_FOUND_MESSAGE) == true) {
+                        locationNotFound()
+                    }
+                }
+//                _districtLocation.value = Success(getDistrictMapper.map(districtLoc))
+            } catch (e: Throwable) {
+//                _districtLocation.value = Fail(e)
+            }
+        }
+    }
+
+    fun getDistrictBoundaries() {
+        viewModelScope.launch {
+            try {
+                val districtBoundary = getDistrictBoundaries(uiModel.districtId)
+                _districtBoundary.value =
+                    Success(districtBoundaryMapper.mapDistrictBoundaryNew(districtBoundary))
+            } catch (e: Throwable) {
+                _districtBoundary.value = Fail(e)
+            }
+        }
+    }
+
+    fun getDistrictCenter() {
+        viewModelScope.launch {
+            try {
+                val data = getDistrictCenter(uiModel.districtId)
+                _districtCenter.value = Success(mapDistrictCenterResponseToUiModel(data))
+            } catch (e: Throwable) {
+                _districtCenter.value = Fail(e)
+            }
+        }
+    }
+
+    private fun mapDistrictCenterResponseToUiModel(data: KeroAddrGetDistrictCenterResponse.Data): DistrictCenterUiModel {
+        return data.keroAddrGetDistrictCenter.district.let {
+            DistrictCenterUiModel(
+                latitude = it.latitude,
+                longitude = it.longitude
+            )
+        }
+    }
+
+    fun setAddress(address: SaveAddressDataModel) {
+        this.saveAddressDataModel = address
+    }
+
+    fun getAddress(): SaveAddressDataModel {
+        return saveAddressDataModel ?: SaveAddressDataModel(
+            districtName = uiModel.districtName,
+            cityName = uiModel.cityName,
+            provinceName = uiModel.provinceName,
+            districtId = uiModel.districtId,
+            cityId = uiModel.cityId,
+            provinceId = uiModel.provinceId,
+            postalCode = uiModel.postalCode,
+            latitude = uiModel.lat.toString(),
+            longitude = uiModel.long.toString(),
+            zipCodes = uiModel.postalCodeList,
+            title = uiModel.title
+        )
+    }
+
+    fun setLatLong(lat: Double, long: Double) {
+        this.uiModel =
+            this.uiModel.copy(lat = lat, long = long)
+    }
+
+    fun getLocationFromLatLong() {
+        getDistrictData(
+            uiModel.lat,
+            uiModel.long
+        )
+    }
+
+    fun getGeocodeByDistrictAndCityName() {
+        viewModelScope.launchCatchError(
+            block = {
+                val response = mapsGeocodeUseCase(
+                    MapsGeocodeParam(
+                        input = MapsGeocodeParam.MapsGeocodeDataParam(
+                            address = "${uiModel.districtName}, ${uiModel.cityName}"
+                        )
+                    )
+                )
+
+                response.firstLocation?.apply {
+                    setLatLong(
+                        lat = lat,
+                        long = lng
+                    )
+
+                    _mapsGeocodeState.value = MapsGeocodeState.Success(this)
+                } ?: kotlin.run {
+                    _mapsGeocodeState.value = MapsGeocodeState.Fail(LOCATION_NOT_FOUND_MESSAGE)
+                }
+            },
+            onError = {
+                _mapsGeocodeState.value = MapsGeocodeState.Fail(it.message.orEmpty())
+            }
+        )
+    }
+}
---
+++ features/logistic/logisticaddaddress/src/main/java/com/tokopedia/logisticaddaddress/features/pinpoint/pinpointnew/uimodel/PinpointAction.kt
@@ -0,0 +1,26 @@
+package com.tokopedia.logisticaddaddress.features.pinpoint.pinpointnew.uimodel
+
+sealed interface PinpointAction {
+    object GetCurrentLocation : PinpointAction
+
+    data class LocationInvalid(
+        val type: Int,
+        val title: String,
+        val detail: String,
+        val imageUrl: String,
+        val showMapIllustration: Boolean,
+        val buttonState: ButtonState?
+    ) : PinpointAction {
+        data class ButtonState(
+            val variant: Int = -1,
+            val type: Int = -1,
+            val text: String = ""
+        )
+
+        val showButton = buttonState != null
+    }
+
+    data class MoveMap(val lat: Double, val long: Double) : PinpointAction
+
+    data class UpdatePinpointDetail(val title: String, val description: String) : PinpointAction
+}
