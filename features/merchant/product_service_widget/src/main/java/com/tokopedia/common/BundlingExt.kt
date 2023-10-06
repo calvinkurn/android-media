ef51c6147733956d1529c651d795f89b91bbf71c
yogieputra8 <yogie.putra@tokopedia.com>
2023-10-06 15:32:07 +0700

    disabled bundlingExt

--- features/merchant/product_service_widget/src/main/java/com/tokopedia/common/BundlingExt.kt
+++ features/merchant/product_service_widget/src/main/java/com/tokopedia/common/BundlingExt.kt
@@ -1,15 +1,11 @@
 package com.tokopedia.common
 
-import android.content.Context
-import androidx.core.content.ContextCompat
-import android.graphics.PorterDuff
 import com.tokopedia.unifycomponents.Label
-import com.tokopedia.unifycomponents.R
 
 fun Label.forceLightRed() {
-    setTextColor(ContextCompat.getColor(context, R.color.dms_static_light_RN500))
-    val drawable = ContextCompat.getDrawable(context, R.drawable.label_bg)
-    drawable?.setColorFilter(context.resources.getColor(R.color.dms_static_light_RN100), PorterDuff.Mode.SRC_ATOP)
+//    setTextColor(ContextCompat.getColor(context, R.color.dms_static_light_RN500))
+//    val drawable = ContextCompat.getDrawable(context, R.drawable.label_bg)
+//    drawable?.setColorFilter(context.resources.getColor(R.color.dms_static_light_RN100), PorterDuff.Mode.SRC_ATOP)
 
-    setBackgroundDrawable(drawable)
+//    setBackgroundDrawable(drawable)
 }
