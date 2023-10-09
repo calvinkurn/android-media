6c6e300b7451751fac275de931ca19476adb992e
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-10-09 14:05:25 +0700

    create view model test

---
+++ features/merchant/seller_persona/src/test/java/com/tokopedia/sellerpersona/view/compose/viewmodel/ComposePersonaResultViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.tokopedia.sellerpersona.view.compose.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.unit.test.rule.UnconfinedTestRule
+import io.mockk.MockKAnnotations
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+
+/**
+ * Created by @ilhamsuaib on 09/10/23.
+ */
+@OptIn(ExperimentalCoroutinesApi::class)
+class ComposePersonaResultViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineTestRule = UnconfinedTestRule()
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
+}
