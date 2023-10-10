84ba6db0fcb26e2c7f99c79c29dfd97b1ef68986
isfaaghyth <isfaaghyth@gmail.com>
2023-10-10 15:10:12 +0700

    fix: global error overlay issue in placement tool

--- features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/main/component/GlobalLoaderUiComponent.kt
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/main/component/GlobalLoaderUiComponent.kt
@@ -1,19 +1,14 @@
 package com.tokopedia.editor.ui.main.component
 
-import android.view.View
 import android.view.ViewGroup
 import com.tokopedia.editor.R
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.picker.common.basecomponent.UiComponent
-import com.tokopedia.unifycomponents.LoaderUnify
 
 class GlobalLoaderUiComponent constructor(
     parent: ViewGroup
 ) : UiComponent(parent, R.id.uc_global_loader) {
-
-    private val overlay: View = findViewById(R.id.overlay_loader)
-    private val loader: LoaderUnify = findViewById(R.id.global_loader)
 
     init {
         /**
@@ -28,12 +23,10 @@ class GlobalLoaderUiComponent constructor(
     }
 
     fun showLoading() {
-        loader.show()
-        overlay.show()
+        container().show()
     }
 
     fun hideLoading() {
-        loader.hide()
-        overlay.hide()
+        container().hide()
     }
 }
