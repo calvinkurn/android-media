f33fa882b59c080b90ec48dfa055ac5e324eb240
edwin91 <edwin.nurwansyah@tokopedia.com>
2023-10-11 09:44:39 +0700

    unit test productlist

--- features/category/catalog/src/test/java/com/tokopedia/catalog/viewmodel/CatalogProductListViewModelTest.kt
+++ features/category/catalog/src/test/java/com/tokopedia/catalog/viewmodel/CatalogProductListViewModelTest.kt
@@ -1,23 +1,52 @@
 package com.tokopedia.catalog.viewmodel
 
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.gson.JsonObject
+import com.tokopedia.atc_common.data.model.request.AddToCartRequestParams
+import com.tokopedia.atc_common.domain.model.response.AddToCartDataModel
 import com.tokopedia.atc_common.domain.usecase.coroutine.AddToCartUseCase
 import com.tokopedia.catalog.domain.GetProductListFromSearchUseCase
+import com.tokopedia.catalog.domain.model.CatalogProductItem
+import com.tokopedia.catalog.domain.model.CatalogSearchProductForReimaganeResponse
+import com.tokopedia.catalog.ui.model.CatalogProductAtcUiModel
 import com.tokopedia.catalog.ui.viewmodel.CatalogProductListViewModel
+import com.tokopedia.common_category.model.filter.FilterResponse
+import com.tokopedia.filter.common.data.DynamicFilterModel
+import com.tokopedia.graphql.CommonUtils
+import com.tokopedia.graphql.GraphqlConstant
+import com.tokopedia.graphql.data.model.GraphqlError
+import com.tokopedia.graphql.data.model.GraphqlResponse
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.oldcatalog.CatalogTestUtils
+import com.tokopedia.oldcatalog.model.raw.SearchFilterResponse
 import com.tokopedia.oldcatalog.usecase.listing.CatalogDynamicFilterUseCase
 import com.tokopedia.oldcatalog.usecase.listing.CatalogQuickFilterUseCase
+import com.tokopedia.oldcatalog.viewmodel.CatalogProductListingViewModelTest
+import com.tokopedia.searchbar.navigation_component.datamodel.TopNavNotificationModel
 import com.tokopedia.searchbar.navigation_component.domain.GetNotificationUseCase
 import com.tokopedia.unit.test.dispatcher.CoroutineTestDispatchersProvider
+import com.tokopedia.usecase.RequestParams
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.spyk
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.AfterEach
-
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import rx.Subscriber
+import java.lang.reflect.Type
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P])
 class CatalogProductListViewModelTest {
 
     // Initialization scope
@@ -26,14 +55,19 @@ class CatalogProductListViewModelTest {
 
     @RelaxedMockK
     lateinit var quickFilterUseCase: CatalogQuickFilterUseCase
+
     @RelaxedMockK
     lateinit var dynamicFilterUseCase: CatalogDynamicFilterUseCase
+
     @RelaxedMockK
     lateinit var addToCartUseCase: AddToCartUseCase
+
     @RelaxedMockK
     lateinit var getNotificationUseCase: GetNotificationUseCase
+
     @RelaxedMockK
     lateinit var getProductListUseCase: GetProductListFromSearchUseCase
+
     @RelaxedMockK
     lateinit var userSession: UserSessionInterface
 
@@ -64,7 +98,219 @@ class CatalogProductListViewModelTest {
 
     // Testing scope
     @Test
-    fun `this is dummy test`() {
+    fun `Get Catalog Product Quick Filter Response Success`() {
+        val mockGqlResponse: GraphqlResponse =
+            CatalogProductListingViewModelTest.createMockGraphqlResponse(
+                getJsonObject("catalog_product_quick_filter_response.json"),
+                SearchFilterResponse().javaClass
+            )
+        val data = mockGqlResponse.getData(SearchFilterResponse::class.java) as SearchFilterResponse
+        every { quickFilterUseCase.execute(any(), any()) }.answers {
+            (secondArg() as Subscriber<DynamicFilterModel>).onNext(data.dynamicAttribute)
+            (secondArg() as Subscriber<DynamicFilterModel>).onCompleted()
+        }
+        viewModel.fetchQuickFilters(RequestParams())
+        if (viewModel.quickFilterResult.value is Success) {
+            assert(true)
+        } else {
+            assert(false)
+        }
+    }
 
+    @Test
+    fun `Get Catalog Product Quick Filter Response Fail`() {
+        every { quickFilterUseCase.execute(any(), any()) }.answers {
+            (secondArg() as Subscriber<DynamicFilterModel>).onError(Throwable("No Data"))
+        }
+        viewModel.fetchQuickFilters(RequestParams())
+
+        if (viewModel.quickFilterResult.value is Fail) {
+            assert(true)
+        } else {
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Get Catalog Product Dynamic Filter Response Success`() {
+        val mockGqlResponse: GraphqlResponse =
+            CatalogProductListingViewModelTest.createMockGraphqlResponse(
+                getJsonObject("catalog_product_dynamic_attribute_response.json"),
+                FilterResponse().javaClass
+            )
+        val data = mockGqlResponse.getData(FilterResponse::class.java) as FilterResponse
+
+        every { dynamicFilterUseCase.execute(any(), any()) }.answers {
+            (secondArg() as Subscriber<DynamicFilterModel>).onNext(data.dynamicAttribute)
+            (secondArg() as Subscriber<DynamicFilterModel>).onCompleted()
+        }
+        viewModel.fetchDynamicAttribute(RequestParams())
+
+        if (viewModel.dynamicFilterResult.value is Success) {
+            assert(true)
+        } else {
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Get Catalog Product Dynamic Filter Response Fail`() {
+        every { dynamicFilterUseCase.execute(any(), any()) }.answers {
+            (secondArg() as Subscriber<DynamicFilterModel>).onError(Throwable("No Data"))
+        }
+        viewModel.fetchDynamicAttribute(RequestParams())
+
+        if (viewModel.mDynamicFilterModel.value is Fail) {
+            assert(true)
+        } else {
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Get Catalog Product Response Success`() {
+        val mockGqlResponse: GraphqlResponse =
+            createMockGraphqlResponse(
+                getJsonObject("catalog_product_listing_response.json"),
+                CatalogSearchProductForReimaganeResponse().javaClass
+            )
+        val data = mockGqlResponse.getData(CatalogSearchProductForReimaganeResponse::class.java) as CatalogSearchProductForReimaganeResponse
+
+        val param = RequestParams()
+        coEvery {
+            getProductListUseCase.execute(param)
+        } returns data
+
+        viewModel.fetchProductListing(param)
+        assert(viewModel.productList.value is Success)
+    }
+
+    //
+    @Test
+    fun `Get Catalog Product Response Empty`() {
+        val param = RequestParams()
+        val mockGqlResponse: GraphqlResponse =
+            createMockGraphqlResponse(
+                getJsonObject("catalog_empty_dummy_response.json"),
+                CatalogSearchProductForReimaganeResponse().javaClass
+            )
+        val data =
+            mockGqlResponse.getData(CatalogSearchProductForReimaganeResponse::class.java) as CatalogSearchProductForReimaganeResponse
+        coEvery {
+            getProductListUseCase.execute(param)
+        } returns data
+
+        viewModel.fetchProductListing(param)
+        if (viewModel.productList.value is Success && (viewModel.productList.value as Success<List<CatalogProductItem>>).data.isEmpty()) {
+            assert(true)
+        } else {
+            assert(false)
+        }
+    }
+
+    @Test
+    fun `Get Catalog Product Response Fail`() {
+        val param = RequestParams()
+        coEvery {
+            getProductListUseCase.execute(param)
+        } throws Throwable()
+
+        viewModel.fetchProductListing(param)
+        if (viewModel.productList.value is Fail) { assert(true) } else { assert(false) }
+    }
+
+    @Test
+    fun `Add To Cart Response Success`() {
+        val paramViewModel = CatalogProductAtcUiModel()
+        val paramUseCase = AddToCartRequestParams(
+            productId = paramViewModel.productId,
+            shopId = paramViewModel.shopId,
+            quantity = paramViewModel.quantity,
+            warehouseId = paramViewModel.warehouseId
+        )
+        coEvery {
+            addToCartUseCase.setParams(paramUseCase)
+            addToCartUseCase.executeOnBackground()
+        } returns AddToCartDataModel(status = "OK")
+
+        viewModel.addProductToCart(paramViewModel)
+
+        assert(viewModel.errorsToaster.value is MessageErrorException)
+    }
+
+    @Test
+    fun `Add To Cart Response Fail`() {
+        val paramViewModel = CatalogProductAtcUiModel()
+        val paramUseCase = AddToCartRequestParams(
+            productId = paramViewModel.productId,
+            shopId = paramViewModel.shopId,
+            quantity = paramViewModel.quantity,
+            warehouseId = paramViewModel.warehouseId
+        )
+        val response = AddToCartDataModel(status = "")
+        coEvery {
+            addToCartUseCase.setParams(paramUseCase)
+            addToCartUseCase.executeOnBackground()
+        } returns response
+
+        viewModel.addProductToCart(paramViewModel)
+
+        assert(viewModel.textToaster.value == response.getAtcErrorMessage())
+    }
+
+    @Test
+    fun `Refresh Notification Success`() {
+        val response = TopNavNotificationModel(totalCart = 2)
+        coEvery {
+            getNotificationUseCase.executeOnBackground()
+        } returns response
+
+        viewModel.refreshNotification()
+
+        assert(viewModel.totalCartItem.value == response.totalCart)
+    }
+
+    @Test
+    fun `Refresh Notification Fail`() {
+        val error = Throwable("Error")
+        val response = TopNavNotificationModel(totalCart = 2)
+        coEvery {
+            getNotificationUseCase.executeOnBackground()
+        } throws error
+
+        viewModel.refreshNotification()
+
+        assert(viewModel.errorsToaster.value == error)
+    }
+
+    @Test
+    fun `User Not Login`() {
+        val emptyUserId = ""
+        userSession.userId = emptyUserId
+        viewModel.getUserId()
+        viewModel.isUserLoggedIn()
+        assert(!viewModel.isUserLoggedIn())
+    }
+
+    companion object {
+        fun createMockGraphqlResponse(
+            response: JsonObject,
+            objectType: Class<Any>
+        ): GraphqlResponse {
+            val result = HashMap<Type, Any>()
+            val errors = HashMap<Type, List<GraphqlError>>()
+            val jsonObject: JsonObject = response
+            val data = jsonObject.get(GraphqlConstant.GqlApiKeys.DATA)
+            val obj: Any = CommonUtils.fromJson(data, objectType)
+            result[objectType] = obj
+            return GraphqlResponse(result, errors, false)
+        }
+
+        private fun getJsonObject(pathString: String): JsonObject {
+            return CommonUtils.fromJson(
+                CatalogTestUtils.getJsonFromFile(pathString),
+                JsonObject::class.java
+            )
+        }
     }
 }
