8930c6b4235a4871a8c5ac225760916bc53e2add
victor <victor@tokopedia.com>
2023-10-12 08:53:06 +0700

    fix ut

--- features/transaction/cart/src/test/java/com/tokopedia/cart/view/viewmodel/UpdatePromoSummaryDataTest.kt
+++ features/transaction/cart/src/test/java/com/tokopedia/cart/view/viewmodel/UpdatePromoSummaryDataTest.kt
@@ -1,12 +1,5 @@
 package com.tokopedia.cart.view.viewmodel
 
-import com.tokopedia.cart.data.model.response.promo.CartPromoData
-import com.tokopedia.cart.data.model.response.promo.LastApplyPromo
-import com.tokopedia.cart.data.model.response.promo.LastApplyPromoData
-import com.tokopedia.cart.data.model.response.promo.PromoAdditionalInfo
-import com.tokopedia.cart.data.model.response.promo.VoucherOrders
-import com.tokopedia.cart.data.model.response.shopgroupsimplified.CartData
-import com.tokopedia.purchase_platform.common.feature.promo.domain.model.UsageSummaries
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyAdditionalInfoUiModel
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUiModel
 import com.tokopedia.purchase_platform.common.feature.promo.view.model.lastapply.LastApplyUsageSummariesUiModel
@@ -21,32 +14,16 @@ class UpdatePromoSummaryDataTest : BaseCartViewModelTest() {
         val lastApplyUiModel = LastApplyUiModel(
             additionalInfo = LastApplyAdditionalInfoUiModel(
                 usageSummaries = listOf(
-                    LastApplyUsageSummariesUiModel()
+                    LastApplyUsageSummariesUiModel(type = "total_product_price", amount = 10000),
+                    LastApplyUsageSummariesUiModel(type = "", amount = 5000)
                 )
             )
         )
-        val cartData = CartData(
-            promo = CartPromoData(
-                lastApplyPromo = LastApplyPromo(
-                    lastApplyPromoData = LastApplyPromoData(
-                        codes = listOf("ABC"),
-                        listVoucherOrders = listOf(VoucherOrders()),
-                        additionalInfo = PromoAdditionalInfo(
-                            usageSummaries = listOf(
-                                UsageSummaries(type = "total_product_price", amount = 10000),
-                                UsageSummaries(type = "", amount = 5000)
-                            )
-                        )
-                    )
-                )
-            )
-        )
-        cartViewModel.cartModel.cartListData = cartData
 
         // WHEN
         cartViewModel.updatePromoSummaryData(lastApplyUiModel)
 
         // THEN
-        assertEquals(5000, cartViewModel.cartModel.discountAmount)
+        assertEquals(10000, cartViewModel.cartModel.discountAmount)
     }
 }
