42d70502d51051a44e98b0fb43cbb77b59551159
said.faisal <said.faisal@tokopedia.com>
2023-10-13 10:42:45 +0700

    adjust plt monitoring add attribution

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/searchcategory/analytics/SearchCategoryPageLoadTimeMonitoring.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/searchcategory/analytics/SearchCategoryPageLoadTimeMonitoring.kt
@@ -4,36 +4,35 @@ import com.tokopedia.analytics.performance.util.PageLoadTimePerformanceCallback
 import com.tokopedia.analytics.performance.util.PageLoadTimePerformanceInterface
 import com.tokopedia.remoteconfig.RemoteConfigInstance
 import com.tokopedia.remoteconfig.RollenceKey
+import com.tokopedia.tokopedianow.common.constant.ConstantKey.DEFAULT_ROWS
 import com.tokopedia.tokopedianow.common.constant.ConstantKey.EXPERIMENT_DISABLED
 import com.tokopedia.tokopedianow.common.constant.ConstantKey.EXPERIMENT_ENABLED
+import com.tokopedia.tokopedianow.common.constant.ConstantKey.EXPERIMENT_ROWS
 
 class SearchCategoryPageLoadTimeMonitoring(
 ) {
     companion object {
-        private const val TOKONOW_SEARCH_DEFAULT_PLT_TRACE = "mp_tokonow_search_default"
-        private const val TOKONOW_SEARCH_EXPERIMENT_PLT_TRACE = "mp_tokonow_search_experiment"
+        private const val TOKONOW_ATTRIBUTION = "itemCount"
+        private const val TOKONOW_SEARCH_PLT_TRACE = "mp_tokonow_search"
+        private const val TOKONOW_CATEGORY_PLT_TRACE = "mp_tokonow_category"
 
-        private const val TOKONOW_SEARCH_DEFAULT_PLT_PREPARE_METRICS = "tokonow_search_default_plt_prepare_metrics"
-        private const val TOKONOW_SEARCH_DEFAULT_PLT_NETWORK_METRICS = "tokonow_search_default_plt_network_metrics"
-        private const val TOKONOW_SEARCH_DEFAULT_PLT_RENDER_METRICS = "tokonow_search_default_plt_render_metrics"
+        private const val TOKONOW_SEARCH_PLT_PREPARE_METRICS = "tokonow_search_plt_prepare_metrics"
+        private const val TOKONOW_SEARCH_PLT_NETWORK_METRICS = "tokonow_search_plt_network_metrics"
+        private const val TOKONOW_SEARCH_PLT_RENDER_METRICS = "tokonow_search_plt_render_metrics"
 
-        private const val TOKONOW_SEARCH_EXPERIMENT_PLT_PREPARE_METRICS = "tokonow_search_experiment_plt_prepare_metrics"
-        private const val TOKONOW_SEARCH_EXPERIMENT_PLT_NETWORK_METRICS = "tokonow_search_experiment_plt_network_metrics"
-        private const val TOKONOW_SEARCH_EXPERIMENT_PLT_RENDER_METRICS = "tokonow_search_experiment_plt_render_metrics"
-
-        private const val TOKONOW_CATEGORY_DEFAULT_PLT_TRACE = "mp_tokonow_category_default"
-        private const val TOKONOW_CATEGORY_EXPERIMENT_PLT_TRACE = "mp_tokonow_category_experiment"
-
-        private const val TOKONOW_CATEGORY_DEFAULT_PLT_PREPARE_METRICS = "tokonow_category_default_plt_prepare_metrics"
-        private const val TOKONOW_CATEGORY_DEFAULT_PLT_NETWORK_METRICS = "tokonow_category_default_plt_network_metrics"
-        private const val TOKONOW_CATEGORY_DEFAULT_PLT_RENDER_METRICS = "tokonow_category_default_plt_render_metrics"
-
-        private const val TOKONOW_CATEGORY_EXPERIMENT_PLT_PREPARE_METRICS = "tokonow_category_experiment_plt_prepare_metrics"
-        private const val TOKONOW_CATEGORY_EXPERIMENT_PLT_NETWORK_METRICS = "tokonow_category_experiment_plt_network_metrics"
-        private const val TOKONOW_CATEGORY_EXPERIMENT_PLT_RENDER_METRICS = "tokonow_category_experiment_plt_render_metrics"
+        private const val TOKONOW_CATEGORY_PLT_PREPARE_METRICS = "tokonow_category_plt_prepare_metrics"
+        private const val TOKONOW_CATEGORY_PLT_NETWORK_METRICS = "tokonow_category_plt_network_metrics"
+        private const val TOKONOW_CATEGORY_PLT_RENDER_METRICS = "tokonow_category_plt_render_metrics"
     }
 
     private var pltPerformanceMonitoring: PageLoadTimePerformanceInterface? = null
+
+    private fun getPaginationExperiment(): Boolean {
+        val experiment = RemoteConfigInstance.getInstance().abTestPlatform.getString(RollenceKey.TOKOPEDIA_NOW_PAGINATION, EXPERIMENT_DISABLED)
+        return experiment == EXPERIMENT_ENABLED
+    }
+
+    private fun getRows(): String = if (getPaginationExperiment()) EXPERIMENT_ROWS else DEFAULT_ROWS
 
     private fun setPeformanceMonitoring(
         tagPrepare: String,
@@ -52,39 +51,20 @@ class SearchCategoryPageLoadTimeMonitoring(
     fun initPerformanceMonitoring(
         isCategoryPage: Boolean
     ) {
-        val experiment = RemoteConfigInstance.getInstance().abTestPlatform.getString(RollenceKey.TOKOPEDIA_NOW_PAGINATION, EXPERIMENT_DISABLED)
-        if (experiment == EXPERIMENT_ENABLED) {
-            if (isCategoryPage) {
-                setPeformanceMonitoring(
-                    tagPrepare = TOKONOW_CATEGORY_EXPERIMENT_PLT_PREPARE_METRICS,
-                    tagNetwork = TOKONOW_CATEGORY_EXPERIMENT_PLT_NETWORK_METRICS,
-                    tagRender = TOKONOW_CATEGORY_EXPERIMENT_PLT_RENDER_METRICS,
-                    traceName = TOKONOW_CATEGORY_EXPERIMENT_PLT_TRACE
-                )
-            } else {
-                setPeformanceMonitoring(
-                    tagPrepare = TOKONOW_SEARCH_EXPERIMENT_PLT_PREPARE_METRICS,
-                    tagNetwork = TOKONOW_SEARCH_EXPERIMENT_PLT_NETWORK_METRICS,
-                    tagRender = TOKONOW_SEARCH_EXPERIMENT_PLT_RENDER_METRICS,
-                    traceName = TOKONOW_SEARCH_EXPERIMENT_PLT_TRACE
-                )
-            }
+        if (isCategoryPage) {
+            setPeformanceMonitoring(
+                tagPrepare = TOKONOW_CATEGORY_PLT_PREPARE_METRICS,
+                tagNetwork = TOKONOW_CATEGORY_PLT_NETWORK_METRICS,
+                tagRender = TOKONOW_CATEGORY_PLT_RENDER_METRICS,
+                traceName = TOKONOW_CATEGORY_PLT_TRACE
+            )
         } else {
-            if (isCategoryPage) {
-                setPeformanceMonitoring(
-                    tagPrepare = TOKONOW_CATEGORY_DEFAULT_PLT_PREPARE_METRICS,
-                    tagNetwork = TOKONOW_CATEGORY_DEFAULT_PLT_NETWORK_METRICS,
-                    tagRender = TOKONOW_CATEGORY_DEFAULT_PLT_RENDER_METRICS,
-                    traceName = TOKONOW_CATEGORY_DEFAULT_PLT_TRACE
-                )
-            } else {
-                setPeformanceMonitoring(
-                    tagPrepare = TOKONOW_SEARCH_DEFAULT_PLT_PREPARE_METRICS,
-                    tagNetwork = TOKONOW_SEARCH_DEFAULT_PLT_NETWORK_METRICS,
-                    tagRender = TOKONOW_SEARCH_DEFAULT_PLT_RENDER_METRICS,
-                    traceName = TOKONOW_SEARCH_DEFAULT_PLT_TRACE
-                )
-            }
+            setPeformanceMonitoring(
+                tagPrepare = TOKONOW_SEARCH_PLT_PREPARE_METRICS,
+                tagNetwork = TOKONOW_SEARCH_PLT_NETWORK_METRICS,
+                tagRender = TOKONOW_SEARCH_PLT_RENDER_METRICS,
+                traceName = TOKONOW_SEARCH_PLT_TRACE
+            )
         }
         pltPerformanceMonitoring?.startPreparePagePerformanceMonitoring()
     }
@@ -100,6 +80,7 @@ class SearchCategoryPageLoadTimeMonitoring(
     }
 
     fun stopRenderPerformanceMonitoring() {
+        pltPerformanceMonitoring?.addAttribution(TOKONOW_ATTRIBUTION, getRows())
         pltPerformanceMonitoring?.stopRenderPerformanceMonitoring()
         pltPerformanceMonitoring?.stopMonitoring()
     }
