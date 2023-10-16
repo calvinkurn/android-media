d74dcc81256ce420a2a589121a7eb1ed1a5d7cf3
edwin91 <edwin.nurwansyah@tokopedia.com>
2023-10-16 14:13:45 +0700

    implement comparison detail

--- features/category/catalog/src/main/java/com/tokopedia/catalog/ui/adapter/CatalogComparisonDetailAdapterFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.tokopedia.catalog.ui.adapter
-
-import com.tokopedia.catalogcommon.uimodel.BlankUiModel
-
-interface CatalogComparisonDetailAdapterFactory {
-    fun type(uiModel: BlankUiModel): Int
-}
--- features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogComparisonDetailFragment.kt
+++ features/category/catalog/src/main/java/com/tokopedia/catalog/ui/fragment/CatalogComparisonDetailFragment.kt
@@ -4,39 +4,148 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
+import com.tokopedia.abstraction.common.utils.view.MethodChecker
 import com.tokopedia.catalog.R
 import com.tokopedia.catalog.databinding.FragmentCatalogComparisonDetailBinding
 import com.tokopedia.catalog.di.DaggerCatalogComponent
+import com.tokopedia.catalog.ui.viewmodel.CatalogDetailPageViewModel
+import com.tokopedia.catalogcommon.adapter.CatalogAdapterFactoryImpl
+import com.tokopedia.catalogcommon.adapter.WidgetCatalogAdapter
+import com.tokopedia.catalogcommon.uimodel.ComparisonUiModel
+import com.tokopedia.catalogcommon.viewholder.ComparisonViewHolder
 import com.tokopedia.kotlin.extensions.view.gone
+import com.tokopedia.kotlin.extensions.view.hide
+import com.tokopedia.kotlin.extensions.view.show
+import com.tokopedia.oldcatalog.listener.CatalogDetailListener
+import com.tokopedia.oldcatalog.ui.bottomsheet.CatalogComponentBottomSheet
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
 import com.tokopedia.utils.lifecycle.autoClearedNullable
+import javax.inject.Inject
+import com.tokopedia.unifyprinciples.R as unifyprinciplesR
 
-class CatalogComparisonDetailFragment : BaseDaggerFragment() {
+class CatalogComparisonDetailFragment : BaseDaggerFragment(),
+    ComparisonViewHolder.ComparisonItemListener, CatalogDetailListener {
+
+
+    companion object {
+        const val CATALOG_COMPARISON_DETAIL_FRAGMENT_TAG = "CATALOG_COMPARISON_DETAIL_FRAGMENT_TAG"
+        const val ARG_PARAM_CATALOG_ID = "catalogId"
+        const val ARG_PARAM_CATEGORY_ID = "categoryId"
+        const val ARG_PARAM_COMPARE_CATALOG_ID = "compareCatalogId"
+
+        fun newInstance(
+            catalogId: String,
+            categoryId: String,
+            compareCatalogId: String
+        ): CatalogComparisonDetailFragment {
+            val fragment = CatalogComparisonDetailFragment()
+            val bundle = Bundle()
+            bundle.putString(ARG_PARAM_CATALOG_ID, catalogId)
+            bundle.putString(ARG_PARAM_CATEGORY_ID, categoryId)
+            bundle.putString(ARG_PARAM_COMPARE_CATALOG_ID, compareCatalogId)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    @Inject
+    lateinit var viewModel: CatalogDetailPageViewModel
 
     private var binding by autoClearedNullable<FragmentCatalogComparisonDetailBinding>()
 
+    var catalogId = ""
+    var categoryId = ""
+    var compareCatalogId = ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var catalogAllReviewBottomSheet: CatalogComponentBottomSheet? = null
+
+    private val widgetAdapter by lazy {
+        WidgetCatalogAdapter(
+            CatalogAdapterFactoryImpl(isDisplayingTopSpec = false, comparisonItemListener = this)
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+        setupContent()
+        setupObserver()
+        if (arguments != null) {
+            catalogId = requireArguments().getString(ARG_PARAM_CATALOG_ID, "")
+            categoryId = requireArguments().getString(ARG_PARAM_CATEGORY_ID, "")
+            compareCatalogId = requireArguments().getString(ARG_PARAM_COMPARE_CATALOG_ID, "")
+            getComparison(catalogId, compareCatalogId)
+            catalogAllReviewBottomSheet = CatalogComponentBottomSheet.newInstance(
+                "",
+                catalogId,
+                "",
+                categoryId,
+                "",
+                CatalogComponentBottomSheet.ORIGIN_ULTIMATE_VERSION,
+                this
+            )
+        }
+
 
     }
 
-    private fun setupToolbar(){
-        binding?.toolbar?.cartButton?.gone()
-        binding?.toolbar?.searchButton?.gone()
+    private fun getComparison(catalogId: String, compareCatalogId: String) {
+        binding?.loadingLayout?.root?.show()
+        viewModel.getProductCatalog(catalogId, compareCatalogId)
+    }
+
+    private fun setupObserver() {
+        viewModel.catalogDetailDataModel.observe(viewLifecycleOwner) {
+            if (it is Success) {
+
+                val comparison = it.data.widgets.find {
+                    it is ComparisonUiModel
+                }
+                if (comparison != null) {
+                    widgetAdapter.addWidget(listOf(comparison))
+                }
+
+            } else if (it is Fail) {
+            }
+
+            binding?.loadingLayout?.root?.gone()
+        }
+    }
+
+    private fun setupContent() {
+        val layoutManager = LinearLayoutManager(context)
+        binding?.rvContent?.layoutManager = layoutManager
+        binding?.rvContent?.adapter = widgetAdapter
+    }
+
+    private fun setupToolbar() {
+        binding?.toolbar?.cartButton?.hide()
+        binding?.toolbar?.searchButton?.hide()
+        context?.let {
+            binding?.toolbar?.apply {
+                title = getString(R.string.text_comparison_title)
+                setColors(MethodChecker.getColor(context, unifyprinciplesR.color.Unify_NN950))
+                setNavigationOnClickListener {
+                    activity?.finish()
+                }
+            }
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog_comparison_detail, container, false)
+        binding = FragmentCatalogComparisonDetailBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
-    override fun getScreenName() = CatalogComparisonDetailFragment::class.java.canonicalName.orEmpty()
+    override fun getScreenName() =
+        CatalogComparisonDetailFragment::class.java.canonicalName.orEmpty()
 
 
     override fun initInjector() {
@@ -46,17 +155,17 @@ class CatalogComparisonDetailFragment : BaseDaggerFragment() {
             .inject(this)
     }
 
-    companion object {
-        const val CATALOG_COMPARISON_DETAIL_FRAGMENT_TAG = "CATALOG_COMPARISON_DETAIL_FRAGMENT_TAG"
-        private const val ARG_PARAM_CATALOG_ID = "catalogId"
-        private const val ARG_PARAM2 = "param2"
-        @JvmStatic
-        fun newInstance(catalogId: String = "", param2: String= "") =
-            CatalogComparisonDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM_CATALOG_ID, catalogId)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onComparisonSwitchButtonClicked(position: Int) {
+        catalogAllReviewBottomSheet?.show(childFragmentManager, "")
     }
+
+    override fun onComparisonSeeMoreButtonClicked() {
+        //no-op
+    }
+
+    override fun changeComparison(comparedCatalogId: String) {
+        getComparison(catalogId, "")
+
+    }
+
 }
