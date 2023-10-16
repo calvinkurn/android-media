1688d95a4d905a8e2b687fb29e25ea1cb8a58bfe
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2023-10-16 14:37:59 +0700

    add conditional for spike draw when line is empty

--- features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/widget/RoundedSpan.kt
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/ui/widget/RoundedSpan.kt
@@ -1,10 +1,12 @@
 package com.tokopedia.editor.ui.widget
 
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
 import android.text.style.LineBackgroundSpan
+import android.util.Log
 import com.tokopedia.unifycomponents.toPx
 import kotlin.math.sign
 import kotlin.math.abs
@@ -24,6 +26,8 @@ class RoundedSpan(
     private var prevRight = -1f
     private var prevBottom = -1f
     private var prevTop = -1f
+
+    private val listOfEmptyLine = mutableMapOf<Int, String>()
 
     init {
         paint.color = backgroundColor
@@ -71,32 +75,52 @@ class RoundedSpan(
             }
         }
 
-        rect.set(shiftLeft, top.toFloat() - 5f, shiftRight, bottom.toFloat() + 8f)
+        rect.set(shiftLeft, top.toFloat() - ADDITIONAL_TOP_GAP, shiftRight, bottom.toFloat() + ADDITIONAL_BOTTOM_GAP)
 
 
         if (lnum == 0) {
             c.drawRoundRect(rect, radius.toFloat(), radius.toFloat(), paint)
         } else {
             path.reset()
+
+            // check if line is empty only contain \n then skip background
+            val textOnLine = text.substring(start, end)
+            if (textOnLine.replace("\n", "").isEmpty()) {
+                prevWidth = 0f
+                prevLeft = rect.left
+                prevRight = rect.right
+                prevBottom = rect.bottom
+                prevTop = rect.top
+
+                listOfEmptyLine[lnum] = "-"
+
+                return
+            }
+
+            // check if draw need to skip spike when prev line is empty (only contain new line)
+            val isSkipSpike = listOfEmptyLine[lnum - 1] != null
+
             val difference = width - prevWidth
             val diff = -sign(difference) * (2f * radius).coerceAtMost(abs(difference / 2f)) / 2f
             path.moveTo(
-                prevLeft, prevBottom - radius
+                prevLeft, prevBottom - (if (isSkipSpike) 0 else padding) - ADDITIONAL_TOP_GAP
             )
 
             if (align != ALIGN_LEFT) {
-                path.cubicTo(//1
-                    prevLeft, prevBottom - radius,
-                    prevLeft, rect.top,
-                    prevLeft + diff, rect.top
-                )
+                if (!isSkipSpike) {
+                    path.cubicTo(//1
+                        prevLeft, prevBottom - radius,
+                        prevLeft, rect.top,
+                        prevLeft + diff, rect.top
+                    )
+                }
             } else {
                 path.lineTo(prevLeft, prevBottom + radius)
             }
             path.lineTo(
                 rect.left - diff, rect.top
             )
-            path.cubicTo(//2
+            path.cubicTo(//2 rounded kiri atas
                 rect.left - diff, rect.top,
                 rect.left, rect.top,
                 rect.left, rect.top + radius
@@ -104,7 +128,7 @@ class RoundedSpan(
             path.lineTo(
                 rect.left, rect.bottom - radius
             )
-            path.cubicTo(//3
+            path.cubicTo(//3 rounded kiri bawah
                 rect.left, rect.bottom - radius,
                 rect.left, rect.bottom,
                 rect.left + radius, rect.bottom
@@ -112,7 +136,7 @@ class RoundedSpan(
             path.lineTo(
                 rect.right - radius, rect.bottom
             )
-            path.cubicTo(//4
+            path.cubicTo(//4 rounded kanan bawah
                 rect.right - radius, rect.bottom,
                 rect.right, rect.bottom,
                 rect.right, rect.bottom - radius
@@ -122,7 +146,7 @@ class RoundedSpan(
             )
 
             if (align != ALIGN_RIGHT) {
-                path.cubicTo(//5
+                path.cubicTo(//5 rounded kanan atas
                     rect.right, rect.top + radius,
                     rect.right, rect.top,
                     rect.right + diff, rect.top
@@ -130,33 +154,42 @@ class RoundedSpan(
                 path.lineTo(
                     prevRight - diff, rect.top
                 )
-                path.cubicTo(//6
-                    prevRight - diff, rect.top,
-                    prevRight, rect.top,
-                    prevRight, prevBottom - radius
-                )
-
+                if (!isSkipSpike) {
+                    path.cubicTo(//6
+                        prevRight - diff, rect.top,
+                        prevRight, rect.top,
+                        prevRight, prevBottom - radius
+                    )
+                }
             } else {
                 path.lineTo(prevRight, prevBottom - radius)
             }
-            path.cubicTo(//7
-                prevRight, prevBottom - radius,
-                prevRight, prevBottom,
-                prevRight - radius, prevBottom
-            )
+
+            if (!isSkipSpike) {
+                path.cubicTo(//7
+                    prevRight, prevBottom - radius,
+                    prevRight, prevBottom,
+                    prevRight - radius, prevBottom
+                )
+            }
 
             path.lineTo(
                 prevLeft + radius, prevBottom
             )
 
-            path.cubicTo(//8
-                prevLeft + radius, prevBottom,
-                prevLeft, prevBottom,
-                prevLeft, rect.top - radius
-            )
+            if (!isSkipSpike) {
+                path.cubicTo(//8
+                    prevLeft + radius, prevBottom,
+                    prevLeft, prevBottom,
+                    prevLeft, rect.top - radius
+                )
+            }
+
             c.drawPath(path, paintStroke)
 
         }
+
+
         prevWidth = width
         prevLeft = rect.left
         prevRight = rect.right
@@ -171,5 +204,8 @@ class RoundedSpan(
 
         private const val DEFAULT_PADDING = 8
         private const val DEFAULT_CORNER = 8
+
+        private const val ADDITIONAL_TOP_GAP = 5f
+        private const val ADDITIONAL_BOTTOM_GAP = 8f
     }
 }
