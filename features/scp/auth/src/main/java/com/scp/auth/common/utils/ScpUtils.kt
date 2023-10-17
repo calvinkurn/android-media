1d7c936b2850601a1beb98ed175e22e86fcdb390
rama.putra <rama.putra@tokopedia.com>
2023-10-17 17:39:19 +0700

    add debug log

--- features/scp/auth/src/main/java/com/scp/auth/common/utils/ScpUtils.kt
+++ features/scp/auth/src/main/java/com/scp/auth/common/utils/ScpUtils.kt
@@ -1,5 +1,6 @@
 package com.scp.auth.common.utils
 
+import android.util.Log
 import com.scp.auth.GotoSdk
 import com.tokopedia.config.GlobalConfig
 import com.tokopedia.logger.ServerLogger
@@ -10,6 +11,7 @@ object ScpUtils {
 
     private val ROLLENCE_KEY_SCP_LOGIN = "exp_scp_goto_login_sdk"
     fun isGotoLoginEnabled(): Boolean {
+        Log.d("SCP_UTILS", "isGotoLoginEnabled: cek is debug ${GlobalConfig.DEBUG} or allowing debug tools ${GlobalConfig.isAllowDebuggingTools()}")
         return RemoteConfigInstance.getInstance()
             .abTestPlatform
             .getString(ROLLENCE_KEY_SCP_LOGIN)
@@ -17,10 +19,12 @@ object ScpUtils {
     }
 
     fun updateSsoToken(accessToken: String) {
+        Log.d("SCP_UTILS", "updateSsoToken: cek is debug ${GlobalConfig.DEBUG} or allowing debug tools ${GlobalConfig.isAllowDebuggingTools()}")
         GotoSdk.LSDKINSTANCE?.updateSsoToken(accessToken = accessToken)
     }
 
     fun saveTokens(accessToken: String, refreshToken: String) {
+        Log.d("SCP_UTILS", "saveTokens: cek is debug ${GlobalConfig.DEBUG} or allowing debug tools ${GlobalConfig.isAllowDebuggingTools()}")
         if (isGotoLoginEnabled()) {
             updateSsoToken(accessToken)
             GotoSdk.LSDKINSTANCE?.save(accessToken, refreshToken)
