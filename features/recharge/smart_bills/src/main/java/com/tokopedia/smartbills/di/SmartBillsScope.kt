8abfc7ed501d7b2bce1988fd59eaab3f35b1b534
firmankagami <firmanda.nugroho@tokopedia.com>
2023-10-17 13:58:02 +0700

    feat: removal module smart_bills

--- features/recharge/smart_bills/src/main/java/com/tokopedia/smartbills/di/SmartBillsScope.kt
+++
@@ -1,9 +0,0 @@
-package com.tokopedia.smartbills.di
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-import javax.inject.Scope
-
-@Scope
-@Retention(RetentionPolicy.CLASS)
-annotation class SmartBillsScope
