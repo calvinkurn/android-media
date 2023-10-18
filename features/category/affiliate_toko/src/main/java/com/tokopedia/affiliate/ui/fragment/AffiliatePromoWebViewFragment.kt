d7dcc2d4451efffef723886f7286eeb32cbaf073
tiwarideepesh <deepesh.tiwari@tokopedia.com>
2023-10-18 15:32:49 +0530

    feat: removed user validation check from promosikan webview

--- features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/fragment/AffiliatePromoWebViewFragment.kt
+++ features/category/affiliate_toko/src/main/java/com/tokopedia/affiliate/ui/fragment/AffiliatePromoWebViewFragment.kt
@@ -5,43 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.tokopedia.abstraction.base.app.BaseMainApplication
-import com.tokopedia.affiliate.ON_REGISTERED
-import com.tokopedia.affiliate.ON_REVIEWED
+import com.tokopedia.abstraction.base.view.fragment.BaseDaggerFragment
 import com.tokopedia.affiliate.PROMO_WEBVIEW_URL_PROD
 import com.tokopedia.affiliate.PROMO_WEBVIEW_URL_STAGING
-import com.tokopedia.affiliate.SYSTEM_DOWN
 import com.tokopedia.affiliate.di.AffiliateComponent
 import com.tokopedia.affiliate.di.DaggerAffiliateComponent
-import com.tokopedia.affiliate.ui.activity.AffiliateRegistrationActivity
-import com.tokopedia.affiliate.ui.custom.AffiliateBaseFragment
-import com.tokopedia.affiliate.viewmodel.AffiliatePromoViewModel
 import com.tokopedia.affiliate_toko.R
 import com.tokopedia.affiliate_toko.databinding.AffiliatePromoWebviewLayoutBinding
-import com.tokopedia.basemvvm.viewmodel.BaseViewModel
 import com.tokopedia.remoteconfig.RemoteConfigInstance
 import com.tokopedia.url.Env
 import com.tokopedia.url.TokopediaUrl
-import com.tokopedia.user.session.UserSessionInterface
 import com.tokopedia.utils.lifecycle.autoClearedNullable
 import javax.inject.Inject
 
-class AffiliatePromoWebViewFragment : AffiliateBaseFragment<AffiliatePromoViewModel>() {
-    private var affiliatePromoViewModel: AffiliatePromoViewModel? = null
+class AffiliatePromoWebViewFragment : BaseDaggerFragment() {
     val url = if (TokopediaUrl.getInstance().TYPE == Env.STAGING) {
         PROMO_WEBVIEW_URL_STAGING
     } else {
         PROMO_WEBVIEW_URL_PROD
     }
-
-    @Inject
-    @JvmField
-    var viewModelProvider: ViewModelProvider.Factory? = null
-
-    @Inject
-    @JvmField
-    var userSessionInterface: UserSessionInterface? = null
 
     @Inject
     @JvmField
@@ -72,18 +55,16 @@ class AffiliatePromoWebViewFragment : AffiliateBaseFragment<AffiliatePromoViewMo
         }
     }
 
+    override fun getScreenName(): String = this::class.java.name
+
+    override fun initInjector() {
+        getComponent().injectPromoWebViewFragment(this)
+    }
+
     companion object {
         fun getFragmentInstance(): Fragment {
             return AffiliatePromoWebViewFragment()
         }
-    }
-
-    override fun getVMFactory(): ViewModelProvider.Factory? {
-        return viewModelProvider
-    }
-
-    override fun initInject() {
-        getComponent().injectPromoWebViewFragment(this)
     }
 
     private fun getComponent(): AffiliateComponent =
@@ -91,40 +72,6 @@ class AffiliatePromoWebViewFragment : AffiliateBaseFragment<AffiliatePromoViewMo
             .builder()
             .baseAppComponent((activity?.application as BaseMainApplication).baseAppComponent)
             .build()
-
-    override fun onSystemDown() {
-        affiliatePromoViewModel?.setValidateUserType(SYSTEM_DOWN)
-    }
-
-    override fun onReviewed() {
-        affiliatePromoViewModel?.setValidateUserType(ON_REVIEWED)
-    }
-
-    override fun onUserNotRegistered() {
-        activity?.let {
-            AffiliateRegistrationActivity.newInstance(it)
-            it.finish()
-        }
-    }
-
-    override fun onNotEligible() {
-        activity?.let {
-            AffiliateRegistrationActivity.newInstance(it)
-            it.finish()
-        }
-    }
-
-    override fun onUserValidated() {
-        affiliatePromoViewModel?.setValidateUserType(ON_REGISTERED)
-    }
-
-    override fun setViewModel(viewModel: BaseViewModel) {
-        affiliatePromoViewModel = viewModel as AffiliatePromoViewModel
-    }
-
-    override fun getViewModelType(): Class<AffiliatePromoViewModel> {
-        return AffiliatePromoViewModel::class.java
-    }
 
     fun handleBack() {
         (webViewFragment as? AffiliateHelpFragment)?.handleBack()
