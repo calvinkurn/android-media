1598a423b51a9f346a22c3150059e2942b2011e3
yogieputra8 <yogie.putra@tokopedia.com>
2023-10-19 14:41:08 +0700

    Disable UI to nvestigate response data

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/info/data/GetEpharmacyShopInfo.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/info/data/GetEpharmacyShopInfo.kt
@@ -7,70 +7,69 @@ data class GetEpharmacyShopInfo(
     @SerializedName("data")
     @Expose
     val data: Data = Data()
+)
+
+data class Data(
+    @SerializedName("getEpharmacyShopInfo")
+    @Expose
+    val getEpharmacyShopInfo: GetEpharmacyShopInfoData = GetEpharmacyShopInfoData()
 ) {
 
-    data class Data(
-        @SerializedName("getEpharmacyShopInfo")
+    data class GetEpharmacyShopInfoData(
+        @SerializedName("header")
         @Expose
-        val getEpharmacyShopInfo: GetEpharmacyShopInfoData = GetEpharmacyShopInfoData()
+        val header: Header = Header(),
+        @SerializedName("data")
+        @Expose
+        val dataEpharm: DataEpharm = DataEpharm()
     ) {
 
-        data class GetEpharmacyShopInfoData(
-            @SerializedName("header")
+        data class DataEpharm(
+            @SerializedName("apj")
             @Expose
-            val header: Header = Header(),
-            @SerializedName("data")
+            val apj: String = "",
+            @SerializedName("epharmacy_working_hours_fmt")
             @Expose
-            val dataEpharm: DataEpharm = DataEpharm()
-        ) {
+            val epharmacyWorkingHoursFmt: List<String> = listOf(),
+            @SerializedName("sia_number")
+            @Expose
+            val siaNumber: String = "",
+            @SerializedName("sipa_number")
+            @Expose
+            val sipaNumber: String = "",
+            @SerializedName("epharmacy_working_hours")
+            @Expose
+            val epharmacyWorkingHours: List<EpharmacyWorkingHour> = listOf()
+        )
 
-            data class DataEpharm(
-                @SerializedName("apj")
-                @Expose
-                val apj: String = "",
-                @SerializedName("epharmacy_working_hours_fmt")
-                @Expose
-                val epharmacyWorkingHoursFmt: List<String> = listOf(),
-                @SerializedName("sia_number")
-                @Expose
-                val siaNumber: String = "",
-                @SerializedName("sipa_number")
-                @Expose
-                val sipaNumber: String = "",
-                @SerializedName("epharmacy_working_hours")
-                @Expose
-                val epharmacyWorkingHours: List<EpharmacyWorkingHour> = listOf()
-            )
+        data class EpharmacyWorkingHour(
+            @SerializedName("days")
+            @Expose
+            val days: List<String> = listOf(),
+            @SerializedName("opening_hours")
+            @Expose
+            val openingHours: OpeningHours = OpeningHours()
+        )
 
-            data class EpharmacyWorkingHour(
-                @SerializedName("days")
-                @Expose
-                val days: List<String> = listOf(),
-                @SerializedName("opening_hours")
-                @Expose
-                val openingHours: OpeningHours = OpeningHours()
-            )
+        data class OpeningHours(
+            @SerializedName("open_time")
+            @Expose
+            val openTime: String = "",
+            @SerializedName("close_time")
+            @Expose
+            val closeTime: String = ""
+        )
 
-            data class OpeningHours(
-                @SerializedName("open_time")
-                @Expose
-                val openTime: String = "",
-                @SerializedName("close_time")
-                @Expose
-                val closeTime: String = ""
-            )
-
-            data class Header(
-                @SerializedName("error_code")
-                @Expose
-                val errorCode: Int = 0,
-                @SerializedName("error_message")
-                @Expose
-                val errorMessage: List<String> = listOf(),
-                @SerializedName("process_time")
-                @Expose
-                val processTime: Double = 0.0
-            )
-        }
+        data class Header(
+            @SerializedName("error_code")
+            @Expose
+            val errorCode: Int = 0,
+            @SerializedName("error_message")
+            @Expose
+            val errorMessage: List<String> = listOf(),
+            @SerializedName("process_time")
+            @Expose
+            val processTime: Double = 0.0
+        )
     }
 }
--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/info/view/fragment/ShopInfoFragment.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/info/view/fragment/ShopInfoFragment.kt
@@ -25,7 +25,6 @@ import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.observe
 import com.tokopedia.kotlin.extensions.view.removeObservers
-import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.extensions.view.toIntOrZero
 import com.tokopedia.kotlin.extensions.view.visible
@@ -43,7 +42,6 @@ import com.tokopedia.shop.common.di.component.ShopComponent
 import com.tokopedia.shop.common.graphql.data.shopinfo.ShopBadge
 import com.tokopedia.shop.databinding.FragmentShopInfoBinding
 import com.tokopedia.shop.extension.transformToVisitable
-import com.tokopedia.shop.info.data.GetEpharmacyShopInfo
 import com.tokopedia.shop.info.di.component.DaggerShopInfoComponent
 import com.tokopedia.shop.info.di.module.ShopInfoModule
 import com.tokopedia.shop.info.view.activity.ShopInfoActivity.Companion.EXTRA_SHOP_INFO
@@ -318,7 +316,7 @@ class ShopInfoFragment :
                         shopViewModel?.let { _viewModel ->
                             shopInfo?.let { _shopInfo ->
                                 if (_viewModel.isShouldShowLicenseForDrugSeller(isGoApotik = _shopInfo.isGoApotik, fsType = _shopInfo.fsType)) {
-                                    renderEpharmDetailsData(epharmData = it.data.data, shopInfoData = _shopInfo)
+//                                    renderEpharmDetailsData(epharmData = it.data.data, shopInfoData = _shopInfo)
                                 }
                             }
                         }
@@ -487,26 +485,26 @@ class ShopInfoFragment :
         }
     }
 
-    private fun renderEpharmDetailsData(epharmData: GetEpharmacyShopInfo.Data, shopInfoData: ShopInfoData) {
-        if (!isErrorGetEpharmData(epharmData.getEpharmacyShopInfo.header)) {
-            fragmentShopInfoBinding?.let { binding ->
-                binding.layoutPartialShopInfoDescription.shopGoApotikContainer.visibility = View.VISIBLE
+//    private fun renderEpharmDetailsData(epharmData: GetEpharmacyShopInfo.Data, shopInfoData: ShopInfoData) {
+//        if (!isErrorGetEpharmData(epharmData.getEpharmacyShopInfo.header)) {
+//            fragmentShopInfoBinding?.let { binding ->
+//                binding.layoutPartialShopInfoDescription.shopGoApotikContainer.visibility = View.VISIBLE
+//
+//                binding.layoutPartialShopInfoDescription.tvSiaDescription.text = shopInfoData.siaNumber.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
+//                binding.layoutPartialShopInfoDescription.tvSipaDescription.text = shopInfoData.sipaNumber.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
+//                binding.layoutPartialShopInfoDescription.tvApjDescription.text = shopInfoData.apj.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
+//            }
+//        } else {
+//            val errMessage: String = epharmData.getEpharmacyShopInfo.header.errorMessage[0]
+//            view?.let {
+//                Toaster.build(it, errMessage, Toaster.LENGTH_SHORT, Toaster.TYPE_ERROR).show()
+//            }
+//        }
+//    }
 
-                binding.layoutPartialShopInfoDescription.tvSiaDescription.text = shopInfoData.siaNumber.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
-                binding.layoutPartialShopInfoDescription.tvSipaDescription.text = shopInfoData.sipaNumber.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
-                binding.layoutPartialShopInfoDescription.tvApjDescription.text = shopInfoData.apj.takeIf { it.isNotEmpty() } ?: EMPTY_DESCRIPTION
-            }
-        } else {
-            val errMessage: String = epharmData.getEpharmacyShopInfo.header.errorMessage[0]
-            view?.let {
-                Toaster.build(it, errMessage, Toaster.LENGTH_SHORT, Toaster.TYPE_ERROR).show()
-            }
-        }
-    }
-
-    private fun isErrorGetEpharmData(errData: GetEpharmacyShopInfo.Data.GetEpharmacyShopInfoData.Header): Boolean {
-        return errData.errorCode != 0 && errData.errorMessage.isNotEmpty()
-    }
+//    private fun isErrorGetEpharmData(errData: GetEpharmacyShopInfo.Data.GetEpharmacyShopInfoData.Header): Boolean {
+//        return errData.errorCode != 0 && errData.errorMessage.isNotEmpty()
+//    }
 
     private fun renderListNote(notes: List<ShopNoteUiModel>) {
         getShopId()?.let {
