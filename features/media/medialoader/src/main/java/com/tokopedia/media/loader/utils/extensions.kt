83634ed512044674693fac03f7be04c237379162
isfaaghyth <isfaaghyth@gmail.com>
2023-10-19 11:02:15 +0700

    feat: remove unused import class

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/extensions.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/extensions.kt
@@ -3,15 +3,6 @@ package com.tokopedia.media.loader.utils
 import android.os.Handler
 import android.os.Looper
 import android.widget.ImageView
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.LazyHeaders
-import com.tokopedia.config.GlobalConfig
-import com.tokopedia.media.loader.data.HEADER_FMT
-import com.tokopedia.media.loader.data.HEADER_KEY_AUTH
-import com.tokopedia.media.loader.data.HEADER_USER_ID
-import com.tokopedia.media.loader.data.HEADER_X_DEVICE
-import com.tokopedia.media.loader.data.PREFIX_BEARER
-import com.tokopedia.media.loader.data.WEBP_SUPPORT
 import com.tokopedia.media.loader.data.Properties
 import com.tokopedia.media.loader.module.GlideRequest
 
