4966c6b04c55a92758c33595316454e12a9d6e86
rezagama <rezagamahidayat@gmail.com>
2023-10-20 19:02:11 +0700

    Increase TokoPointsHomeViewModelTest coverage

--- features/promo/tokopoints/src/test/java/com/tokopedia/tokopoints/view/tokopointhome/TokoPointsHomeViewModelTest.kt
+++ features/promo/tokopoints/src/test/java/com/tokopedia/tokopoints/view/tokopointhome/TokoPointsHomeViewModelTest.kt
@@ -2,26 +2,44 @@ package com.tokopedia.tokopoints.view.tokopointhome
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.tokopedia.productcard.ProductCardModel
+import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationItem
+import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationWidget
 import com.tokopedia.tokopoints.notification.PopupNotifUsecase
+import com.tokopedia.tokopoints.notification.model.popupnotif.PopupNotif
+import com.tokopedia.tokopoints.notification.model.popupnotif.PopupNotifResponse
+import com.tokopedia.tokopoints.notification.model.popupnotif.Tokopoints
 import com.tokopedia.tokopoints.view.model.LuckyEggEntity
 import com.tokopedia.tokopoints.view.model.TokenDetailOuter
+import com.tokopedia.tokopoints.view.model.homeresponse.RecommendationWrapper
+import com.tokopedia.tokopoints.view.model.homeresponse.RewardsRecommendation
 import com.tokopedia.tokopoints.view.model.homeresponse.TokopointSuccess
 import com.tokopedia.tokopoints.view.model.rewardintro.IntroResponse
 import com.tokopedia.tokopoints.view.model.rewardintro.TokopediaRewardIntroPage
 import com.tokopedia.tokopoints.view.model.rewardtopsection.RewardResponse
 import com.tokopedia.tokopoints.view.model.rewardtopsection.TokopediaRewardTopSection
+import com.tokopedia.tokopoints.view.model.rewrdsStatusMatching.RewardTickerListResponse
+import com.tokopedia.tokopoints.view.model.rewrdsStatusMatching.RewardsTickerList
+import com.tokopedia.tokopoints.view.model.rewrdsStatusMatching.TickerListItem
 import com.tokopedia.tokopoints.view.model.section.TokopointsSection
 import com.tokopedia.tokopoints.view.model.section.TokopointsSectionOuter
+import com.tokopedia.tokopoints.view.model.usersaving.TokopointsUserSaving
+import com.tokopedia.tokopoints.view.model.usersaving.UserSavingInfo
+import com.tokopedia.tokopoints.view.model.usersaving.UserSavingResponse
 import com.tokopedia.tokopoints.view.recommwidget.RewardsRecommUsecase
 import com.tokopedia.tokopoints.view.util.*
+import com.tokopedia.unit.test.ext.verifyValueEquals
+import com.tokopedia.usecase.RequestParams
 import io.mockk.*
 import junit.framework.Assert
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -126,5 +144,205 @@ class TokoPointsHomeViewModelTest {
         viewModel.rewardIntroData.observeForever(tokopointIntroObserver)
         viewModel.getRewardIntroData()
         verify(exactly = 1) { tokopointIntroObserver.onChanged(any()) }
+    }
+
+    @Test
+    fun `given getRewardIntroData throws exception when getRewardIntroData should not crash`() {
+        coEvery { repository.getRewardIntroData() } throws NullPointerException()
+
+        viewModel.getRewardIntroData()
+
+        viewModel.rewardIntroData
+            .verifyValueEquals(null)
+    }
+
+    @Test
+    fun `given isShowIntroActivity true when getTokoPointDetail should update rewardIntroData live data`() {
+        val isShowIntroActivity = true
+
+        val sectionOuterResponse = TokopointsSectionOuter(
+            sectionContent = TokopointsSection()
+        )
+        val rewardTopSectionResponse = RewardResponse(
+            tokopediaRewardTopSection = TokopediaRewardTopSection(isShowIntroActivity = isShowIntroActivity)
+        )
+        val rewardIntroResponse = IntroResponse(
+            tokopediaRewardIntroPage = TokopediaRewardIntroPage(title = "Reward")
+        )
+
+        coEvery { repository.getTokoPointDetailData() } returns mockk {
+            every { getData<TokopointsSectionOuter>(TokopointsSectionOuter::class.java) } returns sectionOuterResponse
+            every { getData<RewardResponse>(RewardResponse::class.java) } returns rewardTopSectionResponse
+        }
+
+        coEvery { repository.getRewardIntroData() } returns mockk {
+            every { getData<IntroResponse>(IntroResponse::class.java) } returns rewardIntroResponse
+        }
+
+        viewModel.getTokoPointDetail()
+
+        viewModel.rewardIntroData
+            .verifyValueEquals(Success(rewardIntroResponse))
+    }
+
+    @Test
+    fun `given isShowSavingPage true when getTokoPointDetail should update deferredSavingData`() {
+        runTest {
+            val isShowSavingPage = true
+
+            val sectionOuterResponse = TokopointsSectionOuter(
+                sectionContent = TokopointsSection()
+            )
+            val rewardTopSectionResponse = RewardResponse(
+                tokopediaRewardTopSection = TokopediaRewardTopSection(isShowSavingPage = isShowSavingPage)
+            )
+            val userSavingResponse = UserSavingResponse(
+                tokopointsUserSaving = TokopointsUserSaving(userSavingInfo = UserSavingInfo(title = "Saving Info"))
+            )
+
+            coEvery { repository.getTokoPointDetailData() } returns mockk {
+                every { getData<TokopointsSectionOuter>(TokopointsSectionOuter::class.java) } returns sectionOuterResponse
+                every { getData<RewardResponse>(RewardResponse::class.java) } returns rewardTopSectionResponse
+            }
+
+            coEvery { repository.getUserSavingData() } returns mockk {
+                every { getData<UserSavingResponse>(UserSavingResponse::class.java) } returns userSavingResponse
+            }
+
+            viewModel.getTokoPointDetail()
+
+            val actualSavingData = viewModel.deferredSavingData?.await()
+
+            assertEquals(userSavingResponse, actualSavingData)
+        }
+    }
+
+    @Test
+    fun `given getUserSavingData throws exception when getTokoPointDetail should update deferredSavingData with default response`() {
+        runTest {
+            val sectionOuterResponse = TokopointsSectionOuter(
+                sectionContent = TokopointsSection()
+            )
+            val rewardTopSectionResponse = RewardResponse(
+                tokopediaRewardTopSection = TokopediaRewardTopSection(isShowSavingPage = true)
+            )
+
+            coEvery { repository.getTokoPointDetailData() } returns mockk {
+                every { getData<TokopointsSectionOuter>(TokopointsSectionOuter::class.java) } returns sectionOuterResponse
+                every { getData<RewardResponse>(RewardResponse::class.java) } returns rewardTopSectionResponse
+            }
+
+            coEvery { repository.getUserSavingData() } returns mockk {
+                every { getData<UserSavingResponse>(UserSavingResponse::class.java) } throws NullPointerException()
+            }
+
+            viewModel.getTokoPointDetail()
+
+            val actualSavingData = viewModel.deferredSavingData?.await()
+
+            assertEquals(UserSavingResponse(), actualSavingData)
+        }
+    }
+
+    @Test
+    fun `given get recommendation success when getTokoPointDetail should update defferedRecomData`() {
+        runTest {
+            val title = "Rekomendasi Belanja"
+            val appLink = "tokopedia://tokopoints/home"
+
+            val requestParams = RequestParams()
+            val recommendationItem = RecommendationItem(productId = 1, name = "Buah Jeruk")
+            val recommendationWidgetResponse = RecommendationWidget(
+                title = title,
+                recommendationItemList = listOf(recommendationItem),
+                seeMoreAppLink = appLink
+            )
+            val recommendationWrapperList = listOf(
+                RecommendationWrapper(
+                    recomendationItem = recommendationItem,
+                    recomData = createProductModel(recommendationItem)
+                )
+            )
+
+            coEvery { repository.getTokoPointDetailData() } returns mockk {
+                every { getData<TokopointsSectionOuter>(TokopointsSectionOuter::class.java) } returns TokopointsSectionOuter()
+                every { getData<RewardResponse>(RewardResponse::class.java) } returns RewardResponse()
+            }
+
+            coEvery { recomUsecase.getRequestParams(any(), any()) } returns requestParams
+            coEvery { recomUsecase.getData(requestParams) } returns listOf(recommendationWidgetResponse)
+            coEvery { recomUsecase.mapper.recommWidgetToListOfVisitables(recommendationWidgetResponse) } returns recommendationWrapperList
+
+            viewModel.getTokoPointDetail()
+
+            val expectedRecomData = RewardsRecommendation(
+                recommendationWrapper = recommendationWrapperList,
+                title = title,
+                appLink = appLink
+            )
+            val actualRecomData = viewModel.defferedRecomData?.await()
+
+            assertEquals(expectedRecomData, actualRecomData)
+        }
+    }
+
+    @Test
+    fun `given reward ticker response when getTokoPointDetail should update defferedRewardTickerResponse`() {
+        runTest {
+            val rewardTickerResponse = RewardTickerListResponse(
+                rewardsTickerList = RewardsTickerList(tickerList = listOf(TickerListItem(id = 1)))
+            )
+
+            coEvery { repository.getTokoPointDetailData() } returns mockk {
+                every { getData<TokopointsSectionOuter>(TokopointsSectionOuter::class.java) } returns TokopointsSectionOuter()
+                every { getData<RewardResponse>(RewardResponse::class.java) } returns RewardResponse()
+            }
+
+            coEvery { repository.getUserStatusMatchingData() } returns mockk {
+                every { getData<RewardTickerListResponse>(RewardTickerListResponse::class.java) } returns rewardTickerResponse
+            }
+
+            viewModel.getTokoPointDetail()
+
+            val actualRewardTickerResponse = viewModel.defferedRewardTickerResponse?.await()
+
+            assertEquals(rewardTickerResponse, actualRewardTickerResponse)
+        }
+    }
+
+    @Test
+    fun `when getPopNotifData success should return pop up notif response`() {
+        runTest {
+            val popUpNotifResponse = PopupNotifResponse(tokopoints = Tokopoints(popupNotif = PopupNotif(buttonText = "Click Here")))
+
+            coEvery { popupNotifUsecase.getPopupNotif(any()) } returns mockk {
+                every { getData<PopupNotifResponse>(PopupNotifResponse::class.java) } returns popUpNotifResponse
+            }
+
+            val actualTokopointDetailEntity = viewModel.getPopNotifData().await()
+
+            assertEquals(popUpNotifResponse, actualTokopointDetailEntity)
+        }
+    }
+
+    private fun createProductModel(element: RecommendationItem): ProductCardModel {
+        return ProductCardModel(
+            productName = element.name,
+            formattedPrice = element.price,
+            productImageUrl = element.imageUrl,
+            isTopAds = element.isTopAds,
+            ratingCount = element.rating,
+            hasThreeDots = false,
+            shopRating = element.ratingAverage,
+            isShopRatingYellow = true,
+            shopLocation = element.location,
+            shopBadgeList = element.badgesUrl.map {
+                ProductCardModel.ShopBadge(imageUrl = it)
+            },
+            freeOngkir = ProductCardModel.FreeOngkir(
+                isActive = element.isFreeOngkirActive,
+                imageUrl = element.freeOngkirImageUrl
+            )
+        )
     }
 }
