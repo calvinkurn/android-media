754e5c6d76d53cbb665f6f602f341fadb7e7fd20
habibitkp <101687909+habibitkp@users.noreply.github.com>
2023-10-24 09:26:16 +0700

    remove cache KTP and selfie in webview kyc page

--- features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/webview/GotoKycWebWrapperFragment.kt
+++ features/user/kyc_centralized/src/main/java/com/tokopedia/kyc_centralized/ui/gotoKyc/webview/GotoKycWebWrapperFragment.kt
@@ -3,8 +3,14 @@ package com.tokopedia.kyc_centralized.ui.gotoKyc.webview
 import android.os.Bundle
 import android.webkit.WebView
 import com.gojek.OneKycSdk
+import com.gojek.kyc.plus.OneKycConstants
+import com.gojek.kyc.plus.getKycSdkDocumentDirectoryPath
+import com.gojek.kyc.plus.getKycSdkFrameDirectoryPath
+import com.gojek.kyc.plus.getKycSdkLogDirectoryPath
 import com.gojek.kyc.web.OneKycWebWrapper
 import com.tokopedia.kyc_centralized.di.GoToKycComponent
+import com.tokopedia.kyc_centralized.ui.gotoKyc.utils.removeGotoKycImage
+import com.tokopedia.kyc_centralized.ui.gotoKyc.utils.removeGotoKycPreference
 import com.tokopedia.webview.BaseSessionWebViewFragment
 import javax.inject.Inject
 
@@ -13,10 +19,32 @@ class GotoKycWebWrapperFragment : BaseSessionWebViewFragment() {
     @Inject
     lateinit var oneKycSdk: OneKycSdk
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        removeGotoKycCache()
+    }
+
     override fun addJavascriptInterface(webView: WebView) {
         oneKycSdk.init()
         val wrapper = OneKycWebWrapper(oneKycSdk, requireActivity(), webView)
         webView.addJavascriptInterface(wrapper, OneKycWebWrapper.PLATFORM_CODE)
+    }
+
+    private fun removeGotoKycCache() {
+        val preferenceName = OneKycConstants.KYC_SDK_PREFERENCE_NAME
+        val preferenceKey = OneKycConstants.KYC_UPLOAD_PROGRESS_STATE
+        removeGotoKycPreference(
+            context = requireContext(),
+            preferenceName = preferenceName,
+            preferenceKey = preferenceKey
+        )
+
+        val directory1 = getKycSdkDocumentDirectoryPath(requireContext())
+        val directory2 = getKycSdkFrameDirectoryPath(requireContext())
+        val directory3 = getKycSdkLogDirectoryPath(requireContext())
+        removeGotoKycImage(directory1)
+        removeGotoKycImage(directory2)
+        removeGotoKycImage(directory3)
     }
 
     override fun initInjector() {
