f17a45cca5b2015d48c59f73c10b0f22a7ba8f49
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-10-25 17:05:40 +0700

    feat `stories creation`: remove unused import

--- features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/view/viewmodel/StoriesCreationViewModel.kt
+++ features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/view/viewmodel/StoriesCreationViewModel.kt
@@ -14,7 +14,6 @@ import com.tokopedia.stories.creation.view.model.state.StoriesCreationUiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
