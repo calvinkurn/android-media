a54e4268effc46daffb7212195edc3124c9ac865
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-10-25 14:16:54 +0700

    address comment `stories creation`: remove activityContext injection

--- features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/di/StoriesCreationModule.kt
+++ features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/di/StoriesCreationModule.kt
@@ -1,6 +1,5 @@
 package com.tokopedia.stories.creation.di
 
-import android.content.Context
 import dagger.Module
 import dagger.Provides
 
@@ -8,11 +7,6 @@ import dagger.Provides
  * Created By : Jonathan Darwin on September 05, 2023
  */
 @Module
-class StoriesCreationModule(
-    private val activityContext: Context
-) {
+class StoriesCreationModule {
 
-    @Provides
-    @StoriesCreationScope
-    fun provideActivityContext() = activityContext
 }
