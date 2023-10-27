b777b28a34421336704d8f5285d890525e0b404d
isfaaghyth <isfaaghyth@gmail.com>
2023-10-27 14:38:38 +0700

    feat: propagate the feature toggle

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/FeatureToggle.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/utils/FeatureToggle.kt
@@ -4,31 +4,34 @@ import android.content.Context
 import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
 import com.tokopedia.remoteconfig.RemoteConfigInstance
 
-object FeatureToggle {
+interface FeatureToggle {
 
-    private const val M3U8_THUMBNAIL_LOADER = "android_enable_m3u8_thumbnail_loader"
-    private const val EXPOSE_RESPONSE_HEADER = "android_media_loader_expose_header"
-    private const val WEBP_SUPPORT = "android_webp"
+    fun glideM3U8ThumbnailLoaderEnabled(context: Context): Boolean
+    fun shouldAbleToExposeResponseHeader(context: Context): Boolean
+    fun isWebpFormatEnabled(): Boolean
+}
+
+class FeatureToggleManager : FeatureToggle {
 
     private var glideM3U8ThumbnailLoaderEnabled: Boolean? = null
     private var isExposeResponseHeaderEnabled: Boolean? = null
     private var isWebpFormatDelivered: Boolean? = null
 
-    fun glideM3U8ThumbnailLoaderEnabled(context: Context): Boolean {
+    override fun glideM3U8ThumbnailLoaderEnabled(context: Context): Boolean {
         return glideM3U8ThumbnailLoaderEnabled ?: getRemoteConfigBoolean(
             context,
             M3U8_THUMBNAIL_LOADER
         ).also { glideM3U8ThumbnailLoaderEnabled = it }
     }
 
-    fun shouldAbleToExposeResponseHeader(context: Context): Boolean {
+    override fun shouldAbleToExposeResponseHeader(context: Context): Boolean {
         return isExposeResponseHeaderEnabled ?: getRemoteConfigBoolean(
             context,
             EXPOSE_RESPONSE_HEADER
         ).also { isExposeResponseHeaderEnabled = it }
     }
 
-    fun isWebpFormatEnabled(): Boolean {
+    override fun isWebpFormatEnabled(): Boolean {
         return isWebpFormatDelivered ?: getAbTestBoolean(WEBP_SUPPORT)
             .also { isWebpFormatDelivered = it }
     }
@@ -44,5 +47,19 @@ object FeatureToggle {
             .abTestPlatform
             .getString(key)
             .isNotEmpty()
+    }
+
+    companion object {
+        private const val M3U8_THUMBNAIL_LOADER = "android_enable_m3u8_thumbnail_loader"
+        private const val EXPOSE_RESPONSE_HEADER = "android_media_loader_expose_header"
+        private const val WEBP_SUPPORT = "android_webp"
+
+        private var toggle: FeatureToggle? = null
+
+        fun instance(): FeatureToggle {
+            return toggle ?: FeatureToggleManager().also {
+                toggle = it
+            }
+        }
     }
 }
