e41623f0b88e8abcaece5b1027ee1b7215264b62
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-10-30 11:42:50 +0700

    feat `stories creation`: add MediaUtil class for media utility purpose (get media dimension, orientation, compare ratio)

---
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/util/MediaUtil.kt
@@ -0,0 +1,31 @@
+package com.tokopedia.content.common.util
+
+import android.graphics.BitmapFactory
+import java.io.File
+
+/**
+ * Created By : Jonathan Darwin on October 30, 2023
+ */
+
+fun getMediaSize(filePath: String): Pair<Int, Int> {
+    val options = BitmapFactory.Options()
+    BitmapFactory.decodeFile(File(filePath).absolutePath, options)
+    val width = options.outWidth
+    val height = options.outHeight
+
+    return width to height
+}
+
+fun isMediaPotrait(filePath: String): Boolean {
+    val (width, height) = getMediaSize(filePath)
+    return width < height
+}
+
+fun isMediaLandscape(filePath: String): Boolean {
+    return !isMediaPotrait(filePath)
+}
+
+fun isMediaRatioSame(filePath: String, ratio: Float): Boolean {
+    val (width, height) = getMediaSize(filePath)
+    return ratio == (width / height.toFloat())
+}
