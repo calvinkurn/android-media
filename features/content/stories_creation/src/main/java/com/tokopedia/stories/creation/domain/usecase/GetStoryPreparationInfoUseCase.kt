6a577d6b821080986f6ecd913e028abd1004df45
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-10-31 15:08:34 +0700

    address comment `stories creation`: use GqlQuery annotation & make the class internal

--- features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/domain/usecase/GetStoryPreparationInfoUseCase.kt
+++ features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/domain/usecase/GetStoryPreparationInfoUseCase.kt
@@ -2,6 +2,7 @@ package com.tokopedia.stories.creation.domain.usecase
 
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.gql_query_annotation.GqlQuery
 import com.tokopedia.graphql.coroutines.data.extensions.request
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
@@ -12,12 +13,13 @@ import javax.inject.Inject
 /**
  * Created By : Jonathan Darwin on September 11, 2023
  */
+@GqlQuery(GetStoryPreparationInfoUseCase.QUERY_NAME, GetStoryPreparationInfoUseCase.QUERY)
 class GetStoryPreparationInfoUseCase @Inject constructor(
     dispatchers: CoroutineDispatchers,
     @ApplicationContext private val repository: GraphqlRepository,
 ) : CoroutineUseCase<GetStoryPreparationInfoRequest, GetStoryPreparationInfoResponse>(dispatchers.io) {
 
-    override fun graphqlQuery(): String = QUERY
+    override fun graphqlQuery(): String = ContentCreatorStoryGetPreparationInfoQuery().getQuery()
 
     override suspend fun execute(params: GetStoryPreparationInfoRequest): GetStoryPreparationInfoResponse {
         return repository.request(graphqlQuery(), params.buildRequestParam())
@@ -26,7 +28,8 @@ class GetStoryPreparationInfoUseCase @Inject constructor(
     companion object {
         private const val PARAM_REQ = "req"
 
-        private const val QUERY = """
+        const val QUERY_NAME = "ContentCreatorStoryGetPreparationInfoQuery"
+        const val QUERY = """
             query contentCreatorStoryGetPreparationInfo(
                 ${"$$PARAM_REQ"}: ContentCreatorStoryGetPreparationInfoRequest!,
             ) {
