223576b79fe3d7e953648026ac1d9695ea767f18
willybroduspurnama <willybrodus.purnama@tokopedia.com>
2023-10-31 08:57:04 +0700

    [SRP] Implement add reimagine icon quick filter

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/presenter/product/ProductListPresenter.kt
@@ -1013,6 +1013,7 @@ class ProductListPresenter @Inject constructor(
             title = title,
             isSelected = isChipSelected,
             hasChevron = !isSingleFilter,
+            iconUrl = filter.getIconImage(title)
         )
     }
 
