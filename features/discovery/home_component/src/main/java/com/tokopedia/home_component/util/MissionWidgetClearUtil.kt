0a63fc6b2ae429c25d2ce26fbace1ddb81f8cc5e
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-11-01 09:26:42 +0700

    fix mission widget height

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/MissionWidgetClearUtil.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/util/MissionWidgetClearUtil.kt
@@ -9,17 +9,32 @@ import com.tokopedia.unifyprinciples.Typography
  * Created by frenzel
  */
 class MissionWidgetClearUtil: MissionWidgetUtil() {
+    override fun findMaxTitleHeight(data: MissionWidgetListDataModel, context: Context): Int {
+        var maxHeight = 0
+        val titleWidth = context.resources.getDimensionPixelSize(R.dimen.home_mission_widget_clear_image_size)
+
+        for (missionWidget in data.missionWidgetList) {
+            val heightText = measureTextHeight(
+                context = context,
+                text = missionWidget.subTitle,
+                textWidth = titleWidth,
+                typographyType = Typography.DISPLAY_3,
+                typographyWeight = Typography.REGULAR,
+                maxLines = MAX_LINES,
+                hideWhenEmpty = false,
+            )
+            if (heightText > maxHeight) maxHeight = heightText
+        }
+        return maxHeight
+    }
 
     override fun findMaxSubtitleHeight(
         data: MissionWidgetListDataModel,
         context: Context
     ): Int {
         var maxHeight = 0
-        var subtitleWidth = context.resources.getDimensionPixelSize(R.dimen.home_mission_widget_card_image_size)
+        val subtitleWidth = context.resources.getDimensionPixelSize(R.dimen.home_mission_widget_clear_image_size)
 
-        //substract with margin start and end
-        subtitleWidth -= context.resources.getDimensionPixelSize(R.dimen.home_mission_widget_card_margin_horizontal_subtitle)
-        subtitleWidth -= context.resources.getDimensionPixelSize(R.dimen.home_mission_widget_card_margin_horizontal_subtitle)
         for (missionWidget in data.missionWidgetList) {
             val heightText = measureTextHeight(
                 context = context,
