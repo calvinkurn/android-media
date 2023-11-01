fa37c57b4887d5f62378f29541c27f892b751548
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2023-11-01 13:03:39 +0700

    address comment `stories creation`: change input type of MediaPickerForResult

--- features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/view/model/activityResult/MediaPickerForResult.kt
+++ features/content/stories_creation/src/main/java/com/tokopedia/stories/creation/view/model/activityResult/MediaPickerForResult.kt
@@ -4,15 +4,30 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 import com.tokopedia.picker.common.MediaPicker
+import com.tokopedia.picker.common.PageSource
+import com.tokopedia.picker.common.types.ModeType
+import com.tokopedia.picker.common.types.PageType
+import com.tokopedia.stories.creation.R
 import com.tokopedia.stories.creation.view.model.StoriesMedia
 import com.tokopedia.stories.creation.view.model.StoriesMediaType
 
 /**
  * Created By : Jonathan Darwin on October 31, 2023
  */
-class MediaPickerForResult : ActivityResultContract<Intent, StoriesMedia>() {
+class MediaPickerForResult : ActivityResultContract<MediaPickerIntentData, StoriesMedia>() {
 
-    override fun createIntent(context: Context, input: Intent): Intent = input
+    override fun createIntent(context: Context, input: MediaPickerIntentData): Intent {
+        return MediaPicker.intent(context) {
+            pageSource(PageSource.Stories)
+            minVideoDuration(input.minVideoDuration)
+            maxVideoDuration(input.maxVideoDuration)
+            pageType(PageType.GALLERY)
+            modeType(ModeType.COMMON)
+            singleSelectionMode()
+            withImmersiveEditor()
+            previewActionText(input.previewActionText)
+        }
+    }
 
     override fun parseResult(resultCode: Int, intent: Intent?): StoriesMedia {
         return if (intent != null) {
