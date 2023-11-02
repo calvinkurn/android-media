ff16abafcf4ce9c941691a603afec7135f461341
isfaaghyth <isfaaghyth@gmail.com>
2023-11-03 02:44:34 +0700

    feat: remove unused content type mapper

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/data/mapper.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/data/mapper.kt
@@ -10,14 +10,6 @@ fun List<Header>.getFailureType(): FailureType? {
     return FailureType.fromString(values)
 }
 
-fun List<Header>.getContentType(): String {
-    val key = "content-type"
-    val result = find { it.key().lowercase() == key } ?: return ""
-    if (result.values.isEmpty()) return ""
-
-    return result.firstValue()
-}
-
 fun Map<String, List<String>>.toModel(): List<Header> {
     return map {
         val (key, value) = it
