4f4eb105b28fa559f72c654a47398016404299fc
ilhamsuaib <ilhamsuaib10@gmail.com>
2023-11-02 09:36:57 +0700

    add missing import

--- features/common/app_common/src/main/java/com/tokopedia/app/common/MainApplication.java
+++ features/common/app_common/src/main/java/com/tokopedia/app/common/MainApplication.java
@@ -2,6 +2,7 @@ package com.tokopedia.app.common;
 
 import android.content.SharedPreferences;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.PreferenceManager;
 
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
