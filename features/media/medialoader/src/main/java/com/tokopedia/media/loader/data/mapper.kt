dec39a5140425204325036a897748185a8c34ab9
isfaaghyth <isfaaghyth@gmail.com>
2023-11-02 18:36:40 +0700

    feat: create content-type mapper from headers

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/data/mapper.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/data/mapper.kt
@@ -10,6 +10,14 @@ fun List<Header>.getFailureType(): FailureType? {
     return FailureType.fromString(values)
 }
 
+fun List<Header>.getContentType(): String {
+    val key = "content-type"
+    val result = find { it.key().lowercase() == key } ?: return ""
+    if (result.values.isEmpty()) return ""
+
+    return result.firstValue()
+}
+
 fun Map<String, List<String>>.toModel(): List<Header> {
     return map {
         val (key, value) = it
