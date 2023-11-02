ac678eb4a64c15c46fcd403e5f0fbd710ced7570
isfaaghyth <isfaaghyth@gmail.com>
2023-11-03 02:41:50 +0700

    feat: remove unused APIs during onResourceReady callbacks

--- features/media/medialoader/src/main/java/com/tokopedia/media/loader/listener/MediaListenerBuilder.kt
+++ features/media/medialoader/src/main/java/com/tokopedia/media/loader/listener/MediaListenerBuilder.kt
@@ -6,12 +6,10 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.tokopedia.media.loader.data.getFailureType
 import com.tokopedia.media.loader.data.Properties
-import com.tokopedia.media.loader.data.getContentType
+import com.tokopedia.media.loader.data.getFailureType
 import com.tokopedia.media.loader.internal.NetworkResponseManager
-import com.tokopedia.media.loader.tracker.Logger
-import com.tokopedia.media.loader.tracker.MediaLoaderTracker
+import com.tokopedia.media.loader.tracker.old.MediaLoaderTracker
 import com.tokopedia.media.loader.utils.adaptiveSizeImageRequest
 import com.tokopedia.media.loader.wrapper.MediaDataSource.Companion.mapTo as dataSource
 
@@ -73,26 +71,15 @@ internal object MediaListenerBuilder {
                 ?.shouldAbleToExposeResponseHeader(context)
 
             if (properties.shouldTrackNetwork && shouldAbleToExposeResponseHeader == true) {
-                val result = NetworkResponseManager.getInstance()
-                val headers = result.header(properties.data.toString())
+                val headers = NetworkResponseManager.header(properties.data.toString())
 
-                // save network response and send to the header's callback
                 properties.setNetworkResponse?.header(
                     headers, // get all header responses
                     headers.getFailureType() // get failure type (if any)
                 )
 
-                // send tracker
-                Logger.request(context) {
-                    url = properties.data.toString()
-                    requestLoadTime = loadTime
-                    imageBitmap = resource
-                    contentType = headers.getContentType()
-                }
-
-                // in case we need to clear force the previous cache of headers
                 if (properties.isForceClearHeaderCache) {
-                    result.forceResetCache()
+                    NetworkResponseManager.forceResetCache()
                 }
             }
         }
