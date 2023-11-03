6271901ea070cb2b101e46b8ecef97839bd7107e
rezagama <rezagamahidayat@gmail.com>
2023-11-03 15:28:47 +0700

    Quest Widget Star Lottie

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/view/HomeQuestProgressBarView.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/view/HomeQuestProgressBarView.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.tokopedianow.home.presentation.view
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
@@ -7,8 +9,9 @@ import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.ViewGroup
+import com.tokopedia.imageassets.TokopediaImageUrl
 import com.tokopedia.kotlin.extensions.view.addOneTimeGlobalLayoutListener
+import com.tokopedia.kotlin.extensions.view.dpToPx
 import com.tokopedia.tokopedianow.databinding.LayoutTokopedianowQuestProgressBarViewBinding
 import com.tokopedia.tokopedianow.home.presentation.uimodel.quest.HomeQuestWidgetUiModel
 import com.tokopedia.unifycomponents.BaseCustomView
@@ -25,6 +28,7 @@ class HomeQuestProgressBarView @JvmOverloads constructor(
         private const val PROGRESS_ANIM_START = 0
         private const val ANIM_DURATION = 600L
         private const val DEFAULT_PROGRESS = 1
+        private const val CIRCLE_Y_OFFSET = 6
     }
 
     private val binding = LayoutTokopedianowQuestProgressBarViewBinding.inflate(
@@ -34,6 +38,7 @@ class HomeQuestProgressBarView @JvmOverloads constructor(
     )
 
     private var progressAnimEnd = DEFAULT_PROGRESS
+    private var circleViewList = mutableListOf<HomeQuestProgressCircleView>()
 
     fun bind(uiModel: HomeQuestWidgetUiModel) {
         binding.root.addOneTimeGlobalLayoutListener {
@@ -54,15 +59,27 @@ class HomeQuestProgressBarView @JvmOverloads constructor(
             val lastFinishedQuest = questList.lastOrNull { it.isFinished() }
             val progressIndex = lastFinishedQuest?.let { questList.indexOf(lastFinishedQuest) }
             val xMultiplier = getViewX() / (questCount - 1)
+            val circleYOffset = context.dpToPx(CIRCLE_Y_OFFSET)
 
             for (i in 0 until questCount) {
-                val circle = HomeQuestProgressCircleView(context)
                 val x = i * xMultiplier
+
+                if (circleViewList.getOrNull(i) == null) {
+                    val y = viewProgress.y + circleYOffset
+                    val view = HomeQuestProgressCircleView(context)
+                    circleViewList.add(view)
+                    root.addView(view)
+                    view.y = y
+                    view.x = x
+                } else {
+                    val view = circleViewList[i]
+                    root.removeView(view)
+                    root.addView(view)
+                }
+
                 if (i == progressIndex) {
                     progressAnimEnd = x.toInt()
                 }
-                root.addView(circle)
-                circle.x = x
             }
 
             imageStar.bringToFront()
@@ -87,19 +104,46 @@ class HomeQuestProgressBarView @JvmOverloads constructor(
                 STAR_SCALE_DOWN,
                 STAR_SCALE_UP
             )
+            val lottieXOffset = context.resources.getDimensionPixelSize(
+                unifyprinciplesR.dimen.unify_space_8
+            )
+
+            lottieStar.setAnimationFromUrl(TokopediaImageUrl.TOKOPEDIANOW_LOTTIE_QUEST_STAR)
+            lottieStar.setFailureListener { }
 
             progressAnim.addUpdateListener { valueAnimator ->
                 val width = valueAnimator.animatedValue as Int
-                val layoutParams: ViewGroup.LayoutParams = viewProgress.layoutParams
-                layoutParams.width = width
+                val x = width.toFloat()
 
-                viewProgress.layoutParams = layoutParams
-                imageStar.x = width.toFloat()
+                viewProgress.layoutParams = viewProgress.layoutParams.apply {
+                    this.width = width
+                }
+
+                lottieStar.x = x - lottieXOffset
+                imageStar.x = x
             }
 
             val animSet = AnimatorSet().apply {
                 playTogether(starScaleXAnim, starScaleYAnim, progressAnim)
             }
+
+            animSet.addListener(object : AnimatorListener {
+                override fun onAnimationStart(p0: Animator) {
+
+                }
+
+                override fun onAnimationEnd(p0: Animator) {
+                    lottieStar.playAnimation()
+                }
+
+                override fun onAnimationCancel(p0: Animator) {
+
+                }
+
+                override fun onAnimationRepeat(p0: Animator) {
+
+                }
+            })
 
             animSet.duration = ANIM_DURATION
             animSet.start()
