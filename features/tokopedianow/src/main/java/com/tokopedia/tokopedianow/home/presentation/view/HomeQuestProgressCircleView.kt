fa6e36b14c0ca4893a178613a2436a48ea56b792
rezagama <rezagamahidayat@gmail.com>
2023-11-06 10:50:29 +0700

    Progress circle view setColor

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/view/HomeQuestProgressCircleView.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/view/HomeQuestProgressCircleView.kt
@@ -3,6 +3,8 @@ package com.tokopedia.tokopedianow.home.presentation.view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.annotation.ColorRes
+import com.tokopedia.media.loader.loadImage
 import com.tokopedia.tokopedianow.databinding.LayoutTokopedianowQuestProgressBarCircleViewBinding
 import com.tokopedia.unifycomponents.BaseCustomView
 
@@ -11,11 +13,16 @@ class HomeQuestProgressCircleView @JvmOverloads constructor(
     attrs: AttributeSet? = null
 ) : BaseCustomView(context, attrs) {
 
-    init {
-        LayoutTokopedianowQuestProgressBarCircleViewBinding.inflate(
-            LayoutInflater.from(context),
-            this,
-            true
-        )
+    private val binding = LayoutTokopedianowQuestProgressBarCircleViewBinding.inflate(
+        LayoutInflater.from(context),
+        this,
+        true
+    )
+
+    fun setColor(@ColorRes strokeColorResId: Int, @ColorRes imageColorResId: Int) {
+        binding.apply {
+            imageCircle.setStrokeColorResource(strokeColorResId)
+            imageCircle.loadImage(imageColorResId)
+        }
     }
 }
