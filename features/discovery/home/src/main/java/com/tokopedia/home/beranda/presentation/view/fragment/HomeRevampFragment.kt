db2e7bdaab072be55d1966b51030a331085a79c3
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-11-06 14:44:25 +0700

    feat: revert setHomeBottomNavBasedOnScrolling

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeRevampFragment.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/fragment/HomeRevampFragment.kt
@@ -822,6 +822,7 @@ open class HomeRevampFragment :
                 scrollPositionY = recyclerView.computeVerticalScrollOffset()
                 updateThematicVerticalPosition()
                 evaluateHomeComponentOnScroll(recyclerView)
+                setHomeBottomNavBasedOnScrolling()
             }
         })
         setupEmbraceBreadcrumbListener()
@@ -925,43 +926,43 @@ open class HomeRevampFragment :
 
     private fun observeHomeThematic() {
         getHomeViewModel().thematicLiveData.observe(viewLifecycleOwner) { thematic ->
-            if(thematic.isShown) {
+            if (thematic.isShown) {
                 context?.let { ctx ->
-                    val thematicImageLoadListener = object: ImageHandler.ImageLoaderStateListener {
+                    val thematicImageLoadListener = object : ImageHandler.ImageLoaderStateListener {
                         override fun successLoad(view: ImageView) {
                             view.show()
-                            if(view == thematicBackground) {
+                            if (view == thematicBackground) {
                                 notifyHomeThematicChanges(thematic, true)
                             }
                         }
 
                         override fun failedLoad(view: ImageView) {
                             view.hide()
-                            if(view == thematicBackground) {
+                            if (view == thematicBackground) {
                                 notifyHomeThematicChanges(thematic, false)
                             }
                         }
                     }
 
-                    if(thematic.backgroundImageURL.isNotEmpty()) {
+                    if (thematic.backgroundImageURL.isNotEmpty()) {
                         thematicBackground?.run {
                             setLayoutHeight(thematic.getActualHeightPx(ctx))
                             loadImageWithoutPlaceholder(
                                 thematic.backgroundImageURL,
                                 "thematicBackground",
                                 listener = thematicImageLoadListener,
-                                skipErrorPlaceholder = true,
+                                skipErrorPlaceholder = true
                             )
                         }
                     }
 
-                    if(thematic.foregroundImageURL.isNotEmpty()) {
+                    if (thematic.foregroundImageURL.isNotEmpty()) {
                         thematicForeground?.run {
                             loadImageWithoutPlaceholder(
                                 thematic.foregroundImageURL,
                                 "thematicForeground",
                                 listener = thematicImageLoadListener,
-                                skipErrorPlaceholder = true,
+                                skipErrorPlaceholder = true
                             )
                         }
                     }
@@ -1476,10 +1477,10 @@ open class HomeRevampFragment :
     }
 
     private fun setupThematicStatusBarAndToolbar() {
-        if(getThematicUtil().isLightMode()) {
+        if (getThematicUtil().isLightMode()) {
             requestStatusBarDark()
             navToolbar?.switchToLightToolbar()
-        } else if(getThematicUtil().isDarkMode()) {
+        } else if (getThematicUtil().isDarkMode()) {
             requestStatusBarLight()
             navToolbar?.switchToDarkToolbar()
         } else {
@@ -1489,8 +1490,11 @@ open class HomeRevampFragment :
     }
 
     private fun requestStatusBarBasedOnUiMode() {
-        if(context?.isDarkMode() == true) requestStatusBarLight()
-        else requestStatusBarDark()
+        if (context?.isDarkMode() == true) {
+            requestStatusBarLight()
+        } else {
+            requestStatusBarDark()
+        }
     }
 
     private object FixedTheme {
@@ -1602,7 +1606,7 @@ open class HomeRevampFragment :
             BestSellerWidgetCallback(context, this, getHomeViewModel()),
             SpecialReleaseRevampCallback(this),
             ShopFlashSaleWidgetCallback(this, getHomeViewModel()),
-            getThematicUtil(),
+            getThematicUtil()
         )
         val asyncDifferConfig = AsyncDifferConfig.Builder(HomeVisitableDiffUtil())
             .setBackgroundThreadExecutor(Executors.newSingleThreadExecutor())
@@ -2845,7 +2849,7 @@ open class HomeRevampFragment :
     }
 
     private fun getRemoteConfigController(): HomeRemoteConfigController {
-        if(!this::homeRemoteConfigController.isInitialized) initInjectorHome()
+        if (!this::homeRemoteConfigController.isInitialized) initInjectorHome()
         return homeRemoteConfigController
     }
 
@@ -3080,7 +3084,7 @@ open class HomeRevampFragment :
     }
 
     private fun getThematicUtil(): HomeThematicUtil {
-        if(!::homeThematicUtil.isInitialized) initInjectorHome()
+        if (!::homeThematicUtil.isInitialized) initInjectorHome()
         return homeThematicUtil
     }
 }
