d1a4cf7a25b27f718e2a50bb5b9068f2967ed239
hansenpw <hansen.wijaya@tokopedia.com>
2023-11-08 08:22:17 +0700

    add ut

--- features/transaction/oneclickcheckout/src/test/java/com/tokopedia/oneclickcheckout/order/view/OrderSummaryPageViewModelPaymentTest.kt
+++ features/transaction/oneclickcheckout/src/test/java/com/tokopedia/oneclickcheckout/order/view/OrderSummaryPageViewModelPaymentTest.kt
@@ -1,8 +1,13 @@
 package com.tokopedia.oneclickcheckout.order.view
 
+import com.tokopedia.logisticCommon.data.entity.ratescourierrecommendation.EstimatedTimeArrivalPromo
 import com.tokopedia.logisticCommon.data.entity.ratescourierrecommendation.InsuranceData
+import com.tokopedia.logisticCommon.data.entity.ratescourierrecommendation.ProductData
+import com.tokopedia.logisticcart.shipping.model.LogisticPromoUiModel
+import com.tokopedia.logisticcart.shipping.model.ShippingCourierUiModel
 import com.tokopedia.oneclickcheckout.order.data.payment.AdditionalInfoData
 import com.tokopedia.oneclickcheckout.order.data.payment.BenefitSummaryInfoData
+import com.tokopedia.oneclickcheckout.order.data.payment.CartAddOnData
 import com.tokopedia.oneclickcheckout.order.data.payment.CartAddressData
 import com.tokopedia.oneclickcheckout.order.data.payment.CartData
 import com.tokopedia.oneclickcheckout.order.data.payment.CartDetail
@@ -28,6 +33,10 @@ import com.tokopedia.oneclickcheckout.order.view.model.OrderProfileAddress
 import com.tokopedia.oneclickcheckout.order.view.model.OrderShipment
 import com.tokopedia.oneclickcheckout.order.view.model.OrderShop
 import com.tokopedia.oneclickcheckout.order.view.model.OrderTotal
+import com.tokopedia.purchase_platform.common.feature.addonsproduct.data.model.AddOnsProductDataModel
+import com.tokopedia.purchase_platform.common.feature.gifting.data.model.AddOnGiftingButtonModel
+import com.tokopedia.purchase_platform.common.feature.gifting.data.model.AddOnGiftingDataItemModel
+import com.tokopedia.purchase_platform.common.feature.gifting.data.model.AddOnGiftingDataModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -43,7 +52,23 @@ class OrderSummaryPageViewModelPaymentTest : BaseOrderSummaryPageViewModelTest()
             orderCost = OrderCost(totalPriceWithoutPaymentFees = 1.1)
         )
         orderSummaryPageViewModel.orderShop.value =
-            OrderShop(shopId = "234", warehouseId = "345", shopTier = 456)
+            OrderShop(
+                shopId = "234",
+                warehouseId = "345",
+                shopTier = 456,
+                addOn = AddOnGiftingDataModel(
+                    status = 0,
+                    addOnsDataItemModelList = listOf(
+                        AddOnGiftingDataItemModel(
+                            addOnPrice = 13.0,
+                            addOnQty = 2
+                        )
+                    ),
+                    addOnsButtonModel = AddOnGiftingButtonModel(
+                        title = "gift"
+                    )
+                )
+            )
         orderSummaryPageViewModel.orderCart =
             OrderCart(cartString = "123", shop = orderSummaryPageViewModel.orderShop.value)
         orderSummaryPageViewModel.orderProducts.value = listOf(
@@ -54,7 +79,49 @@ class OrderSummaryPageViewModelPaymentTest : BaseOrderSummaryPageViewModelTest()
                 orderQuantity = 2,
                 categoryId = "12",
                 lastLevelCategory = "asdfasdf",
-                categoryIdentifier = "12asdf"
+                categoryIdentifier = "12asdf",
+                addOn = AddOnGiftingDataModel(
+                    status = 1,
+                    addOnsDataItemModelList = listOf(
+                        AddOnGiftingDataItemModel(
+                            addOnPrice = 12.0,
+                            addOnQty = 2
+                        )
+                    ),
+                    addOnsButtonModel = AddOnGiftingButtonModel(
+                        title = "gift"
+                    )
+                ),
+                addOnsProductData = AddOnsProductDataModel(
+                    data = listOf(
+                        AddOnsProductDataModel.Data(
+                            status = 0,
+                            name = "addon0",
+                            price = 20
+                        ),
+                        AddOnsProductDataModel.Data(
+                            status = 1,
+                            name = "addon1",
+                            price = 21
+                        ),
+                        AddOnsProductDataModel.Data(
+                            status = 2,
+                            name = "addon2",
+                            price = 22
+                        ),
+                        AddOnsProductDataModel.Data(
+                            status = 3,
+                            name = "addon3",
+                            price = 23
+                        ),
+                        AddOnsProductDataModel.Data(
+                            status = 3,
+                            name = "addon4",
+                            price = 24,
+                            fixedQuantity = true
+                        )
+                    )
+                )
             ),
             OrderProduct(
                 isError = true,
@@ -133,6 +200,191 @@ class OrderSummaryPageViewModelPaymentTest : BaseOrderSummaryPageViewModelTest()
                                             shipperName = "kirimin",
                                             eta = "1 hari lagi",
                                             insurancePrice = 3.0
+                                        ),
+                                        shopOrders = listOf(
+                                            CartShopOrderData(
+                                                shopId = "234",
+                                                warehouseId = 345,
+                                                shopTier = 456,
+                                                products = listOf(
+                                                    CartProductData(
+                                                        productId = "9223372036854775807",
+                                                        name = "asdf",
+                                                        price = 4.0,
+                                                        quantity = 2,
+                                                        totalPrice = 8.0,
+                                                        bundleGroupId = "",
+                                                        addonItems = listOf(
+                                                            CartAddOnData(
+                                                                name = "gift",
+                                                                price = 6.0,
+                                                                quantity = 2,
+                                                                totalPrice = 12.0
+                                                            ),
+                                                            CartAddOnData(
+                                                                name = "addon1",
+                                                                price = 21.0,
+                                                                quantity = 2,
+                                                                totalPrice = 42.0
+                                                            ),
+                                                            CartAddOnData(
+                                                                name = "addon3",
+                                                                price = 23.0,
+                                                                quantity = 2,
+                                                                totalPrice = 46.0
+                                                            ),
+                                                            CartAddOnData(
+                                                                name = "addon4",
+                                                                price = 24.0,
+                                                                quantity = 1,
+                                                                totalPrice = 24.0
+                                                            )
+                                                        ),
+                                                        category = CartProductCategoryData(
+                                                            id = "12",
+                                                            name = "asdfasdf",
+                                                            identifier = "12asdf"
+                                                        )
+                                                    )
+                                                ),
+                                                bundle = emptyList(),
+                                                addonItems = emptyList(),
+                                                cartStringOrder = "123"
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                ),
+                promoDetail = PromoDetail(
+                    benefitSummaryInfo = BenefitSummaryInfoData(),
+                    voucherOrders = emptyList(),
+                    additionalInfo = AdditionalInfoData()
+                )
+            ),
+            paymentRequest
+        )
+    }
+
+    @Test
+    fun generatePaymentRequestWithBO() {
+        // Given
+        orderSummaryPageViewModel.orderTotal.value = OrderTotal(
+            buttonState = OccButtonState.NORMAL,
+            orderCost = OrderCost(totalPriceWithoutPaymentFees = 1.1)
+        )
+        orderSummaryPageViewModel.orderShop.value =
+            OrderShop(shopId = "234", warehouseId = "345", shopTier = 456)
+        orderSummaryPageViewModel.orderCart =
+            OrderCart(cartString = "123", shop = orderSummaryPageViewModel.orderShop.value)
+        orderSummaryPageViewModel.orderProducts.value = listOf(
+            OrderProduct(
+                productId = Long.MAX_VALUE.toString(),
+                productName = "asdf",
+                finalPrice = 4.0,
+                orderQuantity = 2,
+                categoryId = "12",
+                lastLevelCategory = "asdfasdf",
+                categoryIdentifier = "12asdf",
+                addOn = AddOnGiftingDataModel(
+                    status = 1,
+                    addOnsDataItemModelList = emptyList(),
+                    addOnsButtonModel = AddOnGiftingButtonModel(
+                        title = "gift"
+                    )
+                )
+            ),
+            OrderProduct(
+                isError = true,
+                productId = Long.MIN_VALUE.toString(),
+                productName = "qwer",
+                finalPrice = 3.0,
+                orderQuantity = 1,
+                categoryId = "23",
+                lastLevelCategory = "qwerqwer",
+                categoryIdentifier = "23qwer"
+            )
+        )
+        orderSummaryPageViewModel.orderProfile.value = OrderProfile(
+            address = OrderProfileAddress(
+                addressId = "1",
+                addressStreet = "a",
+                provinceName = "b",
+                cityName = "c",
+                country = "d",
+                postalCode = "3"
+            )
+        )
+        orderSummaryPageViewModel.orderShipment.value = OrderShipment(
+            shipperProductId = 1,
+            shippingPrice = 10,
+            serviceName = "durasi (1 hari)",
+            shipperName = "kirimin",
+            shippingEta = "1 hari lagi",
+            insurance = OrderInsurance(
+                insuranceData = InsuranceData(
+                    insurancePrice = 3.0
+                ),
+                isCheckInsurance = false
+            ),
+            isApplyLogisticPromo = true,
+            logisticPromoViewModel = LogisticPromoUiModel(
+                shippingRate = 11,
+                title = "BebasOngkir",
+                shipperName = "BOKurir",
+                etaData = EstimatedTimeArrivalPromo(textEta = "2 hari")
+            ),
+            logisticPromoShipping = ShippingCourierUiModel(
+                productData = ProductData(
+                    shipperProductId = 2
+                )
+            )
+        )
+        orderSummaryPageViewModel.orderPayment.value = OrderPayment(
+            gatewayCode = "gatewayC",
+            creditCard = OrderPaymentCreditCard(
+                additionalData = OrderPaymentCreditCardAdditionalData(
+                    profileCode = "profileC"
+                )
+            )
+        )
+
+        // When
+        val paymentRequest =
+            orderSummaryPageViewModel.generatePaymentRequest(orderSummaryPageViewModel.orderTotal.value.orderCost)
+
+        // Then
+        assertEquals(
+            PaymentRequest(
+                payment = PaymentData(
+                    profileCode = "profileC",
+                    gatewayCode = "gatewayC",
+                    paymentAmount = 1.1
+                ),
+                cartDetail = CartDetail(
+                    cart = CartData(
+                        data = listOf(
+                            CartDetailData(
+                                address = CartAddressData(
+                                    id = "1",
+                                    address = "a",
+                                    state = "b",
+                                    city = "c",
+                                    country = "d",
+                                    postalCode = "3"
+                                ),
+                                groupOrders = listOf(
+                                    CartGroupData(
+                                        cartStringGroup = "123",
+                                        shippingInfo = CartShippingInfoData(
+                                            spId = "2",
+                                            originalShippingPrice = 11.0,
+                                            serviceName = "BebasOngkir",
+                                            shipperName = "BOKurir",
+                                            eta = "2 hari",
+                                            insurancePrice = 0.0
                                         ),
                                         shopOrders = listOf(
                                             CartShopOrderData(
