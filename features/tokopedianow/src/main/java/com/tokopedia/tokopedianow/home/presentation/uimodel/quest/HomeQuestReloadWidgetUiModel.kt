24b320b934f586ddb24f78aac5a83b699efc9b33
rezagama <rezagamahidayat@gmail.com>
2023-11-09 11:06:58 +0700

    Change to data class

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/uimodel/quest/HomeQuestReloadWidgetUiModel.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/home/presentation/uimodel/quest/HomeQuestReloadWidgetUiModel.kt
@@ -3,7 +3,7 @@ package com.tokopedia.tokopedianow.home.presentation.uimodel.quest
 import com.tokopedia.tokopedianow.home.presentation.adapter.HomeTypeFactory
 import com.tokopedia.tokopedianow.home.presentation.uimodel.HomeLayoutUiModel
 
-class HomeQuestReloadWidgetUiModel (
+data class HomeQuestReloadWidgetUiModel(
     val id: String,
     val mainTitle: String,
     val finishedWidgetTitle: String,
