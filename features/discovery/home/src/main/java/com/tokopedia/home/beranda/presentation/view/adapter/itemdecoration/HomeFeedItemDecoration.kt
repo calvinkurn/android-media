53fd2ce08323104f2b6af39a399cfe581fe71d3d
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-11-20 09:28:40 +0700

    update for you padding

--- features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/HomeFeedItemDecoration.kt
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/adapter/itemdecoration/HomeFeedItemDecoration.kt
@@ -2,49 +2,89 @@ package com.tokopedia.home.beranda.presentation.view.adapter.itemdecoration
 
 import android.graphics.Rect
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import android.view.View
-
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeBannerFeedViewHolder
+import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeRecommendationBannerTopAdsOldViewHolder
+import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeRecommendationBannerTopAdsViewHolder
 import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeRecommendationItemGridViewHolder
+import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeRecommendationItemListViewHolder
 import com.tokopedia.home.beranda.presentation.view.adapter.viewholder.static_channel.recommendation.HomeRecommendationPlayWidgetViewHolder
+import com.tokopedia.kotlin.extensions.view.orZero
 import com.tokopedia.recommendation_widget_common.widget.entitycard.viewholder.RecomEntityCardViewHolder
+import com.tokopedia.home.R as homeR
 
-class HomeFeedItemDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
+class HomeFeedItemDecoration : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect,
                                 view: View,
                                 parent: RecyclerView,
                                 state: RecyclerView.State) {
-
         val position = parent.getChildAdapterPosition(view)
         val spanIndex = (view.layoutParams as StaggeredGridLayoutManager.LayoutParams).spanIndex
+        val isFullSpan = (view.layoutParams as StaggeredGridLayoutManager.LayoutParams).isFullSpan
 
-        if (isTopProductItem(position)) {
-            outRect.top = spacing * 2
-        }
-
-        if (!isProductItem(parent, position)) {
-            return
-        }
-
-        outRect.left = spacing
-        outRect.right = spacing
-
-        outRect.bottom = spacing * 2
-    }
-
-    private fun isProductItem(parent: RecyclerView, viewPosition: Int): Boolean {
-        val adapter = parent.adapter
-        return if (viewPosition < 0 || viewPosition > adapter!!.itemCount - 1) {
-            false
+        if(isSingleItem(parent)) {
+            val outerPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_outer_padding)
+            outRect.left = outerPadding
+            outRect.right = outerPadding
+            outRect.top = 0
+            outRect.bottom = 0
         } else {
-            adapter.getItemViewType(viewPosition) == HomeRecommendationItemGridViewHolder.LAYOUT ||
-                adapter.getItemViewType(viewPosition) == HomeRecommendationPlayWidgetViewHolder.LAYOUT ||
-                adapter.getItemViewType(viewPosition) == RecomEntityCardViewHolder.LAYOUT
+            val innerVerticalPadding: Int
+            val innerHorizontalPadding: Int
+            val outerPadding: Int
+            if(hasInternalPadding(parent, position)) {
+                innerVerticalPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_inner_vertical_padding_with_internal_padding)/2
+                innerHorizontalPadding = 0
+                outerPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_outer_padding_with_internal_padding)
+            }
+            else {
+                innerVerticalPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_inner_vertical_padding)/2
+                innerHorizontalPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_inner_horizontal_padding)/2
+                outerPadding = view.context.resources.getDimensionPixelSize(homeR.dimen.for_you_outer_padding)
+            }
+
+            if(isFullSpan) {
+                outRect.top = 0
+                outRect.bottom = 0
+                outRect.left = outerPadding
+                outRect.right = outerPadding
+            } else {
+                if(spanIndex == 0) {
+                    outRect.left = outerPadding
+                    outRect.right = innerHorizontalPadding
+                } else if(spanIndex == 1) {
+                    outRect.left = innerHorizontalPadding
+                    outRect.right = outerPadding
+                }
+
+                if(position < 2) {
+                    outRect.top = 0
+                    outRect.bottom = innerVerticalPadding
+                } else {
+                    outRect.top = innerVerticalPadding
+                    outRect.bottom = innerVerticalPadding
+                }
+            }
         }
     }
 
-    private fun isTopProductItem(viewPosition: Int): Boolean {
-        return viewPosition <= 1
+    private fun hasInternalPadding(parent: RecyclerView, viewPosition: Int): Boolean {
+        val adapter = parent.adapter
+        return when(adapter?.getItemViewType(viewPosition)) {
+            HomeRecommendationItemGridViewHolder.LAYOUT,
+            HomeRecommendationItemListViewHolder.LAYOUT,
+            RecomEntityCardViewHolder.LAYOUT,
+            HomeBannerFeedViewHolder.LAYOUT,
+            HomeRecommendationBannerTopAdsOldViewHolder.LAYOUT,
+            HomeRecommendationBannerTopAdsViewHolder.LAYOUT,
+            HomeRecommendationPlayWidgetViewHolder.LAYOUT -> true
+            else -> false
+        }
+    }
+
+    private fun isSingleItem(parent: RecyclerView): Boolean {
+        return parent.adapter?.itemCount == 1
     }
 }
