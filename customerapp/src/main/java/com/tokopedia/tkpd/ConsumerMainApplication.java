9bb0a0959b5e3eaeedc72655ddce3cd46987897a
rizqiaryansa <rizqi.aryansa@tokopedia.com>
2023-11-21 14:57:59 +0700

    fix: create appdownloadmanager and appdownloadmanager_noinstall modules

--- customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
+++ customerapp/src/main/java/com/tokopedia/tkpd/ConsumerMainApplication.java
@@ -59,6 +59,7 @@ public class ConsumerMainApplication extends com.tokopedia.tkpd.app.ConsumerMain
         GlobalConfig.DEBUG = BuildConfig.DEBUG;
         GlobalConfig.ENABLE_DISTRIBUTION = BuildConfig.ENABLE_DISTRIBUTION;
         GlobalConfig.ENABLE_MACROBENCHMARK_UTIL = BuildConfig.ENABLE_MACROBENCHMARK_UTIL;
+        GlobalConfig.IS_NAKAMA_VERSION = BuildConfig.IS_NAKAMA_VERSION;
         com.tokopedia.config.GlobalConfig.DEBUG = BuildConfig.DEBUG;
         com.tokopedia.config.GlobalConfig.ENABLE_DISTRIBUTION = BuildConfig.ENABLE_DISTRIBUTION;
         com.tokopedia.config.GlobalConfig.IS_PREINSTALL = BuildConfig.IS_PREINSTALL;
