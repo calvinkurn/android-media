95ecc3c5423bd2114b3dddb991644b3ee2da361f
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2023-11-21 14:38:56 +0700

    fix mix left, lego, best seller, categoryV2, FST, featured shop, campaign_featuring

--- features/discovery/home_component/src/main/java/com/tokopedia/home_component/viewholders/SpecialReleaseViewHolder.kt
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/viewholders/SpecialReleaseViewHolder.kt
@@ -97,8 +97,8 @@ class SpecialReleaseViewHolder(
                 itemView.context
             ) || isUsingPadding
         ) {
-            val specialReleaseRvPaddingStyleBottom = itemView.resources.getDimensionPixelSize(home_componentR.dimen.special_release_rv_padding_style_bottom)
-            binding?.homeComponentSpecialReleaseRv?.translationY = itemView.context.resources.getDimensionPixelSize(R.dimen.special_release_padding_card_compat_padding).toFloat()
+            val specialReleaseRvPaddingStyleBottom = itemView.resources.getDimensionPixelSize(home_componentR.dimen.home_component_padding_bottom_with_compat_padding_translated)
+            binding?.homeComponentSpecialReleaseRv?.translationY = itemView.context.resources.getDimensionPixelSize(R.dimen.home_component_card_compat_padding_translation_y).toFloat()
             binding?.homeComponentSpecialReleaseRv?.setPadding(
                 Int.ZERO,
                 Int.ZERO,
@@ -107,7 +107,7 @@ class SpecialReleaseViewHolder(
             )
             binding?.background?.gone()
         } else {
-            val specialReleaseRvPadding = itemView.resources.getDimensionPixelSize(home_componentR.dimen.special_release_rv_padding)
+            val specialReleaseRvPadding = itemView.resources.getDimensionPixelSize(home_componentR.dimen.home_component_padding_bottom_with_compat_padding)
             binding?.homeComponentSpecialReleaseRv?.translationY = Float.ZERO
             binding?.homeComponentSpecialReleaseRv?.setPadding(
                 Int.ZERO,
