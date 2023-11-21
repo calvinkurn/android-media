d7e3fbbcd53443b7b1710f8f962ae6242e0ad945
fajarca <fajar.azhar@tokopedia.com>
2023-11-21 15:33:28 +0700

    Update tracker constant

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/model/ShopInfoPageTracker.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/data/model/ShopInfoPageTracker.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.shop.common.data.model
 
+import com.tokopedia.shop.analytic.ShopPageTrackingConstant
 import com.tokopedia.shop.info.domain.entity.ShopNote
 import com.tokopedia.track.builder.Tracker
 import com.tokopedia.user.session.UserSessionInterface
@@ -13,13 +14,22 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
         val isLoggedIn = if (userSession.isLoggedIn) "true" else "false"
 
         Tracker.Builder()
-            .setEvent("openScreen")
-            .setCustomProperty("trackerId", "48699")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
-            .setCustomProperty("isLoggedInStatus", isLoggedIn)
-            .setCustomProperty("pageType", "/shoppage - info page")
-            .setCustomProperty("screenName", "/shoppage - info page")
+            .setEvent(ShopPageTrackingConstant.Event.OPEN_SCREEN)
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_OPEN_SHOP_INFO_SCREEN_IMPRESSION
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
+            .setCustomProperty(ShopPageTrackingConstant.IS_LOGGED_IN_STATUS, isLoggedIn)
+            .setCustomProperty(
+                ShopPageTrackingConstant.PAGE_TYPE,
+                ShopPageTrackingConstant.PageType.SHOP_INFO
+            )
+            .setCustomProperty(
+                ShopPageTrackingConstant.SCREEN_NAME,
+                ShopPageTrackingConstant.PageType.SHOP_INFO
+            )
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
@@ -30,13 +40,16 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
     // Tracker ID: 48700
     fun sendCtaExpandPharmacyInformationEvent(shopId: String) {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("shop info - click expand information")
-            .setEventCategory("shop page - buyer")
+            .setEvent(ShopPageTrackingConstant.CLICK_PG)
+            .setEventAction(ShopPageTrackingConstant.EventAction.CLICK_CTA_EXPAND_PHARMACY_INFORMATION)
+            .setEventCategory(ShopPageTrackingConstant.SHOP_PAGE_BUYER)
             .setEventLabel("")
-            .setCustomProperty("trackerId", "48700")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_CTA_EXPAND_PHARMACY_INFO_CLICK
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
@@ -47,13 +60,16 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
     // Tracker ID: 48701
     fun sendCtaViewPharmacyLocationEvent(shopId: String) {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("shop info - click lihat lokasi")
-            .setEventCategory("shop page - buyer")
+            .setEvent(ShopPageTrackingConstant.CLICK_PG)
+            .setEventAction(ShopPageTrackingConstant.EventAction.CLICK_CTA_PHARMACY_LOCATION)
+            .setEventCategory(ShopPageTrackingConstant.SHOP_PAGE_BUYER)
             .setEventLabel("")
-            .setCustomProperty("trackerId", "48701")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_CTA_VIEW_PHARMACY_LOCATION_CLICK
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
@@ -64,13 +80,16 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
     // Tracker ID: 48702
     fun sendIconViewAllShopReviewEvent(shopId: String) {
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("shop info - click cta all reviews")
-            .setEventCategory("shop page - buyer")
+            .setEvent(ShopPageTrackingConstant.CLICK_PG)
+            .setEventAction(ShopPageTrackingConstant.EventAction.CLICK_ICON_SHOP_REVIEW)
+            .setEventCategory(ShopPageTrackingConstant.SHOP_PAGE_BUYER)
             .setEventLabel("")
-            .setCustomProperty("trackerId", "48702")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_ICON_VIEW_ALL_SHOP_REVIEW_CLICK
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
@@ -81,13 +100,16 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
     // Tracker ID: 48703
     fun sendReviewImpression(reviewIndex: Int, shopId: String) {
         Tracker.Builder()
-            .setEvent("viewPGIris")
-            .setEventAction("shop info - impression review highlight")
-            .setEventCategory("shop page - buyer")
+            .setEvent(ShopPageTrackingConstant.Event.VIEW_PG_IRIS)
+            .setEventAction(ShopPageTrackingConstant.EventAction.IMPRESSION_SHOP_REVIEW)
+            .setEventCategory(ShopPageTrackingConstant.SHOP_PAGE_BUYER)
             .setEventLabel(reviewIndex.toString())
-            .setCustomProperty("trackerId", "48703")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_SHOP_REVIEW_IMPRESSION
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
@@ -99,16 +121,19 @@ class ShopInfoPageTracker @Inject constructor(private val userSession: UserSessi
     fun sendClickShopNoteEvent(shopNote: ShopNote, shopId: String) {
         val shopNoteId = shopNote.id
         val templateId = 0
-        val eventLabel = "$shopNoteId - $templateId" 
-        
+        val eventLabel = "$shopNoteId - $templateId"
+
         Tracker.Builder()
-            .setEvent("clickPG")
-            .setEventAction("shop info - click shop notes")
-            .setEventCategory("shop page - buyer")
+            .setEvent(ShopPageTrackingConstant.CLICK_PG)
+            .setEventAction(ShopPageTrackingConstant.EventAction.CLICK_SHOP_NOTE)
+            .setEventCategory(ShopPageTrackingConstant.SHOP_PAGE_BUYER)
             .setEventLabel(eventLabel)
-            .setCustomProperty("trackerId", "48704")
-            .setBusinessUnit("Physical Goods")
-            .setCurrentSite("tokopediamarketplace")
+            .setCustomProperty(
+                ShopPageTrackingConstant.TRACKER_ID,
+                ShopPageTrackingConstant.TrackerId.TRACKER_ID_SHOP_NOTE_CLICK
+            )
+            .setBusinessUnit(ShopPageTrackingConstant.PHYSICAL_GOODS_PASCAL_CASE)
+            .setCurrentSite(ShopPageTrackingConstant.TOKOPEDIA_MARKETPLACE)
             .setShopId(shopId)
             .setUserId(userSession.userId)
             .build()
