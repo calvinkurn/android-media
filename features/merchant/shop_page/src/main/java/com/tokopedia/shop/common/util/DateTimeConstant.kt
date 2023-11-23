55d7b5be47a08193e1327555f19a7b3f3f10c606
fajarca <fajar.azhar@tokopedia.com>
2023-11-23 10:40:58 +0700

    Fix code review feedback

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/util/DateTimeConstant.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/util/DateTimeConstant.kt
@@ -1,10 +1,6 @@
 package com.tokopedia.shop.common.util
 
 object DateTimeConstant {
-    // Date + Time
-    const val DATE_TIME_DAY_PRECISION = "yyyy-MM-dd"
-    const val DATE_TIME_YEAR_PRECISION = "dd MMM yyyy"
-
     // Time only
     const val TIME_SECOND_PRECISION = "HH:mm:ss"
     const val TIME_MINUTE_PRECISION = "HH:mm"
