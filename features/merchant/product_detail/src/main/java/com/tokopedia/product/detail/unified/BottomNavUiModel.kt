e726bd2e73b7622fcb7f266727cfda11303ac47e
astidhiyaa <asti.anzaria@tokopedia.com>
2023-11-27 17:02:29 +0700

    feat: ui model for bottom nv unified media

---
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/unified/BottomNavUiModel.kt
@@ -0,0 +1,56 @@
+package com.tokopedia.product.detail.unified
+
+/**
+ * @author by astidhiyaa on 27/11/23
+ *
+ * Button State [Ingatkan Saya, +Keranjang, Stok Habis], map product
+ */
+data class BottomNavUiModel (
+    val title: String,
+    val price: Price,
+    val stock: Stock,
+    val buttonState: ButtonState,
+    val variant: Variant, //or just hasVariant
+    val shop: Shop,
+) {
+    sealed interface Price {
+        val price: Double
+    }
+
+    data class NormalPrice(
+        override val price: Double,
+        val priceFmt: String,
+    ) : Price
+
+    data class DiscountedPrice(
+        override val price: Double,
+        val ogPriceFmt: String,
+        val discountedPrice: String,
+        val discountPercentage: String,
+    ) : Price
+
+    sealed interface ButtonState //define
+
+    sealed interface Variant
+
+    object NoVariant : Variant
+    data class WithVariant(val lastSelected: Int) : Variant
+
+    sealed interface Stock {
+        val stock: Int
+    }
+
+    object OutOfStock: Stock {
+        override val stock: Int
+            get() = 0
+    }
+
+    data class Available(override val stock: Int): Stock
+
+    data class Shop (
+        val id: String,
+        val name: String
+    )
+}
+
+//ATC butuh data apa aja?
