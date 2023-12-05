70a54efae902ece4f1d5c6169fe1641186e9da4c
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2023-12-05 16:12:26 +0700

    add new test case

--- features/media/universal-editor/src/androidTest/java/com/tokopedia/editor/ui/EditorTest.kt
+++ features/media/universal-editor/src/androidTest/java/com/tokopedia/editor/ui/EditorTest.kt
@@ -4,16 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents
 import androidx.test.platform.app.InstrumentationRegistry
 import com.tokopedia.applink.internal.ApplinkConstInternalMedia
 import com.tokopedia.editor.common.FakeMainEditorActivity
 import com.tokopedia.picker.common.EXTRA_UNIVERSAL_EDITOR_PARAM
+import com.tokopedia.picker.common.PageSource
 import com.tokopedia.picker.common.UniversalEditorParam
+import org.junit.After
 import org.junit.Before
 
 abstract class EditorTest {
 
     lateinit var mActivity: FakeMainEditorActivity
+
+    var activityScenario: ActivityScenario<FakeMainEditorActivity>? = null
 
     val context: Context = InstrumentationRegistry
         .getInstrumentation()
@@ -26,10 +31,20 @@ abstract class EditorTest {
             .applicationContext
 
     @Before
-    open fun setUp() {}
+    open fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    open fun tearDown() {
+        Intents.release()
+        activityScenario?.close()
+    }
 
     protected fun startActivity(param: UniversalEditorParam.() -> Unit) {
-        startActivity(UniversalEditorParam().apply(param))
+        startActivity(UniversalEditorParam().apply(param).also {
+            it.setPageSource(PageSource.Play)
+        })
     }
 
     private fun startActivity(param: UniversalEditorParam) {
@@ -38,7 +53,7 @@ abstract class EditorTest {
         }
 
         intent.putExtra(EXTRA_UNIVERSAL_EDITOR_PARAM, param)
-        ActivityScenario.launch<FakeMainEditorActivity>(intent)
+        activityScenario = ActivityScenario.launch(intent)
     }
 
     private fun createUriIntent(): Uri.Builder {
--- features/media/universal-editor/src/androidTest/java/com/tokopedia/editor/ui/main/MainEditorTest.kt
+++ features/media/universal-editor/src/androidTest/java/com/tokopedia/editor/ui/main/MainEditorTest.kt
@@ -1,13 +1,20 @@
 package com.tokopedia.editor.ui.main
 
 import android.widget.ImageView
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import com.tokopedia.editor.ui.EditorTest
 import com.tokopedia.editor.util.FileLoader
 import com.tokopedia.editor.R
+import com.tokopedia.editor.data.mapper.NavigationTypeMapper
+import com.tokopedia.editor.ui.placement.PlacementImageActivity
+import com.tokopedia.editor.ui.text.InputTextActivity
 import com.tokopedia.kotlin.extensions.view.getBitmap
+import com.tokopedia.picker.common.types.ToolType
 import com.tokopedia.test.application.annotations.UiTest
 import org.junit.Test
 
@@ -29,16 +36,16 @@ class MainEditorTest : EditorTest() {
     fun should_able_to_set_configurable_toolbar_component() {
         // Given
         val file = FileLoader.imageFile(context)
-        val headerTitle = R.string.universal_editor_nav_bar_image_placement
+        val headerTitle = "CustomCtaText"
 
         // When
         startActivity {
             filePaths(listOf(file.path))
-            setHeaderTitle(headerTitle)
+            custom.setHeaderTitle(headerTitle)
         }
 
         // Then
-        assertTextDisplayedWith(context.getString(headerTitle))
+        assertTextDisplayedWith(headerTitle)
     }
 
     @Test
@@ -86,8 +93,81 @@ class MainEditorTest : EditorTest() {
         assertRecyclerViewDisplayed(R.id.lst_tool)
     }
 
+//    @Test
+//    fun should_open_input_text_page() {
+//        // Given
+//        val file = FileLoader.imageFile(context)
+//        val text = context.getString(NavigationTypeMapper.invoke(ToolType.TEXT).second)
+//
+//        // When
+//        startActivity {
+//            filePaths(listOf(file.path))
+//        }
+//        onView(ViewMatchers.withText(text)).perform(ViewActions.click())
+//
+//        // Then
+//        Thread.sleep(100)
+//        checkIntentCallSuccess(InputTextActivity::class.java.name)
+//    }
+
+    @Test
+    fun should_open_crop_placement_page() {
+        // Given
+        val file = FileLoader.imageFile(context)
+        val text = context.getString(NavigationTypeMapper.invoke(ToolType.PLACEMENT).second)
+
+        // When
+        startActivity {
+            filePaths(listOf(file.path))
+        }
+        onView(ViewMatchers.withText(text)).perform(ViewActions.click())
+
+        // Then
+        Thread.sleep(100)
+        checkIntentCallSuccess(PlacementImageActivity::class.java.name)
+    }
+
+    @Test
+    fun should_able_mute_video() {
+        // Given
+        val file = FileLoader.videoFile(context)
+        val text = context.getString(NavigationTypeMapper.invoke(ToolType.AUDIO_MUTE).second)
+
+        // When
+        startActivity {
+            filePaths(listOf(file.path))
+        }
+        onView(ViewMatchers.withText(text)).perform(ViewActions.click())
+
+        // Then
+        Thread.sleep(100)
+        assertRecyclerViewDisplayed(R.id.ic_audio_state)
+    }
+
+    @Test
+    fun should_render_input_text() {
+        // Given
+        val file = FileLoader.imageFile(context)
+        val actionText = context.getString(NavigationTypeMapper.invoke(ToolType.TEXT).second)
+        val typedText = "Test Input Text"
+
+        // When
+        startActivity {
+            filePaths(listOf(file.path))
+        }
+        onView(ViewMatchers.withText(actionText)).perform(ViewActions.click())
+        Thread.sleep(2000)
+        onView(ViewMatchers.withId(R.id.add_text_input)).perform(ViewActions.typeText(typedText))
+        Thread.sleep(2000)
+        onView(ViewMatchers.withText("Simpan")).perform(ViewActions.click())
+
+        // Then
+        Thread.sleep(2000)
+        assertTextDisplayedWith(typedText)
+    }
+
     private fun onImageView(resId: Int, imgView: (ImageView) -> Unit) {
-        Espresso.onView(ViewMatchers.withId(resId))
+        onView(ViewMatchers.withId(resId))
             .check { view, _ -> imgView(view as ImageView) }
     }
 
@@ -95,7 +175,7 @@ class MainEditorTest : EditorTest() {
         // set waiting for 500ms before assertion
         Thread.sleep(500)
 
-        Espresso.onView(
+        onView(
             ViewMatchers.withText(text)
         ).check(
             ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))
@@ -103,8 +183,12 @@ class MainEditorTest : EditorTest() {
     }
 
     private fun assertRecyclerViewDisplayed(viewId: Int) {
-        Espresso.onView(
+        onView(
             ViewMatchers.withId(viewId)
         ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    private fun checkIntentCallSuccess(className: String) {
+        Intents.intended(IntentMatchers.hasComponent(className))
     }
 }
