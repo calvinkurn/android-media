b73b9e48b4f63098218dd23c00b1945ef81f881d
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-06 13:54:46 +0700

    fix: based on comment

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/usecase/MediaReviewUseCase.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/usecase/MediaReviewUseCase.kt
@@ -1,43 +1,45 @@
 package com.tokopedia.content.product.preview.data.usecase
 
+import com.google.gson.annotations.SerializedName
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
 import com.tokopedia.content.product.preview.data.MediaReviewResponse
+import com.tokopedia.gql_query_annotation.GqlQuery
+import com.tokopedia.gql_query_annotation.GqlQueryInterface
 import com.tokopedia.graphql.coroutines.data.extensions.request
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.GqlParam
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import javax.inject.Inject
 
 /**
  * @author by astidhiyaa on 05/12/23
  */
+@GqlQuery(MediaReviewUseCase.QUERY_NAME, MediaReviewUseCase.QUERY)
 class MediaReviewUseCase @Inject constructor(
     @ApplicationContext private val repo: GraphqlRepository,
     dispatchers: CoroutineDispatchers,
-) : CoroutineUseCase<Map<String, Any>, MediaReviewResponse>(dispatchers.io) {
+) : CoroutineUseCase<MediaReviewUseCase.Param, MediaReviewResponse>(dispatchers.io) {
 
-    override suspend fun execute(params: Map<String, Any>): MediaReviewResponse {
-        return repo.request(graphqlQuery(), params)
+    private val query: GqlQueryInterface = MediaReviewUseCaseQuery()
+
+    override suspend fun execute(params: Param): MediaReviewResponse {
+        return repo.request(query, params)
     }
-
-    override fun graphqlQuery(): String = QUERY
-    fun convertToMap(param: Param) = mapOf(
-        PRODUCT_ID_PARAM to param.productId,
-        PAGE_PARAM to param.page,
-        LIMIT_PARAM to LIMIT_VALUE,
-    )
-
+    override fun graphqlQuery(): String = query.getQuery()
     data class Param(
+        @SerializedName("productID")
         val productId: String,
+        @SerializedName("page")
         val page: Int,
-    )
+        @SerializedName("limit")
+        val limit: Int = LIMIT_VALUE,
+    ) : GqlParam
 
     companion object {
-        private const val PRODUCT_ID_PARAM = "productID"
-        private const val PAGE_PARAM = "page"
-        private const val LIMIT_PARAM = "limit"
         private const val LIMIT_VALUE = 10
-        private const val QUERY = """
+        const val QUERY_NAME = "MediaReviewUseCaseQuery"
+        const val QUERY = """
             query getMediaReview(${'$'}productID: String!, ${'$'}page: Int!, ${'$'}limit: Int!) {
               productrevGetReviewImage(productID: ${'$'}productID, page: ${'$'}page, limit: ${'$'}limit){
                 list {
@@ -84,13 +86,13 @@ class MediaReviewUseCase @Inject constructor(
                     description
                     feedbackID
                   }
-                  video { // array
+                  video { 
                     attachmentID
                     url
                     feedbackID
                   }
-                  imageCountFmt // will be deprecated
-                  imageCount // will be deprecated
+                  imageCountFmt
+                  imageCount 
                   mediaCountFmt
                   mediaCount
                   mediaCountTitle
