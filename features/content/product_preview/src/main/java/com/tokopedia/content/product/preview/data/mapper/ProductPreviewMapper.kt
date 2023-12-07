91095ef020a7ff428e821a04799866e356a28425
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-07 10:22:01 +0700

    feat: map menu

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/mapper/ProductPreviewMapper.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/mapper/ProductPreviewMapper.kt
@@ -1,40 +1,11 @@
 package com.tokopedia.content.product.preview.data.mapper
 
 import com.tokopedia.content.product.preview.data.MediaReviewResponse
-import com.tokopedia.content.product.preview.view.uimodel.AuthorUiModel
-import com.tokopedia.content.product.preview.view.uimodel.DescriptionUiModel
-import com.tokopedia.content.product.preview.view.uimodel.LikeUiState
 import com.tokopedia.content.product.preview.view.uimodel.ReviewUiModel
-import javax.inject.Inject
 
 /**
  * @author by astidhiyaa on 06/12/23
  */
-class ProductPreviewMapper @Inject constructor() {
-    fun map(response: MediaReviewResponse): List<ReviewUiModel> {
-        return response.data.detail.reviewDetail.map {
-            ReviewUiModel(
-                id = it.feedbackId,
-                medias = emptyList(), //TODO: map and sew it later,
-                menus = emptyList(), //TODO: map data
-                likeState = LikeUiState(
-                    count = it.totalLike,
-                    state = if (it.isLiked) LikeUiState.LikeStatus.Like else LikeUiState.LikeStatus.Dislike
-                ),
-                author = AuthorUiModel(
-                    name = it.user.fullName,
-                    type = AuthorUiModel.ReviewerType.Unknown, //TODO: map it later
-                    id = it.user.userId,
-                    avatarUrl = it.user.image,
-                    appLink = it.user.url,
-                ),
-                description = DescriptionUiModel(
-                    stars = it.rating,
-                    productType = it.variantName,
-                    timestamp = it.createTimestamp,
-                    description = it.review,
-                )
-            )
-        }
-    }
+interface ProductPreviewMapper {
+    fun map(response: MediaReviewResponse): List<ReviewUiModel>
 }
--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/di/ProductPreviewBindModule.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/di/ProductPreviewBindModule.kt
@@ -1,5 +1,7 @@
 package com.tokopedia.content.product.preview.di
 
+import com.tokopedia.content.product.preview.data.mapper.ProductPreviewMapper
+import com.tokopedia.content.product.preview.data.mapper.ProductPreviewMapperImpl
 import com.tokopedia.content.product.preview.data.repository.ProductPreviewRepository
 import com.tokopedia.content.product.preview.data.repository.ProductPreviewRepositoryImpl
 import dagger.Binds
@@ -10,4 +12,8 @@ abstract class ProductPreviewBindModule {
     @Binds
     @ProductPreviewScope
     abstract fun bindRepository(repository: ProductPreviewRepositoryImpl): ProductPreviewRepository
+
+    @Binds
+    @ProductPreviewScope
+    abstract fun bindMapper(mapperImpl: ProductPreviewMapperImpl): ProductPreviewMapper
 }
