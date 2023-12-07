cab40b71b0d4ace8d760049e4e31350707174774
firmankagami <firmanda.nugroho@tokopedia.com>
2023-12-07 15:35:14 +0700

    test(umrah): update unit test

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -32,8 +32,8 @@ class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
     companion object {
         // This a reminder to developer.
         // If this size is modified, please also add unit test for the added deeplink.
-        const val SIZE_HOST = 163
-        const val SIZE_PATH = 272
+        const val SIZE_HOST = 161
+        const val SIZE_PATH = 270
     }
 
     override fun setup() {
