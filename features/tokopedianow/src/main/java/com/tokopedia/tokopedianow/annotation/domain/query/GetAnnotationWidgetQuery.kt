2af27b8ced93c7d305b2a8169fd1c1582a3bf76e
rezagama <rezagamahidayat@gmail.com>
2023-12-11 14:48:58 +0700

    Get annotation list query & use case

---
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/domain/query/GetAnnotationWidgetQuery.kt
@@ -0,0 +1,59 @@
+package com.tokopedia.tokopedianow.annotation.domain.query
+
+import com.tokopedia.gql_query_annotation.GqlQueryInterface
+
+internal object GetAnnotationWidgetQuery : GqlQueryInterface {
+
+    const val PARAM_CATEGORY_ID = "categoryID"
+    const val PARAM_WAREHOUSE_IDS = "warehouseIDs"
+    const val PARAM_ANNOTATION_TYPE = "annotationType"
+    const val PARAM_PAGE_SOURCE = "pageSource"
+
+    private const val OPERATION_NAME = "TokonowGetAnnotationList"
+
+    override fun getOperationNameList(): List<String> {
+        return listOf(OPERATION_NAME)
+    }
+
+    override fun getQuery(): String {
+        return """
+            query TokonowGetAnnotationList(
+                ${'$'}$PARAM_CATEGORY_ID: String!, 
+                ${'$'}$PARAM_WAREHOUSE_IDS: String!, 
+                ${'$'}$PARAM_ANNOTATION_TYPE: String!, 
+                ${'$'}$PARAM_PAGE_SOURCE: String!
+            ){
+              TokonowGetAnnotationList(req: {
+                $PARAM_CATEGORY_ID: ${'$'}$PARAM_CATEGORY_ID,
+                $PARAM_WAREHOUSE_IDS: ${'$'}$PARAM_WAREHOUSE_IDS,
+                $PARAM_ANNOTATION_TYPE: ${'$'}$PARAM_ANNOTATION_TYPE,
+              }, $PARAM_PAGE_SOURCE: ${'$'}$PARAM_PAGE_SOURCE){
+                header{
+                  process_time
+                  messages
+                  reason
+                  error_code
+                }
+                status
+                annotationHeader{
+                  title
+                  hasMore
+                  allPageAppLink
+                  allPageWebLink
+                }
+                annotationList{
+                  annotationID
+                  name
+                  imageURL
+                  appLink
+                  webLink
+                }
+              }
+            }
+        """.trimIndent()
+    }
+
+    override fun getTopOperationName(): String {
+        return OPERATION_NAME
+    }
+}
---
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/domain/usecase/GetAnnotationWidgetUseCase.kt
@@ -0,0 +1,49 @@
+package com.tokopedia.tokopedianow.annotation.domain.usecase
+
+import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.network.exception.MessageErrorException
+import com.tokopedia.tokopedianow.annotation.domain.model.TokoNowGetAnnotationListResponse
+import com.tokopedia.tokopedianow.annotation.domain.model.TokoNowGetAnnotationListResponse.GetAnnotationListResponse
+import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery
+import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_ANNOTATION_TYPE
+import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_CATEGORY_ID
+import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_PAGE_SOURCE
+import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_WAREHOUSE_IDS
+import com.tokopedia.usecase.RequestParams
+import javax.inject.Inject
+
+class GetAnnotationWidgetUseCase @Inject constructor(
+    graphqlRepository: GraphqlRepository
+) {
+
+    private val graphql by lazy { GraphqlUseCase<TokoNowGetAnnotationListResponse>(graphqlRepository) }
+
+    suspend fun execute(
+        categoryId: String,
+        warehouseIds: String,
+        annotationType: String,
+        pageSource: String
+    ): GetAnnotationListResponse {
+        graphql.apply {
+            val requestParams = RequestParams().apply {
+                putString(PARAM_CATEGORY_ID, categoryId)
+                putString(PARAM_WAREHOUSE_IDS, warehouseIds)
+                putString(PARAM_ANNOTATION_TYPE, annotationType)
+                putString(PARAM_PAGE_SOURCE, pageSource)
+            }.parameters
+
+            setGraphqlQuery(GetAnnotationWidgetQuery)
+            setTypeClass(TokoNowGetAnnotationListResponse::class.java)
+            setRequestParams(requestParams)
+        }
+        val response = graphql.executeOnBackground().response
+        val messages = response.header.messages
+
+        return if (messages.isNotEmpty()) {
+            throw MessageErrorException(messages.first())
+        } else {
+            response
+        }
+    }
+}
