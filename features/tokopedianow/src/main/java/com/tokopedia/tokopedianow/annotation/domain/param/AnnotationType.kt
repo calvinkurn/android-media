43c51a5092620be83e67c4fb1a7fda175d618cff
rezagama <rezagamahidayat@gmail.com>
2023-12-12 11:10:06 +0700

    AnnotationType enum

---
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/domain/param/AnnotationType.kt
@@ -0,0 +1,5 @@
+package com.tokopedia.tokopedianow.annotation.domain.param
+
+enum class AnnotationType {
+    BRAND
+}
--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/domain/usecase/GetAnnotationWidgetUseCase.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/domain/usecase/GetAnnotationWidgetUseCase.kt
@@ -5,6 +5,7 @@ import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.network.exception.MessageErrorException
 import com.tokopedia.tokopedianow.annotation.domain.model.TokoNowGetAnnotationListResponse
 import com.tokopedia.tokopedianow.annotation.domain.model.TokoNowGetAnnotationListResponse.GetAnnotationListResponse
+import com.tokopedia.tokopedianow.annotation.domain.param.AnnotationType
 import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery
 import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_ANNOTATION_TYPE
 import com.tokopedia.tokopedianow.annotation.domain.query.GetAnnotationWidgetQuery.PARAM_CATEGORY_ID
@@ -22,14 +23,14 @@ class GetAnnotationWidgetUseCase @Inject constructor(
     suspend fun execute(
         categoryId: String,
         warehouseIds: String,
-        annotationType: String,
+        annotationType: AnnotationType,
         pageSource: String
     ): GetAnnotationListResponse {
         graphql.apply {
             val requestParams = RequestParams().apply {
                 putString(PARAM_CATEGORY_ID, categoryId)
                 putString(PARAM_WAREHOUSE_IDS, warehouseIds)
-                putString(PARAM_ANNOTATION_TYPE, annotationType)
+                putString(PARAM_ANNOTATION_TYPE, annotationType.name)
                 putString(PARAM_PAGE_SOURCE, pageSource)
             }.parameters
 
