b41251d599f566fd90bb5069e8d8e266472e61e9
Felicia Widjaja <felicia.widjaja@tokopedia.com>
2023-12-13 17:26:20 +0700

    add timber log to fix sonodabots review

--- features/transaction/unifyorderhistory/src/main/java/com/tokopedia/unifyorderhistory/util/UohRollenceUtil.kt
+++ features/transaction/unifyorderhistory/src/main/java/com/tokopedia/unifyorderhistory/util/UohRollenceUtil.kt
@@ -2,6 +2,7 @@ package com.tokopedia.unifyorderhistory.util
 
 import com.tokopedia.remoteconfig.RemoteConfigInstance
 import com.tokopedia.remoteconfig.RollenceKey
+import timber.log.Timber
 
 object UohRollenceUtil {
 
@@ -19,6 +20,7 @@ object UohRollenceUtil {
             val remoteConfigRollenceValue = RemoteConfigInstance.getInstance().abTestPlatform.getString(RollenceKey.UOH_BUY_AGAIN_WIDGET, RollenceKey.UOH_BUY_AGAIN_WIDGET_CONTROL)
             return (remoteConfigRollenceValue == RollenceKey.UOH_BUY_AGAIN_WIDGET_VARIANT)
         } catch (e: Exception) {
+            Timber.d(e.message)
             false
         }
     }
