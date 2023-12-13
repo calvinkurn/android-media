433c9b8b1bb23836b3514136557b3ac1b81db0a9
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-13 13:51:13 +0700

    feat: change to interface

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/uimodel/ProductPreviewAction.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/uimodel/ProductPreviewAction.kt
@@ -3,6 +3,6 @@ package com.tokopedia.content.product.preview.view.uimodel
 /**
  * @author by astidhiyaa on 12/12/23
  */
-sealed class ProductPreviewAction {
-    object FetchReview: ProductPreviewAction()
+sealed interface ProductPreviewAction {
+    object FetchReview: ProductPreviewAction
 }
