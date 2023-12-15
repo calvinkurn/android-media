4ec50e7449204197fe4763bac0571fe372405378
astidhiyaa <asti.anzaria@tokopedia.com>
2023-12-15 10:35:57 +0700

    fix: dont build menu inside review~~

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/mapper/ProductPreviewMapper.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/data/mapper/ProductPreviewMapper.kt
@@ -1,27 +1,23 @@
 package com.tokopedia.content.product.preview.data.mapper
 
-import com.tokopedia.content.common.report_content.model.ContentMenuIdentifier
-import com.tokopedia.content.common.report_content.model.ContentMenuItem
-import com.tokopedia.content.product.preview.R
 import com.tokopedia.content.product.preview.data.MediaReviewResponse
 import com.tokopedia.content.product.preview.view.uimodel.AuthorUiModel
 import com.tokopedia.content.product.preview.view.uimodel.DescriptionUiModel
 import com.tokopedia.content.product.preview.view.uimodel.LikeUiState
+import com.tokopedia.content.product.preview.view.uimodel.MenuStatus
 import com.tokopedia.content.product.preview.view.uimodel.ReviewUiModel
-import com.tokopedia.iconunify.IconUnify
-import com.tokopedia.user.session.UserSessionInterface
 import javax.inject.Inject
 
 /**
  * @author by astidhiyaa on 06/12/23
  */
-class ProductPreviewMapper @Inject constructor(private val userSession: UserSessionInterface) {
+class ProductPreviewMapper @Inject constructor() {
     fun mapReviews(response: MediaReviewResponse): List<ReviewUiModel> {
         return response.data.review.map {
             ReviewUiModel(
                 reviewId = it.feedbackId,
                 medias = emptyList(), //TODO: map and sew it later,
-                menus = buildMenu(it),
+                menus = MenuStatus(isReportable = it.isReportable),
                 likeState = LikeUiState(
                     count = it.likeStats.totalLike,
                     state = LikeUiState.LikeStatus.getByValue(it.likeStats.likeStatus),
@@ -42,26 +38,4 @@ class ProductPreviewMapper @Inject constructor(private val userSession: UserSess
             )
         }
     }
-
-    private fun buildMenu(detail: MediaReviewResponse.ReviewDetail) =
-        buildList {
-            add(
-                ContentMenuItem(
-                    iconUnify = IconUnify.VISIBILITY,
-                    name = R.string.product_prev_menu_opt_watch,
-                    type = ContentMenuIdentifier.WatchMode
-                )
-            )
-            if (detail.isReportable && !isOwner(detail.user))
-                add(
-                    ContentMenuItem(
-                        iconUnify = IconUnify.WARNING,
-                        name = com.tokopedia.content.common.R.string.content_common_menu_report,
-                        type = ContentMenuIdentifier.Report
-                    )
-                )
-        }
-
-    private fun isOwner(author: MediaReviewResponse.ReviewerUserInfo): Boolean =
-        author.userId == userSession.userId
 }
