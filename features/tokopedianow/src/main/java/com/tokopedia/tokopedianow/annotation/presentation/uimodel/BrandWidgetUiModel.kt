5b5c5bb9db21fd0ba1c19d4d333bfac4116c6e4a
rezagama <rezagamahidayat@gmail.com>
2023-12-18 14:17:31 +0700

    Change items & state type

--- features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/presentation/uimodel/BrandWidgetUiModel.kt
+++ features/tokopedianow/src/main/java/com/tokopedia/tokopedianow/annotation/presentation/uimodel/BrandWidgetUiModel.kt
@@ -2,15 +2,20 @@ package com.tokopedia.tokopedianow.annotation.presentation.uimodel
 
 import com.tokopedia.abstraction.base.view.adapter.Visitable
 import com.tokopedia.tokopedianow.annotation.presentation.adapter.typefactory.BrandWidgetTypeFactory
-import com.tokopedia.tokopedianow.common.constant.TokoNowLayoutState
 import com.tokopedia.tokopedianow.common.model.TokoNowDynamicHeaderUiModel
 
 data class BrandWidgetUiModel(
     val id: String = "",
     val header: TokoNowDynamicHeaderUiModel = TokoNowDynamicHeaderUiModel(),
-    val brandList: List<BrandWidgetItemUiModel> = emptyList(),
-    @TokoNowLayoutState val state: Int = TokoNowLayoutState.LOADING
-): Visitable<BrandWidgetTypeFactory> {
+    val items: List<Visitable<*>> = emptyList(),
+    val state: BrandWidgetState = BrandWidgetState.LOADING
+) : Visitable<BrandWidgetTypeFactory> {
+
+    enum class BrandWidgetState {
+        LOADING,
+        LOADED,
+        ERROR
+    }
 
     override fun type(typeFactory: BrandWidgetTypeFactory): Int {
         return typeFactory.type(this)
