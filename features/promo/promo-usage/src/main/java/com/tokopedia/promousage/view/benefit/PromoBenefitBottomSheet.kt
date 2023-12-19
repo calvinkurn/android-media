182b150e1164ecabbd5984060dbf4e4dbdd5630c
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2023-12-19 17:55:29 +0700

    Styling card layout

--- features/promo/promo-usage/src/main/java/com/tokopedia/promousage/view/benefit/PromoBenefitBottomSheet.kt
+++ features/promo/promo-usage/src/main/java/com/tokopedia/promousage/view/benefit/PromoBenefitBottomSheet.kt
@@ -1,12 +1,16 @@
 package com.tokopedia.promousage.view.benefit
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
 import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.tokopedia.promousage.databinding.PromoBenefitBottomsheetBinding
 import com.tokopedia.utils.lifecycle.autoClearedNullable
@@ -35,22 +39,36 @@ class PromoBenefitBottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val gradientDrawable = GradientDrawable().apply {
-            shape = GradientDrawable.RECTANGLE
-            cornerRadii =
-                floatArrayOf(20f, 20f, 20f, 20f, 0f, 0f, 0f, 0f) // Top corners are rounded
-            colors = intArrayOf(
-                0xFFE53935.toInt(),
-                0xFFFFCDD2.toInt(),
-            ) // Red to Orange to White
-            orientation = GradientDrawable.Orientation.BL_TR // 45-degree angle
-        }
+//        val gradientDrawable = GradientDrawable().apply {
+//            shape = GradientDrawable.RECTANGLE
+//            cornerRadii =
+//                floatArrayOf(20f, 20f, 20f, 20f, 0f, 0f, 0f, 0f) // Top corners are rounded
+//            colors = intArrayOf(
+//                0xFFE53935.toInt(),
+//                0xFFFFCDD2.toInt(),
+//            ) // Red to Orange to White
+//            orientation = GradientDrawable.Orientation.BL_TR // 45-degree angle
+//        }
         binding?.run {
             val frameDialogView = container.parent as FrameLayout
             frameDialogView.setBackgroundColor(Color.TRANSPARENT)
             frameDialogView.bringToFront()
 
-            topSection.background = gradientDrawable
+            val drawable = ContextCompat.getDrawable(
+                requireContext(),
+                com.tokopedia.unifycomponents.R.drawable.bottomsheet_background
+            )
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                drawable?.colorFilter = BlendModeColorFilter(
+                    Color.parseColor("#FFF5F6"), BlendMode.SRC_ATOP
+                )
+            } else {
+                drawable?.setColorFilter(
+                    Color.parseColor("#FFF5F6"),
+                    PorterDuff.Mode.SRC_ATOP
+                )
+            }
+            topSection.background = drawable
         }
     }
 }
