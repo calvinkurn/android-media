cd367b054e7e4af900d593f0ddc36089a425c129
isfaaghyth <isfaaghyth@gmail.com>
2023-12-21 10:14:06 +0700

    feat: remove unused util files

--- features/media/universal-editor/src/main/java/com/tokopedia/editor/util/Extensions.kt
+++ features/media/universal-editor/src/main/java/com/tokopedia/editor/util/Extensions.kt
@@ -1,18 +1,7 @@
 package com.tokopedia.editor.util
 
-import android.content.Context
-import com.google.android.play.core.splitinstall.SplitInstallHelper
 import kotlinx.coroutines.flow.MutableStateFlow
-import timber.log.Timber
 
 internal fun <T: Any> MutableStateFlow<T>.setValue(fn: T.() -> T) {
     value = value.fn()
-}
-
-internal fun safeLoadNativeLibrary(context: Context, name: String) {
-    try {
-        SplitInstallHelper.loadLibrary(context, name)
-    } catch (t: Throwable) {
-        Timber.d("media-editor: $t")
-    }
 }
