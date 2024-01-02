deb62d918503ffb17fabfc9fd158e2c9ee4c338d
hendry.setiadi <hendry.setiadi@tokopedia.com>
2024-01-02 15:43:11 +0700

    fix server log

--- features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/ViewServerLogViewHolder.kt
+++ features/common/developer_options/src/main/java/com/tokopedia/developer_options/presentation/viewholder/ViewServerLogViewHolder.kt
@@ -3,23 +3,21 @@ package com.tokopedia.developer_options.presentation.viewholder
 import android.view.View
 import androidx.annotation.LayoutRes
 import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
-import com.tokopedia.analyticsdebugger.debugger.IrisLogger
 import com.tokopedia.analyticsdebugger.debugger.ServerLogLogger
 import com.tokopedia.developer_options.R
-import com.tokopedia.developer_options.presentation.model.SystemNonSystemAppsUiModel
-import com.tokopedia.developer_options.presentation.model.ViewIrisLogUiModel
+import com.tokopedia.developer_options.presentation.model.ViewServerLogUiModel
 import com.tokopedia.unifycomponents.UnifyButton
 
 class ViewServerLogViewHolder(
     itemView: View
-): AbstractViewHolder<ViewIrisLogUiModel>(itemView)
+): AbstractViewHolder<ViewServerLogUiModel>(itemView)
 {
     companion object {
         @LayoutRes
         val LAYOUT = R.layout.item_view_server_log
     }
 
-    override fun bind(element: ViewIrisLogUiModel) {
+    override fun bind(element: ViewServerLogUiModel) {
         val btn = itemView.findViewById<UnifyButton>(R.id.view_server_log_btn)
         itemView.context.apply {
             btn.setOnClickListener {
