c57690f8ea6db30dffd3a8594ba5271b3a17ce65
yovi.putra <yovi.putra@tokopedia.com>
2024-01-05 22:00:17 +0700

    `fix`[campaign_revamp] optimize campaignBackgroundColor

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/campaign/ui/widget/component/CampaignBackgroundColor.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/viewholder/campaign/ui/widget/component/CampaignBackgroundColor.kt
@@ -1,11 +1,10 @@
 package com.tokopedia.product.detail.view.viewholder.campaign.ui.widget.component
 
 import androidx.compose.foundation.background
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
@@ -14,24 +13,20 @@ import androidx.compose.ui.graphics.Color
  * Project name: android-unify
  **/
 
-@Stable
-class ImmutableList<T>(val values: List<T> = mutableListOf())
-
 private const val CAMPAIGN_COLOR = 0xFFD72C2C
 
-private fun Modifier.campaignBackgroundColor(colors: ImmutableList<Color>) = this.then(
-    when (colors.values.size) {
+private fun Modifier.campaignBackgroundColor(colors: List<Color>) = this.then(
+    when (colors.size) {
         0 -> background(color = Color(CAMPAIGN_COLOR))
-        1 -> background(color = colors.values.first())
+        1 -> background(color = colors.first())
         else -> {
-            val brush = Brush.horizontalGradient(colors.values)
+            val brush = Brush.horizontalGradient(colors)
             background(brush)
         }
     }
 )
 
-@Composable
-internal fun Modifier.campaignBackgroundColor(colorString: String): Modifier {
+internal fun Modifier.campaignBackgroundColor(colorString: String): Modifier = composed {
     val color = remember(colorString) {
         derivedStateOf {
             val listOfColor = colorString.split(",")
@@ -42,7 +37,7 @@ internal fun Modifier.campaignBackgroundColor(colorString: String): Modifier {
         }
     }
 
-    return campaignBackgroundColor(colors = ImmutableList(color.value))
+    campaignBackgroundColor(colors = color.value)
 }
 
 private fun parseColor(colorString: String): Color = runCatching {
