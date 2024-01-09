4ecf15ff1d05d2c1ecef2b85b5fe0b37da98d4a2
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-01-09 11:00:31 +0700

    Commit changes

--- features/entertainment/deals/src/main/java/com/tokopedia/deals/ui/category/domain/GetChipsCategoryUseCase.kt
+++ features/entertainment/deals/src/main/java/com/tokopedia/deals/ui/category/domain/GetChipsCategoryUseCase.kt
@@ -10,3 +10,52 @@ import com.tokopedia.graphql.data.model.CacheType
 import com.tokopedia.graphql.data.model.GraphqlCacheStrategy
 import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
 import javax.inject.Inject
+
+class GetChipsCategoryUseCase @Inject constructor(
+    @ApplicationContext private val repository: GraphqlRepository,
+    dispatcher: CoroutineDispatchers
+) : CoroutineUseCase<Unit, CuratedData>(dispatcher.io) {
+
+    private fun cacheStrategy() = GraphqlCacheStrategy
+        .Builder(CacheType.CACHE_FIRST)
+        .setExpiryTime(GraphqlConstant.ExpiryTimes.MINUTE_30.`val`()).build()
+
+    override suspend fun execute(params: Unit): CuratedData {
+        val response: CuratedData = repository.request(
+            graphqlQuery(),
+            params,
+            cacheStrategy()
+        )
+        return response
+    }
+
+    override fun graphqlQuery(): String = """
+     {
+        event_child_category(categoryName: "deal", categoryID: 15, verticalID: 2)
+          {
+            categories{
+                id,
+                parent_id,
+                name,
+                title,
+                description,
+                meta_title,
+                meta_description,
+                url,
+                media_url,
+                seo_url,
+                priority,
+                status,
+                message,
+                code,
+                message_error,
+                web_url,
+                app_url,
+                is_card,
+                is_hidden,
+                inactiveMediaUrl,
+            }
+          }
+     }
+    """.trimIndent()
+}
