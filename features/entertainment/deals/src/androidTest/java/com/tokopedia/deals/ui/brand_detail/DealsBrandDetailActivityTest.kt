eb1934edc52bf2c3f50a68b07849505629dea927
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-01-10 09:06:05 +0700

    Improve androidTest

--- features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/ui/brand_detail/DealsBrandDetailActivityTest.kt
+++ features/entertainment/deals/src/androidTest/java/com/tokopedia/deals/ui/brand_detail/DealsBrandDetailActivityTest.kt
@@ -3,20 +3,17 @@ package com.tokopedia.deals.ui.brand_detail
 import android.app.Activity
 import android.app.Instrumentation
 import android.content.Intent
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
 import com.tokopedia.analyticsdebugger.cassava.cassavatest.CassavaTestRule
 import com.tokopedia.analyticsdebugger.cassava.cassavatest.hasAllSuccess
-import com.tokopedia.deals.test.R
-import com.tokopedia.graphql.GraphqlCacheManager
-import com.tokopedia.test.application.environment.interceptor.mock.MockModelConfig
-import com.tokopedia.test.application.util.InstrumentationMockHelper
-import com.tokopedia.test.application.util.setupGraphqlMockResponse
+import com.tokopedia.deals.di.DealsComponentFactory
+import com.tokopedia.deals.di.DealsComponentFactoryStub
+import com.tokopedia.test.application.environment.ActivityScenarioTestRule
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -25,26 +22,16 @@ import org.junit.Test
 
 class DealsBrandDetailActivityTest {
 
-    private val context = InstrumentationRegistry.getInstrumentation().targetContext
-    private val graphqlCacheManager = GraphqlCacheManager()
-
     @get:Rule
     var cassavaTestRule = CassavaTestRule()
 
     @get:Rule
-    var activityRule =
-            ActivityTestRule(DealsBrandDetailActivity::class.java, false, false)
+    var activityRule = ActivityScenarioTestRule<DealsBrandDetailActivity>()
 
     @Before
     fun setup() {
         Intents.init()
-        graphqlCacheManager.deleteAll()
-        setupGraphqlMockResponse{
-            addMockResponse(
-                KEY_EVENT_BRAND_DETAIL,
-                    InstrumentationMockHelper.getRawString(context, R.raw.mock_gql_deals_brand_detail),
-                    MockModelConfig.FIND_BY_CONTAINS)
-        }
+        DealsComponentFactory.instance = DealsComponentFactoryStub()
 
         val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
         val intent = Intent(targetContext, DealsBrandDetailActivity::class.java).apply {
@@ -56,12 +43,16 @@ class DealsBrandDetailActivityTest {
 
     @Test
     fun testBrandDetailFlow() {
-        Thread.sleep(3000)
-        Intents.intending(IntentMatchers.anyIntent()).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
+        Thread.sleep(1000)
+        Intents.intending(anyIntent())
+            .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
 
-        Espresso.onView(ViewMatchers.withText("KlikDokter ECG")).perform(ViewActions.click())
+        onView(withText("KlikDokter ECG")).perform(click())
 
-        Assert.assertThat(cassavaTestRule.validate(ANALYTIC_VALIDATOR_QUERY_DEALS_BRAND_DETAIL_PAGE), hasAllSuccess())
+        Assert.assertThat(
+            cassavaTestRule.validate(ANALYTIC_VALIDATOR_QUERY_DEALS_BRAND_DETAIL_PAGE),
+            hasAllSuccess()
+        )
     }
 
     @After
@@ -71,7 +62,7 @@ class DealsBrandDetailActivityTest {
 
     companion object {
         private const val KEY_EVENT_BRAND_DETAIL = "event_brand_detail_v2"
-        private const val ANALYTIC_VALIDATOR_QUERY_DEALS_BRAND_DETAIL_PAGE = "tracker/entertainment/deals/deals_brand_detail_tracking.json"
+        private const val ANALYTIC_VALIDATOR_QUERY_DEALS_BRAND_DETAIL_PAGE =
+            "tracker/entertainment/deals/deals_brand_detail_tracking.json"
     }
-
 }
