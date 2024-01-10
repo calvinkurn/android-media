31f64181149c34d249ffd818630f9607f3634957
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2024-01-10 16:09:33 +0700

    `fix`: clean up code

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/utils/Constants.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/utils/Constants.kt
@@ -9,3 +9,6 @@ internal const val TAB_PRODUCT_KEY = "product_tab"
 internal const val TAB_REVIEW_KEY = "review_tab"
 internal const val TAB_PRODUCT_POS = 0
 internal const val TAB_REVIEW_POS = 1
+internal const val TYPE_IMAGE = 0
+internal const val TYPE_VIDEO = 1
+internal const val TYPE_UNKNOWN = 2
