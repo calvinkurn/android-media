2a66b5e0f2c756a973b20da121e3b82e8cc9b835
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-01-11 16:14:49 +0700

    feat: autoplay video only if user is using wifi

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/view/manager/videoPreview/VideoPreviewManager.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/shorts/view/manager/videoPreview/VideoPreviewManager.kt
@@ -1,5 +1,8 @@
 package com.tokopedia.play.broadcaster.shorts.view.manager.videoPreview
 
+import android.content.Context
+import android.net.ConnectivityManager
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
 import com.tokopedia.play.broadcaster.shorts.view.compose.VideoState
 import com.tokopedia.play_common.player.PlayVideoWrapper
 import com.tokopedia.play_common.state.PlayVideoState
@@ -12,8 +15,14 @@ import javax.inject.Inject
  * Created By : Jonathan Darwin on January 05, 2024
  */
 class VideoPreviewManager @Inject constructor(
+    @ApplicationContext context: Context,
     val videoWrapper: PlayVideoWrapper,
 ) {
+    private val connectivityManager: ConnectivityManager =
+        context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private val isWifi: Boolean
+        get() = !connectivityManager.isActiveNetworkMetered
 
     private val _videoState = MutableStateFlow(VideoState.Unknown)
     val videoState = _videoState.asStateFlow()
@@ -29,7 +38,11 @@ class VideoPreviewManager @Inject constructor(
                     _videoState.update { VideoState.Pause }
                 }
                 is PlayVideoState.Pause -> {
-                    _videoState.update { VideoState.Pause }
+                    if (_videoState.value == VideoState.Unknown) {
+                        if (isWifi) play() else pause()
+                    } else {
+                        _videoState.update { VideoState.Pause }
+                    }
                 }
                 else -> {}
             }
