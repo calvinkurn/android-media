5288452883ac15d01e260a4d6f5a644970789a31
hiradevina <fakhira.devina@tokopedia.com>
2024-01-12 14:09:37 +0700

    feat(compose tracking page): preview driver info

--- features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/view/component/DriverInfoWidget.kt
+++ features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/view/component/DriverInfoWidget.kt
@@ -3,6 +3,7 @@ package com.tokopedia.logisticorder.view.component
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Visibility
@@ -100,6 +102,22 @@ fun DriverInfoWidget(
             ) {
                 NestIcon(modifier = Modifier.size(20.dp, 20.dp), iconId = IconUnify.CALL)
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DriveInfoWidgetPreview() {
+    val data = LastDriverModel(
+        name = "Budi",
+        phone = "+6256648394543",
+        licenseNumber = "B 1238 JED",
+        isChanged = true
+    )
+
+    NestTheme {
+        DriverInfoWidget(modifier = Modifier.fillMaxWidth(), lastDriver = data, callDriver = {}) {
         }
     }
 }
