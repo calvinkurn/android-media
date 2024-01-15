be82d0c2833ec6d51fc8459a25e20c3ac8841e96
varun0922 <varun.sharma@tokopedia.com>
2024-01-15 08:40:27 +0530

    Adding the tracking code for the DivActions

--- libraries/sdui/src/main/java/com/tokopedia/sdui/extention/ActionHandler.kt
+++ libraries/sdui/src/main/java/com/tokopedia/sdui/extention/ActionHandler.kt
@@ -1,15 +1,18 @@
 package com.tokopedia.sdui.extention
 
 import android.content.Context
-import com.google.gson.JsonObject
+import com.google.gson.Gson
 import com.tokopedia.applink.RouteManager
+import com.tokopedia.sdui.utils.DivActionUtils
 import com.tokopedia.track.TrackApp
 import com.yandex.div.core.DivActionHandler
 import com.yandex.div.core.DivViewFacade
 import com.yandex.div2.DivAction
 import com.yandex.div2.DivSightAction
+import org.json.JSONObject
 import com.yandex.div2.DivVisibilityAction
 import java.net.URI
+
 
 class ActionHandler(private val contextDivAction: Context?): DivActionHandler() {
 
@@ -18,9 +21,11 @@ class ActionHandler(private val contextDivAction: Context?): DivActionHandler() 
         const val QUERY_SEPARATOR = "&&"
         const val IDENTIFIER_ANDROID_APPLINK = "android_applink="
         const val IDENTIFIER_APPLINK = "applink="
+        const val KEY_TRACKING_DATA = "tracking_data"
     }
     override fun handleAction(action: DivAction, view: DivViewFacade): Boolean {
         RouteManager.route(contextDivAction, getApplink(parseClickActionUrl(action.url?.rawValue.toString())))
+        sendTracker(action.payload)
         return super.handleAction(action, view)
     }
 
@@ -29,11 +34,13 @@ class ActionHandler(private val contextDivAction: Context?): DivActionHandler() 
         view: DivViewFacade,
         actionUid: String
     ): Boolean {
+        sendTracker(action.payload)
         return super.handleAction(action, view, actionUid)
     }
 
     override fun handleAction(action: DivSightAction, view: DivViewFacade): Boolean {
         //Send impression tracker
+        sendTracker(action.payload)
         return super.handleAction(action, view)
     }
 
@@ -41,9 +48,13 @@ class ActionHandler(private val contextDivAction: Context?): DivActionHandler() 
         return applinkActionStr
     }
 
-    private fun sendTracker(trackerPayload: JsonObject){
-        //Convert JSON to Object
-        //TrackApp.getInstance().gtm.sendGeneralEvent(null)
+    private fun sendTracker(trackerPayload: JSONObject?){
+        //Convert JSON to Hash Map
+        if(trackerPayload != null) {
+            val trackingData = trackerPayload.getJSONObject(APPLINK.KEY_TRACKING_DATA)
+            val evnetDataMap: HashMap<String, Any> = DivActionUtils.toMap(trackingData)
+            TrackApp.getInstance().gtm.sendGeneralEvent(evnetDataMap)
+        }
     }
 
     private fun parseClickActionUrl(actionUrlString: String):String{
