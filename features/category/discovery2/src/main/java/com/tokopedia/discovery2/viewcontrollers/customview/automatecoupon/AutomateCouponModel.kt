820b7c41dd35c82229d69ecb7e6d99fe6ee74792
andri.fashbir <andri.fashbir@tokopedia.com>
2024-01-17 14:56:17 +0700

    Differentiate model for List and Grid

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/customview/automatecoupon/AutomateCouponModel.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/customview/automatecoupon/AutomateCouponModel.kt
@@ -2,14 +2,26 @@ package com.tokopedia.discovery2.viewcontrollers.customview.automatecoupon
 
 import java.util.Date
 
-data class AutomateCouponModel(
-    val type: DynamicColorText,
-    val benefit: DynamicColorText,
-    val tnc: DynamicColorText,
-    val backgroundUrl: String,
-    val iconUrl: String?,
-    val timeLimitPrefix: DynamicColorText?,
-    val endDate: Date?,
-    val shopName: DynamicColorText?,
-    val badgeText: String?
-)
+sealed class AutomateCouponModel {
+    data class List(
+        val type: DynamicColorText,
+        val benefit: DynamicColorText,
+        val tnc: DynamicColorText,
+        val backgroundUrl: String,
+        val iconUrl: String?,
+        val timeLimitPrefix: DynamicColorText?,
+        val endDate: Date?,
+        val shopName: DynamicColorText?,
+        val badgeText: String?
+    ) : AutomateCouponModel()
+
+    data class Grid(
+        val type: DynamicColorText,
+        val benefit: DynamicColorText,
+        val tnc: DynamicColorText,
+        val backgroundUrl: String,
+        val iconUrl: String?,
+        val shopName: DynamicColorText?,
+        val badgeText: String?
+    ) : AutomateCouponModel()
+}
--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/customview/automatecoupon/IAutomateCouponView.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/customview/automatecoupon/IAutomateCouponView.kt
@@ -1,5 +1,5 @@
 package com.tokopedia.discovery2.viewcontrollers.customview.automatecoupon
 
 interface IAutomateCouponView {
-    fun setModel(model: AutomateCouponModel)
+    fun setModel(couponModel: AutomateCouponModel)
 }
