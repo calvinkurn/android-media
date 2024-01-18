319ef445cd573392df1c858f75443d0a1f2709ac
varun0922 <varun.sharma@tokopedia.com>
2024-01-18 16:41:34 +0530

    adding the use case for referral timestamp

--- features/promo/gamification/src/main/java/com/tokopedia/gamification/pdp/domain/usecase/KetupatReferralEventTimeStampUseCase.kt
+++ features/promo/gamification/src/main/java/com/tokopedia/gamification/pdp/domain/usecase/KetupatReferralEventTimeStampUseCase.kt
@@ -1,68 +1,57 @@
 package com.tokopedia.gamification.pdp.domain.usecase
 
+import com.tokopedia.gamification.pdp.data.model.KetupatReferralEventTimeStamp
+import com.tokopedia.gamification.pdp.repository.GamificationRepository
 import com.tokopedia.gql_query_annotation.GqlQuery
+import javax.inject.Inject
 
-@GqlQuery("GetKetupatReferralEventTimeStampUseCase", KetupatReferralEventTimeStampUseCase.REFERRAL_EVENT_TIME_STAMP_DATA)
-class KetupatReferralEventTimeStampUseCase {
+@GqlQuery("GetKetupatReferralEventTimeStamp", KetupatReferralEventTimeStampUseCase.REFERRAL_EVENT_TIME_STAMP_DATA)
+class KetupatReferralEventTimeStampUseCase @Inject constructor(
+    private val repository: GamificationRepository
+){
     companion object{
         private const val EVENT_SLUG = "eventSlug"
         const val REFERRAL_EVENT_TIME_STAMP_DATA = """
-            {
-  query gameReferralStampData(${'$'}eventSlug: String!) {
-  gameReferralStamp(eventSlug: ${"$"}eventSlug) {
-    resultStatus {
-      code
-      message
-      reason
-    }
-    stamps {
-      imageUrl
-      name
-      timestamp
-    }
-    currentStampCount
-    maxStampCount
-    stampLevelData{
-        LevelIndex
-        TotalStampNeeded
-        RewardNumber
-        MilestoneRewardNumber
-  }
-  }
-  }
-  gameReferralEventContent(eventSlug:${"$"}eventSlug){
-    resultStatus{
-      code
-      message
-      reason
-    }
-    eventContent{
-      id
-      eventId
-      name
-      description
-      referralHomepageUrl
-      headerHomepageImageUrl
-      titleDescription
-      remainingTime
-      isShowRecommendation
-      bannerTitle
-      bannerImageURL
-      bannerImageURLMobile
-      bannerRedirectURL
-      timewindowStatus
-      timeUntilStart
-      }
-    }
-}
+            query Gamification_gameReferralStamp(${'$'}slug: String!) {
+              gameReferralStamp(eventSlug: ${'$'}slug) {
+                resultStatus {
+                  code
+                  message
+                  reason
+                }
+                currentStampCount
+                maxStampCount
+                stampLevelData {
+                  LevelIndex
+                  TotalStampNeeded
+                }
+              }
+              gameReferralEventContent(eventSlug:${'$'}slug){
+                resultStatus{
+                  code
+                  message
+                  reason
+                }
+                eventContent{
+                  remainingTime
+                }
+              }
+            }
         """
-
     }
     private fun createRequestParams(slug: String): HashMap<String, Any> {
         val request = HashMap<String, Any>()
-        request[KetupatReferralEventTimeStampUseCase.EVENT_SLUG] = slug
+        request[EVENT_SLUG] = slug
         val input = HashMap<String, Any>()
-        input[KetupatReferralEventTimeStampUseCase.EVENT_SLUG] = request
+        input[EVENT_SLUG] = request
         return input
+    }
+
+    suspend fun getKetupatReferralTimeStampData(slug: String): KetupatReferralEventTimeStamp {
+        return repository.getGQLData(
+            GetKetupatReferralEventTimeStamp.GQL_QUERY,
+            KetupatReferralEventTimeStamp::class.java,
+            createRequestParams(slug)
+        )
     }
 }
