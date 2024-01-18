a1e5b448f4d30b9688a9896eeec496124bdf4f48
habibitkp <101687909+habibitkp@users.noreply.github.com>
2024-01-18 13:00:35 +0700

    remove unused code

--- features/user/onboarding/src/main/java/com/tokopedia/onboarding/di/module/DynamicOnboardingQueryModule.kt
+++ features/user/onboarding/src/main/java/com/tokopedia/onboarding/di/module/DynamicOnboardingQueryModule.kt
@@ -2,14 +2,9 @@ package com.tokopedia.onboarding.di.module
 
 import android.app.Activity
 import android.content.Context
-import com.tokopedia.abstraction.common.utils.GraphqlHelper
-import com.tokopedia.onboarding.R
-import com.tokopedia.onboarding.data.OnboardingConstant
 import com.tokopedia.onboarding.di.OnboardingScope
 import dagger.Module
 import dagger.Provides
-import dagger.multibindings.IntoMap
-import dagger.multibindings.StringKey
 
 @Module
 class DynamicOnboardingQueryModule(private val activity: Activity) {
