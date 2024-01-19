80ed85fe12b677cc7f91e39cd5af85839ce7ca2a
hiradevina <fakhira.devina@tokopedia.com>
2024-01-19 10:50:25 +0700

    refactor(compose tracking page)

--- features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/uimodel/TrackingPageModel.kt
+++ features/logistic/logisticorder/src/main/java/com/tokopedia/logisticorder/uimodel/TrackingPageModel.kt
@@ -1,24 +1,19 @@
 package com.tokopedia.logisticorder.uimodel
 
-import android.os.Parcelable
 import com.tokopedia.logisticorder.utils.TippingConstant
 import com.tokopedia.targetedticker.domain.TargetedTickerParamModel
-import kotlinx.parcelize.Parcelize
 
 data class TrackingPageModel(
     var trackingDataModel: TrackingDataModel = TrackingDataModel()
 )
 
-// @Parcelize
 data class TrackingDataModel(
     var trackOrder: TrackOrderModel = TrackOrderModel(),
     var page: PageModel = PageModel(),
     var tipping: TippingModel = TippingModel(),
     var lastDriver: LastDriverModel = LastDriverModel()
 )
-//    : Parcelable
 
-// @Parcelize
 data class TrackOrderModel(
     var detail: DetailModel = DetailModel(),
     var trackHistory: List<TrackHistoryModel> = listOf(),
@@ -30,9 +25,7 @@ data class TrackOrderModel(
     var shippingRefNum: String = "",
     var invalid: Boolean = false
 )
-//    : Parcelable
 
-// @Parcelize
 data class DetailModel(
     var shipperCity: String = "",
     var shipperName: String = "",
@@ -45,18 +38,14 @@ data class DetailModel(
     var trackingUrl: String = "",
     var eta: EtaModel = EtaModel()
 )
-//    : Parcelable
 
-// @Parcelize
 data class ProofModel(
     var imageId: String = "",
     var description: String = "",
     var imageUrl: String = "",
     var accessToken: String = ""
 )
-//    : Parcelable
 
-// @Parcelize
 data class TrackHistoryModel(
     var dateTime: String = "",
     var date: String = "",
@@ -66,45 +55,36 @@ data class TrackHistoryModel(
     var partnerName: String = "",
     var proof: ProofModel = ProofModel()
 )
-//    : Parcelable
 
-// @Parcelize
 data class PageModel(
     var additionalInfo: List<AdditionalInfoModel> = listOf(),
     var contactUsUrl: String = "",
     var targetedTickerParam: TargetedTickerParamModel = TargetedTickerParamModel()
 )
-//    : Parcelable
 
-// @Parcelize
 data class AdditionalInfoModel(
     var title: String = "",
     var notes: String = "",
     var urlDetail: String = "",
     var urlText: String = ""
 )
-//    : Parcelable
 
-@Parcelize
 data class EtaModel(
     var userInfo: String = "",
     var userUpdatedInfo: String = "",
     var isChanged: Boolean = false
-) :
-    Parcelable
+)
 
-// @Parcelize
 data class TippingModel(
     var status: Int = 0,
     var statusTitle: String = "",
     var statusSubtitle: String = "",
     var refNumber: String = ""
-) { //    : Parcelable
+) {
     val eligibleForTipping: Boolean
         get() = status == TippingConstant.OPEN || status == TippingConstant.WAITING_PAYMENT || status == TippingConstant.SUCCESS_PAYMENT || status == TippingConstant.SUCCESS_TIPPING || status == TippingConstant.REFUND_TIP
 }
 
-// @Parcelize
 data class LastDriverModel(
     var photo: String = "",
     var name: String = "",
@@ -112,4 +92,3 @@ data class LastDriverModel(
     var licenseNumber: String = "",
     var isChanged: Boolean = false
 )
-//    : Parcelable
