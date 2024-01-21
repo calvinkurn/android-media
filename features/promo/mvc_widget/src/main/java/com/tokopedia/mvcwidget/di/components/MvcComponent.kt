8d0335cbd9832b2fa30f88679eaac22ca85f52c7
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-01-21 12:58:53 +0700

    Refactor DI

--- features/promo/mvc_widget/src/main/java/com/tokopedia/mvcwidget/di/components/MvcComponent.kt
+++ features/promo/mvc_widget/src/main/java/com/tokopedia/mvcwidget/di/components/MvcComponent.kt
@@ -2,7 +2,7 @@ package com.tokopedia.mvcwidget.di.components
 
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
-import com.tokopedia.mvcwidget.di.module.DispatcherModule
+import com.tokopedia.mvcwidget.di.module.MvcModule
 import com.tokopedia.mvcwidget.di.module.ViewModelModule
 import com.tokopedia.mvcwidget.multishopmvc.verticallist.MerchantCouponFragment
 import com.tokopedia.mvcwidget.views.MvcDetailView
@@ -14,7 +14,7 @@ import dagger.Component
 @ActivityScope
 @Component(
     dependencies = [BaseAppComponent::class],
-    modules = [DispatcherModule::class, ViewModelModule::class, TokomemberDispatcherModule::class]
+    modules = [MvcModule::class, ViewModelModule::class]
 )
 
 interface MvcComponent {
--- features/promo/mvc_widget/src/main/java/com/tokopedia/mvcwidget/di/module/MvcModule.kt
+++ features/promo/mvc_widget/src/main/java/com/tokopedia/mvcwidget/di/module/MvcModule.kt
@@ -2,6 +2,8 @@ package com.tokopedia.mvcwidget.di.module
 
 import android.content.Context
 import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.mvcwidget.IO
 import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
@@ -12,7 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import javax.inject.Named
 
 @Module
-class DispatcherModule {
+class MvcModule {
     @Provides
     @Named(IO)
     fun provideWorkerDispatcher(): CoroutineDispatcher = Dispatchers.IO
@@ -21,4 +23,10 @@ class DispatcherModule {
     @Provides
     fun provideSession(@ApplicationContext context: Context): UserSessionInterface =
         UserSession(context)
+
+    @Provides
+    fun provideGraphqlRepositoryModule(): GraphqlRepository {
+        return GraphqlInteractor.getInstance().graphqlRepository
+    }
+
 }
