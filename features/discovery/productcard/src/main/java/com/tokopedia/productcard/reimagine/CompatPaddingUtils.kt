5d7250d1b19544614d58a54016dbe94ac519c258
Darian Thedy <darian.thedy@tokopedia.com>
2024-01-23 10:17:00 +0700

    [Product Card] Fix compat padding for ribbon

--- features/discovery/productcard/src/main/java/com/tokopedia/productcard/reimagine/CompatPaddingUtils.kt
+++ features/discovery/productcard/src/main/java/com/tokopedia/productcard/reimagine/CompatPaddingUtils.kt
@@ -1,40 +1,23 @@
 package com.tokopedia.productcard.reimagine
 
-import android.content.Context
-import android.util.AttributeSet
 import android.view.View
-import androidx.core.view.setPadding
-import com.tokopedia.productcard.R
 import com.tokopedia.productcard.utils.getPixel
 import com.tokopedia.productcard.R as productcardR
 
 internal class CompatPaddingUtils(
-    private val context: Context?,
     private val productCardView: View,
-    private val attrs: AttributeSet?,
+    private val useCompatPadding: Boolean,
+    private val productCardModel: ProductCardModel,
 ) {
 
-    fun updateMargin() {
-        if (!useCompatPadding()) return
+    fun updatePadding() {
+        if (!useCompatPadding) return
 
-        productCardView.setPadding(
-            context.getPixel(
-                productcardR.dimen.product_card_reimagine_use_compat_padding_size
-            )
-        )
-    }
+        val padding = productCardView.context?.getPixel(
+            productcardR.dimen.product_card_reimagine_use_compat_padding_size
+        ) ?: 0
 
-    private fun useCompatPadding(): Boolean {
-        val typedArray = context
-            ?.obtainStyledAttributes(attrs, R.styleable.ProductCardView, 0, 0)
-            ?: return false
-
-        return try {
-            typedArray.getBoolean(R.styleable.ProductCardView_useCompatPadding, false)
-        } catch(_: Throwable) {
-            false
-        } finally {
-            typedArray.recycle()
-        }
+        val startPadding = if (productCardModel.hasRibbon()) 0 else padding
+        productCardView.setPadding(startPadding, padding, padding, padding)
     }
 }
