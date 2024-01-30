10003fa35aa674dd18d0e7432b70c2285dfe0d83
bobby.ingkirama <bobby.ingkirama@tokopedia.com>
2024-01-30 15:49:59 +0700

    Feat(cart): add new model for product label

---
+++ features/transaction/cart/src/main/java/com/tokopedia/cart/data/model/response/shopgroupsimplified/ImageAsset.kt
@@ -0,0 +1,8 @@
+package com.tokopedia.cart.data.model.response.shopgroupsimplified
+
+import com.google.gson.annotations.SerializedName
+
+data class ImageAsset(
+    @SerializedName("image_asset")
+    val imageLabel: String = ""
+)
