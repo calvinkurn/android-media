2cb2475a05ce8f377a9273b7280e81011eb677ca
isfaaghyth <isfaaghyth@gmail.com>
2024-01-31 12:01:30 +0700

    feat: create RecomTemporary annotation

---
+++ features/discovery/recommendation_widget_common/src/main/java/com/tokopedia/recommendation_widget_common/widget/foryou/utils/RecomTemporary.kt
@@ -0,0 +1,5 @@
+package com.tokopedia.recommendation_widget_common.widget.foryou.utils
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class RecomTemporary
