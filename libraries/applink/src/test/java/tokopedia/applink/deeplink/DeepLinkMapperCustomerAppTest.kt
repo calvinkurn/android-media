ce834dabef533806b4d0b445d7e2c113482fcf78
Yusuf Hendrawan <yusuf.hendrawan@bytedance.com>
2024-02-01 16:55:03 +0700

    Adjust deeplink mapper unit test

--- libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
+++ libraries/applink/src/test/java/tokopedia/applink/deeplink/DeepLinkMapperCustomerAppTest.kt
@@ -34,7 +34,7 @@ class DeepLinkMapperCustomerAppTest : DeepLinkMapperTestFixture() {
         // This a reminder to developer.
         // If this size is modified, please also add unit test for the added deeplink.
         const val SIZE_HOST = 163
-        const val SIZE_PATH = 276
+        const val SIZE_PATH = 277
     }
 
     override fun setup() {
