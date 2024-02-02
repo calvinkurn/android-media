39655aff84935db90e38db95627e7596e1d6b9ba
ilhamsuaib <ilhamsuaib10@gmail.com>
2024-02-02 15:04:12 +0700

    Revert "feat `ramadhan thematic` : implement ramadhan thematic on seller app"
    This reverts commit c555f74ad000c093fa28bbd5ad3336a94ac226e3.

--- features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
+++ features/merchant/seller_home/src/main/java/com/tokopedia/sellerhome/view/fragment/SellerHomeFragment.kt
@@ -14,6 +14,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
 import android.view.animation.AnimationUtils
 import android.view.animation.LayoutAnimationController
 import androidx.lifecycle.LiveData
@@ -2022,6 +2023,7 @@ class SellerHomeFragment :
             if (it is Success) {
                 val info = it.data
                 this.isNewSellerState = info.isNewSellerState
+                setViewBackgroundNewSeller()
                 if (info.subtitle.isBlank()) return@observe
 
                 if (info.subType == ShopStateInfoUiModel.SubType.TOAST) {
@@ -2035,12 +2037,87 @@ class SellerHomeFragment :
         }
     }
 
-    private fun setViewBackground() = binding?.run {
+    private fun setViewBackgroundNewSeller() {
         binding?.run {
+            if (isNewSellerState) {
+                viewBgShopStatus.visible()
+                viewBgShopStatus.layoutParams.height = LayoutParams.MATCH_PARENT
+                viewBgShopStatus.setImageResource(R.drawable.sah_shop_state_bg_new_seller)
+                viewBgShopStatus.requestLayout()
+                imgSahNewSellerLeft.loadImage(SellerHomeConst.Images.IMG_NEW_SELLER_LEFT) {
+                    useCache(true)
+                    listener(onSuccess = { _, _ ->
+                        imgSahNewSellerLeft.visible()
+                    })
+                }
+
+                imgSahNewSellerRight.loadImage(SellerHomeConst.Images.IMG_NEW_SELLER_RIGHT) {
+                    useCache(true)
+                    listener(onSuccess = { _, _ ->
+                        imgSahNewSellerRight.visible()
+                    })
+                }
+            } else {
+                imgSahNewSellerLeft.gone()
+                imgSahNewSellerRight.gone()
+                setViewBackground()
+            }
+            setSectionWidgetTextColor()
+        }
+    }
+
+    private fun setSectionWidgetTextColor() {
+        recyclerView?.post {
+            val widgets = adapter.data.map {
+                if (it is SectionWidgetUiModel) {
+                    val titleTextColor: Int
+                    val subTitleTextColor: Int
+                    if (isNewSellerState) {
+                        titleTextColor = unifyprinciplesR.color.Unify_NN0
+                        subTitleTextColor = unifyprinciplesR.color.Unify_NN0
+                    } else {
+                        titleTextColor = unifyprinciplesR.color.Unify_NN950_96
+                        subTitleTextColor = unifyprinciplesR.color.Unify_NN950_68
+                    }
+                    return@map it.copy(
+                        titleTextColorId = titleTextColor,
+                        subTitleTextColorId = subTitleTextColor
+                    )
+                }
+                return@map it
+            }
+
+            notifyWidgetWithSdkChecking {
+                updateWidgets(widgets)
+            }
+        }
+    }
+
+    private fun setViewBackground() = binding?.run {
+        val isOfficialStore = userSession.isShopOfficialStore
+        val isPowerMerchant = userSession.isPowerMerchantIdle || userSession.isGoldMerchant
+        when {
+            isOfficialStore -> {
+                showRegularHomeBackground(R.drawable.sah_shop_state_bg_official_store)
+            }
+
+            isPowerMerchant -> {
+                showRegularHomeBackground(R.drawable.sah_shop_state_bg_power_merchant)
+            }
+
+            else -> {
+                viewBgShopStatus.gone()
+            }
+        }
+    }
+
+    private fun showRegularHomeBackground(backgroundResource: Int) {
+        binding?.run {
+            val height = requireActivity().resources.getDimensionPixelSize(R.dimen.sah_dimen_280dp)
+            viewBgShopStatus.layoutParams.height = height
             viewBgShopStatus.visible()
-            viewBgShopStatus.loadImage(R.drawable.sah_shop_state_bg_power_merchant)
-            thematicIllustration.visible()
-            thematicIllustration.loadImage(R.drawable.sah_ramadhan_thematic_backdrop)
+            viewBgShopStatus.setImageResource(backgroundResource)
+            viewBgShopStatus.requestLayout()
         }
     }
 
@@ -2843,6 +2920,7 @@ class SellerHomeFragment :
         if (shopState == ShopStateUiModel.NewRegisteredShop) {
             showNewSellerDialog()
             isNewSellerState = true
+            setViewBackgroundNewSeller()
         } else if (shouldGetShopStateInfo) {
             getShopStateInfoIfEligible()
         }
@@ -2853,6 +2931,7 @@ class SellerHomeFragment :
             sellerHomeViewModel.getShopStateInfo()
         } else {
             isNewSellerState = false
+            setViewBackgroundNewSeller()
         }
     }
 
