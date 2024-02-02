a2a88baa1a9947292996a0fdfde9a774343cfdf4
yovi.putra <yovi.putra@tokopedia.com>
2024-02-03 03:15:38 +0700

    `fix`[kvi_near_name] blinking label icon while scrolling up and down

--- features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/fragment/partialview/PartialContentView.kt
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/fragment/partialview/PartialContentView.kt
@@ -1,33 +1,21 @@
 package com.tokopedia.product.detail.view.fragment.partialview
 
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.SpannableStringBuilder
-import android.text.Spanned
 import android.view.View
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
 import com.tokopedia.abstraction.common.utils.view.MethodChecker
-import com.tokopedia.kotlin.extensions.view.ONE
-import com.tokopedia.kotlin.extensions.view.ZERO
 import com.tokopedia.kotlin.extensions.view.gone
 import com.tokopedia.kotlin.extensions.view.hide
 import com.tokopedia.kotlin.extensions.view.shouldShowWithAction
 import com.tokopedia.kotlin.extensions.view.show
 import com.tokopedia.kotlin.extensions.view.showWithCondition
-import com.tokopedia.kotlin.util.lazyThreadSafetyNone
 import com.tokopedia.media.loader.loadImageWithoutPlaceholder
-import com.tokopedia.media.loader.module.GlideApp
 import com.tokopedia.product.detail.R
 import com.tokopedia.product.detail.common.data.model.pdplayout.CampaignModular
-import com.tokopedia.product.detail.common.data.model.pdplayout.LabelIcons
-import com.tokopedia.product.detail.common.extensions.parseAsHtmlLink
 import com.tokopedia.product.detail.data.model.datamodel.ProductContentMainData
 import com.tokopedia.product.detail.databinding.ItemProductContentBinding
 import com.tokopedia.product.detail.view.listener.DynamicProductDetailListener
 import com.tokopedia.product.detail.view.viewholder.campaign.ui.widget.CampaignRibbon
-import com.tokopedia.product.detail.view.widget.CenteredImageSpan
-import com.tokopedia.unifycomponents.toPx
+import com.tokopedia.product.detail.view.widget.productNameDelegate
 import com.tokopedia.product.detail.common.R as productdetailcommonR
 import com.tokopedia.unifyprinciples.R as unifyprinciplesR
 
@@ -35,19 +23,13 @@ import com.tokopedia.unifyprinciples.R as unifyprinciplesR
  * Created by Yehezkiel on 25/05/20
  */
 class PartialContentView(
-    private val view: View,
+    view: View,
     private val listener: DynamicProductDetailListener
 ) {
 
-    companion object {
-        private val KVI_ICON_HEIGHT = 16.toPx()
-    }
-
     private val context = view.context
     private val binding = ItemProductContentBinding.bind(view)
-    private val glideApp by lazyThreadSafetyNone {
-        GlideApp.with(context).asDrawable()
-    }
+    private val productNameDelegate by binding.productNameDelegate()
 
     fun renderData(
         data: ProductContentMainData,
@@ -61,7 +43,7 @@ class PartialContentView(
             MethodChecker.fromHtml(data.productName)
         )
 
-        setProductName(labelIcons = data.labelIcons, data.productName)
+        productNameDelegate.setTitle(title = data.productName, labelIcons = data.labelIcons)
 
         renderFreeOngkir(freeOngkirImgUrl)
 
@@ -149,110 +131,6 @@ class PartialContentView(
         }
         hideGimmick(campaign)
     }
-
-    //region product name text & label icons
-    private fun setProductName(labelIcons: List<LabelIcons>, title: String) {
-        if (labelIcons.isEmpty()) {
-            setProductNameText(title = title)
-        } else {
-            setProductNameWithIcon(labelIcons = labelIcons, title = title)
-        }
-    }
-
-    private fun setProductNameText(title: String) {
-        binding.productName.text = title.parseAsHtmlLink(context, false)
-    }
-
-    private fun setProductNameWithIcon(labelIcons: List<LabelIcons>, title: String) {
-        val textView = binding.productName
-        val stringBuilder = SpannableStringBuilder().apply {
-            append(title.parseAsHtmlLink(context, false))
-        }
-        textView.text = stringBuilder
-
-        processLabelIcons(stringBuilder = stringBuilder, labelIcons = labelIcons)
-    }
-
-    private fun processLabelIcons(
-        stringBuilder: SpannableStringBuilder,
-        labelIcons: List<LabelIcons>
-    ) {
-        val mapIcons: MutableMap<String, Drawable?> = labelIcons
-            .associate { it.iconURL to null }
-            .toMutableMap()
-
-        prepareLoadIcons(mapIcons = mapIcons, stringBuilder)
-    }
-
-    private fun prepareLoadIcons(
-        mapIcons: MutableMap<String, Drawable?>,
-        stringBuilder: SpannableStringBuilder
-    ) {
-        mapIcons.keys.forEach { url ->
-            loadIcon(url = url) {
-                mapIcons[url] = this
-                renderLabelIcons(stringBuilder = stringBuilder, mapIcons = mapIcons)
-            }
-        }
-    }
-
-    private fun renderLabelIcons(
-        stringBuilder: SpannableStringBuilder,
-        mapIcons: MutableMap<String, Drawable?>
-    ) {
-        // Exit if all icons have not finished load
-        if (!allIconsLoaded(mapIcons)) return
-
-        val textView = binding.productName
-        val textViewLineSpacing = textView.lineSpacingExtra
-        mapIcons.getIcons().forEach { resource ->
-            resource.resizeLabelIconSpec()
-            stringBuilder.setLabelIconSpan(drawable = resource, lineSpacing = textViewLineSpacing)
-            textView.text = stringBuilder
-        }
-    }
-
-    private fun MutableMap<String, Drawable?>.getIcons() = values
-        // reversed this, because each logo put from index 0 always
-        .reversed()
-        .asSequence()
-        .filterNotNull()
-
-    private fun allIconsLoaded(mapIcons: MutableMap<String, Drawable?>): Boolean {
-        // ensure all icons has loaded
-        return mapIcons.values.all { it != null }
-    }
-
-    private fun Drawable.resizeLabelIconSpec() {
-        val ratio = intrinsicWidth.toFloat() / intrinsicHeight.toFloat()
-        val bottom = if (intrinsicHeight > KVI_ICON_HEIGHT) KVI_ICON_HEIGHT else intrinsicHeight
-        val right = bottom * ratio
-        setBounds(Int.ZERO, Int.ZERO, right.toInt(), bottom)
-    }
-
-    private fun SpannableStringBuilder.setLabelIconSpan(drawable: Drawable, lineSpacing: Float) {
-        val imageSpan = CenteredImageSpan(drawable, lineSpacing)
-
-        insert(Int.ZERO, "  ")
-        setSpan(imageSpan, Int.ZERO, Int.ONE, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-    }
-
-    private inline fun loadIcon(url: String, crossinline onLoaded: Drawable?.() -> Unit) {
-        glideApp.load(url)
-            .into(object : CustomTarget<Drawable>() {
-                override fun onResourceReady(
-                    resource: Drawable,
-                    transition: Transition<in Drawable>?
-                ) {
-                    onLoaded(resource)
-                }
-
-                override fun onLoadCleared(placeholder: Drawable?) {
-                    onLoaded(null)
-                }
-            })
-    }
-    // endregion product name & label icons
 
     private fun renderStockAvailable(
         campaign: CampaignModular,
---
+++ features/merchant/product_detail/src/main/java/com/tokopedia/product/detail/view/widget/ProductNameDelegate.kt
@@ -0,0 +1,187 @@
+package com.tokopedia.product.detail.view.widget
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.tokopedia.kotlin.extensions.view.ONE
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.kotlin.extensions.view.orZero
+import com.tokopedia.kotlin.util.lazyThreadSafetyNone
+import com.tokopedia.media.loader.module.GlideApp
+import com.tokopedia.product.detail.common.data.model.pdplayout.LabelIcons
+import com.tokopedia.product.detail.common.extensions.parseAsHtmlLink
+import com.tokopedia.product.detail.databinding.ItemProductContentBinding
+import com.tokopedia.unifycomponents.toPx
+import com.tokopedia.unifyprinciples.Typography
+
+/**
+ * Created by yovi.putra on 02/02/24"
+ * Project name: android-tokopedia-core
+ **/
+
+internal class ProductNameDelegate(
+    private val typography: Typography?
+) {
+    companion object {
+        private val KVI_ICON_HEIGHT = 16.toPx()
+    }
+
+    private val context
+        get() = typography?.context
+
+    private val glideApp by lazyThreadSafetyNone {
+        GlideApp
+            .with(context ?: return@lazyThreadSafetyNone null)
+            .asDrawable()
+    }
+
+    private val iconPlaceholder by lazyThreadSafetyNone { IconLoaded(null, false) }
+
+    // avoid blinking the label icons when scrolling up and down
+    private var previousTitle: String = ""
+    private var previousLabelIcons: List<LabelIcons> = emptyList()
+
+    fun setTitle(title: String, labelIcons: List<LabelIcons> = emptyList()) {
+        if (!hasChanged(title = title, labelIcons = labelIcons)) return
+
+        if (labelIcons.isEmpty()) {
+            setProductNameText(title = title)
+        } else {
+            setProductNameWithIcon(labelIcons = labelIcons, title = title)
+        }
+    }
+
+    private fun hasChanged(title: String, labelIcons: List<LabelIcons> = emptyList()): Boolean {
+        val isNewValue = previousTitle != title && previousLabelIcons != labelIcons
+
+        if (isNewValue) {
+            previousTitle = title
+            previousLabelIcons = labelIcons
+        }
+
+        return isNewValue
+    }
+
+    private fun setProductNameText(title: String) {
+        withContext {
+            val text = title.parseAsHtmlLink(it, false)
+            setText(text = text)
+        }
+    }
+
+    private fun setProductNameWithIcon(labelIcons: List<LabelIcons>, title: String) = withContext {
+        val stringBuilder = SpannableStringBuilder().apply {
+            append(title.parseAsHtmlLink(it, false))
+        }
+
+        setText(text = stringBuilder)
+        stringBuilder.processLabelIcons(labelIcons = labelIcons)
+    }
+
+    private fun SpannableStringBuilder.processLabelIcons(
+        labelIcons: List<LabelIcons>
+    ) {
+        val initialMapIcons: MutableMap<String, IconLoaded> = labelIcons
+            .associate { it.iconURL to iconPlaceholder }
+            .toMutableMap()
+
+        prepareLoadIcons(mapIcons = initialMapIcons)
+    }
+
+    private fun SpannableStringBuilder.prepareLoadIcons(
+        mapIcons: MutableMap<String, IconLoaded>
+    ) {
+        mapIcons.keys.forEach { url ->
+            loadIcon(url = url) {
+                val iconLoaded = mapIcons[url] ?: iconPlaceholder
+                mapIcons[url] = iconLoaded.copy(this, true)
+                renderLabelIcons(mapIcons = mapIcons)
+            }
+        }
+    }
+
+    private fun SpannableStringBuilder.renderLabelIcons(mapIcons: MutableMap<String, IconLoaded>) {
+        // Exit if all icons have not finished load
+        if (!allIconsLoaded(mapIcons)) return
+
+        val textViewLineSpacing = typography?.lineSpacingExtra.orZero()
+        mapIcons.getIcons().forEach { resource ->
+            resource.resizeLabelIconSpec()
+            setLabelIconSpan(drawable = resource, lineSpacing = textViewLineSpacing)
+            setText(text = this)
+        }
+    }
+
+    private fun MutableMap<String, IconLoaded>.getIcons() = values
+        // reversed this, because each logo put from index 0 always
+        .reversed()
+        .asSequence()
+        .map { it.first }
+        .filterNotNull()
+
+    private fun allIconsLoaded(mapIcons: MutableMap<String, IconLoaded>): Boolean {
+        // ensure all icons has loaded
+        return mapIcons.values.all { it.second }
+    }
+
+    private fun Drawable.resizeLabelIconSpec() {
+        val ratio = intrinsicWidth.toFloat() / intrinsicHeight.toFloat()
+        val bottom = if (intrinsicHeight > KVI_ICON_HEIGHT) KVI_ICON_HEIGHT else intrinsicHeight
+        val right = bottom * ratio
+        setBounds(Int.ZERO, Int.ZERO, right.toInt(), bottom)
+    }
+
+    private fun SpannableStringBuilder.setLabelIconSpan(drawable: Drawable, lineSpacing: Float) {
+        val imageSpan = CenteredImageSpan(drawable, lineSpacing)
+
+        insert(Int.ZERO, "  ")
+        setSpan(imageSpan, Int.ZERO, Int.ONE, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+
+    private inline fun loadIcon(url: String, crossinline onLoaded: Drawable?.() -> Unit) {
+        glideApp
+            ?.load(url)
+            ?.into(object : CustomTarget<Drawable>() {
+                override fun onResourceReady(
+                    resource: Drawable,
+                    transition: Transition<in Drawable>?
+                ) {
+                    onLoaded(resource)
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    super.onLoadFailed(errorDrawable)
+                    onLoaded(null)
+                }
+            })
+    }
+
+    private inline fun withContext(block: (Context) -> Unit) {
+        val mContext = context ?: return
+        block(mContext)
+    }
+
+    private fun setText(text: SpannableStringBuilder) {
+        typography?.text = text
+    }
+
+    private fun setText(text: CharSequence?) {
+        typography?.text = text
+    }
+}
+
+/**
+ * simplify by using typealias to store drawable & status after icon loaded
+ */
+private typealias IconLoaded = Pair<Drawable?, Boolean>
+
+internal fun ItemProductContentBinding.productNameDelegate() =
+    lazyThreadSafetyNone {
+        ProductNameDelegate(typography = productName)
+    }
