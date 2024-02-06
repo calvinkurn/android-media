d44532424a27f624f72923bd4e8e6a17df2a3494
isfaaghyth <isfhani.ghiath@tokopedia.com>
2024-02-06 15:43:03 +0700

    feat: create coupon widget data mapper

---
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/mapper/CouponWidgetMapper.kt
@@ -0,0 +1,90 @@
+package com.tokopedia.home_component.mapper
+
+import com.tokopedia.discovery_component.widgets.automatecoupon.AutomateCouponModel
+import com.tokopedia.discovery_component.widgets.automatecoupon.DynamicColorText
+import com.tokopedia.discovery_component.widgets.automatecoupon.TimeLimit
+import com.tokopedia.home_component.model.ChannelGrid
+import com.tokopedia.home_component.model.ChannelModel
+import com.tokopedia.home_component.model.LabelGroup
+import com.tokopedia.home_component.visitable.CouponWidgetDataItemModel
+import com.tokopedia.home_component.visitable.CouponWidgetDataModel
+import com.tokopedia.productcard.reimagine.LabelGroupStyle
+
+object CouponWidgetMapper {
+
+    fun map(model: ChannelModel): CouponWidgetDataModel {
+        return CouponWidgetDataModel(
+            channelModel = model,
+            backgroundGradientColor = model.channelBanner.gradientColor,
+            coupons = model.channelGrids.mapToAutomationCouponWidgetModel()
+        )
+    }
+
+    private fun List<ChannelGrid>.mapToAutomationCouponWidgetModel(): List<CouponWidgetDataItemModel> {
+        val models = mutableListOf<CouponWidgetDataItemModel>()
+
+        if (size == 1) return map { it.single() }
+        if (size == 2) return map { it.grid() }
+
+        forEachIndexed { index, model ->
+            if (index == 0) {
+                models.add(model.single())
+            } else {
+                models.add(model.grid())
+            }
+        }
+
+        return models
+    }
+
+    private fun ChannelGrid.single() =
+        CouponWidgetDataItemModel(toSingleCouponWidget(), labelGroup.getCtaState())
+
+    private fun ChannelGrid.grid() =
+        CouponWidgetDataItemModel(toGridCouponWidget(), labelGroup.getCtaState())
+
+    private fun ChannelGrid.toSingleCouponWidget() =
+        AutomateCouponModel.List(
+            type = labelGroup.toText("benefit-type"),
+            benefit = labelGroup.toText("benefit-value"),
+            tnc = labelGroup.toText("tnc-text"),
+            backgroundUrl = labelGroup.toUrl("background-image"),
+            timeLimit = TimeLimit.Text(
+                prefix = labelGroup.toText("exoired-text"),
+                endText = labelGroup.toText("exoired-value").value
+            ),
+            iconUrl = labelGroup.toUrl("icon-image"),
+            shopName = labelGroup.toText("shop-name"),
+            badgeText = null,
+        )
+
+    private fun ChannelGrid.toGridCouponWidget() =
+        AutomateCouponModel.Grid(
+            type = labelGroup.toText("benefit-type"),
+            benefit = labelGroup.toText("benefit-value"),
+            tnc = labelGroup.toText("tnc-text"),
+            backgroundUrl = labelGroup.toUrl("background-image"),
+            iconUrl = labelGroup.toUrl("icon-image"),
+            shopName = labelGroup.toText("shop-name"),
+            badgeText = null,
+        )
+
+    private fun List<LabelGroup>.getCtaState(): CouponWidgetDataItemModel.CtaState {
+        val label = first { it.position == "cta-text" }
+        return when (label.type) {
+            "claim" -> CouponWidgetDataItemModel.CtaState.Claim
+            "redirect" -> CouponWidgetDataItemModel.CtaState.Redirect
+            else -> CouponWidgetDataItemModel.CtaState.OutOfStock
+        }
+    }
+
+    private fun List<LabelGroup>.toText(positionType: String): DynamicColorText {
+        val label = first { it.position == positionType }
+        val textColor = label.styles.first { it.key == LabelGroupStyle.TEXT_COLOR }
+        return DynamicColorText(label.title, textColor.value)
+    }
+
+    private fun List<LabelGroup>.toUrl(position: String): String {
+        return first { it.position == position }.url
+    }
+}
