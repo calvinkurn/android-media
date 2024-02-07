756141714d7af1124fea61fff766990b10d77998
yogieputra8 <yogie.putra@tokopedia.com>
2024-02-07 14:17:49 +0700

    Add more UT scenarios to shop_showcase modules

--- features/merchant/shop_showcase/src/test/java/com/tokopedia/shop_showcase/viewmodel/shopshowcaseadd/ShopShowCaseAddViewModelTest.kt
+++ features/merchant/shop_showcase/src/test/java/com/tokopedia/shop_showcase/viewmodel/shopshowcaseadd/ShopShowCaseAddViewModelTest.kt
@@ -421,48 +421,280 @@ class ShopShowCaseAddViewModelTest : ShopShowCaseAddViewModelTestFixture() {
             assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
             assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
             assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Success)
-            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<RemoveShowcaseProductResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(2) as Result<RemoveShowcaseProductResponse> is Success)
         }
     }
 
     @Test
     fun `update shop showcase name, append 1 product and remove 1 product return Fail update showcase name, Success append product and Success remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            coEvery {
+                updateShopShowcaseUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAppendAndRemoveProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct, removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Fail)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(2) as Result<RemoveShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 1 product and remove 1 product return Success update showcase name, Fail append product and Success remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            coEvery {
+                appendShopShowcaseProductUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAppendAndRemoveProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct, removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Fail)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(2) as Result<RemoveShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 1 product and remove 1 product return Success update showcase name, Success append product and Fail remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            coEvery {
+                removeShopShowcaseProductUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAppendAndRemoveProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct, removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(2) as Result<RemoveShowcaseProductResponse> is Fail)
+        }
     }
     // ========================================== //
 
     // Update showcase name, append 0 product & remove 1 product
     @Test
     fun `update shop showcase name, append 0 product and remove 1 product return Success update showcase name and Success remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(listAppended = arrayListOf())
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowcaseNameAndRemoveProductUseCaseCalled(removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<RemoveShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 0 product and remove 1 product return Fail update showcase name and Success remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(listAppended = arrayListOf())
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            coEvery {
+                updateShopShowcaseUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowcaseNameAndRemoveProductUseCaseCalled(removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Fail)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<RemoveShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 0 product and remove 1 product return Success update showcase name and Fail remove product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(listAppended = arrayListOf())
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf(
+                    RemovedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+
+            coEvery {
+                removeShopShowcaseProductUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowcaseNameAndRemoveProductUseCaseCalled(removedProductParam = mockRemovedProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<RemoveShowcaseProductResponse> is Fail)
+        }
     }
     // ========================================== //
 
     // Update showcase name, append 1 product & remove 0 product
     @Test
     fun `update shop showcase name, append 1 product and remove 0 product return Success update showcase name and Success append product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf()
+            )
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAndAppendProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 1 product and remove 0 product return Fail update showcase name and Success append product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf()
+            )
+
+            coEvery {
+                updateShopShowcaseUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAndAppendProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Fail)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Success)
+        }
     }
 
     @Test
     fun `update shop showcase name, append 1 product and remove 0 product return Success update showcase name and Fail append product`() {
+        runBlocking {
+            mockkObject(UpdateShopShowcaseUseCase)
+            mockkObject(AppendShopShowcaseProductUseCase)
+            mockkObject(RemoveShopShowcaseProductUseCase)
+            onUpdateShopShowcaseNameAppendAndRemoveProduct_thenReturn()
+            val mockAppendProduct = AppendShowcaseProductParam(
+                listAppended = arrayListOf(
+                    AppendedProduct(product_id = anyString(), menu_id = anyString())
+                )
+            )
+            val mockRemovedProduct = RemoveShowcaseProductParam(
+                listRemoved = arrayListOf()
+            )
+
+            coEvery {
+                appendShopShowcaseProductUseCase.executeOnBackground()
+            } throws Exception()
+
+            shopShowCaseAddViewModel.updateShowcaseAppendAndRemoveProduct(data = UpdateShopShowcaseParam(), newAppendedProduct = mockAppendProduct, removedProduct = mockRemovedProduct)
+            shopShowCaseAddViewModel.coroutineContext[Job]?.children?.forEach { it.join() }
+            verifySuccessUpdateShopShowCaseNameAndAppendProductUseCaseCalled(appendShowcaseProductParam = mockAppendProduct)
+
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.isNotEmpty() == true)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(0) as Result<UpdateShopShowcaseResponse> is Success)
+            assertTrue(shopShowCaseAddViewModel.listOfAppendAndRemoveResponse.value?.get(1) as Result<AppendShowcaseProductResponse> is Fail)
+        }
     }
     // ========================================== //
 
