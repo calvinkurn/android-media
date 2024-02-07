757c51866b203f29277223d1c84b6cfadb141ed9
habibitkp <101687909+habibitkp@users.noreply.github.com>
2024-02-07 16:56:01 +0700

    implement ui test privacy center main page

--- features/user/privacycenter/src/androidTest/java/com/tokopedia/privacycenter/main/PrivacyCenterActivityTest.kt
+++ features/user/privacycenter/src/androidTest/java/com/tokopedia/privacycenter/main/PrivacyCenterActivityTest.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tokopedia.abstraction.base.app.BaseMainApplication
 import com.tokopedia.privacycenter.di.ActivityComponentFactory
+import com.tokopedia.privacycenter.main.di.ConsentWithdrawalState
 import com.tokopedia.privacycenter.main.di.DaggerTestAppComponent
 import com.tokopedia.privacycenter.main.di.FakeActivityComponentFactory
 import com.tokopedia.privacycenter.main.di.FakeAppModule
+import com.tokopedia.privacycenter.main.di.FakeGraphqlRepository
+import com.tokopedia.privacycenter.main.di.RecommendationState
 import com.tokopedia.privacycenter.ui.main.PrivacyCenterActivity
 import com.tokopedia.test.application.annotations.UiTest
 import com.tokopedia.test.application.util.InstrumentationAuthHelper
@@ -16,14 +20,18 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @UiTest
+@RunWith(AndroidJUnit4::class)
 class PrivacyCenterActivityTest {
 
     @get:Rule
     var activityTestRule = IntentsTestRule(PrivacyCenterActivity::class.java, false, false)
 
     private lateinit var ctx: Context
+
+    private lateinit var repositoryStub: FakeGraphqlRepository
 
     @Before
     fun setUp() {
@@ -33,6 +41,7 @@ class PrivacyCenterActivityTest {
 //        fakeGql = component.fakeGraphql() as FakeGraphqlUseCase
         ApplicationProvider.getApplicationContext<BaseMainApplication>().setComponent(component)
         ActivityComponentFactory.instance = FakeActivityComponentFactory()
+        repositoryStub = component.graphqlRepository() as FakeGraphqlRepository
     }
 
     @After
@@ -43,12 +52,9 @@ class PrivacyCenterActivityTest {
     @Test
     fun basic_test() {
         activityTestRule.launchActivity(Intent())
-        Thread.sleep(500)
 
         privacyCenterRobot { } assert {
-            shouldShowCorrectName("Hai, Erick Samuel (test)")
-            shouldShowIconLinked()
-            shouldShowRecommendationSection()
+            shouldShowCorrectHeader(name = "Hai, Erick Samuel (test)")
         }
 
         privacyCenterRobot {
@@ -56,6 +62,115 @@ class PrivacyCenterActivityTest {
             clickRiwayatKebijakan()
         } assert {
             shouldDisplayPrivacyTestData()
+        }
+    }
+
+    @Test
+    fun check_activity_section() {
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot { } assert {
+            shouldShowActivitySection()
+        }
+    }
+
+    @Test
+    fun check_recommendation_section_opt_in() {
+        repositoryStub.setRecommendationResponse(RecommendationState.RECOMMENDATION_FRIEND_OPT_IN)
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot { } assert {
+            shouldShowRecommendationSectionSuccess()
+            shouldShowRecommendationShareFriend(isActivated = true)
+        }
+    }
+
+    @Test
+    fun check_recommendation_section_opt_out() {
+        repositoryStub.setRecommendationResponse(RecommendationState.RECOMMENDATION_FRIEND_OPT_OUT)
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot { } assert {
+            shouldShowRecommendationSectionSuccess()
+            shouldShowRecommendationShareFriend(isActivated = false)
+        }
+    }
+
+    @Test
+    fun check_recommendation_section_failed() {
+        repositoryStub.setRecommendationResponse(RecommendationState.RECOMMENDATION_FRIEND_FAILED)
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot { } assert {
+            shouldShowRecommendationSectionFailed()
+        }
+    }
+
+    @Test
+    fun check_consent_withdrawal_section_success() {
+        repositoryStub.setConsentWithdrawalResponse(ConsentWithdrawalState.CONSENT_WITHDRAWAL_SUCCESS)
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+
+        } assert {
+            shouldShowConsentWithdrawalSuccess()
+        }
+    }
+
+    @Test
+    fun check_consent_withdrawal_section_failed() {
+        repositoryStub.setConsentWithdrawalResponse(ConsentWithdrawalState.CONSENT_WITHDRAWAL_FAILED)
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+
+        } assert {
+            shouldShowConsentWithdrawalFailed()
+        }
+    }
+
+    @Test
+    fun check_privacy_policy_section() {
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+            scrollToBottom()
+        } assert {
+            shouldShowPrivacyPolicy()
+        }
+    }
+
+    @Test
+    fun check_faq_section() {
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+            scrollToBottom()
+        } assert {
+            shouldShowFaq()
+        }
+    }
+
+    @Test
+    fun check_tokopedia_care_section() {
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+            scrollToFooterImage()
+        } assert {
+            shouldShowTokopediaCare()
+        }
+    }
+
+    @Test
+    fun check_image_footer_section() {
+        activityTestRule.launchActivity(Intent())
+
+        privacyCenterRobot {
+            scrollToFooterImage()
+        } assert {
+            shouldShowFooterImage()
         }
     }
 }
--- features/user/privacycenter/src/androidTest/java/com/tokopedia/privacycenter/main/PrivacyCenterRobot.kt
+++ features/user/privacycenter/src/androidTest/java/com/tokopedia/privacycenter/main/PrivacyCenterRobot.kt
@@ -5,6 +5,8 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.tokopedia.privacycenter.R
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.not
 
 class PrivacyCenterRobot {
     fun scrollToBottom() {
@@ -12,24 +14,99 @@ class PrivacyCenterRobot {
             .perform(nestedScrollTo())
     }
 
+    fun scrollToFooterImage() {
+        onView(withId(R.id.img_footer_privacy_center))
+            .perform(nestedScrollTo())
+    }
+
     fun clickRiwayatKebijakan() {
-        onView(withId(R.id.menuListPrivacyPolicy)).perform(click())
+        onView(withText("Riwayat Pemberitahuan Privasi")).perform(click())
     }
 }
 
 class PrivacyResultRobot {
-    fun shouldShowCorrectName(name: String) {
+    fun shouldShowCorrectHeader(name: String) {
         onView(withId(R.id.text_name)).check(matches(withText(name)))
+        onView(withId(R.id.text_desc)).check(matches(withText("Penggunaan datamu bisa diatur di sini")))
     }
-    fun shouldShowIconLinked() {
-        onView(withId(R.id.ic_account_linked)).check(matches(isDisplayed()))
-    }
-    fun shouldShowRecommendationSection() {
+    fun shouldShowRecommendationSectionSuccess() {
         onView(withText("Rekomendasi & promo")).check(matches(isDisplayed()))
+        onView(withText("Pakai perangkatmu buat penawaran spesial")).check(matches(isDisplayed()))
+        onView(withText("Shake Shake")).check(matches(isDisplayed()))
+        onView(withText("Goyang HP untuk dapat promo menarik")).check(matches(isDisplayed()))
+        onView(withText("Geolokasi")).check(matches(isDisplayed()))
+        onView(withText("Atur rekomendasi berdasarkan lokasi")).check(matches(isDisplayed()))
+        onView(withText("Rekomendasi Teman di Feed")).check(matches(isDisplayed()))
+        onView(withText("Tampilkan teman yang bisa di-follow")).check(matches(isDisplayed()))
     }
+
+    fun shouldShowRecommendationSectionFailed() {
+        onView(withText("Rekomendasi & promo")).check(matches(isDisplayed()))
+        onView(withText("Pakai perangkatmu buat penawaran spesial")).check(matches(isDisplayed()))
+        onView(withText("Shake Shake")).check(matches(not(isDisplayed())))
+        onView(withText("Goyang HP untuk dapat promo menarik")).check(matches(not(isDisplayed())))
+        onView(withText("Geolokasi")).check(matches(not(isDisplayed())))
+        onView(withText("Atur rekomendasi berdasarkan lokasi")).check(matches(not(isDisplayed())))
+        onView(withText("Rekomendasi Teman di Feed")).check(matches(not(isDisplayed())))
+        onView(withText("Tampilkan teman yang bisa di-follow")).check(matches(not(isDisplayed())))
+    }
+
+    fun shouldShowConsentWithdrawalSuccess() {
+        onView(withText("Persetujuan kamu")).check(matches(isDisplayed()))
+        onView(withText("Atur fitur apa aja yang boleh akses datamu")).check(matches(isDisplayed()))
+        onView(withText("Testing DPPO Portal")).check(matches(isDisplayed()))
+        onView(withText("Nanti akan ada fitur lain yang bisa kamu atur persetujuannya di sini.")).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowConsentWithdrawalFailed() {
+        onView(withText("Persetujuan kamu")).check(matches(isDisplayed()))
+        onView(withText("Atur fitur apa aja yang boleh akses datamu")).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowPrivacyPolicy() {
+        onView(withText("Pemberitahuan Privasi Tokopedia")).check(matches(isDisplayed()))
+        onView(withText("Pemberitahuan Privasi menjelaskan data pribadimu yang disimpan, bagaimana Tokopedia menggunakannya, dan kenapa Tokopedia memprosesnya.")).check(matches(isDisplayed()))
+        onView(withText("Cek Pemberitahuan Privasi Tokopedia")).check(matches(isDisplayed()))
+        onView(withText("Riwayat Pemberitahuan Privasi")).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowFaq() {
+        onView(withText("Punya pertanyaan seputar privasi?")).check(matches(isDisplayed()))
+        onView(withText("Temukan semua jawabannya disini")).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowTokopediaCare() {
+        onView(withText("Butuh info lebih lanjut?")).check(matches(isDisplayed()))
+        onView(withText("Hubungi Tokopedia Care")).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowFooterImage() {
+        onView(withId(R.id.img_footer_privacy_center)).check(matches(isDisplayed()))
+    }
+
+    fun shouldShowActivitySection() {
+        onView(withText("Aktivitas")).check(matches(isDisplayed()))
+        onView(withText("Atur data dari aktivitasmu di Tokopedia")).check(matches(isDisplayed()))
+        onView(withText("Bagikan Wishlist")).check(matches(isDisplayed()))
+        onView(withText("Riwayat Pencarian")).check(matches(isDisplayed()))
+    }
+
     fun shouldDisplayPrivacyTestData() {
         onView(withText("Privacy test - 06 Apr 2021"))
             .check(matches(isDisplayed()))
+    }
+
+    fun shouldShowRecommendationShareFriend(isActivated: Boolean) {
+        onView(allOf(withId(R.id.itemSwitch), isDescendantOfA(withId(R.id.item_recommendation_friend)))).check(matches(isDisplayed()))
+        onView(allOf(withId(R.id.itemSwitch), isDescendantOfA(withId(R.id.item_recommendation_friend)))).check(
+            matches(
+                if (isActivated) {
+                    isChecked()
+                } else {
+                    isNotChecked()
+                }
+            )
+        )
     }
 }
 
