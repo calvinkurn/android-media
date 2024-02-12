b1642b34503c12d110b883aa338d1c7613c97475
ilhamsuaib <ilhamsuaib10@gmail.com>
2024-02-12 15:33:53 +0700

    `SellerApp` : fix : add missing deeplink path gamification in DeeplinkDFApp

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFApp.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFApp.kt
@@ -513,7 +513,8 @@ object DeeplinkDFApp {
     private fun getDfPromoGamification() = mutableListOf(
         // gamification
         DFP(INTERNAL, HOST_GLOBAL, PathType.PATTERN, "/gamification_gift_daily"),
-        DFP(INTERNAL, HOST_GLOBAL, PathType.PATTERN, "/gamification_gift_60s")
+        DFP(INTERNAL, HOST_GLOBAL, PathType.PATTERN, "/gamification_gift_60s"),
+        DFP(INTERNAL, HOST_GLOBAL, PathType.PATTERN, "/ketupat_rewards_landing_page")
     )
 
     private fun getDfPromoTokopoints() = mutableListOf(
