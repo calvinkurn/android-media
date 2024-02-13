dfd0bfd40fba5821a1282020dbd4edfab58aaf9b
andri.fashbir <andri.fashbir@tokopedia.com>
2024-02-13 14:35:30 +0700

    Handle product carousel load more on Festive section

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/usecase/productCardCarouselUseCase/ProductCardsUseCase.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/usecase/productCardCarouselUseCase/ProductCardsUseCase.kt
@@ -145,6 +145,9 @@ class ProductCardsUseCase @Inject constructor(private val productCardsRepository
     suspend fun getCarouselPaginatedData(componentId: String, pageEndPoint: String, productsLimit: Int = PRODUCT_PER_PAGE): Boolean {
         val component = getComponent(componentId, pageEndPoint)
         val paramWithoutRpc = getMapWithoutRpc(pageEndPoint)
+
+        if (isAlreadyLoaded(component)) return false
+
         component?.let {
             it.properties?.let { properties ->
                 if (properties.limitProduct && properties.limitNumber.toIntOrZero() == it.getComponentsItem()?.size) return false
