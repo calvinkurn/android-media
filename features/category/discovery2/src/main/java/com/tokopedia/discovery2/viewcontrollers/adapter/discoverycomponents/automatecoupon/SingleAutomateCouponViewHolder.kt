6a11786dabba07bb24841b38cf93c3658370f23f
andri.fashbir <andri.fashbir@tokopedia.com>
2024-02-15 14:19:19 +0700

    Handle claim action on automate coupon

--- features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/automatecoupon/SingleAutomateCouponViewHolder.kt
+++ features/category/discovery2/src/main/java/com/tokopedia/discovery2/viewcontrollers/adapter/discoverycomponents/automatecoupon/SingleAutomateCouponViewHolder.kt
@@ -3,10 +3,13 @@ package com.tokopedia.discovery2.viewcontrollers.adapter.discoverycomponents.aut
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
+import com.tokopedia.applink.RouteManager
 import com.tokopedia.discovery2.databinding.SingleAutomateCouponLayoutBinding
 import com.tokopedia.discovery2.di.getSubComponent
 import com.tokopedia.discovery2.viewcontrollers.activity.DiscoveryBaseViewModel
 import com.tokopedia.discovery2.viewcontrollers.adapter.viewholder.AbstractViewHolder
+import com.tokopedia.discovery_component.widgets.automatecoupon.AutomateCouponModel
+import com.tokopedia.discovery_component.widgets.automatecoupon.ButtonState
 import com.tokopedia.utils.view.binding.viewBinding
 
 class SingleAutomateCouponViewHolder(
@@ -33,9 +36,29 @@ class SingleAutomateCouponViewHolder(
         lifecycleOwner?.let {
             viewModel?.getComponentList()?.observe(it) { items ->
                 items.firstOrNull()?.automateCoupons?.firstOrNull()?.let { model ->
-                    binding?.couponView?.setModel(model)
+                    binding?.renderCoupon(model)
                 }
             }
+
+            viewModel?.getRedirectLink()?.observe(it) {
+                binding?.couponView?.setState(
+                    ButtonState.Redirection {
+                        RouteManager.route(fragment.requireContext(), it)
+                    }
+                )
+            }
+        }
+    }
+
+    private fun SingleAutomateCouponLayoutBinding?.renderCoupon(model: AutomateCouponModel) {
+        this?.couponView?.apply {
+            setModel(model)
+
+            setState(
+                ButtonState.Claim {
+                    viewModel?.claim()
+                }
+            )
         }
     }
 
