00e2474e67f2f22beacd7f93844a1ca02bf17605
fajarca <fajar.azhar@tokopedia.com>
2024-02-16 15:03:19 +0700

    Disable direct purchase (qty editor) temporary

--- features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/extension/ProductCardExtension.kt
+++ features/merchant/shop_page/src/main/java/com/tokopedia/shop/common/extension/ProductCardExtension.kt
@@ -4,5 +4,5 @@ import com.tokopedia.productcard.ProductCardModel
 
 //TODO: Remove this ext function once product card v5 global component has quantity editor capability
 fun ProductCardModel.disableDirectPurchaseCapability(): ProductCardModel {
-    return this.copy(hasAddToCartButton = false)
+    return this.copy(hasAddToCartButton = false, nonVariant = null, variant = null)
 }
