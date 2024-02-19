de983a788f034298f6e488c2ec69033601eaf643
fachrizalmrsln <fachrizal.mursalin@tokopedia.com>
2024-02-19 16:34:55 +0700

    `fix`: gesture overlapping

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/components/items/ItemImageProductPreview.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/components/items/ItemImageProductPreview.kt
@@ -1,19 +1,35 @@
 package com.tokopedia.content.product.preview.view.components.items
 
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import com.tokopedia.content.product.preview.view.components.pinchToZoomModifier
+import androidx.compose.ui.graphics.graphicsLayer
 import com.tokopedia.nest.components.NestImage
 import com.tokopedia.nest.principles.ui.NestTheme
 import com.tokopedia.nest.principles.utils.ImageSource
 
 @Composable
 fun ItemImageProductPreview(imageUrl: String) {
-    val scale = remember { mutableStateOf(1f) }
-    val offset = remember { mutableStateOf(Offset(0f, 0f)) }
+    var scale by remember { mutableStateOf(DEFAULT_SCALING) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, _ ->
+        scale *= if (scale < DEFAULT_SCALING) {
+            if (zoomChange < DEFAULT_SCALING) {
+                DEFAULT_SCALING
+            } else {
+                zoomChange
+            }
+        } else {
+            zoomChange
+        }
+        offset += offsetChange
+    }
 
     NestTheme(darkTheme = true) {
         NestImage(
@@ -21,7 +37,16 @@ fun ItemImageProductPreview(imageUrl: String) {
                 source = imageUrl,
                 loaderType = ImageSource.Remote.LoaderType.NONE
             ),
-            modifier = Modifier.pinchToZoomModifier(scale = scale, offset = offset)
+            modifier = Modifier
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                .transformable(state)
         )
     }
 }
+
+private const val DEFAULT_SCALING = 1f
