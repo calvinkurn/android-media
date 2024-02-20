b42b0f86cec960ba4787bdc6a58643f86c5b0784
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2024-02-20 11:24:48 +0700

    replace glide usage on features/promo/promo

--- features/promo/promotions_target/src/main/java/com/tokopedia/promotionstarget/presentation/Extensions.kt
+++ features/promo/promotions_target/src/main/java/com/tokopedia/promotionstarget/presentation/Extensions.kt
@@ -3,74 +3,53 @@ package com.tokopedia.promotionstarget.presentation
 import android.graphics.Bitmap
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
-import android.graphics.drawable.Drawable
 import android.text.TextUtils
 import android.util.DisplayMetrics
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.target.Target
-import com.bumptech.glide.request.transition.Transition
-import com.tokopedia.promotionstarget.R
+import com.tokopedia.media.loader.getBitmapImageUrl
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.media.loader.utils.MediaBitmapEmptyTarget
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 fun AppCompatImageView.loadImageGlide(url: String?, onLoadingFinished: (success: Boolean) -> Unit = {}) {
     if (!TextUtils.isEmpty(url)) {
-        Glide.with(context)
-                .asBitmap()
-                .load(url)
-                .placeholder(R.drawable.t_promo_placeholder)
-                .dontAnimate()
-                .dontTransform()
-                .into(object : CustomTarget<Bitmap>() {
-                    override fun onLoadFailed(errorDrawable: Drawable?) {
-                        super.onLoadFailed(errorDrawable)
-                        onLoadingFinished(false)
-                    }
-
-                    override fun onLoadCleared(placeholder: Drawable?) {
-                        onLoadingFinished(false)
-                    }
-
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        this@loadImageGlide.setImageBitmap(resource)
-                        onLoadingFinished(true)
-                    }
-                })
+        url?.getBitmapImageUrl(context, {
+            isAnimate(false)
+        }, MediaBitmapEmptyTarget(
+            onReady = {
+                onLoadingFinished(true)
+            },
+            onFailed = {
+                onLoadingFinished(false)
+            },
+            onCleared = {
+                onLoadingFinished(false)
+            }
+        ))
     }
 }
 
 fun AppCompatImageView.loadImageWithNoPlaceholder(url: String?, onLoadingFinished: (success: Boolean) -> Unit = {}) {
     if (!TextUtils.isEmpty(url)) {
-        Glide.with(context)
-                .load(url)
-                .dontAnimate()
-                .listener(object : RequestListener<Drawable> {
-                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
-                        onLoadingFinished(true)
-                        return false
-                    }
-
-                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
-                        onLoadingFinished(false)
-                        return false
-                    }
-                })
-                .into(this)
+        url?.getBitmapImageUrl(context, {
+           isAnimate(false)
+        }, MediaBitmapEmptyTarget(
+            onReady = {
+                onLoadingFinished(true)
+            },
+            onFailed = {
+                onLoadingFinished(false)
+            }
+        ))
     }
 }
 
 
 fun AppCompatImageView.loadImageGlide(id: Int) {
-    Glide.with(context)
-            .load(id)
-            .into(this)
+    this.loadImage(id)
 }
 
 fun View.dpToPx(dp: Int): Float {
