9266cb86ba5b5c7389aaae1da013686443d44179
calvinkurniawan <calvin.kurniawan@tokopedia.com>
2024-02-20 11:28:28 +0700

    replace glide usage on features/promo/tokopoints

--- features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/util/ImageUtil.kt
+++ features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/util/ImageUtil.kt
@@ -3,8 +3,8 @@ package com.tokopedia.tokopoints.view.util
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
 import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.Target
+import com.tokopedia.media.loader.loadImage
+import com.tokopedia.design.R as designR
 
 object ImageUtil {
     fun dimImage(imageView: ImageView?) {
@@ -23,13 +23,11 @@ object ImageUtil {
 
     fun loadImage(imageView: ImageView, url: String?) {
         try {
-            Glide.with(imageView.context)
-                .load(url)
-                .dontAnimate()
-                .placeholder(com.tokopedia.design.R.drawable.ic_loading_image)
-                .error(com.tokopedia.design.R.drawable.ic_loading_image)
-                .override(Target.SIZE_ORIGINAL,Target.SIZE_ORIGINAL)
-                .into(imageView)
+            imageView.loadImage(url){
+                isAnimate(false)
+                setPlaceHolder(designR.drawable.ic_loading_image)
+                setErrorDrawable(designR.drawable.ic_loading_image)
+            }
         } catch (e: Exception) {
             e.printStackTrace()
         }
