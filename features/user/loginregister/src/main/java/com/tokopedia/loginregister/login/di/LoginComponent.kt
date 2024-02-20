d61c8c8760091527b8033fa53972abbcd0761006
rama.putra <rama.putra@tokopedia.com>
2024-02-20 11:32:43 +0700

    Remove rpn

--- features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginComponent.kt
+++ features/user/loginregister/src/main/java/com/tokopedia/loginregister/login/di/LoginComponent.kt
@@ -3,7 +3,6 @@ package com.tokopedia.loginregister.login.di
 import com.tokopedia.abstraction.common.di.component.BaseAppComponent
 import com.tokopedia.abstraction.common.di.scope.ActivityScope
 import com.tokopedia.loginregister.login.service.GetDefaultChosenAddressService
-import com.tokopedia.loginregister.login.service.RegisterPushNotifService
 import com.tokopedia.loginregister.login.view.bottomsheet.NeedHelpBottomSheet
 import com.tokopedia.loginregister.login.view.fragment.LoginEmailPhoneFragment
 import com.tokopedia.loginregister.login.view.fragment.SellerSeamlessLoginFragment
@@ -23,7 +22,6 @@ import dagger.Component
 interface LoginComponent {
     fun inject(fragment: LoginEmailPhoneFragment)
     fun inject(fragment: SellerSeamlessLoginFragment)
-    fun inject(service: RegisterPushNotifService)
     fun inject(getDefaultChosenAddressService: GetDefaultChosenAddressService?)
     fun inject(bottomSheet: NeedHelpBottomSheet)
 }
