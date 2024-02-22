f6ea87e2ae21e6c4e73a5fd955f3a08e9009c141
Yusuf Hendrawan <yusuf.hendrawan@bytedance.com>
2024-02-22 11:36:18 +0700

    feat(om): Remove unused AddOn fields

--- features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/domain/models/AddonSummary.kt
+++ features/ordermanagement/buyer_order_detail/src/main/java/com/tokopedia/buyerorderdetail/domain/models/AddonSummary.kt
@@ -6,40 +6,22 @@ import com.google.gson.annotations.SerializedName
 data class AddonSummary(
     @SerializedName("addons")
     val addons: List<Addon>? = listOf(),
-    @SerializedName("total")
-    val total: Int = 0,
-    @SerializedName("total_price")
-    val totalPrice: Double = 0.0,
     @SerializedName("total_price_str")
-    val totalPriceStr: String = "",
-    @SerializedName("total_quantity")
-    val totalQuantity: Int = 0
+    val totalPriceStr: String = ""
 ) {
     data class Addon(
         @SerializedName("id")
         val id: String = "0",
         @SerializedName("image_url")
         val imageUrl: String = "",
-        @SerializedName("level")
-        val level: Int = 0,
         @SerializedName("metadata")
         val metadata: Metadata? = Metadata(),
         @SerializedName("name")
         val name: String = "",
-        @SerializedName("order_id")
-        val orderId: String = "0",
-        @SerializedName("price")
-        val price: Double = 0.0,
         @SerializedName("price_str")
         val priceStr: String = "",
         @SerializedName("quantity")
         val quantity: Int = 0,
-        @SerializedName("reference_id")
-        val referenceId: Long = 0,
-        @SerializedName("subtotal_price")
-        val subtotalPrice: Double = 0.0,
-        @SerializedName("subtotal_price_str")
-        val subtotalPriceStr: String = "",
         @SerializedName("type")
         val type: String = ""
     ) {
