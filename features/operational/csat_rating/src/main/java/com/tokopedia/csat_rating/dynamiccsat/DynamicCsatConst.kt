4c430902d8520391a5e0003b201645d3d771c244
irfankhoirul <irfan.muhlishin@tokopedia.com>
2024-02-23 16:07:02 +0700

    Add csat applink

---
+++ features/operational/csat_rating/src/main/java/com/tokopedia/csat_rating/dynamiccsat/DynamicCsatConst.kt
@@ -0,0 +1,15 @@
+package com.tokopedia.csat_rating.dynamiccsat
+
+object DynamicCsatConst {
+
+    const val EXTRA_CSAT_SELECTED_SCORE = "EXTRA_CSAT_SELECTED_POINT"
+    const val EXTRA_CSAT_DATA = "EXTRA_CSAT_DATA"
+
+    const val CASE_ID = "caseID"
+    const val CASE_CHAT_ID = "caseChatID"
+    const val EMOJI_STATE = "emoji_state"
+    const val SERVICE = "service"
+    const val OTHER_REASON = "other_reason"
+    const val DYNAMIC_REASON = "dynamic_reason"
+
+}
