13d66159a0b980fee15302d007c6f75927e2683a
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2024-02-27 08:12:39 +0700

    [ByteIO Recom] change logic to entrance form

--- libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/Constants.kt
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/Constants.kt
@@ -261,9 +261,3 @@ object AppLogParam {
     const val SALES_PRICE = "sales_price"
     const val MAIN_VIDEO_ID = "main_video_id"
 }
-
-enum class ContentType(val str: String) {
-    VIDEO("video"),
-    LIVE("live"),
-    OTHER("")
-}
