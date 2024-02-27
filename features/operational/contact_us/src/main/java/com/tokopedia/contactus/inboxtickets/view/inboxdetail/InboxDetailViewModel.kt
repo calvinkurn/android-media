57536d654e5ae8a3d7a586153bb61144eabf42b3
irfankhoirul <irfan.muhlishin@tokopedia.com>
2024-02-27 12:55:53 +0700

    Remove dummy csat data

--- features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/inboxdetail/InboxDetailViewModel.kt
+++ features/operational/contact_us/src/main/java/com/tokopedia/contactus/inboxtickets/view/inboxdetail/InboxDetailViewModel.kt
@@ -41,7 +41,6 @@ import com.tokopedia.contactus.utils.CommonConstant.SIZE_ONE
 import com.tokopedia.contactus.utils.CommonConstant.SIZE_ZERO
 import com.tokopedia.csat_rating.data.BadCsatReasonListItem
 import com.tokopedia.csat_rating.dynamiccsat.domain.model.CsatModel
-import com.tokopedia.csat_rating.dynamiccsat.domain.model.PointModel
 import com.tokopedia.usecase.launch_cache_error.launchCatchError
 import com.tokopedia.user.session.UserSessionInterface
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -196,108 +195,7 @@ class InboxDetailViewModel @Inject constructor(
     }
 
     fun getDynamicCsatData(): CsatModel {
-//        return currentState.dynamicCsat
-        // Todo: Remove dummy
-        return CsatModel(
-            caseId = "12345",
-            title = "Gimana pengalamanmu dengan Tokopedia Care?",
-            service = "service",
-            minimumOtherReasonChar = 30,
-            points = mutableListOf(
-                PointModel(
-                    score = 1,
-                    caption = "Sangat Tidak Puas",
-                    reasonTitle = "Apa yang menurut kamu kurang?",
-                    reasons = listOf(
-                        "Reason 1",
-                        "Reason 11",
-                        "Reason 111",
-                        "Tokopedia Care susah diakses",
-                        "Jawaban TANYA (Virtual Assistant) tidak membantu"
-                    ),
-                    otherReasonTitle = "Ada kendala waktu kamu pakai layanan Tokopedia Care?"
-                ),
-                PointModel(
-                    score = 2,
-                    caption = "Tidak Puas",
-                    reasonTitle = "Apa yang menurut kamu kurang?",
-                    reasons = listOf(
-                        "Reason 2",
-                        "Reason 22",
-                        "Reason 222",
-                        "Reason 2",
-                        "Reason 22",
-                        "Reason 222",
-                        "Tokopedia Care susah diakses",
-                        "Jawaban TANYA (Virtual Assistant) tidak membantu"
-                    ),
-                    otherReasonTitle = "Ada kendala waktu kamu pakai layanan Tokopedia Care?"
-                ),
-                PointModel(
-                    score = 3,
-                    caption = "Kurang Puas",
-                    reasonTitle = "Apa yang menurut kamu kurang?",
-                    reasons = listOf(
-                        "Reason 3",
-                        "Reason 33",
-                        "Reason 333",
-                        "Reason 3",
-                        "Reason 33",
-                        "Reason 333",
-                        "Reason 3",
-                        "Reason 33",
-                        "Reason 333",
-                        "Tokopedia Care susah diakses",
-                        "Jawaban TANYA (Virtual Assistant) tidak membantu"
-                    ),
-                    otherReasonTitle = "Ada kendala waktu kamu pakai layanan Tokopedia Care?"
-                ),
-                PointModel(
-                    score = 4,
-                    caption = "Puas",
-                    reasonTitle = "Apa yang menurut kamu kurang?",
-                    reasons = listOf(
-                        "Reason 4",
-                        "Reason 44",
-                        "Reason 444",
-                        "Reason 4444",
-                        "Reason 44444",
-                        "Reason 444444",
-                        "Reason 4444444",
-                        "Reason 44444444",
-                        "Reason 444444444",
-                        "Reason 4444444444",
-                        "Reason 44444444444",
-                        "Reason 444444444444",
-                        "Tokopedia Care susah diakses",
-                        "Jawaban TANYA (Virtual Assistant) tidak membantu"
-                    ),
-                    otherReasonTitle = "Ada kendala waktu kamu pakai layanan Tokopedia Care?"
-                ),
-                PointModel(
-                    score = 5,
-                    caption = "Sangat Puas",
-                    reasonTitle = "Apa yang menurut kamu kurang?",
-                    reasons = listOf(
-                        "Reason 5",
-                        "Reason 55",
-                        "Reason 555",
-                        "Reason 5",
-                        "Reason 55",
-                        "Reason 555",
-                        "Reason 5",
-                        "Reason 55",
-                        "Reason 555",
-                        "Reason 5",
-                        "Reason 55",
-                        "Reason 555",
-                        "Tokopedia Care susah diakses",
-                        "Jawaban TANYA (Virtual Assistant) tidak membantu"
-                    ),
-                    otherReasonTitle = "Ada kendala waktu kamu pakai layanan Tokopedia Care?"
-                )
-            )
-        )
+        return currentState.dynamicCsat
     }
 
     fun refreshLayout() {
