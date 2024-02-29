493597b6adbcf2ad014742f12f5ca61a27bbfaae
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-02-29 18:11:40 +0700

    Implement source previous page as technical solution docs

--- libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/AppLogAnalytics.kt
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/AppLogAnalytics.kt
@@ -156,7 +156,7 @@ object AppLogAnalytics {
 
     internal fun JSONObject.addSourcePreviousPage() {
         // todo need to know what data its expected to be
-        put(SOURCE_PREVIOUS_PAGE, getLastData(SOURCE_PREVIOUS_PAGE))
+        put(SOURCE_PREVIOUS_PAGE, getLastDataBeforeCurrent(PAGE_NAME))
     }
 
     internal fun JSONObject.addSourcePageType() {
