5660772ac4c3cf56c039f18cedcafcb0dd4e154d
Calvyn Julian <calvyn.julian@tokopedia.com>
2024-03-01 14:30:17 +0700

    adjust deeplink

--- libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFApp.kt
+++ libraries/applink/src/main/java/com/tokopedia/applink/DeeplinkDFApp.kt
@@ -253,7 +253,7 @@ object DeeplinkDFApp {
         DFP(INTERNAL, HOST_PLAY_SHORTS, PathType.NO_PATH, ""),
         // live-broadcaster
         DFP(INTERNAL, HOST_MARKETPLACE, PathType.PATH, "/chucker"),
-        DFP(INTERNAL, HOST_MARKETPLACE, PathType.PATTERN, "/shop/.*\\")
+//        DFP(INTERNAL, HOST_MARKETPLACE, PathType.PATTERN, "/shop/.*\\")
 
     )
 
