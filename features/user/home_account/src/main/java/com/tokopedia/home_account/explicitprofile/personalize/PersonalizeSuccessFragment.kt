60b921a5f306d5a374d016c9399205fc697697e2
habibi <habibi@tokopedia.com>
2024-03-05 17:49:12 +0700

    setup timer and set result

--- features/user/home_account/src/main/java/com/tokopedia/home_account/explicitprofile/personalize/PersonalizeSuccessFragment.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/explicitprofile/personalize/PersonalizeSuccessFragment.kt
@@ -1,17 +1,23 @@
 package com.tokopedia.home_account.explicitprofile.personalize
 
 import android.animation.ValueAnimator
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.LottieCompositionFactory
 import com.tokopedia.abstraction.base.view.fragment.TkpdBaseV4Fragment
 import com.tokopedia.home_account.databinding.FragmentPersonalizeSuccessBinding
 import com.tokopedia.home_account.R
 import com.tokopedia.utils.lifecycle.autoClearedNullable
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PersonalizeSuccessFragment: TkpdBaseV4Fragment() {
 
@@ -29,6 +35,21 @@ class PersonalizeSuccessFragment: TkpdBaseV4Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpAnimation()
+        runTimer()
+    }
+
+    private fun runTimer() {
+        lifecycleScope.launch(Dispatchers.Default) {
+            delay(THREE_SECOND)
+            withContext(Dispatchers.Main) {
+                finishResultOk()
+            }
+        }
+    }
+
+    private fun finishResultOk() {
+        activity?.setResult(Activity.RESULT_OK)
+        activity?.finish()
     }
 
     private fun setUpAnimation() {
@@ -53,6 +74,7 @@ class PersonalizeSuccessFragment: TkpdBaseV4Fragment() {
     override fun getScreenName(): String = ""
 
     companion object {
+        private const val THREE_SECOND = 3000L
         const val TAG = "PersonalizeSuccessFragment"
         fun createInstance(): Fragment = PersonalizeSuccessFragment()
     }
