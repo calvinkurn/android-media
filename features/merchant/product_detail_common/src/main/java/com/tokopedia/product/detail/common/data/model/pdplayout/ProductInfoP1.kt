40c22bb3497489bf54da9178e82c1d3735978a00
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-03-06 19:18:10 +0700

    Fix single sku

--- features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/pdplayout/ProductInfoP1.kt
+++ features/merchant/product_detail_common/src/main/java/com/tokopedia/product/detail/common/data/model/pdplayout/ProductInfoP1.kt
@@ -20,7 +20,7 @@ data class ProductInfoP1(
     fun isProductVariant(): Boolean = data.variant.isVariant
 
     val isSingleSku: Boolean
-        get() = if (isProductVariant()) false else data.variants.size == 1
+        get() = if (!isProductVariant()) true else data.variants.size == 1
 
     fun isProductActive(): Boolean = getFinalStock().toIntOrNull() ?: 0 > 0 && basic.isActive()
 
