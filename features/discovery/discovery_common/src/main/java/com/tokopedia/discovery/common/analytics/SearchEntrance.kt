5926213c0e24caf0be22c387dc517d058e6cbeb5
Darian Thedy <darian.thedy@tokopedia.com>
2024-03-07 17:54:30 +0700

    [Search] ByteIO Search Entrance will use IS_MAIN_PARENT

--- features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/analytics/SearchEntrance.kt
+++ features/discovery/discovery_common/src/main/java/com/tokopedia/discovery/common/analytics/SearchEntrance.kt
@@ -1,7 +1,7 @@
 package com.tokopedia.discovery.common.analytics
 
 import com.tokopedia.analytics.byteio.AppLogAnalytics
-import com.tokopedia.analytics.byteio.AppLogParam.ACTIVITY_HASH_CODE
+import com.tokopedia.analytics.byteio.AppLogParam.IS_MAIN_PARENT
 import com.tokopedia.analytics.byteio.AppLogParam.PAGE_NAME
 import com.tokopedia.analytics.byteio.PageName.HOME
 
@@ -17,18 +17,11 @@ class SearchEntrance {
     private fun searchEntrance(): String {
         val pageDataList = AppLogAnalytics.pageDataList
 
-        val homeTabData = pageDataList
-            .find { it[PAGE_NAME] == HOME }
-            ?: return "" // App never goes to home, immediately return ""
-
-        // MainParentActivity hash code
-        val homePageHashCode = homeTabData[ACTIVITY_HASH_CODE]?.toString() ?: return ""
-
         // pageDataList for MainParentActivity
-        val homePageTabDataList = pageDataList.filter { it[ACTIVITY_HASH_CODE]?.toString() == homePageHashCode }
+        val mainParentPageDataList = pageDataList.filter { it[IS_MAIN_PARENT]?.toString().toBoolean() }
 
         // PAGE_NAME of active tab in MainParentActivity
-        val homePageActiveTabPageName = homePageTabDataList.lastOrNull()?.get(PAGE_NAME)?.toString().orEmpty()
+        val homePageActiveTabPageName = mainParentPageDataList.lastOrNull()?.get(PAGE_NAME)?.toString().orEmpty()
 
         // Can only return "homepage" or ""
         return if (homePageActiveTabPageName != HOME) ""
--- features/discovery/search/src/main/java/com/tokopedia/search/result/product/byteio/ByteIOTrackingDataFactoryModule.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/product/byteio/ByteIOTrackingDataFactoryModule.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.search.result.product.byteio
 
+import com.tokopedia.search.di.scope.SearchScope
 import dagger.Binds
 import dagger.Module
 
@@ -7,6 +8,7 @@ import dagger.Module
 abstract class ByteIOTrackingDataFactoryModule {
 
     @Binds
+    @SearchScope
     abstract fun provideByteIOTrackingDataFactory(
         byteIOTrackingDataFactory: ByteIOTrackingDataFactoryImpl,
     ): ByteIOTrackingDataFactory
