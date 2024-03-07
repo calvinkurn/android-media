7836022856c82dedef6a397af5f45aa25de50064
Asti <asti.anzaria@tokopedia.com>
2024-03-07 16:33:36 +0700

    fix: adjust jumping height

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/viewholder/review/ReviewContentViewHolder.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/viewholder/review/ReviewContentViewHolder.kt
@@ -125,7 +125,7 @@ class ReviewContentViewHolder(
 
     private var snapHelperMedia = PagerSnapHelper()
 
-    private val descriptionUiModel = DescriptionUiModel()
+    private val descriptionUiModel : DescriptionUiModel
 
     private val clickableSpan: ClickableSpan =
         object : ClickableSpan() {
@@ -149,6 +149,7 @@ class ReviewContentViewHolder(
         binding.layoutLikeReview.root.setOnClickListener {
             reviewInteractionListener.onLike(false)
         }
+        descriptionUiModel = DescriptionUiModel()
     }
 
     fun bind(item: ReviewContentUiModel) {
@@ -284,6 +285,7 @@ class ReviewContentViewHolder(
             descriptionUiModel.truncatedText = truncatedText
             setupExpanded()
         }
+        binding.tvReviewDescription.maxLines = MAX_LINES_THRESHOLD //Initial state
         tvReviewDescription.show()
     }
 
