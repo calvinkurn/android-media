7ca3c69109de7054dae1f2042a19574c2956cce8
nathaniel.khuana <nathaniel.khuana@bytedance.com>
2024-03-09 10:07:40 +0700

    Remove multiple stack if is shadow more than 1

--- libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/AppLogAnalytics.kt
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/byteio/AppLogAnalytics.kt
@@ -36,7 +36,6 @@ import org.json.JSONObject
 import timber.log.Timber
 import java.lang.ref.WeakReference
 
-
 object AppLogAnalytics {
 
     @JvmField
@@ -214,7 +213,9 @@ object AppLogAnalytics {
             """(%s) 
             |AppLog dId: ${AppLog.getDid()} 
             |userUniqueId: ${AppLog.getUserUniqueID()} 
-            |userId: ${AppLog.getUserUniqueID()}""".trimMargin(), TAG
+            |userId: ${AppLog.getUserUniqueID()}
+            """.trimMargin(),
+            TAG
         )
     }
 
@@ -244,11 +245,17 @@ object AppLogAnalytics {
 
             val shadowPageIndex = index - 1
             // Remove shadow stack
-            if (_pageDataList.getOrNull(shadowPageIndex)?.get(IS_SHADOW) == true) {
-                _pageDataList.removeAt(shadowPageIndex)
-            }
+            removeShadowStack(shadowPageIndex)
         }
         Timber.d("Remove _pageDataList: ${_pageDataList.printForLog()}}")
+    }
+
+    private fun removeShadowStack(currentIndex: Int) {
+        var tempCurrentIndex = currentIndex
+        while (tempCurrentIndex >= 0 && _pageDataList.getOrNull(tempCurrentIndex)?.get(IS_SHADOW) == true) {
+            _pageDataList.removeAt(tempCurrentIndex)
+            tempCurrentIndex--
+        }
     }
 
     private fun clearCurrentPageData() {
@@ -301,8 +308,11 @@ object AppLogAnalytics {
         if (_pageDataList.isEmpty()) return null
         val idx = _pageDataList.lastIndex - 1
 
-        return if (idx >= 0) _pageDataList[idx][key]
-        else null
+        return if (idx >= 0) {
+            _pageDataList[idx][key]
+        } else {
+            null
+        }
     }
 
     fun clearAllPageData() {
@@ -341,7 +351,7 @@ object AppLogAnalytics {
         isAd: Int? = null,
         trackId: String? = null,
         sourcePageType: String? = null,
-        requestId: String? = null,
+        requestId: String? = null
     ) {
         entranceForm?.let {
             putPageData(ENTRANCE_FORM, entranceForm)
