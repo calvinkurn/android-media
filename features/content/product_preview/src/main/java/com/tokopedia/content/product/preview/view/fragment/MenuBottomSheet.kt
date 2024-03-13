0b174f62a411c2880cf33e812ef5eb820b927a31
Asti <asti.anzaria@tokopedia.com>
2024-03-13 11:12:51 +0700

    fix: adjust disposable

--- features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/fragment/MenuBottomSheet.kt
+++ features/content/product_preview/src/main/java/com/tokopedia/content/product/preview/view/fragment/MenuBottomSheet.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.FragmentManager
 import com.tokopedia.content.common.report_content.ThreeDotsPage
 import com.tokopedia.content.common.report_content.model.ContentMenuIdentifier
@@ -54,6 +55,7 @@ class MenuBottomSheet : BottomSheetUnify() {
             setContent {
                 ThreeDotsPage(menuList = menus) { mListener?.onOptionClicked(it) }
             }
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
         }
         setChild(composeView)
         return super.onCreateView(inflater, container, savedInstanceState)
