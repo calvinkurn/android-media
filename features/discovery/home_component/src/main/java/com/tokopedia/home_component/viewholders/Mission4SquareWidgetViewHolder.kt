eff097b5eb8dd928edc8cb450a4f9dd58b69775e
isfaaghyth <isfaaghyth@gmail.com>
2024-03-13 11:59:59 +0700

    feat: setup the viewHolder of a new mission widget in ATF

---
+++ features/discovery/home_component/src/main/java/com/tokopedia/home_component/viewholders/Mission4SquareWidgetViewHolder.kt
@@ -0,0 +1,29 @@
+package com.tokopedia.home_component.viewholders
+
+import android.view.View
+import com.tokopedia.abstraction.base.view.adapter.viewholders.AbstractViewHolder
+import com.tokopedia.home_component.databinding.GlobalComponent4squareMissionWidgetBinding
+import com.tokopedia.home_component.R as home_componentR
+import com.tokopedia.home_component.visitable.MissionWidgetListDataModel
+import com.tokopedia.home_component_header.model.ChannelHeader
+import com.tokopedia.utils.view.binding.viewBinding
+
+class Mission4SquareWidgetViewHolder constructor(
+    view: View
+) : AbstractViewHolder<MissionWidgetListDataModel>(view) {
+
+    private val binding: GlobalComponent4squareMissionWidgetBinding? by viewBinding()
+
+    override fun bind(element: MissionWidgetListDataModel?) {
+        setupHeaderView(element?.header)
+    }
+
+    private fun setupHeaderView(header: ChannelHeader?) {
+        if (header == null) return
+        binding?.headerView?.bind(header)
+    }
+
+    companion object {
+        val LAYOUT = home_componentR.layout.global_component_4square_mission_widget
+    }
+}
