cdfa05dc0dbf839dae9fa9e73abf9841245e3410
faisalramd <faisal.ramdani@bytedance.com>
2024-03-14 11:22:37 +0700

    Fix obfuscated param

--- features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/model/params/add/CustomAttribute.kt
+++ features/merchant/product_add_edit/src/main/java/com/tokopedia/product/addedit/preview/data/model/params/add/CustomAttribute.kt
@@ -1,11 +1,15 @@
 package com.tokopedia.product.addedit.preview.data.model.params.add
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CustomAttribute(
+    @SerializedName("key")
     val key: String = "",
+    @SerializedName("value")
     val value: String = "",
-    val source: String = "",
+    @SerializedName("source")
+    val source: String = ""
 ): Parcelable
