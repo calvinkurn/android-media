8179f71aab6d6c04ef4d0c81db6f36c3dee5111d
kelvinsaputra <kelvin.saputra@tokopedia.com>
2024-03-14 16:59:08 +0700

    `feat`: change review to 2 and remove unused (might be not accurate in the future)

--- features/communication/shareexperience/src/main/java/com/tokopedia/shareexperience/domain/model/ShareExPageTypeEnum.kt
+++ features/communication/shareexperience/src/main/java/com/tokopedia/shareexperience/domain/model/ShareExPageTypeEnum.kt
@@ -8,20 +8,9 @@ import androidx.annotation.Keep
  */
 @Keep
 enum class ShareExPageTypeEnum(val value: String, val valueInt: Int) {
+    OTHERS("others", 0),
     PDP("pdp", 1),
-    SHOP("shop", 2),
-    DISCOVERY("discovery", 3),
-    FLIGHT("flight", 4),
-    CATALOG("catalog", 5),
-    TOKONOW("tokonow", 6),
-    WISHLIST("wishlist", 7),
-    TOKOFOOD("tokofood", 8),
-    PLAY("play", 9),
-    FEED("feed", 10),
-    CATEGORY("category", 11),
-    HELP("help", 12),
-    REVIEW("review", 13),
-    OTHERS("others", 0);
+    REVIEW("review", 2);
 
     companion object {
         fun fromValue(value: String): ShareExPageTypeEnum {
