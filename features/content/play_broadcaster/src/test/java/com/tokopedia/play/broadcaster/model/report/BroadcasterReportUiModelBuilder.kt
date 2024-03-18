dbf0734c328b9eb455c84409cbda268bd508572d
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-03-18 13:42:36 +0700

    feat: add unittest for get live report summary

--- features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/model/report/BroadcasterReportUiModelBuilder.kt
+++ features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/model/report/BroadcasterReportUiModelBuilder.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.play.broadcaster.model.report
 
+import com.tokopedia.play.broadcaster.ui.model.report.live.LiveReportSummaryUiModel
 import com.tokopedia.play.broadcaster.ui.model.report.live.LiveStatsUiModel
 import com.tokopedia.play.broadcaster.ui.model.report.product.ProductReportSummaryUiModel
 import com.tokopedia.play.broadcaster.ui.model.report.product.ProductStatsUiModel
@@ -8,6 +9,29 @@ import com.tokopedia.play.broadcaster.ui.model.report.product.ProductStatsUiMode
  * Created by Jonathan Darwin on 18 March 2024
  */
 class BroadcasterReportUiModelBuilder {
+
+    fun buildLiveReportSummary(
+        estimatedIncome: String = "Rp123.456",
+        viewer: String = "1",
+        totalViewer: String = "3",
+        like: String = "5",
+        timestamp: String = "123",
+        isShuffled: Boolean = true,
+    ) = LiveReportSummaryUiModel(
+        liveStats = listOf(
+            LiveStatsUiModel.EstimatedIncome(estimatedIncome),
+            LiveStatsUiModel.Viewer(viewer),
+            LiveStatsUiModel.TotalViewer(totalViewer),
+            LiveStatsUiModel.Like(like),
+        ).run {
+            if (isShuffled) {
+                shuffled()
+            } else {
+                this
+            }
+        },
+        timestamp = timestamp,
+    )
 
     fun buildProductReportSummary(
         productStatsSize: Int = 5,
--- features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/report/PlayBroadcastReportViewModelTest.kt
+++ features/content/play_broadcaster/src/test/java/com/tokopedia/play/broadcaster/viewmodel/report/PlayBroadcastReportViewModelTest.kt
@@ -6,7 +6,9 @@ import com.tokopedia.play.broadcaster.model.UiModelBuilder
 import com.tokopedia.play.broadcaster.model.report.BroadcasterReportUiModelBuilder
 import com.tokopedia.play.broadcaster.robot.PlayBroadcastViewModelRobot
 import com.tokopedia.play.broadcaster.ui.action.PlayBroadcastAction
+import com.tokopedia.play.broadcaster.ui.model.report.live.LiveStatsUiModel
 import com.tokopedia.play.broadcaster.util.assertEqualTo
+import com.tokopedia.play.broadcaster.util.assertType
 import com.tokopedia.play.broadcaster.util.assertWhenFailed
 import com.tokopedia.play_common.model.result.NetworkResult
 import com.tokopedia.unit.test.rule.CoroutineTestRule
@@ -35,6 +37,7 @@ class PlayBroadcastReportViewModelTest {
     private val mockRepo: PlayBroadcastRepository = mockk(relaxed = true)
 
     private val mockException = uiModelBuilder.buildException()
+    private val mockLiveReportSummary = reportModelBuilder.buildLiveReportSummary()
     private val mockProductReportSummary = reportModelBuilder.buildProductReportSummary()
     private val mockConfig = uiModelBuilder.buildConfigurationUiModel(
         streamAllowed = true,
@@ -46,7 +49,121 @@ class PlayBroadcastReportViewModelTest {
         coEvery { mockRepo.getAccountList() } returns uiModelBuilder.buildAccountListModel()
         coEvery { mockRepo.getChannelConfiguration(any(), any()) } returns mockConfig
         coEvery { mockRepo.getBroadcastingConfig(any(), any()) } returns uiModelBuilder.buildBroadcastingConfigUiModel()
+
+        coEvery { mockRepo.getReportSummary(any(), any()) } returns mockLiveReportSummary
         coEvery { mockRepo.getReportProductSummary(any()) } returns mockProductReportSummary
+    }
+
+    @Test
+    fun `playBroadcaster_report_getReportSummary_success`() {
+
+        val robot = PlayBroadcastViewModelRobot(
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+        )
+
+        robot.use {
+            val state = it.recordState {
+                getAccountConfiguration()
+                it.getViewModel().submitAction(PlayBroadcastAction.GetLiveReportSummary)
+            }
+
+            state.liveReportSummary.liveStats[0].assertType<LiveStatsUiModel.Viewer> {
+                it.value.assertEqualTo(mockLiveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.Viewer>().first().value)
+            }
+
+            state.liveReportSummary.liveStats[1].assertType<LiveStatsUiModel.TotalViewer> {
+                it.value.assertEqualTo(mockLiveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.TotalViewer>().first().value)
+            }
+
+            state.liveReportSummary.liveStats[2].assertType<LiveStatsUiModel.EstimatedIncome> {
+                it.value.assertEqualTo(mockLiveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.EstimatedIncome>().first().value)
+            }
+
+            state.liveReportSummary.liveStats[3].assertType<LiveStatsUiModel.Like> {
+                it.value.assertEqualTo(mockLiveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.Like>().first().value)
+            }
+        }
+    }
+
+    @Test
+    fun `playBroadcaster_report_getReportSummary_error`() {
+        coEvery { mockRepo.getReportSummary(any(), any()) } throws mockException
+
+        val robot = PlayBroadcastViewModelRobot(
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+        )
+
+        robot.use {
+            val prevState = it.recordState {
+                getAccountConfiguration()
+            }
+
+            val currState = it.recordState {
+                it.getViewModel().submitAction(PlayBroadcastAction.GetLiveReportSummary)
+            }
+
+            currState.liveReportSummary.assertEqualTo(prevState.liveReportSummary)
+        }
+    }
+
+    @Test
+    fun `playBroadcaster_report_getReportSummary_raceCondition`() {
+
+        val mockPrevLiveReportSummary = reportModelBuilder.buildLiveReportSummary(
+            estimatedIncome = "2",
+            viewer = "2",
+            totalViewer = "2",
+            like = "2",
+            timestamp = "2"
+        )
+        val mockCurrLiveReportSummary = reportModelBuilder.buildLiveReportSummary(
+            estimatedIncome = "1",
+            viewer = "1",
+            totalViewer = "1",
+            like = "1",
+            timestamp = "1"
+        )
+
+        val robot = PlayBroadcastViewModelRobot(
+            dispatchers = testDispatcher,
+            channelRepo = mockRepo,
+        )
+
+        robot.use {
+
+            coEvery { mockRepo.getReportSummary(any(), any()) } returns mockPrevLiveReportSummary
+
+            val prevState = it.recordState {
+                getAccountConfiguration()
+                it.getViewModel().submitAction(PlayBroadcastAction.GetLiveReportSummary)
+            }
+
+            coEvery { mockRepo.getReportSummary(any(), any()) } returns mockCurrLiveReportSummary
+
+            val currState = it.recordState {
+                it.getViewModel().submitAction(PlayBroadcastAction.GetLiveReportSummary)
+            }
+
+            currState.liveReportSummary.liveStats[0].assertType<LiveStatsUiModel.Viewer> {
+                it.value.assertEqualTo(prevState.liveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.Viewer>().first().value)
+            }
+
+            currState.liveReportSummary.liveStats[1].assertType<LiveStatsUiModel.TotalViewer> {
+                it.value.assertEqualTo(prevState.liveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.TotalViewer>().first().value)
+            }
+
+            currState.liveReportSummary.liveStats[2].assertType<LiveStatsUiModel.EstimatedIncome> {
+                it.value.assertEqualTo(prevState.liveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.EstimatedIncome>().first().value)
+            }
+
+            currState.liveReportSummary.liveStats[3].assertType<LiveStatsUiModel.Like> {
+                it.value.assertEqualTo(prevState.liveReportSummary.liveStats.filterIsInstance<LiveStatsUiModel.Like>().first().value)
+            }
+
+            currState.liveReportSummary.timestamp.assertEqualTo(prevState.liveReportSummary.timestamp)
+        }
     }
 
     @Test
@@ -57,12 +174,12 @@ class PlayBroadcastReportViewModelTest {
         )
 
         robot.use {
-            val states = it.recordStateAsList {
+            val state = it.recordState {
                 getAccountConfiguration()
                 it.getViewModel().submitAction(PlayBroadcastAction.GetProductReportSummary)
             }
 
-            states.last().productReportSummary.assertEqualTo(NetworkResult.Success(mockProductReportSummary))
+            state.productReportSummary.assertEqualTo(NetworkResult.Success(mockProductReportSummary))
         }
     }
 
