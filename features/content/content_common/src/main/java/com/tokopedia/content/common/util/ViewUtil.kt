b9c67eb433341f647074e487b2b2424adadb3109
Asti <asti.anzaria@tokopedia.com>
2024-03-19 11:33:38 +0700

    fix: remove safe click~

--- features/content/content_common/src/main/java/com/tokopedia/content/common/util/ViewUtil.kt
+++ features/content/content_common/src/main/java/com/tokopedia/content/common/util/ViewUtil.kt
@@ -87,11 +87,3 @@ private class SafeClickListener(
         onSafeCLick(v)
     }
 }
-
-fun View.setSafeOnClickListener(onSafeClick: (View) -> Unit) {
-    val safeClickListener = SafeClickListener {
-        onSafeClick(it)
-    }
-    setOnClickListener(safeClickListener)
-}
-
