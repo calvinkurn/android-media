5faff4a419d4cac76f91fe5a4d8ed323fda19f84
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-03-19 07:21:15 +0700

    Create basic unit test

---
+++ libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogAnalyticsTest.kt
@@ -0,0 +1,54 @@
+package com.tokopedia.analytics.byteio
+
+import com.tokopedia.analytics.byteio.AppLogParam.ENTER_FROM
+import com.tokopedia.analytics.byteio.AppLogParam.PAGE_NAME
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class AppLogAnalyticsTest {
+
+    @Before
+    fun setup() {
+        SUT.clearAllPageData()
+    }
+
+    @Test
+    fun `when data is pushed multiple then data on first stack can be obtained`() {
+        val expected = "search123"
+        SUT.pushPageData(ActivityBasicOne)
+        SUT.putPageData("SEARCH_ID", expected)
+        SUT.pushPageData(ActivityBasicTwo)
+
+        val actual = SUT.getLastData("SEARCH_ID")
+        assertEquals(expected, actual)
+        // making sure data is not from ActivityBasicOne
+        assertEquals(SUT.getLastData(PAGE_NAME), "Page Two")
+    }
+
+    @Test
+    fun `when enterFrom activity is pushed then data can be obtained`() {
+        val expected = "Page EnterFrom"
+        SUT.pushPageData(ActivityEnterFrom)
+        SUT.pushPageData(ActivityBasicOne)
+        SUT.pushPageData(ActivityBasicTwo)
+
+        val actual = SUT.getLastData(ENTER_FROM)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `when stack is popped with previous shadow then shadow activity is popped`() {
+        SUT.pushPageData(ActivityBasicOne)
+        SUT.pushPageData(ActivityShadow)
+        SUT.pushPageData(ActivityBasicTwo)
+
+        SUT.removePageData(ActivityBasicTwo)
+        val actual = SUT.getLastData(PAGE_NAME)
+        assertEquals("Page One", actual)
+    }
+
+}
+
+
---
+++ libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogTestHelper.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.analytics.byteio
+
+
+internal typealias SUT = AppLogAnalytics
+
+
+internal val ActivityBasicOne = object : AppLogInterface {
+    override fun getPageName(): String = "Page One"
+}
+
+internal val ActivityBasicTwo = object : AppLogInterface {
+    override fun getPageName(): String = "Page Two"
+}
+
+internal val ActivityEnterFrom = object : AppLogInterface {
+    override fun getPageName(): String = "Page EnterFrom"
+    override fun isEnterFromWhitelisted(): Boolean = true
+}
+
+internal val ActivityShadow = object : AppLogInterface {
+    override fun getPageName(): String = "Shadow"
+    override fun isShadow(): Boolean = true
+}
