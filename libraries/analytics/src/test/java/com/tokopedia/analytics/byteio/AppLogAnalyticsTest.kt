ef852ca6fded721dc1244768a9d3fc3017617583
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-03-19 07:22:11 +0700

    Rename SEARCH_ID

--- libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogAnalyticsTest.kt
+++ libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogAnalyticsTest.kt
@@ -2,6 +2,7 @@ package com.tokopedia.analytics.byteio
 
 import com.tokopedia.analytics.byteio.AppLogParam.ENTER_FROM
 import com.tokopedia.analytics.byteio.AppLogParam.PAGE_NAME
+import com.tokopedia.analytics.byteio.search.AppLogSearch.ParamKey.SEARCH_ID
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -18,10 +19,10 @@ class AppLogAnalyticsTest {
     fun `when data is pushed multiple then data on first stack can be obtained`() {
         val expected = "search123"
         SUT.pushPageData(ActivityBasicOne)
-        SUT.putPageData("SEARCH_ID", expected)
+        SUT.putPageData(SEARCH_ID, expected)
         SUT.pushPageData(ActivityBasicTwo)
 
-        val actual = SUT.getLastData("SEARCH_ID")
+        val actual = SUT.getLastData(SEARCH_ID)
         assertEquals(expected, actual)
         // making sure data is not from ActivityBasicOne
         assertEquals(SUT.getLastData(PAGE_NAME), "Page Two")
