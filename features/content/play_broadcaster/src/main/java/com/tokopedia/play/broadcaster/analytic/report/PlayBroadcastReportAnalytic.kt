1c51616fa7fa5fe753806841bf2dcb7b4b4b24f7
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-03-19 11:49:42 +0700

    feat: add report analytic implementation

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/report/PlayBroadcastReportAnalytic.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/analytic/report/PlayBroadcastReportAnalytic.kt
@@ -39,42 +39,104 @@ class PlayBroadcastReportAnalytic @AssistedInject constructor(
     )
 
     fun impressStatisticIconCoachMark() {
+        analyticManager.sendViewContent(
+            eventAction = "view - estimasi pendapatan coachmark",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50351",
+            sellerAppTrackerId = "50374",
+        )
     }
 
     fun clickLiveStatsArea() {
-
+        analyticManager.sendClickContent(
+            eventAction = "click - top area",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50352",
+            sellerAppTrackerId = "50375",
+        )
     }
 
     fun impressLiveReportBottomSheet() {
-
+        analyticManager.sendViewContent(
+            eventAction = "view - information bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50353",
+            sellerAppTrackerId = "50376",
+        )
     }
 
     fun clickEstimatedIncomeCardOnLiveReport() {
-
+        analyticManager.sendClickContent(
+            eventAction = "click - estimasi pendapatan card bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50354",
+            sellerAppTrackerId = "50377",
+        )
     }
 
     fun clickStatisticIcon() {
-
+        analyticManager.sendClickContent(
+            eventAction = "click - estimasi pendapatan icon",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50355",
+            sellerAppTrackerId = "50378",
+        )
     }
 
     fun impressProductReportBottomSheet() {
-
+        analyticManager.sendViewContent(
+            eventAction = "view - estimasi pendapatan bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50356",
+            sellerAppTrackerId = "50379",
+        )
     }
 
     fun clickEstimatedIncomeInfoIcon() {
-
+        analyticManager.sendClickContent(
+            eventAction = "click - estimasi pendapatan info icon bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50357",
+            sellerAppTrackerId = "50380",
+        )
     }
 
     fun impressEstimatedIncomeInfoBottomSheet() {
-
+        analyticManager.sendViewContent(
+            eventAction = "view - estimasi pendapatan explanation bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50358",
+            sellerAppTrackerId = "50381",
+        )
     }
 
     fun impressProductReportBottomSheetError() {
-
+        analyticManager.sendViewContent(
+            eventAction = "view - estimasi pendapatan error bottomsheet",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50359",
+            sellerAppTrackerId = "50382",
+        )
     }
 
     fun clickEstimatedIncomeCardOnSummaryPage() {
+        analyticManager.sendClickContent(
+            eventAction = "click - estimasi pendapatan icon card",
+            eventLabel = getReportEventLabel(),
+            mainAppTrackerId = "50360",
+            sellerAppTrackerId = "50383",
+        )
+    }
 
+    private fun getReportEventLabel(): String {
+        val account = getAccount().toAnalyticModel()
+
+        return analyticManager.concatLabels(
+            account.id,
+            getChannelId(),
+            getChannelTitle(),
+            account.type,
+        )
     }
 
     companion object {
