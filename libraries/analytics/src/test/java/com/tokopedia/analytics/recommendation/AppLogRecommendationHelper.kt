d08d138ada460651e16fe286f36b9fe86e4870d7
Frenzel Timothy Surya <45281136+frenzelts@users.noreply.github.com>
2024-03-20 07:29:15 +0700

    [ByteIO Recom] add recom applog event unit test

--- libraries/analytics/src/test/java/com/tokopedia/analytics/recommendation/AppLogRecommendationHelper.kt
+++ libraries/analytics/src/test/java/com/tokopedia/analytics/recommendation/AppLogRecommendationHelper.kt
@@ -2,6 +2,7 @@ package com.tokopedia.analytics.recommendation
 
 import com.tokopedia.analytics.byteio.AppLogAnalytics
 import io.mockk.verify
+import org.json.JSONObject
 
 internal fun verifyRecommendationEvent(expectedEvents: Set<String>) {
     verify(exactly = expectedEvents.size) {
@@ -11,6 +12,13 @@ internal fun verifyRecommendationEvent(expectedEvents: Set<String>) {
 
 internal fun verifyRecommendationEvent(expectedEvents: String) {
     verify(exactly = 1) {
+        AppLogAnalytics.send(expectedEvents, any())
+    }
+}
+
+
+internal fun verifyRecommendationEventNot(expectedEvents: String) {
+    verify(inverse = true) {
         AppLogAnalytics.send(expectedEvents, any())
     }
 }
