f4f8d1fd42aaac0e4d1b866380775eda9d0c33e3
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-03-21 16:14:26 +0700

    Add cartids checkout tracker data (#37744)
    * Add cartids checkout tracker data
    
    * Improve cartIds generation occ
    
    * Change os_type to os_name
    
    * Adjust from contract change
    
    * Adjust entrance_info based on contract
    
    * Change buy_type to str in checkout tracker data

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/view/processor/CheckoutProcessor.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/revamp/view/processor/CheckoutProcessor.kt
@@ -359,7 +359,7 @@ class CheckoutProcessor @Inject constructor(
             isExpress = false,
             fingerprintSupport = (fingerprintPublicKey != null).toString(),
             fingerprintPublickey = fingerprintPublicKey ?: "",
-            tracker = AppLogAnalytics.getEntranceInfoForCheckout(AtcBuyType.ATC)
+            tracker = AppLogAnalytics.getEntranceInfoForCheckout(AtcBuyType.ATC, carts.cartIds)
         )
     }
 
