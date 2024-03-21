6a4ab4fced2bf4558d1193062280d59166403fe6
nngarifudin <nanang.arifudin@tokopedia.com>
2024-03-21 13:02:43 +0700

    unit test BudgetingAdsViewModel

--- features/top_ads/top_ads_create/src/test/java/com/tokopedia/topads/view/model/BudgetingAdsViewModelTest.kt
+++ features/top_ads/top_ads_create/src/test/java/com/tokopedia/topads/view/model/BudgetingAdsViewModelTest.kt
@@ -3,7 +3,9 @@ package com.tokopedia.topads.view.model
 import android.content.Context
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
-import com.tokopedia.topads.common.data.model.DataSuggestions
+import com.tokopedia.kotlin.extensions.view.EMPTY
+import com.tokopedia.kotlin.extensions.view.ZERO
+import com.tokopedia.topads.common.data.response.ImpressionPredictionResponse
 import com.tokopedia.topads.common.data.response.KeywordData
 import com.tokopedia.topads.common.data.response.KeywordSuggestionResponse
 import com.tokopedia.topads.common.data.response.ResponseBidInfo
@@ -11,13 +13,18 @@ import com.tokopedia.topads.common.data.response.TopadsBidInfo
 import com.tokopedia.topads.common.domain.interactor.BidInfoUseCase
 import com.tokopedia.topads.common.domain.usecase.SuggestionKeywordUseCase
 import com.tokopedia.topads.common.domain.usecase.TopAdsImpressionPredictionSearchUseCase
+import com.tokopedia.topads.data.TopAdsGetBidSuggestionResponse
 import com.tokopedia.topads.domain.usecase.TopAdsGetBidSuggestionByProductIDsUseCase
 import com.tokopedia.unit.test.rule.CoroutineTestRule
+import com.tokopedia.usecase.coroutines.Fail
+import com.tokopedia.usecase.coroutines.Success
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,16 +46,26 @@ class BudgetingAdsViewModelTest {
     private lateinit var context: Context
     private val bidInfoUseCase: BidInfoUseCase = mockk(relaxed = true)
     private val bidInfoUseCaseDefault: BidInfoUseCase = mockk(relaxed = true)
-    private val topAdsGetBidSuggestionByProductIDsUseCase: TopAdsGetBidSuggestionByProductIDsUseCase = mockk(relaxed = true)
-    private val topAdsImpressionPredictionUseCase: TopAdsImpressionPredictionSearchUseCase = mockk(relaxed = true)
+    private val topAdsGetBidSuggestionByProductIDsUseCase: TopAdsGetBidSuggestionByProductIDsUseCase =
+        mockk(relaxed = true)
+    private val topAdsImpressionPredictionUseCase: TopAdsImpressionPredictionSearchUseCase =
+        mockk(relaxed = true)
     private val suggestionKeywordUseCase: SuggestionKeywordUseCase = mockk(relaxed = true)
 
     @Before
     fun setUp() {
         repository = mockk()
         context = mockk(relaxed = true)
-        viewModel = spyk(BudgetingAdsViewModel(rule.dispatchers,
-            bidInfoUseCase, bidInfoUseCaseDefault, topAdsGetBidSuggestionByProductIDsUseCase, topAdsImpressionPredictionUseCase, suggestionKeywordUseCase))
+        viewModel = spyk(
+            BudgetingAdsViewModel(
+                rule.dispatchers,
+                bidInfoUseCase,
+                bidInfoUseCaseDefault,
+                topAdsGetBidSuggestionByProductIDsUseCase,
+                topAdsImpressionPredictionUseCase,
+                suggestionKeywordUseCase
+            )
+        )
     }
 
 
@@ -57,21 +74,17 @@ class BudgetingAdsViewModelTest {
         val expected = "empty"
         var actual = ""
         val data = listOf<TopadsBidInfo.DataItem>()
-        val onEmpty: () -> Unit = {
-            actual = "empty"
-        }
-        val onSuccess: (List<TopadsBidInfo.DataItem>) -> Unit = {
-            if (it.isEmpty()) {
-                onEmpty()
-            }
-        }
         every {
             bidInfoUseCase.executeQuerySafeMode(captureLambda(), any())
         } answers {
-            onSuccess.invoke(data)
+            firstArg<(ResponseBidInfo.Result) -> Unit>().invoke(
+                ResponseBidInfo.Result(TopadsBidInfo(data = data))
+            )
         }
         viewModel.getBidInfo(
-            suggestions = listOf(), onSuccess = onSuccess, onEmpty = onEmpty
+            suggestions = listOf(), onSuccess = {}, onEmpty = {
+                actual = "empty"
+            }
         )
         Assert.assertEquals(expected, actual)
     }
@@ -81,20 +94,22 @@ class BudgetingAdsViewModelTest {
     fun `check onSuccess invocation in getBidInfo`() {
         val expected = "1000"
         var actual = "0"
-        val bidInfoData: ResponseBidInfo.Result = ResponseBidInfo.Result(TopadsBidInfo(data =
-        listOf(TopadsBidInfo.DataItem(suggestionBid = expected))))
-        val onSuccess: (List<TopadsBidInfo.DataItem>) -> Unit = {
-            actual = it[0].suggestionBid
-
-        }
+        val bidInfoData: ResponseBidInfo.Result = ResponseBidInfo.Result(
+            TopadsBidInfo(
+                data =
+                listOf(TopadsBidInfo.DataItem(suggestionBid = expected))
+            )
+        )
         every {
             bidInfoUseCase.executeQuerySafeMode(captureLambda(), any())
         } answers {
-            onSuccess.invoke(bidInfoData.topadsBidInfo.data)
+            firstArg<(ResponseBidInfo.Result) -> Unit>().invoke(bidInfoData)
         }
 
         viewModel.getBidInfo(
-            suggestions = listOf(), onSuccess = onSuccess, onEmpty = {}
+            suggestions = listOf(), onSuccess = {
+                actual = it[0].suggestionBid
+            }, onEmpty = {}
         )
 
         Assert.assertEquals(expected, actual)
@@ -167,8 +182,12 @@ class BudgetingAdsViewModelTest {
     fun `check onSuccess invocation in getBidInfoDefault`() {
         val expected = "1000"
         var actual = "0"
-        val bidInfoData: ResponseBidInfo.Result = ResponseBidInfo.Result(TopadsBidInfo(data =
-        listOf(TopadsBidInfo.DataItem(suggestionBid = expected))))
+        val bidInfoData: ResponseBidInfo.Result = ResponseBidInfo.Result(
+            TopadsBidInfo(
+                data =
+                listOf(TopadsBidInfo.DataItem(suggestionBid = expected))
+            )
+        )
         val onSuccess: (List<TopadsBidInfo.DataItem>) -> Unit = {
             actual = it[0].suggestionBid
 
@@ -176,7 +195,7 @@ class BudgetingAdsViewModelTest {
         every {
             bidInfoUseCaseDefault.executeQuerySafeMode(captureLambda(), any())
         } answers {
-            onSuccess.invoke(bidInfoData.topadsBidInfo.data)
+            firstArg<(ResponseBidInfo.Result) -> Unit>().invoke(bidInfoData)
         }
 
         viewModel.getBidInfoDefault(
@@ -187,7 +206,7 @@ class BudgetingAdsViewModelTest {
     }
 
     @Test
-    fun `check onerror check`() {
+    fun `check onerror check  in getBidInfoDefault`() {
 
         every {
             bidInfoUseCaseDefault.executeQuerySafeMode(any(), captureLambda())
@@ -203,5 +222,78 @@ class BudgetingAdsViewModelTest {
         Assert.assertEquals(null, actual)
     }
 
+    @Test
+    fun `getProductBid failure`() {
+        coEvery {
+            topAdsGetBidSuggestionByProductIDsUseCase.invoke(any(), any())
+        } answers {
+            throw Throwable()
+        }
+        viewModel.getProductBid(listOf("1"))
+        assertTrue(viewModel.bidProductData.value is Fail)
+    }
+
+    @Test
+    fun `getProductBid success`() {
+        val data = Success(
+            TopAdsGetBidSuggestionResponse(
+                topAdsGetBidSuggestionByProductIDs = TopAdsGetBidSuggestionResponse.TopAdsGetBidSuggestionByProductIDs(
+                    bidData = TopAdsGetBidSuggestionResponse.TopAdsGetBidSuggestionByProductIDs.BidData(
+                        bidSuggestion = Int.ZERO
+                    ),
+                    error = TopAdsGetBidSuggestionResponse.TopAdsGetBidSuggestionByProductIDs.Error(
+                        code = String.EMPTY,
+                        detail = String.EMPTY,
+                        title = String.EMPTY
+                    )
+                )
+            )
+        )
+        coEvery {
+            topAdsGetBidSuggestionByProductIDsUseCase.invoke(any(), any())
+        } answers {
+            data
+        }
+        viewModel.getProductBid(listOf())
+        Assert.assertEquals(data, viewModel.bidProductData.value)
+    }
+
+    @Test
+    fun `getPerformanceData failure`() {
+        coEvery {
+            topAdsImpressionPredictionUseCase.invoke(any(), any(), any(), any(), any())
+        } answers {
+            throw Throwable()
+        }
+        viewModel.getPerformanceData(listOf("1"), Float.ZERO, Float.ZERO, Float.ZERO)
+        assertTrue(viewModel.performanceData.value is Fail)
+    }
+
+    @Test
+    fun `getPerformanceData success`() {
+        val data = Success(
+            ImpressionPredictionResponse(
+                umpGetImpressionPrediction = ImpressionPredictionResponse.UmpGetImpressionPrediction(
+                    impressionPredictionData = ImpressionPredictionResponse.UmpGetImpressionPrediction.ImpressionPredictionData(
+                        impression = ImpressionPredictionResponse.UmpGetImpressionPrediction.ImpressionPredictionData.Impression(
+                            finalImpression = Int.ZERO,
+                            increment = Int.ZERO,
+                            oldImpression = Int.ZERO
+                        )
+                    ),
+                    error = ImpressionPredictionResponse.UmpGetImpressionPrediction.Error(
+                        title = String.EMPTY
+                    )
+                )
+            )
+        )
+        coEvery {
+            topAdsImpressionPredictionUseCase.invoke(any(), any(), any(), any(), any())
+        } answers {
+            data
+        }
+        viewModel.getPerformanceData(listOf(), Float.ZERO, Float.ZERO, Float.ZERO)
+        Assert.assertEquals(data, viewModel.performanceData.value)
+    }
 
 }
