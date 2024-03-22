73c2b071e64522e43b277ae11f66ea1369d276a2
Asti <asti.anzaria@tokopedia.com>
2024-03-22 15:24:29 +0700

    feat: ui - stories settings

---
+++ features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/StoriesSettingsScreen.kt
@@ -0,0 +1,93 @@
+package com.tokopedia.stories.widget.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tokopedia.iconunify.IconUnify
+import com.tokopedia.iconunify.compose.NestIcon
+import com.tokopedia.nest.principles.NestTypography
+import com.tokopedia.nest.principles.ui.NestTheme
+import com.tokopedia.unifycomponents.compose.NestCheckbox
+import com.tokopedia.unifycomponents.compose.NestSwitch
+
+/**
+ * @author by astidhiyaa on 3/22/24
+ */
+@Composable
+internal fun StoriesSettingsScreen() {
+    NestTheme(isOverrideStatusBarColor = false) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            NestTypography(
+                modifier = Modifier.padding(bottom = 16.dp),
+                text = "Story",
+                textStyle = NestTheme.typography.display1.copy(fontWeight = FontWeight.Bold),
+            )
+            Row {
+                NestIcon(iconId = IconUnify.SOCIAL_STORY, modifier = Modifier.padding(end = 12.dp))
+                Column {
+                    NestTypography(
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        text = "Buat Story Otomatis",
+                        textStyle = NestTheme.typography.display2.copy(fontWeight = FontWeight.Bold),
+                    )
+                    NestTypography(
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        text = "Story akan otomatis dibuat setiap ada update produk di tokomu. Selengkapnya",
+                        textStyle = NestTheme.typography.display3.copy(color = NestTheme.colors.NN._600),
+                    )
+                    NestTypography(
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        text = "Kategori update produk yang akan jadi Story:",
+                        textStyle = NestTheme.typography.paragraph3,
+                    )
+
+                    LazyColumn {
+                        items(4) {
+                            SettingOptItem()
+                        }
+                    }
+                }
+                NestSwitch(isChecked = false, onCheckedChanged = {})
+            }
+        }
+    }
+}
+
+@Composable
+private fun SettingOptItem() {
+    Row(modifier = Modifier
+        .fillMaxSize()
+        .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        NestTypography(
+            text = "Item",
+            textStyle = NestTheme.typography.paragraph3,
+        )
+        NestCheckbox(isChecked = false, onCheckedChange = {})
+    }
+}
+
+@Preview
+@Composable
+internal fun SettingScreen() {
+    StoriesSettingsScreen()
+}
+
+data class StoriesSettingOpt(
+    val text: String,
+    val isSelected: Boolean,
+) //TODO: need to wait for BE for contract
