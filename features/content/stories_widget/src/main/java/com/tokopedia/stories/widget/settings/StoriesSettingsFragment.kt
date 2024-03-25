c7908ec5504a6762beb739bafcf1505ec4c30250
Asti <asti.anzaria@tokopedia.com>
2024-03-25 13:54:54 +0700

    feat: add todo

--- features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/StoriesSettingsFragment.kt
+++ features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/StoriesSettingsFragment.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 
+/**
+ * Flow: 1) check if shop available (contentCreatorStoryGetAuthorConfig)
+ * 2) get list of options (contentCreatorStoryGetAuthorOptions)
+ * 3) update toggle (contentCreatorStoryUpdateAuthorOptions)
+ */
 class StoriesSettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
