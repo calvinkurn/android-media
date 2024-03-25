29874e7152c3741589ec9f80ac95bafd3a56efd4
calvinkurniawan <calvinkurniawan>
2024-03-25 17:10:03 +0700

    add new field on GQL for browse searchbar

--- features/content/feed/src/main/java/com/tokopedia/feedplus/browse/data/model/HeaderDataModel.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/browse/data/model/HeaderDataModel.kt
@@ -1,4 +1,17 @@
 package com.tokopedia.feedplus.browse.data.model
 
-class HeaderDataModel {
+import com.tokopedia.content.common.model.FeedXHeaderBrowse
+
+class HeaderDataModel(
+    val title: String,
+    val isShowSearchBar: Boolean,
+    val searchBarPlaceholder: String
+) {
+    companion object {
+        fun create(data: FeedXHeaderBrowse): HeaderDataModel {
+            return HeaderDataModel(data.title, data.searchBar.isActive, data.searchBar.placeholder)
+        }
+
+        val DEFAULT = HeaderDataModel("", false, "")
+    }
 }
