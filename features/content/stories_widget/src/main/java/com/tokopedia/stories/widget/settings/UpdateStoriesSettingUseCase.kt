aa029a2910fa56da529b25912952ffcbe6405dad
Asti <asti.anzaria@tokopedia.com>
2024-03-25 15:07:16 +0700

    feat: add UpdateStoriesSettingUseCase

---
+++ features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/UpdateStoriesSettingUseCase.kt
@@ -0,0 +1,58 @@
+package com.tokopedia.stories.widget.settings
+
+import com.google.gson.annotations.SerializedName
+import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
+import com.tokopedia.gql_query_annotation.GqlQuery
+import com.tokopedia.graphql.coroutines.data.extensions.request
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.graphql.data.GqlParam
+import com.tokopedia.graphql.domain.coroutine.CoroutineUseCase
+import javax.inject.Inject
+
+/**
+ * @author by astidhiyaa on 3/25/24
+ */
+class UpdateStoriesSettingUseCase @Inject constructor(
+    private val graphqlRepository: GraphqlRepository,
+    dispatchers: CoroutineDispatchers
+) : CoroutineUseCase<UpdateStoriesSettingUseCase.Param, StoriesSetupOpt>(dispatchers.io) {
+
+    private val queryObject = UpdateStoriesSettingUseCaseQuery()
+
+    @GqlQuery(QUERY_NAME, QUERY)
+    override fun graphqlQuery(): String {
+        return queryObject.getQuery()
+    }
+
+    override suspend fun execute(params: Param): StoriesSetupOpt {
+        return graphqlRepository.request(queryObject, params)
+    }
+
+    data class Param(
+        @SerializedName("req")
+        val req: Author,
+    ) : GqlParam {
+        data class Author(
+            @SerializedName("authorID")
+            val authorId: String,
+            @SerializedName("authorType")
+            val authorType: String,
+            @SerializedName("optionType")
+            val optionType: String,
+            @SerializedName("isDisabled")
+            val isDisabled: Boolean,
+        )
+    }
+
+    companion object {
+        private const val QUERY_NAME = "UpdateStoriesSettingUseCaseQuery"
+        private const val QUERY = """
+                query updateStoriesSetting(req: contentCreatorStorySetAuthorOptions!){
+                contentCreatorStorySetAuthorOptions(req:${'$'}req){
+                    success
+                }
+            }
+        """
+    }
+}
+
