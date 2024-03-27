87afc87a4606d16985ae727caed7aaf1e7b571c0
devarafikry <devara.akmal@bytedance.com>
2024-03-27 14:52:52 +0700

    add slardar monitoring for network

--- customer_mid_app/src/main/java/com/tokopedia/tkpd/utils/SlardarInit.kt
+++ customer_mid_app/src/main/java/com/tokopedia/tkpd/utils/SlardarInit.kt
@@ -145,7 +145,7 @@ object SlardarInit {
                 }
             })
         builder.batteryDetect(true)
-        builder.useDefaultTTNetImpl(false)
+        builder.useDefaultTTNetImpl(true)
         builder.dynamicParams(object : IDynamicParams {
             override fun getCommonParams(): Map<String, String> {
                 return mapOf()
