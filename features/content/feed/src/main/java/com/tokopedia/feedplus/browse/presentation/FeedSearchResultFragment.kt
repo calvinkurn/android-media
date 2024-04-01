d997dfb5b5e524c6aae75c30af978e5930f9e3c6
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-04-01 17:18:37 +0700

    feat: add cassava test on Feed Search Result Page

--- features/content/feed/src/main/java/com/tokopedia/feedplus/browse/presentation/FeedSearchResultFragment.kt
+++ features/content/feed/src/main/java/com/tokopedia/feedplus/browse/presentation/FeedSearchResultFragment.kt
@@ -146,7 +146,7 @@ internal class FeedSearchResultFragment @Inject constructor(
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding.resultRv.removeOnScrollListener(loadMoreListener)
+        binding.rvSearchResult.removeOnScrollListener(loadMoreListener)
 
         _binding = null
     }
@@ -168,7 +168,7 @@ internal class FeedSearchResultFragment @Inject constructor(
             }
         }
 
-        binding.resultRv.let {
+        binding.rvSearchResult.let {
             val layoutManager = GridLayoutManager(context, adapter.spanCount).apply {
                 spanSizeLookup = adapter.getSpanSizeLookup()
             }
@@ -276,12 +276,12 @@ internal class FeedSearchResultFragment @Inject constructor(
     }
 
     private fun showResult() {
-        binding.resultRv.show()
+        binding.rvSearchResult.show()
         binding.errorView.hide()
     }
 
     private fun showError() {
-        binding.resultRv.hide()
+        binding.rvSearchResult.hide()
         binding.errorView.show()
     }
 
