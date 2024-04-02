65e7c3920c1d76b68851506fbca298eb9b85c45a
hansenpw <hansen.wijaya@tokopedia.com>
2024-04-02 11:22:24 +0700

    fix routing

--- features/transaction/checkout/src/main/java/com/tokopedia/checkout/ShipmentActivity.kt
+++ features/transaction/checkout/src/main/java/com/tokopedia/checkout/ShipmentActivity.kt
@@ -67,7 +67,7 @@ open class ShipmentActivity :
                     isPlusSelected,
                     bundle
                 )
-                return checkoutFragment
+                return backupCheckoutFragment
             }
         } else {
             shipmentFragment = ShipmentFragment.newInstance(
