324043015d406bb811ef082ec62361723a782b41
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-04-17 16:30:10 +0700

    fix lint: add suppresslint for invalid data type in GetReportSummaryRequest

--- features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/report/live/GetReportSummaryRequest.kt
+++ features/content/play_broadcaster/src/main/java/com/tokopedia/play/broadcaster/domain/model/report/live/GetReportSummaryRequest.kt
@@ -1,5 +1,6 @@
 package com.tokopedia.play.broadcaster.domain.model.report.live
 
+import android.annotation.SuppressLint
 import com.google.gson.annotations.SerializedName
 import com.tokopedia.graphql.data.GqlParam
 import com.tokopedia.play.broadcaster.domain.model.ContentType
@@ -8,6 +9,7 @@ import com.tokopedia.play.broadcaster.domain.model.ContentType
  * Created by Jonathan Darwin on 08 March 2024
  */
 data class GetReportSummaryRequest(
+    @SuppressLint("Invalid Data Type")
     @SerializedName("contentIDs")
     val contentIDs: List<String>,
 
