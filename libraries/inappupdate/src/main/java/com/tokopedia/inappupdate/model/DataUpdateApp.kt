e8af5f222dc29fb6636063ce2e795de26780f0b7
habibi <habibi@tokopedia.com>
2024-04-22 17:21:48 +0700

    remove unused field

--- libraries/inappupdate/src/main/java/com/tokopedia/inappupdate/model/DataUpdateApp.kt
+++ libraries/inappupdate/src/main/java/com/tokopedia/inappupdate/model/DataUpdateApp.kt
@@ -12,14 +12,6 @@ class DataUpdateApp {
     @Expose
     var latestVersionOptionalUpdate = 0
 
-    @SerializedName("list_version_specific_force_update")
-    @Expose
-    val listVersionSpecificForceUpdate: List<Int> = emptyList()
-
-    @SerializedName("list_version_specific_optional_update")
-    @Expose
-    val listVersionSpecificOptionalUpdate: List<Int> = emptyList()
-
     @SerializedName("link")
     @Expose
     var link: String = ""
@@ -39,14 +31,6 @@ class DataUpdateApp {
     @SerializedName("is_optional_enabled")
     @Expose
     var isIsOptionalEnabled = false
-
-    @SerializedName("is_specific_force_update_enabled")
-    @Expose
-    val isSpecificForceUpdateEnabled = false
-
-    @SerializedName("is_specific_optional_update_enabled")
-    @Expose
-    private val isSpecificOptionalUpdateEnabled = false
 
     @SerializedName("inapp_update_enabled")
     @Expose
