217fa374f5242009d184e001f929f891274519eb
habibi <habibi@tokopedia.com>
2024-04-22 11:33:04 +0700

    remove tkpd design

--- features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/util/ImageUtil.kt
+++ features/promo/tokopoints/src/main/java/com/tokopedia/tokopoints/view/util/ImageUtil.kt
@@ -5,6 +5,7 @@ import android.graphics.ColorMatrixColorFilter
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.Target
+import com.tokopedia.tokopoints.R
 
 object ImageUtil {
     fun dimImage(imageView: ImageView?) {
@@ -26,8 +27,8 @@ object ImageUtil {
             Glide.with(imageView.context)
                 .load(url)
                 .dontAnimate()
-                .placeholder(com.tokopedia.design.R.drawable.ic_loading_image)
-                .error(com.tokopedia.design.R.drawable.ic_loading_image)
+                .placeholder(R.drawable.ic_loading_image)
+                .error(R.drawable.ic_loading_image)
                 .override(Target.SIZE_ORIGINAL,Target.SIZE_ORIGINAL)
                 .into(imageView)
         } catch (e: Exception) {
