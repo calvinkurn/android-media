f80364b95796b3ce79346fa65be1fe8fe7c05f96
isfaaghyth <isfaaghyth@gmail.com>
2024-04-24 15:16:10 +0700

    feat: fixes missing product-widget callback

---
+++ features/discovery/home/src/main/java/com/tokopedia/home/beranda/presentation/view/listener/shorten/item/ProductTwoSquareWidgetCallback.kt
@@ -0,0 +1,6 @@
+package com.tokopedia.home.beranda.presentation.view.listener.shorten.item
+
+import com.tokopedia.home.beranda.listener.HomeCategoryListener
+import com.tokopedia.home_component.viewholders.shorten.viewholder.listener.ProductWidgetListener
+
+class ProductTwoSquareWidgetCallback(val listener: HomeCategoryListener) : ProductWidgetListener
