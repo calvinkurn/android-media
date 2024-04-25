71ed4d808a0dae76505c30c4b050c18935b8457e
Asti <asti.anzaria@tokopedia.com>
2024-04-25 14:43:02 +0700

    feat: change appLink

--- libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
+++ libraries/applink/src/main/java/com/tokopedia/applink/ApplinkConst.java
@@ -374,7 +374,7 @@ public interface ApplinkConst {
     String CONTENT_DETAIL = "tokopedia://content/{post_id}";
     String INTERNAL_CONTENT_DETAIL = "tokopedia-android-internal://content/post-detail/{post_id}";
 
-    String CONTENT_SETTINGS = "tokopedia-android-internal://content/stories-settings";
+    String CONTENT_SETTINGS = "tokopedia-android-internal://content/settings";
 
     String AFFILIATE_PRODUCT_PICKER_FROM_SHOP_NO_PARAM = "tokopedia://productpickerfromshop";
     String AFFILIATE_PRODUCT_PICKER_FROM_SHOP_HOST = "productpickerfromshop";
