1752e4a3a1c73ecbf7d2627e70fab5361fdd8e43
misael-jonathan <misael.jonathan@tokopedia.com>
2024-04-25 14:25:08 +0700

    feat: update instr: instrumentation test update for buyer_order_detail

--- features/ordermanagement/buyer_order_detail/src/androidTest/java/com/tokopedia/buyerorderdetail/stub/detail/di/module/BuyerOrderDetailModuleStub.kt
+++ features/ordermanagement/buyer_order_detail/src/androidTest/java/com/tokopedia/buyerorderdetail/stub/detail/di/module/BuyerOrderDetailModuleStub.kt
@@ -6,8 +6,12 @@ import com.tokopedia.abstraction.common.utils.GraphqlHelper
 import com.tokopedia.buyerorderdetail.common.constants.BuyerOrderDetailMiscConstant
 import com.tokopedia.buyerorderdetail.di.BuyerOrderDetailScope
 import com.tokopedia.buyerorderdetail.stub.common.user.UserSessionStub
+import com.tokopedia.digital.digital_recommendation.di.DigitalRecommendationCacheEnablerQualifier
 import com.tokopedia.graphql.coroutines.domain.interactor.MultiRequestGraphqlUseCase
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
+import com.tokopedia.remoteconfig.FirebaseRemoteConfigImpl
+import com.tokopedia.remoteconfig.RemoteConfig
+import com.tokopedia.remoteconfig.RemoteConfigKey
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
@@ -34,4 +38,18 @@ class BuyerOrderDetailModuleStub {
     @Provides
     fun provideMultiRequestGraphqlUseCase(graphqlRepository: GraphqlRepository): MultiRequestGraphqlUseCase =
         MultiRequestGraphqlUseCase(graphqlRepository)
+
+
+    @BuyerOrderDetailScope
+    @Provides
+    fun provideRemoteConfig(@ApplicationContext context: Context): RemoteConfig {
+        return FirebaseRemoteConfigImpl(context)
+    }
+
+    @BuyerOrderDetailScope
+    @Provides
+    @DigitalRecommendationCacheEnablerQualifier
+    fun provideCacheEnabler(remoteConfig: RemoteConfig): Boolean {
+        return remoteConfig.getBoolean(RemoteConfigKey.ANDROID_ENABLE_DIGITAL_GQL_CACHE, false)
+    }
 }
