80551f1d9146253372856cfb574fb950175edb41
kelvinsaputra <kelvin.saputra@tokopedia.com>
2024-04-26 18:27:04 +0700

    `test`: update instrument test chat filter add broadcast to filter buyer and refactor

--- features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/usecase/GetChatWhitelistFeatureStub.kt
+++ features/communication/topchat/src/androidTest/java/com/tokopedia/topchat/stub/chatlist/usecase/GetChatWhitelistFeatureStub.kt
@@ -1,28 +1,31 @@
 package com.tokopedia.topchat.stub.chatlist.usecase
 
-import com.tokopedia.graphql.coroutines.domain.interactor.GraphqlUseCase
+import com.tokopedia.abstraction.common.di.qualifier.ApplicationContext
+import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.test.application.dispatcher.CoroutineTestDispatchersProvider
 import com.tokopedia.topchat.chatlist.domain.pojo.whitelist.ChatWhitelistFeatureResponse
 import com.tokopedia.topchat.chatlist.domain.usecase.GetChatWhitelistFeature
+import com.tokopedia.topchat.common.data.TopChatResult
+import com.tokopedia.topchat.common.data.asFlowResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetChatWhitelistFeatureStub @Inject constructor(
-    gqlUseCase: GraphqlUseCase<ChatWhitelistFeatureResponse>
-): GetChatWhitelistFeature(gqlUseCase, CoroutineTestDispatchersProvider) {
+    @ApplicationContext private val repository: GraphqlRepository
+) : GetChatWhitelistFeature(repository, CoroutineTestDispatchersProvider) {
 
     var response = ChatWhitelistFeatureResponse()
     var isError = false
     var errorMessage = ""
 
-    override fun getWhiteList(
-        feature: String,
-        onSuccess: (ChatWhitelistFeatureResponse) -> Unit,
-        onError: (Throwable) -> Unit
-    ) {
-        if (isError) {
-            onError(Throwable(errorMessage))
-        } else {
-            onSuccess(response)
-        }
+    override suspend fun execute(params: String): Flow<TopChatResult<ChatWhitelistFeatureResponse>> {
+        return flow {
+            if (isError) {
+                throw Throwable(errorMessage)
+            } else {
+                emit(response)
+            }
+        }.asFlowResult()
     }
 }
