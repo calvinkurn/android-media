289f636ed261b1ae1aa5ba73814e9d0720de3b65
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-05-06 16:24:59 +0700

    refactor: PlayArchivedAnalyticTest.kt - combine all the test case

--- features/content/play/src/androidTest/java/com/tokopedia/play/di/PlayTestModule.kt
+++ features/content/play/src/androidTest/java/com/tokopedia/play/di/PlayTestModule.kt
@@ -14,6 +14,7 @@ import com.tokopedia.abstraction.common.utils.LocalCacheHandler
 import com.tokopedia.atc_common.AtcConstant
 import com.tokopedia.atc_common.domain.mapper.AddToCartDataMapper
 import com.tokopedia.atc_common.domain.usecase.AddToCartUseCase
+import com.tokopedia.content.common.util.Router
 import com.tokopedia.graphql.coroutines.data.GraphqlInteractor
 import com.tokopedia.graphql.coroutines.domain.repository.GraphqlRepository
 import com.tokopedia.graphql.domain.GraphqlUseCase
@@ -44,6 +45,7 @@ import com.tokopedia.user.session.UserSession
 import com.tokopedia.user.session.UserSessionInterface
 import dagger.Module
 import dagger.Provides
+import io.mockk.mockk
 import javax.inject.Named
 
 /**
@@ -202,4 +204,8 @@ class PlayTestModule(
     fun provideActivityResultRegistry(): ActivityResultRegistry {
         return resultRegistry
     }
+
+    @PlayScope
+    @Provides
+    fun provideRouter(): Router = mockk(relaxed = true)
 }
