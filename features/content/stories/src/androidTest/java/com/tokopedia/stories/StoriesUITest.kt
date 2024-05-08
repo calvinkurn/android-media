367541842fd31152ac1e52b62b26f53e92fa3ec0
Jonathan Darwin <jonathan.darwin@tokopedia.com>
2024-05-08 13:55:03 +0700

    fix: stories ui test error

--- features/content/stories/src/androidTest/java/com/tokopedia/stories/StoriesUITest.kt
+++ features/content/stories/src/androidTest/java/com/tokopedia/stories/StoriesUITest.kt
@@ -6,6 +6,7 @@ import com.tokopedia.analyticsdebugger.cassava.cassavatest.CassavaTestRule
 import com.tokopedia.stories.data.mock.mockInitialDataModel
 import com.tokopedia.stories.data.repository.StoriesRepository
 import com.tokopedia.stories.robot.StoriesRobotUITest
+import com.tokopedia.stories.utils.StoriesPreference
 import com.tokopedia.stories.utils.containsEventAction
 import com.tokopedia.stories.view.model.StoriesArgsModel
 import com.tokopedia.test.application.annotations.CassavaTest
@@ -13,6 +14,7 @@ import com.tokopedia.test.application.annotations.UiTest
 import com.tokopedia.user.session.UserSessionInterface
 import io.mockk.coEvery
 import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -35,18 +37,25 @@ class StoriesUITest {
     )
     private val repository: StoriesRepository = mockk(relaxed = true)
     private val userSession: UserSessionInterface = mockk(relaxed = true)
+    private val storiesPref: StoriesPreference = mockk(relaxed = true)
 
     private fun getStoriesRobot() = StoriesRobotUITest(
         args = args,
         repository = repository,
         userSession = userSession,
+        storiesPref = storiesPref,
     )
+
+    @Before
+    fun setUp() {
+        coEvery { storiesPref.hasVisit() } returns true
+    }
 
     @Test
     fun test_openStoriesRoom() {
         val mockData = mockInitialDataModel()
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -68,7 +77,7 @@ class StoriesUITest {
         val mockData = mockInitialDataModel()
         val duration = mockData.groupItems.first().detail.detailItems.first().content.duration
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -84,7 +93,7 @@ class StoriesUITest {
     fun test_tapNextUntilNextGroup() {
         val mockData = mockInitialDataModel()
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -108,7 +117,7 @@ class StoriesUITest {
             selectedDetail = 2,
         )
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -129,7 +138,7 @@ class StoriesUITest {
     fun test_holdToPauseAndResume() {
         val mockData = mockInitialDataModel()
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -140,7 +149,7 @@ class StoriesUITest {
     fun test_tapGroup() {
         val mockData = mockInitialDataModel()
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
@@ -161,7 +170,7 @@ class StoriesUITest {
     fun test_swipeGroup() {
         val mockData = mockInitialDataModel()
 
-        coEvery { repository.getStoriesInitialData(any()) } returns mockData
+        coEvery { repository.getStoriesInitialData(any(), any(), any(), any(), any(), any(), any()) } returns mockData
 
         getStoriesRobot()
             .openStoriesRoom()
