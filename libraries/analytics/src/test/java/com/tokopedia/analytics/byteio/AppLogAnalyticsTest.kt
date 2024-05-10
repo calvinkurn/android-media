0128312706d6de9c4eb70ab55ba5db851eac3397
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2024-05-10 19:07:08 +0700

    Reorganize test

--- libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogAnalyticsTest.kt
+++ libraries/analytics/src/test/java/com/tokopedia/analytics/byteio/AppLogAnalyticsTest.kt
@@ -1,12 +1,12 @@
 package com.tokopedia.analytics.byteio
 
-import com.tokopedia.analytics.byteio.AppLogParam.ENTER_FROM
+import com.tokopedia.analytics.byteio.AppLogAnalytics.addEnterMethodPdp
 import com.tokopedia.analytics.byteio.AppLogParam.PAGE_NAME
 import com.tokopedia.analytics.byteio.search.AppLogSearch.ParamKey.SEARCH_ID
+import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
 import kotlin.test.assertNull
 
 
@@ -15,41 +15,6 @@ class AppLogAnalyticsTest {
     @Before
     fun setup() {
         SUT.clearAllPageData()
-    }
-
-    @Test
-    fun `given data is pushed multiple then data on first stack can be obtained`() {
-        val expected = "search123"
-        SUT.pushPageData(ActivityBasicOne)
-        SUT.putPageData(SEARCH_ID, expected)
-        SUT.pushPageData(ActivityBasicTwo)
-
-        val actual = SUT.getLastData(SEARCH_ID)
-        assertEquals(expected, actual)
-        // making sure data is not from ActivityBasicOne
-        assertEquals(SUT.getLastData(PAGE_NAME), "Page Two")
-    }
-
-    @Test
-    fun `given enterFrom activity is pushed then data can be obtained`() {
-        val expected = "Page EnterFrom"
-        SUT.pushPageData(ActivityEnterFrom)
-        SUT.pushPageData(ActivityBasicOne)
-        SUT.pushPageData(ActivityBasicTwo)
-
-        val actual = SUT.getLastData(ENTER_FROM)
-        assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `given stack is popped with previous shadow then shadow activity is popped`() {
-        SUT.pushPageData(ActivityBasicOne)
-        SUT.pushPageData(ActivityShadow)
-        SUT.pushPageData(ActivityBasicTwo)
-
-        SUT.removePageData(ActivityBasicTwo)
-        val actual = SUT.getLastData(PAGE_NAME)
-        assertEquals(ActivityBasicOne.getPageName(), actual)
     }
 
     @Test
@@ -93,6 +58,19 @@ class AppLogAnalyticsTest {
         val actual = SUT.getDataBeforeStep(SEARCH_ID, 1)
         assertNull(actual)
     }
+
+    @Test
+    fun `given enter method set in first but not second then third struct enterMethodPdp should get null`() {
+        SUT.pushPageData(ActivityBasicOne)
+        SUT.putPageData(AppLogParam.ENTER_METHOD, "entryOne")
+        SUT.pushPageData(ActivityBasicTwo)
+        SUT.pushPageData(ActivityBasicThree)
+        val actual = JSONObject().apply { addEnterMethodPdp() }
+
+        assertNull(actual.getOrNull(AppLogParam.ENTER_METHOD))
+    }
+
+    private fun JSONObject.getOrNull(key: String): Any? = runCatching { get(key) }.getOrNull()
 
 }
 
