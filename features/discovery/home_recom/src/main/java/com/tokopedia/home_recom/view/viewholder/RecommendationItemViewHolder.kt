1ffe64d0d30520c5ae924a8658c5dac747348bd8
rizqiaryansa <rizqi.aryansa@gmail.com>
2024-05-13 12:07:35 +0700

    feat: remove unused import

--- features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/viewholder/RecommendationItemViewHolder.kt
+++ features/discovery/home_recom/src/main/java/com/tokopedia/home_recom/view/viewholder/RecommendationItemViewHolder.kt
@@ -10,8 +10,6 @@ import com.tokopedia.analytics.byteio.topads.AdsLogConst
 import com.tokopedia.home_recom.R
 import com.tokopedia.home_recom.model.datamodel.RecommendationItemDataModel
 import com.tokopedia.kotlin.extensions.view.ViewHintListener
-import com.tokopedia.kotlin.extensions.view.ZERO
-import com.tokopedia.kotlin.extensions.view.addOnAttachStateChangeListener
 import com.tokopedia.kotlin.extensions.view.addOnImpression1pxListener
 import com.tokopedia.productcard.ATCNonVariantListener
 import com.tokopedia.productcard.ProductCardClickListener
