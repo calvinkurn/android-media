f1fac50ea0fe6b068ccd94058946495234b9fe08
aiyingying <aiyingying@bytedance.com>
2024-05-13 15:20:27 +0800

    feat:add btm const data

---
+++ libraries/analytics/src/main/java/com/tokopedia/analytics/btm/Constants.kt
@@ -0,0 +1,18 @@
+package com.tokopedia.analytics.btm
+
+/**
+ *  page definition
+ */
+enum class Page(val str: String) {
+    HOMEPAGE("a87943.b83792"),
+    FEED("a87943.b3679"),
+    WISHLIST("a87943.b0751"),
+    ORDERLIST("a87943.b1605")
+}
+
+/**
+ *  position definition
+ */
+enum class Position(val str: String) {
+    HOMEPAGE_BANNERS_BANNER("a87943.b83792.c0652.d4396")
+}
