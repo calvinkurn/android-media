6eb45f2eb4da8f356750d1a4b66483e4462ea2f9
said.faisal <said.faisal@tokopedia.com>
2024-05-14 05:47:49 +0700

    replace with new byteio integration

--- features/user/home_account/src/main/java/com/tokopedia/home_account/view/adapter/viewholder/ProductItemViewHolder.kt
+++ features/user/home_account/src/main/java/com/tokopedia/home_account/view/adapter/viewholder/ProductItemViewHolder.kt
@@ -1,16 +1,14 @@
 package com.tokopedia.home_account.view.adapter.viewholder
 
 import android.view.View
-import com.tokopedia.abstraction.base.view.adapter.adapter.listener.IAdsViewHolderTrackListener
 import com.tokopedia.adapterdelegate.BaseViewHolder
 import com.tokopedia.analytics.byteio.topads.AdsLogConst
 import com.tokopedia.home_account.R
 import com.tokopedia.home_account.databinding.HomeAccountRecommendationItemProductCardBinding
 import com.tokopedia.home_account.view.listener.HomeAccountUserListener
 import com.tokopedia.kotlin.extensions.view.ViewHintListener
-import com.tokopedia.kotlin.extensions.view.ZERO
-import com.tokopedia.kotlin.extensions.view.addOnAttachStateChangeListener
 import com.tokopedia.productcard.ProductCardClickListener
+import com.tokopedia.productcard.layout.ProductConstraintLayout
 import com.tokopedia.recommendation_widget_common.byteio.sendRealtimeClickAdsByteIo
 import com.tokopedia.recommendation_widget_common.byteio.sendShowAdsByteIo
 import com.tokopedia.recommendation_widget_common.byteio.sendShowOverAdsByteIo
@@ -18,21 +16,10 @@ import com.tokopedia.recommendation_widget_common.extension.toProductCardModel
 import com.tokopedia.recommendation_widget_common.presentation.model.RecommendationItem
 import com.tokopedia.utils.view.binding.viewBinding
 
-class ProductItemViewHolder(itemView: View, val listener: HomeAccountUserListener) :
-    BaseViewHolder(itemView),
-    IAdsViewHolderTrackListener {
-
+class ProductItemViewHolder(itemView: View, val listener: HomeAccountUserListener) : BaseViewHolder(itemView) {
     private val binding: HomeAccountRecommendationItemProductCardBinding? by viewBinding()
 
-    private var visibleViewPercentage: Int = 0
     private var recommendationItem: RecommendationItem? = null
-
-    init {
-        itemView.addOnAttachStateChangeListener(
-            onViewAttachedToWindow = { onViewAttachedToWindow() },
-            onViewDetachedFromWindow = { onViewDetachedFromWindow(visiblePercentage) }
-        )
-    }
 
     fun bind(element: RecommendationItem) {
         recommendationItem = element
@@ -66,23 +53,20 @@ class ProductItemViewHolder(itemView: View, val listener: HomeAccountUserListene
         binding?.productCardView?.setThreeDotsOnClickListener {
             listener.onProductRecommendationThreeDotsClicked(element, adapterPosition)
         }
-    }
 
-    override fun onViewAttachedToWindow() {
-        recommendationItem?.sendShowAdsByteIo(itemView.context)
-    }
+        binding?.productCardView?.setVisibilityPercentListener(
+            isTopAds = element.isTopAds,
+            eventListener = object : ProductConstraintLayout.OnVisibilityPercentChanged {
+                override fun onShow() {
+                    element.sendShowAdsByteIo(itemView.context)
+                }
 
-    override fun onViewDetachedFromWindow(visiblePercentage: Int) {
-        recommendationItem?.sendShowOverAdsByteIo(itemView.context, visiblePercentage)
-        setVisiblePercentage(Int.ZERO)
+                override fun onShowOver(maxPercentage: Int) {
+                    element.sendShowOverAdsByteIo(itemView.context, maxPercentage)
+                }
+            }
+        )
     }
-
-    override fun setVisiblePercentage(visiblePercentage: Int) {
-        this.visibleViewPercentage = visiblePercentage
-    }
-
-    override val visiblePercentage: Int
-        get() = visibleViewPercentage
 
     companion object {
         val LAYOUT = R.layout.home_account_recommendation_item_product_card
