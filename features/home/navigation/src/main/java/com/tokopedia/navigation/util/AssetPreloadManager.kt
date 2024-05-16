35df1830c5dff500286bc18bef86d33b02063abe
Jegul <kenny.hadisaputra@bytedance.com>
2024-05-16 17:24:44 +0700

    feat: use medialoader function to cache image

--- features/home/navigation/src/main/java/com/tokopedia/navigation/util/AssetPreloadManager.kt
+++ features/home/navigation/src/main/java/com/tokopedia/navigation/util/AssetPreloadManager.kt
@@ -1,21 +1,24 @@
 package com.tokopedia.navigation.util
 
 import android.content.Context
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.tokopedia.abstraction.common.dispatcher.CoroutineDispatchers
-import com.tokopedia.media.loader.module.GlideApp
+import com.tokopedia.media.loader.getBitmapImageUrl
+import com.tokopedia.media.loader.utils.MediaBitmapEmptyTarget
+import com.tokopedia.media.loader.wrapper.MediaCacheStrategy
 import com.tokopedia.navigation_common.ui.BottomNavBarAsset.Type
 import com.tokopedia.navigation_common.ui.BottomNavBarUiModel
 import com.tokopedia.navigation_common.util.LottieCacheManager
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 class AssetPreloadManager @Inject constructor(
     private val context: Context,
     private val lottieCacheManager: LottieCacheManager,
-    private val dispatchers: CoroutineDispatchers,
+    private val dispatchers: CoroutineDispatchers
 ) {
 
     suspend fun preloadByCacheTask(cacheTask: CompletableTask<List<BottomNavBarUiModel>>) = withContext(dispatchers.io) {
@@ -33,14 +36,28 @@ class AssetPreloadManager @Inject constructor(
     private suspend fun cacheImageAssets(assets: List<Type.ImageUrl>) = coroutineScope {
         val results = assets.map {
             async {
-                GlideApp.with(context)
-                    .load(it.url)
-                    .diskCacheStrategy(DiskCacheStrategy.DATA)
-                    .submit()
+                suspendCancellableCoroutine { cont ->
+                    it.url.getBitmapImageUrl(
+                        context,
+                        properties = {
+                            setCacheStrategy(MediaCacheStrategy.DATA)
+                        },
+                        target = MediaBitmapEmptyTarget(
+                            onReady = {
+                                cont.resume(it)
+                            },
+                            onFailed = {
+                                cont.resume(null)
+                            }
+                        )
+                    )
+
+                    cont.invokeOnCancellation { }
+                }
             }
         }.map {
             val result = it.await()
-            result.get() != null
+            result != null
         }
 
         results.all { it }
