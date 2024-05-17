29359c37951ab027ea89052900e9e2ba58a37cc2
jonathan.dariwn <jonathan.darwin@bytedance.com>
2024-05-17 11:44:27 +0700

    test: try to remove non-@CassavaTest in broadcaster uitest to see if it impacts the pr-check-cassava duration

--- features/content/play_broadcaster/src/androidTest/java/com/tokopedia/play/broadcaster/setup/productchooser/ProductChooserTest.kt
+++ features/content/play_broadcaster/src/androidTest/java/com/tokopedia/play/broadcaster/setup/productchooser/ProductChooserTest.kt
@@ -19,122 +19,122 @@ import org.junit.runner.RunWith
 /**
  * Created by kenny.hadisaputra on 02/03/22
  */
-@RunWith(AndroidJUnit4ClassRunner::class)
-@UiTest
-class ProductChooserTest {
-
-    private val mockRepo: ContentProductPickerSellerRepository = mockk(relaxed = true)
-    private val mockSection = List(1) {
-        ProductTagSectionUiModel(
-            "", CampaignStatus.Ongoing,
-            List(2) {
-                ProductUiModel(
-                    it.toString(),
-                    "Product $it",
-                    false,
-                    "",
-                    0,
-                    false,
-                    "",
-                    1,
-                    OriginalPrice("Rp1000.00", 1000.0),
-                    PinProductUiModel.Empty,
-                    "",
-                    "",
-                    "",
-                    "",
-                    ""
-                )
-            }
-        )
-    }
-    private val mockProduct = mockSection.flatMap { it.products }
-
-    private val listener = object : ProductChooserBottomSheet.Listener {
-        override fun onSetupCancelled(bottomSheet: ProductChooserBottomSheet) {
-            bottomSheet.dismiss()
-        }
-
-        override fun onSetupSuccess(bottomSheet: ProductChooserBottomSheet) {
-        }
-
-        override fun openCampaignAndEtalaseList(bottomSheet: ProductChooserBottomSheet) {
-        }
-    }
-
-    init {
-        coEvery {
-            mockRepo.getProductsInEtalase(
-                any(),
-                any(),
-                any(),
-                any()
-            )
-        } returns PagedDataUiModel(
-            dataList = mockProduct,
-            hasNextPage = false
-        )
-    }
-
-    @Test
-    fun testCloseBottomSheet_whenNoSelectedProduct() {
-        val robot = ProductChooserRobot(listener) {
-            productSetupViewModel(
-                productSectionList = emptyList(),
-                repo = mockRepo
-            )
-        }
-
-        robot.close()
-        robot.assertExitDialog(isShown = false)
-        robot.assertBottomSheet(isOpened = false)
-    }
-
-    @Test
-    fun testCloseBottomSheet_whenHasSelectedProduct_andThenNoChangeProduct() {
-        val robot = ProductChooserRobot(listener) {
-            productSetupViewModel(
-                productSectionList = mockSection,
-                repo = mockRepo
-            )
-        }
-
-        robot.close()
-        robot.assertExitDialog(isShown = false)
-        robot.assertBottomSheet(isOpened = false)
-        robot.assertExitDialog(isShown = false)
-        robot.assertBottomSheet(isOpened = false)
-    }
-
-    @Test
-    fun testCloseBottomSheet_whenHasSelectedProduct_andThenChangeProduct() {
-        val robot = ProductChooserRobot(listener) {
-            productSetupViewModel(
-                productSectionList = mockSection,
-                repo = mockRepo
-            )
-        }
-
-        robot.selectProduct(0)
-            .close()
-
-        robot.assertExitDialog(isShown = true)
-    }
-
-    @Test
-    fun testCloseBottomSheet_whenHasSelectedProduct_andThenNoMoreProductSelected() {
-        val robot = ProductChooserRobot(listener) {
-            productSetupViewModel(
-                productSectionList = mockSection,
-                repo = mockRepo
-            )
-        }
-
-        robot.selectProduct(0)
-            .selectProduct(1)
-            .close()
-
-        robot.assertExitDialog(isShown = false)
-        robot.assertBottomSheet(isOpened = false)
-    }
-}
+//@RunWith(AndroidJUnit4ClassRunner::class)
+//@UiTest
+//class ProductChooserTest {
+//
+//    private val mockRepo: ContentProductPickerSellerRepository = mockk(relaxed = true)
+//    private val mockSection = List(1) {
+//        ProductTagSectionUiModel(
+//            "", CampaignStatus.Ongoing,
+//            List(2) {
+//                ProductUiModel(
+//                    it.toString(),
+//                    "Product $it",
+//                    false,
+//                    "",
+//                    0,
+//                    false,
+//                    "",
+//                    1,
+//                    OriginalPrice("Rp1000.00", 1000.0),
+//                    PinProductUiModel.Empty,
+//                    "",
+//                    "",
+//                    "",
+//                    "",
+//                    ""
+//                )
+//            }
+//        )
+//    }
+//    private val mockProduct = mockSection.flatMap { it.products }
+//
+//    private val listener = object : ProductChooserBottomSheet.Listener {
+//        override fun onSetupCancelled(bottomSheet: ProductChooserBottomSheet) {
+//            bottomSheet.dismiss()
+//        }
+//
+//        override fun onSetupSuccess(bottomSheet: ProductChooserBottomSheet) {
+//        }
+//
+//        override fun openCampaignAndEtalaseList(bottomSheet: ProductChooserBottomSheet) {
+//        }
+//    }
+//
+//    init {
+//        coEvery {
+//            mockRepo.getProductsInEtalase(
+//                any(),
+//                any(),
+//                any(),
+//                any()
+//            )
+//        } returns PagedDataUiModel(
+//            dataList = mockProduct,
+//            hasNextPage = false
+//        )
+//    }
+//
+//    @Test
+//    fun testCloseBottomSheet_whenNoSelectedProduct() {
+//        val robot = ProductChooserRobot(listener) {
+//            productSetupViewModel(
+//                productSectionList = emptyList(),
+//                repo = mockRepo
+//            )
+//        }
+//
+//        robot.close()
+//        robot.assertExitDialog(isShown = false)
+//        robot.assertBottomSheet(isOpened = false)
+//    }
+//
+//    @Test
+//    fun testCloseBottomSheet_whenHasSelectedProduct_andThenNoChangeProduct() {
+//        val robot = ProductChooserRobot(listener) {
+//            productSetupViewModel(
+//                productSectionList = mockSection,
+//                repo = mockRepo
+//            )
+//        }
+//
+//        robot.close()
+//        robot.assertExitDialog(isShown = false)
+//        robot.assertBottomSheet(isOpened = false)
+//        robot.assertExitDialog(isShown = false)
+//        robot.assertBottomSheet(isOpened = false)
+//    }
+//
+//    @Test
+//    fun testCloseBottomSheet_whenHasSelectedProduct_andThenChangeProduct() {
+//        val robot = ProductChooserRobot(listener) {
+//            productSetupViewModel(
+//                productSectionList = mockSection,
+//                repo = mockRepo
+//            )
+//        }
+//
+//        robot.selectProduct(0)
+//            .close()
+//
+//        robot.assertExitDialog(isShown = true)
+//    }
+//
+//    @Test
+//    fun testCloseBottomSheet_whenHasSelectedProduct_andThenNoMoreProductSelected() {
+//        val robot = ProductChooserRobot(listener) {
+//            productSetupViewModel(
+//                productSectionList = mockSection,
+//                repo = mockRepo
+//            )
+//        }
+//
+//        robot.selectProduct(0)
+//            .selectProduct(1)
+//            .close()
+//
+//        robot.assertExitDialog(isShown = false)
+//        robot.assertBottomSheet(isOpened = false)
+//    }
+//}
--- features/content/play_broadcaster/src/androidTest/java/com/tokopedia/play/broadcaster/setup/swtichaccount/ui/SwitchAccountTest.kt
+++ features/content/play_broadcaster/src/androidTest/java/com/tokopedia/play/broadcaster/setup/swtichaccount/ui/SwitchAccountTest.kt
@@ -24,117 +24,117 @@ import org.junit.runner.RunWith
 /**
  * Created by fachrizalmrsln on 28/09/22
  */
-@UiTest
-@RunWith(AndroidJUnit4ClassRunner::class)
-class SwitchAccountTest {
-
-    private val mockDispatcher: CoroutineDispatchers = CoroutineDispatchersProvider
-    private val mockDataStore = PlayBroadcastDataStoreImpl(
-        mSetupDataStore = PlayBroadcastSetupDataStoreImpl(
-            coverDataStore = CoverDataStoreImpl(mockDispatcher, mockk(relaxed = true)),
-            titleDataStore = TitleDataStoreImpl(dispatcher = mockDispatcher, mockk(relaxed = true)),
-            tagsDataStore = TagsDataStoreImpl(dispatcher = mockDispatcher, mockk(relaxed = true)),
-            scheduleDataStore = BroadcastScheduleDataStoreImpl(mockDispatcher, mockk(relaxed = true)),
-            interactiveDataStore = InteractiveDataStoreImpl(),
-            productTagDataStore = ProductTagDataStoreImpl()
-        ),
-    )
-    private val mockConfigStore: HydraConfigStore = mockk(relaxed = true)
-    private val mockRepo: PlayBroadcastRepository = mockk(relaxed = true)
-    private val mockGetChannelUseCase: GetChannelUseCase = mockk(relaxed = true)
-    private val mockGetAddedTagUseCase: GetAddedChannelTagsUseCase = mockk(relaxed = true)
-    private val mockHydraSharedPreferences: HydraSharedPreferences = mockk(relaxed = true)
-
-    private val mockAddedTag = GetAddedChannelTagsResponse()
-
-    private fun createRobot() = SwitchAccountRobot(
-        dataStore = mockDataStore,
-        hydraConfigStore = mockConfigStore,
-        userSessionInterface = mockk(relaxed = true),
-        dispatcher = mockDispatcher,
-        repo = mockRepo,
-        channelUseCase = mockGetChannelUseCase,
-        addedChannelTagsUseCase = mockGetAddedTagUseCase,
-        sharedPreferences = mockHydraSharedPreferences,
-        playShortsEntryPointRemoteConfig = mockk(relaxed = true),
-    )
-
-    init {
-        coEvery { mockRepo.getBroadcastingConfig(any(), any()) } returns buildBroadcastingConfigUiModel()
-        coEvery { mockRepo.getAccountList() } returns accountListResponse()
-        coEvery {
-            mockRepo.getChannelConfiguration(any(), any())
-        } returns buildConfigurationUiModel(
-            beautificationConfig = BeautificationConfigUiModel.Empty
-        )
-        coEvery { mockGetChannelUseCase.executeOnBackground() } returns channelResponse
-        coEvery { mockGetAddedTagUseCase.executeOnBackground() } returns mockAddedTag
-        coEvery { mockRepo.getProductTagSummarySection(any()) } returns emptyList()
-    }
-
-    @Test
-    fun test_entryPointWhenBothAccountLive() {
-        coEvery {
-            mockRepo.getChannelConfiguration(any(), any())
-        } returns buildConfigurationUiModel(
-            channelStatus = ChannelStatus.Live,
-            beautificationConfig = BeautificationConfigUiModel.Empty
-        )
-
-        createRobot().entryPointWhenBothAccountLive()
-    }
-
-    @Test
-    fun test_switchAccountSellerToBuyer() {
-        createRobot().switchAccountSellerToBuyer()
-    }
-
-    /** Tests are not relevant since UGC is temporarily disabled */
-//    @Test
-//    fun test_switchAccountSellerToBuyerAndNotHaveUsername() {
-//        coEvery { mockRepo.getAccountList() } returns accountListResponse(buyerHasUsername = false, buyerHasAcceptTnc = false)
+//@UiTest
+//@RunWith(AndroidJUnit4ClassRunner::class)
+//class SwitchAccountTest {
 //
-//        createRobot().switchAccountSellerToBuyerAndNotHaveUsername()
+//    private val mockDispatcher: CoroutineDispatchers = CoroutineDispatchersProvider
+//    private val mockDataStore = PlayBroadcastDataStoreImpl(
+//        mSetupDataStore = PlayBroadcastSetupDataStoreImpl(
+//            coverDataStore = CoverDataStoreImpl(mockDispatcher, mockk(relaxed = true)),
+//            titleDataStore = TitleDataStoreImpl(dispatcher = mockDispatcher, mockk(relaxed = true)),
+//            tagsDataStore = TagsDataStoreImpl(dispatcher = mockDispatcher, mockk(relaxed = true)),
+//            scheduleDataStore = BroadcastScheduleDataStoreImpl(mockDispatcher, mockk(relaxed = true)),
+//            interactiveDataStore = InteractiveDataStoreImpl(),
+//            productTagDataStore = ProductTagDataStoreImpl()
+//        ),
+//    )
+//    private val mockConfigStore: HydraConfigStore = mockk(relaxed = true)
+//    private val mockRepo: PlayBroadcastRepository = mockk(relaxed = true)
+//    private val mockGetChannelUseCase: GetChannelUseCase = mockk(relaxed = true)
+//    private val mockGetAddedTagUseCase: GetAddedChannelTagsUseCase = mockk(relaxed = true)
+//    private val mockHydraSharedPreferences: HydraSharedPreferences = mockk(relaxed = true)
+//
+//    private val mockAddedTag = GetAddedChannelTagsResponse()
+//
+//    private fun createRobot() = SwitchAccountRobot(
+//        dataStore = mockDataStore,
+//        hydraConfigStore = mockConfigStore,
+//        userSessionInterface = mockk(relaxed = true),
+//        dispatcher = mockDispatcher,
+//        repo = mockRepo,
+//        channelUseCase = mockGetChannelUseCase,
+//        addedChannelTagsUseCase = mockGetAddedTagUseCase,
+//        sharedPreferences = mockHydraSharedPreferences,
+//        playShortsEntryPointRemoteConfig = mockk(relaxed = true),
+//    )
+//
+//    init {
+//        coEvery { mockRepo.getBroadcastingConfig(any(), any()) } returns buildBroadcastingConfigUiModel()
+//        coEvery { mockRepo.getAccountList() } returns accountListResponse()
+//        coEvery {
+//            mockRepo.getChannelConfiguration(any(), any())
+//        } returns buildConfigurationUiModel(
+//            beautificationConfig = BeautificationConfigUiModel.Empty
+//        )
+//        coEvery { mockGetChannelUseCase.executeOnBackground() } returns channelResponse
+//        coEvery { mockGetAddedTagUseCase.executeOnBackground() } returns mockAddedTag
+//        coEvery { mockRepo.getProductTagSummarySection(any()) } returns emptyList()
 //    }
 //
 //    @Test
-//    fun test_switchAccountSellerToBuyerAndNotAcceptTnc() {
-//        coEvery { mockRepo.getAccountList() } returns accountListResponse(buyerHasAcceptTnc = false)
+//    fun test_entryPointWhenBothAccountLive() {
+//        coEvery {
+//            mockRepo.getChannelConfiguration(any(), any())
+//        } returns buildConfigurationUiModel(
+//            channelStatus = ChannelStatus.Live,
+//            beautificationConfig = BeautificationConfigUiModel.Empty
+//        )
 //
-//        createRobot().switchAccountSellerToBuyerAndNotAcceptTnc()
+//        createRobot().entryPointWhenBothAccountLive()
 //    }
-
-    @Test
-    fun test_switchAccountBuyerToSellerAndNotAllowed() {
-        coEvery {
-            mockRepo.getAccountList()
-        } returns accountListResponse(shopEligible = false).reversed()
-
-        createRobot().switchAccountBuyerToSellerAndNotAllowed()
-    }
-
-    @Test
-    fun test_switchAccountSellerHaveDraft() {
-        coEvery {
-            mockGetChannelUseCase.executeOnBackground()
-        } returns channelWithTitleResponse
-
-        createRobot().switchAccountSellerHaveDraft()
-    }
-
-    @Test
-    fun test_switchAccountBuyerHaveDraft() {
-        coEvery { mockRepo.getAccountList() } returns accountListResponse().reversed()
-
-        coEvery {
-            mockHydraSharedPreferences.getLastSelectedAccountType()
-        } returns ContentCommonUserType.TYPE_USER
-
-        coEvery {
-            mockGetChannelUseCase.executeOnBackground()
-        } returns channelWithTitleResponse
-
-        createRobot().switchAccountBuyerHaveDraft()
-    }
-
-}
+//
+//    @Test
+//    fun test_switchAccountSellerToBuyer() {
+//        createRobot().switchAccountSellerToBuyer()
+//    }
+//
+//    /** Tests are not relevant since UGC is temporarily disabled */
+////    @Test
+////    fun test_switchAccountSellerToBuyerAndNotHaveUsername() {
+////        coEvery { mockRepo.getAccountList() } returns accountListResponse(buyerHasUsername = false, buyerHasAcceptTnc = false)
+////
+////        createRobot().switchAccountSellerToBuyerAndNotHaveUsername()
+////    }
+////
+////    @Test
+////    fun test_switchAccountSellerToBuyerAndNotAcceptTnc() {
+////        coEvery { mockRepo.getAccountList() } returns accountListResponse(buyerHasAcceptTnc = false)
+////
+////        createRobot().switchAccountSellerToBuyerAndNotAcceptTnc()
+////    }
+//
+//    @Test
+//    fun test_switchAccountBuyerToSellerAndNotAllowed() {
+//        coEvery {
+//            mockRepo.getAccountList()
+//        } returns accountListResponse(shopEligible = false).reversed()
+//
+//        createRobot().switchAccountBuyerToSellerAndNotAllowed()
+//    }
+//
+//    @Test
+//    fun test_switchAccountSellerHaveDraft() {
+//        coEvery {
+//            mockGetChannelUseCase.executeOnBackground()
+//        } returns channelWithTitleResponse
+//
+//        createRobot().switchAccountSellerHaveDraft()
+//    }
+//
+//    @Test
+//    fun test_switchAccountBuyerHaveDraft() {
+//        coEvery { mockRepo.getAccountList() } returns accountListResponse().reversed()
+//
+//        coEvery {
+//            mockHydraSharedPreferences.getLastSelectedAccountType()
+//        } returns ContentCommonUserType.TYPE_USER
+//
+//        coEvery {
+//            mockGetChannelUseCase.executeOnBackground()
+//        } returns channelWithTitleResponse
+//
+//        createRobot().switchAccountBuyerHaveDraft()
+//    }
+//
+//}
