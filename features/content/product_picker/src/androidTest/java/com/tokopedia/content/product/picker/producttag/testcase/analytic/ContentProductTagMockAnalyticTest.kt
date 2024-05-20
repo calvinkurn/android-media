588582366782a91462ff7b27d71eb168edbbfbe0
jonathan.dariwn <jonathan.darwin@bytedance.com>
2024-05-20 10:30:26 +0700

    test: create 1 mock test class for @CassavaTest in content/product_picker module

---
+++ features/content/product_picker/src/androidTest/java/com/tokopedia/content/product/picker/producttag/testcase/analytic/ContentProductTagMockAnalyticTest.kt
@@ -0,0 +1,23 @@
+package com.tokopedia.content.product.picker.producttag.testcase.analytic
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.tokopedia.test.application.annotations.CassavaTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Jonathan Darwin on 20 May 2024
+ */
+
+/**
+ * Just for mocking purpose, please remove this when new config is merged to minion-skynet
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+@CassavaTest
+class ContentProductTagMockAnalyticTest {
+
+    @Test
+    fun test_mock() {
+        assert(true)
+    }
+}
