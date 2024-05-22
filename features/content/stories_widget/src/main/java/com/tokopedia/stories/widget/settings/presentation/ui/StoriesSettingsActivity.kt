4696e1cfff4cac4bfdaa7c929b5a1559afcd3ce7
asti.anzaria <asti.anzaria@bytedance.com>
2024-05-22 11:15:51 +0700

    fix: surpress

--- features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/presentation/ui/StoriesSettingsActivity.kt
+++ features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/presentation/ui/StoriesSettingsActivity.kt
@@ -14,6 +14,7 @@ import com.tokopedia.stories.widget.settings.di.DaggerStoriesSettingsComponent
 import com.tokopedia.stories.widget.settings.tracking.StoriesSettingsTracking
 import javax.inject.Inject
 
+@Suppress("LateinitUsage")
 class StoriesSettingsActivity : AppCompatActivity() {
 
     @Inject
