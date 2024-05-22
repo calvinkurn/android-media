5fa87492a12a09573204d4cdbbbdc2d1606496fd
asti.anzaria <asti.anzaria@bytedance.com>
2024-05-22 16:22:23 +0700

    fix: comment #4

--- features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/presentation/ui/StoriesSettingsScreen.kt
+++ features/content/stories_widget/src/main/java/com/tokopedia/stories/widget/settings/presentation/ui/StoriesSettingsScreen.kt
@@ -275,32 +275,3 @@ private fun SettingOptItem(
         })
     }
 }
-
-data class StoriesSettingOpt(
-    val text: String,
-    val optionType: String,
-    val isSelected: Boolean,
-    val lastClicked: Long = 0,
-)
-
-data class StoriesSettingConfig(
-    val articleCopy: String,
-    val articleAppLink: String,
-    val articleWebLink: String,
-    val isEligible: Boolean
-)
-
-data class StoriesSettingsPageUiModel(
-    val options: List<StoriesSettingOpt>, val config: StoriesSettingConfig, val state: ResultState
-) {
-    companion object {
-        val Empty
-            get() = StoriesSettingsPageUiModel(
-                options = emptyList(),
-                config = StoriesSettingConfig("", "", "", false),
-                state = ResultState.Loading
-            )
-    }
-}
-
-data class CoolDownException(override val message: String = "Tunggu 5 detik dulu ya"): MessageErrorException(message)
