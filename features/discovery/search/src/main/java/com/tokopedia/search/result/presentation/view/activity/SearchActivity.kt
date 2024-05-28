f63b53e4b51e6a8282f1737e3a010d12754baae1
ilhamsuaib <ilhamsuaib10@gmail.com>
2024-05-28 22:08:42 +0700

    [srp] feat : configure inbox top nav icon

--- features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.kt
+++ features/discovery/search/src/main/java/com/tokopedia/search/result/presentation/view/activity/SearchActivity.kt
@@ -262,6 +262,7 @@ class SearchActivity :
 
     private fun configureSearchNavigationToolbar() {
         setSearchNavigationToolbar()
+        fetchNotification()
     }
 
     private fun setSearchNavigationToolbar() {
@@ -271,6 +272,7 @@ class SearchActivity :
             it.setToolbarPageName(SearchConstant.SEARCH_RESULT_PAGE)
             it.setIcon(
                 IconBuilder(builderFlags = IconBuilderFlag(pageSource = NavSource.SRP))
+                    .addIcon(IconList.ID_MESSAGE) {}
                     .addIcon(
                         IconList.ID_CART,
                         disableRouteManager = false,
@@ -286,6 +288,12 @@ class SearchActivity :
             )
 
             AppLogSearch.eventCartEntranceShow()
+        }
+    }
+
+    private fun fetchNotification() {
+        if (userSession.isLoggedIn) {
+            searchNavigationToolbar?.updateNotification()
         }
     }
 
