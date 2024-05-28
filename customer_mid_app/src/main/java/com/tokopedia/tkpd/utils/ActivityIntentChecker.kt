b8c5bbf7e3c67aa3ddf66b7a7a2e6f4eb129acd7
rezagama <reza.hidayat@bytedance.com>
2024-05-28 13:18:40 +0700

    Check activity & change flag to Immutable

---
+++ customer_mid_app/src/main/java/com/tokopedia/tkpd/utils/ActivityIntentChecker.kt
@@ -0,0 +1,21 @@
+package com.tokopedia.tkpd.utils
+
+import android.app.Activity
+import android.content.Intent
+import com.tokopedia.applink.constant.DeeplinkConstant
+import com.tokopedia.config.GlobalConfig
+
+@Throws(SecurityException::class)
+fun Activity.checkActivity(block: (Intent) -> Unit) {
+    val packageName = callingActivity?.packageName ?: packageName
+    evaluate(intent, packageName, block)
+}
+
+private fun evaluate(intent: Intent, packageName: String?, block: (Intent) -> Unit) {
+    if(packageName == GlobalConfig.PACKAGE_CONSUMER_APP &&
+        (intent.scheme == null || intent.scheme == DeeplinkConstant.SCHEME_INTERNAL)) {
+        block.invoke(intent)
+    } else {
+        throw SecurityException("Unsafe Intent Launch: $intent")
+    }
+}
