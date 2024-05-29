6302f18315505d02eb24f58c96435a262006f0b2
rezagama <reza.hidayat@bytedance.com>
2024-05-29 20:01:13 +0700

    Change upload chat image PendingIntent to Immutable

--- features/communication/topchat/src/main/java/com/tokopedia/topchat/chatroom/service/UploadImageChatService.kt
+++ features/communication/topchat/src/main/java/com/tokopedia/topchat/chatroom/service/UploadImageChatService.kt
@@ -66,8 +66,8 @@ open class UploadImageChatService : JobIntentServiceX(), CoroutineScope {
             notificationManager = object : UploadImageNotificationManager(this@UploadImageChatService) {
                 override fun getFailedIntent(errorMessage: String): PendingIntent {
                     val intent = createLocalChatRoomIntent()
-                    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        PendingIntent.getActivity(this@UploadImageChatService, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+                    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        PendingIntent.getActivity(this@UploadImageChatService, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
                     } else {
                         PendingIntent.getActivity(this@UploadImageChatService, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
                     }
