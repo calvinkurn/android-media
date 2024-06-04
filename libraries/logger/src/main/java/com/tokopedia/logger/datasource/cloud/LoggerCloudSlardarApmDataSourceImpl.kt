32203a23f3ef40f02e530da5663555a7b7d14bae
kelvinsaputra <kelvin.saputra@tokopedia.com>
2024-06-04 19:58:33 +0700

    `refactor`: add try catch to apm send

--- libraries/logger/src/main/java/com/tokopedia/logger/datasource/cloud/LoggerCloudSlardarApmDataSourceImpl.kt
+++ libraries/logger/src/main/java/com/tokopedia/logger/datasource/cloud/LoggerCloudSlardarApmDataSourceImpl.kt
@@ -7,31 +7,42 @@ import com.tokopedia.logger.model.newrelic.NewRelicBodySdk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
+import timber.log.Timber
 
 class LoggerCloudSlardarApmDataSourceImpl: LoggerCloudSlardarApmDataSource {
 
     private val gson by lazy { Gson() }
     override suspend fun sendApmLogToServer(newRelicSdkList: List<NewRelicBodySdk>): Boolean {
-        return withContext(Dispatchers.IO) {
-            newRelicSdkList.forEach {
-                val logString = gson.toJson(it)
-                val category = JSONObject()
-                category.put(JSON_LOG_KEY, logString)
-                ApmAgent.monitorEvent(SERVICE_NAME, category, null, null)
+        return try {
+            withContext(Dispatchers.IO) {
+                newRelicSdkList.forEach {
+                    val logString = gson.toJson(it)
+                    val category = JSONObject()
+                    category.put(JSON_LOG_KEY, logString)
+                    ApmAgent.monitorEvent(SERVICE_NAME, category, null, null)
+                }
+                true
             }
-            true
+        } catch (throwable: Throwable) {
+            Timber.d(throwable)
+            false
         }
     }
 
     override suspend fun sendApmLogToServer(newRelicApiMap: Map<String, NewRelicBodyApi>): Boolean {
-        return withContext(Dispatchers.IO) {
-            newRelicApiMap.forEach {  (_, it) ->
-                val logString = gson.toJson(it)
-                val category = JSONObject()
-                category.put(JSON_LOG_KEY, logString)
-                ApmAgent.monitorEvent(SERVICE_NAME, category, null, null)
+        return try {
+            withContext(Dispatchers.IO) {
+                newRelicApiMap.forEach {  (_, it) ->
+                    val logString = gson.toJson(it)
+                    val category = JSONObject()
+                    category.put(JSON_LOG_KEY, logString)
+                    ApmAgent.monitorEvent(SERVICE_NAME, category, null, null)
+                }
+                true
             }
-            true
+        } catch (throwable: Throwable) {
+            Timber.d(throwable)
+            false
         }
     }
 
