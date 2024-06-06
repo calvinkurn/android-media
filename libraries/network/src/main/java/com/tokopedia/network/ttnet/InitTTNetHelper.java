a2c1189f6c6f86dd19f252af142f22ebba56d271
devarafikry <devara.akmal@bytedance.com>
2024-06-06 11:20:11 +0700

    enable packageRequestParamters info in ttnet

--- libraries/network/src/main/java/com/tokopedia/network/ttnet/InitTTNetHelper.java
+++ libraries/network/src/main/java/com/tokopedia/network/ttnet/InitTTNetHelper.java
@@ -2,12 +2,15 @@ package com.tokopedia.network.ttnet;
 
 import com.bytedance.apm.ApmAgent;
 import com.bytedance.apm.internal.ApmDelegate;
+import com.bytedance.common.utility.Logger;
 import com.bytedance.common.utility.StringUtils;
 import com.bytedance.frameworks.baselib.network.http.NetworkParams;
 import com.bytedance.frameworks.baselib.network.http.util.HttpStatus;
+import com.bytedance.ttnet.TTNetInit;
 import com.bytedance.ttnet.http.HttpRequestInfo;
 import com.bytedance.ttnet.utils.TtnetUtil;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class InitTTNetHelper {
@@ -44,7 +47,7 @@ public class InitTTNetHelper {
                     if (StringUtils.isEmpty(remoteIp[0])) {
                        remoteIp[0] = info.remoteIp;
                     }
-//                    packageRequestParamters(info, jsonObject);
+                    packageRequestParamters(info, jsonObject);
                     ApmAgent.monitorApiError(duration, sendTime, url, remoteIp[0], traceCode, status, jsonObject);
                     ApmAgent.monitorSLA(duration, sendTime, url, remoteIp[0], traceCode, status, jsonObject);
                  } catch (Throwable throwable) {
@@ -72,5 +75,42 @@ public class InitTTNetHelper {
 
     public static final NetworkParams.ApiProcessHook<HttpRequestInfo> sApiProcessHook = new IESCronetApiProcessHook();
 
+    private static void packageRequestParamters(HttpRequestInfo info, JSONObject jsonObject) {
+        if (info == null || jsonObject == null) {
+            return;
+        }
+        try {
+            jsonObject.put("cronet_plugin_install", TTNetInit.getTTNetDepend().isCronetPluginInstalled());
+            jsonObject.put("appLevelRequestStart", info.appLevelRequestStart);
+            jsonObject.put("beforeAllInterceptors", info.beforeAllInterceptors);
+            jsonObject.put("requestStart", info.requestStart);
+            jsonObject.put("responseBack", info.responseBack);
+            jsonObject.put("completeReadResponse", info.completeReadResponse);
+            jsonObject.put("requestEnd", info.requestEnd);
+            jsonObject.put("recycleCount", info.recycleCount);
+            jsonObject.put("timing_dns", info.dnsTime);
+            jsonObject.put("timing_connect", info.connectTime);
+            jsonObject.put("timing_ssl", info.sslTime);
+            jsonObject.put("timing_send", info.sendTime);
+            jsonObject.put("timing_waiting", info.ttfbMs);
+            jsonObject.put("timing_receive", info.receiveTime);
+            jsonObject.put("timing_total", info.totalTime);
+            jsonObject.put("timing_isSocketReused", info.isSocketReused);
+            jsonObject.put("timing_totalSendBytes", info.sentByteCount);
+            jsonObject.put("timing_totalReceivedBytes", info.receivedByteCount);
+            jsonObject.put("timing_remoteIP", info.remoteIp);
+            jsonObject.put("native_post_task_start", info.nativePostTaskStartTime);
+            jsonObject.put("native_request_start", info.nativeRequestStartTime);
+            jsonObject.put("native_wait_ctx", info.nativeWaitContext);
+            jsonObject.put("request_log", info.requestLog);
+            jsonObject.put("content_type", info.contentType);
+            if (info.extraInfo != null) {
+                jsonObject.put("req_info", info.extraInfo);
+            }
+            jsonObject.put("streaming", info.downloadFile);
+        } catch (JSONException e) {
+            Logger.w(TAG, "[packageRequestParamters] json op error. ", e);
+        }
+    }
 
 }
